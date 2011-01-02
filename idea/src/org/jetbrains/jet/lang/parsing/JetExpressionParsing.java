cd38e28e15d746eac1507417858ff3700d09d037
Andrey Breslav <abreslav@abreslav-laptop.(none)>
2011-01-02 16:46:10 +0300

    A bug in eol enablement fixed

--- idea/src/org/jetbrains/jet/lang/parsing/JetExpressionParsing.java
+++ idea/src/org/jetbrains/jet/lang/parsing/JetExpressionParsing.java
@@ -416,7 +416,7 @@ public class JetExpressionParsing extends AbstractJetParsing {
         }
 
         expect(DOUBLE_ARROW, "Expecting '=>'", TokenSet.create(RBRACE));
-        myBuilder.enableEols();
+        myBuilder.restoreEolsState();
 
         parseExpression();
 
