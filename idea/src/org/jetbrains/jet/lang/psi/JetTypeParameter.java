7727f38d0e32d9d6a1156bfbf1f1905c0110ec3c
Maxim Shafirov <max@maxbook.labs.intellij.net>
2011-01-10 17:11:30 +0300

    type parameter variance and bound

--- idea/src/org/jetbrains/jet/lang/psi/JetTypeParameter.java
+++ idea/src/org/jetbrains/jet/lang/psi/JetTypeParameter.java
@@ -2,6 +2,9 @@ package org.jetbrains.jet.lang.psi;
 
 import com.intellij.lang.ASTNode;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.jet.JetNodeTypes;
+import org.jetbrains.jet.lexer.JetTokens;
 
 /**
  * @author max
@@ -14,5 +17,20 @@ public class JetTypeParameter extends JetNamedDeclaration {
     @Override
     public void accept(JetVisitor visitor) {
         visitor.visitTypeParameter(this);
+    }
+
+    @NotNull
+    public Variance getVariance() {
+        JetModifierList modifierList = getModifierList();
+        if (modifierList == null) return Variance.INVARIANT;
+
+        if (modifierList.hasModifier(JetTokens.OUT_KEYWORD)) return Variance.OUT_VARIANCE;
+        if (modifierList.hasModifier(JetTokens.IN_KEYWORD)) return Variance.IN_VARIANCE;
+        return Variance.INVARIANT;
+    }
+
+    @Nullable
+    public JetTypeReference getExtendsBound() {
+        return (JetTypeReference) findChildByType(JetNodeTypes.TYPE_REFERENCE);
     }
 }
---
+++ idea/src/org/jetbrains/jet/lang/psi/Variance.java
@@ -0,0 +1,10 @@
+package org.jetbrains.jet.lang.psi;
+
+/**
+ * @author max
+ */
+public enum Variance {
+    INVARIANT,
+    IN_VARIANCE,
+    OUT_VARIANCE
+}
