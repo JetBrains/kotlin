823f4ed6deaef9cffe6d1805f869d3ef946e1bd6
Andrey Breslav <Andrey.Breslav@jetbrains.com>
2011-01-18 18:39:56 +0300

    TODO about whitespace placement

--- idea/src/org/jetbrains/jet/lang/parsing/SemanticWhitespaceAwarePsiBuilder.java
+++ idea/src/org/jetbrains/jet/lang/parsing/SemanticWhitespaceAwarePsiBuilder.java
@@ -6,6 +6,8 @@ import com.intellij.lang.PsiBuilder;
  * @author abreslav
  */
 public interface SemanticWhitespaceAwarePsiBuilder extends PsiBuilder {
+    // TODO: comments go to wrong place when an empty element is created
+
     boolean newlineBeforeCurrentToken();
     void disableNewlines();
     void enableNewlines();
