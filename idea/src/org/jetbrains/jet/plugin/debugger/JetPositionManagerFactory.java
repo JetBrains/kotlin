53fb3483a2f146c094326861f3adcb7b9bb59ce3
Dmitry Jemerov <yole@jetbrains.com>
2011-07-15 15:32:27 +0200

    correctly update to API change

--- idea/src/org/jetbrains/jet/plugin/debugger/JetPositionManagerFactory.java
+++ idea/src/org/jetbrains/jet/plugin/debugger/JetPositionManagerFactory.java
@@ -1,15 +1,15 @@
 package org.jetbrains.jet.plugin.debugger;
 
 import com.intellij.debugger.PositionManager;
-//import com.intellij.debugger.PositionManagerFactory;
+import com.intellij.debugger.PositionManagerFactory;
 import com.intellij.debugger.engine.DebugProcess;
 
 /**
  * @author yole
  */
-public class JetPositionManagerFactory { //implements PositionManagerFactory {
-//    @Override
-    public PositionManager create(DebugProcess debugProcess) {
-        return new JetPositionManager(debugProcess);
+public class JetPositionManagerFactory extends PositionManagerFactory {
+    @Override
+    public PositionManager createPositionManager(DebugProcess process) {
+        return new JetPositionManager(process);
     }
 }
