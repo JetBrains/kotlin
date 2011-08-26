de3941338301b4c9b452c53a89256e0082c709fe
Andrey Breslav <abreslav@abreslav-mac.labs.intellij.net>
2011-08-26 19:53:17 +0400

    Commented code removed

--- idea/src/org/jetbrains/jet/lang/psi/JetFunctionLiteralExpression.java
+++ idea/src/org/jetbrains/jet/lang/psi/JetFunctionLiteralExpression.java
@@ -27,65 +27,8 @@ public class JetFunctionLiteralExpression extends JetExpression implements JetDe
         return (JetFunctionLiteral) findChildByType(JetNodeTypes.FUNCTION_LITERAL);
     }
 
-//    //TODO: Due to the lack of multiple implementation inheritance these getters are copied from JetNamedFunction
-//    @Nullable
-//    public JetParameterList getValueParameterList() {
-//        return (JetParameterList) findChildByType(JetNodeTypes.VALUE_PARAMETER_LIST);
-//    }
-//
-//    @NotNull
-//    public List<JetParameter> getValueParameters() {
-//        JetParameterList list = getValueParameterList();
-//        return list != null ? list.getParameters() : Collections.<JetParameter>emptyList();
-//    }
-//
-//    @NotNull
-//    public ASTNode getBodyNode() {
-//        ASTNode answer = getNode().findChildByType(JetNodeTypes.BODY);
-//        assert answer != null;
-//        return answer;
-//    }
-//
-//    @NotNull
-//    public List<JetElement> getBody() {
-//        return PsiTreeUtil.getChildrenOfTypeAsList(getBodyNode().getPsi(), JetElement.class);
-//    }
-//
-//    @Nullable
-//    public JetTypeReference getReceiverTypeRef() {
-//        PsiElement child = getFirstChild();
-//        while (child != null) {
-//            IElementType tt = child.getNode().getElementType();
-//            if (tt == JetTokens.LPAR || tt == JetTokens.COLON) break;
-//            if (child instanceof JetTypeReference) {
-//                return (JetTypeReference) child;
-//            }
-//            child = child.getNextSibling();
-//        }
-//
-//        return null;
-//    }
-//
-//    @Nullable
-//    public JetTypeReference getReturnTypeRef() {
-//        boolean colonPassed = false;
-//        PsiElement child = getFirstChild();
-//        while (child != null) {
-//            IElementType tt = child.getNode().getElementType();
-//            if (tt == JetTokens.COLON) {
-//                colonPassed = true;
-//            }
-//            if (colonPassed && child instanceof JetTypeReference) {
-//                return (JetTypeReference) child;
-//            }
-//            child = child.getNextSibling();
-//        }
-//
-//        return null;
-//    }
-
     @Override
-    public JetExpression getBodyExpression() {
+    public JetBlockExpression getBodyExpression() {
         return getFunctionLiteral().getBodyExpression();
     }
 
