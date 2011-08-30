4f64a6c7e2d83ef6018e2a8f556c6ba093b08ee8
Alex Tkachman <alex.tkachman@gmail.com>
2011-08-30 16:28:49 +0200

    isPrimitive method added

--- idea/src/org/jetbrains/jet/codegen/JetTypeMapper.java
+++ idea/src/org/jetbrains/jet/codegen/JetTypeMapper.java
@@ -47,6 +47,19 @@ public class JetTypeMapper {
         return type == Type.INT_TYPE || type == Type.SHORT_TYPE || type == Type.BYTE_TYPE || type == Type.CHAR_TYPE;
     }
 
+    public static boolean isPrimitive(Type type) {
+        return type == Type.INT_TYPE
+                || type == Type.SHORT_TYPE
+                || type == Type.BYTE_TYPE
+                || type == Type.CHAR_TYPE
+                || type == Type.SHORT_TYPE
+                || type == Type.FLOAT_TYPE
+                || type == Type.DOUBLE_TYPE
+                || type == Type.LONG_TYPE
+                || type == Type.BOOLEAN_TYPE
+                || type == Type.VOID_TYPE;
+    }
+
     static Type psiTypeToAsm(PsiType type) {
         if (type instanceof PsiPrimitiveType) {
             if (type == PsiType.VOID) {
