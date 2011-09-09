93a6f94726bb51bac3c02d4c099ab4f897b141f5
Dmitry Jemerov <yole@jetbrains.com>
2011-09-09 16:59:59 +0200

    register syntax highlighter factory in plugin.xml

--- idea/src/org/jetbrains/jet/plugin/JetLanguage.java
+++ idea/src/org/jetbrains/jet/plugin/JetLanguage.java
@@ -4,22 +4,11 @@
 package org.jetbrains.jet.plugin;
 
 import com.intellij.lang.Language;
-import com.intellij.openapi.fileTypes.SingleLazyInstanceSyntaxHighlighterFactory;
-import com.intellij.openapi.fileTypes.SyntaxHighlighter;
-import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
-import org.jetbrains.annotations.NotNull;
 
 public class JetLanguage extends Language {
     public static JetLanguage INSTANCE = new JetLanguage();
 
     private JetLanguage() {
         super("jet");
-
-        SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this, new SingleLazyInstanceSyntaxHighlighterFactory() {
-          @NotNull
-          protected SyntaxHighlighter createHighlighter() {
-            return new JetHighlighter();
-          }
-        });
     }
 }
