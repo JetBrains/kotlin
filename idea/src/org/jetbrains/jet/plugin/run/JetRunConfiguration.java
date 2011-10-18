407882eb707aaff97d1e42d801d28f98e4c86c70
Dmitry Jemerov <yole@jetbrains.com>
2011-10-18 17:41:47 +0200

    common Java options in Jet run configuration

--- idea/src/org/jetbrains/jet/plugin/run/JetRunConfiguration.java
+++ idea/src/org/jetbrains/jet/plugin/run/JetRunConfiguration.java
@@ -1,8 +1,7 @@
 package org.jetbrains.jet.plugin.run;
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.Executor;
-import com.intellij.execution.JavaRunConfigurationExtensionManager;
+import com.intellij.execution.*;
+import com.intellij.execution.configuration.EnvironmentVariablesComponent;
 import com.intellij.execution.configurations.*;
 import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.runners.ExecutionEnvironment;
@@ -19,12 +18,21 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author yole
  */
-public class JetRunConfiguration extends ModuleBasedConfiguration<RunConfigurationModule> {
+public class JetRunConfiguration extends ModuleBasedConfiguration<RunConfigurationModule> implements CommonJavaRunConfigurationParameters {
     public String MAIN_CLASS_NAME;
+    public String VM_PARAMETERS;
+    public String PROGRAM_PARAMETERS;
+    public String WORKING_DIRECTORY;
+    public boolean ALTERNATIVE_JRE_PATH_ENABLED;
+    public String ALTERNATIVE_JRE_PATH;
+    private Map<String,String> myEnvs = new LinkedHashMap<String, String>();
+    public boolean PASS_PARENT_ENVS = true;
 
     public JetRunConfiguration(String name, RunConfigurationModule runConfigurationModule, ConfigurationFactory factory) {
         super(name, runConfigurationModule, factory);
@@ -47,21 +55,91 @@ public class JetRunConfiguration extends ModuleBasedConfiguration<RunConfigurati
     }
 
     public void readExternal(final Element element) throws InvalidDataException {
-      PathMacroManager.getInstance(getProject()).expandPaths(element);
-      super.readExternal(element);
+        PathMacroManager.getInstance(getProject()).expandPaths(element);
+        super.readExternal(element);
 
-      JavaRunConfigurationExtensionManager.getInstance().readExternal(this, element);
-      DefaultJDOMExternalizer.readExternal(this, element);
-      readModule(element);
+        JavaRunConfigurationExtensionManager.getInstance().readExternal(this, element);
+        DefaultJDOMExternalizer.readExternal(this, element);
+        readModule(element);
+        EnvironmentVariablesComponent.readExternal(element, getEnvs());
     }
 
     public void writeExternal(final Element element) throws WriteExternalException {
-      super.writeExternal(element);
-      JavaRunConfigurationExtensionManager.getInstance().writeExternal(this, element);
-      DefaultJDOMExternalizer.writeExternal(this, element);
-      writeModule(element);
-      PathMacroManager.getInstance(getProject()).collapsePathsRecursively(element);
+        super.writeExternal(element);
+        JavaRunConfigurationExtensionManager.getInstance().writeExternal(this, element);
+        DefaultJDOMExternalizer.writeExternal(this, element);
+        writeModule(element);
+        EnvironmentVariablesComponent.writeExternal(element, getEnvs());
+        PathMacroManager.getInstance(getProject()).collapsePathsRecursively(element);
     }
+
+    public void setVMParameters(String value) {
+      VM_PARAMETERS = value;
+    }
+
+    public String getVMParameters() {
+      return VM_PARAMETERS;
+    }
+
+    public void setProgramParameters(String value) {
+      PROGRAM_PARAMETERS = value;
+    }
+
+    public String getProgramParameters() {
+      return PROGRAM_PARAMETERS;
+    }
+
+    public void setWorkingDirectory(String value) {
+      WORKING_DIRECTORY = ExternalizablePath.urlValue(value);
+    }
+
+    public String getWorkingDirectory() {
+      return ExternalizablePath.localPathValue(WORKING_DIRECTORY);
+    }
+
+    public void setPassParentEnvs(boolean passParentEnvs) {
+      PASS_PARENT_ENVS = passParentEnvs;
+    }
+
+    @NotNull
+    public Map<String, String> getEnvs() {
+      return myEnvs;
+    }
+
+    public void setEnvs(@NotNull final Map<String, String> envs) {
+      this.myEnvs = envs;
+    }
+
+    public boolean isPassParentEnvs() {
+      return PASS_PARENT_ENVS;
+    }
+
+    @Override
+    public String getRunClass() {
+        return MAIN_CLASS_NAME;
+    }
+
+    @Override
+    public String getPackage() {
+        return null;
+    }
+
+    public boolean isAlternativeJrePathEnabled() {
+       return ALTERNATIVE_JRE_PATH_ENABLED;
+     }
+
+     public void setAlternativeJrePathEnabled(boolean enabled) {
+       ALTERNATIVE_JRE_PATH_ENABLED = enabled;
+     }
+
+     public String getAlternativeJrePath() {
+       return ALTERNATIVE_JRE_PATH;
+     }
+
+     public void setAlternativeJrePath(String path) {
+       ALTERNATIVE_JRE_PATH = path;
+     }
+
     @Override
     public RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment executionEnvironment) throws ExecutionException {
         final JavaCommandLineState state = new MyJavaCommandLineState(executionEnvironment);
