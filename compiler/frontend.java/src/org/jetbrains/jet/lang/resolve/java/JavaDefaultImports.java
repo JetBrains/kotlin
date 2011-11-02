c22b0f33917bde256b46f9ee42e9b81e284700c0
Andrey Breslav <abreslav@abreslav-mac.labs.intellij.net>
2011-11-02 19:56:43 +0300

    Commented code removed

--- compiler/frontend.java/src/org/jetbrains/jet/lang/resolve/java/JavaDefaultImports.java
+++ compiler/frontend.java/src/org/jetbrains/jet/lang/resolve/java/JavaDefaultImports.java
@@ -13,8 +13,6 @@ public class JavaDefaultImports {
     public static final ImportingStrategy JAVA_DEFAULT_IMPORTS = new ImportingStrategy() {
         @Override
         public void addImports(Project project, JetSemanticServices semanticServices, BindingTrace trace, WritableScope rootScope) {
-//            scope.importScope(javaSemanticServices.getDescriptorResolver().resolveNamespace("").getMemberScope());
-//            scope.importScope(javaSemanticServices.getDescriptorResolver().resolveNamespace("java.lang").getMemberScope());
             JavaSemanticServices javaSemanticServices = new JavaSemanticServices(project, semanticServices, trace);
             rootScope.importScope(new JavaPackageScope("", null, javaSemanticServices));
             rootScope.importScope(new JavaPackageScope("java.lang", null, javaSemanticServices));
