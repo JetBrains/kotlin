7b668a7e619a365482cba12bd9c15076b7188143
Dmitry Jemerov <yole@jetbrains.com>
2011-11-04 19:14:30 +0100

    equals/hashCode for descriptors

--- idea/src/org/jetbrains/jet/plugin/codeInsight/DescriptorClassMember.java
+++ idea/src/org/jetbrains/jet/plugin/codeInsight/DescriptorClassMember.java
@@ -37,4 +37,21 @@ public class DescriptorClassMember implements ClassMember {
     public DeclarationDescriptor getDescriptor() {
         return myDescriptor;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DescriptorClassMember that = (DescriptorClassMember) o;
+
+        if (myDescriptor != null ? !myDescriptor.equals(that.myDescriptor) : that.myDescriptor != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return myDescriptor != null ? myDescriptor.hashCode() : 0;
+    }
 }
