JetBrains/kotlin

commit 9ef4790976206d26687a39e34cdc9d99ad93ac98

allow null from CallableDescriptor.getReturnType

compiler/frontend/src/org/jetbrains/jet/resolve/DescriptorRenderer.java
@@ -73,7 +73,11 @@ public class DescriptorRenderer implements Renderer {
     }
 
     public String renderType(JetType type) {
-        return escape(type.toString());
+        if (type != null) {
+            return escape("<?>");
+        } else {
+            return escape(type.toString());
+        }
     }
 
     protected String escape(String s) {
@@ -222,7 +226,6 @@ public class DescriptorRenderer implements Renderer {
 
             renderName(descriptor, builder);
             renderValueParameters(descriptor, builder);
-            // TODO: getReturnType may be uninitialized and throw IllegalStateException // stepan.koltsov@ 2011-11-21
             builder.append(" : ").append(escape(renderType(descriptor.getReturnType())));
             return super.visitFunctionDescriptor(descriptor, builder);
         }

compiler/frontend/src/org/jetbrains/jet/lang/descriptors/CallableDescriptor.java
@@ -21,7 +21,9 @@ public interface CallableDescriptor extends DeclarationDescriptor {
     @NotNull
     List<TypeParameterDescriptor> getTypeParameters();
 
-    @NotNull
+    /**
+     * Method may return null for not yet fully initialized object or if error occurred.
+     */
     JetType getReturnType();
 
     @NotNull

compiler/frontend/src/org/jetbrains/jet/lang/descriptors/FunctionDescriptorImpl.java
@@ -120,7 +120,6 @@ public class FunctionDescriptorImpl extends DeclarationDescriptorImpl implements
     }
 
     @Override
-    @NotNull
     public JetType getReturnType() {
         return unsubstitutedReturnType;
     }

compiler/frontend/src/org/jetbrains/jet/lang/descriptors/PropertyDescriptor.java
@@ -113,7 +113,6 @@ public class PropertyDescriptor extends VariableDescriptorImpl implements Callab
         return expectedThisObject;
     }
 
-    @NotNull
     @Override
     public JetType getReturnType() {
         return getOutType();

compiler/frontend/src/org/jetbrains/jet/lang/descriptors/PropertyGetterDescriptor.java
@@ -34,7 +34,6 @@ public class PropertyGetterDescriptor extends PropertyAccessorDescriptor {
         return Collections.emptyList();
     }
 
-    @NotNull
     @Override
     public JetType getReturnType() {
         return returnType;

compiler/frontend/src/org/jetbrains/jet/lang/descriptors/VariableDescriptorImpl.java
@@ -84,7 +84,6 @@ public abstract class VariableDescriptorImpl extends DeclarationDescriptorImpl i
         return ReceiverDescriptor.NO_RECEIVER;
     }
 
-    @NotNull
     @Override
     public JetType getReturnType() {
         return getOutType();

commit 2396287753ac8e2ddfd4d5c3405c808ad14352e4

fix DescriptionRendeder broken in 9ef4790976206d26687a39e34cdc9d99ad93ac98

compiler/frontend/src/org/jetbrains/jet/resolve/DescriptorRenderer.java
@@ -73,7 +73,7 @@ public class DescriptorRenderer implements Renderer {
     }
 
     public String renderType(JetType type) {
-        if (type != null) {
+        if (type == null) {
             return escape("<?>");
         } else {
             return escape(type.toString());