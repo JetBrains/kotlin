992fabaf1f3ca586dcb2eafe887214ab8eab90e3
Andrey Breslav <abreslav@abreslav-mac.labs.intellij.net>
2011-11-25 15:56:15 +0300

    Unused imports removed

--- compiler/backend/src/org/jetbrains/jet/codegen/NamespaceCodegen.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/NamespaceCodegen.java
@@ -13,12 +13,8 @@ import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.commons.InstructionAdapter;
-import sun.jvm.hotspot.debugger.LongHashMap;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.objectweb.asm.Opcodes.*;
 
