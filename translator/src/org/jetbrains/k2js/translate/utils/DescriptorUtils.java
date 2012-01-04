dbf9bbe2bb4462312a81734d96ca75a4fd669e9f
Pavel Talanov <Pavel.Talanov@jetbrains.com>
2012-01-04 15:13:52 +0400

    fix some code after update

--- translator/src/org/jetbrains/k2js/translate/utils/DescriptorUtils.java
+++ translator/src/org/jetbrains/k2js/translate/utils/DescriptorUtils.java
@@ -83,9 +83,9 @@ public final class DescriptorUtils {
     @NotNull
     public static PropertyDescriptor getPropertyByName(@NotNull JetScope scope,
                                                        @NotNull String name) {
-        VariableDescriptor variable = scope.getLocalVariable(name);
+        VariableDescriptor variable = scope.getVariable(name);
         if (variable == null) {
-            variable = org.jetbrains.jet.lang.resolve.DescriptorUtils.filterNonExtensionProperty(scope.getProperties(name));
+            variable = scope.getPropertyByFieldReference(name);
         }
         PropertyDescriptor descriptor = (PropertyDescriptor) variable;
         assert descriptor != null : "Must have a descriptor.";
