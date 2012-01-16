d51c030803cc70ddd7f724f5562f207f90cefe0a
Maxim Shafirov <max@jetbrains.com>
2012-01-16 20:59:06 +0400

    New module script DSL

--- stdlib/src/jet/modules/Module.java
+++ stdlib/src/jet/modules/Module.java
@@ -5,9 +5,8 @@ import java.util.List;
 /**
  * @author yole
  */
-public interface IModuleBuilder {
+public interface Module {
     String getModuleName();
     List<String> getSourceFiles();
     List<String> getClasspathRoots();
-    String getJarName();
 }
