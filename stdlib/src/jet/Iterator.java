e6b859f7c04b5fcf9777fa3e3f8db8e7abd9c318
Stepan Koltsov <stepan.koltsov@jetbrains.com>
2012-01-18 12:07:15 +0400

    temporary hack for jet.Iterator.hasNext property
    * javabean property reading will be implemented later
    * jet.Iterator will be converted to Islam

--- stdlib/src/jet/Iterator.java
+++ stdlib/src/jet/Iterator.java
@@ -1,6 +1,9 @@
 package jet;
 
+import jet.runtime.typeinfo.JetMethod;
+
 public interface Iterator<T> extends JetObject {
+    @JetMethod(kind = JetMethod.KIND_PROPERTY)
     boolean getHasNext();
     T next ();
 }
