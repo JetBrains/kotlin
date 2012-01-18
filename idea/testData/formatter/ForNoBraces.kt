dd1fd264615e6ae486f5fc3aaa0991fae02d72ad
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-01-18 19:27:41 +0400

    KT-1053  Incorrect auto formatting of when expression

---
+++ idea/testData/formatter/ForNoBraces.kt
@@ -0,0 +1,6 @@
+fun testSome() {
+var a = 12
+for (var a in 1..10)
+a = 11
+a = 10
+}
---
+++ idea/tests/org.jetbrains.jet.formatter/KotlinFormatterTest.java
@@ -0,0 +1,30 @@
+package org.jetbrains.jet.formatter;
+
+/**
+ * Based on com.intellij.psi.formatter.java.JavaFormatterTest
+ */
+public class KotlinFormatterTest extends AbstractKotlinFormatterTest {
+    public void testBlockFor() throws Exception {
+        doTest();
+    }
+
+    public void testClass() throws Exception {
+        doTest();
+    }
+
+    public void testForNoBraces() throws Exception {
+        doTest();
+    }
+
+    public void testIf() throws Exception {
+        doTest();
+    }
+
+    public void testWhen() throws Exception {
+        doTest();
+    }
+
+    public void testWhenEntryExpr() throws Exception {
+        doTest();
+    }
+}
