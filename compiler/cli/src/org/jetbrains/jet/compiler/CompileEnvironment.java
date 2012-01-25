aca633cae0e06dd643a0781bd52266a4b6c5b233
Maxim Shafirov <max@jetbrains.com>
2012-01-25 14:49:41 +0400

    Attempt to fix CompileEnvironmentTest

--- compiler/cli/src/org/jetbrains/jet/compiler/CompileEnvironment.java
+++ compiler/cli/src/org/jetbrains/jet/compiler/CompileEnvironment.java
@@ -154,7 +154,7 @@ public class CompileEnvironment {
     }
 
     private List<Module> runDefineModules(String moduleFile, ClassFileFactory factory) {
-        ClassLoader loader = myStdlib != null ? new GeneratedClassLoader(factory, new URLClassLoader(new URL[] {myStdlib})) : new GeneratedClassLoader(factory);
+        ClassLoader loader = myStdlib != null ? new GeneratedClassLoader(factory, new URLClassLoader(new URL[] {myStdlib}, AllModules.class.getClassLoader())) : new GeneratedClassLoader(factory);
         try {
             Class namespaceClass = loader.loadClass(JvmAbi.PACKAGE_CLASS);
             final Method method = namespaceClass.getDeclaredMethod("project");
