ca1abed3a9f09162f91288d0fc0d67b9c8a90512
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-01-27 18:30:18 +0400

    Added example-vfs project written on Kotlin.

---
+++ examples/example-vfs/src/Main.kt
@@ -0,0 +1,36 @@
+package org.jetbrains.jet.samples.vfs.sandbox;
+
+import org.jetbrains.jet.samples.vfs.*;
+import java.io.InputStreamReader
+import java.util.Scanner
+
+/**
+ * Receives list of directories which should be watched, adds them to virtual file system,
+ * adds virtual file system listener and waits for 1 minutes, printing out all received event.
+ */
+fun main(args : Array<String>) {
+    if (args.size == 0) {
+        println("Provide list of watched directories as command line arguments")
+        return
+    }
+
+    // add watched directory
+    FileSystem.write {
+        for (val arg in args) {
+            FileSystem.addWatchedDirectory(FileSystem.getFileByPath(arg))
+        }
+    }
+
+    // add listener which prints out everything
+    FileSystem.addVirtualFileListener(object : VirtualFileListener {
+        override fun eventHappened(event: VirtualFileEvent) {
+            println(event)
+            if (event is VirtualFileChangedEvent) {
+                println("new file size is ${event.file.size()}")
+            }
+        }
+    })
+
+    // wait for 1 minute
+    Thread.sleep(60000)
+}
---
+++ examples/example-vfs/src/Utils.kt
@@ -0,0 +1,42 @@
+package org.jetbrains.jet.samples.vfs.utils;
+
+import java.util.concurrent.locks.Lock
+import java.util.List
+import std.util.*
+
+/**
+ * Executes task with given lock acquired.
+ */
+private fun locked<T>(lock : Lock, body : () -> T) : T {
+    lock.lock()
+    try {
+        return body()
+    } finally {
+        lock.unlock();
+    }
+}
+
+/**
+ * Checks boolean value which should be true. If it is false, throws Assertion Error
+ *
+ * @param value value to be checked
+ */
+public fun assert(value : Boolean) {
+    if (!value) {
+        throw AssertionError()
+    }
+}
+
+/**
+ * Returns list containing all elements which first contains and second does not.
+ */
+public fun listDifference<T>(first : List<T>, second : List<T>) : List<T> {
+    return first.filter{ !second.contains(it) }.toList()
+}
+
+/**
+ * Returns list containing all elements which both lists contain.
+ */
+public fun listIntersection<T>(first : List<T>, second : List<T>) : List<T> {
+    return first.filter{ second.contains(it) }.toList()
+}
