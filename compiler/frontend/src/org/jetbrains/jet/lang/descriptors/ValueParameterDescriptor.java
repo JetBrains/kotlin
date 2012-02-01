4a86f347b1cf706eb1aa1caa3eef46fc73c6287d
Alex Tkachman <alex.tkachman@gmail.com>
2012-02-01 10:03:30 +0200

    ValueParameterDescriptor must implement Annotated

--- compiler/frontend/src/org/jetbrains/jet/lang/descriptors/ValueParameterDescriptor.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/descriptors/ValueParameterDescriptor.java
@@ -2,12 +2,13 @@ package org.jetbrains.jet.lang.descriptors;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.jet.lang.descriptors.annotations.Annotated;
 import org.jetbrains.jet.lang.types.JetType;
 
 /**
  * @author abreslav
  */
-public interface ValueParameterDescriptor extends VariableDescriptor {
+public interface ValueParameterDescriptor extends VariableDescriptor, Annotated {
     /**
      * Returns the 0-based index of the value parameter in the parameter list of its containing function.
      *
