aa30c264c0d3a4f0dc54952db6ec343f1433bd93
Pavel Talanov <Pavel.Talanov@jetbrains.com>
2012-02-02 15:05:40 +0400

    added canvas_helper file

---
+++ translator/testFiles/canvas_helper.js
@@ -0,0 +1,15 @@
+function getContext() {
+    return document.getElementsByTagName('canvas')[0].getContext('2d');
+}
+
+function random() {
+    return Math.random();
+}
+
+function getCanvasWidth() {
+    return document.getElementsByTagName('canvas')[0].width;
+}
+
+function getCanvasHeight() {
+    return document.getElementsByTagName('canvas')[0].height;
+}
