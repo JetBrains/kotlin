56fa9b29f2364916c5a13afbe1f7f7b289a0379d
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-02-16 16:05:38 +0400

    KT-1229  Packages should be showed in package derictive - more files

---
+++ idea/testData/completion/basic/InMiddleOfPackage.kt
@@ -0,0 +1,10 @@
+package java.ut<caret>il.
+
+import java.util.AbstractList
+
+class Test {
+
+}
+
+// EXIST: util
+// NUMBER: 1
---
+++ idea/testData/completion/basic/InPackage.kt
@@ -0,0 +1,10 @@
+package java.util.<caret>
+
+import java.util.AbstractList
+
+class Test {
+
+}
+
+// EXIST: concurrent
+// ABSENT: AbstractList, Date, Random, import
