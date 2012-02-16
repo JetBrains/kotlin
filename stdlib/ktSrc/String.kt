795b30e7b0f9de24b899461eea666dc1f14f55e2
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-02-16 17:10:45 +0400

    KT-1259  Parameter info fails to extract parameter names from java method

--- stdlib/ktSrc/String.kt
+++ stdlib/ktSrc/String.kt
@@ -2,19 +2,19 @@ package std
 
 import java.io.StringReader
 
-inline fun String.lastIndexOf(s: String)  = (this as java.lang.String).lastIndexOf(s)
+inline fun String.lastIndexOf(str: String)  = (this as java.lang.String).lastIndexOf(str)
 
-inline fun String.lastIndexOf(s: Char) = (this as java.lang.String).lastIndexOf(s.toString())
+inline fun String.lastIndexOf(ch: Char) = (this as java.lang.String).lastIndexOf(ch.toString())
 
-inline fun String.equalsIgnoreCase(s: String) = (this as java.lang.String).equalsIgnoreCase(s)
+inline fun String.equalsIgnoreCase(anotherString: String) = (this as java.lang.String).equalsIgnoreCase(anotherString)
 
-inline fun String.indexOf(s : String) = (this as java.lang.String).indexOf(s)
+inline fun String.indexOf(str : String) = (this as java.lang.String).indexOf(str)
 
-inline fun String.indexOf(p0 : String, p1 : Int) = (this as java.lang.String).indexOf(p0, p1)
+inline fun String.indexOf(str : String, fromIndex : Int) = (this as java.lang.String).indexOf(str, fromIndex)
 
-inline fun String.replace(s: Char, s1 : Char) = (this as java.lang.String).replace(s, s1).sure()
+inline fun String.replace(oldChar: Char, newChar : Char) = (this as java.lang.String).replace(oldChar, newChar).sure()
 
-inline fun String.replaceAll(s: String, s1 : String) = (this as java.lang.String).replaceAll(s, s1).sure()
+inline fun String.replaceAll(regex: String, replacement : String) = (this as java.lang.String).replaceAll(regex, replacement).sure()
 
 inline fun String.trim() = (this as java.lang.String).trim().sure()
 
@@ -28,13 +28,13 @@ inline fun String.getBytes() = (this as java.lang.String).getBytes().sure()
 
 inline fun String.toCharArray() = (this as java.lang.String).toCharArray().sure()
 
-inline fun String.format(s : String, vararg objects : Any?)  = java.lang.String.format(s, objects).sure()
+inline fun String.format(format : String, vararg args : Any?)  = java.lang.String.format(format, args).sure()
 
-inline fun String.split(s : String)  = (this as java.lang.String).split(s)
+inline fun String.split(regex : String)  = (this as java.lang.String).split(regex)
 
-inline fun String.substring(i : Int) = (this as java.lang.String).substring(i).sure()
+inline fun String.substring(beginIndex : Int) = (this as java.lang.String).substring(beginIndex).sure()
 
-inline fun String.substring(i0 : Int, i1 : Int)  = (this as java.lang.String).substring(i0, i1).sure()
+inline fun String.substring(beginIndex : Int, endIndex : Int)  = (this as java.lang.String).substring(beginIndex, endIndex).sure()
 
 inline fun String.startsWith(prefix: String) = (this as java.lang.String).startsWith(prefix)
 
@@ -42,21 +42,21 @@ inline fun String.startsWith(prefix: String, toffset: Int) = (this as java.lang.
 
 inline fun String.contains(seq: CharSequence) : Boolean = (this as java.lang.String).contains(seq)
 
-inline fun String.endsWith(seq: String) : Boolean = (this as java.lang.String).endsWith(seq)
+inline fun String.endsWith(suffix: String) : Boolean = (this as java.lang.String).endsWith(suffix)
 
 inline val String.size : Int
-    get() = length()
+get() = length()
 
 inline val String.reader : StringReader
-    get() = StringReader(this)
+get() = StringReader(this)
 
 // "constructors" for String
 
-inline fun String(bytes : ByteArray, i : Int, i1 : Int, s : String) = java.lang.String(bytes, i, i1, s) as String
+inline fun String(bytes : ByteArray, offset : Int, length : Int, charsetName : String) = java.lang.String(bytes, offset, length, charsetName) as String
 
-inline fun String(bytes : ByteArray, i : Int, i1 : Int, charset : java.nio.charset.Charset) = java.lang.String(bytes, i, i1, charset) as String
+inline fun String(bytes : ByteArray, offset : Int, length : Int, charset : java.nio.charset.Charset) = java.lang.String(bytes, offset, length, charset) as String
 
-inline fun String(bytes : ByteArray, s : String?) = java.lang.String(bytes, s) as String
+inline fun String(bytes : ByteArray, charsetName : String?) = java.lang.String(bytes, charsetName) as String
 
 inline fun String(bytes : ByteArray, charset : java.nio.charset.Charset) = java.lang.String(bytes, charset) as String
 
@@ -74,10 +74,10 @@ inline fun String(stringBuilder : java.lang.StringBuilder) = java.lang.String(st
 Iterator for characters of given CharSequence
 */
 inline fun CharSequence.iterator() : CharIterator = object: jet.CharIterator() {
-    private var index = 0
+   private var index = 0
 
-    public override fun nextChar(): Char = get(index++)
+   public override fun nextChar(): Char = get(index++)
 
-    public override val hasNext: Boolean
-        get() = index < length
+   public override val hasNext: Boolean
+   get() = index < length
 }
