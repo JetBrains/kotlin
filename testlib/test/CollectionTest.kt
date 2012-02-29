13ee00a5aa2d14c9a4541dfc088f73a50ca1c508
James Strachan <james.strachan@gmail.com>
2012-02-29 09:19:44 +0000

    added test case demonstrating subscript operators on lists

--- testlib/test/CollectionTest.kt
+++ testlib/test/CollectionTest.kt
@@ -246,6 +246,29 @@ class CollectionTest() : TestSupport() {
         fails { hashSet<Char>().last() }
     }
 
+    fun testSubscript() {
+        val list = arrayList("foo", "bar")
+        assertEquals("foo", list[0])
+        assertEquals("bar", list[1])
+
+        // lists throw an exception if out of range
+        fails {
+            assertEquals(null, list[2])
+        }
+
+        // lets try update the list
+        list[0] = "new"
+        list[1] = "thing"
+
+        // lists don't allow you to set past the end of the list
+        fails {
+            list[2] = "works"
+        }
+
+        list.add("works")
+        assertEquals(arrayList("new", "thing", "works"), list)
+    }
+
     fun testIndices() {
         val indices = data.indices
         assertEquals(0, indices.start)
