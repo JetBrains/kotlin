821a127655f7ff99a7a20735d4e581290b2194b0
James Strachan <james.strachan@gmail.com>
2012-02-29 11:09:06 +0000

    temporily remove the map function until the code geneartion is working again

--- stdlib/ktSrc/generated/ArraysFromJavaCollections.kt
+++ stdlib/ktSrc/generated/ArraysFromJavaCollections.kt
@@ -2,10 +2,3 @@
 package std
 
 import java.util.*
-
-/** Returns a new collection containing the results of applying the given function to each element in this collection */
-inline fun <T, R> Array<T>.map(result: Collection<R> = ArrayList<R>(this.size), transform : (T) -> R) : Collection<R> {
-  for (item in this)
-    result.add(transform(item))
-  return result
-}
