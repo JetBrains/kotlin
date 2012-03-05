87b193a15535a4c4c764b55561f9acb539b9ea9d
James Strachan <james.strachan@gmail.com>
2012-03-05 08:57:58 +0000

    show local function with javaClass<T>() works fine for KT-1515

--- testlib/test/language/JavaClassTest.kt
+++ testlib/test/language/JavaClassTest.kt
@@ -5,9 +5,16 @@ import kotlin.test.*
 
 import org.jetbrains.kotlin.support.*
 
+fun localUseWorks(): Unit {
+    val c = javaClass<Runnable>()
+    println("class is $c")
+}
+
 class JavaClassTest : TestCase() {
 
     fun testJavaClass() {
+        localUseWorks()
+
         // TODO this function fails!
         // see KT-1515
         // loadAsserter()
