c2026b89b856b3ee7e9923295dbfb885fa7073e8
Stepan Koltsov <stepan.koltsov@jetbrains.com>
2012-03-07 04:07:10 +0400

    missing test in ReadJavaBinaryClassTest

---
+++ compiler/testData/readJavaBinaryClass/javaBean/JavaBeanAbstractGetter.java
@@ -0,0 +1,5 @@
+package test;
+
+interface JavaBeanAbstractGetter {
+    int getRed();
+}
---
+++ compiler/testData/readJavaBinaryClass/javaBean/JavaBeanAbstractGetter.kt
@@ -0,0 +1,6 @@
+package test
+
+trait JavaBeanAbstractGetter {
+    //final val red: Int
+    abstract fun getRed(): Int
+}
