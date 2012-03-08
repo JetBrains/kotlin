dba869a49d9b0530ad114ee4cdd02534b5fe8866
Hiram Chirino <hiram@hiramchirino.com>
2012-03-08 15:11:51 -0500

    Adding check method to stdlib as detailed in KT-1530

--- libraries/testlib/test/PreconditionsTest.kt
+++ libraries/testlib/test/PreconditionsTest.kt
@@ -36,14 +36,66 @@ class PreconditionsTest() : TestCase() {
         }
     }
 
-//    // Not sure why the assert method is not being resolved.
-//    fun ignorePassingAssert() {
-//        assert(true)
+// TODO: uncomment when KT-1540 is resolved.
+//    fun testPassingCheck() {
+//        check1(true)
 //    }
 //
-//    fun ignoreFailingAssert() {
-//        failsWith<AssertionError> {
-//            assert(false)
+//    fun testFailingCheck() {
+//        val error = fails {
+//            check1(false)
+//        }
+//        if(error is IllegalStateException) {
+//            assertNull(error.getMessage())
+//        } else {
+//            fail("Invalid exception type: "+error)
+//        }
+//    }
+//
+//    fun testPassingCheckWithMessage() {
+//        check2(true, "Hello")
+//    }
+//
+//    fun testFailingCheckWithMessage() {
+//        val error = fails {
+//            check2(false, "Hello")
+//        }
+//        if(error is IllegalStateException) {
+//            assertEquals("Hello", error.getMessage())
+//        } else {
+//            fail("Invalid exception type: "+error)
+//        }
+//    }
+
+
+// TODO: uncomment when KT-1540 is resolved.
+//    fun testPassingAssert() {
+//        assert1(true)
+//    }
+//
+//    fun testFailingAssert() {
+//        val error = fails {
+//            assert1(false)
+//        }
+//        if(error is IllegalStateException) {
+//            assertNull(error.getMessage())
+//        } else {
+//            fail("Invalid exception type: "+error)
+//        }
+//    }
+//
+//    fun testPassingAssertWithMessage() {
+//        assert2(true, "Hello")
+//    }
+//
+//    fun testFailingAssertWithMessage() {
+//        val error = fails {
+//            assert2(false, "Hello")
+//        }
+//        if(error is IllegalStateException) {
+//            assertEquals("Hello", error.getMessage())
+//        } else {
+//            fail("Invalid exception type: "+error)
 //        }
 //    }
 }
