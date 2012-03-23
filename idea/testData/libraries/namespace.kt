dc289d0e846c9661a300489171f4fe96c316e04e
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-03-23 19:53:38 +0400

    Added overloaded and extension functions to LibrariesTest.

--- idea/testData/libraries/namespace.kt
+++ idea/testData/libraries/namespace.kt
@@ -3,8 +3,20 @@
 
 package testData.libraries
 
+[final val <T : jet.Any?>#(T, T).exProp : jet.String] /* compiled code */
+
+[final val jet.Int.exProp : jet.Int] /* compiled code */
+
+[final val jet.String.exProp : jet.String] /* compiled code */
+
 [final val globalVal : #(jet.Int, jet.String)] /* compiled code */
 
 [final val globalValWithGetter : jet.Long] /* compiled code */
+
+[final fun func() : Unit { /* compiled code */ }]
+
+[final fun func(val a : jet.Int, val b : jet.Int) : Unit { /* compiled code */ }]
+
+[final fun func(val a : jet.Int, val b : jet.String) : Unit { /* compiled code */ }]
 
 [final fun main(val args : jet.Array<jet.String>) : Unit { /* compiled code */ }]
