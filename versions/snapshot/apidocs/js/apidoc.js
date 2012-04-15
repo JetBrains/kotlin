7c93f7bb77d5aa20cd7150d4d41db815595a44cb
James Strachan <james.strachan@gmail.com>
2012-04-15 08:08:36 +0100

    Wagon: Deploying site to repository

--- versions/snapshot/apidocs/js/apidoc.js
+++ versions/snapshot/apidocs/js/apidoc.js
@@ -5,4 +5,37 @@ $(function(){
       function() { $(this).children("div.source-detail").show(); },
       function() { $(this).children("div.source-detail").hide(); }
  	);
+
+    var searchUrl = $( "#searchBox" ).attr("data-url");
+    $.ajax({
+        url: searchUrl,
+        dataType: "xml",
+        success: function( xmlResponse ) {
+            var data = $( "search", xmlResponse ).map(function() {
+                return {
+                    value: $( "name", this ).text(),
+                    id: $( "href", this ).text()
+                };
+            }).get();
+            $( "#searchBox" ).autocomplete({
+                source: data,
+                minLength: 0,
+                select: function( event, ui ) {
+                    if (ui.item) {
+                        var url = ui.item.id
+                        if (url) {
+                            // lets append it to the search url
+                            var idx = searchUrl.lastIndexOf("/");
+                            var dir = ""
+                            if (idx > 0) {
+                                dir = searchUrl.slice(0, idx) + "/";
+                            }
+                            var fullUrl = dir + url;
+                            window.location.href = fullUrl
+                        }
+                    }
+                }
+            });
+        }
+    });
 });
