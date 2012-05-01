427afefce40b1ddb8ba109169e937d231102d1ff
Stepan Koltsov <stepan.koltsov@jetbrains.com>
2012-05-02 00:01:04 +0400

    replace .sure with !!

--- compiler/tests/org/jetbrains/jet/codegen/ArrayGenTest.java
+++ compiler/tests/org/jetbrains/jet/codegen/ArrayGenTest.java
@@ -246,7 +246,7 @@ public class ArrayGenTest extends CodegenTestCase {
     public void testCollectionAssignGetMultiIndex () throws Exception {
             loadText("import java.util.ArrayList\n" +
                          "fun box() : String { val s = ArrayList<String>(1); s.add(\"\"); s [1, -1] = \"5\"; s[2, -2] += \"7\"; return s[2,-2] }\n" +
-                         "fun ArrayList<String>.get(index1: Int, index2 : Int) = this[index1+index2].sure()\n" +
+                         "fun ArrayList<String>.get(index1: Int, index2 : Int) = this[index1+index2]!!\n" +
                          "fun ArrayList<String>.set(index1: Int, index2 : Int, elem: String) { this[index1+index2] = elem }\n");
 //            System.out.println(generateToText());
             Method foo = generateFunction("box");
@@ -266,7 +266,7 @@ public class ArrayGenTest extends CodegenTestCase {
     public void testCollectionGetMultiIndex () throws Exception {
             loadText("import java.util.ArrayList\n" +
                          "fun box() : String { val s = ArrayList<String>(1); s.add(\"\"); s [1, -1] = \"5\"; return s[2, -2] }\n" +
-                         "fun ArrayList<String>.get(index1: Int, index2 : Int) = this[index1+index2].sure()\n" +
+                         "fun ArrayList<String>.get(index1: Int, index2 : Int) = this[index1+index2]!!\n" +
                          "fun ArrayList<String>.set(index1: Int, index2 : Int, elem: String) { this[index1+index2] = elem }\n");
 //            System.out.println(generateToText());
             Method foo = generateFunction("box");
