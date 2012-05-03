dbef4f5bf817f141720728410d42691822d24382
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-05-03 04:01:47 +0400

    Moved addTypeAnnotation/removeTypeAnnotation methods from refactoring package to SpecifyTypeExplicitlyAction.

--- idea/src/org/jetbrains/jet/plugin/refactoring/introduceVariable/JetChangePropertyActions.java
+++ idea/src/org/jetbrains/jet/plugin/refactoring/introduceVariable/JetChangePropertyActions.java
@@ -32,33 +32,4 @@ public class JetChangePropertyActions {
         }
         property.getValOrVarNode().getPsi().replace(node.getPsi());
     }
-
-    public static void addTypeAnnotation(Project project, JetProperty property, @NotNull JetType exprType) {
-        if (property.getPropertyTypeRef() != null) return;
-        PsiElement anchor = property.getNameIdentifier();
-        if (anchor == null) return;
-        anchor = anchor.getNextSibling();
-        if (anchor == null || !(anchor instanceof PsiWhiteSpace)) return;
-        JetTypeReference typeReference = JetPsiFactory.createType(project, DescriptorRenderer.TEXT.renderType(exprType));
-        ASTNode colon = JetPsiFactory.createColonNode(project);
-        ASTNode anchorNode = anchor.getNode().getTreeNext();
-        property.getNode().addChild(colon, anchorNode);
-        property.getNode().addChild(JetPsiFactory.createWhiteSpace(project).getNode(), anchorNode);
-        property.getNode().addChild(typeReference.getNode(), anchorNode);
-        property.getNode().addChild(JetPsiFactory.createWhiteSpace(project).getNode(), anchorNode);
-        anchor.delete();
-        ReferenceToClassesShortening.compactReferenceToClasses(Collections.singletonList(property));
-    }
-
-    public static void removeTypeAnnotation(Project project, JetProperty property) {
-        JetTypeReference propertyTypeRef = property.getPropertyTypeRef();
-        if (propertyTypeRef == null) return;
-        PsiElement identifier = property.getNameIdentifier();
-        if (identifier == null) return;
-        PsiElement sibling = identifier.getNextSibling();
-        if (sibling == null) return;
-        PsiElement nextSibling = propertyTypeRef.getNextSibling();
-        if (nextSibling == null) return;
-        sibling.getParent().getNode().removeRange(sibling.getNode(), nextSibling.getNode());
-    }
 }
