646cdefc51b7d80863b1117cb4bd50b5f598e564
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-05-04 15:46:40 +0400

    Updated test.
     #KT-1896 fixed

--- idea/testData/highlighter/VariablesAsFunctions.kt
+++ idea/testData/highlighter/VariablesAsFunctions.kt
@@ -8,6 +8,5 @@ val Int.<info textAttributesKey="KOTLIN_EXTENSION_PROPERTY"><info textAttributes
 fun <info textAttributesKey="KOTLIN_FUNCTION_DECLARATION">foo</info>(<info textAttributesKey="KOTLIN_PARAMETER">a</info> : () -> Unit) {
     <info textAttributesKey="KOTLIN_PARAMETER">a</info>()
     <info textAttributesKey="KOTLIN_NAMESPACE_PROPERTY"><info textAttributesKey="KOTLIN_MUTABLE_VARIABLE">global</info></info>()
-    // TODO uncomment next line and replace "[" with "<" when KT-1728 is fixed
-    // 1.[info textAttributesKey="KOTLIN_EXTENSION_PROPERTY">ext</info>()
+    1.<info textAttributesKey="KOTLIN_EXTENSION_PROPERTY"><info textAttributesKey="KOTLIN_NAMESPACE_PROPERTY">ext</info></info>()
 }
