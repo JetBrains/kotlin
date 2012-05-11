162cc7307dcc6fadd84caac77b4f08fed70bfc94
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-05-11 12:52:26 +0400

    KT-1935 Fixed:  ACC_SUPER flag must be set for classes

---
+++ compiler/testData/writeAccessFlags/publicFinalClass.kt
@@ -0,0 +1,10 @@
+public class publicFinalClass() {
+}
+
+// ACC_ABSTRACT : false
+// ACC_ANNOTATION : false
+// ACC_FINAL : true
+// ACC_INTERFACE : false
+// ACC_PUBLIC : true
+// ACC_STATIC : false
+// ACC_SUPER : true
