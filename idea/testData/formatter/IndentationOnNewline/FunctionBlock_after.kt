8996ad8dab318115044ed672802f3866329ed389
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-05-11 18:22:27 +0400

    KT-1912 Formatter: smart indent doesn't work in a function literal  #KT-1099 fixed  #KT-1912 fixed

--- idea/testData/formatter/IndentationOnNewline/FunctionBlock_after.kt
+++ idea/testData/formatter/IndentationOnNewline/FunctionBlock_after.kt
@@ -1,10 +1,6 @@
 package testing
 
-private fun times<T>(times : Int, body : () -> T) {
-    for (var i in 1..times) {
-        body()
-    }
-}
+private fun times<T>(times : Int, body : () -> T) {}
 
 fun main(args: Array<String>) {
     times(3) {
