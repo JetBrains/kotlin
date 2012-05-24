6b3864a12d84c563f748b4fd06c39a047b3a4590
James Strachan <james.strachan@gmail.com>
2012-05-24 16:18:59 +0100

    starting to annotate JavaScript APIs in the standard library

---
+++ libraries/stdlib/src/js/Annotations.kt
@@ -0,0 +1,13 @@
+package js
+
+/**
+ * This annotation marks code as being a native JavaScript expression
+ */
+native
+annotation class native(name : String = "") {}
+
+/**
+ * Represents a function in the standard library
+ */
+native
+annotation class library(name : String = "") {}
