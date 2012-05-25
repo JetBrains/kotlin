8d3aeb9e1eb7a202a576a8b4cfeb2a1b3bfd1580
James Strachan <james.strachan@gmail.com>
2012-05-25 10:30:13 +0100

    made the stub DOM API public just in case

--- js/js.libraries/src/core/dom.kt
+++ js/js.libraries/src/core/dom.kt
@@ -3,25 +3,25 @@ package org.w3c.dom
 /**
  * A stub class definition so we can work with the DOM APIs as native objects
  */
-native interface Document : Node {
+native public interface Document : Node {
 }
 
-native interface Attr : Node {}
-native interface CDATASection : Text {}
-native interface CharacterData : Node {}
-native interface Comment : CharacterData {}
-native interface DOMImplementation {}
-native interface DocumentType : Node {}
-native interface Element : Node {}
-native interface Entity : Node {}
-native interface EntityReference : Node {}
-native interface NameList {}
-native interface NamedNodeMap {}
-native interface Node {}
-native interface NodeList {}
-native interface Notation : Node {}
-native interface ProcessingInstruction : Node {}
-native interface Text : CharacterData {}
-native interface TypeInfo {}
-native interface UserDataHandler {}
+native public interface Attr : Node {}
+native public interface CDATASection : Text {}
+native public interface CharacterData : Node {}
+native public interface Comment : CharacterData {}
+native public interface DOMImplementation {}
+native public interface DocumentType : Node {}
+native public interface Element : Node {}
+native public interface Entity : Node {}
+native public interface EntityReference : Node {}
+native public interface NameList {}
+native public interface NamedNodeMap {}
+native public interface Node {}
+native public interface NodeList {}
+native public interface Notation : Node {}
+native public interface ProcessingInstruction : Node {}
+native public interface Text : CharacterData {}
+native public interface TypeInfo {}
+native public interface UserDataHandler {}
 
