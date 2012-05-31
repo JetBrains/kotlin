5932eb35397d0728456995e59bdd0711865ffb8f
Dmitry Jemerov <yole@jetbrains.com>
2012-05-31 20:26:14 +0200

    fix

--- src/org/jetbrains/jet/j2k/ast/DummyStringExpression.kt
+++ src/org/jetbrains/jet/j2k/ast/DummyStringExpression.kt
@@ -2,5 +2,5 @@ package org.jetbrains.jet.j2k.ast
 
 
 public open class DummyStringExpression(val string: String): Expression() {
-    public override fun toKotlin() = string
+    public override fun toKotlin(): String = string
 }
