e049bb00d8b0a4014a39cb87b70e3386b01fff64
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-06-01 21:44:31 +0400

    Added simplest test for @KotlinSignature parsing.

---
+++ compiler/testData/readJavaBinaryClass/kotlinSignature/MethodWithAlternativeSignature.kt
@@ -0,0 +1,7 @@
+package test
+
+import java.util.*
+
+public open class MethodWithAlternativeSignature : Object() {
+    open fun foo(p0 : String, p1 : List<Map.Entry<String?, String>?>) : String = ""
+}
