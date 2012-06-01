1999c246f29d0f7b218f7dd937c534a5a603ea92
Dmitry Jemerov <yole@jetbrains.com>
2012-06-01 16:30:29 +0200

    to kotlin

--- src/org/jetbrains/jet/j2k/SetupJavaCoreEnvironmentException.kt
+++ src/org/jetbrains/jet/j2k/SetupJavaCoreEnvironmentException.kt
@@ -1,3 +1,6 @@
+package org.jetbrains.jet.j2k
+
+
 /*
  * Copyright 2010-2012 JetBrains s.r.o.
  *
@@ -13,13 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package org.jetbrains.jet.j2k;
-
 /**
  * @author ignatov
  */
-public class SetupJavaCoreEnvironmentException extends RuntimeException {
-    public SetupJavaCoreEnvironmentException(String s) {
-    }
+public open class SetupJavaCoreEnvironmentException(s: String?): RuntimeException() {
 }
