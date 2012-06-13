6112c77b22b7dafd0e3da7fc430e5dd9dac515fb
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-06-13 13:59:36 +0400

    Fixed nullability assertion that was accidentally made too strong

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/DescriptorResolver.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/DescriptorResolver.java
@@ -348,7 +348,7 @@ public class DescriptorResolver {
     }
 
     public static ConstructorDescriptorImpl createPrimaryConstructorForObject(
-            @NotNull PsiElement object,
+            @Nullable PsiElement object,
             @NotNull ClassDescriptor classDescriptor,
             @NotNull BindingTrace trace
     ) {
