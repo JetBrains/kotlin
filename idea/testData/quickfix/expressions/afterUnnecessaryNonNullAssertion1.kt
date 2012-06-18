a4584e96d7987053680d24dbfc3c603970f961ed
Sergey Lukjanov <me@frostman.ru>
2012-06-18 18:47:32 +0400

    Tests has been added

---
+++ idea/testData/quickfix/expressions/afterUnnecessaryNonNullAssertion1.kt
@@ -0,0 +1,4 @@
+// "Replace with dot call" "true"
+fun test(value : Int) : Int {
+    value<caret>
+}
---
+++ idea/testData/quickfix/expressions/beforeUnsafeCall2.kt
@@ -0,0 +1,4 @@
+// "Replace with non-null asserted (!!.) call" "true"
+fun foo(a: Int?) {
+    a<caret>.plus(1)
+}
