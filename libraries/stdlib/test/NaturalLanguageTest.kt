9a6477c121e5843dcb1eefcb5522cb0b7ec0163f
James Strachan <james.strachan@gmail.com>
2012-07-03 06:52:25 +0100

    added simpler natural language text sample

--- libraries/stdlib/test/NaturalLanguageTest.kt
+++ libraries/stdlib/test/NaturalLanguageTest.kt
@@ -1,16 +1,16 @@
-package test
+package test.collections
 
 import org.junit.Test as test
 import kotlin.test.*
 
 class NaturalLanguageTest {
 
-    test fun `test strings equal`() {
+    test fun `strings equal`() {
         val actual = "abc"
         assertEquals("abc", actual)
     }
 
-    test fun `test numbers equal`() {
+    test fun `numbers equal`() {
         val actual = 5
         assertEquals(5, actual)
     }
