88a38d9b5be2e7f534778654c02c7e0a2c5c6cce
James Strachan <james.strachan@gmail.com>
2012-07-05 13:07:04 +0100

    added equals() and toString() methods for AbstractList

--- js/js.translator/testFiles/kotlin_lib.js
+++ js/js.translator/testFiles/kotlin_lib.js
@@ -108,6 +108,39 @@ var kotlin = {set:function (receiver, key, value) {
         },
         contains: function (o) {
             return this.indexOf(o) != -1;
+        },
+        equals: function (o) {
+            if (this.$size === o.$size) {
+                var iter1 = this.iterator();
+                var iter2 = o.iterator();
+                while (true) {
+                    var hn1 = iter1.get_hasNext();
+                    var hn2 = iter2.get_hasNext();
+                    if (hn1 != hn2) return false;
+                    if (!hn2)
+                        return true;
+                    else {
+                        var o1 = iter1.next();
+                        var o2 = iter2.next();
+                        if (!Kotlin.equals(o1, o2)) return false;
+                    }
+                }
+            }
+            return false;
+        },
+        toString: function() {
+            var builder = "[";
+            var iter = this.iterator();
+            var first = true;
+            while (iter.get_hasNext()) {
+                if (first)
+                    first = false;
+                else
+                    builder += ", ";
+                builder += iter.next();
+            }
+            builder += "]";
+            return builder;
         }
     });
 
