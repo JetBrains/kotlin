d97e26cdb92cc648f790c30955556483508d8675
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-07-25 16:00:58 +0400

    Fix confidence test - several results in completion because of Leda middle matching prevented completion auto-insert (Leda only)

--- idea/tests/org/jetbrains/jet/completion/confidence/JetConfidenceTest.java
+++ idea/tests/org/jetbrains/jet/completion/confidence/JetConfidenceTest.java
@@ -77,6 +77,7 @@ public class JetConfidenceTest extends LightCompletionTestCase {
 
     @Override
     protected void complete() {
-        new CodeCompletionHandlerBase(CompletionType.BASIC, false, true, true).invokeCompletion(getProject(), getEditor(), 0, false, false);
+        new CodeCompletionHandlerBase(CompletionType.BASIC, false, true, true).invokeCompletion(
+                getProject(), getEditor(), 0, false, true);
     }
 }
