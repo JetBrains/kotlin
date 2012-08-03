713a2d3bf0065af3b03f69f65137529f75b30c54
James Strachan <james.strachan@gmail.com>
2012-08-03 09:00:25 +0100

    enabled more tests now that KT-2520 is fixed

--- libraries/stdlib/test/language/RangeTest.kt
+++ libraries/stdlib/test/language/RangeTest.kt
@@ -8,33 +8,19 @@ class RangeTest {
         val range = 0.upto(9)
         println("Have created up range: $range")
         assertEquals(10, range.size)
-/*
-        TODO runtime bug: java.lang.NoSuchMethodError: jet.IntRange.contains(I)Z
-        at language.RangeTest.ranges(RangeTest.kt:11)
-
-        see http://youtrack.jetbrains.com/issue/KT-2520
-
         assertTrue(range.contains(0))
         assertTrue(range.contains(1))
         assertTrue(range.contains(9))
         assertFalse(range.contains(10))
-*/
     }
 
     test fun downRange() {
         val range = 9.downto(0)
         println("Have created down range: $range")
         assertEquals(10, range.size)
-/*
-        TODO runtime bug: java.lang.NoSuchMethodError: jet.IntRange.contains(I)Z
-        at language.RangeTest.ranges(RangeTest.kt:11)
-
-        see http://youtrack.jetbrains.com/issue/KT-2520
-
         assertTrue(range.contains(0))
         assertTrue(range.contains(1))
         assertTrue(range.contains(9))
         assertFalse(range.contains(10))
-*/
     }
 }
