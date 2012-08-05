66eaeedd315f230cab53c1af443a401a3208de2e
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2012-08-05 23:49:50 +0400

    Add types in test

--- idea/testData/completion/basic/VisibilityClassMembersFromExternalForce.kt
+++ idea/testData/completion/basic/VisibilityClassMembersFromExternalForce.kt
@@ -1,6 +1,6 @@
 class Some() {
-    public val testPublic = 12
-    protected val testProtected = 12
+    public val testPublic : Int = 12
+    protected val testProtected : Int = 12
     private val testPrivate = 12
     val testPackage = 12
 }
