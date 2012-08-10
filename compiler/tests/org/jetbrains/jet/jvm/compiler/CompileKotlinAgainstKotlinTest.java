dcf2982288abcd68db4dbb785f1f24dade513425
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2012-08-10 17:24:13 +0400

    copy-paste bug fix in test

--- compiler/tests/org/jetbrains/jet/jvm/compiler/CompileKotlinAgainstKotlinTest.java
+++ compiler/tests/org/jetbrains/jet/jvm/compiler/CompileKotlinAgainstKotlinTest.java
@@ -104,7 +104,7 @@ public class CompileKotlinAgainstKotlinTest extends TestCaseWithTmpdir {
 
         String text = FileUtil.loadFile(ktBFile);
 
-        LightVirtualFile virtualFile = new LightVirtualFile(ktAFile.getName(), JetLanguage.INSTANCE, text);
+        LightVirtualFile virtualFile = new LightVirtualFile(ktBFile.getName(), JetLanguage.INSTANCE, text);
         virtualFile.setCharset(CharsetToolkit.UTF8_CHARSET);
         JetFile psiFile = (JetFile) ((PsiFileFactoryImpl) PsiFileFactory.getInstance(jetCoreEnvironment.getProject())).trySetupPsiForFile(virtualFile, JetLanguage.INSTANCE, true, false);
 
