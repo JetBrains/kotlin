4320a5fb61819959c73c3ecb8b7d9da280dc3bc0
Pavel V. Talanov <Pavel.Talanov@jetbrains.com>
2012-08-13 18:35:38 +0400

    Don't generate code for javautilCode.kt.

--- js/js.libraries/src/core/javautilCode.kt
+++ js/js.libraries/src/core/javautilCode.kt
@@ -7,26 +7,35 @@ public object Collections {
     public fun max<T>(col : Collection<T>, comp : Comparator<T>) : T = js.noImpl
 
     // TODO should be immutable!
+    library
     public val emptyList: List<Any> = ArrayList<Any>()
+    library
     public val emptyMap: Map<Any, Any> = HashMap<Any,Any>()
 
+    library
     public val <T> EMPTY_LIST: List<T>
     get() = emptyList<T>()
 
+    library
     public val <K,V> EMPTY_MAP: Map<K,V>
     get() = emptyMap<K,V>()
 
+    library
     public fun <T> emptyList(): List<T> = emptyList as List<T>
+    library
     public fun <K,V> emptyMap(): Map<K,V> = emptyMap as Map<K,V>
 
+    library
     public fun <in T> sort(list: List<T>): Unit {
         throw UnsupportedOperationException()
     }
 
+    library
     public fun <in T> sort(list: List<T>, comparator: java.util.Comparator<T>): Unit {
         throw UnsupportedOperationException()
     }
 
+    library
     public fun <T> reverse(list: List<T>): Unit {
         val size = list.size()
         for (i in 0.upto(size / 2)) {
