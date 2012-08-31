04da2f453af3697dd3040ee01eea4b418760da18
Pavel V. Talanov <Pavel.Talanov@jetbrains.com>
2012-08-31 14:35:09 +0400

    Test for KT-2281.
     #KT-2281 fixed

--- js/js.tests/test/org/jetbrains/k2js/test/semantics/MiscTest.java
+++ js/js.tests/test/org/jetbrains/k2js/test/semantics/MiscTest.java
@@ -195,4 +195,10 @@ public final class MiscTest extends AbstractExpressionTest {
     public void testIterationOverIterator() throws Exception {
         fooBoxTest();
     }
+
+    public void testStringInterpolationEvaluationOrder() throws Exception {
+        fooBoxTest();
+    }
+
+
 }
