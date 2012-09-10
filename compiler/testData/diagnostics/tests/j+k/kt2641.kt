b2b3d6273e1c3c045910d314e108112ea5bfad68
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2012-09-10 18:02:40 +0400

    improved test

--- compiler/testData/diagnostics/tests/j+k/kt2641.kt
+++ compiler/testData/diagnostics/tests/j+k/kt2641.kt
@@ -1,11 +1,16 @@
-//KT-2641 Warn on using j.l.Iterable in Kotlin code
-package n
+package a
 
 import <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>java.util.Iterator<!>
 import <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>java.lang.Comparable<!> as Comp
 
-fun bar() : <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>java.lang.Iterable<Int><!>? {
-    val <!UNUSED_VARIABLE!>a<!> : <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>java.lang.Comparable<String><!>? = null
-    val <!UNUSED_VARIABLE!>b<!> : Iterable<<!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Integer<!>>
+fun bar(any: Any): <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>java.lang.Iterable<Int><!>? {
+    val <!UNUSED_VARIABLE!>a<!>: <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>java.lang.Comparable<String><!>? = null
+    val <!UNUSED_VARIABLE!>b<!>: Iterable<<!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Integer<!>>
+    val <!UNUSED_VARIABLE!>c<!> : <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Iterator<String><!>? = null
+
+    if (any is <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Iterator<*><!>) {
+        any : <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Iterator<*><!>
+    }
+    any as <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Iterator<*><!>
     return null
 }
