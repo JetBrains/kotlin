61d2aba9851e07fe72f7e75f61077a1456b0e81f
develar <develar@gmail.com>
2012-09-11 18:25:48 +0400

    json.kt — kotlin code style (cherry picked from commit e7d413d)

--- js/js.libraries/src/core/json.kt
+++ js/js.libraries/src/core/json.kt
@@ -7,19 +7,19 @@ public class Json() {
 }
 
 library("jsonSet")
-public fun Json.set(paramName : String, value : Any?) : Unit = js.noImpl
+public fun Json.set(paramName: String, value: Any?): Unit = js.noImpl
 
 library("jsonGet")
-public fun Json.get(paramName : String) : Any? = js.noImpl
+public fun Json.get(paramName: String): Any? = js.noImpl
 
 library("jsonFromTuples")
-public fun json(vararg pairs : Pair<String, Any?>) : Json = js.noImpl
+public fun json(vararg pairs: Pair<String, Any?>): Json = js.noImpl
 
 library("jsonFromTuples")
-public fun json2(pairs : Array<Pair<String, Any?>>) : Json = js.noImpl
+public fun json2(pairs: Array<Pair<String, Any?>>): Json = js.noImpl
 
 library("jsonAddProperties")
-public fun Json.add(other : Json) : Json = js.noImpl
+public fun Json.add(other: Json): Json = js.noImpl
 
 native
 public trait JsonClass {
@@ -28,4 +28,4 @@ public trait JsonClass {
 }
 
 native
-public val JSON:JsonClass = noImpl
+public val JSON: JsonClass = noImpl
