47769ed638bceb5d224d9c0067339b118d440676
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2012-09-18 13:22:59 +0400

    tests changed after KT-2745 Write exact Kotlin type (if it's mapped) in signature while generating bytecode  #KT-2745 fixed

--- compiler/testData/writeSignature/ListOfStar.kt
+++ compiler/testData/writeSignature/ListOfStar.kt
@@ -4,4 +4,4 @@ fun listOfStar(): List<*> = throw Exception()
 // method: namespace::listOfStar
 // jvm signature:     ()Ljava/util/List;
 // generic signature: ()Ljava/util/List<+Ljava/lang/Object;>;
-// kotlin signature:  ()Ljava/util/List<+?Ljava/lang/Object;>;
+// kotlin signature:  ()Ljet/List<+?Ljava/lang/Object;>;
