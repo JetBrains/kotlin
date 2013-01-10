47d1d78301efd580ce900db82af0225e405d4c24
Yuri Samsoniuk <sigito@sigma.dlink>
2013-01-10 23:54:24 +0200

    Char extension methos for static Character check methods
    Added extension methods for all Character check methods that where
    added till java 1.5 inclusivly.

--- libraries/stdlib/src/kotlin/Char.kt
+++ libraries/stdlib/src/kotlin/Char.kt
@@ -1,7 +1,13 @@
 package kotlin
 
+/*
+ * Returns true if this character is defined in Unicode
+ */
 public inline fun Char.isDefined(): Boolean = Character.isDefined(this)
 
+/*
+ * Returns true if this character is a digit
+ */
 public inline fun Char.isDigit(): Boolean = Character.isDigit(this)
 
 public inline fun Char.isHighSurrogate(): Boolean = Character.isHighSurrogate(this)
@@ -34,3 +40,25 @@ public inline fun Char.isUpperCase(): Boolean = Character.isUpperCase(this)
  * Returns true if this character is lower case
  */
 public inline fun Char.isLowerCase(): Boolean = Character.isLowerCase(this)
+
+/**
+ * Returns true if this character is a letter
+ */
+public inline fun Char.isLetter(): Boolean = Character.isLetter(this)
+
+/**
+ * Returns true if this character is a letter or digit
+ */
+public inline fun Char.isLetterOrDigit(): Boolean = Character.isLetterOrDigit(this)
+
+public inline fun Char.isLowSurrogate(): Boolean = Character.isLowSurrogate(this)
+
+public inline fun Char.isMirrored(): Boolean = Character.isMirrored(this)
+
+public inline fun Char.isSpaceChar(): Boolean = Character.isSpaceChar(this)
+
+public inline fun Char.isTitleCase(): Boolean = Character.isTitleCase(this)
+
+public inline fun Char.isUnicodeIdentifierPart(): Boolean = Character.isUnicodeIdentifierPart(this)
+
+public inline fun Char.isUnicodeIdentifierStart(): Boolean = Character.isUnicodeIdentifierStart(this)
