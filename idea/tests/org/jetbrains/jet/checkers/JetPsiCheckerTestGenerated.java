823fc14722199cf23385d2130ea3ca5f8980a3db
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2013-01-29 22:46:37 +0400

    Regenerate tests

--- idea/tests/org/jetbrains/jet/checkers/JetPsiCheckerTestGenerated.java
+++ idea/tests/org/jetbrains/jet/checkers/JetPsiCheckerTestGenerated.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jetbrains.jet.checkers;
 
 import junit.framework.Assert;
