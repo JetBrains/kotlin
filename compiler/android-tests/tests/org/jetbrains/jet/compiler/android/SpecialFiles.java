c7e512a38a60b209409b3993a43ec2d33b364169
Alexander Udalov <udalov.alexander@gmail.com>
2013-02-09 21:47:30 +0400

    Correct Android SpecialFiles
    Replace all multi-file tests and tests with java with an exclusion of
    these directories. Delete obsolete exclusions, add a new one

--- compiler/android-tests/tests/org/jetbrains/jet/compiler/android/SpecialFiles.java
+++ compiler/android-tests/tests/org/jetbrains/jet/compiler/android/SpecialFiles.java
@@ -53,62 +53,25 @@ public class SpecialFiles {
     }
 
     private static void fillExcludedFiles() {
-        excludedFiles.add("kt2781.kt"); // Must compile Java files before
-        excludedFiles.add("simpleJavaInnerEnum.kt"); // Must compile Java files before
-        excludedFiles.add("referencesStaticInnerClassMethod.kt"); // Must compile Java files before
-        excludedFiles.add("referencesStaticInnerClassMethodL2.kt"); // Must compile Java files before
-        excludedFiles.add("simpleJavaEnum.kt"); // Must compile Java files before
-        excludedFiles.add("simpleJavaEnumWithFunction.kt"); // Must compile Java files before
-        excludedFiles.add("simpleJavaEnumWithStaticImport.kt"); // Must compile Java files before
-        excludedFiles.add("removeInIterator.kt"); // Must compile Java files before
+        excludedFiles.add("boxWithJava");  // Must compile Java files before
+        excludedFiles.add("boxMultiFile"); // MultiFileTest not supported yet
+
         excludedFiles.add("kt3238.kt"); // Reflection
+        excludedFiles.add("kt1482_2279.kt"); // Reflection
+
+        excludedFiles.add("nestedInPackage.kt"); // Cannot change package name
+        excludedFiles.add("importNestedClass.kt"); // Cannot change package name
         excludedFiles.add("namespaceQualifiedMethod.kt"); // Cannot change package name
-        excludedFiles.add("kt1482_2279.kt"); // Cannot change package name
-        excludedFiles.add("kt1482.kt"); // Cannot change package name
+
         excludedFiles.add("kt326.kt"); // Commented
         excludedFiles.add("kt1213.kt"); // Commented
-
-        excludedFiles.add("box.kt");      // MultiFileTest not supported yet
-        excludedFiles.add("kt2060_1.kt"); // MultiFileTest not supported yet
-        excludedFiles.add("kt2257_1.kt"); // MultiFileTest not supported yet
-        excludedFiles.add("kt1528_1.kt"); // MultiFileTest not supported yet
-        excludedFiles.add("kt1845_1.kt"); // MultiFileTest not supported yet
-        excludedFiles.add("thisPackage.kt"); // MultiFileTest not supported yet
 
         excludedFiles.add("kt684.kt"); // StackOverflow with StringBuilder (escape())
 
         excludedFiles.add("kt529.kt");  // Bug
         excludedFiles.add("kt344.kt");  // Bug
 
-        excludedFiles.add("classWithNestedEnum.kt");
-
-        excludedFiles.add("doGenerateAssertions.kt"); // Multi-file + Java
-        excludedFiles.add("doNotGenerateAssertions.kt"); // Multi-file + Java
-        excludedFiles.add("doGenerateParamAssertions.kt"); // Java
-
-        excludedFiles.add("nestedInPackage.kt"); // Custom packages are not supported
-        excludedFiles.add("importNestedClass.kt"); // Won't work when moved to another package
-
-        excludedFiles.add("funCallInConstructor.kt");        // Java
-        excludedFiles.add("funClassObject.kt");              // Java
-        excludedFiles.add("funGenericClass.kt");             // Java
-        excludedFiles.add("funNestedStaticClass.kt");        // Java
-        excludedFiles.add("funNestedStaticClass2.kt");       // Java
-        excludedFiles.add("funNestedStaticGenericClass.kt"); // Java
-        excludedFiles.add("funNotDirectSuperClass.kt");      // Java
-        excludedFiles.add("funObject.kt");                   // Java
-        excludedFiles.add("simpleClass.kt");                 // Java
-        excludedFiles.add("simpleClass2.kt");                // Java
-        excludedFiles.add("simpleFun.kt");                   // Java
-        excludedFiles.add("simpleProperty.kt");              // Java
-
-        excludedFiles.add("packageClass.kt");                    // Java
-        excludedFiles.add("packageFun.kt");                      // Java
-        excludedFiles.add("packageProperty.kt");                 // Java
-        excludedFiles.add("overrideProtectedFunInPackage.kt");   // Java
-        excludedFiles.add("protectedFunInPackage.kt");           // Java
-        excludedFiles.add("protectedPropertyInPackage.kt");      // Java
-        excludedFiles.add("protectedStaticClass.kt");            // Java
+        excludedFiles.add("comparisonWithNullCallsFun.kt"); // java.lang.NoClassDefFoundError: jet.Nothing
     }
 
     private SpecialFiles() {
