4d8632e0a0a40088d91a963540611fd29b80c537
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-03-12 19:19:13 +0400

    ++ Track the whole chain of dependencies

--- injector-generator/src/org/jetbrains/jet/di/Dependencies.java
+++ injector-generator/src/org/jetbrains/jet/di/Dependencies.java
@@ -178,7 +178,7 @@ public class Dependencies {
 
             @Override
             public String toString() {
-                return "<EMPTY>";
+                return "<itself>";
             }
         };
 
