79cb9a209d7f38b203edee28b9e60c1c7d867ffe
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-03-21 21:51:44 +0400

    Create modules lazily to be able to add a dependency on built-ins

--- compiler/cli/src/org/jetbrains/jet/cli/jvm/compiler/SimpleKotlinModuleManager.java
+++ compiler/cli/src/org/jetbrains/jet/cli/jvm/compiler/SimpleKotlinModuleManager.java
@@ -16,8 +16,10 @@
 
 package org.jetbrains.jet.cli.jvm.compiler;
 
+import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jet.lang.DefaultModuleConfiguration;
 import org.jetbrains.jet.lang.PlatformToKotlinClassMap;
 import org.jetbrains.jet.lang.descriptors.ModuleDescriptor;
 import org.jetbrains.jet.lang.descriptors.PackageFragmentDescriptor;
@@ -25,31 +27,54 @@ import org.jetbrains.jet.lang.descriptors.SubModuleDescriptor;
 import org.jetbrains.jet.lang.descriptors.impl.MutableModuleDescriptor;
 import org.jetbrains.jet.lang.descriptors.impl.MutableSubModuleDescriptor;
 import org.jetbrains.jet.lang.psi.JetFile;
+import org.jetbrains.jet.lang.resolve.ImportPath;
 import org.jetbrains.jet.lang.resolve.KotlinModuleManager;
 import org.jetbrains.jet.lang.resolve.ModuleSourcesManager;
+import org.jetbrains.jet.lang.resolve.java.JavaBridgeConfiguration;
 import org.jetbrains.jet.lang.resolve.name.Name;
+import org.jetbrains.jet.lang.types.lang.KotlinBuiltIns;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * KotlinModuleManager that assumes that all the sources belong to the same submodule
  */
 public class SimpleKotlinModuleManager implements KotlinModuleManager {
 
-    private final ModuleDescriptor module;
-    private final ModuleSourcesManager moduleSourcesManager;
+    private final NotNullLazyValue<Modules> modules;
 
     public SimpleKotlinModuleManager(
+            @NotNull final JetCoreEnvironment jetCoreEnvironment,
+            @NotNull final String baseName,
+            @NotNull final PlatformToKotlinClassMap classMap
+    ) {
+        this.modules = new NotNullLazyValue<Modules>() {
+            @NotNull
+            @Override
+            protected Modules compute() {
+                return createModules(jetCoreEnvironment, baseName, classMap);
+            }
+        };
+    }
+
+    public static Modules createModules(
             @NotNull JetCoreEnvironment jetCoreEnvironment,
             @NotNull String baseName,
             @NotNull PlatformToKotlinClassMap classMap
     ) {
         MutableModuleDescriptor mutableModule = new MutableModuleDescriptor(Name.special("<" + baseName + " module>"), classMap);
-        final SubModuleDescriptor subModule =
-                mutableModule.addSubModule(new MutableSubModuleDescriptor(mutableModule, Name.special("<" + baseName + " sub-module>")));
+        final MutableSubModuleDescriptor subModule = new MutableSubModuleDescriptor(mutableModule, Name.special("<" + baseName + " sub-module>"));
+        mutableModule.addSubModule(subModule);
+
+        subModule.addDependency(KotlinBuiltIns.getInstance().getBuiltInsSubModule());
+
+        addDefaultImports(subModule, JavaBridgeConfiguration.DEFAULT_JAVA_IMPORTS);
+        addDefaultImports(subModule, DefaultModuleConfiguration.DEFAULT_JET_IMPORTS);
+
         final CliIndexManager index = CliIndexManager.getInstance(jetCoreEnvironment.getProject());
-        this.moduleSourcesManager = new ModuleSourcesManager() {
+        ModuleSourcesManager moduleSourcesManager = new ModuleSourcesManager() {
 
             @NotNull
             @Override
@@ -63,18 +88,36 @@ public class SimpleKotlinModuleManager implements KotlinModuleManager {
                 return index.getPackageSources(packageFragment.getFqName());
             }
         };
-        this.module = mutableModule;
+
+        return new Modules(mutableModule, moduleSourcesManager);
+    }
+
+    private static void addDefaultImports(MutableSubModuleDescriptor subModule, List<ImportPath> imports) {
+        for (ImportPath path : imports) {
+            subModule.addDefaultImport(path);
+        }
     }
 
     @NotNull
     @Override
     public Collection<ModuleDescriptor> getModules() {
-        return Collections.singletonList(module);
+        return Collections.singletonList(modules.getValue().module);
     }
 
     @NotNull
     @Override
     public ModuleSourcesManager getSourcesManager() {
-        return moduleSourcesManager;
+        return modules.getValue().moduleSourcesManager;
+    }
+
+    private static class Modules {
+        private final ModuleDescriptor module;
+        private final ModuleSourcesManager moduleSourcesManager;
+
+        private Modules(ModuleDescriptor module, ModuleSourcesManager moduleSourcesManager) {
+            this.module = module;
+            this.moduleSourcesManager = moduleSourcesManager;
+        }
+
     }
 }
