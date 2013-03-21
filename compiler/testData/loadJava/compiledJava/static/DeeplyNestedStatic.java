bad04d609511c05ae2e7607ad6c00f566d6986c9
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2013-03-21 23:35:21 +0400

    Loading deeply nested static method correctly.

---
+++ compiler/testData/loadJava/compiledJava/static/DeeplyNestedStatic.java
@@ -0,0 +1,10 @@
+package test;
+
+public class DeeplyNestedStatic {
+    public static class Foo {
+        public static class Bar {
+            public static void method() {
+            }
+        }
+    }
+}
