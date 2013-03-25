c6e934169ff910914ec0dc632a92dc3b03ba2fa1
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2013-03-25 17:25:03 +0400

    Add method isLocalClass to JetPsiUtil

---
+++ compiler/testData/psiUtil/isLocalClass.kt
@@ -0,0 +1,21 @@
+class A {
+  class B
+  object G
+
+  class object {
+    class C
+    object H
+
+    fun foo() {
+      class DLocal
+      object KLocal
+    }
+  }
+
+  fun foo2() {
+    class ELocal {
+      class FLocal
+    }
+    object LLocal
+  }
+}
