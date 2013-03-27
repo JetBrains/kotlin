ab54d192410b1b7f3dffadec7f6193ffe2bcf9cb
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2013-03-27 19:15:58 +0400

    Added test with overridden j.l.Object methods in trait.

---
+++ compiler/testData/diagnostics/tests/TraitOverrideObjectMethods.kt
@@ -0,0 +1,5 @@
+trait MyTrait: <!PLATFORM_CLASS_MAPPED_TO_KOTLIN!>Object<!> {
+    override fun toString(): String
+    public override fun finalize()
+    public <!OVERRIDING_FINAL_MEMBER!>override<!> fun wait()
+}
