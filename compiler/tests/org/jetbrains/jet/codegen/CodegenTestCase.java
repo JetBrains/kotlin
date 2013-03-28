d612a1167df4aa0282ff7bad45ddd9b2f799a7f0
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-03-28 17:17:27 +0400

    Better error reporting in codegen tests: an exception from generateToText() used to mask the one from the original run

--- compiler/tests/org/jetbrains/jet/codegen/CodegenTestCase.java
+++ compiler/tests/org/jetbrains/jet/codegen/CodegenTestCase.java
@@ -166,7 +166,14 @@ public abstract class CodegenTestCase extends UsefulTestCase {
                     DxChecker.check(classFileFactory);
                 }
             } catch (Throwable e) {
-                System.out.println(generateToText());
+                try {
+                    System.out.println(generateToText());
+                }
+                catch (Throwable e1) {
+                    System.err.println("Exception thrown while trying to generate text, the actual exception follows:");
+                    e1.printStackTrace();
+                    System.err.println("-----------------------------------------------------------------------------");
+                }
                 throw ExceptionUtils.rethrow(e);
             }
         }
