73bbda5b931fe99f12d7e7d11ce502afab7a4405
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2013-03-29 11:45:56 +0400

    AndroidTests: do not run test which use file system

--- compiler/android-tests/tests/org/jetbrains/jet/compiler/android/SpecialFiles.java
+++ compiler/android-tests/tests/org/jetbrains/jet/compiler/android/SpecialFiles.java
@@ -71,6 +71,8 @@ public class SpecialFiles {
         excludedFiles.add("kt529.kt");  // Bug
         excludedFiles.add("kt344.kt");  // Bug
 
+        excludedFiles.add("filenameFilter.kt");  // Work with FileSystem
+
         excludedFiles.add("comparisonWithNullCallsFun.kt"); // java.lang.NoClassDefFoundError: jet.Nothing
     }
 
