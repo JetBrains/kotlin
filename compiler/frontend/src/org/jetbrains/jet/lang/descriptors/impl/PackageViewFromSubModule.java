09776c12c87b213573b84b3715cea1f024af4077
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-04-08 18:15:45 +0400

    Abstract class mrged with its only subclass

--- compiler/frontend/src/org/jetbrains/jet/lang/descriptors/impl/PackageViewFromSubModule.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/descriptors/impl/PackageViewFromSubModule.java
@@ -16,35 +16,64 @@
 
 package org.jetbrains.jet.lang.descriptors.impl;
 
-import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
+import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.jet.lang.descriptors.PackageFragmentDescriptor;
-import org.jetbrains.jet.lang.descriptors.PackageViewDescriptor;
-import org.jetbrains.jet.lang.descriptors.SubModuleDescriptor;
+import org.jetbrains.jet.lang.descriptors.*;
 import org.jetbrains.jet.lang.resolve.name.FqName;
 import org.jetbrains.jet.lang.resolve.name.Name;
+import org.jetbrains.jet.lang.resolve.scopes.ChainedScope;
+import org.jetbrains.jet.lang.resolve.scopes.JetScope;
 
 import java.util.Collection;
 import java.util.List;
 
-public class PackageViewFromSubModule extends PackageViewDescriptorImpl {
+public class PackageViewFromSubModule extends PackageLikeDescriptorBase implements PackageViewDescriptor {
+
+    private final SubModuleDescriptor subModule;
+    private final JetScope scope;
 
     public PackageViewFromSubModule(
             @NotNull SubModuleDescriptor subModule,
             @NotNull FqName fqName,
-            @NotNull List<PackageFragmentDescriptor> fragments
+            @NotNull final List<PackageFragmentDescriptor> fragments
     ) {
-        super(subModule, fqName, fragments);
+        super(fqName);
+        this.subModule = subModule;
+        List<JetScope> fragmentScopes = ContainerUtil.mapNotNull(
+                fragments,
+                new Function<PackageFragmentDescriptor, JetScope>() {
+                    @Override
+                    public JetScope fun(PackageFragmentDescriptor input) {
+                        return input.getMemberScope();
+                    }
+                });
+        this.scope = new ChainedScope(this, "Joint scope for " + this, fragmentScopes) {
+            @Override
+            public PackageViewDescriptor getPackage(@NotNull Name name) {
+                return getSubPackage(name);
+            }
+
+            @NotNull
+            @Override
+            public Collection<DeclarationDescriptor> getAllDescriptors() {
+                Collection<DeclarationDescriptor> descriptors = Lists.newArrayList(super.getAllDescriptors());
+                for (PackageFragmentDescriptor fragment : fragments) {
+                    descriptors.addAll(getSubPackages(fragment.getFqName()));
+                }
+                return descriptors;
+            }
+        };
     }
 
     @NotNull
-    @Override
-    protected Collection<PackageViewDescriptor> getSubPackages(@NotNull FqName fqName) {
+    private Collection<PackageViewDescriptor> getSubPackages(@NotNull FqName fqName) {
         Collection<FqName> subPackages = getViewContext().getPackageFragmentProvider().getSubPackagesOf(fqName);
         return Collections2.transform(subPackages,
-                                      new Function<FqName, PackageViewDescriptor>() {
+                                      new com.google.common.base.Function<FqName, PackageViewDescriptor>() {
                                           @Override
                                           public PackageViewDescriptor apply(FqName subPackage) {
                                               return getViewContext().getPackageView(subPackage);
@@ -55,7 +84,7 @@ public class PackageViewFromSubModule extends PackageViewDescriptorImpl {
     @NotNull
     @Override
     public SubModuleDescriptor getViewContext() {
-        return (SubModuleDescriptor) super.getViewContext();
+        return subModule;
     }
 
     @Nullable
@@ -66,8 +95,18 @@ public class PackageViewFromSubModule extends PackageViewDescriptorImpl {
     }
 
     @Nullable
-    @Override
-    protected PackageViewDescriptor getSubPackage(@NotNull Name name) {
+    private PackageViewDescriptor getSubPackage(@NotNull Name name) {
         return getViewContext().getPackageView(getFqName().child(name));
+    }
+
+    @NotNull
+    @Override
+    public JetScope getMemberScope() {
+        return scope;
+    }
+
+    @Override
+    public <R, D> R accept(DeclarationDescriptorVisitor<R, D> visitor, D data) {
+        return visitor.visitPackageViewDescriptor(this, data);
     }
 }
