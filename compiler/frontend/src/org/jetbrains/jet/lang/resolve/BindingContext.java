d42edc82071a137fad2279be0791d294b6a7e4f4
Jack Zhou <yz282@cornell.edu>
2013-04-19 01:08:38 -0400

    Create from usage: Added EXPECTED_EXPRESSION_TYPE slice in BindingContext.

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/BindingContext.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/BindingContext.java
@@ -75,6 +75,7 @@ public interface BindingContext {
     WritableSlice<JetExpression, CompileTimeConstant<?>> COMPILE_TIME_VALUE = Slices.createSimpleSlice();
     WritableSlice<JetTypeReference, JetType> TYPE = Slices.createSimpleSlice();
     WritableSlice<JetExpression, JetType> EXPRESSION_TYPE = new BasicWritableSlice<JetExpression, JetType>(DO_NOTHING);
+    WritableSlice<JetExpression, JetType> EXPECTED_EXPRESSION_TYPE = new BasicWritableSlice<JetExpression, JetType>(DO_NOTHING);
     WritableSlice<JetExpression, DataFlowInfo> EXPRESSION_DATA_FLOW_INFO = new BasicWritableSlice<JetExpression, DataFlowInfo>(DO_NOTHING);
     WritableSlice<JetExpression, DataFlowInfo> DATAFLOW_INFO_AFTER_CONDITION = Slices.createSimpleSlice();
 
