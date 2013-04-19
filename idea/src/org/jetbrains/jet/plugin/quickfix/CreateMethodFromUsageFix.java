f2dc5e39e786b932c31a5ec88a5d3444bd9aea28
Jack Zhou <univerio@gmail.com>
2013-04-19 12:04:51 -0400

    Create from usage: Removed a TODO.

--- idea/src/org/jetbrains/jet/plugin/quickfix/CreateMethodFromUsageFix.java
+++ idea/src/org/jetbrains/jet/plugin/quickfix/CreateMethodFromUsageFix.java
@@ -990,8 +990,6 @@ public class CreateMethodFromUsageFix extends CreateFromUsageFixBase {
             declaration = variable; // otherwise fall through and guess, based on LHS
         }
 
-        // TODO: nested in a trivial expression (e.g. parentheses or if-else)
-
         // guess based on declaration
         SearchScope scope = expr.getContainingFile().getUseScope();
         Set<JetType> expectedTypes = new HashSet<JetType>();
