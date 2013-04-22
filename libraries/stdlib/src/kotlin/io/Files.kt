79311236078e6f4ff152b14e71e1c99bb1041cf4
U-AMS\xbbj5x6 <xbbj5x6@L328BDS002012US.ams.bnymellon.net>
2013-04-22 00:26:21 -0400

    #KT-3533 Fixed - not to null in encoding name parameter in String, Bytes, and File.

--- libraries/stdlib/src/kotlin/io/Files.kt
+++ libraries/stdlib/src/kotlin/io/Files.kt
@@ -110,33 +110,41 @@ public fun File.appendBytes(data: ByteArray): Unit {
 }
 
 /**
- * Reads the entire content of the file as a String using the optional
- * character encoding.  The default platform encoding is used if the character
- * encoding is not specified or null.
+ * Reads the entire content of the file as a String using the a character encoding.
  *
  * This method is not recommended on huge files.
  */
-public fun File.readText(encoding:String? = null) : String = readBytes().toString(encoding)
+public fun File.readText(encoding:String) : String = readBytes().toString(encoding)
 
 /**
- * Reads the entire content of the file as a String using the
+ * Reads the entire content of the file as a String using the default platform
  * character encoding.
+ *
+ * This method is not recommended on huge files.
+ */
+public fun File.readText() : String = readBytes().toString()
+
+/**
+ * Reads the entire content of the file as a String using a character encoding.
  *
  * This method is not recommended on huge files.
  */
 public fun File.readText(encoding:Charset) : String = readBytes().toString(encoding)
 
 /**
- * Writes the text as the contents of the file using the optional
- * character encoding.  The default platform encoding is used if the character
- * encoding is not specified or null.
+ * Writes the text as the contents of the file using the a
+ * character encoding.
  */
-public fun File.writeText(text: String, encoding:String?=null): Unit { writeBytes(text.toByteArray(encoding)) }
+public fun File.writeText(text: String, encoding:String): Unit { writeBytes(text.toByteArray(encoding)) }
 
 /**
- * Writes the text as the contents of the file using the optional
- * character encoding.  The default platform encoding is used if the character
- * encoding is not specified or null.
+ * Writes the text as the contents of the file using the default platform
+ * character encoding.
+ */
+public fun File.writeText(text: String): Unit { writeBytes(text.toByteArray()) }
+
+/**
+ * Writes the text as the contents of the file using a character encoding.
  */
 public fun File.writeText(text: String, encoding:Charset): Unit { writeBytes(text.toByteArray(encoding)) }
 
@@ -148,12 +156,17 @@ public fun File.appendText(text: String, encoding: Charset): Unit {
 }
 
 /**
- * Appends text to the contents of the file using optional character encoding.
- * The default platform encoding is used if the character encoding is
- * not specified or null.
+ * Appends text to the contents of the file using a character encoding.
  */
-public fun File.appendText(text: String, encoding: String? = null): Unit {
+public fun File.appendText(text: String, encoding: String): Unit {
     appendBytes(text.toByteArray(encoding))
+}
+
+/**
+ * Appends text to the contents of the file using default platform character encoding.
+ */
+public fun File.appendText(text: String): Unit {
+    appendBytes(text.toByteArray())
 }
 
 /**
