959f2fb70b784520d0a3322c3ce38d8ca577d490
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2013-04-22 21:47:17 +0400

    Fix codegen tests on Android
    Since Android tests move files around, changing their packages, and 'toString'
    tests the exact name of a class, we now get it via Java reflection

--- compiler/testData/codegen/box/callableReference/toString.kt
+++ compiler/testData/codegen/box/callableReference/toString.kt
@@ -11,11 +11,13 @@ fun box(): String {
     val f = "${::foo}"
     if (f != "jet.KFunctionImpl1<? super java.lang.String, ? extends jet.Unit>") return "Fail foo: $f"
 
+    val nameOfA = (A() as java.lang.Object).getClass().getName()
+
     val g = "${A::bar}"
-    if (g != "jet.KMemberFunctionImpl0<? super A, ? extends java.lang.String>") return "Fail bar: $f"
+    if (g != "jet.KMemberFunctionImpl0<? super $nameOfA, ? extends java.lang.String>") return "Fail bar: $g"
 
     val h = "${A::baz}"
-    if (h != "jet.KExtensionFunctionImpl0<? super A, ? extends jet.Unit>") return "Fail baz: $h"
+    if (h != "jet.KExtensionFunctionImpl0<? super $nameOfA, ? extends jet.Unit>") return "Fail baz: $h"
 
     return "OK"
 }
