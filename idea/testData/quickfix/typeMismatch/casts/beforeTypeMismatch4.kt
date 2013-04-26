837ab0ac4f21b2ce512780308b546710c509537e
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2013-04-26 14:03:06 +0400

    Updated test data since another quick fix is available.

--- idea/testData/quickfix/typeMismatch/casts/beforeTypeMismatch4.kt
+++ idea/testData/quickfix/typeMismatch/casts/beforeTypeMismatch4.kt
@@ -1,5 +1,6 @@
 // "Cast expression 'a: A' to 'B'" "false"
 // ERROR: <html>Type mismatch.<table><tr><td>Required:</td><td>B</td></tr><tr><td>Found:</td><td>A</td></tr></table></html>
+// ACTION: Change 'foo' function return type to 'A'
 open class A
 class B : A()
 
