a9638d9fa4fbda85e5f97275bdab5f4c3fcbdda3
Maxim Shafirov <max@jetbrains.com>
2013-04-30 18:18:17 +0400

    "Opposite" for with called let.

--- libraries/stdlib/src/kotlin/Standard.kt
+++ libraries/stdlib/src/kotlin/Standard.kt
@@ -56,3 +56,8 @@ public inline fun <T> run(f: () -> T) : T = f()
  * Execute f with given receiver
  */
 public inline fun <T, R> with(receiver: T, f: T.() -> R) : R = receiver.f()
+
+/**
+ * Converts receiver to body parameter
+*/
+public inline fun <T:Any, R> T.let(f: (T) -> R): R = f(this)
