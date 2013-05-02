ea14607576e16314f5b0067482bcc9cf0e03c9a2
Jack Zhou <yz282@cornell.edu>
2013-05-01 22:52:07 -0400

    Create from usage: Made CreateGetFromUsage7 test clearer.

--- idea/testData/quickfix/createFromUsage/get/afterCreateGetFromUsage7.kt
+++ idea/testData/quickfix/createFromUsage/get/afterCreateGetFromUsage7.kt
@@ -2,10 +2,10 @@
 import java.util.ArrayList
 
 class Foo<T> {
-    fun <T, V> x (y: Foo<List<T>>, w: ArrayList<V>) {
-        val z: Iterable<T> = y["", w]
+    fun <T, V> x (y: Foo<List<T>>, w: ArrayList<V>, v: T) {
+        val z: Iterable<T> = y["", w, v]
     }
-    fun <V> get(s: String, w: ArrayList<V>): T {
+    fun <V, T1> get(s: String, w: ArrayList<V>, v: T1): T {
         throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- idea/testData/quickfix/createFromUsage/get/beforeCreateGetFromUsage7.kt
+++ idea/testData/quickfix/createFromUsage/get/beforeCreateGetFromUsage7.kt
@@ -2,7 +2,7 @@
 import java.util.ArrayList
 
 class Foo<T> {
-    fun <T, V> x (y: Foo<List<T>>, w: ArrayList<V>) {
-        val z: Iterable<T> = y<caret>["", w]
+    fun <T, V> x (y: Foo<List<T>>, w: ArrayList<V>, v: T) {
+        val z: Iterable<T> = y<caret>["", w, v]
     }
 }
