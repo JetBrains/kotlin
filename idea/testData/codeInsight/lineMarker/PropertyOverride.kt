9bb2d90bcaf13101a7833616b6f8042cbdc7a976
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2013-05-06 15:58:14 +0400

    Go to implementation line markers for properties

---
+++ idea/testData/codeInsight/lineMarker/PropertyOverride.kt
@@ -0,0 +1,7 @@
+open class <lineMarker></lineMarker>Base {
+  open var <lineMarker></lineMarker>writable: Int = 12
+}
+
+class SubBase: Base() {
+  override var <lineMarker></lineMarker>writable: Int = 42
+}
