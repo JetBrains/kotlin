e644d85e96e5ae717ba8c07a802be4d46bde8feb
chocolateboy <chocolate@cpan.org>
2013-05-08 05:10:19 +0100

    make the preloader's required profiler argument (time|notime) an optional switch: -pp fixes for broken Windows batch files

--- compiler/cli/bin/kotlinc-js.bat
+++ compiler/cli/bin/kotlinc-js.bat
@@ -2,12 +2,16 @@
 rem based on scalac.bat from the Scala distribution
 rem ##########################################################################
 rem # Copyright 2002-2011, LAMP/EPFL
-rem # Copyright 2011, JetBrains
+rem # Copyright 2011-2013, JetBrains
 rem #
 rem # This is free software; see the distribution for copying conditions.
 rem # There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A
 rem # PARTICULAR PURPOSE.
 rem ##########################################################################
+
+rem We adopt the following conventions:
+rem - System/user environment variables start with a letter
+rem - Local batch variables start with an underscore ('_')
 
 @setlocal
 call :set_home
@@ -18,12 +22,24 @@ if not "%JAVA_HOME%"=="" (
 
 if "%_JAVACMD%"=="" set _JAVACMD=java
 
+set _PROFILE_PRELOADER="notime"
+if "%1"=="-pp" (
+  shift
+  set _PROFILE_PRELOADER="time"
+)
+
 rem We use the value of the JAVA_OPTS environment variable if defined
 set _JAVA_OPTS=-Xmx256M -Xms32M -noverify
 
-"%_JAVACMD%" %_JAVA_OPTS% -cp "%_KOTLIN_HOME%\lib\kotlin-preloader.jar" ^
-    org.jetbrains.jet.preloading.Preloader "${KOTLIN_HOME}/lib/kotlin-compiler.jar" ^
-    org.jetbrains.jet.cli.jvm.K2JVMCompiler 4096 org.jetbrains.jet.cli.js.K2JSCompiler %*
+if "%_PROFILE_PRELOADER"=="time" (
+  "%_JAVACMD%" %_JAVA_OPTS% -cp "%_KOTLIN_HOME%\lib\kotlin-preloader.jar" ^
+    org.jetbrains.jet.preloading.Preloader "%_KOTLIN_HOME%\lib\kotlin-compiler.jar" ^
+    org.jetbrains.jet.cli.js.K2JSCompiler 4096 %_PROFILE_PRELOADER% %1 %2 %3 %4 %5 %6 %7 %8 %9
+) else (
+  "%_JAVACMD%" %_JAVA_OPTS% -cp "%_KOTLIN_HOME%\lib\kotlin-preloader.jar" ^
+    org.jetbrains.jet.preloading.Preloader "%_KOTLIN_HOME%\lib\kotlin-compiler.jar" ^
+    org.jetbrains.jet.cli.js.K2JSCompiler 4096 %_PROFILE_PRELOADER% %*
+)
 goto end
 
 rem ##########################################################################
