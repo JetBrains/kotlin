c5d1226b1995b999269b730eb94710e35fb94d2e
Wojciech Lopata <wlopata@gmail.com>
2013-05-15 23:02:36 +0200

    QuickFix: change type of overridden function to type of overridding function

--- idea/tests/org/jetbrains/jet/plugin/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/jet/plugin/quickfix/QuickFixTestGenerated.java
@@ -958,6 +958,11 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
                 doTest("idea/testData/quickfix/override/typeMismatchOnOverride/beforeCantChangePropertyTypeToMatchOverridenProperties.kt");
             }
             
+            @TestMetadata("beforeCantChangeReturnTypeOfOverriddenFunction.kt")
+            public void testCantChangeReturnTypeOfOverriddenFunction() throws Exception {
+                doTest("idea/testData/quickfix/override/typeMismatchOnOverride/beforeCantChangeReturnTypeOfOverriddenFunction.kt");
+            }
+            
             @TestMetadata("beforeChangeOverriddenPropertyType1.kt")
             public void testChangeOverriddenPropertyType1() throws Exception {
                 doTest("idea/testData/quickfix/override/typeMismatchOnOverride/beforeChangeOverriddenPropertyType1.kt");
@@ -971,6 +976,11 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
             @TestMetadata("beforeChangeOverridingPropertyTypeToFunctionType.kt")
             public void testChangeOverridingPropertyTypeToFunctionType() throws Exception {
                 doTest("idea/testData/quickfix/override/typeMismatchOnOverride/beforeChangeOverridingPropertyTypeToFunctionType.kt");
+            }
+            
+            @TestMetadata("beforeChangeReturnTypeOfOverriddenFunction.kt")
+            public void testChangeReturnTypeOfOverriddenFunction() throws Exception {
+                doTest("idea/testData/quickfix/override/typeMismatchOnOverride/beforeChangeReturnTypeOfOverriddenFunction.kt");
             }
             
             @TestMetadata("beforePropertyReturnTypeMismatchOnOverride.kt")
