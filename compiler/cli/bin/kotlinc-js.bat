ed75c2e4c7f24e3ac9149610c51ab2070ac4a317
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-05-18 00:43:32 +0400

    Scripts fixed: notime added
     #KT-3636 Fixed

--- compiler/cli/bin/kotlinc-js.bat
+++ compiler/cli/bin/kotlinc-js.bat
@@ -23,7 +23,7 @@ set _JAVA_OPTS=-Xmx256M -Xms32M -noverify
 
 "%_JAVACMD%" %_JAVA_OPTS% -cp "%_KOTLIN_HOME%\lib\kotlin-preloader.jar" ^
     org.jetbrains.jet.preloading.Preloader "${KOTLIN_HOME}/lib/kotlin-compiler.jar" ^
-    org.jetbrains.jet.cli.jvm.K2JVMCompiler 4096 org.jetbrains.jet.cli.js.K2JSCompiler %*
+    org.jetbrains.jet.cli.jvm.K2JVMCompiler 4096 notime org.jetbrains.jet.cli.js.K2JSCompiler %*
 goto end
 
 rem ##########################################################################
