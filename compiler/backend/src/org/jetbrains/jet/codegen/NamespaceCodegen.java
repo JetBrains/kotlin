ba2dae4db716f86d92c556a130b90feb0b963a78
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2013-06-13 16:25:09 +0400

    Removed obsolete comment.

--- compiler/backend/src/org/jetbrains/jet/codegen/NamespaceCodegen.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/NamespaceCodegen.java
@@ -279,7 +279,6 @@ public class NamespaceCodegen extends MemberCodegen {
 
         String pathHashCode = Integer.toHexString(FileUtil.toSystemDependentName(file.getVirtualFile().getPath()).hashCode());
 
-        // dollar sign in the end is to prevent synthetic class from having "Test" or other parseable suffix
         // path hashCode to prevent same name / different path collision
         return namespaceInternalName + "$src$" + replaceSpecialSymbols(name.substring(substringFrom, substringTo)) + "$" + pathHashCode;
     }
