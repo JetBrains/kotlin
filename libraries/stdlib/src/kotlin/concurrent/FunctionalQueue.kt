82d3ab7b895dfa731036a1c529db7911a285808d
nk-karpov <nk-karpov@yandex-team.ru>
2013-07-22 18:39:09 +0400

    change FunctionalQueue

--- libraries/stdlib/src/kotlin/concurrent/FunctionalQueue.kt
+++ libraries/stdlib/src/kotlin/concurrent/FunctionalQueue.kt
@@ -1,29 +1,51 @@
 package kotlin.concurrent
-
 import java.util.concurrent.Executor
 
-class FunctionalQueue<T> (
-    val input: FunctionalList<T> = FunctionalList.emptyList<T>(),
-    val output: FunctionalList<T> = FunctionalList.emptyList<T>()) {
-
+class FunctionalQueue<T>(
+        val head : T? = null,
+        val middle : FunctionalQueue<Pair<T, T>>? = null,
+        val tail : T? = null) {
+    private fun size() : Int {
+        var result : Int = 0
+        if (head != null) result++
+        if (tail != null) result++
+        if (middle != null) result += middle.size * 2
+        return result
+    }
     val size : Int
-        get() = input.size + output.size
-
+        get() = size()
     val empty : Boolean
-        get() = size == 0
+        get() = head == null && tail == null && (middle == null || middle.empty)
 
-    public fun add(element: T) : FunctionalQueue<T> = FunctionalQueue<T>(input add element, output)
-
-    public fun addFirst(element: T) : FunctionalQueue<T> = FunctionalQueue<T>(input, output add element)
-
-    public fun removeFirst() : Pair<T, FunctionalQueue<T>> =
-        if(output.empty) {
-            if(input.empty)
-                throw java.util.NoSuchElementException()
-            else
-                FunctionalQueue<T>(FunctionalList.emptyList<T>(), input.reversed()).removeFirst()
+    fun add(e : T) : FunctionalQueue<T> {
+        if (head == null) {
+            return FunctionalQueue(e, middle, tail)
+        } else {
+            if (middle != null) {
+                return FunctionalQueue(null, middle.add(Pair(e, head as T)), tail)
+            } else {
+                return FunctionalQueue(null, FunctionalQueue<Pair<T, T>>(Pair(e, head as T), null, null), tail)
+            }
         }
-        else {
-            Pair(output.head, FunctionalQueue<T>(input, output.tail))
+    }
+    fun removeFirst() : Pair<T, FunctionalQueue<T>> {
+        if (tail != null) {
+            return Pair(tail as T, FunctionalQueue(head, middle, null))
+        } else {
+            if (middle == null) {
+                if (head == null) {
+                    throw java.util.NoSuchElementException()
+                } else {
+                    return Pair(head as T, FunctionalQueue<T>())
+                }
+            } else {
+                val current = middle.removeFirst()
+                if (current.second.empty) {
+                    return Pair(current.first.second, FunctionalQueue(head, null, current.first.first))
+                } else {
+                    return Pair(current.first.second, FunctionalQueue(head, current.second, current.first.first))
+                }
+            }
         }
+    }
 }
