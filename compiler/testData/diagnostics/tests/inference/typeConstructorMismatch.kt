8f299681836b6fabe0ec84da231fd9720f3e20f2
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2013-08-16 19:24:02 +0400

    test data updated
    arguments analyzed even if there's type inference error

--- compiler/testData/diagnostics/tests/inference/typeConstructorMismatch.kt
+++ compiler/testData/diagnostics/tests/inference/typeConstructorMismatch.kt
@@ -4,9 +4,9 @@ package typeConstructorMismatch
 import java.util.*
 
 fun test(set: Set<String>) {
-    <!TYPE_INFERENCE_TYPE_CONSTRUCTOR_MISMATCH!>elemAndList<!>("2", set)
+    <!TYPE_INFERENCE_TYPE_CONSTRUCTOR_MISMATCH!>elemAndList<!>("2", <!TYPE_MISMATCH!>set<!>)
 
-    "".<!TYPE_INFERENCE_TYPE_CONSTRUCTOR_MISMATCH!>elemAndListWithReceiver<!>("", set)
+    "".<!TYPE_INFERENCE_TYPE_CONSTRUCTOR_MISMATCH!>elemAndListWithReceiver<!>("", <!TYPE_MISMATCH!>set<!>)
 }
 
 fun <R> elemAndList(r: R, <!UNUSED_PARAMETER!>t<!>: List<R>): R = r
