335e0d7d1b1285480ae999ed120ae8473f3d0d3e
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2013-09-02 19:20:32 +0400

    test data fixed
    can't implicitly convert to Unit any more in 'if'
    as 'if' is resolved as special function call 'fun IF<T>(T, T)'

--- compiler/testData/diagnostics/tests/controlStructures/kt770.kt351.kt735_StatementType.kt
+++ compiler/testData/diagnostics/tests/controlStructures/kt770.kt351.kt735_StatementType.kt
@@ -106,7 +106,7 @@ fun testImplicitCoercion() {
     val <!UNUSED_VARIABLE!>h<!> = <!IMPLICIT_CAST_TO_UNIT_OR_ANY!>if (false) 4 else {}<!>
 
     bar(if (true) {
-        4
+        <!TYPE_MISMATCH!>4<!>
     }
     else {
         z = <!UNUSED_VALUE!>342<!>
