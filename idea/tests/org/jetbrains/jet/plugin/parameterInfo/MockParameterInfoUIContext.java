63941c4e86d12e2be8d2331af2cb48b1851e6287
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2013-09-06 19:10:12 +0400

    Updated to IDEA 132.106 EAP.

--- idea/tests/org/jetbrains/jet/plugin/parameterInfo/MockParameterInfoUIContext.java
+++ idea/tests/org/jetbrains/jet/plugin/parameterInfo/MockParameterInfoUIContext.java
@@ -40,14 +40,17 @@ public class MockParameterInfoUIContext implements ParameterInfoUIContext {
     }
     
     @Override
-    public void setupUIComponentPresentation(String text, int highlightStartOffset, int highlightEndOffset,
+    public String setupUIComponentPresentation(String text, int highlightStartOffset, int highlightEndOffset,
                                              boolean isDisabled, boolean strikeout,
                                              boolean isDisabledBeforeHighlight, Color background) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Text: (").append(text).append("), Disabled: ").append(isDisabled).
-                append(", Strikeout: ").append(strikeout).append(", Green: ").
-                append(background.equals(JetFunctionParameterInfoHandler.GREEN_BACKGROUND));
-        result.add(stringBuilder.toString());
+        String resultText = "Text: (" + text + "), " +
+                            "Disabled: " + isDisabled + ", " +
+                            "Strikeout: " + strikeout + ", " +
+                            "Green: " + background.equals(JetFunctionParameterInfoHandler.GREEN_BACKGROUND);
+        result.add(resultText);
+
+        // return value not used, just return something
+        return resultText;
     }
 
     @Override
