4cf5b94a4a40e93aee947eafe18432161de4e71e
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-09-06 11:20:17 +0400

    Test data fixed according to 73ab7a9a341913f4f28634aee7f05a8df5fc17fd

--- idea/testData/templates/anonymous_1.exp.kt
+++ idea/testData/templates/anonymous_1.exp.kt
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities
 fun main(args : Array<String>) {
     SwingUtilities.invokeLater(object : Runnable {
 
-    <caret>    public override fun run() {
+    <caret>    override fun run() {
             throw UnsupportedOperationException()
         }
     })
