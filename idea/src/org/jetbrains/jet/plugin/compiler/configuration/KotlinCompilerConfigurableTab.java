fa25489a5bda13b323ba7f8ec3b0afa6b7c63dba
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2013-10-04 15:55:02 +0400

    IDEA plugin: dropped the unused setting "JavaScript target version"

--- idea/src/org/jetbrains/jet/plugin/compiler/configuration/KotlinCompilerConfigurableTab.java
+++ idea/src/org/jetbrains/jet/plugin/compiler/configuration/KotlinCompilerConfigurableTab.java
@@ -20,20 +20,15 @@ import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurableEP;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.ex.ConfigurableWrapper;
-import org.jetbrains.annotations.Nls;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
 public class KotlinCompilerConfigurableTab extends ConfigurableWrapper implements Configurable.NoScroll  {
-    private JComboBox javaScriptEcmaCombobox;
     private JPanel contentPane;
 
     public KotlinCompilerConfigurableTab(ConfigurableEP ep) {
         super(ep);
-
-        javaScriptEcmaCombobox.setModel(new DefaultComboBoxModel(new Object[] {"Ecma3", "Ecma5"}));
     }
 
     @Nullable
