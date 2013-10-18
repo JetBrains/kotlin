b28ab5b6977e7dbb1da018612dc0cc9bf8ad0817
Sergey Mashkov <cy6erGn0m@gmail.com>
2013-10-19 00:44:34 +0400

    Tail recursion optimization Check for warning suppression with non-tail recursion call

--- compiler/testData/diagnostics/tests/tailRecursions/functionWithNonTailRecursions.kt
+++ compiler/testData/diagnostics/tests/tailRecursions/functionWithNonTailRecursions.kt
@@ -1,6 +1,10 @@
 TailRecursive fun badTails(x : Int) : Int {
     if (x > 0) {
         return 1 + <!NON_TAIL_RECURSIVE_CALL!>badTails<!>(x - 1)
+    }
+    else if (x == 10) {
+        [suppress("warnings")]
+        return 1 + badTails(x - 1)
     } else if (x == 50) {
         return badTails(x - 1)
     }
