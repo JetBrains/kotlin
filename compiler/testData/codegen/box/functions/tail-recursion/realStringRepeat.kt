8edd82a06a259b4314c349da9d9408602540a5b7
Sergey Mashkov <cy6erGn0m@gmail.com>
2013-10-20 01:30:58 +0400

    Tail recursion optimization few real life tests

---
+++ compiler/testData/codegen/box/functions/tail-recursion/realStringRepeat.kt
@@ -0,0 +1,8 @@
+tailRecursive fun String.repeat(num : Int, acc : StringBuilder = StringBuilder()) : String =
+        if (num == 0) acc.toString()
+        else repeat(num - 1, acc.append(this)!!)
+
+fun box() : String {
+    val s = "a".repeat(10000)
+    return if (s.length == 10000) "OK" else "FAIL: ${s.length}"
+}
