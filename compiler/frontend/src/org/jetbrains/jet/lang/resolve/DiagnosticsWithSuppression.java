23015b5f643545a8d6714e2dea464a9a6d94ef5a
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2013-10-22 16:27:18 +0400

    Get diagnostics for particular element

--- compiler/frontend/src/org/jetbrains/jet/lang/resolve/DiagnosticsWithSuppression.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/resolve/DiagnosticsWithSuppression.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2013 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jetbrains.jet.lang.resolve;
 
 import com.google.common.collect.ImmutableSet;
@@ -43,6 +59,7 @@ public class DiagnosticsWithSuppression implements Diagnostics {
             return !isSuppressed(diagnostic);
         }
     };
+    private final DiagnosticsElementsCache elementsCache = new DiagnosticsElementsCache(this);
 
     public DiagnosticsWithSuppression(@NotNull BindingContext context, @NotNull Collection<Diagnostic> diagnostics) {
         this.context = context;
@@ -70,6 +87,12 @@ public class DiagnosticsWithSuppression implements Diagnostics {
     @Override
     public Collection<Diagnostic> all() {
         return ContainerUtil.filter(diagnostics, filter);
+    }
+
+    @NotNull
+    @Override
+    public Collection<Diagnostic> forElement(@NotNull PsiElement psiElement) {
+        return elementsCache.getDiagnostics(psiElement);
     }
 
     @Override
