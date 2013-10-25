2c93ace78d7bbd516863f8fc1941cf882a0df699
Erokhin Stanislav <Stanislav.Erokhin@jetbrains.com>
2013-10-25 21:35:52 +0400

    Add externalAnnotations ant test and ant-1.8

---
+++ compiler/integration-tests/testData/kotlinCompiler/root1/hello.kt
@@ -0,0 +1,9 @@
+package hello
+
+fun main(args : Array<String>) {
+    for (s in arrayList("a"))
+      println("Hello, $s!")
+    val java: String = j.Java().f()
+    val hello: String = j.Java().f2()
+    println("$java $hello")
+}
