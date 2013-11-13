4348678cea8afc24ddcee24cafaa8b92d87935c7
Nikolay Krasko <goodwinnk@gmail.com>
2013-11-13 02:03:55 -0800

    Add light parameters for functions

--- compiler/jet.as.java.psi/src/org/jetbrains/jet/asJava/KotlinLightMethodForDeclaration.kt
+++ compiler/jet.as.java.psi/src/org/jetbrains/jet/asJava/KotlinLightMethodForDeclaration.kt
@@ -23,13 +23,37 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNamedElement
 import org.jetbrains.jet.lang.psi.JetDeclaration
+import com.intellij.psi.PsiParameterList
+import org.jetbrains.jet.plugin.JetLanguage
+import kotlin.properties.Delegates
+import org.jetbrains.jet.asJava.light.LightParameter
+import org.jetbrains.jet.asJava.light.LightParameterListBuilder
 import org.jetbrains.jet.lang.resolve.java.jetAsJava.KotlinLightMethod
 import com.intellij.psi.PsiParameterList
 import org.jetbrains.jet.plugin.JetLanguage
 import kotlin.properties.Delegates
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiModificationTracker
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.openapi.application.Result
+import com.intellij.psi.util.CachedValue
 
 public class KotlinLightMethodForDeclaration(manager: PsiManager, val method: PsiMethod, val jetDeclaration: JetDeclaration, containingClass: PsiClass):
         LightMethod(manager, method, containingClass), KotlinLightMethod {
+
+    private val paramsList: CachedValue<PsiParameterList> by Delegates.blockingLazy {
+        val cacheManager = CachedValuesManager.getManager(method.getProject())
+        cacheManager.createCachedValue<PsiParameterList>({
+            val parameterBuilder = LightParameterListBuilder(getManager(), JetLanguage.INSTANCE)
+
+            for ((index, parameter) in method.getParameterList().getParameters().withIndices()) {
+                val lightParameter = LightParameter(parameter.getName() ?: "p$index", parameter.getType(), this, JetLanguage.INSTANCE)
+                parameterBuilder.addParameter(lightParameter)
+            }
+
+            CachedValueProvider.Result.create(parameterBuilder, PsiModificationTracker.OUT_OF_CODE_BLOCK_MODIFICATION_COUNT)
+        }, false)
+    }
 
     override fun getNavigationElement() : PsiElement = jetDeclaration
     override fun getOriginalElement() : PsiElement = jetDeclaration
@@ -55,6 +79,8 @@ public class KotlinLightMethodForDeclaration(manager: PsiManager, val method: Ps
 
         return super<LightMethod>.isEquivalentTo(another)
     }
+
+    override fun getParameterList(): PsiParameterList = paramsList.getValue()!!
 
     override fun copy(): PsiElement? {
         return KotlinLightMethodForDeclaration(getManager()!!, method, jetDeclaration.copy() as JetDeclaration, getContainingClass()!!)
