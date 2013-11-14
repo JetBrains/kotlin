e3fffa6b0bdeb9d81e03ad5e33e1365ef7568901
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2013-11-14 21:38:51 +0400

    Fix decompiled data for objects and enum entries
    Also prettify it for enum entries: output only names

--- idea/testData/libraries/decompiled/Color.kt
+++ idea/testData/libraries/decompiled/Color.kt
@@ -4,13 +4,11 @@
 package testData.libraries
 
 [[public final enum class Color(rgb: jet.Int) : jet.Enum<testData.libraries.Color> {
-    [[public class object {
-        [public final val BLUE: testData.libraries.Color] /* compiled code */
-
-        [public final val GREEN: testData.libraries.Color] /* compiled code */
-
-        [public final val RED: testData.libraries.Color] /* compiled code */
-    }]]
-
     [internal final val rgb: jet.Int] /* compiled code */
+
+    [[BLUE]]
+
+    [[GREEN]]
+
+    [[RED]]
 }]]
