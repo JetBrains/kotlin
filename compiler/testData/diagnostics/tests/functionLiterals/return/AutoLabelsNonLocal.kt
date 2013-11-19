9d3a7804ccc91adaf26f85e5267f45c8fa0c84dd
svtk <svetlana.isakova@jetbrains.com>
2013-11-19 22:08:55 +0400

    Fixed tests for labels.
    The bug was automatically fixed by previous commit.

--- compiler/testData/diagnostics/tests/functionLiterals/return/AutoLabelsNonLocal.kt
+++ compiler/testData/diagnostics/tests/functionLiterals/return/AutoLabelsNonLocal.kt
@@ -2,9 +2,7 @@ fun f() {
     foo {(): Int ->
         bar {
             (): Int ->
-            // The actual error message should be "return not allowed",
-            // but due to a bug in label resolution it is "unresolved reference"
-            return<!UNRESOLVED_REFERENCE!>@foo<!> 1
+            <!RETURN_NOT_ALLOWED!>return@foo 1<!>
         }
         return@foo 1
     }
