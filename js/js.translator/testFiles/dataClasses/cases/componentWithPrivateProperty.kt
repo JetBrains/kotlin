28ea0794bf40ffa304ffe5991b39d835db642aaf
Alex Şuhan <alex.suhan@gmail.com>
2013-11-26 02:53:47 -0800

    JS backend: make componentWithPrivateProperty test copy() as well

--- js/js.translator/testFiles/dataClasses/cases/componentWithPrivateProperty.kt
+++ js/js.translator/testFiles/dataClasses/cases/componentWithPrivateProperty.kt
@@ -14,5 +14,37 @@ fun box(): String {
     val b = t.c2()
     if (b != "abba") return "b /*$b*/ != abba"
 
+    val v = t.copy()
+
+    val c = v.c1()
+    if (c != 123) return "c /*$c*/ != 123"
+
+    val d = t.c2()
+    if (d != "abba") return "d /*$d*/ != abba"
+
+    val x = t.copy(a = 456)
+
+    val e = x.c1()
+    if (e != 456) return "e /*$e*/ != 456"
+
+    val f = x.c2()
+    if (f != "abba") return "f /*$f*/ != abba"
+
+    val y = t.copy(b = "cafebabe")
+
+    val g = y.c1()
+    if (g != 123) return "g /*$g*/ != 123"
+
+    val h = y.c2()
+    if (h != "cafebabe") return "h /*$h*/ != cafebabe"
+
+    val z = t.copy(a = 456, b = "cafebabe")
+
+    val i = z.c1()
+    if (i != 456) return "i /*$i*/ != 123"
+
+    val j = z.c2()
+    if (j != "cafebabe") return "j /*$j*/ != cafebabe"
+
     return "OK"
 }
