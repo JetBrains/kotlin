9ae4af2e4b7633b04e13ed98d993079a9e36bae6
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2013-11-28 11:58:01 +0400

    Fix compilation

--- idea/src/org/jetbrains/jet/plugin/completion/SmartCompletion.kt
+++ idea/src/org/jetbrains/jet/plugin/completion/SmartCompletion.kt
@@ -238,14 +238,3 @@ private fun <T> MutableCollection<T>.addAll(iterator: Iterator<T>) {
         add(item)
     }
 }
-
-fun<T> accept1(handler: String.(t: T) -> Unit){}
-fun accept2(handler: Int.() -> Unit){}
-
-fun foo(){
-    accept1<String>({
-                accept2({
-                            val s: String = this@accept1
-                        })
-            })
-}
