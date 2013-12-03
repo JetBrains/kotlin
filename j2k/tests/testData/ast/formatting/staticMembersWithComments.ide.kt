30c016fa2a02259fd896b1af5eca32a030378f62
Pavel V. Talanov <talanov.pavel@gmail.com>
2013-12-03 18:17:45 +0400

    Converter tests: Add tests for preserving white spaces between class members

---
+++ j2k/tests/testData/ast/formatting/staticMembersWithComments.ide.kt
@@ -0,0 +1,28 @@
+class F() {
+    class object {
+
+        //c1
+
+        /*c2*/
+
+        fun f1() {
+        }
+
+
+        //c3
+
+
+
+
+        //c4
+
+        fun f2() {
+        }
+        var i: Int = 0
+
+        fun f3() {
+        }
+
+        //c5
+    }
+}
