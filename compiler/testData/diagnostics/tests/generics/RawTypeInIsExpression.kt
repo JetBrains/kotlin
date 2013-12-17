4afa95fe1b251d176a30d64061d7877cb57ffc3e
Andrey Breslav <andrey.breslav@jetbrains.com>
2013-12-17 12:31:25 +0400

    First parameter of Map made covariant

--- compiler/testData/diagnostics/tests/generics/RawTypeInIsExpression.kt
+++ compiler/testData/diagnostics/tests/generics/RawTypeInIsExpression.kt
@@ -1,10 +1,12 @@
 package p
 
+class MMap<K, out V>
+
 public fun foo(a: Any) {
     a is Map<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><Int><!>
     a is <!NO_TYPE_ARGUMENTS_ON_RHS!>Map<!>
-    a is Map<out Any?, Any?>
-    a is Map<*, *>
+    a is MMap<out Any?, Any?>
+    a is MMap<*, *>
     a is Map<<!SYNTAX!><!>>
     a is List<<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!>Map<!>>
     a is <!NO_TYPE_ARGUMENTS_ON_RHS!>List<!>
--- compiler/testData/diagnostics/tests/generics/RawTypeInIsPattern.kt
+++ compiler/testData/diagnostics/tests/generics/RawTypeInIsPattern.kt
@@ -1,9 +1,11 @@
+class MMap<K, out V>
+
 public fun foo(a: Any, <!UNUSED_PARAMETER!>b<!>: <!WRONG_NUMBER_OF_TYPE_ARGUMENTS!>Map<!>) {
     when (a) {
         is Map<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><Int><!> -> {}
         is <!NO_TYPE_ARGUMENTS_ON_RHS!>Map<!> -> {}
-        is Map<out Any?, Any?> -> {}
-        is Map<*, *> -> {}
+        is MMap<out Any?, Any?> -> {}
+        is MMap<*, *> -> {}
         is Map<<!SYNTAX!><!>> -> {}
         is List<<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!>Map<!>> -> {}
         is <!NO_TYPE_ARGUMENTS_ON_RHS!>List<!> -> {}
