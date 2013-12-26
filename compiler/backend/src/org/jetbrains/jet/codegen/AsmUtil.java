d8c5b0236d14afc557cb6a9bc22a40249c4b3a01
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2013-12-26 18:50:41 +0400

    Fix incorrect usage of mapReturnType in param assertions

--- compiler/backend/src/org/jetbrains/jet/codegen/AsmUtil.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/AsmUtil.java
@@ -491,7 +491,7 @@ public class AsmUtil {
             if (type == null || isNullableType(type)) continue;
 
             int index = frameMap.getIndex(parameter);
-            Type asmType = state.getTypeMapper().mapReturnType(type);
+            Type asmType = state.getTypeMapper().mapType(type);
             if (asmType.getSort() == Type.OBJECT || asmType.getSort() == Type.ARRAY) {
                 v.load(index, asmType);
                 v.visitLdcInsn(parameter.getName().asString());
