d6b25612c15a4991a2f8af974ec05a9112d4763b
Pradyoth Kukkapalli <pk382@cornell.edu>
2014-02-03 11:40:24 -0500

    Added some tests and updated the implementation of intention action to match them.

--- generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
@@ -326,6 +326,7 @@ fun main(args: Array<String>) {
             model("intentions/declarations/convertMemberToExtension", testMethod = "doTestConvertMemberToExtension")
             model("intentions/reconstructedType", testMethod = "doTestReconstructType")
             model("intentions/removeUnnecessaryParentheses", testMethod = "doTestRemoveUnnecessaryParentheses")
+            model("intentions/getCallReplacement", testMethod = "doTestGetCallReplacement")
         }
 
         testClass(javaClass<AbstractHierarchyTest>()) {
