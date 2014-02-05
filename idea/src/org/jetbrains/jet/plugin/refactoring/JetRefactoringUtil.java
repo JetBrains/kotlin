64f67f30f087d1c455983714275ce845925b7b37
Tuomas Tynkkynen <dezgeg@gmail.com>
2014-02-05 16:38:22 +0200

    Prevent smartSelectExpression from breaking up 'this@outerClass'
    The previous commit made JetThisExpression not implement
    JetStatementExpression. This had the side effect that
    smartSelectExpression over a label-qualified this expression
    (e.g 'this@outerClass') would now also list a plain 'this'
    as a potential expression. Restore the old behaviour by adding an
    explicit check for JetThisExpression.

--- idea/src/org/jetbrains/jet/plugin/refactoring/JetRefactoringUtil.java
+++ idea/src/org/jetbrains/jet/plugin/refactoring/JetRefactoringUtil.java
@@ -411,7 +411,7 @@ public class JetRefactoringUtil {
                         addExpression = false;
                     }
                 }
-                else if (element.getParent() instanceof JetCallElement) {
+                else if (element.getParent() instanceof JetCallElement || element.getParent() instanceof JetThisExpression) {
                     addExpression = false;
                 }
                 else if (element.getParent() instanceof JetOperationExpression) {
