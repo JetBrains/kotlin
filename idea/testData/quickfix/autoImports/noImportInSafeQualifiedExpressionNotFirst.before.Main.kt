a455febb300efd6ad5683a0e2a414998fb269935
Zack Grannan <zgrannan@gmail.com>
2014-02-09 10:14:06 -0800

    Modified noImportInSafeQualifiedExpressionNotFirst test to account for new intention actions

--- idea/testData/quickfix/autoImports/noImportInSafeQualifiedExpressionNotFirst.before.Main.kt
+++ idea/testData/quickfix/autoImports/noImportInSafeQualifiedExpressionNotFirst.before.Main.kt
@@ -1,5 +1,8 @@
 // "class com.intellij.codeInsight.daemon.impl.quickfix.ImportClassFixBase" "false"
 // ERROR: Unresolved reference: SomeTest
+// ACTION: Convert to if-then
+// ACTION: Disable 'Convert to if-then'
+// ACTION: Edit intention settings
 
 package testing
 
