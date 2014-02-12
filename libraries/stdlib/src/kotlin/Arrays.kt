9c51d280a29ab7bf83c5b5da993962d9c5374b9e
voddan <dgvodopyan@gmail.com>
2014-02-12 19:49:49 +0400

    Implementation of KT-4288
    Getters to access elements of indexed collections via rangers and iterators.
    
    External functions implemented for:
    List, Map, CharSequence, String, Array and primitive arrays (ByteArray, etc).

--- libraries/stdlib/src/kotlin/Arrays.kt
+++ libraries/stdlib/src/kotlin/Arrays.kt
@@ -32,3 +32,62 @@ public val             CharArray.lastIndex : Int
 
 public val <T: Any?> Array<out T>.lastIndex : Int
     get() = this.size - 1
+
+// "Iterable-getters"
+public fun <T> Array<T>.get(indexes: Iterable<Int>): List<T>{
+    val result = listBuilder<T>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun    ByteArray.get(indexes: Iterable<Int>): List<Byte>{
+    val result = listBuilder<Byte>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun   ShortArray.get(indexes: Iterable<Int>): List<Short>{
+    val result = listBuilder<Short>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun     IntArray.get(indexes: Iterable<Int>): List<Int>{
+    val result = listBuilder<Int>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun    LongArray.get(indexes: Iterable<Int>): List<Long>{
+    val result = listBuilder<Long>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun   FloatArray.get(indexes: Iterable<Int>): List<Float>{
+    val result = listBuilder<Float>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun  DoubleArray.get(indexes: Iterable<Int>): List<Double>{
+    val result = listBuilder<Double>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+public fun BooleanArray.get(indexes: Iterable<Int>): List<Boolean>{
+    val result = listBuilder<Boolean>()
+    for(i in indexes){
+        result.add(get(i))
+    }
+    return result.build()
+}
+
