d8066c8d563d38c26e0537e15432bc6329b1c641
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-02-14 15:59:24 +0400

    Delete unused DefaultJetObject

--- core/runtime.jvm/src/kotlin/ExtensionFunctionsImpl.kt
+++ core/runtime.jvm/src/kotlin/ExtensionFunctionsImpl.kt
@@ -18,72 +18,74 @@
 
 package kotlin
 
-public abstract class ExtensionFunctionImpl0<in T, out R> : ExtensionFunction0<T, R>, DefaultJetObject() {
+import java.io.Serializable
+
+public abstract class ExtensionFunctionImpl0<in T, out R> : ExtensionFunction0<T, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl1<in T, in P1, out R> : ExtensionFunction1<T, P1, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl1<in T, in P1, out R> : ExtensionFunction1<T, P1, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl2<in T, in P1, in P2, out R> : ExtensionFunction2<T, P1, P2, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl2<in T, in P1, in P2, out R> : ExtensionFunction2<T, P1, P2, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl3<in T, in P1, in P2, in P3, out R> : ExtensionFunction3<T, P1, P2, P3, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl3<in T, in P1, in P2, in P3, out R> : ExtensionFunction3<T, P1, P2, P3, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl4<in T, in P1, in P2, in P3, in P4, out R> : ExtensionFunction4<T, P1, P2, P3, P4, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl4<in T, in P1, in P2, in P3, in P4, out R> : ExtensionFunction4<T, P1, P2, P3, P4, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl5<in T, in P1, in P2, in P3, in P4, in P5, out R> : ExtensionFunction5<T, P1, P2, P3, P4, P5, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl5<in T, in P1, in P2, in P3, in P4, in P5, out R> : ExtensionFunction5<T, P1, P2, P3, P4, P5, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl6<in T, in P1, in P2, in P3, in P4, in P5, in P6, out R> : ExtensionFunction6<T, P1, P2, P3, P4, P5, P6, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl6<in T, in P1, in P2, in P3, in P4, in P5, in P6, out R> : ExtensionFunction6<T, P1, P2, P3, P4, P5, P6, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl7<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, out R> : ExtensionFunction7<T, P1, P2, P3, P4, P5, P6, P7, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl7<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, out R> : ExtensionFunction7<T, P1, P2, P3, P4, P5, P6, P7, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl8<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, out R> : ExtensionFunction8<T, P1, P2, P3, P4, P5, P6, P7, P8, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl8<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, out R> : ExtensionFunction8<T, P1, P2, P3, P4, P5, P6, P7, P8, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl9<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, out R> : ExtensionFunction9<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl9<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, out R> : ExtensionFunction9<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl10<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, out R> : ExtensionFunction10<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl10<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, out R> : ExtensionFunction10<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl11<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, out R> : ExtensionFunction11<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl11<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, out R> : ExtensionFunction11<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl12<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, out R> : ExtensionFunction12<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl12<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, out R> : ExtensionFunction12<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl13<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, out R> : ExtensionFunction13<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl13<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, out R> : ExtensionFunction13<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl14<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, out R> : ExtensionFunction14<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl14<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, out R> : ExtensionFunction14<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl15<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, out R> : ExtensionFunction15<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl15<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, out R> : ExtensionFunction15<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl16<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, out R> : ExtensionFunction16<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl16<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, out R> : ExtensionFunction16<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl17<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, out R> : ExtensionFunction17<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl17<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, out R> : ExtensionFunction17<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl18<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, out R> : ExtensionFunction18<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl18<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, out R> : ExtensionFunction18<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl19<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, out R> : ExtensionFunction19<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl19<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, out R> : ExtensionFunction19<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl20<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, in P20, out R> : ExtensionFunction20<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl20<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, in P20, out R> : ExtensionFunction20<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl21<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, in P20, in P21, out R> : ExtensionFunction21<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl21<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, in P20, in P21, out R> : ExtensionFunction21<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
-public abstract class ExtensionFunctionImpl22<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, in P20, in P21, in P22, out R> : ExtensionFunction22<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, R>, DefaultJetObject() {
+public abstract class ExtensionFunctionImpl22<in T, in P1, in P2, in P3, in P4, in P5, in P6, in P7, in P8, in P9, in P10, in P11, in P12, in P13, in P14, in P15, in P16, in P17, in P18, in P19, in P20, in P21, in P22, out R> : ExtensionFunction22<T, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P14, P15, P16, P17, P18, P19, P20, P21, P22, R>, Serializable {
     override fun toString() = "${getClass().getGenericSuperclass()}"
 }
