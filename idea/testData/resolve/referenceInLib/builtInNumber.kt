559ea28a6b6ca037286283e268c3d71a5a77ee76
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2014-02-24 18:37:48 +0400

    Dropped extra caret markers and introduced separate marker for referencing from context.

--- idea/testData/resolve/referenceInLib/builtInNumber.kt
+++ idea/testData/resolve/referenceInLib/builtInNumber.kt
@@ -1,6 +1,6 @@
 val a: <caret>Number? = null
 
-// CONTEXT: public abstract fun toDouble() : <caret>Double
+// CONTEXT: public abstract fun toDouble() : <ref-caret>Double
 // RUNTIME
 
 // REF: (jet).Double
