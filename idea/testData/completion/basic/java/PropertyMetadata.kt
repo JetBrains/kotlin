dab6716664985d6fea7de1efbe1c9b874c24f63f
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-03-04 19:19:52 +0400

    Minor: fix test data for PropertyMetadata completion test

--- idea/testData/completion/basic/java/PropertyMetadata.kt
+++ idea/testData/completion/basic/java/PropertyMetadata.kt
@@ -8,5 +8,3 @@ fun firstFun() {
 // INVOCATION_COUNT: 1
 // EXIST: { lookupString:"PropertyMetadata", itemText:"PropertyMetadata", tailText:" (kotlin)" }
 // EXIST: { lookupString:"PropertyMetadataImpl", itemText:"PropertyMetadataImpl", tailText:" (kotlin)" }
-// EXIST: {"lookupString":"PropertyMetadata$$TImpl","tailText":" (kotlin)","typeText":"","itemText":"PropertyMetadata$$TImpl"}
-// NUMBER: 3
