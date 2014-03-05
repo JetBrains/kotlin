f752527e4869082a762dc5d9d123dfd8ad4dfb88
Andrey Breslav <andrey.breslav@jetbrains.com>
2014-03-05 12:02:14 +0400

    Test data fixed after adding a new intention

--- idea/testData/quickfix/typeMismatch/parameterTypeMismatch/beforeChangeFunctionParameterType4.kt
+++ idea/testData/quickfix/typeMismatch/parameterTypeMismatch/beforeChangeFunctionParameterType4.kt
@@ -1,5 +1,10 @@
 // "Change parameter 'z' type of function 'foo' to '(Int) -> String'" "false"
 // ERROR: <html>Type mismatch.<table><tr><td>Required:</td><td>kotlin.Int</td></tr><tr><td>Found:</td><td>kotlin.String</td></tr></table></html>
+
+// ACTION: Disable 'Move lambda function into parentheses'
+// ACTION: Edit intention settings
+// ACTION: Move lambda function into parentheses
+
 fun foo(y: Int = 0, z: (Int) -> String = {""}) {
     foo {
         ""<caret>: Int
