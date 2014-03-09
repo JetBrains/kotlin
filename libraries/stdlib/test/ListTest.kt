5bdbf9c8ce9c6d05d0d541c19be959408fd4f9f5
voddan <dgvodopyan@gmail.com>
2014-03-10 00:33:21 +0400

    fix for iterators

--- libraries/stdlib/test/ListTest.kt
+++ libraries/stdlib/test/ListTest.kt
@@ -87,7 +87,7 @@ class ListTest {
         assertEquals(listOf('B', 'C', 'D'), list.slice(1..3))
         assertEquals(listOf('D', 'C', 'B'), list.slice(3 downTo 1))
 
-        val iter = CompositeIterator(2, 0, 3)
+        val iter = listOf(2, 0, 3)
         assertEquals(listOf('C', 'A', 'D'), list.slice(iter))
     }
 
