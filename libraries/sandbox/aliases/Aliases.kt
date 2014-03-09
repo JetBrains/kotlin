26dbbba84b6cb6a26122ac9146387eb291cb94f5
voddan <dgvodopyan@gmail.com>
2014-03-10 00:33:21 +0400

    created Aliases.kt

---
+++ libraries/sandbox/aliases/Aliases.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010-2014 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kotlin.aliases
+
+/**
+ * This file contains different aliases and shortcuts
+ * for functions in the standart Kotlin library.
+ * This aliases let users to use a short syntax
+ * for some standart functions.
+ *
+ * All functionality of this library is avaliable
+ * in the standart Koltin library via full-named
+ * functions.
+ *
+ * This library is experimental, and the content
+ * may be changed without keeping compatibility,
+ * so it is not recommended to use this aliases
+ * in industrial projects.
+ *
+ * This library is not supposed to be included in
+ * the standart Kotlin library and must be imported
+ * manually.
+ * */
