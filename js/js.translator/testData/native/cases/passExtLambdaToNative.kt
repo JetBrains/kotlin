206be96509eeb0e784848227b758fd43ee3a5221
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2014-03-12 20:56:42 +0400

    JS backend: added tests which pass an extension lambda to native code and visa versa. Added workarounds in some tests because Rhino wrongly implicitly converts first parameter of `Function.call` to `Object`. But it's contradicts to ES5 specification(par. 15.3.4.4).
    (#KT-4238, #KT-4345) In progress

---
+++ js/js.translator/testData/native/cases/passExtLambdaToNative.kt
@@ -0,0 +1,16 @@
+package foo
+
+native
+class A(val v: String)
+
+native
+fun bar(a: A, extLambda: A.(Int, String) -> String): String = noImpl
+
+fun box(): String {
+    val a = A("test")
+
+    val r = bar(a) { i, s -> "${this.v} $i $s"}
+    if (r != "test 4 boo") return r
+
+    return "OK"
+}
