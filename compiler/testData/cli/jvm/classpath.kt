33e59103ecae1e0e96992f824b0b1ac31b1c3231
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2014-03-14 22:10:32 +0400

    Fixed test when JDK 8 is system default.

--- compiler/testData/cli/jvm/classpath.kt
+++ compiler/testData/cli/jvm/classpath.kt
@@ -2,13 +2,13 @@ import sun.nio.cs.SingleByte
 import sun.net.spi.nameservice.dns.DNSNameService
 import javax.crypto.Cipher
 import com.sun.java.browser.plugin2.DOM
-import com.sun.crypto.provider.AESCipher
+import com.sun.crypto.provider.SunJCE
 
 fun box(): String {
     val a = SingleByte() // charsets.jar
     val c = DNSNameService() // dnsns.ajr
     val e : Cipher? = null // jce.jar
-    val f : AESCipher? = null // sunjce_provider.jar
+    val f : SunJCE? = null // sunjce_provider.jar
     val j : DOM? = null // plugin.jar
     return "OK"
 }
