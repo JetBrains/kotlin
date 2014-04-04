1f3797ffb2e82b3710b1173b62b4d13926da0182
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-04-04 18:42:51 +0400

    JetModifierList#getAnnotations() and getAnnotationEntries() by stub

--- compiler/frontend/src/org/jetbrains/jet/lang/psi/JetModifierList.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/psi/JetModifierList.java
@@ -22,8 +22,8 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.jet.JetNodeTypes;
 import org.jetbrains.jet.lang.psi.stubs.PsiJetModifierListStub;
+import org.jetbrains.jet.lang.psi.stubs.elements.JetStubElementTypes;
 import org.jetbrains.jet.lexer.JetModifierKeywordToken;
 import org.jetbrains.jet.lexer.JetToken;
 
@@ -48,12 +48,12 @@ public abstract class JetModifierList extends JetElementImplStub<PsiJetModifierL
 
     @NotNull
     public List<JetAnnotation> getAnnotations() {
-        return findChildrenByType(JetNodeTypes.ANNOTATION);
+        return getStubOrPsiChildrenAsList(JetStubElementTypes.ANNOTATION);
     }
 
     @NotNull
     public List<JetAnnotationEntry> getAnnotationEntries() {
-        List<JetAnnotationEntry> entries = findChildrenByType(JetNodeTypes.ANNOTATION_ENTRY);
+        List<JetAnnotationEntry> entries = getStubOrPsiChildrenAsList(JetStubElementTypes.ANNOTATION_ENTRY);
         List<JetAnnotationEntry> answer = entries.isEmpty() ? null : Lists.newArrayList(entries);
         for (JetAnnotation annotation : getAnnotations()) {
             if (answer == null) answer = new ArrayList<JetAnnotationEntry>();
