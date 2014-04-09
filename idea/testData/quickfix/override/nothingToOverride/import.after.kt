9da2cbd5e8d178f16b1177edd096f905400722c0
Andrey Breslav <andrey.breslav@jetbrains.com>
2014-04-09 14:07:12 +0400

    Duplicating diagnostics removed from test data

--- idea/testData/quickfix/override/nothingToOverride/import.after.kt
+++ idea/testData/quickfix/override/nothingToOverride/import.after.kt
@@ -1,6 +1,5 @@
 // "Change function signature to 'override fun f(a: A)'" "true"
 // ERROR: 'f' overrides nothing
-// ERROR: 'f' overrides nothing
 import a.B
 import a.A
 
--- idea/testData/quickfix/override/nothingToOverride/twoPackages.before.Main.kt
+++ idea/testData/quickfix/override/nothingToOverride/twoPackages.before.Main.kt
@@ -1,6 +1,5 @@
 // "Change function signature to 'override fun f(a: A)'" "true"
 // ERROR: 'f' overrides nothing
-// ERROR: 'f' overrides nothing
 import a.B
 class A {}
 class BB : B() {
