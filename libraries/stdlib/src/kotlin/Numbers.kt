a6c01f82b255e11d047ac71517257a761dba8cd6
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-04-18 19:20:33 +0400

    Make isNaN check language independent, working in JVM & JS

---
+++ libraries/stdlib/src/kotlin/Numbers.kt
@@ -0,0 +1,13 @@
+package kotlin
+
+/**
+ * Returns {@code true} if the specified number is a
+ * Not-a-Number (NaN) value, {@code false} otherwise.
+ */
+fun Double.isNaN() = this != this
+
+/**
+ * Returns {@code true} if the specified number is a
+ * Not-a-Number (NaN) value, {@code false} otherwise.
+ */
+fun Float.isNaN() = this != this
