13f7ca51b0a18f6154fa872f8b3ba6698b46f201
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2014-05-05 16:17:37 +0400

    Regenerate tests

--- idea/tests/org/jetbrains/jet/plugin/intentions/CodeTransformationTestGenerated.java
+++ idea/tests/org/jetbrains/jet/plugin/intentions/CodeTransformationTestGenerated.java
@@ -7,7 +7,6 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
