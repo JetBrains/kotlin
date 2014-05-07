692f60b1d69d2ef31c678849db84e69df7f2f286
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-05-07 17:21:13 +0400

    Uncommented code in data class test for equality and toString

--- libraries/stdlib/test/DataClassTest.kt
+++ libraries/stdlib/test/DataClassTest.kt
@@ -2,22 +2,23 @@ package test.dataclass
 
 import org.junit.Test
 import kotlin.test.*
+import java.io.Serializable
 
 /**
  */
 class DataClassTest {
     Test fun dataClass() {
         val p = Person("James", 43)
-        // TODO no nice toString() yet
         println("Got $p")
+        assertEquals("Person(name=James, age=43)", "$p", "toString")
 
         val (a, b) = p
-        println("a: $a, b: $b")
         assertEquals("James", a, "a")
         assertEquals(43, b, "b")
 
+        assertEquals(Person("James", 43), Person("James", 43), "person equals")
+
         // TODO not implemented yet
-        // assertEquals(Person("James", 43), Person("James", 43), "person equals")
         // assertTrue(Person("ZZZ", 21) > Person("AAA", 21), "person a > b")
     }
 }
