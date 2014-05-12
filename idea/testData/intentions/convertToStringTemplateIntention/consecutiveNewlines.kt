412b10006226f7f143205aa4e73a23fb0477d9a2
Zack Grannan <zgrannan@gmail.com>
2014-05-11 20:49:13 -0700

    Added more tests

---
+++ idea/testData/intentions/convertToStringTemplateIntention/consecutiveNewlines.kt
@@ -0,0 +1,8 @@
+// WITH_RUNTIME
+fun main(args: Array<String>){
+    val y = "abcd" +<caret> listOf( 1,
+
+
+
+    2 )
+}
---
+++ idea/testData/intentions/convertToStringTemplateIntention/multilineString.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>){
+    var r = "a"
+    val x = "foo" +<caret> """bar
+    $r"""
+}
