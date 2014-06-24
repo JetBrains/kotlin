0716b0992066511f3d54e4f2443ce26f74e9d5b7
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2014-06-24 11:00:30 +0400

    Fix errors in intentions after parser fixes

--- idea/testData/intentions/branched/ifThenToDoubleBang/noCondition.kt
+++ idea/testData/intentions/branched/ifThenToDoubleBang/noCondition.kt
@@ -1,7 +1,5 @@
 // WITH_RUNTIME
 // IS_APPLICABLE: false
-// ERROR: <html>Type mismatch.<table><tr><td>Required:</td><td>kotlin.Boolean</td></tr><tr><td>Found:</td><td>() &rarr; kotlin.String?</td></tr></table></html>
-// ERROR: Condition must be of type kotlin.Boolean, but is of type () -> kotlin.String?
 fun main(args: Array<String>) {
     val foo: String? = "foo"
     if<caret> {
--- idea/testData/intentions/swapBinaryExpression/is.kt
+++ idea/testData/intentions/swapBinaryExpression/is.kt
@@ -1,4 +1,5 @@
 // IS_APPLICABLE: false
+// ERROR: Expression 'if "test" is String' of type 'kotlin.Unit' cannot be invoked as a function. The function invoke() is not found
 
 fun doSomething<T>(a: T) {}
 
