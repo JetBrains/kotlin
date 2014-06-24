c2aa824242656d8d0e5bf41baf9234931ef57048
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2014-06-24 15:26:09 +0400

    Implement light method equality based on the original declarations  #KT-4350 Fixed

--- compiler/jet.as.java.psi/src/org/jetbrains/jet/asJava/KotlinLightMethodForDeclaration.kt
+++ compiler/jet.as.java.psi/src/org/jetbrains/jet/asJava/KotlinLightMethodForDeclaration.kt
@@ -110,4 +110,9 @@ public class KotlinLightMethodForDeclaration(
     }
 
     override fun getUseScope(): SearchScope = origin.getUseScope()
+
+    override fun equals(other: Any?): Boolean =
+            other is KotlinLightMethodForDeclaration && getName() == other.getName() && origin == other.origin
+
+    override fun hashCode(): Int = getName().hashCode() * 31 + origin.hashCode()
 }
