e2f5b7b5dad342667c501f6b1f8d28818bd087f2
Denis Zharkov <bintree@eblans.org>
2014-06-25 17:27:48 +0400

    [boxing-optimization] Tests updated after integration with optimization subsystem

--- compiler/testData/asJava/lightClasses/delegation/Function.java
+++ compiler/testData/asJava/lightClasses/delegation/Function.java
@@ -1,6 +1,6 @@
 public final class Derived implements kotlin.jvm.internal.KObject, Base {
-    public Derived(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "x") Base x) { /* compiled code */ }
+    public Derived(@jet.runtime.typeinfo.JetValueParameter(name = "x") @org.jetbrains.annotations.NotNull Base x) { /* compiled code */ }
 
     @org.jetbrains.annotations.NotNull
-    public java.lang.String baz(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "s") java.lang.String s) { /* compiled code */ }
+    public java.lang.String baz(@jet.runtime.typeinfo.JetValueParameter(name = "s") @org.jetbrains.annotations.NotNull java.lang.String s) { /* compiled code */ }
 }
--- compiler/testData/asJava/lightClasses/nullabilityAnnotations/ClassWithConstructor.java
+++ compiler/testData/asJava/lightClasses/nullabilityAnnotations/ClassWithConstructor.java
@@ -1,3 +1,3 @@
 public final class ClassWithConstructor implements kotlin.jvm.internal.KObject {
-    public ClassWithConstructor(@org.jetbrains.annotations.Nullable @jet.runtime.typeinfo.JetValueParameter(name = "nullable", type = "?") java.lang.String nullable, @org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "notNull") java.lang.String notNull) { /* compiled code */ }
+    public ClassWithConstructor(@jet.runtime.typeinfo.JetValueParameter(name = "nullable", type = "?") @org.jetbrains.annotations.Nullable java.lang.String nullable, @jet.runtime.typeinfo.JetValueParameter(name = "notNull") @org.jetbrains.annotations.NotNull java.lang.String notNull) { /* compiled code */ }
 }
