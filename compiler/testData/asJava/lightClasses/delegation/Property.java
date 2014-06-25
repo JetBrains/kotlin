3fa5fc7b11b39242dd36f95a2e17fa6e7aa2e8e5
Denis Zharkov <denis.zharkov@jetbrains.com>
2014-06-25 17:27:48 +0400

    Tests updated after integration with optimization subsystem

--- compiler/testData/asJava/lightClasses/delegation/Property.java
+++ compiler/testData/asJava/lightClasses/delegation/Property.java
@@ -1,5 +1,5 @@
 public final class Derived implements kotlin.jvm.internal.KObject, Base {
-    public Derived(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "x") Base x) { /* compiled code */ }
+    public Derived(@jet.runtime.typeinfo.JetValueParameter(name = "x") @org.jetbrains.annotations.NotNull Base x) { /* compiled code */ }
 
     @org.jetbrains.annotations.NotNull
     public java.lang.String getBoo() { /* compiled code */ }
--- compiler/testData/asJava/lightClasses/nullabilityAnnotations/_DefaultPackage.java
+++ compiler/testData/asJava/lightClasses/nullabilityAnnotations/_DefaultPackage.java
@@ -9,14 +9,14 @@ public final class _DefaultPackage {
     @org.jetbrains.annotations.NotNull
     public static final java.lang.String getNotNullVar() { /* compiled code */ }
 
-    public static final void setNotNullVar(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "<set-?>") java.lang.String p) { /* compiled code */ }
+    public static final void setNotNullVar(@jet.runtime.typeinfo.JetValueParameter(name = "<set-?>") @org.jetbrains.annotations.NotNull java.lang.String p) { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
     @org.jetbrains.annotations.NotNull
     public static final java.lang.String getNotNullVarWithGetSet() { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
-    public static final void setNotNullVarWithGetSet(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "v") java.lang.String v) { /* compiled code */ }
+    public static final void setNotNullVarWithGetSet(@jet.runtime.typeinfo.JetValueParameter(name = "v") @org.jetbrains.annotations.NotNull java.lang.String v) { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
     public static final java.lang.String getNullableVal() { /* compiled code */ }
@@ -28,14 +28,14 @@ public final class _DefaultPackage {
     @org.jetbrains.annotations.Nullable
     public static final java.lang.String getNullableVar() { /* compiled code */ }
 
-    public static final void setNullableVar(@org.jetbrains.annotations.Nullable @jet.runtime.typeinfo.JetValueParameter(name = "<set-?>", type = "?") java.lang.String p) { /* compiled code */ }
+    public static final void setNullableVar(@jet.runtime.typeinfo.JetValueParameter(name = "<set-?>", type = "?") @org.jetbrains.annotations.Nullable java.lang.String p) { /* compiled code */ }
 
     @org.jetbrains.annotations.NotNull
     @org.jetbrains.annotations.Nullable
     public static final java.lang.String getNullableVarWithGetSet() { /* compiled code */ }
 
     @org.jetbrains.annotations.NotNull
-    public static final void setNullableVarWithGetSet(@org.jetbrains.annotations.Nullable @jet.runtime.typeinfo.JetValueParameter(name = "v", type = "?") java.lang.String v) { /* compiled code */ }
+    public static final void setNullableVarWithGetSet(@jet.runtime.typeinfo.JetValueParameter(name = "v", type = "?") @org.jetbrains.annotations.Nullable java.lang.String v) { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
     public static final java.lang.String getPrivateN() { /* compiled code */ }
@@ -44,7 +44,7 @@ public final class _DefaultPackage {
     public static final java.lang.String getPrivateNn() { /* compiled code */ }
 
     @org.jetbrains.annotations.NotNull
-    public static final java.lang.String notNull(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "a") java.lang.String a) { /* compiled code */ }
+    public static final java.lang.String notNull(@jet.runtime.typeinfo.JetValueParameter(name = "a") @org.jetbrains.annotations.NotNull java.lang.String a) { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
     @org.jetbrains.annotations.NotNull
@@ -54,7 +54,7 @@ public final class _DefaultPackage {
     public static final java.lang.String notNullWithNN() { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
-    public static final java.lang.String nullable(@org.jetbrains.annotations.Nullable @jet.runtime.typeinfo.JetValueParameter(name = "a", type = "?") java.lang.String a) { /* compiled code */ }
+    public static final java.lang.String nullable(@jet.runtime.typeinfo.JetValueParameter(name = "a", type = "?") @org.jetbrains.annotations.Nullable java.lang.String a) { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
     public static final java.lang.String nullableWithN() { /* compiled code */ }
@@ -64,5 +64,5 @@ public final class _DefaultPackage {
     public static final java.lang.String nullableWithNN() { /* compiled code */ }
 
     @org.jetbrains.annotations.Nullable
-    public static final java.lang.String privateFun(@org.jetbrains.annotations.NotNull @jet.runtime.typeinfo.JetValueParameter(name = "a") java.lang.String a, @org.jetbrains.annotations.Nullable @jet.runtime.typeinfo.JetValueParameter(name = "b", type = "?") java.lang.String b) { /* compiled code */ }
+    public static final java.lang.String privateFun(@jet.runtime.typeinfo.JetValueParameter(name = "a") @org.jetbrains.annotations.NotNull java.lang.String a, @jet.runtime.typeinfo.JetValueParameter(name = "b", type = "?") @org.jetbrains.annotations.Nullable java.lang.String b) { /* compiled code */ }
 }
