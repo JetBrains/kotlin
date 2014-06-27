9aa5e85bde116f6597c49d7f767179912c842caa
Denis Zharkov <denis.zharkov@jetbrains.com>
2014-06-27 14:39:59 +0400

    Redundant null check of boxed values detecting and cleaing out

--- compiler/backend/src/org/jetbrains/jet/codegen/optimization/OptimizationMethodVisitor.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/optimization/OptimizationMethodVisitor.java
@@ -20,6 +20,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jet.codegen.inline.InlineCodegenUtil;
 import org.jetbrains.jet.codegen.optimization.boxing.RedundantBoxingMethodTransformer;
+import org.jetbrains.jet.codegen.optimization.boxing.RedundantNullCheckMethodTransformer;
 import org.jetbrains.jet.codegen.optimization.transformer.MethodTransformer;
 import org.jetbrains.org.objectweb.asm.MethodVisitor;
 import org.jetbrains.org.objectweb.asm.tree.LocalVariableNode;
@@ -29,7 +30,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OptimizationMethodVisitor extends MethodVisitor {
-    private final MethodTransformer methodTransformer = new RedundantBoxingMethodTransformer(null);
+    private final MethodTransformer methodTransformer = new RedundantNullCheckMethodTransformer(
+            new RedundantBoxingMethodTransformer(null)
+    );
+
     private final MethodNode methodNode;
     private final MethodVisitor delegate;
 
