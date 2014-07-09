1e8769b5f79fe00e1b0fe75bf8ce47d288050b59
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-07-09 16:24:36 +0400

    android UI xml bridge initial
    - add frontend.android module
    - add xml ids to kotlin extension properties converter
    - add converter basic functional test

---
+++ compiler/frontend.android/src/org/jetbrains/jet/lang/resolve/android/AndroidXmlHandler.kt
@@ -0,0 +1,40 @@
+package org.jetbrains.jet.lang.resolve.android
+
+import org.xml.sax.helpers.DefaultHandler
+import org.xml.sax.Attributes
+import java.util.HashMap
+
+class AndroidXmlHandler(val idCallback: (String)-> Unit): DefaultHandler() {
+
+    override fun startDocument() {
+        super<DefaultHandler>.startDocument()
+    }
+
+    override fun endDocument() {
+        super<DefaultHandler>.endDocument()
+    }
+
+    override fun startElement(uri: String, localName: String, qName: String, attributes: Attributes) {
+        val hashMap = attributes.toMap()
+        val s = hashMap["id"]
+        val idPrefix = "@+id/"
+        if (s != null && s.startsWith(idPrefix)) idCallback(s.replace(idPrefix, ""))
+    }
+
+    override fun endElement(uri: String?, localName: String, qName: String) {
+
+    }
+
+    public fun Attributes.toMap(): HashMap<String, String> {
+        val res = HashMap<String, String>()
+        for (index in 0..getLength()-1) {
+            val attrName = getLocalName(index)!!
+            val attrVal = getValue(index)!!
+            res[attrName] = attrVal
+        }
+        return res
+    }
+}
+
+
+
