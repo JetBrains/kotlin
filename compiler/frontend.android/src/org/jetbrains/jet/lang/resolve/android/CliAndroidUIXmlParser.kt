d557fa8d5dd4ec4b74d7a786a97d53af1b99796c
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-07-18 18:08:14 +0400

    event based file processing
    - use VirtualFileListener to wait for ui xml file updates
    - optimize imports

--- compiler/frontend.android/src/org/jetbrains/jet/lang/resolve/android/CliAndroidUIXmlParser.kt
+++ compiler/frontend.android/src/org/jetbrains/jet/lang/resolve/android/CliAndroidUIXmlParser.kt
@@ -1,6 +1,4 @@
 package org.jetbrains.jet.lang.resolve.android
 
-import com.intellij.openapi.project.Project
-
 class CliAndroidUIXmlParser(override val searchPath: String?): AndroidUIXmlParser()
 
