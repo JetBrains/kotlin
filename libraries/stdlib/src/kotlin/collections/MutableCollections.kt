928020e3e8d415e6c1ecbf9f68fc7dc045687a14
Denis Zharkov <denis.zharkov@jetbrains.com>
2014-07-21 21:21:13 +0400

    Overloaded += and -= for MutableCollection: Actually this may change the behavior of code in some cases, e.g. var x = arrayListOf("a") x += "b" // was the same as x = x + "b" (created NEW list and concatenated, stored in var, not changed the object)
    After my changes x += "b" actually changes the object,
    as IMO it's expected to do so.

--- libraries/stdlib/src/kotlin/collections/MutableCollections.kt
+++ libraries/stdlib/src/kotlin/collections/MutableCollections.kt
@@ -1,6 +1,22 @@
 package kotlin
 
 /**
+ * Adds element to this [[MutableCollection]]
+ * Operator overloading for +=
+ */
+public fun <T> MutableCollection<in T>.plusAssign(element: T) {
+    add(element)
+}
+
+/**
+ * Removes element from this [[MutableCollection]]
+ * Operator overloading for -=
+ */
+public fun <T> MutableCollection<in T>.minusAssign(element: T) {
+    remove(element)
+}
+
+/**
  * Adds all elements of the given *iterable* to this [[MutableCollection]]
  */
 public fun <T> MutableCollection<in T>.addAll(iterable: Iterable<T>) {
@@ -11,6 +27,14 @@ public fun <T> MutableCollection<in T>.addAll(iterable: Iterable<T>) {
 }
 
 /**
+ * Adds all elements of the given *iterable* to this [[MutableCollection]]
+ * Operator overloading for +=
+ */
+public fun <T> MutableCollection<in T>.plusAssign(iterable: Iterable<T>) {
+    addAll(iterable)
+}
+
+/**
  * Adds all elements of the given *stream* to this [[MutableCollection]]
  */
 public fun <T> MutableCollection<in T>.addAll(stream: Stream<T>) {
@@ -18,10 +42,26 @@ public fun <T> MutableCollection<in T>.addAll(stream: Stream<T>) {
 }
 
 /**
+ * Adds all elements of the given *stream* to this [[MutableCollection]]
+ * Operator overloading for +=
+ */
+public fun <T> MutableCollection<in T>.plusAssign(stream: Stream<T>) {
+    addAll(stream)
+}
+
+/**
  * Adds all elements of the given *array* to this [[MutableCollection]]
  */
 public fun <T> MutableCollection<in T>.addAll(array: Array<T>) {
     for (item in array) add(item)
+}
+
+/**
+ * Adds all elements of the given *array* to this [[MutableCollection]]
+ * Operator overloading for +=
+ */
+public fun <T> MutableCollection<in T>.plusAssign(array: Array<T>) {
+    addAll(array)
 }
 
 /**
@@ -35,6 +75,14 @@ public fun <T> MutableCollection<in T>.removeAll(iterable: Iterable<T>) {
 }
 
 /**
+ * Removes all elements of the given *iterable* from this [[MutableCollection]]
+ * Operator overloading for -=
+ */
+public fun <T> MutableCollection<in T>.minusAssign(iterable: Iterable<T>) {
+    removeAll(iterable)
+}
+
+/**
  * Removes all elements of the given *stream* from this [[MutableCollection]]
  */
 public fun <T> MutableCollection<in T>.removeAll(stream: Stream<T>) {
@@ -42,10 +90,26 @@ public fun <T> MutableCollection<in T>.removeAll(stream: Stream<T>) {
 }
 
 /**
+ * Removes all elements of the given *stream* from this [[MutableCollection]]
+ * Operator overloading for -=
+ */
+public fun <T> MutableCollection<in T>.minusAssign(stream: Stream<T>) {
+    removeAll(stream)
+}
+
+/**
  * Removes all elements of the given *array* from this [[MutableCollection]]
  */
 public fun <T> MutableCollection<in T>.removeAll(array: Array<T>) {
     for (item in array) remove(item)
+}
+
+/**
+ * Removes all elements of the given *array* from this [[MutableCollection]]
+ * Operator overloading for -=
+ */
+public fun <T> MutableCollection<in T>.minusAssign(array: Array<T>) {
+    removeAll(array)
 }
 
 /**
