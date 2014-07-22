917938e57b84542de18fb1f3472210c85652769a
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-07-22 17:22:17 +0400

    Update copyrights

--- js/js.tests/test/org/jetbrains/k2js/test/rhino/CompositeRhinoResultsChecker.java
+++ js/js.tests/test/org/jetbrains/k2js/test/rhino/CompositeRhinoResultsChecker.java
@@ -1,11 +1,9 @@
-/**
+/*
+ * Copyright 2010-2014 JetBrains s.r.o.
  *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jetbrains.k2js.test.rhino;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
-/**
- */
 public class CompositeRhinoResultsChecker implements RhinoResultChecker {
     private final RhinoResultChecker[] children;
 
