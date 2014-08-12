ab44343e1c8e4a9d9fb7b77db2c239a391a3a22f
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2014-08-12 20:30:51 +0400

    Made incremental cache per-module.
    Reused IDEA framework which manages per-module storage.
    
    Original commit: 5fade9a5a65d3a919adc064523c59ef388a2812b

--- jps/jps-plugin/src/org/jetbrains/jet/jps/incremental/IncrementlaCacheProviderImpl.kt
+++ jps/jps-plugin/src/org/jetbrains/jet/jps/incremental/IncrementlaCacheProviderImpl.kt
@@ -17,11 +17,15 @@
 package org.jetbrains.jet.jps.incremental
 
 import org.jetbrains.jet.lang.resolve.kotlin.incremental.IncrementalCacheProvider
-import java.io.File
+import org.jetbrains.jps.incremental.ModuleBuildTarget
 import org.jetbrains.jet.lang.resolve.kotlin.incremental.IncrementalCache
+import kotlin.properties.Delegates
 
-public class IncrementalCacheProviderImpl: IncrementalCacheProvider {
-    override fun getIncrementalCache(baseDir: File): IncrementalCache {
-        return IncrementalCacheImpl(baseDir)
+public class IncrementalCacheProviderImpl(caches: Map<ModuleBuildTarget, IncrementalCacheImpl>): IncrementalCacheProvider {
+    private val idToCache = caches.mapKeys { it.key.getId()!! }
+
+    override fun getIncrementalCache(moduleId: String): IncrementalCache {
+        return idToCache[moduleId]!!
     }
 }
+
