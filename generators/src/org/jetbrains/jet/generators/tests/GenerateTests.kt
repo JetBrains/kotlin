e7ebf6fda244e14e1fe125dd25b5fe92f865d3c4
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2014-08-19 14:00:18 +0400

    Minor: change debugger tests structure

--- generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
@@ -564,9 +564,8 @@ fun main(args: Array<String>) {
         }
 
         testClass(javaClass<AbstractKotlinEvaluateExpressionTest>()) {
-            model("debugger/tinyApp/src/evaluate/singleBreakpoint", testMethod = "doSingleBreakpointTest")
+            model("debugger/tinyApp/src/evaluate/singleBreakpoint", testMethod = "doSingleBreakpointTest", recursive = true)
             model("debugger/tinyApp/src/evaluate/multipleBreakpoints", testMethod = "doMultipleBreakpointsTest", recursive = true)
-            model("debugger/tinyApp/src/evaluate/frame", testMethod = "doSingleBreakpointTest")
         }
 
         testClass(javaClass<AbstractStubBuilderTest>()) {
