1a02eb4c6cdea1efd9e493cc21476cb6457031bf
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-09-19 19:27:22 +0400

    Include package part name to top level closure names
     #KT-4234 Fixed
     #KT-4496 Fixed
    
    Original commit: c30aa7db84c9936fde44e5e847bb1d326ef10fd6

--- jps/jps-plugin/test/org/jetbrains/jet/jps/build/AbstractIncrementalJpsTest.kt
+++ jps/jps-plugin/test/org/jetbrains/jet/jps/build/AbstractIncrementalJpsTest.kt
@@ -33,6 +33,7 @@ import kotlin.test.fail
 import java.util.HashMap
 import org.jetbrains.jet.utils.keysToMap
 import org.jetbrains.jps.incremental.messages.BuildMessage
+import java.util.regex.Pattern
 
 public abstract class AbstractIncrementalJpsTest : JpsBuildTestCase() {
     private var testDataDir: File by Delegates.notNull()
@@ -239,18 +240,20 @@ public abstract class AbstractIncrementalJpsTest : JpsBuildTestCase() {
         override fun isEnabled(): Boolean = true
 
         override fun logLine(message: String?) {
-
             fun String.replaceHashWithStar(): String {
-                if (this contains "Package$") {
-                    val lastDollar = this.lastIndexOf('$')
-                    if (lastDollar != -1 && substring(lastDollar + 1).matches("[0-9a-f]{1,8}\\.class")) {
-                        return substring(0, lastDollar) + "$*.class"
-                    }
+                val matcher = STRIP_PACKAGE_PART_HASH_PATTERN.matcher(this)
+                if (matcher.find()) {
+                    return matcher.replaceAll("\\$*")
                 }
                 return this
             }
 
             logBuf.append(message!!.trimLeading(rootPath + "/").replaceHashWithStar()).append('\n')
+        }
+
+        class object {
+            // We suspect sequences of several consecutive hexadecimal digits to be a package part hash code
+            val STRIP_PACKAGE_PART_HASH_PATTERN = Pattern.compile("\\$([0-9a-f]{5,8})")
         }
     }
 
