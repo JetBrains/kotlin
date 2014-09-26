54799b1a344c83b063b3a077d52e1a3d4aa27948
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-09-26 18:47:27 +0400

    Internal visibility works the same as public
    Fix test data
    This change is needed to preserve old behaviour for M9
    isFriend() utilities become unused after this change

--- idea/testData/multiModuleHighlighting/testRoot/m3Test/m3Test.kt
+++ idea/testData/multiModuleHighlighting/testRoot/m3Test/m3Test.kt
@@ -11,19 +11,19 @@ public fun publicInM3Test() {
 
 fun access() {
     <error descr="[INVISIBLE_MEMBER] Cannot access 'privateInM1': it is 'private' in 'shared'">privateInM1</error>()
-    <error descr="[INVISIBLE_MEMBER] Cannot access 'internalInM1': it is 'internal' in 'shared'">internalInM1</error>()
+    internalInM1()
     publicInM1()
 
     <error descr="[INVISIBLE_MEMBER] Cannot access 'privateInM1Test': it is 'private' in 'test'">privateInM1Test</error>()
-    <error descr="[INVISIBLE_MEMBER] Cannot access 'internalInM1Test': it is 'internal' in 'test'">internalInM1Test</error>()
+    internalInM1Test()
     publicInM1Test()
 
     <error descr="[INVISIBLE_MEMBER] Cannot access 'privateInM2': it is 'private' in 'shared'">privateInM2</error>()
-    <error descr="[INVISIBLE_MEMBER] Cannot access 'internalInM2': it is 'internal' in 'shared'">internalInM2</error>()
+    internalInM2()
     publicInM2()
 
     <error descr="[INVISIBLE_MEMBER] Cannot access 'privateInM2Test': it is 'private' in 'test'">privateInM2Test</error>()
-    <error descr="[INVISIBLE_MEMBER] Cannot access 'internalInM2Test': it is 'internal' in 'test'">internalInM2Test</error>()
+    internalInM2Test()
     publicInM2Test()
 
     <error descr="[INVISIBLE_MEMBER] Cannot access 'privateInM3': it is 'private' in 'shared'">privateInM3</error>()
