b05b03a9c019ae9f478a0bf92c37987e743a8c5e
Valentin Kipyatkov <valentin@jetbrains.com>
2014-10-03 19:57:37 +0400

    Corrected gold data - formatting became better after binding comments inside

--- j2k/tests/testData/fileOrElement/comments/comments.kt
+++ j2k/tests/testData/fileOrElement/comments/comments.kt
@@ -4,9 +4,7 @@ package foo
 // we use package 'foo'
 
 // imports:
-import java.util.ArrayList
-
-// we need ArrayList
+import java.util.ArrayList // we need ArrayList
 
 // let's declare a class:
 class A /* just a sample name*/ : Runnable /* let's implement Runnable */ {
