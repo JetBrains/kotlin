12e76f3d044aa2424fb76de62ff24fd0158d1d72
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2014-10-08 16:19:08 +0400

    JS backend: test for KT-2901: nullable type in string template

---
+++ js/js.translator/testData/expression/string/cases/nullableTypeInStringTemplate.kt
@@ -0,0 +1,19 @@
+// KT-2901 nullable type in string template
+package foo
+
+fun box(): String {
+    var a: Int? = null
+
+    assertEquals("a: null", "a: ${a}")
+
+    a = 10
+    assertEquals("a: 10", "a: ${a}")
+
+    var s: String? = null
+    assertEquals("s: null", "s: $s")
+
+    s = "test"
+    assertEquals("s: test", "s: $s")
+
+    return "OK"
+}
