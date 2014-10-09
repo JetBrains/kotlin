ecb955f5b3a3f0e1f30a96245003e959fd62d8ba
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2014-10-09 14:44:01 +0400

    Stdlib: split OrderingJVM.kt to common and jvm parts

--- libraries/stdlib/src/kotlin/OrderingJVM.kt
+++ libraries/stdlib/src/kotlin/OrderingJVM.kt
@@ -3,37 +3,6 @@ package kotlin
 import java.util.Comparator
 
 /**
-* Helper method for implementing [[Comparable]] methods using a list of functions
-* to calculate the values to compare
-*/
-public fun <T : Any> compareBy(a: T?, b: T?, vararg functions: T.() -> Comparable<*>?): Int {
-    require(functions.size > 0)
-    if (a === b) return 0
-    if (a == null) return - 1
-    if (b == null) return 1
-    for (fn in functions) {
-        val v1 = a.fn()
-        val v2 = b.fn()
-        val diff = compareValues(v1, v2)
-        if (diff != 0) return diff
-    }
-    return 0
-}
-
-/**
- * Compares the two values which may be [[Comparable]] otherwise
- * they are compared via [[#equals()]] and if they are not the same then
- * the [[#hashCode()]] method is used as the difference
- */
-public fun <T : Comparable<*>> compareValues(a: T?, b: T?): Int {
-    if (a === b) return 0
-    if (a == null) return - 1
-    if (b == null) return 1
-
-    return (a as Comparable<Any?>).compareTo(b)
-}
-
-/**
  * Creates a comparator using the sequence of functions used to calculate a value to compare on
  */
 public fun <T> comparator(vararg functions: T.() -> Comparable<*>?): Comparator<T> {
