c071ac854e3ed92def9b0e68382269aafaa0c438
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-10-16 21:09:32 +0400

    Prohibit all modifiers for enum entries
     #KT-2679 Fixed

--- compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
@@ -3907,6 +3907,12 @@ public class JetDiagnosticsTestGenerated extends AbstractJetDiagnosticsTest {
                 doTest(fileName);
             }
             
+            @TestMetadata("modifiersOnEnumEntry.kt")
+            public void testModifiersOnEnumEntry() throws Exception {
+                String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/enum/modifiersOnEnumEntry.kt");
+                doTest(fileName);
+            }
+            
             @TestMetadata("openMemberInEnum.kt")
             public void testOpenMemberInEnum() throws Exception {
                 String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/enum/openMemberInEnum.kt");
