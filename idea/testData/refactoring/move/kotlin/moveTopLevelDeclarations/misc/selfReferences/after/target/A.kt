27977cda2389476fe8d899ae46cec5ba089ff60d
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2014-10-21 19:15:40 +0400

    Move: Test data addendum

--- idea/testData/refactoring/move/kotlin/moveTopLevelDeclarations/misc/selfReferences/after/target/A.kt
+++ idea/testData/refactoring/move/kotlin/moveTopLevelDeclarations/misc/selfReferences/after/target/A.kt
@@ -2,4 +2,5 @@ package target
 
 class A(val a: A) {
     val klass = javaClass<A>()
+    val aa = A(a)
 }
--- idea/testData/refactoring/move/kotlin/moveTopLevelDeclarations/misc/selfReferences/before/source/A.kt
+++ idea/testData/refactoring/move/kotlin/moveTopLevelDeclarations/misc/selfReferences/before/source/A.kt
@@ -2,6 +2,7 @@ package source
 
 class <caret>A(val a: A) {
     val klass = javaClass<A>()
+    val aa = A(a)
 }
 
 class B {
