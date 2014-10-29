840e16baa0f75fbb3538b46da1667d74a9e841cb
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2014-10-29 21:53:33 +0300

    Test for IllegalArgumentException bug from JVM backend (KT-6149)
    JVM backend fails with IllegalArgumentException: Argument for @NotNull parameter 'descriptor' of org/jetbrains/jet/codegen/PropertyCodegen.gen must not be null
    
     #KT-6149 Fixed

--- compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
@@ -6258,6 +6258,12 @@ public class JetDiagnosticsTestGenerated extends AbstractJetDiagnosticsTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("InnerClassFromJava.kt")
+            public void testInnerClassFromJava() throws Exception {
+                String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/InnerClassFromJava.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("innerNestedClassFromJava.kt")
             public void testInnerNestedClassFromJava() throws Exception {
                 String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/innerNestedClassFromJava.kt");
@@ -6269,13 +6275,13 @@ public class JetDiagnosticsTestGenerated extends AbstractJetDiagnosticsTest {
                 String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/invisiblePackagePrivateInheritedMember.kt");
                 doTest(fileName);
             }
-            
+
             @TestMetadata("javaStaticImport.kt")
             public void testJavaStaticImport() throws Exception {
                 String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/javaStaticImport.kt");
                 doTest(fileName);
             }
-            
+
             @TestMetadata("KJKInheritance.kt")
             public void testKJKInheritance() throws Exception {
                 String fileName = JetTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/j+k/KJKInheritance.kt");
