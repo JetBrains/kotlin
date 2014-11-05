656bd028bcd820325e0210ae73fee6903863934c
Ilya Ryzhenkov <orangy@jetbrains.com>
2014-11-05 19:08:31 +0300

    Remove dead commented code.

--- libraries/stdlib/src/kotlin/collections/JUtil.kt
+++ libraries/stdlib/src/kotlin/collections/JUtil.kt
@@ -89,24 +89,6 @@ public fun <T> Collection<T>?.orEmpty(): Collection<T> = this ?: stdlib_emptyLis
 public fun <T> List<T>?.orEmpty(): List<T> = this ?: stdlib_emptyList()
 
 /**
-TODO figure out necessary variance/generics ninja stuff... :)
-public inline fun <in T> List<T>.sort(transform: fun(T) : java.lang.Comparable<*>) : List<T> {
-val comparator = java.util.Comparator<T>() {
-public fun compare(o1: T, o2: T): Int {
-val v1 = transform(o1)
-val v2 = transform(o2)
-if (v1 == v2) {
-return 0
-} else {
-return v1.compareTo(v2)
-}
-}
-}
-answer.sort(comparator)
-}
- */
-
-/**
  * Returns the first item in the list or null if the list is empty
  *
  * @includeFunctionBody ../../test/collections/ListSpecificTest.kt first
