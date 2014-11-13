a15edf716aa27edb63ff76970c311f4fc164e577
Valentin Kipyatkov <valentin@jetbrains.com>
2014-11-13 18:04:24 +0300

    Fixed KT-6235 Get rid of `` for identifier "type" in converter
     #KT-6235 Fixed

--- j2k/tests/testData/fileOrElement/importStatement/importWithKeywords.kt
+++ j2k/tests/testData/fileOrElement/importStatement/importWithKeywords.kt
@@ -1,6 +1,6 @@
 // ERROR: Unresolved reference: `as`
 package test
 
-import `as`.`type`.`val`.`var`.`fun`.`is`.`in`.`object`.`when`.`trait`.`This`
+import `as`.type.`val`.`var`.`fun`.`is`.`in`.`object`.`when`.`trait`.`This`
 
 class Test
