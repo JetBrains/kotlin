36a9a99b4fcd1bf8d788492eb940171bf1f740da
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-11-14 20:36:32 +0300

    Minor, fix formatting in Arrays.kt in stdlib

--- libraries/stdlib/src/kotlin/collections/Arrays.kt
+++ libraries/stdlib/src/kotlin/collections/Arrays.kt
@@ -1,28 +1,28 @@
 package kotlin
 
-public val          BooleanArray.lastIndex : Int
+public val BooleanArray.lastIndex: Int
     get() = this.size - 1
 
-public val             ByteArray.lastIndex : Int
+public val ByteArray.lastIndex: Int
     get() = this.size - 1
 
-public val            ShortArray.lastIndex : Int
+public val ShortArray.lastIndex: Int
     get() = this.size - 1
 
-public val              IntArray.lastIndex : Int
+public val IntArray.lastIndex: Int
     get() = this.size - 1
 
-public val             LongArray.lastIndex : Int
+public val LongArray.lastIndex: Int
     get() = this.size - 1
 
-public val            FloatArray.lastIndex : Int
+public val FloatArray.lastIndex: Int
     get() = this.size - 1
 
-public val           DoubleArray.lastIndex : Int
+public val DoubleArray.lastIndex: Int
     get() = this.size - 1
 
-public val             CharArray.lastIndex : Int
+public val CharArray.lastIndex: Int
     get() = this.size - 1
 
-public val <T: Any?> Array<out T>.lastIndex : Int
+public val Array<*>.lastIndex: Int
     get() = this.size - 1
