fdd6cdcdf185b0f42f8fc080dda9eb76849da17d
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2014-11-20 15:14:45 +0300

    fix for KT-5420 store plugin's core class in class object, do not load new class on each invocation.

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPluginWrapper.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPluginWrapper.kt
@@ -15,61 +15,72 @@ import java.lang.reflect.Method
 
 abstract class KotlinBasePluginWrapper: Plugin<Project> {
 
-    val log = Logging.getLogger(this.javaClass)
+    class object {
+        val pluginVersionsMap: MutableMap<String, Class<Plugin<Project>>> = hashMapOf()
+    }
 
-    var pluginInstance : Plugin<Project>? = null
-    var applyMethod: Method? = null
+    val log = Logging.getLogger(this.javaClass)
 
     public override fun apply(project: Project) {
 
-        if (pluginInstance == null || applyMethod == null) {
-            val sourceBuildScript = findSourceBuildScript(project)
-            if (sourceBuildScript == null) {
-                log.error("Failed to determine source cofiguration of kotlin plugin. Can not download core. Please verify that this or any parent project " +
-                        "contains 'kotlin-gradle-plugin' in buildscript's classpath configuration.")
-                return
-            }
-            val dependencyHandler: DependencyHandler = sourceBuildScript.getDependencies()
-            val configurationsContainer: ConfigurationContainer = sourceBuildScript.getConfigurations()
-
-            log.debug("Loading version information")
-            val props = Properties()
-            val propFileName = "project.properties"
-            val inputStream = this.javaClass.getClassLoader()!!.getResourceAsStream(propFileName)
-
-            if (inputStream == null) {
-                throw FileNotFoundException("property file '" + propFileName + "' not found in the classpath")
-            }
-
-            props.load(inputStream)
-
-            val projectVersion = props["project.version"] as String
-            log.debug("Found project version [$projectVersion]")
-            project.getExtensions().getExtraProperties()?.set("kotlin.gradle.plugin.version", projectVersion)
-
-            log.debug("Creating configuration and dependency")
-            val kotlinPluginCoreCoordinates = "org.jetbrains.kotlin:kotlin-gradle-plugin-core:" + projectVersion
-            val dependency = dependencyHandler.create(kotlinPluginCoreCoordinates)
-            val configuration = configurationsContainer.detachedConfiguration(dependency)
-
-            log.debug("Resolving [" + kotlinPluginCoreCoordinates + "]")
-            val kotlinPluginDependencies: List<URL> = configuration.getResolvedConfiguration().getFiles({ true })!!.map { it.toURI().toURL() }
-            log.debug("Resolved files: [" + kotlinPluginDependencies.toString() + "]")
-            log.debug("Load plugin in parent-last URL classloader")
-            val kotlinPluginClassloader = ParentLastURLClassLoader(kotlinPluginDependencies, this.javaClass.getClassLoader())
-            log.debug("Class loader created")
-            val cls = Class.forName(getPluginClassName(), true, kotlinPluginClassloader)
-            log.debug("Plugin class loaded")
-
-            val constructor = cls.getConstructor(javaClass<ScriptHandler>())
-            pluginInstance = constructor.newInstance(sourceBuildScript) as Plugin<Project>?
-            log.debug("Plugin class instantiated")
-            applyMethod = cls.getMethod("apply", javaClass<Project>())
-            log.debug("'apply' method found, invoking...")
+        val sourceBuildScript = findSourceBuildScript(project)
+        if (sourceBuildScript == null) {
+            log.error("Failed to determine source cofiguration of kotlin plugin. Can not download core. Please verify that this or any parent project " +
+                    "contains 'kotlin-gradle-plugin' in buildscript's classpath configuration.")
+            return
         }
 
-        applyMethod?.invoke(pluginInstance, project)
+        val kotlinPluginVersion = loadKotlinVersionFromResource()
+        project.getExtensions().getExtraProperties()?.set("kotlin.gradle.plugin.version", kotlinPluginVersion)
+
+        val cls = pluginVersionsMap.getOrElse("$kotlinPluginVersion:${getPluginClassName()}", { loadPluginInIsolatedClassloader(kotlinPluginVersion, sourceBuildScript) })
+
+        val constructor = cls.getConstructor(javaClass<ScriptHandler>())
+        val method = cls.getMethod("apply", javaClass<Project>())
+        log.debug("'apply' method found, invoking...")
+
+        val plugin = constructor.newInstance(sourceBuildScript)
+        log.debug("Plugin class instantiated")
+
+        method?.invoke(plugin, project)
         log.debug("'apply' method invoked successfully")
+    }
+
+    private fun loadPluginInIsolatedClassloader(projectVersion: String, sourceBuildScript: ScriptHandler): Class<Plugin<Project>> {
+        val dependencyHandler: DependencyHandler = sourceBuildScript.getDependencies()
+        val configurationsContainer: ConfigurationContainer = sourceBuildScript.getConfigurations()
+
+        log.debug("Creating configuration and dependency")
+        val kotlinPluginCoreCoordinates = "org.jetbrains.kotlin:kotlin-gradle-plugin-core:" + projectVersion
+        val dependency = dependencyHandler.create(kotlinPluginCoreCoordinates)
+        val configuration = configurationsContainer.detachedConfiguration(dependency)
+
+        log.debug("Resolving [" + kotlinPluginCoreCoordinates + "]")
+        val kotlinPluginDependencies: List<URL> = configuration.getResolvedConfiguration().getFiles({ true })!!.map { it.toURI().toURL() }
+        log.debug("Resolved files: [" + kotlinPluginDependencies.toString() + "]")
+        log.debug("Load plugin in parent-last URL classloader")
+        val kotlinPluginClassloader = ParentLastURLClassLoader(kotlinPluginDependencies, this.javaClass.getClassLoader())
+        log.debug("Class loader created")
+        val cls = Class.forName(getPluginClassName(), true, kotlinPluginClassloader) as Class<Plugin<Project>>
+        log.debug("Plugin class loaded")
+        return cls
+    }
+
+    private fun loadKotlinVersionFromResource(): String {
+        log.debug("Loading version information")
+        val props = Properties()
+        val propFileName = "project.properties"
+        val inputStream = this.javaClass.getClassLoader()!!.getResourceAsStream(propFileName)
+
+        if (inputStream == null) {
+            throw FileNotFoundException("property file '" + propFileName + "' not found in the classpath")
+        }
+
+        props.load(inputStream)
+
+        val projectVersion = props["project.version"] as String
+        log.debug("Found project version [$projectVersion]")
+        return projectVersion
     }
 
     public abstract fun getPluginClassName():String
