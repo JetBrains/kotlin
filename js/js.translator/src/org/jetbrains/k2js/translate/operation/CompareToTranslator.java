eadb2d8db548cdeb48fc2b8959012a0b9135ee0a
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2014-12-09 20:55:36 +0300

    Minor in JS backend: save operation token as member of BinaryOperationTranslator.

--- js/js.translator/src/org/jetbrains/k2js/translate/operation/CompareToTranslator.java
+++ js/js.translator/src/org/jetbrains/k2js/translate/operation/CompareToTranslator.java
@@ -20,9 +20,11 @@ import com.google.dart.compiler.backend.js.ast.JsBinaryOperation;
 import com.google.dart.compiler.backend.js.ast.JsBinaryOperator;
 import com.google.dart.compiler.backend.js.ast.JsExpression;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jet.lang.descriptors.CallableDescriptor;
 import org.jetbrains.jet.lang.psi.JetBinaryExpression;
 import org.jetbrains.jet.lang.types.expressions.OperatorConventions;
+import org.jetbrains.jet.lexer.JetToken;
 import org.jetbrains.k2js.translate.context.TranslationContext;
 import org.jetbrains.k2js.translate.general.AbstractTranslator;
 
@@ -34,15 +36,10 @@ import static org.jetbrains.k2js.translate.utils.PsiUtils.getOperationToken;
 public final class CompareToTranslator extends AbstractTranslator {
 
     public static boolean isCompareToCall(
-            @NotNull JetBinaryExpression expression,
-            @NotNull TranslationContext context
+            @NotNull JetToken operationToken,
+            @Nullable CallableDescriptor operationDescriptor
     ) {
-        if (!OperatorConventions.COMPARISON_OPERATIONS.contains(getOperationToken(expression))) return false;
-
-        CallableDescriptor operationDescriptor =
-                getCallableDescriptorForOperationExpression(context.bindingContext(), expression);
-
-        if (operationDescriptor == null) return false;
+        if (!OperatorConventions.COMPARISON_OPERATIONS.contains(operationToken) || operationDescriptor == null) return false;
 
         return isCompareTo(operationDescriptor);
     }
