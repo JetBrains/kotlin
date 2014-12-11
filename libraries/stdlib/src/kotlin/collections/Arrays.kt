36287e9ef893e500445cf61f2ea1695753fca09c
Denis Zharkov <denis.zharkov@jetbrains.com>
2014-12-11 10:22:00 +0300

    Added inline function "Array" into stdlib
    It has the same signature and semantics as Array constructor had
    Also made Array constructor private and accepting no arguments

--- libraries/stdlib/src/kotlin/collections/Arrays.kt
+++ libraries/stdlib/src/kotlin/collections/Arrays.kt
@@ -1,5 +1,15 @@
 package kotlin
 
+public inline fun <reified T> Array(size: Int, init: (Int) -> T): Array<T> {
+    val result = arrayOfNulls<T>(size)
+
+    for (i in 0..size - 1) {
+        result[i] = init(i)
+    }
+
+    return result as Array<T>
+}
+
 public val BooleanArray.lastIndex: Int
     get() = this.size - 1
 
