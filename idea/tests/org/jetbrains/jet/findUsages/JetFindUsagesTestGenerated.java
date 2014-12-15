6abe5a425c0db88144583bfb7b4630f504435a94
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2014-12-15 19:20:42 +0300

    Finding usages (which have references) in text when plaint text occurrences are not searched.

--- idea/tests/org/jetbrains/jet/findUsages/JetFindUsagesTestGenerated.java
+++ idea/tests/org/jetbrains/jet/findUsages/JetFindUsagesTestGenerated.java
@@ -141,6 +141,12 @@ public class JetFindUsagesTestGenerated extends AbstractJetFindUsagesTest {
                 JetTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("idea/testData/findUsages/kotlin/findClassUsages"), Pattern.compile("^(.+)\\.0\\.kt$"), true);
             }
 
+            @TestMetadata("classUsedInTextAsRef.0.kt")
+            public void testClassUsedInTextAsRef() throws Exception {
+                String fileName = JetTestUtils.navigationMetadata("idea/testData/findUsages/kotlin/findClassUsages/classUsedInTextAsRef.0.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("javaClassAllUsages.0.kt")
             public void testJavaClassAllUsages() throws Exception {
                 String fileName = JetTestUtils.navigationMetadata("idea/testData/findUsages/kotlin/findClassUsages/javaClassAllUsages.0.kt");
