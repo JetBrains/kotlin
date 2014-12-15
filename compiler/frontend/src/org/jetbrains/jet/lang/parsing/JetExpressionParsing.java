da6e1890bdd0fab3f0461461bd6f2f099003e459
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2014-12-15 15:39:40 +0300

    Don't lookahead too far when looking for '.' between type and parameters and for comma in function literal parameters

--- compiler/frontend/src/org/jetbrains/jet/lang/parsing/JetExpressionParsing.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/parsing/JetExpressionParsing.java
@@ -1134,7 +1134,21 @@ public class JetExpressionParsing extends AbstractJetParsing {
         // it to function params if possible.
         boolean preferParamsToExpressions = false;
 
-        int lastDot = matchTokenStreamPredicate(new LastBefore(new At(DOT), new AtSet(ARROW, RPAR)));
+        // Last dot before ARROW and RPAR, but also stop at top-level keywords and '{', '}'
+        int lastDot = matchTokenStreamPredicate(new LastBefore(
+                new At(DOT),
+                new AtSet(TokenSet.orSet(
+                        TokenSet.create(ARROW, RPAR),
+                        TokenSet.orSet(
+                                TokenSet.create(LBRACE, RBRACE),
+                                TokenSet.andNot(
+                                        KEYWORDS,
+                                        TokenSet.create(CAPITALIZED_THIS_KEYWORD)
+                                )
+                        )
+                ))
+        ));
+
         if (lastDot >= 0) {
             createTruncatedBuilder(lastDot).parseTypeRef();
             if (at(DOT)) {
@@ -1187,7 +1201,7 @@ public class JetExpressionParsing extends AbstractJetParsing {
                 if (at(COMMA)) errorAndAdvance("Expecting a parameter declaration");
 
                 PsiBuilder.Marker parameter = mark();
-                int parameterNamePos = matchTokenStreamPredicate(new LastBefore(new At(IDENTIFIER), new AtSet(COMMA, RPAR, COLON, ARROW)));
+                int parameterNamePos = matchTokenStreamPredicate(new LastBefore(new At(IDENTIFIER), new AtSet(COMMA, RPAR, COLON, ARROW, RBRACE, LBRACE)));
                 createTruncatedBuilder(parameterNamePos).parseModifierList(MODIFIER_LIST, REGULAR_ANNOTATIONS_ONLY_WITH_BRACKETS);
 
                 expect(IDENTIFIER, "Expecting parameter declaration");
