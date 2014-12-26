05be100f53a18e67fe548aeadc6090983ee118e6
Valentin Kipyatkov <valentin@jetbrains.com>
2014-12-26 21:55:10 +0300

    Corrected test data which was incorrect before

--- idea/testData/completion/smart/This.kt
+++ idea/testData/completion/smart/This.kt
@@ -16,12 +16,13 @@ class Outer {
 
 fun takeHandler1(handler: Int.() -> Unit){}
 fun takeHandler2(handler: Char.() -> Unit){}
-fun takeHandler3(handler: Any?.() -> Unit){}
+fun takeHandler3(handler: (Any?).() -> Unit){}
 fun takeHandler4(handler: Any.() -> Unit){}
 
 // EXIST: { lookupString: "this", itemText: "this", tailText: null, typeText: "Any", attributes: "bold" }
 // ABSENT: "this@takeHandler4"
-// ABSENT: "this@takeHandler3"
+// EXIST: { lookupString: "this@takeHandler3", itemText: "!! this", tailText: "@takeHandler3", typeText: "Any?", attributes: "bold" }
+// EXIST: { lookupString: "this@takeHandler3", itemText: "?: this", tailText: "@takeHandler3", typeText: "Any?", attributes: "bold" }
 // EXIST: { lookupString: "this@takeHandler2", itemText: "this", tailText: "@takeHandler2", typeText: "Char", attributes: "bold" }
 // EXIST: { lookupString: "this@takeHandler1", itemText: "this", tailText: "@takeHandler1", typeText: "Int", attributes: "bold" }
 // EXIST: { lookupString: "this@foo", itemText: "this", tailText: "@foo", typeText: "String", attributes: "bold" }
