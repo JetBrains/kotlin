fad40ac11e17c90a5acbb4dc7b71250ac9a7ce68
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-01-03 13:33:56 +0300

    Rename package jet -> kotlin in ant
    org.jetbrains.jet.buildtools.ant -> org.jetbrains.kotlin.ant
    
    The old antlib.xml is kept for migration purposes

--- ant/src/org/jetbrains/kotlin/ant/Kotlin2JsTask.kt
+++ ant/src/org/jetbrains/kotlin/ant/Kotlin2JsTask.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2014 JetBrains s.r.o.
+ * Copyright 2010-2015 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.jetbrains.jet.buildtools.ant
+package org.jetbrains.kotlin.ant
 
 import org.apache.tools.ant.types.Path
 import java.io.File
