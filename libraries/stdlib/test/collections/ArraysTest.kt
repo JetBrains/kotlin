9f6e2ac0366487349da168ca6db26118c778c3f4
Valery Kharitonov <kharvd@gmail.com>
2015-01-12 21:07:31 +0300

    stdlib: additional tests for arrays asIterable()

--- libraries/stdlib/test/collections/ArraysTest.kt
+++ libraries/stdlib/test/collections/ArraysTest.kt
@@ -245,11 +245,25 @@ class ArraysTest {
     }
 
     test fun asIterable() {
-        val a = intArray(1, 2, 3, 4, 5)
-        val iter = a.asIterable()
-        assertEquals(a.toList(), iter.toList())
-        a[0] = 0
-        assertEquals(a.toList(), iter.toList())
+        val arr1 = intArray(1, 2, 3, 4, 5)
+        val iter1 = arr1.asIterable()
+        assertEquals(arr1.toList(), iter1.toList())
+        arr1[0] = 0
+        assertEquals(arr1.toList(), iter1.toList())
+
+        val arr2 = array("one", "two", "three")
+        val iter2 = arr2.asIterable()
+        assertEquals(arr2.toList(), iter2.toList())
+        arr2[0] = ""
+        assertEquals(arr2.toList(), iter2.toList())
+
+        val arr3 = IntArray(0)
+        val iter3 = arr3.asIterable()
+        assertEquals(iter3.toList(), emptyList<Int>())
+
+        val arr4 = Array(0, {"$it"})
+        val iter4 = arr4.asIterable()
+        assertEquals(iter4.toList(), emptyList<String>())
     }
 
     /*
