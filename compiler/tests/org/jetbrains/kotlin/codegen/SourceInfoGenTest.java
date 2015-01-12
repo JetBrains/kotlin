587a92717c801151dca21ac0d3dee60c20da92e6
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-01-12 12:16:01 +0300

    Update copyright in frontend (cfg), tests

--- compiler/tests/org/jetbrains/kotlin/codegen/SourceInfoGenTest.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/SourceInfoGenTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jetbrains.kotlin.codegen;
 
 import org.jetbrains.kotlin.backend.common.output.OutputFile;
