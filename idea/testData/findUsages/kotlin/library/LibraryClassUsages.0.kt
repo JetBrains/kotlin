f7d06e98803f160b2786876ae721d66d31e3bce7
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-01-15 15:00:07 +0300

    Drop library usages tests

--- idea/testData/findUsages/kotlin/library/LibraryClassUsages.0.kt
+++
@@ -1,7 +0,0 @@
-// PSI_ELEMENT: com.intellij.psi.PsiClass
-// OPTIONS: usages
-// FIND_BY_REF
-// FIND_BY_MIRROR_ELEMENT
-fun test() {
-    <caret>System.exit(0)
-}
--- idea/testData/findUsages/kotlin/library/LibraryFieldUsages.0.kt
+++
@@ -1,9 +0,0 @@
-// PSI_ELEMENT: com.intellij.psi.PsiField
-// OPTIONS: usages
-// FIND_BY_REF
-// FIND_BY_MIRROR_ELEMENT
-import java.awt.Dimension
-
-fun test() {
-    Dimension().<caret>width = 1
-}
