7323ef70028ce7ac65fd87945ef78a1c4b824463
Andrey Breslav <andrey.breslav@jetbrains.com>
2015-01-23 17:13:45 +0300

    Minor. Noisy warnings removed from test data

--- compiler/testData/diagnostics/tests/inner/referenceToSelfInLocal.kt
+++ compiler/testData/diagnostics/tests/inner/referenceToSelfInLocal.kt
@@ -1,29 +1,30 @@
+// !DIAGNOSTICS: -UNUSED_VARIABLE
 // KT-4351 Cannot resolve reference to self in init of class local to function
 
 fun f() {
     class MyClass() {
         {
-            val <!UNUSED_VARIABLE!>x<!>: MyClass = MyClass()
+            val x: MyClass = MyClass()
         }
 
         fun member() {
-            val <!UNUSED_VARIABLE!>x<!>: MyClass = MyClass()
+            val x: MyClass = MyClass()
         }
     }
 
     <!LOCAL_OBJECT_NOT_ALLOWED!>object MyObject<!> {
         {
-            val <!UNUSED_VARIABLE!>obj<!>: MyObject = MyObject
+            val obj: MyObject = MyObject
         }
     }
 
-    val <!UNUSED_VARIABLE!>x<!>: MyClass = MyClass()
+    val x: MyClass = MyClass()
 }
 
 val closure = {
     class MyClass {
         {
-            val <!UNUSED_VARIABLE!>x<!>: MyClass = MyClass()
+            val x: MyClass = MyClass()
         }
     }
 }
