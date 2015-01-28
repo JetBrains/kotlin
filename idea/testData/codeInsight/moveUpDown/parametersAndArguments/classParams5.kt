2dbfd1fc4c297fcf8eaf09697be12f2cba9a6b88
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2015-01-28 12:42:07 +0300

    Move Statement: Fix comma placement for class parameter  #KT-6672 Fixed

---
+++ idea/testData/codeInsight/moveUpDown/parametersAndArguments/classParams5.kt
@@ -0,0 +1,10 @@
+// MOVE: down
+// MOVER_CLASS: org.jetbrains.kotlin.idea.codeInsight.upDownMover.JetExpressionMover
+// IS_APPLICABLE: false
+class A(
+        b: Int,
+        c: Int,
+        <caret>a: Int
+) {
+
+}
