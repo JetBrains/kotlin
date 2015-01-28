d689c11848bf3f54a1aafcbd78deac20326e9978
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-01-28 18:26:54 +0300

    Update test for class object completion

--- idea/testData/completion/injava/ObjectInClassObjects.java
+++ idea/testData/completion/injava/ObjectInClassObjects.java
@@ -1,6 +1,5 @@
 public class Testing {
-    public static void test() {
-        mockLib.foo.F.Default.F.Default.<caret>
+    public static void test(mockLib.foo.F.Default.F.Default.<caret>) {
     }
 }
 
