aa2c123e452a2a4d9517a51720895c88b71a2f67
Dmitry Jemerov <yole@jetbrains.com>
2015-02-05 17:20:11 +0100

    inherit documentation for properties as well as functions

---
+++ idea/testData/editor/quickDoc/OnInheritedPropertyUsage.kt
@@ -0,0 +1,17 @@
+open class C() {
+    /**
+     * This property returns zero.
+     */
+    open val foo: Int get() = 0
+}
+
+class D(): C() {
+    override val foo: Int get() = 1
+}
+
+
+fun test() {
+    D().f<caret>oo
+}
+
+// INFO: <b>internal</b> <b>open</b> <b>val</b> foo: Int<br/><p>This property returns zero.</p>
