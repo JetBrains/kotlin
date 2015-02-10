32c371dd4118f3a94f9e2cd20c3747d8267cfca2
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-02-10 20:14:44 +0300

    Minor, uncomment previously failing checks
     #KT-5092 Fixed

--- compiler/testData/codegen/boxWithStdlib/reflection/enclosing/insideLambda/classInLambda.kt
+++ compiler/testData/codegen/boxWithStdlib/reflection/enclosing/insideLambda/classInLambda.kt
@@ -11,9 +11,8 @@ fun box(): String {
     val enclosingClass = classInLambda.javaClass.getEnclosingClass()!!.getName()
     if (!enclosingClass.startsWith("_DefaultPackage\$") || !enclosingClass.endsWith("\$box\$classInLambda\$1")) return "enclosing class: $enclosingClass"
 
-    //KT-5092
-    //val declaringClass = classInLambda.javaClass.getDeclaringClass()
-    //if (declaringClass == null) return "class hasn't a declaring class"
+    val declaringClass = classInLambda.javaClass.getDeclaringClass()
+    if (declaringClass != null) return "class has a declaring class"
 
     return "OK"
 }
--- compiler/testData/codegen/boxWithStdlib/reflection/enclosing/insideLambda/objectInLambda.kt
+++ compiler/testData/codegen/boxWithStdlib/reflection/enclosing/insideLambda/objectInLambda.kt
@@ -10,9 +10,8 @@ fun box(): String {
     val enclosingClass = objectInLambda.javaClass.getEnclosingClass()!!.getName()
     if (!enclosingClass.startsWith("_DefaultPackage\$") || !enclosingClass.endsWith("\$box\$objectInLambda\$1")) return "enclosing class: $enclosingClass"
 
-    //KT-5092
-    //val declaringClass = objectInLambda.javaClass.getDeclaringClass()
-    //if (declaringClass == null) return "anonymous object hasn't a declaring class"
+    val declaringClass = objectInLambda.javaClass.getDeclaringClass()
+    if (declaringClass != null) return "anonymous object has a declaring class"
 
     return "OK"
 }
