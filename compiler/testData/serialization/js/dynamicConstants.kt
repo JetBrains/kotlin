b0b90ff6a9ec760f0b1a563d5d1d06087493ffa8
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-02-17 03:06:26 +0300

    add KotlinJavascriptSerializerTest

---
+++ compiler/testData/serialization/js/dynamicConstants.kt
@@ -0,0 +1,36 @@
+package test
+
+
+enum class Weapon {
+    ROCK
+    PAPER
+    SCISSORS
+}
+
+val byteConst: dynamic = 10.toByte()
+val shortConst: dynamic = 20.toShort()
+val intConst: dynamic = 30
+val longConst: dynamic = 40.toLong()
+val charConst: dynamic = 'A'
+val stringConst: dynamic = "abcd"
+val booleanConst: dynamic = true
+val floatConst: dynamic = 2.0f
+val doubleConst: dynamic = 3.0
+val enumConst: dynamic = Weapon.ROCK
+val arrayConst: dynamic = byteArray(1,2)
+val dynamicConst: dynamic = 100
+
+class Class {
+    val byteConst: dynamic = 10.toByte()
+    val shortConst: dynamic = 20.toShort()
+    val intConst: dynamic = 30
+    val longConst: dynamic = 40.toLong()
+    val charConst: dynamic = 'A'
+    val stringConst: dynamic = "abcd"
+    val booleanConst: dynamic = true
+    val floatConst: dynamic = 2.0f
+    val doubleConst: dynamic = 3.0
+    val enumConst: dynamic = Weapon.ROCK
+    val arrayConst: dynamic = byteArray(1,2)
+    val dynamicConst: dynamic = 100
+}
