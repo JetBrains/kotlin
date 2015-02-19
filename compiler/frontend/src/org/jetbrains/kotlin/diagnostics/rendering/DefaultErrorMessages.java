27978a7e25acf9c07189e15d98cd6d453dd64a14
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-02-19 15:45:34 +0300

    Clarify inapplicable platformName diagnostic message

--- compiler/frontend/src/org/jetbrains/kotlin/diagnostics/rendering/DefaultErrorMessages.java
+++ compiler/frontend/src/org/jetbrains/kotlin/diagnostics/rendering/DefaultErrorMessages.java
@@ -147,7 +147,7 @@ public class DefaultErrorMessages {
         });
         MAP.put(ILLEGAL_MODIFIER, "Illegal modifier ''{0}''", TO_STRING);
         MAP.put(REPEATED_MODIFIER, "Repeated ''{0}''", TO_STRING);
-        MAP.put(INAPPLICABLE_ANNOTATION, "This annotation is not applicable to class members");
+        MAP.put(INAPPLICABLE_ANNOTATION, "This annotation is only applicable to top level functions");
 
         MAP.put(REDUNDANT_MODIFIER, "Modifier ''{0}'' is redundant because ''{1}'' is present", TO_STRING, TO_STRING);
         MAP.put(ABSTRACT_MODIFIER_IN_TRAIT, "Modifier ''abstract'' is redundant in trait");
