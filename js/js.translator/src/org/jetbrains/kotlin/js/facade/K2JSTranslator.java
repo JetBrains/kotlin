54bbe0711dae7c93061dbd04573fec56b012d52a
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-02-20 21:27:35 +0300

    JS backend: reuse analysisResult from K2JSCompiler in translator

--- js/js.translator/src/org/jetbrains/kotlin/js/facade/K2JSTranslator.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/facade/K2JSTranslator.java
@@ -16,46 +16,24 @@
 
 package org.jetbrains.kotlin.js.facade;
 
-import com.google.dart.compiler.backend.js.ast.JsNode;
 import com.google.dart.compiler.backend.js.ast.JsProgram;
-import com.google.dart.compiler.util.TextOutputImpl;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VfsUtilCore;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Consumer;
-import com.intellij.util.Function;
-import com.intellij.util.SmartList;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.backend.common.output.OutputFileCollection;
-import org.jetbrains.kotlin.backend.common.output.SimpleOutputFile;
-import org.jetbrains.kotlin.backend.common.output.SimpleOutputFileCollection;
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor;
 import org.jetbrains.kotlin.js.analyze.TopDownAnalyzerFacadeForJS;
 import org.jetbrains.kotlin.js.analyzer.JsAnalysisResult;
 import org.jetbrains.kotlin.js.config.Config;
 import org.jetbrains.kotlin.js.facade.exceptions.TranslationException;
 import org.jetbrains.kotlin.js.inline.JsInliner;
-import org.jetbrains.kotlin.js.sourceMap.JsSourceGenerationVisitor;
-import org.jetbrains.kotlin.js.sourceMap.SourceMap3Builder;
-import org.jetbrains.kotlin.js.sourceMap.SourceMapBuilder;
 import org.jetbrains.kotlin.js.translate.context.TranslationContext;
 import org.jetbrains.kotlin.js.translate.general.Translation;
 import org.jetbrains.kotlin.psi.JetFile;
 import org.jetbrains.kotlin.resolve.BindingTrace;
 import org.jetbrains.kotlin.resolve.diagnostics.Diagnostics;
-import org.jetbrains.kotlin.utils.fileUtils.FileUtilsPackage;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 import static org.jetbrains.kotlin.diagnostics.DiagnosticUtils.hasError;
-import static org.jetbrains.kotlin.js.facade.FacadeUtils.parseString;
-import static org.jetbrains.kotlin.psi.PsiPackage.JetPsiFactory;
 
 /**
  * An entry point of translator.
@@ -77,7 +55,19 @@ public final class K2JSTranslator {
             @NotNull List<JetFile> files,
             @NotNull MainCallParameters mainCallParameters
     ) throws TranslationException {
-        JsAnalysisResult analysisResult = TopDownAnalyzerFacadeForJS.analyzeFiles(files, config);
+        return translate(files, mainCallParameters, null);
+    }
+
+    @NotNull
+    public TranslationResult translate(
+            @NotNull List<JetFile> files,
+            @NotNull MainCallParameters mainCallParameters,
+            @Nullable JsAnalysisResult analysisResult
+    ) throws TranslationException {
+        if (analysisResult == null) {
+            analysisResult = TopDownAnalyzerFacadeForJS.analyzeFiles(files, config);
+        }
+
         BindingTrace bindingTrace = analysisResult.getBindingTrace();
         TopDownAnalyzerFacadeForJS.checkForErrors(Config.withJsLibAdded(files, config), bindingTrace.getBindingContext());
         ModuleDescriptor moduleDescriptor = analysisResult.getModuleDescriptor();
