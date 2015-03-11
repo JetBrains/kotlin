2987e72d29603c7fbf6511f283569bf3d49a3e27
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-03-11 14:05:35 +0300

    Do not report redeclaration when outer class and default object's members clash
    Previously we prohibited classes and properties with the same name (now it's unnecessary)
    Add test for backends that they support qualified (by default object name) member references inside class body

--- compiler/testData/diagnostics/tests/duplicateJvmSignature/specialNames/classObjectCopiedFieldObject.kt
+++ compiler/testData/diagnostics/tests/duplicateJvmSignature/specialNames/classObjectCopiedFieldObject.kt
@@ -1,9 +1,15 @@
-class C {
-    default <!REDECLARATION!>object<!> {
-        val <!REDECLARATION!>Default<!> = this
+class B {
+    default object <!REDECLARATION!>A<!> {
     }
 
-    val <!REDECLARATION!>Default<!> = C
+    val <!REDECLARATION!>A<!> = this
+}
+
+class C {
+    default <!CONFLICTING_JVM_DECLARATIONS!>object A<!> {
+        <!CONFLICTING_JVM_DECLARATIONS!>val A<!> = this
+    }
+
 }
 
 class D {
