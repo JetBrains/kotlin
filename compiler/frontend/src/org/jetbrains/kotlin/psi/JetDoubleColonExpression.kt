5bac0fe85622a739b5516b2d5c715480a067d543
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-03-12 14:08:11 +0300

    Revert to old findChildByType() signature

--- compiler/frontend/src/org/jetbrains/kotlin/psi/JetDoubleColonExpression.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/psi/JetDoubleColonExpression.kt
@@ -22,9 +22,9 @@ import org.jetbrains.kotlin.JetNodeTypes
 import org.jetbrains.kotlin.lexer.JetTokens
 
 public abstract class JetDoubleColonExpression(node: ASTNode) : JetExpressionImpl(node) {
-    public fun getTypeReference(): JetTypeReference? = findChildByType(JetNodeTypes.TYPE_REFERENCE)
+    public fun getTypeReference(): JetTypeReference? = findChildByType(JetNodeTypes.TYPE_REFERENCE) as JetTypeReference?
 
-    public fun getDoubleColonTokenReference(): PsiElement = findChildByType(JetTokens.COLONCOLON)
+    public fun getDoubleColonTokenReference(): PsiElement = findChildByType(JetTokens.COLONCOLON)!!
 
     override fun <R, D> accept(visitor: JetVisitor<R, D>, data: D): R {
         return visitor.visitDoubleColonExpression(this, data)
