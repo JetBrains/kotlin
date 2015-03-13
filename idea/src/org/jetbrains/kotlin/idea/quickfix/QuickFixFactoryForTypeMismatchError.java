b7a7ae8cb6b3e825cad92222b0531f966ab363a4
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2015-03-13 19:46:35 +0300

    Quick-Fixes: Support "Change function return type" quick-fix for non-local returns   #KT-6910 Fixed (cherry picked from commit f5107be)

--- idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixFactoryForTypeMismatchError.java
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixFactoryForTypeMismatchError.java
@@ -32,6 +32,7 @@ import org.jetbrains.kotlin.idea.caches.resolve.ResolvePackage;
 import org.jetbrains.kotlin.idea.util.UtilPackage;
 import org.jetbrains.kotlin.psi.*;
 import org.jetbrains.kotlin.resolve.BindingContext;
+import org.jetbrains.kotlin.resolve.bindingContextUtil.BindingContextUtilPackage;
 import org.jetbrains.kotlin.resolve.calls.callUtil.CallUtilPackage;
 import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall;
 import org.jetbrains.kotlin.types.JetType;
@@ -102,10 +103,15 @@ public class QuickFixFactoryForTypeMismatchError extends JetIntentionActionsFact
             }
         }
 
+        PsiElement expressionParent = expression.getParent();
+
         // Mismatch in returned expression:
-        JetFunction function = PsiTreeUtil.getParentOfType(expression, JetFunction.class, true);
-        if (function != null && QuickFixUtil.canFunctionOrGetterReturnExpression(function, expression)) {
-            actions.add(new ChangeFunctionReturnTypeFix(function, expressionType));
+
+        JetCallableDeclaration function = expressionParent instanceof JetReturnExpression
+                               ? BindingContextUtilPackage.getTargetFunction((JetReturnExpression) expressionParent, context)
+                               : PsiTreeUtil.getParentOfType(expression, JetFunction.class, true);
+        if (function instanceof JetFunction && QuickFixUtil.canFunctionOrGetterReturnExpression(function, expression)) {
+            actions.add(new ChangeFunctionReturnTypeFix((JetFunction) function, expressionType));
         }
 
         // Fixing overloaded operators:
--- idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -4932,6 +4932,18 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("beforeNonLocalReturn.kt")
+            public void testNonLocalReturn() throws Exception {
+                String fileName = JetTestUtils.navigationMetadata("idea/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/beforeNonLocalReturn.kt");
+                doTest(fileName);
+            }
+
+            @TestMetadata("beforeNonLocalReturnWithLabel.kt")
+            public void testNonLocalReturnWithLabel() throws Exception {
+                String fileName = JetTestUtils.navigationMetadata("idea/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/beforeNonLocalReturnWithLabel.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("beforePropertyGetterInitializerTypeMismatch.kt")
             public void testPropertyGetterInitializerTypeMismatch() throws Exception {
                 String fileName = JetTestUtils.navigationMetadata("idea/testData/quickfix/typeMismatch/typeMismatchOnReturnedExpression/beforePropertyGetterInitializerTypeMismatch.kt");
