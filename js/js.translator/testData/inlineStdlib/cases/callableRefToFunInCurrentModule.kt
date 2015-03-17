ea1121be85267869694e82e0ed1b29927315e62e
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-03-17 14:32:02 +0300

    JS: added test for callable reference as stdlib inline function argument (cherry picked from commit 03f0c6e)

---
+++ js/js.translator/testData/inlineStdlib/cases/callableRefToFunInCurrentModule.kt
@@ -0,0 +1,14 @@
+package foo
+
+// CHECK_NOT_CALLED_IN_SCOPE: scope=test function=even
+// CHECK_NOT_CALLED_IN_SCOPE: scope=test function=filter_azvtw4$
+
+fun even(x: Int) = x % 2 == 0
+
+fun test(a: List<Int>) = a.filter(::even)
+
+fun box(): String {
+    assertEquals(listOf(2, 4), test(listOf(1, 2, 3, 4)))
+
+    return "OK"
+}
