637603b584d70447566b2809497231adc0499ba0
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-03-20 11:48:13 +0300

    Prohibit 'native' annotation on constructors
    Currently doesn't work for primary ones
    
     #KT-7000 Fixed

---
+++ compiler/testData/diagnostics/testsWithStdLib/native/constructor.kt
@@ -0,0 +1,10 @@
+class A {
+    <!INAPPLICABLE_ANNOTATION!>native constructor() {}<!>
+    inner class B {
+        <!INAPPLICABLE_ANNOTATION!>native constructor() {}<!>
+    }
+
+    <!INAPPLICABLE_ANNOTATION!>native constructor(<!UNUSED_PARAMETER!>x<!>: Int)
+<!>}
+
+class C [native] () // TODO KT-7057
