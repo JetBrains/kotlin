cd4c4685eb7b1b245b7696fa26659d233f8f20be
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-03-20 20:44:41 +0300

    Do not cast findViewById to View (cherry picked from commit 2aba0a5)

--- plugins/android-compiler-plugin/testData/android/converter/simple/specialTags/layout.kt
+++ plugins/android-compiler-plugin/testData/android/converter/simple/specialTags/layout.kt
@@ -7,20 +7,20 @@ import android.widget.*
 import kotlin.internal.flexible.ft
 
 val Activity.includeTag: ft<View, View?>
-    get() = findViewById(0) as View
+    get() = findViewById(0) : View
 
 val Fragment.includeTag: ft<View, View?>
-    get() = getView().findViewById(0) as View
+    get() = getView().findViewById(0) : View
 
 val Activity.mergeTag: ft<View, View?>
-    get() = findViewById(0) as View
+    get() = findViewById(0) : View
 
 val Fragment.mergeTag: ft<View, View?>
-    get() = getView().findViewById(0) as View
+    get() = getView().findViewById(0) : View
 
 val Activity.fragmentTag: ft<View, View?>
-    get() = findViewById(0) as View
+    get() = findViewById(0) : View
 
 val Fragment.fragmentTag: ft<View, View?>
-    get() = getView().findViewById(0) as View
+    get() = getView().findViewById(0) : View
 
