7f7de1cbe48b2f0e40c10e6510f703dedd942696
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-03-23 15:52:42 +0300

    Generate synthetic packages even for layouts without @+id/ attributes declared (cherry picked from commit 8df0292)

---
+++ plugins/android-compiler-plugin/testData/android/converter/simple/noIds/layout1.kt
@@ -0,0 +1,8 @@
+package kotlinx.android.synthetic.layout.view
+
+import android.app.Activity
+import android.app.Fragment
+import android.view.View
+import android.widget.*
+import kotlin.internal.flexible.ft
+
