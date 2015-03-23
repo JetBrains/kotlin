e53ebcc877d620334e43533c0ba271b6b838f018
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-03-23 19:42:51 +0300

    Mark value arguments in case of empty delegation call
    It's useful for quickfixes for these diagnostics

--- compiler/testData/diagnostics/tests/secondaryConstructors/expectedPrimaryConstructorCall.kt
+++ compiler/testData/diagnostics/tests/secondaryConstructors/expectedPrimaryConstructorCall.kt
@@ -1,6 +1,6 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 class A(x: Int) {
-    <!PRIMARY_CONSTRUCTOR_DELEGATION_CALL_EXPECTED!>constructor<!>()
+    <!PRIMARY_CONSTRUCTOR_DELEGATION_CALL_EXPECTED!>constructor()<!>
 }
 open class B(x: Int)
 class C(x: Int) : B(x) {
