af425948f7f5b096da7bd240611dd0fd1f4570a4
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-03-25 02:22:22 +0300

    Add imports for XML widget tags, support.v4 fake package files (cherry picked from commit f7400d9)

--- plugins/android-compiler-plugin/testData/android/converter/simple/multiFile/layout.kt
+++ plugins/android-compiler-plugin/testData/android/converter/simple/multiFile/layout.kt
@@ -1,32 +1,38 @@
 package kotlinx.android.synthetic.layout
 
-import android.app.Activity
-import android.app.Fragment
-import android.view.View
+import android.app.*
+import android.view.*
 import android.widget.*
+import android.webkit.*
+import android.inputmethodservice.*
+import android.opengl.*
+import android.appwidget.*
+import android.support.v4.app.*
+import android.support.v4.view.*
+import android.support.v4.widget.*
 import kotlin.internal.flexible.ft
 
-val Activity.item_detail_container: ft<FrameLayout, FrameLayout?>
+val android.app.Activity.item_detail_container: ft<FrameLayout, FrameLayout?>
     get() = findViewById(0) as FrameLayout
 
-val Fragment.item_detail_container: ft<FrameLayout, FrameLayout?>
+val android.app.Fragment.item_detail_container: ft<FrameLayout, FrameLayout?>
     get() = getView().findViewById(0) as FrameLayout
 
-val Activity.textView1: ft<TextView, TextView?>
+val android.app.Activity.textView1: ft<TextView, TextView?>
     get() = findViewById(0) as TextView
 
-val Fragment.textView1: ft<TextView, TextView?>
+val android.app.Fragment.textView1: ft<TextView, TextView?>
     get() = getView().findViewById(0) as TextView
 
-val Activity.password: ft<EditText, EditText?>
+val android.app.Activity.password: ft<EditText, EditText?>
     get() = findViewById(0) as EditText
 
-val Fragment.password: ft<EditText, EditText?>
+val android.app.Fragment.password: ft<EditText, EditText?>
     get() = getView().findViewById(0) as EditText
 
-val Activity.login: ft<Button, Button?>
+val android.app.Activity.login: ft<Button, Button?>
     get() = findViewById(0) as Button
 
-val Fragment.login: ft<Button, Button?>
+val android.app.Fragment.login: ft<Button, Button?>
     get() = getView().findViewById(0) as Button
 
