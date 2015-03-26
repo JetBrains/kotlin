3fd776d14c698d84ed7f41462d5c088da8d0063b
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2015-03-26 12:45:33 +0300

    INVERT: Quick-Fixes: Fix signature configuration for extension functions

--- idea/testData/quickfix/changeSignature/afterFixExtensionLambdaSignature.kt
+++
@@ -1,9 +0,0 @@
-// "Change the signature of function literal" "true"
-
-fun foo(f: Int.(Int, Int) -> Int) {
-
-}
-
-fun test() {
-    foo {(i: Int, i1: Int) -> 0 }
-}
--- idea/testData/quickfix/changeSignature/beforeRemoveUnusedExtensionParameter.kt
+++
@@ -1,9 +0,0 @@
-// "Remove parameter 'x'" "true"
-
-fun Int.f(<caret>x: Int) {
-
-}
-
-fun test() {
-    1.f(2)
-}
