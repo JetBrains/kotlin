aaece3dcab5a414a7362d65bd96a05b1ca4af9a0
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2015-03-26 13:47:18 +0300

    INVERT: Quick-Fixes: Approximate non-denotable type with nearest denotable supertype

--- idea/testData/quickfix/typeMismatch/afterAccessibleLocalClassInReturn.kt
+++
@@ -1,8 +0,0 @@
-// "Change 'bar' function return type to 'A'" "true"
-fun foo() {
-    open class A
-
-    fun bar(): A {
-        return object: A() {}
-    }
-}
--- idea/testData/quickfix/typeMismatch/afterAnonymousObjectInInfixCall.kt
+++
@@ -1,8 +0,0 @@
-// "Change parameter 't' type of function 'foo' to 'T'" "true"
-trait T
-
-fun Int.foo(t: T) = this
-
-fun foo() {
-    1 foo object: T{}
-}
