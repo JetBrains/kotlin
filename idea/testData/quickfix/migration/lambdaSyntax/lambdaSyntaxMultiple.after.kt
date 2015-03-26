7d15cfa60eff43f2dba6b443d349b4f560dfd8fd
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2015-03-26 16:16:11 +0300

    Fixed formatter for function expression

--- idea/testData/quickfix/migration/lambdaSyntax/lambdaSyntaxMultiple.after.kt
+++ idea/testData/quickfix/migration/lambdaSyntax/lambdaSyntaxMultiple.after.kt
@@ -1,9 +1,7 @@
 // "Migrate lambda syntax in whole project" "true"
 
-val a =
-        fun (): Int {
-            val b =
-                    fun (): Int = 5
+val a = fun (): Int {
+    val b = fun (): Int = 5
 
-            return b()
-        }
+    return b()
+}
