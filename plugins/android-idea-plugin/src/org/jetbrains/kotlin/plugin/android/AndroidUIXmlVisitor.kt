169e48e0444bb65d1e472682f0bd78669cf257da
Michal Faber <michalfaber@gmail.com>
2015-03-30 19:22:15 +0200

    Visitor class for parsing xml layouts

---
+++ plugins/android-idea-plugin/src/org/jetbrains/kotlin/plugin/android/AndroidUIXmlVisitor.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.plugin.android
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.XmlElementVisitor
+import com.intellij.psi.xml.XmlElement
+import com.intellij.psi.xml.XmlTag
+import java.util.HashMap
+
+class AndroidUIXmlVisitor(private val elementCallback: (String, HashMap<String, String>) -> Unit) : XmlElementVisitor() {
+
+    override fun visitElement(element: PsiElement) {
+        element.acceptChildren(this)
+    }
+
+    override fun visitXmlElement(element: XmlElement?) {
+        element?.acceptChildren(this)
+    }
+
+    override fun visitXmlTag(tag: XmlTag?) {
+        if (tag != null) {
+            val attributesMap = tag.toMap()
+            elementCallback(tag.getLocalName(), attributesMap)
+            tag.acceptChildren(this)
+        }
+    }
+}
+
+public fun XmlTag.toMap(): HashMap<String, String> {
+    val res = HashMap<String, String>()
+    val attributes = getAttributes()
+    for (index in 0..attributes.size() - 1) {
+        val attrName = attributes.get(index).getLocalName()
+        val attrVal = attributes.get(index).getValue()
+        res[attrName] = attrVal
+    }
+    return res
+}
+
+
