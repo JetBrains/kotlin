36e0ce00eb025e69e1bfeb08bf3ad21409c4535a
Dmitry Jemerov <yole@jetbrains.com>
2015-04-01 16:30:48 +0200

    add test for do/while

--- compiler/testData/diagnostics/tests/BreakContinueInWhen.kt
+++ compiler/testData/diagnostics/tests/BreakContinueInWhen.kt
@@ -30,6 +30,21 @@ fun breakContinueInWhenWithWhile(i: Int, j: Int) {
     }
 }
 
+fun breakContinueInWhenWithDoWhile(i: Int, j: Int) {
+    do {
+        when (i) {
+            0 -> <!BREAK_OR_CONTINUE_IN_WHEN!>continue<!>
+            1 -> <!BREAK_OR_CONTINUE_IN_WHEN!>break<!>
+            2 -> {
+                do {
+                    if (j == 5) break
+                    if (j == 10) continue
+                } while (j > 0)
+            }
+        }
+    } while (i > 0)
+}
+
 fun labeledBreakContinue(i: Int) {
     @outer for (y in 0..10) {
         when (i) {
