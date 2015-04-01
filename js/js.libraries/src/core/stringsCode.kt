cac058d5a33ca67e92ba207c4cfc2d4c38b79e9b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-04-01 17:15:01 +0300

    Fix endsWith in JS always ignored character case, regardless of the ignoreCase parameter value. startsWith, endsWith again use native implementation when possible.

--- js/js.libraries/src/core/stringsCode.kt
+++ js/js.libraries/src/core/stringsCode.kt
@@ -1,25 +1,38 @@
 package kotlin.js
 
+// TODO: make internal
 public inline fun String.nativeIndexOf(ch : Char, fromIndex : Int) : Int = nativeIndexOf(ch.toString(), fromIndex)
 public inline fun String.nativeLastIndexOf(ch : Char, fromIndex : Int) : Int = nativeLastIndexOf(ch.toString(), fromIndex)
 
 /**
  * Returns `true` if this string starts with the specified prefix.
  */
-public fun String.startsWith(prefix: String, ignoreCase: Boolean = false): Boolean =
-        regionMatches(0, prefix, 0, prefix.length(), ignoreCase)
+public fun String.startsWith(prefix: String, ignoreCase: Boolean = false): Boolean {
+    if (!ignoreCase)
+        return nativeStartsWith(prefix, 0)
+    else
+        return regionMatches(0, prefix, 0, prefix.length(), ignoreCase)
+}
 
 /**
  * Returns `true` if a substring of this string starting at the specified offset [thisOffset] starts with the specified prefix.
  */
-public fun String.startsWith(prefix: String, thisOffset: Int, ignoreCase: Boolean = false): Boolean =
-        regionMatches(thisOffset, prefix, 0, prefix.length(), ignoreCase)
+public fun String.startsWith(prefix: String, thisOffset: Int, ignoreCase: Boolean = false): Boolean {
+    if (!ignoreCase)
+        return nativeStartsWith(prefix, thisOffset)
+    else
+        return regionMatches(thisOffset, prefix, 0, prefix.length(), ignoreCase)
+}
 
 /**
  * Returns `true` if this string ends with the specified suffix.
  */
-public fun String.endsWith(suffix: String, ignoreCase: Boolean = false): Boolean =
-        regionMatches(length() - suffix.length(), suffix, 0, suffix.length(), ignoreCase = true)
+public fun String.endsWith(suffix: String, ignoreCase: Boolean = false): Boolean {
+    if (!ignoreCase)
+        return nativeEndsWith(suffix)
+    else
+        return regionMatches(length() - suffix.length(), suffix, 0, suffix.length(), ignoreCase)
+}
 
 
 
