8145c68cb1b8f404f3cd3ade82d608ad7209e077
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-04-02 23:33:25 +0300

    NavigateToLibrarySourceTest -> NavigateToLibrarySourceTestGenerated

---
+++ idea/tests/org/jetbrains/kotlin/idea/decompiler/navigation/AbstractNavigateToLibrarySourceTest.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.idea.decompiler.navigation
+
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.psi.PsiElement
+import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.testFramework.UsefulTestCase
+import junit.framework.TestCase
+import org.jetbrains.kotlin.idea.JdkAndMockLibraryProjectDescriptor
+import org.jetbrains.kotlin.idea.navigation.NavigationTestUtils
+import org.jetbrains.kotlin.idea.references.JetReference
+import org.jetbrains.kotlin.test.JetTestUtils
+import java.util.LinkedHashMap
+
+public abstract class AbstractNavigateToLibrarySourceTest : AbstractNavigateToLibraryTest() {
+    protected fun doTest(path: String) {
+        myFixture.configureByFile(path)
+
+        checkAnnotatedLibraryCode(false)
+        checkAnnotatedLibraryCode(true)
+    }
+
+    override fun tearDown() {
+        JetSourceNavigationHelper.setForceResolve(false)
+        super.tearDown()
+    }
+
+    private fun checkAnnotatedLibraryCode(forceResolve: Boolean) {
+        JetSourceNavigationHelper.setForceResolve(forceResolve)
+        val actualCode = NavigationTestUtils.getNavigateElementsText(myFixture.getProject(), collectInterestingNavigationElements())
+        val expectedCode = getExpectedAnnotatedLibraryCode()
+        UsefulTestCase.assertSameLines(expectedCode, actualCode)
+    }
+
+    private fun collectInterestingReferences(): Collection<JetReference> {
+        val psiFile = myFixture.getFile()
+        val referenceContainersToReferences = LinkedHashMap<PsiElement, JetReference>()
+        for (offset in 0..psiFile.getTextLength() - 1) {
+            val ref = psiFile.findReferenceAt(offset)
+            if (ref is JetReference && !referenceContainersToReferences.containsKey(ref.getElement())) {
+                val target = ref.resolve()
+                if (target == null) continue
+
+                val targetNavPsiFile = target.getNavigationElement().getContainingFile()
+                if (targetNavPsiFile == null) continue
+
+                val targetNavFile = targetNavPsiFile.getVirtualFile()
+                if (targetNavFile == null) continue
+
+                if (ProjectFileIndex.SERVICE.getInstance(getProject()).isInLibrarySource(targetNavFile)) {
+                    referenceContainersToReferences.put(ref.getElement(), ref)
+                }
+            }
+        }
+        return referenceContainersToReferences.values()
+    }
+
+    private fun collectInterestingNavigationElements() =
+        collectInterestingReferences().map {
+            val target = it.resolve()
+            TestCase.assertNotNull(target)
+            target!!.getNavigationElement()
+        }
+
+    private fun getExpectedAnnotatedLibraryCode(): String {
+        val document = myFixture.getDocument(myFixture.getFile())
+        TestCase.assertNotNull(document)
+        return JetTestUtils.getLastCommentedLines(document)
+    }
+
+    override fun getProjectDescriptor(): LightProjectDescriptor =
+        JdkAndMockLibraryProjectDescriptor(AbstractNavigateToLibraryTest.TEST_DATA_PATH + "/library", true)
+}
