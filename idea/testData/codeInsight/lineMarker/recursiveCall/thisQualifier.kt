d7928b725142492b8dad287736e25cf14dc3be88
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2015-04-08 16:43:34 +0300

    Added test cases with labeled diff.

--- idea/testData/codeInsight/lineMarker/recursiveCall/thisQualifier.kt
+++ idea/testData/codeInsight/lineMarker/recursiveCall/thisQualifier.kt
@@ -1,7 +1,15 @@
-class F {
+class Outer {
     fun f(a: Int) {
-        if (a > 0) {
-            this.<lineMarker>f</lineMarker>(a - 1)
+    }
+
+    class F {
+        fun f(a: Int) {
+            if (a > 0) {
+                this.<lineMarker>f</lineMarker>(a - 1)
+                this@F.<lineMarker>f</lineMarker>(a - 1)
+
+                this@Outer.f(a - 1)
+            }
         }
     }
 }
