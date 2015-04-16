73b2de95abea8b7be316f55815474eb9fc409986
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2015-04-16 13:40:08 +0300

    Create from Usage: Do not delete temporary file as this operation is not supported by IDEA anymore

--- idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/callableBuilder/CallableBuilder.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/callableBuilder/CallableBuilder.kt
@@ -216,7 +216,7 @@ class CallableBuilder(val config: CallableBuilderConfiguration) {
         val receiverTypeCandidate: TypeCandidate?
         val mandatoryTypeParametersAsCandidates: List<TypeCandidate>
         val substitutions: List<JetTypeSubstitution>
-        var released: Boolean = false
+        var finished: Boolean = false
 
         init {
             // gather relevant information
@@ -1041,7 +1041,7 @@ class CallableBuilder(val config: CallableBuilderConfiguration) {
                         }
                     }
                     finally {
-                        release()
+                        finished = true
                         onFinish()
                     }
                 }
@@ -1057,16 +1057,8 @@ class CallableBuilder(val config: CallableBuilderConfiguration) {
         }
 
         fun showDialogIfNeeded() {
-            if (!ApplicationManager.getApplication().isUnitTestMode() && dialogWithEditor != null && !released) {
+            if (!ApplicationManager.getApplication().isUnitTestMode() && dialogWithEditor != null && !finished) {
                 dialogWithEditor.show()
-            }
-        }
-
-        private fun release() {
-            if (released) return
-            dialogWithEditor?.let {
-                jetFileToEdit.delete()
-                released = true
             }
         }
     }
