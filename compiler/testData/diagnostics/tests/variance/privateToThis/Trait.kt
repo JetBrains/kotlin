cd9f80628001a42a969273f91187e5b111c7c917
Dmitry Jemerov <yole@jetbrains.com>
2015-04-20 20:59:52 +0200

    add test for private-to-this visibility in traits

---
+++ compiler/testData/diagnostics/tests/variance/privateToThis/Trait.kt
@@ -0,0 +1,7 @@
+internal trait Test</*0*/ in I> {
+    private/*private to this*/ final fun foo(): I {
+        throw Exception()
+    }
+
+    private/*private to this*/ final val i: I get() = foo()
+}
