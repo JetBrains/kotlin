c6b91b0f8168026fd9351d1fab67045155a1b755
Andrey Breslav <andrey.breslav@jetbrains.com>
2015-05-07 21:58:22 +0300

    Java type annotations supported in LazyJavaTypeResolver

--- core/descriptors/src/org/jetbrains/kotlin/descriptors/annotations/Annotations.kt
+++ core/descriptors/src/org/jetbrains/kotlin/descriptors/annotations/Annotations.kt
@@ -65,3 +65,15 @@ class FilteredAnnotations(
 
     override fun isEmpty() = !iterator().hasNext()
 }
+
+class CompositeAnnotations(
+        private val delegates: List<Annotations>
+) : Annotations {
+    override fun isEmpty() = delegates.all { it.isEmpty() }
+
+    override fun findAnnotation(fqName: FqName) = delegates.asSequence().map { it.findAnnotation(fqName) }.filterNotNull().firstOrNull()
+
+    override fun findExternalAnnotation(fqName: FqName) = delegates.asSequence().map { it.findExternalAnnotation(fqName) }.filterNotNull().firstOrNull()
+
+    override fun iterator() = delegates.asSequence().flatMap { it.asSequence() }.iterator()
+}
