ca54fe0883afb233aca051a42ac7119e3274c8af
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2015-05-11 16:35:21 +0300

    Introduce Property: Fix primary occurrence replacement  #KT-7610 Fixed

--- idea/src/org/jetbrains/kotlin/idea/refactoring/introduce/introduceProperty/KotlinInplacePropertyIntroducer.kt
+++ idea/src/org/jetbrains/kotlin/idea/refactoring/introduce/introduceProperty/KotlinInplacePropertyIntroducer.kt
@@ -22,6 +22,9 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Pass
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.SearchScope
+import com.intellij.refactoring.introduce.inplace.InplaceVariableIntroducer
 import com.intellij.ui.NonFocusableCheckBox
 import com.intellij.ui.PopupMenuListenerAdapter
 import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionResult
@@ -30,6 +33,9 @@ import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.generate
 import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.processDuplicatesSilently
 import org.jetbrains.kotlin.idea.refactoring.introduce.introduceVariable.KotlinInplaceVariableIntroducer
 import org.jetbrains.kotlin.idea.refactoring.introduce.introduceVariable.KotlinInplaceVariableIntroducer.ControlWrapper
+import org.jetbrains.kotlin.idea.search.usagesSearch.DefaultSearchHelper
+import org.jetbrains.kotlin.idea.search.usagesSearch.UsagesSearchTarget
+import org.jetbrains.kotlin.idea.search.usagesSearch.search
 import org.jetbrains.kotlin.psi.JetClassOrObject
 import org.jetbrains.kotlin.psi.JetExpression
 import org.jetbrains.kotlin.psi.JetFile
@@ -159,6 +165,10 @@ public class KotlinInplacePropertyIntroducer(
 
     override fun checkLocalScope(): PsiElement? {
         return myElementToRename.parents().first { it is JetClassOrObject || it is JetFile }
+    }
+
+    override fun collectRefs(referencesSearchScope: SearchScope): MutableCollection<PsiReference>? {
+        return DefaultSearchHelper<JetProperty>().newRequest(UsagesSearchTarget(property, referencesSearchScope)).search().toArrayList()
     }
 
     override fun moveOffsetAfter(success: Boolean) {
