ad91d2cd9e156ab9fcad8d0ec24bb6023b206d37
Valentin Kipyatkov <valentin@jetbrains.com>
2015-05-12 18:32:04 +0300

    Removed unused deprecated constructors

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/intentions/JetSelfTargetingIntention.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/intentions/JetSelfTargetingIntention.kt
@@ -33,9 +33,6 @@ public abstract class JetSelfTargetingIntention<TElement : JetElement>(
         private val familyName: String = text,
         private val firstElementOfTypeOnly: Boolean = false
 ) : IntentionAction {
-    deprecated("Use primary constructor, no need to use i18n")
-    public constructor(key: String, elementType: Class<TElement>) : this(elementType, JetBundle.message(key), JetBundle.message(key + ".family")) {
-    }
 
     protected fun setText(text: String) {
         this.text = text
@@ -95,10 +92,6 @@ public abstract class JetSelfTargetingRangeIntention<TElement : JetElement>(
         firstElementOfTypeOnly: Boolean = false
 ) : JetSelfTargetingIntention<TElement>(elementType, text, familyName, firstElementOfTypeOnly) {
 
-    deprecated("Use primary constructor, no need to use i18n")
-    public constructor(key: String, elementType: Class<TElement>) : this(elementType, JetBundle.message(key), JetBundle.message(key + ".family")) {
-    }
-
     public abstract fun applicabilityRange(element: TElement): TextRange?
 
     override final fun isApplicableTo(element: TElement, caretOffset: Int): Boolean {
@@ -113,10 +106,6 @@ public abstract class JetSelfTargetingOffsetIndependentIntention<TElement : JetE
         familyName: String = text,
         firstElementOfTypeOnly: Boolean = false
 ) : JetSelfTargetingRangeIntention<TElement>(elementType, text, familyName, firstElementOfTypeOnly) {
-
-    deprecated("Use primary constructor, no need to use i18n")
-    public constructor(key: String, elementType: Class<TElement>) : this(elementType, JetBundle.message(key), JetBundle.message(key + ".family")) {
-    }
 
     public abstract fun isApplicableTo(element: TElement): Boolean
 
