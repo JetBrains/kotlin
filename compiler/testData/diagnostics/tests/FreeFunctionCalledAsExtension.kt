35ae6b4941300e019cc72a197edf01d421fdd4d9
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-05-26 17:34:43 +0300

    Report 'free function called as extension' on Function subclasses
    You can't make a value of some type invokable as an extension by adding
    'extension' annotation to the type

--- compiler/testData/diagnostics/tests/FreeFunctionCalledAsExtension.kt
+++ compiler/testData/diagnostics/tests/FreeFunctionCalledAsExtension.kt
@@ -1,3 +1,13 @@
 fun foo(a: (String) -> Unit) {
     "".<!FREE_FUNCTION_CALLED_AS_EXTENSION!>a<!>()
 }
+
+
+
+interface A : (String) -> Unit {}
+
+fun foo(a: @extension A) {
+    // @extension annotation on an unrelated type shouldn't have any effect on this diagnostic.
+    // Only kotlin.Function{n} type annotated with @extension should
+    "".<!FREE_FUNCTION_CALLED_AS_EXTENSION!>a<!>()
+}
