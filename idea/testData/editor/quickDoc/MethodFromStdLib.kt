851007cc6cc530ce6231780286016ed86d8c2a79
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-05-28 00:33:43 +0300

    Fix output of test that relied on the documentation wording.

--- idea/testData/editor/quickDoc/MethodFromStdLib.kt
+++ idea/testData/editor/quickDoc/MethodFromStdLib.kt
@@ -2,5 +2,5 @@ fun test() {
     listOf(1, 2, 4).<caret>filter { it > 0 }
 }
 
-//INFO: inline <b>public</b> <b>fun</b> &lt;T&gt; Iterable&lt;T&gt;.filter(predicate: (T) &rarr; Boolean): List&lt;T&gt; <i>defined in</i> kotlin<br/><p>Returns a list containing all elements matching the given <a href="psi_element://predicate">predicate</a>
+//INFO: inline <b>public</b> <b>fun</b> &lt;T&gt; Iterable&lt;T&gt;.filter(predicate: (T) &rarr; Boolean): List&lt;T&gt; <i>defined in</i> kotlin<br/><p>Returns a list containing all elements matching the given <a href="psi_element://predicate">predicate</a>.
 //INFO: </p>
