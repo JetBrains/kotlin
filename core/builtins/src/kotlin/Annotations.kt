4d05f137d4b460f66d45a01cab2bcb57eb812b13
Dmitry Jemerov <yole@jetbrains.com>
2015-05-29 15:38:41 +0200

    clarify description of ReplaceWith

--- core/builtins/src/kotlin/Annotations.kt
+++ core/builtins/src/kotlin/Annotations.kt
@@ -33,11 +33,15 @@ public annotation class data
 public annotation class deprecated(val value: String, val replaceWith: ReplaceWith = ReplaceWith(""))
 
 /**
- * Specifies a code fragment that can be used to replace a deprecated function or property.
+ * Specifies a code fragment that can be used to replace a deprecated function or property. Tools such
+ * as IDEs can automatically apply the replacements specified through this annotation.
  *
- * @property expression the replacement expression. For function calls, the replacement expression may
- *     contain argument names of the deprecated function, which will be substituted with actual parameters
- *     used in the call being updated.
+ * @property expression the replacement expression. The replacement expression is interpreted in the context
+ *     of the function or property being called, and can reference members of enclosing classes etc.
+ *     For function calls, the replacement expression may contain argument names of the deprecated function,
+ *     which will be substituted with actual parameters used in the call being updated. The imports used in the file
+ *     containing the deprecated function or property are NOT accessible; if the replacement expression refers
+ *     on any of those imports, they need to be specified explicitly in the [imports] parmeter.
  * @property imports the qualified names that need to be imported in order for the references in the
  *     replacement expression to be resolved correctly.
  */
