37e0a0b0454983fec9f86f4bf1eb4e30f8582927
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-06-01 15:55:48 +0300

    Generate private constructors for enums in coverter

--- j2k/testData/fileOrElement/enum/constantsWithBody2.kt
+++ j2k/testData/fileOrElement/enum/constantsWithBody2.kt
@@ -1,5 +1,5 @@
 // ERROR: Cannot access 'p': it is 'invisible_fake' in 'A'
-public enum class E(private val p: Int) {
+public enum class E private constructor(private val p: Int) {
     A(1) {
         override fun bar() {
             foo(this.p)
