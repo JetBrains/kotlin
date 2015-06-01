3e231f2e44cc95cc653a655b0d9510ccaa394834
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-06-01 22:58:02 +0300

    Generalize run() scope function. #KT-5235 Fixed

--- libraries/stdlib/src/kotlin/util/Standard.kt
+++ libraries/stdlib/src/kotlin/util/Standard.kt
@@ -11,7 +11,12 @@ public fun <A, B> A.to(that: B): Pair<A, B> = Pair(this, that)
 /**
  * Calls the specified function.
  */
-public inline fun <T> run(f: () -> T): T = f()
+public inline fun <R> run(f: () -> R): R = f()
+
+/**
+ * Calls the specified function with this value as its receiver.
+ */
+public inline fun <T, R> T.run(f: T.() -> R): R = f()
 
 /**
  * Execute f with given receiver
