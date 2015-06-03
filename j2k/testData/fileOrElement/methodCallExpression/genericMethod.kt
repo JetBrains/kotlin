f14615315dff589c96caf7b6e05396e5ac72f4f6
Valentin Kipyatkov <valentin@jetbrains.com>
2015-06-03 21:28:47 +0300

    KT-5482 Redundant type arguments are not detected in some cases
     #KT-5482 Fixed

--- j2k/testData/fileOrElement/methodCallExpression/genericMethod.kt
+++ j2k/testData/fileOrElement/methodCallExpression/genericMethod.kt
@@ -1,13 +1,13 @@
 package demo
 
 class Map {
-    fun <K, V> put(k: K, v: V) {
+    fun <K, V> put(k: K?, v: V) {
     }
 }
 
 class U {
     fun test() {
         val m = Map()
-        m.put<String, Int>("10", 10)
+        m.put<String, Int>(null, 10)
     }
 }
