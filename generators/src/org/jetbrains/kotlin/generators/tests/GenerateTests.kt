f355d4b4e08e2a8776aa105e24f6fc5b38419efd
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-06-10 19:41:35 +0300

    JS/RTTI: use generation for rtti test

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1065,6 +1065,10 @@ fun main(args: Array<String>) {
         testClass<AbstractReifiedTest>() {
             model("reified/cases")
         }
+
+        testClass(javaClass<AbstractRttiTest>()) {
+            model("rtti/cases")
+        }
     }
 
     testGroup("js/js.tests/test", "compiler/testData") {
