7dd6fe9c10e53e6174d366ca6febd9fc39ee086a
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-06-10 16:44:06 +0300

    JS/RTTI: fixed is-check on nullable reified parameters     #KT-7016 fixed     #KT-8012 fixed

--- js/js.translator/testData/reified/cases/isClass.kt
+++ js/js.translator/testData/reified/cases/isClass.kt
@@ -17,5 +17,9 @@ fun box(): String {
     assertEquals(true, isInstance<D>(D()))
     assertEquals(true, isInstance<C>(D()))
 
+    assertEquals(true, isInstance<D?>(D()), "isInstance<D?>(D())")
+    assertEquals(true, isInstance<D?>(null), "isInstance<D?>(null)")
+    assertEquals(false, isInstance<D?>(C()), "isInstance<D?>(C())")
+
     return "OK"
 }
