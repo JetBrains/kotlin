6b747cf6f895e9b0c04b5aa8dfada7831cb4b252
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-06-10 22:03:11 +0300

    Move delegate property interfaces to a separate file.

--- libraries/stdlib/src/kotlin/properties/Delegation.kt
+++ libraries/stdlib/src/kotlin/properties/Delegation.kt
@@ -1,46 +1,5 @@
 package kotlin.properties
 
-/**
- * Base interface that can be used for implementing property delegates of read-only properties. This is provided only for
- * convenience; you don't have to extend this interface as long as your property delegate has methods with the same
- * signatures.
- * @param R the type of object which owns the delegated property.
- * @param T the type of the property value.
- */
-public interface ReadOnlyProperty<in R, out T> {
-    /**
-     * Returns the value of the property for the given object.
-     * @param thisRef the object for which the value is requested.
-     * @param desc the metadata for the property.
-     * @return the property value.
-     */
-    public fun get(thisRef: R, desc: PropertyMetadata): T
-}
-
-/**
- * Base interface that can be used for implementing property delegates of read-only properties. This is provided only for
- * convenience; you don't have to extend this interface as long as your property delegate has methods with the same
- * signatures.
- * @param R the type of object which owns the delegated property.
- * @param T the type of the property value.
- */
-public interface ReadWriteProperty<in R, T> {
-    /**
-     * Returns the value of the property for the given object.
-     * @param thisRef the object for which the value is requested.
-     * @param desc the metadata for the property.
-     * @return the property value.
-     */
-    public fun get(thisRef: R, desc: PropertyMetadata): T
-
-    /**
-     * Sets the value of the property for the given object.
-     * @param thisRef the object for which the value is requested.
-     * @param desc the metadata for the property.
-     * @param value the value to set.
-     */
-    public fun set(thisRef: R, desc: PropertyMetadata, value: T)
-}
 
 /**
  * Standard property delegates.
