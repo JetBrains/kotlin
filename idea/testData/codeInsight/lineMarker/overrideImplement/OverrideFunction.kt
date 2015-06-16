052ff5f8e6e9aad871f74636927cef5fbac96584
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-06-17 02:29:30 +0300

    Update test data: Idea can't parse description with "ctrl+ alt + b" promotion

--- idea/testData/codeInsight/lineMarker/overrideImplement/OverrideFunction.kt
+++ idea/testData/codeInsight/lineMarker/overrideImplement/OverrideFunction.kt
@@ -3,10 +3,8 @@ open class <lineMarker></lineMarker>A {
   }
 }
 
-open class <lineMarker descr="<html><body>Is subclassed by<br>&nbsp;&nbsp;&nbsp;&nbsp;C</body></html>"></lineMarker>B:A(){
-  override fun <lineMarker
-      descr="<b>internal</b> <b>open</b> <b>fun</b> a(): kotlin.Unit <i>defined in</i> B<br/>overrides<br/><b>internal</b> <b>open</b> <b>fun</b> a(): kotlin.Unit <i>defined in</i> A"></lineMarker><lineMarker
-      descr="<html><body>Is overridden in <br>&nbsp;&nbsp;&nbsp;&nbsp;C</body></html>"></lineMarker>a(){
+open class <lineMarker></lineMarker>B:A(){
+  override fun <lineMarker></lineMarker><lineMarker></lineMarker>a(){
   }
 }
 
