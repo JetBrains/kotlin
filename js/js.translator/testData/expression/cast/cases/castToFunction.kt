19a2f6ad1da90341ee4896992cc571485185ae2a
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-06-17 22:44:09 +0300

    JS/RTTI: use typeof f === 'function' to check function instance

---
+++ js/js.translator/testData/expression/cast/cases/castToFunction.kt
@@ -0,0 +1,13 @@
+package foo
+
+fun test(fn: Any?): Function0<Int> =
+        fn as Function0<Int>
+
+fun box(): String {
+    val get11: Any? = { 11 }
+    assertEquals(11, test(get11)(), "get11")
+    failsClassCast("null") { test(null)() }
+    failsClassCast("object {}") { test(object {})() }
+
+    return "OK"
+}
