ef7cc6ae100774a6c3159db005b1aeb5cc2ca60e
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-06-17 15:41:47 +0300

    Force openning project

--- idea/idea-test-framework/src/org/jetbrains/kotlin/idea/test/JetLightCodeInsightFixtureTestCase.kt
+++ idea/idea-test-framework/src/org/jetbrains/kotlin/idea/test/JetLightCodeInsightFixtureTestCase.kt
@@ -16,29 +16,23 @@
 
 package org.jetbrains.kotlin.idea.test
 
+import com.intellij.codeInsight.daemon.impl.EditorTracker
 import com.intellij.ide.startup.impl.StartupManagerImpl
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.ex.ActionManagerEx
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.startup.StartupManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
-import com.intellij.psi.FileViewProvider
-import com.intellij.psi.PsiManager
-import com.intellij.psi.impl.PsiManagerEx
-import com.intellij.psi.impl.file.impl.FileManager
-import com.intellij.psi.impl.source.PsiFileImpl
 import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.actions.internal.KotlinInternalMode
-import org.jetbrains.kotlin.idea.test.KotlinStdJSProjectDescriptor
-import org.jetbrains.kotlin.idea.references.BuiltInsReferenceResolver
-import org.jetbrains.kotlin.psi.JetFile
 import org.jetbrains.kotlin.test.InTextDirectivesUtils
 import org.jetbrains.kotlin.test.JetTestUtils
-import org.jetbrains.kotlin.utils.*
-
+import org.jetbrains.kotlin.utils.rethrow
 import java.io.File
 import java.io.IOException
 
@@ -52,6 +46,8 @@ public abstract class JetLightCodeInsightFixtureTestCase : LightCodeInsightFixtu
 
         kotlinInternalModeOriginalValue = KotlinInternalMode.enabled
         KotlinInternalMode.enabled = true
+
+        getProject().getComponent(javaClass<EditorTracker>())?.projectOpened()
     }
 
     override fun tearDown() {
