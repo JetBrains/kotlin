463b0cda79d4ed78e8419b5a6caf7f242f4aeacb
Valentin Kipyatkov <valentin@jetbrains.com>
2015-06-24 15:57:29 +0200

    Fixed test data

--- idea/testData/quickfix/createFromUsage/createClass/callExpression/callWithMissingArgs.kt
+++ idea/testData/quickfix/createFromUsage/createClass/callExpression/callWithMissingArgs.kt
@@ -2,6 +2,7 @@
 // ACTION: Create function 'Foo'
 // ACTION: Remove parameter 's'
 // ACTION: Create secondary constructor
+// ACTION: Add 'i =' to argument
 // ERROR: No value passed for parameter s
 
 class Foo(i: Int, s: String)
