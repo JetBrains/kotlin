4800147f4e62f437597f9b138790484d56b690e0
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2015-07-03 22:15:47 +0300

    publicField & field in companion object (cherry picked from commit 3bcdee2)

---
+++ compiler/testData/asJava/lightClasses/publicField/CompanionObject.java
@@ -0,0 +1,13 @@
+public final class C {
+    @kotlin.jvm.publicField
+    public static final java.lang.String foo = "A";
+    public static final C.Companion Companion;
+
+    public C() { /* compiled code */ }
+
+    public static final class Companion {
+        private final java.lang.String getFoo() { /* compiled code */ }
+
+        private Companion() { /* compiled code */ }
+    }
+}
