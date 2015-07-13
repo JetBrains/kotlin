d6de6b653d3749a51cda472e6ae28a3f1b8f7832
octomarat <maratx387@gmail.com>
2015-07-13 18:29:58 +0300

    General CFG basic support added, code moved from OutOfBoundChecker to PseudocodeIntegerVariablesDataCollector

---
+++ compiler/frontend/src/org/jetbrains/kotlin/cfg/PseudocodeIntegerVariablesDataCollector.kt
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.cfg
+
+import com.intellij.psi.tree.IElementType
+import org.jetbrains.kotlin.JetNodeType
+import org.jetbrains.kotlin.JetNodeTypes
+import org.jetbrains.kotlin.cfg
+import org.jetbrains.kotlin.cfg.pseudocode.Pseudocode
+import org.jetbrains.kotlin.cfg.pseudocode.instructions.Instruction
+import org.jetbrains.kotlin.cfg.pseudocode.instructions.eval.MagicInstruction
+import org.jetbrains.kotlin.cfg.pseudocode.instructions.eval.MagicKind
+import org.jetbrains.kotlin.cfg.pseudocode.instructions.eval.ReadValueInstruction
+import org.jetbrains.kotlin.cfg.pseudocode.instructions.special.VariableDeclarationInstruction
+import org.jetbrains.kotlin.cfg.pseudocodeTraverser.Edges
+import org.jetbrains.kotlin.cfg.pseudocodeTraverser.TraversalOrder
+import org.jetbrains.kotlin.cfg.pseudocodeTraverser.collectData
+import org.jetbrains.kotlin.descriptors.VariableDescriptor
+import org.jetbrains.kotlin.lexer.JetToken
+import org.jetbrains.kotlin.lexer.JetTokens
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.resolve.BindingContext
+import java.util.*
+import kotlin.properties.Delegates
+
+// This file contains functionality similar to org.jetbrains.kotlin.cfg.PseudocodeVariablesData,
+// but collects information about integer variables' values. Semantically it would be better to
+// merge functionality in this two files
+
+public class PseudocodeIntegerVariablesDataCollector(val pseudocode: Pseudocode) {
+    public var integerVariablesValues: Map<Instruction, Edges<ValuesData>> = collectVariableValuesData()
+        private set
+
+    public data class ValuesData(
+            val variablesToValues: MutableMap<String, IntegerVariableValues>,
+            val fakeVariablesToValues: MutableMap<String, IntegerVariableValues>
+    )
+
+    public fun recollectIntegerVariablesValues() {
+        integerVariablesValues = collectVariableValuesData()
+    }
+
+    private  fun collectVariableValuesData(): Map<Instruction, Edges<ValuesData>> {
+        return pseudocode.collectData(
+                TraversalOrder.FORWARD,
+                /* mergeDataWithLocalDeclarations */ true,
+                { instruction, incomingData: Collection<ValuesData> -> mergeVariablesValues(instruction, incomingData) },
+                { i, j, e -> e},
+                ValuesData(HashMap(), HashMap())
+        )
+    }
+
+    private fun mergeVariablesValues(instruction: Instruction, incomingEdgesData: Collection<ValuesData>): Edges<ValuesData> {
+        val enterInstructionData = unionIncomingVariablesValues(incomingEdgesData)
+        val exitInstructionData = updateValues(instruction, enterInstructionData)
+        return Edges(enterInstructionData, exitInstructionData)
+    }
+
+    private fun unionIncomingVariablesValues(incomingEdgesData: Collection<ValuesData>): ValuesData {
+        val unitedVariables: MutableMap<String, IntegerVariableValues> = HashMap()
+        val unitedFakeVariables: MutableMap<String, IntegerVariableValues> = HashMap()
+        for(data in incomingEdgesData) {
+            mergeCorrespondingVariables(unitedVariables, data.variablesToValues)
+            mergeCorrespondingVariables(unitedFakeVariables, data.fakeVariablesToValues)
+        }
+        return ValuesData(unitedVariables, unitedFakeVariables)
+    }
+
+    private fun mergeCorrespondingVariables(
+            targetVariablesMap: MutableMap<String, IntegerVariableValues>,
+            variablesToConsume: MutableMap<String, IntegerVariableValues>
+    ) {
+        val unionByKey: (String) -> Unit = { key ->
+            val values1 = targetVariablesMap.getOrElse(key, { IntegerVariableValues.empty() })
+            val values2 = variablesToConsume.getOrElse(key, { IntegerVariableValues.empty() })
+            if(values1.cantBeDefined || values2.cantBeDefined) {
+                values1.setUndefined()
+            } else {
+                values1.addAll(values2)
+                targetVariablesMap.put(key, values1)
+            }
+        }
+        val targetMapKeys = HashSet(targetVariablesMap.keySet())
+        val mapToConsumeKeys = HashSet(variablesToConsume.keySet())
+        for(key in targetMapKeys) unionByKey(key)
+        for(key in mapToConsumeKeys) unionByKey(key)
+    }
+
+    private fun updateValues(instruction: Instruction, mergedEdgesData: ValuesData): ValuesData {
+        val updatedData = ValuesData(HashMap(mergedEdgesData.variablesToValues), HashMap(mergedEdgesData.fakeVariablesToValues))
+        when(instruction) {
+            is VariableDeclarationInstruction -> {
+                // process variable declaration
+                val declarationName = instruction.variableDeclarationElement.getName() ?:
+                                      throw Exception("Variable declaration name is null")
+                updatedData.variablesToValues.put(declarationName, IntegerVariableValues.empty())
+            }
+            is ReadValueInstruction -> {
+                // process literal occurrence (all integer literals are stored to fake vars by read instruction)
+                val element = instruction.outputValue.element
+                if (element is JetConstantExpression) {
+                    val node = element.getNode()
+                    val nodeType = node.getElementType() as? JetNodeType ?:
+                                   throw Exception("Node's elementType has wrong type for JetConstantExpression")
+                    if (nodeType == JetNodeTypes.INTEGER_CONSTANT) {
+                        val literalValue = Integer.parseInt(node.getText())
+                        val fakeVariableName = instruction.outputValue.debugName
+                        updatedData.fakeVariablesToValues.put(fakeVariableName, IntegerVariableValues.singleton(literalValue))
+                    }
+                }
+            }
+            is MagicInstruction -> {
+                when(instruction.kind) {
+                    MagicKind.UNSUPPORTED_ELEMENT -> {
+                        // process assignment to variable
+                        assert(instruction.inputValues.size().equals(1), "Assignment instruction is supposed to have one input value")
+                        val fakeVariableName = instruction.inputValues.get(0).debugName
+                        val valuesToAssign = updatedData.fakeVariablesToValues.get(fakeVariableName)
+                        val assignmentTarget = tryGetAssignmentName(instruction)
+                        if(assignmentTarget != null) {
+                            if(valuesToAssign != null) {
+                                updatedData.variablesToValues.put(assignmentTarget, valuesToAssign)
+                            } else {
+                                updatedData.variablesToValues.put(assignmentTarget, IntegerVariableValues.cantBeDefined())
+                            }
+                        } else throw Exception("Assignment target's name can't is null")
+                    }
+                    MagicKind.UNRESOLVED_CALL -> {
+                        when(instruction.element) {
+                            is JetNameReferenceExpression -> {
+                                // process variable reference
+                                val referencedVariableName = instruction.element.getReferencedName()
+                                val referencedVariableValues = updatedData.variablesToValues.getOrElse(referencedVariableName, { null })
+                                if(referencedVariableValues != null) {
+                                    // we have the information about value, so it is definitely of integer type
+                                    // (assuming there are no undeclared variables)
+                                    val newFakeVariableName = instruction.outputValue.debugName
+                                    updatedData.fakeVariablesToValues.put(newFakeVariableName, referencedVariableValues)
+                                }
+                            }
+                            is JetBinaryExpression ->
+                                processBinaryArithmeticOperation(instruction.element.getOperationToken(), instruction, updatedData)
+                        }
+                    }
+                }
+            }
+        }
+        return updatedData
+    }
+
+    private fun tryGetAssignmentName(instruction: MagicInstruction): String? =
+            when(instruction.element) {
+                is JetProperty -> instruction.element.getName()
+                is JetBinaryExpression -> tryGetReferenceExprName(instruction.element.getLeft())
+                else -> null
+            }
+
+    private fun tryGetReferenceExprName(expression: JetExpression?) =
+            (expression as? JetNameReferenceExpression)?.getReferencedName()
+
+    private fun processBinaryArithmeticOperation(token: IElementType, instruction: MagicInstruction, updatedData: ValuesData) {
+        assert(instruction.inputValues.size().equals(2),
+               "Binary expression instruction is supposed to have two input values")
+        val leftOperandValues = updatedData.fakeVariablesToValues.get(instruction.inputValues.get(0).debugName)
+        val rightOperandValues = updatedData.fakeVariablesToValues.get(instruction.inputValues.get(1).debugName)
+        if (leftOperandValues == null || rightOperandValues == null) {
+            return
+        }
+        val result: IntegerVariableValues =
+            if(!leftOperandValues.areDefined || !rightOperandValues.areDefined) {
+                IntegerVariableValues.cantBeDefined()
+            } else when (token) {
+                JetTokens.PLUS -> applyEachToEach(leftOperandValues, rightOperandValues) { l, r -> l + r }
+                JetTokens.MINUS -> applyEachToEach(leftOperandValues, rightOperandValues) { l, r -> l - r }
+                JetTokens.MUL -> applyEachToEach(leftOperandValues, rightOperandValues) { l, r -> l * r }
+                JetTokens.DIV -> applyEachToEach(leftOperandValues, rightOperandValues) { l, r ->
+                    if (rightOperandValues.equals(0)) {
+                        throw Exception("OutOfBoundChecker: Division by zero detected")
+                    } else {
+                        l / r
+                    }
+                }
+                JetTokens.RANGE -> {
+                    // we can safely use casts below because of areDefined checks above
+                    val minOfLeft = leftOperandValues.getValues().min() as Int
+                    val maxOfRight = rightOperandValues.getValues().max() as Int
+                    val rangeValues = IntegerVariableValues.empty()
+                    for(value in minOfLeft..maxOfRight) {
+                        rangeValues.add(value)
+                    }
+                    rangeValues
+                }
+                else -> throw Exception("OutOfBoundChecker: Unsupported binary operation")
+            }
+        updatedData.fakeVariablesToValues.put(instruction.outputValue.debugName, result)
+    }
+
+    private fun applyEachToEach(left: IntegerVariableValues, right: IntegerVariableValues, operation: (Int, Int) -> Int)
+            : IntegerVariableValues {
+        val resultSet = HashSet<Int>()
+        for(l in left.getValues()) {
+            for(r in right.getValues()) {
+                resultSet.add(operation(l, r))
+            }
+        }
+        return IntegerVariableValues.ofCollection(resultSet)
+    }
+}
