102c5f6569b28c679c1624e3c5b813a8135a424a
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-08-05 15:51:51 +0300

    Add SourceElement to LazyJavaPackageFragment

--- core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/lazy/descriptors/LazyJavaPackageFragment.kt
+++ core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/lazy/descriptors/LazyJavaPackageFragment.kt
@@ -16,10 +16,12 @@
 
 package org.jetbrains.kotlin.load.java.lazy.descriptors
 
+import org.jetbrains.kotlin.descriptors.SourceElement
 import org.jetbrains.kotlin.descriptors.impl.PackageFragmentDescriptorImpl
 import org.jetbrains.kotlin.load.java.lazy.LazyJavaResolverContext
 import org.jetbrains.kotlin.load.java.structure.JavaClass
 import org.jetbrains.kotlin.load.java.structure.JavaPackage
+import org.jetbrains.kotlin.load.kotlin.KotlinJvmBinarySourceElement
 
 class LazyJavaPackageFragment(
         private val c: LazyJavaResolverContext,
@@ -37,4 +39,10 @@ class LazyJavaPackageFragment(
     override fun getMemberScope() = scope
 
     override fun toString() = "lazy java package fragment: $fqName"
+
+    override fun getSource(): SourceElement {
+        val binaryClass = scope.kotlinBinaryClass ?: return SourceElement.NO_SOURCE
+
+        return KotlinJvmBinarySourceElement(binaryClass)
+    }
 }
