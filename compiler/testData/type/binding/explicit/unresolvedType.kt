ceae7eda391b5a9ecdc8b8f84b72d7d085e1bba7
Valentin Kipyatkov <valentin@jetbrains.com>
2015-08-08 12:13:27 +0300

    Fixed test data

--- compiler/testData/type/binding/explicit/unresolvedType.kt
+++ compiler/testData/type/binding/explicit/unresolvedType.kt
@@ -1,11 +1,11 @@
 val foo: List<adad<List<dd>>> = null!!
 /*
 psi: List<adad<List<dd>>>
-type: List<[ERROR : adad<List<dd>>]>
+type: List<[ERROR : adad<List<dd>>]<List<[ERROR : dd]>>>
     typeParameter: <out E> defined in kotlin.List
-    typeProjection: [ERROR : adad<List<dd>>]
+    typeProjection: [ERROR : adad<List<dd>>]<List<[ERROR : dd]>>
     psi: adad<List<dd>>
-    type: [ERROR : adad<List<dd>>]
+    type: [ERROR : adad<List<dd>>]<List<[ERROR : dd]>>
         typeParameter: null
         typeProjection: List<[ERROR : dd]>
         psi: List<dd>
