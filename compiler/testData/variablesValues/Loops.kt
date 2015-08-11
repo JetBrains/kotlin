a8305c718b177c6ae51ec9083acf6607388c8433
octomarat <maratx387@gmail.com>
2015-08-11 20:30:39 +0300

    Loops support corrected and improved

--- compiler/testData/variablesValues/Loops.kt
+++ compiler/testData/variablesValues/Loops.kt
@@ -11,3 +11,43 @@ fun simpleWhile() {
     while (a < 2) {
     }
 }
+
+fun simpleDoWhile() {
+    var a = 2
+    do {
+    }
+    while (a < 4)
+}
+
+fun whileWithUpdate() {
+    var a = 2
+    var b = false
+    val c: Int
+    var d: Boolean
+    while (a < 10) {
+        a = a + 1
+        b = !b
+        c = 5
+        d = true
+        val e = 8
+        var f = false
+    }
+    42
+}
+
+fun forWithUpdate() {
+    var a = 2
+    var b = false
+    val c: Int
+    var d: Boolean
+    for (i in 1..10) {
+        a = a + 1
+        b = !b
+        c = 5
+        d = true
+        var j = i
+        val e = 8
+        var f = false
+    }
+    42
+}
