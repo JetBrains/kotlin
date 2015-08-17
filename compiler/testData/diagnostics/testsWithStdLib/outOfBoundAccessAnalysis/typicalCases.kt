9ba0972bea9ef557c9f1ee5aea3ebe2a300aed3f
octomarat <maratx387@gmail.com>
2015-08-17 19:33:09 +0300

    Tests for some typical cases added

--- compiler/testData/diagnostics/testsWithStdLib/outOfBoundAccessAnalysis/typicalCases.kt
+++ compiler/testData/diagnostics/testsWithStdLib/outOfBoundAccessAnalysis/typicalCases.kt
@@ -13,3 +13,42 @@ fun append(): IntArray {
     }
     return res
 }
+
+fun setLast() {
+    val arr = Array(3, { 1 })
+    arr[arr.size() - 1] = 0                           // correct
+    <!OUT_OF_BOUND_ACCESS!>arr[arr.size()] = 0<!>     // `-1` is omitted
+}
+
+fun setLast2(cond: Boolean) {
+    var size = 3
+    if (cond) { size = 0 }
+    val arr = Array(size, { it })
+    <!OUT_OF_BOUND_ACCESS!>arr[arr.size() - 1] = 0<!>         // arr.size() can return 0
+}
+
+fun listAppend() {
+    val arr1 = arrayListOf(1, 2, 3, 4)
+    val arr2 = Array(3, { it })
+    for (i in 0.. arr2.size() - 1) {
+        arr1.add(arr2[i])
+    }
+    arr1.get(500)                       // no info about `arr1` size, so no warning here
+}
+
+fun listAppend2(resetNeeded: Boolean, notTwo: Boolean, appendLineEnd: Boolean) {
+    val arr = arrayListOf('!', '!')
+    if (resetNeeded) {
+        arr.clear()
+    }
+    if (notTwo) {
+        arr.add('!')
+    }
+    if (appendLineEnd) {
+        arr.add('\n')
+    }
+    println(arr)
+    <!OUT_OF_BOUND_ACCESS!>arr[0] = '?'<!>
+    <!OUT_OF_BOUND_ACCESS!>arr[1] = '?'<!>            // `arr` size can be 1
+    println(arr)
+}
