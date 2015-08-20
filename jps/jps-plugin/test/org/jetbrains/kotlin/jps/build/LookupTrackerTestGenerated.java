4032df49605656b9dee553d6ed79f016bd6a1b56
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2015-08-20 16:22:26 +0300

    Add test for lookups to classifier members
    Original commit: 3b21d391c5c01c2b753bd777e5cc6e9d5af7137a

--- jps/jps-plugin/test/org/jetbrains/kotlin/jps/build/LookupTrackerTestGenerated.java
+++ jps/jps-plugin/test/org/jetbrains/kotlin/jps/build/LookupTrackerTestGenerated.java
@@ -35,6 +35,12 @@ public class LookupTrackerTestGenerated extends AbstractLookupTrackerTest {
         JetTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("jps-plugin/testData/incremental/lookupTracker"), Pattern.compile("^([^\\.]+)$"), true);
     }
 
+    @TestMetadata("classifierMembers")
+    public void testClassifierMembers() throws Exception {
+        String fileName = JetTestUtils.navigationMetadata("jps-plugin/testData/incremental/lookupTracker/classifierMembers/");
+        doTest(fileName);
+    }
+
     @TestMetadata("packageDeclarations")
     public void testPackageDeclarations() throws Exception {
         String fileName = JetTestUtils.navigationMetadata("jps-plugin/testData/incremental/lookupTracker/packageDeclarations/");
