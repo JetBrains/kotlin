be2324031d4a7ca63512dbe6b7b8b49473795da2
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-09-01 02:06:30 +0300

    decompiled text: add test for inherited interface

---
+++ idea/testData/decompiler/decompiledText/Inherited.expected.kt
@@ -0,0 +1,10 @@
+// IntelliJ API Decompiler stub source generated from a class file
+// Implementation of methods is not available
+
+package test
+
+internal interface Inherited : dependency.Base {
+    public companion object {
+        internal final val INT_CONST: kotlin.Int /* compiled code */
+    }
+}
---
+++ idea/testData/decompiler/decompiledText/Inherited/Inherited.kt
@@ -0,0 +1,9 @@
+package test
+
+import dependency.*
+
+interface Inherited : Base {
+    companion object {
+        val INT_CONST = 1
+    }
+}
