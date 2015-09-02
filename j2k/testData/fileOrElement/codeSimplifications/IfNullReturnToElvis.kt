712df5682caf0f5fec4f58010750c569923073b2
Valentin Kipyatkov <valentin@jetbrains.com>
2015-09-02 20:10:17 +0300

    Fixed test data after stdlib change

--- j2k/testData/fileOrElement/codeSimplifications/IfNullReturnToElvis.kt
+++ j2k/testData/fileOrElement/codeSimplifications/IfNullReturnToElvis.kt
@@ -3,6 +3,6 @@ import java.io.File
 class C {
     fun foo(file: File): String {
         val parent = file.parentFile ?: return ""
-        return parent.getName()
+        return parent.name
     }
 }
