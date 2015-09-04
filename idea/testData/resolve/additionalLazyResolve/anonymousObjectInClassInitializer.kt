2684b7abb0604005fc8a6e17542bed6792ef076a
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-09-04 19:54:28 +0300

    fix tests in org.jetbrains.kotlin.idea.resolve

--- idea/testData/resolve/additionalLazyResolve/anonymousObjectInClassInitializer.kt
+++ idea/testData/resolve/additionalLazyResolve/anonymousObjectInClassInitializer.kt
@@ -11,10 +11,10 @@ class MyClass() {
 }
 
 //package test
-//internal open class A defined in test
+//public open class A defined in test
 //public constructor A() defined in test.A
-//internal final class MyClass defined in test
+//public final class MyClass defined in test
 //public constructor MyClass() defined in test.MyClass
 //val a: test.MyClass.<init>.<no name provided> defined in test.MyClass.<init>
 //local final class <no name provided> : test.A defined in test.MyClass.<init>
-//internal constructor <no name provided>() defined in test.MyClass.<init>.<no name provided>
+//public constructor <no name provided>() defined in test.MyClass.<init>.<no name provided>
