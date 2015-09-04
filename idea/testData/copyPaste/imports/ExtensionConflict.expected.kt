68c6c7790a9471c9210ab04d5c651619fcb54a08
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-09-04 20:12:14 +0300

    fix tests in org.jetbrains.kotlin.idea.codeInsight

--- idea/testData/copyPaste/imports/ExtensionConflict.expected.kt
+++ idea/testData/copyPaste/imports/ExtensionConflict.expected.kt
@@ -1,8 +1,8 @@
-// ERROR: Overload resolution ambiguity:  internal fun a.A.ext(): kotlin.Unit defined in a internal fun a.A.ext(): kotlin.Unit defined in to
-// ERROR: Overload resolution ambiguity:  internal fun a.A.plus(a: a.A): kotlin.Unit defined in a internal fun a.A.plus(a: a.A): kotlin.Unit defined in to
-// ERROR: Overload resolution ambiguity:  internal fun a.A.infix(a: a.A): kotlin.Unit defined in a internal fun a.A.infix(a: a.A): kotlin.Unit defined in to
-// ERROR: Overload resolution ambiguity:  internal fun a.A.minus(): kotlin.Unit defined in a internal fun a.A.minus(): kotlin.Unit defined in to
-// ERROR: Overload resolution ambiguity:  internal val a.A.p: kotlin.Int defined in a internal val a.A.p: kotlin.Int defined in to
+// ERROR: Overload resolution ambiguity:  public fun a.A.ext(): kotlin.Unit defined in a public fun a.A.ext(): kotlin.Unit defined in to
+// ERROR: Overload resolution ambiguity:  public fun a.A.plus(a: a.A): kotlin.Unit defined in a public fun a.A.plus(a: a.A): kotlin.Unit defined in to
+// ERROR: Overload resolution ambiguity:  public fun a.A.infix(a: a.A): kotlin.Unit defined in a public fun a.A.infix(a: a.A): kotlin.Unit defined in to
+// ERROR: Overload resolution ambiguity:  public fun a.A.minus(): kotlin.Unit defined in a public fun a.A.minus(): kotlin.Unit defined in to
+// ERROR: Overload resolution ambiguity:  public val a.A.p: kotlin.Int defined in a public val a.A.p: kotlin.Int defined in to
 package to
 
 import a.A
