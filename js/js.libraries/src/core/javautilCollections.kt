fb149790515e5cd9e000277cfeab788e8bea15aa
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2015-09-04 22:38:08 +0300

    Created object java.util.Collections as workaround for js-stdlib.

--- js/js.libraries/src/core/javautilCollections.kt
+++ js/js.libraries/src/core/javautilCollections.kt
@@ -1,13 +1,31 @@
-package java.util.Collections
+package java.util
 
 import java.lang.*
 import java.util.*
 
+public object Collections {
+    public fun max<T>(col: Collection<T>, comp: Comparator<in T>): T = java.util.max(col, comp)
+
+    public fun <T> sort(list: MutableList<T>): Unit = java.util.sort(list)
+
+    public fun <T> sort(list: MutableList<T>, comparator: java.util.Comparator<in T>): Unit = java.util.sort(list, comparator)
+
+    public fun <T> reverse(list: MutableList<T>): Unit {
+        val size = list.size()
+        for (i in 0..(size / 2) - 1) {
+            val i2 = size - i - 1
+            val tmp = list[i]
+            list[i] = list[i2]
+            list[i2] = tmp
+        }
+    }
+}
+
 @library("collectionsMax")
-public fun max<T>(col : Collection<T>, comp : Comparator<in T>) : T = noImpl
+internal fun max<T>(col: Collection<T>, comp: Comparator<in T>): T = noImpl
 
 @library("collectionsSort")
-public fun <T> sort(list: MutableList<T>): Unit = noImpl
+internal fun <T> sort(list: MutableList<T>): Unit = noImpl
 
 @library("collectionsSort")
-public fun <T> sort(list: MutableList<T>, comparator: java.util.Comparator<in T>): Unit = noImpl
+internal fun <T> sort(list: MutableList<T>, comparator: java.util.Comparator<in T>): Unit = noImpl
