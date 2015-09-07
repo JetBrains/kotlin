e9901e7351b00fe1917b8d2926107df1e34c163b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-09-07 18:57:26 +0300

    Deprecate assert with non-lazy message. #KT-8253

--- libraries/stdlib/src/kotlin/util/AssertionsJVM.kt
+++ libraries/stdlib/src/kotlin/util/AssertionsJVM.kt
@@ -7,9 +7,18 @@ deprecated("Must be public to make assert() inlinable")
 public val ASSERTIONS_ENABLED: Boolean = _Assertions.javaClass.desiredAssertionStatus()
 
 /**
+ * Throws an [AssertionError] if the [value] is false
+ * and runtime assertions have been enabled on the JVM using the *-ea* JVM option.
+ */
+public fun assert(value: Boolean) {
+    assert(value) { "Assertion failed" }
+}
+
+/**
 * Throws an [AssertionError] with an optional [message] if the [value] is false
 * and runtime assertions have been enabled on the JVM using the *-ea* JVM option.
 */
+@deprecated("Use assert with lazy message instead.", ReplaceWith("assert(value) { message }"))
 public fun assert(value: Boolean, message: Any = "Assertion failed") {
     if (ASSERTIONS_ENABLED) {
         if (!value) {
