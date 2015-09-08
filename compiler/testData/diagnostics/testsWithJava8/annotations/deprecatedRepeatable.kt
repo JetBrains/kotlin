dbaf01feedc7380583b78f92c33fe667056b0cd5
Denis Zharkov <denis.zharkov@jetbrains.com>
2015-09-08 18:56:21 +0300

    Minor. Add '@' symbol in testData where necessary (fix Java 8 build)

--- compiler/testData/diagnostics/testsWithJava8/annotations/deprecatedRepeatable.kt
+++ compiler/testData/diagnostics/testsWithJava8/annotations/deprecatedRepeatable.kt
@@ -2,7 +2,7 @@ import java.lang.annotation.Repeatable
 
 <!DEPRECATED_JAVA_ANNOTATION!>@java.lang.annotation.Repeatable(Annotations::class)<!> annotation class RepAnn
 
-<!DEPRECATED_JAVA_ANNOTATION!>Repeatable(OtherAnnotations::class)<!> annotation class OtherAnn
+<!DEPRECATED_JAVA_ANNOTATION!>@Repeatable(OtherAnnotations::class)<!> annotation class OtherAnn
 
 annotation class Annotations(vararg val value: RepAnn)
 
