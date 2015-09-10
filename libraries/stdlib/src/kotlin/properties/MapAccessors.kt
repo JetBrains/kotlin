017db3d36cfc7cc229e47f4ecfc9f3657b3dab05
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-09-10 21:11:16 +0300

    Correct reference to withDefault method in documentation.

--- libraries/stdlib/src/kotlin/properties/MapAccessors.kt
+++ libraries/stdlib/src/kotlin/properties/MapAccessors.kt
@@ -12,7 +12,7 @@ import kotlin.platform.platformName
  * @param property the metadata for the property, used to get the name of property and lookup the value corresponding to this name in the map.
  * @return the property value.
  *
- * @throws NoSuchElementException when the map doesn't contain value for the property name and doesn't provide an implicit default (see [Map.withDefault]).
+ * @throws NoSuchElementException when the map doesn't contain value for the property name and doesn't provide an implicit default (see [withDefault]).
  */
 public fun <V> Map<in String, *>.get(thisRef: Any?, property: PropertyMetadata): V = getOrImplicitDefault(property.name) as V
 
@@ -22,7 +22,7 @@ public fun <V> Map<in String, *>.get(thisRef: Any?, property: PropertyMetadata):
  * @param property the metadata for the property, used to get the name of property and lookup the value corresponding to this name in the map.
  * @return the property value.
  *
- * @throws NoSuchElementException when the map doesn't contain value for the property name and doesn't provide an implicit default (see [Map.withDefault]).
+ * @throws NoSuchElementException when the map doesn't contain value for the property name and doesn't provide an implicit default (see [withDefault]).
  */
 platformName("getVar")
 public fun <V> MutableMap<in String, in V>.get(thisRef: Any?, property: PropertyMetadata): V = getOrImplicitDefault(property.name) as V
