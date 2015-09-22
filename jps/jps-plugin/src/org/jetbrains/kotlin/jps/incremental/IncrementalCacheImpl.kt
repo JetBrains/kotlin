44293a581f7323c1a59ec726a3c49f20d435c05f
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2015-09-22 15:43:43 +0300

    Move CacheFormatVersion to separate file
    Original commit: 0904b4bae895a08b50bfd6402ae61b9b32f8482d

--- jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/IncrementalCacheImpl.kt
+++ jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/IncrementalCacheImpl.kt
@@ -30,12 +30,10 @@ import org.jetbrains.jps.incremental.ModuleBuildTarget
 import org.jetbrains.jps.incremental.storage.BuildDataManager
 import org.jetbrains.jps.incremental.storage.PathStringDescriptor
 import org.jetbrains.jps.incremental.storage.StorageOwner
-import org.jetbrains.kotlin.config.IncrementalCompilation
 import org.jetbrains.kotlin.jps.build.GeneratedJvmClass
 import org.jetbrains.kotlin.jps.build.KotlinBuilder
 import org.jetbrains.kotlin.jps.incremental.storage.BasicMap
 import org.jetbrains.kotlin.jps.incremental.storage.BasicStringMap
-import org.jetbrains.kotlin.load.java.JvmAbi
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames
 import org.jetbrains.kotlin.load.kotlin.ModuleMapping
 import org.jetbrains.kotlin.load.kotlin.PackageClassUtils
@@ -56,51 +54,8 @@ import java.util.*
 
 val INLINE_ANNOTATION_DESC = "Lkotlin/inline;"
 
-private val CACHE_DIRECTORY_NAME = "kotlin"
+internal val CACHE_DIRECTORY_NAME = "kotlin"
 
-
-class CacheFormatVersion(targetDataRoot: File) {
-    companion object {
-        // Change this when incremental cache format changes
-        private val INCREMENTAL_CACHE_OWN_VERSION = 5
-
-        private val CACHE_FORMAT_VERSION =
-                INCREMENTAL_CACHE_OWN_VERSION * 1000000 +
-                JvmAbi.VERSION.major * 1000 +
-                JvmAbi.VERSION.minor
-
-        private val NON_INCREMENTAL_MODE_PSEUDO_VERSION = Int.MAX_VALUE
-
-        val FORMAT_VERSION_FILE_PATH: String = "$CACHE_DIRECTORY_NAME/format-version.txt"
-    }
-
-    private val file = File(targetDataRoot, FORMAT_VERSION_FILE_PATH)
-
-    private fun actualCacheFormatVersion() = if (IncrementalCompilation.ENABLED) CACHE_FORMAT_VERSION else NON_INCREMENTAL_MODE_PSEUDO_VERSION
-
-    public fun isIncompatible(): Boolean {
-        if (!file.exists()) return false
-
-        val versionNumber = file.readText().toInt()
-        val expectedVersionNumber = actualCacheFormatVersion()
-
-        if (versionNumber != expectedVersionNumber) {
-            KotlinBuilder.LOG.info("Incompatible incremental cache version, expected $expectedVersionNumber, actual $versionNumber")
-            return true
-        }
-        return false
-    }
-
-    fun saveIfNeeded() {
-        if (file.parentFile.exists() && !file.exists()) {
-            file.writeText(actualCacheFormatVersion().toString())
-        }
-    }
-
-    fun clean() {
-        file.delete()
-    }
-}
 
 public class IncrementalCacheImpl(
         targetDataRoot: File,
