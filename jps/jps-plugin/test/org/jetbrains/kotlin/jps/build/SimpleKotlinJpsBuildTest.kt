2da6169a4e7553357ac8cd7e4ac266072895fa32
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2015-09-23 12:55:18 +0200

    Utils for checking presense of certain lines in a sequence, using this tool for checking log after daemon builds in tests, fixing explicit daemon shutdown used in the test, some minor fixes
    Original commit: c294a682dee86abbdbe80adf68891a84edf62ed0

--- jps/jps-plugin/test/org/jetbrains/kotlin/jps/build/SimpleKotlinJpsBuildTest.kt
+++ jps/jps-plugin/test/org/jetbrains/kotlin/jps/build/SimpleKotlinJpsBuildTest.kt
@@ -20,6 +20,7 @@ import com.intellij.util.PathUtil
 import org.jetbrains.jps.model.java.JpsJavaExtensionService
 import org.jetbrains.kotlin.rmi.COMPILE_DAEMON_CLIENT_ALIVE_PATH_PROPERTY
 import org.jetbrains.kotlin.rmi.COMPILE_DAEMON_ENABLED_PROPERTY
+import org.jetbrains.kotlin.rmi.COMPILE_DAEMON_LOG_PATH_PROPERTY
 import org.jetbrains.kotlin.rmi.COMPILE_DAEMON_VERBOSE_REPORT_PROPERTY
 import org.jetbrains.kotlin.test.JetTestUtils
 import java.io.File
@@ -71,12 +72,15 @@ public class SimpleKotlinJpsBuildTest : AbstractKotlinJpsBuildTestCase() {
         System.setProperty(COMPILE_DAEMON_VERBOSE_REPORT_PROPERTY, "")
         // spaces in the name to test proper file name handling
         val flagFile = File.createTempFile("kotlin-jps - tests-", "-is-running");
+        val logFile = File.createTempFile("kotlin-daemon", ".log")
+        System.setProperty(COMPILE_DAEMON_CLIENT_ALIVE_PATH_PROPERTY, flagFile.absolutePath)
+        System.setProperty(COMPILE_DAEMON_LOG_PATH_PROPERTY, logFile.absolutePath)
         try {
-            System.setProperty(COMPILE_DAEMON_CLIENT_ALIVE_PATH_PROPERTY, flagFile.absolutePath)
             testLoadingKotlinFromDifferentModules()
         }
         finally {
             flagFile.delete()
+            System.clearProperty(COMPILE_DAEMON_LOG_PATH_PROPERTY)
             System.clearProperty(COMPILE_DAEMON_CLIENT_ALIVE_PATH_PROPERTY)
             System.clearProperty(COMPILE_DAEMON_VERBOSE_REPORT_PROPERTY)
             System.clearProperty(COMPILE_DAEMON_ENABLED_PROPERTY)
