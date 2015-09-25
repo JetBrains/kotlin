4b9d136cedb27cf53462c08d0a7efa0c926adfd7
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-09-25 19:44:04 +0300

    identityEquals is deprecated

--- compiler/testData/diagnostics/tests/thisAndSuper/superInExtensionFunctionCall.kt
+++ compiler/testData/diagnostics/tests/thisAndSuper/superInExtensionFunctionCall.kt
@@ -1,6 +1,6 @@
 // No supertype at all
 class A1 {
     fun test() {
-        <!SUPER_CANT_BE_EXTENSION_RECEIVER!>super<!>.identityEquals(null) // Call to an extension function
+        <!SUPER_CANT_BE_EXTENSION_RECEIVER!>super<!>.<!DEPRECATED_SYMBOL_WITH_MESSAGE!>identityEquals<!>(null) // Call to an extension function
     }
 }
