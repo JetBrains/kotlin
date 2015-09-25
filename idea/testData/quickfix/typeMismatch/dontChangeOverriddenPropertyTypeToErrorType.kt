48bcc93ec29f903590bf603415827a06513c6895
Dmitry Jemerov <yole@jetbrains.com>
2015-09-25 19:14:01 +0200

    revert incorrect testdata changes

--- idea/testData/quickfix/typeMismatch/dontChangeOverriddenPropertyTypeToErrorType.kt
+++ idea/testData/quickfix/typeMismatch/dontChangeOverriddenPropertyTypeToErrorType.kt
@@ -1,6 +1,5 @@
 // "Change 'B.x' type to '(String) -> [ERROR : Ay]'" "false"
 // ACTION: Change 'A.x' type to '(Int) -> Int'
-// ACTION: Introduce backing propertty
 // ERROR: <html>Return type is '(kotlin.Int) &rarr; kotlin.Int', which is not a subtype of overridden<br/><b>public</b> <b>abstract</b> <b>val</b> x: (kotlin.String) &rarr; [ERROR : Ay] <i>defined in</i> A</html>
 // ERROR: Unresolved reference: Ay
 interface A {
