a8ae730c7fab28dc8b4af3ee7103eb243ed1802f
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-09-28 01:56:03 +0300

    No method with icon create from vararg

--- idea/idea-completion/src/org/jetbrains/kotlin/idea/completion/OverridesCompletion.kt
+++ idea/idea-completion/src/org/jetbrains/kotlin/idea/completion/OverridesCompletion.kt
@@ -80,7 +80,9 @@ class OverridesCompletion(
                 AllIcons.Gutter.ImplementingMethod
             else
                 AllIcons.Gutter.OverridingMethod
-            val icon = RowIcon(baseIcon, additionalIcon)
+            val icon = RowIcon(2)
+            icon.setIcon(baseIcon, 0)
+            icon.setIcon(additionalIcon, 1)
 
             val baseClassDeclaration = DescriptorToSourceUtilsIde.getAnyDeclaration(position.project, baseClass)
             val baseClassIcon = JetDescriptorIconProvider.getIcon(baseClass, baseClassDeclaration, 0)
