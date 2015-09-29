423a146bcec794cccbcec725a651d661875d4c79
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-09-29 20:36:37 +0300

    Preprocessor: do not use legacy package facade name in build.xml.

--- compiler/conditional-preprocessor/src/org.jetbrains.kotlin.preprocessor/PreprocessorCLI.kt
+++ compiler/conditional-preprocessor/src/org.jetbrains.kotlin.preprocessor/PreprocessorCLI.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+@file:JvmName("PreprocessorCLI")
 package org.jetbrains.kotlin.preprocessor
 
 import java.io.File
