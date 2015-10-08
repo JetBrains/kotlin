a3e0205aec892dff6803b2eea6e3d74d94e1f392
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2015-10-08 15:47:01 +0300

    JvmAbi increased

--- core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/JvmAbi.java
+++ core/descriptor.loader.java/src/org/jetbrains/kotlin/load/java/JvmAbi.java
@@ -34,7 +34,7 @@ public final class JvmAbi {
      * - Patch version can be increased freely and is only supposed to be used for debugging. Increase the patch version when you
      *   make a change to the metadata format or the bytecode which is both forward- and backward compatible.
      */
-    public static final BinaryVersion VERSION = BinaryVersion.create(0, 28, 0);
+    public static final BinaryVersion VERSION = BinaryVersion.create(0, 29, 0);
 
     public static final String DEFAULT_IMPLS_CLASS_NAME = "DefaultImpls";
     public static final String DEFAULT_IMPLS_SUFFIX = "$" + DEFAULT_IMPLS_CLASS_NAME;
