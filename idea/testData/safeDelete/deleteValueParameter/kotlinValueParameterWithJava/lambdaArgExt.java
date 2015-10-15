12d5aca9d64e109b4424f2508f2ef3cc99d43d60
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-10-15 22:31:21 +0300

    Fix  test data for safe delete test (cherry picked from commit 25f363d)

--- idea/testData/safeDelete/deleteValueParameter/kotlinValueParameterWithJava/lambdaArgExt.java
+++ idea/testData/safeDelete/deleteValueParameter/kotlinValueParameterWithJava/lambdaArgExt.java
@@ -2,7 +2,7 @@ package test;
 
 class A {
     void bar() {
-        TestPackage.foo(
+        LambdaArgExtKt.foo(
                 "", 10, new Function1<Integer, Unit>() {
                     public Unit invoke(Integer n) {
                         System.out.println(n);
