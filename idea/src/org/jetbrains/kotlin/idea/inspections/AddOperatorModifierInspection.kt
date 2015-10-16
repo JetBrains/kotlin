d1de2f65c93a77e11ae9f852360c7f2633a258f6
Valentin Kipyatkov <valentin@jetbrains.com>
2015-10-16 23:11:05 +0300

    Rewritten AddOperatorModifierInspection to be based on an intention (shouldn't we drop the inspection at all?)

---
+++ idea/src/org/jetbrains/kotlin/idea/inspections/AddOperatorModifierInspection.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.idea.inspections
+
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptor
+import org.jetbrains.kotlin.idea.intentions.JetSelfTargetingRangeIntention
+import org.jetbrains.kotlin.lexer.JetTokens
+import org.jetbrains.kotlin.psi.JetNamedFunction
+import org.jetbrains.kotlin.util.OperatorChecks
+
+//TODO: do we really need it?
+public class AddOperatorModifierInspection : IntentionBasedInspection<JetNamedFunction>(
+        listOf(IntentionBasedInspection.IntentionData(AddOperatorModifierIntention())),
+        "Function defines an operator but isn't annotated as such",
+        JetNamedFunction::class.java)
+
+class AddOperatorModifierIntention : JetSelfTargetingRangeIntention<JetNamedFunction>(JetNamedFunction::class.java, "Add 'operator' modifier") {
+    override fun applicabilityRange(element: JetNamedFunction): TextRange? {
+        val nameIdentifier = element.nameIdentifier ?: return null
+        val functionDescriptor = element.resolveToDescriptor() as? FunctionDescriptor ?: return null
+        if (functionDescriptor.isOperator || !OperatorChecks.canBeOperator(functionDescriptor)) return null
+        return nameIdentifier.textRange
+    }
+
+    override fun applyTo(element: JetNamedFunction, editor: Editor) {
+        element.addModifier(JetTokens.OPERATOR_KEYWORD)
+    }
+}
+
