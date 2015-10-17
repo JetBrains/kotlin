10784644a5e246244fb8e422079e5763980fc5de
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-10-17 12:57:35 +0300

    Throwable stackTrace property instead of getStackTrace() function.

--- libraries/stdlib/src/kotlin/util/StandardJVM.kt
+++ libraries/stdlib/src/kotlin/util/StandardJVM.kt
@@ -1,5 +1,6 @@
 @file:kotlin.jvm.JvmMultifileClass
 @file:kotlin.jvm.JvmName("ExceptionsKt")
+@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 package kotlin
 
 import java.io.PrintStream
@@ -24,8 +25,13 @@ public fun Throwable.printStackTrace(stream: PrintStream): Unit {
 /**
  * Returns the list of stack trace elements in a Kotlin stack trace.
  */
-public fun Throwable.getStackTrace(): Array<StackTraceElement> {
-    val jlt = this as java.lang.Throwable
-    return jlt.getStackTrace()!!
-}
+@Deprecated("Use 'stackTrace' property instead.", ReplaceWith("stackTrace"))
+@JvmName("getStackTraceDeprecated")
+public inline fun Throwable.getStackTrace(): Array<StackTraceElement> = stackTrace
 
+/**
+ * Returns an array of stack trace elements representing the stack trace
+ * pertaining to this throwable.
+ */
+public val Throwable.stackTrace: Array<StackTraceElement>
+    get() = (this as java.lang.Throwable).stackTrace!!
