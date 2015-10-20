7150be7c675955055a85ae129a732787b1ae8607
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2015-10-20 15:30:25 +0300

    Minor: fixed error message

--- idea/testData/quickfix/createFromUsage/createClass/callExpression/typeArguments/classMemberInner.kt
+++ idea/testData/quickfix/createFromUsage/createClass/callExpression/typeArguments/classMemberInner.kt
@@ -1,6 +1,6 @@
 // "Create class 'Foo'" "true"
 // ERROR: <html>Type mismatch.<table><tr><td>Required:</td><td>V</td></tr><tr><td>Found:</td><td>kotlin.String</td></tr></table></html>
-// ERROR: An integer literal does not conform to the expected type U
+// ERROR: The integer literal does not conform to the expected type U
 
 class B<T>(val t: T) {
 
