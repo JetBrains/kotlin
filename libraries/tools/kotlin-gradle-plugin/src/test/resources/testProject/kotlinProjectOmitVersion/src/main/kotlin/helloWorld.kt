3c1e589b61b9250c4f68789d1f32b304c0dfd221
Danny Thomas <dannyt@netflix.com>
2015-10-27 14:19:59 -0700

    Add resolution strategy to allow the Kotlin version to be omitted for the runtime and provided by the plugin automatically

---
+++ libraries/tools/kotlin-gradle-plugin/src/test/resources/testProject/kotlinProjectOmitVersion/src/main/kotlin/helloWorld.kt
@@ -0,0 +1,22 @@
+package demo
+
+import com.google.common.primitives.Ints
+import com.google.common.base.Joiner
+import java.util.ArrayList
+
+class KotlinGreetingJoiner(val greeter : Greeter) {
+
+    val names = ArrayList<String?>()
+
+    fun addName(name : String?): Unit{
+        names.add(name)
+    }
+
+    fun getJoinedGreeting() : String? {
+        val joiner = Joiner.on(" and ").skipNulls();
+        return "${greeter.greeting} ${joiner.join(names)}"
+    }
+}
+
+public open class Greeter(val greeting : String)
+
