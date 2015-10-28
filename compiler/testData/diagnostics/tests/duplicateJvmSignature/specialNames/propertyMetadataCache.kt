894a97a5490af06d41cabba3a355a996d844d4b3
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-10-28 18:24:19 +0300

    Rename field for property metadata cache to prevent problems in inline

--- compiler/testData/diagnostics/tests/duplicateJvmSignature/specialNames/propertyMetadataCache.kt
+++ compiler/testData/diagnostics/tests/duplicateJvmSignature/specialNames/propertyMetadataCache.kt
@@ -4,8 +4,8 @@ operator fun Any.getValue(x: Any?, y: Any): Any = null!!
 
 class <!CONFLICTING_JVM_DECLARATIONS!>C<!> {
     val x by 1
-    <!CONFLICTING_JVM_DECLARATIONS!>val `$delegatedProperties`: Array<PropertyMetadata><!> = null!!
+    <!CONFLICTING_JVM_DECLARATIONS!>val `$$delegatedProperties`: Array<PropertyMetadata><!> = null!!
 }
 
 val x by 1
-<!CONFLICTING_JVM_DECLARATIONS!>val `$delegatedProperties`: Array<PropertyMetadata><!> = null!!
+<!CONFLICTING_JVM_DECLARATIONS!>val `$$delegatedProperties`: Array<PropertyMetadata><!> = null!!
