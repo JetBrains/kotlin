8f2c05668e58167bc1a02d080d414f6ae8d1aa32
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-11-09 21:58:27 +0300

    Make MutableMap.set return Unit #KT-7853 Fixed

--- libraries/stdlib/src/kotlin/collections/MapsJVM.kt
+++ libraries/stdlib/src/kotlin/collections/MapsJVM.kt
@@ -14,7 +14,13 @@ import java.util.concurrent.ConcurrentMap
  * Allows to use the index operator for storing values in a mutable map.
  */
 // this code is JVM-specific, because JS has native set function
-public operator fun <K, V> MutableMap<K, V>.set(key: K, value: V): V? = put(key, value)
+public operator fun <K, V> MutableMap<K, V>.set(key: K, value: V): Unit {
+    put(key, value)
+}
+
+@Deprecated("Provided for binary compatibility", level = DeprecationLevel.HIDDEN)
+@JvmName("set")
+public fun <K, V> MutableMap<K, V>.set(key: K, value: V): V? = put(key, value)
 
 /**
  * getOrPut is not supported on [ConcurrentMap] since it cannot be implemented correctly in terms of concurrency.
