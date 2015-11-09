737763cc67699677f30699e43af86058022fe6ab
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-11-09 20:07:15 +0300

    Restore correct sorting order (after cleanup in 17fc1d95)

--- jps-plugin/test/org/jetbrains/kotlin/jps/build/classFilesComparison/classFilesComparison.kt
+++ jps-plugin/test/org/jetbrains/kotlin/jps/build/classFilesComparison/classFilesComparison.kt
@@ -53,7 +53,7 @@ fun getDirectoryString(dir: File, interestingPaths: List<String>): String {
         val listFiles = dir.listFiles()
         assertNotNull(listFiles)
 
-        val children = listFiles!!.sortedWith(compareBy ({ it.directory }, { it.name } ))
+        val children = listFiles!!.sortedWith(compareBy ({ it.isDirectory }, { it.name } ))
         for (child in children) {
             if (child.isDirectory()) {
                 p.println(child.name)
