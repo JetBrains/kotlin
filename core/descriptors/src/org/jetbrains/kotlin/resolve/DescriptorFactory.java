dc60c62781727b6c6d82d010c5334b586d1c5ee4
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-11-23 13:22:57 +0300

    Enum.values is now deprecated but Enum.values() is no more deprecated

--- core/descriptors/src/org/jetbrains/kotlin/resolve/DescriptorFactory.java
+++ core/descriptors/src/org/jetbrains/kotlin/resolve/DescriptorFactory.java
@@ -121,12 +121,8 @@ public class DescriptorFactory {
 
     @NotNull
     public static SimpleFunctionDescriptor createEnumValuesMethod(@NotNull ClassDescriptor enumClass) {
-        AnnotationsImpl annotations = AnnotationsImpl.createWithNoTarget(
-                AnnotationUtilKt.createDeprecatedAnnotation(getBuiltIns(enumClass), "Use 'values' property instead", "this.values")
-        );
-
         SimpleFunctionDescriptorImpl values =
-                SimpleFunctionDescriptorImpl.create(enumClass, annotations, DescriptorUtils.ENUM_VALUES,
+                SimpleFunctionDescriptorImpl.create(enumClass, Annotations.Companion.getEMPTY(), DescriptorUtils.ENUM_VALUES,
                                                     CallableMemberDescriptor.Kind.SYNTHESIZED, enumClass.getSource());
         return values.initialize(null, null, Collections.<TypeParameterDescriptor>emptyList(),
                                  Collections.<ValueParameterDescriptor>emptyList(),
@@ -136,9 +132,13 @@ public class DescriptorFactory {
 
     @NotNull
     public static PropertyDescriptor createEnumValuesProperty(@NotNull ClassDescriptor enumClass) {
+        AnnotationsImpl annotations = AnnotationsImpl.createWithNoTarget(
+                AnnotationUtilKt.createDeprecatedAnnotation(getBuiltIns(enumClass), "Use 'values()' function instead", "this.values()")
+        );
+
         PropertyDescriptorImpl values =
                 PropertyDescriptorImpl.create(
-                        enumClass, Annotations.Companion.getEMPTY(), Modality.FINAL, Visibilities.PUBLIC, /* isVar */ false,
+                        enumClass, annotations, Modality.FINAL, Visibilities.PUBLIC, /* isVar */ false,
                         DescriptorUtils.ENUM_VALUES, CallableMemberDescriptor.Kind.SYNTHESIZED, enumClass.getSource(),
                         /* lateInit = */ false, /* isConst = */ false
                 );
