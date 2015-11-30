14ca0f4aace6de322095e48df10777964de25bd1
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2015-11-30 20:57:06 +0300

    KT-2917 maven archetype for kotlin projects

---
+++ libraries/tools/maven-archetypes/kotlin-archetype-js/src/main/resources/archetype-resources/src/main/kotlin/Hello.kt
@@ -0,0 +1,13 @@
+package ${package}
+
+import kotlin.browser.window
+
+/**
+ * Example main function. Started at script body.
+ * At first time you have to run `mvn package`. 
+ * Open example index.html in browser once you compile it.
+ */
+fun main(args: Array<String>) {
+    window.alert("Hello, World!")
+}
+
---
+++ libraries/tools/maven-archetypes/kotlin-archetype-jvm/src/main/resources/archetype-resources/src/test/kotlin/HelloTest.kt
@@ -0,0 +1,8 @@
+package ${package}
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
