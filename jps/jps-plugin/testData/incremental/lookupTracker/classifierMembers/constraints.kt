bbd5932a23ac4e5ec550312a4a69a11d8b026fc9
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2015-11-30 22:50:20 +0300

    Report error when type parameter has a type parameter bound and any other bound
    To prevent issues with erasure on JVM: it's unclear what such type parameter
    should be erased to
    
    Original commit: d3c17ec337fe35c9c747296bee651dc114a5a587

--- jps/jps-plugin/testData/incremental/lookupTracker/classifierMembers/constraints.kt
+++ jps/jps-plugin/testData/incremental/lookupTracker/classifierMembers/constraints.kt
@@ -2,6 +2,6 @@ package foo
 
 import bar.*
 
-/*p:foo*/fun <T : /*p:foo*/A?, B : /*p:foo*/Iterable</*p:foo*/Number>, C> test()
-        where C : /*p:foo*/Number, C : /*p:foo*/Comparable</*p:foo*/Number>, C : B
+/*p:foo*/fun <T : /*p:foo*/A?, B : /*p:foo*/Iterable</*p:foo*/Number>, C, D> test()
+        where C : /*p:foo*/Number, C : /*p:foo*/Comparable</*p:foo*/Number>, D : B
 {}
