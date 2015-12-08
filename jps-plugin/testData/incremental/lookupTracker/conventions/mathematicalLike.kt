79b30dddf9f66d9130859ba5a7e3faeb5c90cba4
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2015-12-08 18:59:05 +0300

    Update lookupTracker test data for tower resolution algorithm

--- jps-plugin/testData/incremental/lookupTracker/conventions/mathematicalLike.kt
+++ jps-plugin/testData/incremental/lookupTracker/conventions/mathematicalLike.kt
@@ -5,20 +5,20 @@ package foo.bar
 
     d/*c:foo.bar.A(inc)*/++
     /*c:foo.bar.A(inc)*/++d
-    d/*c:foo.bar.A(dec) p:foo.bar(dec) p:java.lang(dec) p:kotlin(dec) p:kotlin.annotation(dec) p:kotlin.jvm(dec) p:kotlin.io(dec)*/--
-    /*c:foo.bar.A(dec) p:foo.bar(dec) p:java.lang(dec) p:kotlin(dec) p:kotlin.annotation(dec) p:kotlin.jvm(dec) p:kotlin.io(dec)*/--d
+    d/*c:foo.bar.A(dec) p:foo.bar(dec)*/--
+    /*c:foo.bar.A(dec) p:foo.bar(dec)*/--d
 
     a /*c:foo.bar.A(plus)*/+ b
-    a /*c:foo.bar.A(minus) p:foo.bar(minus) p:java.lang(minus) p:kotlin(minus) p:kotlin.annotation(minus) p:kotlin.jvm(minus) p:kotlin.io(minus)*/- b
-    /*c:foo.bar.A(not) p:foo.bar(not) p:java.lang(not) p:kotlin(not) p:kotlin.annotation(not) p:kotlin.jvm(not) p:kotlin.io(not)*/!a
+    a /*c:foo.bar.A(minus) p:foo.bar(minus)*/- b
+    /*c:foo.bar.A(not) p:foo.bar(not)*/!a
 
     // for val
     a /*c:foo.bar.A(timesAssign)*/*= b
-    a /*c:foo.bar.A(divAssign) p:foo.bar(divAssign) p:java.lang(divAssign) p:kotlin(divAssign) p:kotlin.annotation(divAssign) p:kotlin.jvm(divAssign) p:kotlin.io(divAssign)*//= b
+    a /*c:foo.bar.A(divAssign) p:foo.bar(divAssign)*//= b
 
     // for var
     d /*c:foo.bar.A(plusAssign) p:foo.bar(plusAssign) p:java.lang(plusAssign) p:kotlin(plusAssign) p:kotlin.annotation(plusAssign) p:kotlin.jvm(plusAssign) p:kotlin.io(plusAssign) c:foo.bar.A(getPlusAssign) c:foo.bar.A(getPLUSAssign) c:foo.bar.A(plus)*/+= b
-    d /*c:foo.bar.A(minusAssign) p:foo.bar(minusAssign) p:java.lang(minusAssign) p:kotlin(minusAssign) p:kotlin.annotation(minusAssign) p:kotlin.jvm(minusAssign) p:kotlin.io(minusAssign) c:foo.bar.A(getMinusAssign) c:foo.bar.A(getMINUSAssign) c:foo.bar.A(minus) p:foo.bar(minus) p:java.lang(minus) p:kotlin(minus) p:kotlin.annotation(minus) p:kotlin.jvm(minus) p:kotlin.io(minus)*/-= b
+    d /*c:foo.bar.A(minusAssign) p:foo.bar(minusAssign) p:java.lang(minusAssign) p:kotlin(minusAssign) p:kotlin.annotation(minusAssign) p:kotlin.jvm(minusAssign) p:kotlin.io(minusAssign) c:foo.bar.A(getMinusAssign) c:foo.bar.A(getMINUSAssign) c:foo.bar.A(minus) p:foo.bar(minus)*/-= b
     d /*c:foo.bar.A(timesAssign) c:foo.bar.A(times) p:foo.bar(times) p:java.lang(times) p:kotlin(times) p:kotlin.annotation(times) p:kotlin.jvm(times) p:kotlin.io(times) c:foo.bar.A(getTimes) c:foo.bar.A(getTIMES)*/*= b
-    d /*c:foo.bar.A(divAssign) p:foo.bar(divAssign) p:java.lang(divAssign) p:kotlin(divAssign) p:kotlin.annotation(divAssign) p:kotlin.jvm(divAssign) p:kotlin.io(divAssign) c:foo.bar.A(div) p:foo.bar(div) p:java.lang(div) p:kotlin(div) p:kotlin.annotation(div) p:kotlin.jvm(div) p:kotlin.io(div) c:foo.bar.A(getDiv) c:foo.bar.A(getDIV)*//= b
+    d /*c:foo.bar.A(divAssign) p:foo.bar(divAssign) c:foo.bar.A(div) p:foo.bar(div) p:java.lang(div) p:kotlin(div) p:kotlin.annotation(div) p:kotlin.jvm(div) p:kotlin.io(div) c:foo.bar.A(getDiv) c:foo.bar.A(getDIV)*//= b
 }
--- jps-plugin/testData/incremental/lookupTracker/syntheticProperties/usages.kt
+++ jps-plugin/testData/incremental/lookupTracker/syntheticProperties/usages.kt
@@ -4,8 +4,8 @@ import /*p:<root>*/JavaClass
 import foo./*p:foo*/KotlinClass
 
 /*p:foo.bar*/fun test() {
-    val j = /*p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io*/JavaClass()
-    val k = /*p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io*/KotlinClass()
+    val j = JavaClass()
+    val k = KotlinClass()
 
     j./*c:JavaClass*/getFoo()
     j./*c:JavaClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:JavaClass(getSetFoo) c:JavaClass(getSETFoo)*/setFoo(2)
@@ -17,14 +17,14 @@ import foo./*p:foo*/KotlinClass
     j./*c:JavaClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:JavaClass(getBazBaz) c:JavaClass(getBAZBaz)*/bazBaz = ""
     j./*c:JavaClass*/setBoo(2)
     j./*c:JavaClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:JavaClass(getBoo) c:JavaClass(getBOO)*/boo = 2
-    k./*c:foo.KotlinClass*/getFoo()
-    k./*c:foo.KotlinClass*/setFoo(2)
+    k./*c:foo.KotlinClass c:JavaClass*/getFoo() // getFoo may be an inner class in JavaClass
+    k./*c:foo.KotlinClass c:JavaClass*/setFoo(2)
     k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getFoo) c:foo.KotlinClass(getFOO) c:foo.KotlinClass(setFoo)*/foo = 2
     k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getFoo) c:foo.KotlinClass(getFOO) c:foo.KotlinClass(setFoo)*/foo
     k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBar) c:foo.KotlinClass(getBAR) c:foo.KotlinClass(setBar)*/bar
     k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBar) c:foo.KotlinClass(getBAR) c:foo.KotlinClass(setBar)*/bar = ""
-    k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBazBaz) c:foo.KotlinClass(getBAZBaz)*/bazBaz
-    k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBazBaz) c:foo.KotlinClass(getBAZBaz)*/bazBaz = ""
-    k./*c:foo.KotlinClass*/setBoo(2)
-    k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBoo) c:foo.KotlinClass(getBOO)*/boo = 2
+    k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBazBaz) c:foo.KotlinClass(getBAZBaz) c:JavaClass*/bazBaz
+    k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBazBaz) c:foo.KotlinClass(getBAZBaz) c:JavaClass*/bazBaz = ""
+    k./*c:foo.KotlinClass c:JavaClass*/setBoo(2)
+    k./*c:foo.KotlinClass p:foo.bar p:java.lang p:kotlin p:kotlin.annotation p:kotlin.jvm p:kotlin.io c:foo.KotlinClass(getBoo) c:foo.KotlinClass(getBOO) c:JavaClass*/boo = 2
 }
