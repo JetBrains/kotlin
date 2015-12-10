5dfdababfda2ce384e216ec0e7ef0ece3076b35b
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-12-10 14:52:57 +0300

    Extract jps-tests to separate module
    Remove dependency to idea-full to build jps-plugin with Java 1.6

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -897,7 +897,7 @@ fun main(args: Array<String>) {
         }
     }
 
-    testGroup("jps-plugin/test", "jps-plugin/testData") {
+    testGroup("jps-plugin/jps-tests/test", "jps-plugin/testData") {
         testClass<AbstractIncrementalJpsTest>() {
             model("incremental/multiModule", extension = null, excludeParentDirs = true)
             model("incremental/pureKotlin", extension = null, excludeParentDirs = true)
@@ -917,7 +917,7 @@ fun main(args: Array<String>) {
         }
     }
 
-    testGroup("jps-plugin/test", "jps-plugin/testData") {
+    testGroup("jps-plugin/jps-tests/test", "jps-plugin/testData") {
         testClass<AbstractExperimentalIncrementalJpsTest>() {
             model("incremental/multiModule", extension = null, excludeParentDirs = true)
             model("incremental/pureKotlin", extension = null, excludeParentDirs = true)
@@ -943,7 +943,7 @@ fun main(args: Array<String>) {
         }
     }
 
-    testGroup("jps-plugin/test", "jps-plugin/testData") {
+    testGroup("jps-plugin/jps-tests/test", "jps-plugin/testData") {
         testClass<AbstractProtoComparisonTest>() {
             model("comparison/classSignatureChange", extension = null, excludeParentDirs = true)
             model("comparison/classPrivateOnlyChange", extension = null, excludeParentDirs = true)
