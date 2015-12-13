91b920f18ee6d6d5591a61968f93d7533b49fb14
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-12-13 17:28:53 +0300

    Move IndexedValue, IndexingIterable and IndexingIterator back to kotlin package

--- libraries/stdlib/src/kotlin/collections/Iterators.kt
+++ libraries/stdlib/src/kotlin/collections/Iterators.kt
@@ -26,27 +26,3 @@ public inline fun <T> Iterator<T>.forEach(operation: (T) -> Unit) : Unit {
     for (element in this) operation(element)
 }
 
-/**
- * Data class representing a value from a collection or sequence, along with its index in that collection or sequence.
- *
- * @property value the underlying value.
- * @property index the index of the value in the collection or sequence.
- */
-public data class IndexedValue<out T>(public val index: Int, public val value: T)
-
-/**
- * A wrapper over another [Iterable] (or any other object that can produce an [Iterator]) that returns
- * an indexing iterator.
- */
-public class IndexingIterable<out T>(private val iteratorFactory: () -> Iterator<T>) : Iterable<IndexedValue<T>> {
-    override fun iterator(): Iterator<IndexedValue<T>> = IndexingIterator(iteratorFactory())
-}
-
-/**
- * Iterator transforming original `iterator` into iterator of [IndexedValue], counting index from zero.
- */
-public class IndexingIterator<out T>(private val iterator: Iterator<T>) : Iterator<IndexedValue<T>> {
-    private var index = 0
-    final override fun hasNext(): Boolean = iterator.hasNext()
-    final override fun next(): IndexedValue<T> = IndexedValue(index++, iterator.next())
-}
