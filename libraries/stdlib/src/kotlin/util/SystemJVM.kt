4ffd60cf52476a6a88b3f9b01be43eff73a39526
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-12-14 21:07:33 +0300

    Correct deprecation messages.

--- libraries/stdlib/src/kotlin/util/SystemJVM.kt
+++ libraries/stdlib/src/kotlin/util/SystemJVM.kt
@@ -4,7 +4,7 @@ package kotlin.util
 /**
  * Executes the given block and returns elapsed time in milliseconds.
  */
-@Deprecated("Use measureTimeMillis from system.timing instead.", ReplaceWith("kotlin.system.measureTimeMillis(block)", "kotlin.system"))
+@Deprecated("Use measureTimeMillis from kotlin.system instead.", ReplaceWith("kotlin.system.measureTimeMillis(block)", "kotlin.system"))
 public fun measureTimeMillis(block: () -> Unit) : Long {
     val start = System.currentTimeMillis()
     block()
@@ -14,7 +14,7 @@ public fun measureTimeMillis(block: () -> Unit) : Long {
 /**
  * Executes the given block and returns elapsed time in nanoseconds.
  */
-@Deprecated("Use measureTimeNano from system.timing instead.", ReplaceWith("kotlin.system.measureTimeNano(block)", "kotlin.system"))
+@Deprecated("Use measureTimeNano from kotlin.system instead.", ReplaceWith("kotlin.system.measureTimeNano(block)", "kotlin.system"))
 public fun measureTimeNano(block: () -> Unit) : Long {
     val start = System.nanoTime()
     block()
