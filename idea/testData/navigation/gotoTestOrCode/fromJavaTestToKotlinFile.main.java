18b1790a90b60f89e9acc2d16a03e0daac94f975
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2015-12-16 18:44:30 +0300

    Junit 4.12->4.11 in br141 and modify test data for one test

--- idea/testData/navigation/gotoTestOrCode/fromJavaTestToKotlinFile.main.java
+++ idea/testData/navigation/gotoTestOrCode/fromJavaTestToKotlinFile.main.java
@@ -1,7 +1,6 @@
-// CONFIGURE_LIBRARY: JUnit@lib/junit-4.12.jar
+// CONFIGURE_LIBRARY: JUnit@lib/junit-4.11.jar
 // REF: (<root>).Foo
 // REF: /src.fromJavaTestToKotlinFile.kt
-// REF: /src.fromJavaTestToKotlinFile.2.kt
 import junit.framework.TestCase;
 
 public class <caret>FooUtilsTest extends TestCase {
