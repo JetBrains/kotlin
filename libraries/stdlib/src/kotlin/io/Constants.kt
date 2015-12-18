fbfeb98ecf17ca468c0648c8af050db035d58bb9
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2015-12-18 22:50:21 +0300

    Deprecate some top-level constants to make 'em private later.

--- libraries/stdlib/src/kotlin/io/Constants.kt
+++ libraries/stdlib/src/kotlin/io/Constants.kt
@@ -7,11 +7,14 @@ package kotlin.io
  * Returns the default buffer size when working with buffered streams.
 */
 public val defaultBufferSize: Int = 64 * 1024
+
 /**
  * Returns the default block size for forEachBlock().
  */
+@Deprecated("This constant will become private soon, use its value instead.", ReplaceWith("4096"))
 public val defaultBlockSize: Int = 4096
 /**
  * Returns the minimum block size for forEachBlock().
  */
+@Deprecated("This constant will become private soon, use its value instead.", ReplaceWith("512"))
 public val minimumBlockSize: Int = 512
