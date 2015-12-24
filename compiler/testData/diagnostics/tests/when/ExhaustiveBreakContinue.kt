8dd3f438d84c9d5f87e67dd25733ba0016faf074
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-12-24 13:23:49 +0300

    Exhaustive whens without else and 'Nothing' as the result are considered 'implicit exhaustive' (cherry picked from commit b938949)

--- compiler/testData/diagnostics/tests/when/ExhaustiveBreakContinue.kt
+++ compiler/testData/diagnostics/tests/when/ExhaustiveBreakContinue.kt
@@ -2,11 +2,11 @@ enum class Color { RED, GREEN, BLUE }
 
 fun foo(arr: Array<Color>): Color {
     loop@ for (color in arr) {
-        when (color) {
+        <!DEBUG_INFO_IMPLICIT_EXHAUSTIVE!>when (color) {
             Color.RED -> return color
             Color.GREEN -> break@loop
             Color.BLUE -> if (arr.size == 1) return color else continue@loop
-        }
+        }<!>
         // Unreachable
         <!UNREACHABLE_CODE!>return Color.BLUE<!>
     }
