011a9f23b9776584bce8102f3a6fe7d719b2c2fd
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2015-12-25 14:25:38 +0300

    Implicit exhaustive when check for definite variable initialization (KT-8700)

--- compiler/testData/diagnostics/tests/when/ExhaustiveNoInitialization.kt
+++ compiler/testData/diagnostics/tests/when/ExhaustiveNoInitialization.kt
@@ -1,10 +1,10 @@
 fun foo(b: Boolean): Int {
     val x: Int
     val y: Int
-    when (b) {
+    <!DEBUG_INFO_IMPLICIT_EXHAUSTIVE!>when (b) {
         true -> y = 1
         false -> y = 0
-    }
+    }<!>
     // x is initialized here
     x = 3
     return x + y
