85ae0cb78ee6d68f7b1c49933834babae65f6706
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2015-12-25 11:46:57 +0300

    Restrict companion properties copying to interface by const

--- compiler/testData/writeFlags/property/classObject/trait/internalVal.kt
+++ compiler/testData/writeFlags/property/classObject/trait/internalVal.kt
@@ -6,7 +6,7 @@ interface Test {
 
 // TESTED_OBJECT_KIND: property
 // TESTED_OBJECTS: Test, prop
-// FLAGS: ACC_PUBLIC, ACC_FINAL, ACC_STATIC, ACC_DEPRECATED
+// ABSENT: TRUE
 
 // TESTED_OBJECT_KIND: property
 // TESTED_OBJECTS: Test$Companion, prop
