90ec548bd50d884b4a0e44e25ba829d19b207dbc
Dmitry Jemerov <yole@jetbrains.com>
2016-01-04 19:39:39 +0100

    [KDM] cleanup: remove redundant 'public' modifiers
    Original commit: https://github.com/Kotlin/dokka/commit/3b3c2841674d9b7044494d16d4396662d273f1f9

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/Utilities/Html.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/Utilities/Html.kt
@@ -5,4 +5,4 @@ package org.jetbrains.dokka
  * Replaces symbols reserved in HTML with their respective entities.
  * Replaces & with &amp;, < with &lt; and > with &gt;
  */
-public fun String.htmlEscape(): String = replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+fun String.htmlEscape(): String = replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
