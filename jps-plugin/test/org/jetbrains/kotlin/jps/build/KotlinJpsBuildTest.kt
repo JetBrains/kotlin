39c6e3712bd8e8f8592c80ea0e885ba29fefe08e
Dmitry Jemerov <yole@jetbrains.com>
2016-01-14 14:30:23 +0100

    add test to check that "Only the Kotlin standard library is allowed to use the 'kotlin' package" is reported in JPS builds

--- jps-plugin/test/org/jetbrains/kotlin/jps/build/KotlinJpsBuildTest.kt
+++ jps-plugin/test/org/jetbrains/kotlin/jps/build/KotlinJpsBuildTest.kt
@@ -697,6 +697,16 @@ class KotlinJpsBuildTest : AbstractKotlinJpsBuildTestCase() {
         )
     }
 
+    fun testCodeInKotlinPackage() {
+        initProject()
+
+        val result = makeAll()
+        result.assertFailed()
+        val errors = result.getMessages(BuildMessage.Kind.ERROR)
+
+        Assert.assertEquals("Only the Kotlin standard library is allowed to use the 'kotlin' package", errors.single().messageText)
+    }
+
     fun testDoNotCreateUselessKotlinIncrementalCaches() {
         initProject()
         makeAll().assertSuccessful()
