d728a1a6c7221e86d75514c662b456efbc3bc3fd
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-01-20 01:01:37 +0300

    Replace File.relativePath from stdlib with File.descendantRelativeTo in fileUtils. IO utils: cleanup java usages.

--- compiler/util/src/org/jetbrains/kotlin/utils/fileUtils.kt
+++ compiler/util/src/org/jetbrains/kotlin/utils/fileUtils.kt
@@ -32,3 +32,23 @@ fun File.withReplacedExtensionOrNull(oldExt: String, newExt: String): File? {
 
     return null
 }
+
+/**
+ * Calculates the relative path to this file from [base] file.
+ * Note that the [base] file is treated as a directory.
+ *
+ * If this file matches the [base] directory an empty path is returned.
+ * If this file does not belong to the [base] directory, it is returned unchanged.
+ */
+public fun File.descendantRelativeTo(base: File): File {
+    val prefix = base.canonicalPath
+    val answer = this.canonicalPath
+    return if (answer.startsWith(prefix)) {
+        val prefixSize = prefix.length
+        if (answer.length > prefixSize) {
+            File(answer.substring(prefixSize + 1))
+        } else File("")
+    } else {
+        this
+    }
+}
