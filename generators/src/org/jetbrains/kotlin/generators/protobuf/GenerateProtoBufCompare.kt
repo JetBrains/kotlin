aebf681809c716669075c8de1274ca48a48e00ac
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2016-01-22 16:39:44 +0300

    Rename module "build" -> "build-common"

--- generators/src/org/jetbrains/kotlin/generators/protobuf/GenerateProtoBufCompare.kt
+++ generators/src/org/jetbrains/kotlin/generators/protobuf/GenerateProtoBufCompare.kt
@@ -27,7 +27,7 @@ import java.util.*
 
 class GenerateProtoBufCompare {
     companion object {
-        val DEST_FILE: File = File("build/src/org/jetbrains/kotlin/incremental/ProtoCompareGenerated.kt")
+        val DEST_FILE: File = File("build-common/src/org/jetbrains/kotlin/incremental/ProtoCompareGenerated.kt")
 
         @JvmStatic
         fun main(args: Array<String>) {
