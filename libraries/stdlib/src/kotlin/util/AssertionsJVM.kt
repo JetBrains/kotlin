1b03ba305a3872e3f91d07f06bd830b0e0410ba7
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-01-29 22:34:52 +0300

    Make all assert inline. Field _Assertions.ENABLED instead of property ASSERTIONS_ENABLED (cherry picked from commit dacf25f)

--- libraries/stdlib/src/kotlin/util/AssertionsJVM.kt
+++ libraries/stdlib/src/kotlin/util/AssertionsJVM.kt
@@ -3,15 +3,17 @@
 package kotlin
 
 
-private object _Assertions
-
-internal val ASSERTIONS_ENABLED: Boolean = _Assertions.javaClass.desiredAssertionStatus()
+internal object _Assertions {
+    @JvmField
+    internal val ENABLED: Boolean = javaClass.desiredAssertionStatus()
+}
 
 /**
  * Throws an [AssertionError] if the [value] is false
  * and runtime assertions have been enabled on the JVM using the *-ea* JVM option.
  */
-public fun assert(value: Boolean) {
+@kotlin.internal.InlineOnly
+public inline fun assert(value: Boolean) {
     assert(value) { "Assertion failed" }
 }
 
@@ -22,7 +24,7 @@ public fun assert(value: Boolean) {
 @kotlin.internal.InlineOnly
 public inline fun assert(value: Boolean, lazyMessage: () -> Any) {
     @Suppress("INVISIBLE_MEMBER_FROM_INLINE")
-    if (ASSERTIONS_ENABLED) {
+    if (_Assertions.ENABLED) {
         if (!value) {
             val message = lazyMessage()
             throw AssertionError(message)
