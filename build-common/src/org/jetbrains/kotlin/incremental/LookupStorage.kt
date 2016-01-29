054a40796b26b73afac0b7db0a3ecc67f61a253f
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2016-01-29 10:17:17 +0100

    Preparing build lib for migrating gradle plugin on it: moving ArgumentUtils from jps plugin, adding buildUtils with incremental compilation functions extracted from kotlinBuilder, minor tweak to lookup storage dump

--- build-common/src/org/jetbrains/kotlin/incremental/LookupStorage.kt
+++ build-common/src/org/jetbrains/kotlin/incremental/LookupStorage.kt
@@ -171,7 +171,7 @@ open class LookupStorage(private val targetDataDir: File) : BasicMapsOwner() {
         flush(false)
     }
 
-    @TestOnly fun dump(lookupSymbols: Set<LookupSymbol>): String {
+    @TestOnly fun dump(lookupSymbols: Set<LookupSymbol>, basePath: File? = null): String {
         flush(false)
 
         val sb = StringBuilder()
@@ -188,7 +188,7 @@ open class LookupStorage(private val targetDataDir: File) : BasicMapsOwner() {
                 lookup.toString()
             }
 
-            val value = fileIds.map { idToFile[it]?.absolutePath ?: it.toString() }.sorted().joinToString(", ")
+            val value = fileIds.map { idToFile[it]?.let { if (basePath == null) it.absolutePath else it.toRelativeString(basePath) } ?: it.toString() }.sorted().joinToString(", ")
             p.println("$key -> $value")
         }
 
