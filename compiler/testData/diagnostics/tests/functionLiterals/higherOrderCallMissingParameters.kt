de853b8961495f2076cd23350543a8777c4bbf89
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-02-01 14:34:05 +0300

    Correct report of NO_VALUE_FOR_PARAMETER when function call contains no brackets and lambda at the end #KT-7813 Fixed (cherry picked from commit d194af5)

---
+++ compiler/testData/diagnostics/tests/functionLiterals/higherOrderCallMissingParameters.kt
@@ -0,0 +1,16 @@
+// See KT-7813: Call to functional parameter with missing argument: no error detected but compiler crashes
+
+fun foo(p: (Int, () -> Int) -> Unit) {
+    // Errors except last call
+    <!NO_VALUE_FOR_PARAMETER!>p<!> { 1 }
+    p(<!NO_VALUE_FOR_PARAMETER!>)<!> { 2 }
+    p(3) { 4 }
+}
+
+fun bar(p: (String, Any, () -> String) -> Unit) {
+    // Errors except last call
+    <!NO_VALUE_FOR_PARAMETER, NO_VALUE_FOR_PARAMETER!>p<!> { "" }
+    p(<!NO_VALUE_FOR_PARAMETER, NO_VALUE_FOR_PARAMETER!>)<!> { "x" }
+    p("y"<!NO_VALUE_FOR_PARAMETER!>)<!> { "z" }
+    p("v", Any()) { "w" }
+}
