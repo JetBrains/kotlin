05192547da07a7268c16df6b2793bf8109c63838
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-02-01 19:08:51 +0300

    Make DEFAULT_BUFFER_SIZE a constant.

--- libraries/stdlib/src/kotlin/io/Constants.kt
+++ libraries/stdlib/src/kotlin/io/Constants.kt
@@ -1,12 +1,12 @@
 @file:JvmVersion
-@file:JvmMultifileClass
-@file:JvmName("FilesKt")
+@file:JvmName("ConstantsKt")
 package kotlin.io
 
 /**
  * Returns the default buffer size when working with buffered streams.
 */
-public val DEFAULT_BUFFER_SIZE: Int = 64 * 1024
+
+public const val DEFAULT_BUFFER_SIZE: Int = 64 * 1024
 
 @Deprecated("Use DEFAULT_BUFFER_SIZE constant instead.", ReplaceWith("kotlin.io.DEFAULT_BUFFER_SIZE"), level = DeprecationLevel.ERROR)
 public val defaultBufferSize: Int = DEFAULT_BUFFER_SIZE
@@ -14,8 +14,8 @@ public val defaultBufferSize: Int = DEFAULT_BUFFER_SIZE
 /**
  * Returns the default block size for forEachBlock().
  */
-internal val defaultBlockSize: Int = 4096
+internal const val defaultBlockSize: Int = 4096
 /**
  * Returns the minimum block size for forEachBlock().
  */
-internal val minimumBlockSize: Int = 512
+internal const val minimumBlockSize: Int = 512
