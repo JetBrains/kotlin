9f786c00a7d63d9106c22f83720652cc855656ac
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-02-07 09:57:04 +0530

    Fix test data after removing old annotation classes

--- compiler/testData/codegen/boxWithStdlib/annotations/annotatedObjectLiteral.kt
+++ compiler/testData/codegen/boxWithStdlib/annotations/annotatedObjectLiteral.kt
@@ -4,12 +4,11 @@ fun box(): String {
     val v = @Ann("OK") object: My() {}
     val klass = v.javaClass
 
-    val annotations = klass.annotations
-    // Ann, kotlin.Metadata, kotlin.jvm.internal.KotlinClass
-    if (annotations.size != 3) return "Fail annotations size is ${annotations.size}: ${annotations.toList()}"
+    val annotations = klass.annotations.toList()
+    // Ann, kotlin.Metadata
+    if (annotations.size != 2) return "Fail annotations size is ${annotations.size}: $annotations"
     val annotation = annotations.filterIsInstance<Ann>().firstOrNull()
-                     ?: return "Fail no @Ann: ${annotations.toList()}"
+                     ?: return "Fail no @Ann: $annotations"
 
     return annotation.v
 }
-
