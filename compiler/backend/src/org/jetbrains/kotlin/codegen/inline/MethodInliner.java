405c21a17e2c72490075d8b1916d606a220e8566
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-02-18 15:23:39 +0300

    Fix for KT-11081: Reified type parameters are lost in anonymous object in inline function when using default value parameters
     #KT-11081 Fixed

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/MethodInliner.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/MethodInliner.java
@@ -285,7 +285,8 @@ public class MethodInliner {
                         super.visitMethodInsn(opcode, changeOwnerForExternalPackage(owner, opcode), name, desc, itf);
                     }
                 }
-                else if (ReifiedTypeInliner.isNeedClassReificationMarker(new MethodInsnNode(opcode, owner, name, desc, false))) {
+                else if (ReifiedTypeInliner.isNeedClassReificationMarker(new MethodInsnNode(opcode, owner, name, desc, false)) &&
+                         !isDefaultCompilation()) {
                     // we will put it if needed in anew processing
                 }
                 else {
@@ -372,7 +373,7 @@ public class MethodInliner {
             ) {
                 if (isInliningLambda || InlineCodegenUtil.GENERATE_SMAP) {
                     String varSuffix = inliningContext.isRoot() &&
-                                       !((RootInliningContext) inliningContext).isDefaultCompilation &&
+                                       !isDefaultCompilation() &&
                                        !InlineCodegenUtil.isFakeLocalVariableForInline(name) ?
                                        INLINE_FUN_VAR_SUFFIX : "";
                     String varName = !varSuffix.isEmpty() && name.equals("this") ? name + "_" : name;
@@ -440,7 +441,7 @@ public class MethodInliner {
 
             if (frame != null) {
                 if (ReifiedTypeInliner.isNeedClassReificationMarker(cur)) {
-                    awaitClassReification = true;
+                    awaitClassReification = !isDefaultCompilation();
                 }
                 else if (cur.getType() == AbstractInsnNode.METHOD_INSN) {
                     if (InlineCodegenUtil.isFinallyStart(cur)) {
@@ -804,4 +805,7 @@ public class MethodInliner {
 
     }
 
+    private boolean isDefaultCompilation() {
+        return inliningContext.isRoot() && ((RootInliningContext) inliningContext).isDefaultCompilation;
+    }
 }
