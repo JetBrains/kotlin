69b42f4716b50278da211dafd4bf7dcb817883c5
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-02-25 11:14:23 +0300

    Prohibited @Strictfp annotation on classes (it's not supported yet), Fix for KT-11109: Strictfp annotation do nothing when applied to class
      #KT-11109 Fixed
    (cherry picked from commit 1b94904)

---
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/strictfpOnClass.kt
@@ -0,0 +1,11 @@
+<!WRONG_ANNOTATION_TARGET!>@kotlin.jvm.Strictfp<!> class A {
+
+}
+
+<!WRONG_ANNOTATION_TARGET!>@kotlin.jvm.Strictfp<!> object B {
+
+}
+
+<!WRONG_ANNOTATION_TARGET!>@kotlin.jvm.Strictfp<!> interface C {
+
+}
