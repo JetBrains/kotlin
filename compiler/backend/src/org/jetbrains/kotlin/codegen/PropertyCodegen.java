fc16d7c4fd9fc76dfe72471677af60de5b31b312
Pavel V. Talanov <talanov.pavel@gmail.com>
2016-03-09 17:12:12 +0300

    Refactor, minor: extract exception logging to an util object

--- compiler/backend/src/org/jetbrains/kotlin/codegen/PropertyCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/PropertyCodegen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,9 +100,9 @@ public class PropertyCodegen {
     public void gen(@NotNull KtProperty property) {
         VariableDescriptor variableDescriptor = bindingContext.get(BindingContext.VARIABLE, property);
         if (!(variableDescriptor instanceof PropertyDescriptor)) {
-            String problem = "Property " + property.getName() + " should have a property descriptor: " + variableDescriptor;
-            LOG.error(problem, PsiUtilsKt.getElementTextWithContext(property));
-            throw new AssertionError(problem);
+            throw ExceptionLogger.logDescriptorNotFound(
+                    "Property " + property.getName() + " should have a property descriptor: " + variableDescriptor, property
+            );
         }
 
         PropertyDescriptor propertyDescriptor = (PropertyDescriptor) variableDescriptor;
