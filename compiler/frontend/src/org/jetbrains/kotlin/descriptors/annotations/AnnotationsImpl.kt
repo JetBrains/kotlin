777ef32d65dabba4bc44606711ce0834ff081673
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-03-14 19:57:03 +0300

    Move AnnotationsImpl to frontend, delete unused DefaultAnnotationArgumentVisitor

--- compiler/frontend/src/org/jetbrains/kotlin/descriptors/annotations/AnnotationsImpl.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/descriptors/annotations/AnnotationsImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,7 +60,8 @@ class AnnotationsImpl : Annotations {
     override fun toString() = annotations.toString()
 
     companion object {
-        @JvmStatic fun create(annotationsWithTargets: List<AnnotationWithTarget>): AnnotationsImpl {
+        @JvmStatic
+        fun create(annotationsWithTargets: List<AnnotationWithTarget>): AnnotationsImpl {
             return AnnotationsImpl(annotationsWithTargets, 0)
         }
     }
