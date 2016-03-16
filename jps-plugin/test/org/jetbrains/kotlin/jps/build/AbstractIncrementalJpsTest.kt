f00615c00ba487a2a6a81affcc41c04b31131cde
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2016-03-16 14:42:13 +0300

    Minor: source sources of SourceToOutputMap when dump it to make IC tests more stable

--- jps-plugin/test/org/jetbrains/kotlin/jps/build/AbstractIncrementalJpsTest.kt
+++ jps-plugin/test/org/jetbrains/kotlin/jps/build/AbstractIncrementalJpsTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -337,7 +337,7 @@ abstract class AbstractIncrementalJpsTest(
             result.pushIndent()
 
             val mapping = project.dataManager.getSourceToOutputMap(target)
-            mapping.sources.forEach {
+            mapping.sources.sorted().forEach {
                 val outputs = mapping.getOutputs(it)!!.sorted()
                 if (outputs.isNotEmpty()) {
                     result.println("source $it -> $outputs")
