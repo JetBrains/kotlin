da3897536a3d8cd43a238d811ece969fcfc01665
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-03-18 21:19:28 +0300

    Minor: format android gradle test

--- libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinAndroidGradleIT.kt
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinAndroidGradleIT.kt
@@ -4,57 +4,57 @@ import org.junit.Ignore
 import org.junit.Test
 
 @Ignore("Requires Android SDK")
-class KotlinAndroidGradleIT: BaseGradleIT() {
+class KotlinAndroidGradleIT : BaseGradleIT() {
 
-        override fun defaultBuildOptions() =
-                BuildOptions(withDaemon = true, assertThreadLeaks = false)
+    override fun defaultBuildOptions() =
+            BuildOptions(withDaemon = true, assertThreadLeaks = false)
 
-        @Test
-        fun testSimpleCompile() {
-            val project = Project("AndroidProject", "2.3")
+    @Test
+    fun testSimpleCompile() {
+        val project = Project("AndroidProject", "2.3")
 
-            project.build("build") {
-                assertSuccessful()
-                assertContains(":Lib:compileReleaseKotlin",
-                               ":compileFlavor1DebugKotlin",
-                               ":compileFlavor2DebugKotlin",
-                               ":compileFlavor1JnidebugKotlin",
-                               ":compileFlavor1ReleaseKotlin",
-                               ":compileFlavor2JnidebugKotlin",
-                               ":compileFlavor2ReleaseKotlin",
-                               ":compileFlavor1Debug",
-                               ":compileFlavor2Debug",
-                               ":compileFlavor1Jnidebug",
-                               ":compileFlavor2Jnidebug",
-                               ":compileFlavor1Release",
-                               ":compileFlavor2Release")
-            }
-
-            // Run the build second time, assert everything is up-to-date
-            project.build("build") {
-                assertSuccessful()
-                assertContains(":Lib:compileReleaseKotlin UP-TO-DATE")
-            }
-
-            // Run the build third time, re-run tasks
-
-            project.build("build", "--rerun-tasks") {
-                assertSuccessful()
-                assertContains(":Lib:compileReleaseKotlin",
-                        ":compileFlavor1DebugKotlin",
-                        ":compileFlavor2DebugKotlin",
-                        ":compileFlavor1JnidebugKotlin",
-                        ":compileFlavor1ReleaseKotlin",
-                        ":compileFlavor2JnidebugKotlin",
-                        ":compileFlavor2ReleaseKotlin",
-                        ":compileFlavor1Debug",
-                        ":compileFlavor2Debug",
-                        ":compileFlavor1Jnidebug",
-                        ":compileFlavor2Jnidebug",
-                        ":compileFlavor1Release",
-                        ":compileFlavor2Release")
-            }
+        project.build("build") {
+            assertSuccessful()
+            assertContains(":Lib:compileReleaseKotlin",
+                    ":compileFlavor1DebugKotlin",
+                    ":compileFlavor2DebugKotlin",
+                    ":compileFlavor1JnidebugKotlin",
+                    ":compileFlavor1ReleaseKotlin",
+                    ":compileFlavor2JnidebugKotlin",
+                    ":compileFlavor2ReleaseKotlin",
+                    ":compileFlavor1Debug",
+                    ":compileFlavor2Debug",
+                    ":compileFlavor1Jnidebug",
+                    ":compileFlavor2Jnidebug",
+                    ":compileFlavor1Release",
+                    ":compileFlavor2Release")
         }
+
+        // Run the build second time, assert everything is up-to-date
+        project.build("build") {
+            assertSuccessful()
+            assertContains(":Lib:compileReleaseKotlin UP-TO-DATE")
+        }
+
+        // Run the build third time, re-run tasks
+
+        project.build("build", "--rerun-tasks") {
+            assertSuccessful()
+            assertContains(":Lib:compileReleaseKotlin",
+                    ":compileFlavor1DebugKotlin",
+                    ":compileFlavor2DebugKotlin",
+                    ":compileFlavor1JnidebugKotlin",
+                    ":compileFlavor1ReleaseKotlin",
+                    ":compileFlavor2JnidebugKotlin",
+                    ":compileFlavor2ReleaseKotlin",
+                    ":compileFlavor1Debug",
+                    ":compileFlavor2Debug",
+                    ":compileFlavor1Jnidebug",
+                    ":compileFlavor2Jnidebug",
+                    ":compileFlavor1Release",
+                    ":compileFlavor2Release")
+        }
+    }
 
     @Test
     fun testModuleNameAndroid() {
