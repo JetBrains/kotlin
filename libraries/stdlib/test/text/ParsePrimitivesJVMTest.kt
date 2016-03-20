dbf576a90ef4186cd4e572efb8128276ed41339e
voddan <dgvodopyan@gmail.com>
2016-03-20 18:43:32 +0300

    Added tests on wrong number formatting

--- libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
+++ libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
@@ -24,9 +24,43 @@ class ParsePrimitivesJVMTest {
 
     @test fun toInt() {
         assertEquals(77, "77".toInt())
+        assertFails { "2147483648".toInt() }
+        assertFails { "-2147483649".toInt() }
     }
 
     @test fun toLong() {
         assertEquals(77.toLong(), "77".toLong())
+        assertFails { "9223372036854775808".toLong() }
+        assertFails { "-9223372036854775809".toLong() }
+    }
+
+    @test fun toFloat() {
+        assertEquals(77.0f, "77.0".toFloat())
+        assertEquals(Float.NEGATIVE_INFINITY, "-1e39".toFloat())
+        assertEquals(Float.POSITIVE_INFINITY, "1000000000000000000000000000000000000000".toFloat())
+        assertFails { "dark side".toFloat() }
+    }
+
+    @test fun toDouble() {
+        assertEquals(-77.0, "-77".toDouble())
+        assertEquals(77.0, "77.".toDouble())
+        assertEquals(77.0, "77.0".toDouble())
+
+        assertEquals(-1.77, "-1.77".toDouble())
+        assertEquals(0.77, "+.77".toDouble())
+
+        assertEquals(-77.0, "\t-77 \n".toDouble())
+        assertEquals(77.0, "7.7e1".toDouble())
+        assertEquals(77.0, "+770e-1".toDouble())
+
+        assertEquals(-Double.NaN, "-NaN".toDouble())
+        assertEquals(Double.POSITIVE_INFINITY, "+Infinity".toDouble())
+
+        assertEquals((0x77 shl 1).toDouble(), "0x77p1".toDouble())
+        assertEquals(0x77.toDouble(), "0x.77P8".toDouble())
+
+        assertFails { "7..7".toDouble() }
+        assertFails { "0x77e1".toDouble() }
+        assertFails { "007 not a number".toDouble() }
     }
 }
