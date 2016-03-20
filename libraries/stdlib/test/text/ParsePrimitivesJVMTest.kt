68d3b0151844806ebf49f863c8ca6f0ee5b3802e
voddan <dgvodopyan@gmail.com>
2016-03-20 23:42:18 +0300

    Improved test coverage

--- libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
+++ libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
@@ -19,59 +19,40 @@ class ParsePrimitivesJVMTest {
 
     @test fun toShort() {
         assertEquals(77.toShort(), "77".toShort())
+        assertFails { "32768".toShort() }
     }
 
     @test fun toInt() {
         assertEquals(77, "77".toInt())
         assertFails { "2147483648".toInt() }
-        assertFails { "-2147483649".toInt() }
     }
 
     @test fun toLong() {
         assertEquals(77.toLong(), "77".toLong())
-        assertFails { "9223372036854775808".toLong() }
         assertFails { "-9223372036854775809".toLong() }
     }
 
     @test fun toFloat() {
         assertEquals(77.0f, "77.0".toFloat())
-        assertEquals(Float.NEGATIVE_INFINITY, "-1e39".toFloat())
-        assertEquals(Float.POSITIVE_INFINITY, "1000000000000000000000000000000000000000".toFloat())
         assertFails { "dark side".toFloat() }
     }
 
     @test fun toDouble() {
-        assertEquals(-77.0, "-77".toDouble())
-        assertEquals(77.0, "77.".toDouble())
         assertEquals(77.0, "77.0".toDouble())
-
-        assertEquals(-1.77, "-1.77".toDouble())
-        assertEquals(0.77, "+.77".toDouble())
-
-        assertEquals(-77.0, "\t-77 \n".toDouble())
-        assertEquals(77.0, "7.7e1".toDouble())
-        assertEquals(77.0, "+770e-1".toDouble())
-
-        assertEquals(-Double.NaN, "-NaN".toDouble())
-        assertEquals(Double.POSITIVE_INFINITY, "+Infinity".toDouble())
-
-        assertEquals((0x77 shl 1).toDouble(), "0x77p1".toDouble())
-        assertEquals(0x77.toDouble(), "0x.77P8".toDouble())
-
-        assertFails { "7..7".toDouble() }
         assertFails { "0x77e1".toDouble() }
-        assertFails { "007 not a number".toDouble() }
     }
 
 
     @test fun toByteOrNull() {
-        assertEquals(77.toByte(), "77".toByteOrNull())
-        assertNull("255".toByteOrNull())
+        assertEquals(77.toByte(), "+77".toByteOrNull())
+        assertEquals(Byte.MIN_VALUE, "-128".toByteOrNull())
+        assertNull("128".toByteOrNull())
     }
 
     @test fun toShortOrNull() {
         assertEquals(77.toShort(), "77".toShortOrNull())
-        assertNull("32768".toShortOrNull())
+        assertEquals(Short.MIN_VALUE, "-32768".toShortOrNull())
+        assertNull("+32768".toShortOrNull())
     }
 
     @test fun toIntOrNull() {
@@ -80,6 +61,7 @@ class ParsePrimitivesJVMTest {
         assertEquals(Int.MIN_VALUE, "-2147483648".toIntOrNull())
         assertNull("2147483648".toIntOrNull())
         assertNull("-2147483649".toIntOrNull())
+        assertNull("239239kotlin".toIntOrNull())
     }
 
     @test fun toLongOrNull() {
@@ -88,6 +70,11 @@ class ParsePrimitivesJVMTest {
         assertEquals(Long.MIN_VALUE, "-9223372036854775808".toLongOrNull())
         assertNull("9223372036854775808".toLongOrNull())
         assertNull("-9223372036854775809".toLongOrNull())
+
+        assertNull("922337 75809".toLongOrNull())
+        assertNull("92233,75809".toLongOrNull())
+        assertNull("92233`75809".toLongOrNull())
+        assertNull("-922337KOTLIN775809".toLongOrNull())
     }
 
     @test fun toFloatOrNull() {
