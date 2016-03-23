d46da322f34cc38aac278f8a6753b49569798f2f
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-03-23 23:00:40 +0300

    [ABI Validation] Infer lateinit-exposed field effective visibility from its setter visibility.
    Moved from https://github.com/Kotlin/binary-compatibility-validator/commit/abddf13a28c67366a47e7d29a592ed84095f7039

--- libraries/tools/abi-validation/src/main/kotlin/org.jetbrains.kotlin.tools/kotlinVisibilities.kt
+++ libraries/tools/abi-validation/src/main/kotlin/org.jetbrains.kotlin.tools/kotlinVisibilities.kt
@@ -5,13 +5,24 @@ import com.google.gson.stream.JsonReader
 import java.io.File
 
 data class ClassVisibility(val name: String, val visibility: String?, val members: Map<MemberSignature, MemberVisibility>)
-data class MemberVisibility(val member: MemberSignature, val visibility: String?)
+data class MemberVisibility(val member: MemberSignature, val declaration: String?, val visibility: String?)
 data class MemberSignature(val name: String, val desc: String)
 
 private fun isPublic(visibility: String?, isInlineExposed: Boolean) = visibility == null || visibility == "public" || visibility == "protected"  || (isInlineExposed && visibility == "internal")
 fun ClassVisibility.isPublic(isInlineExposed: Boolean) = isPublic(visibility, isInlineExposed)
 fun MemberVisibility.isPublic(isInlineExposed: Boolean) = isPublic(visibility, isInlineExposed)
 
+fun MemberVisibility.isLateInit() = declaration != null && "lateinit var " in declaration
+
+private val varValPrefix = Regex("va[lr]\\s+")
+fun ClassVisibility.findSetterForProperty(property: MemberVisibility): MemberVisibility? {
+    // ad-hoc solution:
+    val declaration = property.declaration ?: return null
+    val match = varValPrefix.find(declaration) ?: return null
+    val name = declaration.substring(match.range.endInclusive + 1).substringBefore(':')
+    val setterName = "<set-$name>"
+    return members.values.find { it.declaration?.contains(setterName) ?: false }
+}
 
 fun readKotlinVisibilities(declarationFile: File): Map<String, ClassVisibility> {
     val result = mutableListOf<ClassVisibility>()
@@ -27,8 +38,9 @@ fun readKotlinVisibilities(declarationFile: File): Map<String, ClassVisibility> 
                     with(it.asJsonObject) {
                         val name = getAsJsonPrimitive("name").asString
                         val desc = getAsJsonPrimitive("desc").asString
+                        val declaration = getAsJsonPrimitive("declaration")?.asString
                         val visibility = getAsJsonPrimitive("visibility")?.asString
-                        MemberVisibility(MemberSignature(name, desc), visibility)
+                        MemberVisibility(MemberSignature(name, desc), declaration, visibility)
                     }
                 }
                 ClassVisibility(name, visibility, members.associateByTo(hashMapOf()) { it.member })
