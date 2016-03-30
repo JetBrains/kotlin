b11a2ac0f27356c8a35bf5601b7d85f3d8195db3
voddan <dgvodopyan@gmail.com>
2016-03-30 20:03:06 +0300

    refactored dsl into 2 separate asserts

--- libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
+++ libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
@@ -15,80 +15,80 @@ class ParsePrimitivesJVMTest {
     @test fun toByte() {
         CompareBehaviourContext(String::toByte, String::toByteOrNull).apply {
 
-            assertProduce(77.toByte(), "+77")
-            assertProduce(Byte.MIN_VALUE, "-128")
-            assertProduce(null, "128")
+            assertProduce("+77", 77.toByte())
+            assertProduce("-128", Byte.MIN_VALUE)
+            assertFailsOrNull("128")
         }
     }
 
     @test fun toShort() {
         CompareBehaviourContext(String::toShort, String::toShortOrNull).apply {
 
-            assertProduce(77.toShort(), "77")
-            assertProduce(Short.MIN_VALUE, "-32768")
-            assertProduce(null, "+32768")
+            assertProduce("77", 77.toShort())
+            assertProduce("-32768", Short.MIN_VALUE)
+            assertFailsOrNull("+32768")
         }
     }
 
     @test fun toInt() {
         CompareBehaviourContext(String::toInt, String::toIntOrNull).apply {
 
-            assertProduce(77, "77")
-            assertProduce(Int.MAX_VALUE, "+2147483647")
-            assertProduce(Int.MIN_VALUE, "-2147483648")
+            assertProduce("77", 77)
+            assertProduce("+2147483647", Int.MAX_VALUE)
+            assertProduce("-2147483648", Int.MIN_VALUE)
 
-            assertProduce(null, "2147483648")
-            assertProduce(null, "-2147483649")
-            assertProduce(null, "239239kotlin")
+            assertFailsOrNull("2147483648")
+            assertFailsOrNull("-2147483649")
+            assertFailsOrNull("239239kotlin")
         }
     }
 
     @test fun toLong() {
         CompareBehaviourContext(String::toLong, String::toLongOrNull).apply {
 
-            assertProduce(77.toLong(), "77")
-            assertProduce(Long.MAX_VALUE, "+9223372036854775807")
-            assertProduce(Long.MIN_VALUE, "-9223372036854775808")
+            assertProduce("77", 77.toLong())
+            assertProduce("+9223372036854775807", Long.MAX_VALUE)
+            assertProduce("-9223372036854775808", Long.MIN_VALUE)
 
-            assertProduce(null, "9223372036854775808")
-            assertProduce(null, "-9223372036854775809")
-            assertProduce(null, "922337 75809")
-            assertProduce(null, "92233,75809")
-            assertProduce(null, "92233`75809")
-            assertProduce(null, "-922337KOTLIN775809")
+            assertFailsOrNull("9223372036854775808")
+            assertFailsOrNull("-9223372036854775809")
+            assertFailsOrNull("922337 75809")
+            assertFailsOrNull("92233,75809")
+            assertFailsOrNull("92233`75809")
+            assertFailsOrNull("-922337KOTLIN775809")
         }
     }
 
     @test fun toFloat() {
         CompareBehaviourContext(String::toFloat, String::toFloatOrNull).apply {
 
-            assertProduce(77.0f, "77.0")
-            assertProduce(Float.NEGATIVE_INFINITY, "-1e39")
-            assertProduce(Float.POSITIVE_INFINITY, "1000000000000000000000000000000000000000")
-            assertProduce(null, "dark side")
+            assertProduce("77.0", 77.0f)
+            assertProduce("-1e39", Float.NEGATIVE_INFINITY)
+            assertProduce("1000000000000000000000000000000000000000", Float.POSITIVE_INFINITY)
+            assertFailsOrNull("dark side")
         }
     }
 
     @test fun toDouble() {
         CompareBehaviourContext(String::toDouble, String::toDoubleOrNull).apply {
 
-            assertProduce(-77.0, "-77")
-            assertProduce(77.0, "77.")
-            assertProduce(77.0, "77.0")
-            assertProduce(-1.77, "-1.77")
-            assertProduce(0.77, "+.77")
-            assertProduce(-77.0, "\t-77 \n")
-            assertProduce(77.0, "7.7e1")
-            assertProduce(77.0, "+770e-1")
+            assertProduce("-77", -77.0)
+            assertProduce("77.", 77.0)
+            assertProduce("77.0", 77.0)
+            assertProduce("-1.77", -1.77)
+            assertProduce("+.77", 0.77)
+            assertProduce("\t-77 \n", -77.0)
+            assertProduce("7.7e1", 77.0)
+            assertProduce("+770e-1", 77.0)
 
-            assertProduce(-Double.NaN, "-NaN")
-            assertProduce(Double.POSITIVE_INFINITY, "+Infinity")
-            assertProduce((0x77 shl 1).toDouble(), "0x77p1")
-            assertProduce(0x77.toDouble(), "0x.77P8")
+            assertProduce("-NaN", -Double.NaN)
+            assertProduce("+Infinity", Double.POSITIVE_INFINITY)
+            assertProduce("0x77p1", (0x77 shl 1).toDouble())
+            assertProduce("0x.77P8", 0x77.toDouble())
 
-            assertProduce(null, "7..7")
-            assertProduce(null, "0x77e1")
-            assertProduce(null, "007 not a number")
+            assertFailsOrNull("7..7")
+            assertFailsOrNull("0x77e1")
+            assertFailsOrNull("007 not a number")
         }
     }
 }
@@ -96,13 +96,13 @@ class ParsePrimitivesJVMTest {
 
 private class CompareBehaviourContext<T: Any>(val convertOrFail: (String) -> T,
                                               val convertOrNull: (String) -> T?) {
-    fun assertProduce(output: T?, input: String) {
-        if(output == null) {
-            assertFails { convertOrFail(input) }
-            assertNull (convertOrNull(input) )
-        } else {
-            assertEquals(output, convertOrFail(input))
-            assertEquals(output, convertOrNull(input))
-        }
+    fun assertProduce(input: String, output: T) {
+        assertEquals(output, convertOrFail(input))
+        assertEquals(output, convertOrNull(input))
+    }
+
+    fun assertFailsOrNull(input: String) {
+        assertFails { convertOrFail(input) }
+        assertNull (convertOrNull(input) )
     }
 }
