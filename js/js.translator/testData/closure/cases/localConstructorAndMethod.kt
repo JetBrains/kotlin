4ac730e5ed2beec62b6ee0713c3325b0e33c9b50
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-03-30 16:40:41 +0300

    KT-11030 Fix referencing wrong field generated for captured variable when this captured variable referenced both from constructor and non-constructor method. Avoid duplicate generation of call to delegated constructor.

---
+++ js/js.translator/testData/closure/cases/localConstructorAndMethod.kt
@@ -0,0 +1,18 @@
+package foo
+
+interface B {
+    fun result(): Int
+}
+
+class A(private val x: Int) {
+    fun test() = object : B {
+        val y = x + 1
+
+        override fun result() = x * 10 + y
+    }
+}
+
+fun box(): String {
+    assertEquals(23, A(2).test().result())
+    return "OK"
+}
