7cbe1bd49a2d7d63bf9da9b208e7fd07833d4111
voddan <dgvodopyan@gmail.com>
2016-03-30 19:56:40 +0300

    improved: added dsl for testing

--- libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
+++ libraries/stdlib/test/text/ParsePrimitivesJVMTest.kt
@@ -13,79 +13,96 @@ class ParsePrimitivesJVMTest {
     }
 
     @test fun toByte() {
-        assertEqualsOrFailsNullable(77.toByte(), "+77", String::toByte, String::toByteOrNull)
-        assertEqualsOrFailsNullable(Byte.MIN_VALUE, "-128", String::toByte, String::toByteOrNull)
-        assertEqualsOrFailsNullable(null, "128", String::toByte, String::toByteOrNull)
+        CompareBehaviourContext(String::toByte, String::toByteOrNull).apply {
+
+            assertProduce(77.toByte(), "+77")
+            assertProduce(Byte.MIN_VALUE, "-128")
+            assertProduce(null, "128")
+        }
     }
 
     @test fun toShort() {
-        assertEqualsOrFailsNullable(77.toShort(), "77", String::toShort, String::toShortOrNull)
-        assertEqualsOrFailsNullable(Short.MIN_VALUE, "-32768", String::toShort, String::toShortOrNull)
-        assertEqualsOrFailsNullable(null, "+32768", String::toShort, String::toShortOrNull)
+        CompareBehaviourContext(String::toShort, String::toShortOrNull).apply {
+
+            assertProduce(77.toShort(), "77")
+            assertProduce(Short.MIN_VALUE, "-32768")
+            assertProduce(null, "+32768")
+        }
     }
 
     @test fun toInt() {
-        assertEqualsOrFailsNullable(77, "77", String::toInt, String::toIntOrNull)
-        assertEqualsOrFailsNullable(Int.MAX_VALUE, "+2147483647", String::toInt, String::toIntOrNull)
-        assertEqualsOrFailsNullable(Int.MIN_VALUE, "-2147483648", String::toInt, String::toIntOrNull)
+        CompareBehaviourContext(String::toInt, String::toIntOrNull).apply {
 
-        assertEqualsOrFailsNullable(null, "2147483648", String::toInt, String::toIntOrNull)
-        assertEqualsOrFailsNullable(null, "-2147483649", String::toInt, String::toIntOrNull)
-        assertEqualsOrFailsNullable(null, "239239kotlin", String::toInt, String::toIntOrNull)
+            assertProduce(77, "77")
+            assertProduce(Int.MAX_VALUE, "+2147483647")
+            assertProduce(Int.MIN_VALUE, "-2147483648")
+
+            assertProduce(null, "2147483648")
+            assertProduce(null, "-2147483649")
+            assertProduce(null, "239239kotlin")
+        }
     }
 
     @test fun toLong() {
-        assertEqualsOrFailsNullable(77.toLong(), "77", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(Long.MAX_VALUE, "+9223372036854775807", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(Long.MIN_VALUE, "-9223372036854775808", String::toLong, String::toLongOrNull)
+        CompareBehaviourContext(String::toLong, String::toLongOrNull).apply {
 
-        assertEqualsOrFailsNullable(null, "9223372036854775808", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(null, "-9223372036854775809", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(null, "922337 75809", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(null, "92233,75809", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(null, "92233`75809", String::toLong, String::toLongOrNull)
-        assertEqualsOrFailsNullable(null, "-922337KOTLIN775809", String::toLong, String::toLongOrNull)
+            assertProduce(77.toLong(), "77")
+            assertProduce(Long.MAX_VALUE, "+9223372036854775807")
+            assertProduce(Long.MIN_VALUE, "-9223372036854775808")
+
+            assertProduce(null, "9223372036854775808")
+            assertProduce(null, "-9223372036854775809")
+            assertProduce(null, "922337 75809")
+            assertProduce(null, "92233,75809")
+            assertProduce(null, "92233`75809")
+            assertProduce(null, "-922337KOTLIN775809")
+        }
     }
 
     @test fun toFloat() {
-        assertEqualsOrFailsNullable(77.0f, "77.0", String::toFloat, String::toFloatOrNull)
-        assertEqualsOrFailsNullable(Float.NEGATIVE_INFINITY, "-1e39", String::toFloat, String::toFloatOrNull)
-        assertEqualsOrFailsNullable(Float.POSITIVE_INFINITY, "1000000000000000000000000000000000000000",
-                String::toFloat, String::toFloatOrNull)
+        CompareBehaviourContext(String::toFloat, String::toFloatOrNull).apply {
 
-        assertEqualsOrFailsNullable(null, "dark side", String::toFloat, String::toFloatOrNull)
+            assertProduce(77.0f, "77.0")
+            assertProduce(Float.NEGATIVE_INFINITY, "-1e39")
+            assertProduce(Float.POSITIVE_INFINITY, "1000000000000000000000000000000000000000")
+            assertProduce(null, "dark side")
+        }
     }
 
     @test fun toDouble() {
-        assertEqualsOrFailsNullable(-77.0, "-77", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(77.0, "77.", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(77.0, "77.0", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(-1.77, "-1.77", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(0.77, "+.77", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(-77.0, "\t-77 \n", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(77.0, "7.7e1", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(77.0, "+770e-1", String::toDouble, String::toDoubleOrNull)
+        CompareBehaviourContext(String::toDouble, String::toDoubleOrNull).apply {
 
-        assertEqualsOrFailsNullable(-Double.NaN, "-NaN", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(Double.POSITIVE_INFINITY, "+Infinity", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable((0x77 shl 1).toDouble(), "0x77p1", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(0x77.toDouble(), "0x.77P8", String::toDouble, String::toDoubleOrNull)
+            assertProduce(-77.0, "-77")
+            assertProduce(77.0, "77.")
+            assertProduce(77.0, "77.0")
+            assertProduce(-1.77, "-1.77")
+            assertProduce(0.77, "+.77")
+            assertProduce(-77.0, "\t-77 \n")
+            assertProduce(77.0, "7.7e1")
+            assertProduce(77.0, "+770e-1")
 
-        assertEqualsOrFailsNullable(null, "7..7", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(null, "0x77e1", String::toDouble, String::toDoubleOrNull)
-        assertEqualsOrFailsNullable(null, "007 not a number", String::toDouble, String::toDoubleOrNull)
+            assertProduce(-Double.NaN, "-NaN")
+            assertProduce(Double.POSITIVE_INFINITY, "+Infinity")
+            assertProduce((0x77 shl 1).toDouble(), "0x77p1")
+            assertProduce(0x77.toDouble(), "0x.77P8")
+
+            assertProduce(null, "7..7")
+            assertProduce(null, "0x77e1")
+            assertProduce(null, "007 not a number")
+        }
     }
 }
 
-private inline fun <T: Any> assertEqualsOrFailsNullable(output: T?,
-                                                        input: String,
-                                                        crossinline converOrFail: (String) -> T,
-                                                        crossinline convertOrNull: (String) -> T?) {
-    if(output == null) {
-        assertFails { converOrFail(input) }
-        assertNull (convertOrNull(input) )
-    } else {
-        assertEquals(output, converOrFail(input))
-        assertEquals(output, convertOrNull(input))
+
+private class CompareBehaviourContext<T: Any>(val convertOrFail: (String) -> T,
+                                              val convertOrNull: (String) -> T?) {
+    fun assertProduce(output: T?, input: String) {
+        if(output == null) {
+            assertFails { convertOrFail(input) }
+            assertNull (convertOrNull(input) )
+        } else {
+            assertEquals(output, convertOrFail(input))
+            assertEquals(output, convertOrNull(input))
+        }
     }
 }
