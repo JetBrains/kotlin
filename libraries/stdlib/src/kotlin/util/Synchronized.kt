7433683011653fc58fb8180421757e58e21dcd9b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-03-30 17:41:07 +0300

    Minor: Make internal monitorEnter/monitorExit functions private to remove them from the public API  #KT-11485 (cherry picked from commit 9caa17d)

--- libraries/stdlib/src/kotlin/util/Synchronized.kt
+++ libraries/stdlib/src/kotlin/util/Synchronized.kt
@@ -10,13 +10,13 @@ import kotlin.jvm.internal.unsafe.*
  */
 @kotlin.internal.InlineOnly
 public inline fun <R> synchronized(lock: Any, block: () -> R): R {
-    @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
+    @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE", "INVISIBLE_MEMBER")
     monitorEnter(lock)
     try {
         return block()
     }
     finally {
-        @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
+        @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE", "INVISIBLE_MEMBER")
         monitorExit(lock)
     }
 }
