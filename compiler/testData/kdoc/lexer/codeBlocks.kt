8e8542e98408e049f6fa3ffbf2f6ed3a1eb25dee
Dmitry Jemerov <yole@jetbrains.com>
2016-03-31 20:48:35 +0200

    don't parse lines starting with @ as KDoc tags if they're inside a Markdown code fence  #KT-11789 Fixed (cherry picked from commit 01b0cfb)

---
+++ compiler/testData/kdoc/lexer/codeBlocks.kt
@@ -0,0 +1,14 @@
+/**
+ * @foo This is text.
+ * ``` kotlin
+ * @foo This is code block.
+ * ```
+ * @foo This is text.
+ * ~~~
+ * @foo This is code block
+ * With multiple lines.
+ * ~~~
+ * @foo This is yet more text.
+ * ```
+ * @foo This is an unclosed code block.
+ */
