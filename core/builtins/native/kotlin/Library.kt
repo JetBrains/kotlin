bd44cd9a3c61a86fd0711320e039909f4fec2ba9
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-03-31 19:46:10 +0300

    Minor: commit missing file (related to Remove implementations of intrinsic array creation functions)

--- core/builtins/native/kotlin/Library.kt
+++ core/builtins/native/kotlin/Library.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,3 +34,48 @@ public operator fun String?.plus(other: Any?): String
  * Returns an array of objects of the given type with the given [size], initialized with null values.
  */
 public fun <reified @PureReifiable T> arrayOfNulls(size: Int): Array<T?>
+
+/**
+ * Returns an array containing the specified elements.
+ */
+public inline fun <reified @PureReifiable T> arrayOf(vararg elements: T): Array<T>
+
+/**
+ * Returns an array containing the specified [Double] numbers.
+ */
+public fun doubleArrayOf(vararg elements: Double): DoubleArray
+
+/**
+ * Returns an array containing the specified [Float] numbers.
+ */
+public fun floatArrayOf(vararg elements: Float): FloatArray
+
+/**
+ * Returns an array containing the specified [Long] numbers.
+ */
+public fun longArrayOf(vararg elements: Long): LongArray
+
+/**
+ * Returns an array containing the specified [Int] numbers.
+ */
+public fun intArrayOf(vararg elements: Int): IntArray
+
+/**
+ * Returns an array containing the specified characters.
+ */
+public fun charArrayOf(vararg elements: Char): CharArray
+
+/**
+ * Returns an array containing the specified [Short] numbers.
+ */
+public fun shortArrayOf(vararg elements: Short): ShortArray
+
+/**
+ * Returns an array containing the specified [Byte] numbers.
+ */
+public fun byteArrayOf(vararg elements: Byte): ByteArray
+
+/**
+ * Returns an array containing the specified boolean values.
+ */
+public fun booleanArrayOf(vararg elements: Boolean): BooleanArray
