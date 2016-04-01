f6c1ff038cd9ff067e344a1194cfb4c8ad194090
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-04-01 20:49:22 +0300

    Remove stopping JobSchedule executor. It now fails on shutdown attempt.

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/CleanUpBuildListener.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/CleanUpBuildListener.kt
@@ -122,7 +122,6 @@ class CompilerServicesCleanup(private var pluginClassLoader: ClassLoader?) {
             if (it < comparableVersionStr("2.4")!!) {
                 // TODO: remove ZipFileCache cleanup after switching to recent idea libs
                 stopZipFileCache()
-                stopJobScheduler()
             }
         }
 
@@ -132,18 +131,6 @@ class CompilerServicesCleanup(private var pluginClassLoader: ClassLoader?) {
     private fun stopZipFileCache() {
         callVoidStaticMethod("org.jetbrains.kotlin.com.intellij.openapi.util.io.ZipFileCache", "stopBackgroundThread")
         log.kotlinDebug("ZipFileCache finished successfully")
-    }
-
-    private fun stopJobScheduler() {
-        log.kotlinDebug("Stop JobScheduler")
-
-        val jobSchedulerClass = Class.forName("com.intellij.concurrency.JobScheduler", false, pluginClassLoader)
-
-        val getSchedulerMethod = jobSchedulerClass.getMethod("getScheduler")
-        val executorService = getSchedulerMethod.invoke(this) as ScheduledExecutorService
-
-        executorService.shutdown()
-        log.kotlinDebug("JobScheduler stopped")
     }
 
     private fun callVoidStaticMethod(classFqName: String, methodName: String) {
