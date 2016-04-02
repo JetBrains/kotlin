a1f6f4454474d3cefec15fd323742203253d9824
voddan <dgvodopyan@gmail.com>
2016-04-02 20:27:28 +0300

    fixed the difference in semantics of `Integer.parseInt()` between JDK6 and JDK7, emulated behaviour of JDK7

--- libraries/stdlib/src/kotlin/text/ParsePrimitivesJVM.kt
+++ libraries/stdlib/src/kotlin/text/ParsePrimitivesJVM.kt
@@ -16,28 +16,28 @@ public inline fun String.toBoolean(): Boolean = java.lang.Boolean.parseBoolean(t
  * @throws NumberFormatException if the string is not a valid representation of a number.
  */
 @kotlin.internal.InlineOnly
-public inline fun String.toByte(): Byte = java.lang.Byte.parseByte(this)
+public inline fun String.toByte(): Byte = java.lang.Byte.parseByte(this.removePrefix("+"))
 
 /**
  * Parses the string as a [Short] number and returns the result.
  * @throws NumberFormatException if the string is not a valid representation of a number.
  */
 @kotlin.internal.InlineOnly
-public inline fun String.toShort(): Short = java.lang.Short.parseShort(this)
+public inline fun String.toShort(): Short = java.lang.Short.parseShort(this.removePrefix("+"))
 
 /**
  * Parses the string as an [Int] number and returns the result.
  * @throws NumberFormatException if the string is not a valid representation of a number.
  */
 @kotlin.internal.InlineOnly
-public inline fun String.toInt(): Int = java.lang.Integer.parseInt(this)
+public inline fun String.toInt(): Int = java.lang.Integer.parseInt(this.removePrefix("+"))
 
 /**
  * Parses the string as a [Long] number and returns the result.
  * @throws NumberFormatException if the string is not a valid representation of a number.
  */
 @kotlin.internal.InlineOnly
-public inline fun String.toLong(): Long = java.lang.Long.parseLong(this)
+public inline fun String.toLong(): Long = java.lang.Long.parseLong(this.removePrefix("+"))
 
 /**
  * Parses the string as a [Float] number and returns the result.
