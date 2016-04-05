070ee108729970d13536288f7ddececc669e7439
Natalia Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2016-04-05 18:57:17 +0300

    Debugger: do not insert ambiguous imports inside codeFragments  #KT-11927 Fixed

--- idea/testData/debugger/tinyApp/src/evaluate/singleBreakpoint/createExpression/createExpressionWithArray.kt
+++ idea/testData/debugger/tinyApp/src/evaluate/singleBreakpoint/createExpression/createExpressionWithArray.kt
@@ -1,6 +1,8 @@
 package createExpressionWithArray
 
 import forTests.MyJavaClass
+// do not remove this import, it checks that we do not insert ambiguous imports during EE
+import forTests.MyJavaClass.InnerClass
 
 fun main(args: Array<String>) {
     val baseArray = arrayOf(MyJavaClass().getBaseClassValue())
