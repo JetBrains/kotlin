0d4e3ed0f6ea74d03a615cdb91c5583ed484404c
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-04-05 15:35:39 +0300

    KT-6201 KT-6203 KT-6326 KT-6777 Add test cases to prove that issues are no longer reproducible (cherry picked from commit 0869eb7)

---
+++ js/js.translator/testData/classObject/cases/enumCompanionObject.kt
@@ -0,0 +1,15 @@
+// See KT-6326, KT-6777
+package foo
+
+enum class Foo {
+    A;
+
+    companion object {
+        val a = A
+    }
+}
+
+fun box(): String {
+    assertEquals("A", Foo.a.name)
+    return "OK"
+}
