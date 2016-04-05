0869eb7ef38dd34f283536eda16edc8c7a2d2dd3
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-04-05 15:35:39 +0300

    KT-6201 KT-6203 KT-6326 KT-6777 Add test cases to prove that issues are no longer reproducible

---
+++ js/js.translator/testData/classObject/cases/objectInCompanionObject.kt
@@ -0,0 +1,25 @@
+// See KT-6203
+package foo
+
+class TestClass {
+    object a {
+        override fun toString() = "a"
+    }
+
+    companion object {
+        object b {
+            override fun toString() = "b"
+        }
+    }
+
+    fun test() = convert(a) + convert(b)
+}
+
+fun convert(o: Any) = o.toString()
+
+fun box(): String {
+    assertEquals("ab", TestClass().test())
+    assertEquals("ab2", convert(TestClass.a) + convert(TestClass.Companion.b) + "2")
+
+    return "OK"
+}
