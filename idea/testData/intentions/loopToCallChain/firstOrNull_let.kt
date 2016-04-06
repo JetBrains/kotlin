89ce911a4501c3a123d7a2a726e240c341950938
Valentin Kipyatkov <valentin@jetbrains.com>
2016-04-06 12:16:22 +0300

    Generating "firstOrNull()?.let {...}" (cherry picked from commit a760ef3)

---
+++ idea/testData/intentions/loopToCallChain/firstOrNull_let.kt
@@ -0,0 +1,12 @@
+// WITH_RUNTIME
+fun foo(list: List<String>) {
+    var result: String? = null
+    <caret>for (s in list) {
+        if (s.length > 0) {
+            result = bar(s)
+            break
+        }
+    }
+}
+
+fun bar(s: String): String = s
--- idea/testData/intentions/loopToCallChain/firstOrNull_returnExpression.kt
+++ idea/testData/intentions/loopToCallChain/firstOrNull_returnExpression.kt
@@ -1,5 +1,4 @@
 // WITH_RUNTIME
-// IS_APPLICABLE: false
 fun foo(list: List<String>): Int? {
     <caret>for (s in list) {
         if (s.isNotEmpty()) {
