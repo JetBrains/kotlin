9fd19dd7fd165bb0014ffe92e9766f60044390e8
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-04-06 10:52:43 +0300

    KT-7819 Add test to ensure that lambda capturing `this` literal can be inlined

---
+++ js/js.translator/testData/inline/cases/inlineCapturingThis.kt
@@ -0,0 +1,21 @@
+package foo
+
+// CHECK_CONTAINS_NO_CALLS: test
+
+inline fun block(p: () -> Unit) = p()
+
+class A(val x: Int) {
+    fun test(): Int {
+        var result: Int = 0
+        block {
+            result = x
+        }
+        return result
+    }
+}
+
+fun box(): String {
+    assertEquals(23, A(23).test())
+
+    return "OK"
+}
