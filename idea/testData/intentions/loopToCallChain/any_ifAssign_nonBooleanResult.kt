2bdc71ed655a3d8244c7dc364dbceb021c0e8fba
Valentin Kipyatkov <valentin@jetbrains.com>
2016-04-06 12:00:35 +0300

    More cases supported for "any" (cherry picked from commit fba9b12)

---
+++ idea/testData/intentions/loopToCallChain/any_ifAssign_nonBooleanResult.kt
@@ -0,0 +1,10 @@
+// WITH_RUNTIME
+fun foo(list: List<String>) {
+    var result = 0
+    <caret>for (s in list) {
+        if (s.length > 0) {
+            result = 1
+            break
+        }
+    }
+}
