84b573d1ea149df1189de76b6fa500503e5eb4d5
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-04-13 14:53:47 +0300

    J2K backend-common classes: convert
    Also merge CodegenUtilKt into CodegenUtil
    
    (cherry picked from commit c1c26519882ae3a0306afa6ca323f430a90d0088)

--- compiler/backend-common/src/org/jetbrains/kotlin/backend/common/CodegenUtil.kt
+++ compiler/backend-common/src/org/jetbrains/kotlin/backend/common/CodegenUtil.kt
@@ -14,183 +14,200 @@
  * limitations under the License.
  */
 
-package org.jetbrains.kotlin.backend.common;
+package org.jetbrains.kotlin.backend.common
 
-import com.intellij.openapi.editor.Document;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.backend.common.bridges.ImplKt;
-import org.jetbrains.kotlin.descriptors.*;
-import org.jetbrains.kotlin.incremental.components.NoLookupLocation;
-import org.jetbrains.kotlin.name.Name;
-import org.jetbrains.kotlin.psi.*;
-import org.jetbrains.kotlin.resolve.BindingContext;
-import org.jetbrains.kotlin.resolve.DescriptorUtils;
-import org.jetbrains.kotlin.resolve.calls.callResolverUtil.CallResolverUtilKt;
-import org.jetbrains.kotlin.resolve.calls.callUtil.CallUtilKt;
-import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall;
-import org.jetbrains.kotlin.resolve.descriptorUtil.DescriptorUtilsKt;
-import org.jetbrains.kotlin.types.KotlinType;
-import org.jetbrains.kotlin.types.TypeUtils;
-import org.jetbrains.kotlin.types.checker.KotlinTypeChecker;
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.backend.common.bridges.findTraitImplementation
+import org.jetbrains.kotlin.descriptors.*
+import org.jetbrains.kotlin.incremental.components.NoLookupLocation
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.DescriptorUtils
+import org.jetbrains.kotlin.resolve.MemberComparator
+import org.jetbrains.kotlin.resolve.calls.callResolverUtil.isOrOverridesSynthesized
+import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
+import org.jetbrains.kotlin.resolve.descriptorUtil.builtIns
+import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.TypeUtils
+import org.jetbrains.kotlin.types.checker.KotlinTypeChecker
+import org.jetbrains.kotlin.types.isDynamic
+import org.jetbrains.kotlin.utils.keysToMapExceptNulls
 
-import java.util.*;
+object CodegenUtil {
+    // class Foo : Bar by baz
+    //   descriptor = Foo
+    //   toInterface = Bar
+    //   delegateExpressionType = typeof(baz)
+    // return Map<member of Foo, corresponding member of typeOf(baz)>
+    @JvmStatic
+    fun getDelegates(
+            descriptor: ClassDescriptor,
+            toInterface: ClassDescriptor,
+            delegateExpressionType: KotlinType? = null
+    ): Map<CallableMemberDescriptor, CallableDescriptor> {
+        if (delegateExpressionType?.isDynamic() ?: false) return emptyMap()
 
-public class CodegenUtil {
+        return descriptor.defaultType.memberScope.getContributedDescriptors().asSequence()
+                .filterIsInstance<CallableMemberDescriptor>()
+                .filter { it.kind == CallableMemberDescriptor.Kind.DELEGATION }
+                .asIterable()
+                .sortedWith(MemberComparator.INSTANCE)
+                .keysToMapExceptNulls { delegatingMember ->
+                    val actualDelegates = DescriptorUtils.getAllOverriddenDescriptors(delegatingMember)
+                            .mapNotNull { overriddenDescriptor ->
+                                if (overriddenDescriptor.containingDeclaration == toInterface) {
+                                    val scope = (delegateExpressionType ?: toInterface.defaultType).memberScope
+                                    val name = overriddenDescriptor.name
 
-    private CodegenUtil() {
-    }
+                                    // this is the actual member of delegateExpressionType that we are delegating to
+                                    (scope.getContributedFunctions(name, NoLookupLocation.FROM_BACKEND) +
+                                     scope.getContributedVariables(name, NoLookupLocation.FROM_BACKEND))
+                                            .firstOrNull {
+                                                (listOf(it) + DescriptorUtils.getAllOverriddenDescriptors(it))
+                                                        .map(CallableMemberDescriptor::getOriginal)
+                                                        .contains(overriddenDescriptor.original)
+                                            }
+                                }
+                                else null
+                            }
 
-    @Nullable
-    public static FunctionDescriptor getDeclaredFunctionByRawSignature(
-            @NotNull ClassDescriptor owner,
-            @NotNull Name name,
-            @NotNull ClassifierDescriptor returnedClassifier,
-            @NotNull ClassifierDescriptor... valueParameterClassifiers
-    ) {
-        Collection<SimpleFunctionDescriptor> functions = owner.getDefaultType().getMemberScope().getContributedFunctions(name, NoLookupLocation.FROM_BACKEND);
-        for (FunctionDescriptor function : functions) {
-            if (!CallResolverUtilKt.isOrOverridesSynthesized(function)
-                && function.getTypeParameters().isEmpty()
-                && valueParameterClassesMatch(function.getValueParameters(), Arrays.asList(valueParameterClassifiers))
-                && rawTypeMatches(function.getReturnType(), returnedClassifier)) {
-                return function;
-            }
-        }
-        return null;
-    }
+                    assert(actualDelegates.size <= 1) { "Many delegates found for $delegatingMember: $actualDelegates" }
 
-    @Nullable
-    public static PropertyDescriptor getDelegatePropertyIfAny(KtExpression expression, ClassDescriptor classDescriptor, BindingContext bindingContext) {
-        PropertyDescriptor propertyDescriptor = null;
-        if (expression instanceof KtSimpleNameExpression) {
-            ResolvedCall<?> call = CallUtilKt.getResolvedCall(expression, bindingContext);
-            if (call != null) {
-                CallableDescriptor callResultingDescriptor = call.getResultingDescriptor();
-                if (callResultingDescriptor instanceof ValueParameterDescriptor) {
-                    ValueParameterDescriptor valueParameterDescriptor = (ValueParameterDescriptor) callResultingDescriptor;
-                    // constructor parameter
-                    if (valueParameterDescriptor.getContainingDeclaration() instanceof ConstructorDescriptor) {
-                        // constructor of my class
-                        if (valueParameterDescriptor.getContainingDeclaration().getContainingDeclaration() == classDescriptor) {
-                            propertyDescriptor = bindingContext.get(BindingContext.VALUE_PARAMETER_AS_PROPERTY, valueParameterDescriptor);
-                        }
-                    }
+                    actualDelegates.firstOrNull()
                 }
+    }
 
-                // todo: when and if frontend will allow properties defined not as constructor parameters to be used in delegation specifier
+    @JvmStatic
+    fun getDeclaredFunctionByRawSignature(
+            owner: ClassDescriptor,
+            name: Name,
+            returnedClassifier: ClassifierDescriptor,
+            vararg valueParameterClassifiers: ClassifierDescriptor
+    ): FunctionDescriptor? {
+        return owner.defaultType.memberScope.getContributedFunctions(name, NoLookupLocation.FROM_BACKEND).firstOrNull { function ->
+            !isOrOverridesSynthesized(function) &&
+            function.typeParameters.isEmpty() &&
+            valueParameterClassesMatch(function.valueParameters, valueParameterClassifiers.toList()) &&
+            rawTypeMatches(function.returnType!!, returnedClassifier)
+        }
+    }
+
+    @JvmStatic
+    fun getDelegatePropertyIfAny(
+            expression: KtExpression, classDescriptor: ClassDescriptor, bindingContext: BindingContext
+    ): PropertyDescriptor? {
+        val call = (expression as? KtSimpleNameExpression)?.getResolvedCall(bindingContext) ?: return null
+        val callResultingDescriptor = call.resultingDescriptor as? ValueParameterDescriptor ?: return null
+        // constructor parameter
+        if (callResultingDescriptor.containingDeclaration is ConstructorDescriptor) {
+            // constructor of my class
+            if (callResultingDescriptor.containingDeclaration.containingDeclaration === classDescriptor) {
+                return bindingContext.get(BindingContext.VALUE_PARAMETER_AS_PROPERTY, callResultingDescriptor)
             }
         }
-        return propertyDescriptor;
+        return null
     }
 
-    public static boolean isFinalPropertyWithBackingField(PropertyDescriptor propertyDescriptor, BindingContext bindingContext) {
+    @JvmStatic
+    fun isFinalPropertyWithBackingField(propertyDescriptor: PropertyDescriptor?, bindingContext: BindingContext): Boolean {
         return propertyDescriptor != null &&
-               !propertyDescriptor.isVar() &&
-               Boolean.TRUE.equals(bindingContext.get(BindingContext.BACKING_FIELD_REQUIRED, propertyDescriptor));
+               !propertyDescriptor.isVar &&
+               (bindingContext.get(BindingContext.BACKING_FIELD_REQUIRED, propertyDescriptor) ?: false)
     }
 
-    @NotNull
-    public static Map<FunctionDescriptor, FunctionDescriptor> getNonPrivateTraitMethods(ClassDescriptor descriptor) {
-        Map<FunctionDescriptor, FunctionDescriptor> result = new LinkedHashMap<FunctionDescriptor, FunctionDescriptor>();
-        for (DeclarationDescriptor declaration : DescriptorUtils.getAllDescriptors(descriptor.getDefaultType().getMemberScope())) {
-            if (!(declaration instanceof CallableMemberDescriptor)) continue;
+    @JvmStatic
+    fun getNonPrivateTraitMethods(descriptor: ClassDescriptor): Map<FunctionDescriptor, FunctionDescriptor> {
+        val result = linkedMapOf<FunctionDescriptor, FunctionDescriptor>()
+        for (declaration in DescriptorUtils.getAllDescriptors(descriptor.defaultType.memberScope)) {
+            if (declaration !is CallableMemberDescriptor) continue
 
-            CallableMemberDescriptor inheritedMember = (CallableMemberDescriptor) declaration;
-            CallableMemberDescriptor traitMember = ImplKt.findTraitImplementation(inheritedMember);
-            if (traitMember == null || Visibilities.isPrivate(traitMember.getVisibility())) continue;
+            val traitMember = findTraitImplementation(declaration)
+            if (traitMember == null || Visibilities.isPrivate(traitMember.visibility)) continue
 
-            assert traitMember.getModality() != Modality.ABSTRACT : "Cannot delegate to abstract trait method: " + inheritedMember;
+            assert(traitMember.modality !== Modality.ABSTRACT) { "Cannot delegate to abstract trait method: $declaration" }
 
             // inheritedMember can be abstract here. In order for FunctionCodegen to generate the method body, we're creating a copy here
             // with traitMember's modality
-            result.putAll(copyFunctions(inheritedMember, traitMember, inheritedMember.getContainingDeclaration(), traitMember.getModality(), Visibilities.PUBLIC,
-                                                     CallableMemberDescriptor.Kind.DECLARATION, true));
+            result.putAll(copyFunctions(declaration, traitMember, declaration.containingDeclaration, traitMember.modality,
+                                        Visibilities.PUBLIC, CallableMemberDescriptor.Kind.DECLARATION, true))
         }
-        return result;
+        return result
     }
 
-    @NotNull
-    public static Map<FunctionDescriptor, FunctionDescriptor> copyFunctions(
-            @NotNull CallableMemberDescriptor inheritedMember,
-            @NotNull CallableMemberDescriptor traitMember,
-            DeclarationDescriptor newOwner,
-            Modality modality,
-            Visibility visibility,
-            CallableMemberDescriptor.Kind kind,
-            boolean copyOverrides
-    ) {
-        CallableMemberDescriptor copy = inheritedMember.copy(newOwner, modality, visibility, kind, copyOverrides);
-        Map<FunctionDescriptor, FunctionDescriptor> result = new LinkedHashMap<FunctionDescriptor, FunctionDescriptor>(0);
-        if (traitMember instanceof SimpleFunctionDescriptor) {
-            result.put((FunctionDescriptor) traitMember, (FunctionDescriptor) copy);
+    fun copyFunctions(
+            inheritedMember: CallableMemberDescriptor,
+            traitMember: CallableMemberDescriptor,
+            newOwner: DeclarationDescriptor,
+            modality: Modality,
+            visibility: Visibility,
+            kind: CallableMemberDescriptor.Kind,
+            copyOverrides: Boolean
+    ): Map<FunctionDescriptor, FunctionDescriptor> {
+        val copy = inheritedMember.copy(newOwner, modality, visibility, kind, copyOverrides)
+        val result = linkedMapOf<FunctionDescriptor, FunctionDescriptor>()
+        if (traitMember is SimpleFunctionDescriptor) {
+            result[traitMember] = copy as FunctionDescriptor
         }
-        else if (traitMember instanceof PropertyDescriptor) {
-            for (PropertyAccessorDescriptor traitAccessor : ((PropertyDescriptor) traitMember).getAccessors()) {
-                for (PropertyAccessorDescriptor inheritedAccessor : ((PropertyDescriptor) copy).getAccessors()) {
-                    if (inheritedAccessor.getClass() == traitAccessor.getClass()) { // same accessor kind
-                        result.put(traitAccessor, inheritedAccessor);
+        else if (traitMember is PropertyDescriptor) {
+            for (traitAccessor in traitMember.accessors) {
+                for (inheritedAccessor in (copy as PropertyDescriptor).accessors) {
+                    if (inheritedAccessor.javaClass == traitAccessor.javaClass) { // same accessor kind
+                        result.put(traitAccessor, inheritedAccessor)
                     }
                 }
             }
         }
-        return result;
+        return result
     }
 
-    @NotNull
-    public static ClassDescriptor getSuperClassBySuperTypeListEntry(@NotNull KtSuperTypeListEntry specifier, @NotNull BindingContext bindingContext) {
-        KotlinType superType = bindingContext.get(BindingContext.TYPE, specifier.getTypeReference());
-        assert superType != null : "superType should not be null: " + specifier.getText();
+    @JvmStatic
+    fun getSuperClassBySuperTypeListEntry(specifier: KtSuperTypeListEntry, bindingContext: BindingContext): ClassDescriptor {
+        val superType = bindingContext.get(BindingContext.TYPE, specifier.typeReference!!)
+                        ?: error("superType should not be null: ${specifier.text}")
 
-        ClassDescriptor superClassDescriptor = (ClassDescriptor) superType.getConstructor().getDeclarationDescriptor();
-        assert superClassDescriptor != null : "superClassDescriptor should not be null: " + specifier.getText();
-        return superClassDescriptor;
+        return superType.constructor.declarationDescriptor as? ClassDescriptor
+               ?: error("ClassDescriptor of superType should not be null: ${specifier.text}")
     }
 
-    private static boolean valueParameterClassesMatch(
-            @NotNull List<ValueParameterDescriptor> parameters,
-            @NotNull List<ClassifierDescriptor> classifiers
-    ) {
-        if (parameters.size() != classifiers.size()) return false;
-        for (int i = 0; i < parameters.size(); i++) {
-            ValueParameterDescriptor parameterDescriptor = parameters.get(i);
-            ClassifierDescriptor classDescriptor = classifiers.get(i);
-            if (!rawTypeMatches(parameterDescriptor.getType(), classDescriptor)) {
-                return false;
+    private fun valueParameterClassesMatch(
+            parameters: List<ValueParameterDescriptor>,
+            classifiers: List<ClassifierDescriptor>
+    ): Boolean {
+        if (parameters.size != classifiers.size) return false
+        for ((parameterDescriptor, classDescriptor) in parameters.zip(classifiers)) {
+            if (!rawTypeMatches(parameterDescriptor.type, classDescriptor)) {
+                return false
             }
         }
-        return true;
+        return true
     }
 
-    private static boolean rawTypeMatches(KotlinType type, ClassifierDescriptor classifier) {
-        return type.getConstructor().equals(classifier.getTypeConstructor());
+    private fun rawTypeMatches(type: KotlinType, classifier: ClassifierDescriptor): Boolean =
+            type.constructor == classifier.typeConstructor
+
+    @JvmStatic
+    fun isEnumValueOfMethod(functionDescriptor: FunctionDescriptor): Boolean {
+        val methodTypeParameters = functionDescriptor.valueParameters
+        val nullableString = TypeUtils.makeNullable(functionDescriptor.builtIns.stringType)
+        return DescriptorUtils.ENUM_VALUE_OF == functionDescriptor.name
+               && methodTypeParameters.size == 1
+               && KotlinTypeChecker.DEFAULT.isSubtypeOf(methodTypeParameters[0].type, nullableString)
     }
 
-    public static boolean isEnumValueOfMethod(@NotNull FunctionDescriptor functionDescriptor) {
-        List<ValueParameterDescriptor> methodTypeParameters = functionDescriptor.getValueParameters();
-        KotlinType nullableString = TypeUtils.makeNullable(DescriptorUtilsKt.getBuiltIns(functionDescriptor).getStringType());
-        return DescriptorUtils.ENUM_VALUE_OF.equals(functionDescriptor.getName())
-               && methodTypeParameters.size() == 1
-               && KotlinTypeChecker.DEFAULT.isSubtypeOf(methodTypeParameters.get(0).getType(), nullableString);
-    }
-
-    @Nullable
-    public static Integer getLineNumberForElement(@NotNull PsiElement statement, boolean markEndOffset) {
-        PsiFile file = statement.getContainingFile();
-        if (file instanceof KtFile) {
-            if (KtPsiFactoryKt.getDoNotAnalyze((KtFile) file) != null) {
-                return null;
-            }
+    @JvmStatic
+    fun getLineNumberForElement(statement: PsiElement, markEndOffset: Boolean): Int? {
+        val file = statement.containingFile
+        if (file is KtFile && file.doNotAnalyze != null) {
+            return null
         }
 
-        if (statement instanceof KtConstructorDelegationReferenceExpression && statement.getTextLength() == 0) {
+        if (statement is KtConstructorDelegationReferenceExpression && statement.textLength == 0) {
             // PsiElement for constructor delegation reference is always generated, so we shouldn't mark it's line number if it's empty
-            return null;
+            return null
         }
 
-        Document document = file.getViewProvider().getDocument();
-        return document != null ? document.getLineNumber(markEndOffset ? statement.getTextRange().getEndOffset() : statement.getTextOffset()) + 1 : null;
+        val document = file.viewProvider.document
+        return document?.getLineNumber(if (markEndOffset) statement.textRange.endOffset else statement.textOffset)?.plus(1)
     }
 }
