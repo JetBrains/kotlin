1f115801aa3c38c94239909f06ddcc4a4ebf04a8
Valentin Kipyatkov <valentin@jetbrains.com>
2016-04-22 11:48:10 +0300

    Supported other patterns for mapNotNull (cherry picked from commit 8c7cdf1)

---
+++ idea/testData/intentions/loopToCallChain/mapNotNull.kt
@@ -0,0 +1,9 @@
+// WITH_RUNTIME
+// INTENTION_TEXT: "Replace with 'mapNotNull{}.mapTo(){}'"
+fun foo(list: List<String?>, target: MutableList<String>) {
+    <caret>for (s in list) {
+        val length = s?.length
+        if (length == null) continue
+        target.add(length.toString())
+    }
+}
