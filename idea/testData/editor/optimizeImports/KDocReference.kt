6405316facefd13bf091a7e8bf62b0f4c3c15fa0
Valentin Kipyatkov <valentin@jetbrains.com>
2016-04-25 20:40:40 +0300

    KT-10770 IDEA cannot keep import if a type is only referenced by kdoc.
     #KT-10770 Fixed

---
+++ idea/testData/editor/optimizeImports/KDocReference.kt
@@ -0,0 +1,12 @@
+// RUNTIME
+import kotlin.concurrent.fixedRateTimer
+import kotlin.concurrent.read
+import java.util.ArrayList
+
+/**
+ * Reference to [ArrayList]
+ * Reference to [fixedRateTimer]
+ * Reference to [kotlin.concurrent.read]
+ */
+fun foo() {
+}
