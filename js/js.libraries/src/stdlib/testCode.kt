bdfe1891532e46f0896f48b98a7e0dd54cbef259
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-04-25 19:44:10 +0300

    kotlin-test: add assertFails overload with message.

--- js/js.libraries/src/stdlib/testCode.kt
+++ js/js.libraries/src/stdlib/testCode.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package kotlin.test
 
 /**
@@ -14,7 +30,7 @@ public fun todo(block: () -> Any) {
  */
 @kotlin.internal.InlineOnly
 inline fun <reified T : Throwable> assertFailsWith(message: String? = null, noinline block: () -> Unit): T {
-    val exception = assertFails(block) // TODO: message (in 1.1)
+    val exception = assertFails(message, block)
     assertTrue(exception is T, (message?.let { "$it. " } ?: "") + "An exception thrown is not of the expected type: $exception")
     return exception as T
 }
