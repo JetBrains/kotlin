6fda0816f3cde8bce8b2a3bc42dbe4bc8e433782
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2016-04-27 07:34:38 +0200

    buildString with initial capacity for the StringBuilder instance
    this method helps save computation time when the developer knows or have a good
    educated guess about the size of the generated string and have to use control
    structures while filling the string builder
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>
    
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- libraries/stdlib/src/kotlin/text/StringBuilder.kt
+++ libraries/stdlib/src/kotlin/text/StringBuilder.kt
@@ -10,6 +10,12 @@ package kotlin.text
 public inline fun buildString(builderAction: StringBuilder.() -> Unit): String = StringBuilder().apply(builderAction).toString()
 
 /**
+ * Builds new string by populating newly created [StringBuilder] initialized with the given capacity using provided [builderAction] and then converting it to [String].
+ */
+@kotlin.internal.InlineOnly
+inline fun buildString(capacity : Int, builderAction: StringBuilder.() -> Unit): String = StringBuilder(capacity).apply(builderAction).toString()
+
+/**
  * Appends all arguments to the given [Appendable].
  */
 public fun <T : Appendable> T.append(vararg value: CharSequence?): T {
