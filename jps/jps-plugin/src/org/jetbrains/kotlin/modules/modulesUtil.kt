03cb2f04fd8c60d3beabed3fbf2f398dffb90947
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2016-04-28 22:47:42 +0300

    Fix NoSuchMethodError when try to access in test to internal member from production for gradle projects imported into IDEA 16 or higher
     #KT-11993 Fixed
    
    Original commit: ed0fa2c26cd11b01c9a8fcc2c34b460e7b19387d

--- jps/jps-plugin/src/org/jetbrains/kotlin/modules/modulesUtil.kt
+++ jps/jps-plugin/src/org/jetbrains/kotlin/modules/modulesUtil.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,17 @@
 package org.jetbrains.kotlin.modules
 
 import org.jetbrains.jps.incremental.ModuleBuildTarget
+import org.jetbrains.kotlin.jps.build.KotlinBuilderModuleScriptGenerator.getRelatedProductionModule
 
-fun TargetId(moduleBuildTarget: ModuleBuildTarget): TargetId =
-        TargetId(moduleBuildTarget.id, moduleBuildTarget.targetType.typeId)
+fun TargetId(moduleBuildTarget: ModuleBuildTarget): TargetId {
+    // Since IDEA 2016 each gradle source root is imported as a separate module.
+    // One gradle module X is imported as two JPS modules:
+    // 1. X-production with one production target;
+    // 2. X-test with one test target.
+    // This breaks kotlin code since internal members' names are mangled using module name.
+    // For example, a declaration of a function 'f' in 'X-production' becomes 'fXProduction', but a call 'f' in 'X-test' becomes 'fXTest()'.
+    // The workaround is to replace a name of such test target with the name of corresponding production module.
+    // See KT-11993.
+    val name = getRelatedProductionModule(moduleBuildTarget.module)?.name ?: moduleBuildTarget.id
+    return TargetId(name, moduleBuildTarget.targetType.typeId)
+}
