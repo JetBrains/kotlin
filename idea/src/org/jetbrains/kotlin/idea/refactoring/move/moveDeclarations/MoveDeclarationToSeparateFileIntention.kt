1adefff577aa864c60b2f04610f9ed714e2b69ee
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-04-29 18:18:44 +0300

    Do not request write action for MoveDeclarationToSeparateFileIntention (EA-81511)
    There's nothing but delegation to move refactoring. This started to fail in br146 because of this commit:
    https://github.com/JetBrains/intellij-community/commit/334772b7a8dfe3afb32665c09ff8e27781a01512

--- idea/src/org/jetbrains/kotlin/idea/refactoring/move/moveDeclarations/MoveDeclarationToSeparateFileIntention.kt
+++ idea/src/org/jetbrains/kotlin/idea/refactoring/move/moveDeclarations/MoveDeclarationToSeparateFileIntention.kt
@@ -58,6 +58,8 @@ class MoveDeclarationToSeparateFileIntention :
         return TextRange(startOffset, endOffset)
     }
 
+    override fun startInWriteAction() = false
+
     override fun applyTo(element: KtClassOrObject, editor: Editor?) {
         if (editor == null) throw IllegalArgumentException("This intention requires an editor")
         val file = element.getContainingKtFile()
