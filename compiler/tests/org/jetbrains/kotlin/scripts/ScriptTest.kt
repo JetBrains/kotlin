956bcf6d7ab69f119d08c659debaa5b75df53611
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2016-05-04 12:43:50 +0200

    Fix permgen problems with script launching by avoiding reflection as much as possible

--- compiler/tests/org/jetbrains/kotlin/scripts/ScriptTest.kt
+++ compiler/tests/org/jetbrains/kotlin/scripts/ScriptTest.kt
@@ -53,7 +53,7 @@ class ScriptTest {
     fun testStandardScriptWithParams() {
         val aClass = compileScript("fib_std.kts", StandardScriptDefinition)
         Assert.assertNotNull(aClass)
-        val anObj = KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!.kotlin, listOf("4", "comment"))
+        val anObj = KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!, listOf("4", "comment"))
         Assert.assertNotNull(anObj)
     }
 
@@ -61,7 +61,7 @@ class ScriptTest {
     fun testStandardScriptWithoutParams() {
         val aClass = compileScript("fib_std.kts", StandardScriptDefinition)
         Assert.assertNotNull(aClass)
-        val anObj = KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!.kotlin, emptyList())
+        val anObj = KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!, emptyList())
         Assert.assertNotNull(anObj)
     }
 
@@ -69,7 +69,7 @@ class ScriptTest {
     fun testScriptWithParamConversion() {
         val aClass = compileScript("fib.kts", SimpleParamsTestScriptDefinition(".kts", numIntParam()))
         Assert.assertNotNull(aClass)
-        val anObj = KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!.kotlin, listOf("4"))
+        val anObj = KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!, listOf("4"))
         Assert.assertNotNull(anObj)
     }
 
@@ -138,7 +138,7 @@ class ScriptTest {
         Assert.assertNotNull(aClass)
         var exceptionThrown = false
         try {
-            KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!.kotlin, emptyList())
+            KotlinToJVMBytecodeCompiler.tryConstructClassPub(aClass!!, emptyList())
         }
         catch (e: InvocationTargetException) {
             Assert.assertTrue(e.cause is IllegalStateException)
