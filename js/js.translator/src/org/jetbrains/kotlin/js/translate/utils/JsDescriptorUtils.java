50aa1b271fbf99fc3d1acc3868405262ef03c3a9
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-05-05 18:14:38 +0300

    JS/Inlining: when generating expression for default property accessor, mark this expression as side effect free, so optimizer has opportunity to move it

--- js/js.translator/src/org/jetbrains/kotlin/js/translate/utils/JsDescriptorUtils.java
+++ js/js.translator/src/org/jetbrains/kotlin/js/translate/utils/JsDescriptorUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -145,6 +145,10 @@ public final class JsDescriptorUtils {
 
     private static boolean isDefaultAccessor(@Nullable PropertyAccessorDescriptor accessorDescriptor) {
         return accessorDescriptor == null || accessorDescriptor.isDefault();
+    }
+
+    public static boolean sideEffectsPossible(@NotNull PropertyDescriptor property) {
+        return isDefaultAccessor(property.getGetter()) && !ModalityKt.isOverridableOrOverrides(property);
     }
 
     public static boolean isSimpleFinalProperty(@NotNull PropertyDescriptor propertyDescriptor) {
