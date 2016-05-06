e207b560097274be40ab29cd6db7befc9fc94c5a
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-05-06 16:16:44 +0300

    Support increment and '+=' on local delegated properties in JS backend

---
+++ js/js.translator/testData/delegateProperty/cases/localVarPlusAssign.kt
@@ -0,0 +1,20 @@
+//TODO: reuse same tests from JVM backend
+package foo
+
+import kotlin.reflect.KProperty
+
+class Delegate {
+    var inner = 1
+    operator fun getValue(t: Any?, p: KProperty<*>): Int = inner
+    operator fun setValue(t: Any?, p: KProperty<*>, i: Int) {
+        inner = i
+    }
+}
+
+fun box(): String {
+    var prop: Int by Delegate()
+    prop += 1
+    if (prop != 2) return "fail : $prop"
+    return "OK"
+}
+
