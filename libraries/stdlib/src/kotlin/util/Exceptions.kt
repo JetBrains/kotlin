9bfb226948c6860243b35182a656948a321102c5
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-05-06 17:28:27 +0300

    Drop inline-only extensions and hide non-inline-only ones that are available now as mapped methods of mapped builtins.  #KT-10887 Fixed

--- libraries/stdlib/src/kotlin/util/Exceptions.kt
+++ libraries/stdlib/src/kotlin/util/Exceptions.kt
@@ -1,33 +1,14 @@
 @file:kotlin.jvm.JvmMultifileClass
 @file:kotlin.jvm.JvmName("ExceptionsKt")
 @file:kotlin.jvm.JvmVersion
-@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
+@file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN", "DeprecatedCallableAddReplaceWith")
 package kotlin
 
-import java.io.PrintStream
-import java.io.PrintWriter
-
-/**
- * Prints the stack trace of this throwable to the standard output.
- */
-@kotlin.internal.InlineOnly
-public inline fun Throwable.printStackTrace(): Unit = (this as java.lang.Throwable).printStackTrace()
-
-/**
- * Prints the stack trace of this throwable to the specified [writer].
- */
-@kotlin.internal.InlineOnly
-public inline fun Throwable.printStackTrace(writer: PrintWriter): Unit = (this as java.lang.Throwable).printStackTrace(writer)
-
-/**
- * Prints the stack trace of this throwable to the specified [stream].
- */
-@kotlin.internal.InlineOnly
-public inline fun Throwable.printStackTrace(stream: PrintStream): Unit = (this as java.lang.Throwable).printStackTrace(stream)
 
 /**
  * Returns an array of stack trace elements representing the stack trace
  * pertaining to this throwable.
  */
+@Deprecated("Provided for binary compatibility", level = DeprecationLevel.HIDDEN)
 public val Throwable.stackTrace: Array<StackTraceElement>
-    get() = (this as java.lang.Throwable).stackTrace!!
+    get() = stackTrace!!
