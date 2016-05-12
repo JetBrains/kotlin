4ffa2eaf393b74846555fc46fcce683466c6409e
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-05-12 19:56:53 +0300

    Fix serialVersionUID for EmptyList after introducing RandomAccess interface. #KT-10794 (cherry picked from commit 6a07d5f)

--- libraries/stdlib/src/kotlin/collections/Collections.kt
+++ libraries/stdlib/src/kotlin/collections/Collections.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:kotlin.jvm.JvmMultifileClass
 @file:kotlin.jvm.JvmName("CollectionsKt")
 
@@ -17,6 +33,8 @@ internal object EmptyIterator : ListIterator<Nothing> {
 }
 
 internal object EmptyList : List<Nothing>, Serializable, RandomAccess {
+    private const val serialVersionUID: Long = -7390468764508069838L
+
     override fun equals(other: Any?): Boolean = other is List<*> && other.isEmpty()
     override fun hashCode(): Int = 1
     override fun toString(): String = "[]"
