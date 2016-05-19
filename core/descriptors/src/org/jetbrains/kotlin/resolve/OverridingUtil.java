543ae45eecfeb4f701c21dac559b299a1f0e9184
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2016-05-19 15:49:40 +0300

    Fixed type checking recursive problem.
    #KT-11995 Fixed

--- core/descriptors/src/org/jetbrains/kotlin/resolve/OverridingUtil.java
+++ core/descriptors/src/org/jetbrains/kotlin/resolve/OverridingUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,12 +73,7 @@ public class OverridingUtil {
     }
 
     @NotNull
-    public OverrideCompatibilityInfo isOverridableByIncludingReturnType(@NotNull CallableDescriptor superDescriptor, @NotNull CallableDescriptor subDescriptor) {
-        return isOverridableBy(superDescriptor, subDescriptor, null, true);
-    }
-
-    @NotNull
-    private OverrideCompatibilityInfo isOverridableBy(
+    public OverrideCompatibilityInfo isOverridableBy(
             @NotNull CallableDescriptor superDescriptor,
             @NotNull CallableDescriptor subDescriptor,
             @Nullable ClassDescriptor subClassDescriptor,
