1d8b70788567530ba26978ff7dc8b21caab3f4e8
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-05-19 14:07:24 +0300

    Revert "Quick fix for deprecated async syntax"
    This reverts commit afc1e24571d3f6ce4102ec16d6aff3686e537217.

--- idea/src/org/jetbrains/kotlin/idea/codeInsight/surroundWith/expression/KotlinParenthesesSurrounder.java
+++ idea/src/org/jetbrains/kotlin/idea/codeInsight/surroundWith/expression/KotlinParenthesesSurrounder.java
@@ -40,25 +40,18 @@ public class KotlinParenthesesSurrounder extends KotlinExpressionSurrounder {
 
     @Nullable
     @Override
-    public TextRange surroundExpression(@NotNull Project project, @NotNull Editor editor, @NotNull KtExpression expression) {
-        expression = surroundWithParentheses(expression);
-
-        CodeInsightUtilBase.forcePsiPostprocessAndRestoreElement(expression);
-
-        int offset = expression.getTextRange().getEndOffset();
-        return new TextRange(offset, offset);
-    }
-
-    @NotNull
-    public static KtExpression surroundWithParentheses(@NotNull KtExpression expression) {
-        KtParenthesizedExpression parenthesizedExpression =
-                (KtParenthesizedExpression) KtPsiFactoryKt.KtPsiFactory(expression).createExpression("(a)");
-
+    public TextRange surroundExpression( @NotNull Project project, @NotNull Editor editor, @NotNull KtExpression expression) {
+        KtParenthesizedExpression parenthesizedExpression = (KtParenthesizedExpression) KtPsiFactoryKt
+                .KtPsiFactory(expression).createExpression("(a)");
         KtExpression expressionWithoutParentheses = parenthesizedExpression.getExpression();
         assert expressionWithoutParentheses != null : "JetExpression should exists for " + parenthesizedExpression.getText() + " expression";
         expressionWithoutParentheses.replace(expression);
 
         expression = (KtExpression) expression.replace(parenthesizedExpression);
-        return expression;
+
+        CodeInsightUtilBase.forcePsiPostprocessAndRestoreElement(expression);
+
+        int offset = expression.getTextRange().getEndOffset();
+        return new TextRange(offset, offset);
     }
 }
