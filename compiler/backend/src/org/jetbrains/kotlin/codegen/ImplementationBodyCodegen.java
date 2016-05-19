c778af518b3d5cb941d72d1bab3cf0682d125b78
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-05-19 15:54:59 +0300

    isJvm6Interface -> isAnnotationOrJvm6Interface

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ImplementationBodyCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ImplementationBodyCodegen.java
@@ -1321,7 +1321,7 @@ public class ImplementationBodyCodegen extends ClassBodyCodegen {
     }
 
     private void generateTraitMethods() {
-        if (isJvm6Interface(descriptor, state)) return;
+        if (isAnnotationOrJvm6Interface(descriptor, state)) return;
 
         for (Map.Entry<FunctionDescriptor, FunctionDescriptor> entry : CodegenUtil.getNonPrivateTraitMethods(descriptor).entrySet()) {
             FunctionDescriptor traitFun = entry.getKey();
