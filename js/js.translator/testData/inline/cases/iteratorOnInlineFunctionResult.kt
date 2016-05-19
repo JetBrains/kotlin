db5baf7fa70824c57dcc01ceb72024c530c195f7
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-05-19 19:59:28 +0300

    KT-8386: pass mutable list to JsInvocation from intrinsic generator, so that inliner had a chance to replace arguments

---
+++ js/js.translator/testData/inline/cases/iteratorOnInlineFunctionResult.kt
@@ -0,0 +1,12 @@
+package foo
+
+inline fun bar(f: () -> Int): Array<Int> = arrayOf(f())
+
+fun box(): String {
+    val iterator = bar { 23 }.iterator()
+    assertEquals(true, iterator.hasNext())
+    assertEquals(23, iterator.next())
+    assertEquals(false, iterator.hasNext())
+
+    return "OK"
+}
