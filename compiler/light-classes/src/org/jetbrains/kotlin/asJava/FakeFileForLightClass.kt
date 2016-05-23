3cb38e7f02b3612d9f0741d0e70b3b39a57f86b2
Pavel V. Talanov <talanov.pavel@gmail.com>
2016-05-23 14:04:47 +0300

    Implement getLanguageLevel for FakeFileForLightClass
     #KT-12006 Fixed

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/FakeFileForLightClass.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/FakeFileForLightClass.kt
@@ -1,21 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +16,7 @@
 
 package org.jetbrains.kotlin.asJava
 
+import com.intellij.pom.java.LanguageLevel
 import com.intellij.psi.ClassFileViewProvider
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.impl.compiled.ClsFileImpl
@@ -57,4 +42,7 @@ open class FakeFileForLightClass(
         // Prevent access to compiled PSI
         // TODO: More complex traversal logic may be implemented when necessary
     }
+
+    // this should be equal to current compiler target language level
+    override fun getLanguageLevel() = LanguageLevel.JDK_1_6
 }
