5512043ed48f61e11d85df9a52690e725e6bdcfc
Dmitry Jemerov <yole@jetbrains.com>
2016-05-24 20:58:43 +0200

    FunctionsHighlightingVisitor: cleanup after J2K, remove overlaid highlighting of function calls

--- idea/testData/highlighter/NamedArguments.kt
+++ idea/testData/highlighter/NamedArguments.kt
@@ -3,5 +3,5 @@ fun <info textAttributesKey="KOTLIN_FUNCTION_DECLARATION">foo</info>(<info textA
 }
 
 fun <info textAttributesKey="KOTLIN_FUNCTION_DECLARATION">bar</info>() {
-    <info textAttributesKey="KOTLIN_PACKAGE_FUNCTION_CALL"><info textAttributesKey="KOTLIN_FUNCTION_CALL">foo</info></info>(1, <info textAttributesKey="KOTLIN_NAMED_ARGUMENT">p2 =</info> "")
+    <info textAttributesKey="KOTLIN_PACKAGE_FUNCTION_CALL">foo</info>(1, <info textAttributesKey="KOTLIN_NAMED_ARGUMENT">p2 =</info> "")
 }
