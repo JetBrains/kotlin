a8af91fa8f519222070418f71c1c32cf24e6746b
Dmitry Jemerov <yole@jetbrains.com>
2016-05-25 20:10:01 +0200

    fix PsiCheckerTestGenerated according to removed "property with backing field" highlighting

--- idea/testData/checker/infos/PropertiesWithBackingFields.kt
+++ idea/testData/checker/infos/PropertiesWithBackingFields.kt
@@ -5,9 +5,9 @@
 
     <error>val a : Int</error>
     <error>val b : Int</error> <info>get</info>
-    val <info>c</info> = 1
+    val c = 1
 
-    val <info>c1</info> = 1
+    val c1 = 1
       <info>get</info>
     val c2 : Int
         <info>get</info>() = 1
@@ -42,9 +42,9 @@
 
 <info>open</info> class Super(<warning>i</warning> : Int)
 
-class TestPCParameters(w : Int, <warning>x</warning> : Int, val <info>y</info> : Int, var <info>z</info> : Int) : Super(w) {
+class TestPCParameters(w : Int, <warning>x</warning> : Int, val y : Int, var z : Int) : Super(w) {
 
-  val <info>xx</info> = w
+  val xx = w
 
   <info>init</info> {
     w + 1
