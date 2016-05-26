b1189eff233cf640e1243788b82815e22a018245
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-05-26 14:10:26 +0300

    Make suspension calls generation more stable
    Instead of performing signature change during transformation
    it's convinient to make just when generating corresponding call,
    for example there is no need to think about default parameters as they just work as is
    
    See comment above replaceSuspensionFunctionViewWithRealDescriptor for clarification

--- compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/coroutineCodegenUtil.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/coroutines/coroutineCodegenUtil.kt
@@ -16,11 +16,20 @@
 
 package org.jetbrains.kotlin.codegen.coroutines
 
-import org.jetbrains.kotlin.coroutines.CONTINUATION_INTERFACE_FQ_NAME
-import org.jetbrains.kotlin.resolve.jvm.JvmClassName
-import org.jetbrains.org.objectweb.asm.Type
-
-val CONTINUATION_INTERFACE_ASM_TYPE = Type.getObjectType(JvmClassName.byFqNameWithoutInnerClasses(CONTINUATION_INTERFACE_FQ_NAME).internalName)
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.resolve.BindingTraceContext
+import org.jetbrains.kotlin.resolve.DelegatingBindingTrace
+import org.jetbrains.kotlin.resolve.calls.model.ExpressionValueArgument
+import org.jetbrains.kotlin.resolve.calls.model.MutableDataFlowInfoForArguments
+import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
+import org.jetbrains.kotlin.resolve.calls.model.ResolvedCallImpl
+import org.jetbrains.kotlin.resolve.calls.smartcasts.DataFlowInfo
+import org.jetbrains.kotlin.resolve.calls.tasks.TracingStrategy
+import org.jetbrains.kotlin.types.TypeConstructorSubstitution
+import org.jetbrains.kotlin.types.typeUtil.asTypeProjection
 
 // These classes do not actually exist at runtime
 val CONTINUATION_METHOD_ANNOTATION_DESC = "Lkotlin/ContinuationMethod;"
@@ -30,3 +39,65 @@ const val SUSPENSION_POINT_MARKER_NAME = "suspensionPoint"
 
 const val COROUTINE_CONTROLLER_FIELD_NAME = "controller"
 const val COROUTINE_LABEL_FIELD_NAME = "label"
+
+data class ResolvedCallWithRealDescriptor(val resolvedCall: ResolvedCall<*>, val fakeThisExpression: KtExpression)
+
+// Resolved calls to suspension function contain descriptors as they visible within coroutines:
+// E.g. `fun <V> await(f: CompletableFuture<V>): V` instead of `fun <V> await(f: CompletableFuture<V>, machine: Continuation<V>): Unit`
+// See `createCoroutineSuspensionFunctionView` and it's usages for clarification
+// But for call generation it's convenient to have `machine` (continuation) parameter/argument within resolvedCall.
+// So this function returns resolved call with descriptor looking like `fun <V> await(f: CompletableFuture<V>, machine: Continuation<V>): V`
+// and fake `this` expression that used as argument for second parameter
+fun ResolvedCall<*>.replaceSuspensionFunctionViewWithRealDescriptor(
+        project: Project
+): ResolvedCallWithRealDescriptor? {
+    val function = candidateDescriptor as? FunctionDescriptor ?: return null
+    if (!function.isSuspend) return null
+
+    val initialSignatureDescriptor = function.initialSignatureDescriptor ?: return null
+    val newCandidateDescriptor =
+            initialSignatureDescriptor.createCustomCopy {
+                // Here we know that last parameter should be Continuation<T> where T is return type
+                setReturnType(it.valueParameters.last().type.arguments.single().type)
+            }
+
+    val newCall = ResolvedCallImpl(
+            call,
+            newCandidateDescriptor,
+            dispatchReceiver, extensionReceiver, explicitReceiverKind,
+            null, DelegatingBindingTrace(BindingTraceContext().bindingContext, "Temporary trace for unwrapped suspension function"),
+            TracingStrategy.EMPTY, MutableDataFlowInfoForArguments.WithoutArgumentsCheck(DataFlowInfo.EMPTY))
+
+    this.valueArguments.forEach {
+        newCall.recordValueArgument(newCandidateDescriptor.valueParameters[it.key.index], it.value)
+    }
+
+    val psiFactory = KtPsiFactory(project)
+    val arguments = psiFactory.createCallArguments("(this)").arguments.single()
+    val thisExpression = arguments.getArgumentExpression()!!
+    newCall.recordValueArgument(
+            newCandidateDescriptor.valueParameters.last(),
+            ExpressionValueArgument(arguments))
+
+    newCall.setResultingSubstitutor(
+            TypeConstructorSubstitution.createByParametersMap(typeArguments.mapValues { it.value.asTypeProjection() }).buildSubstitutor())
+
+    return ResolvedCallWithRealDescriptor(newCall, thisExpression)
+}
+
+private fun FunctionDescriptor.createCustomCopy(
+        copySettings: FunctionDescriptor.CopyBuilder<out FunctionDescriptor>.(FunctionDescriptor) -> FunctionDescriptor.CopyBuilder<out FunctionDescriptor>
+): FunctionDescriptor {
+
+    val newOriginal =
+            if (original !== this)
+                original.createCustomCopy(copySettings)
+            else
+                null
+
+    val result = newCopyBuilder().copySettings(this).setOriginal(newOriginal).build()!!
+
+    result.overriddenDescriptors = this.overriddenDescriptors.map { it.createCustomCopy(copySettings) }
+
+    return result
+}
