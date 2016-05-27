9fa0623fdca835bdeafc77b193a0d1a7bd57395d
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-05-27 18:31:25 +0300

    "Make abstract" is no longer applicable to object / enum entry members #KT-3797 Fixed

---
+++ idea/testData/quickfix/abstract/makeObjectMemberAbstract.kt
@@ -0,0 +1,10 @@
+// "Make 'foo' abstract" "false"
+// ACTION: Add function body
+// ACTION: Make internal
+// ACTION: Make private
+// ERROR: Function 'foo' without a body must be abstract
+
+
+object O {
+    <caret>fun foo()
+}
