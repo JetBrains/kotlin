ddbf7906e1845cae85d326c1fc401a63103dc8a4
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-05-27 16:24:15 +0300

    Shade in kotlin-android-extensions

--- compiler/compiler.pro
+++ compiler/compiler.pro
@@ -149,3 +149,6 @@ messages/**)
     *** SKIP_DEBUG;
     *** SKIP_FRAMES;
 }
+
+# for kotlin-android-extensions in maven
+-keep class com.intellij.openapi.module.ModuleServiceManager { public *; }
--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/CleanUpBuildListener.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/CleanUpBuildListener.kt
@@ -130,7 +130,7 @@ class CompilerServicesCleanup(private var pluginClassLoader: ClassLoader?) {
     }
 
     private fun stopZipFileCache() {
-        callVoidStaticMethod("com.intellij.openapi.util.io.ZipFileCache", "stopBackgroundThread")
+        callVoidStaticMethod("org.jetbrains.kotlin.com.intellij.openapi.util.io.ZipFileCache", "stopBackgroundThread")
         log.kotlinDebug("ZipFileCache finished successfully")
     }
 
@@ -161,7 +161,7 @@ class CompilerServicesCleanup(private var pluginClassLoader: ClassLoader?) {
 
     private fun cleanJarCache() {
         log.kotlinDebug("Clean JAR cache")
-        callVoidStaticMethod("com.intellij.openapi.vfs.impl.ZipHandler", "clearFileAccessorCache")
+        callVoidStaticMethod("org.jetbrains.kotlin.com.intellij.openapi.vfs.impl.ZipHandler", "clearFileAccessorCache")
         log.kotlinDebug("JAR cache cleared")
     }
 }
