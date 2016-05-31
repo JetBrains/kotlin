5f07ae9328ffa33001dce5fcd575b364f0510b43
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-05-31 14:47:27 +0300

    KT-2752: fix old tests, add test for clashing between class and its companion object

---
+++ js/js.translator/testData/nameClashes/cases/classAndCompanionObjectMembers.kt
@@ -0,0 +1,23 @@
+package foo
+
+class A {
+    fun foo() = 23
+
+    val bar = 123
+
+    companion object {
+        fun foo() = 42
+
+        val bar = 142
+    }
+}
+
+fun box(): String {
+    assertEquals(23, A().foo())
+    assertEquals(42, A.foo())
+
+    assertEquals(123, A().bar)
+    assertEquals(142, A.bar)
+
+    return "OK"
+}
