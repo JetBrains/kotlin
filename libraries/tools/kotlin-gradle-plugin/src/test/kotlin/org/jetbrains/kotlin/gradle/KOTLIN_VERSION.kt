11d971c4bf85af00db1af45c0547842d9c03fd2b
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-06-01 21:16:43 +0300

    Refactoring: extract kotlin_version constant for gradle tests

---
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/KOTLIN_VERSION.kt
@@ -0,0 +1,5 @@
+package org.jetbrains.kotlin.gradle
+
+// constant is held in separate file intentionally for better discoverability
+// and to prevent vcs conflicts (its value is 1.1-* in master branch, 0.1-* in 1.0.x branches)
+const val KOTLIN_VERSION = "1.1-SNAPSHOT"
