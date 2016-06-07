af1b26b993552c6956063072f8d7bff224603734
Kirill <kirill.rakhman@gmail.com>
2016-06-07 21:59:42 +0300

    Add quickfix tests for HasPlatformTypeInspection

---
+++ idea/testData/quickfix/platformTypesInspection/nestedNoAssertRuntime.kt
@@ -0,0 +1,8 @@
+// "Add non-null asserted (!!) call" "false"
+// ACTION: Convert function to property
+// ACTION: Convert to block body
+// ACTION: Create test
+// ACTION: Specify return type explicitly
+// ACTION: Specify return type explicitly
+
+fun foo<caret>() = arrayOf(java.lang.String.valueOf(1))
