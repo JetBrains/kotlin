89ed9e902553e1f5b838d4a7bbcfd7d8b2448fe4
Dmitry Petrov <Dmitry.Petrov@jetbrains.com>
2016-06-08 16:46:14 +0300

    Tests for type alias diagnostics: type parameter bound violation in type alias constructor

---
+++ compiler/testData/diagnostics/tests/typealias/boundViolationInTypeAliasConstructor.kt
@@ -0,0 +1,17 @@
+class Num<T: Number>(val x: T)
+
+typealias N<T> = Num<T>
+typealias N2<T> = N<T>
+
+val x1 = Num<<!UPPER_BOUND_VIOLATED!>String<!>>("")
+val x2 = N<<!UPPER_BOUND_VIOLATED!>String<!>>("")
+val x3 = N2<<!UPPER_BOUND_VIOLATED!>String<!>>("")
+
+class TColl<T, C : Collection<T>>
+
+typealias TC<T, C> = TColl<T, C>
+typealias TC2<T, C> = TC<T, C>
+
+val y1 = TColl<Any, <!UPPER_BOUND_VIOLATED!>Any<!>>()
+val y2 = TC<Any, <!UPPER_BOUND_VIOLATED!>Any<!>>()
+val y3 = TC2<Any, <!UPPER_BOUND_VIOLATED!>Any<!>>()
