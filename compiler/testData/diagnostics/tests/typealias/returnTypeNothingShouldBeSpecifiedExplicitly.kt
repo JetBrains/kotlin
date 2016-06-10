74d1dd64a4dbd863089153df4c58cd20a7ba2bc7
Dmitry Petrov <Dmitry.Petrov@jetbrains.com>
2016-06-10 13:44:55 +0300

    'Nothing' as function return type and property type can't be abbreviated.

---
+++ compiler/testData/diagnostics/tests/typealias/returnTypeNothingShouldBeSpecifiedExplicitly.kt
@@ -0,0 +1,5 @@
+typealias N = Nothing
+
+fun <!ABBREVIATED_NOTHING_RETURN_TYPE!>testFun<!>(): N = null!!
+val <!ABBREVIATED_NOTHING_PROPERTY_TYPE!>testVal<!>: N = null!!
+val <!ABBREVIATED_NOTHING_PROPERTY_TYPE!>testValWithGetter<!>: N get() = null!!
