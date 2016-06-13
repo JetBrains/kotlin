b1734920b6dc918048b00007c4c5abbb28bc2728
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-06-13 15:56:41 +0300

    Surround with null check : applicable also for TYPE_MISMATCH

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -7438,6 +7438,12 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
             KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("idea/testData/quickfix/surroundWithNullCheck"), Pattern.compile("^([\\w\\-_]+)\\.kt$"), true);
         }
 
+        @TestMetadata("argumentNullable.kt")
+        public void testArgumentNullable() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("idea/testData/quickfix/surroundWithNullCheck/argumentNullable.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("chainedUnsafeCall.kt")
         public void testChainedUnsafeCall() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("idea/testData/quickfix/surroundWithNullCheck/chainedUnsafeCall.kt");
