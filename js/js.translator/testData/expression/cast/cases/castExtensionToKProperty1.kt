2e6a48576d6e1921a013f8bf81fa79f0c2da279a
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-06-14 16:38:17 +0300

    KT-12417: add support of RTTI against K(Mutable)Property* types

---
+++ js/js.translator/testData/expression/cast/cases/castExtensionToKProperty1.kt
@@ -0,0 +1,17 @@
+package foo
+
+import kotlin.reflect.KProperty1
+
+class A
+
+val A.x: Int
+    get() = 23
+
+fun box(): String {
+    assertEquals(true, (A::x as Any) is KProperty1<*, *>)
+    assertEquals(23, ((A::x as Any) as KProperty1<A, Any>)(A()))
+    assertEquals(false, (23 as Any) is KProperty1<*, *>)
+    assertEquals(false, ({ A().x } as Any) is KProperty1<*, *>)
+
+    return "OK"
+}
