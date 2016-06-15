fff193d7e5ee05dfdc972612a0f00d851a725646
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-06-15 23:44:34 +0300

    Provide specialized stdlib function implementations depending on current JRE version #KT-8254
    (cherry picked from commit 25974be3f8c945c08c460bb692b6606a9116af6a)

---
+++ libraries/stdlib/src/kotlin/internal/PlatformImplementations.kt
@@ -0,0 +1,56 @@
+@file:JvmVersion
+package kotlin.internal
+
+import java.io.Closeable
+
+internal open class PlatformImplementations {
+
+    public open fun closeSuppressed(instance: Closeable, cause: Throwable) {
+        try {
+            instance.close()
+        } catch (closeException: Throwable) {
+            // eat the closeException as we are already throwing the original cause
+            // and we don't want to mask the real exception;
+            // on Java 7 we should call
+            // e.addSuppressed(closeException)
+        }
+    }
+
+    companion object {
+        @JvmField
+        @InlineExposed
+        val INSTANCE: PlatformImplementations = run {
+            val version = getJavaVersion()
+            try {
+                if (version >= 0x10008)
+                    return@run Class.forName("kotlin.internal.JDK8PlatformImplementations").newInstance() as PlatformImplementations
+            } catch (e: ClassNotFoundException) {}
+
+            try {
+                if (version >= 0x10007)
+                    return@run Class.forName("kotlin.internal.JDK7PlatformImplementations").newInstance() as PlatformImplementations
+            } catch (e: ClassNotFoundException) {}
+
+            PlatformImplementations()
+        }
+
+        private fun getJavaVersion(): Int {
+            val default = 0x10006
+            val version = System.getProperty("java.version") ?: return default
+            val firstDot = version.indexOf('.')
+            if (firstDot < 0) return default
+            var secondDot = version.indexOf('.', firstDot + 1)
+            if (secondDot < 0) secondDot = version.length
+
+            val firstPart = version.substring(0, firstDot)
+            val secondPart = version.substring(firstDot + 1, secondDot)
+            return try {
+                firstPart.toInt() * 0x10000 + secondPart.toInt()
+            } catch (e: NumberFormatException) {
+                default
+            }
+        }
+
+    }
+}
+
