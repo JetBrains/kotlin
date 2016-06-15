dc57005d2410c1b5872264944736dff74398b49d
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-06-16 00:44:58 +0300

    Adjust exposed visibility of platform specific implementations.

--- libraries/stdlib/src/kotlin/internal/PlatformImplementations.kt
+++ libraries/stdlib/src/kotlin/internal/PlatformImplementations.kt
@@ -20,44 +20,42 @@ internal open class PlatformImplementations {
     public open fun getMatchResultNamedGroup(matchResult: MatchResult, name: String): MatchGroup? {
         throw UnsupportedOperationException("Retrieving groups by name is not supported on this platform.")
     }
+}
 
-    companion object {
-        @JvmField
-        @InlineExposed
-        val INSTANCE: PlatformImplementations = run {
-            val version = getJavaVersion()
-            try {
-                if (version >= 0x10008)
-                    return@run Class.forName("kotlin.internal.JDK8PlatformImplementations").newInstance() as PlatformImplementations
-            } catch (e: ClassNotFoundException) {}
+@kotlin.internal.InlineExposed
+internal fun platformCloseSuppressed(instance: Closeable, cause: Throwable) = IMPLEMENTATIONS.closeSuppressed(instance, cause)
 
-            try {
-                if (version >= 0x10007)
-                    return@run Class.forName("kotlin.internal.JDK7PlatformImplementations").newInstance() as PlatformImplementations
-            } catch (e: ClassNotFoundException) {}
 
-            PlatformImplementations()
-        }
+@JvmField
+internal val IMPLEMENTATIONS: PlatformImplementations = run {
+    val version = getJavaVersion()
+    try {
+        if (version >= 0x10008)
+            return@run Class.forName("kotlin.internal.JRE8PlatformImplementations").newInstance() as PlatformImplementations
+    } catch (e: ClassNotFoundException) {}
 
-        private fun getJavaVersion(): Int {
-            val default = 0x10006
-            val version = System.getProperty("java.version") ?: return default
-            val firstDot = version.indexOf('.')
-            if (firstDot < 0) return default
-            var secondDot = version.indexOf('.', firstDot + 1)
-            if (secondDot < 0) secondDot = version.length
+    try {
+        if (version >= 0x10007)
+            return@run Class.forName("kotlin.internal.JRE7PlatformImplementations").newInstance() as PlatformImplementations
+    } catch (e: ClassNotFoundException) {}
 
-            val firstPart = version.substring(0, firstDot)
-            val secondPart = version.substring(firstDot + 1, secondDot)
-            return try {
-                firstPart.toInt() * 0x10000 + secondPart.toInt()
-            } catch (e: NumberFormatException) {
-                default
-            }
-        }
+    PlatformImplementations()
+}
 
+private fun getJavaVersion(): Int {
+    val default = 0x10006
+    val version = System.getProperty("java.version") ?: return default
+    val firstDot = version.indexOf('.')
+    if (firstDot < 0) return default
+    var secondDot = version.indexOf('.', firstDot + 1)
+    if (secondDot < 0) secondDot = version.length
+
+    val firstPart = version.substring(0, firstDot)
+    val secondPart = version.substring(firstDot + 1, secondDot)
+    return try {
+        firstPart.toInt() * 0x10000 + secondPart.toInt()
+    } catch (e: NumberFormatException) {
+        default
     }
-
-
 }
 
