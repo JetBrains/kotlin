25974be3f8c945c08c460bb692b6606a9116af6a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-06-15 23:44:34 +0300

    Provide specialized stdlib function implementations depending on current JRE version #KT-8254

---
+++ libraries/stdlib/jre8/src/kotlin/internal/JRE8PlatformImplementations.kt
@@ -0,0 +1,5 @@
+package kotlin.internal
+
+@Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER", "CANNOT_OVERRIDE_INVISIBLE_MEMBER")
+internal open class JRE8PlatformImplementations : JRE7PlatformImplementations() {
+}
