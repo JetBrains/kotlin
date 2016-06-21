e1ebaae0770171466969b097d487c07a2db1bb70
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-06-21 19:41:59 +0300

    Add tests for 'jdk' compiler option for CLI, maven and gradle.
    (cherry picked from commit 13016cae930bae858cb54f86599022fcff5c8a08)

--- compiler/tests/org/jetbrains/kotlin/cli/CliTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/cli/CliTestGenerated.java
@@ -265,6 +265,12 @@ public class CliTestGenerated extends AbstractCliTest {
             doJvmTest(fileName);
         }
 
+        @TestMetadata("warningJdkWithNoJdk.args")
+        public void testWarningJdkWithNoJdk() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/cli/jvm/warningJdkWithNoJdk.args");
+            doJvmTest(fileName);
+        }
+
         @TestMetadata("warningsInDummy.args")
         public void testWarningsInDummy() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/cli/jvm/warningsInDummy.args");
