49d5368f419e42d8eaa9830b0a9ade43fe7095a7
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-06-28 10:10:58 +0300

    Small changes in tests to support them on Android

--- compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/SpecialFiles.java
+++ compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/SpecialFiles.java
@@ -90,6 +90,7 @@ public class SpecialFiles {
 
         excludedFiles.add("nestedClasses.kt"); // additional nested class in 'Thread' class on Android
         excludedFiles.add("kt12200Const.kt"); // no 'modifiers' field in 'java.lang.reflect.Field' class
+        excludedFiles.add("testImmutableRemove.kt"); // need to compile against java 6
 
         excludedFiles.add("closureOfInnerLocalClass.kt"); // KT-8120
         excludedFiles.add("closureWithSelfInstantiation.kt"); // KT-8120
