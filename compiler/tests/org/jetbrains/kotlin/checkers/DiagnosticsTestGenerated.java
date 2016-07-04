d45330c184b15485a635fc85d2a0676e96be7ca8
Dmitry Petrov <Dmitry.Petrov@jetbrains.com>
2016-07-04 08:43:55 +0300

    Fix CCE when typealias is used in a class reference ('TA::class')

--- compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -19647,6 +19647,12 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("classReference.kt")
+            public void testClassReference() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/typealias/classReference.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("conflictingProjections.kt")
             public void testConflictingProjections() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/typealias/conflictingProjections.kt");
@@ -19728,6 +19734,12 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
             @TestMetadata("localTypeAliasConstructor.kt")
             public void testLocalTypeAliasConstructor() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/typealias/localTypeAliasConstructor.kt");
+                doTest(fileName);
+            }
+
+            @TestMetadata("methodReference.kt")
+            public void testMethodReference() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/typealias/methodReference.kt");
                 doTest(fileName);
             }
 
