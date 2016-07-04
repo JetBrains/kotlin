a1fa8ebe816b584ed504356aa43d595ee30cb7b0
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-07-04 12:25:20 +0300

    Minor. Move KotlinFrontEndException to frontend module

--- compiler/frontend/src/org/jetbrains/kotlin/util/KotlinFrontEndException.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/util/KotlinFrontEndException.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-package org.jetbrains.kotlin.utils
+package org.jetbrains.kotlin.util
 
 class KotlinFrontEndException(message: String, cause: Throwable) : RuntimeException(message, cause)
