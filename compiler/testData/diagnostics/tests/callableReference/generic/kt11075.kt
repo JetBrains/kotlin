8df12e5933b709f3126b2814c5345d1896833a2e
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-07-05 18:06:10 +0300

    Fix test data for KT-11075
    References to object members are not supported in 1.0.x branch
    
     #KT-11075 Fixed

--- compiler/testData/diagnostics/tests/callableReference/generic/kt11075.kt
+++ compiler/testData/diagnostics/tests/callableReference/generic/kt11075.kt
@@ -1,10 +1,10 @@
 // KT-11075 NONE_APPLICABLE reported for callable reference to an overloaded generic function with expected type provided
 
-object TestCallableReferences {
+class Test {
     fun <A> foo(x: A) = x
     fun <B> foo(x: List<B>) = x
 
-    fun test0(): (String) -> String = TestCallableReferences::foo
+    fun test0(): (Test, String) -> String = Test::foo
 
-    fun <T> test1(): (List<T>) -> List<T> = TestCallableReferences::foo
+    fun <T> test1(): (Test, List<T>) -> List<T> = Test::foo
 }
