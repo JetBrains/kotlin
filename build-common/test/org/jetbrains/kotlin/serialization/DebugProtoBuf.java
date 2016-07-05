845178cefcc5a43b720b99e2058ad09423e5317a
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-07-05 15:31:27 +0300

    Change default visibility for TypeAlias from 'internal' to 'public'
    Since public type aliases will supposedly be more common than internal ones, it
    makes sense to save on writing flags for the former rather than the latter

--- build-common/test/org/jetbrains/kotlin/serialization/DebugProtoBuf.java
+++ build-common/test/org/jetbrains/kotlin/serialization/DebugProtoBuf.java
@@ -22172,7 +22172,7 @@ public final class DebugProtoBuf {
           ExtendableMessageOrBuilder<TypeAlias> {
 
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -22181,7 +22181,7 @@ public final class DebugProtoBuf {
      */
     boolean hasFlags();
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -22455,7 +22455,7 @@ public final class DebugProtoBuf {
     public static final int FLAGS_FIELD_NUMBER = 1;
     private int flags_;
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -22466,7 +22466,7 @@ public final class DebugProtoBuf {
       return ((bitField0_ & 0x00000001) == 0x00000001);
     }
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -22635,7 +22635,7 @@ public final class DebugProtoBuf {
     }
 
     private void initFields() {
-      flags_ = 0;
+      flags_ = 6;
       name_ = 0;
       typeParameter_ = java.util.Collections.emptyList();
       underlyingType_ = org.jetbrains.kotlin.serialization.DebugProtoBuf.Type.getDefaultInstance();
@@ -22881,7 +22881,7 @@ public final class DebugProtoBuf {
 
       public Builder clear() {
         super.clear();
-        flags_ = 0;
+        flags_ = 6;
         bitField0_ = (bitField0_ & ~0x00000001);
         name_ = 0;
         bitField0_ = (bitField0_ & ~0x00000002);
@@ -23137,9 +23137,9 @@ public final class DebugProtoBuf {
       }
       private int bitField0_;
 
-      private int flags_ ;
+      private int flags_ = 6;
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -23150,7 +23150,7 @@ public final class DebugProtoBuf {
         return ((bitField0_ & 0x00000001) == 0x00000001);
       }
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -23161,7 +23161,7 @@ public final class DebugProtoBuf {
         return flags_;
       }
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -23175,7 +23175,7 @@ public final class DebugProtoBuf {
         return this;
       }
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -23184,7 +23184,7 @@ public final class DebugProtoBuf {
        */
       public Builder clearFlags() {
         bitField0_ = (bitField0_ & ~0x00000001);
-        flags_ = 0;
+        flags_ = 6;
         onChanged();
         return this;
       }
@@ -24642,7 +24642,7 @@ public final class DebugProtoBuf {
       "ararg_element_type\030\004 \001(\0132(.org.jetbrains" +
       ".kotlin.serialization.Type\022\036\n\026vararg_ele",
       "ment_type_id\030\006 \001(\005*\005\010d\020\310\001\"\201\003\n\tTypeAlias\022" +
-      "\020\n\005flags\030\001 \001(\005:\0010\022\022\n\004name\030\002 \002(\005B\004\210\265\030\001\022I\n" +
+      "\020\n\005flags\030\001 \001(\005:\0016\022\022\n\004name\030\002 \002(\005B\004\210\265\030\001\022I\n" +
       "\016type_parameter\030\003 \003(\01321.org.jetbrains.ko" +
       "tlin.serialization.TypeParameter\022A\n\017unde" +
       "rlying_type\030\004 \001(\0132(.org.jetbrains.kotlin" +
--- core/deserialization/src/org/jetbrains/kotlin/serialization/ProtoBuf.java
+++ core/deserialization/src/org/jetbrains/kotlin/serialization/ProtoBuf.java
@@ -17012,7 +17012,7 @@ public final class ProtoBuf {
             ExtendableMessageOrBuilder<TypeAlias> {
 
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -17021,7 +17021,7 @@ public final class ProtoBuf {
      */
     boolean hasFlags();
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -17259,7 +17259,7 @@ public final class ProtoBuf {
     public static final int FLAGS_FIELD_NUMBER = 1;
     private int flags_;
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -17270,7 +17270,7 @@ public final class ProtoBuf {
       return ((bitField0_ & 0x00000001) == 0x00000001);
     }
     /**
-     * <code>optional int32 flags = 1 [default = 0];</code>
+     * <code>optional int32 flags = 1 [default = 6];</code>
      *
      * <pre>
      *hasAnnotations
@@ -17427,7 +17427,7 @@ public final class ProtoBuf {
     }
 
     private void initFields() {
-      flags_ = 0;
+      flags_ = 6;
       name_ = 0;
       typeParameter_ = java.util.Collections.emptyList();
       underlyingType_ = org.jetbrains.kotlin.serialization.ProtoBuf.Type.getDefaultInstance();
@@ -17644,7 +17644,7 @@ public final class ProtoBuf {
 
       public Builder clear() {
         super.clear();
-        flags_ = 0;
+        flags_ = 6;
         bitField0_ = (bitField0_ & ~0x00000001);
         name_ = 0;
         bitField0_ = (bitField0_ & ~0x00000002);
@@ -17822,9 +17822,9 @@ public final class ProtoBuf {
       }
       private int bitField0_;
 
-      private int flags_ ;
+      private int flags_ = 6;
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -17835,7 +17835,7 @@ public final class ProtoBuf {
         return ((bitField0_ & 0x00000001) == 0x00000001);
       }
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -17846,7 +17846,7 @@ public final class ProtoBuf {
         return flags_;
       }
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -17860,7 +17860,7 @@ public final class ProtoBuf {
         return this;
       }
       /**
-       * <code>optional int32 flags = 1 [default = 0];</code>
+       * <code>optional int32 flags = 1 [default = 6];</code>
        *
        * <pre>
        *hasAnnotations
@@ -17869,7 +17869,7 @@ public final class ProtoBuf {
        */
       public Builder clearFlags() {
         bitField0_ = (bitField0_ & ~0x00000001);
-        flags_ = 0;
+        flags_ = 6;
         
         return this;
       }
