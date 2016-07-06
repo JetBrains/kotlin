aedca4eea9d5e9905f6be2de9a3e97a0800342b3
Dmitry Jemerov <yole@jetbrains.com>
2016-07-06 19:10:32 +0200

    to restore pre-1.0.3 highlighting behavior, inherit "extension function" and "extension property" colors from "static method" and "static field"  #KT-12937 Fixed

--- idea/testData/highlighter/JavaTypes.kt
+++ idea/testData/highlighter/JavaTypes.kt
@@ -1,2 +1,3 @@
 <info textAttributesKey="KOTLIN_ANNOTATION">@SuppressWarnings</info> class <info textAttributesKey="KOTLIN_CLASS">TheClass</info> : <info textAttributesKey="KOTLIN_TRAIT">Runnable</info>, <info textAttributesKey="KOTLIN_CONSTRUCTOR">Thread</info>() {
+    val <info textAttributesKey="KOTLIN_INSTANCE_PROPERTY">x</info> = <info textAttributesKey="KOTLIN_EXTENSION_PROPERTY">id</info>
 }
