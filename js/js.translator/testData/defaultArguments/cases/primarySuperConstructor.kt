61ddd39f7eb6381c4838e022f42860b1e3732145
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-08 20:06:08 +0300

    KT-13043: fix translation of super call from secondary constructor when base constructor has default arguments

---
+++ js/js.translator/testData/defaultArguments/cases/primarySuperConstructor.kt
@@ -0,0 +1,16 @@
+package foo
+
+open class Base(a: Int, b: Int = 3) {
+    val c: Int
+
+    init {
+        c = a + b
+    }
+}
+
+class Derived(a: Int) : Base(a)
+
+fun box(): String {
+    assertEquals(5, Derived(2).c)
+    return "OK"
+}
