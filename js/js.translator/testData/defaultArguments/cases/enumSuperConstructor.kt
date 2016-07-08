00e925549f1f5a3bbdf4c3285b97649c6e035b45
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-08 20:06:08 +0300

    KT-13043: fix translation of super call from secondary constructor when base constructor has default arguments

---
+++ js/js.translator/testData/defaultArguments/cases/enumSuperConstructor.kt
@@ -0,0 +1,16 @@
+package foo
+
+enum class A {
+    B(2);
+
+    val c: Int
+
+    constructor (a: Int, b: Int = 3) {
+        c = a + b
+    }
+}
+
+fun box(): String {
+    assertEquals(5, A.B.c)
+    return "OK"
+}
