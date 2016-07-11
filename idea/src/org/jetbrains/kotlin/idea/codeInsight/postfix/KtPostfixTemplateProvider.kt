93e9a3efdb37dfd1edf4c07e75eb4d32c32c0680
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-07-11 11:17:58 +0300

    Introduce var/val postfix templates
      #KT-4710 In Progress

--- idea/src/org/jetbrains/kotlin/idea/codeInsight/postfix/KtPostfixTemplateProvider.kt
+++ idea/src/org/jetbrains/kotlin/idea/codeInsight/postfix/KtPostfixTemplateProvider.kt
@@ -24,6 +24,7 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.intentions.negate
+import org.jetbrains.kotlin.idea.refactoring.introduce.introduceVariable.KotlinIntroduceVariableHandler
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.parentsWithSelf
 import org.jetbrains.kotlin.resolve.calls.callUtil.getType
@@ -41,7 +42,9 @@ class KtPostfixTemplateProvider : PostfixTemplateProvider {
             KtNotNullPostfixTemplate("nn"),
             KtIsNullPostfixTemplate,
             KtWhenExpressionPostfixTemplate,
-            KtTryPostfixTemplate
+            KtTryPostfixTemplate,
+            KtIntroduceVariablePostfixTemplate("val"),
+            KtIntroduceVariablePostfixTemplate("var")
     )
 
     override fun isTerminalSymbol(currentChar: Char) = currentChar == '.' || currentChar == '!'
@@ -59,6 +62,19 @@ private object KtNotPostfixTemplate : NotPostfixTemplate(
         KtPostfixTemplatePsiInfo,
         createExpressionSelector { it.isBoolean() }
 )
+
+private class KtIntroduceVariablePostfixTemplate(
+        val kind: String
+) : PostfixTemplateWithExpressionSelector(kind, "$kind name = expression", createExpressionSelector()) {
+    override fun expandForChooseExpression(expression: PsiElement, editor: Editor) {
+        KotlinIntroduceVariableHandler.doRefactoring(
+                expression.project, editor, expression as KtExpression,
+                isVar = kind == "var",
+                occurrencesToReplace = null,
+                onNonInteractiveFinish = null
+        )
+    }
+}
 
 internal object KtPostfixTemplatePsiInfo : PostfixTemplatePsiInfo() {
     override fun createExpression(context: PsiElement, prefix: String, suffix: String) =
