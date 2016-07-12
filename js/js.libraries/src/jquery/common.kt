8c932bd06a030a480bd5599d9eec36f0f0706313
rocoty <oksandum@gmail.com>
2016-07-12 13:09:56 +0200

    remove useless imports

--- js/js.libraries/src/jquery/common.kt
+++ js/js.libraries/src/jquery/common.kt
@@ -3,8 +3,6 @@ package jquery
 import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Window
-import kotlin.browser.document
-import kotlin.browser.window
 
 @native
 public class JQuery() {
