8c2e61604ed1c3073d826bf56e13b62b5b4f2e22
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2016-07-12 13:45:21 +0300

    Test for "Debugger: "Step over" dives into recursive call" (KT-12924)
    Fixed in 7992df7b931b76d64800d89890dd50bc079049da
    
    (cherry picked from commit 78e2f05)
    
     #KT-12924 Fixed

---
+++ idea/testData/debugger/tinyApp/src/stepping/stepOver/stepOverInlineFunWithRecursionCall.kt
@@ -0,0 +1,21 @@
+package stepOverInlineFunWithRecursionCall
+
+fun foo(v: Int): Int {
+    if (v == 2) {
+        //Breakpoint!
+        inlineCall { foo(1) }                                           // 1, 2 (lambda)
+    }
+
+    return 2 // This line should be visited                             // 3
+}
+
+
+fun main(args: Array<String>) {
+    foo(2)                                                              // 4
+}
+
+inline fun inlineCall(l: () -> Unit) {
+    l()
+}
+
+// STEP_OVER: 3
