e9ccc480b09dfd672122603a79f9c0da56876171
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-07-13 19:38:37 +0300

    Docs: improve docs for Closeable.use and AutoCloseable.use

--- libraries/stdlib/jre7/src/kotlin/Standard.kt
+++ libraries/stdlib/jre7/src/kotlin/Standard.kt
@@ -1,6 +1,16 @@
 @file:JvmName("StandardJRE7Kt")
 package kotlin
 
+/**
+ * Executes the given [block] function on this resource and then closes it down correctly whether an exception
+ * is thrown or not.
+ *
+ * In case if the resource is being closed due to an exception occurred in [block], and the closing also fails with an exception,
+ * the latter is added to the [suppressed][java.lang.Throwable.addSuppressed] exceptions of the former.
+ *
+ * @param block a function to process this [AutoCloseable] resource.
+ * @return the result of [block] function invoked on this resource.
+ */
 @Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
 @kotlin.internal.InlineOnly
 public inline fun <T : AutoCloseable?, R> T.use(block: (T) -> R): R {
