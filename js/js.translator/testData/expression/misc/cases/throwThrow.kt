f8b96a1c6272a850a48b2b282c63f8b35bcf769a
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-07-14 20:01:52 +0300

    JS: add tests to prove that KT-8003, KT-8318 and KT-12157 are no longer reproducible

---
+++ js/js.translator/testData/expression/misc/cases/throwThrow.kt
@@ -0,0 +1,16 @@
+package foo
+
+fun test() {
+    throw throw Exception("catch me")
+}
+
+fun box(): String {
+    try {
+        test()
+        error("exception not thrown")
+    }
+    catch (e: Exception) {
+        assertEquals("catch me", e.message)
+    }
+    return "OK"
+}
