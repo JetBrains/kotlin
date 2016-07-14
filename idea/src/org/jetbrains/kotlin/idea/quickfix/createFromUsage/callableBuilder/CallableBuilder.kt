82bdd1e42b8b7a23f6d836a2d3c0f771c844f05a
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2016-07-14 20:30:24 +0300

    Minor: Convert CallablePlacement to sealed class (cherry picked from commit 47073b5)

--- idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/callableBuilder/CallableBuilder.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/createFromUsage/callableBuilder/CallableBuilder.kt
@@ -125,9 +125,9 @@ class CallableBuilderConfiguration(
         val enableSubstitutions: Boolean = true
 )
 
-interface CallablePlacement {
-    class WithReceiver(val receiverTypeCandidate: TypeCandidate): CallablePlacement
-    class NoReceiver(val containingElement: PsiElement): CallablePlacement
+sealed class CallablePlacement {
+    class WithReceiver(val receiverTypeCandidate: TypeCandidate): CallablePlacement()
+    class NoReceiver(val containingElement: PsiElement): CallablePlacement()
 }
 
 class CallableBuilder(val config: CallableBuilderConfiguration) {
@@ -243,7 +243,7 @@ class CallableBuilder(val config: CallableBuilderConfiguration) {
                     val classDeclaration = receiverClassDescriptor?.let { DescriptorToSourceUtils.getSourceFromDescriptor(it) }
                     containingElement = if (!config.isExtension && classDeclaration != null) classDeclaration else config.currentFile
                 }
-                else -> throw IllegalArgumentException("Unexpected placement: $placement")
+                else -> throw IllegalArgumentException("Placement wan't initialized")
             }
             val receiverType = receiverClassDescriptor?.defaultType
 
