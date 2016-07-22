6ff91d13997093209cbe898dedc8dfbf414517ee
Valentin Kipyatkov <valentin@jetbrains.com>
2016-07-22 17:54:01 +0300

    KT-12793 Code completion doesn't suggest abstract protected extension methods
     #KT-12793 Fixed

--- idea/idea-completion/tests/org/jetbrains/kotlin/idea/completion/test/JvmBasicCompletionTestGenerated.java
+++ idea/idea-completion/tests/org/jetbrains/kotlin/idea/completion/test/JvmBasicCompletionTestGenerated.java
@@ -649,6 +649,12 @@ public class JvmBasicCompletionTestGenerated extends AbstractJvmBasicCompletionT
             doTest(fileName);
         }
 
+        @TestMetadata("ProtectedExtensionMethod.kt")
+        public void testProtectedExtensionMethod() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("idea/idea-completion/testData/basic/common/ProtectedExtensionMethod.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("QualifiedSuperMembers.kt")
         public void testQualifiedSuperMembers() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("idea/idea-completion/testData/basic/common/QualifiedSuperMembers.kt");
