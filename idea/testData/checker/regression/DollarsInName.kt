e51957a431bc817de76081497cb3f41ae7b1cd70
Pavel V. Talanov <talanov.pavel@gmail.com>
2016-07-25 16:29:42 +0300

    PsiChecker test for classes with dollars in name

---
+++ idea/testData/checker/regression/DollarsInName.kt
@@ -0,0 +1,15 @@
+class `A$B` {
+    class `$$$` {
+        class `$`
+    }
+}
+
+object `$$$$B$C`
+
+fun f() {
+    fun g() {
+        class `A$B` {
+            inner class `C$D`
+        }
+    }
+}
