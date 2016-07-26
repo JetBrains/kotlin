328868ef7842ac438d06d4af5fc024d576d48a31
MaximZaitsev <AnterKan1@gmail.com>
2016-07-26 11:33:44 +0300

    added compile of proto files and copy protokot lib src files

--- server/compileProto.sh
+++ server/compileProto.sh
@@ -1,1 +1,1 @@
-../proto/compiler/google/src/google/protobuf/compiler/kotlin/protoc --kotlin_out="./src/" --proto_path="../proto/server_car" ../proto/server_car/*.proto
+../proto/compiler/google/src/google/protobuf/compiler/kotlin/protoc --kotlin_out="./src/main/java" --proto_path="../proto/server_car" ../proto/server_car/*.proto
