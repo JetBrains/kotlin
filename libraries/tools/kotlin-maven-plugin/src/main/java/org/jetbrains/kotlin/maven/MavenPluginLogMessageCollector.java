3c002625c6d556b84de1e4e4e8518f16e3363af8
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-07-26 21:22:13 +0300

    KT-13211 KotlinCompileMojoBase could provide better compilation failure info

--- libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/MavenPluginLogMessageCollector.java
+++ libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/MavenPluginLogMessageCollector.java
@@ -16,23 +16,38 @@
 
 package org.jetbrains.kotlin.maven;
 
+import com.intellij.openapi.util.Pair;
+import kotlin.collections.CollectionsKt;
+import kotlin.jvm.functions.Function1;
 import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.compiler.CompilerMessage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation;
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity;
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class MavenPluginLogMessageCollector implements MessageCollector {
     private final Log log;
-    private boolean hasErrors = false;
+    private final ArrayList<Pair<CompilerMessageLocation, String>> collectedErrors = new ArrayList<Pair<CompilerMessageLocation, String>>();
 
     public MavenPluginLogMessageCollector(Log log) {
-        this.log = log;
+        this.log = checkNotNull(log, "log shouldn't be null");
     }
 
     @Override
     public boolean hasErrors() {
-        return hasErrors;
+        return !collectedErrors.isEmpty();
+    }
+
+    @NotNull
+    public List<Pair<CompilerMessageLocation, String>> getCollectedErrors() {
+        return Collections.unmodifiableList(collectedErrors);
     }
 
     @Override
@@ -45,12 +60,43 @@ public class MavenPluginLogMessageCollector implements MessageCollector {
         if (CompilerMessageSeverity.VERBOSE.contains(severity)) {
             log.debug(text);
         } else if (CompilerMessageSeverity.ERRORS.contains(severity)) {
-            hasErrors = true;
+            collectedErrors.add(new Pair<CompilerMessageLocation, String>(location, message));
             log.error(text);
         } else if (severity == CompilerMessageSeverity.INFO) {
             log.info(text);
         } else {
             log.warn(text);
         }
+    }
+
+    public void throwKotlinCompilerException() throws KotlinCompilationFailureException {
+        throw new KotlinCompilationFailureException(
+                CollectionsKt.map(getCollectedErrors(), new Function1<Pair<CompilerMessageLocation, String>, CompilerMessage>() {
+                    @Override
+                    public CompilerMessage invoke(Pair<CompilerMessageLocation, String> pair) {
+                        CompilerMessageLocation location = pair.getFirst();
+                        String message = pair.getSecond();
+                        String lineContent = location.getLineContent();
+                        int lineContentLength = lineContent == null ? 0 : lineContent.length();
+
+                        return new CompilerMessage(
+                                location.getPath(),
+                                CompilerMessage.Kind.ERROR,
+                                fixLocation(location.getLine()),
+                                fixLocation(location.getColumn()),
+                                fixLocation(location.getLine()),
+                                Math.min(fixLocation(location.getColumn()), lineContentLength),
+                                message
+                        );
+                    }
+                })
+        );
+    }
+
+    private static int fixLocation(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        return n;
     }
 }
