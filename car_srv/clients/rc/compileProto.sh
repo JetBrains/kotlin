5ad2e643557475049b56ff357f75e31fbc16caa1
MaximZaitsev <AnterKan1@gmail.com>
2016-07-28 15:58:20 +0300

    add compile to new proto files

--- car_srv/clients/rc/compileProto.sh
+++ car_srv/clients/rc/compileProto.sh
@@ -1,1 +1,1 @@
-../../../proto/compiler/google/src/google/protobuf/compiler/kotlin/protoc --kotlin_out="./src/main/java" --proto_path="../../../proto/server_car" ../../../proto/server_car/direction.proto
+../../../proto/compiler/google/src/google/protobuf/compiler/kotlin/protoc --kotlin_out="./src/main/java" --proto_path="../../../proto/server_car" ../../../proto/server_car/direction.proto ../../../proto/server_car/rc_session.proto
