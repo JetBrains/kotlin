0e82058cd5ba48b70e6e60b9608b2ef344d2a17a
shiraji <isogai.shiraji@gmail.com>
2016-08-01 01:47:19 +0900

    Fix a problem with analyzing the same constructor multiple times

--- idea/src/org/jetbrains/kotlin/idea/intentions/RemoveUnnecessaryLateinitIntention.kt
+++ idea/src/org/jetbrains/kotlin/idea/intentions/RemoveUnnecessaryLateinitIntention.kt
@@ -25,7 +25,10 @@ import org.jetbrains.kotlin.idea.caches.resolve.analyzeFully
 import org.jetbrains.kotlin.idea.conversion.copy.range
 import org.jetbrains.kotlin.idea.inspections.IntentionBasedInspection
 import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtBinaryExpression
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.psiUtil.getNonStrictParentOfType
 import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCallWithAssert
 import org.jetbrains.kotlin.resolve.source.getPsi
@@ -39,18 +42,37 @@ class RemoveUnnecessaryLateinitIntention : SelfTargetingRangeIntention<KtPropert
     override fun applicabilityRange(element: KtProperty): TextRange? {
         if (!element.hasModifier(KtTokens.LATEINIT_KEYWORD)) return null
         val ktClass = element.getNonStrictParentOfType<KtClass>() ?: return null
-        if (ktClass.getAnonymousInitializers().firstOrNull { hasAssignmentExpression(it.body?.children, element.name) } != null) return element.modifierList?.getModifier(KtTokens.LATEINIT_KEYWORD)?.range
+        val elementName = element.name ?: return null
+        if (ktClass.getAnonymousInitializers().firstOrNull { hasAssignmentExpression(it.body?.children, elementName) } != null) return createTextRangeOfLateinit(element)
         if (ktClass.hasPrimaryConstructor()) return null
         val secondaryConstructors = ktClass.getSecondaryConstructors()
-        return if (secondaryConstructors.all { hasAssignmentExpression(it.bodyExpression?.children, element.name) || hasAssignmentExpressionInDelegationCall(it, secondaryConstructors, element.name) }) element.modifierList?.getModifier(KtTokens.LATEINIT_KEYWORD)?.range else null
+        val secondaryConstructorsInfo = secondaryConstructors.mapIndexed { index, secondaryConstructor ->
+            val delegationCall = secondaryConstructor.getDelegationCall()
+            val hasAssignmentExpression = hasAssignmentExpression(secondaryConstructor.bodyExpression?.children, elementName)
+            if (!hasAssignmentExpression && !delegationCall.isCallToThis) return null
+            val resolvedCall = delegationCall.getResolvedCallWithAssert(delegationCall.analyzeFully())
+            val delegationCallIndex = secondaryConstructors.indexOfFirst { isSameKtParametersAndValueParameterDescriptor(it.valueParameters, resolvedCall.candidateDescriptor.valueParameters) }
+            SecondaryConstructorInfo(index, hasAssignmentExpression, delegationCallIndex)
+        }
+        return if (hasAssignmentExpressionForSecondaryConstructors(secondaryConstructorsInfo)) createTextRangeOfLateinit(element) else null
     }
 
-    private fun hasAssignmentExpressionInDelegationCall(targetSecondaryConstructor: KtSecondaryConstructor, secondaryConstructors: List<KtSecondaryConstructor>, variableName: String?): Boolean {
-        val delegationCall = targetSecondaryConstructor.getDelegationCall()
-        if (!delegationCall.isCallToThis) return false
-        val resolvedCall = delegationCall.getResolvedCallWithAssert(delegationCall.analyzeFully())
-        val delegatedConstructor = secondaryConstructors.singleOrNull { isSameKtParametersAndValueParameterDescriptor(it.valueParameters, resolvedCall.candidateDescriptor.valueParameters) } ?: return false
-        return hasAssignmentExpression(delegatedConstructor.bodyExpression?.children, variableName) || hasAssignmentExpressionInDelegationCall(delegatedConstructor, secondaryConstructors, variableName)
+    private fun hasAssignmentExpressionForSecondaryConstructors(secondaryConstructorsInfo: List<SecondaryConstructorInfo>): Boolean {
+        val isCalledIndex: MutableList<Int> = arrayListOf()
+        secondaryConstructorsInfo.forEach {
+            var currentSecondaryConstructor = it
+            while (!currentSecondaryConstructor.hasAssignmentExpression) {
+                if (isCalledIndex.contains(currentSecondaryConstructor.index)) return false
+                isCalledIndex.add(currentSecondaryConstructor.index)
+                currentSecondaryConstructor = secondaryConstructorsInfo.getOrNull(currentSecondaryConstructor.delegationCallIndex) ?: return false
+            }
+            isCalledIndex.clear()
+        }
+        return true
+    }
+
+    private fun createTextRangeOfLateinit(element: KtProperty): TextRange? {
+        return element.modifierList?.getModifier(KtTokens.LATEINIT_KEYWORD)?.range
     }
 
     private fun isSameKtParametersAndValueParameterDescriptor(ktParameters: List<KtParameter>, valueParameterDescriptor: List<ValueParameterDescriptor>): Boolean {
@@ -61,7 +83,7 @@ class RemoveUnnecessaryLateinitIntention : SelfTargetingRangeIntention<KtPropert
         return true
     }
 
-    private fun hasAssignmentExpression(elements: Array<PsiElement>?, variableName: String?): Boolean {
+    private fun hasAssignmentExpression(elements: Array<PsiElement>?, variableName: String): Boolean {
         return elements?.firstOrNull {
             it is KtBinaryExpression &&
             it.operationToken == KtTokens.EQ &&
@@ -73,3 +95,5 @@ class RemoveUnnecessaryLateinitIntention : SelfTargetingRangeIntention<KtPropert
         element.removeModifier(KtTokens.LATEINIT_KEYWORD)
     }
 }
+
+private data class SecondaryConstructorInfo(val index: Int, val hasAssignmentExpression: Boolean, val delegationCallIndex: Int)
