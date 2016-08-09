e5903f90f2dcf3f79679204ba8bc03b79e2e82c8
Valentin Kipyatkov <valentin@jetbrains.com>
2016-08-09 19:26:08 +0300

    Added test and asserts

---
+++ idea/testData/intentions/loopToCallChain/filterIndexed_merge4.kt
@@ -0,0 +1,11 @@
+// WITH_RUNTIME
+// INTENTION_TEXT: "Replace with 'filterIndexed{}.firstOrNull()'"
+fun foo(list: List<String>): String? {
+    var index2 = 0
+    <caret>for ((index1, s) in list.withIndex()) {
+        if (s.length > index1) continue
+        if (s.length < index2++) continue
+        return s
+    }
+    return null
+}
