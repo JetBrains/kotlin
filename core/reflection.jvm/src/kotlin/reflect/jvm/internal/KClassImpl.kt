9e6812408c53aca287d4bb796c33f9be7bde3465
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-08-09 15:42:42 +0300

    Make KClasses for primitives equal to KClasses for wrapper types
    Both primitive int and wrapper type java.lang.Integer are represented by the
    single type kotlin.Int in Kotlin, so inequality between the corresponding
    KClasses was confusing here. To keep the old behavior, one may call 'k1.java ==
    k2.java' instead of `k1 == k2`
    
     #KT-13462 Fixed
    
    (cherry picked from commit 5b1ee13db8dddf008218587ba08cf46ecb80d1c7)

--- core/reflection.jvm/src/kotlin/reflect/jvm/internal/KClassImpl.kt
+++ core/reflection.jvm/src/kotlin/reflect/jvm/internal/KClassImpl.kt
@@ -150,10 +150,10 @@ internal class KClassImpl<T : Any>(override val jClass: Class<T>) : KDeclaration
         get() = objectInstance_()
 
     override fun equals(other: Any?): Boolean =
-            other is KClassImpl<*> && jClass == other.jClass
+            other is KClassImpl<*> && javaObjectType == other.javaObjectType
 
     override fun hashCode(): Int =
-            jClass.hashCode()
+            javaObjectType.hashCode()
 
     override fun toString(): String {
         return "class " + classId.let { classId ->
