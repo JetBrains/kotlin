56869950c83298f4da6c2c8094550c3f394fe4ed
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-08-09 16:48:11 +0300

    Do not report USELESS_CAST when casting null to nullable (special case) (cherry picked from commit 687698d)

--- compiler/testData/diagnostics/tests/cast/NullableToNullable.kt
+++ compiler/testData/diagnostics/tests/cast/NullableToNullable.kt
@@ -1,5 +1,5 @@
 // From KT-13324: always succeeds
-val x = null <!USELESS_CAST!>as String?<!>
+val x = null as String?
 // From KT-260: sometimes succeeds
 fun foo(a: String?): Int? {
     val c = a as? Int?
