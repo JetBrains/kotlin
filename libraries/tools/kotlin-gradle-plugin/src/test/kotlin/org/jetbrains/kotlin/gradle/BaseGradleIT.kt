acef77a326e7f5805f82bdaac09c5cb09c37bf53
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-08-11 16:05:25 +0300

    Join paths to string for better assertion message in idea (cherry picked from commit c3c4e7d)

--- libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/BaseGradleIT.kt
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/BaseGradleIT.kt
@@ -7,6 +7,7 @@ import org.jetbrains.kotlin.gradle.util.createGradleCommand
 import org.jetbrains.kotlin.gradle.util.runProcess
 import org.junit.After
 import org.junit.AfterClass
+import org.junit.Assert
 import org.junit.Before
 import java.io.File
 import kotlin.test.*
@@ -216,10 +217,10 @@ abstract class BaseGradleIT {
         return map { it.canonicalFile.toRelativeString(project.projectDir) }
     }
 
-    fun CompiledProject.assertSameFiles(expected: Iterable<String>, actual: Iterable<String>, messagePrefix: String = ""): CompiledProject {
-        val expectedSet = expected.toSortedSet()
-        val actualSet = actual.toSortedSet()
-        assertTrue(actualSet == expectedSet, messagePrefix + "expected files: ${expectedSet.joinToString()}\n  != actual files: ${actualSet.joinToString()}")
+    fun CompiledProject.assertSameFiles(expected: Iterable<String>, actual: Iterable<String>, messagePrefix: String): CompiledProject {
+        val expectedSet = expected.toSortedSet().joinToString("\n")
+        val actualSet = actual.toSortedSet().joinToString("\n")
+        Assert.assertEquals(messagePrefix, expectedSet, actualSet)
         return this
     }
 
