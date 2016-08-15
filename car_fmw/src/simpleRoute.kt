c42e82b2093a0ed8dba8dcd921e230a823b4539d
e5l <leonid.stashevsky@gmail.com>
2016-08-15 10:41:34 +0300

    translator: send proto

--- car_fmw/src/simpleRoute.kt
+++ car_fmw/src/simpleRoute.kt
@@ -16,20 +16,8 @@ fun blink() {
 
 fun simpleRoute() {
     while (true) {
-        engine_forward()
         wait(PROGRAM_DURATION)
         blink()
-
-        engine_turn_right()
-        wait(PROGRAM_DURATION)
-        blink()
-
-        engine_backward()
-        wait(PROGRAM_DURATION)
-        blink()
-
-        engine_turn_left()
-        wait(PROGRAM_DURATION)
-        blink()
+        writeProto()
     }
 }
--- car_hw/src/communication.c
+++ car_hw/src/communication.c
@@ -9,9 +9,11 @@ void send_int(int n)
     int i = 0;
     char* buffer = (char*) &n;
 
+    VCP_put_char(0xAA);
     for (; i < sizeof(int); ++i) {
         VCP_put_char(buffer[i]);
     }
+    VCP_put_char(0xAA);
 }
 
 void send_buffer(int size, int pointer)
@@ -19,10 +21,12 @@ void send_buffer(int size, int pointer)
     int i = 0;
 
     send_int(size);
+    VCP_put_char(0xAA);
     char* buffer = (char*) pointer;
     for (; i < size; ++i) {
         VCP_put_char(buffer[i]);
     }
+    VCP_put_char(0xAA);
 }
 
 int receive_int()
