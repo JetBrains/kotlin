eea01f468b4103e5e96c4c42fd3abd4784dc9d9a
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-08-16 19:50:20 +0300

    Introduce KotlinGradleBuildServices that is created once per build

--- libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/AbstractKotlinAndroidGradleTests.kt
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/AbstractKotlinAndroidGradleTests.kt
@@ -43,6 +43,7 @@ abstract class AbstractKotlinAndroidGradleTests(
                     ":compileFlavor1DebugUnitTestKotlin",
                     "InternalDummyTest PASSED",
                     ":compileFlavor1DebugAndroidTestKotlin")
+            checkKotlinGradleBuildServices()
         }
 
         // Run the build second time, assert everything is up-to-date
@@ -69,6 +70,7 @@ abstract class AbstractKotlinAndroidGradleTests(
                     ":compileFlavor2Jnidebug",
                     ":compileFlavor1Release",
                     ":compileFlavor2Release")
+            checkKotlinGradleBuildServices()
         }
     }
 
