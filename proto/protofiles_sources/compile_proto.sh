ec61337e4bf4f8d9b519a35a913d8e89e90b341e
MaximZaitsev <AnterKan1@gmail.com>
2016-08-16 17:06:31 +0300

    corrected compile proto script

--- proto/protofiles_sources/compile_proto.sh
+++ proto/protofiles_sources/compile_proto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../proto/compiler/google/src/google/protobuf/compiler/kotlin/protoc --kotlin_out="./src/main/java" --proto_path="../proto/server_car" ../proto/server_car/*.proto
+../compiler/build/protoc --kotlin_out="./out/" --proto_path="./server_car" ./server_car/*.proto
