b1cc1ffbc9809f5a3cbbb95b77f6a609a1baf498
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-08-16 16:29:15 +0300

    Refactor loading kotlin version in gradle plugin (cherry picked from commit 845d174)

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPluginWrapper.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPluginWrapper.kt
@@ -3,23 +3,24 @@ package org.jetbrains.kotlin.gradle.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.file.FileResolver
+import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 import org.jetbrains.kotlin.gradle.internal.KotlinSourceSetProviderImpl
 import org.jetbrains.kotlin.gradle.tasks.AndroidTasksProvider
 import org.jetbrains.kotlin.gradle.tasks.KotlinTasksProvider
+import java.io.FileNotFoundException
+import java.util.*
 import javax.inject.Inject
 
 // TODO: simplify: the complicated structure is a leftover from dynamic loading of plugin core, could be significantly simplified now
 
 abstract class KotlinBasePluginWrapper(protected val fileResolver: FileResolver): Plugin<Project> {
     private val log = Logging.getLogger(this.javaClass)
+    protected val kotlinPluginVersion = loadKotlinVersionFromResource(log)
 
     override fun apply(project: Project) {
         // TODO: consider only set if if daemon or parallel compilation are enabled, though this way it should be safe too
         System.setProperty(org.jetbrains.kotlin.cli.common.KOTLIN_COMPILER_ENVIRONMENT_KEEPALIVE_PROPERTY, "true")
-
-        val kotlinPluginVersion = loadKotlinVersionFromResource(log)
-        project.extensions.extraProperties?.set("kotlin.gradle.plugin.version", kotlinPluginVersion)
 
         val plugin = getPlugin()
         plugin.apply(project)
@@ -34,16 +35,29 @@ abstract class KotlinBasePluginWrapper(protected val fileResolver: FileResolver)
 
 open class KotlinPluginWrapper @Inject constructor(fileResolver: FileResolver): KotlinBasePluginWrapper(fileResolver) {
     override fun getPlugin() =
-            KotlinPlugin(KotlinTasksProvider(), KotlinSourceSetProviderImpl(fileResolver))
+            KotlinPlugin(KotlinTasksProvider(), KotlinSourceSetProviderImpl(fileResolver), kotlinPluginVersion)
 }
 
 open class KotlinAndroidPluginWrapper @Inject constructor(fileResolver: FileResolver): KotlinBasePluginWrapper(fileResolver) {
     override fun getPlugin() =
-            KotlinAndroidPlugin(AndroidTasksProvider(), KotlinSourceSetProviderImpl(fileResolver))
+            KotlinAndroidPlugin(AndroidTasksProvider(), KotlinSourceSetProviderImpl(fileResolver), kotlinPluginVersion)
 }
 
 open class Kotlin2JsPluginWrapper @Inject constructor(fileResolver: FileResolver): KotlinBasePluginWrapper(fileResolver) {
     override fun getPlugin() =
-            Kotlin2JsPlugin(KotlinTasksProvider(), KotlinSourceSetProviderImpl(fileResolver))
+            Kotlin2JsPlugin(KotlinTasksProvider(), KotlinSourceSetProviderImpl(fileResolver), kotlinPluginVersion)
 }
 
+private fun Any.loadKotlinVersionFromResource(log: Logger): String {
+    log.kotlinDebug("Loading version information")
+    val props = Properties()
+    val propFileName = "project.properties"
+    val inputStream = javaClass.classLoader!!.getResourceAsStream(propFileName) ?:
+            throw FileNotFoundException("property file '$propFileName' not found in the classpath")
+
+    props.load(inputStream)
+
+    val projectVersion = props["project.version"] as String
+    log.kotlinDebug("Found project version [$projectVersion]")
+    return projectVersion
+}
