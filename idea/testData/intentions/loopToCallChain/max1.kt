61d1826980196e5e44ee21f504c64d9056aec0c9
Valentin Kipyatkov <valentin@jetbrains.com>
2016-08-17 23:31:12 +0300

    Better test

--- idea/testData/intentions/loopToCallChain/max1.kt
+++ idea/testData/intentions/loopToCallChain/max1.kt
@@ -5,9 +5,8 @@ fun getMaxLineWidth(lineCount: Int): Float {
     var max_width = 0.0f
     <caret>for (i in 0..lineCount - 1) {
         val width = getLineWidth(i)
-        if (width > max_width) {
+        if (width > max_width)
             max_width = width
-        }
     }
     return max_width
 }
