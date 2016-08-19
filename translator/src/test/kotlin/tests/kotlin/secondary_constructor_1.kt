df3fd4912e75a18ee378a25f1b9ee6ffd7a70748
Alexey Stepanov <alejes@yandex.ru>
2016-08-19 17:22:21 +0300

    translator: add class test for secondary constructor

--- translator/src/test/kotlin/tests/kotlin/secondary_constructor_1.kt
+++ translator/src/test/kotlin/tests/kotlin/secondary_constructor_1.kt
@@ -8,3 +8,21 @@ fun secondary_constructor_1(x: Int): Int {
     val cls = secondary_constructor_1_class(12, x)
     return cls.field1
 }
+
+class secondary_constructor_1_class2() {
+    var field1: IntArray
+
+    init {
+        field1 = IntArray(0)
+    }
+
+    constructor(x: Int) : this() {
+        this.field1 = this.field1.plus(x)
+    }
+}
+
+fun secondary_constructor_1_arrays(): Int {
+    val cls = secondary_constructor_1_class2(56)
+    println(cls.field1)
+    return cls.field1.size + cls.field1[0]
+}
