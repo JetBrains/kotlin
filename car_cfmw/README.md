faa63544485aa0707e30d06842848551988993a4
Eugene Batalov <eabatalov89@gmail.com>
2016-08-20 14:48:32 +0300

    car_hw: urm37 and rotating servo C driver
    + new C firmware with two car_sonar module
    usage examples also used as manual tests.

---
+++ car_cfmw/README.md
@@ -0,0 +1,2 @@
+Car firmware project for low level experimentation
+using C.
---
+++ car_hw/src/car_sonar.c
@@ -0,0 +1,64 @@
+#include <car_sonar.h>
+#include <car_time.h>
+#include <usart.h>
+
+/*
+ * Sonar reference:
+ * http://www.dfrobot.com/wiki/index.php?title=URM37_V4.0_Ultrasonic_Sensor_(SKU:SEN0001)
+ */
+
+static uint8_t sonar_degree(uint8_t degree)
+{
+    // urm37 accepts values [0;46]
+    // so we have 47 steps.
+    // But we don't use the last 46 value.
+    // It gives us angle > 180.
+    if (degree > 180)
+        degree = 180;
+    return degree / 4;
+}
+
+#define SONAR_CMD_GET_DIST 0x22
+
+static uint8_t sonar_cmd[4];
+static void sonar_snd_dist_req(uint8_t rot_degree)
+{
+    size_t i = 0;
+    sonar_cmd[0] = SONAR_CMD_GET_DIST;
+    sonar_cmd[1] = sonar_degree(rot_degree);
+    sonar_cmd[2] = sonar_cmd[3] = 0;
+
+    for (; i < 3; ++i)
+        sonar_cmd[3] += sonar_cmd[i];
+    usart_send_data(USART3_ID, sonar_cmd, 4);
+}
+
+static uint8_t sonar_resp[4];
+static uint16_t sonar_wait_dist_resp(void)
+{
+    size_t i = 0;
+    uint8_t sum = 0;
+
+    usart_rcv_data(USART3_ID, sonar_resp, 4);
+    sum = sonar_resp[0] + sonar_resp[1] + sonar_resp[2];
+    if ((sonar_resp[0] != SONAR_CMD_GET_DIST)
+            || (sum != sonar_resp[3]))
+        return CAR_SONAR_DIST_ERR;
+
+    return ((uint16_t)sonar_resp[1] << 8) + sonar_resp[2];
+}
+
+uint16_t car_sonar_get_dist(uint8_t rot_degree)
+{
+    sonar_snd_dist_req(rot_degree);
+    return sonar_wait_dist_resp();
+}
+
+void car_sonar_init(void)
+{
+    usart_init(USART3_ID);
+    // Sonar boots in ~2.2 secs
+    car_time_wait(3000);
+    // Some really needed magical tweak
+    sonar_snd_dist_req(0);
+}
