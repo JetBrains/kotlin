627c8641ee72637f5343ba545a186077cacee280
MaximZaitsev <AnterKan1@gmail.com>
2016-08-22 13:10:44 +0300

    make auto creating out dir in compile proto script

--- proto/protofiles_sources/compile_proto.sh
+++ proto/protofiles_sources/compile_proto.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+cd "$parent_path"
+mkdir out -p
 ../compiler/build/protoc --kotlin_out="./out/" --proto_path="./server_car" ./server_car/*.proto
