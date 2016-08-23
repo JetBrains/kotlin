c99d4e35df05d47a9c59953a10a134d223f4485c
Alexey Stepanov <alejes@yandex.ru>
2016-08-23 09:44:41 +0300

    translator: add big test for bytearray

--- translator/src/test/kotlin/tests/kotlin/bytearray_1.kt
+++ translator/src/test/kotlin/tests/kotlin/bytearray_1.kt
@@ -4,3 +4,20 @@ fun bytearray_1(x: Byte): Byte {
     val r = z.clone()
     return r.get(1)
 }
+
+fun bytearray_1_array(): Int {
+    val size = 256
+    val z= ByteArray(size)
+    var ind = 0
+    while(ind < size){
+        z[ind] = ind.toByte()
+        ind +=1
+    }
+    val newInstance = z.clone()
+    ind = 0
+    while(ind < size){
+        assert(newInstance[ind] == ind.toByte())
+        ind += 1
+    }
+    return 1
+}
