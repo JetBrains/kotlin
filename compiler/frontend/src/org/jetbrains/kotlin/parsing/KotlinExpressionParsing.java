8a1206d56f3d8c9df55c8dbdb23bb76c05fb1970
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2016-08-24 18:12:39 +0300

    Minor: remove warnings and refactoring
    (cherry picked from commit 043502f)

--- compiler/frontend/src/org/jetbrains/kotlin/parsing/KotlinExpressionParsing.java
+++ compiler/frontend/src/org/jetbrains/kotlin/parsing/KotlinExpressionParsing.java
@@ -1068,16 +1068,14 @@ public class KotlinExpressionParsing extends AbstractKotlinParsing {
             }
         }
 
-        if (!paramsFound) {
-            if (preferBlock) {
-                literal.drop();
-                parseStatements();
-                expect(RBRACE, "Expecting '}'");
-                literalExpression.done(BLOCK);
-                myBuilder.restoreNewlinesState();
+        if (!paramsFound && preferBlock) {
+            literal.drop();
+            parseStatements();
+            expect(RBRACE, "Expecting '}'");
+            literalExpression.done(BLOCK);
+            myBuilder.restoreNewlinesState();
 
-                return;
-            }
+            return;
         }
 
         PsiBuilder.Marker body = mark();
