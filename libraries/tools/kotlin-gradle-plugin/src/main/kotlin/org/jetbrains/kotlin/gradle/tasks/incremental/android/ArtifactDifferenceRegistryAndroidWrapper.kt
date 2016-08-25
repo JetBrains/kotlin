06bc681c85fd647f2c804520692e07eabce55522
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-08-25 18:37:14 +0300

    Fix compatibility with AS 2.2 gradle plugin
        #KT-13594 fixed
    (cherry picked from commit b857df2)

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/incremental/android/ArtifactDifferenceRegistryAndroidWrapper.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/incremental/android/ArtifactDifferenceRegistryAndroidWrapper.kt
@@ -16,36 +16,18 @@
 
 package org.jetbrains.kotlin.gradle.tasks.incremental.android
 
-import com.android.build.gradle.internal.variant.BaseVariantData
 import org.jetbrains.kotlin.gradle.tasks.ArtifactDifference
 import org.jetbrains.kotlin.gradle.tasks.ArtifactDifferenceRegistry
 import java.io.File
-import java.util.*
 
 // When lib is compiled, changes are associated with .aar files.
 // However when app is compiled, there is just .jar in classpath.
-// This class maps jar to aar via BaseVariantData
 internal class ArtifactDifferenceRegistryAndroidWrapper(
         private val registry: ArtifactDifferenceRegistry,
-        variantData: BaseVariantData<*>
+        private val jarToAarMapping: Map<File, File>
 ): ArtifactDifferenceRegistry by registry {
-    private val jarToLibraryArtifactMap: MutableMap<File, File> = HashMap()
-
-    init {
-        for (lib in variantData.variantDependency.libraries) {
-            jarToLibraryArtifactMap[lib.jarFile] = lib.bundle
-
-            // local dependencies are detected as changed by gradle, because they are seem to be
-            // rewritten every time when bundle changes
-            // when local dep will actually change, record for bundle will be removed from registry
-            for (localDep in lib.localDependencies) {
-                jarToLibraryArtifactMap[localDep.jarFile] = lib.bundle
-            }
-        }
-    }
-
     override fun get(artifact: File): Iterable<ArtifactDifference>? {
-        val mappedFile = jarToLibraryArtifactMap[artifact] ?: return null
+        val mappedFile = jarToAarMapping[artifact] ?: return null
         return registry[mappedFile]
     }
 }
