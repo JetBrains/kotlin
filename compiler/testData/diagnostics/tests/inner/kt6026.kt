cd717467f090b6b81588f587faed3d595d47a1c2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-08-25 19:48:57 +0300

    Nested classes are no more allowed inside any anonymous objects or local classes #KT-13510 Fixed (cherry picked from commit 5c3ec6c)

--- compiler/testData/diagnostics/tests/inner/kt6026.kt
+++ compiler/testData/diagnostics/tests/inner/kt6026.kt
@@ -1,7 +1,8 @@
 // KT-6026 Exception on instantiating a nested class in an anonymous object
 
 val oo = object {
-    class Nested
+    // Forbidden in KT-13510
+    <!NESTED_CLASS_NOT_ALLOWED!>class Nested<!>
 
     fun f1() = Nested(<!TOO_MANY_ARGUMENTS!>11<!>)
 }
