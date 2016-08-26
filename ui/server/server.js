63652dab0b97633f047ec8eaac4805d5e73126c8
dsavvinov <dsavvinov@gmail.com>
2016-08-26 16:10:44 +0300

    UI: Cleaned carkot/ui from trash-files

--- ui/server/server.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-http.createServer(function(request, response) {
-	response.writeHead(200, {"Content-type" : "text/plain"});
-	response.write("Hello World");
-  	response.end();
-}).listen(8888);
