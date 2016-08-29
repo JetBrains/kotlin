9127788f4a76f54b510885216286f1cf26493b35
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2016-08-29 21:13:17 +0300

    Kapt: Fix erasure() for executable types (KT-13629) (cherry picked from commit 461c29d)

---
+++ plugins/annotation-processing/testData/processors/Erasure2.kt
@@ -0,0 +1,14 @@
+abstract class Base<A> {
+    fun baseF(): A = null!!
+}
+
+@Deprecated("")
+class Test<T> : Base<Int>() {
+    fun a(): String = ""
+    fun b(i: String, b: CharSequence) {}
+    fun c(): Int = 5
+    fun d(): T = null!!
+    fun <D : Any> e(item: D): D = item
+    fun f(items: List<Map<String, Int>>, i: Int) {}
+    fun <D : String> g(item: D) {}
+}
