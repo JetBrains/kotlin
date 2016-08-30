819ec7babebc23ad2b2df12e04e97806c4e978f4
Valentin Kipyatkov <valentin@jetbrains.com>
2016-08-30 20:15:23 +0300

    Fixed bug with class using data class as type parameter in supers list (cherry picked from commit 06eea53)

--- idea/testData/findUsages/kotlin/conventions/components/extensionComponentFun.0.kt
+++ idea/testData/findUsages/kotlin/conventions/components/extensionComponentFun.0.kt
@@ -5,6 +5,13 @@ interface X
 
 class Y : X
 
+open class G<T> {
+    fun get(): T = TODO()
+}
+
+abstract class Z1 : List<X>
+abstract class Z2 : G<X>()
+
 operator fun X.component1(): Int = 0
 operator fun X.<caret>component2(): Int = 1
 
@@ -16,4 +23,9 @@ fun test() {
 
 fun Y.ext() {
     val (a, b) = this
+}
+
+fun g(z1: Z1, z2: Z2) {
+    val (x1, y1) = z1[0]
+    val (x2, y2) = z2.get()
 }
