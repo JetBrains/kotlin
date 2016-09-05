28112cf799ab0cee5ff155d4c9e2fd44dddd7b03
Valentin Kipyatkov <valentin@jetbrains.com>
2016-09-05 19:15:20 +0300

    Supported object value access expressions (cherry picked from commit 6d028fb)

--- idea/testData/findUsages/kotlin/conventions/invoke.0.kt
+++ idea/testData/findUsages/kotlin/conventions/invoke.0.kt
@@ -1,13 +1,21 @@
 // PSI_ELEMENT: org.jetbrains.kotlin.psi.KtNamedFunction
 // OPTIONS: usages
+package pack
 
-class B(val n: Int) {
+open class B(val n: Int) {
     operator fun <caret>invoke(i: Int){}
 }
+
+object Obj : B(0)
 
 fun f() = B(1)
 
 fun test() {
     f(1).invoke(2)
     f(2)(2)
+
+    val v = Obj
+    v(1)
+
+    listOf(pack.Obj)[0](1)
 }
