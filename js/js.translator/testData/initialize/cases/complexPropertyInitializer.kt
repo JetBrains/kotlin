d5f24582e460d8ecaceec57f011521a84070bd82
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-09-07 14:11:01 +0300

    KT-12868: if a property initializer is a complex expression that emits additional statements, emit these statements directly before assignment to the property, not at the beginning of constructor

---
+++ js/js.translator/testData/initialize/cases/complexPropertyInitializer.kt
@@ -0,0 +1,21 @@
+package foo
+
+fun f() {}
+
+class A(selector: Boolean, val y: Int) {
+    val x = if (selector) { f(); y } else 999
+    val z = if (selector) { f(); x + 1 } else 999
+}
+
+class B(selector: Boolean, val y: Int, val x: Int = if (selector) { f(); y } else { 999 })
+
+fun box(): String {
+    val a = A(true, 23)
+    if (a.x != 23) return "fail: wrong evaluation order for property initializer (1): ${a.x}"
+    if (a.z != 24) return "fail: wrong evaluation order for property initializer (2): ${a.z}"
+
+    val b = B(true, 23)
+    if (b.x != 23) return "fail: wrong evaluation order for default constructor arguments"
+
+    return "OK"
+}
