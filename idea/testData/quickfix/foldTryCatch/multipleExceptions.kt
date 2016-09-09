d8078fbdd190ddd3e5838d63a4fef2805c95ac57
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-09-09 18:13:16 +0300

    Quick-fix for reassignment in try / catch (fold to assignment) #KT-13778 Fixed (cherry picked from commit edacc7f)

---
+++ idea/testData/quickfix/foldTryCatch/multipleExceptions.kt
@@ -0,0 +1,18 @@
+// "Lift assignment out of 'try' expression" "true"
+// WITH_RUNTIME
+
+fun foo() {
+    val x: Int
+    try {
+        x = 1
+    }
+    catch (e: RuntimeException) {
+        <caret>x = 2
+    }
+    catch (e: Exception) {
+        x = 3
+    }
+    catch (e: Throwable) {
+        x = 4
+    }
+}
