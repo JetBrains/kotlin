42f5d4212b03634c6353308e1d62b9d8cf5bc985
shiraji <isogai.shiraji@gmail.com>
2016-09-09 07:47:47 +0300

    KT-13674 Add quickfix to remove the final upper bound  #KT-13674 Fixed (cherry picked from commit 7c3835b)

--- idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -411,6 +411,7 @@ class QuickFixRegistrar : QuickFixContributor {
         VALUE_PARAMETER_WITH_NO_TYPE_ANNOTATION.registerFactory(AddTypeAnnotationToValueParameterFix)
 
         FINAL_UPPER_BOUND.registerFactory(InlineTypeParameterFix)
+        FINAL_UPPER_BOUND.registerFactory(RemoveFinalUpperBoundFix)
 
         TYPE_PARAMETER_AS_REIFIED.registerFactory(AddReifiedToTypeParameterOfFunctionFix)
     }
