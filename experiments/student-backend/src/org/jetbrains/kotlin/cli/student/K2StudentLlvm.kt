7fd7e176b5908b52c3adbc94046f4cba4e9540f3
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2016-09-14 17:10:54 +0300

    student-backend: only take placers and picked modules from kotlin-ir. this commit to switch from imac to pro

---
+++ experiments/student-backend/src/org/jetbrains/kotlin/cli/student/K2StudentLlvm.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.kotlin.cli.student
+
+import org.jetbrains.kotlin.cli.common.CLICompiler
+import org.jetbrains.kotlin.cli.common.ExitCode
+
+/**
+ * Created by minamoto on 14/09/16.
+ */
+
+public class K2StudentLlvm : CLICompiler<K2StudentLlvmArguments> {
+    override fun createArguments(): K2StudentLlvmArguments {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setupPlatformSpecificArgumentsAndServices(configuration: CompilerConfiguration?, arguments: K2StudentLlvmArguments?, services: Services?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun doExecute(arguments: K2StudentLlvmArguments?, configuration: CompilerConfiguration?, rootDisposable: MediaDisposer.Disposable?): ExitCode {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
