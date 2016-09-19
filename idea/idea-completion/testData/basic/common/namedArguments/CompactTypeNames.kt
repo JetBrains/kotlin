c508f75de4ae5ddaba1c1caf9b55068b5505d5ee
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-09-20 01:09:53 +0300

    Test data: change fully qualified type so that it's available as type (and not an alias) in both jvm and js runtimes to have the same output.

--- idea/idea-completion/testData/basic/common/namedArguments/CompactTypeNames.kt
+++ idea/idea-completion/testData/basic/common/namedArguments/CompactTypeNames.kt
@@ -1,11 +1,9 @@
-import java.util.ArrayList
-
 val paramTest = 12
 
-fun small(paramFirst: ArrayList<String>, paramSecond: Comparable<kotlin.collections.List<kotlin.Any>>) {
+fun small(paramFirst: Sequence<String>, paramSecond: Comparable<kotlin.collections.List<kotlin.Any>>) {
 }
 
 fun test() = small(<caret>)
 
 // EXIST: {"lookupString":"paramSecond","tailText":" Comparable<List<Any>>","itemText":"paramSecond ="}
-// EXIST: {"lookupString":"paramFirst","tailText":" ArrayList<String>","itemText":"paramFirst ="}
+// EXIST: {"lookupString":"paramFirst","tailText":" Sequence<String>","itemText":"paramFirst ="}
