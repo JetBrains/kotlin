9fffcebc42a761a7ab840dd561342b7fc1a8579c
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-09-19 19:35:47 +0300

    Split generic code and example. Add RTTI, arrays and such.

--- experiments/placer/placer.cc
+++ experiments/placer/placer.cc
@@ -6,186 +6,23 @@
 #include <cassert>
 #include <cstddef>
 
-class Container {
- private:
-  uint8_t* data_;
-  uint8_t* current_;
-  int size_;
-  int ref_count_;
-
- public:
-  Container(int size)
-    : size_(size), ref_count_(1) {
-    data_ = reinterpret_cast<uint8_t*>(calloc(size_, 1));
-    current_ = data_;
-  }
-
-  ~Container() {
-    assert(ref_count_ == 0);
-    free(data_);
-  }
-
-  void* Place(int size) {
-    size += sizeof(Container*);
-    if (current_ + size > data_ + size_) {
-      return nullptr;
-    }
-    Container** result = reinterpret_cast<Container**>(current_);
-    *result = this;
-    current_ += size;
-    return result;
-  }
-
-  void AddRef() {
-    if (data_) {
-      //printf("addref %d\n", ref_count_);
-      ref_count_++;
-    }
-  }
-
-  void Release() {
-    if (data_) {
-      // printf("release %d\n", ref_count_);
-      ref_count_--;
-    }
-  }
-
-  void Dispose() {
-    // Destroy container ignoring non-zero refcount. Use with care.
-    ref_count_ = 0;
-    free(data_);
-    data_ = nullptr;
-    current_ = nullptr;
-  }
-};
-
-// Raw reference to data, meaning T*, invented only for cleaness of intentions.
-template <class T>
-class RawRef {
- private:
-  T* ptr_;
- public:
-  RawRef(T* ptr) : ptr_(ptr) {}
-  const T& get() const { return *ptr_; }
-  void set(const T& value) { *ptr_ = value; }
-};
-
-class AnyObjRef {
- protected:
-  void* ptr_;
-
-  explicit AnyObjRef(void* ptr) : ptr_(ptr) {
-    if (ptr_) {
-      container()->AddRef();
-    }
-  }
-
- public:
-  ~AnyObjRef() {
-    if (ptr_) {
-      container()->Release();
-    }
-  }
-
-  Container* container() {
-    return *reinterpret_cast<Container**>(ptr_);
-  }
-
-  template<typename M, int offset>
-  RawRef<M> at() const {
-    return RawRef<M>(
-      reinterpret_cast<M*>(reinterpret_cast<uint8_t*>(any_ref()) + offset));
-  }
-
-  void Assign(const AnyObjRef& other) {
-    // TODO: optimize for an important case where containers match?
-    if (ptr_) {
-      container()->Release();
-    }
-    ptr_ = other.ptr_;
-    if (ptr_) {
-      container()->AddRef();
-    }
-  }
-
-  uint8_t* any_ref() const {
-    if (!ptr_) return nullptr;
-    return reinterpret_cast<uint8_t*>(ptr_) + sizeof(Container*);
-  }
-
-  AnyObjRef any_obj_at(int offset) const {
-    assert(ptr_);
-    return AnyObjRef(*reinterpret_cast<void**>(any_ref() + offset));
-  }
-
-  bool null() const { return ptr_ == nullptr; }
-};
-
-// Object reference, adds reference counting in container.
-template <class T>
-class ObjRef : public AnyObjRef {
- private:
-  explicit ObjRef(void* ptr) : AnyObjRef(ptr) {}
-
-  T* ref() const {
-    if (!ptr_) return nullptr;
-    return reinterpret_cast<T*>(any_ref());
-  }
-
- public:
-  ObjRef(const ObjRef& other) : AnyObjRef(nullptr) {
-    Assign(other);
-  }
-  ObjRef& operator=(const ObjRef& other) {
-    Assign(other);
-    return *this;
-  }
-
-  void Assign(const ObjRef<T>& other) {
-    AnyObjRef::Assign(other);
-  }
-
-  void CopyTo(ObjRef<T> other) const {
-    assert(!other.null());
-    if (ref()) {
-      memcpy(other.ref(), ref(), sizeof(T));
-      for (int i = 0; i < sizeof(T::obj_offsets) / sizeof(T::obj_offsets[0]); ++i) {
-	AnyObjRef any = other.any_obj_at(T::obj_offsets[i]);
-	if (!any.null()) {
-	  any.container()->AddRef();
-	}
-      }
-    } else {
-      // TODO: shall we do anything if copy from/to null?
-    }
-  }
-
-  ObjRef<T> Clone(Container* container) {
-    ObjRef<T> result = Alloc(container);
-    CopyTo(result);
-    return result;
-  }
-
-  template<typename M, int offset>
-  ObjRef<M> obj_at() const {
-    return ObjRef<M>(
-      reinterpret_cast<M*>(any_ref() + offset));
-  }
-
-  static ObjRef<T> Alloc(Container* container) {
-    return ObjRef<T>(container->Place(sizeof(T)));
-  }
-};
+#include "layout.h"
 
 struct List {
+  static const TypeInfo* GetTypeInfo() {
+    const static TypeInfo result = {
+      NameHash("List"), sizeof(List), nullptr, {0, 8},
+      {}, // implemented interfaces
+      {}, // methods
+      {}  // fields
+    };
+    return &result;
+  }
+
   ObjRef<List> next_;
   ObjRef<List> prev_;
   int data_;
-  // Object offsets in the class, needed for CopyTo() operation to update refs and for GC.
-  static constexpr int obj_offsets[] = { 0 /* =offsetof(struct List, next_) */, 8 /* =offsetof(struct List, prev_) */ };
 };
-
-constexpr int List::obj_offsets[];
 
 void UpdateElement(ObjRef<List> element) {
   element.at<int, offsetof(struct List, data_)>().set
