63943c60d38470b42ad1cdf5a52c5ef8cec7015b
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-09-21 22:18:32 +0300

    Patch ThreadTracker during Kotlin plugin init

--- idea/src/org/jetbrains/kotlin/idea/PluginStartupComponent.java
+++ idea/src/org/jetbrains/kotlin/idea/PluginStartupComponent.java
@@ -54,6 +54,10 @@ public class PluginStartupComponent implements ApplicationComponent {
     public void initComponent() {
         registerPathVariable();
 
+        if (ApplicationManager.getApplication().isUnitTestMode()) {
+            ForkJoinPoolPatcherForTeamCityTesting.INSTANCE.patchThreadTracker();
+        }
+
         JarUserDataManager.INSTANCE.register(KotlinJavaScriptLibraryDetectionUtil.HasKotlinJSMetadataInJar.INSTANCE);
 
         DebuggerFiltersUtilKt.addKotlinStdlibDebugFilterIfNeeded();
