e8bf2300115284dfff614a72663c5e71d1bbd1e9
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-09-22 13:05:03 +0300

    Apply diagnostics test data difference between 1.1 and 1.0.5

--- compiler/testData/diagnostics/tests/inference/capturedTypes/captureTypeOnlyOnTopLevel.kt
+++ compiler/testData/diagnostics/tests/inference/capturedTypes/captureTypeOnlyOnTopLevel.kt
@@ -3,7 +3,7 @@
 fun <T> foo(array: Array<Array<T>>): Array<Array<T>> = array
 
 fun test(array: Array<Array<out Int>>) {
-    <!TYPE_INFERENCE_PARAMETER_CONSTRAINT_ERROR!>foo<!>(array)
+    <!TYPE_INFERENCE_PARAMETER_CONSTRAINT_ERROR!>foo<!>(<!TYPE_MISMATCH!>array<!>)
 
     val f: Array<out Array<out Int>> = <!TYPE_INFERENCE_PARAMETER_CONSTRAINT_ERROR!>foo<!>(<!TYPE_MISMATCH!>array<!>)
 }
