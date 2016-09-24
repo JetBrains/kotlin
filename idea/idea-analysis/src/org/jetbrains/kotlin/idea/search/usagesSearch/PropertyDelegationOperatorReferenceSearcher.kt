ba76e4d24d80a035e6fdb78b0de951e7890bad87
Valentin Kipyatkov <valentin@jetbrains.com>
2016-09-24 23:00:04 +0300

    Quick search of getValue/setValue operator usages

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/usagesSearch/PropertyDelegationOperatorReferenceSearcher.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/usagesSearch/PropertyDelegationOperatorReferenceSearcher.kt
@@ -21,36 +21,30 @@ import com.intellij.psi.PsiReference
 import com.intellij.psi.search.SearchRequestCollector
 import com.intellij.psi.search.SearchScope
 import com.intellij.util.Processor
-import org.jetbrains.kotlin.KtNodeTypes
-import org.jetbrains.kotlin.idea.references.KtForLoopInReference
 import org.jetbrains.kotlin.idea.references.KtPropertyDelegationMethodsReference
 import org.jetbrains.kotlin.idea.search.ideaExtensions.KotlinReferencesSearchOptions
 import org.jetbrains.kotlin.psi.KtElement
 import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtForExpression
 import org.jetbrains.kotlin.psi.KtPropertyDelegate
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstance
 
-class IteratorOperatorReferenceSearcher(
+class PropertyDelegationOperatorReferenceSearcher(
         targetFunction: PsiElement,
         searchScope: SearchScope,
         consumer: Processor<PsiReference>,
         optimizer: SearchRequestCollector,
         options: KotlinReferencesSearchOptions
-) : OperatorReferenceSearcher<KtForExpression>(targetFunction, searchScope, consumer, optimizer, options, wordsToSearch = listOf("in")) {
+) : OperatorReferenceSearcher<KtPropertyDelegate>(targetFunction, searchScope, consumer, optimizer, options, wordsToSearch = listOf("by")) {
 
     override fun processPossibleReceiverExpression(expression: KtExpression) {
-        val parent = expression.parent
-        if (parent.node.elementType == KtNodeTypes.LOOP_RANGE) {
-            processReferenceElement(parent.parent as KtForExpression)
-        }
+        (expression.parent as? KtPropertyDelegate)?.let { processReferenceElement(it) }
     }
 
     override fun isReferenceToCheck(ref: PsiReference): Boolean {
-        return ref is KtForLoopInReference
+        return ref is KtPropertyDelegationMethodsReference
     }
 
     override fun extractReference(element: KtElement): PsiReference? {
-        return (element as? KtForExpression)?.references?.firstIsInstance<KtForLoopInReference>()
+        return (element as? KtPropertyDelegate)?.references?.firstIsInstance<KtPropertyDelegationMethodsReference>()
     }
 }
