c5408c99e58127c620c09a03d3420fae7a81a909
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-09-26 21:21:49 +0300

    Refactoring: move gradle tests imported from jps to separate dir
    This will allow skipping jps tests when running all tests in directory from Intellij

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/jps/BaseIncrementalGradleIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/jps/BaseIncrementalGradleIT.kt
@@ -1,22 +1,7 @@
-/*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.jetbrains.kotlin.gradle
+package org.jetbrains.kotlin.jps
 
 import org.gradle.api.logging.LogLevel
+import org.jetbrains.kotlin.gradle.BaseGradleIT
 import org.jetbrains.kotlin.gradle.incremental.BuildStep
 import org.jetbrains.kotlin.gradle.incremental.parseTestBuildLog
 import org.jetbrains.kotlin.incremental.testingUtils.*
@@ -103,7 +88,3 @@ abstract class BaseIncrementalGradleIT : BaseGradleIT() {
         }
     }
 }
-
-fun isJpsTestProject(projectRoot: File): Boolean = projectRoot.listFiles { f: File -> f.name.endsWith("build.log") }?.any() ?: false
-
-
--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/jps/KotlinGradlePluginJpsParametrizedIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/jps/KotlinGradlePluginJpsParametrizedIT.kt
@@ -1,35 +1,12 @@
-/*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.jetbrains.kotlin.gradle
+package org.jetbrains.kotlin.jps
 
 import org.jetbrains.kotlin.incremental.testingUtils.BuildLogFinder
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.io.File
-
-// Test does not follow maven failsafe naming convention
-// so it is ignored by maven integration-test phase,
-// but it is possible run it from CLI (under separate CI configuration for example)
-//
-// To run this test from CLI:
-// mvn integration-test -pl :kotlin-gradle-plugin -Dit.test=KotlinGradlePluginJpsParametrizedCLIOnly
 @RunWith(Parameterized::class)
-class KotlinGradlePluginJpsParametrizedCLIOnly : BaseIncrementalGradleIT() {
+class KotlinGradlePluginJpsParametrizedIT : BaseIncrementalGradleIT() {
 
     @Parameterized.Parameter
     @JvmField
