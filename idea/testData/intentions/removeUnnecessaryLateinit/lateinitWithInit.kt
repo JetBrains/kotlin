e17f22225ff43b371ef2f218744f4a7828939143
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-09-26 12:01:08 +0300

    Remove unnecessary lateinit intention / inspection removed (supported in compiler CFA instead)

--- idea/testData/intentions/removeUnnecessaryLateinit/lateinitWithInit.kt
+++
@@ -1,9 +0,0 @@
-// INTENTION_TEXT: Remove unnecessary lateinit
-
-class Foo {
-    <caret>lateinit var bar: String
-
-    init {
-        bar = ""
-    }
-}
--- idea/testData/intentions/removeUnnecessaryLateinit/normalLateinit.kt
+++
@@ -1,9 +0,0 @@
-// IS_APPLICABLE: false
-
-class Foo {
-    lateinit var bar: String
-
-    fun init() {
-        bar = ""
-    }
-}
