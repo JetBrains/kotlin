3dd94eeb8c264cb6230ed2d225b231471a527322
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-09-30 16:30:43 +0300

    JS: fix exception during translation of for expression with iterator having generic type constrained by Iterable. Fix #KT-8385

---
+++ js/js.translator/testData/box/expression/for/forIteratesOverTypeParameter.kt
@@ -0,0 +1,8 @@
+public fun <T, C : Collection<T>> bar(collection: C, f: (T) -> Unit) { for (item in collection) f(item) }
+
+fun box(): String {
+    val collection = listOf("O", "K")
+    var result = ""
+    bar(collection) { result += it }
+    return result
+}
