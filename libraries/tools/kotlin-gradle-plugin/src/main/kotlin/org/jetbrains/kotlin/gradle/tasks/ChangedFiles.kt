248c8702a38fd6b0e13f5ee933db702de9bdf73b
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-10-03 19:06:55 +0300

    Refactoring: introduce class ChangedFiles

---
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/ChangedFiles.kt
@@ -0,0 +1,22 @@
+package org.jetbrains.kotlin.gradle.tasks
+
+import org.gradle.api.tasks.incremental.IncrementalTaskInputs
+import java.io.File
+import java.util.*
+
+internal sealed class ChangedFiles {
+    class Known(val modified: List<File>, val removed: List<File>) : ChangedFiles()
+    class Unknown : ChangedFiles()
+}
+
+internal fun ChangedFiles(taskInputs: IncrementalTaskInputs): ChangedFiles {
+    if (!taskInputs.isIncremental) return ChangedFiles.Unknown()
+
+    val modified = ArrayList<File>()
+    val removed = ArrayList<File>()
+
+    taskInputs.outOfDate { modified.add(it.file) }
+    taskInputs.removed { removed.add(it.file) }
+
+    return ChangedFiles.Known(modified, removed)
+}
