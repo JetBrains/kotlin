3163d28b298dedec2836e701512c64d6080e65d6
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-10-03 11:36:18 +0300

    Minor: test fixed

--- idea/testData/quickfix/typeMismatch/casts/typeMismatch2.kt
+++ idea/testData/quickfix/typeMismatch/casts/typeMismatch2.kt
@@ -1,5 +1,5 @@
 // "Cast expression 'Foo<Number>()' to 'Foo<Int>'" "false"
-// ACTION: Change 'foo' function return type to 'Foo<Number>'
+// ACTION: Change return type of enclosing function 'foo' to 'Foo<Number>'
 // ACTION: Convert to expression body
 // ERROR: Type mismatch: inferred type is Foo<Number> but Foo<Int> was expected
 class Foo<T>
