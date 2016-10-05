3a100d4139c3646ba4e7e980edeeadca433cefab
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-10-05 14:15:17 +0300

    Improve exception message in collection stubs

--- compiler/testData/codegen/box/builtinStubMethods/immutableRemove.kt
+++ compiler/testData/codegen/box/builtinStubMethods/immutableRemove.kt
@@ -43,7 +43,7 @@ fun box(): String {
         method.invoke(c, "")
         return "fail 4"
     } catch (e: java.lang.reflect.InvocationTargetException) {
-        if (e.cause!!.message != "Mutating immutable collection") return "fail 5: ${e.cause!!.message}"
+        if (e.cause!!.message != "Operation is not supported for read-only collection") return "fail 5: ${e.cause!!.message}"
     }
 
     return "OK"
