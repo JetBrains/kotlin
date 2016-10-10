a6a6aef352260fa37beacbd27bd76f506562e468
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-10-10 16:16:02 +0300

    Add integration test for Kotlin compiler plugin for Maven
    (cherry picked from commit 1e146a4)
    
    (cherry picked from commit b50a336)

---
+++ libraries/tools/kotlin-maven-plugin/src/it/plugins/test-extension/src/main/kotlin/org/jetbrains/kotlin/test/TestKotlinPluginRegistrar.kt
@@ -0,0 +1,41 @@
+package org.jetbrains.kotlin.test
+
+import com.intellij.mock.*
+import org.jetbrains.kotlin.cli.common.*
+import org.jetbrains.kotlin.cli.common.messages.*
+import org.jetbrains.kotlin.compiler.plugin.*
+import org.jetbrains.kotlin.config.*
+
+object TestPluginKeys {
+    val TestOption = CompilerConfigurationKey.create<String>("test option")!!
+}
+
+class TestCommandLineProcessor : CommandLineProcessor {
+    companion object {
+        val TestPluginId = "org.jetbrains.kotlin.test.test-plugin"
+        val MyTestOption = CliOption("test-option", "", "", true, false)
+    }
+
+    override val pluginId: String = TestPluginId
+
+    override val pluginOptions: Collection<CliOption> = listOf(MyTestOption)
+
+    override fun processOption(option: CliOption, value: String, configuration: CompilerConfiguration) {
+        when (option) {
+            MyTestOption -> {
+                configuration.put(TestPluginKeys.TestOption, value)
+            }
+            else -> throw CliOptionProcessingException("Unknown option: ${option.name}")
+        }
+    }
+}
+
+class TestKotlinPluginRegistrar : ComponentRegistrar {
+    override fun registerProjectComponents(project: MockProject, configuration: CompilerConfiguration) {
+        val collector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY)!!
+        val option = configuration.get(TestPluginKeys.TestOption)!!
+
+        collector.report(CompilerMessageSeverity.INFO, "Plugin applied", CompilerMessageLocation.NO_LOCATION)
+        collector.report(CompilerMessageSeverity.INFO, "Option value: $option", CompilerMessageLocation.NO_LOCATION)
+    }
+}
