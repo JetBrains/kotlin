7e0f100ccfd342eaa7b29eedfbf7b24fadaf5ef6
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-10-10 20:35:58 +0300

    Refactoring: inline method + inline variable

--- idea/src/org/jetbrains/kotlin/idea/debugger/stepping/KotlinSteppingCommandProvider.kt
+++ idea/src/org/jetbrains/kotlin/idea/debugger/stepping/KotlinSteppingCommandProvider.kt
@@ -331,23 +331,14 @@ sealed class Action(val position: XSourcePositionImpl?) {
     fun apply(debugProcess: DebugProcessImpl,
               suspendContext: SuspendContextImpl,
               ignoreBreakpoints: Boolean) {
-        val command = createCommand(debugProcess, suspendContext, ignoreBreakpoints)
-        command.contextAction()
-    }
-
-    private fun createCommand(
-            debugProcess: DebugProcessImpl,
-            suspendContext: SuspendContextImpl,
-            ignoreBreakpoints: Boolean
-    ): SuspendContextCommandImpl {
-        return when (this) {
+        when (this) {
             is Action.RUN_TO_CURSOR -> {
                 runReadAction {
                     debugProcess.createRunToCursorCommand(suspendContext, position!!, ignoreBreakpoints)
-                }
+                }.contextAction()
             }
-            is Action.STEP_OUT -> debugProcess.createStepOutCommand(suspendContext)
-            is Action.STEP_OVER -> debugProcess.createStepOverCommand(suspendContext, ignoreBreakpoints)
+            is Action.STEP_OUT -> debugProcess.createStepOutCommand(suspendContext).contextAction()
+            is Action.STEP_OVER -> debugProcess.createStepOverCommand(suspendContext, ignoreBreakpoints).contextAction()
         }
     }
 }
