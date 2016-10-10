1a7c507cc5a031d22d2b7a6ad0b8227ffb156372
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2016-10-10 20:32:37 +0300

    Fix for KT-13818 Javadoc @code conversion

---
+++ j2k/testData/fileOrElement/docComments/quoted.kt
@@ -0,0 +1,11 @@
+/**
+ * Start.
+ * Plain quote: 'quoted'
+ * KT-13818: `'quoted'`
+ * Code: `someCode()`
+ * Operators: `a > b`
+ * String: `str = "String"`
+ * Generic: `List<T>`
+ * End.
+ */
+class Quote
