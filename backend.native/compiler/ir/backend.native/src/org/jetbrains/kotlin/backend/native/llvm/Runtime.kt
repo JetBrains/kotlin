e730ee9f37f4c1e2c8004f46ceb60b5d95e51266
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-10-11 19:08:46 +0300

    Fix spaces.

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/native/llvm/Runtime.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/native/llvm/Runtime.kt
@@ -4,41 +4,41 @@ import kotlin_native.interop.*
 import llvm.*
 
 class Runtime(private val bitcodeFile: String) {
-	val llvmModule: LLVMOpaqueModule
+    val llvmModule: LLVMOpaqueModule
 
-	init {
-		val arena = Arena()
-		try {
+    init {
+        val arena = Arena()
+        try {
 
-			val bufRef = arena.alloc(LLVMOpaqueMemoryBuffer.ref)
-			val errorRef = arena.alloc(Int8Box.ref)
-			val res = LLVMCreateMemoryBufferWithContentsOfFile(bitcodeFile, bufRef, errorRef)
-			if (res != 0) {
-				throw Error(errorRef.value?.asCString()?.toString())
-			}
+            val bufRef = arena.alloc(LLVMOpaqueMemoryBuffer.ref)
+            val errorRef = arena.alloc(Int8Box.ref)
+            val res = LLVMCreateMemoryBufferWithContentsOfFile(bitcodeFile, bufRef, errorRef)
+            if (res != 0) {
+                throw Error(errorRef.value?.asCString()?.toString())
+            }
 
-			val moduleRef = arena.alloc(LLVMOpaqueModule.ref)
-			val parseRes = LLVMParseBitcode2(bufRef.value, moduleRef)
-			if (parseRes != 0) {
-				throw Error(parseRes.toString())
-			}
+            val moduleRef = arena.alloc(LLVMOpaqueModule.ref)
+            val parseRes = LLVMParseBitcode2(bufRef.value, moduleRef)
+            if (parseRes != 0) {
+                throw Error(parseRes.toString())
+            }
 
-			llvmModule = moduleRef.value!!
+            llvmModule = moduleRef.value!!
 
-		} finally {
-			arena.clear()
-		}
-	}
+        } finally {
+            arena.clear()
+        }
+    }
 
-	val typeInfoType = LLVMGetTypeByName(llvmModule, "struct.TypeInfo")
-	val fieldTableRecordType = LLVMGetTypeByName(llvmModule, "struct.FieldTableRecord")
-	val methodTableRecordType = LLVMGetTypeByName(llvmModule, "struct.MethodTableRecord")
-	val globalhHashType = LLVMGetTypeByName(llvmModule, "struct.GlobalHash")
+    val typeInfoType = LLVMGetTypeByName(llvmModule, "struct.TypeInfo")
+    val fieldTableRecordType = LLVMGetTypeByName(llvmModule, "struct.FieldTableRecord")
+    val methodTableRecordType = LLVMGetTypeByName(llvmModule, "struct.MethodTableRecord")
+    val globalhHashType = LLVMGetTypeByName(llvmModule, "struct.GlobalHash")
 
-	val target = LLVMGetTarget(llvmModule)!!.asCString().toString()
+    val target = LLVMGetTarget(llvmModule)!!.asCString().toString()
 
-	val dataLayout = LLVMGetDataLayout(llvmModule)!!.asCString().toString()
+    val dataLayout = LLVMGetDataLayout(llvmModule)!!.asCString().toString()
 
-	val targetData = LLVMCreateTargetData(dataLayout)
+    val targetData = LLVMCreateTargetData(dataLayout)
 
 }
