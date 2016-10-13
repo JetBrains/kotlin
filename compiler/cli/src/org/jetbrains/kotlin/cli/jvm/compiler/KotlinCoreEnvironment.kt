aa063113b47af2c15ac22916326123e0d9858796
Dmitry Jemerov <yole@jetbrains.com>
2016-10-13 14:21:34 +0200

    set plugins compatible build to a version which is compatible with the Kotlin plugin

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -287,7 +287,7 @@ class KotlinCoreEnvironment private constructor(
 
     companion object {
         init {
-            System.getProperties().setProperty("idea.plugins.compatible.build", "162.9999")
+            System.getProperties().setProperty("idea.plugins.compatible.build", "161.9999")
         }
 
         private val APPLICATION_LOCK = Object()
