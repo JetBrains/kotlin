dbfc078835074af058b1ddf61b934de143fe955f
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2016-10-13 19:22:58 +0300

    Fix test to complete on 1.0.5
    for 3304bb3

--- j2k/testData/fileOrElement/issues/unfinishedReferenceExpression.kt
+++ j2k/testData/fileOrElement/issues/unfinishedReferenceExpression.kt
@@ -1,4 +1,4 @@
-// ERROR: The expression cannot be a selector (occur after a dot)
+// ERROR: Expression 'this' cannot be a selector (occur after a dot)
 class JavaClass {
     var v = ""
     fun m(s: String) {
