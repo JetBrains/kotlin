703ee6bd784f8eae670ec2c7f0ca776f83fdb657
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-10-13 22:16:30 +0300

    Have mutable collection interfaces specified explicitly to get rid of platform types in parameter and return types.

--- libraries/stdlib/src/kotlin/collections/AbstractMutableList.kt
+++ libraries/stdlib/src/kotlin/collections/AbstractMutableList.kt
@@ -4,7 +4,7 @@ package kotlin.collections
 import java.util.AbstractList
 
 @SinceKotlin("1.1")
-public abstract class AbstractMutableList<E> protected constructor() : AbstractList<E>() {
+public abstract class AbstractMutableList<E> protected constructor() : MutableList<E>, AbstractList<E>() {
     abstract override fun set(index: Int, element: E): E
     abstract override fun removeAt(index: Int): E
     abstract override fun add(index: Int, element: E)
