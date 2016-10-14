78284c0eafdb15fdfeb180e65f5687579931341e
Michael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-10-14 10:51:46 +0300

    Fix for KT-14330 java.lang.IllegalArgumentException: Parameter specified as non-null is null: method ... parameter value
     #KT-14330 Fixed
    
    (cherry picked from commit 0501392)

---
+++ compiler/testData/codegen/box/callableReference/property/kt14330.kt
@@ -0,0 +1,8 @@
+data class Foo(var bar: Int?)
+
+fun box(): String {
+    val receiver = Foo(1)
+    Foo::bar.set(receiver, null)
+    return if (receiver.bar == null) "OK" else "fail ${receiver.bar}"
+
+}
