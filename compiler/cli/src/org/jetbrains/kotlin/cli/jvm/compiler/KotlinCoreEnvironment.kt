022d49ab7cc14ea799caa08dd09cfb44b40c0900
Dmitry Jemerov <yole@jetbrains.com>
2016-10-14 12:46:02 +0200

    remove idea.plugins.compatible.build entirely

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -286,10 +286,6 @@ class KotlinCoreEnvironment private constructor(
     }
 
     companion object {
-        init {
-            System.getProperties().setProperty("idea.plugins.compatible.build", "161.9999")
-        }
-
         private val APPLICATION_LOCK = Object()
         private var ourApplicationEnvironment: JavaCoreApplicationEnvironment? = null
         private var ourProjectCount = 0
@@ -297,7 +293,6 @@ class KotlinCoreEnvironment private constructor(
         @JvmStatic fun createForProduction(
                 parentDisposable: Disposable, configuration: CompilerConfiguration, configFilePaths: List<String>
         ): KotlinCoreEnvironment {
-            setCompatibleBuild()
             val appEnv = getOrCreateApplicationEnvironmentForProduction(configuration, configFilePaths)
             // Disposing of the environment is unsafe in production then parallel builds are enabled, but turning it off universally
             // breaks a lot of tests, therefore it is disabled for production and enabled for tests
