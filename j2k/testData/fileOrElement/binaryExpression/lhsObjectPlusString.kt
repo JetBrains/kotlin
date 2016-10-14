5c730bb1a6db9aac502b575e096bf19d1571f3d7
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2016-10-14 17:35:01 +0300

    Fix for KT-5410 J2K: `someObject + "someString"` should be converted to `someObject.toString() + "someString"`
    Otherwise `+` operator will not resolve

---
+++ j2k/testData/fileOrElement/binaryExpression/lhsObjectPlusString.kt
@@ -0,0 +1,23 @@
+class Test {
+    class B {
+        class C
+
+        fun c(): C {
+            return C()
+        }
+    }
+
+    fun a() {
+        val b = B()
+        println(b.toString() + "")
+        val a = 1.toString() + "0"
+        println(b.c().toString() + "")
+    }
+
+    companion object {
+
+        @JvmStatic fun main(args: Array<String>) {
+            val p = Test().toString() + "123"
+        }
+    }
+}
