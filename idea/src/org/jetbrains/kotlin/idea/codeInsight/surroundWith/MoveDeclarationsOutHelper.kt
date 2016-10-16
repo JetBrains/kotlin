20ce6c1eac062df0885908c01dc4e25f85073ea5
shiraji <isogai.shiraji@gmail.com>
2016-10-16 15:49:43 +0900

    Move Java to Kotlin

--- idea/src/org/jetbrains/kotlin/idea/codeInsight/surroundWith/MoveDeclarationsOutHelper.kt
+++ idea/src/org/jetbrains/kotlin/idea/codeInsight/surroundWith/MoveDeclarationsOutHelper.kt
@@ -14,145 +14,129 @@
  * limitations under the License.
  */
 
-package org.jetbrains.kotlin.idea.codeInsight.surroundWith;
+package org.jetbrains.kotlin.idea.codeInsight.surroundWith
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.search.LocalSearchScope;
-import com.intellij.psi.search.SearchScope;
-import com.intellij.psi.search.searches.ReferencesSearch;
-import com.intellij.psi.util.PsiUtilCore;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.descriptors.VariableDescriptor;
-import org.jetbrains.kotlin.idea.caches.resolve.ResolutionUtils;
-import org.jetbrains.kotlin.idea.codeInsight.CodeInsightUtils;
-import org.jetbrains.kotlin.idea.util.IdeDescriptorRenderers;
-import org.jetbrains.kotlin.idea.core.ShortenReferences;
-import org.jetbrains.kotlin.psi.*;
-import org.jetbrains.kotlin.resolve.BindingContext;
-import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode;
-import org.jetbrains.kotlin.types.KotlinType;
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.psi.util.PsiUtilCore
+import org.jetbrains.kotlin.descriptors.VariableDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.*
+import org.jetbrains.kotlin.idea.codeInsight.CodeInsightUtils
+import org.jetbrains.kotlin.idea.util.IdeDescriptorRenderers
+import org.jetbrains.kotlin.idea.core.ShortenReferences
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
+import org.jetbrains.kotlin.types.KotlinType
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList
 
-public class MoveDeclarationsOutHelper {
+object MoveDeclarationsOutHelper {
 
-    public static PsiElement[] move(@NotNull PsiElement container, @NotNull PsiElement[] statements, boolean generateDefaultInitializers) {
-        if (statements.length == 0) {
-            return statements;
+    fun move(container: PsiElement, statements: Array<PsiElement>, generateDefaultInitializers: Boolean): Array<PsiElement> {
+        if (statements.size == 0) {
+            return statements
         }
 
-        Project project = container.getProject();
+        val project = container.project
 
-        List<PsiElement> resultStatements = new ArrayList<PsiElement>();
-        List<KtProperty> propertiesDeclarations = new ArrayList<KtProperty>();
+        val resultStatements = ArrayList<PsiElement>()
+        val propertiesDeclarations = ArrayList<KtProperty>()
 
         // Dummy element to add new declarations at the beginning
-        KtPsiFactory psiFactory = KtPsiFactoryKt.KtPsiFactory(project);
-        PsiElement dummyFirstStatement = container.addBefore(psiFactory.createExpression("dummyStatement"), statements[0]);
+        val psiFactory = KtPsiFactory(project)
+        val dummyFirstStatement = container.addBefore(psiFactory.createExpression("dummyStatement"), statements[0])
 
         try {
-            SearchScope scope = new LocalSearchScope(container);
-            int lastStatementOffset = statements[statements.length - 1].getTextRange().getEndOffset();
+            val scope = LocalSearchScope(container)
+            val lastStatementOffset = statements[statements.size - 1].textRange.endOffset
 
-            for (PsiElement statement : statements) {
+            for (statement in statements) {
                 if (needToDeclareOut(statement, lastStatementOffset, scope)) {
-                    if (statement instanceof KtProperty && ((KtProperty) statement).getInitializer() != null) {
-                        KtProperty property = (KtProperty) statement;
-                        KtProperty declaration = createVariableDeclaration(property, generateDefaultInitializers);
-                        declaration = (KtProperty) container.addBefore(declaration, dummyFirstStatement);
-                        propertiesDeclarations.add(declaration);
-                        container.addAfter(psiFactory.createNewLine(), declaration);
+                    if (statement is KtProperty && statement.initializer != null) {
+                        var declaration = createVariableDeclaration(statement, generateDefaultInitializers)
+                        declaration = container.addBefore(declaration, dummyFirstStatement) as KtProperty
+                        propertiesDeclarations.add(declaration)
+                        container.addAfter(psiFactory.createNewLine(), declaration)
 
-                        KtBinaryExpression assignment = createVariableAssignment(property);
-                        resultStatements.add(property.replace(assignment));
+                        val assignment = createVariableAssignment(statement)
+                        resultStatements.add(statement.replace(assignment))
                     }
                     else {
-                        PsiElement newStatement = container.addBefore(statement, dummyFirstStatement);
-                        container.addAfter(psiFactory.createNewLine(), newStatement);
-                        container.deleteChildRange(statement, statement);
+                        val newStatement = container.addBefore(statement, dummyFirstStatement)
+                        container.addAfter(psiFactory.createNewLine(), newStatement)
+                        container.deleteChildRange(statement, statement)
                     }
                 }
                 else {
-                    resultStatements.add(statement);
+                    resultStatements.add(statement)
                 }
             }
         }
         finally {
-            dummyFirstStatement.delete();
+            dummyFirstStatement.delete()
         }
 
-        ShortenReferences.DEFAULT.process(propertiesDeclarations);
+        ShortenReferences.DEFAULT.process(propertiesDeclarations)
 
-        return PsiUtilCore.toPsiElementArray(resultStatements);
+        return PsiUtilCore.toPsiElementArray(resultStatements)
     }
 
-    @NotNull
-    private static KtBinaryExpression createVariableAssignment(@NotNull KtProperty property) {
-        String propertyName = property.getName();
-        assert propertyName != null : "Property should have a name " + property.getText();
-        KtBinaryExpression assignment = (KtBinaryExpression) KtPsiFactoryKt
-                .KtPsiFactory(property).createExpression(propertyName + " = x");
-        KtExpression right = assignment.getRight();
-        assert right != null : "Created binary expression should have a right part " + assignment.getText();
-        KtExpression initializer = property.getInitializer();
-        assert initializer != null : "Initializer should exist for property " + property.getText();
-        right.replace(initializer);
-        return assignment;
+    private fun createVariableAssignment(property: KtProperty): KtBinaryExpression {
+        val propertyName = property.name ?: error("Property should have a name " + property.text)
+        val assignment = KtPsiFactory(property).createExpression(propertyName + " = x") as KtBinaryExpression
+        val right = assignment.right ?: error("Created binary expression should have a right part " + assignment.text)
+        val initializer = property.initializer ?: error("Initializer should exist for property " + property.text)
+        right.replace(initializer)
+        return assignment
     }
 
-    @NotNull
-    private static KtProperty createVariableDeclaration(@NotNull KtProperty property, boolean generateDefaultInitializers) {
-        KotlinType propertyType = getPropertyType(property);
-        String defaultInitializer = null;
-        if (generateDefaultInitializers && property.isVar()) {
-            defaultInitializer = CodeInsightUtils.defaultInitializer(propertyType);
+    private fun createVariableDeclaration(property: KtProperty, generateDefaultInitializers: Boolean): KtProperty {
+        val propertyType = getPropertyType(property)
+        var defaultInitializer: String? = null
+        if (generateDefaultInitializers && property.isVar) {
+            defaultInitializer = CodeInsightUtils.defaultInitializer(propertyType)
         }
-        return createProperty(property, propertyType, defaultInitializer);
+        return createProperty(property, propertyType, defaultInitializer)
     }
 
-    @NotNull
-    private static KotlinType getPropertyType(@NotNull KtProperty property) {
-        BindingContext bindingContext = ResolutionUtils.analyze(property, BodyResolveMode.PARTIAL);
+    private fun getPropertyType(property: KtProperty): KotlinType {
+        val bindingContext = property.analyze(BodyResolveMode.PARTIAL)
 
-        VariableDescriptor propertyDescriptor = bindingContext.get(BindingContext.VARIABLE, property);
-        assert propertyDescriptor != null : "Couldn't resolve property to property descriptor " + property.getText();
-        return propertyDescriptor.getType();
+        val propertyDescriptor = bindingContext.get(BindingContext.VARIABLE, property) ?: error("Couldn't resolve property to property descriptor " + property.text)
+        return propertyDescriptor.type
     }
 
-    @NotNull
-    private static KtProperty createProperty(@NotNull KtProperty property, @NotNull KotlinType propertyType, @Nullable String initializer) {
-        KtTypeReference typeRef = property.getTypeReference();
-        String typeString = null;
+    private fun createProperty(property: KtProperty, propertyType: KotlinType, initializer: String?): KtProperty {
+        val typeRef = property.typeReference
+        var typeString: String? = null
         if (typeRef != null) {
-            typeString = typeRef.getText();
+            typeString = typeRef.text
         }
-        else if (!propertyType.isError()) {
-            typeString = IdeDescriptorRenderers.SOURCE_CODE.renderType(propertyType);
+        else if (!propertyType.isError) {
+            typeString = IdeDescriptorRenderers.SOURCE_CODE.renderType(propertyType)
         }
 
-        return KtPsiFactoryKt.KtPsiFactory(property).createProperty(property.getName(), typeString, property.isVar(), initializer);
+        return KtPsiFactory(property).createProperty(property.name!!, typeString, property.isVar, initializer)
     }
 
-    private static boolean needToDeclareOut(@NotNull PsiElement element, int lastStatementOffset, @NotNull SearchScope scope) {
-        if (element instanceof KtProperty ||
-            element instanceof KtClassOrObject ||
-            element instanceof KtFunction) {
+    private fun needToDeclareOut(element: PsiElement, lastStatementOffset: Int, scope: SearchScope): Boolean {
+        if (element is KtProperty ||
+            element is KtClassOrObject ||
+            element is KtFunction) {
 
-            PsiReference[] refs = ReferencesSearch.search(element, scope, false).toArray(PsiReference.EMPTY_ARRAY);
-            if (refs.length > 0) {
-                PsiReference lastRef = refs[refs.length - 1];
-                if (lastRef.getElement().getTextOffset() > lastStatementOffset) {
-                    return true;
+            val refs = ReferencesSearch.search(element, scope, false).toArray(PsiReference.EMPTY_ARRAY)
+            if (refs.size > 0) {
+                val lastRef = refs[refs.size - 1]
+                if (lastRef.element.textOffset > lastStatementOffset) {
+                    return true
                 }
             }
         }
-        return false;
-    }
-
-    private MoveDeclarationsOutHelper() {
+        return false
     }
 }
--- idea/src/org/jetbrains/kotlin/idea/codeInsight/surroundWith/statement/KotlinFunctionLiteralSurrounder.java
+++ idea/src/org/jetbrains/kotlin/idea/codeInsight/surroundWith/statement/KotlinFunctionLiteralSurrounder.java
@@ -31,7 +31,7 @@ public class KotlinFunctionLiteralSurrounder extends KotlinStatementsSurrounder 
     @Nullable
     @Override
     protected TextRange surroundStatements(Project project, Editor editor, PsiElement container, PsiElement[] statements) {
-        statements = MoveDeclarationsOutHelper.move(container, statements, true);
+        statements = MoveDeclarationsOutHelper.INSTANCE.move(container, statements, true);
 
         if (statements.length == 0) {
             KotlinSurrounderUtils.showErrorHint(project, editor, KotlinSurrounderUtils.SURROUND_WITH_ERROR);
