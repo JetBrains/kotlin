d1e2e5c38d9caaf0ec1a13713f1ffc1eca90c05a
Dmitry Jemerov <yole@jetbrains.com>
2016-10-17 17:21:04 +0200

    fix idea.plugins.compatible.build

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -317,7 +317,7 @@ class KotlinCoreEnvironment private constructor(
 
     companion object {
         init {
-            System.getProperties().setProperty("idea.plugins.compatible.build", "162.9999")
+            System.getProperties().setProperty("idea.plugins.compatible.build", "163.9999")
         }
 
         private val APPLICATION_LOCK = Object()
