acca9e0eb8df1b7609098762d8f89f8814f42926
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-10-17 13:59:33 +0300

    FFI on iOS test.

---
+++ experiments/ffi_on_ios/Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by jetbrains on 14/10/16.
+//  Copyright © 2016 jetbrains. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ experiments/ffi_on_ios/Hello/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hello
+//
+//  Created by jetbrains on 14/10/16.
+//  Copyright © 2016 jetbrains. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
