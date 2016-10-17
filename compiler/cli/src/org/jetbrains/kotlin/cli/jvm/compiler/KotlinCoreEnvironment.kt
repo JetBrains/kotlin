a147b4c83d7c1a12106c30dfc9f0e7eebd2071cd
Dmitry Jemerov <yole@jetbrains.com>
2016-10-17 17:21:04 +0200

    Set idea.plugins.compatible.build for 163 branch

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -339,7 +339,7 @@ class KotlinCoreEnvironment private constructor(
     }
 
     companion object {
-        private val ideaCompatibleBuildNumber = "172.9999"
+        private val ideaCompatibleBuildNumber = "163.9999"
 
         init {
             setCompatibleBuild()
