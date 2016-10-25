5f45b41d5361dc01a08288234de93fa8a1deabf8
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2016-10-25 23:34:37 +0300

    Kapt3: Initial implementation of JCTreeConverter

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -136,6 +136,7 @@ import org.jetbrains.kotlin.js.test.semantics.*
 import org.jetbrains.kotlin.jvm.compiler.*
 import org.jetbrains.kotlin.jvm.runtime.AbstractJvm8RuntimeDescriptorLoaderTest
 import org.jetbrains.kotlin.jvm.runtime.AbstractJvmRuntimeDescriptorLoaderTest
+import org.jetbrains.kotlin.kapt3.test.AbstractJCTreeConverterTest
 import org.jetbrains.kotlin.kdoc.AbstractKDocLexerTest
 import org.jetbrains.kotlin.lang.resolve.android.test.AbstractAndroidBoxTest
 import org.jetbrains.kotlin.lang.resolve.android.test.AbstractAndroidBytecodeShapeTest
@@ -1134,6 +1135,12 @@ fun main(args: Array<String>) {
 
         testClass<AbstractBytecodeListingTestForSourceRetention>() {
             model("sourceRetention", extension = "kt")
+        }
+    }
+
+    testGroup("plugins/kapt3/test", "plugins/kapt3/testData") {
+        testClass<AbstractJCTreeConverterTest> {
+            model("converter")
         }
     }
 
