95dc898f6efbdf4f81b062dbed794fa19bf0d77f
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2016-10-26 03:41:13 +0300

    Kapt3: Put static modifiers on nested non-inner classes. Do not initialize synthetic and static fields in constructor.

--- plugins/kapt3/testData/runner/Simple.java
+++ plugins/kapt3/testData/runner/Simple.java
@@ -10,3 +10,18 @@ class Simple {
 @interface MyAnnotation {
 
 }
+
+enum EnumClass {
+    BLACK, WHITE
+}
+
+
+enum EnumClass2 {
+    WHITE("A"), RED("B");
+
+    private final String blah;
+
+    EnumClass2(String blah) {
+        this.blah = blah;
+    }
+}
