54fa8b3a01a7ff61293c3d7c7a78a52d38a3c4e5
Dmitry Jemerov <yole@jetbrains.com>
2016-10-27 13:48:29 +0200

    "Show expression type" no longer shows elements which enclose the element at caret (KT-14436)
    (cherry picked from commit 4246fcf)

--- idea/testData/codeInsight/expressionType/IfAsExpressionInsideBlock.kt
+++ idea/testData/codeInsight/expressionType/IfAsExpressionInsideBlock.kt
@@ -3,5 +3,3 @@ val x = if (flag) {
 } else 42
 
 // TYPE: if (flag2) 13 else 7 -> <html>Int</html>
-// TYPE: if (flag) {     if (flag2) 13 else 7 } else 42 -> <html>Int</html>
-// TYPE: val x = if (flag) {     if (flag2) 13 else 7 } else 42 -> <html>Int</html>
