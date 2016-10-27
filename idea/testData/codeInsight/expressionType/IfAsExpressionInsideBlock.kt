57680ae003cd96f707f83a2e13082b7c9e3230a6
Dmitry Jemerov <yole@jetbrains.com>
2016-10-27 13:44:58 +0200

    Don't show literals in "show expression type" popup

--- idea/testData/codeInsight/expressionType/IfAsExpressionInsideBlock.kt
+++ idea/testData/codeInsight/expressionType/IfAsExpressionInsideBlock.kt
@@ -2,7 +2,6 @@ val x = if (flag) {
     if (flag2) 1<caret>3 else 7
 } else 42
 
-// TYPE: 13 -> <html>Int</html>
 // TYPE: if (flag2) 13 else 7 -> <html>Int</html>
 // TYPE: if (flag) {     if (flag2) 13 else 7 } else 42 -> <html>Int</html>
 // TYPE: val x = if (flag) {     if (flag2) 13 else 7 } else 42 -> <html>Int</html>
