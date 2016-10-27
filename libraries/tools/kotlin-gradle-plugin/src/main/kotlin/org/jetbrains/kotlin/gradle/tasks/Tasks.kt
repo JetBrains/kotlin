764231478052030445b28b25bb24b73770bf9f00
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-10-28 00:14:27 +0300

    Always flush caches

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
@@ -195,7 +195,14 @@ open class KotlinCompile : AbstractKotlinCompile<K2JVMCompilerArguments>(), Kotl
         )
         args.classpathAsList = classpath.toList()
         args.destinationAsFile = destinationDir
-        compiler.compile(allKotlinSources, args, messageCollector, { changedFiles })
+        try {
+            val exitCode = compiler.compile(allKotlinSources, args, messageCollector, { changedFiles })
+            processCompilerExitCode(exitCode)
+        }
+        catch (e: Throwable) {
+            cleanupOnError()
+            throw e
+        }
         anyClassesCompiled = compiler.anyClassesCompiled
     }
 
