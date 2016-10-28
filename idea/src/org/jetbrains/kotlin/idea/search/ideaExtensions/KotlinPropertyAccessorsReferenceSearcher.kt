35130e7e5f2a41a0a6bc451560196ec459901e4b
Dmitry Jemerov <yole@jetbrains.com>
2016-10-28 13:07:05 +0200

    Don't calculate property name if we aren't going to search for property accessors in Kotlin sources (KT-14508)

--- idea/src/org/jetbrains/kotlin/idea/search/ideaExtensions/KotlinPropertyAccessorsReferenceSearcher.kt
+++ idea/src/org/jetbrains/kotlin/idea/search/ideaExtensions/KotlinPropertyAccessorsReferenceSearcher.kt
@@ -19,6 +19,7 @@ package org.jetbrains.kotlin.idea.search.ideaExtensions
 import com.intellij.openapi.application.QueryExecutorBase
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiReference
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.UsageSearchContext
 import com.intellij.psi.search.searches.MethodReferencesSearch
 import com.intellij.util.Processor
@@ -34,9 +35,10 @@ import org.jetbrains.kotlin.synthetic.SyntheticJavaPropertyDescriptor
 class KotlinPropertyAccessorsReferenceSearcher() : QueryExecutorBase<PsiReference, MethodReferencesSearch.SearchParameters>(true) {
     override fun processQuery(queryParameters: MethodReferencesSearch.SearchParameters, consumer: Processor<PsiReference>) {
         val method = queryParameters.method
-        val propertyName = propertyName(method) ?: return
-
         val onlyKotlinFiles = queryParameters.effectiveSearchScope.restrictToKotlinSources()
+        if (onlyKotlinFiles == GlobalSearchScope.EMPTY_SCOPE) return
+
+        val propertyName = propertyName(method) ?: return
 
         queryParameters.optimizer!!.searchWord(
                 propertyName,
