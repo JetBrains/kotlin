502ea9295d4eb056d25f43f3e72e77b23eb96440
shiraji <isogai.shiraji@gmail.com>
2016-10-30 06:27:47 +0900

    KT-14575 Implement "Redundant if" quickfix's negations  #KT-14575 Fixed

--- idea/src/org/jetbrains/kotlin/idea/inspections/RedundantIfInspection.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/RedundantIfInspection.kt
@@ -19,6 +19,7 @@ package org.jetbrains.kotlin.idea.inspections
 import com.intellij.codeInspection.*
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.idea.intentions.negate
 import org.jetbrains.kotlin.psi.*
 
 class RedundantIfInspection : AbstractKotlinInspection(), CleanupLocalInspectionTool {
@@ -27,30 +28,36 @@ class RedundantIfInspection : AbstractKotlinInspection(), CleanupLocalInspection
         return object : KtVisitorVoid() {
             override fun visitIfExpression(expression: KtIfExpression) {
                 super.visitIfExpression(expression)
-
-                if (expression.condition != null && isRedundant(expression)) {
-                    holder.registerProblem(expression,
-                                           "Redundant 'if' statement",
-                                           ProblemHighlightType.LIKE_UNUSED_SYMBOL,
-                                           RemoveRedundantIf)
-                }
+                expression.condition ?: return
+                val type = isRedundant(expression)
+                if (type == RedundantType.NONE) return
+                holder.registerProblem(expression,
+                                       "Redundant 'if' statement",
+                                       ProblemHighlightType.LIKE_UNUSED_SYMBOL,
+                                       RemoveRedundantIf(type))
             }
         }
     }
 
-    private fun isRedundant(expression: KtIfExpression): Boolean {
-        val thenReturn = getReturnedExpression(expression.then) ?: return false
-        val elseReturn = getReturnedExpression(expression.`else`) ?: return false
+    private enum class RedundantType {
+        NONE,
+        THEN_TRUE,
+        ELSE_TRUE
+    }
+
+    private fun isRedundant(expression: KtIfExpression): RedundantType {
+        val thenReturn = getReturnedExpression(expression.then) ?: return RedundantType.NONE
+        val elseReturn = getReturnedExpression(expression.`else`) ?: return RedundantType.NONE
 
         if (KtPsiUtil.isTrueConstant(thenReturn) && KtPsiUtil.isFalseConstant(elseReturn)) {
-            return true
+            return RedundantType.THEN_TRUE
         }
 
         if (KtPsiUtil.isFalseConstant(thenReturn) && KtPsiUtil.isTrueConstant(elseReturn)) {
-            return true
+            return RedundantType.ELSE_TRUE
         }
 
-        return false
+        return RedundantType.NONE
     }
 
     private fun getReturnedExpression(expression: KtExpression?) : KtExpression? {
@@ -64,13 +71,18 @@ class RedundantIfInspection : AbstractKotlinInspection(), CleanupLocalInspection
         }
     }
 
-    private object RemoveRedundantIf : LocalQuickFix {
+    private class RemoveRedundantIf(val redundantType: RedundantType) : LocalQuickFix {
         override fun getName() = "Remove redundant 'if' statement"
         override fun getFamilyName() = name
 
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
             val element = descriptor.psiElement as KtIfExpression
-            element.replace(KtPsiFactory(element).createExpressionByPattern("return $0", element.condition!!.text));
+            val condition = when (redundantType) {
+                RedundantType.NONE -> return
+                RedundantType.THEN_TRUE -> element.condition!!
+                RedundantType.ELSE_TRUE -> element.condition!!.negate()
+            }
+            element.replace(KtPsiFactory(element).createExpressionByPattern("return $0", condition.text))
         }
     }
 
