5bf22c51264119c7d32a180ccd2b86264e343b16
Dmitry Jemerov <yole@jetbrains.com>
2016-11-14 15:56:41 +0100

    workaround for incorrectly compiled PsiTreeUtil.class in AS 2.3: specify explicit type for PsiTreeUtil

--- idea/idea-core/src/org/jetbrains/kotlin/idea/core/ShortenReferences.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/core/ShortenReferences.kt
@@ -439,7 +439,7 @@ class ShortenReferences(val options: (KtElement) -> Options = { Options.DEFAULT 
             val receiver = element.receiverExpression
             if (receiver !is KtThisExpression && bindingContext[BindingContext.QUALIFIER, receiver] == null) return AnalyzeQualifiedElementResult.Skip
 
-            if (PsiTreeUtil.getParentOfType(
+            if (PsiTreeUtil.getParentOfType<KtElement>(
                     element,
                     KtImportDirective::class.java, KtPackageDirective::class.java) != null) return AnalyzeQualifiedElementResult.Skip
 
@@ -616,7 +616,7 @@ class ShortenReferences(val options: (KtElement) -> Options = { Options.DEFAULT 
 
             val receiver = element.receiverExpression
 
-            if (PsiTreeUtil.getParentOfType(
+            if (PsiTreeUtil.getParentOfType<KtElement>(
                     element,
                     KtImportDirective::class.java, KtPackageDirective::class.java) != null) return AnalyzeQualifiedElementResult.Skip
 
