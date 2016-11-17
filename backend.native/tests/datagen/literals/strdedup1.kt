5db25a3985056ca27f0a3f9d5b237ba9f97c7e7d
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2016-11-17 10:34:57 +0700

    backend: add tests for duplicated string literals

---
+++ backend.native/tests/datagen/literals/strdedup1.kt
@@ -0,0 +1,6 @@
+fun main(args : Array<String>) {
+    val str1 = "Hello"
+    val str2 = "Hello"
+    println(str1 == str2)
+    println(str1 === str2)
+}
---
+++ backend.native/tests/datagen/literals/strdedup2.kt
@@ -0,0 +1,6 @@
+fun main(args : Array<String>) {
+    val str1 = "kotlin.Unit"
+    val str2 = Unit.toString()
+    println(str1 == str2)
+    println(str1 === str2)
+}
