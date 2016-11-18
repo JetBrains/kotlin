6c554a0cbc423bc569a2c587d863d8046ebf19d8
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2016-11-18 20:23:01 +0300

    Android tests: use directives from tests-common to determine if the test is supposed to pass on JVM (adds support for IGNORE_BACKEND and IGNORE_BACKEND_WItHOUT_CHECK" directives).

--- compiler/tests-common/org/jetbrains/kotlin/test/InTextDirectivesUtils.java
+++ compiler/tests-common/org/jetbrains/kotlin/test/InTextDirectivesUtils.java
@@ -247,4 +247,9 @@ public final class InTextDirectivesUtils {
     public static boolean isIgnoredTargetWithoutCheck(TargetBackend targetBackend, File file) {
         return isIgnoredTargetByPrefix(targetBackend, file, "// IGNORE_BACKEND_WITHOUT_CHECK: ");
     }
+
+    // Whether the target test is supposed to pass successfully on targetBackend
+    public static boolean isPassingTarget(TargetBackend targetBackend, File file) {
+        return isCompatibleTarget(targetBackend, file) && !isIgnoredTarget(targetBackend, file) && !isIgnoredTargetWithoutCheck(targetBackend, file);
+    }
 }
