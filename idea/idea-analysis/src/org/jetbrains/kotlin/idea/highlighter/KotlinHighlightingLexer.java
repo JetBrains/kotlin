4b0bf5efbcb386e9113250883c85821744e4e81a
Dmitry Jemerov <yole@jetbrains.com>
2016-11-18 13:18:13 +0100

    Highlight escape sequences in character literals  #KT-13795 Fixed
    (cherry picked from commit baf321a)

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/highlighter/KotlinHighlightingLexer.java
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/highlighter/KotlinHighlightingLexer.java
@@ -17,6 +17,7 @@
 package org.jetbrains.kotlin.idea.highlighter;
 
 import com.intellij.lexer.LayeredLexer;
+import com.intellij.lexer.StringLiteralLexer;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.kotlin.kdoc.lexer.KDocLexer;
 import org.jetbrains.kotlin.lexer.KotlinLexer;
@@ -27,5 +28,9 @@ public class KotlinHighlightingLexer extends LayeredLexer {
         super(new KotlinLexer());
 
         registerSelfStoppingLayer(new KDocLexer(), new IElementType[]{KtTokens.DOC_COMMENT}, IElementType.EMPTY_ARRAY);
+        registerSelfStoppingLayer(new StringLiteralLexer('\'', KtTokens.CHARACTER_LITERAL),
+                                  new IElementType[]{KtTokens.CHARACTER_LITERAL}, IElementType.EMPTY_ARRAY);
+
+
     }
 }
