514d1a16ec80e07bc0c513f020ddbf8b66e961e2
Vasily Levchenko <vasily.levchenko@jetbrains.com>
2016-11-18 13:54:34 +0300

    test: minus_eq fix

--- backend.native/tests/codegen/function/minus_eq-test.c
+++ backend.native/tests/codegen/function/minus_eq-test.c
@@ -2,9 +2,9 @@ extern void *resolve_symbol(const char*);
 
 int
 run_test() {
-  int (*plus_eq)(int) = resolve_symbol("kfun:minus_eq(Int)");
+  int (*minus_eq)(int) = resolve_symbol("kfun:minus_eq(Int)");
 
-  if (plus_eq(23) != 12) return 1;
+  if (minus_eq(23) != -12) return 1;
 
   return 0;
 }
