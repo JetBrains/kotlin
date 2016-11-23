f41505091360a2f523dc6ac0f2526e9d6c744ab4
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2016-11-23 19:39:55 +0300

    Suppress TOPLEVEL_TYPEALIASES_ONLY in decompiler and stubBuilder tests.

--- idea/testData/decompiler/decompiledText/TypeAliases.expected.kt
+++ idea/testData/decompiler/decompiledText/TypeAliases.expected.kt
@@ -4,9 +4,10 @@
 package test
 
 public final class TypeAliases public constructor() {
-    public final fun foo(a: dependency.A /* = () -> kotlin.Unit */, b: test.TypeAliases.B /* = (dependency.A /* = () -> kotlin.Unit */) -> kotlin.Unit */, ta: test.Outer<kotlin.String, kotlin.Double>.Inner<kotlin.Int>.TA<kotlin.Boolean> /* = kotlin.collections.Map<kotlin.collections.Map<kotlin.String, out kotlin.Double>, kotlin.collections.Map<kotlin.Int, out kotlin.Boolean>> */): kotlin.Unit { /* compiled code */ }
+    public final fun foo(a: dependency.A /* = () -> kotlin.Unit */, b: test.TypeAliases.B /* = (dependency.A /* = () -> kotlin.Unit */) -> kotlin.Unit */, ta: test.Outer<kotlin.String, kotlin.Double>.Inner<kotlin.Int>.TA<kotlin.Boolean> /* = kotlin.collections.Map<kotlin.collections.Map<kotlin.String, kotlin.Double>, kotlin.collections.Map<kotlin.Int, kotlin.Boolean>> */): kotlin.Unit { /* compiled code */ }
 
-    public typealias B = (dependency.A) -> kotlin.Unit
+    @kotlin.Suppress public typealias B = (dependency.A) -> kotlin.Unit
 
-    @test.Ann private typealias Parametrized<E, F>  = kotlin.collections.Map<E, F>
+    @test.Ann @kotlin.Suppress private typealias Parametrized<E, F>  = kotlin.collections.Map<E, F>
 }
+
--- idea/testData/decompiler/stubBuilder/TypeAliases/TypeAliases.kt
+++ idea/testData/decompiler/stubBuilder/TypeAliases/TypeAliases.kt
@@ -5,6 +5,7 @@ import kotlin.annotation.AnnotationTarget
 
 class Outer<E, F> {
     inner class Inner<G> {
+        @Suppress("TOPLEVEL_TYPEALIASES_ONLY")
         typealias TA<H> = Map<Map<E, F>, Map<G, H>>
     }
 }
@@ -14,6 +15,7 @@ annotation class Ann
 
 class TypeAliases {
 
+    @Suppress("TOPLEVEL_TYPEALIASES_ONLY")
     typealias B = (A) -> Unit
 
     fun foo(a: A, b: B, ta: Outer<String, Double>.Inner<Int>.TA<Boolean>) {
@@ -21,5 +23,6 @@ class TypeAliases {
     }
 
     @Ann
+    @Suppress("TOPLEVEL_TYPEALIASES_ONLY")
     private typealias Parametrized<E, F> = Map<E, F>
 }
