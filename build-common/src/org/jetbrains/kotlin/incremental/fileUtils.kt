cbe680d87621d0301303b46a89c56bfe088ccda2
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-11-25 16:23:34 +0300

    Move file utils
    Fixing IC after moving it from Gradle to the compiler in commit f40a3eff20f463659be66041d78d255a80d12322

--- build-common/src/org/jetbrains/kotlin/incremental/fileUtils.kt
+++ build-common/src/org/jetbrains/kotlin/incremental/fileUtils.kt
@@ -18,23 +18,14 @@ package org.jetbrains.kotlin.incremental
 
 import java.io.File
 
-internal fun File.isJavaFile() =
+fun File.isJavaFile() =
         extension.equals("java", ignoreCase = true)
 
-internal fun File.isKotlinFile(): Boolean =
+fun File.isKotlinFile(): Boolean =
     extension.let {
         "kt".equals(it, ignoreCase = true) ||
         "kts".equals(it, ignoreCase = true)
     }
 
-internal fun File.isClassFile(): Boolean =
+fun File.isClassFile(): Boolean =
         extension.equals("class", ignoreCase = true)
-
-internal fun listClassFiles(path: String): Sequence<File> =
-        File(path).walk().filter { it.isFile && it.isClassFile() }
-
-internal fun File.relativeOrCanonical(base: File): String =
-        relativeToOrNull(base)?.path ?: canonicalPath
-
-internal fun Iterable<File>.pathsAsStringRelativeTo(base: File): String =
-        map { it.relativeOrCanonical(base) }.sorted().joinToString()
