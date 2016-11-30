cb8c3c8cfa24bafe311ae854f143e5d9ed1b95f8
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-11-30 14:26:41 +0100

    [ABI Validation] InlineExposed usages are changed to PublishedApi
    Moved from https://github.com/Kotlin/binary-compatibility-validator/commit/ec7a3e50a24fd95a02f6e98ff019c18e3166fbdd

--- libraries/tools/abi-validation/src/test/kotlin/cases/inline/inlineExposed.kt
+++ libraries/tools/abi-validation/src/test/kotlin/cases/inline/inlineExposed.kt
@@ -1,24 +1,23 @@
-@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 package cases.inline
 
-@kotlin.internal.InlineExposed
+@PublishedApi
 internal fun exposedForInline() {}
 
-@kotlin.internal.InlineExposed
+@PublishedApi
 internal class InternalClassExposed
-    @kotlin.internal.InlineExposed
+    @PublishedApi
     internal constructor() {
 
-    @kotlin.internal.InlineExposed
+    @PublishedApi
     internal fun funExposed() {}
 
     // TODO: Cover unsupported cases: requires correctly reflecting annotations from properties
     /*
-    @kotlin.internal.InlineExposed
+    @PublishedApi
     internal var propertyExposed: String? = null
 
     @JvmField
-    @kotlin.internal.InlineExposed
+    @PublishedApi
     internal var fieldExposed: String? = null
     */
 
