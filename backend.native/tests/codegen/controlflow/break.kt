9afdfc316b245b0d4851de808d7f4e4b0174499a
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2016-12-02 15:55:02 +0300

    TEST: test includes labeled continue-break

--- backend.native/tests/codegen/controlflow/break.kt
+++ backend.native/tests/codegen/controlflow/break.kt
@@ -1,31 +1,52 @@
-fun foo1() {
+fun foo() {
   var i = 0
-  while (true) {
-    if ((i++ % 2) == 0) continue
-    println("while " + i.toString())
-    if (i > 4) break
+  l1@while (true) {
+    println("foo@l1")
+    try {
+      l2@while (true) {
+        if ((i++ % 2) == 0) continue@l2
+        println("foo@l2")
+        if (i > 4) break@l1
+      }
+    } finally {
+    }
   }
 }
 
-fun foo2() {
+fun bar() {
   var i = 0
-  do {
-    if ((i++ % 2) == 0) continue
-    println("while " + i.toString())
-    if (i > 4) break
+  l1@do {
+    try {
+      println("bar@l1")
+      throw Exception()
+    } catch (e: Exception) {
+      l2@do {
+        if ((i++ % 2) == 0) continue@l2
+        println("bar@l2")
+        if (i > 4) break@l1
+      } while (true)
+    }
   } while (true)
 }
 
-fun foo3() {
-  for (i in 1..6) {
-    if ((i % 2) == 0) continue
-    println("while " + i.toString())
-    if (i > 4) break
+fun qux() {
+  l1@for (i in 1..6) {
+    t1@try {
+      println("qux@t1")
+      throw Exception()
+    }
+    finally {
+      l2@ for (j in 1..6) {
+        if ((j % 2) == 0) continue@l2
+        println("qux@l2")
+        if (j > 4) break@l1
+      }
+    }
   }
 }
 
 fun main(args: Array<String>) {
-  foo1()
-  foo2()
-  foo3()
+  foo()
+  bar()
+  qux()
 }
