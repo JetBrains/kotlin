790a6552bb9edaf51157dec50c779f0249fb11e5
Nikolay Krasko <Nikolay.Krasko@jetbrains.com>
2016-12-02 18:19:14 +0300

    LightQuickFixTestCase.parseActionHint() method was removed

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/AbstractQuickFixMultiFileTest.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/AbstractQuickFixMultiFileTest.java
@@ -18,6 +18,7 @@ package org.jetbrains.kotlin.idea.quickfix;
 
 import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.codeInsight.daemon.quickFix.ActionHint;
 import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInsight.intention.impl.ShowIntentionActionsHandler;
@@ -285,10 +286,10 @@ public abstract class AbstractQuickFixMultiFileTest extends KotlinDaemonAnalyzer
                     try {
                         PsiFile psiFile = getFile();
 
-                        Pair<String, Boolean> pair = LightQuickFixTestCase.parseActionHint(psiFile, beforeFile.content);
-                        String text = pair.getFirst();
+                        ActionHint actionHint = ActionHint.parse(psiFile, beforeFile.content);
+                        String text = actionHint.getExpectedText();
 
-                        boolean actionShouldBeAvailable = pair.getSecond();
+                        boolean actionShouldBeAvailable = actionHint.shouldPresent();
 
                         if (psiFile instanceof KtFile) {
                             DirectiveBasedActionUtils.INSTANCE.checkForUnexpectedErrors((KtFile) psiFile);
@@ -300,7 +301,7 @@ public abstract class AbstractQuickFixMultiFileTest extends KotlinDaemonAnalyzer
                         String afterText =
                                 new StringBuilder(actualText).insert(getEditor().getCaretModel().getOffset(), "<caret>").toString();
 
-                        if (pair.second) {
+                        if (actionShouldBeAvailable) {
                             assertNotNull(".after file should exist", afterFile);
                             if (!afterText.equals(afterFile.content)) {
                                 StringBuilder actualTestFile = new StringBuilder();
@@ -388,10 +389,10 @@ public abstract class AbstractQuickFixMultiFileTest extends KotlinDaemonAnalyzer
                     try {
                         PsiFile psiFile = getFile();
 
-                        Pair<String, Boolean> pair = LightQuickFixTestCase.parseActionHint(psiFile, originalFileText);
-                        String text = pair.getFirst();
+                        ActionHint actionHint = ActionHint.parse(psiFile, originalFileText);
+                        String text = actionHint.getExpectedText();
 
-                        boolean actionShouldBeAvailable = pair.getSecond();
+                        boolean actionShouldBeAvailable = actionHint.shouldPresent();
 
                         if (psiFile instanceof KtFile) {
                             DirectiveBasedActionUtils.INSTANCE.checkForUnexpectedErrors((KtFile) psiFile);
