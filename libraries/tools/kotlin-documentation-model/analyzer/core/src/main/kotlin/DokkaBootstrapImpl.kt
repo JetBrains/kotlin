cbf8c43aff8412b8c888081ea9da96a9c6546952
Simon Ogorodnik <sem-oro@yandex.ru>
2016-12-06 16:24:43 +0300

    [KDM] Merge pull request #123 from Kotlin/isolated-classloader
    Now dokka-fatjar complete isolated from poisonous Gradle environment
    Original commit: https://github.com/Kotlin/dokka/commit/2d03ad01f650bc997dce076abaab0a61c2301a96

---
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/DokkaBootstrapImpl.kt
@@ -0,0 +1,69 @@
+package org.jetbrains.dokka
+
+import java.io.File
+import java.util.function.BiConsumer
+
+fun parseSourceLinkDefinition(srcLink: String): SourceLinkDefinition {
+    val (path, urlAndLine) = srcLink.split('=')
+    return SourceLinkDefinition(File(path).absolutePath,
+            urlAndLine.substringBefore("#"),
+            urlAndLine.substringAfter("#", "").let { if (it.isEmpty()) null else "#" + it })
+}
+
+class DokkaBootstrapImpl : DokkaBootstrap {
+
+    class DokkaProxyLogger(val consumer: BiConsumer<String, String>) : DokkaLogger {
+        override fun info(message: String) {
+            consumer.accept("info", message)
+        }
+
+        override fun warn(message: String) {
+            consumer.accept("warn", message)
+        }
+
+        override fun error(message: String) {
+            consumer.accept("error", message)
+        }
+    }
+
+    lateinit var generator: DokkaGenerator
+
+    override fun configure(logger: BiConsumer<String, String>,
+                           moduleName: String,
+                           classpath: List<String>,
+                           sources: List<String>,
+                           samples: List<String>,
+                           includes: List<String>,
+                           outputDir: String,
+                           format: String,
+                           includeNonPublic: Boolean,
+                           reportUndocumented: Boolean,
+                           skipEmptyPackages: Boolean,
+                           skipDeprecated: Boolean,
+                           jdkVersion: Int,
+                           generateIndexPages: Boolean,
+                           sourceLinks: List<String>) {
+        generator = DokkaGenerator(
+                DokkaProxyLogger(logger),
+                classpath,
+                sources,
+                samples,
+                includes,
+                moduleName,
+                DocumentationOptions(
+                        outputDir,
+                        format,
+                        includeNonPublic,
+                        reportUndocumented,
+                        skipEmptyPackages,
+                        skipDeprecated,
+                        jdkVersion,
+                        generateIndexPages,
+                        sourceLinks.map(::parseSourceLinkDefinition)
+                )
+        )
+
+    }
+
+    override fun generate() = generator.generate()
+}
