5a846f94fccc9a2148ada8fc0afccda292aeda7a
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-12-07 15:16:41 +0300

    Rename: SourceRoots.ForJs->KotlinOnly

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
@@ -337,10 +337,10 @@ open class Kotlin2JsCompile() : AbstractKotlinCompile<K2JSCompilerArguments>(), 
         return args
     }
 
-    override fun getSourceRoots() = SourceRoots.ForJs.create(getSource())
+    override fun getSourceRoots() = SourceRoots.KotlinOnly.create(getSource())
 
     override fun callCompiler(args: K2JSCompilerArguments, sourceRoots: SourceRoots, changedFiles: ChangedFiles) {
-        sourceRoots as SourceRoots.ForJs
+        sourceRoots as SourceRoots.KotlinOnly
 
         logger.debug("Calling compiler")
         destinationDir.mkdirs()
