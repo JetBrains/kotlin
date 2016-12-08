282f4fee078165fe4dcc1d441d55ba92a3121968
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2016-12-08 11:23:56 +0300

    TEST: yet another string concatenation test

---
+++ backend.native/tests/codegen/basics/concatenation.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val s = "world"
+    val i = 1
+    println("Hello $s $i ${2*i}")
+
+    for (item in listOf("a", "b")) {
+        println("Hello, $item")
+    }
+}
+
