4ed030d7c5cb5a942861d89ed9b112efff92cd48
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2016-12-12 16:26:58 +0300

    Kotlin Facet: Do not serialize default values of facet configuration

--- idea/src/org/jetbrains/kotlin/idea/facet/KotlinFacetConfiguration.kt
+++ idea/src/org/jetbrains/kotlin/idea/facet/KotlinFacetConfiguration.kt
@@ -20,6 +20,7 @@ import com.intellij.facet.FacetConfiguration
 import com.intellij.facet.ui.FacetEditorContext
 import com.intellij.facet.ui.FacetEditorTab
 import com.intellij.facet.ui.FacetValidatorsManager
+import com.intellij.util.xmlb.SkipDefaultsSerializationFilter
 import com.intellij.util.xmlb.XmlSerializer
 import org.jdom.DataConversionException
 import org.jdom.Element
@@ -50,7 +51,7 @@ class KotlinFacetConfiguration : FacetConfiguration {
     @Suppress("OverridingDeprecatedMember")
     override fun writeExternal(element: Element) {
         element.setAttribute("version", KotlinFacetSettings.CURRENT_VERSION.toString())
-        XmlSerializer.serializeInto(settings, element)
+        XmlSerializer.serializeInto(settings, element, SkipDefaultsSerializationFilter())
     }
 
     override fun createEditorTabs(
