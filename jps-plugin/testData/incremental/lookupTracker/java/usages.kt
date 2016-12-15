509a50431878f63501409d08d43eb337fd66c47d
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2016-12-15 04:07:06 +0300

    Update test data for LookupTracker (removed package kotlin.coroutines from default imports)

--- jps-plugin/testData/incremental/lookupTracker/java/usages.kt
+++ jps-plugin/testData/incremental/lookupTracker/java/usages.kt
@@ -11,27 +11,27 @@ import baz.*
     /*p:bar(C)*/c./*c:bar.C*/func()
     /*p:bar(C) c:bar.C(B)*/c./*c:bar.C*/B()
 
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/C./*c:bar.C*/sfield
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/C./*c:bar.C*/sfield = /*p:kotlin(String)*/"new"
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/C./*c:bar.C*/sfunc()
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io c:bar.C(S)*/C./*c:bar.C*/S()
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/C./*c:bar.C*/sfield
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/C./*c:bar.C*/sfield = /*p:kotlin(String)*/"new"
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/C./*c:bar.C*/sfunc()
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io c:bar.C(S)*/C./*c:bar.C*/S()
 
     // inherited from I
     /*p:bar(C)*/c./*c:bar.C*/ifunc()
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/C./*c:bar.C*/isfield
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/C./*c:bar.C*/isfield
     // expected error: Unresolved reference: IS
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/C./*c:bar.C*/IS()
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/C./*c:bar.C*/IS()
 
 
     val i: /*p:foo*/I = /*p:bar(C)*/c
     /*p:foo(I)*/i./*c:foo.I*/ifunc()
 
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/I./*c:foo.I*/isfield
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io c:foo.I(IS)*/I./*c:foo.I*/IS()
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(String)*/I./*c:foo.I*/isfield
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io c:foo.I(IS)*/I./*c:foo.I*/IS()
 
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/E./*c:baz.E*/F
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(Int)*/E./*c:baz.E*/F./*c:baz.E*/field
-    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.coroutines p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/E./*c:baz.E*/S./*c:baz.E*/func()
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/E./*c:baz.E*/F
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io p:kotlin(Int)*/E./*c:baz.E*/F./*c:baz.E*/field
+    /*p:foo p:baz p:kotlin p:kotlin.annotation p:kotlin.collections p:kotlin.ranges p:kotlin.sequences p:kotlin.text p:java.lang p:kotlin.jvm p:kotlin.io*/E./*c:baz.E*/S./*c:baz.E*/func()
 }
 
 /*p:foo*/fun classifiers(
