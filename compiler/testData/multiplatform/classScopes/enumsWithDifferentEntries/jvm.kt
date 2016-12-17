ee75a9591278fa1d50b2424e35e523df10595f27
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2016-12-17 20:20:04 +0300

    Minor, remove unnecessary test data
    These cases are already tested in diagnostic tests

--- compiler/testData/multiplatform/classScopes/enumsWithDifferentEntries/jvm.kt
+++ compiler/testData/multiplatform/classScopes/enumsWithDifferentEntries/jvm.kt
@@ -1,7 +1,3 @@
 impl enum class AB { A, C }
 
 impl enum class CD { C }
-
-impl enum class EF { E, G, F }
-
-impl enum class GH { G, H }
--- compiler/testData/multiplatform/incompatibleFunctions/common.kt
+++ compiler/testData/multiplatform/incompatibleFunctions/common.kt
@@ -1,14 +1,9 @@
 header infix fun Int.plus(s: CharSequence): Int
-header fun Int.minus(s: String): Int
 
 header operator fun Double.times(x: CharArray)
-header fun Double.divide(x: ByteArray)
 
 header external fun f1()
-header fun g1()
 
 header inline fun f2()
-header fun g2()
 
 header tailrec fun f3()
-header fun g3()
