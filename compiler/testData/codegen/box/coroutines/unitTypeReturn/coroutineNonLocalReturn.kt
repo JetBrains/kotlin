b3142784ff9837ee081007800cf2597a330c659b
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-12-18 11:27:56 +0300

    Refine returns codegen for suspend functions
    This change should make the logic a bit more simple.
    For all suspend functions/coroutines treat them in expression codegen
    like they return boxed version of the original type.
    
    Everything works fine then, except Unit type functions:
    their bodies must be generated just like they're VOID and then load
    Unit on stack manually.

---
+++ compiler/testData/codegen/box/coroutines/unitTypeReturn/coroutineNonLocalReturn.kt
@@ -0,0 +1,46 @@
+// WITH_RUNTIME
+// WITH_COROUTINES
+import kotlin.coroutines.*
+
+suspend fun suspendHere(): String = CoroutineIntrinsics.suspendCoroutineOrReturn { x ->
+    x.resume("OK")
+    CoroutineIntrinsics.SUSPENDED
+}
+
+fun builder(c: suspend () -> Unit) {
+    var wasResumeCalled = false
+    c.startCoroutine(object : Continuation<Unit> {
+        override fun resume(value: Unit) {
+            wasResumeCalled = true
+        }
+
+        override fun resumeWithException(exception: Throwable) {
+
+        }
+    })
+
+    if (!wasResumeCalled) throw RuntimeException("fail 1")
+}
+
+fun box(): String {
+    var result = ""
+
+    builder {
+        run {
+            if (result == "") return@builder
+        }
+        suspendHere()
+        throw RuntimeException("fail 2")
+    }
+
+    result = "fail1"
+
+    builder {
+        run {
+            if (result == "") return@builder
+        }
+        result = suspendHere()
+    }
+
+    return result
+}
---
+++ compiler/testData/codegen/box/coroutines/unitTypeReturn/coroutineReturn.kt
@@ -0,0 +1,42 @@
+// WITH_RUNTIME
+// WITH_COROUTINES
+import kotlin.coroutines.*
+
+suspend fun suspendHere(): String = CoroutineIntrinsics.suspendCoroutineOrReturn { x ->
+    x.resume("OK")
+    CoroutineIntrinsics.SUSPENDED
+}
+
+fun builder(c: suspend () -> Unit) {
+    var wasResumeCalled = false
+    c.startCoroutine(object : Continuation<Unit> {
+        override fun resume(value: Unit) {
+            wasResumeCalled = true
+        }
+
+        override fun resumeWithException(exception: Throwable) {
+
+        }
+    })
+
+    if (!wasResumeCalled) throw RuntimeException("fail 1")
+}
+
+fun box(): String {
+    var result = ""
+
+    builder {
+        if (result == "") return@builder
+        suspendHere()
+        throw RuntimeException("fail 2")
+    }
+
+    result = "fail"
+
+    builder {
+        if (result == "") return@builder
+        result = suspendHere()
+    }
+
+    return result
+}
