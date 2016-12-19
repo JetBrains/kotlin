d72c1cda8e06b9d1f986baa6b2574ac7f10fcd39
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-12-19 18:11:18 +0300

    Minor: remove default value of `retryOnConnectionError` parameter

--- compiler/compiler-runner/src/org/jetbrains/kotlin/compilerRunner/KotlinCompilerRunner.kt
+++ compiler/compiler-runner/src/org/jetbrains/kotlin/compilerRunner/KotlinCompilerRunner.kt
@@ -152,7 +152,7 @@ abstract class KotlinCompilerRunner<in Env : CompilerEnvironment> {
             else -> throw IllegalArgumentException("Unknown compiler type $compilerClassName")
         }
 
-        val res: Int = withDaemon(environment) { daemon, sessionId ->
+        val res: Int = withDaemon(environment, retryOnConnectionError = true) { daemon, sessionId ->
             KotlinCompilerClient.incrementalCompile(daemon, sessionId, targetPlatform, argsArray, services, compilerOut, daemonOut)
         } ?: return null
 
@@ -164,11 +164,11 @@ abstract class KotlinCompilerRunner<in Env : CompilerEnvironment> {
         return exitCode
     }
 
-    protected fun <T> withDaemon(environment: Env, retryOnConnectionError: Boolean = true, fn: (CompileService, sessionId: Int)->T): T? {
+    protected fun <T> withDaemon(environment: Env, retryOnConnectionError: Boolean, fn: (CompileService, sessionId: Int)->T): T? {
         fun retryOrFalse(e: Exception): T? {
             if (retryOnConnectionError) {
                 log.debug("retrying once on daemon connection error: ${e.message}")
-                return withDaemon(environment, false, fn)
+                return withDaemon(environment, retryOnConnectionError = false, fn = fn)
             }
             log.info("daemon connection error: ${e.message}")
             return null
