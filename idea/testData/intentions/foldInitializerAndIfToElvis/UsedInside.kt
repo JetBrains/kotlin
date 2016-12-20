7c0199ead6268b9b2b13ecc4e22c522e8c648f11
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-12-20 17:44:26 +0300

    Fold initializer & if to elvis: search for variable usage inside if first #KT-14889 Fixed
    (cherry picked from commit 67788e3)

---
+++ idea/testData/intentions/foldInitializerAndIfToElvis/UsedInside.kt
@@ -0,0 +1,13 @@
+// IS_APPLICABLE: false
+
+interface A {
+    val s: String
+}
+
+fun foo() = Any()
+
+fun test(): String {
+    val y = foo()
+    <caret>if (y !is A) return y.toString()
+    return y.s
+}
