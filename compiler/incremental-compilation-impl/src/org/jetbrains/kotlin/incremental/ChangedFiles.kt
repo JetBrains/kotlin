e57bbf108e28183b6803b79438555500e1a2dd47
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2016-12-22 15:36:39 +0300

    Move IC from Gradle to compiler
    Reasons for moving:
    1. Integration with Kotlin daemon for Gradle.
    Gradle bundles Kotlin compiler (kotlin-compiler-embeddable) since Gradle 3.2.
    There is no plugin isolation in Gradle, so strange compilation errors/exceptions may happen.
    We decided to fix this problem by compiling out-of-process using Kotlin daemon.
    
    Reasons for moving IC to the compiler:
    * Better isolation from Gradle process.
    * Incremental compilation logic is not scattered across two process and multiple modules.
    * Makes it possible to implement standalone CLI IC
    
    # Conflicts:
    #	build.xml
    #	libraries/pom.xml
    #	libraries/tools/kotlin-build-common/pom.xml

--- compiler/incremental-compilation-impl/src/org/jetbrains/kotlin/incremental/ChangedFiles.kt
+++ compiler/incremental-compilation-impl/src/org/jetbrains/kotlin/incremental/ChangedFiles.kt
@@ -20,14 +20,7 @@ import org.gradle.api.tasks.incremental.IncrementalTaskInputs
 import java.io.File
 import java.util.*
 
-internal fun ChangedFiles(taskInputs: IncrementalTaskInputs): ChangedFiles {
-    if (!taskInputs.isIncremental) return ChangedFiles.Unknown()
-
-    val modified = ArrayList<File>()
-    val removed = ArrayList<File>()
-
-    taskInputs.outOfDate { modified.add(it.file) }
-    taskInputs.removed { removed.add(it.file) }
-
-    return ChangedFiles.Known(modified, removed)
+internal sealed class ChangedFiles {
+    class Known(val modified: List<File>, val removed: List<File>) : ChangedFiles()
+    class Unknown : ChangedFiles()
 }
