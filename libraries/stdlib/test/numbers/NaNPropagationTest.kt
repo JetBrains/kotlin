897e9658ac2033976c893b94cdbf884926c84111
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-12-28 04:19:13 +0300

    Do not run total order tests on JS

--- libraries/stdlib/test/numbers/NaNPropagationTest.kt
+++ libraries/stdlib/test/numbers/NaNPropagationTest.kt
@@ -5,10 +5,10 @@ import kotlin.test.assertTrue
 import kotlin.comparisons.*
 import kotlin.test.assertEquals
 
-class NaNPropagationTest {
+val Double.Companion.values get() = listOf(0.0, NEGATIVE_INFINITY, MIN_VALUE, MAX_VALUE, POSITIVE_INFINITY, NaN)
+val Float.Companion.values get() = listOf(0.0f, NEGATIVE_INFINITY, MIN_VALUE, MAX_VALUE, POSITIVE_INFINITY, NaN)
 
-    val Double.Companion.values get() = listOf(0.0, NEGATIVE_INFINITY, MIN_VALUE, MAX_VALUE, POSITIVE_INFINITY, NaN)
-    val Float.Companion.values get() = listOf(0.0f, NEGATIVE_INFINITY, MIN_VALUE, MAX_VALUE, POSITIVE_INFINITY, NaN)
+class NaNPropagationTest {
 
     private fun propagateOf2(f2d: (Double, Double) -> Double,
                              f2f: (Float, Float) -> Float,
@@ -104,7 +104,10 @@ class NaNPropagationTest {
                      "sequenceOf().max()")
     }
 
+}
 
+@JvmVersion
+class NaNTotalOrderTest {
 
     private fun <T : Comparable<T>> totalOrderMinOf2(f2t: (T, T) -> T, function: String) {
         @Suppress("UNCHECKED_CAST")
@@ -151,6 +154,4 @@ class NaNPropagationTest {
     @Test fun sequenceTMax() {
         totalOrderMaxOf2<Comparable<Any>>({ a, b -> sequenceOf(a, b).max()!! }, "sequenceOf().max()")
     }
-
-
 }
