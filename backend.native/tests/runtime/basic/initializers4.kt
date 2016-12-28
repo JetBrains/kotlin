ddf47e93591b3fde9c8548e2d7b5fa602ec9d4fb
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-12-28 15:26:07 +0300

    Add more init tests.

---
+++ backend.native/tests/runtime/basic/initializers4.kt
@@ -0,0 +1,7 @@
+const val INT_MAX_POWER_OF_TWO: Int = Int.MAX_VALUE / 2 + 1
+val DOUBLE = Double.MAX_VALUE - 1.0
+
+fun main(args: Array<String>) {
+    println(INT_MAX_POWER_OF_TWO)
+    println(DOUBLE > 0.0)
+}
