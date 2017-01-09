9a363ceaf6f1ab72a268024b710416dc0896edca
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2017-01-09 16:56:26 +0700

    TESTS: new tests added

---
+++ backend.native/tests/codegen/inline/inline2.kt
@@ -0,0 +1,16 @@
+@Suppress("NOTHING_TO_INLINE")
+inline fun foo(i4: Int, i5: Int): Int {
+    try {
+        return i4 / i5
+    } catch (e: Throwable) {
+        return i4
+    }
+}
+
+fun bar(i1: Int, i2: Int, i3: Int): Int {
+    return i1 + foo(i2, i3)
+}
+
+fun main(args: Array<String>) {
+    println(bar(1, 8, 2).toString())
+}
