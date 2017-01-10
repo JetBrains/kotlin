e647f561240537887c56616733662fc9bee731c6
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-01-10 14:41:03 +0300

    Maven: multiplatform integration test

---
+++ libraries/tools/kotlin-maven-plugin-test/src/it/test-multiplatform/jvm/src/main/kotlin/org/jetbrains/HelloWorld.kt
@@ -0,0 +1,9 @@
+package org.jetbrains
+
+fun main(args : Array<String>) {
+    doMain()
+}
+
+impl fun doMain() {
+    System.out?.println(getGreeting())
+}
---
+++ libraries/tools/kotlin-maven-plugin-test/src/it/test-multiplatform/shared/src/main/kotlin/org/jetbrains/api.kt
@@ -0,0 +1,7 @@
+package org.jetbrains
+
+header fun doMain()
+
+fun getGreeting() : String {
+    return "Hello, World!"
+}
