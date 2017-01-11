379306b500b9ae0481156c24f6ee71f0b1dd306a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-01-11 19:56:59 +0300

    Add headers for print and println. #KT-15458 fixed

---
+++ libraries/stdlib/common/src/kotlin/ConsoleH.kt
@@ -0,0 +1,11 @@
+package kotlin.io
+
+
+/** Prints a newline to the standard output stream. */
+public header fun println()
+
+/** Prints the given message and newline to the standard output stream. */
+public header fun println(message: Any?)
+
+/** Prints the given message to the standard output stream. */
+public header fun print(message: Any?)
