ae117b020e92a5bbc9abf97885c4a8f2ef3c32bd
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2017-01-11 16:03:51 +0700

    runtime: enable some stdlib code

--- runtime/src/main/kotlin/konan/Annotations.kt
+++ runtime/src/main/kotlin/konan/Annotations.kt
@@ -27,19 +27,9 @@ public annotation class Used
 // Following annotations can be used to mark functions that need to be fixed,
 // once certain language feature is implemented.
 /**
- * Need to be fixed because of boxing.
- */
-public annotation class FixmeBoxing
-
-/**
  * Need to be fixed because of inner classes.
  */
 public annotation class FixmeInner
-
-/**
- * Need to be fixed because of lambdas.
- */
-public annotation class FixmeLambda
 
 /**
  * Need to be fixed.
