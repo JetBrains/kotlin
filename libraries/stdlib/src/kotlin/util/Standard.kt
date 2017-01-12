4d5396b1c04d5b4c39cfcb2108f6df14889c6c96
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-01-12 04:57:38 +0300

    Introduce new scope function: 'also'. Works like 'apply' but with 'it' instead of 'this' inside lambda. #KT-6903
    (cherry picked from commit 1ac9815)

--- libraries/stdlib/src/kotlin/util/Standard.kt
+++ libraries/stdlib/src/kotlin/util/Standard.kt
@@ -49,11 +49,17 @@ public inline fun <T, R> with(receiver: T, block: T.() -> R): R = receiver.block
 public inline fun <T> T.apply(block: T.() -> Unit): T { block(); return this }
 
 /**
+ * Calls the specified function [block] with `this` value as its argument and returns `this` value.
+ */
+@kotlin.internal.InlineOnly
+@SinceKotlin("1.1")
+public inline fun <T> T.also(block: (T) -> Unit): T { block(this); return this }
+
+/**
  * Calls the specified function [block] with `this` value as its argument and returns its result.
  */
 @kotlin.internal.InlineOnly
 public inline fun <T, R> T.let(block: (T) -> R): R = block(this)
-
 
 /**
  * Executes the given function [action] specified number of [times].
