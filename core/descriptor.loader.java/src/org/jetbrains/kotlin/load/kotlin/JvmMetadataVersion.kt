8499db1f42e555fb9ad2b7d4c16cfa1a36849b5f
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-01-17 15:00:52 +0300

    Update JvmMetadataVersion to 1.1.3
    This is needed because 1.1.2 binaries are considered pre-release (see
    DeserializedDescriptorResolver), so it wasn't possible to compile
    non-pre-release binaries with -language-version 1.0

--- core/descriptor.loader.java/src/org/jetbrains/kotlin/load/kotlin/JvmMetadataVersion.kt
+++ core/descriptor.loader.java/src/org/jetbrains/kotlin/load/kotlin/JvmMetadataVersion.kt
@@ -32,7 +32,7 @@ class JvmMetadataVersion(vararg numbers: Int) : BinaryVersion(*numbers) {
         var skipCheck: Boolean = false
 
         @JvmField
-        val INSTANCE = JvmMetadataVersion(1, 1, 2)
+        val INSTANCE = JvmMetadataVersion(1, 1, 3)
 
         @JvmField
         val INVALID_VERSION = JvmMetadataVersion()
