76a20fc104897feb719b7bcc69cebae82fb5d072
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2017-01-20 15:11:37 +0300

    Minor: fix copyright in polyfills.js

--- js/js.libraries/src/js/polyfills.js
+++ js/js.libraries/src/js/polyfills.js
@@ -1,11 +1,11 @@
-/**
- * Copyright 2010 Tim Down.
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
