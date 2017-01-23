a856b0d9ceb350484fc6904a9940d0a72affd10c
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-01-23 20:32:24 +0300

    More accurate host- and cross- target separations.

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/Runtime.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/Runtime.kt
@@ -11,6 +11,7 @@ class Runtime(private val bitcodeFile: String) {
 
             val bufRef = alloc<LLVMMemoryBufferRefVar>()
             val errorRef = allocPointerTo<CInt8Var>()
+
             val res = LLVMCreateMemoryBufferWithContentsOfFile(bitcodeFile, bufRef.ptr, errorRef.ptr)
             if (res != 0) {
                 throw Error(errorRef.value?.asCString()?.toString())
