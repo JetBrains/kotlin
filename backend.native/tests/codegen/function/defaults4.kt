cf946a7676e028615954a652244230bf6f1ff03c
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2017-01-26 13:53:40 +0700

    backend/tests: update and disable 'defaults4'

--- backend.native/tests/codegen/function/defaults4.kt
+++ backend.native/tests/codegen/function/defaults4.kt
@@ -2,10 +2,12 @@ open class A {
     open fun foo(x: Int = 42) = println(x)
 }
 
-class B : A() {
+open class B : A()
+
+class C : B() {
     override fun foo(x: Int) = println(x + 1)
 }
 
 fun main(args: Array<String>) {
-    B().foo()
+    C().foo()
 }
