6ef96017e8ba6258ba7a6be1ef7b28672051c7c1
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2017-01-26 10:28:43 +0300

    Mute test and create issue KT-15972.

--- compiler/testData/diagnostics/tests/substitutions/starProjections.kt
+++ compiler/testData/diagnostics/tests/substitutions/starProjections.kt
@@ -16,9 +16,9 @@ interface B<R, T: B<List<R>, <!UPPER_BOUND_VIOLATED!>T<!>>> {
 }
 
 fun testB(b: B<*, *>) {
-    b.r().checkType { _<Any?>() }
-    b.t().checkType { _<B<List<*>, *>>() }
+    <!TYPE_MISMATCH(B<out Any?, out B<List<*>, *>>; B<*, *>)!>b<!>.r().checkType { _<Any?>() }
+    <!TYPE_MISMATCH(B<out Any?, out B<List<*>, *>>; B<*, *>)!>b<!>.t().checkType { _<B<List<*>, *>>() }
 
-    b.t().r().size
+    <!TYPE_MISMATCH(B<List<Any?>, out B<List<*>, *>>; B<List<*>, *>)!><!TYPE_MISMATCH(B<out Any?, out B<List<*>, *>>; B<*, *>)!>b<!>.t()<!>.r().size
 }
 
