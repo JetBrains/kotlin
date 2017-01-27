96d0f91afc8d4db1d2729bd63eff363d97e52884
Kirill Rakhman <kirill.rakhman@gmail.com>
2017-01-27 12:35:33 +0100

    Implement Smart Enter handler for class body (#1022)
    Fixes #KT-9011

--- idea/tests/org/jetbrains/kotlin/idea/codeInsight/smartEnter/SmartEnterTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/codeInsight/smartEnter/SmartEnterTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1352,6 +1352,30 @@ class SmartEnterTest : KotlinLightCodeInsightFixtureTestCase() {
                 init {
                     <caret>
                 }
+            }
+            """
+    )
+
+    fun testClassBody1() = doFileTest(
+            """
+            class Foo<caret>
+            """
+            ,
+            """
+            class Foo {
+                <caret>
+            }
+            """
+    )
+
+    fun testClassBody2() = doFileTest(
+            """
+            class <caret>Foo
+            """
+            ,
+            """
+            class Foo {
+                <caret>
             }
             """
     )
