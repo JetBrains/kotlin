f516a08415d4f57c620e71d3e53e17e77e643a2b
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-01-30 13:24:17 +0300

    Report error on callable reference or class literal with "-no-stdlib"
    Also workaround the NoSuchElementException that was happening because error
    type never has any type arguments
    
     #KT-14547 Fixed
    
    (cherry picked from commit 8811165de8e2dbffc5422942d41704392ed34578)

--- compiler/frontend/src/org/jetbrains/kotlin/types/expressions/DoubleColonExpressionResolver.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/types/expressions/DoubleColonExpressionResolver.kt
@@ -99,6 +99,9 @@ class DoubleColonExpressionResolver(
                 checkClassLiteral(c, expression, result)
                 val variance = if (result is DoubleColonLHS.Expression && !result.isObject) Variance.OUT_VARIANCE else Variance.INVARIANT
                 val kClassType = reflectionTypes.getKClassType(Annotations.EMPTY, type, variance)
+                if (kClassType.isError) {
+                    c.trace.report(MISSING_DEPENDENCY_CLASS.on(expression.receiverExpression!!, KotlinBuiltIns.FQ_NAMES.kClass.toSafe()))
+                }
                 val dataFlowInfo = (result as? DoubleColonLHS.Expression)?.dataFlowInfo ?: c.dataFlowInfo
                 return dataFlowAnalyzer.checkType(createTypeInfo(kClassType, dataFlowInfo), expression, c)
             }
@@ -509,6 +512,9 @@ class DoubleColonExpressionResolver(
         checkReferenceIsToAllowedMember(descriptor, context.trace, expression)
 
         val type = createKCallableTypeForReference(descriptor, lhs, reflectionTypes, context.scope.ownerDescriptor) ?: return null
+        if (type.isError) {
+            context.trace.report(MISSING_DEPENDENCY_CLASS.on(expression, KotlinBuiltIns.FQ_NAMES.kCallable.toSafe()))
+        }
 
         when (descriptor) {
             is FunctionDescriptor -> bindFunctionReference(expression, type, context)
@@ -551,10 +557,13 @@ class DoubleColonExpressionResolver(
                 /* isCoroutine = */ false
         )
 
+        val parameterTypes = if (type.isError) emptyList() else type.arguments.dropLast(1)
+        val returnType = if (type.isError) null else type.arguments.last().type
+
         functionDescriptor.initialize(
                 null, null, emptyList(),
-                createValueParametersForInvokeInFunctionType(functionDescriptor, type.arguments.dropLast(1)),
-                type.arguments.last().type,
+                createValueParametersForInvokeInFunctionType(functionDescriptor, parameterTypes),
+                returnType,
                 Modality.FINAL,
                 Visibilities.PUBLIC
         )
