dcb19df3d30c545c731581c2b069256a36c488c9
mglukhikh <glukhikh@mail.ru>
2017-01-31 13:18:50 +0300

    Quick doc provider test fix (2016.1 platform specific)

--- idea/testData/editor/quickDoc/KotlinPackageClassUsedFromJava.java
+++ idea/testData/editor/quickDoc/KotlinPackageClassUsedFromJava.java
@@ -7,4 +7,4 @@ class KotlinPackageClassUsedFromJava {
 }
 
 //INFO: <html><head>    <style type="text/css">        #error {            background-color: #eeeeee;            margin-bottom: 10px;        }        p {            margin: 5px 0;        }    </style></head><body><small><b>testing</b></small><PRE>public final class <b>testing.KotlinPackageClassUsedFromJava_DataKt</b>
-//INFO: extends <a href="psi_element://java.lang.Object"><code>Object</code></a></PRE></body></html>
+//INFO: extends <a href="psi_element://java.lang.Object"><code>java.lang.Object</code></a></PRE></body></html>
