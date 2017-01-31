e57b3651c238ffc5f778b7346da890e69649fadc
Nikolay Krasko <nikolay.krasko@jebrains.com>
2017-01-31 16:38:26 +0300

    Replace removed method in LibraryPresentationProviders

--- idea/src/org/jetbrains/kotlin/idea/framework/JavaRuntimePresentationProvider.java
+++ idea/src/org/jetbrains/kotlin/idea/framework/JavaRuntimePresentationProvider.java
@@ -18,7 +18,6 @@ package org.jetbrains.kotlin.idea.framework;
 
 import com.intellij.framework.library.LibraryVersionProperties;
 import com.intellij.openapi.roots.libraries.LibraryPresentationProvider;
-import com.intellij.openapi.roots.libraries.LibraryProperties;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -38,7 +37,7 @@ public class JavaRuntimePresentationProvider extends LibraryPresentationProvider
 
     @Nullable
     @Override
-    public Icon getIcon(@Nullable LibraryProperties properties) {
+    public Icon getIcon(@Nullable LibraryVersionProperties properties) {
         return KotlinIcons.SMALL_LOGO;
     }
 
