e10d9b62e2f21644524150da67c76c0e1a570202
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2017-01-31 16:01:46 +0700

    TESTS: new tests added

---
+++ backend.native/tests/codegen/inline/inline5.kt
@@ -0,0 +1,13 @@
+@Suppress("NOTHING_TO_INLINE")
+inline fun foo(i2: Int, body: () -> Int): Int {
+    return i2 + body()
+}
+
+fun bar(i1: Int): Int {
+    return foo(i1) { 33 }
+}
+
+fun main(args: Array<String>) {
+    println(bar(1).toString())
+}
+
--- backend.native/tests/codegen/inline/inline9.kt
+++ backend.native/tests/codegen/inline/inline9.kt
@@ -1,12 +1,17 @@
 @Suppress("NOTHING_TO_INLINE")
-inline fun foo(i4: Int, i5: Int): Int {
-    return i4 + i5
+inline fun foo(i3: Int, i4: Int): Int {
+    return i3 + i3 + i4
 }
 
-fun bar(i1: Int, i2: Int, i3: Int): Int {
-    return foo(i1 + i2, i3)
+fun quiz(i: Int) : Int {
+    println("hello")
+    return i + 1
+}
+
+fun bar(i1: Int, i2: Int): Int {
+    return foo(quiz(i1), i2)
 }
 
 fun main(args: Array<String>) {
-    println(bar(1, 2, 3).toString())
+    println(bar(1, 2).toString())
 }
