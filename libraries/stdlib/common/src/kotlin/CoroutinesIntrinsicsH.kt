d73d42cc2374e9c18ed257f14d3c77d4658a17de
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-02-03 14:15:38 +0300

    Put startCoroutineUninterceptedOrReturn header to common stdlib
    (cherry picked from commit 9697196)

--- libraries/stdlib/common/src/kotlin/CoroutinesIntrinsicsH.kt
+++ libraries/stdlib/common/src/kotlin/CoroutinesIntrinsicsH.kt
@@ -18,6 +18,31 @@ package kotlin.coroutines.experimental.intrinsics
 
 import kotlin.coroutines.experimental.Continuation
 
+/**
+ * Starts unintercepted coroutine without receiver and with result type [T] and executes it until its first suspension.
+ * Returns the result of the coroutine or throws its exception if it does not suspend or [COROUTINE_SUSPENDED] if it suspends.
+ * In the later case, the [completion] continuation is invoked when coroutine completes with result or exception.
+ * This function is designed to be used from inside of [suspendCoroutineOrReturn] to resume the execution of suspended
+ * coroutine using a reference to the suspending function.
+ */
+@SinceKotlin("1.1")
+public header inline fun <T> (suspend () -> T).startCoroutineUninterceptedOrReturn(
+        completion: Continuation<T>
+): Any?
+
+/**
+ * Starts unintercepted coroutine with receiver type [R] and result type [T] and executes it until its first suspension.
+ * Returns the result of the coroutine or throws its exception if it does not suspend or [COROUTINE_SUSPENDED] if it suspends.
+ * In the later case, the [completion] continuation is invoked when coroutine completes with result or exception.
+ * This function is designed to be used from inside of [suspendCoroutineOrReturn] to resume the execution of suspended
+ * coroutine using a reference to the suspending function.
+ */
+@SinceKotlin("1.1")
+public header inline fun <R, T> (suspend R.() -> T).startCoroutineUninterceptedOrReturn(
+        receiver: R,
+        completion: Continuation<T>
+): Any?
+
 @SinceKotlin("1.1")
 public header fun <T> (suspend () -> T).createCoroutineUnchecked(
         completion: Continuation<T>
