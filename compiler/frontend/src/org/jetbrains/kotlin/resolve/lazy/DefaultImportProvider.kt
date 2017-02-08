08915389e271a996fddfaa231f848414d3c37302
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2017-02-08 17:47:05 +0300

    Fix for complete slowdown of DefaultImportProvider after introducing languageVersion for default imports KT-16243 Very slow completion after variable of type `ArrayList`  #KT-16243 fixed
    (cherry picked from commit a9f2f5c)

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/DefaultImportProvider.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/DefaultImportProvider.kt
@@ -39,7 +39,7 @@ class DefaultImportProvider(
         private val languageVersionSettings: LanguageVersionSettings
 ) {
     val defaultImports: List<ImportPath>
-        get() = targetPlatform.getDefaultImports(languageVersionSettings)
+            by storageManager.createLazyValue { targetPlatform.getDefaultImports(languageVersionSettings) }
 
     val excludedImports: List<FqName> by storageManager.createLazyValue {
         val packagesWithAliases = listOf(KotlinBuiltIns.BUILT_INS_PACKAGE_FQ_NAME, KotlinBuiltIns.TEXT_PACKAGE_FQ_NAME)
