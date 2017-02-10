296fe698b82fd7fa6a4abd07996027a51bc96f35
Dmitry Jemerov <yole@jetbrains.com>
2017-02-10 18:15:36 +0100

    KotlinAnnotatedElementsSearcher optimization
    When possible, try to resolve references in types using only the PSI
    without building the full resolve session.

--- idea/tests/org/jetbrains/kotlin/search/AnnotatedMembersSearchTest.java
+++ idea/tests/org/jetbrains/kotlin/search/AnnotatedMembersSearchTest.java
@@ -19,6 +19,10 @@ package org.jetbrains.kotlin.search;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.search.searches.AnnotatedMembersSearch;
+import com.intellij.testFramework.LightProjectDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.kotlin.idea.search.PsiBasedClassResolver;
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor;
 import org.jetbrains.kotlin.idea.test.PluginTestCaseBase;
 import org.jetbrains.kotlin.test.InTextDirectivesUtils;
 
@@ -27,6 +31,11 @@ import java.io.IOException;
 import java.util.List;
 
 public class AnnotatedMembersSearchTest extends AbstractSearcherTest {
+    @NotNull
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE;
+    }
 
     public void testAnnotationsOnClass() throws IOException {
         doTest();
@@ -40,14 +49,51 @@ public class AnnotatedMembersSearchTest extends AbstractSearcherTest {
         doTest();
     }
 
+    public void testNestedClassAsAnnotation() throws IOException {
+        doTest();
+    }
+
+    public void testAmbiguousNestedNonAnnotationClass() throws IOException {
+        doTest();
+    }
+
+    public void testAmbiguousNestedPrivateAnnotationClass() throws IOException {
+        doTest();
+    }
+
+    public void testNestedPrivateAnnotationClass() throws IOException {
+        doTest();
+    }
+
+    public void testTypeAlias() throws IOException {
+        doTest();
+    }
+
+    public void testDefaultImport() throws IOException {
+        doTest();
+    }
+
     private void doTest() throws IOException {
         myFixture.configureByFile(getFileName());
-        List<String> directives = InTextDirectivesUtils.findListWithPrefixes(FileUtil.loadFile(new File(getPathToFile()), true),
+        String fileText = FileUtil.loadFile(new File(getPathToFile()), true);
+        List<String> directives = InTextDirectivesUtils.findListWithPrefixes(fileText,
                                                                              "// ANNOTATION: ");
         assertFalse("Specify ANNOTATION directive in test file", directives.isEmpty());
         String annotationClassName = directives.get(0);
         PsiClass psiClass = getPsiClass(annotationClassName);
+        PsiBasedClassResolver.Companion.getTrueHits().set(0);
+        PsiBasedClassResolver.Companion.getFalseHits().set(0);
+
         checkResult(AnnotatedMembersSearch.search(psiClass, getProjectScope()));
+
+        Integer optimizedTrue = InTextDirectivesUtils.getPrefixedInt(fileText, "// OPTIMIZED_TRUE:");
+        if (optimizedTrue != null) {
+            assertEquals(optimizedTrue.intValue(), PsiBasedClassResolver.Companion.getTrueHits().get());
+        }
+        Integer optimizedFalse = InTextDirectivesUtils.getPrefixedInt(fileText, "// OPTIMIZED_FALSE:");
+        if (optimizedFalse != null) {
+            assertEquals(optimizedFalse.intValue(), PsiBasedClassResolver.Companion.getFalseHits().get());
+        }
     }
 
     @Override
