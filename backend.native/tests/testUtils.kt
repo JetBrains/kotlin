661d66477c03745d4fa17684cc7d799a135bab86
Vasily Levchenko <vasily.levchenko@jetbrains.com>
2017-02-13 11:53:01 +0300

    TEST: assertEquals msg paramter is optional

--- backend.native/tests/testUtils.kt
+++ backend.native/tests/testUtils.kt
@@ -2,4 +2,4 @@ package kotlin.test
 
 class TestFailedException(val msg:String):RuntimeException(msg)
 
-fun <T> assertEquals(a:T, b:T, msg:String) { if (a != b) throw TestFailedException(msg) }
+fun <T> assertEquals(a:T, b:T, msg:String = "") { if (a != b) throw TestFailedException(msg) }
