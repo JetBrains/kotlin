1298a7c18332a86a69305da7c0d54f95c0720898
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-02-16 16:03:45 +0300

    Fix compilation for 1.1

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inline/SMAP.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inline/SMAP.kt
@@ -120,7 +120,7 @@ open class NestedSourceMapper(
             value, key ->
             if (key.dest in value) 0 else RangeMapping.Comparator.compare(value, key)
         })
-        return if (index < 0) null else ranges[index];
+        return if (index < 0) null else ranges[index]
     }
 }
 
@@ -189,7 +189,7 @@ open class DefaultSourceMapper(val sourceInfo: SourceInfo) : SourceMapper {
 
     protected val origin: RawFileMapping
 
-    var callSiteMarker: CallSiteMarker? = null;
+    var callSiteMarker: CallSiteMarker? = null
         set(value) {
             lastMappedWithChanges = null
             field = value
