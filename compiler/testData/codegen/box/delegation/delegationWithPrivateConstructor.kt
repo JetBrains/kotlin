fe956668b3fa96ff16b6e4af8d96d8e0bc8b6350
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-02 14:31:30 +0300

    Add test for class delegation with private constructor
     #KT-16583 Obsolete

---
+++ compiler/testData/codegen/box/delegation/delegationWithPrivateConstructor.kt
@@ -0,0 +1,17 @@
+class MyObject private constructor(val delegate: Interface) : Interface by delegate {
+    constructor() : this(Delegate())
+}
+
+class Delegate : Interface {
+    override fun greet(): String {
+        return "OK"
+    }
+}
+
+private interface Interface {
+    fun greet(): String
+}
+
+fun box(): String {
+    return MyObject().greet()
+}
