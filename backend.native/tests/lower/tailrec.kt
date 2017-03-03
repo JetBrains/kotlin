8427612d59d911670806cb26e3fddfc1636441f1
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2017-03-03 12:08:00 +0700

    backend/tests: add trivial tests for `tailrec`

---
+++ backend.native/tests/lower/tailrec.kt
@@ -0,0 +1,64 @@
+fun main(args: Array<String>) {
+    println(add(5, 7))
+    println(add(100000000, 0))
+
+    println(fib(6))
+
+    println(one(5))
+
+    countdown(3)
+
+    println(listOf(1, 2, 3).indexOf(3))
+    println(listOf(1, 2, 3).indexOf(4))
+
+    println(Integer(5).isLessOrEqualThan(7))
+    println(Integer(42).isLessOrEqualThan(1))
+}
+
+tailrec fun add(x: Int, y: Int): Int = if (x > 0) add(x - 1, y + 1) else y
+
+fun fib(n: Int): Int {
+    tailrec fun fibAcc(n: Int, acc: Int): Int = if (n < 2) {
+        n + acc
+    } else {
+        fibAcc(n - 1, fibAcc(n - 2, acc))
+    }
+
+    return fibAcc(n, 0)
+}
+
+tailrec fun one(delay: Int, result: Int = delay + 1): Int = if (delay > 0) one(delay - 1) else result
+
+tailrec fun countdown(iterations: Int): Unit {
+    if (iterations > 0) {
+        println("$iterations ...")
+        countdown(iterations - 1)
+    } else {
+        println("ready!")
+    }
+}
+
+tailrec fun <T> List<T>.indexOf(x: T, startIndex: Int = 0): Int {
+    if (startIndex >= this.size) {
+        return -1
+    }
+
+    if (this[startIndex] != x) {
+        return this.indexOf(x, startIndex + 1)
+    }
+
+    return startIndex
+
+}
+
+open class Integer(val value: Int) {
+    open tailrec fun isLessOrEqualThan(value: Int): Boolean {
+        if (this.value == value) {
+            return true
+        } else if (value > 0) {
+            return this.isLessOrEqualThan(value - 1)
+        } else {
+            return false
+        }
+    }
+}
