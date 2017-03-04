1296c5444b6e30304893b02fb6a96f4b2ba50fd1
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-03-04 06:25:49 +0300

    Fix warning that failed maven plugin smoke test.

--- libraries/tools/kotlin-maven-plugin/src/it/simple/verify.groovy
+++ libraries/tools/kotlin-maven-plugin/src/it/simple/verify.groovy
@@ -34,6 +34,8 @@ State state = new File(basedir, "build.log").readLines().collect { it.replaceAll
                 replace("\\", "/").
                 replaceAll(/[0-9]+\s*ms/, "LLL ms").
                 trim().
+                // TODO: Remove after build is launched only under java 8
+                replaceAll(/\[WARNING] Running the Kotlin compiler under Java 6 or 7 is unsupported and will no longer be possible in a future update./, "").
                 replaceAll(/^\[[A-Z]+\]$/, "").
                 replaceAll(/version [0-9a-z-+\.]+/, "version @snapshot@")
 
