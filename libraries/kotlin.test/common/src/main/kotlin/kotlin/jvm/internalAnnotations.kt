08fa304b6fd413f1005388a8a49578313a48508a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-03-06 19:47:51 +0300

    Build common standard library headers and kotlin-test (common and jvm) as multiplatform projects.

--- libraries/kotlin.test/common/src/main/kotlin/kotlin/jvm/internalAnnotations.kt
+++
@@ -1,9 +0,0 @@
-package kotlin.jvm
-
-@Suppress("HEADER_WITHOUT_IMPLEMENTATION")
-@Target(AnnotationTarget.FILE)
-internal header annotation class JvmMultifileClass
-
-@Suppress("HEADER_WITHOUT_IMPLEMENTATION")
-@Target(AnnotationTarget.FILE)
-internal header annotation class JvmName(val name: String)
