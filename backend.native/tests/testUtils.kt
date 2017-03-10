9e15873d6c59c32bf0ee3343b5acd664d952f331
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2017-03-10 12:16:17 +0700

    backend/testUtils: add assertTrue and assertFalse

--- backend.native/tests/testUtils.kt
+++ backend.native/tests/testUtils.kt
@@ -3,3 +3,15 @@ package kotlin.test
 class TestFailedException(val msg:String):RuntimeException(msg)
 
 fun <T> assertEquals(a:T, b:T, msg:String = "") { if (a != b) throw TestFailedException(msg) }
+
+/** Asserts that the expression is `true` with an optional [message]. */
+fun assertTrue(actual: Boolean, message: String? = null) {
+    if (actual != true) {
+        throw TestFailedException(message ?: "Expected value to be true.")
+    }
+}
+
+/** Asserts that the expression is `false` with an optional [message]. */
+fun assertFalse(actual: Boolean, message: String? = null) {
+    assertTrue(!actual, message ?: "Expected value to be false.")
+}
