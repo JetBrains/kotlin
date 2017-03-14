10eef395f4d78cedffdb44c9d757aae56f887a8c
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-03-14 12:08:08 +0300

    stdlib: Add empty kotlin.experimental package
    This patch adds empty kotlin.experimental package to avoid unresolved
    reference errors in bitwiseOp tests:
    
    external_codegen_box_binaryOp/bitwiseOp.kt
    external_codegen_box_binaryOp/bitwiseOpAny.kt
    external_codegen_box_binaryOp/bitwiseOpNullable.kt
    
    Byte/Short bitwise operations are supported in Kotlin Native by default.

---
+++ runtime/src/main/kotlin/kotlin/Experimental.kt
@@ -0,0 +1,7 @@
+/**
+ * Empty package to avoid unresolved reference errors in bitwiseOp tests
+ * (e.g. external_codegen_box_binaryOp/bitwiseOp.kt).
+ * Byte/Short bitwise operations are supported in Kotlin Native by default.
+ */
+
+package kotlin.experimental
