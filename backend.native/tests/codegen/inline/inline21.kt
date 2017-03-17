449b4cd4b1daaca06efd328a20d8a78d2fba4349
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2017-03-17 15:58:14 +0700

    Minor test improvement

--- backend.native/tests/codegen/inline/inline21.kt
+++ backend.native/tests/codegen/inline/inline21.kt
@@ -1,13 +1,18 @@
-inline fun foo(block: () -> Unit) {
-    block()
+inline fun foo2(block2: () -> Int) : Int {
+    println("foo2")
+    return block2()
 }
 
-fun bar(block: () -> Unit) {
-    foo(block)
+inline fun foo1(block1: () -> Int) : Int {
+    println("foo1")
+    return foo2(block1)
+}
+
+fun bar(block: () -> Int) : Int {
+    println("bar")
+    return foo1(block)
 }
 
 fun main(args: Array<String>) {
-    bar {
-        println("OK")
-    }
+    println(bar { 33 })
 }
