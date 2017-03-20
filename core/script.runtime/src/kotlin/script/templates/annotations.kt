0f21db1ecbc00a63c8933ad667c9a301856b7007
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-03-20 14:01:16 +0100

    Copy base script classes to script-runtime before building bootstrap

---
+++ core/script.runtime/src/kotlin/script/templates/annotations.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("unused")
+
+package kotlin.script.templates
+
+import kotlin.reflect.KClass
+import kotlin.script.dependencies.BasicScriptDependenciesResolver
+import kotlin.script.dependencies.ScriptDependenciesResolver
+
+const val DEFAULT_SCRIPT_FILE_PATTERN = ".*\\.kts"
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ScriptTemplateDefinition(val resolver: KClass<out ScriptDependenciesResolver> = BasicScriptDependenciesResolver::class,
+                                          val scriptFilePattern: String = DEFAULT_SCRIPT_FILE_PATTERN)
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AcceptedAnnotations(vararg val supportedAnnotationClasses: KClass<out Annotation>)
