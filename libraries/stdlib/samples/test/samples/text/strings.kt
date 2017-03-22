3088586ac332f45c878f392665bb405b71e876cd
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-03-22 03:22:20 +0300

    Add runnable samples for trimIndent and trimMargin. #KT-9786 Fixed

--- libraries/stdlib/samples/test/samples/text/strings.kt
+++ libraries/stdlib/samples/test/samples/text/strings.kt
@@ -24,4 +24,31 @@ class Strings {
         assertPrints("Word".repeat(0), "")
     }
 
+    @Sample
+    fun trimIndent() {
+        val withoutIndent =
+                """
+                    ABC
+                    123
+                    456
+                """.trimIndent()
+        assertPrints(withoutIndent, "ABC\n123\n456")
+    }
+
+    @Sample
+    fun trimMargin() {
+        val withoutMargin1 = """ABC
+                        |123
+                        |456""".trimMargin()
+        assertPrints(withoutMargin1, "ABC\n123\n456")
+
+        val withoutMargin2 = """
+            #XYZ
+            #foo
+            #bar
+        """.trimMargin("#")
+        assertPrints(withoutMargin2, "XYZ\nfoo\nbar")
+    }
+
+
 }
