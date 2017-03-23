35904ee575d927ac6668d107a04fd423e28b5efb
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-23 14:07:52 +0300

    Revert "Fix testdata according to contract inference changes in 2017.1"
    This reverts commit a539939388f53d739f1330ab308a60f684237976.

--- idea/testData/editor/quickDoc/JavaMethodUsedInKotlin.kt
+++ idea/testData/editor/quickDoc/JavaMethodUsedInKotlin.kt
@@ -2,7 +2,6 @@ fun ktTest() {
     Test.<caret>foo("SomeTest")
 }
 
-//INFO: <html><head>    <style type="text/css">        #error {            background-color: #eeeeee;            margin-bottom: 10px;        }        p {            margin: 5px 0;        }    </style></head><body><small><b><a href="psi_element://Test"><code>Test</code></a></b></small><PRE><i>@Contract(pure = true)</i>&nbsp;
-//INFO: <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>org.jetbrains.annotations.NotNull</code></a></i>&nbsp;
+//INFO: <html><head>    <style type="text/css">        #error {            background-color: #eeeeee;            margin-bottom: 10px;        }        p {            margin: 5px 0;        }    </style></head><body><small><b><a href="psi_element://Test"><code>Test</code></a></b></small><PRE><i>@Contract(value = &quot;_ -&gt; !null&quot;, pure = true)</i>&nbsp;
 //INFO: public static&nbsp;<a href="psi_element://java.lang.Object"><code>Object</code></a>[]&nbsp;<b>foo</b>(<a href="psi_element://java.lang.String"><code>String</code></a>&nbsp;param)</PRE>
 //INFO:        Java Method</body></html>
