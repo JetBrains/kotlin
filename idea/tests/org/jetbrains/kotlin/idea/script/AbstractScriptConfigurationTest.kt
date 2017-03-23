1413800a97fd9506a8e8f9f02743744731c389ab
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-23 14:08:22 +0300

    Revert "Fix test code after UsefulTestCase api changes"
    This reverts commit 4be5fcc14ab0400cd5e4d5af609086387757bb9c.

--- idea/tests/org/jetbrains/kotlin/idea/script/AbstractScriptConfigurationTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/script/AbstractScriptConfigurationTest.kt
@@ -128,7 +128,7 @@ abstract class AbstractScriptConfigurationTest : AbstractPsiCheckerTest() {
                 Extensions.getArea(project),
                 ScriptTemplatesProvider.EP_NAME,
                 provider,
-                testRootDisposable
+                myTestRootDisposable
         )
         KotlinScriptConfigurationManager.reloadScriptDefinitions(project)
     }
