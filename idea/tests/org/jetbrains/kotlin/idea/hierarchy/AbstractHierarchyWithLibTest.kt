2c297995fdc4b9040596b281e3ddf884c4b00761
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-23 14:08:18 +0300

    Revert "Workaround for hanging test"
    This reverts commit 715410a9bc8abcfd2a598381e9652f9ca5ebbb55.

--- idea/tests/org/jetbrains/kotlin/idea/hierarchy/AbstractHierarchyWithLibTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/hierarchy/AbstractHierarchyWithLibTest.kt
@@ -50,7 +50,7 @@ abstract class AbstractHierarchyWithLibTest : AbstractHierarchyTest() {
                     TypeHierarchyTreeStructure(
                             project,
                             targetClass,
-                            if (folderName.contains("annotation")) HierarchyBrowserBaseEx.SCOPE_PROJECT else HierarchyBrowserBaseEx.SCOPE_ALL)
+                            HierarchyBrowserBaseEx.SCOPE_ALL)
                 }, *filesToConfigure)
     }
 
