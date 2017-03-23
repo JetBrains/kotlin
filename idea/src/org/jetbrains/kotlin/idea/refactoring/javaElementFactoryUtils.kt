c8e2b196efa8973728f065e3d407f414982a3b85
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-03-23 14:07:59 +0300

    Revert "Update to 171.SNAPSHOT to pick up fix for IDEA-169570; fix compilation"
    This reverts commit 12fc89f35d80628a6bba5c8f3767d3b9074080c0.

--- idea/src/org/jetbrains/kotlin/idea/refactoring/javaElementFactoryUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/refactoring/javaElementFactoryUtils.kt
@@ -32,6 +32,5 @@ fun PsiElementFactory.createReferenceListWithRole(
         Role.EXTENDS_LIST -> createClassFromText("class Foo extends $refListText {}", null).innerClasses[0].extendsList
         Role.IMPLEMENTS_LIST -> createClassFromText("class Foo implements $refListText {}", null).innerClasses[0].implementsList
         Role.EXTENDS_BOUNDS_LIST -> createTypeParameterFromText("T extends $refListText", null).extendsList
-        else -> throw UnsupportedOperationException("Unsupported role $role")
     }
 }
