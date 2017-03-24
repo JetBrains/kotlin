5028ad3840588826522dbea05d5924020ac1c5b6
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-03-24 16:08:15 +0300

    Revert "Android Extensions: Allow to disable IDE support (KT-12741)"
    This reverts commit 8ac14ab9305e6bd1efeb9fe7551fa73fbb62527a.

--- plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/synthetic/idea/res/IDEAndroidPackageFragmentProviderExtension.kt
+++ plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/synthetic/idea/res/IDEAndroidPackageFragmentProviderExtension.kt
@@ -16,11 +16,8 @@
 
 package org.jetbrains.kotlin.android.synthetic.idea.res
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleServiceManager
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiManager
 import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.android.synthetic.idea.androidExtensionsIsEnabled
@@ -29,15 +26,12 @@ import org.jetbrains.kotlin.android.synthetic.res.AndroidLayoutXmlFileManager
 import org.jetbrains.kotlin.android.synthetic.res.AndroidPackageFragmentProviderExtension
 import org.jetbrains.kotlin.idea.caches.resolve.ModuleSourceInfo
 
-class IDEAndroidPackageFragmentProviderExtension(val project: Project) : AndroidPackageFragmentProviderExtension() {
-    override fun isExperimental(moduleInfo: ModuleInfo?): Boolean {
-        return moduleInfo?.androidExtensionsIsExperimental ?: false
-    }
+class IDEAndroidPackageFragmentProviderExtension : AndroidPackageFragmentProviderExtension() {
 
     override fun getLayoutXmlFileManager(project: Project, moduleInfo: ModuleInfo?): AndroidLayoutXmlFileManager? {
         val moduleSourceInfo = moduleInfo as? ModuleSourceInfo ?: return null
         val module = moduleSourceInfo.module
-        if (!isAndroidExtensionsEnabled(module) && !isTestMode(module)) return null
+        if (AndroidFacet.getInstance(module) == null) return null
         return ModuleServiceManager.getService(module, AndroidLayoutXmlFileManager::class.java)
     }
 
