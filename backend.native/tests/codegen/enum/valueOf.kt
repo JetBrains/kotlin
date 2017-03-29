8e38e28f89be55e619b8657a64c885131657a25b
Igor Chevdar <igor.chevdar@jetbrains.com>
2017-03-29 12:37:13 +0300

    Added tests

--- backend.native/tests/codegen/enum/valueOf.kt
+++ backend.native/tests/codegen/enum/valueOf.kt
@@ -8,4 +8,7 @@ fun main(args: Array<String>) {
     println(E.valueOf("E1").toString())
     println(E.valueOf("E2").toString())
     println(E.valueOf("E3").toString())
+    println(enumValueOf<E>("E1").toString())
+    println(enumValueOf<E>("E2").toString())
+    println(enumValueOf<E>("E3").toString())
 }
--- backend.native/tests/codegen/enum/values.kt
+++ backend.native/tests/codegen/enum/values.kt
@@ -8,4 +8,7 @@ fun main(args: Array<String>) {
     println(E.values()[0].toString())
     println(E.values()[1].toString())
     println(E.values()[2].toString())
+    println(enumValues<E>()[0].toString())
+    println(enumValues<E>()[1].toString())
+    println(enumValues<E>()[2].toString())
 }
