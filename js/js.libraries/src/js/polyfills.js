8fa55ccbdea2d1b2a5e1bf0996783afc05444bfa
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2017-03-29 19:55:24 +0300

    JS: polyfill ArrayBuffer.isView in order to support PhantomJS
    (cherry picked from commit 3eb5ce7d9a4d118a262e05cf2eb096feca4fe7b2)

--- js/js.libraries/src/js/polyfills.js
+++ js/js.libraries/src/js/polyfills.js
@@ -31,3 +31,9 @@ if (typeof String.prototype.endsWith === "undefined") {
         return lastIndex !== -1 && lastIndex === position;
     };
 }
+// For HtmlUnit and PhantomJs
+if (typeof ArrayBuffer.isView === "undefined") {
+    ArrayBuffer.isView = function(a) {
+        return a != null && a.__proto__ != null && a.__proto__.__proto__ === Int8Array.prototype.__proto__;
+    };
+}
