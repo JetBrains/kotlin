0cbd34086e81cf877bcc7f12c6a7cb6c565332fb
Nikolay Igotti <olonho@users.noreply.github.com>
2017-03-30 09:51:58 +0300

    Added JB copyright (#417)

--- experiments/MemLayout/native-src/build-bridge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-clang -shared -o../out/production/MemLayout/libbridge.dylib bridge.c -I /System/Library/Frameworks/JavaVM.framework/Headers
-
--- experiments/dwarf-2/2-functions.c
+++
@@ -1,7 +0,0 @@
-int foo() {
-  return 0;
-}
-
-int bar() {
-  return 0;
-}
