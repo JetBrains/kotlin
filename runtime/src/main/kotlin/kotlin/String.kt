05b6d8ab811846cba83ff798a705ae7872564b80
Igor Chevdar <igor.chevdar@jetbrains.com>
2017-04-03 14:33:05 +0300

    Synchronized Any?.plus with JVM Kotlin

--- runtime/src/main/kotlin/kotlin/String.kt
+++ runtime/src/main/kotlin/kotlin/String.kt
@@ -59,7 +59,7 @@ public final class String : Comparable<String>, CharSequence {
 private val kNullString = "null"
 
 public operator fun kotlin.String?.plus(other: kotlin.Any?): kotlin.String =
-    this?.plus(other?.toString() ?: kNullString) ?: other?.toString() ?: kNullString
+    (this?.toString() ?: kNullString).plus(other?.toString() ?: kNullString)
 
 
 public fun Any?.toString() = this?.toString() ?: kNullString
