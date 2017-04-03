d07295d73267c51bf3d77b97831640db832bacb5
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2017-04-03 11:39:53 +0300

    Remove some hacks from libcurl sample

--- samples/libcurl/callbacks.h
+++ samples/libcurl/callbacks.h
@@ -1,8 +1,5 @@
-#include <stdio.h>
+#include <stddef.h>
 
 // Those types are needed but not defined or used in libcurl headers.
 typedef size_t header_callback(char *buffer,   size_t size,   size_t nitems, void *userdata);
 typedef size_t write_data_callback(void *buffer, size_t size, size_t nmemb, void *userp);
-
-extern void something_using_callback1(header_callback*);
-extern void something_using_callback2(write_data_callback*);
