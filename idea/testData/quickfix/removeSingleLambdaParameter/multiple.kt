9dedc7a15ae49022812dadbf161649ac7b32931e
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-04-05 16:19:28 +0300

    Revert "Rewrite AbstractQuickFixTest based on light fixture test case"
    This reverts commit e2f75463d71439f28c06b1b4ea576d91db9cc397.
    
    # Conflicts:
    #	idea/idea-test-framework/src/org/jetbrains/kotlin/idea/test/KotlinLightCodeInsightFixtureTestCase.kt

--- idea/testData/quickfix/removeSingleLambdaParameter/multiple.kt
+++ idea/testData/quickfix/removeSingleLambdaParameter/multiple.kt
@@ -3,7 +3,7 @@
 // ACTION: Rename to _
 // ACTION: Specify explicit lambda signature
 // ACTION: Specify type explicitly
-// RUNTIME_WITH_FULL_JDK
+// WITH_RUNTIME
 
 fun main() {
     mapOf(1 to 2).forEach { t, <caret>u -> println(t) }
