JetBrains/kotlin

commit 9819de1abd4539edb555b0c0e24f1a4090ef473f

Improve daemon client debug reports
Before this change a daemon client debug messages were printed
only when the client could not connect and the 'kotlin.daemon.verbose'
system property was set up.

Now messages are printed if the debug logging is enabled and
the 'kotlin.daemon.verbose' is set up.

compiler/compiler-runner/src/org/jetbrains/kotlin/compilerRunner/KotlinCompilerRunner.kt
@@ -42,6 +42,8 @@ interface KotlinLogger {
     fun warn(msg: String)
     fun info(msg: String)
     fun debug(msg: String)
+
+    val isDebugEnabled: Boolean
 }
 
 abstract class KotlinCompilerRunner<in Env : CompilerEnvironment> {
@@ -61,11 +63,15 @@ abstract class KotlinCompilerRunner<in Env : CompilerEnvironment> {
         val daemonJVMOptions = configureDaemonJVMOptions(inheritMemoryLimits = true, inheritAdditionalProperties = true)
 
         val daemonReportMessages = ArrayList<DaemonReportMessage>()
+        val daemonReportingTargets = when {
+            log.isDebugEnabled ->  DaemonReportingTargets(messages = daemonReportMessages)
+            else ->  DaemonReportingTargets(messageCollector = environment.messageCollector)
+        }
 
         val profiler = if (daemonOptions.reportPerf) WallAndThreadAndMemoryTotalProfiler(withGC = false) else DummyProfiler()
 
         val connection = profiler.withMeasure(null) {
-            val daemon = KotlinCompilerClient.connectToCompileService(compilerId, daemonJVMOptions, daemonOptions, DaemonReportingTargets(null, daemonReportMessages), true, true)
+            val daemon = KotlinCompilerClient.connectToCompileService(compilerId, daemonJVMOptions, daemonOptions, daemonReportingTargets, true, true)
             DaemonConnection(daemon, daemon?.leaseCompileSession(flagFile.absolutePath)?.get() ?: CompileService.NO_SESSION)
         }
 

compiler/daemon/daemon-client/src/org/jetbrains/kotlin/daemon/client/KotlinCompilerClient.kt
@@ -395,11 +395,13 @@ object KotlinCompilerClient {
                         daemon.inputStream
                                 .reader()
                                 .forEachLine {
+                                    reportingTargets.report(DaemonReportCategory.DEBUG, it, "daemon")
+
                                     if (it == COMPILE_DAEMON_IS_READY_MESSAGE) {
+                                        reportingTargets.report(DaemonReportCategory.DEBUG, "Received the message signalling that the daemon is ready")
                                         isEchoRead.release()
                                         return@forEachLine
                                     }
-                                    reportingTargets.report(DaemonReportCategory.DEBUG, it, "daemon")
                                 }
                     }
                     finally {

jps-plugin/src/org/jetbrains/kotlin/compilerRunner/JpsKotlinLogger.kt
@@ -34,4 +34,7 @@ internal class JpsKotlinLogger(private val log: Logger) : KotlinLogger {
     override fun debug(msg: String) {
         log.debug(msg)
     }
+
+    override val isDebugEnabled: Boolean
+        get() = log.isDebugEnabled
 }

libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/compilerRunner/GradleKotlinLogger.kt
@@ -34,4 +34,7 @@ internal class GradleKotlinLogger(private val log: Logger) : KotlinLogger {
     override fun warn(msg: String) {
         log.warn(msg)
     }
+
+    override val isDebugEnabled: Boolean
+        get() = log.isDebugEnabled
 }

commit 56182746b5739fc9c952b1edebb80f0daedc1851

Fix printing daemon client messages
Fixing the problem introduced in the commit 9819de1abd4539edb555b0c0e24f1a4090ef473f:
the daemon client messages should be reported
when the debug log level is enabled
or when the connection not successful

    #KT-17199 fixed

compiler/compiler-runner/src/org/jetbrains/kotlin/compilerRunner/KotlinCompilerRunner.kt
@@ -68,10 +68,7 @@ abstract class KotlinCompilerRunner<in Env : CompilerEnvironment> {
         val daemonJVMOptions = configureDaemonJVMOptions(inheritMemoryLimits = true, inheritAdditionalProperties = true)
 
         val daemonReportMessages = ArrayList<DaemonReportMessage>()
-        val daemonReportingTargets = when {
-            log.isDebugEnabled ->  DaemonReportingTargets(messages = daemonReportMessages)
-            else ->  DaemonReportingTargets(messageCollector = environment.messageCollector)
-        }
+        val daemonReportingTargets = DaemonReportingTargets(messages = daemonReportMessages)
 
         val profiler = if (daemonOptions.reportPerf) WallAndThreadAndMemoryTotalProfiler(withGC = false) else DummyProfiler()
 
@@ -86,10 +83,15 @@ abstract class KotlinCompilerRunner<in Env : CompilerEnvironment> {
                                                  sessionAliveFlagFile = sessionAliveFlagFile)
         }
 
-        for (msg in daemonReportMessages) {
-            environment.messageCollector.report(CompilerMessageSeverity.INFO,
-                                    (if (msg.category == DaemonReportCategory.EXCEPTION && connection == null) "Falling  back to compilation without daemon due to error: " else "") + msg.message,
-                                                CompilerMessageLocation.NO_LOCATION)
+        if (connection == null || log.isDebugEnabled) {
+            for (message in daemonReportMessages) {
+                val severity = when(message.category) {
+                    DaemonReportCategory.DEBUG -> CompilerMessageSeverity.INFO
+                    DaemonReportCategory.INFO -> CompilerMessageSeverity.INFO
+                    DaemonReportCategory.EXCEPTION -> CompilerMessageSeverity.EXCEPTION
+                }
+                environment.messageCollector.report(severity, message.message, CompilerMessageLocation.NO_LOCATION)
+            }
         }
 
         fun reportTotalAndThreadPerf(message: String, daemonOptions: DaemonOptions, messageCollector: MessageCollector, profiler: Profiler) {