d44bf27ec3ab0c83bc7a9a67c7eb3635fffde8f4
Kirill Rakhman <kirill.rakhman@gmail.com>
2017-04-10 23:08:42 +0200

    Completion of keywords (like `constructor`) doesn't insert parentheses/braces if they're already there
     #KT-15603 Fixed
     #KT-13524 Fixed

--- compiler/frontend/src/org/jetbrains/kotlin/psi/psiUtil/psiUtils.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/psi/psiUtil/psiUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -143,6 +143,10 @@ inline fun <reified T : PsiElement> PsiElement.getChildrenOfType(): Array<T> {
 
 fun PsiElement.getNextSiblingIgnoringWhitespaceAndComments(withItself: Boolean = false): PsiElement? {
     return siblings(withItself = withItself).filter { it !is PsiWhiteSpace && it !is PsiComment }.firstOrNull()
+}
+
+fun PsiElement.getNextSiblingIgnoringWhitespace(withItself: Boolean = false): PsiElement? {
+    return siblings(withItself = withItself).filter { it !is PsiWhiteSpace }.firstOrNull()
 }
 
 fun PsiElement.getPrevSiblingIgnoringWhitespaceAndComments(withItself: Boolean = false): PsiElement? {
