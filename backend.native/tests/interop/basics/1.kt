2a3450047cd6dc9cc47d3424a2340742272ac4cb
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2017-04-10 11:16:35 +0300

    Add interop test for returning struct by value

--- backend.native/tests/interop/basics/1.kt
+++ backend.native/tests/interop/basics/1.kt
@@ -1,5 +1,12 @@
 import cstdlib.*
+import kotlinx.cinterop.*
 
 fun main(args: Array<String>) {
     println(atoi("257"))
+
+    val divResult = div(-5, 3)
+    val (quot, rem) = divResult.useContents { Pair(quot, rem) }
+    println(quot)
+    println(rem)
+
 }
