438f318d3e325a0d287efcca3cb1ce579ca8d1f7
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-04-10 16:34:01 +0300

    Minor, update test data

--- idea/testData/checker/TypeArgumentsNotAllowed.kt
+++ idea/testData/checker/TypeArgumentsNotAllowed.kt
@@ -2,6 +2,6 @@ package foo
 
 open class A<T>
 
-fun <T> f(<warning>t</warning>: T<error descr="[TYPE_ARGUMENTS_NOT_ALLOWED] Type arguments are not allowed for type parameters"><T></error>) {}
+fun <T> f(<warning descr="[UNUSED_PARAMETER] Parameter 't' is never used">t</warning>: T<error descr="[TYPE_ARGUMENTS_NOT_ALLOWED] Type arguments are not allowed for type parameters"><<error descr="[DEBUG] Reference is not resolved to anything, but is not marked unresolved">T</error>></error>) {}
 
-fun <T> use(<warning>b</warning>: foo<error descr="[TYPE_ARGUMENTS_NOT_ALLOWED] Type arguments are not allowed here"><T></error>.A<T>) {}
+fun <T> use(<warning descr="[UNUSED_PARAMETER] Parameter 'b' is never used">b</warning>: foo<error descr="[TYPE_ARGUMENTS_NOT_ALLOWED] Type arguments are not allowed here"><T></error>.A<T>) {}
