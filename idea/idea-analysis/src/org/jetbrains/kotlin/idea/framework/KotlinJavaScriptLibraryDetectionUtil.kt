eb0c0a88695217351887deeb558fb2c8bc678ff5
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-04-12 17:22:28 +0300

    Fix incorrect logic in KotlinJavaScriptLibraryDetectionUtil
    Previously the lambda almost always returned false, because the library
    root is never a JS file with metadata, which led to
    processFilesRecursively always returning false, which led to every
    library being detected as a Kotlin/JS library here

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/framework/KotlinJavaScriptLibraryDetectionUtil.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/framework/KotlinJavaScriptLibraryDetectionUtil.kt
@@ -43,7 +43,7 @@ object KotlinJavaScriptLibraryDetectionUtil {
                 return hasMetadata
             }
 
-            if (!VfsUtilCore.processFilesRecursively(root, { isJsFileWithMetadata(root) })) {
+            if (!VfsUtilCore.processFilesRecursively(root) { !isJsFileWithMetadata(it) }) {
                 return true
             }
         }
