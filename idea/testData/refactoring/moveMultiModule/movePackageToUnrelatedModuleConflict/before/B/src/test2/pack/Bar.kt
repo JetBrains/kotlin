a876f5529888d7eb2d06e077045c864d68777bff
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-04-12 15:18:50 +0300

    Revert "Move: Add conflict checking for "Move packages" case"
    This reverts commit cd6177534aacbf761d4d03d50dcd0340b25a5a7c.

--- idea/testData/refactoring/moveMultiModule/movePackageToUnrelatedModuleConflict/before/B/src/test2/pack/Bar.kt
+++
@@ -1,8 +0,0 @@
-package test2.pack
-
-import test2.J
-
-class Bar {
-    internal val foo = Foo()
-    val j = J()
-}
--- idea/testData/refactoring/moveMultiModule/movePackageToUnrelatedModuleConflict/before/B/src/test2/pack/Foo.kt
+++
@@ -1,7 +0,0 @@
-package test2.pack
-
-import test2.J
-
-internal class Foo {
-    val j = J()
-}
