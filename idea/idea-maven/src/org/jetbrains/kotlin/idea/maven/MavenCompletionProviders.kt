711398f14ff5e9f5f2299954c658475d6ba428fb
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-04-13 17:56:35 +0300

    Maven: eliminate constants in maven.xml

---
+++ idea/idea-maven/src/org/jetbrains/kotlin/idea/maven/MavenCompletionProviders.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.idea.maven
+
+import com.intellij.openapi.util.text.StringUtil
+import org.jetbrains.idea.maven.plugins.api.MavenFixedValueReferenceProvider
+import org.jetbrains.kotlin.cli.common.arguments.DefaultValues
+import org.jetbrains.kotlin.config.JvmTarget
+import org.jetbrains.kotlin.config.LanguageVersion
+
+class LanguageVersionsProvider : MavenFixedValueReferenceProvider(
+        LanguageVersion.values().map { it.versionString }.toTypedArray()
+)
+
+class ApiVersionsProvider : MavenFixedValueReferenceProvider(
+        LanguageVersion.values().map { it.versionString }.toTypedArray()
+)
+
+class JvmTargetsProvider : MavenFixedValueReferenceProvider(
+        JvmTarget.values().map { it.description }.toTypedArray()
+)
+
+class JsModuleKindsProvider : MavenFixedValueReferenceProvider(
+        DefaultValues.JsModuleKinds.possibleValues?.map { StringUtil.unquoteString(it) }?.toTypedArray() ?: emptyArray()
+)
+
+class JsMainCallProvider : MavenFixedValueReferenceProvider(
+        DefaultValues.JsMain.possibleValues?.map { StringUtil.unquoteString(it) }?.toTypedArray() ?: emptyArray()
+)
