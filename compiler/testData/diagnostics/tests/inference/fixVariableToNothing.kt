f37221373414b53a5151556e7de0153815e9ad54
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2017-04-13 13:47:06 +0300

    [NI] Resolve type variable to Nothing for TO_SUBTYPE direction.

---
+++ compiler/testData/diagnostics/tests/inference/fixVariableToNothing.kt
@@ -0,0 +1,15 @@
+
+// completion order here: X, Y, WHEN_VARIABLE
+fun <T> List<T>.optimizeReadOnlyList() = when (size) {
+    0 -> emptyList() // here type variable Y will be fixed to Nothing
+    1 -> listOf(this[0])
+    else -> this
+}
+fun <X> listOf(<!UNUSED_PARAMETER!>element<!>: X): List<X> = TODO()
+fun <Y> emptyList(): List<Y> = TODO()
+
+
+fun test(l: List<String>): List<String> {
+    val foo = l.optimizeReadOnlyList()
+    return foo
+}
