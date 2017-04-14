a35ad64454c50b1845604423fb680451a162f999
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-04-14 10:32:52 +0200

    Disable reflection tests on android

--- compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/SpecialFiles.java
+++ compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/SpecialFiles.java
@@ -47,6 +47,8 @@ public class SpecialFiles {
         excludedFiles.add("arrayOfKClasses.kt");
         excludedFiles.add("enumKClassAnnotation.kt");
         excludedFiles.add("primitivesAndArrays.kt");
+        excludedFiles.add("kt6485.kt");
+        excludedFiles.add("getDelegateWithoutReflection.kt");
 
         // Reflection is used to check full class name
         excludedFiles.add("native");
