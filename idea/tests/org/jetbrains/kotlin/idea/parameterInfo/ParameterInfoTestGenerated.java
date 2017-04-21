bc071bf543da60c674e1805d4c09985113e716c1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-04-21 16:20:51 +0300

    Add parameter info test for #KT-14484 Obsolete

--- idea/tests/org/jetbrains/kotlin/idea/parameterInfo/ParameterInfoTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/parameterInfo/ParameterInfoTestGenerated.java
@@ -323,6 +323,12 @@ public class ParameterInfoTestGenerated extends AbstractParameterInfoTest {
             doTest(fileName);
         }
 
+        @TestMetadata("TwoSmartCasts.kt")
+        public void testTwoSmartCasts() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("idea/testData/parameterInfo/functionCall/TwoSmartCasts.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("TypeAliasConstructor.kt")
         public void testTypeAliasConstructor() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("idea/testData/parameterInfo/functionCall/TypeAliasConstructor.kt");
