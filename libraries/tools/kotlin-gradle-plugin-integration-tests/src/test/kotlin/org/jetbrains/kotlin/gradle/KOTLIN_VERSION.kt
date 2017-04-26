f079358a76f215d8d2047c2105e9fff9499b79b5
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-04-26 21:44:33 +0300

    Set 1.2-SNAPSHOT in maven build

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KOTLIN_VERSION.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KOTLIN_VERSION.kt
@@ -2,4 +2,4 @@ package org.jetbrains.kotlin.gradle
 
 // constant is held in separate file intentionally for better discoverability
 // and to prevent vcs conflicts (its value is 1.1-* in master branch, 0.1-* in 1.0.x branches)
-const val KOTLIN_VERSION = "1.1-SNAPSHOT"
+const val KOTLIN_VERSION = "1.2-SNAPSHOT"
