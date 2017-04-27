5a3ef0bfc198196013cb8159cd9ffc63d86a4c01
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-04-27 12:50:01 +0300

    Remove old IC: update on-off tests
    Original commit: 8143ca158b88f2f87cc7de8f02901447d873fe84

--- jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/experimentalIncrementalCompilationTests.kt
+++ jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/experimentalIncrementalCompilationTests.kt
@@ -22,8 +22,6 @@ import org.jetbrains.kotlin.jps.incremental.CacheVersionProvider
 
 abstract class AbstractExperimentalIncrementalJpsTest : AbstractIncrementalJpsTest()
 
-abstract class AbstractExperimentalChangeIncrementalOptionTest : AbstractIncrementalLazyCachesTest()
-
 abstract class AbstractExperimentalIncrementalCacheVersionChangedTest : AbstractIncrementalCacheVersionChangedTest() {
     override fun getVersions(cacheVersionProvider: CacheVersionProvider, targets: Iterable<ModuleBuildTarget>) =
             targets.map { cacheVersionProvider.experimentalVersion(it) }
