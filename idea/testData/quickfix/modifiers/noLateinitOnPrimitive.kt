9e9f16d16ec39b64e8e78231117c420693ee1d46
Kirill Rakhman <kirill.rakhman@gmail.com>
2017-05-01 16:09:06 +0300

    Add quickfix for adding 'lateinit'
    Fixes #KT-17650
    
    (cherry picked from commit cd07aea)

---
+++ idea/testData/quickfix/modifiers/noLateinitOnPrimitive.kt
@@ -0,0 +1,10 @@
+// "Add 'lateinit' modifier" "false"
+// ACTION: Add initializer
+// ACTION: Make 'a' abstract
+// ACTION: Move to constructor parameters
+// ACTION: Move to constructor
+// ERROR: Property must be initialized or be abstract
+
+class A {
+    private var a: Int<caret>
+}
