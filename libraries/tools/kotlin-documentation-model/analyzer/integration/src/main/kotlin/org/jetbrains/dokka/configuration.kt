b73c84ab810bf3ae295235d3168bc9e2fc3d4b52
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-05-02 15:08:52 +0300

    [KDM] Refactoring internal communication for gradle
    Original commit: https://github.com/Kotlin/dokka/commit/54c3c87acfb31afc22afc5f20229384f755b677f

---
+++ libraries/tools/kotlin-documentation-model/analyzer/integration/src/main/kotlin/org/jetbrains/dokka/configuration.kt
@@ -0,0 +1,47 @@
+package org.jetbrains.dokka
+
+
+interface DokkaConfiguration {
+    val moduleName: String
+    val classpath: List<String>
+    val sourceRoots: List<SourceRoot>
+    val samples: List<String>
+    val includes: List<String>
+    val outputDir: String
+    val format: String
+    val includeNonPublic: Boolean
+    val includeRootPackage: Boolean
+    val reportUndocumented: Boolean
+    val skipEmptyPackages: Boolean
+    val skipDeprecated: Boolean
+    val jdkVersion: Int
+    val generateIndexPages: Boolean
+    val sourceLinks: List<SourceLinkDefinition>
+
+    interface SourceRoot {
+        val path: String
+        val defaultPlatforms: List<String>
+    }
+
+    interface SourceLinkDefinition {
+        val path: String
+        val url: String
+        val lineSuffix: String?
+    }
+}
+
+data class SerializeOnlyDokkaConfiguration(override val moduleName: String,
+                                           override val classpath: List<String>,
+                                           override val sourceRoots: List<DokkaConfiguration.SourceRoot>,
+                                           override val samples: List<String>,
+                                           override val includes: List<String>,
+                                           override val outputDir: String,
+                                           override val format: String,
+                                           override val includeNonPublic: Boolean,
+                                           override val includeRootPackage: Boolean,
+                                           override val reportUndocumented: Boolean,
+                                           override val skipEmptyPackages: Boolean,
+                                           override val skipDeprecated: Boolean,
+                                           override val jdkVersion: Int,
+                                           override val generateIndexPages: Boolean,
+                                           override val sourceLinks: List<DokkaConfiguration.SourceLinkDefinition>) : DokkaConfiguration
