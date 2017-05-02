d54d9b806672dd607f5e889946b1fe614239baad
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-05-02 15:08:52 +0300

    [KDM] Refactoring internal communication for gradle
    Original commit: https://github.com/Kotlin/dokka/commit/54c3c87acfb31afc22afc5f20229384f755b677f

--- libraries/tools/kotlin-documentation-model/analyzer/integration/src/main/kotlin/org/jetbrains/dokka/DokkaBootstrap.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/integration/src/main/kotlin/org/jetbrains/dokka/DokkaBootstrap.kt
@@ -4,22 +4,7 @@ import java.util.function.BiConsumer
 
 interface DokkaBootstrap {
 
-    fun configure(logger: BiConsumer<String, String>,
-                  moduleName: String,
-                  classpath: List<String>,
-                  sources: List<String>,
-                  samples: List<String>,
-                  includes: List<String>,
-                  outputDir: String,
-                  format: String,
-                  includeNonPublic: Boolean,
-                  includeRootPackage: Boolean,
-                  reportUndocumented: Boolean,
-                  skipEmptyPackages: Boolean,
-                  skipDeprecated: Boolean,
-                  jdkVersion: Int,
-                  generateIndexPages: Boolean,
-                  sourceLinks: List<String>)
+    fun configure(logger: BiConsumer<String, String>, serializedConfigurationJSON: String)
 
     fun generate()
 }
