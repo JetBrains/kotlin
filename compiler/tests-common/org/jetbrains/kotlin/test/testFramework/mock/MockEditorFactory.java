4fc3aa5cf87498da7f436fc71df0b97cd32cd6d3
Dmitry Jemerov <yole@jetbrains.com>
2017-05-03 09:52:32 +0200

    Compilation fixes for 172 branch

--- compiler/tests-common/org/jetbrains/kotlin/test/testFramework/mock/MockEditorFactory.java
+++ compiler/tests-common/org/jetbrains/kotlin/test/testFramework/mock/MockEditorFactory.java
@@ -19,6 +19,7 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.EditorKind;
 import com.intellij.openapi.editor.event.EditorEventMulticaster;
 import com.intellij.openapi.editor.event.EditorFactoryListener;
 import com.intellij.openapi.editor.impl.DocumentImpl;
@@ -27,6 +28,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.text.CharArrayCharSequence;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class MockEditorFactory extends EditorFactory {
   @Override
@@ -56,6 +58,27 @@ public class MockEditorFactory extends EditorFactory {
 
   @Override
   public Editor createViewer(@NotNull Document document, Project project) {
+    return null;
+  }
+
+  @Override
+  public Editor createEditor(
+          @NotNull Document document, @Nullable Project project, @NotNull EditorKind kind
+  ) {
+    return null;
+  }
+
+  @Override
+  public Editor createEditor(
+          @NotNull Document document, Project project, @NotNull VirtualFile file, boolean isViewer, @NotNull EditorKind kind
+  ) {
+    return null;
+  }
+
+  @Override
+  public Editor createViewer(
+          @NotNull Document document, @Nullable Project project, @NotNull EditorKind kind
+  ) {
     return null;
   }
 
