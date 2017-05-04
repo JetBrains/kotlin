1a995d4d0c3f722a220aa764d9c32124d856c9fb
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-05-04 15:54:08 +0300

    Drop custom Kotlin Android lint, register quickfixes with extension point

--- idea/testData/android/lintQuickfix/suppressLint/methodParameter.kt
+++ idea/testData/android/lintQuickfix/suppressLint/methodParameter.kt
@@ -1,4 +1,4 @@
 // INTENTION_TEXT: Suppress: Add @SuppressLint("SdCardPath") annotation
-// INSPECTION_CLASS: org.jetbrains.android.inspections.klint.AndroidLintInspectionToolProvider$AndroidKLintSdCardPathInspection
+// INSPECTION_CLASS: org.jetbrains.android.inspections.lint.AndroidLintInspectionToolProvider$AndroidLintSdCardPathInspection
 
 fun foo(path: String = "<caret>/sdcard") = path
--- idea/testData/android/lintQuickfix/targetVersionCheck/when.kt
+++ idea/testData/android/lintQuickfix/targetVersionCheck/when.kt
@@ -1,5 +1,5 @@
 // INTENTION_TEXT: Surround with if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) { ... }
-// INSPECTION_CLASS: org.jetbrains.android.inspections.klint.AndroidLintInspectionToolProvider$AndroidKLintNewApiInspection
+// INSPECTION_CLASS: org.jetbrains.android.inspections.lint.AndroidLintInspectionToolProvider$AndroidLintNewApiInspection
 
 import android.graphics.drawable.VectorDrawable
 
