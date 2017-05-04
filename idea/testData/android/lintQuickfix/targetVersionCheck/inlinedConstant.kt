1e1199a51e0b4b22795d8df41cb9a753b908fbcf
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-05-04 15:54:08 +0300

    Drop custom Kotlin Android lint, register quickfixes with extension point

--- idea/testData/android/lintQuickfix/targetVersionCheck/inlinedConstant.kt
+++ idea/testData/android/lintQuickfix/targetVersionCheck/inlinedConstant.kt
@@ -1,5 +1,5 @@
 // INTENTION_TEXT: Surround with if (VERSION.SDK_INT >= VERSION_CODES.KITKAT) { ... }
-// INSPECTION_CLASS: org.jetbrains.android.inspections.klint.AndroidLintInspectionToolProvider$AndroidKLintInlinedApiInspection
+// INSPECTION_CLASS: org.jetbrains.android.inspections.lint.AndroidLintInspectionToolProvider$AndroidLintInlinedApiInspection
 
 class Test {
     fun foo(): Int {
