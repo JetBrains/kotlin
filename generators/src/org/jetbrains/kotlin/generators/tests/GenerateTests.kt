18798f8e49d858955495dc95dc2dfaf78ab71297
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-05-04 15:54:08 +0300

    Drop custom Kotlin Android lint, register quickfixes with extension point

--- generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -27,7 +27,6 @@ import org.jetbrains.kotlin.android.configure.AbstractConfigureProjectTest
 import org.jetbrains.kotlin.android.folding.AbstractAndroidResourceFoldingTest
 import org.jetbrains.kotlin.android.intention.AbstractAndroidIntentionTest
 import org.jetbrains.kotlin.android.intention.AbstractAndroidResourceIntentionTest
-import org.jetbrains.kotlin.android.lint.AbstractKotlinLintTest
 import org.jetbrains.kotlin.android.parcel.AbstractParcelBytecodeListingTest
 import org.jetbrains.kotlin.android.quickfix.AbstractAndroidLintQuickfixTest
 import org.jetbrains.kotlin.android.quickfix.AbstractAndroidQuickFixMultiFileTest
@@ -1377,10 +1376,6 @@ fun main(args: Array<String>) {
 
         testClass<AbstractAndroidQuickFixMultiFileTest> {
             model("android/quickfix", pattern = """^(\w+)\.((before\.Main\.\w+)|(test))$""", testMethod = "doTestWithExtraFile")
-        }
-
-        testClass<AbstractKotlinLintTest> {
-            model("android/lint", excludeParentDirs = true)
         }
 
         testClass<AbstractAndroidLintQuickfixTest> {
--- idea/testData/android/lintQuickfix/parcelable/noImplementation.kt
+++ idea/testData/android/lintQuickfix/parcelable/noImplementation.kt
@@ -1,5 +1,5 @@
 // INTENTION_TEXT: Add Parcelable Implementation
-// INSPECTION_CLASS: org.jetbrains.android.inspections.klint.AndroidLintInspectionToolProvider$AndroidKLintParcelCreatorInspection
+// INSPECTION_CLASS: org.jetbrains.android.inspections.lint.AndroidLintInspectionToolProvider$AndroidLintParcelCreatorInspection
 import android.os.Parcelable
 
 class <caret>NoImplementation : Parcelable
