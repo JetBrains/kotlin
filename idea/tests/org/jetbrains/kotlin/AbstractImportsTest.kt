a4c384aed8f3cc63701512fc24b8512b822ba745
Alexey Andreev <alexey.andreev@jetbrains.com>
2017-05-05 13:46:30 +0300

    Fix test compilation errors

--- idea/tests/org/jetbrains/kotlin/AbstractImportsTest.kt
+++ idea/tests/org/jetbrains/kotlin/AbstractImportsTest.kt
@@ -36,7 +36,7 @@ abstract class AbstractImportsTest : KotlinLightCodeInsightFixtureTestCase() {
     protected fun doTest(testPath: String) {
         val settingManager = CodeStyleSettingsManager.getInstance()
         val tempSettings = settingManager.currentSettings.clone()
-        settingManager.temporarySettings = tempSettings
+        settingManager.setTemporarySettings(tempSettings)
 
         val codeStyleSettings = KotlinCodeStyleSettings.getInstance(project)
 
