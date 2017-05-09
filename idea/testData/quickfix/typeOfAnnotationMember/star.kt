e76f1acc0bfd13fd9ba3cb13a6bef1dedcba3169
Nadia Humbert-Labeaumaz <np.humbert@gmail.com>
2017-05-09 09:27:37 +0200

    Remove "Move to class body" intention for annotation parameters (#1076)
    #KT-17708 Fixed

--- idea/testData/quickfix/typeOfAnnotationMember/star.kt
+++ idea/testData/quickfix/typeOfAnnotationMember/star.kt
@@ -1,5 +1,4 @@
 // "Replace array of boxed with array of primitive" "false"
-// ACTION: Move to class body
 // ERROR: Invalid type of annotation member
 annotation class SuperAnnotation(
         val foo: <caret>Array<*>,
--- idea/testData/quickfix/typeOfAnnotationMember/string.kt
+++ idea/testData/quickfix/typeOfAnnotationMember/string.kt
@@ -1,5 +1,4 @@
 // "Replace array of boxed with array of primitive" "false"
-// ACTION: Move to class body
 annotation class SuperAnnotation(
         val str: <caret>Array<String>
 )
