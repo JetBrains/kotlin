91412b0ee962c409054d99600966de2d230de4e1
baratynskiy <baratynskiy@gmail.com>
2017-05-12 13:28:22 +0300

    Add tools.jar for JPS plugin

--- compiler/javac-wrapper/src/org/jetbrains/kotlin/javac/JavacWrapper.kt
+++ compiler/javac-wrapper/src/org/jetbrains/kotlin/javac/JavacWrapper.kt
@@ -324,7 +324,7 @@ class JavacWrapper(javaFiles: Collection<File>,
             }
         }
 
-        return symbol?.let { SymbolBasedClass(it, this@JavacWrapper, it.classfile) }
+        return symbol.let { SymbolBasedClass(it, this@JavacWrapper, it.classfile) }
     }
 
 }
