8fbb627de77eed9e3489c9eb2297978ae4b42fc3
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-05-12 19:38:32 +0300

    Remove "EXPERIMENTAL" from 1.2 version description in tests and gradle options

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinCommonOptions.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinCommonOptions.kt
@@ -6,14 +6,14 @@ interface KotlinCommonOptions  : org.jetbrains.kotlin.gradle.dsl.KotlinCommonToo
 
     /**
      * Allow to use declarations only from the specified version of bundled libraries
-     * Possible values: "1.0", "1.1", "1.2 (EXPERIMENTAL)"
+     * Possible values: "1.0", "1.1", "1.2"
      * Default value: null
      */
      var apiVersion: kotlin.String?
 
     /**
      * Provide source compatibility with specified language version
-     * Possible values: "1.0", "1.1", "1.2 (EXPERIMENTAL)"
+     * Possible values: "1.0", "1.1", "1.2"
      * Default value: null
      */
      var languageVersion: kotlin.String?
