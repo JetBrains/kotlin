d28f8ce7f20c6e984a8835c622e40ac0dbc51503
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-05-12 18:05:14 +0700

    samples: Add scripts for samples building
    This pach adds a root gradle project in the samples directory
    which allows one to build all gradle samples using `./gradlew build`
    command. It also adds a shell script in the samples directory which
    can build all samples (excluding tensorflow one) using their build.sh
    scripts.

--- samples/gtk/README.md
+++ samples/gtk/README.md
@@ -4,7 +4,10 @@
  applications with the GTK toolkit.
 
 To build use `./build.sh` script without arguments (or specify `TARGET` variable if cross-compiling).
+You also may use gradle to build the sample: `../gradlew build`.
+
 Do not forget to install GTK3.
+
 On Mac use `port install gtk3`, on Debian flavours of Linux - `apt-get install libgtk-3-dev`.
 To run on Mac also install XQuartz X server (https://www.xquartz.org/), and then
 
--- samples/gtk/build.sh
+++ samples/gtk/build.sh
@@ -4,6 +4,7 @@ PATH=../../dist/bin:../../bin:$PATH
 DIR=.
 
 IPREFIX_macbook=-I/opt/local/include
+#IPREFIX_macbook=-I/usr/local/include
 IPREFIX_linux=-I/usr/include
 LINKER_ARGS_macbook="-L/opt/local/lib -lglib-2.0 -lgdk-3.0 -lgtk-3 -lgio-2.0 -lgobject-2.0"
 LINKER_ARGS_linux="-L/usr/lib/x86_64-linux-gnu -lglib-2.0 -lgdk-3 -lgtk-3 -lgio-2.0 -lgobject-2.0"
@@ -26,7 +27,8 @@ COMPILER_ARGS=${!var} # add -opt for an optimized build.
 if [ ! -f $DIR/gtk3.bc ]; then
   echo "Generating GTK stubs (once), may take few mins depending on the hardware..."
   cinterop -J-Xmx8g -copt $IPREFIX/atk-1.0 -copt $IPREFIX/gdk-pixbuf-2.0 -copt $IPREFIX/cairo -copt $IPREFIX/pango-1.0 \
-  -copt -I/opt/local/lib/glib-2.0/include -copt -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -copt $IPREFIX/gtk-3.0 -copt $IPREFIX/glib-2.0 -def $DIR/gtk3.def \
+  -copt -I/opt/local/lib/glib-2.0/include -copt -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -copt -I/usr/local/lib/glib-2.0/include \
+  -copt $IPREFIX/gtk-3.0 -copt $IPREFIX/glib-2.0 -def $DIR/gtk3.def \
   -target $TARGET -o $DIR/gtk3.bc || exit 1
 fi
 konanc -target $TARGET $DIR/src -library $DIR/gtk3.bc -linkerArgs "$LINKER_ARGS" -o $DIR/Gtk3Demo.kexe || exit 1
