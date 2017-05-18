ff6a28b64c1a5a9127a19d28afee320e1c6d2b64
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2017-05-18 14:25:49 +0300

    Add util method to BuildIns is `kotlin.Function` type.

--- core/descriptors/src/org/jetbrains/kotlin/builtins/KotlinBuiltIns.java
+++ core/descriptors/src/org/jetbrains/kotlin/builtins/KotlinBuiltIns.java
@@ -275,6 +275,8 @@ public abstract class KotlinBuiltIns {
 
         public final FqNameUnsafe _enum = fqNameUnsafe("Enum");
 
+        public final FqNameUnsafe functionSupertype = fqNameUnsafe("Function");
+
 
 
         public final FqName throwable = fqName("Throwable");
@@ -1092,6 +1094,10 @@ public abstract class KotlinBuiltIns {
         }
 
         return false;
+    }
+
+    public static boolean isNotNullOrNullableFunctionSupertype(@NotNull KotlinType type) {
+        return isConstructedFromGivenClass(type, FQ_NAMES.functionSupertype);
     }
 
     public static FqName getPrimitiveFqName(@NotNull PrimitiveType primitiveType) {
