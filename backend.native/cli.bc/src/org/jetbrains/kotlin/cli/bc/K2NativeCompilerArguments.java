34491cfb01de623a4e49874c506ea1d31ccd1499
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-05-23 15:29:03 +0300

    Lexically sort compiler option definitions.

--- backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2NativeCompilerArguments.java
+++ backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2NativeCompilerArguments.java
@@ -20,41 +20,9 @@ import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments;
 import org.jetbrains.kotlin.cli.common.arguments.Argument;
 
 public class K2NativeCompilerArguments extends CommonCompilerArguments {
-    @Argument(value = "-output", shortName = "-o", valueDescription = "<path>", description = "Output file path")
-    public String outputFile;
-
-    @Argument(value = "-runtime", valueDescription = "<path>", description = "Override standard 'runtime.bc' location")
-    public String runtimeFile;
-
-    @Argument(value = "-properties", valueDescription = "<path>", description = "Override standard 'konan.properties' location")
-    public String propertyFile;
-
-    @Argument(value = "-library", shortName = "-l", valueDescription = "<path>", description = "Link with the library")
-    public String[] libraries;
-
-    @Argument(value = "-nativelibrary", shortName = "-nl", valueDescription = "<path>", description = "Include the native library")
-    public String[] nativeLibraries;
-
-    @Argument(value = "-nolink", description = "Don't link, just produce a bitcode file")
-    public boolean nolink;
-
-    @Argument(value = "-nopack", description = "Don't pack the library into a klib file")
-    public boolean nopack;
-
-    @Argument(value = "-nomain", description = "Assume 'main' entry point to be provided by external libraries")
-    public boolean nomain;
-
-    @Argument(value = "-linkerArgs", valueDescription = "<arg>", description = "Pass arguments to linker", delimiter = " ")
-    public String[] linkerArguments;
-
-    @Argument(value = "-nostdlib", description = "Don't link with stdlib")
-    public boolean nostdlib;
-
-    @Argument(value = "-opt", description = "Enable optimizations during compilation")
-    public boolean optimization;
-
-    @Argument(value = "-target", valueDescription = "<target>", description = "Set hardware target")
-    public String target;
+    // First go the options interesting to the general public.
+    // Prepend them with a single dash.
+    // Keep the list lexically sorted.
 
     @Argument(value = "-g", description = "Enable emitting debug information")
     public boolean debug;
@@ -62,38 +30,51 @@ public class K2NativeCompilerArguments extends CommonCompilerArguments {
     @Argument(value = "-enable_assertions", shortName = "-ea", description = "Enable runtime assertions in generated code")
     public boolean enableAssertions;
 
-    // The rest of the options are only interesting for developers.
-    // Make sure to prepend them with double dash.
+    @Argument(value = "-library", shortName = "-l", valueDescription = "<path>", description = "Link with the library")
+    public String[] libraries;
+
+    @Argument(value = "-linkerArgs", valueDescription = "<arg>", description = "Pass arguments to linker", delimiter = " ")
+    public String[] linkerArguments;
 
     @Argument(value = "-list_targets", description = "List available hardware targets")
     public boolean listTargets;
 
-    @Argument(value = "--print_ir", description = "Print IR")
-    public boolean printIr;
+    @Argument(value = "-nativelibrary", shortName = "-nl", valueDescription = "<path>", description = "Include the native library")
+    public String[] nativeLibraries;
 
-    @Argument(value = "--print_ir_with_descriptors", description = "Print IR with descriptors")
-    public boolean printIrWithDescriptors;
+    @Argument(value = "-nolink", description = "Don't link, just produce a bitcode file")
+    public boolean nolink;
 
-    @Argument(value = "--print_descriptors", description = "Print descriptor tree")
-    public boolean printDescriptors;
+    @Argument(value = "-nomain", description = "Assume 'main' entry point to be provided by external libraries")
+    public boolean nomain;
 
-    @Argument(value = "--print_locations", description = "Print locations")
-    public boolean printLocations;
+    @Argument(value = "-nopack", description = "Don't pack the library into a klib file")
+    public boolean nopack;
 
-    @Argument(value = "--print_bitcode", description = "Print llvm bitcode")
-    public boolean printBitCode;
+    @Argument(value = "-nostdlib", description = "Don't link with stdlib")
+    public boolean nostdlib;
+
+    @Argument(value = "-opt", description = "Enable optimizations during compilation")
+    public boolean optimization;
+
+    @Argument(value = "-output", shortName = "-o", valueDescription = "<path>", description = "Output file path")
+    public String outputFile;
+
+    @Argument(value = "-properties", valueDescription = "<path>", description = "Override standard 'konan.properties' location")
+    public String propertyFile;
 
     @Argument(value = "-repo", shortName = "-r", valueDescription = "<path>", description = "Library search path")
     public String[] repositories;
 
-    @Argument(value = "--verify_ir", description = "Verify IR")
-    public boolean verifyIr;
+    @Argument(value = "-runtime", valueDescription = "<path>", description = "Override standard 'runtime.bc' location")
+    public String runtimeFile;
 
-    @Argument(value = "--verify_descriptors", description = "Verify descriptor tree")
-    public boolean verifyDescriptors;
+    @Argument(value = "-target", valueDescription = "<target>", description = "Set hardware target")
+    public String target;
 
-    @Argument(value = "--verify_bitcode", description = "Verify llvm bitcode after each method")
-    public boolean verifyBitCode;
+    // The rest of the options are only interesting to the developers.
+    // Make sure to prepend them with a double dash.
+    // Keep the list lexically sorted.
 
     @Argument(value = "--enable", valueDescription = "<Phase>", description = "Enable backend phase")
     public String[] enablePhases;
@@ -101,12 +82,37 @@ public class K2NativeCompilerArguments extends CommonCompilerArguments {
     @Argument(value = "--disable", valueDescription = "<Phase>", description = "Disable backend phase")
     public String[] disablePhases;
 
-    @Argument(value = "--verbose", valueDescription = "<Phase>", description = "Trace phase execution")
-    public String[] verbosePhases;
-
     @Argument(value = "--list_phases", description = "List all backend phases")
     public boolean listPhases;
 
+    @Argument(value = "--print_bitcode", description = "Print llvm bitcode")
+    public boolean printBitCode;
+
+    @Argument(value = "--print_descriptors", description = "Print descriptor tree")
+    public boolean printDescriptors;
+
+    @Argument(value = "--print_ir", description = "Print IR")
+    public boolean printIr;
+
+    @Argument(value = "--print_ir_with_descriptors", description = "Print IR with descriptors")
+    public boolean printIrWithDescriptors;
+
+    @Argument(value = "--print_locations", description = "Print locations")
+    public boolean printLocations;
+
     @Argument(value = "--time", description = "Report execution time for compiler phases")
     public boolean timePhases;
+
+    @Argument(value = "--verbose", valueDescription = "<Phase>", description = "Trace phase execution")
+    public String[] verbosePhases;
+
+    @Argument(value = "--verify_bitcode", description = "Verify llvm bitcode after each method")
+    public boolean verifyBitCode;
+
+    @Argument(value = "--verify_descriptors", description = "Verify descriptor tree")
+    public boolean verifyDescriptors;
+
+    @Argument(value = "--verify_ir", description = "Verify IR")
+    public boolean verifyIr;
+
 }
