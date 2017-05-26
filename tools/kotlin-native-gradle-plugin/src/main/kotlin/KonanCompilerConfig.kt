62134a0573b8ffaed5eb0c2f559055292460890a
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-05-26 13:15:06 +0700

    gradle-plugin: Fix interop artifact path

--- tools/kotlin-native-gradle-plugin/src/main/kotlin/KonanCompilerConfig.kt
+++ tools/kotlin-native-gradle-plugin/src/main/kotlin/KonanCompilerConfig.kt
@@ -91,7 +91,7 @@ open class KonanCompilerConfig(
         anotherTask.target?.let { target(it) }
         anotherTask.languageVersion?.let { languageVersion(it) }
         anotherTask.apiVersion?.let { apiVersion(it) }
-        anotherTask.produce?.let { produce(it) }
+        anotherTask.produce.let { produce(it) }
 
         if (anotherTask.noStdLib) noStdLib()
         if (anotherTask.noMain) noMain()
@@ -107,10 +107,7 @@ open class KonanCompilerConfig(
         compilationTask.dependsOn(generateStubsTask)
 
         linkerOpts(generateStubsTask.linkerOpts)
-        libraries(project.fileTree(compileStubsTask.outputDir).apply {
-            builtBy(compileStubsTask)
-            include("**/*.bc")
-        })
+        library(compileStubsTask.artifactPath)
         nativeLibraries(project.fileTree(generateStubsTask.libsDir).apply {
             builtBy(generateStubsTask)
             include("**/*.bc")
