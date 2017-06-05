fe5945aebe9615fc8007e89307663b7e9a52be7a
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-06-05 17:58:46 +0300

    Using a single shell script for both 'cinterop' and 'konanc'.

---
+++ utilities/src/main/kotlin/org/jetbrains/kotlin/cli/utilities/main.kt
@@ -0,0 +1,54 @@
+package org.jetbrains.kotlin.cli.utilities
+
+import org.jetbrains.kotlin.backend.konan.util.File
+import org.jetbrains.kotlin.cli.bc.main as konancMain
+import org.jetbrains.kotlin.native.interop.gen.jvm.main as cinteropMain
+
+fun invokeCinterop(args: Array<String>) {
+    var outputFileName = "nativelib"
+    var target = "host"
+    for (i in args.indices) {
+        if (args[i].startsWith("-o")) 
+            outputFileName = args.getOrElse(i+1){outputFileName}
+        if (args[i] == "-target") 
+            target = args.getOrElse(i+1){target}
+    }
+
+    val buildDir = File("$outputFileName-build")
+    val generatedDir = File(buildDir, "kotlin")
+    val nativesDir = File(buildDir, "natives")
+    val cstubsName ="cstubs"
+
+    val additionalArgs = listOf<String>(
+        "-generated", generatedDir.path, 
+        "-natives", nativesDir.path, 
+        "-cstubsname", cstubsName,
+        "-flavor", "native")
+
+    val cinteropArgs = (additionalArgs + args.toList()).toTypedArray()
+    cinteropMain(cinteropArgs)
+
+    val konancArgs = arrayOf(
+        generatedDir.path, 
+        "-produce", "library", 
+        "-nativelibrary", File(nativesDir, "$cstubsName.bc").path,
+        "-o", outputFileName,
+        "-target", target)
+    konancMain(konancArgs)
+}
+
+fun main(args: Array<String>) {
+    val utilityName = args[0]
+    val utilityArgs = args.drop(1).toTypedArray()
+    when (utilityName) {
+        "konanc" ->
+            konancMain(utilityArgs)
+        "cinterop" ->
+            invokeCinterop(utilityArgs)
+        "klib" ->
+            println("would run: klib $utilityArgs")
+        else ->
+            println("Unexpected utility name")
+    }
+}
+
