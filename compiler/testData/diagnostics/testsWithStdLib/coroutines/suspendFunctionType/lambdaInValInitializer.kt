26cc08be65c918b528ee480f96ed3d98e9cc4ee1
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-06-07 13:06:45 +0300

    Fix anonymous function literals handling in type checker
    - [NI] Create type info directly if the expected type is functional.
    - Properly handle suspended function expected type.

--- compiler/testData/diagnostics/testsWithStdLib/coroutines/suspendFunctionType/lambdaInValInitializer.kt
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/suspendFunctionType/lambdaInValInitializer.kt
@@ -4,3 +4,8 @@ val test1: suspend () -> Unit = {}
 val test2: suspend Any.() -> Unit = {}
 val test3: suspend Any.(Int) -> Int = { k: Int -> k + 1 }
 val test4: SuspendFn = {}
+
+val test1f: suspend () -> Unit = fun () {}
+val test2f: suspend Any.() -> Unit = fun Any.() {}
+val test3f: suspend Any.(Int) -> Int = fun (k: Int) = k + 1
+val test4f: SuspendFn = fun Any.() {}
