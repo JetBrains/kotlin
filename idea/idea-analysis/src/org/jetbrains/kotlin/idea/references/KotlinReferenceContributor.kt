3445db2d50e4840ac4b8df94fe7e1c05ed98c466
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-06-20 23:57:59 +0300

    Register reference contributor for collection literals
     #KT-18551 Fixed

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/references/KotlinReferenceContributor.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/references/KotlinReferenceContributor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,6 +52,8 @@ class KotlinReferenceContributor() : AbstractKotlinReferenceContributor() {
             registerProvider(factory = ::KtInvokeFunctionReference)
 
             registerProvider(factory = ::KtArrayAccessReference)
+
+            registerProvider(factory = ::KtCollectionLiteralReference)
 
             registerProvider(factory = ::KtForLoopInReference)
 
