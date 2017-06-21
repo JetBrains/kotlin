7fe03ad73a07e926bbca1d1b4e6316c6646afdc1
Dimach <dimach2142@gmail.com>
2017-06-21 15:02:06 +0300

    KT-14799: added inspection to simplify successive null checks.

---
+++ idea/testData/inspectionsLocal/nullChecksToSafeCall/orCase.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val a: Testtt? = Testtt()
+    if (a<caret> == null || a.a == null || a.a.a == null || a.a.a.a == null) {
+
+    }
+}
+
+class Testtt {
+    val a: Testtt? = null
+}
