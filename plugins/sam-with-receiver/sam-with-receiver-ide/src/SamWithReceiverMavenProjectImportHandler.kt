f39f0be56d39909bddb4728f618a8e83a680d87d
Dmitry Jemerov <yole@jetbrains.com>
2017-06-21 18:47:34 +0200

    No Maven in AS

--- plugins/sam-with-receiver/sam-with-receiver-ide/src/SamWithReceiverMavenProjectImportHandler.kt
+++ plugins/sam-with-receiver/sam-with-receiver-ide/src/SamWithReceiverMavenProjectImportHandler.kt
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jetbrains.kotlin.samWithReceiver.ide
 
+/*
 import org.jetbrains.kotlin.annotation.plugin.ide.AbstractMavenImportHandler
 import org.jetbrains.kotlin.samWithReceiver.SamWithReceiverCommandLineProcessor
 import org.jetbrains.kotlin.utils.PathUtil
@@ -52,3 +52,4 @@ class SamWithReceiverMavenProjectImportHandler : AbstractMavenImportHandler() {
         return annotations
     }
 }
+*/
