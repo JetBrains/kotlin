4320b639221d1301e66cf0349826ce19ed1ef4f2
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-06-30 16:19:39 +0300

    Ensure that no boolean negation is generated for !in expression
     #KT-18777 Fixed Target versions 1.1.4

---
+++ compiler/testData/codegen/bytecodeText/ranges/ifNotInRange.kt
@@ -0,0 +1,16 @@
+fun testInt(x: Int, a: Int, b: Int) =
+        if (x !in a .. b) "no" else "yes"
+
+fun testLong(x: Long, a: Long, b: Long) =
+        if (x !in a .. b) "no" else "yes"
+
+fun testDouble(x: Double, a: Double, b: Double) =
+        if (x !in a .. b) "no" else "yes"
+
+fun testString(x: String, a: String, b: String) =
+        if (x !in a .. b) "no" else "yes"
+
+fun testCollection(x: Any, xs: List<Any>) =
+        if (x !in xs) "no" else "yes"
+
+// 0 IXOR
