1605475cc415f739df65d0941f854d7e0b44220f
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2017-07-04 16:18:41 +0200

    NoArg: Do not invoke initializers by default. Require "invokeInitializers" option to be set explicitly (KT-18667, KT-18668)
    (cherry picked from commit b990079)

--- libraries/tools/kotlin-noarg/src/main/kotlin/org/jetbrains/kotlin/noarg/gradle/NoArgSubplugin.kt
+++ libraries/tools/kotlin-noarg/src/main/kotlin/org/jetbrains/kotlin/noarg/gradle/NoArgSubplugin.kt
@@ -71,6 +71,7 @@ class NoArgKotlinGradleSubplugin : KotlinGradleSubplugin<AbstractCompile> {
 
         private val ANNOTATION_ARG_NAME = "annotation"
         private val PRESET_ARG_NAME = "preset"
+        private val INVOKE_INITIALIZERS_ARG_NAME = "invokeInitializers"
     }
 
     override fun isApplicable(project: Project, task: AbstractCompile) = NoArgGradleSubplugin.isEnabled(project)
@@ -94,6 +95,10 @@ class NoArgKotlinGradleSubplugin : KotlinGradleSubplugin<AbstractCompile> {
 
         for (preset in noArgExtension.myPresets) {
             options += SubpluginOption(PRESET_ARG_NAME, preset)
+        }
+
+        if (noArgExtension.invokeInitializers) {
+            options += SubpluginOption(INVOKE_INITIALIZERS_ARG_NAME, "true")
         }
 
         return options
