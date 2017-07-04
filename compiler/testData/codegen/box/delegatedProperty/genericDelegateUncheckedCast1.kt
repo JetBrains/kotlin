fd380f75454eea2115d7ec86be9a32559a4a497b
Igor Chevdar <igor.chevdar@jetbrains.com>
2017-07-04 12:52:37 +0300

    Split up test on map access onto two tests: stdlib & compiler box test

---
+++ compiler/testData/codegen/box/delegatedProperty/genericDelegateUncheckedCast1.kt
@@ -0,0 +1,38 @@
+// IGNORE_BACKEND: JS
+
+import kotlin.reflect.KProperty
+
+class Delegate<T>(var inner: T) {
+    operator fun getValue(t: Any?, p: KProperty<*>): T = inner
+    operator fun setValue(t: Any?, p: KProperty<*>, i: T) { inner = i }
+}
+
+val del = Delegate("zzz")
+
+class A {
+    inner class B {
+        var prop: String by del
+    }
+}
+
+inline fun asFailsWithCCE(block: () -> Unit) {
+    try {
+        block()
+    }
+    catch (e: ClassCastException) {
+        return
+    }
+    catch (e: Throwable) {
+        throw AssertionError("Should throw ClassCastException, got $e")
+    }
+    throw AssertionError("Should throw ClassCastException, no exception thrown")
+}
+
+fun box(): String {
+    val c = A().B()
+
+    (del as Delegate<Int>).inner = 10
+    asFailsWithCCE { c.prop }  // does not fail in JS due KT-8135.
+
+    return "OK"
+}
