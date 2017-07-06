9772ce9de3ea834b778c13a04304fce57575146f
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-07-06 17:16:25 +0300

    Minor: test modification after review

--- compiler/testData/codegen/bytecodeText/ranges/inComparableRangeLiteral.kt
+++ compiler/testData/codegen/bytecodeText/ranges/inComparableRangeLiteral.kt
@@ -1,5 +1,6 @@
 fun test1(a: String) = a in "alpha" .. "omega"
 fun test2(a: String) = a !in "alpha" .. "omega"
 
-// 0 INVOKESTATIC kotlin/ranges/RangesKt.rangeTo
-// 0 INVOKEINTERFACE kotlin/ranges/ClosedRange.contains
+// 0 rangeTo
+// 0 contains
+// 4 compareTo
