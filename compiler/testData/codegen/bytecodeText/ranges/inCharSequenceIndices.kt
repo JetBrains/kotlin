6a3ff5ca463a2edd9ffc369a7772a1f59e223bdd
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-07-06 17:34:22 +0300

    Add tests for intrinsified in/!in and mismatching range types

--- compiler/testData/codegen/bytecodeText/ranges/inCharSequenceIndices.kt
+++ compiler/testData/codegen/bytecodeText/ranges/inCharSequenceIndices.kt
@@ -2,7 +2,10 @@
 
 fun testCharSequence(i: Int, cs: CharSequence) = i in cs.indices
 
+fun testLongWithCharSequence(i: Long, cs: CharSequence) = i in cs.indices
+
 // 0 INVOKESTATIC kotlin/text/StringsKt.getIndices
 // 0 INVOKEVIRTUAL kotlin/ranges/IntRange.contains
-// 1 INVOKEINTERFACE java/lang/CharSequence.length
+// 2 INVOKEINTERFACE java/lang/CharSequence.length
+// 2 I2L
 
