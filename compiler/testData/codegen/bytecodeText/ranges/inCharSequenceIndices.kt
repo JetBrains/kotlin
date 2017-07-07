22e12dc13923a940655ee385f5cca45dcadba0ea
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-07-07 12:55:10 +0300

    Minor: use method names in bytecode text tests

--- compiler/testData/codegen/bytecodeText/ranges/inCharSequenceIndices.kt
+++ compiler/testData/codegen/bytecodeText/ranges/inCharSequenceIndices.kt
@@ -4,8 +4,8 @@ fun testCharSequence(i: Int, cs: CharSequence) = i in cs.indices
 
 fun testLongWithCharSequence(i: Long, cs: CharSequence) = i in cs.indices
 
-// 0 INVOKESTATIC kotlin/text/StringsKt.getIndices
-// 0 INVOKEVIRTUAL kotlin/ranges/IntRange.contains
-// 2 INVOKEINTERFACE java/lang/CharSequence.length
+// 0 getIndices
+// 0 contains
+// 2 length
 // 2 I2L
 
