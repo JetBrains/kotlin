63ec0f1b1541306ac42128e94d9b851bd36e9da2
Dmitry Jemerov <yole@jetbrains.com>
2017-07-12 14:31:19 +0200

    Set library kind when importing projects from Gradle

---
+++ idea/idea-android/src/org/jetbrains/kotlin/android/configure/KotlinAndroidGradleLibraryDataService.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.android.configure
+
+import com.android.tools.idea.gradle.AndroidProjectKeys
+import com.android.tools.idea.gradle.JavaProject
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.project.ModuleData
+import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
+import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.impl.libraries.LibraryEx
+import org.jetbrains.kotlin.config.TargetPlatformKind
+import org.jetbrains.kotlin.idea.configuration.detectPlatformByPlugin
+import org.jetbrains.kotlin.idea.framework.JSLibraryKind
+
+class KotlinAndroidGradleLibraryDataService : AbstractProjectDataService<JavaProject, Void>() {
+    override fun getTargetDataKey() = AndroidProjectKeys.JAVA_PROJECT
+
+    override fun postProcess(
+            toImport: MutableCollection<DataNode<JavaProject>>,
+            projectData: ProjectData?,
+            project: Project,
+            modelsProvider: IdeModifiableModelsProvider
+    ) {
+        for (dataNode in toImport) {
+            val platform = detectPlatformByPlugin(dataNode.parent as DataNode<ModuleData>)
+            if (platform == TargetPlatformKind.JavaScript) {
+                for (dep in dataNode.data.jarLibraryDependencies) {
+                    val library = modelsProvider.getLibraryByName(dep.name) as LibraryEx? ?: continue
+                    if (library.kind == null) {
+                        (modelsProvider.getModifiableLibraryModel(library) as LibraryEx.ModifiableModelEx).kind = JSLibraryKind
+                    }
+                }
+            }
+        }
+    }
+}
