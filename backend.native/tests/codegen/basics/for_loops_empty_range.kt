a97a3109876cf29705adcb13dc87e1cedcc9f140
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-07-13 12:29:48 +0700

    tests: Add tests for empty ranges in 'for' loops

---
+++ backend.native/tests/codegen/basics/for_loops_empty_range.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    // Simple loops
+    for (i in 4..0) { print(i) }
+    for (i in 4 until 0) { print(i) }
+    for (i in 0 downTo 4) { print(i) }
+    // Steps
+    for (i in 4..0 step 2) { print(i) }
+    for (i in 4 until 0 step 2) { print(i) }
+    for (i in 0 downTo 4 step 2) { print(i) }
+    // Two steps
+    for (i in 6..0 step 2 step 3) { print(i) }
+    for (i in 6 until 0 step 2 step 3) { print(i) }
+    for (i in 0 downTo 6 step 2 step 3) { print(i) }
+
+    println("OK")
+}
