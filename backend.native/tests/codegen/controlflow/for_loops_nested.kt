d6e0f67d4e81f221fbb4eb125025f65fa6d8d779
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-07-14 12:58:01 +0700

    tests: Add test for nested 'for' loops

---
+++ backend.native/tests/codegen/controlflow/for_loops_nested.kt
@@ -0,0 +1,59 @@
+fun main(args: Array<String>) {
+    // Simple
+    for (i in 0..2) {
+        for (j in 0..2) {
+            print("$i$j ")
+        }
+    }
+    println()
+
+    // Break
+    l1@for (i in 0..2) {
+        l2@for (j in 0..2) {
+            print("$i$j ")
+            if (j == 1) break
+        }
+    }
+    println()
+
+    l1@for (i in 0..2) {
+        l2@for (j in 0..2) {
+            print("$i$j ")
+            if (j == 1) break@l2
+        }
+    }
+    println()
+
+    l1@for (i in 0..2) {
+        l2@for (j in 0..2) {
+            print("$i$j ")
+            if (j == 1) break@l1
+        }
+    }
+    println()
+
+    // Continue
+    l1@for (i in 0..2) {
+        l2@for (j in 0..2) {
+            if (j == 1) continue
+            print("$i$j ")
+        }
+    }
+    println()
+
+    l1@for (i in 0..2) {
+        l2@for (j in 0..2) {
+            if (j == 1) continue@l2
+            print("$i$j ")
+        }
+    }
+    println()
+
+    l1@for (i in 0..2) {
+        l2@for (j in 0..2) {
+            if (j == 1) continue@l1
+            print("$i$j ")
+        }
+    }
+    println()
+}
