7555bb6c3cdb299b4c46697029b3edca374ba013
Dmitry Jemerov <yole@jetbrains.com>
2017-07-18 18:49:36 +0200

    Support formatting of 'where' clauses  #KT-14083 Fixed

---
+++ idea/testData/formatter/WhereClause.kt
@@ -0,0 +1,12 @@
+abstract class RustNavigationContributorBase<T> protected constructor(
+        private val indexKey: StubIndexKey<String, T>,
+        private val clazz: Class<T>
+) : ChooseByNameContributor, GotoClassContributor
+where T : NavigationItem,
+T : RustNamedElement {
+}
+
+fun <T> foo()
+where T : NavigationItem,
+T : RustNamedElement {
+}
