04da7a161e4029e0fc01e6c69d92037ca693f7b1
Dmitry Jemerov <yole@jetbrains.com>
2017-07-20 12:32:13 +0200

    Fix tests affected by annotation wrapping changes

--- j2k/testData/fileOrElement/annotations/modifiersToAnnotationsFromPropertyAccessors.kt
+++ j2k/testData/fileOrElement/annotations/modifiersToAnnotationsFromPropertyAccessors.kt
@@ -1,8 +1,11 @@
 class WithModifiersOnAccessors {
 
-    @get:Synchronized @set:Synchronized var sync = 0
+    @get:Synchronized
+    @set:Synchronized
+    var sync = 0
 
-    @get:Strictfp var strict = 0.0
+    @get:Strictfp
+    var strict = 0.0
     @Synchronized private fun methSync() {}
     @Strictfp protected fun methStrict() {}
 }
