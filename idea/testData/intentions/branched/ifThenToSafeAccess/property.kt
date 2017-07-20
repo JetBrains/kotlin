982ea8900c0a04e5a50c9a836892985fdb77c2b0
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-07-20 15:27:23 +0300

    If-then to safe access: more correct receiver calculation
    So #KT-18928 Fixed

---
+++ idea/testData/intentions/branched/ifThenToSafeAccess/property.kt
@@ -0,0 +1,11 @@
+// IS_APPLICABLE: true
+// INTENTION_TEXT: Replace 'if' expression with safe cast expression
+
+interface Foo
+interface Bar : Foo
+
+data class Data(val foo: Foo)
+
+fun handle(data: Data) {
+    val bar = <caret>if (data.foo is Bar) data.foo else null
+}
