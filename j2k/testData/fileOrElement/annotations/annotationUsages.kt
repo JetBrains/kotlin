ae6973b8e4fed49f6108c5237c8d3939fea8d4de
Dmitry Jemerov <yole@jetbrains.com>
2017-07-20 12:32:13 +0200

    Fix tests affected by annotation wrapping changes
    (cherry picked from commit fbcc519)

--- j2k/testData/fileOrElement/annotations/annotationUsages.kt
+++ j2k/testData/fileOrElement/annotations/annotationUsages.kt
@@ -12,12 +12,14 @@ import javaApi.*
 @Anon7(String::class, StringBuilder::class)
 @Anon8(classes = arrayOf(String::class, StringBuilder::class))
 internal class C {
-    @Anon5(1) @Deprecated("") private val field1 = 0
+    @Anon5(1)
+    @Deprecated("") private val field1 = 0
 
     @Anon5(1)
     private val field2 = 0
 
-    @Anon5(1) var field3 = 0
+    @Anon5(1)
+    var field3 = 0
 
     @Anon5(1)
     var field4 = 0
