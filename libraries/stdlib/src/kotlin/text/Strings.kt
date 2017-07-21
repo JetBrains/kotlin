da6fa391feb4043d275002e038c7fbec0094a27f
dsavvinov <dsavvinov@gmail.com>
2017-07-21 17:56:17 +0300

    Annotated String.isNullOrEmpty() and String.isNullOrBlank() with effects

--- libraries/stdlib/src/kotlin/text/Strings.kt
+++ libraries/stdlib/src/kotlin/text/Strings.kt
@@ -225,7 +225,8 @@ public fun String.padEnd(length: Int, padChar: Char = ' '): String
  * Returns `true` if this nullable char sequence is either `null` or empty.
  */
 @kotlin.internal.InlineOnly
-public inline fun CharSequence?.isNullOrEmpty(): Boolean = this == null || this.length == 0
+@kotlin.internal.Returns(kotlin.internal.ConstantValue.FALSE)
+public inline fun @receiver:kotlin.internal.Equals(kotlin.internal.ConstantValue.NOT_NULL) CharSequence?.isNullOrEmpty(): Boolean = this == null || this.length == 0
 
 /**
  * Returns `true` if this char sequence is empty (contains no characters).
@@ -253,7 +254,8 @@ public inline fun CharSequence.isNotBlank(): Boolean = !isBlank()
  * Returns `true` if this nullable char sequence is either `null` or empty or consists solely of whitespace characters.
  */
 @kotlin.internal.InlineOnly
-public inline fun CharSequence?.isNullOrBlank(): Boolean = this == null || this.isBlank()
+@kotlin.internal.Returns(kotlin.internal.ConstantValue.FALSE)
+public inline fun @receiver:kotlin.internal.Equals(kotlin.internal.ConstantValue.NOT_NULL) CharSequence?.isNullOrBlank(): Boolean = this == null || this.isBlank()
 
 /**
  * Iterator for characters of the given char sequence.
