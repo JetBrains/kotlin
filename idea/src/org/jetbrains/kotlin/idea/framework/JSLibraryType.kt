c8452f736f343a30cadc3f54afe1aa80d78c3c9d
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2017-07-24 16:25:46 +0300

    Minor: Fix compilation in 163 branch

--- idea/src/org/jetbrains/kotlin/idea/framework/JSLibraryType.kt
+++ idea/src/org/jetbrains/kotlin/idea/framework/JSLibraryType.kt
@@ -48,7 +48,7 @@ class JSLibraryType : LibraryType<DummyLibraryProperties>(JSLibraryKind) {
                                                                        project)
     }
 
-    override fun getIcon(properties: LibraryProperties<*>?) = KotlinIcons.JS
+    override fun getIcon(properties: LibraryProperties<Any>?) = KotlinIcons.JS
 
     companion object {
         fun getInstance() = Extensions.findExtension(EP_NAME, JSLibraryType::class.java)
