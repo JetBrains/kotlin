f29cf07fa4693294d4f4ab764f057d20815d7db8
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-07-26 15:43:30 +0300

    Android Extensions, minor: Fix test

--- plugins/android-extensions/android-extensions-compiler/testData/codegen/bytecodeShape/simple/simple.kt
+++ plugins/android-extensions/android-extensions-compiler/testData/codegen/bytecodeShape/simple/simple.kt
@@ -14,8 +14,5 @@ public class MyActivity : Activity() {
 // 1 GETSTATIC test/R\$id\.login
 // 1 INVOKEVIRTUAL test/MyActivity\._\$_findCachedViewById
 // 1 CHECKCAST android/widget/Button
-// 12 android/util/SparseArray
-// 1 INVOKEVIRTUAL android/util/SparseArray\.get
-// 1 INVOKEVIRTUAL android/util/SparseArray\.put
-// 1 INVOKEVIRTUAL android/util/SparseArray\.clear
-// 0 java/util/HashMap
+// 0 android/util/SparseArray
+// 12 java/util/HashMap
