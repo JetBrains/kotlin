e9ad4a765dc8fd54e1338b383b85d499f981319e
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2017-07-27 18:26:44 +0300

    Ultimate tests repair: SpringInspectionTestGenerated.testAutowiring_inspectionData_Inspections_test  (KT-18847)

--- tests/org/jetbrains/kotlin/idea/spring/tests/inspections/AbstractSpringInspectionTest.kt
+++ tests/org/jetbrains/kotlin/idea/spring/tests/inspections/AbstractSpringInspectionTest.kt
@@ -5,6 +5,9 @@ import org.jetbrains.kotlin.idea.codeInsight.AbstractInspectionTest
 import org.jetbrains.kotlin.idea.spring.tests.configureSpringFileSetByDirective
 
 abstract class AbstractSpringInspectionTest : AbstractInspectionTest() {
+
+    override val forceUsePackageFolder: Boolean = true
+
     override fun configExtra(psiFiles: List<PsiFile>, options: String) {
         configureSpringFileSetByDirective(myFixture.module, options, psiFiles)
     }
