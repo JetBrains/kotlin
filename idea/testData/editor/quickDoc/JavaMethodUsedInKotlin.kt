f68f804280c1fe7b6fe0e359bf37edef6f25bfcb
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-08-04 21:05:40 +0300

    Fix test behaviour in branch 172

--- idea/testData/editor/quickDoc/JavaMethodUsedInKotlin.kt
+++ idea/testData/editor/quickDoc/JavaMethodUsedInKotlin.kt
@@ -3,6 +3,11 @@ fun ktTest() {
 }
 
 //INFO: <html><head>    <style type="text/css">        #error {            background-color: #eeeeee;            margin-bottom: 10px;        }        p {            margin: 5px 0;        }    </style></head><body><small><b><a href="psi_element://Test"><code>Test</code></a></b></small><PRE><i>@Contract(pure = true)</i>&nbsp;
-//INFO: <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>org.jetbrains.annotations.NotNull</code></a></i>&nbsp;
+//INFO: <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>NotNull</code></a></i>&nbsp;
 //INFO: public static&nbsp;<a href="psi_element://java.lang.Object"><code>Object</code></a>[]&nbsp;<b>foo</b>(<a href="psi_element://java.lang.String"><code>String</code></a>&nbsp;param)</PRE>
+//INFO: <i>Inferred</i> annotations available:<br>
+//INFO: <ul>
+//INFO: <li><i>@org.jetbrains.annotations.Contract(pure = true)</i> <i>@<a href="psi_element://org.jetbrains.annotations.NotNull"><code>org.jetbrains.annotations.NotNull</code></a></i></li>
+//INFO: </ul>
+//INFO:
 //INFO:        Java Method</body></html>
