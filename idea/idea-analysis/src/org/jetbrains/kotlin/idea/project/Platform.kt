5919f5e2b3743fbf34f1ba1446befbbb9fd3734b
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2017-08-04 19:33:43 +0300

    Do not overwrite facet settings in Module.languageVersionSettings

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/Platform.kt
@@ -26,6 +26,7 @@ import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.cli.common.arguments.Argument
 import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.common.arguments.parseCommandLineArguments
 import org.jetbrains.kotlin.config.*
 import org.jetbrains.kotlin.idea.caches.resolve.getResolutionFacade
 import org.jetbrains.kotlin.idea.compiler.configuration.Kotlin2JvmCompilerArgumentsHolder
@@ -110,6 +111,13 @@ val Module.languageVersionSettings: LanguageVersionSettings
                 facetSettings.compilerSettings,
                 this
         )
+
+        val arguments = facetSettings.compilerArguments
+        if (arguments != null) {
+            facetSettings.compilerSettings?.let { compilerSettings ->
+                parseCommandLineArguments(compilerSettings.additionalArgumentsAsList, arguments)
+            }
+        }
 
         return LanguageVersionSettingsImpl(
                 languageVersion,
