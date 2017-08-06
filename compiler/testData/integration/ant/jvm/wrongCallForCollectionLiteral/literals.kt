0b358fb693cac63d5b0bea89e213be07d293c0de
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-08-06 18:42:09 +0200

    Fix collection literals resolve in gradle-based projects
     #KT-19441 Fixed

---
+++ compiler/testData/integration/ant/jvm/wrongCallForCollectionLiteral/literals.kt
@@ -0,0 +1,15 @@
+package lit
+
+annotation class AnnInt(val a: IntArray)
+
+@Suppress("UNSUPPORTED_FEATURE")
+@AnnInt([1, 2])
+fun foo() {}
+
+@AnnInt(intArrayOf(1, 2))
+fun bar() {}
+
+fun main(args: Array<String>) {
+    foo()
+    bar()
+}
