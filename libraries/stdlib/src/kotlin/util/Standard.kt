1be40c5a6a7c199059bd2c1db51a005b16d96839
dsavvinov <dsavvinov@gmail.com>
2017-08-09 14:19:53 +0300

    Annotated Standard.kt with effect annotations

--- libraries/stdlib/src/kotlin/util/Standard.kt
+++ libraries/stdlib/src/kotlin/util/Standard.kt
@@ -28,52 +28,52 @@ public inline fun TODO(reason: String): Nothing = throw NotImplementedError("An 
  * Calls the specified function [block] and returns its result.
  */
 @kotlin.internal.InlineOnly
-public inline fun <R> run(block: () -> R): R = block()
+public inline fun <R> run(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) block: () -> R): R = block()
 
 /**
  * Calls the specified function [block] with `this` value as its receiver and returns its result.
  */
 @kotlin.internal.InlineOnly
-public inline fun <T, R> T.run(block: T.() -> R): R = block()
+public inline fun <T, R> T.run(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) block: T.() -> R): R = block()
 
 /**
  * Calls the specified function [block] with the given [receiver] as its receiver and returns its result.
  */
 @kotlin.internal.InlineOnly
-public inline fun <T, R> with(receiver: T, block: T.() -> R): R = receiver.block()
+public inline fun <T, R> with(receiver: T, @kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) block: T.() -> R): R = receiver.block()
 
 /**
  * Calls the specified function [block] with `this` value as its receiver and returns `this` value.
  */
 @kotlin.internal.InlineOnly
-public inline fun <T> T.apply(block: T.() -> Unit): T { block(); return this }
+public inline fun <T> T.apply(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) block: T.() -> Unit): T { block(); return this }
 
 /**
  * Calls the specified function [block] with `this` value as its argument and returns `this` value.
  */
 @kotlin.internal.InlineOnly
 @SinceKotlin("1.1")
-public inline fun <T> T.also(block: (T) -> Unit): T { block(this); return this }
+public inline fun <T> T.also(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) block: (T) -> Unit): T { block(this); return this }
 
 /**
  * Calls the specified function [block] with `this` value as its argument and returns its result.
  */
 @kotlin.internal.InlineOnly
-public inline fun <T, R> T.let(block: (T) -> R): R = block(this)
+public inline fun <T, R> T.let(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) block: (T) -> R): R = block(this)
 
 /**
  * Returns `this` value if it satisfies the given [predicate] or `null`, if it doesn't.
  */
 @kotlin.internal.InlineOnly
 @SinceKotlin("1.1")
-public inline fun <T> T.takeIf(predicate: (T) -> Boolean): T? = if (predicate(this)) this else null
+public inline fun <T> T.takeIf(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) predicate: (T) -> Boolean): T? = if (predicate(this)) this else null
 
 /**
  * Returns `this` value if it _does not_ satisfy the given [predicate] or `null`, if it does.
  */
 @kotlin.internal.InlineOnly
 @SinceKotlin("1.1")
-public inline fun <T> T.takeUnless(predicate: (T) -> Boolean): T? = if (!predicate(this)) this else null
+public inline fun <T> T.takeUnless(@kotlin.internal.CalledInPlace(kotlin.internal.InvocationCount.EXACTLY_ONCE) predicate: (T) -> Boolean): T? = if (!predicate(this)) this else null
 
 /**
  * Executes the given function [action] specified number of [times].
@@ -81,7 +81,7 @@ public inline fun <T> T.takeUnless(predicate: (T) -> Boolean): T? = if (!predica
  * A zero-based index of current iteration is passed as a parameter to [action].
  */
 @kotlin.internal.InlineOnly
-public inline fun repeat(times: Int, action: (Int) -> Unit) {
+public inline fun repeat(times: Int, @kotlin.internal.CalledInPlace action: (Int) -> Unit) {
     for (index in 0..times - 1) {
         action(index)
     }
