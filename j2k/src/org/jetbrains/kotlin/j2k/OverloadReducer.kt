88bc74551b4957ea6781c7920359f726bf1b3a67
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-08-10 20:14:51 +0300

    Fix of EA-91690 (NPE in OverloadReducer via flexible types)

--- j2k/src/org/jetbrains/kotlin/j2k/OverloadReducer.kt
+++ j2k/src/org/jetbrains/kotlin/j2k/OverloadReducer.kt
@@ -181,8 +181,8 @@ fun Converter.convertParameterList(
 ): ParameterList {
     val parameterList = method.parameterList
     val params = parameterList.parameters
-    val lParen = parameterList.node.findChildByType(JavaTokenType.LPARENTH)?.psi
-    val rParen = parameterList.node.findChildByType(JavaTokenType.RPARENTH)?.psi
+    val lParen = parameterList.node?.findChildByType(JavaTokenType.LPARENTH)?.psi
+    val rParen = parameterList.node?.findChildByType(JavaTokenType.RPARENTH)?.psi
     return ParameterList(
             params.indices.map { i ->
                 val parameter = params[i]
