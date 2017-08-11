edc1c49506ca8492b77fbcf8f1c37ee3825156cb
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-08-11 12:58:56 +0300

    Revert "Ultimate tests repair (KT-18847)"
    This reverts commit 384ae8d8b50a9ad1dcedcc18e43fdde6b923fc11.

--- ultimate/tests/org/jetbrains/kotlin/idea/spring/tests/generate/AbstractGenerateSpringDependencyActionTest.kt
+++ ultimate/tests/org/jetbrains/kotlin/idea/spring/tests/generate/AbstractGenerateSpringDependencyActionTest.kt
@@ -37,19 +37,13 @@ abstract class AbstractGenerateSpringDependencyActionTest : AbstractCodeInsightA
     }
 
     override fun configureExtra(mainFilePath: String, mainFileText: String) {
-        val packagePath = mainFileText.lines().let { it.find { it.trim().startsWith("package") } }
-                                  ?.removePrefix("package")
-                                  ?.trim()?.replace(".", "/") ?: ""
-        val mainFilePathInProject = packagePath + "/" + File(mainFilePath).name
-        myFixture.addFileToProject(mainFilePathInProject, mainFileText)
-
         configFilePath = InTextDirectivesUtils.findStringWithPrefixes(mainFileText, "// CONFIG_FILE: ")?.let {
             "${PathUtil.getParentPath(mainFilePath)}/$it"
-        } ?: mainFilePathInProject
+        } ?: mainFilePath
         val springFileSet = TestFixtureExtension
                 .getFixture<SpringTestFixtureExtension>()!!
                 .configureFileSet(myFixture, listOf(configFilePath!!))
-        if (configFilePath != mainFilePathInProject && PathUtil.getFileName(configFilePath!!) != "spring-config.xml") {
+        if (configFilePath != mainFilePath && PathUtil.getFileName(configFilePath!!) != "spring-config.xml") {
             configFile = springFileSet.files.single().file!!
         }
 
