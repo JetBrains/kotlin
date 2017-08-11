248200352983c84fdfe8752bf72807274342e263
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-08-11 12:58:52 +0300

    Revert "JPS: completely skip chunk containing only dummy targets"
    This reverts commit 99439620d4d48a90c2c71c42561e7ae17e13103c.

--- jps-plugin/src/org/jetbrains/kotlin/jps/build/KotlinBuilder.kt
+++ jps-plugin/src/org/jetbrains/kotlin/jps/build/KotlinBuilder.kt
@@ -114,8 +114,6 @@ class KotlinBuilder : ModuleLevelBuilder(BuilderCategory.SOURCE_PROCESSOR) {
     override fun chunkBuildStarted(context: CompileContext, chunk: ModuleChunk) {
         super.chunkBuildStarted(context, chunk)
 
-        if (chunk.isDummy(context)) return
-
         context.testingContext?.buildLogger?.buildStarted(context, chunk)
 
         if (JavaBuilderUtil.isForcedRecompilationAllJavaModules(context)) return
@@ -186,10 +184,8 @@ class KotlinBuilder : ModuleLevelBuilder(BuilderCategory.SOURCE_PROCESSOR) {
         applyActionsOnCacheVersionChange(actions, cacheVersionsProvider, context, dataManager, targets, fsOperations)
     }
 
-    override fun chunkBuildFinished(context: CompileContext, chunk: ModuleChunk) {
+    override fun chunkBuildFinished(context: CompileContext?, chunk: ModuleChunk?) {
         super.chunkBuildFinished(context, chunk)
-
-        if (chunk.isDummy(context)) return
 
         LOG.debug("------------------------------------------")
     }
@@ -200,8 +196,6 @@ class KotlinBuilder : ModuleLevelBuilder(BuilderCategory.SOURCE_PROCESSOR) {
             dirtyFilesHolder: DirtyFilesHolder<JavaSourceRootDescriptor, ModuleBuildTarget>,
             outputConsumer: ModuleLevelBuilder.OutputConsumer
     ): ModuleLevelBuilder.ExitCode {
-        if (chunk.isDummy(context)) return NOTHING_DONE
-
         val messageCollector = MessageCollectorAdapter(context)
         val fsOperations = FSOperationsHelper(context, chunk, LOG)
 
