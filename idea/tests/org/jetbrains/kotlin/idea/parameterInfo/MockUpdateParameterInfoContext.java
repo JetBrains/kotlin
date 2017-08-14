ea4d812912c53017858fbe2ac79388e32b58b423
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-08-14 15:37:26 +0300

    Fix compilation

--- idea/tests/org/jetbrains/kotlin/idea/parameterInfo/MockUpdateParameterInfoContext.java
+++ idea/tests/org/jetbrains/kotlin/idea/parameterInfo/MockUpdateParameterInfoContext.java
@@ -87,6 +87,21 @@ public class MockUpdateParameterInfoContext implements UpdateParameterInfoContex
     }
 
     @Override
+    public boolean isPreservedOnHintHidden() {
+        return false;
+    }
+
+    @Override
+    public void setPreservedOnHintHidden(boolean value) {
+
+    }
+
+    @Override
+    public boolean isInnermostContext() {
+        return false;
+    }
+
+    @Override
     public Project getProject() {
         return null;
     }
