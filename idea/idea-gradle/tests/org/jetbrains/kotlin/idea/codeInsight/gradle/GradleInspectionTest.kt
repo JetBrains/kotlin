5dcfbb36ecbe0307be6bb4e7288cf5d65ecee408
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-08-14 15:37:26 +0300

    Fix compilation

--- idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleInspectionTest.kt
+++ idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleInspectionTest.kt
@@ -197,7 +197,6 @@ class GradleInspectionTest : GradleImportingTestCase() {
 
             val foundProblems = presentation.problemElements
                     .values
-                    .flatMap { it.toList() }
                     .mapNotNull { it as? ProblemDescriptorBase }
                     .map { it.descriptionTemplate }
 
