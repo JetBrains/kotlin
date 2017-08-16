99a402ee3032462822e32b1c41b6d1228eea99a4
Dmitry Jemerov <yole@jetbrains.com>
2017-08-16 17:57:38 +0200

    Correctly show quick doc for lookup elements that only have descriptors
      #KT-19716 Fixed

---
+++ idea/testData/kdoc/inCompletion/prop.kt
@@ -0,0 +1,1 @@
+val s = "foo".le<caret>
--- idea/tests/org/jetbrains/kotlin/idea/editor/quickDoc/QuickDocInCompletionTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/editor/quickDoc/QuickDocInCompletionTest.kt
@@ -16,23 +16,37 @@
 
 package org.jetbrains.kotlin.idea.editor.quickDoc
 
-import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
-import org.jetbrains.kotlin.idea.KotlinFileType
+import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.idea.KotlinQuickDocumentationProvider
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.PluginTestCaseBase
 import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtProperty
 import org.junit.Assert
 
-class QuickDocInCompletionTest(): LightPlatformCodeInsightFixtureTestCase() {
+class QuickDocInCompletionTest: KotlinLightCodeInsightFixtureTestCase() {
     override fun getTestDataPath(): String {
         return PluginTestCaseBase.getTestDataPathBase() + "/kdoc/inCompletion/"
     }
+
+    override fun getProjectDescriptor() = KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
+
     fun testSimple() {
+        val element = getElementFromLookup()
+        Assert.assertTrue(element is KtClass)
+    }
+
+    fun testProp() {
+        val element = getElementFromLookup()
+        Assert.assertTrue(element is KtProperty)
+    }
+
+    private fun getElementFromLookup(): PsiElement? {
         myFixture.configureByFile(getTestName(true) + ".kt")
         val lookupElements = myFixture.completeBasic()
         val lookupObject = lookupElements.first().`object`
-        val element = KotlinQuickDocumentationProvider().getDocumentationElementForLookupItem(
+        return KotlinQuickDocumentationProvider().getDocumentationElementForLookupItem(
                 myFixture.psiManager, lookupObject, null)
-        Assert.assertTrue(element is KtClass)
     }
 }
