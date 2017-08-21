b451bc8b09a37b5dc35b48faa4e8e1542aa7d7e5
Toshiaki Kameyama <kmymtsak@gmail.com>
2017-08-21 11:57:36 +0900

    @deprecated with multi-line message in Javadoc converted to @Deprecated("long ugly string") #KT-19644 Fixed

--- j2k/tests/org/jetbrains/kotlin/j2k/JavaToKotlinConverterSingleFileTestGenerated.java
+++ j2k/tests/org/jetbrains/kotlin/j2k/JavaToKotlinConverterSingleFileTestGenerated.java
@@ -1790,6 +1790,12 @@ public class JavaToKotlinConverterSingleFileTestGenerated extends AbstractJavaTo
             doTest(fileName);
         }
 
+        @TestMetadata("deprecatedDocTagWithMultiLineMessage.java")
+        public void testDeprecatedDocTagWithMultiLineMessage() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("j2k/testData/fileOrElement/docComments/deprecatedDocTagWithMultiLineMessage.java");
+            doTest(fileName);
+        }
+
         @TestMetadata("docCommentWithParamTag.java")
         public void testDocCommentWithParamTag() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("j2k/testData/fileOrElement/docComments/docCommentWithParamTag.java");
