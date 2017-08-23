46a1c8ca12a80cf1417edca99fc974f15f662c8b
Toshiaki Kameyama <kmymtsak@gmail.com>
2017-08-24 00:05:04 +0900

    Repeated annotation @Deprecated after J2K for method with annotation and javadoc #KT-19351 Fixed (#1251)

---
+++ j2k/testData/fileOrElement/docComments/deprecatedDocTagWithAnnotation.java
@@ -0,0 +1,12 @@
+/**
+ * This is a deprecated class.
+ * @deprecated This class is deprecated
+ */
+@Deprecated
+class TestDeprecated {
+    /**
+     * @deprecated This method is deprecated
+     */
+    @Deprecated
+    public void deprecatedMethod() {}
+}
