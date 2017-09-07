0e96af2f1b0283d7caedc230ce2049be10be5568
Alexey Andreev <alexey.andreev@jetbrains.com>
2017-09-07 11:10:33 +0300

    Fix tests for inline cycle diagnostics in JS
    (cherry picked from commit c65a79b)

--- compiler/testData/diagnostics/testsWithJsStdLibAndBackendCompilation/inline/recursionCycleLambda.kt
+++ compiler/testData/diagnostics/testsWithJsStdLibAndBackendCompilation/inline/recursionCycleLambda.kt
@@ -2,6 +2,6 @@
 
 inline fun f(): Unit = <!INLINE_CALL_CYCLE!>g()<!>
 
-inline fun g(): Unit = <!INLINE_CALL_CYCLE!>h { <!INLINE_CALL_CYCLE!>f()<!> }<!>
+inline fun g(): Unit = h { <!INLINE_CALL_CYCLE!>f()<!> }
 
-inline fun h(fn: ()->Unit): Unit = <!INLINE_CALL_CYCLE!>fn()<!>
+inline fun h(fn: ()->Unit): Unit = fn()
