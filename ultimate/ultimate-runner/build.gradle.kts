bcd31de84a49e77a9e0180c0b72d8888c087793f
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-09-07 17:39:30 +0300

    Implement ultimate-runner

---
+++ ultimate/ultimate-runner/build.gradle.kts
@@ -0,0 +1,44 @@
+apply { plugin("kotlin") }
+
+dependencies {
+
+    compile(ideaUltimateSdkDeps("*.jar"))
+    compileOnly(project(":idea"))
+    compileOnly(project(":idea:idea-maven"))
+    compileOnly(project(":idea:idea-gradle"))
+    compileOnly(project(":idea:idea-jvm"))
+
+    runtimeOnly(files(toolsJar()))
+}
+
+
+val runUltimate by task<JavaExec> {
+    dependsOn(":dist", ":prepare:kotlin-plugin:idea-plugin", ":dist-plugin", ":ultimate:idea-ultimate-plugin")
+
+    classpath = the<JavaPluginConvention>().sourceSets["main"].runtimeClasspath
+
+    main = "com.intellij.idea.Main"
+
+    workingDir = File(projectDir.parentFile, "ideaSDK", "bin")
+
+    val ideaUltimatePluginDir: File by rootProject.extra
+
+    jvmArgs(
+            "-Xmx1250m",
+            "-XX:ReservedCodeCacheSize=240m",
+            "-XX:+HeapDumpOnOutOfMemoryError",
+            "-ea",
+            "-Didea.is.internal=true",
+            "-Didea.debug.mode=true",
+            "-Didea.system.path=../system-idea",
+            "-Didea.config.path=../config-idea",
+            "-Dapple.laf.useScreenMenuBar=true",
+            "-Dapple.awt.graphics.UseQuartz=true",
+            "-Dsun.io.useCanonCaches=false",
+            "-Dplugin.path=${ideaUltimatePluginDir.absolutePath}",
+            "-Dkotlin.internal.mode.enabled=true",
+            "-Didea.additional.classpath=../idea-kotlin-runtime/kotlin-runtime.jar,../idea-kotlin-runtime/kotlin-reflect.jar"
+    )
+
+    args()
+}
