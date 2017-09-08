ae509d598093334c8bbcaeea292ffcc7972e61a6
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-09-08 19:03:41 +0300

    Unused symbol: add test to fix behaviour #KT-16318 Obsolete

--- idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
@@ -1850,6 +1850,12 @@ public class LocalInspectionTestGenerated extends AbstractLocalInspectionTest {
             KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("idea/testData/inspectionsLocal/unusedSymbol"), Pattern.compile("^([\\w\\-_]+)\\.kt$"), TargetBackend.ANY, true);
         }
 
+        @TestMetadata("asDefaultConstructorParameter.kt")
+        public void testAsDefaultConstructorParameter() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("idea/testData/inspectionsLocal/unusedSymbol/asDefaultConstructorParameter.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("companionViaImport.kt")
         public void testCompanionViaImport() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("idea/testData/inspectionsLocal/unusedSymbol/companionViaImport.kt");
