af49993143a9f146a534e6e8d070b0a8e6a46689
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2017-09-11 15:30:37 +0300

    Kotlin Facet: Configure facet based on Android build variant (in AS)
     #KT-19958 Fixed

--- idea/src/org/jetbrains/kotlin/idea/facet/facetUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/facet/facetUtils.kt
@@ -296,7 +296,7 @@ fun parseCompilerArgumentsToFacet(
         arguments: List<String>,
         defaultArguments: List<String>,
         kotlinFacet: KotlinFacet,
-        modelsProvider: IdeModifiableModelsProvider
+        modelsProvider: IdeModifiableModelsProvider?
 ) {
     with(kotlinFacet.configuration.settings) {
         val compilerArguments = this.compilerArguments ?: return
