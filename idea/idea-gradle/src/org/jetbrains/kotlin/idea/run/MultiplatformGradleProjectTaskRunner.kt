a420f20acca159cffba6a85c1aaecd34c16c7ff9
Dmitry Jemerov <yole@jetbrains.com>
2017-09-12 12:08:35 +0300

    Fix compilation of MultiplatformGradleProjectTaskRunner in 171
    (cherry picked from commit f8c72a8)

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/run/MultiplatformGradleProjectTaskRunner.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/run/MultiplatformGradleProjectTaskRunner.kt
@@ -18,9 +18,10 @@ package org.jetbrains.kotlin.idea.run
 
 import com.intellij.execution.configurations.JavaRunConfigurationModule
 import com.intellij.execution.configurations.ModuleBasedConfiguration
-import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.model.project.ExternalSystemSourceType
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.externalSystem.util.ExternalSystemConstants
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleOrderEntry
@@ -95,10 +96,10 @@ class MultiplatformGradleProjectTaskRunner : GradleProjectTaskRunner() {
                 is ModuleFilesBuildTask -> this
 
                 is ModuleBuildTask ->
-                        if (module == origin)
-                            ModuleBuildTaskImpl(replacement, isIncrementalBuild, isIncludeDependentModules, isIncludeRuntimeDependencies)
-                        else
-                            this
+                    if (module == origin)
+                        ModuleBuildTaskImpl(replacement, isIncrementalBuild, isIncludeDependentModules, isIncludeRuntimeDependencies)
+                    else
+                        this
 
                 else -> this
             }
@@ -113,7 +114,7 @@ class MultiplatformGradleOrderEnumeratorHandler(val factory: MultiplatformGradle
             if (!ExternalSystemApiUtil.isExternalSystemAwareModule(GradleConstants.SYSTEM_ID, rootModel.module)) return false
 
             if (!GradleSystemRunningSettings.getInstance().isUseGradleAwareMake) {
-                val gradleProjectPath = ExternalSystemModulePropertyManager.getInstance(rootModel.module).getRootProjectPath() ?: return false
+                val gradleProjectPath = rootModel.module.getOptionValue(ExternalSystemConstants.ROOT_PROJECT_PATH_KEY);
                 val externalProjectDataCache = ExternalProjectDataCache.getInstance(rootModel.module.project)!!
                 val externalRootProject = externalProjectDataCache.getRootExternalProject(GradleConstants.SYSTEM_ID,
                                                                                           File(gradleProjectPath)) ?: return false
@@ -146,7 +147,11 @@ class MultiplatformGradleOrderEnumeratorHandler(val factory: MultiplatformGradle
     }
 
     private fun addOutputModuleRoots(directorySet: ExternalSourceDirectorySet?, result: MutableCollection<String>) {
-        directorySet?.gradleOutputDirs?.mapTo(result) { VfsUtilCore.pathToUrl(it.absolutePath) }
+        if (directorySet == null) return;
+
+        if (directorySet.isCompilerOutputPathInherited) return
+        val path = directorySet.outputDir.absolutePath
+        result.add(VfsUtilCore.pathToUrl(path))
     }
 
     class FactoryImpl : Factory() {
