ech0s7r/kotlin-native

HTML5 canvas drawing demo.

samples/html5Canvas/src/canvas/kotlin/canvas.kt
@@ -1,0 +1,80 @@
+package html5.minimal
+
+// This file will be auto-generated.
+
+import kotlinx.wasm.jsinterop.*
+
+@SymbolName("knjs_addDocumentToArena")
+external public fun addDocumentToArena(arena: Arena): Int
+
+@SymbolName("knjs_getElementById")
+external public fun knjs_getElementById(arena: Int, obj: Int, idPtr: Int, idLen: Int): Int;
+
+@SymbolName("knjs_getContext")
+external public fun knjs_getContext(arena: Int, obj: Int, idPtr: Int, idLen: Int): Int;
+
+@SymbolName("knjs_getBoundingClientRect")
+external public fun knjs_getBoundingClientRect(arena: Int, obj: Int): Int;
+
+@SymbolName("knjs_stroke")
+external public fun knjs_stroke(arena: Int, obj: Int);
+
+@SymbolName("knjs_moveTo")
+external public fun knjs_moveTo(arena: Int, obj: Int, x: Int, y: Int): Int;
+
+@SymbolName("knjs_lineTo")
+external public fun knjs_lineTo(arena: Int, obj: Int, x: Int, y: Int): Int;
+
+@SymbolName("knjs_setInterval")
+external public fun knjs_setInterval(functionIndex: Int, interval: Int);
+
+open class Document(arena: Int, index: Int): JsValue(arena, index) {
+    fun getElementById(id: String): JsValue {
+        val resultIndex = knjs_getElementById(this.arena, this.index, stringPointer(id), stringLength(id))
+        return JsValue(this.arena, resultIndex)
+    }
+}
+
+open class DOMRect(arena: Int, index: Int): JsValue(arena, index) {
+}
+
+open class Canvas(arena: Int, index: Int): JsValue(arena, index) {
+    fun getContext(context: String): Context {
+        val resultIndex = knjs_getContext(this.arena, this.index, stringPointer(context), stringLength(context))
+        return Context(this.arena, resultIndex)
+    }
+    fun getBoundingClientRect(): DOMRect {
+        return DOMRect(this.arena, knjs_getBoundingClientRect(this.arena, this.index))
+    }
+}
+
+// TODO: this is awful.
+val JsValue.asCanvas: Canvas
+    get() {
+        return Canvas(this.arena, this.index)
+    }
+
+open class Context(arena: Int, index: Int): JsValue(arena, index) {
+    fun lineTo(x: Int, y: Int) {
+        knjs_lineTo(this.arena, this.index, x, y)
+    }
+    fun moveTo(x: Int, y: Int) {
+        knjs_moveTo(this.arena, this.index, x, y)
+    }
+    fun stroke() {
+        knjs_stroke(this.arena, this.index);
+    }
+}
+fun setInterval(lambda: (ArrayList<JsValue>)->Unit, interval: Int) {
+    knjs_setInterval(wrapFunction(lambda), interval);
+}
+
+// TODO: The arguments are flipped to have lambda within the last position.
+fun setInterval(interval: Int, lambda: (ArrayList<JsValue>)->Unit) 
+    = setInterval(lambda, interval)
+
+class html5 {
+    val arena = allocateArena()
+    val document = Document(arena, addDocumentToArena(arena))
+}
+

samples/html5Canvas/src/jsinterop/kotlin/jsinterop.kt
@@ -1,0 +1,62 @@
+package kotlinx.wasm.jsinterop
+
+import konan.internal.ExportForCppRuntime
+import kotlinx.cinterop.*
+
+typealias Arena = Int
+typealias Object = Int
+typealias Pointer = Int
+
+@SymbolName("Konan_js_allocateArena")
+external public fun allocateArena(): Arena
+
+@SymbolName("Konan_js_freeArena")
+external public fun freeArena(arena: Arena)
+
+@SymbolName("Kotlin_String_utf16pointer")
+external public fun stringPointer(message: String): Pointer
+
+@SymbolName("Kotlin_String_utf16length")
+external public fun stringLength(message: String): Int
+
+typealias KtFunction = (ArrayList<JsValue>)->Unit
+
+fun wrapFunction(func: KtFunction): Int {
+    val ptr: Long = StableObjPtr.create(func).value.toLong() 
+    return ptr.toInt() // TODO: LP64 unsafe.
+}
+
+@ExportForCppRuntime("Konan_js_runLambda")
+fun runLambda(userData: Int, arena: Arena, arenaSize: Int) {
+    val arguments = arrayListOf<JsValue>()
+    for (i in 0..arenaSize-1) {
+        arguments.add(JsValue(arena, i));
+    }
+    println("Want to run lambda $userData ()");
+    // TODO: LP64 unsafe: wasm32 passes Int, not Long.
+    val func = StableObjPtr.fromValue(userData.toLong().toCPointer()!!).get() as KtFunction 
+    func(arguments);
+
+    
+}
+
+open class JsValue(val arena: Arena, val index: Object) {
+    fun getInt(property: String): Int {
+        return getInt(arena, index, stringPointer(property), stringLength(property))
+    }
+}
+
+@SymbolName("Konan_js_getInt")
+external public fun getInt(arena: Arena, obj: Object, propertyPtr: Pointer, propertyLen: Int): Int;
+
+@SymbolName("Konan_js_setFunction")
+external public fun setFunction(arena: Arena, obj: Object, propertyName: Pointer, propertyLength: Int , function: Int)
+
+fun setter(obj: JsValue, property: String, lambda: (arguments: ArrayList<JsValue>) -> Unit) {
+    val index = wrapFunction(lambda);
+    setFunction(obj.arena, obj.index, stringPointer(property), stringLength(property), index)
+}
+
+fun JsValue.setter(property: String, lambda: (ArrayList<JsValue>) -> Unit) {
+    setter(this, property, lambda)
+}

JetBrains/kotlin

samples/html5Canvas/src/canvas/kotlin/canvas.kt
@@ -1,0 +1,80 @@
+package html5.minimal
+
+// This file will be auto-generated.
+
+import kotlinx.wasm.jsinterop.*
+
+@SymbolName("knjs_addDocumentToArena")
+external public fun addDocumentToArena(arena: Arena): Int
+
+@SymbolName("knjs_getElementById")
+external public fun knjs_getElementById(arena: Int, obj: Int, idPtr: Int, idLen: Int): Int;
+
+@SymbolName("knjs_getContext")
+external public fun knjs_getContext(arena: Int, obj: Int, idPtr: Int, idLen: Int): Int;
+
+@SymbolName("knjs_getBoundingClientRect")
+external public fun knjs_getBoundingClientRect(arena: Int, obj: Int): Int;
+
+@SymbolName("knjs_stroke")
+external public fun knjs_stroke(arena: Int, obj: Int);
+
+@SymbolName("knjs_moveTo")
+external public fun knjs_moveTo(arena: Int, obj: Int, x: Int, y: Int): Int;
+
+@SymbolName("knjs_lineTo")
+external public fun knjs_lineTo(arena: Int, obj: Int, x: Int, y: Int): Int;
+
+@SymbolName("knjs_setInterval")
+external public fun knjs_setInterval(functionIndex: Int, interval: Int);
+
+open class Document(arena: Int, index: Int): JsValue(arena, index) {
+    fun getElementById(id: String): JsValue {
+        val resultIndex = knjs_getElementById(this.arena, this.index, stringPointer(id), stringLengthBytes(id))
+        return JsValue(this.arena, resultIndex)
+    }
+}
+
+open class DOMRect(arena: Int, index: Int): JsValue(arena, index) {
+}
+
+open class Canvas(arena: Int, index: Int): JsValue(arena, index) {
+    fun getContext(context: String): Context {
+        val resultIndex = knjs_getContext(this.arena, this.index, stringPointer(context), stringLengthBytes(context))
+        return Context(this.arena, resultIndex)
+    }
+    fun getBoundingClientRect(): DOMRect {
+        return DOMRect(this.arena, knjs_getBoundingClientRect(this.arena, this.index))
+    }
+}
+
+// TODO: this is awful.
+val JsValue.asCanvas: Canvas
+    get() {
+        return Canvas(this.arena, this.index)
+    }
+
+open class Context(arena: Int, index: Int): JsValue(arena, index) {
+    fun lineTo(x: Int, y: Int) {
+        knjs_lineTo(this.arena, this.index, x, y)
+    }
+    fun moveTo(x: Int, y: Int) {
+        knjs_moveTo(this.arena, this.index, x, y)
+    }
+    fun stroke() {
+        knjs_stroke(this.arena, this.index);
+    }
+}
+fun setInterval(lambda: (ArrayList<JsValue>)->Unit, interval: Int) {
+    knjs_setInterval(wrapFunction(lambda), interval);
+}
+
+// TODO: The arguments are flipped to have lambda within the last position.
+fun setInterval(interval: Int, lambda: (ArrayList<JsValue>)->Unit) 
+    = setInterval(lambda, interval)
+
+class html5 {
+    val arena = allocateArena()
+    val document = Document(arena, addDocumentToArena(arena))
+}
+

samples/html5Canvas/src/jsinterop/kotlin/jsinterop.kt
@@ -1,0 +1,61 @@
+package kotlinx.wasm.jsinterop
+
+import konan.internal.ExportForCppRuntime
+import kotlinx.cinterop.*
+
+typealias Arena = Int
+typealias Object = Int
+typealias Pointer = Int
+
+@SymbolName("Konan_js_allocateArena")
+external public fun allocateArena(): Arena
+
+@SymbolName("Konan_js_freeArena")
+external public fun freeArena(arena: Arena)
+
+@SymbolName("Kotlin_String_utf16pointer")
+external public fun stringPointer(message: String): Pointer
+
+@SymbolName("Kotlin_String_utf16length")
+external public fun stringLengthBytes(message: String): Int
+
+typealias KtFunction = (ArrayList<JsValue>)->Unit
+
+fun wrapFunction(func: KtFunction): Int {
+    val ptr: Long = StableObjPtr.create(func).value.toLong() 
+    return ptr.toInt() // TODO: LP64 unsafe.
+}
+
+@ExportForCppRuntime("Konan_js_runLambda")
+fun runLambda(userData: Int, arena: Arena, arenaSize: Int) {
+    val arguments = arrayListOf<JsValue>()
+    for (i in 0..arenaSize-1) {
+        arguments.add(JsValue(arena, i));
+    }
+    // TODO: LP64 unsafe: wasm32 passes Int, not Long.
+    val func = StableObjPtr.fromValue(userData.toLong().toCPointer()!!).get() as KtFunction 
+    func(arguments);
+
+    
+}
+
+open class JsValue(val arena: Arena, val index: Object) {
+    fun getInt(property: String): Int {
+        return getInt(arena, index, stringPointer(property), stringLengthBytes(property))
+    }
+}
+
+@SymbolName("Konan_js_getInt")
+external public fun getInt(arena: Arena, obj: Object, propertyPtr: Pointer, propertyLen: Int): Int;
+
+@SymbolName("Konan_js_setFunction")
+external public fun setFunction(arena: Arena, obj: Object, propertyName: Pointer, propertyLength: Int , function: Int)
+
+fun setter(obj: JsValue, property: String, lambda: (arguments: ArrayList<JsValue>) -> Unit) {
+    val index = wrapFunction(lambda);
+    setFunction(obj.arena, obj.index, stringPointer(property), stringLengthBytes(property), index)
+}
+
+fun JsValue.setter(property: String, lambda: (ArrayList<JsValue>) -> Unit) {
+    setter(this, property, lambda)
+}