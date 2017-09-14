ee482995bee954bf70700b663b9b92fe9b12d18f
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-09-14 15:38:41 +0300

    Fix compilation with gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,7 @@ extra["versions.ant"] = "1.8.2"
 extra["versions.android"] = "2.3.1"
 
 extra["ideaCoreSdkJars"] = arrayOf("annotations", "asm-all", "guava", "intellij-core", "jdom", "jna", "log4j", "picocontainer",
-                                   "snappy-in-java", "streamex", "trove4j", "xpp3-1.1.4-min", "xstream")
+                                   "snappy-in-java", "trove4j", "xpp3-1.1.4-min", "xstream")
 
 extra["compilerModules"] = arrayOf(":compiler:util",
                                    ":compiler:container",
