956b0aca923c3b9b67efdc7421fd7062fba133f6
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2017-09-15 12:42:56 +0300

    Rename header/impl in DescriptorRendererModifier

--- core/descriptors/src/org/jetbrains/kotlin/renderer/DescriptorRendererImpl.kt
+++ core/descriptors/src/org/jetbrains/kotlin/renderer/DescriptorRendererImpl.kt
@@ -509,8 +509,8 @@ internal class DescriptorRendererImpl(
 
     private fun renderMemberModifiers(descriptor: MemberDescriptor, builder: StringBuilder) {
         renderModifier(builder, descriptor.isExternal, "external")
-        renderModifier(builder, DescriptorRendererModifier.HEADER in modifiers && descriptor.isExpect, "expect")
-        renderModifier(builder, DescriptorRendererModifier.IMPL in modifiers && descriptor.isActual, "actual")
+        renderModifier(builder, DescriptorRendererModifier.EXPECT in modifiers && descriptor.isExpect, "expect")
+        renderModifier(builder, DescriptorRendererModifier.ACTUAL in modifiers && descriptor.isActual, "actual")
     }
 
     private fun renderAdditionalModifiers(functionDescriptor: FunctionDescriptor, builder: StringBuilder) {
