42334faea72468c72faa490e2c54ce84fdb4db8d
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-09-15 16:05:06 +0300

    Force canceling injection search when it's executed without progress (KT-19901)
    More accurate fix for KT-19901.
    There're cases when injectors are counted without progress indicator but
    under read action. In that cases typing can't be started until exit from
    the computing that produces lags in typing.
    
    Tested with long spek-like test file.
    
     #KT-19901 Fixed

--- idea/src/org/jetbrains/kotlin/idea/ProgressUtil.kt
+++ idea/src/org/jetbrains/kotlin/idea/ProgressUtil.kt
@@ -39,6 +39,18 @@ fun <T : Any> runInReadActionWithWriteActionPriorityWithPCE(f: () -> T): T {
     return r!!
 }
 
+fun <T : Any> runInReadActionWithWriteActionPriority(f: () -> T): T? {
+    var r: T? = null
+    val complete = ProgressIndicatorUtils.runInReadActionWithWriteActionPriority {
+        r = f()
+    }
+
+    if (!complete) return null
+    return r
+}
+
+
+
 fun <T : Any> Project.runSynchronouslyWithProgress(progressTitle: String, canBeCanceled: Boolean, action: () -> T): T? {
     var result: T? = null
     ProgressManager.getInstance().runProcessWithProgressSynchronously({ result = action() }, progressTitle, canBeCanceled, this)
