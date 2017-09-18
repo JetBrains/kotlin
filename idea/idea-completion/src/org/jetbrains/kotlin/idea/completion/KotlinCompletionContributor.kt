6b384ac53cd99ae7d47e6df04cd1e62f88e5b923
Toshiaki Kameyama <kmymtsak@gmail.com>
2017-09-18 15:26:41 +0900

    AssertionError on completing expression after template in string literal #KT-16402 Fixed

--- idea/idea-completion/src/org/jetbrains/kotlin/idea/completion/KotlinCompletionContributor.kt
+++ idea/idea-completion/src/org/jetbrains/kotlin/idea/completion/KotlinCompletionContributor.kt
@@ -331,8 +331,7 @@ class KotlinCompletionContributor : CompletionContributor() {
                 val psiDocumentManager = PsiDocumentManager.getInstance(context.project)
                 psiDocumentManager.commitAllDocuments()
 
-                assert(startOffset > 1 && document.charsSequence[startOffset - 1] == '.')
-                val token = context.file.findElementAt(startOffset - 2)!!
+                val token = getToken(context.file, document, startOffset)
                 assert(token.node.elementType == KtTokens.IDENTIFIER || token.node.elementType == KtTokens.THIS_KEYWORD)
                 val nameRef = token.parent as KtNameReferenceExpression
 
@@ -343,6 +342,15 @@ class KotlinCompletionContributor : CompletionContributor() {
                 context.tailOffset = tailOffset
 
                 super.handleInsert(context)
+            }
+
+            private fun getToken(file: PsiFile, document: Document, startOffset: Int): PsiElement {
+                assert(startOffset > 1 && document.charsSequence[startOffset - 1] == '.')
+                val token = file.findElementAt(startOffset - 2)!!
+                return if (token.node.elementType == KtTokens.IDENTIFIER || token.node.elementType == KtTokens.THIS_KEYWORD)
+                    token
+                else
+                    getToken(file, document, token.startOffset + 1)
             }
         }
     }
