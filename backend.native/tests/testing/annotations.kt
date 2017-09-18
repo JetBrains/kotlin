2d757c41502ce5b194b784ec04226f99441f13f4
Ilya Matveev <ilya.matveev@jetbrains.com>
2017-09-18 16:20:58 +0300

    unit-tests: Add assertion and annotation tests

---
+++ backend.native/tests/testing/annotations.kt
@@ -0,0 +1,64 @@
+import kotlin.test.*
+import konan.test.*
+
+
+@Ignore
+class IgnoredClass {
+    @Ignore object IgnoredObject {
+        @Test fun test() { throw AssertionError("Ignored test") }
+    }
+
+    @Test fun test() { throw AssertionError("Ignored test") }
+}
+
+@Ignore
+object IgnoredObject {
+    @Test fun test() { throw AssertionError("Ignored test") }
+}
+
+class A {
+    companion object {
+        @BeforeEach fun before() { println("before (A.companion)") }
+        @AfterEach fun after() { println("after (A.companion)") }
+        @Test fun test1() { println("test1 (A.companion)") }
+
+        @BeforeClass fun beforeClass() { println("beforeClass (A.companion)") }
+        @AfterClass fun afterClass() { println("afterClass (A.companion)") }
+    }
+
+    @BeforeEach fun before() { println("before (A)") }
+    @AfterEach fun after() { println("after (A)") }
+    @Test fun test1() { println("test1 (A)") }
+    @Ignore @Test fun ignoredTest() { throw AssertionError("Ignored test") }
+
+    @BeforeClass fun beforeClass() { println("beforeClass (A)") }
+    @AfterClass fun afterClass() { println("afterClass (A)") }
+
+    object O {
+        @BeforeEach fun before() { println("before (A.object)") }
+        @AfterEach fun after() { println("after (A.object)") }
+        @Test fun test1() { println("test1 (A.object)") }
+        @Ignore @Test fun ignoredTest() { throw AssertionError("Ignored test") }
+
+        @BeforeClass fun beforeClass() { println("beforeClass (A.object)") }
+        @AfterClass fun afterClass() { println("afterClass (A.object)") }
+    }
+}
+
+object O {
+    @BeforeEach fun before() { println("before (object)") }
+    @AfterEach fun after() { println("after (object)") }
+    @Test fun test1() { println("test1 (object)") }
+    @Ignore @Test fun ignoredTest() { throw AssertionError("Ignored test") }
+
+    @BeforeClass fun beforeClass() { println("beforeClass (object)") }
+    @AfterClass fun afterClass() { println("afterClass (object)") }
+}
+
+@BeforeEach fun before() { println("before (file)") }
+@AfterEach fun after() { println("after (file)") }
+@Test fun test1() { println("test1 (file)") }
+@Ignore @Test fun ignoredTest() { throw AssertionError("Ignored test") }
+
+@BeforeClass fun beforeClass() { println("beforeClass (file)") }
+@AfterClass fun afterClass() { println("afterClass (file)") }
---
+++ backend.native/tests/testing/assertions.kt
@@ -0,0 +1,180 @@
+package kotlin.test.tests
+
+import kotlin.test.*
+
+class BasicAssertionsTest {
+    @Test
+    fun testAssertEquals() {
+        assertEquals(1, 1)
+    }
+
+    @Test
+    fun testAssertEqualsString() {
+        assertEquals("a", "a")
+    }
+
+    @Test
+    fun testAssertFailsWith() {
+        assertFailsWith<IllegalStateException> { throw IllegalStateException() }
+        assertFailsWith<AssertionError> { throw AssertionError() }
+    }
+
+// TODO: Uncomment with the catch serialization bug is fixed.
+//    @Test fun testAssertFailsWithFails() {
+//        assertTrue(true) // at least one assertion required for qunit
+//
+//        withDefaultAsserter run@ {
+//            try {
+//                assertFailsWith<IllegalStateException> { throw IllegalArgumentException() }
+//            }
+//            catch (e: AssertionError) {
+//                return@run
+//            }
+//            throw AssertionError("Expected to fail")
+//        }
+//
+//        withDefaultAsserter run@ {
+//            try {
+//                assertFailsWith<IllegalStateException> {  }
+//            }
+//            catch (e: AssertionError) {
+//                return@run
+//            }
+//            throw AssertionError("Expected to fail")
+//        }
+//    }
+
+    @Test
+    fun testAssertFailsWithClass() {
+        assertFailsWith<IllegalArgumentException> {
+            throw IllegalArgumentException("This is illegal")
+        }
+    }
+
+    @Test
+    fun testAssertFailsWithClassFails() {
+        checkFailedAssertion {
+            assertFailsWith<IllegalArgumentException> { throw IllegalStateException() }
+        }
+
+        checkFailedAssertion {
+            assertFailsWith<Exception> { }
+        }
+    }
+
+    @Test
+    fun testAssertEqualsFails() {
+        checkFailedAssertion { assertEquals(1, 2) }
+    }
+
+    @Test
+    fun testAssertTrue() {
+        assertTrue(true)
+        assertTrue { true }
+    }
+
+    @Test()
+    fun testAssertTrueFails() {
+        checkFailedAssertion { assertTrue(false) }
+        checkFailedAssertion { assertTrue { false } }
+    }
+
+    @Test
+    fun testAssertFalse() {
+        assertFalse(false)
+        assertFalse { false }
+    }
+
+    @Test
+    fun testAssertFalseFails() {
+        checkFailedAssertion { assertFalse(true) }
+        checkFailedAssertion{ assertFalse { true } }
+    }
+
+    @Test
+    fun testAssertFails() {
+        assertFails { throw IllegalStateException() }
+    }
+
+    @Test()
+    fun testAssertFailsFails() {
+        checkFailedAssertion { assertFails {  } }
+    }
+
+
+    @Test
+    fun testAssertNotEquals() {
+        assertNotEquals(1, 2)
+    }
+
+    @Test()
+    fun testAssertNotEqualsFails() {
+        checkFailedAssertion { assertNotEquals(1, 1) }
+    }
+
+    @Test
+    fun testAssertNotNull() {
+        assertNotNull(true)
+    }
+
+    @Test()
+    fun testAssertNotNullFails() {
+        checkFailedAssertion { assertNotNull(null) }
+    }
+
+    @Test
+    fun testAssertNotNullLambda() {
+        assertNotNull("") { assertEquals("", it) }
+    }
+
+    @Test
+    fun testAssertNotNullLambdaFails() {
+        checkFailedAssertion {
+            val value: String? = null
+            assertNotNull(value) {
+                it.substring(0, 0)
+            }
+        }
+    }
+
+    @Test
+    fun testAssertNull() {
+        assertNull(null)
+    }
+
+    @Test
+    fun testAssertNullFails() {
+        checkFailedAssertion { assertNull("") }
+    }
+
+    @Test()
+    fun testFail() {
+        checkFailedAssertion { fail("should fail") }
+    }
+
+    @Test
+    fun testExpect() {
+        expect(1) { 1 }
+    }
+
+    @Test
+    fun testExpectFails() {
+        checkFailedAssertion { expect(1) { 2 } }
+    }
+}
+
+
+private fun checkFailedAssertion(assertion: () -> Unit) {
+    assertFailsWith<AssertionError> { withDefaultAsserter(assertion) }
+}
+
+@Suppress("INVISIBLE_MEMBER")
+private fun withDefaultAsserter(block: () -> Unit) {
+    val current = overrideAsserter(DefaultAsserter)
+    try {
+        block()
+    }
+    finally {
+        overrideAsserter(current)
+    }
+}
