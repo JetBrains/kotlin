1a9fc4fdc814e207b752b0aa27aa914e885263b2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-09-19 17:52:19 +0300

    Fix a pair of multi-platform messages #KT-20327 Fixed

--- idea/testData/quickfix/implement/doNotAddHeader.kt
+++ idea/testData/quickfix/implement/doNotAddHeader.kt
@@ -1,6 +1,6 @@
 // "Implement members" "true"
 // WITH_RUNTIME
-// ERROR: Expected interface 'InterfaceWithFuns' has no actual in module light_idea_test_case for JVM
+// ERROR: Expected interface 'InterfaceWithFuns' has no actual declaration in module light_idea_test_case for JVM
 expect interface InterfaceWithFuns {
     fun funInInterface()
 }
