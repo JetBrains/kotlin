bf739198aa467499bd4187392e005c3b1c0f824b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2017-09-19 14:01:31 +0300

    Fix regular quick-fix test with multi-platforms required

--- idea/testData/quickfix/implement/doNotAddHeader.kt
+++ idea/testData/quickfix/implement/doNotAddHeader.kt
@@ -1,6 +1,9 @@
 // "Implement members" "true"
-// WITH_RUNTIME
+// ENABLE_MULTIPLATFORM
 // ERROR: Expected interface 'InterfaceWithFuns' has no actual in module light_idea_test_case for JVM
+
+fun TODO(s: String): Nothing = null!!
+
 expect interface InterfaceWithFuns {
     fun funInInterface()
 }
