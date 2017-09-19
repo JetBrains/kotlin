0f199835a330b6d60cb2070a0df12dc3ee4c441a
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-09-19 23:41:04 +0200

    Fix idea-gradle tests

--- idea/idea-gradle/build.gradle.kts
+++ idea/idea-gradle/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 
     testRuntime(project(":idea:idea-jvm"))
     testRuntime(project(":idea:idea-android"))
+    testRuntime(project(":plugins:kapt3-idea"))
     testRuntime(project(":plugins:android-extensions-ide"))
     testRuntime(project(":plugins:lint"))
     testRuntime(project(":sam-with-receiver-ide-plugin"))
