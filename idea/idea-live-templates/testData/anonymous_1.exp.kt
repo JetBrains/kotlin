690eaa95bd76e8031b9933d16c221b4df696f418
Dmitry Jemerov <yole@jetbrains.com>
2017-09-21 17:33:55 +0200

    Update expected testdata of live templates test

--- idea/idea-live-templates/testData/anonymous_1.exp.kt
+++ idea/idea-live-templates/testData/anonymous_1.exp.kt
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities
 fun main(args : Array<String>) {
     SwingUtilities.invokeLater(object : Runnable {
         override fun run() {
-            throw UnsupportedOperationException()
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         }
     })
 }
