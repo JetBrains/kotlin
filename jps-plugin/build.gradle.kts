4b3f1c74bfe13471b20f3b422c4e1474884a22df
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-09-26 21:09:03 +0300

    Remove dependencies on jps/test

--- jps-plugin/build.gradle.kts
+++ jps-plugin/build.gradle.kts
@@ -37,9 +37,9 @@ dependencies {
 sourceSets {
     "main" { projectDefault() }
     "test" {
-        java.srcDirs("jps-tests/test"
+        /*java.srcDirs("jps-tests/test"
                      /*, "kannotator-jps-plugin-test/test"*/ // Obsolete
-        )
+        )*/
     }
 }
 
