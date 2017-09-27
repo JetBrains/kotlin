606f98f9d34c1a117faf55ab5514a3081234ed75
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-09-27 13:05:10 +0300

    Added minimal posix.def for windows.

--- backend.native/tests/link/default/default.kt
+++ backend.native/tests/link/default/default.kt
@@ -2,6 +2,6 @@ import posix.*
 
 fun main(args: Array<String>) {
     // Just check the typealias is in scope.
-    val ptrdiff: ptrdiff_t = 0L
-    println("ptrdiff = $ptrdiff")
+    val sizet: size_t = 0L
+    println("sizet = $sizet")
 }
