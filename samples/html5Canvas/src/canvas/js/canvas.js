langzhenjun/kotlin-native

A little more stubs for html demo. Tweaked arena management a little.

samples/html5Canvas/src/canvas/js/canvas.js
@@ -2,32 +2,61 @@
 konan.libraries.push ({          // This one will be auto generated.
         knjs_addDocumentToArena: function(arena) {
             // Should be resolved by jsinterop.
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, document);
+            return toArena(arena, document);
         },
-        knjs_setInterval: function (func, interval) {
-            setInterval(konan_dependencies.env.Konan_js_wrapLambda(func), interval);
+        knjs_setInterval: function (arena, func, interval) {
+            setInterval(konan_dependencies.env.Konan_js_wrapLambda(arena, func), interval);
         },
-        knjs_getElementById: function (arena, obj, id, idLength) {
+        knjs_getElementById: function (arena, obj, id, idLength, resultArena) {
             var name = toUTF16String(id, idLength);
-            var result = konan_dependencies.env.arenas[arena][obj].getElementById(name);
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, result);
+            var result = kObj(arena, obj).getElementById(name);
+            return toArena(resultArena, result);
         },
-        knjs_getContext: function (arena, obj, context, contextLength) {
+        knjs_getContext: function (arena, obj, context, contextLength, resultArena) {
             var name = toUTF16String(context, contextLength);
-            var result = konan_dependencies.env.arenas[arena][obj].getContext(name);
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, result);
+            var result = kObj(arena, obj).getContext(name);
+            return toArena(resultArena, result);
         },
-        knjs_getBoundingClientRect: function (arena, obj) {
-            var result = konan_dependencies.env.arenas[arena][obj].getBoundingClientRect();
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, result);
+        knjs_getBoundingClientRect: function (arena, obj, resultArena) {
+            var result = kObj(arena, obj).getBoundingClientRect();
+            return toArena(resultArena, result);
         },
         knjs_moveTo: function(arena, obj, x, y) {
-            konan_dependencies.env.arenas[arena][obj].moveTo(x, y);
+            kObj(arena, obj).moveTo(x, y);
         },
         knjs_lineTo: function(arena, obj, x, y) {
-            konan_dependencies.env.arenas[arena][obj].lineTo(x, y);
+            kObj(arena, obj).lineTo(x, y);
+        },
+        knjs_fillRect: function(arena, obj, x1, y1, width, height) {
+            kObj(arena, obj).fillRect(x1, y1, width, height);
+        },
+        knjs_setLineWidth: function(arena, obj, value) {
+            kObj(arena, obj).lineWidth = value;
+        },
+        knjs_setFillStyle: function(arena, obj, value_ptr, value_length) {
+            var value = toUTF16String(value_ptr, value_length);
+            kObj(arena, obj).fillStyle = value;
+        },
+        knjs_beginPath: function(arena, obj) {
+            kObj(arena, obj).beginPath();
         },
         knjs_stroke: function(arena, obj) {
-            konan_dependencies.env.arenas[arena][obj].stroke();
+            kObj(arena, obj).stroke();
         },
+        knjs_fetch: function(arena, urlPtr, urlLength, resultArena) {
+            var url = toUTF16String(urlPtr, urlLength);
+            var result = fetch(url);
+            return toArena(resultArena, result);
+        },
+        knjs_then: function(arena, obj, func, resultArena) {
+            var value = konan_dependencies.env.Konan_js_wrapLambda(arena, func);
+            var result = kObj(arena, obj).then(value);
+            var index = toArena(resultArena, result);
+            return index;
+        },
+        knjs_json: function(arena, obj, resultArena) {
+            var object = kObj(arena, obj);
+            result = object.json();
+            return toArena(resultArena, result);
+        }
 });

samples/html5Canvas/src/jsinterop/js/jsinterop.js
@@ -1,41 +1,82 @@
 
 konan.libraries.push ({
-    arenas: [],
+    arenas: new Map(),
+    nextArena: 0,
     Konan_js_allocateArena: function (array) {
-        konan_dependencies.env.arenas.push(array || []);
-        return konan_dependencies.env.arenas.length - 1;
+        var index = konan_dependencies.env.nextArena++;
+        konan_dependencies.env.arenas.set(index, array || []);
+        return index;
         
     },
     Konan_js_freeArena: function(arenaIndex) {
-        var arena = konan_dependencies.env.arenas[arenaIndex];
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
         arena.forEach(function(element, index) {
             arena[index] = null;
         });
-        konan_dependencies.env.arenas[arenaIndex] = null;
+        konan_dependencies.env.arenas.delete(arenaIndex);
+    },
+    Konan_js_pushIntToArena: function (arenaIndex, value) {
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
+        arena.push(value);
+        return arena.length - 1;
     },
     Konan_js_addObjectToArena: function (arenaIndex, object) {
-        var arena = konan_dependencies.env.arenas[arenaIndex];
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
         arena.push(object);
         return arena.length - 1;
     },
-    Konan_js_wrapLambda: function (index) {
+    Konan_js_wrapLambda: function (functionArenaIndex, index) {
         return (function () { 
+            var functionArena = konan_dependencies.env.arenas.get(functionArenaIndex);
+
             // convert Arguments to an array
             // to be provided by launcher.js
-            var arenaIndex = konan_dependencies.env.Konan_js_allocateArena(Array.prototype.slice.call(arguments)); 
-            // To be provided by Kotlin runtime. 
-            instance.exports.Konan_js_runLambda(index, arenaIndex, arguments.length); 
-            konan_dependencies.env.Konan_js_freeArena(arenaIndex);
+            var argumentArenaIndex = konan_dependencies.env.Konan_js_allocateArena(Array.prototype.slice.call(arguments));
+
+            var resultIndex = instance.exports.Konan_js_runLambda(index, argumentArenaIndex, arguments.length);
+            var result = kObj(argumentArenaIndex, resultIndex);
+            konan_dependencies.env.Konan_js_freeArena(argumentArenaIndex);
+
+            return result;
         });
     },
     Konan_js_getInt: function(arenaIndex, objIndex, propertyNamePtr, propertyNameLength) {
         // TODO:  The toUTF16String() is to be resolved by launcher.js runtime.
         var property = toUTF16String(propertyNamePtr, propertyNameLength); 
-        var value =  konan_dependencies.env.arenas[arenaIndex][objIndex][property];
+        var value =  kObj(arenaIndex, objIndex)[property];
         return value;
+    },
+    Konan_js_getProperty: function(arenaIndex, objIndex, propertyNamePtr, propertyNameLength) {
+        // TODO:  The toUTF16String() is to be resolved by launcher.js runtime.
+        var property = toUTF16String(propertyNamePtr, propertyNameLength); 
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
+        var value = arena[objIndex][property];
+        arena.push(value);
+        return arena.length - 1;
     },
     Konan_js_setFunction: function (arena, obj, propertyName, propertyNameLength, func) {
         var name = toUTF16String(propertyName, propertyNameLength);
-        konan_dependencies.env.arenas[arena][obj][name] = konan_dependencies.env.Konan_js_wrapLambda(func);
+        kObj(arena, obj)[name] = konan_dependencies.env.Konan_js_wrapLambda(arena, func);
+    },
+
+    Konan_js_setString: function (arena, obj, propertyName, propertyNameLength, stringPtr, stringLength) {
+        var name = toUTF16String(propertyName, propertyNameLength);
+        var string = toUTF16String(stringPtr, stringLength);
+        kObj(arena, obj)[name] = string;
     },
 });
+
+// TODO: This is just a shorthand notation.
+function kObj(arenaIndex, objectIndex) {
+    var arena = konan_dependencies.env.arenas.get(arenaIndex);
+    if (typeof arena == "undefined") {
+        console.log("No arena index " + arenaIndex + "for object" + objectIndex);
+        console.trace()
+    } 
+    return arena[objectIndex]
+}
+
+function toArena(arenaIndex, object) {
+    return konan_dependencies.env.Konan_js_addObjectToArena(arenaIndex, object);
+}
+

JetBrains/kotlin

samples/html5Canvas/src/canvas/js/canvas.js
@@ -2,32 +2,67 @@
 konan.libraries.push ({          // This one will be auto generated.
         knjs_addDocumentToArena: function(arena) {
             // Should be resolved by jsinterop.
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, document);
+            return toArena(arena, document);
         },
-        knjs_setInterval: function (func, interval) {
-            setInterval(konan_dependencies.env.Konan_js_wrapLambda(func), interval);
+        knjs_setInterval: function (arena, func, interval) {
+            setInterval(konan_dependencies.env.Konan_js_wrapLambda(arena, func), interval);
         },
-        knjs_getElementById: function (arena, obj, id, idLength) {
+        knjs_getElementById: function (arena, obj, id, idLength, resultArena) {
             var name = toUTF16String(id, idLength);
-            var result = konan_dependencies.env.arenas[arena][obj].getElementById(name);
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, result);
+            var result = kotlinObject(arena, obj).getElementById(name);
+            return toArena(resultArena, result);
         },
-        knjs_getContext: function (arena, obj, context, contextLength) {
+        knjs_getContext: function (arena, obj, context, contextLength, resultArena) {
             var name = toUTF16String(context, contextLength);
-            var result = konan_dependencies.env.arenas[arena][obj].getContext(name);
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, result);
+            var result = kotlinObject(arena, obj).getContext(name);
+            return toArena(resultArena, result);
         },
-        knjs_getBoundingClientRect: function (arena, obj) {
-            var result = konan_dependencies.env.arenas[arena][obj].getBoundingClientRect();
-            return konan_dependencies.env.Konan_js_addObjectToArena(arena, result);
+        knjs_getBoundingClientRect: function (arena, obj, resultArena) {
+            var result = kotlinObject(arena, obj).getBoundingClientRect();
+            return toArena(resultArena, result);
         },
         knjs_moveTo: function(arena, obj, x, y) {
-            konan_dependencies.env.arenas[arena][obj].moveTo(x, y);
+            kotlinObject(arena, obj).moveTo(x, y);
         },
         knjs_lineTo: function(arena, obj, x, y) {
-            konan_dependencies.env.arenas[arena][obj].lineTo(x, y);
+            kotlinObject(arena, obj).lineTo(x, y);
+        },
+        knjs_fillRect: function(arena, obj, x1, y1, width, height) {
+            kotlinObject(arena, obj).fillRect(x1, y1, width, height);
+        },
+        knjs_fill: function(arena, obj) {
+            kotlinObject(arena, obj).fill();
+        },
+        knjs_closePath: function(arena, obj) {
+            kotlinObject(arena, obj).closePath();
+        },
+        knjs_setLineWidth: function(arena, obj, value) {
+            kotlinObject(arena, obj).lineWidth = value;
+        },
+        knjs_setFillStyle: function(arena, obj, value_ptr, value_length) {
+            var value = toUTF16String(value_ptr, value_length);
+            kotlinObject(arena, obj).fillStyle = value;
+        },
+        knjs_beginPath: function(arena, obj) {
+            kotlinObject(arena, obj).beginPath();
         },
         knjs_stroke: function(arena, obj) {
-            konan_dependencies.env.arenas[arena][obj].stroke();
+            kotlinObject(arena, obj).stroke();
         },
+        knjs_fetch: function(arena, urlPtr, urlLength, resultArena) {
+            var url = toUTF16String(urlPtr, urlLength);
+            var result = fetch(url);
+            return toArena(resultArena, result);
+        },
+        knjs_then: function(arena, obj, func, resultArena) {
+            var value = konan_dependencies.env.Konan_js_wrapLambda(arena, func);
+            var result = kotlinObject(arena, obj).then(value);
+            var index = toArena(resultArena, result);
+            return index;
+        },
+        knjs_json: function(arena, obj, resultArena) {
+            var object = kotlinObject(arena, obj);
+            result = object.json();
+            return toArena(resultArena, result);
+        }
 });

samples/html5Canvas/src/jsinterop/js/jsinterop.js
@@ -1,41 +1,82 @@
 
 konan.libraries.push ({
-    arenas: [],
+    arenas: new Map(),
+    nextArena: 0,
     Konan_js_allocateArena: function (array) {
-        konan_dependencies.env.arenas.push(array || []);
-        return konan_dependencies.env.arenas.length - 1;
+        var index = konan_dependencies.env.nextArena++;
+        konan_dependencies.env.arenas.set(index, array || []);
+        return index;
         
     },
     Konan_js_freeArena: function(arenaIndex) {
-        var arena = konan_dependencies.env.arenas[arenaIndex];
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
         arena.forEach(function(element, index) {
             arena[index] = null;
         });
-        konan_dependencies.env.arenas[arenaIndex] = null;
+        konan_dependencies.env.arenas.delete(arenaIndex);
+    },
+    Konan_js_pushIntToArena: function (arenaIndex, value) {
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
+        arena.push(value);
+        return arena.length - 1;
     },
     Konan_js_addObjectToArena: function (arenaIndex, object) {
-        var arena = konan_dependencies.env.arenas[arenaIndex];
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
         arena.push(object);
         return arena.length - 1;
     },
-    Konan_js_wrapLambda: function (index) {
+    Konan_js_wrapLambda: function (functionArenaIndex, index) {
         return (function () { 
+            var functionArena = konan_dependencies.env.arenas.get(functionArenaIndex);
+
             // convert Arguments to an array
             // to be provided by launcher.js
-            var arenaIndex = konan_dependencies.env.Konan_js_allocateArena(Array.prototype.slice.call(arguments)); 
-            // To be provided by Kotlin runtime. 
-            instance.exports.Konan_js_runLambda(index, arenaIndex, arguments.length); 
-            konan_dependencies.env.Konan_js_freeArena(arenaIndex);
+            var argumentArenaIndex = konan_dependencies.env.Konan_js_allocateArena(Array.prototype.slice.call(arguments));
+
+            var resultIndex = instance.exports.Konan_js_runLambda(index, argumentArenaIndex, arguments.length);
+            var result = kotlinObject(argumentArenaIndex, resultIndex);
+            konan_dependencies.env.Konan_js_freeArena(argumentArenaIndex);
+
+            return result;
         });
     },
     Konan_js_getInt: function(arenaIndex, objIndex, propertyNamePtr, propertyNameLength) {
         // TODO:  The toUTF16String() is to be resolved by launcher.js runtime.
         var property = toUTF16String(propertyNamePtr, propertyNameLength); 
-        var value =  konan_dependencies.env.arenas[arenaIndex][objIndex][property];
+        var value =  kotlinObject(arenaIndex, objIndex)[property];
         return value;
+    },
+    Konan_js_getProperty: function(arenaIndex, objIndex, propertyNamePtr, propertyNameLength) {
+        // TODO:  The toUTF16String() is to be resolved by launcher.js runtime.
+        var property = toUTF16String(propertyNamePtr, propertyNameLength); 
+        var arena = konan_dependencies.env.arenas.get(arenaIndex);
+        var value = arena[objIndex][property];
+        arena.push(value);
+        return arena.length - 1;
     },
     Konan_js_setFunction: function (arena, obj, propertyName, propertyNameLength, func) {
         var name = toUTF16String(propertyName, propertyNameLength);
-        konan_dependencies.env.arenas[arena][obj][name] = konan_dependencies.env.Konan_js_wrapLambda(func);
+        kotlinObject(arena, obj)[name] = konan_dependencies.env.Konan_js_wrapLambda(arena, func);
+    },
+
+    Konan_js_setString: function (arena, obj, propertyName, propertyNameLength, stringPtr, stringLength) {
+        var name = toUTF16String(propertyName, propertyNameLength);
+        var string = toUTF16String(stringPtr, stringLength);
+        kotlinObject(arena, obj)[name] = string;
     },
 });
+
+// TODO: This is just a shorthand notation.
+function kotlinObject(arenaIndex, objectIndex) {
+    var arena = konan_dependencies.env.arenas.get(arenaIndex);
+    if (typeof arena == "undefined") {
+        console.log("No arena index " + arenaIndex + "for object" + objectIndex);
+        console.trace()
+    } 
+    return arena[objectIndex]
+}
+
+function toArena(arenaIndex, object) {
+    return konan_dependencies.env.Konan_js_addObjectToArena(arenaIndex, object);
+}
+