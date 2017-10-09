f50254d55543f717e43f7996a2c0bc0ecaf9306d
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2017-10-10 00:26:48 +0300

    New J2K: Move TODO to NewJavaToKotlinConverter

--- j2k/tests/org/jetbrains/kotlin/j2k/AbstractNewJavaToKotlinConverterSingleFileTest.kt
+++ j2k/tests/org/jetbrains/kotlin/j2k/AbstractNewJavaToKotlinConverterSingleFileTest.kt
@@ -23,6 +23,5 @@ abstract class AbstractNewJavaToKotlinConverterSingleFileTest : AbstractJavaToKo
     override fun fileToKotlin(text: String, settings: ConverterSettings, project: Project): String {
         val file = createJavaFile(text)
         NewJavaToKotlinConverter(project, settings).filesToKotlin(listOf(file))
-        TODO("Output generation")
     }
 }
