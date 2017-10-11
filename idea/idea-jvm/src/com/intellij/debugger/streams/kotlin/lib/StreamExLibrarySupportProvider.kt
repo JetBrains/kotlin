a7177a623d3eab86408a9dd577adebe3258729a2
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-11 16:32:07 +0300

    Add streamex support

---
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/lib/StreamExLibrarySupportProvider.kt
@@ -0,0 +1,32 @@
+package com.intellij.debugger.streams.kotlin.lib
+
+import com.intellij.debugger.streams.kotlin.psi.impl.KotlinJavaStreamChainBuilder
+import com.intellij.debugger.streams.kotlin.trace.dsl.KotlinStatementFactory
+import com.intellij.debugger.streams.kotlin.trace.impl.KotlinTraceExpressionBuilder
+import com.intellij.debugger.streams.lib.LibrarySupport
+import com.intellij.debugger.streams.lib.LibrarySupportProvider
+import com.intellij.debugger.streams.lib.impl.StreamExLibrarySupport
+import com.intellij.debugger.streams.trace.TraceExpressionBuilder
+import com.intellij.debugger.streams.trace.dsl.impl.DslImpl
+import com.intellij.debugger.streams.wrapper.StreamChainBuilder
+import com.intellij.openapi.project.Project
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+class StreamExLibrarySupportProvider : LibrarySupportProvider {
+  private companion object {
+    val streamChainBuilder = KotlinJavaStreamChainBuilder()
+    val support = StreamExLibrarySupport()
+    val dsl = DslImpl(KotlinStatementFactory())
+    val expressionBuilder = KotlinTraceExpressionBuilder(dsl, support.createHandlerFactory(dsl))
+  }
+
+  override fun getLanguageId(): String = LibraryUtil.KOTLIN_LANGUAGE_ID
+
+  override fun getChainBuilder(): StreamChainBuilder = streamChainBuilder
+
+  override fun getLibrarySupport(): LibrarySupport = support
+
+  override fun getExpressionBuilder(project: Project): TraceExpressionBuilder = expressionBuilder
+}
