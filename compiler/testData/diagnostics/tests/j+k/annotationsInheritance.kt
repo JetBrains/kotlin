e4b44752350f365707db92b90961a70c3407ed8e
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-10-16 10:43:10 +0300

    Refine ClassDescriptor::isCommonFinalClass definition
    Do not treat annotations as final classes as they are not final in Java
    
     #KT-20776 Fixed

---
+++ compiler/testData/diagnostics/tests/j+k/annotationsInheritance.kt
@@ -0,0 +1,19 @@
+// FILE: MyAnnotation.java
+public @interface MyAnnotation {
+}
+// FILE: MyAnnoClass.java
+public class MyAnnoClass implements MyAnnotation {
+//...
+}
+
+// FILE: main.kt
+
+val ann: MyAnnotation = MyAnnoClass()
+
+fun foo(x: MyAnnoClass) {
+    bar(x)
+}
+
+fun bar(y: MyAnnotation) {
+    y.hashCode()
+}
