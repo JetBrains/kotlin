82b033b85db84a87da848219ddbdc6c2c9069e52
Sergey Igushkin <hotkeytlt@gmail.com>
2017-10-16 20:07:02 +0200

    Add `android-extensions-compiler` content to `kotlin-android-extensions`
    (cherry picked from commit e94e62b)

--- prepare/android-extensions-compiler-gradle/build.gradle.kts
+++ prepare/android-extensions-compiler-gradle/build.gradle.kts
@@ -23,9 +23,14 @@ sourceSets {
     "test" {}
 }
 
+// fixes a deadlock in projects evaluation order for :plugins:android-extensions-compiler
+evaluationDependsOn(":plugins")
+
 val jar: Jar by tasks
 jar.apply {
-    from(getSourceSetsFrom(":kotlin-android-extensions-runtime")["main"].output.classesDirs)
+    from(getSourceSetsFrom(":plugins:android-extensions-compiler")["main"].output)
+    from(getSourceSetsFrom(":kotlin-android-extensions-runtime")["main"].output)
+    duplicatesStrategy = DuplicatesStrategy.FAIL
 }
 
 runtimeJar(rewriteDepsToShadedCompiler(jar))
