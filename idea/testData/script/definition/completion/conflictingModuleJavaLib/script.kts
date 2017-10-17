6db79146b1804cc2623b4b68fec1988e8b089bce
Pavel V. Talanov <talanov.pavel@gmail.com>
2017-10-17 18:45:58 +0300

    ~~ completion test

--- idea/testData/script/definition/completion/conflictingModuleJavaLib/script.kts
+++ idea/testData/script/definition/completion/conflictingModuleJavaLib/script.kts
@@ -1,6 +1,7 @@
-test.JavaClass.stat<caret>
+staticMeth<caret>
 
 // DEPENDENCIES: classpath:lib-classes; sources:lib-source
 // CONFLICTING_MODULE
 // EXIST: staticMethod
+// INVOCATION_COUNT: 2
 // NOTHING_ELSE
