93d3f42e1a3fbabb06c646112265e21bf39dfcdb
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-17 19:05:32 +0300

    Support psi tests for streamex library

---
+++ idea/tests/com/intellij/debugger/streams/kotlin/LibraryManager.kt
@@ -0,0 +1,10 @@
+package com.intellij.debugger.streams.kotlin
+
+import java.io.File
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+object LibraryManager {
+  val LIBRARIES_DIRECTORY: String = File("lib").absolutePath
+}
