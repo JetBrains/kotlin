46b005bafcd164c9621829258d448025155c8e6c
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-10-17 18:32:19 +0300

    Fix android extensions maven dependencies
    `kotlin-android-extensions-runtime` and `kotlin-android-extensions-compiler`
    had `com.google.android:android` as a provided dependency, but
    during transition to Kotlin Gradle build the dependency was declared
    as `runtime` instead of `compileOnly`.

--- plugins/android-extensions/android-extensions-runtime/build.gradle.kts
+++ plugins/android-extensions/android-extensions-runtime/build.gradle.kts
@@ -5,8 +5,7 @@ apply { plugin("kotlin") }
 
 dependencies {
     compile(project(":kotlin-stdlib"))
-    compile(ideaPluginDeps("layoutlib", plugin = "android"))
-    runtime(commonDep("com.google.android", "android"))
+    compileOnly(commonDep("com.google.android", "android"))
 }
 
 sourceSets {
