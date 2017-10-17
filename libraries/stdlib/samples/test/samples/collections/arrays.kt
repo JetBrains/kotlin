93bd98bdc7fa4c13abf3ffff65eaa70e8ef15c34
Alexey Belkov <knots@ya.ru>
2017-10-18 01:57:38 +0300

    Provide samples for stdlib functions (KT-20357)

--- libraries/stdlib/samples/test/samples/collections/arrays.kt
+++ libraries/stdlib/samples/test/samples/collections/arrays.kt
@@ -43,6 +43,40 @@ class Arrays {
             assertPrints(matrix.contentDeepToString(), "[[3, 7, 9], [0, 1, 0], [2, 4, 8]]")
         }
 
+        @Sample
+        fun flattenArray() {
+            val deepArray = arrayOf(
+                    arrayOf(1),
+                    arrayOf(2, 3),
+                    arrayOf(4, 5, 6)
+            )
+
+            assertPrints(deepArray.flatten(), "[1, 2, 3, 4, 5, 6]")
+        }
+
+        @Sample
+        fun unzipArray() {
+            val array = arrayOf(1 to 'a', 2 to 'b', 3 to 'c')
+            assertPrints(array.unzip(), "([1, 2, 3], [a, b, c])")
+        }
+
+        @Sample
+        @Suppress("UselessCallOnNotNull")
+        fun arrayOrEmpty() {
+            val nullArray: Array<Any>? = null
+            assertPrints(nullArray.orEmpty().contentToString(), "[]")
+
+            val array = arrayOf('a', 'b', 'c')
+            assertPrints(array.orEmpty().contentToString(), "[a, b, c]")
+        }
+
+        @Sample
+        fun byteArrayToString() {
+            val array = ByteArray(5) { (it + 65).toByte() }
+            val charset = java.nio.charset.Charset.forName("ASCII")
+            assertPrints(array.toString(charset), "ABCDE")
+        }
+
     }
 
 }
