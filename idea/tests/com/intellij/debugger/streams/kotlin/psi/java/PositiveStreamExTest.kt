b916f9e8e6d669d1a13f33d1af6b65b67955c92a
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-17 19:05:32 +0300

    Support psi tests for streamex library

--- idea/tests/com/intellij/debugger/streams/kotlin/psi/java/PositiveStreamExTest.kt
+++ idea/tests/com/intellij/debugger/streams/kotlin/psi/java/PositiveStreamExTest.kt
@@ -1,14 +1,34 @@
 package com.intellij.debugger.streams.kotlin.psi.java
 
 import com.intellij.debugger.streams.kotlin.KotlinPsiChainBuilderTestCase
+import com.intellij.debugger.streams.kotlin.LibraryManager
 import com.intellij.debugger.streams.kotlin.lib.StreamExLibrarySupportProvider
 import com.intellij.debugger.streams.wrapper.StreamChainBuilder
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
+import com.intellij.testFramework.LightPlatformTestCase
+import com.intellij.testFramework.PsiTestUtil
 
 /**
  * @author Vitaliy.Bibaev
  */
 class PositiveStreamExTest : KotlinPsiChainBuilderTestCase.Positive("streams/positive/streamex") {
   override val kotlinChainBuilder: StreamChainBuilder = StreamExLibrarySupportProvider().chainBuilder
+  override fun setUp() {
+    super.setUp()
+    ApplicationManager.getApplication().runWriteAction {
+      VfsRootAccess.allowRootAccess(LibraryManager.LIBRARIES_DIRECTORY)
+      PsiTestUtil.addLibrary(testRootDisposable, LightPlatformTestCase.getModule(),
+          "StreamEx", LibraryManager.LIBRARIES_DIRECTORY, "streamex-0.6.5.jar")
+    }
+  }
+
+  override fun tearDown() {
+    super.tearDown()
+    ApplicationManager.getApplication().runWriteAction {
+      VfsRootAccess.disallowRootAccess(LibraryManager.LIBRARIES_DIRECTORY)
+    }
+  }
 
   fun testSimple() {
     doTest()
