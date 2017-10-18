caa010f0ffcb1ced7aa1935675ff76546504834b
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2017-10-18 17:45:20 +0300

    Compile kotlin-android-extensions-runtime for JDK 1.6
    (cherry picked from commit 8a3a4df)

--- plugins/android-extensions/android-extensions-runtime/build.gradle.kts
+++ plugins/android-extensions/android-extensions-runtime/build.gradle.kts
@@ -3,6 +3,8 @@ description = "Kotlin Android Extensions Runtime"
 
 apply { plugin("kotlin") }
 
+jvmTarget = "1.6"
+
 dependencies {
     compile(project(":kotlin-stdlib"))
     compileOnly(commonDep("com.google.android", "android"))
