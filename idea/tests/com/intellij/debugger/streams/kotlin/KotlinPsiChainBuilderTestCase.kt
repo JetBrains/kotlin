07cc21200aa14a146971284be63ca9152443fee4
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-19 11:03:17 +0300

    Do not add a blank line after copyright header

--- idea/tests/com/intellij/debugger/streams/kotlin/KotlinPsiChainBuilderTestCase.kt
+++ idea/tests/com/intellij/debugger/streams/kotlin/KotlinPsiChainBuilderTestCase.kt
@@ -1,5 +1,4 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
 package com.intellij.debugger.streams.kotlin
 
 import com.intellij.debugger.streams.test.StreamChainBuilderTestCase
