3c5602017fda134ea2304f897ff92e5e8b2a7b18
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2017-10-20 15:52:25 +0300

    Several typos in comments and messages.

--- klib/src/testLibrary/kotlin/test_platform_lib.kt
+++ klib/src/testLibrary/kotlin/test_platform_lib.kt
@@ -2,7 +2,7 @@ package test.konan.platform
 
 fun produceMessage() {
     println("""This is a side effect of a test library linked into the binary.
-You should not be seing this.
+You should not be seeing this.
 """)
 }
 
