929fb546c938fc2466ec7f873cdb9172df85f24b
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2017-10-20 20:51:02 +0300

    Fix 163 ultimate build: remove nodejs_plugin/NodeJS/lib preloaded dep

--- ultimate/build.gradle.kts
+++ ultimate/build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
     compile(project(":idea:ide-common")) { isTransitive = false }
     compile(project(":idea:idea-gradle")) { isTransitive = false }
 
-    compile(ideaUltimatePreloadedDeps("*.jar", subdir = "nodejs_plugin/NodeJS/lib"))
     compile(ideaUltimateSdkCoreDeps("annotations", "trove4j", "intellij-core"))
     compile(ideaUltimateSdkDeps("openapi", "idea", "util"))
     compile(ideaUltimatePluginDeps("*.jar", plugin = "CSS"))
