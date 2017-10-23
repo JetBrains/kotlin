5700fa8cea9a265ed801c063790e9b8ec155306b
alexander-gorshenev <alexander.gorshenev@jetbrains.com>
2017-10-23 15:09:45 +0300

    Allow a couple of test pass on `linux_mips32` target.

--- backend.native/tests/link/default/default.kt
+++ backend.native/tests/link/default/default.kt
@@ -1,7 +1,8 @@
+import kotlinx.cinterop.signExtend
 import platform.posix.*
 
 fun main(args: Array<String>) {
     // Just check the typealias is in scope.
-    val sizet: size_t = 0L
+    val sizet: size_t = 0.signExtend<size_t>()
     println("sizet = $sizet")
 }
--- backend.native/tests/link/purge1/lib.kt
+++ backend.native/tests/link/purge1/lib.kt
@@ -1,8 +1,9 @@
+import kotlinx.cinterop.signExtend
 import platform.posix.*
 
 fun foo() {
     println("linked library")
-    val size: size_t = 17L
+    val size: size_t = 17.signExtend<size_t>()
     val e = fabs(1.toDouble())
     println("and symbols from posix available: $size; $e")
 }
