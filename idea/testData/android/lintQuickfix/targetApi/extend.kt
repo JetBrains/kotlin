c9f1b50b308f1e0cde023f87f63c34ed8f21d406
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-10-23 18:03:24 +0300

    Fix test data for android lint based tests for AS 3.0

--- idea/testData/android/lintQuickfix/targetApi/extend.kt
+++ idea/testData/android/lintQuickfix/targetApi/extend.kt
@@ -1,5 +1,5 @@
 // INTENTION_TEXT: Add @TargetApi(LOLLIPOP) Annotation
-// INSPECTION_CLASS: org.jetbrains.android.inspections.lint.AndroidLintInspectionToolProvider$AndroidLintNewApiInspection
+// INSPECTION_CLASS: com.android.tools.idea.lint.AndroidLintNewApiInspection
 
 import android.graphics.drawable.VectorDrawable
 
