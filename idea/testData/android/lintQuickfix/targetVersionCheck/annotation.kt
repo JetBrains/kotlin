00d32169f57a2af40e461cb429457b4860d8a26c
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-10-23 18:03:24 +0300

    Fix test data for android lint based tests for AS 3.0

--- idea/testData/android/lintQuickfix/targetVersionCheck/annotation.kt
+++ idea/testData/android/lintQuickfix/targetVersionCheck/annotation.kt
@@ -1,6 +1,6 @@
 // INTENTION_TEXT: Surround with if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) { ... }
 // INTENTION_NOT_AVAILABLE
-// INSPECTION_CLASS: org.jetbrains.android.inspections.lint.AndroidLintInspectionToolProvider$AndroidLintNewApiInspection
+// INSPECTION_CLASS: com.android.tools.idea.lint.AndroidLintNewApiInspection
 
 import android.graphics.drawable.VectorDrawable
 import kotlin.reflect.KClass
