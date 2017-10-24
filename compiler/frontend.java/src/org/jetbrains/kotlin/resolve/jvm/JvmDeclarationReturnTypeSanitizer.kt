57602c420d711a7c03311520c5cadc103a232d57
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-10-24 13:08:29 +0300

    Sanitize declaration return types
    On JVM, strip @EnhancedNullability annotation from inferred types for
    functions, properties, and local variables, so that these annotations
    do not "escape" from Kotlin declarations.

---
+++ compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/JvmDeclarationReturnTypeSanitizer.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.resolve.jvm
+
+import org.jetbrains.kotlin.config.LanguageFeature
+import org.jetbrains.kotlin.config.LanguageVersionSettings
+import org.jetbrains.kotlin.descriptors.annotations.FilteredAnnotations
+import org.jetbrains.kotlin.load.java.JvmAnnotationNames
+import org.jetbrains.kotlin.resolve.BindingTrace
+import org.jetbrains.kotlin.resolve.DeclarationReturnTypeSanitizer
+import org.jetbrains.kotlin.types.UnwrappedType
+import org.jetbrains.kotlin.types.WrappedTypeFactory
+
+object JvmDeclarationReturnTypeSanitizer : DeclarationReturnTypeSanitizer {
+    override fun sanitizeReturnType(
+            inferred: UnwrappedType,
+            wrappedTypeFactory: WrappedTypeFactory,
+            trace: BindingTrace,
+            languageVersionSettings: LanguageVersionSettings
+    ): UnwrappedType =
+            if (languageVersionSettings.supportsFeature(LanguageFeature.StrictJavaNullabilityAssertions)) {
+                // NB can't check for presence of EnhancedNullability here,
+                // because it will also cause recursion in declaration type resolution.
+                inferred.replaceAnnotations(FilteredAnnotations(inferred.annotations) {
+                    it != JvmAnnotationNames.ENHANCED_NULLABILITY_ANNOTATION
+                })
+            }
+            else inferred
+}
