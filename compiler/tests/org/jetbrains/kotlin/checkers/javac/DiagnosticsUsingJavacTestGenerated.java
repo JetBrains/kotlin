2df4d9496af6c093a3835fe521da12f95e02aa2b
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-10-26 10:57:37 +0300

    Fix incorrect UNINITIALIZED_ENUM_COMPANION diagnostic
     #KT-20959 Fixed

--- compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -7705,6 +7705,12 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                 doTest(fileName);
             }
 
+            @TestMetadata("wrongUnitializedEnumCompanion.kt")
+            public void testWrongUnitializedEnumCompanion() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/enum/wrongUnitializedEnumCompanion.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("compiler/testData/diagnostics/tests/enum/inner")
             @TestDataPath("$PROJECT_ROOT")
             @RunWith(JUnit3RunnerWithInners.class)
