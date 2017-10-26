ad1a91a6c8edf143107dc08cad8fb2285771d31d
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-26 16:40:31 +0300

    Add extension for kotlin collections support

---
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/lib/KotlinCollectionLibrarySupport.kt
@@ -0,0 +1,12 @@
+package com.intellij.debugger.streams.kotlin.lib
+
+import com.intellij.debugger.streams.lib.impl.LibrarySupportBase
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+class KotlinCollectionLibrarySupport : LibrarySupportBase() {
+  init {
+    TODO("add supported operations")
+  }
+}
--- idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/lib/KotlinCollectionSupportProvider.kt
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/lib/KotlinCollectionSupportProvider.kt
@@ -1,9 +1,13 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.streams.kotlin.lib
 
+import com.intellij.debugger.streams.kotlin.psi.impl.KotlinCollectionChainBuilder
+import com.intellij.debugger.streams.kotlin.trace.dsl.KotlinStatementFactory
+import com.intellij.debugger.streams.kotlin.trace.impl.KotlinTraceExpressionBuilder
 import com.intellij.debugger.streams.lib.LibrarySupport
 import com.intellij.debugger.streams.lib.LibrarySupportProvider
 import com.intellij.debugger.streams.trace.TraceExpressionBuilder
+import com.intellij.debugger.streams.trace.dsl.impl.DslImpl
 import com.intellij.debugger.streams.wrapper.StreamChainBuilder
 import com.intellij.openapi.project.Project
 
@@ -11,17 +15,18 @@ import com.intellij.openapi.project.Project
  * @author Vitaliy.Bibaev
  */
 class KotlinCollectionSupportProvider : LibrarySupportProvider {
+  private companion object {
+    val builder: StreamChainBuilder = KotlinCollectionChainBuilder()
+    val support: LibrarySupport = KotlinCollectionLibrarySupport()
+    val dsl = DslImpl(KotlinStatementFactory())
+  }
+
   override fun getLanguageId(): String = LibraryUtil.KOTLIN_LANGUAGE_ID
 
-  override fun getChainBuilder(): StreamChainBuilder {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  override fun getChainBuilder(): StreamChainBuilder = builder
 
-  override fun getLibrarySupport(): LibrarySupport {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  override fun getLibrarySupport(): LibrarySupport = support
 
-  override fun getExpressionBuilder(p0: Project): TraceExpressionBuilder {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  override fun getExpressionBuilder(project: Project): TraceExpressionBuilder
+      = KotlinTraceExpressionBuilder(dsl, support.createHandlerFactory(dsl))
 }
