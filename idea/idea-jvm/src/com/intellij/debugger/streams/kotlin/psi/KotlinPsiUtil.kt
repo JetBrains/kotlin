e15f3624cfe9c46896ca7574aa34f841237708a9
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-26 20:05:10 +0300

    Implement chain builder for kotlin collections

--- idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/psi/KotlinPsiUtil.kt
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/psi/KotlinPsiUtil.kt
@@ -1,13 +1,12 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.streams.kotlin.psi
 
-import com.intellij.openapi.util.text.StringUtil
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
-import org.jetbrains.kotlin.js.descriptorUtils.getJetTypeFqName
 import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtValueArgument
 import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
+import org.jetbrains.kotlin.resolve.scopes.receivers.ReceiverValue
 import org.jetbrains.kotlin.types.KotlinType
 
 /**
@@ -17,13 +16,19 @@ import org.jetbrains.kotlin.types.KotlinType
 fun KtExpression.resolveType(): KotlinType =
     this.analyze().getType(this)!!
 
-fun KtValueArgument.resolveType(): KotlinType = getArgumentExpression()!!.resolveType()
-
-fun KotlinType.getPackage(withGenerics: Boolean): String = StringUtil.getPackageName(getJetTypeFqName(withGenerics))
-
 fun KtCallExpression.callName(): String = this.calleeExpression!!.text
 
-fun KtCallExpression.receiverType(): KotlinType? {
-  val resolvedCall = getResolvedCall(analyze())
-  return resolvedCall?.dispatchReceiver?.type
+fun KtCallExpression.receiver(): ReceiverValue? {
+  val resolvedCall = getResolvedCall(analyze()) ?: return null
+  return resolvedCall.dispatchReceiver ?: resolvedCall.extensionReceiver
 }
+
+fun KtCallExpression.previousCall(): KtCallExpression? {
+  val parent = this.parent as? KtDotQualifiedExpression ?: return null
+  val receiverExpression = parent.receiverExpression
+  if (receiverExpression is KtCallExpression) return receiverExpression
+  if (receiverExpression is KtDotQualifiedExpression) return receiverExpression.selectorExpression as? KtCallExpression
+  return null
+}
+
+fun KtCallExpression.receiverType(): KotlinType? = receiver()?.type
