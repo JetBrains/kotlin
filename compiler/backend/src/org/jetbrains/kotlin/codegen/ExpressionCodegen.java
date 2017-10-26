7c3d079eeb9def7186884ef20e71e4ba810f6086
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-10-26 11:43:31 +0300

    Fix issues with enum entry self-reference
    Given a singleton class 'S' with possibly uninitialized static instance
    (enum entry, interface companion object).
    Such singleton can be referenced by name, or as an explicit or implicit
    'this'.
    For a given singleton class 'S' we
    either use 'this@S' from context (local or captured),
    or 'S' as a static instance.
    
    Local or captured 'this@S' should be used if:
      - we are in the constructor for 'S',
        and corresponding instance is initialized
            by super or delegating constructor call;
      - we are in any other member of 'S' or any of its inner classes.
    
    Otherwise, a static instance should be used.
    
    (cherry picked from commit 41b1594)

--- compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/ExpressionCodegen.java
@@ -1723,12 +1723,22 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
     }
 
     private boolean shouldGenerateSingletonAsThisOrOuterFromContext(ClassDescriptor classDescriptor) {
-        return isPossiblyUninitializedSingleton(classDescriptor) &&
-               isInsideSingleton(classDescriptor) &&
-               isThisInitialized(classDescriptor);
-    }
+        if (!isPossiblyUninitializedSingleton(classDescriptor)) return false;
+        if (!isInsideSingleton(classDescriptor)) return false;
 
-    private boolean isThisInitialized(ClassDescriptor classDescriptor) {
+        // We are inside a singleton class 'S' with possibly uninitialized static instance
+        // (enum entry, interface companion object).
+        // Such singleton can be referenced by name, or as an explicit or implicit 'this'.
+        // For a given singleton class 'S' we either use 'this@S' from context (local or captured),
+        // or 'S' as a static instance.
+        //
+        // Local or captured 'this@S' should be used if:
+        // - we are in the constructor for 'S',
+        //   and corresponding instance is initialized by super or delegating constructor call;
+        // - we are in any other member of 'S' or any of its inner classes.
+        //
+        // Otherwise, a static instance should be used.
+
         CodegenContext context = this.context;
         while (context != null) {
             if (context instanceof ConstructorContext) {
@@ -1738,9 +1748,24 @@ public class ExpressionCodegen extends KtVisitor<StackValue, StackValue> impleme
                     return constructorContext.isThisInitialized();
                 }
             }
+            else if (context instanceof ClassContext) {
+                ClassDescriptor contextClass = ((ClassContext) context).getContextDescriptor();
+                if (isInInnerClassesChainFor(contextClass, classDescriptor)) {
+                    return true;
+                }
+            }
             context = context.getParentContext();
         }
-        return true;
+        return false;
+    }
+
+    private static boolean isInInnerClassesChainFor(ClassDescriptor innerClass, ClassDescriptor outerClass) {
+        if (innerClass == outerClass) return true;
+        if (!innerClass.isInner()) return false;
+
+        DeclarationDescriptor containingDeclaration = innerClass.getContainingDeclaration();
+        if (!(containingDeclaration instanceof ClassDescriptor)) return false;
+        return isInInnerClassesChainFor((ClassDescriptor) containingDeclaration, outerClass);
     }
 
     @Nullable
