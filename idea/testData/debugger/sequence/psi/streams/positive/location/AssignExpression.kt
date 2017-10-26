1f7f65f28a6a7a7d6ed95ab82e7ca8b4eb294a3b
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-26 13:57:58 +0300

    Add tests for java stream chain building

---
+++ idea/testData/debugger/sequence/psi/streams/positive/location/AssignExpression.kt
@@ -0,0 +1,5 @@
+import java.util.stream.IntStream
+
+fun main(args: Array<String>) {
+<caret>  val res = IntStream.of(1, 2).sum()
+}
---
+++ idea/testData/debugger/sequence/psi/streams/positive/location/SingleExpression.kt
@@ -0,0 +1,5 @@
+import java.util.stream.IntStream
+
+fun main(args: Array<String>) {
+<caret> IntStream.range(1, 2).forEach { x -> }
+}
