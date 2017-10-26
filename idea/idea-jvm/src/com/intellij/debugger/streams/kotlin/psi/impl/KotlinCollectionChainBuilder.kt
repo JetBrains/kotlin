bf7f7e6ef24a3bc6f838c22643a33945711929c2
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-26 16:40:31 +0300

    Add extension for kotlin collections support

--- idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/psi/impl/KotlinCollectionChainBuilder.kt
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/psi/impl/KotlinCollectionChainBuilder.kt
@@ -1,14 +1,36 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.streams.kotlin.psi.impl
 
+import org.jetbrains.kotlin.psi.KtCallExpression
+
 /**
  * @author Vitaliy.Bibaev
  */
 class KotlinCollectionChainBuilder : KotlinChainBuilderBase(TODO("transmit here a correct transformer")) {
-  override val existenceChecker: ExistenceChecker
-    get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+  override val existenceChecker: ExistenceChecker = object : ExistenceChecker() {
+    override fun visitCallExpression(expression: KtCallExpression) {
+      if (isFound()) return
+      if (isCollectionTransformationCall(expression)) {
+        fireElementFound()
+      } else {
+        super.visitCallExpression(expression)
+      }
+    }
 
-  override fun createChainsBuilder(): ChainBuilder {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun isCollectionTransformationCall(expression: KtCallExpression): Boolean {
+      TODO("determine that expression is a collection transformation")
+    }
+  }
+
+  override fun createChainsBuilder(): ChainBuilder = object : ChainBuilder() {
+    private val previousCalls: MutableMap<KtCallExpression, KtCallExpression> = mutableMapOf()
+
+    override fun visitCallExpression(expression: KtCallExpression) {
+      super.visitCallExpression(expression)
+    }
+
+    override fun chains(): List<List<KtCallExpression>> {
+      return emptyList()
+    }
   }
 }
