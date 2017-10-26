acf459a14a619d56d90a042c6f766fd2a298c891
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-10-26 17:47:34 +0300

    Fix test data for testAddArrayOfTypeForJavaAnnotation

--- idea/testData/quickfix/typeMismatch/addArrayOfTypeForJavaAnnotation.before.Main.kt
+++ idea/testData/quickfix/typeMismatch/addArrayOfTypeForJavaAnnotation.before.Main.kt
@@ -1,6 +1,7 @@
 // "class org.jetbrains.kotlin.idea.quickfix.AddArrayOfTypeFix" "false"
 // ACTION: Add 'value =' to argument
 // ACTION: Create test
+// ACTION: Do not show hints for current method
 // ACTION: Make internal
 // ACTION: Make private
 // ACTION: To raw string literal
