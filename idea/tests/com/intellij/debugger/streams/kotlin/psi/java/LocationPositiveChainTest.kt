bb90c5ad08cfe15d1f87171208cea237adbaf573
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-10-26 13:57:58 +0300

    Add tests for java stream chain building

---
+++ idea/tests/com/intellij/debugger/streams/kotlin/psi/java/LocationPositiveChainTest.kt
@@ -0,0 +1,38 @@
+package com.intellij.debugger.streams.kotlin.psi.java
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+class LocationPositiveChainTest : PositiveJavaStreamTest("location") {
+  fun testAnonymousBody() = doTest()
+
+  fun testAssignExpression() = doTest()
+
+  fun testFirstParameterOfFunction() = doTest()
+  fun testLambdaBody() = doTest()
+
+  fun testParameterInAssignExpression() = doTest()
+  fun testParameterInReturnExpression() = doTest()
+
+  fun testReturnExpression() = doTest()
+
+  fun testSecondParameterOfFunction() = doTest()
+
+  fun testSingleExpression() = doTest()
+
+  fun testBeforeStatement() = doTest()
+
+  fun testBetweenChainCallsBeforeDot() = doTest()
+  fun testBetweenChainCallsAfterDot() = doTest()
+
+  fun testInEmptyParameterList() = doTest()
+
+  fun testBetweenParametersBeforeComma() = doTest()
+  fun testBetweenParametersAfterComma() = doTest()
+
+  fun testInAnyLambda() = doTest()
+  fun testInAnyAnonymous() = doTest()
+  fun testInString() = doTest()
+  fun testInVariableName() = doTest()
+  fun testInMethodReference() = doTest()
+}
