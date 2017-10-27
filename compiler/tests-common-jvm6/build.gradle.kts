3cfb1fd6a726916234afcdebf8247bc116f98b48
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-10-27 20:06:34 +0300

    Simplify dependency setup for jvm6 codegen test server

--- compiler/tests-common-jvm6/build.gradle.kts
+++ compiler/tests-common-jvm6/build.gradle.kts
@@ -4,16 +4,8 @@ apply { plugin("kotlin") }
 jvmTarget = "1.6"
 
 dependencies {
-    testCompile(project(":core"))
-    testCompile(project(":core::util.runtime"))
-    testCompile(project(":compiler:util"))
-    testCompile(project(":compiler:backend"))
-    testCompile(project(":compiler:frontend"))
-    testCompile(project(":compiler:frontend.java"))
-    testCompile(project(":compiler:util"))
-    testCompile(project(":compiler:cli-common"))
-    testCompile(project(":compiler:cli"))
-    testCompile(project(":kotlin-test:kotlin-test-jvm"))
+    compile(project(":kotlin-stdlib"))
+    compile(project(":kotlin-test:kotlin-test-jvm"))
 }
 
 sourceSets {
