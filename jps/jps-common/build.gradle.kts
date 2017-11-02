efdf6f2e8dddd7a77928b843d9a8d9d16a99a286
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-11-02 18:55:20 +0100

    Convert idea plugin modules to intellij plugin
    Original commit: 0d264793cef038f50d6c505d9b5008189a8079c1

--- jps/jps-common/build.gradle.kts
+++ jps/jps-common/build.gradle.kts
@@ -1,13 +1,23 @@
 
 apply { plugin("kotlin") }
 
+configureIntellijPlugin {
+    setExtraDependencies("intellij-core")
+}
+
 dependencies {
     compile(projectDist(":kotlin-stdlib"))
     compileOnly(project(":kotlin-reflect-api"))
     compile(project(":compiler:util"))
     compile(project(":compiler:cli-common"))
     compile(project(":compiler:frontend.java"))
-    compile(ideaSdkCoreDeps("intellij-core", "util"))
+}
+
+afterEvaluate {
+    dependencies {
+        compile(intellijCoreJar())
+        compile(intellij { include("util.jar") })
+    }
 }
 
 sourceSets {
