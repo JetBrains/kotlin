d117b24a19c04a72975f8c5273db63e040b8cca6
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2017-11-04 00:30:04 +0300

    UAST: yet another fight with string literals in string literals (KT-20990)

--- plugins/uast-kotlin/testData/StringTemplate.kt
+++ plugins/uast-kotlin/testData/StringTemplate.kt
@@ -5,3 +5,11 @@ val baz = "dolor"
 val foobarbaz = "$foo $bar $baz"
 
 val case4 = "a ${"literal"} z"
+
+fun simpleForTemplate(i: Int = 0) = "$i"
+
+fun foo() {
+    println("$baz")
+    val template1 = "${simpleForTemplate()}"
+    val template2 = ".${simpleForTemplate()}"
+}
