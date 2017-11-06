bbb688563a0863aad6e8106b229cda9276ce6ad1
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2017-11-06 13:16:19 +0100

    UAST: Complex test data moved to separate file (KT-20990)

--- plugins/uast-kotlin/testData/StringTemplate.kt
+++ plugins/uast-kotlin/testData/StringTemplate.kt
@@ -3,13 +3,3 @@ val bar = "ipsum"
 val baz = "dolor"
 
 val foobarbaz = "$foo $bar $baz"
-
-val case4 = "a ${"literal"} z"
-
-fun simpleForTemplate(i: Int = 0) = "$i"
-
-fun foo() {
-    println("$baz")
-    val template1 = "${simpleForTemplate()}"
-    val template2 = ".${simpleForTemplate()}"
-}
