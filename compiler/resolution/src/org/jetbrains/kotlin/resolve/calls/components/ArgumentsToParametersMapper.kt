204545290bdc48b92194d425b5373a3c4bd2e9c4
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-11-14 11:35:51 +0300

    [NI] Fix state for named arguments in arguments to parameters mapper

--- compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/components/ArgumentsToParametersMapper.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/components/ArgumentsToParametersMapper.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -200,7 +200,7 @@ class ArgumentsToParametersMapper {
                     if (state == State.VARARG_POSITION) {
                         completeVarargPositionArguments()
                     }
-                    state = State.POSITION_ARGUMENTS
+                    state = State.NAMED_ARGUMENT
 
                     processNamedArgument(argument, argumentName)
                 }
