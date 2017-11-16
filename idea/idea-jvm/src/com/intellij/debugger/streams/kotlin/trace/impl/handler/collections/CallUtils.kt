246f79ab8676bfaaab3ec6c0f28792271e3fb5ad
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-11-16 10:41:48 +0300

    Implement handler for filter call for kotlin collections

---
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/trace/impl/handler/collections/CallUtils.kt
@@ -0,0 +1,21 @@
+package com.intellij.debugger.streams.kotlin.trace.impl.handler.collections
+
+import com.intellij.debugger.streams.kotlin.trace.dsl.KotlinTypes
+import com.intellij.debugger.streams.wrapper.*
+import com.intellij.debugger.streams.wrapper.impl.IntermediateStreamCallImpl
+import com.intellij.debugger.streams.wrapper.impl.TerminatorStreamCallImpl
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+fun IntermediateStreamCall.withArgs(args: List<CallArgument>) =
+    IntermediateStreamCallImpl(name, args, typeBefore, typeAfter, textRange)
+
+fun TerminatorStreamCall.withArgs(args: List<CallArgument>) =
+    TerminatorStreamCallImpl(name, args, typeBefore, resultType, textRange)
+
+fun StreamCall.typeBefore() =
+    if (StreamCall@ this is TypeBeforeAware) StreamCall@ this.typeBefore else KotlinTypes.ANY
+
+fun StreamCall.typeAfter() =
+    if (StreamCall@ this is TypeAfterAware) StreamCall@ this.typeAfter else KotlinTypes.ANY
