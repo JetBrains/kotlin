a2f41cfab3f872e49fd9656804dac2b637b2176b
kenji tomita <destinys.lotus.1108mn@gmail.com>
2017-11-17 21:38:57 +0900

    add reversed views sample

---
+++ libraries/stdlib/samples/test/samples/collections/reversedViews.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package samples.collections
+
+import samples.Sample
+import samples.assertPrints
+
+class ReversedViews {
+    @Sample
+    fun asReversedList() {
+        val reversed = ('a'..'e').toList().asReversed()
+        assertPrints(reversed, "[e, d, c, b, a]")
+    }
+
+    @Sample
+    fun asReversedMutableList() {
+        val reversed = mutableListOf(1, 2, 3, 4, 5).asReversed()
+        assertPrints(reversed, "[5, 4, 3, 2, 1]")
+    }
+}
