c54b934383c0f45cb32f4918af73d6645c33763a
Alexey Sedunov <alexey.sedunov@jetbrains.com>
2017-11-21 14:27:49 +0300

    Configuration: Fix deserialization of immutable freeArgs collection
     #KT-21363 Fixed

--- jps-plugin/src/org/jetbrains/kotlin/jps/model/KotlinCommonCompilerArgumentsSerializer.kt
+++ jps-plugin/src/org/jetbrains/kotlin/jps/model/KotlinCommonCompilerArgumentsSerializer.kt
@@ -17,26 +17,18 @@
 package org.jetbrains.kotlin.jps.model
 
 import com.intellij.util.text.VersionComparatorUtil
-import com.intellij.util.xmlb.XmlSerializer
-import org.jdom.Element
 import org.jetbrains.jps.model.JpsProject
-import org.jetbrains.jps.model.serialization.JpsProjectExtensionSerializer
 import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
 import org.jetbrains.kotlin.config.SettingConstants.KOTLIN_COMMON_COMPILER_ARGUMENTS_SECTION
-import org.jetbrains.kotlin.config.SettingConstants.KOTLIN_COMPILER_SETTINGS_FILE
 import org.jetbrains.kotlin.jps.JpsKotlinCompilerSettings
 
-internal class KotlinCommonCompilerArgumentsSerializer : JpsProjectExtensionSerializer(KOTLIN_COMPILER_SETTINGS_FILE,
-                                                                                       KOTLIN_COMMON_COMPILER_ARGUMENTS_SECTION) {
-
-    override fun loadExtension(project: JpsProject, componentTag: Element) {
-        val settings = XmlSerializer.deserialize(componentTag, CommonCompilerArguments.DummyImpl::class.java)
+internal class KotlinCommonCompilerArgumentsSerializer : BaseJpsCompilerSettingsSerializer<CommonCompilerArguments.DummyImpl>(
+        KOTLIN_COMMON_COMPILER_ARGUMENTS_SECTION, CommonCompilerArguments::DummyImpl
+) {
+    override fun onLoad(project: JpsProject, settings: CommonCompilerArguments.DummyImpl) {
         if (VersionComparatorUtil.compare(settings.languageVersion, settings.apiVersion) < 0) {
             settings.apiVersion = settings.languageVersion
         }
         JpsKotlinCompilerSettings.setCommonCompilerArguments(project, settings)
-    }
-
-    override fun saveExtension(project: JpsProject, componentTag: Element) {
     }
 }
