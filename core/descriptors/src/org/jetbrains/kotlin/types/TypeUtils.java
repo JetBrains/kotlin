64f0688b71eb6d4a596af1c53075a67d21bb3a54
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-11-24 15:04:10 +0300

    [NI] Introduce `DefinitelyNotNullType` as type for `T!!` or `{T & Any}`

--- core/descriptors/src/org/jetbrains/kotlin/types/TypeUtils.java
+++ core/descriptors/src/org/jetbrains/kotlin/types/TypeUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
+ * Copyright 2010-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -392,6 +392,11 @@ public class TypeUtils {
         FlexibleType flexibleType = unwrappedType instanceof FlexibleType ? (FlexibleType) unwrappedType : null;
         if (flexibleType != null
             && (contains(flexibleType.getLowerBound(), isSpecialType) || contains(flexibleType.getUpperBound(), isSpecialType))) {
+            return true;
+        }
+
+        if (unwrappedType instanceof DefinitelyNotNullType &&
+            contains(((DefinitelyNotNullType) unwrappedType).getOriginal(), isSpecialType)) {
             return true;
         }
 
