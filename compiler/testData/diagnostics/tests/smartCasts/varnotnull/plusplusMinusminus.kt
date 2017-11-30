f6a8fae8909410e20c5a8e28f4ce77bd6e57badb
Dmitry Savvinov <dsavvinov@gmail.com>
2017-11-30 20:28:57 +0300

    [NI] Testdata changes after fixes in error types

--- compiler/testData/diagnostics/tests/smartCasts/varnotnull/plusplusMinusminus.kt
+++ compiler/testData/diagnostics/tests/smartCasts/varnotnull/plusplusMinusminus.kt
@@ -12,7 +12,7 @@ operator fun Long?.inc() = this?.let { it + 1 }
 fun bar(arg: Long?): Long {
     var i = arg
     if (i++ == 5L) {
-        return <!NI;TYPE_MISMATCH, NI;TYPE_MISMATCH!>i<!UNSAFE_CALL!>--<!> <!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>+<!> i<!>
+        return i<!UNSAFE_CALL!>--<!> <!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>+<!> i
     }
     if (i++ == 7L) {
         return i++ <!NI;NONE_APPLICABLE, OI;UNSAFE_OPERATOR_CALL!>+<!> <!OI;TYPE_MISMATCH!>i<!>
