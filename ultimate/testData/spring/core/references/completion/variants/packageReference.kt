825095e7c8af3df8a9c59431aba3bebdfbced9ab
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2017-11-30 18:02:59 +0300

    Spring: fix for package-completion tests
    because otherwise `java` package name interfere with `java` directory name completion from somewhere

--- ultimate/testData/spring/core/references/completion/variants/packageReference.kt
+++ ultimate/testData/spring/core/references/completion/variants/packageReference.kt
@@ -1,12 +1,13 @@
-// NUMBER: 2
-// EXIST: { lookupString:"java" }
-// EXIST: { lookupString:"javax" }
+// NUMBER: 3
+// EXIST: { lookupString:"lang" }
+// EXIST: { lookupString:"util" }
+// EXIST: { lookupString:"sql" }
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan(basePackages = arrayOf("ja<caret>va"))
+@ComponentScan(basePackages = arrayOf("java.l<caret>ang"))
 open class App {
 
 }
