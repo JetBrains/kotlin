116edf91ec9ff9281cb3fa1231743ec9688e9d66
Yan Zhulanow <Yan.Zhulanow@jetbrains.com>
2017-12-02 00:49:24 +0900

    REPL, Refactoring: Get rid of nasty enum-like String literals

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/repl/messages/ReplSystemOutWrapperForIde.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/repl/messages/ReplSystemOutWrapperForIde.kt
@@ -18,26 +18,14 @@ package org.jetbrains.kotlin.cli.jvm.repl.messages
 
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.LineSeparator
+import org.jetbrains.kotlin.utils.repl.ReplEscapeType
 import java.io.PrintStream
+import org.jetbrains.kotlin.utils.repl.ReplEscapeType.*
 
-val END_LINE: String = LineSeparator.getSystemLineSeparator().separatorString
-val XML_PREAMBLE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+internal val END_LINE: String = LineSeparator.getSystemLineSeparator().separatorString
+internal val XML_PREAMBLE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
 class ReplSystemOutWrapperForIde(standardOut: PrintStream) : PrintStream(standardOut, true), ReplWriter {
-    private enum class EscapeType {
-        INITIAL_PROMPT,
-        HELP_PROMPT,
-        USER_OUTPUT,
-        REPL_RESULT,
-        READLINE_START,
-        READLINE_END,
-        REPL_INCOMPLETE,
-        COMPILE_ERROR,
-        RUNTIME_ERROR,
-        INTERNAL_ERROR,
-        SUCCESS
-    }
-
     override fun print(x: Boolean) = printWithEscaping(x.toString())
     override fun print(x: Char) = printWithEscaping(x.toString())
     override fun print(x: Int) = printWithEscaping(x.toString())
@@ -47,25 +35,27 @@ class ReplSystemOutWrapperForIde(standardOut: PrintStream) : PrintStream(standar
     override fun print(x: String) = printWithEscaping(x)
     override fun print(x: Any?) = printWithEscaping(x.toString())
 
-    private fun printlnWithEscaping(text: String, escapeType: EscapeType = EscapeType.USER_OUTPUT) = printWithEscaping("$text\n", escapeType)
+    private fun printlnWithEscaping(text: String, escapeType: ReplEscapeType = USER_OUTPUT) {
+        printWithEscaping("$text\n", escapeType)
+    }
 
-    private fun printWithEscaping(text: String, escapeType: EscapeType = EscapeType.USER_OUTPUT) {
+    private fun printWithEscaping(text: String, escapeType: ReplEscapeType = USER_OUTPUT) {
         super.print("${xmlEscape(text, escapeType)}$END_LINE")
     }
 
-    private fun xmlEscape(s: String, escapeType: EscapeType): String {
+    private fun xmlEscape(s: String, escapeType: ReplEscapeType): String {
         val singleLine = StringUtil.replace(s, SOURCE_CHARS, XML_REPLACEMENTS)
         return "$XML_PREAMBLE<output type=\"$escapeType\">${StringUtil.escapeXml(singleLine)}</output>"
     }
 
-    override fun printlnWelcomeMessage(x: String) = printlnWithEscaping(x, EscapeType.INITIAL_PROMPT)
-    override fun printlnHelpMessage(x: String) = printlnWithEscaping(x, EscapeType.HELP_PROMPT)
-    override fun outputCommandResult(x: String) = printlnWithEscaping(x, EscapeType.REPL_RESULT)
-    override fun notifyReadLineStart() = printlnWithEscaping("", EscapeType.READLINE_START)
-    override fun notifyReadLineEnd() = printlnWithEscaping("", EscapeType.READLINE_END)
-    override fun notifyCommandSuccess() = printlnWithEscaping("", EscapeType.SUCCESS)
-    override fun notifyIncomplete() = printlnWithEscaping("", EscapeType.REPL_INCOMPLETE)
-    override fun outputCompileError(x: String) = printlnWithEscaping(x, EscapeType.COMPILE_ERROR)
-    override fun outputRuntimeError(x: String) = printlnWithEscaping(x, EscapeType.RUNTIME_ERROR)
-    override fun sendInternalErrorReport(x: String) = printlnWithEscaping(x, EscapeType.INTERNAL_ERROR)
+    override fun printlnWelcomeMessage(x: String) = printlnWithEscaping(x, INITIAL_PROMPT)
+    override fun printlnHelpMessage(x: String) = printlnWithEscaping(x, HELP_PROMPT)
+    override fun outputCommandResult(x: String) = printlnWithEscaping(x, REPL_RESULT)
+    override fun notifyReadLineStart() = printlnWithEscaping("", READLINE_START)
+    override fun notifyReadLineEnd() = printlnWithEscaping("", READLINE_END)
+    override fun notifyCommandSuccess() = printlnWithEscaping("", SUCCESS)
+    override fun notifyIncomplete() = printlnWithEscaping("", REPL_INCOMPLETE)
+    override fun outputCompileError(x: String) = printlnWithEscaping(x, COMPILE_ERROR)
+    override fun outputRuntimeError(x: String) = printlnWithEscaping(x, RUNTIME_ERROR)
+    override fun sendInternalErrorReport(x: String) = printlnWithEscaping(x, INTERNAL_ERROR)
 }
