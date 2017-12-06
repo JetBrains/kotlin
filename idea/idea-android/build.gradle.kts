7e64c577a527200f6402d165829cb71a8115dfe0
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2017-12-06 22:59:16 +0300

    Build against Android plugin 3.0

--- idea/idea-android/build.gradle.kts
+++ idea/idea-android/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
     compile(ideaSdkDeps("openapi", "idea"))
     compile(ideaPluginDeps("gradle-api", plugin = "gradle"))
-    compile(ideaPluginDeps("android", "android-common", "sdklib", "sdk-common", "sdk-tools", "layoutlib-api", plugin = "android"))
+    compile(ideaPluginDeps("android", "android-common", "android-base-common", "sdklib", "sdk-common", "sdk-tools", "layoutlib-api", plugin = "android"))
     compile(preloadedDeps("dx", subdir = "android-5.0/lib"))
 
     testCompile(projectDist(":kotlin-test:kotlin-test-jvm"))
