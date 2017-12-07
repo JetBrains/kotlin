3b5f942d5ee2fdc10d47e29fc3eb9030e52777c4
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2017-12-07 14:49:26 +0300

    ~ Fix stdlib for NI
    e: kotlin/libraries/stdlib/src/generated/_Collections.kt: (550, 20): Type mismatch: inferred type is CapturedType(*) but T was expected
    e: kotlin/libraries/stdlib/src/generated/_Collections.kt: (550, 20): Type mismatch: inferred type is List<Any?> but List<T> was expected
    e: kotlin/libraries/stdlib/src/kotlin/text/CharCategory.kt: (165, 36): Property delegate must have a 'getValue(CharCategory.Companion, KProperty<*>)' method. None of the following functions is suitable:
    @InlineOnly public operator inline fun <T> Lazy<Nothing>.getValue(thisRef: Any?, property: KProperty<*>): Nothing defined in kotlin
    e: kotlin/libraries/stdlib/src/kotlin/text/CharCategory.kt: (165, 43): Type mismatch: inferred type is Map<Int, CharCategory> but Nothing was expected
    e: kotlin/libraries/stdlib/src/kotlin/text/CharCategory.kt: (165, 43): Type mismatch: inferred type is Map<TypeVariable(K), TypeVariable(T)> but Nothing was expected
    e: kotlin/libraries/stdlib/src/kotlin/text/CharCategory.kt: (165, 43): Type mismatch: inferred type is Map<TypeVariable(K), CharCategory> but Nothing was expected
    e: kotlin/libraries/stdlib/src/kotlin/text/CharDirectionality.kt: (114, 42): Property delegate must have a 'getValue(CharDirectionality.Companion, KProperty<*>)' method. None of the following functions is suitable:
    @InlineOnly public operator inline fun <T> Lazy<Nothing>.getValue(thisRef: Any?, property: KProperty<*>): Nothing defined in kotlin
    e: kotlin/libraries/stdlib/src/kotlin/text/CharDirectionality.kt: (114, 49): Type mismatch: inferred type is Map<Int, CharDirectionality> but Nothing was expected
    e: kotlin/libraries/stdlib/src/kotlin/text/CharDirectionality.kt: (114, 49): Type mismatch: inferred type is Map<TypeVariable(K), TypeVariable(T)> but Nothing was expected
    e: kotlin/libraries/stdlib/src/kotlin/text/CharDirectionality.kt: (114, 49): Type mismatch: inferred type is Map<TypeVariable(K), CharDirectionality> but Nothing was expected

--- libraries/stdlib/src/kotlin/text/CharCategory.kt
+++ libraries/stdlib/src/kotlin/text/CharCategory.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:JvmVersion
 package kotlin.text
 
@@ -162,7 +178,7 @@ public enum class CharCategory(public val value: Int, public val code: String) {
 
 
     public companion object {
-        private val categoryMap by lazy { CharCategory.values().associateBy { it.value } }
+        private val categoryMap by lazy<Map<Int, CharCategory>> { CharCategory.values().associateBy { it.value } }
 
         public fun valueOf(category: Int): CharCategory = categoryMap[category] ?: throw IllegalArgumentException("Category #$category is not defined.")
     }
--- libraries/stdlib/src/kotlin/text/CharDirectionality.kt
+++ libraries/stdlib/src/kotlin/text/CharDirectionality.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:JvmVersion
 package kotlin.text
 
@@ -111,7 +127,7 @@ public enum class CharDirectionality(public val value: Int) {
 
 
     public companion object {
-        private val directionalityMap by lazy { CharDirectionality.values().associateBy { it.value } }
+        private val directionalityMap by lazy<Map<Int, CharDirectionality>> { CharDirectionality.values().associateBy { it.value } }
 
         public fun valueOf(directionality: Int): CharDirectionality = directionalityMap[directionality] ?: throw IllegalArgumentException("Directionality #$directionality is not defined.")
     }
