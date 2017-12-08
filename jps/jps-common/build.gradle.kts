a870208a3bcf7cb0bec587a4adeefb4a1c95cda6
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2017-12-08 23:19:21 +0100

    Convert the rest of the project to intellij repo prepared in buildSrc
    Original commit: 7e21573cf488886baa5b8908c65b9cae0ab49182

--- jps/jps-common/build.gradle.kts
+++ jps/jps-common/build.gradle.kts
@@ -1,23 +1,14 @@
 
 apply { plugin("kotlin") }
 
-configureIntellijPlugin {
-    setExtraDependencies("intellij-core")
-}
-
 dependencies {
     compile(projectDist(":kotlin-stdlib"))
     compileOnly(project(":kotlin-reflect-api"))
     compile(project(":compiler:util"))
     compile(project(":compiler:cli-common"))
     compile(project(":compiler:frontend.java"))
-}
-
-afterEvaluate {
-    dependencies {
-        compileOnly(intellijCoreJar())
-        compileOnly(intellij { include("jdom.jar", "util.jar") })
-    }
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
+    compileOnly(intellijDep()) { includeJars("jdom", "util") }
 }
 
 sourceSets {
