001ba6ebf655f7edcdfba13f08cca8c55da169f1
Dmitry Jemerov <yole@jetbrains.com>
2017-12-08 14:07:21 +0100

    Implement option for wrapping assignment statements
     #KT-21718 Fixed

--- idea/tests/org/jetbrains/kotlin/formatter/FormatterTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/formatter/FormatterTestGenerated.java
@@ -74,6 +74,12 @@ public class FormatterTestGenerated extends AbstractFormatterTest {
             doTest(fileName);
         }
 
+        @TestMetadata("AssignmentWrap.after.kt")
+        public void testAssignmentWrap() throws Exception {
+            String fileName = KotlinTestUtils.navigationMetadata("idea/testData/formatter/AssignmentWrap.after.kt");
+            doTest(fileName);
+        }
+
         @TestMetadata("BinaryExpressionAlignmentSpread.after.kt")
         public void testBinaryExpressionAlignmentSpread() throws Exception {
             String fileName = KotlinTestUtils.navigationMetadata("idea/testData/formatter/BinaryExpressionAlignmentSpread.after.kt");
