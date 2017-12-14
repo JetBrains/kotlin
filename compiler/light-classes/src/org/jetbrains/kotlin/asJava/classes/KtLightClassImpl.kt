e09b43aa422ae28a788d2693d5534e52672ba473
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2017-12-14 14:51:21 +0300

    revert shared holder for `KtLightClassImpl`

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/classes/KtLightClassImpl.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/classes/KtLightClassImpl.kt
@@ -16,8 +16,6 @@
 
 package org.jetbrains.kotlin.asJava.classes
 
-import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.UserDataHolderBase
 import org.jetbrains.kotlin.psi.KtClassOrObject
 
 // light class for top level or (inner/nested of top level) source declarations
@@ -30,35 +28,4 @@ class KtLightClassImpl(classOrObject: KtClassOrObject) : KtLightClassForSourceDe
         containingClass
 
     override fun copy() = KtLightClassImpl(classOrObject.copy() as KtClassOrObject)
-
-    private val sharedDataHolder: UserDataHolderBase
-        get() = classOrObject.getUserData(LIGHT_CLASS_SHARED_DATA_HOLDER) ?: synchronized(classOrObject) {
-            classOrObject.getUserData(LIGHT_CLASS_SHARED_DATA_HOLDER)?.let { return@synchronized it }
-            val holder = UserDataHolderBase()
-            classOrObject.putUserData(LIGHT_CLASS_SHARED_DATA_HOLDER, holder)
-            holder
-        }
-
-    override fun <T : Any?> putUserData(key: Key<T>, value: T?) = sharedDataHolder.putUserData(key, value)
-
-    override fun <T : Any?> getUserData(key: Key<T>): T? = sharedDataHolder.getUserData(key)
-
-    override fun <T : Any?> putUserDataIfAbsent(key: Key<T>, value: T) = sharedDataHolder.putUserDataIfAbsent(key, value)
-
-    override fun <T : Any?> replace(key: Key<T>, oldValue: T?, newValue: T?): Boolean = sharedDataHolder.replace(key, oldValue, newValue)
-
-    override fun <T : Any?> putCopyableUserData(key: Key<T>, value: T?) = sharedDataHolder.putCopyableUserData(key, value)
-
-    override fun getUserDataString(): String = sharedDataHolder.getUserDataString()
-
-    override fun copyCopyableDataTo(clone: UserDataHolderBase) = sharedDataHolder.copyCopyableDataTo(clone)
-
-    override fun copyUserDataTo(other: UserDataHolderBase) = sharedDataHolder.copyUserDataTo(other)
-
-    override fun <T : Any?> getCopyableUserData(key: Key<T>): T? = sharedDataHolder.getCopyableUserData(key)
-
-    override fun isUserDataEmpty(): Boolean = sharedDataHolder.isUserDataEmpty()
-
 }
-
-private val LIGHT_CLASS_SHARED_DATA_HOLDER = Key.create<UserDataHolderBase>("LIGHT_CLASS_SHARED_DATA_HOLDER")
