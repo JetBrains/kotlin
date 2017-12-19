5ab258112133e0093304ecba41250a5e2f950862
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-19 20:00:10 +0300

    Ignore kotlin collections execution tests

--- idea/tests/com/intellij/debugger/streams/kotlin/exec/collection/FlatMapOperationsTest.kt
+++ idea/tests/com/intellij/debugger/streams/kotlin/exec/collection/FlatMapOperationsTest.kt
@@ -1,9 +1,12 @@
 // Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.debugger.streams.kotlin.exec.collection
 
+import org.junit.Ignore
+
 /**
  * @author Vitaliy.Bibaev
  */
+@Ignore("enable to test collection transform operations")
 class FlatMapOperationsTest : CollectionTestCase("flatMap") {
   fun testFlatMap() {
     doTestWithResult()
