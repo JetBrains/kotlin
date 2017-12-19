96fd6046322fea8423a20ffa0db745486d4c94ab
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-19 20:23:09 +0300

    Add library support extension for kotlin sequence support

---
+++ idea/idea-jvm/src/com/intellij/debugger/streams/kotlin/lib/KotlinSequencesSupport.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.debugger.streams.kotlin.lib
+
+import com.intellij.debugger.streams.lib.impl.LibrarySupportBase
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+class KotlinSequencesSupport : LibrarySupportBase() {
+  init {
+    // todo[bibaev]: add supported operations
+  }
+}
