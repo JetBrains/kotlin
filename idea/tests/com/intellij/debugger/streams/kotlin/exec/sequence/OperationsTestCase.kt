d5e7eebd4ceccdde8163abd368c6bf5cf6da1abb
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-21 00:50:36 +0300

    Add base test case for the sequence execution tests

---
+++ idea/tests/com/intellij/debugger/streams/kotlin/exec/sequence/OperationsTestCase.kt
@@ -0,0 +1,16 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.debugger.streams.kotlin.exec.sequence
+
+import com.intellij.debugger.streams.kotlin.exec.KotlinTraceEvaluationTestCase
+import com.intellij.debugger.streams.kotlin.lib.sequence.KotlinSequenceSupportProvider
+import com.intellij.debugger.streams.lib.LibrarySupportProvider
+
+abstract class OperationsTestCase(private val packageName: String) : KotlinTraceEvaluationTestCase() {
+    override val appName: String = "sequence"
+    override val librarySupport: LibrarySupportProvider = KotlinSequenceSupportProvider()
+
+    fun doTestWithResult() = doTest(false, fullyQualifiedClassName())
+    fun doTestWithoutResult() = doTest(true, fullyQualifiedClassName())
+
+    private fun fullyQualifiedClassName() = "$packageName.${getTestName(false)}"
+}
