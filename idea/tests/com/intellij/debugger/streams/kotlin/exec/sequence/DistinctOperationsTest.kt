d30a3b2495dcc89e0d9afc332d847c9a80c75d55
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-21 11:44:40 +0300

    Add tests for distinct operations

---
+++ idea/tests/com/intellij/debugger/streams/kotlin/exec/sequence/DistinctOperationsTest.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.debugger.streams.kotlin.exec.sequence
+
+/**
+ * @author Vitaliy.Bibaev
+ */
+class DistinctOperationsTest : OperationsTestCase("distinct") {
+  fun testDistinct() = doTestWithResult()
+  fun testDistinctObjects() = doTestWithResult()
+  fun testDistinctBy() = doTestWithResult()
+}
