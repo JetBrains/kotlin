62920c99850a90b81fbfeb0b423d11019c6f2163
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2017-12-21 11:05:53 +0300

    Remove redundant copyright header in test data

--- idea/testData/debugger/sequence/exec/sequence/src/map/Map.kt
+++ idea/testData/debugger/sequence/exec/sequence/src/map/Map.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package map
 
 fun main(args: Array<String>) {
