05bf204862c2a0a93586b529ca3fcf36528afa98
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2017-12-21 10:04:02 +0100

    Don't delete nested default lambda classes during inline transformation

---
+++ compiler/testData/codegen/boxInline/defaultValues/lambdaInlining/checkObjectClassIsPresent.kt
@@ -0,0 +1,24 @@
+// NO_CHECK_LAMBDA_INLINING
+// FILE: 1.kt
+package test
+
+interface Call {
+    fun run(): String
+}
+
+inline fun test(p: String, s: () -> Call = {
+    object : Call {
+        override fun run() = p
+    } as Call
+}) = s()
+
+val same = test("O")
+
+// FILE: 2.kt
+
+import test.*
+
+fun box(): String {
+    val inlined = test("K")
+    return same.run() + inlined.run()
+}
