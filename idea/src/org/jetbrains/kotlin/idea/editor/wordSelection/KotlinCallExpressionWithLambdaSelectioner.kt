6265671eeeb3142e47d1452f1042c2c52b81ab2e
Joscha Alisch <joscha.alisch@gmail.com>
2017-12-25 17:44:35 +0100

    KT-21214: Simplify logic

--- idea/src/org/jetbrains/kotlin/idea/editor/wordSelection/KotlinCallExpressionWithLambdaSelectioner.kt
+++ idea/src/org/jetbrains/kotlin/idea/editor/wordSelection/KotlinCallExpressionWithLambdaSelectioner.kt
@@ -21,14 +21,13 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtLambdaArgument
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 class KotlinCallExpressionWithLambdaSelectioner : ExtendWordSelectionHandlerBase() {
 
     override fun canSelect(e: PsiElement): Boolean
-            = e is KtCallExpression && e.lastArgumentIsLambda() && e.lambdaIsOutsideBrackets()
+            = e is KtCallExpression && e.hasLambda()
 
     override fun select(e: PsiElement?, editorText: CharSequence?, cursorOffset: Int, editor: Editor?): List<TextRange>? {
         if (e !is KtCallExpression) return null
@@ -37,9 +36,7 @@ class KotlinCallExpressionWithLambdaSelectioner : ExtendWordSelectionHandlerBase
         return listOf(TextRange(e.startOffset, endOffset))
     }
 
-    private fun KtCallExpression.lastArgumentIsLambda(): Boolean
-            = valueArguments.isNotEmpty() && valueArguments.last() is KtLambdaArgument
+    private fun KtCallExpression.hasLambda(): Boolean
+            = lambdaArguments.isNotEmpty()
 
-    private fun KtCallExpression.lambdaIsOutsideBrackets(): Boolean
-            = valueArgumentList?.children?.size != valueArguments.size
 }
