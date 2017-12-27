b7ff00ec34f2adea680089c342b1ddaac3d9aaa6
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-12-27 13:27:59 +0300

    Remove author tag from JavaSdkUtil to fix CodeConformanceTest

--- compiler/util/src/org/jetbrains/kotlin/utils/JavaSdkUtil.java
+++ compiler/util/src/org/jetbrains/kotlin/utils/JavaSdkUtil.java
@@ -34,9 +34,6 @@ import java.util.Set;
 import static com.intellij.util.ObjectUtils.notNull;
 
 //todo[Alefas]: copypaste from org.jetbrains.jps.model.java.impl.JavaSdkUtil
-/**
- * @author nik
- */
 public class JavaSdkUtil {
   @NotNull
   public static List<File> getJdkClassesRoots(@NotNull File home, boolean isJre) {
