fd52deb9c17cb2b0757e1f605c185a9f35585256
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-12-28 18:32:05 +0100

    Remove unneeded dependencies of module 'generators'
    Previously, the classpath of this module was used to run all tests in
    the project. This is why it depended on almost all modules in the
    project. Now, in the Gradle build, these dependencies are no longer
    needed

--- generators/build.gradle.kts
+++ generators/build.gradle.kts
@@ -2,27 +2,7 @@
 apply { plugin("kotlin") }
 
 dependencies {
-    compile(protobufFull())
-    compile(project(":idea"))
-    compile(project(":idea:idea-jvm"))
-    compile(project(":j2k"))
-    compile(project(":compiler:util"))
-    compile(project(":compiler:cli"))
-    compile(project(":compiler:backend"))
-    compile(project(":compiler:frontend"))
-    compile(project(":compiler:frontend.java"))
-    compile(project(":compiler:backend"))
-    compile(project(":js:js.ast"))
-    compile(project(":js:js.frontend"))
-    compile(project(":idea:idea-test-framework"))
-    compile(projectDist(":kotlin-test:kotlin-test-jvm"))
-    compile(projectTests(":kotlin-build-common"))
-    compile(projectTests(":compiler:tests-common"))
-    compile(projectTests(":compiler:container"))
-    compile(projectTests(":compiler:incremental-compilation-impl"))
     compile(projectTests(":compiler:cli"))
-    compile(projectTests(":idea"))
-    compile(projectTests(":idea:idea-gradle"))
     compile(projectTests(":idea:idea-maven"))
     compile(projectTests(":j2k"))
     compile(projectTests(":idea:idea-android"))
@@ -34,17 +14,10 @@ dependencies {
     compile(projectTests(":kotlin-allopen-compiler-plugin"))
     compile(projectTests(":kotlin-noarg-compiler-plugin"))
     compile(projectTests(":kotlin-sam-with-receiver-compiler-plugin"))
-    compile(projectTests(":plugins:uast-kotlin"))
-    compile(projectTests(":js:js.tests"))
     compile(projectTests(":generators:test-generator"))
     compileOnly(intellijDep("jps-build-test"))
     compileOnly(project(":kotlin-reflect-api"))
-
-    testCompile(project(":idea:idea-test-framework")) { isTransitive = false }
-    testCompile(project(":compiler:incremental-compilation-impl"))
-    testCompile(commonDep("junit:junit"))
     testCompile(intellijDep("jps-build-test"))
-
     testRuntime(intellijDep()) { includeJars("idea_rt") }
     testRuntime(projectDist(":kotlin-reflect"))
 }
