254f766aafd6d34b1db99880ba7d27f05af68fb2
Dmitry Jemerov <yole@jetbrains.com>
2017-12-28 12:11:17 +0100

    Take longer read action in forEachKotlinOverride()
    EA-114124 - assert: CompositeElement.getChildrenAsPsiElements

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/declarationsSearch/overridersSearch.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/search/declarationsSearch/overridersSearch.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2017 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.search.declarationsSearch
@@ -43,7 +32,6 @@ import org.jetbrains.kotlin.idea.core.getDeepestSuperDeclarations
 import org.jetbrains.kotlin.idea.core.isOverridable
 import org.jetbrains.kotlin.idea.search.allScope
 import org.jetbrains.kotlin.idea.search.excludeKotlinSources
-import org.jetbrains.kotlin.idea.search.restrictToKotlinSources
 import org.jetbrains.kotlin.idea.util.application.runReadAction
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
@@ -140,19 +128,22 @@ private fun forEachKotlinOverride(
     val baseDescriptors = runReadAction { members.mapNotNull { it.unsafeResolveToDescriptor() as? CallableMemberDescriptor }.filter { it.isOverridable } }
     if (baseDescriptors.isEmpty()) return true
 
-    HierarchySearchRequest(ktClass, scope, true).searchInheritors().forEach {
-        val inheritor = it.unwrapped as? KtClassOrObject ?: return@forEach
-        val inheritorDescriptor = runReadAction { inheritor.unsafeResolveToDescriptor() as ClassDescriptor }
-        val substitutor = getTypeSubstitutor(baseClassDescriptor.defaultType, inheritorDescriptor.defaultType) ?: return@forEach
-        baseDescriptors.forEach {
-            val superMember = it.source.getPsi()!!
-            val overridingDescriptor = inheritorDescriptor.findCallableMemberBySignature(it.substitute(substitutor) as CallableMemberDescriptor)
-            val overridingMember = overridingDescriptor?.source?.getPsi()
-            if (overridingMember != null) {
-                if (!processor(superMember, overridingMember)) return false
+    HierarchySearchRequest(ktClass, scope, true).searchInheritors().forEach(Processor {
+        val inheritor = it.unwrapped as? KtClassOrObject ?: return@Processor true
+        runReadAction {
+            val inheritorDescriptor = inheritor.unsafeResolveToDescriptor() as ClassDescriptor
+            val substitutor = getTypeSubstitutor(baseClassDescriptor.defaultType, inheritorDescriptor.defaultType) ?: return@runReadAction true
+            baseDescriptors.forEach {
+                val superMember = it.source.getPsi()!!
+                val overridingDescriptor = inheritorDescriptor.findCallableMemberBySignature(it.substitute(substitutor) as CallableMemberDescriptor)
+                val overridingMember = overridingDescriptor?.source?.getPsi()
+                if (overridingMember != null) {
+                    if (!processor(superMember, overridingMember)) return@runReadAction false
+                }
             }
+            true
         }
-    }
+    })
 
     return true
 }
