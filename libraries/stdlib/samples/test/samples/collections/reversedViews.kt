ad6e9a7a3ef78fab600235ac4ea14ec6da89830d
kenji tomita <destinys.lotus.1108mn@gmail.com>
2018-01-05 12:07:12 +0900

    improve reversed views sample

--- libraries/stdlib/samples/test/samples/collections/reversedViews.kt
+++ libraries/stdlib/samples/test/samples/collections/reversedViews.kt
@@ -22,13 +22,39 @@ import samples.assertPrints
 class ReversedViews {
     @Sample
     fun asReversedList() {
-        val reversed = ('a'..'e').toList().asReversed()
+        val original = ('a'..'e').toList()
+        val reversed = original.asReversed()
+
+        assertPrints(original, "[a, b, c, d, e]")
         assertPrints(reversed, "[e, d, c, b, a]")
     }
 
     @Sample
     fun asReversedMutableList() {
-        val reversed = mutableListOf(1, 2, 3, 4, 5).asReversed()
+        val original = mutableListOf(1, 2, 3, 4, 5)
+        val reversed = original.asReversed()
+
+        assertPrints(original, "[1, 2, 3, 4, 5]")
         assertPrints(reversed, "[5, 4, 3, 2, 1]")
+
+        original.add(6)
+        assertPrints(original, "[1, 2, 3, 4, 5, 6]")
+        assertPrints(reversed, "[6, 5, 4, 3, 2, 1]")
+
+        original.add(0, 0)
+        assertPrints(original, "[0, 1, 2, 3, 4, 5, 6]")
+        assertPrints(reversed, "[6, 5, 4, 3, 2, 1, 0]")
+
+        original[original.lastIndex] = 10
+        assertPrints(original, "[0, 1, 2, 3, 4, 5, 10]")
+        assertPrints(reversed, "[10, 5, 4, 3, 2, 1, 0]")
+
+        original.removeAt(original.lastIndex)
+        assertPrints(original, "[0, 1, 2, 3, 4, 5]")
+        assertPrints(reversed, "[5, 4, 3, 2, 1, 0]")
+
+        original.remove(5)
+        assertPrints(original, "[0, 1, 2, 3, 4]")
+        assertPrints(reversed, "[4, 3, 2, 1, 0]")
     }
 }
