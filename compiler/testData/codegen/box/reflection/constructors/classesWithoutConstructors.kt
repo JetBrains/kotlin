d2908c38941a0f7caf9c205e3efbe42e457a9974
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2018-01-05 16:21:23 +0100

    Do not serialize constructors for anonymous objects
    The only client of this data is reflection, and since anonymous objects
    do not have constructors in the source code, they shouldn't in
    reflection as well
    
     #KT-20442 Fixed

--- compiler/testData/codegen/box/reflection/constructors/classesWithoutConstructors.kt
+++ compiler/testData/codegen/box/reflection/constructors/classesWithoutConstructors.kt
@@ -17,6 +17,7 @@ fun box(): String {
     assertTrue(Interface::class.constructors.isEmpty())
     assertTrue(Obj::class.constructors.isEmpty())
     assertTrue(C.Companion::class.constructors.isEmpty())
+    assertTrue(object {}::class.constructors.isEmpty())
 
     return "OK"
 }
--- core/metadata.jvm/src/org/jetbrains/kotlin/metadata/jvm/deserialization/JvmMetadataVersion.kt
+++ core/metadata.jvm/src/org/jetbrains/kotlin/metadata/jvm/deserialization/JvmMetadataVersion.kt
@@ -18,7 +18,7 @@ class JvmMetadataVersion(vararg numbers: Int) : BinaryVersion(*numbers) {
 
     companion object {
         @JvmField
-        val INSTANCE = JvmMetadataVersion(1, 1, 10)
+        val INSTANCE = JvmMetadataVersion(1, 1, 11)
 
         @JvmField
         val INVALID_VERSION = JvmMetadataVersion()
