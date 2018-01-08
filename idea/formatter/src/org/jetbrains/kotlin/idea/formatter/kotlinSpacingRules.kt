67897d9b3df2e861202f043257bdb895f4e97cd9
Dmitry Jemerov <yole@jetbrains.com>
2018-01-08 15:18:09 +0100

    Don't wrap argument list containing anonymous functions
    Just like for objects and lambdas, don't consider line breaks inside
    anonymous functions as line breaks inside argument list

--- idea/formatter/src/org/jetbrains/kotlin/idea/formatter/kotlinSpacingRules.kt
+++ idea/formatter/src/org/jetbrains/kotlin/idea/formatter/kotlinSpacingRules.kt
@@ -581,6 +581,13 @@ private fun excludeLambdasAndObjects(parent: ASTBlock): List<TextRange> {
             super.visitObjectLiteralExpression(expression)
             rangesToExclude.add(expression.textRange)
         }
+
+        override fun visitNamedFunction(function: KtNamedFunction) {
+            super.visitNamedFunction(function)
+            if (function.name == null) {
+                rangesToExclude.add(function.textRange)
+            }
+        }
     })
     return TextRangeUtil.excludeRanges(parent.textRange, rangesToExclude).toList()
 }
