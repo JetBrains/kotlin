a4e36bbfface66808dca6a97c021b56647d3b216
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-01-11 20:12:01 +0300

    [Spring] Revert "Spring: removing needless EP-s because they ported to UAST in platform"
    This reverts commit 9355f79c6b06fdcd365f23b40e93ea841b372b27.

--- ultimate/testData/spring/core/references/completion/handler/packageReferenceTab.kt
+++ ultimate/testData/spring/core/references/completion/handler/packageReferenceTab.kt
@@ -1,11 +1,11 @@
-// ELEMENT: lang
+// ELEMENT: java
 // CHAR: \t
 
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan(basePackages = arrayOf("java.la<caret>ng"))
+@ComponentScan(basePackages = arrayOf("ja<caret>va"))
 open class App {
 
 }
