3a17c8d5b32c70e13a2aeb97b1901b12be1134cf
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2018-01-11 19:35:34 +0300

    `KotlinCoreEnvironment`: set `ideaCompatibleBuildNumber = "181.1"`

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -382,7 +382,7 @@ class KotlinCoreEnvironment private constructor(
     }
 
     companion object {
-        private val ideaCompatibleBuildNumber = "173.1"
+        private val ideaCompatibleBuildNumber = "181.1"
 
         init {
             setCompatibleBuild()
