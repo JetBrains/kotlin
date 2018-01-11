6afe378542683ce155ca8d3bdc54926ce7ad9975
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-01-11 20:12:44 +0300

    Revert "Fix GradleNoduleBuilder use qualified names check"
    This reverts commit aec020a404d367195d58c7bb5d30185bfb85f202.

--- idea/idea-gradle/src/org/jetbrains/kotlin/gradle/kdsl/GradleModuleBuilder.java
+++ idea/idea-gradle/src/org/jetbrains/kotlin/gradle/kdsl/GradleModuleBuilder.java
@@ -28,7 +28,6 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.StorageScheme;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager;
 import com.intellij.openapi.externalSystem.importing.ImportSpec;
 import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder;
 import com.intellij.openapi.externalSystem.model.ExternalSystemDataKeys;
@@ -134,7 +133,7 @@ public class GradleModuleBuilder extends AbstractExternalModuleBuilder<GradlePro
       moduleName = getName();
     }
     else {
-      moduleName = getExternalProjectSettings().isUseQualifiedModuleNames() && StringUtil.isNotEmpty(myProjectId.getGroupId())
+      moduleName = ModuleGrouperKt.isQualifiedModuleNamesEnabled(myWizardContext.getProject()) && StringUtil.isNotEmpty(myProjectId.getGroupId())
                    ? (myProjectId.getGroupId() + '.' + myProjectId.getArtifactId())
                    : myProjectId.getArtifactId();
     }
@@ -232,9 +231,6 @@ public class GradleModuleBuilder extends AbstractExternalModuleBuilder<GradlePro
     catch (IOException e) {
       LOG.warn("Unexpected exception on applying frameworks templates", e);
     }
-
-    // it will be set later in any case, but save is called immediately after project creation, so, to ensure that it will be properly saved as external system module
-    ExternalSystemModulePropertyManager.getInstance(module).setExternalId(GradleConstants.SYSTEM_ID);
 
     final Project project = module.getProject();
     if (myWizardContext.isCreatingNewProject()) {
