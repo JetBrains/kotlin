f520fe3d10046e8db783cf3a5d15f039faa92307
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-01-11 20:12:56 +0300

    Revert "`KtLightAbstractAnnotation` build fix for 173 (#1283)"
    This reverts commit b401cc19e07ad632d61ca0d402dc6017f9559d18.

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/lightAnnotations.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/lightAnnotations.kt
@@ -55,12 +55,6 @@ abstract class KtLightAbstractAnnotation(parent: PsiElement, computeDelegate: ()
 
     override fun getParameterList() = clsDelegate.parameterList
 
-    override fun canNavigate(): Boolean = super<KtLightElementBase>.canNavigate()
-
-    override fun canNavigateToSource(): Boolean = super<KtLightElementBase>.canNavigateToSource()
-
-    override fun navigate(requestFocus: Boolean) = super<KtLightElementBase>.navigate(requestFocus)
-
     open fun fqNameMatches(fqName: String): Boolean = qualifiedName == fqName
 }
 
@@ -318,12 +312,6 @@ class KtLightNonExistentAnnotation(parent: KtLightElement<*, *>) : KtLightElemen
     override fun findDeclaredAttributeValue(attributeName: String?) = null
     override fun getMetaData() = null
     override fun getParameterList() = KtLightEmptyAnnotationParameterList(this)
-
-    override fun canNavigate(): Boolean = super<KtLightElementBase>.canNavigate()
-
-    override fun canNavigateToSource(): Boolean = super<KtLightElementBase>.canNavigateToSource()
-
-    override fun navigate(requestFocus: Boolean) = super<KtLightElementBase>.navigate(requestFocus)
 }
 
 class KtLightEmptyAnnotationParameterList(parent: PsiElement) : KtLightElementBase(parent), PsiAnnotationParameterList {
