8168845e80d0ff1c442ba937c5176eb93da7460c
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-01-11 20:13:05 +0300

    Revert "Fix compilation after moving to idea 173"
    This reverts commit fe121cc6aeadf63ca2c2819c9c7df59cc81caf5d.

--- idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleInspectionTest.kt
+++ idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleInspectionTest.kt
@@ -259,6 +259,7 @@ class GradleInspectionTest : GradleImportingTestCase() {
 
             val foundProblems = presentation.problemElements
                     .values
+                    .flatMap { it.toList() }
                     .mapNotNull { it as? ProblemDescriptorBase }
                     .map { it.descriptionTemplate }
 
