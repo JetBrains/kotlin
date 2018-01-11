b45cf53e9f2bb618b2318330b8d94ab9b3cae2ee
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-01-11 20:12:36 +0300

    Revert "Update file structure tests as FileStructurePopup api was changed in 173"
    This reverts commit 5244d48fde8e373b046a8ccb602a280b946f8d41.

--- idea/tests/org/jetbrains/kotlin/idea/structureView/AbstractKotlinFileStructureTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/structureView/AbstractKotlinFileStructureTest.kt
@@ -16,28 +16,43 @@
 
 package org.jetbrains.kotlin.idea.structureView
 
+import com.intellij.ide.actions.ViewStructureAction
 import com.intellij.ide.util.FileStructurePopup
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.intellij.openapi.ui.Queryable.PrintInfo
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.ui.treeStructure.filtered.FilteringTreeStructure
+import com.intellij.util.ui.tree.TreeUtil
 import org.jetbrains.kotlin.idea.completion.test.configureWithExtraFile
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 import org.jetbrains.kotlin.idea.test.PluginTestCaseBase
 import org.jetbrains.kotlin.test.InTextDirectivesUtils
+import org.jetbrains.kotlin.test.KotlinTestUtils
 import java.io.File
 
-abstract class AbstractKotlinFileStructureTest : KotlinFileStructureTestBase() {
+abstract class AbstractKotlinFileStructureTest : KotlinLightCodeInsightFixtureTestCase() {
     override fun getTestDataPath() = PluginTestCaseBase.getTestDataPathBase() + "/structureView/fileStructure"
 
     override fun getProjectDescriptor() = KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
 
-    override val fileExtension = "kt"
-    override val treeFileName: String get() = getFileName("after")
-
     fun doTest(path: String) {
         myFixture.configureWithExtraFile(path)
 
-        popupFixture.popup.setup()
+        val textEditor = TextEditorProvider.getInstance()!!.getTextEditor(myFixture.editor)
+        val popup = ViewStructureAction.createPopup(myFixture.project, textEditor)
 
-        checkTree()
+        if (popup == null) throw AssertionError("popup mustn't be null")
+
+        popup.createCenterPanel()
+        popup.treeBuilder.ui!!.updater!!.setPassThroughMode(true)
+        popup.update()
+
+        popup.setup()
+
+        val printInfo = PrintInfo(arrayOf("text"), arrayOf("location"))
+        val popupText = StructureViewUtil.print(popup.tree, false, printInfo, null)
+        KotlinTestUtils.assertEqualsToFile(File("${FileUtil.getNameWithoutExtension(path)}.after"), popupText)
     }
 
     protected fun FileStructurePopup.setup() {
@@ -45,5 +60,28 @@ abstract class AbstractKotlinFileStructureTest : KotlinFileStructureTestBase() {
 
         val withInherited = InTextDirectivesUtils.isDirectiveDefined(fileText, "WITH_INHERITED")
         setTreeActionState(KotlinInheritedMembersNodeProvider::class.java, withInherited)
+    }
+
+    fun FileStructurePopup.update() {
+        treeBuilder.refilter()!!.doWhenProcessed {
+            getStructure().rebuild()
+            updateRecursively(getRootNode())
+            treeBuilder.updateFromRoot()
+
+            TreeUtil.expandAll(tree)
+        }
+    }
+
+    fun FileStructurePopup.getFileStructureSpeedSearch() = speedSearch as FileStructurePopup.MyTreeSpeedSearch
+
+    fun FileStructurePopup.getStructure() = treeBuilder.treeStructure as FilteringTreeStructure
+
+    fun FileStructurePopup.getRootNode() = treeBuilder.rootElement as FilteringTreeStructure.FilteringNode
+
+    fun FileStructurePopup.updateRecursively(node: FilteringTreeStructure.FilteringNode) {
+        node.update()
+        for (child in node.children()!!) {
+            updateRecursively(child)
+        }
     }
 }
