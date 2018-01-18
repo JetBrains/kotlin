82584fb287cbbe5c03b4fef1283f509372210df3
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2018-01-18 17:01:38 +0100

    Add support for proxies to scripts
    Update jcabi-aether dependency from 0.10.1 to 1.0-SNAPSHOT. In order to
    do that, add https://oss.sonatype.org/content/repositories/snapshots to
    the repositories in tools/kotlin-script-util/build.gradle.kts
    
    Script dependencies are resolved using jcabi-aether. Unfortunately "stable"
    release (0.10.1) does not support proxies. Therefore, script dependencies
    are not pulled behind a proxy. Issue is fixed in SNAPSHOT release of
    jcabi-aether.
    
    FIXED: KT-22363

--- libraries/tools/kotlin-script-util/build.gradle.kts
+++ libraries/tools/kotlin-script-util/build.gradle.kts
@@ -4,13 +4,17 @@ description = "Kotlin scripting support utilities"
 apply { plugin("kotlin") }
 apply { plugin("jps-compatible") }
 
+repositories {
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots") // for jcabi-aether:1.0-SNAPSHOT
+}
+
 dependencies {
     compile(project(":kotlin-stdlib"))
     compile(project(":kotlin-script-runtime"))
     compileOnly(project(":compiler:cli"))
     compileOnly(project(":compiler:daemon-common"))
     compile(project(":kotlin-daemon-client"))
-    compileOnly("com.jcabi:jcabi-aether:0.10.1")
+    compileOnly("com.jcabi:jcabi-aether:1.0-SNAPSHOT")
     compileOnly("org.sonatype.aether:aether-api:1.13.1")
     compileOnly("org.apache.maven:maven-core:3.0.3")
     testCompileOnly(project(":compiler:cli"))
@@ -18,7 +22,7 @@ dependencies {
     testRuntime(project(":kotlin-reflect"))
     testCompile(commonDep("junit:junit"))
     testRuntime(projectRuntimeJar(":kotlin-compiler"))
-    testRuntime("com.jcabi:jcabi-aether:0.10.1")
+    testRuntime("com.jcabi:jcabi-aether:1.0-SNAPSHOT")
     testRuntime("org.sonatype.aether:aether-api:1.13.1")
     testRuntime("org.apache.maven:maven-core:3.0.3")
     compileOnly(intellijDep()) { includeJars("openapi", "util") }
