b8ddda69863ef37b42fd2cd533577f28515c6532
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-01-24 21:21:43 +0300

    Add smali plugin required by android plugin to test runtime

--- j2k/build.gradle.kts
+++ j2k/build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     testRuntime(intellijPluginDep("coverage"))
     testRuntime(intellijPluginDep("maven"))
     testRuntime(intellijPluginDep("android"))
+    testRuntime(intellijPluginDep("smali"))
     testRuntime(intellijPluginDep("junit"))
     testRuntime(intellijPluginDep("testng"))
     testRuntime(intellijPluginDep("IntelliLang"))
--- plugins/android-extensions/android-extensions-idea/build.gradle.kts
+++ plugins/android-extensions/android-extensions-idea/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     testRuntime(intellijPluginDep("java-decompiler"))
     testRuntime(intellijPluginDep("maven"))
     testRuntime(intellijPluginDep("android"))
+    testRuntime(intellijPluginDep("smali"))
 }
 
 sourceSets {
