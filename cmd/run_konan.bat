67c723b93b7622d41568351971e9797dfc2e854e
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2018-01-25 16:00:15 +0300

    [win][build][batch] fix path variable calculation

--- cmd/run_konan.bat
+++ cmd/run_konan.bat
@@ -93,7 +93,7 @@ goto :eof
 :set_path
   rem libclang.dll is dynamically linked and thus requires correct PATH to be loaded.
   rem TODO: remove this hack.
-  set "PATH=%HOME%\.konan\dependencies\msys2-mingw-w64-x86_64-gcc-7.2.0-clang-llvm-5.0.0-windows-x86-64\bin;%PATH%"
+  set "PATH=%USERPROFILE%\.konan\dependencies\msys2-mingw-w64-x86_64-gcc-7.2.0-clang-llvm-5.0.0-windows-x86-64\bin;%PATH%"
 goto :eof
 
 :end
