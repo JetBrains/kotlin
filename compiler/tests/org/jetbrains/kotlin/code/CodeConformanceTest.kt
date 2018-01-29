5d06c2c584591c060f7d4cfb34d9defea9c9b1fb
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-01-29 21:37:56 +0300

    Code conformance test: actualize stdlib and tools exclusions

--- compiler/tests/org/jetbrains/kotlin/code/CodeConformanceTest.kt
+++ compiler/tests/org/jetbrains/kotlin/code/CodeConformanceTest.kt
@@ -59,12 +59,12 @@ class CodeConformanceTest : TestCase() {
                 "js/js.tests/.gradle",
                 "js/js.translator/testData/node_modules",
                 "libraries/stdlib/common/build",
-                "libraries/stdlib/common/target",
                 "libraries/stdlib/js/.gradle",
                 "libraries/stdlib/js/build",
                 "libraries/kotlin.test/js/it/.gradle",
                 "libraries/kotlin.test/js/it/node_modules",
                 "libraries/stdlib/js/node_modules",
+                "libraries/tools/kotlin-maven-plugin-test/target",
                 "buildSrc/prepare-deps/android-dx/build",
                 "buildSrc/prepare-deps/intellij-sdk/build"
         )
