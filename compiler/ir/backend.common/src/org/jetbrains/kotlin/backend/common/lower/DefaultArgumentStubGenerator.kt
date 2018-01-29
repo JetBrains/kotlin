a648b9676c52c055b9109c5e039268731b4fac45
Igor Chevdar <igor.chevdar@jetbrains.com>
2018-01-29 19:02:12 +0300

    Fixes in IR lowerings: DeepCopyIrTree: typo fix. Default arguments: made proper overridden descriptor selection. Local declarations: made proper determination whether a function is locally defined.

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/DefaultArgumentStubGenerator.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/DefaultArgumentStubGenerator.kt
@@ -108,7 +108,7 @@ open class DefaultArgumentStubGenerator constructor(val context: CommonBackendCo
                         val expressionBody = getDefaultParameterExpressionBody(irFunction, valueParameter)
 
                         /* Use previously calculated values in next expression. */
-                        expressionBody.transformChildrenVoid(object:IrElementTransformerVoid() {
+                        expressionBody.transformChildrenVoid(object: IrElementTransformerVoid() {
                             override fun visitGetValue(expression: IrGetValue): IrExpression {
                                 log { "GetValue: ${expression.descriptor}" }
                                 val valueSymbol = variables[expression.descriptor] ?: return expression
@@ -200,7 +200,7 @@ private fun Scope.createTemporaryVariable(symbol: IrVariableSymbol, initializer:
             this.initializer = initializer
         }
 
-private fun getDefaultParameterExpressionBody(irFunction: IrFunction, valueParameter: ValueParameterDescriptor):IrExpressionBody {
+private fun getDefaultParameterExpressionBody(irFunction: IrFunction, valueParameter: ValueParameterDescriptor): IrExpressionBody {
     return irFunction.getDefault(valueParameter) ?: TODO("FIXME!!!")
 }
 
@@ -292,14 +292,14 @@ class DefaultParameterInjector constructor(val context: CommonBackendContext): B
             private fun parametersForCall(expression: IrMemberAccessExpression): Pair<IrFunctionSymbol, List<Pair<ValueParameterDescriptor, IrExpression?>>> {
                 val descriptor = expression.descriptor as FunctionDescriptor
                 val keyDescriptor = if (DescriptorUtils.isOverride(descriptor))
-                    DescriptorUtils.getAllOverriddenDescriptors(descriptor).first()
+                    DescriptorUtils.getAllOverriddenDescriptors(descriptor).first { it.needsDefaultArgumentsLowering }
                 else
                     descriptor.original
                 val realFunction = keyDescriptor.generateDefaultsFunction(context)
                 val realDescriptor = realFunction.descriptor
 
                 log { "$descriptor -> $realDescriptor" }
-                val maskValues = Array((descriptor.valueParameters.size + 31) / 32, {0})
+                val maskValues = Array((descriptor.valueParameters.size + 31) / 32, { 0 })
                 val params = mutableListOf<Pair<ValueParameterDescriptor, IrExpression?>>()
                 params.addAll(descriptor.valueParameters.mapIndexed { i, _ ->
                     val valueArgument = expression.getValueArgument(i)
@@ -447,7 +447,7 @@ private fun FunctionDescriptor.generateDefaultsFunction(context: CommonBackendCo
 object DECLARATION_ORIGIN_FUNCTION_FOR_DEFAULT_PARAMETER :
         IrDeclarationOriginImpl("DEFAULT_PARAMETER_EXTENT")
 
-private fun valueParameter(descriptor: FunctionDescriptor, index: Int, name: Name, type: KotlinType):ValueParameterDescriptor {
+private fun valueParameter(descriptor: FunctionDescriptor, index: Int, name: Name, type: KotlinType): ValueParameterDescriptor {
     return ValueParameterDescriptorImpl(
             containingDeclaration = descriptor,
             original              = null,
