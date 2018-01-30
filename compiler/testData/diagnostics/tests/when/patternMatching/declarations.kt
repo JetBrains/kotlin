058471b52816aeb98d49a0c2ec1a6b020d36b9e5
Sergei Vorobyov <hacker.mad.cat@gmail.com>
2018-01-30 14:16:27 +0300

    minor
    Signed-off-by: DeveloperHacker <hacker.mad.cat@gmail.com>

--- compiler/testData/diagnostics/tests/when/patternMatching/declarations.kt
+++ compiler/testData/diagnostics/tests/when/patternMatching/declarations.kt
@@ -16,7 +16,7 @@ fun foo(x: Any?, a: Int) = when (x) {
     is <!UNRESOLVED_REFERENCE!>pair<!>(val <!NAME_SHADOWING!>a<!>: Int, val b: Int) -> a + b
     is <!DUPLICATE_LABEL_IN_WHEN!>Pair<!>(val <!NAME_SHADOWING!>a<!>: Int, val b: Int) && a > b -> a + b
     is <!USELESS_IS_CHECK, DUPLICATE_LABEL_IN_WHEN!>Pair<!>(10, 20) -> a + <!UNRESOLVED_REFERENCE!>b<!>
-    is <!USELESS_IS_CHECK, DUPLICATE_LABEL_IN_WHEN, NO_TYPE_ARGUMENTS_ON_RHS!>Pair<!> -> 1
+    is <!NO_TYPE_ARGUMENTS_ON_RHS, USELESS_IS_CHECK, DUPLICATE_LABEL_IN_WHEN!>Pair<!> -> 1
     is <!USELESS_IS_CHECK, DUPLICATE_LABEL_IN_WHEN!>Pair<!>(_, val <!NAME_SHADOWING, UNUSED_VARIABLE!>a<!>: Pair<*, *>) -> 1
     is C<*, *> -> 2
     is val <!NAME_SHADOWING!>a<!>: Int -> a
