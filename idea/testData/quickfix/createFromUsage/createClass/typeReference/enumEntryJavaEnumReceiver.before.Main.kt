17d4e92e6c8dd519bd8f018f7931490cfe2f09c2
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-01-31 13:49:25 +0300

    Fix "create enum constant" test (create type alias is no longer here)
    (cherry picked from commit 00ceac5)

--- idea/testData/quickfix/createFromUsage/createClass/typeReference/enumEntryJavaEnumReceiver.before.Main.kt
+++ idea/testData/quickfix/createFromUsage/createClass/typeReference/enumEntryJavaEnumReceiver.before.Main.kt
@@ -5,6 +5,5 @@
 // ACTION: Create class 'A'
 // ACTION: Create enum 'A'
 // ACTION: Create interface 'A'
-// ACTION: Create type alias 'A'
 // ERROR: Unresolved reference: A
 internal fun foo(): J.<caret>A = throw Throwable("")
