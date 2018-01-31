cfad8b17c01a0ac87ebcf3aa39fe9df74b4235d4
Vadim Brilyantov <v.brilyantov@yandex.ru>
2018-01-31 20:24:07 +0300

    build.kts : Ktor added

--- compiler/daemon/daemon-common/build.gradle.kts
+++ compiler/daemon/daemon-common/build.gradle.kts
@@ -3,6 +3,13 @@ apply { plugin("kotlin") }
 
 jvmTarget = "1.6"
 
+repositories {
+    mavenCentral()
+    maven {
+        setUrl("http://dl.bintray.com/kotlin/ktor")
+    }
+}
+
 dependencies {
     compile(project(":core:descriptors"))
     compile(project(":core:descriptors.jvm"))
@@ -11,6 +18,9 @@ dependencies {
     compile(project(":kotlin-stdlib"))
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeIntellijCoreJarDependencies(project) }
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.22")
+    compile("io.ktor:ktor-network:0.9.1-alpha-10")
+
 }
 
 sourceSets {
