c06204d0d4cd28d77b5bfb7271e3863ad4c7155a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-01-31 21:17:09 +0300

    Remove obsolete test classpath helpers

--- compiler/tests-common/tests/org/jetbrains/kotlin/codegen/forTestCompile/ForTestCompileRuntime.java
+++ compiler/tests-common/tests/org/jetbrains/kotlin/codegen/forTestCompile/ForTestCompileRuntime.java
@@ -45,10 +45,6 @@ public class ForTestCompileRuntime {
     public static File kotlinTestJarForTests() {
         return assertExists(new File("dist/kotlinc/lib/kotlin-test.jar"));
     }
-    @NotNull
-    public static File kotlinTestJunitJarForTests() {
-        return assertExists(new File("dist/kotlinc/lib/kotlin-test-junit.jar"));
-    }
 
     @NotNull
     public static File reflectJarForTests() {
@@ -83,14 +79,6 @@ public class ForTestCompileRuntime {
     @NotNull
     public static File androidAnnotationsForTests() {
         return assertExists(new File("dist/kotlinc/lib/kotlin-annotations-android.jar"));
-    }
-
-    // TODO: Do not use these classes, remove them after stdlib tests are merged in the same build as the compiler
-    @NotNull
-    @Deprecated
-    public static File[] runtimeClassesForTests() {
-        // TODO: replace hardcoded path with something flexible
-        return new File[] { assertExists(new File("dist/builtins")), assertExists(new File("build/kotlin-stdlib/classes/java/builtins")), assertExists(new File("build/kotlin-stdlib/classes/java/main")) };
     }
 
     @NotNull
