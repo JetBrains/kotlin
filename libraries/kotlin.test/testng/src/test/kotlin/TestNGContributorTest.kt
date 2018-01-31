a2a340dd741a5e3c6013c1a656bafa48b45f007c
Valeriy Zhirnov <neonailol@gmail.com>
2018-01-31 20:29:56 +0300

    TestNG support in kotlin.test #KT-22620
    (cherry picked from commit ad77019)

---
+++ libraries/kotlin.test/testng/src/test/kotlin/TestNGContributorTest.kt
@@ -0,0 +1,25 @@
+package kotlinx.testing.tests
+
+import org.testng.annotations.*
+import org.testng.*
+import java.util.concurrent.*
+
+class TestNGContributorTest {
+
+    @Test
+    fun smokeTest() {
+        Assert.assertEquals("TestNGAsserter", kotlin.test.asserter.javaClass.simpleName)
+    }
+
+    @Test
+    fun `should fail to contribute if it was run outside of testng`() {
+        val q = ArrayBlockingQueue<Any>(1)
+
+        Thread {
+            q.put(kotlin.test.asserter)
+        }.start()
+
+        Assert.assertEquals("DefaultAsserter", q.take().javaClass.simpleName)
+    }
+
+}
