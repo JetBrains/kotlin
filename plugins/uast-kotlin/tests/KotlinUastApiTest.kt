9ee653af4c71e6c042618fef5b977e11125cb201
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2018-02-01 17:15:30 +0300

    Uast: `KotlinUNestedAnnotation` with light annotation (IDEA-185890)

--- plugins/uast-kotlin/tests/KotlinUastApiTest.kt
+++ plugins/uast-kotlin/tests/KotlinUastApiTest.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.uast.test.kotlin
 
+import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiModifier
 import com.intellij.testFramework.UsefulTestCase
 import org.jetbrains.kotlin.asJava.toLightAnnotation
@@ -288,6 +289,9 @@ class KotlinUastApiTest : AbstractKotlinUastTest() {
                         ?: throw AssertionError("haven't got annotation from $referenceExpression(${referenceExpression?.javaClass})")
 
                 assertEquals("Annotation", convertedUAnnotation.qualifiedName)
+                val lightAnnotation = convertedUAnnotation.getAsJavaPsiElement(PsiAnnotation::class.java)
+                        ?: throw AssertionError("can't get lightAnnotation from $convertedUAnnotation")
+                assertEquals("Annotation", lightAnnotation.qualifiedName)
             }
     }
 
