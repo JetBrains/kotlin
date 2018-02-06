81122ed236f52c3865eb04412d4b46208173a338
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-02-06 15:01:21 +0300

    Fix a pack of broken IDE action tests
    Related to KT-20281.
    For Java resolve we can use also resolve with 'null' platform,
    which is possible situation in tests.

--- compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/JvmAnalyzerFacade.kt
+++ compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/JvmAnalyzerFacade.kt
@@ -70,7 +70,9 @@ object JvmAnalyzerFacade : AnalyzerFacade() {
             @Suppress("UNCHECKED_CAST")
             val resolverForReferencedModule = referencedClassModule?.let { resolverForProject.tryGetResolverForModule(it as M) }
 
-            val resolverForModule = resolverForReferencedModule?.takeIf { referencedClassModule.platform == JvmPlatform } ?: run {
+            val resolverForModule = resolverForReferencedModule?.takeIf {
+                referencedClassModule.platform == JvmPlatform || referencedClassModule.platform == null
+            } ?: run {
                 // in case referenced class lies outside of our resolver, resolve the class as if it is inside our module
                 // this leads to java class being resolved several times
                 resolverForProject.resolverForModule(moduleInfo)
