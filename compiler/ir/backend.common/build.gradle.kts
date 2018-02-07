9241942e5bc3fb429a207724a500dba7e23e2976
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2018-02-07 19:42:01 +0300

    ~ ir.backend.common

--- compiler/ir/backend.common/build.gradle.kts
+++ compiler/ir/backend.common/build.gradle.kts
@@ -4,10 +4,13 @@ apply { plugin("kotlin") }
 jvmTarget = "1.6"
 
 dependencies {
+    val compile by configurations
+    val compileOnly by configurations
+
     compile(project(":core:descriptors"))
-    compile(project(":core:descriptors.jvm"))
     compile(project(":compiler:util"))
     compile(project(":compiler:frontend"))
+    compile(project(":compiler:ir.tree"))
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
 }
 
