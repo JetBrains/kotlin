5499078fccb119a99291b8d6ddcd9b325b4effad
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-02-09 09:48:21 +0300

    Make "Specify explicit lambda signature" available on whole lambda
    So #KT-22492 Fixed

--- idea/testData/quickfix/replaceWithSafeCallForScopeFunction/letWithWrongImplicitThis.kt
+++ idea/testData/quickfix/replaceWithSafeCallForScopeFunction/letWithWrongImplicitThis.kt
@@ -4,6 +4,7 @@
 // ACTION: Introduce local variable
 // ACTION: Move lambda argument into parentheses
 // ACTION: Replace with safe (this?.) call
+// ACTION: Specify explicit lambda signature
 // ERROR: Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type String?
 
 fun String?.foo(a: String?) {
