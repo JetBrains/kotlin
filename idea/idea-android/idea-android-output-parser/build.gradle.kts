4c187a4061be02ca605df3ad31de568052219375
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-02-09 15:51:50 +0300

    Fix gradle plugin dependencies for AS 3.2

--- idea/idea-android/idea-android-output-parser/build.gradle.kts
+++ idea/idea-android/idea-android-output-parser/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     compile(project(":compiler:util"))
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeJars("guava", "android-base-common", rootProject = rootProject) }
-    compileOnly(intellijPluginDep("gradle")) { includeJars("gradle-tooling-api", rootProject = rootProject) }
+    compileOnly(intellijPluginDep("gradle")) { includeJars("gradle-api", rootProject = rootProject) }
     compileOnly(intellijPluginDep("android")) { includeJars("android", "android-common", "sdk-common") }
 }
 
