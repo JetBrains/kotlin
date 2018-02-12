d047f72a2e9b734918e5d4bf12251591e284219f
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2018-02-12 16:26:15 +0300

    Ask user to specify zephyr base.

--- samples/zephyr/build.sh
+++ samples/zephyr/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 BOARD=stm32f4_disco
-#export ZEPHYR_BASE="PLEASE_SET_ZEPHYR_BASE"
-export ZEPHYR_BASE=~/kotlin-native/zephyr
+export ZEPHYR_BASE="PLEASE_SET_ZEPHYR_BASE"
 
 if [ "$ZEPHYR_BASE" == "PLEASE_SET_ZEPHYR_BASE" ] ; then
     echo "Please set ZEPHYR_BASE in this build.sh."
