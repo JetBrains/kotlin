790ffb0c3d540bb3610da1e88973ce5b499e64fa
Gabryel Monteiro <gabryelarm@gmail.com>
2018-02-12 15:50:36 -0200

    Adding Intention and Quickfix to Sealed SubClasses that Can Be Changed To Object

---
+++ idea/testData/inspections/sealedSubClassCanBeObject/sealed.kt
@@ -0,0 +1,31 @@
+sealed class Sealed {
+    open class OpenSubSealedNestedNoChild() : Sealed()
+
+    open class SubSealedNestedWithChild  : Sealed()
+
+    class SubSealedNestedChild() : SubSealedNestedWithChild()
+
+    final class FinalSubSealedNested() : Sealed()
+
+    class SubSealedNested() : Sealed()
+}
+
+class SubSealedParentheses() : Sealed()
+
+class SubSealedNoParentheses : Sealed()
+
+open class SubSealedWithChild : Sealed()
+
+class SubSealedChild : SubSealedWithChild()
+
+class SubSealedParameters(val value : String) : Sealed()
+
+object SubSealedObject() : Sealed()
+
+class SubSealedObjectWithPrimaryConstructor(parameter: String) : Sealed()
+
+class SubSealedObjectWithSecondaryConstructor() : Sealed() {
+    constructor(parameter: String): this()
+}
+
+object AlreadyObject: Sealed()
