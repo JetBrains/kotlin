a127c31db94c3d4c7fc2b90af6f6d82276705873
Mikaël Peltier <mikaelpeltier@google.com>
2018-02-15 21:04:24 +0100

    Remove bad condition from couldUseDirectAccessToProperty

--- compiler/backend/src/org/jetbrains/kotlin/codegen/JvmCodegenUtil.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/JvmCodegenUtil.java
@@ -194,8 +194,6 @@ public class JvmCodegenUtil {
             @NotNull MethodContext contextBeforeInline,
             boolean shouldInlineConstVals
     ) {
-        if (hasJvmFieldAnnotation(property)) return true;
-
         if (shouldInlineConstVals && property.isConst()) return true;
 
         if (KotlinTypeMapper.isAccessor(property)) return false;
