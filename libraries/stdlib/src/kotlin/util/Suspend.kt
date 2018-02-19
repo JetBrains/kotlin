03567a1c01a16916b74dd9dd94e52aa37f1def5b
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-02-19 17:30:08 +0300

    Add @SinceKotlin("1.2") to kotlin.suspend

--- libraries/stdlib/src/kotlin/util/Suspend.kt
+++ libraries/stdlib/src/kotlin/util/Suspend.kt
@@ -8,6 +8,7 @@ import kotlin.internal.RequireKotlin
 import kotlin.internal.RequireKotlinVersionKind
 
 @kotlin.internal.InlineOnly
+@SinceKotlin("1.2")
 @Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 @RequireKotlin("1.2.30", level = DeprecationLevel.HIDDEN, versionKind = RequireKotlinVersionKind.COMPILER_VERSION)
 public inline fun <R> suspend(noinline block: suspend () -> R): suspend () -> R = block
