507915d4df3ceec5db6d4d9f3b7a0aa7cfbe8fed
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-02-27 15:10:22 +0900

    KT-22970 Add explicit this intention/inspection missing for lambda invocation

--- idea/src/org/jetbrains/kotlin/idea/inspections/ImplicitThisInspection.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/ImplicitThisInspection.kt
@@ -23,6 +23,7 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.idea.intentions.getCallableDescriptor
 import org.jetbrains.kotlin.idea.util.getFactoryForImplicitReceiverWithSubtypeOf
 import org.jetbrains.kotlin.idea.util.getResolutionScope
 import org.jetbrains.kotlin.psi.*
@@ -53,7 +54,10 @@ class ImplicitThisInspection : AbstractKotlinInspection() {
             val scope = reference.getResolutionScope(context) ?: return
 
             val descriptor = context[BindingContext.REFERENCE_TARGET, reference] as? CallableDescriptor ?: return
-            val receiverDescriptor = descriptor.extensionReceiverParameter ?: descriptor.dispatchReceiverParameter ?: return
+            val receiverDescriptor = descriptor.extensionReceiverParameter
+                    ?: descriptor.dispatchReceiverParameter
+                    ?: expression.getCallableDescriptor()?.extensionReceiverParameter
+                    ?: return
             val receiverType = receiverDescriptor.type
 
             val expressionFactory = scope.getFactoryForImplicitReceiverWithSubtypeOf(receiverType) ?: return
