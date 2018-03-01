4ae3c8505488b2a36671642eed82ba1e6c2ec429
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-03-01 14:40:51 +0300

    Fix test runtime for 181 platform compiler tests

--- compiler/tests-common/build.gradle.kts
+++ compiler/tests-common/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     testCompile(androidDxJar()) { isTransitive = false }
     testCompile(intellijCoreDep()) { includeJars("intellij-core"); isTransitive = false }
     testCompile(intellijDep()) {
-        includeJars("openapi", "platform-api", "idea", "idea_rt", "guava", "trove4j", "picocontainer", "asm-all", "log4j", "jdom", rootProject = rootProject)
+        includeJars("openapi", "platform-api", "platform-impl", "idea", "idea_rt", "guava", "trove4j", "picocontainer", "asm-all", "log4j", "jdom", "bootstrap", rootProject = rootProject)
         isTransitive = false
     }
 }
