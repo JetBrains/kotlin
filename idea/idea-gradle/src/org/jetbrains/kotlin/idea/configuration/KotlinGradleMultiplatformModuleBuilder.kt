b55f6525bdc63e428b891662c9ffe3a1069d48a2
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-03-02 13:06:39 +0300

    MPP wizard: add common module at the same level with platform ones
    Related to KT-23097

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/configuration/KotlinGradleMultiplatformModuleBuilder.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/configuration/KotlinGradleMultiplatformModuleBuilder.kt
@@ -21,7 +21,7 @@ import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.externalSystem.service.project.wizard.ExternalModuleSettingsStep
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
@@ -31,40 +31,35 @@ import org.jetbrains.kotlin.idea.util.rootManager
 import org.jetbrains.plugins.gradle.frameworkSupport.BuildScriptDataBuilder
 import org.jetbrains.plugins.gradle.service.project.wizard.GradleModuleBuilder
 import org.jetbrains.plugins.gradle.service.settings.GradleProjectSettingsControl
+import javax.swing.Icon
 
 class KotlinGradleMultiplatformModuleBuilder : GradleModuleBuilder() {
+    var commonModuleName: String? = null
     var jvmModuleName: String? = null
     var jsModuleName: String? = null
 
     override fun getBuilderId() = "kotlin.gradle.multiplatform"
 
-    override fun getNodeIcon() = KotlinIcons.MPP
+    override fun getNodeIcon(): Icon = KotlinIcons.MPP
 
     override fun getPresentableName() = "Kotlin (Multiplatform - Experimental)"
 
     override fun getDescription() =
-            "Multiplatform projects allow reusing the same code between multiple platforms supported by Kotlin. Such projects are built with Gradle."
+        "Multiplatform projects allow reusing the same code between multiple platforms supported by Kotlin. Such projects are built with Gradle."
 
     override fun createWizardSteps(wizardContext: WizardContext, modulesProvider: ModulesProvider): Array<ModuleWizardStep> {
         super.createWizardSteps(wizardContext, modulesProvider)  // initializes GradleModuleBuilder.myWizardContext
-        return arrayOf(KotlinGradleMultiplatformWizardStep(this, wizardContext),
-                       ExternalModuleSettingsStep(
-                               wizardContext, this, GradleProjectSettingsControl(externalProjectSettings))
+        return arrayOf(
+            KotlinGradleMultiplatformWizardStep(this, wizardContext),
+            ExternalModuleSettingsStep(wizardContext, this, GradleProjectSettingsControl(externalProjectSettings))
         )
-    }
-
-    override fun setupRootModel(modifiableRootModel: ModifiableRootModel) {
-        super.setupRootModel(modifiableRootModel)
-        val module = modifiableRootModel.module
-        val buildScriptData = getBuildScriptData(module) ?: return
-        val sdk = modifiableRootModel.sdk
-        GradleKotlinMPPCommonFrameworkSupportProvider().addSupport(buildScriptData, sdk)
     }
 
     override fun setupModule(module: Module) {
         super.setupModule(module)
 
         val contentRoot = module.rootManager.contentRoots.firstOrNull() ?: return
+        setupCommonModule(module, contentRoot)
         setupPlatformModule(module, contentRoot, jvmModuleName, GradleKotlinMPPJavaFrameworkSupportProvider())
         setupPlatformModule(module, contentRoot, jsModuleName, GradleKotlinMPPJSFrameworkSupportProvider())
 
@@ -72,24 +67,42 @@ class KotlinGradleMultiplatformModuleBuilder : GradleModuleBuilder() {
         settingsGradle?.let {
             module.project.executeCommand("Update settings.gradle") {
                 val doc = FileDocumentManager.getInstance().getDocument(it) ?: return@executeCommand
-                doc.insertString(doc.textLength, "include '$jvmModuleName', '$jsModuleName'")
+                val commonPrefix = commonModuleName?.let { "'$it', " } ?: ""
+                doc.insertString(doc.textLength, "include $commonPrefix'$jvmModuleName', '$jsModuleName'")
                 FileDocumentManager.getInstance().saveDocument(doc)
             }
         }
     }
 
-    private fun setupPlatformModule(module: Module,
-                                    contentRoot: VirtualFile,
-                                    platformModuleName: String?,
-                                    supportProvider: GradleKotlinFrameworkSupportProvider) {
-        if (platformModuleName.isNullOrEmpty()) return
-
-        val sdk = module.rootManager.sdk
-        val moduleDir = contentRoot.createChildDirectory(this, platformModuleName!!)
+    private fun setupChildModule(
+        rootModule: Module,
+        contentRoot: VirtualFile,
+        childModuleName: String?,
+        extendScript: (BuildScriptDataBuilder, Sdk?) -> Unit = { _, _ -> }
+    ) {
+        if (childModuleName.isNullOrEmpty()) return
+        val moduleDir = contentRoot.createChildDirectory(this, childModuleName!!)
         val buildGradle = moduleDir.createChildData(null, "build.gradle")
         val buildScriptData = BuildScriptDataBuilder(buildGradle)
-        supportProvider.addSupport(buildScriptData, sdk)
-        buildScriptData.addDependencyNotation("expectedBy project(\":\")")
+        extendScript(buildScriptData, rootModule.rootManager.sdk)
         VfsUtil.saveText(buildGradle, buildScriptData.buildConfigurationPart() + buildScriptData.buildMainPart())
+    }
+
+    private fun setupCommonModule(
+        rootModule: Module,
+        contentRoot: VirtualFile
+    ) = setupChildModule(rootModule, contentRoot, commonModuleName) { builder, sdk ->
+        GradleKotlinMPPCommonFrameworkSupportProvider().addSupport(builder, sdk)
+    }
+
+    private fun setupPlatformModule(
+        rootModule: Module,
+        contentRoot: VirtualFile,
+        platformModuleName: String?,
+        supportProvider: GradleKotlinFrameworkSupportProvider
+    ) = setupChildModule(rootModule, contentRoot, platformModuleName) { builder, sdk ->
+        supportProvider.addSupport(builder, sdk)
+        val dependency = commonModuleName ?: ""
+        builder.addDependencyNotation("expectedBy project(\":$dependency\")")
     }
 }
