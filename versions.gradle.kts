c32484856249ade7675315c90c5b5ddf54aaadaa
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2018-03-14 14:29:54 +0300

    Update Idea to 181.4203.6

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -1,5 +1,5 @@
 
-extra["versions.intellijSdk"] = "181.3870.7"
+extra["versions.intellijSdk"] = "181.4203.6"
 extra["versions.androidBuildTools"] = "r23.0.1"
 extra["versions.idea.NodeJS"] = "181.3494.12"
 //extra["versions.androidStudioRelease"] = "3.1.0.5"
