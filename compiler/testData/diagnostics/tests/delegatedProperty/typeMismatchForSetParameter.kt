bf92280f520396085d168c8bfa87d77efda4d92a
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-03-20 02:45:28 +0300

    [NI] Update testdata about inference for delegated properties

--- compiler/testData/diagnostics/tests/delegatedProperty/typeMismatchForSetParameter.kt
+++ compiler/testData/diagnostics/tests/delegatedProperty/typeMismatchForSetParameter.kt
@@ -1,12 +1,13 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
+// !WITH_NEW_INFERENCE
 
 import kotlin.reflect.KProperty
 
 class A {
-  var a: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>Delegate()<!>
+  var a: Int by <!NI;DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE, DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>Delegate()<!>
 }
 
-var aTopLevel: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>Delegate()<!>
+var aTopLevel: Int by <!NI;DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE, DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>Delegate()<!>
 
 class Delegate {
   operator fun getValue(t: Any?, p: KProperty<*>): Int {
