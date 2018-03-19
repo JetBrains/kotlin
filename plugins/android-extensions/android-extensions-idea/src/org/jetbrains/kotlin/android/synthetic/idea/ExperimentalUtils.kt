b4f90a90d9543f189a911717c6dae96d718b59a0
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-03-19 22:58:13 +0300

    Android: Extract dependencies to Android plugin to extensions
    (cherry picked from commit 8f02d338a63b4d8f11630f4adc7ddf64e3504c9f)

--- plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/synthetic/idea/ExperimentalUtils.kt
+++ plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/synthetic/idea/ExperimentalUtils.kt
@@ -19,8 +19,9 @@ package org.jetbrains.kotlin.android.synthetic.idea
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.module.Module
 import kotlinx.android.extensions.CacheImplementation
-import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.kotlin.analyzer.ModuleInfo
+import org.jetbrains.kotlin.android.model.AndroidModuleInfoProvider
+import org.jetbrains.kotlin.android.model.isAndroidModule
 import org.jetbrains.kotlin.android.synthetic.AndroidCommandLineProcessor.Companion.ANDROID_COMPILER_PLUGIN_ID
 import org.jetbrains.kotlin.android.synthetic.AndroidCommandLineProcessor.Companion.EXPERIMENTAL_OPTION
 import org.jetbrains.kotlin.android.synthetic.AndroidCommandLineProcessor.Companion.ENABLED_OPTION
@@ -46,7 +47,7 @@ private fun Module.getOptionValueInFacet(option: CliOption): String? {
 }
 
 private fun isTestMode(module: Module): Boolean {
-    return ApplicationManager.getApplication().isUnitTestMode && AndroidFacet.getInstance(module) != null
+    return ApplicationManager.getApplication().isUnitTestMode && module.isAndroidModule
 }
 
 internal val Module.androidExtensionsIsEnabled: Boolean
