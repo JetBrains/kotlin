1f9983ec274a453eca049a4e613a94e5385505b2
Dmitriy Novozhilov <dimon2x@gmail.com>
2018-03-20 17:21:38 +0300

    remove redundant checks in from parent `AbstractDiagnosticTest`

--- compiler/tests-common/tests/org/jetbrains/kotlin/cli/jvm/analyzer/AbstractIrAnalyzerTest.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/cli/jvm/analyzer/AbstractIrAnalyzerTest.kt
@@ -5,20 +5,81 @@
 
 package org.jetbrains.kotlin.cli.jvm.analyzer
 
+import org.jetbrains.kotlin.checkers.AbstractDiagnosticsTest
 import org.jetbrains.kotlin.checkers.AbstractDiagnosticsTestWithStdLib
+import org.jetbrains.kotlin.checkers.LazyOperationsLog
+import org.jetbrains.kotlin.checkers.LoggingStorageManager
+import org.jetbrains.kotlin.cli.jvm.compiler.NoScopeRecordCliBindingTrace
+import org.jetbrains.kotlin.context.SimpleGlobalContext
+import org.jetbrains.kotlin.context.withModule
+import org.jetbrains.kotlin.context.withProject
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
 import org.jetbrains.kotlin.ir.AbstractIrGeneratorTestCase
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.storage.ExceptionTracker
+import org.jetbrains.kotlin.storage.LockBasedStorageManager
+import org.jetbrains.kotlin.storage.StorageManager
 import java.io.File
+import java.util.*
 
-abstract class AbstractIrAnalyzerTest: AbstractDiagnosticsTestWithStdLib() {
-    override fun skipDescriptorsValidation(): Boolean = true
+abstract class AbstractIrAnalyzerTest : AbstractDiagnosticsTestWithStdLib() {
+    override fun analyzeAndCheck(testDataFile: File, files: List<TestFile>) {
+        val groupedByModule = files.groupBy(TestFile::module)
 
-    override fun shouldSkipJvmSignatureDiagnostics(groupedByModule: Map<TestModule?, List<TestFile>>): Boolean = true
+        val lazyOperationsLog: LazyOperationsLog?
 
-    override fun performAdditionalChecksAfterDiagnostics(
-        testDataFile: File,
-        testFiles: List<TestFile>,
+        val tracker = ExceptionTracker()
+        val storageManager: StorageManager
+        if (files.any(TestFile::checkLazyLog)) {
+            lazyOperationsLog = LazyOperationsLog(AbstractDiagnosticsTest.HASH_SANITIZER)
+            storageManager = LoggingStorageManager(
+                LockBasedStorageManager.createWithExceptionHandling(tracker),
+                lazyOperationsLog.addRecordFunction
+            )
+        } else {
+            storageManager = LockBasedStorageManager.createWithExceptionHandling(tracker)
+        }
+
+        val context = SimpleGlobalContext(storageManager, tracker)
+
+        val modules = createModules(groupedByModule, context.storageManager)
+        val moduleBindings = HashMap<TestModule?, BindingContext>()
+
+        for ((testModule, testFilesInModule) in groupedByModule) {
+            val ktFiles = getKtFiles(testFilesInModule, true)
+
+            val oldModule = modules[testModule]!!
+
+            val languageVersionSettings = loadLanguageVersionSettings(testFilesInModule)
+            val moduleContext = context.withProject(project).withModule(oldModule)
+
+            val separateModules = groupedByModule.size == 1 && groupedByModule.keys.single() == null
+            val result = analyzeModuleContents(
+                moduleContext, ktFiles, NoScopeRecordCliBindingTrace(),
+                languageVersionSettings, separateModules, loadJvmTarget(testFilesInModule)
+            )
+            if (oldModule != result.moduleDescriptor) {
+                modules[testModule] = result.moduleDescriptor as ModuleDescriptorImpl
+                for (module in modules.values) {
+                    @Suppress("DEPRECATION")
+                    val it = (module.testOnly_AllDependentModules as MutableList).listIterator()
+                    while (it.hasNext()) {
+                        if (it.next() == oldModule) {
+                            it.set(result.moduleDescriptor as ModuleDescriptorImpl)
+                        }
+                    }
+                }
+            }
+
+            moduleBindings[testModule] = result.bindingContext
+        }
+
+        extractStructuresForModules(groupedByModule, modules, moduleBindings)
+    }
+
+    private fun extractStructuresForModules(
         moduleFiles: Map<TestModule?, List<TestFile>>,
         moduleDescriptors: Map<TestModule?, ModuleDescriptorImpl>,
         moduleBindings: Map<TestModule?, BindingContext>
@@ -28,11 +89,19 @@ abstract class AbstractIrAnalyzerTest: AbstractDiagnosticsTestWithStdLib() {
             val moduleDescriptor = moduleDescriptors[module] ?: continue
             val moduleBindingContext = moduleBindings[module] ?: continue
             val irModule = AbstractIrGeneratorTestCase.generateIrModule(ktFiles, moduleDescriptor, moduleBindingContext, true)
-            // here we have BindingContext and IrTree
-            val analyzer = functionUsageAnalyzer()
-            analyzer.execute(irModule, moduleDescriptor, moduleBindingContext)
-            println("------------")
+
+            checkOneSource(irModule, moduleDescriptor, moduleBindingContext)
         }
+    }
+
+    private fun checkOneSource(
+        irModule: IrModuleFragment,
+        moduleDescriptor: ModuleDescriptor,
+        bindingContext: BindingContext
+    ) {
+        val analyzer = functionUsageAnalyzer()
+        analyzer.execute(irModule, moduleDescriptor, bindingContext)
+        println("------------")
     }
 
     private fun functionUsageAnalyzer(): Analyzer =
