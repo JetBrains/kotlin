e937a350fe85f54dbc3889badf846369dff726ff
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-03-23 14:52:02 +0300

    Fix broken QF tests

--- idea/testData/quickfix/addAnnotationTarget/fromLib.kt
+++ idea/testData/quickfix/addAnnotationTarget/fromLib.kt
@@ -3,6 +3,7 @@
 // ACTION: Make internal
 // ACTION: Make private
 // ACTION: Specify type explicitly
+// ACTION: Add use-site target 'property'
 // ERROR: This annotation is not applicable to target 'top level property without backing field or delegate'
 
 <caret>@JvmField
