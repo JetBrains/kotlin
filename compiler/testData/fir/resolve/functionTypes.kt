c31513837bb8b4d56bea189042dc6aa1cfd4f79e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-03-23 20:21:59 +0300

    FIR: fix resolve of function type & its parameters in supertype position

--- compiler/testData/fir/resolve/functionTypes.kt
+++ compiler/testData/fir/resolve/functionTypes.kt
@@ -6,3 +6,11 @@ fun <T, R> List<T>.simpleMap(f: (T) -> R): R {
 
 fun <T> simpleWith(t: T, f: T.() -> Unit): Unit = t.f()
 
+interface KMutableProperty1<T, R> : KProperty1<T, R>, KMutableProperty<R>
+
+interface KProperty1<T, out R> : KProperty<R>, (T) -> R
+
+interface KProperty<out R>
+
+interface KMutableProperty<R>
+
