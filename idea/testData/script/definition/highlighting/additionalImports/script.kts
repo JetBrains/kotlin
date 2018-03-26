f8ad244899e22936123541f7e872cf250f43182a
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-03-26 14:26:52 +0300

    Rewrite AbstractScriptConfigurationTest to test file from project sources
    Changes in testData are needed to avoid running injection for string literals

--- idea/testData/script/definition/highlighting/additionalImports/script.kts
+++ idea/testData/script/definition/highlighting/additionalImports/script.kts
@@ -1,9 +1,9 @@
 val c = 3
 
-val g = listOf("a", "b", "c")
+val g = listOf(1, 2, 3)
 
 val time = measureNanoTime {
-    println("text")
+    println()
 }
 
 // DEPENDENCIES: classpath:runtime-classes; imports:kotlin.system.*
