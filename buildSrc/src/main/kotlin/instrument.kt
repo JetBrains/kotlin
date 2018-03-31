fbb5ada699a31fa1021e2fa6d51eb6e3a4d3327a
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2018-03-31 15:58:47 +0300

    `velocity`-dependency fix for idea 182

--- buildSrc/src/main/kotlin/instrument.kt
+++ buildSrc/src/main/kotlin/instrument.kt
@@ -55,7 +55,7 @@ fun Project.configureInstrumentation() {
     val instrumentationClasspathCfg = configurations.create("instrumentationClasspath")
 
     dependencies {
-        instrumentationClasspathCfg(intellijDep()) { includeJars("javac2", "jdom", "asm-all", "jgoodies-forms") }
+        instrumentationClasspathCfg(intellijDep()) { includeJars("javac2", "jdom", "asm-all") }
     }
 
     afterEvaluate {
