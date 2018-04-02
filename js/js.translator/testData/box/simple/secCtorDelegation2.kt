3ce324fa9d5daa4533f91ac9794d7f7ff7b51d63
Roman Artemev <Roman.Artemev@jetbrains.com>
2018-04-02 14:46:31 +0300

    Add tests for secondary constructors

---
+++ js/js.translator/testData/box/simple/secCtorDelegation2.kt
@@ -0,0 +1,36 @@
+// EXPECTED_REACHABLE_NODES: 1114
+package foo
+
+open class Base(val bs: String) {
+
+
+    val i: Int
+    val i2: Int
+    val i3: Int
+
+    fun foo() = bs
+
+    init {
+        i = 10
+        i2 = 20
+        i3 = 30
+    }
+}
+
+class Test : Base {
+
+    val t1: Int
+    val t2: Int
+
+    constructor(tt1: Int, tt2: Int): super("OK") {
+        t1 = tt1
+        t2 = tt2
+    }
+
+}
+
+fun box(): String {
+
+    val t = Test(1, 2)
+    return t.foo()
+}
