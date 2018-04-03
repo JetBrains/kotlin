7c0c1c81bc43e4c38f784b22863336b62cb3df0a
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-04-03 23:25:03 +0300

    Fix API wrapper in Android extensions

--- plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/model/impl/AndroidModuleInfoProviderImpl.kt
+++ plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/model/impl/AndroidModuleInfoProviderImpl.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.android.model.impl
 import com.android.builder.model.SourceProvider
 import com.android.tools.idea.gradle.project.GradleProjectInfo
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel
+import com.android.tools.idea.res.AppResourceRepository
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.android.facet.AndroidFacet
@@ -35,7 +36,7 @@ class AndroidModuleInfoProviderImpl(override val module: Module) : AndroidModule
     }
 
     override fun getApplicationResourceDirectories(createIfNecessary: Boolean): Collection<VirtualFile> {
-        return androidFacet?.getAppResources(createIfNecessary)?.resourceDirs ?: emptyList()
+        return AppResourceRepository.findExistingInstance(module)?.resourceDirs ?: emptyList()
     }
 
     override fun getAllSourceProviders(): List<AndroidModuleInfoProvider.SourceProviderMirror> {
