e05b54a2b47895d11192903362d21d402e1c6638
Dmitry Jemerov <yole@jetbrains.com>
2018-04-03 20:06:17 +0200

    Fix unsupported syntax in code style settings example

--- idea/src/org/jetbrains/kotlin/idea/formatter/KotlinLanguageCodeStyleSettingsProvider.kt
+++ idea/src/org/jetbrains/kotlin/idea/formatter/KotlinLanguageCodeStyleSettingsProvider.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.formatter
@@ -51,7 +40,7 @@ class KotlinLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvide
                // todo: something
                        try {            return foo1(12, 13, 14)
                        }        catch (e: Exception) {            return 0        }        finally {           if (true) {               return 1           }           else {               return 2           }        }    }
-                   private val f = {(a: Int)->a*2}
+                   private val f = {a: Int->a*2}
 
                    fun longMethod(@Named("param1") param1: Int,
                     param2: String) {
@@ -137,7 +126,7 @@ class KotlinLanguageCodeStyleSettingsProvider : LanguageCodeStyleSettingsProvide
                }""".trimIndent()
 
         else -> """open class Some {
-                       private val f: (Int)->Int = { (a: Int) -> a * 2 }
+                       private val f: (Int)->Int = { a: Int -> a * 2 }
                        fun foo(): Int {
                            val test: Int = 12
                            for (i in 10..42) {
