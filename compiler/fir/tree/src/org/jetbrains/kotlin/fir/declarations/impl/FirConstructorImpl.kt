78d45f3f907f45b0b536ee0b78b7e03c065a04cb
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-04-03 21:59:08 +0300

    FIR: resolve constructor delegated calls (types are all error yet)

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/impl/FirConstructorImpl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/impl/FirConstructorImpl.kt
@@ -7,15 +7,25 @@ package org.jetbrains.kotlin.fir.declarations.impl
 
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.descriptors.Visibility
+import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirConstructor
 import org.jetbrains.kotlin.fir.expressions.FirBody
 import org.jetbrains.kotlin.fir.expressions.FirDelegatedConstructorCall
+import org.jetbrains.kotlin.fir.transformSingle
+import org.jetbrains.kotlin.fir.visitors.FirTransformer
 
 open class FirConstructorImpl(
     session: FirSession,
     psi: PsiElement?,
     final override val visibility: Visibility,
-    final override val delegatedConstructor: FirDelegatedConstructorCall?,
+    final override var delegatedConstructor: FirDelegatedConstructorCall?,
     body: FirBody?
-) : FirAbstractFunction(session, psi, body), FirConstructor
+) : FirAbstractFunction(session, psi, body), FirConstructor {
+
+    override fun <D> transformChildren(transformer: FirTransformer<D>, data: D): FirElement {
+        delegatedConstructor = delegatedConstructor?.transformSingle(transformer, data)
+
+        return super<FirAbstractFunction>.transformChildren(transformer, data)
+    }
+}
