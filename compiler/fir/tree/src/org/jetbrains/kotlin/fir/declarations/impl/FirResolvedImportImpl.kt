34d7eb11b767216c8c4e1079f196d61cd3e27fd1
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2018-04-11 00:01:18 +0300

    FIR: Get rid of delegation in resolved imports
    This prevents strange visitors behaviour

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/impl/FirResolvedImportImpl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/impl/FirResolvedImportImpl.kt
@@ -5,22 +5,36 @@
 
 package org.jetbrains.kotlin.fir.declarations.impl
 
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirImport
 import org.jetbrains.kotlin.fir.declarations.FirResolvedImport
-import org.jetbrains.kotlin.fir.visitors.FirVisitor
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
 
 class FirResolvedImportImpl(
     val delegate: FirImport,
     override val resolvedFqName: ClassId
-) : FirResolvedImport, FirImport by delegate {
+) : FirResolvedImport, FirImport {
     override val packageFqName: FqName
         get() = resolvedFqName.packageFqName
 
     override val relativeClassName: FqName
         get() = resolvedFqName.relativeClassName
 
-    override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R =
-        visitor.visitResolvedImport(this, data)
+    override val aliasName: Name?
+        get() = delegate.aliasName
+
+    override val importedFqName: FqName?
+        get() = delegate.importedFqName
+
+    override val isAllUnder: Boolean
+        get() = delegate.isAllUnder
+
+    override val psi: PsiElement?
+        get() = delegate.psi
+
+    override val session: FirSession
+        get() = delegate.session
 }
