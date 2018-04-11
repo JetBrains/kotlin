a2f3e633c29228838c70eceddc79583d7e931739
Pavel Punegov <32519625+PavelPunegov@users.noreply.github.com>
2018-04-11 14:24:12 +0300

    Fix debugger tests

--- backend.native/debugger-tests/src/main/kotlin/org/jetbrains/kotlin/compiletest/DistProperties.kt
+++ backend.native/debugger-tests/src/main/kotlin/org/jetbrains/kotlin/compiletest/DistProperties.kt
@@ -8,7 +8,7 @@ import java.nio.file.Paths
 
 object DistProperties {
     private val dist: Path = Paths.get(requireProp("konan.home"))
-    private val konancDriver = if (HostManager.host.family == Family.WINDOWS) "konanc.bat" else "konanc"
+    private val konancDriver = if (HostManager.host.family == Family.MINGW) "konanc.bat" else "konanc"
     val konanc: Path = dist.resolve("bin/$konancDriver")
     val lldb: Path = Paths.get("lldb")
     val lldbPrettyPrinters: Path = dist.resolve("tools/konan_lldb.py")
