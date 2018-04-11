d438988fc6f1bad26909ef3678503038b598f611
Dmitry Savvinov <dsavvinov@gmail.com>
2018-04-11 16:03:19 +0300

    Fix javadoc for StorageManager.createLazyValueWithPostCompute

--- core/util.runtime/src/org/jetbrains/kotlin/storage/LockBasedStorageManager.java
+++ core/util.runtime/src/org/jetbrains/kotlin/storage/LockBasedStorageManager.java
@@ -290,7 +290,18 @@ public class LockBasedStorageManager implements StorageManager {
         RECURSION_WAS_DETECTED
     }
 
-    // Being static is memory optimization to prevent capturing outer-class reference at each level of inheritance hierarchy
+    /**
+     * Important thread-safety note!
+     *
+     * This implementation publishes value **BEFORE** calling postCompute on it.
+     *
+     * It means that thread-safety of actions in postCompute() and recursion prevention
+     * rely *solely* on the `storageManager.lock()`.
+     *
+     * And yes, there are a LockBasedStorageManager.NO_LOCKS, which doesn't have lock at all,
+     * so if you have some `StorageManager` (or even if it is an instanceof `LockBasedStorageManager`),
+     * thread-safety of produced lazy values still not guaranteed.
+     */
     private static class LockBasedLazyValue<T> implements NullableLazyValue<T> {
         private final LockBasedStorageManager storageManager;
         private final Function0<? extends T> computable;
