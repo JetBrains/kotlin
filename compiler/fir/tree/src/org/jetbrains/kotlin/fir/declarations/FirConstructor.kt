65d89a61bf54616d85f98421afa2ffcba0fe0e2d
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-04-13 17:27:58 +0300

    FIR: constructor refactoring, now there are callable members

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/FirConstructor.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/FirConstructor.kt
@@ -5,23 +5,20 @@
 
 package org.jetbrains.kotlin.fir.declarations
 
-import org.jetbrains.kotlin.descriptors.Visibility
 import org.jetbrains.kotlin.fir.BaseTransformedType
-import org.jetbrains.kotlin.fir.expressions.FirAnnotationContainer
+import org.jetbrains.kotlin.fir.VisitedSupertype
 import org.jetbrains.kotlin.fir.expressions.FirDelegatedConstructorCall
 import org.jetbrains.kotlin.fir.visitors.FirVisitor
 
 @BaseTransformedType
-interface FirConstructor : FirFunction, FirAnnotationContainer {
+interface FirConstructor : @VisitedSupertype FirFunction, FirCallableMember {
     val delegatedConstructor: FirDelegatedConstructorCall?
-
-    val visibility: Visibility
 
     override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R =
         visitor.visitConstructor(this, data)
 
     override fun <R, D> acceptChildren(visitor: FirVisitor<R, D>, data: D) {
-        acceptAnnotations(visitor, data)
+        super<FirCallableMember>.acceptChildren(visitor, data)
         delegatedConstructor?.accept(visitor, data)
         for (parameter in valueParameters) {
             parameter.accept(visitor, data)
--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/impl/FirConstructorImpl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/impl/FirConstructorImpl.kt
@@ -6,26 +6,43 @@
 package org.jetbrains.kotlin.fir.declarations.impl
 
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Visibility
 import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirConstructor
+import org.jetbrains.kotlin.fir.declarations.FirMemberPlatformStatus
+import org.jetbrains.kotlin.fir.declarations.FirValueParameter
 import org.jetbrains.kotlin.fir.expressions.FirBody
 import org.jetbrains.kotlin.fir.expressions.FirDelegatedConstructorCall
+import org.jetbrains.kotlin.fir.transformInplace
 import org.jetbrains.kotlin.fir.transformSingle
+import org.jetbrains.kotlin.fir.types.FirType
 import org.jetbrains.kotlin.fir.visitors.FirTransformer
+import org.jetbrains.kotlin.name.Name
 
 open class FirConstructorImpl(
     session: FirSession,
     psi: PsiElement?,
-    final override val visibility: Visibility,
+    visibility: Visibility,
+    platformStatus: FirMemberPlatformStatus,
+    delegatedSelfType: FirType,
     final override var delegatedConstructor: FirDelegatedConstructorCall?,
-    body: FirBody?
-) : FirAbstractFunction(session, psi, body), FirConstructor {
+    override val body: FirBody?
+) : FirAbstractCallableMember(
+    session, psi, NAME, visibility, Modality.FINAL,
+    platformStatus, false, null, delegatedSelfType
+), FirConstructor {
+    override val valueParameters = mutableListOf<FirValueParameter>()
 
     override fun <D> transformChildren(transformer: FirTransformer<D>, data: D): FirElement {
-        delegatedConstructor = delegatedConstructor?.transformSingle(transformer, data)
+        valueParameters.transformInplace(transformer, data)
+        delegatedConstructor?.transformSingle(transformer, data)
 
-        return super<FirAbstractFunction>.transformChildren(transformer, data)
+        return super<FirAbstractCallableMember>.transformChildren(transformer, data)
+    }
+
+    companion object {
+        val NAME = Name.special("<init>")
     }
 }
