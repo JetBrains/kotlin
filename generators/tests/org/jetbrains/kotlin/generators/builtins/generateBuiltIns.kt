d3156c33be09fae459df1247476a401605ae92ae
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-04-17 23:21:00 +0300

    Update copyrights in builtin sources
    Read copyright template from .idea/copyright/apache.xml profile

--- generators/tests/org/jetbrains/kotlin/generators/builtins/generateBuiltIns.kt
+++ generators/tests/org/jetbrains/kotlin/generators/builtins/generateBuiltIns.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.generators.builtins.generateBuiltIns
@@ -24,8 +13,10 @@ import org.jetbrains.kotlin.generators.builtins.progressionIterators.GeneratePro
 import org.jetbrains.kotlin.generators.builtins.progressions.GenerateProgressions
 import org.jetbrains.kotlin.generators.builtins.ranges.GeneratePrimitives
 import org.jetbrains.kotlin.generators.builtins.ranges.GenerateRanges
+import org.xml.sax.InputSource
 import java.io.File
 import java.io.PrintWriter
+import javax.xml.xpath.XPathFactory
 
 fun assertExists(file: File) {
     if (!file.exists()) error("Output dir does not exist: ${file.absolutePath}")
@@ -46,7 +37,7 @@ abstract class BuiltInsSourceGenerator(val out: PrintWriter) {
     }
 
     fun generate() {
-        out.println(File("license/LICENSE.txt").readText())
+        out.println(readCopyrightNoticeFromProfile(File(".idea/copyright/apache.xml")))
         // Don't include generator class name in the message: these are built-in sources,
         // and we don't want to scare users with any internal information about our project
         out.println("// Auto-generated file. DO NOT EDIT!")
@@ -57,6 +48,17 @@ abstract class BuiltInsSourceGenerator(val out: PrintWriter) {
 
         generateBody()
     }
+}
+
+fun readCopyrightNoticeFromProfile(copyrightProfile: File): String {
+    val template = copyrightProfile.reader().use { reader ->
+        XPathFactory.newInstance().newXPath().evaluate("/component/copyright/option[@name='notice']/@value", InputSource(reader))
+    }
+    val yearTemplate = "&#36;today.year"
+    val year = java.time.LocalDate.now().year.toString()
+    assert(yearTemplate in template)
+
+    return template.replace(yearTemplate, year).lines().joinToString("", prefix = "/*\n", postfix = " */\n") { " * $it\n" }
 }
 
 fun generateBuiltIns(generate: (File, (PrintWriter) -> BuiltInsSourceGenerator) -> Unit) {
