cdb49511e6a7a4a6c4257063694a67de9b42431d
Pavel V. Talanov <talanov.pavel@gmail.com>
2018-04-19 12:23:45 +0200

    Mpp highlighting test: make generated, adjust test data
    Note: this spawns new test cases which were ignored previously

--- idea/testData/multiModuleHighlighting/multiplatform/basic/jvm/jvm.kt
+++ idea/testData/multiModuleHighlighting/multiplatform/basic/jvm/jvm.kt
@@ -2,7 +2,7 @@ actual class <error descr="[ACTUAL_WITHOUT_EXPECT] Actual class 'Your' has no co
 
 }
 
-expect class <error descr="[NO_ACTUAL_FOR_EXPECT] Expected class 'His' has no actual declaration in module jvm for JVM">His</error> {
+expect class <error descr="[NO_ACTUAL_FOR_EXPECT] Expected class 'His' has no actual declaration in module testModule_JVM for JVM">His</error> {
 
 }
 
