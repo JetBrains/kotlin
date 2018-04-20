cf31e544eb8204c946ce095aa47717d27c23619a
Roman Artemev <roman.artemev@jetbrains.com>
2018-04-20 19:07:32 +0300

    Implement default interface members

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsClassGenerator.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsClassGenerator.kt
@@ -6,22 +6,27 @@
 package org.jetbrains.kotlin.ir.backend.js.transformers.irToJs
 
 import org.jetbrains.kotlin.backend.common.onlyIf
-import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.ClassKind
-import org.jetbrains.kotlin.descriptors.Modality
-import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.ir.backend.js.utils.*
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
-import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.symbols.impl.IrClassSymbolImpl
+import org.jetbrains.kotlin.ir.util.isReal
+import org.jetbrains.kotlin.ir.util.resolveFakeOverride
 import org.jetbrains.kotlin.js.backend.ast.*
 
 class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationContext) {
 
     private val className = context.getNameForSymbol(irClass.symbol)
     private val classNameRef = className.makeRef()
+    private val baseClass = irClass.superClasses.firstOrNull { it.kind != ClassKind.INTERFACE }
+    private val baseClassName = baseClass?.let { context.getNameForSymbol(it) }
     private val classPrototypeRef = prototypeOf(classNameRef)
     private val classBlock = JsBlock()
+    private val classModel = JsClassModel(className, baseClassName)
 
     fun generate(): JsStatement {
 
@@ -29,32 +34,60 @@ class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationCo
         val transformer = IrFunctionToJsTransformer()
 
         for (declaration in irClass.declarations) {
-            if (declaration is IrConstructor) {
-                classBlock.statements += declaration.accept(transformer, context).makeStmt()
-                classBlock.statements += generateInheritanceCode()
-            } else if (declaration is IrFunction) {
-                if (declaration.symbol.kind != CallableMemberDescriptor.Kind.FAKE_OVERRIDE &&
-                    declaration.symbol.modality != Modality.ABSTRACT
-                ) {
-                    classBlock.statements += declaration.accept(transformer, context).let {
-                        if (declaration.isStatic) {
-                            it.makeStmt()
-                        } else {
-                            val memberName = context.getNameForSymbol(declaration.symbol)
-                            val memberRef = JsNameRef(memberName, classPrototypeRef)
-                            jsAssignment(memberRef, it).makeStmt()
-                        }
-                    }
+            when (declaration) {
+                is IrConstructor -> {
+                    classBlock.statements += declaration.accept(transformer, context).makeStmt()
+                    classBlock.statements += generateInheritanceCode()
                 }
-            } else if (declaration is IrClass) {
-                //TODO: redesign inner classes generation
-                classBlock.statements += JsClassGenerator(declaration, context).generate()
+                is IrSimpleFunction -> {
+                    generateMemberFunction(declaration)?.let { classBlock.statements += it }
+                }
+                is IrClass -> {
+                    classBlock.statements += JsClassGenerator(declaration, context).generate()
+                }
+                else -> {
+                }
             }
         }
 
         classBlock.statements += generateClassMetadata()
         irClass.onlyIf({ kind == ClassKind.OBJECT }) { classBlock.statements += maybeGenerateObjectInstance() }
+        context.staticContext.classModels[className] = classModel
         return classBlock
+    }
+
+    private fun generateMemberFunction(declaration: IrSimpleFunction): JsStatement? {
+
+        val translatedFunction = declaration.run { if (isReal) accept(IrFunctionToJsTransformer(), context) else null }
+        if (declaration.isStatic) {
+            return translatedFunction!!.makeStmt()
+        }
+
+        val memberName = context.getNameForSymbol(declaration.symbol)
+        val memberRef = JsNameRef(memberName, classPrototypeRef)
+
+        translatedFunction?.let { return jsAssignment(memberRef, it).makeStmt() }
+
+        // do not generate code like
+        // interface I { foo() = "OK" }
+        // interface II : I
+        // II.prototype.foo = I.prototype.foo
+        if (irClass.kind != ClassKind.INTERFACE) {
+            declaration.resolveFakeOverride()?.let {
+                val implClassDesc = it.descriptor.containingDeclaration as ClassDescriptor
+                if (!KotlinBuiltIns.isAny(implClassDesc)) {
+                    val implMethodName = context.getNameForSymbol(it.symbol)
+                    val implClassName = context.getNameForSymbol(IrClassSymbolImpl(implClassDesc))
+
+                    val implClassPrototype = prototypeOf(implClassName.makeRef())
+                    val implMemberRef = JsNameRef(implMethodName, implClassPrototype)
+
+                    classModel.postDeclarationBlock.statements += jsAssignment(memberRef, implMemberRef).makeStmt()
+                }
+            }
+        }
+
+        return null
     }
 
     private fun maybeGenerateObjectInstance(): List<JsStatement> {
@@ -90,8 +123,7 @@ class JsClassGenerator(private val irClass: IrClass, val context: JsGenerationCo
     }
 
     private fun generateInheritanceCode(): List<JsStatement> {
-        val baseClass = irClass.superClasses.first { it.kind != ClassKind.INTERFACE }
-        if (baseClass.isAny) {
+        if (baseClass == null || baseClass.isAny) {
             return emptyList()
         }
 
--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/JsStaticContext.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/JsStaticContext.kt
@@ -8,7 +8,9 @@ package org.jetbrains.kotlin.ir.backend.js.utils
 import org.jetbrains.kotlin.ir.backend.js.JsIrBackendContext
 import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.JsIntrinsicTransformers
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
+import org.jetbrains.kotlin.js.backend.ast.JsClassModel
 import org.jetbrains.kotlin.js.backend.ast.JsGlobalBlock
+import org.jetbrains.kotlin.js.backend.ast.JsName
 import org.jetbrains.kotlin.js.backend.ast.JsRootScope
 
 
@@ -19,6 +21,7 @@ class JsStaticContext(
     backendContext: JsIrBackendContext
 ) {
     val intrinsics = JsIntrinsicTransformers(backendContext)
+    val classModels = mutableMapOf<JsName, JsClassModel>()
 
     fun getNameForSymbol(irSymbol: IrSymbol, context: JsGenerationContext) = nameGenerator.getNameForSymbol(irSymbol, context)
 }
