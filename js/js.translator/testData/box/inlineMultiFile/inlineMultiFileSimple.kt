34587d91929682c2c716b8102d0a8bdbdc498a2d
Zalim Bashorov <Zalim.Bashorov@jetbrains.com>
2018-04-23 22:22:09 +0300

    [JS IR BE] unmute tests

--- js/js.translator/testData/box/inlineMultiFile/inlineMultiFileSimple.kt
+++ js/js.translator/testData/box/inlineMultiFile/inlineMultiFileSimple.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JS_IR
 // EXPECTED_REACHABLE_NODES: 1112
 // FILE: a.kt
 package foo
--- js/js.translator/testData/box/jsCode/numberLiteralOverflow.kt
+++ js/js.translator/testData/box/jsCode/numberLiteralOverflow.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JS_IR
 // EXPECTED_REACHABLE_NODES: 1108
 fun box(): String {
     val a = js("0xff000000")
