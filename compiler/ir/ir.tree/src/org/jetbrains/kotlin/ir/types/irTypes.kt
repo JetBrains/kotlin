74ce29122fb1c51221517ade8531870f5f00d0c9
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-04-27 14:09:11 +0300

    IrTypes: basic built-in types

---
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/types/irTypes.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.ir.types
+
+import org.jetbrains.kotlin.ir.symbols.IrClassifierSymbol
+import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
+import org.jetbrains.kotlin.utils.addToStdlib.cast
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+
+fun IrType.withHasQuestionMark(hasQuestionMark: Boolean): IrType =
+    when (this) {
+        is IrSimpleType ->
+            if (this.hasQuestionMark == hasQuestionMark)
+                this
+            else
+                IrSimpleTypeImpl(classifier, hasQuestionMark, arguments, annotations)
+        else -> this
+    }
+
+val IrType.classifierOrFail: IrClassifierSymbol
+    get() = cast<IrSimpleType>().classifier
+
+val IrType.classifierOrNull: IrClassifierSymbol?
+    get() = safeAs<IrSimpleType>()?.classifier
