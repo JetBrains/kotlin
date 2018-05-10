064bc723dfc739711389b04e15cc31b34bcb9085
Dmitry Savvinov <dsavvinov@gmail.com>
2018-05-10 15:14:13 +0300

    jvmOverloads

--- compiler/testData/asJava/lightClasses/nullabilityAnnotations/JvmOverloads.java
+++ compiler/testData/asJava/lightClasses/nullabilityAnnotations/JvmOverloads.java
@@ -1,15 +1,7 @@
 public final class C {
-    @kotlin.jvm.JvmOverloads
-    @org.jetbrains.annotations.NotNull
-    public final java.lang.String foo(@org.jetbrains.annotations.NotNull java.lang.String o, @org.jetbrains.annotations.NotNull java.lang.String o1, @org.jetbrains.annotations.NotNull java.lang.String o3, @org.jetbrains.annotations.Nullable java.lang.String o4) { /* compiled code */ }
-
-    @kotlin.jvm.JvmOverloads
-    @org.jetbrains.annotations.NotNull
-    public final java.lang.String foo(@org.jetbrains.annotations.NotNull java.lang.String o, @org.jetbrains.annotations.NotNull java.lang.String o1, @org.jetbrains.annotations.Nullable java.lang.String o4) { /* compiled code */ }
-
-    @kotlin.jvm.JvmOverloads
-    @org.jetbrains.annotations.NotNull
-    public final java.lang.String foo(@org.jetbrains.annotations.NotNull java.lang.String o1, @org.jetbrains.annotations.Nullable java.lang.String o4) { /* compiled code */ }
-
     public C() { /* compiled code */ }
+
+    @kotlin.jvm.JvmOverloads
+    @org.jetbrains.annotations.NotNull
+    public final java.lang.String foo(@org.jetbrains.annotations.NotNull java.lang.String p, @org.jetbrains.annotations.NotNull java.lang.String p1, @org.jetbrains.annotations.NotNull java.lang.String p2, @org.jetbrains.annotations.Nullable java.lang.String s3) { /* compiled code */ }
 }
