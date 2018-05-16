da7b59984fdf55abe6708698f305f69689e533a0
Dmitry Savvinov <dsavvinov@gmail.com>
2018-05-16 16:25:29 +0300

    Move importsDumper to compiler plugin

---
+++ plugins/imports-dumper/build.gradle.kts
@@ -0,0 +1,39 @@
+
+description = "Extension for saving imports of .kt-files in JSON"
+
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+val kotlinxSerializationVersion = "0.4.2"
+
+dependencies {
+    compile(project(":compiler:frontend.java"))
+    compile(project(":compiler:plugin-api"))
+    compileOnly("org.jetbrains.kotlinx", "kotlinx-serialization-runtime", kotlinxSerializationVersion) { isTransitive = false }
+
+    compile(intellijCoreDep()) { includeJars("intellij-core") }
+
+    testCompile(project(":compiler:tests-common"))
+    testCompile(projectTests(":compiler:tests-common"))
+
+
+    embeddedComponents("org.jetbrains.kotlinx", "kotlinx-serialization-runtime", kotlinxSerializationVersion) { isTransitive = false }
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" { projectDefault() }
+}
+
+projectTest {
+    workingDir = rootDir
+    dependsOn(":dist")
+}
+
+runtimeJar {
+    fromEmbeddedComponents()
+}
+
+dist()
