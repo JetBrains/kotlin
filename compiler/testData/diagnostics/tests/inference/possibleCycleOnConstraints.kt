e613ff8c194e17e71cad1a0a20e198e653c9daa9
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2018-05-19 16:24:54 +0300

    [NI] Update test data

--- compiler/testData/diagnostics/tests/inference/possibleCycleOnConstraints.kt
+++ compiler/testData/diagnostics/tests/inference/possibleCycleOnConstraints.kt
@@ -7,8 +7,8 @@ fun <T> g (<!UNUSED_PARAMETER!>f<!>: () -> List<T>) : T {<!NO_RETURN_IN_FUNCTION
 
 fun test() {
     //here possibly can be a cycle on constraints
-    <!NI;UNREACHABLE_CODE!>val <!UNUSED_VARIABLE!>x<!> =<!> g { Collections.<!OI;TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>emptyList<!>() }
+    val <!UNUSED_VARIABLE!>x<!> = g { Collections.<!OI;TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>emptyList<!>() }
 
-    <!NI;UNREACHABLE_CODE!>val <!OI;UNUSED_VARIABLE!>y<!> = g<Int> { Collections.emptyList() }<!>
-    <!NI;UNREACHABLE_CODE!>val <!OI;UNUSED_VARIABLE!>z<!> : List<Int> = g { Collections.<!OI;TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>emptyList<!>() }<!>
+    val <!UNUSED_VARIABLE!>y<!> = g<Int> { Collections.emptyList() }
+    val <!UNUSED_VARIABLE!>z<!> : List<Int> = g { Collections.<!OI;TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>emptyList<!>() }
 }
