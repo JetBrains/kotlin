2f84a22f511e5bc80448aa340133210c324c5d50
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-05-20 18:36:47 +0300

    [ABI Validation] bcv: use kotlinx-metadata-jvm to read kotlin visibilities
    It's no longer required to dump kotlin-to-java declaration mapping to json
    before using binary-compatibility-validator.
    
    @JvmOverloads are not supported yet, so remove them from test.
    
    Use asm:6.0 in bcv
    
    Moved from https://github.com/Kotlin/binary-compatibility-validator/commit/502d73caf503658a10fba1e8d27019249c6903d2

--- libraries/tools/abi-validation/src/test/kotlin/cases/default/jvmOverloads.kt
+++ libraries/tools/abi-validation/src/test/kotlin/cases/default/jvmOverloads.kt
@@ -8,6 +8,9 @@
 package cases.default
 
 
+/* JvmOverloads are not supported by metadata reader
+
+
 @JvmOverloads
 public fun publicFunWithOverloads(a: Int = 0, b: String? = null) {}
 
@@ -22,3 +25,21 @@ internal constructor(val a: Int = 0, val b: String? = null) {
     internal fun internalFunWithOverloads(a: Int = 0, b: String? = null) {}
 
 }
+
+*/
+
+/* expected:
+
+public final class cases/default/JvmOverloadsClass {
+	public final fun getA ()I
+	public final fun getB ()Ljava/lang/String;
+}
+
+public final class cases/default/JvmOverloadsKt {
+	public static final fun publicFunWithOverloads ()V
+	public static final fun publicFunWithOverloads (I)V
+	public static final fun publicFunWithOverloads (ILjava/lang/String;)V
+	public static synthetic fun publicFunWithOverloads$default (ILjava/lang/String;ILjava/lang/Object;)V
+}
+
+ */
