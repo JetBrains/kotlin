a16949a7c8b7efef31cdd2d4a434f08b40378907
Neonailol <neonailol@gmail.com>
2018-05-21 18:40:13 +0300

    KT-23932 add "PointerEvent" for kotlin-stdlib-js

--- libraries/stdlib/js/src/org.w3c/org.w3c.dom.events.kt
+++ libraries/stdlib/js/src/org.w3c/org.w3c.dom.events.kt
@@ -9,19 +9,9 @@
 @file:Suppress("NESTED_CLASS_IN_EXTERNAL_INTERFACE")
 package org.w3c.dom.events
 
-import kotlin.js.*
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.css.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
+import org.w3c.dom.EventInit
+import org.w3c.dom.UnionElementOrMouseEvent
+import org.w3c.dom.Window
 
 /**
  * Exposes the JavaScript [UIEvent](https://developer.mozilla.org/en/docs/Web/API/UIEvent) to Kotlin
@@ -483,3 +473,133 @@ public external interface EventListener {
     fun handleEvent(event: Event): Unit
 }
 
+/**
+ * Exposes the JavaScript [PointerEvent](https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent) to Kotlin
+ */
+public external open class PointerEvent(type: String, eventInitDict: PointerEventInit = definedExternally) : MouseEvent {
+    open val pointerId: Int
+    open val width: Int
+    open val height: Int
+    open val pressure: Double
+    open val tangentialPressure: Double
+    open val tiltX: Int
+    open val tiltY: Int
+    open val twist: Int
+    open val pointerType: String
+    open val isPrimary: Boolean
+    fun getCoalescedEvents(): Array<PointerEvent>
+}
+
+public external interface PointerEventInit : MouseEventInit {
+    var pointerId: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+    var width: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+    var height: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+    var pressure: Double?
+        get() = definedExternally
+        set(value) = definedExternally
+    var tangentialPressure: Double?
+        get() = definedExternally
+        set(value) = definedExternally
+    var tiltX: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+    var tiltY: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+    var twist: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+    var pointerType: String?
+        get() = definedExternally
+        set(value) = definedExternally
+    var isPrimary: Boolean?
+        get() = definedExternally
+        set(value) = definedExternally
+}
+
+@kotlin.internal.InlineOnly
+public inline fun PointerEventInit(
+    pointerId: Int? = 0,
+    width: Int? = 0,
+    height: Int? = 0,
+    pressure: Double? = 0.0,
+    tangentialPressure: Double? = 0.0,
+    tiltX: Int? = 0,
+    tiltY: Int? = 0,
+    twist: Int? = 0,
+    pointerType: String? = "",
+    isPrimary: Boolean? = false,
+    screenX: Int? = 0,
+    screenY: Int? = 0,
+    clientX: Int? = 0,
+    clientY: Int? = 0,
+    button: Short? = 0,
+    buttons: Short? = 0,
+    relatedTarget: EventTarget? = null,
+    ctrlKey: Boolean? = false,
+    shiftKey: Boolean? = false,
+    altKey: Boolean? = false,
+    metaKey: Boolean? = false,
+    modifierAltGraph: Boolean? = false,
+    modifierCapsLock: Boolean? = false,
+    modifierFn: Boolean? = false,
+    modifierFnLock: Boolean? = false,
+    modifierHyper: Boolean? = false,
+    modifierNumLock: Boolean? = false,
+    modifierScrollLock: Boolean? = false,
+    modifierSuper: Boolean? = false,
+    modifierSymbol: Boolean? = false,
+    modifierSymbolLock: Boolean? = false,
+    view: Window? = null,
+    detail: Int? = 0,
+    bubbles: Boolean? = false,
+    cancelable: Boolean? = false,
+    composed: Boolean? = false
+): PointerEventInit {
+    val o = js("({})")
+
+    o["pointerId"] = pointerId
+    o["width"] = width
+    o["height"] = height
+    o["pressure"] = pressure
+    o["tangentialPressure"] = tangentialPressure
+    o["tiltX"] = tiltX
+    o["tiltY"] = tiltY
+    o["twist"] = twist
+    o["pointerType"] = pointerType
+    o["isPrimary"] = isPrimary
+    o["screenX"] = screenX
+    o["screenY"] = screenY
+    o["clientX"] = clientX
+    o["clientY"] = clientY
+    o["button"] = button
+    o["buttons"] = buttons
+    o["relatedTarget"] = relatedTarget
+    o["ctrlKey"] = ctrlKey
+    o["shiftKey"] = shiftKey
+    o["altKey"] = altKey
+    o["metaKey"] = metaKey
+    o["modifierAltGraph"] = modifierAltGraph
+    o["modifierCapsLock"] = modifierCapsLock
+    o["modifierFn"] = modifierFn
+    o["modifierFnLock"] = modifierFnLock
+    o["modifierHyper"] = modifierHyper
+    o["modifierNumLock"] = modifierNumLock
+    o["modifierScrollLock"] = modifierScrollLock
+    o["modifierSuper"] = modifierSuper
+    o["modifierSymbol"] = modifierSymbol
+    o["modifierSymbolLock"] = modifierSymbolLock
+    o["view"] = view
+    o["detail"] = detail
+    o["bubbles"] = bubbles
+    o["cancelable"] = cancelable
+    o["composed"] = composed
+
+    return o
+}
