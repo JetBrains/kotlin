cb2aa9eddc7ef3f72e701b02165951043dd1e9b0
Paul de Vrieze <paul.devrieze@gmail.com>
2018-05-22 13:14:22 +0100

    Create a version that actually passes a type parameter in write$Self. This version should take the correct type parameters from the class rather than adding a function type parameter shared with all serializers.

--- plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/resolve/KSerializerDescriptorResolver.kt
+++ plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/resolve/KSerializerDescriptorResolver.kt
@@ -366,6 +366,7 @@ object KSerializerDescriptorResolver {
 
         val args = mutableListOf<ValueParameterDescriptor>()
         var i = 0
+
         args.add(ValueParameterDescriptorImpl(
                 f,
                 null,
@@ -394,9 +395,13 @@ object KSerializerDescriptorResolver {
                 f.source)
         )
 
-        val kSerialClass = thisClass.getClassFromSerializationPackage("KSerializer").toSimpleType(false)
+        val kSerialClassDesc = thisClass.getClassFromSerializationPackage("KSerializer")
 
         thisClass.declaredTypeParameters.forEach {
+            val typeArgument = TypeProjectionImpl(it.defaultType)
+            val kSerialClass = KotlinTypeFactory.simpleNotNullType(Annotations.EMPTY, kSerialClassDesc, listOf(typeArgument))
+
+
             args.add(ValueParameterDescriptorImpl(
                     f,
                     null,
