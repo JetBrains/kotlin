03b9a18328177c47715a79a9404f846d992d3a7d
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2018-05-23 14:39:00 +0300

    jps: support dev kit module types (and all other ModuleBasedBuildTargetType)
    #KT-24500 fixed
    
    (cherry picked from commit ec5110e)

--- jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/KotlinJpsBuildTest.kt
+++ jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/KotlinJpsBuildTest.kt
@@ -35,6 +35,7 @@ import org.jetbrains.jps.builders.TestProjectBuilderLogger
 import org.jetbrains.jps.builders.impl.BuildDataPathsImpl
 import org.jetbrains.jps.builders.logging.BuildLoggingManager
 import org.jetbrains.jps.cmdline.ProjectDescriptor
+import org.jetbrains.jps.devkit.model.JpsPluginModuleType
 import org.jetbrains.jps.incremental.BuilderRegistry
 import org.jetbrains.jps.incremental.CompileContext
 import org.jetbrains.jps.incremental.IncProjectBuilder
@@ -675,6 +676,12 @@ open class KotlinJpsBuildTest : AbstractKotlinJpsBuildTestCase() {
 
         val result = buildAllModules()
         result.assertSuccessful()
+    }
+
+    fun testDevKitProject() {
+        initProject(JVM_MOCK_RUNTIME)
+        assertEquals(myProject.modules.single().moduleType, JpsPluginModuleType.INSTANCE)
+        buildAllModules().assertSuccessful()
     }
 
     fun testAccessToInternalInProductionFromTests() {
