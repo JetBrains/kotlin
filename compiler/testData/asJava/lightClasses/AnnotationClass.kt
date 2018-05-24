af2d7be81cbd0801042a5fe76cc6cf6d1144bcc8
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-05-24 13:47:47 +0300

    ~ tests

--- compiler/testData/asJava/lightClasses/AnnotationClass.kt
+++ compiler/testData/asJava/lightClasses/AnnotationClass.kt
@@ -1,10 +1,7 @@
 // Anno
 
-annotation class Anno(
-        val i: Int,
-        val j: Int = 5,
-        val value: String = "a",
-        val d: Double = 0.0,
-        val ia: IntArray,
-        val ia2: IntArray = intArrayOf(1, 2, 3)
-)
+class Anno {
+        fun foo() = 1
+}
+
+// LAZINESS:NoLaziness
