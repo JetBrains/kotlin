81d965add2a86e7a83c9eb297c1def1967b10468
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-05-29 14:36:30 +0200

    Support properties in compatibility mode
    (cherry picked from commit ad717d5)

---
+++ compiler/testData/codegen/java8/box/jvm8/defaults/compatibility/bridgeWithProperties3.kt
@@ -0,0 +1,42 @@
+// !API_VERSION: 1.3
+// !JVM_DEFAULT_MODE: compatibility
+// FILE: Simple.java
+
+public interface Simple extends KInterface3 {
+    default String test() {
+        return KInterface3.DefaultImpls.getBar(this);
+    }
+}
+
+// FILE: Foo.java
+public class Foo implements Simple {
+
+}
+
+// FILE: main.kt
+// JVM_TARGET: 1.8
+// WITH_RUNTIME
+
+interface KInterface<T>  {
+
+    val foo: T
+
+    @JvmDefault
+    val bar: T
+        get() = foo
+}
+
+interface KInterface2 : KInterface<String> {
+    @JvmDefault
+    override val foo: String
+        get() = "OK"
+}
+
+interface KInterface3 : KInterface2 {
+
+}
+
+
+fun box(): String {
+    return Foo().test()
+}
