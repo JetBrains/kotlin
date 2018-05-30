5fa54dc854f447c79fe369ac1818496e8cb2c74d
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-05-30 15:04:16 +0200

    Fix array constructors coercion type

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/ArrayConstructor.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/ArrayConstructor.kt
@@ -19,10 +19,11 @@ package org.jetbrains.kotlin.backend.jvm.intrinsics
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.codegen.BlockInfo
 import org.jetbrains.kotlin.backend.jvm.codegen.ExpressionCodegen
-import org.jetbrains.kotlin.codegen.AsmUtil
 import org.jetbrains.kotlin.codegen.StackValue
 import org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression
+import org.jetbrains.kotlin.resolve.jvm.AsmTypes
 import org.jetbrains.kotlin.resolve.jvm.jvmSignature.JvmMethodSignature
+import org.jetbrains.org.objectweb.asm.Type
 import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
 
 object ArrayConstructor : IntrinsicMethod() {
@@ -34,7 +35,9 @@ object ArrayConstructor : IntrinsicMethod() {
         return object : IrIntrinsicFunction(expression, signature, context, expression.argTypes(context)) {
 
             override fun invoke(v: InstructionAdapter, codegen: ExpressionCodegen, data: BlockInfo): StackValue =
-                codegen.generateCall(expression, this, data)
+                codegen.generateCall(expression, this, data).let {
+                    return StackValue.onStack(Type.getObjectType("[" + AsmTypes.OBJECT_TYPE.internalName))
+                }
         }
     }
 }
