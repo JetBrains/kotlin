adfa7a2418cab6ef3de9bda5f3c0fafcc77e9e7f
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-06-05 11:26:12 +0300

    Create new module with dependency on IDEA Git plugin

---
+++ idea/idea-git/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    compileOnly(project(":idea"))
+
+    compileOnly(intellijDep())
+    compileOnly(intellijPluginDep("git4idea"))
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {  }
+}
+
+ideaPlugin()
