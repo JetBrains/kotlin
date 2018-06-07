3292137acba6c6a2a9d9824eda84e54e4b812c12
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-06-07 07:06:17 +0300

    "Convert lambda to anonymous function": Don't suggest in inline function
    So #KT-24792 Fixed

--- idea/tests/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
@@ -9767,6 +9767,11 @@ public class IntentionTestGenerated extends AbstractIntentionTest {
             runTest("idea/testData/intentions/lambdaToAnonymousFunction/implicitParameterName.kt");
         }
 
+        @TestMetadata("inInlineFunction.kt")
+        public void testInInlineFunction() throws Exception {
+            runTest("idea/testData/intentions/lambdaToAnonymousFunction/inInlineFunction.kt");
+        }
+
         @TestMetadata("incorrectOffset.kt")
         public void testIncorrectOffset() throws Exception {
             runTest("idea/testData/intentions/lambdaToAnonymousFunction/incorrectOffset.kt");
