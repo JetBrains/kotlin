dc20b95a0380599eb9299dbd6026731e76cd64f0
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2018-06-09 14:24:12 +0300

    backport LocalDeclarationsLowering from Native

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/LocalDeclarationsLowering.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/LocalDeclarationsLowering.kt
@@ -151,6 +151,18 @@ class LocalDeclarationsLowering(val context: BackendContext, val localNameProvid
             "LocalClassContext for $descriptor"
     }
 
+    private class LocalClassMemberContext(val member: IrFunction, val classContext: LocalClassContext) : LocalContext() {
+        override fun irGet(startOffset: Int, endOffset: Int, descriptor: ValueDescriptor): IrExpression? {
+            val field = classContext.capturedValueToField[descriptor] ?: return null
+
+            return IrGetFieldImpl(
+                startOffset, endOffset, field.symbol,
+                receiver = IrGetValueImpl(startOffset, endOffset, member.dispatchReceiverParameter!!.symbol)
+            )
+        }
+
+    }
+
     private inner class LocalDeclarationsTransformer(val memberDeclaration: IrDeclaration) {
         val localFunctions: MutableMap<FunctionDescriptor, LocalFunctionContext> = LinkedHashMap()
         val localClasses: MutableMap<ClassDescriptor, LocalClassContext> = LinkedHashMap()
@@ -194,6 +206,7 @@ class LocalDeclarationsLowering(val context: BackendContext, val localNameProvid
                 }
 
                 localClasses.values.mapTo(this) {
+                    it.declaration.parent = memberDeclaration.parent
                     it.declaration
                 }
 
@@ -216,7 +229,14 @@ class LocalDeclarationsLowering(val context: BackendContext, val localNameProvid
                     // Replace local function definition with an empty composite.
                     return IrCompositeImpl(declaration.startOffset, declaration.endOffset, context.builtIns.unitType)
                 } else {
-                    return super.visitFunction(declaration)
+                    if (localContext is LocalClassContext && declaration.parent == localContext.declaration) {
+                        return declaration.apply {
+                            val classMemberLocalContext = LocalClassMemberContext(declaration, localContext)
+                            transformChildrenVoid(FunctionBodiesRewriter(classMemberLocalContext))
+                        }
+                    } else {
+                        return super.visitFunction(declaration)
+                    }
                 }
             }
 
@@ -512,7 +532,7 @@ class LocalDeclarationsLowering(val context: BackendContext, val localNameProvid
             localFunctionContext.transformedDeclaration = with(localFunctionContext.declaration) {
                 IrFunctionImpl(startOffset, endOffset, origin, newDescriptor)
             }.apply {
-                parent = localFunctionContext.declaration.parent
+                parent = memberDeclaration.parent
                 createParameterDeclarations()
                 recordTransformedValueParameters(localFunctionContext)
                 transformedDeclarations[oldDescriptor] = this.symbol
