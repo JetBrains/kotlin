5f886fa5d4c436812faa4660bdf890c1b546b11c
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-06-13 16:29:48 +0300

    Do not coerce function literals to suspend.
    The design is to use `suspend fun` instead of coercion, just as suspend
    lambdas.
    However, this syntax is not supported in the parser. But this is not a
    problem, since the coercion lead to internal compiler error.
    As a workaround everybody uses suspend lambdas.
     #KT-24860: Fixed

--- compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -9225,6 +9225,11 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                 runTest("compiler/testData/diagnostics/tests/inference/cannotCompleteResolveWithFunctionLiterals.kt");
             }
 
+            @TestMetadata("coerceFunctionLiteralToSuspend.kt")
+            public void testCoerceFunctionLiteralToSuspend() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/inference/coerceFunctionLiteralToSuspend.kt");
+            }
+
             @TestMetadata("completeInferenceIfManyFailed.kt")
             public void testCompleteInferenceIfManyFailed() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/inference/completeInferenceIfManyFailed.kt");
