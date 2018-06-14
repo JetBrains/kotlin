37ed8e2617e9a55527ccbcf1bac071cb785baf86
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-06-14 17:19:18 +0300

    Build: Reformat CommonUtil.kt

--- buildSrc/src/main/kotlin/CommonUtil.kt
+++ buildSrc/src/main/kotlin/CommonUtil.kt
@@ -1,4 +1,5 @@
-@file:Suppress("unused") // usages in build scripts are not tracked properly
+// usages in build scripts are not tracked properly
+@file:Suppress("unused")
 
 import groovy.lang.Closure
 import org.gradle.api.Project
@@ -6,7 +7,6 @@ import org.gradle.api.Task
 import org.gradle.api.file.CopySourceSpec
 import org.gradle.api.file.SourceDirectorySet
 import org.gradle.api.plugins.JavaPluginConvention
-import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.SourceSetOutput
 import org.gradle.kotlin.dsl.creating
@@ -22,7 +22,7 @@ fun Project.callGroovy(name: String, vararg args: Any?): Any? {
     return (property(name) as Closure<*>).call(*args)
 }
 
-inline fun<T: Any> Project.withJavaPlugin(crossinline body: () -> T?): T? {
+inline fun <T : Any> Project.withJavaPlugin(crossinline body: () -> T?): T? {
     var res: T? = null
     pluginManager.withPlugin("java") {
         res = body()
@@ -47,11 +47,15 @@ fun File(root: String, vararg children: String): File = children.fold(File(root)
 
 var Project.jvmTarget: String?
     get() = extra.takeIf { it.has("jvmTarget") }?.get("jvmTarget") as? String
-    set(v) { extra["jvmTarget"] = v }
+    set(v) {
+        extra["jvmTarget"] = v
+    }
 
 var Project.javaHome: String?
     get() = extra.takeIf { it.has("javaHome") }?.get("javaHome") as? String
-    set(v) { extra["javaHome"] = v }
+    set(v) {
+        extra["javaHome"] = v
+    }
 
 fun Project.generator(fqName: String, sourceSet: SourceSet? = null) = smartJavaExec {
     classpath = (sourceSet ?: javaPluginConvention().sourceSets["test"]).runtimeClasspath
