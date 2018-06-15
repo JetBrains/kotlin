74652898112f4c0ac34e7c48b442b46c23feef16
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-06-15 13:57:57 +0300

    Introduce jvm backend class resolver to resolve asm types
     Original author is @dnpetrov

--- compiler/backend/src/org/jetbrains/kotlin/codegen/state/GenerationState.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/state/GenerationState.kt
@@ -51,7 +51,8 @@ class GenerationState private constructor(
     moduleName: String?,
     val outDirectory: File?,
     private val onIndependentPartCompilationEnd: GenerationStateEventCallback,
-    wantsDiagnostics: Boolean
+    wantsDiagnostics: Boolean,
+    val jvmBackendClassResolver: JvmBackendClassResolver
 ) {
 
     class Builder(
@@ -94,11 +95,16 @@ class GenerationState private constructor(
         fun wantsDiagnostics(v: Boolean) =
             apply { wantsDiagnostics = v }
 
+        var jvmBackendClassResolver: JvmBackendClassResolver = JvmBackendClassResolverForModuleWithDependencies(module); private set
+        fun jvmBackendClassResolver(v: JvmBackendClassResolver) =
+            apply { jvmBackendClassResolver = v }
+
         fun build() =
             GenerationState(
                 project, builderFactory, module, bindingContext, files, configuration,
                 generateDeclaredClassFilter, codegenFactory, targetId,
-                moduleName, outDirectory, onIndependentPartCompilationEnd, wantsDiagnostics
+                moduleName, outDirectory, onIndependentPartCompilationEnd, wantsDiagnostics,
+                jvmBackendClassResolver
             )
     }
 
