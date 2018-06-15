1819878c2b0a2e791c25b4a194f0e8b18de9d616
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-06-15 15:08:57 +0300

    Switch off inspections on Kotlin files not in project source
    To switch them off, KotlinDefaultHighlightingSettingsProvider
    was introduced.
    So #KT-19377 Fixed

---
+++ idea/src/org/jetbrains/kotlin/idea/highlighter/KotlinDefaultHighlightingSettingsProvider.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.highlighter
+
+import com.intellij.codeInsight.daemon.impl.analysis.DefaultHighlightingSettingProvider
+import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
+
+class KotlinDefaultHighlightingSettingsProvider : DefaultHighlightingSettingProvider() {
+    override fun getDefaultSetting(project: Project, file: VirtualFile): FileHighlightingSetting? {
+        return when {
+            ProjectRootsUtil.isProjectSourceFile(project, file) -> null
+            else -> FileHighlightingSetting.SKIP_INSPECTION
+        }
+    }
+}
