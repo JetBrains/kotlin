a75f30fed6749edad64469597e893b90cea0539f
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-06-15 15:56:47 +0300

    Remove inline functions with reified type parameters
    Otherwise we cannot handle type casts

---
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/inline/RemoveInlineFunctionsWithReifiedTypeParametersLowering.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.ir.backend.js.lower.inline
+
+import org.jetbrains.kotlin.backend.common.DeclarationContainerLoweringPass
+import org.jetbrains.kotlin.ir.backend.js.utils.isReified
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationContainer
+import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.util.deepCopyOld
+import org.jetbrains.kotlin.ir.util.transformFlat
+
+
+object RemoveInlineFunctionsWithReifiedTypeParametersLowering: DeclarationContainerLoweringPass {
+    override fun lower(irDeclarationContainer: IrDeclarationContainer) {
+        irDeclarationContainer.declarations.transformFlat {
+            if (it is IrFunction && it.isInline && it.typeParameters.any { it.isReified }) listOf() else null
+        }
+    }
+}
