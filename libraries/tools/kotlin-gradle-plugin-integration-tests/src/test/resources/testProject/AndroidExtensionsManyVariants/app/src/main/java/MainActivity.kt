6e3c2e80158c08bec1168ab00f121fde58d97650
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-06-25 16:21:46 +0300

    Revert "Android Extensions: Allow to access library project resources in Gradle setup (#KT-22430)"
    This reverts commit a70707b

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/AndroidExtensionsManyVariants/app/src/main/java/MainActivity.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/AndroidExtensionsManyVariants/app/src/main/java/MainActivity.kt
@@ -3,7 +3,6 @@ package org.example.manyvariants
 import android.app.Activity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.layout_in_library.text_view
 import kotlinx.android.parcel.Parcelize
 import android.os.Parcelable
 
@@ -12,7 +11,6 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewMain
-        text_view
     }
 }
 
