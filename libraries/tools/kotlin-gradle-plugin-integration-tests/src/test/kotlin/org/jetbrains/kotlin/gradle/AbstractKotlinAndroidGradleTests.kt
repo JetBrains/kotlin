d24b7b0a62ef0ed22e1146f980389bdf85829dc0
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2018-06-27 05:55:43 +0300

    Remove redundant test
    `testMultiModuleICNonAndroidModuleIsChanged` already tested
    android and non-android change

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/AbstractKotlinAndroidGradleTests.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/AbstractKotlinAndroidGradleTests.kt
@@ -221,25 +221,6 @@ fun getSomething() = 10
 
     @Test
     fun testMultiModuleIC() {
-        val project = Project("AndroidProject")
-        val options = defaultBuildOptions().copy(incremental = true)
-
-        project.build("assembleDebug", options = options) {
-            assertSuccessful()
-        }
-
-        val libUtilKt = project.projectDir.getFileByName("libUtil.kt")
-        libUtilKt.modify { it.replace("fun libUtil(): String", "fun libUtil(): CharSequence") }
-
-        project.build("assembleDebug", options = options) {
-            assertSuccessful()
-            val affectedSources = project.projectDir.getFilesByNames("libUtil.kt", "MainActivity2.kt")
-            assertCompiledKotlinSources(project.relativize(affectedSources), weakTesting = false)
-        }
-    }
-
-    @Test
-    fun testMultiModuleICNonAndroidModuleIsChanged() {
         val project = Project("AndroidIncrementalMultiModule")
         val options = defaultBuildOptions().copy(incremental = true, kotlinDaemonDebugPort = null)
 
