2f34791dbcc5eda93b402bf9b5e9b9afe18de9c3
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-06-29 17:34:29 +0300

    Add ContinuationAdapter to coroutine tests helpers
    It allows having the same Continuation implementations for different
    API versions

--- compiler/testData/codegen/java8/box/asyncException.kt
+++ compiler/testData/codegen/java8/box/asyncException.kt
@@ -1,6 +1,7 @@
 // WITH_RUNTIME
 // COMMON_COROUTINES_TEST
 // FULL_JDK
+// WITH_COROUTINES
 
 import java.util.concurrent.CompletableFuture
 import COROUTINES_PACKAGE.*
@@ -45,7 +46,7 @@ fun box(): String {
 
 fun <T> async(c: suspend () -> T): CompletableFuture<T> {
     val future = CompletableFuture<T>()
-    c.startCoroutine(object : Continuation<T> {
+    c.startCoroutine(object : helpers.ContinuationAdapter<T>() {
         override val context = EmptyCoroutineContext
 
         override fun resume(data: T) {
