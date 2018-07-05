e2fe2a63543d996f6f48a7e5ea609524b3cdf5b2
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-07-05 06:06:39 +0300

    More efficient double generation in JS

--- libraries/stdlib/js/src/kotlin/random/PlatformRandom.kt
+++ libraries/stdlib/js/src/kotlin/random/PlatformRandom.kt
@@ -5,6 +5,8 @@
 
 package kotlin.random
 
+import kotlin.math.pow
+
 internal actual fun defaultPlatformRandom(): Random =
     Random(js("(Math.random() * Math.pow(2, 32)) | 0").unsafeCast<Int>())
 
@@ -19,3 +21,8 @@ internal actual fun fastLog2(value: Int): Int {
     }
     return log
 }
+
+private val INV_2_26: Double = 2.0.pow(-26)
+private val INV_2_53: Double = 2.0.pow(-53)
+internal actual fun doubleFromParts(hi26: Int, low27: Int): Double =
+    hi26 * INV_2_26 + low27 * INV_2_53
