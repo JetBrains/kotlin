8c2aaa365b9893c16626443a63071ba1b5e0f32f
Juan Chen <juanch@google.com>
2018-07-09 15:49:33 -0700

    Use "implementation" in build scripts when configuring Kotlin (KT-22571)
    Currently "compile" is used when adding kotlin dependencies to build
    scripts, which is deprecated.

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/configuration/GroovyBuildScriptManipulator.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/configuration/GroovyBuildScriptManipulator.kt
@@ -348,7 +348,7 @@ class GroovyBuildScriptManipulator(
     private fun getGroovyDependencySnippet(
         artifactName: String,
         withVersion: Boolean
-    ) = "compile \"org.jetbrains.kotlin:$artifactName${if (withVersion) ":\$kotlin_version" else ""}\""
+    ) = "implementation \"org.jetbrains.kotlin:$artifactName${if (withVersion) ":\$kotlin_version" else ""}\""
 
     private fun getApplyPluginDirective(pluginName: String) = "apply plugin: '$pluginName'"
 
