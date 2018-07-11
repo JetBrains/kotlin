88d37f619c2728bfda810068920bb7e1b5413fbc
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-07-11 04:42:17 +0300

    Migrate stepping and evaluation tests to the new coroutine API

--- idea/testData/debugger/tinyApp/src/forTests/coroutines.kt
+++ idea/testData/debugger/tinyApp/src/forTests/coroutines.kt
@@ -1,17 +1,17 @@
 package forTests
 
-import kotlin.coroutines.experimental.Continuation
-import kotlin.coroutines.experimental.CoroutineContext
-import kotlin.coroutines.experimental.EmptyCoroutineContext
-import kotlin.coroutines.experimental.startCoroutine
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.startCoroutine
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 open class EmptyContinuation(override val context: CoroutineContext = EmptyCoroutineContext) : Continuation<Any?> {
     companion object : EmptyContinuation()
-    override fun resume(data: Any?) {}
-    override fun resumeWithException(exception: Throwable) { throw exception }
+    override fun resumeWith(result: SuccessOrFailure<Any?>) { result.getOrThrow() }
 }
+
 
 fun builder(c: suspend () -> Unit) {
     c.startCoroutine(EmptyContinuation)
