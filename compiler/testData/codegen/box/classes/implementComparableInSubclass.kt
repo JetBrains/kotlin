9c6452778f8af0a920da504e6253f9a9383bfc13
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-07-11 15:25:41 +0300

    JS IR: unmute tests

--- compiler/testData/codegen/box/classes/implementComparableInSubclass.kt
+++ compiler/testData/codegen/box/classes/implementComparableInSubclass.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JS_IR
 // See KT-12865
 
 package foo
--- compiler/testData/codegen/box/inlineClasses/bridgesWhenInlineClassImplementsGenericInterface.kt
+++ compiler/testData/codegen/box/inlineClasses/bridgesWhenInlineClassImplementsGenericInterface.kt
@@ -1,6 +1,5 @@
 // !LANGUAGE: +InlineClasses
 // IGNORE_BACKEND: JVM_IR
-// IGNORE_BACKEND: JS_IR
 
 inline class InlinedComparable(val x: Int) : Comparable<InlinedComparable> {
     override fun compareTo(other: InlinedComparable): Int {
