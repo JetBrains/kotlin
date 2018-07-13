7fae4fed65aa11463cba928e314c21f17feb99e7
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-07-13 18:36:31 +0300

    [KDM] Add missing urlEncoded function for JLH
    Original commit: https://github.com/Kotlin/dokka/commit/ad35def2efbb6eae6fae423bb639890aaa2cb93f

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/Utilities/Html.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/Utilities/Html.kt
@@ -1,8 +1,12 @@
 package org.jetbrains.dokka
 
+import java.net.URLEncoder
+
 
 /**
  * Replaces symbols reserved in HTML with their respective entities.
  * Replaces & with &amp;, < with &lt; and > with &gt;
  */
 fun String.htmlEscape(): String = replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+
+fun String.urlEncoded(): String = URLEncoder.encode(this, "UTF-8")
