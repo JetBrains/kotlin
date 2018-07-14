4cc82a1e36aeb19b238229d844948251cfeac4d3
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2018-07-14 12:16:30 +0300

    Kotlin/Native support in IntelliJ IDEA

---
+++ idea/idea-native/src/org/jetbrains/konan/analyser/index/KonanDescriptorManager.kt
@@ -0,0 +1,68 @@
+package org.jetbrains.konan.analyser.index
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.ApplicationComponent
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileEvent
+import com.intellij.openapi.vfs.VirtualFileListener
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.stubs.StubTree
+import com.intellij.util.containers.ContainerUtil
+import org.jetbrains.kotlin.backend.konan.library.impl.LibraryReaderImpl
+import org.jetbrains.kotlin.config.LanguageVersionSettings
+import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
+import org.jetbrains.kotlin.konan.file.File
+import org.jetbrains.kotlin.metadata.KonanLinkData
+import java.util.concurrent.ConcurrentMap
+
+private const val currentAbiVersion = 1
+
+class KonanDescriptorManager : ApplicationComponent {
+  companion object {
+    @JvmStatic
+    val INSTANCE: KonanDescriptorManager
+      get() = ApplicationManager.getApplication().getComponent(KonanDescriptorManager::class.java)
+  }
+
+  override fun initComponent() {
+    VirtualFileManager.getInstance().addVirtualFileListener(object : VirtualFileListener {
+      override fun fileCreated(event: VirtualFileEvent) {
+        val file = event.file
+
+        descriptorCache.values.forEach { it.remove(file) }
+        protoCache.remove(file)
+        stubCache.remove(file)
+      }
+    })
+  }
+
+  private val descriptorCache = ContainerUtil.createConcurrentSoftValueMap<LanguageVersionSettings, ConcurrentMap<VirtualFile, ModuleDescriptorImpl>>()
+  private val protoCache = ContainerUtil.createConcurrentSoftValueMap<VirtualFile, KonanLinkData.LinkDataPackageFragment>()
+  private val stubCache = ContainerUtil.createConcurrentSoftValueMap<VirtualFile, StubTree>()
+
+  fun getDescriptor(file: VirtualFile, specifics: LanguageVersionSettings): ModuleDescriptorImpl {
+    val cache = descriptorCache.computeIfAbsent(specifics, {
+      ContainerUtil.createConcurrentSoftValueMap()
+    })
+
+    return cache.computeIfAbsent(file, {
+      val reader = LibraryReaderImpl(File(file.path), currentAbiVersion)
+      reader.moduleDescriptor(specifics)
+    })
+  }
+
+  fun parsePackageFragment(file: VirtualFile): KonanLinkData.LinkDataPackageFragment {
+    return protoCache.computeIfAbsent(file, {
+      val bytes = file.contentsToByteArray(false)
+      org.jetbrains.kotlin.backend.konan.serialization.parsePackageFragment(bytes)
+    })
+  }
+
+  fun getStub(file: VirtualFile): StubTree? {
+    return stubCache[file]
+  }
+
+  fun cacheStub(file: VirtualFile, stubTree: StubTree?) {
+    stubCache[file] = stubTree
+  }
+}
---
+++ idea/idea-native/src/org/jetbrains/konan/settings/KonanPaths.kt
@@ -0,0 +1,47 @@
+package org.jetbrains.konan.settings
+
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.project.Project
+import com.intellij.util.io.exists
+import com.intellij.util.io.isDirectory
+import org.jetbrains.konan.KotlinNativeToolchain
+import org.jetbrains.kotlin.konan.target.HostManager
+import org.jetbrains.kotlin.konan.target.KonanTarget
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.stream.Collectors
+
+open class KonanPaths(protected val project: Project) : ProjectComponent {
+  companion object {
+    private const val STDLIB_PATH = "klib/common/stdlib" //todo: move to kotlin/native
+
+    fun bundledKonanDist(): Path = KotlinNativeToolchain.BUNDLED.baseDir
+
+    fun getInstance(project: Project): KonanPaths = project.getComponent(KonanPaths::class.java)
+  }
+
+  override fun getComponentName() = "Konan Compiler Paths"
+
+  fun konanStdlib(): Path? = konanDist()?.resolve(STDLIB_PATH)
+
+  open fun konanDist(): Path? {
+    for (provider in KonanModelProvider.EP_NAME.extensions) {
+      return provider.getKonanHome(project)
+    }
+    return bundledKonanDist()
+  }
+
+  open fun libraryPaths(): Set<Path> = emptySet()
+
+  fun konanPlatformLibraries(): List<Path> {
+    val resolvedTargetName = HostManager.resolveAlias(target().name)
+    val klibPath = konanDist()?.resolve("klib/platform/${resolvedTargetName}") ?: return emptyList()
+    if (!klibPath.exists()) return emptyList()
+    return Files.walk(klibPath, 1).filter {
+      it.isDirectory() && it.fileName.toString() != "stdlib" && it != klibPath
+    }.collect(Collectors.toList())
+  }
+
+  //todo: this is wrong, we are not allowing multiple targets in project
+  open fun target(): KonanTarget = HostManager.host
+}
