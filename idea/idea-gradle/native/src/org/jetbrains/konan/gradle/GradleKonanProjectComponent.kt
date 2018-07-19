53dcd8f45e0df2d447e78db405203d553819f785
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2018-07-19 19:49:09 +0300

    K/N: Using correct module when constructing the KonanAnalyzerFacade

--- idea/idea-gradle/native/src/org/jetbrains/konan/gradle/GradleKonanProjectComponent.kt
+++ idea/idea-gradle/native/src/org/jetbrains/konan/gradle/GradleKonanProjectComponent.kt
@@ -1,21 +1,10 @@
 package org.jetbrains.konan.gradle
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsManager
 import com.intellij.openapi.project.Project
 import org.jetbrains.konan.settings.KonanProjectComponent
 import org.jetbrains.plugins.gradle.settings.GradleSettings
 
 class GradleKonanProjectComponent(project: Project) : KonanProjectComponent(project) {
-  override fun projectOpened() {
-    super.projectOpened()
-    ExternalProjectsManager.getInstance(project).runWhenInitialized {
-      ApplicationManager.getApplication().invokeLater {
-        reloadLibraries()
-      }
-    }
-  }
-
   override fun looksLikeKotlinNativeProject(): Boolean {
     //TODO not just any gradle project
     return GradleSettings.getInstance(project).linkedProjectsSettings.isNotEmpty()
