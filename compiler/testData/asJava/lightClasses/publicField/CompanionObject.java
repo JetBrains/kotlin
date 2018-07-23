6a38d67e00aa83e609acecd9895ada8aa51e4a51
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-07-23 18:06:01 +0300

    Fix ConstantValue-related testData for lightClasses tests
    TODO support custom language version settings for lightClasses tests

--- compiler/testData/asJava/lightClasses/publicField/CompanionObject.java
+++ compiler/testData/asJava/lightClasses/publicField/CompanionObject.java
@@ -1,7 +1,7 @@
 public final class C {
     @kotlin.jvm.JvmField
     @org.jetbrains.annotations.NotNull
-    public static final java.lang.String foo = "A";
+    public static final java.lang.String foo;
     public static final C.Companion Companion;
 
     public C() { /* compiled code */ }
