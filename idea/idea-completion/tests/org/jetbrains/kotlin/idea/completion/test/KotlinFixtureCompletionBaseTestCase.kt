2a57807c855633fa9abba7e53754265ee41b3574
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2018-07-26 13:00:21 +0300

    Minor: reformat and clean-up KotlinFixtureCompletionBaseTestCase

--- idea/idea-completion/tests/org/jetbrains/kotlin/idea/completion/test/KotlinFixtureCompletionBaseTestCase.kt
+++ idea/idea-completion/tests/org/jetbrains/kotlin/idea/completion/test/KotlinFixtureCompletionBaseTestCase.kt
@@ -28,8 +28,8 @@ import java.io.File
 abstract class KotlinFixtureCompletionBaseTestCase : KotlinLightCodeInsightFixtureTestCase() {
     abstract fun getPlatform(): TargetPlatform
 
-    protected open fun complete(completionType: CompletionType, invocationCount: Int): Array<LookupElement>?
-            = myFixture.complete(completionType, invocationCount)
+    protected open fun complete(completionType: CompletionType, invocationCount: Int): Array<LookupElement>? =
+        myFixture.complete(completionType, invocationCount)
 
     protected abstract fun defaultCompletionType(): CompletionType
     protected open fun defaultInvocationCount(): Int = 0
@@ -41,15 +41,20 @@ abstract class KotlinFixtureCompletionBaseTestCase : KotlinLightCodeInsightFixtu
             val fileText = FileUtil.loadFile(File(testPath), true)
             configureCompilerOptions(fileText, project, module)
 
-            assertTrue("\"<caret>\" is missing in file \"$testPath\"", fileText.contains("<caret>"));
+            assertTrue("\"<caret>\" is missing in file \"$testPath\"", fileText.contains("<caret>"))
 
             if (ExpectedCompletionUtils.shouldRunHighlightingBeforeCompletion(fileText)) {
                 myFixture.doHighlighting()
             }
 
-            testCompletion(fileText, getPlatform(), { completionType, count -> complete(completionType, count) }, defaultCompletionType(), defaultInvocationCount())
-        }
-        finally {
+            testCompletion(
+                fileText,
+                getPlatform(),
+                { completionType, count -> complete(completionType, count) },
+                defaultCompletionType(),
+                defaultInvocationCount()
+            )
+        } finally {
             tearDownFixture()
         }
     }
