1bf3f4bda796fb1232b8f51c74d57a3bbca57065
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-07-31 09:09:49 +0200

    Extract ReferenceSymbolTable, support lazy type parameters

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/lazy/IrLazySymbolTable.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/lazy/IrLazySymbolTable.kt
@@ -5,57 +5,66 @@
 
 package org.jetbrains.kotlin.ir.declarations.lazy
 
-import org.jetbrains.kotlin.descriptors.ClassConstructorDescriptor
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.descriptors.FunctionDescriptor
-import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
+import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.ir.symbols.*
 import org.jetbrains.kotlin.ir.util.DeclarationStubGenerator
+import org.jetbrains.kotlin.ir.util.ReferenceSymbolTable
 import org.jetbrains.kotlin.ir.util.SymbolTable
 
 
-class IrLazySymbolTable(private val stubGenerator: DeclarationStubGenerator, val originalTable: SymbolTable) : SymbolTable() {
+class IrLazySymbolTable(private val originalTable: SymbolTable) : ReferenceSymbolTable by originalTable {
+    override val declarationSystemTable: SymbolTable = originalTable
+
+    override val referenceSymbolTable: ReferenceSymbolTable = this
 
     /*Don't force builtins class linking before unbound symbols linking: otherwise stdlib compilation will failed*/
-    internal var unboundSymbolGeneration = false
+    var stubGenerator: DeclarationStubGenerator? = null
 
     override fun referenceClass(descriptor: ClassDescriptor): IrClassSymbol {
         return originalTable.referenceClass(descriptor).also {
-            if (unboundSymbolGeneration && !it.isBound) {
-                stubGenerator.generateClassStub(descriptor).symbol
+            if (!it.isBound) {
+                stubGenerator?.generateClassStub(descriptor)
             }
         }
     }
 
     override fun referenceConstructor(descriptor: ClassConstructorDescriptor): IrConstructorSymbol {
         return originalTable.referenceConstructor(descriptor).also {
-            if (!it.isBound && unboundSymbolGeneration) {
-                stubGenerator.generateConstructorStub(descriptor).symbol
+            if (!it.isBound) {
+                stubGenerator?.generateConstructorStub(descriptor)
             }
         }
     }
 
     override fun referenceEnumEntry(descriptor: ClassDescriptor): IrEnumEntrySymbol {
         return originalTable.referenceEnumEntry(descriptor).also {
-            if (!it.isBound && unboundSymbolGeneration) {
-                stubGenerator.generateEnumEntryStub(descriptor).symbol
+            if (!it.isBound) {
+                stubGenerator?.generateEnumEntryStub(descriptor)
             }
         }
     }
 
     override fun referenceSimpleFunction(descriptor: FunctionDescriptor): IrSimpleFunctionSymbol {
         return originalTable.referenceSimpleFunction(descriptor).also {
-            if (!it.isBound && unboundSymbolGeneration) {
-                stubGenerator.generateFunctionStub(descriptor).symbol
+            if (!it.isBound) {
+                stubGenerator?.generateFunctionStub(descriptor)
             }
         }
     }
 
     override fun referenceTypeParameter(classifier: TypeParameterDescriptor): IrTypeParameterSymbol {
         return originalTable.referenceTypeParameter(classifier).also {
-            if (!it.isBound && unboundSymbolGeneration) {
-                stubGenerator.generateOrGetTypeParameterStub(classifier).symbol
+            if (!it.isBound) {
+                stubGenerator?.generateOrGetTypeParameterStub(classifier)
             }
         }
+    }
+
+    override fun referenceFunction(callable: CallableDescriptor): IrFunctionSymbol {
+        return super.referenceFunction(callable)
+    }
+
+    override fun referenceClassifier(classifier: ClassifierDescriptor): IrClassifierSymbol {
+        return super.referenceClassifier(classifier)
     }
 }
--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/TypeTranslator.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/TypeTranslator.kt
@@ -22,9 +22,10 @@ import org.jetbrains.kotlin.types.*
 import org.jetbrains.kotlin.types.typesApproximation.approximateCapturedTypes
 
 class TypeTranslator(
-    private val symbolTable: SymbolTable,
+    private val symbolTable: ReferenceSymbolTable,
     val languageVersionSettings: LanguageVersionSettings,
-    private val typeParametersResolver: TypeParametersResolver = ScopedTypeParametersResolver()
+    private val typeParametersResolver: TypeParametersResolver = ScopedTypeParametersResolver(),
+    private val enterTableScope: Boolean = false
 ) {
 
     private val typeApproximatorForNI = TypeApproximator()
@@ -32,16 +33,22 @@ class TypeTranslator(
 
     fun enterScope(irElement: IrTypeParametersContainer) {
         typeParametersResolver.enterTypeParameterScope(irElement)
+        if(enterTableScope) {
+            symbolTable.enterScope(irElement.descriptor)
+        }
     }
 
-    fun leaveScope() {
+    fun leaveScope(irElement: IrTypeParametersContainer) {
         typeParametersResolver.leaveTypeParameterScope()
+        if(enterTableScope) {
+            symbolTable.leaveScope(irElement.descriptor)
+        }
     }
 
     inline fun <T> buildWithScope(container: IrTypeParametersContainer, builder: () -> T): T {
         enterScope(container)
         val result = builder()
-        leaveScope()
+        leaveScope(container)
         return result
     }
 
