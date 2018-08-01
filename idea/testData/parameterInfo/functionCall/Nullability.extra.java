ceeeffd814fa60e0f5b969ed5c4fdb7d3c681b94
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-08-01 19:11:20 +0300

    Extend list of annotations ignored in parameter info (KT-24911, KT-25622)
     #KT-24911 Fixed

--- idea/testData/parameterInfo/functionCall/Nullability.extra.java
+++ idea/testData/parameterInfo/functionCall/Nullability.extra.java
@@ -1,8 +1,10 @@
 import org.jetbrains.annotations.Nullable
 import org.jetbrains.annotations.NotNull
+import androidx.annotation.RecentlyNonNull;
+import androidx.annotation.RecentlyNullable;
 
 class J {
-    static void foo(@Nullable String s1, @NotNull String s2, String s3) {
+    static void foo(@Nullable String s1, @NotNull String s2, @RecentlyNullable String a, @RecentlyNonNull String b, String s3) {
 
     }
 }
