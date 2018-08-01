e30fa3d80f1c08ebf267eb013a6cecbf9b917c03
victor.petukhov <victor.petukhov@jetbrains.com>
2018-08-01 14:52:39 +0300

    Move spec tests configuration and util files to the tests-spec module

---
+++ compiler/tests-spec/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerSpecTests.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.generators.tests
+
+import org.jetbrains.kotlin.checkers.*
+import org.jetbrains.kotlin.generators.tests.generator.testGroup
+
+fun main(args: Array<String>) {
+    System.setProperty("java.awt.headless", "true")
+
+    testGroup("compiler/tests-spec/tests", "compiler/tests-spec/testData") {
+        testClass<AbstractDiagnosticsTestSpec> {
+            model("diagnostics")
+        }
+    }
+}
