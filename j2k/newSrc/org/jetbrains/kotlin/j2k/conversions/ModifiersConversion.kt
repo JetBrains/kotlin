5add339bea2b3b2cd3d7b967a715e20f05dc8b37
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-08-04 08:14:29 +0300

    Support relaxed visibility for members of package-local classes

--- j2k/newSrc/org/jetbrains/kotlin/j2k/conversions/ModifiersConversion.kt
+++ j2k/newSrc/org/jetbrains/kotlin/j2k/conversions/ModifiersConversion.kt
@@ -5,20 +5,38 @@
 
 package org.jetbrains.kotlin.j2k.conversions
 
+import org.jetbrains.kotlin.j2k.ConversionContext
 import org.jetbrains.kotlin.j2k.tree.*
 import org.jetbrains.kotlin.j2k.tree.impl.JKKtModifierImpl
+import org.jetbrains.kotlin.j2k.tree.impl.visibility
 
-class ModifiersConversion : RecursiveApplicableConversionBase() {
+class ModifiersConversion(private val context: ConversionContext) : RecursiveApplicableConversionBase() {
 
     override fun applyToElement(element: JKTreeElement): JKTreeElement {
-        return if (element is JKModifierListOwner && element !is JKLocalVariable)
-            element.also {
-                var modifiers = it.modifierList.modifiers
-                if (element !is JKField) {
-                    modifiers = mapOtherModifiers(modifiers)
-                }
-                it.modifierList.modifiers = modifiers
-            } else recurse(element)
+        if (element is JKModifierListOwner && element !is JKLocalVariable) {
+            var modifiers = element.modifierList.modifiers
+            if (element !is JKField) {
+                modifiers = mapOtherModifiers(modifiers)
+            }
+            element.modifierList.modifiers = modifiers
+            // Is declared inside some class
+            // TODO: Cleanup
+            if (context.converter.settings.noInternalForMembersOfInternal) {
+                relaxVisibility(element)
+            }
+        }
+        return recurse(element)
+    }
+
+    private fun relaxVisibility(element: JKTreeElement) {
+        if (element is JKParameter) return
+        element as JKModifierListOwner // TODO: Assert
+        val modifierList = element.modifierList
+        val parent = element.parent as? JKClass
+        if (parent == null
+            || modifierList.visibility != JKAccessModifier.Visibility.PACKAGE_PRIVATE
+            || parent.modifierList.visibility != JKAccessModifier.Visibility.PACKAGE_PRIVATE) return
+        modifierList.visibility = JKAccessModifier.Visibility.PUBLIC
     }
 
     private fun mapOtherModifiers(modifiers: List<JKModifier>): List<JKModifier> {
