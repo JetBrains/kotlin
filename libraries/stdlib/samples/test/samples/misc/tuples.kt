2f4ddb10c3644cc8ccd33dcad8df186562024af4
takasy <ticktack016@gmail.com>
2018-08-06 20:43:12 +0900

    KT-20357 Add sample code for toList

--- libraries/stdlib/samples/test/samples/misc/tuples.kt
+++ libraries/stdlib/samples/test/samples/misc/tuples.kt
@@ -20,4 +20,23 @@ class Tuples {
         assertPrints(c, "[null]")
     }
 
+    @Sample
+    fun pairToList() {
+        val list = Pair(1, "a").toList()
+        assertPrints(list, "[1, a]")
+        assertTrue(list[0] is Int)
+        assertTrue(list[1] is String)
+        assertFalse(list[1] is Int)
+    }
+
+    @Sample
+    fun tripleToList() {
+        val list = Triple(1, "a", 0.5).toList()
+        assertPrints(list, "[1, a, 0.5]")
+        assertTrue(list[0] is Int)
+        assertTrue(list[1] is String)
+        assertTrue(list[2] is Double)
+        assertFalse(list[2] is String)
+    }
+
 }
