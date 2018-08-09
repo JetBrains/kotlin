6179459c107dcd05020ed4ec1558c1ff9d5d69c1
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-08-09 23:39:17 +0300

    Introduce common ArithmeticException
    Make divisionByZero test still fail in JS after introducing ArithmeticException

--- libraries/stdlib/test/ExceptionTest.kt
+++ libraries/stdlib/test/ExceptionTest.kt
@@ -22,6 +22,7 @@ class ExceptionTest {
     @Test fun nullPointerException() = testCreateException(::NullPointerException, ::NullPointerException)
     @Test fun classCastException() = testCreateException(::ClassCastException, ::ClassCastException)
     @Test fun noSuchElementException() = testCreateException(::NoSuchElementException, ::NoSuchElementException)
+    @Test fun arithmeticException() = testCreateException(::ArithmeticException, ::ArithmeticException)
 
     @Test fun noWhenBranchMatchedException() = testCreateException(::NoWhenBranchMatchedException, ::NoWhenBranchMatchedException, ::NoWhenBranchMatchedException, ::NoWhenBranchMatchedException)
     @Test fun uninitializedPropertyAccessException() = testCreateException(::UninitializedPropertyAccessException, ::UninitializedPropertyAccessException, ::UninitializedPropertyAccessException, ::UninitializedPropertyAccessException)
