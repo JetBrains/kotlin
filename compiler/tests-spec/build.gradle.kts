e989cf2bf43bef756f88730e4756c0df7cc2fc9b
victor.petukhov <victor.petukhov@jetbrains.com>
2018-08-10 17:41:29 +0300

    Add tests-spec module with basic gradle configuration

---
+++ compiler/tests-spec/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+dependencies {
+    testCompile(projectTests(":compiler"))
+}
+
+sourceSets {
+    "main" { }
+    "test" { projectDefault() }
+}
+
+projectTest {
+    workingDir = rootDir
+}
