417de7f18fcc3486605d8835a4183359e153c02e
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-08-21 13:12:57 +0300

    Advance bootstrap to 1.3-M2-eap-82

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ import proguard.gradle.ProGuardTask
 buildscript {
     extra["defaultSnapshotVersion"] = "1.3-SNAPSHOT"
 
-    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3-M2-eap-35", projectExtId = "Kotlin_13M2_Compiler", onlySuccessBootstrap = false))
+    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3-M2-eap-82", projectExtId = "Kotlin_13M2_Compiler", onlySuccessBootstrap = false))
 
     val mirrorRepo: String? = findProperty("maven.repository.mirror")?.toString()
 
