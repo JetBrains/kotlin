605ab1687e5c6c41a676660c1478f2c63dcdbd77
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-08-21 16:27:01 +0300

    Adjust coroutines test data to changing the default LV to 1.3

--- compiler/testData/diagnostics/testsWithStdLib/coroutines/coroutinesEnabledWithWarning.kt
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/coroutinesEnabledWithWarning.kt
@@ -1,5 +1,5 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
-// !LANGUAGE: warn:Coroutines
+// !LANGUAGE: warn:Coroutines -ReleaseCoroutines
 
 <!EXPERIMENTAL_FEATURE_WARNING!>suspend<!> fun suspendHere(): String = "OK"
 
