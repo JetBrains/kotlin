55f4fe7df7b45074bceca2c943d3b3ae4ab79fd7
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-08-21 11:41:08 +0900

    "Nested lambda has shadowed implicit parameter": Do not report if 'it' paramerter is not used #KT-26268 Fixed

--- idea/testData/inspectionsLocal/nestedLambdaShadowedImplicitParameter/implicitGrandParent.kt
+++ idea/testData/inspectionsLocal/nestedLambdaShadowedImplicitParameter/implicitGrandParent.kt
@@ -1,9 +1,11 @@
 fun foo(f: (String) -> Unit) {}
+fun bar(s: String) {}
 
 fun test() {
     foo {
         foo { s ->
             <caret>foo {
+                bar(it)
             }
         }
     }
