2e5940e46d8a760d364c29eeff010fcb6b23f3e5
Roman Artemev <roman.artemev@jetbrains.com>
2018-08-23 19:26:04 +0300

    [JS IR BE] Fix object instance reference  * set it in primary constructor prior to constructor body

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrFunctionToJsTransformer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrFunctionToJsTransformer.kt
@@ -6,19 +6,22 @@
 package org.jetbrains.kotlin.ir.backend.js.transformers.irToJs
 
 import org.jetbrains.kotlin.ir.backend.js.utils.JsGenerationContext
+import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrConstructor
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.util.isObject
 import org.jetbrains.kotlin.js.backend.ast.JsFunction
 
 open class IrFunctionToJsTransformer : BaseIrElementToJsNodeTransformer<JsFunction, JsGenerationContext> {
     override fun visitSimpleFunction(declaration: IrSimpleFunction, context: JsGenerationContext): JsFunction {
         val funcName = context.getNameForSymbol(declaration.symbol)
-        return translateFunction(declaration, funcName, context)
+        return translateFunction(declaration, funcName, false, context)
     }
 
     override fun visitConstructor(declaration: IrConstructor, context: JsGenerationContext): JsFunction {
         assert(declaration.isPrimary)
         val funcName = context.getNameForSymbol(declaration.symbol)
-        return translateFunction(declaration, funcName, context)
+        val constructedClass = declaration.parent as IrClass
+        return translateFunction(declaration, funcName, constructedClass.isObject, context)
     }
 }
