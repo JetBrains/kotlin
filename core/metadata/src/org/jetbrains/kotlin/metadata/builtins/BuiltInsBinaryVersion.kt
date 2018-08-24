9c68f2be3666371194e54023fdafc77cd84633e9
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2018-08-24 13:01:53 +0200

    Advance metadata versions and JVM bytecode version
    JVM versions are increased in order to differentiate pre-1.3-M2 .class
    files where signatures mentioning inline classes were not mangled. Other
    versions are increased in case something similar will need to be
    detected

--- core/metadata/src/org/jetbrains/kotlin/metadata/builtins/BuiltInsBinaryVersion.kt
+++ core/metadata/src/org/jetbrains/kotlin/metadata/builtins/BuiltInsBinaryVersion.kt
@@ -19,7 +19,7 @@ class BuiltInsBinaryVersion(vararg numbers: Int) : BinaryVersion(*numbers) {
 
     companion object {
         @JvmField
-        val INSTANCE = BuiltInsBinaryVersion(1, 0, 4)
+        val INSTANCE = BuiltInsBinaryVersion(1, 0, 5)
 
         @JvmField
         val INVALID_VERSION = BuiltInsBinaryVersion()
