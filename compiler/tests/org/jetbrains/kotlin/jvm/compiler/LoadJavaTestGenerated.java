fd0eb6ee3434c38ccadc4e0f6e5f6f5f7745e513
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2018-08-27 11:49:05 +0300

    Implement support for class literals as annotation arguments...
    on deserialization and reflection

--- compiler/tests/org/jetbrains/kotlin/jvm/compiler/LoadJavaTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/jvm/compiler/LoadJavaTestGenerated.java
@@ -1712,6 +1712,11 @@ public class LoadJavaTestGenerated extends AbstractLoadJavaTest {
                 runTest("compiler/testData/loadJava/compiledKotlin/annotations/AnnotationInAnnotationArguments.kt");
             }
 
+            @TestMetadata("ClassLiteralArguments.kt")
+            public void testClassLiteralArguments() throws Exception {
+                runTest("compiler/testData/loadJava/compiledKotlin/annotations/ClassLiteralArguments.kt");
+            }
+
             @TestMetadata("EnumArgumentWithCustomToString.kt")
             public void testEnumArgumentWithCustomToString() throws Exception {
                 runTest("compiler/testData/loadJava/compiledKotlin/annotations/EnumArgumentWithCustomToString.kt");
