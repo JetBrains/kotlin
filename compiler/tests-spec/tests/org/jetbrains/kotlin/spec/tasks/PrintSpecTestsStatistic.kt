88d8bb84ac1f73656ca8f5d4c9c2a0ddf47384be
victor.petukhov <victor.petukhov@jetbrains.com>
2018-08-28 19:28:22 +0300

    Add not linked spec tests support in printSpecTestStatistic gradle task

--- compiler/tests-spec/tests/org/jetbrains/kotlin/spec/tasks/PrintSpecTestsStatistic.kt
+++ compiler/tests-spec/tests/org/jetbrains/kotlin/spec/tasks/PrintSpecTestsStatistic.kt
@@ -5,32 +5,68 @@
 
 package org.jetbrains.kotlin.spec.tasks
 
+import org.jetbrains.kotlin.spec.SpecTestsStatElement
+import org.jetbrains.kotlin.spec.SpecTestsStatElementType
 import org.jetbrains.kotlin.spec.TestsStatisticCollector
+import org.jetbrains.kotlin.spec.validators.SpecTestLinkedType
 
-fun main(args: Array<String>) {
-    val statistic = TestsStatisticCollector.collect()
+const val PRINT_BASE_INDENT = "  "
 
-    println("--------------------------------------------------")
+fun linkedSpecTestsPrint() {
     println("SPEC TESTS STATISTIC")
     println("--------------------------------------------------")
 
+    val statistic = TestsStatisticCollector.collect(SpecTestLinkedType.LINKED)
+
     for ((areaName, areaElement) in statistic) {
-        println("$areaName: ${areaElement.counter} tests")
-
+        println("$areaName: ${areaElement.number} tests")
         for ((sectionName, sectionElement) in areaElement.elements) {
-            println("  ${sectionName.toUpperCase()}: ${sectionElement.counter} tests")
-
+            println("  $sectionName: ${sectionElement.number} tests")
             for ((paragraphName, paragraphElement) in sectionElement.elements) {
                 val testsStatByType = mutableListOf<String>()
-
-                for ((typeName, typeElement) in paragraphElement.elements) {
-                    testsStatByType.add("$typeName: ${typeElement.counter}")
-                }
-
-                println("    PARAGRAPH $paragraphName: ${paragraphElement.counter} tests (${testsStatByType.joinToString(", ")})")
+                for ((typeName, typeElement) in paragraphElement.elements)
+                    testsStatByType.add(" [ $typeName: ${typeElement.number} ]")
+                print(PRINT_BASE_INDENT.repeat(2))
+                println("PARAGRAPH $paragraphName: ${paragraphElement.number} tests${testsStatByType.joinToString("")}")
             }
         }
     }
+}
 
+fun notLinkedSpecTestsCategoriesPrint(elements: Map<Any, SpecTestsStatElement>, level: Int = 1) {
+    for ((name, element) in elements) {
+        if (element.type == SpecTestsStatElementType.TYPE) {
+            print(" [ $name: ${element.number} ]")
+            continue
+        }
+
+        println()
+        print("${PRINT_BASE_INDENT.repeat(level)}$name: ${element.number} tests")
+
+        notLinkedSpecTestsCategoriesPrint(element.elements, level + 1)
+    }
+}
+
+fun notLinkedSpecTestsPrint() {
+    println("NOT LINKED SPEC TESTS STATISTIC")
     println("--------------------------------------------------")
+
+    val statistic = TestsStatisticCollector.collect(SpecTestLinkedType.NOT_LINKED)
+
+    for ((areaName, areaElement) in statistic) {
+        println("$areaName: ${areaElement.number} tests")
+        for ((sectionName, sectionElement) in areaElement.elements) {
+            print("  $sectionName: ${sectionElement.number} tests")
+            notLinkedSpecTestsCategoriesPrint(sectionElement.elements)
+            println()
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    println("==================================================")
+    linkedSpecTestsPrint()
+    println("==================================================")
+    notLinkedSpecTestsPrint()
+    println("==================================================")
 }
