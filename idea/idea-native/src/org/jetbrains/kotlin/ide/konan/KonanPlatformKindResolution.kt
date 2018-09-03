beb12b693f43251024024acb1429db88acf3375e
Dmitriy Dolovov <dmitriy.dolovov@jetbrains.com>
2018-09-03 16:50:49 +0300

    MPP: Keep original capabilities in descriptors created for all KLIBs

--- idea/idea-native/src/org/jetbrains/kotlin/ide/konan/KonanPlatformKindResolution.kt
+++ idea/idea-native/src/org/jetbrains/kotlin/ide/konan/KonanPlatformKindResolution.kt
@@ -6,24 +6,28 @@
 package org.jetbrains.kotlin.ide.konan
 
 import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.libraries.PersistentLibraryKind
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.io.exists
 import org.jetbrains.konan.KONAN_CURRENT_ABI_VERSION
 import org.jetbrains.konan.analyser.KonanAnalyzerFacade
-import org.jetbrains.konan.settings.KonanPaths
 import org.jetbrains.kotlin.builtins.DefaultBuiltIns
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.caches.resolve.IdePlatformKindResolution
 import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
 import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
-import org.jetbrains.kotlin.context.GlobalContextImpl
 import org.jetbrains.kotlin.context.ProjectContext
+import org.jetbrains.kotlin.idea.caches.project.LibraryInfo
 import org.jetbrains.kotlin.idea.caches.resolve.PlatformAnalysisSettings
 import org.jetbrains.kotlin.konan.file.File
+import org.jetbrains.kotlin.konan.library.KONAN_STDLIB_NAME
 import org.jetbrains.kotlin.konan.library.createKonanLibrary
 import org.jetbrains.kotlin.konan.utils.KonanFactories.DefaultDeserializedDescriptorFactory
+import java.nio.file.Path
+import java.nio.file.Paths
 
 class KonanPlatformKindResolution : IdePlatformKindResolution {
     override fun isLibraryFileForPlatform(virtualFile: VirtualFile): Boolean {
@@ -42,20 +46,45 @@ class KonanPlatformKindResolution : IdePlatformKindResolution {
         createKonanBuiltIns(projectContext)
 }
 
+val Module.isKonanModule: Boolean
+    get() {
+        val settings = KotlinFacetSettingsProvider.getInstance(project).getInitializedSettings(this)
+        return settings.platform.isKonan
+    }
+
 private fun createKonanBuiltIns(projectContext: ProjectContext): KotlinBuiltIns {
 
-    // TODO: it depends on a random project's stdlib, propagate the actual project here
-    val stdlibPath = ProjectManager.getInstance().openProjects.asSequence().mapNotNull { project ->
-        KonanPaths.getInstance(project).konanStdlib()?.takeIf { it.exists() }
-    }.firstOrNull()
+    // TODO: It depends on a random project's stdlib, propagate the actual project here.
+    val stdlib: Pair<Path, LibraryInfo>? = ProjectManager.getInstance().openProjects.asSequence().mapNotNull { project ->
 
-    if (stdlibPath != null) {
-        val library = createKonanLibrary(stdlibPath.File(), KONAN_CURRENT_ABI_VERSION)
+        ModuleManager.getInstance(project).modules.asSequence().filter { it.isKonanModule }.mapNotNull { module ->
+
+            var result: Pair<Path, LibraryInfo>? = null
+
+            ModuleRootManager.getInstance(module).orderEntries().forEachLibrary { library ->
+                if (library.name == KONAN_STDLIB_NAME) {
+                    val libraryInfo = LibraryInfo(project, library)
+                    val path = libraryInfo.getLibraryRoots().firstOrNull()?.let { Paths.get(it) }?.takeIf { it.exists() }
+                    if (path != null) result = path to libraryInfo
+                }
+
+                result == null
+            }
+            result
+        }
+    }.flatten().firstOrNull()
+
+    if (stdlib != null) {
+
+        val (path, libraryInfo) = stdlib
+        val library = createKonanLibrary(path.File(), KONAN_CURRENT_ABI_VERSION)
 
         val builtInsModule = DefaultDeserializedDescriptorFactory.createDescriptorAndNewBuiltIns(
             library,
             LanguageVersionSettingsImpl.DEFAULT,
-            projectContext.storageManager
+            projectContext.storageManager,
+            // This is to preserve "capabilities" from the original IntelliJ LibraryInfo:
+            customCapabilities = libraryInfo.capabilities
         )
         builtInsModule.setDependencies(listOf(builtInsModule))
 
