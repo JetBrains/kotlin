f6c61fbe300cd3ff7e6e586a363746cef37f0fcc
Dmitriy Dolovov <dmitriy.dolovov@jetbrains.com>
2018-09-03 17:37:53 +0300

    Remove useless TODOs

--- idea/idea-native/src/org/jetbrains/konan/KonanApplicationComponent.kt
+++ idea/idea-native/src/org/jetbrains/konan/KonanApplicationComponent.kt
@@ -10,12 +10,13 @@ import com.intellij.ide.util.TipAndTrickBean
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.extensions.Extensions
 import com.intellij.openapi.fileTypes.FileTypeManager
+import org.jetbrains.kotlin.konan.library.KLIB_FILE_EXTENSION
 
 class KonanApplicationComponent : ApplicationComponent {
     override fun getComponentName(): String = "KonanApplicationComponent"
 
     override fun initComponent() {
-        FileTypeManager.getInstance().associateExtension(ArchiveFileType.INSTANCE, "klib")
+        FileTypeManager.getInstance().associateExtension(ArchiveFileType.INSTANCE, KLIB_FILE_EXTENSION)
 
         val extensionPoint = Extensions.getRootArea().getExtensionPoint(TipAndTrickBean.EP_NAME)
         for (name in arrayOf("Kotlin.html", "Kotlin_project.html", "Kotlin_mix.html", "Kotlin_Java_convert.html")) {
