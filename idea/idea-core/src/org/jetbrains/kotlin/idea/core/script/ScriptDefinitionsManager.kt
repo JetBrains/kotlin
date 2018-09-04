21470e59f56038b7dfd72a8bfd5fe527814a1188
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-09-04 15:27:55 +0300

    Use correct classpath for Ide Scripting Console files during highlighting
    KT-25606
    
    (cherry picked from commit 3979ad7a1ddaf56a2064991b70793955c03b63af)

--- idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/ScriptDefinitionsManager.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/ScriptDefinitionsManager.kt
@@ -16,6 +16,7 @@
 
 package org.jetbrains.kotlin.idea.core.script
 
+import com.intellij.execution.console.IdeConsoleRootType
 import com.intellij.ide.projectView.impl.ProjectRootsUtil.isInTestSource
 import com.intellij.ide.scratch.ScratchFileService
 import com.intellij.openapi.application.ApplicationManager
@@ -38,6 +39,7 @@ import org.jetbrains.kotlin.idea.caches.project.getScriptRelatedModuleInfo
 import org.jetbrains.kotlin.idea.util.ProjectRootsUtil.isInContent
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.script.*
+import org.jetbrains.kotlin.script.util.scriptCompilationClasspathFromContextOrStlib
 import org.jetbrains.kotlin.scripting.compiler.plugin.KotlinScriptDefinitionAdapterFromNewAPI
 import org.jetbrains.kotlin.scripting.compiler.plugin.KotlinScriptDefinitionAdapterFromNewAPIBase
 import org.jetbrains.kotlin.utils.PathUtil
@@ -269,17 +271,15 @@ class BundledKotlinScriptDependenciesResolver(private val project: Project) : De
         val virtualFile = scriptContents.file?.let { VfsUtil.findFileByIoFile(it, true) }
 
         val javaHome = getScriptSDK(project, virtualFile)
-        val dependencies = ScriptDependencies(
-            javaHome = javaHome?.let(::File),
-            classpath = with(PathUtil.kotlinPathsForIdeaPlugin) {
-                listOf(
-                    reflectPath,
-                    stdlibPath,
-                    scriptRuntimePath
-                )
-            }
-        )
-        return dependencies.asSuccess()
+
+        var classpath = with(PathUtil.kotlinPathsForIdeaPlugin) {
+            listOf(reflectPath, stdlibPath, scriptRuntimePath)
+        }
+        if (ScratchFileService.getInstance().getRootType(virtualFile) is IdeConsoleRootType) {
+            classpath = scriptCompilationClasspathFromContextOrStlib(wholeClasspath = true) + classpath
+        }
+
+        return ScriptDependencies(javaHome = javaHome?.let(::File), classpath = classpath).asSuccess()
     }
 
     private fun getScriptSDK(project: Project, virtualFile: VirtualFile?): String? {
@@ -292,8 +292,8 @@ class BundledKotlinScriptDependenciesResolver(private val project: Project) : De
         }
 
         val jdk = ProjectRootManager.getInstance(project).projectSdk
-                ?: ProjectJdkTable.getInstance().allJdks.firstOrNull { sdk -> sdk.sdkType is JavaSdk }
-                ?: PathUtilEx.getAnyJdk(project)
+            ?: ProjectJdkTable.getInstance().allJdks.firstOrNull { sdk -> sdk.sdkType is JavaSdk }
+            ?: PathUtilEx.getAnyJdk(project)
         return jdk?.homePath
     }
 }
