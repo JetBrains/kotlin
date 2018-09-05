d24b0fd3b2b8510ed6a5cce6633070ace954ea9b
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-09-05 14:44:15 +0300

    Test: 'constructor' methods visibility

--- compiler/testData/codegen/bytecodeListing/inlineClasses/inlineClassConstructors.kt
+++ compiler/testData/codegen/bytecodeListing/inlineClasses/inlineClassConstructors.kt
@@ -2,4 +2,6 @@
 
 inline class Foo(val x: Int) {
     constructor(x: Long) : this(x.toInt())
+    internal constructor(x: Int, y: Int) : this(x + y)
+    private constructor(x: Short) : this(x.toInt())
 }
