43eeccbcb8ae6be114a0278f4cbde1d2282c1032
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-09-05 23:01:30 +0300

    Advance bootstrap to 1.3.0-dev-169

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ import proguard.gradle.ProGuardTask
 buildscript {
     extra["defaultSnapshotVersion"] = "1.3-SNAPSHOT"
 
-    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3.0-dev-25", onlySuccessBootstrap = true))
+    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3.0-dev-169", onlySuccessBootstrap = false))
 
     repositories {
         bootstrapKotlinRepo?.let(::maven)
