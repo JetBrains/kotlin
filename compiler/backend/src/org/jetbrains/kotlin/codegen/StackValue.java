da93ff1a3080410a3edddc51926e143a18b45b06
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-09-06 14:15:14 +0200

    Properly cast right-hand side value for inline property setter
     #KT-22649 Fixed

--- compiler/backend/src/org/jetbrains/kotlin/codegen/StackValue.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/StackValue.java
@@ -1619,7 +1619,10 @@ public abstract class StackValue {
                     putReceiver(v, false);
                 }
                 callGenerator.processAndPutHiddenParameters(true);
-                callGenerator.putValueIfNeeded(new JvmKotlinType(rightSide.type, rightSide.kotlinType), rightSide);
+                callGenerator.putValueIfNeeded(new JvmKotlinType(
+                                                       CollectionsKt.last(setter.getValueParameters()).getAsmType(),
+                                                       CollectionsKt.last(setterDescriptor.getValueParameters()).getType()),
+                                               rightSide);
                 callGenerator.putHiddenParamsIntoLocals();
                 callGenerator.genCall(setter, resolvedCall, false, codegen);
             }
