35f42c7d35a917b6a60a0869d2a3364f0f633cd7
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-09-06 20:52:39 +0300

    Raise deprecation level for kotlin.Synchronized and Volatile, remove replacements
    They are js-only but not supposed to be used in Kotlin/JS code, so their
    usages should be just removed.
    
    #KT-21703

--- libraries/stdlib/js/src/kotlin/concurrent.kt
+++ libraries/stdlib/js/src/kotlin/concurrent.kt
@@ -8,10 +8,10 @@ package kotlin
 import kotlin.contracts.*
 
 
-@Deprecated("Use Synchronized annotation from kotlin.jvm package", ReplaceWith("kotlin.jvm.Synchronized"), level = DeprecationLevel.WARNING)
+@Deprecated("Do not use Synchronized annotation in pure Kotlin/JS code", level = DeprecationLevel.ERROR)
 public typealias Synchronized = kotlin.jvm.Synchronized
 
-@Deprecated("Use Volatile annotation from kotlin.jvm package", ReplaceWith("kotlin.jvm.Volatile"), level = DeprecationLevel.WARNING)
+@Deprecated("Do not use Volatile annotation in pure Kotlin/JS code", level = DeprecationLevel.ERROR)
 public typealias Volatile = kotlin.jvm.Volatile
 
 @kotlin.internal.InlineOnly
