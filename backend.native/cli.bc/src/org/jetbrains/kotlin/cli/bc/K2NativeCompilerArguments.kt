669e38f56636634d81260f0ccda52722141e406e
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2018-09-10 17:33:42 +0300

    [compiler][command line] normalization of kotlin native compiler arguments.

--- backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2NativeCompilerArguments.kt
+++ backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2NativeCompilerArguments.kt
@@ -15,31 +15,31 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
     // Prepend them with a single dash.
     // Keep the list lexically sorted.
 
-    @Argument(value = "-enable_assertions", shortName = "-ea", description = "Enable runtime assertions in generated code")
+    @Argument(value = "-enable-assertions", deprecatedName = "-enable_assertions", shortName = "-ea", description = "Enable runtime assertions in generated code")
     var enableAssertions: Boolean = false
 
     @Argument(value = "-g", description = "Enable emitting debug information")
     var debug: Boolean = false
 
-    @Argument(value = "-generate_test_runner", shortName = "-tr", description = "Produce a runner for unit tests")
+    @Argument(value = "-generate-test-runner", deprecatedName = "-generate_test_runner", shortName = "-tr", description = "Produce a runner for unit tests")
     var generateTestRunner: Boolean = false
 
-    @Argument(value = "-includeBinary", shortName = "-ib", valueDescription = "<path>", description = "Pack external binary within the klib")
+    @Argument(value="-include-binary", deprecatedName = "-includeBinary", shortName = "-ib", valueDescription = "<path>", description = "Pack external binary within the klib")
     var includeBinaries: Array<String>? = null
 
     @Argument(value = "-library", shortName = "-l", valueDescription = "<path>", description = "Link with the library")
     var libraries: Array<String>? = null
 
-    @Argument(value = "-list_targets", description = "List available hardware targets")
+    @Argument(value = "-list-targets", deprecatedName = "-list_targets", description = "List available hardware targets")
     var listTargets: Boolean = false
 
     @Argument(value = "-manifest", valueDescription = "<path>", description = "Provide a maniferst addend file")
     var manifestFile: String? = null
 
-    @Argument(value = "-module_name", valueDescription = "<name>", description = "Spicify a name for the compilation module")
+    @Argument(value="-module-name", deprecatedName = "-module_name", valueDescription = "<name>", description = "Spicify a name for the compilation module")
     var moduleName: String? = null
 
-    @Argument(value = "-nativelibrary", shortName = "-nl", valueDescription = "<path>", description = "Include the native bitcode library")
+    @Argument(value = "-native-library", deprecatedName = "-nativelibrary", shortName = "-nl", valueDescription = "<path>", description = "Include the native bitcode library")
     var nativeLibraries: Array<String>? = null
 
     @Argument(value = "-nodefaultlibs", description = "Don't link the libraries from dist/klib automatically")
@@ -51,7 +51,7 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
     @Argument(value = "-nopack", description = "Don't pack the library into a klib file")
     var nopack: Boolean = false
 
-    @Argument(value = "-linkerOpts", valueDescription = "<arg>", description = "Pass arguments to linker", delimiter = " ")
+    @Argument(value="-linker-options", deprecatedName = "-linkerOpts", valueDescription = "<arg>", description = "Pass arguments to linker", delimiter = " ")
     var linkerArguments: Array<String>? = null
 
     @Argument(value = "-nostdlib", description = "Don't link with stdlib")
@@ -81,55 +81,55 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
     // Make sure to prepend them with a double dash.
     // Keep the list lexically sorted.
 
-    @Argument(value = "--check_dependencies", description = "Check dependencies and download the missing ones")
+    @Argument(value="-Xcheck-dependencies", deprecatedName = "--check_dependencies", description = "Check dependencies and download the missing ones")
     var checkDependencies: Boolean = false
 
-    @Argument(value = "--disable", valueDescription = "<Phase>", description = "Disable backend phase")
+    @Argument(value = "-Xdisable", deprecatedName = "--disable", valueDescription = "<Phase>", description = "Disable backend phase")
     var disablePhases: Array<String>? = null
 
-    @Argument(value = "--enable", valueDescription = "<Phase>", description = "Enable backend phase")
+    @Argument(value = "-Xenable", deprecatedName = "--enable", valueDescription = "<Phase>", description = "Enable backend phase")
     var enablePhases: Array<String>? = null
 
-    @Argument(value = "--list_phases", description = "List all backend phases")
+    @Argument(value=  "-Xlist-phases", deprecatedName = "--list_phases", description = "List all backend phases")
     var listPhases: Boolean = false
 
-    @Argument(value = "--print_bitcode", description = "Print llvm bitcode")
+    @Argument(value = "-Xprint-bitcode", deprecatedName = "--print_bitcode", description = "Print llvm bitcode")
     var printBitCode: Boolean = false
 
-    @Argument(value = "--print_descriptors", description = "Print descriptor tree")
+    @Argument(value = "-Xprint-descriptors", deprecatedName = "--print_descriptors", description = "Print descriptor tree")
     var printDescriptors: Boolean = false
 
-    @Argument(value = "--print_ir", description = "Print IR")
+    @Argument(value = "-Xprint-ir", deprecatedName = "--print_ir", description = "Print IR")
     var printIr: Boolean = false
 
-    @Argument(value = "--print_ir_with_descriptors", description = "Print IR with descriptors")
+    @Argument(value = "-Xprint-ir-with-descriptors", deprecatedName = "--print_ir_with_descriptors", description = "Print IR with descriptors")
     var printIrWithDescriptors: Boolean = false
 
-    @Argument(value = "--print_locations", description = "Print locations")
+    @Argument(value = "-Xprint-locations", deprecatedName = "--print_locations", description = "Print locations")
     var printLocations: Boolean = false
 
-    @Argument(value = "--purge_user_libs", description = "Don't link unused libraries even explicitly specified")
+    @Argument(value="-Xpurge-user-libs", deprecatedName = "--purge_user_libs", description = "Don't link unused libraries even explicitly specified")
     var purgeUserLibs: Boolean = false
 
-    @Argument(value = "--runtime", valueDescription = "<path>", description = "Override standard 'runtime.bc' location")
+    @Argument(value = "-Xruntime", deprecatedName = "--runtime", valueDescription = "<path>", description = "Override standard 'runtime.bc' location")
     var runtimeFile: String? = null
 
-    @Argument(value = "--temporary_files_dir", valueDescription = "<path>", description = "Save temporary files to the given directory")
+    @Argument(value = "-Xtemporary-files-dir", deprecatedName = "--temporary_files_dir", valueDescription = "<path>", description = "Save temporary files to the given directory")
     var temporaryFilesDir: String? = null
 
-    @Argument(value = "--time", description = "Report execution time for compiler phases")
+    @Argument(value = "-Xtime", deprecatedName = "--time", description = "Report execution time for compiler phases")
     var timePhases: Boolean = false
 
-    @Argument(value = "--verbose", valueDescription = "<Phase>", description = "Trace phase execution")
+    @Argument(value = "-Xverbose", deprecatedName = "--verbose", valueDescription = "<Phase>", description = "Trace phase execution")
     var verbosePhases: Array<String>? = null
 
-    @Argument(value = "--verify_bitcode", description = "Verify llvm bitcode after each method")
+    @Argument(value = "-Xverify-bitcode", deprecatedName = "--verify_bitcode", description = "Verify llvm bitcode after each method")
     var verifyBitCode: Boolean = false
 
-    @Argument(value = "--verify_descriptors", description = "Verify descriptor tree")
+    @Argument(value = "-Xverify-descriptors", deprecatedName = "--verify_descriptors", description = "Verify descriptor tree")
     var verifyDescriptors: Boolean = false
 
-    @Argument(value = "--verify_ir", description = "Verify IR")
+    @Argument(value = "-Xverify-ir", deprecatedName = "--verify_ir", description = "Verify IR")
     var verifyIr: Boolean = false
 
     @Argument(
