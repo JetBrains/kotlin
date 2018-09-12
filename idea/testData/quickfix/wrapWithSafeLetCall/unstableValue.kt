81406a2bac23a8a56d3e88b34855f49f912a0798
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-09-12 17:31:05 +0300

    Change .hashCode() to .bar() for some nullability fixes tests
    Hash code can be no more used because in 1.3 we have nullable extension.
    This fixes two wrap with safe let call tests and
    one surround with null check test.

--- idea/testData/quickfix/wrapWithSafeLetCall/unstableValue.kt
+++ idea/testData/quickfix/wrapWithSafeLetCall/unstableValue.kt
@@ -1,13 +1,14 @@
 // "Wrap with '?.let { ... }' call" "false"
 // WITH_RUNTIME
 // ACTION: Add non-null asserted (!!) call
-// ACTION: Introduce local variable
 // ACTION: Replace with safe (?.) call
 // ERROR: Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type Int?
+
+fun Int.bar() {}
 
 class My(var x: Int?) {
 
     fun foo() {
-        x<caret>.hashCode()
+        x<caret>.bar()
     }
 }
