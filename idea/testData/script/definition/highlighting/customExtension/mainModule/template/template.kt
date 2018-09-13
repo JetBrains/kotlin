4b138ae2b8a63e79e27567fddeecf6c5af60b458
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-09-13 12:16:06 +0300

    Do not use async script  dependencies resolver in tests

--- idea/testData/script/definition/highlighting/customExtension/mainModule/template/template.kt
+++ idea/testData/script/definition/highlighting/customExtension/mainModule/template/template.kt
@@ -1,12 +1,6 @@
 package custom.scriptDefinition
 
-import java.io.File
-import kotlin.script.dependencies.*
-import kotlin.script.experimental.dependencies.*
-import kotlin.script.experimental.location.*
-import kotlin.script.templates.ScriptTemplateDefinition
 import kotlin.script.experimental.annotations.KotlinScript
-import kotlin.script.experimental.annotations.KotlinScriptEvaluator
 
 @KotlinScript(displayName = "Kotlin Script with custom extension", fileExtension = "mykts")
 open class Template
