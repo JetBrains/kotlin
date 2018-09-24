cd26de8251bf12e9ce1bfe0fdfa1d8c6566a1f59
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2018-09-24 14:14:49 +0300

    Add methods for combine InvocationKind's
    There is two methods added -- `or` and `and`
    
    `or` is used by CFA for combining two kinds that came from different
    edges of control flow graph
    
    `and` is analog of `+` operator for invocation kinds

--- compiler/resolution/src/org/jetbrains/kotlin/contracts/description/InvocationKind.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/contracts/description/InvocationKind.kt
@@ -5,12 +5,31 @@
 
 package org.jetbrains.kotlin.contracts.description
 
-enum class InvocationKind {
-    ZERO,
-    AT_MOST_ONCE,
-    EXACTLY_ONCE,
-    AT_LEAST_ONCE,
-    UNKNOWN
+import kotlin.math.max
+import kotlin.math.min
+
+enum class InvocationKind(private val left: Int, private val right: Int) {
+    ZERO(0, 0),          // 0..0
+    AT_MOST_ONCE(0, 1),  // 0..1
+    EXACTLY_ONCE(1, 1),  // 1..1
+    AT_LEAST_ONCE(1, 2), // 1..*
+    UNKNOWN(0, 2);       // 0..*
+
+    companion object {
+        private fun fromRange(left: Int, right: Int): InvocationKind = when (min(left, 1) to min(right, 2)) {
+            0 to 0 -> ZERO
+            0 to 1 -> AT_MOST_ONCE
+            1 to 1 -> EXACTLY_ONCE
+            1 to 2 -> AT_LEAST_ONCE
+            0 to 2 -> UNKNOWN
+            else -> throw IllegalArgumentException()
+        }
+
+        fun or(x: InvocationKind, y: InvocationKind): InvocationKind = fromRange(min(x.left, y.left), max(x.right, y.right))
+
+        fun and(x: InvocationKind, y: InvocationKind): InvocationKind = fromRange(x.left + y.left, x.right + y.right)
+    }
+
 }
 
 fun InvocationKind.isDefinitelyVisited(): Boolean = this == InvocationKind.EXACTLY_ONCE || this == InvocationKind.AT_LEAST_ONCE
