ba71bbde6abf474cef29120d23f8c148645224eb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2018-09-24 14:08:32 +0300

    Extract `InvocationKind` class into separate file

---
+++ compiler/resolution/src/org/jetbrains/kotlin/contracts/description/InvocationKind.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.contracts.description
+
+enum class InvocationKind {
+    AT_MOST_ONCE,
+    EXACTLY_ONCE,
+    AT_LEAST_ONCE,
+    UNKNOWN
+}
+
+fun InvocationKind.isDefinitelyVisited(): Boolean = this == InvocationKind.EXACTLY_ONCE || this == InvocationKind.AT_LEAST_ONCE
+fun InvocationKind.canBeRevisited(): Boolean = this == InvocationKind.UNKNOWN || this == InvocationKind.AT_LEAST_ONCE
+
