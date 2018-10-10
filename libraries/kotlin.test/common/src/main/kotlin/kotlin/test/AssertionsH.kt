ae9e3e87c48849861018b15a3fdfd3843ea9e858
Piotr Krzeminski <git@krzeminski.it>
2018-10-10 22:47:52 +0200

    kotlin-test: Add documentation for value returned by assertFailsWith
    A follow-up after https://youtrack.jetbrains.com/issue/KT-27418
    
    Co-authored-by: Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
    (cherry picked from commit 3c46bb9d03e78279c492c949d495d3493ed43c35)

--- libraries/kotlin.test/common/src/main/kotlin/kotlin/test/AssertionsH.kt
+++ libraries/kotlin.test/common/src/main/kotlin/kotlin/test/AssertionsH.kt
@@ -17,5 +17,8 @@ expect fun todo(block: () -> Unit)
  * Asserts that a [block] fails with a specific exception of type [exceptionClass] being thrown.
  *
  * If the assertion fails, the specified [message] is used unless it is null as a prefix for the failure message.
+ *
+ * @return An exception of the expected exception type [T] that successfully caught.
+ * The returned exception can be inspected further, for example by asserting its property values.
  */
 expect fun <T : Throwable> assertFailsWith(exceptionClass: KClass<T>, message: String?, block: () -> Unit): T
