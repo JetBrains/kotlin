7df814c6acbfd76fd52b977590faa0f7c36c7f3f
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-10-12 00:28:12 +0900

    Add BACKING_FIELD_REQUIRED check

--- idea/src/org/jetbrains/kotlin/idea/inspections/PropertyVarInspection.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/PropertyVarInspection.kt
@@ -8,17 +8,23 @@ package org.jetbrains.kotlin.idea.inspections
 import com.intellij.codeInspection.IntentionWrapper
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.quickfix.ChangeVariableMutabilityFix
 import org.jetbrains.kotlin.idea.references.mainReference
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.anyDescendantOfType
+import org.jetbrains.kotlin.resolve.BindingContext
 
 class PropertyVarInspection : AbstractKotlinInspection() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
         propertyVisitor(fun(property: KtProperty) {
             if (property.isLocal || !property.isVar || property.initializer == null || property.setter != null) return
             val getter = property.getter ?: return
+            val descriptor = property.analyze()[BindingContext.DECLARATION_TO_DESCRIPTOR, property] as? PropertyDescriptor ?: return
+            if (property.analyze()[BindingContext.BACKING_FIELD_REQUIRED, descriptor] == false) return
             if (getter.hasBackingFieldReference()) return
+
             holder.registerProblem(
                 property.valOrVarKeyword,
                 "Property setter does not influence its getter result",
