2dd8b526fb1f34e44cdca5fa1df2604f9288c1fa
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-10-16 13:08:47 +0300

    Live templates test: fix typo & cleanup

--- idea/idea-live-templates/tests/org/jetbrains/kotlin/idea/liveTemplates/LiveTemplatesTest.kt
+++ idea/idea-live-templates/tests/org/jetbrains/kotlin/idea/liveTemplates/LiveTemplatesTest.kt
@@ -29,51 +29,51 @@ class LiveTemplatesTest : KotlinLightCodeInsightFixtureTestCase() {
     }
 
     fun testSout() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSout_BeforeCall() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSout_BeforeCallSpace() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSout_BeforeBinary() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSout_InCallArguments() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSout_BeforeQualifiedCall() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSout_AfterSemicolon() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSoutf() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSoutf_InCompanion() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSerr() {
-        paremeterless()
+        parameterless()
     }
 
     fun testMain() {
-        paremeterless()
+        parameterless()
     }
 
     fun testMaina() {
-        paremeterless()
+        parameterless()
     }
 
     fun testSoutv() {
@@ -86,7 +86,7 @@ class LiveTemplatesTest : KotlinLightCodeInsightFixtureTestCase() {
     }
 
     fun testSoutp() {
-        paremeterless()
+        parameterless()
     }
 
     fun testFun0() {
@@ -232,7 +232,7 @@ class LiveTemplatesTest : KotlinLightCodeInsightFixtureTestCase() {
         doTestIfnInn()
     }
 
-    private fun paremeterless() {
+    private fun parameterless() {
         start()
 
         checkAfter()
@@ -272,17 +272,18 @@ class LiveTemplatesTest : KotlinLightCodeInsightFixtureTestCase() {
         val project = project
         UIUtil.invokeAndWaitIfNeeded(Runnable {
             CommandProcessor.getInstance().executeCommand(
-                    project,
-                    {
-                        templateState!!.nextTab()
-                    },
-                    "nextTab",
-                    null)
+                project,
+                {
+                    templateState!!.nextTab()
+                },
+                "nextTab",
+                null
+            )
         })
     }
 
     private fun nextTab(times: Int) {
-        for (i in 0..times - 1) {
+        for (i in 0 until times) {
             nextTab()
         }
     }
