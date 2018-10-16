64800266f5cb63152be3a172c2e0367b0430f823
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-10-16 18:05:06 +0300

    CLion/AppCode: Provide proper default platform instead of JVM

--- idea/src/org/jetbrains/kotlin/idea/facet/facetUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/facet/facetUtils.kt
@@ -39,6 +39,7 @@ import org.jetbrains.kotlin.idea.core.isAndroidModule
 import org.jetbrains.kotlin.idea.framework.KotlinSdkType
 import org.jetbrains.kotlin.idea.platform.tooling
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
+import org.jetbrains.kotlin.platform.DefaultIdeTargetPlatformKindProvider
 import org.jetbrains.kotlin.platform.IdePlatform
 import org.jetbrains.kotlin.platform.IdePlatformKind
 import org.jetbrains.kotlin.platform.impl.JvmIdePlatformKind
@@ -51,7 +52,7 @@ var Module.hasExternalSdkConfiguration: Boolean
 private fun getDefaultTargetPlatform(module: Module, rootModel: ModuleRootModel?): IdePlatform<*, *> {
     val platformKind = IdePlatformKind.ALL_KINDS.firstOrNull {
         getRuntimeLibraryVersions(module, rootModel, it).isNotEmpty()
-    } ?: JvmIdePlatformKind
+    } ?: DefaultIdeTargetPlatformKindProvider.defaultPlatform.kind
     if (platformKind == JvmIdePlatformKind) {
         var jvmTarget = Kotlin2JvmCompilerArgumentsHolder.getInstance(module.project).settings.jvmTarget?.let { JvmTarget.fromString(it) }
         if (jvmTarget == null) {
