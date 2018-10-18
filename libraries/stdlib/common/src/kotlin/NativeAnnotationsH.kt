89310a250aca089d55eadfb6f67d4bdfd70b9d93
Pavel Punegov <pavel.punegov@jetbrains.com>
2018-10-18 17:14:00 +0300

    Add ThreadLocal and SharedImmutable native annotations as OptionalExpectation to kotlin.native.concurrent

---
+++ libraries/stdlib/common/src/kotlin/NativeAnnotationsH.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.native.concurrent
+
+/**
+ * Marks a top level variable with a backing field or an object as thread local.
+ * The object remains mutable and it is possible to change its state,
+ * but every thread will have a distinct copy of this object,
+ * so changes in one thread are not reflected in another.
+ *
+ * PLEASE NOTE THAT THIS ANNOTATION MAY GO AWAY IN UPCOMING RELEASES.
+ */
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.BINARY)
+@OptionalExpectation
+public expect annotation class ThreadLocal()
+
+/**
+ * Marks a top level variable with a backing field or an object as immutable.
+ * It is possible to share such object between multiple threads, but it becomes deeply frozen,
+ * so no changes can be made to its state or the state of objects it refers to.
+ *
+ * PLEASE NOTE THAT THIS ANNOTATION MAY GO AWAY IN UPCOMING RELEASES.
+ */
+@Target(AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.BINARY)
+@OptionalExpectation
+public expect annotation class SharedImmutable()
+
