5ee6a6037db1531247b0262bcd0e49aab3bdc1db
Sergey Igushkin <hotkeytlt@gmail.com>
2018-10-23 16:06:38 +0300

    (minor) Move the unused source sets check to the taskGraph.whenReady
    This is a workaround for Android compilations being configured in
    an afterEvaluate callback.
    
    Issue #KT-26963 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/UnusedSourceSetsChecker.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/UnusedSourceSetsChecker.kt
@@ -35,7 +35,8 @@ object UnusedSourceSetsChecker {
     }
 
     fun checkSourceSets(project: Project) {
-        project.afterEvaluate {
+        // TODO once Android compilations are configured eagerly, move this to afterEvaluate { ... } instead of taskGraph.whenReady { ... }
+        project.gradle.taskGraph.whenReady { _ ->
             val compilationsBySourceSet = compilationsBySourceSet(project)
             val unusedSourceSets = project.kotlinExtension.sourceSets.filter {
                 compilationsBySourceSet[it]?.isEmpty() ?: true
