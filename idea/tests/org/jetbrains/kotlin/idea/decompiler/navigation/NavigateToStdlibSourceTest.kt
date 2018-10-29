2d0ecf8f50af7ef30fed77b4656bfecd9ba4181f
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-10-29 14:46:53 +0300

    Update testdata: println is moved to ioH.kt

--- idea/tests/org/jetbrains/kotlin/idea/decompiler/navigation/NavigateToStdlibSourceTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/decompiler/navigation/NavigateToStdlibSourceTest.kt
@@ -29,11 +29,11 @@ class NavigateToStdlibSourceTest : KotlinCodeInsightTestCase() {
     private val FILE_TEXT = "fun foo() { <caret>println() }"
 
     fun testRefToPrintlnWithJVM() {
-        doTest("Console.kt", ModuleKind.KOTLIN_JVM_WITH_STDLIB_SOURCES)
+        doTest("ioH.kt", ModuleKind.KOTLIN_JVM_WITH_STDLIB_SOURCES)
     }
 
     fun testRefToPrintlnWithJVMAndJS() {
-        doTest("Console.kt", ModuleKind.KOTLIN_JVM_WITH_STDLIB_SOURCES, ModuleKind.KOTLIN_JAVASCRIPT)
+        doTest("ioH.kt", ModuleKind.KOTLIN_JVM_WITH_STDLIB_SOURCES, ModuleKind.KOTLIN_JAVASCRIPT)
     }
 
     fun testRefToPrintlnWithJS() {
