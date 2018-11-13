0397af79b436a69c0eb07820e1b6104d5999da06
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-11-13 16:18:43 +0300

    JS: convert from java to kotlin JsProgramFragment

---
+++ js/js.ast/src/org/jetbrains/kotlin/js/backend/ast/JsProgramFragment.kt
@@ -0,0 +1,18 @@
+// Copyright (c) 2011, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+package org.jetbrains.kotlin.js.backend.ast
+
+import java.util.*
+
+class JsProgramFragment(val scope: JsScope) {
+    val importedModules: MutableList<JsImportedModule> = ArrayList()
+    val imports: MutableMap<String, JsExpression> = LinkedHashMap()
+    val declarationBlock = JsGlobalBlock()
+    val exportBlock = JsGlobalBlock()
+    val initializerBlock = JsGlobalBlock()
+    val nameBindings: MutableList<JsNameBinding> = ArrayList()
+    val classes: MutableMap<JsName, JsClassModel> = LinkedHashMap()
+    val inlineModuleMap: MutableMap<String, JsExpression> = LinkedHashMap()
+}
