f8a102a023068fd8ed3b3c3949c88a1ef46ecd37
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2018-11-14 21:22:58 +0700

    CLion/AppCode: Redraw debugger views after applying custom LLDB formatting for K/N
    To avoid displaying frame variables as raw C pointers for the first hit breakpoint.

--- cidr-native/src/org/jetbrains/konan/debugger/KonanValueRendererFactory.kt
+++ cidr-native/src/org/jetbrains/konan/debugger/KonanValueRendererFactory.kt
@@ -7,6 +7,7 @@ package org.jetbrains.konan.debugger
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.intellij.xdebugger.XDebuggerManager
 import com.jetbrains.cidr.execution.debugger.backend.lldb.LLDBDriver
 import com.jetbrains.cidr.execution.debugger.evaluation.ValueRendererFactory
 import com.jetbrains.cidr.execution.debugger.evaluation.renderers.ValueRenderer
@@ -33,8 +34,11 @@ class KonanValueRendererFactory : ValueRendererFactory {
 
 private fun initLLDBDriver(project: Project, driver: LLDBDriver) {
     KotlinNativePathProvider.getInstance(project).getKotlinNativePath()?.let { path ->
+        // Apply custom formatting for Kotlin/Native structs:
         val lldbPrettyPrinters = Paths.get(path, "tools", "konan_lldb.py")
         driver.executeConsoleCommand("command script import \"$lldbPrettyPrinters\"")
+        // Re-draw debugger views that may be drawn by concurrent threads while formatting hasn't been applied:
+        XDebuggerManager.getInstance(project).currentSession?.rebuildViews()
     }
 
     driver.executeConsoleCommand("settings set target.process.thread.step-avoid-regexp ^::Kotlin_")
