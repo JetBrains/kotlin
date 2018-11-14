2d514d410fc9528ec9cb32b4041222a6f612a4f2
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-11-14 20:07:48 +0300

    Update testData due to platform changes

--- idea/testData/kdoc/multiModuleSamples/simple/code/usage.kt
+++ idea/testData/kdoc/multiModuleSamples/simple/code/usage.kt
@@ -5,6 +5,6 @@ fun some<caret>() {
 
 }
 
-//INFO: <div class='definition'><pre><font color="808080"><i>usage.kt</i></font><br>public fun <b>some</b>(): Unit</pre></div><div class='content'></div><table class='sections'><tr><td valign='top' class='section'><p>Samples:</td><td><p><a href="psi_element://samples.sample"><code>samples.sample</code></a><pre><code>
+//INFO: <div class='definition'><pre><font color="808080"><i>usage.kt</i></font><br>public fun <b>some</b>(): Unit</pre></div><div class='content'></div><table class='sections'><tr><td valign='top' class='section'><p>Samples:</td><td valign='top'><p><a href="psi_element://samples.sample"><code>samples.sample</code></a><pre><code>
 //INFO: println("Hello, world")
 //INFO: </code></pre></td></table>
