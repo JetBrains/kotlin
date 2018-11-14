58bb26e42583ad1d36e1c5199a205a74c75f3a26
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-11-14 19:47:13 +0300

    Update testData due to changes in stdlib
    New function equals, and kotlin.js.JsName in common

--- idea/idea-completion/testData/smart/BooleanArgumentExpected.kt
+++ idea/idea-completion/testData/smart/BooleanArgumentExpected.kt
@@ -13,4 +13,5 @@ fun bar() {
 // EXIST: { lookupString: "maxOf", tailText:"(a: Boolean, b: Boolean, c: Boolean) (kotlin.comparisons)", typeText:"Boolean"}
 // EXIST: { lookupString: "minOf", tailText:"(a: Boolean, b: Boolean) (kotlin.comparisons)","typeText":"Boolean"}
 // EXIST: { lookupString: "minOf", tailText:"(a: Boolean, b: Boolean, c: Boolean) (kotlin.comparisons)", typeText:"Boolean"}
+// EXIST: {"lookupString":"equals","tailText":"(other: Any?) (kotlin)","typeText":"Boolean"}
 // NOTHING_ELSE
