5d8fa11b07701c53c2e251766ea1f647bafc347b
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-11-16 13:29:08 +0300

    Implement another method in KotlinShortNamesCache with processor

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/KotlinShortNamesCache.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/KotlinShortNamesCache.kt
@@ -53,6 +53,10 @@ class KotlinShortNamesCache(private val project: Project) : PsiShortNamesCache()
                 KotlinFileFacadeShortNameIndex.INSTANCE.processAllKeys(project, processor)
     }
 
+    override fun processAllClassNames(processor: Processor<String>, scope: GlobalSearchScope, filter: IdFilter?): Boolean {
+        return processAllClassNames(processor)
+    }
+
     /**
      * Return kotlin class names from project sources which should be visible from java.
      */
