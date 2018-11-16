762c3bd89f1cd6e78fbde9d19fda8a16b29f38de
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-11-16 18:23:13 +0300

    Introduce KtReferenceSearch to use LocalSearchScope, when possible
    This fixes a set of problems in J2K tests in 183
    #KT-28368 Fixed

--- idea/src/org/jetbrains/kotlin/idea/findUsages/findUsageUtils.kt
+++ idea/src/org/jetbrains/kotlin/idea/findUsages/findUsageUtils.kt
@@ -21,13 +21,18 @@ import com.intellij.find.findUsages.FindUsagesOptions
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 import com.intellij.psi.impl.light.LightMemberReference
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.usageView.UsageInfo
 import com.intellij.usages.UsageViewManager
+import com.intellij.util.Query
 import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.asJava.toLightElements
 import org.jetbrains.kotlin.idea.references.KtReference
 import org.jetbrains.kotlin.psi.KtConstructor
 import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtPsiUtil
 import org.jetbrains.kotlin.utils.SmartList
 
 fun PsiElement.processAllExactUsages(
@@ -74,4 +79,15 @@ fun KtDeclaration.processAllUsages(
             },
             options
     )
+}
+
+object ReferencesSearchScopeHelper {
+    fun search(declaration: KtDeclaration, defaultScope: SearchScope? = null): Query<PsiReference> {
+        val enclosingElement = KtPsiUtil.getEnclosingElementForLocalDeclaration(declaration)
+        return when {
+            enclosingElement != null -> ReferencesSearch.search(declaration, LocalSearchScope(enclosingElement))
+            defaultScope != null -> ReferencesSearch.search(declaration, defaultScope)
+            else -> ReferencesSearch.search(declaration)
+        }
+    }
 }
