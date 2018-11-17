f48551243d5ef3d0ca378f909092afdcc706fe79
penglong <penglong@codemao.cn>
2018-11-17 20:52:22 +0800

    Add `JumboEnumSet` for enum types those with more than 32 elements

--- libraries/stdlib/js/src/kotlin/collections/EnumSet.kt
+++ libraries/stdlib/js/src/kotlin/collections/EnumSet.kt
@@ -6,34 +6,36 @@
 package kotlin.collections
 
 actual abstract class EnumSet<E : Enum<E>>(
-    protected val clazz: JsClass<E>,
+    protected val type: JsClass<E>,
     protected val universe: Array<E>
-) : MutableSet<E> {
+) : AbstractMutableSet<E>(), MutableSet<E> {
 
     actual abstract override val size: Int
     actual abstract override fun isEmpty(): Boolean
     actual abstract override fun contains(element: @UnsafeVariance E): Boolean
-    actual abstract override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
     actual abstract override fun iterator(): MutableIterator<E>
     actual abstract override fun add(element: E): Boolean
     actual abstract override fun remove(element: E): Boolean
-    actual abstract override fun addAll(elements: Collection<E>): Boolean
-    actual abstract override fun removeAll(elements: Collection<E>): Boolean
-    actual abstract override fun retainAll(elements: Collection<E>): Boolean
     actual abstract override fun clear()
-    abstract fun addAll()
+
+    /** Fill up itself and return itself */
+    internal abstract fun filledUp(): EnumSet<E>
 }
 
-fun <E : Enum<E>> enumSetOf(clazz: JsClass<E>, universe: Array<E>): EnumSet<E> = if (universe.size > 32) {
-    TODO() // implementation JumboEnumSet
+fun <E : Enum<E>> enumSetOf(clazz: JsClass<E>, universe: Array<E>): EnumSet<E> = if (universe.size > Int.SIZE_BITS) {
+    JumboEnumSet(clazz, universe)
 } else {
     RegularEnumSet(clazz, universe)
 }
 
-@kotlin.internal.InlineOnly
-public actual inline fun <reified E : Enum<E>> enumSetOf(): EnumSet<E> {
-    return enumSetOf(E::class.js, enumValues<E>())
+fun <E : Enum<E>> enumSetAllOf(clazz: JsClass<E>, universe: Array<E>): EnumSet<E> = if (universe.size > Int.SIZE_BITS) {
+    JumboEnumSet(clazz, universe).filledUp()
+} else {
+    RegularEnumSet(clazz, universe).filledUp()
 }
+
+@kotlin.internal.InlineOnly
+public actual inline fun <reified E : Enum<E>> enumSetOf(): EnumSet<E> = enumSetOf(E::class.js, enumValues<E>())
 
 @kotlin.internal.InlineOnly
 public actual inline fun <reified E : Enum<E>> enumSetOf(element: E): EnumSet<E> {
@@ -54,10 +56,4 @@ public actual inline fun <reified E : Enum<E>> enumSetOf(vararg elements: E): En
 }
 
 @kotlin.internal.InlineOnly
-public actual inline fun <reified E : Enum<E>> enumSetAllOf(): EnumSet<E> {
-    val result = enumSetOf(E::class.js, enumValues<E>())
-
-    result.addAll()
-
-    return result
-}
+public actual inline fun <reified E : Enum<E>> enumSetAllOf(): EnumSet<E> = enumSetAllOf(E::class.js, enumValues<E>())
