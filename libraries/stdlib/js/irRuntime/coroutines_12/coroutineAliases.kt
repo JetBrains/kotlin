d7ff6c48eaf7abd23c60e43524d89c78b4008f7b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-11-19 18:11:03 +0300

    Leave only 1.3 JS IR runtime sources
    Update exclusions after merging coroutines sources into stdlib.

--- libraries/stdlib/js/irRuntime/coroutines_12/coroutineAliases.kt
+++
@@ -1,9 +0,0 @@
-
-package kotlin.js.coroutineAliases
-
-import kotlin.coroutines.experimental.*
-
-
-typealias ContinuationAlias<T> = Continuation<T>
-typealias CoroutineContextAlias = CoroutineContext
-typealias ContinuationInterceptorAlias = ContinuationInterceptor
