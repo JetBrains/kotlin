f9546a4d7405fe1938f61025ff8820bf8fafcd8e
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-11-19 17:52:12 +0300

    Support @JvmField annotation in light classes

---
+++ compiler/testData/asJava/ultraLightClasses/jvmField.kt
@@ -0,0 +1,20 @@
+class A {
+    @JvmField
+    val a: Collection<*> = emptyList()
+    @JvmField
+    var b: Int = 1
+
+    companion object {
+        @JvmField
+        val c: Collection<*> = emptyList()
+        @JvmField
+        var d: Int = 1
+    }
+}
+
+interface B {
+    companion object {
+        @JvmField
+        val a: Collection<*> = emptyList()
+    }
+}
