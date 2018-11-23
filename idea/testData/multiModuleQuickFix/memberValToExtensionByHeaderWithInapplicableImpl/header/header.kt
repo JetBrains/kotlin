2af9efa4a098d87feb8370637bcfd62f3bbdc034
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-11-23 15:43:39 +0300

    Fix failing test (related to KT-28381, KT-28382 commits)

--- idea/testData/multiModuleQuickFix/memberValToExtensionByHeaderWithInapplicableImpl/header/header.kt
+++ idea/testData/multiModuleQuickFix/memberValToExtensionByHeaderWithInapplicableImpl/header/header.kt
@@ -1,8 +1,6 @@
 // "Convert member to extension" "false"
 // ACTION: Convert property to function
-// ACTION: Introduce backing property
 // ACTION: Move to companion object
-// ACTION: Move to constructor
 
 expect class Foo {
     val <caret>foo: Int
