cb7f7b1de5c4c3506559c81dc6e10abc01b8f646
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-11-23 17:02:41 +0300

    Report "use of non-const Kotlin property" also on some assignments
    #KT-25536 Fixed

--- idea/testData/multiFileInspections/fakeJvmFieldConstant/before/First/src/JavaUser.java
+++ idea/testData/multiFileInspections/fakeJvmFieldConstant/before/First/src/JavaUser.java
@@ -21,4 +21,24 @@ public class JavaUser {
                 break;
         }
     }
+
+    public void fau() {
+        byte b1 = KotlinPropertiesKt.importantNumber; // Dangerous
+        byte b2 = KotlinPropertiesKt.constantNumber; // Safe (const)
+        byte b3 = 0 + KotlinPropertiesKt.importantNumber; // Dangerous
+        int b4 = 0 + KotlinPropertiesKt.importantNumber; // Safe (types are the same)
+        b4 += KotlinPropertiesKt.importantNumber; // Safe (modification)
+        long l1 = KotlinPropertiesKt.importantNumber; // Safe (type widening)
+        b2 = KotlinPropertiesKt.importantNumber; // Dangerous
+        b1 = KotlinPropertiesKt.constantNumber; // Safe (const)
+        b3 = (0 + KotlinPropertiesKt.importantNumber); // Dangerous
+        b3 = (byte) (KotlinPropertiesKt.importantNumber); // Safe (explicit cast)
+        b3 = convert(KotlinPropertiesKt.importantNumber); // Safe (explicit cast)
+    }
+
+    public byte convert(int x) {
+        return (byte) x;
+    }
+
+    byte importantNumber = KotlinPropertiesKt.importantNumber; // Also dangerous
 }
