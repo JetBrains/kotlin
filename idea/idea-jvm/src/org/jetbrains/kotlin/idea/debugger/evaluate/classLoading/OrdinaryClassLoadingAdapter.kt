6590497b8d82e8b2955e98626a0eecb1e8793c5d
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-11-28 02:24:00 +0900

    Revert "Minor: Remove a deprecated method usage"
    The method is still actual in IDEA 181 and Android Studio 3.1/3.2.

--- idea/idea-jvm/src/org/jetbrains/kotlin/idea/debugger/evaluate/classLoading/OrdinaryClassLoadingAdapter.kt
+++ idea/idea-jvm/src/org/jetbrains/kotlin/idea/debugger/evaluate/classLoading/OrdinaryClassLoadingAdapter.kt
@@ -20,6 +20,7 @@ import com.intellij.debugger.engine.DebugProcessImpl
 import com.intellij.debugger.engine.evaluation.EvaluateException
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
 import com.intellij.debugger.impl.ClassLoadingUtils
+import com.intellij.debugger.impl.DebuggerUtilsEx
 import com.intellij.openapi.projectRoots.JavaSdkVersion
 import com.intellij.openapi.util.SystemInfo
 import com.sun.jdi.ClassLoaderReference
@@ -110,7 +111,7 @@ class OrdinaryClassLoadingAdapter : ClassLoadingAdapter {
             val defineMethod = classLoaderType.concreteMethodByName("defineClass", "(Ljava/lang/String;[BII)Ljava/lang/Class;")
             val nameObj = vm.mirrorOf(name)
 
-            context.keep(nameObj)
+            DebuggerUtilsEx.keep(nameObj, context)
 
             process.invokeMethod(
                     context, classLoader, defineMethod,
