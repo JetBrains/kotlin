93fff99d8d7d2c9a63d174043265605af705b387
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-11-28 13:57:28 +0300

    Extract stub library file from "Result is Result" inspection tests

--- idea/testData/inspectionsLocal/coroutines/resultIsSuccessOrFailure/simpleBlock.kt
+++ idea/testData/inspectionsLocal/coroutines/resultIsSuccessOrFailure/simpleBlock.kt
@@ -1,10 +1,6 @@
 // FIX: Add '.getOrThrow()' to function result (breaks use-sites!)
 package kotlin
 
-class Result<T>(val value: T?) {
-    fun getOrThrow(): T = value ?: throw AssertionError("")
-}
-
 fun <caret>incorrectBlock(arg: Boolean, arg2: Boolean?): Result<Int> {
     if (arg) {
         return Result(1)
