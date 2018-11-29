93fd29021b93c91ad33182e8b5d67ee88dc71933
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2018-11-29 16:18:14 +0300

    [JS IR BE] Fix inline classes workarounds
    - Remove uninitialized return type check
    - Remove unbound classifier symbol check

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/utils/kotlinTypeBasedUtils.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/utils/kotlinTypeBasedUtils.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.toIrType
 import org.jetbrains.kotlin.ir.types.toKotlinType
+import org.jetbrains.kotlin.ir.util.SymbolTable
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.types.CommonSupertypes
 import org.jetbrains.kotlin.types.typeUtil.isSubtypeOf
@@ -23,7 +24,8 @@ fun IrType.isPrimitiveArray() = KotlinBuiltIns.isPrimitiveArray(toKotlinType())
 fun IrType.getPrimitiveArrayElementType() = KotlinBuiltIns.getPrimitiveArrayElementType(toKotlinType())
 
 @Deprecated("Use pure Ir helper")
-fun List<IrType>.commonSupertype() = CommonSupertypes.commonSupertype(map(IrType::toKotlinType)).toIrType()!!
+fun List<IrType>.commonSupertype(symbolTable: SymbolTable) =
+    CommonSupertypes.commonSupertype(map(IrType::toKotlinType)).toIrType(symbolTable)!!
 
 @Deprecated("Use pure Ir helper")
 fun IrType.isSubtypeOf(superType: IrType) = toKotlinType().isSubtypeOf(superType.toKotlinType())
