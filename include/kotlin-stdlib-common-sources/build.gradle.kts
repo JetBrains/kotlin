d90a92ae0662d857386c731b54870a46b830dcf5
Pavel Punegov <pavel.punegov@jetbrains.com>
2018-12-03 14:48:40 +0300

    Add stdlib to composite build of Kotlin + Kotlin/Native

---
+++ include/kotlin-stdlib-common-sources/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm")
+}
+
+val sources by configurations.creating
+
+dependencies {
+    sources(project(":kotlin-stdlib-common", configuration = "sources"))
+}
+
+artifacts {
+    add("runtime", provider { sources.singleFile })
+}
