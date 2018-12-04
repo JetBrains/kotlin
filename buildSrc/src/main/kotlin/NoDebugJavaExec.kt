a2fd356b747d5e4faf44bdf55dee8e10f540c6a6
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-12-04 18:59:39 +0300

    Build: Add NoDebugJavaExec to filter out debugger arguments added by Idea
    Workaround for IDEA-200192:
    IDEA makes all JavaExec tasks not up-to-date and attaches debugger making our breakpoints trigger during irrelevant task execution

---
+++ buildSrc/src/main/kotlin/NoDebugJavaExec.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+import org.gradle.api.tasks.JavaExec
+
+/**
+ * Workaround for IDEA-200192:
+ * IDEA makes all JavaExec tasks not up-to-date and attaches debugger making our breakpoints trigger during irrelevant task execution
+ */
+open class NoDebugJavaExec : JavaExec() {
+    private fun String.isDebuggerArgument(): Boolean =
+        startsWith("-agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=")
+
+    override fun setJvmArgs(arguments: MutableList<String>?) {
+        super.setJvmArgs(arguments?.filterNot { it.isDebuggerArgument() })
+    }
+}
