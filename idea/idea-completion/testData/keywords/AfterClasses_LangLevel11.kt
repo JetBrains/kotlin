383933eb32a3313fea9210ed5bcfe571bcf5a232
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-12-05 17:23:01 +0900

    Keyword completion: add "class/interface/object + filename" completion for top level
    #KT-28394 Fixed

--- idea/idea-completion/testData/keywords/AfterClasses_LangLevel11.kt
+++ idea/idea-completion/testData/keywords/AfterClasses_LangLevel11.kt
@@ -14,25 +14,32 @@ class B {
 
 // EXIST:  abstract
 // EXIST:  class
+// EXIST:  class AfterClasses_LangLevel11
 // EXIST:  enum class
+// EXIST:  enum class AfterClasses_LangLevel11
 // EXIST:  final
 // EXIST:  fun
 // EXIST:  internal
 // EXIST:  object
+// EXIST:  object AfterClasses_LangLevel11
 // EXIST:  open
 // EXIST:  private
 // EXIST:  public
 // EXIST:  interface
+// EXIST:  interface AfterClasses_LangLevel11
 // EXIST:  val
 // EXIST:  var
 // EXIST:  operator
 // EXIST:  infix
 // EXIST:  sealed class
+// EXIST:  sealed class AfterClasses_LangLevel11
 // EXIST:  data class
+// EXIST:  data class AfterClasses_LangLevel11
 // EXIST:  inline
 // EXIST:  tailrec
 // EXIST:  external
 // EXIST:  annotation class
+// EXIST:  annotation class AfterClasses_LangLevel11
 // EXIST:  const val
 // EXIST:  suspend
 // EXIST:  typealias
