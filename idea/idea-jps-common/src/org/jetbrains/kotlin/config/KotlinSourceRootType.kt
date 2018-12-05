273dbc823e39ecb357b7e9402abf7e2efc08d566
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2018-12-05 14:25:58 +0300

    Fix "Kotlin not configured" issue in common MPP tests.
    #KT-27718 Fixed

--- idea/idea-jps-common/src/org/jetbrains/kotlin/config/KotlinSourceRootType.kt
+++ idea/idea-jps-common/src/org/jetbrains/kotlin/config/KotlinSourceRootType.kt
@@ -7,17 +7,22 @@ package org.jetbrains.kotlin.config
 
 import org.jetbrains.jps.model.ex.JpsElementTypeBase
 import org.jetbrains.jps.model.java.JavaSourceRootProperties
+import org.jetbrains.jps.model.java.JavaSourceRootType
 import org.jetbrains.jps.model.java.JpsJavaExtensionService
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType
 
-sealed class KotlinSourceRootType : JpsElementTypeBase<JavaSourceRootProperties>(), JpsModuleSourceRootType<JavaSourceRootProperties> {
-    object Source : KotlinSourceRootType()
-    object TestSource : KotlinSourceRootType()
+sealed class KotlinSourceRootType(val isTest: Boolean) : JpsElementTypeBase<JavaSourceRootProperties>(), JpsModuleSourceRootType<JavaSourceRootProperties>, KotlinRootType {
+    object Source : KotlinSourceRootType(false)
+    object TestSource : KotlinSourceRootType(true)
 
     override fun createDefaultProperties() = JpsJavaExtensionService.getInstance().createSourceRootProperties("")
 
     companion object {
         val ALL_SOURCES = setOf(Source, TestSource)
     }
+
+    override fun isTestRoot() = isTest
+
+    override fun equals(other: Any?) = if (super.equals(other)) true else isSameRootType(this, other)
 }
 
