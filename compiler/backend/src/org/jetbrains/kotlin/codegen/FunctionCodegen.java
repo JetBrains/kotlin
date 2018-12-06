5e40ab7c50679fd8cfea1589d9a20cbe5579a392
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2018-12-06 15:38:56 +0900

    Support new convention for local variables name mangling: '$receiver' -> '$this$<label>' (KT-26913)

--- compiler/backend/src/org/jetbrains/kotlin/codegen/FunctionCodegen.java
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/FunctionCodegen.java
@@ -782,7 +782,7 @@ public class FunctionCodegen {
         }
 
         generateLocalVariablesForParameters(mv,
-                                            jvmMethodSignature,
+                                            jvmMethodSignature, functionDescriptor,
                                             thisType, methodBegin, methodEnd, functionDescriptor.getValueParameters(),
                                             destructuredParametersForSuspendLambda,
                                             AsmUtil.isStaticMethod(ownerKind, functionDescriptor), typeMapper, shiftForDestructuringVariables
@@ -792,6 +792,7 @@ public class FunctionCodegen {
     public static void generateLocalVariablesForParameters(
             @NotNull MethodVisitor mv,
             @NotNull JvmMethodSignature jvmMethodSignature,
+            @NotNull FunctionDescriptor functionDescriptor,
             @Nullable Type thisType,
             @NotNull Label methodBegin,
             @NotNull Label methodEnd,
@@ -800,13 +801,15 @@ public class FunctionCodegen {
             KotlinTypeMapper typeMapper
     ) {
         generateLocalVariablesForParameters(
-                mv, jvmMethodSignature, thisType, methodBegin, methodEnd, valueParameters, Collections.emptyList(), isStatic, typeMapper,
+                mv, jvmMethodSignature, functionDescriptor,
+                thisType, methodBegin, methodEnd, valueParameters, Collections.emptyList(), isStatic, typeMapper,
                 0);
     }
 
     private static void generateLocalVariablesForParameters(
             @NotNull MethodVisitor mv,
             @NotNull JvmMethodSignature jvmMethodSignature,
+            @NotNull FunctionDescriptor functionDescriptor,
             @Nullable Type thisType,
             @NotNull Label methodBegin,
             @NotNull Label methodEnd,
@@ -844,6 +847,10 @@ public class FunctionCodegen {
                         nameForDestructuredParameter == null
                         ? computeParameterName(i, parameter)
                         : nameForDestructuredParameter;
+            }
+            else if (kind == JvmMethodParameterKind.RECEIVER) {
+                parameterName = AsmUtil.getNameForReceiverParameter(
+                        functionDescriptor, typeMapper.getBindingContext(), typeMapper.getLanguageVersionSettings());
             }
             else {
                 String lowercaseKind = kind.name().toLowerCase();
