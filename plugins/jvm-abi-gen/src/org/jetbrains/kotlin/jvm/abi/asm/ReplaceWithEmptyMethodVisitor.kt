b3be3c6520e8cd336263077d056b67ba89e7f528
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2018-12-07 19:28:53 +0300

    Reuse TransformationMethodVisitor in ABI gen plugin

--- plugins/jvm-abi-gen/src/org/jetbrains/kotlin/jvm/abi/asm/ReplaceWithEmptyMethodVisitor.kt
+++ plugins/jvm-abi-gen/src/org/jetbrains/kotlin/jvm/abi/asm/ReplaceWithEmptyMethodVisitor.kt
@@ -5,36 +5,19 @@
 
 package org.jetbrains.kotlin.jvm.abi.asm
 
+import org.jetbrains.kotlin.codegen.TransformationMethodVisitor
 import org.jetbrains.org.objectweb.asm.*
+import org.jetbrains.org.objectweb.asm.tree.MethodNode
 
 internal class ReplaceWithEmptyMethodVisitor(
-    private val newMaxLocals: Int,
-    private val delegate: MethodVisitor,
-    api: Int
-) : MethodVisitor(api, null) {
-    override fun visitCode() {
-        delegate.visitCode()
-        delegate.visitMaxs(0, newMaxLocals)
-        delegate.visitEnd()
-    }
-
-    override fun visitParameter(name: String?, access: Int) {
-        delegate.visitParameter(name, access)
-    }
-
-    override fun visitParameterAnnotation(parameter: Int, desc: String?, visible: Boolean): AnnotationVisitor =
-        delegate.visitParameterAnnotation(parameter, desc, visible)
-
-    override fun visitAnnotationDefault(): AnnotationVisitor =
-        delegate.visitAnnotationDefault()
-
-    override fun visitAnnotation(desc: String?, visible: Boolean): AnnotationVisitor =
-        delegate.visitAnnotation(desc, visible)
-
-    override fun visitTypeAnnotation(typeRef: Int, typePath: TypePath?, desc: String?, visible: Boolean): AnnotationVisitor =
-        delegate.visitTypeAnnotation(typeRef, typePath, desc, visible)
-
-    override fun visitAttribute(attr: Attribute?) {
-        delegate.visitAttribute(attr)
+    delegate: MethodVisitor,
+    access: Int,
+    name: String,
+    desc: String,
+    signature: String?,
+    exceptions: Array<out String>?
+) : TransformationMethodVisitor(delegate, access, name, desc, signature, exceptions, api = AbiExtensionAsmApiVersion) {
+    override fun performTransformations(methodNode: MethodNode) {
+        methodNode.instructions.clear()
     }
 }
