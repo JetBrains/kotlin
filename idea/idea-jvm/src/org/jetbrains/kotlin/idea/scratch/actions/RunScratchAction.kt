1702c8df25fcb14d2e69900914292f89f4a83699
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-12-11 10:31:57 +0300

    Scratch: implement stop action
    (cherry picked from commit 2511f70bd9b006c5594898120040f3bb2dc0cb1e)
    
    ^KT-28643 Fixed

--- idea/idea-jvm/src/org/jetbrains/kotlin/idea/scratch/actions/RunScratchAction.kt
+++ idea/idea-jvm/src/org/jetbrains/kotlin/idea/scratch/actions/RunScratchAction.kt
@@ -23,11 +23,8 @@ import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.project.DumbService
 import com.intellij.task.ProjectTaskManager
 import org.jetbrains.kotlin.idea.KotlinBundle
-import org.jetbrains.kotlin.idea.scratch.ScratchFile
-import org.jetbrains.kotlin.idea.scratch.ScratchFileLanguageProvider
-import org.jetbrains.kotlin.idea.scratch.getScratchPanelFromSelectedEditor
+import org.jetbrains.kotlin.idea.scratch.*
 import org.jetbrains.kotlin.idea.scratch.output.ScratchOutputHandlerAdapter
-import org.jetbrains.kotlin.idea.scratch.printDebugMessage
 import org.jetbrains.kotlin.idea.scratch.LOG as log
 
 class RunScratchAction : ScratchAction(
@@ -75,11 +72,13 @@ class RunScratchAction : ScratchAction(
 
         executor.addOutputHandler(object : ScratchOutputHandlerAdapter() {
             override fun onStart(file: ScratchFile) {
-                e.presentation.isEnabled = false
+                ScratchCompilationSupport.start(file, executor)
+                scratchPanel.updateToolbar()
             }
 
             override fun onFinish(file: ScratchFile) {
-                e.presentation.isEnabled = true
+                ScratchCompilationSupport.stop()
+                scratchPanel.updateToolbar()
             }
         })
 
@@ -108,5 +107,22 @@ class RunScratchAction : ScratchAction(
         } else {
             executeScratch()
         }
+    }
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+
+        e.presentation.isEnabled = !ScratchCompilationSupport.isAnyInProgress()
+
+        if (e.presentation.isEnabled) {
+            e.presentation.text = templatePresentation.text
+        } else {
+            e.presentation.text = "Other Scratch file execution is in progress"
+        }
+
+        val project = e.project ?: return
+        val panel = getScratchPanelFromSelectedEditor(project) ?: return
+
+        e.presentation.isVisible = !ScratchCompilationSupport.isInProgress(panel.scratchFile)
     }
 }
