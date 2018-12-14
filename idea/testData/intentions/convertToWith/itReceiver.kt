61198f6afb7ac8173ee9432f4e4950335560960d
Toshiaki Kameyama <kmymtsak@gmail.com>
2018-12-14 19:10:20 +0900

    Add "Convert to also" intention
    #KT-28699 Fixed

--- idea/testData/intentions/convertToWith/itReceiver.kt
+++ idea/testData/intentions/convertToWith/itReceiver.kt
@@ -1,16 +1,13 @@
 // WITH_RUNTIME
-// IS_APPLICABLE: false
 
 class MyClass {
     fun foo1() = Unit
     fun foo2() = Unit
     fun foo3() = Unit
 
-    fun foo4(a: MyClass) {
-        a.let {
-            it.foo1()<caret>
-            it.foo2()
-            it.foo3()
-        }
+    fun foo4(it: MyClass) {
+        it.foo1()<caret>
+        it.foo2()
+        it.foo3()
     }
 }
