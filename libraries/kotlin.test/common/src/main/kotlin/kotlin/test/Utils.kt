d6237ed9f515d8c2dcdcc506e69f02637d7247e1
Pavel Punegov <pavel.punegov@jetbrains.com>
2018-12-17 19:26:06 +0300

    DefaultAsserter object. Add function to replace constructor invoke.

--- libraries/kotlin.test/common/src/main/kotlin/kotlin/test/Utils.kt
+++ libraries/kotlin.test/common/src/main/kotlin/kotlin/test/Utils.kt
@@ -9,9 +9,4 @@ internal fun messagePrefix(message: String?) = if (message == null) "" else "$me
 internal expect fun lookupAsserter(): Asserter
 
 @PublishedApi // required to get stable name as it's called from box tests
-internal fun overrideAsserter(value: Asserter?): Asserter? {
-    // TODO: Replace with return _asserter.also { _asserter = value } after KT-17540 is fixed
-    val previous = _asserter
-    _asserter = value
-    return previous
-}
+internal fun overrideAsserter(value: Asserter?): Asserter? = _asserter.also { _asserter = value }
