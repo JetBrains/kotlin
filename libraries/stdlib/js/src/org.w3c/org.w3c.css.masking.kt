bc9e2122077afdcd6dbadd01881b91730ae71f42
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2018-12-18 15:07:34 +0100

    Support ClipboardEvent in definitions generated from idl

--- libraries/stdlib/js/src/org.w3c/org.w3c.css.masking.kt
+++ libraries/stdlib/js/src/org.w3c/org.w3c.css.masking.kt
@@ -12,6 +12,7 @@ package org.w3c.css.masking
 import kotlin.js.*
 import org.khronos.webgl.*
 import org.w3c.dom.*
+import org.w3c.dom.clipboard.*
 import org.w3c.dom.css.*
 import org.w3c.dom.events.*
 import org.w3c.dom.mediacapture.*
