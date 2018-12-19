160f7aef9ebff5254e20caa7512b896d2dacb4b5
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2018-12-19 10:11:49 +0300

    Update Kotlin/Native: RC2 1.1.0.5323

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/utils/NativeCompilerDownloader.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/utils/NativeCompilerDownloader.kt
@@ -25,7 +25,7 @@ class NativeCompilerDownloader(
 ) {
 
     internal companion object {
-        val DEFAULT_KONAN_VERSION = KonanVersionImpl(MetaVersion.RC1, 1, 1, 0, 5323)
+        val DEFAULT_KONAN_VERSION = KonanVersionImpl(MetaVersion.RC2, 1, 1, 0, 5323)
         const val BASE_DOWNLOAD_URL = "https://download.jetbrains.com/kotlin/native/builds"
     }
 
