d76cb32781ce256c17370ecec55aef2b2e486b6e
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-12-21 12:12:45 +0100

    Specify JVM target backend for test with java type inheritance

--- compiler/testData/codegen/box/classes/kt2288.kt
+++ compiler/testData/codegen/box/classes/kt2288.kt
@@ -1,6 +1,5 @@
-// IGNORE_BACKEND: JS_IR
 // TODO: Enable when JS backend supports Java class library
-// IGNORE_BACKEND: JS, NATIVE
+// TARGET_BACKEND: JVM
 public open class Test(): java.util.RandomAccess, Cloneable, java.io.Serializable
 {
         public override fun clone(): Test = Test() // Override 'clone()' with more precise type 'Test'
