3e6f8db97576c2857437e58c52768606e0f049a5
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2018-12-21 10:34:54 +0100

    Specify JVM target backend for test with 'import kotlin.reflect.jvm'

--- compiler/testData/codegen/box/reflection/mapping/openSuspendFun.kt
+++ compiler/testData/codegen/box/reflection/mapping/openSuspendFun.kt
@@ -1,7 +1,6 @@
-// IGNORE_BACKEND: JVM_IR
-// IGNORE_BACKEND: JS_IR
 // WITH_REFLECT
-// IGNORE_BACKEND: JS, NATIVE
+// IGNORE_BACKEND: JVM_IR
+// TARGET_BACKEND: JVM
 
 import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.reflect.jvm.javaMethod
