571ee4a1fee74846bff5387d49067785df9b0ffd
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2018-12-24 21:43:26 +0300

    Don't ruin indentation for comments inside expression declarations (KT-23295)
     #KT-23295 Fixed

---
+++ idea/testData/formatter/CommentInExpressionBodies.kt
@@ -0,0 +1,17 @@
+fun foo() =
+  // comment
+  1
+
+val some =
+                            // Comment
+                            2
+
+fun some() {
+    val (a, b) =
+    // Some
+    1 to 3
+}
+
+fun nice() =
+ /* Ha! */
+2
