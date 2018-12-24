9eef85a4842633b3753e386ca1dcf2f14c88f8be
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-12-24 16:43:37 +0300

    Minor: add non-intrinsified cases for javaObjectType/javaPrimitiveType

--- compiler/testData/codegen/box/inlineClasses/kt28920_javaPrimitiveType.kt
+++ compiler/testData/codegen/box/inlineClasses/kt28920_javaPrimitiveType.kt
@@ -3,5 +3,9 @@
 
 fun box(): String {
     if (UInt::class.javaPrimitiveType != null) throw AssertionError()
+
+    val uIntClass = UInt::class
+    if (uIntClass.javaPrimitiveType != null) throw AssertionError()
+
     return "OK"
 }
