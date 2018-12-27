5338b5881add5d71c1006e4e4ea05ef2abcb732c
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2018-12-27 13:12:38 +0100

    Minor, uncomment previously non-working code in test data

--- compiler/testData/codegen/box/intrinsics/defaultObjectMapping.kt
+++ compiler/testData/codegen/box/intrinsics/defaultObjectMapping.kt
@@ -1,5 +1,5 @@
 // TARGET_BACKEND: JVM
-
+// IGNORE_BACKEND: JVM_IR
 // WITH_RUNTIME
 
 import kotlin.test.assertEquals
@@ -8,15 +8,12 @@ fun box(): String {
     assertEquals(java.lang.Integer.MIN_VALUE, Int.MIN_VALUE)
     assertEquals(java.lang.Byte.MAX_VALUE, Byte.MAX_VALUE)
 
-/*
-// TODO: uncomment when callable references to object members are supported
     assertEquals("MIN_VALUE", (Int.Companion::MIN_VALUE).name)
     assertEquals("MAX_VALUE", (Double.Companion::MAX_VALUE).name)
     assertEquals("MIN_VALUE", (Float.Companion::MIN_VALUE).name)
     assertEquals("MAX_VALUE", (Long.Companion::MAX_VALUE).name)
     assertEquals("MIN_VALUE", (Short.Companion::MIN_VALUE).name)
     assertEquals("MAX_VALUE", (Byte.Companion::MAX_VALUE).name)
-*/
 
     return "OK"
 }
