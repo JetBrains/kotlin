66ccb990305fd00fa98d22ac7246c60a859814b3
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2018-12-31 20:31:34 +0300

    ~ fix function class descriptor issues

--- core/descriptors/src/org/jetbrains/kotlin/builtins/functions/FunctionClassDescriptor.kt
+++ core/descriptors/src/org/jetbrains/kotlin/builtins/functions/FunctionClassDescriptor.kt
@@ -11,7 +11,7 @@ import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.descriptors.annotations.Annotations
 import org.jetbrains.kotlin.descriptors.impl.AbstractClassDescriptor
 import org.jetbrains.kotlin.descriptors.impl.TypeParameterDescriptorImpl
-import org.jetbrains.kotlin.incremental.components.NoLookupLocation
+import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.resolve.DescriptorUtils.COROUTINES_PACKAGE_FQ_NAME_RELEASE
@@ -104,14 +104,15 @@ class FunctionClassDescriptor(
     override fun getDeclaredTypeParameters() = parameters
 
     private inner class FunctionTypeConstructor : AbstractClassTypeConstructor(storageManager) {
+        val functionClassId = ClassId(BUILT_INS_PACKAGE_FQ_NAME, Name.identifier("Function"))
+        val kFunctionClassId = ClassId(KOTLIN_REFLECT_FQ_NAME, Name.identifier("KFunction"))
+
         override fun computeSupertypes(): Collection<KotlinType> {
             val result = ArrayList<KotlinType>(2)
 
-            fun add(packageFragments: List<PackageFragmentDescriptor>, name: Name) {
-                val descriptor = packageFragments
-                    .mapNotNull { it.getMemberScope().getContributedClassifier(name, NoLookupLocation.FROM_BUILTINS) as? ClassDescriptor }
-                    .firstOrNull()
-                    ?: error("Class $name not found")
+            fun add(id: ClassId) {
+                val moduleDescriptor = containingDeclaration.containingDeclaration
+                val descriptor = moduleDescriptor.findClassAcrossModuleDependencies(id) ?: error("TODO")
 
                 val typeConstructor = descriptor.typeConstructor
 
@@ -125,32 +126,24 @@ class FunctionClassDescriptor(
 
             when (functionKind) {
                 Kind.SuspendFunction -> // SuspendFunction$N<...> <: Function
-                    add(getBuiltInPackage(BUILT_INS_PACKAGE_FQ_NAME), Name.identifier("Function"))
+                    add(functionClassId)
                 Kind.KSuspendFunction -> // KSuspendFunction$N<...> <: KFunction
-                    add(getBuiltInPackage(KOTLIN_REFLECT_FQ_NAME), Name.identifier("KFunction"))
-                Kind.Function ->
-                    add(getBuiltInPackage(BUILT_INS_PACKAGE_FQ_NAME), Name.identifier("Function"))
-                Kind.KFunction ->
-                    add(getBuiltInPackage(KOTLIN_REFLECT_FQ_NAME), Name.identifier("KFunction"))
+                    add(kFunctionClassId)
+                Kind.Function -> // Function$N <: Function
+                    add(functionClassId)
+                Kind.KFunction -> // KFunction$N <: KFunction
+                    add(kFunctionClassId)
             }
 
             // For K{Suspend}Function{n}, add corresponding numbered {Suspend}Function{n} class, e.g. {Suspend}Function2 for K{Suspend}Function2
             when (functionKind) {
-                Kind.KFunction -> add(getBuiltInPackage(BUILT_INS_PACKAGE_FQ_NAME), Kind.Function.numberedClassName(arity))
-                Kind.KSuspendFunction -> add(
-                    getBuiltInPackage(COROUTINES_PACKAGE_FQ_NAME_RELEASE),
-                    Kind.SuspendFunction.numberedClassName(arity)
-                )
-                else -> {
-                }
+                Kind.KFunction ->
+                    add(ClassId(BUILT_INS_PACKAGE_FQ_NAME, Kind.Function.numberedClassName(arity)))
+                Kind.KSuspendFunction ->
+                    add(ClassId(COROUTINES_PACKAGE_FQ_NAME_RELEASE, Kind.SuspendFunction.numberedClassName(arity)))
             }
 
             return result.toList()
-        }
-
-        private fun getBuiltInPackage(fqName: FqName): List<PackageFragmentDescriptor> {
-            val packageView = containingDeclaration.containingDeclaration.getPackage(fqName)
-            return packageView.fragments
         }
 
         override fun getParameters() = this@FunctionClassDescriptor.parameters
