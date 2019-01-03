d4f5cdef6acdd9f82a9c83f54a4c14ee260942b4
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-01-03 13:22:03 +0100

    Test for obsolete KT-16496
     #KT-16496 Obsolete

--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/InlineDefaultValuesTestsGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/InlineDefaultValuesTestsGenerated.java
@@ -89,6 +89,11 @@ public class InlineDefaultValuesTestsGenerated extends AbstractInlineDefaultValu
         runTest("compiler/testData/codegen/boxInline/defaultValues/kt14564_2.kt");
     }
 
+    @TestMetadata("kt16496.kt")
+    public void testKt16496() throws Exception {
+        runTest("compiler/testData/codegen/boxInline/defaultValues/kt16496.kt");
+    }
+
     @TestMetadata("kt18689.kt")
     public void testKt18689() throws Exception {
         runTest("compiler/testData/codegen/boxInline/defaultValues/kt18689.kt");
