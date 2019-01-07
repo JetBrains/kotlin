8ce7112123102f49f71e7b9e98e8dbef48d909d7
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-01-07 10:41:45 +0100

    New tests for TYPE_USE annotations in enums and inner classes
     Main test data  (testName.txt) a not totally valid cause of IDEA-205039.
     Javac test data (testName.javac.txt) a not valid
     cause of type annotations support absence.
     Runtime tests are disabled cause reflection support absence.

---
+++ compiler/testData/loadJava/compiledJava/annotations/AnnotatedTypeInEnumClassConstructor2.java
@@ -0,0 +1,17 @@
+// SKIP_IN_RUNTIME_TEST
+package test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+class AnnotatedParameterInInnerClassConstructor {
+
+    @Target({ElementType.TYPE_USE, ElementType.PARAMETER})
+    public @interface Anno {
+        String value();
+    }
+
+    class JavaEnum {
+        JavaEnum(@Anno("a") String a , @Anno("b")  String b) {}
+    }
+}
