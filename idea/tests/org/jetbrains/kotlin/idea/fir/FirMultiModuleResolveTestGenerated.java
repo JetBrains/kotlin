fba7d23a88ed164d0def00347ce0eb9c41626eed
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-11 15:09:36 +0300

    Regenerate FIR test

--- idea/tests/org/jetbrains/kotlin/idea/fir/FirMultiModuleResolveTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/fir/FirMultiModuleResolveTestGenerated.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the license/LICENSE.txt file.
  */
 
@@ -21,13 +21,16 @@ import java.util.regex.Pattern;
 @TestDataPath("$PROJECT_ROOT")
 @RunWith(JUnit3RunnerWithInners.class)
 public class FirMultiModuleResolveTestGenerated extends AbstractFirMultiModuleResolveTest {
+    private void runTest(String testDataFilePath) throws Exception {
+        KotlinTestUtils.runTest(this::doTest, TargetBackend.ANY, testDataFilePath);
+    }
+
     public void testAllFilesPresentInMultiModule() throws Exception {
         KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("idea/testData/fir/multiModule"), Pattern.compile("^([^\\.]+)$"), TargetBackend.ANY, false);
     }
 
     @TestMetadata("basic")
     public void testBasic() throws Exception {
-        String fileName = KotlinTestUtils.navigationMetadata("idea/testData/fir/multiModule/basic/");
-        doTest(fileName);
+        runTest("idea/testData/fir/multiModule/basic/");
     }
 }
