48cf6ff31300111db1a1ef3d6154da2ff97c7707
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-01-14 15:47:18 +0300

    Put default value to vars iff they are not boxed
     #KT-24672 Fixed

---
+++ compiler/testData/codegen/bytecodeText/localInitializationLVT/boxingVar.kt
@@ -0,0 +1,12 @@
+// IGNORE_BACKEND: JVM_IR
+
+fun test(): java.lang.Integer {
+    var c: java.lang.Integer
+    run {
+        c = java.lang.Integer(1)
+    }
+    return c
+}
+
+// 1 ASTORE 0
+// 1 LOCALVARIABLE c Lkotlin/jvm/internal/Ref\$ObjectRef; L1 L11 0
