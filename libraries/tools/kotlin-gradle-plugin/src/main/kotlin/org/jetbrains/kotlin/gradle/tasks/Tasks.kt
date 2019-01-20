5a6df105226ff87ef0ae044f1c70ce36c676fead
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-01-20 22:16:47 +0300

    Measure time spent on backing up and restoring outputs

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/tasks/Tasks.kt
@@ -28,16 +28,13 @@ import org.jetbrains.kotlin.gradle.internal.CompilerArgumentAwareWithInput
 import org.jetbrains.kotlin.gradle.internal.prepareCompilerArguments
 import org.jetbrains.kotlin.gradle.internal.tasks.TaskWithLocalState
 import org.jetbrains.kotlin.gradle.internal.tasks.allOutputFiles
-import org.jetbrains.kotlin.gradle.logging.GradlePrintingMessageCollector
-import org.jetbrains.kotlin.gradle.logging.kotlinDebug
-import org.jetbrains.kotlin.gradle.logging.kotlinWarn
+import org.jetbrains.kotlin.gradle.logging.*
 import org.jetbrains.kotlin.gradle.plugin.*
 import org.jetbrains.kotlin.gradle.utils.*
 import org.jetbrains.kotlin.incremental.ChangedFiles
 import org.jetbrains.kotlin.incremental.classpathAsList
 import org.jetbrains.kotlin.incremental.destinationAsFile
 import org.jetbrains.kotlin.utils.LibraryUtils
-import org.jetbrains.kotlin.utils.keysToMap
 import java.io.File
 import java.util.*
 import javax.inject.Inject
@@ -254,6 +251,8 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments>() : AbstractKo
         }
     }
 
+    private val kotlinLogger by lazy { GradleKotlinLogger(logger) }
+
     internal open fun compilerRunner(): GradleCompilerRunner =
         GradleCompilerRunner(this)
 
@@ -266,18 +265,12 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments>() : AbstractKo
         // If task throws exception, but its outputs are changed during execution,
         // then Gradle forces next build to be non-incremental (see Gradle's DefaultTaskArtifactStateRepository#persistNewOutputs)
         // To prevent this, we backup outputs before incremental build and restore when exception is thrown
-        val prevOutputs: Map<File, ByteArray>? = if (incremental && inputs.isIncremental) {
-            val outputFiles = HashSet<File>()
-            allOutputFiles().forEach {
-                if (it.isDirectory) {
-                    it.walk().filterTo(outputFiles, File::isFile)
-                } else if (it.isFile) {
-                    outputFiles.add(it)
+        val outputsBackup: TaskOutputsBackup? =
+            if (incremental && inputs.isIncremental)
+                kotlinLogger.logTime("Backing up outputs for incremental build") {
+                    TaskOutputsBackup(allOutputFiles())
                 }
-            }
-
-            outputFiles.keysToMap { it.readBytes() }
-        } else null
+            else null
 
         if (!incremental) {
             clearLocalState("IC is disabled")
@@ -286,26 +279,11 @@ abstract class AbstractKotlinCompile<T : CommonCompilerArguments>() : AbstractKo
         try {
             executeImpl(inputs)
         } catch (t: Throwable) {
-            allOutputFiles().forEach {
-                if (it.isDirectory) {
-                    it.deleteRecursively()
-                } else if (it.isFile) {
-                    it.delete()
+            if (outputsBackup != null) {
+                kotlinLogger.logTime("Restoring previous outputs on error") {
+                    outputsBackup.restoreOutputs()
                 }
             }
-
-            if (prevOutputs != null) {
-                val dirs = HashSet<File>()
-
-                for ((file, bytes) in prevOutputs) {
-                    val dir = file.parentFile
-                    if (dirs.add(dir)) {
-                        dir.mkdirs()
-                    }
-                    file.writeBytes(bytes)
-                }
-            }
-
             throw t
         }
     }
