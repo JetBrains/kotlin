efdb4e1d5d0f199a46b1bca8b7509d6223c8f79e
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-01-22 09:48:34 +0300

    Fix assertion caused by wrong equality of DescriptorBasedFunctionHandleForJvm
    In case of the same class defined in different modules they contain
    different descriptors instances in their scope that might lead
    to failing assertion in findConcreteSuperDeclaration
    because there are overridden from different modules
    
    It might happen even in the compiler because of different built-in modules
    defined for sources-module and for dependencies' one
    
    ^KT-29320 Fixed

---
+++ compiler/testData/codegen/box/jvm8/treeMapBridge.kt
@@ -0,0 +1,16 @@
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND: JVM_IR
+
+// FULL_JDK
+
+import java.util.*
+
+private class IntervalTreeMap : TreeMap<String, String>()
+
+fun box(): String {
+    val intervalTreeMap = IntervalTreeMap()
+    intervalTreeMap.put("123", "356")
+
+    if (!intervalTreeMap.remove("123", "356")) return "fail 1"
+    return intervalTreeMap.getOrDefault("123", "OK")
+}
