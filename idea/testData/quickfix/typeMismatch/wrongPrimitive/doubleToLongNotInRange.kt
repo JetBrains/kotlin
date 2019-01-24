5ec7643b5a2a0ad3380dd4ce7b10592a2f8e8901
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-24 10:50:01 +0300

    Fix two QF tests (new add underscores action, KT-28953)

--- idea/testData/quickfix/typeMismatch/wrongPrimitive/doubleToLongNotInRange.kt
+++ idea/testData/quickfix/typeMismatch/wrongPrimitive/doubleToLongNotInRange.kt
@@ -3,6 +3,7 @@
 // ACTION: Change type of 'a' to 'Double'
 // ACTION: Convert expression to 'Long'
 // ACTION: Convert property initializer to getter
+// ACTION: Add underscores
 // ERROR: The floating-point literal does not conform to the expected type Long
 
 val a : Long = 10000000000000000000.0<caret>
