0e179f6b44d548288d5c1ae53ea9273ae87054f1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-01-28 16:33:20 +0300

    Extract FirAbstractProviderBasedScope & fix potential KNPE

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirAbstractStarImportingScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirAbstractStarImportingScope.kt
@@ -7,25 +7,16 @@ package org.jetbrains.kotlin.fir.scopes.impl
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirResolvedImport
-import org.jetbrains.kotlin.fir.resolve.FirSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.impl.FirCompositeSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.impl.FirDependenciesSymbolProviderImpl
 import org.jetbrains.kotlin.fir.scopes.FirPosition
-import org.jetbrains.kotlin.fir.scopes.FirScope
 import org.jetbrains.kotlin.fir.symbols.ConeSymbol
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
 
-abstract class FirAbstractStarImportingScope(val session: FirSession, lookupInFir: Boolean = true) : FirScope {
+abstract class FirAbstractStarImportingScope(
+    session: FirSession, lookupInFir: Boolean = true
+) : FirAbstractProviderBasedScope(session, lookupInFir) {
 
     protected abstract val starImports: List<FirResolvedImport>
-
-    val provider = FirSymbolProvider.getInstance(session).let {
-        when {
-            it is FirCompositeSymbolProvider && !lookupInFir -> it.providers.find { it is FirDependenciesSymbolProviderImpl }!!
-            else -> it
-        }
-    }
 
     override fun processClassifiersByName(
         name: Name,
