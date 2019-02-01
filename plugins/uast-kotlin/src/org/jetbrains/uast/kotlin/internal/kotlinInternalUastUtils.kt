3d788670e43e71d4cce1b0f0c665ac1304aeae40
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-02-01 13:25:09 +0300

    UAST: do not analyze KtPsiFactory-created files #KT-29486 Fixed
    (cherry picked from commit bef02493475e96c4ec4c07164ce0e11735e49c1c)

--- plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/internal/kotlinInternalUastUtils.kt
+++ plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/internal/kotlinInternalUastUtils.kt
@@ -283,7 +283,9 @@ internal fun KtExpression.unwrapBlockOrParenthesis(): KtExpression {
 }
 
 internal fun KtElement.analyze(): BindingContext {
+    val containingFile = containingFile
     if (!isValid || containingFile !is KtFile) return BindingContext.EMPTY // EA-114080, EA-113475, EA-134193
+    if (containingFile.doNotAnalyze != null) return BindingContext.EMPTY // To prevent exceptions during analysis
     return ServiceManager.getService(project, KotlinUastResolveProviderService::class.java)
         ?.getBindingContext(this) ?: BindingContext.EMPTY
 }
