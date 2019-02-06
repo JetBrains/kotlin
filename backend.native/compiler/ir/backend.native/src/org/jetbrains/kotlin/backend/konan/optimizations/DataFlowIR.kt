greck2908/kotlin-native

Split up EnumClassLowering on two parts EnumConstructorsLowering will be moved to common part Also moved EnumClassLowering down along the pipeline

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/DataFlowIR.kt
@@ -488,7 +488,7 @@ internal object DataFlowIR {
             }, data = null)
         }
 
-        private fun ClassDescriptor.isFinal() = modality == Modality.FINAL && kind != ClassKind.ENUM_CLASS
+        private fun ClassDescriptor.isFinal() = modality == Modality.FINAL
 
         fun mapClassReferenceType(descriptor: ClassDescriptor, eraseLocalObjects: Boolean = true): Type {
             // Do not try to devirtualize ObjC classes.

JetBrains/kotlin

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/DataFlowIR.kt
@@ -488,7 +488,7 @@ internal object DataFlowIR {
             }, data = null)
         }
 
-        private fun IrClass.isFinal() = modality == Modality.FINAL && kind != ClassKind.ENUM_CLASS
+        private fun IrClass.isFinal() = modality == Modality.FINAL
 
         fun mapClassReferenceType(irClass: IrClass, eraseLocalObjects: Boolean = true): Type {
             // Do not try to devirtualize ObjC classes.