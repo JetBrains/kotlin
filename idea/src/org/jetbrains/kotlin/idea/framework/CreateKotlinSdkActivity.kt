9a5fe4346816fb935ea632e88c6d35337d17a6ed
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2019-02-06 15:52:43 +0300

    Create Kotlin SDK action moved to pooled thread #KT-29551 Fixed

--- idea/src/org/jetbrains/kotlin/idea/framework/CreateKotlinSdkActivity.kt
+++ idea/src/org/jetbrains/kotlin/idea/framework/CreateKotlinSdkActivity.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.idea.framework
 
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import org.jetbrains.kotlin.idea.project.platform
@@ -13,11 +14,11 @@ import org.jetbrains.kotlin.platform.impl.CommonIdePlatformKind
 import org.jetbrains.kotlin.platform.impl.JsIdePlatformKind
 
 /**
- * This StartupActivity creates KotlinSdk for projects containing JS or Common modules.
+ * This StartupActivity creates KotlinSdk for projects containing non-jvm modules.
  * This activity is work-around required until the issue IDEA-203655 is fixed. The major case is to create
  * Kotlin SDK when the KotlinSourceRootType is created
  */
-class CreateKotlinSdkActivity : StartupActivity {
+class CreateKotlinSdkActivity : StartupActivity, DumbAware {
 
     override fun runActivity(project: Project) {
         val kotlinSdkIsRequired = project.allModules().any {
