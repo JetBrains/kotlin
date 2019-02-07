267925ef14e7872f0012ee697b0bc05ba6b8cad7
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-02-07 11:55:02 +0100

    Update bootstrap to 1.3.30-dev-1419

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ import org.gradle.kotlin.dsl.*
 buildscript {
     extra["defaultSnapshotVersion"] = "1.3-SNAPSHOT"
 
-    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3.30-dev-1297", onlySuccessBootstrap = false))
+    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3.30-dev-1419", onlySuccessBootstrap = false))
 
     repositories.withRedirector(project) {
         bootstrapKotlinRepo?.let(::maven)
