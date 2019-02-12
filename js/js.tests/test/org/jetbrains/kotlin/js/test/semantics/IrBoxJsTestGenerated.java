3be42487915ccf1e30958ccaa2f6dc27dfbb7cb1
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2019-02-12 19:36:35 +0300

    [JS IR BE] Disable deprecated jsName tests

--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/IrBoxJsTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/IrBoxJsTestGenerated.java
@@ -7015,31 +7015,6 @@ public class IrBoxJsTestGenerated extends AbstractIrBoxJsTest {
             runTest("js/js.translator/testData/box/reflection/external.kt");
         }
 
-        @TestMetadata("jsClass.kt")
-        public void testJsClass() throws Exception {
-            runTest("js/js.translator/testData/box/reflection/jsClass.kt");
-        }
-
-        @TestMetadata("jsClassName.kt")
-        public void testJsClassName() throws Exception {
-            runTest("js/js.translator/testData/box/reflection/jsClassName.kt");
-        }
-
-        @TestMetadata("jsClassOnReifiedType.kt")
-        public void testJsClassOnReifiedType() throws Exception {
-            runTest("js/js.translator/testData/box/reflection/jsClassOnReifiedType.kt");
-        }
-
-        @TestMetadata("jsClassOnReifiedTypeInLambda.kt")
-        public void testJsClassOnReifiedTypeInLambda() throws Exception {
-            runTest("js/js.translator/testData/box/reflection/jsClassOnReifiedTypeInLambda.kt");
-        }
-
-        @TestMetadata("jsClassSimpleName.kt")
-        public void testJsClassSimpleName() throws Exception {
-            runTest("js/js.translator/testData/box/reflection/jsClassSimpleName.kt");
-        }
-
         @TestMetadata("kClass.kt")
         public void testKClass() throws Exception {
             runTest("js/js.translator/testData/box/reflection/kClass.kt");
