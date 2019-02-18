565727e302d124f1b6b411dff4b3e15656b842ea
Kevin Peek <kevpeek@gmail.com>
2019-02-18 13:05:46 -0500

    KT-20357 - Add sample for Regex.findAll()

--- libraries/stdlib/common/src/kotlin/TextH.kt
+++ libraries/stdlib/common/src/kotlin/TextH.kt
@@ -53,6 +53,8 @@ expect class Regex {
 
     /**
      * Returns a sequence of all occurrences of a regular expression within the [input] string, beginning at the specified [startIndex].
+     *
+     * @sample samples.text.Regexps.findAll
      */
     fun findAll(input: CharSequence, startIndex: Int = 0): Sequence<MatchResult>
 
--- libraries/stdlib/samples/test/samples/text/regex.kt
+++ libraries/stdlib/samples/test/samples/text/regex.kt
@@ -45,4 +45,13 @@ class Regexps {
         // Because the search starts from the index 2, it finds the last "to be".
         assertPrints(regex3.find(inputString, 2)!!.range, "13..17")
     }
+
+    @Sample
+    fun findAll() {
+        val text = "Hello Alice. Hello Bob. Hello Eve."
+        val regex = Regex("Hello (.*?)[.]")
+        val matches = regex.findAll(text)
+        val names = matches.map { it.groupValues[1] }.joinToString()
+        assertPrints(names, "Alice, Bob, Eve")
+    }
 }
