1f44831c8ed9613cfe176710e19d8dcd3ab91681
Pavel Punegov <pavel.punegov@jetbrains.com>
2019-02-22 14:32:14 +0300

    Create an empty inlining rule that suppresses a warning about performance of inlining/

--- extracted/konan.serializer/src/org/jetbrains/kotlin/resolve/konan/platform/KonanPlatformConfigurator.kt
+++ extracted/konan.serializer/src/org/jetbrains/kotlin/resolve/konan/platform/KonanPlatformConfigurator.kt
@@ -3,11 +3,14 @@ package org.jetbrains.kotlin.resolve.konan.platform
 import org.jetbrains.kotlin.builtins.PlatformToKotlinClassMap
 import org.jetbrains.kotlin.container.StorageComponentContainer
 import org.jetbrains.kotlin.container.useInstance
+import org.jetbrains.kotlin.descriptors.CallableMemberDescriptor
+import org.jetbrains.kotlin.psi.KtCallableDeclaration
 import org.jetbrains.kotlin.resolve.*
 import org.jetbrains.kotlin.resolve.calls.checkers.ReifiedTypeParameterSubstitutionChecker
 import org.jetbrains.kotlin.resolve.calls.components.SamConversionTransformer
 import org.jetbrains.kotlin.resolve.calls.results.TypeSpecificityComparator
 import org.jetbrains.kotlin.resolve.checkers.ExpectedActualDeclarationChecker
+import org.jetbrains.kotlin.resolve.inline.ReasonableInlineRule
 import org.jetbrains.kotlin.resolve.lazy.DelegationFilter
 import org.jetbrains.kotlin.resolve.scopes.SyntheticScopes
 import org.jetbrains.kotlin.types.DynamicTypesSettings
@@ -33,5 +36,14 @@ object KonanPlatformConfigurator : PlatformConfiguratorBase(
         container.useInstance(SyntheticScopes.Empty)
         container.useInstance(TypeSpecificityComparator.NONE)
         container.useInstance(SamConversionTransformer.Empty)
+        container.useInstance(NativeInliningRule)
     }
+}
+
+object NativeInliningRule : ReasonableInlineRule {
+    override fun isInlineReasonable(
+            descriptor: CallableMemberDescriptor,
+            declaration: KtCallableDeclaration,
+            context: BindingContext
+    ): Boolean = true
 }
