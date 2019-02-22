1a88e7031060af451692b0473410bfff392472b0
Matthew Runo <inktomi@users.noreply.github.com>
2019-02-21 17:31:20 -0800

    KT-29666: Updated Deprecated Java Quickfix to also migrate arguments, if the argument type is a RetentionPolicy argument

--- idea/src/org/jetbrains/kotlin/idea/quickfix/DeprecatedJavaAnnotationFix.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/DeprecatedJavaAnnotationFix.kt
@@ -10,9 +10,11 @@ import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.ImportPath
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.ErrorsJvm
 
-internal class DeprecatedJavaAnnotationFix(element: KtAnnotationEntry,
-                                           private val annotationFqName: FqName,
-                                           private val arguments: List<KtValueArgument>) : KotlinQuickFixAction<KtAnnotationEntry>(element) {
+internal class DeprecatedJavaAnnotationFix(
+    element: KtAnnotationEntry,
+    private val annotationFqName: FqName,
+    private val arguments: List<KtValueArgument>
+) : KotlinQuickFixAction<KtAnnotationEntry>(element) {
     override fun getFamilyName() = "Replace Annotation"
     override fun getText(): String = "Replace annotation with ${annotationFqName.asString()}"
 
@@ -21,23 +23,52 @@ internal class DeprecatedJavaAnnotationFix(element: KtAnnotationEntry,
 
         val psiFactory = KtPsiFactory(project)
 
-        val argumentString = if(arguments.isEmpty()) {
+        val argumentString = if (arguments.isEmpty()) {
             ""
         } else {
-            "("+ arguments.joinToString(",") { it.text } + ")"
+            "(" + updateAnnotationArgument(file, arguments) + ")"
         }
 
         element.replace(psiFactory.createAnnotationEntry("@" + annotationFqName.shortName() + argumentString))
 
         for ((java, kotlin) in JavaAnnotationMapper.javaToKotlinNameMap) {
             if (kotlin == annotationFqName) {
-                val oldImport = file.importDirectives.find { it -> it.importedFqName == java } ?: return
+                val oldImport = file.importDirectives.find { it.importedFqName == java } ?: return
                 oldImport.delete()
                 break
             }
         }
 
         file.importList?.add(psiFactory.createImportDirective(ImportPath(annotationFqName, false, null)))
+    }
+
+    private fun updateAnnotationArgument(file: KtFile, args: List<KtValueArgument>): String {
+        return args.joinToString(",") {
+            val type = it.text.split('.')
+            var argumentOutput = ""
+
+            if (type.size == 2) {
+                val first = type[0]
+                val second = type[1]
+
+                when (first) {
+                    "RetentionPolicy" -> {
+                        argumentOutput = argumentOutput.plus("AnnotationRetention")
+
+                        argumentOutput = when (second) {
+                            "CLASS" -> argumentOutput.plus(".BINARY")
+                            else -> argumentOutput.plus(".$second")
+                        }
+
+                        val oldImport =
+                            file.importDirectives.find { import -> import.importedFqName?.asString() == "java.lang.annotation.RetentionPolicy" }
+                        oldImport?.delete()
+                    }
+                }
+            }
+
+            argumentOutput
+        }
     }
 
     companion object Factory : KotlinSingleIntentionActionFactory() {
