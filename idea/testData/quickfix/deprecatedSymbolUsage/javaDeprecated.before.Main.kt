93905f91774600936b5ff7f38cdfac7568cc2a70
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-02-25 18:15:08 +0900

    Fix quick fix tests for "Convert to scope function"
    #KT-29099

--- idea/testData/quickfix/deprecatedSymbolUsage/javaDeprecated.before.Main.kt
+++ idea/testData/quickfix/deprecatedSymbolUsage/javaDeprecated.before.Main.kt
@@ -1,4 +1,8 @@
 // "class org.jetbrains.kotlin.idea.quickfix.replaceWith.DeprecatedSymbolUsageFix" "false"
+// ACTION: Convert to also
+// ACTION: Convert to apply
+// ACTION: Convert to run
+// ACTION: Convert to with
 
 fun foo() {
     val c = JavaClass()
