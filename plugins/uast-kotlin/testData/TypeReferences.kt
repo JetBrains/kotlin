72860fb6955896bf568b7274bd6fa9f6657b520f
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2019-02-26 20:16:05 +0300

    Uast: making `KotlinUSimpleReferenceExpression` able to resolve to types

--- plugins/uast-kotlin/testData/TypeReferences.kt
+++ plugins/uast-kotlin/testData/TypeReferences.kt
@@ -1,4 +1,6 @@
-fun foo(parameter: Int) {
+fun foo(parameter: Int): String {
     val varWithType: String? = "Not Null"
     val varWithoutType = "lorem ipsum"
+    var result = varWithType + varWithoutType
+    return result
 }
