adb13b2f9ecfa1846351a1064532afebf8d6912e
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2019-02-28 12:46:15 +0300

    Uast: `ResolveEverythingTest` tests for generics added

--- plugins/uast-kotlin/testData/TypeReferences.kt
+++ plugins/uast-kotlin/testData/TypeReferences.kt
@@ -4,3 +4,17 @@ fun foo(parameter: Int): String {
     var result = varWithType + varWithoutType
     return result
 }
+
+
+fun <T> parameterizedFoo(arg: T?) {
+    val a = arg
+    val at: T = arg ?: return
+
+    val tl: List<T> = listOf(at)
+    val tsl: List<String> = tl.map { it.toString() }
+    val lls: List<List<String>>
+    val llt: List<List<T>>
+
+    parameterizedFoo<List<String>>(emptyList())
+
+}
