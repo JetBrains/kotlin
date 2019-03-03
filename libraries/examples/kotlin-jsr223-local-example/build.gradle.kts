8a8de052a30b12b8649861eabf9bc80ce64fcab0
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-03-03 12:22:03 +0100

    Fix JSR-223 and scripting samples dependencies

--- libraries/examples/kotlin-jsr223-local-example/build.gradle.kts
+++ libraries/examples/kotlin-jsr223-local-example/build.gradle.kts
@@ -14,16 +14,16 @@ pill {
 dependencies {
     compile(kotlinStdlib())
     compile(project(":kotlin-script-runtime"))
-    compile(projectRuntimeJar(":kotlin-compiler-embeddable"))
+    compile(project(":kotlin-compiler-embeddable"))
     compile(project(":kotlin-script-util"))
-    runtime(projectRuntimeJar(":kotlin-scripting-compiler-embeddable"))
+    runtime(project(":kotlin-scripting-compiler-embeddable"))
     testCompile(project(":kotlin-test:kotlin-test-junit"))
     testCompile(commonDep("junit:junit"))
     testRuntime(project(":kotlin-reflect"))
     compileOnly(project(":compiler:cli-common")) // TODO: fix import (workaround for jps build)
     testCompileOnly(project(":core:util.runtime")) // TODO: fix import (workaround for jps build)
     testCompileOnly(project(":compiler:daemon-common")) // TODO: fix import (workaround for jps build)
-    testRuntime(projectRuntimeJar(":kotlin-scripting-compiler-embeddable"))
+    testRuntime(project(":kotlin-scripting-compiler-embeddable"))
 }
 
 projectTest()
