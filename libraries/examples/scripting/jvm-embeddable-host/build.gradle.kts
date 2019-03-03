60b3fa3b07301d855240851064cbf3db1433e004
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-03-03 11:11:15 +0100

    Fix embeddable host test

--- libraries/examples/scripting/jvm-embeddable-host/build.gradle.kts
+++ libraries/examples/scripting/jvm-embeddable-host/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     compileOnly(project(":kotlin-scripting-jvm-host"))
     compile(project(":kotlin-script-util"))
     testRuntimeOnly(projectRuntimeJar(":kotlin-compiler-embeddable"))
+    testRuntimeOnly(projectRuntimeJar(":kotlin-scripting-compiler-embeddable"))
     testRuntimeOnly(project(":kotlin-scripting-jvm-host-embeddable"))
     testRuntimeOnly(project(":kotlin-reflect"))
     testRuntimeOnly(intellijDep()) { includeJars("guava", rootProject = rootProject) }
