9e2ee6183cba2f7429de19b6b2f04fa92b32bae0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-03-04 12:32:48 +0300

    Fix "make constructor parameter a property" for vararg case
    Same should work for annotated parameters
    #KT-29312 Fixed

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -7709,6 +7709,11 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
         public void testVar() throws Exception {
             runTest("idea/testData/quickfix/makeConstructorParameterProperty/var.kt");
         }
+
+        @TestMetadata("vararg.kt")
+        public void testVararg() throws Exception {
+            runTest("idea/testData/quickfix/makeConstructorParameterProperty/vararg.kt");
+        }
     }
 
     @TestMetadata("idea/testData/quickfix/makeTypeParameterReified")
