9f75fd0d6235170ed8a60f648c23cc13f2689558
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-03-04 19:28:59 +0100

    Minor, remove confusing logic from GenericReplChecker

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/repl/GenericReplChecker.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/repl/GenericReplChecker.kt
@@ -56,7 +56,8 @@ open class GenericReplChecker(
             if (get(JVMConfigurationKeys.JVM_TARGET) == null) {
                 put(JVMConfigurationKeys.JVM_TARGET,
                     System.getProperty(KOTLIN_REPL_JVM_TARGET_PROPERTY)?.let { JvmTarget.fromString(it) }
-                        ?: if (getJavaVersion() >= 0x10008) JvmTarget.JVM_1_8 else JvmTarget.JVM_1_6)
+                        ?: System.getProperty("java.specification.version")?.let { JvmTarget.fromString(it) }
+                        ?: JvmTarget.DEFAULT)
             }
         }
         KotlinCoreEnvironment.createForProduction(disposable, compilerConfiguration, EnvironmentConfigFiles.JVM_CONFIG_FILES)
@@ -95,23 +96,5 @@ open class GenericReplChecker(
                 else -> ReplCheckResult.Ok()
             }
         }
-    }
-}
-
-// initially taken from libraries/stdlib/src/kotlin/internal/PlatformImplementations.kt
-// fixed according to JEP 223 - http://openjdk.java.net/jeps/223
-// TODO: consider to place it to some common place
-private fun getJavaVersion(): Int {
-    val default = 0x10006
-    val version = System.getProperty("java.specification.version") ?: return default
-    val components = version.split('.')
-    return try {
-        when (components.size) {
-            0 -> default
-            1 -> components[0].toInt() * 0x10000
-            else -> components[0].toInt() * 0x10000 + components[1].toInt()
-        }
-    } catch (e: NumberFormatException) {
-        default
     }
 }
