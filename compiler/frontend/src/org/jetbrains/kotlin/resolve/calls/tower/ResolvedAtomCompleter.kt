JetBrains/kotlin

commit cbf1d773f75aee5440479f532d001c5a9fc51684

NI. Coerce return type of lambda function descriptor to Unit. #KT-30242 Fixed

compiler/frontend/src/org/jetbrains/kotlin/resolve/calls/tower/ResolvedAtomCompleter.kt
@@ -31,13 +31,12 @@ import org.jetbrains.kotlin.resolve.calls.tasks.ExplicitReceiverKind
 import org.jetbrains.kotlin.resolve.calls.tasks.TracingStrategyImpl
 import org.jetbrains.kotlin.resolve.calls.util.CallMaker
 import org.jetbrains.kotlin.resolve.deprecation.DeprecationResolver
-import org.jetbrains.kotlin.types.IndexedParametersSubstitution
-import org.jetbrains.kotlin.types.TypeUtils
-import org.jetbrains.kotlin.types.UnwrappedType
-import org.jetbrains.kotlin.types.Variance
+import org.jetbrains.kotlin.types.*
 import org.jetbrains.kotlin.types.expressions.DoubleColonExpressionResolver
 import org.jetbrains.kotlin.types.expressions.ExpressionTypingServices
 import org.jetbrains.kotlin.types.typeUtil.asTypeProjection
+import org.jetbrains.kotlin.types.typeUtil.isUnit
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 class ResolvedAtomCompleter(
     private val resultSubstitutor: NewTypeSubstitutor,
@@ -108,8 +107,27 @@ class ResolvedAtomCompleter(
         return resolvedCall
     }
 
+    private val ResolvedLambdaAtom.isCoercedToUnit: Boolean
+        get() {
+            val returnTypes =
+                resultArguments.mapNotNull {
+                    val type = it.safeAs<SimpleKotlinCallArgument>()?.receiver?.receiverValue?.type ?: return@mapNotNull null
+                    val unwrappedType = when (type) {
+                        is WrappedType -> type.unwrap()
+                        is UnwrappedType -> type
+                    }
+                    resultSubstitutor.safeSubstitute(unwrappedType)
+                }
+            val commonReturnType = CommonSupertypes.commonSupertype(returnTypes)
+            return commonReturnType.isUnit()
+        }
+
     private fun completeLambda(lambda: ResolvedLambdaAtom) {
-        val returnType = resultSubstitutor.safeSubstitute(lambda.returnType)
+        val returnType = if (lambda.isCoercedToUnit) {
+            builtIns.unitType
+        } else {
+            resultSubstitutor.safeSubstitute(lambda.returnType)
+        }
 
         updateTraceForLambda(lambda, topLevelTrace, returnType)
 

compiler/testData/diagnostics/tests/inference/coercionToUnit/kt30242.kt
@@ -1,0 +1,64 @@
+// !WITH_NEW_INFERENCE
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+// ISSUE: KT-30242
+
+class A
+
+fun println(s: String = "") {}
+
+fun foo(f: () -> Any) {}
+
+fun test1(b: Boolean) {
+    foo {
+        if (b) {
+            println("meh")
+        }
+    }
+}
+
+fun test2(b: Boolean) {
+    foo {
+        when {
+            b -> println("meh")
+        }
+    }
+}
+
+fun test3(b: Boolean) {
+    foo {
+        <!INVALID_IF_AS_EXPRESSION!>if<!> (b) {
+            return@foo A()
+        }
+    }
+}
+
+fun test4(b: Boolean) {
+    foo {
+        if (b) {
+            return@foo println("meh")
+        }
+
+        if (b) {
+            println()
+        }
+    }
+}
+
+fun bar(block: () -> String) {}
+
+fun test_5(b: Boolean) {
+    bar {
+        <!NI;TYPE_MISMATCH, TYPE_MISMATCH!>if (b) {
+            println("meh")
+        }<!>
+    }
+}
+
+fun test_6(b: Boolean) {
+    foo {
+        if (b) {
+            return@foo Unit
+        }
+        if (b) {}
+    }
+}

compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -9805,6 +9805,11 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                     runTest("compiler/testData/diagnostics/tests/inference/coercionToUnit/indirectCoercionWithExpectedType.kt");
                 }
 
+                @TestMetadata("kt30242.kt")
+                public void testKt30242() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/coercionToUnit/kt30242.kt");
+                }
+
                 @TestMetadata("noCoercion.kt")
                 public void testNoCoercion() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/coercionToUnit/noCoercion.kt");

compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -9800,6 +9800,11 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                     runTest("compiler/testData/diagnostics/tests/inference/coercionToUnit/indirectCoercionWithExpectedType.kt");
                 }
 
+                @TestMetadata("kt30242.kt")
+                public void testKt30242() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/coercionToUnit/kt30242.kt");
+                }
+
                 @TestMetadata("noCoercion.kt")
                 public void testNoCoercion() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/coercionToUnit/noCoercion.kt");

commit b681e4bda397b3a1064c26df8b67903d8d8e8b01

NI. Fix call `commonSuperType` for 0 types (bug introduced in cbf1d773)

compiler/frontend/src/org/jetbrains/kotlin/resolve/calls/tower/ResolvedAtomCompleter.kt
@@ -110,14 +110,18 @@ class ResolvedAtomCompleter(
     private val ResolvedLambdaAtom.isCoercedToUnit: Boolean
         get() {
             val returnTypes =
-                resultArguments.mapNotNull {
-                    val type = it.safeAs<SimpleKotlinCallArgument>()?.receiver?.receiverValue?.type ?: return@mapNotNull null
+                resultArguments.map {
+                    val type = it.safeAs<SimpleKotlinCallArgument>()?.receiver?.receiverValue?.type ?: return@map null
                     val unwrappedType = when (type) {
                         is WrappedType -> type.unwrap()
                         is UnwrappedType -> type
                     }
                     resultSubstitutor.safeSubstitute(unwrappedType)
                 }
+            if (returnTypes.isEmpty()) return true
+            val substitutedTypes = returnTypes.filterNotNull()
+            // we have some unsubstituted types
+            if (substitutedTypes.isEmpty()) return false
             val commonReturnType = CommonSupertypes.commonSupertype(returnTypes)
             return commonReturnType.isUnit()
         }