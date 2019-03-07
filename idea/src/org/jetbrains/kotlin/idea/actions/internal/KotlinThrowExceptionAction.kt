f90ff238843bccb891c35514025c26aeac9c590d
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-03-07 13:08:14 +0300

    Allow to throw plugin exception when plugin version is patched

--- idea/src/org/jetbrains/kotlin/idea/actions/internal/KotlinThrowExceptionAction.kt
+++ idea/src/org/jetbrains/kotlin/idea/actions/internal/KotlinThrowExceptionAction.kt
@@ -7,8 +7,16 @@ package org.jetbrains.kotlin.idea.actions.internal
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationManager
+import org.jetbrains.kotlin.idea.KotlinPluginUtil
 
 class KotlinThrowExceptionAction : AnAction() {
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+
+        e.presentation.isEnabledAndVisible = KotlinPluginUtil.isPatched() || ApplicationManager.getApplication().isInternal
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         throw IllegalStateException("Kotlin Test Exception")
     }
