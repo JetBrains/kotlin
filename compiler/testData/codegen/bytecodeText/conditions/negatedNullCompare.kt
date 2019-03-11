07b56597a55e4d5b902e1cca76f711b6f4d5babb
Mark Punzalan <markpun@google.com>
2019-03-11 10:10:43 -0700

    Generate conditional jumps with optimizations for loops, if possible.

--- compiler/testData/codegen/bytecodeText/conditions/negatedNullCompare.kt
+++ compiler/testData/codegen/bytecodeText/conditions/negatedNullCompare.kt
@@ -1,14 +1,45 @@
-fun main(p: String?) {
+fun main(p: String?, p2: String?) {
+    // Negated == null comparisons
+    // Generates IFNULL and GOTO
     if (!(p == null)) {
         "then"
     } else {
         "else"
     }
+
+    // Generates IFNULL and GOTO
+    while (!(p == null)) {
+        "loop"
+    }
+
+    // Generates IFNONNULL
+    do {
+        "loop"
+    } while (!(p == null))
+
+    // != null comparisons
+    // Generates IFNULL and GOTO
+    if (p2 != null) {
+        "then"
+    } else {
+        "else"
+    }
+
+    // Generates IFNULL and GOTO
+    while (p2 != null) {
+        "loop"
+    }
+
+    // Generates IFNONNULL
+    do {
+        "loop"
+    } while (p2 != null)
 }
 
 //0 ICONST_0
 //0 ICONST_1
 //0 ACONST_NULL
-//1 IFNULL
-//1 IF
-//1 GOTO
+//4 IFNULL
+//2 IFNONNULL
+//6 IF
+//4 GOTO
--- compiler/testData/codegen/bytecodeText/conditions/negatedZeroCompare.kt
+++ compiler/testData/codegen/bytecodeText/conditions/negatedZeroCompare.kt
@@ -1,16 +1,123 @@
+// Generates ICONST_1
 val a = 1
 
 fun main() {
+    // Negated == comparisons
+    // Generates IFEQ and GOTO
     if (!(a == 0)) {
         "then"
     } else {
         "else"
     }
+
+    // Generates IFEQ and GOTO
+    while (!(a == 0)) {
+        "loop"
+    }
+
+    // Generates IFNE
+    do {
+        "loop"
+    } while (!(a == 0))
+
+    // != comparisons
+    // Generates IFEQ and GOTO
+    if (a != 0) {
+        "then"
+    } else {
+        "else"
+    }
+
+    // Generates IFEQ and GOTO
+    while (a != 0) {
+        "loop"
+    }
+
+    // Generates IFNE
+    do {
+        "loop"
+    } while (a != 0)
+
+    // Negated > comparisons
+    // Generates IFGT and GOTO
+    if (!(a > 0)) {
+        "then"
+    } else {
+        "else"
+    }
+
+    // Generates IFGT and GOTO
+    while (!(a > 0)) {
+        "loop"
+    }
+
+    // Generates IFLE
+    do {
+        "loop"
+    } while (!(a > 0))
+
+    // Negated >= comparisons
+    // Generates IFGE and GOTO
+    if (!(a >= 0)) {
+        "then"
+    } else {
+        "else"
+    }
+
+    // Generates IFGE and GOTO
+    while (!(a >= 0)) {
+        "loop"
+    }
+
+    // Generates IFLT
+    do {
+        "loop"
+    } while (!(a >= 0))
+
+    // Negated < comparisons
+    // Generates IFLT and GOTO
+    if (!(a < 0)) {
+        "then"
+    } else {
+        "else"
+    }
+
+    // Generates IFLT and GOTO
+    while (!(a < 0)) {
+        "loop"
+    }
+
+    // Generates IFGE
+    do {
+        "loop"
+    } while (!(a < 0))
+
+    // Negated <= comparisons
+    // Generates IFLE and GOTO
+    if (!(a <= 0)) {
+        "then"
+    } else {
+        "else"
+    }
+
+    // Generates IFLE and GOTO
+    while (!(a <= 0)) {
+        "loop"
+    }
+
+    // Generates IFGT
+    do {
+        "loop"
+    } while (!(a <= 0))
 }
 
 //0 ICONST_0
 //1 ICONST_1
-//1 IFEQ
-//0 IFNE
-//1 IF
-//1 GOTO
+//2 IFNE
+//4 IFEQ
+//3 IFLE
+//3 IFLT
+//3 IFGE
+//3 IFGT
+//18 IF
+//12 GOTO
