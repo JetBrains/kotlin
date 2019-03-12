7adba40ea2260307cdf75dedc7256287761fef34
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2019-03-12 12:46:24 +0300

    New J2K: Fix existing test data

--- j2k/testData/fileOrElement/tryWithResource/Simple.new.kt
+++ j2k/testData/fileOrElement/tryWithResource/Simple.new.kt
@@ -4,6 +4,8 @@ import java.io.IOException
 class C {
     @Throws(IOException::class)
     internal fun foo() {
-        ByteArrayInputStream(ByteArray(10)).use { stream -> println(stream.read()) }
+        ByteArrayInputStream(ByteArray(10)).use { stream ->
+            println(stream.read())
+        }
     }
 }
