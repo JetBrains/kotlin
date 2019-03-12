c730de13dfba99a91cfde65b62587f50c9393a3d
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-03-12 17:36:52 +0100

    Minor, add missing copyright to compiler modules

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/serialization/ir/JsDescriptorReferenceDeserializer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/serialization/ir/JsDescriptorReferenceDeserializer.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlin.ir.backend.js.lower.serialization.ir
 
 import org.jetbrains.kotlin.backend.common.serialization.DescriptorReferenceDeserializer
@@ -30,4 +35,3 @@ class JsDescriptorReferenceDeserializer(
         else null
 
 }
-
