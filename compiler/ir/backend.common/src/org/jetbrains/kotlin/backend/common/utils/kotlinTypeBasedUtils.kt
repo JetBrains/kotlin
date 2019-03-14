a2d65e8a6051c8d2cdfcb03aa908a1e6c03b2a45
Roman Artemev <roman.artemev@jetbrains.com>
2019-03-14 20:51:48 +0300

    [IR] Code clean up

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/utils/kotlinTypeBasedUtils.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/utils/kotlinTypeBasedUtils.kt
@@ -6,26 +6,10 @@
 package org.jetbrains.kotlin.backend.common.utils
 
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
-import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.types.toIrType
 import org.jetbrains.kotlin.ir.types.toKotlinType
-import org.jetbrains.kotlin.ir.util.SymbolTable
-import org.jetbrains.kotlin.resolve.DescriptorUtils
-import org.jetbrains.kotlin.types.CommonSupertypes
-import org.jetbrains.kotlin.types.typeUtil.isSubtypeOf
 
 // TODO: implement pure Ir-based function (see IrTypeUtils.kt)
 
 @Deprecated("Use pure Ir helper")
 fun IrType.getPrimitiveArrayElementType() = KotlinBuiltIns.getPrimitiveArrayElementType(toKotlinType())
-
-@Deprecated("Use pure Ir helper")
-fun List<IrType>.commonSupertype(symbolTable: SymbolTable) =
-    CommonSupertypes.commonSupertype(map(IrType::toKotlinType)).toIrType(symbolTable)!!
-
-@Deprecated("Use pure Ir helper")
-fun IrType.isSubtypeOf(superType: IrType) = toKotlinType().isSubtypeOf(superType.toKotlinType())
-
-@Deprecated("Use pure Ir helper")
-fun IrType.isSubtypeOfClass(superClass: IrClassSymbol) = DescriptorUtils.isSubtypeOfClass(toKotlinType(), superClass.descriptor)
