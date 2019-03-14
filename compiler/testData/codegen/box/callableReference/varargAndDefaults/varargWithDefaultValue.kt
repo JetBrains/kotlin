1f98eaa27be349495df0fb3a98be5e3ac5eeaa89
Roman Artemev <roman.artemev@jetbrains.com>
2019-03-14 20:53:34 +0300

    [JS IR BE] Fix Callable reference with vararg  * Update tests

--- compiler/testData/codegen/box/callableReference/varargAndDefaults/varargWithDefaultValue.kt
+++ compiler/testData/codegen/box/callableReference/varargAndDefaults/varargWithDefaultValue.kt
@@ -1,13 +1,15 @@
 // !LANGUAGE: +NewInference
 // IGNORE_BACKEND: JS, JVM_IR
 
-fun foo(x: Int, vararg y: String = arrayOf("Aaa")): String =
-        if (y[0].length == x) "OK" else "Fail"
+fun foo(x: Int, s: Int, vararg y: CharSequence = arrayOf("Aaa")): String =
+        if (y.size == s && y[0].length == x) "OK" else "Fail"
 
-fun use0(f: (Int) -> String): String = f(3)
-fun use1(f: (Int, String) -> String): String = f(5, "Bbbbb")
+fun use0(f: (Int, Int) -> String): String = f(3, 1)
+fun use1(f: (Int, Int, String) -> String): String = f(5, 1, "Bbbbb")
+fun use2(f: (Int, Int, String, String) -> String): String = f(5, 2, "Bbbbb", "Ccccc")
 
 fun box(): String {
-    if (use0(::foo) != "OK") return "Fail"
-    return use1(::foo)
+    if (use0(::foo) != "OK") return "Fail0"
+    if (use1(::foo) != "OK") return "Fail1"
+    return use2(::foo)
 }
