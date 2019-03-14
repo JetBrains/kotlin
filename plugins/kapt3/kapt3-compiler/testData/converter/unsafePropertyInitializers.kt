348fc18376c1cdf1fcf41feba32ca333c725dc43
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2019-03-14 15:58:16 +0300

    Kapt: Generate constant value initializers for mutable properties (KT-30164)

---
+++ plugins/kapt3/kapt3-compiler/testData/converter/unsafePropertyInitializers.kt
@@ -0,0 +1,24 @@
+object Foo {
+    const val aString: String = "foo"
+    const val aInt: Int = 3
+
+    val bString: String = "bar"
+    val bInt: Int = 5
+
+    var cString: String = "baz"
+    var cInt: Int = 7
+
+    val d = Boo.z
+    val e = Boo.z.length
+    val f = 5 + 3
+    val g = "a" + "b"
+    val h = -4
+    val i = Int.MAX_VALUE
+    val j = "$e$g"
+    val k = g + j
+}
+
+object Boo {
+    val z = foo()
+    fun foo() = "abc"
+}
