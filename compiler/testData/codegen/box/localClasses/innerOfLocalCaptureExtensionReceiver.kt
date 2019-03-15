82ccf81da805d8dabafed59b11ab91a951e45a72
pyos <pyos@google.com>
2019-03-15 10:51:48 +0100

    Fix `this` remapping in inner class constructors
    Inner class constructors should use the argument instead of reading
    outer `this` from a field because if such an access happens before a
    delegating constructor call, e.g. when evaluating an argument, a JVM
    bytecode validation error will be thrown. (The only operation on `this`
    allowed before a delegating constructor call is SETFIELD, and only if
    the field in question is declared in the same class.)

--- compiler/testData/codegen/box/localClasses/innerOfLocalCaptureExtensionReceiver.kt
+++ compiler/testData/codegen/box/localClasses/innerOfLocalCaptureExtensionReceiver.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
 fun String.bar(): String {
     open class Local {
         fun result() = this@bar
--- compiler/testData/codegen/box/typealias/innerClassTypeAliasConstructorInSupertypes.kt
+++ compiler/testData/codegen/box/typealias/innerClassTypeAliasConstructorInSupertypes.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
 class Outer(val x: String) {
     abstract inner class InnerBase
 
