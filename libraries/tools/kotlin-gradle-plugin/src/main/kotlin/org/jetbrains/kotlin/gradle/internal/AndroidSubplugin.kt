b5e4e07ccac502a5b93a73b1ddca22720d00fafa
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2019-03-20 19:24:18 +0300

    Always attach kotlin-android-extensions-runtime library
    @Parcelize is now enabled by default, and it requires the runtime library.
    Enabling it for all clients is a temporary solution.
    In future, the runtime library will be split to two separate artifacts.

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/AndroidSubplugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/AndroidSubplugin.kt
@@ -43,18 +43,12 @@ import javax.xml.parsers.DocumentBuilderFactory
 class AndroidExtensionsSubpluginIndicator @Inject internal constructor(private val registry: ToolingModelBuilderRegistry) :
     Plugin<Project> {
     override fun apply(project: Project) {
-        val extension = project.extensions.create("androidExtensions", AndroidExtensionsExtension::class.java)
-
-        extension.setEvaluatedHandler { evaluatedExtension ->
-            if (evaluatedExtension.isExperimental) {
-                addAndroidExtensionsRuntimeIfNeeded(project)
-            }
-        }
-
+        project.extensions.create("androidExtensions", AndroidExtensionsExtension::class.java)
+        addAndroidExtensionsRuntime(project)
         registry.register(KotlinAndroidExtensionModelBuilder())
     }
 
-    private fun addAndroidExtensionsRuntimeIfNeeded(project: Project) {
+    private fun addAndroidExtensionsRuntime(project: Project) {
         val kotlinPluginVersion = project.getKotlinPluginVersion() ?: run {
             project.logger.error("Kotlin plugin should be enabled before 'kotlin-android-extensions'")
             return
