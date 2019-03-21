215da10687c8e89550a8881e5527a7b2b088ec34
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2019-03-21 18:09:07 +0300

    A little refactoring of deserializer in preparation for the next commit.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/serialization/ir/JsIrLinker.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/serialization/ir/JsIrLinker.kt
@@ -11,6 +11,8 @@ import org.jetbrains.kotlin.backend.common.library.DeclarationId
 import org.jetbrains.kotlin.backend.common.serialization.*
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.backend.js.JS_KLIBRARY_CAPABILITY
+import org.jetbrains.kotlin.ir.backend.js.moduleHeaderFileName
 import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
 import org.jetbrains.kotlin.ir.symbols.IrClassifierSymbol
 import org.jetbrains.kotlin.ir.util.SymbolTable
@@ -20,8 +22,8 @@ class JsIrLinker(
     currentModule: ModuleDescriptor,
     logger: LoggingContext,
     builtIns: IrBuiltIns,
-    symbolTable: SymbolTable) :
-    KotlinIrLinker(logger, builtIns, symbolTable, null, 0x1_0000_0000L),
+    symbolTable: SymbolTable
+) : KotlinIrLinker(logger, builtIns, symbolTable, emptyList<ModuleDescriptor>(), null, 0x1_0000_0000L),
     DescriptorUniqIdAware by JsDescriptorUniqIdAware {
 
     private val FUNCTION_INDEX_START: Long = indexAfterKnownBuiltins
@@ -34,19 +36,16 @@ class JsIrLinker(
     override val descriptorReferenceDeserializer =
         JsDescriptorReferenceDeserializer(currentModule, builtIns, FUNCTION_INDEX_START)
 
-    override fun reader(moduleDescriptor: ModuleDescriptor, uniqId: UniqId) =
-            moduleToReaderMap[moduleDescriptor]!!.declarationBytes(DeclarationId(uniqId.index, uniqId.isLocal))
-
-    fun deserializeIrModuleHeader(
-        moduleDescriptor: ModuleDescriptor,
-        byteArray: ByteArray,
-        klibLocation: File,
-        deserializationStrategy: DeserializationStrategy
-    ): IrModuleFragment {
-        val irFile = File(klibLocation, "ir/irCombined.knd")
-        moduleToReaderMap[moduleDescriptor] = CombinedIrFileReader(irFile)
-        return deserializeIrModuleHeader(moduleDescriptor, byteArray, deserializationStrategy)
+    override fun reader(moduleDescriptor: ModuleDescriptor, uniqId: UniqId): ByteArray {
+        val irFileReader = moduleToReaderMap.getOrPut(moduleDescriptor) {
+            val irFile = File(moduleDescriptor.getCapability(JS_KLIBRARY_CAPABILITY)!!, "ir/irCombined.knd")
+            CombinedIrFileReader(irFile)
+        }
+        return irFileReader.declarationBytes(DeclarationId(uniqId.index, uniqId.isLocal))
     }
+
+    override val ModuleDescriptor.irHeader: ByteArray? get() =
+        this.getCapability(JS_KLIBRARY_CAPABILITY)?.let { File(it, moduleHeaderFileName).readBytes() }
 
     override fun declareForwardDeclarations() {
         // since for `knownBuiltIns` such as FunctionN it is possible to have unbound symbols after deserialization
@@ -61,3 +60,4 @@ class JsIrLinker(
         }
     }
 }
+
