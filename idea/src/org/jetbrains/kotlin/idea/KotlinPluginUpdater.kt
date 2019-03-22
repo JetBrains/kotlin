a9c28ab4f15a963af1850a91bb988ebbd907c1af
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-03-22 17:52:27 +0300

    Use xmlId for fetching release date (KT-30388)

--- idea/src/org/jetbrains/kotlin/idea/KotlinPluginUpdater.kt
+++ idea/src/org/jetbrains/kotlin/idea/KotlinPluginUpdater.kt
@@ -343,23 +343,9 @@ class KotlinPluginUpdater(val propertiesComponent: PropertiesComponent) : Dispos
             var approve: Boolean = true
         }
 
-        // TODO: remove when fetching by string id is implemented
-        private fun toMarketplaceId(pluginId: PluginId): Int {
-            return when (pluginId.idString) {
-                "org.jetbrains.kotlin" -> 6954
-                "org.jetbrains.kotlin.native.clion" -> 10454
-                "org.jetbrains.kotlin.native.appcode" -> 10619
-                else -> {
-                    LOG.error("Unknown plugin id: ${pluginId.idString}")
-                    6954
-                }
-            }
-        }
-
         @Throws(IOException::class, ResponseParseException::class)
         fun fetchPluginReleaseDate(pluginId: PluginId, version: String, channel: String?): LocalDate? {
-            val marketplaceId = toMarketplaceId(pluginId)
-            val url = "https://plugins.jetbrains.com/api/plugins/$marketplaceId/updates/version/$version"
+            val url = "https://plugins.jetbrains.com/api/plugins/${pluginId.idString}/updates?version=$version"
 
             val pluginDTOs: Array<PluginDTO> = try {
                 HttpRequests.request(url).connect {
