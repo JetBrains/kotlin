82c6b51fabd765e728072b35628f7427d533e76b
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-03-25 17:41:33 +0300

    Simplify initialization process for JavaClassFinderImpl
    After this change, it's not required to call "initialize" with
    binding trace and KotlinCodeAnalyzer that is rather useful for FIR

--- compiler/frontend.java/src/org/jetbrains/kotlin/load/java/JavaClassFinderImpl.kt
+++ compiler/frontend.java/src/org/jetbrains/kotlin/load/java/JavaClassFinderImpl.kt
@@ -16,25 +16,27 @@
 
 package org.jetbrains.kotlin.load.java
 
+import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.load.java.structure.JavaClass
 import org.jetbrains.kotlin.load.java.structure.impl.JavaPackageImpl
 import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.resolve.BindingTrace
 import org.jetbrains.kotlin.resolve.jvm.KotlinJavaPsiFacade
-import org.jetbrains.kotlin.resolve.lazy.KotlinCodeAnalyzer
+import javax.inject.Inject
 
 class JavaClassFinderImpl : AbstractJavaClassFinder() {
 
     private lateinit var javaFacade: KotlinJavaPsiFacade
 
-    override fun initialize(trace: BindingTrace, codeAnalyzer: KotlinCodeAnalyzer) {
+    @Inject
+    override fun setProjectInstance(project: Project) {
+        super.setProjectInstance(project)
         javaFacade = KotlinJavaPsiFacade.getInstance(project)
-        super.initialize(trace, codeAnalyzer)
     }
 
     override fun findClass(request: JavaClassFinder.Request): JavaClass? = javaFacade.findClass(request, javaSearchScope)
 
-    override fun findPackage(fqName: FqName) = javaFacade.findPackage(fqName.asString(), javaSearchScope)?.let { JavaPackageImpl(it, javaSearchScope) }
+    override fun findPackage(fqName: FqName) =
+        javaFacade.findPackage(fqName.asString(), javaSearchScope)?.let { JavaPackageImpl(it, javaSearchScope) }
 
     override fun knownClassNamesInPackage(packageFqName: FqName): Set<String>? = javaFacade.knownClassNamesInPackage(packageFqName)
 
