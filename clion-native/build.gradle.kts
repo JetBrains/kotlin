2f8994f023c9ba1d75531e5cad7df3b62bdeea88
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2019-03-25 11:31:48 +0700

    Add cache-redirector for JB markdown plugin

--- clion-native/build.gradle.kts
+++ clion-native/build.gradle.kts
@@ -6,8 +6,13 @@ plugins {
 
 val clionUnscrambledJarDir: File by rootProject.extra
 
-repositories {
-    maven("https://dl.bintray.com/jetbrains/markdown")
+val cacheRedirectorEnabled = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() == true
+
+if (!isStandaloneBuild) {
+    repositories {
+        if (cacheRedirectorEnabled) { maven("https://cache-redirector.jetbrains.com/jetbrains.bintray.com/markdown") }
+        maven("https://jetbrains.bintray.com/markdown")
+    }
 }
 
 dependencies {
