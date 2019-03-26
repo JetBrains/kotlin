JetBrains/kotlin

commit 6fa610156e60a1be8c6c2cfa983ce1934f2a9afe

Create and configure Android compilations earlier (KT-29964)
As Android variants are created after the project is evaluated,
the build script and plugins might have added some item handlers to the
domain object containers which are yet to be filled.

To ensure that those handlers see properly configured compilations and
their tasks, we need to add a compilations to the container only after
it has been properly configured.

Also, the `forEachVariant` handler is already executed in
`afterEvaluate`, and there seems to be no need to wrap our
code working with variants in `afterEvaluate`.

Issue #KT-29964 Fixed

libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
@@ -727,44 +727,48 @@ abstract class AbstractAndroidProjectHandler<V>(private val kotlinConfigurationT
         kotlinOptions.noJdk = true
         ext.addExtension(KOTLIN_OPTIONS_DSL_NAME, kotlinOptions)
 
-        project.afterEvaluate { project ->
-        forEachVariant(project) { variant ->
-            val variantName = getVariantName(variant)
-            val compilation = kotlinAndroidTarget.compilations.create(variantName)
-            setUpDependencyResolution(variant, compilation)
-        }
+        val androidPluginIds = listOf(
+            "android", "com.android.application", "android-library", "com.android.library",
+            "com.android.test", "com.android.feature", "com.android.dynamic-feature", "com.android.instantapp"
+        )
 
-            val androidPluginIds = listOf(
-                "android", "com.android.application", "android-library", "com.android.library",
-                "com.android.test", "com.android.feature", "com.android.dynamic-feature", "com.android.instantapp"
-            )
-            val plugin = androidPluginIds.asSequence()
+        val plugin by lazy {
+            androidPluginIds.asSequence()
                 .mapNotNull { project.plugins.findPlugin(it) as? BasePlugin }
                 .firstOrNull()
                 ?: throw InvalidPluginException("'kotlin-android' expects one of the Android Gradle " +
                                                         "plugins to be applied to the project:\n\t" +
                                                         androidPluginIds.joinToString("\n\t") { "* $it" })
+        }
 
-            checkAndroidAnnotationProcessorDependencyUsage(project)
+        forEachVariant(project) { variant ->
+            val variantName = getVariantName(variant)
 
-            forEachVariant(project) {
-                processVariant(
-                    it, kotlinAndroidTarget, project, ext, plugin, kotlinOptions, kotlinConfigurationTools.kotlinTasksProvider
-                )
+            // Create the compilation and configure it first, then add to the compilations container. As this code is executed
+            // in afterEvaluate, a user's build script might have already attached item handlers to the compilations container, and those
+            // handlers might break when fired on a compilation that is not yet properly configured (e.g. KT-29964):
+            kotlinAndroidTarget.compilationFactory.create(variantName).let { compilation ->
+                setUpDependencyResolution(variant, compilation)
+
+                processVariant(variant, compilation, project, ext, plugin, kotlinOptions, kotlinConfigurationTools.kotlinTasksProvider)
+
+                @Suppress("UNCHECKED_CAST")
+                (kotlinAndroidTarget.compilations as NamedDomainObjectCollection<in KotlinJvmAndroidCompilation>).add(compilation)
             }
 
             val subpluginEnvironment = SubpluginEnvironment.loadSubplugins(project, kotlinConfigurationTools.kotlinPluginVersion)
+            val compilation = kotlinAndroidTarget.compilations.getByName(getVariantName(variant))
+            applySubplugins(project, compilation, variant, subpluginEnvironment)
+        }
 
-            forEachVariant(project) { variant ->
-                val compilation = kotlinAndroidTarget.compilations.getByName(getVariantName(variant))
-                applySubplugins(project, compilation, variant, subpluginEnvironment)
-            }
+        project.whenEvaluated {
+            checkAndroidAnnotationProcessorDependencyUsage(project)
         }
     }
 
     private fun processVariant(
         variantData: V,
-        target: KotlinAndroidTarget,
+        compilation: KotlinJvmAndroidCompilation,
         project: Project,
         androidExt: BaseExtension,
         androidPlugin: BasePlugin,
@@ -782,7 +786,6 @@ abstract class AbstractAndroidProjectHandler<V>(private val kotlinConfigurationT
             return
         }
 
-        val compilation = target.compilations.getByName(variantDataName)
         val defaultSourceSet = project.kotlinExtension.sourceSets.maybeCreate(compilation.defaultSourceSetName)
 
         val kotlinTaskName = compilation.compileKotlinTaskName

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/new-mpp-android/lib/build.gradle
@@ -36,6 +36,21 @@ dependencies {
     androidTestImplementation 'com.android.support.test.espresso:espresso-core:3.0.2'
 }
 
+kotlin.targets.all {
+    compilations.all {
+        // KT-29964: check that Android compilations can be configured with an `all { ... }` handler:
+        kotlinOptions { 
+            verbose = true
+        }
+        compileKotlinTask.doFirst {
+            if (!compileKotlinTask.kotlinOptions.verbose) {
+                throw new AssertionError("kotlinOptions were not configured properly")
+            }
+            println "KT-29964 OK"
+        }
+    }
+}
+
 kotlin {
     sourceSets {
         jvmLibMain {

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/AbstractKotlinAndroidGradleTests.kt
@@ -21,6 +21,8 @@ class KotlinAndroid32GradleIT : KotlinAndroid3GradleIT(androidGradlePluginVersio
         build("assemble", "compileDebugUnitTestJavaWithJavac", "printCompilerPluginOptions") {
             assertSuccessful()
 
+            assertContains("KT-29964 OK") // Output from lib/build.gradle
+
             assertTasksExecuted(
                 ":lib:compileDebugKotlinAndroidLib",
                 ":lib:compileReleaseKotlinAndroidLib",

libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/android/KotlinAndroidTarget.kt
@@ -26,7 +26,7 @@ open class KotlinAndroidTarget(
     override val platformType: KotlinPlatformType
         get() = KotlinPlatformType.androidJvm
 
-    private val compilationFactory = KotlinJvmAndroidCompilationFactory(project, this)
+    internal val compilationFactory = KotlinJvmAndroidCompilationFactory(project, this)
 
     override val compilations: NamedDomainObjectContainer<out KotlinJvmAndroidCompilation> =
         project.container(compilationFactory.itemClass, compilationFactory)

commit 7aed9fd5929464319abea1bc89cec7a0b1baf541

Fix Android AP in Kapt (KT-30632), run Android Kapt IT with AGP 3.3.2
Fixes a bug introduced by the commit 6fa610156e6, which led to Kapt
options being imported from Android AP options too early (immediately at
creation time for each Android variant, while the Android plugin added
the options only afterwards).

Move Android subplugin options configuration to the time & scope where
it was prior to 6fa610156e6.

libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
@@ -772,12 +772,14 @@ abstract class AbstractAndroidProjectHandler<V>(private val kotlinConfigurationT
                 (kotlinAndroidTarget.compilations as NamedDomainObjectCollection<in KotlinJvmAndroidCompilation>).add(compilation)
             }
 
-            val subpluginEnvironment = SubpluginEnvironment.loadSubplugins(project, kotlinConfigurationTools.kotlinPluginVersion)
-            val compilation = kotlinAndroidTarget.compilations.getByName(getVariantName(variant))
-            applySubplugins(project, compilation, variant, subpluginEnvironment)
         }
 
         project.whenEvaluated {
+            forEachVariant(project) { variant ->
+                val subpluginEnvironment = SubpluginEnvironment.loadSubplugins(project, kotlinConfigurationTools.kotlinPluginVersion)
+                val compilation = kotlinAndroidTarget.compilations.getByName(getVariantName(variant))
+                applySubplugins(project, compilation, variant, subpluginEnvironment)
+            }
             checkAndroidAnnotationProcessorDependencyUsage(project)
         }
     }

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kapt3AndroidIT.kt
@@ -20,6 +20,14 @@ open class Kapt3Android32IT : Kapt3AndroidIT() {
         get() = GradleVersionRequired.AtLeast("4.6")
 }
 
+open class Kapt3Android33IT : Kapt3AndroidIT() {
+    override val androidGradlePluginVersion: AGPVersion
+        get() = AGPVersion.v3_3_2
+
+    override val defaultGradleVersion: GradleVersionRequired
+        get() = GradleVersionRequired.AtLeast("5.2")
+}
+
 class Kapt3Android31IT : Kapt3AndroidIT() {
     override val androidGradlePluginVersion: AGPVersion
         get() = AGPVersion.v3_1_0

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/util/AGPVersion.kt
@@ -21,5 +21,6 @@ class AGPVersion private constructor(private val versionNumber: VersionNumber) {
         val v3_0_0 = fromString("3.0.0")
         val v3_1_0 = fromString("3.1.0")
         val v3_2_0 = fromString("3.2.0")
+        val v3_3_2 = fromString("3.3.2")
     }
 }

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kapt2/android-databinding/build.gradle
@@ -1,3 +1,5 @@
+import org.gradle.util.VersionNumber
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
@@ -20,5 +22,19 @@ allprojects {
         mavenLocal()
         maven { url 'https://maven.google.com' }
         jcenter()
+    }
+
+    if (VersionNumber.parse(android_tools_version) >= VersionNumber.parse("3.3.0")) {
+        // Workaround: the AGP 3.3.2 databinding library depends on these exact versions, and the espresso test library
+        // transitively brings newer incompatible ones. Force these versions:
+        configurations.all {
+            resolutionStrategy {
+                force 'com.android.support:support-annotations:26.1.0'
+                force 'com.android.support:support-core-utils:26.1.0'
+                force 'com.android.support:support-compat:26.1.0'
+                force 'android.arch.lifecycle:common:1.0.3'
+                force 'android.arch.core:common:1.0.0'
+            }
+        }
     }
 }

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kapt2/android-realm/build.gradle
@@ -41,6 +41,19 @@ android {
     dexOptions {
         incremental false
     }
+
+    packagingOptions {
+        // Workaround for org.jetbrains.kotlin.gradle.Kapt3Android33IT#testRealm
+        // Android Plugin 3.3.2 fails with:
+        // Execution failed for task ':transformResourcesWithMergeJavaResForDebug'.
+        // com.android.builder.merge.DuplicateRelativeFileException: (these appear one by one if you exclude single files)
+        // - More than one file was found with OS independent path 'kotlin/reflect/reflect.kotlin_builtins'
+        // - More than one file was found with OS independent path 'kotlin/coroutines/coroutines.kotlin_builtins'
+        // - More than one file was found with OS independent path 'kotlin/collections/collections.kotlin_builtins'
+        // - More than one file was found with OS independent path 'kotlin/internal/internal.kotlin_builtins'
+        // (maybe more)
+        exclude '**/*.kotlin_builtins'
+    }
 }
 
 repositories {