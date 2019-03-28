b75be7520b9e8115ca2fc66159b637cce39d7e93
pyos <pyos@google.com>
2019-03-28 11:02:49 +0100

    Return an immaterial value from Boolean.not

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/Not.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/intrinsics/Not.kt
@@ -17,20 +17,22 @@
 package org.jetbrains.kotlin.backend.jvm.intrinsics
 
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
-import org.jetbrains.kotlin.codegen.Callable
-import org.jetbrains.kotlin.codegen.CallableMethod
-import org.jetbrains.kotlin.codegen.ExpressionCodegen
-import org.jetbrains.kotlin.codegen.StackValue
+import org.jetbrains.kotlin.backend.jvm.codegen.BlockInfo
+import org.jetbrains.kotlin.codegen.*
 import org.jetbrains.kotlin.ir.expressions.IrMemberAccessExpression
 import org.jetbrains.kotlin.psi.KtPrefixExpression
 import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
 import org.jetbrains.kotlin.resolve.jvm.jvmSignature.JvmMethodSignature
-import org.jetbrains.org.objectweb.asm.Type
+import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
 
 class Not : IntrinsicMethod() {
     override fun toCallable(expression: IrMemberAccessExpression, signature: JvmMethodSignature, context: JvmBackendContext): IrIntrinsicFunction {
-        return IrIntrinsicFunction.create(expression, signature, context) {
-            StackValue.not(StackValue.onStack(Type.BOOLEAN_TYPE)).put(it)
+        return object : IrIntrinsicFunction(expression, signature, context, expression.argTypes(context)) {
+            override fun invoke(
+                v: InstructionAdapter,
+                codegen: org.jetbrains.kotlin.backend.jvm.codegen.ExpressionCodegen,
+                data: BlockInfo
+            ): StackValue = StackValue.not(expression.dispatchReceiver!!.accept(codegen, data))
         }
     }
 
