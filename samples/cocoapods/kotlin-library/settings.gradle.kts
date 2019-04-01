15746490d5516bff9b965f18df3e37c80520b4d1
Ilya Matveev <ilya.matveev@jetbrains.com>
2019-04-01 20:12:25 +0700

    Samples: Don't reuse the root gradle.properties
    Earlier we used a single gradle.properties in all samples. But some
    of samples are represented by separate gradle builds. In such samples
    we had to read this gradle.properties manually making these samples
    less clear. This patch creates a separate gradle.properties for
    each gradle build and gets rid of manual property reading.

--- samples/cocoapods/kotlin-library/settings.gradle.kts
+++ samples/cocoapods/kotlin-library/settings.gradle.kts
@@ -6,36 +6,15 @@ pluginManagement {
     }
 
     resolutionStrategy {
-        // Workaround to be able to read plugin version from the properties file.
-        // See: https://stackoverflow.com/questions/52800536/how-to-use-plugin-version-from-gradle-properties-in-gradle-kotlin-dsl.
-        val kotlinVersion = getRootProperties().getValue("kotlin_version") as String
+        val kotlin_version: String by settings
         eachPlugin {
             when {
                 requested.id.id == "org.jetbrains.kotlin.native.cocoapods" ||
                 requested.id.id == "kotlin-native-cocoapods" ->
-                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
                 requested.id.id.startsWith("org.jetbrains.kotlin") ->
-                    useVersion(kotlinVersion)
+                    useVersion(kotlin_version)
             }
         }
-    }
-}
-
-// Reuse Kotlin version and other properties from the root samples project.
-fun getRootProperties() = java.util.Properties().apply {
-    val rootProjectGradlePropertiesFile = file("${rootProject.projectDir}/../../gradle.properties")
-    if (!rootProjectGradlePropertiesFile.isFile) {
-        throw Exception("File $rootProjectGradlePropertiesFile does not exist or is not a file")
-    }
-
-    rootProjectGradlePropertiesFile.inputStream().use { inputStream ->
-        load(inputStream)
-    }
-}
-
-gradle.beforeProject {
-    getRootProperties().forEach { key, value ->
-        if (!project.hasProperty(key as String))
-            project.extra[key] = value
     }
 }
