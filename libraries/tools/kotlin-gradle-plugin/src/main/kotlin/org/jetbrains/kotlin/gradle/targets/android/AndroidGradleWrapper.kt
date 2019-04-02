d5e434e95b7699851c890fa02c07dfa04f7cc4f3
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2019-04-02 12:30:41 +0300

    Gradle: move JS and Android platform specific files to targets package: add suppress PackageDirectoryMismatch

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/android/AndroidGradleWrapper.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/android/AndroidGradleWrapper.kt
@@ -1,8 +1,9 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("PackageDirectoryMismatch") // Old package for compatibility
 package org.jetbrains.kotlin.gradle.plugin.android
 
 import com.android.build.gradle.BaseExtension
