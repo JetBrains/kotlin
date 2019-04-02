09611a612af575df390dd20cea2cb4c097c68df8
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2019-04-02 12:00:09 +0300

    Gradle, Native: move all Native-related files to targets.native directory: add suppress PackageDirectoryMismatch

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/KotlinNativeTasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/KotlinNativeTasks.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("PackageDirectoryMismatch") // Old package for compatibility
 package org.jetbrains.kotlin.gradle.tasks
 
 import groovy.lang.Closure
