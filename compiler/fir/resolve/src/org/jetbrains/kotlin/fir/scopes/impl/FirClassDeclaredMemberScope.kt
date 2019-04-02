a25f928ac108573be0d9d24dbb93bd975d829773
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-04-02 17:18:02 +0300

    Fix declared member scope

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirClassDeclaredMemberScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirClassDeclaredMemberScope.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.fir.scopes.impl
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirRegularClass
+import org.jetbrains.kotlin.fir.scopes.FirScope
 import org.jetbrains.kotlin.fir.scopes.ProcessorAction
 import org.jetbrains.kotlin.fir.scopes.ProcessorAction.NEXT
 import org.jetbrains.kotlin.fir.scopes.ProcessorAction.STOP
@@ -15,10 +16,8 @@ import org.jetbrains.kotlin.fir.symbols.*
 import org.jetbrains.kotlin.name.Name
 
 class FirClassDeclaredMemberScope(
-    klass: FirRegularClass,
-    session: FirSession,
-    lookupInFir: Boolean = true
-) : FirAbstractProviderBasedScope(session, lookupInFir) {
+    klass: FirRegularClass
+) : FirScope {
     private val classId = klass.symbol.classId
     private val callablesIndex by lazy {
         klass.declarations.filterIsInstance<FirCallableDeclaration>()
@@ -33,6 +32,8 @@ class FirClassDeclaredMemberScope(
                 return STOP
             }
         }
+        return NEXT
+    }
 
     override fun processPropertiesByName(name: Name, processor: (ConeVariableSymbol) -> ProcessorAction): ProcessorAction {
         val symbols = callablesIndex[CallableId(classId.packageFqName, classId.relativeClassName, name)] ?: emptyList()
