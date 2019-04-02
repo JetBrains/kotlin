b3cf3c657981e60c7cae642903340ab69518fefa
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-02 15:41:37 +0300

    FIR structure: include calleeReference into FirDelegatedConstructorCall

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/impl/FirDelegatedConstructorCallImpl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/impl/FirDelegatedConstructorCallImpl.kt
@@ -7,8 +7,12 @@ package org.jetbrains.kotlin.fir.expressions.impl
 
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.fir.FirElement
+import org.jetbrains.kotlin.fir.FirReference
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.expressions.FirDelegatedConstructorCall
+import org.jetbrains.kotlin.fir.expressions.FirQualifiedAccess
+import org.jetbrains.kotlin.fir.references.FirExplicitSuperReference
+import org.jetbrains.kotlin.fir.references.FirExplicitThisReference
 import org.jetbrains.kotlin.fir.transformSingle
 import org.jetbrains.kotlin.fir.types.FirTypeRef
 import org.jetbrains.kotlin.fir.visitors.FirTransformer
@@ -19,8 +23,17 @@ class FirDelegatedConstructorCallImpl(
     override var constructedTypeRef: FirTypeRef,
     override val isThis: Boolean
 ) : FirAbstractCall(session, psi), FirDelegatedConstructorCall {
+    override var calleeReference: FirReference =
+        if (isThis) FirExplicitThisReference(session, psi, null) else FirExplicitSuperReference(session, psi, constructedTypeRef)
+
+    override fun <D> transformCalleeReference(transformer: FirTransformer<D>, data: D): FirQualifiedAccess {
+        calleeReference = calleeReference.transformSingle(transformer, data)
+        return this
+    }
+
     override fun <D> transformChildren(transformer: FirTransformer<D>, data: D): FirElement {
         constructedTypeRef = constructedTypeRef.transformSingle(transformer, data)
+        calleeReference = calleeReference.transformSingle(transformer, data)
 
         return super<FirAbstractCall>.transformChildren(transformer, data)
     }
