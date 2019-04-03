f067689f1ae5ab4289b8a1bd547e32d6c05b7bac
Ivan Kylchik <cilcic.v22@gmail.com>
2019-04-03 18:41:07 +0300

    Modification of previously added test

---
+++ compiler/psi/src/org/jetbrains/kotlin/parsing/MyKotlinParser.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.parsing;
+
+import com.intellij.lang.LighterASTNode;
+import com.intellij.lang.PsiBuilder;
+import com.intellij.util.diff.FlyweightCapableTreeStructure;
+
+public class MyKotlinParser {
+    public static FlyweightCapableTreeStructure<LighterASTNode> parse(PsiBuilder builder) {
+        KotlinParsing ktParsing = KotlinParsing.createForTopLevel(new SemanticWhitespaceAwarePsiBuilderImpl(builder));
+        ktParsing.parseFile();
+
+        return builder.getLightTree();
+    }
+}
