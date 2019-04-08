26a9b2b251efe2c37edd3f8b26f36ca342996802
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2019-04-08 11:51:45 +0300

    JPS Build: fix kotlin-main-kts-test dependency to kotlin-main-kts

--- libraries/tools/kotlin-main-kts-test/build.gradle.kts
+++ libraries/tools/kotlin-main-kts-test/build.gradle.kts
@@ -6,13 +6,17 @@ plugins {
 }
 
 dependencies {
-    testCompile(projectRuntimeJar(":kotlin-main-kts")) // runtimeJar needed to for proper dependency on the jar with relocations
+    if (!kotlinBuildProperties.isInJpsBuildIdeaSync) {
+        testCompile(projectRuntimeJar(":kotlin-main-kts")) // runtimeJar needed to for proper dependency on the jar with relocations
+    }
+
     testCompile(project(":kotlin-scripting-jvm-host-embeddable"))
     testCompile(commonDep("junit"))
     compileOnly("org.apache.ivy:ivy:2.4.0") // for jps/pill
 
     if (kotlinBuildProperties.isInJpsBuildIdeaSync) {
         testCompile(project(":kotlin-scripting-jvm-host"))
+        testCompile(project(":kotlin-main-kts"))
     }
 }
 
