c84c33cbd6d691c52d7713f7993d4889cff73ca1
Caleb Brinkman <floralvikings@gmail.com>
2019-04-09 10:22:03 -0500

    KT-30816 BasicJvmScriptEvaluator incorrect arg order
    https://youtrack.jetbrains.com/issue/KT-30816
    
    Change BasicJvmScriptEvaluator to pass constructor parameters in the
    correct order, include tests for scripts w/ provided properties, and
    implicit receiver, and both.

---
+++ libraries/scripting/jvm-host/test/kotlin/script/experimental/jvmhost/test/ConstructorArgumentOrderTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.script.experimental.jvmhost.test
+
+import org.junit.Test
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.implicitReceivers
+import kotlin.script.experimental.api.providedProperties
+import kotlin.test.assertTrue
+
+class ConstructorArgumentsOrderTest {
+
+    @Test
+    fun testScriptWithProvidedProperties() {
+        val res = evalString<ScriptWithProvidedProperties>("""println(providedString)""") {
+            providedProperties("providedString" to "Hello Provided!")
+        }
+
+        assertTrue(
+            res is ResultWithDiagnostics.Success,
+            "test failed:\n  ${res.reports.joinToString("\n  ") { it.message + if (it.exception == null) "" else ": ${it.exception}" }}"
+        )
+    }
+
+    @Test
+    fun testScriptWithImplicitReceiver() {
+        val res = evalString<ScriptWithImplicitReceiver>("""println(receiverString)""") {
+            implicitReceivers(ImplicitReceiverClass("Hello Receiver!"))
+        }
+
+        assertTrue(
+            res is ResultWithDiagnostics.Success,
+            "test failed:\n  ${res.reports.joinToString("\n  ") { it.message + if (it.exception == null) "" else ": ${it.exception}" }}"
+        )
+    }
+
+    @Test
+    fun testScriptWithBoth() {
+        val res = evalString<ScriptWithBoth>("""println(providedString + receiverString)""") {
+            providedProperties("providedString" to "Hello")
+            implicitReceivers(ImplicitReceiverClass(" Both!"))
+        }
+
+        assertTrue(
+            res is ResultWithDiagnostics.Success,
+            "test failed:\n  ${res.reports.joinToString("\n  ") { it.message + if (it.exception == null) "" else ": ${it.exception}" }}"
+        )
+    }
+}
