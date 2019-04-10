52c6cafb9cf2916cf2d3baa3df30cd1581dfd041
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-10 17:37:10 +0300

    Preliminary: argument mapper

---
+++ compiler/fir/resolve/testData/resolve/arguments/vararg.kt
@@ -0,0 +1,16 @@
+fun foo(x: Int, vararg y: String) {}
+fun bar(x: Int, vararg y: String, z: Boolean) {}
+
+fun test() {
+    foo(1)
+    foo(1, "")
+    foo(1, "my", "yours")
+
+    foo("")
+    foo(1, 2)
+
+    bar(1, z = true, y = *arrayOf("my", "yours"))
+
+    bar(0, z = false, y = "", y = "other")
+    bar(0, "", true)
+}
