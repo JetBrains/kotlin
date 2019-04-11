c76d9a29bb62033a03390b403525b02a0b504096
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2019-04-11 21:30:29 +0300

    Build: use default jar task in kotlin-daemon-client

--- compiler/daemon/daemon-client/build.gradle.kts
+++ compiler/daemon/daemon-client/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 description = "Kotlin Daemon Client"
 
 plugins {
@@ -47,11 +45,7 @@ sourceSets {
 
 publish()
 
-noDefaultJar()
-
-runtimeJar(task<ShadowJar>("shadowJar")) {
-    from(mainSourceSet.output)
-}
+runtimeJar()
 
 sourcesJar()
 
