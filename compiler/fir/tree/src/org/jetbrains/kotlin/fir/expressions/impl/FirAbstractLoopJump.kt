c6f54e85772719b703051c127c8b7f3f44c21099
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-11 16:56:19 +0300

    FIR: implement trivial typing for break & continue

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/impl/FirAbstractLoopJump.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/impl/FirAbstractLoopJump.kt
@@ -6,15 +6,18 @@
 package org.jetbrains.kotlin.fir.expressions.impl
 
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.fir.FirAbstractElement
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.FirTarget
 import org.jetbrains.kotlin.fir.expressions.FirJump
 import org.jetbrains.kotlin.fir.expressions.FirLoop
+import org.jetbrains.kotlin.fir.types.FirTypeRef
+import org.jetbrains.kotlin.fir.types.impl.FirImplicitNothingTypeRef
 
 abstract class FirAbstractLoopJump(
     session: FirSession,
     psi: PsiElement?
 ) : FirAbstractExpression(session, psi), FirJump<FirLoop> {
     override lateinit var target: FirTarget<FirLoop>
+
+    override var typeRef: FirTypeRef = FirImplicitNothingTypeRef(session, psi)
 }
