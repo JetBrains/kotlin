1bc078769beccf54a917a6f02a4e507298fb6fbf
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2019-04-12 18:17:48 +0300

    Regenerate tests

--- compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -9777,11 +9777,6 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                     runTest("compiler/testData/diagnostics/tests/inference/capturedTypes/capturedType.kt");
                 }
 
-                @TestMetadata("capturedTypeAgainstNullableInvariantType.kt")
-                public void testCapturedTypeAgainstNullableInvariantType() throws Exception {
-                    runTest("compiler/testData/diagnostics/tests/inference/capturedTypes/capturedTypeAgainstNullableInvariantType.kt");
-                }
-
                 @TestMetadata("capturedTypeAndApproximation.kt")
                 public void testCapturedTypeAndApproximation() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/capturedTypes/capturedTypeAndApproximation.kt");
