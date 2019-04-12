ac7f71148ee5918769fbc6ea4ee4aeab0f695312
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2019-04-12 18:23:35 +0300

    A bit more testdata change due to the fact that we were not checking for expect/actual matching in misconfigured projects

--- idea/testData/multiModuleHighlighting/multiplatform/triangleWithDependency/base_common/My.kt
+++ idea/testData/multiModuleHighlighting/multiplatform/triangleWithDependency/base_common/My.kt
@@ -1,3 +1,3 @@
-expect class My
+expect class <error descr="[NO_ACTUAL_FOR_EXPECT] Expected class 'My' has no actual declaration in module base_Common">My</error>
 
-expect fun bar()
+expect fun <error descr="[NO_ACTUAL_FOR_EXPECT] Expected function 'bar' has no actual declaration in module base_Common">bar</error>()
