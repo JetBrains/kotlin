65244b4bea81f737466618927d4f3afe339cad0d
nikita.movshin <nikita.movshin@jetbrains.com>
2019-04-18 12:02:27 +0300

    Update copyright. Change the copyright from "JetBrains s.r.o." to "JetBrains s.r.o. and Kotlin Project contributors" Update only 2 lines copyright.

--- buildSrc/src/main/kotlin/buildProperties.kt
+++ buildSrc/src/main/kotlin/buildProperties.kt
@@ -5,8 +5,8 @@ import java.io.File
 import java.util.*
 
 /*
- * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 interface PropertiesProvider {
