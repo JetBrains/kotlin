00c76593f20b8958e3381e85f375ff83e3dc1f8c
Kirill Rakhman <kirill.rakhman@gmail.com>
2019-04-19 13:25:35 +0200

    fix typo in test data

--- idea/testData/inspectionsLocal/replaceJavaStaticMethodWithKotlinAnalog/collections/mutableSetOf.kt
+++ idea/testData/inspectionsLocal/replaceJavaStaticMethodWithKotlinAnalog/collections/mutableSetOf.kt
@@ -1,6 +1,6 @@
 // WITH_RUNTIME
 // MIN_JAVA_VERSION: 9
-// FIX: Replace with `mtableSetOf` function
+// FIX: Replace with `mutableSetOf` function
 
 fun test() {
     val a = java.util.Set.of<caret><String>()
