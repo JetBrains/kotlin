16d7d202d26aa7df4ae3a6f29d3b113fb0501f0b
Ivan Kylchik <cilcic.v22@gmail.com>
2019-04-19 19:13:50 +0300

    Add separated tests for tree generation and fir convertion

--- compiler/fir/lightTree/src/org/jetbrains/kotlin/fir/lightTree/LightTree2Fir.kt
+++ compiler/fir/lightTree/src/org/jetbrains/kotlin/fir/lightTree/LightTree2Fir.kt
@@ -5,31 +5,38 @@
 
 package org.jetbrains.kotlin.fir.lightTree
 
+import com.intellij.lang.LighterASTNode
 import com.intellij.lang.ParserDefinition
 import com.intellij.lang.impl.PsiBuilderFactoryImpl
 import com.intellij.lexer.Lexer
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.CharsetToolkit
+import com.intellij.util.diff.FlyweightCapableTreeStructure
 import org.jetbrains.kotlin.fir.FirSessionBase
 import org.jetbrains.kotlin.fir.declarations.FirFile
 import org.jetbrains.kotlin.parsing.MyKotlinParser
 import java.io.File
 import java.nio.file.Path
 
-class LightTree2Fir(val stubMode: Boolean, private val parserDefinition: ParserDefinition, private val lexer: Lexer) {
+class LightTree2Fir(private val stubMode: Boolean, private val parserDefinition: ParserDefinition, private val lexer: Lexer) {
     fun buildFirFile(path: Path): FirFile {
         return buildFirFile(path.toFile())
     }
 
     fun buildFirFile(file: File): FirFile {
-        val text = FileUtil.loadFile(file, CharsetToolkit.UTF8, true).trim()
-        return buildFirFile(text, file.name)
+        val code = FileUtil.loadFile(file, CharsetToolkit.UTF8, true).trim()
+        return buildFirFile(code, file.name)
     }
 
-    fun buildFirFile(text: String, fileName: String): FirFile {
-        val builder = PsiBuilderFactoryImpl().createBuilder(parserDefinition, lexer, text)
+    fun buildLightTree(code: String): FlyweightCapableTreeStructure<LighterASTNode> {
+        val builder = PsiBuilderFactoryImpl().createBuilder(parserDefinition, lexer, code)
         MyKotlinParser.parse(builder)
+        return builder.lightTree
+    }
 
-        return Converter(object : FirSessionBase() {}, stubMode, builder.lightTree).convertFile(builder.lightTree.root, fileName)
+    fun buildFirFile(code: String, fileName: String): FirFile {
+        val lightTree = buildLightTree(code)
+
+        return Converter(object : FirSessionBase() {}, stubMode, lightTree).convertFile(lightTree.root, fileName)
     }
 }
