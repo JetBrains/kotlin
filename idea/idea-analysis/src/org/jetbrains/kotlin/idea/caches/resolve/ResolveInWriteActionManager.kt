b83d07ae9ab57ea692b4d3d1bd6ff623f74352ab
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-04-19 17:49:53 +0300

    Assert new quick fixes don't use resolve

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/ResolveInWriteActionManager.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/ResolveInWriteActionManager.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.idea.caches.resolve
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.util.registry.Registry
+import org.jetbrains.annotations.TestOnly
 
 /**
  * Temporary allow resolve in write action.
@@ -19,10 +20,24 @@ fun <T> allowResolveInWriteAction(runnable: () -> T): T {
     return ResolveInWriteActionManager.runWithResolveAllowedInWriteAction(runnable)
 }
 
+/**
+ * Force resolve check in tests.
+ */
+@TestOnly
+fun <T> forceResolveInWriteActionCheckInTests(errorHandler: (() -> Unit)? = null, runnable: () -> T): T {
+    return ResolveInWriteActionManager.runWithForceResolveAllowedCheckInTests(errorHandler, runnable)
+}
+
+private const val RESOLVE_IN_WRITE_ACTION_ERROR_MESSAGE = "Resolve is not allowed under the write action!"
+
+class ResolveInWriteActionException(message: String? = null) : IllegalThreadStateException(message ?: RESOLVE_IN_WRITE_ACTION_ERROR_MESSAGE)
+
 internal object ResolveInWriteActionManager {
     private val LOG = Logger.getInstance(ResolveInWriteActionManager::class.java)
 
     private val isResolveInWriteActionAllowed: ThreadLocal<Boolean> = ThreadLocal.withInitial { false }
+    private val isForceCheckInTests: ThreadLocal<Boolean> = ThreadLocal.withInitial { false }
+    private val errorHandler: ThreadLocal<(() -> Unit)?> = ThreadLocal.withInitial { null }
 
     fun assertNoResolveUnderWriteAction() {
         if (isResolveInWriteActionAllowed.get()) return
@@ -31,15 +46,25 @@ internal object ResolveInWriteActionManager {
 
         if (!application.isWriteAccessAllowed) return
 
-        if (application.isUnitTestMode) return
+        if (application.isUnitTestMode) {
+            if (!isForceCheckInTests.get()) return
 
-        @Suppress("InvalidBundleOrProperty")
-        if (!Registry.`is`("kotlin.write.resolve.check", false)) return
+            val handler = errorHandler.get()
+            if (handler != null) {
+                handler()
+                return
+            }
 
-        LOG.error("Resolve is not allowed under the write action!")
+            throw ResolveInWriteActionException()
+        } else {
+            @Suppress("InvalidBundleOrProperty")
+            if (!Registry.`is`("kotlin.write.resolve.check", false)) return
+
+            LOG.error(RESOLVE_IN_WRITE_ACTION_ERROR_MESSAGE)
+        }
     }
 
-    inline fun <T> runWithResolveAllowedInWriteAction(runnable: () -> T): T {
+    internal inline fun <T> runWithResolveAllowedInWriteAction(runnable: () -> T): T {
         val wasSet =
             if (ApplicationManager.getApplication()?.isWriteAccessAllowed == true && isResolveInWriteActionAllowed.get() == false) {
                 isResolveInWriteActionAllowed.set(true)
@@ -53,6 +78,25 @@ internal object ResolveInWriteActionManager {
         } finally {
             if (wasSet) {
                 isResolveInWriteActionAllowed.set(false)
+            }
+        }
+    }
+
+    internal fun <T> runWithForceResolveAllowedCheckInTests(errorHandler: (() -> Unit)?, runnable: () -> T): T {
+        val wasSet = if (isForceCheckInTests.get() == false) {
+            isForceCheckInTests.set(true)
+            this.errorHandler.set(errorHandler)
+            true
+        } else {
+            false
+        }
+
+        try {
+            return runnable()
+        } finally {
+            if (wasSet) {
+                isForceCheckInTests.set(false)
+                this.errorHandler.set(null)
             }
         }
     }
