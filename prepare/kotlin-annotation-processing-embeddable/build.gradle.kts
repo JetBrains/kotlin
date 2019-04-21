9f3495138f4ee660fd3395c923c68e265e0a6428
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2019-04-21 16:50:49 +0300

    Build: Use embedded configuration in kapt embeddable project
     #KT-31047

--- prepare/kotlin-annotation-processing-embeddable/build.gradle.kts
+++ prepare/kotlin-annotation-processing-embeddable/build.gradle.kts
@@ -1,5 +1,3 @@
-
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.jvm.tasks.Jar
 
 description = "Annotation Processor for Kotlin (for using with embeddable compiler)"
@@ -8,19 +6,15 @@ plugins {
     `java`
 }
 
-val packedJars by configurations.creating
-
 dependencies {
-    packedJars(project(":kotlin-annotation-processing")) { isTransitive = false }
+    embedded(project(":kotlin-annotation-processing")) { isTransitive = false }
 }
 
 publish()
 
-runtimeJar(rewriteDepsToShadedCompiler(
-        task<ShadowJar>("shadowJar")  {
-            from(packedJars)
-        }
-))
+val jar: Jar by tasks
+runtimeJar(rewriteDepsToShadedCompiler(jar))
 
 sourcesJar()
+
 javadocJar()
