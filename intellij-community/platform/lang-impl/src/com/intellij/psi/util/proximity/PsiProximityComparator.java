1e86598fc2adef57a357ffc385f8ee9b5950498c
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2019-04-22 15:28:18 +0300

    remove deprecated PsiElementBase and MetadataPsiElementBase
    intellij-monorepo: d7ce681e05bbe031c0489962838f97b19dba4e09

--- intellij-community/platform/lang-impl/src/com/intellij/psi/util/proximity/PsiProximityComparator.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/util/proximity/PsiProximityComparator.java
@@ -16,7 +16,6 @@
 
 package com.intellij.psi.util.proximity;
 
-import com.intellij.extapi.psi.MetadataPsiElementBase;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.util.Computable;
@@ -86,8 +85,6 @@ public class PsiProximityComparator implements Comparator<Object> {
   @Nullable
   public static WeighingComparable<PsiElement, ProximityLocation> getProximity(final PsiElement element, final PsiElement context) {
     if (element == null) return null;
-    //noinspection deprecation,ScheduledForRemoval
-    if (element instanceof MetadataPsiElementBase) return null;
     final Module contextModule = context != null ? ModuleUtilCore.findModuleForPsiElement(context) : null;
     return WeighingService.weigh(WEIGHER_KEY, element, new ProximityLocation(context, contextModule));
   }
@@ -95,10 +92,7 @@ public class PsiProximityComparator implements Comparator<Object> {
   @Nullable
   public static WeighingComparable<PsiElement, ProximityLocation> getProximity(final Computable<? extends PsiElement> elementComputable, final PsiElement context, ProcessingContext processingContext) {
     PsiElement element = elementComputable.compute();
-    if (element == null) return null;
-    //noinspection deprecation,ScheduledForRemoval
-    if (element instanceof MetadataPsiElementBase) return null;
-    if (context == null) return null;
+    if (element == null || context == null) return null;
     Module contextModule = processingContext.get(MODULE_BY_LOCATION);
     if (contextModule == null) {
       contextModule = ModuleUtilCore.findModuleForPsiElement(context);
