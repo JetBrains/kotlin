28be6c5b50e25928cf52217c9be03da7664de6c7
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-04-22 14:38:33 +0300

    Properly update current code style on Save
    intellij-monorepo: a589d65bc542b40a1ae9ec038a5f15407ff3135a

--- intellij-community/platform/lang-impl/src/com/intellij/formatting/contextConfiguration/ConfigureCodeStyleOnSelectedFragment.java
+++ intellij-community/platform/lang-impl/src/com/intellij/formatting/contextConfiguration/ConfigureCodeStyleOnSelectedFragment.java
@@ -34,6 +34,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CodeStyleSettingsCodeFragmentFilter;
+import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
 import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider;
 import com.intellij.psi.util.PsiUtilBase;
 import com.intellij.util.IncorrectOperationException;
@@ -177,6 +178,7 @@ public class ConfigureCodeStyleOnSelectedFragment implements IntentionAction, Lo
     private void applyFromUiToSettings() {
       try {
         myTabbedLanguagePanel.apply(mySettings);
+        CodeStyleSettingsManager.getInstance(myEditor.getProject()).notifyCodeStyleSettingsChanged();
       }
       catch (ConfigurationException e) {
         LOG.debug("Can not apply code style settings from context menu to project code style settings");
