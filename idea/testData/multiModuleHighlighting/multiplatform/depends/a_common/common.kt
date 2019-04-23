cac326bf4a8242e03e22621b0f808535f7ab2759
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2019-04-23 20:58:49 +0300

    [Expect/Actual] Adjust old testdata after changes in expect/actual matching
    This commit makes several changes in testdata:
    
    - compiler multiplatform tests now contain newly introduced diagnostics
    about AMBIGIOUTS_ACTUALS
    - MultiModuleHighlighitng tests now contain proper reports about
    ACTUAL_MISSING:
      - ACTUAL_MISSING should be indeed reported in them, because those
      tests don't contain dependsOn edges, only usual intermodule
      dependencies
      - This error wasn't reported here because expect/actual diagnostics in
      common module used to be reported by PlatformExpectedAnnotator, which
      had a bit flawed logic for deremining common-modules: it checked for
      presence of implementing modules. In those tests, common module has no
      implementing modules, so Annotator was returning silently
      Note that such configurations (common module without implementing
      modules) are almost impossible in real-life projects
      - After removal of PlatformExpectedAnnotator, we use
      ExpectedActualDeclarationChecker in common modules, which launches all
      checks properly
    - some QuickFixMultuModuleTests now contain proper reports about
    ACTUAL_MISSING. This change is also connected with
    PlatformExpectedAnnotator, but now for different reason:
      - QuickFixMultiModuleTest used to check for errors in file by running
      'analyzeWithAllCompilerChecks' and inspecting returned BindingTrace.
      For common modules, there were no diagnostics about expect/actuals in
      that trace, because there were no ExpectedActualDeclarationChecker
      (and PlatformExpectedAnnotator was reporting diagnostics in ad hoc
      trace).
      - Again, now we inject EADC in common modules properly, so we see
      those errors in trace and report them in test

--- idea/testData/multiModuleHighlighting/multiplatform/depends/a_common/common.kt
+++ idea/testData/multiModuleHighlighting/multiplatform/depends/a_common/common.kt
@@ -1,2 +1,2 @@
-expect class My
-expect fun bar()
+expect class <error descr="[NO_ACTUAL_FOR_EXPECT] Expected class 'My' has no actual declaration in module a_Common">My</error>
+expect fun <error descr="[NO_ACTUAL_FOR_EXPECT] Expected function 'bar' has no actual declaration in module a_Common">bar</error>()
