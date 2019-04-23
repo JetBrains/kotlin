337a977f4d964f77f849470a878e180469367768
develar <develar@gmail.com>
2019-04-23 18:49:43 +0200

    IDEA-181157 Cannot compile Groovy if Groovy is global library
    intellij-monorepo: 6c96511a50352704e4ca7fc956ef726ffc75a7b5

--- intellij-community/platform/configuration-store-impl/src/ApplicationStoreImpl.kt
+++ intellij-community/platform/configuration-store-impl/src/ApplicationStoreImpl.kt
@@ -77,7 +77,7 @@ class ApplicationStorageManager(application: Application?, pathMacroManager: Pat
 
   override fun providerDataStateChanged(storage: FileBasedStorage, writer: DataWriter?, type: DataStateChanged) {
     // IDEA-144052 When "Settings repository" is enabled changes in 'Path Variables' aren't saved to default path.macros.xml file causing errors in build process
-    if (storage.fileSpec == "path.macros.xml") {
+    if (storage.fileSpec == "path.macros.xml" || storage.fileSpec == "applicationLibraries.xml") {
       LOG.runAndLogException {
         writer.writeTo(storage.file)
       }
