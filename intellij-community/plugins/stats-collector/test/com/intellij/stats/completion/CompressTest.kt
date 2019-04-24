14d521472a1ab4c2a3bed487be002b9faf63d78d
develar <develar@gmail.com>
2019-04-24 12:00:26 +0200

    get rid of apache Base64 out stream
    intellij-monorepo: 3cb162f112e7740002f850ffafb22ebbec3c46a7

--- intellij-community/plugins/stats-collector/test/com/intellij/stats/completion/CompressTest.kt
+++ intellij-community/plugins/stats-collector/test/com/intellij/stats/completion/CompressTest.kt
@@ -1,7 +1,7 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.stats.completion
 
-import com.intellij.stats.network.service.GzipBase64Compressor
+import com.intellij.reporting.compressBase64Gzip
 import org.apache.commons.codec.binary.Base64InputStream
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
@@ -20,7 +20,7 @@ class CompressTest {
       | javaInheritance\u003dnull, groovyReferenceListWeigher\u003dunknown, openedInEditor\u003dtrue, sameDirectory\u003dtrue,
       | sameLogicalRoot\u003dtrue, sameModule\u003d2, knownElement\u003d0, inResolveScope\u003dtrue, sdkOrLibrary\u003dfalse]",
       |"sameWords":"0","shorter":"0","grouping":"0"}}],"currentPosition":0,"userUid":"cd1f9318cd9f"}""".trimMargin()
-    val encoded = GzipBase64Compressor.compress(text)
+    val encoded = compressBase64Gzip(text.toByteArray())
     val decoded = GZIPInputStream(Base64InputStream(ByteArrayInputStream(encoded))).reader().readText()
     assertThat(decoded).isEqualTo(text)
   }
