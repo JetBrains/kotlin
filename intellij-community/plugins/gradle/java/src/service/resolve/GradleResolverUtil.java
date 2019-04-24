97509881b22ea3ee804068618afec2db1df139b9
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-04-24 18:55:20 +0300

    [gradle] dsl: delegate settings.gradle properties/calls to o.g.api.initialization.Settings type via standard api
    intellij-monorepo: b5378ebe948c08db8e6ecaa0c9b44fe145612ead

--- intellij-community/plugins/gradle/java/src/service/resolve/GradleResolverUtil.java
+++ intellij-community/plugins/gradle/java/src/service/resolve/GradleResolverUtil.java
@@ -1,23 +1,8 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.resolve;
 
 import com.intellij.openapi.util.RecursionManager;
 import com.intellij.psi.*;
-import com.intellij.psi.scope.PsiScopeProcessor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.gradle.util.GradleLog;
@@ -93,21 +78,6 @@ public class GradleResolverUtil {
     methodWithClosure.setReturnType(retType);
     methodWithClosure.setContainingClass(retType.resolve());
     return methodWithClosure;
-  }
-
-  public static boolean processDeclarations(@NotNull PsiScopeProcessor processor,
-                                            @NotNull ResolveState state,
-                                            @NotNull PsiElement place,
-                                            @NotNull String... fqNames) {
-    JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(place.getProject());
-    for (String fqName : fqNames) {
-      if(fqName == null) continue;
-      PsiClass psiClass = javaPsiFacade.findClass(fqName, place.getResolveScope());
-      if (psiClass != null) {
-        if (!UtilKt.processDeclarations(psiClass, processor, state, place)) return false;
-      }
-    }
-    return true;
   }
 
   @Nullable
