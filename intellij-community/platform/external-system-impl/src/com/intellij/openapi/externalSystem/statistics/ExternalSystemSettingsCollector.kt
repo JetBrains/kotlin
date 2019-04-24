55231e2b637f3c9529ecaa267c48c8ffcb3a138f
Anton Makeev <Anton.Makeev@jetbrains.com>
2019-04-24 12:33:44 +0200

    FUS-171 Build tools statistics: numberOfLinkedProject and per-systemID counters added to ExternalSystemSettingsCollector
    intellij-monorepo: 69f61017186d7006fa17bdad76a17727f99550e7

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/statistics/ExternalSystemSettingsCollector.kt
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/statistics/ExternalSystemSettingsCollector.kt
@@ -2,31 +2,36 @@
 package com.intellij.openapi.externalSystem.statistics
 
 import com.intellij.internal.statistic.beans.UsageDescriptor
-import com.intellij.internal.statistic.service.fus.collectors.FUSUsageContext
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
 import com.intellij.internal.statistic.utils.getBooleanUsage
 import com.intellij.internal.statistic.utils.getCountingUsage
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
 
-class ExternalSystemSettingsCollector() : ProjectUsagesCollector() {
+class ExternalSystemSettingsCollector : ProjectUsagesCollector() {
   override fun getGroupId() = "build.tools.state"
 
   override fun getUsages(project: Project): Set<UsageDescriptor> {
     val usages = mutableSetOf<UsageDescriptor>()
 
     for (manager in ExternalSystemApiUtil.getAllManagers()) {
-      val context = FUSUsageContext.create(getAnonymizedSystemId(manager.getSystemId()))
-      for (projectsSetting in manager.getSettingsProvider().`fun`(project).getLinkedProjectsSettings()) {
-        usages.add(addContext(getBooleanUsage("autoImport", projectsSetting.isUseAutoImport), context))
-        usages.add(addContext(getBooleanUsage("useQualifiedModuleNames", projectsSetting.isUseQualifiedModuleNames), context))
-        usages.add(addContext(getCountingUsage("modules.count", projectsSetting.modules.size), context))
+      val systemId = getAnonymizedSystemId(manager.getSystemId())
+      fun addWithSustemId(desc: UsageDescriptor) {
+        desc.data.addData("externalSystemId", systemId)
+        usages.add(desc)
+      }
+
+      val projects = manager.getSettingsProvider().`fun`(project).getLinkedProjectsSettings()
+
+      addWithSustemId(getCountingUsage("numberOfLinkedProject", projects.size))
+
+      for (projectsSetting in projects) {
+        addWithSustemId(getBooleanUsage("autoImport", projectsSetting.isUseAutoImport))
+        addWithSustemId(getBooleanUsage("useQualifiedModuleNames", projectsSetting.isUseQualifiedModuleNames))
+        addWithSustemId(getCountingUsage("modules.count", projectsSetting.modules.size))
       }
     }
-    return usages
-  }
 
-  companion object {
-    private fun addContext(desc: UsageDescriptor, ctx: FUSUsageContext?) = UsageDescriptor(desc.key, desc.value, ctx)
+    return usages
   }
 }
