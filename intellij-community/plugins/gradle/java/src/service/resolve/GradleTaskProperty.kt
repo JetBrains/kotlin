7d9f6f44e8ff0d33f3b00667c3049bd8186fc384
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-04-25 19:22:03 +0300

    [groovy] extract LazyTypeProperty
    intellij-monorepo: f13b83dfa2901a4edeff761114c8eb85cbdab329

--- intellij-community/plugins/gradle/java/src/service/resolve/GradleTaskProperty.kt
+++ intellij-community/plugins/gradle/java/src/service/resolve/GradleTaskProperty.kt
@@ -5,33 +5,25 @@ import com.intellij.ide.presentation.Presentation
 import com.intellij.openapi.util.Key
 import com.intellij.psi.OriginInfoAwareElement
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiType
 import com.intellij.util.lazyPub
 import icons.ExternalSystemIcons
 import org.jetbrains.plugins.gradle.settings.GradleExtensionsSettings.GradleTask
 import org.jetbrains.plugins.groovy.dsl.holders.NonCodeMembersHolder
-import org.jetbrains.plugins.groovy.lang.psi.impl.statements.expressions.TypesUtil
-import org.jetbrains.plugins.groovy.lang.resolve.api.GroovyPropertyBase
+import org.jetbrains.plugins.groovy.lang.resolve.api.LazyTypeProperty
 import javax.swing.Icon
 
 @Presentation(typeName = "Gradle Task")
 class GradleTaskProperty(
   val task: GradleTask,
-  private val myContext: PsiElement
-) : GroovyPropertyBase(task.name, myContext),
+  context: PsiElement
+) : LazyTypeProperty(task.name, task.typeFqn, context),
     OriginInfoAwareElement {
-
-  override fun isValid(): Boolean = super.isValid() && myContext.isValid
-
-  private val type by lazyPub { TypesUtil.createType(task.typeFqn, myContext) }
-
-  override fun getPropertyType(): PsiType? = type
 
   override fun getIcon(flags: Int): Icon? = ExternalSystemIcons.Task
 
   override fun getOriginInfo(): String? = "task"
 
-  private val doc by lazyPub { GradleExtensionsContributor.getDocumentation(task, type, myContext) }
+  private val doc by lazyPub { GradleExtensionsContributor.getDocumentation(task, propertyType, myContext) }
 
   override fun <T : Any?> getUserData(key: Key<T>): T? {
     if (key == NonCodeMembersHolder.DOCUMENTATION) {
