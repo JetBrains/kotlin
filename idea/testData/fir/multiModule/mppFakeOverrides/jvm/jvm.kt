be181342707372467b31a3fda6cf83f77a7effea
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-25 17:01:27 +0300

    FIR multi-module: minor test data fixes after resolve enhancements

--- idea/testData/fir/multiModule/mppFakeOverrides/jvm/jvm.kt
+++ idea/testData/fir/multiModule/mppFakeOverrides/jvm/jvm.kt
@@ -7,6 +7,6 @@ class D : C() {
     fun test() {
         foo("")
         bar("") // should be resolved to just C.bar
-        baz("") // ambiguity (we have C.bar with CharSequence argument and fake override of A.bar with String argument)
+        baz("")
     }
 }
