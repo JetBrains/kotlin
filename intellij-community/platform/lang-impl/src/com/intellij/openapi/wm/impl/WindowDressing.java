108fa42c62f2a3a9ca05b118c46c7b5b6c47ed08
develar <develar@gmail.com>
2019-04-26 12:19:32 +0200

    convert project component WindowDressing to applicationInitializedListener
    intellij-monorepo: 70687faff0976d5d78aac38c878a258257c926a5

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/wm/impl/WindowDressing.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/wm/impl/WindowDressing.java
@@ -1,28 +1,38 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.wm.impl;
 
+import com.intellij.ide.ApplicationInitializedListener;
 import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.extensions.ExtensionNotApplicableException;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.project.ProjectManagerListener;
 import org.jetbrains.annotations.NotNull;
 
-public class WindowDressing implements ProjectComponent {
-  @NotNull private final Project myProject;
-
-  public WindowDressing(@NotNull Project project) {
-    myProject = project;
-  }
-
+public final class WindowDressing implements ApplicationInitializedListener {
   @Override
-  public void projectOpened() {
-    getWindowActionGroup().addProject(myProject);
+  public void componentsInitialized() {
+    Application app = ApplicationManager.getApplication();
+    if (app.isHeadlessEnvironment()) {
+      throw ExtensionNotApplicableException.INSTANCE;
+    }
+
+    app.getMessageBus().connect().subscribe(ProjectManager.TOPIC, new ProjectManagerListener() {
+      @Override
+      public void projectOpened(@NotNull Project project) {
+        getWindowActionGroup().addProject(project);
+      }
+
+      @Override
+      public void projectClosed(@NotNull Project project) {
+        getWindowActionGroup().removeProject(project);
+      }
+    });
   }
 
-  @Override
-  public void projectClosed() {
-    getWindowActionGroup().removeProject(myProject);
-  }
-
+  @NotNull
   public static ProjectWindowActionGroup getWindowActionGroup() {
     return (ProjectWindowActionGroup)ActionManager.getInstance().getAction("OpenProjectWindows");
   }
