74d49934fb5d6d3b43bc3f6b76d5e704e9c37837
develar <develar@gmail.com>
2019-04-27 09:44:50 +0200

    BaseState - introduce convenient methods for linkedMap/map instead of generic map
    intellij-monorepo: d0d6ac52bd785ab6555f70fdc0f45b41593b3e96

--- intellij-community/platform/configuration-store-impl/testSrc/StoredPropertyStateTest.kt
+++ intellij-community/platform/configuration-store-impl/testSrc/StoredPropertyStateTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore
 
 import com.intellij.openapi.components.BaseState
@@ -6,24 +6,8 @@ import com.intellij.openapi.util.JDOMUtil
 import com.intellij.testFramework.assertions.Assertions.assertThat
 import com.intellij.util.xmlb.annotations.Attribute
 import com.intellij.util.xmlb.annotations.CollectionBean
+import com.intellij.util.xmlb.annotations.XMap
 import org.junit.Test
-
-internal class AState(languageLevel: String? = null, nestedComplex: NestedState? = null) : BaseState() {
-  @get:Attribute("customName")
-  var languageLevel by property(languageLevel)
-
-  var bar by string()
-
-  var property2 by property(0)
-
-  var floatProperty by property(0.3f)
-
-  var nestedComplex by property(nestedComplex)
-}
-
-internal class NestedState : BaseState() {
-  var childProperty by string()
-}
 
 class StoredPropertyStateTest {
   private class Foo : BaseState() {
@@ -102,12 +86,12 @@ class StoredPropertyStateTest {
 
   @Test
   fun listModificationCount() {
-    class UpdateOptions : BaseState() {
+    class TestOptions : BaseState() {
       @get:CollectionBean
       val pluginHosts by list<String>()
     }
 
-    val state = UpdateOptions()
+    val state = TestOptions()
     val oldModificationCount = state.modificationCount
 
     val list = state.pluginHosts
@@ -118,10 +102,68 @@ class StoredPropertyStateTest {
 
     val element = state.serialize()
     assertThat(element).isEqualTo("""
-    <UpdateOptions>
+    <TestOptions>
       <pluginHosts>
         <item value="foo" />
       </pluginHosts>
-    </UpdateOptions>""")
+    </TestOptions>""")
   }
+
+  @Test
+  fun `map modification count`() {
+    class TestOptions : BaseState() {
+      @get:XMap
+      val foo by map<String, String>()
+    }
+
+    val state = TestOptions()
+    var oldModificationCount = state.modificationCount
+
+    val list = state.foo
+    list.clear()
+    list.put("a", "b")
+    assertThat(state.modificationCount).isNotEqualTo(oldModificationCount)
+    assertThat(state.isEqualToDefault()).isFalse()
+
+    val element = state.serialize()
+    assertThat(element).isEqualTo("""
+    <TestOptions>
+      <foo>
+        <entry key="a" value="b" />
+      </foo>
+    </TestOptions>""")
+
+    oldModificationCount = state.modificationCount
+    list.clear()
+    assertThat(state.modificationCount).isNotEqualTo(oldModificationCount)
+    assertThat(state.isEqualToDefault()).isTrue()
+
+    oldModificationCount = state.modificationCount
+    list.put("a", "v")
+    list.put("b", "v")
+    assertThat(state.modificationCount).isNotEqualTo(oldModificationCount)
+    assertThat(state.isEqualToDefault()).isFalse()
+
+    oldModificationCount = state.modificationCount
+    list.remove("a")
+    assertThat(state.modificationCount).isNotEqualTo(oldModificationCount)
+    assertThat(state.isEqualToDefault()).isFalse()
+  }
+}
+
+internal class AState(languageLevel: String? = null, nestedComplex: NestedState? = null) : BaseState() {
+  @get:Attribute("customName")
+  var languageLevel by property(languageLevel)
+
+  var bar by string()
+
+  var property2 by property(0)
+
+  var floatProperty by property(0.3f)
+
+  var nestedComplex by property(nestedComplex)
+}
+
+internal class NestedState : BaseState() {
+  var childProperty by string()
 }
