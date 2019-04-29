afda8a9f5adf5980e91e9ca38e650e6a72ec9ef6
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-04-29 15:33:13 +0300

    [stats-collector] Do not build diff for factors after navigation actions
    intellij-monorepo: a4723fb62d1dc6afd94b062c006114eba91b92ae

--- intellij-community/plugins/stats-collector/src/com/intellij/stats/completion/LookupStateManager.kt
+++ intellij-community/plugins/stats-collector/src/com/intellij/stats/completion/LookupStateManager.kt
@@ -24,8 +24,7 @@ class LookupStateManager {
     private val elementToId = mutableMapOf<String, Int>()
     private val idToEntryInfo = mutableMapOf<Int, LookupEntryInfo>()
 
-    fun update(lookup: LookupImpl): LookupState {
-
+    fun update(lookup: LookupImpl, factorsUpdated: Boolean): LookupState {
         val ids = mutableListOf<Int>()
         val newIds = mutableSetOf<Int>()
 
@@ -42,13 +41,19 @@ class LookupStateManager {
             ids.add(id)
         }
 
-        val infos = items.toLookupInfos(lookup)
-        val newInfos = infos.filter { it.id in newIds }
+        if (factorsUpdated) {
+            val infos = items.toLookupInfos(lookup)
+            val newInfos = infos.filter { it.id in newIds }
 
-        val itemsDiff = infos.mapNotNull { idToEntryInfo[it.id]?.calculateDiff(it) }
-        infos.forEach { idToEntryInfo[it.id] = it }
-
-        return LookupState(ids, newInfos, itemsDiff, currentPosition)
+            val itemsDiff = infos.mapNotNull { idToEntryInfo[it.id]?.calculateDiff(it) }
+            infos.forEach { idToEntryInfo[it.id] = it }
+            return LookupState(ids, newInfos, itemsDiff, currentPosition)
+        }
+        else {
+            val newItems = items.filter { getElementId(it) in newIds }.toLookupInfos(lookup)
+            newItems.forEach { idToEntryInfo[it.id] = it }
+            return LookupState(ids, newItems, emptyList(), currentPosition)
+        }
     }
 
 
