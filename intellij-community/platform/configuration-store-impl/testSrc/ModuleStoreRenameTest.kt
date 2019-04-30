12f08acf05010e60cf47f7e6f21c8796ce918079
Alexey Kudravtsev <cdr@intellij.com>
2019-04-30 19:23:27 +0300

    OMG fix project leak at last (which crashed UsageViewTest)
    intellij-monorepo: c1a6cc0d0f66f3c8ea1a4f0e5ae8cc91555a6fce

--- intellij-community/platform/configuration-store-impl/testSrc/ModuleStoreRenameTest.kt
+++ intellij-community/platform/configuration-store-impl/testSrc/ModuleStoreRenameTest.kt
@@ -3,10 +3,13 @@ package com.intellij.configurationStore
 
 import com.intellij.ProjectTopics
 import com.intellij.ide.highlighter.ModuleFileType
+import com.intellij.idea.IdeaTestApplication
 import com.intellij.openapi.application.AppUIExecutor
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.impl.coroutineDispatchingContext
 import com.intellij.openapi.application.impl.inWriteAction
+import com.intellij.openapi.command.impl.UndoManagerImpl
+import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.components.StoragePathMacros
 import com.intellij.openapi.components.stateStore
 import com.intellij.openapi.module.Module
@@ -25,6 +28,7 @@ import com.intellij.util.io.readText
 import com.intellij.util.io.systemIndependentPath
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.junit.After
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
@@ -86,6 +90,14 @@ internal class ModuleStoreRenameTest {
     },
     DisposeModulesRule(projectRule)
   )
+
+  @After
+  fun tearDown() {
+    ApplicationManager.getApplication().invokeAndWait {
+      (UndoManager.getInstance(projectRule.project) as UndoManagerImpl).dropHistoryInTests()
+      (UndoManager.getInstance(projectRule.project) as UndoManagerImpl).flushCurrentCommandMerger()
+    }
+  }
 
   // project structure
   @Test
