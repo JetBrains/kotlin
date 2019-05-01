6dbbf0bb7250b1c8ca11a1a925eb3fe468f2e89a
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2019-05-01 20:54:45 +0200

    Move server process creation to JDK 6 suite
     It's allow to avoid problem with wrong inherited output
     (gradle instead of test process)

--- compiler/tests-different-jdk/tests/org/jetbrains/kotlin/codegen/jdk/CustomJvmTargetOnJvmBaseTest.kt
+++ compiler/tests-different-jdk/tests/org/jetbrains/kotlin/codegen/jdk/CustomJvmTargetOnJvmBaseTest.kt
@@ -6,10 +6,15 @@
 package org.jetbrains.kotlin.codegen.jdk
 
 import org.jetbrains.kotlin.codegen.*
-import org.jetbrains.kotlin.test.SuiteRunnerForCustomJdk
+import org.jetbrains.kotlin.codegen.CodegenTestCase.RUN_BOX_TEST_IN_SEPARATE_PROCESS_PORT
 import org.jetbrains.kotlin.test.RunOnlyJdk6Test
+import org.jetbrains.kotlin.test.SuiteRunnerForCustomJdk
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
+import java.io.File
+import kotlin.test.assertTrue
 
 /*
  * NB: ALL NECESSARY FLAGS ARE PASSED THROUGH Gradle
@@ -26,7 +31,52 @@ abstract class CustomJvmTargetOnJvmBaseTest
 
 @RunOnlyJdk6Test
 @RunWith(SuiteRunnerForCustomJdk::class)
-class JvmTarget6OnJvm6 : CustomJvmTargetOnJvmBaseTest()
+class JvmTarget6OnJvm6 : CustomJvmTargetOnJvmBaseTest() {
+
+    companion object {
+
+        private lateinit var jdkProcess: Process
+
+        @JvmStatic
+        @BeforeClass
+        fun setUp() {
+            println("Configuring JDK6 Test server...")
+            val jdkPath = System.getenv("JDK_16") ?: error("JDK_16 is not optional to run this test")
+
+            val executable = File(jdkPath, "bin/java").canonicalPath
+            val main = "org.jetbrains.kotlin.test.clientserver.TestProcessServer"
+            val classpath =
+                System.getProperty("kotlin.test.box.in.separate.process.server.classpath") ?: System.getProperty("java.class.path")
+
+            println("Server classpath: $classpath")
+            val port = System.getProperty(RUN_BOX_TEST_IN_SEPARATE_PROCESS_PORT)
+                ?: error("$RUN_BOX_TEST_IN_SEPARATE_PROCESS_PORT is not specified")
+            val builder = ProcessBuilder(executable, "-cp", classpath, main, port)
+
+            builder.inheritIO()
+
+            println("Starting JDK 6 server $executable...")
+            jdkProcess = builder.start()
+            Thread.sleep(2000)
+            assertTrue(jdkProcess.isAlive, "Test server process hasn't started")
+            println("Test server started!")
+            Runtime.getRuntime().addShutdownHook(object : Thread() {
+                override fun run() {
+                    tearDown()
+                }
+            })
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun tearDown() {
+            println("Stopping JDK 6 server...")
+            if (::jdkProcess.isInitialized) {
+                jdkProcess.destroy()
+            }
+        }
+    }
+}
 
 @RunWith(SuiteRunnerForCustomJdk::class)
 class JvmTarget8OnJvm8 : CustomJvmTargetOnJvmBaseTest()
