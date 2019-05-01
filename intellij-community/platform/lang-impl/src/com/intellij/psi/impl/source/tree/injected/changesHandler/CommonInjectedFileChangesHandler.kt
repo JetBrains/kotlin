7d0cf3d97ee74fd5827e314ad5ada82f1a39f4b0
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2019-05-01 13:29:20 +0300

    [fragment-editor] `CommonInjectedFileChangesHandler` converted to Kotlin: Step 2
    intellij-monorepo: 36c43cb4bfff0c3dc39c4e92fd52a08d5193a11a

--- intellij-community/platform/lang-impl/src/com/intellij/psi/impl/source/tree/injected/changesHandler/CommonInjectedFileChangesHandler.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/impl/source/tree/injected/changesHandler/CommonInjectedFileChangesHandler.kt
@@ -1,133 +1,102 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.psi.impl.source.tree.injected.changesHandler;
+package com.intellij.psi.impl.source.tree.injected.changesHandler
 
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.RangeMarker;
-import com.intellij.openapi.editor.event.DocumentEvent;
-import com.intellij.openapi.util.ProperTextRange;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.Trinity;
-import com.intellij.psi.*;
-import com.intellij.util.containers.ContainerUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.util.ProperTextRange
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.Trinity
+import com.intellij.psi.*
+import com.intellij.util.containers.ContainerUtil
+import java.util.*
+import kotlin.math.max
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-public class CommonInjectedFileChangesHandler extends AbstractMarkerBasedInjectedFileChangesHandler {
-  private final List<Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>>> myMarkers =
-    ContainerUtil.newLinkedList();
-
-  public CommonInjectedFileChangesHandler(List<? extends PsiLanguageInjectionHost.Shred> shreds,
-                                          Editor editor,
-                                          Document newDocument,
-                                          PsiFile injectedFile) {
-    super(editor, newDocument, injectedFile);
-
-    myMarkers.addAll(getMarkersFromShreds(shreds));
-  }
-
-  @NotNull
-  protected ArrayList<Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>>> getMarkersFromShreds(List<? extends PsiLanguageInjectionHost.Shred> shreds) {
-    ArrayList<Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>>> result =
-      new ArrayList<>(shreds.size());
-
-    SmartPointerManager smartPointerManager = SmartPointerManager.getInstance(myProject);
-    int curOffset = -1;
-    for (PsiLanguageInjectionHost.Shred shred : shreds) {
-      final RangeMarker rangeMarker = localRangeMarkerFromShred(shred);
-      final TextRange rangeInsideHost = shred.getRangeInsideHost();
-      PsiLanguageInjectionHost host = shred.getHost();
-      if (host == null) {
-        throw getReportException("host should not be null", null, null);
-      }
-      RangeMarker origMarker = myOrigDocument.createRangeMarker(rangeInsideHost.shiftRight(host.getTextRange().getStartOffset()));
-      SmartPsiElementPointer<PsiLanguageInjectionHost> elementPointer = smartPointerManager.createSmartPsiElementPointer(host);
-      Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>> markers =
-        Trinity.create(origMarker, rangeMarker, elementPointer);
-      result.add(markers);
-
-      origMarker.setGreedyToRight(true);
-      rangeMarker.setGreedyToRight(true);
-      if (origMarker.getStartOffset() > curOffset) {
-        origMarker.setGreedyToLeft(true);
-        rangeMarker.setGreedyToLeft(true);
-      }
-      curOffset = origMarker.getEndOffset();
+open class CommonInjectedFileChangesHandler(shreds: List<PsiLanguageInjectionHost.Shred>,
+                                            editor: Editor,
+                                            newDocument: Document,
+                                            injectedFile: PsiFile) : AbstractMarkerBasedInjectedFileChangesHandler(editor, newDocument,
+                                                                                                                   injectedFile) {
+  override val markers: MutableList<Marker> =
+    ContainerUtil.newLinkedList<Marker>().apply {
+      addAll(getMarkersFromShreds(shreds))
     }
-    return result;
-  }
 
 
-  @Override
-  public boolean isValid() {
-    boolean valid = myInjectedFile.isValid();
-    if (valid) {
-      for (Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>> t : myMarkers) {
-        if (!t.first.isValid() || !t.second.isValid() || t.third.getElement() == null) {
-          valid = false;
-          break;
-        }
+  protected fun getMarkersFromShreds(shreds: List<PsiLanguageInjectionHost.Shred>): List<Marker> {
+    val result = ArrayList<Marker>(shreds.size)
+
+    val smartPointerManager = SmartPointerManager.getInstance(myProject)
+    var curOffset = -1
+    for (shred in shreds) {
+      val rangeMarker = localRangeMarkerFromShred(shred)
+      val rangeInsideHost = shred.rangeInsideHost
+      val host = shred.host ?: failAndReport("host should not be null", null, null)
+      val origMarker = myOrigDocument.createRangeMarker(rangeInsideHost.shiftRight(host.textRange.startOffset))
+      val elementPointer = smartPointerManager.createSmartPsiElementPointer(host)
+      val markers = Trinity.create(origMarker, rangeMarker, elementPointer)
+      result.add(markers)
+
+      origMarker.isGreedyToRight = true
+      rangeMarker.isGreedyToRight = true
+      if (origMarker.startOffset > curOffset) {
+        origMarker.isGreedyToLeft = true
+        rangeMarker.isGreedyToLeft = true
       }
+      curOffset = origMarker.endOffset
     }
-    return valid;
+    return result
   }
 
-  @Override
-  public void commitToOriginal(@NotNull DocumentEvent e) {
-    final String text = myNewDocument.getText();
-    final Map<PsiLanguageInjectionHost, Set<Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>>>> map =
-      ContainerUtil.classify(myMarkers.iterator(), o -> o.third.getElement());
-    PsiDocumentManager documentManager = PsiDocumentManager.getInstance(myProject);
-    documentManager.commitDocument(myOrigDocument); // commit here and after each manipulator update
-    int localInsideFileCursor = 0;
-    for (PsiLanguageInjectionHost host : map.keySet()) {
-      if (host == null) continue;
-      String hostText = host.getText();
-      ProperTextRange insideHost = null;
-      StringBuilder sb = new StringBuilder();
-      for (Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>> entry : map.get(host)) {
-        RangeMarker origMarker = entry.first; // check for validity?
-        int hostOffset = host.getTextRange().getStartOffset();
-        ProperTextRange localInsideHost =
-          new ProperTextRange(origMarker.getStartOffset() - hostOffset, origMarker.getEndOffset() - hostOffset);
-        RangeMarker rangeMarker = entry.second;
-        ProperTextRange localInsideFile =
-          new ProperTextRange(Math.max(localInsideFileCursor, rangeMarker.getStartOffset()), rangeMarker.getEndOffset());
+
+  override fun isValid(): Boolean =
+    myInjectedFile.isValid && markers.all { it.first.isValid && it.second.isValid && it.third.element != null }
+
+  override fun commitToOriginal(e: DocumentEvent) {
+    val text = myNewDocument.text
+    val map = markers.groupByTo(LinkedHashMap()) { it.host }
+
+    val documentManager = PsiDocumentManager.getInstance(myProject)
+    documentManager.commitDocument(myOrigDocument) // commit here and after each manipulator update
+    var localInsideFileCursor = 0
+    for (host in map.keys) {
+      if (host == null) continue
+      val hostText = host.text
+      var insideHost: ProperTextRange? = null
+      val sb = StringBuilder()
+      for (entry in map[host].orEmpty()) {
+        val origMarker = entry.origin // check for validity?
+        val hostOffset = host.textRange.startOffset
+        val localInsideHost = ProperTextRange(origMarker.startOffset - hostOffset, origMarker.endOffset - hostOffset)
+        val rangeMarker = entry.second
+        val localInsideFile = ProperTextRange(max(localInsideFileCursor, rangeMarker.startOffset), rangeMarker.endOffset)
         if (insideHost != null) {
           //append unchanged inter-markers fragment
-          sb.append(hostText, insideHost.getEndOffset(), localInsideHost.getStartOffset());
+          sb.append(hostText, insideHost.endOffset, localInsideHost.startOffset)
         }
-        sb.append(localInsideFile.getEndOffset() <= text.length() && !localInsideFile.isEmpty() ? localInsideFile.substring(text) : "");
-        localInsideFileCursor = localInsideFile.getEndOffset();
-        insideHost = insideHost == null ? localInsideHost : insideHost.union(localInsideHost);
+
+        if (localInsideFile.endOffset <= text.length && !localInsideFile.isEmpty) {
+          sb.append(localInsideFile.substring(text))
+        }
+        localInsideFileCursor = localInsideFile.endOffset
+        insideHost = insideHost?.union(localInsideHost) ?: localInsideHost
       }
-      assert insideHost != null;
-      updateInjectionHostElement(host, insideHost, sb.toString());
-      documentManager.commitDocument(myOrigDocument);
+      if (insideHost == null) failAndReport("insideHost is null", e)
+      updateInjectionHostElement(host, insideHost, sb.toString())
+      documentManager.commitDocument(myOrigDocument)
     }
   }
 
-  protected void updateInjectionHostElement(PsiLanguageInjectionHost host, ProperTextRange insideHost, String content) {
-    ElementManipulators.getManipulator(host).handleContentChange(host, insideHost, content);
+  protected fun updateInjectionHostElement(host: PsiLanguageInjectionHost, insideHost: ProperTextRange, content: String) {
+    ElementManipulators.getManipulator(host).handleContentChange(host, insideHost, content)
   }
 
-  @Override
-  public boolean handlesRange(@NotNull TextRange range) {
-    if (!myMarkers.isEmpty()) {
-      TextRange hostRange = TextRange.create(myMarkers.get(0).first.getStartOffset(),
-                                             myMarkers.get(myMarkers.size() - 1).first.getEndOffset());
-      return range.intersects(hostRange);
+  override fun handlesRange(range: TextRange): Boolean {
+    if (markers.isNotEmpty()) {
+      val hostRange = TextRange.create(markers[0].first.startOffset,
+                                       markers[markers.size - 1].first.endOffset)
+      return range.intersects(hostRange)
     }
-    return false;
-  }
-
-  @NotNull
-  @Override
-  protected List<Trinity<RangeMarker, RangeMarker, SmartPsiElementPointer<PsiLanguageInjectionHost>>> getMarkers() {
-    return myMarkers;
+    return false
   }
 }
