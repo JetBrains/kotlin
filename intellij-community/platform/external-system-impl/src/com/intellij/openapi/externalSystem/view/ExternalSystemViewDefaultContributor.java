68370129eba34bd5e01a743894bca9c309f892bd
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-05-03 09:37:37 +0200

    deprecate ContainerUtil.newHashMap(initialCapacity)
    intellij-monorepo: 8c5c54c28c5b3c7dbf6b71f5c94eda95b6fefc68

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/view/ExternalSystemViewDefaultContributor.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/view/ExternalSystemViewDefaultContributor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.view;
 
 import com.intellij.icons.AllIcons;
@@ -157,7 +143,7 @@ public class ExternalSystemViewDefaultContributor extends ExternalSystemViewCont
       final AbstractExternalSystemSettings systemSettings =
         ExternalSystemApiUtil.getSettings(externalProjectsView.getProject(), externalProjectsView.getSystemId());
 
-      final Map<String, ModuleNode> groupToModule = ContainerUtil.newHashMap(moduleDataNodes.size());
+      final Map<String, ModuleNode> groupToModule = new HashMap<String, ModuleNode>(moduleDataNodes.size());
 
       List<ModuleNode> moduleNodes = ContainerUtil.newArrayList();
 
