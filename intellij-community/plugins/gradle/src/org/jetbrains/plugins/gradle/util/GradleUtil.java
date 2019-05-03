4b69e683df2735897857717bc7564e0392ef20e2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-05-03 10:27:50 +0200

    deprecate ContainerUtil.newStack()
    intellij-monorepo: f2063d0c1e3ad3b11b925cb5fd18882e48876b2b

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleUtil.java
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleUtil.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.util;
 
 import com.intellij.ide.util.PropertiesComponent;
@@ -25,7 +11,6 @@ import com.intellij.openapi.fileChooser.FileTypeDescriptor;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileFilters;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.util.containers.ContainerUtilRt;
 import com.intellij.util.containers.Stack;
 import org.gradle.tooling.model.GradleProject;
 import org.gradle.tooling.model.gradle.GradleScript;
@@ -182,7 +167,7 @@ public class GradleUtil {
     }
     File rootProjectParent = new File(rootProjectPath);
     StringBuilder buffer = new StringBuilder(FileUtil.toCanonicalPath(rootProjectParent.getAbsolutePath()));
-    Stack<String> stack = ContainerUtilRt.newStack();
+    Stack<String> stack = new Stack<String>();
     for (GradleProject p = subProject; p != null; p = p.getParent()) {
       stack.push(p.getName());
     }
