066e016dba0d9cb046fbffa84a877969a590ffb9
Max Medvedev <maxim.medvedev@jetbrains.com>
2019-05-03 20:26:28 +0100

    KT-28858 per-file symbol tables: collect kotlin sources from external system api

--- appcode-native/src/org/jetbrains/konan/resolve/KotlinFrameworkTranslator.kt
+++ appcode-native/src/org/jetbrains/konan/resolve/KotlinFrameworkTranslator.kt
@@ -1,6 +1,11 @@
 package org.jetbrains.konan.resolve
 
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.ProjectKeys
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileVisitor
@@ -9,17 +14,13 @@ import com.jetbrains.cidr.lang.symbols.OCSymbol
 import com.jetbrains.cidr.lang.symbols.cpp.OCIncludeSymbol
 import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.plugins.gradle.util.GradleConstants
 
 class KotlinFrameworkTranslator(val project: Project) {
     fun translateModule(konanFile: KonanBridgeVirtualFile): Sequence<OCSymbol> {
-        val sourceRoot = findSourceRoot(konanFile) ?: return emptySequence()
-
-        val sources = findAllSources(sourceRoot)
-        if (sources.isEmpty()) return emptySequence()
-
-        val psiManager = PsiManager.getInstance(project)
-
-        val ktFile = sources.asSequence().mapToKtFiles(psiManager).firstOrNull() ?: return emptySequence()
+        val sources = collectSources(konanFile)
+        val ktFile = sources.firstOrNull()?.let { PsiManager.getInstance(project).findFile(it) } as? KtFile
+                     ?: return emptySequence()
 
         val baseDeclarations = KotlinFileTranslator(project).translateBase(ktFile)
         val includes = sources.asSequence().map { include(konanFile, it) }
@@ -27,15 +28,20 @@ class KotlinFrameworkTranslator(val project: Project) {
         return baseDeclarations + includes
     }
 
-    private fun findSourceRoot(virtualFile: KonanBridgeVirtualFile): VirtualFile? {
-        /*todo[medvedev] implement proper module stuff here
-        ExternalSystemApiUtil.findAll(ProjectDataManager.getInstance().getExternalProjectsData(virtualFile.project, GradleConstants.SYSTEM_ID).first().externalProjectStructure as DataNode<*>, ProjectKeys.MODULE)
-        ask Vlad Soroka what should I get next*/
-        return virtualFile.target.file.baseDir
-                .findChild("KotlinNativeFramework")
-                ?.findChild("src")
-                ?.findChild("KotlinNativeFrameworkMain")
-                ?.findChild("kotlin")
+    private fun collectSources(konanFile: KonanBridgeVirtualFile): List<VirtualFile> {
+        val projectNode = ProjectDataManager.getInstance().getExternalProjectsData(project, GradleConstants.SYSTEM_ID).first().externalProjectStructure as DataNode<*>
+        val moduleNodes = ExternalSystemApiUtil.findAll(projectNode, ProjectKeys.MODULE)
+        val taskName = ":" + konanFile.target.name
+        val moduleNode = moduleNodes.find { module -> module.data.id == taskName } ?: return emptyList()
+        val contentRoots = ExternalSystemApiUtil.findAll(moduleNode, ProjectKeys.CONTENT_ROOT)
+
+        val vfs = LocalFileSystem.getInstance()
+
+        return contentRoots.asSequence()
+            .mapNotNull { node -> vfs.findFileByPath(node.data.rootPath) }
+            //todo process only `./***Main/kotlin/`
+            .flatMap { dir -> findAllSources(dir).asSequence() }
+            .toList()
     }
 
     private fun findAllSources(sourceRoot: VirtualFile): List<VirtualFile> {
@@ -55,9 +61,5 @@ class KotlinFrameworkTranslator(val project: Project) {
 
     private fun include(konanFile: KonanBridgeVirtualFile, target: VirtualFile): OCIncludeSymbol {
         return OCIncludeSymbol(konanFile, 0, target, OCIncludeSymbol.IncludePath.EMPTY, true, false, 0, null, true)
-    }
-
-    private fun Sequence<VirtualFile>.mapToKtFiles(psiManager: PsiManager): Sequence<KtFile> {
-        return this.mapNotNull { file -> psiManager.findFile(file) as? KtFile }
     }
 }
