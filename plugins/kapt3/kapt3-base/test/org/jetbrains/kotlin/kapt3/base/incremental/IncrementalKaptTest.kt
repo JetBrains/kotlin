7a17bccf23e9012c49c94a8f2c45bdb8fe71ff92
Ivan Gavrilovic <gavra@google.com>
2019-05-06 14:37:40 -0700

    Remove generated sources and classes for full KAPT build
    When running incremental annotation processing in KAPT,
    even if incremental flag is enabled, and full rebuild should
    be performed (e.g. in presence of dynamic non-incremental APs),
    make sure generated sources and classes are removed.
    
     #KT-31322 fixed
    
    (cherry picked from commit f60bfc34c0fd414a7eaaade444c54cb1cd5a18c9)

--- plugins/kapt3/kapt3-base/test/org/jetbrains/kotlin/kapt3/base/incremental/IncrementalKaptTest.kt
+++ plugins/kapt3/kapt3-base/test/org/jetbrains/kotlin/kapt3/base/incremental/IncrementalKaptTest.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.base.kapt3.KaptOptions
 import org.jetbrains.kotlin.base.kapt3.collectJavaSourceFiles
 import org.jetbrains.kotlin.kapt3.base.KaptContext
 import org.jetbrains.kotlin.kapt3.base.doAnnotationProcessing
+import org.jetbrains.kotlin.kapt3.base.incremental.RuntimeProcType
 import org.jetbrains.kotlin.kapt3.base.incremental.SourcesToReprocess
 import org.jetbrains.kotlin.kapt3.base.util.WriterBackedKaptLogger
 import org.junit.Assert.*
@@ -155,6 +156,69 @@ class IncrementalKaptTest {
 
             it.doAnnotationProcessing(
                 optionsForSecondRun.collectJavaSourceFiles(sourcesToReprocess), listOf(SimpleGeneratingIfTypeDoesNotExist().toIsolating())
+            )
+        }
+
+        assertTrue(outputDir.resolve("test/UserGenerated.java").exists())
+    }
+
+    /** Regression test for KT-31322. */
+    @Test
+    fun testCleanupWithDynamicNonIncremental() {
+        val sourcesDir = tmp.newFolder().resolve("test").also { base ->
+            base.mkdir()
+            listOf("User.java", "Address.java", "Observable.java").map {
+                TEST_DATA_DIR.resolve(it).copyTo(base.resolve(it))
+            }
+        }
+
+        val outputDir = tmp.newFolder()
+        val incrementalCacheDir = tmp.newFolder()
+        val options = KaptOptions.Builder().apply {
+            projectBaseDir = tmp.newFolder()
+            javaSourceRoots.add(sourcesDir)
+
+            sourcesOutputDir = outputDir
+            classesOutputDir = outputDir
+            stubsOutputDir = outputDir
+            incrementalDataOutputDir = outputDir
+
+            incrementalCache = incrementalCacheDir
+        }.build()
+
+        val logger = WriterBackedKaptLogger(isVerbose = true)
+        KaptContext(options, true, logger).use {
+            it.doAnnotationProcessing(
+                options.collectJavaSourceFiles(SourcesToReprocess.FullRebuild),
+                listOf(DynamicProcessor(RuntimeProcType.NON_INCREMENTAL).toDynamic())
+            )
+        }
+
+        val optionsForSecondRun = KaptOptions.Builder().apply {
+            projectBaseDir = tmp.newFolder()
+            javaSourceRoots.add(sourcesDir)
+
+            sourcesOutputDir = outputDir
+            classesOutputDir = outputDir
+            stubsOutputDir = outputDir
+            incrementalDataOutputDir = outputDir
+
+            incrementalCache = incrementalCacheDir
+            changedFiles.add(sourcesDir.resolve("User.java"))
+            flags.add(KaptFlag.INCREMENTAL_APT)
+        }.build()
+
+        KaptContext(optionsForSecondRun, true, logger).use {
+            val sourcesToReprocess =
+                it.cacheManager!!.invalidateAndGetDirtyFiles(optionsForSecondRun.changedFiles, emptyList())
+            assertEquals(SourcesToReprocess.FullRebuild, sourcesToReprocess)
+
+            // check output dir is empty
+            assertEquals(listOf(outputDir), outputDir.walkTopDown().toList())
+
+            it.doAnnotationProcessing(
+                optionsForSecondRun.collectJavaSourceFiles(sourcesToReprocess),
+                listOf(DynamicProcessor(RuntimeProcType.NON_INCREMENTAL).toDynamic())
             )
         }
 
