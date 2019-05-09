9ea632b385fcb6fd33ca4c054222edd842cad92c
Dmitry Jemerov <yole@jetbrains.com>
2019-05-09 14:34:02 +0200

    Show descriptive name for Inline action
    intellij-monorepo: 891a970f2917c91fdfdfd1a0cb85cf1deda801f0

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/actions/BaseRefactoringAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/actions/BaseRefactoringAction.java
@@ -167,23 +167,8 @@ public abstract class BaseRefactoringAction extends AnAction implements UpdateIn
       }
     }
     else {
-      PsiElement element = e.getData(CommonDataKeys.PSI_ELEMENT);
-      Language[] languages = e.getData(LangDataKeys.CONTEXT_LANGUAGES);
-      if (element == null || !isAvailableForLanguage(element.getLanguage())) {
-        if (file == null) {
-          hideAction(e);
-          return;
-        }
-        element = getElementAtCaret(editor, file);
-      }
-
-      if (element == null || element instanceof SyntheticElement || languages == null) {
-        hideAction(e);
-        return;
-      }
-
-      boolean isVisible = ContainerUtil.find(languages, myLanguageCondition) != null;
-      if (isVisible) {
+      PsiElement element = findRefactoringTargetInEditor(dataContext);
+      if (element != null) {
         boolean isEnabled = file != null && isAvailableOnElementInEditorAndFile(element, editor, file, dataContext, e.getPlace());
         if (!isEnabled) {
           disableAction(e);
@@ -196,6 +181,28 @@ public abstract class BaseRefactoringAction extends AnAction implements UpdateIn
         hideAction(e);
       }
     }
+  }
+
+  protected PsiElement findRefactoringTargetInEditor(DataContext dataContext) {
+    Editor editor = dataContext.getData(CommonDataKeys.EDITOR);
+    PsiFile file = dataContext.getData(CommonDataKeys.PSI_FILE);
+    PsiElement element = dataContext.getData(CommonDataKeys.PSI_ELEMENT);
+    Language[] languages = dataContext.getData(LangDataKeys.CONTEXT_LANGUAGES);
+    if (element == null || !isAvailableForLanguage(element.getLanguage())) {
+      if (file == null || editor == null) {
+        return null;
+      }
+      element = getElementAtCaret(editor, file);
+    }
+
+    if (element == null || element instanceof SyntheticElement || languages == null) {
+      return null;
+    }
+
+    if (ContainerUtil.find(languages, myLanguageCondition) == null) {
+      return null;
+    }
+    return element;
   }
 
   private void updateActionText(AnActionEvent e) {
@@ -223,7 +230,7 @@ public abstract class BaseRefactoringAction extends AnAction implements UpdateIn
     return false;
   }
 
-  public static PsiElement getElementAtCaret(final Editor editor, final PsiFile file) {
+  public static PsiElement getElementAtCaret(@NotNull final Editor editor, final PsiFile file) {
     final int offset = fixCaretOffset(editor);
     PsiElement element = file.findElementAt(offset);
     if (element == null && offset == file.getTextLength()) {
@@ -236,7 +243,7 @@ public abstract class BaseRefactoringAction extends AnAction implements UpdateIn
     return element;
   }
 
-  private static int fixCaretOffset(final Editor editor) {
+  private static int fixCaretOffset(@NotNull final Editor editor) {
     final int caret = editor.getCaretModel().getOffset();
     if (editor.getSelectionModel().hasSelection()) {
       if (caret == editor.getSelectionModel().getSelectionEnd()) {
--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/actions/InlineAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/actions/InlineAction.java
@@ -7,6 +7,7 @@ import com.intellij.lang.refactoring.InlineActionHandler;
 import com.intellij.lang.refactoring.InlineHandler;
 import com.intellij.lang.refactoring.InlineHandlers;
 import com.intellij.lang.refactoring.RefactoringSupportProvider;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiElement;
@@ -72,5 +73,21 @@ public class InlineAction extends BasePlatformRefactoringAction {
       }
     }
     return InlineHandlers.getInlineHandlers(language).size() > 0;
+  }
+
+  @Nullable
+  @Override
+  protected String getActionName(@NotNull DataContext dataContext) {
+    Editor editor = dataContext.getData(CommonDataKeys.EDITOR);
+    PsiElement element = findRefactoringTargetInEditor(dataContext);
+    if (element != null && editor != null) {
+      for (InlineActionHandler handler: InlineActionHandler.EP_NAME.getExtensionList()) {
+        if (handler.isEnabledOnElement(element, editor)) {
+          return handler.getActionName(element);
+        }
+      }
+    }
+
+    return super.getActionName(dataContext);
   }
 }
