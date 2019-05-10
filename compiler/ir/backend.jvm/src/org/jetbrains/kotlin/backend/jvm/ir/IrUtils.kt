c59c24b96f6719c154372cb728e571812966443b
Steven Schäfer <stevenschaefer@google.com>
2019-05-10 10:41:31 +0200

    Fix IR types in JVM_IR BridgeLowering
    Properly implement the java erasure rules on IrTypes and fix the types
    for dispatch and extension receivers.

---
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/ir/IrUtils.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.jvm.ir
+
+import org.jetbrains.kotlin.backend.jvm.codegen.isJvmInterface
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrTypeParameter
+import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
+import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
+import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.classOrNull
+import org.jetbrains.kotlin.ir.types.classifierOrNull
+
+/**
+ * Computes the erased class for this type parameter according to the java erasure rules.
+ */
+val IrTypeParameter.erasedUpperBound: IrClass
+    get() {
+        // Pick the (necessarily unique) non-interface upper bound if it exists
+        for (type in superTypes) {
+            val irClass = type.classOrNull?.owner ?: continue
+            if (!irClass.isJvmInterface) return irClass
+        }
+
+        // Otherwise, choose either the first IrClass supertype or recurse.
+        // In the first case, all supertypes are interface types and the choice was arbitrary.
+        // In the second case, there is only a single supertype.
+        return superTypes.first().erasedUpperBound
+    }
+
+val IrType.erasedUpperBound: IrClass
+    get() = when (val classifier = classifierOrNull) {
+        is IrClassSymbol -> classifier.owner
+        is IrTypeParameterSymbol -> classifier.owner.erasedUpperBound
+        else -> throw IllegalStateException()
+    }
