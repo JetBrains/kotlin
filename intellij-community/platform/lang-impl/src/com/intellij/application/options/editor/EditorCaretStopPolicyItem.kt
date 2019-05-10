fed5e62829010b1304c5a8b17fe03327b7930bbe
Eldar Abusalimov <eldar.abusalimov@jetbrains.com>
2019-05-10 05:15:29 +0300

    IDEABKL-5365: EditorOptionsPanel: Show "IDE default" caret option hint
    intellij-monorepo: 744d59486a2129a813af3288c0c050490366129a

--- intellij-community/platform/lang-impl/src/com/intellij/application/options/editor/EditorCaretStopPolicyItem.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/application/options/editor/EditorCaretStopPolicyItem.kt
@@ -2,6 +2,7 @@
 package com.intellij.application.options.editor
 
 import com.intellij.openapi.application.ApplicationBundle
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.openapi.editor.actions.CaretStopBoundary
 import com.intellij.openapi.editor.actions.CaretStopOptions
 import com.intellij.openapi.editor.actions.CaretStopOptionsTransposed
@@ -22,7 +23,7 @@ internal interface EditorCaretStopPolicyItem {
     internal inline fun <reified E> findMatchingItem(caretStopBoundary: CaretStopBoundary): E
       where E : Enum<E>,
             E : EditorCaretStopPolicyItem = enumValues<E>().find { it.caretStopBoundary == caretStopBoundary }
-                                            ?: checkNotNull(enumValues<E>().find { it.osDefault.isCurrentOsDefault })
+                                            ?: checkNotNull(enumValues<E>().find { it.osDefault.isIdeDefault })
     internal fun String.appendHint(hint: String): String =
       if (hint.isBlank()) this else "$this ($hint)"
   }
@@ -66,24 +67,28 @@ internal interface EditorCaretStopPolicyItem {
     }
   }
 
-  enum class OsDefault(val isCurrentOsDefault: Boolean = false,
-                       open val hint: String = "") {
-    UNIX(SystemInfo.isUnix,
-         hint = when {
+  enum class OsDefault(open val hint: String = "") {
+    UNIX(hint = when {
            SystemInfo.isLinux -> ApplicationBundle.message("combobox.item.hint.os.default.linux")
            SystemInfo.isMac -> ApplicationBundle.message("combobox.item.hint.os.default.mac")
            else -> ApplicationBundle.message("combobox.item.hint.os.default.unix")
          }),
-    WINDOWS(SystemInfo.isWindows,
-            hint = ApplicationBundle.message("combobox.item.hint.os.default.windows")),
+    WINDOWS(hint = ApplicationBundle.message("combobox.item.hint.os.default.windows")),
+    IDE(hint = run {
+      // Don't let long product names like "Android Studio" stretch the combobox presentation.
+      val shortProductName = ApplicationNamesInfo.getInstance().fullProductName.takeIf { it.length <= 8 } ?: "IDE"
+      ApplicationBundle.message("combobox.item.hint.ide.default", shortProductName)
+    }),
     NONE;
+
+    val isIdeDefault: Boolean get() = this == IDE
   }
 
   enum class WordBoundary(override val title: String,
                           override val caretStopBoundary: CaretStopBoundary,
                           override val osDefault: OsDefault = OsDefault.NONE) : EditorCaretStopPolicyItem {
     STICK_TO_WORD_BOUNDARIES(ApplicationBundle.message("combobox.item.stick.to.word.boundaries"), CaretStopBoundary.CURRENT,
-                             OsDefault.UNIX),
+                             OsDefault.IDE),
     JUMP_TO_WORD_START(ApplicationBundle.message("combobox.item.jump.to.word.start"), CaretStopBoundary.START,
                        OsDefault.WINDOWS),
     JUMP_TO_WORD_END(ApplicationBundle.message("combobox.item.jump.to.word.end"), CaretStopBoundary.END),
@@ -101,12 +106,13 @@ internal interface EditorCaretStopPolicyItem {
   enum class LineBoundary(override val title: String,
                           override val caretStopBoundary: CaretStopBoundary,
                           override val osDefault: OsDefault = OsDefault.NONE) : EditorCaretStopPolicyItem {
+    JUMP_TO_NEIGHBORING_LINE(ApplicationBundle.message("combobox.item.jump.to.neighboring.line"), CaretStopBoundary.NEIGHBOR,
+                             OsDefault.IDE),
     SKIP_LINE_BREAK(ApplicationBundle.message("combobox.item.proceed.to.word.boundary"), CaretStopBoundary.NONE,
                     OsDefault.UNIX),
     STOP_AT_BOTH_LINE_BOUNDARIES(ApplicationBundle.message("combobox.item.stop.at.both.line.ends"), CaretStopBoundary.BOTH,
                                  OsDefault.WINDOWS),
     STAY_ON_CURRENT_LINE(ApplicationBundle.message("combobox.item.stay.on.current.line"), CaretStopBoundary.CURRENT),
-    JUMP_TO_NEIGHBORING_LINE(ApplicationBundle.message("combobox.item.jump.to.neighboring.line"), CaretStopBoundary.NEIGHBOR),
     JUMP_TO_LINE_START(ApplicationBundle.message("combobox.item.stop.at.line.start"), CaretStopBoundary.START),
     JUMP_TO_LINE_END(ApplicationBundle.message("combobox.item.stop.at.line.end"), CaretStopBoundary.END);
 
