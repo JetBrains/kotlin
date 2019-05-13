15f832f8feb8b778a2efd5e2f11e9f27548b3830
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-05-13 15:16:30 +0300

    Gradle: daemon startup issue checker IDEA-209457
    intellij-monorepo: 255c5288b08ada55d65742a2644b8fa94bf82a77

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/issue/quickfix/GradleWrapperSettingsOpenQuickFix.kt
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/issue/quickfix/GradleWrapperSettingsOpenQuickFix.kt
@@ -1,23 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.issue.quickfix
 
-import com.intellij.codeInsight.highlighting.HighlightManager
-import com.intellij.codeInsight.highlighting.HighlightUsagesHandler
-import com.intellij.find.FindManager
-import com.intellij.find.FindModel
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.editor.colors.EditorColors
-import com.intellij.openapi.editor.colors.EditorColorsManager
-import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.fileEditor.OpenFileDescriptor
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtil
-import org.jetbrains.annotations.ApiStatus
 import com.intellij.build.issue.BuildIssueQuickFix
+import com.intellij.build.issue.quickfix.OpenFileQuickFix
+import com.intellij.openapi.project.Project
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.gradle.util.GradleUtil
-
 import java.util.concurrent.CompletableFuture
-
 import java.util.concurrent.CompletableFuture.completedFuture
 
 /**
@@ -36,17 +25,7 @@ class GradleWrapperSettingsOpenQuickFix(private val myProjectPath: String, priva
   companion object {
     fun showWrapperPropertiesFile(project: Project, projectPath: String, search: String?) {
       val wrapperPropertiesFile = GradleUtil.findDefaultWrapperPropertiesFile(projectPath) ?: return
-      ApplicationManager.getApplication().invokeLater {
-        val file = VfsUtil.findFileByIoFile(wrapperPropertiesFile, false) ?: return@invokeLater
-        val editor = FileEditorManager.getInstance(project).openTextEditor(OpenFileDescriptor(project, file), false)
-        if (search == null || editor == null) return@invokeLater
-
-        val attributes = EditorColorsManager.getInstance().globalScheme.getAttributes(EditorColors.SEARCH_RESULT_ATTRIBUTES)
-        val findModel = FindModel().apply { FindModel.initStringToFind(this, search) }
-        val findResult = FindManager.getInstance(project).findString(editor.document.charsSequence, 0, findModel, file)
-        val highlightManager = HighlightManager.getInstance(project)
-        HighlightUsagesHandler.highlightRanges(highlightManager, editor, attributes, false, listOf(findResult))
-      }
+      OpenFileQuickFix.showFile(project, wrapperPropertiesFile.toPath(), search)
     }
   }
 }
