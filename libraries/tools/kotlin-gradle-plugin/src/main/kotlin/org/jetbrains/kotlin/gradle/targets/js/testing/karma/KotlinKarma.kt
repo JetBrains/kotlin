e1e56a8a5208a6d83fac2d7ac88c4355bbd9b272
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2019-05-13 08:49:28 +0300

    Gradle, JS: extract npm package constants 	#KT-30530

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarma.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarma.kt
@@ -7,16 +7,14 @@ package org.jetbrains.kotlin.gradle.targets.js.testing.karma
 
 import com.google.gson.GsonBuilder
 import org.gradle.api.Project
-import org.gradle.api.tasks.Input
 import org.gradle.process.ProcessForkOptions
 import org.jetbrains.kotlin.gradle.internal.testing.TCServiceMessagesClientSettings
 import org.jetbrains.kotlin.gradle.internal.testing.TCServiceMessagesTestExecutionSpec
 import org.jetbrains.kotlin.gradle.plugin.HasKotlinDependencies
-import org.jetbrains.kotlin.gradle.targets.js.NpmPackageVersion
 import org.jetbrains.kotlin.gradle.targets.js.internal.parseNodeJsStackTraceAsJvm
-import org.jetbrains.kotlin.gradle.targets.js.karmaChromeLauncher
-import org.jetbrains.kotlin.gradle.targets.js.karmaPhantomjsLauncher
-import org.jetbrains.kotlin.gradle.targets.js.npm.NpmProject
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.nodeJs
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmPackageVersion
+import org.jetbrains.kotlin.gradle.targets.js.npm.npmProject
 import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest
 import org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTestFramework
 
@@ -24,15 +22,32 @@ class KotlinKarma(val project: Project) : KotlinJsTestFramework {
     private val config: KarmaConfig = KarmaConfig()
     private val requiredDependencies = mutableSetOf<NpmPackageVersion>()
 
-    fun useChrome() = useBrowser("Chrome", karmaChromeLauncher)
+    private val versions = project.nodeJs.versions
 
-    fun useChromeCanary() = useBrowser("ChromeCanary", karmaChromeLauncher)
+    init {
+        requiredDependencies.add(versions.karma)
 
-    fun useChromeHeadless() = useBrowser("ChromeHeadless", karmaChromeLauncher)
+        requiredDependencies.add(versions.karmaTeamcityReporter)
+        config.reporters.add("teamcity")
 
-    fun usePhantomJS() = useBrowser("PhantomJS", karmaPhantomjsLauncher)
+        config.singleRun = true
+    }
 
-    fun useFirefox() = useBrowser("Firefox", TODO())
+    fun useChrome() = useBrowser("Chrome", versions.karmaChromeLauncher)
+
+    fun useChromeCanary() = useBrowser("ChromeCanary", versions.karmaChromeLauncher)
+
+    fun useChromeHeadless() = useBrowser("ChromeHeadless", versions.karmaChromeLauncher)
+
+    fun usePhantomJS() = useBrowser("PhantomJS", versions.karmaPhantomJsLauncher)
+
+    fun useFirefox() = useBrowser("Firefox", versions.karmaFirefoxLauncher)
+
+    fun useOpera() = useBrowser("Opera", versions.karmaOperaLauncher)
+
+    fun useSafari() = useBrowser("Safari", versions.karmaSafariLauncher)
+
+    fun useIe() = useBrowser("Ie", versions.karmaIeLauncher)
 
     fun useBrowser(id: String, dependency: NpmPackageVersion) {
         config.browsers.add(id)
@@ -40,24 +55,24 @@ class KotlinKarma(val project: Project) : KotlinJsTestFramework {
     }
 
     fun useMocha() {
-
+        requiredDependencies.add(versions.karmaMocha)
+        requiredDependencies.add(versions.mocha)
     }
 
     fun useWebpack() {
+        requiredDependencies.add(versions.karmaWebpack)
+    }
 
+    fun useSourceMapSupport() {
+        config.frameworks.add("source-map-support")
+        requiredDependencies.add(versions.karmaSourceMapSupport)
     }
 
     override fun configure(dependenciesHolder: HasKotlinDependencies) {
         dependenciesHolder.dependencies {
-            runtimeOnly(npm("karma", "4.0.1"))
-            runtimeOnly(npm("karma-chrome-launcher", "2.2.0"))
-            runtimeOnly(npm("puppeteer", "1.14.0"))
-            runtimeOnly(npm("karma-source-map-support", "1.4.0"))
-            runtimeOnly(npm("karma-browserify", "6.0.0"))
-            runtimeOnly(npm("browserify", "16.2.3"))
-            runtimeOnly(npm("karma-mocha", "1.3.0"))
-            runtimeOnly(npm("mocha", "6.1.2"))
-            runtimeOnly(npm("karma-teamcity-reporter", "1.1.0"))
+            requiredDependencies.forEach {
+                npm(it.name, it.version)
+            }
         }
     }
 
@@ -74,48 +89,24 @@ class KotlinKarma(val project: Project) : KotlinJsTestFramework {
             ignoreOutOfRootNodes = true
         )
 
-        val npmProjectLayout = NpmProject[task.project]
+        val npmProject = task.project.npmProject
 
-        val files = task.nodeModulesToLoad.map {
-            npmProjectLayout.nodeModulesDir.resolve(it).also { file ->
-                check(file.isFile) { "Cannot find ${file.canonicalPath}" }
-            }.canonicalPath
-        }.let {
-            GsonBuilder().create().toJson(it)
-        }
+        config.files.addAll(task.nodeModulesToLoad.map {
+            npmProject.getModuleEntryPath(it)
+        })
 
-        val karmaConfJs = npmProjectLayout.nodeWorkDir.resolve("karma.conf.js")
+        config.basePath = npmProject.nodeModulesDir.absolutePath
+
+        val karmaConfJs = npmProject.nodeWorkDir.resolve("karma.conf.js")
         karmaConfJs.printWriter().use {
-            //language=JavaScript
-            it.println(
-                """
-                process.env.CHROME_BIN = require('puppeteer').executablePath();
-                
-                module.exports = (config) => {
-                  config.set({
-                    basePath: 'node_modules',
-                    files: $files,
-                    frameworks: ['browserify', 'source-map-support', 'mocha'],
-                    browsers: ['ChromeHeadless'],
-                    reporters: ['teamcity'],
-                    singleRun: true,
-                    preprocessors: {
-                      '*.js': [ 'browserify' ]
-                    },
-                    browserify: {
-                      debug: true
-                    }
-                  });
-                }
-            """.trimIndent()
-            )
+            GsonBuilder().setPrettyPrinting().create().toJson(config, it)
         }
 
         val nodeModules = listOf("karma/bin/karma")
 
         val args = nodeJsArgs +
                 nodeModules.map {
-                    npmProjectLayout.nodeModulesDir.resolve(it).also { file ->
+                    npmProject.nodeModulesDir.resolve(it).also { file ->
                         check(file.isFile) { "Cannot find ${file.canonicalPath}" }
                     }.canonicalPath
                 } +
