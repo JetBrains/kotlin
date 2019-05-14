77301174ee7e31983956d82653990c09055443ad
Yann Cébron <yann.cebron@jetbrains.com>
2019-05-14 18:11:04 +0200

    GotoDeclarationHandler.getActionText: add @Nls capitalization
    intellij-monorepo: 862e22e37bc6683833d07ee4b56f541ccab5de21

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationHandler.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationHandler.java
@@ -1,10 +1,11 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.navigation.actions;
 
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -32,6 +33,7 @@ public interface GotoDeclarationHandler {
    * @return the custom text or null to use the default text
    */
   @Nullable
+  @Nls(capitalization = Nls.Capitalization.Title)
   default String getActionText(@NotNull DataContext context) {
     return null;
   }
