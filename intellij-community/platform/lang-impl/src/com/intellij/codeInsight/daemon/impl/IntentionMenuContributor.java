f20d4f4540cedfb7afe3e5fc453bfd25a650e27b
Yann Cébron <yann.cebron@jetbrains.com>
2019-05-15 10:46:21 +0200

    IntentionMenuContributor: mark as @ApiStatus.Internal
    intellij-monorepo: 690bab88d0479ac9ee3bfa9016ef983c14c1680c

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/IntentionMenuContributor.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/IntentionMenuContributor.java
@@ -1,9 +1,10 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl;
 
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -11,6 +12,7 @@ import org.jetbrains.annotations.NotNull;
  * be used by plugins. Plugin developers should implement {@link com.intellij.codeInsight.intention.IntentionAction} or
  * {@link com.intellij.codeInspection.QuickFix} instead.
  */
+@ApiStatus.Internal
 public interface IntentionMenuContributor {
   ExtensionPointName<IntentionMenuContributor> EP_NAME = ExtensionPointName.create("com.intellij.intentionMenuContributor");
 
