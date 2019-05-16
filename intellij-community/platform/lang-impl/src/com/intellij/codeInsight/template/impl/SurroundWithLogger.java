b8f883367bd05b73893eea7f467566cd7a61f19b
Shaverdova Elena <Elena.Shaverdova@jetbrains.com>
2019-05-16 19:07:25 +0200

    Use class names for surrounders and custom remplates IDEA-CR-47182 FUS-366
    intellij-monorepo: 85a839681cf19d6ff5227199408e6fb8eda9abbe

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/SurroundWithLogger.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/SurroundWithLogger.java
@@ -10,25 +10,27 @@ import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogg
 import com.intellij.internal.statistic.utils.PluginInfo;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
 import com.intellij.lang.Language;
-import com.intellij.lang.LanguageSurrounders;
-import com.intellij.lang.folding.CustomFoldingSurroundDescriptor;
-import com.intellij.lang.surroundWith.SurroundDescriptor;
 import com.intellij.lang.surroundWith.Surrounder;
 import com.intellij.openapi.project.Project;
 import kotlin.Triple;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
 public class SurroundWithLogger {
   private final static String USAGE_GROUP = "surround.with";
 
   public static void logSurrounder(Surrounder surrounder, @NotNull Language language, @NotNull Project project) {
-    PluginInfo pluginInfo = PluginInfoDetectorKt.getPluginInfo(surrounder.getClass());
+    log("surrounder", surrounder.getClass(), language, project);
+  }
+
+  private static void log(@NotNull String type,
+                          @NotNull Class elementClass,
+                          @NotNull Language language,
+                          @NotNull Project project) {
+    PluginInfo pluginInfo = PluginInfoDetectorKt.getPluginInfo(elementClass);
     FeatureUsageData data = new FeatureUsageData().addPluginInfo(pluginInfo).addLanguage(language);
-    data.addData("type", "surrounder");
-    String description = pluginInfo.getType().isDevelopedByJetBrains() ? surrounder.getTemplateDescription() : "third.party";
+    data.addData("type", type);
+    String description = pluginInfo.getType().isDevelopedByJetBrains() ? elementClass.getName() : "third.party";
     FUCounterUsageLogger.getInstance().logEvent(project, USAGE_GROUP, description, data);
   }
 
@@ -47,11 +49,7 @@ public class SurroundWithLogger {
   }
 
   static void logCustomTemplate(@NotNull CustomLiveTemplate template, @NotNull Language language, @NotNull Project project) {
-    PluginInfo pluginInfo = PluginInfoDetectorKt.getPluginInfo(template.getClass());
-    FeatureUsageData data = new FeatureUsageData().addPluginInfo(pluginInfo).addLanguage(language);
-    data.addData("type", "custom.template");
-    String title = pluginInfo.getType().isDevelopedByJetBrains() ? template.getTitle() : "third.party";
-    FUCounterUsageLogger.getInstance().logEvent(project, USAGE_GROUP, title, data);
+    log("custom.template", template.getClass(), language, project);
   }
 
   public static class SurroundWithIdValidator extends CustomUtilsWhiteListRule {
@@ -69,39 +67,13 @@ public class SurroundWithLogger {
         return ValidationResultType.REJECTED;
       }
 
-      if ("surrounder".equals(typeObject)) {
-        for (Surrounder surrounder : CustomFoldingSurroundDescriptor.INSTANCE.getSurrounders()) {
-          if (data.equals(surrounder.getTemplateDescription())) {
-            PluginInfo info = PluginInfoDetectorKt.getPluginInfo(surrounder.getClass());
-            return info.isSafeToReport() ? ValidationResultType.ACCEPTED : ValidationResultType.REJECTED;
-          }
-        }
-
-        Object languageId = context.eventData.get("lang");
-        if (!(languageId instanceof String)) return ValidationResultType.REJECTED;
-        Language language = Language.findLanguageByID((String)languageId);
-        if (language == null) return ValidationResultType.REJECTED;
-        List<SurroundDescriptor> descriptors = LanguageSurrounders.INSTANCE.allForLanguage(language);
-        for (SurroundDescriptor descriptor : descriptors) {
-          for (Surrounder surrounder : descriptor.getSurrounders()) {
-            if (data.equals(surrounder.getTemplateDescription())) {
-              PluginInfo info = PluginInfoDetectorKt.getPluginInfo(surrounder.getClass());
-              return info.isSafeToReport() ? ValidationResultType.ACCEPTED : ValidationResultType.REJECTED;
-            }
-          }
-        }
+      if ("surrounder".equals(typeObject) || "custom.template".equals(typeObject)) {
+        boolean isFromPluginRepository = PluginInfoDetectorKt.getPluginInfo(data).isSafeToReport();
+        return isFromPluginRepository ? ValidationResultType.ACCEPTED : ValidationResultType.REJECTED;
       }
       if ("template".equals(typeObject)) {
         Object group = context.eventData.get("group");
         return LiveTemplateRunLogger.LiveTemplateValidator.validateKeyGroup(data, group);
-      }
-      if ("custom.template".equals(typeObject)) {
-        for (CustomLiveTemplate template : CustomLiveTemplate.EP_NAME.getExtensions()) {
-          if (data.equals(template.getTitle())) {
-            PluginInfo info = PluginInfoDetectorKt.getPluginInfo(template.getClass());
-            return info.isSafeToReport() ? ValidationResultType.ACCEPTED : ValidationResultType.REJECTED;
-          }
-        }
       }
 
       return ValidationResultType.REJECTED;
