096429342ecaf0fb8ac1e6bed312670a395a2e96
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-05-17 14:01:47 +0300

    Build tw: quickfix link text cleanup IDEA-212028
    intellij-monorepo: 59b6bc8bb09a3290afd4ead2e80f0d98ae66af2a

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/issue/IncompatibleGradleJdkIssueChecker.kt
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/issue/IncompatibleGradleJdkIssueChecker.kt
@@ -3,6 +3,7 @@ package org.jetbrains.plugins.gradle.issue
 
 import com.intellij.build.issue.BuildIssue
 import com.intellij.build.issue.BuildIssueQuickFix
+import com.intellij.openapi.application.ApplicationNamesInfo
 import com.intellij.util.PlatformUtils.getPlatformPrefix
 import org.gradle.util.GradleVersion
 import org.jetbrains.annotations.ApiStatus
@@ -48,12 +49,13 @@ class IncompatibleGradleJdkIssueChecker : GradleIssueChecker {
     val gradleVersionString = if (gradleVersionUsed != null) gradleVersionUsed.version else "version"
     if (isToolingClientIssue) {
       issueDescription.append(
-        "\n\nThe project uses Gradle $gradleVersionString which is incompatible with IDE running under Java 10 or newer.")
+        "\n\nThe project uses Gradle $gradleVersionString which is incompatible with " +
+        "${ApplicationNamesInfo.getInstance().productName} running on Java 10 or newer.")
     }
     else {
       issueDescription.append("\n\nThe project uses Gradle $gradleVersionString which is incompatible with Java 10 or newer.")
     }
-    issueDescription.append("\nYou can:\n")
+    issueDescription.append("\nPossible solution:\n")
     val wrapperPropertiesFile = GradleUtil.findDefaultWrapperPropertiesFile(issueData.projectPath)
     if (wrapperPropertiesFile == null || isToolingClientIssue || gradleVersionUsed != null && gradleVersionUsed.baseVersion < gradleMinimumVersionRequired) {
       val gradleVersionFix = GradleVersionQuickFix(issueData.projectPath, gradleMinimumVersionRequired, true)
