77dd806742b5b548664d288607aa77d6aa0cd4ba
Sergey Rostov <Sergey.Rostov@jetbrains.com>
2019-05-17 11:16:37 +0300

    Gradle, js, yarn: split into workspaces and simple implementations 	#KT-31018 	#KT-31017

---
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnBasics.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.targets.js.yarn
+
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.internal.execWithProgress
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmApi
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmProject
+import java.io.File
+
+abstract class YarnBasics : NpmApi {
+    override fun setup(project: Project) {
+        YarnPlugin.apply(project).executeSetup()
+    }
+
+    fun yarnExec(
+        project: Project,
+        dir: File,
+        description: String,
+        vararg args: String
+    ) {
+        val nodeJsEnv = NodeJsPlugin.apply(project).root.environment
+        val yarnEnv = YarnPlugin.apply(project).environment
+
+        project.execWithProgress(description) { exec ->
+            exec.executable = nodeJsEnv.nodeExecutable
+            exec.args = listOf(yarnEnv.home.resolve("bin/yarn.js").absolutePath) + args
+            exec.workingDir = dir
+        }
+
+    }
+
+    protected fun yarnLockReadTransitiveDependencies(
+        nodeWorkDir: File,
+        srcDependenciesList: Collection<NpmDependency>
+    ) {
+        val yarnLock = nodeWorkDir.resolve("yarn.lock")
+        if (yarnLock.isFile) {
+            val byKey = YarnLock.parse(yarnLock).entries.associateBy { it.key }
+            val visited = mutableSetOf<NpmDependency>()
+
+            fun resolveRecursively(src: NpmDependency): NpmDependency {
+                if (src in visited) return src
+                visited.add(src)
+
+                val key = YarnLock.key(src.key, src.version)
+                val deps = byKey[key]
+                if (deps != null) {
+                    src.dependencies.addAll(deps.dependencies.map { dep ->
+                        val scopedName = dep.scopedName
+
+                        resolveRecursively(
+                            NpmDependency(
+                                src.project,
+                                scopedName.scope,
+                                scopedName.name,
+                                dep.version ?: "*"
+                            )
+                        )
+                    })
+                } else {
+                    // todo: [WARN] cannot find $key in yarn.lock
+                }
+
+                return src
+            }
+
+            srcDependenciesList.forEach { src ->
+                resolveRecursively(src)
+            }
+        }
+    }
+}
