ef70e9db1921f3870bfc0c3f493f108ceb92dfb9
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-05-17 18:13:38 +0300

    Don't use light classes for navigation element (KT-10215)
    Place KotlinNavBarModelExtension before Java extension because it
    substitutes non-java elements to files.
    
     #KT-10215 Fixed

--- idea/src/org/jetbrains/kotlin/idea/navigationToolbar/KotlinNavBarModelExtension.kt
+++ idea/src/org/jetbrains/kotlin/idea/navigationToolbar/KotlinNavBarModelExtension.kt
@@ -9,16 +9,20 @@ import com.intellij.ide.navigationToolbar.AbstractNavBarModelExtension
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.idea.KotlinIconProvider
+import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtFile
 
 class KotlinNavBarModelExtension : AbstractNavBarModelExtension() {
-    override fun getPresentableText(`object`: Any?): String? {
-        return null
+    override fun getPresentableText(item: Any?): String? {
+        return when (item) {
+            is KtClassOrObject -> item.name ?: "unknown"
+            else -> null
+        }
     }
 
     override fun adjustElement(psiElement: PsiElement?): PsiElement? {
         val containingFile = psiElement?.containingFile as? KtFile ?: return psiElement
         if (containingFile.isScript()) return psiElement
-        return KotlinIconProvider.getSingleClass(containingFile)?.toLightClass() ?: psiElement
+        return KotlinIconProvider.getSingleClass(containingFile) ?: psiElement
     }
 }
