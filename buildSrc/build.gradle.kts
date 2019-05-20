0fd298eac22a6c29487d555ac4db8383474e32dc
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2019-05-20 17:28:21 +0300

    Finally clean up buildSrc from compiled Kotlin code

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,41 +1,9 @@
 // NOTE: This buildfile file is completely ignored when running composite build `kotlin` + `kotlin-ultimate`.
 
-buildscript {
-    val cacheRedirectorEnabled: Boolean = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() == true
-
-    repositories {
-        if (cacheRedirectorEnabled) {
-            maven("https://cache-redirector.jetbrains.com/jcenter.bintray.com")
-        }
-        jcenter()
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
-    }
-}
-
-group = "org.jetbrains.kotlin.ultimate"
-version = "1.0"
-
 plugins {
-    kotlin("jvm") version embeddedKotlinVersion
-    `kotlin-dsl-base`
-}
-
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
+    base
 }
 
 rootProject.apply {
     from(rootProject.file("../gradle/cidrPluginProperties.gradle.kts"))
-}
-
-val cacheRedirectorEnabled: Boolean = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() == true
-
-repositories {
-    if (cacheRedirectorEnabled) {
-        maven("https://cache-redirector.jetbrains.com/jcenter.bintray.com")
-    }
-    jcenter()
 }
--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,18 +1,5 @@
 // NOTE: This settings file is completely ignored when running composite build `kotlin` + `kotlin-ultimate`.
 
-import java.lang.Boolean.parseBoolean
-
-val cacheRedirectorEnabled: String? by settings
-
-pluginManagement {
-    repositories {
-        if (parseBoolean(cacheRedirectorEnabled)) {
-            maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
-        }
-        gradlePluginPortal()
-    }
-}
-
 include(":prepare-deps:platform-deps",
         ":prepare-deps:cidr",
         ":prepare-deps:idea-plugin"
