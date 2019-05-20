eca96e2923b3c6f423f00ba1318b55a404992aaf
Dmitry.Krasilschikov <kradima@jetbrains.com>
2019-05-20 17:15:42 +0300

    IDEA-207961 Hide copy TBX Refence action under a registry key
    intellij-monorepo: 0f10a1a7b806ced6697c1a8e78658b66503e972b

--- intellij-community/platform/lang-impl/src/com/intellij/ide/actions/CopyTBXReferenceAction.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/actions/CopyTBXReferenceAction.kt
@@ -22,6 +22,7 @@ import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileSystemItem
@@ -38,6 +39,11 @@ class CopyTBXReferenceAction : DumbAwareAction() {
   }
 
   override fun update(e: AnActionEvent) {
+    if (!Registry.`is`("copy.tbx.reference.enabled")) {
+      e.presentation.isEnabledAndVisible = false
+      return
+    }
+
     var plural = false
     var enabled: Boolean
     var paths = false
