9648d9557338c1ce317e9daa0dba20370e3f6862
Svetlana.Zemlyanskaya <svetlana.zemlyanskaya@jetbrains.com>
2019-05-21 11:46:48 +0200

    FUS: implement util validator for run config executor (FUS-389)
    intellij-monorepo: 83a8ed7582219d10481ffdee4792a1a6ea7148de

--- intellij-community/platform/lang-impl/src/com/intellij/execution/impl/statistics/RunConfigurationUsageTriggerCollector.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/impl/statistics/RunConfigurationUsageTriggerCollector.java
@@ -4,12 +4,16 @@ package com.intellij.execution.impl.statistics;
 import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.internal.statistic.eventLog.FeatureUsageData;
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType;
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext;
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomUtilsWhiteListRule;
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger;
 import com.intellij.internal.statistic.utils.PluginInfo;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class RunConfigurationUsageTriggerCollector {
 
@@ -17,10 +21,29 @@ public class RunConfigurationUsageTriggerCollector {
     final FeatureUsageData data = new FeatureUsageData().addProject(project);
     final String key = AbstractRunConfigurationTypeUsagesCollector.toReportedId(factory, data);
     if (StringUtil.isNotEmpty(key)) {
-      final PluginInfo info = PluginInfoDetectorKt.getPluginInfo(executor.getClass());
-      data.addData("executor", info.isSafeToReport() ? executor.getId() : "UNKNOWN");
+      data.addData("executor", executor.getId());
 
       FUCounterUsageLogger.getInstance().logEvent(project, "run.configuration.exec", key, data);
+    }
+  }
+
+  public static class RunConfigurationExecutorUtilValidator extends CustomUtilsWhiteListRule {
+
+    @Override
+    public boolean acceptRuleId(@Nullable String ruleId) {
+      return "run_config_executor".equals(ruleId);
+    }
+
+    @NotNull
+    @Override
+    protected ValidationResultType doValidate(@NotNull String data, @NotNull EventContext context) {
+      for (Executor executor : Executor.EXECUTOR_EXTENSION_NAME.getExtensions()) {
+        if (StringUtil.equals(executor.getId(), data)) {
+          final PluginInfo info = PluginInfoDetectorKt.getPluginInfo(executor.getClass());
+          return info.isSafeToReport() ? ValidationResultType.ACCEPTED : ValidationResultType.THIRD_PARTY;
+        }
+      }
+      return ValidationResultType.REJECTED;
     }
   }
 }
