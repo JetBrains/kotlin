76f2945fdc1eea182c12bf62c0581f488ce32faf
Anton Makeev <Anton.Makeev@jetbrains.com>
2019-05-22 17:09:26 +0300

    FUS-392 Rename context path with unified name "place" instead of "data_1" (+other contexts renamed)
    intellij-monorepo: 548982da04449a25343b3f425ddd3a172af3a624

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/statistics/ExternalSystemActionsCollector.kt
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/statistics/ExternalSystemActionsCollector.kt
@@ -1,15 +1,14 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.statistics
 
+import com.intellij.internal.statistic.collectors.fus.actions.persistence.ActionsCollectorImpl
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
-import com.intellij.internal.statistic.service.fus.collectors.FUSUsageContext
 import com.intellij.internal.statistic.service.fus.collectors.UsageDescriptorKeyValidator
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.project.Project
-import com.intellij.util.text.nullize
 
 class ExternalSystemActionsCollector {
 
@@ -20,31 +19,31 @@ class ExternalSystemActionsCollector {
                 featureId: String,
                 place: String?,
                 isFromContextMenu: Boolean,
-                vararg additionalContextData: String) {
-      if (project == null) return
+                vararg additionalContextData: Pair<String, String>) {
+      val data = FeatureUsageData().addOS().addProject(project)
 
-      // preserve context data ordering
-      val context = FUSUsageContext.create(
-        place.nullize(true) ?: "undefined place",
-        "fromContextMenu.$isFromContextMenu",
-        systemId?.let { getAnonymizedSystemId(it) } ?: "undefined.system",
-        *additionalContextData
-      )
+      if (place != null) {
+        data.addPlace(place).addData("context_menu", isFromContextMenu)
+      }
+      for (each in additionalContextData) {
+        data.addData(each.first, each.second)
+      }
+      addSystemId(data, systemId)
 
-      FUCounterUsageLogger.getInstance().logEvent(project, "build.tools.actions",
-                                                  UsageDescriptorKeyValidator.ensureProperKey(featureId),
-                                                  FeatureUsageData().addFeatureContext(context)
-      )
+      FUCounterUsageLogger.getInstance().logEvent("build.tools.actions", UsageDescriptorKeyValidator.ensureProperKey(featureId), data)
     }
 
+    private fun addSystemId(data: FeatureUsageData,
+                            systemId: ProjectSystemId?) {
+      data.addData("system_id", systemId?.let { getAnonymizedSystemId(it) } ?: "undefined.system")
+    }
 
     @JvmStatic
     fun trigger(project: Project?,
                 systemId: ProjectSystemId?,
                 action: AnAction,
-                event: AnActionEvent,
-                vararg additionalContextData: String) {
-      trigger(project, systemId, action.javaClass.simpleName, event, *additionalContextData)
+                event: AnActionEvent) {
+      ActionsCollectorImpl.record("build.tools.actions", project, action, event) { data -> addSystemId(data, systemId) }
     }
 
     @JvmStatic
@@ -52,7 +51,7 @@ class ExternalSystemActionsCollector {
                 systemId: ProjectSystemId?,
                 featureId: String,
                 event: AnActionEvent,
-                vararg additionalContextData: String) {
+                vararg additionalContextData: Pair<String, String>) {
       trigger(project, systemId, featureId, event.place, event.isFromContextMenu, *additionalContextData)
     }
   }
