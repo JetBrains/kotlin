e8b01e3edd2f59f8f5438b4499fe5413323dcc5f
Anton Makeev <Anton.Makeev@jetbrains.com>
2019-05-22 11:50:55 +0300

    FUS-392 Rename context path with unified name "place" instead of "data_1" (+other contexts renamed)
    intellij-monorepo: fc18b3449bbacb28d134671b164b365907e5505a

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/statistics/GradleActionsUsagesCollector.kt
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/statistics/GradleActionsUsagesCollector.kt
@@ -1,10 +1,8 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.statistics
 
-import com.intellij.internal.statistic.eventLog.FeatureUsageData
+import com.intellij.internal.statistic.collectors.fus.actions.persistence.ActionsCollectorImpl
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
-import com.intellij.internal.statistic.service.fus.collectors.FUSUsageContext
-import com.intellij.internal.statistic.service.fus.collectors.UsageDescriptorKeyValidator
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.Project
@@ -13,17 +11,7 @@ class GradleActionsUsagesCollector {
   companion object {
     @JvmStatic
     fun trigger(project: Project?, action: AnAction, event: AnActionEvent?) {
-      if (project == null) return
-
-      // preserve context data ordering
-      val context = FUSUsageContext.create(
-        event?.place ?: "undefined.place",
-        "fromContextMenu.${event?.isFromContextMenu?.toString() ?: "false"}"
-      )
-
-      val actionClassName = UsageDescriptorKeyValidator.ensureProperKey(action.javaClass.simpleName)
-
-      FUCounterUsageLogger.getInstance().logEvent(project, "build.gradle.actions", actionClassName, FeatureUsageData().addFeatureContext(context))
+      ActionsCollectorImpl.record("build.gradle.actions", project, action, event, null)
     }
 
     @JvmStatic
