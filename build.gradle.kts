e7c78d8d7ef18ef09a4ff6fdf5eeb2fb67c6c07a
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2019-05-22 19:14:56 +0300

    Update bootstrap
    Update reference public API. Before 266976ac1e006e47e510e293c554a6018c987a09
    suspend inline functions with crossinline parameters were effectively
    inline-only. Fixing it exposed suspendCoroutine as public API.
    Update jps artifacts after bootstrap.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
     extra["defaultSnapshotVersion"] = "1.3-SNAPSHOT"
 
     // when updating please also update JPS artifacts configuration: https://jetbrains.quip.com/zzGUAYSJ6gv3/JPS-Build-update-bootstrap
-    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3.50-dev-44", onlySuccessBootstrap = false))
+    kotlinBootstrapFrom(BootstrapOption.TeamCity("1.3.50-dev-397", onlySuccessBootstrap = false))
 
     repositories.withRedirector(project) {
         bootstrapKotlinRepo?.let(::maven)
