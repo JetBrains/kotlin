a089cd5e9de8c047267e9dee0b6e7437826ae3c2
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-05-23 19:38:36 +0900

    Add test

--- idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
@@ -4703,6 +4703,11 @@ public class LocalInspectionTestGenerated extends AbstractLocalInspectionTest {
             runTest("idea/testData/inspectionsLocal/moveVariableDeclarationIntoWhen/withComment2.kt");
         }
 
+        @TestMetadata("withComment3.kt")
+        public void testWithComment3() throws Exception {
+            runTest("idea/testData/inspectionsLocal/moveVariableDeclarationIntoWhen/withComment3.kt");
+        }
+
         @TestMetadata("withNewLine.kt")
         public void testWithNewLine() throws Exception {
             runTest("idea/testData/inspectionsLocal/moveVariableDeclarationIntoWhen/withNewLine.kt");
