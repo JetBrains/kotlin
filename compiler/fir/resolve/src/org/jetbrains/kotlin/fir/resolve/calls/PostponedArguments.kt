70a852f0726e92c592141f81633ffbbdaf44d682
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-05-24 13:12:20 +0300

    FIR: fix exception in ConeKotlinType.returnType

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/PostponedArguments.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/PostponedArguments.kt
@@ -78,12 +78,11 @@ private fun isFunctionalTypeWithReceiver(typeRef: FirTypeRef) =
         coneTypeSafe.lookupTag.classId.asString() == "kotlin/ExtensionFunctionType"
     }
 
-private val ConeKotlinType.returnType: ConeKotlinType
+private val ConeKotlinType.returnType: ConeKotlinType?
     get() {
         require(this is ConeClassType)
         val projection = typeArguments.last()
-        require(projection is ConeTypedProjection)
-        return projection.type
+        return (projection as? ConeTypedProjection)?.type
     }
 
 private val ConeKotlinType.valueParameterTypes: List<ConeKotlinType?>
@@ -139,8 +138,8 @@ internal fun extractLambdaInfoFromFunctionalType(
     val parameters = extractLambdaParameters(expectedType, argument)
 
     val argumentAsFunctionExpression = argument//.safeAs<FunctionExpression>()
-    val receiverType = argumentAsFunctionExpression?.receiverType ?: expectedType.receiverType(expectedTypeRef)
-    val returnType = argumentAsFunctionExpression?.returnType ?: expectedType.returnType
+    val receiverType = argumentAsFunctionExpression.receiverType ?: expectedType.receiverType(expectedTypeRef)
+    val returnType = argumentAsFunctionExpression.returnType ?: expectedType.returnType ?: return null
 
     return ResolvedLambdaAtom(
         argument,
