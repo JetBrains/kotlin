41daf993460002f6a071510356f8c2db1f7fa254
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-05-27 19:16:34 +0700

    Remove IfThenToDoubleBangIntention Relates to #KT-31502

--- idea/testData/intentions/branched/ifThenToDoubleBang/acceptableEmptyElseBlockForStatement.kt
+++
@@ -1,9 +0,0 @@
-// WITH_RUNTIME
-fun main(args: Array<String>) {
-    val foo: String? = "foo"
-    if (foo == null<caret>) {
-        throw NullPointerException()
-    }
-    else {
-    }
-}
--- idea/testData/intentions/branched/ifThenToDoubleBang/willNotInlineClassProperty.kt
+++
@@ -1,9 +0,0 @@
-// WITH_RUNTIME
-class F(a: Int?) {
-    val b = a
-    val c = if (b != <caret> null) b else throw NullPointerException()
-}
-
-fun main(args: Array<String>) {
-    F(1).c
-}
