2ca1e5c8c785d900eb46a40a35510aa93ce70455
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-05-27 20:41:06 +0300

    [FIR] Pass properties to test

--- compiler/fir/modularized-tests/build.gradle.kts
+++ compiler/fir/modularized-tests/build.gradle.kts
@@ -41,6 +41,7 @@ sourceSets {
 }
 
 projectTest {
+    systemProperties(project.properties.filterKeys { it.startsWith("fir.") })
     workingDir = rootDir
     jvmArgs!!.removeIf { it.contains("-Xmx") }
     maxHeapSize = "3g"
