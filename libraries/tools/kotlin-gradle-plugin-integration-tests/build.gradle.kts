78ce34f2234c33ed150825060ccd1e4bfe636863
Ilya Matveev <ilya.matveev@jetbrains.com>
2019-05-28 16:10:04 +0300

    Gradle, native: Remove K/N repo from Gradle integration tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
@@ -13,10 +13,6 @@ pill {
 
 val kotlinGradlePluginTest = project(":kotlin-gradle-plugin").sourceSets.getByName("test")
 
-repositories {
-    maven(kotlinNativeRepo)
-}
-
 dependencies {
     testCompile(project(":kotlin-gradle-plugin"))
     testCompile(kotlinGradlePluginTest.output)
