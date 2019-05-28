52fede12aa0fd547d850d6d7b71466e1ef971295
Dmitry Jemerov <yole@jetbrains.com>
2019-05-28 16:51:25 +0200

    Enable "Introduce parameter" in injected context always, not only when XPath plugin is installed
    intellij-monorepo: f0851d9319595f27ea17419f80212442a1c502b9

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/actions/IntroduceParameterAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/actions/IntroduceParameterAction.java
@@ -22,6 +22,10 @@ import com.intellij.refactoring.RefactoringActionHandler;
 import org.jetbrains.annotations.NotNull;
 
 public class IntroduceParameterAction extends BasePlatformRefactoringAction {
+  public IntroduceParameterAction() {
+    setInjectedContext(true);
+  }
+
   @Override
   protected boolean isAvailableInEditorOnly() {
     return true;
