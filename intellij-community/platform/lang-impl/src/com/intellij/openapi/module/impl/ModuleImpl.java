c87971a1d77b8bd9570a82d4f199988f8a8a89eb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-05-28 19:40:19 +0200

    move “app component registration” to `idea main` thread from EDT
    currently, it depends on race condition and still sometimes in EDT — what will be first, plugin descriptor loading or Swing related tasks
    
    intellij-monorepo: 57b8c511306f2e9fbaa0ff9e75947dacc36d553c

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/module/impl/ModuleImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/module/impl/ModuleImpl.java
@@ -74,11 +74,8 @@ public class ModuleImpl extends PlatformComponentManagerImpl implements ModuleEx
   public void init(@Nullable Runnable beforeComponentCreation) {
     // do not measure (activityNamePrefix method not overridden by this class)
     // because there are a lot of modules and no need to measure each one
-    init(PluginManagerCore.getLoadedPlugins(), null, () -> {
-      if (beforeComponentCreation != null) {
-        beforeComponentCreation.run();
-      }
-    });
+    registerComponents(PluginManagerCore.getLoadedPlugins());
+    init(null, beforeComponentCreation);
   }
 
   @Nullable
