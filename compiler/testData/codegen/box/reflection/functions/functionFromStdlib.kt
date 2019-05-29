eee8621cd9bd46c0f1a6cb6a8a113ee5e8cf28c3
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-05-29 06:47:32 +0300

    Fix method reference ambiguity in test after introducing 'capitalize' overload
    (cherry picked from commit fe9bd9ee20fffe57b3341c7c4a01e9b296c2d0b7)

--- compiler/testData/codegen/box/reflection/functions/functionFromStdlib.kt
+++ compiler/testData/codegen/box/reflection/functions/functionFromStdlib.kt
@@ -8,5 +8,6 @@ import kotlin.reflect.jvm.isAccessible
 fun doStuff(fn: KFunction1<String, String>) = fn.call("oK")
 
 fun box(): String {
-    return doStuff(String::capitalize.apply { isAccessible = true })
+    val method: KFunction1<String, String> = String::capitalize
+    return doStuff(method.apply { isAccessible = true })
 }
