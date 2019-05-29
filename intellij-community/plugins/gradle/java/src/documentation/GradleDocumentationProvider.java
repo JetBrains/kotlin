a14dc5c07656725fdca1aca690e1e2697080e782
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-05-29 17:36:11 +0300

    make DocumentationProvider methods default and remove dummy implementations
    intellij-monorepo: 69056c1ee25ac3631f87e5b924f181921660ad38

--- intellij-community/plugins/gradle/java/src/documentation/GradleDocumentationProvider.java
+++ intellij-community/plugins/gradle/java/src/documentation/GradleDocumentationProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.documentation;
 
 import com.intellij.codeInsight.javadoc.JavaDocUtil;
@@ -34,8 +20,6 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrCall;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral;
 import org.jetbrains.plugins.groovy.lang.psi.impl.synthetic.GrLightVariable;
 
-import java.util.List;
-
 /**
  * @author Vladislav.Soroka
  */
@@ -53,12 +37,6 @@ public class GradleDocumentationProvider implements DocumentationProvider {
         if (doc != null) return doc;
       }
     }
-    return null;
-  }
-
-  @Nullable
-  @Override
-  public List<String> getUrlFor(PsiElement element, PsiElement originalElement) {
     return null;
   }
 
