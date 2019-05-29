5409110bf828ff8ae46f36b80c94cc0195400c7e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-05-29 10:12:44 +0200

    cleanup - remove StartupProgress
    intellij-monorepo: 65b1461985efd8961b156b212ddd08647adfa969

--- intellij-community/platform/lang-impl/src/com/intellij/ui/ShowSplashAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ui/ShowSplashAction.java
@@ -1,22 +1,7 @@
-/*
- * Copyright 2000-2010 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ui;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ex.ApplicationInfoEx;
 import com.intellij.openapi.application.impl.ApplicationInfoImpl;
 import com.intellij.openapi.project.DumbAwareAction;
 import org.jetbrains.annotations.NotNull;
@@ -26,16 +11,14 @@ import java.awt.event.*;
 /**
  * @author Konstantin Bulenkov
  */
-public class ShowSplashAction extends DumbAwareAction {
+public final class ShowSplashAction extends DumbAwareAction {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    final ApplicationInfoEx app = ApplicationInfoImpl.getShadowInstance();
-    final Splash splash = new Splash(app);
+    final Splash splash = new Splash(ApplicationInfoImpl.getShadowInstance());
     final SplashListener listener = new SplashListener(splash);
     splash.addFocusListener(listener);
     splash.addKeyListener(listener);
     splash.addMouseListener(listener);
-    splash.show();
   }
 
   private static class SplashListener implements KeyListener, MouseListener, FocusListener {
