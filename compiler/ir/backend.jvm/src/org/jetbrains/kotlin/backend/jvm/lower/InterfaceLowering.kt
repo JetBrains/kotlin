20c0d851e741438fe3778e4767c162bc95c4f9c8
Ting-Yuan Huang <laszio@google.com>
2019-05-29 11:04:10 -0700

    JVM_IR: support @JvmDefaults in compatibility mode.

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InterfaceLowering.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InterfaceLowering.kt
@@ -9,16 +9,22 @@ import org.jetbrains.kotlin.backend.common.ClassLoweringPass
 import org.jetbrains.kotlin.backend.common.descriptors.WrappedSimpleFunctionDescriptor
 import org.jetbrains.kotlin.backend.common.ir.copyTo
 import org.jetbrains.kotlin.backend.common.ir.copyTypeParametersFrom
+import org.jetbrains.kotlin.backend.common.ir.passTypeArgumentsFrom
 import org.jetbrains.kotlin.backend.common.lower.InitializersLowering.Companion.clinitName
 import org.jetbrains.kotlin.backend.common.lower.VariableRemapper
 import org.jetbrains.kotlin.backend.common.phaser.makeIrFilePhase
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.JvmLoweredDeclarationOrigin
+import org.jetbrains.kotlin.backend.jvm.ir.hasJvmDefault
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.descriptors.annotations.Annotations
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.declarations.impl.IrFunctionImpl
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrExpressionBodyImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrGetValueImpl
 import org.jetbrains.kotlin.ir.symbols.impl.IrSimpleFunctionSymbolImpl
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.isInterface
@@ -51,8 +57,15 @@ private class InterfaceLowering(val context: JvmBackendContext) : IrElementTrans
                 val element = context.declarationFactory.getDefaultImplsFunction(function)
                 members.add(element)
                 element.body = function.body?.patchDeclarationParents(element)
-                function.body = null
-                //TODO reset modality to abstract
+                if (function.hasJvmDefault() &&
+                    function.origin != JvmLoweredDeclarationOrigin.SYNTHETIC_METHOD_FOR_PROPERTY_ANNOTATIONS
+                ) {
+                    // TODO: don't touch function and only generate element / DefaultImpls when needed.
+                    function.body = IrExpressionBodyImpl(callDefaultImpls(element, function))
+                } else {
+                    function.body = null
+                    //TODO reset modality to abstract
+                }
             }
         }
 
@@ -67,6 +80,26 @@ private class InterfaceLowering(val context: JvmBackendContext) : IrElementTrans
         Visibilities.isPrivate(function.visibility) && function.name != clinitName ||
                 function.origin == IrDeclarationOrigin.FUNCTION_FOR_DEFAULT_PARAMETER ||
                 function.origin == JvmLoweredDeclarationOrigin.SYNTHETIC_METHOD_FOR_PROPERTY_ANNOTATIONS
+
+    private fun callDefaultImpls(defaultImpls: IrFunction, interfaceMethod: IrFunction): IrCall {
+        val startOffset = interfaceMethod.startOffset
+        val endOffset = interfaceMethod.endOffset
+
+        return IrCallImpl(interfaceMethod.startOffset, interfaceMethod.endOffset, interfaceMethod.returnType, defaultImpls.symbol).apply {
+            passTypeArgumentsFrom(interfaceMethod)
+
+            var offset = 0
+            interfaceMethod.dispatchReceiverParameter?.let {
+                putValueArgument(offset++, IrGetValueImpl(startOffset, endOffset, it.symbol))
+            }
+            interfaceMethod.extensionReceiverParameter?.let {
+                putValueArgument(offset++, IrGetValueImpl(startOffset, endOffset, it.symbol))
+            }
+            interfaceMethod.valueParameters.forEachIndexed { i, it ->
+                putValueArgument(i + offset, IrGetValueImpl(startOffset, endOffset, it.symbol))
+            }
+        }
+    }
 }
 
 
