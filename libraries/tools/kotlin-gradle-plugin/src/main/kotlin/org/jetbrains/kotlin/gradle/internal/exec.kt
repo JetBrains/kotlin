9b886344e2067571038737637228cb9ed8ceef70
Sergey Rostov <sergey.rostov@jetbrains.com>
2019-05-30 16:01:26 +0300

    Gradle, execWithProgress: report description as first progress

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/exec.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/exec.kt
@@ -13,7 +13,6 @@ import org.gradle.process.internal.ExecActionFactory
 import java.io.ByteArrayOutputStream
 import java.io.PipedInputStream
 import java.io.PipedOutputStream
-import java.nio.CharBuffer
 import kotlin.concurrent.thread
 
 internal fun Project.execWithProgress(description: String, readStdErr: Boolean = false, body: (ExecAction) -> Unit): ExecResult {
@@ -25,6 +24,7 @@ internal fun Project.execWithProgress(description: String, readStdErr: Boolean =
     val exec = services.get(ExecActionFactory::class.java).newExecAction()
     body(exec)
     return project.operation(description) {
+        progress(description)
         exec.standardOutput = PipedOutputStream(stdInPipe)
         val outputReaderThread = thread(name = "output reader for [$description]") {
             stdInPipe.reader().use { reader ->
