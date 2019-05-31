b56ecc05d8578e8f2908af6fc1e64ecc6409d94a
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-05-31 17:11:15 +0300

    CPP-16283 fixed gradle project data deserialization
    intellij-monorepo: 96ee12b8669251e45b42e4dc1358370c5c4d0689

--- intellij-community/plugins/gradle/native/tooling/src/model/impl/CppSharedLibraryImpl.java
+++ intellij-community/plugins/gradle/native/tooling/src/model/impl/CppSharedLibraryImpl.java
@@ -1,7 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.nativeplatform.tooling.model.impl;
 
+import com.intellij.serialization.PropertyMapping;
+import org.jetbrains.plugins.gradle.nativeplatform.tooling.model.CompilationDetails;
 import org.jetbrains.plugins.gradle.nativeplatform.tooling.model.CppSharedLibrary;
+import org.jetbrains.plugins.gradle.nativeplatform.tooling.model.LinkageDetails;
 
 public class CppSharedLibraryImpl extends CppBinaryImpl implements CppSharedLibrary {
   public CppSharedLibraryImpl(String name, String baseName, String variantName) {
@@ -10,5 +13,14 @@ public class CppSharedLibraryImpl extends CppBinaryImpl implements CppSharedLibr
 
   public CppSharedLibraryImpl(CppSharedLibrary binary) {
     super(binary);
+  }
+
+  @PropertyMapping({"name", "baseName", "variantName", "compilationDetails", "linkageDetails"})
+  private CppSharedLibraryImpl(String name,
+                               String baseName,
+                               String variantName,
+                               CompilationDetails compilationDetails,
+                               LinkageDetails linkageDetails) {
+    super(name, baseName, variantName, compilationDetails, linkageDetails);
   }
 }
