a10deb5f6944ef35d6e74e35d96ad8111295a957
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-05-31 17:38:25 +0300

    Remove classId from FirClassSymbol equals / hashCode

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/symbols/impl/FirClassSymbol.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/symbols/impl/FirClassSymbol.kt
@@ -16,11 +16,10 @@ class FirClassSymbol(override val classId: ClassId) : ConeClassSymbol, AbstractF
     override fun toLookupTag(): ConeClassLikeLookupTag = ConeClassLikeLookupTagImpl(classId)
 
     override fun equals(other: Any?): Boolean =
-        other is FirClassSymbol && classId == other.classId && fir == other.fir
+        other is FirClassSymbol && fir == other.fir
 
     override fun hashCode(): Int {
         var result = 31
-        result = result * 19 + classId.hashCode()
         result = result * 19 + fir.hashCode()
         return result
     }
