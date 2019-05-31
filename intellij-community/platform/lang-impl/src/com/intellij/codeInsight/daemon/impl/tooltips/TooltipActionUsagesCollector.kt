c90e944bc4ba586570f87fade0fadb70829e7675
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-05-31 13:43:18 +0300

    platform: change statistics for tooltip actions FUS-419
    intellij-monorepo: 7ebcdf30e4013e609d2ac328edd90ce69aac3849

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/tooltips/TooltipActionUsagesCollector.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/tooltips/TooltipActionUsagesCollector.kt
@@ -1,18 +1,16 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl.tooltips
 
-import com.intellij.internal.statistic.beans.UsageDescriptor
+import com.intellij.internal.statistic.beans.MetricEvent
+import com.intellij.internal.statistic.beans.newBooleanMetric
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
-import com.intellij.internal.statistic.utils.getBooleanUsage
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.registry.Registry
+import java.util.*
 
 class TooltipActionUsagesCollector : ProjectUsagesCollector() {
-  override fun getGroupId(): String = "tooltip.actions"
+  override fun getGroupId(): String = "editor.settings.ide"
 
-  override fun getUsages(project: Project): Set<UsageDescriptor> {
-    if (!Registry.`is`("ide.tooltip.show.with.actions")) return emptySet()
-    
-    return setOf(getBooleanUsage("panel.show.value", TooltipActionProvider.isShowActions()))
+  override fun getMetrics(project: Project): Set<MetricEvent> {
+    return setOf(newBooleanMetric("show.actions.in.tooltip", TooltipActionProvider.isShowActions()))
   }
 } 
