5b691885fa1e1768c09c30e314ddb1424b7029c1
Alexander Zolotov <goldifit@gmail.com>
2019-05-31 16:52:59 +0300

    Status bar: hide widgets without content
    + hide hector and read-only widgets
    
    intellij-monorepo: 838298c078804efe73a32089738720321ad6d4fd

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/wm/impl/status/TogglePopupHintsPanel.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/wm/impl/status/TogglePopupHintsPanel.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.openapi.wm.impl.status;
 
@@ -70,7 +56,7 @@ public class TogglePopupHintsPanel extends EditorBasedWidget implements StatusBa
   }
 
   @Override
-  @NotNull
+  @Nullable
   public Icon getIcon() {
     return myCurrentIcon;
   }
@@ -140,7 +126,7 @@ public class TogglePopupHintsPanel extends EditorBasedWidget implements StatusBa
       myToolTipText += UIBundle.message("popup.hints.panel.click.to.configure.highlighting.tooltip.text");
     }
     else {
-      myCurrentIcon = getDisabledIcon(AllIcons.Ide.HectorOff);
+      myCurrentIcon = file != null ? getDisabledIcon(AllIcons.Ide.HectorOff) : null;
       myToolTipText = null;
     }
 
