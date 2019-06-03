680d98474289356a000c3d1aaa8d8f959dcefe5b
Alexander Zolotov <zolotov@jetbrains.com>
2019-06-03 20:13:42 +0300

    Status bar: add missing popup titles
    intellij-monorepo: 820de1f83462ab86b7a870cf03374ccbce73b564

--- intellij-community/platform/lang-impl/src/com/intellij/psi/codeStyle/statusbar/CodeStyleStatusBarWidget.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/codeStyle/statusbar/CodeStyleStatusBarWidget.java
@@ -106,8 +106,7 @@ public class CodeStyleStatusBarWidget extends EditorBasedStatusBarPopup implemen
 
   @Nullable
   @Override
-  protected ListPopup createPopup(DataContext context)
-  {
+  protected ListPopup createPopup(DataContext context) {
     WidgetState state = getWidgetState(context.getData(CommonDataKeys.VIRTUAL_FILE));
     Editor editor = getEditor();
     PsiFile psiFile = getPsiFile();
@@ -121,7 +120,7 @@ public class CodeStyleStatusBarWidget extends EditorBasedStatusBarPopup implemen
         }
       };
       return JBPopupFactory.getInstance().createActionGroupPopup(
-        null, actionGroup, context,
+        "Code Style", actionGroup, context,
         JBPopupFactory.ActionSelectionAid.SPEEDSEARCH, false);
     }
     return null;
