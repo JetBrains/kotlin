7e20b2c58ac0e0ac7b04f3559bdf8f91877c01b3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-03 12:11:36 +0200

    [tests] updating keyboard shortcut test data (IDEA-CR-47901)
    intellij-monorepo: 09aea2836a54e6d1c9a1a022dfeba1a65434e40e

--- intellij-community/platform/testFramework/extensions/src/com/intellij/keymap/KeymapsTestCase.java
+++ intellij-community/platform/testFramework/extensions/src/com/intellij/keymap/KeymapsTestCase.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.keymap;
 
 import com.intellij.util.containers.ContainerUtil;
@@ -166,6 +166,7 @@ public abstract class KeymapsTestCase extends KeymapsTestCaseBase {
     { "shift meta LEFT",          "EditorLineStartWithSelection", "ResizeToolWindowLeft"},
     { "shift meta RIGHT",         "EditorLineEndWithSelection", "ResizeToolWindowRight"},
     { "alt R",                    "Django.RunManageTaskAction", "org.jetbrains.plugins.ruby.tasks.rake.actions.RakeTasksPopupAction"},
+    { "ctrl SPACE",               "ChangesView.SetDefault", "CodeCompletion"}
     });
     put("Emacs", new String[][] {
     { "TAB",                      "EditorChooseLookupItemReplace", "NextTemplateVariable", "NextParameter", "EditorIndentSelection",
