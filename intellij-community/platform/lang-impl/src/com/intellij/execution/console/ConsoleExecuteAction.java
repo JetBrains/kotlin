d0e9fc1140aaca24ae68792b6e0d5c5b9d576eee
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-04 15:41:18 +0200

    ConsoleExecuteAction: suppress "ComponentNotRegistered"
    intellij-monorepo: 75f2a42b59806d62576a0b455ce5a3de3fcaf521

--- intellij-community/platform/lang-impl/src/com/intellij/execution/console/ConsoleExecuteAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/console/ConsoleExecuteAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 JetBrains s.r.o.
+ * Copyright 2000-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,6 +34,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("ComponentNotRegistered")
 public class ConsoleExecuteAction extends DumbAwareAction {
   static final String CONSOLE_EXECUTE_ACTION_ID = "Console.Execute";
 
