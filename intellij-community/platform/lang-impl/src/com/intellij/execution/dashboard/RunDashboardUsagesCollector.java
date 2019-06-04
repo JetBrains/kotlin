f42055946fe2ba072a0c3ddab95238aec925eca1
sergey.vasiliev <sergey.vasiliev@jetbrains.com>
2019-06-04 16:50:27 +0200

    FUS-405 Run Dashboard + Spring Boot: extract dynamic parts from event id to event data
    intellij-monorepo: 1d027a0b4e07fb3561f96229e5a0a53d1d6056b8

--- intellij-community/platform/lang-impl/src/com/intellij/execution/dashboard/RunDashboardUsagesCollector.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/dashboard/RunDashboardUsagesCollector.java
@@ -2,13 +2,17 @@
 package com.intellij.execution.dashboard;
 
 import com.intellij.execution.configurations.ConfigurationType;
-import com.intellij.internal.statistic.beans.UsageDescriptor;
+import com.intellij.internal.statistic.beans.MetricEvent;
+import com.intellij.internal.statistic.beans.MetricEventFactoryKt;
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType;
+import com.intellij.internal.statistic.eventLog.validator.rules.EventContext;
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomWhiteListRule;
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
-import com.intellij.internal.statistic.utils.StatisticsUtilKt;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
 import java.util.List;
@@ -26,21 +30,42 @@ public class RunDashboardUsagesCollector extends ProjectUsagesCollector {
 
   @NotNull
   @Override
-  public Set<UsageDescriptor> getUsages(@NotNull Project project) {
-    final Set<UsageDescriptor> usages = new HashSet<>();
+  public Set<MetricEvent> getMetrics(@NotNull Project project) {
+    final Set<MetricEvent> metricEvents = new HashSet<>();
     final Set<String> dashboardTypes = RunDashboardManager.getInstance(project).getTypes();
-    usages.add(StatisticsUtilKt.getBooleanUsage("run.dashboard", !dashboardTypes.isEmpty()));
+    metricEvents.add(MetricEventFactoryKt.newBooleanMetric("run.dashboard", !dashboardTypes.isEmpty()));
+
     if (!dashboardTypes.isEmpty()) {
       List<ConfigurationType> configurationTypes = ConfigurationType.CONFIGURATION_TYPE_EP.getExtensionList();
       for (String dashboardType : dashboardTypes) {
         ConfigurationType configurationType = ContainerUtil.find(configurationTypes, type -> type.getId().equals(dashboardType));
         if (configurationType == null) continue;
 
-        String key = PluginInfoDetectorKt.getPluginInfo(configurationType.getClass()).isDevelopedByJetBrains() ?
-                     dashboardType : "third.party";
-        usages.add(new UsageDescriptor(key));
+        String key = PluginInfoDetectorKt.getPluginInfo(configurationType.getClass()).isDevelopedByJetBrains() ?  dashboardType : "third.party";
+        metricEvents.add(MetricEventFactoryKt.newMetric(key));
       }
     }
-    return usages;
+    return metricEvents;
+  }
+
+
+  public static class RunConfigurationTypeValidator extends CustomWhiteListRule {
+    @Override
+    public boolean acceptRuleId(@Nullable String ruleId) {
+      return "run_config".equals(ruleId);
+    }
+
+    @NotNull
+    @Override
+    protected ValidationResultType doValidate(@NotNull String data, @NotNull EventContext context) {
+      if (isThirdPartyValue(data)) return ValidationResultType.ACCEPTED;
+      ConfigurationType configurationType = findConfigurationTypeById(data);
+      if (configurationType == null) return ValidationResultType.REJECTED;
+      return PluginInfoDetectorKt.getPluginInfo(configurationType.getClass()).isDevelopedByJetBrains() ? ValidationResultType.ACCEPTED : ValidationResultType.THIRD_PARTY;
+    }
+
+    private static ConfigurationType findConfigurationTypeById(@NotNull String data) {
+      return ContainerUtil.find(ConfigurationType.CONFIGURATION_TYPE_EP.getExtensionList(), type -> type.getId().equals(data));
+    }
   }
 }
