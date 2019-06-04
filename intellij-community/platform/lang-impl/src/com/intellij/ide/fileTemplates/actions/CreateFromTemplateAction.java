7c1e0d684ea34fc925d1496877189078cb302c55
Yann Cébron <yann.cebron@jetbrains.com>
2019-06-04 15:44:24 +0200

    CreateFromTemplateAction: suppress "ComponentNotRegistered"
    intellij-monorepo: 7f9ca009941c6751e3cde4a76903ec29930c9b5b

--- intellij-community/platform/lang-impl/src/com/intellij/ide/fileTemplates/actions/CreateFromTemplateAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/fileTemplates/actions/CreateFromTemplateAction.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.util.function.Supplier;
 
+@SuppressWarnings("ComponentNotRegistered")
 public class CreateFromTemplateAction extends CreateFromTemplateActionBase {
   private final Supplier<? extends FileTemplate> myTemplate;
 
