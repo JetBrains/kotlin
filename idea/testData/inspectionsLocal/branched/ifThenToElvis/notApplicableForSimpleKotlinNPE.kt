8eb11d9d3da38b8a72345fe27780c8c49c43ad5b
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-06-06 17:36:39 +0700

    Fix some tests

--- idea/testData/inspectionsLocal/branched/ifThenToElvis/notApplicableForSimpleKotlinNPE.kt
+++ idea/testData/inspectionsLocal/branched/ifThenToElvis/notApplicableForSimpleKotlinNPE.kt
@@ -1,5 +1,6 @@
 // PROBLEM: none
 // WITH_RUNTIME
+// DISABLE-ERRORS
 fun main(args: Array<String>) {
     val t: String? = "abc"
     if (t != null<caret>) t else throw KotlinNullPointerException()
--- idea/testData/inspectionsLocal/branched/ifThenToElvis/notApplicableForSimpleNPE.kt
+++ idea/testData/inspectionsLocal/branched/ifThenToElvis/notApplicableForSimpleNPE.kt
@@ -1,5 +1,6 @@
 // PROBLEM: none
 // WITH_RUNTIME
+// DISABLE-ERRORS
 fun main(args: Array<String>) {
     val t: String? = "abc"
     if (t == null<caret>) throw NullPointerException() else t
