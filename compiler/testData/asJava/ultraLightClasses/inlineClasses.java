7d542eda007b15f8aabf29f8a84eecdbbaedc8ee
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2019-06-06 23:48:37 +0300

    Fix UL classes test for inline classes support

---
+++ compiler/testData/asJava/ultraLightClasses/inlineClasses.java
@@ -0,0 +1,15 @@
+public final class UInt /* UInt*/ {
+}
+
+public enum Foo /* Foo*/ {
+  ;
+
+  public final int getX();
+
+}
+
+public final class InlinedDelegate /* InlinedDelegate*/<T>  {
+  @null()
+  public final T getNode();
+
+}
--- compiler/testData/asJava/ultraLightClasses/inlineClasses.kt
+++ compiler/testData/asJava/ultraLightClasses/inlineClasses.kt
@@ -1,14 +1,11 @@
-/** should load cls */
 inline class UInt(private val value: Int) { }
 
-/** should load cls */
 inline enum class Foo(val x: Int) {
     A(0), B(1);
 
     fun example() { }
 }
 
-/** should load cls */
 inline class InlinedDelegate<T>(var node: T) {
     operator fun setValue(thisRef: A, property: KProperty<*>, value: T) {
         if (node !== value) {
