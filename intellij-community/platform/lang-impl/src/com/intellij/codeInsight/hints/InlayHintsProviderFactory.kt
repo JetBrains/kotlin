cedde6937b74721ea913e7b7e75cbc329ab4ae9a
Anton Lobov <Anton.Lobov@jetbrains.com>
2019-06-06 16:29:13 +0200

    IDEA-CR-48027: rename to isLanguageSupported and invert the meaning to positive
    intellij-monorepo: 14575cfafe1fd96a1a14b10c5bfb5d8583a14d69

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/InlayHintsProviderFactory.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/InlayHintsProviderFactory.kt
@@ -21,7 +21,7 @@ object HintUtils {
     val config = ServiceManager.getService(InlayHintsSettings::class.java)
     return getAllMetaProviders()
       .flatMap { it.getProvidersInfo(project) }
-      .filter { language.isKindOf(it.language) && !it.provider.shouldSkipLanguage(language) }
+      .filter { language.isKindOf(it.language) && it.provider.isLanguageSupported(language) }
       .map { it.provider.withSettings(it.language, config) }
   }
 }
