d11f5a79ef44a9325dd1cdd4a6ee2b943a0ddd13
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-06-06 19:33:08 +0200

    fix tests - add IconManager.activate (part 2)
    intellij-monorepo: ff17be62d0c003fb107549a2999aeee86107b730

--- intellij-community/platform/lang-impl/src/com/intellij/ide/actions/QuickChangeCodeStyleSchemeAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/actions/QuickChangeCodeStyleSchemeAction.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
@@ -37,7 +24,7 @@ public class QuickChangeCodeStyleSchemeAction extends QuickSwitchSchemeAction {
     if (manager.getMainProjectCodeStyle() != null) {
       //noinspection HardCodedStringLiteral
       group.add(new AnAction("<project>", "",
-                             manager.USE_PER_PROJECT_SETTINGS ? ourCurrentAction : ourNotCurrentAction) {
+                             manager.USE_PER_PROJECT_SETTINGS ? AllIcons.Actions.Forward : ourNotCurrentAction) {
         @Override
         public void actionPerformed(@NotNull AnActionEvent e) {
           manager.USE_PER_PROJECT_SETTINGS = true;
@@ -59,7 +46,8 @@ public class QuickChangeCodeStyleSchemeAction extends QuickSwitchSchemeAction {
                                 final boolean addScheme,
                                 Project project) {
     group.add(new DumbAwareAction(scheme.getName(), "",
-                                  scheme == currentScheme && !manager.USE_PER_PROJECT_SETTINGS ? ourCurrentAction : ourNotCurrentAction) {
+                                  scheme == currentScheme && !manager.USE_PER_PROJECT_SETTINGS ? AllIcons.Actions.Forward
+                                                                                               : ourNotCurrentAction) {
       @Override
       public void actionPerformed(@NotNull AnActionEvent e) {
         if (addScheme) {
--- intellij-community/platform/lang-impl/src/com/intellij/ide/actions/QuickChangeInspectionProfileAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/actions/QuickChangeInspectionProfileAction.java
@@ -1,21 +1,8 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions;
 
 import com.intellij.codeInspection.InspectionProfile;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
@@ -24,7 +11,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.profile.codeInspection.InspectionProjectProfileManager;
 import com.intellij.profile.codeInspection.ProjectInspectionProfileManager;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class QuickChangeInspectionProfileAction extends QuickSwitchSchemeAction {
   @Override
@@ -40,7 +26,7 @@ public class QuickChangeInspectionProfileAction extends QuickSwitchSchemeAction 
                                 final ProjectInspectionProfileManager projectProfileManager,
                                 final InspectionProfile current,
                                 final InspectionProfile scheme) {
-    group.add(new DumbAwareAction(scheme.getName(), "", scheme == current ? ourCurrentAction : ourNotCurrentAction) {
+    group.add(new DumbAwareAction(scheme.getName(), "", scheme == current ? AllIcons.Actions.Forward : ourNotCurrentAction) {
       @Override
       public void actionPerformed(@NotNull AnActionEvent e) {
         projectProfileManager.setRootProfile(scheme.getName());
