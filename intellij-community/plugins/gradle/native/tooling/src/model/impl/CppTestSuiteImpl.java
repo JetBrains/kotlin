7754d5d7ce142f730b5d8620b95af487c2ce3432
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-06-11 16:16:26 +0300

    IDEA-CR-48003 added tests for CPP tooling model serialization
    intellij-monorepo: aea7ed65f3175aa56c3ecca6691e2af489edea63

--- intellij-community/plugins/gradle/native/tooling/src/model/impl/CppTestSuiteImpl.java
+++ intellij-community/plugins/gradle/native/tooling/src/model/impl/CppTestSuiteImpl.java
@@ -1,9 +1,19 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.nativeplatform.tooling.model.impl;
 
+import com.intellij.serialization.PropertyMapping;
+import org.jetbrains.plugins.gradle.nativeplatform.tooling.model.CppBinary;
 import org.jetbrains.plugins.gradle.nativeplatform.tooling.model.CppTestSuite;
 
+import java.util.Set;
+
 public class CppTestSuiteImpl extends CppComponentImpl implements CppTestSuite {
+
+  @PropertyMapping({"name", "baseName", "binaries"})
+  private CppTestSuiteImpl(String name, String baseName, Set<? extends CppBinary> binaries) {
+    super(name, baseName, binaries);
+  }
+
   public CppTestSuiteImpl(String name, String baseName) {
     super(name, baseName);
   }
