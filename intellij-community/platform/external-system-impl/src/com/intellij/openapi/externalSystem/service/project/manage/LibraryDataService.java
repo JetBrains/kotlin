c74a9c89a42446d0fd8a79607fb857a975ebee13
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-06-11 18:52:26 +0300

    ES: distinguish `ModifiableModelsProvider` created from ProjectStructure dialog IDEA-187832
    intellij-monorepo: e226d6fc2f94e5a5aa35d87672fee1eab3fc7341

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/LibraryDataService.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/LibraryDataService.java
@@ -162,7 +162,7 @@ public final class LibraryDataService extends AbstractProjectDataService<Library
     // do not cleanup orphan project libraries if import runs from Project Structure Dialog
     // since libraries order entries cannot be imported for modules in that case
     // and hence orphans will be detected incorrectly
-    if (modelsProvider.getClass().getSimpleName().equals("IdeUIModifiableModelsProvider")) return;
+    if (modelsProvider instanceof ProjectStructureModifiableModelsProvider) return;
 
     final List<Library> orphanIdeLibraries = ContainerUtil.newSmartList();
     final LibraryTable.ModifiableModel librariesModel = modelsProvider.getModifiableProjectLibrariesModel();
