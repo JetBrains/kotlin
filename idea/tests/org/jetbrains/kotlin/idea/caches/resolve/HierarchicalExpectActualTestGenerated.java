2f8e83d6b9e39351f29b3822d49075a3fa381c97
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2019-06-11 18:11:35 +0300

    Minor: regenerate tests

--- idea/tests/org/jetbrains/kotlin/idea/caches/resolve/HierarchicalExpectActualTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/caches/resolve/HierarchicalExpectActualTestGenerated.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.caches.resolve;
@@ -27,6 +27,11 @@ public class HierarchicalExpectActualTestGenerated extends AbstractHierarchicalE
 
     public void testAllFilesPresentInHierarchicalExpectActualMatching() throws Exception {
         KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("idea/testData/multiModuleHighlighting/hierarchicalExpectActualMatching"), Pattern.compile("^([^\\.]+)$"), TargetBackend.ANY, false);
+    }
+
+    @TestMetadata("diamondActualInBottom")
+    public void testDiamondActualInBottom() throws Exception {
+        runTest("idea/testData/multiModuleHighlighting/hierarchicalExpectActualMatching/diamondActualInBottom/");
     }
 
     @TestMetadata("diamondActualOnOnePath")
