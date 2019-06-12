f826702599ecd3772c58694c994ceb0647825ad0
Dmitry.Krasilschikov <kradima@jetbrains.com>
2019-06-12 15:49:19 +0300

    FUS-418 extract dynamic part from RA stats collectors
    (cherry picked from commit 6dd24d6db3612b0a7a62578880c47659b3bfd93f)
    
    intellij-monorepo: 1c685adf2687d3b44dbbfc081a056b4cf92a79b6

--- intellij-community/platform/lang-impl/src/com/intellij/ide/actions/runAnything/RunAnythingUsageCollector.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/actions/runAnything/RunAnythingUsageCollector.kt
@@ -1,8 +1,8 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions.runAnything
 
-import com.intellij.ide.actions.runAnything.RunAnythingAction.RUN_ANYTHING
 import com.intellij.ide.actions.runAnything.groups.RunAnythingGroup
+import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
 import com.intellij.internal.statistic.utils.getPluginInfo
 import com.intellij.openapi.project.Project
@@ -11,27 +11,24 @@ private const val GROUP_ID = "actions.runAnything"
 
 class RunAnythingUsageCollector {
   companion object {
-    private val allowedCompletionGroupTitles: Array<String> = arrayOf("Bundler", "rails generators", "Gradle tasks", "npm scripts",
-                                                                      "Maven goals",
-                                                                      "rvm use", "rake", "rbenv shell", "General", "Recent projects",
-                                                                      "Run configurations")
-
-    private val allowedHelpGroupTitles: Array<String> = arrayOf("Gradle", "Maven", "npm", "Python", "Recent projects", "ruby", "General",
-                                                                "Recent")
-
     fun trigger(project: Project, featureId: String) {
       FUCounterUsageLogger.getInstance().logEvent(project, GROUP_ID, featureId)
     }
 
     fun triggerExecCategoryStatistics(project: Project,
-                                      groups: Collection<RunAnythingGroup>,
+                                      groups: MutableCollection<out RunAnythingGroup>,
                                       clazz: Class<out RunAnythingSearchListModel>,
-                                      index: Int) {
+                                      index: Int,
+                                      shiftPressed: Boolean,
+                                      altPressed: Boolean) {
       for (i in index downTo 0) {
         val group = RunAnythingGroup.findGroup(groups, i)
         if (group != null) {
-          RunAnythingUsageCollector.trigger(project, getSafeToReportClazzName(clazz) + ": " +
-                                                     RUN_ANYTHING + " - execution - " + getSafeToReportTitle(group))
+          FUCounterUsageLogger.getInstance().logEvent(project, GROUP_ID, "execute", FeatureUsageData()
+            .addData("list", getSafeToReportClazzName(clazz))
+            .addData("group", getSafeToReportTitle(group))
+            .addData("with_shift", shiftPressed)
+            .addData("with_alt", altPressed))
           break
         }
       }
@@ -40,9 +37,11 @@ class RunAnythingUsageCollector {
     fun triggerMoreStatistics(project: Project,
                               group: RunAnythingGroup,
                               clazz: Class<out RunAnythingSearchListModel>) {
-      RunAnythingUsageCollector.trigger(project, getSafeToReportClazzName(clazz) + ": " +
-                                                 RUN_ANYTHING + " - more - " + getSafeToReportTitle(group))
+      FUCounterUsageLogger.getInstance().logEvent(project, GROUP_ID, "click.more", FeatureUsageData()
+        .addData("list", getSafeToReportClazzName(clazz))
+        .addData("group", getSafeToReportTitle(group)))
     }
+
 
     private fun getSafeToReportClazzName(clazz: Class<*>): String {
       return if (getPluginInfo(clazz).isSafeToReport()) clazz.simpleName else "third.party"
@@ -50,7 +49,7 @@ class RunAnythingUsageCollector {
 
     private fun getSafeToReportTitle(group: RunAnythingGroup): String {
       return if (!getPluginInfo(group.javaClass).isSafeToReport()) "third.party"
-      else return if (group.title in allowedCompletionGroupTitles || group.title in allowedHelpGroupTitles) group.title else "run.anything.group.unknown.title"
+      else return group.title
     }
   }
 }
