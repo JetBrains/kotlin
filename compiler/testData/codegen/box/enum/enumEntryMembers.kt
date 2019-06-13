6ad7310df4d85c77c5495976abcdf475dfb1ec11
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2019-06-13 19:31:25 +0300

    Added a test for members of EnumEntry in a library.

---
+++ compiler/testData/codegen/box/enum/enumEntryMembers.kt
@@ -0,0 +1,30 @@
+// WITH_RUNTIME
+// MODULE: lib
+// FILE: lib.kt
+enum class Foo {
+    FOO() {
+        override fun foo() = "foo"
+       
+        override var xxx: String
+            get() =  "xxx"
+            set(value: String) {
+            }
+    };
+
+    abstract fun foo(): String
+    abstract var xxx: String
+}
+
+// MODULE: main(lib)
+// FILE: main.kt
+import kotlin.test.assertEquals
+
+fun box(): String {
+    assertEquals(Foo.FOO.foo(), "foo")
+    Foo.FOO.xxx = "zzzz"
+    assertEquals(Foo.FOO.xxx, "xxx")
+    assertEquals(Foo.FOO.toString(), "FOO")
+    assertEquals(Foo.valueOf("FOO").toString(), "FOO")
+    return "OK"
+}
+
--- js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
@@ -8927,6 +8927,11 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
             runTest("compiler/testData/codegen/box/enum/enumCompanionInit.kt");
         }
 
+        @TestMetadata("enumEntryMembers.kt")
+        public void testEnumEntryMembers() throws Exception {
+            runTest("compiler/testData/codegen/box/enum/enumEntryMembers.kt");
+        }
+
         @TestMetadata("enumEntryReferenceFromInnerClassConstructor1.kt")
         public void testEnumEntryReferenceFromInnerClassConstructor1() throws Exception {
             runTest("compiler/testData/codegen/box/enum/enumEntryReferenceFromInnerClassConstructor1.kt");
