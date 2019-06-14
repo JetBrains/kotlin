769ccde43ba1c0f2f5cb850e6f66fe4e5004d924
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2019-06-14 14:08:12 +0300

    Closure over dependencies in multiPlatformSetup

--- idea/tests/org/jetbrains/kotlin/idea/multiplatform/multiPlatformSetup.kt
+++ idea/tests/org/jetbrains/kotlin/idea/multiplatform/multiPlatformSetup.kt
@@ -28,6 +28,7 @@ import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.platform.js.isJs
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.projectModel.*
+import org.jetbrains.kotlin.types.typeUtil.closure
 import java.io.File
 
 // allows to configure a test mpp project
@@ -74,7 +75,7 @@ fun AbstractMultiModuleTest.doSetup(projectModel: ProjectResolveModel) {
     }.toMap()
 
     for ((resolveModule, ideaModule) in resolveModulesToIdeaModules.entries) {
-        resolveModule.dependencies.forEach {
+        resolveModule.dependencies.closure { it.to.dependencies }.forEach {
             when (val to = it.to) {
                 FullJdk -> ConfigLibraryUtil.configureSdk(ideaModule, PluginTestCaseBase.addJdk(testRootDisposable) {
                     PluginTestCaseBase.jdk(TestJdkKind.FULL_JDK)
