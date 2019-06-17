5f896936ae7ee86efdc92e5555b6099e89748bac
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-06-17 11:05:53 +0300

    Fix broken test (related to "Convert to lazy property")

--- idea/testData/multiModuleQuickFix/mayBeConstantWithActual/jvm/x.kt
+++ idea/testData/multiModuleQuickFix/mayBeConstantWithActual/jvm/x.kt
@@ -2,6 +2,7 @@
 // ACTION: Add getter
 // ACTION: Convert property initializer to getter
 // ACTION: Convert property to function
+// ACTION: Convert to lazy property
 // ACTION: Specify type explicitly
 
 actual val x<caret> = 42
