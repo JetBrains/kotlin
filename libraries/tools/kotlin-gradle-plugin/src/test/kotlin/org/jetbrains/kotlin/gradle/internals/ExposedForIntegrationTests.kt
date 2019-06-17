d693620cdadfb97b41a3257c6c1515faeff114d9
Sergey Igushkin <hotkeytlt@gmail.com>
2019-06-17 19:09:02 +0300

    Update Gradle module metadata warnings: don't warn when it's enabled
    Also add a suggestion to enable the module metadata publishing in
    Gradle 5.3+ projects that consume Gradle module metadata by default but
    don't produce it unless explicitly enabled.
    
    Issue #KT-31023 Fixed

---
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/internals/ExposedForIntegrationTests.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.internals
+
+const val GRADLE_NO_METADATA_WARNING =
+    org.jetbrains.kotlin.gradle.plugin.mpp.KotlinMultiplatformPlugin.GRADLE_NO_METADATA_WARNING
