5cf4a1b6a59f86c036d23c1c1525a9002c1b30b8
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-17 14:05:57 +0200

    [platform] reducing allocations in CStyleCommentBlock
    intellij-monorepo: 57ee1c562513520b9cc64e2f1482b50b4c58fe07

--- intellij-community/platform/lang-impl/src/com/intellij/formatting/blocks/CStyleComment.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/formatting/blocks/CStyleComment.kt
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.formatting.blocks
 
 import com.intellij.formatting.Block
@@ -22,86 +8,59 @@ import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.formatter.common.AbstractBlock
 
-
 class CStyleCommentBlock(comment: ASTNode, private val indent: Indent?): AbstractBlock(comment, null, null) {
-
-  private val lines by lazy { lineBlocks() }
-  val isCommentFormattable: Boolean by lazy {
-    lines.drop(1).all { it.text.startsWith("*") }
-  }
-
   val spacing: Spacing?
-    get() = if (isCommentFormattable) null else Spacing.getReadOnlySpacing()
+    get() = if (ranges.isEmpty()) Spacing.getReadOnlySpacing() else null
 
-  override fun getSpacing(child1: Block?, child2: Block): Spacing? {
-    val isLicenseComment = child1 == null && node.prev() == null
-    if (isLicenseComment) {
-      return Spacing.getReadOnlySpacing()
-    }
-
-    return child2.getSpacing(null, this)
-  }
+  override fun getSpacing(child1: Block?, child2: Block): Spacing? =
+    if (child1 == null && node.prev() == null) Spacing.getReadOnlySpacing()  // a file header comment
+    else child2.getSpacing(null, this)
 
   override fun getIndent(): Indent? = indent
 
   override fun buildChildren(): List<Block> {
-    if (!isCommentFormattable) return emptyList()
+    val ranges = ranges
+    if (ranges.isEmpty()) return emptyList()
 
-    return lines.map {
-      val text = it.text
-      val indent = when {
-        !isCommentFormattable -> null
-        text.startsWith("/*") -> Indent.getNoneIndent()
-        else -> Indent.getSpaceIndent(1)
-      }
-      TextLineBlock(text, it.textRange, null, indent, null)
+    val children = ArrayList<Block>(ranges.size)
+    val nodeStart = node.startOffset
+    for (i in 0 until ranges.size) {
+      val indent = if (i == 0) Indent.getNoneIndent() else Indent.getSpaceIndent(1)
+      children += TextLineBlock(ranges[i].shiftRight(nodeStart), null, indent, null)
     }
+    return children
   }
 
+  override fun isLeaf(): Boolean = ranges.isEmpty()
 
-  private fun lineBlocks(): List<LineInfo> {
-    return node.text
-        .mapIndexed { index, char -> index to char }
-        .split { it.second == '\n' }
-        .mapNotNull {
-          val block = it.dropWhile { Character.isWhitespace(it.second) }
-          if (block.isEmpty()) return@mapNotNull null
+  private val ranges: List<TextRange> by lazy {
+    val result = mutableListOf<TextRange>()
 
-          val text = block.map { it.second }.joinToString("").trimEnd()
-
-          val startOffset = node.startOffset + block.first().first
-          val range = TextRange(startOffset, startOffset + text.length)
-
-          LineInfo(text, range)
+    val text = node.text
+    var start = 0
+    for (i in 2 until text.length) {
+      val c = text[i]
+      // looking for a line start (first non-WS character)
+      if (start < 0) {
+        if (!Character.isWhitespace(c)) {
+          if (c != '*') {
+            return@lazy emptyList<TextRange>()  // a line doesn't start with '*'
+          }
+          start = i
         }
-  }
-
-  override fun isLeaf(): Boolean = !isCommentFormattable
-
-}
-
-
-private class LineInfo(val text: String, val textRange: TextRange)
-
-
-fun <T> List<T>.split(predicate: (T) -> Boolean): List<List<T>> {
-  if (indices.isEmpty()) return listOf()
-  val result = mutableListOf<List<T>>()
-
-  val current = mutableListOf<T>()
-  for (e in this) {
-    if (predicate(e)) {
-      result.add(current.toList())
-      current.clear()
+      }
+      // looking for a line end ('\n' or end of the comment)
+      else if (i == text.length - 1) {
+        result += TextRange(start, text.length)
+      }
+      else if (c == '\n') {
+        var end = i - 1
+        while (Character.isWhitespace(text[end])) --end
+        result += TextRange(start, end + 1)
+        start = -1
+      }
     }
-    else {
-      current.add(e)
-    }
-  }
 
-  if (current.isNotEmpty()) {
-    result.add(current)
+    result
   }
-
-  return result
 }
