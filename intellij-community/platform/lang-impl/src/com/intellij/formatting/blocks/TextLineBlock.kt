354e73ff8326500c325b22abf35b068ab6372c7f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-06-17 14:05:57 +0200

    IDEA-CR-48514: [platform] reducing allocations in CStyleCommentBlock
    intellij-monorepo: 3e729c86d8d5dba3154cb9c5b5c8846602f3b346

--- intellij-community/platform/lang-impl/src/com/intellij/formatting/blocks/TextLineBlock.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/formatting/blocks/TextLineBlock.kt
@@ -1,34 +1,11 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.formatting.blocks
 
 import com.intellij.formatting.*
 import com.intellij.openapi.util.TextRange
 
-class TextLineBlock(
-  val text: String,
-  private val textRange: TextRange,
-  private val alignment: Alignment?,
-  private val indent: Indent?,
-  val spacing: Spacing?
-) : Block {
-
-  override fun getTextRange(): TextRange {
-    return textRange
-  }
+class TextLineBlock(private val range: TextRange, private val alignment: Alignment?, private val indent: Indent?, val spacing: Spacing?) : Block {
+  override fun getTextRange(): TextRange = range
 
   override fun getSubBlocks(): List<Block> = emptyList()
 
@@ -40,16 +17,9 @@ class TextLineBlock(
 
   override fun getSpacing(child1: Block?, child2: Block): Spacing? = spacing
 
-  override fun getChildAttributes(newChildIndex: Int): ChildAttributes {
-    throw UnsupportedOperationException("Should not be called")
-  }
+  override fun getChildAttributes(newChildIndex: Int): ChildAttributes = throw UnsupportedOperationException("Should not be called")
 
   override fun isIncomplete(): Boolean = false
 
   override fun isLeaf(): Boolean = true
-
-  override fun toString(): String {
-    return "TextLineBlock(text='$text', textRange=$textRange)"
-  }
-
 }
