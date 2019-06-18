311964acea400ced2b335c666cac92a42d3318f0
Ivan Cilcic <cilcic.v22@gmail.com>
2019-06-18 17:41:48 +0300

    Made some code refactoring to make it more readable

--- compiler/fir/lightTree/src/org/jetbrains/kotlin/fir/lightTree/fir/ValueParameter.kt
+++ compiler/fir/lightTree/src/org/jetbrains/kotlin/fir/lightTree/fir/ValueParameter.kt
@@ -5,7 +5,50 @@
 
 package org.jetbrains.kotlin.fir.lightTree.fir
 
+import org.jetbrains.kotlin.fir.declarations.FirProperty
 import org.jetbrains.kotlin.fir.declarations.FirValueParameter
+import org.jetbrains.kotlin.fir.declarations.impl.FirDefaultPropertyGetter
+import org.jetbrains.kotlin.fir.declarations.impl.FirDefaultPropertySetter
+import org.jetbrains.kotlin.fir.declarations.impl.FirMemberPropertyImpl
+import org.jetbrains.kotlin.fir.lightTree.ClassNameUtil
+import org.jetbrains.kotlin.fir.lightTree.fir.modifier.MemberModifier
 import org.jetbrains.kotlin.fir.lightTree.fir.modifier.Modifier
+import org.jetbrains.kotlin.fir.lightTree.fir.modifier.PlatformModifier
+import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
 
-class ValueParameter(val isVal: Boolean, val isVar: Boolean, val modifier: Modifier, val firValueParameter: FirValueParameter)
+class ValueParameter(
+    val isVal: Boolean,
+    val isVar: Boolean,
+    val modifier: Modifier,
+    val firValueParameter: FirValueParameter
+) {
+    fun isValOrVar(): Boolean {
+        return isVal || isVar
+    }
+
+    fun toFirProperty(): FirProperty {
+        val name = this.firValueParameter.name
+        val type = this.firValueParameter.returnTypeRef
+
+        return FirMemberPropertyImpl(
+            this.firValueParameter.session,
+            null,
+            FirPropertySymbol(ClassNameUtil.callableIdForName(name)),
+            name,
+            modifier.visibilityModifier.toVisibility(),
+            modifier.inheritanceModifier?.toModality(),
+            modifier.platformModifier == PlatformModifier.EXPECT,
+            modifier.platformModifier == PlatformModifier.ACTUAL,
+            isOverride = modifier.memberModifier == MemberModifier.OVERRIDE,
+            isConst = false,
+            isLateInit = false,
+            receiverTypeRef = null,
+            returnTypeRef = type,
+            isVar = this.isVar,
+            initializer = null,
+            getter = FirDefaultPropertyGetter(this.firValueParameter.session, null, type, modifier.visibilityModifier.toVisibility()),
+            setter = FirDefaultPropertySetter(this.firValueParameter.session, null, type, modifier.visibilityModifier.toVisibility()),
+            delegate = null
+        ).apply { annotations += this@ValueParameter.firValueParameter.annotations }
+    }
+}
