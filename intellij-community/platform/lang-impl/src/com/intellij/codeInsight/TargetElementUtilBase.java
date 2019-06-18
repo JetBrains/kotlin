87540b8ab207a7c73b9b107b1dd4a2cc4e8a554b
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-06-18 12:55:52 +0300

    deprecate and schedule for removal com.intellij.codeInsight.TargetElementUtilBase#inVirtualSpace
    intellij-monorepo: 2b0d868c869e3e34a032f4ebdc1f76eb9fd59629

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/TargetElementUtilBase.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/TargetElementUtilBase.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight;
 
 import com.intellij.openapi.editor.Document;
@@ -21,6 +7,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.SearchScope;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -59,6 +46,8 @@ public abstract class TargetElementUtilBase {
     return TargetElementUtil.adjustOffset(file, document, offset);
   }
 
+  @ApiStatus.ScheduledForRemoval(inVersion = "2020.1")
+  @Deprecated
   public static boolean inVirtualSpace(@NotNull Editor editor, int offset) {
     return TargetElementUtil.inVirtualSpace(editor, offset);
   }
