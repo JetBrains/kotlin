2663161f1857f176bb1942c18a7ebd9d4e14dd54
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-06-18 11:52:51 +0900

    Add "Convert to notNull delegate" quick fix for INAPPLICABLE_LATEINIT_MODIFIER
    #KT-12515 Fixed

---
+++ idea/testData/quickfix/convertLateinitPropertyToNotNullDelegate/basic2.kt
@@ -0,0 +1,3 @@
+// "Convert to notNull delegate" "true"
+// WITH_RUNTIME
+<caret>lateinit var x: Int
---
+++ idea/testData/quickfix/convertLateinitPropertyToNotNullDelegate/nullablePrimitive.kt
@@ -0,0 +1,7 @@
+// "Convert to notNull delegate" "false"
+// DISABLE-ERRORS
+// ACTION: Make internal
+// ACTION: Make not-nullable
+// ACTION: Make private
+// ACTION: Remove 'lateinit' modifier
+<caret>lateinit var x: Boolean?
