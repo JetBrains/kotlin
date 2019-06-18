6c383617431df1d257f53007445adfeaa18ddc42
Alexey Kudravtsev <cdr@intellij.com>
2019-06-18 14:56:17 +0300

    cleanup: remove unused fields, methods
    intellij-monorepo: 1cfc4582a822a8d8888a45e44388ebcb76454ee1

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/InlayHintsPassFactory.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/InlayHintsPassFactory.kt
@@ -8,55 +8,29 @@ import com.intellij.codeHighlighting.TextEditorHighlightingPassRegistrar
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
-import com.intellij.openapi.util.Key
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiManager
 
 class InlayHintsPassFactory : TextEditorHighlightingPassFactory, TextEditorHighlightingPassFactoryRegistrar {
   override fun registerHighlightingPassFactory(registrar: TextEditorHighlightingPassRegistrar, project: Project) {
     registrar.registerTextEditorHighlightingPass(this, null, null, false, -1)
   }
 
-  override fun createHighlightingPass(file: PsiFile, editor: Editor): TextEditorHighlightingPass? = createPass(file, editor)
-
+  override fun createHighlightingPass(file: PsiFile, editor: Editor): TextEditorHighlightingPass? {
+    if (editor.isOneLineMode) return null
+    val settings = ServiceManager.getService(InlayHintsSettings::class.java)
+    val language = file.language
+    val collectors = HintUtils.getHintProvidersForLanguage(language, file.project)
+      .mapNotNull { it.getCollectorWrapperFor(file, editor, language) }
+    return InlayHintsPass(file, collectors, editor, settings)
+  }
 
   companion object {
-    private val INLAY_PSI_MODIFICATION_STAMP = Key.create<Long>("inlay.hints.stamp")
-
     fun forceHintsUpdateOnNextPass() {
-      for (editor in EditorFactory.getInstance().allEditors) {
-        editor.putUserData<Long>(INLAY_PSI_MODIFICATION_STAMP, null)
-      }
-      restartDaemon()
-    }
-
-    @JvmStatic
-    fun restartDaemon() {
       ProjectManager.getInstance().openProjects.forEach { project ->
-        val psiManager = PsiManager.getInstance(project)
-        val daemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(project)
-        val fileEditorManager = FileEditorManager.getInstance(project)
-
-        fileEditorManager.selectedFiles.forEach { file ->
-          psiManager.findFile(file)?.let { daemonCodeAnalyzer.restart(it) }
-        }
+        DaemonCodeAnalyzer.getInstance(project).restart()
       }
-    }
-
-    fun createPass(file: PsiFile, editor: Editor, forceRefresh: Boolean = false): InlayHintsPass? {
-      if (editor.isOneLineMode) return null
-      val currentStamp = file.manager.modificationTracker.modificationCount
-      val savedStamp = editor.getUserData<Long>(INLAY_PSI_MODIFICATION_STAMP)
-      if (!forceRefresh && savedStamp != null && savedStamp == currentStamp) return null
-      val settings = ServiceManager.getService(InlayHintsSettings::class.java)
-      val language = file.language
-      val collectors = HintUtils.getHintProvidersForLanguage(language, file.project)
-        .mapNotNull { it.getCollectorWrapperFor(file, editor, language) }
-      return InlayHintsPass(file, collectors, editor, settings)
     }
   }
 }
