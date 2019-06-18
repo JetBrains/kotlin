de02fcb6263a536175a2271447e726af39f00ef7
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2019-06-18 15:28:56 +0300

    it doesn't need to override second invoke(..) for FUS, as they both are called during refactoring

--- idea/src/org/jetbrains/kotlin/idea/refactoring/pullUp/KotlinPullUpHandler.kt
+++ idea/src/org/jetbrains/kotlin/idea/refactoring/pullUp/KotlinPullUpHandler.kt
@@ -116,11 +116,6 @@ class KotlinPullUpHandler : AbstractPullPushMembersHandler(
         }
     }
 
-    override fun invoke(project: Project, elements: Array<out PsiElement>, dataContext: DataContext?) {
-        super.invoke(project, elements, dataContext)
-        KotlinFUSLogger.log(FUSEventGroups.Refactoring, this.javaClass.simpleName)
-    }
-
     override fun invoke(project: Project, editor: Editor, file: PsiFile, dataContext: DataContext?) {
         super.invoke(project, editor, file, dataContext)
         KotlinFUSLogger.log(FUSEventGroups.Refactoring, this.javaClass.simpleName)
