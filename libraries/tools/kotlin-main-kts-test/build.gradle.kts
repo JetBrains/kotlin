ca37cb0028e4e205fb79f670c986442f4f8a2411
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2019-06-20 02:48:46 +0300

    Build: Remove jps hacks from kotlin-main-kts-test

--- libraries/tools/kotlin-main-kts-test/build.gradle.kts
+++ libraries/tools/kotlin-main-kts-test/build.gradle.kts
@@ -6,18 +6,9 @@ plugins {
 }
 
 dependencies {
-    if (!kotlinBuildProperties.isInJpsBuildIdeaSync) {
-        testCompile(projectRuntimeJar(":kotlin-main-kts")) // runtimeJar needed to for proper dependency on the jar with relocations
-    }
-
+    testCompile(project(":kotlin-main-kts"))
     testCompile(project(":kotlin-scripting-jvm-host-embeddable"))
     testCompile(commonDep("junit"))
-    compileOnly("org.apache.ivy:ivy:2.4.0") // for jps/pill
-
-    if (kotlinBuildProperties.isInJpsBuildIdeaSync) {
-        testCompile(project(":kotlin-scripting-jvm-host"))
-        testCompile(project(":kotlin-main-kts"))
-    }
 }
 
 sourceSets {
