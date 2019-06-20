39bdf34b9f6a705b6170cdfffdef9ebdd17f1547
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-06-20 20:56:56 +0300

    Use common implementation of fastLog2 based on countLeadingZeroBits

--- libraries/stdlib/js/src/kotlin/random/PlatformRandom.kt
+++ libraries/stdlib/js/src/kotlin/random/PlatformRandom.kt
@@ -11,17 +11,6 @@ internal actual fun defaultPlatformRandom(): Random =
     Random(js("(Math.random() * Math.pow(2, 32)) | 0").unsafeCast<Int>())
 
 
-internal actual fun fastLog2(value: Int): Int {
-    // TODO: not so fast, make faster
-    var v = value
-    var log = -1
-    while (v != 0) {
-        v = v.ushr(1)
-        log++
-    }
-    return log
-}
-
 private val INV_2_26: Double = 2.0.pow(-26)
 private val INV_2_53: Double = 2.0.pow(-53)
 internal actual fun doubleFromParts(hi26: Int, low27: Int): Double =
