5a01ad799e3675ab694deb1272e2c0c488140093
Igor Chevdar <igor.chevdar@jetbrains.com>
2019-06-24 17:35:53 +0300

    [Serializer] Bumped ABI version

--- compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinAbiVersion.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinAbiVersion.kt
@@ -22,7 +22,7 @@ fun String.parseKonanAbiVersion(): KotlinAbiVersion {
 
 data class KotlinAbiVersion(val version: Int) {
     companion object {
-        val CURRENT = KotlinAbiVersion(13)
+        val CURRENT = KotlinAbiVersion(14)
     }
     override fun toString() = "$version"
 }
