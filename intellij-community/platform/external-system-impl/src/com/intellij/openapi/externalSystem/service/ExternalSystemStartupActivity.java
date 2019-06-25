2a5e84ebb942e1a8943b8ff04060dd54dc535f15
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-06-25 16:27:59 +0200

    cleanup, update start-up visualizer deps
    intellij-monorepo: 544221ed0c69bf17534796a28752776b59aeb468

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ExternalSystemStartupActivity.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/ExternalSystemStartupActivity.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.service;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -31,17 +17,14 @@ import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.util.DisposeAwareRunnable;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * @author Denis Zhdanov
- */
-public class ExternalSystemStartupActivity implements StartupActivity, DumbAware {
-
+final class ExternalSystemStartupActivity implements StartupActivity, DumbAware {
   @Override
   public void runActivity(@NotNull final Project project) {
     ExternalProjectsManagerImpl.getInstance(project).init();
     if (ApplicationManager.getApplication().isUnitTestMode()) {
       return;
     }
+
     Runnable task = () -> {
       for (ExternalSystemManager<?, ?, ?, ?, ?> manager: ExternalSystemApiUtil.getAllManagers()) {
         if (manager instanceof StartupActivity) {
--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsManagerImpl.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/manage/ExternalProjectsManagerImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.service.project.manage;
 
 import com.intellij.openapi.Disposable;
@@ -147,7 +147,9 @@ public class ExternalProjectsManagerImpl implements ExternalProjectsManager, Per
   }
 
   public void init() {
-    if (isInitializationStarted.getAndSet(true)) return;
+    if (isInitializationStarted.getAndSet(true)) {
+      return;
+    }
     myWatcher.start();
 
     // load external projects data
