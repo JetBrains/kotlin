3172403060734f80a5176edda1adfe6d81a67fc5
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2019-06-26 11:27:04 +0700

    Parameter hints: proper capitalization
    intellij-monorepo: 5b88e9c8c9adbeeb01d46756094fb13f79b5090f

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/config/SingleLanguageInlayHintsConfigurable.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/config/SingleLanguageInlayHintsConfigurable.kt
@@ -23,7 +23,7 @@ class SingleLanguageInlayHintsConfigurable(project: Project, val language: Langu
     val parameterHintsProvider = InlayParameterHintsExtension.forLanguage(language)
     if (parameterHintsProvider != null) {
       val enabledForLanguage = ParameterNameHintsSettings.getInstance().isEnabledForLanguage(language)
-      options.add(HintProviderOption(oldParameterHintsKey, "Parameter Hints", true, enabledForLanguage))
+      options.add(HintProviderOption(oldParameterHintsKey, "Parameter hints", true, enabledForLanguage))
     }
     val keyToProvider = providers.associateBy { it.provider.key }
     val settingsWrappers = providers.map { it.toSettingsWrapper(settings, language) }
