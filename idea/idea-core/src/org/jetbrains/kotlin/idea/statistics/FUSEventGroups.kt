d972d50296753c61b73249711b5326e4d96d145c
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2019-06-27 21:43:57 +0300

    small fix of FUS J2K merge conflict

--- idea/idea-core/src/org/jetbrains/kotlin/idea/statistics/FUSEventGroups.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/statistics/FUSEventGroups.kt
@@ -8,7 +8,9 @@ package org.jetbrains.kotlin.idea.statistics
 /* Note: along with adding a group to this enum you should also add its GROUP_ID to plugin.xml and get it whitelisted
  * (see https://confluence.jetbrains.com/display/FUS/IntelliJ+Reporting+API).
  *
- * Default value for [events] parameter is intended for collectors which don't have yet a set of allowed values for FUS Whitelist
+ * Default value for [events] parameter is intended for collectors which
+ *    1. don't have yet a set of allowed values for FUS Whitelist
+ *    2. set of possible values is defined by enums in a feature's source code
  */
 enum class FUSEventGroups(groupIdSuffix: String, val events: Set<String> = setOf()) {
 
