9595c815f12b7ea0942da0341b9c23ea4031b357
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-06-28 15:36:29 +0200

    JVM IR: invent names for SAM wrapper classes
    Similarly to how it's done for callable references. This is needed for
    IrTypeMapper to be able to correctly compute the name of the SAM wrapper
    IrClass without resorting to the name of the underlying descriptor
    (computed in CodegenAnnotatingVisitor)

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InventNamesForLocalClasses.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InventNamesForLocalClasses.kt
@@ -11,9 +11,9 @@ import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.codegen.JvmCodegenUtil
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.*
-import org.jetbrains.kotlin.ir.expressions.IrFunctionReference
-import org.jetbrains.kotlin.ir.expressions.IrPropertyReference
+import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.symbols.IrFunctionSymbol
+import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
 import org.jetbrains.kotlin.ir.util.isAnonymousObject
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitor
 import org.jetbrains.kotlin.name.Name
@@ -158,6 +158,27 @@ class InventNamesForLocalClasses(private val context: JvmBackendContext) : FileL
             // IrAnonymousInitializer is not an IrDeclaration, so we need to manually make all its children aware that they're local
             // and might need new invented names.
             declaration.acceptChildren(this, data.makeLocal())
+        }
+
+        override fun visitTypeOperator(expression: IrTypeOperatorCall, data: Data) {
+            if (expression.operator == IrTypeOperator.SAM_CONVERSION) {
+                val invokable = expression.argument
+                // Function references (even those that are transformed into SAM wrappers) are handled in visitFunctionReference.
+                if (invokable !is IrFunctionReference && !(invokable is IrBlock && invokable.statements.last() is IrFunctionReference)) {
+                    val superClass = expression.typeOperandClassifier.owner as IrClass
+                    val superClassName = superClass.fqNameWhenAvailable!!.pathSegments().joinToString("_") { it.toString() }
+
+                    // TODO: consider dropping "sam\$$superClassName$0" from the name here.
+                    // It's only here to give resemblance of the name generated for SAM wrappers by the old backend. The exact logic
+                    // of the old backend is somewhat difficult to emulate consistently, see `SamWrapperCodegen.getWrapperName`.
+                    val internalName = inventName(Name.identifier("sam\$$superClassName$0"), data)
+                    context.putLocalClassInfo(expression, JvmBackendContext.LocalClassInfo(internalName))
+                    invokable.accept(this, data.withName(internalName))
+                    return
+                }
+            }
+
+            expression.acceptChildren(this, data)
         }
 
         override fun visitElement(element: IrElement, data: Data) {
