4c880a699f3d7f6ca78a3953e1687d308494134e
Vassiliy.Kudryashov <Vassiliy.Kudryashov@jetbrains.com>
2019-06-30 00:12:05 +0300

    IDEA-216428 Debugger: hidden content (like Memory View) appears again after restarting of debugger session
    intellij-monorepo: 6150f0c83182eefc7a888585b4a28e1ea7036098

--- intellij-community/platform/lang-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/ui/layout/impl/RunnerContentUi.java
@@ -1562,10 +1562,12 @@ public class RunnerContentUi implements ContentUI, Disposable, CellTransform.Fac
 
   @Override
   public void minimize(final Content content, final CellTransform.Restore restore) {
+    getStateFor(content).setMinimizedInGrid(true);
     myManager.removeContent(content, false);
     saveUiState();
     updateTabsUI(false);
   }
+  
   public void restore(Content content) {
     final GridImpl grid = getGridFor(content, false);
     if (grid == null) {
