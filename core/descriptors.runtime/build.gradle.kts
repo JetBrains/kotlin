35dea296fa289146ab34563fe9f4e78447f12436
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-07-02 17:30:38 +0300

    Add ext.jvmTarget and ext.javaHome to core:descriptors.runtime
    After the following changes:
    * 290aded94f7d88706354640d156db5f32fabf2c4
    * 33de71f7920d0d7e9986145e584a38613856e551
    
    default values of jvmTarget and javaHome started to overwrite properties
    set through compileJava in core:descriptors.runtime

--- core/descriptors.runtime/build.gradle.kts
+++ core/descriptors.runtime/build.gradle.kts
@@ -24,15 +24,8 @@ sourceSets {
     "test" { projectDefault() }
 }
 
-val compileJava by tasks.getting(JavaCompile::class) {
-    sourceCompatibility = "1.6"
-    targetCompatibility = "1.6"
-}
-
-val compileKotlin by tasks.getting(KotlinCompile::class) {
-    kotlinOptions.jvmTarget = "1.6"
-    kotlinOptions.jdkHome = rootProject.extra["JDK_16"] as String
-}
+jvmTarget = "1.6"
+javaHome = rootProject.extra["JDK_16"] as String
 
 val generateTests by generator("org.jetbrains.kotlin.generators.tests.GenerateRuntimeDescriptorTestsKt")
 
