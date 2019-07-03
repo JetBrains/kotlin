9340bffd02ad5cf40ceb3d28a04dc4f209fc1d81
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2019-07-03 12:40:02 +0300

    Revert "Add ext.jvmTarget and ext.javaHome to core:descriptors.runtime"
    This reverts commit 35dea296fa289146ab34563fe9f4e78447f12436.

--- core/descriptors.runtime/build.gradle.kts
+++ core/descriptors.runtime/build.gradle.kts
@@ -24,8 +24,15 @@ sourceSets {
     "test" { projectDefault() }
 }
 
-jvmTarget = "1.6"
-javaHome = rootProject.extra["JDK_16"] as String
+val compileJava by tasks.getting(JavaCompile::class) {
+    sourceCompatibility = "1.6"
+    targetCompatibility = "1.6"
+}
+
+val compileKotlin by tasks.getting(KotlinCompile::class) {
+    kotlinOptions.jvmTarget = "1.6"
+    kotlinOptions.jdkHome = rootProject.extra["JDK_16"] as String
+}
 
 val generateTests by generator("org.jetbrains.kotlin.generators.tests.GenerateRuntimeDescriptorTestsKt")
 
