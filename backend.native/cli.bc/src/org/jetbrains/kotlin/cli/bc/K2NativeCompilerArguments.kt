3fec182cab833d00e6b7e3e3b39ae501a4ba81f2
LepilkinaElena <Elena.Lepilkina@jetbrains.com>
2019-07-04 11:42:55 +0300

    Fixed compiler options with value expected to be path to cover all characters in pathes (#3148)

--- backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2NativeCompilerArguments.kt
+++ backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2NativeCompilerArguments.kt
@@ -35,7 +35,7 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
     @Argument(value="-include-binary", deprecatedName = "-includeBinary", shortName = "-ib", valueDescription = "<path>", description = "Pack external binary within the klib")
     var includeBinaries: Array<String>? = null
 
-    @Argument(value = "-library", shortName = "-l", valueDescription = "<path>", description = "Link with the library")
+    @Argument(value = "-library", shortName = "-l", valueDescription = "<path>", description = "Link with the library", delimiter = "")
     var libraries: Array<String>? = null
 
     @Argument(value = "-library-version", shortName = "-lv", valueDescription = "<version>", description = "Set library version")
@@ -53,7 +53,8 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
     @Argument(value="-module-name", deprecatedName = "-module_name", valueDescription = "<name>", description = "Specify a name for the compilation module")
     var moduleName: String? = null
 
-    @Argument(value = "-native-library", deprecatedName = "-nativelibrary", shortName = "-nl", valueDescription = "<path>", description = "Include the native bitcode library")
+    @Argument(value = "-native-library", deprecatedName = "-nativelibrary", shortName = "-nl",
+            valueDescription = "<path>", description = "Include the native bitcode library", delimiter = "")
     var nativeLibraries: Array<String>? = null
 
     @Argument(value = "-nodefaultlibs", description = "Don't link the libraries from dist/klib automatically")
@@ -120,7 +121,8 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
             value = "-Xexport-library",
             valueDescription = "<path>",
             description = "Path to the library to be included into produced framework API\n" +
-                    "Must be the path of a library passed with '-library'"
+                    "Must be the path of a library passed with '-library'",
+            delimiter = ""
     )
     var exportedLibraries: Array<String>? = null
 
@@ -183,7 +185,8 @@ class K2NativeCompilerArguments : CommonCompilerArguments() {
     @Argument(
             value = "-Xlibrary-to-cover",
             valueDescription = "<path>",
-            description = "Path to library that should be covered."
+            description = "Path to library that should be covered.",
+            delimiter = ""
     )
     var coveredLibraries: Array<String>? = null
 
