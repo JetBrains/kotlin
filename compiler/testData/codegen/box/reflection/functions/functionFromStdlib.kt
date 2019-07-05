7573fcde53c47e12525094d9dcd2ee2c3be3baa2
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-07-05 19:54:50 +0300

    Demote new experimental capitalize overloads in overload resolution
    Revert "Fix method reference ambiguity in test after introducing 'capitalize' overload"
    This reverts commit fe9bd9ee

--- compiler/testData/codegen/box/reflection/functions/functionFromStdlib.kt
+++ compiler/testData/codegen/box/reflection/functions/functionFromStdlib.kt
@@ -7,6 +7,5 @@ import kotlin.reflect.jvm.isAccessible
 fun doStuff(fn: KFunction1<String, String>) = fn.call("oK")
 
 fun box(): String {
-    val method: KFunction1<String, String> = String::capitalize
-    return doStuff(method.apply { isAccessible = true })
+    return doStuff(String::capitalize.apply { isAccessible = true })
 }
