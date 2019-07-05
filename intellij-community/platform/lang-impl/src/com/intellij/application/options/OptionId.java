e87506674af1781c08cf66e21ceeca5dfe61b07f
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2019-07-05 15:02:06 +0300

    IDEA-172513: Add OptionId to remove Show Visibility Icons from Project View
    intellij-monorepo: 09079adc8625e832bc0ecaf9b6f0d8a7ebcd5b03

--- intellij-community/platform/lang-impl/src/com/intellij/application/options/OptionId.java
+++ intellij-community/platform/lang-impl/src/com/intellij/application/options/OptionId.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.application.options;
 
@@ -27,4 +13,5 @@ public class OptionId {
   public static final OptionId COMPLETION_SMART_TYPE = new OptionId();
   public static final OptionId AUTOCOMPLETE_ON_BASIC_CODE_COMPLETION = new OptionId();
   public static final OptionId SHOW_PARAMETER_NAME_HINTS_ON_COMPLETION = new OptionId();
+  public static final OptionId PROJECT_VIEW_SHOW_VISIBILITY_ICONS = new OptionId();
 }
