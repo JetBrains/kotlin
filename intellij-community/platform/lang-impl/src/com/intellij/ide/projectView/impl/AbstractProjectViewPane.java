cd3da519923b3510d56bb53fc372c699f21f2346
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-07-09 15:45:59 +0300

    dnd in changes view may show 1 file, not 1 file(s)
    intellij-monorepo: 915ab25373cdbbb1f0b7c6e1a477dcc670cac303

--- intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/AbstractProjectViewPane.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/AbstractProjectViewPane.java
@@ -30,6 +30,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.*;
 import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.openapi.wm.ToolWindowId;
@@ -754,6 +755,7 @@ public abstract class AbstractProjectViewPane implements DataProvider, Disposabl
       });
     }
 
+    // copy/paste from com.intellij.ide.dnd.aware.DnDAwareTree.createDragImage
     @Nullable
     @Override
     public Pair<Image, Point> createDraggedImage(DnDAction action, Point dragOrigin, @NotNull DnDDragStartBean bean) {
@@ -762,7 +764,7 @@ public abstract class AbstractProjectViewPane implements DataProvider, Disposabl
 
       final int count = paths.length;
 
-      final JLabel label = new JLabel(String.format("%s item%s", count, count == 1 ? "" : "s"));
+      final JLabel label = new JLabel(count + " " + StringUtil.pluralize("item", count));
       label.setOpaque(true);
       label.setForeground(myTree.getForeground());
       label.setBackground(myTree.getBackground());
