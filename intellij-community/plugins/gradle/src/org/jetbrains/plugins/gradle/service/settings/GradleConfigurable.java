f2867c1673ee0b60cea3e34ac147af051266770c
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-07-10 14:16:55 +0300

    [Gradle] IDEA-217985 Incorrect redirection to Gradle settings
    intellij-monorepo: 792019b41e0599843e4071f262783d970f1ef1f3

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/service/settings/GradleConfigurable.java
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/service/settings/GradleConfigurable.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.service.settings;
 
 import com.intellij.openapi.externalSystem.service.settings.AbstractExternalSystemConfigurable;
@@ -31,7 +17,9 @@ import org.jetbrains.plugins.gradle.util.GradleConstants;
  */
 public class GradleConfigurable extends AbstractExternalSystemConfigurable<GradleProjectSettings, GradleSettingsListener, GradleSettings> {
 
-  @NonNls public static final String HELP_TOPIC = "reference.settingsdialog.project.gradle";
+  public static final String DISPLAY_NAME = GradleConstants.SYSTEM_ID.getReadableName();
+  public static final String ID = "reference.settingsdialog.project.gradle";
+  @NonNls public static final String HELP_TOPIC = ID;
 
   public GradleConfigurable(@NotNull Project project) {
     super(project, GradleConstants.SYSTEM_ID);
@@ -58,7 +46,7 @@ public class GradleConfigurable extends AbstractExternalSystemConfigurable<Gradl
   @NotNull
   @Override
   public String getId() {
-    return getHelpTopic();
+    return ID;
   }
 
   @NotNull
