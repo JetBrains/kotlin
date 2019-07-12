76704c87cb3ad6d1377dd128a980393dc196da80
Max Medvedev <maxim.medvedev@jetbrains.com>
2019-07-12 14:44:44 +0300

    KT-28858 provide empty symbol table for KNM files

---
+++ ide/appcode-native/src/org/jetbrains/konan/resolve/konan/KonanKNMSymbolTableProvider.kt
@@ -0,0 +1,29 @@
+package org.jetbrains.konan.resolve.konan
+
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.PsiTreeChangeEventImpl
+import com.jetbrains.cidr.lang.preprocessor.OCInclusionContext
+import com.jetbrains.cidr.lang.symbols.symtable.ContextSignature
+import com.jetbrains.cidr.lang.symbols.symtable.FileSymbolTable
+import com.jetbrains.cidr.lang.symbols.symtable.SymbolTableProvider
+import org.jetbrains.kotlin.ide.konan.decompiler.KotlinNativeMetaFileType
+
+class KonanKNMSymbolTableProvider : SymbolTableProvider() {
+    override fun onOutOfCodeBlockModification(p0: Project, p1: PsiFile?) {}
+
+    override fun isOutOfCodeBlockChange(event: PsiTreeChangeEventImpl): Boolean = false
+
+    override fun calcTableUsingPSI(psiFile: PsiFile, virtualFile: VirtualFile, context: OCInclusionContext): FileSymbolTable = empty(virtualFile)
+
+    private fun empty(virtualFile: VirtualFile) = FileSymbolTable(virtualFile, ContextSignature())
+
+    override fun isSource(psiFile: PsiFile): Boolean = psiFile.virtualFile.fileType is KotlinNativeMetaFileType
+
+    override fun isSource(project: Project, virtualFile: VirtualFile, lazyFileType: Lazy<FileType>): Boolean =
+        lazyFileType.value == KotlinNativeMetaFileType
+
+    override fun calcTable(virtualFile: VirtualFile, context: OCInclusionContext): FileSymbolTable = empty(virtualFile)
+}
