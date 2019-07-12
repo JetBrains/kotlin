6058d9a77cd0b25d9084fc5c04ab68e64689035c
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-07-12 16:05:06 +0300

    annotate with @ApiStatus.OverrideOnly
    intellij-monorepo: fee7ddfbfbca91d983d21f8aac0cef3be2f8b1f2

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/CustomImplementationFileBasedIndexExtension.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/CustomImplementationFileBasedIndexExtension.java
@@ -6,10 +6,12 @@
 package com.intellij.util.indexing;
 
 import com.intellij.util.indexing.impl.IndexStorage;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
+@ApiStatus.OverrideOnly
 public interface CustomImplementationFileBasedIndexExtension<K, V> {
   @NotNull
   UpdatableIndex<K, V, FileContent> createIndexImplementation(@NotNull FileBasedIndexExtension<K, V> extension,
