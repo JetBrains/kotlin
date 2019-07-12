6963424dfc27ec2c8cbe790081aad3619ab91c00
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2019-07-12 14:30:37 +0300

    psi2ir: Empty 'if' branches have type 'kotlin.Unit'

--- compiler/testData/ir/irText/expressions/ifElseIf.kt
+++ compiler/testData/ir/irText/expressions/ifElseIf.kt
@@ -1,4 +1,19 @@
 fun test(i: Int) =
-        if (i > 0) 1
-        else if (i < 0) -1
-        else 0
+    if (i > 0) 1
+    else if (i < 0) -1
+    else 0
+
+fun testEmptyBranches1(flag: Boolean) {
+    if (flag) ; else true
+    if (flag) true else;
+}
+
+fun testEmptyBranches2(flag: Boolean) {
+    if (flag) {} else true
+    if (flag) true else {}
+}
+
+fun testEmptyBranches3(flag: Boolean) {
+    if (flag)
+    else true
+}
