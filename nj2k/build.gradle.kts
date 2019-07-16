5490687d10dd4d970fa1647f4025ee7ffd32d309
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2019-07-16 14:31:46 +0300

    New J2K: remove unused dependencies from nj2k modules

--- nj2k/build.gradle.kts
+++ nj2k/build.gradle.kts
@@ -11,18 +11,13 @@ dependencies {
     compile(kotlinStdlib())
     compile(project(":idea:idea-core"))
     compile(project(":j2k"))
-    compile(project(":compiler:frontend"))
-    compile(project(":compiler:frontend.java"))
-    compile(project(":compiler:light-classes"))
-    compile(project(":compiler:util"))
-
+    compile(project(":compiler:psi"))
     compileOnly(intellijCoreDep()) { includeJars("intellij-core", "guava", rootProject = rootProject) }
     compileOnly(intellijDep()) { includeJars("platform-api", "platform-impl", rootProject = rootProject) }
 
     Platform[191].orLower {
         compileOnly(intellijDep()) { includeJars("java-impl") }
     }
-
     Platform[192].orHigher {
         compileOnly(intellijPluginDep("java")) { includeJars("java-api", "java-impl") }
         testCompileOnly(intellijPluginDep("java")) { includeJars("java-api", "java-impl") }
@@ -34,44 +29,16 @@ dependencies {
     testCompile(projectTests(":idea"))
     testCompile(project(":nj2k:nj2k-services"))
     testCompile(projectTests(":idea:idea-test-framework"))
-    testCompile(project(":compiler:light-classes"))
     testCompile(project(":kotlin-test:kotlin-test-junit"))
     testCompile(commonDep("junit:junit"))
 
     testCompileOnly(intellijDep())
 
-    testCompile(project(":idea:idea-native")) { isTransitive = false }
-    testCompile(project(":idea:idea-gradle-native")) { isTransitive = false }
-
-    testRuntime(project(":kotlin-native:kotlin-native-library-reader")) { isTransitive = false }
-    testRuntime(project(":kotlin-native:kotlin-native-utils")) { isTransitive = false }
-    testRuntime(project(":plugins:kapt3-idea")) { isTransitive = false }
     testRuntime(project(":idea:idea-jvm"))
-    testRuntime(project(":idea:idea-android"))
-    testRuntime(project(":plugins:android-extensions-ide"))
     testRuntime(project(":sam-with-receiver-ide-plugin"))
     testRuntime(project(":allopen-ide-plugin"))
     testRuntime(project(":noarg-ide-plugin"))
-    testRuntime(project(":kotlin-scripting-idea"))
     testRuntime(project(":kotlinx-serialization-ide-plugin"))
-    testRuntime(intellijPluginDep("properties"))
-    testRuntime(intellijPluginDep("gradle"))
-    testRuntime(intellijPluginDep("Groovy"))
-    testRuntime(intellijPluginDep("coverage"))
-    Ide.IJ {
-        testRuntime(intellijPluginDep("maven"))
-    }
-    testRuntime(intellijPluginDep("android"))
-    testRuntime(intellijPluginDep("smali"))
-    testRuntime(intellijPluginDep("junit"))
-    testRuntime(intellijPluginDep("testng"))
-    testRuntime(intellijPluginDep("IntelliLang"))
-    testRuntime(intellijPluginDep("testng"))
-    testRuntime(intellijPluginDep("copyright"))
-    testRuntime(intellijPluginDep("properties"))
-    testRuntime(intellijPluginDep("java-i18n"))
-    testRuntime(intellijPluginDep("java-decompiler"))
-    testRuntime(project(":plugins:kapt3-idea")) { isTransitive = false }
 }
 
 sourceSets {
--- nj2k/nj2k-services/build.gradle.kts
+++ nj2k/nj2k-services/build.gradle.kts
@@ -5,70 +5,18 @@ plugins {
 
 
 dependencies {
-    testRuntime(intellijDep())
-
     compileOnly(project(":kotlin-reflect-api"))
-    compile(project(":core:descriptors"))
-    compile(project(":core:descriptors.jvm"))
-    compile(project(":compiler:backend"))
-    compile(project(":compiler:cli-common"))
-    compile(project(":compiler:frontend"))
-    compile(project(":compiler:frontend.common"))
-    compile(project(":compiler:frontend.java"))
-    compile(project(":js:js.frontend"))
-    compile(project(":js:js.serializer"))
-    compile(project(":compiler:light-classes"))
-    compile(project(":compiler:util"))
-    compile(project(":kotlin-build-common"))
-    compile(project(":daemon-common"))
-    compile(projectRuntimeJar(":kotlin-daemon-client"))
-    compile(project(":kotlin-compiler-runner")) { isTransitive = false }
-    compile(project(":compiler:plugin-api"))
     compile(project(":j2k"))
     compile(project(":nj2k"))
     compile(project(":idea"))
-    compile(project(":idea:formatter"))
-    compile(project(":idea:fir-view"))
-    compile(project(":idea:idea-core"))
-    compile(project(":idea:ide-common"))
     compile(project(":idea:idea-jvm"))
-    compile(project(":idea:idea-jps-common"))
-    compile(project(":idea:kotlin-gradle-tooling"))
-    compile(project(":plugins:uast-kotlin"))
-    compile(project(":plugins:uast-kotlin-idea"))
-    compile(project(":kotlin-script-util")) { isTransitive = false }
-    compile(project(":kotlin-scripting-intellij"))
 
     compile(commonDep("org.jetbrains.kotlinx", "kotlinx-coroutines-core")) { isTransitive = false }
-
-    compileOnly(project(":kotlin-daemon-client"))
 
     compileOnly(intellijDep())
     Platform[192].orHigher {
         compileOnly(intellijPluginDep("java")) { includeJars("java-api", "java-impl") }
     }
-
-    compileOnly(commonDep("com.google.code.findbugs", "jsr305"))
-    compileOnly(intellijPluginDep("IntelliLang"))
-    compileOnly(intellijPluginDep("copyright"))
-    compileOnly(intellijPluginDep("properties"))
-    compileOnly(intellijPluginDep("java-i18n"))
-
-    testCompile(project(":kotlin-test:kotlin-test-junit"))
-    testCompile(projectTests(":compiler:tests-common"))
-    testCompile(projectTests(":idea:idea-test-framework")) { isTransitive = false }
-    testCompile(project(":idea:idea-jvm")) { isTransitive = false }
-    testCompile(project(":idea:idea-gradle")) { isTransitive = false }
-    testCompile(project(":idea:idea-maven")) { isTransitive = false }
-    testCompile(project(":idea:idea-native")) { isTransitive = false }
-    testCompile(project(":idea:idea-gradle-native")) { isTransitive = false }
-    testCompile(commonDep("junit:junit"))
-
-    testRuntime(project(":plugins:kapt3-idea")) { isTransitive = false }
-    testRuntime(project(":kotlin-reflect"))
-    testRuntime(project(":kotlin-preloader"))
-
-    testCompile(project(":kotlin-sam-with-receiver-compiler-plugin")) { isTransitive = false }
 }
 
 sourceSets {
