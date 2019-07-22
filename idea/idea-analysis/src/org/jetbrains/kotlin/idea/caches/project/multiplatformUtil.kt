0993fb82fe4a61bb696e56595924266050cabf37
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-07-22 17:56:25 +0300

    multiplatformUtil: `implementedModules` shouldn't ignore android test module Relates to #KT-30799

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/project/multiplatformUtil.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/project/multiplatformUtil.kt
@@ -19,6 +19,7 @@ import org.jetbrains.kotlin.config.KotlinFacetSettings
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.idea.caches.project.SourceType.PRODUCTION
 import org.jetbrains.kotlin.idea.caches.project.SourceType.TEST
+import org.jetbrains.kotlin.idea.core.isAndroidModule
 import org.jetbrains.kotlin.idea.facet.KotlinFacet
 import org.jetbrains.kotlin.idea.facet.KotlinFacetType
 import org.jetbrains.kotlin.idea.facet.KotlinFacetType.Companion.ID
@@ -68,7 +69,8 @@ val Module.implementedModules: List<Module>
             CachedValueProvider.Result(
                 if (isNewMPPModule) {
                     rootManager.dependencies.filter {
-                        it.isNewMPPModule && it.platform.isCommon() && it.externalProjectId == externalProjectId && it.isTestModule == isTestModule
+                        // TODO: remove additional android check
+                        it.isNewMPPModule && it.platform.isCommon() && it.externalProjectId == externalProjectId && (isAndroidModule() || it.isTestModule == isTestModule)
                     }
                 } else {
                     val modelsProvider = IdeModelsProviderImpl(project)
