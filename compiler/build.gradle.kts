58f294a75728a52ac3408e47e5b6044bf06658b8
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-07-22 18:43:45 +0300

    Fix kotlin compiler tests in 192 (KT-32193)
     #KT-32193 Fixed

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -66,6 +66,10 @@ dependencies {
     testCompileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     testCompileOnly(intellijDep()) { includeJars("openapi", "idea", "idea_rt", "util", "asm-all", rootProject = rootProject) }
 
+    Platform[192].orHigher {
+        testRuntimeOnly(intellijPluginDep("java"))
+    }
+
     testRuntime(project(":kotlin-reflect"))
     testRuntime(project(":kotlin-daemon-client-new"))
     testRuntime(project(":kotlin-daemon")) // +
