16b92cc5c6c73d245861d463bc2b7298a7d34235
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2019-07-23 15:04:40 +0300

    SQL formatting: improve performance when a part of text (not the whole text) is re-formatted or re-indented
    intellij-monorepo: 47b36511fb544fe45a9f770865f90c22cd47fd67

--- intellij-community/platform/lang-impl/src/com/intellij/formatting/FormattingModelBuilderEx.java
+++ intellij-community/platform/lang-impl/src/com/intellij/formatting/FormattingModelBuilderEx.java
@@ -15,6 +15,7 @@
  */
 package com.intellij.formatting;
 
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
@@ -35,9 +36,28 @@ public interface FormattingModelBuilderEx extends FormattingModelBuilder {
    * @param settings the code style settings used for formatting.
    * @param mode     formatting mode
    * @return the formatting model for the file.
+   * @see FormattingModelBuilderEx#createModel(PsiElement, TextRange, CodeStyleSettings, FormattingMode)
    */
   @NotNull
   FormattingModel createModel(@NotNull final PsiElement element, @NotNull final CodeStyleSettings settings, @NotNull FormattingMode mode);
+
+  /**
+   * Requests building the formatting model for a section of the file containing
+   * the specified PSI element and its children.
+   *
+   * @param element  the top element for which formatting is requested.
+   * @param range    the range for which a model should be built; wider range requires longer time for model building.
+   * @param settings the code style settings used for formatting.
+   * @param mode     formatting mode.
+   * @return the formatting model for the file.
+   */
+  @NotNull
+  default FormattingModel createModel(@NotNull final PsiElement element,
+                                      @NotNull final TextRange range,
+                                      @NotNull final CodeStyleSettings settings,
+                                      @NotNull final FormattingMode mode) {
+    return createModel(element, settings, mode); // just for compatibility with old implementations
+  }
 
   /**
    * Allows to adjust indent options to used during performing formatting operation on the given ranges of the given file.
