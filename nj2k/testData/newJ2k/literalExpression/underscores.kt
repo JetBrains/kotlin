72a09ab59bd4788fa6c6dad0d523af8d69378c3b
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2019-07-24 22:35:02 +0300

    New J2K: correctly convert number literals with underscores
    #KT-32837 fixed

---
+++ nj2k/testData/newJ2k/literalExpression/underscores.kt
@@ -0,0 +1,9 @@
+class TestNumber {
+    internal var longLiteral: Long = 1234567
+    internal var longHexLiteral: Long = -0x1321a2
+    internal var intLiteral = 1234
+    internal var floatLiteral = 1234f
+    internal var doubleLiteral = 1234.0
+    internal var shortLiteral: Short = 123
+    internal var byteLiteral: Byte = 123
+}
