00956cd6c6e95eee660a5d7c115a476b8919accc
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2019-07-24 11:20:54 +0300

    [gradle] fix project leak: cancel subscription on project disposal
    intellij-monorepo: 45d86c69710cdeae4226a133259b48e711d1f3f7

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/startup/GradleUnlinkedProjectProcessor.kt
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/startup/GradleUnlinkedProjectProcessor.kt
@@ -73,6 +73,7 @@ class GradleUnlinkedProjectProcessor : StartupActivity, DumbAware {
           Disposer.dispose(subscription)
         }
       }
+      Disposer.register(project, subscription)
       ExternalSystemApiUtil.subscribe(project, GradleConstants.SYSTEM_ID, settingsListener, subscription)
 
       notification.notify(project)
