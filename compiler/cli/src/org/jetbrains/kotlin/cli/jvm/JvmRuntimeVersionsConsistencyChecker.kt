945676357690dcfc20d9bf38dc444c12c95b8756
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-07-26 16:09:27 +0200

    Minor, fix debug info when inferring API version

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/JvmRuntimeVersionsConsistencyChecker.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/JvmRuntimeVersionsConsistencyChecker.kt
@@ -136,17 +136,15 @@ object JvmRuntimeVersionsConsistencyChecker {
                 // we infer API = the version of that stdlib.
                 // Note that "no explicit -api-version" requirement is necessary because for example, in
                 // "kotlinc-1.2 -language-version 1.0 -cp kotlin-runtime-1.1.jar" we should still infer API = 1.0
-                val newSettings = object : LanguageVersionSettings by languageVersionSettings {
+                configuration.languageVersionSettings = object : LanguageVersionSettings by languageVersionSettings {
                     override val apiVersion: ApiVersion get() = actualApi
                 }
 
                 messageCollector.issue(
                     null, "Old runtime has been found in the classpath. " +
                             "Initial language version settings: $languageVersionSettings. " +
-                            "Updated language version settings: $newSettings", CompilerMessageSeverity.LOGGING
+                            "Updated API version: $actualApi", CompilerMessageSeverity.LOGGING
                 )
-
-                configuration.languageVersionSettings = newSettings
             }
         } else if (consistency != ClasspathConsistency.Consistent) {
             messageCollector.issue(
