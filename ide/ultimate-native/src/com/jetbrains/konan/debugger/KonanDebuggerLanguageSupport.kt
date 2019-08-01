55393ecf3a11776d1fa923cd4fcac112514ac992
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2019-08-01 23:12:39 +0300

    Guess K/N language from stack frames

--- ide/ultimate-native/src/com/jetbrains/konan/debugger/KonanDebuggerLanguageSupport.kt
+++ ide/ultimate-native/src/com/jetbrains/konan/debugger/KonanDebuggerLanguageSupport.kt
@@ -5,18 +5,13 @@
 
 package com.jetbrains.konan.debugger
 
-import com.intellij.execution.configurations.RunProfile
 import com.jetbrains.cidr.execution.debugger.CidrDebugProcess
 import com.jetbrains.cidr.execution.debugger.CidrDebuggerLanguageSupport
 import com.jetbrains.cidr.execution.debugger.backend.DebuggerDriver
+import com.jetbrains.cidr.execution.debugger.backend.DebuggerDriver.StandardDebuggerLanguage.C
 import com.jetbrains.cidr.execution.debugger.evaluation.CidrDebuggerTypesHelper
-import com.jetbrains.konan.debugger.KonanDebuggerTypesHelper
-import com.jetbrains.cidr.execution.debugger.backend.DebuggerDriver.StandardDebuggerLanguage.C // TODO: C -> KOTLIN_NATIVE
 
 class KonanDebuggerLanguageSupport : CidrDebuggerLanguageSupport() {
-    // TODO disables frame-based language detection globally, even for non-kotlin projects
-    override fun useFrameLanguageFromDebugger(profile: RunProfile?) = false
-
     override fun createTypesHelper(process: CidrDebugProcess): CidrDebuggerTypesHelper {
         return KonanDebuggerTypesHelper(process)
     }
@@ -24,4 +19,4 @@ class KonanDebuggerLanguageSupport : CidrDebuggerLanguageSupport() {
     override fun getSupportedDebuggerLanguages(): MutableSet<DebuggerDriver.DebuggerLanguage> {
         return mutableSetOf(C)
     }
-}
+}`
