ee3a0c4c6d8152302b4944ccee74d2aede22c5dd
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-08-01 23:48:36 +0300

    ProvidedIndexExtension
    intellij-monorepo: bf78336eac56315ca68728e2601bd1a85528d7f9

---
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/provided/ProvidedIndexExtension.java
@@ -0,0 +1,23 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.util.indexing.provided;
+
+import com.intellij.util.indexing.ID;
+import com.intellij.util.io.DataExternalizer;
+import com.intellij.util.io.KeyDescriptor;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+public interface ProvidedIndexExtension<K, V> {
+  @NotNull
+  File getIndexPath();
+
+  @NotNull
+  ID<K, V> getIndexId();
+
+  @NotNull
+  KeyDescriptor<K> createKeyDescriptor();
+
+  @NotNull
+  DataExternalizer<V> createValueExternalizer();
+}
