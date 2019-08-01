8b650b1bcc298f8892e935a8e01dc87c904ca957
Mikhail Mazurkevich <mikhail.mazurkevich@jetbrains.com>
2019-08-01 11:40:01 +0300

    Switch from usage HashStorage to FileStampStorage
    FileStampStorage consist of file timestamp and hash calculated by file content.
    Improvement is in avoiding costly hash calculation if timestamps are different.
    
    intellij-monorepo: 7147a8d63f9cf73c29a51e15169eca0fd7a1cd5a

--- intellij-community/java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
+++ intellij-community/java/compiler/impl/src/com/intellij/compiler/server/BuildManager.java
@@ -1158,7 +1158,7 @@ public final class BuildManager implements Disposable {
     if (Registry.is("compiler.build.portable.caches")) {
       cmdLine.addParameter("-Didea.resizeable.file.truncate.on.close=true");
       cmdLine.addParameter("-Dkotlin.jps.non.caching.storage=true");
-      cmdLine.addParameter("-Dorg.jetbrains.jps.use.hashes=true");
+      cmdLine.addParameter("-Dorg.jetbrains.jps.portable.caches=true");
     }
 
     // javac's VM should use the same default locale that IDEA uses in order for javac to print messages in 'correct' language
