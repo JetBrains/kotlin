7182df225013364eddd16eaac498b0567dfa9861
epeshk <evgeny.peshkov@jetbrains.com>
2019-08-02 19:07:39 +0300

    Revert "IDEA-CR-50954: `GenericBackgroundUpdaterTask` -> `BackgroundUpdaterTaskBase`"
    This reverts commit f119008cd1a84f9ac59b3854524d553f09467c03.
    
    intellij-monorepo: bb30da97f9039771d0a284736048d51e66843654

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/GenericBackgroundUpdaterTask.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/GenericBackgroundUpdaterTask.java
@@ -18,7 +18,7 @@ import org.jetbrains.annotations.TestOnly;
 
 import java.util.*;
 
-public abstract class BackgroundUpdaterTaskBase<T> extends Task.Backgroundable {
+public abstract class GenericBackgroundUpdaterTask<T> extends Task.Backgroundable {
   protected JBPopup myPopup;
   private GenericListComponentUpdater<T> myUpdater;
   private Ref<? extends UsageView> myUsageView;
@@ -31,7 +31,7 @@ public abstract class BackgroundUpdaterTaskBase<T> extends Task.Backgroundable {
   private volatile boolean myFinished;
   private volatile ProgressIndicator myIndicator;
 
-  public BackgroundUpdaterTaskBase(@Nullable Project project, @NotNull String title, @Nullable Comparator<T> comparator) {
+  public GenericBackgroundUpdaterTask(@Nullable Project project, @NotNull String title, @Nullable Comparator<T> comparator) {
     super(project, title);
     myData = comparator == null ? ContainerUtil.newSmartList() : new TreeSet<>(comparator);
   }
@@ -67,7 +67,7 @@ public abstract class BackgroundUpdaterTaskBase<T> extends Task.Backgroundable {
   }
 
   /**
-   * @deprecated Use {@link #BackgroundUpdaterTaskBase(Project, String, Comparator)} and {@link #updateComponent(T)} instead
+   * @deprecated Use {@link #GenericBackgroundUpdaterTask(Project, String, Comparator)} and {@link #updateComponent(T)} instead
    */
   @Deprecated
   public boolean updateComponent(@NotNull T element, @Nullable Comparator comparator) {
