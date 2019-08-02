a29d46ca8e5a3fdc49845e2c7849a67eeb36792b
Jim <jsproch@google.com>
2019-08-02 02:24:34 -0700

    Ban try-catch blocks around composable calls.
    Some day, we intend to support them.  But for now, we'll ban them to ensure correctness.
    
    Change-Id: Ie4fd9d0c37bef0f673a433d43e37252f802b629e ( https://android-review.googlesource.com/q/Ie4fd9d0c37bef0f673a433d43e37252f802b629e )
    
    Moved from: https://github.com/androidx/androidx/commit/19ae8b799e11c85e34e4faa933a6c731e36c6117

---
+++ compose-compiler-hosted/integration-tests/src/test/java/androidx/compose/plugins/kotlin/analysis/TryCatchComposableCheckerTests.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.plugins.kotlin.analysis
+
+import androidx.compose.plugins.kotlin.AbstractComposeDiagnosticsTest
+
+
+/**
+ * We're strongly considering supporting try-catch-finally blocks in the future.
+ * If/when we do support them, these tests should be deleted.
+ */
+class TryCatchComposableCheckerTests : AbstractComposeDiagnosticsTest() {
+
+    fun testTryCatchReporting001() {
+        doTest(
+            """
+            import androidx.compose.*;
+
+            @Composable fun foo() { }
+
+            @Composable fun bar() {
+                <!ILLEGAL_TRY_CATCH_AROUND_COMPOSABLE!>try<!> {
+                    foo()
+                } catch(e: Exception) {
+                }
+            }
+        """)
+    }
+
+    fun testTryCatchReporting002() {
+        doTest(
+            """
+            import androidx.compose.*;
+
+            fun foo() { }
+
+            @Composable fun bar() {
+                try {
+                    foo()
+                } catch(e: Exception) {
+                }
+            }
+        """)
+    }
+
+    fun testTryCatchReporting003() {
+        doTest(
+            """
+            import androidx.compose.*;
+
+            @Composable fun foo() { }
+
+            @Composable fun bar() {
+                try {
+                } catch(e: Exception) {
+                    foo()
+                } finally {
+                    foo()
+                }
+            }
+        """)
+    }
+
+    fun testTryCatchReporting004() {
+        doTest(
+            """
+            import androidx.compose.*;
+
+            @Composable fun foo() { }
+
+            @Composable fun bar() {
+                <!ILLEGAL_TRY_CATCH_AROUND_COMPOSABLE!>try<!> {
+                    (1..10).forEach { foo() }
+                } catch(e: Exception) {
+                }
+            }
+        """)
+    }
+}
---
+++ compose-compiler-hosted/src/main/java/androidx/compose/plugins/kotlin/TryCatchComposableChecker.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.plugins.kotlin
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.container.StorageComponentContainer
+import org.jetbrains.kotlin.container.useInstance
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.diagnostics.reportFromPlugin
+import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
+import org.jetbrains.kotlin.psi.KtFunction
+import androidx.compose.plugins.kotlin.analysis.ComposeDefaultErrorMessages
+import androidx.compose.plugins.kotlin.analysis.ComposeErrors
+import org.jetbrains.kotlin.psi.KtTryExpression
+import org.jetbrains.kotlin.resolve.TargetPlatform
+import org.jetbrains.kotlin.resolve.calls.checkers.CallChecker
+import org.jetbrains.kotlin.resolve.calls.checkers.CallCheckerContext
+import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
+import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatform
+
+open class TryCatchComposableChecker: CallChecker, StorageComponentContainerContributor {
+
+    override fun registerModuleComponents(
+        container: StorageComponentContainer,
+        platform: TargetPlatform,
+        moduleDescriptor: ModuleDescriptor
+    ) {
+        if (platform != JvmPlatform) return
+        container.useInstance(this)
+    }
+
+    override fun check(
+        resolvedCall: ResolvedCall<*>,
+        reportOn: PsiElement,
+        context: CallCheckerContext
+    ) {
+        val trace = context.trace
+        val call = resolvedCall.call.callElement
+        val shouldBeTag = ComposableAnnotationChecker.get(call.project).shouldInvokeAsTag(trace, resolvedCall)
+        if(shouldBeTag) {
+            var walker: PsiElement? = call
+            while (walker != null) {
+                val parent = walker.parent
+                if(parent is KtTryExpression) {
+                    if(walker == parent.tryBlock)
+                        trace.reportFromPlugin(
+                            ComposeErrors.ILLEGAL_TRY_CATCH_AROUND_COMPOSABLE.on(parent.tryKeyword!!),
+                            ComposeDefaultErrorMessages
+                        )
+                }
+                walker = try { walker.parent } catch (e: Throwable) { null }
+            }
+        }
+    }
+
+}
