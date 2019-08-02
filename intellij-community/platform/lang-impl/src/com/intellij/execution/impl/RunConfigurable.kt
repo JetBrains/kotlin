8812fc2fe04c9dc069c2e62f5c9efa1199f87e62
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-02 15:15:20 +0200

    IDEA-219775 TeamCity: can't remote debug test
    intellij-monorepo: 9bd36a6a54649e0bc3ea61ee6377b70814a0c3e2

--- intellij-community/platform/lang-impl/src/com/intellij/execution/impl/RunConfigurable.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/impl/RunConfigurable.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.impl
 
 import com.intellij.execution.*
@@ -126,6 +126,9 @@ open class RunConfigurable @JvmOverloads constructor(protected val project: Proj
       }
     }
   }
+
+  // https://youtrack.jetbrains.com/issue/TW-61353
+  fun getSelectedConfigurable() = selectedConfigurable
 
   override fun getDisplayName() = ExecutionBundle.message("run.configurable.display.name")
 
