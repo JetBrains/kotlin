b0d403d47bcad897bff80d661da880e96ea19774
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-07 00:44:25 +0200

    Cleanup (warnings; formatting)
    intellij-monorepo: 70c679e93b9d7ed5174f19007db3c1c6ee1835b9

--- intellij-community/platform/lang-impl/src/com/intellij/analysis/BaseAnalysisAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/analysis/BaseAnalysisAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.analysis;
 
+import com.intellij.analysis.dialog.ModelScopeItem;
 import com.intellij.codeInspection.ui.InspectionResultsView;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
@@ -12,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.util.List;
 
 public abstract class BaseAnalysisAction extends AnAction {
   private static final String DIMENSION_KEY_PREFIX = "ANALYSIS_DLG_";
@@ -44,8 +46,8 @@ public abstract class BaseAnalysisAction extends AnAction {
     boolean rememberScope = ActionPlaces.isMainMenuOrActionSearch(e.getPlace());
     AnalysisUIOptions uiOptions = AnalysisUIOptions.getInstance(project);
     PsiElement element = CommonDataKeys.PSI_ELEMENT.getData(dataContext);
-    BaseAnalysisActionDialog dlg = new BaseAnalysisActionDialog(title, noon, project, BaseAnalysisActionDialog.standardItems(project, scope, module, element),
-                                                                uiOptions, rememberScope) {
+    List<ModelScopeItem> items = BaseAnalysisActionDialog.standardItems(project, scope, module, element);
+    BaseAnalysisActionDialog dlg = new BaseAnalysisActionDialog(title, noon, project, items, uiOptions, rememberScope) {
       @Override
       protected String getDimensionServiceKey() {
         return DIMENSION_KEY_PREFIX + getClass().getName();
@@ -67,7 +69,7 @@ public abstract class BaseAnalysisAction extends AnAction {
     }
 
     int oldScopeType = uiOptions.SCOPE_TYPE;
-    scope = dlg.getScope(uiOptions, scope, project, module);
+    scope = dlg.getScope(scope);
     if (!rememberScope) {
       uiOptions.SCOPE_TYPE = oldScopeType;
     }
