1ea5051e37f3bfc60caff801ce24f938e147f529
Yann Cébron <yann.cebron@jetbrains.com>
2019-08-07 17:03:52 +0200

    DefaultLiveTemplatesProvider: javadoc
    intellij-monorepo: 0692970eb6de9b031ef97f2f2e8254fd2a3af4c1

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/DefaultLiveTemplatesProvider.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/DefaultLiveTemplatesProvider.java
@@ -1,34 +1,33 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.template.impl;
 
+import com.intellij.codeInsight.template.TemplateContextType;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * Provides bundled live templates.
+ * <p/>
+ * See <a href="http://www.jetbrains.org/intellij/sdk/docs/tutorials/live_templates.html">Live Templates Tutorial</a>.
+ *
  * @author yole
+ * @see TemplateContextType
  */
 public interface DefaultLiveTemplatesProvider {
+
   ExtensionPointName<DefaultLiveTemplatesProvider> EP_NAME = ExtensionPointName.create("com.intellij.defaultLiveTemplatesProvider");
 
+  /**
+   * User-visible and editable templates.
+   *
+   * @return paths to resources, without {@code .xml} extension (e.g. {@code /templates/foo})
+   */
   String[] getDefaultLiveTemplateFiles();
 
   /**
-   * @return paths to resources, without .xml extension (e.g. /templates/foo)
+   * Templates for programmatic use only.
+   *
+   * @return paths to resources, without {@code .xml} extension (e.g. {@code /templates/foo})
    */
   @Nullable
   String[] getHiddenLiveTemplateFiles();
