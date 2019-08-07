59400af0e29e7a18ff6b311ae608afdda1fc8361
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2019-08-07 16:42:25 +0300

    IDEA-220137 Do not run inspection iterations if LocalInspectionTool returned PsiElementVisitor.EMPTY_VISITOR
    intellij-monorepo: 782b85b56e7b93c09005491ea6baf0e8c8057063

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LocalInspectionsPass.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/LocalInspectionsPass.java
@@ -296,9 +296,11 @@ public class LocalInspectionsPass extends ProgressableTextEditorHighlightingPass
 
     PsiElementVisitor visitor = InspectionEngine.createVisitorAndAcceptElements(tool, holder, isOnTheFly, session, elements, elementDialectIds,
                                                                                 dialectIdsSpecifiedForTool);
-
-    synchronized (init) {
-      init.add(new InspectionContext(toolWrapper, holder, holder.getResultCount(), visitor, dialectIdsSpecifiedForTool));
+    // if inspection returned empty visitor then it should be skipped
+    if (visitor != PsiElementVisitor.EMPTY_VISITOR) {
+      synchronized (init) {
+        init.add(new InspectionContext(toolWrapper, holder, holder.getResultCount(), visitor, dialectIdsSpecifiedForTool));
+      }
     }
     advanceProgress(1);
 
