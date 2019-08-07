f34a08cbbf41f7e3bb2db4f3fa7972085081d4e0
Steven Schäfer <stevenschaefer@google.com>
2019-08-07 14:19:17 +0200

    JVM IR: Convert interface companion fields to static fields.

--- compiler/testData/codegen/box/objects/interfaceCompanion.kt
+++ compiler/testData/codegen/box/objects/interfaceCompanion.kt
@@ -1,3 +1,7 @@
+// IGNORE_BACKEND: JVM_IR
+// Inside of the companion we have to access the instance through the local Companion field,
+// not by indirection through the Companion field of the enclosing class.
+// Class initialization might not have finished yet.
 var result = ""
 
 interface A {
