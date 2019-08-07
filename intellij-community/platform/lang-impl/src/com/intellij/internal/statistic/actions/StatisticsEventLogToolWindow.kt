0c30598c886a02add936f9d8180d4ec524c26194
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2019-08-07 15:45:11 +0700

    IDEA-CR-51080: FUS: log incorrect events as errors in tool window
    intellij-monorepo: 6b80fe101bee2a2e815457876207aac281833f3d

--- intellij-community/platform/lang-impl/src/com/intellij/internal/statistic/actions/StatisticsEventLogToolWindow.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/internal/statistic/actions/StatisticsEventLogToolWindow.kt
@@ -3,8 +3,11 @@ package com.intellij.internal.statistic.actions
 
 import com.intellij.execution.impl.ConsoleViewImpl
 import com.intellij.execution.ui.ConsoleViewContentType
+import com.intellij.execution.ui.ConsoleViewContentType.ERROR_OUTPUT
+import com.intellij.execution.ui.ConsoleViewContentType.NORMAL_OUTPUT
 import com.intellij.internal.statistic.eventLog.EventLogNotificationService
 import com.intellij.internal.statistic.eventLog.LogEvent
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType.*
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.DefaultActionGroup
@@ -15,9 +18,11 @@ import com.intellij.util.text.DateFormatUtil
 
 const val eventLogToolWindowsId = "FUS Event Log"
 
-class StatisticsEventLogToolWindow(myProject: Project) : SimpleToolWindowPanel(false, true), Disposable{
+class StatisticsEventLogToolWindow(myProject: Project) : SimpleToolWindowPanel(false, true), Disposable {
   private val consoleView = ConsoleViewImpl(myProject, true)
-  private val eventLogListener: (LogEvent) -> Unit = { consoleView.print(buildLogMessage(it), ConsoleViewContentType.NORMAL_OUTPUT) }
+  private val eventLogListener: (LogEvent) -> Unit = { logEvent ->
+    buildLogMessage(logEvent).forEach { (text, contentType) -> consoleView.print(text, contentType) }
+  }
 
   init {
     setContent(consoleView.component)
@@ -32,36 +37,52 @@ class StatisticsEventLogToolWindow(myProject: Project) : SimpleToolWindowPanel(f
     EventLogNotificationService.unsubscribe(eventLogListener)
   }
 
-
   companion object {
     private val systemFields = setOf("last", "created")
     private const val projectIdPrefixSize = 8
     private const val projectIdSuffixSize = 2
     private const val maxProjectIdSize = projectIdPrefixSize + projectIdSuffixSize
+    val incorrectValidationTypes = setOf(REJECTED, INCORRECT_RULE, UNDEFINED_RULE, UNREACHABLE_WHITELIST, PERFORMANCE_ISSUE)
+    private val incorrectDescriptions = incorrectValidationTypes.map { it.description }
 
-    fun buildLogMessage(logEvent: LogEvent): String {
-      return buildString {
-        append("${DateFormatUtil.formatDate(logEvent.time)} ${DateFormatUtil.formatTimeWithSeconds(logEvent.time)}")
-        append(" - ")
-        append("['${logEvent.group.id}', v${logEvent.group.version}]: '${logEvent.event.id}' ")
+    fun buildLogMessage(logEvent: LogEvent): List<Pair<String, ConsoleViewContentType>> {
+      val messages = arrayListOf<Pair<String, ConsoleViewContentType>>()
+      messages.add(
+        "${DateFormatUtil.formatTimeWithSeconds(logEvent.time)} - ['${logEvent.group.id}', v${logEvent.group.version}]: " to NORMAL_OUTPUT)
+      messages.add("'${logEvent.event.id}' " to defineContentType(logEvent.event.id))
+      messages.add("{" to NORMAL_OUTPUT)
+      messages.addAll(buildEventDataMessages(logEvent))
+      messages.add("}\n" to NORMAL_OUTPUT)
+      return messages
+    }
 
-        append("{")
-        append(logEvent.event.data
-                 .filter { (key, _) -> !systemFields.contains(key) }
-                 .map { (key, value) ->
-                   val valueAsString = value.toString()
-                   val length = valueAsString.length
-                   if (key == "project" && valueAsString.isNotBlank() && length > maxProjectIdSize) {
-                     "$key='${valueAsString.substring(0, projectIdPrefixSize)}...${valueAsString.substring(length - projectIdSuffixSize, length)}'"
-                   }
-                   else {
-                     "$key='$valueAsString'"
-                   }
-                 }
-                 .joinToString(", "))
-        appendln("}")
+    private fun buildEventDataMessages(logEvent: LogEvent): List<Pair<String, ConsoleViewContentType>> {
+      val eventDataMessages = arrayListOf<Pair<String, ConsoleViewContentType>>()
+      var count = 0
+      for ((key, value) in logEvent.event.data) {
+        if (systemFields.contains(key)) continue
+        var valueAsString = value.toString()
+        if (++count > 1) eventDataMessages.add(", " to NORMAL_OUTPUT)
+        val contentType = defineContentType(valueAsString)
+        if (contentType != ERROR_OUTPUT && key == "project") {
+          valueAsString = shortenProjectId(valueAsString)
+        }
+        eventDataMessages.add("$key='$valueAsString'" to contentType)
+      }
+      return eventDataMessages
+    }
+
+    private fun shortenProjectId(projectId: String): String {
+      val length = projectId.length
+      if (projectId.isNotBlank() && length > maxProjectIdSize) {
+        return "${projectId.substring(0, projectIdPrefixSize)}...${projectId.substring(length - projectIdSuffixSize, length)}"
+      }
+      else {
+        return projectId
       }
     }
 
+    private fun defineContentType(value: String): ConsoleViewContentType =
+      if (incorrectDescriptions.contains(value)) ERROR_OUTPUT else NORMAL_OUTPUT
   }
 }
--- intellij-community/platform/lang-impl/testSources/com/intellij/internal/statistic/actions/StatisticsEventLogToolWindowTest.kt
+++ intellij-community/platform/lang-impl/testSources/com/intellij/internal/statistic/actions/StatisticsEventLogToolWindowTest.kt
@@ -1,33 +1,86 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.internal.statistic.actions
 
+import com.intellij.execution.ui.ConsoleViewContentType
 import com.intellij.internal.statistic.actions.StatisticsEventLogToolWindow.Companion.buildLogMessage
 import com.intellij.internal.statistic.eventLog.LogEvent
 import com.intellij.internal.statistic.eventLog.LogEventAction
+import com.intellij.internal.statistic.eventLog.validator.ValidationResultType.*
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class StatisticsEventLogToolWindowTest {
-    @Test
-    fun testBuildLogMessage() {
-        val action = LogEventAction("third.party")
-        action.addData("project", "5410c65eafb1f0abd78c6d9bdf33752f13c17b17ed57c3ae26801ae6ee7d17ea")
-        action.addData("last", "1564643442610")
-        action.addData("plugin_type", "PLATFORM")
-        val expected = "01.08.2019 14:05:14 - ['toolwindow', v21]: 'third.party' {plugin_type='PLATFORM', project='5410c65e...ea'}\r\n"
-        assertEquals(expected, buildLogMessage(buildLogEvent(action)))
-    }
+  private val eventId = "third.party"
 
-    @Test
-    fun testBuildLogMessageWithShortProjectName() {
-        val action = LogEventAction("third.party")
-        val projectId = "12345"
-        action.addData("project", projectId)
-        val expected = "01.08.2019 14:05:14 - ['toolwindow', v21]: 'third.party' {project='$projectId'}\r\n"
-        assertEquals(expected, buildLogMessage(buildLogEvent(action)))
-    }
+  @Test
+  fun testShortenProjectId() {
+    val action = LogEventAction(eventId)
+    action.addData("project", "5410c65eafb1f0abd78c6d9bdf33752f13c17b17ed57c3ae26801ae6ee7d17ea")
+    action.addData("plugin_type", "PLATFORM")
 
-    private fun buildLogEvent(action: LogEventAction) =
-            LogEvent("2e5b2e32e061", "193.1801", "176",
-                    1564643114456, "toolwindow", "21", "32", action)
+    val actual = buildLogMessage(buildLogEvent(action))
+    assertTrue("Not all messages have NORMAL_OUTPUT type", actual.all { it.second == ConsoleViewContentType.NORMAL_OUTPUT })
+    assertEquals("14:05:14 - ['toolwindow', v21]: '$eventId' {plugin_type='PLATFORM', project='5410c65e...ea'}\n",
+                 actual.joinToString("") { it.first })
+  }
+
+  @Test
+  fun testNotShortenProjectId() {
+    val action = LogEventAction(eventId)
+    val projectId = "12345"
+    action.addData("project", projectId)
+
+    val actual = buildLogMessage(buildLogEvent(action))
+    assertTrue("Not all messages have NORMAL_OUTPUT type", actual.all { it.second == ConsoleViewContentType.NORMAL_OUTPUT })
+    assertEquals("14:05:14 - ['toolwindow', v21]: '$eventId' {project='$projectId'}\n",
+                 actual.joinToString("") { it.first })
+  }
+
+  @Test
+  fun testFilterSystemFields() {
+    val action = LogEventAction(eventId)
+    action.addData("last", "1564643442610")
+    action.addData("created", "1564643442610")
+
+    val actual = buildLogMessage(buildLogEvent(action))
+    assertTrue("Not all messages have NORMAL_OUTPUT type", actual.all { it.second == ConsoleViewContentType.NORMAL_OUTPUT })
+    assertEquals("14:05:14 - ['toolwindow', v21]: '$eventId' {}\n",
+                 actual.joinToString("") { it.first })
+  }
+
+  @Test
+  fun testLogIncorrectEventIdAsError() {
+    val incorrectEventId = INCORRECT_RULE.description
+    val action = LogEventAction(incorrectEventId)
+
+    val actual = buildLogMessage(buildLogEvent(action))
+    assertEquals(4, actual.size)
+    assertEquals(ConsoleViewContentType.ERROR_OUTPUT, actual[1].second)
+  }
+
+  @Test
+  fun testLogIncorrectEventDataAsError() {
+    val action = LogEventAction(eventId)
+    action.addData("test", INCORRECT_RULE.description)
+    action.addData("project", UNDEFINED_RULE.description)
+
+    val actual = buildLogMessage(buildLogEvent(action))
+    assertEquals(7, actual.size)
+    assertEquals("test='validation.incorrect_rule'", actual[3].first)
+    assertEquals(ConsoleViewContentType.ERROR_OUTPUT, actual[3].second)
+    assertEquals("project='validation.undefined_rule'", actual[5].first)
+    assertEquals(ConsoleViewContentType.ERROR_OUTPUT, actual[5].second)
+  }
+
+  @Test
+  fun testAllValidationTypesUsed() {
+    val correctValidationTypes = setOf(ACCEPTED, THIRD_PARTY)
+    for (resultType in values()) {
+      assert(StatisticsEventLogToolWindow.incorrectValidationTypes.contains(resultType) || correctValidationTypes.contains(resultType))
+    }
+  }
+
+  private fun buildLogEvent(action: LogEventAction) = LogEvent("2e5b2e32e061", "193.1801", "176", 1564643114456,
+                                                               "toolwindow", "21", "32", action)
 }
