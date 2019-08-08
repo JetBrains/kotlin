e8b786ffb0a78e33368b987b8da6a03dd1ed21bb
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-08-08 15:40:01 +0300

    fix typo
    intellij-monorepo: 6686f54643808e0f842fc409dc8ea282e2a35126

--- intellij-community/platform/lang-impl/src/com/intellij/find/findUsages/AbstractFindUsagesDialog.java
+++ intellij-community/platform/lang-impl/src/com/intellij/find/findUsages/AbstractFindUsagesDialog.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.find.findUsages;
 
 import com.intellij.find.FindBundle;
@@ -230,7 +230,7 @@ public abstract class AbstractFindUsagesDialog extends DialogWrapper {
 
   protected void addUsagesOptions(JPanel optionsPanel) {
     if (mySearchForTextOccurrencesAvailable) {
-      myCbToSearchForTextOccurrences = addCheckboxToPanel(FindBundle.message("find.options.search.for.text.occurences.checkbox"),
+      myCbToSearchForTextOccurrences = addCheckboxToPanel(FindBundle.message("find.options.search.for.text.occurrences.checkbox"),
                                                          myFindUsagesOptions.isSearchForTextOccurrences, optionsPanel, false);
 
     }
