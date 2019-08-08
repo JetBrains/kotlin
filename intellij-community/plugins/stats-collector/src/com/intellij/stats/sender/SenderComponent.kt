9b5fcb25f2725ded8f51708cc6c839cd954a14ef
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-08 18:48:10 +0200

    Sender as PreloadingActivity
    intellij-monorepo: 2b6db1aa4dfd42c3dfac3c63cba94bd06e3576c8

--- intellij-community/plugins/stats-collector/src/com/intellij/stats/sender/SenderComponent.kt
+++ intellij-community/plugins/stats-collector/src/com/intellij/stats/sender/SenderComponent.kt
@@ -1,53 +1,46 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.stats.sender
 
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.BaseComponent
-import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.application.PreloadingActivity
+import com.intellij.openapi.components.service
+import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.reporting.isSendAllowed
 import com.intellij.stats.experiment.WebServiceStatus
 import com.intellij.util.Alarm
 import com.intellij.util.Time
 
-class SenderComponent(private val sender: StatisticSender, private val statusHelper: WebServiceStatus) : BaseComponent, Disposable {
-  private companion object {
-    val LOG = logger<SenderComponent>()
-  }
-
-  private val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, this)
+internal class SenderComponent : PreloadingActivity() {
+  private val alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, ApplicationManager.getApplication())
   private val sendInterval = 5 * Time.MINUTE
 
+  override fun preload(indicator: ProgressIndicator) {
+    if (isSendAllowed()) {
+      send()
+    }
+  }
+
   private fun send() {
-    if (ApplicationManager.getApplication().isUnitTestMode || ApplicationManager.getApplication().isHeadlessEnvironment) return
+    if (ApplicationManager.getApplication().isUnitTestMode || ApplicationManager.getApplication().isHeadlessEnvironment) {
+      return
+    }
 
     try {
       ApplicationManager.getApplication().executeOnPooledThread {
-        if(!isSendAllowed()) return@executeOnPooledThread
+        if (!isSendAllowed()) {
+          return@executeOnPooledThread
+        }
+
+        val statusHelper = WebServiceStatus.getInstance()
         statusHelper.updateStatus()
         if (statusHelper.isServerOk()) {
           val dataServerUrl = statusHelper.dataServerUrl()
-          sender.sendStatsData(dataServerUrl)
+          service<StatisticSender>().sendStatsData(dataServerUrl)
         }
       }
-    }
-    catch (e: Exception) {
-      LOG.error(e.message)
     }
     finally {
       alarm.addRequest({ send() }, sendInterval)
     }
-  }
-
-  override fun initComponent() {
-    if (isSendAllowed()) {
-      ApplicationManager.getApplication().executeOnPooledThread {
-        send()
-      }
-    }
-  }
-
-  override fun dispose() {
   }
 }
