fd66752d932ba0ba07c5d7171254aa06529b9d3c
Mike Sinkovsky <msink@users.noreply.github.com>
2019-08-12 19:28:36 +0500

    Bellard-Pi benchmark (#3225)

---
+++ performance/numerical/src/nativeInterop/cinterop/pi.h
@@ -0,0 +1,16 @@
+/*
+ * Computation of the n'th decimal digit of \pi with very little memory.
+ * Written by Fabrice Bellard on January 8, 1997.
+ *
+ * We use a slightly modified version of the method described by Simon
+ * Plouffe in "On the Computation of the n'th decimal digit of various
+ * transcendental numbers" (November 1996). We have modified the algorithm
+ * to get a running time of O(n^2) instead of O(n^3log(n)^3).
+ */
+
+#ifndef _BELLARD_PI_H
+#define _BELLARD_PI_H
+
+int pi_nth_digit(int n);
+
+#endif /*_BELLARD_PI_H*/
