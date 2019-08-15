44195d436ed985f938eda7ec76db09fcac9eac40
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-08-15 19:07:39 +0300

    Rename: Clock.markNow, ClockMark.elapsedNow
    Add "Now" suffix to the names of functions that depend on the current
    time moment.

--- libraries/stdlib/test/time/ClockMarkTest.kt
+++ libraries/stdlib/test/time/ClockMarkTest.kt
@@ -14,7 +14,7 @@ class ClockMarkTest {
     fun adjustment() {
         val clock = TestClock()
 
-        val mark = clock.mark()
+        val mark = clock.markNow()
         val markFuture1 = mark + 1.milliseconds
         val markFuture2 = mark - (-1).milliseconds
 
@@ -23,15 +23,15 @@ class ClockMarkTest {
 
         clock += 500_000.nanoseconds
 
-        val elapsed = mark.elapsed()
+        val elapsed = mark.elapsedNow()
         val elapsedFromFuture = elapsed - 1.milliseconds
         val elapsedFromPast = elapsed + 1.milliseconds
 
         assertEquals(0.5.milliseconds, elapsed)
-        assertEquals(elapsedFromFuture, markFuture1.elapsed())
-        assertEquals(elapsedFromFuture, markFuture2.elapsed())
+        assertEquals(elapsedFromFuture, markFuture1.elapsedNow())
+        assertEquals(elapsedFromFuture, markFuture2.elapsedNow())
 
-        assertEquals(elapsedFromPast, markPast1.elapsed())
-        assertEquals(elapsedFromPast, markPast2.elapsed())
+        assertEquals(elapsedFromPast, markPast1.elapsedNow())
+        assertEquals(elapsedFromPast, markPast2.elapsedNow())
     }
 }
