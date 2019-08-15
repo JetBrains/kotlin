2ea1afeb8950775e0d082d15dda87470a776bdaa
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-08-15 11:10:36 +0300

    don't show thumbnails images action in the project view
    fix keymap test
    
    intellij-monorepo: 7f8c5b74d3138dea8a6b0c6c9b2c10593f60f7a0

--- intellij-community/platform/testFramework/extensions/src/com/intellij/keymap/KeymapsTestCase.java
+++ intellij-community/platform/testFramework/extensions/src/com/intellij/keymap/KeymapsTestCase.java
@@ -96,7 +96,7 @@ public abstract class KeymapsTestCase extends KeymapsTestCaseBase {
     { "shift control H",          "ChangesView.ShelveSilently", "MethodHierarchy"},
     { "shift control LEFT",       "EditorPreviousWordWithSelection", "ResizeToolWindowLeft"},
     { "shift control RIGHT",      "EditorNextWordWithSelection", "ResizeToolWindowRight"},
-    { "shift control T",          "GotoTest", "Images.ShowThumbnails", "RunDashboard.ShowConfigurations"},
+    { "shift control T",          "GotoTest", "RunDashboard.ShowConfigurations"},
     { "shift control UP",         "ResizeToolWindowUp", "MoveStatementUp"},
     { "shift control U",          "ShelveChanges.UnshelveWithDialog", "EditorToggleCase"},
     { "shift control alt D",      "UML.ShowChanges", "Console.TableResult.CloneColumn"},
@@ -361,7 +361,7 @@ public abstract class KeymapsTestCase extends KeymapsTestCaseBase {
     { "shift ctrl D",             "Console.TableResult.CompareCells", "EditorDuplicate", "Uml.ShowDiff"},
     { "shift ctrl DOWN",          "MoveLineDown", "ResizeToolWindowDown"},
     { "shift ctrl ENTER",         "Console.Jpa.GenerateSql", "EditorStartNewLineBefore"},
-    { "shift ctrl T",             "Images.ShowThumbnails", "ReopenClosedTab", "RunDashboard.ShowConfigurations"},
+    { "shift ctrl T",             "ReopenClosedTab", "RunDashboard.ShowConfigurations"},
     { "shift ctrl UP",            "MoveLineUp", "ResizeToolWindowUp"}
     });
     put("Sublime Text (Mac OS X)", new String[][] {
@@ -384,7 +384,7 @@ public abstract class KeymapsTestCase extends KeymapsTestCaseBase {
     { "shift ENTER",              "CollapseExpandableComponent", "Console.TableResult.EditValueMaximized", "EditorSplitLine", "ExpandExpandableComponent", "JupyterRunCellSelectBelowAction", "OpenElementInNewWindow"},
     { "shift meta D",             "Console.TableResult.CompareCells", "EditorDuplicate", "Uml.ShowDiff"},
     { "shift meta ENTER",         "Console.Jpa.GenerateSql", "EditorStartNewLineBefore"},
-    { "shift meta T",             "Images.ShowThumbnails", "ReopenClosedTab", "RunDashboard.ShowConfigurations"}
+    { "shift meta T",             "ReopenClosedTab", "RunDashboard.ShowConfigurations"}
     });
   }};
   // @formatter:on
