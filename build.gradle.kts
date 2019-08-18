6bd229ce33887d62a365933da0d45be385560d3a
nikita.movshin <nikita.movshin@jetbrains.com>
2019-08-19 00:05:26 +0300

    Update Kotlin Native version

--- build.gradle.kts
+++ build.gradle.kts
@@ -171,7 +171,7 @@ extra["versions.trove4j"] = "1.0.20181211"
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.3.50-eap-11751"
+    extra["versions.kotlin-native"] = "1.3.50-release-11850"
 }
 
 val isTeamcityBuild = project.kotlinBuildProperties.isTeamcityBuild
