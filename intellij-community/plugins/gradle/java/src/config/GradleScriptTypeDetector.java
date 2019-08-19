f8c06ca519964f162e4f7ead6cce2311b5190676
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-08-19 14:51:33 +0300

    [groovy] register script type extensions via fileType in xml-s
    intellij-monorepo: 7b78b95b028160c5a2d066865a4045434a375a4d

--- intellij-community/plugins/gradle/java/src/config/GradleScriptTypeDetector.java
+++ intellij-community/plugins/gradle/java/src/config/GradleScriptTypeDetector.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2010 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.gradle.config;
 
 import com.intellij.internal.statistic.collectors.fus.fileTypes.FileTypeUsageSchemaDescriptor;
@@ -28,8 +14,9 @@ import org.jetbrains.plugins.groovy.lang.psi.GroovyFile;
  * @author sergey.evdokimov
  */
 public class GradleScriptTypeDetector extends GroovyScriptTypeDetector implements FileTypeUsageSchemaDescriptor {
+
   public GradleScriptTypeDetector() {
-    super(GradleScriptType.INSTANCE, GradleConstants.EXTENSION);
+    super(GradleScriptType.INSTANCE);
   }
 
   @Override
