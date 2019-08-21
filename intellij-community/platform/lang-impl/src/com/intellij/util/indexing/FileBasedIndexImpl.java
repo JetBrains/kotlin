169b7e72828e8b6c8256dbac508072435e7248c4
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-08-21 10:44:09 +0300

    throw assertion error when indexes are accessed during indexing
    intellij-monorepo: b763dc8768c912a499f78bc85599302244d776a6

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
@@ -725,7 +725,9 @@ public final class FileBasedIndexImpl extends FileBasedIndex implements Disposab
     ProgressManager.checkCanceled();
     myChangedFilesCollector.ensureUpToDate();
     ApplicationManager.getApplication().assertReadAccessAllowed();
-
+    if (getFileBeingCurrentlyIndexed() != null) {
+      throw new AssertionError("Indexes should not been accessed while indexing in progress");
+    }
     NoAccessDuringPsiEvents.checkCallContext();
 
     if (!needsFileContentLoading(indexId)) {
