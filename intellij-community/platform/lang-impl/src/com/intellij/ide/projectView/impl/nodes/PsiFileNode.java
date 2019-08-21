4302b9357d33096e1c5e3ad54aa0cb35c66ff54c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-08-21 22:22:23 +0200

    [ui] moves "invalid symlink" tree tooltip message to a platform bundle
    intellij-monorepo: bbeff79310f162a2b5592c0872f71b359d2e8a4f

--- intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/nodes/PsiFileNode.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/nodes/PsiFileNode.java
@@ -1,21 +1,7 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView.impl.nodes;
 
-import com.intellij.CommonBundle;
+import com.intellij.ide.IdeBundle;
 import com.intellij.ide.highlighter.ArchiveFileType;
 import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.projectView.ViewSettings;
@@ -80,7 +66,7 @@ public class PsiFileNode extends BasePsiNode<PsiFile> implements NavigatableWith
       String target = file.getCanonicalPath();
       if (target == null) {
         data.setAttributesKey(CodeInsightColors.WRONG_REFERENCES_ATTRIBUTES);
-        data.setTooltip(CommonBundle.message("vfs.broken.link"));
+        data.setTooltip(IdeBundle.message("node.project.view.bad.link"));
       }
       else {
         data.setTooltip(FileUtil.toSystemDependentName(target));
