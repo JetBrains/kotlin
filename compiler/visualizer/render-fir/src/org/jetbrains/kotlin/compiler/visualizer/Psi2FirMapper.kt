1c680552084fe6682164439a99713a8000d03b7b
Ivan Cilcic <cilcic.v22@gmail.com>
2019-08-28 15:37:24 +0300

    Create fir renderer class

---
+++ compiler/visualizer/render-fir/src/org/jetbrains/kotlin/compiler/visualizer/Psi2FirMapper.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.compiler.visualizer
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.fir.FirElement
+import org.jetbrains.kotlin.fir.visitors.FirVisitorVoid
+
+class Psi2FirMapper(val map: MutableMap<PsiElement, MutableList<FirElement>>) : FirVisitorVoid() {
+    override fun visitElement(element: FirElement) {
+        val psi = element.psi
+        if (psi != null) {
+            if (map.putIfAbsent(psi, mutableListOf(element)) != null) {
+                map[psi]?.add(element)
+            }
+        }
+        element.acceptChildren(this)
+    }
+}
