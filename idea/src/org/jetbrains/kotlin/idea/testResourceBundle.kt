51cfafc619c2b88bd136b6392bfd55b8c68d4829
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-08-28 13:26:35 +0300

    Minor: advance comment
     #KT-33536 Fixed

--- idea/src/org/jetbrains/kotlin/idea/testResourceBundle.kt
+++ idea/src/org/jetbrains/kotlin/idea/testResourceBundle.kt
@@ -6,6 +6,6 @@
 package org.jetbrains.kotlin.idea
 
 // Remove the function, when there's no dependency to cidr during running Kotlin tests.
-// BUNCH: as33
+// BUNCH: as34
 fun registerAdditionalResourceBundleInTests() {
 }
