f8b5464f63e817dcca4682369dbb8509cf824ee2
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-08-29 17:09:37 +0300

    after invocation remove only `QuickFixWrapper`s from HighlightInfo, leave other `IntentionAction`s (IDEA-171439)
    intellij-monorepo: 541ae8721de2c9f1a9a7f592eb00ec54ec8a0e18

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/intention/impl/CachedIntentions.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/intention/impl/CachedIntentions.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.intention.impl;
 
 import com.intellij.codeInsight.daemon.impl.HighlightInfo;
@@ -236,8 +236,12 @@ public class CachedIntentions {
                                             @Nullable PsiFile containingFile,
                                             @Nullable Editor containingEditor) {
     IntentionActionWithTextCaching cachedAction = new IntentionActionWithTextCaching(descriptor, (cached, action)->{
-      removeActionFromCached(cached);
-      markInvoked(action);
+      if (action instanceof QuickFixWrapper) {
+        // remove only inspection fixes after invocation,
+        // since intention actions might be still available
+        removeActionFromCached(cached);
+        markInvoked(action);
+      }
     });
     if (element == null) return cachedAction;
     final List<IntentionAction> options = descriptor.getOptions(element, containingEditor);
