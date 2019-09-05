d39dc259528c742d840167acc38061c3ff6d02a2
Steven Schäfer <stevenschaefer@google.com>
2019-09-05 11:12:40 +0200

    Split up Result API boxing tests

---
+++ compiler/testData/codegen/bytecodeText/inlineClasses/resultApiRunCatchingDoesntBox.kt
@@ -0,0 +1,16 @@
+// IGNORE_BACKEND: JVM
+// IGNORE_BACKEND: JVM_IR
+// WITH_RUNTIME
+// FILE: test.kt
+fun test() {
+    val ans1 = runCatching { 42 }
+    println(ans1)
+
+    val ans2 = 42.runCatching { this }
+    println(ans2)
+}
+
+// @TestKt.class:
+// 0 INVOKESTATIC kotlin/Result.box-impl
+// 0 INVOKEVIRTUAL kotlin/Result.unbox-impl
+// 0 Result\$Failure
