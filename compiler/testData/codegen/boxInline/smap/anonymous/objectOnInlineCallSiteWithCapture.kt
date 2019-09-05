86d7e3537b09de1af4bbe9e3f233d36103f16759
pyos <pyos@google.com>
2019-09-05 10:22:59 +0200

    JVM_IR: discard parameter annotations in anonymous object constructors

--- compiler/testData/codegen/boxInline/smap/anonymous/objectOnInlineCallSiteWithCapture.kt
+++ compiler/testData/codegen/boxInline/smap/anonymous/objectOnInlineCallSiteWithCapture.kt
@@ -1,6 +1,6 @@
 // FILE: 1.kt
-// IGNORE_BACKEND: JVM_IR
-// IGNORE_BACKEND_MULTI_MODULE: JVM_IR
+
+
 package builders
 //TODO there is a bug in asm it's skips linenumber on same line on reading bytecode
 inline fun call(crossinline init: () -> Unit) {
