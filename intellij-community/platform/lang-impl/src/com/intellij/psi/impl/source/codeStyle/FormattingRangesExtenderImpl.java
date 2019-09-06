f400eccbedd4bea5fbf2d8bfeec00cdd7d1286cb
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-09-06 12:32:10 +0300

    Use proper ranges within document boundaries
    intellij-monorepo: 2da3fc235f7e13f701075268899f99ba2b1b679e

--- intellij-community/platform/lang-impl/src/com/intellij/psi/impl/source/codeStyle/FormattingRangesExtenderImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/impl/source/codeStyle/FormattingRangesExtenderImpl.java
@@ -3,19 +3,26 @@ package com.intellij.psi.impl.source.codeStyle;
 
 import com.intellij.formatting.FormatTextRanges;
 import com.intellij.formatting.FormattingRangesExtender;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.UnfairTextRange;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 class FormattingRangesExtenderImpl implements FormattingRangesExtender {
+  private final Document myDocument;
+
+  FormattingRangesExtenderImpl(@NotNull Document document) {
+    myDocument = document;
+  }
 
   @Override
   public List<TextRange> getExtendedRanges(@NotNull FormatTextRanges ranges) {
     return ContainerUtil.map(ranges.getTextRanges(), (range) -> {
-      return new UnfairTextRange(range.getStartOffset() - 500, range.getEndOffset() + 500);
+      int startOffset = Math.max(range.getStartOffset() - 500, 0);
+      int endOffset = Math.min(range.getEndOffset() + 500, myDocument.getTextLength());
+      return new TextRange(startOffset, endOffset);
     });
   }
 }
