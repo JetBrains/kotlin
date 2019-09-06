9b36fa9c96b094072616daea2db7dca2a199c901
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2019-09-06 19:14:59 +0300

    Mute LVT test for variadic generic functions

--- compiler/testData/checkLocalVariablesTable/variadicParameters.kt
+++ compiler/testData/checkLocalVariablesTable/variadicParameters.kt
@@ -1,4 +1,5 @@
 // IGNORE_BACKEND: JVM_IR
+// IGNORE_BACKEND: JVM
 // WITH_RUNTIME
 // !LANGUAGE: +NewInference
 
@@ -19,6 +20,7 @@ fun test() {
 // METHOD : VariadicParametersKt$test$1.invoke(Ljava/lang/Object;Lkotlin/Tuple;)V
 // VARIABLE : NAME=this TYPE=LVariadicParametersKt$test$1; INDEX=0
 // VARIABLE : NAME=boo TYPE=Ljava/lang/Object; INDEX=1
-// VARIABLE : NAME=one TYPE=I INDEX=2
-// VARIABLE : NAME=two TYPE=I INDEX=3
-// VARIABLE : NAME=foo TYPE=Ljava/lang/String; INDEX=4
+// VARIABLE : NAME=variadicLambdaArguments TYPE=Lkotlin/Tuple; INDEX=2
+// VARIABLE : NAME=one TYPE=I INDEX=3
+// VARIABLE : NAME=two TYPE=I INDEX=4
+// VARIABLE : NAME=foo TYPE=Ljava/lang/String; INDEX=5
