540da53807a41415eb03ef400d61897807ddca90
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2019-09-09 20:33:46 +0300

    [WASM] Remove copied passed common tests
    They are not needed since proper common tests are included

--- js/js.translator/testData/wasmBox/passedCommonTests/primitiveTypes/kt877.kt
+++
@@ -1,9 +0,0 @@
-fun box() : String {
-  var a : Int = -1
-  if((+a) != -1) return "fail 1"
-  a = 1
-  if((+a) != 1) return "fail 2"
-  if((+-1) != -1) return "fail 3"
-  if((-+a) != -1) return "fail 4"
-  return "OK"
-}
