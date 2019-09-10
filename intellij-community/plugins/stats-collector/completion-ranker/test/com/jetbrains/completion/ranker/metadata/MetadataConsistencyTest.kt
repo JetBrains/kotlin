8c09f7274c72932a3aeface26e3aa5d5be56745b
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2019-09-10 16:29:19 +0300

    [stats-collector] Add ability to test consistency of model's metadata
    intellij-monorepo: dc1676d288e69da349bec38a0d892cc9d9ed60d5

--- intellij-community/plugins/stats-collector/completion-ranker/test/com/jetbrains/completion/ranker/metadata/MetadataConsistencyTest.kt
+++ intellij-community/plugins/stats-collector/completion-ranker/test/com/jetbrains/completion/ranker/metadata/MetadataConsistencyTest.kt
@@ -1,21 +1,14 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.completion.ranker.metadata
 
-import com.intellij.internal.ml.completion.RankingModelProvider
 import com.jetbrains.completion.ranker.KotlinMLRankingProvider
 import com.jetbrains.completion.ranker.PythonMLRankingProvider
 import org.junit.Test
 
 class MetadataConsistencyTest {
   @Test
-  fun testKotlinMetadata() = doTest(KotlinMLRankingProvider())
+  fun testKotlinMetadata() = KotlinMLRankingProvider().assertModelMetadataConsistent()
 
   @Test
-  fun testPythonMetadata() = doTest(PythonMLRankingProvider())
-
-  private fun doTest(provider: RankingModelProvider) {
-    val model = provider.model
-    val order = model.featuresOrder
-    model.predict(DoubleArray(order.size))
-  }
+  fun testPythonMetadata() = PythonMLRankingProvider().assertModelMetadataConsistent()
 }
