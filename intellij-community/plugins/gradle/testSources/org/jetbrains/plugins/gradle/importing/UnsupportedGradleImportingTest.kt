d8cc5331fbca3fd186a76910fd27c95cb5355499
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2019-09-11 15:34:10 +0300

    [Gradle] importing tests added for Build tw error messages related to unsupported Gradle versions
    intellij-monorepo: 429a9b47eee03c55cf933ceb8fbee1831b4ad59f

--- intellij-community/plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/UnsupportedGradleImportingTest.kt
+++ intellij-community/plugins/gradle/testSources/org/jetbrains/plugins/gradle/importing/UnsupportedGradleImportingTest.kt
@@ -54,9 +54,9 @@ class UnsupportedGradleImportingTest : SyncViewMessagesImportingTestCase() {
 
   companion object {
     private val OLD_GRADLE_VERSIONS = arrayOf(
-      arrayOf<Any>("0.7"), arrayOf<Any>("0.8"), arrayOf<Any>("0.9.2"),
+      arrayOf<Any>("0.7"), /*arrayOf<Any>("0.8"), arrayOf<Any>("0.9"), ..., */arrayOf<Any>("0.9.2"),
       arrayOf<Any>("1.0"), /*arrayOf<Any>("1.1"), arrayOf<Any>("1.2"), ..., */arrayOf<Any>("1.12"),
-      arrayOf<Any>("2.0"), /*arrayOf<Any>("2.1"),arrayOf<Any>("2.2"), ..., */arrayOf<Any>("2.5"))
+      arrayOf<Any>("2.0"), /*arrayOf<Any>("2.1"), arrayOf<Any>("2.2"), ..., */arrayOf<Any>("2.5"))
 
     /**
      * Run the test against very old not-supported Gradle versions also
