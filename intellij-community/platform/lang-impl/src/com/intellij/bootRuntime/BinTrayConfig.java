c3e5d1c952b60aa3e641dc9a3d31f3c7de465eb2
Denis Fokin <denis.fokin@jetbrains.com>
2019-09-12 18:48:31 +0300

    Eliminate Switch Boot JDK/Runtime actions. Choose Runtime plugin is available on the Marketplace
    intellij-monorepo: 33d0f5996c9bff57d3d60611739bfceb1eafccd2

--- intellij-community/platform/lang-impl/src/com/intellij/bootRuntime/BinTrayConfig.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.bootRuntime;
-
-public interface BinTrayConfig {
-  String subject = "jetbrains";
-  String repoName = "intellij-jdk";
-  String jbrRepoName = "intellij-jbr";
-  String urlPattern = "https://dl.bintray.com/%s/%s/%s";
-}
