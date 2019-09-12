a82b9bed79d4536d3f401e32331823683562c208
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-09-12 12:10:01 +0200

    Cleanup (deprecated API)
    intellij-monorepo: 1260e3a06aa57dbc3d9f563d0e52719101aa7c05

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/lang/ExtractIncludeDialog.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/lang/ExtractIncludeDialog.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.lang;
 
 import com.intellij.ide.util.DirectoryUtil;
@@ -21,7 +21,6 @@ import com.intellij.refactoring.RefactoringBundle;
 import com.intellij.refactoring.util.CommonRefactoringUtil;
 import com.intellij.ui.DocumentAdapter;
 import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -158,15 +157,9 @@ public class ExtractIncludeDialog extends DialogWrapper {
     super.doOKAction();
   }
 
-  @Deprecated
-  @ApiStatus.ScheduledForRemoval(inVersion = "2019.1")
-  protected String getHelpTopic() {
-    return ExtractIncludeFileBase.HELP_ID;
-  }
-
   @Override
   protected String getHelpId() {
-    return getHelpTopic();
+    return ExtractIncludeFileBase.HELP_ID;
   }
 
   protected void hideTargetDirectory() {
