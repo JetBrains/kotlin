69097e7c7cfb3dee38d19342d12aa5b4062348a4
Yann Cébron <yann.cebron@jetbrains.com>
2019-09-12 11:55:36 +0200

    [external system] add @deprecation javadoc
    intellij-monorepo: 745361a9752bb43dabf49ce6d8d278e045c72a29

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/autoimport/ConfigurationFileCrcFactory.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/project/autoimport/ConfigurationFileCrcFactory.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.service.project.autoimport;
 
 import com.intellij.lang.Language;
@@ -28,7 +28,10 @@ public class ConfigurationFileCrcFactory {
 
   private final VirtualFile myFile;
 
-  @Deprecated // left for plugin compatibility
+  /**
+   * @deprecated left for plugin compatibility
+   */
+  @Deprecated
   public ConfigurationFileCrcFactory(@NotNull Project project, @NotNull VirtualFile file) {
     this(file);
   }
