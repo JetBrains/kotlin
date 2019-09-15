5e24e432e209993e51ff09da65cd9bf22f601342
Michael Golubev <michael.golubev@jetbrains.com>
2019-09-15 22:51:56 +0200

    Remote environments: new target wizard - initial API
    intellij-monorepo: 50beba0e77d8ff7afa62d963fdf9dcf3d49beb3a

---
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/remote/RemoteTargetWizard.kt
@@ -0,0 +1,20 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.remote
+
+import com.intellij.ide.wizard.AbstractWizardEx
+import com.intellij.ide.wizard.AbstractWizardStepEx
+import com.intellij.openapi.project.Project
+
+class RemoteTargetWizard(title: String, project: Project, steps: List<AbstractWizardStepEx>)
+  : AbstractWizardEx(title, project, steps) {
+
+  companion object {
+    fun createWizard(project: Project, target: RemoteTargetType<*>, runtime: LanguageRuntimeType<*>?): RemoteTargetWizard? {
+      if (!target.providesNewWizard(project, runtime)) return null
+
+      val title = "Where is Wizard title?" //FIXME
+      return target.createStepsForNewWizard(project, runtime)
+        ?.let { RemoteTargetWizard(title, project, it) }
+    }
+  }
+}
