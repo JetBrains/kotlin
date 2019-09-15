e392785a1f7f7ed5077a824ba00c19fefd44cd14
Alexander Zolotov <zolotov@jetbrains.com>
2019-09-15 18:19:04 +0300

    Runtime environments: simplify execution targets, added configuration into runner
    intellij-monorepo: cb567d8511336d19262715e7a50d25640f8fe5e0

--- intellij-community/platform/lang-impl/src/com/intellij/execution/remote/RemoteRunnersExecutionTargetProvider.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/remote/RemoteRunnersExecutionTargetProvider.kt
@@ -7,7 +7,6 @@ import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
 
 class RemoteRunnersExecutionTargetProvider : ExecutionTargetProvider() {
-
   override fun getTargets(project: Project, configuration: RunConfiguration): List<ExecutionTarget> {
     return RemoteTargetsManager.instance.targets.resolvedConfigs()
       .map { it.getTargetType().createExecutionTarget(project, it) }
