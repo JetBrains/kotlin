ad6399041e91cefadc514ecc62b5c2c0de6f8a2c
Michael Golubev <michael.golubev@jetbrains.com>
2019-09-16 10:56:50 +0200

    Remote environments: docker target wizard
    - RemoteTargetType: allow to pass instance to be edited in wizard from outside, to provide wizard "results"
    - LanguageRuntimeType: allow language specific wizard title
    
    intellij-monorepo: 95afc720c3473408961eba54b36b74b7f753961c

--- intellij-community/platform/lang-impl/src/com/intellij/execution/remote/RemoteTargetWizard.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/remote/RemoteTargetWizard.kt
@@ -5,16 +5,20 @@ import com.intellij.ide.wizard.AbstractWizardEx
 import com.intellij.ide.wizard.AbstractWizardStepEx
 import com.intellij.openapi.project.Project
 
-class RemoteTargetWizard(title: String, project: Project, steps: List<AbstractWizardStepEx>)
+class RemoteTargetWizard(project: Project, title: String, val subject: RemoteTargetConfiguration, steps: List<AbstractWizardStepEx>)
   : AbstractWizardEx(title, project, steps) {
 
   companion object {
-    fun createWizard(project: Project, target: RemoteTargetType<*>, runtime: LanguageRuntimeType<*>?): RemoteTargetWizard? {
+    fun <TC : RemoteTargetConfiguration> createWizard(project: Project,
+                                                      target: RemoteTargetType<TC>,
+                                                      runtime: LanguageRuntimeType<*>?): RemoteTargetWizard? {
+
       if (!target.providesNewWizard(project, runtime)) return null
 
-      val title = "Where is Wizard title?" //FIXME
-      return target.createStepsForNewWizard(project, runtime)
-        ?.let { RemoteTargetWizard(title, project, it) }
+      val instanceToConfigure: TC = target.createDefaultConfig()
+
+      return target.createStepsForNewWizard(project, instanceToConfigure, runtime)
+        ?.let { RemoteTargetWizard(project, "", instanceToConfigure, it) }
     }
   }
 }
