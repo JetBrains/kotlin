022ae83759541ba0f7190177a29f980ee9fbe5b0
Eugene Zhuravlev <jeka@intellij.com>
2019-09-17 15:53:06 +0200

    context help label for the --processor-module-path option (followup IDEA-220430)
    intellij-monorepo: fdec40573e5aebf44df18ae55b04d8108fcb602f

--- intellij-community/java/compiler/impl/src/com/intellij/compiler/options/ProcessorProfilePanel.java
+++ intellij-community/java/compiler/impl/src/com/intellij/compiler/options/ProcessorProfilePanel.java
@@ -82,6 +82,10 @@ public class ProcessorProfilePanel extends JPanel {
       ButtonGroup group = new ButtonGroup();
       group.add(myRbClasspath);
       group.add(myRbProcessorsPath);
+      //myCbProcessorModulePath.setToolTipText(
+      //  "Assume specified path is a java module path. A 'module path' is path of jars or directories containing packaged java modules." +
+      //  "\nIn order to be discovered and run from the module path an annotation processor should be packaged as a java module."
+      //);
     }
 
     {
@@ -138,7 +142,12 @@ public class ProcessorProfilePanel extends JPanel {
     add(myProcessorPathField,
         new GridBagConstraints(0, GridBagConstraints.RELATIVE, 3, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, JBUI.insets(5, 5, 0, 0), 0, 0));
     add(myCbProcessorModulePath,
-        new GridBagConstraints(0, GridBagConstraints.RELATIVE, 3, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, JBUI.insets(2, 5, 0, 0), 0, 0));
+        new GridBagConstraints(0, GridBagConstraints.RELATIVE, 2, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, JBUI.insets(2, 5, 0, 0), 0, 0));
+
+    add(ContextHelpLabel.create(
+      "Assume specified path is a 'module path': a path of jars or directories containing packaged java modules." +
+      "\nIn order to be discovered and run from the module path an annotation processor should be packaged as a java module and registered with the ServiceLoader in the module declaration."
+    ), new GridBagConstraints(2, GridBagConstraints.RELATIVE, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, JBUI.insets(2, 2, 0, 0), 0, 0));
 
     myStoreGenSourcesLabel = new JLabel("Store generated sources relative to: ");
     add(myStoreGenSourcesLabel,
