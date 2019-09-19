6754f568fb65f07b8232e513658659fcb07f93af
Roman Artemev <roman.artemev@jetbrains.com>
2019-09-19 14:38:52 +0300

    Implement carriers

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/nextgen/Carriers.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/nextgen/Carriers.kt
@@ -5,22 +5,44 @@
 
 package org.jetbrains.kotlin.backend.common.serialization.nextgen
 
-class DeclarationBaseCarrier {
+import org.jetbrains.kotlin.descriptors.Visibility
+import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
+import org.jetbrains.kotlin.ir.declarations.IrTypeParameter
+import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.IrStatementOrigin
+import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.types.IrTypeArgument
 
-}
+class DeclarationBaseCarrier(
+    val symbolId: Int,
+    val origin: IrDeclarationOrigin,
+    val coordinates: Pair<Int, Int>,
+    val annotations: List<IrConstructorCall>
+)
 
-class FunctionBaseCarrier {
+class FunctionBaseCarrier(
+    val declarationBaseCarrier: DeclarationBaseCarrier,
+    val nameId: Int,
+    val visibility: Visibility,
+    val isInline: Boolean,
+    val isExternal: Boolean,
+    val typeParameters: List<IrTypeParameter>,
+    val dispathReceiver: IrValueParameter?,
+    val extensionReceiver: IrValueParameter?,
+    val valueParameters: List<IrValueParameter>,
+    val bodyIndex: Int,
+    val returnTypeIndex: Int
+)
 
-}
+class MemberAccessCarrier(
+    val dispatchReceiver: IrExpression?,
+    val extensionReceiver: IrExpression?,
+    val valueArguments: List<IrExpression>,
+    val typeArguments: List<IrType>
+)
 
-class MemberAccessCarrier {
+class FieldAccessCarrier(val symbolId: Int, val superId: Int, val receiver: IrExpression?)
 
-}
-
-class FieldAccessCarrier {
-
-}
-
-class LoopCarrier {
-
-}
+class LoopCarrier(val loopId: Int, val condition: IrExpression, val label: Int?, val body: IrExpression?, val origin: IrStatementOrigin?)
