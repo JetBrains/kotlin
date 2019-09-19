b59fba6707127f63eb6a1a90a6205accfbd1fab1
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2019-09-19 13:46:23 +0300

    Fix mocha run
    #KT-33579 fixed

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/mocha/KotlinMocha.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/mocha/KotlinMocha.kt
@@ -55,12 +55,10 @@ class KotlinMocha(override val compilation: KotlinJsCompilation) : KotlinJsTestF
 
         val args = nodeJsArgs +
                 nodeModules.map {
-                    npmProject.nodeModulesDir.resolve(it).also { file ->
-                        check(file.isFile) { "Cannot find ${file.canonicalPath}" }
-                    }.canonicalPath
+                    npmProject.require(it)
                 } +
                 listOf(
-                    "-r", "kotlin-nodejs-source-map-support.js",
+                    "-r", "kotlin-test-nodejs-runner/kotlin-nodejs-source-map-support.js",
                     "--reporter", "mocha-teamcity-reporter"
                 )
 
