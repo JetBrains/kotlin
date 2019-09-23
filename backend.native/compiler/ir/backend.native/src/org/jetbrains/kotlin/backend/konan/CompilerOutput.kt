a98e5c799cd7612b014b6bc926c899922eea2453
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2019-09-23 14:11:08 +0300

    [REVERTME][compiler] {STATIC,DYNAMIC}_CACHE holder

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/CompilerOutput.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/CompilerOutput.kt
@@ -23,6 +23,7 @@ val CompilerOutputKind.isFinalBinary: Boolean get() = when (this) {
     CompilerOutputKind.PROGRAM, CompilerOutputKind.DYNAMIC,
     CompilerOutputKind.STATIC, CompilerOutputKind.FRAMEWORK -> true
     CompilerOutputKind.LIBRARY, CompilerOutputKind.BITCODE -> false
+    CompilerOutputKind.DYNAMIC_CACHE, CompilerOutputKind.STATIC_CACHE -> TODO("$this unsupported")
 }
 
 val CompilerOutputKind.involvesBitcodeGeneration: Boolean
@@ -36,6 +37,7 @@ val CompilerOutputKind.involvesLinkStage: Boolean
         CompilerOutputKind.PROGRAM, CompilerOutputKind.DYNAMIC,
         CompilerOutputKind.STATIC, CompilerOutputKind.FRAMEWORK -> true
         CompilerOutputKind.LIBRARY, CompilerOutputKind.BITCODE -> false
+        CompilerOutputKind.DYNAMIC_CACHE, CompilerOutputKind.STATIC_CACHE -> TODO("$this unsupported")
     }
 
 internal fun produceCStubs(context: Context) {
