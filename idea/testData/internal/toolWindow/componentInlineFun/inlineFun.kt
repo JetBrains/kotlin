78affdd2b466a1a83ca40f5e1ee8ea8544a8bf85
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2019-09-24 09:50:03 +0300

    Fix old testdata: add missing operator modifier
     This testdata was written when there was no "operator" modifier.
    
     After a83225218f9a597e366c7bfd45530df7c1da94b0 rules for operators become stricter and now functions without operators are not considered as such

--- idea/testData/internal/toolWindow/componentInlineFun/inlineFun.kt
+++ idea/testData/internal/toolWindow/componentInlineFun/inlineFun.kt
@@ -1,8 +1,8 @@
 package inlineFun1
 
 class A {
-    inline fun component1() = foo { 1 }
-    inline fun component2() = foo { 2 }
+    inline operator fun component1() = foo { 1 }
+    inline operator fun component2() = foo { 2 }
 
     inline fun foo(f: () -> Int) = f()
 }
