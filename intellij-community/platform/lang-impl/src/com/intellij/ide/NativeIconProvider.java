65c0f43eb4c577cce9453ee8c1ef8e8c032134a3
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2019-09-25 19:41:19 +0300

    IDEA-171222: Module uses wrong icon in project tree
    intellij-monorepo: 8223ce53f7dfd3b6f26357cc93fd750f77c5717b

--- intellij-community/platform/lang-impl/src/com/intellij/ide/NativeIconProvider.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/NativeIconProvider.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.util.Iconable;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFileSystemItem;
 import com.intellij.psi.impl.ElementBase;
@@ -39,6 +40,7 @@ public class NativeIconProvider extends IconProvider implements DumbAware {
   @Nullable
   @Override
   public Icon getIcon(@NotNull PsiElement element, @Iconable.IconFlags int flags) {
+    if (element instanceof PsiDirectory) return null;
     if (element instanceof PsiFileSystemItem) {
       VirtualFile file = ((PsiFileSystemItem)element).getVirtualFile();
       if (file != null) return doGetIcon(file, flags);
