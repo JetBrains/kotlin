759141545a1bd380e9d57391e2e4e522a8298ba4
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-09-28 08:51:10 +0900

    Change parameter type quick fix: don't use qualified name
    #KT-32012 Fixed
    
    (cherry picked from commit ac7e955d3e7a0450b653effe713b4377607d8a37)

--- idea/testData/quickfix/typeMismatch/parameterTypeMismatch/changeFunctionParameterType1.kt
+++ idea/testData/quickfix/typeMismatch/parameterTypeMismatch/changeFunctionParameterType1.kt
@@ -1,4 +1,4 @@
-// "Change parameter 'x' type of function 'bar.foo' to '(String) -> Int'" "true"
+// "Change parameter 'x' type of function 'foo' to '(String) -> Int'" "true"
 package bar
 fun foo(w: Int = 0, x: Int, y: Int = 0, z: (Int) -> Int = {42}) {
     foo(1, { a: String -> 42}<caret>, 1)
