d74e5b3f40d7aaa75b02f2eb431d5fbc365cc528
Anatoly Nikitin <Anatoly.Nikitin@jetbrains.com>
2019-09-27 14:09:15 +0300

    Added bridge to GenericResolver

--- libraries/scripting/dependencies/src/kotlin/script/experimental/bridges.kt
+++ libraries/scripting/dependencies/src/kotlin/script/experimental/bridges.kt
@@ -8,11 +8,14 @@ package kotlin.script.experimental
 import org.jetbrains.kotlin.script.util.DependsOn
 import org.jetbrains.kotlin.script.util.Repository
 import org.jetbrains.kotlin.script.util.resolvers.Resolver
+import org.jetbrains.kotlin.script.util.resolvers.experimental.GenericArtifactCoordinates
+import org.jetbrains.kotlin.script.util.resolvers.experimental.GenericRepositoryCoordinates
+import org.jetbrains.kotlin.script.util.resolvers.experimental.GenericResolver
 import org.jetbrains.kotlin.script.util.resolvers.experimental.MavenArtifactCoordinates
 import java.io.File
 
-fun GenericDependenciesResolver.asOldResolver() : Resolver {
-    return object: Resolver{
+fun GenericDependenciesResolver.asOldResolver(): Resolver =
+    object : Resolver {
         override fun tryResolve(dependsOn: DependsOn): Iterable<File>? =
             tryResolve(
                 with(dependsOn) {
@@ -27,5 +30,18 @@ fun GenericDependenciesResolver.asOldResolver() : Resolver {
                     id.takeIf { it.isNotBlank() }
                 )
             }
+    }
+
+
+fun GenericDependenciesResolver.asOldGenericResolver(): GenericResolver {
+    val me = this
+    return object : GenericResolver {
+        override fun tryAddRepository(repositoryCoordinates: GenericRepositoryCoordinates): Boolean {
+            return me.tryAddRepository(repositoryCoordinates)
+        }
+
+        override fun tryResolve(artifactCoordinates: GenericArtifactCoordinates): Iterable<File>? {
+            return me.tryResolve(artifactCoordinates)
+        }
     }
 }
