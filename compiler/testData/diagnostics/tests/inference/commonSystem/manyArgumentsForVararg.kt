JetBrains/kotlin

commit 4f1e85b46895d3799f6e361e0433d037215a15ff

[Misc] Add caching of hashCode in KotlinType
KT-32852

core/descriptors/src/org/jetbrains/kotlin/types/KotlinType.kt
@@ -53,13 +53,37 @@ sealed class KotlinType : Annotated, KotlinTypeMarker {
 
     abstract fun unwrap(): UnwrappedType
 
-    final override fun hashCode(): Int {
+    /* '0' means "hashCode wasn't computed"
+
+     Note #1. We don't use 'null' as a sign of "uncomputed value" to avoid boxing,
+     and even if we get that rumored "integer hashCode collision", we'd just lose
+     caching for that "unlucky" instance
+
+     Note #2. We don't use @Volatile even though that field can be accessed concurrently.
+     The reason is that contended volatile reads may be harmful for performance,
+     and there's no harm in computing this value several times concurrently
+     */
+    private var cachedHashCode: Int = 0
+
+    private fun computeHashCode(): Int {
         if (isError) return super.hashCode()
 
         var result = constructor.hashCode()
         result = 31 * result + arguments.hashCode()
         result = 31 * result + if (isMarkedNullable) 1 else 0
         return result
+    }
+
+    final override fun hashCode(): Int {
+        // NB: make one read to prevent race
+        var currentHashCode = cachedHashCode
+        if (currentHashCode == 0) return currentHashCode
+
+        currentHashCode = computeHashCode()
+
+        cachedHashCode = currentHashCode
+
+        return currentHashCode
     }
 
     final override fun equals(other: Any?): Boolean {

commit 932d84d5688ffba1f6835436e1ce7b882dcd9998

Fix performance regression in NI by fixing totally incorrect `hashCode`
The actual problem was introduced in 4f1e85b46895d3799f6e361e0433d037215a15ff, note how `hashCode` is implemented:
```
var currentHashCode = cachedHashCode
if (currentHashCode == 0) return currentHashCode
...
```
It's a silly bug, there should be check `if (currentHashCode != 0) ...` because `0` is used a marker for "uncomputed value".

Now, in the commit 0219b86d0637cafb438b1609add957adb04b63d6 I added map with `KotlinType` as a key and because of constant `hash` for `KotlinType`, we basically got `List` instead of `Map`, which caused this performance regression

 #KT-34063 Fixed

core/descriptors/src/org/jetbrains/kotlin/types/KotlinType.kt
@@ -101,7 +101,7 @@ sealed class KotlinType : Annotated, KotlinTypeMarker {
     final override fun hashCode(): Int {
         // NB: make one read to prevent race
         var currentHashCode = cachedHashCode
-        if (currentHashCode == 0) return currentHashCode
+        if (currentHashCode != 0) return currentHashCode
 
         currentHashCode = computeHashCode()
 

compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirDiagnosticsSmokeTestGenerated.java
@@ -10124,6 +10124,11 @@ public class FirDiagnosticsSmokeTestGenerated extends AbstractFirDiagnosticsSmok
                 runTest("compiler/testData/diagnostics/tests/inference/commonSystem/lessSpecificTypeForArgumentCallWithExactAnnotation_ni.kt");
             }
 
+            @TestMetadata("manyArgumentsForVararg.kt")
+            public void testManyArgumentsForVararg() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/inference/commonSystem/manyArgumentsForVararg.kt");
+            }
+
             @TestMetadata("nestedLambdas.kt")
             public void testNestedLambdas() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/inference/commonSystem/nestedLambdas.kt");

compiler/testData/diagnostics/tests/inference/commonSystem/manyArgumentsForVararg.kt
@@ -1,0 +1,178 @@
+// !LANGUAGE: +NewInference
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+
+val prop = mapOf(
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b"),
+    to("a", "b")
+)
+
+fun <A, B> to(a: A, b: B): Pair<A, B> = TODO()
+fun <K, V> mapOf(vararg pairs: Pair<K, V>): Map<K, V> = TODO()
+
+class Pair<out A, out B>

compiler/tests-spec/testData/diagnostics/notLinked/dfa/pos/41.kt
@@ -19,7 +19,7 @@ fun case_1(x: Any) {
     if (x is Interface1) {
         if (x is Interface2) {
             <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & Interface2 & kotlin.Any")!>x<!>
-            <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any"), DEBUG_INFO_SMARTCAST!>x<!>.itest()
+            <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any"), DEBUG_INFO_SMARTCAST!>x<!>.itest()
             <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any"), DEBUG_INFO_SMARTCAST!>x<!>.itest1()
             <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any"), DEBUG_INFO_SMARTCAST!>x<!>.itest2()
         }

compiler/tests-spec/testData/diagnostics/notLinked/dfa/pos/6.kt
@@ -1297,7 +1297,7 @@ fun case_71(t: Any?) {
                 <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & Interface2 & kotlin.Any & kotlin.Any?")!>t<!>
                 <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest1()
                 <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest2()
-                <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
+                <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
 
                 <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.let { <!DEBUG_INFO_EXPRESSION_TYPE("{Any & Interface1 & Interface2}")!>it<!>.itest1(); <!DEBUG_INFO_EXPRESSION_TYPE("{Any & Interface1 & Interface2}")!>it<!>.itest2() }
             }
@@ -1318,7 +1318,7 @@ fun case_72(t: Any?, z1: Nothing?) {
         <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & Interface2 & kotlin.Any & kotlin.Any?")!>t<!>
         <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest1()
         <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest2()
-        <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
+        <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
 
         <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.let { <!DEBUG_INFO_EXPRESSION_TYPE("{Any & Interface1 & Interface2}")!>it<!>.itest1(); <!DEBUG_INFO_EXPRESSION_TYPE("{Any & Interface1 & Interface2}")!>it<!>.itest2() }
     }
@@ -1340,7 +1340,7 @@ fun case_73(t: Any?) {
                     if (t != <!DEBUG_INFO_CONSTANT!>`null`<!>) {
                         <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest2()
                         <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest1()
-                        <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
+                        <!DEBUG_INFO_EXPRESSION_TYPE("Interface3 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
                         <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.test1()
                         <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.test2()
                         <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel1 & ClassLevel2 & Interface1 & Interface2 & Interface3 & kotlin.Any & kotlin.Any?")!>t<!>
@@ -1365,7 +1365,7 @@ fun case_74(t: Any?) {
                     if (t is Interface3?) {
                         <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest2()
                         <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest1()
-                        <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
+                        <!DEBUG_INFO_EXPRESSION_TYPE("Interface3 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
                         <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.test1()
                         <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.test2()
                         <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel1 & ClassLevel2 & Interface1 & Interface2 & Interface3 & kotlin.Any & kotlin.Any?")!>t<!>
@@ -1388,7 +1388,7 @@ fun case_75(t: Any?, z: Nothing?) {
             if (t !is Interface2? || t !is Interface3?) {} else {
                 <!DEBUG_INFO_EXPRESSION_TYPE("Interface2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest2()
                 <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest1()
-                <!DEBUG_INFO_EXPRESSION_TYPE("Interface1 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
+                <!DEBUG_INFO_EXPRESSION_TYPE("Interface3 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.itest()
                 <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.test1()
                 <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>.test2()
                 <!DEBUG_INFO_EXPRESSION_TYPE("ClassLevel2 & kotlin.Any?"), DEBUG_INFO_SMARTCAST!>t<!>

compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -10131,6 +10131,11 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                     runTest("compiler/testData/diagnostics/tests/inference/commonSystem/lessSpecificTypeForArgumentCallWithExactAnnotation_ni.kt");
                 }
 
+                @TestMetadata("manyArgumentsForVararg.kt")
+                public void testManyArgumentsForVararg() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/commonSystem/manyArgumentsForVararg.kt");
+                }
+
                 @TestMetadata("nestedLambdas.kt")
                 public void testNestedLambdas() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/commonSystem/nestedLambdas.kt");

compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -10126,6 +10126,11 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                     runTest("compiler/testData/diagnostics/tests/inference/commonSystem/lessSpecificTypeForArgumentCallWithExactAnnotation_ni.kt");
                 }
 
+                @TestMetadata("manyArgumentsForVararg.kt")
+                public void testManyArgumentsForVararg() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/commonSystem/manyArgumentsForVararg.kt");
+                }
+
                 @TestMetadata("nestedLambdas.kt")
                 public void testNestedLambdas() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/commonSystem/nestedLambdas.kt");