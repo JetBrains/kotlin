d5d3e9a58b46db4e0119697122bac13cf1516ee0
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-10-01 21:26:08 +0700

    DescriptorRenderer: shouldn't render `private` modifier by default #KT-31587 Fixed
    (cherry picked from commit 9da6dcf840e98c442578afa86d364ceae704881b)

--- idea/testData/multiModuleQuickFix/createActual/sealed/header/header.kt
+++ idea/testData/multiModuleQuickFix/createActual/sealed/header/header.kt
@@ -1,6 +1,6 @@
 // "Create actual class for module testModule_JS (JS)" "true"
 
-expect sealed class <caret>Sealed {
+expect sealed class <caret>Sealed() {
     object Obj : Sealed
 
     class Klass(x: Int) : Sealed
