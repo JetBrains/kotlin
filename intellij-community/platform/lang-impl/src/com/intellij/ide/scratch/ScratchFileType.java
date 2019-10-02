d9a9c88872e10dedca93883269cb7a1027616b2c
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2019-10-02 11:01:10 +0300

    remove useless LanguageFileType#getCharset overriders
    intellij-monorepo: a697e68ef6ffe725a07eab111821a4aff225c98a

--- intellij-community/platform/lang-impl/src/com/intellij/ide/scratch/ScratchFileType.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/scratch/ScratchFileType.java
@@ -70,10 +70,4 @@ public class ScratchFileType extends LanguageFileType implements FileTypeIdentif
   public boolean isReadOnly() {
     return true;
   }
-
-  @Nullable
-  @Override
-  public String getCharset(@NotNull VirtualFile file, @NotNull byte[] content) {
-    return null;
-  }
 }
