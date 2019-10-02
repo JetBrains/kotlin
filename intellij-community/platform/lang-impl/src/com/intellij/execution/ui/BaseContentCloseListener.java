55cfdbb74b7826c82edde180ff5291ed903024f6
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2019-10-02 13:28:39 +0300

    terminate processes forcibly if the process cannot be terminated gracefully (to ensure IDE won't block on exit)
    intellij-monorepo: 8c4b435bf2a9acc33d48c22c17d8f3b44c6ad84f

--- intellij-community/platform/lang-impl/src/com/intellij/execution/ui/BaseContentCloseListener.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/ui/BaseContentCloseListener.java
@@ -4,6 +4,7 @@ package com.intellij.execution.ui;
 import com.intellij.execution.ExecutionBundle;
 import com.intellij.execution.TerminateRemoteProcessDialog;
 import com.intellij.execution.process.ProcessHandler;
+import com.intellij.execution.process.ScriptRunnerUtil;
 import com.intellij.ide.GeneralSettings;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -111,7 +112,7 @@ public abstract class BaseContentCloseListener extends ContentManagerAdapter imp
     }
     boolean destroyProcess = rc == GeneralSettings.ProcessCloseConfirmation.TERMINATE;
     if (destroyProcess) {
-      processHandler.destroyProcess();
+      ScriptRunnerUtil.terminateProcessHandler(processHandler, 2000, null);
     }
     else {
       processHandler.detachProcess();
