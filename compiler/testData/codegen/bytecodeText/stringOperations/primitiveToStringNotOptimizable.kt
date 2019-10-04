7f50f263b167f4018dd14c9284d54ede508dc5df
Steven Schäfer <stevenschaefer@google.com>
2019-10-04 11:36:17 +0200

    Minor: Make String.valueOf bytecode tests more precise

--- compiler/testData/codegen/bytecodeText/stringOperations/primitiveToStringNotOptimizable.kt
+++ compiler/testData/codegen/bytecodeText/stringOperations/primitiveToStringNotOptimizable.kt
@@ -10,5 +10,10 @@ fun main(a: Boolean, b:Byte, c: Short, d: Int, e: Long, f: Float, g: Double, h: 
 }
 
 /*Check that all "valueOf" are String ones and there is no boxing*/
+// 1 INVOKESTATIC java/lang/String.valueOf \(Z\)
+// 3 INVOKESTATIC java/lang/String.valueOf \(I\)
+// 1 INVOKESTATIC java/lang/String.valueOf \(C\)
+// 1 INVOKESTATIC java/lang/String.valueOf \(J\)
+// 1 INVOKESTATIC java/lang/String.valueOf \(F\)
+// 1 INVOKESTATIC java/lang/String.valueOf \(D\)
 // 8 valueOf
-// 8 INVOKESTATIC java/lang/String.valueOf
