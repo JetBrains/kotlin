89259e2ec1085cb25b1bac95093b0c28a7139322
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-10-08 23:43:53 +0900

    Unused import: do not report for 'invoke' function import that is used from same file
    #KT-24281 Fixed

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -9425,6 +9425,16 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
         public void testImportAlias() throws Exception {
             runTest("idea/testData/quickfix/optimizeImports/importAlias.kt");
         }
+
+        @TestMetadata("invoke.kt")
+        public void testInvoke() throws Exception {
+            runTest("idea/testData/quickfix/optimizeImports/invoke.kt");
+        }
+
+        @TestMetadata("invoke2.kt")
+        public void testInvoke2() throws Exception {
+            runTest("idea/testData/quickfix/optimizeImports/invoke2.kt");
+        }
     }
 
     @TestMetadata("idea/testData/quickfix/override")
