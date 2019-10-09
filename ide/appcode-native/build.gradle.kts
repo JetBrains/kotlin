be3f53fe9995dc908eff3c74d1efea171ccc5457
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2019-10-09 15:41:24 +0200

    Use backend.native.jar from TeamCity

--- ide/appcode-native/build.gradle.kts
+++ ide/appcode-native/build.gradle.kts
@@ -1,8 +1,11 @@
+import com.github.jk1.tcdeps.KotlinScriptDslAdapter.tc
+import com.github.jk1.tcdeps.KotlinScriptDslAdapter.teamcityServer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
 
 plugins {
     kotlin("jvm")
+    id("com.github.jk1.tcdeps") version "1.2"
 }
 
 val ultimateTools: Map<String, Any> by rootProject.extensions
@@ -12,10 +15,17 @@ val disableBuildTasks: Project.(String) -> Unit by ultimateTools
 val appcodeVersion: String by rootProject.extra
 val appcodeUnscrambledJarDir: File by rootProject.extra
 
+repositories {
+    teamcityServer {
+        setUrl("https://buildserver.labs.intellij.net")
+    }
+}
+
 dependencies {
     compile(project(":kotlin-ultimate:ide:common-cidr-native"))
     compileOnly(fileTree(appcodeUnscrambledJarDir) { include("**/*.jar") })
     compileOnly(intellijDep()) { includeJars("trove4j", "external-system-rt", "objenesis-3.0.1") }
+    compile(tc("Kotlin_KotlinNative_Master_KotlinNativeLinuxBundle:1.3.50-dev-12975:backend.native.jar"))
 }
 
 the<JavaPluginConvention>().sourceSets["main"].apply {
