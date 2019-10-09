d8ad0ec43a9303d816d05b8d6bc6e656a1f6e02c
Leonid Startsev <leonid.startsev@jetbrains.com>
2019-10-09 16:34:35 +0300

    Do not add synthetic deserialization constructor if .deserialize method is customized
    Otherwise, there was an added descriptor without generated IR on Native

--- plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/extensions/SerializationResolveExtension.kt
+++ plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/extensions/SerializationResolveExtension.kt
@@ -29,6 +29,7 @@ import org.jetbrains.kotlin.resolve.extensions.SyntheticResolveExtension
 import org.jetbrains.kotlin.resolve.lazy.LazyClassContext
 import org.jetbrains.kotlin.resolve.lazy.declarations.ClassMemberDeclarationProvider
 import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlinx.serialization.compiler.backend.common.SerializerCodegen
 import org.jetbrains.kotlinx.serialization.compiler.resolve.*
 import org.jetbrains.kotlinx.serialization.compiler.resolve.SerializationAnnotations.serialInfoFqName
 import java.util.*
@@ -78,6 +79,8 @@ open class SerializationResolveExtension : SyntheticResolveExtension {
         result: MutableCollection<ClassConstructorDescriptor>
     ) {
         if (thisDescriptor.isInternalSerializable) {
+            // do not add synthetic deserialization constructor if .deserialize method is customized
+            if (thisDescriptor.hasCompanionObjectAsSerializer && SerializerCodegen.getSyntheticLoadMember(thisDescriptor.companionObjectDescriptor!!) == null) return
             result.add(KSerializerDescriptorResolver.createLoadConstructorDescriptor(thisDescriptor, bindingContext))
         }
     }
