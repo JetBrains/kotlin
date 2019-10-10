b4d2eae12d7292486b197f01eb5c8458ee93a69b
Mark Punzalan <markpun@google.com>
2019-10-10 02:22:00 -0700

    Add test data generator for stepped progression box tests.

--- compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BlackBoxCodegenTestGenerated.java
@@ -20775,6 +20775,11 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
                         runTest("compiler/testData/codegen/box/ranges/stepped/expression/until/mixedTypeStep.kt");
                     }
 
+                    @TestMetadata("progressionToNonConst.kt")
+                    public void testProgressionToNonConst() throws Exception {
+                        runTest("compiler/testData/codegen/box/ranges/stepped/expression/until/progressionToNonConst.kt");
+                    }
+
                     @TestMetadata("singleElementStepTwo.kt")
                     public void testSingleElementStepTwo() throws Exception {
                         runTest("compiler/testData/codegen/box/ranges/stepped/expression/until/singleElementStepTwo.kt");
