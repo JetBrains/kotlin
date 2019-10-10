87ac31d95743e5de29c50b1b7366be39e9825c3c
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-10-10 18:09:51 +0700

    UsageReplacementStrategy: shouldn't catch `ControlFlowException`

--- idea/src/org/jetbrains/kotlin/idea/codeInliner/UsageReplacementStrategy.kt
+++ idea/src/org/jetbrains/kotlin/idea/codeInliner/UsageReplacementStrategy.kt
@@ -1,22 +1,12 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.codeInliner
 
 import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.diagnostic.ControlFlowException
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
@@ -139,6 +129,7 @@ private fun UsageReplacementStrategy.processUsages(
 
             createReplacer(usage)?.invoke()
         } catch (e: Throwable) {
+            if (e is ControlFlowException) throw e
             LOG.error(e)
         }
     }
