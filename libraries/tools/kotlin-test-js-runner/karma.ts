55fb9be7f610b3c557ba3139dd89a6e81b7121cd
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2019-10-10 17:40:02 +0300

    [Gradle, JS] Remove copypaste for karma and nodejs test adapters

--- libraries/tools/kotlin-test-js-runner/karma.ts
+++ libraries/tools/kotlin-test-js-runner/karma.ts
@@ -4,9 +4,7 @@
  */
 
 import {CliArgsParser, getDefaultCliDescription} from "./src/CliArgsParser";
-import {runWithFilteringAdapter} from "./src/Adapter";
-import {TeamCityMessagesFlow} from "./src/TeamCityMessagesFlow";
-import {runWithTeamCityConsoleAdapter} from "./src/KotlinTestTeamCityReporter";
+import {runWithFilteringAndConsoleAdapters} from "./src/Adapter";
 
 const kotlin_test = require('kotlin-test');
 
@@ -14,19 +12,15 @@ process.exit = (exitCode) => {
     throw new Error(`Exit with ${exitCode}`)
 };
 
-const processArgs = window.__karma__.config.args;
 const cliDescription = getDefaultCliDescription();
 cliDescription.freeArgsTitle = null;
 const parser = new CliArgsParser(cliDescription);
+
+const processArgs = window.__karma__.config.args;
 const untypedArgs = parser.parse(processArgs);
 
 const initialAdapter = kotlin_test.kotlin.test.detectAdapter_8be2vx$();
-
-const realConsoleLog = console.log;
-const teamCity = new TeamCityMessagesFlow(null, (payload) => realConsoleLog(payload));
-const teamCityAdapter = runWithTeamCityConsoleAdapter(initialAdapter, teamCity);
-
-kotlin_test.setAdapter(runWithFilteringAdapter(teamCityAdapter, untypedArgs));
+kotlin_test.setAdapter(runWithFilteringAndConsoleAdapters(initialAdapter, untypedArgs));
 
 const resultFun = window.__karma__.result;
 window.__karma__.result = function (result) {
