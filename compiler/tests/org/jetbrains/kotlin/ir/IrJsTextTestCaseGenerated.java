aecd08e562edb1f30c6da5363bf45220be0b2797
Roman Artemev <roman.artemev@jetbrains.com>
2019-10-10 18:10:49 +0300

    [Psi2Ir] Fix IrText test generation

--- compiler/tests/org/jetbrains/kotlin/ir/IrJsTextTestCaseGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/ir/IrJsTextTestCaseGenerated.java
@@ -26,7 +26,7 @@ public class IrJsTextTestCaseGenerated extends AbstractIrJsTextTestCase {
     }
 
     public void testAllFilesPresentInIrJsText() throws Exception {
-        KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText"), Pattern.compile("^(.+)\\.kt$"), TargetBackend.ANY, true);
+        KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText"), Pattern.compile("^(.+)\\.kt(s)?$"), TargetBackend.ANY, true);
     }
 
     @TestMetadata("compiler/testData/ir/irJsText/dynamic")
@@ -38,7 +38,7 @@ public class IrJsTextTestCaseGenerated extends AbstractIrJsTextTestCase {
         }
 
         public void testAllFilesPresentInDynamic() throws Exception {
-            KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText/dynamic"), Pattern.compile("^(.+)\\.kt$"), TargetBackend.ANY, true);
+            KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText/dynamic"), Pattern.compile("^(.+)\\.kt(s)?$"), TargetBackend.ANY, true);
         }
 
         @TestMetadata("dynamicAndMembersOfAny.kt")
@@ -161,7 +161,7 @@ public class IrJsTextTestCaseGenerated extends AbstractIrJsTextTestCase {
         }
 
         public void testAllFilesPresentInNative() throws Exception {
-            KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText/native"), Pattern.compile("^(.+)\\.kt$"), TargetBackend.ANY, true);
+            KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText/native"), Pattern.compile("^(.+)\\.kt(s)?$"), TargetBackend.ANY, true);
         }
 
         @TestMetadata("nativeNativeKotlin.kt")
@@ -179,7 +179,22 @@ public class IrJsTextTestCaseGenerated extends AbstractIrJsTextTestCase {
         }
 
         public void testAllFilesPresentInScripting() throws Exception {
-            KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText/scripting"), Pattern.compile("^(.+)\\.kt$"), TargetBackend.ANY, true);
+            KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("compiler/testData/ir/irJsText/scripting"), Pattern.compile("^(.+)\\.kt(s)?$"), TargetBackend.ANY, true);
+        }
+
+        @TestMetadata("arrayAssignment.kts")
+        public void testArrayAssignment() throws Exception {
+            runTest("compiler/testData/ir/irJsText/scripting/arrayAssignment.kts");
+        }
+
+        @TestMetadata("fun.kts")
+        public void testFun() throws Exception {
+            runTest("compiler/testData/ir/irJsText/scripting/fun.kts");
+        }
+
+        @TestMetadata("safeCalls.kts")
+        public void testSafeCalls() throws Exception {
+            runTest("compiler/testData/ir/irJsText/scripting/safeCalls.kts");
         }
     }
 }
