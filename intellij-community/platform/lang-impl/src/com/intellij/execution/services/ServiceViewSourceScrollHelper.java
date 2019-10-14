76c9c0366a041ef901fb10fddcc0125cb816d258
dmitriy.tseyler <dmitriy.tseyler@jetbrains.com>
2019-10-15 01:01:42 +0300

    [dbe] enable services autoscroll from source by default in DataGrip
    intellij-monorepo: 5c6680e3a686ed3d2880d7aa00cbfe7c2ce6f8fb

--- intellij-community/platform/lang-impl/src/com/intellij/execution/services/ServiceViewSourceScrollHelper.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/services/ServiceViewSourceScrollHelper.java
@@ -17,6 +17,7 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ex.ToolWindowEx;
 import com.intellij.ui.AutoScrollFromSourceHandler;
 import com.intellij.ui.AutoScrollToSourceHandler;
+import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.concurrency.Promise;
 import org.jetbrains.concurrency.Promises;
@@ -50,7 +51,7 @@ class ServiceViewSourceScrollHelper {
   }
 
   private static boolean isAutoScrollFromSourceEnabled(@NotNull Project project) {
-    return PropertiesComponent.getInstance(project).getBoolean(AUTO_SCROLL_FROM_SOURCE_PROPERTY);
+    return PropertiesComponent.getInstance(project).getBoolean(AUTO_SCROLL_FROM_SOURCE_PROPERTY, PlatformUtils.isDataGrip());
   }
 
   private static class ServiceViewAutoScrollToSourceHandler extends AutoScrollToSourceHandler {
