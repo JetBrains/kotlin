b235eace140380661ceba17e815d4ec97a920d65
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-10-15 18:15:55 +0200

    Cleanup (deprecating an unfortunate API class)
    intellij-monorepo: d4555fcd5d73083b1dfaaaaf6fa7e838a37d6262

--- intellij-community/platform/lang-impl/src/com/intellij/formatting/blocks/FormatterTreeUtil.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/formatting/blocks/FormatterTreeUtil.kt
@@ -1,23 +1,13 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.formatting.blocks
 
 import com.intellij.lang.ASTNode
 import com.intellij.psi.TokenType
+import org.jetbrains.annotations.ApiStatus
 
+@Deprecated("Bad API (non-descriptive name, cryptic behaviour, rarely needed). Implement your own using `TreeUtil#skipElementsBack`.")
+@ApiStatus.ScheduledForRemoval(inVersion = "2021.1")
+@Suppress("DEPRECATION")
 fun ASTNode.prev(): ASTNode? {
   var prev = treePrev
   while (prev != null && prev.elementType == TokenType.WHITE_SPACE) {
