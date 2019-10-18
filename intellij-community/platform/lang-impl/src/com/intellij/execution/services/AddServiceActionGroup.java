0468a66d7a317896500cb739af4cfddab26c3e02
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2019-10-18 16:31:49 +0300

    [service-view] fix Add Service action group enablement
    intellij-monorepo: 599f99d26e143c48a570f04250dec855fc013256

--- intellij-community/platform/lang-impl/src/com/intellij/execution/services/AddServiceActionGroup.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/services/AddServiceActionGroup.java
@@ -1,11 +1,20 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.services;
 
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import org.jetbrains.annotations.NotNull;
 
+import static com.intellij.execution.services.ServiceViewActionProvider.getSelectedView;
+
 public class AddServiceActionGroup extends DefaultActionGroup {
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    ServiceView selectedView = getSelectedView(e);
+    e.getPresentation().setEnabled(selectedView != null);
+  }
+
   @Override
   public boolean canBePerformed(@NotNull DataContext context) {
     return false;
