f63c3aeac9399925101263e78c3cd881e9013d13
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-10-23 19:27:06 +0300

    Extract FirMemberScopeProvider to a separate file

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirClassDeclaredMemberScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirClassDeclaredMemberScope.kt
@@ -5,68 +5,18 @@
 
 package org.jetbrains.kotlin.fir.scopes.impl
 
-import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.fir.declarations.FirCallableMemberDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirConstructor
 import org.jetbrains.kotlin.fir.declarations.FirRegularClass
-import org.jetbrains.kotlin.fir.resolve.memberScopeProvider
 import org.jetbrains.kotlin.fir.scopes.FirScope
 import org.jetbrains.kotlin.fir.scopes.ProcessorAction
 import org.jetbrains.kotlin.fir.scopes.ProcessorAction.NEXT
 import org.jetbrains.kotlin.fir.scopes.ProcessorAction.STOP
-import org.jetbrains.kotlin.fir.symbols.impl.*
-import org.jetbrains.kotlin.name.ClassId
-import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirClassifierSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirVariableSymbol
 import org.jetbrains.kotlin.name.Name
-
-class FirMemberScopeProvider : FirSessionComponent {
-
-    private val declaredMemberCache = mutableMapOf<FirRegularClass, FirClassDeclaredMemberScope>()
-    private val nestedClassifierCache = mutableMapOf<FirRegularClass, FirNestedClassifierScope>()
-    private val selfImportingCache = mutableMapOf<FqName, FirSelfImportingScope>()
-
-    fun declaredMemberScope(klass: FirRegularClass): FirClassDeclaredMemberScope {
-        return declaredMemberCache.getOrPut(klass) {
-            FirClassDeclaredMemberScope(klass)
-        }
-    }
-
-    fun nestedClassifierScope(klass: FirRegularClass): FirNestedClassifierScope {
-        return nestedClassifierCache.getOrPut(klass) {
-            FirNestedClassifierScope(klass)
-        }
-    }
-
-    // TODO: it's better to cache this scope in ScopeSession
-    fun selfImportingScope(fqName: FqName, session: FirSession): FirSelfImportingScope {
-        return selfImportingCache.getOrPut(fqName) {
-            FirSelfImportingScope(fqName, session)
-        }
-    }
-}
-
-fun declaredMemberScope(klass: FirRegularClass): FirClassDeclaredMemberScope {
-    return klass
-        .session
-        .memberScopeProvider
-        .declaredMemberScope(klass)
-}
-
-fun nestedClassifierScope(klass: FirRegularClass): FirNestedClassifierScope {
-    return klass
-        .session
-        .memberScopeProvider
-        .nestedClassifierScope(klass)
-}
-
-fun nestedClassifierScope(classId: ClassId, session: FirSession): FirLazyNestedClassifierScope {
-    return FirLazyNestedClassifierScope(classId, session)
-}
-
-fun selfImportingScope(fqName: FqName, session: FirSession): FirSelfImportingScope {
-    return session.memberScopeProvider.selfImportingScope(fqName, session)
-}
 
 class FirClassDeclaredMemberScope(klass: FirRegularClass) : FirScope() {
     private val nestedClassifierScope = nestedClassifierScope(klass)
