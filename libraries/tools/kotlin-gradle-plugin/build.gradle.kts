3643ae476509c1082c27bb4d6d0cff6339cd4242
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-10-24 11:40:28 +0300

    Update 193 to more recent 193.4778.7-EAP

--- libraries/tools/kotlin-gradle-plugin/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin/build.gradle.kts
@@ -65,31 +65,15 @@ dependencies {
     runtime(project(":kotlin-reflect"))
 
     jarContents(compileOnly(intellijDep()) {
-        includeJars("asm-all", "gson", rootProject = rootProject)
+        includeJars("asm-all", "gson", "serviceMessages", rootProject = rootProject)
     })
-
-    jarContents(compileOnly(intellijDep()) {
-        if (Platform.P193.orHigher()) {
-            includeJars("teamcity-service-messages", rootProject = rootProject)
-        } else {
-            includeJars("serviceMessages", rootProject = rootProject)
-        }
-    })
-
 
     // com.android.tools.build:gradle has ~50 unneeded transitive dependencies
     compileOnly("com.android.tools.build:gradle:3.0.0") { isTransitive = false }
     compileOnly("com.android.tools.build:gradle-core:3.0.0") { isTransitive = false }
     compileOnly("com.android.tools.build:builder-model:3.0.0") { isTransitive = false }
 
-    testCompile(intellijDep()) { includeJars( "junit", rootProject = rootProject) }
-    testCompile(intellijDep()) {
-        if (Platform.P193.orHigher()) {
-            includeJars("teamcity-service-messages", rootProject = rootProject)
-        } else {
-            includeJars("serviceMessages", rootProject = rootProject)
-        }
-    }
+    testCompile(intellijDep()) { includeJars( "junit", "serviceMessages", rootProject = rootProject) }
 
     testCompileOnly(project(":compiler"))
     testCompile(projectTests(":kotlin-build-common"))
