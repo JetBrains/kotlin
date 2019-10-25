865e1bf63144a95db2ea7ca864ff52196e629a8b
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2019-10-25 19:05:40 +0700

    KotlinInternalInJavaInspection: cleanup code

--- idea/src/org/jetbrains/kotlin/idea/inspections/KotlinInternalInJavaInspection.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/KotlinInternalInJavaInspection.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.inspections
@@ -41,11 +30,11 @@ class KotlinInternalInJavaInspection : LocalInspectionTool() {
     private fun PsiElement.checkAndReport(holder: ProblemsHolder) {
         val lightElement = (this as? PsiReference)?.resolve() as? KtLightElement<*, *> ?: return
         val modifierListOwner = lightElement.kotlinOrigin as? KtModifierListOwner ?: return
-        if(inSameModule(modifierListOwner)) {
+        if (inSameModule(modifierListOwner)) {
             return
         }
 
-        if(modifierListOwner.hasModifier(INTERNAL_KEYWORD)) {
+        if (modifierListOwner.hasModifier(INTERNAL_KEYWORD)) {
             holder.registerProblem(this, "Usage of Kotlin internal declaration from different module")
         }
     }
