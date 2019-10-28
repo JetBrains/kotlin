4bea99d18e7951f967e0b5947fb7711d99727898
Alexander Zolotov <zolotov@jetbrains.com>
2019-10-28 19:40:01 +0300

    Runtime environments: add 'Manage Targets' to the Run menu (IDEA-224928)
    intellij-monorepo: e5e29d9f4008fa7c5fa91b02358bbe21a355023f

---
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/target/ManageTargetEnvironmentsAction.kt
@@ -0,0 +1,19 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.execution.target
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.Experiments
+import com.intellij.openapi.options.ShowSettingsUtil
+import com.intellij.openapi.project.DumbAwareAction
+
+class ManageTargetEnvironmentsAction : DumbAwareAction("Manage Targets...") {
+  override fun actionPerformed(e: AnActionEvent) {
+    ShowSettingsUtil.getInstance().editConfigurable(e.project, TargetEnvironmentsConfigurable(e.project!!, null))
+  }
+
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    e.presentation.isEnabledAndVisible = Experiments.getInstance().isFeatureEnabled("runtime.environments") &&
+                                         RemoteTargetType.EXTENSION_NAME.extensionList.isNotEmpty()
+  }
+}
