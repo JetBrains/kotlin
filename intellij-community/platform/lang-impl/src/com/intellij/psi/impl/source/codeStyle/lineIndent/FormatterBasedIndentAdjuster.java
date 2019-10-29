4e301fc9e6fc43fee7fa453c7f09dd3bdd3ab81f
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2019-10-29 13:03:45 +0300

    Added BlockSupport.isTooDeep() heuristic rule for sync vs async handling
    intellij-monorepo: 4929a217082a4a9fe23e016d96ce70bf8ea6a711

--- intellij-community/platform/lang-impl/src/com/intellij/psi/impl/source/codeStyle/lineIndent/FormatterBasedIndentAdjuster.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/impl/source/codeStyle/lineIndent/FormatterBasedIndentAdjuster.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.text.BlockSupport;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.text.CharArrayUtil;
 import org.jetbrains.annotations.ApiStatus;
@@ -26,49 +27,57 @@ public class FormatterBasedIndentAdjuster  {
   private FormatterBasedIndentAdjuster() {
   }
 
-  public static void scheduleIndentAdjustment(@NotNull Project myProject,
-                                              @NotNull Document myDocument,
-                                              int myOffset) {
-    IndentAdjusterRunnable fixer = new IndentAdjusterRunnable(myProject, myDocument, myOffset);
-    PsiDocumentManager documentManager = PsiDocumentManager.getInstance(myProject);
-    if (isSynchronousAdjustment(myDocument)) {
-      documentManager.commitDocument(myDocument);
+  public static void scheduleIndentAdjustment(@NotNull Project project,
+                                              @NotNull Document document,
+                                              int offset) {
+    PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(document);
+    if (file != null) {
+      IndentAdjusterRunnable fixer = new IndentAdjusterRunnable(project, document, file, offset);
+      PsiDocumentManager documentManager = PsiDocumentManager.getInstance(project);
+      if (isSynchronousAdjustment(document, file)) {
+        documentManager.commitDocument(document);
+      }
+      fixer.run();
     }
-    fixer.run();
   }
 
-  private static boolean isSynchronousAdjustment(@NotNull Document document) {
-    return ApplicationManager.getApplication().isUnitTestMode() || document.getTextLength() <= MAX_SYNCHRONOUS_ADJUSTMENT_DOC_SIZE;
+  private static boolean isSynchronousAdjustment(@NotNull Document document, @NotNull PsiFile file) {
+    return ApplicationManager.getApplication().isUnitTestMode() ||
+           document.getTextLength() <= MAX_SYNCHRONOUS_ADJUSTMENT_DOC_SIZE && !BlockSupport.isTooDeep(file);
   }
 
   public static class IndentAdjusterRunnable implements Runnable {
     private final Project myProject;
     private final int myLine;
     private final Document myDocument;
+    private final PsiFile myFile;
 
-    public IndentAdjusterRunnable(Project project, Document document, int offset) {
+    public IndentAdjusterRunnable(@NotNull Project project,
+                                  @NotNull Document document,
+                                  @NotNull PsiFile file,
+                                  int offset) {
       myProject = project;
       myDocument = document;
       myLine = myDocument.getLineNumber(offset);
+      myFile = file;
     }
 
     @Override
     public void run() {
       int lineStart = myDocument.getLineStartOffset(myLine);
       int indentEnd = CharArrayUtil.shiftForward(myDocument.getCharsSequence(), lineStart, " \t");
-      PsiFile file = PsiDocumentManager.getInstance(myProject).getPsiFile(myDocument);
-      if (file != null) {
-        RangeMarker indentMarker = myDocument.createRangeMarker(lineStart, indentEnd);
-        CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(myProject);
-        if (isSynchronousAdjustment(myDocument)) {
-          updateIndent(indentMarker, codeStyleManager.getLineIndent(file, lineStart, FormattingMode.ADJUST_INDENT_ON_ENTER));
-        }
-        else {
-          ReadAction.nonBlocking(() -> codeStyleManager.getLineIndent(file, lineStart, FormattingMode.ADJUST_INDENT_ON_ENTER))
-            .withDocumentsCommitted(myProject)
-            .finishOnUiThread(ModalityState.NON_MODAL, indentString -> updateIndent(indentMarker, indentString))
-            .submit(AppExecutorUtil.getAppExecutorService());
-        }
+      RangeMarker indentMarker = myDocument.createRangeMarker(lineStart, indentEnd);
+      CodeStyleManager codeStyleManager = CodeStyleManager.getInstance(myProject);
+      if (isSynchronousAdjustment(myDocument, myFile)) {
+        updateIndent(indentMarker,
+                     codeStyleManager.getLineIndent(myFile, lineStart, FormattingMode.ADJUST_INDENT_ON_ENTER));
+      }
+      else {
+        ReadAction
+          .nonBlocking(() -> codeStyleManager.getLineIndent(myFile, lineStart, FormattingMode.ADJUST_INDENT_ON_ENTER))
+          .withDocumentsCommitted(myProject)
+          .finishOnUiThread(ModalityState.NON_MODAL, indentString -> updateIndent(indentMarker, indentString))
+          .submit(AppExecutorUtil.getAppExecutorService());
       }
     }
 
