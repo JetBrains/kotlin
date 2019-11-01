01f09af608e776724a5f051f6f181c99ba9eda64
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2019-11-01 12:54:16 +0300

    [FIR] Fix generated name for `FirErrorNameReferenceImpl`

--- compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/ImplementationConfigurator.kt
+++ compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/ImplementationConfigurator.kt
@@ -522,7 +522,7 @@ object ImplementationConfigurator : AbstractFirTreeImplementationConfigurator() 
         }
 
         impl(errorNamedReference) {
-            default("name", "Name.special(\"<\$diagnostic.reason>\")")
+            default("name", "Name.special(\"<\${diagnostic.reason}>\")")
             defaultNull("candidateSymbol", withGetter = true)
         }
 
