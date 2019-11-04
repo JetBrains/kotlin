1e987b31582536a66cb1afff2ae7eb17376829cd
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-11-04 18:28:52 +0100

    cleanup
    intellij-monorepo: 8e732ddf99929e6ac7508998dc2097559b2b1509

--- intellij-community/platform/lang-impl/src/com/intellij/application/options/editor/AutoImportOptionsConfigurable.java
+++ intellij-community/platform/lang-impl/src/com/intellij/application/options/editor/AutoImportOptionsConfigurable.java
@@ -19,10 +19,9 @@ package com.intellij.application.options.editor;
 import com.intellij.openapi.application.ApplicationBundle;
 import com.intellij.openapi.options.CompositeConfigurable;
 import com.intellij.openapi.options.Configurable.VariableProjectAppLevel;
-import com.intellij.openapi.options.ConfigurableEP;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.NullableFunction;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -48,8 +47,7 @@ public class AutoImportOptionsConfigurable
   @NotNull
   @Override
   protected List<AutoImportOptionsProvider> createConfigurables() {
-    return ContainerUtil.mapNotNull(AutoImportOptionsProviderEP.EP_NAME.getExtensions(myProject),
-                                    (NullableFunction<ConfigurableEP<AutoImportOptionsProvider>, AutoImportOptionsProvider>)ep -> ep.createConfigurable());
+    return ContainerUtil.mapNotNull(AutoImportOptionsProviderEP.EP_NAME.getExtensions(myProject), ep -> ep.createConfigurable());
   }
 
   @Override
@@ -69,15 +67,15 @@ public class AutoImportOptionsConfigurable
     for (int i = 0; i < getConfigurables().size(); i++) {
       AutoImportOptionsProvider provider = getConfigurables().get(i);
       myProvidersPanel.add(provider.createComponent(), new GridBagConstraints(0, i, 1, 1, 0, 0,
-                                                                     GridBagConstraints.NORTH,
-                                                                     GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0), 0,0));
+                                                                              GridBagConstraints.NORTH,
+                                                                              GridBagConstraints.HORIZONTAL, JBUI.emptyInsets(), 0, 0));
     }
     myProvidersPanel.add(Box.createVerticalGlue(), new GridBagConstraints(0, getConfigurables().size(), 1, 1, 0, 1,
-                                                                     GridBagConstraints.NORTH,
-                                                                     GridBagConstraints.BOTH, new Insets(0,0,0,0), 0,0));
+                                                                          GridBagConstraints.NORTH,
+                                                                          GridBagConstraints.BOTH, JBUI.emptyInsets(), 0, 0));
     myProvidersPanel.add(Box.createVerticalGlue(), new GridBagConstraints(1, 0, getConfigurables().size() + 1, 1, 1, 0,
-                                                                     GridBagConstraints.NORTH,
-                                                                     GridBagConstraints.BOTH, new Insets(0,0,0,0), 0,0));
+                                                                          GridBagConstraints.NORTH,
+                                                                          GridBagConstraints.BOTH, JBUI.emptyInsets(), 0, 0));
     return myPanel;
   }
 
