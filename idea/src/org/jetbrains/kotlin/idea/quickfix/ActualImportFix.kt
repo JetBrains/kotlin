722f7ff05664541eb3fa4dffe3bca7b59abb8001
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2019-11-05 11:57:08 +0300

    Now using Kotlin setting instead of Java one

--- idea/src/org/jetbrains/kotlin/idea/quickfix/ActualImportFix.kt
+++ idea/src/org/jetbrains/kotlin/idea/quickfix/ActualImportFix.kt
@@ -5,17 +5,18 @@
 
 package org.jetbrains.kotlin.idea.quickfix
 
-import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.openapi.editor.Editor
 import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.idea.codeInsight.KotlinCodeInsightWorkspaceSettings
 import org.jetbrains.kotlin.psi.KtSimpleNameExpression
 
 internal class ImportFix(expression: KtSimpleNameExpression): AbstractImportFix(expression, MyFactory) {
     override fun fixSilently(editor: Editor): Boolean {
-        if (!CodeInsightSettings.getInstance().ADD_UNAMBIGIOUS_IMPORTS_ON_THE_FLY) return false
         if (isOutdated()) return false
         val element = element ?: return false
-        val addImportAction = createAction(element.project, editor, element)
+        val project = element.project
+        if (!KotlinCodeInsightWorkspaceSettings.getInstance(project).addUnambiguousImportsOnTheFly) return false
+        val addImportAction = createAction(project, editor, element)
         if (addImportAction.isUnambiguous()) {
             addImportAction.execute()
             return true
