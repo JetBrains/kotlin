ae1cabe1a4db74998ba013b8438e8f20d6c45785
Yann Cébron <yann.cebron@jetbrains.com>
2019-11-05 15:57:34 +0100

    DefaultLiveTemplateEP: javadoc
    intellij-monorepo: d0fd184344ab72686e0c70f29b421f400983a085

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/DefaultLiveTemplateEP.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/DefaultLiveTemplateEP.kt
@@ -7,13 +7,23 @@ import com.intellij.openapi.extensions.RequiredElement
 import com.intellij.util.xmlb.annotations.Attribute
 
 /**
+ * Provides bundled live templates.
+ * See [Live Templates Tutorial](http://www.jetbrains.org/intellij/sdk/docs/tutorials/live_templates.html).
+ *
  * @author yole
  */
 class DefaultLiveTemplateEP : AbstractExtensionPointBean() {
+
+  /**
+   * Path to resource, without `.xml` extension (e.g. `/templates/foo`).
+   */
   @Attribute("file")
   @RequiredElement
   var file: String? = null
 
+  /**
+   * `true` if not user-visible/editable.
+   */
   @Attribute("hidden")
   var hidden: Boolean = false
 
