3dd342143770b0277b623359ab533df7469ff3c8
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-11-05 15:39:34 +0300

    Temporary: resolve type to kotlin/reflect/Function instead of kotlin/Function
    This arises in callable reference resolve test due to incorrect fictitious symbols caching

--- compiler/fir/resolve/testData/resolve/stdlib/callableReferences/fromBasicDiagnosticTests/moreSpecificAmbiguousExtensions.kt
+++ compiler/fir/resolve/testData/resolve/stdlib/callableReferences/fromBasicDiagnosticTests/moreSpecificAmbiguousExtensions.kt
@@ -10,6 +10,7 @@ fun test() {
 }
 
 fun testWithExpectedType() {
+    // NB: should be resolved to kotlin/FunctionX, not kotlin/reflect/FunctionX
     val extFun_AB_A: IA.(IB) -> Unit = IA::extFun
     val extFun_AA_B: IA.(IA) -> Unit = IB::extFun
     val extFun_BB_A: IB.(IB) -> Unit = IA::extFun
