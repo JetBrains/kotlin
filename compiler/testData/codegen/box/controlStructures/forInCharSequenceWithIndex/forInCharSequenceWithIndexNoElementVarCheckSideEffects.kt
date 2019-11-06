8c571f888161e58eea87878fc621d596e7c70086
Mark Punzalan <markpun@google.com>
2019-11-05 23:42:29 -0800

    JVM: Always invoke get()/charAt() in optimized for-loop over CharSequence.withIndex(), even if element variable is unused in destructuring declaration.
    #KT-34779 Fixed

---
+++ compiler/testData/codegen/box/controlStructures/forInCharSequenceWithIndex/forInCharSequenceWithIndexNoElementVarCheckSideEffects.kt
@@ -0,0 +1,30 @@
+// KJS_WITH_FULL_RUNTIME
+// WITH_RUNTIME
+
+class CountingString(private val s: String) : CharSequence {
+    var lengthCtr = 0
+    var getCtr = 0
+
+    override val length: Int
+        get() = s.length.also { lengthCtr++ }
+
+    override fun subSequence(startIndex: Int, endIndex: Int) = TODO()
+    override fun get(index: Int) = s.get(index).also { getCtr++ }
+}
+
+val cs = CountingString("abcd")
+
+fun box(): String {
+    val s = StringBuilder()
+
+    for ((index, _) in cs.withIndex()) {
+        s.append("$index;")
+    }
+
+    val ss = s.toString()
+    if (ss != "0;1;2;3;") return "fail: '$ss'"
+    if (cs.lengthCtr != 5) return "lengthCtr != 5, was: '${cs.lengthCtr}'"
+    if (cs.getCtr != 4) return "getCtr != 4, was: '${cs.getCtr}'"
+
+    return "OK"
+}
---
+++ compiler/testData/codegen/box/controlStructures/forInIterableWithIndex/forInIterableWithIndexNoElementVarCheckSideEffects.kt
@@ -0,0 +1,31 @@
+// KJS_WITH_FULL_RUNTIME
+// WITH_RUNTIME
+
+class CountingIterable<out T>(private val s: Iterable<T>) : Iterable<T> {
+    var hasNextCtr = 0
+    var nextCtr = 0
+
+    inner class CountingIterableIterator(private val it: Iterator<T>) : Iterator<T> {
+        override fun hasNext() = it.hasNext().also { hasNextCtr++ }
+        override fun next() = it.next().also { nextCtr++ }
+    }
+
+    override fun iterator() = CountingIterableIterator(s.iterator())
+}
+
+val xs = CountingIterable(listOf("a", "b", "c", "d"))
+
+fun box(): String {
+    val s = StringBuilder()
+
+    for ((index, _) in xs.withIndex()) {
+        s.append("$index;")
+    }
+
+    val ss = s.toString()
+    if (ss != "0;1;2;3;") return "fail: '$ss'"
+    if (xs.hasNextCtr != 5) return "hasNextCtr != 5, was: '${xs.hasNextCtr}'"
+    if (xs.nextCtr != 4) return "nextCtr != 4, was: '${xs.nextCtr}'"
+
+    return "OK"
+}
