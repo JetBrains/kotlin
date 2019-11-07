569262dcc215e9ba4af5037c55384f28be1a4c97
Sergey Sinchuk <sergey.sinchuk@jetbrains.com>
2019-11-07 13:25:50 +0300

    Enable brace highlighting for read-only viewers
    close #1218
    
    intellij-monorepo: c7a650d2db3773e6c96e8fe790f0474d7945a390

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/highlighting/BraceHighlightingHandler.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/highlighting/BraceHighlightingHandler.java
@@ -111,7 +111,7 @@ public class BraceHighlightingHandler {
 
   private static boolean isValidEditor(@NotNull Editor editor) {
     Project editorProject = editor.getProject();
-    return editorProject != null && !editorProject.isDisposed() && !editor.isDisposed() && editor.getComponent().isShowing() && !editor.isViewer();
+    return editorProject != null && !editorProject.isDisposed() && !editor.isDisposed() && editor.getComponent().isShowing();
   }
 
   @NotNull
