61c337588bcbe3f2f2b43a26f963b5f9431564b8
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2019-11-07 10:41:15 +0300

    Revert testdata accidentally committed in 665405c4

--- compiler/testData/diagnostics/testsWithStdLib/coroutines/inference/variableCallInsideBuilderFunction.kt
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/inference/variableCallInsideBuilderFunction.kt
@@ -2,6 +2,17 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 // !USE_EXPERIMENTAL: kotlin.Experimental
 
-fun test(x: List<Int>) {
-    x + x
+import kotlin.experimental.ExperimentalTypeInference
+
+interface ProducerScope<E> {
+    fun yield(e: E)
 }
+
+@UseExperimental(ExperimentalTypeInference::class)
+fun <E> produce(@BuilderInference block: ProducerScope<E>.() -> Unit): ProducerScope<E> = TODO()
+
+fun <K> filter(e: K, predicate: (K) -> Boolean) =
+    produce {
+        predicate(e)
+        yield(42)
+    }
