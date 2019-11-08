ecb08303a254a0542d7c36d5de8dff5ddb74eead
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-11-08 18:54:33 +0100

    Don't optimize null checks for unsafe primitive type boxes as well
     #KT-34829 Fixed
    
    (cherry picked from commit 228fdb4436bbf7b0d772bfa4acdcdb7dcb1bdccb)

---
+++ compiler/testData/codegen/box/platformTypes/unsafeNullCheckWithPrimitive.kt
@@ -0,0 +1,21 @@
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND: JVM_IR
+// FILE: Unsound.java
+
+import test.Wrap;
+
+public class Unsound {
+    public static <T> Wrap<T> get() {
+        return new Wrap<T>(null);
+    }
+}
+
+// FILE: 1.kt
+
+package test
+
+class Wrap<T>(val x: T)
+
+// JVM IR generates bytecode that fails with NPE because it unwraps the value with `Number.intValue()`,
+// whereas JVM generates a simple null check without unwrapping the box.
+fun box(): String = if ((Unsound.get<Int>() as Wrap<Int>).x == null) "OK" else "Fail"
