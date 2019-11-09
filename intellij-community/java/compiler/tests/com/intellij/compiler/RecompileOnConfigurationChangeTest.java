3c9943258d2e8f3a28a5f5aec2374d53d1776a0e
Eugene Zhuravlev <jeka@intellij.com>
2019-11-09 12:16:05 +0100

    correctly clean old output for a target, if output root changed and at the same time explicit target rebuild executed
    intellij-monorepo: a1d995ff654001285ab293e8fe5c1529b98178f6

--- intellij-community/java/compiler/tests/com/intellij/compiler/RecompileOnConfigurationChangeTest.java
+++ intellij-community/java/compiler/tests/com/intellij/compiler/RecompileOnConfigurationChangeTest.java
@@ -30,4 +30,19 @@ public class RecompileOnConfigurationChangeTest extends BaseCompilerTestCase {
     File[] files = oldOutput.listFiles();
     assertTrue(files == null || files.length == 0);
   }
+
+  public void testChangeOutputWithRebuild() throws IOException {
+    VirtualFile srcRoot = createFile("src/A.java", "class A{}").getParent();
+    Module m = addModule("m", srcRoot);
+    make(m);
+    assertOutput(m, fs().file("A.class"));
+
+    File oldOutput = getOutputDir(m);
+    File newOutput = createTempDir("new-output");
+    PsiTestUtil.setCompilerOutputPath(m, VfsUtilCore.pathToUrl(FileUtil.toSystemIndependentName(newOutput.getAbsolutePath())), false);
+    rebuild();
+    assertOutput(m, fs().file("A.class"));
+    File[] files = oldOutput.listFiles();
+    assertTrue(files == null || files.length == 0);
+  }
 }
