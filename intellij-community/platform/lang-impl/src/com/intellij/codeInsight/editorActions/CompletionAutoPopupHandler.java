1c29608fce0e57c2cc02fb16a23d0220be9833e6
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-11-12 16:57:34 +0100

    clean up: make CompletionAutoPopupHandler.ourTestingAutopopup final
    intellij-monorepo: 44a509ec5fbe88a53fd9062da8009ff11d1aa089

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/editorActions/CompletionAutoPopupHandler.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/editorActions/CompletionAutoPopupHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.editorActions;
 
 import com.intellij.codeInsight.AutoPopupController;
@@ -20,7 +20,7 @@ import org.jetbrains.annotations.NotNull;
  */
 public class CompletionAutoPopupHandler extends TypedHandlerDelegate {
   private static final Logger LOG = Logger.getInstance(CompletionAutoPopupHandler.class);
-  public static volatile Key<Boolean> ourTestingAutopopup = Key.create("TestingAutopopup");
+  public static final Key<Boolean> ourTestingAutopopup = Key.create("TestingAutopopup");
 
   @NotNull
   @Override
