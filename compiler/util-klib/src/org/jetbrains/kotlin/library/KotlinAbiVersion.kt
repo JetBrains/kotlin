86570a2f910d4c868ff367b2720a2f4decd1575a
Roman Artemev <roman.artemev@jetbrains.com>
2019-11-12 21:00:32 +0300

    [KLIB] Promote ABI version

--- compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinAbiVersion.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinAbiVersion.kt
@@ -22,7 +22,7 @@ fun String.parseKonanAbiVersion(): KotlinAbiVersion {
 
 data class KotlinAbiVersion(val version: Int) {
     companion object {
-        val CURRENT = KotlinAbiVersion(19)
+        val CURRENT = KotlinAbiVersion(20)
     }
     override fun toString() = "$version"
 }
