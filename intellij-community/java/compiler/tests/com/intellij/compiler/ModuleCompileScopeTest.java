3e22034737c116510bf3de1f99aa1916a82f6f5e
Eugene Zhuravlev <jeka@intellij.com>
2019-11-12 08:47:48 +0100

    fix: Recompile single file in production target compiles classes from the corresponding test target (IDEA-226671)
    intellij-monorepo: 540fe68054f2377fa780995b0d5fb0176a39f367

--- intellij-community/java/compiler/tests/com/intellij/compiler/ModuleCompileScopeTest.java
+++ intellij-community/java/compiler/tests/com/intellij/compiler/ModuleCompileScopeTest.java
@@ -70,7 +70,8 @@ public class ModuleCompileScopeTest extends BaseCompilerTestCase {
 
   public void testForceCompileUpToDateFileAndDoNotCompileDependentTestClass() {
     VirtualFile a = createFile("src/A.java", "class A{ public static void foo(int param) {} }");
-    VirtualFile b = createFile("testSrc/B.java", "class B { void bar() {A.foo(10);}}");
+    final String textB = "class B { void bar() {A.foo(10);}}";
+    VirtualFile b = createFile("testSrc/B.java", textB);
     Module module = addModule("a", a.getParent(), b.getParent());
     make(module);
     assertOutput(module, fs().file("A.class"), false);
@@ -87,10 +88,11 @@ public class ModuleCompileScopeTest extends BaseCompilerTestCase {
     deleteFile(testClassFile);
     make(module);
     assertOutput(module, fs());
+    changeFile(b, textB); // touch b
+    
     compile(true, a);
     assertOutput(module, fs().file("A.class"), false);
-    assertOutput(module, fs(), true);
-    assertModulesUpToDate();
+    assertOutput(module, fs(), true);  // make sure B is not compiled, even if it is modified
   }
 
   public void testMakeTwoModules() {
