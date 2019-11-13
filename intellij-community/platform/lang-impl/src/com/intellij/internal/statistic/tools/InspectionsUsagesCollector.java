f3365f0a0c49806a4a844dece8277d3573d6ccd9
Kirill Smelov <kirill.smelov@jetbrains.com>
2019-11-13 11:34:17 +0100

    FUS-568 Log changes inspection settings
    intellij-monorepo: 58fec42a5c48e8ecabad10ff827294d515b549cd

--- intellij-community/platform/lang-impl/src/com/intellij/internal/statistic/tools/InspectionsUsagesCollector.java
+++ intellij-community/platform/lang-impl/src/com/intellij/internal/statistic/tools/InspectionsUsagesCollector.java
@@ -14,22 +14,27 @@ import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesColle
 import com.intellij.internal.statistic.utils.PluginInfo;
 import com.intellij.internal.statistic.utils.PluginInfoDetectorKt;
 import com.intellij.lang.Language;
-import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.profile.codeInspection.InspectionProjectProfileManager;
+import org.jdom.Attribute;
+import org.jdom.Content;
+import org.jdom.DataConversionException;
+import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class InspectionsUsagesCollector extends ProjectUsagesCollector {
   private static final Predicate<ScopeToolState> ENABLED = state -> !state.getTool().isEnabledByDefault() && state.isEnabled();
 
   private static final Predicate<ScopeToolState> DISABLED = state -> state.getTool().isEnabledByDefault() && !state.isEnabled();
+  private static final String SETTING_VALUE = "setting_value";
+  private static final String SETTING_TYPE = "setting_type";
+  private static final String SETTING_INDEX = "setting_index";
+  private static final String INSPECTION_ID = "inspection_id";
 
   @NotNull
   @Override
@@ -39,7 +44,7 @@ public class InspectionsUsagesCollector extends ProjectUsagesCollector {
 
   @Override
   public int getVersion() {
-    return 2;
+    return 3;
   }
 
   @NotNull
@@ -48,33 +53,105 @@ public class InspectionsUsagesCollector extends ProjectUsagesCollector {
     final Set<MetricEvent> result = new HashSet<>();
     final List<ScopeToolState> tools = InspectionProjectProfileManager.getInstance(project).getCurrentProfile().getAllTools();
     for (ScopeToolState state : tools) {
+      InspectionToolWrapper<?, ?> tool = state.getTool();
+      PluginInfo pluginInfo = getInfo(tool);
       if (ENABLED.test(state)) {
-        result.add(create(state, true));
+        result.add(create(tool, pluginInfo, true));
       }
       else if (DISABLED.test(state)) {
-        result.add(create(state, false));
+        result.add(create(tool, pluginInfo, false));
+      }
+
+      result.addAll(getChangedSettingsEvents(tool, pluginInfo, state.isEnabled()));
+    }
+    return result;
+  }
+
+  private static Collection<MetricEvent> getChangedSettingsEvents(InspectionToolWrapper<?, ?> tool,
+                                                                  PluginInfo pluginInfo,
+                                                                  boolean enabled) {
+    if (!isSafeToReport(pluginInfo)) return Collections.emptyList();
+    Collection<MetricEvent> result = new ArrayList<>();
+    String inspectionId = tool.getID();
+    List<Content> options = getOptions(tool);
+    for (int i = 0; i < options.size(); i++) {
+      Content option = options.get(i);
+      if (option instanceof Element) {
+        Attribute settingValue = ((Element)option).getAttribute("value");
+        if (settingValue == null) continue;
+        FeatureUsageData data = new FeatureUsageData();
+        // setting the index instead of name is here because name can contain sensitive data
+        data.addData(SETTING_INDEX, i);
+        data.addData(INSPECTION_ID, inspectionId);
+        data.addData("enabled", enabled);
+        data.addPluginInfo(pluginInfo);
+        if (addSettingValue(settingValue, data)) {
+          result.add(MetricEventFactoryKt.newMetric("setting.non.default.state", data));
+        }
       }
     }
     return result;
   }
 
+  private static boolean addSettingValue(Attribute settingValueAttribute, FeatureUsageData data) {
+    try {
+      boolean booleanValue = settingValueAttribute.getBooleanValue();
+      data.addData(SETTING_VALUE, booleanValue);
+      data.addData(SETTING_TYPE, "boolean");
+      return true;
+    }
+    catch (DataConversionException e) {
+      return addIntValue(settingValueAttribute, data);
+    }
+  }
+
+  private static boolean addIntValue(Attribute value, FeatureUsageData data) {
+    try {
+      int intValue = value.getIntValue();
+      data.addData(SETTING_VALUE, intValue);
+      data.addData(SETTING_TYPE, "integer");
+      return true;
+    }
+    catch (DataConversionException e) {
+      return false;
+    }
+
+  }
+
   @NotNull
-  private static MetricEvent create(@NotNull ScopeToolState state, boolean enabled) {
-    InspectionToolWrapper<?, ?> tool = state.getTool();
+  private static MetricEvent create(InspectionToolWrapper<?, ?> tool, PluginInfo info, boolean enabled) {
     final FeatureUsageData data = new FeatureUsageData().addData("enabled", enabled);
     final String language = tool.getLanguage();
     if (StringUtil.isNotEmpty(language)) {
       data.addLanguage(Language.findLanguageByID(language));
     }
-
-    InspectionEP extension = tool.getExtension();
-    PluginDescriptor pluginDescriptor = extension == null ? null : extension.getPluginDescriptor();
-    PluginInfo info = pluginDescriptor != null ? PluginInfoDetectorKt.getPluginInfoByDescriptor(pluginDescriptor) : null;
     if (info != null) {
       data.addPluginInfo(info);
     }
-    data.addData("inspection_id", info != null && info.isSafeToReport() ? state.getTool().getID() : "third.party");
+    data.addData(INSPECTION_ID, isSafeToReport(info) ? tool.getID() : "third.party");
     return MetricEventFactoryKt.newMetric("not.default.state", data);
+  }
+
+  private static boolean isSafeToReport(PluginInfo info) {
+    return info != null && info.isSafeToReport();
+  }
+
+  @Nullable
+  private static PluginInfo getInfo(InspectionToolWrapper<?, ?> tool) {
+    final InspectionEP extension = tool.getExtension();
+    return extension != null ? PluginInfoDetectorKt.getPluginInfoById(extension.getPluginDescriptor().getPluginId()) : null;
+  }
+
+  @NotNull
+  private static List<Content> getOptions(InspectionToolWrapper<?, ?> tool) {
+    Element options = new Element("options");
+    try {
+      ScopeToolState.tryWriteSettings(tool.getTool(), options);
+      return options.getContent();
+    }
+    catch (Exception e) {
+      return Collections.emptyList();
+    }
   }
 
   public static class InspectionToolValidator extends CustomWhiteListRule {
