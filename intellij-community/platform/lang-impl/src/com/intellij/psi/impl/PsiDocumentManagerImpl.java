d0ddbe2ef58a88611d3ad454d7642905f83f7378
peter <peter@jetbrains.com>
2019-11-14 16:34:37 +0100

    simplify DocumentCommitThread by using ReadAction.nonBlocking (also helps with GO-8230)
    intellij-monorepo: d25c3181daaef3fd45a0663dd985aa220ada81e1

--- intellij-community/platform/lang-impl/src/com/intellij/psi/impl/PsiDocumentManagerImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/impl/PsiDocumentManagerImpl.java
@@ -19,7 +19,6 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectLocator;
 import com.intellij.openapi.project.impl.ProjectImpl;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Segment;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
@@ -58,7 +57,6 @@ public final class PsiDocumentManagerImpl extends PsiDocumentManagerBase {
         fireDocumentCreated(document, psiFile);
       }
     });
-    Disposer.register(this, () -> ((DocumentCommitThread)myDocumentCommitProcessor).cancelTasksOnProjectDispose(project));
   }
 
   @Nullable
