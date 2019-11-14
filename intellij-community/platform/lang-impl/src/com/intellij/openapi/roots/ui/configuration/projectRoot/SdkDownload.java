1772f4431639c21b79fc0e49c598d6afed9ec6e0
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2019-11-14 18:56:29 +0100

    jdk - show JDK download progress inside ProjectStructure dialog, rework components to support it, work in progress
    intellij-monorepo: 773443d2d77919f790b85707c54e66cf64dc270f

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/projectRoot/SdkDownload.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/projectRoot/SdkDownload.java
@@ -1,11 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.roots.ui.configuration.projectRoot;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkModel;
 import com.intellij.openapi.projectRoots.SdkType;
 import com.intellij.openapi.projectRoots.SdkTypeId;
+import com.intellij.openapi.wm.ex.ProgressIndicatorEx;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -45,4 +47,42 @@ public interface SdkDownload {
                       @NotNull JComponent parentComponent,
                       @Nullable Sdk selectedSdk,
                       @NotNull Consumer<Sdk> sdkCreatedCallback);
+
+  interface DownloadProgressListener {
+    @NotNull
+    ProgressIndicatorEx getProgressIndicator();
+
+    /**
+     * Notifies the UI from the EDT thread that the background download is completed
+     * and the related SDK instance is update to reflect the changes.
+     */
+    void onDownloadCompleted();
+
+    /**
+     * Notifies the UI from the EDT thread that the background download has failed
+     * with the following error message.
+     * //TODO[jo] how should UI handle that failure?
+     * //TODO[jo] should we drop such an SDK from the model?
+     */
+    void onDownloadFailed(@NotNull String message);
+  }
+
+  /**
+   * Allows to connect the UI (e.g. Project Structure dialog) with the pending SDK download progress.
+   * The listener is automatically unsubscribed after either {@link DownloadProgressListener#onDownloadCompleted()}
+   * or {@link DownloadProgressListener#onDownloadFailed(String)} callbacks are executed.
+   * Multiple attempts to register the same instance of {@link DownloadProgressListener} are merged together
+   * without duplication of callbacks
+   *
+   * @param sdk      the SDK instance to test
+   * @param lifetime disposable object for that subscription
+   * @param listener the callback interface for the UI
+   *
+   * @return {@code true} if the given {@param sdk} instance was created by that extension
+   *         and there are some planned activities for that {@link Sdk} to make it fully ready;
+   *         {@code false} otherwise
+   */
+  boolean addChangesListener(@NotNull Sdk sdk,
+                             @NotNull Disposable lifetime,
+                             @NotNull DownloadProgressListener listener);
 }
