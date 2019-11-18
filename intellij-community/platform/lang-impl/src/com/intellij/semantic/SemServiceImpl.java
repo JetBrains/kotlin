d6172db23411948fb3673c873e6994b66c0ae72c
peter <peter@jetbrains.com>
2019-11-18 07:21:23 +0100

    make semContributor and metaDataContributor EPs dynamic (part of IDEA-225622)
    intellij-monorepo: ba9b14a177cfa69291dd3af812dee2b466285f5b

--- intellij-community/platform/lang-impl/src/com/intellij/semantic/SemServiceImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/semantic/SemServiceImpl.java
@@ -3,6 +3,8 @@ package com.intellij.semantic;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionNotApplicableException;
+import com.intellij.openapi.extensions.ExtensionPointListener;
+import com.intellij.openapi.extensions.PluginDescriptor;
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.RecursionGuard;
@@ -38,6 +40,17 @@ public final class SemServiceImpl extends SemService {
   public SemServiceImpl(Project project) {
     myProject = project;
     myCVManager = CachedValuesManager.getManager(myProject);
+    SemContributor.EP_NAME.addExtensionPointListener(new ExtensionPointListener<SemContributorEP>() {
+      @Override
+      public void extensionAdded(@NotNull SemContributorEP extension, @NotNull PluginDescriptor pluginDescriptor) {
+        myProducers = null;
+      }
+
+      @Override
+      public void extensionRemoved(@NotNull SemContributorEP extension, @NotNull PluginDescriptor pluginDescriptor) {
+        myProducers = null;
+      }
+    }, project);
   }
 
   private MultiMap<SemKey<?>, NullableFunction<PsiElement, Collection<? extends SemElement>>> collectProducers() {
