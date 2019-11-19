97678ea6217f0bbe1ff0488686c62bdffe5a16c0
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-11-19 16:09:51 +0100

    [tests] postponing test data creation to avoid NCDFE in tests
    intellij-monorepo: e2541369d0fb196d746cc6722b71cf292e6261fe

--- intellij-community/platform/lang-impl/testSources/com/intellij/openapi/wm/impl/HiddenSidebarButtonTest.java
+++ intellij-community/platform/lang-impl/testSources/com/intellij/openapi/wm/impl/HiddenSidebarButtonTest.java
@@ -12,21 +12,26 @@ import java.util.Arrays;
  * @author Vassiliy Kudryashov
  */
 public class HiddenSidebarButtonTest extends ToolWindowManagerTestCase {
-  private static final String LAYOUT = "<layout>" +
-                                 "<window_info id=\"TODO\" active=\"false\" anchor=\"bottom\" auto_hide=\"false\" internal_type=\"DOCKED\" type=\"DOCKED\" visible=\"false\" show_stripe_button=\"false\" weight=\"0.42947903\" sideWeight=\"0.4874552\" order=\"6\" side_tool=\"false\" content_ui=\"tabs\" x=\"119\" y=\"106\" width=\"619\" height=\"748\"/>" +
-                                 "<window_info id=\"Find\" active=\"false\" anchor=\"bottom\" auto_hide=\"false\" internal_type=\"DOCKED\" type=\"DOCKED\" visible=\"false\" show_stripe_button=\"true\" weight=\"0.47013977\" sideWeight=\"0.5\" order=\"1\" side_tool=\"false\" content_ui=\"tabs\" x=\"443\" y=\"301\" width=\"702\" height=\"388\"/>" +
-                                 "<window_info id=\"Project\" active=\"false\" anchor=\"left\" auto_hide=\"false\" internal_type=\"DOCKED\" type=\"DOCKED\" visible=\"false\" show_stripe_button=\"true\" weight=\"0.37235227\" sideWeight=\"0.6060991\" order=\"0\" side_tool=\"false\" content_ui=\"tabs\" x=\"116\" y=\"80\" width=\"487\" height=\"787\"/>" +
-                                 "</layout>";
-
-  private static final String[] IDS = {ToolWindowId.TODO_VIEW, ToolWindowId.FIND, ToolWindowId.PROJECT_VIEW};
-  private static final boolean[] ESTIMATED_TO_SHOW = {false, true, true};
-  private static final boolean[] ESTIMATED_VISIBILITY = {false, false, true};
-
   public void testHiddenButton() throws Exception {
+    String[] toolWindows = {ToolWindowId.TODO_VIEW, ToolWindowId.FIND, ToolWindowId.PROJECT_VIEW};
+    boolean[] expectedStripes = {false, true, true};
+    boolean[] expectedVisibility = {false, false, true};
+
     DesktopLayout layout = myManager.getLayout();
-    layout.readExternal(JDOMUtil.load(LAYOUT));
-    for (String ID : IDS) {
-      assertFalse(layout.isToolWindowRegistered(ID));
+    layout.readExternal(JDOMUtil.load(
+      "<layout>" +
+      "<window_info id=\"TODO\" active=\"false\" anchor=\"bottom\" auto_hide=\"false\" internal_type=\"DOCKED\" type=\"DOCKED\" visible=\"false\"" +
+      " show_stripe_button=\"false\" weight=\"0.42947903\" sideWeight=\"0.4874552\" order=\"6\" side_tool=\"false\" content_ui=\"tabs\" x=\"119\"" +
+      " y=\"106\" width=\"619\" height=\"748\"/>" +
+      "<window_info id=\"Find\" active=\"false\" anchor=\"bottom\" auto_hide=\"false\" internal_type=\"DOCKED\" type=\"DOCKED\" visible=\"false\"" +
+      " show_stripe_button=\"true\" weight=\"0.47013977\" sideWeight=\"0.5\" order=\"1\" side_tool=\"false\" content_ui=\"tabs\" x=\"443\" y=\"301\"" +
+      " width=\"702\" height=\"388\"/>" +
+      "<window_info id=\"Project\" active=\"false\" anchor=\"left\" auto_hide=\"false\" internal_type=\"DOCKED\" type=\"DOCKED\" visible=\"false\"" +
+      " show_stripe_button=\"true\" weight=\"0.37235227\" sideWeight=\"0.6060991\" order=\"0\" side_tool=\"false\" content_ui=\"tabs\" x=\"116\"" +
+      " y=\"80\" width=\"487\" height=\"787\"/>" +
+      "</layout>"));
+    for (String id : toolWindows) {
+      assertFalse(layout.isToolWindowRegistered(id));
     }
 
     for (ToolWindowEP extension : ToolWindowEP.EP_NAME.getExtensionList()) {
@@ -36,10 +41,10 @@ public class HiddenSidebarButtonTest extends ToolWindowManagerTestCase {
     }
     new UsageViewContentManagerImpl(myManager.getProject(), myManager);
 
-    for (int i = 0; i < IDS.length; i++) {
-      assertTrue(layout.isToolWindowRegistered(IDS[i]));
-      assertEquals(ESTIMATED_TO_SHOW[i], layout.getInfo(IDS[i], true).isShowStripeButton());
-      assertEquals(ESTIMATED_VISIBILITY[i], myManager.getStripeButton(IDS[i]).isVisible());
+    for (int i = 0; i < toolWindows.length; i++) {
+      assertTrue(layout.isToolWindowRegistered(toolWindows[i]));
+      assertEquals(expectedStripes[i], layout.getInfo(toolWindows[i], true).isShowStripeButton());
+      assertEquals(expectedVisibility[i], myManager.getStripeButton(toolWindows[i]).isVisible());
     }
   }
 }
