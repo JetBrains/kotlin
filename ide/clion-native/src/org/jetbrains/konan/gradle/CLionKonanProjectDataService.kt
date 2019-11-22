42d33b8f2290657b059a0e5eecacbde9ead72911
Egor Zhdan <egor.zhdan@jetbrains.com>
2019-11-22 13:07:08 +0300

    Mobile plugin: move CLion-specific component to CLion plugin module, part 2

--- ide/clion-native/src/org/jetbrains/konan/gradle/CLionKonanProjectDataService.kt
+++ ide/clion-native/src/org/jetbrains/konan/gradle/CLionKonanProjectDataService.kt
@@ -12,7 +12,6 @@ import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.Key
 import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.service.project.IdeModelsProvider
-import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
@@ -30,15 +29,6 @@ import org.jetbrains.plugins.gradle.util.GradleConstants
 class CLionKonanProjectDataService : AbstractProjectDataService<KonanModel, Module>() {
 
     override fun getTargetDataKey(): Key<KonanModel> = KONAN_MODEL_KEY
-
-    override fun postProcess(
-        toImport: Collection<DataNode<KonanModel>>,
-        projectData: ProjectData?,
-        project: Project,
-        modelsProvider: IdeModifiableModelsProvider
-    ) {
-        GradleKonanWorkspace.getInstance(project).update()
-    }
 
     override fun onSuccessImport(
         imported: Collection<DataNode<KonanModel>>,
---
+++ ide/common-cidr-native/src/org/jetbrains/konan/gradle/KonanProjectDataService.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.konan.gradle
+
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.Key
+import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
+import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+
+class KonanProjectDataService : AbstractProjectDataService<KonanModel, Module>() {
+    override fun getTargetDataKey(): Key<KonanModel> = KonanProjectResolver.KONAN_MODEL_KEY
+
+    override fun postProcess(
+        toImport: Collection<DataNode<KonanModel>>,
+        projectData: ProjectData?,
+        project: Project,
+        modelsProvider: IdeModifiableModelsProvider
+    ) {
+        GradleKonanWorkspace.getInstance(project).update()
+    }
+}
