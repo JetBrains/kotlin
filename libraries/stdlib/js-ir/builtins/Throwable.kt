adfb296e4563fa6243e8904ba5a717723e8ad234
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2019-11-28 18:51:41 +0300

    [stdlib-js-ir] Add equals, hashCode, toString to some builtins
    Namely:
    * Throwable;
    * primitive numbers except Long (which implemented in Kotlin).

--- libraries/stdlib/js-ir/builtins/Throwable.kt
+++ libraries/stdlib/js-ir/builtins/Throwable.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -13,11 +13,16 @@ package kotlin
  */
 @JsName("Error")
 public external open class Throwable {
-    open val message: String?
-    open val cause: Throwable?
+    public open val message: String?
+    public open val cause: Throwable?
 
-    constructor(message: String?, cause: Throwable?)
-    constructor(message: String?)
-    constructor(cause: Throwable?)
-    constructor()
+    public constructor(message: String?, cause: Throwable?)
+    public constructor(message: String?)
+    public constructor(cause: Throwable?)
+    public constructor()
+
+    // TODO: add specialized version to runtime
+//    public override fun equals(other: Any?): Boolean
+//    public override fun hashCode(): Int
+    public override fun toString(): String
 }
