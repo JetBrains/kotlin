003c0069bed5a20c469ec533d25534462a70a5c5
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-11-28 23:44:43 +0900

    Convert member to extension: do not suggest for delegated properties
    #KT-35128 Fixed

--- idea/src/org/jetbrains/kotlin/idea/intentions/declarations/ConvertMemberToExtensionIntention.kt
+++ idea/src/org/jetbrains/kotlin/idea/intentions/declarations/ConvertMemberToExtensionIntention.kt
@@ -59,7 +59,7 @@ class ConvertMemberToExtensionIntention :
         if (element.receiverTypeReference != null) return false
         if (element.hasModifier(KtTokens.OVERRIDE_KEYWORD)) return false
         when (element) {
-            is KtProperty -> if (element.hasInitializer()) return false
+            is KtProperty -> if (element.hasInitializer() || element.hasDelegate()) return false
             is KtSecondaryConstructor -> return false
         }
         return true
