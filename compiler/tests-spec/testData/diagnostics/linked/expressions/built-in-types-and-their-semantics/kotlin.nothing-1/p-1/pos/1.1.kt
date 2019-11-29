ed9d837b5eb6d5b036e02386c79ecb059a5c80cd
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2019-11-29 13:34:03 +0300

    kotlin.Nothing diag tests for Returns and Jumps (fix)

--- compiler/tests-spec/testData/diagnostics/linked/expressions/built-in-types-and-their-semantics/kotlin.nothing-1/p-1/pos/1.1.kt
+++ compiler/tests-spec/testData/diagnostics/linked/expressions/built-in-types-and-their-semantics/kotlin.nothing-1/p-1/pos/1.1.kt
@@ -18,11 +18,18 @@
 fun case1() {
     var name: Any? = null
     val men = arrayListOf(Person("Phill"), Person(), Person("Bob"))
-    loop@ for (i in men) {
-        i.name
-        <!UNREACHABLE_CODE!>val valeua : Int =<!>    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>break@loop<!>
-        <!UNREACHABLE_CODE!>i.name<!>
+    for (k in men) {
+        k.name
+        loop@ for (i in men) {
+            i.name
+            <!UNREACHABLE_CODE!>val valeua : Int =<!>      <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>break@loop<!>
+            <!UNREACHABLE_CODE!>i.name<!>
+        }
+        k.name
+        val s = k.name ?: <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>break<!>
+        k.name
     }
+    val a = 1
 }
 
 class Person(var name: String? = null) {}
@@ -32,19 +39,31 @@ class Person(var name: String? = null) {}
 fun case2() {
     var name: Any? = null
     val men = arrayListOf(Person("Phill"), Person(), Person("Bob"))
-    loop@ for (i in men) {
-        i.name
-        <!UNREACHABLE_CODE!>val valeua : Int =<!>    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>continue@loop<!>
-        <!UNREACHABLE_CODE!>i.name<!>
+    for (k in men) {
+        loop@ for (i in men) {
+            i.name
+            <!UNREACHABLE_CODE!>val valeua : Int =<!>     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>continue@loop<!>
+            <!UNREACHABLE_CODE!>i.name<!>
+        }
+        val s = k.name ?: <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>break<!>
+        k.name
     }
+    val a = 1
 }
 
 // TESTCASE NUMBER: 3
 
 fun case3() {
-    listOf(1, 2, 3, 4, 5).forEach lit@{
-        it
-        <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>return@lit<!>
-        <!UNREACHABLE_CODE!>print(it)<!>
+    listOf(1, 2, 3, 4, 5).forEach { x ->
+        val k = x
+
+        listOf(1, 2, 3, 4, 5).forEach lit@{
+            it
+            <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>return@lit<!>
+            <!UNREACHABLE_CODE!>print(it)<!>
+        }
+        val y = x
+        if (x == 3) <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing")!>return<!>
     }
+    val a = 1
 }
