5abb6bc7a54ed7b3536687f4448a073bc6a4b58d
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-12-04 18:04:34 +0900

    Add quickfix for SENSELESS_NULL_IN_WHEN to remove redundant when branch
    #KT-24556 Fixed

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -15517,6 +15517,11 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
             runTest("idea/testData/quickfix/when/noElseInWhenWithoutBranches.kt");
         }
 
+        @TestMetadata("removeRedundantBranch.kt")
+        public void testRemoveRedundantBranch() throws Exception {
+            runTest("idea/testData/quickfix/when/removeRedundantBranch.kt");
+        }
+
         @TestMetadata("removeRedundantElse.kt")
         public void testRemoveRedundantElse() throws Exception {
             runTest("idea/testData/quickfix/when/removeRedundantElse.kt");
