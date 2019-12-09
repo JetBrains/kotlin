f031b47363e306d2a999f83c79260141ad51dca2
Mark Punzalan <markpun@google.com>
2019-12-09 11:25:19 -0800

    [FIR] Fix issue with multiple smartcasts of `this`.

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/Utils.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/Utils.kt
@@ -8,9 +8,26 @@ package org.jetbrains.kotlin.fir
 import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.fir.expressions.FirBlock
 import org.jetbrains.kotlin.fir.expressions.FirExpression
+import org.jetbrains.kotlin.fir.expressions.FirExpressionWithSmartcast
+import org.jetbrains.kotlin.fir.expressions.FirWhenSubjectExpression
 import org.jetbrains.kotlin.fir.visitors.FirTransformer
 
 fun ModuleInfo.dependenciesWithoutSelf(): Sequence<ModuleInfo> = dependencies().asSequence().filter { it != this }
 
 // TODO: rewrite
 fun FirBlock.returnExpressions(): List<FirExpression> = listOfNotNull(statements.lastOrNull() as? FirExpression)
+
+fun FirElement.unwrapWhenSubjectExpression(): FirElement = if (this is FirWhenSubjectExpression) {
+    val whenExpression = whenSubject.whenExpression
+    whenExpression.subjectVariable
+        ?: whenExpression.subject
+        ?: throw IllegalStateException("Subject or subject variable must be not null")
+} else {
+    this
+}
+
+tailrec fun FirElement.unwrapSmartcast(): FirElement = if (this is FirExpressionWithSmartcast) {
+    originalExpression.unwrapSmartcast()
+} else {
+    this
+}
