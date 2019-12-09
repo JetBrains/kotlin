70e1058fb46fb2f9cc28d7085344ebb0b6618f8e
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2019-12-09 11:59:21 +0300

    IDEA-227040 Export to HTML: no apparent result
    updates following IDEA-CR-55984
    
    intellij-monorepo: c8778815058dec522a2be0648443abb9467261ff

--- intellij-community/platform/lang-impl/src/com/intellij/codeEditor/printing/ExportToHTMLManager.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeEditor/printing/ExportToHTMLManager.java
@@ -14,7 +14,7 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.file.PsiDirectoryFactory;
 import org.jetbrains.annotations.NotNull;
@@ -126,7 +126,7 @@ class ExportToHTMLManager {
       }
     }
     finally {
-      LocalFileSystem.getInstance().refreshIoFiles(Collections.singleton(new File(outputDirectoryName)), true, false, null);
+      VfsUtil.markDirtyAndRefresh(true, true, false, new File(outputDirectoryName));
     }
   }
 
