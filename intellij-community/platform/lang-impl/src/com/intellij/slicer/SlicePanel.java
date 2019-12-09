dd928828eadd5889d0dfbfa2e6a6f45b72274b76
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-12-09 15:43:54 +0100

    cleanup
    intellij-monorepo: 43c4d3a8fd29ff7a03644769452b8760d52d5099

--- intellij-community/platform/lang-impl/src/com/intellij/slicer/SlicePanel.java
+++ intellij-community/platform/lang-impl/src/com/intellij/slicer/SlicePanel.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.slicer;
 
 import com.intellij.icons.AllIcons;
@@ -39,8 +39,8 @@ import javax.swing.tree.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.*;
 import java.util.List;
+import java.util.*;
 
 /**
  * @author cdr
@@ -79,9 +79,6 @@ public abstract class SlicePanel extends JPanel implements TypeSafeDataProvider,
 
     myProject.getMessageBus().connect(this).subscribe(ToolWindowManagerListener.TOPIC, new ToolWindowManagerListener() {
       ToolWindowAnchor myAnchor = toolWindow.getAnchor();
-      @Override
-      public void toolWindowRegistered(@NotNull String id) {
-      }
 
       @Override
       public void stateChanged() {
