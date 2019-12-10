270c2b342d438df48e7e1a038144d384d8fc7ff9
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-12-11 00:53:53 +0900

    Redundant overriding method: do not report when super method is not called
    #KT-34959 Fixed

--- idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
@@ -7452,6 +7452,11 @@ public class LocalInspectionTestGenerated extends AbstractLocalInspectionTest {
             runTest("idea/testData/inspectionsLocal/redundantOverride/callDifferentSuperMethod.kt");
         }
 
+        @TestMetadata("callDifferentSuperMethod2.kt")
+        public void testCallDifferentSuperMethod2() throws Exception {
+            runTest("idea/testData/inspectionsLocal/redundantOverride/callDifferentSuperMethod2.kt");
+        }
+
         @TestMetadata("classAndInterface.kt")
         public void testClassAndInterface() throws Exception {
             runTest("idea/testData/inspectionsLocal/redundantOverride/classAndInterface.kt");
