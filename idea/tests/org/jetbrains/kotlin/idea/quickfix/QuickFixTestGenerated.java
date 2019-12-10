f20a6b7fe1c0d810066b97ee50f1a3ce7062a1b6
Toshiaki Kameyama <kmymtsak@gmail.com>
2019-12-10 17:31:50 +0900

    Change function signature: add/change receiver type
    #KT-21799 Fixed

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/QuickFixTestGenerated.java
@@ -10162,6 +10162,11 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
                 runTest("idea/testData/quickfix/override/nothingToOverride/addFunctionNonUnitReturnType.kt");
             }
 
+            @TestMetadata("addFunctionReciever.kt")
+            public void testAddFunctionReciever() throws Exception {
+                runTest("idea/testData/quickfix/override/nothingToOverride/addFunctionReciever.kt");
+            }
+
             @TestMetadata("addFunctionSealedClass.kt")
             public void testAddFunctionSealedClass() throws Exception {
                 runTest("idea/testData/quickfix/override/nothingToOverride/addFunctionSealedClass.kt");
@@ -10224,6 +10229,11 @@ public class QuickFixTestGenerated extends AbstractQuickFixTest {
 
             public void testAllFilesPresentInNothingToOverride() throws Exception {
                 KotlinTestUtils.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("idea/testData/quickfix/override/nothingToOverride"), Pattern.compile("^([\\w\\-_]+)\\.kt$"), null, true);
+            }
+
+            @TestMetadata("changeFunctionReciever.kt")
+            public void testChangeFunctionReciever() throws Exception {
+                runTest("idea/testData/quickfix/override/nothingToOverride/changeFunctionReciever.kt");
             }
 
             @TestMetadata("changeParameterType.kt")
