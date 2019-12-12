9011eecfdfc961ba84b76a37d7626c32cad71df2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2019-12-12 16:37:33 +0300

    Wizard: change wizard title to experimental

--- idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/NewProjectWizardModuleType.kt
+++ idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/NewProjectWizardModuleType.kt
@@ -4,8 +4,8 @@ import com.intellij.openapi.module.ModuleType
 import org.jetbrains.kotlin.idea.KotlinIcons
 import javax.swing.Icon
 
-class NewProjectWizardModuleType: ModuleType<NewProjectWizardModuleBuilder>(NewProjectWizardModuleBuilder.MODULE_BUILDER_ID) {
-    override fun getName(): String = "New Kotlin Project Wizard"
+class NewProjectWizardModuleType : ModuleType<NewProjectWizardModuleBuilder>(NewProjectWizardModuleBuilder.MODULE_BUILDER_ID) {
+    override fun getName(): String = "Kotlin (Experimental)"
     override fun getDescription(): String = name
     override fun getNodeIcon(isOpened: Boolean): Icon = KotlinIcons.SMALL_LOGO
     override fun createModuleBuilder(): NewProjectWizardModuleBuilder = NewProjectWizardModuleBuilder()
