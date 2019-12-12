5a67532b139120b64641b6461e4938408b23f4bc
Henry Wylde <henry.wylde@jetbrains.com>
2019-12-12 13:27:13 +0100

    RUBY-25512 add text-based resolve to "super" method calls in ruby modules
    There is a design pattern where a module can be used to "extend" behaviour to methods by overriding it, doing something (e.g., logging) and calling super at some point. Unfortunately we cannot tell what "super" refers to as the module doesn't actually override anything yet. This allows us to offer all possible definitions of it as a safety net.
    
    This required a tiny update to GotoDeclarationAction to ensure we can "choose ambiguous targets" when invoking resolve on keywords, as in the ruby language "super" is a keyword but it's also a method call (that can resolve somewhere).
    
    intellij-monorepo: 8d9d6ddfafa246f0bdcc918c4b063a0731ee0a6c

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationAction.java
@@ -88,6 +88,9 @@ public class GotoDeclarationAction extends BaseCodeInsightAction implements Code
           }
         }
 
+        //disable 'no declaration found' notification for keywords
+        if (isKeywordUnderCaret(project, file, offset)) return;
+
         chooseAmbiguousTarget(project, editor, offset, elements, file);
         return;
       }
@@ -156,9 +159,7 @@ public class GotoDeclarationAction extends BaseCodeInsightAction implements Code
     boolean found =
       chooseAmbiguousTarget(project, editor, offset, navigateProcessor, CodeInsightBundle.message("declaration.navigation.title"),
                             elements);
-
-    // Disable the 'no declaration found' notification for keywords
-    if (!found && !isKeywordUnderCaret(project, currentFile, offset)) {
+    if (!found) {
       HintManager.getInstance().showErrorHint(editor, "Cannot find declaration to go to");
     }
   }
