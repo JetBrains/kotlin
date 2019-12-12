f1f2e1830b0f26b6dafce2fad9e6d4d87ced9fdd
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2019-12-12 10:45:01 +0300

    [FIR-TEST] Fix incorrect testdata

--- compiler/fir/resolve/testData/resolve/arguments/incorrectFunctionalType.kt
+++ compiler/fir/resolve/testData/resolve/arguments/incorrectFunctionalType.kt
@@ -1,7 +1,7 @@
-fun foo(func: (Int).Int -> Int) {}
+fun foo(func: Int.(Int) -> Int) {}
 
 fun test() {
-    <!INAPPLICABLE_CANDIDATE!>foo<!> {
-        this + <!UNRESOLVED_REFERENCE!>it<!>
+    foo {
+        this + it
     }
 }
