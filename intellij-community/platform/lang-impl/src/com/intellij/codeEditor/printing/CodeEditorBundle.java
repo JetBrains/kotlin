8fe772515d11eaac14cbd08212a50d6142c90535
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-18 13:13:09 +0300

    more dynamic bundles
    intellij-monorepo: f440550fdfe119a71860961b9bcbdddd3cea0eda

--- intellij-community/platform/lang-impl/src/com/intellij/codeEditor/printing/CodeEditorBundle.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeEditor/printing/CodeEditorBundle.java
@@ -16,36 +16,19 @@
 
 package com.intellij.codeEditor.printing;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
-
-/**
- * @author yole
- */
-public class CodeEditorBundle {
-
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
+public class CodeEditorBundle extends DynamicBundle {
   @NonNls private static final String BUNDLE = "messages.CodeEditorBundle";
+  private static final CodeEditorBundle INSTANCE = new CodeEditorBundle();
 
-  private CodeEditorBundle() {
-  }
+  private CodeEditorBundle() { super(BUNDLE); }
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+  @NotNull
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return INSTANCE.getMessage(key, params);
   }
 }
--- intellij-community/platform/lang-impl/src/com/intellij/tools/ToolsBundle.java
+++ intellij-community/platform/lang-impl/src/com/intellij/tools/ToolsBundle.java
@@ -16,36 +16,21 @@
 
 package com.intellij.tools;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
-
-/**
- * @author yole
- */
-public class ToolsBundle {
-
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
+public class ToolsBundle extends DynamicBundle {
   @NonNls private static final String BUNDLE = "messages.ToolsBundle";
+  private static final ToolsBundle INSTANCE = new ToolsBundle();
 
   private ToolsBundle() {
+    super(BUNDLE);
   }
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+  @NotNull
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return INSTANCE.getMessage(key, params);
   }
 }
