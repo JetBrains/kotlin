2c684c56a3a0a0943edd542b28e3b539aece8e40
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-12-19 13:40:17 +0300

    Mute testVersionsAreConsistent test (KT-35567)

--- compiler/tests/org/jetbrains/kotlin/util/KotlinVersionsTest.kt
+++ compiler/tests/org/jetbrains/kotlin/util/KotlinVersionsTest.kt
@@ -20,6 +20,7 @@ import com.intellij.openapi.util.io.FileUtil
 import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.test.isIgnoredInDatabaseWithLog
 import org.jetbrains.kotlin.test.testFramework.KtUsefulTestCase
 import org.jetbrains.kotlin.utils.addIfNotNull
 import org.junit.Assert
@@ -141,4 +142,8 @@ class KotlinVersionsTest : KtUsefulTestCase() {
     }
 
     private fun Collection<Any>.areEqual(): Boolean = all(first()::equals)
+
+    override fun shouldRunTest(): Boolean {
+        return super.shouldRunTest() && !isIgnoredInDatabaseWithLog(this)
+    }
 }
