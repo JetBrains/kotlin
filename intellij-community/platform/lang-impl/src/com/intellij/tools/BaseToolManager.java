40e8912218e6eade088c024f045a1b0ac1a517cc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-12-19 11:10:58 +0100

    cleanup
    intellij-monorepo: e64bfe83d8b37009ff92fce914a1e5864b9e2995

--- intellij-community/platform/lang-impl/src/com/intellij/tools/BaseToolManager.java
+++ intellij-community/platform/lang-impl/src/com/intellij/tools/BaseToolManager.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.tools;
 
 import com.intellij.openapi.actionSystem.ex.ActionManagerEx;
@@ -84,7 +70,7 @@ public abstract class BaseToolManager<T extends Tool> {
     return mySchemeManager.getAllSchemes();
   }
 
-  public void setTools(@NotNull List<? extends ToolsGroup<T>> tools) {
+  public void setTools(@NotNull List<ToolsGroup<T>> tools) {
     mySchemeManager.setSchemes(tools);
     registerActions();
   }
