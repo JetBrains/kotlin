2746c244fe3464441b57032cdb6495094a2c0474
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2019-12-19 15:06:20 +0300

    IDEA-CR-56535 added scanning for sdk to SdkComboBox
    intellij-monorepo: 03b7dc248456d6a773e00098d78bd790b780ebec

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/SdkComboBox.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/SdkComboBox.kt
@@ -1,7 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.roots.ui.configuration
 
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.util.Disposer
+import com.intellij.ui.PopupMenuListenerAdapter
+import javax.swing.event.PopupMenuEvent
 
 class SdkComboBox(model: SdkComboBoxModel) : SdkComboBoxBase<SdkListItem>(model.modelBuilder) {
   val model get() = getModel() as SdkComboBoxModel
@@ -17,6 +22,7 @@ class SdkComboBox(model: SdkComboBoxModel) : SdkComboBoxBase<SdkListItem>(model.
   override fun setSelectedItem(anObject: Any?) {
     if (anObject is SdkListItem.ActionableItem) {
       anObject.executeAction()
+      return
     }
     reloadModel()
     super.setSelectedItem(anObject)
@@ -40,19 +46,35 @@ class SdkComboBox(model: SdkComboBoxModel) : SdkComboBoxBase<SdkListItem>(model.
     }
   }
 
-  override fun reloadModel() {
-    super.reloadModel()
-    myModel.reloadActions(this, getSelectedSdk())
-    // TODO (jonnyzzz) fix warn -- Cannot find DialogWrapper parent for this comboBox
-    myModel.detectItems(this)
-  }
-
   init {
     setModel(model)
     setRenderer(object : SdkListPresenter(this.model.sdksModel) {
       override fun getModel(): SdkListModel = this@SdkComboBox.model.listModel
-      override fun showProgressIcon() = this@SdkComboBox.isPopupVisible
+      override fun showProgressIcon() = true
     })
+    addPopupMenuListener(ModelReloadProvider())
     reloadModel()
+  }
+
+  private inner class ModelReloadProvider : PopupMenuListenerAdapter() {
+    private var disposable: Disposable? = null
+
+    override fun popupMenuWillBecomeVisible(e: PopupMenuEvent) {
+      val disposable = Disposer.newDisposable()
+      setReloadDisposable(disposable)
+      myModel.reloadActions(this@SdkComboBox, getSelectedSdk())
+      myModel.detectItems(this@SdkComboBox, disposable)
+    }
+
+    override fun popupMenuWillBecomeInvisible(e: PopupMenuEvent) {
+      setReloadDisposable(null)
+    }
+
+    private fun setReloadDisposable(parentDisposable: Disposable?) {
+      ApplicationManager.getApplication().assertIsDispatchThread()
+      parentDisposable?.let { Disposer.register(model.project, it) }
+      disposable?.let { Disposer.dispose(it) }
+      disposable = parentDisposable
+    }
   }
 }
