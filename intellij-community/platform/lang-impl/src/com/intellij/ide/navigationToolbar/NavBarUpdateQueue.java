bcede25cd75e36f5475bba3557781c74467a04cf
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-12-21 07:23:57 +0100

    more strict API — ensure that client doesn't pass incorrect IdeFrame instance, require Window instead
    intellij-monorepo: 2d892870f10e54dddbed444fc05ae5e77aceaf77

--- intellij-community/platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarUpdateQueue.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/navigationToolbar/NavBarUpdateQueue.java
@@ -114,7 +114,7 @@ public class NavBarUpdateQueue extends MergingUpdateQueue {
       if (!wnd.isActive()) {
         IdeFrame frame = ComponentUtil.getParentOfType((Class<? extends IdeFrame>)IdeFrame.class, (Component)myPanel);
         if (frame != null) {
-          focus = IdeFocusManager.getInstance(project).getLastFocusedFor(frame);
+          focus = IdeFocusManager.getInstance(project).getLastFocusedFor((Window)frame);
         }
       }
       else {
