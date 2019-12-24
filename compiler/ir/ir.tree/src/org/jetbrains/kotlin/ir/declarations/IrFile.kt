c506f800bffcbbbc95c67df219658d9d5375c1b1
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2019-12-25 02:51:38 +0300

    ~ for ir.psi2ir

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrFile.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrFile.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.ir.declarations
@@ -19,7 +8,6 @@ package org.jetbrains.kotlin.ir.declarations
 import org.jetbrains.kotlin.descriptors.PackageFragmentDescriptor
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.SourceManager
-import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.symbols.IrExternalPackageFragmentSymbol
 import org.jetbrains.kotlin.ir.symbols.IrFileSymbol
 import org.jetbrains.kotlin.ir.symbols.IrPackageFragmentSymbol
@@ -43,7 +31,7 @@ interface IrFile : IrPackageFragment, IrMutableAnnotationContainer, IrMetadataSo
 
     val fileEntry: SourceManager.FileEntry
 
-    override val metadata: MetadataSource.File?
+    override var metadata: MetadataSource.File?
 
     override fun <D> transform(transformer: IrElementTransformer<D>, data: D): IrFile =
         accept(transformer, data) as IrFile
