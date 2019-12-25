bbce7060e7691d2af8efbff9aba5c08fa35e9b97
Alexander Zolotov <zolotov@jetbrains.com>
2019-12-25 17:27:48 +0300

    @NonNls
    intellij-monorepo: cdd96ee534b0fe851125f481de536e1041108a38

--- intellij-community/platform/lang-impl/src/com/intellij/application/options/ModuleAwareProjectConfigurable.java
+++ intellij-community/platform/lang-impl/src/com/intellij/application/options/ModuleAwareProjectConfigurable.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.application.options;
 
 import com.intellij.icons.AllIcons;
@@ -31,6 +17,7 @@ import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -54,7 +41,7 @@ public abstract class ModuleAwareProjectConfigurable<T extends UnnamedConfigurab
   private final Map<Module, T> myModuleConfigurables = new HashMap<>();
   private final static String PROJECT_ITEM_KEY = "thisisnotthemoduleyouarelookingfor";
 
-  public ModuleAwareProjectConfigurable(@NotNull Project project, String displayName, String helpTopic) {
+  public ModuleAwareProjectConfigurable(@NotNull Project project, String displayName, @NonNls String helpTopic) {
     myProject = project;
     myDisplayName = displayName;
     myHelpTopic = helpTopic;
