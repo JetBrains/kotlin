3e7b2beac9dd45b718f986813936f158965793b5
Denis Zharkov <denis.zharkov@jetbrains.com>
2019-12-25 16:41:17 +0300

    NI: Fix type mismatch caused by smartcast
    ^KT-25434 Fixed

--- compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -10025,6 +10025,11 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                 runTest("compiler/testData/diagnostics/tests/inference/starApproximationFlexible.kt");
             }
 
+            @TestMetadata("tooEagerSmartcast.kt")
+            public void testTooEagerSmartcast() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/inference/tooEagerSmartcast.kt");
+            }
+
             @TestMetadata("tryNumberLowerBoundsBeforeUpperBounds.kt")
             public void testTryNumberLowerBoundsBeforeUpperBounds() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/inference/tryNumberLowerBoundsBeforeUpperBounds.kt");
