ba57ab531b23158e521f958b44c69951a7e6e3fc
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2019-12-26 13:59:43 +0300

    Build: Fix android-wizardTemplate-plugin dependencies
    Present only in AS 3.6
    
    (cherry picked from commit 7b1c4e72baf309d9e359d7808bf0ecaaa357c2ed)

--- idea/idea-gradle/build.gradle.kts
+++ idea/idea-gradle/build.gradle.kts
@@ -76,6 +76,9 @@ dependencies {
 
     if (Ide.AS36.orHigher()) {
         testRuntime(intellijPluginDep("android-layoutlib"))
+    }
+
+    if (Ide.AS36()) {
         testRuntime(intellijPluginDep("android-wizardTemplate-plugin"))
     }
 }
