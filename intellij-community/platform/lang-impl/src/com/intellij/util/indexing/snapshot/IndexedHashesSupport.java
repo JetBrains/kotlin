a49001eca840d35a117c156ebdad608749e1c74b
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2019-12-28 11:37:58 +0300

    simplify HashEnumerator (remove suspicious thread local field)
    intellij-monorepo: 123746faaef218f11e3acb5382e2a6760c92065f

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/snapshot/IndexedHashesSupport.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/snapshot/IndexedHashesSupport.java
@@ -35,7 +35,7 @@ public class IndexedHashesSupport {
       if (ourHashesWithFileType != null) return;
       final File hashEnumeratorFile = new File(IndexInfrastructure.getPersistentIndexRoot(), "hashesWithFileType");
       try {
-        ContentHashesUtil.HashEnumerator hashEnumerator = new ContentHashesUtil.HashEnumerator(hashEnumeratorFile.toPath(), null);
+        ContentHashesUtil.HashEnumerator hashEnumerator = new ContentHashesUtil.HashEnumerator(hashEnumeratorFile.toPath());
         FlushingDaemon.everyFiveSeconds(IndexedHashesSupport::flushContentHashes);
         ShutDownTracker.getInstance().registerShutdownTask(IndexedHashesSupport::flushContentHashes);
         ourHashesWithFileType = hashEnumerator;
