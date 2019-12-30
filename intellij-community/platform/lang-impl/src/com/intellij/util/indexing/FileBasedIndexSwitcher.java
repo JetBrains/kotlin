3ccabafb54f5bd8ebded57f4ee8e44b51531cbb8
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2019-12-30 11:48:12 +0300

    turn off/on file based index in write action (no synchronous dumb mode required)
    intellij-monorepo: c124ef727f8442212aee9475c293eb8157e95e77

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexSwitcher.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexSwitcher.java
@@ -1,22 +1,16 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing;
 
-import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.DumbModeTask;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.util.concurrency.Semaphore;
+import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
 public class FileBasedIndexSwitcher {
+    private static final Logger LOG = Logger.getInstance(FileBasedIndexSwitcher.class);
+
     @NotNull
     private final FileBasedIndexImpl myFileBasedIndex;
-    @NotNull
-    private final Semaphore mySemaphore = new Semaphore();
 
     @TestOnly
     public FileBasedIndexSwitcher() {
@@ -28,33 +22,14 @@ public class FileBasedIndexSwitcher {
     }
 
     public void turnOff() {
-        assert ApplicationManager.getApplication().isDispatchThread();
-        FileBasedIndexImpl.LOG.assertTrue(mySemaphore.isUp());
-        mySemaphore.down();
-
-        if (!ApplicationManager.getApplication().isUnitTestMode()) {
-            for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-                DumbService.getInstance(project).queueTask(new DumbModeTask() {
-                    @Override
-                    public void performInDumbMode(@NotNull ProgressIndicator indicator) {
-                        indicator.setText(IdeBundle.message("progress.indexing.reload"));
-                        mySemaphore.waitFor();
-                    }
-                });
-            }
-        }
-
+        LOG.assertTrue(ApplicationManager.getApplication().isWriteAccessAllowed());
         myFileBasedIndex.performShutdown(true);
         myFileBasedIndex.dropRegisteredIndexes();
     }
 
     public void turnOn() {
-        assert ApplicationManager.getApplication().isDispatchThread();
+        LOG.assertTrue(ApplicationManager.getApplication().isWriteAccessAllowed());
         myFileBasedIndex.initComponent();
-
-        FileBasedIndexImpl.LOG.assertTrue(!mySemaphore.isUp());
-        mySemaphore.up();
-
         if (ApplicationManager.getApplication().isUnitTestMode()) {
             myFileBasedIndex.waitUntilIndicesAreInitialized();
         }
