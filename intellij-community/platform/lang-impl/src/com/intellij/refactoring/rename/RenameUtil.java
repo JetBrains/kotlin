4937a432ff84acbadec0014de65bca0a27aca266
Sergey Stupin <sergey.stupin@jetbrains.com>
2020-01-02 12:40:02 +0100

    LAB-31 move doRenameGenericNamedElement to analysis module
    intellij-monorepo: 32d0b82dd18a85d60ca636f417339ab7be90d3c1

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/rename/RenameUtil.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/rename/RenameUtil.java
@@ -21,7 +21,6 @@ import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.*;
 import com.intellij.psi.meta.PsiMetaData;
 import com.intellij.psi.meta.PsiMetaOwner;
-import com.intellij.psi.meta.PsiWritableMetaData;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.LocalSearchScope;
 import com.intellij.psi.search.PsiSearchHelper;
@@ -213,64 +212,11 @@ public class RenameUtil {
 
   public static void doRenameGenericNamedElement(@NotNull PsiElement namedElement, String newName, UsageInfo[] usages,
                                                  @Nullable RefactoringElementListener listener) throws IncorrectOperationException {
-    PsiWritableMetaData writableMetaData = null;
-    if (namedElement instanceof PsiMetaOwner) {
-      final PsiMetaData metaData = ((PsiMetaOwner)namedElement).getMetaData();
-      if (metaData instanceof PsiWritableMetaData) {
-        writableMetaData = (PsiWritableMetaData)metaData;
-      }
-    }
-    if (writableMetaData == null && !(namedElement instanceof PsiNamedElement)) {
-      LOG.error("Unknown element type:" + namedElement);
-    }
-
-    boolean hasBindables = false;
-    for (UsageInfo usage : usages) {
-      if (!(usage.getReference() instanceof BindablePsiReference)) {
-        rename(usage, newName);
-      } else {
-        hasBindables = true;
-      }
-    }
-
-    if (writableMetaData != null) {
-      writableMetaData.setName(newName);
-    }
-    else {
-      PsiElement namedElementAfterRename = ((PsiNamedElement)namedElement).setName(newName);
-      if (namedElementAfterRename != null) namedElement = namedElementAfterRename;
-    }
-
-    if (hasBindables) {
-      for (UsageInfo usage : usages) {
-        final PsiReference ref = usage.getReference();
-        if (ref instanceof BindablePsiReference) {
-          boolean fallback = true;
-          if (!(ref instanceof FragmentaryPsiReference
-                && ((FragmentaryPsiReference)ref).isFragmentOnlyRename())) {
-            try {
-              ref.bindToElement(namedElement);
-              fallback = false;
-            }
-            catch (IncorrectOperationException ignored) {
-            }
-          }
-          if (fallback) {//fall back to old scheme
-            ref.handleElementRename(newName);
-          }
-        }
-      }
-    }
-    if (listener != null) {
-      listener.elementRenamed(namedElement);
-    }
+    RenameUtilBase.doRenameGenericNamedElement(namedElement, newName, usages, listener);
   }
 
   public static void rename(UsageInfo info, String newName) throws IncorrectOperationException {
-    if (info.getElement() == null) return;
-    PsiReference ref = info.getReference();
-    if (ref == null) return;
-    ref.handleElementRename(newName);
+    RenameUtilBase.rename(info, newName);
   }
 
   @Nullable
