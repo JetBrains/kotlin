79bb80a4dc9f4a6d1ea518ddbce194dd2fc33e8a
Toshiaki Kameyama <kmymtsak@gmail.com>
2020-01-05 14:50:47 +0900

    FoldInitializerAndIfToElvis: should not add new line for multiline initializer
    #KT-35805 Fixed
    
    Original commit: 6cb0190fad6682374e9c515cfd688ed83908c8ec

---
+++ kotlin-ide/idea/testData/inspectionsLocal/foldInitializerAndIfToElvis/LongName2.kt
@@ -0,0 +1,7 @@
+fun foo(): Any {
+    val yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy = 7
+    val xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+            = 24
+    if (xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<caret> == null) return yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+    return 42
+}
