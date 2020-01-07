6d205abd3bc011a3afa4a49d84c4c213b6cd5504
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-07 17:04:57 +0100

    cleanup
    intellij-monorepo: d0419ba31bec9c2fd338bcfa90a8a579e66c2bf9

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/internal/ExternalSystemProcessingManager.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/internal/ExternalSystemProcessingManager.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.service.internal;
 
 import com.intellij.openapi.Disposable;
@@ -131,7 +131,7 @@ public final class ExternalSystemProcessingManager implements ExternalSystemTask
   @Override
   public void onStart(@NotNull ExternalSystemTaskId id, String workingDir) {
     myTasksInProgress.put(id, System.currentTimeMillis() + TOO_LONG_EXECUTION_MS);
-    if (myAlarm.getActiveRequestCount() <= 0) {
+    if (myAlarm.isEmpty()) {
       myAlarm.addRequest(() -> update(), TOO_LONG_EXECUTION_MS);
     }
   }
