a1e91192324e98f27ad60ed99fc5ebe19eab62e3
Mark Punzalan <markpun@google.com>
2020-01-07 22:50:51 -0800

    [JVM IR] Support target templates in AbstractCheckLocalVariablesTableTest and fix `checkLocalVariablesTable/localFun.kt` for JVM IR.
    Also improved diagnosability of CheckLocalVariablesTableTest failures.

--- compiler/tests-common/tests/org/jetbrains/kotlin/codegen/ir/AbstractIrCheckLocalVariablesTableTest.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/codegen/ir/AbstractIrCheckLocalVariablesTableTest.kt
@@ -5,43 +5,9 @@
 
 package org.jetbrains.kotlin.codegen.ir
 
-import com.intellij.openapi.util.Comparing
 import org.jetbrains.kotlin.codegen.AbstractCheckLocalVariablesTableTest
 import org.jetbrains.kotlin.test.TargetBackend
-import org.junit.ComparisonFailure
-import java.io.File
 
 abstract class AbstractIrCheckLocalVariablesTableTest : AbstractCheckLocalVariablesTableTest() {
-    override fun doCompare(
-        testFile: File,
-        text: String,
-        actualLocalVariables: List<LocalVariable>
-    ) {
-        val actual = getActualVariablesAsList(actualLocalVariables)
-        val expected = getExpectedVariablesAsList(testFile)
-        if (!Comparing.equal(expected, actual)) {
-            throw ComparisonFailure(
-                "Variables differ from expected",
-                expected.joinToString("\n"),
-                actual.joinToString("\n")
-            )
-        }
-    }
-
-    private fun getActualVariablesAsList(list: List<LocalVariable>): List<String> {
-        return list.map { it.toString() }
-            // Ignore local index.
-            .map { line -> line.replaceFirst("INDEX=\\d+".toRegex(), "INDEX=*") }
-            .sorted()
-    }
-
-    private fun getExpectedVariablesAsList(testFile: File): List<String> {
-        return testFile.readLines()
-            .filter { line -> line.startsWith("// VARIABLE ") }
-            // Ignore local index.
-            .map { line -> line.replaceFirst("INDEX=\\d+".toRegex(), "INDEX=*") }
-            .sorted()
-    }
-
     override fun getBackend() = TargetBackend.JVM_IR
 }
