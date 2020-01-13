605ef647e6e30219f7362dc2e51dab4240c775cb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-01-13 16:19:30 +0300

    Regenerate tests

--- idea/tests/org/jetbrains/kotlin/idea/codeInsight/ChangeLocalityDetectorTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/codeInsight/ChangeLocalityDetectorTestGenerated.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -25,7 +25,7 @@ public class ChangeLocalityDetectorTestGenerated extends AbstractChangeLocalityD
     }
 
     public void testAllFilesPresentInChangeLocality() throws Exception {
-        KotlinTestUtils.assertAllTestsPresentByMetadata(this.getClass(), new File("idea/testData/codeInsight/changeLocality"), Pattern.compile("^(.+)\\.(kt|kts)$"), true);
+        KotlinTestUtils.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("idea/testData/codeInsight/changeLocality"), Pattern.compile("^(.+)\\.(kt|kts)$"), null, true);
     }
 
     @TestMetadata("Comment.kt")
