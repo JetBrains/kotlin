9e7f72382fbc109b5ba62901796d46087d1963fe
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-01-13 20:54:37 +0300

    [stdlib-js-ir] Remove BitUtils object and make all members toplevel
    It works slightly better with IR DCE --
    we get about 6% less code on box tests.

--- libraries/stdlib/js-ir/src/kotlin/math_js-ir.kt
+++ libraries/stdlib/js-ir/src/kotlin/math_js-ir.kt
@@ -1,10 +1,8 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 package kotlin.math
-
-import kotlin.js.internal.BitUtils
 
 /**
  * Returns this value with the sign bit same as of the [sign] value.
@@ -13,7 +11,7 @@ import kotlin.js.internal.BitUtils
  */
 @SinceKotlin("1.2")
 public actual fun Double.withSign(sign: Double): Double {
-    val thisSignBit = BitUtils.doubleSignBit(this)
-    val newSignBit = BitUtils.doubleSignBit(sign)
+    val thisSignBit = doubleSignBit(this)
+    val newSignBit = doubleSignBit(sign)
     return if (thisSignBit == newSignBit) this else -this
 }
