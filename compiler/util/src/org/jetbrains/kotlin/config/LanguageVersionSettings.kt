3d85e5da5ff96a0b10f9b24d6eeddf062d3d146a
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-01-14 16:13:42 +0300

    Enable ProperVisibilityForCompanionObjectInstanceField in 1.4

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -123,8 +123,8 @@ enum class LanguageFeature(
     PreferJavaFieldOverload(KOTLIN_1_4),
     AllowContractsForNonOverridableMembers(KOTLIN_1_4),
     AllowReifiedGenericsInContracts(KOTLIN_1_4),
+    ProperVisibilityForCompanionObjectInstanceField(KOTLIN_1_4, kind = BUG_FIX),
 
-    ProperVisibilityForCompanionObjectInstanceField(sinceVersion = null, kind = BUG_FIX),
     // Temporarily disabled, see KT-27084/KT-22379
     SoundSmartcastFromLoopConditionForLoopAssignedVariables(sinceVersion = null, kind = BUG_FIX),
 
