a3037a081ec9da48e63fb57def0d368e5bec1859
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-01-15 17:24:25 +0300

    [NI] Fix resolve of lambdas with expected function type with receiver
    #KT-30245

--- compiler/testData/diagnostics/tests/functionAsExpression/ReceiverByExpectedType.kt
+++ compiler/testData/diagnostics/tests/functionAsExpression/ReceiverByExpectedType.kt
@@ -1,3 +1,3 @@
 // !WITH_NEW_INFERENCE
 fun foo(<!UNUSED_PARAMETER!>f<!>: String.() -> Int) {}
-val test = foo(<!OI;TYPE_MISMATCH!>fun () = <!UNRESOLVED_REFERENCE!>length<!><!>)
+val test = foo(<!TYPE_MISMATCH!>fun <!NI;EXPECTED_PARAMETERS_NUMBER_MISMATCH!>()<!> = <!UNRESOLVED_REFERENCE!>length<!><!>)
