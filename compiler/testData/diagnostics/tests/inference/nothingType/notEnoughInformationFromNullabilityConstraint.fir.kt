3c482180977ae2689176ff887383a8f69f37afae
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-01-15 17:01:44 +0300

    [NI] Update filtering of constraints with equal types
    Make check for synthetic nullability constraint status before skipping
    new constraint with nonunique type.

--- compiler/testData/diagnostics/tests/inference/nothingType/notEnoughInformationFromNullabilityConstraint.fir.kt
+++ compiler/testData/diagnostics/tests/inference/nothingType/notEnoughInformationFromNullabilityConstraint.fir.kt
@@ -1,9 +1,16 @@
-// !LANGUAGE: +NewInference
 // !WITH_NEW_INFERENCE
+// !DIAGNOSTICS: -UNUSED_PARAMETER
 
 fun <M> make(): M? = null
 fun <I> id(arg: I): I = arg
+fun <S> select(vararg args: S): S = TODO()
 
-val v = id(
-    make()
-)
+fun test() {
+    id(
+        make()
+    )
+
+    select(make(), null)
+
+    if (true) make() else TODO()
+}
