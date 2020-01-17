463728a96acfeb8a3ecf402ba226ec50c7a6bfb7
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2020-01-17 12:44:15 +0100

    Fix compiler test on Throws+delegation for 1.4
    No exceptions are generated for delegation in 1.4 (see KT-35834).

--- compiler/testData/compileJavaAgainstKotlin/method/throws/Delegation.java
+++ compiler/testData/compileJavaAgainstKotlin/method/throws/Delegation.java
@@ -4,16 +4,7 @@ class JavaClass {
     void testMethod() {
         Test test = new Test();
         test.none();
-
-        try {
-            test.one();
-        }
-        catch (E1 e) {}
-
-        try {
-            test.two();
-        }
-        catch (E1 e) {}
-        catch (E2 e) {}
+        test.one();
+        test.two();
     }
 }
