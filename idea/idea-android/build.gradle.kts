f3bb45c59c2fa2a423c6a1c7a33a49cece119f72
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-01-17 15:39:21 +0300

    as36: Update to AS 3.6 RC (192.7142.36.36.6071332)
    ^KT-36005 Fixed
    
    (cherry picked from commit 606279b462720dbd0e87a271fc0309d24b1ef47c)

--- idea/idea-android/build.gradle.kts
+++ idea/idea-android/build.gradle.kts
@@ -73,10 +73,6 @@ dependencies {
     if (Ide.AS36.orHigher()) {
         testRuntime(intellijPluginDep("android-layoutlib"))
     }
-
-    if (Ide.AS36()) {
-        testRuntime(intellijPluginDep("android-wizardTemplate-plugin"))
-    }
 }
 
 sourceSets {
