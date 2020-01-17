7df1b54c821d37332065825bf5885d5100594dc2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-01-17 16:55:00 +0300

    Wizard: Show message when module has no configurable settings

--- idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/ui/setting/SettingsList.kt
+++ idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/ui/setting/SettingsList.kt
@@ -1,18 +1,19 @@
 package org.jetbrains.kotlin.tools.projectWizard.wizard.ui.setting
 
 import com.intellij.ui.components.panels.VerticalLayout
-import org.jetbrains.kotlin.tools.projectWizard.core.entity.SettingReference
 import org.jetbrains.kotlin.tools.projectWizard.core.ValuesReadingContext
+import org.jetbrains.kotlin.tools.projectWizard.core.entity.SettingReference
 import org.jetbrains.kotlin.tools.projectWizard.wizard.ui.Component
 import org.jetbrains.kotlin.tools.projectWizard.wizard.ui.DynamicComponent
+import org.jetbrains.kotlin.tools.projectWizard.wizard.ui.PanelWithStatusText
 import javax.swing.JComponent
-import javax.swing.JPanel
 
 class SettingsList(
     settings: List<SettingReference<*, *>>,
     private val valuesReadingContext: ValuesReadingContext
 ) : DynamicComponent(valuesReadingContext) {
-    private val panel = JPanel(VerticalLayout(5))
+    private val panel = PanelWithStatusText(VerticalLayout(5), "This module has no settings to configure")
+
     private var settingComponents: List<Component> = emptyList()
 
     init {
@@ -27,11 +28,11 @@ class SettingsList(
     }
 
     fun setSettings(settings: List<SettingReference<*, *>>) {
+        panel.isStatusTextVisible = settings.isEmpty()
         panel.removeAll()
         settingComponents = settings.map { setting ->
             DefaultSettingComponent.create(setting, valuesReadingContext)
         }
         settingComponents.forEach { setting -> setting.onInit(); panel.add(setting.component) }
     }
-
 }
