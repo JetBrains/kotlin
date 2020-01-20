b82e0d1e8065a1a6825b633c6e730fae7cc1c43f
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-01-20 14:59:09 +0300

    Symbol [impl] default searcher for declarations of PSI elements
    intellij-monorepo: d20af9e4dce8930a83c74fdb08ac264cc37d1b9b

---
+++ intellij-community/platform/lang-impl/src/com/intellij/model/psi/impl/PsiElementDeclarationSearcher.kt
@@ -0,0 +1,65 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.model.psi.impl
+
+import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.model.psi.PsiSymbolDeclaration
+import com.intellij.model.psi.PsiSymbolService
+import com.intellij.model.search.PsiSymbolDeclarationSearchParameters
+import com.intellij.model.search.PsiSymbolDeclarationSearcher
+import com.intellij.pom.PomTargetPsiElement
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiTarget
+import com.intellij.psi.search.LocalSearchScope
+import com.intellij.psi.search.SearchScope
+import com.intellij.util.ArrayQuery
+import com.intellij.util.Query
+
+class PsiElementDeclarationSearcher : PsiSymbolDeclarationSearcher {
+
+  override fun collectSearchRequests(parameters: PsiSymbolDeclarationSearchParameters): Collection<Query<out PsiSymbolDeclaration>> {
+    val psi = PsiSymbolService.getInstance().extractElementFromSymbol(parameters.symbol) ?: return emptyList()
+    val declaration = getDeclaration(psi, parameters.searchScope) ?: return emptyList()
+    return listOf(ArrayQuery(declaration))
+  }
+
+  private fun getDeclaration(psi: PsiElement, searchScope: SearchScope): PsiSymbolDeclaration? {
+    if (psi is PomTargetPsiElement) {
+      val target = psi.target
+      if (target is PsiTarget) {
+        val navigationElement = target.navigationElement
+        if (navigationElement in searchScope) {
+          return PsiElement2Declaration.createFromPom(target, navigationElement)
+        }
+      }
+      return null
+    }
+    else {
+      if (psi !in searchScope) {
+        return null
+      }
+      return PsiElement2Declaration.createFromPsi(psi, psi)
+    }
+  }
+
+  private operator fun SearchScope.contains(psi: PsiElement): Boolean {
+    val containingFile = psi.containingFile
+                         ?: return false
+    if (this is LocalSearchScope) {
+      val range = psi.navigationElement.textRange
+                  ?: return false
+      if (containsRange(containingFile, range)) {
+        return true
+      }
+      if (isIgnoreInjectedPsi) {
+        return false
+      }
+      val host = InjectedLanguageManager.getInstance(psi.project).getInjectionHost(containingFile) ?: return false
+      return host in this
+    }
+    else {
+      val virtualFile = containingFile.virtualFile
+                        ?: return false
+      return contains(virtualFile)
+    }
+  }
+}
