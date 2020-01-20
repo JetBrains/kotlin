cea612cba400abb267ea35ab3d486aa59b90b3a2
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2020-01-20 16:47:59 +0300

    Add test on expect class with test methods inside
    ^KT-34503 In Progress
    
    (current behaviour is undesired, see next commit for fix)

---
+++ idea/testData/gradle/testRunConfigurations/expectClassWithTests/src/commonTest/kotlin/sample/SampleTests.kt
@@ -0,0 +1,9 @@
+package sample
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+expect class <lineMarker descr="Run Test" settings="Nothing here"><lineMarker descr="Has actuals in JS, JVM">SampleTests</lineMarker></lineMarker> {
+    @Test
+    fun <lineMarker descr="Run Test" settings="Nothing here"><lineMarker descr="Has actuals in JS, JVM">testMe</lineMarker></lineMarker>()
+}
