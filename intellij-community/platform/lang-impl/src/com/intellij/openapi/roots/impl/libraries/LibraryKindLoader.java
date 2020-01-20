e4aeaab5632a8d3e7af6340a0c804d29386d9916
nik <Nikolay.Chashnikov@jetbrains.com>
2020-01-20 14:17:49 +0300

    [platform] make libraryType extension point dynamic (IDEA-225619)
    intellij-monorepo: aa3117bc5bf370b65431e4ddc4d768e51802da97

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/impl/libraries/LibraryKindLoader.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/impl/libraries/LibraryKindLoader.java
@@ -2,7 +2,16 @@
 package com.intellij.openapi.roots.impl.libraries;
 
 import com.intellij.ide.ApplicationInitializedListener;
-import com.intellij.openapi.roots.libraries.LibraryType;
+import com.intellij.openapi.extensions.ExtensionPointListener;
+import com.intellij.openapi.extensions.PluginDescriptor;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.impl.OrderEntryUtil;
+import com.intellij.openapi.roots.libraries.*;
+import org.jetbrains.annotations.NotNull;
 
 public class LibraryKindLoader implements ApplicationInitializedListener {
   @Override
@@ -10,5 +19,41 @@ public class LibraryKindLoader implements ApplicationInitializedListener {
     //todo[nik] this is temporary workaround for IDEA-98118: we need to initialize all library types to ensure that their kinds are created and registered in LibraryKind.ourAllKinds
     //In order to properly fix the problem we should extract all UI-related methods from LibraryType to a separate class and move LibraryType to intellij.platform.projectModel.impl module
     LibraryType.EP_NAME.getExtensionList();
+
+    LibraryType.EP_NAME.addExtensionPointListener(new ExtensionPointListener<LibraryType<?>>() {
+      @Override
+      public void extensionRemoved(@NotNull LibraryType<?> extension, @NotNull PluginDescriptor pluginDescriptor) {
+        clearKindInAllLibraries(extension.getKind());
+      }
+    }, null);
+  }
+
+  private static void clearKindInAllLibraries(@NotNull PersistentLibraryKind<?> kind) {
+    clearKindInLibraries(kind, LibraryTablesRegistrar.getInstance().getLibraryTable());
+    for (LibraryTable table : LibraryTablesRegistrar.getInstance().getCustomLibraryTables()) {
+      clearKindInLibraries(kind, table);
+    }
+    for (Project project : ProjectManager.getInstance().getOpenProjects()) {
+      clearKindInLibraries(kind, LibraryTablesRegistrar.getInstance().getLibraryTable(project));
+      for (Module module : ModuleManager.getInstance(project).getModules()) {
+        for (Library library : OrderEntryUtil.getModuleLibraries(ModuleRootManager.getInstance(module))) {
+          clearKind(kind, library);
+        }
+      }
+    }
+  }
+
+  private static void clearKindInLibraries(@NotNull PersistentLibraryKind<?> kind, @NotNull LibraryTable table) {
+    for (Library library : table.getLibraries()) {
+      clearKind(kind, library);
+    }
+  }
+
+  private static void clearKind(@NotNull PersistentLibraryKind<?> kind, @NotNull Library library) {
+    if (kind.equals(((LibraryEx)library).getKind())) {
+      LibraryEx.ModifiableModelEx model = (LibraryEx.ModifiableModelEx)library.getModifiableModel();
+      model.clearKind();
+      model.commit();
+    }
   }
 }
