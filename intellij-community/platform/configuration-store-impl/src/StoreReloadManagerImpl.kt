6c63bf193db49b22f31672c844434d912278c68d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-21 15:45:02 +0100

    cleanup
    intellij-monorepo: 7d340a7c2a76954a7ffe9b5a5d9b874fbe702f06

--- intellij-community/platform/configuration-store-impl/src/StoreReloadManagerImpl.kt
+++ intellij-community/platform/configuration-store-impl/src/StoreReloadManagerImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore
 
 import com.intellij.configurationStore.schemeManager.SchemeChangeApplicator
@@ -238,7 +238,7 @@ open class StoreReloadManagerImpl : StoreReloadManager, Disposable {
       return true
     }
 
-    val changes = LinkedHashSet<StateStorage>(changedApplicationFiles)
+    val changes = LinkedHashSet(changedApplicationFiles)
     changedApplicationFiles.clear()
 
     return reloadAppStore(changes)
