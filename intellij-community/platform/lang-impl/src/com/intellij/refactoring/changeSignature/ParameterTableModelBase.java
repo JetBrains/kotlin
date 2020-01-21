3a6798e9a36641d66d5c5bf38da0e27ad9a6778e
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-01-21 14:05:07 +0100

    add ParameterInfo#isNew, replace usages of obscure -1 constant
    intellij-monorepo: 54dc2b09e8024bba632d32b41610db15e80d70ee

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/changeSignature/ParameterTableModelBase.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/changeSignature/ParameterTableModelBase.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.changeSignature;
 
 import com.intellij.openapi.fileTypes.FileType;
@@ -215,7 +201,7 @@ public abstract class ParameterTableModelBase<P extends ParameterInfo, TableItem
 
     @Override
     public boolean isCellEditable(TableItem item) {
-      return !item.isEllipsisType() && item.parameter.getOldIndex() == -1;
+      return !item.isEllipsisType() && item.parameter.isNew();
     }
 
     @Override
@@ -242,7 +228,7 @@ public abstract class ParameterTableModelBase<P extends ParameterInfo, TableItem
 
     @Override
     public boolean isCellEditable(TableItem item) {
-      return !item.isEllipsisType() && item.parameter.getOldIndex() == -1;
+      return !item.isEllipsisType() && item.parameter.isNew();
     }
 
     @Override
