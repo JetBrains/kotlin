9c1156404d918ee89f992ce86cd062e2cc24f2c3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-22 16:59:53 +0100

    avoid extending DefaultProgramRunner
    intellij-monorepo: eff82b061351c1aaa49dc8753a478f96fbcee2fc

--- intellij-community/platform/lang-impl/src/com/intellij/execution/runners/DefaultProgramRunner.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/runners/DefaultProgramRunner.kt
@@ -1,23 +1,33 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.execution.runners
 
 import com.intellij.execution.ExecutionException
+import com.intellij.execution.ExecutionManager
 import com.intellij.execution.ExecutionResult
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.configurations.RunnerSettings
 import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import org.jetbrains.concurrency.resolvedPromise
 
-abstract class DefaultProgramRunner : GenericProgramRunner<RunnerSettings>() {
+abstract class DefaultProgramRunner : ProgramRunner<RunnerSettings> {
   @Throws(ExecutionException::class)
-  override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
+  final override fun execute(environment: ExecutionEnvironment, callback: ProgramRunner.Callback?) {
+    val state = environment.state ?: return
+    ExecutionManager.getInstance(environment.project).startRunProfile(environment, callback, {
+      resolvedPromise(doExecute(state, environment))
+    })
+  }
+
+  @Throws(ExecutionException::class)
+  protected open fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
     return executeState(state, environment, this)
   }
 }
 
-fun executeState(state: RunProfileState, env: ExecutionEnvironment, runner: ProgramRunner<*>): RunContentDescriptor? {
+fun executeState(state: RunProfileState, environment: ExecutionEnvironment, runner: ProgramRunner<*>): RunContentDescriptor? {
   FileDocumentManager.getInstance().saveAllDocuments()
-  return showRunContent(state.execute(env.executor, runner), env)
+  return showRunContent(state.execute(environment.executor, runner), environment)
 }
 
 fun showRunContent(executionResult: ExecutionResult?, environment: ExecutionEnvironment): RunContentDescriptor? {
