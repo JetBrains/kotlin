8cacc8285508029215ba1e9d6c0d088066427177
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-01-23 13:10:11 +0100

    IndexChunk - move code
    intellij-monorepo: b798e7e088c83328ee3ead1723292054d587c986

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/hash/building/DumpIndexAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/hash/building/DumpIndexAction.java
@@ -7,27 +7,25 @@ import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.AdditionalLibraryRootsProvider;
-import com.intellij.openapi.roots.ProjectFileIndex;
-import com.intellij.openapi.roots.SyntheticLibrary;
+import com.intellij.openapi.roots.*;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.indexing.IndexableSetContributor;
 import gnu.trove.THashSet;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -64,8 +62,8 @@ public class DumpIndexAction extends AnAction {
   private static List<IndexChunk> buildChunks(@NotNull Project project) {
     Collection<IndexChunk> projectChunks = Arrays
       .stream(ModuleManager.getInstance(project).getModules())
-      .flatMap(m -> IndexChunk.generate(m))
-      .collect(Collectors.toMap(ch -> ch.getName(), ch -> ch, IndexChunk::mergeUnsafe))
+      .flatMap(m -> generateIndexChunk(m))
+      .collect(Collectors.toMap(ch -> ch.getName(), ch -> ch, DumpIndexAction::mergeUnsafe))
       .values();
 
     Set<VirtualFile>
@@ -89,5 +87,65 @@ public class DumpIndexAction extends AnAction {
     return Stream.concat(projectChunks.stream(),
                          Stream.of(new IndexChunk(additionalRoots, "ADDITIONAL"),
                                    new IndexChunk(synthRoots, "SYNTH"))).collect(Collectors.toList());
+  }
+
+  @NotNull
+  static IndexChunk mergeUnsafe(@NotNull IndexChunk ch1, @NotNull IndexChunk ch2) {
+    ch1.getRoots().addAll(ch2.getRoots());
+    return ch1;
+  }
+
+  @NotNull
+  private static Stream<IndexChunk> generateIndexChunk(@NotNull Module module) {
+    Stream<IndexChunk> libChunks = Arrays.stream(ModuleRootManager.getInstance(module).getOrderEntries())
+      .map(orderEntry -> {
+        if (orderEntry instanceof LibraryOrSdkOrderEntry) {
+          VirtualFile[] sources = orderEntry.getFiles(OrderRootType.SOURCES);
+          VirtualFile[] classes = orderEntry.getFiles(OrderRootType.CLASSES);
+          String name = null;
+          if (orderEntry instanceof JdkOrderEntry) {
+            name = ((JdkOrderEntry)orderEntry).getJdkName();
+          }
+          else if (orderEntry instanceof LibraryOrderEntry) {
+            name = ((LibraryOrderEntry)orderEntry).getLibraryName();
+          }
+          if (name == null) {
+            name = "unknown";
+          }
+          return new IndexChunk(ContainerUtil.union(Arrays.asList(sources), Arrays.asList(classes)), reducePath(splitByDots(name)));
+        }
+        return null;
+      })
+      .filter(Objects::nonNull);
+
+    Set<VirtualFile> roots =
+      ContainerUtil.union(ContainerUtil.newTroveSet(ModuleRootManager.getInstance(module).getContentRoots()),
+                          ContainerUtil.newTroveSet(ModuleRootManager.getInstance(module).getSourceRoots()));
+    Stream<IndexChunk> srcChunks = Stream.of(new IndexChunk(roots, getChunkName(module)));
+
+    return Stream.concat(libChunks, srcChunks);
+  }
+
+  @NotNull
+  private static String getChunkName(@NotNull Module module) {
+    ModuleManager moduleManager = ModuleManager.getInstance(module.getProject());
+    String[] path;
+    if (moduleManager.hasModuleGroups()) {
+      path = moduleManager.getModuleGroupPath(module);
+      assert path != null;
+    } else {
+      path = splitByDots(module.getName());
+    }
+    return reducePath(path);
+  }
+
+  @NotNull
+  private static String reducePath(String[] path) {
+    String[] reducedPath = Arrays.copyOfRange(path, 0, Math.min(1, path.length));
+    return StringUtil.join(reducedPath, ".");
+  }
+
+  private static String[] splitByDots(String name) {
+    return name.split("[-|:.]");
   }
 }
