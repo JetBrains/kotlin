5918bbde21c61779c0876bc9254983546f0dcc7f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-01-23 23:55:04 +0300

    Wizard: change new wizard title

--- idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/NewProjectWizardModuleType.kt
+++ idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/NewProjectWizardModuleType.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.idea.KotlinIcons
 import javax.swing.Icon
 
 class NewProjectWizardModuleType : ModuleType<NewProjectWizardModuleBuilder>(NewProjectWizardModuleBuilder.MODULE_BUILDER_ID) {
-    override fun getName(): String = "Kotlin (Experimental)"
+    override fun getName(): String = "Experimental Project Generator"
     override fun getDescription(): String = name
     override fun getNodeIcon(isOpened: Boolean): Icon = KotlinIcons.SMALL_LOGO
     override fun createModuleBuilder(): NewProjectWizardModuleBuilder = NewProjectWizardModuleBuilder()
