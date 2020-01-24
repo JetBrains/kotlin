7420e95a01b615f64e990dbbd4f2ce89b3426bcc
Egor Zhdan <egor.zhdan@jetbrains.com>
2020-01-24 15:17:40 +0300

    Mobile: fix bundle location

--- ide/mobile-native/src/com/jetbrains/mobile/MobileBundle.kt
+++ ide/mobile-native/src/com/jetbrains/mobile/MobileBundle.kt
@@ -8,7 +8,7 @@ package com.jetbrains.mobile
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.PropertyKey
 
-private const val BUNDLE_PATH = "MobileBundle"
+private const val BUNDLE_PATH = "messages.MobileBundle"
 
 object MobileBundle : AbstractBundle(BUNDLE_PATH) {
     fun message(@PropertyKey(resourceBundle = BUNDLE_PATH) key: String, vararg params: Any): String {
