1fae69264ced6a7e635a78349f2dea2ad7220e91
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-01-24 13:12:51 +0300

    build.gradle.kts: change notification text

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/GradleScriptOutOfSourceNotificationProvider.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/GradleScriptOutOfSourceNotificationProvider.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.idea.scripting.gradle
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
@@ -24,10 +25,14 @@ class GradleScriptOutOfSourceNotificationProvider(private val project: Project) 
         if (isInAffectedGradleProjectFiles(project, file.path)) return null
 
         return EditorNotificationPanel().apply {
-            text("This script isn't related to any Gradle Project. To update its highlighting, please, load its script configuration.")
-            createActionLabel("Load configuration") {
+            text("The associated Gradle Project isn't imported.")
+            createActionLabel("Load script configuration") {
                 ScriptConfigurationManager.getInstance(project).forceReloadConfiguration(file, loaderForOutOfProjectScripts)
             }
+            val link = createActionLabel("") {}
+            link.setIcon(AllIcons.General.ContextHelp)
+            link.setUseIconAsLink(true)
+            link.toolTipText = "Script configuration is used by IDE in highlighting, completion and other functionalities"
         }
     }
 
