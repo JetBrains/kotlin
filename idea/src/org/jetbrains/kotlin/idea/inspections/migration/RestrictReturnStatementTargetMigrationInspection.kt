2441ee80e49e13593b0bca533ce412e3f5b3e7ad
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-01-27 23:39:57 +0700

    Create `RestrictReturnStatementTargetMigrationInspection`
    #KT-36262

---
+++ idea/src/org/jetbrains/kotlin/idea/inspections/migration/RestrictReturnStatementTargetMigrationInspection.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.inspections.migration
+
+import com.intellij.codeInspection.CleanupLocalInspectionTool
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.diagnostics.Errors
+import org.jetbrains.kotlin.idea.configuration.MigrationInfo
+import org.jetbrains.kotlin.idea.configuration.isLanguageVersionUpdate
+import org.jetbrains.kotlin.idea.quickfix.migration.MigrationFix
+import org.jetbrains.kotlin.psi.KtExpressionWithLabel
+
+
+class RestrictReturnStatementTargetMigrationInspection :
+    AbstractDiagnosticBasedMigrationInspection<KtExpressionWithLabel>(Errors.NOT_A_FUNCTION_LABEL, KtExpressionWithLabel::class.java),
+    MigrationFix,
+    CleanupLocalInspectionTool {
+    override fun isApplicable(migrationInfo: MigrationInfo): Boolean {
+        return migrationInfo.isLanguageVersionUpdate(LanguageVersion.KOTLIN_1_3, LanguageVersion.KOTLIN_1_4)
+    }
+}
+
