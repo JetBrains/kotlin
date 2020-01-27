e7057cf2b33a71972eddae9d7b4b0f22938d012c
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2020-01-27 18:59:33 +0300

    Fix classpath of IDEA plugin tests

--- idea/idea-gradle/build.gradle.kts
+++ idea/idea-gradle/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     testRuntime(project(":noarg-ide-plugin"))
     testRuntime(project(":kotlin-scripting-idea"))
     testRuntime(project(":kotlinx-serialization-ide-plugin"))
+    testRuntime(project(":kotlin-gradle-statistics"))
     // TODO: the order of the plugins matters here, consider avoiding order-dependency
     testRuntime(intellijPluginDep("junit"))
     testRuntime(intellijPluginDep("testng"))
