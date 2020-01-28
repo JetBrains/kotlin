5f2885ace28f3875b724ce3dc585186d9ad3636d
Roman Vasiliev <Roman.Vasiliev@jetbrains.com>
2020-01-28 15:22:18 +0300

    [ml-completion] add lines diff feature for completion elements
    intellij-monorepo: 881459e0e738999749f88a32ad2f1f629e6bcae5

--- intellij-community/plugins/stats-collector/src/com/intellij/completion/ml/common/LocationFeaturesUtil.kt
+++ intellij-community/plugins/stats-collector/src/com/intellij/completion/ml/common/LocationFeaturesUtil.kt
@@ -1,7 +1,14 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.completion.ml.common
 
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionUtilCore.DUMMY_IDENTIFIER
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.psi.PsiElement
+
 object LocationFeaturesUtil {
+  private val LOG = Logger.getInstance(LocationFeaturesUtil.javaClass)
+
   fun indentLevel(line: String, tabSize: Int): Int {
     if (tabSize <= 0) return 0
 
@@ -26,5 +33,39 @@ object LocationFeaturesUtil {
     }
 
     return indentLevel
+  }
+
+  fun linesDiff(completionParameters: CompletionParameters, completionElement: PsiElement?): Int {
+
+    if (completionElement == null) {
+      return Int.MIN_VALUE
+    }
+
+    fun line(element: PsiElement?): Int? {
+      if (element == null) {
+        return null
+      }
+
+      val offset =
+        if (completionParameters.position.containingFile == element.containingFile) {
+          if (element.textOffset <= completionParameters.position.textOffset) element.textOffset
+          else element.textOffset - DUMMY_IDENTIFIER.length
+        }
+        else if (completionParameters.originalFile == element.containingFile) element.textOffset
+        else {
+          return null
+        }
+
+      return completionParameters.editor.document.getLineNumber(offset)
+    }
+
+    try {
+      val positionLine = line(completionParameters.position) ?: return Int.MIN_VALUE
+      val elementLine = line(completionElement) ?: return Int.MIN_VALUE
+      return positionLine - elementLine
+    } catch (e: Throwable) {
+      LOG.error("Error while calculating lines diff")
+      return Int.MIN_VALUE
+    }
   }
 }
