b6672c20fc6c4bef806eb70b72e7f711b6d0c831
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2020-01-28 22:32:31 +0300

    POM: support @Presentation.provider for PomTarget
    intellij-monorepo: bf7db1312bf17b4b36790e4fac3a170e101455e8

--- intellij-community/platform/lang-impl/src/com/intellij/psi/impl/DefaultPomTargetDescriptionProvider.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/impl/DefaultPomTargetDescriptionProvider.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.impl;
 
+import com.intellij.codeInsight.highlighting.HighlightUsagesDescriptionLocation;
 import com.intellij.ide.TypePresentationService;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.pom.PomDescriptionProvider;
@@ -24,7 +11,6 @@ import com.intellij.psi.ElementDescriptionLocation;
 import com.intellij.psi.PsiElement;
 import com.intellij.usageView.UsageViewNodeTextLocation;
 import com.intellij.usageView.UsageViewTypeLocation;
-import com.intellij.codeInsight.highlighting.HighlightUsagesDescriptionLocation;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -48,8 +34,11 @@ public class DefaultPomTargetDescriptionProvider extends PomDescriptionProvider 
   }
 
   private static String getTypeName(PomTarget element) {
+    TypePresentationService presentationService = TypePresentationService.getService();
+    String elementTypeName = presentationService.getTypeName(element);
+    if (elementTypeName != null) return elementTypeName;
 
-    final String s = TypePresentationService.getService().getTypePresentableName(element.getClass());
-    return s == null ? "Element" : s;
+    String classTypeName = presentationService.getTypePresentableName(element.getClass());
+    return classTypeName == null ? "Element" : classTypeName;
   }
 }
