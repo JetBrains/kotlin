a342acdd061f85dd92358e2fa28303a5662b7d58
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2020-01-29 14:43:25 +0300

    Use more robust way to find bundled binaries
    Previously, we've expected 'PathManager.getPluginsPath()' to return path
    to folder with IDE plugins installed. That's actually might not be the
    case, as there are some environment variables (like 'plugin.path') which
    might override installation folder of plugins without affecting
    PathManager.
    
    So far, the only known such case is running a debug IDE in Kotlin build.
    
    Still, we've decided not to rely on invariants about locations of IDEA
    plugins, and, instead, realy on the layout of our plugin: now we look
    for mobile-jar plugin, and look for bundled binaries relative from that.
    This should be more robust because we have control over this layout
    during plugin build.

--- ide/android-studio-native/src/com/jetbrains/mpp/PathManagerCustomizer.kt
+++ ide/android-studio-native/src/com/jetbrains/mpp/PathManagerCustomizer.kt
@@ -5,14 +5,35 @@
 
 package com.jetbrains.mpp
 
-import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.util.io.FileUtil
 import com.jetbrains.cidr.OCPathManagerCustomization
+import org.jetbrains.kotlin.utils.PathUtil
 import java.io.File
 
 class PathManagerCustomization : OCPathManagerCustomization() {
-    override fun getBinFile(relativePath: String): File =
-        File(FileUtil.join(PathManager.getPluginsPath(), "mobile-mpp", "bin", relativePath)).also {
+    override fun getBinFile(relativePath: String): File {
+        /*
+         Expected layout:
+         <folder-with-installed-idea-plugins>
+            |- Kotlin                  (Kotlin plugin installation folder)
+            |- mobile-mpp              (Mobile plugin installation folder)
+            |  |- lib
+            |  |  |- mobile-mpp.jar    (jar with all native-plugin classes)
+            |  |  |- ... other jars needed for native-plugin ...
+            |  |- native
+            |     |- Bridge.framework
+            |     |- JBDevice.framework
+            |     |- ... other binaries needed for native-plugin ...
+            |- ... other plugins installation folders ...
+
+         */
+        val mobileMppPluginJar = PathUtil.getResourcePathForClass(this::class.java).takeIf { it.name == "mobile-mpp.jar" }!!
+        val libFolder = mobileMppPluginJar.parentFile
+        val mobileMppPluginInstallationFolder = libFolder.parentFile
+        val nativeFolder = File(mobileMppPluginInstallationFolder, "bin")
+
+        return File(nativeFolder, relativePath).also {
             FileUtil.setExecutable(it) // FIXME omit when CLion build script is corrected
         }
+    }
 }
