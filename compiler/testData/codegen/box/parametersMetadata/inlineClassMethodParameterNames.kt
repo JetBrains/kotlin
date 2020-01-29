66dd5aa32bd92a1e1519cae4872f9e0f76d7e345
Jinseong Jeon <jsjeon@google.com>
2020-01-28 22:06:04 -0800

    JVM_IR: Change name for (dispatch|extension) receiver of inline class according to naming convention.

--- compiler/testData/codegen/box/parametersMetadata/inlineClassMethodParameterNames.kt
+++ compiler/testData/codegen/box/parametersMetadata/inlineClassMethodParameterNames.kt
@@ -1,5 +1,4 @@
 // IGNORE_BACKEND_FIR: JVM_IR
-// IGNORE_BACKEND: JVM_IR
 // SKIP_JDK6
 // TARGET_BACKEND: JVM
 // WITH_RUNTIME
@@ -9,19 +8,20 @@
 // FILE: A.kt
 
 inline class A(val i: Int) {
-    fun f() = i
+    fun foo(v: Int) = i + v
 }
 
-fun A.extension() = this.i
+fun A.bar() = this.i
 
 fun box(): String {
-    val method = Class.forName("A").declaredMethods.single { it.name == "f-impl" }
+    val method = Class.forName("A").declaredMethods.single { it.name == "foo-impl" }
     val parameters = method.getParameters()
     if (parameters[0].name != "arg0") return "wrong name on receiver parameter: ${parameters[0].name}"
+    if (parameters[1].name != "v") return "wrong name on actual parameter: ${parameters[1].name}"
 
-    val extensionMethod = Class.forName("AKt").declaredMethods.single { it.name.contains("extension") }
+    val extensionMethod = Class.forName("AKt").declaredMethods.single { it.name.startsWith("bar") }
     val extensionMethodParameters = extensionMethod.getParameters()
-    if (extensionMethodParameters[0].name != "\$this\$extension")
+    if (extensionMethodParameters[0].name != "\$this\$bar")
         return "wrong name on extension receiver parameter: ${extensionMethodParameters[0].name}"
 
     return "OK"
