a85bf0c5e422c763089ef3a1a2a7aa55db29abda
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-01-30 11:09:21 +0700

    [Interop][Test] Add test for allocation of vector variable

--- backend.native/tests/interop/basics/types.kt
+++ backend.native/tests/interop/basics/types.kt
@@ -29,5 +29,12 @@ fun main() {
 
     assertEquals(49, sendV4I(vectorOf(1, 2, 3, 4)))
     assertEquals(49, (sendV4F(vectorOf(1f, 2f, 3f, 4f)) + 0.00001).toInt())
+
+    memScoped {
+        val vector = alloc<KVector4i32Var>().also {
+            it.value = vectorOf(1, 2, 3, 4)
+        }
+        assertEquals(vector.value, vectorOf(1, 2, 3, 4))
+    }
 }
 
