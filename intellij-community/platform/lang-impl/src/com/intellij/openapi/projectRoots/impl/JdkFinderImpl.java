2fa8dda0d664821c4a3e3af287d51a98cd3277df
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-01-31 11:22:32 +0100

    Make SimpleJavaSdkType support JDK finder
    Move JavaHomeFinder to lang-impl, wrap it as Application service (to avoid moving to the lang-api module)
    
    intellij-monorepo: 2252622d14bb6289ca3e0b00dce3231c763b8615

---
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/JdkFinderImpl.java
@@ -0,0 +1,15 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.projectRoots.impl;
+
+import com.intellij.openapi.projectRoots.JdkFinder;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class JdkFinderImpl implements JdkFinder {
+  @NotNull
+  @Override
+  public List<String> suggestHomePaths() {
+    return JavaHomeFinder.suggestHomePaths(false);
+  }
+}
