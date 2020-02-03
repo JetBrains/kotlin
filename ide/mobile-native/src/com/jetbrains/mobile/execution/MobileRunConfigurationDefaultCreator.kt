cbb11e39e7295638ac61566e6bfea6218fb2b010
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2020-02-03 20:03:50 +0100

    Create default iOS configurations only on a Mac

--- ide/mobile-native/src/com/jetbrains/mobile/execution/MobileRunConfigurationDefaultCreator.kt
+++ ide/mobile-native/src/com/jetbrains/mobile/execution/MobileRunConfigurationDefaultCreator.kt
@@ -8,6 +8,7 @@ package com.jetbrains.mobile.execution
 import com.intellij.execution.RunManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
 import com.jetbrains.mobile.MobileBundle
 import com.jetbrains.mobile.isAndroid
 import com.jetbrains.mobile.isApple
@@ -19,6 +20,7 @@ fun MobileRunConfiguration.Companion.createDefaults(project: Project, modules: L
         val factory = type.factory
         val configuration = factory.createTemplateConfiguration(project, runManager) as MobileRunConfiguration
         configuration.module = module
+        if (module.isApple && !SystemInfo.isMac) continue
         val name = when {
             module.isApple -> MobileBundle.message("run.configuration.name.apple")
             module.isAndroid -> MobileBundle.message("run.configuration.name.android")
