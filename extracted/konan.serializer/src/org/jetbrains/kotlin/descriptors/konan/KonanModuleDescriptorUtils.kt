1bd6e0a7eb87cc586fbac8da9cc00578de7ceda1
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2020-02-04 17:48:03 +0100

    [serialization][REVERT] workaround of KONAN_STDLIB_NAME symbol ambugity (need to be investigated and fixed)

--- extracted/konan.serializer/src/org/jetbrains/kotlin/descriptors/konan/KonanModuleDescriptorUtils.kt
+++ extracted/konan.serializer/src/org/jetbrains/kotlin/descriptors/konan/KonanModuleDescriptorUtils.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.konan.library.KONAN_STDLIB_NAME
 import org.jetbrains.kotlin.name.Name
 
-private val STDLIB_MODULE_NAME = Name.special("<$KONAN_STDLIB_NAME>")
+//TODO: this is should be reverted!!!
+private val STDLIB_MODULE_NAME = Name.special("<stdlib>")
 
 fun ModuleDescriptor.isKonanStdlib() = name == STDLIB_MODULE_NAME
