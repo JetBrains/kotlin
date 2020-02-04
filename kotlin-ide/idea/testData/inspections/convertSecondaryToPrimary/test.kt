1a255bffa6eb03e0a3d1e67b9ddc087b63c462b1
Toshiaki Kameyama <kmymtsak@gmail.com>
2020-02-04 18:54:25 +0900

    "Convert to primary constructor" inspection: do not report when property has setter
    #KT-22142 Fixed
    
    Original commit: 64f6ed586b6e8e836c98969a84d917e9ba576df7

--- kotlin-ide/idea/testData/inspections/convertSecondaryToPrimary/test.kt
+++ kotlin-ide/idea/testData/inspections/convertSecondaryToPrimary/test.kt
@@ -17,3 +17,16 @@ class NotSingle {
         this.x = x
     }
 }
+
+// KT-22142
+class Person {
+    constructor(email: String) {
+        this.email = email
+    }
+
+    var email: String
+        set(value) {
+            require(value.isNotBlank(), { "The email cannot be blank" })
+            field = value
+        }
+}
