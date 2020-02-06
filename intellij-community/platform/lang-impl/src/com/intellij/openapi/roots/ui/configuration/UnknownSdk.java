c8bd27a136c1f8e561e2dc6ae1fdb92b49f7f380
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-02-06 16:15:36 +0100

    remove dangling import
    intellij-monorepo: c8aaaf655cc8c867934ef3adb359fab3689eda76

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/UnknownSdk.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/roots/ui/configuration/UnknownSdk.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.roots.ui.configuration;
 
-import com.google.common.base.Predicates;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkType;
 import org.jetbrains.annotations.NotNull;
