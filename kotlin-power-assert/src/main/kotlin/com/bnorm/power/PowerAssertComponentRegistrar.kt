54202a7818c693930319bc3cfe44f2b781af514a
Jake Wharton <jakew@google.com>
2020-02-05 23:28:47 -0500

    Write service info from annotation processor

--- kotlin-power-assert/src/main/kotlin/com/bnorm/power/PowerAssertComponentRegistrar.kt
+++ kotlin-power-assert/src/main/kotlin/com/bnorm/power/PowerAssertComponentRegistrar.kt
@@ -16,11 +16,13 @@
 
 package com.bnorm.power
 
+import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 
+@AutoService(ComponentRegistrar::class)
 class PowerAssertComponentRegistrar : ComponentRegistrar {
   override fun registerProjectComponents(
     project: MockProject,
