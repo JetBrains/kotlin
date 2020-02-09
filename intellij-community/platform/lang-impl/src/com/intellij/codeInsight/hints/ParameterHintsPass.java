10714064106b04887756fe578efbd19ea9473246
Max Medvedev <maxim.medvedev@jetbrains.com>
2020-02-09 10:32:56 +0300

    Inlay hints: add a couple of not-nulls
    intellij-monorepo: 767cb371f54737b22c9f8e91ca8533ee7e11ed6f

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsPass.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsPass.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.hints;
 
 import com.intellij.codeHighlighting.EditorBoundHighlightingPass;
@@ -71,7 +71,7 @@ public class ParameterHintsPass extends EditorBoundHighlightingPass {
     return HintUtilsKt.isParameterHintsEnabledForLanguage(language);
   }
 
-  private void process(PsiElement element, InlayParameterHintsProvider provider) {
+  private void process(@NotNull PsiElement element, @NotNull InlayParameterHintsProvider provider) {
     List<InlayInfo> hints = provider.getParameterHints(element);
     if (hints.isEmpty()) return;
     HintInfo info = provider.getHintInfo(element);
