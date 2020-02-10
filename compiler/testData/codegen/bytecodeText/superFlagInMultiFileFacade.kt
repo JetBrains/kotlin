6d23e50142df38fd1596fa68771457eb58e86933
Steven Schäfer <stevenschaefer@google.com>
2020-02-10 11:57:17 +0100

    JVM IR: Create non-synthetic multifile parts for -Xmultifile-parts-inherit

--- compiler/testData/codegen/bytecodeText/superFlagInMultiFileFacade.kt
+++ compiler/testData/codegen/bytecodeText/superFlagInMultiFileFacade.kt
@@ -6,8 +6,10 @@ fun foo() {
 }
 
 
-/*ACC_PUBLIC ACC_FINAL ACC_SUPER */
+/* ACC_PUBLIC ACC_FINAL ACC_SUPER */
 // 1 access flags 0x31
+// 1 public final class Test
 
-/*ACC_SYNTHETIC ACC_FINAL ACC_SUPER */
+/* ACC_SYNTHETIC ACC_FINAL ACC_SUPER */
 // 1 access flags 0x1030
+// 1 final synthetic class Test__SuperFlagInMultiFileFacadeKt
