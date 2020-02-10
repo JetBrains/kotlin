ae578e0a5d7823e7b8399e90a855cedfa06e4331
Valentin Kipyatkov <valentin@jetbrains.com>
2020-02-10 16:56:18 +0200

    Copyright comments

--- idea/src/org/jetbrains/kotlin/idea/slicer/OutflowSlicer.kt
+++ idea/src/org/jetbrains/kotlin/idea/slicer/OutflowSlicer.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlin.idea.slicer
 
 import com.intellij.codeInsight.highlighting.ReadWriteAccessDetector
