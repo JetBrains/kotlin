b8aacf078635e095e686112a79b3e3592f268b75
victor.petukhov <victor.petukhov@jetbrains.com>
2020-02-10 19:23:35 +0300

    Fix test data around flexibility after ae39d748e4d4215148ce2c46a62579464131824f

---
+++ compiler/testData/diagnostics/tests/platformTypes/methodCall/flexibilityThroughTypeVariable.fir.kt
@@ -0,0 +1,18 @@
+// !LANGUAGE: +NewInference
+// !DIAGNOSTICS: -UNUSED_PARAMETER
+
+// Issue: KT-36254
+
+// FILE: Convertor.java
+
+public interface Convertor<Src, Dst> {
+    Dst convert(Src o);
+}
+
+// FILE: main.kt
+
+fun takeConvertor(c: Convertor<String, String>) {}
+
+fun main() {
+    takeConvertor(Convertor { null })
+}
