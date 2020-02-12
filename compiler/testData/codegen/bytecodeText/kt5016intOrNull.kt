3ecab505ff12838c446c2f78cd4b2a5b04980753
Mark Punzalan <markpun@google.com>
2020-02-12 11:26:59 -0800

    [JVM IR] Use `append(Char)` for 1-length string literals in string templates and plus concatenations.
    This is slightly more efficient and mirrors the behavior of the non-IR
    backend for templates (but not for plus concatenations).
    
    #KT-36638 Fixed

--- compiler/testData/codegen/bytecodeText/kt5016intOrNull.kt
+++ compiler/testData/codegen/bytecodeText/kt5016intOrNull.kt
@@ -1,6 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
-// TODO KT-36638 Use 'java/lang/StringBuilder.append (C)Ljava/lang/StringBuilder;' when appending single character in JVM_IR
-
 // KT-5016 wrong StringBuilder append method invoked
 class kt5016intOrNull {
     fun f1(num : Int?) : String {
@@ -10,4 +7,6 @@ class kt5016intOrNull {
 
 // 1 INVOKEVIRTUAL java/lang/StringBuilder.append \(Ljava/lang/Object;\)Ljava/lang/StringBuilder
 // 1 INVOKEVIRTUAL java/lang/StringBuilder.append \(Ljava/lang/String;\)Ljava/lang/StringBuilder
+// 1 INVOKEVIRTUAL java/lang/StringBuilder.append \(C\)Ljava/lang/StringBuilder
+// 3 INVOKEVIRTUAL java/lang/StringBuilder.append
 // 1 INVOKEVIRTUAL java/lang/StringBuilder.toString
