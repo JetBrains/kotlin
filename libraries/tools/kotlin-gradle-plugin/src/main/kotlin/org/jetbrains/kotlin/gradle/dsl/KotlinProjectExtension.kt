f0dc8092559bade1e075275e0b47670ed7fbab64
Leonid Startsev <leonid.startsev@jetbrains.com>
2020-02-13 13:34:19 +0300

    Add explicit api mode flag to top-level kotlin {} DSL block in Gradle
    for both MPP and single-target projects.
    
    For now, it is only applied to source sets that are connected to 'main'
    compilation. It seems fine for all projects except android ones.
    There's a way to determine android variant for source set,
    but importing android projects seems to be made via other code path.
    
     #KT-36019 Fixed
     #KT-37652 Open

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinProjectExtension.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinProjectExtension.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.gradle.dsl
@@ -56,6 +45,12 @@ internal val Project.multiplatformExtension: KotlinMultiplatformExtension
 open class KotlinProjectExtension : KotlinSourceSetContainer {
     val experimental: ExperimentalExtension
         get() = DslObject(this).extensions.getByType(ExperimentalExtension::class.java)
+
+    var explicitApi: ExplicitApiMode? = null
+
+    fun explicitApi() {
+        explicitApi = ExplicitApiMode.Strict
+    }
 
     override var sourceSets: NamedDomainObjectContainer<KotlinSourceSet>
         @Suppress("UNCHECKED_CAST")
@@ -228,4 +223,12 @@ enum class NativeCacheKind(val produce: String?, val outputKind: CompilerOutputK
         fun byCompilerArgument(argument: String): NativeCacheKind? =
             NativeCacheKind.values().firstOrNull { it.name.equals(argument, ignoreCase = true) }
     }
+}
+
+enum class ExplicitApiMode(private val cliOption: String) {
+    Strict("strict"),
+    Warning("warning"),
+    Disabled("disabled");
+
+    fun toCompilerArg() = "-Xexplicit-api=$cliOption"
 }
--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package org.jetbrains.kotlin.gradle.plugin
 
 import com.android.build.gradle.*
@@ -498,6 +503,7 @@ internal abstract class AbstractKotlinPlugin(
         fun configureProjectGlobalSettings(project: Project, kotlinPluginVersion: String) {
             configureDefaultVersionsResolutionStrategy(project, kotlinPluginVersion)
             configureClassInspectionForIC(project)
+            project.setupGeneralKotlinExtensionParameters()
         }
 
         fun configureTarget(
