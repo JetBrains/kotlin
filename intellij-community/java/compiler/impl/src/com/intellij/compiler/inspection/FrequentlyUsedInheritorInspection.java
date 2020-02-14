627501028b0a42d85a5643908c3d289291fb43e5
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-14 17:19:35 +0100

    do not export StreamEx lib from platform-impl
    intellij-monorepo: b4b5c70dc99510609083c3ff097936be94e0548c

--- intellij-community/java/compiler/impl/src/com/intellij/compiler/inspection/FrequentlyUsedInheritorInspection.java
+++ intellij-community/java/compiler/impl/src/com/intellij/compiler/inspection/FrequentlyUsedInheritorInspection.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.compiler.inspection;
 
 import com.intellij.codeInspection.*;
@@ -27,7 +27,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class FrequentlyUsedInheritorInspection extends AbstractBaseJavaLocalInspectionTool {
+public final class FrequentlyUsedInheritorInspection extends AbstractBaseJavaLocalInspectionTool {
   private static final Logger LOG = Logger.getInstance(FrequentlyUsedInheritorInspection.class);
 
   public static final byte MAX_RESULT = 3;
