801d697e57ad467090a5dd9750d9e31463ae244a
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-02-14 15:14:41 +0300

    [Gradle, JS] Add all possible values of library target as enums
    (cherry picked from commit 9b462b76d4e7c46585ce5318b86bd5c80a4fbb6f)

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/webpack/KotlinWebpackOutput.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/webpack/KotlinWebpackOutput.kt
@@ -6,12 +6,47 @@
 package org.jetbrains.kotlin.gradle.targets.js.webpack
 
 import org.gradle.api.tasks.Input
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackLibraryTarget.Companion.fromString
 
 data class KotlinWebpackOutput(
     @Input var library: String?,
-    @Input var libraryTarget: String?
+    @Input var libraryTarget: KotlinWebpackLibraryTarget?
 ) {
+    fun getLibraryTarget(): String {
+        return this.libraryTarget.toString()
+    }
+
+    fun setLibraryTarget(value: String) {
+        this.libraryTarget = fromString(value)
+    }
+}
+
+enum class KotlinWebpackLibraryTarget {
+    VAR,
+    ASSIGN,
+    THIS,
+    WINDOW,
+    SELF,
+    GLOBAL,
+    COMMONJS,
+    COMMONJS2,
+    COMMONJS_MODULE,
+    AMD,
+    AMD_REQUIRE,
+    UMD,
+    UMD2,
+    JSONP,
+    SYSTEM;
+
+    override fun toString(): String {
+        return super.toString()
+            .toLowerCase()
+            .replace("_", "-")
+    }
+
     companion object {
-        const val UMD = "umd"
+        internal fun fromString(value: String): KotlinWebpackLibraryTarget {
+            return valueOf(value.toUpperCase().replace("-", "_"))
+        }
     }
 }
