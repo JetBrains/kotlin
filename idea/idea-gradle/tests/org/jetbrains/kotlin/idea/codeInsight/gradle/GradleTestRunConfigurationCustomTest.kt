b360fb51192cc524ae32ec23e62af5c8e56cbe58
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-02-14 17:49:59 +0900

    Revert "Import Kotlin JUnit run configuration settings from Gradle"

--- idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleTestRunConfigurationCustomTest.kt
+++ idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleTestRunConfigurationCustomTest.kt
@@ -5,18 +5,14 @@
 
 package org.jetbrains.kotlin.idea.codeInsight.gradle
 
-import com.intellij.execution.RunManagerEx
-import com.intellij.execution.configurations.ConfigurationType
-import com.intellij.execution.configurations.ConfigurationTypeUtil
-import com.intellij.execution.junit.JUnitConfiguration
-import com.intellij.execution.junit.JUnitConfigurationType
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.runInEdtAndWait
 import com.intellij.util.PlatformUtils
 import org.jetbrains.kotlin.gradle.textWithoutTags
-import org.jetbrains.kotlin.idea.run.*
+import org.jetbrains.kotlin.idea.run.KotlinJvmTestClassGradleConfigurationProducer
+import org.jetbrains.kotlin.idea.run.KotlinJvmTestMethodGradleConfigurationProducer
+import org.jetbrains.kotlin.idea.run.getConfiguration
 import org.jetbrains.kotlin.idea.util.application.runReadAction
-import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.jetbrains.plugins.gradle.execution.test.runner.TestClassGradleConfigurationProducer
 import org.jetbrains.plugins.gradle.execution.test.runner.TestMethodGradleConfigurationProducer
 import org.jetbrains.plugins.gradle.tooling.annotation.TargetVersions
@@ -52,37 +48,6 @@ class GradleTestRunConfigurationCustomTest : GradleImportingTestCase() {
                 val kotlinFunctionConfiguration = getConfiguration(kotlinFile, myProject, "testA")
                 kotlinFunctionConfiguration.isProducedBy(KotlinJvmTestMethodGradleConfigurationProducer::class.java)
                 assert(kotlinFunctionConfiguration.configuration is KotlinGradleRunConfiguration)
-            }
-        }
-    }
-
-    @Test
-    @TargetVersions("4.7+")
-    fun testKotlinJUnitSettings() {
-        if (!PlatformUtils.isIntelliJ()) {
-            return
-        }
-
-        importProjectFromTestData()
-
-        runInEdtAndWait {
-            runReadAction {
-                fun getConfiguration(cls: Class<out ConfigurationType>): JUnitConfiguration {
-                    val configurationFactory = ConfigurationTypeUtil
-                        .findConfigurationType(cls).configurationFactories[0]
-                    val runManager = RunManagerEx.getInstanceEx(myProject)
-                    val kotlinRunnerAndConfigurationSettings = runManager.getConfigurationTemplate(configurationFactory)
-                    return kotlinRunnerAndConfigurationSettings.configuration as JUnitConfiguration
-                }
-
-                val javaConfiguration = getConfiguration(JUnitConfigurationType::class.java)
-                val kotlinConfiguration = getConfiguration(KotlinJUnitConfigurationType::class.java)
-
-                assert(javaConfiguration !== kotlinConfiguration)
-                assert(javaConfiguration.javaClass != kotlinConfiguration.javaClass)
-
-                assertEquals("-Dfoo=bar", javaConfiguration.persistentData.vmParameters)
-                assertEquals("-Dfoo=bar", kotlinConfiguration.persistentData.vmParameters)
             }
         }
     }
