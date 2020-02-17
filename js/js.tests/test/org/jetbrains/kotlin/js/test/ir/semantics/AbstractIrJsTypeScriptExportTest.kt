a01e66a61873875f40261fec72f9bdcbfe0a0ece
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2020-02-17 17:48:13 +0300

    Run IrJsTypeScriptExportTestGenerated tests with `jsIrTest` instead of `jsTest`

--- js/js.tests/test/org/jetbrains/kotlin/js/test/ir/semantics/AbstractIrJsTypeScriptExportTest.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/ir/semantics/AbstractIrJsTypeScriptExportTest.kt
@@ -1,10 +1,11 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.js.test
+package org.jetbrains.kotlin.js.test.ir.semantics
 
+import org.jetbrains.kotlin.js.test.BasicIrBoxTest
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.jetbrains.kotlin.utils.fileUtils.withReplacedExtensionOrNull
 import java.io.File
