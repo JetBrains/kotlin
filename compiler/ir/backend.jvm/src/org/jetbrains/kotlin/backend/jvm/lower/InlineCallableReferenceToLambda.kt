8c9ebc1bf902ee82ea0c2c0256d2052330f86e67
Steven Schäfer <stevenschaefer@google.com>
2020-02-17 15:56:51 +0100

    JVM IR: Initialize parent field in InlineCallableReferenceToLambda

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InlineCallableReferenceToLambda.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/InlineCallableReferenceToLambda.kt
@@ -84,6 +84,7 @@ internal class InlineCallableReferenceToLambdaPhase(val context: JvmBackendConte
                 returnType = field.type
                 isSuspend = false
             }.apply {
+                parent = currentDeclarationParent
                 val boundReceiver = expression.dispatchReceiver ?: expression.extensionReceiver
 
                 val receiver =
@@ -140,6 +141,7 @@ internal class InlineCallableReferenceToLambdaPhase(val context: JvmBackendConte
                 returnType = referencedFunction.returnType
                 isSuspend = referencedFunction.isSuspend
             }.apply {
+                parent = currentDeclarationParent
                 for ((index, argumentType) in argumentTypes.withIndex()) {
                     addValueParameter {
                         name = Name.identifier("p$index")
