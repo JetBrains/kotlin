50c6bb35aaa846967f526710b929c39c51aaf952
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-02-17 17:12:25 +0300

    make UsagesPreviewPanelProvider depend on UsageView instead of UsageViewImpl
    intellij-monorepo: 8689d6db56557f6937ef089a8e3249f63db54972

--- intellij-community/platform/lang-impl/src/com/intellij/find/UsagesPreviewPanelProvider.java
+++ intellij-community/platform/lang-impl/src/com/intellij/find/UsagesPreviewPanelProvider.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.find;
 
 import com.intellij.icons.AllIcons;
@@ -21,7 +7,7 @@ import com.intellij.openapi.preview.PreviewProviderId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
-import com.intellij.usages.impl.UsageViewImpl;
+import com.intellij.usages.UsageView;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,11 +16,11 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class UsagesPreviewPanelProvider extends PreviewPanelProvider<Pair<UsageViewImpl, ? extends JTable>, Boolean> {
-  public static final PreviewProviderId<Pair<UsageViewImpl, ? extends JTable>, Boolean> ID = PreviewProviderId.create("Usages");
+public class UsagesPreviewPanelProvider extends PreviewPanelProvider<Pair<UsageView, ? extends JTable>, Boolean> {
+  public static final PreviewProviderId<Pair<UsageView, ? extends JTable>, Boolean> ID = PreviewProviderId.create("Usages");
 
   private final JComponent myComponent;
-  private final Collection<UsageViewImpl> myViews = new ArrayList<>();
+  private final Collection<UsageView> myViews = new ArrayList<>();
 
   public UsagesPreviewPanelProvider(Project project) {
     super(ID);
@@ -50,7 +36,7 @@ public class UsagesPreviewPanelProvider extends PreviewPanelProvider<Pair<UsageV
   public void dispose() {
     //myPresentation = null;
     myComponent.removeAll();
-    for (UsageViewImpl view : myViews) {
+    for (UsageView view : myViews) {
       Disposer.dispose(view);
     }
   }
@@ -63,13 +49,13 @@ public class UsagesPreviewPanelProvider extends PreviewPanelProvider<Pair<UsageV
 
   @NotNull
   @Override
-  protected String getTitle(@NotNull Pair<UsageViewImpl, ? extends JTable> content) {
+  protected String getTitle(@NotNull Pair<UsageView, ? extends JTable> content) {
     return content.first.getPresentation().getTabText();
   }
 
   @Nullable
   @Override
-  protected Icon getIcon(@NotNull Pair<UsageViewImpl, ? extends JTable> content) {
+  protected Icon getIcon(@NotNull Pair<UsageView, ? extends JTable> content) {
     return AllIcons.Actions.Find;
   }
 
@@ -79,7 +65,7 @@ public class UsagesPreviewPanelProvider extends PreviewPanelProvider<Pair<UsageV
   }
 
   @Override
-  public void showInStandardPlace(@NotNull Pair<UsageViewImpl, ? extends JTable> content) {
+  public void showInStandardPlace(@NotNull Pair<UsageView, ? extends JTable> content) {
   }
 
   @Override
@@ -88,24 +74,24 @@ public class UsagesPreviewPanelProvider extends PreviewPanelProvider<Pair<UsageV
   }
 
   @Override
-  public boolean isModified(Pair<UsageViewImpl, ? extends JTable> content, boolean beforeReuse) {
+  public boolean isModified(Pair<UsageView, ? extends JTable> content, boolean beforeReuse) {
     return beforeReuse;
   }
 
   @Override
-  public void release(@NotNull Pair<UsageViewImpl, ? extends JTable> content) {
+  public void release(@NotNull Pair<UsageView, ? extends JTable> content) {
     myViews.remove(content.first);
     Disposer.dispose(content.first);
     myComponent.remove(content.second);
   }
 
   @Override
-  public boolean contentsAreEqual(@NotNull Pair<UsageViewImpl, ? extends JTable> content1, @NotNull Pair<UsageViewImpl, ? extends JTable> content2) {
+  public boolean contentsAreEqual(@NotNull Pair<UsageView, ? extends JTable> content1, @NotNull Pair<UsageView, ? extends JTable> content2) {
     return content1.getFirst().getPresentation().equals(content2.getFirst().getPresentation());
   }
 
   @Override
-  protected Boolean initComponent(final Pair<UsageViewImpl, ? extends JTable> content, boolean requestFocus) {
+  protected Boolean initComponent(final Pair<UsageView, ? extends JTable> content, boolean requestFocus) {
     myComponent.removeAll();
     myComponent.add(content.second);
     myViews.add(content.first);
