0f07aa3c261058edb06372b5dbdf0e31069193f9
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-02-17 16:01:23 +0900

    i18n: Add bundle for New J2K

---
+++ nj2k/src/org/jetbrains/kotlin/nj2k/KotlinNJ2KBundle.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.nj2k
+
+import com.intellij.CommonBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+import org.jetbrains.kotlin.idea.core.util.KotlinBundleBase
+import java.util.*
+
+object KotlinNJ2KBundle : KotlinBundleBase() {
+    @NonNls
+    private const val BUNDLE = "org.jetbrains.kotlin.nj2k.KotlinNJ2KBundle"
+
+    override fun createBundle(): ResourceBundle = ResourceBundle.getBundle(BUNDLE)
+
+    @JvmStatic
+    fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any?): String {
+        return CommonBundle.message(bundle, key, *params)
+    }
+}
