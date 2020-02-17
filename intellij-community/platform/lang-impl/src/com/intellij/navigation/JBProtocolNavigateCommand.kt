8294c042ac70ead176432bc5dc40b8a59d716ec6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-17 11:51:42 +0100

    fix test JBNavigateCommandTest#pathOpenProject
    intellij-monorepo: 7796ce591007414688c13bd0de24b9be19e86341

--- intellij-community/platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommand.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/navigation/JBProtocolNavigateCommand.kt
@@ -23,6 +23,7 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.progress.util.StatusBarProgress
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupManager
 import com.intellij.openapi.util.io.FileUtil
@@ -34,6 +35,15 @@ import java.nio.file.Paths
 import java.util.regex.Pattern
 
 open class JBProtocolNavigateCommand : JBProtocolCommand(NAVIGATE_COMMAND) {
+  companion object {
+    const val NAVIGATE_COMMAND = "navigate"
+    const val PROJECT_NAME_KEY = "project"
+    const val REFERENCE_TARGET = "reference"
+    const val PATH_KEY = "path"
+    const val FQN_KEY = "fqn"
+    const val SELECTION = "selection"
+  }
+
   override fun perform(target: String, parameters: Map<String, String>) {
     /**
      * The handler parses the following 'navigate' command parameters:
@@ -64,135 +74,133 @@ open class JBProtocolNavigateCommand : JBProtocolCommand(NAVIGATE_COMMAND) {
         continue
       }
 
-      val project = ProjectUtil.getOpenProjects().find { project -> project.name == projectName }
-      if (project != null) {
-        findAndNavigateToReference(project, parameters)
-        return
+      for (project in ProjectUtil.getOpenProjects()) {
+        if (project.name == projectName) {
+          findAndNavigateToReference(project, parameters)
+          return
+        }
       }
 
       ApplicationManager.getApplication().invokeLater(Runnable {
-        RecentProjectsManagerBase.instanceEx.openProject(Paths.get(recentProjectAction.projectPath), OpenProjectTask())?.let {
-          StartupManager.getInstance(it).registerPostStartupActivity { findAndNavigateToReference(it, parameters) }
+        val project = RecentProjectsManagerBase.instanceEx.openProject(Paths.get(recentProjectAction.projectPath), OpenProjectTask()) ?: return@Runnable
+        StartupManager.getInstance(project).runAfterOpened {
+          DumbService.getInstance(project).runWhenSmart {
+            findAndNavigateToReference(project, parameters)
+          }
         }
       }, ModalityState.NON_MODAL)
     }
   }
+}
 
-  companion object {
-    private val LOG = logger<JBProtocolNavigateCommand>()
-    const val NAVIGATE_COMMAND = "navigate"
-    const val PROJECT_NAME_KEY = "project"
-    const val REFERENCE_TARGET = "reference"
-    const val PATH_KEY = "path"
-    const val FQN_KEY = "fqn"
-    const val SELECTION = "selection"
-    const val FILE_PROTOCOL = "file://"
-    private const val PATH_GROUP = "path"
-    private const val LINE_GROUP = "line"
-    private const val COLUMN_GROUP = "column"
-    private val PATH_WITH_LOCATION = Pattern.compile("(?<$PATH_GROUP>[^:]*)(:(?<$LINE_GROUP>[\\d]+))?(:(?<$COLUMN_GROUP>[\\d]+))?")
+private val LOG = logger<JBProtocolNavigateCommand>()
 
-    private fun findAndNavigateToReference(project: Project, parameters: Map<String, String>) {
-      for (it in parameters) {
-        if (it.key.startsWith(FQN_KEY)) {
-          navigateByFqn(project, parameters, it.value)
-        }
-      }
+private const val FILE_PROTOCOL = "file://"
 
-      for (it in parameters) {
-        if (it.key.startsWith(PATH_KEY)) {
-          navigateByPath(project, parameters, it.value)
-        }
-      }
+private const val PATH_GROUP = "path"
+private const val LINE_GROUP = "line"
+private const val COLUMN_GROUP = "column"
+private val PATH_WITH_LOCATION = Pattern.compile("(?<${PATH_GROUP}>[^:]*)(:(?<${LINE_GROUP}>[\\d]+))?(:(?<${COLUMN_GROUP}>[\\d]+))?")
+
+private fun findAndNavigateToReference(project: Project, parameters: Map<String, String>) {
+  for (it in parameters) {
+    if (it.key.startsWith(JBProtocolNavigateCommand.FQN_KEY)) {
+      navigateByFqn(project, parameters, it.value)
     }
+  }
 
-    private fun navigateByPath(project: Project, parameters: Map<String, String>, pathText: String) {
-      val matcher = PATH_WITH_LOCATION.matcher(pathText)
-      if (!matcher.matches()) {
-        return
-      }
-
-      var path: String? = matcher.group(PATH_GROUP)
-      val line: String? = matcher.group(LINE_GROUP)
-      val column: String? = matcher.group(COLUMN_GROUP)
-
-      if (path == null) {
-        return
-      }
-
-      path = FileUtil.expandUserHome(path)
-      if (!FileUtil.isAbsolute(path)) {
-        path = File(project.basePath, path).absolutePath
-      }
-
-      val virtualFile = VirtualFileManager.getInstance().findFileByUrl(FILE_PROTOCOL + path) ?: return
-      FileEditorManager.getInstance(project).openFile(virtualFile, true)
-        .filterIsInstance<TextEditor>().first().let { textEditor ->
-          val editor = textEditor.editor
-          editor.caretModel.moveToOffset(editor.logicalPositionToOffset(LogicalPosition(line?.toInt() ?: 0, column?.toInt() ?: 0)))
-          setSelections(parameters, project)
-        }
+  for (it in parameters) {
+    if (it.key.startsWith(JBProtocolNavigateCommand.PATH_KEY)) {
+      navigateByPath(project, parameters, it.value)
     }
+  }
+}
 
-    private fun navigateByFqn(project: Project, parameters: Map<String, String>, reference: String) {
-      // handle single reference to method: com.intellij.navigation.JBProtocolNavigateCommand#perform
-      // multiple references are encoded and decoded properly
-      val fqn = parameters[FRAGMENT_PARAM_NAME]?.let { "$reference#$it" } ?: reference
+private fun navigateByPath(project: Project, parameters: Map<String, String>, pathText: String) {
+  val matcher = PATH_WITH_LOCATION.matcher(pathText)
+  if (!matcher.matches()) {
+    return
+  }
 
-      ProgressManager.getInstance().run(
-        object : Task.Backgroundable(project, IdeBundle.message("navigate.command.search.reference.progress.title", fqn), true) {
-          override fun run(indicator: ProgressIndicator) {
-            val dataContext = SimpleDataContext.getProjectContext(project)
-            SymbolSearchEverywhereContributor(AnActionEvent.createFromDataContext(ActionPlaces.UNKNOWN, null, dataContext))
-              .search(fqn, ProgressManager.getInstance().progressIndicator ?: StatusBarProgress())
-              .filterIsInstance<PsiElement>()
-              .forEach {
-                ApplicationManager.getApplication().invokeLater {
-                  PsiNavigateUtil.navigate(it)
-                  setSelections(parameters, project)
-                }
-              }
+  var path: String? = matcher.group(PATH_GROUP)
+  val line: String? = matcher.group(LINE_GROUP)
+  val column: String? = matcher.group(COLUMN_GROUP)
+
+  if (path == null) {
+    return
+  }
+
+  path = FileUtil.expandUserHome(path)
+  if (!FileUtil.isAbsolute(path)) {
+    path = File(project.basePath, path).absolutePath
+  }
+
+  val virtualFile = VirtualFileManager.getInstance().findFileByUrl(FILE_PROTOCOL + path) ?: return
+  FileEditorManager.getInstance(project).openFile(virtualFile, true)
+    .filterIsInstance<TextEditor>().first().let { textEditor ->
+      val editor = textEditor.editor
+      editor.caretModel.moveToOffset(editor.logicalPositionToOffset(LogicalPosition(line?.toInt() ?: 0, column?.toInt() ?: 0)))
+      setSelections(parameters, project)
+    }
+}
+
+private fun navigateByFqn(project: Project, parameters: Map<String, String>, reference: String) {
+  // handle single reference to method: com.intellij.navigation.JBProtocolNavigateCommand#perform
+  // multiple references are encoded and decoded properly
+  val fqn = parameters[FRAGMENT_PARAM_NAME]?.let { "$reference#$it" } ?: reference
+
+  ProgressManager.getInstance().run(
+    object : Task.Backgroundable(project, IdeBundle.message("navigate.command.search.reference.progress.title", fqn), true) {
+      override fun run(indicator: ProgressIndicator) {
+        val dataContext = SimpleDataContext.getProjectContext(project)
+        SymbolSearchEverywhereContributor(AnActionEvent.createFromDataContext(ActionPlaces.UNKNOWN, null, dataContext))
+          .search(fqn, ProgressManager.getInstance().progressIndicator ?: StatusBarProgress())
+          .filterIsInstance<PsiElement>()
+          .forEach {
+            ApplicationManager.getApplication().invokeLater {
+              PsiNavigateUtil.navigate(it)
+              setSelections(parameters, project)
+            }
           }
-
-          override fun shouldStartInBackground(): Boolean = !ApplicationManager.getApplication().isUnitTestMode
-          override fun isConditionalModal(): Boolean = !ApplicationManager.getApplication().isUnitTestMode
-        })
-    }
-
-    private fun setSelections(parameters: Map<String, String>, project: Project) {
-      parseSelections(parameters).forEach { selection -> setSelection(project, selection) }
-    }
-
-    private fun setSelection(project: Project, selection: Pair<LogicalPosition, LogicalPosition>) {
-      val editor = FileEditorManager.getInstance(project).selectedTextEditor
-      editor?.selectionModel?.setSelection(editor.logicalPositionToOffset(selection.first),
-                                           editor.logicalPositionToOffset(selection.second))
-    }
-
-    private fun parseSelections(parameters: Map<String, String>): List<Pair<LogicalPosition, LogicalPosition>> {
-      return parameters.filterKeys { it.startsWith(SELECTION) }.values.mapNotNull {
-        val split = it.split('-')
-        if (split.size != 2) return@mapNotNull null
-
-        val position1 = parsePosition(split[0])
-        val position2 = parsePosition(split[1])
-
-        if (position1 != null && position2 != null) {
-          return@mapNotNull Pair(position1, position1)
-        }
-        return@mapNotNull null
       }
-    }
 
-    private fun parsePosition(range: String): LogicalPosition? {
-      val position = range.split(':')
-      if (position.size != 2) return null
-      try {
-        return LogicalPosition(Integer.parseInt(position[0]), Integer.parseInt(position[1]))
-      }
-      catch (e: Exception) {
-        return null
-      }
+      override fun shouldStartInBackground(): Boolean = !ApplicationManager.getApplication().isUnitTestMode
+      override fun isConditionalModal(): Boolean = !ApplicationManager.getApplication().isUnitTestMode
+    })
+}
+
+private fun setSelections(parameters: Map<String, String>, project: Project) {
+  parseSelections(parameters).forEach { selection -> setSelection(project, selection) }
+}
+
+private fun setSelection(project: Project, selection: Pair<LogicalPosition, LogicalPosition>) {
+  val editor = FileEditorManager.getInstance(project).selectedTextEditor
+  editor?.selectionModel?.setSelection(editor.logicalPositionToOffset(selection.first),
+                                       editor.logicalPositionToOffset(selection.second))
+}
+
+private fun parseSelections(parameters: Map<String, String>): List<Pair<LogicalPosition, LogicalPosition>> {
+  return parameters.filterKeys { it.startsWith(JBProtocolNavigateCommand.SELECTION) }.values.mapNotNull {
+    val split = it.split('-')
+    if (split.size != 2) return@mapNotNull null
+
+    val position1 = parsePosition(split[0])
+    val position2 = parsePosition(split[1])
+
+    if (position1 != null && position2 != null) {
+      return@mapNotNull Pair(position1, position1)
     }
+    return@mapNotNull null
+  }
+}
+
+private fun parsePosition(range: String): LogicalPosition? {
+  val position = range.split(':')
+  if (position.size != 2) return null
+  try {
+    return LogicalPosition(Integer.parseInt(position[0]), Integer.parseInt(position[1]))
+  }
+  catch (e: Exception) {
+    return null
   }
 }
