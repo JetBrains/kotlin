90b524906a0c20e476a2676aec82abbcf087ffa5
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-02-18 19:36:26 +0300

    index: shared indexes is enabled if we have a shared index downloader
    intellij-monorepo: 21be0a9b21f88356d6b7091be0e0d154b9d75718

--- intellij-community/platform/lang-impl/src/com/intellij/index/SharedIndexExtensions.java
+++ intellij-community/platform/lang-impl/src/com/intellij/index/SharedIndexExtensions.java
@@ -1,24 +1,27 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.index;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.registry.Registry;
 import com.intellij.psi.stubs.StubSharedIndexExtension;
 import com.intellij.psi.stubs.StubUpdatingIndex;
 import com.intellij.util.indexing.FileBasedIndexExtension;
-import com.intellij.util.indexing.ID;
 import com.intellij.util.indexing.provided.OnDiskSharedIndexChunkLocator;
 import com.intellij.util.indexing.provided.SharedIndexExtension;
 import com.intellij.util.io.DataExternalizer;
 import com.intellij.util.io.KeyDescriptor;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.nio.file.Path;
 
 public class SharedIndexExtensions {
+  public static final String SHARED_INDEXES_DOWNLOAD_KEY = "shared.indexes.download";
   private static final String SHARED_INDEX_ENABLED = "shared.index.enabled";
 
   public static boolean areSharedIndexesEnabled() {
-    return System.getProperty(OnDiskSharedIndexChunkLocator.ROOT_PROP) != null || System.getProperty(SHARED_INDEX_ENABLED) != null;
+    return System.getProperty(OnDiskSharedIndexChunkLocator.ROOT_PROP) != null ||
+           System.getProperty(SHARED_INDEX_ENABLED) != null ||
+           (!ApplicationManager.getApplication().isUnitTestMode() && Registry.is(SHARED_INDEXES_DOWNLOAD_KEY));
   }
 
   @SuppressWarnings("unchecked")
