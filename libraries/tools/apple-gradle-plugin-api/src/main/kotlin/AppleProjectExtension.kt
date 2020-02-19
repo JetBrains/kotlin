3d7e1656e077279815c35e387d82eb0e607eac12
Florian Kistner <florian.kistner@jetbrains.com>
2020-02-19 16:06:50 +0100

    Mobile: Provide overloads for configuration via Groovy closures

--- libraries/tools/apple-gradle-plugin-api/src/main/kotlin/AppleProjectExtension.kt
+++ libraries/tools/apple-gradle-plugin-api/src/main/kotlin/AppleProjectExtension.kt
@@ -1,5 +1,7 @@
+import groovy.lang.Closure
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
+import org.gradle.util.ConfigureUtil
 import java.io.Serializable
 
 interface AppleProjectExtension : Serializable {
@@ -13,11 +15,19 @@ interface AppleProjectExtension : Serializable {
     fun iosApp(configure: AppleTarget.() -> Unit): AppleTarget
 
     @JvmDefault
+    fun iosApp(configureClosure: Closure<Any?>): AppleTarget =
+        iosApp { ConfigureUtil.configure(configureClosure, this) }
+
+    @JvmDefault
     fun iosApp(name: String): AppleTarget = iosApp(name) {}
 
     fun iosApp(name: String, configure: AppleTarget.() -> Unit): AppleTarget
+
+    @JvmDefault
+    fun iosApp(name: String, configureClosure: Closure<Any?>): AppleTarget =
+        iosApp(name) { ConfigureUtil.configure(configureClosure, this) }
 }
 
 @get:JvmName("apple")
 val Project.apple: AppleProjectExtension get() = extensions.getByType(AppleProjectExtension::class.java)
-fun Project.apple(configure: AppleProjectExtension.() -> Unit) = apple.configure()
+fun Project.apple(configure: AppleProjectExtension.() -> Unit): AppleProjectExtension = apple.apply { configure() }
