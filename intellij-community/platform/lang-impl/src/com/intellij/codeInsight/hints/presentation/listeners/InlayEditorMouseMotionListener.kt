968a7d0ca98be793d2865901a8e5e1ae52115ac6
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2020-02-19 13:43:16 +0700

    Inlay hints: move listeners into separate files
    intellij-monorepo: 44db636edec9b5dcfdb5edca456952c31feb2785

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/listeners/InlayEditorMouseMotionListener.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/listeners/InlayEditorMouseMotionListener.kt
@@ -1,30 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.codeInsight.hints.presentation
+package com.intellij.codeInsight.hints.presentation.listeners
 
+import com.intellij.codeInsight.hints.presentation.InlayPresentation
+import com.intellij.codeInsight.hints.presentation.PresentationRenderer
 import com.intellij.openapi.editor.event.EditorMouseEvent
 import com.intellij.openapi.editor.event.EditorMouseEventArea
-import com.intellij.openapi.editor.event.EditorMouseListener
 import com.intellij.openapi.editor.event.EditorMouseMotionListener
 import java.awt.Point
-
-/**
- * Global mouse listeners, that provide events to inlay hints at mouse coordinates.
- */
-class InlayEditorMouseListener : EditorMouseListener {
-  override fun mouseClicked(e: EditorMouseEvent) {
-    if (!e.isConsumed) {
-      val editor = e.editor
-      val event = e.mouseEvent
-      if (editor.getMouseEventArea(event) != EditorMouseEventArea.EDITING_AREA) return
-      val point = event.point
-      val inlay = editor.inlayModel.getElementAt(point, PresentationRenderer::class.java) ?: return
-      val bounds = inlay.bounds ?: return
-      val inlayPoint = Point(bounds.x, bounds.y)
-      val translated = Point(event.x - inlayPoint.x, event.y - inlayPoint.y)
-      inlay.renderer.presentation.mouseClicked(event, translated)
-    }
-  }
-}
 
 class InlayEditorMouseMotionListener : EditorMouseMotionListener {
   private var activePresentation: InlayPresentation? = null
