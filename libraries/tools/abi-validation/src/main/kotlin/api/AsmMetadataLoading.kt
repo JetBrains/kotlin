215ef8e7dadc196fb591dfc652ff2b3be41133a8
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-02-19 19:41:08 +0300

    [ABI Validation] Rename files and packages
    Moved from https://github.com/Kotlin/binary-compatibility-validator/commit/647694935be0725eb8b955d26054c52d0c964aff

--- libraries/tools/abi-validation/src/main/kotlin/api/AsmMetadataLoading.kt
+++ libraries/tools/abi-validation/src/main/kotlin/api/AsmMetadataLoading.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.tools
+package kotlinx.validation.api
 
 import kotlinx.metadata.jvm.*
 import org.objectweb.asm.Opcodes
