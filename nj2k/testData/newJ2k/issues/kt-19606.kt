285aa123b7a3b54f3bc821d5e18ce0fe83ac6763
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-02-19 11:45:16 +0300

    New J2K: fix testdata

--- nj2k/testData/newJ2k/issues/kt-19606.kt
+++ nj2k/testData/newJ2k/issues/kt-19606.kt
@@ -3,6 +3,6 @@ import java.util.HashMap
 class TestMethodReference {
     private val hashMap = HashMap<String, String>()
     fun update(key: String, msg: String) {
-        hashMap.merge(key, msg) { obj: String, s: String -> obj + s }
+        hashMap.merge(key, msg) { obj: String, str: String -> obj + str }
     }
 }
