14523ce213bb87d5d20d48dca9f7e48062abb9ad
Eugene Petrenko <eugene.petrenko@gmail.com>
2020-02-21 11:46:32 +0100

    jdk-auto - do not show missing JDK notifications for unrelevant files. Added SdkType#isRelevantForFile API
    intellij-monorepo: c32b11f1821953100ae93c5ec233e0fe641becf2

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkEditorNotification.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkEditorNotification.java
@@ -4,7 +4,6 @@ package com.intellij.openapi.projectRoots.impl;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.intellij.codeInsight.intention.PriorityAction;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
@@ -29,10 +28,9 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class UnknownSdkEditorNotification implements Disposable {
+public class UnknownSdkEditorNotification {
   public static final Key<List<EditorNotificationPanel>> NOTIFICATIONS = Key.create("notifications added to the editor");
   private static final Key<?> EDITOR_NOTIFICATIONS_KEY = Key.create("SdkSetupNotificationNew");
 
@@ -43,13 +41,13 @@ public class UnknownSdkEditorNotification implements Disposable {
 
   private final Project myProject;
   private final FileEditorManager myFileEditorManager;
-  private final AtomicReference<Set<SdkFixInfo>> myNotifications = new AtomicReference<>(new LinkedHashSet<>());
+  private final AtomicReference<Set<SimpleSdkFixInfo>> myNotifications = new AtomicReference<>(new LinkedHashSet<>());
 
   UnknownSdkEditorNotification(@NotNull Project project) {
     myProject = project;
     myFileEditorManager = FileEditorManager.getInstance(myProject);
     myProject.getMessageBus()
-      .connect(this)
+      .connect()
       .subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
         @Override
         public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
@@ -74,24 +72,21 @@ public class UnknownSdkEditorNotification implements Disposable {
     return panel;
   }
 
-  @Override
-  public void dispose() { }
-
   @NotNull
-  public List<SdkFixInfo> getNotifications() {
+  public List<SimpleSdkFixInfo> getNotifications() {
     return ImmutableList.copyOf(myNotifications.get());
   }
 
   public void showNotifications(@NotNull List<UnknownSdk> unfixableSdks,
                                 @NotNull Map<UnknownSdk, UnknownSdkDownloadableSdkFix> files) {
-    ImmutableSet.Builder<SdkFixInfo> notifications = ImmutableSet.builder();
+    ImmutableSet.Builder<SimpleSdkFixInfo> notifications = ImmutableSet.builder();
 
     if (Registry.is("unknown.sdk.show.editor.actions")) {
       for (UnknownSdk e : unfixableSdks) {
         @Nullable String name = e.getSdkName();
         SdkType type = e.getSdkType();
         if (name == null) continue;
-        notifications.add(new SimpleSdkFixInfo(name, type));
+        notifications.add(new SimpleSdkFixInfo(name, type, null, null));
       }
 
       for (Map.Entry<UnknownSdk, UnknownSdkDownloadableSdkFix> e : files.entrySet()) {
@@ -100,7 +95,7 @@ public class UnknownSdkEditorNotification implements Disposable {
         if (name == null) continue;
 
         UnknownSdkDownloadableSdkFix fix = e.getValue();
-        notifications.add(new SimpleSdkFixInfo(name, unknownSdk, fix));
+        notifications.add(new SimpleSdkFixInfo(name, unknownSdk.getSdkType(), unknownSdk, fix));
       }
     }
 
@@ -129,11 +124,11 @@ public class UnknownSdkEditorNotification implements Disposable {
       editor.putUserData(NOTIFICATIONS, notifications);
     }
 
-    for (SdkFixInfo info : myNotifications.get()) {
+    for (SimpleSdkFixInfo info : myNotifications.get()) {
       VirtualFile file = editor.getFile();
       if (file == null) continue;
 
-      EditorNotificationPanel notification = info.createNotificationPanel(file, editor, myProject);
+      EditorNotificationPanel notification = info.createNotificationPanel(file, myProject);
       if (notification == null) continue;
 
       notifications.add(notification);
@@ -141,39 +136,30 @@ public class UnknownSdkEditorNotification implements Disposable {
     }
   }
 
-  public interface SdkFixInfo {
-    @Nullable
-    EditorNotificationPanel createNotificationPanel(@NotNull VirtualFile file,
-                                                    @NotNull FileEditor fileEditor,
-                                                    @NotNull Project project);
-  }
-
-  private class SimpleSdkFixInfo implements SdkFixInfo {
+  public class SimpleSdkFixInfo {
     @NotNull private final String mySdkName;
-    @Nullable private final SdkType mySdkType;
+    @NotNull private final SdkType mySdkType;
     @Nullable private final UnknownSdk mySdk;
     @Nullable private final UnknownSdkDownloadableSdkFix myFix;
 
-    SimpleSdkFixInfo(@NotNull String sdkName, @NotNull SdkType sdkType) {
+    SimpleSdkFixInfo(@NotNull String sdkName,
+                     @NotNull SdkType sdkType,
+                     @Nullable UnknownSdk sdk,
+                     @Nullable UnknownSdkDownloadableSdkFix fix) {
       mySdkName = sdkName;
       mySdkType = sdkType;
-      mySdk = null;
-      myFix = null;
-    }
-
-    SimpleSdkFixInfo(@NotNull String name, @NotNull UnknownSdk sdk, @NotNull UnknownSdkDownloadableSdkFix fix) {
-      mySdkName = name;
-      mySdkType = sdk.getSdkType();
       mySdk = sdk;
       myFix = fix;
     }
 
-    @NotNull
-    @Override
-    public final EditorNotificationPanel createNotificationPanel(@NotNull VirtualFile file,
-                                                                 @NotNull FileEditor fileEditor,
-                                                                 @NotNull Project project) {
-      String sdkTypeName = mySdkType != null ? mySdkType.getPresentableName() : ProjectBundle.message("config.unknown.sdk.text");
+    @Nullable
+    final EditorNotificationPanel createNotificationPanel(@NotNull VirtualFile file, @NotNull Project project) {
+      //we must not show the notification for an unrelevant files in the project
+      if (!mySdkType.isRelevantForFile(project, file)) {
+        return null;
+      }
+
+      String sdkTypeName = mySdkType.getPresentableName();
       String quotedSdkName = "\"" + mySdkName + "\"";
       String notificationText = ProjectBundle.message("config.unknown.sdk.notification.text", sdkTypeName, quotedSdkName);
       String configureText = ProjectBundle.message("config.unknown.sdk.configure");
