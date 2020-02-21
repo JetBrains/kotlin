f837b01f855be09978bf5a9d103b8a266d35bb15
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-02-21 13:06:12 +0700

    i18n: add bundle for idea/hierarchy

--- idea/src/org/jetbrains/kotlin/idea/hierarchy/calls/KotlinCallHierarchyNodeDescriptor.kt
+++ idea/src/org/jetbrains/kotlin/idea/hierarchy/calls/KotlinCallHierarchyNodeDescriptor.kt
@@ -30,6 +30,7 @@ import com.intellij.psi.PsiReference
 import com.intellij.ui.LayeredIcon
 import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToDescriptorIfAny
+import org.jetbrains.kotlin.idea.hierarchy.KotlinHierarchyBundle
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.renderer.DescriptorRenderer
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
@@ -162,7 +163,7 @@ class KotlinCallHierarchyNodeDescriptor(
                                     elementText = if (element.name != null) {
                                         renderClassOrObject(descriptor as ClassDescriptor)
                                     } else {
-                                        "[anonymous]"
+                                        KotlinHierarchyBundle.message("text.anonymous")
                                     }
                                 }
                             }
