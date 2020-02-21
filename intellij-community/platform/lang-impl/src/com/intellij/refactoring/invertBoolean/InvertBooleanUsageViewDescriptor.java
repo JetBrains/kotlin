3d716d8d3928f3c1cef393e889694bcfbf071ac6
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-02-21 19:37:56 +0300

    make UsageViewDescriptor#getCommentReferencesText default and remove dummy implementations
    intellij-monorepo: 526b4df66265a02e66fa8fd8fe149dceedeb86b9

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/invertBoolean/InvertBooleanUsageViewDescriptor.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/invertBoolean/InvertBooleanUsageViewDescriptor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.invertBoolean;
 
 import com.intellij.psi.PsiElement;
@@ -32,10 +32,5 @@ public class InvertBooleanUsageViewDescriptor implements UsageViewDescriptor {
   @Override
   public String getCodeReferencesText(int usagesCount, int filesCount) {
     return RefactoringBundle.message("invert.boolean.refs.to.invert", UsageViewBundle.getReferencesString(usagesCount, filesCount));
-  }
-
-  @Override
-  public String getCommentReferencesText(int usagesCount, int filesCount) {
-    return null;
   }
 }
--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/memberPushDown/PushDownUsageViewDescriptor.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/memberPushDown/PushDownUsageViewDescriptor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.memberPushDown;
 
 import com.intellij.lang.findUsages.DescriptiveNameUtil;
@@ -43,10 +43,4 @@ public class PushDownUsageViewDescriptor<MemberInfo extends MemberInfoBase<Membe
   public String getCodeReferencesText(int usagesCount, int filesCount) {
     return RefactoringBundle.message("classes.to.push.down.members.to", UsageViewBundle.getReferencesString(usagesCount, filesCount));
   }
-
-  @Override
-  public String getCommentReferencesText(int usagesCount, int filesCount) {
-    return null;
-  }
-
 }
