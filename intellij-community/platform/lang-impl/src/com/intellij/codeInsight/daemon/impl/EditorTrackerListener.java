6999a56f67aed67c881c205514e790321e8ce2f9
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-21 13:34:32 +0100

    cleanup
    intellij-monorepo: 4a353d2fbb0890af270f22c38057d0024103205d

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/EditorTrackerListener.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/EditorTrackerListener.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.daemon.impl;
 
 import com.intellij.openapi.editor.Editor;
@@ -9,8 +9,7 @@ import java.util.EventListener;
 import java.util.List;
 
 public interface EditorTrackerListener extends EventListener{
-  Topic<EditorTrackerListener> TOPIC =
-    new Topic<>(EditorTrackerListener.class.getSimpleName(), EditorTrackerListener.class, Topic.BroadcastDirection.NONE);
+  Topic<EditorTrackerListener> TOPIC = new Topic<>(EditorTrackerListener.class, Topic.BroadcastDirection.NONE);
 
   void activeEditorsChanged(@NotNull List<Editor> activeEditors);
 }
