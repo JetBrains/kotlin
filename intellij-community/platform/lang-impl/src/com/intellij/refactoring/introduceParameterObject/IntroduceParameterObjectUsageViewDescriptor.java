316164ca08f02865460543be726f7a0191c524ec
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-02-21 20:09:02 +0300

    don't use UsageViewDescriptorAdapter if the implementation overrides #getCodeReferencesText
    intellij-monorepo: 532ef0bb8d83438b89847b27c09994d50516b114

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/introduceParameterObject/IntroduceParameterObjectUsageViewDescriptor.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/introduceParameterObject/IntroduceParameterObjectUsageViewDescriptor.java
@@ -1,13 +1,13 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.introduceParameterObject;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.refactoring.RefactoringBundle;
-import com.intellij.refactoring.ui.UsageViewDescriptorAdapter;
 import com.intellij.usageView.UsageViewBundle;
+import com.intellij.usageView.UsageViewDescriptor;
 import org.jetbrains.annotations.NotNull;
 
-public class IntroduceParameterObjectUsageViewDescriptor extends UsageViewDescriptorAdapter {
+public class IntroduceParameterObjectUsageViewDescriptor implements UsageViewDescriptor {
 
   private final PsiElement method;
 
--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/safeDelete/SafeDeleteUsageViewDescriptor.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/safeDelete/SafeDeleteUsageViewDescriptor.java
@@ -1,31 +1,17 @@
-/*
- * Copyright 2000-2009 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.refactoring.safeDelete;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.refactoring.RefactoringBundle;
-import com.intellij.refactoring.ui.UsageViewDescriptorAdapter;
 import com.intellij.usageView.UsageViewBundle;
+import com.intellij.usageView.UsageViewDescriptor;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author dsl
  */
-public class SafeDeleteUsageViewDescriptor extends UsageViewDescriptorAdapter {
+public class SafeDeleteUsageViewDescriptor implements UsageViewDescriptor {
   private final PsiElement[] myElementsToDelete;
 
   public SafeDeleteUsageViewDescriptor(PsiElement[] elementsToDelete) {
