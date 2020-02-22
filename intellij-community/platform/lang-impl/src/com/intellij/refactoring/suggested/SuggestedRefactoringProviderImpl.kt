306f14c2ea70e4aca7196e6a6e6e8df78b012176
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-02-22 10:41:06 +0100

    use processImplementations to not even create extension if not white-listed
    intellij-monorepo: 722a75d3e0456b58bd3502eace537478645f4919

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/suggested/SuggestedRefactoringProviderImpl.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/suggested/SuggestedRefactoringProviderImpl.kt
@@ -1,5 +1,4 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-
 package com.intellij.refactoring.suggested
 
 import com.intellij.openapi.project.Project
@@ -14,7 +13,7 @@ class SuggestedRefactoringProviderImpl(project: Project) : SuggestedRefactoringP
   val state: SuggestedRefactoringState?
     get() = changeCollector.state
 
-  class Startup : StartupActivity.DumbAware {
+  internal class Startup : StartupActivity.DumbAware {
     override fun runActivity(project: Project) {
       val listener = getInstance(project).listener
       listener.attach()
