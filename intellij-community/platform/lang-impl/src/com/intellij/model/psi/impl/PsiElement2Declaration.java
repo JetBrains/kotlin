03584cb7046ca87816b9a89901fcd8f15caf9240
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-02-26 20:50:07 +0300

    EA-223329 EA-223430 report cases when identifying element doesn't have text range
    intellij-monorepo: 52e6696adcc4fb8aec44befca260a7c7bd9fae90

--- intellij-community/platform/lang-impl/src/com/intellij/model/psi/impl/PsiElement2Declaration.java
+++ intellij-community/platform/lang-impl/src/com/intellij/model/psi/impl/PsiElement2Declaration.java
@@ -107,9 +107,27 @@ class PsiElement2Declaration implements PsiSymbolDeclaration {
   @Nullable
   private static PsiElement getIdentifyingElement(@NotNull PsiElement targetElement) {
     if (targetElement instanceof PsiNameIdentifierOwner) {
-      return ((PsiNameIdentifierOwner)targetElement).getIdentifyingElement();
+      return getIdentifyingElement((PsiNameIdentifierOwner)targetElement);
     }
     return null;
+  }
+
+  @Nullable
+  private static PsiElement getIdentifyingElement(@NotNull PsiNameIdentifierOwner nameIdentifierOwner) {
+    PsiElement identifyingElement = nameIdentifierOwner.getIdentifyingElement();
+    if (identifyingElement == null) {
+      return null;
+    }
+    TextRange identifyingElementRange = identifyingElement.getTextRange();
+    if (identifyingElementRange == null) {
+      LOG.error("Identifying element has no text range;\n" +
+                "target: " + nameIdentifierOwner + ";\n" +
+                "target class: " + nameIdentifierOwner.getClass().getName() + ";\n" +
+                "identifying element: " + identifyingElement + ";\n" +
+                "identifying element class: " + identifyingElement.getClass().getName());
+      return null;
+    }
+    return identifyingElement;
   }
 
   /**
