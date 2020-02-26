5aee9380241e4e967ff59d4979c45ea2ccdde7af
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2020-02-26 20:12:20 +0300

    [FIR] Add tests for KT-37070 , KT-37066

--- compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirDiagnosticsTestGenerated.java
+++ compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/FirDiagnosticsTestGenerated.java
@@ -208,6 +208,16 @@ public class FirDiagnosticsTestGenerated extends AbstractFirDiagnosticsTest {
         runTest("compiler/fir/resolve/testData/resolve/javaStaticScopeInheritance.kt");
     }
 
+    @TestMetadata("lambdaArgInScopeFunction.kt")
+    public void testLambdaArgInScopeFunction() throws Exception {
+        runTest("compiler/fir/resolve/testData/resolve/lambdaArgInScopeFunction.kt");
+    }
+
+    @TestMetadata("lambdaPropertyTypeInference.kt")
+    public void testLambdaPropertyTypeInference() throws Exception {
+        runTest("compiler/fir/resolve/testData/resolve/lambdaPropertyTypeInference.kt");
+    }
+
     @TestMetadata("localFunctionsHiding.kt")
     public void testLocalFunctionsHiding() throws Exception {
         runTest("compiler/fir/resolve/testData/resolve/localFunctionsHiding.kt");
