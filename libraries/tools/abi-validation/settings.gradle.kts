5f6b31e00a4bf583726a9b53d580721f0f79b582
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-02-28 13:02:26 +0300

    [ABI Validation] Publish plugin using both simple and FQ names
    Moved from https://github.com/Kotlin/binary-compatibility-validator/commit/31f9d02c76e8274429bbdf43f5d99a1c854d99c1

--- libraries/tools/abi-validation/settings.gradle.kts
+++ libraries/tools/abi-validation/settings.gradle.kts
@@ -1,12 +1,16 @@
 rootProject.name = "binary-compatibility-validator"
 
 val kotlinVersion: String by settings
+val pluginPublishVersion: String by settings
 
 pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.namespace?.startsWith("org.jetbrains.kotlin") == true) {
                 useVersion(kotlinVersion)
+            }
+            if (requested.id.id == "com.gradle.plugin-publish") {
+                useVersion(pluginPublishVersion)
             }
         }
     }
