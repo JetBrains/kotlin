6620111330b5396e206bff16ccde455498d91eda
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-03-01 14:52:49 +0200

    IDEA-232159 localize "New" popup
    intellij-monorepo: 002e1bfd536841133630892d5a98a66c8da48bd2

--- intellij-community/platform/lang-impl/src/com/intellij/ide/fileTemplates/actions/FileTemplateSeparator.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/fileTemplates/actions/FileTemplateSeparator.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.fileTemplates.actions;
 
+import com.intellij.ide.IdeBundle;
 import com.intellij.openapi.actionSystem.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -12,7 +13,7 @@ public class FileTemplateSeparator extends ActionGroup {
 
   @Override
   public AnAction @NotNull [] getChildren(@Nullable AnActionEvent e) {
-    return new AnAction[]{Separator.create(shouldShowNamedSeparator(e) ? "File templates" : null)};
+    return new AnAction[]{Separator.create(shouldShowNamedSeparator(e) ? IdeBundle.message("action.separator.file.templates") : null)};
   }
 
   private static boolean shouldShowNamedSeparator(@Nullable AnActionEvent e) {
