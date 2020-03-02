f0407e5ed9413add3718eb656df6adaeb1773a75
Brian Norman <brian.e.norman@gmail.com>
2020-03-02 12:40:18 -0600

    Update sample to be a multiplatform project

---
+++ sample/src/commonTest/kotlin/com/bnorm/power/PowerAssertTest.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 Brian Norman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bnorm.power
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class PowerAssertTest {
+  @Test
+  fun assertTrue() {
+    assertTrue(Person.UNKNOWN.size == 1)
+  }
+
+  @Test
+  fun require() {
+    require(Person.UNKNOWN.size == 1)
+  }
+}
