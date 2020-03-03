9f00b7640267842a7d20112f8312c85a3f9bfc4a
Alexey Kudravtsev <cdr@intellij.com>
2020-03-03 18:08:55 +0300

    make code simpler
    intellij-monorepo: d8b0455ab9f68f90bbff4768545f2b989d464a95

--- intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/AbstractExternalSystemTaskConfigurationType.java
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/execution/AbstractExternalSystemTaskConfigurationType.java
@@ -68,8 +68,8 @@ public abstract class AbstractExternalSystemTaskConfigurationType implements Con
   @NonNls
   @NotNull
   protected String getConfigurationFactoryId() {
-    DeprecatedMethodException.reportDefaultImplementation("AbstractExternalSystemTaskConfigurationType::getConfigurationFactoryId",
-                                                          "The default implementation delegates to 'ProjectSystemId::getReadableName' which is supposed to be localized but return value of this method must not be localized.", getClass());
+    DeprecatedMethodException.reportDefaultImplementation(
+      "The default implementation delegates to 'ProjectSystemId::getReadableName' which is supposed to be localized but return value of this method must not be localized.");
     return myExternalSystemId.getReadableName();
   }
 
