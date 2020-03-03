d6ccdb3caa9919c332ba80a113ea8f7734b7cd7d
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-03-03 13:54:44 +0300

    NI: fix `extensionLambdasAndArrow` test

--- compiler/testData/diagnostics/tests/inference/extensionLambdasAndArrow.fir.kt
+++ compiler/testData/diagnostics/tests/inference/extensionLambdasAndArrow.fir.kt
@@ -1,5 +1,7 @@
 // !DIAGNOSTICS: -UNUSED_VARIABLE -UNUSED_ANONYMOUS_PARAMETER
 
+fun <T> select(vararg x: T) = x[0]
+
 fun main() {
     val x1: String.() -> String = if (true) {{ this }} else {{ this }}
     val x2: String.() -> String = if (true) {{ -> this }} else {{ -> this }}
@@ -7,4 +9,6 @@ fun main() {
     val x4: String.() -> String = if (true) {{ str: String -> "this" }} else {{ str: String -> "this" }}
     val x5: String.() -> String = if (true) {{ str -> "this" }} else {{ str -> "this" }}
     val x6: String.() -> String = if (true) {{ str -> "this" }} else {{ "this" }}
+    val x7: String.() -> String = select({ -> this }, { -> this })
+    val x8: String.() -> String = select({ this }, { this })
 }
