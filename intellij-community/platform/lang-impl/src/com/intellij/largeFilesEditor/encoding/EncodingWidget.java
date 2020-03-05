9330d36090b0f15b752d21080008171840bbc2de
Alexander Zolotov <zolotov@jetbrains.com>
2020-03-05 19:35:46 +0300

    Status bar: support press-drag-release click-behaviour for status bar widgets (IDEA-234396)
    intellij-monorepo: 009d9e7d17bd214ad79d6c8b8e949129e1010be0

--- intellij-community/platform/lang-impl/src/com/intellij/largeFilesEditor/encoding/EncodingWidget.java
+++ intellij-community/platform/lang-impl/src/com/intellij/largeFilesEditor/encoding/EncodingWidget.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.largeFilesEditor.encoding;
 
 import com.intellij.openapi.application.ModalityState;
@@ -79,7 +79,7 @@ public class EncodingWidget extends EditorBasedWidget implements StatusBarWidget
         tryShowPopup();
         return true;
       }
-    }.installOn(myComponent);
+    }.installOn(myComponent, true);
   }
 
   private void tryShowPopup() {
