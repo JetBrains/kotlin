cf2907d8ef21b24c3afce853a4a0b598f5ebe094
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-03-05 19:12:15 +0300

    [Gradle, JS] Optimize gradle configurators for debuggers
    #KT-37037 fixed

--- ultimate/resources/org/jetbrains/kotlin/idea/debugger/GradleBrowserDebugConfigurator.groovy
+++ ultimate/resources/org/jetbrains/kotlin/idea/debugger/GradleBrowserDebugConfigurator.groovy
@@ -1,41 +1,43 @@
 import org.gradle.api.Task
 
 ({
-    def doIfInstance = { Task task, String fqn, Closure action ->
-        def taskSuperClass = task.class
-        while (taskSuperClass != Object.class) {
-            if (taskSuperClass.canonicalName == fqn) {
-                action()
+    Class kotlinTestClass = null
+    try {
+        kotlinTestClass = Class.forName("org.jetbrains.kotlin.gradle.tasks.KotlinTest")
+    } catch (ClassNotFoundException ex) {
+        // ignore, class not available
+    }
 
-                return
-            } else {
-                taskSuperClass = taskSuperClass.superclass
-            }
-        }
+    Class karmaClass = null
+    try {
+        karmaClass = Class.forName("org.jetbrains.kotlin.gradle.targets.js.testing.karma.KotlinKarma")
+    } catch (ClassNotFoundException ex) {
+        // ignore, class not available
     }
 
     def forJsTestTask = { Task task, Closure action ->
-        doIfInstance(task, "org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest", action)
-    }
-
-    def forJsBrowserTestTask = { Task task, Closure action ->
-        if (task.name.toLowerCase().contains("browser".toLowerCase())) {
-            forJsTestTask(task, action)
+        if (
+        kotlinTestClass.isAssignableFrom(task.class)
+                && task.hasProperty('testFramework')
+                && karmaClass != null
+                && karmaClass.isAssignableFrom(task.testFramework.class)
+        ) {
+            action()
         }
     }
 
-    gradle.taskGraph.beforeTask { Task task ->
-        forJsBrowserTestTask(task) {
-            if (task.hasProperty('debug')) {
-                ForkedDebuggerHelper.setupDebugger('%id', task.path, '', '%dispatchPort'.toInteger())
-                task.debug = true
+    if (kotlinTestClass != null) {
+        gradle.taskGraph.beforeTask { Task task ->
+            forJsTestTask(task) {
+                if (task.hasProperty('debug')) {
+                    ForkedDebuggerHelper.setupDebugger('%id', task.path, '', '%dispatchPort'.toInteger())
+                    task.debug = true
+                }
             }
         }
-    }
 
-    gradle.taskGraph.afterTask { Task task ->
-        forJsBrowserTestTask(task) {
-            if (task.hasProperty('debug')) {
+        gradle.taskGraph.afterTask { Task task ->
+            forJsTestTask(task) {
                 ForkedDebuggerHelper.signalizeFinish('%id', task.path, '%dispatchPort'.toInteger())
             }
         }
--- ultimate/resources/org/jetbrains/kotlin/idea/debugger/GradleNodeJsDebugConfigurator.groovy
+++ ultimate/resources/org/jetbrains/kotlin/idea/debugger/GradleNodeJsDebugConfigurator.groovy
@@ -1,52 +1,75 @@
 import org.gradle.api.Task
 
 ({
-    def doIfInstance = { Task task, String fqn, Closure action ->
-        def taskSuperClass = task.class
-        while (taskSuperClass != Object.class) {
-            if (taskSuperClass.canonicalName == fqn) {
-                action()
-
-                return
-            } else {
-                taskSuperClass = taskSuperClass.superclass
-            }
-        }
+    Class kotlinTestClass = null
+    try {
+        kotlinTestClass = Class.forName("org.jetbrains.kotlin.gradle.tasks.KotlinTest")
+    } catch (ClassNotFoundException ex) {
+        // ignore, class not available
     }
 
-    def forNodeJsTask = { Task task, Closure action ->
-        doIfInstance(task, "org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec", action)
+    Class mochaClass = null
+    try {
+        mochaClass = Class.forName("org.jetbrains.kotlin.gradle.targets.js.testing.mocha.KotlinMocha")
+    } catch (ClassNotFoundException ex) {
+        // ignore, class not available
     }
 
     def forNodeJsTestTask = { Task task, Closure action ->
-        if (task.name.toLowerCase().contains("node".toLowerCase())) {
-            doIfInstance(task, "org.jetbrains.kotlin.gradle.targets.js.testing.KotlinJsTest", action)
+        if (
+        kotlinTestClass.isAssignableFrom(task.class)
+                && task.hasProperty('testFramework')
+                && mochaClass != null
+                && mochaClass.isAssignableFrom(task.testFramework.class)
+        ) {
+            action()
         }
     }
 
-    gradle.taskGraph.beforeTask { Task task ->
-        forNodeJsTask(task) {
-            if (task.hasProperty('args') && task.args) {
-                ForkedDebuggerHelper.setupDebugger('%id', task.path, '', '%dispatchPort'.toInteger())
-                task.args = ['--inspect-brk'] + task.args
+    if (kotlinTestClass != null) {
+        gradle.taskGraph.beforeTask { Task task ->
+            forNodeJsTestTask(task) {
+                if (task.hasProperty('debug')) {
+                    ForkedDebuggerHelper.setupDebugger('%id', task.path, '', '%dispatchPort'.toInteger())
+                    task.debug = true
+                }
             }
         }
 
-        forNodeJsTestTask(task) {
-            if (task.hasProperty('debug')) {
-                ForkedDebuggerHelper.setupDebugger('%id', task.path, '', '%dispatchPort'.toInteger())
-                task.debug = true
+        gradle.taskGraph.afterTask { Task task ->
+            forNodeJsTestTask(task) {
+                ForkedDebuggerHelper.signalizeFinish('%id', task.path, '%dispatchPort'.toInteger())
             }
         }
     }
 
-    gradle.taskGraph.afterTask { Task task ->
-        forNodeJsTask(task) {
-            ForkedDebuggerHelper.signalizeFinish('%id', task.path, '%dispatchPort'.toInteger())
+    Class nodeJsExecClass = null
+    try {
+        nodeJsExecClass = Class.forName("org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec")
+    } catch (ClassNotFoundException ex) {
+        // ignore, class not available
+    }
+
+    def forNodeJsTask = { Task task, Closure action ->
+        if (nodeJsExecClass.isAssignableFrom(task.class)) {
+            action()
+        }
+    }
+
+    if (nodeJsExecClass != null) {
+        gradle.taskGraph.beforeTask { Task task ->
+            forNodeJsTask(task) {
+                if (task.hasProperty('args') && task.args) {
+                    ForkedDebuggerHelper.setupDebugger('%id', task.path, '', '%dispatchPort'.toInteger())
+                    task.args = ['--inspect-brk'] + task.args
+                }
+            }
         }
 
-        forNodeJsTestTask(task) {
-            ForkedDebuggerHelper.signalizeFinish('%id', task.path, '%dispatchPort'.toInteger())
+        gradle.taskGraph.afterTask { Task task ->
+            forNodeJsTask(task) {
+                ForkedDebuggerHelper.signalizeFinish('%id', task.path, '%dispatchPort'.toInteger())
+            }
         }
     }
 })()
