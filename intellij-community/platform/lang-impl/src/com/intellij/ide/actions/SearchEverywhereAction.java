2126db52e6a0d9354ede0350a5b9d265659388e5
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2020-03-05 20:23:04 +0300

    Don't show Search Everywhere in LightEdit mode (IDEA-231253)
    intellij-monorepo: cbe4efb812abc0fc6849aed579652d38f4b70cbc

--- intellij-community/platform/lang-impl/src/com/intellij/ide/actions/SearchEverywhereAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/actions/SearchEverywhereAction.java
@@ -18,6 +18,7 @@ import com.intellij.ide.actions.searcheverywhere.SearchEverywhereManager;
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereManagerImpl;
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereUI;
 import com.intellij.ide.actions.searcheverywhere.statistics.SearchEverywhereUsageTriggerCollector;
+import com.intellij.ide.lightEdit.LightEdit;
 import com.intellij.ide.structureView.StructureView;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.ide.structureView.StructureViewModel;
@@ -560,6 +561,8 @@ public class SearchEverywhereAction extends AnAction implements CustomComponentA
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
+    if (LightEdit.owns(e.getProject())) return;
+
     if (Registry.is("ide.suppress.double.click.handler") && e.getInputEvent() instanceof KeyEvent) {
       if (((KeyEvent)e.getInputEvent()).getKeyCode() == KeyEvent.VK_SHIFT) {
         return;
