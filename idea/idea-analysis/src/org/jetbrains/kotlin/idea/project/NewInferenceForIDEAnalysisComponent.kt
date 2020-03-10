4a7160b51aa3b26adf26060ccefb95e2eb359a9e
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-03-10 14:50:19 +0700

    i18n: update bundle for idea-analysis

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/NewInferenceForIDEAnalysisComponent.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/project/NewInferenceForIDEAnalysisComponent.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -7,10 +7,6 @@ package org.jetbrains.kotlin.idea.project
 
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.config.KotlinCompilerVersion
-import org.jetbrains.kotlin.idea.util.isDev
-import org.jetbrains.kotlin.idea.util.isEap
-import org.jetbrains.kotlin.idea.util.isSnapshot
 
 object NewInferenceForIDEAnalysisComponent {
     private const val inferenceOptionV1 = "kotlin.use.new.inference.for.ide.analysis"
