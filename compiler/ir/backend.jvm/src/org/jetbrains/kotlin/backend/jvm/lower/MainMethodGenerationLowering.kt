fd70b10b17512c4d6ec2339f090c4aff64af71b7
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2020-03-10 15:42:12 +0100

    JVM_IR: Generate suspend fun main wrapper as class instead of reference
    This way, there is no getName, getOwner and getSignature generated.
     #KT-37404: Open

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/MainMethodGenerationLowering.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/lower/MainMethodGenerationLowering.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -7,6 +7,8 @@ package org.jetbrains.kotlin.backend.jvm.lower
 
 import org.jetbrains.kotlin.backend.common.ClassLoweringPass
 import org.jetbrains.kotlin.backend.common.ir.allParameters
+import org.jetbrains.kotlin.backend.common.ir.copyTo
+import org.jetbrains.kotlin.backend.common.ir.createImplicitParameterDeclarationWithWrappedDescriptor
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.phaser.makeIrFilePhase
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
@@ -18,15 +20,18 @@ import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
 import org.jetbrains.kotlin.ir.builders.*
-import org.jetbrains.kotlin.ir.builders.declarations.addFunction
-import org.jetbrains.kotlin.ir.builders.declarations.addValueParameter
+import org.jetbrains.kotlin.ir.builders.declarations.*
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.declarations.copyAttributes
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.impl.IrConstructorCallImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrFunctionReferenceImpl
 import org.jetbrains.kotlin.ir.types.*
-import org.jetbrains.kotlin.ir.util.functions
-import org.jetbrains.kotlin.ir.util.isFileClass
+import org.jetbrains.kotlin.ir.util.*
+import org.jetbrains.kotlin.load.java.JavaVisibilities
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.types.Variance
 
@@ -130,19 +135,74 @@ private class MainMethodGenerationLowering(private val context: JvmBackendContex
             body = context.createIrBuilder(symbol).irBlockBody { makeBody(args) }
         }
 
-    private fun IrBuilderWithScope.irRunSuspend(target: IrSimpleFunction, args: IrValueParameter?) =
-        irCall(this@MainMethodGenerationLowering.context.ir.symbols.runSuspendFunction).apply {
-            val reference = IrFunctionReferenceImpl(
-                UNDEFINED_OFFSET,
-                UNDEFINED_OFFSET,
-                this@MainMethodGenerationLowering.context.ir.symbols.suspendFunctionN(0).typeWith(context.irBuiltIns.anyNType),
-                target.symbol,
-                typeArgumentsCount = 0,
-                reflectionTarget = null
-            )
-            if (args != null) {
-                reference.putValueArgument(0, irGet(args))
+    private fun IrBuilderWithScope.irRunSuspend(target: IrSimpleFunction, args: IrValueParameter?): IrExpression {
+        val backendContext = this@MainMethodGenerationLowering.context
+        return irBlock {
+            val wrapperConstructor = buildClass {
+                name = Name.special("<main-wrapper>")
+                visibility = JavaVisibilities.PACKAGE_VISIBILITY
+                modality = Modality.FINAL
+                origin = JvmLoweredDeclarationOrigin.FUNCTION_REFERENCE_IMPL
+            }.let { wrapper ->
+                +wrapper
+
+                wrapper.createImplicitParameterDeclarationWithWrappedDescriptor()
+
+                val lambdaSuperClass = backendContext.ir.symbols.lambdaClass
+                val functionClass = backendContext.ir.symbols.getJvmSuspendFunctionClass(0)
+
+                wrapper.superTypes += lambdaSuperClass.defaultType
+                wrapper.superTypes += functionClass.typeWith(backendContext.irBuiltIns.anyNType)
+                wrapper.parent = target.parent
+
+                val stringArrayType = backendContext.irBuiltIns.arrayClass.typeWith(backendContext.irBuiltIns.stringType)
+                val argsField = args?.let { wrapper.addField("args", stringArrayType) }
+
+                wrapper.addFunction("invoke", backendContext.irBuiltIns.anyNType, isSuspend = true).also { invoke ->
+                    val invokeToOverride = functionClass.functions.single()
+
+                    invoke.overriddenSymbols += invokeToOverride
+                    invoke.body = backendContext.createIrBuilder(invoke.symbol).irBlockBody {
+                        +irReturn(irCall(target.symbol).also { call ->
+                            if (args != null) {
+                                call.putValueArgument(0, irGetField(irGet(invoke.dispatchReceiverParameter!!), argsField!!))
+                            }
+                        })
+                    }
+                }
+
+                wrapper.addConstructor {
+                    isPrimary = true
+                    visibility = JavaVisibilities.PACKAGE_VISIBILITY
+                }.also { constructor ->
+                    val superClassConstructor = lambdaSuperClass.owner.constructors.single()
+                    val param = args?.let { constructor.addValueParameter("args", stringArrayType) }
+
+                    constructor.body = backendContext.createIrBuilder(constructor.symbol).irBlockBody {
+                        +irDelegatingConstructorCall(superClassConstructor).also {
+                            it.putValueArgument(0, irInt(1))
+                        }
+                        if (args != null) {
+                            +irSetField(irGet(wrapper.thisReceiver!!), argsField!!, irGet(param!!))
+                        }
+                    }
+                }
             }
-            putValueArgument(0, reference)
+
+            +irCall(backendContext.ir.symbols.runSuspendFunction).apply {
+                putValueArgument(
+                    0, IrConstructorCallImpl.fromSymbolOwner(
+                        UNDEFINED_OFFSET,
+                        UNDEFINED_OFFSET,
+                        wrapperConstructor.returnType,
+                        wrapperConstructor.symbol
+                    ).also {
+                        if (args != null) {
+                            it.putValueArgument(0, irGet(args))
+                        }
+                    }
+                )
+            }
         }
+    }
 }
--- compiler/testData/codegen/bytecodeListing/main/multifileSuspend.kt
+++ compiler/testData/codegen/bytecodeListing/main/multifileSuspend.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND: JVM_IR
 // WITH_RUNTIME
 @file:JvmMultifileClass
 @file:JvmName("Foo")
