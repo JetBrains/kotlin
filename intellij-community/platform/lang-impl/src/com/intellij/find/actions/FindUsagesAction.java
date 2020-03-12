23a72996539c11f23f1696ee114242fe986dd8e5
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-03-12 15:10:54 +0300

    extract FindUsagesAction#findUsageTargetUsages
    intellij-monorepo: c5c8807e45693c408180e480d3e17c7437a48087

--- intellij-community/platform/lang-impl/src/com/intellij/find/actions/FindUsagesAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/find/actions/FindUsagesAction.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.find.actions;
 
 import com.intellij.CommonBundle;
@@ -24,6 +9,7 @@ import com.intellij.find.FindManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -37,6 +23,7 @@ import com.intellij.usages.UsageView;
 import org.jetbrains.annotations.NotNull;
 
 public class FindUsagesAction extends AnAction {
+
   public FindUsagesAction() {
     setInjectedContext(true);
   }
@@ -48,10 +35,13 @@ public class FindUsagesAction extends AnAction {
       return;
     }
     PsiDocumentManager.getInstance(project).commitAllDocuments();
+    findUsageTargetUsages(project, e.getDataContext());
+  }
 
-    UsageTarget[] usageTargets = e.getData(UsageView.USAGE_TARGETS_KEY);
+  private void findUsageTargetUsages(@NotNull Project project, @NotNull DataContext dataContext) {
+    UsageTarget[] usageTargets = dataContext.getData(UsageView.USAGE_TARGETS_KEY);
     if (usageTargets == null) {
-      final Editor editor = e.getData(CommonDataKeys.EDITOR);
+      final Editor editor = dataContext.getData(CommonDataKeys.EDITOR);
       chooseAmbiguousTargetAndPerform(project, editor, element -> {
         startFindUsages(element);
         return false;
@@ -64,7 +54,8 @@ public class FindUsagesAction extends AnAction {
         if (element != null) {
           startFindUsages(element);
         }
-      } else {
+      }
+      else {
         target.findUsages();
       }
     }
@@ -75,7 +66,7 @@ public class FindUsagesAction extends AnAction {
   }
 
   @Override
-  public void update(@NotNull AnActionEvent event){
+  public void update(@NotNull AnActionEvent event) {
     FindUsagesInFileAction.updateFindUsagesAction(event);
   }
 
@@ -88,7 +79,9 @@ public class FindUsagesAction extends AnAction {
     }
     else {
       int offset = editor.getCaretModel().getOffset();
-      boolean chosen = GotoDeclarationAction.chooseAmbiguousTarget(project, editor, offset, processor, FindBundle.message("find.usages.ambiguous.title"), null);
+      boolean chosen = GotoDeclarationAction.chooseAmbiguousTarget(
+        project, editor, offset, processor, FindBundle.message("find.usages.ambiguous.title"), null
+      );
       if (!chosen) {
         ApplicationManager.getApplication().invokeLater(() -> {
           if (editor.isDisposed() || !editor.getComponent().isShowing()) return;
