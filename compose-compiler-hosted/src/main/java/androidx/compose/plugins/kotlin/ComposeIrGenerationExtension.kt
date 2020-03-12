bc9aec47a07b1c56be4db3d15b1aac920211e016
Leland Richardson <lelandr@google.com>
2020-03-11 19:07:23 -0700

    Add ControlFlowTransformer WIP and tests
    Change-Id: I1f96ab3e11e3f26bf4f9e375e00420d25463510d ( https://android-review.git.corp.google.com/q/I1f96ab3e11e3f26bf4f9e375e00420d25463510d )

--- compose-compiler-hosted/src/main/java/androidx/compose/plugins/kotlin/ComposeIrGenerationExtension.kt
+++ compose-compiler-hosted/src/main/java/androidx/compose/plugins/kotlin/ComposeIrGenerationExtension.kt
@@ -22,6 +22,7 @@ import androidx.compose.plugins.kotlin.compiler.lower.ComposeResolutionMetadataT
 import androidx.compose.plugins.kotlin.compiler.lower.ComposerIntrinsicTransformer
 import androidx.compose.plugins.kotlin.compiler.lower.ComposerLambdaMemoization
 import androidx.compose.plugins.kotlin.compiler.lower.ComposerParamTransformer
+import androidx.compose.plugins.kotlin.compiler.lower.ControlFlowTransformer
 import androidx.compose.plugins.kotlin.frames.FrameIrTransformer
 import org.jetbrains.kotlin.backend.common.BackendContext
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
@@ -104,6 +105,10 @@ class ComposeIrGenerationExtension : IrGenerationExtension {
             // previous transform
             if (transforms and ComposeTransforms.INTRINSICS != 0) {
                 ComposerIntrinsicTransformer(jvmContext).lower(module)
+            }
+
+            if (transforms and ComposeTransforms.CONTROL_FLOW_GROUPS != 0) {
+                ControlFlowTransformer(jvmContext, symbolRemapper, bindingTrace).lower(module)
             }
 
             // transform composable calls and emits into their corresponding calls appealing
