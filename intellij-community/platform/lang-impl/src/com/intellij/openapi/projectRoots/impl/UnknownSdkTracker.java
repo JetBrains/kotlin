d0cdd3e67280e5cb6b052ab9d16c27e7ba1263aa
Eugene Petrenko <eugene.petrenko@gmail.com>
2020-03-12 19:20:05 +0100

    UnknownSdkTracker - fix a typoe, ensure MergingUpdateQueue works as expected, do less checks
    intellij-monorepo: 1639863b79fbd03b8bd43cd853cbfcc2833a5d1d

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTracker.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/UnknownSdkTracker.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.projectRoots.impl;
 
 import com.intellij.openapi.Disposable;
@@ -59,7 +59,7 @@ public class UnknownSdkTracker {
   }
 
   public void updateUnknownSdks() {
-    myUpdateQueue.run(new Update("update") {
+    myUpdateQueue.queue(new Update("update") {
       @Override
       public void run() {
         if (!Registry.is("unknown.sdk") || !UnknownSdkResolver.EP_NAME.hasAnyExtensions()) {
