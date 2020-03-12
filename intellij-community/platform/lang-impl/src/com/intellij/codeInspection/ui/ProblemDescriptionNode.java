029ed08f220e5acb65123abafbc70744c5fa4a0d
Anna.Kozlova <anna.kozlova@jetbrains.com>
2020-03-12 13:49:35 +0100

    inspection messages: ensure xml is escaped in html case only
    IDEA-235093; IDEA-186991; WEB-39648
    
    intellij-monorepo: 5f790e19805d1c35542151ab449f043294e8166b

--- intellij-community/platform/lang-impl/src/com/intellij/codeInspection/ui/ProblemDescriptionNode.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInspection/ui/ProblemDescriptionNode.java
@@ -14,7 +14,6 @@ import com.intellij.codeInspection.reference.RefElement;
 import com.intellij.codeInspection.reference.RefEntity;
 import com.intellij.lang.annotation.HighlightSeverity;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.profile.codeInspection.InspectionProfileManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ObjectUtils;
@@ -165,7 +164,7 @@ public class ProblemDescriptionNode extends SuppressableInspectionTreeNode {
     CommonProblemDescriptor descriptor = getDescriptor();
     if (descriptor == null) return "";
     PsiElement element = descriptor instanceof ProblemDescriptor ? ((ProblemDescriptor)descriptor).getPsiElement() : null;
-    String name = StringUtil.removeHtmlTags(ProblemDescriptorUtil.renderDescriptionMessage(descriptor, element, ProblemDescriptorUtil.TRIM_AT_TREE_END), true);
+    String name = ProblemDescriptorUtil.renderDescriptionMessage(descriptor, element, ProblemDescriptorUtil.TRIM_AT_TREE_END);
     return NAME_INTERNER.intern(name);
   }
 
