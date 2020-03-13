ddfc3d78d0a886cb4d72077f2499e983b7e7918a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-03-13 07:37:16 +0100

    add NotNull for icon class
    intellij-monorepo: 6651a21134be715b980de5de4d042914dd989989

--- intellij-community/platform/external-system-impl/src/icons/ExternalSystemIcons.java
+++ intellij-community/platform/external-system-impl/src/icons/ExternalSystemIcons.java
@@ -1,7 +1,8 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package icons;
 
 import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
@@ -10,9 +11,9 @@ import javax.swing.*;
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
 public final class ExternalSystemIcons {
-  private static Icon load(String path) {
+  private static @NotNull Icon load(@NotNull String path) {
     return IconManager.getInstance().getIcon(path, ExternalSystemIcons.class);
   }
 
-  /** 16x16 */ public static final Icon Task = load("/icons/task.svg");
+  /** 16x16 */ public static final @NotNull Icon Task = load("/icons/task.svg");
 }
