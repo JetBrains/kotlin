7933b06fdb37cf9a742458ca4a4f0c3e03bb27cb
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2020-03-13 14:41:38 +0100

    Fix ExtensionTestUtil compilation for 191; CustomUsageSearcherTest compilation for 201
    Relates to #KT-36657

--- idea/tests/org/jetbrains/kotlin/findUsages/CustomUsageSearcherTest.kt
+++ idea/tests/org/jetbrains/kotlin/findUsages/CustomUsageSearcherTest.kt
@@ -7,24 +7,25 @@ package org.jetbrains.kotlin.findUsages
 
 import com.intellij.find.findUsages.CustomUsageSearcher
 import com.intellij.find.findUsages.FindUsagesOptions
-import com.intellij.openapi.application.runReadAction
 import com.intellij.psi.PsiElement
-import com.intellij.testFramework.ExtensionTestUtil
 import com.intellij.usageView.UsageInfo
 import com.intellij.usages.Usage
 import com.intellij.usages.UsageInfo2UsageAdapter
-import com.intellij.util.Processor
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.maskExtensions
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCaseBase
+import org.jetbrains.kotlin.idea.util.application.runReadAction
+
 
 class CustomUsageSearcherTest : KotlinLightCodeInsightFixtureTestCaseBase() {
+
     fun testAddCustomUsagesForKotlin() {
         val customUsageSearcher = object : CustomUsageSearcher() {
-            override fun processElementUsages(element: PsiElement, processor: Processor<Usage>, options: FindUsagesOptions) {
+            override fun processElementUsages(element: PsiElement, processor: ProcessorInCompat<Usage>, options: FindUsagesOptions) {
                 runReadAction { processor.process(UsageInfo2UsageAdapter(UsageInfo(element))) }
             }
         }
-        ExtensionTestUtil.maskExtensions(CustomUsageSearcher.EP_NAME, listOf(customUsageSearcher), testRootDisposable)
+        maskExtensions(CustomUsageSearcher.EP_NAME, listOf(customUsageSearcher), testRootDisposable)
         myFixture.configureByText(KotlinFileType.INSTANCE, """val <caret>selfUsed = 1""")
 
         val usages = myFixture.getUsageViewTreeTextRepresentation(myFixture.elementAtCaret)
