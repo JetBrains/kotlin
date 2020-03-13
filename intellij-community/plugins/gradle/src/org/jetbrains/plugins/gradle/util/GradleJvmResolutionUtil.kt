da74b299aa8adc914c25ebda6682be15049dcd29
Sergei Vorobyov <Sergey.Vorobyov@jetbrains.com>
2020-03-13 15:48:00 +0300

    IDEA-CR-59127: extracted gradle java home validation to background startup activity
    intellij-monorepo: a41dd2d228f92d79bd92beebc7f095529e2f37c4

--- intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleJvmResolutionUtil.kt
+++ intellij-community/plugins/gradle/src/org/jetbrains/plugins/gradle/util/GradleJvmResolutionUtil.kt
@@ -1,14 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 @file:JvmName("GradleJvmResolutionUtil")
+@file:ApiStatus.Internal
 
 package org.jetbrains.plugins.gradle.util
 
-import com.intellij.ide.impl.ProjectViewSelectInTarget
-import com.intellij.ide.projectView.impl.ProjectViewPane
-import com.intellij.ide.util.EditorHelper
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationListener
-import com.intellij.notification.NotificationType.WARNING
 import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUtil
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
@@ -16,15 +11,13 @@ import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.util.io.FileUtil
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.psi.PsiManager
-import com.intellij.util.lang.JavaVersion
 import org.gradle.util.GradleVersion
-import org.jetbrains.plugins.gradle.service.project.GradleNotification.NOTIFICATION_GROUP
+import org.jetbrains.annotations.ApiStatus
 import org.jetbrains.plugins.gradle.settings.GradleProjectSettings
 import org.jetbrains.plugins.gradle.settings.GradleSettings
-import org.jetbrains.plugins.gradle.util.GradleProperties.GradleProperty
-import javax.swing.event.HyperlinkEvent
+import org.jetbrains.plugins.gradle.startup.GradleProjectSettingsValidator.Companion.JavaHomeValidationStatus.Success
+import org.jetbrains.plugins.gradle.startup.GradleProjectSettingsValidator.Companion.isSupported
+import org.jetbrains.plugins.gradle.startup.GradleProjectSettingsValidator.Companion.validateGradleJavaHome
 
 const val JAVA_HOME = "JAVA_HOME"
 
@@ -66,11 +59,6 @@ private class GradleJvmResolutionContext(
   val projectSdk: Sdk? by lazy { projectSdk ?: ProjectRootManager.getInstance(project).projectSdk }
 }
 
-sealed class FailureReason {
-  object Invalid : FailureReason()
-  data class Unsupported(val javaVersion: JavaVersion, val gradleVersion: GradleVersion) : FailureReason()
-}
-
 private fun GradleJvmResolutionContext.resolveReference(sdk: Sdk?): String? {
   return when (sdk) {
     null -> null
@@ -79,38 +67,17 @@ private fun GradleJvmResolutionContext.resolveReference(sdk: Sdk?): String? {
   }
 }
 
-private fun GradleJvmResolutionContext.checkGradleJvm(javaHome: String): FailureReason? {
-  if (!ExternalSystemJdkUtil.isValidJdk(javaHome)) return FailureReason.Invalid
-  val javaSdkType = ExternalSystemJdkUtil.getJavaSdkType()
-  val versionString = javaSdkType.getVersionString(javaHome) ?: return FailureReason.Invalid
-  val javaVersion = JavaVersion.tryParse(versionString) ?: return FailureReason.Invalid
-  if (!isSupported(versionString)) return FailureReason.Unsupported(javaVersion, gradleVersion)
-  return null
-}
-
 private fun GradleJvmResolutionContext.isValidAndSupported(homePath: String): Boolean {
   if (!ExternalSystemJdkUtil.isValidJdk(homePath)) return false
   val javaSdkType = ExternalSystemJdkUtil.getJavaSdkType()
   val versionString = javaSdkType.getVersionString(homePath) ?: return false
-  return isSupported(versionString)
+  return isSupported(gradleVersion, versionString)
 }
 
 private fun GradleJvmResolutionContext.isValidAndSupported(sdk: Sdk): Boolean {
   if (!ExternalSystemJdkUtil.isValidJdk(sdk)) return false
   val versionString = sdk.versionString ?: return false
-  return isSupported(versionString)
-}
-
-private fun GradleJvmResolutionContext.isSupported(versionString: String): Boolean {
-  val version = JavaVersion.tryParse(versionString) ?: return false
-  return when {
-    gradleVersion >= GradleVersion.version("6.0") -> version.feature in 8..13
-    gradleVersion >= GradleVersion.version("5.4.1") -> version.feature in 8..12
-    gradleVersion >= GradleVersion.version("5.0") -> version.feature in 8..11
-    gradleVersion >= GradleVersion.version("4.1") -> version.feature in 7..9
-    gradleVersion >= GradleVersion.version("4.0") -> version.feature in 7..8
-    else -> version.feature in 6..8
-  }
+  return isSupported(gradleVersion, versionString)
 }
 
 private fun GradleJvmResolutionContext.resolvePossibleGradleJvms(): List<Sdk> {
@@ -150,25 +117,14 @@ private fun GradleJvmResolutionContext.getOrAddGradleJavaHomeJdkReference(): Sdk
   val properties = getGradleProperties(externalProjectPath)
   val javaHomeProperty = properties.javaHomeProperty ?: return null
   val javaHome = javaHomeProperty.value
-  val failureReason = checkGradleJvm(javaHome)
-  if (failureReason != null) {
-    notifyInvalidGradleJavaHomeWarning(javaHomeProperty, failureReason)
-  }
   return findOrAddJdk(javaHome)
 }
 
 private fun GradleJvmResolutionContext.getOrAddEnvJavaHomeJdkReference(): Sdk? {
   val javaHome = Environment.getEnvVariable(JAVA_HOME)
-  if (javaHome == null) {
-    notifyUndefinedJavaHomeWarning()
-    return null
-  }
-  val failureReason = checkGradleJvm(javaHome)
-  if (failureReason != null) {
-    notifyInvalidJavaHomeWarning(failureReason)
-    return null
-  }
-  return findOrAddGradleJdk(javaHome)
+  val validationStatus = validateGradleJavaHome(gradleVersion, javaHome)
+  if (validationStatus !is Success) return null
+  return findOrAddGradleJdk(validationStatus.javaHome)
 }
 
 private fun GradleJvmResolutionContext.getProjectJdkReference(): Sdk? {
@@ -191,51 +147,4 @@ private fun GradleJvmResolutionContext.getAndAddExternalJdkReference(): Sdk? {
     .map { it to javaSdkType.getVersionString(it) }
     .maxWith(Comparator { (_, v1), (_, v2) -> versionComparator.compare(v1, v2) })
     ?.let { (homePath, _) -> findOrAddGradleJdk(homePath) }
-}
-
-private fun GradleJvmResolutionContext.notifyInvalidGradleJavaHomeWarning(javaHomeProperty: GradleProperty<String>, reason: FailureReason) {
-  val propertyLocation = createLinkToFile(javaHomeProperty.location)
-  val notificationContent = GradleBundle.message("gradle.notifications.java.home.property.content", propertyLocation)
-  notifyInvalidGradleJvmWarning(notificationContent, reason)
-}
-
-private fun GradleJvmResolutionContext.notifyInvalidJavaHomeWarning(reason: FailureReason) {
-  val notificationContent = GradleBundle.message("gradle.notifications.java.home.variable.content")
-  notifyInvalidGradleJvmWarning(notificationContent, reason)
-}
-
-private fun GradleJvmResolutionContext.createLinkToFile(path: String): String {
-  val presentablePath = when {
-    FileUtil.isAncestor(externalProjectPath, path, true) -> FileUtil.getRelativePath(externalProjectPath, path, '/')
-    else -> FileUtil.getLocationRelativeToUserHome(path)
-  }
-  return "<a href='$path'>$presentablePath</a>"
-}
-
-private fun GradleJvmResolutionContext.notifyInvalidGradleJvmWarning(notificationHint: String, reason: FailureReason) {
-  val notificationTitle = GradleBundle.message("gradle.notifications.java.home.invalid.title")
-  var notificationContent = notificationHint
-  if (reason is FailureReason.Unsupported) {
-    val javaVersion = reason.javaVersion.toString()
-    val gradleVersion = reason.gradleVersion.version
-    val additionalFailureHint = GradleBundle.message("gradle.notifications.java.home.unsupported.content", javaVersion, gradleVersion)
-    notificationContent = "$additionalFailureHint $notificationContent"
-  }
-  val hyperLinkProcessor = object : NotificationListener.Adapter() {
-    override fun hyperlinkActivated(notification: Notification, e: HyperlinkEvent) {
-      val file = LocalFileSystem.getInstance().findFileByPath(e.description) ?: return
-      ProjectViewSelectInTarget.select(project, file, ProjectViewPane.ID, null, file, true)
-      val psiFile = PsiManager.getInstance(project).findFile(file) ?: return
-      EditorHelper.openInEditor(psiFile)
-    }
-  }
-  val notification = NOTIFICATION_GROUP.createNotification(notificationTitle, notificationContent, WARNING, hyperLinkProcessor)
-  notification.notify(project)
-}
-
-private fun GradleJvmResolutionContext.notifyUndefinedJavaHomeWarning() {
-  val notificationTitle = GradleBundle.message("gradle.notifications.java.home.undefined.title")
-  val notificationContent = GradleBundle.message("gradle.notifications.java.home.undefined.content")
-  val notification = NOTIFICATION_GROUP.createNotification(notificationTitle, notificationContent, WARNING)
-  notification.notify(project)
 }
