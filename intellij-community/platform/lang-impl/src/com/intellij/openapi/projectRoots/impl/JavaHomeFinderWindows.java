bfa6dfd77d2131c522831ff6d97dc61289322ba9
Leonid Bushuev <leonid.bushuev@jetbrains.com>
2020-03-13 22:48:03 +0300

    Convert class JavaHomeFinderWindows to Kotlin (convert the content)
    intellij-monorepo: 578d8c758235de2c0d2474b51cbdee33db4839ec

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/JavaHomeFinderWindows.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/projectRoots/impl/JavaHomeFinderWindows.java
@@ -1,29 +1,21 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.intellij.openapi.projectRoots.impl;
+package com.intellij.openapi.projectRoots.impl
 
-import java.io.File;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.File
+import java.util.*
 
-class JavaHomeFinderWindows extends JavaHomeFinderBasic {
-  JavaHomeFinderWindows(boolean forceEmbeddedJava) {
-    super(forceEmbeddedJava);
-
-    registerFinder(() -> {
-      File[] fsRoots = File.listRoots();
-      if (fsRoots == null) return Collections.emptySet();
-
-      Set<File> roots = new HashSet<>();
-      for (File root : fsRoots) {
-        if (!root.exists()) continue;
-
-        roots.add(new File(new File(root, "Program Files"), "Java"));
-        roots.add(new File(new File(root, "Program Files (x86)"), "Java"));
-        roots.add(new File(root, "Java"));
+internal class JavaHomeFinderWindows : JavaHomeFinderBasic {
+  constructor(forceEmbeddedJava: Boolean) : super(forceEmbeddedJava) {
+    registerFinder {
+      val fsRoots = File.listRoots() ?: return@registerFinder emptySet<String>()
+      val roots: MutableSet<File> = HashSet()
+      for (root in fsRoots) {
+        if (!root.exists()) continue
+        roots.add(File(File(root, "Program Files"), "Java"))
+        roots.add(File(File(root, "Program Files (x86)"), "Java"))
+        roots.add(File(root, "Java"))
       }
-
-      return scanAll(roots, true);
-    });
+      scanAll(roots, true)
+    }
   }
 }
