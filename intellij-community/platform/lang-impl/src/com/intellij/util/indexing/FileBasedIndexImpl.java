9ce2244388f398a3ffe2b5764a8070ef22472d42
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-03-13 08:21:41 +0300

    shared indexes: synchronize lifecycle of SharedIndexChunkConfiguration with FileBasedIndexImpl
    intellij-monorepo: 5ef199769712cbe0d055db8c716b40eec8dc27f9

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexImpl.java
@@ -300,6 +300,7 @@ public final class FileBasedIndexImpl extends FileBasedIndexEx {
     myStorageBufferingHandler.resetState();
     myRegisteredIndexes = new RegisteredIndexes(myFileDocumentManager, this);
     myRegisteredIndexes.initializeIndexes(new FileBasedIndexDataInitialization(this));
+    FileBasedIndexInfrastructureExtension.EP_NAME.extensions().forEach(ex -> ex.initialize());
   }
 
   @Override
@@ -489,7 +490,7 @@ public final class FileBasedIndexImpl extends FileBasedIndexEx {
           }
         }
 
-        FileBasedIndexInfrastructureExtension.EP_NAME.extensions().forEach(ex -> ex.performShutdown());
+        FileBasedIndexInfrastructureExtension.EP_NAME.extensions().forEach(ex -> ex.shutdown());
         IndexedHashesSupport.flushContentHashes();
         SharedIndicesData.flushData();
         if (!keepConnection) {
--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexInfrastructureExtension.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/FileBasedIndexInfrastructureExtension.java
@@ -68,11 +68,20 @@ public interface FileBasedIndexInfrastructureExtension {
   void onStubIndexVersionChanged(@NotNull StubIndexKey<?, ?> indexId);
 
   /**
+   * Executed when IntelliJ is open it's indexes (IDE start or plugin load/unload).
+   * All necessarily needed connections and resources should be open here.
+   *
+   * This method and {@link FileBasedIndexInfrastructureExtension#shutdown()} synchronize
+   * lifecycle of an extension with {@link FileBasedIndexImpl}.
+   **/
+  void initialize();
+
+  /**
    * Executed when IntelliJ is shutting down it's indexes (IDE shutdown or plugin load/unload). It is the best time
    * for the component to flush it's state to the disk and close all pending connections.
-   * TODO: Meanwhile, the implementation should be able to re-open necessary connections if
-   * TODO: other methods of the interface are executed after the call to {@link #performShutdown()}
+   *
+   * This method and {@link FileBasedIndexInfrastructureExtension#initialize()} synchronize
+   * lifecycle of an extension with {@link FileBasedIndexImpl}.
    */
-  void performShutdown();
-
+  void shutdown();
 }
