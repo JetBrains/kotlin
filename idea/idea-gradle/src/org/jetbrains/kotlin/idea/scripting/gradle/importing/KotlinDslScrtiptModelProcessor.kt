c23da8c360ffa4a0d8be755c9c3d89f71c20a6a3
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-03-17 13:24:04 +0300

    201: KotlinDslScriptsModel: use ProjectModelContributor to process models
    Instead of populateProjectExtraModels in Resolver

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslScrtiptModelProcessor.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslScrtiptModelProcessor.kt
@@ -8,6 +8,9 @@ package org.jetbrains.kotlin.idea.scripting.gradle.importing
 import com.intellij.openapi.components.service
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
+import org.gradle.tooling.model.kotlin.dsl.EditorReportSeverity
+import org.gradle.tooling.model.kotlin.dsl.KotlinDslScriptsModel
+import org.jetbrains.kotlin.idea.KotlinIdeaGradleBundle
 import org.jetbrains.kotlin.idea.core.script.ScriptConfigurationManager
 import org.jetbrains.kotlin.idea.core.script.configuration.cache.CachedConfigurationInputs
 import org.jetbrains.kotlin.idea.core.script.configuration.cache.ScriptConfigurationSnapshot
@@ -23,6 +26,74 @@ import kotlin.script.experimental.api.*
 import kotlin.script.experimental.jvm.JvmDependency
 import kotlin.script.experimental.jvm.jdkHome
 import kotlin.script.experimental.jvm.jvm
+
+fun processScriptModel(
+    resolverCtx: ProjectResolverContext,
+    model: KotlinDslScriptsModel,
+    projectName: String
+) {
+    if (model is BrokenKotlinDslScriptsModel) {
+        LOG.error(
+            "Couldn't get KotlinDslScriptsModel for $projectName:\n${model.message}\n${model.stackTrace}"
+        )
+    } else {
+        val models = model.toListOfScriptModels()
+
+        saveScriptModels(resolverCtx, models)
+
+        if (models.containsErrors()) {
+            throw IllegalStateException(KotlinIdeaGradleBundle.message("title.kotlin.build.script"))
+        }
+    }
+}
+
+private fun Collection<KotlinDslScriptModel>.containsErrors(): Boolean {
+    return any { it.messages.any { it.severity == KotlinDslScriptModel.Severity.ERROR } }
+}
+
+private fun KotlinDslScriptsModel.toListOfScriptModels(): List<KotlinDslScriptModel> =
+    scriptModels.map { (file, model) ->
+        val messages = mutableListOf<KotlinDslScriptModel.Message>()
+
+        model.exceptions.forEach {
+            val fromException = parsePositionFromException(it)
+            if (fromException != null) {
+                val (filePath, _) = fromException
+                if (filePath != file.path) return@forEach
+            }
+            messages.add(
+                KotlinDslScriptModel.Message(
+                    KotlinDslScriptModel.Severity.ERROR,
+                    it.substringBefore(System.lineSeparator()),
+                    it,
+                    fromException?.second
+                )
+            )
+        }
+
+        model.editorReports.forEach {
+            messages.add(
+                KotlinDslScriptModel.Message(
+                    when (it.severity) {
+                        EditorReportSeverity.WARNING -> KotlinDslScriptModel.Severity.WARNING
+                        else -> KotlinDslScriptModel.Severity.ERROR
+                    },
+                    it.message,
+                    position = KotlinDslScriptModel.Position(it.position?.line ?: 0, it.position?.column ?: 0)
+                )
+            )
+        }
+
+        // todo(KT-34440): take inputs snapshot before starting import
+        KotlinDslScriptModel(
+            file.absolutePath,
+            System.currentTimeMillis(),
+            model.classPath.map { it.absolutePath },
+            model.sourcePath.map { it.absolutePath },
+            model.implicitImports,
+            messages
+        )
+    }
 
 fun saveScriptModels(
     resolverContext: ProjectResolverContext,
