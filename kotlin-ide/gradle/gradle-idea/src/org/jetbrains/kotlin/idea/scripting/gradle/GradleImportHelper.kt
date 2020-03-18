bcfdf85e04e8b1da20246bd682fc9a56a94cb6dc
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-03-18 10:39:03 +0300

    201: Run partial import in IDEA 2020.1 to get script configurations
    It only requests for KotlinDslScriptsModel
    ^KT-34481 Fixed
    
    Original commit: 426c0c9ff0e4f5a9fce6fad4597097ca696aad68

---
+++ kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/scripting/gradle/GradleImportHelper.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+package org.jetbrains.kotlin.idea.scripting.gradle
+
+import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
+import com.intellij.openapi.project.Project
+import org.jetbrains.kotlin.idea.scripting.gradle.importing.KotlinDslScriptModelResolver
+import org.jetbrains.plugins.gradle.service.project.GradleIncrementalResolverPolicy
+import org.jetbrains.plugins.gradle.util.GradleConstants
+
+fun runPartialGradleImport(project: Project) {
+    val gradleSettings = ExternalSystemApiUtil.getSettings(project, GradleConstants.SYSTEM_ID)
+    val projectSettings = gradleSettings.getLinkedProjectsSettings()
+        .filterIsInstance<GradleProjectSettings>()
+        .firstOrNull() ?: return
+
+    ExternalSystemUtil.refreshProject(
+        projectSettings.externalProjectPath,
+        ImportSpecBuilder(project, GradleConstants.SYSTEM_ID)
+            .projectResolverPolicy(
+                GradleIncrementalResolverPolicy {
+                    it is KotlinDslScriptModelResolver
+                }
+            )
+    )
+}
