e4e905f27e8fbd0b97b7909af63d57802d240df5
Alexander Zolotov <zolotov@jetbrains.com>
2020-03-18 22:12:00 +0300

    Document file path format for live template extension point
    intellij-monorepo: d381232cbd537344455c62059514e9e952205976

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/DefaultLiveTemplateEP.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/DefaultLiveTemplateEP.kt
@@ -15,7 +15,10 @@ import com.intellij.util.xmlb.annotations.Attribute
 class DefaultLiveTemplateEP : AbstractExtensionPointBean() {
 
   /**
-   * Path to resource (e.g. `/templates/customTemplates.xml`). `.xml` extension can be omitted.
+   * Relative path to resource (e.g. `templates/customTemplates.xml`). `.xml` extension can be omitted.
+   *
+   * Note, that even though absolute path will work, it heavily depends on class-loader that loads resource,
+   * so it's better to use relative path without leading slash since it will work for any class-loader.
    */
   @Attribute("file")
   @RequiredElement
