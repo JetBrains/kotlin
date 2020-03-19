73ca9d1b5410e52fe6de75ca37220a3a364935dd
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-03-19 15:07:41 +0300

    build.gradle.kts: report error to build tool window
    Move errors found in KotlinDslScriptModels from Messages Toolwindow to Build Toolwindow

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslScrtiptModelProcessor.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslScrtiptModelProcessor.kt
@@ -5,19 +5,9 @@
 
 package org.jetbrains.kotlin.idea.scripting.gradle.importing
 
-import com.intellij.build.SyncViewManager
 import com.intellij.openapi.components.service
-import com.intellij.openapi.externalSystem.service.execution.ExternalSystemEventDispatcher
-import com.intellij.openapi.externalSystem.service.notification.ExternalSystemNotificationManager
-import com.intellij.openapi.externalSystem.service.notification.NotificationCategory
-import com.intellij.openapi.externalSystem.service.notification.NotificationData
-import com.intellij.openapi.externalSystem.service.notification.NotificationSource
-import com.intellij.openapi.fileEditor.OpenFileDescriptor
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.pom.Navigatable
-import org.jetbrains.kotlin.idea.KotlinIdeaGradleBundle
 import org.jetbrains.kotlin.idea.core.script.ScriptConfigurationManager
 import org.jetbrains.kotlin.idea.core.script.configuration.cache.CachedConfigurationInputs
 import org.jetbrains.kotlin.idea.core.script.configuration.cache.ScriptConfigurationSnapshot
@@ -29,7 +19,6 @@ import org.jetbrains.kotlin.scripting.resolve.ScriptCompilationConfigurationWrap
 import org.jetbrains.kotlin.scripting.resolve.VirtualFileScriptSource
 import org.jetbrains.kotlin.scripting.resolve.adjustByDefinition
 import org.jetbrains.plugins.gradle.service.project.ProjectResolverContext
-import org.jetbrains.plugins.gradle.util.GradleConstants
 import java.io.File
 import kotlin.script.experimental.api.*
 import kotlin.script.experimental.jvm.JvmDependency
@@ -46,19 +35,17 @@ fun saveScriptModels(
 
     val scriptConfigurations = mutableListOf<Pair<VirtualFile, ScriptConfigurationSnapshot>>()
 
-    val syncViewManager = project.service<SyncViewManager>()
-    val buildEventDispatcher =
-        ExternalSystemEventDispatcher(task, syncViewManager)
+    val errorReporter = KotlinGradleDslErrorReporter(project, task)
 
     val javaHome = settings.javaHome?.let { File(it) }
-    models.forEach { buildScript ->
-        val scriptFile = File(buildScript.file)
+    models.forEach { model ->
+        val scriptFile = File(model.file)
         val virtualFile = VfsUtil.findFile(scriptFile.toPath(), true)!!
 
         val inputs = getGradleScriptInputsStamp(
             project,
             virtualFile,
-            givenTimeStamp = buildScript.inputsTimeStamp
+            givenTimeStamp = model.inputsTimeStamp
         )
 
         val definition = virtualFile.findScriptDefinition(project) ?: return@forEach
@@ -68,13 +55,13 @@ fun saveScriptModels(
                 if (javaHome != null) {
                     jvm.jdkHome(javaHome)
                 }
-                defaultImports(buildScript.imports)
-                dependencies(JvmDependency(buildScript.classPath.map {
+                defaultImports(model.imports)
+                dependencies(JvmDependency(model.classPath.map {
                     File(
                         it
                     )
                 }))
-                ide.dependenciesSources(JvmDependency(buildScript.sourcePath.map {
+                ide.dependenciesSources(JvmDependency(model.sourcePath.map {
                     File(
                         it
                     )
@@ -96,9 +83,7 @@ fun saveScriptModels(
             ),
         )
 
-        buildScript.messages.forEach {
-            addBuildScriptDiagnosticMessage(it, virtualFile, project)
-        }
+        errorReporter.reportError(scriptFile, model)
     }
 
     saveGradleProjectRootsAfterImport(
@@ -107,51 +92,4 @@ fun saveScriptModels(
 
     project.service<ScriptConfigurationManager>().saveCompilationConfigurationAfterImport(scriptConfigurations)
     project.service<GradleScriptInputsWatcher>().clearState()
-}
-
-private fun addBuildScriptDiagnosticMessage(
-    message: KotlinDslScriptModel.Message,
-    virtualFile: VirtualFile,
-    project: Project
-) {
-    val notification = NotificationData(
-        KotlinIdeaGradleBundle.message("title.kotlin.build.script"),
-        message.text,
-        when (message.severity) {
-            KotlinDslScriptModel.Severity.WARNING -> NotificationCategory.WARNING
-            KotlinDslScriptModel.Severity.ERROR -> NotificationCategory.ERROR
-        },
-        NotificationSource.PROJECT_SYNC
-    )
-
-    notification.navigatable =
-        LazyNavigatable(
-            virtualFile,
-            project,
-            message.position
-        )
-
-    ExternalSystemNotificationManager.getInstance(project).showNotification(
-        GradleConstants.SYSTEM_ID,
-        notification
-    )
-}
-
-class LazyNavigatable internal constructor(
-    private val virtualFile: VirtualFile,
-    private val project: Project,
-    val position: KotlinDslScriptModel.Position?
-) : Navigatable {
-    private val openFileDescriptor: Navigatable by lazy {
-        if (position != null) OpenFileDescriptor(project, virtualFile, position.line, position.column)
-        else OpenFileDescriptor(project, virtualFile, -1)
-    }
-
-    override fun navigate(requestFocus: Boolean) {
-        if (openFileDescriptor.canNavigate()) openFileDescriptor.navigate(requestFocus)
-    }
-
-    override fun canNavigate(): Boolean = virtualFile.exists()
-
-    override fun canNavigateToSource(): Boolean = canNavigate()
 }
