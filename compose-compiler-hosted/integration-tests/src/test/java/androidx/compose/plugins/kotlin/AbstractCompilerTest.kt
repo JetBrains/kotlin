6ac202eb8b17c6ec147be4d43c77f8120cb7bf47
Aurimas Liutikas <aurimas@google.com>
2020-03-20 01:04:38 +0000

    Revert "Attempt to update to JDK 11."
    This reverts commit f3132ea237f2cbc84543317a0d05e6bfc04f6e56.
    
    Reason for revert: This seems to break Android Studio
    
    Change-Id: I4e9b4ff91faf09ac79087f3eca1c638c62a9c5ce ( https://android-review.googlesource.com/q/I4e9b4ff91faf09ac79087f3eca1c638c62a9c5ce )
    
    Moved from: https://github.com/androidx/androidx/commit/1dcad3fb4cb22bf6fd3d59c6ff934c5b8e62f84b

--- compose-compiler-hosted/integration-tests/src/test/java/androidx/compose/plugins/kotlin/AbstractCompilerTest.kt
+++ compose-compiler-hosted/integration-tests/src/test/java/androidx/compose/plugins/kotlin/AbstractCompilerTest.kt
@@ -245,11 +245,7 @@ abstract class AbstractCompilerTest : TestCase() {
 }
 
 private fun systemClassLoaderJars(): List<File> {
-    val classpath = System.getProperty("java.class.path")!!.split(
-        System.getProperty("path.separator")!!
-    )
-    val urls = classpath.map { URL("file://$it") }
-    val result = URLClassLoader(urls.toTypedArray()).urLs?.filter {
+    val result = (ClassLoader.getSystemClassLoader() as? URLClassLoader)?.urLs?.filter {
         it.protocol == "file"
     }?.map {
         File(it.path)
