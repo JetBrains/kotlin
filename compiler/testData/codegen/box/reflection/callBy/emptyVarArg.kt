e4c088cfa00ec4ccc3c85f67d04f29a8f511ea72
Mathias Quintero <me@quintero.io>
2020-03-20 10:31:51 +0100

    Updating tests to include arrays of primitive types as well

--- compiler/testData/codegen/box/reflection/callBy/emptyVarArg.kt
+++ compiler/testData/codegen/box/reflection/callBy/emptyVarArg.kt
@@ -8,8 +8,12 @@ import kotlin.test.assertEquals
 
 fun join(vararg strings: String) = strings.toList().joinToString("")
 
+fun sum(vararg bytes: Byte) = bytes.toList().fold(0) { acc, el -> acc + el }
+
 fun box(): String {
-    val f = ::join
-    assertEquals("", f.callBy(emptyMap()))
+    val j = ::join
+    val s = ::sum
+    assertEquals("", j.callBy(emptyMap()))
+    assertEquals(0, s.callBy(emptyMap()))
     return "OK"
 }
--- compiler/testData/codegen/box/reflection/callBy/emptyVarArgAnnotationConstructor.kt
+++ compiler/testData/codegen/box/reflection/callBy/emptyVarArgAnnotationConstructor.kt
@@ -10,9 +10,16 @@ import kotlin.test.assert
 
 annotation class Foo(vararg val strings: String)
 
+annotation class Bar(vararg val bytes: Byte)
+
 fun box(): String {
-    val constructor = Foo::class.primaryConstructor!!
-    val annotation = constructor.callBy(emptyMap())
-    assert(annotation.strings.isEmpty())
+    val fooConstructor = Foo::class.primaryConstructor!!
+    val foo = fooConstructor.callBy(emptyMap())
+    assert(foo.strings.isEmpty())
+
+    val barConstructor = Bar::class.primaryConstructor!!
+    val bar = barConstructor.callBy(emptyMap())
+    assert(bar.bytes.isEmpty())
+
     return "OK"
 }
