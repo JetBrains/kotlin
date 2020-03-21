2e8f73adbac958bafc4d38a682dbcb3308c65ee3
rapturemain <rapture.main@gmail.com>
2020-03-21 15:37:11 +0300

    [FIR] Change errors location from Errors.java to FirErrors.kt

--- compiler/fir/analysis-tests/testData/resolve/visibility/exposedTypeAlias.kt
+++ compiler/fir/analysis-tests/testData/resolve/visibility/exposedTypeAlias.kt
@@ -2,7 +2,7 @@ class A {
     private inner class Inner
 }
 class B {
-    <!FIR_EXPOSED_TYPEALIAS_EXPANDED_TYPE!>typealias AInner = A.Inner<!>
+    <!EXPOSED_TYPEALIAS_EXPANDED_TYPE!>typealias AInner = A.Inner<!>
     inner class Inner
 }
 
@@ -10,5 +10,5 @@ class C {
     typealias BInner = B.Inner
 }
 
-<!FIR_EXPOSED_TYPEALIAS_EXPANDED_TYPE!>typealias AInner0 = A.Inner<!>
+<!EXPOSED_TYPEALIAS_EXPANDED_TYPE!>typealias AInner0 = A.Inner<!>
 typealias BInner0 = B.Inner
