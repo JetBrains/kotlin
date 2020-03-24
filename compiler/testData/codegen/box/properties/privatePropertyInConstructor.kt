de0c9a5c734ab5616cb382b327ee9bd40e8151d0
Jinseong Jeon <jsjeon@google.com>
2020-03-24 15:58:40 -0700

    FIR: use dispatch receiver of the enclosing function if any.

--- compiler/testData/codegen/box/properties/privatePropertyInConstructor.kt
+++ compiler/testData/codegen/box/properties/privatePropertyInConstructor.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND_FIR: JVM_IR
 class A(
         private val x: String,
         private var y: Double
--- compiler/testData/codegen/box/traits/kt36973.kt
+++ compiler/testData/codegen/box/traits/kt36973.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND_FIR: JVM_IR
 // FILE: kt36973.kt
 import other.*
 
