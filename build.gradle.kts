38b1a3d9f1ca09dd34458778b7b1c7aa65f4676f
Ilya Matveev <ilya.matveev@jetbrains.com>
2020-03-24 19:19:56 +0700

    Update K/N: 1.4-M2-dev-15039

--- build.gradle.kts
+++ build.gradle.kts
@@ -180,7 +180,7 @@ extra["versions.r8"] = "1.5.70"
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.4-M2-dev-14898"
+    extra["versions.kotlin-native"] = "1.4-M2-dev-15039"
 }
 
 val intellijUltimateEnabled by extra(project.kotlinBuildProperties.intellijUltimateEnabled)
