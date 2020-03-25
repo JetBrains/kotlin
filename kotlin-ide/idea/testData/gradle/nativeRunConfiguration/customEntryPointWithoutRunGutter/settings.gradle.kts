5c430a494bf9a716b879ec0ba98168e2c50d8b91
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2020-03-25 15:33:18 +0300

    Add tests for setup native run gutters.
    Original commit: d61e40e49be2012a442509c09cb0e4d7a1d2a28b

---
+++ kotlin-ide/idea/testData/gradle/nativeRunConfiguration/customEntryPointWithoutRunGutter/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+        maven("https://dl.bintray.com/kotlin/kotlin-dev")
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "test"
