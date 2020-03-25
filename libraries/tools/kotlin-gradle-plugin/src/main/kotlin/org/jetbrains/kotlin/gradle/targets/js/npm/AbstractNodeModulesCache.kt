be4c3018082bfcb5144e5666f122af652b587620
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-03-25 17:53:53 +0300

    [Gradle, JS] Abstractize GradleNodeModulesCache.kt
    #KT-32466 fixed
    #KT-36784 fixed
    #KT-36864 fixed

---
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/AbstractNodeModulesCache.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.targets.js.npm
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedArtifact
+import org.gradle.api.artifacts.ResolvedDependency
+import org.jetbrains.kotlin.gradle.internal.ProcessedFilesCache
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import java.io.File
+
+/**
+ * Cache for storing already created [GradleNodeModule]s
+ */
+internal abstract class AbstractNodeModulesCache(val nodeJs: NodeJsRootExtension) : AutoCloseable {
+    companion object {
+        const val STATE_FILE_NAME = ".visited"
+    }
+
+    val project: Project get() = nodeJs.rootProject
+    internal val dir = nodeJs.nodeModulesGradleCacheDir
+    private val cache = ProcessedFilesCache(project, dir, STATE_FILE_NAME, "9")
+
+    @Synchronized
+    fun get(
+        dependency: ResolvedDependency,
+        artifact: ResolvedArtifact
+    ): GradleNodeModule? = cache.getOrCompute(artifact.file) {
+        buildImportedPackage(dependency, artifact)
+    }?.let {
+        GradleNodeModule(it)
+    }
+
+    abstract fun buildImportedPackage(
+        dependency: ResolvedDependency,
+        artifact: ResolvedArtifact
+    ): File?
+
+    @Synchronized
+    override fun close() {
+        cache.close()
+    }
+}
