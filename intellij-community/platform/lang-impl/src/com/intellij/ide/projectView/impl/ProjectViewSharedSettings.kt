d27d245f8d725cdc04223d8a1b31a14cd6b98db5
Dmitry Jemerov <yole@jetbrains.com>
2020-03-25 17:11:07 +0100

    Report project view settings to FUS (FUS-456)
    intellij-monorepo: 774afef4df1785fafe9484b7b22ad9fccd7f9c25

--- intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewSharedSettings.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewSharedSettings.kt
@@ -10,7 +10,7 @@ import com.intellij.util.xmlb.XmlSerializerUtil
 /**
  * @author Konstantin Bulenkov
  */
-@State(name = "ProjectViewSharedSettings", storages = [(Storage(value = "projectView.xml"))])
+@State(name = "ProjectViewSharedSettings", storages = [(Storage(value = "projectView.xml"))], reportStatistic = true)
 class ProjectViewSharedSettings : PersistentStateComponent<ProjectViewSharedSettings> {
   var flattenPackages: Boolean = false
   var showMembers: Boolean = false
