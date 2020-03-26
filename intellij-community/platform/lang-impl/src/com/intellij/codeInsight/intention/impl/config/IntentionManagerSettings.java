5a8eccf7ebd8b45f2e00eb8592d6510290e8624d
Alexander Zolotov <zolotov@jetbrains.com>
2020-03-26 19:00:01 +0300

    Do not try to delete meta data if it wasn't added
    intellij-monorepo: bcfda8f5e1db1ca6566ae65b6939b2ce5063f52d

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/intention/impl/config/IntentionManagerSettings.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/intention/impl/config/IntentionManagerSettings.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.intention.impl.config;
 
 import com.intellij.codeInsight.intention.IntentionAction;
@@ -226,8 +226,9 @@ public final class IntentionManagerSettings implements PersistentStateComponent<
 
   private synchronized void unregisterMetaDataForEP(IntentionActionBean extension) {
     MetaDataKey key = myExtensionMapping.remove(extension);
-    assert key != null : extension;
-    myMetaData.remove(key);
+    if (key != null) {
+      myMetaData.remove(key);
+    }
   }
 
   private static class IntentionSearchableOptionContributor extends SearchableOptionContributor {
