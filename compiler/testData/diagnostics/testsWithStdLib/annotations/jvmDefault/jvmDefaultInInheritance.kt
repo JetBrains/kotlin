182d840e547a1ac1df8c27e961e921e4a8974247
Mikhail Bogdanov <mikhael.bogdanov@jetbrains.com>
2020-03-27 15:56:55 +0100

    Deprecate @JvmDefault
     #KT-40392 Fixed
    
    (cherry picked from commit c11f38688e271fcfd7e6781941a4bd7a0acc4b9a)
    (cherry picked from commit 95654bb9bcba358118ae8d1b8162cedf2069660c)

--- compiler/testData/diagnostics/testsWithStdLib/annotations/jvmDefault/jvmDefaultInInheritance.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/jvmDefault/jvmDefaultInInheritance.kt
@@ -1,7 +1,7 @@
 // !JVM_TARGET: 1.8
 
 interface A {
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     fun test()<!> {
     }
 }
@@ -28,7 +28,7 @@ class Bar2 : Foo(), A
 class Bar3 : Foo(), B
 
 open class BarWithJvmDefault : B {
-    <!JVM_DEFAULT_NOT_IN_INTERFACE!>@JvmDefault<!>
+    <!JVM_DEFAULT_NOT_IN_INTERFACE!>@<!DEPRECATION!>JvmDefault<!><!>
     override fun test() {
     }
 }
--- compiler/testData/diagnostics/testsWithStdLib/annotations/jvmDefault/noJvmDefaultFlag.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/jvmDefault/noJvmDefaultFlag.kt
@@ -2,27 +2,27 @@
 
 interface B {
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     fun test()<!> {}
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     abstract fun test2(s: String = "")<!>
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     abstract fun test3()<!>
 
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     abstract val prop: String<!>
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     abstract val prop2: String<!>
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     val prop3: String<!>
         get() = ""
 
-    <!JVM_DEFAULT_IN_DECLARATION!>@JvmDefault
+    <!JVM_DEFAULT_IN_DECLARATION!>@<!DEPRECATION!>JvmDefault<!>
     var prop4: String<!>
         get() = ""
         set(<!UNUSED_PARAMETER!>value<!>) {}
