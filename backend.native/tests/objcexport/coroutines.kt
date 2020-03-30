9163d8d5f33145540dd49febd2c22cc39ecd7f9f
SvyatoslavScherbina <Svyatoslav.Scherbina@jetbrains.com>
2020-03-30 16:26:31 +0300

    Fix objcexport test (#4037)
    Apparently compiler was using different copy of NativePlatformAnalyzerServices
    until recently, so it didn't include new frontend checkers for `@Throws`

--- backend.native/tests/objcexport/coroutines.kt
+++ backend.native/tests/objcexport/coroutines.kt
@@ -79,7 +79,7 @@ fun callSuspendFun(suspendFun: SuspendFun, doYield: Boolean, doThrow: Boolean, r
             .startCoroutine(ResultHolderCompletion(resultHolder))
 }
 
-@Throws
+@Throws(CoroutineException::class)
 suspend fun callSuspendFun2(suspendFun: SuspendFun, doYield: Boolean, doThrow: Boolean): Int {
     return suspendFun.suspendFun(doYield = doYield, doThrow = doThrow)
 }
@@ -127,7 +127,7 @@ private suspend fun callAbstractSuspendBridgeImpl(bridge: AbstractSuspendBridge)
     assertFailsWith<ObjCErrorException> { bridge.nothingAsUnit(10) }
 }
 
-@Throws
+@Throws(CoroutineException::class)
 fun callSuspendBridge(bridge: AbstractSuspendBridge, resultHolder: ResultHolder<Unit>) {
     suspend {
         callSuspendBridgeImpl(bridge)
