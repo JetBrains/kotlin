aa0eeba32756a26df4930470954ecbb666ab0e6c
Steven Schäfer <stevenschaefer@google.com>
2020-04-01 19:18:29 +0200

    Parcelize: Add a test for KT-36658

---
+++ plugins/android-extensions/android-extensions-compiler/testData/parcel/box/kt36658.kt
@@ -0,0 +1,22 @@
+// IGNORE_BACKEND: JVM
+// StackOverflowError caused by infinite loop in MyObject.writeToParcel
+// WITH_RUNTIME
+
+@file:JvmName("TestKt")
+package test
+
+import kotlinx.android.parcel.*
+import android.os.Parcel
+import android.os.Parcelable
+
+@Parcelize
+private object MyObject : Parcelable
+
+fun box() = parcelTest { parcel ->
+    MyObject.writeToParcel(parcel, 0)
+
+    val bytes = parcel.marshall()
+    parcel.unmarshall(bytes, 0, bytes.size)
+
+    readFromParcel<MyObject>(parcel)
+}
