3b466369ad16a9e4a700251e30d85741e9327c0a
Alexander Zolotov <zolotov@jetbrains.com>
2020-04-02 21:16:02 +0300

    GO-8672 Widen selection on string literal selects wrong part
    intellij-monorepo: 1b1f25213d666a92d3950f1654a57b22cbb4e010

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/editorActions/wordSelection/InjectedFileReferenceSelectioner.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/editorActions/wordSelection/InjectedFileReferenceSelectioner.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.codeInsight.editorActions.wordSelection;
 
@@ -44,6 +44,7 @@ public class InjectedFileReferenceSelectioner extends AbstractWordSelectioner {
 
     TextRange realRange = ElementManipulators.getValueTextRange(host)
       .shiftRight(host.getTextRange().getStartOffset());
+    if (!realRange.contains(cursorOffset)) return Collections.emptyList();
 
     PsiElement valueElement = findValueElement(host, realRange);
 
