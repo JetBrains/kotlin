1261dc0b04bf61e23cb47cdadf775786bb64ce41
Toshiaki Kameyama <kmymtsak@gmail.com>
2020-04-03 09:26:41 +0900

    "Replace 'invoke' with direct call" intention: fix false positive when function is not operator
    #KT-37977 Fixed

--- idea/src/org/jetbrains/kotlin/idea/intentions/conventionNameCalls/ReplaceInvokeIntention.kt
+++ idea/src/org/jetbrains/kotlin/idea/intentions/conventionNameCalls/ReplaceInvokeIntention.kt
@@ -8,12 +8,15 @@ package org.jetbrains.kotlin.idea.intentions.conventionNameCalls
 import com.intellij.codeInsight.intention.HighPriorityAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.intentions.SelfTargetingRangeIntention
 import org.jetbrains.kotlin.idea.intentions.callExpression
 import org.jetbrains.kotlin.idea.intentions.calleeName
+import org.jetbrains.kotlin.idea.intentions.toResolvedCall
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
 class ReplaceInvokeIntention : SelfTargetingRangeIntention<KtDotQualifiedExpression>(
@@ -21,7 +24,10 @@ class ReplaceInvokeIntention : SelfTargetingRangeIntention<KtDotQualifiedExpress
     KotlinBundle.lazyMessage("replace.invoke.with.direct.call")
 ), HighPriorityAction {
     override fun applicabilityRange(element: KtDotQualifiedExpression): TextRange? {
-        if (element.calleeName != OperatorNameConventions.INVOKE.asString() || element.callExpression?.typeArgumentList != null) return null
+        if (element.calleeName != OperatorNameConventions.INVOKE.asString() ||
+            element.callExpression?.typeArgumentList != null ||
+            (element.toResolvedCall(BodyResolveMode.PARTIAL)?.resultingDescriptor as? FunctionDescriptor)?.isOperator != true
+        ) return null
         return element.callExpression?.calleeExpression?.textRange
     }
 
