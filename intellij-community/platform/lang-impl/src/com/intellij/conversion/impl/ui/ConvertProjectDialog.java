e49bb7fc5e23770f1324851cab283d82b2659169
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-04-03 18:02:39 +0200

    [i18n] add button text for ConvertProjectDialog
    intellij-monorepo: da81cbe72ea1ea02acc83a6c2364af8c84743a20

--- intellij-community/platform/lang-impl/src/com/intellij/conversion/impl/ui/ConvertProjectDialog.java
+++ intellij-community/platform/lang-impl/src/com/intellij/conversion/impl/ui/ConvertProjectDialog.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.conversion.impl.ui;
 
@@ -77,7 +77,7 @@ public class ConvertProjectDialog extends DialogWrapper {
       }
     });
     init();
-    setOKButtonText("Convert");
+    setOKButtonText(IdeBundle.message("convert.project.dialog.button.text"));
   }
 
   @Override
