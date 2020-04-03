072cd849820aa95e54b6209fe1f2ea2de427add8
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-04-03 20:56:56 +0300

    [i18n] no declaration to go to
    intellij-monorepo: 139a951d2ceffd26e4ecda39a99e642a48204116

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/ui.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/ui.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.navigation.actions
 
+import com.intellij.codeInsight.CodeInsightBundle
 import com.intellij.codeInsight.hint.HintManager
 import com.intellij.ide.IdeEventQueue
 import com.intellij.lang.LanguageNamesValidation
@@ -12,7 +13,7 @@ import java.awt.event.MouseEvent
 internal fun notifyNowhereToGo(project: Project, editor: Editor, file: PsiFile, offset: Int) {
   // Disable the 'no declaration found' notification for keywords
   if (!isUnderDoubleClick() && !isKeywordUnderCaret(project, file, offset)) {
-    HintManager.getInstance().showErrorHint(editor, "Cannot find declaration to go to")
+    HintManager.getInstance().showErrorHint(editor, CodeInsightBundle.message("declaration.navigation.nowhere.to.go"))
   }
 }
 
