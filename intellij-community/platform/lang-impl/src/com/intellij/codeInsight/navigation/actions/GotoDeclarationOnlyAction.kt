a717bd6fe02dd8c1144e46cb946ba555e382b1d7
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2020-04-05 20:40:35 +0300

    do not return any ctrl+mouse info from GotoDeclarationOnlyAction because it never worked
    and it definitely shouldn't return info from GotoDeclarationAction (which it overrides).
    
    intellij-monorepo: 3be46b859598b65ca4e1e80eae547f9c0a0a7061

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationOnlyAction.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/navigation/actions/GotoDeclarationOnlyAction.kt
@@ -1,9 +1,16 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.navigation.actions
 
 import com.intellij.codeInsight.CodeInsightActionHandler
+import com.intellij.codeInsight.navigation.CtrlMouseInfo
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiFile
 
 class GotoDeclarationOnlyAction : GotoDeclarationAction() {
 
   override fun getHandler(): CodeInsightActionHandler = GotoDeclarationOnlyHandler
+
+  override fun getCtrlMouseInfo(editor: Editor, file: PsiFile, offset: Int): CtrlMouseInfo? {
+    return null
+  }
 }
