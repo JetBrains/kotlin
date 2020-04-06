76d82cd173b28cede1dc1b94fe7d2625787a30f3
Dmitro Batko <dmitry.batkovich@jetbrains.com>
2020-04-06 12:45:31 +0300

    enforced plain text manager should not invalidate indexes for plain text files if it's already did
    intellij-monorepo: d9627fb44d0cc17fb853db8dedc2cdb11924f556

--- intellij-community/platform/lang-impl/src/com/intellij/openapi/file/exclude/ProjectPlainTextFileTypeManager.java
+++ intellij-community/platform/lang-impl/src/com/intellij/openapi/file/exclude/ProjectPlainTextFileTypeManager.java
@@ -1,9 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.file.exclude;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.util.indexing.FileBasedIndex;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,6 +14,12 @@ import org.jetbrains.annotations.NotNull;
  */
 @State(name = "ProjectPlainTextFileTypeManager")
 public class ProjectPlainTextFileTypeManager extends PersistentFileSetManager {
+  @NotNull
+  private final Project myProject;
+
+  public ProjectPlainTextFileTypeManager(@NotNull Project project) {
+    myProject = project;
+  }
 
   public static ProjectPlainTextFileTypeManager getInstance(@NotNull Project project) {
     return project.getService(ProjectPlainTextFileTypeManager.class);
@@ -19,7 +27,10 @@ public class ProjectPlainTextFileTypeManager extends PersistentFileSetManager {
 
   @Override
   protected void onFileAdded(@NotNull VirtualFile file) {
-    FileBasedIndex.getInstance().requestReindex(file);
+    ApplicationManager.getApplication().assertReadAccessAllowed();
+    if (FileTypeIndex.getIndexedFileType(file, myProject) != EnforcedPlainTextFileType.INSTANCE) {
+      FileBasedIndex.getInstance().requestReindex(file);
+    }
   }
 
   @Override
