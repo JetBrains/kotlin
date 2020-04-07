9941c255b97d1e372f6a0bc2504fe74759657bfc
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-04-07 12:33:59 +0300

    KT-30330 KotlinNothingValueException

--- libraries/stdlib/common/src/kotlin/ExceptionsH.kt
+++ libraries/stdlib/common/src/kotlin/ExceptionsH.kt
@@ -108,3 +108,11 @@ public expect class UninitializedPropertyAccessException : RuntimeException {
     constructor(message: String?, cause: Throwable?)
     constructor(cause: Throwable?)
 }
+
+@SinceKotlin("1.4")
+internal class KotlinNothingValueException : RuntimeException {
+    constructor() : super()
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    constructor(cause: Throwable?) : super(cause)
+}
