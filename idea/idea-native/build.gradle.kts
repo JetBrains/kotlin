2f6099feb9a29e816966a0769b83dce5327c3a0c
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2020-04-07 16:05:27 +0300

    Add KotlinNativeRunConfigurationProducer.

--- idea/idea-native/build.gradle.kts
+++ idea/idea-native/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 dependencies {
     compile(project(":idea"))
+    compileOnly(intellijPluginDep("gradle"))
     compile(project(":idea:idea-core"))
     compile(project(":idea:idea-jvm"))
     compile(project(":compiler:frontend"))
