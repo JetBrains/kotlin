7b9591d055f96b1a137efdb36d297259ed43d76a
nik <Nikolay.Chashnikov@jetbrains.com>
2020-04-08 14:36:04 +0300

    [configuration store] add more tests which check serialization of project elements imported from external sources
    intellij-monorepo: 4bc7e4a94e54384657e11a924f23c30d288fea3d

--- intellij-community/platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/configurationStore/ExternalSystemStorageTest.kt
+++ intellij-community/platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/configurationStore/ExternalSystemStorageTest.kt
@@ -1,34 +1,38 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.openapi.externalSystem.configurationStore
 
-import com.intellij.configurationStore.ESCAPED_MODULE_DIR
-import com.intellij.configurationStore.useAndDispose
+import com.intellij.facet.FacetManager
+import com.intellij.facet.impl.invalid.InvalidFacetType
 import com.intellij.openapi.application.AppUIExecutor
+import com.intellij.openapi.application.ex.PathManagerEx
 import com.intellij.openapi.application.impl.coroutineDispatchingContext
-import com.intellij.openapi.application.impl.inWriteAction
-import com.intellij.openapi.components.stateStore
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager
 import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsDataStorage
 import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjectsManagerImpl
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleTypeId
-import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.CompilerProjectExtension
+import com.intellij.openapi.roots.ExternalProjectSystemRegistry
+import com.intellij.openapi.roots.LanguageLevelProjectExtension
 import com.intellij.openapi.roots.ModuleRootModificationUtil
-import com.intellij.openapi.roots.impl.ModuleRootManagerImpl
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.packaging.artifacts.ArtifactManager
+import com.intellij.packaging.elements.PackagingElementFactory
+import com.intellij.packaging.impl.artifacts.PlainArtifactType
+import com.intellij.pom.java.LanguageLevel
 import com.intellij.project.stateStore
-import com.intellij.testFramework.ProjectRule
-import com.intellij.testFramework.RunsInActiveStoreMode
-import com.intellij.testFramework.TemporaryDirectory
-import com.intellij.testFramework.assertions.Assertions.assertThat
-import com.intellij.testFramework.createProjectAndUseInLoadComponentStateMode
-import com.intellij.util.io.delete
-import com.intellij.util.io.parentSystemIndependentPath
-import com.intellij.util.io.systemIndependentPath
+import com.intellij.testFramework.*
+import com.intellij.util.io.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
+import java.nio.file.Path
 import java.nio.file.Paths
 
 @RunsInActiveStoreMode
@@ -36,7 +40,7 @@ class ExternalSystemStorageTest {
   companion object {
     @JvmField
     @ClassRule
-    val projectRule = ProjectRule()
+    val appRule = ApplicationRule()
   }
 
   @JvmField
@@ -44,60 +48,69 @@ class ExternalSystemStorageTest {
   val tempDirManager = TemporaryDirectory()
 
   @Test
-  fun `must be empty if external system storage`() = runBlocking {
-    createProjectAndUseInLoadComponentStateMode(tempDirManager, directoryBased = true) { project ->
-      ExternalProjectsManagerImpl.getInstance(project).setStoreExternally(true)
+  fun `single module`() = saveProjectAndCheckResult("singleModule") { project, projectDir ->
+    val module = ModuleManager.getInstance(project).newModule(projectDir.resolve("test.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
+    ModuleRootModificationUtil.addContentRoot(module, projectDir.systemIndependentPath)
+    ExternalSystemModulePropertyManager.getInstance(module).setMavenized(true)
+  }
 
-      val dotIdeaDir = Paths.get(project.stateStore.directoryStorePath)
+  @Test
+  fun `mixed modules`() = saveProjectAndCheckResult("mixedModules") { project, projectDir ->
+    val regular = ModuleManager.getInstance(project).newModule(projectDir.resolve("regular.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
+    ModuleRootModificationUtil.addContentRoot(regular, projectDir.resolve("regular").systemIndependentPath)
+    val imported = ModuleManager.getInstance(project).newModule(projectDir.resolve("imported.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
+    ModuleRootModificationUtil.addContentRoot(imported, projectDir.resolve("imported").systemIndependentPath)
+    ExternalSystemModulePropertyManager.getInstance(imported).setMavenized(true)
+  }
 
-      val cacheDir = ExternalProjectsDataStorage.getProjectConfigurationDir(project).resolve("modules")
-      cacheDir.delete()
+  @Test
+  fun libraries() = saveProjectAndCheckResult("libraries") { project, projectDir ->
+    val libraryTable = LibraryTablesRegistrar.getInstance().getLibraryTable(project)
+    val model = libraryTable.modifiableModel
+    model.createLibrary("regular", null)
+    model.createLibrary("imported", null, externalSource)
+    model.commit()
+  }
 
-      // we must not use VFS here, file must not be created
-      val moduleFile = dotIdeaDir.parent.resolve("test.iml")
-      withContext(AppUIExecutor.onUiThread().inWriteAction().coroutineDispatchingContext()) {
-        ModuleManager.getInstance(project).newModule(moduleFile.systemIndependentPath, ModuleTypeId.JAVA_MODULE)
-      }
-        .useAndDispose {
-          assertThat(cacheDir).doesNotExist()
+  @Test
+  fun artifacts() = saveProjectAndCheckResult("artifacts") { project, projectDir ->
+    CompilerProjectExtension.getInstance(project)!!.compilerOutputPointer = null
+    val model = ArtifactManager.getInstance(project).createModifiableModel()
+    model.addArtifact("regular", PlainArtifactType.getInstance())
+    model.addArtifact("import", PlainArtifactType.getInstance(), PackagingElementFactory.getInstance().createArchive("a.jar"),
+                      externalSource)
+    model.commit()
+  }
 
-          ModuleRootModificationUtil.addContentRoot(this, moduleFile.parentSystemIndependentPath)
+  private val externalSource get() = ExternalProjectSystemRegistry.getInstance().getSourceById("test")
 
-          stateStore.save()
-          assertThat(cacheDir).doesNotExist()
-          assertThat(moduleFile).isEqualTo("""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <module type="JAVA_MODULE" version="4">
-        <component name="NewModuleRootManager" inherit-compiler-output="true">
-          <exclude-output />
-          <content url="file://$ESCAPED_MODULE_DIR" />
-          <orderEntry type="sourceFolder" forTests="false" />
-        </component>
-      </module>""")
+  private fun saveProjectAndCheckResult(testDataDirName: String, setupProject: (Project, Path) -> Unit) {
+    runBlocking {
+      createProjectAndUseInLoadComponentStateMode(tempDirManager, directoryBased = true) { project ->
+        ExternalProjectsManagerImpl.getInstance(project).setStoreExternally(true)
+        val projectDir = Paths.get(project.stateStore.directoryStorePath).parent
+        val cacheDir = ExternalProjectsDataStorage.getProjectConfigurationDir(project)
+        cacheDir.delete()
 
-          ExternalSystemModulePropertyManager.getInstance(this).setMavenized(true)
-          // force re-save: this call not in the setMavenized because ExternalSystemModulePropertyManager in the API (since in production we have the only usage, it is ok for now)
-          (ModuleRootManager.getInstance(this) as ModuleRootManagerImpl).stateChanged()
-
-          assertThat(cacheDir).doesNotExist()
-          stateStore.save()
-          assertThat(cacheDir).isDirectory
-          assertThat(moduleFile).isEqualTo("""
-      <?xml version="1.0" encoding="UTF-8"?>
-      <module type="JAVA_MODULE" version="4" />""")
-
-          assertThat(cacheDir.resolve("test.xml")).isEqualTo("""
-      <module>
-        <component name="ExternalSystem" externalSystem="Maven" />
-        <component name="NewModuleRootManager" inherit-compiler-output="true">
-          <exclude-output />
-          <content url="file://$ESCAPED_MODULE_DIR" />
-          <orderEntry type="sourceFolder" forTests="false" />
-        </component>
-      </module>""")
-
-          assertThat(dotIdeaDir.resolve("modules.xml")).doesNotExist()
+        withContext(AppUIExecutor.onUiThread().coroutineDispatchingContext()) {
+          runWriteAction {
+            //we need to set language level explicitly because otherwise if some tests modifies language level in the default project, we'll
+            // get different content in misc.xml
+            LanguageLevelProjectExtension.getInstance(project)!!.languageLevel = LanguageLevel.JDK_1_8
+            setupProject(project, projectDir)
+          }
         }
+
+        project.stateStore.save()
+
+        val expectedDir = tempDirManager.newPath("expectedStorage")
+        val testDataRoot = Paths.get(PathManagerEx.getCommunityHomePath()).resolve("platform/external-system-impl/testData/jpsSerialization")
+        FileUtil.copyDir(testDataRoot.resolve("common").toFile(), expectedDir.toFile())
+        FileUtil.copyDir(testDataRoot.resolve(testDataDirName).toFile(), expectedDir.toFile())
+
+        projectDir.toFile().assertMatches(directoryContentOf(expectedDir.resolve("project")))
+        cacheDir.toFile().assertMatches(directoryContentOf(expectedDir.resolve("cache")))
+      }
     }
   }
 }
