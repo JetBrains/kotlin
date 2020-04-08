49383c735251b4bbbc8c432fdc6366c172b33c8d
Aydar Mukhametzyanov <aydar.mukhametzyanov@jetbrains.com>
2020-04-08 15:21:24 +0200

    [OC-19702] Make placeholders folding and parameters traverse DumbAware
    intellij-monorepo: 1f84bf1cb96db8210c89c14f07fec8681ee4eb06

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/CodeFoldingPassFactory.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/daemon/impl/CodeFoldingPassFactory.java
@@ -3,11 +3,12 @@ package com.intellij.codeInsight.daemon.impl;
 
 import com.intellij.codeHighlighting.*;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
-final class CodeFoldingPassFactory implements TextEditorHighlightingPassFactory, TextEditorHighlightingPassFactoryRegistrar {
+final class CodeFoldingPassFactory implements TextEditorHighlightingPassFactory, TextEditorHighlightingPassFactoryRegistrar, DumbAware {
   @Override
   public void registerHighlightingPassFactory(@NotNull TextEditorHighlightingPassRegistrar registrar, @NotNull Project project) {
     registrar.registerTextEditorHighlightingPass(this, null, null, false, Pass.UPDATE_FOLDING);
