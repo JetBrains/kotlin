2a139e54075b15cd13abc426cb4e25f71cd65d00
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-04-09 11:09:56 +0200

    [i18n] add find.usages.progress.title to FindBundle
    intellij-monorepo: 663f2d0e07e64a456f9d95c984f787e6f707a159

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/inline/GenericInlineHandler.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/inline/GenericInlineHandler.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.intellij.refactoring.inline;
 
@@ -67,7 +53,7 @@ public class GenericInlineHandler {
     else {
       final Ref<Collection<? extends PsiReference>> usagesRef = new Ref<>();
       ProgressManager.getInstance().runProcessWithProgressSynchronously(() -> usagesRef.set(ReferencesSearch.search(element).findAll()),
-                                                                        FindBundle.message("find.usages.dialog.title"), false, element.getProject());
+                                                                        FindBundle.message("find.usages.progress.title"), false, element.getProject());
       allReferences = usagesRef.get();
     }
 
