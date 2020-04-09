7e6a78aa2f7224d9e4d44ab44cd6db389db9ced9
Juan Chen <juanch@google.com>
2020-04-09 16:56:14 -0700

    [FIR] fix argument ordering of Fir2Ir.
    Add an IrBlock for assigning arguments to temp variables if needed.

--- compiler/testData/codegen/box/argumentOrder/defaults.kt
+++ compiler/testData/codegen/box/argumentOrder/defaults.kt
@@ -1,4 +1,3 @@
-// IGNORE_BACKEND_FIR: JVM_IR
 var invokeOrder: String = ""
 
 fun test(x: Double = { invokeOrder += "x"; 1.0 }(), a: String, y: Long = { invokeOrder += "y"; 1 }(), b: String): String {
