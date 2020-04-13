17c6122987cddd55d6ad6ae0beb5e99eb63d1fe4
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-04-13 14:44:48 +0300

    [FIR] Make `STATUS` resolve phase as required for body resolve

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/FirResolvePhase.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/FirResolvePhase.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.fir.declarations
 
+import java.util.*
+
 enum class FirResolvePhase {
     RAW_FIR,
     IMPORTS,
@@ -16,7 +18,7 @@ enum class FirResolvePhase {
     IMPLICIT_TYPES_BODY_RESOLVE,
     BODY_RESOLVE;
 
-    val requiredToLaunch: FirResolvePhase get() = if (this == BODY_RESOLVE) STATUS else values()[ordinal - 1]
+    val requiredToLaunch: FirResolvePhase get() = if (this in bodyResolvePhases) STATUS else values()[ordinal - 1]
 
     val next: FirResolvePhase get() = values()[ordinal + 1]
 
@@ -24,5 +26,7 @@ enum class FirResolvePhase {
         // Short-cut
         val DECLARATIONS = STATUS
         val ANALYZED_DEPENDENCIES = BODY_RESOLVE
+
+        private val bodyResolvePhases = EnumSet.copyOf(setOf(IMPLICIT_TYPES_BODY_RESOLVE, BODY_RESOLVE))
     }
 }
