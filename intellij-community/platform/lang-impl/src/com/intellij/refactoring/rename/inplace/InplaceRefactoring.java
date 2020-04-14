576eb0642058e084a01e1528ba740d0f9de06e9d
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-04-14 15:48:08 +0700

    Revert InplaceRefactoring ImaginaryEditor check (otherwise many tests fail)
    intellij-monorepo: c5ff327ba42dbc0eb8b801571b5946398e3731fb

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/rename/inplace/InplaceRefactoring.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/rename/inplace/InplaceRefactoring.java
@@ -30,6 +30,7 @@ import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.ex.util.EditorUtil;
 import com.intellij.openapi.editor.impl.EditorImpl;
+import com.intellij.openapi.editor.impl.ImaginaryEditor;
 import com.intellij.openapi.editor.markup.RangeHighlighter;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.fileEditor.FileEditor;
@@ -165,7 +166,7 @@ public abstract class InplaceRefactoring {
 
 
   public boolean performInplaceRefactoring(@Nullable final LinkedHashSet<String> nameSuggestions) {
-    if (!myElementToRename.isPhysical()) return false;
+    if (myEditor instanceof ImaginaryEditor) return false;
     myNameSuggestions = nameSuggestions;
     if (InjectedLanguageUtil.isInInjectedLanguagePrefixSuffix(myElementToRename)) {
       return false;
