12c47ab9aef2f4d76dcdd1182f2abb4797ef0057
Anna Kozlova <anna.kozlova@jetbrains.com>
2020-04-14 08:26:53 +0200

    copy refactoring: restore default OK action (IDEA-237130)
    intellij-monorepo: 1e276a0df17902c15ede54d681f89272573dfd09

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/copy/CopyFilesOrDirectoriesDialog.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/copy/CopyFilesOrDirectoriesDialog.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.copy;
 
+import com.intellij.CommonBundle;
 import com.intellij.ide.scratch.ScratchUtil;
 import com.intellij.ide.util.DirectoryUtil;
 import com.intellij.ide.util.PropertiesComponent;
@@ -143,6 +144,7 @@ public class CopyFilesOrDirectoriesDialog extends RefactoringDialog implements D
       getTargetDirectoryComponent().setText(targetPath);
     }
     validateButtons();
+    getRefactorAction().putValue(Action.NAME, CommonBundle.getOkButtonText());
   }
 
   private static boolean canBeOpenedInEditor(PsiElement[] elements) {
