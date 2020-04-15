3511c66dfc8161128e6ec30a8c5823d6704c220b
Louis Vignier <louis.vignier@jetbrains.com>
2020-04-15 20:22:52 +0200

    KtBinaryExpression matching

--- kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/impl/matcher/KotlinMatchingVisitor.kt
+++ kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/impl/matcher/KotlinMatchingVisitor.kt
@@ -1,15 +1,28 @@
 package com.jetbrains.kotlin.structuralsearch.impl.matcher
 
 import com.intellij.structuralsearch.impl.matcher.GlobalMatchingVisitor
+import org.jetbrains.kotlin.psi.KtBinaryExpression
 import org.jetbrains.kotlin.psi.KtConstantExpression
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 
 
 class KotlinMatchingVisitor(private val myMatchingVisitor: GlobalMatchingVisitor) : KtVisitorVoid() {
-    
+
+    override fun visitBinaryExpression(expression: KtBinaryExpression) {
+        val other = myMatchingVisitor.element
+        if (myMatchingVisitor.setResult(other is KtBinaryExpression)) {
+            val expr2 = other as KtBinaryExpression
+            // Same operation
+            if (myMatchingVisitor.setResult(expression.operationToken == expr2.operationToken)) {
+                // Same operands
+                myMatchingVisitor.result = myMatchingVisitor.match(expression.left, expr2.left)
+                        && myMatchingVisitor.match(expression.right, expr2.right)
+            }
+        }
+    }
+
     override fun visitConstantExpression(expression: KtConstantExpression) {
         myMatchingVisitor.result = myMatchingVisitor.element.text == expression.text
-        super.visitConstantExpression(expression)
     }
 
 }
