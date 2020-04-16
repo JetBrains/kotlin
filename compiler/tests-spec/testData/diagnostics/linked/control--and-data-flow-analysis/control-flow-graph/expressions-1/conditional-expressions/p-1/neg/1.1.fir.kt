0df635489680000a3fddbf44f2ca23c621d08791
Denis Zharkov <denis.zharkov@jetbrains.com>
2020-04-16 12:23:02 +0300

    FIR: Adjust testData for spec tests: conditional-expressions

---
+++ compiler/tests-spec/testData/diagnostics/linked/control--and-data-flow-analysis/control-flow-graph/expressions-1/conditional-expressions/p-1/neg/1.1.fir.kt
@@ -0,0 +1,28 @@
+// !LANGUAGE: +NewInference
+// !DIAGNOSTICS:  -IMPLICIT_CAST_TO_ANY -UNUSED_VARIABLE -ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE -UNUSED_VALUE -UNUSED_PARAMETER -UNUSED_EXPRESSION
+// SKIP_TXT
+
+/*
+ * KOTLIN DIAGNOSTICS SPEC TEST (NEGATIVE)
+ *
+ * SPEC VERSION: 0.1-296
+ * PLACE: control--and-data-flow-analysis, control-flow-graph, expressions-1, conditional-expressions -> paragraph 1 -> sentence 1
+ * NUMBER: 1
+ * DESCRIPTION: check if-expressions must have both branches.
+ */
+
+// TESTCASE NUMBER: 1
+
+fun case1() {
+    val b = true
+    val a =  if (b) {
+        "true"
+    }
+}
+
+// TESTCASE NUMBER: 2
+
+fun case2() {
+    val b = true
+    val a = if (b) "true"
+}
