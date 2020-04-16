5c7c4b4cb6e6af142ace9494095aa07ed4fc9406
Denis Zharkov <denis.zharkov@jetbrains.com>
2020-04-16 13:07:35 +0300

    FIR: Adjust testData for spec tests: infix priority
    ^KT-37374 Please, fix the tests

---
+++ compiler/tests-spec/testData/diagnostics/linked/overload-resolution/building-the-overload-candidate-set-ocs/infix-function-call/p-2/neg/1.1.fir.kt
@@ -0,0 +1,96 @@
+// !LANGUAGE: +NewInference
+// !DIAGNOSTICS: -UNUSED_VARIABLE -ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE -UNUSED_VALUE -UNUSED_PARAMETER -UNUSED_EXPRESSION -EXTENSION_SHADOWED_BY_MEMBER
+// SKIP_TXT
+
+/*
+ * KOTLIN DIAGNOSTICS SPEC TEST (NEGATIVE)
+ *
+ * SPEC VERSION: 0.1-313
+ * PLACE: overload-resolution, building-the-overload-candidate-set-ocs, infix-function-call -> paragraph 2 -> sentence 1
+ * RELEVANT PLACES: overload-resolution, building-the-overload-candidate-set-ocs, infix-function-call -> paragraph 2 -> sentence 2
+ * overload-resolution, building-the-overload-candidate-set-ocs, call-with-an-explicit-receiver -> paragraph 6 -> sentence 1
+ * NUMBER: 1
+ * DESCRIPTION: Implicitly imported extension callable without infix modifier
+ */
+
+// FILE: Extensions.kt
+package libPackage
+
+class A() {
+     fun foo(x: Int) = "member fun foo"
+}
+
+// FILE: Extensions.kt
+// TESTCASE NUMBER: 1, 2, 3, 4
+
+package sentence3
+import libPackage.A
+
+fun A.foo(x: Int) = "pack scope extension fun foo"
+
+// FILE: TestCase1.kt
+// TESTCASE NUMBER: 1
+
+package sentence3
+import libPackage.A
+
+class Case1() {
+    fun A.foo(x: Int) = "local extension fun foo"
+
+    fun case1() {
+        val a = A()
+        a foo 1
+        A() foo 1
+    }
+}
+// FILE: TestCase2.kt
+// TESTCASE NUMBER: 2
+
+package sentence3
+import libPackage.A
+
+interface Case2 {
+    fun A.foo(x: Int) = "local extension fun foo"
+
+    fun case2() {
+        val a = A()
+        a foo 1
+        A() foo 1
+    }
+}
+
+// FILE: TestCase3.kt
+// TESTCASE NUMBER: 3
+package testPack
+import libPackage.A
+
+fun A.foo(x: Int) = "my package scope top level contains"
+
+
+fun case3() {
+    fun A.foo(x: Int) ="my local scope contains"
+
+    val a = A()
+    a foo 1
+    A() foo 1
+}
+
+// FILE: TestCase4.kt
+// TESTCASE NUMBER: 4
+package testPackNew
+import libPackage.A
+
+fun A.foo(x: Int) = "my package scope top level contains"
+
+
+fun case4() {
+
+    fun A.foo(x: Int) = "my local contains"
+
+    fun subfun() {
+        fun A.foo(x: Int) = "my local contains"
+        val a = A()
+        a foo 1
+        A() foo 1
+    }
+}
---
+++ compiler/tests-spec/testData/diagnostics/linked/overload-resolution/building-the-overload-candidate-set-ocs/infix-function-call/p-2/neg/1.4.fir.kt
@@ -0,0 +1,58 @@
+// !LANGUAGE: +NewInference
+// !DIAGNOSTICS: -UNUSED_VARIABLE -ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE -UNUSED_VALUE -UNUSED_PARAMETER -UNUSED_EXPRESSION
+// SKIP_TXT
+
+/*
+ * KOTLIN DIAGNOSTICS SPEC TEST (NEGATIVE)
+ *
+ * SPEC VERSION: 0.1-313
+ * PLACE: overload-resolution, building-the-overload-candidate-set-ocs, infix-function-call -> paragraph 2 -> sentence 1
+ * RELEVANT PLACES: overload-resolution, building-the-overload-candidate-set-ocs, infix-function-call -> paragraph 2 -> sentence 1
+ * overload-resolution, building-the-overload-candidate-set-ocs, infix-function-call -> paragraph 2 -> sentence 2
+ * overload-resolution, building-the-overload-candidate-set-ocs, call-with-an-explicit-receiver -> paragraph 6 -> sentence 4
+ * NUMBER: 4
+ * DESCRIPTION: Star-imported infix extension callables
+ */
+
+// FILE: Extensions.kt
+package libPackage
+
+ operator fun CharSequence.contains(regex: Regex): Boolean {
+    println("my contains")
+    return true
+}
+// FILE: Extensions.kt
+
+package sentence3
+
+ operator fun CharSequence.contains(regex: Regex): Boolean {
+    println("my package scope contains")
+    return true
+}
+
+// FILE: TestCase1.kt
+// TESTCASE NUMBER: 1
+
+package sentence3
+import libPackage.*
+
+
+fun case1() {
+    val regex = Regex("")
+   "" contains  regex
+}
+
+// FILE: TestCase2.kt
+// TESTCASE NUMBER: 2
+package testPack
+import libPackage.*
+
+ operator fun CharSequence.contains(regex: Regex): Boolean {
+    println("my package scope top level contains")
+    return true
+}
+
+fun case2() {
+    val regex = Regex("")
+    "" contains  regex
+}
