da76fff83c025f932a0e54070c6e9a0ead5f2ef7
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2020-04-16 16:26:01 +0300

    Remove unused parts

--- idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/configuration/cache/ScriptConfigurationMemoryCache.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/configuration/cache/ScriptConfigurationMemoryCache.kt
@@ -37,29 +37,6 @@ open class ScriptConfigurationMemoryCache(
     }
 
     @Synchronized
-    override fun markOutOfDate(scope: ScriptConfigurationCacheScope) {
-        when (scope) {
-            is ScriptConfigurationCacheScope.File -> {
-                val file = scope.file.originalFile.virtualFile
-                markFileOutOfDate(file)
-            }
-            is ScriptConfigurationCacheScope.Except -> {
-                val file = scope.file.originalFile.virtualFile
-                memoryCache.entrySet().forEach {
-                    if (it.key != file) {
-                        markFileOutOfDate(it.key)
-                    }
-                }
-            }
-            is ScriptConfigurationCacheScope.All ->{
-                memoryCache.entrySet().forEach {
-                    markFileOutOfDate(it.key)
-                }
-            }
-        }
-    }
-
-    @Synchronized
     @Suppress("UNCHECKED_CAST")
     override fun allApplied(): Map<VirtualFile, ScriptCompilationConfigurationWrapper> {
         val result = hashMapOf<VirtualFile, ScriptCompilationConfigurationWrapper>()
@@ -76,14 +53,4 @@ open class ScriptConfigurationMemoryCache(
         memoryCache.clear()
     }
 
-    @Synchronized
-    private fun markFileOutOfDate(file: VirtualFile) {
-        val old = memoryCache[file] ?: return
-        memoryCache.put(
-            file, old.copy(
-                applied = old.applied?.copy(inputs = CachedConfigurationInputs.OutOfDate),
-                loaded = old.loaded?.copy(inputs = CachedConfigurationInputs.OutOfDate)
-            )
-        )
-    }
 }
