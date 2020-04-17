7ba12a8ba76eec16e153e93d2b8308d4de652549
Ilya Matveev <ilya.matveev@jetbrains.com>
2020-04-17 13:36:31 +0300

    [K/N in IDE] Display short names of platform libs

--- kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/configuration/klib/KlibInfoProvider.kt
+++ kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/configuration/klib/KlibInfoProvider.kt
@@ -12,6 +12,7 @@ import org.jetbrains.kotlin.konan.target.HostManager
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.konan.target.TargetSupportException
 import org.jetbrains.kotlin.library.KLIB_MANIFEST_FILE_NAME
+import org.jetbrains.kotlin.library.KLIB_PROPERTY_SHORT_NAME
 import org.jetbrains.kotlin.library.KLIB_PROPERTY_UNIQUE_NAME
 import java.io.File
 import java.io.IOException
@@ -57,7 +58,11 @@ class KlibInfoProvider(kotlinNativeHome: File) {
         val manifestFile = findManifestFile(libraryPath) ?: return null
         val manifest = loadProperties(manifestFile)
 
-        val name = manifest.getProperty(KLIB_PROPERTY_UNIQUE_NAME) ?: return null
+        val name = with(manifest) {
+            getProperty(KLIB_PROPERTY_SHORT_NAME)
+                ?: getProperty(KLIB_PROPERTY_UNIQUE_NAME)
+                ?: return null
+        }
         val target = (detectTarget(libraryPath) ?: return null).result
 
         return when (origin) {
