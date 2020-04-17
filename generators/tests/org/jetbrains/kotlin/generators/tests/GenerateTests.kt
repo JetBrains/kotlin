876d6d209fca8784bbe143db998c10383a112af5
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2020-04-17 12:56:27 +0300

    Minor: move non-JPS MPP IC tests under incremental/mpp dir

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1296,10 +1296,10 @@ fun main(args: Array<String>) {
         }
 
         testClass<AbstractIncrementalMultiplatformJvmCompilerRunnerTest> {
-            model("incremental/singleModule/common", extension = null, excludeParentDirs = true)
+            model("incremental/mpp/allPlatforms", extension = null, excludeParentDirs = true)
         }
         testClass<AbstractIncrementalMultiplatformJsCompilerRunnerTest> {
-            model("incremental/singleModule/common", extension = null, excludeParentDirs = true)
+            model("incremental/mpp/allPlatforms", extension = null, excludeParentDirs = true)
         }
     }
 
