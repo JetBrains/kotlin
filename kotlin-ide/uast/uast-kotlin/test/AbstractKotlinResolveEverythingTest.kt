157c717965ffa43e073baa2765b8e15151f16bd3
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2020-04-17 13:37:07 +0300

    Uast: Making `AbstractKotlinResolveEverythingTest` test also `UCallExpression`
    current test data is just frezing the current state which is probable incorrect

--- kotlin-ide/uast/uast-kotlin/test/AbstractKotlinResolveEverythingTest.kt
+++ kotlin-ide/uast/uast-kotlin/test/AbstractKotlinResolveEverythingTest.kt
@@ -6,13 +6,13 @@
 package org.jetbrains.uast.test.kotlin
 
 import com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.psi.KtBlockExpression
-import org.jetbrains.uast.UFile
-import org.jetbrains.uast.UReferenceExpression
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.utils.addToStdlib.cast
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+import org.jetbrains.uast.*
 import org.jetbrains.uast.test.common.kotlin.IndentedPrintingVisitor
 import org.jetbrains.uast.test.common.kotlin.visitUFileAndGetResult
 import org.jetbrains.uast.test.env.kotlin.assertEqualsToFile
-import org.jetbrains.uast.toUElementOfType
 import java.io.File
 
 
@@ -23,16 +23,33 @@ abstract class AbstractKotlinResolveEverythingTest : AbstractKotlinUastTest() {
 
 
     private fun UFile.resolvableWithTargets() = object : IndentedPrintingVisitor(KtBlockExpression::class) {
-        override fun render(element: PsiElement) = element.toUElementOfType<UReferenceExpression>()?.let { ref ->
-            StringBuilder().apply {
-                val parent = ref.uastParent
-                append(parent?.asLogString())
-                append(" -> ")
-                append(ref.asLogString())
-                append(" -> ")
-                append(ref.resolve())
-                append(": ")
-                append(ref.resolvedName)
+        override fun render(element: PsiElement) =
+            UastFacade.convertToAlternatives<UExpression>(element, arrayOf(UReferenceExpression::class.java, UCallExpression::class.java))
+                .filter {
+                    when (it) {
+                        is UCallExpression -> it.sourcePsi.safeAs<KtCallElement>()?.calleeExpression !is KtSimpleNameExpression
+                        else -> true
+                    }
+                }.takeIf { it.any() }
+                ?.joinTo(StringBuilder(), "\n") { ref ->
+                    StringBuilder().apply {
+                        val parent = ref.uastParent
+                        append(parent?.asLogString())
+                        if (parent is UCallExpression) {
+                            append("(resolves to ${parent.resolve()})")
+                        }
+                        append(" -> ")
+                        append(ref.asLogString())
+                        append(" -> ")
+                        append(ref.cast<UResolvable>().resolve())
+                        append(": ")
+                        append(
+                            when (ref) {
+                                is UReferenceExpression -> ref.resolvedName
+                                is UCallExpression -> ""
+                                else -> "<none>"
+                            }
+                        )
             }
         }
     }.visitUFileAndGetResult(this)
