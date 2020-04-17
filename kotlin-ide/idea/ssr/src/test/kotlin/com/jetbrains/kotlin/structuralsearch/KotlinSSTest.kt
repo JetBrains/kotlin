52f5f688c37080105738d1f63d6d30bcd2fc1f5e
Louis Vignier <louis.vignier@jetbrains.com>
2020-04-17 15:41:45 +0200

    Fixed stdlib related test problems

--- kotlin-ide/idea/ssr/src/test/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinSSTest.kt
+++ kotlin-ide/idea/ssr/src/test/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinSSTest.kt
@@ -1,14 +1,33 @@
 package com.jetbrains.kotlin.structuralsearch
 
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.module.StdModuleTypes
+import com.intellij.openapi.projectRoots.JavaSdk
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.impl.libraries.LibraryEx
+import com.intellij.openapi.roots.ui.configuration.libraryEditor.NewLibraryEditor
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.structuralsearch.Matcher
 import com.intellij.structuralsearch.inspection.highlightTemplate.SSBasedInspection
 import com.intellij.structuralsearch.plugin.ui.SearchConfiguration
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import org.jetbrains.kotlin.idea.KotlinFileType
+import java.io.File
 import java.util.*
 
 @Suppress("UnstableApiUsage")
-abstract class KotlinSSTest : LightJavaCodeInsightFixtureTestCase() {
+abstract class KotlinSSTest : BasePlatformTestCase() {
+
+    override fun getProjectDescriptor(): LightProjectDescriptor {
+        return KotlinLightProjectDescriptor()
+    }
 
     protected fun doTest(pattern: String) {
         myFixture.configureByFile(getTestName(true) + ".kt")
@@ -31,4 +50,37 @@ abstract class KotlinSSTest : LightJavaCodeInsightFixtureTestCase() {
         return "src/test/resources/$basePath/"
     }
 
+    internal class KotlinLightProjectDescriptor : LightProjectDescriptor() {
+
+        val moduleType: ModuleType<*>
+            get() = StdModuleTypes.JAVA
+
+        override fun getSdk(): Sdk? {
+            val javaHome = System.getProperty("java.home")
+            assert(File(javaHome).isDirectory)
+            val table = ProjectJdkTable.getInstance()
+            val existing = table.findJdk("Full JDK")
+            return existing ?: JavaSdk.getInstance().createJdk("Full JDK", javaHome, true)
+        }
+
+        override fun configureModule(module: Module, model: ModifiableRootModel, contentEntry: ContentEntry) {
+            val editor = NewLibraryEditor()
+            editor.name = "LIBRARY"
+            val file = File(PathManager.getJarPathForClass(AccessDeniedException::class.java) ?: "")
+            assert(file.exists())
+            editor.addRoot(
+                VfsUtil.getUrlForLibraryRoot(file),
+                OrderRootType.CLASSES
+            )
+
+            val libraryTableModifiableModel = model.moduleLibraryTable.modifiableModel
+            val library = libraryTableModifiableModel.createLibrary(editor.name)
+
+            val libModel = library.modifiableModel
+            editor.applyTo(libModel as LibraryEx.ModifiableModelEx)
+
+            libModel.commit()
+            libraryTableModifiableModel.commit()
+        }
+    }
 }
