07343c8002ed97ec0060e0165be2eb253200eddb
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-04-17 13:38:28 +0300

    KT-37144 Respect quotes in package name during decompilation
    - Odd package names (for example `try` in `arrow-core`) were pasted
    as-is to the decompiled text
    - Because of this, stub checks were failing, forcing completion to start
    over and over again, as if something changed in the files
    - See EA-5572315 for the error
    - ^KT-37144 Fixed

---
+++ kotlin-ide/idea/testData/decompiler/decompiledTextJvm/PackageWithQuotes.expected.kt
@@ -0,0 +1,7 @@
+// IntelliJ API Decompiler stub source generated from a class file
+// Implementation of methods is not available
+
+package `try`.`package`
+
+public final class PackageWithQuotes public constructor() {
+}
