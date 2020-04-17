0290d7b41c4758227f7748be7c10ca20f1a5bba0
AJ <aj@alt>
2020-04-17 09:56:52 -0700

    Add java.nio.Path extensions to stdlib-jdk7
    This PR adds most extensions on `java.io.File` in `kotlin.io` to `java.nio.Path`. This includes extensions from `FileReadWrite.kt`, `Utils.kt`, and `FileTreeWalk.kt`.
    
    I attempted to keep the implementations, documentation, and tests as similar as possible to the existing implementations.
    
    I am happy to add, remove, or move to separate PRs any of the functions of this PR.
    
    ### `File` extensions that were not added to `Path`
    
    ##### `createTempDir`, `createTempFile`
    
    These functions have no `File` parameters, so can't be overloaded. Equivalents exist as `Files.createTempFile()` and `Files.createTempDirectory()`.
    
    ##### `startsWith`, `endsWith`, `normalize`, `resolve`, `resolveSibling`
    
    These exist as member functions on `Path`
    
    ##### `relativeTo`, `relativeToOrNull`, `relativeToOrSelf`, `toRelativeString`, `toRelativeStringOrNull`
    
    This functionality exists as the `Path.relativize` member function, which is equivalent to `relativeTo`, but with the receiver and parameter flipped. `foo.relativeTo(bar)` is equivalent to `bar.relativize(foo)`. We could potentially add a `relativizeOrNull` extension to make that pattern simpler.
    
    ##### `isRooted`
    
    `Path` has a `root` method, so `isRooted` is equivalent to `root != null`
    
    ### New extensions
    
    All of the simple boolean attribute checks from `java.nio.Files` were added as extensions on `Path`. These extensions are used commonly enough that it seems worth supporting them. This functionality for `File` is implemented as member methods.
    
    The following `Path` extensions were added:
    
    - `exists`
    - `isDirectory`
    - `isExecutable`
    - `isFile`
    - `isHidden`
    - `isReadable`
    - `isSameFile`
    - `isSymbolicLink`
    - `isWritable`
    
    Some of these extensions take options that are forwarded to their `Files` method, so all of the extensions were implemented as functions rather than properties for consistency.
    
    Additionally, `Path.listFiles` was added to match the `File.listFiles` method. One motivation for its addition was that it's used several times in the implementation of other file extensions. The way to list directory contents with `java.nio` is via `Files.newDirectoryStream()`, which returns an iterable object that must be closed to avoid leaking resources. It's difficult to use correctly with functions like `map` and `filter`, so this extension was added as a simpler, less error-prone alternative.
    
    ### Other changes
    
    I added overloads of several of the read-write that take `OpenOptions` to expose the greater control that `java.nio` introduces. For example, you can use `printWriter(APPEND)` to create a `PrintWriter` that doesn't delete the contents of an existing file.
    
    All the new extensions throw exceptions (such as `NoSuchFileException`) from `java.nio` rather than the copies from `kotlin.io`. The `kotlin.io` copies take `File` objects as parameters, and so aren't compatible with `Path`s.

---
+++ libraries/stdlib/jdk7/src/kotlin/io/PathUtils.kt
@@ -0,0 +1,353 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER", "RedundantVisibilityModifier")
+@file:JvmMultifileClass
+@file:JvmName("PathsKt")
+@file:kotlin.jvm.JvmPackageName("kotlin.io.jdk7")
+
+package kotlin.io
+
+import java.io.File
+import java.io.IOException
+import java.nio.channels.FileChannel
+import java.nio.file.*
+import java.nio.file.FileAlreadyExistsException
+import java.nio.file.NoSuchFileException
+
+/**
+ * Returns the extension of this file (not including the dot), or an empty string if it doesn't have one.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public val Path.extension: String
+    get() = fileName.toString().substringAfterLast('.', "")
+
+/**
+ * Returns [path][File.path] of this File using the invariant separator '/' to
+ * separate the names in the name sequence.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public val Path.invariantSeparatorsPath: String
+    get() {
+        val separator = fileSystem.separator
+        return if (separator != "/") toString().replace(separator, "/") else toString()
+    }
+
+/**
+ * Returns file's name without an extension.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public val Path.nameWithoutExtension: String
+    get() = fileName.toString().substringBeforeLast(".")
+
+
+/**
+ * Copies this path to the given [target] path.
+ *
+ * If some directories on a way to the [target] are missing, then they will be created.
+ * If the [target] path already exists, this function will fail unless [overwrite] argument is set to `true`.
+ *
+ * When [overwrite] is `true` and [target] is a directory, it is replaced only if it is empty.
+ *
+ * If this file is a directory, it is copied without its content, i.e. an empty [target] directory is created.
+ * If you want to copy directory including its contents, use [copyRecursively].
+ *
+ * The operation doesn't preserve copied file attributes such as creation/modification date, permissions, etc.
+ *
+ * @param overwrite `true` if destination overwrite is allowed.
+ * @return the [target] file.
+ * @throws NoSuchFileException if the source file doesn't exist.
+ * @throws FileAlreadyExistsException if the destination file already exists and [overwrite] argument is set to `false`.
+ * @throws IOException if any errors occur while copying.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.copyTo(target: Path, overwrite: Boolean = false): Path {
+    val options = if (overwrite) arrayOf(StandardCopyOption.REPLACE_EXISTING) else emptyArray()
+    return copyTo(target, *options)
+}
+
+/**
+ * Copies this path to the given [target] path.
+ *
+ * If some directories on a way to the [target] are missing, then they will be created.
+ * If the [target] path already exists, this function will fail unless the
+ * [REPLACE_EXISTING][StandardCopyOption.REPLACE_EXISTING] is option is used.
+ *
+ * When [REPLACE_EXISTING][StandardCopyOption.REPLACE_EXISTING] is used and [target] is a directory,
+ * it is replaced only if it is empty.
+ *
+ * If this file is a directory, it is copied without its content, i.e. an empty [target] directory is created.
+ * If you want to copy directory including its contents, use [copyRecursively].
+ *
+ * The operation doesn't preserve copied file attributes such as creation/modification date,
+ * permissions, etc. unless [COPY_ATTRIBUTES][StandardCopyOption.COPY_ATTRIBUTES] is used.
+ *
+ * @param options options to control how the path is copied.
+ * @return the [target] file.
+ * @throws NoSuchFileException if the source file doesn't exist.
+ * @throws FileAlreadyExistsException if the destination file already exists and [REPLACE_EXISTING][StandardCopyOption.REPLACE_EXISTING] is not used.
+ * @throws IOException if any errors occur while copying.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.copyTo(target: Path, vararg options: CopyOption): Path {
+    if (!this.exists()) {
+        throw NoSuchFileException(toString(), null, "The source file doesn't exist.")
+    }
+
+    if (target.exists() && StandardCopyOption.REPLACE_EXISTING !in options) {
+        throw FileAlreadyExistsException(toString(), null, "The destination file already exists.")
+    }
+
+    if (this.isDirectory()) {
+        if (target.isDirectory() && Files.newDirectoryStream(target).use { it.firstOrNull() } != null) {
+            throw FileAlreadyExistsException(toString(), null, "The destination file already exists.")
+        }
+        try {
+            Files.createDirectories(target)
+        } catch (e: FileAlreadyExistsException) {
+            // File already exists and is not a directory
+            Files.delete(target)
+            Files.createDirectories(target)
+        }
+    } else {
+        target.parent?.let { Files.createDirectories(it) }
+        Files.copy(this, target, *options)
+    }
+
+    return target
+}
+
+/** Private exception class, used to terminate recursive copying. */
+internal class TerminateException : IOException()
+
+/**
+ * Copies this file with all its children to the specified destination [target] path.
+ * If some directories on the way to the destination are missing, then they will be created.
+ *
+ * If this file path points to a single file, then it will be copied to a file with the path [target].
+ * If this file path points to a directory, then its children will be copied to a directory with the path [target].
+ *
+ * If the [target] already exists, it will be deleted before copying when the [overwrite] parameter permits so.
+ *
+ * The operation doesn't preserve copied file attributes such as creation/modification date, permissions, etc.
+ *
+ * If any errors occur during the copying, then further actions will depend on the result of the call
+ * to `onError(File, IOException)` function, that will be called with arguments,
+ * specifying the file that caused the error and the exception itself.
+ * By default this function rethrows exceptions.
+ *
+ * Exceptions that can be passed to the `onError` function:
+ *
+ * - [NoSuchFileException] - if there was an attempt to copy a non-existent file
+ * - [FileAlreadyExistsException] - if there is a conflict
+ * - [AccessDeniedException] - if there was an attempt to open a directory that didn't succeed.
+ * - [IOException] - if some problems occur when copying.
+ *
+ * Note that if this function fails, then partial copying may have taken place.
+ *
+ * @param overwrite `true` if it is allowed to overwrite existing destination files and directories.
+ * @return `false` if the copying was terminated, `true` otherwise.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.copyRecursively(
+    target: Path,
+    overwrite: Boolean = false,
+    onError: (Path, IOException) -> OnErrorAction = { _, exception -> throw exception }
+): Boolean {
+    if (!this.exists()) {
+        return onError(this, NoSuchFileException(toString(), null, "The source file doesn't exist.")) !=
+                OnErrorAction.TERMINATE
+    }
+    try {
+        // We cannot break for loop from inside a lambda, so we have to use an exception here
+        for (src in walkTopDown().onFail { f, e -> if (onError(f, e) == OnErrorAction.TERMINATE) throw TerminateException() }) {
+            if (!src.exists()) {
+                if (onError(src, NoSuchFileException(src.toString(), null, "The source file doesn't exist.")) == OnErrorAction.TERMINATE)
+                    return false
+            } else {
+                val dstFile = target.resolve(relativize(src))
+                if (dstFile.exists() && !(src.isDirectory() && dstFile.isDirectory())) {
+                    val stillExists = if (!overwrite) true else {
+                        if (dstFile.isDirectory())
+                            !dstFile.deleteRecursively()
+                        else {
+                            try {
+                                Files.deleteIfExists(dstFile)
+                                Files.exists(dstFile)
+                            } catch (e: IOException) {
+                                if (onError(dstFile, e) == OnErrorAction.TERMINATE) return false
+                                continue
+                            }
+                        }
+                    }
+
+                    if (stillExists) {
+                        if (onError(
+                                dstFile, FileAlreadyExistsException(
+                                    src.toString(),
+                                    dstFile.toString(),
+                                    "The destination file already exists."
+                                )
+                            ) == OnErrorAction.TERMINATE
+                        ) {
+                            return false
+                        }
+
+                        continue
+                    }
+                }
+
+                if (src.isDirectory()) {
+                    try {
+                        Files.createDirectories(dstFile)
+                    } catch (e: IOException) {
+                        // handled in the next iteration
+                    }
+                } else {
+                    val copy = try {
+                        src.copyTo(dstFile, overwrite)
+                    } catch (e: IOException) {
+                        if (onError(src, e) == OnErrorAction.TERMINATE) {
+                            return false
+                        }
+                        continue
+                    }
+
+                    fun fileLength(p: Path): Long {
+                        return try {
+                            if (p.isFile()) FileChannel.open(p).use { it.size() } else 0L
+                        } catch (e: IOException) {
+                            0L
+                        }
+                    }
+
+                    if (fileLength(copy) != fileLength(src)) {
+                        if (onError(
+                                src,
+                                IOException("Source file wasn't copied completely, length of destination file differs.")
+                            ) == OnErrorAction.TERMINATE
+                        ) {
+                            return false
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    } catch (e: TerminateException) {
+        return false
+    }
+}
+
+/**
+ * Delete this file with all its children.
+ * Note that if this operation fails then partial deletion may have taken place.
+ *
+ * @return `true` if the file or directory is successfully deleted, `false` otherwise.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.deleteRecursively(): Boolean = walkBottomUp().fold(true, { res, it ->
+    try {
+        Files.delete(it)
+        true
+    } catch (e: NoSuchFileException) {
+        true
+    } catch (e: IOException) {
+        false
+    } && res
+})
+
+/**
+ * Check if this file exists.
+ *
+ * @param options Options to control how symbolic links are handled.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.exists(vararg options: LinkOption): Boolean = Files.exists(this, *options)
+
+/**
+ * Check if this path is a file.
+ *
+ * @param options Options to control how symbolic links are handled.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isFile(vararg options: LinkOption): Boolean = Files.isRegularFile(this, *options)
+
+/**
+ * Check if this path is a directory.
+ *
+ * By default, symbolic links in the path are followed.
+ *
+ * @param options Options to control how symbolic links are handled.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isDirectory(vararg options: LinkOption): Boolean = Files.isDirectory(this, *options)
+
+/**
+ * Check if this path exists and is a symbolic link.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isSymbolicLink(): Boolean = Files.isSymbolicLink(this)
+
+/**
+ * Check if this path exists and is executable.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isExecutable(): Boolean = Files.isExecutable(this)
+
+/**
+ * Check if this path is considered hidden.
+ *
+ * This check is dependant on the current filesystem. For example, on UNIX-like operating systems, a
+ * path is considered hidden if its name begins with a dot. On Windows, file attributes are checked.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isHidden(): Boolean = Files.isHidden(this)
+
+/**
+ * Check if this path exists and is readable.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isReadable(): Boolean = Files.isReadable(this)
+
+/**
+ * Check that this path exists and is writable.
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isWritable(): Boolean = Files.isWritable(this)
+
+/**
+ * Check if this path points to the same file or directory as [other].
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.isSameFile(other: Path): Boolean = Files.isSameFile(this, other)
+
+/**
+ * Return a list of the files and directories in this directory.
+ *
+ * @throws NotDirectoryException If this path does not refer to a directory
+ * @throws IOException If an I/O error occurs
+ */
+@SinceKotlin("1.3")
+@ExperimentalStdlibApi
+public fun Path.listFiles(): List<Path> {
+    return Files.newDirectoryStream(this).use { it.toList() }
+}
---
+++ libraries/stdlib/jdk7/test/PathExtensionsTest.kt
@@ -0,0 +1,353 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.jdk7.test
+
+import java.io.File
+import java.io.IOException
+import java.nio.file.*
+import java.nio.file.attribute.PosixFilePermission
+import java.nio.file.attribute.PosixFilePermission.*
+import kotlin.test.*
+
+class PathExtensionsTest {
+
+    private val isCaseInsensitiveFileSystem = Paths.get("C:/") == Paths.get("c:/")
+    private val isBackslashSeparator = File.separatorChar == '\\'
+
+    @Test
+    fun extension() {
+        assertEquals("bbb", Paths.get("aaa.bbb").extension)
+        assertEquals("", Paths.get("aaa").extension)
+        assertEquals("", Paths.get("aaa.").extension)
+        // maybe we should think that such files have name .bbb and no extension
+        assertEquals("bbb", Paths.get(".bbb").extension)
+        assertEquals("", Paths.get("/my.dir/log").extension)
+    }
+
+    @Test
+    fun nameWithoutExtension() {
+        assertEquals("aaa", Paths.get("aaa.bbb").nameWithoutExtension)
+        assertEquals("aaa", Paths.get("aaa").nameWithoutExtension)
+        assertEquals("aaa", Paths.get("aaa.").nameWithoutExtension)
+        assertEquals("", Paths.get(".bbb").nameWithoutExtension)
+        assertEquals("log", Paths.get("/my.dir/log").nameWithoutExtension)
+    }
+
+    @Test
+    fun testCopyTo() {
+        val srcFile = Files.createTempFile(null, null)
+        val dstFile = Files.createTempFile(null, null)
+        try {
+            srcFile.writeText("Hello, World!")
+            assertFailsWith(FileAlreadyExistsException::class, "copy do not overwrite existing file") {
+                srcFile.copyTo(dstFile)
+            }
+
+            var dst = srcFile.copyTo(dstFile, overwrite = true)
+            assertSame(dst, dstFile)
+            compareFiles(srcFile, dst, "copy with overwrite over existing file")
+
+            assertTrue(Files.deleteIfExists(dstFile))
+            dst = srcFile.copyTo(dstFile)
+            compareFiles(srcFile, dst, "copy to new file")
+
+            assertTrue(Files.deleteIfExists(dstFile))
+            Files.createDirectory(dstFile)
+            val child = dstFile.resolve("child")
+            Files.createFile(child)
+            assertFailsWith(DirectoryNotEmptyException::class, "copy with overwrite do not overwrite non-empty dir") {
+                srcFile.copyTo(dstFile, overwrite = true)
+            }
+            Files.delete(child)
+
+            srcFile.copyTo(dstFile, overwrite = true)
+            assertEquals(srcFile.readText(), dstFile.readText(), "copy with overwrite over empty dir")
+
+            assertTrue(Files.deleteIfExists(srcFile))
+            assertTrue(Files.deleteIfExists(dstFile))
+
+            assertFailsWith(NoSuchFileException::class) {
+                srcFile.copyTo(dstFile)
+            }
+
+            Files.createDirectory(srcFile)
+            srcFile.resolve("somefile").writeText("some content")
+            dstFile.writeText("")
+            assertFailsWith(FileAlreadyExistsException::class, "copy dir do not overwrite file") {
+                srcFile.copyTo(dstFile)
+            }
+            srcFile.copyTo(dstFile, overwrite = true)
+            assertTrue(dstFile.isDirectory())
+            assertTrue(dstFile.listFiles().isEmpty(), "only directory is copied, but not its content")
+
+            assertFailsWith(FileAlreadyExistsException::class, "copy dir do not overwrite dir") {
+                srcFile.copyTo(dstFile)
+            }
+
+            srcFile.copyTo(dstFile, overwrite = true)
+            assertTrue(dstFile.isDirectory())
+            assertTrue(dstFile.listFiles().isEmpty(), "only directory is copied, but not its content")
+
+            dstFile.resolve("somefile2").writeText("some content2")
+            assertFailsWith(FileAlreadyExistsException::class, "copy dir do not overwrite non-empty dir") {
+                srcFile.copyTo(dstFile, overwrite = true)
+            }
+        } finally {
+            srcFile.deleteRecursively()
+            dstFile.deleteRecursively()
+        }
+    }
+
+    @Test
+    fun copyToNameWithoutParent() {
+        val currentDir = Paths.get("").toAbsolutePath()
+        val srcFile = Files.createTempFile(null, null)
+        val dstFile = Files.createTempFile(currentDir, null, null)
+        try {
+            srcFile.writeText("Hello, World!", Charsets.UTF_8)
+            Files.delete(dstFile)
+
+            val dstRelative = Paths.get(dstFile.fileName.toString())
+
+            srcFile.copyTo(dstRelative)
+
+            assertEquals(srcFile.readText(), dstFile.readText())
+        } finally {
+            Files.delete(dstFile)
+            Files.delete(srcFile)
+        }
+    }
+
+    @Test
+    fun deleteRecursively() {
+        val dir = Files.createTempDirectory(null)
+        val subDir = dir.resolve("subdir")
+        Files.createDirectory(subDir)
+        Files.createFile(dir.resolve("test1.txt"))
+        Files.createFile(subDir.resolve("test2.txt"))
+
+        assertTrue(dir.deleteRecursively())
+        assertFalse(dir.exists())
+        assertTrue(dir.deleteRecursively())
+    }
+
+    @Test
+    fun deleteRecursivelyWithFail() {
+        val basedir = PathTreeWalkTest.createTestFiles()
+        val restricted = basedir.resolve("1")
+        val restrictedPermissions = restricted.setNotReadable() ?: return
+        val seven = basedir.resolve("7.txt")
+        val sevenPermissions = seven.setNotReadable() ?: return
+        try {
+            assertFalse(basedir.deleteRecursively(), "Expected incomplete recursive deletion.")
+            restricted.restorePermission(restrictedPermissions)
+            seven.restorePermission(sevenPermissions)
+            var i = 0
+            for (file in basedir.walkTopDown()) {
+                i++
+            }
+            assertEquals(6, i)
+        } finally {
+            restricted.restorePermission(restrictedPermissions)
+            seven.restorePermission(sevenPermissions)
+            basedir.deleteRecursively()
+        }
+    }
+
+    private fun compareFiles(src: Path, dst: Path, message: String? = null) {
+        assertTrue(dst.exists())
+        assertEquals(src.isFile(), dst.isFile(), message)
+        if (dst.isFile()) {
+            assertTrue(src.readBytes().contentEquals(dst.readBytes()), message)
+        }
+    }
+
+    private fun compareDirectories(src: Path, dst: Path) {
+        for (srcFile in src.walkTopDown()) {
+            val dstFile = dst.resolve(src.relativize(srcFile))
+            compareFiles(srcFile, dstFile)
+        }
+    }
+
+    @Test
+    fun copyRecursively() {
+        val src = Files.createTempDirectory(null)
+        val dst = Files.createTempDirectory(null)
+        Files.delete(dst)
+        fun check() = compareDirectories(src, dst)
+
+        try {
+            val subDir1 = Files.createTempDirectory(src, "d1")
+            val subDir2 = Files.createTempDirectory(src, "d2")
+            Files.createTempDirectory(subDir1, "d1_")
+            val file1 = Files.createTempFile(src, "f1", null)
+            val file2 = Files.createTempFile(subDir1, "f2_", null)
+            file1.writeText("hello")
+            file2.writeText("wazzup")
+            Files.createTempDirectory(subDir2, "d1_")
+
+            assertTrue(src.copyRecursively(dst))
+            check()
+
+            assertFailsWith(FileAlreadyExistsException::class) {
+                src.copyRecursively(dst)
+            }
+
+            var conflicts = 0
+            src.copyRecursively(dst) { _: Path, e: IOException ->
+                if (e is FileAlreadyExistsException) {
+                    conflicts++
+                    OnErrorAction.SKIP
+                } else {
+                    throw e
+                }
+            }
+            assertEquals(2, conflicts)
+
+            val oldPermissions = subDir1.setNotReadable()
+            if (oldPermissions != null) {
+                try {
+                    dst.deleteRecursively()
+                    var caught = false
+                    assertTrue(src.copyRecursively(dst) { _: Path, e: IOException ->
+                        if (e is AccessDeniedException) {
+                            caught = true
+                            OnErrorAction.SKIP
+                        } else {
+                            throw e
+                        }
+                    })
+                    assertTrue(caught)
+                    check()
+                } finally {
+                    subDir1.restorePermission(oldPermissions)
+                }
+            }
+
+            src.deleteRecursively()
+            dst.deleteRecursively()
+            assertFailsWith(NoSuchFileException::class) {
+                src.copyRecursively(dst)
+            }
+
+            assertFalse(src.copyRecursively(dst) { _, _ -> OnErrorAction.TERMINATE })
+        } finally {
+            src.deleteRecursively()
+            dst.deleteRecursively()
+        }
+    }
+
+    @Test
+    fun copyRecursivelyWithOverwrite() {
+        val src = Files.createTempDirectory(null)
+        val dst = Files.createTempDirectory(null)
+        fun check() = compareDirectories(src, dst)
+
+        try {
+            val srcFile = src.resolve("test")
+            val dstFile = dst.resolve("test")
+            srcFile.writeText("text1")
+
+            src.copyRecursively(dst)
+
+            srcFile.writeText("text1 modified")
+            src.copyRecursively(dst, overwrite = true)
+            check()
+
+            Files.delete(dstFile)
+            Files.createDirectory(dstFile)
+            dstFile.resolve("subFile").writeText("subfile")
+            src.copyRecursively(dst, overwrite = true)
+            check()
+
+            Files.delete(srcFile)
+            Files.createDirectory(srcFile)
+            srcFile.resolve("subFile").writeText("text2")
+            src.copyRecursively(dst, overwrite = true)
+            check()
+        } finally {
+            src.deleteRecursively()
+            dst.deleteRecursively()
+        }
+    }
+
+    @Test
+    fun testPrintWriter() {
+        val file = Files.createTempFile(null, null)
+        val writer = file.printWriter()
+        val str1 = "Hello, world!"
+        val str2 = "Everything is wonderful!"
+        writer.println(str1)
+        writer.println(str2)
+        writer.close()
+        val writer2 = file.printWriter(StandardOpenOption.APPEND)
+        val str3 = "Hello again!"
+        writer2.println(str3)
+        writer2.close()
+        val reader = file.bufferedReader()
+        assertEquals(str1, reader.readLine())
+        assertEquals(str2, reader.readLine())
+        assertEquals(str3, reader.readLine())
+    }
+
+    @Test
+    fun testWriteBytes() {
+        val file = Files.createTempFile(null, null)
+        file.writeBytes("Hello".encodeToByteArray())
+        file.appendBytes(" world!".encodeToByteArray())
+        assertEquals(file.readText(), "Hello world!")
+    }
+
+    @Test
+    fun testAttributeGetters() {
+        val file = Files.createTempFile(null, null)
+        assertTrue(file.exists())
+        assertTrue(file.isFile())
+        assertFalse(file.isDirectory())
+        assertFalse(file.isSymbolicLink())
+        assertTrue(file.isReadable())
+        assertTrue(file.isWritable())
+        assertTrue(file.isSameFile(file))
+
+        // The default value of these depends on the current operating system, so just check that
+        // they don't throw an exception.
+        file.isExecutable()
+        file.isHidden()
+    }
+
+    @Test
+    fun testListFiles() {
+        val dir = Files.createTempDirectory(null)
+        assertEquals(dir.listFiles().size, 0)
+
+        val file = dir.resolve("f1")
+        Files.createFile(file)
+        assertEquals(dir.listFiles().size, 1)
+
+        assertFailsWith<NotDirectoryException> { file.listFiles() }
+    }
+}
+
+fun Path.setNotReadable(): Set<PosixFilePermission>? {
+    val oldPermissions = try {
+        Files.getPosixFilePermissions(this)
+    } catch (_: UnsupportedOperationException) {
+        return null
+    }
+    Files.setPosixFilePermissions(
+        this,
+        oldPermissions - setOf(GROUP_READ, OTHERS_READ, OWNER_READ)
+    )
+    return oldPermissions
+}
+
+fun Path.restorePermission(permissions: Set<PosixFilePermission>?) {
+    if (permissions == null || !this.exists()) return
+    try {
+        Files.setPosixFilePermissions(this, permissions)
+    } catch (_: UnsupportedOperationException) {
+    }
+}
