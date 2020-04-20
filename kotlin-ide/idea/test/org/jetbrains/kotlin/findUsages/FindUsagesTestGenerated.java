02192f765cce8c4a4901c4b4fc80f891ca36d20e
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-04-20 13:23:36 +0300

    Fix invalid signature for UL method
    Fixed #KT-32245
    
    Original commit: 8a7aac728d3a8373155cb02489707457c2c344a2

--- kotlin-ide/idea/test/org/jetbrains/kotlin/findUsages/FindUsagesTestGenerated.java
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/findUsages/FindUsagesTestGenerated.java
@@ -1764,6 +1764,11 @@ public class FindUsagesTestGenerated extends AbstractFindUsagesTest {
                 runTest("idea/testData/findUsages/java/findJavaMethodUsages/OverridenRawGenericSignatureBase.0.java");
             }
 
+            @TestMetadata("OverridenRawGenericSignatureBase2.0.java")
+            public void testOverridenRawGenericSignatureBase2() throws Exception {
+                runTest("idea/testData/findUsages/java/findJavaMethodUsages/OverridenRawGenericSignatureBase2.0.java");
+            }
+
             @TestMetadata("SyntheticProperties.0.java")
             public void testSyntheticProperties() throws Exception {
                 runTest("idea/testData/findUsages/java/findJavaMethodUsages/SyntheticProperties.0.java");
