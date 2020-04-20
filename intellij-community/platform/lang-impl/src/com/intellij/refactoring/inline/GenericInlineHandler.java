e028d401b39b53c52019b846abb456c90b6351e4
Peter Gromov <peter@jetbrains.com>
2020-04-20 07:39:29 +0200

    inline refactorings: take write action inside command
    to fully enable postponed formatting (which in tests differently from production, IDEA-238172)
    
    intellij-monorepo: 2576afd639a0c9c48324f279dccd2d6fed5d9f86

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/inline/GenericInlineHandler.java
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/inline/GenericInlineHandler.java
@@ -7,8 +7,7 @@ import com.intellij.find.FindBundle;
 import com.intellij.lang.Language;
 import com.intellij.lang.refactoring.InlineHandler;
 import com.intellij.lang.refactoring.InlineHandlers;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.progress.ProgressManager;
@@ -78,10 +77,9 @@ public class GenericInlineHandler {
     if (!CommonRefactoringUtil.checkReadOnlyStatusRecursively(project, elements, true)) {
       return true;
     }
-    ApplicationManager.getApplication().runWriteAction(() -> {
-      final String subj = element instanceof PsiNamedElement ? ((PsiNamedElement)element).getName() : "element";
-
-      CommandProcessor.getInstance().executeCommand(project, () -> {
+    String subj = element instanceof PsiNamedElement ? ((PsiNamedElement)element).getName() : "element";
+    WriteCommandAction.runWriteCommandAction(
+      project, RefactoringBundle.message("inline.command", StringUtil.notNullize(subj, "<nameless>")), null, () -> {
         final PsiReference[] references = sortDepthFirstRightLeftOrder(allReferences);
 
 
@@ -97,8 +95,7 @@ public class GenericInlineHandler {
         if (!settings.isOnlyOneReferenceToInline()) {
           languageSpecific.removeDefinition(element, settings);
         }
-      }, RefactoringBundle.message("inline.command", StringUtil.notNullize(subj, "<nameless>")), null);
-    });
+      });
     return true;
   }
 
