e3d20a2f5cdb7181eb99d650796403653a2e0ddb
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-04-20 23:25:18 +0300

    Update module-info exports for stdlib and suppress warnings
    - include kotlin.time package
    
    Suppress module-info warnings:
    
    - terminal digits in module name
    - open/export for another not yet built dependee module
    
    #KT-21266

--- libraries/stdlib/jdk7/java9/module-info.java
+++ libraries/stdlib/jdk7/java9/module-info.java
@@ -1,3 +1,4 @@
+@SuppressWarnings("module") // suppress warnings about terminal digit and exporting packages to not-yet-built kotlin-stdlib-jdk8
 module kotlin.stdlib.jdk7 {
     requires transitive kotlin.stdlib;
 
