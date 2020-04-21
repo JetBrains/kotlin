f89a176c3ce6bf84cd6c09d205e1061b5d8f72e9
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-04-21 19:05:49 +0700

    [Linker][Interop] Do not search in interop dependencies
    (cherry picked from commit df3d1f7a204c3c052a3f06e4b1fec1c33d4a9670)

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrlinker.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrlinker.kt
@@ -86,7 +86,10 @@ internal class KonanIrLinker(
             assert(moduleDescriptor.kotlinLibrary.isInteropLibrary())
         }
 
-        private val descriptorByIdSignatureFinder = DescriptorByIdSignatureFinder(moduleDescriptor, KonanManglerDesc)
+        private val descriptorByIdSignatureFinder = DescriptorByIdSignatureFinder(
+                moduleDescriptor, KonanManglerDesc,
+                DescriptorByIdSignatureFinder.LookupMode.MODULE_ONLY
+        )
         private fun IdSignature.isInteropSignature(): Boolean = IdSignature.Flags.IS_NATIVE_INTEROP_LIBRARY.test()
 
         override fun contains(idSig: IdSignature): Boolean {
