1f3ecf4555c261a15a338c668e766caa88602436
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-04-22 15:42:23 +0200

    fix checking _LastInSuiteTest check for dynamic plugins
    intellij-monorepo: c1800df23756afbec05ba4bc4299f87df79dd329

--- intellij-community/platform/lang-impl/src/com/intellij/framework/detection/impl/FrameworkDetectorRegistryImpl.java
+++ intellij-community/platform/lang-impl/src/com/intellij/framework/detection/impl/FrameworkDetectorRegistryImpl.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.framework.detection.impl;
 
 import com.intellij.framework.FrameworkType;
@@ -27,7 +13,6 @@ import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.util.Pair;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.util.containers.MultiMap;
-import com.intellij.util.indexing.FileBasedIndex;
 import com.intellij.util.indexing.FileContent;
 import com.intellij.util.io.PathKt;
 import gnu.trove.TIntObjectHashMap;
@@ -40,7 +25,7 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class FrameworkDetectorRegistryImpl extends FrameworkDetectorRegistry implements Disposable {
+public final class FrameworkDetectorRegistryImpl extends FrameworkDetectorRegistry implements Disposable {
   private static final Logger LOG = Logger.getInstance(FrameworkDetectorRegistryImpl.class);
   private static final int REGISTRY_VERSION = 0;
   private TObjectIntHashMap<String> myDetectorIds;
@@ -194,7 +179,6 @@ public class FrameworkDetectorRegistryImpl extends FrameworkDetectorRegistry imp
     myDetectorsMap = null;
     myDetectorsVersion++;
     saveDetectors();
-    ApplicationManager.getApplication().invokeLater(() -> FileBasedIndex.getInstance().requestRebuild(FrameworkDetectionIndex.NAME));
   }
 
   private static Path getDetectorsRegistryFile() {
