4a53458f1a1e901145a7c6642c968796275bdc75
Eugene Petrenko <eugene.petrenko@gmail.com>
2020-04-23 16:21:08 +0200

    cleanup API in FilePropertyPusher (update deprecated usages)
    intellij-monorepo: 3aa1dbbba763de9ae0e5d30fc1c30e790d49994e

--- intellij-community/platform/lang-impl/src/com/intellij/psi/templateLanguages/TemplateDataLanguagePusher.java
+++ intellij-community/platform/lang-impl/src/com/intellij/psi/templateLanguages/TemplateDataLanguagePusher.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.psi.templateLanguages;
 
 import com.intellij.lang.Language;
@@ -78,7 +64,7 @@ public class TemplateDataLanguagePusher implements FilePropertyPusher<Language> 
   }
 
   @Override
-  public boolean acceptsFile(@NotNull VirtualFile file) {
+  public boolean acceptsFile(@NotNull VirtualFile file, @NotNull Project project) {
     FileType type = file.getFileType();
     return type instanceof LanguageFileType && ((LanguageFileType)type).getLanguage() instanceof TemplateLanguage;
   }
@@ -106,7 +92,7 @@ public class TemplateDataLanguagePusher implements FilePropertyPusher<Language> 
       try (DataOutputStream oStream = PERSISTENCE.writeAttribute(fileOrDir)) {
         DataInputOutputUtil.writeINT(oStream, ourLanguagesEnumerator.getId(value.getID()));
       }
-      PushedFilePropertiesUpdater.getInstance(project).filePropertiesChanged(fileOrDir, this::acceptsFile);
+      PushedFilePropertiesUpdater.getInstance(project).filePropertiesChanged(fileOrDir, file -> acceptsFile(file, project));
     }
   }
 }
