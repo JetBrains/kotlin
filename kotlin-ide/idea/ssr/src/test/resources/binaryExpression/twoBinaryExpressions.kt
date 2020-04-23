3cec65d5b5735e4898e940a9cc93a3c685089009
Louis Vignier <louis.vignier@jetbrains.com>
2020-04-23 14:39:11 +0200

    Removed SkipBlockHandler, skipping block element in createPatternTree

--- kotlin-ide/idea/ssr/src/test/resources/binaryExpression/twoBinaryExpressions.kt
+++ kotlin-ide/idea/ssr/src/test/resources/binaryExpression/twoBinaryExpressions.kt
@@ -2,7 +2,9 @@ fun a() {
     var a = 0
     var b = 0
     print(a + b)
-    <warning descr="SSR">a = 1
-    b = 2</warning>
+    <warning descr="SSR">a = 1</warning>
+    b = 2
+    print(a + b)
+    a = 1
     print(a + b)
 }
