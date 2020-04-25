a867c2122e66845150e1e11f7cbe72485256594d
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-04-25 18:43:39 +0300

    Indexes: extract abstraction (deserializer of a map) from InputMapExternalizer.
    intellij-monorepo: 3680fc5b468a23849f558fa96a72000046dcdfa2

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/InputMapExternalizer.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/InputMapExternalizer.java
@@ -1,80 +1,36 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing;
 
-import com.intellij.util.SmartList;
 import com.intellij.util.indexing.impl.InputIndexDataExternalizer;
 import com.intellij.util.io.DataExternalizer;
-import com.intellij.util.io.DataInputOutputUtil;
-import gnu.trove.THashMap;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
-public class InputMapExternalizer<Key, Value> implements DataExternalizer<Map<Key, Value>> {
-  private final DataExternalizer<Value> myValueExternalizer;
-  private final DataExternalizer<Collection<Key>> mySnapshotIndexExternalizer;
+public final class InputMapExternalizer<Key, Value> implements DataExternalizer<Map<Key, Value>> {
 
-  public InputMapExternalizer(IndexExtension<Key, Value, ?> extension) {
-    myValueExternalizer = extension.getValueExternalizer();
-    mySnapshotIndexExternalizer = extension instanceof CustomInputsIndexFileBasedIndexExtension
-                                  ? ((CustomInputsIndexFileBasedIndexExtension<Key>)extension).createExternalizer()
-                                  : new InputIndexDataExternalizer<>(extension.getKeyDescriptor(), ((IndexExtension<Key, ?, ?>)extension).getName());
-  }
+  private final DataExternalizer<Map<Key, Value>> myMapDataExternalizer;
 
-
-  @NotNull
-  public DataExternalizer<Value> getValueExternalizer() {
-    return myValueExternalizer;
+  public InputMapExternalizer(@NotNull IndexExtension<Key, Value, ?> extension) {
+    DataExternalizer<Value> myValueExternalizer = extension.getValueExternalizer();
+    DataExternalizer<Collection<Key>> myKeysExternalizer =
+      extension instanceof CustomInputsIndexFileBasedIndexExtension
+      ? ((CustomInputsIndexFileBasedIndexExtension<Key>)extension).createExternalizer()
+      : new InputIndexDataExternalizer<>(extension.getKeyDescriptor(), ((IndexExtension<Key, ?, ?>)extension).getName());
+    myMapDataExternalizer = new MapDataExternalizer<>(myValueExternalizer, myKeysExternalizer);
   }
 
   @Override
-  public void save(@NotNull DataOutput stream, Map<Key, Value> data) throws IOException {
-    int size = data.size();
-    DataInputOutputUtil.writeINT(stream, size);
-
-    if (size > 0) {
-      THashMap<Value, List<Key>> values = new THashMap<>();
-      List<Key> keysForNullValue = null;
-      for (Map.Entry<Key, Value> e : data.entrySet()) {
-        Value value = e.getValue();
-
-        List<Key> keys = value != null ? values.get(value):keysForNullValue;
-        if (keys == null) {
-          if (value != null) values.put(value, keys = new SmartList<>());
-          else keys = keysForNullValue = new SmartList<>();
-        }
-        keys.add(e.getKey());
-      }
-
-      if (keysForNullValue != null) {
-        myValueExternalizer.save(stream, null);
-        mySnapshotIndexExternalizer.save(stream, keysForNullValue);
-      }
-
-      for(Value value:values.keySet()) {
-        myValueExternalizer.save(stream, value);
-        mySnapshotIndexExternalizer.save(stream, values.get(value));
-      }
-    }
+  public void save(@NotNull DataOutput out, Map<Key, Value> value) throws IOException {
+    myMapDataExternalizer.save(out, value);
   }
 
   @Override
   public Map<Key, Value> read(@NotNull DataInput in) throws IOException {
-    int pairs = DataInputOutputUtil.readINT(in);
-    if (pairs == 0) return Collections.emptyMap();
-    Map<Key, Value> result = new THashMap<>(pairs);
-    while (((InputStream)in).available() > 0) {
-      Value value = myValueExternalizer.read(in);
-      Collection<Key> keys = mySnapshotIndexExternalizer.read(in);
-      for(Key k:keys) result.put(k, value);
-    }
-    return result;
+    return myMapDataExternalizer.read(in);
   }
 }
