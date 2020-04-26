6fac5c1fabcd7b35d575bdcf995592b0be2ff53d
Aleksey Pivovarov <AMPivovarov@gmail.com>
2020-04-26 17:31:12 +0300

    ui: extract loading decorator with progress bar
    intellij-monorepo: a852b8fb7fe6563f1f49433e5661cd88276bc605

--- intellij-community/platform/lang-impl/src/com/intellij/build/BuildProgressStripe.java
+++ intellij-community/platform/lang-impl/src/com/intellij/build/BuildProgressStripe.java
@@ -2,25 +2,21 @@
 package com.intellij.build;
 
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.ui.LoadingDecorator;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.ui.components.JBPanel;
-import com.intellij.ui.components.panels.NonOpaquePanel;
-import com.intellij.util.ui.AsyncProcessIcon;
+import com.intellij.ui.components.ProgressBarLoadingDecorator;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @ApiStatus.Internal
 class BuildProgressStripe extends JBPanel {
   @NotNull
   private final JBPanel myPanel;
-  private final NotNullLazyValue<MyLoadingDecorator> myCreateLoadingDecorator;
-  private MyLoadingDecorator myDecorator;
+  private final NotNullLazyValue<ProgressBarLoadingDecorator> myCreateLoadingDecorator;
+  private ProgressBarLoadingDecorator myDecorator;
 
   BuildProgressStripe(@NotNull JComponent targetComponent, @NotNull Disposable parent, int startDelayMs) {
     super(new BorderLayout());
@@ -28,7 +24,7 @@ class BuildProgressStripe extends JBPanel {
     myPanel.setOpaque(false);
     myPanel.add(targetComponent);
     myCreateLoadingDecorator = NotNullLazyValue.createValue(() -> {
-      return new MyLoadingDecorator(myPanel, parent, startDelayMs);
+      return new ProgressBarLoadingDecorator(myPanel, parent, startDelayMs);
     });
     createLoadingDecorator();
   }
@@ -66,49 +62,12 @@ class BuildProgressStripe extends JBPanel {
   }
 
   private JProgressBar getProgressBar() {
-    return myCreateLoadingDecorator.getValue().myProgressBar;
+    return myCreateLoadingDecorator.getValue().getProgressBar();
   }
 
   private void createLoadingDecorator() {
     myDecorator = myCreateLoadingDecorator.getValue();
     add(myDecorator.getComponent(), BorderLayout.CENTER);
     myDecorator.setLoadingText("");
-  }
-
-  private static class MyLoadingDecorator extends LoadingDecorator {
-    private final AtomicBoolean loadingStarted = new AtomicBoolean(false);
-    private @Nullable JProgressBar myProgressBar;
-
-
-    private MyLoadingDecorator(@NotNull JPanel contentPanel, @NotNull Disposable disposable, int startDelayMs) {
-      super(contentPanel, disposable, startDelayMs, true);
-    }
-
-    @Override
-    protected NonOpaquePanel customizeLoadingLayer(JPanel parent, JLabel text, AsyncProcessIcon icon) {
-      parent.setLayout(new BorderLayout());
-      NonOpaquePanel result = new NonOpaquePanel();
-      result.setLayout(new BoxLayout(result, BoxLayout.Y_AXIS));
-      myProgressBar = new JProgressBar();
-      myProgressBar.setIndeterminate(true);
-      myProgressBar.putClientProperty("ProgressBar.stripeWidth", 2);
-      myProgressBar.putClientProperty("ProgressBar.flatEnds", Boolean.TRUE);
-      result.add(myProgressBar);
-      parent.add(result, BorderLayout.NORTH);
-      return result;
-    }
-
-    public void startLoading() {
-      if (loadingStarted.compareAndSet(false, true)) {
-        super.startLoading(false);
-      }
-    }
-
-    @Override
-    public void stopLoading() {
-      if (loadingStarted.compareAndSet(true, false)) {
-        super.stopLoading();
-      }
-    }
   }
 }
