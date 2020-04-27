658c30b6b326df7a25bc1b4d68c2a16081f63f2a
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2020-04-28 00:47:35 +0300

    Combine usage of kotlin.ide.settings FUS collector #KT-38620 Fixed

--- idea/src/org/jetbrains/kotlin/idea/IDESettingsFUSCollector.kt
+++ idea/src/org/jetbrains/kotlin/idea/IDESettingsFUSCollector.kt
@@ -1,15 +1,16 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.idea.statistics
+package org.jetbrains.kotlin.idea
 
 import com.intellij.internal.statistic.beans.MetricEvent
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.idea.KotlinPluginUtil
+import org.jetbrains.kotlin.idea.codeInsight.KotlinCodeInsightSettings
+import org.jetbrains.kotlin.idea.codeInsight.KotlinCodeInsightWorkspaceSettings
 import org.jetbrains.kotlin.idea.core.script.settings.KotlinScriptingSettings
 import org.jetbrains.kotlin.idea.project.NewInferenceForIDEAnalysisComponent
 
@@ -23,6 +24,12 @@ class IDESettingsFUSCollector : ProjectUsagesCollector() {
 
         val scriptingAutoReloadEnabled = KotlinScriptingSettings.getInstance(project).isAutoReloadEnabled
         metrics.add(MetricEvent("scriptingAutoReloadEnabled", flagUsage(scriptingAutoReloadEnabled)))
+
+        val settings: KotlinCodeInsightSettings = KotlinCodeInsightSettings.getInstance()
+        val projectSettings: KotlinCodeInsightWorkspaceSettings = KotlinCodeInsightWorkspaceSettings.getInstance(project)
+
+        metrics.add(MetricEvent("addUnambiguousImportsOnTheFly", flagUsage(settings.addUnambiguousImportsOnTheFly)))
+        metrics.add(MetricEvent("optimizeImportsOnTheFly", flagUsage(projectSettings.optimizeImportsOnTheFly)))
 
         return metrics
     }
