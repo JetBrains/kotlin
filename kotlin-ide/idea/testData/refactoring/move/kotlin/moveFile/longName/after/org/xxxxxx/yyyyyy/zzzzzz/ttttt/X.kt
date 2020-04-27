9f4b7e83268cd7b7de458bbc05c7ccc1a6447bb8
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-04-27 23:33:55 +0700

    MoveKotlinDeclarationsProcessor: fix formatting for long qualifier names
    #KT-35338 Fixed
    
    Original commit: db5751616a56c548923155bc5439c0e1947d4fb7

---
+++ kotlin-ide/idea/testData/refactoring/move/kotlin/moveFile/longName/after/org/xxxxxx/yyyyyy/zzzzzz/ttttt/X.kt
@@ -0,0 +1,9 @@
+package org.xxxxxx.yyyyyy.zzzzzz.ttttt
+
+import com.xxxxxxx.yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy.zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.SomeVeryImportantClass
+
+class X {
+    fun foo(parameter: Any) {
+        val data = SomeVeryImportantClass.getInstance(parameter).getSomeData()
+    }
+}
