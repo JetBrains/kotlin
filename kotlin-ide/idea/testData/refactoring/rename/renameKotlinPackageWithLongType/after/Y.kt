5e90150b42e7bf86d8dad2d10f58baf6df3edc69
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-04-28 12:20:15 +0700

    KotlinChangePackageRefactoring: fix formatting for long qualifier names
    #KT-38627 Fixed
    
    Original commit: a11e6b2eeda79433e5569bc9029cc6fbd7ff0a91

---
+++ kotlin-ide/idea/testData/refactoring/rename/renameKotlinPackageWithLongType/after/Y.kt
@@ -0,0 +1,9 @@
+package cccc.zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+
+class SomeVeryImportantClass {
+    fun getSomeData(): Any? = null
+
+    companion object {
+        fun getInstance(o: Any): SomeVeryImportantClass = SomeVeryImportantClass()
+    }
+}
