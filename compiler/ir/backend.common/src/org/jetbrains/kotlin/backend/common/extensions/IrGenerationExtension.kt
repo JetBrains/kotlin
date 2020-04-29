2f0840388d0494caaa4e9c123f6b8d5740b15364
Roman Artemev <roman.artemev@jetbrains.com>
2020-04-29 20:07:31 +0300

    [IR] Fix context

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/extensions/IrGenerationExtension.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/extensions/IrGenerationExtension.kt
@@ -19,7 +19,7 @@ import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
 import org.jetbrains.kotlin.ir.symbols.*
 import org.jetbrains.kotlin.ir.util.IrDeserializer
 import org.jetbrains.kotlin.ir.util.IrExtensionGenerator
-import org.jetbrains.kotlin.ir.util.SymbolTable
+import org.jetbrains.kotlin.ir.util.ReferenceSymbolTable
 import org.jetbrains.kotlin.ir.util.TypeTranslator
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.BindingContext
@@ -29,7 +29,7 @@ class IrPluginContext(
     val moduleDescriptor: ModuleDescriptor,
     val bindingContext: BindingContext,
     val languageVersionSettings: LanguageVersionSettings,
-    val symbolTable: SymbolTable,
+    val symbolTable: ReferenceSymbolTable,
     val typeTranslator: TypeTranslator,
     override val irBuiltIns: IrBuiltIns,
     private val linker: IrDeserializer,
