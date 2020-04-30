cb84c2859e23991f3409a60fb0ccb05f7bda2403
Alexander Zolotov <zolotov@jetbrains.com>
2020-04-30 22:04:03 +0300

    Check whether editor is backed by text field using user data
    otherwise, the check requires editorComponent which is unavailable on imaginary editor
    
    intellij-monorepo: 9c30d5907b4c40c1ca5725381d7bc9d361bf65d8

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/LiveTemplateCompletionContributor.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/template/impl/LiveTemplateCompletionContributor.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.template.impl;
 
 import com.intellij.codeInsight.completion.*;
@@ -60,7 +60,7 @@ public class LiveTemplateCompletionContributor extends CompletionContributor imp
                                     @NotNull CompletionResultSet result) {
         ProgressManager.checkCanceled();
         final PsiFile file = parameters.getPosition().getContainingFile();
-        if (file instanceof PsiPlainTextFile && parameters.getEditor().getComponent().getParent() instanceof EditorTextField) {
+        if (file instanceof PsiPlainTextFile && EditorTextField.managesEditor(parameters.getEditor())) {
           return;
         }
 
