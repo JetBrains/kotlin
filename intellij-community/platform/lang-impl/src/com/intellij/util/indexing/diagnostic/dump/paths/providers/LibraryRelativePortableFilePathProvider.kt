da57c90c1b106b35debd13c798a3ae1532816f9c
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-05-04 20:03:19 +0200

    shared-indexes: reduce nesting in the code (minor)
    intellij-monorepo: a370633ab0b3a48525bdd82bf6dc6986019343e4

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/paths/providers/LibraryRelativePortableFilePathProvider.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dump/paths/providers/LibraryRelativePortableFilePathProvider.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing.diagnostic.dump.paths.providers
 
 import com.intellij.openapi.project.Project
@@ -11,21 +12,19 @@ import com.intellij.util.indexing.diagnostic.dump.paths.PortableFilePath
 object LibraryRelativePortableFilePathProvider : PortableFilePathProvider {
   override fun getRelativePortableFilePath(project: Project, virtualFile: VirtualFile): PortableFilePath.RelativePath? {
     val orderEntry = LibraryUtil.findLibraryEntry(virtualFile, project)
-    if (orderEntry is LibraryOrderEntry) {
-      val libraryName = orderEntry.libraryName
-      if (libraryName != null) {
-        for (rootType in listOf(OrderRootType.CLASSES, OrderRootType.SOURCES)) {
-          val inClassFiles = rootType == OrderRootType.CLASSES
-          for ((rootIndex, rootFile) in orderEntry.getRootFiles(rootType).withIndex()) {
-            val relativePath = VfsUtilCore.getRelativePath(virtualFile, rootFile)
-            if (relativePath != null) {
-              return PortableFilePath.RelativePath(
-                PortableFilePath.LibraryRoot(libraryName, rootIndex, inClassFiles),
-                relativePath
-              )
-            }
-          }
-        }
+    if (orderEntry !is LibraryOrderEntry) return null
+    val libraryName = orderEntry.libraryName
+    if (libraryName == null) return null
+
+    for (rootType in listOf(OrderRootType.CLASSES, OrderRootType.SOURCES)) {
+      val inClassFiles = rootType == OrderRootType.CLASSES
+      for ((rootIndex, rootFile) in orderEntry.getRootFiles(rootType).withIndex()) {
+        val relativePath = VfsUtilCore.getRelativePath(virtualFile, rootFile)
+        if (relativePath == null) continue
+        return PortableFilePath.RelativePath(
+          PortableFilePath.LibraryRoot(libraryName, rootIndex, inClassFiles),
+          relativePath
+        )
       }
     }
     return null
