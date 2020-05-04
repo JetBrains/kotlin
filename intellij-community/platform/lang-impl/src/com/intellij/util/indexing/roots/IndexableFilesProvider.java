6a68681b118dfbc7061764a2b032a4f6b7a6b866
Eugene Petrenko <eugene.petrenko@jetbrains.com>
2020-05-04 15:43:14 +0200

    IndexableFilesProvider: add javadoc and annotations
    intellij-monorepo: 9501f60e535a22b159e1593bdf4a87e56161ac2f

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/roots/IndexableFilesProvider.java
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/roots/IndexableFilesProvider.java
@@ -3,8 +3,13 @@ package com.intellij.util.indexing.roots;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ContentIterator;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileWithId;
 import com.intellij.util.containers.ConcurrentBitSet;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Provides files to be indexed for a project structure entity (module, library, SDK, etc.)
@@ -19,22 +24,36 @@ public interface IndexableFilesProvider {
   /**
    * Presentable name that can be shown in logs and used for debugging purposes.
    */
+  @NonNls
   String getDebugName();
 
   /**
    * Presentable text shown in progress indicator during indexing of files of this provider.
    */
+  @NlsContexts.ProgressText
   String getIndexingProgressText();
 
   /**
    * Presentable text shown in progress indicator during traversing of files of this provider.
    */
+  @NlsContexts.ProgressText
   String getRootsScanningProgressText();
 
   /**
    * Iterates through all files and directories corresponding to this provider.
-   * Skips already visited files by looking to [visitedFileSet].
+   * <br />
+   * The {@param visitedFileSet} is used to store positive {@link VirtualFileWithId#getId()} of Virtual Files,
+   * the implementation is free to skip other {@link VirtualFile} implementations and
+   * non-positive {@link VirtualFileWithId#getId()}.
+   * The {@param visitedFileSet} is used to implement filtering to skip already visited files by looking to [visitedFileSet].
+   * <br />
+   * The {@param fileIterator} should be invoked on every new file (with respect to {@oaram visitedFileSet},
+   * should the {@link ContentIterator#processFile(VirtualFile)} returns false, the processing should be
+   * stopped and the {@code false} should be returned from the method.
+   *
    * @return `false` if [fileIterator] has stopped iteration by returning `false`, `true` otherwise.
    */
-  boolean iterateFiles(Project project, ContentIterator fileIterator, ConcurrentBitSet visitedFileSet);
+  boolean iterateFiles(@NotNull Project project,
+                       @NotNull ContentIterator fileIterator,
+                       @NotNull ConcurrentBitSet visitedFileSet);
 }
