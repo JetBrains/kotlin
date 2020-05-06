4fd9b7308b6fcdbb9e0ee15ff82870262440a4b1
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2020-05-06 12:32:53 +0300

    [service-view] make Show Services action enabled only in Services tool window
    intellij-monorepo: bad66ec273a4800e77ea251593722f5565f6a62f

--- intellij-community/platform/lang-impl/src/com/intellij/execution/services/ShowServicesAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/services/ShowServicesAction.java
@@ -2,12 +2,29 @@
 package com.intellij.execution.services;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ui.content.Content;
 import org.jetbrains.annotations.NotNull;
 
 public class ShowServicesAction extends ToggleAction implements DumbAware {
+  @Override
+  public void update(@NotNull AnActionEvent e) {
+    super.update(e);
+    Presentation presentation = e.getPresentation();
+    ToolWindow toolWindow = e.getData(PlatformDataKeys.TOOL_WINDOW);
+    if (toolWindow == null) {
+      presentation.setEnabledAndVisible(false);
+      return;
+    }
+    Content content = toolWindow.getContentManager().getSelectedContent();
+    presentation.setEnabledAndVisible(content != null && content.getComponent() instanceof ServiceView);
+  }
+
   @Override
   public boolean isSelected(@NotNull AnActionEvent e) {
     Project project = e.getProject();
