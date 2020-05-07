ed82465c3cba6140b787f2c67dc21d12ff4ec213
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-05-07 21:57:15 +0900

    Fix eager inter-project dependency

--- prepare/ide-plugin-dependencies/kotlin-dist-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/kotlin-dist-for-ide/build.gradle.kts
@@ -8,12 +8,15 @@ idePluginDependency {
     val jar: Jar by tasks
 
     jar.apply {
-        dependsOn(":kotlin-compiler:distKotlinc")
-//        from {
-//            val distKotlincTask = project(":kotlin-compiler").tasks.getByName("distKotlinc")
-//            (distKotlincTask)
-//        }
+        val compilerProjectName = ":kotlin-compiler"
+        val distTaskName = "distKotlinc"
 
+        dependsOn("$compilerProjectName:$distTaskName")
+
+        from {
+            val distKotlincTask = project(compilerProjectName).tasks.getByName(distTaskName)
+            distKotlincTask.outputs.files
+        }
     }
 
     sourcesJar()
