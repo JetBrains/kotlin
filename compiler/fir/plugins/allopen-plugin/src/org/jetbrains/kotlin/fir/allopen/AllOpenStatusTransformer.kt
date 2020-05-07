8ecd3d8efefe33edb54fe70a232a30171402687f
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-05-07 12:09:38 +0300

    [FIR-PLUGIN] Fix allopen plugin due to previous commits

--- compiler/fir/plugins/allopen-plugin/src/org/jetbrains/kotlin/fir/allopen/AllOpenStatusTransformer.kt
+++ compiler/fir/plugins/allopen-plugin/src/org/jetbrains/kotlin/fir/allopen/AllOpenStatusTransformer.kt
@@ -9,12 +9,22 @@ import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclarationStatus
+import org.jetbrains.kotlin.fir.extensions.AnnotationFqn
 import org.jetbrains.kotlin.fir.extensions.FirStatusTransformerExtension
 import org.jetbrains.kotlin.fir.extensions.transform
+import org.jetbrains.kotlin.name.FqName
 
 class AllOpenStatusTransformer(session: FirSession) : FirStatusTransformerExtension(session) {
     override fun transformStatus(declaration: FirDeclaration, status: FirDeclarationStatus): FirDeclarationStatus {
         if (status.modality != null) return status
         return status.transform(modality = Modality.OPEN)
     }
+
+    override val mode: Mode
+        get() = Mode.ALL_IN_ANNOTATED_ELEMENT
+
+    override val annotations: Set<AnnotationFqn> = setOf(FqName.fromSegments(listOf("allopen", "Open")))
+
+    override val metaAnnotations: Set<AnnotationFqn>
+        get() = emptySet()
 }
--- compiler/fir/plugins/allopen-plugin/testData/simple.kt
+++ compiler/fir/plugins/allopen-plugin/testData/simple.kt
@@ -1,9 +1,21 @@
+// FILE: annotations.kt
+
+package allopen
+
+annotation class Open
+
+// FILE: main.kt
+
+import allopen.Open
+
+@Open
 class A {
     fun foo() {
 
     }
 }
 
+@Open
 class B : A() {
     override fun foo() {
 
