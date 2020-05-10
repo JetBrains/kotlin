4f3d47eed308a37e49c2a061aea884e70043599d
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-05-10 20:13:29 +0300

    Remove unused equals and hashCode methods from Primitive class
    They were used only when primitive objects were put in the collection
    as they were, in Primitive state wrapper

--- compiler/ir/ir.interpreter/src/org/jetbrains/kotlin/ir/interpreter/state/Primitive.kt
+++ compiler/ir/ir.interpreter/src/org/jetbrains/kotlin/ir/interpreter/state/Primitive.kt
@@ -26,26 +26,6 @@ internal class Primitive<T>(val value: T, val type: IrType) : State {
         return if (owner.isFakeOverride) owner.getLastOverridden() else owner
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Primitive<*>
-
-        if (value != other.value) return false
-        if (type != other.type) return false
-        if (fields != other.fields) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = value?.hashCode() ?: 0
-        result = 31 * result + type.hashCode()
-        result = 31 * result + fields.hashCode()
-        return result
-    }
-
     override fun toString(): String {
         return "Primitive(value=$value, type=${irClass.defaultType})"
     }
