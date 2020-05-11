b52c76de2072639dcbae22ff5561c734a8f82915
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2020-05-11 23:40:01 +0300

    Fixed import of MPP modules with single artifacts
    #KT-38037 Fixed
    
    Original commit: 2eb354f21178f7c9428d2b00464d6b94070d405b

--- kotlin-ide/gradle/gradle-idea/test/org/jetbrains/kotlin/gradle/NewMultiplatformProjectImportingTest.kt
+++ kotlin-ide/gradle/gradle-idea/test/org/jetbrains/kotlin/gradle/NewMultiplatformProjectImportingTest.kt
@@ -942,6 +942,30 @@ class NewMultiplatformProjectImportingTest : MultiplePluginVersionGradleImportin
     }
 
 
+    @Test
+    @PluginTargetVersions(gradleVersion = "4.0+", pluginVersion = "1.3.30+")
+    fun testMutableArtifactLists() {
+        configureByFiles()
+        importProject(true)
+        checkProjectStructure(true, false, false) {
+            module("KT38037") {}
+            module("KT38037.mpp-bottom-actual") {}
+            module("KT38037.mpp-bottom-actual.commonMain") {}
+            module("KT38037.mpp-bottom-actual.commonTest") {}
+            module("KT38037.mpp-bottom-actual.jvm18Main") {}
+            module("KT38037.mpp-bottom-actual.jvm18Test") {}
+            module("KT38037.mpp-bottom-actual.main") {}
+            module("KT38037.mpp-bottom-actual.test") {}
+            module("KT38037.mpp-mid-actual") {}
+            module("KT38037.mpp-mid-actual.commonMain") {}
+            module("KT38037.mpp-mid-actual.commonTest") {}
+            module("KT38037.mpp-mid-actual.jvmWithJavaMain") {}
+            module("KT38037.mpp-mid-actual.jvmWithJavaTest") {}
+            module("KT38037.mpp-mid-actual.main") {}
+            module("KT38037.mpp-mid-actual.test") {}
+        }
+    }
+
     private fun checkProjectStructure(
         exhaustiveModuleList: Boolean = true,
         exhaustiveSourceSourceRootList: Boolean = true,
