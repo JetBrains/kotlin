ba50a8275c4e5a02424234212958bd0ec6fa6644
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2020-05-12 06:35:33 +0300

    Add test for [KT-3706]
    Issue is already fixed by metadata-based cinterop
    
    (cherry picked from commit fd4f5b2c07ebdc431d6243ac087f911ee30e88ca)

---
+++ kotlin-native/backend.native/tests/interop/objc/tests/KT37067_prefix.h
@@ -0,0 +1,12 @@
+#import <objc/NSObject.h>
+
+
+@interface KT37067_Impl : NSObject
+
+@property (nonatomic, readonly) NSObject *newValue;
+@property (nonatomic, readonly) NSObject *allocValue;
+@property (nonatomic, readonly) NSObject *copyValue;
+@property (nonatomic, readonly) NSObject *mutableCopyValue;
+
+@end;
+
