842dbb900645fb04da7a40e9eff3cadada070661
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2020-05-12 06:35:33 +0300

    Add test for [KT-3706]
    Issue is already fixed by metadata-based cinterop
    
    (cherry picked from commit fd4f5b2c07ebdc431d6243ac087f911ee30e88ca)

---
+++ kotlin-native/backend.native/tests/interop/objc/tests/KT37067_prefix.m
@@ -0,0 +1,20 @@
+#include "KT37067_prefix.h"
+
+@implementation KT37067_Impl : NSObject
+
+/// KT-37067 is a cinterop issue, so .m implementaion is not relevant at all,
+// but I still need to make objc compilable with `-fobjc-arc` option
+- (NSObject *)newValue {
+    return nil;
+}
+- (NSObject *)allocValue {
+    return nil;
+}
+- (NSObject *)copyValue {
+    return nil;
+}
+- (NSObject *)mutableCopyValue {
+    return nil;
+}
+
+@end;
