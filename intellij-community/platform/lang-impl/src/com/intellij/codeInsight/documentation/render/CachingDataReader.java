a33c2cbb1b80404c17125d939873538f0c3256d0
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2020-05-13 11:56:34 +0300

    IDEA-238114 IntelliJ Idea 2020.1 memory runaway and crash
    make cache restore the data if files are deleted due to some reason
    
    intellij-monorepo: aae744aeb73d2d7b3005df711a0bffa168876847

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/documentation/render/CachingDataReader.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/documentation/render/CachingDataReader.java
@@ -66,7 +66,7 @@ public final class CachingDataReader implements Disposable {
     File file = new File(folder, generateFileName());
     try {
       Files.write(file.toPath(), data);
-      myCache.putIfAbsent(url, file);
+      myCache.put(url, file);
     }
     catch (IOException e) {
       LOG.warn("Error writing to " + file, e);
