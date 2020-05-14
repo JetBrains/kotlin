34bdd95e50fa2c87aba889c1024b7984b6f9694b
Roman Artemev <roman.artemev@jetbrains.com>
2020-05-14 14:03:04 +0300

    [JS SCRIPT] Enable js script test along with other script tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -607,6 +607,7 @@ tasks {
         dependsOn(":kotlin-scripting-jsr223-test:embeddableTest")
         dependsOn(":kotlin-main-kts-test:test")
         dependsOn(":kotlin-scripting-ide-services-test:test")
+        dependsOn(":kotlin-scripting-js-test:test")
     }
 
     register("compilerTest") {
