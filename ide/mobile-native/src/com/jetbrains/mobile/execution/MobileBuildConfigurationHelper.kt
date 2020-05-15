a91dba563b318373f74e1ba20e2eb52ca69740d6
Egor Zhdan <egor.zhdan@jetbrains.com>
2020-05-15 17:46:49 +0300

    Mobile: remove unneeded code

--- ide/mobile-native/src/com/jetbrains/mobile/execution/MobileBuildConfigurationHelper.kt
+++ ide/mobile-native/src/com/jetbrains/mobile/execution/MobileBuildConfigurationHelper.kt
@@ -7,23 +7,12 @@ package com.jetbrains.mobile.execution
 
 import com.intellij.openapi.project.Project
 import com.jetbrains.cidr.execution.BuildTargetAndConfigurationData
-import com.jetbrains.cidr.execution.BuildTargetData
 import com.jetbrains.cidr.execution.CidrBuildConfigurationHelper
 
 class MobileBuildConfigurationHelper(val project: Project) : CidrBuildConfigurationHelper<MobileBuildConfiguration, MobileBuildTarget>() {
     override fun allowEditBuildConfiguration(): Boolean = false
 
-    override fun getTargets(): List<MobileBuildTarget> {
-        // TODO retrieve from external system project tree
-        return emptyList()
-    }
-
-    override fun <T : MobileBuildTarget> findTarget(data: BuildTargetData?, buildTargets: List<T>): T? =
-        if (data == null) null else
-            buildTargets.find { it.projectName == data.projectName && it.name == data.targetName }
-
-    override fun getDefaultConfiguration(buildTarget: MobileBuildTarget?): MobileBuildConfiguration? =
-        getConfigurations(buildTarget).firstOrNull()
+    override fun getTargets(): List<MobileBuildTarget> = emptyList()
 
     override fun findSimilarValidInTargets(
         buildTarget: MobileBuildTarget?,
