087e8420059120236b436f2c48f195e0e183a81b
Florian Kistner <florian.kistner@jetbrains.com>
2020-05-15 17:50:24 +0200

    Revert "Allow lookup tracking directly with VirtualFiles where available"
    This reverts commit 0dfcc672f50ec54941fdaba59b7b3ce6a647ccd1.

--- compiler/frontend/src/org/jetbrains/kotlin/incremental/KotlinLookupLocation.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/incremental/KotlinLookupLocation.kt
@@ -1,6 +1,17 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jetbrains.kotlin.incremental
@@ -18,11 +29,10 @@ class KotlinLookupLocation(val element: KtElement) : LookupLocation {
         get() {
             val containingJetFile = element.containingKtFile
             if (containingJetFile.doNotAnalyze != null) return null
-            val virtualFile = containingJetFile.virtualFile ?: return null
+            val filePath = containingJetFile.virtualFilePath ?: return null
 
-            return object : KotlinLocationInfo(virtualFile) {
-                override val filePath: String
-                    get() = checkNotNull(containingJetFile.virtualFilePath)
+            return object : LocationInfo {
+                override val filePath = filePath
 
                 override val position: Position
                     get() = getLineAndColumnInPsiFile(containingJetFile, element.textRange).let { Position(it.line, it.column) }
--- core/descriptors/src/org/jetbrains/kotlin/incremental/utils.kt
+++ core/descriptors/src/org/jetbrains/kotlin/incremental/utils.kt
@@ -1,6 +1,17 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jetbrains.kotlin.incremental
@@ -9,6 +20,7 @@ import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.PackageFragmentDescriptor
 import org.jetbrains.kotlin.incremental.components.LookupLocation
 import org.jetbrains.kotlin.incremental.components.LookupTracker
+import org.jetbrains.kotlin.incremental.components.Position
 import org.jetbrains.kotlin.incremental.components.ScopeKind
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.resolve.DescriptorUtils
@@ -17,7 +29,9 @@ import org.jetbrains.kotlin.resolve.DescriptorUtils
 
 fun LookupTracker.record(from: LookupLocation, scopeOwner: ClassDescriptor, name: Name) {
     if (this === LookupTracker.DO_NOTHING) return
-    record(from.location ?: return, DescriptorUtils.getFqName(scopeOwner).asString(), ScopeKind.CLASSIFIER, name.asString())
+    val location = from.location ?: return
+    val position = if (requiresPosition) location.position else Position.NO_POSITION
+    record(location.filePath, position, DescriptorUtils.getFqName(scopeOwner).asString(), ScopeKind.CLASSIFIER, name.asString())
 }
 
 fun LookupTracker.record(from: LookupLocation, scopeOwner: PackageFragmentDescriptor, name: Name) {
@@ -26,7 +40,9 @@ fun LookupTracker.record(from: LookupLocation, scopeOwner: PackageFragmentDescri
 
 fun LookupTracker.recordPackageLookup(from: LookupLocation, packageFqName: String, name: String) {
     if (this === LookupTracker.DO_NOTHING) return
-    record(from.location ?: return, packageFqName, ScopeKind.PACKAGE, name)
+    val location = from.location ?: return
+    val position = if (requiresPosition) location.position else Position.NO_POSITION
+    record(location.filePath, position, packageFqName, ScopeKind.PACKAGE, name)
 }
 
 const val ANDROID_LAYOUT_CONTENT_LOOKUP_NAME = "<LAYOUT-CONTENT>"
