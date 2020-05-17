6c4530f1068a3be84d9568f45373adc85ee78831
Henrik Tunedal <tunedal@gmail.com>
2020-05-17 21:07:03 +0200

    Use system-specific user cache directory

--- libraries/tools/kotlin-main-kts/src/org/jetbrains/kotlin/mainKts/scriptDef.kt
+++ libraries/tools/kotlin-main-kts/src/org/jetbrains/kotlin/mainKts/scriptDef.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.script.util.Repository
 import java.io.File
 import java.nio.ByteBuffer
 import java.security.MessageDigest
+import java.util.Properties
 import kotlin.script.dependencies.ScriptContents
 import kotlin.script.dependencies.ScriptDependenciesResolver
 import kotlin.script.experimental.annotations.KotlinScript
@@ -65,8 +66,8 @@ class MainKtsScriptDefinition : ScriptCompilationConfiguration(
                 val cacheExtSetting = System.getProperty(COMPILED_SCRIPTS_CACHE_DIR_PROPERTY)
                     ?: System.getenv(COMPILED_SCRIPTS_CACHE_DIR_ENV_VAR)
                 val cacheBaseDir = when {
-                    cacheExtSetting == null -> System.getProperty("java.io.tmpdir")
-                        ?.let(::File)?.takeIf { it.exists() && it.isDirectory }
+                    cacheExtSetting == null -> Directories(System.getProperties(), System.getenv()).cache
+                        ?.takeIf { it.exists() && it.isDirectory }
                         ?.let { File(it, "main.kts.compiled.cache").apply { mkdir() } }
                     cacheExtSetting.isBlank() -> null
                     else -> File(cacheExtSetting)
@@ -173,3 +174,66 @@ private fun compiledScriptUniqueName(script: SourceCode, scriptCompilationConfig
 private fun ByteArray.toHexString(): String = joinToString("", transform = { "%02x".format(it) })
 
 private fun Int.toByteArray() = ByteBuffer.allocate(Int.SIZE_BYTES).also { it.putInt(this) }.array()
+
+class Directories(
+    private val systemProperties: Properties,
+    private val environment: Map<String, String>)
+{
+    // Links to recommendations for storing various kinds of files on different platforms:
+    // Windows: http://www.microsoft.com/security/portal/mmpc/shared/variables.aspx
+    // unix (freedesktop): http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+    // OS X: https://developer.apple.com/library/mac/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/AccessingFilesandDirectories/AccessingFilesandDirectories.html
+    //
+    // Note that the temp directory must not be used on Unix, as it is shared between users.
+    val cache: File?
+        get() = when (os) {
+            OSKind.Windows -> getEnv("LOCALAPPDATA")?.toFile() ?: temp
+            OSKind.OSX -> userHome?.resolve("Library/Caches")
+            OSKind.Unix -> getEnv("XDG_CACHE_HOME")?.toFile() ?: userHome?.resolve(".cache")
+            OSKind.Unknown -> userHome?.resolve(".cache")
+        }
+
+    private val userHome: File?
+        get() = getProperty("user.home")?.toFile()
+
+    private val temp: File?
+        get() = getProperty("java.io.tmpdir")?.toFile()
+
+    private enum class OSKind {
+        Windows,
+        OSX,
+        Unix,
+        Unknown
+    }
+
+    // OS detection based on
+    // compiler/daemon/daemon-common/src/org/jetbrains/kotlin/daemon/common/FileSystemUtils.kt
+    // which in turn is based on: http://www.code4copy.com/java/post/detecting-os-type-in-java
+    private val os: OSKind
+        get() = getProperty("os.name")?.toLowerCase().let { name ->
+            when {
+                name == null -> OSKind.Unknown
+                name.startsWith("windows") -> OSKind.Windows
+                name.startsWith("mac os") -> OSKind.OSX
+                name.contains("unix") -> OSKind.Unix
+                name.startsWith("linux") -> OSKind.Unix
+                name.contains("bsd") -> OSKind.Unix
+                name.startsWith("irix") -> OSKind.Unix
+                name.startsWith("mpe/ix") -> OSKind.Unix
+                name.startsWith("aix") -> OSKind.Unix
+                name.startsWith("hp-ux") -> OSKind.Unix
+                name.startsWith("sunos") -> OSKind.Unix
+                name.startsWith("sun os") -> OSKind.Unix
+                name.startsWith("solaris") -> OSKind.Unix
+                else -> OSKind.Unknown
+            }
+        }
+
+    private fun getProperty(name: String) = systemProperties.getProperty(name).nullIfBlank()
+
+    private fun getEnv(name: String) = environment[name].nullIfBlank()
+
+    private fun String.toFile() = File(this)
+
+    private fun String?.nullIfBlank() = if (this == null || this.isBlank()) null else this
+}
