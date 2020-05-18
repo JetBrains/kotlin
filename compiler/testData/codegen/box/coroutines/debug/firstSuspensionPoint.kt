16e7a42aea802abacd12763d1d06461c10f0d27e
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-05-18 19:55:45 +0300

    JVM: Fix line numbers in test

--- compiler/testData/codegen/box/coroutines/debug/firstSuspensionPoint.kt
+++ compiler/testData/codegen/box/coroutines/debug/firstSuspensionPoint.kt
@@ -1,11 +1,10 @@
-// This test depends on line numbers
 // IGNORE_BACKEND_FIR: JVM_IR
 // TARGET_BACKEND: JVM
 // WITH_RUNTIME
 // FULL_JDK
 // WITH_COROUTINES
 // IGNORE_BACKEND: JVM_IR
-package test
+package test // NB This test depends on line numbers
 
 import helpers.*
 import kotlin.coroutines.*
