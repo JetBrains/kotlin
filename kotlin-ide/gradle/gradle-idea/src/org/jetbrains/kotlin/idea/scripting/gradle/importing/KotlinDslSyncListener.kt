fce038c35132d99b8fe6c8c4ab74627da9ad5eea
Sergey Rostov <sergey.rostov@jetbrains.com>
2020-05-19 18:14:01 +0300

    Revert "Fix order of ExternalSystemListeners"
    This reverts commit 879fe9b4
    
    Original commit: 8f75f7283570ad108f3ac830753c6cca041dde29

--- kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
+++ kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
@@ -8,60 +8,26 @@ package org.jetbrains.kotlin.idea.scripting.gradle.importing
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
-import org.jetbrains.kotlin.idea.core.script.ScriptDefinitionContributor
 import org.jetbrains.kotlin.idea.framework.GRADLE_SYSTEM_ID
-import org.jetbrains.kotlin.idea.scripting.gradle.GradleScriptDefinitionsContributor
 
 class KotlinDslSyncListener : ExternalSystemTaskNotificationListenerAdapter() {
     override fun onStart(id: ExternalSystemTaskId, workingDir: String?) {
-        if (!isGradleProjectImport(id)) return
+        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
+        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+
         if (workingDir == null) return
-
-        gradleState.isSyncInProgress = true
-
         val project = id.findProject() ?: return
 
         project.kotlinGradleDslSync[id] = KotlinDslGradleBuildSync(workingDir, id)
     }
 
     override fun onEnd(id: ExternalSystemTaskId) {
-        if (!isGradleProjectImport(id)) return
-
-        gradleState.isSyncInProgress = false
-
-        val project = id.findProject() ?: return
-
-        @Suppress("DEPRECATION")
-        ScriptDefinitionContributor.find<GradleScriptDefinitionsContributor>(project)?.reloadIfNecessary()
-
-        val sync = project.kotlinGradleDslSync.remove(id)
-        if (sync != null) {
-            // For Gradle 6.0 or higher
-            saveScriptModels(project, sync)
-        }
-    }
-
-    override fun onCancel(id: ExternalSystemTaskId) {
-        if (!isGradleProjectImport(id)) return
-
-        gradleState.isSyncInProgress = false
+        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
+        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
 
         val project = id.findProject() ?: return
+        val sync = project.kotlinGradleDslSync.remove(id) ?: return
 
-        project.kotlinGradleDslSync.remove(id)
+        saveScriptModels(project, sync)
     }
-
-    private fun isGradleProjectImport(id: ExternalSystemTaskId): Boolean {
-        return id.type == ExternalSystemTaskType.RESOLVE_PROJECT || id.projectSystemId == GRADLE_SYSTEM_ID
-    }
-
-    companion object {
-        internal val gradleState = GradleSyncState()
-    }
-}
-
-// TODO: state should be stored by gradle build,
-// now it is marked as complete after first gradle project was imported
-internal class GradleSyncState {
-    var isSyncInProgress: Boolean = false
 }
