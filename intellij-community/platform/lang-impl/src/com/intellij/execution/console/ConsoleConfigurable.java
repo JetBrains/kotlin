6638b31aba7f271822a273f40feff90cb1b601c2
Semyon Kolton <semyon.kolton@jetbrains.com>
2020-05-19 11:10:04 +0300

    CPP-11641 Replace usage of NO_ENCODING with EncodingReference in EncodingManagerImpl
    intellij-monorepo: b15d74b7639ce3b75271a847bbeabb4dcf123cd2

--- intellij-community/platform/lang-impl/src/com/intellij/execution/console/ConsoleConfigurable.java
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/console/ConsoleConfigurable.java
@@ -19,6 +19,7 @@ import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.encoding.EncodingManager;
 import com.intellij.openapi.vfs.encoding.EncodingManagerImpl;
+import com.intellij.openapi.vfs.encoding.EncodingReference;
 import com.intellij.ui.*;
 import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.containers.ContainerUtil;
@@ -31,12 +32,9 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.event.DocumentEvent;
 import java.awt.*;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-
-import static com.intellij.openapi.vfs.encoding.ChooseFileEncodingAction.NO_ENCODING;
 
 /**
  * @author peter
@@ -171,13 +169,13 @@ public class ConsoleConfigurable implements SearchableConfigurable, Configurable
   private boolean isEncodingModified() {
     EncodingManager encodingManager = EncodingManager.getInstance();
 
-    Charset defaultEncoding = NO_ENCODING;
+    EncodingReference defaultEncoding = EncodingReference.DEFAULT;
     if (encodingManager instanceof EncodingManagerImpl) {
-      defaultEncoding = ((EncodingManagerImpl)encodingManager).getDefaultConsoleEncodingInternal();
+      defaultEncoding = ((EncodingManagerImpl)encodingManager).getDefaultConsoleEncodingReference();
     }
 
-    Charset consoleEncoding = myEncodingComboBox.getSelectedCharset();
-    return defaultEncoding.compareTo(consoleEncoding) != 0;
+    EncodingReference consoleEncoding = myEncodingComboBox.getSelectedEncodingReference();
+    return !defaultEncoding.equals(consoleEncoding);
   }
 
   @Override
@@ -208,7 +206,7 @@ public class ConsoleConfigurable implements SearchableConfigurable, Configurable
     }
     if (isEncodingModified()) {
       if (encodingManager instanceof EncodingManagerImpl) {
-        ((EncodingManagerImpl)encodingManager).setDefaultConsoleEncodingInternal(myEncodingComboBox.getSelectedCharset());
+        ((EncodingManagerImpl)encodingManager).setDefaultConsoleEncodingReference(myEncodingComboBox.getSelectedEncodingReference());
       }
     }
 
@@ -230,13 +228,13 @@ public class ConsoleConfigurable implements SearchableConfigurable, Configurable
     myConsoleCycleBufferSizeField.setEnabled(ConsoleBuffer.useCycleBuffer() && uiSettings.getOverrideConsoleCycleBufferSize());
     myConsoleCycleBufferSizeField.setText(Integer.toString(uiSettings.getConsoleCycleBufferSizeKb()));
 
-    Charset encoding = NO_ENCODING;
+    EncodingReference encodingReference = EncodingReference.DEFAULT;
     if (encodingManager instanceof EncodingManagerImpl) {
-      encoding = ((EncodingManagerImpl)encodingManager).getDefaultConsoleEncodingInternal();
+      encodingReference = ((EncodingManagerImpl)encodingManager).getDefaultConsoleEncodingReference();
     } else {
-      LOG.error("Expected EncodingManagerImpl but got " + encodingManager.getClass().getName());
+      LOG.warn("Expected EncodingManagerImpl but got " + encodingManager.getClass().getName());
     }
-    myEncodingComboBox.reset(encoding);
+    myEncodingComboBox.reset(encodingReference);
 
     myNegativePanel.resetFrom(mySettings.getNegativePatterns());
     myPositivePanel.resetFrom(mySettings.getPositivePatterns());
--- intellij-community/platform/lang-impl/src/com/intellij/execution/console/ConsoleEncodingComboBox.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/execution/console/ConsoleEncodingComboBox.kt
@@ -7,8 +7,8 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.CharsetToolkit
-import com.intellij.openapi.vfs.encoding.ChooseFileEncodingAction.NO_ENCODING
 import com.intellij.openapi.vfs.encoding.EncodingManager
+import com.intellij.openapi.vfs.encoding.EncodingReference
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.SeparatorWithText
 import com.intellij.ui.SimpleListCellRenderer
@@ -23,15 +23,20 @@ class ConsoleEncodingComboBox : ComboBox<ConsoleEncodingComboBox.EncodingItem>()
 
   private interface SelectableItem : EncodingItem
 
-  private data class CharsetItem(val charset: Charset) : SelectableItem {
-    override fun toString(): String =
-      charset.displayName()
+  private data class CharsetItem(val reference: EncodingReference) : SelectableItem {
+    constructor(charset: Charset) : this(EncodingReference(charset))
+
+    override fun toString(): String {
+      return reference.charset?.displayName()
+             ?: IdeBundle.message("encoding.name.system.default", CharsetToolkit.getDefaultSystemCharset().displayName())
+
+    }
+
+    companion object {
+      val DEFAULT = CharsetItem(EncodingReference.DEFAULT)
+    }
   }
 
-  private object NoEncodingItem : SelectableItem {
-    override fun toString(): String =
-      IdeBundle.message("encoding.name.system.default", CharsetToolkit.getDefaultSystemCharset().displayName())
-  }
 
   init {
     model = object : CollectionComboBoxModel<EncodingItem>() {
@@ -90,41 +95,27 @@ class ConsoleEncodingComboBox : ComboBox<ConsoleEncodingComboBox.EncodingItem>()
    *    Charset n
    * ```
    */
-  fun reset(charset: Charset) {
+  fun reset(reference: EncodingReference) {
     val encodingManager = EncodingManager.getInstance()
     val favorites = encodingManager.favorites.map { CharsetItem(it) }
     val available = Charset.availableCharsets().values.map { CharsetItem(it) }
 
-    listModel.add(NoEncodingItem)
+    listModel.add(CharsetItem.DEFAULT)
     listModel.add(LabelItem(ApplicationBundle.message("combobox.console.favorites.separator.label")))
     listModel.add(favorites)
     listModel.add(LabelItem(ApplicationBundle.message("combobox.console.more.separator.label")))
     listModel.add(available)
 
-    listModel.selectedItem = if (charset == NO_ENCODING) {
-      NoEncodingItem
-    } else {
-      CharsetItem(charset)
-    }
+    listModel.selectedItem = CharsetItem(reference)
   }
 
-  fun getSelectedCharset(): Charset {
-    return when (selectedItem) {
-      is CharsetItem -> (selectedItem as CharsetItem).charset
-      is NoEncodingItem -> NO_ENCODING
-      else -> {
-        LOG.error("Encoding should be either NO_ENCODING or an actual Charset. Got $selectedItem")
-        NO_ENCODING
-      }
+  fun getSelectedEncodingReference(): EncodingReference {
+    return if (selectedItem is CharsetItem) {
+      (selectedItem as CharsetItem).reference
     }
-  }
-
-  fun getSelectedCharsetName(): String {
-    val selectedCharset = getSelectedCharset()
-    return if (selectedCharset === NO_ENCODING) {
-      ""
-    } else {
-      selectedCharset.name()
+    else {
+      LOG.error("Encoding should be either DEFAULT or an actual Charset. Got $selectedItem")
+      EncodingReference.DEFAULT
     }
   }
 }
