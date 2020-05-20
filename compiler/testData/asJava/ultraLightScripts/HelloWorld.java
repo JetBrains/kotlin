6accc8fcf9ef8efb74e7ca486b1bc17ea10c1a95
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-05-20 22:38:15 +0300

    [ULC] Fixed invalid constructor parameter list for UL script

---
+++ compiler/testData/asJava/ultraLightScripts/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld /* HelloWorld*/ {
+  @null()
+  public  HelloWorld(@null() java.lang.String[]);//  .ctor(java.lang.String[])
+
+  public static final void main(@null() java.lang.String[]);//  main(java.lang.String[])
+
+}
