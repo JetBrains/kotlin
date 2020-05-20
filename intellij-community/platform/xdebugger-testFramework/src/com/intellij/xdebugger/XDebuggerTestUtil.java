fd202f251145cec534d844bc670290cad66ae299
Egor Ushakov <egor.ushakov@jetbrains.com>
2020-05-20 15:23:06 +0300

    fixed tests to support async jdi
    intellij-monorepo: 23b75509e6b12b73257011cabaa62ae1fe5aa38d

--- intellij-community/platform/xdebugger-testFramework/src/com/intellij/xdebugger/XDebuggerTestUtil.java
+++ intellij-community/platform/xdebugger-testFramework/src/com/intellij/xdebugger/XDebuggerTestUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.xdebugger;
 
 import com.intellij.execution.impl.ConsoleViewImpl;
@@ -260,6 +260,7 @@ public class XDebuggerTestUtil {
   }
 
   public static XTestValueNode computePresentation(@NotNull XValue value) {
+
     return computePresentation(value, XDebuggerTestUtil::waitFor);
   }
   public static XTestValueNode computePresentation(@NotNull XValue value, BiFunction<? super Semaphore, ? super Long, Boolean> waitFunction) {
