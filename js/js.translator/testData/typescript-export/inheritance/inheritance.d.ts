d8b3b6fd78a80a786942e12ea27745b8fd4783f0
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2020-05-21 18:34:02 +0300

    [JS] Add `;` after functions in .d.ts
    Fixes nit from KT-37752

--- js/js.translator/testData/typescript-export/inheritance/inheritance.d.ts
+++ js/js.translator/testData/typescript-export/inheritance/inheritance.d.ts
@@ -4,31 +4,31 @@ declare namespace JS_TESTS {
         interface I<T, S, U> {
             x: T;
             readonly y: S;
-            z(u: U): void
+            z(u: U): void;
         }
         interface I2 {
             x: string;
             readonly y: boolean;
-            z(z: number): void
+            z(z: number): void;
         }
     }
     namespace foo {
         abstract class AC implements foo.I2 {
-            constructor()
+            constructor();
             x: string;
             abstract readonly y: boolean;
-            abstract z(z: number): void
+            abstract z(z: number): void;
             readonly acProp: string;
             abstract readonly acAbstractProp: string;
         }
         class OC extends foo.AC implements foo.I<string, boolean, number> {
-            constructor(y: boolean, acAbstractProp: string)
+            constructor(y: boolean, acAbstractProp: string);
             readonly y: boolean;
             readonly acAbstractProp: string;
-            z(z: number): void
+            z(z: number): void;
         }
         class FC extends foo.OC {
-            constructor()
+            constructor();
         }
     }
 }
