06dbbe2452d5d7078acc7f97c2c3081792469402
Sergey Rostov <sergey.rostov@jetbrains.com>
2020-05-22 18:19:11 +0300

    KotlinDslModels: project can be null in ExternalSystemTaskNotificationListener.onStart

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/scripting/gradle/importing/KotlinDslSyncListener.kt
@@ -6,53 +6,63 @@
 package org.jetbrains.kotlin.idea.scripting.gradle.importing
 
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
+import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListener
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
 import org.jetbrains.kotlin.idea.framework.GRADLE_SYSTEM_ID
 import org.jetbrains.kotlin.idea.scripting.gradle.roots.GradleBuildRootsManager
-import java.lang.Exception
+import java.util.*
 
 class KotlinDslSyncListener : ExternalSystemTaskNotificationListenerAdapter() {
+    companion object {
+        val instance: KotlinDslSyncListener
+            get() = ExternalSystemTaskNotificationListener.EP_NAME
+                .extensionList.filterIsInstance<KotlinDslSyncListener>().single()
+    }
+
+    val tasks = WeakHashMap<ExternalSystemTaskId, KotlinDslGradleBuildSync>()
+
     override fun onStart(id: ExternalSystemTaskId, workingDir: String?) {
-        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
-        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+        if (!isGradleProjectResolve(id)) return
 
         if (workingDir == null) return
-        val project = id.findProject() ?: return
+        synchronized(tasks) { tasks[id] = KotlinDslGradleBuildSync(workingDir, id) }
 
+        // project may be null in case of new project
+        val project = id.findProject() ?: return
         GradleBuildRootsManager.getInstance(project).markImportingInProgress(workingDir)
-        project.kotlinGradleDslSync[id] = KotlinDslGradleBuildSync(workingDir, id)
     }
 
     override fun onEnd(id: ExternalSystemTaskId) {
-        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
-        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+        if (!isGradleProjectResolve(id)) return
 
+        val sync = synchronized(tasks) { tasks.remove(id) } ?: return
+
+        // project may be null in case of new project
         val project = id.findProject() ?: return
-        val sync = project.kotlinGradleDslSync.remove(id) ?: return
-
         saveScriptModels(project, sync)
     }
 
     override fun onFailure(id: ExternalSystemTaskId, e: Exception) {
-        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
-        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+        if (!isGradleProjectResolve(id)) return
 
-        val project = id.findProject() ?: return
-        val sync = project.kotlinGradleDslSync[id] ?: return
-
+        val sync = synchronized(tasks) { tasks[id] } ?: return
         sync.failed = true
     }
 
     override fun onCancel(id: ExternalSystemTaskId) {
-        if (id.type != ExternalSystemTaskType.RESOLVE_PROJECT) return
-        if (id.projectSystemId != GRADLE_SYSTEM_ID) return
+        if (!isGradleProjectResolve(id)) return
 
+        val cancelled = synchronized(tasks) { tasks.remove(id) }
+
+        // project may be null in case of new project
         val project = id.findProject() ?: return
-
-        val cancelled = project.kotlinGradleDslSync.remove(id)
         if (cancelled != null) {
             GradleBuildRootsManager.getInstance(project).markImportingInProgress(cancelled.workingDir, false)
         }
     }
+
+    private fun isGradleProjectResolve(id: ExternalSystemTaskId) =
+        id.type == ExternalSystemTaskType.RESOLVE_PROJECT &&
+                id.projectSystemId == GRADLE_SYSTEM_ID
 }
