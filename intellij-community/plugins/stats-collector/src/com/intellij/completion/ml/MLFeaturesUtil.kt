fd0b7c448fa1161439f00d24c3e1204d39a01a8b
Vitaliy.Bibaev <vitaliy.bibaev@jetbrains.com>
2020-05-24 00:39:07 +0300

    IDEA-CR-62931: IDEA-241559 Create a util method to get ML weigher
    (cherry picked from commit 429d5d188708f73d5361c4cbcf7e18c086ed8dfb)
    
    intellij-monorepo: d46e088f08f1a5eb3080a31630c1c8b34a969e1a

--- intellij-community/plugins/stats-collector/src/com/intellij/completion/ml/MLFeaturesUtil.kt
+++ intellij-community/plugins/stats-collector/src/com/intellij/completion/ml/MLFeaturesUtil.kt
@@ -3,15 +3,8 @@ package com.intellij.completion.ml
 
 import com.google.common.cache.Cache
 import com.google.common.cache.CacheBuilder
-import com.intellij.codeInsight.completion.CompletionLocation
-import com.intellij.codeInsight.completion.CompletionService
-import com.intellij.codeInsight.completion.CompletionSorter
 import com.intellij.codeInsight.completion.ml.MLFeatureValue
-import com.intellij.codeInsight.lookup.LookupElement
-import com.intellij.codeInsight.lookup.LookupElementWeigher
 import com.intellij.internal.statistic.utils.getPluginInfo
-import com.intellij.psi.ForceableComparable
-import com.intellij.psi.WeighingService
 
 object MLFeaturesUtil {
   fun valueAsString(featureValue: MLFeatureValue): String {
@@ -40,44 +33,5 @@ object MLFeaturesUtil {
     val clazz = feature.value
     val names = CLASS_NAMES_CACHE.get(clazz.name) { if (getPluginInfo(clazz).isSafeToReport()) clazz.getNames() else THIRD_PARTY_NAME }
     return if (feature.useSimpleName) names.simpleName else names.fullName
-  }
-
-  fun addWeighersToNonDefaultSorter(sorter: CompletionSorter, location: CompletionLocation, vararg weigherIds: String): CompletionSorter {
-      // from BaseCompletionService.defaultSorter
-    var result = sorter
-    for (weigher in WeighingService.getWeighers(CompletionService.RELEVANCE_KEY)) {
-      val id = weigher.toString()
-      if (weigherIds.contains(id)) {
-        result = result.weigh(object : LookupElementWeigher(id, true, false) {
-          override fun weigh(element: LookupElement): Comparable<*>? {
-            val weigh = weigher.weigh(element, location) ?: return DummyWeigherComparableDelegate.EMPTY
-            return DummyWeigherComparableDelegate(weigh)
-          }
-        })
-      }
-    }
-    return result
-  }
-}
-
-private class DummyWeigherComparableDelegate(private val weigh: Comparable<*>?)
-  : Comparable<DummyWeigherComparableDelegate>, ForceableComparable {
-
-  companion object {
-    val EMPTY = DummyWeigherComparableDelegate(null)
-  }
-
-  override fun force() {
-    if (weigh is ForceableComparable) {
-      (weigh as ForceableComparable).force()
-    }
-  }
-
-  override operator fun compareTo(other: DummyWeigherComparableDelegate): Int {
-    return 0
-  }
-
-  override fun toString(): String {
-    return weigh?.toString() ?: ""
   }
 }
