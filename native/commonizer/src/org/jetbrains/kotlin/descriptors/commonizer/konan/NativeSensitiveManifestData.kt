b5245e2df6f83dbc82d1dfd72d678de22ecf8873
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2020-05-25 15:57:32 +0700

    [Commonizer] Avoid N^2 during merge of KLIB manifest dependencies

--- native/commonizer/src/org/jetbrains/kotlin/descriptors/commonizer/konan/NativeSensitiveManifestData.kt
+++ native/commonizer/src/org/jetbrains/kotlin/descriptors/commonizer/konan/NativeSensitiveManifestData.kt
@@ -56,7 +56,7 @@ internal data class NativeSensitiveManifestData(
         return NativeSensitiveManifestData(
             uniqueName = uniqueName,
             versions = versions,
-            dependencies = dependencies.filter { other.dependencies.contains(it) },
+            dependencies = (dependencies union other.dependencies).toList(),
             isInterop = isInterop,
             packageFqName = packageFqName,
             exportForwardDeclarations = exportForwardDeclarations,
