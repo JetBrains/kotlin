b8c7ec77af6e7aa5337dbdf64e2f3d0045fb0de6
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-05-27 17:42:52 +0300

    [workspace model] fix loading modules configuration from external_build_system directory
    ...and add tests on that.
    
    intellij-monorepo: fe687f1047538e0d22e84d10f33a4f4289608c2c

--- intellij-community/platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/configurationStore/ExternalSystemStorageTest.kt
+++ intellij-community/platform/external-system-impl/testSrc/com/intellij/openapi/externalSystem/configurationStore/ExternalSystemStorageTest.kt
@@ -8,6 +8,7 @@ import com.intellij.facet.mock.MockSubFacetType
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.AppUIExecutor
 import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.application.appSystemDir
 import com.intellij.openapi.application.ex.PathManagerEx
 import com.intellij.openapi.application.impl.coroutineDispatchingContext
 import com.intellij.openapi.application.runWriteAction
@@ -17,12 +18,16 @@ import com.intellij.openapi.externalSystem.service.project.manage.ExternalProjec
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.module.ModuleTypeId
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.getProjectCacheFileName
 import com.intellij.openapi.roots.ExternalProjectSystemRegistry
 import com.intellij.openapi.roots.LanguageLevelProjectExtension
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.packaging.artifacts.ArtifactManager
 import com.intellij.packaging.elements.PackagingElementFactory
 import com.intellij.packaging.impl.artifacts.PlainArtifactType
@@ -32,10 +37,12 @@ import com.intellij.testFramework.*
 import com.intellij.util.io.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
+import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -56,14 +63,22 @@ class ExternalSystemStorageTest {
   val tempDirManager = TemporaryDirectory()
 
   @Test
-  fun `single module`() = saveProjectAndCheckResult("singleModule") { project, projectDir ->
+  fun `save single module`() = saveProjectAndCheckResult("singleModule") { project, projectDir ->
     val module = ModuleManager.getInstance(project).newModule(projectDir.resolve("test.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
     ModuleRootModificationUtil.addContentRoot(module, projectDir.systemIndependentPath)
     ExternalSystemModulePropertyManager.getInstance(module).setMavenized(true)
   }
 
   @Test
-  fun `mixed modules`() = saveProjectAndCheckResult("mixedModules") { project, projectDir ->
+  fun `load single module`() = loadProjectAndCheckResults("singleModule") { project ->
+    val module = ModuleManager.getInstance(project).modules.single()
+    assertThat(module.name).isEqualTo("test")
+    assertThat(module.moduleTypeName).isEqualTo(ModuleTypeId.JAVA_MODULE)
+    assertThat(ExternalSystemModulePropertyManager.getInstance(module).isMavenized()).isTrue()
+  }
+
+  @Test
+  fun `save mixed modules`() = saveProjectAndCheckResult("mixedModules") { project, projectDir ->
     val regular = ModuleManager.getInstance(project).newModule(projectDir.resolve("regular.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
     ModuleRootModificationUtil.addContentRoot(regular, projectDir.resolve("regular").systemIndependentPath)
     val imported = ModuleManager.getInstance(project).newModule(projectDir.resolve("imported.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
@@ -72,10 +87,36 @@ class ExternalSystemStorageTest {
   }
 
   @Test
-  fun `regular facet in imported module`() = saveProjectAndCheckResult("regularFacetInImportedModule") { project, projectDir ->
+  fun `load mixed modules`() = loadProjectAndCheckResults("mixedModules") { project ->
+    val modules = ModuleManager.getInstance(project).modules.sortedBy { it.name }
+    assertThat(modules).hasSize(2)
+    val (imported, regular) = modules
+    assertThat(imported.name).isEqualTo("imported")
+    assertThat(regular.name).isEqualTo("regular")
+    assertThat(imported.moduleTypeName).isEqualTo(ModuleTypeId.JAVA_MODULE)
+    assertThat(regular.moduleTypeName).isEqualTo(ModuleTypeId.JAVA_MODULE)
+    assertThat(ModuleRootManager.getInstance(imported).contentRootUrls.single()).isEqualTo(VfsUtil.pathToUrl("${project.basePath}/imported"))
+    assertThat(ModuleRootManager.getInstance(regular).contentRootUrls.single()).isEqualTo(VfsUtil.pathToUrl("${project.basePath}/regular"))
+    assertThat(ExternalSystemModulePropertyManager.getInstance(imported).isMavenized()).isTrue()
+    assertThat(ExternalSystemModulePropertyManager.getInstance(regular).isMavenized()).isFalse()
+  }
+
+  @Test
+  fun `save regular facet in imported module`() = saveProjectAndCheckResult("regularFacetInImportedModule") { project, projectDir ->
     val imported = ModuleManager.getInstance(project).newModule(projectDir.resolve("imported.iml").systemIndependentPath, ModuleTypeId.JAVA_MODULE)
     FacetManager.getInstance(imported).addFacet(MockFacetType.getInstance(), "regular", null)
     ExternalSystemModulePropertyManager.getInstance(imported).setMavenized(true)
+  }
+
+  @Test
+  fun `load regular facet in imported module`() = loadProjectAndCheckResults("regularFacetInImportedModule") { project ->
+    val module = ModuleManager.getInstance(project).modules.single()
+    assertThat(module.name).isEqualTo("imported")
+    assertThat(ExternalSystemModulePropertyManager.getInstance(module).isMavenized()).isTrue()
+    val facet = FacetManager.getInstance(module).allFacets.single()
+    assertThat(facet.name).isEqualTo("regular")
+    assertThat(facet.type).isEqualTo(MockFacetType.getInstance())
+    assertThat(facet.externalSource).isNull()
   }
 
   @Test
@@ -140,12 +181,38 @@ class ExternalSystemStorageTest {
         project.stateStore.save()
 
         val expectedDir = tempDirManager.newPath("expectedStorage")
-        val testDataRoot = Paths.get(PathManagerEx.getCommunityHomePath()).resolve("platform/external-system-impl/testData/jpsSerialization")
         FileUtil.copyDir(testDataRoot.resolve("common").toFile(), expectedDir.toFile())
         FileUtil.copyDir(testDataRoot.resolve(testDataDirName).toFile(), expectedDir.toFile())
 
         projectDir.toFile().assertMatches(directoryContentOf(expectedDir.resolve("project")))
         cacheDir.toFile().assertMatches(directoryContentOf(expectedDir.resolve("cache")), FileTextMatcher.ignoreBlankLines())
+      }
+    }
+  }
+
+  private val testDataRoot
+    get() = Paths.get(PathManagerEx.getCommunityHomePath()).resolve("platform/external-system-impl/testData/jpsSerialization")
+
+  private fun loadProjectAndCheckResults(testDataDirName: String, checkProject: (Project) -> Unit) {
+    @Suppress("RedundantSuspendModifier")
+    suspend fun copyProjectFiles(dir: VirtualFile): Path {
+      val projectDir = VfsUtil.virtualToIoFile(dir)
+      FileUtil.copyDir(testDataRoot.resolve("common/project").toFile(), projectDir)
+      val testProjectFilesDir = testDataRoot.resolve(testDataDirName).resolve("project").toFile()
+      if (testProjectFilesDir.exists()) {
+        FileUtil.copyDir(testProjectFilesDir, projectDir)
+      }
+      val testCacheFilesDir = testDataRoot.resolve(testDataDirName).resolve("cache").toFile()
+      if (testCacheFilesDir.exists()) {
+        val cachePath = appSystemDir.resolve("external_build_system").resolve(getProjectCacheFileName(projectDir.absolutePath))
+        FileUtil.copyDir(testCacheFilesDir, cachePath.toFile())
+      }
+      VfsUtil.markDirtyAndRefresh(false, true, true, dir)
+      return projectDir.toPath()
+    }
+    runBlocking {
+      createOrLoadProject(tempDirManager, ::copyProjectFiles, loadComponentState = true, useDefaultProjectSettings = false) {
+        checkProject(it)
       }
     }
   }
