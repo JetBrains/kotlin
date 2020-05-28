bdfa8e2c981d97cb855394afee0625f4f88c065a
Alexandr Suhinin <alexandr.suhinin@jetbrains.com>
2020-05-28 13:30:52 +0300

    move inlay settings presentation to util
    intellij-monorepo: b1ce6a0a6d17114f78fe9357189f4ac2cd7af892

--- intellij-community/platform/lang-impl/src/com/intellij/refactoring/rename/inplace/TemplateInlayUtil.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/refactoring/rename/inplace/TemplateInlayUtil.kt
@@ -1,14 +1,20 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.refactoring.rename.inplace
 
+import com.intellij.codeInsight.hints.InlayPresentationFactory
+import com.intellij.codeInsight.hints.presentation.PresentationFactory
 import com.intellij.codeInsight.hints.presentation.PresentationRenderer
 import com.intellij.codeInsight.template.impl.TemplateState
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.Inlay
+import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.ui.popup.JBPopupListener
 import com.intellij.openapi.ui.popup.LightweightWindowEvent
 import com.intellij.openapi.util.Disposer
+import com.intellij.ui.JBColor
 import com.intellij.ui.popup.PopupFactoryImpl
+import java.util.concurrent.atomic.AtomicReference
 import javax.swing.JComponent
 
 object TemplateInlayUtil {
@@ -35,7 +41,7 @@ object TemplateInlayUtil {
                                        panel: JComponent): Inlay<PresentationRenderer>? {
     val editor = templateState.editor
     val inlay = createNavigatableButton(templateState, inEditorOffset, presentation) ?: return null
-    fun showPopup(){
+    fun showPopup() {
       try {
         editor.putUserData(PopupFactoryImpl.ANCHOR_POPUP_POSITION, inlay.visualPosition)
         val popup = JBPopupFactory.getInstance()
@@ -48,7 +54,8 @@ object TemplateInlayUtil {
           })
           .createPopup()
         popup.showInBestPositionFor(editor)
-      } finally {
+      }
+      finally {
         editor.putUserData(PopupFactoryImpl.ANCHOR_POPUP_POSITION, null)
       }
     }
@@ -58,5 +65,30 @@ object TemplateInlayUtil {
       }
     })
     return inlay
+  }
+
+  @JvmStatic
+  fun createSettingsPresentation(editor: EditorImpl, inlayToUpdate: AtomicReference<Inlay<PresentationRenderer>>): SelectableInlayPresentation {
+    val factory = PresentationFactory(editor)
+
+    val roundedCorners = InlayPresentationFactory.RoundedCorners(6, 6)
+    val padding = InlayPresentationFactory.Padding(4, 4, 4, 4)
+
+    val inactiveIcon = factory.container(
+      presentation = factory.icon(AllIcons.Actions.InlayGear),
+      padding = padding,
+      roundedCorners = roundedCorners,
+      background = JBColor.LIGHT_GRAY
+    )
+    val activeIcon = factory.container(
+      presentation = factory.icon(AllIcons.Actions.InlayGear),
+      padding = padding,
+      roundedCorners = roundedCorners,
+      background = JBColor.DARK_GRAY
+    )
+    val inactivePadded = factory.container(inactiveIcon, padding = InlayPresentationFactory.Padding(3, 6, 0, 0))
+    val activePadded = factory.container(activeIcon, padding = InlayPresentationFactory.Padding(3, 6, 0, 0))
+
+    return SelectableInlayButton(editor, inactivePadded, activePadded, activePadded, inlayToUpdate)
   }
 }
