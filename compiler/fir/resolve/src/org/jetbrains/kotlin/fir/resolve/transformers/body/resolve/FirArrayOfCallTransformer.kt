4a511c7721c0bc4d5377f6d1a5f957dfb2e92de6
Jinseong Jeon <jsjeon@google.com>
2020-05-27 22:39:03 -0700

    FIR: extend arrayOf call transformation to other variants

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirArrayOfCallTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirArrayOfCallTransformer.kt
@@ -23,11 +23,8 @@ internal class FirArrayOfCallTransformer {
         get() {
             val function: FirCallableDeclaration<*> = getOriginalFunction() ?: return false
             return function is FirSimpleFunction &&
-                    // TODO: extend it to other variants, like emptyArray, intArrayOf, floatArrayOf, etc.
-                    function.name.asString() == "arrayOf" &&
                     function.returnTypeRef.isArrayType &&
-                    function.valueParameters.size == 1 && function.valueParameters[0].isVararg &&
-                    arguments.size == 1 &&
+                    isArrayOf(function, arguments) &&
                     function.receiverTypeRef == null
         }
 
@@ -50,5 +47,20 @@ internal class FirArrayOfCallTransformer {
         }.apply {
             replaceTypeRef(typeRef)
         }
+    }
+
+    companion object {
+        private val arrayOfNames = hashSetOf("kotlin/arrayOf") +
+                hashSetOf(
+                    "boolean", "byte", "char", "double", "float", "int", "long", "short",
+                    "ubyte", "uint", "ulong", "ushort"
+                ).map { "kotlin/" + it + "ArrayOf" }
+
+        private fun isArrayOf(function: FirSimpleFunction, arguments: List<FirExpression>): Boolean =
+            when (function.symbol.callableId.toString()) {
+                "kotlin/emptyArray" -> function.valueParameters.isEmpty() && arguments.isEmpty()
+                in arrayOfNames -> function.valueParameters.size == 1 && function.valueParameters[0].isVararg && arguments.size == 1
+                else -> false
+            }
     }
 }
