6c1efbe8126074ba9a9b13cdeac0754718aa3c04
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2020-05-28 14:05:28 +0300

    [Spec tests] Update spec testData

--- compiler/tests-spec/testData/diagnostics/notLinked/overload-resolution/building-the-overload-candidate-set-ocs/infix-function-call/pos/2.kt
+++ compiler/tests-spec/testData/diagnostics/notLinked/overload-resolution/building-the-overload-candidate-set-ocs/infix-function-call/pos/2.kt
@@ -18,16 +18,16 @@ class C() {
 // TESTCASE NUMBER: 1
 fun case1() {
     val b: B = B(C())
-    b <!UNRESOLVED_REFERENCE!>memberVal<!> 1           //nok UNRESOLVED_REFERENCE
-    b.memberVal.<!UNRESOLVED_REFERENCE!>invoke<!>(2)   //nok UNRESOLVED_REFERENCE
-    b.<!FUNCTION_EXPECTED!>memberVal<!>(1)          //nok FUNCTION_EXPECTED
+    b <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>memberVal<!> 1           //nok UNRESOLVED_REFERENCE_WRONG_RECEIVER
+    b.memberVal.<!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>invoke<!>(2)   //nok UNRESOLVED_REFERENCE_WRONG_RECEIVER
+    b.<!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>memberVal<!>(1)          //nok UNRESOLVED_REFERENCE_WRONG_RECEIVER
 
     if (b.memberVal is C) {
 
-        b <!UNRESOLVED_REFERENCE!>memberVal<!> 1           //nok UNRESOLVED_REFERENCE !!!!
+        b <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>memberVal<!> 1           //nok UNRESOLVED_REFERENCE_WRONG_RECEIVER !!!!
         <!DEBUG_INFO_SMARTCAST!>b.memberVal<!>.invoke(1)   //ok
 
-        b.<!FUNCTION_EXPECTED!>memberVal<!>(1)          //nok FUNCTION_EXPECTED  !!!
+        b.<!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>memberVal<!>(1)          //nok UNRESOLVED_REFERENCE_WRONG_RECEIVER  !!!
         <!DEBUG_INFO_SMARTCAST!>(b.memberVal)<!>(1)        //ok
     }
 }
