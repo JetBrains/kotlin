9fd211cfcdb8e1a2b7c145a6cf5130ae0c262c71
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-05-28 11:49:02 +0300

    [Gradle, JS] Manually singleton because Gradle Daemon
    ^KT-39210 fixed
    ^KT-31669 fixed

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/MultiplePluginDeclarationDetector.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/MultiplePluginDeclarationDetector.kt
@@ -6,10 +6,12 @@
 package org.jetbrains.kotlin.gradle.targets.js
 
 import org.gradle.api.Project
+import org.gradle.api.invocation.Gradle
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginInMultipleProjectsHolder
 import org.jetbrains.kotlin.gradle.plugin.MULTIPLE_KOTLIN_PLUGINS_LOADED_WARNING
 
-internal object MultiplePluginDeclarationDetector {
+internal class MultiplePluginDeclarationDetector
+private constructor() {
     private val pluginInMultipleProjectsHolder = KotlinPluginInMultipleProjectsHolder(
         differentVersionsInDifferentProject = false
     )
@@ -20,6 +22,33 @@ internal object MultiplePluginDeclarationDetector {
 
         if (pluginInMultipleProjectsHolder.isInMultipleProjects(project)) {
             error(MULTIPLE_KOTLIN_PLUGINS_LOADED_WARNING)
+        }
+    }
+
+    // We can't use Kotlin object because we need new instance on each Gradle rebuild
+    // But if we inside Gradle daemon, Kotlin object will be shared between builds
+    companion object {
+        @field:Volatile
+        private var instance: MultiplePluginDeclarationDetector? = null
+
+        @JvmStatic
+        @Synchronized
+        private fun getInstance(gradle: Gradle): MultiplePluginDeclarationDetector {
+            if (instance != null) {
+                return instance!!
+            }
+
+            val detector = MultiplePluginDeclarationDetector()
+            instance = detector
+            gradle.buildFinished {
+                instance = null
+            }
+
+            return detector
+        }
+
+        fun detect(project: Project) {
+            getInstance(project.gradle).detect(project)
         }
     }
 }
