60bbbaeddd0e4b4c18a3998905900c819e479dba
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-05-31 01:47:17 +0300

    Remove :libraries:tools:new-project-wizard:new-project-wizard-cli module

--- libraries/tools/new-project-wizard/new-project-wizard-cli/src/org/jetbrains/kotlin/tools/projectWizard/wizard/core/errors.kt
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.kotlin.tools.projectWizard.wizard.core
-
-import org.jetbrains.kotlin.tools.projectWizard.core.ExceptionError
-import org.yaml.snakeyaml.parser.ParserException
-
-data class YamlParsingError(override val exception: ParserException) : ExceptionError()
