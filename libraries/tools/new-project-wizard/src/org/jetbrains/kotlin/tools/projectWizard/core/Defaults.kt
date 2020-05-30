c5584471c63a8dec9e696c243c9df4315ba077a4
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-05-31 01:47:54 +0300

    Remove :libraries:tools:new-project-wizard module

--- libraries/tools/new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/core/Defaults.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.kotlin.tools.projectWizard.core
-
-import java.nio.file.Paths
-
-object Defaults {
-    val SRC_DIR = Paths.get("src")
-    val KOTLIN_DIR = Paths.get("kotlin")
-    val RESOURCES_DIR = Paths.get("resources")
-}
