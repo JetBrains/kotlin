64c192c30093a7c1107ea03a37b94acc04350b0f
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-05-30 20:38:03 +0300

    Remove :idea:idea-jvm module

--- prepare/idea-plugin/build.gradle.kts
+++ prepare/idea-plugin/build.gradle.kts
@@ -102,7 +102,6 @@ val projectsToShadow by extra(listOf(
         ":kotlinx-serialization-ide-plugin",
         ":idea:idea-android",
         ":idea:idea-android-output-parser",
-        ":idea:idea-jvm",
         ":idea:idea-git",
         ":idea:idea-jps-common",
         ":idea:idea-frontend-independent",
