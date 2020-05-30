4e3b8ab28528de20c738d3bf2f22d932be451e08
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-05-31 01:47:54 +0300

    Remove :libraries:tools:new-project-wizard module

--- libraries/tools/new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/plugins/templates/JsTemplatesPlugin.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.kotlin.tools.projectWizard.plugins.templates
-
-import org.jetbrains.kotlin.tools.projectWizard.core.Context
-import org.jetbrains.kotlin.tools.projectWizard.templates.SimpleJsClientTemplate
-
-class JsTemplatesPlugin(context: Context) : TemplatePlugin(context) {
-    val addTemplate by addTemplateTask(SimpleJsClientTemplate())
-}
