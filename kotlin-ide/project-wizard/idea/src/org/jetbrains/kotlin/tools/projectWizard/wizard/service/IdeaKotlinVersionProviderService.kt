359aa09ce7633882fd43f8efbf32c5e115413835
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-06-01 23:14:57 +0300

    Wizard: download latest dev version for tests from bintray
    Original commit: cce5a9a1ca5b080edaa71e5c1fc83c6d76ca8404

--- kotlin-ide/project-wizard/idea/src/org/jetbrains/kotlin/tools/projectWizard/wizard/service/IdeaKotlinVersionProviderService.kt
+++ kotlin-ide/project-wizard/idea/src/org/jetbrains/kotlin/tools/projectWizard/wizard/service/IdeaKotlinVersionProviderService.kt
@@ -11,18 +11,13 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.jetbrains.kotlin.idea.framework.ui.ConfigureDialogWithModulesAndVersion
 import org.jetbrains.kotlin.tools.projectWizard.Versions
-import org.jetbrains.kotlin.tools.projectWizard.core.TaskResult
 import org.jetbrains.kotlin.tools.projectWizard.core.asNullable
-import org.jetbrains.kotlin.tools.projectWizard.core.compute
 import org.jetbrains.kotlin.tools.projectWizard.core.safe
+import org.jetbrains.kotlin.tools.projectWizard.core.service.EapVersionDownloader
 import org.jetbrains.kotlin.tools.projectWizard.core.service.KotlinVersionProviderService
 import org.jetbrains.kotlin.tools.projectWizard.settings.version.Version
 import org.jetbrains.kotlin.tools.projectWizard.wizard.KotlinNewProjectWizardUIBundle
 import org.jetbrains.kotlin.tools.projectWizard.wizard.ui.runWithProgressBar
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.URL
-import java.util.stream.Collectors
 
 class IdeaKotlinVersionProviderService : KotlinVersionProviderService, IdeaWizardService {
     override fun getKotlinVersion(): Version =
@@ -67,29 +62,4 @@ private object VersionsDownloader {
     private fun getLatestStableVersion() = safe {
         ConfigureDialogWithModulesAndVersion.loadVersions("1.0.0")
     }.asNullable?.firstOrNull { !it.contains(SNAPSHOT_TAG, ignoreCase = true) }?.let { Version.fromString(it) }
-}
-
-private object EapVersionDownloader {
-    fun getLatestEapVersion() = downloadVersions(EAP_URL).firstOrNull()
-
-    private fun downloadPage(url: String): TaskResult<String> = safe {
-        BufferedReader(InputStreamReader(URL(url).openStream())).lines().collect(Collectors.joining("\n"))
-    }
-
-    @Suppress("SameParameterValue")
-    private fun downloadVersions(url: String): List<Version> = compute {
-        val (text) = downloadPage(url)
-        versionRegexp.findAll(text)
-            .map { it.groupValues[1].removeSuffix("/") }
-            .filter { it.isNotEmpty() && it[0].isDigit() }
-            .map { Version.fromString(it) }
-            .toList()
-            .asReversed()
-    }.asNullable.orEmpty()
-
-    @NonNls
-    private val EAP_URL = "https://dl.bintray.com/kotlin/kotlin-eap/org/jetbrains/kotlin/jvm/org.jetbrains.kotlin.jvm.gradle.plugin/"
-
-    @NonNls
-    private val versionRegexp = """href="([^"\\]+)"""".toRegex()
 }
