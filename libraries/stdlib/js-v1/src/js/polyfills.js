420b889b55269046d5ba138fc42f444537909706
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-06-01 18:47:31 +0300

    String prototypes polyfills on Object.defineProperty

--- libraries/stdlib/js-v1/src/js/polyfills.js
+++ libraries/stdlib/js-v1/src/js/polyfills.js
@@ -4,21 +4,25 @@
  */
 
 if (typeof String.prototype.startsWith === "undefined") {
-    String.prototype.startsWith = function(searchString, position) {
-        position = position || 0;
-        return this.lastIndexOf(searchString, position) === position;
-    };
+    Object.defineProperty(String.prototype, "startsWith", {
+        value: function (searchString, position) {
+            position = position || 0;
+            return this.lastIndexOf(searchString, position) === position;
+        }
+    });
 }
 if (typeof String.prototype.endsWith === "undefined") {
-    String.prototype.endsWith = function(searchString, position) {
-        var subjectString = this.toString();
-        if (position === undefined || position > subjectString.length) {
-            position = subjectString.length;
+    Object.defineProperty(String.prototype, "endsWith", {
+        value: function (searchString, position) {
+            var subjectString = this.toString();
+            if (position === undefined || position > subjectString.length) {
+                position = subjectString.length;
+            }
+            position -= searchString.length;
+            var lastIndex = subjectString.indexOf(searchString, position);
+            return lastIndex !== -1 && lastIndex === position;
         }
-        position -= searchString.length;
-        var lastIndex = subjectString.indexOf(searchString, position);
-        return lastIndex !== -1 && lastIndex === position;
-    };
+    });
 }
 // ES6 Math polyfills
 if (typeof Math.sign === "undefined") {
