3384a462ec6033fd6c9b3c3214f25d541186f0b3
Sergey Patrikeev <Sergey.Patrikeev@jetbrains.com>
2020-06-02 13:48:02 +0300

    Indexing diagnostic: exclude null values from jsons.
    intellij-monorepo: 6dac54b711c1b09e053e4dae2500b533885d96da

--- intellij-community/platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dto/JsonFileProviderIndexStatistics.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/util/indexing/diagnostic/dto/JsonFileProviderIndexStatistics.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.util.indexing.diagnostic.dto
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class JsonFileProviderIndexStatistics(
   val providerName: String,
   val totalNumberOfFiles: Int,
