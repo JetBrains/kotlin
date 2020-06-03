e3d6a6dfaf42c226169267ec50a6cdae9e50df06
Roman.Ivanov <Roman.Ivanov@jetbrains.com>
2020-06-03 15:56:38 +0700

    IconPresentation: fire content changed if icon is changed
    intellij-monorepo: 82cfa955bc5e684817dcc5434bf839cf50bde1d6

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/IconPresentation.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/presentation/IconPresentation.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.hints.presentation
 
+import com.intellij.codeInsight.hints.fireContentChanged
 import com.intellij.openapi.editor.markup.TextAttributes
 import java.awt.AlphaComposite
 import java.awt.Component
@@ -10,7 +11,13 @@ import javax.swing.Icon
 /**
  * Draws image. If you need to position image inside inlay, use [InsetPresentation]
  */
-class IconPresentation(var icon: Icon, private val component: Component) : BasePresentation() {
+class IconPresentation(icon: Icon, private val component: Component) : BasePresentation() {
+  var icon = icon
+  set(value) {
+    field = value
+    fireContentChanged()
+  }
+
   override val width: Int
     get() = icon.iconWidth
   override val height: Int
