bebbe9ad6fc738b63ed2e03c517bca76fb810cf9
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-06-04 19:25:27 +0300

    Change testData for `ShortenRefsTestGenerated` to not use `sql` package
    - Those packages are missing in the mockJdk 1.8 in intellij repository,
    and it leads to those tests failing in kotlin-ide repository
    - Test in `ShortenRefsTestGenerated` do not fail, but they are working
    for the wrong reason (they do not shorten unresolved references)
    - Cherry-picked from the f9b3daabd1f62cbceae84f2ab52afaee3a83484d

--- kotlin-ide/idea/testData/shortenRefs/type/FunctionType.kt
+++ kotlin-ide/idea/testData/shortenRefs/type/FunctionType.kt
@@ -1,3 +1,3 @@
 class A {
-    var x: <selection>((java.io.File, java.util.ArrayList<String>) -> java.awt.Container)?</selection> = null
+    var x: <selection>((java.io.File) -> java.util.ArrayList<String>)?</selection> = null
 }
