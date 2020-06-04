a3c881da59e94151285a41e5ca2b4593432de391
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2020-06-04 18:25:08 +0200

    KT-38027 Support Code Vision feature in Kotlin // experimental status
    Feature received "experimental" status due to the low performance.
    The root cause is slow find-usages functionality the feature is based
    on. Once the later is improved Code-Vision should be revisited.
    
    Previous commit of KT-38027 series contains production ready state.
    
    To launch performance check again one needs to enable Code Vision in
    AbstractPerformanceProjectsTest.setUp() with the following fun:
    
    private fun enabledCodeVision() {
      val codeVisionProvider = KotlinCodeVisionProvider()
      val settings = codeVisionProvider.createSettings().apply {
         showUsages = true
         showInheritors = true
      }
    
      InlayHintsSettings.instance().storeSettings(codeVisionProvider.key,
       KotlinLanguage.INSTANCE, settings)
    }

--- idea/src/org/jetbrains/kotlin/idea/codeInsight/codevision/KotlinCodeVisionProvider.kt
+++ idea/src/org/jetbrains/kotlin/idea/codeInsight/codevision/KotlinCodeVisionProvider.kt
@@ -8,14 +8,15 @@ package org.jetbrains.kotlin.idea.codeInsight.codevision
 import com.intellij.codeInsight.hints.*
 import com.intellij.lang.Language
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.KotlinBundle
 import org.jetbrains.kotlin.idea.KotlinLanguage
 
 @Suppress("UnstableApiUsage")
-class KotlinCodeVisionProvider : InlayHintsProvider<KotlinCodeVisionProvider.KotlinCodeVisionSettings> {
+class KotlinCodeVisionProvider : InlayHintsProvider<NoSettings> {
 
-    override val key: SettingsKey<KotlinCodeVisionSettings> = SettingsKey("CodeVision")
+    override val key: SettingsKey<NoSettings> = SettingsKey("CodeVision")
     override val name: String = KotlinBundle.message("hints.title.codevision")
     override val previewText: String? = null
 
@@ -24,16 +25,19 @@ class KotlinCodeVisionProvider : InlayHintsProvider<KotlinCodeVisionProvider.Kot
 
     override fun isLanguageSupported(language: Language): Boolean = language is KotlinLanguage
 
-    override fun createConfigurable(settings: KotlinCodeVisionSettings): ImmediateConfigurable = createImmediateConfigurable(settings)
+    override fun createConfigurable(settings: NoSettings): ImmediateConfigurable = createImmediateConfigurable()
 
-    override fun createSettings(): KotlinCodeVisionSettings = KotlinCodeVisionSettings()
+    override fun createSettings(): NoSettings = NoSettings()
 
     override fun getCollectorFor(
-        file: PsiFile, editor: Editor, settings: KotlinCodeVisionSettings, sink: InlayHintsSink
+        file: PsiFile, editor: Editor, settings: NoSettings, sink: InlayHintsSink
     ): InlayHintsCollector? {
-        if (!settings.showUsages && !settings.showInheritors) return null
-        return KotlinCodeVisionHintsCollector(editor, settings.showUsages, settings.showInheritors, usagesLimit, inheritorsLimit)
-    }
 
-    data class KotlinCodeVisionSettings(var showUsages: Boolean = false, var showInheritors: Boolean = false)
+        val showUsages = Registry.`is`("kotlin.code-vision.usages", false)
+        val showInheritors = Registry.`is`("kotlin.code-vision.inheritors", false)
+
+        if (!showUsages && !showInheritors) return null
+
+        return KotlinCodeVisionHintsCollector(editor, showUsages, showInheritors, usagesLimit, inheritorsLimit)
+    }
 }
