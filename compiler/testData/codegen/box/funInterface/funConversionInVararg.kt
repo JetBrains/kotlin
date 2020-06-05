bd2a0563add91211fc495abae31aa4aa4dbf7ecb
Anton Bannykh <anton.bannykh@jetbrains.com>
2020-06-05 11:52:13 +0300

    JS: fix explicit cross-module SAM constructor calls

--- compiler/testData/codegen/box/funInterface/funConversionInVararg.kt
+++ compiler/testData/codegen/box/funInterface/funConversionInVararg.kt
@@ -1,6 +1,6 @@
 // !LANGUAGE: +NewInference +FunctionalInterfaceConversion +SamConversionPerArgument
-// IGNORE_BACKEND: JS_IR
 // IGNORE_BACKEND_FIR: JVM_IR
+// SKIP_DCE_DRIVEN
 
 fun interface MyRunnable {
     fun run()
