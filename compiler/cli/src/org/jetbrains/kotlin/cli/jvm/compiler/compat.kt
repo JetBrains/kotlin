3f9421f9bddb54ebe61cc5c055c006d7c2b1a122
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2020-06-05 23:47:26 +0300

    Update idea.plugins.compatible.build in compiler

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/compat.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/compat.kt
@@ -6,7 +6,7 @@
 package org.jetbrains.kotlin.cli.jvm.compiler
 
 fun setupIdeaStandaloneExecution() {
-    System.getProperties().setProperty("idea.plugins.compatible.build", "201.6668.13")
+    System.getProperties().setProperty("idea.plugins.compatible.build", "202.5103")
     System.getProperties().setProperty("project.structure.add.tools.jar.to.new.jdk", "false")
     System.getProperties().setProperty("psi.track.invalidation", "true")
     System.getProperties().setProperty("psi.incremental.reparse.depth.limit", "1000")
