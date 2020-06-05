25589279205a9b03e77b2247098c5778f58d8afb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-06-05 18:12:01 +0200

    first attempt to fix CidrProjectFixture - try to open project not in EDT if possible (but in most cases CidrProjectFixture.openProject is called in EDT anyway) execute post start-up activities in tests directly in a calling thread if not in EDT
    intellij-monorepo: 3b3b7e85312a5e7070a5635f8a5f18eae8c2dbe6

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsSettingsMigration.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/hints/ParameterHintsSettingsMigration.kt
@@ -1,15 +1,24 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.hints
 
 import com.intellij.codeInsight.hints.settings.ParameterNameHintsSettings
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
-class ParameterHintsSettingsMigration : StartupActivity {
+internal class ParameterHintsSettingsMigration : StartupActivity {
+  init {
+    if (ApplicationManager.getApplication().isUnitTestMode) {
+      throw ExtensionNotApplicableException.INSTANCE
+    }
+  }
+
   override fun runActivity(project: Project) {
-    if (!EditorSettingsExternalizable.getInstance().isShowParameterNameHints) {
-      EditorSettingsExternalizable.getInstance().isShowParameterNameHints = true
+    val editorSettingsExternalizable = EditorSettingsExternalizable.getInstance()
+    if (!editorSettingsExternalizable.isShowParameterNameHints) {
+      editorSettingsExternalizable.isShowParameterNameHints = true
       for (language in getBaseLanguagesWithProviders()) {
         ParameterNameHintsSettings.getInstance().setIsEnabledForLanguage(false, language)
       }
