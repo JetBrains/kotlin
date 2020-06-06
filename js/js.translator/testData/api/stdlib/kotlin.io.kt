cbabb4f76a67196c69e3691bd19f273c77cad240
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2020-06-06 16:19:04 +0300

    JS stdlib api test: various changes
    - Setting -Doverwrite.output=true updates gold data
    - Big packages don't get split into chunks
    - Unique lines in the API are marked with /*∆*/ and diff test is removed
    - Annotations on separate lines and other dump format tweaks
    - Test data moved to libraries/stdlib/api/
    - Minor visibility fix to Enum members

--- js/js.translator/testData/api/stdlib/kotlin.io.kt
+++
@@ -1,5 +0,0 @@
-package kotlin.io
-
-public fun print(/*0*/ message: kotlin.Any?): kotlin.Unit
-public fun println(): kotlin.Unit
-public fun println(/*0*/ message: kotlin.Any?): kotlin.Unit
---
+++ libraries/stdlib/api/js-v1/kotlin.experimental.kt
@@ -0,0 +1,41 @@
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline infix fun kotlin.Byte.and(other: kotlin.Byte): kotlin.Byte
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline infix fun kotlin.Short.and(other: kotlin.Short): kotlin.Short
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline fun kotlin.Byte.inv(): kotlin.Byte
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline fun kotlin.Short.inv(): kotlin.Short
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline infix fun kotlin.Byte.or(other: kotlin.Byte): kotlin.Byte
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline infix fun kotlin.Short.or(other: kotlin.Short): kotlin.Short
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline infix fun kotlin.Byte.xor(other: kotlin.Byte): kotlin.Byte
+
+@kotlin.SinceKotlin(version = "1.1")
+@kotlin.internal.InlineOnly
+public inline infix fun kotlin.Short.xor(other: kotlin.Short): kotlin.Short
+
+@kotlin.Experimental(level = Level.ERROR)
+@kotlin.RequiresOptIn(level = Level.ERROR)
+@kotlin.annotation.MustBeDocumented
+@kotlin.annotation.Retention(value = AnnotationRetention.BINARY)
+@kotlin.annotation.Target(allowedTargets = {AnnotationTarget.ANNOTATION_CLASS})
+@kotlin.SinceKotlin(version = "1.3")
+public final annotation class ExperimentalTypeInference : kotlin.Annotation {
+    public constructor ExperimentalTypeInference()
+}
