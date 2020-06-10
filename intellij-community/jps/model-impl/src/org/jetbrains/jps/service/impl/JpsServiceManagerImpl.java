6aca6784a1fa32755188f19a97ebb2897227bf89
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2020-06-10 09:51:47 +0300

    [Dynamic plugins] cleanup Jps extension cache on plugin unload IDEA-238179
    intellij-monorepo: cfccfb31c3e7cf7fcdfc561d73e6628520bf4ca7

--- intellij-community/jps/model-impl/src/org/jetbrains/jps/service/impl/JpsServiceManagerImpl.java
+++ intellij-community/jps/model-impl/src/org/jetbrains/jps/service/impl/JpsServiceManagerImpl.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.jps.service.impl;
 
 import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.plugin.JpsPluginManager;
 import org.jetbrains.jps.service.JpsServiceManager;
@@ -82,16 +69,19 @@ public class JpsServiceManagerImpl extends JpsServiceManager {
     return (List<T>)cached;
   }
 
-  private boolean cleanupExtensionCache() {
-    JpsPluginManager manager = myPluginManager;
-    if (manager != null) {
-      int stamp = manager.getModificationStamp();
-      if (myModificationStamp.getAndSet(stamp) != stamp) {
-        myExtensions.clear();
-        return true;
+  @ApiStatus.Internal
+  public boolean cleanupExtensionCache() {
+    synchronized (myExtensions) {
+      JpsPluginManager manager = myPluginManager;
+      if (manager != null) {
+        int stamp = manager.getModificationStamp();
+        if (myModificationStamp.getAndSet(stamp) != stamp) {
+          myExtensions.clear();
+          return true;
+        }
       }
+      return false;
     }
-    return false;
   }
 
   @NotNull
