44294f310eceff4297dd7eefd8c0a17381f6a738
Anastasia Ivanova <anastasia.ivanova@jetbrains.com>
2020-06-14 16:21:47 +0700

    IDEA-CR-62611: IDEA-236152 remove redundant reportStatistic=true
    intellij-monorepo: f19c8721461e2eeb3bd4e0dcecff2c1b514df7a0

--- intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewSharedSettings.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/projectView/impl/ProjectViewSharedSettings.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.projectView.impl
 
 import com.intellij.openapi.components.PersistentStateComponent
@@ -10,7 +10,7 @@ import com.intellij.util.xmlb.XmlSerializerUtil
 /**
  * @author Konstantin Bulenkov
  */
-@State(name = "ProjectViewSharedSettings", storages = [(Storage(value = "projectView.xml"))], reportStatistic = true)
+@State(name = "ProjectViewSharedSettings", storages = [(Storage(value = "projectView.xml"))])
 class ProjectViewSharedSettings : PersistentStateComponent<ProjectViewSharedSettings> {
   var flattenPackages: Boolean = false
   var showMembers: Boolean = false
