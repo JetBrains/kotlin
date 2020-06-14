32a392079ba404a03632adc548c5f284e13c4f24
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-06-14 18:54:48 +0300

    Fix null passed to @NotNull in `PomModelImpl`
    Fixes `AdditionalResolveDescriptorRendererTestGenerated#testAnonymousObjectInClassParameterInitializer`
    and others

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/resolve/compat.kt
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/resolve/compat.kt
@@ -7,14 +7,7 @@ package org.jetbrains.kotlin.idea.resolve
 
 import com.intellij.mock.MockProject
 import com.intellij.pom.PomModel
-import com.intellij.pom.tree.TreeAspect
 import org.jetbrains.kotlin.idea.caches.trackers.KotlinCodeBlockModificationListener
-
-internal fun createAndRegisterKotlinCodeBlockModificationListener(project: MockProject, pomModel: PomModel, treeAspect: TreeAspect) {
-    project.registerService(PomModel::class.java, pomModel)
-    project.registerService(TreeAspect::class.java, treeAspect)
-    project.registerService(KotlinCodeBlockModificationListener::class.java, KotlinCodeBlockModificationListener(project))
-}
 
 internal fun unregisterKotlinCodeBlockModificationListener(project: MockProject) {
     val picoContainer = project.picoContainer
