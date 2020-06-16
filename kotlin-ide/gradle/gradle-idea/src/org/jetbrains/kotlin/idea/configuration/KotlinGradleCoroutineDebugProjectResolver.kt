18c428181d4a93b060c63ed2e4997c4657e88b0f
Vladimir Ilmov <vladimir.ilmov@jetbrains.com>
2020-06-16 20:52:19 +0200

    (CoroutineDebugger) fails to start in gradle mode
     #KT-39634 fixed
    
    Original commit: db127bb414b987ba2503c6d63e2c7484e85b8d97

--- kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/configuration/KotlinGradleCoroutineDebugProjectResolver.kt
+++ kotlin-ide/gradle/gradle-idea/src/org/jetbrains/kotlin/idea/configuration/KotlinGradleCoroutineDebugProjectResolver.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.idea.configuration
 
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.util.Consumer
+import org.jetbrains.kotlin.idea.debugger.KotlinDebuggerSettings
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
 
 class KotlinGradleCoroutineDebugProjectResolver : AbstractProjectResolverExtension() {
@@ -14,7 +15,9 @@ class KotlinGradleCoroutineDebugProjectResolver : AbstractProjectResolverExtensi
 
     override fun enhanceTaskProcessing(taskNames: MutableList<String>, jvmParametersSetup: String?, initScriptConsumer: Consumer<String>) {
         try {
-            setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer)
+            val disableCoroutineAgent = KotlinDebuggerSettings.getInstance().debugDisableCoroutineAgent
+            if (!disableCoroutineAgent)
+                setupCoroutineAgentForJvmForkedTestTasks(initScriptConsumer)
         } catch (e: Exception) {
             log.error("Gradle: not possible to attach a coroutine debugger agent.", e)
         }
@@ -25,11 +28,22 @@ class KotlinGradleCoroutineDebugProjectResolver : AbstractProjectResolverExtensi
             //language=Gradle
             """
             gradle.taskGraph.beforeTask { Task task ->
-              if (task instanceof Test) {
-                def kotlinxCoroutinesDebugJar = task.classpath.find { it.name.startsWith("kotlinx-coroutines-debug") }
-                if (kotlinxCoroutinesDebugJar)
-                    task.jvmArgs ("-javaagent:${'$'}{kotlinxCoroutinesDebugJar?.absolutePath}", "-ea")
-              }
+                if (task instanceof Test) {
+                    def kotlinxCoroutinesDebugJar = task.classpath.find { it.name.startsWith("kotlinx-coroutines-debug") }
+                    if (kotlinxCoroutinesDebugJar)
+                        task.jvmArgs ("-javaagent:${'$'}{kotlinxCoroutinesDebugJar?.absolutePath}", "-ea")
+                    else {
+                        for (lib in task.getClasspath()) {
+                            def results = (lib.getName() =~ /kotlinx-coroutines-core\-([\d\.]+)\.jar${'$'}/).findAll()
+                            if (results) {
+                                def version = results.first()[1]
+                                if (org.gradle.util.VersionNumber.parse( version ) >= org.gradle.util.VersionNumber.parse( '1.3.6' )) {
+                                    task.jvmArgs ("-javaagent:${'$'}{lib?.absolutePath}", "-ea")
+                                }
+                            }
+                        }
+                    }
+                }
             }
             """.trimIndent()
         initScriptConsumer.consume(script)
