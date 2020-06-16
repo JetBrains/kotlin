d56de7b2ed1b0a37134011b44a46a7fd30cf6e9b
Peter Gromov <peter@jetbrains.com>
2020-06-16 15:46:48 +0200

    ImaginaryEditor: add javadoc describing the intended usage, move some simplified logic into clients
    (cherry picked from commit c22fc84ab8015e64c1e85175329333ca1101e485)
    
    intellij-monorepo: e2b79d6a9d6eba5c1303e6009dee6c268ab9b6a0

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/intention/impl/preview/IntentionPreviewEditor.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/intention/impl/preview/IntentionPreviewEditor.kt
@@ -1,8 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.intention.impl.preview
 
+import com.intellij.openapi.editor.impl.EmptySoftWrapModel
+import com.intellij.openapi.editor.LogicalPosition
+import com.intellij.openapi.editor.SoftWrapModel
+import com.intellij.openapi.editor.VisualPosition
 import com.intellij.openapi.editor.impl.ImaginaryEditor
 import com.intellij.psi.PsiFile
+import kotlin.math.min
 
 internal class IntentionPreviewEditor(psiFileCopy: PsiFile, caretOffset: Int)
   : ImaginaryEditor(psiFileCopy.project, psiFileCopy.viewProvider.document!!) {
@@ -15,6 +20,30 @@ internal class IntentionPreviewEditor(psiFileCopy: PsiFile, caretOffset: Int)
 
   override fun isViewer(): Boolean = true
 
+  override fun logicalPositionToOffset(pos: LogicalPosition): Int {
+    val document = document
+    val lineStart = document.getLineStartOffset(pos.line)
+    val lineEnd = document.getLineEndOffset(pos.line)
+    return min(lineEnd, lineStart + pos.column)
+  }
+
+  override fun logicalToVisualPosition(logicalPos: LogicalPosition): VisualPosition {
+    // No folding support: logicalPos is always the same as visual pos
+    return VisualPosition(logicalPos.line, logicalPos.column)
+  }
+
+  override fun visualToLogicalPosition(visiblePos: VisualPosition): LogicalPosition {
+    return LogicalPosition(visiblePos.line, visiblePos.column)
+  }
+
+  override fun offsetToLogicalPosition(offset: Int): LogicalPosition {
+    val document = document
+    val line = document.getLineNumber(offset)
+    val col = offset - document.getLineStartOffset(line)
+    return LogicalPosition(line, col)
+  }
+
+  override fun getSoftWrapModel(): SoftWrapModel = EmptySoftWrapModel()
 }
 
 class IntentionPreviewUnsupportedOperationException
