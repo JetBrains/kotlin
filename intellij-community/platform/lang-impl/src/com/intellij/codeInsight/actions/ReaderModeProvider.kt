081f1eab847cce2d26c59984e14981c7f3335dd9
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-06-17 21:38:37 +0300

    IDEA-CR-63632 IDEA-243010 IJP-778 Reader mode: add docs
    (cherry picked from commit 154272584b58229183abac1090b08c7247137454)
    
    intellij-monorepo: 968bea3e96a49889ee061e0e58fd5a7696a08f35

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/actions/ReaderModeProvider.kt
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/actions/ReaderModeProvider.kt
@@ -8,5 +8,11 @@ import com.intellij.openapi.project.Project
  * Use it to provide an activity that should be applied for editor in Read Mode
  */
 interface ReaderModeProvider {
+  /**
+   * It's triggered on Reader Mode turning on or turning off.
+   *
+   * If {@param fileIsOpenAlready} is true then provider should apply changes only for already opened files,
+   * otherwise, if it's false, it should apply changes for every opening file
+   */
   fun applyModeChanged(project: Project, editor: Editor, readerMode: Boolean, fileIsOpenAlready: Boolean) {}
 }
