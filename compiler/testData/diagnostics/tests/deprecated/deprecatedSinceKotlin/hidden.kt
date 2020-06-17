158013ef3a32779e6e38001520c60565c80cb94e
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-06-17 18:52:59 +0300

    Remove message and replaceWith parameters from DeprecatedSinceKotlin

--- compiler/testData/diagnostics/tests/deprecated/deprecatedSinceKotlin/hidden.kt
+++ compiler/testData/diagnostics/tests/deprecated/deprecatedSinceKotlin/hidden.kt
@@ -1,20 +1,26 @@
 // !API_VERSION: 1.3
 
+@Deprecated("")
 @DeprecatedSinceKotlin("", hiddenSince = "1.3")
 class ClassCur
 
+@Deprecated("")
 @DeprecatedSinceKotlin("", hiddenSince = "1.3")
 fun funCur() {}
 
+@Deprecated("")
 @DeprecatedSinceKotlin("", hiddenSince = "1.3")
 val valCur = Unit
 
+@Deprecated("")
 @DeprecatedSinceKotlin("", hiddenSince = "1.4")
 class ClassNext
 
+@Deprecated("")
 @DeprecatedSinceKotlin("", hiddenSince = "1.4")
 fun funNext() {}
 
+@Deprecated("")
 @DeprecatedSinceKotlin("", hiddenSince = "1.4")
 val valNext = Unit
 
