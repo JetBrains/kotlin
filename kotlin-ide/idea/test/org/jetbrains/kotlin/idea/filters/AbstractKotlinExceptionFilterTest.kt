ff916a7a4238cb97d690501f868abca204e74de1
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-06-17 21:42:42 +0900

    Fix AbstractKotlinExceptionFilterTest

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/filters/AbstractKotlinExceptionFilterTest.kt
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/filters/AbstractKotlinExceptionFilterTest.kt
@@ -6,18 +6,19 @@
 package org.jetbrains.kotlin.idea.filters
 
 import com.intellij.execution.filters.FileHyperlinkInfo
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.OrderRootType
+import com.intellij.openapi.roots.ui.configuration.libraryEditor.NewLibraryEditor
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.testFramework.PsiTestUtil
 import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.fileClasses.JvmFileClassUtil
 import org.jetbrains.kotlin.idea.core.util.toVirtualFile
+import org.jetbrains.kotlin.idea.test.ConfigLibraryUtil
 import org.jetbrains.kotlin.idea.test.KotlinCodeInsightTestCase
+import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.test.PluginTestCaseBase
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.test.InTextDirectivesUtils
@@ -27,16 +28,16 @@ import java.lang.reflect.InvocationTargetException
 import java.net.URL
 import java.net.URLClassLoader
 
-private var MOCK_LIBRARY_JAR: File? = null
-private val MOCK_LIBRARY_SOURCES = PluginTestCaseBase.getTestDataPathBase() + "/debugger/mockLibraryForExceptionFilter"
-
 abstract class AbstractKotlinExceptionFilterTest : KotlinCodeInsightTestCase() {
+    private val MOCK_LIBRARY_NAME = "mockLibrary"
+    private val MOCK_LIBRARY_SOURCES = PluginTestCaseBase.getTestDataPathBase() + "/debugger/mockLibraryForExceptionFilter"
+
     override fun getTestDataPath() = ""
 
     protected fun doTest(path: String) {
         val rootDir = File(path)
         val mainFile = File(rootDir, rootDir.name + ".kt")
-        rootDir.listFiles().filter { it != mainFile }.forEach { configureByFile(it.canonicalPath) }
+        rootDir.listFiles().orEmpty().filter { it != mainFile }.forEach { configureByFile(it.canonicalPath) }
         configureByFile(mainFile.canonicalPath)
 
         val fileText = file.text
@@ -44,29 +45,26 @@ abstract class AbstractKotlinExceptionFilterTest : KotlinCodeInsightTestCase() {
         val outDir = runWriteAction {
             project.baseDir.findChild("out") ?: project.baseDir.createChildDirectory(this, "out")
         }
+
+        // Clean the output directory as it shared between tests
+        File(outDir.path).listFiles()?.forEach { it.deleteRecursively() }
+
         PsiTestUtil.setCompilerOutputPath(module, outDir.url, false)
 
         val extraOptions = InTextDirectivesUtils.findListWithPrefixes(fileText, "// !LANGUAGE: ").map { "-XXLanguage:$it" }
         val classLoader: URLClassLoader
-        if (InTextDirectivesUtils.getPrefixedBoolean(fileText, "// WITH_MOCK_LIBRARY: ") ?: false) {
-            if (MOCK_LIBRARY_JAR == null) {
-                val sources = listOf(File(MOCK_LIBRARY_SOURCES))
-                MOCK_LIBRARY_JAR = KotlinCompilerStandalone(sources).compile()
-            }
-
-            val mockLibraryJar = MOCK_LIBRARY_JAR ?: throw AssertionError("Mock library JAR is null")
+        if (InTextDirectivesUtils.getPrefixedBoolean(fileText, "// WITH_MOCK_LIBRARY: ") == true) {
+            val mockLibraryJar = compileMockLibrary()
             val mockLibraryPath = FileUtilRt.toSystemIndependentName(mockLibraryJar.canonicalPath)
             val libRootUrl = "jar://$mockLibraryPath!/"
 
-            ApplicationManager.getApplication().runWriteAction {
-                val moduleModel = ModuleRootManager.getInstance(myModule).modifiableModel
-                with(moduleModel.moduleLibraryTable.modifiableModel.createLibrary("mockLibrary").modifiableModel) {
-                    addRoot(libRootUrl, OrderRootType.CLASSES)
-                    addRoot(libRootUrl + "src/", OrderRootType.SOURCES)
-                    commit()
-                }
-                moduleModel.commit()
+            val editor = NewLibraryEditor().apply {
+                name = MOCK_LIBRARY_NAME
+                addRoot(libRootUrl, OrderRootType.CLASSES)
+                addRoot("file://" + MOCK_LIBRARY_SOURCES, OrderRootType.SOURCES)
             }
+
+            ConfigLibraryUtil.addLibrary(editor, module)
 
             val sources = listOf(File(path))
             val classpath = listOf(File(mockLibraryPath))
@@ -78,7 +76,9 @@ abstract class AbstractKotlinExceptionFilterTest : KotlinCodeInsightTestCase() {
             )
         } else {
             val sources = listOf(File(path))
-            KotlinCompilerStandalone(sources, target = File(outDir.path), options = extraOptions)
+            val outIoDir = File(outDir.path)
+            outIoDir.deleteRecursively()
+            KotlinCompilerStandalone(sources, target = outIoDir, options = extraOptions).compile()
 
             classLoader = URLClassLoader(
                 arrayOf(URL(outDir.url + "/")),
@@ -89,7 +89,7 @@ abstract class AbstractKotlinExceptionFilterTest : KotlinCodeInsightTestCase() {
         val stackTraceElement = try {
             val className = JvmFileClassUtil.getFileClassInfoNoResolve(file as KtFile).fileClassFqName
             val clazz = classLoader.loadClass(className.asString())
-            clazz.getMethod("box")?.invoke(null)
+            clazz.getMethod("box").invoke(null)
             throw AssertionError("class ${className.asString()} should have box() method and throw exception")
         } catch (e: InvocationTargetException) {
             e.targetException.stackTrace[0]
@@ -140,5 +140,15 @@ abstract class AbstractKotlinExceptionFilterTest : KotlinCodeInsightTestCase() {
             "$expectedFileName:$expectedOffset",
             descriptor.file.name + ":" + descriptor.offset
         )
+    }
+
+    override fun tearDown() {
+        ConfigLibraryUtil.removeLibrary(module, MOCK_LIBRARY_NAME)
+        super.tearDown()
+    }
+
+    private fun compileMockLibrary(): File {
+        val sources = listOf(File(MOCK_LIBRARY_SOURCES))
+        return KotlinCompilerStandalone(sources).compile()
     }
 }
