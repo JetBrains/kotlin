5b0f882fc028d3828644f0a86ea9ad3898a5f1b1
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-06-18 19:18:06 +0300

    202: Fix `MultiFileJvmBasicCompletionTestGenerated`
    It was broken by 4bef803e1b5994e9ea9731acfb5095a94b1b1383 in intellij

--- idea/idea-completion/testData/basic/multifile/PropertyKeysWithPrefix/PropertyKeysWithPrefix.kt
+++ idea/idea-completion/testData/basic/multifile/PropertyKeysWithPrefix/PropertyKeysWithPrefix.kt
@@ -6,7 +6,7 @@ fun test() {
     message("foo.<caret>")
 }
 
-// EXIST: { lookupString: "foo.bar", itemText: "foo.bar", tailText: "1", typeText: "PropertyKeysWithPrefix" }
-// EXIST: { lookupString: "foo.bar.baz", itemText: "foo.bar.baz", tailText: "3", typeText: "PropertyKeysWithPrefix" }
-// EXIST: { lookupString: "foo.test", itemText: "foo.test", tailText: "4", typeText: "PropertyKeysWithPrefix" }
+// EXIST: { lookupString: "foo.bar", itemText: "foo.bar", tailText: "=1", typeText: "PropertyKeysWithPrefix" }
+// EXIST: { lookupString: "foo.bar.baz", itemText: "foo.bar.baz", tailText: "=3", typeText: "PropertyKeysWithPrefix" }
+// EXIST: { lookupString: "foo.test", itemText: "foo.test", tailText: "=4", typeText: "PropertyKeysWithPrefix" }
 // NOTHING_ELSE
