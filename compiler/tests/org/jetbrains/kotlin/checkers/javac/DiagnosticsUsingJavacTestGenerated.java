127156042b5367acf3cf25dbae0d2659b4a51d38
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-06-18 13:05:19 +0300

    Add test for obsolete issue
     The issue was fixed in df1595e
    
     #KT-39630 Fixed
    
    (cherry picked from commit d44a7ff8f9b186ebcbb5cc2d64377840d4e4c56c)

--- compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -13688,6 +13688,11 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                     runTest("compiler/testData/diagnostics/tests/j+k/sam/kt37920.kt");
                 }
 
+                @TestMetadata("kt39630.kt")
+                public void testKt39630() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/j+k/sam/kt39630.kt");
+                }
+
                 @TestMetadata("privateCandidatesWithWrongArguments.kt")
                 public void testPrivateCandidatesWithWrongArguments() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/j+k/sam/privateCandidatesWithWrongArguments.kt");
