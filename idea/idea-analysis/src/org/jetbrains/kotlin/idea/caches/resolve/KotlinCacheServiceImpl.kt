1ebb11605600be623b861e7fef9f77576391f4ee
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-06-19 17:11:48 +0300

    Revert "Invalidate library caches on OOCBM with enabled resolution anchors"
    This reverts commit 91b371789e27e46fb918b2914af6301b64faa564.
    ^KT-39734 Open

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/KotlinCacheServiceImpl.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/KotlinCacheServiceImpl.kt
@@ -49,7 +49,6 @@ import org.jetbrains.kotlin.idea.compiler.IDELanguageSettingsProvider
 import org.jetbrains.kotlin.idea.core.script.ScriptDependenciesModificationTracker
 import org.jetbrains.kotlin.idea.core.script.dependencies.ScriptAdditionalIdeaDependenciesProvider
 import org.jetbrains.kotlin.idea.project.TargetPlatformDetector
-import org.jetbrains.kotlin.idea.project.libraryToSourceAnalysisEnabled
 import org.jetbrains.kotlin.idea.project.useCompositeAnalysis
 import org.jetbrains.kotlin.idea.resolve.ResolutionFacade
 import org.jetbrains.kotlin.idea.util.ProjectRootsUtil
@@ -205,7 +204,7 @@ class KotlinCacheServiceImpl(val project: Project) : KotlinCacheService {
             project, librariesContext, settings,
             reuseDataFrom = facadeForSdk,
             moduleFilter = { it is LibraryInfo },
-            invalidateOnOOCB = project.libraryToSourceAnalysisEnabled,
+            invalidateOnOOCB = false,
             dependencies = listOf(
                 LibraryModificationTracker.getInstance(project),
                 ProjectRootModificationTracker.getInstance(project)
