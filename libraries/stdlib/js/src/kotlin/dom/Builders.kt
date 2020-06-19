1f04feddf3048d1d85e1682faa3ed37853bc24d2
Alexey Trilis <alexey.trilis@jetbrains.com>
2020-06-19 15:05:44 +0300

    Deprecate kotlin.browser and kotlin.dom packages and provide
    replacements in packages kotlinx.dom and kotlinx.browser
    
    KT-39330 Fixed
    
    (cherry picked from commit 99d844dcfb2703934c7d2e4eb2d1eaaf4080fd9b)

--- libraries/stdlib/js/src/kotlin/dom/Builders.kt
+++ libraries/stdlib/js/src/kotlin/dom/Builders.kt
@@ -5,20 +5,33 @@
 
 package kotlin.dom
 
-import org.w3c.dom.*
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import kotlin.internal.LowPriorityInOverloadResolution
+import kotlinx.dom.appendElement as newAppendElement
+import kotlinx.dom.createElement as newCreateElement
 
 /**
  * Creates a new element with the specified [name].
  *
  * The element is initialized with the speicifed [init] function.
  */
-public fun Document.createElement(name: String, init: Element.() -> Unit): Element = createElement(name).apply(init)
+@LowPriorityInOverloadResolution
+@Deprecated(
+    message = "This API is moved to another package, use 'kotlinx.dom.createElement' instead.",
+    replaceWith = ReplaceWith("this.createElement(name, init)", "kotlinx.dom.createElement")
+)
+public inline fun Document.createElement(name: String, noinline init: Element.() -> Unit): Element = this.newCreateElement(name, init)
 
 /**
  * Appends a newly created element with the specified [name] to this element.
  *
  * The element is initialized with the speicifed [init] function.
  */
-public fun Element.appendElement(name: String, init: Element.() -> Unit): Element =
-    ownerDocument!!.createElement(name, init).also { appendChild(it) }
+@LowPriorityInOverloadResolution
+@Deprecated(
+    message = "This API is moved to another package, use 'kotlinx.dom.appendElement' instead.",
+    replaceWith = ReplaceWith("this.appendElement(name, init)", "kotlinx.dom.appendElement")
+)
+public inline fun Element.appendElement(name: String, noinline init: Element.() -> Unit): Element = this.newAppendElement(name, init)
 
