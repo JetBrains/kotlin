b657d60bd6100bb142915d472ff1d352eab76eac
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-06-23 17:22:04 +0700

    Suggest `Create Class` before `Create File`
    #KT-37528 Fixed

--- idea/src/org/jetbrains/kotlin/idea/actions/NewKotlinFileAction.kt
+++ idea/src/org/jetbrains/kotlin/idea/actions/NewKotlinFileAction.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.actions
@@ -88,14 +77,14 @@ class NewKotlinFileAction : CreateFileFromTemplateAction(
     override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
         builder.setTitle(KotlinBundle.message("action.new.file.dialog.title"))
             .addKind(
-                KotlinBundle.message("action.new.file.dialog.file.title"),
-                KotlinFileType.INSTANCE.icon,
-                "Kotlin File"
-            )
-            .addKind(
                 KotlinBundle.message("action.new.file.dialog.class.title"),
                 KotlinIcons.CLASS,
                 "Kotlin Class"
+            )
+            .addKind(
+                KotlinBundle.message("action.new.file.dialog.file.title"),
+                KotlinFileType.INSTANCE.icon,
+                "Kotlin File"
             )
             .addKind(
                 KotlinBundle.message("action.new.file.dialog.interface.title"),
