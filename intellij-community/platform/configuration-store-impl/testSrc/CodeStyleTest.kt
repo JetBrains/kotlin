9a8c030eeda894e080a11ec280e0ad8f5c126ad6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-06-23 17:54:48 +0200

    no need to runPostStartUpActivities for these tests
    (cherry picked from commit d173d2e22e1654eda4e47335a743cd7c9e40ae4e)
    
    intellij-monorepo: 1f825f590858839ad3ce2bc186f757b51ffae490

--- intellij-community/platform/configuration-store-impl/testSrc/CodeStyleTest.kt
+++ intellij-community/platform/configuration-store-impl/testSrc/CodeStyleTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.configurationStore
 
 import com.intellij.application.options.CodeStyle
@@ -20,7 +20,7 @@ internal class CodeStyleTest {
   companion object {
     @JvmField
     @ClassRule
-    val projectRule = ProjectRule()
+    val projectRule = ProjectRule(runPostStartUpActivities = false)
   }
 
   @JvmField
