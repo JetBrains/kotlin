81f60bf252b6a4660d0065ae1c2f729384024e5a
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-06-24 18:39:09 +0300

    Add test for smartcast

---
+++ idea/idea-completion/testData/basic/common/primitiveCompletion/smartCast.kt
@@ -0,0 +1,21 @@
+interface A {
+    fun a()
+}
+
+interface B {
+    fun b()
+}
+
+interface C {
+    fun c()
+}
+
+fun take(a: A) {
+    if (a is B && a is C) {
+        a.<caret>
+    }
+}
+
+// EXIST: a
+// EXIST: b
+// EXIST: c
