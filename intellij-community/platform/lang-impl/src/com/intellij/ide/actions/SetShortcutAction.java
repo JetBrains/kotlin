3dab6aaa4a63ccc6720a5ca55ce8c728d716fd35
Nikolay Kuznetsov <nikolay.kuznetsov@jetbrains.com>
2020-06-26 16:06:23 +0200

    IDEA-CR-1982: Fix review note from IDEA-CR-1249
    (cherry picked from commit e396385deda5cd7bb2bde9d68504e02c0c04c5dc)
    
    intellij-monorepo: 1c9f6cc50e250044484b0229cc1716ea05521083

--- intellij-community/platform/lang-impl/src/com/intellij/ide/actions/SetShortcutAction.java
+++ intellij-community/platform/lang-impl/src/com/intellij/ide/actions/SetShortcutAction.java
@@ -1,19 +1,18 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.ide.actions;
 
+import com.intellij.codeWithMe.ClientId;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.keymap.Keymap;
 import com.intellij.openapi.keymap.KeymapManager;
 import com.intellij.openapi.keymap.impl.ActionShortcutRestrictions;
 import com.intellij.openapi.keymap.impl.ui.KeymapPanel;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
-import com.intellij.codeWithMe.ClientId;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Optional;
 
 public class SetShortcutAction extends AnAction implements DumbAware {
 
@@ -25,12 +24,7 @@ public class SetShortcutAction extends AnAction implements DumbAware {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-
-    if (project == null) return;
-    ConcurrentHashMap<ClientId, JBPopup> map = project.getUserData(SearchEverywhereAction.SEARCH_EVERYWHERE_POPUP);
-    if(map == null) return;
-    JBPopup seDialog = map.get(ClientId.getCurrent());
+    JBPopup seDialog = getPopup(e).orElse(null);
     if (seDialog == null) return;
 
     KeymapManager km = KeymapManager.getInstance();
@@ -50,17 +44,18 @@ public class SetShortcutAction extends AnAction implements DumbAware {
     KeymapPanel.addKeyboardShortcut(id, ActionShortcutRestrictions.getInstance().getForActionId(id), activeKeymap, component);
   }
 
+  @NotNull
+  private static Optional<JBPopup> getPopup(@NotNull AnActionEvent e) {
+    return Optional.ofNullable(e.getProject()).map(it -> it.getUserData(SearchEverywhereAction.SEARCH_EVERYWHERE_POPUP))
+      .map(it -> it.get(ClientId.getCurrent()));
+  }
+
   @Override
   public void update(@NotNull AnActionEvent e) {
     Presentation presentation = e.getPresentation();
 
-    Project project = e.getProject();
-    JBPopup seDialog;
-    if (project == null) seDialog = null; else {
-      ConcurrentHashMap<ClientId, JBPopup> map = project.getUserData(SearchEverywhereAction.SEARCH_EVERYWHERE_POPUP);
-      seDialog = map == null ? null : map.get(ClientId.getCurrent());
-    }
-    if (seDialog == null) {
+    Optional<JBPopup> seDialog = getPopup(e);
+    if (!seDialog.isPresent()) {
       presentation.setEnabled(false);
       return;
     }
