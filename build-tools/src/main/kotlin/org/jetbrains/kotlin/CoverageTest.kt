26defca5c3b21d25ace222cf8ac8e20bcd463ae3
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2020-06-29 20:03:30 +0300

    Fix running coverage tests on cross-targets

--- build-tools/src/main/kotlin/org/jetbrains/kotlin/CoverageTest.kt
+++ build-tools/src/main/kotlin/org/jetbrains/kotlin/CoverageTest.kt
@@ -72,7 +72,7 @@ open class CoverageTest : DefaultTask() {
     fun run() {
         val suffix = target.family.exeSuffix
         val pathToBinary = "$outputDir/$binaryName/$target/$binaryName.$suffix"
-        runProcess(localExecutor(project), pathToBinary)
+        runProcess({ project.executor.execute(it) }, pathToBinary)
                 .ensureSuccessful(pathToBinary)
         exec("llvm-profdata", "merge", profrawFile, "-o", profdataFile)
         val llvmCovResult = exec("llvm-cov", "export", pathToBinary, "-instr-profile", profdataFile)
