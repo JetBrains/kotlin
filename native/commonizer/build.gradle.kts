12c766bf29369449be1d8b617149f2ab51aa9c78
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2020-06-30 12:20:46 +0700

    [Commonizer] Embed :kotlinx-metadata-klib into the commonizer Jar

--- native/commonizer/build.gradle.kts
+++ native/commonizer/build.gradle.kts
@@ -14,6 +14,13 @@ configurations {
 }
 
 dependencies {
+    embedded(project(":kotlinx-metadata-klib")) { isTransitive = false }
+    embedded(project(":kotlinx-metadata")) { isTransitive = false }
+
+    // N.B. The order of "kotlinx-metadata*" dependencies makes sense for runtime classpath
+    // of the "runCommonizer" task. Please, don't mix them up.
+    compileOnly(project(":kotlinx-metadata-klib")) { isTransitive = false }
+    compileOnly(project(":kotlinx-metadata")) { isTransitive = false }
     compileOnly(project(":compiler:cli-common"))
     compileOnly(project(":compiler:ir.serialization.common"))
     compileOnly(project(":compiler:frontend"))
