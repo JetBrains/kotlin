fa36739fa57eeecdbc359c5a4305f92e3468a698
Roman Golyshev <anstreth@gmail.com>
2020-07-01 14:23:00 +0300

    Completion with FIR (prototype, work-in-progress)
    Original commit: a1b621d987ee41226d4b45af5522415debb71174

--- kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/LowLevelFirApiFacade.kt
+++ kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/LowLevelFirApiFacade.kt
@@ -7,9 +7,12 @@ package org.jetbrains.kotlin.idea.fir.low.level.api
 
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.fir.FirElement
+import org.jetbrains.kotlin.fir.declarations.FirFile
+import org.jetbrains.kotlin.fir.declarations.FirFunction
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 object LowLevelFirApiFacade {
     fun getResolveStateFor(element: KtElement): FirModuleResolveState =
@@ -24,6 +27,26 @@ object LowLevelFirApiFacade {
 
     fun getOrBuildFirFor(element: KtElement, resolveState: FirModuleResolveState, phase: FirResolvePhase): FirElement =
         element.getOrBuildFir(resolveState, phase)
+
+
+    fun getFirOfClosestParent(element: KtElement): FirElement? = element.getFirOfClosestParent(element.firResolveState())?.second
+
+    fun buildFunctionWithResolvedBody(
+        firFile: FirFile,
+        element: KtNamedFunction,
+        phase: FirResolvePhase = FirResolvePhase.BODY_RESOLVE
+    ): FirFunction<*> {
+        val state = element.firResolveState()
+        val firIdeProvider = firFile.session.firIdeProvider
+        val builtFunction = firIdeProvider.buildFunctionWithBody(element)
+
+        return builtFunction.apply {
+            runResolve(firFile, firIdeProvider, phase, state)
+
+            // TODO this PSI caching should be somewhere else
+            state.recordElementsFrom(this)
+        }
+    }
 
     fun getDiagnosticsFor(element: KtElement, resolveState: FirModuleResolveState): Collection<Diagnostic> {
         val file = element.containingKtFile
