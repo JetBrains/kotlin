17c15cfe0a4a77bc9089769efa75f3e7d52911d0
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-07-02 15:23:27 +0300

    JVM, JVM_IR: update bytecode listing testData for inline classes

--- compiler/testData/codegen/bytecodeListing/inlineClasses/publishedApiAnnotationOnInlineClassConstructor.kt
+++ compiler/testData/codegen/bytecodeListing/inlineClasses/publishedApiAnnotationOnInlineClassConstructor.kt
@@ -1,5 +1,5 @@
 // !LANGUAGE: +InlineClasses
-// IGNORE_BACKEND: JVM_IR
+// IGNORE_BACKEND: JVM
 
 @Suppress("NON_PUBLIC_PRIMARY_CONSTRUCTOR_OF_INLINE_CLASS")
 inline class Z @PublishedApi internal constructor(val value: Int)
