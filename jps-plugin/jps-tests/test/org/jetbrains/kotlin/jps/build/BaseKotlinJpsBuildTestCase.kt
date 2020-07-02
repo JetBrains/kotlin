6378124afdfb005ce41c04965d36b41ae6d03bfc
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-07-02 16:50:34 +0300

    [CHERRY PICKED FROM IJ] Fix compilation with intellij
    GitOrigin-RevId: 2fa9aa992b9d4673a641a7e1d06402c78ca29400
    Original commit: https://github.com/JetBrains/intellij-community/commit/0f44ee9b644fa98801aec31468edc919c1abbb9f

--- jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/BaseKotlinJpsBuildTestCase.kt
+++ jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/BaseKotlinJpsBuildTestCase.kt
@@ -16,6 +16,7 @@
 
 package org.jetbrains.kotlin.jps.build
 
+import com.intellij.util.ThrowableRunnable
 import org.jetbrains.jps.builders.JpsBuildTestCase
 import org.jetbrains.jps.model.library.JpsLibrary
 import org.jetbrains.kotlin.compilerRunner.JpsKotlinCompilerRunner
@@ -45,9 +46,9 @@ abstract class BaseKotlinJpsBuildTestCase : JpsBuildTestCase() {
         library.create(myProject)
     }
 
-    override fun runTest() {
+    override fun runTestRunnable(testRunnable: ThrowableRunnable<Throwable>) {
         runTest {
-            super.runTest()
+            super.runTestRunnable(testRunnable)
         }
     }
 }
