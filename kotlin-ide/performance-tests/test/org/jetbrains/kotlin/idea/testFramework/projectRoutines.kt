755aebd6172aae42182b837edac1a40f6d47717c
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2020-07-04 23:33:02 +0200

    Add dispatchAllInvocationEvents to editor actions in perf tests
    Original commit: 03d5fe8d6cd9fd9d93fa1cc68c82c68e7777ec8b

--- kotlin-ide/performance-tests/test/org/jetbrains/kotlin/idea/testFramework/projectRoutines.kt
+++ kotlin-ide/performance-tests/test/org/jetbrains/kotlin/idea/testFramework/projectRoutines.kt
@@ -14,6 +14,7 @@ import com.intellij.lang.LanguageAnnotators
 import com.intellij.lang.LanguageExtensionPoint
 import com.intellij.lang.annotation.Annotator
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.impl.NonBlockingReadActionImpl
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.fileEditor.FileDocumentManager
@@ -66,6 +67,7 @@ fun enableHints(enable: Boolean) =
 fun dispatchAllInvocationEvents() {
     runInEdtAndWait {
         UIUtil.dispatchAllInvocationEvents()
+        NonBlockingReadActionImpl.waitForAsyncTaskCompletion();
     }
 }
 
