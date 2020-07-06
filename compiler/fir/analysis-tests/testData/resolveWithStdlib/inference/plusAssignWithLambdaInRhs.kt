a3a79e429575ccde76b9d0ffaa13f75674ff6ee7
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-07-06 16:29:07 +0300

    [FIR] Update testdata due to incorrect SAM detection

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/inference/plusAssignWithLambdaInRhs.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/inference/plusAssignWithLambdaInRhs.kt
@@ -3,17 +3,17 @@
 
 fun test() {
     val list: MutableList<(String) -> String> = null!!
-    list += { it }
+    <!AMBIGUITY!>list += { <!UNRESOLVED_REFERENCE!>it<!> }<!>
 }
 
-//class A<T>(private val executor: ((T) -> Unit) -> Unit)
-//
-//fun <T> postpone(computation: () -> T): A<T> {
-//    val queue = mutableListOf<() -> Unit>()
-//
-//    return A { resolve ->
-//        queue += {
-//            resolve(computation())
-//        }
-//    }
-//}
+class A<T>(private val executor: ((T) -> Unit) -> Unit)
+
+fun <T> postpone(computation: () -> T): A<T> {
+    val queue = mutableListOf<() -> Unit>()
+
+    return A { resolve ->
+        <!AMBIGUITY!>queue += {
+            resolve(computation())
+        }<!>
+    }
+}
