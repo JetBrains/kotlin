d62500531b5fda8148cd4ca5fa3eb4ed97fe01f5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-07-07 14:37:36 +0300

    FIR: add getClassifierNames for FirScope

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirClassDeclaredMemberScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirClassDeclaredMemberScope.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -79,6 +79,10 @@ class FirClassDeclaredMemberScope(
 
     override fun getCallableNames(): Set<Name> {
         return callablesIndex.keys
+    }
+
+    override fun getClassifierNames(): Set<Name> {
+        return nestedClassifierScope?.getClassifierNames().orEmpty()
     }
 }
 
--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirNestedClassifierScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/impl/FirNestedClassifierScope.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -43,6 +43,10 @@ class FirNestedClassifierScope(val klass: FirClass<*>) : FirScope() {
     }
 
     fun getClassifierByName(name: Name): FirRegularClassSymbol? = classIndex[name]
+
+    override fun getClassifierNames(): Set<Name> {
+        return classIndex.keys
+    }
 }
 
 fun FirTypeParameterRef.toConeType(): ConeKotlinType = ConeTypeParameterTypeImpl(ConeTypeParameterLookupTag(symbol), isNullable = false)
