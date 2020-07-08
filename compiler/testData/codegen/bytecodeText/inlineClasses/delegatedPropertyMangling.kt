88dfaf6f9da01091f7b2fd1b511600e29da118ba
Steven Schäfer <stevenschaefer@google.com>
2020-07-08 18:32:53 +0200

    JVM IR: Mangle delegated properties with inline class type (KT-40125)

---
+++ compiler/testData/codegen/bytecodeText/inlineClasses/delegatedPropertyMangling.kt
@@ -0,0 +1,22 @@
+// LANGUAGE: +InlineClasses
+import kotlin.reflect.KProperty
+
+inline class I(val x: Int)
+
+interface A {
+    val i: I
+}
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): I {
+        return I(1)
+    }
+}
+
+class B : A {
+    override val i by Delegate()
+}
+
+// 1 public final getValue-lPtA-2M\(Ljava/lang/Object;Lkotlin/reflect/KProperty;\)I
+// 1 public getI-lPtA-2M\(\)I
+// 1 public abstract getI-lPtA-2M\(\)I
