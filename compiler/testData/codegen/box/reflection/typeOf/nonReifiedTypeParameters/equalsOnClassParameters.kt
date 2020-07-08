47a1822123c77cf5f50f25ae0c710cae1eb7bfdb
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-07-08 16:20:23 +0300

    [KJS FE] Allow using typeOf with non-reified type parameters
    #KT-38771 fixed
    
    (cherry picked from commit ca37c6bfe65022673d0ee1b22c77628ecae1995f)

--- compiler/testData/codegen/box/reflection/typeOf/nonReifiedTypeParameters/equalsOnClassParameters.kt
+++ compiler/testData/codegen/box/reflection/typeOf/nonReifiedTypeParameters/equalsOnClassParameters.kt
@@ -1,7 +1,7 @@
 // !USE_EXPERIMENTAL: kotlin.ExperimentalStdlibApi
-// IGNORE_BACKEND: JS, JS_IR, NATIVE
-// IGNORE_BACKEND: JS_IR_ES6
+// IGNORE_BACKEND: NATIVE
 // WITH_REFLECT
+// KJS_WITH_FULL_RUNTIME
 
 package test
 
@@ -36,7 +36,12 @@ fun box(): String {
     assertEquals(c.x1, c.x2)
     assertEquals(c.x1.hashCode(), c.x2.hashCode())
 
-    assertNotEquals(c.x1, c.xFun)
+
+    if (!isJS) {
+        assertNotEquals(c.x1, c.xFun)
+    }
     assertNotEquals(c.x1, c.y)
     return "OK"
 }
+
+val isJS = 1 as Any is Double
