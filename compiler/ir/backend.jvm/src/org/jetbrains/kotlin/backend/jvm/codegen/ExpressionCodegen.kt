mano7onam/kotlin-1

JVM_IR: ClassCodegen.getOrCreate -> context.getClassCodegen

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
@@ -701,9 +701,9 @@ class ExpressionCodegen(
     override fun visitClass(declaration: IrClass, data: BlockInfo): PromisedValue {
         if (declaration.origin != JvmLoweredDeclarationOrigin.CONTINUATION_CLASS) {
             closureReifiedMarkers.getOrPut(declaration) {
-                val inner = ClassCodegen.getOrCreate(declaration, context, generateSequence(this) { it.inlinedInto }.last().irFunction)
-                inner.generate()
-                inner.reifiedTypeParametersUsages
+                val innerClass = context.getClassCodegen(declaration, generateSequence(this) { it.inlinedInto }.last().irFunction)
+                innerClass.generate()
+                innerClass.reifiedTypeParametersUsages
             }
         }
         return unitValue

JetBrains/kotlin

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
@@ -725,7 +725,7 @@ class ExpressionCodegen(
     override fun visitClass(declaration: IrClass, data: BlockInfo): PromisedValue {
         if (declaration.origin != JvmLoweredDeclarationOrigin.CONTINUATION_CLASS) {
             closureReifiedMarkers.getOrPut(declaration) {
-                val codegen = ClassCodegen.getOrCreate(declaration, context, generateSequence(this) { it.inlinedInto }.last().irFunction)
+                val codegen = context.getClassCodegen(declaration, generateSequence(this) { it.inlinedInto }.last().irFunction)
                 codegen.generate()
                 codegen.reifiedTypeParametersUsages
             }