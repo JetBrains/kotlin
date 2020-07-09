549366e2eb468a345752d9bd7e59628486453f2c
Egor Zhdan <egor.zhdan@jetbrains.com>
2020-07-09 14:18:28 +0300

    MI-30 Set up iOS XCTest target properly

--- ide/mobile-native/src/com/jetbrains/mobile/execution/testing/AppleXCTestFramework.kt
+++ ide/mobile-native/src/com/jetbrains/mobile/execution/testing/AppleXCTestFramework.kt
@@ -4,10 +4,12 @@ import com.intellij.psi.PsiFile
 import com.jetbrains.cidr.execution.CidrBuildConfiguration
 import com.jetbrains.cidr.execution.CidrBuildTarget
 import com.jetbrains.cidr.lang.OCTestFramework
+import com.jetbrains.mobile.execution.MobileBuildConfigurationHelper
 import com.jetbrains.swift.execution.testing.SwiftUnitTestFrameworkBase
 
 class AppleXCTestFramework : SwiftUnitTestFrameworkBase() {
-    override fun getTargetsForFile(file: PsiFile): Set<CidrBuildTarget<out CidrBuildConfiguration>> = emptySet()
+    override fun getTargetsForFile(file: PsiFile): Set<CidrBuildTarget<out CidrBuildConfiguration>> =
+        MobileBuildConfigurationHelper(file.project).targets.toSet()
 
     companion object {
         val instance: AppleXCTestFramework
