4476cc2bd76f3323272b30297a4bb7a075fc2e52
Alex Saveau <asaveau@google.com>
2020-07-09 07:48:35 +0000

    [KDM] Improve JSON parsing for CLI
    Original commit: https://github.com/Kotlin/dokka/commit/0b35364e7bbe7c7365622f316a8c89ad204f74ce

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/defaultConfiguration.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/defaultConfiguration.kt
@@ -1,57 +1,62 @@
 package org.jetbrains.dokka
 
+import kotlinx.serialization.*
 import org.jetbrains.dokka.DokkaConfiguration.DokkaSourceSet
 import java.io.File
 import java.net.URL
-import java.io.Serializable
 
+@Serializable
 data class DokkaConfigurationImpl(
-    override val outputDir: String,
-    override val cacheRoot: String?,
-    override val offlineMode: Boolean,
-    override val sourceSets: List<DokkaSourceSetImpl>,
-    override val pluginsClasspath: List<File>,
-    override val pluginsConfiguration: Map<String, String>,
-    override val modules: List<DokkaModuleDescriptionImpl>,
-    override val failOnWarning: Boolean
+    override val outputDir: String = DokkaDefaults.outputDir,
+    override val cacheRoot: String? = DokkaDefaults.cacheRoot,
+    override val offlineMode: Boolean = DokkaDefaults.offlineMode,
+    override val sourceSets: List<DokkaSourceSetImpl> = emptyList(),
+    override val pluginsClasspath: List<@Serializable(with = FileSerializer::class) File> = emptyList(),
+    override val pluginsConfiguration: Map<String, String> = emptyMap(),
+    override val modules: List<DokkaModuleDescriptionImpl> = emptyList(),
+    override val failOnWarning: Boolean = DokkaDefaults.failOnWarning
 ) : DokkaConfiguration
 
+@Serializable
 data class DokkaSourceSetImpl(
     override val moduleDisplayName: String,
-    override val displayName: String,
+    override val displayName: String = DokkaDefaults.displayName,
     override val sourceSetID: DokkaSourceSetID,
-    override val classpath: List<String>,
+    override val classpath: List<String> = emptyList(),
     override val sourceRoots: List<SourceRootImpl>,
-    override val dependentSourceSets: Set<DokkaSourceSetID>,
-    override val samples: List<String>,
-    override val includes: List<String>,
-    override val includeNonPublic: Boolean,
-    override val includeRootPackage: Boolean,
-    override val reportUndocumented: Boolean,
-    override val skipEmptyPackages: Boolean,
-    override val skipDeprecated: Boolean,
-    override val jdkVersion: Int,
-    override val sourceLinks: List<SourceLinkDefinitionImpl>,
-    override val perPackageOptions: List<PackageOptionsImpl>,
-    override var externalDocumentationLinks: List<ExternalDocumentationLinkImpl>,
-    override val languageVersion: String?,
-    override val apiVersion: String?,
-    override val noStdlibLink: Boolean,
-    override val noJdkLink: Boolean,
-    override val suppressedFiles: List<String>,
-    override val analysisPlatform: Platform
+    override val dependentSourceSets: Set<DokkaSourceSetID> = emptySet(),
+    override val samples: List<String> = emptyList(),
+    override val includes: List<String> = emptyList(),
+    override val includeNonPublic: Boolean = DokkaDefaults.includeNonPublic,
+    override val includeRootPackage: Boolean = DokkaDefaults.includeRootPackage,
+    override val reportUndocumented: Boolean = DokkaDefaults.reportUndocumented,
+    override val skipEmptyPackages: Boolean = DokkaDefaults.skipEmptyPackages,
+    override val skipDeprecated: Boolean = DokkaDefaults.skipDeprecated,
+    override val jdkVersion: Int = DokkaDefaults.jdkVersion,
+    override val sourceLinks: List<SourceLinkDefinitionImpl> = emptyList(),
+    override val perPackageOptions: List<PackageOptionsImpl> = emptyList(),
+    override var externalDocumentationLinks: List<ExternalDocumentationLinkImpl> = emptyList(),
+    override val languageVersion: String? = null,
+    override val apiVersion: String? = null,
+    override val noStdlibLink: Boolean = DokkaDefaults.noStdlibLink,
+    override val noJdkLink: Boolean = DokkaDefaults.noJdkLink,
+    override val suppressedFiles: List<String> = emptyList(),
+    override val analysisPlatform: Platform = DokkaDefaults.analysisPlatform
 ) : DokkaSourceSet
 
+@Serializable
 data class DokkaModuleDescriptionImpl(
     override val name: String,
     override val path: String,
     override val docFile: String
 ) : DokkaConfiguration.DokkaModuleDescription
 
+@Serializable
 data class SourceRootImpl(
     override val path: String
 ) : DokkaConfiguration.SourceRoot
 
+@Serializable
 data class SourceLinkDefinitionImpl(
     override val path: String,
     override val url: String,
@@ -68,6 +73,7 @@ data class SourceLinkDefinitionImpl(
     }
 }
 
+@Serializable
 data class PackageOptionsImpl(
     override val prefix: String,
     override val includeNonPublic: Boolean,
@@ -76,8 +82,28 @@ data class PackageOptionsImpl(
     override val suppress: Boolean
 ) : DokkaConfiguration.PackageOptions
 
-
+@Serializable
 data class ExternalDocumentationLinkImpl(
-    override val url: URL,
-    override val packageListUrl: URL
-) : DokkaConfiguration.ExternalDocumentationLink, Serializable
+    @Serializable(with = URLSerializer::class) override val url: URL,
+    @Serializable(with = URLSerializer::class) override val packageListUrl: URL
+) : DokkaConfiguration.ExternalDocumentationLink
+
+private object FileSerializer : KSerializer<File> {
+    override val descriptor: SerialDescriptor = PrimitiveDescriptor("File", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: File) {
+        encoder.encodeString(value.path)
+    }
+
+    override fun deserialize(decoder: Decoder): File = File(decoder.decodeString())
+}
+
+private object URLSerializer : KSerializer<URL> {
+    override val descriptor: SerialDescriptor = PrimitiveDescriptor("URL", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: URL) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): URL = URL(decoder.decodeString())
+}
