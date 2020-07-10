796d688a8f6ff042be9fd10f467c13c8c35425f6
Dmitrii Naumenko <Dmitrii.Naumenko@jetbrains.com>
2020-07-10 18:02:02 +0300

    IDEA-CR-64905: ToggleRenderAllDocs: remove unused import #IDEA-245688
    (cherry picked from commit af6afa11321758ee411b13e7e156fdc91e0fc96c)
    
    intellij-monorepo: 029ab18d491e1f971176422f98a18ca1f50f29b0

--- intellij-community/platform/lang-impl/src/com/intellij/codeInsight/documentation/render/ToggleRenderAllDocs.java
+++ intellij-community/platform/lang-impl/src/com/intellij/codeInsight/documentation/render/ToggleRenderAllDocs.java
@@ -1,7 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.codeInsight.documentation.render;
 
-import com.intellij.idea.ActionsBundle;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
@@ -9,7 +8,6 @@ import com.intellij.openapi.project.DumbAware;
 import org.jetbrains.annotations.NotNull;
 
 public class ToggleRenderAllDocs extends ToggleAction implements DumbAware {
-
   @Override
   public boolean isSelected(@NotNull AnActionEvent e) {
     return EditorSettingsExternalizable.getInstance().isDocCommentRenderingEnabled();
