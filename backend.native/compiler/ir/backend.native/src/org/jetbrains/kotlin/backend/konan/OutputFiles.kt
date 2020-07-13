jparachoniak/kotlin-native

[caches] Make output for cache a directory

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/OutputFiles.kt
@@ -27,33 +27,41 @@ class OutputFiles(outputPath: String?, target: KonanTarget, val produce: Compile
     /**
      * Header file for dynamic library.
      */
-    val cAdapterHeader      by lazy { File("${outputName}_api.h") }
-    val cAdapterDef         by lazy { File("${outputName}.def") }
+    val cAdapterHeader by lazy { File("${outputName}_api.h") }
+    val cAdapterDef    by lazy { File("${outputName}.def") }
 
     /**
      * Main compiler's output file.
      */
-    val mainFile = outputName
-            .prefixBaseNameIfNeeded(prefix)
-            .suffixIfNeeded(suffix)
+    val mainFile =
+            if (produce.isCache)
+                outputName
+            else
+                outputName.fullOutputName()
 
-    val mainFileMangled = if (!produce.isCache) mainFile else {
-        (outputName + Random.nextLong().toString())
-                .prefixBaseNameIfNeeded(prefix)
-                .suffixIfNeeded(suffix)
-    }
+    val tempCacheDirectory =
+            if (produce.isCache)
+                File(outputName + Random.nextLong().toString())
+            else null
 
-    private fun String.prefixBaseNameIfNeeded(prefix: String): String {
-        return if (produce.isCache)
-            prefixBaseNameAlways(prefix)
-        else prefixBaseNameIfNot(prefix)
-    }
+    val compilationResultFile =
+            if (produce.isCache)
+                tempCacheDirectory!!.child(File(outputName.fullOutputName()).absoluteFile.name).absolutePath
+            else mainFile
 
-    private fun String.suffixIfNeeded(prefix: String): String {
-        return if (produce.isCache)
-            suffixAlways(prefix)
-        else suffixIfNot(prefix)
-    }
+    val symbolicInfoFile = "$compilationResultFile.dSYM"
+
+    private fun String.fullOutputName() = prefixBaseNameIfNeeded(prefix).suffixIfNeeded(suffix)
+
+    private fun String.prefixBaseNameIfNeeded(prefix: String) =
+            if (produce.isCache)
+                prefixBaseNameAlways(prefix)
+            else prefixBaseNameIfNot(prefix)
+
+    private fun String.suffixIfNeeded(prefix: String) =
+            if (produce.isCache)
+                suffixAlways(prefix)
+            else suffixIfNot(prefix)
 
     private fun String.prefixBaseNameAlways(prefix: String): String {
         val file = File(this).absoluteFile

JetBrains/kotlin

(cherry picked from commit 9850bb322bee2bc98fc453a440e41af0dac0b0f2)

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/OutputFiles.kt
@@ -27,33 +27,41 @@ class OutputFiles(outputPath: String?, target: KonanTarget, val produce: Compile
     /**
      * Header file for dynamic library.
      */
-    val cAdapterHeader      by lazy { File("${outputName}_api.h") }
-    val cAdapterDef         by lazy { File("${outputName}.def") }
+    val cAdapterHeader by lazy { File("${outputName}_api.h") }
+    val cAdapterDef    by lazy { File("${outputName}.def") }
 
     /**
      * Main compiler's output file.
      */
-    val mainFile = outputName
-            .prefixBaseNameIfNeeded(prefix)
-            .suffixIfNeeded(suffix)
+    val mainFile =
+            if (produce.isCache)
+                outputName
+            else
+                outputName.fullOutputName()
 
-    val mainFileMangled = if (!produce.isCache) mainFile else {
-        (outputName + Random.nextLong().toString())
-                .prefixBaseNameIfNeeded(prefix)
-                .suffixIfNeeded(suffix)
-    }
+    val tempCacheDirectory =
+            if (produce.isCache)
+                File(outputName + Random.nextLong().toString())
+            else null
 
-    private fun String.prefixBaseNameIfNeeded(prefix: String): String {
-        return if (produce.isCache)
-            prefixBaseNameAlways(prefix)
-        else prefixBaseNameIfNot(prefix)
-    }
+    val nativeBinaryFile =
+            if (produce.isCache)
+                tempCacheDirectory!!.child(File(outputName.fullOutputName()).absoluteFile.name).absolutePath
+            else mainFile
 
-    private fun String.suffixIfNeeded(prefix: String): String {
-        return if (produce.isCache)
-            suffixAlways(prefix)
-        else suffixIfNot(prefix)
-    }
+    val symbolicInfoFile = "$nativeBinaryFile.dSYM"
+
+    private fun String.fullOutputName() = prefixBaseNameIfNeeded(prefix).suffixIfNeeded(suffix)
+
+    private fun String.prefixBaseNameIfNeeded(prefix: String) =
+            if (produce.isCache)
+                prefixBaseNameAlways(prefix)
+            else prefixBaseNameIfNot(prefix)
+
+    private fun String.suffixIfNeeded(prefix: String) =
+            if (produce.isCache)
+                suffixAlways(prefix)
+            else suffixIfNot(prefix)
 
     private fun String.prefixBaseNameAlways(prefix: String): String {
         val file = File(this).absoluteFile