fcf7a55ccc44fae420979942c272585dc44bd431
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-07-14 12:58:27 +0300

    Fix delegated property resolve on number literals and proper types
     There is no need to update type of delegate expression if it's already
     resolved correctly (doesn't include non-proper types). In almost all
     cases it was fine except number literals as there we didn't box
     expression in backend and got problems at bytecode verification stage
    
     #KT-40057 Fixed

--- compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -10836,6 +10836,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
                 runTest("compiler/testData/codegen/box/delegatedProperty/provideDelegate/genericDelegateWithNoAdditionalInfo.kt");
             }
 
+            @TestMetadata("genericProvideDelegateOnNumberLiteral.kt")
+            public void testGenericProvideDelegateOnNumberLiteral() throws Exception {
+                runTest("compiler/testData/codegen/box/delegatedProperty/provideDelegate/genericProvideDelegateOnNumberLiteral.kt");
+            }
+
             @TestMetadata("hostCheck.kt")
             public void testHostCheck() throws Exception {
                 runTest("compiler/testData/codegen/box/delegatedProperty/provideDelegate/hostCheck.kt");
