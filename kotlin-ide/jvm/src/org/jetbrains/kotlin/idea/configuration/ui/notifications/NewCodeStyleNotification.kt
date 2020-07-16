794869a630ec02a40843ea0590d2a1ae940a9349
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-07-16 21:38:51 +0700

    Rewrite NewCodeStyleNotification to suggest apply official code style
    #KT-40388 Fixed

--- kotlin-ide/jvm/src/org/jetbrains/kotlin/idea/configuration/ui/notifications/NewCodeStyleNotification.kt
+++ kotlin-ide/jvm/src/org/jetbrains/kotlin/idea/configuration/ui/notifications/NewCodeStyleNotification.kt
@@ -12,12 +12,11 @@ import com.intellij.notification.*
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.KotlinJvmBundle
 import org.jetbrains.kotlin.idea.facet.KotlinFacetType
-import org.jetbrains.kotlin.idea.formatter.*
-import org.jetbrains.kotlin.idea.util.isDefaultIntellijOrObsoleteCodeStyle
+import org.jetbrains.kotlin.idea.formatter.KotlinStyleGuideCodeStyle
+import org.jetbrains.kotlin.idea.formatter.kotlinCodeStyleDefaults
 
 private const val KOTLIN_UPDATE_CODE_STYLE_GROUP_ID = "Update Kotlin code style"
 private const val KOTLIN_UPDATE_CODE_STYLE_PROPERTY_NAME = "update.kotlin.code.style.notified"
@@ -25,25 +24,20 @@ private const val KOTLIN_UPDATE_CODE_STYLE_PROPERTY_NAME = "update.kotlin.code.s
 fun notifyKotlinStyleUpdateIfNeeded(project: Project) {
     val modulesWithFacet = ProjectFacetManager.getInstance(project).getModulesWithFacet(KotlinFacetType.TYPE_ID)
     if (modulesWithFacet.isEmpty()) return
-    val codeStyle = CodeStyle.getSettings(project)
-    if (!codeStyle.kotlinCommonSettings.isDefaultIntellijOrObsoleteCodeStyle) return
 
-    val isProjectSettings = CodeStyle.usesOwnSettings(project)
-    val settingsComponent: PropertiesComponent = if (isProjectSettings) {
-        PropertiesComponent.getInstance(project)
-    } else {
-        PropertiesComponent.getInstance()
-    }
+    if (CodeStyle.getSettings(project).kotlinCodeStyleDefaults() == KotlinStyleGuideCodeStyle.CODE_STYLE_ID) return
 
+    val settingsComponent = PropertiesComponent.getInstance(project)
     if (settingsComponent.getBoolean(KOTLIN_UPDATE_CODE_STYLE_PROPERTY_NAME, false)) {
         return
     }
 
-    val notification = KotlinCodeStyleChangedNotification.create(project, isProjectSettings) ?: return
-    notification.isImportant = true
-
-    NotificationsConfiguration.getNotificationsConfiguration()
-        .register(KOTLIN_UPDATE_CODE_STYLE_GROUP_ID, NotificationDisplayType.STICKY_BALLOON, true)
+    val notification = createNotification()
+    NotificationsConfiguration.getNotificationsConfiguration().register(
+        KOTLIN_UPDATE_CODE_STYLE_GROUP_ID,
+        NotificationDisplayType.STICKY_BALLOON,
+        true
+    )
 
     if (ApplicationManager.getApplication().isUnitTestMode) {
         return
@@ -54,56 +48,24 @@ fun notifyKotlinStyleUpdateIfNeeded(project: Project) {
     notification.notify(project)
 }
 
-private class KotlinCodeStyleChangedNotification(val project: Project, isProjectSettings: Boolean) : Notification(
+private fun createNotification(): Notification = Notification(
     KOTLIN_UPDATE_CODE_STYLE_GROUP_ID,
     KotlinJvmBundle.message("kotlin.code.style"),
-    KotlinJvmBundle.htmlMessage("default.code.style.was.updated.to.kotlin.coding.conventions"),
-    NotificationType.WARNING,
-    null
-) {
-    init {
-        val ktFormattingSettings = ktCodeStyleSettings(project)
+    KotlinJvmBundle.htmlMessage("notification.update.code.style.to.official"),
+    NotificationType.WARNING
+).apply {
+    val notificationAction = NotificationAction.create(
+        KotlinJvmBundle.message("apply.new.code.style")
+    ) { e: AnActionEvent, notification: Notification ->
+        notification.expire()
 
-        if (isProjectSettings) {
-            addAction(object : NotificationAction(KotlinJvmBundle.message("apply.new.code.style")) {
-                override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-                    notification.expire()
-
-                    val ktSettings = ktCodeStyleSettings(project) ?: return
-
-                    runWriteAction {
-                        KotlinStyleGuideCodeStyle.apply(ktSettings.all)
-                    }
-                }
-            })
-        }
-
-        if (ktFormattingSettings != null && ktFormattingSettings.canRestore()) {
-            addAction(object : NotificationAction(KotlinJvmBundle.message("restore.old.settings")) {
-                override fun actionPerformed(e: AnActionEvent, notification: Notification) {
-                    notification.expire()
-
-                    val ktSettings = ktCodeStyleSettings(project) ?: return
-
-                    runWriteAction {
-                        ktSettings.restore()
-                    }
-                }
-            })
-        }
-    }
-
-    companion object {
-        val LOG = Logger.getInstance("KotlinCodeStyleChangedNotification")
-
-        fun create(project: Project, isProjectSettings: Boolean): KotlinCodeStyleChangedNotification? {
-            val ktFormattingSettings = ktCodeStyleSettings(project) ?: return null
-
-            if (isProjectSettings && !ktFormattingSettings.hasDefaultLoadScheme()) {
-                return null
+        e.project?.takeIf { !it.isDisposed }?.let { project ->
+            runWriteAction {
+                KotlinStyleGuideCodeStyle.apply(CodeStyle.getSettings(project))
             }
-
-            return KotlinCodeStyleChangedNotification(project, isProjectSettings)
         }
     }
+
+    addAction(notificationAction)
+    isImportant = true
 }
