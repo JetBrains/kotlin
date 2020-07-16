f35933bedf1cf83e5159bed35d21aab4727d8257
Nikita Skvortsov <nikita.skvortsov@jetbrains.com>
2020-07-16 16:00:34 +0300

    [external system] add keywords for projects auto-reload configuration Code review: IDEA-CR-2309 (cherry picked from commit dff82e8d4dd17fa85edfab1d88d08d61e33cd638)
    intellij-monorepo: e94c4be2ab3ba34a42333e1762ea87fbe0e5598d

---
+++ intellij-community/platform/external-system-impl/src/com/intellij/openapi/externalSystem/service/settings/ExternalSystemGroupSearchableContributor.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.openapi.externalSystem.service.settings
+
+import com.intellij.ide.ui.search.SearchableOptionContributor
+import com.intellij.ide.ui.search.SearchableOptionProcessor
+import com.intellij.openapi.externalSystem.util.ExternalSystemBundle.message
+
+class ExternalSystemGroupSearchableContributor: SearchableOptionContributor() {
+
+  private val buildToolsId = "build.tools"
+  private val buildToolsName = "Build Tools"
+
+  override fun processOptions(processor: SearchableOptionProcessor) {
+    processor.addOptions(
+      "reload autoreload auto-reload import autoimport auto-import",
+      null,
+      message("settings.build.tools.auto.reload.hit"),
+      buildToolsId,
+      buildToolsName,
+      false)
+  }
+
+}
