4dd4601449b7bfe4af90faf4cde5c4378dc79e3e
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-07-16 21:53:25 +0700

    [FUS] Update KotlinFormatterUsageCollector

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/formatter/KotlinFormatterUsageCollector.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/formatter/KotlinFormatterUsageCollector.kt
@@ -11,138 +11,78 @@ import com.intellij.internal.statistic.beans.newMetric
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
 import com.intellij.openapi.project.Project
+import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CodeStyleSettingsManager
 import org.jetbrains.kotlin.idea.formatter.KotlinFormatterUsageCollector.KotlinFormatterKind.*
-import org.jetbrains.kotlin.idea.util.isDefaultIntellijOrObsoleteCodeStyle
 
 class KotlinFormatterUsageCollector : ProjectUsagesCollector() {
-
     override fun getGroupId() = "kotlin.ide.formatter"
-    override fun getVersion(): Int = 1
+    override fun getVersion(): Int = 2
 
     override fun getMetrics(project: Project): Set<MetricEvent> {
         val usedFormatter = getKotlinFormatterKind(project)
 
-        val data = FeatureUsageData()
-            .addData("kind", usedFormatter.name)
-            .addData("defaults", getDefaultCodeStyle(project))
+        val data = FeatureUsageData().addData("kind", usedFormatter.name)
 
-        return setOf(
-            newMetric("settings", data)
-        )
+        return setOf(newMetric("settings", data))
     }
-
-    private fun getDefaultCodeStyle(project: Project): String {
-
-        val settings = CodeStyle.getSettings(project)
-        val kotlinCommonSettings = settings.kotlinCommonSettings
-        val kotlinCustomSettings = settings.kotlinCustomSettings
-
-        val defaults = kotlinCustomSettings.CODE_STYLE_DEFAULTS ?: kotlinCommonSettings.CODE_STYLE_DEFAULTS
-
-        return defaults ?: "ide_defaults"
-    }
-
 
     companion object {
-
-        private val KOTLIN_DEFAULT_COMMON = CodeStyle.getDefaultSettings().kotlinCommonSettings.also {
-            KotlinStyleGuideCodeStyle.applyToCommonSettings(it)
+        private val KOTLIN_OFFICIAL_CODE_STYLE: CodeStyleSettings by lazy {
+            CodeStyleSettingsManager.getInstance().cloneSettings(CodeStyle.getDefaultSettings()).also(KotlinStyleGuideCodeStyle::apply)
         }
 
-        private val KOTLIN_DEFAULT_CUSTOM by lazy {
-            CodeStyle.getDefaultSettings().kotlinCustomSettings.cloneSettings().also {
-                KotlinStyleGuideCodeStyle.applyToKotlinCustomSettings(it)
-            }
+        private val KOTLIN_OBSOLETE_CODE_STYLE: CodeStyleSettings by lazy {
+            CodeStyleSettingsManager.getInstance().cloneSettings(CodeStyle.getDefaultSettings()).also(KotlinObsoleteCodeStyle::apply)
         }
 
-        private val KOTLIN_OBSOLETE_DEFAULT_COMMON = KotlinLanguageCodeStyleSettingsProvider().defaultCommonSettings.also {
-            KotlinObsoleteCodeStyle.applyToCommonSettings(it)
-        }
-
-        private val KOTLIN_OBSOLETE_DEFAULT_CUSTOM by lazy {
-            CodeStyle.getDefaultSettings().kotlinCustomSettings.cloneSettings().also {
-                KotlinObsoleteCodeStyle.applyToKotlinCustomSettings(it)
-            }
-        }
+        private fun codeStylesIsEquals(lhs: CodeStyleSettings, rhs: CodeStyleSettings): Boolean =
+            lhs.kotlinCustomSettings == rhs.kotlinCustomSettings && lhs.kotlinCommonSettings == rhs.kotlinCommonSettings
 
         fun getKotlinFormatterKind(project: Project): KotlinFormatterKind {
             val isProject = CodeStyle.usesOwnSettings(project)
+            val currentSettings = CodeStyle.getSettings(project)
 
-            val settings = CodeStyle.getSettings(project)
-            val kotlinCommonSettings = settings.kotlinCommonSettings
-            val kotlinCustomSettings = settings.kotlinCustomSettings
-            val isDefaultIntellijOrObsoleteCodeStyle = kotlinCommonSettings.isDefaultIntellijOrObsoleteCodeStyle && kotlinCustomSettings.isDefaultIntellijOrObsoleteCodeStyle
-
-            val isDefaultKotlinCommonSettings = kotlinCommonSettings == CodeStyle.getDefaultSettings().kotlinCommonSettings
-            val isDefaultKotlinCustomSettings = kotlinCustomSettings == CodeStyle.getDefaultSettings().kotlinCustomSettings
-
-            if (isDefaultKotlinCommonSettings && isDefaultKotlinCustomSettings) {
-                return if (isDefaultIntellijOrObsoleteCodeStyle) {
-                    paired(IDEA_OFFICIAL_DEFAULT, isProject)
-                } else {
-                    paired(IDEA_DEFAULT, isProject)
+            return when (currentSettings.kotlinCodeStyleDefaults()) {
+                KotlinStyleGuideCodeStyle.CODE_STYLE_ID -> {
+                    if (codeStylesIsEquals(currentSettings, KOTLIN_OFFICIAL_CODE_STYLE))
+                        paired(IDEA_OFFICIAL_KOTLIN, isProject)
+                    else
+                        paired(IDEA_OFFICIAL_KOTLIN_WITH_CUSTOM, isProject)
                 }
-            }
 
-            if (kotlinCommonSettings == KOTLIN_OBSOLETE_DEFAULT_COMMON && kotlinCustomSettings == KOTLIN_OBSOLETE_DEFAULT_CUSTOM) {
-                return paired(IDEA_OBSOLETE_KOTLIN, isProject)
-            }
+                KotlinObsoleteCodeStyle.CODE_STYLE_ID -> {
+                    if (codeStylesIsEquals(currentSettings, KOTLIN_OBSOLETE_CODE_STYLE))
+                        paired(IDEA_OBSOLETE_KOTLIN, isProject)
+                    else
+                        paired(IDEA_OBSOLETE_KOTLIN_WITH_CUSTOM, isProject)
+                }
 
-            if (kotlinCommonSettings == KOTLIN_DEFAULT_COMMON && kotlinCustomSettings == KOTLIN_DEFAULT_CUSTOM) {
-                return paired(IDEA_KOTLIN, isProject)
+                else -> paired(IDEA_CUSTOM, isProject)
             }
-
-            val codeStyleSettingsManager = CodeStyleSettingsManager.getInstance(project)
-            val isKotlinOfficialLikeSettings = settings == codeStyleSettingsManager.cloneSettings(settings).also {
-                KotlinStyleGuideCodeStyle.apply(it)
-            }
-            if (isKotlinOfficialLikeSettings) {
-                return paired(IDEA_OFFICIAL_KOTLIN_WITH_CUSTOM, isProject)
-            }
-
-            val isKotlinObsoleteLikeSettings = settings == codeStyleSettingsManager.cloneSettings(settings).also {
-                KotlinObsoleteCodeStyle.apply(it)
-            }
-            if (isKotlinObsoleteLikeSettings) {
-                return paired(IDEA_KOTLIN_WITH_CUSTOM, isProject)
-            }
-
-            return paired(IDEA_CUSTOM, isProject)
         }
 
         private fun paired(kind: KotlinFormatterKind, isProject: Boolean): KotlinFormatterKind {
             if (!isProject) return kind
 
             return when (kind) {
-                IDEA_DEFAULT -> PROJECT_DEFAULT
-                IDEA_OFFICIAL_DEFAULT -> PROJECT_OFFICIAL_DEFAULT
                 IDEA_CUSTOM -> PROJECT_CUSTOM
-                IDEA_KOTLIN_WITH_CUSTOM -> PROJECT_KOTLIN_WITH_CUSTOM
-                IDEA_KOTLIN -> PROJECT_KOTLIN
+                IDEA_OFFICIAL_KOTLIN -> PROJECT_OFFICIAL_KOTLIN
+                IDEA_OFFICIAL_KOTLIN_WITH_CUSTOM -> PROJECT_OFFICIAL_KOTLIN_WITH_CUSTOM
                 IDEA_OBSOLETE_KOTLIN -> PROJECT_OBSOLETE_KOTLIN
-                IDEA_OFFICIAL_KOTLIN_WITH_CUSTOM -> PROJECT_OBSOLETE_KOTLIN_WITH_CUSTOM
+                IDEA_OBSOLETE_KOTLIN_WITH_CUSTOM -> PROJECT_OBSOLETE_KOTLIN_WITH_CUSTOM
                 else -> kind
             }
         }
     }
 
     enum class KotlinFormatterKind {
-        IDEA_DEFAULT,
-        IDEA_CUSTOM,
-        IDEA_KOTLIN_WITH_CUSTOM,
-        IDEA_KOTLIN,
+        IDEA_CUSTOM, PROJECT_CUSTOM,
 
-        PROJECT_DEFAULT,
-        PROJECT_CUSTOM,
-        PROJECT_KOTLIN_WITH_CUSTOM,
-        PROJECT_KOTLIN,
+        IDEA_OFFICIAL_KOTLIN, PROJECT_OFFICIAL_KOTLIN,
+        IDEA_OFFICIAL_KOTLIN_WITH_CUSTOM, PROJECT_OFFICIAL_KOTLIN_WITH_CUSTOM,
 
-        IDEA_OFFICIAL_DEFAULT,
-        IDEA_OBSOLETE_KOTLIN,
-        IDEA_OFFICIAL_KOTLIN_WITH_CUSTOM,
-        PROJECT_OFFICIAL_DEFAULT,
-        PROJECT_OBSOLETE_KOTLIN,
-        PROJECT_OBSOLETE_KOTLIN_WITH_CUSTOM
+        IDEA_OBSOLETE_KOTLIN, PROJECT_OBSOLETE_KOTLIN,
+        IDEA_OBSOLETE_KOTLIN_WITH_CUSTOM, PROJECT_OBSOLETE_KOTLIN_WITH_CUSTOM,
     }
 }
