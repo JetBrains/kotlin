df5f38fec878018b012408d0e510482ff5e57d8c
Leonid Startsev <leonid.startsev@jetbrains.com>
2020-07-16 22:10:14 +0300

    Implement reading kx.serialization runtime metadata from jar manifest
    That allows to check whether current compiler plugin can work with
    a given runtime version. Implementation-Version can help to detect whether
    runtime version is too low, and Require-Kotlin-Version detects whether
    runtime version is too high.
    
    Corresponding diagnostic for Require-Kotlin included.
    Implement minimal runtime version check for kotlinx.serialization
    plugin.
    
     #KT-40036 Fixed
    
    Cache result from getVersionsForCurrentModule in BindingTrace.
    Hide check in the IDE, because caching in trace does not work there.

--- plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/diagnostic/SerializationErrors.java
+++ plugins/kotlin-serialization/kotlin-serialization-compiler/src/org/jetbrains/kotlinx/serialization/compiler/diagnostic/SerializationErrors.java
@@ -6,10 +6,7 @@
 package org.jetbrains.kotlinx.serialization.compiler.diagnostic;
 
 import com.intellij.psi.PsiElement;
-import org.jetbrains.kotlin.diagnostics.DiagnosticFactory0;
-import org.jetbrains.kotlin.diagnostics.DiagnosticFactory1;
-import org.jetbrains.kotlin.diagnostics.DiagnosticFactory2;
-import org.jetbrains.kotlin.diagnostics.Errors;
+import org.jetbrains.kotlin.diagnostics.*;
 import org.jetbrains.kotlin.psi.KtAnnotationEntry;
 import org.jetbrains.kotlin.types.KotlinType;
 
@@ -33,6 +30,9 @@ public interface SerializationErrors {
     DiagnosticFactory0<PsiElement> TRANSIENT_IS_REDUNDANT = DiagnosticFactory0.create(WARNING);
 
     DiagnosticFactory0<PsiElement> INCORRECT_TRANSIENT = DiagnosticFactory0.create(WARNING);
+
+    DiagnosticFactory3<KtAnnotationEntry, String, String, String> REQUIRED_KOTLIN_TOO_HIGH = DiagnosticFactory3.create(ERROR);
+    DiagnosticFactory3<KtAnnotationEntry, String, String, String> PROVIDED_RUNTIME_TOO_LOW = DiagnosticFactory3.create(ERROR);
 
     @SuppressWarnings("UnusedDeclaration")
     Object _initializer = new Object() {
