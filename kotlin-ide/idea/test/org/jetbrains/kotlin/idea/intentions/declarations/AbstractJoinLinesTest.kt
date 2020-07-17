6c3008e5466cdce026f70cdafb789879c4cf32a5
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-07-17 21:16:05 +0900

    Fix file finding in AbstractJoinLinesTest
    The tests are still failing, though the reason is a bit more obscure.

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/intentions/declarations/AbstractJoinLinesTest.kt
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/intentions/declarations/AbstractJoinLinesTest.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.idea.intentions.declarations
 
 import com.intellij.codeInsight.editorActions.JoinLinesHandler
 import com.intellij.ide.DataManager
-import com.intellij.rt.execution.junit.FileComparisonFailure
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.util.application.executeWriteCommand
 import org.jetbrains.kotlin.test.KotlinTestUtils
@@ -15,19 +14,18 @@ import java.io.File
 
 abstract class AbstractJoinLinesTest : KotlinLightCodeInsightFixtureTestCase() {
     @Throws(Exception::class)
-    fun doTest(path: String) {
-        myFixture.configureByFile(path)
+    fun doTest(unused: String) {
+        myFixture.configureByFile(fileName())
+
         val dataContext = DataManager.getInstance().getDataContext(editor.contentComponent)
-        myFixture.project.executeWriteCommand("") {
+
+        myFixture.project.executeWriteCommand("Join lines") {
             JoinLinesHandler(null).execute(editor, editor.caretModel.currentCaret, dataContext)
         }
 
-        val afterFilePath = "$path.after"
-        try {
-            myFixture.checkResultByFile(afterFilePath)
-        } catch (e: FileComparisonFailure) {
-            KotlinTestUtils.assertEqualsToFile(File(afterFilePath), editor)
-        }
+        val testFile = testDataFile()
+        val expectedFile = File(testFile.parentFile, testFile.name + ".after")
+        KotlinTestUtils.assertEqualsToFile(expectedFile, editor)
     }
 
     override fun getTestDataPath(): String = ""
