8b88d1a26f68d70e688b771078e81ec90aa4a7f5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-07-20 16:42:11 +0300

    FIR IDE: introduce FirIdeAllSourceDependenciesSymbolProvider
    Co-authored-by: Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
    
    Original commit: cfc46b0cc8757bec840c2203fe23cf94c791aede

--- kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirIdeModuleLibraryDependenciesSymbolProvider.kt
+++ kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirIdeModuleLibraryDependenciesSymbolProvider.kt
@@ -5,44 +5,38 @@
 
 package org.jetbrains.kotlin.idea.fir.low.level.api
 
-import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.providers.impl.FirDependenciesSymbolProviderImpl
 import org.jetbrains.kotlin.fir.dependenciesWithoutSelf
-import org.jetbrains.kotlin.fir.resolve.firSymbolProvider
-import org.jetbrains.kotlin.idea.caches.project.IdeaModuleInfo
 import org.jetbrains.kotlin.fir.java.FirLibrarySession
 import org.jetbrains.kotlin.fir.java.FirProjectSessionProvider
-import org.jetbrains.kotlin.idea.caches.project.ModuleSourceInfo
+import org.jetbrains.kotlin.fir.resolve.firSymbolProvider
+import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
+import org.jetbrains.kotlin.fir.resolve.providers.impl.FirDependenciesSymbolProviderImpl
+import org.jetbrains.kotlin.idea.caches.project.IdeaModuleInfo
 import org.jetbrains.kotlin.idea.caches.project.isLibraryClasses
 import org.jetbrains.kotlin.idea.caches.resolve.IDEPackagePartProvider
 
 
-internal class FirIdeModuleDependenciesSymbolProvider(
+internal class FirIdeModuleLibraryDependenciesSymbolProvider(
     session: FirIdeJavaModuleBasedSession
 ) : FirDependenciesSymbolProviderImpl(session) {
-
+    //todo invalidate cache on libraries changed
     override val dependencyProviders: List<FirSymbolProvider> by lazy {
         val moduleInfo = session.moduleInfo
-        val sessionProvider = session.sessionProvider ?: return@lazy emptyList<FirSymbolProvider>()
+        val sessionProvider = session.sessionProvider ?: error("No session provider found")
         val project = sessionProvider.project
-        val stateService = FirIdeResolveStateService.getInstance(project)
-        moduleInfo.dependenciesWithoutSelf().filterIsInstance<IdeaModuleInfo>().mapNotNull { dependencyInfo ->
-            val resolveState = stateService.getResolveState(dependencyInfo)
-            val dependencySession = when {
-                dependencyInfo is ModuleSourceInfo -> {
-                    resolveState.getSession(project, dependencyInfo)
-                }
-                dependencyInfo.isLibraryClasses() -> {
+
+        moduleInfo.dependenciesWithoutSelf()
+            .filterIsInstance<IdeaModuleInfo>()
+            .mapNotNull { dependencyInfo ->
+                val dependencySession = if (dependencyInfo.isLibraryClasses()) {
                     val dependencyScope = dependencyInfo.contentScope()
                     FirLibrarySession.create(
                         dependencyInfo, sessionProvider as FirProjectSessionProvider,
                         dependencyScope, project, IDEPackagePartProvider(dependencyScope)
                     )
-                }
-                else -> return@mapNotNull null
-            }
-            dependencySession.firSymbolProvider
-        }.toList()
+                } else null
+                dependencySession?.firSymbolProvider
+            }.toList()
     }
 }
 
