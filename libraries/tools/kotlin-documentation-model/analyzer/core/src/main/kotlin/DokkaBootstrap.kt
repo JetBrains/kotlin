a9a6483079896a1b6cb67636cee29878829f3acf
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-07-20 14:20:17 +0200

    [KDM] Re-introduce default DokkaCollectorTask, run on all submodules by default, add Collector0IntegrationTest
    Original commit: https://github.com/Kotlin/dokka/commit/f39971875c4f7d98dc1add2a8ed949d22b25600b

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/DokkaBootstrap.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/DokkaBootstrap.kt
@@ -4,6 +4,7 @@ import java.util.function.BiConsumer
 import kotlin.jvm.Throws
 
 interface DokkaBootstrap {
+    @Throws(Throwable::class)
     fun configure(serializedConfigurationJSON: String, logger: BiConsumer<String, String>)
 
     @Throws(Throwable::class)
