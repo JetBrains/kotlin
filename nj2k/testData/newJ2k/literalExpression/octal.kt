cba671a3ef853764a9fb6c7968009f822dfe9cdf
Roman Golyshev <anstreth@gmail.com>
2020-07-20 14:46:14 +0300

    KT-40359 Catch `NumberFormatException` during literal conversion
    - Instead of failing J2K conversion completely, insert TODO expression
    to the resulting code (it is better to have a converted code with TODO
    than to have nothing at all)
    - See EA-210233 for similar exceptions
    - ^KT-40359 Fixed

--- nj2k/testData/newJ2k/literalExpression/octal.kt
+++ nj2k/testData/newJ2k/literalExpression/octal.kt
@@ -1,8 +1,10 @@
 internal class Test {
     var negativeOctalInt = -1670764774
     var octalInt = 1013589786
+    var invalidOctalInt: Int = TODO("Could not convert int literal '08' to Kotlin")
     var negativeOctalLong = -1L
     var octalLong = 9223372036854775807L
+    var invalidOctalLong: Long = TODO("Could not convert long literal '08L' to Kotlin")
     var octalShort: Short = 20479
     var octalByte: Byte = 63
 }
