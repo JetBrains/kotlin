092bd98a2d491e915c40b6ac9f0cc3a0315862e0
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-07-21 20:59:16 +0900

    J2K AbstractJvmBasicCompletionTest

--- kotlin-ide/completion/test/org/jetbrains/kotlin/idea/completion/test/AbstractJvmBasicCompletionTest.kt
+++ kotlin-ide/completion/test/org/jetbrains/kotlin/idea/completion/test/AbstractJvmBasicCompletionTest.kt
@@ -2,32 +2,15 @@
  * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.idea.completion.test
 
-package org.jetbrains.kotlin.idea.completion.test;
+import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms.unspecifiedJvmPlatform
+import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor
 
-import com.intellij.codeInsight.completion.CompletionType;
-import com.intellij.testFramework.LightProjectDescriptor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.idea.test.KotlinWithJdkAndRuntimeLightProjectDescriptor;
-import org.jetbrains.kotlin.platform.TargetPlatform;
-import org.jetbrains.kotlin.platform.jvm.JvmPlatforms;
-
-public abstract class AbstractJvmBasicCompletionTest extends KotlinFixtureCompletionBaseTestCase {
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE;
-    }
-
-    @NotNull
-    @Override
-    public TargetPlatform getPlatform() {
-        return JvmPlatforms.INSTANCE.getUnspecifiedJvmPlatform();
-    }
-
-    @NotNull
-    @Override
-    protected CompletionType defaultCompletionType() {
-        return CompletionType.BASIC;
-    }
+abstract class AbstractJvmBasicCompletionTest : KotlinFixtureCompletionBaseTestCase() {
+  override fun getProjectDescriptor(): LightProjectDescriptor = KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE
+  override fun getPlatform() = unspecifiedJvmPlatform
+  override fun defaultCompletionType() = CompletionType.BASIC
 }
