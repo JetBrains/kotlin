13919cc0b4190089dbc46e4bc53f0ed70d01b129
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-07-21 16:29:21 +0700

    JoinBlockIntoSingleStatementHandler: update to `JoinLinesHandlerDelegate`

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/joinLines/JoinBlockIntoSingleStatementHandler.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/joinLines/JoinBlockIntoSingleStatementHandler.kt
@@ -1,57 +1,56 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.joinLines
 
-import com.intellij.codeInsight.editorActions.JoinRawLinesHandlerDelegate
+import com.intellij.codeInsight.editorActions.JoinLinesHandlerDelegate
+import com.intellij.codeInsight.editorActions.JoinLinesHandlerDelegate.CANNOT_JOIN
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.elementType
 import org.jetbrains.kotlin.idea.inspections.UseExpressionBodyInspection
 import org.jetbrains.kotlin.idea.intentions.MergeIfsIntention
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
-class JoinBlockIntoSingleStatementHandler : JoinRawLinesHandlerDelegate {
-    override fun tryJoinRawLines(document: Document, file: PsiFile, start: Int, end: Int): Int {
-        if (file !is KtFile) return -1
+class JoinBlockIntoSingleStatementHandler : JoinLinesHandlerDelegate {
+    override fun tryJoinLines(document: Document, file: PsiFile, start: Int, end: Int): Int {
+        if (file !is KtFile) return CANNOT_JOIN
 
-        if (start == 0) return -1
+        if (start == 0) return CANNOT_JOIN
         val c = document.charsSequence[start]
         val index = if (c == '\n') start - 1 else start
 
-        val brace = file.findElementAt(index)!!
-        if (brace.node!!.elementType != KtTokens.LBRACE) return -1
+        val brace = file.findElementAt(index) ?: return CANNOT_JOIN
+        if (brace.elementType != KtTokens.LBRACE) return CANNOT_JOIN
 
-        val block = brace.parent as? KtBlockExpression ?: return -1
-        val statement = block.statements.singleOrNull() ?: return -1
+        val block = brace.parent as? KtBlockExpression ?: return CANNOT_JOIN
+        val statement = block.statements.singleOrNull() ?: return CANNOT_JOIN
 
         val parent = block.parent
         val useExpressionBodyInspection = UseExpressionBodyInspection(convertEmptyToUnit = false)
         val oneLineReturnFunction = (parent as? KtDeclarationWithBody)?.takeIf { useExpressionBodyInspection.isActiveFor(it) }
-        if (parent !is KtContainerNode && parent !is KtWhenEntry && oneLineReturnFunction == null) return -1
+        if (parent !is KtContainerNode && parent !is KtWhenEntry && oneLineReturnFunction == null) return CANNOT_JOIN
 
-        if (block.node.getChildren(KtTokens.COMMENTS).isNotEmpty()) return -1 // otherwise we will loose comments
+        if (block.node.getChildren(KtTokens.COMMENTS).isNotEmpty()) return CANNOT_JOIN // otherwise we will loose comments
 
         // handle nested if's
         val pparent = parent.parent
         if (pparent is KtIfExpression) {
             if (block == pparent.then && statement is KtIfExpression && statement.`else` == null) {
                 // if outer if has else-branch and inner does not have it, do not remove braces otherwise else-branch will belong to different if!
-                if (pparent.`else` != null) return -1
+                if (pparent.`else` != null) return CANNOT_JOIN
 
                 return MergeIfsIntention.applyTo(pparent)
             }
 
             if (block == pparent.`else`) {
                 val ifParent = pparent.parent
-                if (!(ifParent is KtBlockExpression ||
-                            ifParent is KtDeclaration ||
-                            KtPsiUtil.isAssignment(ifParent))
-                ) {
-                    return -1
+                if (!(ifParent is KtBlockExpression || ifParent is KtDeclaration || KtPsiUtil.isAssignment(ifParent))) {
+                    return CANNOT_JOIN
                 }
             }
         }
@@ -64,6 +63,4 @@ class JoinBlockIntoSingleStatementHandler : JoinRawLinesHandlerDelegate {
             newStatement.textRange!!.startOffset
         }
     }
-
-    override fun tryJoinLines(document: Document, file: PsiFile, start: Int, end: Int) = -1
 }
