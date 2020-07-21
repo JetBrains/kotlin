799e475de1ec27c700692af1588745a2350f83cd
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-07-21 21:01:30 +0900

    Remove hacky work-around not needed anymore

--- kotlin-ide/completion/test/org/jetbrains/kotlin/idea/completion/test/KotlinFixtureCompletionBaseTestCase.kt
+++ kotlin-ide/completion/test/org/jetbrains/kotlin/idea/completion/test/KotlinFixtureCompletionBaseTestCase.kt
@@ -8,7 +8,6 @@ package org.jetbrains.kotlin.idea.completion.test
 import com.intellij.codeInsight.completion.CompletionType
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.openapi.util.io.FileUtil
-import org.jetbrains.kotlin.idea.caches.project.LibraryModificationTracker
 import org.jetbrains.kotlin.idea.test.CompilerTestDirectives
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.idea.test.withCustomCompilerOptions
@@ -52,9 +51,6 @@ abstract class KotlinFixtureCompletionBaseTestCase : KotlinLightCodeInsightFixtu
     }
 
     protected open fun setUpFixture(testPath: String) {
-        //TODO: this is a hacky workaround for js second completion tests failing with PsiInvalidElementAccessException
-        LibraryModificationTracker.getInstance(project).incModificationCount()
-
         myFixture.configureByFile(File(testPath).name)
     }
 
