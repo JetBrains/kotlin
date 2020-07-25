8f585b823feaf584a2d1c596cdc1ef0bd6c89b92
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-07-25 15:43:11 +0900

    Support both Ultimate and Community repository layouts in KotlinRoot

--- kotlin-ide/tests-common/test/org/jetbrains/kotlin/test/KotlinRoot.kt
+++ kotlin-ide/tests-common/test/org/jetbrains/kotlin/test/KotlinRoot.kt
@@ -7,7 +7,7 @@ object KotlinRoot {
     @JvmField
     val DIR: File = run {
         var current = File(".").canonicalFile
-        while (!current.resolve("kotlin.kotlin-ide.iml").isFile) {
+        while (!current.isRepositoryRootDirectory()) {
             current = current.parentFile
         }
         checkNotNull(current) { "Can't find kotlin-ide root" }
@@ -16,4 +16,18 @@ object KotlinRoot {
 
     @JvmField
     val PATH: Path = DIR.toPath()
+}
+
+private fun File.isRepositoryRootDirectory(): Boolean {
+    if (resolve("kotlin.kotlin-ide.iml").isFile && resolve("intellij").isDirectory && resolve("kotlin/idea/kotlin.idea.iml").isFile) {
+        // Ultimate repository
+        return true
+    }
+
+    if (resolve("kotlin.intellij-kotlin.iml").isFile && resolve("intellij").isDirectory && resolve("idea/kotlin.idea.iml").isFile) {
+        // Community (intellij-kotlin) repository
+        return true
+    }
+
+    return false
 }
