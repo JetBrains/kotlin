8cace2bab4bf084ab7bb0988a3d404fc9295f3da
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-07-27 14:24:42 +0300

    [Gradle, JS] Add tests on kotlin/js module with js files

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kotlin-js-dependency-with-js-files/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("js") version "<pluginMarkerVersion>"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("it.unibo.tuprolog:parser-core-js:0.11.1")
+}
+
+kotlin {
+    js {
+        nodejs()
+    }
+}
