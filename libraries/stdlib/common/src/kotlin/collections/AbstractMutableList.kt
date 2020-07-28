76f9ed96bcf60164e7b1ecf1bdc647f3efdb5dfe
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-07-28 14:38:08 +0300

    Fix doc wording around covariance/invariance
    (cherry picked from commit 99eb7f391ba1fa33e280a3d5b7f61e3b1dfa255d)

--- libraries/stdlib/common/src/kotlin/collections/AbstractMutableList.kt
+++ libraries/stdlib/common/src/kotlin/collections/AbstractMutableList.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,7 +8,7 @@ package kotlin.collections
 /**
  * Provides a skeletal implementation of the [MutableList] interface.
  *
- * @param E the type of elements contained in the list. The list is invariant on its element type.
+ * @param E the type of elements contained in the list. The list is invariant in its element type.
  */
 public expect abstract class AbstractMutableList<E> : MutableList<E> {
     protected constructor()
--- libraries/stdlib/js/src/kotlin/collections/AbstractMutableList.kt
+++ libraries/stdlib/js/src/kotlin/collections/AbstractMutableList.kt
@@ -1,7 +1,8 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+
 /*
  * Based on GWT AbstractList
  * Copyright 2007 Google Inc.
@@ -13,7 +14,7 @@ package kotlin.collections
 /**
  * Provides a skeletal implementation of the [MutableList] interface.
  *
- * @param E the type of elements contained in the list. The list is invariant on its element type.
+ * @param E the type of elements contained in the list. The list is invariant in its element type.
  */
 public actual abstract class AbstractMutableList<E> protected actual constructor() : AbstractMutableCollection<E>(), MutableList<E> {
     protected var modCount: Int = 0
