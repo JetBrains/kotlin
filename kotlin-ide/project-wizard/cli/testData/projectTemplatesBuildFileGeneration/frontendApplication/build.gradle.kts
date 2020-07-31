0eb26cbed96e5c9492db6e20e24c64168b21242b
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-07-31 12:15:26 +0300

    [Gradle, JS] Update test data
    ^KT-40884 fixed
    
    Original commit: 13766d50757a2d6a08d485914a84c838de7e4ff3

--- kotlin-ide/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/frontendApplication/build.gradle.kts
+++ kotlin-ide/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/frontendApplication/build.gradle.kts
@@ -20,9 +20,9 @@ repositories {
 dependencies {
     testImplementation(kotlin("test-js"))
     implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.1-KOTLIN_VERSION")
-    implementation("org.jetbrains:kotlin-react:16.13.1-pre.109-kotlin-KOTLIN_VERSION")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.109-kotlin-KOTLIN_VERSION")
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.109-kotlin-KOTLIN_VERSION")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.110-kotlin-KOTLIN_VERSION")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.110-kotlin-KOTLIN_VERSION")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.110-kotlin-KOTLIN_VERSION")
 }
 kotlin {
     js {
