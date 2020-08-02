39d4b79324bf7302d77705415599854b85101436
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-08-02 17:16:21 +0300

    [FIR TEST] Fix FE 1.0 / FIR test data for nested / local type alias case

--- compiler/testData/diagnostics/tests/typealias/localTypeAlias.fir.kt
+++ compiler/testData/diagnostics/tests/typealias/localTypeAlias.fir.kt
@@ -5,12 +5,12 @@ fun <T> emptyList(): List<T> = null!!
 fun <T> foo() {
     typealias LT = List<T>
 
-    val a: LT = emptyList()
+    val a: <!UNRESOLVED_REFERENCE, UNRESOLVED_REFERENCE!>LT<!> = emptyList()
 
-    fun localFun(): LT {
+    fun localFun(): <!UNRESOLVED_REFERENCE!>LT<!> {
         typealias LLT = List<T>
 
-        val b: LLT = a
+        val b: <!UNRESOLVED_REFERENCE, UNRESOLVED_REFERENCE!>LLT<!> = a
 
         return b
     }
