151aecc8ae16a41521a907ee6c6895a26b682d39
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2020-08-04 00:34:15 +0300

    Minor: fix toJavaDuration docs
    (cherry picked from commit a5178bb826fe57fbf84ff69124b16890a1365767)

--- libraries/stdlib/jdk8/src/kotlin/time/DurationConversions.kt
+++ libraries/stdlib/jdk8/src/kotlin/time/DurationConversions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -24,7 +24,7 @@ public inline fun java.time.Duration.toKotlinDuration(): Duration =
 
 
 /**
- * Converts [kotlin.time.Duration][Duration] value to value [java.time.Duration][java.time.Duration].
+ * Converts [kotlin.time.Duration][Duration] value to [java.time.Duration][java.time.Duration] value.
  *
  * Durations greater than [Long.MAX_VALUE] seconds are cut to that value.
  */
