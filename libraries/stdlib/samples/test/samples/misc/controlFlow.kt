f7f249f19a7bcec2426ad9127fce37300d189369
Julian Kotrba <me@juliankotrba.xyz>
2020-08-03 23:48:06 +0200

    Make repeat example more expressive
    This commit adds the zero-based index of current iteration from the passed HOF "action" of the repeat function to its associated code sample.
    
    KT-20357
    
    (cherry picked from commit 88f508446adb7ed1895313f225170b39096b6bbc)

--- libraries/stdlib/samples/test/samples/misc/controlFlow.kt
+++ libraries/stdlib/samples/test/samples/misc/controlFlow.kt
@@ -15,6 +15,11 @@ class ControlFlow {
         repeat(3) {
             println("Hello")
         }
+        
+        // greets with an index
+        repeat(3) { index ->
+            println("Hello with index $index")
+        }
 
         repeat(0) {
             error("We should not get here!")
