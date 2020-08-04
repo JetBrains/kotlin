1957be8757b11e82cf9edae28d532b4b3eeaa6f6
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-08-04 13:08:00 +0300

    FIR IDE: fix testdata of tests which now pass

--- idea/idea-fir/testData/highlighterFir/extensionProperty.kt
+++ idea/idea-fir/testData/highlighterFir/extensionProperty.kt
@@ -1,3 +1,3 @@
-class C(val a: A) {
-    val e = a.extension
+class <info descr="null">C</info>(val <info descr="null">a</info>: <info descr="null">A</info>) {
+    val <info descr="null">e</info> = <info descr="null">a</info>.<info descr="null">extension</info>
 }
--- idea/idea-fir/testData/highlighterFir/withCall.kt
+++ idea/idea-fir/testData/highlighterFir/withCall.kt
@@ -1,8 +1,8 @@
-class B {
-    fun q(): C {}
-    private val y = q()
+class <info descr="null">B</info> {
+    fun <info descr="null">q</info>(): <info descr="null">C</info> {}
+    <info descr="null">private</info> val <info descr="null">y</info> = <info descr="null">q</info>()
 
-    fun foo(a: A) = with(a) {
-        bar("a", y)
+    fun <info descr="null">foo</info>(<info descr="null">a</info>: <info descr="null">A</info>) = <info descr="null">with</info>(<info descr="null">a</info>) {
+        <info descr="null">bar</info>("a", <info descr="null">y</info>)
     }
 }
