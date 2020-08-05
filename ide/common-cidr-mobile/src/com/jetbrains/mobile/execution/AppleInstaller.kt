16ba669d5b89cf457874f278515138a4f5211abe
Egor Zhdan <egor.zhdan@jetbrains.com>
2020-08-05 17:19:21 +0300

    Cleanup: remove copyright from proprietary CIDR plugin sources

--- ide/common-cidr-mobile/src/com/jetbrains/mobile/execution/AppleInstaller.kt
+++ ide/common-cidr-mobile/src/com/jetbrains/mobile/execution/AppleInstaller.kt
@@ -1,8 +1,3 @@
-/*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
-
 package com.jetbrains.mobile.execution
 
 import com.intellij.execution.configurations.GeneralCommandLine
