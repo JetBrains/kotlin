75be9cf31fad2ef73f6bc8e42b6fd3a74396fdc2
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2020-08-07 14:40:28 +0700

    [Commonizer] Tests on rewriting callables names to succeed commonization
    ^KT-34602

---
+++ native/commonizer/testData/functionCommonization/valueParameters/commonized/macos/package_kotlinx_cinterop.kt
@@ -0,0 +1,6 @@
+// this is to avoid missing Kotlin/Native stdlib
+package kotlinx.cinterop
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.BINARY)
+annotation class ObjCMethod() // fake annotation class without properties
