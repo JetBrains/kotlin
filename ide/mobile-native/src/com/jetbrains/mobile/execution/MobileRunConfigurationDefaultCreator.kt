2ee7e622c47dcd7cd736bbf0220340f1d9376515
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2020-08-10 19:26:22 +0200

    Mobile IDE: select the automatically created run configuration

--- ide/mobile-native/src/com/jetbrains/mobile/execution/MobileRunConfigurationDefaultCreator.kt
+++ ide/mobile-native/src/com/jetbrains/mobile/execution/MobileRunConfigurationDefaultCreator.kt
@@ -9,22 +9,29 @@ import com.jetbrains.mobile.isAndroid
 import com.jetbrains.mobile.isApple
 
 fun MobileRunConfigurationBase.Companion.createDefaults(project: Project, modules: List<Module>) {
-    val runManager = RunManager.getInstance(project)
-    for (module in modules) {
+    if (modules.isNotEmpty()) {
+        val runManager = RunManager.getInstance(project)
         val type = MobileAppRunConfigurationType.instance
         val factory = type.factory
-        val configuration = factory.createTemplateConfiguration(project, runManager) as MobileRunConfigurationBase
-        configuration.module = module
-        if (module.isApple && !SystemInfo.isMac) continue
-        val name = when {
-            module.isApple -> MobileBundle.message("run.configuration.name.apple")
-            module.isAndroid -> MobileBundle.message("run.configuration.name.android")
-            else -> throw IllegalStateException()
-        }
-        configuration.name = if (modules.size > 1) "$name [${module.name}]" else name
+        for (module in modules) {
+            val configuration = factory.createTemplateConfiguration(project, runManager) as MobileRunConfigurationBase
+            configuration.module = module
+            if (module.isApple && !SystemInfo.isMac) continue
+            val name = when {
+                module.isApple -> MobileBundle.message("run.configuration.name.apple")
+                module.isAndroid -> MobileBundle.message("run.configuration.name.android")
+                else -> throw IllegalStateException()
+            }
+            configuration.name = if (modules.size > 1) "$name [${module.name}]" else name
 
-        if (runManager.findConfigurationByTypeAndName(type, configuration.name) == null) {
-            runManager.addConfiguration(runManager.createConfiguration(configuration, factory))
+            if (runManager.findConfigurationByTypeAndName(type, configuration.name) == null) {
+                runManager.addConfiguration(runManager.createConfiguration(configuration, factory))
+            }
+        }
+        if (runManager.selectedConfiguration == null) {
+            runManager.getConfigurationSettingsList(type).firstOrNull()?.let {
+                runManager.selectedConfiguration = it
+            }
         }
     }
 }
