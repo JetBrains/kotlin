26e1af5b993f7760b2b9e80b8118a30af4614ec7
Louis Vignier <louis.vignier@jetbrains.com>
2020-08-10 19:15:29 +0200

    Simplify KotlinExprTypePredicate
    By using KtPsiFactory instead of KotlinLexer, the code becomes lighter and more future-proof.

--- kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinExprTypePredicate.kt
+++ kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinExprTypePredicate.kt
@@ -3,34 +3,25 @@ package com.jetbrains.kotlin.structuralsearch
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.tree.IElementType
 import com.intellij.structuralsearch.StructuralSearchUtil
 import com.intellij.structuralsearch.impl.matcher.MatchContext
 import com.intellij.structuralsearch.impl.matcher.predicates.MatchPredicate
 import com.intellij.structuralsearch.impl.matcher.predicates.RegExpPredicate
-import org.jetbrains.kotlin.cfg.pseudocode.getSubtypesPredicate
+import org.jetbrains.kotlin.builtins.getReceiverTypeFromFunctionType
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.diagnostics.rendering.Renderers
 import org.jetbrains.kotlin.idea.debugger.sequence.psi.resolveType
 import org.jetbrains.kotlin.idea.refactoring.fqName.fqName
 import org.jetbrains.kotlin.idea.search.allScope
 import org.jetbrains.kotlin.idea.search.usagesSearch.descriptor
 import org.jetbrains.kotlin.idea.stubindex.KotlinClassShortNameIndex
 import org.jetbrains.kotlin.idea.stubindex.KotlinFullClassNameIndex
-import org.jetbrains.kotlin.lexer.KotlinLexer
-import org.jetbrains.kotlin.lexer.KtModifierKeywordToken
-import org.jetbrains.kotlin.lexer.KtToken
-import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.nj2k.postProcessing.type
-import org.jetbrains.kotlin.psi.KtDeclaration
-import org.jetbrains.kotlin.psi.KtExpression
-import org.jetbrains.kotlin.psi.KtSimpleNameStringTemplateEntry
-import org.jetbrains.kotlin.psi.KtStringTemplateEntry
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.isNull
 import org.jetbrains.kotlin.renderer.DescriptorRenderer
 import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.TypeProjection
 import org.jetbrains.kotlin.types.Variance
-import org.jetbrains.kotlin.types.typeUtil.immediateSupertypes
 import org.jetbrains.kotlin.types.typeUtil.isSubtypeOf
 import org.jetbrains.kotlin.types.typeUtil.supertypes
 import org.jetbrains.kotlinx.serialization.compiler.resolve.toSimpleType
@@ -74,143 +65,91 @@ class KotlinExprTypePredicate(
             }
         }
 
+        val factory = KtPsiFactory(project, false)
         return searchedTypeNames
             .filter { it != "null" }
-            .mapNotNull(Type.Companion::fromString)
-            .any { it.compareWith(type, project, scope, withinHierarchy) }
+            .map(factory::createType)
+            .any { matchTypeReference(type, it, project, scope, withinHierarchy) }
     }
 
-    class Type {
-        var className: String = ""
-        var arguments: MutableList<Type> = mutableListOf()
-        var nullable: Boolean = false
-        var variance: KtToken? = null
-        var functionType: Boolean = false
-
-        fun compareWith(type: KotlinType, project: Project, scope: GlobalSearchScope, withinHierarchy: Boolean): Boolean {
-            val index =
-                if (className.contains(".")) KotlinFullClassNameIndex.getInstance()
-                else KotlinClassShortNameIndex.getInstance()
-
-            if (functionType) {
-                if (type.fqName.toString() == className && type.isMarkedNullable == nullable) return (0..arguments.lastIndex).all { i ->
-                    arguments[i].compareWith(type.arguments[i].type, project, scope, false)
-                }
-            } else
-                index.get(className, project, scope).forEach {
-                    val searchedKotlinClass = (it.descriptor as ClassDescriptor).toSimpleType(nullable)
-                    if (searchedKotlinClass.fqName == type.fqName && searchedKotlinClass.isMarkedNullable == type.isMarkedNullable
-                        || withinHierarchy && type.isSubtypeOf(searchedKotlinClass)
-                    ) {
-                        if (arguments.any()) {
-                            if (arguments.size != type.arguments.size) return@compareWith false
-                            return@compareWith (0..arguments.lastIndex).all { i ->
-                                compareVariance(arguments[i].variance, type.arguments[i].projectionKind)
-                                        && arguments[i].compareWith(type.arguments[i].type, project, scope, false)
-                            }
-                        }
-                        return@compareWith true
-                    }
-                }
-            return false
+    companion object {
+        private fun matchTypeReference(
+            type: KotlinType?,
+            typeReference: KtTypeReference?,
+            project: Project,
+            scope: GlobalSearchScope,
+            withinHierarchy: Boolean
+        ): Boolean {
+            if (type == null || typeReference == null) return type == null && typeReference == null
+            val element = typeReference.typeElement ?: return false
+            return matchTypeElement(type, element, project, scope, withinHierarchy)
         }
 
-        private fun compareVariance(variance: KtToken?, projectionKind: Variance) = when (projectionKind) {
-            Variance.INVARIANT -> variance == null
-            Variance.IN_VARIANCE -> variance == KtTokens.IN_KEYWORD
-            Variance.OUT_VARIANCE -> variance == KtTokens.OUT_KEYWORD
+        private fun matchTypeElement(
+            type: KotlinType?,
+            typeElement: KtTypeElement?,
+            project: Project,
+            scope: GlobalSearchScope,
+            withinHierarchy: Boolean
+        ): Boolean {
+            if (type == null || typeElement == null) return type == null && typeElement == null
+
+            val matchArguments = typeElement.typeArgumentsAsTypes.isEmpty() ||
+                    type.arguments.size == typeElement.typeArgumentsAsTypes.size
+                    && type.arguments.zip(typeElement.typeArgumentsAsTypes).all { (projection, reference) ->
+                compareProjections(projection, reference) && matchTypeReference(
+                    projection.type,
+                    reference,
+                    project,
+                    scope,
+                    withinHierarchy
+                )
+            }
+
+            val matchSpecific = when (typeElement) {
+                is KtFunctionType ->
+                    "$type".startsWith("Function${typeElement.typeArgumentsAsTypes.size - 1}")
+                            && matchTypeReference(
+                        type.getReceiverTypeFromFunctionType(),
+                        typeElement.receiverTypeReference,
+                        project,
+                        scope,
+                        withinHierarchy
+                    )
+                is KtUserType -> {
+                    val className = typeElement.referencedName ?: return false
+                    val index = if (className.contains(".")) KotlinFullClassNameIndex.getInstance()
+                    else KotlinClassShortNameIndex.getInstance()
+                    index.get(className, project, scope).any {
+                        val searchedType = (it.descriptor as ClassDescriptor).toSimpleType(typeElement.parent is KtNullableType)
+                        searchedType.fqName == type.fqName || withinHierarchy && type.isSubtypeOf(searchedType)
+                    }
+                }
+                is KtNullableType -> type.isMarkedNullable && matchTypeElement(
+                    type,
+                    typeElement.innerType,
+                    project,
+                    scope,
+                    withinHierarchy
+                )
+                else -> throw Error("Malformed type: $typeElement")
+            }
+
+            return matchArguments
+                    && matchSpecific
         }
 
-        companion object {
-            fun fromString(string: String): Type? {
-                val lex = KotlinLexer()
-                val buf = "var x : $string"
-                lex.start(buf, 8, buf.length)
-                return getType(lex)
+        private fun compareProjections(projection: TypeProjection, typeReference: KtTypeReference?): Boolean {
+            val parent = typeReference?.parent
+            if (parent !is KtTypeProjection) return projection.projectionKind == Variance.INVARIANT
+
+            // TODO: Match star projections
+            return when (parent.projectionKind) {
+                KtProjectionKind.IN -> projection.projectionKind == Variance.IN_VARIANCE
+                KtProjectionKind.OUT -> projection.projectionKind == Variance.OUT_VARIANCE
+                KtProjectionKind.NONE -> projection.projectionKind == Variance.INVARIANT
+                else -> false
             }
-
-            private fun getType(lexer: KotlinLexer): Type? =
-                if (lexer.nextUseful == KtTokens.LPAR) getFunctionType(lexer) else getRegularType(lexer)
-
-            private fun getFunctionType(lexer: KotlinLexer): Type? {
-                val typeList = getTypeList(lexer, KtTokens.LPAR to KtTokens.RPAR) ?: return null
-
-                if (lexer.nextUseful != KtTokens.ARROW) return null
-                lexer.advance()
-
-                val destinationType = getType(lexer) ?: return null
-
-                return Type().apply {
-                    className = "kotlin.Function${typeList.size}"
-                    arguments.addAll(typeList)
-                    arguments.add(destinationType)
-                    if (lexer.nextUseful == KtTokens.QUEST) {
-                        nullable = true
-                        lexer.advance()
-                    }
-                    functionType = true
-                }
-            }
-
-            private fun getRegularType(lexer: KotlinLexer): Type? {
-                var variance: KtToken? = null
-
-                // out is not recognised as OUT_KEYWORD so we need to use lexer.tokenText
-                while (lexer.tokenType == KtTokens.WHITE_SPACE) lexer.advance()
-                val text = lexer.tokenText
-                lexer.advance()
-                val identifier: String
-                if ((text == "in" || text == "out") && lexer.tokenType == KtTokens.WHITE_SPACE) {
-                    variance = if (text == "in") KtTokens.IN_KEYWORD else KtTokens.OUT_KEYWORD
-                    identifier = getIdentifier(lexer)
-                } else {
-                    identifier = text + getIdentifier(lexer)
-                }
-
-                val arguments = getTypeList(lexer, KtTokens.LT to KtTokens.GT) ?: return null
-
-                var nullable = false
-                if (lexer.nextUseful == KtTokens.QUEST) {
-                    nullable = true
-                    lexer.advance()
-                }
-
-                return Type().apply {
-                    className = identifier
-                    this.arguments.addAll(arguments)
-                    this.nullable = nullable
-                    this.variance = variance
-                }
-            }
-
-            private fun getTypeList(lexer: KotlinLexer, limits: Pair<KtToken, KtToken>): List<Type>? {
-                val arguments = mutableListOf<Type>()
-                if (lexer.nextUseful == limits.first) {
-                    lexer.advance()
-                    while (lexer.nextUseful != limits.second) {
-                        arguments.add(getType(lexer) ?: return null)
-                        if (lexer.nextUseful == KtTokens.COMMA) lexer.advance()
-                    }
-                    lexer.advance()
-                }
-                return arguments
-            }
-
-            private fun getIdentifier(lexer: KotlinLexer): String {
-                var identifier = ""
-                while (lexer.tokenType == KtTokens.WHITE_SPACE) lexer.advance()
-                while (lexer.tokenType == KtTokens.IDENTIFIER || lexer.tokenType == KtTokens.DOT) {
-                    identifier += lexer.tokenText
-                    lexer.advance()
-                }
-                return identifier
-            }
-
-            private val KotlinLexer.nextUseful: IElementType?
-                get() {
-                    while (tokenType == KtTokens.WHITE_SPACE) advance()
-                    return tokenType
-                }
         }
     }
 
