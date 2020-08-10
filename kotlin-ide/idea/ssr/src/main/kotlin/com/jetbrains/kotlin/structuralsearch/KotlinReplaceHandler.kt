f4fd90d2a619f866e77627adabf23d8d5ca0ed95
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-08-10 15:25:55 +0200

    Add support for all modifiers

--- kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinReplaceHandler.kt
+++ kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinReplaceHandler.kt
@@ -3,6 +3,7 @@ package com.jetbrains.kotlin.structuralsearch
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.codeStyle.CodeStyleManager
+import com.intellij.psi.impl.DebugUtil
 import com.intellij.structuralsearch.StructuralReplaceHandler
 import com.intellij.structuralsearch.impl.matcher.MatcherImplUtil
 import com.intellij.structuralsearch.impl.matcher.PatternTreeContext
@@ -11,7 +12,10 @@ import com.intellij.structuralsearch.plugin.replace.ReplacementInfo
 import org.jetbrains.kotlin.idea.core.addTypeParameter
 import org.jetbrains.kotlin.lexer.KtModifierKeywordToken
 import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtModifierListOwner
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.visibilityModifierType
 
 class KotlinReplaceHandler(private val project: Project) : StructuralReplaceHandler() {
@@ -23,22 +27,31 @@ class KotlinReplaceHandler(private val project: Project) : StructuralReplaceHand
             info.replacement, PatternTreeContext.Block, options.matchOptions.fileType, project
         ).first()
         replaceTemplate.structuralReplace(searchTemplate, info.matchResult.match)
-        CodeStyleManager.getInstance(project).reformatNewlyAddedElement(info.matchResult.match.parent.node, replaceTemplate.node)
-        (0 until info.matchesCount).mapNotNull(info::getMatch).forEach { it.replace(replaceTemplate) }
+        println(DebugUtil.psiToString(replaceTemplate, false))
+        val codeStyleManager = CodeStyleManager.getInstance(project)
+        (0 until info.matchesCount).mapNotNull(info::getMatch).forEach {
+            val replacement = it.replace(replaceTemplate)
+            codeStyleManager.reformat(replacement)
+        }
     }
 
-    // TODO add more language constructs
     private fun PsiElement.structuralReplace(searchTemplate: PsiElement, match: PsiElement): PsiElement {
         if(searchTemplate is KtDeclaration && this is KtDeclaration && match is KtDeclaration) {
             replaceDeclaration(searchTemplate, match)
-            if(this is KtClassOrObject) replaceClassOrObject(searchTemplate, match)
-            if(this is KtNamedFunction) replaceNamedFunction(searchTemplate, match)
-        } else if (searchTemplate is KtExpression && this is KtExpression && match is KtExpression) {
-            // TODO
-        } else throw IllegalStateException(
-            "Search template, replace template and match should either all be expressions or declarations."
-        )
+            when(this) {
+                is KtClassOrObject -> replaceClassOrObject(searchTemplate, match)
+                is KtNamedFunction -> replaceNamedFunction(searchTemplate, match)
+            }
+        }
         return this
+    }
+
+    private fun KtModifierListOwner.replaceModifier(
+        searchTemplate: KtModifierListOwner,
+        match: KtModifierListOwner,
+        modifier: KtModifierKeywordToken
+    ) {
+        if(!hasModifier(modifier) && match.hasModifier(modifier) && !searchTemplate.hasModifier(modifier)) addModifier(modifier)
     }
 
     private fun KtDeclaration.replaceDeclaration(searchTemplate: KtDeclaration, match: KtDeclaration): PsiElement {
@@ -48,23 +61,7 @@ class KotlinReplaceHandler(private val project: Project) : StructuralReplaceHand
             }
             return this
         }
-
-        fun KtModifierListOwner.replaceModifier(
-            searchTemplate: KtModifierListOwner,
-            match: KtModifierListOwner,
-            modifier: KtModifierKeywordToken
-        ) {
-            if(!hasModifier(modifier) && match.hasModifier(modifier) && !searchTemplate.hasModifier(modifier)) {
-                addModifier(modifier)
-            }
-        }
-
         replaceVisibilityModifiers(searchTemplate, match)
-        replaceModifier(searchTemplate, match, KtTokens.DATA_KEYWORD)
-        replaceModifier(searchTemplate, match, KtTokens.ENUM_KEYWORD)
-        replaceModifier(searchTemplate, match, KtTokens.INNER_KEYWORD)
-        replaceModifier(searchTemplate, match, KtTokens.SEALED_KEYWORD)
-        replaceModifier(searchTemplate, match, KtTokens.ABSTRACT_KEYWORD)
         return this
     }
 
@@ -75,14 +72,12 @@ class KotlinReplaceHandler(private val project: Project) : StructuralReplaceHand
                 addSuperTypeListEntry(it)
             }
             if(body == null && searchTemplate.body == null) match.body?.let(this::add)
+            CLASS_MODIFIERS.forEach { replaceModifier(searchTemplate, match, it) }
         }
         return this
     }
 
     private fun KtNamedFunction.replaceNamedFunction(searchTemplate: PsiElement, match: PsiElement): PsiElement {
-        check(match is KtDeclaration) {
-            "Can't replace klass $text by ${match.text} because it is not a declaration."
-        }
         if(searchTemplate is KtNamedFunction && match is KtNamedFunction) {
             if(typeParameterList == null && searchTemplate.typeParameterList == null) match.typeParameters.forEach {
                 addTypeParameter(it)
@@ -91,7 +86,47 @@ class KotlinReplaceHandler(private val project: Project) : StructuralReplaceHand
                 match.valueParameters.add(it)
             }
             if(!hasBody() && !searchTemplate.hasBody()) match.bodyExpression?.let(this::add)
+            FUN_MODIFIERS.forEach { replaceModifier(searchTemplate, match, it) }
         }
         return this
+    }
+
+    companion object {
+        private val CLASS_MODIFIERS = arrayOf(
+            KtTokens.ABSTRACT_KEYWORD,
+            KtTokens.ENUM_KEYWORD,
+            KtTokens.OPEN_KEYWORD,
+            KtTokens.INNER_KEYWORD,
+            KtTokens.FINAL_KEYWORD,
+            KtTokens.COMPANION_KEYWORD,
+            KtTokens.SEALED_KEYWORD,
+            KtTokens.DATA_KEYWORD,
+            KtTokens.INLINE_KEYWORD,
+            KtTokens.EXTERNAL_KEYWORD,
+            KtTokens.ANNOTATION_KEYWORD,
+            KtTokens.CROSSINLINE_KEYWORD,
+            KtTokens.HEADER_KEYWORD,
+            KtTokens.IMPL_KEYWORD,
+            KtTokens.EXPECT_KEYWORD,
+            KtTokens.ACTUAL_KEYWORD
+        )
+
+        private val FUN_MODIFIERS = arrayOf(
+            KtTokens.ABSTRACT_KEYWORD,
+            KtTokens.OPEN_KEYWORD,
+            KtTokens.INNER_KEYWORD,
+            KtTokens.OVERRIDE_KEYWORD,
+            KtTokens.FINAL_KEYWORD,
+            KtTokens.INLINE_KEYWORD,
+            KtTokens.TAILREC_KEYWORD,
+            KtTokens.EXTERNAL_KEYWORD,
+            KtTokens.OPERATOR_KEYWORD,
+            KtTokens.INFIX_KEYWORD,
+            KtTokens.SUSPEND_KEYWORD,
+            KtTokens.HEADER_KEYWORD,
+            KtTokens.IMPL_KEYWORD,
+            KtTokens.EXPECT_KEYWORD,
+            KtTokens.ACTUAL_KEYWORD
+        )
     }
 }
