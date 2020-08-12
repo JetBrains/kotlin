09189da92a68978803ff79ca3a5f02bfe7ef825d
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-08-12 08:19:26 +0200

    [KDM] Move PreMergeDocumentableTransformer utils out of the interface
    Original commit: https://github.com/Kotlin/dokka/commit/e783eefe0c3b124aa25fdb2b28b9f238d48e293b

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/transformers/documentation/PreMergeDocumentableTransformer.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/transformers/documentation/PreMergeDocumentableTransformer.kt
@@ -1,30 +1,36 @@
 package org.jetbrains.dokka.transformers.documentation
 
-import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.DokkaConfiguration.DokkaSourceSet
+import org.jetbrains.dokka.DokkaConfiguration.PackageOptions
 import org.jetbrains.dokka.model.DModule
 import org.jetbrains.dokka.model.Documentable
-import org.jetbrains.dokka.model.DocumentableSource
 import org.jetbrains.dokka.model.WithExpectActual
 
 interface PreMergeDocumentableTransformer {
     operator fun invoke(modules: List<DModule>): List<DModule>
-
-    /* Convenience functions */
-    /**
-     * A [PreMergeDocumentableTransformer] can safely assume that documentables are not merged and therefore
-     * only belong to a single source set
-     */
-    val Documentable.sourceSet: DokkaSourceSet get() = sourceSets.single()
-
-    val Documentable.perPackageOptions: DokkaConfiguration.PackageOptions?
-        get() {
-            val packageName = dri.packageName ?: return null
-            return sourceSet.perPackageOptions
-                .sortedByDescending { packageOptions -> packageOptions.prefix.length }
-                .firstOrNull { packageOptions -> packageName.startsWith(packageOptions.prefix) }
-        }
-
-    val <T> T.source: DocumentableSource where T : Documentable, T : WithExpectActual
-        get() = checkNotNull(sources[sourceSet])
 }
+
+/* Utils */
+
+/**
+ * It is fair to assume that a given [Documentable] is not merged when seen by the [PreMergeDocumentableTransformer].
+ * Therefore, it can also be assumed, that there is just a single source set connected to the given [documentable]
+ * @return the single source set associated with this [documentable].
+ */
+@Suppress("unused") // Receiver is used for scoping this function
+fun PreMergeDocumentableTransformer.sourceSet(documentable: Documentable): DokkaSourceSet {
+    return documentable.sourceSets.single()
+}
+
+/**
+ * @return The [PackageOptions] associated with this documentable, or null
+ */
+fun PreMergeDocumentableTransformer.perPackageOptions(documentable: Documentable): PackageOptions? {
+    val packageName = documentable.dri.packageName ?: return null
+    return sourceSet(documentable).perPackageOptions
+        .sortedByDescending { packageOptions -> packageOptions.prefix.length }
+        .firstOrNull { packageOptions -> packageName.startsWith(packageOptions.prefix) }
+}
+
+fun <T> PreMergeDocumentableTransformer.source(documentable: T) where T : Documentable, T : WithExpectActual =
+    checkNotNull(documentable.sources[sourceSet(documentable)])
