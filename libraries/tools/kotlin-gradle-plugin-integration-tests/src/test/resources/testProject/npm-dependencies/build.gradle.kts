e3c215c7c09c4904e0c84893ffd8690968851495
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-08-12 16:04:20 +0300

    [Gradle, JS] Fix public package json test

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/npm-dependencies/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/npm-dependencies/build.gradle.kts
@@ -21,7 +21,7 @@ kotlin.sourceSets {
             implementation(npm(projectDir.resolve("file-dependency-3")))
             implementation(devNpm("42", "0.0.1"))
             implementation(npm("react", " >= 16.4.0 < 16.9.0"))
-            implementation(npm("date-arithmetic", "4.1.0"))
+            implementation(peerNpm("date-arithmetic", "4.1.0"))
         }
     }
 
