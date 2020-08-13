7d883f18068f4ace26c2b651a9a766113c0a0aba
Roman Golyshev <roman.golyshev@jetbrains.com>
2020-08-13 17:29:45 +0300

    FIR Completion: Add idea-fir dependency to run completion tests
    - Without this dependency, FIR plugin completion tests fail because the
    fir completion contributor is not available

--- idea/build.gradle.kts
+++ idea/build.gradle.kts
@@ -187,7 +187,7 @@ dependencies {
     testRuntime(intellijPluginDep("smali"))
     testRuntime(intellijPluginDep("testng"))
 
-    testRuntime(project(":idea:idea-frontend-fir"))
+    testRuntime(project(":idea:idea-fir"))
 
     if (Ide.AS36.orHigher()) {
         testRuntime(intellijPluginDep("android-layoutlib"))
