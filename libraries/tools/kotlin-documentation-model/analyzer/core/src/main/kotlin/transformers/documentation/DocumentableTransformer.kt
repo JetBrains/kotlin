5c154fff6ddebea15893864c8ed34472020dec53
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-08-13 09:59:39 +0200

    [KDM] Implement `ContentSourceSet`
    Original commit: https://github.com/Kotlin/dokka/commit/f6ac2b0f0a0183171aa2f6806ec67d8d21692a36

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/transformers/documentation/DocumentableTransformer.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/transformers/documentation/DocumentableTransformer.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.dokka.transformers.documentation
 
 import org.jetbrains.dokka.model.DModule
+import org.jetbrains.dokka.model.Documentable
 import org.jetbrains.dokka.plugability.DokkaContext
 
 interface DocumentableTransformer {
