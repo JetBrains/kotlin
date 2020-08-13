913ea9b56de20da50d4b6e5dd71494abf4113e1c
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-08-13 11:55:12 +0300

    [FIR-TEST] Get rid of deprecated methods usage in AbstractFirLoadCompiledKotlin
    Now complied declaration names are collected via ModuleDescriptor,
      so tests pass more correctly. Before this change tests didn't render
      compiled typealiases and properties.
    Also some testdata was changed because of new order of declaration

--- compiler/fir/analysis-tests/tests/org/jetbrains/kotlin/fir/AbstractFirLoadCompiledKotlin.kt
+++ compiler/fir/analysis-tests/tests/org/jetbrains/kotlin/fir/AbstractFirLoadCompiledKotlin.kt
@@ -8,11 +8,13 @@ package org.jetbrains.kotlin.fir
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.fir.resolve.firSymbolProvider
-import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.jvm.compiler.LoadDescriptorUtil.compileKotlinToDirAndGetModule
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.test.ConfigurationKind
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.jetbrains.kotlin.test.KotlinTestUtils.newConfiguration
@@ -32,7 +34,11 @@ abstract class AbstractFirLoadCompiledKotlin : AbstractFirResolveWithSessionTest
     }
 
     fun doTest(path: String) {
-        compileKtFileToTmpDir(path)
+        val moduleDescriptor = compileKtFileToTmpDir(path)
+
+        val packageFqName = FqName("test")
+        val allCompiledNames = DescriptorUtils.getAllDescriptors(moduleDescriptor.getPackage(packageFqName).memberScope)
+            .mapTo(sortedSetOf()) { it.name }
 
         val configuration = newConfiguration(ConfigurationKind.JDK_ONLY, TestJdkKind.MOCK_JDK, listOf(tmpdir), emptyList<File>())
         val environment = KotlinCoreEnvironment.createForTests(testRootDisposable, configuration, EnvironmentConfigFiles.JVM_CONFIG_FILES)
@@ -40,22 +46,22 @@ abstract class AbstractFirLoadCompiledKotlin : AbstractFirResolveWithSessionTest
         prepareProjectExtensions(environment.project)
         val sessionWithDependency = createSession(environment, GlobalSearchScope.EMPTY_SCOPE)
 
-        checkPackageContent(sessionWithDependency, FqName("test"), path)
+        checkPackageContent(sessionWithDependency, packageFqName, allCompiledNames, path)
     }
 
-    private fun compileKtFileToTmpDir(path: String) {
+    private fun compileKtFileToTmpDir(path: String): ModuleDescriptor {
         val file = File(path)
 
         val configuration = newConfiguration(ConfigurationKind.JDK_ONLY, TestJdkKind.MOCK_JDK, emptyList(), emptyList<File>())
         val environment = KotlinCoreEnvironment.createForTests(testRootDisposable, configuration, EnvironmentConfigFiles.JVM_CONFIG_FILES)
 
-        // We don't use ModuleDescriptor
-        compileKotlinToDirAndGetModule(listOf(file), tmpdir, environment)
+        return compileKotlinToDirAndGetModule(listOf(file), tmpdir, environment)
     }
 
     private fun checkPackageContent(
         session: FirSession,
         packageFqName: FqName,
+        declarationNames: Set<Name>,
         testDataPath: String
     ) {
         val provider = session.firSymbolProvider
@@ -63,17 +69,15 @@ abstract class AbstractFirLoadCompiledKotlin : AbstractFirResolveWithSessionTest
         val builder = StringBuilder()
         val firRenderer = FirRenderer(builder)
 
-        for (name in provider.getAllCallableNamesInPackage(packageFqName)) {
+        for (name in declarationNames) {
             for (symbol in provider.getTopLevelCallableSymbols(packageFqName, name)) {
                 symbol.fir.accept(firRenderer)
                 builder.appendLine()
             }
         }
 
-        for (name in provider.getClassNamesInPackage(packageFqName)) {
-            val classLikeSymbol =
-                provider.getClassLikeSymbolByFqName(ClassId.topLevel(packageFqName.child(name))) as FirClassSymbol?
-                    ?: continue
+        for (name in declarationNames) {
+            val classLikeSymbol = provider.getClassLikeSymbolByFqName(ClassId.topLevel(packageFqName.child(name))) ?: continue
             classLikeSymbol.fir.accept(firRenderer)
             builder.appendLine()
         }
