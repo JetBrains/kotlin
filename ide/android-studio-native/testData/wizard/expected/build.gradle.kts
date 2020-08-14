ae9265d9c55c5a7b7ddbca143987bf1b5ef7bc93
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-08-14 17:41:46 +0300

    Fix KMM Wizard test kotlin version

--- ide/android-studio-native/testData/wizard/expected/build.gradle.kts
+++ ide/android-studio-native/testData/wizard/expected/build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4-M1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0-rc")
         classpath("com.android.tools.build:gradle:3.5.2")
     }
 }
