a7b1ef74b9402ccd716d3a56e9b5769659ccaf3c
vldf <vladislavf7@gmail.com>
2020-08-18 11:05:26 +0300

    [FIR] Add missing tests

---
+++ compiler/tests-common/tests/org/jetbrains/kotlin/fir/AbstractExtendedFirWithLightTreeDiagnosticsTest.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.fir
+
+import org.jetbrains.kotlin.fir.analysis.checkers.declaration.ExtendedDeclarationCheckers
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.ExtendedExpressionCheckers
+import org.jetbrains.kotlin.fir.analysis.checkersComponent
+
+abstract class AbstractExtendedFirWithLightTreeDiagnosticsTest : AbstractFirDiagnosticsWithLightTreeTest() {
+    override fun configureSession(session: FirSession) {
+        session.checkersComponent.register(ExtendedDeclarationCheckers)
+        session.checkersComponent.register(ExtendedExpressionCheckers)
+    }
+}
