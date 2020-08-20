a452346385e75834708a78de72dcb004278c7d99
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-08-20 14:26:21 +0300

    [Runtime testing] Add example tests

--- runtime/src/test_support/cpp/TestLauncher.cpp
+++ runtime/src/test_support/cpp/TestLauncher.cpp
@@ -6,7 +6,7 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
