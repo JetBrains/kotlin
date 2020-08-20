00bd30260398581896e05e295e47dcbd470bf02d
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-08-20 15:33:28 +0700

    [formatter] remove old code

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/formatter/KotlinFormattingModelBuilder.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/formatter/KotlinFormattingModelBuilder.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -7,30 +7,22 @@ package org.jetbrains.kotlin.idea.formatter
 
 import com.intellij.formatting.*
 import com.intellij.lang.ASTNode
-import com.intellij.openapi.editor.impl.DocumentImpl
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
-import com.intellij.psi.formatter.FormattingDocumentModelImpl
-import com.intellij.psi.formatter.PsiBasedFormattingModel
-import org.jetbrains.kotlin.idea.KotlinLanguage
 
 class KotlinFormattingModelBuilder : FormattingModelBuilder {
     override fun createModel(formattingContext: FormattingContext): FormattingModel {
         val settings = formattingContext.codeStyleSettings
         val element = formattingContext.psiElement
-        val containingFile = element.containingFile.viewProvider.getPsi(KotlinLanguage.INSTANCE)
+        val containingFile = formattingContext.containingFile
         val block = KotlinBlock(
-            containingFile.node, NodeAlignmentStrategy.getNullStrategy(), Indent.getNoneIndent(), null, settings,
+            containingFile.node,
+            NodeAlignmentStrategy.getNullStrategy(),
+            Indent.getNoneIndent(),
+            wrap = null,
+            settings,
             createSpacingBuilder(settings, KotlinSpacingBuilderUtilImpl)
         )
-
-        //TODO: this is temporary code to allow formatting non-physical files in non-UI thread (used by conversion from Java to Kotlin)
-        // it's needed until IDEA's issue with this document being created with wrong threading policy is fixed
-        if (!element.isPhysical) {
-            val formattingDocumentModel =
-                FormattingDocumentModelImpl(DocumentImpl(containingFile.viewProvider.contents, true), containingFile)
-            return PsiBasedFormattingModel(containingFile, block, formattingDocumentModel)
-        }
 
         if (element is PsiFile) {
             val collectChangesModel = createCollectFormattingChangesModel(element, block)
@@ -39,7 +31,7 @@ class KotlinFormattingModelBuilder : FormattingModelBuilder {
             }
         }
 
-        return FormattingModelProvider.createFormattingModelForPsiFile(element.containingFile, block, settings)
+        return FormattingModelProvider.createFormattingModelForPsiFile(containingFile, block, settings)
     }
 
     override fun getRangeAffectingIndent(psiFile: PsiFile, i: Int, astNode: ASTNode): TextRange? {
