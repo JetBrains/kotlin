c16cffa9f4e64553b7a43836520d1f9a19348e72
Roman Artemev <roman.artemev@jetbrains.com>
2020-08-20 18:19:41 +0300

    [KLIB] Corresponding fix to kotlin (@JsExport)
    (cherry picked from commit 0142385cdc78af49890374c7aec1db25ddd13778)

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrFileSerializer.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrFileSerializer.kt
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.backend.common.serialization.DeclarationTable
 import org.jetbrains.kotlin.backend.common.serialization.IrFileSerializer
 import org.jetbrains.kotlin.backend.konan.RuntimeNames
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.ir.declarations.IrAnnotationContainer
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
@@ -18,11 +19,15 @@ class KonanIrFileSerializer(
     bodiesOnlyForInlines: Boolean = false
 ): IrFileSerializer(logger, declarationTable, expectDescriptorToSymbol, skipExpects = skipExpects, bodiesOnlyForInlines = bodiesOnlyForInlines) {
 
-    override fun backendSpecificExplicitRoot(declaration: IrFunction) =
-            declaration.annotations.hasAnnotation(RuntimeNames.exportForCppRuntime)
+    override fun backendSpecificExplicitRoot(node: IrAnnotationContainer): Boolean {
+        val fqn = when (node) {
+            is IrFunction -> RuntimeNames.exportForCppRuntime
+            is IrClass -> RuntimeNames.exportTypeInfoAnnotation
+            else -> return false
+        }
 
-    override fun backendSpecificExplicitRoot(declaration: IrClass) =
-            declaration.annotations.hasAnnotation(RuntimeNames.exportTypeInfoAnnotation)
+        return node.annotations.hasAnnotation(fqn)
+    }
 
     override fun backendSpecificSerializeAllMembers(irClass: IrClass) = !KonanFakeOverrideClassFilter.constructFakeOverrides(irClass)
 }
