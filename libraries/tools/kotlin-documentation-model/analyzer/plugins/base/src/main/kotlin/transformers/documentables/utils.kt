12e809df95cd8189696bebd25bf48f037e122cdf
Błażej Kardyś <bkardys@virtuslab.com>
2020-08-21 15:26:17 +0200

    [KDM] Adding missing deprecated exception section to Deprecated page
    Original commit: https://github.com/Kotlin/dokka/commit/39b64feead5718ae9d3e4293407cf0858057493a

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/utils.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/utils.kt
@@ -2,6 +2,7 @@ package org.jetbrains.dokka.base.transformers.documentables
 
 import org.jetbrains.dokka.model.Annotations
 import org.jetbrains.dokka.model.Documentable
+import org.jetbrains.dokka.model.WithSupertypes
 import org.jetbrains.dokka.model.properties.WithExtraProperties
 
 fun <T> T.isDeprecated() where T : WithExtraProperties<out Documentable> =
@@ -13,4 +14,11 @@ val <T> T.deprecatedAnnotation where T : WithExtraProperties<out Documentable>
             it.dri.toString() == "kotlin/Deprecated///PointingToDeclaration/" ||
                     it.dri.toString() == "java.lang/Deprecated///PointingToDeclaration/"
         }
+    }
+
+val WithSupertypes.isException: Boolean
+    get() = supertypes.values.flatten().any {
+        val dri = it.typeConstructor.dri.toString()
+        dri == "kotlin/Exception///PointingToDeclaration/" ||
+                dri == "java.lang/Exception///PointingToDeclaration/"
     }
