6995c8e6f3578f04f07d65e743d333d69a730cec
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-08-25 14:28:44 +0200

    [KDM] Implement ModuleAndPackageDocumentationReader
    Original commit: https://github.com/Kotlin/dokka/commit/29cc17ba80018e3e45a5fabcf2e370c735fd5674

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/ModuleAndPackageDocumentationTransformer.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/ModuleAndPackageDocumentationTransformer.kt
@@ -1,101 +1,32 @@
 package org.jetbrains.dokka.base.transformers.documentables
 
-import org.jetbrains.dokka.analysis.KotlinAnalysis
+import org.jetbrains.dokka.DokkaConfiguration.DokkaSourceSet
 import org.jetbrains.dokka.model.DModule
 import org.jetbrains.dokka.model.doc.DocumentationNode
-import org.jetbrains.dokka.DokkaConfiguration.DokkaSourceSet
-import org.jetbrains.dokka.base.parsers.MarkdownParser
-import org.jetbrains.dokka.plugability.DokkaContext
 import org.jetbrains.dokka.transformers.documentation.PreMergeDocumentableTransformer
-import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.name.Name
-import java.nio.file.Files
-import java.nio.file.Paths
 
-
+// TODO NOW: Test
 internal class ModuleAndPackageDocumentationTransformer(
-    private val context: DokkaContext,
-    private val kotlinAnalysis: KotlinAnalysis
+    private val moduleAndPackageDocumentationReader: ModuleAndPackageDocumentationReader
 ) : PreMergeDocumentableTransformer {
 
     override fun invoke(modules: List<DModule>): List<DModule> {
-
-        val modulesAndPackagesDocumentation =
-            context.configuration.sourceSets
-                .map { sourceSet ->
-                    Pair(sourceSet.moduleDisplayName, sourceSet) to
-                            sourceSet.includes.map { it.toPath() }
-                                .also {
-                                    it.forEach {
-                                        if (Files.notExists(it))
-                                            context.logger.warn("Not found file under this path ${it.toAbsolutePath()}")
-                                    }
-                                }
-                                .filter { Files.exists(it) }
-                                .flatMap {
-                                    it.toFile()
-                                        .readText()
-                                        .split(Regex("(\n|^)# (?=(Module|Package))")) // Matches heading with Module/Package to split by
-                                        .filter { it.isNotEmpty() }
-                                        .map { it.split(Regex(" "), 2) } // Matches space between Module/Package and fully qualified name
-                                }.groupBy({ it[0] }, {
-                                    it[1].split(Regex("\n"), 2) // Matches new line after fully qualified name
-                                        .let { it[0].trim() to it[1].trim() }
-                                }).mapValues {
-                                    it.value.toMap()
-                                }
-                }.toMap()
-
         return modules.map { module ->
-
-            val moduleDocumentation =
-                module.sourceSets.mapNotNull { pd ->
-                    val doc = modulesAndPackagesDocumentation[Pair(module.name, pd)]
-                    val facade = kotlinAnalysis[pd].facade
-                    try {
-                        doc?.get("Module")?.get(module.name)?.run {
-                            pd to MarkdownParser(
-                                facade,
-                                facade.moduleDescriptor.getPackage(FqName.topLevel(Name.identifier(""))),
-                                context.logger
-                            ).parse(this)
-                        }
-                    } catch (e: IllegalArgumentException) {
-                        context.logger.error(e.message.orEmpty())
-                        null
-                    }
-                }.toMap()
-
-            val packagesDocumentation = module.packages.map {
-                it.name to it.sourceSets.mapNotNull { pd ->
-                    val doc = modulesAndPackagesDocumentation[Pair(module.name, pd)]
-                    val facade = kotlinAnalysis[pd].facade
-                    val descriptor = facade.moduleDescriptor.getPackage(FqName(it.name.let { if(it == "[JS root]") "" else it }))
-                    doc?.get("Package")?.get(it.name)?.run {
-                        pd to MarkdownParser(
-                            facade,
-                            descriptor,
-                            context.logger
-                        ).parse(this)
-                    }
-                }.toMap()
-            }.toMap()
-
             module.copy(
-                documentation = mergeDocumentation(module.documentation, moduleDocumentation),
-                packages = module.packages.map {
-                    val packageDocumentation = packagesDocumentation[it.name]
-                    if (packageDocumentation != null && packageDocumentation.isNotEmpty())
-                        it.copy(documentation = mergeDocumentation(it.documentation, packageDocumentation))
-                    else
-                        it
+                documentation = module.documentation + moduleAndPackageDocumentationReader[module],
+                packages = module.packages.map { pkg ->
+                    pkg.copy(
+                        documentation = pkg.documentation + moduleAndPackageDocumentationReader[pkg]
+                    )
                 }
             )
         }
     }
 
-    private fun mergeDocumentation(origin: Map<DokkaSourceSet, DocumentationNode>, new: Map<DokkaSourceSet, DocumentationNode>): Map<DokkaSourceSet, DocumentationNode> =
-        (origin.asSequence() + new.asSequence())
+    private operator fun Map<DokkaSourceSet, DocumentationNode>.plus(
+        other: Map<DokkaSourceSet, DocumentationNode>
+    ): Map<DokkaSourceSet, DocumentationNode> =
+        (asSequence() + other.asSequence())
             .distinct()
             .groupBy({ it.key }, { it.value })
             .mapValues { (_, values) -> DocumentationNode(values.flatMap { it.children }) }
