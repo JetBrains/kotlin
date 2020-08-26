9d5dbf0e35a3618e7894d5e0d8629bf5acb3addf
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-08-26 14:13:44 +0300

    Consider T <: Nothing! as trivial during constraint incorporation
     #KT-41149 In Progress
    
    (cherry picked from commit 6a7ed96be587d4c703c45ecc674536adf7c31fa7)

--- compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/inference/components/TrivialConstraintTypeInferenceOracle.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/calls/inference/components/TrivialConstraintTypeInferenceOracle.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.resolve.calls.inference.components
 import org.jetbrains.kotlin.resolve.calls.inference.model.Constraint
 import org.jetbrains.kotlin.resolve.calls.inference.model.ConstraintKind
 import org.jetbrains.kotlin.types.model.KotlinTypeMarker
+import org.jetbrains.kotlin.types.model.SimpleTypeMarker
 import org.jetbrains.kotlin.types.model.TypeSystemInferenceExtensionContext
 import org.jetbrains.kotlin.types.model.TypeSystemInferenceExtensionContextDelegate
 
@@ -63,7 +64,10 @@ class TrivialConstraintTypeInferenceOracle private constructor(context: TypeSyst
 
 
     private fun KotlinTypeMarker.containsOnlyNonNullableNothing(): Boolean =
-        contains { it.isNothing() } && !contains { it.isNullableNothing() }
+        contains {
+            (it.isNothing() || it.isFlexibleNothing()) &&
+                    !(it is SimpleTypeMarker && it.typeConstructor().isNothingConstructor() && it.isMarkedNullable())
+        }
 
 
     companion object {
