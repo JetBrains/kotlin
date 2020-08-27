09ed0c3e821ecdf7bedc3c5f6ce8f57a327f4f8e
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-08-27 12:30:00 +0300

    [FIR-TEST] Update testdata

--- compiler/fir/analysis-tests/testData/resolve/typeParameterVsNested.kt
+++ compiler/fir/analysis-tests/testData/resolve/typeParameterVsNested.kt
@@ -13,7 +13,7 @@ abstract class My<T : Some> {
 
     abstract val z: test.My.T
 
-    class Some : <!UNRESOLVED_REFERENCE!>T<!>()
+    class Some : <!OTHER_ERROR, UNRESOLVED_REFERENCE!>T<!>()
 }
 
 abstract class Your<T : Some> : <!OTHER_ERROR!>T<!>
