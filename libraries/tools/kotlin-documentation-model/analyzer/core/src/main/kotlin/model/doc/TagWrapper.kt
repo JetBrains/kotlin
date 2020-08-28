0e5c158985c1ef20bf24edcdda479ee3555f2b7d
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-08-28 11:52:21 +0200

    [KDM] Minor readability improvement in ModuleAndPackageDocumentationTransformer.kt
    Original commit: https://github.com/Kotlin/dokka/commit/e1bc27df4b50cf09d45db41462944a01d428be56

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/model/doc/TagWrapper.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/model/doc/TagWrapper.kt
@@ -6,17 +6,18 @@ import org.jetbrains.dokka.model.WithChildren
 sealed class TagWrapper(val root: DocTag) : WithChildren<DocTag> {
 
     override val children: List<DocTag>
-        get() =  root.children
+        get() = root.children
 
     override fun equals(other: Any?): Boolean =
         (
-            other != null &&
-            this::class == other::class &&
-            this.root == (other as TagWrapper).root
-        )
+                other != null &&
+                        this::class == other::class &&
+                        this.root == (other as TagWrapper).root
+                )
 
     override fun hashCode(): Int = root.hashCode()
 }
+
 sealed class NamedTagWrapper(root: DocTag, val name: String) : TagWrapper(root) {
     override fun equals(other: Any?): Boolean = super.equals(other) && this.name == (other as NamedTagWrapper).name
     override fun hashCode(): Int = super.hashCode() + name.hashCode()
