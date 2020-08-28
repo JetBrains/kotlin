887c318efe332111da2703ee63ad1afe7d9a6324
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2020-08-28 19:22:00 +0900

    Update test data in MultiFileHighlightingTestGenerated

--- kotlin-ide/idea/testData/multiFileHighlighting/topLevelMembersReference.kt
+++ kotlin-ide/idea/testData/multiFileHighlighting/topLevelMembersReference.kt
@@ -6,18 +6,18 @@ fun test() {
     topLevelFun(topLevelVar)
     topLevelFun(topLevelVal)
     val c = C("ff", 1)
-    c.s.<error>invalid</error>
-    val <warning>b</warning> = B()
+    c.s.<error descr="[UNRESOLVED_REFERENCE] Unresolved reference: invalid">invalid</error>
+    val <warning descr="[UNUSED_VARIABLE] Variable 'b' is never used">b</warning> = B()
     funWithVararg(1, 2, 3)
-    val <warning>i</warning> = Invalid()
+    val <warning descr="[UNUSED_VARIABLE] Variable 'i' is never used">i</warning> = Invalid()
     topLevelObject.f()
     topLevelObject.g()
  }
 
 fun testWhere(list: List<Int>) {
     funWithWhere(1, list)
-    <error>funWithWhere</error>(1, 2)
+    funWithWhere(1, <error descr="[CONSTANT_EXPECTED_TYPE_MISMATCH] The integer literal does not conform to the expected type Collection<TypeVariable(G)>">2</error>)
 }
 
 class Example : C("foo", 0)
-class Example2 : <error>A</error>(2)
+class Example2 : <error descr="[FINAL_SUPERTYPE] This type is final, so it cannot be inherited from">A</error>(2)
