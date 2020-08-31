b176930cf8666f173ee8448cfde0d08f9a193959
Mathias Quintero <me@quintero.io>
2020-08-31 13:34:06 +0200

    [Scripting] [Type Providers] [Code Generation DSL] Add functionality to implement an interface and use override for the members in a single statement

--- libraries/scripting/type-providers/src/kotlin/script/experimental/typeProviders/generatedCode/ClassLikeBuilder.kt
+++ libraries/scripting/type-providers/src/kotlin/script/experimental/typeProviders/generatedCode/ClassLikeBuilder.kt
@@ -5,6 +5,16 @@
 
 package kotlin.script.experimental.typeProviders.generatedCode
 
+import kotlin.reflect.KType
+import kotlin.reflect.jvm.jvmErasure
+import kotlin.reflect.typeOf
+import kotlin.script.experimental.typeProviders.generatedCode.impl.*
+import kotlin.script.experimental.typeProviders.generatedCode.impl.appendJoined
+import kotlin.script.experimental.typeProviders.generatedCode.impl.appendLine
+import kotlin.script.experimental.typeProviders.generatedCode.internal.InternalGeneratedCode
+import kotlin.script.experimental.typeProviders.generatedCode.internal.visit
+import kotlin.script.experimental.typeProviders.generatedCode.internal.visitor.GeneratedCodeVisitor
+import kotlin.script.experimental.typeProviders.generatedCode.internal.visitor.useImports
 
 /**
  * Builder for a provided type. It can implement an interface and have some code inside.
@@ -38,6 +48,37 @@ fun ClassLikeBuilder<*>.implement(name: String) {
     implement(IdentifiableMember(name).asInterface())
 }
 
+/**
+ * Implement the interface and specify override methods and properties
+ */
+fun <O : ClassLikeBuilder<O>> ClassLikeBuilder<O>.implement(generated: GeneratedInterface, init: O.() -> Unit) {
+    implement(generated)
+    override(init)
+}
+
+/**
+ * Implement the interface and specify override methods and properties
+ */
+fun <O : ClassLikeBuilder<O>> ClassLikeBuilder<O>.implement(name: String, init: O.() -> Unit) {
+    implement(name)
+    override(init)
+}
+
+/**
+ * Implement the interface and specify override methods and properties
+ */
+inline fun <O : ClassLikeBuilder<O>, reified T> ClassLikeBuilder<O>.implement(noinline init: O.() -> Unit) {
+    implement<T>()
+    override(init)
+}
+
+/**
+ * Implement the interface and specify override methods and properties
+ */
+fun <O : ClassLikeBuilder<O>> ClassLikeBuilder<O>.implement(kType: KType, init: O.() -> Unit) {
+    implement(kType)
+    override(init)
+}
 
 internal class ClassLike(
     private val interfaces: Set<GeneratedInterface>,
