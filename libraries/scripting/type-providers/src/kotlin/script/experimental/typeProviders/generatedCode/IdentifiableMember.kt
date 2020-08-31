c6b7457b955c9de8f1438cf31c0de4fb8608a59d
Mathias Quintero <me@quintero.io>
2020-08-31 13:28:32 +0200

    [Scripting] [Type Providers] [Code Generation DSL] Add a way to create identifiable members from types and strings

--- libraries/scripting/type-providers/src/kotlin/script/experimental/typeProviders/generatedCode/IdentifiableMember.kt
+++ libraries/scripting/type-providers/src/kotlin/script/experimental/typeProviders/generatedCode/IdentifiableMember.kt
@@ -5,6 +5,10 @@
 
 package kotlin.script.experimental.typeProviders.generatedCode
 
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.jvm.jvmErasure
+import kotlin.reflect.typeOf
 
 /**
  * References a member that can be used as a type
@@ -13,4 +17,30 @@ interface IdentifiableMember {
     val name: String
     fun imports(): Set<String> = emptySet()
 
+    companion object {
+        @OptIn(ExperimentalStdlibApi::class)
+        inline operator fun <reified T> invoke(): IdentifiableMember = invoke(typeOf<T>())
+
+        operator fun invoke(kType: KType): IdentifiableMember = KTypeIdentifiable(kType)
+        operator fun invoke(kClass: KClass<*>): IdentifiableMember = KClassIdentifiable(kClass)
+        operator fun invoke(name: String): IdentifiableMember = NamedIdentifiable(name)
+    }
 }
+
+private class KTypeIdentifiable(kType: KType) : IdentifiableMember {
+    override val name: String = kType.toString()
+
+    private val imports = setOfNotNull(kType.jvmErasure.qualifiedName) +
+            kType.arguments.mapNotNull { it.type?.jvmErasure?.qualifiedName }.toSet()
+
+    override fun imports(): Set<String> = imports
+}
+
+private class KClassIdentifiable(kClass: KClass<*>) : IdentifiableMember {
+    override val name: String = kClass.simpleName ?: kClass.qualifiedName!!
+    private val import = kClass.qualifiedName
+
+    override fun imports() = setOfNotNull(import)
+}
+
+private class NamedIdentifiable(override val name: String) : IdentifiableMember
