92c94982a586b865ce3209728b342da709eda53b
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-08-31 14:08:46 +0200

    Add replacement test template

---
+++ kotlin-ide/idea/ssr/src/test/kotlin/com/jetbrains/kotlin/structuralsearch/KotlinSSRReplaceTest.kt
@@ -0,0 +1,55 @@
+package com.jetbrains.kotlin.structuralsearch
+
+import com.intellij.psi.search.GlobalSearchScopes
+import com.intellij.structuralsearch.Matcher
+import com.intellij.structuralsearch.PatternContext
+import com.intellij.structuralsearch.plugin.replace.ReplacementInfo
+import com.intellij.structuralsearch.plugin.replace.impl.Replacer
+import com.intellij.structuralsearch.plugin.replace.ui.ReplaceConfiguration
+import com.intellij.structuralsearch.plugin.ui.SearchConfiguration
+import com.intellij.structuralsearch.plugin.util.CollectingMatchResultSink
+import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.util.SmartList
+import com.jetbrains.rd.util.string.printToString
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.util.application.executeWriteCommand
+
+abstract class KotlinSSRReplaceTest : BasePlatformTestCase() {
+    private val searchConfiguration = SearchConfiguration().apply {
+        name = "SSR"
+        matchOptions.fileType = KotlinFileType.INSTANCE
+    }
+
+    override fun getProjectDescriptor(): LightProjectDescriptor = KotlinLightProjectDescriptor()
+
+    protected fun doTest(
+        searchPattern: String,
+        replacePattern: String,
+        match: String,
+        result: String,
+        reformat: Boolean = false,
+        context: PatternContext = KotlinStructuralSearchProfile.DEFAULT_CONTEXT
+    ) {
+        myFixture.configureByText(KotlinFileType.INSTANCE, match)
+        val matchOptions = searchConfiguration.matchOptions.apply {
+            fillSearchCriteria(searchPattern)
+            patternContext = context
+            fileType = KotlinFileType.INSTANCE
+            scope = GlobalSearchScopes.openFilesScope(project)
+        }
+        val matcher = Matcher(project, matchOptions)
+        val sink = CollectingMatchResultSink()
+        matcher.findMatches(sink)
+        val replaceOptions = ReplaceConfiguration(searchConfiguration).replaceOptions.apply {
+            isToReformatAccordingToStyle = reformat
+            replacement = replacePattern
+        }
+        val replacer = Replacer(project, replaceOptions)
+        val replacements: MutableList<ReplacementInfo> = SmartList()
+        sink.matches.mapTo(replacements, replacer::buildReplacement)
+        println(myFixture.file.printToString())
+        myFixture.project.executeWriteCommand("Structural Replace") { replacements.forEach(replacer::replace) }
+        assert(myFixture.file.text == result)
+    }
+}
