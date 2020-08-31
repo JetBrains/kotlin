ca2cc55ae58f7bde226e826a34b43a7711ea1cd9
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-08-31 12:09:43 +0200

    Implement JVM IR support for scripts
    Original commit: d7112a4500c39416df59e7bd319478eaeed369e8

---
+++ kotlin-ide/tests-common/test/org/jetbrains/kotlin/codegen/ir/AbstractFirScriptCodegenTest.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.codegen.ir
+
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.JVMConfigurationKeys
+
+abstract class AbstractFirScriptCodegenTest : AbstractIrScriptCodegenTest() {
+    override fun updateConfiguration(configuration: CompilerConfiguration) {
+        super.updateConfiguration(configuration)
+        configuration.put(CommonConfigurationKeys.USE_FIR, true)
+        configuration.put(JVMConfigurationKeys.IR, true)
+    }
+}
