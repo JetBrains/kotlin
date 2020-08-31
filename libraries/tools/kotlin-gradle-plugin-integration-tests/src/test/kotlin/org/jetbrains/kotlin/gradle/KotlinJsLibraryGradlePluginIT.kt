4a1bccc1e6c790849426f8cf4b86f65f6d3ee2b2
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-08-31 15:51:40 +0300

    [Gradle, JS] Add test on simple js binary library
    ^KT-41566 fixed

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinJsLibraryGradlePluginIT.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle
+
+import org.jetbrains.kotlin.gradle.plugin.KotlinJsCompilerType
+import org.jetbrains.kotlin.gradle.util.modify
+import org.junit.Test
+
+class KotlinJsIrLibraryGradlePluginIT : BaseGradleIT() {
+    override fun defaultBuildOptions(): BuildOptions =
+        super.defaultBuildOptions().copy(
+            jsIrBackend = true,
+            jsCompilerType = KotlinJsCompilerType.IR
+        )
+
+    @Test
+    fun testSimpleJsBinaryLibrary() {
+        val project = Project("js-library")
+
+        project.setupWorkingDir()
+        project.gradleBuildScript().modify(::transformBuildScriptWithPluginsDsl)
+        project.build("build") {
+            assertSuccessful()
+
+            assertFileExists("build/productionLibrary/js-library.js")
+            assertFileExists("build/productionLibrary/package.json")
+            assertFileExists("build/productionLibrary/main.js")
+        }
+    }
+}
