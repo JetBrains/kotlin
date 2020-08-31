fdb5b27f6b15ae2510efe1da685037a5a9e8f6ef
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-08-31 19:27:45 +0300

    [NI] Fix resolution status for UnstableSmartCastDiagnostic
    RESOLVED_WITH_ERROR was initially added for reproducing OI behavior.
    The intention was to preserve resolution into unstable smart cast and to keep failing candidate.
    However, this idea was abandoned after OI behavior proved inconsistent in cases involving generics.
    
    This commit restores RUNTIME_ERROR status of UnstableSmartCastDiagnostic.
    
    ^KT-41357 Fixed

---
+++ compiler/testData/codegen/box/regressions/kt41357.kt
@@ -0,0 +1,16 @@
+// TARGET_BACKEND: JVM
+// WITH_RUNTIME
+
+open class Foo(val id: Int)
+
+class CustomFoo : Foo(1)
+
+fun test(): Boolean {
+    val fooList = listOf(CustomFoo(), Foo(2))
+    return fooList.first() is CustomFoo && fooList.last().id == 2 // ClassCastException
+}
+
+fun box(): String {
+    check(test())
+    return "OK"
+}
