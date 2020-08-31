ad9c45bc743c1cea5e2f6dde370fa7ed7b5de076
Mathias Quintero <me@quintero.io>
2020-08-31 11:47:45 +0200

    [Scripting] [Type Providers] [Code Generation DSL] Add basic class like structure

---
+++ libraries/scripting/type-providers/src/kotlin/script/experimental/typeProviders/generatedCode/ClassLikeBuilder.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.script.experimental.typeProviders.generatedCode
+
+
+/**
+ * Builder for a provided type. It can implement an interface and have some code inside.
+ */
+@GeneratedCodeDSL
+interface ClassLikeBuilder<Overridden : ClassLikeBuilder<Overridden>> : GeneratedCode.Builder, IdentifiableMember, OverridableBuilder<Overridden> {
+    /**
+     * Implement a generated interface
+     */
+    fun implement(generated: GeneratedInterface)
+}
+
+
+internal class ClassLike(
+    private val interfaces: Set<GeneratedInterface>,
+    private val children: GeneratedCode
+) : InternalGeneratedCode() {
+
+    internal class Builder(
+        override val name: String,
+        private val childrenBuilder: StandardBuilder = StandardBuilder()
+    ) : ClassLikeBuilder<Builder.OverriddenBuilder>, GeneratedCode.Builder by childrenBuilder {
+        private val interfaces = mutableSetOf<GeneratedInterface>()
+
+        class OverriddenBuilder(
+            private val builder: Builder,
+            private val overrideBuilder: OverrideBuilder = OverrideBuilder(builder)
+        ) : ClassLikeBuilder<OverriddenBuilder> by builder {
+            override fun GeneratedCode.unaryPlus() {
+                with(overrideBuilder) { +this@unaryPlus }
+            }
+
+            override fun override(init: OverriddenBuilder.() -> Unit) {
+                init()
+            }
+        }
+
+        override fun implement(generated: GeneratedInterface) {
+            interfaces.add(generated)
+        }
+
+        fun build(): ClassLike {
+            return ClassLike(interfaces, childrenBuilder.build())
+        }
+
+        override fun override(init: OverriddenBuilder.() -> Unit) {
+            OverriddenBuilder(this).init()
+        }
+    }
+
+    override fun GeneratedCodeVisitor.visit(indent: Int) {
+        useImports(interfaces)
+        writeScript {
+            if (interfaces.isNotEmpty()) {
+                append(" : ")
+                appendJoined(interfaces, ", ") { append(it.name) }
+            }
+
+            if (children != GeneratedCode.Empty) {
+                appendLine(" {")
+                visit(children, indent + 1)
+                appendLine("}", indent)
+            } else {
+                appendLine()
+            }
+        }
+    }
+}
---
+++ libraries/scripting/type-providers/src/kotlin/script/experimental/typeProviders/generatedCode/impl/interface.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.script.experimental.typeProviders.generatedCode.impl
+
+import kotlin.script.experimental.typeProviders.generatedCode.*
+
+/**
+ * References an interface that can be implemented
+ */
+interface GeneratedInterface : IdentifiableMember
