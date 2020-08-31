b1b3d327c3e8286e7937a29ee88086df83d942bd
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-08-31 13:58:31 +0300

    [FIR-IDE] Introduce `FirIdeSessionFactory` for creating ide sessions
    Original commit: 011bb0924ae1b06c1e015bc6ec7960a25db9db89

--- kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/sessions/FirIdeLibrarySession.kt
+++ kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/sessions/FirIdeLibrarySession.kt
@@ -5,78 +5,16 @@
 
 package org.jetbrains.kotlin.idea.fir.low.level.api.sessions
 
-import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.kotlin.fir.PrivateSessionConstructor
-import org.jetbrains.kotlin.fir.SessionConfiguration
 import org.jetbrains.kotlin.analyzer.ModuleInfo
-import org.jetbrains.kotlin.fir.java.JavaSymbolProvider
-import org.jetbrains.kotlin.fir.java.deserialization.KotlinDeserializedJvmSymbolsProvider
-import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.providers.impl.FirBuiltinSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.providers.impl.FirCloneableSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.providers.impl.FirCompositeSymbolProvider
-import org.jetbrains.kotlin.fir.resolve.scopes.wrapScopeWithJvmMapped
-import org.jetbrains.kotlin.fir.scopes.KotlinScopeProvider
-import org.jetbrains.kotlin.fir.session.registerCommonComponents
-import org.jetbrains.kotlin.idea.caches.project.ModuleSourceInfo
-import org.jetbrains.kotlin.idea.caches.resolve.IDEPackagePartProvider
-import org.jetbrains.kotlin.idea.fir.low.level.api.IdeSessionComponents
-import org.jetbrains.kotlin.load.java.JavaClassFinderImpl
-import org.jetbrains.kotlin.load.kotlin.VirtualFileFinderFactory
+import org.jetbrains.kotlin.fir.PrivateSessionConstructor
 
 /**
  * [org.jetbrains.kotlin.fir.FirSession] responsible for all libraries analysing module transitively depends on
  */
 @OptIn(PrivateSessionConstructor::class)
-internal class FirIdeLibrariesSession private constructor(
+internal class FirIdeLibrariesSession @PrivateSessionConstructor constructor(
     moduleInfo: ModuleInfo,
     sessionProvider: FirIdeSessionProvider,
     override val scope: GlobalSearchScope
-) : FirIdeSession(moduleInfo, sessionProvider) {
-    companion object {
-        @OptIn(SessionConfiguration::class)
-        fun create(
-            moduleInfo: ModuleSourceInfo,
-            sessionProvider: FirIdeSessionProvider,
-            project: Project,
-        ): FirIdeLibrariesSession {
-            val searchScope = moduleInfo.module.moduleWithLibrariesScope
-            val javaClassFinder = JavaClassFinderImpl().apply {
-                setProjectInstance(project)
-                setScope(searchScope)
-            }
-            val packagePartProvider = IDEPackagePartProvider(searchScope)
-
-            val kotlinClassFinder = VirtualFileFinderFactory.getInstance(project).create(searchScope)
-            return FirIdeLibrariesSession(moduleInfo, sessionProvider, searchScope).apply {
-                registerCommonComponents()
-
-                val javaSymbolProvider = JavaSymbolProvider(this, sessionProvider.project, searchScope)
-
-                val kotlinScopeProvider = KotlinScopeProvider(::wrapScopeWithJvmMapped)
-                register(IdeSessionComponents::class, IdeSessionComponents.create(this))
-                register(
-                    FirSymbolProvider::class,
-                    FirCompositeSymbolProvider(
-                        this,
-                        listOf(
-                            KotlinDeserializedJvmSymbolsProvider(
-                                this,
-                                project,
-                                packagePartProvider,
-                                javaSymbolProvider,
-                                kotlinClassFinder,
-                                javaClassFinder,
-                                kotlinScopeProvider
-                            ),
-                            FirBuiltinSymbolProvider(this, kotlinScopeProvider),
-                            FirCloneableSymbolProvider(this, kotlinScopeProvider),
-                            javaSymbolProvider,
-                        )
-                    )
-                )
-            }
-        }
-    }
-}
+) : FirIdeSession(moduleInfo, sessionProvider)
