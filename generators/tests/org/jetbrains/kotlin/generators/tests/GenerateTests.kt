87898021a1f85dcddc96f1f4d735ae97c2151a81
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-08-31 18:02:27 +0300

    FIR IDE: introduce multi module lazy resolve tests

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -88,6 +88,7 @@ import org.jetbrains.kotlin.idea.filters.AbstractKotlinExceptionFilterTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirLazyResolveTest
 import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirMultiModuleResolveTest
 import org.jetbrains.kotlin.idea.fir.AbstractKtDeclarationAndFirDeclarationEqualityChecker
+import org.jetbrains.kotlin.idea.fir.low.level.api.AbstractFirMultiModuleLazyResolveTest
 import org.jetbrains.kotlin.idea.folding.AbstractKotlinFoldingTest
 import org.jetbrains.kotlin.idea.frontend.api.fir.AbstractResolveCallTest
 import org.jetbrains.kotlin.idea.frontend.api.scopes.AbstractMemberScopeByFqNameTest
@@ -1012,6 +1013,12 @@ fun main(args: Array<String>) {
 
             testClass<AbstractFirLazyResolveTest> {
                 model("fir/lazyResolve", extension = "test", singleClass = true, filenameStartsLowerCase = true)
+            }
+        }
+
+        testGroup("idea/idea-frontend-fir/idea-fir-low-level-api/tests", "idea/idea-frontend-fir/idea-fir-low-level-api/testdata") {
+            testClass<AbstractFirMultiModuleLazyResolveTest> {
+                model("multiModuleLazyResolve", recursive = false, extension = null)
             }
         }
 
---
+++ idea/idea-frontend-fir/idea-fir-low-level-api/testdata/multiModuleLazyResolve/whenWithHeiarchy/dependency/dependency.kt
@@ -0,0 +1,8 @@
+interface A
+
+interface X : A
+interface Y : A
+
+interface F : Y, X
+interface G : Y, X
+
