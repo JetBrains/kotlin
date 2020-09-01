c0e9f05921794bb1489b09b2e69772300ddda412
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-09-01 19:12:48 +0300

    FIR: make FirTypeParameterScope to be name aware

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/FirTypeParameterScope.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/FirTypeParameterScope.kt
@@ -10,7 +10,7 @@ import org.jetbrains.kotlin.fir.resolve.substitution.ConeSubstitutor
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassifierSymbol
 import org.jetbrains.kotlin.name.Name
 
-abstract class FirTypeParameterScope : FirScope() {
+abstract class FirTypeParameterScope : FirScope(), FirContainingNamesAwareScope {
     abstract val typeParameters: Map<Name, List<FirTypeParameter>>
 
     override fun processClassifiersByNameWithSubstitution(
@@ -21,4 +21,8 @@ abstract class FirTypeParameterScope : FirScope() {
 
         matchedTypeParameters.forEach { processor(it.symbol, ConeSubstitutor.Empty) }
     }
+
+    override fun getCallableNames(): Set<Name> = emptySet()
+
+    override fun getClassifierNames(): Set<Name> = typeParameters.keys
 }
