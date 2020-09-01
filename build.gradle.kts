5f37f9b065eb88d843b604dcffcac89dbdb372df
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-09-01 19:00:27 +0700

    Update Kotlin/Native: 1.4.10-rc1-12

--- build.gradle.kts
+++ build.gradle.kts
@@ -187,7 +187,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.4"
+    extra["versions.kotlin-native"] = "1.4.10-rc1-12"
 }
 
 val intellijUltimateEnabled by extra(project.kotlinBuildProperties.intellijUltimateEnabled)
