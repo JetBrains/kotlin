c82d9ea9f4993ccf322f8fa30c2de798d75dd72f
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-09-02 15:07:47 +0300

    Remove dependency on :compiler:frontend.java from :compiler:fir:java

--- compiler/fir/java/build.gradle.kts
+++ compiler/fir/java/build.gradle.kts
@@ -5,12 +5,14 @@ plugins {
 }
 
 dependencies {
+    api(project(":core:compiler.common.jvm"))
+    api(project(":core:metadata.jvm"))
+    api(project(":compiler:resolution.common.jvm"))
     api(project(":compiler:frontend.common"))
     api(project(":compiler:fir:resolve"))
     api(project(":compiler:fir:checkers"))
     api(project(":compiler:fir:jvm"))
     api(project(":compiler:fir:fir-deserialization"))
-    implementation(project(":compiler:frontend.java"))
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
 }
