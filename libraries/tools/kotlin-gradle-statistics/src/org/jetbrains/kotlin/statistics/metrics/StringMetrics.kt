05d3e48564ddf3361090491d9055dfe431ca02a2
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2020-09-03 14:45:20 +0300

    Implemented collecting statistics about JS target mode

--- libraries/tools/kotlin-gradle-statistics/src/org/jetbrains/kotlin/statistics/metrics/StringMetrics.kt
+++ libraries/tools/kotlin-gradle-statistics/src/org/jetbrains/kotlin/statistics/metrics/StringMetrics.kt
@@ -41,6 +41,7 @@ enum class StringMetrics(val type: StringOverridePolicy, val anonymization: Stri
     KOTLIN_LANGUAGE_VERSION(OVERRIDE, COMPONENT_VERSION),
     KOTLIN_API_VERSION(OVERRIDE, COMPONENT_VERSION),
     JS_GENERATE_EXECUTABLE_DEFAULT(CONCAT, SAFE),
+    JS_TARGET_MODE(CONCAT, SAFE),
 
     // Compiler parameters
     JVM_DEFAULTS(CONCAT, SAFE),
