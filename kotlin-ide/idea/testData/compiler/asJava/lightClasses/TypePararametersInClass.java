01b2dbb90fb844b8af9a40d62552b5cbaabe9efe
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-09-04 11:58:32 +0300

    [ULC] Add annotations for PsiTypes in UltraLight classes
    WIP on KT-41148, KT-41406, KT-41420
    
    Original commit: e08763f3dd5c358c2150fe966ee6536b4cfc5c82

--- kotlin-ide/idea/testData/compiler/asJava/lightClasses/TypePararametersInClass.java
+++ kotlin-ide/idea/testData/compiler/asJava/lightClasses/TypePararametersInClass.java
@@ -6,7 +6,7 @@ public abstract class A /* A*/<T extends A<T>>  extends B<java.util.Collection<?
 public class Inner /* A.Inner*/<D>  extends B<java.util.Collection<? extends T>> implements C<D> {
   public  Inner();//  .ctor()
 
-}public final class Inner2 /* A.Inner2*/<X>  extends A<T>.Inner<X> implements C<X> {
+}public final class Inner2 /* A.Inner2*/<X>  extends A.Inner<X> implements C<X> {
   public  Inner2();//  .ctor()
 
 }}
