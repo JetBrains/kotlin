5bae776d6b2bb50f734011e8a45ca948b51326dd
Sebastian Kaspari <s.kaspari@gmail.com>
2020-09-07 12:17:06 +0200

    ParameterizedClassConstructor: Remove unnecessary use function.

--- idea/testData/parameterInfo/typeArguments/ParameterizedClassConstructor.kt
+++ idea/testData/parameterInfo/typeArguments/ParameterizedClassConstructor.kt
@@ -6,8 +6,6 @@ class RC : Runnable {
     override fun run() {}
 }
 
-fun use(p: Parameterized<AC, CC, RC>) {}
-
 class UsageClass : Parameterized<AC, CC<caret>, RC>()
 
 //Text: (A, <highlight>C : Cloneable</highlight>, R : Runnable), Disabled: false, Strikeout: false, Green: false
