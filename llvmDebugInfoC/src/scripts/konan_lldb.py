a7e228b16fe0ab93a785663b73f92ceafd266221
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2020-09-07 13:34:28 +0200

    Revert "[debugger][bindings] don't enter to recursion on type info calculation (KT-41291)"
    This reverts commit b604b58223f5d45622eb9a75f4c541ff3a879efc.

--- llvmDebugInfoC/src/scripts/konan_lldb.py
+++ llvmDebugInfoC/src/scripts/konan_lldb.py
@@ -79,7 +79,7 @@ def is_string_or_array(value):
 def type_info(value):
     """This method checks self-referencing of pointer of first member of TypeInfo including case when object has an
     meta-object pointed by TypeInfo. Two lower bits are reserved for memory management needs see runtime/src/main/cpp/Memory.h."""
-    if value.GetTypeName() != "struct ObjHeader *":
+    if str(value.type) != "struct ObjHeader *":
         return False
     expr = "*(void **)((uintptr_t)(*(void**){0:#x}) & ~0x3) == **(void***)((uintptr_t)(*(void**){0:#x}) & ~0x3) ? *(void **)((uintptr_t)(*(void**){0:#x}) & ~0x3) : (void *)0".format(value.unsigned)
     result = evaluate(expr)
