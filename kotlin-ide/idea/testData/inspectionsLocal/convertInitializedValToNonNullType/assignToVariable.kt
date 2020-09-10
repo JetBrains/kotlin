19265000f331c87abb0aaee7a0f4979dff19f128
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2020-09-10 13:32:07 +0200

    Revert back Inspection: convert initialized val to non-null type
    Relates to ^KT-35757
    Relates to ^KT-19321
    
    Original commit: 6f03e42ef7632c2087a7874ccf0ec00a23b4038c

--- kotlin-ide/idea/testData/inspectionsLocal/convertInitializedValToNonNullType/assignToVariable.kt
+++
@@ -1,4 +0,0 @@
-fun foo() {
-    val s0: String = ""
-    val s1: String?<caret> = s0
-}
--- kotlin-ide/idea/testData/inspectionsLocal/convertInitializedValToNonNullType/negative.kt
+++
@@ -1,7 +0,0 @@
-// PROBLEM: none
-fun foo() {
-    val s1: String? = bar()
-    val s2: String?<caret> = null
-}
-
-fun bar(): String? = ""
