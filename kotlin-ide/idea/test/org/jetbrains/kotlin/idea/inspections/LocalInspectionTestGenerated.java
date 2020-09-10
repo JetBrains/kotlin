71f7c656c630ed118bc0be6c414860cb9c552c43
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2020-09-10 16:02:20 +0200

    Revert back Inspection: convert initialized val to non-null type
    Relates to ^KT-35757
    Relates to ^KT-19321

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/inspections/LocalInspectionTestGenerated.java
@@ -3265,34 +3265,6 @@ public abstract class LocalInspectionTestGenerated extends AbstractLocalInspecti
     }
 
     @RunWith(JUnit3RunnerWithInners.class)
-    @TestMetadata("testData/inspectionsLocal/convertInitializedValToNonNullType")
-    public static class ConvertInitializedValToNonNullType extends AbstractLocalInspectionTest {
-        private void runTest(String testDataFilePath) throws Exception {
-            KotlinTestUtils.runTest(this::doTest, this, testDataFilePath);
-        }
-
-        @TestMetadata("assignToMethodCall.kt")
-        public void testAssignToMethodCall() throws Exception {
-            runTest("testData/inspectionsLocal/convertInitializedValToNonNullType/assignToMethodCall.kt");
-        }
-
-        @TestMetadata("assignToVariable.kt")
-        public void testAssignToVariable() throws Exception {
-            runTest("testData/inspectionsLocal/convertInitializedValToNonNullType/assignToVariable.kt");
-        }
-
-        @TestMetadata("basic.kt")
-        public void testBasic() throws Exception {
-            runTest("testData/inspectionsLocal/convertInitializedValToNonNullType/basic.kt");
-        }
-
-        @TestMetadata("negative.kt")
-        public void testNegative() throws Exception {
-            runTest("testData/inspectionsLocal/convertInitializedValToNonNullType/negative.kt");
-        }
-    }
-
-    @RunWith(JUnit3RunnerWithInners.class)
     @TestMetadata("testData/inspectionsLocal/convertNaNEquality")
     public static class ConvertNaNEquality extends AbstractLocalInspectionTest {
         private void runTest(String testDataFilePath) throws Exception {
