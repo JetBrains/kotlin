12c6cfcec260a28636c9de116805a50d3f06db49
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-09-13 20:56:05 +0700

    [tests] FindUsagesTestGenerated$PropertyFiles.testPropertyUsages: update testData

--- kotlin-ide/idea/testData/findUsages/propertyFiles/propertyUsages.1.kt
+++ kotlin-ide/idea/testData/findUsages/propertyFiles/propertyUsages.1.kt
@@ -1,16 +1,16 @@
 import org.jetbrains.annotations.PropertyKey
 
-public fun message(@PropertyKey(resourceBundle = "propertyUsages.0") key: String) = key
+fun message(@PropertyKey(resourceBundle = "propertyUsages.0") key: String) = key
 
-public fun String.infixMessage(@PropertyKey(resourceBundle = "propertyUsages.0") key: String) = key
+infix fun String.infixMessage(@PropertyKey(resourceBundle = "propertyUsages.0") key: String) = key
 
-public fun @receiver:PropertyKey(resourceBundle = "propertyUsages.0") String.infixMessage2(s: String) = this
+infix fun @receiver:PropertyKey(resourceBundle = "propertyUsages.0") String.infixMessage2(s: String) = this
 
-public fun @receiver:PropertyKey(resourceBundle = "propertyUsages.0") String.unaryMinus() = this
+operator fun @receiver:PropertyKey(resourceBundle = "propertyUsages.0") String.unaryMinus() = this
 
-public fun Int.get(@PropertyKey(resourceBundle = "propertyUsages.0") key: String) = this
+operator fun Int.get(@PropertyKey(resourceBundle = "propertyUsages.0") key: String) = this
 
-public fun @receiver:PropertyKey(resourceBundle = "propertyUsages.0") String.get(s: String) = this
+operator fun @receiver:PropertyKey(resourceBundle = "propertyUsages.0") String.get(s: String) = this
 
 fun test() {
     @PropertyKey(resourceBundle = "propertyUsages.0") val s1 = "foo.bar"
