28c47a0f9ba1a3c5750d62b0d032110fd7f1e5dc
Alexander Udalov <alexander.udalov@jetbrains.com>
2020-09-14 22:19:27 +0200

    JVM IR: unmute stepping test noParameterExtensionLambdaArgumentCallInInline3.kt
    Bytecode generated by JVM IR ensures that on multiline expressions,
    every call is stepped over, which seems better than in the old backend.
    
    Also remove kt34905.kt since it duplicates this test case.
    
    Original commit: 86cc5840ed3f487a6a1669bc5cd969b2f5eca932

--- kotlin-ide/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinSteppingTestGenerated.java
+++ kotlin-ide/jvm-debugger/test/test/org/jetbrains/kotlin/idea/debugger/test/IrKotlinSteppingTestGenerated.java
@@ -462,11 +462,6 @@ public class IrKotlinSteppingTestGenerated extends AbstractIrKotlinSteppingTest 
             runTest("idea/jvm-debugger/jvm-debugger-test/testData/stepping/stepOver/kt24343.kt");
         }
 
-        @TestMetadata("kt34905.kt")
-        public void testKt34905() throws Exception {
-            runTest("idea/jvm-debugger/jvm-debugger-test/testData/stepping/stepOver/kt34905.kt");
-        }
-
         @TestMetadata("lambdaToInlineFold.kt")
         public void testLambdaToInlineFold() throws Exception {
             runTest("idea/jvm-debugger/jvm-debugger-test/testData/stepping/stepOver/lambdaToInlineFold.kt");
