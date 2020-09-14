natario1/kotlin-native

[IR] Rewrote debug output using Context.log

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/Context.kt
@@ -429,3 +429,12 @@ private fun MemberScope.getContributedClassifier(name: String) =
 
 private fun MemberScope.getContributedFunctions(name: String) =
         this.getContributedFunctions(Name.identifier(name), NoLookupLocation.FROM_BUILTINS)
+
+internal class LogBuilder(val context: Context) {
+    operator fun String.unaryPlus() = context.log { this }
+}
+
+internal fun Context.buildLog(messageBuilder: LogBuilder.() -> Unit) {
+    if (!inVerbosePhase) return
+    with(LogBuilder(this)) { messageBuilder() }
+}

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/descriptors/ClassLayoutBuilder.kt
@@ -262,19 +262,12 @@ internal class GlobalHierarchyAnalysis(val context: Context, val irModule: IrMod
 }
 
 internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, val isLowered: Boolean) {
-    private val DEBUG = 0
-
-    private inline fun DEBUG_OUTPUT(severity: Int, block: () -> Unit) {
-        if (DEBUG > severity) block()
-    }
-
     val vtableEntries: List<OverriddenFunctionInfo> by lazy {
-
         assert(!irClass.isInterface)
 
-        DEBUG_OUTPUT(0) {
-            println()
-            println("BUILDING vTable for ${irClass.render()}")
+        context.buildLog {
+            +""
+            +"BUILDING vTable for ${irClass.render()}"
         }
 
         val superVtableEntries = if (irClass.isSpecialClassWithNoSupertypes()) {
@@ -288,17 +281,17 @@ internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, va
         val newVtableSlots = mutableListOf<OverriddenFunctionInfo>()
         val overridenVtableSlots = mutableMapOf<IrSimpleFunction, OverriddenFunctionInfo>()
 
-        DEBUG_OUTPUT(0) {
-            println()
-            println("SUPER vTable:")
-            superVtableEntries.forEach { println("    ${it.overriddenFunction.render()} -> ${it.function.render()}") }
+        context.buildLog {
+            +""
+            +"SUPER vTable:"
+            superVtableEntries.forEach { +"    ${it.overriddenFunction.render()} -> ${it.function.render()}" }
 
-            println()
-            println("METHODS:")
-            methods.forEach { println("    ${it.render()}") }
+            +""
+            +"METHODS:"
+            methods.forEach { +"    ${it.render()}" }
 
-            println()
-            println("BUILDING INHERITED vTable")
+            +""
+            +"BUILDING INHERITED vTable"
         }
 
         val superVtableMap = superVtableEntries.groupBy { it.function }
@@ -316,13 +309,9 @@ internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, va
         }
         val inheritedVtableSlots = superVtableEntries.map { superMethod ->
             overridenVtableSlots[superMethod.overriddenFunction]?.also {
-                DEBUG_OUTPUT(0) {
-                    println("Taking overridden ${superMethod.overriddenFunction.render()} -> ${it.function.render()}")
-                }
+                context.log { "Taking overridden ${superMethod.overriddenFunction.render()} -> ${it.function.render()}" }
             } ?: superMethod.also {
-                DEBUG_OUTPUT(0) {
-                    println("Taking super ${superMethod.overriddenFunction.render()} -> ${superMethod.function.render()}")
-                }
+                context.log { "Taking super ${superMethod.overriddenFunction.render()} -> ${superMethod.function.render()}" }
             }
         }
 
@@ -336,15 +325,15 @@ internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, va
             .distinctBy { it.function to it.bridgeDirections }
             .filter { it.function.isOverridable }
 
-        DEBUG_OUTPUT(0) {
-            println()
-            println("INHERITED vTable slots:")
-            inheritedVtableSlots.forEach { println("    ${it.overriddenFunction.render()} -> ${it.function.render()}") }
+        context.buildLog {
+            +""
+            +"INHERITED vTable slots:"
+            inheritedVtableSlots.forEach { +"    ${it.overriddenFunction.render()} -> ${it.function.render()}" }
 
-            println()
-            println("MY OWN vTable slots:")
-            filteredNewVtableSlots.forEach { println("    ${it.overriddenFunction.render()} -> ${it.function.render()} ${it.function}") }
-            println("DONE vTable for ${irClass.render()}")
+            +""
+            +"MY OWN vTable slots:"
+            filteredNewVtableSlots.forEach { +"    ${it.overriddenFunction.render()} -> ${it.function.render()} ${it.function}" }
+            +"DONE vTable for ${irClass.render()}"
         }
 
         inheritedVtableSlots + filteredNewVtableSlots.sortedBy { it.overriddenFunction.uniqueId }

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/DFGBuilder.kt
@@ -177,12 +177,6 @@ internal class ModuleDFG(val functions: Map<DataFlowIR.FunctionSymbol, DataFlowI
 
 internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFragment) {
 
-    private val DEBUG = 0
-
-    private inline fun DEBUG_OUTPUT(severity: Int, block: () -> Unit) {
-        if (DEBUG > severity) block()
-    }
-
     private val TAKE_NAMES = true // Take fqNames for all functions and types (for debug purposes).
 
     private inline fun takeName(block: () -> String) = if (TAKE_NAMES) block() else null
@@ -211,9 +205,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                 assert (body != null || declaration.constructedClass.isNonGeneratedAnnotation()) {
                     "Non-annotation class constructor has empty body"
                 }
-                DEBUG_OUTPUT(0) {
-                    println("Analysing function ${declaration.descriptor}")
-                    println("IR: ${ir2stringWhole(declaration)}")
+                context.buildLog {
+                    +"Analysing function ${declaration.descriptor}"
+                    +"IR: ${ir2stringWhole(declaration)}"
                 }
                 analyze(declaration, body)
             }
@@ -224,9 +218,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                     // External function or intrinsic.
                     symbolTable.mapFunction(declaration)
                 } else {
-                    DEBUG_OUTPUT(0) {
-                        println("Analysing function ${declaration.descriptor}")
-                        println("IR: ${ir2stringWhole(declaration)}")
+                    context.buildLog {
+                        +"Analysing function ${declaration.descriptor}"
+                        +"IR: ${ir2stringWhole(declaration)}"
                     }
                     analyze(declaration, body)
                 }
@@ -235,9 +229,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
             override fun visitField(declaration: IrField) {
                 if (declaration.parent is IrFile)
                     declaration.initializer?.let {
-                        DEBUG_OUTPUT(0) {
-                            println("Analysing global field ${declaration.descriptor}")
-                            println("IR: ${ir2stringWhole(declaration)}")
+                        context.buildLog {
+                            +"Analysing global field ${declaration.descriptor}"
+                            +"IR: ${ir2stringWhole(declaration)}"
                         }
                         analyze(declaration, IrSetFieldImpl(it.startOffset, it.endOffset, declaration.symbol, null,
                                 it.expression, context.irBuiltIns.unitType))
@@ -249,29 +243,25 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                 val visitor = ElementFinderVisitor()
                 body?.acceptVoid(visitor)
 
-                DEBUG_OUTPUT(0) {
-                    println("FIRST PHASE")
-                    visitor.variableValues.elementData.forEach { t, u ->
-                        println("VAR $t [LOOP ${u.loop}]:")
-                        u.values.forEach {
-                            println("    ${ir2stringWhole(it)}")
-                        }
+                context.buildLog {
+                    +"FIRST PHASE"
+                    visitor.variableValues.elementData.forEach { (t, u) ->
+                        +"VAR $t [LOOP ${u.loop}]:"
+                        u.values.forEach { +"    ${ir2stringWhole(it)}" }
                     }
                     visitor.expressions.forEach { t ->
-                        println("EXP [LOOP ${t.value}] ${ir2stringWhole(t.key)}")
+                        +"EXP [LOOP ${t.value}] ${ir2stringWhole(t.key)}"
                     }
                 }
 
                 // Compute transitive closure of possible values for variables.
                 visitor.variableValues.computeClosure()
 
-                DEBUG_OUTPUT(0) {
-                    println("SECOND PHASE")
-                    visitor.variableValues.elementData.forEach { t, u ->
-                        println("VAR $t [LOOP ${u.loop}]:")
-                        u.values.forEach {
-                            println("    ${ir2stringWhole(it)}")
-                        }
+                context.buildLog {
+                    +"SECOND PHASE"
+                    visitor.variableValues.elementData.forEach { (t, u) ->
+                        +"VAR $t [LOOP ${u.loop}]:"
+                        u.values.forEach { +"    ${ir2stringWhole(it)}" }
                     }
                 }
 
@@ -279,27 +269,28 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                         declaration, visitor.expressions, visitor.parentLoops, visitor.returnValues,
                         visitor.thrownValues, visitor.catchParameters).build()
 
-                DEBUG_OUTPUT(0) {
-                    function.debugOutput()
+                context.buildLog {
+                    +function.debugString()
+                    +""
                 }
 
-                functions.put(function.symbol, function)
+                functions[function.symbol] = function
             }
         }, data = null)
 
-        DEBUG_OUTPUT(1) {
-            println("SYMBOL TABLE:")
+        context.buildLog {
+            +"SYMBOL TABLE:"
             symbolTable.classMap.forEach { irClass, type ->
-                println("    DESCRIPTOR: ${irClass.descriptor}")
-                println("    TYPE: $type")
+                +"    DESCRIPTOR: ${irClass.descriptor}"
+                +"    TYPE: $type"
                 if (type !is DataFlowIR.Type.Declared)
                     return@forEach
-                println("        SUPER TYPES:")
-                type.superTypes.forEach { println("            $it") }
-                println("        VTABLE:")
-                type.vtable.forEach { println("            $it") }
-                println("        ITABLE:")
-                type.itable.forEach { println("            ${it.key} -> ${it.value}") }
+                +"        SUPER TYPES:"
+                type.superTypes.forEach { +"            $it" }
+                +"        VTABLE:"
+                type.vtable.forEach { +"            $it" }
+                +"        ITABLE:"
+                type.itable.forEach { +"            ${it.key} -> ${it.value}" }
             }
         }
 
@@ -618,9 +609,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                 return variables[valueDeclaration]!!
             }
             return nodes.getOrPut(expression) {
-                DEBUG_OUTPUT(0) {
-                    println("Converting expression")
-                    println(ir2stringWhole(expression))
+                context.buildLog {
+                    +"Converting expression"
+                    +ir2stringWhole(expression)
                 }
                 val values = mutableListOf<IrExpression>()
                 val edges = mutableListOf<DataFlowIR.Edge>()

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/LocalEscapeAnalysis.kt
@@ -8,15 +8,10 @@ package org.jetbrains.kotlin.backend.konan.optimizations
 import org.jetbrains.kotlin.backend.konan.llvm.Lifetime
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.backend.konan.Context
+import org.jetbrains.kotlin.backend.konan.buildLog
 import org.jetbrains.kotlin.backend.konan.descriptors.isArrayWithFixedSizeItems
 
 internal object LocalEscapeAnalysis {
-    private val DEBUG = 0
-
-    private inline fun DEBUG_OUTPUT(severity: Int, block: () -> Unit) {
-        if (DEBUG > severity) block()
-    }
-
     private enum class EscapeState {
         GLOBAL_ESCAPE,       // escape through global reference
         ARG_ESCAPE,          // escapes through function arguments, return or throw
@@ -187,7 +182,7 @@ internal object LocalEscapeAnalysis {
                 }
                 ir?.let {
                     lifetimes.put(it, Lifetime.STACK)
-                    DEBUG_OUTPUT(3) { println("${ir} does not escape") }
+                    context.log { "$ir does not escape" }
                 }
             }
         }
@@ -195,18 +190,18 @@ internal object LocalEscapeAnalysis {
 
     fun analyze(context: Context, moduleDFG: ModuleDFG, lifetimes: MutableMap<IrElement, Lifetime>) {
         moduleDFG.functions.forEach { (name, function) ->
-            DEBUG_OUTPUT(5) {
-                println("===============================================")
-                println("Visiting $name")
-                println("DATA FLOW IR:")
-                function.debugOutput()
+            context.buildLog {
+                +"==============================================="
+                +"Visiting $name"
+                +"DATA FLOW IR:"
+                +function.debugString()
             }
             FunctionAnalyzer(function, context).analyze(lifetimes)
         }
     }
 
     fun computeLifetimes(context: Context, moduleDFG: ModuleDFG, lifetimes: MutableMap<IrElement, Lifetime>) {
-        DEBUG_OUTPUT(1) { println("In local EA") }
+        context.log { "In local EA" }
         assert(lifetimes.isEmpty())
         analyze(context, moduleDFG, lifetimes)
     }

JetBrains/kotlin

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/Context.kt
@@ -43,9 +43,6 @@ import org.jetbrains.kotlin.backend.konan.llvm.coverage.CoverageManager
 import org.jetbrains.kotlin.ir.descriptors.WrappedSimpleFunctionDescriptor
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
 import org.jetbrains.kotlin.ir.symbols.impl.IrFieldSymbolImpl
-import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.types.defaultType
-import org.jetbrains.kotlin.ir.types.makeNullable
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.konan.library.KonanLibraryLayout
 import org.jetbrains.kotlin.library.SerializedIrModule
@@ -429,3 +426,12 @@ private fun MemberScope.getContributedClassifier(name: String) =
 
 private fun MemberScope.getContributedFunctions(name: String) =
         this.getContributedFunctions(Name.identifier(name), NoLookupLocation.FROM_BUILTINS)
+
+internal class ContextLogger(val context: Context) {
+    operator fun String.unaryPlus() = context.log { this }
+}
+
+internal fun Context.logMultiple(messageBuilder: ContextLogger.() -> Unit) {
+    if (!inVerbosePhase) return
+    with(ContextLogger(this)) { messageBuilder() }
+}

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/descriptors/ClassLayoutBuilder.kt
@@ -262,19 +262,12 @@ internal class GlobalHierarchyAnalysis(val context: Context, val irModule: IrMod
 }
 
 internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, val isLowered: Boolean) {
-    private val DEBUG = 0
-
-    private inline fun DEBUG_OUTPUT(severity: Int, block: () -> Unit) {
-        if (DEBUG > severity) block()
-    }
-
     val vtableEntries: List<OverriddenFunctionInfo> by lazy {
-
         assert(!irClass.isInterface)
 
-        DEBUG_OUTPUT(0) {
-            println()
-            println("BUILDING vTable for ${irClass.render()}")
+        context.logMultiple {
+            +""
+            +"BUILDING vTable for ${irClass.render()}"
         }
 
         val superVtableEntries = if (irClass.isSpecialClassWithNoSupertypes()) {
@@ -288,17 +281,17 @@ internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, va
         val newVtableSlots = mutableListOf<OverriddenFunctionInfo>()
         val overridenVtableSlots = mutableMapOf<IrSimpleFunction, OverriddenFunctionInfo>()
 
-        DEBUG_OUTPUT(0) {
-            println()
-            println("SUPER vTable:")
-            superVtableEntries.forEach { println("    ${it.overriddenFunction.render()} -> ${it.function.render()}") }
+        context.logMultiple {
+            +""
+            +"SUPER vTable:"
+            superVtableEntries.forEach { +"    ${it.overriddenFunction.render()} -> ${it.function.render()}" }
 
-            println()
-            println("METHODS:")
-            methods.forEach { println("    ${it.render()}") }
+            +""
+            +"METHODS:"
+            methods.forEach { +"    ${it.render()}" }
 
-            println()
-            println("BUILDING INHERITED vTable")
+            +""
+            +"BUILDING INHERITED vTable"
         }
 
         val superVtableMap = superVtableEntries.groupBy { it.function }
@@ -316,13 +309,9 @@ internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, va
         }
         val inheritedVtableSlots = superVtableEntries.map { superMethod ->
             overridenVtableSlots[superMethod.overriddenFunction]?.also {
-                DEBUG_OUTPUT(0) {
-                    println("Taking overridden ${superMethod.overriddenFunction.render()} -> ${it.function.render()}")
-                }
+                context.log { "Taking overridden ${superMethod.overriddenFunction.render()} -> ${it.function.render()}" }
             } ?: superMethod.also {
-                DEBUG_OUTPUT(0) {
-                    println("Taking super ${superMethod.overriddenFunction.render()} -> ${superMethod.function.render()}")
-                }
+                context.log { "Taking super ${superMethod.overriddenFunction.render()} -> ${superMethod.function.render()}" }
             }
         }
 
@@ -336,15 +325,15 @@ internal class ClassLayoutBuilder(val irClass: IrClass, val context: Context, va
             .distinctBy { it.function to it.bridgeDirections }
             .filter { it.function.isOverridable }
 
-        DEBUG_OUTPUT(0) {
-            println()
-            println("INHERITED vTable slots:")
-            inheritedVtableSlots.forEach { println("    ${it.overriddenFunction.render()} -> ${it.function.render()}") }
+        context.logMultiple {
+            +""
+            +"INHERITED vTable slots:"
+            inheritedVtableSlots.forEach { +"    ${it.overriddenFunction.render()} -> ${it.function.render()}" }
 
-            println()
-            println("MY OWN vTable slots:")
-            filteredNewVtableSlots.forEach { println("    ${it.overriddenFunction.render()} -> ${it.function.render()} ${it.function}") }
-            println("DONE vTable for ${irClass.render()}")
+            +""
+            +"MY OWN vTable slots:"
+            filteredNewVtableSlots.forEach { +"    ${it.overriddenFunction.render()} -> ${it.function.render()} ${it.function}" }
+            +"DONE vTable for ${irClass.render()}"
         }
 
         inheritedVtableSlots + filteredNewVtableSlots.sortedBy { it.overriddenFunction.uniqueId }

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/DFGBuilder.kt
@@ -13,7 +13,6 @@ import org.jetbrains.kotlin.backend.common.pop
 import org.jetbrains.kotlin.backend.common.push
 import org.jetbrains.kotlin.backend.konan.*
 import org.jetbrains.kotlin.backend.konan.descriptors.allOverriddenFunctions
-import org.jetbrains.kotlin.backend.konan.descriptors.target
 import org.jetbrains.kotlin.backend.konan.ir.*
 import org.jetbrains.kotlin.backend.konan.llvm.functionName
 import org.jetbrains.kotlin.backend.konan.llvm.localHash
@@ -177,12 +176,6 @@ internal class ModuleDFG(val functions: Map<DataFlowIR.FunctionSymbol, DataFlowI
 
 internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFragment) {
 
-    private val DEBUG = 0
-
-    private inline fun DEBUG_OUTPUT(severity: Int, block: () -> Unit) {
-        if (DEBUG > severity) block()
-    }
-
     private val TAKE_NAMES = true // Take fqNames for all functions and types (for debug purposes).
 
     private inline fun takeName(block: () -> String) = if (TAKE_NAMES) block() else null
@@ -211,9 +204,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                 assert (body != null || declaration.constructedClass.isNonGeneratedAnnotation()) {
                     "Non-annotation class constructor has empty body"
                 }
-                DEBUG_OUTPUT(0) {
-                    println("Analysing function ${declaration.descriptor}")
-                    println("IR: ${ir2stringWhole(declaration)}")
+                context.logMultiple {
+                    +"Analysing function ${declaration.descriptor}"
+                    +"IR: ${ir2stringWhole(declaration)}"
                 }
                 analyze(declaration, body)
             }
@@ -224,9 +217,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                     // External function or intrinsic.
                     symbolTable.mapFunction(declaration)
                 } else {
-                    DEBUG_OUTPUT(0) {
-                        println("Analysing function ${declaration.descriptor}")
-                        println("IR: ${ir2stringWhole(declaration)}")
+                    context.logMultiple {
+                        +"Analysing function ${declaration.descriptor}"
+                        +"IR: ${ir2stringWhole(declaration)}"
                     }
                     analyze(declaration, body)
                 }
@@ -235,9 +228,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
             override fun visitField(declaration: IrField) {
                 if (declaration.parent is IrFile)
                     declaration.initializer?.let {
-                        DEBUG_OUTPUT(0) {
-                            println("Analysing global field ${declaration.descriptor}")
-                            println("IR: ${ir2stringWhole(declaration)}")
+                        context.logMultiple {
+                            +"Analysing global field ${declaration.descriptor}"
+                            +"IR: ${ir2stringWhole(declaration)}"
                         }
                         analyze(declaration, IrSetFieldImpl(it.startOffset, it.endOffset, declaration.symbol, null,
                                 it.expression, context.irBuiltIns.unitType))
@@ -249,29 +242,25 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                 val visitor = ElementFinderVisitor()
                 body?.acceptVoid(visitor)
 
-                DEBUG_OUTPUT(0) {
-                    println("FIRST PHASE")
-                    visitor.variableValues.elementData.forEach { t, u ->
-                        println("VAR $t [LOOP ${u.loop}]:")
-                        u.values.forEach {
-                            println("    ${ir2stringWhole(it)}")
-                        }
+                context.logMultiple {
+                    +"FIRST PHASE"
+                    visitor.variableValues.elementData.forEach { (t, u) ->
+                        +"VAR $t [LOOP ${u.loop}]:"
+                        u.values.forEach { +"    ${ir2stringWhole(it)}" }
                     }
                     visitor.expressions.forEach { t ->
-                        println("EXP [LOOP ${t.value}] ${ir2stringWhole(t.key)}")
+                        +"EXP [LOOP ${t.value}] ${ir2stringWhole(t.key)}"
                     }
                 }
 
                 // Compute transitive closure of possible values for variables.
                 visitor.variableValues.computeClosure()
 
-                DEBUG_OUTPUT(0) {
-                    println("SECOND PHASE")
-                    visitor.variableValues.elementData.forEach { t, u ->
-                        println("VAR $t [LOOP ${u.loop}]:")
-                        u.values.forEach {
-                            println("    ${ir2stringWhole(it)}")
-                        }
+                context.logMultiple {
+                    +"SECOND PHASE"
+                    visitor.variableValues.elementData.forEach { (t, u) ->
+                        +"VAR $t [LOOP ${u.loop}]:"
+                        u.values.forEach { +"    ${ir2stringWhole(it)}" }
                     }
                 }
 
@@ -279,27 +268,28 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                         declaration, visitor.expressions, visitor.parentLoops, visitor.returnValues,
                         visitor.thrownValues, visitor.catchParameters).build()
 
-                DEBUG_OUTPUT(0) {
-                    function.debugOutput()
+                context.logMultiple {
+                    +function.debugString()
+                    +""
                 }
 
-                functions.put(function.symbol, function)
+                functions[function.symbol] = function
             }
         }, data = null)
 
-        DEBUG_OUTPUT(1) {
-            println("SYMBOL TABLE:")
+        context.logMultiple {
+            +"SYMBOL TABLE:"
             symbolTable.classMap.forEach { irClass, type ->
-                println("    DESCRIPTOR: ${irClass.descriptor}")
-                println("    TYPE: $type")
+                +"    DESCRIPTOR: ${irClass.descriptor}"
+                +"    TYPE: $type"
                 if (type !is DataFlowIR.Type.Declared)
                     return@forEach
-                println("        SUPER TYPES:")
-                type.superTypes.forEach { println("            $it") }
-                println("        VTABLE:")
-                type.vtable.forEach { println("            $it") }
-                println("        ITABLE:")
-                type.itable.forEach { println("            ${it.key} -> ${it.value}") }
+                +"        SUPER TYPES:"
+                type.superTypes.forEach { +"            $it" }
+                +"        VTABLE:"
+                type.vtable.forEach { +"            $it" }
+                +"        ITABLE:"
+                type.itable.forEach { +"            ${it.key} -> ${it.value}" }
             }
         }
 
@@ -618,9 +608,9 @@ internal class ModuleDFGBuilder(val context: Context, val irModule: IrModuleFrag
                 return variables[valueDeclaration]!!
             }
             return nodes.getOrPut(expression) {
-                DEBUG_OUTPUT(0) {
-                    println("Converting expression")
-                    println(ir2stringWhole(expression))
+                context.logMultiple {
+                    +"Converting expression"
+                    +ir2stringWhole(expression)
                 }
                 val values = mutableListOf<IrExpression>()
                 val edges = mutableListOf<DataFlowIR.Edge>()

backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/LocalEscapeAnalysis.kt
@@ -8,15 +8,10 @@ package org.jetbrains.kotlin.backend.konan.optimizations
 import org.jetbrains.kotlin.backend.konan.llvm.Lifetime
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.backend.konan.Context
+import org.jetbrains.kotlin.backend.konan.logMultiple
 import org.jetbrains.kotlin.backend.konan.descriptors.isArrayWithFixedSizeItems
 
 internal object LocalEscapeAnalysis {
-    private val DEBUG = 0
-
-    private inline fun DEBUG_OUTPUT(severity: Int, block: () -> Unit) {
-        if (DEBUG > severity) block()
-    }
-
     private enum class EscapeState {
         GLOBAL_ESCAPE,       // escape through global reference
         ARG_ESCAPE,          // escapes through function arguments, return or throw
@@ -187,7 +182,7 @@ internal object LocalEscapeAnalysis {
                 }
                 ir?.let {
                     lifetimes.put(it, Lifetime.STACK)
-                    DEBUG_OUTPUT(3) { println("${ir} does not escape") }
+                    context.log { "$ir does not escape" }
                 }
             }
         }
@@ -195,18 +190,18 @@ internal object LocalEscapeAnalysis {
 
     fun analyze(context: Context, moduleDFG: ModuleDFG, lifetimes: MutableMap<IrElement, Lifetime>) {
         moduleDFG.functions.forEach { (name, function) ->
-            DEBUG_OUTPUT(5) {
-                println("===============================================")
-                println("Visiting $name")
-                println("DATA FLOW IR:")
-                function.debugOutput()
+            context.logMultiple {
+                +"==============================================="
+                +"Visiting $name"
+                +"DATA FLOW IR:"
+                +function.debugString()
             }
             FunctionAnalyzer(function, context).analyze(lifetimes)
         }
     }
 
     fun computeLifetimes(context: Context, moduleDFG: ModuleDFG, lifetimes: MutableMap<IrElement, Lifetime>) {
-        DEBUG_OUTPUT(1) { println("In local EA") }
+        context.log { "In local EA" }
         assert(lifetimes.isEmpty())
         analyze(context, moduleDFG, lifetimes)
     }