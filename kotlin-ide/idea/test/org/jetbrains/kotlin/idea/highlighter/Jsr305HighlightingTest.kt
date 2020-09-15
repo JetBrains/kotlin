b9d4e573c8025346f6f31fbd265a415a9e48ec19
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-09-15 18:49:25 +0700

    [tests] fix Jsr305HighlightingTest

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/highlighter/Jsr305HighlightingTest.kt
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/highlighter/Jsr305HighlightingTest.kt
@@ -5,13 +5,14 @@
 
 package org.jetbrains.kotlin.idea.highlighter
 
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.kotlin.config.KotlinFacetSettingsProvider
-
 import org.jetbrains.kotlin.idea.artifacts.KotlinArtifacts
 import org.jetbrains.kotlin.idea.stubs.createFacet
+import org.jetbrains.kotlin.idea.test.IDEA_TEST_DATA_DIR
 import org.jetbrains.kotlin.idea.test.KotlinJdkAndLibraryProjectDescriptor
 import org.jetbrains.kotlin.idea.test.KotlinLightCodeInsightFixtureTestCase
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
@@ -26,18 +27,17 @@ import java.io.File
 @RunWith(JUnit38ClassRunner::class)
 class Jsr305HighlightingTest : KotlinLightCodeInsightFixtureTestCase() {
     override fun getProjectDescriptor(): LightProjectDescriptor {
-        val foreignAnnotationsJar = KotlinCompilerStandalone(listOf(File("third-party/annotations"))).compile()
-
+        val foreignAnnotationsJar = File(PathManager.getCommunityHomePath() + "/lib/jsr305.jar")
         val libraryJar = KotlinCompilerStandalone(
-            listOf(KotlinRoot.DIR.resolve("idea/testData/highlighterJsr305/library")),
+            listOf(IDEA_TEST_DATA_DIR.resolve("highlighterJsr305/library")),
             classpath = listOf(foreignAnnotationsJar)
         ).compile()
 
         return object : KotlinJdkAndLibraryProjectDescriptor(
             listOf(
-              KotlinArtifacts.instance.kotlinStdlib,
-              foreignAnnotationsJar,
-              libraryJar
+                KotlinArtifacts.instance.kotlinStdlib,
+                foreignAnnotationsJar,
+                libraryJar
             )
         ) {
             override fun configureModule(module: Module, model: ModifiableRootModel) {
