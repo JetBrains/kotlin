750343aa0bb761524d994a2e51bf05afdc37369c
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-09-18 15:53:37 +0200

    [KDM] Add default values for primitive proprties. Add presenting in signature default values
    Original commit: https://github.com/Kotlin/dokka/commit/2274d9261a59570cc3a1a26c3f7ddc167678fe8b

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/model/defaultValues.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/model/defaultValues.kt
@@ -3,11 +3,11 @@ package org.jetbrains.dokka.model
 import org.jetbrains.dokka.model.properties.ExtraProperty
 import org.jetbrains.dokka.model.properties.MergeStrategy
 
-class DefaultValue(val value: String): ExtraProperty<DParameter> {
-    companion object : ExtraProperty.Key<DParameter, DefaultValue> {
-        override fun mergeStrategyFor(left: DefaultValue, right: DefaultValue): MergeStrategy<DParameter> = MergeStrategy.Remove // TODO pass a logger somehow and log this
+class DefaultValue(val value: String): ExtraProperty<Documentable> {
+    companion object : ExtraProperty.Key<Documentable, DefaultValue> {
+        override fun mergeStrategyFor(left: DefaultValue, right: DefaultValue): MergeStrategy<Documentable> = MergeStrategy.Remove // TODO pass a logger somehow and log this
     }
 
-    override val key: ExtraProperty.Key<DParameter, *>
+    override val key: ExtraProperty.Key<Documentable, *>
         get() = Companion
 }
