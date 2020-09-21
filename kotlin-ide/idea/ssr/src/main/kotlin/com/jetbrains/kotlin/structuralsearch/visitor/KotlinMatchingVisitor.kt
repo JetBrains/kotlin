108892be623c3a8dffc2168cc8bdad3247549095
Bart van Helvert <b.a.j.v.helvert@student.tue.nl>
2020-09-21 19:19:48 +0200

    Match when condition with redundant parantheses

--- kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/visitor/KotlinMatchingVisitor.kt
+++ kotlin-ide/idea/ssr/src/main/kotlin/com/jetbrains/kotlin/structuralsearch/visitor/KotlinMatchingVisitor.kt
@@ -929,7 +929,8 @@ class KotlinMatchingVisitor(private val myMatchingVisitor: GlobalMatchingVisitor
     override fun visitWhenConditionWithExpression(condition: KtWhenConditionWithExpression) {
         val handler = getHandler(condition)
         if (handler is SubstitutionHandler) {
-            myMatchingVisitor.result = handler.handle(myMatchingVisitor.element, myMatchingVisitor.matchContext)
+            val other = getTreeElementDepar<KtWhenCondition>() ?: return
+            myMatchingVisitor.result = handler.handle(other, myMatchingVisitor.matchContext)
         } else {
             val other = getTreeElementDepar<KtWhenConditionWithExpression>() ?: return
             myMatchingVisitor.result = myMatchingVisitor.match(condition.expression, other.expression)
