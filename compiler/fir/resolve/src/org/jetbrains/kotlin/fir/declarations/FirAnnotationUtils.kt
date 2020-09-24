f3db113a8218be5b9bd6fcf0f8c35d817e0252e6
Jinseong Jeon <jsjeon@google.com>
2020-09-24 15:30:37 -0700

    FIR2IR: use @Target to put annotations on either property or backing field

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/declarations/FirAnnotationUtils.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/declarations/FirAnnotationUtils.kt
@@ -5,10 +5,10 @@
 
 package org.jetbrains.kotlin.fir.declarations
 
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.fir.FirAnnotationContainer
 import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.expressions.FirAnnotationCall
-import org.jetbrains.kotlin.fir.expressions.FirQualifiedAccessExpression
+import org.jetbrains.kotlin.fir.expressions.*
 import org.jetbrains.kotlin.fir.references.FirResolvedNamedReference
 import org.jetbrains.kotlin.fir.resolve.toSymbol
 import org.jetbrains.kotlin.fir.symbols.ConeClassLikeLookupTag
@@ -17,6 +17,10 @@ import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.coneType
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
+
+private val RETENTION_CLASS_ID = ClassId.fromString("kotlin/annotation/Retention")
+private val TARGET_CLASS_ID = ClassId.fromString("kotlin/annotation/Target")
+private val JVM_FIELD_CLASS_ID = ClassId.fromString("kotlin/jvm/JvmField")
 
 private fun FirAnnotationCall.toAnnotationLookupTag(): ConeClassLikeLookupTag =
     (annotationTypeRef.coneType as ConeClassLikeType).lookupTag
@@ -27,28 +31,59 @@ fun FirAnnotationCall.toAnnotationClassId(): ClassId =
 private fun FirAnnotationCall.toAnnotationClass(session: FirSession): FirRegularClass? =
     toAnnotationLookupTag().toSymbol(session)?.fir as? FirRegularClass
 
+// TODO: this is temporary solution, we need something better
+private val FirExpression.callableNameOfMetaAnnotationArgument: Name?
+    get() =
+        (this as? FirQualifiedAccessExpression)?.let {
+            val callableSymbol = (it.calleeReference as? FirResolvedNamedReference)?.resolvedSymbol as? FirCallableSymbol<*>
+            callableSymbol?.callableId?.callableName
+        }
+
 fun FirAnnotationContainer.nonSourceAnnotations(session: FirSession): List<FirAnnotationCall> =
     annotations.filter { annotation ->
         val firAnnotationClass = annotation.toAnnotationClass(session)
         firAnnotationClass != null && firAnnotationClass.annotations.none { meta ->
-            val firMetaAnnotationClassId = meta.toAnnotationClassId()
-            firMetaAnnotationClassId.asString() == "kotlin/annotation/Retention" &&
-                    // TODO: this is temporary solution, we need something better
-                    (meta.argumentList.arguments.singleOrNull() as? FirQualifiedAccessExpression)?.let {
-                        val callableSymbol = (it.calleeReference as? FirResolvedNamedReference)?.resolvedSymbol as? FirCallableSymbol<*>
-                        callableSymbol?.callableId?.callableName
-                    } == Name.identifier("SOURCE")
+            meta.toAnnotationClassId() == RETENTION_CLASS_ID &&
+                    meta.argumentList.arguments.singleOrNull()?.callableNameOfMetaAnnotationArgument == Name.identifier("SOURCE")
         }
     }
 
 inline val FirProperty.hasJvmFieldAnnotation: Boolean
     get() = annotations.any { it.isJvmFieldAnnotation }
 
-inline val FirAnnotationCall.isJvmFieldAnnotation: Boolean
-    get() {
-        val classId = toAnnotationClassId()
-        return classId.packageFqName.asString() == "kotlin.jvm" && classId.relativeClassName.asString() == "JvmField"
+val FirAnnotationCall.isJvmFieldAnnotation: Boolean
+    get() = toAnnotationClassId() == JVM_FIELD_CLASS_ID
+
+private fun FirAnnotationCall.useSiteTargetsFromMetaAnnotation(session: FirSession): List<AnnotationUseSiteTarget> {
+    val metaAnnotationAboutTarget =
+        toAnnotationClass(session)?.annotations?.find { it.toAnnotationClassId() == TARGET_CLASS_ID }
+            ?: return emptyList()
+    return metaAnnotationAboutTarget.argumentList.arguments.toAnnotationUseSiteTargets()
+}
+
+private fun List<FirExpression>.toAnnotationUseSiteTargets(): List<AnnotationUseSiteTarget> =
+    flatMap { arg ->
+        val unwrappedArg = if (arg is FirNamedArgumentExpression) arg.expression else arg
+        if (unwrappedArg is FirArrayOfCall) {
+            unwrappedArg.argumentList.arguments.toAnnotationUseSiteTargets()
+        } else {
+            unwrappedArg.toAnnotationUseSiteTarget()?.let { listOf(it) } ?: emptyList()
+        }
     }
+
+private val USE_SITE_TARGET_NAME_MAP =
+    AnnotationUseSiteTarget.values().map { it.name to it }.toMap()
+
+private fun FirExpression.toAnnotationUseSiteTarget(): AnnotationUseSiteTarget? =
+    // TODO: depending on the context, "PARAMETER" can be mapped to either CONSTRUCTOR_PARAMETER or SETTER_PARAMETER ?
+    callableNameOfMetaAnnotationArgument?.identifier?.let {
+        USE_SITE_TARGET_NAME_MAP[it]
+    }
+
+fun FirAnnotationCall.hasMetaAnnotationUseSiteTargets(session: FirSession, vararg useSiteTargets: AnnotationUseSiteTarget): Boolean {
+    val meta = useSiteTargetsFromMetaAnnotation(session)
+    return useSiteTargets.any { meta.contains(it) }
+}
 
 fun FirAnnotatedDeclaration.hasAnnotation(classId: ClassId): Boolean {
     return annotations.any { it.toAnnotationClassId() == classId }
