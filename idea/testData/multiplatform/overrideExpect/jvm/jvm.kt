04da7b53190a74fe5658e72cf4ee85edfd0fb87b
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2020-09-25 14:17:56 +0300

    Add test case on overriding property of expect-type
    Current behaviour is undesired, the fix is in the next commit

--- idea/testData/multiplatform/overrideExpect/jvm/jvm.kt
+++ idea/testData/multiplatform/overrideExpect/jvm/jvm.kt
@@ -6,4 +6,8 @@ interface Derived : Base {
     override fun expectInArgument(e: Expect)
 
     override fun Expect.expectInReceiver()
+
+    override val expectVal: <!PROPERTY_TYPE_MISMATCH_ON_OVERRIDE!>Expect<!>
+
+    override var expectVar: <!VAR_TYPE_MISMATCH_ON_OVERRIDE!>Expect<!>
 }
