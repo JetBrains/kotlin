c209696711fe58e81f28170cd32ee238147c1338
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2020-09-25 13:25:51 +0300

    Refine types of type parameters before checking their equality in OverridingUtil
    Original commit: ac107f362e0f0f72971be3f414fb82e141d5147b
    
    (cherry picked from commit 9c8788666f45c5a04bbea788cf2442fe21430a61)

--- kotlin-ide/idea/testData/multiplatform/overrideExpect/jvm/jvm.kt
+++ kotlin-ide/idea/testData/multiplatform/overrideExpect/jvm/jvm.kt
@@ -1,7 +1,7 @@
 actual typealias Expect = String
 
 interface Derived : Base {
-    override fun expectInReturnType(): <!RETURN_TYPE_MISMATCH_ON_OVERRIDE!>Expect<!>
+    override fun expectInReturnType(): Expect
 
     override fun expectInArgument(e: Expect)
 
