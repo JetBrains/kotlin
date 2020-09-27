6d38c7acaf0acd267627fb59257346525baaca5a
Yaroslav Chernyshev <yaroslav.chernyshev@jetbrains.com>
2020-09-27 21:34:53 +0300

    [Gradle, Cocoapods] Improve logging when deprecated podspec path is used
    #KT-41948 Fixed
    
    (cherry picked from commit 5f4aa4db27f199265686a9f1e37ef71630746fe3)

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/cocoapods/CocoapodsExtension.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/cocoapods/CocoapodsExtension.kt
@@ -109,12 +109,26 @@ open class CocoapodsExtension(private val project: Project) {
     fun pod(name: String, version: String? = null, path: File? = null, moduleName: String = name.asModuleName()) {
         // Empty string will lead to an attempt to create two podDownload tasks.
         // One is original podDownload and second is podDownload + pod.name
+        require(name.isNotEmpty()) { "Please provide not empty pod name to avoid ambiguity" }
         var podSource = path
         if (path != null && !path.isDirectory) {
-            project.logger.warn("Please use directory with podspec file, not podspec file itself")
+            val pattern = "\\W*pod(.*\"${name}\".*)".toRegex()
+            val buildScript = project.buildFile
+            val lines = buildScript.readLines()
+            val lineNumber = lines.indexOfFirst { pattern.matches(it) }
+            val lineContent = lines[lineNumber].trimIndent()
+            val newContent = lineContent.replace(path.name, "")
+            project.logger.warn(
+                """
+                |Deprecated DSL used on ${buildScript.absolutePath}${File.pathSeparator}${lineNumber + 1}:
+                |Found: "${lineContent}"
+                |Expected: "${newContent}"
+                |Please, change the path to avoid this warning.
+                |
+            """.trimMargin()
+            )
             podSource = path.parentFile
         }
-        require(name.isNotEmpty()) { "Please provide not empty pod name to avoid ambiguity" }
         addToPods(CocoapodsDependency(name, moduleName, version, podSource?.let { Path(it) }))
     }
 
