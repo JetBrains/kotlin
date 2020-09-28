d9906ae8da8b2b6d1f81bc6d7fb4cb3459467d97
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2020-09-28 16:07:54 +0300

    Minor: unmute some bytecode listing tests in JVM_IR

--- compiler/testData/codegen/bytecodeListing/noRemoveAtInReadOnly.kt
+++ compiler/testData/codegen/bytecodeListing/noRemoveAtInReadOnly.kt
@@ -1,3 +1,2 @@
-// IGNORE_BACKEND: JVM_IR
 abstract class A<T> : List<T>
 abstract class B : List<Int>
--- compiler/testData/codegen/bytecodeListing/specialBridges/noSpecialBridgeIfPresentInSuperClass.kt
+++ compiler/testData/codegen/bytecodeListing/specialBridges/noSpecialBridgeIfPresentInSuperClass.kt
@@ -1,3 +1,2 @@
-// IGNORE_BACKEND: JVM_IR
 abstract class A<T> : List<T>
 abstract class B<E> : A<E>()
