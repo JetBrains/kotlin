f48f7c6dcabe7c9cd6f5f0b73f3298cd3952c838
Uzi Landsmann <uzi.landsmann@bisnode.com>
2020-10-01 23:04:53 +0200

    Added samples for the kotlin.system package (measureTime* functions)
    Co-authored-by: Ilya Gorbunov <ilya.gorbunov@jetbrains.com>

--- libraries/stdlib/jvm/src/kotlin/system/Timing.kt
+++ libraries/stdlib/jvm/src/kotlin/system/Timing.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -10,6 +10,8 @@ import kotlin.contracts.*
 
 /**
  * Executes the given [block] and returns elapsed time in milliseconds.
+ *
+ * @sample samples.system.Timing.measureBlockTimeMillis
  */
 public inline fun measureTimeMillis(block: () -> Unit): Long {
     contract {
@@ -22,6 +24,8 @@ public inline fun measureTimeMillis(block: () -> Unit): Long {
 
 /**
  * Executes the given [block] and returns elapsed time in nanoseconds.
+ *
+ * @sample samples.system.Timing.measureBlockNanoTime
  */
 public inline fun measureNanoTime(block: () -> Unit): Long {
     contract {
