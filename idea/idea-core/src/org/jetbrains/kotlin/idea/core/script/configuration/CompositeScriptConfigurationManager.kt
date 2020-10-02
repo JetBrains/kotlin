abc4aef403348703165e90dd4f51f58821677b89
Vladimir Dolzhenko <Vladimir.Dolzhenko@jetbrains.com>
2020-10-02 13:23:43 +0000

    Store kotlin-script-cache-dependencies as project file attribute
    To avoid extra root project scan we need to store already processed script cache dependencies
    
    ^KT-41640 Fixed

--- idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/configuration/CompositeScriptConfigurationManager.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/configuration/CompositeScriptConfigurationManager.kt
@@ -162,10 +162,10 @@ class CompositeScriptConfigurationManager(val project: Project) : ScriptConfigur
     override fun getAllScriptDependenciesSourcesScope(): GlobalSearchScope =
         classpathRoots.allDependenciesSourcesScope
 
-    override fun getAllScriptsDependenciesClassFiles(): List<VirtualFile> =
+    override fun getAllScriptsDependenciesClassFiles(): Collection<VirtualFile> =
         classpathRoots.allDependenciesClassFiles
 
-    override fun getAllScriptDependenciesSources(): List<VirtualFile> =
+    override fun getAllScriptDependenciesSources(): Collection<VirtualFile> =
         classpathRoots.allDependenciesSources
 
     ///////////////////
