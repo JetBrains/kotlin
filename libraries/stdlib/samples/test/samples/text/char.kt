52f7f02fdfe76e958f0aed87a6ea19021ed20b1d
Kris <dezcont@gmail.com>
2020-10-02 15:36:57 -0500

    added sample for  with predicate

--- libraries/stdlib/samples/test/samples/text/char.kt
+++ libraries/stdlib/samples/test/samples/text/char.kt
@@ -129,9 +129,15 @@ class Chars {
 	val chars = listOf('a', 'z', '1', '2')
 	val empty = listOf()
 	val last = chars.last()
+	val predLast = chars.last{it > '1'}
 	assertPrints(last, '2')
+	assertPrints(predLast, '2')
+	assertPrints(chars.last{it > 'a'}, 'z')
 	assertFailsWith<NoSuchElementException>{
 	    empty.last()
+	}
+	assertFailsWith<NoSuchElementException>{
+	    chars.last{it > 'z'}
 	}
     }
 }
