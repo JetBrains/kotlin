062c3f4f8919880e82e41c565fde1c94fedde032
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2020-10-05 18:00:22 +0300

    Add test on overriding declaration with a composite type with expect declaration
    Original commit: 01d618105049d206bf19acd2aed463a68bf25cf1
    
    (cherry picked from commit 0f55e0d103a06da667502a9268370c31e2710e7f)

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/caches/resolve/MultiplatformAnalysisTestGenerated.java
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/caches/resolve/MultiplatformAnalysisTestGenerated.java
@@ -181,6 +181,11 @@ public class MultiplatformAnalysisTestGenerated extends AbstractMultiplatformAna
         runTest("idea/testData/multiplatform/overrideExpect/");
     }
 
+    @TestMetadata("overrideExpectWithCompositeType")
+    public void testOverrideExpectWithCompositeType() throws Exception {
+        runTest("idea/testData/multiplatform/overrideExpectWithCompositeType/");
+    }
+
     @TestMetadata("platformSpecificChecksInCommon")
     public void testPlatformSpecificChecksInCommon() throws Exception {
         runTest("testData/multiplatform/platformSpecificChecksInCommon/");
