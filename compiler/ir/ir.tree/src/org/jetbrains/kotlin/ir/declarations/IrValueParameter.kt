e9dc79367d6c2e1914c0aec509ae4a949414e524
Mads Ager <ager@google.com>
2020-10-06 18:14:45 +0200

    [IR] Move isAssignable to IrValueDeclaration.
    (cherry picked from commit 3817aa32a18cc97ecf43a967be5da1d9f546b672)

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrValueParameter.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/IrValueParameter.kt
@@ -38,11 +38,6 @@ abstract class IrValueParameter : IrValueDeclaration(), IrSymbolDeclaration<IrVa
     // Once we are able to load any top-level declaration from klib this hack should be deprecated and removed.
     abstract val isHidden: Boolean
 
-    // Parameters are not assignable by default. However, in the IR, assignments to parameters are used
-    // to implement default argument stubs. If the default value is to be used, the value is assigned
-    // to the parameter.
-    abstract val isAssignable: Boolean
-
     abstract var defaultValue: IrExpressionBody?
 
     override fun <R, D> accept(visitor: IrElementVisitor<R, D>, data: D): R =
