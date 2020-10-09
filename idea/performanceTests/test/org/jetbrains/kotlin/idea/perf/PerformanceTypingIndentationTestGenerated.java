23beaa5883a6a19e715f40ce80838bfb33a6207a
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-10-09 20:03:48 +0700

    [formatter] add tests for line indent after properties
    ^KT-42560

--- idea/performanceTests/test/org/jetbrains/kotlin/idea/perf/PerformanceTypingIndentationTestGenerated.java
+++ idea/performanceTests/test/org/jetbrains/kotlin/idea/perf/PerformanceTypingIndentationTestGenerated.java
@@ -34,6 +34,16 @@ public class PerformanceTypingIndentationTestGenerated extends AbstractPerforman
         runTest("idea/testData/indentationOnNewline/AfterClassNameBeforeFun.kt");
     }
 
+    @TestMetadata("AfterExtensionPropertyGetter.kt")
+    public void testAfterExtensionPropertyGetter() throws Exception {
+        runTest("idea/testData/indentationOnNewline/AfterExtensionPropertyGetter.kt");
+    }
+
+    @TestMetadata("AfterExtensionPropertySetter.kt")
+    public void testAfterExtensionPropertySetter() throws Exception {
+        runTest("idea/testData/indentationOnNewline/AfterExtensionPropertySetter.kt");
+    }
+
     @TestMetadata("AfterFinally.kt")
     public void testAfterFinally() throws Exception {
         runTest("idea/testData/indentationOnNewline/AfterFinally.kt");
@@ -42,6 +52,16 @@ public class PerformanceTypingIndentationTestGenerated extends AbstractPerforman
     @TestMetadata("AfterImport.kt")
     public void testAfterImport() throws Exception {
         runTest("idea/testData/indentationOnNewline/AfterImport.kt");
+    }
+
+    @TestMetadata("AfterPropertyGetter.kt")
+    public void testAfterPropertyGetter() throws Exception {
+        runTest("idea/testData/indentationOnNewline/AfterPropertyGetter.kt");
+    }
+
+    @TestMetadata("AfterPropertySetter.kt")
+    public void testAfterPropertySetter() throws Exception {
+        runTest("idea/testData/indentationOnNewline/AfterPropertySetter.kt");
     }
 
     @TestMetadata("AfterTry.kt")
---
+++ idea/testData/indentationOnNewline/AfterPropertyGetter.after.kt
@@ -0,0 +1,5 @@
+var foo: Int
+    get() = length
+<caret>
+
+// WITHOUT_CUSTOM_LINE_INDENT_PROVIDER
