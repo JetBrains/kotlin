5c798d4e132ea357138a594a23361c39cf2b513a
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-10-14 00:54:15 +0300

    FIR IDE: fix test data in fir-ide module

--- idea/testData/checker/Nullability.kt
+++ idea/testData/checker/Nullability.kt
@@ -1,3 +1,4 @@
+// FIR_COMPARISON
 fun test() {
   val a : Int? = 0
   if (a != null) {
--- idea/testData/findUsages/kotlin/internal/findFunctionUsages/kotlinTraitImplThroughDelegate.0.kt
+++ idea/testData/findUsages/kotlin/internal/findFunctionUsages/kotlinTraitImplThroughDelegate.0.kt
@@ -1,5 +1,6 @@
 // PSI_ELEMENT: org.jetbrains.kotlin.psi.KtNamedFunction
 // OPTIONS: usages, skipImports
+// FIR_COMPARISON
 package server
 
 interface TraitWithImpl {
