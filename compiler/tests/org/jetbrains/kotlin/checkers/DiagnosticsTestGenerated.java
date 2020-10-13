7120e07302d0461a750eb05b57106f815e23635c
Jinseong Jeon <jsjeon@google.com>
2020-10-12 22:45:31 -0700

    FIR DFA: add tests about smartcasts in/after try-catch-finally

--- compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -21992,6 +21992,16 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTestWithFirVali
                     runTest("compiler/testData/diagnostics/tests/smartCasts/castchecks/basicOn.kt");
                 }
 
+                @TestMetadata("castInTryWithCatch.kt")
+                public void testCastInTryWithCatch() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/smartCasts/castchecks/castInTryWithCatch.kt");
+                }
+
+                @TestMetadata("castInTryWithoutCatch.kt")
+                public void testCastInTryWithoutCatch() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/smartCasts/castchecks/castInTryWithoutCatch.kt");
+                }
+
                 @TestMetadata("impossible.kt")
                 public void testImpossible() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/smartCasts/castchecks/impossible.kt");
