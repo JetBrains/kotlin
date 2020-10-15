fecdd1f9a82e33e059f542a74e4a744ca693009b
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-10-15 14:15:15 +0300

    [FIR] Rename FirDeclarationOrigin.FakeOverride to SubstitutionOverride
    Original commit: e75f218f87bd8354071aa06eb44da0b61d4ba2f7

--- kotlin-ide/frontend-fir/src/org/jetbrains/kotlin/idea/fir/FirUtils.kt
+++ kotlin-ide/frontend-fir/src/org/jetbrains/kotlin/idea/fir/FirUtils.kt
@@ -5,7 +5,6 @@
 
 package org.jetbrains.kotlin.idea.fir
 
-import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.FirSymbolOwner
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclarationOrigin
@@ -16,6 +15,8 @@ import org.jetbrains.kotlin.fir.references.FirReference
 import org.jetbrains.kotlin.fir.references.FirResolvedNamedReference
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
+import org.jetbrains.kotlin.idea.frontend.api.fir.KtSymbolByFirBuilder
+import org.jetbrains.kotlin.idea.frontend.api.symbols.KtSymbol
 import org.jetbrains.kotlin.idea.fir.low.level.api.FirModuleResolveState
 import org.jetbrains.kotlin.idea.fir.low.level.api.LowLevelFirApiFacade
 import org.jetbrains.kotlin.psi.KtElement
@@ -81,5 +82,5 @@ internal inline fun <reified D> D.unrollFakeOverrides(): D where D : FirDeclarat
 private inline val FirBasedSymbol<*>.isFakeOrIntersectionOverride: Boolean
     get() {
         val origin = (fir as? FirDeclaration)?.origin ?: return false
-        return origin == FirDeclarationOrigin.FakeOverride || origin == FirDeclarationOrigin.IntersectionOverride
+        return origin == FirDeclarationOrigin.SubstitutionOverride || origin == FirDeclarationOrigin.IntersectionOverride
     }
