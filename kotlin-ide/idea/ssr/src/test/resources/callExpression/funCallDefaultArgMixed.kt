22eda1849376bb934549fd04055aee760138257d
Louis Vignier <louis.vignier@jetbrains.com>
2020-10-16 15:25:07 +0200

    Include warnings in testFunCallDefaultArgMixed

--- kotlin-ide/idea/ssr/src/test/resources/callExpression/funCallDefaultArgMixed.kt
+++ kotlin-ide/idea/ssr/src/test/resources/callExpression/funCallDefaultArgMixed.kt
@@ -2,7 +2,7 @@ fun a(k: Int, b: Int = 0, c: String = "Hello World!", d: Double, vararg t: Strin
 
 fun c() {
     <warning descr="SSR">a(0, 0, "This is test!", 0.0, "Test")</warning>
-    <warning descr="SSR">a(0, d = 0.0, t = *arrayOf("Test"))</warning>
-    <warning descr="SSR">a(b = 0, c = "This is a test!", k = 0, t = *arrayOf("Test"), d = 0.0)</warning>
-    <warning descr="SSR">a(k = 0, d = 0.0, t = *arrayOf("Test"))</warning>
+    <warning descr="SSR">a(0, d = 0.0, t = *<warning descr="[REDUNDANT_SPREAD_OPERATOR_IN_NAMED_FORM_IN_FUNCTION] Redundant spread (*) operator">arrayOf("Test")</warning>)</warning>
+    <warning descr="SSR">a(b = 0, c = "This is a test!", k = 0, t = *<warning descr="[REDUNDANT_SPREAD_OPERATOR_IN_NAMED_FORM_IN_FUNCTION] Redundant spread (*) operator">arrayOf("Test")</warning>, d = 0.0)</warning>
+    <warning descr="SSR">a(k = 0, d = 0.0, t = *<warning descr="[REDUNDANT_SPREAD_OPERATOR_IN_NAMED_FORM_IN_FUNCTION] Redundant spread (*) operator">arrayOf("Test")</warning>)</warning>
 }
