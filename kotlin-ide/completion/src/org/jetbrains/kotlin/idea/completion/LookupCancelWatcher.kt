9e976d2e93160b7b286854794488feea665c03a8
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-10-16 14:10:11 +0700

    LookupCancelWatcher: migrate LookupManager from listener to subscriber

--- kotlin-ide/completion/src/org/jetbrains/kotlin/idea/completion/LookupCancelWatcher.kt
+++ kotlin-ide/completion/src/org/jetbrains/kotlin/idea/completion/LookupCancelWatcher.kt
@@ -1,23 +1,11 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.idea.completion
 
-import com.intellij.codeInsight.lookup.Lookup
-import com.intellij.codeInsight.lookup.LookupManager
+import com.intellij.codeInsight.lookup.LookupManagerListener
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.EditorFactoryEvent
 import com.intellij.openapi.editor.event.EditorFactoryListener
@@ -25,22 +13,20 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
 class LookupCancelWatcher : StartupActivity {
-
     override fun runActivity(project: Project) {
+        val connection = project.messageBus.connect()
         EditorFactory.getInstance().addEditorFactoryListener(
             object : EditorFactoryListener {
                 override fun editorReleased(event: EditorFactoryEvent) {
                     LookupCancelService.getServiceIfCreated(project)?.disposeLastReminiscence(event.editor)
                 }
             },
-            project
+            connection
         )
 
-        LookupManager.getInstance(project).addPropertyChangeListener { event ->
-            if (event.propertyName == LookupManager.PROP_ACTIVE_LOOKUP) {
-                val lookup = event.newValue as Lookup?
-                lookup?.addLookupListener(LookupCancelService.getInstance(project).lookupCancelListener)
-            }
-        }
+        connection.subscribe(LookupManagerListener.TOPIC, LookupManagerListener { _, newLookup ->
+            if (newLookup == null) return@LookupManagerListener
+            newLookup.addLookupListener(LookupCancelService.getInstance(newLookup.project).lookupCancelListener)
+        })
     }
 }
