1fbad07ad9c7aba1c671dd38d3a0f18a6e9f3961
Yuya Urano <yuyaurano@me.com>
2020-10-16 12:31:12 +0900

    KT-20357: Add another sample for requireNotNull
    review comment: https://github.com/JetBrains/kotlin/pull/3584#discussion_r504946248

--- libraries/stdlib/samples/test/samples/misc/preconditions.kt
+++ libraries/stdlib/samples/test/samples/misc/preconditions.kt
@@ -30,17 +30,26 @@ class Preconditions {
     @Sample
     fun failRequireNotNullWithLazyMessage() {
 
-        fun execWithParams(params: Map<String, String?>) {
-            val required = requireNotNull(params["required"]) { "Required key must be non-null" } // returns a non-null value
+        fun printRequiredParam(params: Map<String, String?>) {
+            val required: String = requireNotNull(params["required"]) { "Required value must be non-null" } // returns a non-null value
             println(required)
             // ...
         }
 
+        fun printRequiredParamByUpperCase(params: Map<String, String?>) {
+            val requiredParam: String? = params["required"]
+            requireNotNull(requiredParam) { "Required value must be non-null" }
+            // now requiredParam is smartcast to String so that it is unnecessary to use the safe call(?.)
+            println(requiredParam.toUpperCase())
+        }
+
         val params: MutableMap<String, String?> = mutableMapOf("required" to null)
-        assertFailsWith<IllegalArgumentException> { execWithParams(params) }
+        assertFailsWith<IllegalArgumentException> { printRequiredParam(params) }
+        assertFailsWith<IllegalArgumentException> { printRequiredParamByUpperCase(params) }
 
         params["required"] = "non-empty-param"
-        execWithParams(params) // prints "non-empty-param"
+        printRequiredParam(params) // prints "non-empty-param"
+        printRequiredParamByUpperCase(params) // prints "NON-EMPTY-PARAM"
     }
 
     @Sample
