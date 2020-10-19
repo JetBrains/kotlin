190ff5ed43d0360a7b98e0ff9211b3a9f1ee11ba
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-10-19 17:33:23 +0700

    KotlinFunctionParameterTableModel: convert to Kotlin

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/refactoring/changeSignature/ui/KotlinFunctionParameterTableModel.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/refactoring/changeSignature/ui/KotlinFunctionParameterTableModel.kt
@@ -13,98 +13,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.idea.refactoring.changeSignature.ui
 
-package org.jetbrains.kotlin.idea.refactoring.changeSignature.ui;
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase
+import com.intellij.refactoring.ui.StringTableCellEditor
+import com.intellij.ui.BooleanTableCellRenderer
+import com.intellij.util.ui.ColumnInfo
+import org.jetbrains.kotlin.idea.KotlinBundle.message
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinMethodDescriptor
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinParameterInfo
+import javax.swing.table.TableCellEditor
+import javax.swing.table.TableCellRenderer
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
-import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase;
-import com.intellij.refactoring.ui.StringTableCellEditor;
-import com.intellij.ui.BooleanTableCellRenderer;
-import com.intellij.util.ui.ColumnInfo;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.idea.KotlinBundle;
-import org.jetbrains.kotlin.idea.KotlinFileType;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinMethodDescriptor;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinParameterInfo;
+class KotlinFunctionParameterTableModel(
+    methodDescriptor: KotlinMethodDescriptor,
+    typeContext: PsiElement,
+    defaultValueContext: PsiElement
+) : KotlinCallableParameterTableModel(
+    methodDescriptor,
+    typeContext,
+    defaultValueContext,
+    NameColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.project),
+    TypeColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.project, KotlinFileType.INSTANCE),
+    DefaultValueColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(
+        typeContext.project,
+        KotlinFileType.INSTANCE
+    ),
+    ReceiverColumn<ParameterTableModelItemBase<KotlinParameterInfo?>>(typeContext.project, methodDescriptor)
+) {
+    override fun removeRow(idx: Int) {
+        if (getRowValue(idx).parameter == receiver) {
+            receiver = null
+        }
 
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableCellRenderer;
+        super.removeRow(idx)
+    }
 
-public class KotlinFunctionParameterTableModel extends KotlinCallableParameterTableModel {
-    public KotlinFunctionParameterTableModel(
-            KotlinMethodDescriptor methodDescriptor,
-            PsiElement typeContext,
-            PsiElement defaultValueContext
+    override var receiver: KotlinParameterInfo?
+        get() = (columnInfos[columnCount - 1] as ReceiverColumn<*>).receiver
+        set(receiver) {
+            (columnInfos[columnCount - 1] as ReceiverColumn<*>).receiver = receiver
+        }
+
+    private class ReceiverColumn<TableItem : ParameterTableModelItemBase<KotlinParameterInfo?>>(
+        private val project: Project, methodDescriptor: KotlinMethodDescriptor
+    ) : ColumnInfoBase<KotlinParameterInfo?, TableItem, Boolean>(
+        message("column.name.receiver")
     ) {
-        super(methodDescriptor,
-              typeContext,
-              defaultValueContext,
-              new NameColumn(typeContext.getProject()),
-              new TypeColumn(typeContext.getProject(), KotlinFileType.INSTANCE),
-              new DefaultValueColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.getProject(),
-                                                                                                            KotlinFileType.INSTANCE),
-              new ReceiverColumn(typeContext.getProject(), methodDescriptor));
+        var receiver: KotlinParameterInfo? = methodDescriptor.receiver
+        override fun valueOf(item: TableItem): Boolean = item.parameter == receiver
+
+        override fun setValue(item: TableItem, value: Boolean) {
+            receiver = if (value) item.parameter else null
+        }
+
+        override fun isCellEditable(pParameterTableModelItemBase: TableItem): Boolean = true
+
+        public override fun doCreateRenderer(item: TableItem): TableCellRenderer = BooleanTableCellRenderer()
+
+        public override fun doCreateEditor(o: TableItem): TableCellEditor = StringTableCellEditor(project)
     }
 
-    @Override
-    public void removeRow(int idx) {
-        if (getRowValue(idx).parameter == getReceiver()) {
-            setReceiver(null);
-        }
-        super.removeRow(idx);
-    }
-
-    @Override
-    @Nullable
-    public KotlinParameterInfo getReceiver() {
-        return ((ReceiverColumn) getColumnInfos()[getColumnCount() - 1]).receiver;
-    }
-
-    public void setReceiver(@Nullable KotlinParameterInfo receiver) {
-        ((ReceiverColumn) getColumnInfos()[getColumnCount() - 1]).receiver = receiver;
-    }
-
-    public static boolean isReceiverColumn(ColumnInfo column) {
-        return column instanceof ReceiverColumn;
-    }
-
-    protected static class ReceiverColumn<TableItem extends ParameterTableModelItemBase<KotlinParameterInfo>>
-            extends ColumnInfoBase<KotlinParameterInfo, TableItem, Boolean> {
-        private final Project project;
-        @Nullable
-        private KotlinParameterInfo receiver;
-
-        public ReceiverColumn(Project project, @Nullable KotlinMethodDescriptor methodDescriptor) {
-            super(KotlinBundle.message("column.name.receiver"));
-            this.project = project;
-            this.receiver = methodDescriptor != null ? methodDescriptor.getReceiver() : null;
-        }
-
-        @Override
-        public Boolean valueOf(TableItem item) {
-            return item.parameter == receiver;
-        }
-
-        @Override
-        public void setValue(TableItem item, Boolean value) {
-            if (value == null) return;
-            receiver = value ? item.parameter : null;
-        }
-
-        @Override
-        public boolean isCellEditable(TableItem pParameterTableModelItemBase) {
-            return true;
-        }
-
-        @Override
-        public TableCellRenderer doCreateRenderer(TableItem item) {
-            return new BooleanTableCellRenderer();
-        }
-
-        @Override
-        public TableCellEditor doCreateEditor(TableItem o) {
-            return new StringTableCellEditor(project);
-        }
+    companion object {
+        fun isReceiverColumn(column: ColumnInfo<*, *>?): Boolean = column is ReceiverColumn<*>
     }
 }
