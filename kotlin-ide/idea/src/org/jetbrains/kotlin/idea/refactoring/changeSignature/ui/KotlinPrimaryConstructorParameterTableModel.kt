57805167f12a365dc865393e0735d19cf97b63e6
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-10-19 17:39:39 +0700

    KotlinPrimaryConstructorParameterTableModel: convert to Kotlin

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/refactoring/changeSignature/ui/KotlinPrimaryConstructorParameterTableModel.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/refactoring/changeSignature/ui/KotlinPrimaryConstructorParameterTableModel.kt
@@ -13,77 +13,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.idea.refactoring.changeSignature.ui
 
-package org.jetbrains.kotlin.idea.refactoring.changeSignature.ui;
+import com.intellij.openapi.ui.ComboBoxTableRenderer
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase
+import com.intellij.util.ui.ColumnInfo
+import org.jetbrains.kotlin.idea.KotlinBundle
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinMethodDescriptor
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinParameterInfo
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinValVar
+import javax.swing.DefaultCellEditor
+import javax.swing.JComboBox
+import javax.swing.JTable
+import javax.swing.table.TableCellEditor
+import javax.swing.table.TableCellRenderer
 
-import com.intellij.openapi.ui.ComboBoxTableRenderer;
-import com.intellij.psi.PsiElement;
-import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase;
-import com.intellij.util.ui.ColumnInfo;
-import org.jetbrains.kotlin.idea.KotlinBundle;
-import org.jetbrains.kotlin.idea.KotlinFileType;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinMethodDescriptor;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinParameterInfo;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinValVar;
-
-import javax.swing.*;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableCellRenderer;
-
-public class KotlinPrimaryConstructorParameterTableModel extends KotlinCallableParameterTableModel {
-    public KotlinPrimaryConstructorParameterTableModel(
-            KotlinMethodDescriptor methodDescriptor,
-            PsiElement typeContext,
-            PsiElement defaultValueContext
+class KotlinPrimaryConstructorParameterTableModel(
+    methodDescriptor: KotlinMethodDescriptor,
+    typeContext: PsiElement,
+    defaultValueContext: PsiElement
+) : KotlinCallableParameterTableModel(
+    methodDescriptor,
+    typeContext,
+    defaultValueContext,
+    ValVarColumn(),
+    NameColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.project),
+    TypeColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.project, KotlinFileType.INSTANCE),
+    DefaultValueColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(
+        typeContext.project,
+        KotlinFileType.INSTANCE,
+    )
+) {
+    private class ValVarColumn : ColumnInfoBase<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>, KotlinValVar>(
+        KotlinBundle.message("column.name.val.var")
     ) {
-        super(methodDescriptor,
-              typeContext,
-              defaultValueContext,
-              new ValVarColumn(),
-              new NameColumn(typeContext.getProject()),
-              new TypeColumn(typeContext.getProject(), KotlinFileType.INSTANCE),
-              new DefaultValueColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.getProject(),
-                                                                                                            KotlinFileType.INSTANCE));
+        override fun isCellEditable(item: ParameterTableModelItemBase<KotlinParameterInfo>): Boolean {
+            return !item.isEllipsisType && item.parameter.isNewParameter
+        }
+
+        override fun valueOf(item: ParameterTableModelItemBase<KotlinParameterInfo>): KotlinValVar = item.parameter.valOrVar
+
+        override fun setValue(item: ParameterTableModelItemBase<KotlinParameterInfo>, value: KotlinValVar) {
+            item.parameter.valOrVar = value
+        }
+
+        override fun doCreateRenderer(item: ParameterTableModelItemBase<KotlinParameterInfo>): TableCellRenderer {
+            return ComboBoxTableRenderer(KotlinValVar.values())
+        }
+
+        override fun doCreateEditor(item: ParameterTableModelItemBase<KotlinParameterInfo>): TableCellEditor {
+            return DefaultCellEditor(JComboBox<ParameterTableModelItemBase<KotlinParameterInfo>>())
+        }
+
+        override fun getWidth(table: JTable): Int = table.getFontMetrics(table.font).stringWidth(name) + 8
     }
 
-    public static boolean isValVarColumn(ColumnInfo column) {
-        return column instanceof ValVarColumn;
-    }
-
-    protected static class ValVarColumn
-            extends ColumnInfoBase<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>, KotlinValVar> {
-        public ValVarColumn() {
-            super(KotlinBundle.message("column.name.val.var"));
-        }
-
-        @Override
-        public boolean isCellEditable(ParameterTableModelItemBase<KotlinParameterInfo> item) {
-            return !item.isEllipsisType() && item.parameter.isNewParameter();
-        }
-
-        @Override
-        public KotlinValVar valueOf(ParameterTableModelItemBase<KotlinParameterInfo> item) {
-            return item.parameter.getValOrVar();
-        }
-
-        @Override
-        public void setValue(ParameterTableModelItemBase<KotlinParameterInfo> item, KotlinValVar value) {
-            item.parameter.setValOrVar(value);
-        }
-
-        @Override
-        public TableCellRenderer doCreateRenderer(ParameterTableModelItemBase<KotlinParameterInfo> item) {
-            return new ComboBoxTableRenderer<KotlinValVar>(KotlinValVar.values());
-        }
-
-        @Override
-        public TableCellEditor doCreateEditor(ParameterTableModelItemBase<KotlinParameterInfo> item) {
-            return new DefaultCellEditor(new JComboBox());
-        }
-
-        @Override
-        public int getWidth(JTable table) {
-            return table.getFontMetrics(table.getFont()).stringWidth(getName()) + 8;
-        }
+    companion object {
+        fun isValVarColumn(column: ColumnInfo<*, *>?): Boolean = column is ValVarColumn
     }
 }
