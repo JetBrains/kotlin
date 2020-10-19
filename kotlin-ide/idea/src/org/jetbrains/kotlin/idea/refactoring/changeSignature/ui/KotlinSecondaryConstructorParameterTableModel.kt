bd3f6564ebc04e338c438c907aee8d25d8352522
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2020-10-19 17:41:23 +0700

    KotlinSecondaryConstructorParameterTableModel: convert to Kotlin

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/refactoring/changeSignature/ui/KotlinSecondaryConstructorParameterTableModel.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/refactoring/changeSignature/ui/KotlinSecondaryConstructorParameterTableModel.kt
@@ -13,27 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.idea.refactoring.changeSignature.ui
 
-package org.jetbrains.kotlin.idea.refactoring.changeSignature.ui;
+import com.intellij.psi.PsiElement
+import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinMethodDescriptor
+import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinParameterInfo
 
-import com.intellij.psi.PsiElement;
-import com.intellij.refactoring.changeSignature.ParameterTableModelItemBase;
-import org.jetbrains.kotlin.idea.KotlinFileType;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinMethodDescriptor;
-import org.jetbrains.kotlin.idea.refactoring.changeSignature.KotlinParameterInfo;
-
-public class KotlinSecondaryConstructorParameterTableModel extends KotlinCallableParameterTableModel {
-    public KotlinSecondaryConstructorParameterTableModel(
-            KotlinMethodDescriptor methodDescriptor,
-            PsiElement typeContext,
-            PsiElement defaultValueContext
-    ) {
-        super(methodDescriptor,
-              typeContext,
-              defaultValueContext,
-              new NameColumn(typeContext.getProject()),
-              new TypeColumn(typeContext.getProject(), KotlinFileType.INSTANCE),
-              new DefaultValueColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.getProject(),
-                                                                                                            KotlinFileType.INSTANCE));
-    }
-}
+class KotlinSecondaryConstructorParameterTableModel(
+    methodDescriptor: KotlinMethodDescriptor,
+    typeContext: PsiElement,
+    defaultValueContext: PsiElement
+) : KotlinCallableParameterTableModel(
+    methodDescriptor,
+    typeContext,
+    defaultValueContext,
+    NameColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.project),
+    TypeColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(typeContext.project, KotlinFileType.INSTANCE),
+    DefaultValueColumn<KotlinParameterInfo, ParameterTableModelItemBase<KotlinParameterInfo>>(
+        typeContext.project,
+        KotlinFileType.INSTANCE
+    )
+)
