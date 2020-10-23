8708bdec0d5d5390d5f9602f03f871f3d438196b
Jinseong Jeon <jsjeon@google.com>
2020-10-23 14:45:03 -0700

    FIR2IR: refactor implicit cast insertion, part 2: coerce-to-Unit

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
@@ -36,7 +36,6 @@ import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.expressions.impl.*
 import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.types.*
-import org.jetbrains.kotlin.ir.util.coerceToUnitIfNeeded
 import org.jetbrains.kotlin.ir.util.constructors
 import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.ir.util.parentClassOrNull
@@ -553,7 +552,11 @@ class Fir2IrVisitor(
                 } else {
                     emptyList()
                 }
-            ).insertImplicitCasts()
+            ).also {
+                with(implicitCastInserter) {
+                    it.insertImplicitCasts()
+                }
+            }
         }
     }
 
@@ -571,42 +574,19 @@ class Fir2IrVisitor(
                 IrCompositeImpl(
                     startOffset, endOffset, type, origin,
                     mapToIrStatements().filterNotNull()
-                ).insertImplicitCasts()
+                )
             } else {
                 IrBlockImpl(
                     startOffset, endOffset, type, origin,
                     mapToIrStatements().filterNotNull()
-                ).insertImplicitCasts()
-            }
-        }
-    }
-
-    private fun IrBlockBody.insertImplicitCasts(): IrBlockBody {
-        if (statements.isEmpty()) return this
-
-        statements.forEachIndexed { i, irStatement ->
-            if (irStatement !is IrErrorCallExpression && irStatement is IrExpression) {
-                statements[i] = irStatement.coerceToUnitIfNeeded(irStatement.type, irBuiltIns)
-            }
-        }
-        return this
-    }
-
-    private fun IrContainerExpression.insertImplicitCasts(): IrContainerExpression {
-        if (statements.isEmpty()) return this
-
-        val lastIndex = statements.lastIndex
-        statements.forEachIndexed { i, irStatement ->
-            if (irStatement !is IrErrorCallExpression && irStatement is IrExpression) {
-                if (i != lastIndex) {
-                    statements[i] = irStatement.coerceToUnitIfNeeded(irStatement.type, irBuiltIns)
-                } else {
-                    // TODO: for the last statement, need to cast to the return type if mismatched
+                )
+            }.also {
+                // TODO: can remove this once implicit cast inserter visits more expression kinds directly
+                with(implicitCastInserter) {
+                    it.insertImplicitCasts()
                 }
             }
         }
-
-        return this
     }
 
     override fun visitErrorExpression(errorExpression: FirErrorExpression, data: Any?): IrElement {
