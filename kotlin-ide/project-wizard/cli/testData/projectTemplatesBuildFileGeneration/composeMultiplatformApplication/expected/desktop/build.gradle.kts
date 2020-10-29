07f83c58dce756016f80d9f8b26a76414c183418
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-10-29 14:01:45 +0300

    Wizard: update compose templates
    ^KT-43032 fixed
    
    Original commit: 8eefcbb1d4891337bae872038f3cf799ab62bfc0

--- kotlin-ide/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/desktop/build.gradle.kts
+++ kotlin-ide/project-wizard/cli/testData/projectTemplatesBuildFileGeneration/composeMultiplatformApplication/expected/desktop/build.gradle.kts
@@ -1,22 +1,29 @@
 import org.jetbrains.compose.compose
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose") version "0.1.0-build63"
+    kotlin("multiplatform")
+    id("org.jetbrains.compose") version "0.1.0-dev106"
     application
 }
 
 group = "me.user"
 version = "1.0-SNAPSHOT"
 
-dependencies {
-    implementation(project(":common"))
-    implementation(compose.desktop.all)
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":common"))
+                implementation(compose.desktop.currentOs)
+            }
+        }
+        val jvmTest by getting
+    }
 }
 
 application {
