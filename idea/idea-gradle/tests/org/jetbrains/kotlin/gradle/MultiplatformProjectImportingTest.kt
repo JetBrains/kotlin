a0d5af8dd17e81f8e4d37d998bc7092b0e8913b9
Andrey Uskov <Andrey.Uskov@jetbrains.com>
2020-11-03 01:39:03 +0300

    Execute gutters calculation in read action in tests

--- idea/idea-gradle/tests/org/jetbrains/kotlin/gradle/MultiplatformProjectImportingTest.kt
+++ idea/idea-gradle/tests/org/jetbrains/kotlin/gradle/MultiplatformProjectImportingTest.kt
@@ -31,6 +31,7 @@ import org.jetbrains.kotlin.idea.util.rootManager
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.jetbrains.plugins.gradle.tooling.annotation.PluginTargetVersions
 import org.junit.Test
+import org.jetbrains.kotlin.idea.util.application.runReadAction
 
 class MultiplatformProjectImportingTest : MultiplePluginVersionGradleImportingTestCase() {
 
@@ -51,7 +52,9 @@ class MultiplatformProjectImportingTest : MultiplePluginVersionGradleImportingTe
     }
 
     private fun assertFileInModuleScope(file: VirtualFile, moduleName: String) {
-        assert(getModule(moduleName).getModuleWithDependenciesAndLibrariesScope(true).contains(file))
+        runReadAction {
+            assert(getModule(moduleName).getModuleWithDependenciesAndLibrariesScope(true).contains(file))
+        }
     }
 
     @Test
