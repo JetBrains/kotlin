d7ecd8d034340c2ef213e24f97aed980c105a38f
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2020-11-03 18:40:04 +0300

    [MPP] Allow smart casts for properties from dependsOn modules
    Smartcasts for public properties from different module are not
    stable because module declaring a property in general can be
    compiled separately from the module using it. However, if client
    module has dependsOn relation with declaring module their simultaneous
    compilation is guaranteed which makes this smart cast safe.
    
    Cache all transitive 'expected by' modules in module dependencies.
    Extend test to check smart casts are allowed for properties from transitive
    'expected by' dependencies and prohibited otherwise.
    
    ^KT-42754 Fixed
    
    Original commit: b79b94fe75e79c910d919a78af696ed52d057298

--- kotlin-ide/idea/testData/multiplatform/smartCastOnPropertyFromDependentModule/jvm2/jvm2.kt
+++ kotlin-ide/idea/testData/multiplatform/smartCastOnPropertyFromDependentModule/jvm2/jvm2.kt
@@ -1,9 +1,21 @@
-fun test(fromCommon: CommonDataClass, fromJvm: JvmDataClass) {
-    if (fromCommon.property != null) {
-        <!SMARTCAST_IMPOSSIBLE!>fromCommon.property<!>.doSomething()
+fun test(c1: CommonDataClass1, c2: CommonDataClass2, c3: CommonDataClass3, c4: CommonDataClass4, jvm: JvmDataClass) {
+    if (c1.property != null) {
+        <!DEBUG_INFO_SMARTCAST!>c1.property<!>.doSomething()
     }
 
-    if (fromJvm.property != null) {
-        <!SMARTCAST_IMPOSSIBLE!>fromJvm.property<!>.doSomething()
+    if (c2.property != null) {
+        <!DEBUG_INFO_SMARTCAST!>c2.property<!>.doSomething()
+    }
+
+    if (c3.property != null) {
+        <!DEBUG_INFO_SMARTCAST!>c3.property<!>.doSomething()
+    }
+
+    if (c4.property != null) {
+        <!DEBUG_INFO_SMARTCAST!>c4.property<!>.doSomething()
+    }
+
+    if (jvm.property != null) {
+        <!SMARTCAST_IMPOSSIBLE!>jvm.property<!>.doSomething()
     }
 }
