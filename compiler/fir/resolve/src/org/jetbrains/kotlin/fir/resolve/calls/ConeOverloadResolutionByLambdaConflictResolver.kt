a936386e53afe1bda9544984d628ca8767110141
Denis Zharkov <denis.zharkov@jetbrains.com>
2020-11-03 13:32:08 +0300

    FIR: Add more complicated workaround for OverloadResolutionByLambdaReturnType
    That helps for minOfOrNull/maxOfOrNull

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/ConeOverloadResolutionByLambdaConflictResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/ConeOverloadResolutionByLambdaConflictResolver.kt
@@ -7,7 +7,12 @@ package org.jetbrains.kotlin.fir.resolve.calls
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
+import org.jetbrains.kotlin.fir.declarations.FirFunction
 import org.jetbrains.kotlin.fir.resolve.fqName
+import org.jetbrains.kotlin.fir.resolve.inference.isBuiltinFunctionalType
+import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.fir.types.ConeTypeParameterType
+import org.jetbrains.kotlin.fir.types.coneTypeSafe
 import org.jetbrains.kotlin.resolve.descriptorUtil.OVERLOAD_RESOLUTION_BY_LAMBDA_ANNOTATION
 
 class ConeOverloadResolutionByLambdaConflictResolve(private val session: FirSession) : ConeCallConflictResolver() {
@@ -18,10 +23,20 @@ class ConeOverloadResolutionByLambdaConflictResolve(private val session: FirSess
     ): Set<Candidate> {
         if (candidates.size == 1) return candidates
 
-        candidates.singleOrNull { candidate ->
-            (candidate.symbol.fir as? FirCallableDeclaration<*>)?.annotations?.any {
-                it.fqName(session) == OVERLOAD_RESOLUTION_BY_LAMBDA_ANNOTATION
+        val (candidatesWithAnnotation, candidatesWithoutAnnotation) = candidates.partition { candidate ->
+            (candidate.symbol.fir as? FirCallableDeclaration<*>)?.annotations?.any { annotation ->
+                annotation.fqName(session) == OVERLOAD_RESOLUTION_BY_LAMBDA_ANNOTATION
             } == true
+        }
+
+        if (candidatesWithoutAnnotation.size == 1) return setOf(candidatesWithoutAnnotation.single())
+
+        candidatesWithAnnotation.singleOrNull { candidate ->
+            (candidate.symbol.fir as? FirFunction<*>)
+                ?.valueParameters?.singleOrNull()?.returnTypeRef?.coneTypeSafe<ConeKotlinType>()
+                .let { type ->
+                    type != null && type.isBuiltinFunctionalType(session) && type.typeArguments.last() is ConeTypeParameterType
+                }
         }?.let {
             return setOf(it)
         }
