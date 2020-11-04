b01ee163d11a1463e2daedafd7acb8bc10d70f73
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-11-04 16:57:16 +0300

    FIR IDE: wrap KotlinDeserializedJvmSymbolsProvider into thread safe cache
    This is needed by the two reasons:
    - KotlinDeserializedJvmSymbolsProvider does not cache all symbols by itself
    and as KtSymbol's holds fir elements under via weak refs, this weak refs
    may be garbage collected
    - KotlinDeserializedJvmSymbolsProvider is not thread safe

--- idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/annotations/annotations.kt
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/annotations/annotations.kt
@@ -5,6 +5,6 @@
 
 package org.jetbrains.kotlin.idea.fir.low.level.api.annotations
 
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
+@Target(AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.CLASS)
 @RequiresOptIn
 annotation class PrivateForInline
