7a993d5f73113b24c987a8556346951935cc2fc7
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2020-11-06 11:44:59 +0300

    Bumped versions for Wizards FUS collectors after adding the event for module templates
    (cherry picked from commit 24a356d42c570b44e53d0ab4262418a3d783968d)

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/projectWizard/WizardStatsService.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/projectWizard/WizardStatsService.kt
@@ -25,7 +25,7 @@ class WizardStatsService : CounterUsagesCollector() {
     companion object {
 
         // Collector ID
-        private val GROUP = EventLogGroup("kotlin.ide.new.project", 2)
+        private val GROUP = EventLogGroup("kotlin.ide.new.project", 3)
 
         // Whitelisted values for the events fields
         private val allowedProjectTemplates = listOf( // Modules
