e59c0fdd36cab16437153fe81a57432aeec1a7fc
Nikita Bobko <nikita.bobko@jetbrains.com>
2020-11-07 17:51:45 +0300

    Disable kotlin plugin modules

--- plugins/lint/build.gradle.kts
+++ plugins/lint/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 dependencies {
     compile(project(":compiler:frontend"))
-    compile(project(":idea"))
-    compile(project(":idea:idea-jvm"))
-    compile(project(":idea:idea-core"))
-    compile(project(":idea:idea-android"))
-    compile(project(":plugins:uast-kotlin"))
+    // compile(project(":idea"))
+    // compile(project(":idea:idea-jvm"))
+    // compile(project(":idea:idea-core"))
+    // compile(project(":idea:idea-android"))
+    // compile(project(":plugins:uast-kotlin"))
     compileOnly(project(":kotlin-android-extensions-runtime"))
     compileOnly(intellijDep())
     compileOnly(intellijPluginDep("android"))
