6d6805677bd226aa4fde2ba510a62e632a19b13d
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-11-10 14:36:01 +0300

    JSpecify: Update annotations in accordance with the jspecify repo

--- third-party/jdk8-annotations/org/jspecify/annotations/DefaultNonNull.java
+++ third-party/jdk8-annotations/org/jspecify/annotations/DefaultNonNull.java
@@ -1,19 +1,37 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ * Copyright 2018-2020 The JSpecify Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.jspecify.annotations;
+
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
 
 @Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE_USE, ElementType.PARAMETER, ElementType.CONSTRUCTOR, ElementType.PACKAGE, ElementType.TYPE})
-public @interface DefaultNonNull {
-
-}
+@Target({TYPE, PACKAGE})
+@Retention(CLASS)
+/**
+ * <b>WARNING:</b> This is not the final class name or package name for this annotation. In
+ * addition, we are still discussing questions about semantics, particularly around type-variable
+ * usages. After that, changes and documentation will follow.
+ *
+ * <p>These annotations exist only as a skeleton for the final product. At this point, we are not
+ * even building prototypes that use them.
+ */
+public @interface DefaultNonNull {}
--- third-party/jdk8-annotations/org/jspecify/annotations/NullnessUnspecified.java
+++ third-party/jdk8-annotations/org/jspecify/annotations/NullnessUnspecified.java
@@ -1,19 +1,35 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ * Copyright 2018-2020 The JSpecify Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.jspecify.annotations;
+
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
 
 @Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE_USE, ElementType.PARAMETER})
-public @interface NullnessUnspecified {
-
-}
+@Target(TYPE_USE)
+@Retention(CLASS)
+/**
+ * <b>WARNING:</b> This is not the final class name or package name for this annotation. We are not
+ * even sure if an annotation like this one will make the 1.0 release.
+ *
+ * <p>These annotations exist only as a skeleton for the final product. At this point, we are not
+ * even building prototypes that use them.
+ */
+public @interface NullnessUnspecified {}
