b2d35700301ed6ef4ceca9f5e89714dc3baf2250
Igor Chevdar <igor.chevdar@jetbrains.com>
2020-11-10 10:59:17 +0500

    Bumped K/N version

--- build.gradle.kts
+++ build.gradle.kts
@@ -187,7 +187,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.4.20-eap-99"
+    extra["versions.kotlin-native"] = "1.4.20-release-114"
 }
 
 val intellijUltimateEnabled by extra(project.kotlinBuildProperties.intellijUltimateEnabled)
