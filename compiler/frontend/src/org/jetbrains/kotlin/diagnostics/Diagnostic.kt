9040999b55ad4f5873518c23ae97b2801a3c1085
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-11-17 10:45:36 +0300

    Convert Diagnostic.java to Kotlin

--- compiler/frontend/src/org/jetbrains/kotlin/diagnostics/Diagnostic.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/diagnostics/Diagnostic.kt
@@ -13,32 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.diagnostics
 
-package org.jetbrains.kotlin.diagnostics;
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 
-import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-
-public interface Diagnostic {
-
-    @NotNull
-    DiagnosticFactory<?> getFactory();
-
-    @NotNull
-    Severity getSeverity();
-
-    @NotNull
-    PsiElement getPsiElement();
-
-    @NotNull
-    List<TextRange> getTextRanges();
-
-    @NotNull
-    PsiFile getPsiFile();
-
-    boolean isValid();
+interface Diagnostic {
+    val factory: DiagnosticFactory<*>
+    val severity: Severity
+    val psiElement: PsiElement
+    val textRanges: List<TextRange>
+    val psiFile: PsiFile
+    val isValid: Boolean
 }
