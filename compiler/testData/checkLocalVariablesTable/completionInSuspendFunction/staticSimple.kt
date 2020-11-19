bcb63212535291f35986604bd82c37fd98fed1db
Kristoffer Andersen <kjaa@google.com>
2020-11-19 14:25:17 +0100

    [Test] Port checkVariableTable suspend lambda tests
    Reveals discrepancy in LVT presence on lambda implementations on the
    old and new backend.
    
    The generated code in the constructors of Suspend Lambda objects is
    identical, but the IR backend generates an LVT with the constructor
    parameters.
    
    The user has to be very insistent to see this ("for step into" +
    disabling "Show only kotlin variables"), but it is an observable
    difference.

--- compiler/testData/checkLocalVariablesTable/completionInSuspendFunction/staticSimple.kt
+++
@@ -1,6 +0,0 @@
-// WITH_RUNTIME
-
-suspend fun foo() {}
-
-// METHOD : StaticSimpleKt.foo(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;
-// VARIABLE : NAME=$completion TYPE=Lkotlin/coroutines/Continuation; INDEX=0
---
+++ compiler/testData/debug/localVariables/suspend/completion/staticStateMachine.kt
@@ -0,0 +1,38 @@
+
+// WITH_COROUTINES
+// FILE: test.kt
+suspend fun foo() {}
+suspend fun foo1(l: Long) {
+    foo()
+    foo()
+    val dead = l
+}
+
+suspend fun box() {
+    foo1(42)
+}
+
+// The lambda object constructor has a local variables table on the IR backend.
+
+// LOCAL VARIABLES
+// test.kt:12 box: $completion:kotlin.coroutines.Continuation=helpers.ResultContinuation
+// test.kt:5 foo1:
+// CoroutineUtil.kt:28 getContext:
+
+// LOCAL VARIABLES JVM
+// test.kt:-1 <init>:
+// LOCAL VARIABLES JVM_IR
+// test.kt:-1 <init>: $completion:kotlin.coroutines.Continuation=helpers.ResultContinuation
+
+// LOCAL VARIABLES
+// test.kt:5 foo1:
+// test.kt:6 foo1: $continuation:kotlin.coroutines.Continuation=TestKt$foo1$1, $result:java.lang.Object=null, l:long=42:long
+// test.kt:4 foo: $completion:kotlin.coroutines.Continuation=TestKt$foo1$1
+// test.kt:6 foo1: $continuation:kotlin.coroutines.Continuation=TestKt$foo1$1, $result:java.lang.Object=null, l:long=42:long
+// test.kt:7 foo1: $continuation:kotlin.coroutines.Continuation=TestKt$foo1$1, $result:java.lang.Object=null, l:long=42:long
+// test.kt:4 foo: $completion:kotlin.coroutines.Continuation=TestKt$foo1$1
+// test.kt:7 foo1: $continuation:kotlin.coroutines.Continuation=TestKt$foo1$1, $result:java.lang.Object=null, l:long=42:long
+// test.kt:8 foo1: $continuation:kotlin.coroutines.Continuation=TestKt$foo1$1, $result:java.lang.Object=null, l:long=42:long
+// test.kt:9 foo1: $continuation:kotlin.coroutines.Continuation=TestKt$foo1$1, $result:java.lang.Object=null
+// test.kt:12 box: $completion:kotlin.coroutines.Continuation=helpers.ResultContinuation
+// test.kt:13 box: $completion:kotlin.coroutines.Continuation=helpers.ResultContinuation
