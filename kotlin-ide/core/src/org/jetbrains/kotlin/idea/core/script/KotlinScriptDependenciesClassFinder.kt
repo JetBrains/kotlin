468cc9519247ce55cc57a0ef1198eaaaf2efd250
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2020-11-24 12:33:14 +0300

    KotlinScriptDependenciesClassFinder optimization: caching results of the `everythingScope`
    KT-43554, KT-42949, KT-43230, KT-42690, KT-43057
    
    (cherry picked from commit 7bcbc719fb74d982679c0b81c3d4b63510426bf1)
    
    KT-CR-1866

--- kotlin-ide/core/src/org/jetbrains/kotlin/idea/core/script/KotlinScriptDependenciesClassFinder.kt
+++ kotlin-ide/core/src/org/jetbrains/kotlin/idea/core/script/KotlinScriptDependenciesClassFinder.kt
@@ -7,13 +7,19 @@ package org.jetbrains.kotlin.idea.core.script
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.roots.ProjectRootModificationTracker
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.NonClasspathClassFinder
 import com.intellij.psi.PsiClass
 import com.intellij.psi.impl.java.stubs.index.JavaFullClassNameIndex
 import com.intellij.psi.search.EverythingGlobalScope
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiModificationTracker
+import com.intellij.util.containers.ConcurrentFactoryMap
 import org.jetbrains.kotlin.resolve.jvm.KotlinSafeClassFinder
 
 
@@ -23,7 +29,26 @@ class KotlinScriptDependenciesClassFinder(
     override fun calcClassRoots(): List<VirtualFile> = ScriptConfigurationManager.getInstance(project)
         .getAllScriptsDependenciesClassFiles().filter { it.isValid }.toList()
 
+    private val everywhereCache = CachedValuesManager.getManager(project).createCachedValue {
+        CachedValueProvider.Result.create(
+            ConcurrentFactoryMap.createMap { qualifiedName: String ->
+                findClassInternal(qualifiedName, GlobalSearchScope.everythingScope(project))
+            },
+            PsiModificationTracker.MODIFICATION_COUNT, ProjectRootModificationTracker.getInstance(project), VirtualFileManager.getInstance()
+        )
+    }
+
     override fun findClass(qualifiedName: String, scope: GlobalSearchScope): PsiClass? {
+        val foundAnywhere = everywhereCache.value[qualifiedName]
+        if (foundAnywhere == null) {
+            return null // the most important thing for performance
+        }
+        foundAnywhere.isInScope(scope)?.let { return it }
+
+        return findClassInternal(qualifiedName, scope)
+    }
+
+    private fun findClassInternal(qualifiedName: String, scope: GlobalSearchScope): PsiClass? {
         val classByFileName = super.findClass(qualifiedName, scope)
         if (classByFileName != null) {
             return classByFileName.isInScope(scope)
@@ -35,7 +60,7 @@ class KotlinScriptDependenciesClassFinder(
             JavaFullClassNameIndex.getInstance().key,
             qualifiedName.hashCode(),
             project,
-            scope,
+            scope.takeUnless { it is EverythingGlobalScope },
             PsiClass::class.java
         ).filter {
             it.qualifiedName == qualifiedName
@@ -55,7 +80,7 @@ class KotlinScriptDependenciesClassFinder(
 
         val file = this.containingFile?.virtualFile ?: return null
         val index = ProjectFileIndex.SERVICE.getInstance(myProject)
-        if (index.isInContent(file) || index.isInLibrary(file)) {
+        if (index.isInContent(file) || index.isInLibrary(file) || !scope.contains(file)) {
             return null
         }
         return this
