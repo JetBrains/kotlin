2b91001a7d43ff6ad2b23a06ce2fe1a9afef2fa4
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2020-11-24 15:16:28 +0300

    Use specified version of Kotlin to compile benchmark scenarios

--- build-benchmarks/build.gradle.kts
+++ build-benchmarks/build.gradle.kts
@@ -7,8 +7,19 @@ plugins {
 }
 
 buildscript {
+    val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.4.255-SNAPSHOT"
+    val kotlinRepo = "https://buildserver.labs.intellij.net/guestAuth/app/rest/builds/buildType:(id:Kotlin_KotlinDev_CompilerDistAndMavenArtifacts),number:$kotlinVersion,branch:default:any/artifacts/content/maven"
+    extra["kotlinRepo"] = kotlinRepo
+
+    repositories {
+        mavenLocal()
+        maven {
+            url = uri(kotlinRepo)
+        }
+    }
+
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
@@ -16,9 +27,14 @@ apply {
     plugin("kotlin")
 }
 
+val kotlinRepo: String by extra
+
 repositories {
     maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
     mavenLocal()
+    maven {
+        url = uri(kotlinRepo)
+    }
 }
 
 dependencies {
@@ -33,4 +49,12 @@ application {
 tasks.register("runGavra0", JavaExec::class) {
     classpath = sourceSets.main.get().runtimeClasspath
     main = "RunGavra0BenchmarksKt"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
+    kotlinOptions {
+        freeCompilerArgs += listOf(
+            "-Xuse-experimental=kotlin.ExperimentalUnsignedTypes"
+        )
+    }
 }
