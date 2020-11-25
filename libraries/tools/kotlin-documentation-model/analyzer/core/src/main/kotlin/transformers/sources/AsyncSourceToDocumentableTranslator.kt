01d59017aa0010f288d9ebd378e14f3d79585799
Kamil Doległo <9080183+kamildoleglo@users.noreply.github.com>
2020-11-25 13:17:51 +0100

    [KDM] Remove suspend from SourceToDocumentableTranslator
    See https://github.com/Kotlin/dokka/pull/1622
    
    * Remove suspend from SourceToDocumentableTranslator
    
    * Introduce AsyncSourceToDocumentableTranslator
    
    Co-authored-by: Marcin Aman <marcin.aman@gmail.com>
    Original commit: https://github.com/Kotlin/dokka/commit/3fe3a934092cd844a6e0b691c87c367b9fc4d5b3

---
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/transformers/sources/AsyncSourceToDocumentableTranslator.kt
@@ -0,0 +1,15 @@
+package org.jetbrains.dokka.transformers.sources
+
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.dokka.DokkaConfiguration
+import org.jetbrains.dokka.model.DModule
+import org.jetbrains.dokka.plugability.DokkaContext
+
+interface AsyncSourceToDocumentableTranslator : SourceToDocumentableTranslator {
+    suspend fun invokeSuspending(sourceSet: DokkaConfiguration.DokkaSourceSet, context: DokkaContext): DModule
+
+    override fun invoke(sourceSet: DokkaConfiguration.DokkaSourceSet, context: DokkaContext): DModule =
+        runBlocking {
+            invokeSuspending(sourceSet, context)
+        }
+}
