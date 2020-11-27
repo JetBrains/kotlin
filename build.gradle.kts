29bed39a54c756f18cf1a18f759e5e2b1153997c
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-11-27 19:39:58 +0700

    Bump to native version version with watchos_x64 enabled

--- build.gradle.kts
+++ build.gradle.kts
@@ -188,7 +188,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.4.30-dev-17200"
+    extra["versions.kotlin-native"] = "1.4.30-dev-17395"
 }
 
 val intellijUltimateEnabled by extra(project.kotlinBuildProperties.intellijUltimateEnabled)
