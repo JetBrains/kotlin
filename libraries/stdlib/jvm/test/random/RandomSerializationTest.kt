1cb90d5ae0dc83cb58d8121ecba8f44f24c1c201
Iaroslav Postovalov <postovalovya@gmail.com>
2020-11-29 22:57:30 +0700

    Improve tests of Random implementations serialization

--- libraries/stdlib/jvm/test/random/RandomSerializationTest.kt
+++ libraries/stdlib/jvm/test/random/RandomSerializationTest.kt
@@ -24,6 +24,13 @@ class RandomSerializationTest {
     private fun testPersistedDeserialization(hexValue: String, expected: Any) =
         assertEquals(expected = expected, actual = deserializeFromHex(hexValue))
 
+    private fun testRandomsHaveSameState(first: Random, second: Random) {
+        assertEquals(first.nextInt(), second.nextInt())
+        assertEquals(first.nextDouble(), second.nextDouble())
+        assertEquals(first.nextLong(), second.nextLong())
+        assertEquals(first.nextBytes(64), second.nextBytes(64))
+    }
+
     @Test
     fun deserializeDefault() = testPersistedDeserialization(
         "ac ed 00 05 73 72 00 1c 6b 6f 74 6c 69 6e 2e 72 61 6e 64 6f 6d 2e 52 61 6e 64 6f 6d 24 44 65 66 61 75 6c 74 59 81 49 e9 14 4e a8 28 02 00 00 78 70",
@@ -34,20 +41,20 @@ class RandomSerializationTest {
     fun xorwowIsSerializable() {
         val instance = Random(0)
         val deserialized = serializeAndDeserialize(instance)
-        assertEquals(instance.nextInt(), deserialized.nextInt())
+        testRandomsHaveSameState(instance, deserialized)
     }
 
     @Test
     fun wrapperOfKotlinRandomIsSerializable() {
         val java = Random(0).asJavaRandom()
         val deserialized = serializeAndDeserialize(java)
-        assertEquals(java.nextInt(), deserialized.nextInt())
+        testRandomsHaveSameState(java, deserialized)
     }
 
     @Test
     fun wrapperOfJavaRandomIsSerializable() {
         val kotlin = java.util.Random(0).asKotlinRandom()
         val deserialized = serializeAndDeserialize(kotlin)
-        assertEquals(kotlin.nextInt(), deserialized.nextInt())
+        testRandomsHaveSameState(kotlin, deserialized)
     }
 }
