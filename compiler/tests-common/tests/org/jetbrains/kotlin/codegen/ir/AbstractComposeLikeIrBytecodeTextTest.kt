a7efa5c98bbe162d45cd8c671bb0d7215573425f
Mads Ager <ager@google.com>
2020-11-30 13:16:23 +0100

    [IR] Fix remapping of arguments in LocalDeclarationsLowering.
    It only remapped arguments for IrGetValue and not for IrSetValue.
    This is hitting Compose which has non-standard default argument
    handling.

---
+++ compiler/tests-common/tests/org/jetbrains/kotlin/codegen/ir/AbstractComposeLikeIrBytecodeTextTest.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.codegen.ir
+
+import org.jetbrains.kotlin.codegen.AbstractBytecodeTextTest
+import org.jetbrains.kotlin.test.TargetBackend
+
+abstract class AbstractComposeLikeIrBytecodeTextTest : AbstractBytecodeTextTest() {
+    override val backend = TargetBackend.JVM_IR
+}
