e940a1d6bb2427e1c8d5d311700585fd65aa9a3f
LepilkinaElena <Elena.Lepilkina@jetbrains.com>
2020-11-30 22:05:21 +0300

    Fix usages of ArgType.Choice in benchmarks (#4562)

--- kotlin-native/samples/videoplayer/src/videoPlayerMain/kotlin/VideoPlayer.kt
+++ kotlin-native/samples/videoplayer/src/videoPlayerMain/kotlin/VideoPlayer.kt
@@ -156,11 +156,17 @@ class VideoPlayer(val requestedSize: Dimensions?) : DisposableContainer() {
     }
 }
 
+enum class Mode {
+    VIDEO,
+    AUDIO,
+    BOTH
+}
+
 fun main(args: Array<String>) {
     val argParser = ArgParser("videoplayer")
     val mode by argParser.option(
-            ArgType.Choice(listOf("video", "audio", "both")), shortName = "m", description = "Play mode")
-            .default("both")
+            ArgType.Choice<Mode>(), shortName = "m", description = "Play mode")
+            .default(BOTH)
     val size by argParser.option(ArgType.Int, shortName = "s", description = "Required size of videoplayer window")
             .delimiter(",")
     val fileName by argParser.argument(ArgType.String, description = "File to play")
