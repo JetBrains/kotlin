1d77408ce85321e20a068f847080a3ee9f0dd3cc
Mads Ager <ager@google.com>
2020-11-30 16:41:07 +0100

    [JVM_IR] Add tests of Compose-like default argument handling.
    (cherry picked from commit 83588e9f221d6dcca95ba297d446acd9c5f758ee)

--- compiler/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerTests.kt
+++ compiler/tests/org/jetbrains/kotlin/generators/tests/GenerateCompilerTests.kt
@@ -256,8 +256,6 @@ fun main(args: Array<String>) {
                 model("ir/sourceRanges")
             }
 
-
-
             testClass<AbstractBytecodeListingTest> {
                 model("codegen/bytecodeListing", targetBackend = TargetBackend.JVM)
             }
@@ -465,6 +463,10 @@ fun main(args: Array<String>) {
 
             testClass<AbstractKotlinLexerTest> {
                 model("lexer/kotlin")
+            }
+
+            testClass<AbstractComposeLikeIrBlackBoxCodegenTest> {
+                model("codegen/composeLike", targetBackend = TargetBackend.JVM_IR)
             }
 
             testClass<AbstractIrBlackBoxCodegenTest> {
