7d29ae7cce36cebc5e327c8f8dc15b0f1304300b
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-11-30 10:32:29 +0100

    FIR: add incremental compilation tests

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1593,6 +1593,7 @@ fun main(args: Array<String>) {
             }
             testClass<AbstractIncrementalJvmCompilerRunnerTest>(init = incrementalJvmTestData(TargetBackend.JVM_IR))
             testClass<AbstractIncrementalJvmOldBackendCompilerRunnerTest>(init = incrementalJvmTestData(TargetBackend.JVM))
+            testClass<AbstractIncrementalFirJvmCompilerRunnerTest>(init = incrementalJvmTestData(TargetBackend.JVM_IR))
 
             testClass<AbstractIncrementalJsCompilerRunnerTest> {
                 model("incremental/pureKotlin", extension = null, recursive = false)
