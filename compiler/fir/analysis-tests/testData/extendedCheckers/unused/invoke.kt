aa0cafb9914f09c6bc522e4a0c68c4bdc5698e7e
Jinseong Jeon <jsjeon@google.com>
2020-12-01 15:58:00 -0800

    FIR checker: make unused checker handle invoke properly
    #KT-43688 Fixed

--- compiler/fir/analysis-tests/testData/extendedCheckers/unused/invoke.kt
+++ compiler/fir/analysis-tests/testData/extendedCheckers/unused/invoke.kt
@@ -1,5 +1,5 @@
 fun foo(): Int {
-    val <!UNUSED_VARIABLE!>x<!> = fun() = 4
-    val <!UNUSED_VARIABLE!>y<!> = fun() = 2
+    val x = fun() = 4
+    val y = fun() = 2
     return 10 * x() + y()
 }
