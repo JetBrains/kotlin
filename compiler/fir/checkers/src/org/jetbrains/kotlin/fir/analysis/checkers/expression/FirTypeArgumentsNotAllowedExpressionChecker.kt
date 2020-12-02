4626f21c585c940dad08afea722e2b4a03419ec7
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-12-02 13:21:35 +0300

    Record type arguments for FirResolvedQualifier

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirTypeArgumentsNotAllowedExpressionChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirTypeArgumentsNotAllowedExpressionChecker.kt
@@ -5,22 +5,12 @@
 
 package org.jetbrains.kotlin.fir.analysis.checkers.expression
 
-import com.intellij.lang.LighterASTNode
-import com.intellij.openapi.util.Ref
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiErrorElement
-import com.intellij.util.diff.FlyweightCapableTreeStructure
-import org.jetbrains.kotlin.KtNodeTypes.TYPE_ARGUMENT_LIST
-import org.jetbrains.kotlin.fir.FirLightSourceElement
 import org.jetbrains.kotlin.fir.FirSourceElement
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
-import org.jetbrains.kotlin.fir.analysis.checkers.getChildren
 import org.jetbrains.kotlin.fir.analysis.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.expressions.FirQualifiedAccessExpression
 import org.jetbrains.kotlin.fir.expressions.FirResolvedQualifier
-import org.jetbrains.kotlin.fir.psi
-import org.jetbrains.kotlin.psi.KtTypeArgumentList
 
 object FirTypeArgumentsNotAllowedExpressionChecker : FirQualifiedAccessChecker() {
     override fun check(expression: FirQualifiedAccessExpression, context: CheckerContext, reporter: DiagnosticReporter) {
@@ -29,34 +19,11 @@ object FirTypeArgumentsNotAllowedExpressionChecker : FirQualifiedAccessChecker()
         val explicitReceiver = expression.explicitReceiver
 
         if (explicitReceiver is FirResolvedQualifier && explicitReceiver.symbol == null) {
-            if (explicitReceiver.source?.hasAnyArguments() == true) {
+            if (explicitReceiver.typeArguments.isNotEmpty()) {
                 reporter.report(explicitReceiver.source)
                 return
             }
         }
-    }
-
-    private fun FirSourceElement.hasAnyArguments(): Boolean {
-        val localPsi = this.psi
-        val localLight = this.lighterASTNode
-
-        if (localPsi != null && localPsi !is PsiErrorElement) {
-            return localPsi.hasAnyArguments()
-        } else if (this is FirLightSourceElement) {
-            return localLight.hasAnyArguments(this.treeStructure)
-        }
-
-        return false
-    }
-
-    private fun PsiElement.hasAnyArguments(): Boolean {
-        val children = this.children // this is a method call and it collects children
-        return children.size > 1 && children[1] is KtTypeArgumentList
-    }
-
-    private fun LighterASTNode.hasAnyArguments(tree: FlyweightCapableTreeStructure<LighterASTNode>): Boolean {
-        val children = getChildren(tree)
-        return children.count { it != null } > 1 && children[1]?.tokenType == TYPE_ARGUMENT_LIST
     }
 
     private fun DiagnosticReporter.report(source: FirSourceElement?) {
