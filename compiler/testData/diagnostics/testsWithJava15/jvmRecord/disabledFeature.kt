a0478cc75b4bd4271535b7ecb2c9348a1a91624e
Denis.Zharkov <denis.zharkov@jetbrains.com>
2020-12-02 13:29:42 +0300

    Check JvmRecordSupport language feature before generating synthetic properties

--- compiler/testData/diagnostics/testsWithJava15/jvmRecord/disabledFeature.kt
+++ compiler/testData/diagnostics/testsWithJava15/jvmRecord/disabledFeature.kt
@@ -1,5 +1,8 @@
 // !LANGUAGE: -JvmRecordSupport
 // SKIP_TXT
+// FILE: JRecord.java
+public record JRecord(int x, CharSequence y) {}
+// FILE: main.kt
 
 <!UNSUPPORTED_FEATURE!>@JvmRecord<!>
 class MyRec(
@@ -7,3 +10,13 @@ class MyRec(
     val y: Int,
     vararg val z: Double,
 )
+
+fun foo(jr: JRecord) {
+    JRecord(1, "")
+
+    jr.x()
+    jr.y()
+
+    jr.<!FUNCTION_CALL_EXPECTED!>x<!>
+    jr.<!FUNCTION_CALL_EXPECTED!>y<!>
+}
