04777d2ff202347bb6f0fa5a7a45e380c9d96a41
Roman Artemev <roman.artemev@jetbrains.com>
2020-12-04 12:59:14 +0300

    [IC TEST] Generate legacy js multi module IC tests

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -1534,6 +1534,10 @@ fun main(args: Array<String>) {
                 model("incremental/js", extension = null, excludeParentDirs = true)
             }
 
+            testClass<AbstractIncrementalMultiModuleJsCompilerRunnerTest> {
+                model("incremental/multiModule/common", extension = null, excludeParentDirs = true)
+            }
+
             testClass<AbstractIncrementalMultiModuleJsKlibCompilerRunnerTest> {
                 model("incremental/multiModule/common", extension = null, excludeParentDirs = true)
             }
