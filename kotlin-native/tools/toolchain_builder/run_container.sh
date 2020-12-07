e6d202a2aa7d0de74a64292c0cc331265b124511
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-12-07 12:36:16 +0700

    More robust toolchain builder script

--- kotlin-native/tools/toolchain_builder/run_container.sh
+++ kotlin-native/tools/toolchain_builder/run_container.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eou pipefail
 
 CONTAINER_NAME=kotlin-toolchain-builder
@@ -6,13 +6,9 @@ IMAGE_NAME=kotlin-toolchain-builder
 TARGET=$1
 VERSION=$2
 
-if ! "docker ps -a | grep $CONTAINER_NAME";
-then
-  echo "Removing previous container..."
-  docker stop $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
-  echo "Done."
-fi
+docker ps -a | grep $CONTAINER_NAME > /dev/null \
+  && docker stop $CONTAINER_NAME > /dev/null \
+  && docker rm $CONTAINER_NAME > /dev/null
 
 echo "Running build script in container..."
 docker run -it -v "$PWD"/artifacts:/artifacts --env TARGET="$TARGET" --env VERSION="$VERSION" --name=$CONTAINER_NAME $IMAGE_NAME
