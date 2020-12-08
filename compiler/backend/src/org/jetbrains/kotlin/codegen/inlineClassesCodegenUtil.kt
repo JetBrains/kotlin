9795d13b9c980a9776c073ae230d7a15ca91cc11
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2020-12-08 15:25:33 +0100

    IC Mangling: search parents for method if descriptor is fake override
    Otherwise, the compiler generates call using old mangling scheme
    because classfile does not contain the method.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/inlineClassesCodegenUtil.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/inlineClassesCodegenUtil.kt
@@ -6,10 +6,7 @@
 package org.jetbrains.kotlin.codegen
 
 import org.jetbrains.kotlin.codegen.state.GenerationState
-import org.jetbrains.kotlin.descriptors.CallableDescriptor
-import org.jetbrains.kotlin.descriptors.ClassDescriptor
-import org.jetbrains.kotlin.descriptors.FunctionDescriptor
-import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
+import org.jetbrains.kotlin.descriptors.*
 import org.jetbrains.kotlin.descriptors.impl.AnonymousFunctionDescriptor
 import org.jetbrains.kotlin.load.kotlin.JvmPackagePartSource
 import org.jetbrains.kotlin.load.kotlin.VirtualFileFinder
@@ -43,6 +40,10 @@ fun CallableDescriptor.isGenericParameter(): Boolean {
 
 fun classFileContainsMethod(descriptor: FunctionDescriptor, state: GenerationState, method: Method): Boolean? {
     if (descriptor !is DeserializedSimpleFunctionDescriptor) return null
+
+    if (descriptor.kind == CallableMemberDescriptor.Kind.FAKE_OVERRIDE) {
+        return descriptor.overriddenDescriptors.any { classFileContainsMethod(it, state, method) == true }
+    }
 
     val classId: ClassId = when {
         descriptor.containingDeclaration is DeserializedClassDescriptor -> {
