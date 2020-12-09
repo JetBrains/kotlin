3a997c607b8cbdd22f1a1bfd85abb25c94405796
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2020-12-09 16:59:47 +0300

    Exclude test for old JS compiler

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/semantics/JsCodegenBoxTestGenerated.java
@@ -17933,11 +17933,6 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
             public void testAnotherFile() throws Exception {
                 runTest("compiler/testData/codegen/box/properties/const/anotherFile.kt");
             }
-
-            @TestMetadata("constPropertyAccessor.kt")
-            public void testConstPropertyAccessor() throws Exception {
-                runTest("compiler/testData/codegen/box/properties/const/constPropertyAccessor.kt");
-            }
         }
 
         @TestMetadata("compiler/testData/codegen/box/properties/lateinit")
