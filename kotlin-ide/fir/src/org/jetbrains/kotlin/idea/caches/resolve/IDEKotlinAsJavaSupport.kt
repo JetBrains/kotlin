1cb0e05d7df0405c0e7ecc40880d52e3cd94aed7
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2020-12-11 21:40:16 +0300

    [FIR IDE] LC add caching to light facades
    Original commit: f5d8ae0550a3eb0ed17422379237145741b1156b

--- kotlin-ide/fir/src/org/jetbrains/kotlin/idea/caches/resolve/IDEKotlinAsJavaSupport.kt
+++ kotlin-ide/fir/src/org/jetbrains/kotlin/idea/caches/resolve/IDEKotlinAsJavaSupport.kt
@@ -6,25 +6,32 @@
 package org.jetbrains.kotlin.idea.caches.resolve
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.UserDataHolder
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import org.jetbrains.kotlin.analyzer.KotlinModificationTrackerService
 import org.jetbrains.kotlin.asJava.classes.KtLightClass
 import org.jetbrains.kotlin.idea.asJava.FirLightClassForFacade
+import org.jetbrains.kotlin.idea.asJava.classes.createFirLightClassNoCache
 import org.jetbrains.kotlin.idea.asJava.classes.getOrCreateFirLightClass
+import org.jetbrains.kotlin.idea.asJava.classes.getOrCreateFirLightFacade
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtScript
 
 class IDEKotlinAsJavaFirSupport(project: Project) : IDEKotlinAsJavaSupport(project) {
 
-    //TODO Make caching
     override fun createLightClassForFacade(manager: PsiManager, facadeClassFqName: FqName, searchScope: GlobalSearchScope): KtLightClass? {
         val sources = findFilesForFacade(facadeClassFqName, searchScope)
             .filterNot { it.isCompiled }
 
         if (sources.isEmpty()) return null
 
-        return FirLightClassForFacade(manager, facadeClassFqName, sources)
+        return getOrCreateFirLightFacade(sources, facadeClassFqName)
     }
 
     override fun createLightClassForScript(script: KtScript): KtLightClass? = null
