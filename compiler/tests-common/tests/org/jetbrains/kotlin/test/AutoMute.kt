d9848544dc16a45ffe3d0b8239b17867a804c3db
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-14 12:15:23 +0300

    [TEST] Move auto mute wrapping utils to :compiler:tests-mutes

--- compiler/tests-common/tests/org/jetbrains/kotlin/test/AutoMute.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/test/AutoMute.kt
@@ -5,54 +5,12 @@
 
 package org.jetbrains.kotlin.test
 
+import org.jetbrains.kotlin.test.mutes.DO_AUTO_MUTE
+import org.jetbrains.kotlin.test.mutes.muteTest
 import org.junit.runner.notification.Failure
 import org.junit.runner.notification.RunListener
 import org.junit.runner.notification.RunNotifier
 import java.io.File
-
-class AutoMute(
-    val file: String,
-    val issue: String
-)
-
-val DO_AUTO_MUTE: AutoMute? by lazy {
-    val autoMuteFile = File("tests/automute")
-    if (autoMuteFile.exists()) {
-        val lines = autoMuteFile.readLines().filter { it.isNotBlank() }.map { it.trim() }
-        AutoMute(
-            lines.getOrNull(0) ?: error("A file path is expected in tne first line"),
-            lines.getOrNull(1) ?: error("An issue description is the second line")
-        )
-    } else {
-        null
-    }
-}
-
-fun AutoMute.muteTest(testKey: String) {
-    val file = File(file)
-    val lines = file.readLines()
-    val firstLine = lines[0] // Drop file header
-    val muted = lines.drop(1).toMutableList()
-    muted.add("$testKey, $issue")
-    val newMuted: List<String> = mutableListOf<String>() + firstLine + muted.sorted()
-    file.writeText(newMuted.joinToString("\n"))
-}
-
-internal fun wrapWithAutoMute(f: () -> Unit, testKey: String): (() -> Unit)? {
-    val doAutoMute = DO_AUTO_MUTE
-    if (doAutoMute != null) {
-        return {
-            try {
-                f()
-            } catch (e: Throwable) {
-                doAutoMute.muteTest(testKey)
-                throw e
-            }
-        }
-    } else {
-        return null
-    }
-}
 
 internal inline fun RunNotifier.withAutoMuteListener(
     testKey: String,
