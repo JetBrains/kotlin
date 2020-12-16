02c617468fa29c3e242ac0ba342cd1fafd0467ad
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-12-16 19:37:00 +0100

    Add support for a callback on recursion for memoized functions

--- core/util.runtime/src/org/jetbrains/kotlin/storage/ObservableStorageManager.kt
+++ core/util.runtime/src/org/jetbrains/kotlin/storage/ObservableStorageManager.kt
@@ -26,12 +26,20 @@ abstract class ObservableStorageManager(private val delegate: StorageManager) : 
         return delegate.createMemoizedFunction(compute.observable)
     }
 
+    override fun <K, V : Any> createMemoizedFunction(compute: (K) -> V, onRecursiveCall: (K, Boolean) -> V): MemoizedFunctionToNotNull<K, V> {
+        return delegate.createMemoizedFunction(compute.observable, onRecursiveCall)
+    }
+
     override fun <K, V: Any> createMemoizedFunctionWithNullableValues(compute: (K) -> V?): MemoizedFunctionToNullable<K, V> {
         return delegate.createMemoizedFunctionWithNullableValues(compute.observable)
     }
 
     override fun <K, V: Any> createMemoizedFunction(compute: (K) -> V, map: ConcurrentMap<K, Any>): MemoizedFunctionToNotNull<K, V> {
         return delegate.createMemoizedFunction(compute.observable, map)
+    }
+
+    override fun <K, V : Any> createMemoizedFunction(compute: (K) -> V, onRecursiveCall: (K, Boolean) -> V, map: ConcurrentMap<K, Any>): MemoizedFunctionToNotNull<K, V> {
+        return delegate.createMemoizedFunction(compute.observable, onRecursiveCall, map)
     }
 
     override fun <K, V: Any> createMemoizedFunctionWithNullableValues(compute: (K) -> V, map: ConcurrentMap<K, Any>): MemoizedFunctionToNullable<K, V> {
