d32d0a65f0fdde69e5a8fde48e82a2a396cd8e9c
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-12-16 10:24:18 +0300

    Revert "Report warning on @JvmStatic in private companion objects"
    This reverts commit 9669ab14

--- compiler/testData/diagnostics/testsWithStdLib/annotations/jvmStatic/privateCompanionObject.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/jvmStatic/privateCompanionObject.kt
@@ -1,29 +1,30 @@
 // Issue: KT-25114
+// FIR_IDENTICAL
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 
 class WithPrivateCompanion {
     private companion object {
-        <!JVM_STATIC_IN_PRIVATE_COMPANION!>@JvmStatic<!>
+        @JvmStatic
         val staticVal1: Int = 42
 
         val staticVal2: Int
-            <!JVM_STATIC_IN_PRIVATE_COMPANION!>@JvmStatic<!> get() = 42
+            @JvmStatic get() = 42
 
-        <!JVM_STATIC_IN_PRIVATE_COMPANION!>@get:JvmStatic<!>
+        @get:JvmStatic
         val staticVal3: Int = 42
 
-        <!JVM_STATIC_IN_PRIVATE_COMPANION!>@JvmStatic<!>
+        @JvmStatic
         var staticVar1: Int = 42
 
         var staticVar2: Int
-            <!JVM_STATIC_IN_PRIVATE_COMPANION!>@JvmStatic<!> get() = 42
-            <!JVM_STATIC_IN_PRIVATE_COMPANION!>@JvmStatic<!> set(value) {}
+            @JvmStatic get() = 42
+            @JvmStatic set(value) {}
 
-        <!JVM_STATIC_IN_PRIVATE_COMPANION!>@get: JvmStatic<!>
-        <!JVM_STATIC_IN_PRIVATE_COMPANION!>@set: JvmStatic<!>
+        @get: JvmStatic
+        @set: JvmStatic
         var staticVar3: Int = 42
 
-        <!JVM_STATIC_IN_PRIVATE_COMPANION!>@JvmStatic<!>
+        @JvmStatic
         fun staticFunction() {}
     }
 }
