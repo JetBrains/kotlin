9a34c8bbe1f13c973a80a678db8020b533d9da8f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-12-17 13:04:12 +0300

    Allow KtEnumEntry...RefExpression.referencedElement be nullable
    This commit fixes KNPE provoked by RemoveExplicitTypeArgumentsIntention
    ^KT-29735 Fixed
    
    Original commit: d907c48d9ce378425f4a664a68ac0715fc2b3c08

--- kotlin-ide/idea/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
+++ kotlin-ide/idea/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
@@ -13513,6 +13513,11 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
             runTest("testData/intentions/removeExplicitTypeArguments/insideOtherCall.kt");
         }
 
+        @TestMetadata("kt29735.kt")
+        public void testKt29735() throws Exception {
+            runTest("idea/testData/intentions/removeExplicitTypeArguments/kt29735.kt");
+        }
+
         @TestMetadata("kt31441.kt")
         public void testKt31441() throws Exception {
             runTest("testData/intentions/removeExplicitTypeArguments/kt31441.kt");
