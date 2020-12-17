bff0591f30612fc9a14a17cfbf5986eb39ec6310
Marcin Aman <marcin.aman@gmail.com>
2020-12-17 12:14:40 +0100

    [KDM] Multimodule tests
    See https://github.com/Kotlin/dokka/pull/1670
    
    * Multimodule tests
    
    * Multimodule tests
    Original commit: https://github.com/Kotlin/dokka/commit/fea7c42733a339ed19fc7471bb064f53de71cc6b

--- libraries/tools/kotlin-documentation-model/analyzer/core/test-api/src/main/kotlin/testApi/testRunner/TestRunner.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/test-api/src/main/kotlin/testApi/testRunner/TestRunner.kt
@@ -7,7 +7,6 @@ import org.jetbrains.dokka.pages.RootPageNode
 import org.jetbrains.dokka.plugability.DokkaContext
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.jetbrains.dokka.testApi.logger.TestLogger
-import org.jetbrains.dokka.utilities.DokkaConsoleLogger
 import org.jetbrains.dokka.utilities.DokkaLogger
 import org.junit.rules.TemporaryFolder
 import testApi.testRunner.TestDokkaConfigurationBuilder
@@ -20,7 +19,7 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 // TODO: take dokka configuration from file
-abstract class AbstractTest< M: TestMethods, T : TestBuilder<M>, D: DokkaTestGenerator<M> >(
+abstract class AbstractTest<M : TestMethods, T : TestBuilder<M>, D : DokkaTestGenerator<M>>(
     protected val testBuilder: () -> T,
     protected val dokkaTestGenerator: (DokkaConfiguration, DokkaLogger, M, List<DokkaPlugin>) -> D,
     protected val logger: TestLogger,
@@ -32,19 +31,22 @@ abstract class AbstractTest< M: TestMethods, T : TestBuilder<M>, D: DokkaTestGen
     protected fun testFromData(
         configuration: DokkaConfigurationImpl,
         cleanupOutput: Boolean = true,
+        preserveOutputLocation: Boolean = false,
         pluginOverrides: List<DokkaPlugin> = emptyList(),
         block: T.() -> Unit
     ) {
         val testMethods = testBuilder().apply(block).build()
-        val tempDir = getTempDir(cleanupOutput)
-        if (!cleanupOutput)
-            logger.info("Output generated under: ${tempDir.root.absolutePath}")
-        val newConfiguration =
+        val configurationToUse = if (!preserveOutputLocation) {
+            val tempDir = getTempDir(cleanupOutput)
+            if (!cleanupOutput)
+                logger.info("Output generated under: ${tempDir.root.absolutePath}")
             configuration.copy(
                 outputDir = tempDir.root
             )
+        } else configuration
+
         dokkaTestGenerator(
-            newConfiguration,
+            configurationToUse,
             logger,
             testMethods,
             pluginOverrides
@@ -169,7 +171,9 @@ abstract class AbstractTest< M: TestMethods, T : TestBuilder<M>, D: DokkaTestGen
     }
 }
 
-open class TestMethods(
+interface TestMethods
+
+open class CoreTestMethods(
     open val pluginsSetupStage: (DokkaContext) -> Unit,
     open val verificationStage: (() -> Unit) -> Unit,
     open val documentablesCreationStage: (List<DModule>) -> Unit,
@@ -178,13 +182,13 @@ open class TestMethods(
     open val pagesGenerationStage: (RootPageNode) -> Unit,
     open val pagesTransformationStage: (RootPageNode) -> Unit,
     open val renderingStage: (RootPageNode, DokkaContext) -> Unit
-)
+) : TestMethods
 
-abstract class TestBuilder<M: TestMethods> {
+abstract class TestBuilder<M : TestMethods> {
     abstract fun build(): M
 }
 
-abstract class DokkaTestGenerator<T: TestMethods>(
+abstract class DokkaTestGenerator<T : TestMethods>(
     protected val configuration: DokkaConfiguration,
     protected val logger: DokkaLogger,
     protected val testMethods: T,
