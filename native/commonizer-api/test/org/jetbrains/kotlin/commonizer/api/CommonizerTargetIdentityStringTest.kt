705a488291192f967e9ef1dfe3410bdf271e906e
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2020-12-22 11:23:11 +0100

    fixup! Introduce user defined library commonization through "interop-bundles"

--- native/commonizer-api/test/org/jetbrains/kotlin/commonizer/api/CommonizerTargetIdentityStringTest.kt
+++ native/commonizer-api/test/org/jetbrains/kotlin/commonizer/api/CommonizerTargetIdentityStringTest.kt
@@ -63,4 +63,76 @@ class CommonizerTargetIdentityStringTest {
         assertEquals(hierarchy, parseCommonizerTarget(hierarchy.identityString))
     }
 
+    @Test
+    fun `parsing CommonizerTarget`() {
+        val target = parseCommonizerTarget("[x, [x, y, [a, b], [b, c]]]")
+        assertEquals(
+            SharedCommonizerTarget(
+                LeafCommonizerTarget("x"),
+                SharedCommonizerTarget(
+                    LeafCommonizerTarget("x"),
+                    LeafCommonizerTarget("y"),
+                    SharedCommonizerTarget(
+                        LeafCommonizerTarget("a"),
+                        LeafCommonizerTarget("b"),
+                    ),
+                    SharedCommonizerTarget(
+                        LeafCommonizerTarget("b"),
+                        LeafCommonizerTarget("c")
+                    )
+                )
+            ),
+            target
+        )
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 1`() {
+        parseCommonizerTarget("xxx,")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 2`() {
+        parseCommonizerTarget("")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 3`() {
+        parseCommonizerTarget("[]")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 4`() {
+        parseCommonizerTarget("[xxx")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 5`() {
+        parseCommonizerTarget("xxx]")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 6`() {
+        parseCommonizerTarget("[xxx")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 7`() {
+        parseCommonizerTarget("[xxx yyy]")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 8`() {
+        parseCommonizerTarget(" ")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 9`() {
+        parseCommonizerTarget("xxx?")
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `fail parsing CommonizerTarget 10`() {
+        parseCommonizerTarget("[x, [x, y]")
+    }
 }
