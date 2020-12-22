bd139d2257bc0efc3beb83cd762f5a1262b77701
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2020-12-22 18:58:55 +0300

    ~ Fix naming for local decl

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrFunctionToJsTransformer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/IrFunctionToJsTransformer.kt
@@ -6,16 +6,18 @@
 package org.jetbrains.kotlin.ir.backend.js.transformers.irToJs
 
 import org.jetbrains.kotlin.ir.backend.js.utils.JsGenerationContext
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrConstructor
-import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.js.backend.ast.JsFunction
 
 @Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
 class IrFunctionToJsTransformer : BaseIrElementToJsNodeTransformer<JsFunction, JsGenerationContext> {
     override fun visitSimpleFunction(declaration: IrSimpleFunction, context: JsGenerationContext): JsFunction {
         val funcName = if (declaration.dispatchReceiverParameter == null) {
-            context.getNameForStaticFunction(declaration)
+            if (declaration.parent is IrFunction) {
+                context.getNameForLocalDeclaration(declaration)
+            } else {
+                context.getNameForStaticFunction(declaration)
+            }
         } else {
             context.getNameForMemberFunction(declaration)
         }
