531ba4bb482866a8066e4b5851cb907138e64c6b
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-12-24 21:28:20 +0300

    [IR] Narrow usage scope of ObsoleteDescriptorBasedAPI in CheckIrElementVisitor

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CheckIrElementVisitor.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/CheckIrElementVisitor.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.backend.common
@@ -32,7 +21,6 @@ import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 
 typealias ReportError = (element: IrElement, message: String) -> Unit
 
-@OptIn(ObsoleteDescriptorBasedAPI::class)
 class CheckIrElementVisitor(
     val irBuiltIns: IrBuiltIns,
     val reportError: ReportError,
@@ -260,6 +248,7 @@ class CheckIrElementVisitor(
         expression.ensureTypeIs(irBuiltIns.nothingType)
     }
 
+    @OptIn(ObsoleteDescriptorBasedAPI::class)
     override fun visitClass(declaration: IrClass) {
         super.visitClass(declaration)
 
