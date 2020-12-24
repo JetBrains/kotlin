95c4cd4c494559ca172f20a701077063381de5ba
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-24 17:52:57 +0300

    Update testdata of PsiCheckerSealedTest after 07b980fe
    Original commit: daa6255ab79453dd07040c32797a0e68fe2fd6e6

--- kotlin-ide/idea/testData/checker/sealed/SealedOutsidePackageInheritors.kt
+++ kotlin-ide/idea/testData/checker/sealed/SealedOutsidePackageInheritors.kt
@@ -3,5 +3,5 @@ package sealed.otherpackage
 import sealed.SealedDeclarationInterface
 import sealed.SealedDeclarationClass
 
-class D: <error descr="[SEALED_INHERITOR_IN_DIFFERENT_PACKAGE] Inheritor of sealed class or interface declared in package sealed but it must be in package {2} where base class is declared">SealedDeclarationInterface</error>
-class DClass: <error descr="[SEALED_INHERITOR_IN_DIFFERENT_PACKAGE] Inheritor of sealed class or interface declared in package sealed but it must be in package {2} where base class is declared">SealedDeclarationClass</error>()
+class D: <error descr="[SEALED_INHERITOR_IN_DIFFERENT_PACKAGE] Inheritor of sealed class or interface declared in package sealed.otherpackage but it must be in package sealed where base class is declared">SealedDeclarationInterface</error>
+class DClass: <error descr="[SEALED_INHERITOR_IN_DIFFERENT_PACKAGE] Inheritor of sealed class or interface declared in package sealed.otherpackage but it must be in package sealed where base class is declared">SealedDeclarationClass</error>()
