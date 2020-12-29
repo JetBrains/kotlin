14e65cdfec85ff3b406fc6b49c8992ea01d24ece
Denis.Zharkov <denis.zharkov@jetbrains.com>
2020-12-29 18:17:14 +0300

    Revert "Fix ISE when inferring type of a property that delegates to itself"
    This reverts commit 42e253929f39d03e4edc763865d725f678197583.
    
    The reason is that original change seems to be breaking (see KT-44137).
    
    ^KT-44137 Relates
    ^KT-37796 Open

--- compiler/tests-gen/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -6001,11 +6001,6 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTestWithFirVali
                 runTest("compiler/testData/diagnostics/tests/delegatedProperty/incompleteTypeInference.kt");
             }
 
-            @TestMetadata("kt37796.kt")
-            public void testKt37796() throws Exception {
-                runTest("compiler/testData/diagnostics/tests/delegatedProperty/kt37796.kt");
-            }
-
             @TestMetadata("kt4640.kt")
             public void testKt4640() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/delegatedProperty/kt4640.kt");
--- compiler/tests-gen/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/checkers/javac/DiagnosticsUsingJavacTestGenerated.java
@@ -5996,11 +5996,6 @@ public class DiagnosticsUsingJavacTestGenerated extends AbstractDiagnosticsUsing
                 runTest("compiler/testData/diagnostics/tests/delegatedProperty/incompleteTypeInference.kt");
             }
 
-            @TestMetadata("kt37796.kt")
-            public void testKt37796() throws Exception {
-                runTest("compiler/testData/diagnostics/tests/delegatedProperty/kt37796.kt");
-            }
-
             @TestMetadata("kt4640.kt")
             public void testKt4640() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/delegatedProperty/kt4640.kt");
