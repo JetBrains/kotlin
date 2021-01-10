d304ca8b878fa3a5b1113a355ec7f0332ff2a3ea
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-01-10 15:51:17 +0100

    WIP! Return back idea-runners (run IDEA without gradle?)

--- idea-runner/build.gradle.kts
+++ idea-runner/build.gradle.kts
@@ -5,19 +5,13 @@ plugins {
 }
 
 dependencies {
-    compileOnly(project(":idea"))
-    compileOnly(project(":idea:idea-maven"))
-    compileOnly(project(":idea:idea-gradle"))
-    compileOnly(project(":idea:idea-jvm"))
-
     runtimeOnly(intellijDep())
     runtimeOnly(intellijRuntimeAnnotations())
     runtimeOnly(toolsJar())
 }
 
-val ideaPluginDir: File by rootProject.extra
 val ideaSandboxDir: File by rootProject.extra
 
-runIdeTask("runIde", ideaPluginDir, ideaSandboxDir) {
-    dependsOn(":dist", ":ideaPlugin")
+runIdeTask("runIde", rootDir.resolve("out/artifacts/KotlinPluginCommunity"), ideaSandboxDir) {
+    dependsOn(":jarsForIde")
 }
