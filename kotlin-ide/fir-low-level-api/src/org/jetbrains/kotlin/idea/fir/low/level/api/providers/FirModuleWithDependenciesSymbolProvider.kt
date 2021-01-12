a0fb1135ccbaf720bf2ee06796fba209f6d41e87
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-01-12 13:45:45 +0100

    FIR IDE: implement new getTopLevelFunction(/property)Symbols in FirModuleWithDependenciesSymbolProvider
    Original commit: a5e6c1d82b1aa4486e61c531e676b66a39fe04d7

--- kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/providers/FirModuleWithDependenciesSymbolProvider.kt
+++ kotlin-ide/fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/providers/FirModuleWithDependenciesSymbolProvider.kt
@@ -45,18 +45,28 @@ internal class FirModuleWithDependenciesSymbolProvider(
 
     @FirSymbolProviderInternals
     override fun getTopLevelFunctionSymbolsTo(destination: MutableList<FirNamedFunctionSymbol>, packageFqName: FqName, name: Name) {
-        providers.forEach { it.getTopLevelFunctionSymbolsTo(destination, packageFqName, name) }
-        withDependent {
-            dependentProviders.forEach { it.getTopLevelFunctionSymbolsTo(destination, packageFqName, name) }
-        }
+        getTopLevelFunctionSymbolsToWithoutDependencies(destination, packageFqName, name)
+        dependentProviders.getTopLevelFunctionSymbolsTo(destination, packageFqName, name)
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelPropertySymbolsTo(destination: MutableList<FirPropertySymbol>, packageFqName: FqName, name: Name) {
+        getTopLevelPropertySymbolsToWithoutDependencies(destination, packageFqName, name)
+        dependentProviders.getTopLevelPropertySymbolsTo(destination, packageFqName, name)
+    }
+
+    @FirSymbolProviderInternals
+    fun getTopLevelFunctionSymbolsToWithoutDependencies(
+        destination: MutableList<FirNamedFunctionSymbol>,
+        packageFqName: FqName,
+        name: Name
+    ) {
+        providers.forEach { it.getTopLevelFunctionSymbolsTo(destination, packageFqName, name) }
+    }
+
+    @FirSymbolProviderInternals
+    fun getTopLevelPropertySymbolsToWithoutDependencies(destination: MutableList<FirPropertySymbol>, packageFqName: FqName, name: Name) {
         providers.forEach { it.getTopLevelPropertySymbolsTo(destination, packageFqName, name) }
-        withDependent {
-            dependentProviders.forEach { it.getTopLevelPropertySymbolsTo(destination, packageFqName, name) }
-        }
     }
 
     override fun getPackage(fqName: FqName): FqName? =
@@ -85,6 +95,28 @@ private class DependentModuleProviders(session: FirSession, private val provider
                 is FirModuleWithDependenciesSymbolProvider ->
                     provider.getTopLevelCallableSymbolsToWithoutDependencies(destination, packageFqName, name)
                 else -> provider.getTopLevelCallableSymbolsTo(destination, packageFqName, name)
+            }
+        }
+    }
+
+    @FirSymbolProviderInternals
+    override fun getTopLevelFunctionSymbolsTo(destination: MutableList<FirNamedFunctionSymbol>, packageFqName: FqName, name: Name) {
+        providers.forEach { provider ->
+            when (provider) {
+                is FirModuleWithDependenciesSymbolProvider ->
+                    provider.getTopLevelFunctionSymbolsToWithoutDependencies(destination, packageFqName, name)
+                else -> provider.getTopLevelFunctionSymbolsTo(destination, packageFqName, name)
+            }
+        }
+    }
+
+    @FirSymbolProviderInternals
+    override fun getTopLevelPropertySymbolsTo(destination: MutableList<FirPropertySymbol>, packageFqName: FqName, name: Name) {
+        providers.forEach { provider ->
+            when (provider) {
+                is FirModuleWithDependenciesSymbolProvider ->
+                    provider.getTopLevelPropertySymbolsToWithoutDependencies(destination, packageFqName, name)
+                else -> provider.getTopLevelPropertySymbolsTo(destination, packageFqName, name)
             }
         }
     }
