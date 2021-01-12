197102543bcb33c18ba24f83e25e5ba0c4352c2e
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2021-01-12 12:28:21 +0300

    Update Kotlin/Native: 1.4.30-rc1-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -188,7 +188,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.4.30-rc1-56"
+    extra["versions.kotlin-native"] = "1.4.30-rc1-8"
 }
 
 val intellijUltimateEnabled by extra(project.kotlinBuildProperties.intellijUltimateEnabled)
