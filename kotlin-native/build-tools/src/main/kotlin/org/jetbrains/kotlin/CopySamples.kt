fe2799273d91f405632b56fe1fb0177704a79650
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2021-01-13 13:53:41 +0100

    [build] drop dead repositories

--- kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/CopySamples.kt
+++ kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/CopySamples.kt
@@ -58,7 +58,6 @@ open class CopySamples : Copy() {
     }
 
     private val replacements = listOf(
-        "https://dl.bintray.com/kotlin/kotlin-dev" to "https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-dev",
         "https://dl.bintray.com/kotlin/kotlin-eap" to "https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/kotlin-eap",
         "https://dl.bintray.com/kotlin/ktor" to "https://cache-redirector.jetbrains.com/dl.bintray.com/kotlin/ktor",
         "https://plugins.gradle.org/m2" to "https://cache-redirector.jetbrains.com/plugins.gradle.org/m2",
