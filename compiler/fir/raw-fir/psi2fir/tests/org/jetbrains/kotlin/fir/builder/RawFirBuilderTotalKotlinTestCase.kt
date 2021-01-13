5a3f825e6813fb6a1fde3c91f7d4e1cda2c906d2
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2021-01-13 12:27:29 +0100

    [fir][test] ignore native tree

--- compiler/fir/raw-fir/psi2fir/tests/org/jetbrains/kotlin/fir/builder/RawFirBuilderTotalKotlinTestCase.kt
+++ compiler/fir/raw-fir/psi2fir/tests/org/jetbrains/kotlin/fir/builder/RawFirBuilderTotalKotlinTestCase.kt
@@ -53,7 +53,11 @@ class RawFirBuilderTotalKotlinTestCase : AbstractRawFirBuilderTestCase() {
         for (file in root.walkTopDown()) {
             if (file.isDirectory) continue
             val path = file.path
-            if ("testData" in path || "resources" in path || "api/js" in path.replace('\\', '/')) continue
+            if ("testData" in path ||
+                "kotlin-native" in path ||
+                "resources" in path ||
+                "api/js" in path.replace('\\', '/')
+            ) continue
             if (file.extension != "kt") continue
             try {
                 val ktFile = createKtFile(file.toRelativeString(root))
@@ -177,7 +181,11 @@ class RawFirBuilderTotalKotlinTestCase : AbstractRawFirBuilderTestCase() {
         for (file in root.walkTopDown()) {
             if (file.isDirectory) continue
             val path = file.path
-            if ("testData" in path || "resources" in path || "api/js" in path.replace('\\', '/')) continue
+            if ("kotlin-native" in path ||
+                "testData" in path ||
+                "resources" in path ||
+                "api/js" in path.replace('\\', '/')
+            ) continue
             if (file.extension != "kt") continue
             val ktFile = createKtFile(file.toRelativeString(root))
             val firFile = ktFile.toFirFile()
@@ -204,7 +212,11 @@ class RawFirBuilderTotalKotlinTestCase : AbstractRawFirBuilderTestCase() {
         for (file in root.walkTopDown()) {
             if (file.isDirectory) continue
             val path = file.path
-            if ("testData" in path || "testdata" in path || "resources" in path || "api/js" in path.replace('\\', '/')) continue
+            if ("kotlin-native" in path ||
+                "testData" in path ||
+                "testdata" in path ||
+                "resources" in path ||
+                "api/js" in path.replace('\\', '/')) continue
             if (file.extension != "kt") continue
             val ktFile = createKtFile(file.toRelativeString(root))
             val firFile: FirFile = ktFile.toFirFile()
