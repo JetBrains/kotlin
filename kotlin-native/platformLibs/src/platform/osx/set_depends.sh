3796a6a94c75f8fde3f88e55f83f2ab62e3bf214
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2021-01-20 15:51:55 +0700

    Platform libs for macos_arm64
    (cherry picked from commit abaa99d6cc993ec30b4b8342078b69cf9e4ee6ef)

--- kotlin-native/platformLibs/src/platform/osx/set_depends.sh
+++ kotlin-native/platformLibs/src/platform/osx/set_depends.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../../../dist/bin/run_konan defFileDependencies -target macos_x64 *.def
+../../../../dist/bin/run_konan defFileDependencies -target macos_x64 -target macos_arm64 *.def
