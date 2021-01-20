71e74497b55173a4c0c033cee95a6bf116c7acf0
Alexander Dudinsky <Alexander.Dudinsky@jetbrains.com>
2021-01-20 09:46:03 +0300

    Use `addTestJdk`&`removeTestJdk` methods instead `addJdk`&`removeJdk`

--- idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleFacetImportTest.kt
+++ idea/idea-gradle/tests/org/jetbrains/kotlin/idea/codeInsight/gradle/GradleFacetImportTest.kt
@@ -250,7 +250,7 @@ class GradleFacetImportTest : GradleImportingTestCase() {
     @Test
     fun testJsImportTransitive() {
         configureByFiles()
-        importProject()
+        importProject(false)
 
         with(facetSettings) {
             Assert.assertEquals("1.3", languageLevel!!.versionString)
@@ -867,10 +867,14 @@ class GradleFacetImportTest : GradleImportingTestCase() {
     }
 
     override fun importProject() {
+        importProject(true)
+    }
+
+    fun importProject(skipIndexing: Boolean = true) {
         val isCreateEmptyContentRootDirectories = currentExternalProjectSettings.isCreateEmptyContentRootDirectories
         try {
             currentExternalProjectSettings.isCreateEmptyContentRootDirectories = true
-            super.importProject(true)
+            super.importProject(skipIndexing)
         } finally {
             currentExternalProjectSettings.isCreateEmptyContentRootDirectories = isCreateEmptyContentRootDirectories
         }
