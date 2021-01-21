e17153fab93ad961d0ec0be8e86675aa52e3d877
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-01-21 18:25:49 +0100

    Minor, improve Ant test on includeRuntime
    Also check that we _do_ include runtime if includeRuntime is true.

--- compiler/testData/integration/ant/jvm/doNotIncludeRuntimeByDefault/test.kt
+++ compiler/testData/integration/ant/jvm/doNotIncludeRuntimeByDefault/test.kt
@@ -4,11 +4,12 @@ import java.util.jar.JarFile
 fun main(args: Array<String>) {
     val jar = File(args[0])
     // if "includeRuntime" is specified to be true
-    var specifiedBeTrue = args[1].toBoolean()
-    for (entry in JarFile(jar).entries()) {
-        if (!specifiedBeTrue && entry.name.startsWith("kotlin/")) {
-            println("Error: Kotlin runtime is expected to be excluded if the attribute \"includeRuntime\" is not specified to be true")
-            break;
-        }
+    var shouldIncludeRuntime = args[1].toBoolean()
+    val hasKotlinPackage = JarFile(jar).entries().toList().any { it.name.startsWith("kotlin/") }
+    if (shouldIncludeRuntime != hasKotlinPackage) {
+        println(
+            "Error: Kotlin runtime is expected to be included only if the attribute \"includeRuntime\" is specified to be true\n" +
+                    "includeRuntime = ${shouldIncludeRuntime}; hasKotlinPackage = $hasKotlinPackage"
+        )
     }
 }
