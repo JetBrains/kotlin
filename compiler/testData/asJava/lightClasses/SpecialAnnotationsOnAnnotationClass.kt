64e97225b8aefb2156b08a8af22e047a2d7379d8
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-01-21 15:29:44 +0100

    Light classes: map annotation targets depending on JVM target
    Since the default JVM target (`JvmTarget.DEFAULT`) is now 1.8, the
    changed test started to fail. This change fixes it.

--- compiler/testData/asJava/lightClasses/SpecialAnnotationsOnAnnotationClass.kt
+++ compiler/testData/asJava/lightClasses/SpecialAnnotationsOnAnnotationClass.kt
@@ -1,7 +1,7 @@
 // Anno
 
 @Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.TYPE_PARAMETER)
+@Target(AnnotationTarget.TYPE_PARAMETER, AnnotationTarget.TYPE)
 @MustBeDocumented
 @Repeatable
 annotation class Anno(val i: Int)
