a33877a9b9784402d5a6f8eedbbf63d8adf9ae38
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-01-21 16:24:26 +0300

    Fix add import quick fix for unresolved conventional invoke operator
     After adding extension invoke on DeepRecursiveFunction in 1.4,
     the compiler start reporting another error and now it doesn't write
     info about callable descriptor. Therefore it's needed to use another
     source of info for expression type
    
     ^KT-40926 Fixed

---
+++ idea/testData/quickfix/autoImports/ImportOperatorInvokeWithConvention.after.kt
@@ -0,0 +1,12 @@
+// "Import" "true"
+// WITH_RUNTIME
+// ERROR: Expression 'topVal' of type 'SomeType' cannot be invoked as a function. The function 'invoke()' is not found
+
+package mig
+
+import another.invoke
+import another.topVal
+
+fun use() {
+    topVal<selection></selection>()
+}
