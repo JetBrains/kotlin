646a88a1f9d10923d20d33a565bb21c2e94fc1c9
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-01-21 16:23:03 +0300

    Report more specific diagnostic for variable + invoke calls
     Do not report same set of diagnostics for variable call if actual
     error was happened with a function candidate. Here the candidate is
     invoke function on DeepRecursiveFunction
    
     ^KT-40991 Fixed
     ^KT-41491 Fixed
     ^KT-40926 In Progress

--- compiler/testData/diagnostics/tests/resolve/invoke/reportFunctionExpectedWhenOneInvokeExist.kt
+++ compiler/testData/diagnostics/tests/resolve/invoke/reportFunctionExpectedWhenOneInvokeExist.kt
@@ -6,7 +6,7 @@ fun Int.invoke() {}
 class SomeClass
 
 fun test(identifier: SomeClass, fn: String.() -> Unit) {
-    <!NI;UNRESOLVED_REFERENCE_WRONG_RECEIVER, OI;DEBUG_INFO_MISSING_UNRESOLVED, OI;FUNCTION_EXPECTED!>identifier<!>()
+    <!DEBUG_INFO_MISSING_UNRESOLVED, FUNCTION_EXPECTED!>identifier<!>()
     <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>identifier<!>(123)
     <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>identifier<!>(1, 2)
     1.<!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>fn<!>()
