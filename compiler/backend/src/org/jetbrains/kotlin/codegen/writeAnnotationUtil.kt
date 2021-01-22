c6f5ce683703ae7a73e5c74b4a4c02f9e501003b
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-01-22 13:43:48 +0100

    Do not write bytecode version to class file
     #KT-41758

--- compiler/backend/src/org/jetbrains/kotlin/codegen/writeAnnotationUtil.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/writeAnnotationUtil.kt
@@ -20,7 +20,6 @@ import org.jetbrains.kotlin.codegen.state.GenerationState
 import org.jetbrains.kotlin.config.JvmAnalysisFlags
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames
 import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
-import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmBytecodeBinaryVersion
 import org.jetbrains.org.objectweb.asm.AnnotationVisitor
 
 fun writeKotlinMetadata(
@@ -32,7 +31,6 @@ fun writeKotlinMetadata(
 ) {
     val av = cb.newAnnotation(JvmAnnotationNames.METADATA_DESC, true)
     av.visit(JvmAnnotationNames.METADATA_VERSION_FIELD_NAME, state.metadataVersion.toArray())
-    av.visit(JvmAnnotationNames.BYTECODE_VERSION_FIELD_NAME, JvmBytecodeBinaryVersion.INSTANCE.toArray())
     av.visit(JvmAnnotationNames.KIND_FIELD_NAME, kind.id)
     var flags = extraFlags
     if (state.languageVersionSettings.isPreRelease()) {
