c4237509621ab29921ab67dc9636a977bf1afc1d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-01-22 10:30:25 +0300

    [TD] Fix incorrect module structure directives in test data file

--- compiler/testData/codegen/box/properties/lazyInitializationMultiModule.kt
+++ compiler/testData/codegen/box/properties/lazyInitializationMultiModule.kt
@@ -2,9 +2,11 @@
 // PROPERTY_LAZY_INITIALIZATION
 
 // MODULE: lib1
+// FILE: lib.kt
 var log = ""
 val a = 1.also { log += "a" }
 val b = 2.also { log += "b" }
 
 // MODULE: main(lib1)
+// FILE: main.kt
 fun box(): String = if (log + a == "ab1") "OK" else "fail"
