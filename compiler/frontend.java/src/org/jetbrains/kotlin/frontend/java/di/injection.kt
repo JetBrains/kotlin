adfa8c788ca97e4ba173fc51a10853171ff0f62b
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-01-25 18:59:38 +0100

    Light classes: use JVM target from the module
    Using "JVM_1_8" always resulted in incorrect mapping of Kotlin
    annotation targets to Java element types.
    
    The change in AbstractKotlinRenderLogTest is needed because while
    CliTraceHolder.module is technically a descriptor leak, it was never
    detected by this test accidentally, because of the depth cutoff equal to
    10, which started to not be enough after the minor refactoring of
    replacing `Delegates.notNull` with `lateinit`.

--- compiler/frontend.java/src/org/jetbrains/kotlin/frontend/java/di/injection.kt
+++ compiler/frontend.java/src/org/jetbrains/kotlin/frontend/java/di/injection.kt
@@ -47,7 +47,6 @@ import org.jetbrains.kotlin.resolve.jvm.JavaDescriptorResolver
 import org.jetbrains.kotlin.resolve.jvm.JvmDiagnosticComponents
 import org.jetbrains.kotlin.resolve.jvm.multiplatform.OptionalAnnotationPackageFragmentProvider
 import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatformAnalyzerServices
-import org.jetbrains.kotlin.resolve.lazy.KotlinCodeAnalyzer
 import org.jetbrains.kotlin.resolve.lazy.declarations.DeclarationProviderFactory
 
 fun createContainerForLazyResolveWithJava(
@@ -95,7 +94,9 @@ fun createContainerForLazyResolveWithJava(
 }
 
 fun StorageComponentContainer.initializeJavaSpecificComponents(bindingTrace: BindingTrace) {
-    get<AbstractJavaClassFinder>().initialize(bindingTrace, get<KotlinCodeAnalyzer>(), get<LanguageVersionSettings>())
+    get<AbstractJavaClassFinder>().initialize(
+        bindingTrace, codeAnalyzer = get(), languageVersionSettings = get(), jvmTarget = get()
+    )
 }
 
 fun StorageComponentContainer.configureJavaSpecificComponents(
---
+++ compiler/testData/asJava/ultraLightClasses/annotationTargets_1_6.java
@@ -0,0 +1,16 @@
+@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
+@kotlin.annotation.Target()
+public abstract @interface A0 /* A0*/ {
+}
+
+@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Target(value = {java.lang.annotation.ElementType.PARAMETER})
+@kotlin.annotation.Target(allowedTargets = {kotlin.annotation.AnnotationTarget.VALUE_PARAMETER, kotlin.annotation.AnnotationTarget.TYPE})
+public abstract @interface A1 /* A1*/ {
+}
+
+@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Target(value = {java.lang.annotation.ElementType.PARAMETER})
+@kotlin.annotation.Target(allowedTargets = {kotlin.annotation.AnnotationTarget.VALUE_PARAMETER, kotlin.annotation.AnnotationTarget.TYPE, kotlin.annotation.AnnotationTarget.TYPE_PARAMETER})
+public abstract @interface A2 /* A2*/ {
+}
