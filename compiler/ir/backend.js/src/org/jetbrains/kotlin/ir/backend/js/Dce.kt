5fa9acc17ac27b8844b150eab3d3f818059986bc
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-01-25 17:17:52 +0300

    [JS IR] Add additional exported declarations inside dce loop
    [JS IR] Remove useless filter in dce for more log information
    
    ^KT-44469 fixed

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/Dce.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/Dce.kt
@@ -407,13 +407,16 @@ fun usefulDeclarations(roots: Iterable<IrDeclaration>, context: JsIrBackendConte
                 }
             }
         }
+
+        context.additionalExportedDeclarations
+            .forEach { it.enqueue(null, "from additionalExportedDeclarations", altFromFqn = "<ROOT>") }
     }
 
     if (printReachabilityInfo) {
         reachabilityInfo.forEach(::println)
     }
 
-    return result + context.additionalExportedDeclarations
+    return result
 }
 
 private fun Collection<IrClass>.filterDescendantsOf(bases: Collection<IrClass>): Collection<IrClass> {
