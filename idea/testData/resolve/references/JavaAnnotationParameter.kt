659d2c13b4aee40949d53975b934ec919a86dbf0
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-01-26 02:37:47 +0300

    [FIR IDE] Fix annotation value parameter and ctor resolve

--- idea/testData/resolve/references/JavaAnnotationParameter.kt
+++ idea/testData/resolve/references/JavaAnnotationParameter.kt
@@ -1,6 +1,5 @@
-// IGNORE_FIR
-
-@java.lang.annotation.Retention(va<caret>lue = java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Re<caret>tention(va<caret>lue = java.lang.annotation.RetentionPolicy.RUNTIME)
 annotation class Anno()
 
-// REF: (in java.lang.annotation.Retention).value()
+// REF1: (java.lang.annotation).Retention
+// REF2: (in java.lang.annotation.Retention).value()
