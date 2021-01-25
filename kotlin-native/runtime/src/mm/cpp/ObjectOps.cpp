e99ba1f97eb0fd35064ce4a19e78e2e62dda934b
Ilya Matveev <ilya.matveev@jetbrains.com>
2021-01-25 12:46:09 +0700

    [K/N][New MM] Add thread state asserts to reference updates

--- kotlin-native/runtime/src/mm/cpp/ObjectOps.cpp
+++ kotlin-native/runtime/src/mm/cpp/ObjectOps.cpp
@@ -13,10 +13,12 @@ using namespace kotlin;
 // TODO: Memory barriers.
 
 ALWAYS_INLINE void mm::SetStackRef(ObjHeader** location, ObjHeader* value) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     *location = value;
 }
 
 ALWAYS_INLINE void mm::SetHeapRef(ObjHeader** location, ObjHeader* value) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     *location = value;
 }
 
@@ -27,16 +29,19 @@ ALWAYS_INLINE void mm::SetHeapRef(ObjHeader** location, ObjHeader* value) noexce
 #pragma clang diagnostic ignored "-Watomic-alignment"
 
 ALWAYS_INLINE void mm::SetHeapRefAtomic(ObjHeader** location, ObjHeader* value) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     __atomic_store_n(location, value, __ATOMIC_RELEASE);
 }
 
 ALWAYS_INLINE OBJ_GETTER(mm::ReadHeapRefAtomic, ObjHeader** location) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     // TODO: Make this work with GCs that can stop thread at any point.
     auto result = __atomic_load_n(location, __ATOMIC_ACQUIRE);
     RETURN_OBJ(result);
 }
 
 ALWAYS_INLINE OBJ_GETTER(mm::CompareAndSwapHeapRef, ObjHeader** location, ObjHeader* expected, ObjHeader* value) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     // TODO: Make this work with GCs that can stop thread at any point.
     ObjHeader* actual = expected;
     // TODO: Do we need this strong memory model? Do we need to use strong CAS?
@@ -50,12 +55,14 @@ ALWAYS_INLINE OBJ_GETTER(mm::CompareAndSwapHeapRef, ObjHeader** location, ObjHea
 #pragma clang diagnostic pop
 
 OBJ_GETTER(mm::AllocateObject, ThreadData* threadData, const TypeInfo* typeInfo) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     // TODO: Make this work with GCs that can stop thread at any point.
     auto* object = threadData->objectFactoryThreadQueue().CreateObject(typeInfo);
     RETURN_OBJ(object);
 }
 
 OBJ_GETTER(mm::AllocateArray, ThreadData* threadData, const TypeInfo* typeInfo, uint32_t elements) noexcept {
+    AssertThreadState(ThreadState::kRunnable);
     // TODO: Make this work with GCs that can stop thread at any point.
     auto* array = threadData->objectFactoryThreadQueue().CreateArray(typeInfo, static_cast<uint32_t>(elements));
     // `ArrayHeader` and `ObjHeader` are expected to be compatible.
