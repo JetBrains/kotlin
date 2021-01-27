c1cdf273a60e77abbcca30595e631d9cf9ea6374
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-01-27 20:11:45 +0300

    [FIR IDE] Fix test data fir parameters highlighting test

--- idea/testData/highlighter/NamedArguments.kt
+++ idea/testData/highlighter/NamedArguments.kt
@@ -1,8 +1,8 @@
-<info textAttributesKey="KOTLIN_ANNOTATION">@Suppress</info>(<info textAttributesKey="KOTLIN_ANNOTATION_ATTRIBUTE_NAME_ATTRIBUTES">names =</info> ["foo"])
-fun <info textAttributesKey="KOTLIN_FUNCTION_DECLARATION">foo</info>(<info textAttributesKey="KOTLIN_PARAMETER">p1</info>: <info textAttributesKey="KOTLIN_CLASS">Int</info>, <info textAttributesKey="KOTLIN_PARAMETER">p2</info>: <info textAttributesKey="KOTLIN_CLASS">String</info>): <info textAttributesKey="KOTLIN_CLASS">String</info> {
-    return <info textAttributesKey="KOTLIN_PARAMETER">p2</info> + <info textAttributesKey="KOTLIN_PARAMETER">p1</info>
+@Suppress(<info descr="null" textAttributesKey="KOTLIN_ANNOTATION_ATTRIBUTE_NAME_ATTRIBUTES">names =</info> ["foo"])
+fun <info descr="null" textAttributesKey="KOTLIN_FUNCTION_DECLARATION">foo</info>(<info descr="null" textAttributesKey="KOTLIN_PARAMETER">p1</info>: <info descr="null" textAttributesKey="KOTLIN_CLASS">Int</info>, <info descr="null" textAttributesKey="KOTLIN_PARAMETER">p2</info>: <info descr="null" textAttributesKey="KOTLIN_CLASS">String</info>): <info descr="null" textAttributesKey="KOTLIN_CLASS">String</info> {
+    return <info descr="null" textAttributesKey="KOTLIN_PARAMETER">p2</info> + <info descr="null" textAttributesKey="KOTLIN_PARAMETER">p1</info>
 }
 
-fun <info textAttributesKey="KOTLIN_FUNCTION_DECLARATION">bar</info>() {
-    <info textAttributesKey="KOTLIN_PACKAGE_FUNCTION_CALL">foo</info>(1, <info textAttributesKey="KOTLIN_NAMED_ARGUMENT">p2 =</info> "")
+fun <info descr="null" textAttributesKey="KOTLIN_FUNCTION_DECLARATION">bar</info>() {
+    <info descr="null" textAttributesKey="KOTLIN_PACKAGE_FUNCTION_CALL">foo</info>(1, <info descr="null" textAttributesKey="KOTLIN_NAMED_ARGUMENT">p2 =</info> "")
 }
