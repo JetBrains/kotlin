c13f38f6dfed54fd3e0f0dc2f18c529718155144
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2021-01-27 17:19:01 +0100

    Support proper frame maps for enumValues

---
+++ compiler/testData/codegen/boxInline/reified/kt35511_try_values.kt
@@ -0,0 +1,46 @@
+// JVM_TARGET: 1.8
+// FILE: 1.kt
+// WITH_RUNTIME
+
+package test
+
+enum class Base(val value: String) {
+    OK("OK"),
+    B("FAIL");
+}
+
+enum class Base2(val value: String) {
+    A("OK2"),
+    B("FAIL2");
+}
+
+var result = "fail"
+
+fun foo(base: Enum<*>) {
+    result = base.name
+}
+
+fun foo(base: Array<out Enum<*>>) {
+    result = base[0].name
+}
+
+fun cond() = true
+
+inline fun <reified T : Enum<T>, reified Y : Enum<Y>> process(a: String) {
+    val z = try {
+        enumValues<T>()
+    } catch (e: Exception) {
+        enumValues<Y>()
+    }
+
+    foo(z)
+}
+
+// FILE: 2.kt
+import test.*
+
+fun box(): String {
+    process<Base, Base2>("OK")
+
+    return result
+}
