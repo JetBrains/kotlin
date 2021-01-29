2d952330b54fff0ccc3426d7e7607643490c8caf
Leland Richardson <lelandr@google.com>
2021-01-28 17:37:39 -0800

    Propagate and enforce DisallowComposableCall
    Change-Id: I65374fe2aa61b1d7ee9d4565f50372a994d7a11d ( https://android-review.googlesource.com/q/I65374fe2aa61b1d7ee9d4565f50372a994d7a11d )
    
    Moved from: https://github.com/androidx/androidx/commit/43d0389112fda12907dca02469186982c62f006e

--- plugins/compose/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/analysis/ComposableCheckerTests.kt
+++ plugins/compose/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/analysis/ComposableCheckerTests.kt
@@ -1075,4 +1075,20 @@ class ComposableCheckerTests : AbstractComposeDiagnosticsTest() {
         """
         )
     }
+
+    fun testDisallowComposableCallPropagation() = check(
+        """
+        import androidx.compose.runtime.*
+        class Foo
+        @Composable inline fun a(block1: @DisallowComposableCalls () -> Foo): Foo {
+            return block1()
+        }
+        @Composable inline fun b(<!MISSING_DISALLOW_COMPOSABLE_CALLS_ANNOTATION!>block2: () -> Foo<!>): Foo {
+          return a { block2() }
+        }
+        @Composable inline fun c(block2: @DisallowComposableCalls () -> Foo): Foo {
+          return a { block2() }
+        }
+    """
+    )
 }
