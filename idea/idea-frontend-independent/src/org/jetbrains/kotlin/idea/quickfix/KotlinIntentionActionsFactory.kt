6dd2037f857fe22fc857da525d24c4d01873f32d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-01-29 13:25:11 +0100

    Move QuickFixActionBase & KotlinIntentionActionsFactory to frontend-independent module

--- idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/KotlinIntentionActionsFactory.kt
+++ idea/idea-frontend-independent/src/org/jetbrains/kotlin/idea/quickfix/KotlinIntentionActionsFactory.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -9,7 +9,7 @@ import com.intellij.codeInsight.intention.IntentionAction
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.psi.KtCodeFragment
 
-abstract class KotlinIntentionActionsFactory {
+abstract class KotlinIntentionActionsFactory: QuickFixFactory {
     protected open fun isApplicableForCodeFragment(): Boolean = false
 
     protected abstract fun doCreateActions(diagnostic: Diagnostic): List<IntentionAction>
