0373cc4ecceee56fcbe8c6eef4698e32c38779ef
eugenpolytechnic <zenya.01@mail.ru>
2021-01-31 17:37:11 +0300

    Fir checkers: catch parameter type is Throwable check

--- compiler/fir/analysis-tests/testData/resolve/catchParameter.kt
+++ compiler/fir/analysis-tests/testData/resolve/catchParameter.kt
@@ -1,4 +1,6 @@
-fun <T> test() {
+typealias StringType = String
+
+fun <T : Throwable> test() {
     try {
 
     } catch (<!CATCH_PARAMETER_WITH_DEFAULT_VALUE!>e: NullPointerException = NullPointerException()<!>) {
@@ -6,8 +8,16 @@ fun <T> test() {
     }
 
     try {} catch (<!TYPE_PARAMETER_IN_CATCH_CLAUSE!>e: T<!>) {}
+
+    try {} catch (<!TYPE_MISMATCH!>e: () -> Int<!>) {}
+
+    try {} catch (<!TYPE_MISMATCH!>e: StringType<!>) {}
+
+    try {} catch (<!CATCH_PARAMETER_WITH_DEFAULT_VALUE, TYPE_MISMATCH!>e: Int = 5<!>) {}
+
+    try {} catch (e: Throwable) {}
 }
 
 inline fun <reified T> anotherTest() {
-    try {} catch (<!REIFIED_TYPE_IN_CATCH_CLAUSE!>e: T<!>) {}
+    try {} catch (<!REIFIED_TYPE_IN_CATCH_CLAUSE, TYPE_MISMATCH!>e: T<!>) {}
 }
