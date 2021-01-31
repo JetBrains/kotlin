441cbc2f726349b9e400963b7d6d5504f1162850
eugenpolytechnic <zenya.01@mail.ru>
2021-01-31 19:17:10 +0300

    Fir checkers: move throwableClassLikeType and isThrowable fun to FirHelpers

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirCatchParameterChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirCatchParameterChecker.kt
@@ -5,24 +5,16 @@
 
 package org.jetbrains.kotlin.fir.analysis.checkers.expression
 
-import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.analysis.cfa.FirReturnsImpliesAnalyzer.isSupertypeOf
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.analysis.checkers.isThrowable
+import org.jetbrains.kotlin.fir.analysis.checkers.throwableClassLikeType
 import org.jetbrains.kotlin.fir.analysis.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.expressions.FirTryExpression
-import org.jetbrains.kotlin.fir.resolve.fullyExpandedType
-import org.jetbrains.kotlin.fir.symbols.StandardClassIds
-import org.jetbrains.kotlin.fir.typeCheckerContext
-import org.jetbrains.kotlin.fir.types.ConeKotlinType
 import org.jetbrains.kotlin.fir.types.ConeTypeParameterType
 import org.jetbrains.kotlin.fir.types.FirResolvedTypeRef
-import org.jetbrains.kotlin.fir.types.constructClassLikeType
 
 object FirCatchParameterChecker : FirTryExpressionChecker() {
-    private val throwable = StandardClassIds.byName("Throwable")
-        .constructClassLikeType(emptyArray(), false)
-
     override fun check(expression: FirTryExpression, context: CheckerContext, reporter: DiagnosticReporter) {
         for (catchEntry in expression.catches) {
             val catchParameter = catchEntry.parameter
@@ -45,10 +37,7 @@ object FirCatchParameterChecker : FirTryExpressionChecker() {
             }
 
             if (!coneType.isThrowable(context.session))
-                catchParameter.source?.let { reporter.report(FirErrors.TYPE_MISMATCH.on(it, throwable, coneType)) }
+                catchParameter.source?.let { reporter.report(FirErrors.TYPE_MISMATCH.on(it, throwableClassLikeType, coneType)) }
         }
     }
-
-    private fun ConeKotlinType.isThrowable(session: FirSession) =
-        throwable.isSupertypeOf(session.typeCheckerContext, this.fullyExpandedType(session))
 }
