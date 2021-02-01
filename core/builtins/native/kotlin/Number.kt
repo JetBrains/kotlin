9c3c599db6693ff5aedff1a9cce6575a3d985fd6
hfazai <hf.fazai@gmail.com>
2021-02-01 13:59:05 +0100

    Changing Number class to interface

--- core/builtins/native/kotlin/Number.kt
+++ core/builtins/native/kotlin/Number.kt
@@ -17,42 +17,41 @@
 package kotlin
 
 /**
- * Superclass for all platform classes representing numeric values.
+ * High level interface for all platform classes representing numeric values.
  */
-public abstract class Number {
+public interface Number {
     /**
      * Returns the value of this number as a [Double], which may involve rounding.
      */
-    public abstract fun toDouble(): Double
+    public fun toDouble(): Double
 
     /**
      * Returns the value of this number as a [Float], which may involve rounding.
      */
-    public abstract fun toFloat(): Float
+    public fun toFloat(): Float
 
     /**
      * Returns the value of this number as a [Long], which may involve rounding or truncation.
      */
-    public abstract fun toLong(): Long
+    public fun toLong(): Long
 
     /**
      * Returns the value of this number as an [Int], which may involve rounding or truncation.
      */
-    public abstract fun toInt(): Int
+    public fun toInt(): Int
 
     /**
      * Returns the [Char] with the numeric value equal to this number, truncated to 16 bits if appropriate.
      */
-    public abstract fun toChar(): Char
+    public fun toChar(): Char
 
     /**
      * Returns the value of this number as a [Short], which may involve rounding or truncation.
      */
-    public abstract fun toShort(): Short
+    public fun toShort(): Short
 
     /**
      * Returns the value of this number as a [Byte], which may involve rounding or truncation.
      */
-    public abstract fun toByte(): Byte
+    public fun toByte(): Byte
 }
-
