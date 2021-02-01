8f675fe757ff6d5aeff74f4aedfc3d0135795382
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2021-02-01 12:48:49 +0300

    Split combined test into two to avoid flaky behaviour
    Prevent simultaneous access to the library directory

--- compiler/tests/org/jetbrains/kotlin/jvm/compiler/CompileKotlinAgainstCustomBinariesTest.kt
+++ compiler/tests/org/jetbrains/kotlin/jvm/compiler/CompileKotlinAgainstCustomBinariesTest.kt
@@ -714,7 +714,9 @@ class CompileKotlinAgainstCustomBinariesTest : AbstractKotlinCompilerIntegration
             listOf(library),
             K2MetadataCompiler(),
         )
+    }
 
+    fun testAnonymousObjectTypeMetadataKlib() {
         val klibLibrary = compileCommonLibrary(
             libraryName = "library",
             listOf("-Xexpect-actual-linker"),
