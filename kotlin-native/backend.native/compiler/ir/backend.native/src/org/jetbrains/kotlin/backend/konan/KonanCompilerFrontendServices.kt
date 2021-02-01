438e1d34c51177215af43b3f148cb5d09e79f8b6
Florian Kistner <florian.kistner@jetbrains.com>
2021-02-01 15:08:58 +0100

    Restore previous exception behavior for `-Xemit-lazy-objc-header`

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanCompilerFrontendServices.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanCompilerFrontendServices.kt
@@ -11,15 +11,20 @@ import org.jetbrains.kotlin.backend.konan.objcexport.ObjCExportLazyImpl
 import org.jetbrains.kotlin.backend.konan.objcexport.ObjCExportProblemCollector
 import org.jetbrains.kotlin.backend.konan.objcexport.dumpObjCHeader
 import org.jetbrains.kotlin.container.*
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.deprecation.DeprecationResolver
 
 internal fun StorageComponentContainer.initContainer(config: KonanConfig) {
-    this.useImpl<FrontendServices>()
+    useImpl<FrontendServices>()
 
     if (config.configuration.get(KonanConfigKeys.EMIT_LAZY_OBJC_HEADER_FILE) != null) {
-        this.useImpl<ObjCExportLazyImpl>()
-        this.useInstance(ObjCExportProblemCollector.SILENT)
+        useImpl<ObjCExportLazyImpl>()
+        useInstance(object : ObjCExportProblemCollector {
+            override fun reportWarning(text: String) {}
+            override fun reportWarning(method: FunctionDescriptor, text: String) {}
+            override fun reportException(throwable: Throwable) = throw throwable
+        })
 
         useInstance(object : ObjCExportLazy.Configuration {
             override val frameworkName: String
