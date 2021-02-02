79c74e63a719b776bf707b4688e315050a0a502a
andrey.zinovyev@jetbrains.com <andrey.zinovyev@jetbrains.com>
2021-02-02 17:17:09 +0300

    one more test case

--- plugins/kapt3/kapt3-compiler/testData/converter/anonymousDelegate.kt
+++ plugins/kapt3/kapt3-compiler/testData/converter/anonymousDelegate.kt
@@ -25,3 +25,10 @@ class Test {
     }
 
 }
+
+var delegate by object {
+    operator fun getValue(nothing: Nothing?, property: KProperty<*>) = Any()
+    operator fun setValue(nothing: Nothing?, property: KProperty<*>, any: Any) {
+        //empty
+    }
+}
