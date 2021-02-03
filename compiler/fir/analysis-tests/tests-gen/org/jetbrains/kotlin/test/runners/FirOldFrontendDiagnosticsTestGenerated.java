150732d2552d08051191aff9bfad7f401bcf6aab
Jinseong Jeon <jsjeon@google.com>
2021-02-03 15:02:57 -0800

    FIR checker: reproduce a false alarm on type mismatch of overridden property

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
@@ -30639,6 +30639,12 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirDiagnosti
         }
 
         @Test
+        @TestMetadata("buildLazyValueForMap.kt")
+        public void testBuildLazyValueForMap() throws Exception {
+            runTest("compiler/testData/diagnostics/testsWithStdLib/buildLazyValueForMap.kt");
+        }
+
+        @Test
         @TestMetadata("CallCompanionProtectedNonStatic.kt")
         public void testCallCompanionProtectedNonStatic() throws Exception {
             runTest("compiler/testData/diagnostics/testsWithStdLib/CallCompanionProtectedNonStatic.kt");
