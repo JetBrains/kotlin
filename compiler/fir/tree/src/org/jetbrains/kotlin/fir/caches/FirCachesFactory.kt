8f0e1035fa7831bb9e76cba49086ddce9fbc2ff0
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-02-06 12:56:33 +0300

    [FIR] Rename `KEY` and `VALUE` generics of FirCache to `K` and `V`

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/caches/FirCachesFactory.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/caches/FirCachesFactory.kt
@@ -18,12 +18,12 @@ abstract class FirCachesFactory : FirSessionComponent {
      * Where:
      * [CONTEXT] -- type of value which be used to create value by [createValue]
      */
-    abstract fun <KEY : Any, VALUE, CONTEXT> createCache(createValue: (KEY, CONTEXT) -> VALUE): FirCache<KEY, VALUE, CONTEXT>
+    abstract fun <K : Any, V, CONTEXT> createCache(createValue: (K, CONTEXT) -> V): FirCache<K, V, CONTEXT>
 
     /**
      * Creates a cache with returns a caches value on demand if it is computed
      * Otherwise computes the value in two phases:
-     *  - [createValue] -- creates values and stores [VALUE] to cache and passes [VALUE] & [DATA] to [postCompute]
+     *  - [createValue] -- creates values and stores value of type [V] to cache and passes [V] & [DATA] to [postCompute]
      *  - [postCompute] -- performs some operations on computed value after it placed into map
      *
      * [FirCache.getValue] can be safely called in postCompute from the same thread and correct value computed by [createValue] will be returned
@@ -33,40 +33,40 @@ abstract class FirCachesFactory : FirSessionComponent {
      *  [CONTEXT] -- type of value which be used to create value by [createValue]
      *  [DATA] -- type of additional data which will be passed from [createValue] to [postCompute]
      */
-    abstract fun <KEY : Any, VALUE, CONTEXT, DATA> createCacheWithPostCompute(
-        createValue: (KEY, CONTEXT) -> Pair<VALUE, DATA>,
-        postCompute: (KEY, VALUE, DATA) -> Unit
-    ): FirCache<KEY, VALUE, CONTEXT>
+    abstract fun <K : Any, V, CONTEXT, DATA> createCacheWithPostCompute(
+        createValue: (K, CONTEXT) -> Pair<V, DATA>,
+        postCompute: (K, V, DATA) -> Unit
+    ): FirCache<K, V, CONTEXT>
 }
 
 val FirSession.firCachesFactory: FirCachesFactory by FirSession.sessionComponentAccessor()
 
-inline fun <KEY : Any, VALUE> FirCachesFactory.createCache(
-    crossinline createValue: (KEY) -> VALUE,
-): FirCache<KEY, VALUE, Nothing?> = createCache(
+inline fun <K : Any, V> FirCachesFactory.createCache(
+    crossinline createValue: (K) -> V,
+): FirCache<K, V, Nothing?> = createCache(
     createValue = { key, _ -> createValue(key) },
 )
 
-inline fun <KEY : Any, VALUE, CONTEXT> FirCachesFactory.createCacheWithPostCompute(
-    crossinline createValue: (KEY, CONTEXT) -> VALUE,
-    crossinline postCompute: (KEY, VALUE) -> Unit
-): FirCache<KEY, VALUE, CONTEXT> = createCacheWithPostCompute(
+inline fun <K : Any, V, CONTEXT> FirCachesFactory.createCacheWithPostCompute(
+    crossinline createValue: (K, CONTEXT) -> V,
+    crossinline postCompute: (K, V) -> Unit
+): FirCache<K, V, CONTEXT> = createCacheWithPostCompute(
     createValue = { key, context -> createValue(key, context) to null },
     postCompute = { key, value, _ -> postCompute(key, value) }
 )
 
-inline fun <KEY : Any, VALUE> FirCachesFactory.createCacheWithPostCompute(
-    crossinline createValue: (KEY) -> VALUE,
-    crossinline postCompute: (KEY, VALUE) -> Unit
-): FirCache<KEY, VALUE, Nothing?> = createCacheWithPostCompute(
+inline fun <K : Any, V> FirCachesFactory.createCacheWithPostCompute(
+    crossinline createValue: (K) -> V,
+    crossinline postCompute: (K, V) -> Unit
+): FirCache<K, V, Nothing?> = createCacheWithPostCompute(
     createValue = { key, _ -> createValue(key) to null },
     postCompute = { key, value, _ -> postCompute(key, value) }
 )
 
-inline fun <KEY : Any, VALUE, DATA> FirCachesFactory.createCacheWithPostCompute(
-    crossinline createValue: (KEY) -> Pair<VALUE, DATA>,
-    crossinline postCompute: (KEY, VALUE, DATA) -> Unit
-): FirCache<KEY, VALUE, Nothing?> = createCacheWithPostCompute(
+inline fun <K : Any, V, DATA> FirCachesFactory.createCacheWithPostCompute(
+    crossinline createValue: (K) -> Pair<V, DATA>,
+    crossinline postCompute: (K, V, DATA) -> Unit
+): FirCache<K, V, Nothing?> = createCacheWithPostCompute(
     createValue = { key, _ -> createValue(key) },
     postCompute = { key, value, data -> postCompute(key, value, data) }
 )
