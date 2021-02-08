25432bd33acf8c102352da075ff4b257d776f99b
Roman Artemev <roman.artemev@jetbrains.com>
2021-02-08 14:43:33 +0300

    [Plugin API] Add API to load top level declaration by its signature
     - request from JP Compose, #KT-44100
    
    (cherry picked from commit d97a2b13c0623595cd07b6b6b948b8b4a572364b)

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/extensions/IrPluginContext.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/extensions/IrPluginContext.kt
@@ -10,7 +10,9 @@ import org.jetbrains.kotlin.config.LanguageVersionSettings
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.builders.IrGeneratorContext
+import org.jetbrains.kotlin.ir.linkage.IrDeserializer
 import org.jetbrains.kotlin.ir.symbols.*
+import org.jetbrains.kotlin.ir.util.IdSignature
 import org.jetbrains.kotlin.ir.util.ReferenceSymbolTable
 import org.jetbrains.kotlin.ir.util.TypeTranslator
 import org.jetbrains.kotlin.name.FqName
@@ -42,4 +44,7 @@ interface IrPluginContext : IrGeneratorContext {
     fun referenceConstructors(classFqn: FqName): Collection<IrConstructorSymbol>
     fun referenceFunctions(fqName: FqName): Collection<IrSimpleFunctionSymbol>
     fun referenceProperties(fqName: FqName): Collection<IrPropertySymbol>
+
+    // temporary solution to load synthetic top-level declaration
+    fun referenceTopLevel(signature: IdSignature, kind: IrDeserializer.TopLevelSymbolKind, moduleDescriptor: ModuleDescriptor): IrSymbol?
 }
