d97a2b13c0623595cd07b6b6b948b8b4a572364b
Roman Artemev <roman.artemev@jetbrains.com>
2021-02-08 14:43:33 +0300

    [Plugin API] Add API to load top level declaration by its signature
     - request from JP Compose, #KT-44100

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/linkage/IrDeserializer.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/linkage/IrDeserializer.kt
@@ -9,6 +9,8 @@ import org.jetbrains.kotlin.ir.builders.TranslationPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
+import org.jetbrains.kotlin.ir.util.IdSignature
+import org.jetbrains.kotlin.name.Name
 
 interface IrDeserializer : IrProvider {
 
@@ -16,6 +18,14 @@ interface IrDeserializer : IrProvider {
         fun resolveSymbol(symbol: IrSymbol, context: TranslationPluginContext): IrDeclaration? = null
     }
 
+    enum class TopLevelSymbolKind {
+        FUNCTION_SYMBOL,
+        CLASS_SYMBOL,
+        PROPERTY_SYMBOL,
+        TYPEALIAS_SYMBOL;
+    }
+
     fun init(moduleFragment: IrModuleFragment?, extensions: Collection<IrLinkerExtension>) {}
+    fun resolveBySignatureInModule(signature: IdSignature, kind: TopLevelSymbolKind, moduleName: Name): IrSymbol
     fun postProcess() {}
 }
