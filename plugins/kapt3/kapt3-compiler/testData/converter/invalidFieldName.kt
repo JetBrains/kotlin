1bdbcffda6fa55a0c060598a4de8eda77445aa69
Andrey Zinovyev <andrey.zinovyev@jetbrains.com>
2021-02-09 14:09:22 +0300

    Remove error we ignore from expected

--- plugins/kapt3/kapt3-compiler/testData/converter/invalidFieldName.kt
+++ plugins/kapt3/kapt3-compiler/testData/converter/invalidFieldName.kt
@@ -5,5 +5,4 @@ enum class Color {
 @Anno(Color.`WHI-TE`)
 annotation class Anno(val color: Color)
 
-// EXPECTED_ERROR(kotlin:5:1) cannot find symbol
 // EXPECTED_ERROR(other:-1:-1) 'WHI-TE' is an invalid Java enum value name
