d64f7dd8c7d49a3d20a35ecafb85c911509de2e5
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2021-02-10 08:33:15 +0100

    Revert "Add :kotlin-scripting-compiler.test to modules with disabled -Werror flag"
    This reverts commit 21f022de

--- build.gradle.kts
+++ build.gradle.kts
@@ -332,7 +332,6 @@ extra["tasksWithWarnings"] = listOf(
     ":compiler:fir:checkers:compileKotlin",
     ":compiler:fir:java:compileKotlin",
     ":kotlin-scripting-compiler:compileKotlin",
-    ":kotlin-scripting-compiler:compileTestKotlin",
     ":plugins:uast-kotlin:compileKotlin",
     ":plugins:uast-kotlin:compileTestKotlin",
     ":plugins:uast-kotlin-idea:compileKotlin"
