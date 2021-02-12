9e198926ad44c03645c0d514f8218485f3db09ac
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-02-12 04:22:43 +0300

    Suggest duration static factories instead of number extension properties
    Introduce Duration companion functions to convert numbers to Duration.
    Deprecate number extension properties and propose to use these
    new functions instead.
    
    (cherry picked from commit a7fda66fa1b8a5a89f0b609f833b78a1db389a44)

--- libraries/stdlib/test/time/TimeMarkTest.kt
+++ libraries/stdlib/test/time/TimeMarkTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -19,23 +19,27 @@ class TimeMarkTest {
             assertEquals(!hasPassed, this.hasNotPassedNow(), "Expected mark in the future")
             assertEquals(hasPassed, this.hasPassedNow(), "Expected mark in the past")
 
-            assertEquals(!hasPassed, this.elapsedNow() < Duration.ZERO, "Mark elapsed: ${this.elapsedNow()}, expected hasPassed: $hasPassed")
+            assertEquals(
+                !hasPassed,
+                this.elapsedNow() < Duration.ZERO,
+                "Mark elapsed: ${this.elapsedNow()}, expected hasPassed: $hasPassed"
+            )
         }
 
         val mark = timeSource.markNow()
-        val markFuture1 = (mark + 1.milliseconds).apply { assertHasPassed(false) }
-        val markFuture2 = (mark - (-1).milliseconds).apply { assertHasPassed(false) }
+        val markFuture1 = (mark + Duration.milliseconds(1)).apply { assertHasPassed(false) }
+        val markFuture2 = (mark - Duration.milliseconds((-1))).apply { assertHasPassed(false) }
 
-        val markPast1 = (mark - 1.milliseconds).apply { assertHasPassed(true) }
-        val markPast2 = (markFuture1 + (-2).milliseconds).apply { assertHasPassed(true) }
+        val markPast1 = (mark - Duration.milliseconds(1)).apply { assertHasPassed(true) }
+        val markPast2 = (markFuture1 + Duration.milliseconds((-2))).apply { assertHasPassed(true) }
 
-        timeSource += 500_000.nanoseconds
+        timeSource += Duration.nanoseconds(500_000)
 
         val elapsed = mark.elapsedNow()
-        val elapsedFromFuture = elapsed - 1.milliseconds
-        val elapsedFromPast = elapsed + 1.milliseconds
+        val elapsedFromFuture = elapsed - Duration.milliseconds(1)
+        val elapsedFromPast = elapsed + Duration.milliseconds(1)
 
-        assertEquals(0.5.milliseconds, elapsed)
+        assertEquals(Duration.milliseconds(0.5), elapsed)
         assertEquals(elapsedFromFuture, markFuture1.elapsedNow())
         assertEquals(elapsedFromFuture, markFuture2.elapsedNow())
 
@@ -45,7 +49,7 @@ class TimeMarkTest {
         markFuture1.assertHasPassed(false)
         markPast1.assertHasPassed(true)
 
-        timeSource += 1.milliseconds
+        timeSource += Duration.milliseconds(1)
 
         markFuture1.assertHasPassed(true)
         markPast1.assertHasPassed(true)
