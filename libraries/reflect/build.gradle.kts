8c95b783463f20029c74e613d9802b22bb2ff40d
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-02-15 18:07:38 +0100

    Update JVM metadata version to 1.5.0
    Improve the test which checks that we use correct metadata version if
    `-language-version` is passed by checking all supported language
    versions.
    
    The change in libraries/reflect/build.gradle.kts is needed because
    kotlinx-metadata-jvm of version 0.1.0 is based on pre-1.4 Kotlin, which
    doesn't support the new module file metadata generated with metadata
    version 1.4 and later, and module files need to be readable there to be
    able to transform them for the shadow plugin.
    
    Similarly override dependency on kotlinx-metadata-jvm in the
    binary-compatibility-validator module.

--- libraries/reflect/build.gradle.kts
+++ libraries/reflect/build.gradle.kts
@@ -11,7 +11,7 @@ description = "Kotlin Full Reflection Library"
 
 buildscript {
     dependencies {
-        classpath("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.1.0")
+        classpath("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.2.0")
     }
 }
 
