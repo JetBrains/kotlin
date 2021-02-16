5ce36a528ec50cb00ff7a6ef77c3b5ba5232a220
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-02-16 12:50:51 +0300

    [FE] Prohibit sealed fun interfaces
    #KT-44947 Fixed

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
@@ -24587,6 +24587,12 @@ public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
                 }
 
                 @Test
+                @TestMetadata("sealedFunInterface.kt")
+                public void testSealedFunInterface() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/sealed/interfaces/sealedFunInterface.kt");
+                }
+
+                @Test
                 @TestMetadata("sealedInterfacesDisabled.kt")
                 public void testSealedInterfacesDisabled() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/sealed/interfaces/sealedInterfacesDisabled.kt");
