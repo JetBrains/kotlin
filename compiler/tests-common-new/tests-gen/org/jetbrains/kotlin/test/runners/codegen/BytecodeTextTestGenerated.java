134fda8bad8d8e1cd6da49e3d298a9f6e11a6104
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2021-02-17 11:07:28 +0100

    Support Unit/V types in string-concat indy calls
     unitComponent.kt test fails with JVM target 9+

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BytecodeTextTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BytecodeTextTestGenerated.java
@@ -4949,6 +4949,12 @@ public class BytecodeTextTestGenerated extends AbstractBytecodeTextTest {
         }
 
         @Test
+        @TestMetadata("concatDynamicUnit.kt")
+        public void testConcatDynamicUnit() throws Exception {
+            runTest("compiler/testData/codegen/bytecodeText/stringOperations/concatDynamicUnit.kt");
+        }
+
+        @Test
         @TestMetadata("concatNotDynamic.kt")
         public void testConcatNotDynamic() throws Exception {
             runTest("compiler/testData/codegen/bytecodeText/stringOperations/concatNotDynamic.kt");
