9bbd6ccb87c2712faafcf339eaa20766eda1b079
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2021-02-18 07:22:40 +0100

    Fix test data

--- compiler/testData/codegen/bytecodeText/properties/openDataClass.kt
+++ compiler/testData/codegen/bytecodeText/properties/openDataClass.kt
@@ -1,3 +1,4 @@
+// IGNORE_BACKEND_FIR: JVM_IR
 // This test emulates 'allopen' compiler plugin.
 
 @Suppress("INCOMPATIBLE_MODIFIERS")
--- compiler/testData/codegen/bytecodeText/statements/labeled.kt
+++ compiler/testData/codegen/bytecodeText/statements/labeled.kt
@@ -1,5 +1,3 @@
-// IGNORE_BACKEND_FIR: JVM_IR
-
 fun main()
 {
     l@ if (2 != 1) "fail 3"
