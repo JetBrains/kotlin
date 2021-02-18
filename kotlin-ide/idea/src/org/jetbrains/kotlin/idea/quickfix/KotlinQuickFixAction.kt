5fe69ad6676d8fe4d0e804b04247dbdb2b0fddb0
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-02-18 10:19:35 +0300

    Propgate `-Xskip-prerelease-check` in kotlin-ultimate

--- kotlin-ide/idea/src/org/jetbrains/kotlin/idea/quickfix/KotlinQuickFixAction.kt
+++ kotlin-ide/idea/src/org/jetbrains/kotlin/idea/quickfix/KotlinQuickFixAction.kt
@@ -12,6 +12,11 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.caches.resolve.allowCachedResolveInDispatchThread
 import org.jetbrains.kotlin.psi.KtFile
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.application.ApplicationManager
+import org.jetbrains.kotlin.psi.CREATE_BY_PATTERN_MAY_NOT_REFORMAT
+import org.jetbrains.kotlin.psi.KtCodeFragment
+import org.jetbrains.kotlin.psi.psiUtil.createSmartPointer
 
 abstract class KotlinQuickFixAction<out T : PsiElement>(element: T) : QuickFixActionBase<T>(element) {
     protected open fun isAvailable(project: Project, editor: Editor?, file: KtFile) = true
@@ -33,6 +38,35 @@ abstract class KotlinQuickFixAction<out T : PsiElement>(element: T) : QuickFixAc
     }
 
     protected abstract operator fun invoke(project: Project, editor: Editor?, file: KtFile)
+
+    override fun startInWriteAction() = true
+}
+
+abstract class QuickFixActionBase<out T : PsiElement>(element: T) : IntentionAction {
+    private val elementPointer = element.createSmartPointer()
+
+    protected val element: T?
+        get() = elementPointer.element
+
+    open val isCrossLanguageFix: Boolean = false
+
+    protected open fun isAvailableImpl(project: Project, editor: Editor?, file: PsiFile) = true
+
+    final override fun isAvailable(project: Project, editor: Editor?, file: PsiFile): Boolean {
+        if (ApplicationManager.getApplication().isUnitTestMode) {
+            CREATE_BY_PATTERN_MAY_NOT_REFORMAT = true
+        }
+        try {
+            val element = element ?: return false
+            return element.isValid &&
+                    !element.project.isDisposed &&
+                    (file.manager.isInProject(file) || file is KtCodeFragment || (file is KtFile && file.isScript())) &&
+                    (file is KtFile || isCrossLanguageFix) &&
+                    isAvailableImpl(project, editor, file)
+        } finally {
+            CREATE_BY_PATTERN_MAY_NOT_REFORMAT = false
+        }
+    }
 
     override fun startInWriteAction() = true
 }
