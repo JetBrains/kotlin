141b6b0e5503272d018ac69e6d9d1d6d704002f3
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-02-19 14:33:11 +0100

    FIR IDE: use different .after testdata for AbstractHLIntentionTest.kt

--- idea/idea-fir/tests/org/jetbrains/kotlin/idea/intentions/AbstractHLIntentionTest.kt
+++ idea/idea-fir/tests/org/jetbrains/kotlin/idea/intentions/AbstractHLIntentionTest.kt
@@ -12,6 +12,12 @@ import java.io.File
 
 abstract class AbstractHLIntentionTest : AbstractIntentionTest() {
     override fun intentionFileName() = ".firIntention"
+
+    override fun afterFileNameSuffix(ktFilePath: File): String {
+        return if (ktFilePath.resolveSibling(ktFilePath.name + AFTER_FIR_EXTENSION).exists()) AFTER_FIR_EXTENSION
+        else super.afterFileNameSuffix(ktFilePath)
+    }
+
     override fun isFirPlugin() = true
 
     override fun doTestFor(mainFile: File, pathToFiles: Map<String, PsiFile>, intentionAction: IntentionAction, fileText: String) {
@@ -22,4 +28,8 @@ abstract class AbstractHLIntentionTest : AbstractIntentionTest() {
 
     override fun checkForErrorsAfter(fileText: String) {}
     override fun checkForErrorsBefore(fileText: String) {}
+
+    companion object {
+        private const val AFTER_FIR_EXTENSION = ".after.fir"
+    }
 }
