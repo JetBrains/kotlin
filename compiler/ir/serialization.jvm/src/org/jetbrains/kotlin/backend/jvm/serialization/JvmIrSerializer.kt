44c1a9148b5230399e0d6fee8195bf47c912b51a
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2021-02-20 10:49:32 +0300

    JVM_IR: JvmIrSerializer

---
+++ compiler/ir/serialization.jvm/src/org/jetbrains/kotlin/backend/jvm/serialization/JvmIrSerializer.kt
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.jvm.serialization
+
+import org.jetbrains.kotlin.backend.common.serialization.DeclarationTable
+import org.jetbrains.kotlin.backend.common.serialization.IrFileSerializer
+import org.jetbrains.kotlin.backend.jvm.serialization.proto.JvmIr
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.fileClasses.JvmFileClassUtil
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrFile
+import org.jetbrains.kotlin.ir.symbols.IrSymbol
+import org.jetbrains.kotlin.ir.util.IrMessageLogger
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi2ir.PsiSourceManager
+
+class JvmIrSerializer(
+    messageLogger: IrMessageLogger,
+    declarationTable: DeclarationTable,
+    expectDescriptorToSymbol: MutableMap<DeclarationDescriptor, IrSymbol>,
+    private val psiSourceManager: PsiSourceManager,
+    externallyVisibleOnly: Boolean = true,
+    skipExpects: Boolean = false,
+) : IrFileSerializer(messageLogger, declarationTable, expectDescriptorToSymbol, externallyVisibleOnly, skipExpects) {
+
+    // Usage protocol: construct an instance, call only one of `serializeIrFile()` and `serializeTopLevelClass()` only once.
+
+    fun serializeJvmIrFile(irFile: IrFile): JvmIr.JvmIrFile {
+        val proto = JvmIr.JvmIrFile.newBuilder()
+
+        irFile.declarations.filter { it !is IrClass }.forEach { declaration ->
+            proto.addDeclaration(serializeDeclaration(declaration))
+        }
+        proto.addAllAnnotation(serializeAnnotations(irFile.annotations))
+
+
+        val facadeFqName = irFile.facadeFqName()
+        proto.addAllFacadeFqName(serializeFqName(facadeFqName.toString()))
+
+        // TODO -- serialize referencesToTopLevelMap
+        proto.auxTables = serializeAuxTables()
+
+        return proto.build()
+    }
+
+    fun serializeTopLevelClass(irClass: IrClass): JvmIr.JvmIrClass {
+        val proto = JvmIr.JvmIrClass.newBuilder()
+        proto.irClass = serializeIrClass(irClass)
+        proto.auxTables = serializeAuxTables()
+        return proto.build()
+    }
+
+    private fun serializeAuxTables(): JvmIr.AuxTables {
+        val proto = JvmIr.AuxTables.newBuilder()
+        proto.addAllType(protoTypeArray)
+        proto.addAllSignature(protoIdSignatureArray)
+        proto.addAllString(protoStringArray)
+        for (body in protoBodyArray) {
+            val bodyProto = JvmIr.XStatementOrExpression.newBuilder()
+            when (body) {
+                is XStatementOrExpression.XStatement -> bodyProto.statement = body.toProtoStatement()
+                is XStatementOrExpression.XExpression -> bodyProto.expression = body.toProtoExpression()
+            }
+            proto.addBody(bodyProto.build())
+        }
+        return proto.build()
+    }
+
+    private fun IrFile.facadeFqName(): FqName {
+        val fileEntry = fileEntry
+        val ktFile = psiSourceManager.getKtFile(fileEntry as PsiSourceManager.PsiFileEntry)
+            ?: throw AssertionError("Unexpected file entry: $fileEntry")
+        val fileClassInfo = JvmFileClassUtil.getFileClassInfoNoResolve(ktFile)
+        return fileClassInfo.facadeClassFqName
+    }
+}
