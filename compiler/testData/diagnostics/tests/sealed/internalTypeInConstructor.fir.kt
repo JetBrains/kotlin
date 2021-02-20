6a0e9d5d383de5f78dae4bed12a78185c6e2ba14
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-02-20 16:18:48 +0300

    Fix FIR testdata

--- compiler/testData/diagnostics/tests/sealed/internalTypeInConstructor.fir.kt
+++
@@ -1,9 +0,0 @@
-// ISSUE: KT-45033
-// DIAGNOSTICS: -UNUSED_PARAMETER
-
-internal class Bar
-
-sealed class Foo(
-    <!EXPOSED_PARAMETER_TYPE!>internal val x: Bar<!>,
-    <!EXPOSED_PARAMETER_TYPE!>y: Bar<!>
-)
