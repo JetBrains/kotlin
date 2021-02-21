54d20b5768b42afb0e71aab58ae5ef44e251725d
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-02-21 06:05:03 +0300

    Support mod and floorDiv extensions in constant evaluator
    Ignore evaluation tests for floorDiv and mod with FIR for now.
    
    (cherry picked from commit 9cc8f443905c661f0c4f471a941c4f68694e6f3d)

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -14225,6 +14225,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         }
 
         @Test
+        @TestMetadata("floorDiv.kt")
+        public void testFloorDiv() throws Exception {
+            runTest("compiler/testData/codegen/box/evaluate/floorDiv.kt");
+        }
+
+        @Test
         @TestMetadata("intrinsics.kt")
         public void testIntrinsics() throws Exception {
             runTest("compiler/testData/codegen/box/evaluate/intrinsics.kt");
@@ -14258,6 +14264,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         @TestMetadata("minus.kt")
         public void testMinus() throws Exception {
             runTest("compiler/testData/codegen/box/evaluate/minus.kt");
+        }
+
+        @Test
+        @TestMetadata("mod.kt")
+        public void testMod() throws Exception {
+            runTest("compiler/testData/codegen/box/evaluate/mod.kt");
         }
 
         @Test
--- compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -11694,6 +11694,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             runTest("compiler/testData/codegen/box/evaluate/divide.kt");
         }
 
+        @TestMetadata("floorDiv.kt")
+        public void testFloorDiv() throws Exception {
+            runTest("compiler/testData/codegen/box/evaluate/floorDiv.kt");
+        }
+
         @TestMetadata("intrinsics.kt")
         public void testIntrinsics() throws Exception {
             runTest("compiler/testData/codegen/box/evaluate/intrinsics.kt");
@@ -11722,6 +11727,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
         @TestMetadata("minus.kt")
         public void testMinus() throws Exception {
             runTest("compiler/testData/codegen/box/evaluate/minus.kt");
+        }
+
+        @TestMetadata("mod.kt")
+        public void testMod() throws Exception {
+            runTest("compiler/testData/codegen/box/evaluate/mod.kt");
         }
 
         @TestMetadata("multiply.kt")
