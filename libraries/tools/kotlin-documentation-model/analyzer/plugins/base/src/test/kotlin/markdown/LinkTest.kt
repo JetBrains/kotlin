b2afff9c6b48160d8ea2f60248a5ebc36f1cbe72
Marcin Aman <marcin.aman@gmail.com>
2021-03-01 11:57:59 +0100

    [KDM] Bump markdown to 0.2.0 and fix negation in links being interpreted as img
    See https://github.com/Kotlin/dokka/pull/1754
    
    * Bump markdown library to 0.2.0
    
    * Render shorthand images as link with text
    Original commit: https://github.com/Kotlin/dokka/commit/32fc2899cbe08e4e30326128e0f642ac4e08a342

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/markdown/LinkTest.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/markdown/LinkTest.kt
@@ -6,7 +6,8 @@ import org.jetbrains.dokka.pages.ClasslikePageNode
 import org.jetbrains.dokka.pages.ContentDRILink
 import org.jetbrains.dokka.pages.MemberPageNode
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
-import org.jetbrains.dokka.model.doc.DocumentationLink
+import org.jetbrains.dokka.links.*
+import org.jetbrains.dokka.model.doc.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
@@ -39,7 +40,8 @@ class LinkTest : BaseAbstractTest() {
                     .content
                     .dfs { node ->
                         node is ContentDRILink &&
-                                node.address.toString() == "parser//test/#java.lang.ClassLoader/PointingToDeclaration/"}
+                                node.address.toString() == "parser//test/#java.lang.ClassLoader/PointingToDeclaration/"
+                    }
                 )
             }
         }
@@ -75,7 +77,7 @@ class LinkTest : BaseAbstractTest() {
                 val destinationDri = (root.documentable as WithGenerics).generics.first().dri.toString()
 
                 assertEquals(destinationDri, "/Outer///PointingToGenericParameters(0)/")
-                assertNotNull(foo.content.dfs { it is ContentDRILink && it.address.toString() == destinationDri } )
+                assertNotNull(foo.content.dfs { it is ContentDRILink && it.address.toString() == destinationDri })
             }
         }
     }
@@ -91,7 +93,7 @@ class LinkTest : BaseAbstractTest() {
         }
 
         testInline(
-                """
+            """
             |/src/main/kotlin/Test.kt
             |package example
             |
@@ -101,13 +103,132 @@ class LinkTest : BaseAbstractTest() {
             |fun stop(hammerTime: String, waitAMinute: String) {}
             |
         """.trimMargin(),
-                configuration
+            configuration
         ) {
             documentablesMergingStage = { module ->
                 val parameter = module.dfs { it.name == "waitAMinute" }
-                val link = module.dfs { it.name == "stop" }!!.documentation.values.single().dfs { it is DocumentationLink } as DocumentationLink
+                val link = module.dfs { it.name == "stop" }!!.documentation.values.single()
+                    .dfs { it is DocumentationLink } as DocumentationLink
 
                 assertEquals(parameter!!.dri, link.dri)
+            }
+        }
+    }
+
+    @Test
+    fun `link with exclamation mark`() {
+        val configuration = dokkaConfiguration {
+            sourceSets {
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                }
+            }
+        }
+
+        testInline(
+            """
+            |/src/main/kotlin/Test.kt
+            |package example
+            |
+            |/** 
+            |* Link to ![waitAMinute]
+            |*/
+            |fun stop(hammerTime: String, waitAMinute: String) {}
+            |
+        """.trimMargin(),
+            configuration
+        ) {
+            documentablesMergingStage = { module ->
+                val functionDocs = module.packages.flatMap { it.functions }.first().documentation.values.first()
+                val expected = Description(
+                    root = CustomDocTag(
+                        children = listOf(
+                            P(
+                                children = listOf(
+                                    Text("Link to !"),
+                                    DocumentationLink(
+                                        dri = DRI(
+                                            packageName = "example",
+                                            callable = Callable(
+                                                "stop",
+                                                receiver = null,
+                                                params = listOf(
+                                                    TypeConstructor("kotlin.String", emptyList()),
+                                                    TypeConstructor("kotlin.String", emptyList())
+                                                )
+                                            ),
+                                            target = PointingToCallableParameters(1)
+                                        ),
+                                        children = listOf(
+                                            Text("waitAMinute")
+                                        ),
+                                        params = mapOf("href" to "[waitAMinute]")
+                                    )
+                                )
+                            )
+                        ),
+                        name = "MARKDOWN_FILE"
+                    )
+                )
+
+                assertEquals(expected, functionDocs.children.first())
+            }
+        }
+    }
+
+    @Test
+    fun `link to property with exclamation mark`() {
+        val configuration = dokkaConfiguration {
+            sourceSets {
+                sourceSet {
+                    sourceRoots = listOf("src/")
+                }
+            }
+        }
+
+        testInline(
+            """
+            |/src/main/kotlin/Testing.kt
+            |package example
+            |
+            |/** 
+            |* Link to ![Testing.property]
+            |*/
+            |class Testing {
+            |   var property = ""
+            |}
+            |
+        """.trimMargin(),
+            configuration
+        ) {
+            documentablesMergingStage = { module ->
+                val functionDocs = module.packages.flatMap { it.classlikes }.first().documentation.values.first()
+                val expected = Description(
+                    root = CustomDocTag(
+                        children = listOf(
+                            P(
+                                children = listOf(
+                                    Text("Link to !"),
+                                    DocumentationLink(
+                                        dri = DRI(
+                                            packageName = "example",
+                                            classNames = "Testing",
+                                            callable = Callable("property", null, emptyList()),
+                                            target = PointingToDeclaration
+                                        ),
+                                        children = listOf(
+                                            Text("Testing.property")
+                                        ),
+                                        params = mapOf("href" to "[Testing.property]")
+                                    )
+                                )
+                            )
+                        ),
+                        name = "MARKDOWN_FILE"
+                    )
+                )
+
+                assertEquals(expected, functionDocs.children.first())
             }
         }
     }
