6b5379e6c0253ee47f14a4608a384c68df764099
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2021-03-01 23:09:15 +0300

    Implement String case conversion generators #KT-45213
    (cherry picked from commit fc5e4c20c19ece3ac808095207ae4431421e4389)

---
+++ libraries/tools/kotlin-stdlib-gen/src/generators/unicode/PropertyLine.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package generators.unicode
+
+internal class PropertyLine(properties: List<String>) {
+    init {
+        require(properties.size == 2)
+    }
+
+    val rangeStart: String = properties[0].split("..").first()
+    val rangeEnd: String = properties[0].split("..").last()
+    val property: String = properties[1].takeWhile { it != ' ' }
+
+    override fun toString(): String {
+        return "PropertyLine{rangeStart=$rangeStart" +
+                ", rangeEnd=$rangeEnd" +
+                ", property=$property" +
+                "}"
+    }
+}
