81716d0ca25828494bc29f5f3b243ef62182ed66
Marcin Aman <marcin.aman@gmail.com>
2021-03-01 11:57:30 +0100

    [KDM] Suppress tag support
    See https://github.com/Kotlin/dokka/pull/1742
    
    * Suppress tag support
    
    * Support Hide tag in javadoc
    
    * Extract hide tag to be in separate plugin
    Original commit: https://github.com/Kotlin/dokka/commit/c01e49eec9558736959d12820361624a3c3e41e5

---
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/SuppressTagDocumentableFilter.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.dokka.base.transformers.documentables
+
+import org.jetbrains.dokka.model.Documentable
+import org.jetbrains.dokka.model.dfs
+import org.jetbrains.dokka.plugability.DokkaContext
+import org.jetbrains.dokka.model.doc.Suppress
+
+class SuppressTagDocumentableFilter(val dokkaContext: DokkaContext) :
+    SuppressedByTagDocumentableFilterTransformer(dokkaContext) {
+    override fun shouldBeSuppressed(d: Documentable): Boolean =
+        d.documentation.any { (_, docs) -> docs.dfs { it is Suppress } != null }
+}
---
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/transformers/SuppressTagFilterTest.kt
@@ -0,0 +1,187 @@
+package transformers
+
+import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
+import org.jetbrains.dokka.model.DEnum
+import org.jetbrains.dokka.model.WithCompanion
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class SuppressTagFilterTest : BaseAbstractTest() {
+    private val configuration = dokkaConfiguration {
+        sourceSets {
+            sourceSet {
+                sourceRoots = listOf("src")
+            }
+        }
+    }
+
+    @Test
+    fun `should filter classes with suppress tag`() {
+        testInline(
+            """
+            |/src/suppressed/NotSuppressed.kt
+            |/**
+            | * sample docs
+            |*/
+            |class NotSuppressed 
+            |/src/suppressed/Suppressed.kt
+            |/**
+            | * sample docs
+            | * @suppress
+            |*/
+            |class Suppressed 
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                assertEquals(
+                    "NotSuppressed",
+                    modules.flatMap { it.packages }.flatMap { it.classlikes }.singleOrNull()?.name
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `should filter functions with suppress tag`() {
+        testInline(
+            """
+            |/src/suppressed/Suppressed.kt
+            |class Suppressed {
+            |   /**
+            |    * sample docs
+            |    * @suppress
+            |    */
+            |   fun suppressedFun(){ }
+            |}
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                assertNull(modules.flatMap { it.packages }.flatMap { it.classlikes }.flatMap { it.functions }
+                    .firstOrNull { it.name == "suppressedFun" })
+            }
+        }
+    }
+
+    @Test
+    fun `should filter top level functions`() {
+        testInline(
+            """
+            |/src/suppressed/Suppressed.kt
+            |/**
+            | * sample docs
+            | * @suppress
+            | */
+            |fun suppressedFun(){ }
+            |
+            |/**
+            | * Sample
+            | */
+            |fun notSuppressedFun() { }
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                assertNull(modules.flatMap { it.packages }.flatMap { it.functions }
+                    .firstOrNull { it.name == "suppressedFun" })
+            }
+        }
+    }
+
+    @Test
+    fun `should filter top level type aliases`() {
+        testInline(
+            """
+            |/src/suppressed/suppressed.kt
+            |/**
+            | * sample docs
+            | * @suppress
+            | */
+            |typealias suppressedTypeAlias = String
+            |
+            |/**
+            | * Sample
+            | */
+            |typealias notSuppressedTypeAlias = String
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                assertNull(modules.flatMap { it.packages }.flatMap { it.typealiases }
+                    .firstOrNull { it.name == "suppressedTypeAlias" })
+                assertNotNull(modules.flatMap { it.packages }.flatMap { it.typealiases }
+                    .firstOrNull { it.name == "notSuppressedTypeAlias" })
+            }
+        }
+    }
+
+    @Test
+    fun `should filter companion object`() {
+        testInline(
+            """
+            |/src/suppressed/Suppressed.kt
+            |class Suppressed {
+            |/**
+            | * @suppress
+            | */
+            |companion object {
+            |    val x = 1
+            |}
+            |}
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                assertNull((modules.flatMap { it.packages }.flatMap { it.classlikes }
+                    .firstOrNull { it.name == "Suppressed" } as? WithCompanion)?.companion)
+            }
+        }
+    }
+
+    @Test
+    fun `should suppress inner classlike`() {
+        testInline(
+            """
+            |/src/suppressed/Testing.kt
+            |class Testing {
+            |    /**
+            |     * Sample
+            |     * @suppress
+            |     */
+            |    inner class Suppressed {
+            |        val x = 1
+            |    }
+            |}
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                val testingClass = modules.flatMap { it.packages }.flatMap { it.classlikes }.single()
+                assertNull(testingClass.classlikes.firstOrNull())
+            }
+        }
+    }
+
+    @Test
+    fun `should suppress enum entry`() {
+        testInline(
+            """
+            |/src/suppressed/Testing.kt
+            |enum class Testing {
+            |    /**
+            |     * Sample
+            |     * @suppress
+            |     */
+            |    SUPPRESSED,
+            | 
+            |   /**
+            |     * Not suppressed
+            |     */
+            |    NOT_SUPPRESSED
+            |}
+            """.trimIndent(), configuration
+        ) {
+            documentablesFirstTransformationStep = { modules ->
+                val testingClass = modules.flatMap { it.packages }.flatMap { it.classlikes }.single() as DEnum
+                assertEquals(listOf("NOT_SUPPRESSED"), testingClass.entries.map { it.name })
+            }
+        }
+    }
+}
