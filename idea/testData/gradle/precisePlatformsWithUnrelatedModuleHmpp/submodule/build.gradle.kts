ee50a584a72445374e93a3de0ab2f5fc17ca996c
Yaroslav Chernyshev <yaroslav.chernyshev@jetbrains.com>
2021-03-11 22:30:38 +0300

    Precise platform importing changes ported from new repository
    (cherry picked from commit d053ee33a8fb0fcbf7fa5620ac25ccb7a0c99310)

---
+++ idea/testData/gradle/precisePlatformsWithUnrelatedModuleHmpp/submodule/build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        {{kts_kotlin_plugin_repositories}}
+    }
+}
+
+repositories {
+    {{kts_kotlin_plugin_repositories}}
+}
+
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    js()
+    jvm()
+}
