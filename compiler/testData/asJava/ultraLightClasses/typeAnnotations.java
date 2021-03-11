d4493104d2c83b8aec6d52d5b5aa3936e6cd2b65
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2021-03-11 17:44:18 +0100

    [ULC] Fix invalid primitive type annotating
    Fixed #KT-45417

--- compiler/testData/asJava/ultraLightClasses/typeAnnotations.java
+++ compiler/testData/asJava/ultraLightClasses/typeAnnotations.java
@@ -54,9 +54,19 @@ public final class Y /* Y*/ {
 }
 
 public final class klass /* klass*/ {
+  @org.jetbrains.annotations.Nullable()
+  private final java.util.List<@A0() java.lang.Integer> y;
+
+  private final @A0() int x;
+
   @org.jetbrains.annotations.NotNull()
   public final @A6() X annotatedMethod(@org.jetbrains.annotations.NotNull() @A0() P<@A1() X, P<@A2() @A3() X, @A4() Y>>, @org.jetbrains.annotations.NotNull() @A5() Y[]);//  annotatedMethod(@A0() P<@A1() X, P<@A2() @A3() X, @A4() Y>>, @A5() Y[])
 
+  @org.jetbrains.annotations.Nullable()
+  public final java.util.List<@A0() java.lang.Integer> getY();//  getY()
+
   public  klass();//  .ctor()
+
+  public final @A0() int getX();//  getX()
 
 }
