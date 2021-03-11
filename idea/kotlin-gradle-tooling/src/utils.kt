aff49c76a913fb814e7687d0d8882a792d3e6c5d
Yaroslav Chernyshev <yaroslav.chernyshev@jetbrains.com>
2021-03-11 22:51:05 +0300

    loadClassOrNull: catch 'ClassNotFoundException' and 'LinkageError' over 'Exception'

--- idea/kotlin-gradle-tooling/src/utils.kt
+++ idea/kotlin-gradle-tooling/src/utils.kt
@@ -5,8 +5,6 @@
 
 package org.jetbrains.kotlin.gradle
 
-import org.gradle.api.Named
-
 fun Class<*>.getMethodOrNull(name: String, vararg parameterTypes: Class<*>) =
     try {
         getMethod(name, *parameterTypes)
@@ -24,7 +22,9 @@ fun Class<*>.getDeclaredMethodOrNull(name: String, vararg parameterTypes: Class<
 fun ClassLoader.loadClassOrNull(name: String): Class<*>? {
     return try {
         loadClass(name)
-    } catch (e: Exception) {
+    } catch (e: LinkageError) {
+        return null
+    } catch (e: ClassNotFoundException) {
         return null
     }
 }
