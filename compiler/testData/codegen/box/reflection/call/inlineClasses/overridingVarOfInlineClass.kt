71f79a1ffc0b701a164e0b634c81d1df31985e34
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2021-03-12 03:19:06 +0100

    Revert "Raise RESERVED_VAR_PROPERTY_OF_VALUE_CLASS to error"
    This reverts commit bad197e07599cbca1841bea4b9abc1d27b6bb7c2.

--- compiler/testData/codegen/box/reflection/call/inlineClasses/overridingVarOfInlineClass.kt
+++ compiler/testData/codegen/box/reflection/call/inlineClasses/overridingVarOfInlineClass.kt
@@ -10,7 +10,6 @@ interface ITest {
 }
 
 inline class Z(val x: Int) : ITest {
-    @Suppress("RESERVED_VAR_PROPERTY_OF_VALUE_CLASS")
     override var test: S
         get() = S("${global.x}$x")
         set(value) {
@@ -19,7 +18,6 @@ inline class Z(val x: Int) : ITest {
 }
 
 inline class L(val x: Long) : ITest {
-    @Suppress("RESERVED_VAR_PROPERTY_OF_VALUE_CLASS")
     override var test: S
         get() = S("${global.x}$x")
         set(value) {
@@ -28,7 +26,6 @@ inline class L(val x: Long) : ITest {
 }
 
 inline class S(val x: String) : ITest {
-    @Suppress("RESERVED_VAR_PROPERTY_OF_VALUE_CLASS")
     override var test: S
         get() = S("${global.x}$x")
         set(value) {
@@ -37,7 +34,6 @@ inline class S(val x: String) : ITest {
 }
 
 inline class A(val x: Any) : ITest {
-    @Suppress("RESERVED_VAR_PROPERTY_OF_VALUE_CLASS")
     override var test: S
         get() = S("${global.x}$x")
         set(value) {
