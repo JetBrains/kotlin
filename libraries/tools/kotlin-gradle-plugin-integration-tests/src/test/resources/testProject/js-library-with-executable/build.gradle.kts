da539384439ad1090656e3e1b0fe51fd7c6e660d
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-03-17 20:17:09 +0300

    [Gradle, JS] Use one folder for sync tasks
    [Gradle, JS] WA for tests with multiple binaries
    
    ^KT-45574 fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/js-library-with-executable/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/js-library-with-executable/build.gradle.kts
@@ -12,8 +12,16 @@ repositories {
 
 kotlin {
     js {
+        // For now we do not recommend multiple binaries, so we test it just in case
         binaries.library()
         binaries.executable()
         nodejs()
     }
+}
+
+
+// We need it for suppress warnings of Gradle 7.0
+// We need to think about it, when we will support multiple binaries
+tasks.named("nodeProductionLibraryPrepare") {
+    mustRunAfter("productionExecutableCompileSync")
 }
