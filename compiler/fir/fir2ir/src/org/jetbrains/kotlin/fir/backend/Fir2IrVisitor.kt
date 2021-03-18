7898d167f3ae7a767e7007967a33a94ee13db3fb
Jinseong Jeon <jsjeon@google.com>
2021-03-18 00:29:09 -0700

    FIR2IR: wrap do-while loop in IrBlock
    "so that variables declared in loop body are not visible outside of the
    loop" (from commit d096f1d)

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
@@ -728,7 +728,7 @@ class Fir2IrVisitor(
     private val loopMap = mutableMapOf<FirLoop, IrLoop>()
 
     override fun visitDoWhileLoop(doWhileLoop: FirDoWhileLoop, data: Any?): IrElement {
-        return doWhileLoop.convertWithOffsets { startOffset, endOffset ->
+        val irLoop = doWhileLoop.convertWithOffsets { startOffset, endOffset ->
             IrDoWhileLoopImpl(
                 startOffset, endOffset, irBuiltIns.unitType,
                 IrStatementOrigin.DO_WHILE_LOOP
@@ -741,6 +741,9 @@ class Fir2IrVisitor(
             }
         }.also {
             doWhileLoop.accept(implicitCastInserter, it)
+        }
+        return IrBlockImpl(irLoop.startOffset, irLoop.endOffset, irBuiltIns.unitType).apply {
+            statements.add(irLoop)
         }
     }
 
