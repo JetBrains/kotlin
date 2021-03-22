3154234107731047d6464f1ef3c69666234f7431
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-03-22 12:22:24 +0100

    FIR IDE: do not run STATUS phase under lock

--- idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirPhaseRunner.kt
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirPhaseRunner.kt
@@ -23,14 +23,10 @@ import kotlin.concurrent.withLock
 
 internal class FirPhaseRunner {
     private val superTypesBodyResolveLock = ReentrantLock()
-    private val statusResolveLock = ReentrantLock()
     private val implicitTypesResolveLock = ReentrantLock()
 
     fun runPhase(firFile: FirFile, phase: FirResolvePhase, scopeSession: ScopeSession) = when (phase) {
         FirResolvePhase.SUPER_TYPES -> superTypesBodyResolveLock.withLock {
-            runPhaseWithoutLock(firFile, phase, scopeSession)
-        }
-        FirResolvePhase.STATUS -> statusResolveLock.withLock {
             runPhaseWithoutLock(firFile, phase, scopeSession)
         }
         FirResolvePhase.IMPLICIT_TYPES_BODY_RESOLVE -> implicitTypesResolveLock.withLock {
@@ -43,9 +39,6 @@ internal class FirPhaseRunner {
 
     inline fun runPhaseWithCustomResolve(phase: FirResolvePhase, crossinline resolve: () -> Unit) = when (phase) {
         FirResolvePhase.SUPER_TYPES -> superTypesBodyResolveLock.withLock {
-            runPhaseWithCustomResolveWithoutLock(resolve)
-        }
-        FirResolvePhase.STATUS -> statusResolveLock.withLock {
             runPhaseWithCustomResolveWithoutLock(resolve)
         }
         FirResolvePhase.IMPLICIT_TYPES_BODY_RESOLVE -> implicitTypesResolveLock.withLock {
