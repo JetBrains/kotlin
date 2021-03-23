2b7866402d10dba602c7db90978fd52685c762d9
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-03-23 15:34:39 +0100

    [Commonizer] Move `KonanDistribution` to :native:kotlin-klib-commonizer-api

--- native/commonizer-api/src/org/jetbrains/kotlin/commonizer/KonanDistribution.kt
+++ native/commonizer-api/src/org/jetbrains/kotlin/commonizer/KonanDistribution.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,13 +8,13 @@ package org.jetbrains.kotlin.commonizer
 import org.jetbrains.kotlin.konan.library.*
 import java.io.File
 
-internal data class KonanDistribution(val root: File)
+public data class KonanDistribution(val root: File)
 
-internal val KonanDistribution.stdlib: File
+public val KonanDistribution.stdlib: File
     get() = root.resolve(konanCommonLibraryPath(KONAN_STDLIB_NAME))
 
-internal val KonanDistribution.klibDir: File
+public val KonanDistribution.klibDir: File
     get() = root.resolve(KONAN_DISTRIBUTION_KLIB_DIR)
 
-internal val KonanDistribution.platformLibsDir: File
+public val KonanDistribution.platformLibsDir: File
     get() = klibDir.resolve(KONAN_DISTRIBUTION_PLATFORM_LIBS_DIR)
