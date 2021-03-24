45fa3cafd28a3b718bae541d7ba81e6fafc39644
Nick Anthony <nickanthony@google.com>
2021-03-24 09:26:45 -0400

    Update the Compose Compiler plugin version checker for beta04
    This should have landed with the bump to 1.0.0-beta04
    
    This also aligns androidx.hilt:hilt-navigation-compose
    to use the same version of compose and kotlin. Furthermore
    it also updates navigation-compose to not pin its dependency
    to viewmodel-compose since that will downgrade the runtime
    on Android Test variants due to AGP constrains.
    
    Bug: 183511654 ( https://issuetracker.google.com/issues/183511654 )
    Test: gw bOS
    Test: gw :hilt:hilt-navigation-compose:compileReleaseKotlin
    Test: gw :navigation:navigation-compose:compileDebugAndroidTestKotlin
    Change-Id: I8e79af3f1a27d0c2adaa3ad80e1666be8ded2027 ( https://android-review.git.corp.google.com/q/I8e79af3f1a27d0c2adaa3ad80e1666be8ded2027 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -38,19 +38,20 @@ class VersionChecker(val context: IrPluginContext) {
             2200 to "1.0.0-alpha11",
             2300 to "1.0.0-alpha12",
             2400 to "1.0.0-alpha13",
+            2500 to "1.0.0-beta04",
         )
 
         /**
          * The minimum version int that this compiler is guaranteed to be compatible with. Typically
          * this will match the version int that is in ComposeVersion.kt in the runtime.
          */
-        private val minimumRuntimeVersionInt: Int = 2400
+        private val minimumRuntimeVersionInt: Int = 2500
 
         /**
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        val compilerVersion: String = "1.0.0-alpha13"
+        val compilerVersion: String = "1.0.0-beta04"
         private val minimumRuntimeVersion: String
             get() = versionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
