a26a195c35755ec50523fd5eff24fcd9fe4eb069
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-03-24 13:49:30 +0300

    Add FIR_IDENTICAL to some tests

--- compiler/testData/diagnostics/tests/multiplatform/inlineClasses/jvmInlineExpectValueClass.kt
+++ compiler/testData/diagnostics/tests/multiplatform/inlineClasses/jvmInlineExpectValueClass.kt
@@ -1,3 +1,4 @@
+// FIR_IDENTICAL
 // !LANGUAGE: +MultiPlatformProjects
 // SKIP_TXT
 // MODULE: m1-common
--- compiler/testData/diagnostics/testsWithStdLib/coroutines/allowResultInReturnType_1_3.kt
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/allowResultInReturnType_1_3.kt
@@ -1,3 +1,4 @@
+// FIR_IDENTICAL
 // !LANGUAGE: +InlineClasses -AllowResultInReturnType
 
 fun result(): Result<Int> = TODO()
