ae4dd80254ff0c6aae15794a152a8a4333cf2aa3
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-03-25 10:19:24 +0100

    Regenerate missed test
    Relates to #KT-45339

--- idea/idea-fir-performance-tests/tests/org/jetbrains/kotlin/idea/highlighter/FirHighlightingPerformanceTestGenerated.java
+++ idea/idea-fir-performance-tests/tests/org/jetbrains/kotlin/idea/highlighter/FirHighlightingPerformanceTestGenerated.java
@@ -69,6 +69,11 @@ public class FirHighlightingPerformanceTestGenerated extends AbstractFirHighligh
         runTest("idea/testData/highlighter/Functions.kt");
     }
 
+    @TestMetadata("InaccessibleExpression.kt")
+    public void testInaccessibleExpression() throws Exception {
+        runTest("idea/testData/highlighter/InaccessibleExpression.kt");
+    }
+
     @TestMetadata("InvokeCall.kt")
     public void testInvokeCall() throws Exception {
         runTest("idea/testData/highlighter/InvokeCall.kt");
