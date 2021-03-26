93230d72f53bf74a7d8351bde0c07bcc0e57057a
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-03-26 15:20:22 +0100

    Add build option to control Gradle parallelism.
    Limit workers count, so when running tests parallel, they will not
    compete over CPU usage.
    
    ^KT-45744 In Progress

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/KGPBaseTest.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/KGPBaseTest.kt
@@ -42,7 +42,9 @@ abstract class KGPBaseTest {
         val kotlinVersion: String = KOTLIN_VERSION,
         val warningMode: WarningMode = WarningMode.Fail,
         val configurationCache: Boolean = false,
-        val configurationCacheProblems: BaseGradleIT.ConfigurationCacheProblems = BaseGradleIT.ConfigurationCacheProblems.FAIL
+        val configurationCacheProblems: BaseGradleIT.ConfigurationCacheProblems = BaseGradleIT.ConfigurationCacheProblems.FAIL,
+        val parallel: Boolean = true,
+        val maxWorkers: Int = (Runtime.getRuntime().availableProcessors() / 4 - 1).coerceAtLeast(2),
     ) {
         fun toArguments(
             gradleVersion: GradleVersion
@@ -66,6 +68,12 @@ abstract class KGPBaseTest {
             if (gradleVersion >= GradleVersion.version("6.6.0")) {
                 arguments.add("-Dorg.gradle.unsafe.configuration-cache=$configurationCache")
                 arguments.add("-Dorg.gradle.unsafe.configuration-cache-problems=${configurationCacheProblems.name.toLowerCase()}")
+            }
+            if (parallel) {
+                arguments.add("--parallel")
+                arguments.add("--max-workers=$maxWorkers")
+            } else {
+                arguments.add("--no-parallel")
             }
 
             return arguments.toList()
