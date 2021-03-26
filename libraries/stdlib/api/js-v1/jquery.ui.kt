a5e445492fcd6cf3547bfef977ebd4dd489ee3ca
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2021-03-26 10:48:56 +0300

    Move Bintray js-externals to Space (KTI-528)

--- libraries/stdlib/api/js-v1/jquery.ui.kt
+++ libraries/stdlib/api/js-v1/jquery.ui.kt
@@ -1,29 +1,29 @@
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.accordion(): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.button(): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.buttonset(): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.dialog(): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.dialog(mode: kotlin.String): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.dialog(mode: kotlin.String, param: kotlin.String): kotlin.Any?
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.dialog(mode: kotlin.String, param: kotlin.String, value: kotlin.Any?): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.dialog(params: kotlin.js.Json): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.draggable(params: kotlin.js.Json): jquery.JQuery
 
-@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.")
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.")
 public inline fun jquery.JQuery.selectable(): jquery.JQuery
--- libraries/stdlib/js/src/jquery/ui.kt
+++ libraries/stdlib/js/src/jquery/ui.kt
@@ -11,32 +11,32 @@ package jquery.ui
 import jquery.JQuery
 import kotlin.js.Json
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.buttonset(): JQuery = asDynamic().buttonset()
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.dialog(): JQuery = asDynamic().dialog()
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.dialog(params: Json): JQuery = asDynamic().dialog(params)
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.dialog(mode: String, param: String): Any? = asDynamic().dialog(mode, param)
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.dialog(mode: String): JQuery = asDynamic().dialog(mode)
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.dialog(mode: String, param: String, value: Any?): JQuery = asDynamic().dialog(mode, param, value)
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.button(): JQuery = asDynamic().button()
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.accordion(): JQuery = asDynamic().accordion()
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.draggable(params: Json): JQuery = asDynamic().draggable(params)
 
-@Deprecated("Use declarations from 'https://bintray.com/kotlin/js-externals/kotlin-js-jquery' package instead.", level = DeprecationLevel.ERROR)
+@Deprecated("Use declarations from 'https://maven.pkg.jetbrains.space/kotlin/p/kotlin/js-externals/kotlin/js/externals/kotlin-js-jquery/' package instead.", level = DeprecationLevel.ERROR)
 public inline fun JQuery.selectable(): JQuery = asDynamic().selectable()
