051e2b0869d5a5aeee3db9522bb9b167124a25f2
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-03-29 11:13:13 +0300

    [FIR] Update diagnostic test forgotten in dd20dd98

--- compiler/testData/diagnostics/testsWithStdLib/native/nonFunction.fir.kt
+++
@@ -1,9 +0,0 @@
-<!WRONG_MODIFIER_TARGET!>external<!> class A
-
-<!WRONG_MODIFIER_TARGET!>external<!> val foo: Int = 23
-
-class B {
-    <!WRONG_MODIFIER_TARGET!>external<!> class A
-
-    <!WRONG_MODIFIER_TARGET!>external<!> val foo: Int = 23
-}
