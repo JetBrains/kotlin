c98746364b3a10eba2cdc4fd927017f233185d5c
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-03-29 23:13:54 +0700

    [CHERRY PICKED FROM IJ] [jps] convert KotlinCommonJpsModelSerializerExtension to Kotlin
    GitOrigin-RevId: 437f6d66690434a9853d544015d93b17448f8f7c
    Original commit: https://github.com/JetBrains/intellij-community/commit/2b8032aafcf561c7d834bb857eb5b3d32eff8317

--- idea/idea-jps-common/src/org/jetbrains/kotlin/config/KotlinCommonJpsModelSerializerExtension.kt
+++ idea/idea-jps-common/src/org/jetbrains/kotlin/config/KotlinCommonJpsModelSerializerExtension.kt
@@ -1,26 +1,17 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.config
 
-package org.jetbrains.kotlin.config;
+import org.jetbrains.jps.model.serialization.JpsModelSerializerExtension
+import org.jetbrains.jps.model.serialization.module.JpsModuleSourceRootPropertiesSerializer
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.jps.model.serialization.JpsModelSerializerExtension;
-import org.jetbrains.jps.model.serialization.module.JpsModuleSourceRootPropertiesSerializer;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class KotlinCommonJpsModelSerializerExtension extends JpsModelSerializerExtension {
-    @NotNull
-    @Override
-    public List<? extends JpsModuleSourceRootPropertiesSerializer<?>> getModuleSourceRootPropertiesSerializers() {
-        return Arrays.asList(
-                KotlinSourceRootPropertiesSerializer.Source.INSTANCE,
-                KotlinSourceRootPropertiesSerializer.TestSource.INSTANCE,
-                KotlinResourceRootPropertiesSerializer.Resource.INSTANCE,
-                KotlinResourceRootPropertiesSerializer.TestResource.INSTANCE
-        );
-    }
+open class KotlinCommonJpsModelSerializerExtension : JpsModelSerializerExtension() {
+    override fun getModuleSourceRootPropertiesSerializers(): List<JpsModuleSourceRootPropertiesSerializer<*>> = listOf(
+        KotlinSourceRootPropertiesSerializer.Source,
+        KotlinSourceRootPropertiesSerializer.TestSource,
+        KotlinResourceRootPropertiesSerializer.Resource,
+        KotlinResourceRootPropertiesSerializer.TestResource
+    )
 }
