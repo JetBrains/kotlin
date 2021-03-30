f4117e91d175cf0464f387b74e7a80b00345170f
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-03-30 13:39:04 +0700

    [CHERRY PICKED FROM IJ] [jps] cleanup deprecated usages of `KotlinFacetSettings.isHmppEnabled`
    GitOrigin-RevId: 6b20c99f5e96989c47ca3b921bd68916c8e14a89
    Original commit: https://github.com/JetBrains/intellij-community/commit/6edf1a0fb3acc036f015c9263c5289f83385e9aa

--- idea/idea-jps-common/src/org/jetbrains/kotlin/config/facetSerialization.kt
+++ idea/idea-jps-common/src/org/jetbrains/kotlin/config/facetSerialization.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -322,7 +322,7 @@ private fun KotlinFacetSettings.writeLatestConfig(element: Element) {
     if (externalProjectId.isNotEmpty()) {
         element.setAttribute("externalProjectId", externalProjectId)
     }
-    if (isHmppEnabled) {
+    if (mppVersion.isHmpp) {
         element.setAttribute("isHmppProject", mppVersion.isHmpp.toString())
     }
     if (externalSystemRunTasks.isNotEmpty()) {
