5f73a20a2346c631efdd5682d694ae2b3c3b3c7c
Ivan Gavrilovic <gavra@google.com>
2021-03-30 11:34:01 +0100

    Make init script for KotlinMPP tests compatible with Gradle configuration cache
    Avoid using `beforeTask` API and switch to ones that are
    compatible with Gradle configuration cache.

--- idea/resources/org/jetbrains/kotlin/idea/gradle/testing/addKotlinMppTestListener.groovy
+++ idea/resources/org/jetbrains/kotlin/idea/gradle/testing/addKotlinMppTestListener.groovy
@@ -1,18 +1,20 @@
-gradle.taskGraph.beforeTask { task ->
-    def taskSuperClass = task.class
-    while (taskSuperClass != Object.class) {
-        if (taskSuperClass.canonicalName == "org.jetbrains.kotlin.gradle.tasks.KotlinTest") {
-            try {
-                KotlinMppTestLogger.logTestReportLocation(task.reports?.html?.entryPoint?.path)
-                KotlinMppTestLogger.configureTestEventLogging(task)
-                task.testLogging.showStandardStreams = false
+gradle.taskGraph.whenReady { taskGraph ->
+    taskGraph.allTasks.each { task ->
+        def taskSuperClass = task.class
+        while (taskSuperClass != Object.class) {
+            if (taskSuperClass.canonicalName == "org.jetbrains.kotlin.gradle.tasks.KotlinTest") {
+                try {
+                    KotlinMppTestLogger.logTestReportLocation(task.reports?.html?.entryPoint?.path)
+                    KotlinMppTestLogger.configureTestEventLogging(task)
+                    task.testLogging.showStandardStreams = false
+                }
+                catch (all) {
+                    logger.error("", all)
+                }
+                return
+            } else {
+                taskSuperClass = taskSuperClass.superclass
             }
-            catch (all) {
-                logger.error("", all)
-            }
-            return
-        } else {
-            taskSuperClass = taskSuperClass.superclass
         }
     }
 }
