0d3969597c7fd068fe16f513ed2483834c2250a9
Jinseong Jeon <jsjeon@google.com>
2021-04-01 22:58:58 -0700

    FIR: more accurate nullability of intersection of flexible types

--- compiler/testData/diagnostics/tests/smartCasts/varnotnull/toFlexibleType.fir.kt
+++ compiler/testData/diagnostics/tests/smartCasts/varnotnull/toFlexibleType.fir.kt
@@ -1,7 +1,12 @@
 // FILE: J.java
+import org.jetbrains.annotations.*;
+import java.util.List;
 
 class J {
     static String foo() { return "abc"; }
+
+    @NotNull
+    static List<String> bar() { return new List<String>(); }
 }
 
 // FILE: test.kt
@@ -11,6 +16,10 @@ fun bar() {
     v = J.foo()
     v<!UNSAFE_CALL!>.<!>length
     <!INAPPLICABLE_CANDIDATE!>gav<!>(v)
+
+    var l: List<String>?
+    l = J.bar()
+    l.isEmpty()
 }
 
 fun gav(v: String) = v
--- compiler/testData/diagnostics/tests/smartCasts/varnotnull/toFlexibleType.kt
+++ compiler/testData/diagnostics/tests/smartCasts/varnotnull/toFlexibleType.kt
@@ -1,7 +1,12 @@
 // FILE: J.java
+import org.jetbrains.annotations.*;
+import java.util.List;
 
 class J {
     static String foo() { return "abc"; }
+
+    @NotNull
+    static List<String> bar() { return new List<String>(); }
 }
 
 // FILE: test.kt
@@ -11,6 +16,10 @@ fun bar() {
     v = J.foo()
     <!DEBUG_INFO_SMARTCAST!>v<!>.length
     gav(<!DEBUG_INFO_SMARTCAST!>v<!>)
+
+    var l: List<String>?
+    l = J.bar()
+    <!DEBUG_INFO_SMARTCAST!>l<!>.isEmpty()
 }
 
 fun gav(v: String) = v
