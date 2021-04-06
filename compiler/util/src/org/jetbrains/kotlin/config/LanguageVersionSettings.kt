59372fd15f2761613ccacb182b06fde5ed0b44c1
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-04-06 18:31:03 +0300

    Enable ProhibitUsingNullableTypeParameterAgainstNotNullAnnotated since 1.6
    ^KT-36770 Fixed

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -170,9 +170,6 @@ enum class LanguageFeature(
     // Temporarily disabled, see KT-27084/KT-22379
     SoundSmartcastFromLoopConditionForLoopAssignedVariables(sinceVersion = null, kind = BUG_FIX),
 
-    // Looks like we can't enable it until KT-26245 is fixed because otherwise some use cases become broken because of overrides
-    ProhibitUsingNullableTypeParameterAgainstNotNullAnnotated(sinceVersion = null, kind = BUG_FIX),
-
     // Experimental features
 
     Coroutines(
@@ -207,6 +204,7 @@ enum class LanguageFeature(
 
     // 1.6
     DefinitelyNotNullTypeParameters(sinceVersion = KOTLIN_1_6),
+    ProhibitUsingNullableTypeParameterAgainstNotNullAnnotated(sinceVersion = KOTLIN_1_6, kind = BUG_FIX),
     ;
 
     val presentableName: String
