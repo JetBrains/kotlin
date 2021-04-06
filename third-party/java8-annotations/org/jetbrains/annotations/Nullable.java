eccd2c389ed30c151c58f544e2fa8224dada3823
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-04-06 16:28:25 +0300

    Update some third-party annotations
    (cherry picked from commit 89270399db31e534fc0b2642453ba945535244cb)

--- third-party/java8-annotations/org/jetbrains/annotations/Nullable.java
+++ third-party/java8-annotations/org/jetbrains/annotations/Nullable.java
@@ -27,5 +27,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
 public @interface Nullable {
-    @NonNls String value() default "";
+    String value() default "";
 }
