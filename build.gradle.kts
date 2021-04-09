8f80deefc3929523cfc61e57442600424f1998cf
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2021-04-09 10:51:31 +0300

    Update Kotlin/Native: 1.5-rc1-17

--- build.gradle.kts
+++ build.gradle.kts
@@ -187,7 +187,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.5-eap-4"
+    extra["versions.kotlin-native"] = "1.5-rc1-17"
 }
 
 val intellijUltimateEnabled by extra(project.kotlinBuildProperties.intellijUltimateEnabled)
