481cf3b21348c0ea30e799169cac873b73c92740
Marcin Aman <marcin.aman@gmail.com>
2021-04-14 21:51:16 +0200

    [KDM] Introduce binary compatibility plugin
    See https://github.com/Kotlin/dokka/pull/1774
    
    * Introduce binary compatibility plugin
    
    * Update diffs
    Original commit: https://github.com/Kotlin/dokka/commit/7bfab097f35f86bafbc2feff23e6658ad17da2ae

--- libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.4.0")
 }
