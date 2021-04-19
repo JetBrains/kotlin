08dca2e72dc4c63e5f8b4af2d47012422123711d
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-04-19 03:02:20 +0300

    more review fixes

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/InteropUtils.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/InteropUtils.kt
@@ -117,7 +117,17 @@ internal class InteropBuiltIns(builtIns: KonanBuiltIns) {
 
     val CreateNSStringFromKString = packageScope.getContributedFunctions("CreateNSStringFromKString").single()
     val nativeHeap = packageScope.getContributedClass("nativeHeap")
-    val interopGetPtr = packageScope.getContributedVariables("ptr").single().getter!!
+    val cPointed = packageScope.getContributedClass("CPointed")
+    val interopGetPtr = packageScope.getContributedVariables("ptr").single {
+        val singleTypeParameter = it.typeParameters.singleOrNull()
+        val singleTypeParameterUpperBound = singleTypeParameter?.upperBounds?.singleOrNull()
+        val extensionReceiverParameter = it.extensionReceiverParameter
+
+        singleTypeParameterUpperBound != null &&
+        extensionReceiverParameter != null &&
+        TypeUtils.getClassDescriptor(singleTypeParameterUpperBound) == cPointed &&
+        extensionReceiverParameter.type == singleTypeParameter.defaultType
+    }.getter!!
 }
 
 private fun MemberScope.getContributedVariables(name: String) =
