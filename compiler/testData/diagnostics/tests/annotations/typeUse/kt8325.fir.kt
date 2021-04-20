6911801ebfe61f4e5bbdab4988898b2fde10c567
Mark Punzalan <markpun@google.com>
2021-04-20 08:29:14 +0000

    FIR: Remove duplicated diagnostics on annotations on types.

--- compiler/testData/diagnostics/tests/annotations/typeUse/kt8325.fir.kt
+++
@@ -1,6 +0,0 @@
-// !LANGUAGE: +ProperCheckAnnotationsTargetInTypeUsePositions
-// ISSUE: KT-8325
-
-fun foo() {
-    object : @<!UNRESOLVED_REFERENCE, UNRESOLVED_REFERENCE!>__UNRESOLVED__<!> Any() {}
-}
