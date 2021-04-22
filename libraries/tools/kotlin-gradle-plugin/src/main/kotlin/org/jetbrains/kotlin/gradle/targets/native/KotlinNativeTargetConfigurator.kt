c8ef0715b9c68c7d92a085a62b6d0c05723a9afe
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2021-04-22 12:55:26 +0300

    Register "assembleAppleFrameworkForXcode" and "embedAndSignAppleFrameworkForXcode" tasks when gradle is run from Xcode.
    #KT-27240

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/KotlinNativeTargetConfigurator.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/KotlinNativeTargetConfigurator.kt
@@ -29,6 +29,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.Companion.MAIN_COMPI
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation.Companion.TEST_COMPILATION_NAME
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.Companion.KOTLIN_NATIVE_IGNORE_INCORRECT_DEPENDENCIES
 import org.jetbrains.kotlin.gradle.plugin.mpp.*
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.registerAssembleAppleFrameworkTask
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.*
 import org.jetbrains.kotlin.gradle.targets.metadata.isKotlinGranularMetadataEnabled
 import org.jetbrains.kotlin.gradle.targets.native.*
@@ -241,6 +242,10 @@ open class KotlinNativeTargetConfigurator<T : KotlinNativeTarget> : AbstractKotl
         configureFrameworkExport(target)
         configureCInterops(target)
 
+        if (target.konanTarget.family.isAppleFamily) {
+            registerAssembleAppleFrameworkTasks(target)
+        }
+
         if (PropertiesProvider(target.project).ignoreIncorrectNativeDependencies != true) {
             warnAboutIncorrectDependencies(target)
         }
@@ -352,6 +357,13 @@ open class KotlinNativeTargetConfigurator<T : KotlinNativeTarget> : AbstractKotl
                 attributes.attribute(USAGE_ATTRIBUTE, KotlinUsages.consumerApiUsage(target))
                 description = "Dependenceis to be exported in framework ${framework.name} for target ${target.targetName}"
             }
+        }
+    }
+
+    private fun registerAssembleAppleFrameworkTasks(target: KotlinNativeTarget) {
+        val project = target.project
+        target.binaries.withType(Framework::class.java).all { framework ->
+            project.registerAssembleAppleFrameworkTask(framework)
         }
     }
 
