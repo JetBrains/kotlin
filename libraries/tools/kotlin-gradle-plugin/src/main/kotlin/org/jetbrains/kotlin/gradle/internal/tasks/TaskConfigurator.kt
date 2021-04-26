571c9c10d4ef3c0918c7d244f4cb6d109ac1b6f6
Ivan Gavrilovic <gavra@google.com>
2021-04-26 17:20:00 +0100

    [Gradle] Update compile tasks to use properties and clean up code
    Update AbstractKotlinCompile, KotlinCompile, and Kotlin2JsCompile to
    use Gradle properties, and introduce Configurator classes that are
    using configuration-time data to configure task. Also, introduce
    TaskConfigurator interface that should be implemented by classes that
    are used to configure tasks.

---
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/internal/tasks/TaskConfigurator.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.internal.tasks
+
+import org.gradle.api.Task
+
+/** Classes that participate in task configuration should implement this interface. */
+interface TaskConfigurator<T : Task> {
+    fun configure(task: T)
+}
