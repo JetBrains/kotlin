65eb1507c6d2190c8886407f145b82e7b124f954
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-04-28 17:23:32 +0700

    [CHERRY PICKED FROM IJ] [kotlin] migrate some io operations to nio
    FLEET-T-2032
    
    GitOrigin-RevId: a616d57c763cda61386ddb754ea45ee075764eaf
    Original commit: https://github.com/JetBrains/intellij-community/commit/99b6620c424773a42f44fdf398c98fbaa3b55b5c

--- jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/lookups.kt
+++ jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/lookups.kt
@@ -1,17 +1,16 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.jps.incremental
 
-import java.io.File
+import java.nio.file.Path
 
-private val DATA_CONTAINER_VERSION_FILE_NAME = "data-container-format-version.txt"
-private val DATA_CONTAINER_VERSION = 5
+private const val DATA_CONTAINER_VERSION_FILE_NAME = "data-container-format-version.txt"
+private const val DATA_CONTAINER_VERSION = 5
 
-fun lookupsCacheVersionManager(dataRoot: File, isEnabled: Boolean) =
-    CacheVersionManager(
-        File(dataRoot, DATA_CONTAINER_VERSION_FILE_NAME),
-        if (isEnabled) DATA_CONTAINER_VERSION else null
-    )
+fun lookupsCacheVersionManager(dataRoot: Path, isEnabled: Boolean) = CacheVersionManager(
+    dataRoot.resolve(DATA_CONTAINER_VERSION_FILE_NAME),
+    if (isEnabled) DATA_CONTAINER_VERSION else null
+)
