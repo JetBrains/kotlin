0839b6a62bd371ef071373cd87ff04a4652cb388
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-04-28 17:23:32 +0700

    [CHERRY PICKED FROM IJ] [kotlin] migrate some io operations to nio
    FLEET-T-2032
    
    GitOrigin-RevId: a616d57c763cda61386ddb754ea45ee075764eaf
    Original commit: https://github.com/JetBrains/intellij-community/commit/99b6620c424773a42f44fdf398c98fbaa3b55b5c

--- jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/CacheVersionManager.kt
+++ jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/CacheVersionManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -10,6 +10,8 @@ import org.jetbrains.kotlin.load.kotlin.JvmBytecodeBinaryVersion
 import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmMetadataVersion
 import java.io.File
 import java.io.IOException
+import java.nio.file.Path
+import kotlin.io.path.*
 
 /**
  * Manages files with actual version [loadActual] and provides expected version [expected].
@@ -18,7 +20,7 @@ import java.io.IOException
  * Based on that status system may perform required actions (i.e. rebuild something, clearing caches, etc...).
  */
 class CacheVersionManager(
-    private val versionFile: File,
+    private val versionFile: Path,
     expectedOwnVersion: Int?
 ) : CacheAttributesManager<CacheVersion> {
     override val expected: CacheVersion? =
@@ -26,7 +28,7 @@ class CacheVersionManager(
         else CacheVersion(expectedOwnVersion, JvmBytecodeBinaryVersion.INSTANCE, JvmMetadataVersion.INSTANCE)
 
     override fun loadActual(): CacheVersion? =
-        if (!versionFile.exists()) null
+        if (versionFile.notExists()) null
         else try {
             CacheVersion(versionFile.readText().toInt())
         } catch (e: NumberFormatException) {
@@ -36,16 +38,16 @@ class CacheVersionManager(
         }
 
     override fun writeVersion(values: CacheVersion?) {
-        if (values == null) versionFile.delete()
+        if (values == null) versionFile.deleteIfExists()
         else {
-            versionFile.parentFile.mkdirs()
+            versionFile.parent.createDirectories()
             versionFile.writeText(values.intValue.toString())
         }
     }
 
     @get:TestOnly
     val versionFileForTesting: File
-        get() = versionFile
+        get() = versionFile.toFile()
 }
 
 fun CacheVersion(own: Int, bytecode: JvmBytecodeBinaryVersion, metadata: JvmMetadataVersion): CacheVersion {
--- jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/CompositeLookupsCacheAttributes.kt
+++ jps/jps-plugin/src/org/jetbrains/kotlin/jps/incremental/CompositeLookupsCacheAttributes.kt
@@ -1,13 +1,14 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.jps.incremental
 
 import org.jetbrains.annotations.TestOnly
-import java.io.File
 import java.io.IOException
+import java.nio.file.Path
+import kotlin.io.path.*
 
 /**
  * Attributes manager for global lookups cache that may contain lookups for several compilers (jvm, js).
@@ -16,7 +17,7 @@ import java.io.IOException
  * TODO(1.2.80): got rid of shared lookup cache, replace with individual lookup cache for each compiler
  */
 class CompositeLookupsCacheAttributesManager(
-    rootPath: File,
+    rootPath: Path,
     expectedComponents: Set<String>
 ) : CacheAttributesManager<CompositeLookupsCacheAttributes> {
     private val versionManager = lookupsCacheVersionManager(
@@ -24,7 +25,7 @@ class CompositeLookupsCacheAttributesManager(
         expectedComponents.isNotEmpty()
     )
 
-    private val actualComponentsFile = File(rootPath, "components.txt")
+    private val actualComponentsFile = rootPath.resolve("components.txt")
 
     override val expected: CompositeLookupsCacheAttributes? =
         if (expectedComponents.isEmpty()) null
@@ -33,7 +34,7 @@ class CompositeLookupsCacheAttributesManager(
     override fun loadActual(): CompositeLookupsCacheAttributes? {
         val version = versionManager.loadActual() ?: return null
 
-        if (!actualComponentsFile.exists()) return null
+        if (actualComponentsFile.notExists()) return null
 
         val components = try {
             actualComponentsFile.readLines().toSet()
@@ -47,11 +48,11 @@ class CompositeLookupsCacheAttributesManager(
     override fun writeVersion(values: CompositeLookupsCacheAttributes?) {
         if (values == null) {
             versionManager.writeVersion(null)
-            actualComponentsFile.delete()
+            actualComponentsFile.deleteIfExists()
         } else {
             versionManager.writeVersion(CacheVersion(values.version))
 
-            actualComponentsFile.parentFile.mkdirs()
+            actualComponentsFile.parent.createDirectories()
             actualComponentsFile.writeText(values.components.joinToString("\n"))
         }
     }
