9e084a7256064701c455f81496300cd8e7d0ef67
Anton Lakotka <anton.lakotka@jetbrains.com>
2021-04-28 12:34:53 +0200

    Fix overload call of project.files in abstractVariants.kt

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/abstractVariants.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/abstractVariants.kt
@@ -98,12 +98,12 @@ abstract class KotlinGradleVariantWithRuntimeInternal(
 
     override lateinit var runtimeDependencyFiles: FileCollection
 
-    override val runtimeFiles: ConfigurableFileCollection by lazy {
-        project.files(
+    override val runtimeFiles: ConfigurableFileCollection = project.files(
+        listOf(
             { compilationOutputs.allOutputs },
             { runtimeDependencyFiles }
         )
-    }
+    )
 
     // TODO generalize exposing outputs: what if a variant has more than one such configurations or none?
     override val runtimeElementsConfigurationName: String
