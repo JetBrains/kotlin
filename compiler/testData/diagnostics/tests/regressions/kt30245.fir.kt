601500fd99372adae870085cdf93d1233c397196
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-04-28 12:17:29 +0300

    FIR: Update test data (extension function type -> function type)
    See KT-46371

--- compiler/testData/diagnostics/tests/regressions/kt30245.fir.kt
+++ compiler/testData/diagnostics/tests/regressions/kt30245.fir.kt
@@ -7,8 +7,8 @@ fun <K> id(x: K): K = x
 
 fun test() {
     val f00: Sample.() -> Unit = id { val a = 1 }
-    val f01: Sample.() -> Unit = id { s: Sample -> }
-    val f02: Sample.() -> Unit = id<Sample.() -> Unit> { s: Sample -> }
+    val f01: Sample.() -> Unit = id <!TYPE_MISMATCH!>{ s: Sample -> }<!>
+    val f02: Sample.() -> Unit = id<Sample.() -> Unit> <!TYPE_MISMATCH!>{ s: Sample -> }<!>
 }
 
 enum class E { VALUE }
@@ -44,11 +44,11 @@ fun test1() { // to extension lambda 0
     val f10 = W1(fun Int.(): Int = this) // oi+ ni+
     val g10: E0 = id(fun Int.(): Int = this) // oi+ ni+
 
-    val w11 = W1 { i: Int -> <!ARGUMENT_TYPE_MISMATCH!>i<!> } // oi- ni-
-    val i11: E0 = id { i: Int -> i } // o1+ ni+
-    val w12 = W1 { <!CANNOT_INFER_PARAMETER_TYPE!>i<!> -> <!ARGUMENT_TYPE_MISMATCH!>i<!> } // oi- ni-
-    val i12: E0 = id { <!CANNOT_INFER_PARAMETER_TYPE!>i<!> -> i } // oi- ni-
-    val j12 = id<E0> { <!CANNOT_INFER_PARAMETER_TYPE!>i<!> -> i } // oi- ni-
+    val w11 = W1 <!ARGUMENT_TYPE_MISMATCH!>{ i: Int -> i }<!> // oi- ni-
+    val i11: E0 = id <!TYPE_MISMATCH!>{ i: Int -> i }<!> // o1+ ni+
+    val w12 = W1 <!ARGUMENT_TYPE_MISMATCH!>{ <!CANNOT_INFER_PARAMETER_TYPE!>i<!> -> i }<!> // oi- ni-
+    val i12: E0 = id <!TYPE_MISMATCH!>{ <!CANNOT_INFER_PARAMETER_TYPE!>i<!> -> i }<!> // oi- ni-
+    val j12 = id<E0> <!TYPE_MISMATCH!>{ <!CANNOT_INFER_PARAMETER_TYPE!>i<!> -> i }<!> // oi- ni-
 
     // yet unsupported cases - considering lambdas as extension ones unconditionally
 //    val w13 = W1 { it } // this or it: oi- ni-
@@ -83,10 +83,10 @@ fun test2() { // to extension lambda 1
     val i27: E1 = <!INITIALIZER_TYPE_MISMATCH!>when (e) { E.VALUE ->  { s: String -> <!NO_THIS!>this<!> + s.length } }<!> // oi+ ni+
     val i27a: E1 = <!INITIALIZER_TYPE_MISMATCH!>when (e) { E.VALUE ->  { s -> <!NO_THIS!>this<!> + s.<!UNRESOLVED_REFERENCE!>length<!> } }<!> // oi+ ni+
 
-    val w28 = W2 { i: Int, <!CANNOT_INFER_PARAMETER_TYPE!>s<!> -> <!ARGUMENT_TYPE_MISMATCH!>i <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> s.<!UNRESOLVED_REFERENCE!>length<!><!> } // oi- ni-
-    val i28: E1 = id { i: Int, <!CANNOT_INFER_PARAMETER_TYPE!>s<!> -> i <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> s.<!UNRESOLVED_REFERENCE!>length<!> } // oi- ni-
-    val w29 = W2 { i: Int, s: String -> <!ARGUMENT_TYPE_MISMATCH!>i + s.length<!> } // oi- ni-
-    val i29: E1 = id { i: Int, s: String -> i + s.length } // oi+ ni+
+    val w28 = W2 <!ARGUMENT_TYPE_MISMATCH!>{ i: Int, <!CANNOT_INFER_PARAMETER_TYPE!>s<!> -> i <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> s.<!UNRESOLVED_REFERENCE!>length<!> }<!> // oi- ni-
+    val i28: E1 = id <!TYPE_MISMATCH!>{ i: Int, <!CANNOT_INFER_PARAMETER_TYPE!>s<!> -> i <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> s.<!UNRESOLVED_REFERENCE!>length<!> }<!> // oi- ni-
+    val w29 = W2 <!ARGUMENT_TYPE_MISMATCH!>{ i: Int, s: String -> i + s.length }<!> // oi- ni-
+    val i29: E1 = id <!TYPE_MISMATCH!>{ i: Int, s: String -> i + s.length }<!> // oi+ ni+
 
     // yet unsupported cases with ambiguity for the lambda conversion (commented constructors in wrappers above)
 //    val w2a = W2 { i, s -> i + s.length } // overload oi- ni-
