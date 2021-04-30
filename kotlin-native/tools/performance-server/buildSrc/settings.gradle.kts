6ee9a9a6bf77b4ba38eae46c11a03dfd2597b62d
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2021-04-30 07:04:03 +0200

    [build][kotlin-build-gradle-plugin] version 0.0.27
     - reading konanVersion and konanMetaVersion from project properties
    
    (cherry picked from commit 788d3c022aba80c3b8ab7d438e7ac70357e75c00)

--- kotlin-native/tools/performance-server/buildSrc/settings.gradle.kts
+++ kotlin-native/tools/performance-server/buildSrc/settings.gradle.kts
@@ -10,8 +10,9 @@ pluginManagement {
 buildscript {
     repositories {
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-dependencies")
+        mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.26")
+        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.27")
     }
 }
