8a0a481ce775269f92adea0d986a513f3e139448
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2021-05-06 19:11:05 +0300

    [Native] [Interop] Garbage collection capable wrappers for skia interop

--- kotlin-native/Interop/Skia/src/main/kotlin/org/jetbrains/kotlin/native/interop/skia/SkiaStubsBuildingContextImpl.kt
+++ kotlin-native/Interop/Skia/src/main/kotlin/org/jetbrains/kotlin/native/interop/skia/SkiaStubsBuildingContextImpl.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.native.interop.gen.Classifier
 import org.jetbrains.kotlin.native.interop.gen.StubIrContext
 import org.jetbrains.kotlin.native.interop.gen.StubsBuildingContextImpl
 import org.jetbrains.kotlin.native.interop.indexer.StructDecl
+import org.jetbrains.kotlin.native.interop.indexer.StructDef
 
 class SkiaStubsBuildingContextImpl(stubIrContext: StubIrContext) : StubsBuildingContextImpl(stubIrContext) {
     override val declarationMapper = SkiaDeclarationMapperImpl()
@@ -22,5 +23,22 @@ class SkiaStubsBuildingContextImpl(stubIrContext: StubIrContext) : StubsBuilding
             } ?: error("Expected to find a single template arg struct by name: ${struct}")
             return getKotlinClassForPointed(structArgument)
         }
+    }
+
+    override fun isCppClass(spelling: String): Boolean {
+        val decl = nativeIndex.structs.firstOrNull { it.spelling == spelling } ?: return false
+        return decl.def?.kind == StructDef.Kind.CLASS
+    }
+
+    override fun managedWrapperClassifier(cppClassifier: Classifier): Classifier? {
+        if (cppClassifier.pkg != "org.jetbrains.skiko.skia.native") return null
+        // TODO: it'd be nice to check inheritance from SkiaRefCnt or CPlusPlusClass,
+        // but unable to do that at StubType level.
+        if (!(cppClassifier.topLevelName.startsWith("Sk") || cppClassifier.topLevelName.startsWith("Gr"))) return null
+        if (cppClassifier.topLevelName == "SkString") return null
+        // TODO: We only managed C++ classes, not structs for now.
+        if (!isCppClass(cppClassifier.topLevelName)) return null
+
+        return Classifier.topLevel(cppClassifier.pkg, cppClassifier.topLevelName.drop(2))
     }
 }
