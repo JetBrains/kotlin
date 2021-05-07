6c4a500a67c8c307c2a02cd7ec8d6590fc27909d
pyos <pyos@google.com>
2021-05-07 10:44:00 +0200

    JVM_IR: permit `@NoArg` if super constructor is already zero-argument
     #KT-46537 Fixed

--- plugins/noarg/noarg-cli/src/NoArgIrGenerationExtension.kt
+++ plugins/noarg/noarg-cli/src/NoArgIrGenerationExtension.kt
@@ -7,29 +7,19 @@ package org.jetbrains.kotlin.noarg
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
-import org.jetbrains.kotlin.backend.common.lower.SYNTHESIZED_INIT_BLOCK
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.extensions.AnnotationBasedExtension
 import org.jetbrains.kotlin.ir.IrElement
-import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.builders.declarations.buildConstructor
 import org.jetbrains.kotlin.ir.declarations.*
-import org.jetbrains.kotlin.ir.descriptors.IrBuiltIns
 import org.jetbrains.kotlin.ir.descriptors.toIrBasedDescriptor
-import org.jetbrains.kotlin.ir.expressions.IrBlock
-import org.jetbrains.kotlin.ir.expressions.IrGetValue
-import org.jetbrains.kotlin.ir.expressions.IrStatementOrigin
-import org.jetbrains.kotlin.ir.expressions.impl.IrBlockImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrDelegatingConstructorCallImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrGetValueImpl
-import org.jetbrains.kotlin.ir.expressions.impl.IrSetFieldImpl
+import org.jetbrains.kotlin.ir.expressions.impl.*
 import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.getClass
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.psi.KtModifierListOwner
 import org.jetbrains.kotlin.resolve.jvm.annotations.JVM_OVERLOADS_FQ_NAME
-import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
 internal class NoArgIrGenerationExtension(
     private val annotations: List<String>,
@@ -56,7 +46,7 @@ private class NoArgIrTransformer(
 
         if (declaration.kind == ClassKind.CLASS &&
             declaration.isAnnotatedWithNoarg() &&
-            declaration.constructors.none(::isZeroParameterConstructor)
+            declaration.constructors.none { it.isZeroParameterConstructor() }
         ) {
             declaration.declarations.add(getOrGenerateNoArgConstructor(declaration))
         }
@@ -72,7 +62,7 @@ private class NoArgIrTransformer(
         val superConstructor =
             if (superClass.isAnnotatedWithNoarg())
                 getOrGenerateNoArgConstructor(superClass)
-            else superClass.constructors.singleOrNull { it.valueParameters.isEmpty() }
+            else superClass.constructors.singleOrNull { it.isZeroParameterConstructor() }
                 ?: error("No noarg super constructor for ${klass.render()}:\n" + superClass.constructors.joinToString("\n") { it.render() })
 
         context.irFactory.buildConstructor {
@@ -86,9 +76,9 @@ private class NoArgIrTransformer(
                         ctor.startOffset, ctor.endOffset, context.irBuiltIns.unitType,
                         superConstructor.symbol, 0, superConstructor.valueParameters.size
                     ),
-                    if (invokeInitializers)
-                        NoArgInitializersLowering(context.irBuiltIns).createInitializersBlock(ctor)
-                    else null
+                    IrInstanceInitializerCallImpl(
+                        ctor.startOffset, ctor.endOffset, klass.symbol, context.irBuiltIns.unitType
+                    ).takeIf { invokeInitializers }
                 )
             )
         }
@@ -97,50 +87,7 @@ private class NoArgIrTransformer(
     private fun IrClass.isAnnotatedWithNoarg(): Boolean =
         toIrBasedDescriptor().hasSpecialAnnotation(null)
 
-    private fun isZeroParameterConstructor(constructor: IrConstructor): Boolean {
-        val parameters = constructor.valueParameters
-        return parameters.isEmpty() ||
-                (parameters.all { it.defaultValue != null } && (constructor.isPrimary || constructor.hasAnnotation(JVM_OVERLOADS_FQ_NAME)))
-    }
-}
-
-/** Main parts copied from [org.jetbrains.kotlin.backend.common.lower.InitializersLowering]. */
-private class NoArgInitializersLowering(private val builtIns: IrBuiltIns) {
-    fun createInitializersBlock(ctor: IrConstructor): IrBlock {
-        val irClass = ctor.constructedClass
-        return IrBlockImpl(irClass.startOffset, irClass.endOffset, builtIns.unitType, null, extractInitializers(irClass))
-            .deepCopyWithSymbols(ctor)
-    }
-
-    private fun extractInitializers(irClass: IrClass): List<IrStatement> {
-        val result = mutableListOf<IrStatement>()
-        for (declaration in irClass.declarations) {
-            when (declaration) {
-                is IrAnonymousInitializer -> if (!declaration.isStatic) {
-                    result.add(with(declaration) {
-                        IrBlockImpl(startOffset, endOffset, builtIns.unitType, SYNTHESIZED_INIT_BLOCK, body.statements)
-                    })
-                }
-                is IrProperty -> declaration.backingField.let { field ->
-                    if (field != null && !field.isStatic) {
-                        val initializer = field.initializer
-                        // Take all field initializers except those for properties in the primary constructor, for which we have no values
-                        // in the noarg constructor (and thus those properties will be uninitialized).
-                        if (initializer != null && initializer.expression.safeAs<IrGetValue>()?.origin !=
-                            IrStatementOrigin.INITIALIZE_PROPERTY_FROM_PARAMETER
-                        ) {
-                            result.add(with(initializer) {
-                                IrSetFieldImpl(
-                                    startOffset, endOffset, field.symbol,
-                                    IrGetValueImpl(startOffset, endOffset, irClass.thisReceiver!!.type, irClass.thisReceiver!!.symbol),
-                                    expression, builtIns.unitType, IrStatementOrigin.INITIALIZE_FIELD
-                                )
-                            })
-                        }
-                    }
-                }
-            }
-        }
-        return result
-    }
+    // Returns true if this constructor is callable with no arguments by JVM rules, i.e. will have descriptor `()V`.
+    private fun IrConstructor.isZeroParameterConstructor(): Boolean =
+        valueParameters.all { it.defaultValue != null } && (valueParameters.isEmpty() || isPrimary || hasAnnotation(JVM_OVERLOADS_FQ_NAME))
 }
