d5d6736e672ced8a9578e3a583a48d272b63abff
pyos <pyos@google.com>
2021-05-10 13:12:02 +0200

    PSI2IR/FIR2IR: do not approximate T!! before translation
    This leads to weird effects when it's in a contravariant position,
    because it's approximated by Nothing.

--- compiler/testData/codegen/box/callableReference/function/argumentTypesNoinline.kt
+++ compiler/testData/codegen/box/callableReference/function/argumentTypesNoinline.kt
@@ -1,13 +1,11 @@
-// IGNORE_BACKEND: JVM_IR
-// IGNORE_BACKEND_FIR: JVM_IR
 // DONT_TARGET_EXACT_BACKEND: WASM
-// WASM_MUTE_REASON: CALLABLE_REFERENCES_FAIL
+// WASM_MUTE_REASON: BINDING_RECEIVERS
 fun <T> id(x: T): T = x
 fun <T> String.extId(x: T): T = x
 
 fun <T, R> T.myLet(block: (T) -> R): R = block(this)
 
-fun <T> foo(value: T?): T? = value?.myLet(::id) // KFunction1<Nothing, T>
+fun <T> foo(value: T?): T? = value?.myLet(::id) // ::id = KFunction1<T!!, T!!>
 fun <T> bar(value: T?): T? = value?.myLet(""::extId)
 
-fun box() = foo("O")!! + foo("K")!!
+fun box() = foo("O")!! + bar("K")!!
