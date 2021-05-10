cdfe8b9e9491a7f6771eb178b814a2f77d60a71a
Mads Ager <ager@google.com>
2021-05-10 15:57:31 +0200

    [JVM IR] Copy attributes for JVM_STATIC_WRAPPERS.
    (cherry picked from commit d397efb2bd81392ccb995e7c12b05d40d6c94bcf)

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/JvmCachedDeclarations.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/JvmCachedDeclarations.kt
@@ -182,6 +182,7 @@ class JvmCachedDeclarations(
             isSuspend = target.isSuspend
         }.apply proxy@{
             parent = this@makeProxy
+            copyAttributes(target)
             copyTypeParametersFrom(target)
             copyAnnotationsFrom(target)
             if (!isStatic) {
