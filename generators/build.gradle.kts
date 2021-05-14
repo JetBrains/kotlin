c225c292e4edf55a8c24fd812fd5850ef6c57f64
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-05-14 17:39:35 +0200

    GenerateTests: extract IDE part into separate module
    It's refactoring-preparation commit. In later commits I will add possibility
    to disable IDE modules with flags in gradle build (first step towards
    removing IDE modules from kotlin repo). Thus, we need to extract IDE tests
    generator in separate module for being possible to disable it with flags

--- generators/build.gradle.kts
+++ generators/build.gradle.kts
@@ -44,37 +44,17 @@ dependencies {
     testCompile(interpreterSourceSet.output)
 
     testCompile(projectTests(":compiler:cli"))
-    testCompile(projectTests(":idea:idea-maven"))
-    testCompile(projectTests(":idea:idea-fir"))
-    testCompile(projectTests(":idea:idea-fir-performance-tests"))
-    testCompile(projectTests(":idea:idea-frontend-fir"))
-    testCompile(projectTests(":idea:idea-frontend-fir:idea-fir-low-level-api"))
-    testCompile(projectTests(":j2k"))
-    testCompile(projectTests(":nj2k"))
-    if (Ide.IJ()) {
-        testCompile(projectTests(":libraries:tools:new-project-wizard:new-project-wizard-cli"))
-        testCompile(projectTests(":idea:idea-new-project-wizard"))
-    }
-    testCompile(projectTests(":idea:idea-android"))
-    testCompile(projectTests(":idea:performanceTests"))
-    testCompile(projectTests(":idea:scripting-support"))
-    testCompile(projectTests(":jps-plugin"))
     testCompile(projectTests(":plugins:jvm-abi-gen"))
     testCompile(projectTests(":plugins:android-extensions-compiler"))
-    testCompile(projectTests(":plugins:android-extensions-ide"))
     testCompile(projectTests(":plugins:parcelize:parcelize-compiler"))
-    testCompile(projectTests(":plugins:parcelize:parcelize-ide"))
     testCompile(projectTests(":kotlin-annotation-processing"))
     testCompile(projectTests(":kotlin-annotation-processing-cli"))
     testCompile(projectTests(":kotlin-allopen-compiler-plugin"))
     testCompile(projectTests(":kotlin-noarg-compiler-plugin"))
     testCompile(projectTests(":kotlin-sam-with-receiver-compiler-plugin"))
     testCompile(projectTests(":kotlinx-serialization-compiler-plugin"))
-    testCompile(projectTests(":kotlinx-serialization-ide-plugin"))
     testCompile(projectTests(":plugins:fir:fir-plugin-prototype"))
-    testCompile(projectTests(":idea:jvm-debugger:jvm-debugger-test"))
     testCompile(projectTests(":generators:test-generator"))
-    testCompile(projectTests(":idea"))
     testCompileOnly(project(":kotlin-reflect-api"))
     testRuntime(intellijDep()) { includeJars("idea_rt") }
     testRuntime(project(":kotlin-reflect"))
@@ -90,7 +70,11 @@ projectTest(parallel = true) {
     workingDir = rootDir
 }
 
-val generateTests by generator("org.jetbrains.kotlin.generators.tests.GenerateTestsKt")
+val generateTests by generator("org.jetbrains.kotlin.generators.tests.GenerateTestsKt") {
+    if (kotlinBuildProperties.getOrNull("attachedIntellijVersion") == null) {
+        dependsOn(":generators:idea-generator:generateIdeaTests")
+    }
+}
 
 val generateProtoBuf by generator("org.jetbrains.kotlin.generators.protobuf.GenerateProtoBufKt")
 val generateProtoBufCompare by generator("org.jetbrains.kotlin.generators.protobuf.GenerateProtoBufCompare")
