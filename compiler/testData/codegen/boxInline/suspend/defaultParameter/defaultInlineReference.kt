7eedcf75f9a0651ae5b32de7cba32b47c4db1406
pyos <pyos@google.com>
2021-05-17 12:55:18 +0200

    Add tests for default inline suspend lambdas
    Broken on JVM and JVM_IR

---
+++ compiler/testData/codegen/boxInline/suspend/defaultParameter/defaultInlineReference.kt
@@ -0,0 +1,26 @@
+// SKIP_INLINE_CHECK_IN: bar$default
+// WITH_RUNTIME
+// WITH_COROUTINES
+// IGNORE_BACKEND: JVM, JVM_IR
+// IGNORE_BACKEND_FIR: JVM_IR
+// IGNORE_BACKEND_MULTI_MODULE: JVM, JVM_IR, JVM_MULTI_MODULE_OLD_AGAINST_IR, JVM_MULTI_MODULE_IR_AGAINST_OLD
+// FILE: 1.kt
+package test
+
+suspend fun foo(x: String) = x
+
+inline fun bar(crossinline block: suspend (String) -> String = ::foo): suspend () -> String =
+    { block("OK") }
+
+// FILE: 2.kt
+import test.*
+import helpers.*
+import kotlin.coroutines.*
+
+fun box(): String {
+    var result = "fail"
+    suspend {
+        result = bar()()
+    }.startCoroutine(EmptyContinuation)
+    return result
+}
