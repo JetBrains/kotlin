7f33c9633ba5d71ab1726043439e308380e153eb
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2021-05-17 16:53:38 +0300

    ~ Don't generate nested classes at codegen

--- js/js.tests/build.gradle.kts
+++ js/js.tests/build.gradle.kts
@@ -266,7 +266,7 @@ projectTest(parallel = true) {
 
 projectTest("jsTest", true) {
     // PIR temporary disabled
-    systemProperty("kotlin.js.ir.pir", "false")
+    systemProperty("kotlin.js.ir.pir", "true")
     setUpJsBoxTests(jsEnabled = true, jsIrEnabled = false)
 }
 
