f0a6d9f33f6be57408bb12ad339b10abac287724
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2021-05-18 15:28:39 +0300

    VFS: reset KOTLIN_IS_COMPILED_FILE_ATTRIBUTE
    This commit resets mentioned file attribute from Kotlin binary files.
    It finalizes the fix 929b6dd9 - users might have mistakenly set 'false'
    value left on their file systems.

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/IDEKotlinBinaryClassCache.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/IDEKotlinBinaryClassCache.kt
@@ -105,7 +105,7 @@ class IDEKotlinBinaryClassCache {
         )
 
     private val KOTLIN_IS_COMPILED_FILE_ATTRIBUTE: String = "kotlin-is-binary-compiled".apply {
-        ServiceManager.getService(FileAttributeService::class.java)?.register(this, 1)
+        ServiceManager.getService(FileAttributeService::class.java)?.register(this, 2)
     }
 
     private val KOTLIN_BINARY_DATA_KEY = Key.create<SoftReference<KotlinBinaryData>>(KOTLIN_IS_COMPILED_FILE_ATTRIBUTE)
