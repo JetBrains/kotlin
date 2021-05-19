cc2d7340dc2894741ee27fc3a8bfb47870fe0608
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2021-05-19 16:38:13 +0300

    Implement simple test system to check ir interpreter correctness

---
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/handlers/IrInterpreterBackendHandler.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.backend.handlers
+
+import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.declarations.IrField
+import org.jetbrains.kotlin.ir.declarations.IrFile
+import org.jetbrains.kotlin.ir.expressions.*
+import org.jetbrains.kotlin.ir.expressions.impl.IrCallImpl
+import org.jetbrains.kotlin.ir.interpreter.IrInterpreter
+import org.jetbrains.kotlin.ir.interpreter.checker.IrCompileTimeChecker
+import org.jetbrains.kotlin.ir.util.copyTypeAndValueArgumentsFrom
+import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import org.jetbrains.kotlin.test.backend.ir.IrBackendInput
+import org.jetbrains.kotlin.test.model.TestFile
+import org.jetbrains.kotlin.test.model.TestModule
+import org.jetbrains.kotlin.test.services.GlobalMetadataInfoHandler
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.globalMetadataInfoHandler
+
+open class IrInterpreterBackendHandler(testServices: TestServices) : AbstractIrHandler(testServices) {
+    private val globalMetadataInfoHandler = testServices.globalMetadataInfoHandler
+
+    override fun processAfterAllModules(someAssertionWasFailed: Boolean) {}
+
+    override fun processModule(module: TestModule, info: IrBackendInput) {
+        val moduleFragment = info.backendInput.irModuleFragment
+        val evaluator = Evaluator(IrInterpreter(moduleFragment.irBuiltins), globalMetadataInfoHandler)
+        for ((irFile, testFile) in moduleFragment.files.zip(module.files)) {
+            if (testFile.isAdditional) continue
+            evaluator.evaluate(irFile, testFile)
+        }
+    }
+}
+
+private class Evaluator(private val interpreter: IrInterpreter, private val globalMetadataInfoHandler: GlobalMetadataInfoHandler) {
+    fun evaluate(irFile: IrFile, testFile: TestFile) {
+        object : IrElementTransformerVoid() {
+            private fun IrExpression.report(original: IrExpression): IrExpression {
+                if (this == original) return this
+                val isError = this is IrErrorExpression
+                val message = when (this) {
+                    is IrConst<*> -> this.value.toString()
+                    is IrErrorExpression -> this.description
+                    else -> TODO("unsupported type ${this::class.java}")
+                }
+                val metaInfo = IrInterpreterCodeMetaInfo(this.startOffset, this.endOffset, message, isError)
+                globalMetadataInfoHandler.addMetadataInfosForFile(testFile, listOf(metaInfo))
+                return if (this !is IrErrorExpression) this else original
+            }
+
+            override fun visitCall(expression: IrCall): IrExpression {
+                // try to calculate default args of inline function at call site
+                // used in `sourceLocation` test
+                expression.symbol.owner.valueParameters.forEachIndexed { index, parameter ->
+                    if (expression.getValueArgument(index) != null || !expression.symbol.owner.isInline) return@forEachIndexed
+                    val default = parameter.defaultValue?.expression as? IrCall ?: return@forEachIndexed
+                    val callWithNewOffsets = IrCallImpl(
+                        expression.startOffset, expression.endOffset, default.type, default.symbol,
+                        default.typeArgumentsCount, default.valueArgumentsCount, default.origin, default.superQualifierSymbol
+                    )
+                    callWithNewOffsets.copyTypeAndValueArgumentsFrom(default)
+                    interpreter.interpret(callWithNewOffsets, irFile)
+                        .report(callWithNewOffsets)
+                        .takeIf { it != callWithNewOffsets }
+                        ?.apply { expression.putArgument(parameter, this) }
+                }
+                return super.visitCall(expression)
+            }
+
+            override fun visitField(declaration: IrField): IrStatement {
+                val initializer = declaration.initializer
+                val expression = initializer?.expression ?: return declaration
+                if (expression is IrConst<*>) return declaration
+
+                val isConst = declaration.correspondingPropertySymbol?.owner?.isConst == true
+                if (isConst) initializer.expression = interpreter.interpret(expression, irFile).report(expression)
+                return declaration
+            }
+        }.visitFile(irFile)
+    }
+}
+
---
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/runners/ir/interpreter/AbstractIrInterpreterAfterFir2IrTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.runners.ir.interpreter
+
+import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
+import org.jetbrains.kotlin.test.TargetBackend
+import org.jetbrains.kotlin.test.backend.handlers.IrInterpreterBackendHandler
+import org.jetbrains.kotlin.test.backend.ir.JvmIrBackendFacade
+import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
+import org.jetbrains.kotlin.test.frontend.fir.Fir2IrResultsConverter
+import org.jetbrains.kotlin.test.frontend.fir.FirFrontendFacade
+import org.jetbrains.kotlin.test.model.*
+import org.jetbrains.kotlin.test.preprocessors.IrInterpreterImplicitKotlinImports
+import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerWithTargetBackendTest
+import org.jetbrains.kotlin.test.services.configuration.CommonEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.configuration.JvmEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.sourceProviders.IrInterpreterHelpersSourceFilesProvider
+
+open class AbstractIrInterpreterAfterFir2IrTest : AbstractKotlinCompilerWithTargetBackendTest(TargetBackend.JVM_IR) {
+    override fun TestConfigurationBuilder.configuration() {
+        globalDefaults {
+            frontend = FrontendKinds.FIR
+            targetPlatform = JvmPlatforms.defaultJvmPlatform
+            artifactKind = BinaryKind.NoArtifact
+            targetBackend = TargetBackend.JVM_IR
+            dependencyKind = DependencyKind.Source
+        }
+
+        defaultDirectives {
+            +JvmEnvironmentConfigurationDirectives.FULL_JDK
+            +JvmEnvironmentConfigurationDirectives.NO_RUNTIME
+            +JvmEnvironmentConfigurationDirectives.ALLOW_KOTLIN_PACKAGE
+        }
+
+        useConfigurators(
+            ::CommonEnvironmentConfigurator,
+            ::JvmEnvironmentConfigurator,
+        )
+
+        useAdditionalSourceProviders(::IrInterpreterHelpersSourceFilesProvider)
+        useSourcePreprocessor(::IrInterpreterImplicitKotlinImports)
+
+        useFrontendFacades(::FirFrontendFacade)
+        useFrontend2BackendConverters(::Fir2IrResultsConverter)
+        useBackendFacades(::JvmIrBackendFacade)
+
+        useBackendHandlers(::IrInterpreterBackendHandler)
+        enableMetaInfoHandler()
+    }
+}
+
