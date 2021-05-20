792ac6ab63cab897c52c80314015718260b687f0
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-05-21 00:04:48 +0200

    FIR IDE: update resolve call testdata after adding targetFunction to KtFunctionalTypeVariableCall

--- idea/idea-frontend-fir/testData/analysisSession/resolveCall/variableAsFunction.kt
+++ idea/idea-frontend-fir/testData/analysisSession/resolveCall/variableAsFunction.kt
@@ -2,4 +2,4 @@ fun call(x: (Int) -> String) {
     <selection>x(1)</selection>
 }
 
-// CALL: KtFunctionalTypeVariableCall: target = x: kotlin.Function1<kotlin.Int, kotlin.String>
+// CALL: KtFunctionalTypeVariableCall: target = x: kotlin.Function1<kotlin.Int, kotlin.String>, targetFunction = kotlin/Function1.invoke(p1: kotlin.Int): kotlin.String
