bdfe2c28626b39ac1e5511c55080a27d76d7b83f
Jim Sproch <jsproch@google.com>
2021-05-24 17:18:38 -0700

    Bump up to Kotlin 1.5.10
    Change-Id: I4ba2a2ed71d77132a22a636b42ea08604cb58552 ( https://android-review.googlesource.com/q/I4ba2a2ed71d77132a22a636b42ea08604cb58552 )
    
    Moved from: https://github.com/androidx/androidx/commit/a9e3f2890288078e7850426c9247842cb5354fc8

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposePlugin.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposePlugin.kt
@@ -159,7 +159,7 @@ class ComposeComponentRegistrar : ComponentRegistrar {
             project: Project,
             configuration: CompilerConfiguration
         ) {
-            val KOTLIN_VERSION_EXPECTATION = "1.5.0"
+            val KOTLIN_VERSION_EXPECTATION = "1.5.10"
             KotlinCompilerVersion.getVersion()?.let { version ->
                 val suppressKotlinVersionCheck = configuration.get(
                     ComposeConfiguration.SUPPRESS_KOTLIN_VERSION_COMPATIBILITY_CHECK,
