6a7770573c8d97bf3be2770b2c5a147908238ed8
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-05-27 18:18:24 +0200

    [imltogradle] Support attaching 211 IDEA

--- generators/ide-iml-to-gradle-generator/src/org/jetbrains/kotlin/generators/imltogradle/Main.kt
+++ generators/ide-iml-to-gradle-generator/src/org/jetbrains/kotlin/generators/imltogradle/Main.kt
@@ -41,6 +41,7 @@ private val intellijModulesForWhichGenerateBuildGradle = listOf(
 val jsonUrlPrefixes = mapOf(
     "202" to "https://buildserver.labs.intellij.net/guestAuth/repository/download/ijplatform_IjPlatform202_IntellijArtifactMappings/113235432:id",
     "203" to "https://buildserver.labs.intellij.net/guestAuth/repository/download/ijplatform_IjPlatform203_IntellijArtifactMappings/117989041:id",
+    "211" to "https://buildserver.labs.intellij.net/guestAuth/repository/download/ijplatform_IjPlatform211_IntellijArtifactMappings/121258191:id",
 )
 
 fun main() {
