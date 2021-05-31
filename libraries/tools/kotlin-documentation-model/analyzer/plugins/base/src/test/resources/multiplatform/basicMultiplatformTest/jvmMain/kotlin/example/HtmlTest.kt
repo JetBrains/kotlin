f9fe00c85f0646e4f10922f512cd4c5f38c90487
Marcin Aman <marcin.aman@gmail.com>
2021-05-31 13:33:48 +0200

    [KDM] Fix rendering html in briefs
    See https://github.com/Kotlin/dokka/pull/1931
    Original commit: https://github.com/Kotlin/dokka/commit/d4e70e113ac093bdd35edd9c112b4a0addc78ab5

---
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/resources/multiplatform/basicMultiplatformTest/jvmMain/kotlin/example/HtmlTest.kt
@@ -0,0 +1,20 @@
+package example
+
+/**
+ * <!-- this shouldn't be visible -->
+ */
+class HtmlTest {
+    /**
+     * This is an example <!-- not visible --> of html
+     */
+    fun test(){
+
+    }
+
+    /**
+     * This is an <b> documentation </b>
+     */
+    fun testP(){
+
+    }
+}
