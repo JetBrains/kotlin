0607000463e215c08b7391e8fe70d14edb98fb9f
Louis Pullen-Freilich <lpf@google.com>
2021-06-01 17:55:49 +0100

    Deletes old compose:androidview:androidview module
    Moves interop related demos to ui-demos
    
    Test: bOS
    Change-Id: I1241e517a3e4ab225000f593d695992d91064da8 ( https://android-review.git.corp.google.com/q/I1241e517a3e4ab225000f593d695992d91064da8 )

--- plugins/compose/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/AbstractLoweringTests.kt
+++ plugins/compose/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/AbstractLoweringTests.kt
@@ -82,7 +82,6 @@ abstract class AbstractLoweringTests : AbstractCodegenTest() {
         val compiledClasses = classLoader(
             """
        import android.widget.*
-       import androidx.compose.androidview.adapters.*
        import androidx.compose.runtime.*
 
        $COMPOSE_VIEW_STUBS_IMPORTS
