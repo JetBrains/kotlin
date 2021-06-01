1517fd0ca0753789d4c81a2fbb8c1cf77a437852
Jinseong Jeon <jsjeon@google.com>
2021-06-01 16:16:02 -0700

    FIR/UAST: commonoize array access expression

--- plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/expressions/KotlinUArrayAccessExpression.kt
+++ plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/expressions/KotlinUArrayAccessExpression.kt
@@ -19,23 +19,19 @@ package org.jetbrains.uast.kotlin
 import com.intellij.psi.PsiType
 import org.jetbrains.kotlin.psi.KtArrayAccessExpression
 import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.uast.UArrayAccessExpression
 import org.jetbrains.uast.UElement
 
 class KotlinUArrayAccessExpression(
-        override val sourcePsi: KtArrayAccessExpression,
-        givenParent: UElement?
-) : KotlinAbstractUExpression(givenParent), UArrayAccessExpression, KotlinUElementWithType, KotlinEvaluatableUElement {
-    override val receiver by lz { KotlinConverter.convertOrEmpty(sourcePsi.arrayExpression, this) }
-    override val indices by lz { sourcePsi.indexExpressions.map { KotlinConverter.convertOrEmpty(it, this) } }
+    override val sourcePsi: KtArrayAccessExpression,
+    givenParent: UElement?
+) : KotlinAbstractUArrayAccessExpression(sourcePsi, givenParent) {
 
     override fun getExpressionType(): PsiType? {
-        super<KotlinUElementWithType>.getExpressionType()?.let { return it }
+        super.getExpressionType()?.let { return it }
 
-        // for unknown reason in assigment position there is no `EXPRESSION_TYPE_INFO` so we getting it from the array type
+        // for unknown reason in assignment position there is no `EXPRESSION_TYPE_INFO` so we getting it from the array type
         val arrayExpression = sourcePsi.arrayExpression ?: return null
         val arrayType = arrayExpression.analyze()[BindingContext.EXPRESSION_TYPE_INFO, arrayExpression]?.type ?: return null
         return arrayType.arguments.firstOrNull()?.type?.toPsiType(this, arrayExpression, false )
     }
-
 }
