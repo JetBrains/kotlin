97ea9015072f3019c32da97fe2c1d026dfaac35e
Mark Punzalan <markpun@google.com>
2021-06-01 22:55:23 +0000

    FIR IDE: Add isDefinitelyNull() and isDefinitelyNotNull() to KtExpressionTypeProvider.

--- idea/idea-frontend-api/src/org/jetbrains/kotlin/idea/frontend/api/components/KtExpressionTypeProvider.kt
+++ idea/idea-frontend-api/src/org/jetbrains/kotlin/idea/frontend/api/components/KtExpressionTypeProvider.kt
@@ -13,8 +13,9 @@ import org.jetbrains.kotlin.psi.KtExpression
 abstract class KtExpressionTypeProvider : KtAnalysisSessionComponent() {
     abstract fun getReturnTypeForKtDeclaration(declaration: KtDeclaration): KtType
     abstract fun getKtExpressionType(expression: KtExpression): KtType
-
     abstract fun getExpectedType(expression: PsiElement): KtType?
+    abstract fun isDefinitelyNull(expression: KtExpression): Boolean
+    abstract fun isDefinitelyNotNull(expression: KtExpression): Boolean
 }
 
 interface KtExpressionTypeProviderMixIn : KtAnalysisSessionMixIn {
@@ -30,4 +31,35 @@ interface KtExpressionTypeProviderMixIn : KtAnalysisSessionMixIn {
      */
     fun PsiElement.getExpectedType(): KtType? =
         analysisSession.expressionTypeProvider.getExpectedType(this)
+
+    /**
+     * Returns `true` if this expression is definitely null, based on declared nullability and smart cast types derived from
+     * data-flow analysis facts. Examples:
+     * ```
+     *   fun <T : Any> foo(t: T, nt: T?, s: String, ns: String?) {
+     *     t     // t.isDefinitelyNull()  == false && t.isDefinitelyNotNull()  == true
+     *     nt    // nt.isDefinitelyNull() == false && nt.isDefinitelyNotNull() == false
+     *     s     // s.isDefinitelyNull()  == false && s.isDefinitelyNotNull()  == true
+     *     ns    // ns.isDefinitelyNull() == false && ns.isDefinitelyNotNull() == false
+     *
+     *     if (ns != null) {
+     *       ns  // ns.isDefinitelyNull() == false && ns.isDefinitelyNotNull() == true
+     *     } else {
+     *       ns  // ns.isDefinitelyNull() == true  && ns.isDefinitelyNotNull() == false
+     *     }
+     *
+     *     ns!!  // From this point on: ns.isDefinitelyNull() == false && ns.isDefinitelyNotNull() == true
+     *   }
+     * ```
+     * Note that only nullability from "stable" smart cast types is considered. The
+     * [spec](https://kotlinlang.org/spec/type-inference.html#smart-cast-sink-stability) provides an explanation on smart cast stability.
+     */
+    fun KtExpression.isDefinitelyNull(): Boolean =
+        analysisSession.expressionTypeProvider.isDefinitelyNull(this)
+
+    /**
+     * Returns `true` if this expression is definitely not null. See [isDefinitelyNull] for examples.
+     */
+    fun KtExpression.isDefinitelyNotNull(): Boolean =
+        analysisSession.expressionTypeProvider.isDefinitelyNotNull(this)
 }
