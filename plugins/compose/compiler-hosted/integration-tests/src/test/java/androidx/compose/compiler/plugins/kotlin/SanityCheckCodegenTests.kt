db8f88974e5e098968bce8b4902b2ce0aad899aa
Jim Sproch <jsproch@google.com>
2021-06-02 00:05:15 -0700

    Annotate composables in frontend instead of backend
    Add annotations to the descriptors of lambdas which are inferred composable
    in the frontend.  Delete the IrComposableAnnotator which was annotating in
    the backend.  We still need to figure out if this is kosher especially as
    the FIR becomes more of a thing (this may be an abuse of an extension
    point that I added previously, so we may need to revert this CL in the
    future), but it would be nice if annotating in the frontend is a viable
    long-term way of passing information from the frontend to backend.
    
    Change-Id: Ib5a71a35eef2d4e581201823dcddc72bbf3fba30 ( https://android-review.googlesource.com/q/Ib5a71a35eef2d4e581201823dcddc72bbf3fba30 )
    
    Moved from: https://github.com/androidx/androidx/commit/193b3a917f09da6c896dd13dc530bf546db06ea2

---
+++ plugins/compose/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/SanityCheckCodegenTests.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.compiler.plugins.kotlin
+
+class SanityCheckCodegenTests : AbstractCodegenTest() {
+
+    fun testCallAbstractSuperWithTypeParameters() = ensureSetup {
+        testCompile(
+            """
+                abstract class AbstractB<Type>(d: Type) : AbstractA<Int, Type>(d) {
+                    override fun test(key: Int): Type {
+                        return super.test(key)
+                    }
+                }
+                abstract class AbstractA<Type1, Type2>(var d: Type2) {
+                    open fun test(key: Type1): Type2 = d
+                }
+        """
+        )
+    }
+}
