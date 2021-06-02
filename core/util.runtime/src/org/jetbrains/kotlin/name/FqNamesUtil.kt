8f4c44056aab070df87e6ca7c20cbb8483cf474d
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-06-02 13:24:08 +0300

    Support picking settings of exacter annotations by the length of the matching part of fqname
    (cherry picked from commit d5180f79aa6c38052fbb2f7930123c93975c207d)

--- core/util.runtime/src/org/jetbrains/kotlin/name/FqNamesUtil.kt
+++ core/util.runtime/src/org/jetbrains/kotlin/name/FqNamesUtil.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.name
@@ -72,11 +61,17 @@ fun isValidJavaFqName(qualifiedName: String?): Boolean {
             State.MIDDLE -> {
                 if (c == '.') {
                     state = State.AFTER_DOT
-                }
-                else if (!Character.isJavaIdentifierPart(c)) return false
+                } else if (!Character.isJavaIdentifierPart(c)) return false
             }
         }
     }
 
     return state != State.AFTER_DOT
+}
+
+fun <V> FqName.findValueForMostSpecificFqname(values: Map<FqName, V>): V? {
+    val suitableItems = values.filter { (fqName, _) -> this == fqName || this.isChildOf(fqName) }
+        .takeIf { it.isNotEmpty() } ?: return null
+
+    return suitableItems.minByOrNull { (fqName, _) -> fqName.tail(this).asString().length }?.value
 }
