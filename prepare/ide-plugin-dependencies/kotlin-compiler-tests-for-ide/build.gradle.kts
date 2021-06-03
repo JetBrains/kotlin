3c7b07b90f4a442040ce705003528a7e42acfcfa
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-06-03 15:00:37 +0200

    Include `tests-compiler-utils` into kotlin-compiler-tests-for-ide artifact
    It fixes:
    ```
    java.lang.NoClassDefFoundError: org/jetbrains/kotlin/fir/FirCfgConsistencyChecker
    
    	at org.jetbrains.kotlin.test.frontend.fir.handlers.FirCfgConsistencyHandler.processModule(FirCfgConsistencyHandler.kt:15)
    	at org.jetbrains.kotlin.test.frontend.fir.handlers.FirCfgConsistencyHandler.processModule(FirCfgConsistencyHandler.kt:13)
    	at org.jetbrains.kotlin.test.TestRunnerKt.processModule(TestRunner.kt:236)
    	at org.jetbrains.kotlin.test.TestRunnerKt.hackyProcess(TestRunner.kt:231)
    	at org.jetbrains.kotlin.test.TestRunnerKt.access$hackyProcess(TestRunner.kt:1)
    ```
    while running `DiagnosisCompilerTestFE10TestdataTestGenerated.Tests#testBinaryCallsOnNullableValues` (and probably other tests) in new infrastructure

--- prepare/ide-plugin-dependencies/kotlin-compiler-tests-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/kotlin-compiler-tests-for-ide/build.gradle.kts
@@ -2,4 +2,11 @@ plugins {
     kotlin("jvm")
 }
 
-publishTestJarsForIde(listOf(":compiler:test-infrastructure", ":compiler:tests-common-new", ":compiler:test-infrastructure-utils"))
+publishTestJarsForIde(
+    listOf(
+        ":compiler:test-infrastructure",
+        ":compiler:tests-common-new",
+        ":compiler:test-infrastructure-utils",
+        ":compiler:tests-compiler-utils"
+    )
+)
