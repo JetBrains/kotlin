e838edc4001f4cad6d696f0a7b01b0bb72630f9e
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-06-04 15:29:18 +0300

    Move type enhancement state stuff into core:descriptors.jvm
    (cherry picked from commit 18384788a44ceefdfa149294d5794742eec0a5ce)

---
+++ core/descriptors.jvm/src/org/jetbrains/kotlin/load/java/JavaTypeEnhancementState.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.load.java
+
+import org.jetbrains.kotlin.name.FqName
+
+class JavaTypeEnhancementState(
+    val jsr305: Jsr305Settings = Jsr305Settings.DEFAULT,
+    val getReportLevelForAnnotation: (FqName) -> ReportLevel = ::getDefaultReportLevelForAnnotation
+) {
+    val disabledDefaultAnnotations = jsr305.isDisabled || getReportLevelForAnnotation(JSPECIFY_ANNOTATIONS_PACKAGE) == ReportLevel.IGNORE
+
+    companion object {
+        val DEFAULT = JavaTypeEnhancementState()
+    }
+}
