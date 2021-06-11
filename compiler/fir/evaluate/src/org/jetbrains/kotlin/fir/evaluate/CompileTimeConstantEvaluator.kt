fc5d1d8cb5217fcdf33af900f7b07eb244e1c166
Jinseong Jeon <jsjeon@google.com>
2021-06-11 00:33:52 -0700

    FirConstKind has been renamed to ConstantValueKind (d243319)

--- compiler/fir/evaluate/src/org/jetbrains/kotlin/fir/evaluate/CompileTimeConstantEvaluator.kt
+++ compiler/fir/evaluate/src/org/jetbrains/kotlin/fir/evaluate/CompileTimeConstantEvaluator.kt
@@ -16,6 +16,7 @@ import org.jetbrains.kotlin.fir.types.*
 import org.jetbrains.kotlin.fir.types.impl.FirImplicitBuiltinTypeRef
 import org.jetbrains.kotlin.fir.types.impl.FirImplicitIntTypeRef
 import org.jetbrains.kotlin.resolve.constants.ConstantValue
+import org.jetbrains.kotlin.types.ConstantValueKind
 
 /**
  * An evaluator that transform numeric operation, such as div, into compile-time constant iff involved operands, such as explicit receiver
@@ -60,7 +61,7 @@ class CompileTimeConstantEvaluator {
     private val FirFunctionCall.isNumericOperatorCall: Boolean
         get() {
             val function = getOriginalFunction() ?: return false
-            return typeRef.toFirConstKind() != null &&
+            return typeRef.toConstantValueKind() != null &&
                     // LHS is const expression with numeric constant value.
                     explicitReceiver != null && explicitReceiver!!.isNumericConst &&
                     function is FirSimpleFunction &&
@@ -74,7 +75,7 @@ class CompileTimeConstantEvaluator {
             this is FirConstExpression<*> && typedValue is Number
 
     private fun FirConstExpression<*>.adjustType(expectedType: FirTypeRef): FirConstExpression<*> {
-        val expectedKind = expectedType.toFirConstKind()
+        val expectedKind = expectedType.toConstantValueKind()
         // Note that the resolved type for the const expression is not always matched with the const kind. For example,
         //   fun foo(x: Int) {
         //     when (x) {
@@ -101,7 +102,7 @@ class CompileTimeConstantEvaluator {
         // TODO: need some systematic check, e.g., integer overflow
         return try {
             val r = f(typedValue)
-            r.toFirConstKind()!!.toConstExpression(source, r)
+            r.toConstantValueKind()!!.toConstExpression(source, r)
         } catch (e: Exception) {
             null
         }
@@ -116,7 +117,7 @@ class CompileTimeConstantEvaluator {
         // TODO: need some systematic check, e.g., div by zero (1 / 0 v.s. 1 / 0.f)
         return try {
             val r = f(typedValue, other.typedValue)
-            r.toFirConstKind()!!.toConstExpression(source, r)
+            r.toConstantValueKind()!!.toConstExpression(source, r)
         } catch (e: Exception) {
             null
         }
@@ -124,63 +125,63 @@ class CompileTimeConstantEvaluator {
 
     ////// KINDS
 
-    private fun FirTypeRef.toFirConstKind(): FirConstKind<out Number>? =
+    private fun FirTypeRef.toConstantValueKind(): ConstantValueKind<out Number>? =
         when (this) {
             !is FirResolvedTypeRef -> null
-            !is FirImplicitBuiltinTypeRef -> type.toFirConstKind()
-            is FirImplicitIntTypeRef -> FirConstKind.Int
+            !is FirImplicitBuiltinTypeRef -> type.toConstantValueKind()
+            is FirImplicitIntTypeRef -> ConstantValueKind.Int
             else -> null
         }
 
-    private fun ConeKotlinType.toFirConstKind(): FirConstKind<out Number>? =
+    private fun ConeKotlinType.toConstantValueKind(): ConstantValueKind<out Number>? =
         when (this) {
             is ConeKotlinErrorType -> null
             is ConeLookupTagBasedType -> {
                 when (lookupTag.name.asString()) {
-                    "Byte" -> FirConstKind.Byte
-                    "Double" -> FirConstKind.Double
-                    "Float" -> FirConstKind.Float
-                    "Int" -> FirConstKind.Int
-                    "Long" -> FirConstKind.Long
-                    "Short" -> FirConstKind.Short
+                    "Byte" -> ConstantValueKind.Byte
+                    "Double" -> ConstantValueKind.Double
+                    "Float" -> ConstantValueKind.Float
+                    "Int" -> ConstantValueKind.Int
+                    "Long" -> ConstantValueKind.Long
+                    "Short" -> ConstantValueKind.Short
                     else -> null
                 }
             }
-            is ConeFlexibleType -> upperBound.toFirConstKind()
-            is ConeCapturedType -> lowerType?.toFirConstKind() ?: constructor.supertypes!!.first().toFirConstKind()
-            is ConeDefinitelyNotNullType -> original.toFirConstKind()
-            is ConeIntersectionType -> intersectedTypes.first().toFirConstKind()
+            is ConeFlexibleType -> upperBound.toConstantValueKind()
+            is ConeCapturedType -> lowerType?.toConstantValueKind() ?: constructor.supertypes!!.first().toConstantValueKind()
+            is ConeDefinitelyNotNullType -> original.toConstantValueKind()
+            is ConeIntersectionType -> intersectedTypes.first().toConstantValueKind()
             is ConeStubType -> null
             is ConeIntegerLiteralType -> null
         }
 
-    private fun <T : Number> T.toFirConstKind(): FirConstKind<out Number>? =
+    private fun <T : Number> T.toConstantValueKind(): ConstantValueKind<out Number>? =
         when (this) {
-            is Byte -> FirConstKind.Byte
-            is Double -> FirConstKind.Double
-            is Float -> FirConstKind.Float
-            is Int -> FirConstKind.Int
-            is Long -> FirConstKind.Long
-            is Short -> FirConstKind.Short
+            is Byte -> ConstantValueKind.Byte
+            is Double -> ConstantValueKind.Double
+            is Float -> ConstantValueKind.Float
+            is Int -> ConstantValueKind.Int
+            is Long -> ConstantValueKind.Long
+            is Short -> ConstantValueKind.Short
             else -> null
         }
 
-    private fun FirConstKind<*>.convert(value: Number?): Number? {
+    private fun ConstantValueKind<*>.convert(value: Number?): Number? {
         if (value == null) {
             return null
         }
         return when {
-            this == FirConstKind.Byte -> value.toByte()
-            this == FirConstKind.Double -> value.toDouble()
-            this == FirConstKind.Float -> value.toFloat()
-            this == FirConstKind.Int -> value.toInt()
-            this == FirConstKind.Long -> value.toLong()
-            this == FirConstKind.Short -> value.toShort()
+            this == ConstantValueKind.Byte -> value.toByte()
+            this == ConstantValueKind.Double -> value.toDouble()
+            this == ConstantValueKind.Float -> value.toFloat()
+            this == ConstantValueKind.Int -> value.toInt()
+            this == ConstantValueKind.Long -> value.toLong()
+            this == ConstantValueKind.Short -> value.toShort()
             else -> null
         }
     }
 
-    private fun <T : Number> FirConstKind<T>.toConstExpression(source: FirSourceElement?, value: Number): FirConstExpression<T> =
+    private fun <T : Number> ConstantValueKind<T>.toConstExpression(source: FirSourceElement?, value: Number): FirConstExpression<T> =
         @Suppress("UNCHECKED_CAST")
         buildConstExpression(source, this, value as T)
 }
@@ -190,11 +191,11 @@ val <T> FirConstExpression<T>.typedValue: T
     get() =
         (value as? Long)?.let {
             when (kind) {
-                FirConstKind.Byte -> it.toByte()
-                FirConstKind.Short -> it.toShort()
-                FirConstKind.Int -> it.toInt()
-                FirConstKind.Float -> it.toFloat()
-                FirConstKind.Double -> it.toDouble()
+                ConstantValueKind.Byte -> it.toByte()
+                ConstantValueKind.Short -> it.toShort()
+                ConstantValueKind.Int -> it.toInt()
+                ConstantValueKind.Float -> it.toFloat()
+                ConstantValueKind.Double -> it.toDouble()
                 else -> it
             }
         } as T ?: value
