85f82fdf9b2c2920f99335b84db5a37024f1f5d3
Tianyu Geng <tgeng@google.com>
2021-06-15 11:20:54 -0700

    FIR: Fix positioning of ENUM_ENTRY_AS_TYPE
    The current SELECTOR_BY_QUALIFIED positioning strategy is closely
    related what should be done here. But it only works on qualified access
    expressions. This change also makes it work for type references.

--- compiler/fir/checkers/gen/org/jetbrains/kotlin/fir/analysis/diagnostics/FirErrors.kt
+++ compiler/fir/checkers/gen/org/jetbrains/kotlin/fir/analysis/diagnostics/FirErrors.kt
@@ -468,7 +468,7 @@ object FirErrors {
     val USELESS_CAST by warning0<KtBinaryExpressionWithTypeRHS>(SourceElementPositioningStrategies.AS_TYPE)
     val USELESS_IS_CHECK by warning1<KtElement, Boolean>()
     val IS_ENUM_ENTRY by error0<KtTypeReference>()
-    val ENUM_ENTRY_AS_TYPE by error0<KtTypeReference>()
+    val ENUM_ENTRY_AS_TYPE by error0<KtTypeReference>(SourceElementPositioningStrategies.SELECTOR_BY_QUALIFIED)
 
     // When expressions
     val NO_ELSE_IN_WHEN by error1<KtWhenExpression, List<WhenMissingCase>>(SourceElementPositioningStrategies.WHEN_EXPRESSION)
--- compiler/testData/diagnostics/tests/enum/inheritFromEnumEntry.fir.kt
+++
@@ -1,5 +0,0 @@
-enum class E {
-    ENTRY
-}
-
-class A : <!ENUM_ENTRY_AS_TYPE!>E.ENTRY<!>
