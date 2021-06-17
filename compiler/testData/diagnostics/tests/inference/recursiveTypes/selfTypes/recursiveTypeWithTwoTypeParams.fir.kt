44cf4be1e5158cdee562c611de4d477d89464bbf
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-06-18 02:50:22 +0300

    Inference for some kind of self types

---
+++ compiler/testData/diagnostics/tests/inference/recursiveTypes/selfTypes/recursiveTypeWithTwoTypeParams.fir.kt
@@ -0,0 +1,8 @@
+interface BodySpec<B, S : BodySpec<B, S>> {
+    fun <T : S> isEqualTo(expected: B): T
+}
+
+fun test(b: BodySpec<String, *>) {
+    val x = b.isEqualTo("")
+    x
+}
---
+++ compiler/testData/diagnostics/tests/inference/recursiveTypes/selfTypes/recursiveTypeWithTwoTypeParams.kt
@@ -0,0 +1,8 @@
+interface BodySpec<B, S : BodySpec<B, S>> {
+    fun <T : S> isEqualTo(expected: B): T
+}
+
+fun test(b: BodySpec<String, *>) {
+    val x = b.isEqualTo("")
+    <!DEBUG_INFO_EXPRESSION_TYPE("BodySpec<*, *>")!>x<!>
+}
