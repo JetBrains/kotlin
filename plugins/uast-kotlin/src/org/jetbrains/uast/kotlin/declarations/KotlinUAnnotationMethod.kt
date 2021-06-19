cda1a7edbe2cb9d34fd41ff3a11c028f44ffbc7c
Jinseong Jeon <jsjeon@google.com>
2021-06-18 23:34:18 -0700

    FIR/UAST: commonize UAnnotationMethod

---
+++ plugins/uast-kotlin/src/org/jetbrains/uast/kotlin/declarations/KotlinUAnnotationMethod.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.uast.kotlin
+
+import org.jetbrains.kotlin.asJava.elements.KtLightMethod
+import org.jetbrains.uast.UElement
+
+class KotlinUAnnotationMethod(
+    psi: KtLightMethod,
+    givenParent: UElement?
+) : BaseKotlinUAnnotationMethod(psi, givenParent), KotlinUMethodParametersProducer {
+
+    override val uastParameters by lz { produceUastParameters(this, receiverTypeReference) }
+}
