4915d8dda33140b7f4ee7a7cc8b9d7d9cb9e4a33
Tianyu Geng <tgeng@google.com>
2021-06-23 13:31:40 -0700

    FIR checker: support DUPLICATE_LABEL_IN_WHEN
    Changes from FE1.0:
    1. As discussed previously, no expression evaluation happens during this
    check.
    2. FE1.0 doesn't check redundant object comparisons.

--- idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/frontend/api/fir/diagnostics/KtFirDiagnostics.kt
+++ idea/idea-frontend-fir/src/org/jetbrains/kotlin/idea/frontend/api/fir/diagnostics/KtFirDiagnostics.kt
@@ -2050,6 +2050,10 @@ sealed class KtFirDiagnostic<PSI : PsiElement> : KtDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = CommaInWhenConditionWithoutArgument::class
     }
 
+    abstract class DuplicateLabelInWhen : KtFirDiagnostic<KtElement>() {
+        override val diagnosticClass get() = DuplicateLabelInWhen::class
+    }
+
     abstract class TypeParameterIsNotAnExpression : KtFirDiagnostic<KtSimpleNameExpression>() {
         override val diagnosticClass get() = TypeParameterIsNotAnExpression::class
         abstract val typeParameter: KtTypeParameterSymbol
