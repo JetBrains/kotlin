9e42138c0863557464443d2246ad49070ad5a35e
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-06-28 13:12:36 +0300

    Prevent recursion on erasion of raw types with interdependent type parameters
    ^KT-47480 Fixed

---
+++ compiler/testData/diagnostics/tests/platformTypes/rawTypes/intermediateRecursion.kt
@@ -0,0 +1,14 @@
+// FIR_IDENTICAL
+// FILE: Boo.java
+public class Boo<N> {}
+
+// FILE: Foo.java
+public class Foo<T extends Boo<K>, K extends Boo<X>, X extends Boo<K>> {
+    T test2() { return null; }
+    static Foo test1() { return null; }
+}
+
+// FILE: main.kt
+fun main() {
+    val x = Foo.test1().test2()
+}
