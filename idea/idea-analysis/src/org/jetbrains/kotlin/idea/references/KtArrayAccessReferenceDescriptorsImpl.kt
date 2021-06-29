de703ff4389497c58fcb55f5959b4ec6b7e3b1a1
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-06-29 10:46:47 +0700

    fix idea compilation
    (cherry picked from commit fe855d09d847dac8e79b2620033c3a953fdf686a)

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/references/KtArrayAccessReferenceDescriptorsImpl.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/references/KtArrayAccessReferenceDescriptorsImpl.kt
@@ -5,14 +5,9 @@
 
 package org.jetbrains.kotlin.idea.references
 
-import com.intellij.psi.MultiRangeReference
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
-import org.jetbrains.kotlin.idea.core.canMoveLambdaOutsideParentheses
-import org.jetbrains.kotlin.idea.core.moveFunctionLiteralOutsideParentheses
 import org.jetbrains.kotlin.psi.KtArrayAccessExpression
-import org.jetbrains.kotlin.psi.KtCallExpression
-import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_GET
 import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_SET
@@ -20,18 +15,9 @@ import org.jetbrains.kotlin.resolve.BindingContext.INDEXED_LVALUE_SET
 internal class KtArrayAccessReferenceDescriptorsImpl(
     expression: KtArrayAccessExpression
 ) : KtArrayAccessReference(expression), KtDescriptorsBasedReference {
-    override fun isReferenceTo(element: PsiElement): Boolean =
-        super<KtDescriptorsBasedReference>.isReferenceTo(element)
+    override fun isReferenceTo(element: PsiElement): Boolean = super<KtDescriptorsBasedReference>.isReferenceTo(element)
 
-    override fun moveFunctionLiteralOutsideParentheses(callExpression: KtCallExpression) {
-        callExpression.moveFunctionLiteralOutsideParentheses()
-    }
-
-    override fun canMoveLambdaOutsideParentheses(callExpression: KtCallExpression): Boolean =
-        callExpression.canMoveLambdaOutsideParentheses()
-
-    override fun doRenameImplicitConventionalCall(newName: String?): KtExpression =
-        renameImplicitConventionalCall(newName)
+    override fun handleElementRename(newElementName: String): PsiElement = renameImplicitConventionalCall(newElementName)
 
     override fun getTargetDescriptors(context: BindingContext): Collection<DeclarationDescriptor> {
         val getFunctionDescriptor = context[INDEXED_LVALUE_GET, expression]?.candidateDescriptor
