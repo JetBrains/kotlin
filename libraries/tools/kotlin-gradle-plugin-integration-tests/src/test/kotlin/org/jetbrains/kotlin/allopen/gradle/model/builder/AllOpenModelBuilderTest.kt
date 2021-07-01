0214e2da6ba88e501f43538d7f5d2894d3a996db
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2021-07-01 11:02:37 +0200

    Fix AllOpen plugin exposes common with Gradle runtime dependencies.
    ^KT-47354 Fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/allopen/gradle/model/builder/AllOpenModelBuilderTest.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/allopen/gradle/model/builder/AllOpenModelBuilderTest.kt
@@ -1,20 +1,19 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.allopen.gradle.model.builder
 
 import org.jetbrains.kotlin.gradle.model.AllOpen
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert
 import org.junit.Test
 
 class AllOpenModelBuilderTest {
     @Test
     fun testCanBuild() {
         val modelBuilder = AllOpenModelBuilder()
-        assertTrue(modelBuilder.canBuild(AllOpen::class.java.name))
-        assertFalse(modelBuilder.canBuild("wrongModel"))
+        Assert.assertTrue(modelBuilder.canBuild(AllOpen::class.java.name))
+        Assert.assertFalse(modelBuilder.canBuild("wrongModel"))
     }
 }
