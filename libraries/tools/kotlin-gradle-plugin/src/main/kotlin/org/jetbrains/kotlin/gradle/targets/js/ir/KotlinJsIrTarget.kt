4c4bcb1e3e6dd45606184e028e82cc02bee87e22
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2021-07-03 00:40:57 +0300

    [Gradle, JS] Turn on by default sourcemap generation for JS IR
    #KT-46551 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/ir/KotlinJsIrTarget.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/js/ir/KotlinJsIrTarget.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -228,5 +228,7 @@ constructor(
 
     private fun KotlinJsOptions.configureCommonJsOptions() {
         moduleKind = "commonjs"
+        sourceMap = true
+        sourceMapEmbedSources = "never"
     }
 }
