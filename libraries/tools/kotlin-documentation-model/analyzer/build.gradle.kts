3c7396449fc1f309e260c8e7fd5df9a3c2597625
Kamil Doległo <kamilok1965@interia.pl>
2021-07-07 17:51:57 +0200

    [KDM] Update the docs to 1.5.0
    Original commit: https://github.com/Kotlin/dokka/commit/4ef5075a78fd7b65b63c746975b749b3c8b68436

--- libraries/tools/kotlin-documentation-model/analyzer/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") apply false
     id("java")
-    id("org.jetbrains.dokka") version "1.4.32"
+    id("org.jetbrains.dokka") version "1.5.0"
     id("io.github.gradle-nexus.publish-plugin")
 }
 
