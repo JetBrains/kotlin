d8ef221cb5b6f8fc5bb8d4926e6714d31dfda139
negishitakuro <t_negi@hotmail.com>
2021-07-11 01:35:16 +0900

    fix Exception Type & add sample refelence every platform

--- libraries/stdlib/js/src/generated/_StringsJs.kt
+++ libraries/stdlib/js/src/generated/_StringsJs.kt
@@ -15,7 +15,7 @@ import kotlin.js.*
 /**
  * Returns a character at the given [index] or throws an [IndexOutOfBoundsException] if the [index] is out of bounds of this char sequence.
  * 
- * @sample samples.collections.Collections.Elements.elementAt
+ * @sample samples.text.Strings.elementAt
  */
 public actual fun CharSequence.elementAt(index: Int): Char {
     return elementAtOrElse(index) { throw IndexOutOfBoundsException("index: $index, length: $length}") }
