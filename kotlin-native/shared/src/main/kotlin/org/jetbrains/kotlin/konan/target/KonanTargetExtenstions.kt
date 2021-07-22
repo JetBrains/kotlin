572f08151ae5c07916cc19387e15804f1efeee47
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2021-07-22 20:16:35 +0700

    [K/N] Disable code coverage testing
    Since LLVM 8, coverage format has changed.
    We don't officially provide code coverage for end-users, so it's ok to
    disable it for now and fix later.

--- kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
+++ kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
@@ -3,10 +3,13 @@ package org.jetbrains.kotlin.konan.target
 // TODO: This all needs to go to konan.properties
 
 fun KonanTarget.supportsCodeCoverage(): Boolean =
-        this == KonanTarget.MINGW_X64 ||
-        this == KonanTarget.LINUX_X64 ||
-        this == KonanTarget.MACOS_X64 ||
-        this == KonanTarget.IOS_X64
+        // TODO: Disabled for now, because we don't support
+        //  coverage format from LLVM 11.
+        false
+//        this == KonanTarget.MINGW_X64 ||
+//        this == KonanTarget.LINUX_X64 ||
+//        this == KonanTarget.MACOS_X64 ||
+//        this == KonanTarget.IOS_X64
 
 fun KonanTarget.supportsMimallocAllocator(): Boolean =
      when(this) {
