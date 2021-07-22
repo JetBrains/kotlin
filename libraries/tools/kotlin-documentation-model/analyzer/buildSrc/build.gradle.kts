cd46896d071925030eb2d215dcb6af8c3bde7f2d
Marcin Aman <marcin.aman@gmail.com>
2021-07-22 14:27:55 +0200

    [KDM] Bump kotlin 1.5.21
    See https://github.com/Kotlin/dokka/pull/2031
    
    * bump kotlin to 1.5.21
    
    * Bump binary compatibility plugin
    
    * update stdlib submodule
    
    * Bump IJ to 202.8194.7
    Original commit: https://github.com/Kotlin/dokka/commit/b6dce5370b0c90932127499a903453b199e69c5a

--- libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 dependencies {
     implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.4.0")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.0.0")
 }
