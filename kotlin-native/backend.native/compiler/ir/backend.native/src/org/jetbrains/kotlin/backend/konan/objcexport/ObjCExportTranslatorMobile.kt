d72a399982a3dddbb2295cce17b493a93e25cf7e
Aleksandr Samofalov <aleksandr.samofalov@jetbrains.com>
2021-07-22 14:52:44 +0300

    KMA-51 Create API to use ObjCExportTranslator to translate functions in IDE

---
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/objcexport/ObjCExportTranslatorMobile.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.konan.objcexport
+
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
+
+class ObjCExportTranslatorMobile internal constructor(private val delegate: ObjCExportTranslatorImpl) : ObjCExportTranslator by delegate {
+    companion object {
+        fun create(namer: ObjCExportNamer): ObjCExportTranslatorMobile {
+            return ObjCExportTranslatorMobile(ObjCExportTranslatorImpl(null, ObjCExportMapper(), namer, ObjCExportProblemCollector.SILENT, false))
+        }
+    }
+
+    fun translateBaseFunction(descriptor: FunctionDescriptor): ObjCMethod {
+        val classDescriptor = descriptor.containingDeclaration as? ClassDescriptor
+        val scope = classDescriptor?.let { delegate.createGenericExportScope(it) } ?: ObjCNoneExportScope
+        return delegate.buildMethod(descriptor, descriptor, scope)
+    }
+}
