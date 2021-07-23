5cf4db8c44ec20f28f8fecfcbe6ef42f7d362add
Marina Shishkina <marina.shishkina@jetbrains.com>
2021-07-23 11:45:02 +0000

    FE: avoid stack overflow on star projection of enhanced type parameter
     #KT-47846 Fixed
    
    
    Co-authored-by: pyos <pyos@google.com>

---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/misc/enhancedRecursiveStarProjection.fir.kt
@@ -0,0 +1,11 @@
+// SKIP_TXT
+// FILE: I1.java
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public interface I1<@NonNull T> { }
+
+// FILE: I2.java
+public interface I2<T extends I1<T>> { }
+
+// FILE: main.kt
+fun foo(): I2<*> = TODO()
