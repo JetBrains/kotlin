cbdd0f579a080812b23a681150c0f753fbdd7f54
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-07-26 14:30:04 +0200

    Regenerate Gradle options

--- libraries/tools/kotlin-gradle-plugin-api/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinCommonOptions.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/main/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinCommonOptions.kt
@@ -6,14 +6,14 @@ interface KotlinCommonOptions  : org.jetbrains.kotlin.gradle.dsl.KotlinCommonToo
 
     /**
      * Allow using declarations only from the specified version of bundled libraries
-     * Possible values: "1.3 (DEPRECATED)", "1.4", "1.5", "1.6 (EXPERIMENTAL)"
+     * Possible values: "1.3 (DEPRECATED)", "1.4", "1.5", "1.6 (EXPERIMENTAL)", "1.7 (EXPERIMENTAL)"
      * Default value: null
      */
      var apiVersion: kotlin.String?
 
     /**
      * Provide source compatibility with the specified version of Kotlin
-     * Possible values: "1.3 (DEPRECATED)", "1.4", "1.5", "1.6 (EXPERIMENTAL)"
+     * Possible values: "1.3 (DEPRECATED)", "1.4", "1.5", "1.6 (EXPERIMENTAL)", "1.7 (EXPERIMENTAL)"
      * Default value: null
      */
      var languageVersion: kotlin.String?
