24e615fbf0bac6fde0386f2aad34cb1b9b9fa9d0
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2021-07-29 08:40:29 +0300

    JVM_IR Minor: update testdata

--- compiler/testData/debug/localVariables/tryFinally15.kt
+++ compiler/testData/debug/localVariables/tryFinally15.kt
@@ -23,5 +23,5 @@ fun box(): String {
 // test.kt:7 box: i:int=0:int
 // test.kt:8 box: i:int=0:int, x:java.lang.String="x":java.lang.String
 // test.kt:10 box: i:int=0:int
-// test.kt:5 box: i:int=0:int
+// test.kt:5 box:
 // test.kt:14 box:
