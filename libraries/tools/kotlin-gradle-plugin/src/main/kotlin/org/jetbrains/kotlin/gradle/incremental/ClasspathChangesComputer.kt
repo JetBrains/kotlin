c5aa470fe9d5cdff71202fbd1552180a5f09d200
Hung Nguyen <hungnv@google.com>
2021-07-29 09:58:40 +0100

    KT-45777: Take snapshots and compute changes for Kotlin classes
    Reuse the existing IncrementalJvmCache to take snapshots and compute
    changes for Kotlin classes.
    
    Java classes will be handled next.
    
    Bug: KT-45777
    Test: New ClasspathSnapshotterTest, ClasspathChangesComputerTest

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/incremental/ClasspathChangesComputer.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/incremental/ClasspathChangesComputer.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.gradle.incremental
 
+import com.intellij.openapi.util.io.FileUtil
+import org.jetbrains.annotations.TestOnly
 import org.jetbrains.kotlin.build.report.BuildReporter
 import org.jetbrains.kotlin.build.report.ICReporter
 import org.jetbrains.kotlin.build.report.metrics.BuildAttribute
@@ -16,6 +18,8 @@ import org.jetbrains.kotlin.incremental.*
 import java.io.File
 import org.jetbrains.kotlin.gradle.incremental.ChangesCollectorResult.Success
 import org.jetbrains.kotlin.gradle.incremental.ChangesCollectorResult.Failure
+import org.jetbrains.kotlin.incremental.storage.FileToCanonicalPathConverter
+import java.util.*
 
 /** Computes [ClasspathChanges] between two [ClasspathSnapshot]s .*/
 object ClasspathChangesComputer {
@@ -65,21 +69,39 @@ object ClasspathChangesComputer {
         for (key in current.classSnapshots.keys) {
             val currentSnapshot = current.classSnapshots[key]!!
             val previousSnapshot = previous.classSnapshots[key] ?: return Failure.AddedRemovedClasses
-            val result = collectClassChanges(currentSnapshot, previousSnapshot, changesCollector)
-            if (result is Failure) {
-                return result
+            if (currentSnapshot !is KotlinClassSnapshot || previousSnapshot !is KotlinClassSnapshot) {
+                return Failure.NotYetImplemented
             }
+            // TODO: Store results in changesCollector
+            collectKotlinClassChanges(currentSnapshot, previousSnapshot)
         }
         return Success
     }
 
-    @Suppress("UNUSED_PARAMETER")
-    private fun collectClassChanges(
-        current: ClassSnapshot,
-        previous: ClassSnapshot,
-        changesCollector: ChangesCollector
-    ): ChangesCollectorResult {
-        return Failure.NotYetImplemented
+    @TestOnly
+    internal fun collectKotlinClassChanges(current: KotlinClassSnapshot, previous: KotlinClassSnapshot): DirtyData {
+        // TODO Create IncrementalJvmCache early once and reuse it here
+        val workingDir =
+            FileUtil.createTempDirectory(this::class.java.simpleName, "_WorkingDir_${UUID.randomUUID()}", /* deleteOnExit */ true)
+        val incrementalJvmCache = IncrementalJvmCache(workingDir, /* targetOutputDir */ null, FileToCanonicalPathConverter)
+
+        // Store previous snapshot in incrementalJvmCache, the returned ChangesCollector result is not used.
+        incrementalJvmCache.saveClassToCache(
+            kotlinClassInfo = previous.classInfo,
+            sourceFiles = null,
+            changesCollector = ChangesCollector()
+        )
+
+        // Compute changes between the current snapshot and the previously stored snapshot, and store the result in changesCollector.
+        val changesCollector = ChangesCollector()
+        incrementalJvmCache.saveClassToCache(
+            kotlinClassInfo = current.classInfo,
+            sourceFiles = null,
+            changesCollector = changesCollector
+        )
+
+        workingDir.deleteRecursively()
+        return changesCollector.getDirtyData(listOf(incrementalJvmCache), NoOpBuildReporter.NoOpICReporter)
     }
 }
 
---
+++ libraries/tools/kotlin-gradle-plugin/src/testData/kotlin.incremental.useClasspathSnapshot/src/changedPublicMethodSignature/com/example/SimpleKotlinClass.kt
@@ -0,0 +1,12 @@
+package com.example
+
+class SimpleKotlinClass {
+
+    fun changedPublicMethod() {
+        println("I'm in a public method")
+    }
+
+    private fun privateMethod() {
+        println("I'm in a private method")
+    }
+}
