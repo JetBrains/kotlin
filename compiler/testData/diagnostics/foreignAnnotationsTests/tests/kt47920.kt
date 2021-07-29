cdbd0eb93267491643b716457e1134c7bd31d91f
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2021-07-29 17:21:28 +0200

    Reopen KT-47920 test and add missed .txt files
     #KT-47920 Open

--- compiler/testData/diagnostics/foreignAnnotationsTests/tests/kt47920.kt
+++ compiler/testData/diagnostics/foreignAnnotationsTests/tests/kt47920.kt
@@ -7,5 +7,5 @@ public class J1<@NonNull T> {}
 
 // FILE: main.kt
 fun main() {
-    J1<<!UPPER_BOUND_VIOLATED_BASED_ON_JAVA_ANNOTATIONS!>Any?<!>>() // violated nullability, no warnings; but there is an error with -Xtype-enhancement-improvements-strict-mode
+    J1<Any?>() // violated nullability, no warnings; but there is an error with -Xtype-enhancement-improvements-strict-mode
 }
