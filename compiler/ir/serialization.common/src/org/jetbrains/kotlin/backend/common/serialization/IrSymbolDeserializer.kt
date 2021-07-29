083ef064df50cad35450856b5c83b8ef1117b0da
Roman Artemev <roman.artemev@jetbrains.com>
2021-07-29 12:14:58 +0300

    [JS IC] Drop redundant enqueue

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrSymbolDeserializer.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrSymbolDeserializer.kt
@@ -73,9 +73,7 @@ class IrSymbolDeserializer(
     }
 
     fun referenceLocalIrSymbol(symbol: IrSymbol, signature: IdSignature) {
-//        assert(signature.isLocal)
         deserializedSymbols.putIfAbsent(signature, symbol)
-        if (enqueueAllDeclarations) enqueueLocalTopLevelDeclaration(signature)
     }
 
     fun referenceSimpleFunctionByLocalSignature(idSignature: IdSignature) : IrSimpleFunctionSymbol =
