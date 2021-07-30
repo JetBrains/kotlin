2c068b8c5a8c6a454addea4ffb6ce59b4a0a92b3
Igor Chevdar <igor.chevdar@jetbrains.com>
2021-07-30 20:32:56 +0500

    [K/N][IR] Fix for https://youtrack.jetbrains.com/issue/KT-47814

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ToplevelPhases.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ToplevelPhases.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.backend.common.serialization.metadata.KlibMetadataMo
 import org.jetbrains.kotlin.backend.konan.MemoryModel
 import org.jetbrains.kotlin.backend.konan.llvm.*
 import org.jetbrains.kotlin.backend.konan.lower.ExpectToActualDefaultValueCopier
+import org.jetbrains.kotlin.backend.konan.lower.SamSuperTypesChecker
 import org.jetbrains.kotlin.backend.konan.objcexport.ObjCExport
 import org.jetbrains.kotlin.backend.konan.serialization.KonanIdSignaturer
 import org.jetbrains.kotlin.backend.konan.serialization.KonanIrModuleSerializer
@@ -147,6 +148,26 @@ internal val copyDefaultValuesToActualPhase = konanUnitPhase(
         },
         name = "CopyDefaultValuesToActual",
         description = "Copy default values from expect to actual declarations"
+)
+
+/*
+ * Sometimes psi2ir produces IR with non-trivial variance in super types of SAM conversions (this is a language design issue).
+ * Earlier this was solved with just erasing all such variances but this might lead to some other hard to debug problems,
+ * so after handling the majority of corner cases correctly in psi2ir it is safe to assume that such cases don't get here and
+ * even if they do, then it's better to throw an error right away than to dig out weird crashes down the pipeline or even at runtime.
+ * We explicitly check this, also fixing older klibs built with previous compiler versions by applying the same trick as before.
+ */
+internal val checkSamSuperTypesPhase = konanUnitPhase(
+        op = {
+            irModule!!.files
+                    .forEach { SamSuperTypesChecker(this, it, mode = SamSuperTypesChecker.Mode.THROW).run() }
+            // TODO: This is temporary for handling klibs produced with earlier compiler versions.
+            irModules.values
+                    .flatMap { it.files }
+                    .forEach { SamSuperTypesChecker(this, it, mode = SamSuperTypesChecker.Mode.ERASE).run() }
+        },
+        name = "CheckSamSuperTypes",
+        description = "Check SAM conversions super types"
 )
 
 internal val serializerPhase = konanUnitPhase(
@@ -424,6 +445,7 @@ val toplevelPhase: CompilerPhase<*, Unit, Unit> = namedUnitPhase(
                 psiToIrPhase then
                 destroySymbolTablePhase then
                 copyDefaultValuesToActualPhase then
+                checkSamSuperTypesPhase then
                 serializerPhase then
                 specialBackendChecksPhase then
                 namedUnitPhase(
@@ -484,5 +506,6 @@ internal fun PhaseConfig.konanPhasesConfig(config: KonanConfig) {
         disableIf(destroySymbolTablePhase, isDescriptorsOnlyLibrary)
         disableIf(copyDefaultValuesToActualPhase, isDescriptorsOnlyLibrary)
         disableIf(specialBackendChecksPhase, isDescriptorsOnlyLibrary)
+        disableIf(checkSamSuperTypesPhase, isDescriptorsOnlyLibrary)
     }
 }
