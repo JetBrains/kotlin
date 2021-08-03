d804ee0775d3cda2e20c83d81ffa1d93add6ef86
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-08-03 19:41:24 +0900

    Embed Parcelize runtime library back to fix backwards compatibility in the Kotlin IDE plugin

--- plugins/parcelize/parcelize-compiler/build.gradle.kts
+++ plugins/parcelize/parcelize-compiler/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     compileOnly(project(":compiler:ir.backend.common"))
     compileOnly(project(":compiler:backend.jvm"))
     compileOnly(project(":compiler:ir.tree.impl"))
+    compileOnly(project(":plugins:parcelize:parcelize-runtime"))
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeJars("asm-all", rootProject = rootProject) }
 
@@ -39,6 +40,8 @@ dependencies {
     robolectricClasspath("org.robolectric:android-all:4.4_r1-robolectric-1")
     robolectricClasspath(project(":plugins:parcelize:parcelize-runtime")) { isTransitive = false }
     robolectricClasspath(project(":kotlin-android-extensions-runtime")) { isTransitive = false }
+
+    embedded(project(":plugins:parcelize:parcelize-runtime")) { isTransitive = false }
 
     parcelizeRuntimeForTests(project(":plugins:parcelize:parcelize-runtime")) { isTransitive = false }
     parcelizeRuntimeForTests(project(":kotlin-android-extensions-runtime")) { isTransitive = false }
