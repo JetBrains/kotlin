38079372e198151c11b44a18f991a7cf8ad816a7
Nikolay Lunyak <lunyak.kolya@mail.ru>
2021-08-05 11:45:04 +0300

    [FIR] Add RawFirBuilder & DeclarationsConverter support

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -18,10 +18,7 @@ import org.jetbrains.kotlin.fir.contracts.FirContractDescription
 import org.jetbrains.kotlin.fir.contracts.builder.buildRawContractDescription
 import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.declarations.builder.*
-import org.jetbrains.kotlin.fir.declarations.impl.FirDeclarationStatusImpl
-import org.jetbrains.kotlin.fir.declarations.impl.FirDefaultPropertyAccessor
-import org.jetbrains.kotlin.fir.declarations.impl.FirDefaultPropertyGetter
-import org.jetbrains.kotlin.fir.declarations.impl.FirDefaultPropertySetter
+import org.jetbrains.kotlin.fir.declarations.impl.*
 import org.jetbrains.kotlin.fir.declarations.utils.*
 import org.jetbrains.kotlin.fir.diagnostics.*
 import org.jetbrains.kotlin.fir.expressions.*
@@ -370,6 +367,7 @@ open class RawFirBuilder(
             propertyTypeRef: FirTypeRef,
             propertySymbol: FirPropertySymbol,
             isGetter: Boolean,
+            hasExplicitBackingField: Boolean,
         ): FirPropertyAccessor? {
             val accessorVisibility =
                 if (this?.visibility != null && this.visibility != Visibilities.Unknown) this.visibility else property.visibility
@@ -426,7 +424,11 @@ open class RawFirBuilder(
                         this@RawFirBuilder.context.firFunctionTargets.removeLast()
                     }
                 }
-                isGetter || property.isVar -> {
+                // If an explicit backing field is present,
+                // the default accessors might not be compatible
+                // with it. We should check the types first, and
+                // only then see if we can create the default accessors.
+                !hasExplicitBackingField && (isGetter || property.isVar) -> {
                     // Default getter for val/var properties, and default setter for var properties.
                     val propertySource =
                         this?.toFirSourceElement() ?: property.toFirPsiSourceElement(FirFakeSourceElementKind.DefaultAccessor)
@@ -452,6 +454,55 @@ open class RawFirBuilder(
                     // No default setter for val properties.
                     null
                 }
+            }
+        }
+
+        private fun obtainPropertyComponentStatus(
+            componentVisibility: Visibility,
+            declaration: KtDeclaration?,
+            property: KtProperty,
+        ): FirDeclarationStatus {
+            return FirDeclarationStatusImpl(componentVisibility, declaration?.modality).apply {
+                isInline = property.hasModifier(INLINE_KEYWORD) ||
+                        declaration?.hasModifier(INLINE_KEYWORD) == true
+                isExternal = property.hasModifier(EXTERNAL_KEYWORD) ||
+                        declaration?.hasModifier(EXTERNAL_KEYWORD) == true
+            }
+        }
+
+        private fun KtBackingField?.toFirBackingField(
+            property: KtProperty,
+            propertySymbol: FirPropertySymbol,
+        ): FirBackingField? {
+            val componentVisibility = if (this?.visibility != null && this.visibility != Visibilities.Unknown) {
+                this.visibility
+            } else {
+                Visibilities.Private
+            }
+            val status = obtainPropertyComponentStatus(componentVisibility, this, property)
+            val backingFieldInitializer = when (mode) {
+                BodyBuildingMode.LAZY_BODIES -> buildLazyExpression {
+                    source = this@toFirBackingField?.initializer?.toFirSourceElement()
+                }
+                BodyBuildingMode.STUBS -> buildExpressionStub()
+                else -> this@toFirBackingField?.initializer.toFirExpression("Should have initializer")
+            }
+            val returnType = this?.returnTypeReference.toFirOrImplicitType()
+            val source = this?.toFirSourceElement()
+            return if (this != null) {
+                buildBackingField {
+                    this.source = source
+                    moduleData = baseModuleData
+                    origin = FirDeclarationOrigin.Source
+                    returnTypeRef = returnType
+                    this.status = status
+                    extractAnnotationsTo(this)
+                    symbol = FirPropertyFieldDeclarationSymbol()
+                    this.propertySymbol = propertySymbol
+                    this.initializer = backingFieldInitializer
+                }
+            } else {
+                null
             }
         }
 
@@ -1423,6 +1474,12 @@ open class RawFirBuilder(
                 if (this@toFirProperty.isLocal) {
                     isLocal = true
                     symbol = FirPropertySymbol(propertyName)
+
+                    backingField = this@toFirProperty.fieldDeclaration.toFirBackingField(
+                        this@toFirProperty,
+                        propertySymbol = symbol,
+                    )
+
                     status = FirDeclarationStatusImpl(Visibilities.Local, Modality.FINAL).apply {
                         isLateInit = hasModifier(LATEINIT_KEYWORD)
                     }
@@ -1455,17 +1512,25 @@ open class RawFirBuilder(
                     dispatchReceiverType = currentDispatchReceiverType()
                     extractTypeParametersTo(this, symbol)
                     withCapturedTypeParameters(true, this.typeParameters) {
+                        backingField = this@toFirProperty.fieldDeclaration.toFirBackingField(
+                            this@toFirProperty,
+                            propertySymbol = symbol,
+                        )
+
+                        val hasExplicitBackingField = backingField != null
                         getter = this@toFirProperty.getter.toFirPropertyAccessor(
                             this@toFirProperty,
                             propertyType,
                             propertySymbol = symbol,
-                            isGetter = true
+                            isGetter = true,
+                            hasExplicitBackingField,
                         )
                         setter = this@toFirProperty.setter.toFirPropertyAccessor(
                             this@toFirProperty,
                             propertyType,
                             propertySymbol = symbol,
-                            isGetter = false
+                            isGetter = false,
+                            hasExplicitBackingField,
                         )
 
                         status = FirDeclarationStatusImpl(visibility, modality).apply {
@@ -2420,4 +2485,3 @@ enum class PsiHandlingMode {
      */
     IDE;
 }
-
