927723c7663fe51d77be0960815dcf05c5e5647b
Tianyu Geng <tgeng@google.com>
2021-08-09 11:06:30 -0700

    FIR checker: fix JAVA_TYPE_MISMATCH checker
    First, the order of arguments is swapped.
    Second, projection erasure for argType was too aggressive. We should
    instead retain the projections that are under an invariant position.

--- compiler/testData/diagnostics/tests/platformTypes/genericVarianceViolation/javaOutProjection.kt
+++ compiler/testData/diagnostics/tests/platformTypes/genericVarianceViolation/javaOutProjection.kt
@@ -8,11 +8,30 @@ import java.util.*;
 public class A {
     void foo(List<Object> x) {}
     <T> void foo2(List<T> x) {}
+    void foo3(Collection<? extends Object> x) {}
+
     List<? extends Number> bar() {}
+    List<List<? extends Number>> bar2() {}
+}
+
+// FILE: B.java
+
+import java.util.*;
+
+public class B<T> {
+    public B(Collection<T> c) {
+
+    }
 }
 // FILE: main.kt
 
 fun main(a: A) {
     a.foo(<!JAVA_TYPE_MISMATCH!>a.bar()<!>)
     a.foo2(a.bar())
+    a.foo3(a.bar())
+    B(a.bar())
+    a.foo(<!JAVA_TYPE_MISMATCH!>a.bar2()<!>)
+    a.foo2(a.bar2())
+    a.foo3(a.bar2())
+    B(a.bar2())
 }
