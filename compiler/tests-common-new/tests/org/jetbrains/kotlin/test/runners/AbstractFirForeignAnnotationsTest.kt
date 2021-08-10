58110709f3b017a3edb5d884ef0a4e40811ce7ce
pyos <pyos@google.com>
2021-08-10 12:36:40 +0200

    Get rid of lambdas in AbstractForeignAnnotationsTestBase
    Also enable the features from the previous two commits

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/runners/AbstractFirForeignAnnotationsTest.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/runners/AbstractFirForeignAnnotationsTest.kt
@@ -6,51 +6,56 @@
 package org.jetbrains.kotlin.test.runners
 
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.builders.configureFirHandlersStep
 import org.jetbrains.kotlin.test.builders.firFrontendStep
 import org.jetbrains.kotlin.test.builders.firHandlersStep
+import org.jetbrains.kotlin.test.frontend.classic.handlers.FirJspecifyDiagnosticComplianceHandler
 import org.jetbrains.kotlin.test.frontend.fir.FirFailingTestSuppressor
 import org.jetbrains.kotlin.test.frontend.fir.handlers.*
 import org.jetbrains.kotlin.test.model.FrontendKinds
 import org.jetbrains.kotlin.test.preprocessors.JspecifyMarksCleanupPreprocessor
 import org.jetbrains.kotlin.test.services.fir.FirOldFrontendMetaConfigurator
 
-private val configureFir: TestConfigurationBuilder.() -> Unit = {
-    globalDefaults {
-        frontend = FrontendKinds.FIR
-    }
+abstract class AbstractFirForeignAnnotationsTestBase(kind: ForeignAnnotationsTestKind) : AbstractForeignAnnotationsTestBase(kind) {
+    override fun TestConfigurationBuilder.configureFrontend() {
+        globalDefaults {
+            frontend = FrontendKinds.FIR
+        }
 
-    useMetaInfoProcessors(::PsiLightTreeMetaInfoProcessor)
-    firFrontendStep()
-    firHandlersStep {
-        useHandlers(
-            ::FirDiagnosticsHandler,
-            ::FirDumpHandler,
-            ::FirCfgDumpHandler,
-            ::FirCfgConsistencyHandler,
-            ::FirNoImplicitTypesHandler,
-        )
-    }
+        useMetaInfoProcessors(::PsiLightTreeMetaInfoProcessor)
+        firFrontendStep()
+        firHandlersStep {
+            useHandlers(
+                ::FirDiagnosticsHandler,
+                ::FirDumpHandler,
+                ::FirCfgDumpHandler,
+                ::FirCfgConsistencyHandler,
+                ::FirNoImplicitTypesHandler,
+            )
+        }
 
-    forTestsMatching("compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/jspecify/*") {
-        // TODO: port JspecifyDiagnosticComplianceHandler (it doesn't really use the frontend artifact)
-        useSourcePreprocessor(::JspecifyMarksCleanupPreprocessor)
-    }
+        forTestsMatching("compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/jspecify/*") {
+            configureFirHandlersStep {
+                useHandlers(::FirJspecifyDiagnosticComplianceHandler)
+            }
+            useSourcePreprocessor(::JspecifyMarksCleanupPreprocessor)
+        }
 
-    forTestsMatching("compiler/testData/diagnostics/*") {
-        useAfterAnalysisCheckers(
-            ::FirIdenticalChecker,
-            ::FirFailingTestSuppressor,
-        )
-        useMetaTestConfigurators(::FirOldFrontendMetaConfigurator)
+        forTestsMatching("compiler/testData/diagnostics/*") {
+            useAfterAnalysisCheckers(
+                ::FirIdenticalChecker,
+                ::FirFailingTestSuppressor,
+            )
+            useMetaTestConfigurators(::FirOldFrontendMetaConfigurator)
+        }
     }
 }
 
-abstract class AbstractFirForeignAnnotationsTestBase : AbstractForeignAnnotationsTestBase(configureFir)
-
-abstract class AbstractFirForeignAnnotationsSourceJavaTest : AbstractFirForeignAnnotationsTestBase()
+abstract class AbstractFirForeignAnnotationsSourceJavaTest :
+    AbstractFirForeignAnnotationsTestBase(ForeignAnnotationsTestKind.SOURCE)
 
 abstract class AbstractFirForeignAnnotationsCompiledJavaTest :
-    AbstractForeignAnnotationsCompiledJavaTest(configureFir)
+    AbstractFirForeignAnnotationsTestBase(ForeignAnnotationsTestKind.COMPILED_JAVA)
 
 abstract class AbstractFirForeignAnnotationsCompiledJavaWithPsiClassReadingTest :
-    AbstractForeignAnnotationsCompiledJavaWithPsiClassReadingTest(configureFir)
+    AbstractFirForeignAnnotationsTestBase(ForeignAnnotationsTestKind.COMPILED_JAVA_WITH_PSI_CLASS_LOADING)
