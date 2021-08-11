7ff006e850b9dc6d658a853f4118894b80dec79c
vmishenev <vad-mishenev@yandex.ru>
2021-08-11 17:26:43 +0300

    [KDM] Handling `@code` tag
    See https://github.com/Kotlin/dokka/pull/2059
    Original commit: https://github.com/Kotlin/dokka/commit/42c0320e0c5f2d79a5438558bb87d4668aa4c3cc

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/parsers/JavadocParserTest.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/parsers/JavadocParserTest.kt
@@ -1,13 +1,26 @@
 package parsers
 
+import com.jetbrains.rd.util.first
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
 import org.jetbrains.dokka.model.DEnum
 import org.jetbrains.dokka.model.DModule
+import org.jetbrains.dokka.model.doc.CodeBlock
+import org.jetbrains.dokka.model.doc.CodeInline
+import org.jetbrains.dokka.model.doc.Text
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import utils.text
+import utils.*
 
 class JavadocParserTest : BaseAbstractTest() {
+
+    private val configuration = dokkaConfiguration {
+        sourceSets {
+            sourceSet {
+                sourceRoots = listOf("src/")
+                analysisPlatform = "jvm"
+            }
+        }
+    }
 
     private fun performJavadocTest(testOperation: (DModule) -> Unit) {
         val configuration = dokkaConfiguration {
@@ -45,6 +58,53 @@ class JavadocParserTest : BaseAbstractTest() {
         performJavadocTest { module ->
             val docs = (module.packages.single().classlikes.single() as DEnum).entries.single().documentation.values.single().children.single().root.text()
             assertEquals("content being refreshed, which can be a result of invalidation, refresh that may contain content updates, or the initial load.", docs.trimEnd())
+        }
+    }
+
+    @Test
+    fun `code tag`() {
+        val source = """
+            |/src/main/kotlin/test/Test.java
+            |package example
+            |
+            | /**
+            | * Identifies calls to {@code assertThat}.
+            | *
+            | * {@code
+            | * Set<String> s;
+            | * System.out.println("s1 = " + s);
+            | * }
+            | * <pre>{@code
+            | * Set<String> s2;
+            | * System.out
+            | *         .println("s2 = " + s2);
+            | * }</pre>
+            | * 
+            | */
+            | public class Test {}
+            """.trimIndent()
+        testInline(
+            source,
+            configuration,
+        ) {
+            documentablesCreationStage = { modules ->
+                val docs = modules.first().packages.first().classlikes.single().documentation.first().value
+                val root = docs.children.first().root
+
+                kotlin.test.assertEquals(
+                    listOf(
+                        Text(body = "Identifies calls to "),
+                        CodeInline(children = listOf(Text(body = "assertThat"))),
+                        Text(body = ". "),
+                        CodeInline(children = listOf(Text(body = "\nSet<String> s;\nSystem.out.println(\"s1 = \" + s);\n")))
+                    ),
+                    root.children[0].children
+                )
+                kotlin.test.assertEquals(
+                    CodeBlock(children = listOf(Text(body = "\nSet<String> s2;\nSystem.out\n        .println(\"s2 = \" + s2);\n"))),
+                    root.children[1]
+                )
+            }
         }
     }
 }
