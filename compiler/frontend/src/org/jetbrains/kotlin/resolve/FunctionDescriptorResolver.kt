c4558da49d8e9b0651204c198334a9ed403fc4eb
Anastasiya Shadrina <anastasia.shadrina@jetbrains.com>
2021-08-14 00:07:06 +0700

    [FE] Support functional literals with context receivers

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/FunctionDescriptorResolver.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/FunctionDescriptorResolver.kt
@@ -198,9 +198,11 @@ class FunctionDescriptorResolver(
             }
 
         val contextReceivers = function.contextReceivers
-        val contextReceiverTypes = contextReceivers.mapNotNull { it.typeReference() }.map {
-            typeResolver.resolveType(headerScope, it, trace, true)
-        }
+        val contextReceiverTypes =
+            if (function is KtFunctionLiteral) expectedFunctionType.getContextReceiversTypes()
+            else contextReceivers
+                .mapNotNull { it.typeReference() }
+                .map { typeResolver.resolveType(headerScope, it, trace, true) }
 
         val valueParameterDescriptors =
             createValueParameterDescriptors(function, functionDescriptor, headerScope, trace, expectedFunctionType, inferenceSession)
@@ -350,6 +352,9 @@ class FunctionDescriptorResolver(
     private fun KotlinType.functionTypeExpected() = !TypeUtils.noExpectedType(this) && isBuiltinFunctionalType
     private fun KotlinType.getReceiverType(): KotlinType? =
         if (functionTypeExpected()) this.getReceiverTypeFromFunctionType() else null
+
+    private fun KotlinType.getContextReceiversTypes(): List<KotlinType> =
+        if (functionTypeExpected()) this.getContextReceiverTypesFromFunctionType() else emptyList()
 
     private fun KotlinType.getValueParameters(owner: FunctionDescriptor): List<ValueParameterDescriptor>? =
         if (functionTypeExpected()) {
