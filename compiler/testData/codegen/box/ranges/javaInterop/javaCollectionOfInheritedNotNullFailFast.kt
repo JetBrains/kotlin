8f03eea5541d46f55320331a95169e5e262a1f96
pyos <pyos@google.com>
2021-08-13 13:53:26 +0200

    FIR: unmute some more blackbox tests

--- compiler/testData/codegen/box/ranges/javaInterop/javaCollectionOfInheritedNotNullFailFast.kt
+++ compiler/testData/codegen/box/ranges/javaInterop/javaCollectionOfInheritedNotNullFailFast.kt
@@ -1,7 +1,6 @@
 // !LANGUAGE: +StrictJavaNullabilityAssertions
 // TARGET_BACKEND: JVM
 // IGNORE_BACKEND: JVM, JVM_IR
-// IGNORE_BACKEND_FIR: JVM_IR
 // WITH_RUNTIME
 
 // Note: This fails on JVM (non-IR) with "Fail: should throw on get() in loop header". The not-null assertion is not generated when
