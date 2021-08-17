bc26176d85c2abbbb18fa79aea1bef163030b615
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2021-08-17 16:07:13 +0300

    [K/N] Fix test, now it catch unhandled Kotlin exception

---
+++ kotlin-native/backend.native/tests/interop/objc/exceptions/exceptions.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+import kotlinx.cinterop.*
+import exceptions.*
+
+fun main() {
+    ExceptionThrowerManager.throwExceptionWith(object : NSObject(), ExceptionThrowerProtocol {
+        override fun throwException() {
+            throw MyException()
+        }
+    })
+}
+
+private class MyException : Throwable()
