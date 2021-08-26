cd4a08cb185cc4bb58c50b579506d28e42054f6e
Jinseong Jeon <jsjeon@google.com>
2021-08-26 11:41:13 -0700

    Migrate ReadWriteAccess and checker logic (from frontend-independent)

--- compiler/psi/src/org/jetbrains/kotlin/idea/references/referenceUtils.kt
+++ compiler/psi/src/org/jetbrains/kotlin/idea/references/referenceUtils.kt
@@ -6,9 +6,12 @@
 package org.jetbrains.kotlin.idea.references
 
 import org.jetbrains.kotlin.kdoc.psi.impl.KDocName
-import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtReferenceExpression
-import org.jetbrains.kotlin.psi.KtSimpleNameExpression
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getAssignmentByLHS
+import org.jetbrains.kotlin.psi.psiUtil.getQualifiedExpressionForSelectorOrThis
+import org.jetbrains.kotlin.resolve.references.ReferenceAccess
+import org.jetbrains.kotlin.utils.addToStdlib.constant
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstance
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
@@ -27,3 +30,37 @@ val KtElement.mainReference: KtReference?
         is KDocName -> mainReference
         else -> references.firstIsInstanceOrNull()
     }
+
+fun KtExpression.readWriteAccess(useResolveForReadWrite: Boolean) =
+    readWriteAccessWithFullExpression(useResolveForReadWrite).first
+
+fun KtExpression.readWriteAccessWithFullExpression(
+    useResolveForReadWrite: Boolean,
+    readWriteAccessWithFullExpressionByResolve: (KtBinaryExpression) -> Pair<ReferenceAccess, KtExpression>? = { null },
+): Pair<ReferenceAccess, KtExpression> {
+    var expression = getQualifiedExpressionForSelectorOrThis()
+    loop@ while (true) {
+        when (val parent = expression.parent) {
+            is KtParenthesizedExpression, is KtAnnotatedExpression, is KtLabeledExpression -> expression = parent as KtExpression
+            else -> break@loop
+        }
+    }
+
+    val assignment = expression.getAssignmentByLHS()
+    if (assignment != null) {
+        return when (assignment.operationToken) {
+            KtTokens.EQ -> ReferenceAccess.WRITE to assignment
+
+            else -> {
+                (if (useResolveForReadWrite) readWriteAccessWithFullExpressionByResolve(assignment) else null)
+                    ?: (ReferenceAccess.READ_WRITE to assignment)
+            }
+        }
+    }
+
+    val unaryExpression = expression.parent as? KtUnaryExpression
+    return if (unaryExpression != null && unaryExpression.operationToken in constant { setOf(KtTokens.PLUSPLUS, KtTokens.MINUSMINUS) })
+        ReferenceAccess.READ_WRITE to unaryExpression
+    else
+        ReferenceAccess.READ to expression
+}
