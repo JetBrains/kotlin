facdb91469a244074198ccd4e5553e9e854b7386
Pavel Punegov <pavel.punegov@jetbrains.com>
2021-08-27 16:17:25 +0300

    [Native][Tools] Add input directory in the bundles repack script
    (cherry picked from commit 39b07d23f40e7b09c7d4a9f6e9d1bb0f55c3b294)

--- kotlin-native/tools/scripts/repack_bundles.py
+++ kotlin-native/tools/scripts/repack_bundles.py
@@ -5,36 +5,39 @@ import os
 import os.path
 import shutil
 
-if len(sys.argv) != 2:
-    print('Usage: ' + sys.argv[0] + ' <kotlin_version>')
+if len(sys.argv) != 3:
+    print('Usage: ' + sys.argv[0] + ' <kotlin_version> <input_dir>')
     sys.exit(0)
 
 kotlinVersion = sys.argv[1]
-print('Repacking bundles for Kotlin/Native version ' + kotlinVersion)
+inputDir = sys.argv[2]
+print('Repacking bundles for Kotlin/Native version ' + kotlinVersion + ' from ' + inputDir)
+os.chdir(inputDir)
 
-bundles = [f for f in os.listdir('.') if f.startswith('kotlin-native-prebuilt-') and (f.endswith('.tar.gz') or f.endswith('.zip')) and os.path.isfile(f)]
+bundles = [f for f in os.listdir('.')
+           if f.startswith('kotlin-native-prebuilt-') and (f.endswith('.tar.gz') or f.endswith('.zip')) and os.path.isfile(f)]
 print('Found ' + str(len(bundles)) + ' bundle files to repack: ' + ', '.join(bundles))
 
 for bundle in bundles:
-	print('')
-	print('Unpacking ' + bundle)
-	unpackCommand = 'unzip -qq' if bundle.endswith('.zip') else 'tar -xzf'
-	os.system(unpackCommand + ' ' + bundle)
+    print('')
+    print('Unpacking ' + bundle)
+    unpackCommand = 'unzip -qq' if bundle.endswith('.zip') else 'tar -xzf'
+    os.system(unpackCommand + ' ' + bundle)
 
-	extractedDir = bundle.rstrip('.tar.gz').rstrip('.zip')
-	renamedDir = extractedDir.replace('-prebuilt-', '-')
-	print('Renaming ' + extractedDir + ' to ' + renamedDir)
-	os.rename(extractedDir, renamedDir)
+    extractedDir = bundle.rstrip('.tar.gz').rstrip('.zip')
+    renamedDir = extractedDir.replace('-prebuilt-', '-')
+    print('Renaming ' + extractedDir + ' to ' + renamedDir)
+    os.rename(extractedDir, renamedDir)
 
-	repackedBundle = renamedDir + ('.zip' if bundle.endswith('.zip') else '.tar.gz')
-	print('Packing ' + repackedBundle)
-	packCommand = 'zip -qq -r' if bundle.endswith('.zip') else 'COPYFILE_DISABLE=true tar -czf'
-	os.system(packCommand + ' ' + repackedBundle + ' ' + renamedDir)
-	shutil.rmtree(renamedDir)
+    repackedBundle = renamedDir + ('.zip' if bundle.endswith('.zip') else '.tar.gz')
+    print('Packing ' + repackedBundle)
+    packCommand = 'zip -qq -r' if bundle.endswith('.zip') else 'COPYFILE_DISABLE=true tar -czf'
+    os.system(packCommand + ' ' + repackedBundle + ' ' + renamedDir)
+    shutil.rmtree(renamedDir)
 
-	print('Calculating SHA256')
-	shaCommand = 'shasum -a 256'
-	os.system(shaCommand + ' ' + repackedBundle + ' > ' + repackedBundle + '.sha256')
+    print('Calculating SHA256')
+    shaCommand = 'shasum -a 256'
+    os.system(shaCommand + ' ' + repackedBundle + ' > ' + repackedBundle + '.sha256')
 
 print('')
 print('Done.')
