e454c824d5e85dd87dac24c14f5f5a97a84f5638
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2021-08-31 18:07:26 +0300

    [K/N] Enable mimalloc support on Windows and Mac Arm64

--- kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
+++ kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
@@ -14,9 +14,10 @@ fun KonanTarget.supportsCodeCoverage(): Boolean =
 fun KonanTarget.supportsMimallocAllocator(): Boolean =
      when(this) {
         is KonanTarget.LINUX_X64 -> true
-        is KonanTarget.MINGW_X86 -> HostManager.hostIsMingw
-        is KonanTarget.MINGW_X64 -> HostManager.hostIsMingw
+        is KonanTarget.MINGW_X86 -> true
+        is KonanTarget.MINGW_X64 -> true
         is KonanTarget.MACOS_X64 -> true
+         is KonanTarget.MACOS_ARM64 -> true
         is KonanTarget.LINUX_ARM64 -> true
         is KonanTarget.LINUX_ARM32_HFP -> true
         is KonanTarget.ANDROID_X64 -> true
