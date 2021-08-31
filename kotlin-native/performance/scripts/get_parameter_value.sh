a3158841fb3e5d47849710b06ceecd8b2342e571
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2021-08-31 15:34:47 +0300

    [K/N][perf] Added script to get and redefine compiler args on TC

---
+++ kotlin-native/performance/scripts/get_parameter_value.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ALL_PARAMS="$konanCompilerArgs"
+MEMORY_MODEL=$1
+
+if [ $MEMORY_MODEL = "experimental" ]; then
+  if [ "$ALL_PARAMS" != "" ]; then
+    ALL_PARAMS=" $ALL_PARAMS"
+  fi
+  ALL_PARAMS="-memory-model experimental$ALL_PARAMS"
+fi
+if [ "$ALL_PARAMS" != "" ]; then
+  ALL_PARAMS="-PcompilerArgs=$ALL_PARAMS"
+fi
+
+echo "##teamcity[setParameter name='env.konanCompilerArgs' value='$ALL_PARAMS']"
