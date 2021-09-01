498ed91f6dd6b1d0c2652574507415cab9415c22
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-09-01 16:47:32 +0300

    KT-36932 Add required functions for IR interpreter tests

---
+++ compiler/testData/ir/interpreter/helpers/floorDivMod.kt
@@ -0,0 +1,13 @@
+// taken from libraries/stdlib/src/kotlin/util/FloorDivMod.kt
+
+package kotlin
+
+public inline fun Int.mod(other: Int): Int {
+    val r = this % other
+    return r + (other and (((r xor other) and (r or -r)) shr 31))
+}
+
+public inline fun Long.mod(other: Long): Long {
+    val r = this % other
+    return r + (other and (((r xor other) and (r or -r)) shr 63))
+}
