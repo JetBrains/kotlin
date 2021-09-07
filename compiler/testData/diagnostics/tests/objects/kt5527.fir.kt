6e6be7c9534051a65264a1c4a147da3fe7749a07
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-09-07 13:13:21 +0300

    FIR: Adjust test data
    Currently, we've got two places where NO_COMPANION_OBJECT is reported
    and they perform it kind of differently

--- compiler/testData/diagnostics/tests/objects/kt5527.fir.kt
+++ compiler/testData/diagnostics/tests/objects/kt5527.fir.kt
@@ -9,7 +9,7 @@ fun foo() {
     val i1: Int = <!INITIALIZER_TYPE_MISMATCH!>Boo<!>
     val i2: Int = <!INITIALIZER_TYPE_MISMATCH!>A.Boo<!>
     useInt(<!ARGUMENT_TYPE_MISMATCH!>Boo<!>)
-    useInt(A.<!ARGUMENT_TYPE_MISMATCH!>Boo<!>)
+    useInt(<!ARGUMENT_TYPE_MISMATCH!>A.Boo<!>)
 }
 fun bar() {
     val i1: Int = <!INITIALIZER_TYPE_MISMATCH!>Unit<!>
