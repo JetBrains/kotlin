724194dec0fde789aa8dec589839b840848e6c00
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2021-09-08 19:48:41 +0700

    [ULC] fix testData in the plugin (related to repeatable annotation)

--- compiler/testData/asJava/lightClasses/SpecialAnnotationsOnAnnotationClass_1_6.kt
+++ compiler/testData/asJava/lightClasses/SpecialAnnotationsOnAnnotationClass_1_6.kt
@@ -1,6 +1,8 @@
 // Anno
 // JVM_TARGET: 1.6
 // FULL_JDK
+// SKIP_SANITY_TEST
+// SKIP_IDE_TEST
 
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.TYPE_PARAMETER, AnnotationTarget.TYPE)
