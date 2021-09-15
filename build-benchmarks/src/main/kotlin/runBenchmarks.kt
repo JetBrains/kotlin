2b1806c62ab16d4e8befb2f66f5624dbde1b5090
nataliya.valtman <nataliya.valtman@jetbrains.com>
2021-09-15 12:22:34 +0300

    Run tests for transformation

--- build-benchmarks/src/main/kotlin/runBenchmarks.kt
+++ build-benchmarks/src/main/kotlin/runBenchmarks.kt
@@ -3,6 +3,5 @@ import org.jetbrains.kotlin.build.benchmarks.*
 fun main() {
     mainImpl(kotlinBenchmarks(), "../.") // expected working dir is %KOTLIN_PROJECT_PATH%/build-benchmarks/
     mainImpl(abiSnapshotBenchmarks(), "../.")
-    // temporally mute benchmarks for transformation
-//    mainImpl(artifactTransformBenchmarks(), "../.")
+    mainImpl(artifactTransformBenchmarks(), "../.")
 }
