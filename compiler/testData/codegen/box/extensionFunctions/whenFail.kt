0b2a8c0c25491ff76ec51843878d5696a7d10b83
Pavel Punegov <pavel.punegov@jetbrains.com>
2021-09-20 12:16:54 +0300

    [K/N][Test] Ignore test with deprecated StringBuilder.deleteCharAt

--- compiler/testData/codegen/box/extensionFunctions/whenFail.kt
+++ compiler/testData/codegen/box/extensionFunctions/whenFail.kt
@@ -4,6 +4,7 @@
 // IGNORE_BACKEND: JS_IR_ES6
 // TODO: muted automatically, investigate should it be ran for JS or not
 // IGNORE_BACKEND: JS
+// IGNORE_BACKEND: NATIVE
 
 fun StringBuilder.takeFirst(): Char {
   if (this.length == 0) return 0.toChar()
