7c2db0e0a453397cc89848e49db3adcd3ef1fcf8
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2021-09-30 01:36:06 +0200

    ULC: do not break laziness contract when computing declaration qualified name
    ^KTIJ-19780 fixed
    
    (cherry picked from commit f90fc8a46e75f53adc8346c08984ae2d1a602025)

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/lightAnnotations.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/lightAnnotations.kt
@@ -294,7 +294,12 @@ open class KtLightNullabilityAnnotation<D : KtLightElement<*, PsiModifierListOwn
         }
 
         // don't annotate property setters
-        if (annotatedElement is KtValVarKeywordOwner && member is KtLightMethod && member.returnType == PsiType.VOID) return null
+        if (annotatedElement is KtValVarKeywordOwner
+            && member is KtLightMethod
+            && (member.originalElement as? KtPropertyAccessor)?.isSetter == true
+        ) {
+            return null
+        }
 
         if (annotatedElement is KtNamedFunction && annotatedElement.modifierList?.hasSuspendModifier() == true) {
             return Nullable::class.java.name
