17634c9ccad892aa1468a87bfb873805454b070d
Александр Райш <leidenn2509@gmail.com>
2021-09-30 02:39:31 +0700

    [FIR] update debug test

--- compiler/cli/cli-runner/testData/firTest.kt
+++ compiler/cli/cli-runner/testData/firTest.kt
@@ -5,27 +5,6 @@
 
 package org.jetbrains.kotlin.runner
 
-//open class A<T>(val a: T) {
-//    companion object
-//}
-//
-//class B<T>(a: T) : A<T>(a) {
-//
-//}
-
-//operator fun <T> A<T>.invoke(): T {
-//    return a
-//}
-
-
-//operator fun List.Companion.build(): List<Any> {
-//    return TODO()
-//}
-//
-//operator fun Set.Companion.build(): Set<Any> {
-//    return TODO()
-//}
-
 
 // ВАЖНО Посмотреть где именно ILT превратится в шорт
 //fun foo(s: Short) {
@@ -57,7 +36,7 @@ package org.jetbrains.kotlin.runner
 
 //class A {
 //    companion object {
-//        fun <T> build(size: Int, ianit: CollectionLiteralBuilder<Set<T>, T>.() -> Unit = {}): Set<T> {
+//        fun <T> build(size: Int, ianit: CollectionLiteralBuilder<A, T>.() -> Unit = {}): A {
 //            return TODO()
 //        }
 //    }
@@ -66,32 +45,32 @@ package org.jetbrains.kotlin.runner
 fun <T> Int.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<Int, T>.() -> Unit = {}) : Int {
     return TODO()
 }
-
-fun <T> Double.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<Double, T>.() -> Unit = {}) : Double {
-    return TODO()
-}
-
-//fun <T> Set.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<Set<T>, T>.() -> Unit = {}): Set<T> {
+//
+//fun <T> Double.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<Double, T>.() -> Unit = {}) : Double {
 //    return TODO()
 //}
 //
-//fun <T> List.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<List<T>, T>.() -> Unit = {}): List<T> {
-//    return TODO()
-////    return object : CollectionLiteralBuilder<List<T>, T> {
-////        private val buf = mutableSetOf<T>()
-////
-////        override fun add(element: T) {
-////            buf.add(element)
-////        }
-////
-////        override fun build(): List<T> {
-////            return buf
-////        }
-////    }.apply(init).build()
-//}
+fun <T> Set.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<Set<T>, T>.() -> Unit = {}): Set<T> {
+    return TODO()
+}
 
+fun <T> List.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<List<T>, T>.() -> Unit = {}): List<T> {
+    return TODO()
+//    return object : CollectionLiteralBuilder<List<T>, T> {
+//        private val buf = mutableSetOf<T>()
+//
+//        override fun add(element: T) {
+//            buf.add(element)
+//        }
+//
+//        override fun build(): List<T> {
+//            return buf
+//        }
+//    }.apply(init).build()
+}
+//
 interface A
-class B : A {
+open class B : A {
     companion object {
 
     }
@@ -100,21 +79,24 @@ class B : A {
 fun <T> B.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<B, T>.() -> Unit = {}): B {
     TODO()
 }
-class C : A {
+open class C : A {
     companion object {
 
     }
 }
 
-fun <T> C.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<С, T>.() -> Unit = {}): C {
+fun <T> C.Companion.buildSeq(size: Int, init: CollectionLiteralBuilder<C, T>.() -> Unit = {}): C {
     TODO()
 }
 
-fun <T: C> foo(a: T) {
+//fun <T: A, Z> foo(a: T, b: Z) {
+//
+//}
 
-}
+fun <T> foo(c: Set<T>) {}
+//fun foo(b: B) {}
 
-fun <T: A> a(): T = TODO()
+//fun <T: A> a(): T = TODO()
 //fun foo(s: Set<Int>, i: String, l: List<Int>) {
 //}
 
@@ -125,19 +107,13 @@ fun <T: A> a(): T = TODO()
 //fun <T> a(a: T): T = a
 
 fun main() {
-//    val a = A.build<Int>(1) {
-//        add(1)
-//        add(2)
-//        add(3)
-//    }
 //    val a: Set<Int> = [1, 2, 3]
 //    val a = [1, 2, 3]
 //    val a = [1, 2, 3.0]
 //    listOf(1, 2, 3)
 //    listOf(listOf(1, 2, 3), setOf(4, 5, 6))
-//    foo([1, 2, 3])
-//    foo([1, 2, 3])
-    foo(a())
+    foo([1, 2, 3])
+//    foo(a())
 //    foo([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 //    foo([listOf(), setOf()])
 //    foo([1, 2, 3], "str", [4.2, 5, 6])
