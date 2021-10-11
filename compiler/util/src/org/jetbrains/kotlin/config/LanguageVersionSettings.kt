8a99199f588462dcf1a0b4a9d75f40b5efb3cc6e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-10-11 10:43:18 +0300

    Delay ExtensionPropertyTypeParameterInDelegate forbiddance to 1.7
    Related to KT-24643

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -198,7 +198,6 @@ enum class LanguageFeature(
     TypeEnhancementImprovementsInStrictMode(KOTLIN_1_6),
     ProhibitJvmFieldOnOverrideFromInterfaceInPrimaryConstructor(KOTLIN_1_6, kind = BUG_FIX),
     PrivateInFileEffectiveVisibility(KOTLIN_1_6, kind = BUG_FIX),
-    ForbidUsingExtensionPropertyTypeParameterInDelegate(KOTLIN_1_6, kind = BUG_FIX),
     ProhibitSelfCallsInNestedObjects(KOTLIN_1_6, kind = BUG_FIX),
     ProperCheckAnnotationsTargetInTypeUsePositions(KOTLIN_1_6, kind = BUG_FIX),
 
@@ -234,6 +233,7 @@ enum class LanguageFeature(
     RefineTypeCheckingOnAssignmentsToJavaFields(KOTLIN_1_7),
     AbstractClassMemberNotImplementedWithIntermediateAbstractClass(KOTLIN_1_7, kind = BUG_FIX),
     IgnoreNullabilityForErasedValueParameters(KOTLIN_1_7, kind = BUG_FIX),
+    ForbidUsingExtensionPropertyTypeParameterInDelegate(KOTLIN_1_7, kind = BUG_FIX),
 
     // Temporarily disabled, see KT-27084/KT-22379
     SoundSmartcastFromLoopConditionForLoopAssignedVariables(sinceVersion = null, kind = BUG_FIX),
