d58b3d005ccc78938057da383fe72f3db8db4756
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2021-10-13 16:40:13 +0300

    Update K/N version to "1.6.20-dev-3759"

--- build.gradle.kts
+++ build.gradle.kts
@@ -148,7 +148,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.6.20-dev-320"
+    extra["versions.kotlin-native"] = "1.6.20-dev-3759"
 }
 
 val useJvmFir by extra(project.kotlinBuildProperties.useFir)
