dbe87a244f2ce297201eb2b724520c32d6f263f1
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2021-10-22 02:46:07 +0900

    ~ Remove CachingKtAnalysisSessionProvider.kt

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/KtFirAnalysisSessionProvider.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/KtFirAnalysisSessionProvider.kt
@@ -6,33 +6,73 @@
 package org.jetbrains.kotlin.analysis.api.fir
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.analysis.api.InvalidWayOfUsingAnalysisSession
-import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
-import org.jetbrains.kotlin.analysis.api.fir.symbols.KtFirSymbol
-import org.jetbrains.kotlin.analysis.api.impl.base.CachingKtAnalysisSessionProvider
-import org.jetbrains.kotlin.analysis.api.symbols.KtSymbol
-import org.jetbrains.kotlin.analysis.api.tokens.ValidityToken
+import com.intellij.openapi.roots.ProjectRootModificationTracker
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiModificationTracker
+import org.jetbrains.annotations.TestOnly
+import org.jetbrains.kotlin.analysis.providers.createProjectWideOutOfBlockModificationTracker
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.FirModuleResolveState
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getResolveState
+import org.jetbrains.kotlin.analysis.api.*
+import org.jetbrains.kotlin.analysis.api.fir.symbols.KtFirSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KtSymbol
+import org.jetbrains.kotlin.analysis.api.tokens.ValidityToken
+import org.jetbrains.kotlin.analysis.api.tokens.ValidityTokenFactory
 import org.jetbrains.kotlin.psi.KtElement
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.reflect.KClass
 
 @OptIn(InvalidWayOfUsingAnalysisSession::class)
-class KtFirAnalysisSessionProvider(project: Project) : CachingKtAnalysisSessionProvider<FirModuleResolveState>(project) {
-    override fun getResolveState(contextElement: KtElement): FirModuleResolveState {
-        return contextElement.getResolveState()
+class KtFirAnalysisSessionProvider(private val project: Project) : KtAnalysisSessionProvider() {
+    private val cache = KtAnalysisSessionCache<Pair<FirModuleResolveState, KClass<out ValidityToken>>>(project)
+
+    @InvalidWayOfUsingAnalysisSession
+    override fun getAnalysisSession(contextElement: KtElement, factory: ValidityTokenFactory): KtAnalysisSession {
+        val resolveState = contextElement.getResolveState()
+        return cache.getAnalysisSession(resolveState to factory.identifier) {
+            val validityToken = factory.create(project)
+            @Suppress("DEPRECATION")
+            KtFirAnalysisSession.createAnalysisSessionByResolveState(resolveState, validityToken, contextElement)
+        }
     }
 
-    override fun getResolveState(contextSymbol: KtSymbol): FirModuleResolveState {
+    override fun getAnalysisSessionBySymbol(contextSymbol: KtSymbol): KtAnalysisSession {
         require(contextSymbol is KtFirSymbol<*>)
-        return contextSymbol.firRef.resolveState
+        val resolveState = contextSymbol.firRef.resolveState
+        val token = contextSymbol.token
+        return getCachedAnalysisSession(resolveState, token)
+            ?: error("analysis session was not found for ${contextSymbol::class}, symbol.isValid=${contextSymbol.isValid()}")
     }
 
-    override fun createAnalysisSession(
-        resolveState: FirModuleResolveState,
-        validityToken: ValidityToken,
-        contextElement: KtElement
-    ): KtAnalysisSession {
-        @Suppress("DEPRECATION")
-        return KtFirAnalysisSession.createAnalysisSessionByResolveState(resolveState, validityToken, contextElement)
+    private fun getCachedAnalysisSession(resolveState: FirModuleResolveState, token: ValidityToken): KtAnalysisSession? {
+        return cache.getCachedAnalysisSession(resolveState to token::class)
     }
+
+    @TestOnly
+    override fun clearCaches() {
+        cache.clear()
+    }
+}
+
+private class KtAnalysisSessionCache<KEY : Any>(project: Project) {
+    private val cache = CachedValuesManager.getManager(project).createCachedValue {
+        CachedValueProvider.Result(
+            ConcurrentHashMap<KEY, KtAnalysisSession>(),
+            PsiModificationTracker.MODIFICATION_COUNT,
+            ProjectRootModificationTracker.getInstance(project),
+            project.createProjectWideOutOfBlockModificationTracker()
+        )
+    }
+
+    @TestOnly
+    fun clear() {
+        cache.value.clear()
+    }
+
+    inline fun getAnalysisSession(key: KEY, create: () -> KtAnalysisSession): KtAnalysisSession =
+        cache.value.getOrPut(key) { create() }
+
+    fun getCachedAnalysisSession(key: KEY): KtAnalysisSession? =
+        cache.value[key]
 }
