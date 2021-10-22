0104ab67c29e52025ad185937eed5830ffb431e5
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2021-10-22 14:22:20 +0300

    K/N new DSL: FatFramework artifact support.

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/artifact/KotlinNativeOutputFramework.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/artifact/KotlinNativeOutputFramework.kt
@@ -8,14 +8,22 @@ package org.jetbrains.kotlin.gradle.targets.native.tasks.artifact
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.plugins.BasePlugin
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.TaskProvider
 import org.jetbrains.kotlin.gradle.plugin.mpp.BitcodeEmbeddingMode
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeOutputKind
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFrameworkTaskHolder
 import org.jetbrains.kotlin.gradle.plugin.mpp.enabledOnCurrentHost
+import org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask
+import org.jetbrains.kotlin.gradle.tasks.FrameworkDescriptor
 import org.jetbrains.kotlin.gradle.tasks.dependsOn
 import org.jetbrains.kotlin.gradle.tasks.registerTask
 import org.jetbrains.kotlin.gradle.utils.lowerCamelCaseName
+import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.konan.target.presetName
 import org.jetbrains.kotlin.konan.util.visibleName
+import java.io.File
 
 class KotlinNativeFrameworkConfig {
     var embedBitcode: BitcodeEmbeddingMode? = null
@@ -46,32 +54,105 @@ private class KotlinNativeOutputFramework(
             val librariesConfigurationName = project.registerLibsDependencies(target, name, config.exportDeps)
             val exportConfigurationName = project.registerExportDependencies(target, name, config.exportDeps)
             config.modes.forEach { buildType ->
-                val targetTask = project.registerTask<KotlinNativeLinkArtifactTask>(
-                    lowerCamelCaseName("assemble", buildType.visibleName, kind.taskNameClassifier, name, target.presetName),
-                    listOf(target, kind.compilerOutputKind)
-                ) { task ->
-                    task.group = BasePlugin.BUILD_GROUP
-                    task.description = "Assemble ${kind.description} '$name' for a target '${target.name}'."
-                    task.enabled = target.enabledOnCurrentHost && kind.availableFor(target)
-
-                    task.baseName = name
-                    task.optimized = buildType.optimized
-                    task.debuggable = buildType.debuggable
-                    task.linkerOptions = config.linkerOptions
-                    task.binaryOptions = config.binaryOptions
-
-                    task.isStaticFramework = config.isStatic
-                    task.embedBitcode = frameworkConfig.embedBitcode ?: buildType.embedBitcode(target)
-
-                    task.librariesConfiguration = librariesConfigurationName
-                    task.exportLibrariesConfiguration = exportConfigurationName
-
-                    task.languageSettings(config.languageSettingsFn)
-                    task.kotlinOptions(config.kotlinOptionsFn)
-                }
+                val targetTask = project.registerLinkFrameworkTask(
+                    name,
+                    target,
+                    buildType,
+                    config,
+                    librariesConfigurationName,
+                    exportConfigurationName,
+                    frameworkConfig.embedBitcode
+                )
                 resultTask.dependsOn(targetTask)
             }
         }
+    }
+}
+
+private class KotlinNativeOutputFatFramework(
+    val frameworkConfig: KotlinNativeFrameworkConfig
+) : KotlinNativeLibraryArtifact {
+    override fun registerAssembleTask(
+        project: Project,
+        name: String,
+        config: KotlinNativeLibraryConfig
+    ) {
+        val kind = NativeOutputKind.FRAMEWORK
+
+        config.targets.firstOrNull { !kind.availableFor(it) }?.let { target ->
+            project.logger.error("Native library '${name}' wasn't configured because ${kind.description} is not available for ${target.visibleName}")
+            return
+        }
+
+        val parentTask = project.registerTask<Task>(lowerCamelCaseName("assemble", name, "FatFramework")) {
+            it.group = "build"
+            it.description = "Assemble all types of registered '$name' FatFramework"
+        }
+
+        config.modes.forEach { buildType ->
+
+            val fatTask = project.registerTask<FatFrameworkTask>(
+                lowerCamelCaseName("assemble", name, buildType.visibleName, "FatFramework")
+            ) {
+                it.baseName = name
+                val type = if (buildType.debuggable) "Debug" else "Release"
+                it.destinationDir = project.buildDir.resolve("out/fat-framework/$type")
+            }
+            parentTask.dependsOn(fatTask)
+
+            val frameworkDescriptors: List<FrameworkDescriptor> = config.targets.map { target ->
+                val librariesConfigurationName = project.registerLibsDependencies(target, name, config.exportDeps)
+                val exportConfigurationName = project.registerExportDependencies(target, name, config.exportDeps)
+                val targetTask = project.registerLinkFrameworkTask(
+                    name,
+                    target,
+                    buildType,
+                    config,
+                    librariesConfigurationName,
+                    exportConfigurationName,
+                    frameworkConfig.embedBitcode
+                )
+                fatTask.dependsOn(targetTask)
+                val frameworkFileProvider = targetTask.map { it.outputFile }
+                FrameworkDescriptor(frameworkFileProvider.get(), config.isStatic, target)
+            }
+            fatTask.configure { it.fromFrameworkDescriptors(frameworkDescriptors) }
+        }
+    }
+}
+
+private fun Project.registerLinkFrameworkTask(
+    name: String,
+    target: KonanTarget,
+    buildType: NativeBuildType,
+    config: KotlinNativeLibraryConfig,
+    librariesConfigurationName: String,
+    exportConfigurationName: String,
+    embedBitcode: BitcodeEmbeddingMode?
+): TaskProvider<KotlinNativeLinkArtifactTask> {
+    val kind = NativeOutputKind.FRAMEWORK
+    return registerTask(
+        lowerCamelCaseName("assemble", buildType.visibleName, kind.taskNameClassifier, name, target.presetName),
+        listOf(target, kind.compilerOutputKind)
+    ) { task ->
+        task.group = BasePlugin.BUILD_GROUP
+        task.description = "Assemble ${kind.description} '$name' for a target '${target.name}'."
+        task.enabled = target.enabledOnCurrentHost && kind.availableFor(target)
+
+        task.baseName = name
+        task.optimized = buildType.optimized
+        task.debuggable = buildType.debuggable
+        task.linkerOptions = config.linkerOptions
+        task.binaryOptions = config.binaryOptions
+
+        task.isStaticFramework = config.isStatic
+        task.embedBitcode = embedBitcode ?: buildType.embedBitcode(target)
+
+        task.librariesConfiguration = librariesConfigurationName
+        task.exportLibrariesConfiguration = exportConfigurationName
+
+        task.languageSettings(config.languageSettingsFn)
+        task.kotlinOptions(config.kotlinOptionsFn)
     }
 }
 
@@ -83,4 +164,13 @@ fun KotlinNativeLibraryConfig.framework(
     val frameworkConfig = KotlinNativeFrameworkConfig()
     frameworkConfig.fn()
     KotlinNativeOutputFramework(frameworkConfig)
+}
+
+val KotlinNativeLibraryConfig.fatFramework: () -> KotlinNativeLibraryArtifact get() = fatFramework {}
+fun KotlinNativeLibraryConfig.fatFramework(
+    fn: KotlinNativeFrameworkConfig.() -> Unit
+): () -> KotlinNativeLibraryArtifact = {
+    val frameworkConfig = KotlinNativeFrameworkConfig()
+    frameworkConfig.fn()
+    KotlinNativeOutputFatFramework(frameworkConfig)
 }
