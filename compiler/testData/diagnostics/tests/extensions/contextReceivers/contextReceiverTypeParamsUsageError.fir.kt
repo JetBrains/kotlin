c59ed6344e0035af35eac56cc64864e60d44971c
Anastasiya Shadrina <anastasia.shadrina@jetbrains.com>
2021-10-26 19:29:36 +0700

    [Tests] Make build green again

---
+++ compiler/testData/diagnostics/tests/extensions/contextReceivers/contextReceiverTypeParamsUsageError.fir.kt
@@ -0,0 +1,5 @@
+fun <T> f(t: @ContextFunctionTypeParams(42) T, tt: @ContextFunctionTypeParams(1) Int) {}
+
+fun test() {
+    val f: @ContextFunctionTypeParams(1) @ExtensionFunctionType (Int, String) -> Unit = {}
+}
--- compiler/testData/diagnostics/tests/extensions/contextReceivers/contextReceiverTypeParamsUsageError.kt
+++ compiler/testData/diagnostics/tests/extensions/contextReceivers/contextReceiverTypeParamsUsageError.kt
@@ -1,5 +1,3 @@
-// FIR_IDENTICAL
-
 fun <T> f(t: <!UNSUPPORTED_FEATURE!>@ContextFunctionTypeParams(42)<!> T, tt: <!UNSUPPORTED_FEATURE!>@ContextFunctionTypeParams(1)<!> Int) {}
 
 fun test() {
