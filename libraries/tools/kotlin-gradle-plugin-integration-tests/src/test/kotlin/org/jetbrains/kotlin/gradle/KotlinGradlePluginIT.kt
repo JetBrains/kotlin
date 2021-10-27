3f320dfdb15228e879d5e8eed849f178fab23d96
nataliya.valtman <nataliya.valtman@jetbrains.com>
2021-10-27 12:38:01 +0300

    Fix testBuildReportSmokeTest

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
@@ -923,11 +923,6 @@ class KotlinGradleIT : BaseGradleIT() {
             assertSuccessful()
             assertContains("Kotlin build report is written to")
         }
-
-        build("clean", "assemble", "-Pkotlin.build.report.enable=true") {
-            assertSuccessful()
-            assertContains("Kotlin build report is written to")
-        }
     }
 
     @Test
