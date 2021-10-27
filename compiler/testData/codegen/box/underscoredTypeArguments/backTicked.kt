b7a9a7d9225014a55c6d92fd1d3d0395ce10b8c5
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-10-27 15:49:59 +0300

    Make backTicked.kt test more reasonable, though real invoking the passed lambda

--- compiler/testData/codegen/box/underscoredTypeArguments/backTicked.kt
+++ compiler/testData/codegen/box/underscoredTypeArguments/backTicked.kt
@@ -1,11 +1,11 @@
 // !LANGUAGE: +PartiallySpecifiedTypeArguments
 // WITH_RUNTIME
 
-fun <K, T> foo(x: (K) -> T): Pair<K, T> = (1 as K) to (1f as T)
+fun <K, T> foo(x: (K) -> T): Pair<K, T> = 1 as K to x(1 as K)
 
 class `_` {}
 
 fun box(): String {
-    val x1 = foo<Int, `_`> { it.toFloat() as `_` } // Pair<Int, Float>
+    val x1 = foo<Int, `_`> { `_`() } // Pair<Int, Float>
     return "OK"
 }
