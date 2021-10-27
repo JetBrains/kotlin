266ad2509c153c018a60d1039e42be15387aed04
zhelenskiy <zhelenskiy2000@yandex.ru>
2021-10-28 00:34:20 +0300

    Checker rewritten according to advice
    Signed-off-by: zhelenskiy <zhelenskiy2000@yandex.ru>

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/calls/checkers/SynchronizedByValueChecker.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/calls/checkers/SynchronizedByValueChecker.kt
@@ -8,14 +8,11 @@ package org.jetbrains.kotlin.resolve.calls.checkers
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.isTopLevelInPackage
 import org.jetbrains.kotlin.diagnostics.Errors
-import org.jetbrains.kotlin.renderer.render
-import org.jetbrains.kotlin.resolve.calls.components.stableType
-import org.jetbrains.kotlin.resolve.calls.model.ExpressionKotlinCallArgument
 import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
-import org.jetbrains.kotlin.resolve.calls.tower.NewAbstractResolvedCall
+import org.jetbrains.kotlin.resolve.calls.util.getType
 import org.jetbrains.kotlin.resolve.constants.IntegerLiteralTypeConstructor
-import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 import org.jetbrains.kotlin.types.KotlinType
 
 class SynchronizedByValueChecker : CallChecker {
@@ -24,11 +21,9 @@ class SynchronizedByValueChecker : CallChecker {
             || constructor.let { manyTypes -> manyTypes is IntegerLiteralTypeConstructor && manyTypes.possibleTypes.any { it.isValueOrPrimitive() } }
 
     override fun check(resolvedCall: ResolvedCall<*>, reportOn: PsiElement, context: CallCheckerContext) {
-        if (resolvedCall.resultingDescriptor?.fqNameOrNull()?.render() != "kotlin.synchronized") return
-        val argumentMappingByOriginal = (resolvedCall as? NewAbstractResolvedCall)?.resolvedCallAtom?.argumentMappingByOriginal ?: return
-        val callArgument = argumentMappingByOriginal.entries.firstOrNull { it.key.name.asString() == "lock" }?.value ?: return
-        val lockArgument = callArgument.arguments.firstOrNull() as? ExpressionKotlinCallArgument ?: return
-        val type = lockArgument.receiver.stableType
+        if (resolvedCall.resultingDescriptor?.isTopLevelInPackage("synchronized", "kotlin") != true) return
+        val argument = resolvedCall.valueArgumentsByIndex?.get(0)?.arguments?.firstOrNull() ?: return
+        val type = argument.getArgumentExpression()?.getType(context.trace.bindingContext) ?: return
         if (type.isValueOrPrimitive()) {
             context.trace.report(Errors.FORBIDDEN_SYNCHRONIZED_BY_VALUE_CLASSES_OR_PRIMITIVES.on(reportOn, type))
         }
