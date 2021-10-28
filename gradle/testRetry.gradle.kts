b76e670ad51dad740492fe4d5e68d1b4b45d159d
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2021-10-28 14:03:32 +0300

    Allow disabling retry tests with parameters on CI

--- gradle/testRetry.gradle.kts
+++ gradle/testRetry.gradle.kts
@@ -1,9 +1,12 @@
 
 apply(plugin = "org.gradle.test-retry")
 
+val testRetryMaxRetries = findProperty("kotlin.build.testRetry.maxRetries")?.toString()?.toInt() ?:
+    (if (kotlinBuildProperties.isTeamcityBuild) 3 else 0)
+
 tasks.withType<Test>().configureEach {
     configure<org.gradle.testretry.TestRetryTaskExtension> {
-        maxRetries.set(if (kotlinBuildProperties.isTeamcityBuild) 3 else 0)
+        maxRetries.set(testRetryMaxRetries)
         maxFailures.set(20)
         failOnPassedAfterRetry.set(false)
     }
