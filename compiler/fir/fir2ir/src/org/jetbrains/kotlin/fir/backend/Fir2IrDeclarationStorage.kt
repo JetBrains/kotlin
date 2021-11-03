accd544695aacdadf0d88cc91b1a8009a70ab0e7
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2021-11-03 12:29:39 +0100

    Fix lambda detection in fir2ir for LT case

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrDeclarationStorage.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrDeclarationStorage.kt
@@ -463,7 +463,7 @@ class Fir2IrDeclarationStorage(
         containingClass: ConeClassLikeLookupTag? = null,
     ): IrSimpleFunction = convertCatching(function) {
         val simpleFunction = function as? FirSimpleFunction
-        val isLambda = function.source?.elementType == KtNodeTypes.FUNCTION_LITERAL
+        val isLambda = function.source?.elementType.let { it == KtNodeTypes.FUNCTION_LITERAL || it == KtNodeTypes.LAMBDA_EXPRESSION }
         val updatedOrigin = when {
             isLambda -> IrDeclarationOrigin.LOCAL_FUNCTION_FOR_LAMBDA
             function.symbol.callableId.isKFunctionInvoke() -> IrDeclarationOrigin.FAKE_OVERRIDE
