d5ab0382f69868ec4f171af20104d5b22c6ff454
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2021-11-09 11:47:31 +0100

    Revert "Update K/N version to "1.6.20-dev-3759""
    This reverts commit d119976b4616f47a2db385f5a55fac821589313a.

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,7 +147,7 @@ extra["versions.kotlinx-collections-immutable-jvm"] = immutablesVersion
 extra["versions.ktor-network"] = "1.0.1"
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.6.20-dev-3759"
+    extra["versions.kotlin-native"] = "1.6.20-dev-320"
 }
 
 val useJvmFir by extra(project.kotlinBuildProperties.useFir)
