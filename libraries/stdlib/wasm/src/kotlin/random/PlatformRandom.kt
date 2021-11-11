9e00a2c5b42dd57b6d835f210e9cb921a3a8333a
Igor Laevsky <igor.laevsky@jetbrains.com>
2021-11-11 19:05:53 +0300

    [Wasm] Implement platform random in stdlib

---
+++ libraries/stdlib/wasm/src/kotlin/random/PlatformRandom.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.random
+
+@JsFun("() => ((Math.random() * Math.pow(2, 32)) | 0)")
+private external fun initialSeed(): Int
+
+internal actual fun defaultPlatformRandom(): Random =
+    Random(initialSeed())
+
+internal actual fun doubleFromParts(hi26: Int, low27: Int): Double =
+    (hi26.toLong().shl(27) + low27) / (1L shl 53).toDouble()
