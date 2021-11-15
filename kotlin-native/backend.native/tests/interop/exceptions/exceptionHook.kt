c06875b8e98f701584b74be80b766c275e760a6e
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2021-11-15 11:58:21 +0300

    [K/N] Added test for exceptions behaviour in case of dynamic libs (KT-47828)

---
+++ kotlin-native/backend.native/tests/interop/exceptions/exceptionHook.kt
@@ -0,0 +1,11 @@
+import kotlin.native.concurrent.freeze
+
+// KT-47828
+fun setHookAndThrow() {
+    val hook = { _: Throwable ->
+        println("OK. Kotlin unhandled exception hook")
+    }
+    hook.freeze()
+    setUnhandledExceptionHook(hook)
+    throw Exception("Error")
+}
