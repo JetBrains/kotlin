02fe8919e501b93d7bc9e37a3661fafe686fed30
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2021-11-15 18:31:04 +0300

    FIR: Update testData for delegate inference

--- compiler/fir/analysis-tests/testData/resolve/delegates/delegateWithLambda.kt
+++ compiler/fir/analysis-tests/testData/resolve/delegates/delegateWithLambda.kt
@@ -8,9 +8,10 @@ fun <T> lazy(block: () -> T): LazyDelegate<T> = LazyDelegate(block())
 
 fun getAny(): Any? = null
 
+fun <Q> materialize(): Q = null!!
+
 class Test {
-    val x by lazy {
-        val y = getAny() as? String ?: ""
-        y
+    val x: String by lazy {
+        materialize()
     }
 }
--- compiler/testData/diagnostics/tests/delegatedProperty/inference/extensionProperty.kt
+++ compiler/testData/diagnostics/tests/delegatedProperty/inference/extensionProperty.kt
@@ -4,10 +4,10 @@ package foo
 import kotlin.reflect.KProperty
 
 open class A {
-    val B.w: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>MyProperty()<!>
+    val B.w: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!><!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>MyProperty<!>()<!>
 }
 
-val B.r: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>MyProperty()<!>
+val B.r: Int by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!><!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>MyProperty<!>()<!>
 
 val A.e: Int by MyProperty()
 
