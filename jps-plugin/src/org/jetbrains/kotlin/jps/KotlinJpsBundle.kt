f1f08582798ab27573065a824c30e7506bfc7ed8
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-11-16 18:24:40 +0100

    [CHERRY PICKED FROM IJ] Avoid adding unnecessary intellij.platform.core into JPS classpath just to be able to use DynamicBundle
    DynamicBundle functionality isn't used actually. com.intellij.AbstractBundle is enough
    
    GitOrigin-RevId: 50d30ab3d2cdc5ab8f3e1a1b449ace62e1f0f12b
    Original commit: https://github.com/JetBrains/intellij-community/commit/88ab1b0a74ceef1756d953a5ef648f45a8a7f32d

--- jps-plugin/src/org/jetbrains/kotlin/jps/KotlinJpsBundle.kt
+++ jps-plugin/src/org/jetbrains/kotlin/jps/KotlinJpsBundle.kt
@@ -1,25 +1,16 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.kotlin.jps
 
+import com.intellij.AbstractBundle
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import org.jetbrains.kotlin.util.AbstractKotlinBundle
 
 @NonNls
 private const val BUNDLE = "messages.KotlinJpsBundle"
 
-object KotlinJpsBundle : AbstractKotlinBundle(BUNDLE) {
+object KotlinJpsBundle : AbstractBundle(BUNDLE) {
     @Nls
     @JvmStatic
     fun message(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
-
-    @Nls
-    @JvmStatic
-    fun htmlMessage(@NonNls @PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String =
-        getMessage(key, *params).withHtml()
-
-    @Nls
-    @JvmStatic
-    fun lazyMessage(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): () -> String = { getMessage(key, *params) }
 }
