4d0eb74d79a614b4629949bda64a35a7117d4edc
Anastasiya Shadrina <anastasia.shadrina@jetbrains.com>
2021-11-17 17:39:49 +0700

    [Tests] Make sure desugaring works with context receivers

---
+++ compiler/testData/codegen/box/extensionFunctions/contextReceivers/arrayAccessCompositeOperators.kt
@@ -0,0 +1,33 @@
+// !LANGUAGE: +ContextReceivers
+// TARGET_BACKEND: JVM_IR
+// IGNORE_BACKEND_FIR: JVM_IR
+
+data class MyContainer(var i: Int)
+
+var operationScore = 0
+
+context(Int)
+operator fun MyContainer.get(index: Int): Int {
+    operationScore += this@Int
+    return if (index == 0) i else -1
+}
+
+context(Int)
+operator fun MyContainer.plusAssign(other: MyContainer) {
+    operationScore += this@Int
+    i += other.i
+}
+
+context(Int)
+operator fun MyContainer.inc(): MyContainer {
+    operationScore += this@Int
+    return MyContainer(i + 1)
+}
+
+fun box(): String {
+    var myContainer = MyContainer(0)
+    with(1) {
+        myContainer += MyContainer(myContainer++[0])
+    }
+    return if (myContainer.i == 1 && operationScore == 3) "OK" else "fail"
+}
---
+++ compiler/testData/codegen/box/extensionFunctions/contextReceivers/arrayAccessOperators.kt
@@ -0,0 +1,24 @@
+// !LANGUAGE: +ContextReceivers
+// TARGET_BACKEND: JVM_IR
+// IGNORE_BACKEND_FIR: JVM_IR
+
+data class MyContainer(var s: String)
+
+context(Int)
+operator fun MyContainer.get(index: Int): String? {
+    return if (index == 0 && this@Int == 42) s else null
+}
+
+context(Int)
+operator fun MyContainer.set(index: Int, value: String) {
+    if (index != 0  || this@Int != 42) return
+    s = value
+}
+
+fun box(): String {
+    return with(42) {
+        val myContainer = MyContainer("fail")
+        myContainer[0] = "OK"
+        myContainer[0] ?: "fail"
+    }
+}
