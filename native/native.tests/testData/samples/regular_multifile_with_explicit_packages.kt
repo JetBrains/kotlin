502fb96ccca4704265280fbcbbe31adce7f63add
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2021-11-18 18:18:31 +0300

    [Native][tests] Multi-file testData file sample with the same test methods in different packages

--- native/native.tests/testData/samples/regular_multifile_with_explicit_packages.kt
+++ native/native.tests/testData/samples/regular_multifile_with_explicit_packages.kt
@@ -1,4 +1,4 @@
-// FILE: addition.kt
+// FILE: main.kt
 package samples.regular_multifile_with_explicit_packages
 
 import kotlin.test.*
@@ -8,30 +8,90 @@ fun addition() {
     assertEquals(42, 40 + 2)
 }
 
-// FILE: multiplication.kt
-package samples.regular_multifile_with_explicit_packages.a
-
-import kotlin.test.*
-
 @Test
 fun multiplication () {
     assertEquals(42, 21 * 2)
 }
-
-// FILE: subtraction.kt
-package samples.regular_multifile_with_explicit_packages.b
-
-import kotlin.test.*
 
 @Test
 fun subtraction () {
     assertEquals(42, 50 - 8)
 }
 
-// FILE: division.kt
+@Test
+fun division () {
+    assertEquals(42, 126 / 3)
+}
+
+// FILE: a.kt
+package samples.regular_multifile_with_explicit_packages.a
+
+import kotlin.test.*
+
+@Test
+fun addition() {
+    assertEquals(42, 40 + 2)
+}
+
+@Test
+fun multiplication () {
+    assertEquals(42, 21 * 2)
+}
+
+@Test
+fun subtraction () {
+    assertEquals(42, 50 - 8)
+}
+
+@Test
+fun division () {
+    assertEquals(42, 126 / 3)
+}
+
+// FILE: b.kt
+package samples.regular_multifile_with_explicit_packages.b
+
+import kotlin.test.*
+
+@Test
+fun addition() {
+    assertEquals(42, 40 + 2)
+}
+
+@Test
+fun multiplication () {
+    assertEquals(42, 21 * 2)
+}
+
+@Test
+fun subtraction () {
+    assertEquals(42, 50 - 8)
+}
+
+@Test
+fun division () {
+    assertEquals(42, 126 / 3)
+}
+
+// FILE: a_b.kt
 package samples.regular_multifile_with_explicit_packages.a.b
 
 import kotlin.test.*
+
+@Test
+fun addition() {
+    assertEquals(42, 40 + 2)
+}
+
+@Test
+fun multiplication () {
+    assertEquals(42, 21 * 2)
+}
+
+@Test
+fun subtraction () {
+    assertEquals(42, 50 - 8)
+}
 
 @Test
 fun division () {
