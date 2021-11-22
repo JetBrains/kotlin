13c8531f3a7c19200d08a9040e4caf5205f404a9
Anastasiya Shadrina <anastasia.shadrina@jetbrains.com>
2021-11-22 18:48:04 +0700

    [Tests] Regenerate NativeExtBlackBoxTestGenerated.kt

--- native/native.tests/tests-gen/org/jetbrains/kotlin/konan/blackboxtest/NativeExtBlackBoxTestGenerated.java
+++ native/native.tests/tests-gen/org/jetbrains/kotlin/konan/blackboxtest/NativeExtBlackBoxTestGenerated.java
@@ -16118,7 +16118,7 @@ public class NativeExtBlackBoxTestGenerated extends AbstractNativeBlackBoxTest {
         @Nested
         @TestMetadata("compiler/testData/codegen/box/extensionClasses")
         @TestDataPath("$PROJECT_ROOT")
-        @CustomNativeBlackBoxTestCaseGroupProvider(ExtTestCaseGroupProvider.class)
+        @NativeBlackBoxTestCaseGroupProvider(ExtTestCaseGroupProvider.class)
         public class ExtensionClasses {
             @Test
             public void testAllFilesPresentInExtensionClasses() throws Exception {
@@ -16355,7 +16355,7 @@ public class NativeExtBlackBoxTestGenerated extends AbstractNativeBlackBoxTest {
             @Nested
             @TestMetadata("compiler/testData/codegen/box/extensionFunctions/contextReceivers")
             @TestDataPath("$PROJECT_ROOT")
-            @CustomNativeBlackBoxTestCaseGroupProvider(ExtTestCaseGroupProvider.class)
+            @NativeBlackBoxTestCaseGroupProvider(ExtTestCaseGroupProvider.class)
             public class ContextReceivers {
                 @Test
                 public void testAllFilesPresentInContextReceivers() throws Exception {
@@ -16491,7 +16491,7 @@ public class NativeExtBlackBoxTestGenerated extends AbstractNativeBlackBoxTest {
                 @Nested
                 @TestMetadata("compiler/testData/codegen/box/extensionFunctions/contextReceivers/fromKEEP")
                 @TestDataPath("$PROJECT_ROOT")
-                @CustomNativeBlackBoxTestCaseGroupProvider(ExtTestCaseGroupProvider.class)
+                @NativeBlackBoxTestCaseGroupProvider(ExtTestCaseGroupProvider.class)
                 public class FromKEEP {
                     @Test
                     public void testAllFilesPresentInFromKEEP() throws Exception {
