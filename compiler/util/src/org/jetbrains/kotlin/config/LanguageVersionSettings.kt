9a9a8f199978da30b9bb3a8f9848389c95357b82
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-11-23 19:32:48 +0100

    Restore support of language version 1.3
     #KT-50251 Fixed
    
    (cherry picked from commit a0004ed03adc9f50f26c8b11d178afcc2abf2a9c)

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -394,15 +394,15 @@ enum class LanguageVersion(val major: Int, val minor: Int) : DescriptionAware, L
             str.split(".", "-").let { if (it.size >= 2) fromVersionString("${it[0]}.${it[1]}") else null }
 
         // Version status
-        //            1.0  1.1  1.2   1.3  1.4           1.5  1.6     1.7  1.8
-        // Language:  UNSUPPORTED -------> DEPRECATED -> STABLE ---> EXPERIMENTAL
+        //            1.0  1.1  1.2   1.3  1.4           1.5  1.6    1.7 1.8 1.9
+        // Language:  UNSUPPORTED --> DEPRECATED ------> STABLE ---> EXPERIMENTAL
         // API:       UNSUPPORTED --> DEPRECATED ------> STABLE ---> EXPERIMENTAL
 
         @JvmField
         val FIRST_API_SUPPORTED = KOTLIN_1_3
 
         @JvmField
-        val FIRST_SUPPORTED = KOTLIN_1_4
+        val FIRST_SUPPORTED = KOTLIN_1_3
 
         @JvmField
         val FIRST_NON_DEPRECATED = KOTLIN_1_5
