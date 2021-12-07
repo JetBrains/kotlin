7b9db5b920540088e4e50811d9551e30533664ea
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2021-12-07 18:05:37 +0100

    KT-50106 Add gradle property with global K/N linker flags

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeLibraryDslIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeLibraryDslIT.kt
@@ -77,6 +77,40 @@ class NativeLibraryDslIT : BaseGradleIT() {
     }
 
     @Test
+    fun `link shared library from single gradle module with custom link args`() {
+        with(Project("new-kn-library-dsl")) {
+            setupWorkingDir()
+            gradleProperties().appendText("\nkotlin.native.linkArgs=-Xfoo=bar -Xbaz=qux")
+            gradleBuildScript("shared").appendText("""
+                kotlinArtifact("car", Library) {
+                    target = LINUX_X64
+                    kotlinOptions {
+                        freeCompilerArgs += "-Xmen=pool"
+                    }
+                }
+            """.trimIndent())
+            build(":shared:assembleCarDebugSharedLibraryLinuxX64") {
+                assertSuccessful()
+                assertTasksExecuted(
+                    ":shared:compileKotlinLinuxX64",
+                    ":shared:assembleCarDebugSharedLibraryLinuxX64"
+                )
+                assertTasksNotExecuted(
+                    ":lib:compileKotlinLinuxX64"
+                )
+                assertContains("-Xfoo=bar")
+                assertContains("-Xbaz=qux")
+                assertContains("-Xmen=pool")
+                assertContains("w: Flag is not supported by this version of the compiler: -Xfoo=bar")
+                assertContains("w: Flag is not supported by this version of the compiler: -Xbaz=qux")
+                assertContains("w: Flag is not supported by this version of the compiler: -Xmen=pool")
+                assertFileExists("/shared/build/out/dynamic/linux_x64/debug/libcar.so")
+                assertFileExists("/shared/build/out/dynamic/linux_x64/debug/libcar_api.h")
+            }
+        }
+    }
+
+    @Test
     fun `link release XCFramework from two gradle modules`() {
         Assume.assumeTrue(HostManager.hostIsMac)
         with(Project("new-kn-library-dsl")) {
