3d886c70c924d523790af0f0ccd9fbea4c9b633b
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2021-12-10 01:03:55 +0100

    Support generic underlying type of inline class in FIR
     #KT-32162

--- compiler/testData/diagnostics/tests/inlineClasses/reifiedGenericUnderlyingType.fir.kt
+++ compiler/testData/diagnostics/tests/inlineClasses/reifiedGenericUnderlyingType.fir.kt
@@ -1,4 +1,4 @@
 // !LANGUAGE: +InlineClasses, -JvmInlineValueClasses
 // SKIP_TXT
 
-inline class ICAny<reified T>(val value: T)
+inline class ICAny<<!REIFIED_TYPE_PARAMETER_NO_INLINE!>reified<!> T>(val value: <!VALUE_CLASS_HAS_INAPPLICABLE_PARAMETER_TYPE!>T<!>)
