18743189549b23890e11d2e685082670ff0761c5
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2021-12-13 14:41:23 +0300

    [Tests] Windows regeneration does not change tests

--- generators/test-generator/tests/org/jetbrains/kotlin/generators/NewTestGeneratorImpl.kt
+++ generators/test-generator/tests/org/jetbrains/kotlin/generators/NewTestGeneratorImpl.kt
@@ -293,6 +293,8 @@ object NewTestGeneratorImpl : TestGenerator(METHOD_GENERATORS) {
 
     private fun TestClassModel.predefinedNativeTransformers(recursive: Boolean): List<Pair<String, String>> =
         methods.mapNotNull { method ->
-            (method as? TransformingTestMethodModel)?.takeIf { it.registerInConstructor }?.let { it.source.file.path to it.transformer }
-        } + if (recursive) innerTestClasses.flatMap { it.predefinedNativeTransformers(recursive) } else listOf()
+            (method as? TransformingTestMethodModel)
+                ?.takeIf { it.registerInConstructor }
+                ?.let { it.source.file.invariantSeparatorsPath to it.transformer }
+        } + if (recursive) innerTestClasses.flatMap { it.predefinedNativeTransformers(true) } else listOf()
 }
