e36cc87b195a9cc7d28ae4d6d1a5139a16b8e54f
Jinseong Jeon <jsjeon@google.com>
2021-12-14 14:10:45 -0800

    LC: introduce KotlinLightTypeParameterBuilder (w/ origin)

---
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/elements/KotlinLightTypeParameterBuilder.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.asJava.elements
+
+import com.intellij.psi.PsiTypeParameterListOwner
+import com.intellij.psi.impl.light.LightTypeParameterBuilder
+import org.jetbrains.kotlin.psi.KtTypeParameter
+
+open class KotlinLightTypeParameterBuilder(
+    name: String,
+    owner: PsiTypeParameterListOwner,
+    index: Int,
+    override val origin: KtTypeParameter
+) : LightTypeParameterBuilder(name, owner, index),
+    PsiElementWithOrigin<KtTypeParameter>
