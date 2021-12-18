760c496000f4679fd1b4ac71f928715e1b658eb3
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-12-18 09:18:04 +0100

    [MPP][Test] Enable 'SKIP_TXT' default directive for AbstractDiagnosticsWithMultiplatformCompositeAnalysisTest
    ^KT-50120

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/runners/AbstractDiagnosticsWithMultiplatformCompositeAnalysisTest.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/runners/AbstractDiagnosticsWithMultiplatformCompositeAnalysisTest.kt
@@ -6,15 +6,19 @@
 package org.jetbrains.kotlin.test.runners
 
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.directives.DiagnosticsDirectives.SKIP_TXT
 import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
+import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives.LANGUAGE
 import org.jetbrains.kotlin.test.directives.MultiplatformDiagnosticsDirectives
+import org.jetbrains.kotlin.test.directives.MultiplatformDiagnosticsDirectives.MULTIPLATFORM_COMPOSITE_ANALYSIS_MODE_ENABLED
 
 abstract class AbstractDiagnosticsWithMultiplatformCompositeAnalysisTest : AbstractDiagnosticTest() {
     override fun configure(builder: TestConfigurationBuilder) {
         super.configure(builder)
         builder.defaultDirectives {
-            +MultiplatformDiagnosticsDirectives.MULTIPLATFORM_COMPOSITE_ANALYSIS_MODE_ENABLED
-            LanguageSettingsDirectives.LANGUAGE with "+MultiPlatformProjects"
+            +MULTIPLATFORM_COMPOSITE_ANALYSIS_MODE_ENABLED
+            +SKIP_TXT
+            LANGUAGE with "+MultiPlatformProjects"
         }
     }
 }
