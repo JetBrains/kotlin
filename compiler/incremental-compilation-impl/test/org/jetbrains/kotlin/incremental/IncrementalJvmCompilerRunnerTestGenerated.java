2a4964c908cbcedf1628069a074c943cdc3e1568
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-12-20 15:04:41 +0100

    Regenerate Kotlin JPS plugin tests after cherry-picking its commits from intellij repository

--- compiler/incremental-compilation-impl/test/org/jetbrains/kotlin/incremental/IncrementalJvmCompilerRunnerTestGenerated.java
+++ compiler/incremental-compilation-impl/test/org/jetbrains/kotlin/incremental/IncrementalJvmCompilerRunnerTestGenerated.java
@@ -2060,6 +2060,11 @@ public class IncrementalJvmCompilerRunnerTestGenerated extends AbstractIncrement
                 runTest("jps-plugin/testData/incremental/withJava/other/multifileClassRemoved/");
             }
 
+            @TestMetadata("multifileDependantUsage")
+            public void testMultifileDependantUsage() throws Exception {
+                runTest("jps-plugin/testData/incremental/withJava/other/multifileDependantUsage/");
+            }
+
             @TestMetadata("multifilePackagePartMethodAdded")
             public void testMultifilePackagePartMethodAdded() throws Exception {
                 runTest("jps-plugin/testData/incremental/withJava/other/multifilePackagePartMethodAdded/");
@@ -2375,6 +2380,19 @@ public class IncrementalJvmCompilerRunnerTestGenerated extends AbstractIncrement
 
                 public void testAllFilesPresentInMultifileClassRemoved() throws Exception {
                     KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("jps-plugin/testData/incremental/withJava/other/multifileClassRemoved"), Pattern.compile("^([^\\.]+)$"), null, TargetBackend.JVM_IR, true);
+                }
+            }
+
+            @TestMetadata("jps-plugin/testData/incremental/withJava/other/multifileDependantUsage")
+            @TestDataPath("$PROJECT_ROOT")
+            @RunWith(JUnit3RunnerWithInners.class)
+            public static class MultifileDependantUsage extends AbstractIncrementalJvmCompilerRunnerTest {
+                private void runTest(String testDataFilePath) throws Exception {
+                    KotlinTestUtils.runTest(this::doTest, TargetBackend.JVM_IR, testDataFilePath);
+                }
+
+                public void testAllFilesPresentInMultifileDependantUsage() throws Exception {
+                    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("jps-plugin/testData/incremental/withJava/other/multifileDependantUsage"), Pattern.compile("^([^\\.]+)$"), null, TargetBackend.JVM_IR, true);
                 }
             }
 
