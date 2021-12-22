b164e20f6934a9bc065efa6790fff9907bb66c54
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-12-22 16:12:28 +0100

    Attach JPS plugin modules to the project model

--- prepare/ide-plugin-dependencies/kotlin-jps-plugin-classpath/build.gradle.kts
+++ prepare/ide-plugin-dependencies/kotlin-jps-plugin-classpath/build.gradle.kts
@@ -2,7 +2,7 @@ idePluginDependency {
     @Suppress("UNCHECKED_CAST")
     val compilerComponents = rootProject.extra["compilerModulesForJps"] as List<String>
 
-    val otherProjects = listOf(":kotlin-daemon-client", ":jps-plugin", ":idea:idea-jps-common", ":kotlin-reflect")
+    val otherProjects = listOf(":kotlin-daemon-client", ":jps:jps-plugin", ":jps:jps-common", ":kotlin-reflect")
 
     publishProjectJars(compilerComponents + otherProjects, libraryDependencies = listOf(protobufFull()))
 }
