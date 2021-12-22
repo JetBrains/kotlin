ec2f24722c544d5be6390ef7ab37a6d5241e8a46
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-12-22 18:22:20 +0100

    [MPP] allModulesProvidingActualsFor: Support non-hmpp ide case
    ^KT-50156
    
    (cherry picked from commit ba201a249ec63d23ec527928b042fb5032fa4e55)

--- compiler/resolution/src/org/jetbrains/kotlin/resolve/multiplatform/moduleFilterUtils.kt
+++ compiler/resolution/src/org/jetbrains/kotlin/resolve/multiplatform/moduleFilterUtils.kt
@@ -19,8 +19,28 @@ fun allModulesProvidingExpectsFor(platformModule: ModuleDescriptor): ModuleFilte
     it == platformModule || it in platformModule.allExpectedByModules
 }
 
-fun allModulesProvidingActualsFor(commonModule: ModuleDescriptor): ModuleFilter = {
-    it == commonModule || commonModule in it.allExpectedByModules
+/**
+ * @param commonModule: The module for which the allowed modules can provide actuals for.
+ * Meaning that all allowed modules will have declared a dependsOn edge to said [commonModule]
+ *
+ * @param platformModule: This parameter is only required to support pre-hmpp IDE
+ * consumers. In this mode, leaf/platform modules will get wrapped using PlatformModuleInfo which
+ * will be one [ModuleDescriptor] which wrap all dependsOn sources as well.
+ * This module will not declare 'proper' [ModuleDescriptor.allExpectedByModules] to the common module
+ * and therefore has to be passed into this filter as well to manually check if an actual was provided by this module.
+ * This parameter can be dropped and removed once 'non-hmpp' mode shall not be supported anymore.
+ */
+fun allModulesProvidingActualsFor(
+    commonModule: ModuleDescriptor,
+    platformModule: ModuleDescriptor
+): ModuleFilter = { module ->
+    when {
+        module == commonModule -> true
+        commonModule in module.allExpectedByModules -> true
+        /* Support non-hmpp IDE case */
+        module == platformModule -> true
+        else -> false
+    }
 }
 
 fun <T : DeclarationDescriptor> Iterable<T>.applyFilter(filter: ModuleFilter): List<T> = filter { filter(it.module) }
