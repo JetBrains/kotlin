2c51799e0a824aeac1d5625a9e15cfa1c01e0965
Victor Petukhov <victor.petukhov@jetbrains.com>
2021-12-23 10:27:26 +0300

    Revert "Temporary workaround for failing tests on teamcity."
    This reverts commit 2436ca3d8ba849c968ee22a7cbd8185274f3995c.

--- core/compiler.common.jvm/src/org/jetbrains/kotlin/load/java/JavaNullabilityAnnotationSettings.kt
+++ core/compiler.common.jvm/src/org/jetbrains/kotlin/load/java/JavaNullabilityAnnotationSettings.kt
@@ -72,7 +72,7 @@ fun getDefaultReportLevelForAnnotation(annotationFqName: FqName) =
 fun getReportLevelForAnnotation(
     annotation: FqName,
     configuredReportLevels: NullabilityAnnotationStates<ReportLevel>,
-    configuredKotlinVersion: KotlinVersion = KotlinVersion(1, 6, 20)
+    configuredKotlinVersion: KotlinVersion = KotlinVersion.CURRENT
 ): ReportLevel {
     configuredReportLevels[annotation]?.let { return it }
 
