1a5de89cbab2b7525f30cfe042296f05a429ef16
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-12-23 22:14:23 +0100

    Kotlin JPS plugin: fix paths in tests

--- jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/IncrementalConstantSearchTest.kt
+++ jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/IncrementalConstantSearchTest.kt
@@ -18,26 +18,26 @@ package org.jetbrains.kotlin.jps.build
 
 class IncrementalConstantSearchTest : AbstractIncrementalJpsTest() {
     fun testJavaConstantChangedUsedInKotlin() {
-        doTest("jps-plugin/testData/incremental/custom/javaConstantChangedUsedInKotlin/")
+        doTest("jps/jps-plugin/testData/incremental/custom/javaConstantChangedUsedInKotlin/")
     }
 
     fun testJavaConstantUnchangedUsedInKotlin() {
-        doTest("jps-plugin/testData/incremental/custom/javaConstantUnchangedUsedInKotlin/")
+        doTest("jps/jps-plugin/testData/incremental/custom/javaConstantUnchangedUsedInKotlin/")
     }
 
     fun testKotlinConstantChangedUsedInJava() {
-        doTest("jps-plugin/testData/incremental/custom/kotlinConstantChangedUsedInJava/")
+        doTest("jps/jps-plugin/testData/incremental/custom/kotlinConstantChangedUsedInJava/")
     }
 
     fun testKotlinJvmFieldChangedUsedInJava() {
-        doTest("jps-plugin/testData/incremental/custom/kotlinJvmFieldChangedUsedInJava/")
+        doTest("jps/jps-plugin/testData/incremental/custom/kotlinJvmFieldChangedUsedInJava/")
     }
 
     fun testKotlinConstantUnchangedUsedInJava() {
-        doTest("jps-plugin/testData/incremental/custom/kotlinConstantUnchangedUsedInJava/")
+        doTest("jps/jps-plugin/testData/incremental/custom/kotlinConstantUnchangedUsedInJava/")
     }
 
     fun testKotlinJvmFieldUnchangedUsedInJava() {
-        doTest("jps-plugin/testData/incremental/custom/kotlinJvmFieldUnchangedUsedInJava/")
+        doTest("jps/jps-plugin/testData/incremental/custom/kotlinJvmFieldUnchangedUsedInJava/")
     }
 }
