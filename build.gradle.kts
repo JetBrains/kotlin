adb1eb2c06240b66a2be58a8e484a01257e6e077
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-12-23 22:14:23 +0100

    5/5 Kotlin JPS plugin: post rename fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -805,7 +805,7 @@ tasks {
 
     register("jps-tests") {
         dependsOn("dist")
-        dependsOn(":jps-plugin:test")
+        dependsOn(":jps:jps-plugin:test")
     }
 
     register("idea-plugin-additional-tests") {
