e9eb5834baa4c02ddaa9547dee17b161b308064b
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2021-12-23 01:48:25 +0100

    Move AbstractJsScriptlikeCodeAnalyser to the kotlin-scripting-compiler-js module
    (cherry picked from commit 61526c9700b63e08696e9364d87b8f25931582b7)

--- plugins/scripting/scripting-compiler-js/src/org/jetbrains/kotlin/scripting/js/AbstractJsScriptlikeCodeAnalyser.kt
+++ plugins/scripting/scripting-compiler-js/src/org/jetbrains/kotlin/scripting/js/AbstractJsScriptlikeCodeAnalyser.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.scripting.compiler.plugin.impl
+package org.jetbrains.kotlin.scripting.js
 
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
