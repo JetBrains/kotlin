81666f4d7c7bb2ba147aab53dec3a4d9271255bf
Nikita Bobko <nikita.bobko@jetbrains.com>
2021-12-29 18:47:28 +0100

    Adopt Kotlin JPS plugin modules Gradle model to kotlin/master
    Recenlty, Kotlin repository started using Gradle 7.1.
    Deprecated `compile`/`testCompile` Gradle API were dropped in that release

--- jps/jps-plugin/build.gradle.kts
+++ jps/jps-plugin/build.gradle.kts
@@ -15,48 +15,53 @@ val generateTests by generator("org.jetbrains.kotlin.jps.GenerateJpsPluginTestsK
 }
 
 dependencies {
-    compile(project(":kotlin-build-common"))
-    compile(project(":core:descriptors"))
-    compile(project(":core:descriptors.jvm"))
-    compile(project(":kotlin-compiler-runner"))
-    compile(project(":daemon-common"))
-    compile(project(":daemon-common-new"))
-    compile(projectRuntimeJar(":kotlin-daemon-client"))
-    compile(projectRuntimeJar(":kotlin-daemon"))
+    api(project(":kotlin-build-common"))
+    api(project(":core:descriptors"))
+    api(project(":core:descriptors.jvm"))
+    api(project(":kotlin-compiler-runner-unshaded"))
+    api(project(":kotlin-compiler-runner"))
+    api(project(":daemon-common"))
+    api(project(":daemon-common-new"))
+    api(project(":kotlin-daemon-client"))
+    api(project(":kotlin-daemon"))
     testImplementation(projectTests(":generators:test-generator"))
-    compile(project(":compiler:frontend.java"))
-    compile(project(":js:js.frontend"))
-    compile(projectRuntimeJar(":kotlin-preloader"))
-    compile(project(":jps:jps-common"))
-    compileOnly(intellijDep()) {
-        includeJars("jdom", "trove4j", "jps-model", "platform-api", "util", "asm-all", rootProject = rootProject)
+    api(project(":compiler:frontend.java"))
+    api(project(":js:js.frontend"))
+    api(project(":kotlin-preloader"))
+    api(project(":jps:jps-common"))
+    compileOnly(commonDependency("org.jetbrains.intellij.deps.fastutil:intellij-deps-fastutil"))
+    compileOnly(intellijCore())
+    compileOnly(jpsModel())
+    compileOnly(jpsModelImpl())
+    compileOnly(jpsBuild())
+    compileOnly(jpsModelSerialization())
+    testApi(jpsModel())
+    testApi(testFramework()) {
+        exclude("com.jetbrains.rd", "rd-core")
+        exclude("com.jetbrains.rd", "rd-framework")
+        exclude("com.jetbrains.rd", "rd-swing")
     }
-    compileOnly(jpsStandalone()) { includeJars("jps-builders", "jps-builders-6") }
     testCompileOnly(project(":kotlin-reflect-api"))
-    testCompile(project(":compiler:incremental-compilation-impl"))
-    testCompile(projectTests(":compiler:tests-common"))
-    testCompile(projectTests(":compiler:incremental-compilation-impl"))
-    testCompile(commonDep("junit:junit"))
-    testCompile(project(":kotlin-test:kotlin-test-jvm"))
-    testCompile(projectTests(":kotlin-build-common"))
+    testApi(project(":compiler:incremental-compilation-impl"))
+    testApi(projectTests(":compiler:tests-common"))
+    testApi(projectTests(":compiler:incremental-compilation-impl"))
+    testApi(commonDependency("junit:junit"))
+    testApi(project(":kotlin-test:kotlin-test-jvm"))
+    testApi(projectTests(":kotlin-build-common"))
     testApi(projectTests(":compiler:test-infrastructure-utils"))
-    testCompileOnly(jpsStandalone()) { includeJars("jps-builders", "jps-builders-6") }
-    Ide.IJ {
-        testCompile(intellijDep("devkit"))
-    }
+    testCompileOnly(jpsBuild())
+    testApi(devKitJps())
 
-    testCompile(intellijDep())
+    testApi(intellijCore())
 
-    testCompile(jpsBuildTest())
+    testApi(jpsBuildTest())
     compilerModules.forEach {
-        testRuntime(project(it))
+        testRuntimeOnly(project(it))
     }
-
-    testRuntimeOnly(intellijPluginDep("java"))
 
     testRuntimeOnly(toolsJar())
-    testRuntime(project(":kotlin-reflect"))
-    testRuntime(project(":kotlin-script-runtime"))
+    testRuntimeOnly(project(":kotlin-reflect"))
+    testRuntimeOnly(project(":kotlin-script-runtime"))
 }
 
 sourceSets {
