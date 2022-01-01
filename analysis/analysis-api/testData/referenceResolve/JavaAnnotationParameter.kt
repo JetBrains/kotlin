5e42be98497fa00777765ac72b37fbcff559f90d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-01-01 20:54:43 +0300

    Analysis API: fix redundant caret symbol in testdata

--- analysis/analysis-api/testData/referenceResolve/JavaAnnotationParameter.kt
+++ analysis/analysis-api/testData/referenceResolve/JavaAnnotationParameter.kt
@@ -1,4 +1,4 @@
-@java.lang.annotation.Re<caret>tention(va<caret>lue = java.lang.annotation.RetentionPolicy.RUNTIME)
+@java.lang.annotation.Retention(va<caret>lue = java.lang.annotation.RetentionPolicy.RUNTIME)
 annotation class Anno()
 
 
