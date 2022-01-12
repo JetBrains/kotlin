56207abc274f21298238ff7e70cc35c6868aeb21
nataliya.valtman <nataliya.valtman@jetbrains.com>
2022-01-12 15:17:35 +0300

    KT-34862: Add kotlin build dir as output
    build history files are non cacheable outputs
    #KT-34862

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/BuildCacheIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/BuildCacheIT.kt
@@ -22,6 +22,7 @@ import org.jetbrains.kotlin.gradle.testbase.*
 import org.junit.jupiter.api.DisplayName
 import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.readText
+import kotlin.io.path.relativeTo
 import kotlin.io.path.writeText
 
 @ExperimentalPathApi
@@ -147,4 +148,29 @@ class BuildCacheIT : KGPBaseTest() {
             }
         }
     }
+
+    //doesn't work for build history files approach
+    @DisplayName("Restore from build cache should not break incremental compilation")
+    @GradleTest
+    fun testIncrementalCompilationAfterCacheHit(gradleVersion: GradleVersion) {
+        val withSnapshotProperty = "-Dkotlin.incremental.classpath.snapshot.enabled=true"
+        project("incrementalMultiproject", gradleVersion) {
+            enableLocalBuildCache(localBuildCacheDir)
+            build("assemble", withSnapshotProperty)
+            build("clean", "assemble", withSnapshotProperty) {
+                assertTasksFromCache(":lib:compileKotlin")
+                assertTasksFromCache(":app:compileKotlin")
+            }
+            val bKtSourceFile = projectPath.resolve("lib/src/main/kotlin/bar/B.kt")
+
+            bKtSourceFile.modify { it.replace("fun b() {}", "fun b() {}\nfun b2() {}") }
+
+            build("assemble", withSnapshotProperty, buildOptions = defaultBuildOptions.copy(logLevel = LogLevel.DEBUG)) {
+                assertOutputDoesNotContain("[KOTLIN] [IC] Non-incremental compilation will be performed")
+                assertCompiledKotlinSources(setOf(bKtSourceFile).map { it.relativeTo(projectPath)}, output)
+            }
+
+        }
+    }
+
 }
