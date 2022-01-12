a55d066eac349f92775bfd1e08ae2a7c56667fda
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2022-01-12 12:25:44 +0100

    Return parcelize-runtime to the parcelize-compiler-plugin-for-ide.jar
    Classes like kotlinx.parcelize.Parcelize still used in the Android
    plugin in the IDEA, so they should be included into plugin.
    Otherwise, we have issues like KTIJ-20220.
    
    #KTIJ-20220 In progress
    
    (cherry picked from commit 088d4891412bd05390c0659c24e693e7f30eef00)

--- prepare/ide-plugin-dependencies/parcelize-compiler-plugin-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/parcelize-compiler-plugin-for-ide/build.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     kotlin("jvm")
 }
 
-publishJarsForIde(listOf(":plugins:parcelize:parcelize-compiler"))
+publishJarsForIde(listOf(":plugins:parcelize:parcelize-compiler", ":plugins:parcelize:parcelize-runtime"))
