6448d59c62e85d1787a3591c2c99e7213bf55bc0
Anastasia.Shadrina <anastasia.shadrina@jetbrains.com>
2022-01-21 16:37:37 +0700

    [Tests] Add CompileKotlinAgainstCustomBinaries test for CR

---
+++ compiler/testData/compileKotlinAgainstCustomBinaries/contextualDeclarationUse/library/source.kt
@@ -0,0 +1,19 @@
+package test
+
+context(Int) val p get() = 42
+
+context(Int)
+class A {
+    context(Int) val p get() = 42
+
+    context(Int) fun m() {}
+}
+
+context(String)
+fun f() {
+    println(this@String)
+}
+
+fun ordinary() {
+    println("I do not require context receivers!")
+}
