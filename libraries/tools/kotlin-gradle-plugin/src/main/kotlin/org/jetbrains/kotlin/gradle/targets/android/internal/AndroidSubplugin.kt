a796eec7bd43d79b4501e3297c7954480fa85576
Mads Ager <ager@google.com>
2022-01-24 15:26:14 +0100

    Support `namespace` Android Gradle Plugin BaseExtension value.
    This is in addition to supporting the `package` attribute in
    AndroidManifest.xml.
    
    In AGP 7 developers can use `namespace` instead of the manifest
    attribute. In AGP 8 it will no longer be possible to use the `package`
    attribute in AndroidManifest.xml.
    
    ^KT-50887 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/android/internal/AndroidSubplugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/targets/android/internal/AndroidSubplugin.kt
@@ -115,7 +115,7 @@ class AndroidSubplugin :
 
         val mainSourceSet = sourceSets.getByName("main")
         val manifestFile = mainSourceSet.manifest.srcFile
-        val applicationPackage = getApplicationPackageFromManifest(manifestFile) ?: run {
+        val applicationPackage = getApplicationPackage(androidExtension, manifestFile) ?: run {
             project.logger.warn(
                 "Application package name is not present in the manifest file (${manifestFile.absolutePath})"
             )
@@ -181,7 +181,7 @@ class AndroidSubplugin :
         )
 
         val mainSourceSet = androidExtension.sourceSets.getByName("main")
-        pluginOptions += SubpluginOption("package", getApplicationPackage(project, mainSourceSet))
+        pluginOptions += SubpluginOption("package", getApplicationPackage(androidExtension, project, mainSourceSet))
 
         fun addVariant(name: String, resDirectories: FileCollection) {
             val optionValue = lazy {
@@ -260,9 +260,9 @@ class AndroidSubplugin :
         return project.files(Callable { lazyFiles.value })
     }
 
-    private fun getApplicationPackage(project: Project, mainSourceSet: AndroidSourceSet): String {
+    private fun getApplicationPackage(androidExtension: BaseExtension, project: Project, mainSourceSet: AndroidSourceSet): String {
         val manifestFile = mainSourceSet.manifest.srcFile
-        val applicationPackage = getApplicationPackageFromManifest(manifestFile)
+        val applicationPackage = getApplicationPackage(androidExtension, manifestFile)
 
         if (applicationPackage == null) {
             project.logger.warn(
@@ -276,7 +276,34 @@ class AndroidSubplugin :
         }
     }
 
-    private fun getApplicationPackageFromManifest(manifestFile: File): String? {
+    private fun getApplicationPackage(androidExtension: BaseExtension, manifestFile: File): String? {
+        // Starting AGP 7 the package can be set via the DSL namespace value:
+        //
+        // android {
+        //   namespace "com.example"
+        // }
+        //
+        // instead of via the "package" attribute in the manifest file.
+        //
+        // Starting AGP 8, the package *must* be set via the DSL and the manifest file
+        // attribute cannot be used.
+        //
+        // See https://issuetracker.google.com/issues/172361895
+        //
+        // Therefore, we try to get the package from there first. Since we support AGP versions
+        // prior to AGP 7, we need to reflectively find and call it.
+        try {
+            val method = androidExtension.javaClass.getDeclaredMethod("getNamespace")
+            val result = method.invoke(androidExtension)
+            if (result is String && result.isNotEmpty()) {
+                return result
+            }
+        } catch (e: ReflectiveOperationException) {
+            // Ignore and try parsing manifest.
+        }
+
+        // Didn't find the namespace getter, or it was not set. Try parsing the
+        // manifest to find the "package" attribute from there.
         try {
             return manifestFile.parseXml().documentElement.getAttribute("package")
         } catch (e: Exception) {
