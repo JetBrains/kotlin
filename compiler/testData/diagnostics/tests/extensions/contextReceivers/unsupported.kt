e33d5fccdfb6d1d38c3ecea8c85de90cb750c96c
Anastasia.Shadrina <anastasia.shadrina@jetbrains.com>
2022-01-24 11:07:28 +0000

    [FE] KT-50878 Prohibit using contextual declarations without `-Xcontext-receivers`
    Merge-request: KT-MR-5549
    Merged-by: Anastasia Shadrina <Anastasia.Shadrina@jetbrains.com>

--- compiler/testData/diagnostics/tests/extensions/contextReceivers/unsupported.kt
+++ compiler/testData/diagnostics/tests/extensions/contextReceivers/unsupported.kt
@@ -5,6 +5,17 @@ fun f(g: <!UNSUPPORTED_FEATURE!>context(Any)<!> () -> Unit, value: Any): <!UNSUP
     return value as (<!UNSUPPORTED_FEATURE!>context(A)<!> () -> Unit)
 }
 
+fun f(g: () -> Unit, value: Any) : () -> Unit {
+    return g
+}
+
+<!UNSUPPORTED_FEATURE!>context(Any)<!>
+fun sameAsFWithoutNonContextualCounterpart(g: () -> Unit, value: Any) : () -> Unit {
+    return g
+}
+
+<!UNSUPPORTED_FEATURE!>context(Any)<!> val p get() = 42
+
 <!UNSUPPORTED_FEATURE!>context(String, Int)<!>
 class A {
     <!UNSUPPORTED_FEATURE!>context(Any)<!>
@@ -12,4 +23,17 @@ class A {
 
     <!UNSUPPORTED_FEATURE!>context(String, Int)<!>
     fun m() {}
+}
+
+fun useWithContextReceivers() {
+    with(42) {
+        with("") {
+            f({}, 42)
+            <!UNSUPPORTED_CONTEXTUAL_DECLARATION_CALL!>sameAsFWithoutNonContextualCounterpart({}, 42)<!>
+            <!UNSUPPORTED_CONTEXTUAL_DECLARATION_CALL!>p<!>
+            val a = <!UNSUPPORTED_CONTEXTUAL_DECLARATION_CALL!>A()<!>
+            a.<!UNSUPPORTED_CONTEXTUAL_DECLARATION_CALL!>p<!>
+            a.<!UNSUPPORTED_CONTEXTUAL_DECLARATION_CALL!>m()<!>
+        }
+    }
 }
