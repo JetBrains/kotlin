ebf345a9f56532641c195a0fe4c7e04e34f80cda
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2022-01-24 12:12:00 +0100

    Add AbstractDecoysLowering and make all decoys lowerings extend it
    This CL aligns the decoy lowerings with kotlin 1.6.0 changes to properly calculate signatures.
    Decoy transformations rely on a proper signature calculation.
    AbstractDecoysLowering implements the common logic needed for all decoy lowerings involving signature calculations.
    
    This change is only for k/js targets (decoys are not used for jvm and native).
    
    Test: tested in JB fork
    Change-Id: Ifb68d5288d67444ee12fc3def2bc3fc0e7560ae0 ( https://android-review.git.corp.google.com/q/Ifb68d5288d67444ee12fc3def2bc3fc0e7560ae0 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/decoys/CreateDecoysTransformer.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/decoys/CreateDecoysTransformer.kt
@@ -18,7 +18,6 @@ package androidx.compose.compiler.plugins.kotlin.lower.decoys
 
 import androidx.compose.compiler.plugins.kotlin.ComposeFqNames
 import androidx.compose.compiler.plugins.kotlin.ModuleMetrics
-import androidx.compose.compiler.plugins.kotlin.lower.AbstractComposeLowering
 import androidx.compose.compiler.plugins.kotlin.lower.ModuleLoweringPass
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.ir.addChild
@@ -92,16 +91,16 @@ class CreateDecoysTransformer(
     pluginContext: IrPluginContext,
     symbolRemapper: DeepCopySymbolRemapper,
     bindingTrace: BindingTrace,
-    override val signatureBuilder: IdSignatureSerializer,
+    signatureBuilder: IdSignatureSerializer,
     metrics: ModuleMetrics,
-) : AbstractComposeLowering(
-    context = pluginContext,
+) : AbstractDecoysLowering(
+    pluginContext = pluginContext,
     symbolRemapper = symbolRemapper,
     bindingTrace = bindingTrace,
-    metrics = metrics
-),
-    ModuleLoweringPass,
-    DecoyTransformBase {
+    metrics = metrics,
+    signatureBuilder = signatureBuilder
+), ModuleLoweringPass {
+
     private val originalFunctions: MutableMap<IrFunction, IrDeclarationParent> = mutableMapOf()
 
     private val decoyAnnotation by lazy {
