ece0f83ab22f19ef8fa7f806c47f34d7b6997cd7
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-01-25 13:03:23 +0100

    [WASM] Implement std kotlin.time

--- libraries/stdlib/wasm/src/kotlin/time/TimeSources.kt
+++ libraries/stdlib/wasm/src/kotlin/time/TimeSources.kt
@@ -1,11 +1,29 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+
 package kotlin.time
+
+import kotlin.wasm.internal.ExternalInterfaceType
+
+@JsFun("() => typeof globalThis !== 'undefined' && typeof globalThis.performance !== 'undefined' ? globalThis.performance : null")
+private external fun tryGetPerformance(): ExternalInterfaceType?
+
+@JsFun("(performance) => performance.now()")
+private external fun getPerformanceNow(performance: ExternalInterfaceType): Double
+
+@JsFun("() => Date.now()")
+private external fun dateNow(): Double
 
 @SinceKotlin("1.3")
 @ExperimentalTime
-internal actual object MonotonicTimeSource : TimeSource {
-    override fun markNow(): TimeMark = TODO("Wasm stdlib: MonotonicTimeSource::markNow")
+internal actual object MonotonicTimeSource : TimeSource, AbstractDoubleTimeSource(unit = DurationUnit.MILLISECONDS) {
+    private val performance: ExternalInterfaceType? = tryGetPerformance()
+
+    override fun read(): Double =
+        if (performance != null) getPerformanceNow(performance) else dateNow()
+
+    override fun toString(): String =
+        if (performance != null) "TimeSource(globalThis.performance.now())" else "TimeSource(Date.now())"
 }
