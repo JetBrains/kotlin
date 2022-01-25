5147a9e42d4a2d986b20f73bc653ea052d69f844
Mads Ager <ager@google.com>
2022-01-25 13:45:32 +0100

    Remove code that was migrated to android studio repo.

--- plugins/android-extensions/android-extensions-idea/testData/android/parcel/checker/simple.kt
+++
@@ -1,7 +0,0 @@
-package test
-
-import kotlinx.android.parcel.Parcelize
-import android.os.Parcelable
-
-@Parcelize
-class User(val firstName: String, val secondName: String, val age: Int) : Parcelable
