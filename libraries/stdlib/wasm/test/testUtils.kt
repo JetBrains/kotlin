5ed104063d5a07c6a6054c340d55fb17a7799b53
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-01-26 17:57:50 +0100

    [WASM] Implement missing test asserter

--- libraries/stdlib/wasm/test/testUtils.kt
+++ libraries/stdlib/wasm/test/testUtils.kt
@@ -6,10 +6,10 @@
 package test
 
 import kotlin.test.*
+import kotlin.reflect.qualifiedOrSimpleName
 
 public actual fun assertTypeEquals(expected: Any?, actual: Any?) {
-    TODO("Implement class references")
-    //assertEquals(expected?.let { it::class.js }, actual?.let { it::class.js })
+    assertEquals(expected?.let { it::class }, actual?.let { it::class })
 }
 
 @Suppress("NOTHING_TO_INLINE")
