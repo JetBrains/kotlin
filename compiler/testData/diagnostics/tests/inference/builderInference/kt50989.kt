60e0806b4ab6110925d6664bce3c1d990b296299
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-01-27 11:33:31 +0300

    [FE 1.0] Don't try to report specialized resolution ambiguity error for builder inference if the corresponding substitutor is empty
    ^KT-50989 Fixed

---
+++ compiler/testData/diagnostics/tests/inference/builderInference/kt50989.kt
@@ -0,0 +1,7 @@
+// WITH_STDLIB
+
+fun main() {
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>buildList<!> {
+        <!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE, OVERLOAD_RESOLUTION_AMBIGUITY!>println<!>(get(0))
+    }
+}
