570019a82137e5e229b3ac052afc6245250305f5
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-01-28 13:14:27 +0300

    [FE 1.0] Fix mistakes in error messages for resolution ambiguity with stub types
    ^KT-51022 Fixed

--- compiler/frontend/src/org/jetbrains/kotlin/diagnostics/rendering/DefaultErrorMessages.java
+++ compiler/frontend/src/org/jetbrains/kotlin/diagnostics/rendering/DefaultErrorMessages.java
@@ -919,8 +919,8 @@ public class DefaultErrorMessages {
                                                                      "Cannot choose which overloaded function `{2}` to call. " +
                                                                      "To disambiguate this call, either use an explicit type cast for a parameter or a receiver (see specific errors on them) or specify the type `{1}` explicitly.",
                 STRING, STRING, STRING);
-        MAP.put(STUB_TYPE_IN_ARGUMENT_CAUSES_AMBIGUITY, "The type of an argument hasn't been inferred yet. To disambiguate this call, explicitly cast it to `{1}` if you want the builder's type parameter(s) `{2}` to be inferred to `{3}`.", RENDER_TYPE, STRING, STRING);
-        MAP.put(STUB_TYPE_IN_RECEIVER_CAUSES_AMBIGUITY, "The type of a receiver hasn't been inferred yet. To disambiguate this call, explicitly cast it to `{1}` if you want the builder's type parameter(s) `{2}` to be inferred to `{3}`.", RENDER_TYPE, STRING, STRING, null);
+        MAP.put(STUB_TYPE_IN_ARGUMENT_CAUSES_AMBIGUITY, "The type of an argument hasn''t been inferred yet. To disambiguate this call, explicitly cast it to `{0}` if you want the builder''s type parameter(s) `{1}` to be inferred to `{2}`.", RENDER_TYPE, STRING, STRING);
+        MAP.put(STUB_TYPE_IN_RECEIVER_CAUSES_AMBIGUITY, "The type of a receiver hasn''t been inferred yet. To disambiguate this call, explicitly cast it to `{0}` if you want the builder''s type parameter(s) `{1}` to be inferred to `{2}`.", RENDER_TYPE, STRING, STRING, null);
         MAP.put(NONE_APPLICABLE, "None of the following functions can be called with the arguments supplied: {0}", AMBIGUOUS_CALLS);
         MAP.put(CANNOT_COMPLETE_RESOLVE, "Cannot choose among the following candidates without completing type inference: {0}", AMBIGUOUS_CALLS);
         MAP.put(UNRESOLVED_REFERENCE_WRONG_RECEIVER, "Unresolved reference. None of the following candidates is applicable because of receiver type mismatch: {0}", AMBIGUOUS_CALLS);
