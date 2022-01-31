f5895d319dc64e45a456b3a908bfb13c3644236a
Sergey Igushkin <sergey.igushkin@jetbrains.com>
2022-02-01 00:41:49 +0400

    KPM: print fragment names in the error from illegal refines(...) calls

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/KotlinGradleFragment.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/KotlinGradleFragment.kt
@@ -70,7 +70,9 @@ open class KotlinGradleFragmentInternal @Inject constructor(
     }
 
     private fun checkCanRefine(other: KotlinGradleFragment) {
-        check(containingModule == other.containingModule) { "Fragments can only refine each other within one module." }
+        check(containingModule == other.containingModule) {
+            "Fragments can only refine each other within one module. Can't make $this refine $other"
+        }
     }
 
     override fun dependencies(configure: KotlinDependencyHandler.() -> Unit): Unit =
