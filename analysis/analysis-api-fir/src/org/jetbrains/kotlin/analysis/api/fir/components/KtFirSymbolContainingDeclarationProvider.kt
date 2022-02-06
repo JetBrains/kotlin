52eb31b8197fd7732a9d537ad3e2399a5c068e98
Roman Golyshev <roman.golyshev@jetbrains.com>
2022-02-06 12:32:04 +0300

    FIR IDE: Add the simplest support for `PLUGIN` KtDeclarationOrigin

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KtFirSymbolContainingDeclarationProvider.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KtFirSymbolContainingDeclarationProvider.kt
@@ -58,6 +58,7 @@ internal class KtFirSymbolContainingDeclarationProvider(
                 KtSymbolOrigin.PROPERTY_BACKING_FIELD -> getContainingDeclarationForBackingFieldSymbol(symbol)
                 KtSymbolOrigin.INTERSECTION_OVERRIDE -> TODO()
                 KtSymbolOrigin.SAM_CONSTRUCTOR -> null
+                KtSymbolOrigin.PLUGIN -> TODO("Containing declaration is requested for ${ DebugSymbolRenderer.render(symbol) }")
                 KtSymbolOrigin.DELEGATED -> TODO()
             }
             else -> null
