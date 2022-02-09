73b45a12545092e27f58524f2f3060c8cf894490
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2022-02-09 17:14:23 +0300

    [FIR] Report UNSUPPORTED on array literals not from annotation classes ^KT-50750 Fixed

--- compiler/testData/diagnostics/tests/annotations/arrayLiteralInAnnotationCompanion_before.fir.kt
+++ compiler/testData/diagnostics/tests/annotations/arrayLiteralInAnnotationCompanion_before.fir.kt
@@ -3,46 +3,46 @@
 
 annotation class Ann(val x: IntArray = [1, 2, 3]) { // OK
     companion object {
-        val y1: IntArray = [1, 2, 3] // Error
+        val y1: IntArray = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
         val z1: IntArray
-            get() = [1, 2, 3] // Error
+            get() = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
         fun test_1(): IntArray {
-            return [1, 2, 3] // Error
+            return <!UNSUPPORTED!>[1, 2, 3]<!> // Error
         }
 
         class Nested {
-            val y2: IntArray = [1, 2, 3] // Error
+            val y2: IntArray = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
             val z2: IntArray
-                get() = [1, 2, 3] // Error
+                get() = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
             fun test_2(): IntArray {
-                return [1, 2, 3] // Error
+                return <!UNSUPPORTED!>[1, 2, 3]<!> // Error
             }
         }
     }
 
     object Foo {
-        val y3: IntArray = [1, 2, 3] // Error
+        val y3: IntArray = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
         val z3: IntArray
-            get() = [1, 2, 3] // Error
+            get() = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
         fun test_3(): IntArray {
-            return [1, 2, 3] // Error
+            return <!UNSUPPORTED!>[1, 2, 3]<!> // Error
         }
     }
 
     class Nested {
-        val y4: IntArray = [1, 2, 3] // Error
+        val y4: IntArray = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
         val z4: IntArray
-            get() = [1, 2, 3] // Error
+            get() = <!UNSUPPORTED!>[1, 2, 3]<!> // Error
 
         fun test_4(): IntArray {
-            return [1, 2, 3] // Error
+            return <!UNSUPPORTED!>[1, 2, 3]<!> // Error
         }
     }
 }
--- compiler/testData/diagnostics/tests/regressions/resolveCollectionLiteralInsideLambda.fir.kt
+++ compiler/testData/diagnostics/tests/regressions/resolveCollectionLiteralInsideLambda.fir.kt
@@ -3,5 +3,5 @@
 fun foo(l: () -> Unit) {}
 fun bar(l: () -> String) {}
 
-val a = foo { [] }
-val b = bar { [] }
+val a = foo { <!UNSUPPORTED!>[]<!> }
+val b = bar { <!UNSUPPORTED!>[]<!> }
