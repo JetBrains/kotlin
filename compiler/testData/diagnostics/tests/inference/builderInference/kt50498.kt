1df9fea542228403103e8e996a9028368742a05f
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-02-09 11:36:10 +0300

    Add test for KT-50498

---
+++ compiler/testData/diagnostics/tests/inference/builderInference/kt50498.kt
@@ -0,0 +1,11 @@
+// FIR_IDENTICAL
+fun <T, R> baz(body: (List<R>) -> T): T = TODO()
+
+fun test3() {
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>baz<!> {
+        true
+    }
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>baz<!> { x ->
+        true
+    }
+}
