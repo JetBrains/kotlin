5bd1cad0c70e963b46f33072cbc0201da9805177
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2022-02-10 17:36:19 +0300

    Native: fix cinterop for Swift-produced Objective-C categories
    For some reason, libclang's indexer doesn't index categories with
    __attribute__((external_source_symbol(language="Swift",...))),
    so we have to additionally enumerate them explicitly.
    
    ^KT-49455 Fixed

---
+++ kotlin-native/backend.native/tests/interop/objc/tests/kt49455.h
@@ -0,0 +1,17 @@
+#import <Foundation/NSObject.h>
+
+// https://youtrack.jetbrains.com/issue/KT-49455
+
+@interface KT49455 : NSObject
+@end;
+
+__attribute__((external_source_symbol(language="Swift", defined_in="sample",generated_declaration)))
+@interface KT49455 (KT49455Ext)
+- (int)extensionFunction;
+@end
+
+// Just to ensure that unavailable categories don't break anything.
+__attribute__((unavailable("unavailableExtensionFunction is unavailable")))
+@interface KT49455 (KT49455UnavailableExt)
+- (int)unavailableExtensionFunction;
+@end
---
+++ kotlin-native/backend.native/tests/interop/objc/tests/kt49455.m
@@ -0,0 +1,10 @@
+#import "kt49455.h"
+
+@implementation KT49455
+@end;
+
+@implementation KT49455 (KT49455Ext)
+- (int)extensionFunction {
+    return 111;
+}
+@end
