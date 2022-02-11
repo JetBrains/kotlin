faba9c4272eb820b6e93e02e027436be672439cf
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-02-11 10:23:16 +0300

    [CLI] Don't register classpath roots twice
    There are two ways how content roots are registered in compiler configuration
      in JVM CLI compiler:
    1. Directly from arguments
    2. From Module, which build from arguments
    
    And there was a problem that both ways used at the same time in some
      circumstances (regular compilation without .xml module file) which
      caused duplication of all content roots.
    
    Ideal solution for this problem is removal of Module usages at all,
      because it looks like redundant abstraction which just complicate
      things, but it's too scary to remove it, because it hove some none
      trivial usages inside compiler. So to fix problem with duplicated
      roots this commit just removes registration of roots from arguments
      if Module is used

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/K2JVMCompiler.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/K2JVMCompiler.kt
@@ -74,13 +74,18 @@ class K2JVMCompiler : CLICompiler<K2JVMCompilerArguments>() {
         val moduleName = arguments.moduleName ?: JvmProtoBufUtil.DEFAULT_MODULE_NAME
         configuration.put(CommonConfigurationKeys.MODULE_NAME, moduleName)
 
-        configuration.configureExplicitContentRoots(arguments)
+        configuration.configureJavaModulesContentRoots(arguments)
         configuration.configureStandardLibs(paths, arguments)
         configuration.configureAdvancedJvmOptions(arguments)
         configuration.configureKlibPaths(arguments)
 
-        if (arguments.buildFile == null && !arguments.version  && !arguments.allowNoSourceFiles &&
-            (arguments.script || arguments.expression != null || arguments.freeArgs.isEmpty())) {
+        if (
+            arguments.buildFile == null &&
+            !arguments.version &&
+            !arguments.allowNoSourceFiles &&
+            (arguments.script || arguments.expression != null || arguments.freeArgs.isEmpty())
+        ) {
+            configuration.configureContentRootsFromClassPath(arguments)
 
             // script or repl
             if (arguments.script && arguments.freeArgs.isEmpty()) {
@@ -131,7 +136,7 @@ class K2JVMCompiler : CLICompiler<K2JVMCompilerArguments>() {
                 if (arguments.javaPackagePrefix != null) {
                     strongWarning("The '-Xjava-package-prefix' option is ignored because '-Xbuild-file' is specified")
                 }
-
+                configuration.configureContentRootsFromClassPath(arguments)
                 val sanitizedCollector = FilteringMessageCollector(messageCollector, VERBOSE::contains)
                 configuration.put(JVMConfigurationKeys.MODULE_XML_FILE, buildFile)
                 CompileEnvironmentUtil.loadModuleChunk(buildFile, sanitizedCollector)
