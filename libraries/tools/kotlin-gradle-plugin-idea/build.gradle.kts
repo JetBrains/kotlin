113e363b223787263f88e3b288f9c88e5d647226
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-02-13 09:40:56 +0100

    [KPM] :kotlin-gradle-plugin-idea: Set language- and apiVersion to 1.4
    ^KT-51262

--- libraries/tools/kotlin-gradle-plugin-idea/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-idea/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     kotlin("jvm")
 }
 
+kotlin {
+    sourceSets.configureEach {
+        languageSettings.languageVersion = "1.4"
+        languageSettings.apiVersion = "1.4"
+    }
+}
+
 dependencies {
     implementation(kotlinStdlib())
 }
