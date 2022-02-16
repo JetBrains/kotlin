c116565c0e459292e8b9bdb3942c50f48114a581
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-02-16 15:45:18 +0300

    [FIR] Allow named arguments for calls of expect functions

--- compiler/testData/diagnostics/tests/multiplatform/namedArguments.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/namedArguments.fir.kt
@@ -10,10 +10,10 @@ expect class Foo(zzz: Int) {
 expect fun f2(xxx: Int)
 
 fun testCommon() {
-    Foo(<!NAMED_ARGUMENTS_NOT_ALLOWED!>zzz<!> = 0)
-    val f = Foo(<!NAMED_ARGUMENTS_NOT_ALLOWED!>aaa<!> = true)
-    f.f1(<!NAMED_ARGUMENTS_NOT_ALLOWED!>xxx<!> = "")
-    f2(<!NAMED_ARGUMENTS_NOT_ALLOWED!>xxx<!> = 42)
+    Foo(zzz = 0)
+    val f = Foo(aaa = true)
+    f.f1(xxx = "")
+    f2(xxx = 42)
 }
 
 // MODULE: m2-jvm()()(m1-common)
