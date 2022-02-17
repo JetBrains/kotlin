a39e0db2887e6d20ae629c1f0251c518cb0276df
Andrzej Ratajczak <32793002+BarkingBad@users.noreply.github.com>
2022-02-17 16:27:28 +0100

    [KDM] Refactor Ancestry Graphs
    See https://github.com/Kotlin/dokka/pull/2326
    Original commit: https://github.com/Kotlin/dokka/commit/9a1434d583b931671e2c5e9c5275af725938d503

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/translators/isException.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/translators/isException.kt
@@ -1,6 +1,15 @@
 package org.jetbrains.dokka.base.translators
 
 import org.jetbrains.dokka.links.DRI
+import org.jetbrains.dokka.model.AncestryNode
+import org.jetbrains.dokka.model.TypeConstructor
+
+internal fun AncestryNode.typeConstructorsBeingExceptions(): List<TypeConstructor> {
+    fun traverseSupertypes(ancestry: AncestryNode): List<TypeConstructor> =
+        listOf(ancestry.typeConstructor) + (ancestry.superclass?.let(::traverseSupertypes) ?: emptyList())
+
+    return superclass?.let(::traverseSupertypes)?.filter { type -> type.dri.isDirectlyAnException() } ?: emptyList()
+}
 
 internal fun DRI.isDirectlyAnException(): Boolean =
     toString().let { stringed ->
