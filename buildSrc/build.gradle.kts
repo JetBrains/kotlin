3ad4f1919b20d83214c1bb895287e05ecd5aa563
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-02-18 11:10:14 +0300

    [Build] Update version of kotlin-build-gradle-plugin to 0.0.35

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,7 +22,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.32")
+        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.35")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.bootstrapKotlinVersion}")
         classpath("org.jetbrains.kotlin:kotlin-sam-with-receiver:${project.bootstrapKotlinVersion}")
     }
@@ -149,7 +149,7 @@ java {
 dependencies {
     implementation(kotlin("stdlib", embeddedKotlinVersion))
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.bootstrapKotlinVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.32")
+    implementation("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.35")
     implementation("com.gradle.publish:plugin-publish-plugin:0.14.0")
 
     implementation("net.rubygrapefruit:native-platform:${property("versions.native-platform")}")
--- native/commonizer-api/build.gradle.kts
+++ native/commonizer-api/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.utils.NativeCompilerDownloader
 buildscript {
     val cacheRedirectorEnabled = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() ?: false
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.26")
+        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.35")
     }
     repositories {
         if (cacheRedirectorEnabled)
