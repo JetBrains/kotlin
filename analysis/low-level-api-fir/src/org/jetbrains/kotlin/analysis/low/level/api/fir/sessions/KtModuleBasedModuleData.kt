7ca2a33667dbfb7677e915a53d24249c26e9fc3e
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-02-18 15:40:18 +0300

    [LL] Add proper equals and hashCode for KtModuleBasedModuleData

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/KtModuleBasedModuleData.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/KtModuleBasedModuleData.kt
@@ -31,4 +31,19 @@ internal class KtModuleBasedModuleData(
     override val platform: TargetPlatform get() = module.platform
 
     override val analyzerServices: PlatformDependentAnalyzerServices get() = module.analyzerServices
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as KtModuleBasedModuleData
+
+        if (module != other.module) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return module.hashCode()
+    }
 }
