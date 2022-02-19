0074e0209cf42ae09e223c915b21bac1102882a9
Ivan Gavrilovic <gavra@google.com>
2022-02-19 15:06:35 +0000

    KT-41456: Incremental KAPT - check compiled sources before running incrementally
    Because incremental KAPT tries to reuse .class files produced by kotlinc
    and javac, it should check for their existence before starting
    an incremental run. Otherwise, annotation processors that perform type
    validation will fail to run.
    
    Current check counts the number of declared types in processed .java
    sources, and it makes sure the total number of .class files in compiled
    sources dirs is equal or higher. Otherwise, KAPT runs non-incrementally.
    
    Tests: KaptIncrementalWithIsolatingApt.testMissingKotlinOutputForcesNonIncrementalRun
    
    ^KT-41456 In Progress

--- plugins/kapt3/kapt3-base/test/org/jetbrains/kotlin/kapt3/base/incremental/TestSimpleIncrementalAptCache.kt
+++ plugins/kapt3/kapt3-base/test/org/jetbrains/kotlin/kapt3/base/incremental/TestSimpleIncrementalAptCache.kt
@@ -5,10 +5,7 @@
 
 package org.jetbrains.kotlin.kapt.base.test.org.jetbrains.kotlin.kapt3.base.incremental
 
-import org.jetbrains.kotlin.kapt3.base.incremental.IncrementalProcessor
-import org.jetbrains.kotlin.kapt3.base.incremental.JavaClassCacheManager
-import org.jetbrains.kotlin.kapt3.base.incremental.MentionedTypesTaskListener
-import org.jetbrains.kotlin.kapt3.base.incremental.SourcesToReprocess
+import org.jetbrains.kotlin.kapt3.base.incremental.*
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -24,11 +21,13 @@ class TestSimpleIncrementalAptCache {
 
     private lateinit var cache: JavaClassCacheManager
     private lateinit var generatedSources: File
+    private lateinit var compiledSources: List<File>
 
     @Before
     fun setUp() {
         cache = JavaClassCacheManager(tmp.newFolder())
         generatedSources = tmp.newFolder()
+        compiledSources = listOf(tmp.newFolder().also { it.resolve(TEST_PACKAGE_NAME).mkdir() })
         cache.close()
     }
 
@@ -36,7 +35,11 @@ class TestSimpleIncrementalAptCache {
     fun testAggregatingAnnotations() {
         runProcessor(SimpleProcessor().toAggregating())
 
-        val dirtyFiles = cache.invalidateAndGetDirtyFiles(listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile), emptyList()) as SourcesToReprocess.Incremental
+        val dirtyFiles = cache.invalidateAndGetDirtyFiles(
+            listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile),
+            emptyList(),
+            compiledSources
+        ) as SourcesToReprocess.Incremental
         assertEquals(
             listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile, TEST_DATA_DIR.resolve("Address.java").absoluteFile),
             dirtyFiles.toReprocess
@@ -49,7 +52,11 @@ class TestSimpleIncrementalAptCache {
     fun testIsolatingAnnotations() {
         runProcessor(SimpleProcessor().toIsolating())
 
-        val dirtyFiles = cache.invalidateAndGetDirtyFiles(listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile), emptyList()) as SourcesToReprocess.Incremental
+        val dirtyFiles = cache.invalidateAndGetDirtyFiles(
+            listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile),
+            emptyList(),
+            compiledSources
+        ) as SourcesToReprocess.Incremental
         assertFalse(generatedSources.resolve("test/UserGenerated.java").exists())
         assertEquals(
             listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile),
@@ -61,7 +68,7 @@ class TestSimpleIncrementalAptCache {
     fun testNonIncremental() {
         runProcessor(SimpleProcessor().toNonIncremental())
 
-        val dirtyFiles = cache.invalidateAndGetDirtyFiles(listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile), emptyList())
+        val dirtyFiles = cache.invalidateAndGetDirtyFiles(listOf(TEST_DATA_DIR.resolve("User.java").absoluteFile), emptyList(), compiledSources)
         assertTrue(dirtyFiles is SourcesToReprocess.FullRebuild)
     }
 
@@ -73,5 +80,14 @@ class TestSimpleIncrementalAptCache {
             generatedSources
         ) { elementUtils, trees -> MentionedTypesTaskListener(cache.javaCache, elementUtils, trees) }
         cache.updateCache(listOf(processor), false)
+
+        // add mock compiled source files
+        compiledSources.single().resolve("test/User.class").createNewFile()
+        compiledSources.single().resolve("test/Address.class").createNewFile()
+        compiledSources.single().resolve("test/Observable.class").createNewFile()
+        compiledSources.single().resolve("test/UserGenerated.class").createNewFile()
+        compiledSources.single().resolve("test/AddressGenerated.class").createNewFile()
     }
 }
+
+private const val TEST_PACKAGE_NAME = "test"
