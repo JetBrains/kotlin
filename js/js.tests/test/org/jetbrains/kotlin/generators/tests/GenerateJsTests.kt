f4764cdf2ebda86ab236a55eb8471852706226af
Nikolay Lunyak <lunyak.kolya@mail.ru>
2022-02-24 17:34:44 +0300

    [FIR JS] Add a Fir2IrJsText runner

--- js/js.tests/test/org/jetbrains/kotlin/generators/tests/GenerateJsTests.kt
+++ js/js.tests/test/org/jetbrains/kotlin/generators/tests/GenerateJsTests.kt
@@ -17,6 +17,7 @@ import org.jetbrains.kotlin.js.testOld.wasm.semantics.AbstractIrCodegenBoxWasmTe
 import org.jetbrains.kotlin.js.testOld.wasm.semantics.AbstractIrCodegenWasmJsInteropWasmTest
 import org.jetbrains.kotlin.js.testOld.wasm.semantics.AbstractJsTranslatorWasmTest
 import org.jetbrains.kotlin.test.TargetBackend
+import org.jetbrains.kotlin.test.runners.ir.AbstractFir2IrJsTextTest
 
 fun main(args: Array<String>) {
     System.setProperty("java.awt.headless", "true")
@@ -151,6 +152,12 @@ fun main(args: Array<String>) {
 
             testClass<AbstractIrCodegenWasmJsInteropJsTest> {
                 model("codegen/boxWasmJsInterop")
+            }
+
+            testClass<AbstractFir2IrJsTextTest>(
+                suiteTestClassName = "Fir2IrJsTextTestGenerated"
+            ) {
+                model("ir/irJsText")
             }
         }
     }
