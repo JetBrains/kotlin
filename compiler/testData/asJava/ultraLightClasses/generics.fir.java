4a5aee688e84e3e8ffe2c309af34d17e4535b91e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-02-25 10:27:28 +0100

    FIR LC: fix invalid type param upper bound

--- compiler/testData/asJava/ultraLightClasses/generics.fir.java
+++ compiler/testData/asJava/ultraLightClasses/generics.fir.java
@@ -52,7 +52,7 @@ public abstract class C /* C*/<T>  {
 
   public  C(@org.jetbrains.annotations.NotNull() java.util.List<? extends java.lang.CharSequence>);//  .ctor(java.util.List<? extends java.lang.CharSequence>)
 
-  public final <V, U> T foo(V, @org.jetbrains.annotations.NotNull() C<V>, error.NonExistentClass);// <V, U>  foo(V, C<V>, error.NonExistentClass)
+  public final <V, U extends V> T foo(V, @org.jetbrains.annotations.NotNull() C<V>, error.NonExistentClass);// <V, U extends V>  foo(V, C<V>, error.NonExistentClass)
 
   public final Q getW(Q);//  getW(Q)
 
