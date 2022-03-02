f81e49f2104ffef7e4800f75f05782e6125d0dec
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-03-02 21:57:28 +0100

    Remove sourceFileExtensions input
    This and new KotlinCompile task inputs dedicated for scripting
    allowed providing proper Kotlin scripts compilation support without
    eager configuration dependencies resolve.
    
    ^KT-32805 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/kotlinCompilations.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/kotlinCompilations.kt
@@ -164,7 +164,11 @@ internal fun addSourcesToKotlinCompileTask(
         // In this call, the super-implementation of `source` adds the directories files to the roots of the union file tree,
         // so it's OK to pass just the source roots.
         setSource(Callable(sources))
-        sourceFilesExtensions.addAll(sourceFileExtensions)
+        with(sourceFileExtensions.toSet()) {
+            if (isNotEmpty()) {
+                include(flatMap { ext -> ext.fileExtensionCasePermutations().map { "**/*.$it" } })
+            }
+        }
 
         // The `commonSourceSet` is passed to the compiler as-is, converted with toList
         commonSourceSet.from(
