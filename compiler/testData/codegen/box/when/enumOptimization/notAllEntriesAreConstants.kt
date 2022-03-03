507a6ed706ee64bbcbed8134a4d79c4a75709182
Xin Wang <scaventz@outlook.com>
2022-03-03 12:44:44 +0800

    Don't transform irBranch if tableswitch can't be generated
    #KT-34466 Fixed

---
+++ compiler/testData/codegen/box/when/enumOptimization/notAllEntriesAreConstants.kt
@@ -0,0 +1,22 @@
+// TARGET_BACKEND: JVM_IR
+
+enum class E {
+    A, B;
+}
+
+fun bar(): E = E.A
+
+fun foo(e: E): String {
+    val c = when (e) {
+        E.B -> "B"
+        bar() -> "OK"
+        else -> "else"
+    }
+    return c
+}
+
+fun box() = foo(bar())
+
+// CHECK_BYTECODE_TEXT
+// 0 WhenMappings
+// 0 TABLESWITCH
