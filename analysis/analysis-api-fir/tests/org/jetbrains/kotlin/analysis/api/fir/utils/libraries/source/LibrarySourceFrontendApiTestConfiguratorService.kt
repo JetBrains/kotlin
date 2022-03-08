6fad939fe1593bd461009af631d2b1a8d50eb5f4
Jinseong Jeon <jsjeon@google.com>
2022-03-07 23:22:09 -0800

    AA: reuse CoreJarFileSystem from KotlinCoreEnvironment in tests

--- analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/utils/libraries/source/LibrarySourceFrontendApiTestConfiguratorService.kt
+++ analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/utils/libraries/source/LibrarySourceFrontendApiTestConfiguratorService.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.analysis.api.fir.utils.libraries.source
 import com.intellij.mock.MockApplication
 import com.intellij.mock.MockProject
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.vfs.impl.jar.CoreJarFileSystem
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.analysis.api.fir.FirFrontendApiTestConfiguratorService
 import org.jetbrains.kotlin.analysis.api.impl.barebone.test.FrontendApiTestConfiguratorService
@@ -35,7 +36,8 @@ object LibrarySourceFrontendApiTestConfiguratorService : FrontendApiTestConfigur
         compilerConfig: CompilerConfiguration,
         files: List<KtFile>,
         packagePartProvider: (GlobalSearchScope) -> PackagePartProvider,
-        projectStructureProvider: ProjectStructureProvider
+        projectStructureProvider: ProjectStructureProvider,
+        jarFileSystem: CoreJarFileSystem,
     ) {
         project.registerTestServices(files, packagePartProvider, projectStructureProvider)
     }
