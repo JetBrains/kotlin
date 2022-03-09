3805a01c38f4abd1f330c49da3303c6d77ea82a2
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-03-09 14:33:51 +0300

    Drop unused members from DescriptorStorageForContextReceivers

--- compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/generators/DescriptorStorageForContextReceivers.kt
+++ compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/generators/DescriptorStorageForContextReceivers.kt
@@ -5,24 +5,15 @@
 
 package org.jetbrains.kotlin.psi2ir.generators
 
-import org.jetbrains.kotlin.descriptors.VariableDescriptor
 import org.jetbrains.kotlin.ir.declarations.IrField
-import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.resolve.scopes.receivers.ReceiverValue
 
 class DescriptorStorageForContextReceivers {
-    private val variableStorage: MutableMap<KtExpression, VariableDescriptor> = mutableMapOf()
     private val syntheticFieldStorage: MutableMap<ReceiverValue, IrField> = mutableMapOf()
-
-    fun put(expression: KtExpression, descriptor: VariableDescriptor) {
-        variableStorage[expression] = descriptor
-    }
 
     fun put(receiverValue: ReceiverValue, irField: IrField) {
         syntheticFieldStorage[receiverValue] = irField
     }
-
-    fun getVariable(expression: KtExpression) = variableStorage[expression] ?: error("No variable descriptor for receiver $expression")
 
     fun getSyntheticField(receiverValue: ReceiverValue) =
         syntheticFieldStorage[receiverValue] ?: error("No synthetic field for receiver value $receiverValue")
