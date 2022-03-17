a8f3c22ae10b5187870f894ca97d8a54eae7b3a7
Nick Anthony <nickanthony@google.com>
2022-03-17 03:25:09 -0400

    Update versions for release id 1648054800000
    This commit was generated from the command:
    ./update_versions_for_release.py 1648054800000
    
    Test: ./gradlew checkApi
    Change-Id: I135db809468d36bdf14ad3923a113e1121049338 ( https://android-review.git.corp.google.com/q/I135db809468d36bdf14ad3923a113e1121049338 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -74,6 +74,7 @@ class VersionChecker(val context: IrPluginContext) {
             6300 to "1.2.0-alpha04",
             6400 to "1.2.0-alpha05",
             6500 to "1.2.0-alpha06",
+            6600 to "1.2.0-alpha07",
         )
 
         /**
@@ -86,7 +87,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.2.0-alpha06"
+        const val compilerVersion: String = "1.2.0-alpha07"
         private val minimumRuntimeVersion: String
             get() = versionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
