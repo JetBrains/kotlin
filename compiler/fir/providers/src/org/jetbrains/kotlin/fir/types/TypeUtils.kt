a0cdd8140c19acddc48a9cedea2656bbf4fe4aac
Nikolay Lunyak <lunyak.kolya@mail.ru>
2022-03-18 11:34:33 +0300

    [FIR JS] Fix `withAttributes`

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/types/TypeUtils.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/types/TypeUtils.kt
@@ -118,6 +118,7 @@ fun <T : ConeKotlinType> T.withAttributes(attributes: ConeAttributes): T {
         is ConeDefinitelyNotNullType -> ConeDefinitelyNotNullType(original.withAttributes(attributes))
         is ConeTypeParameterTypeImpl -> ConeTypeParameterTypeImpl(lookupTag, nullability.isNullable, attributes)
         is ConeRawType -> ConeRawType(lowerBound.withAttributes(attributes), upperBound.withAttributes(attributes))
+        is ConeDynamicType -> ConeDynamicType(lowerBound.withAttributes(attributes), upperBound.withAttributes(attributes))
         is ConeFlexibleType -> ConeFlexibleType(lowerBound.withAttributes(attributes), upperBound.withAttributes(attributes))
         is ConeTypeVariableType -> ConeTypeVariableType(nullability, lookupTag, attributes)
         is ConeCapturedType -> ConeCapturedType(
