9b1a02f5e7fbd12ae9c58af6efb9907ba8188b86
Goooler <wangzongler@gmail.com>
2022-03-22 22:17:13 +0800

    [KDM] Remove bintray publish plugins
    See https://github.com/Kotlin/dokka/pull/2364
    
    * Bump binary-compatibility-validator to 0.8.0
    Original commit: https://github.com/Kotlin/dokka/commit/dd0fd03ea37554d79a02a8c6bcfd5f081dc6a1e1

--- libraries/tools/kotlin-documentation-model/analyzer/settings.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/settings.gradle.kts
@@ -43,7 +43,6 @@ pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.jvm") version kotlin_version
         id("com.github.johnrengelman.shadow") version "5.2.0"
-        id("com.jfrog.bintray") version "1.8.5"
         id("com.gradle.plugin-publish") version "0.20.0"
     }
 }
