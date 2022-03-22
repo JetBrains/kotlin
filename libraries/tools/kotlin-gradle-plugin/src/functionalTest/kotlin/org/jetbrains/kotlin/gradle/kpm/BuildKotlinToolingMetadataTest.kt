07b65f78d79e74a36846c67d013592dba8e58fcc
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-03-22 14:42:21 +0100

    [Gradle] Add BuildKotlinToolingMetadataTest for KPM

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/kpm/BuildKotlinToolingMetadataTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/kpm/BuildKotlinToolingMetadataTest.kt
@@ -3,49 +3,74 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-/* Associate compilations are not yet supported by the IDE. KT-34102 */
-@file:Suppress("invisible_reference", "invisible_member", "FunctionName", "DuplicatedCode")
-
-package org.jetbrains.kotlin.gradle
+package org.jetbrains.kotlin.gradle.kpm
 
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.JavaVersion
+import org.gradle.api.Project
 import org.gradle.api.internal.project.ProjectInternal
-import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.testfixtures.ProjectBuilder
 import org.jetbrains.kotlin.compilerRunner.konanVersion
-import org.jetbrains.kotlin.gradle.dsl.KotlinJsProjectExtension
+import org.jetbrains.kotlin.gradle.addBuildEventsListenerRegistryMock
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
-import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.enableKpmModelMapping
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType.*
-import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinMetadataTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
-import org.jetbrains.kotlin.gradle.targets.js.KotlinJsTarget
-import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
-import org.jetbrains.kotlin.gradle.tooling.BuildKotlinToolingMetadataTask
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.*
 import org.jetbrains.kotlin.gradle.tooling.buildKotlinToolingMetadataTask
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.library.KotlinAbiVersion
 import org.jetbrains.kotlin.tooling.KotlinToolingMetadata
 import org.jetbrains.kotlin.tooling.toJsonString
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import org.gradle.kotlin.dsl.create
+import org.jetbrains.kotlin.gradle.plugin.*
+import kotlin.test.*
 
-class BuildKotlinToolingMetadataTest {
+private fun Project.kotlinToolingMetadataOfModule(moduleName: String = KotlinGradleModule.MAIN_MODULE_NAME): KotlinToolingMetadata {
+    val module = kpmModules.getByName(moduleName)
+    return module.buildKotlinToolingMetadataTask!!.get().kotlinToolingMetadata
+}
 
-    private val project = ProjectBuilder.builder().build().also{ addBuildEventsListenerRegistryMock(it) } as ProjectInternal
+class BuildKotlinToolingMetadataTest : AbstractKpmExtensionTest() {
+    @Test
+    fun `multiple targets`() {
+        // Given
+        with(kotlin) {
+            mainAndTest {
+                jvm
+                val linux = fragments.create("linux")
+                fragments.create<KotlinLinuxX64Variant>("linuxX64").apply { refines(linux) }
+                fragments.create<KotlinLinuxArm64Variant>("linuxArm64").apply { refines(linux) }
+                // No JS & Android variants available at the moment, only through [LegacyMappedVariant] which is tested below
+            }
+        }
+
+        // When
+        val metadata = project.kotlinToolingMetadataOfModule()
+
+        // Then
+        assertEquals("Gradle", metadata.buildSystem)
+        assertEquals(project.gradle.gradleVersion, metadata.buildSystemVersion)
+        assertEquals(KotlinPm20PluginWrapper::class.java.canonicalName, metadata.buildPlugin)
+        assertEquals(project.getKotlinPluginVersion(), metadata.buildPluginVersion)
+        assertEquals(3, metadata.projectTargets.size, "Expected 3 targets in KPM")
+
+        val jvmTarget = metadata.projectTargets.single { it.platformType == jvm.name }
+        assertEquals(KotlinJvmVariant::class.java.canonicalName, jvmTarget.target)
+
+        val nativeTargets = metadata.projectTargets.filter { it.platformType == native.name }.sortedBy { it.platformType }
+        assertEquals(KotlinLinuxArm64Variant::class.java.canonicalName, nativeTargets[0].target)
+        assertEquals(KotlinLinuxX64Variant::class.java.canonicalName, nativeTargets[1].target)
+    }
+}
+
+class KotlinToolingMetadataWithModelMappingTest {
+    private val project = ProjectBuilder.builder().build().also { addBuildEventsListenerRegistryMock(it) } as ProjectInternal
     private val multiplatformExtension get() = project.extensions.getByType(KotlinMultiplatformExtension::class.java)
-    private val jsExtension get() = project.extensions.getByType(KotlinJsProjectExtension::class.java)
+    private fun getKotlinToolingMetadata() = project.kotlinToolingMetadataOfModule()
 
-    init {
-        project.extensions.getByType(ExtraPropertiesExtension::class.java).set("kotlin.mpp.enableKotlinToolingMetadataArtifact", "true")
+    @BeforeTest
+    fun setup() {
+        project.enableKpmModelMapping()
     }
 
     @Test
@@ -58,12 +83,7 @@ class BuildKotlinToolingMetadataTest {
         assertEquals(project.gradle.gradleVersion, metadata.buildSystemVersion)
         assertEquals(KotlinMultiplatformPluginWrapper::class.java.canonicalName, metadata.buildPlugin)
         assertEquals(project.getKotlinPluginVersion(), metadata.buildPluginVersion)
-        assertEquals(1, metadata.projectTargets.size, "Expected one target (metadata)")
-        assertTrue(
-            KotlinMetadataTarget::class.java.isAssignableFrom(Class.forName(metadata.projectTargets.single().target)),
-            "Expect target to be implement ${KotlinMetadataTarget::class.simpleName}"
-        )
-        assertEquals(common.name, metadata.projectTargets.single().platformType)
+        assertEquals(0, metadata.projectTargets.size, "Expected no targets in KPM")
         assertTrue(metadata.toJsonString().isNotBlank(), "Expected non blank json representation")
     }
 
@@ -89,12 +109,15 @@ class BuildKotlinToolingMetadataTest {
         val metadata = getKotlinToolingMetadata()
         assertEquals(KotlinMultiplatformPluginWrapper::class.java.canonicalName, metadata.buildPlugin)
 
+        assertFalse("Kotlin Tooling Metadata generated from KPM should not contain target of 'common' platform") {
+            metadata.projectTargets.any { it.platformType == common.name }
+        }
+
         val expectedTargets = mapOf(
-            common to KotlinMetadataTarget::class,
-            androidJvm to KotlinAndroidTarget::class,
-            jvm to KotlinJvmTarget::class,
-            js to KotlinJsTarget::class,
-            native to KotlinNativeTargetWithHostTests::class
+            androidJvm to LegacyMappedVariantWithRuntime::class,
+            jvm to KotlinJvmVariant::class,
+            js to LegacyMappedVariantWithRuntime::class,
+            native to KotlinLinuxX64Variant::class
         )
 
         assertEquals(
@@ -110,7 +133,7 @@ class BuildKotlinToolingMetadataTest {
             )
         }
 
-        assertFalse(metadata.projectSettings.isKPMEnabled, "projectSettings.isKPMEnabled must not be set")
+        assertTrue(metadata.projectSettings.isKPMEnabled, "projectSettings.isKPMEnabled must be set")
     }
 
     @Test
@@ -121,8 +144,8 @@ class BuildKotlinToolingMetadataTest {
         val kotlin = multiplatformExtension
         android.compileSdkVersion(28)
         kotlin.android()
-        android.compileOptions.setSourceCompatibility(JavaVersion.VERSION_1_6)
-        android.compileOptions.setTargetCompatibility(JavaVersion.VERSION_1_8)
+        android.compileOptions.sourceCompatibility = JavaVersion.VERSION_1_6
+        android.compileOptions.targetCompatibility = JavaVersion.VERSION_1_8
         project.evaluate()
 
         val androidTargetMetadata = getKotlinToolingMetadata()
@@ -161,27 +184,6 @@ class BuildKotlinToolingMetadataTest {
     }
 
     @Test
-    fun js() {
-        project.plugins.apply("org.jetbrains.kotlin.js")
-        val kotlin = jsExtension
-        kotlin.js { nodejs() }
-
-        val metadata = getKotlinToolingMetadata()
-        assertEquals(org.jetbrains.kotlin.gradle.plugin.KotlinJsPluginWrapper::class.java.canonicalName, metadata.buildPlugin)
-
-        val jsTarget = metadata.projectTargets.single { it.platformType == js.name }
-        assertEquals(true, jsTarget.extras.js?.isNodejsConfigured)
-        assertEquals(false, jsTarget.extras.js?.isBrowserConfigured)
-    }
-
-    @Test
-    fun jvm() {
-        project.plugins.apply("org.jetbrains.kotlin.jvm")
-        val metadata = getKotlinToolingMetadata()
-        assertEquals(org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper::class.java.canonicalName, metadata.buildPlugin)
-    }
-
-    @Test
     fun `multiple native targets`() {
         project.plugins.apply("kotlin-multiplatform")
         val kotlin = multiplatformExtension
@@ -202,12 +204,14 @@ class BuildKotlinToolingMetadataTest {
             linuxX64.extras.native?.konanTarget
         )
 
-        assertEquals(KotlinNativeTarget::class.java.canonicalName, linuxArm64.target)
-        assertEquals(KotlinNativeTargetWithHostTests::class.java.canonicalName, linuxX64.target)
+        assertEquals(
+            KotlinLinuxArm64Variant::class.java.canonicalName,
+            linuxArm64.target
+        )
+        assertEquals(
+            KotlinLinuxX64Variant::class.java.canonicalName,
+            linuxX64.target
+        )
     }
 
-    private fun getKotlinToolingMetadata(): KotlinToolingMetadata {
-        val task = project.buildKotlinToolingMetadataTask?.get() ?: error("No ${BuildKotlinToolingMetadataTask.defaultTaskName} task")
-        return task.kotlinToolingMetadata
-    }
 }
