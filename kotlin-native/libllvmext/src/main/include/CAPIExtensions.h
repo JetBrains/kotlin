30006b51433c0f2cf1bf5f2fa3f3f2c612a74157
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-03-24 18:22:29 +0300

    [K/N] Add CMakeLists.txt to simplify work with libllvmext
    Also moved functions that have nothing to do with code coverage
    into a separate file.

---
+++ kotlin-native/libllvmext/src/main/include/CAPIExtensions.h
@@ -0,0 +1,28 @@
+//
+// Created by Sergey.Bogolepov on 24.03.2022.
+//
+
+#ifndef LIBLLVMEXT_EXTENSIONS_H
+#define LIBLLVMEXT_EXTENSIONS_H
+
+#endif //LIBLLVMEXT_EXTENSIONS_H
+
+#include <llvm-c/Core.h>
+#include <llvm-c/Target.h>
+
+
+# ifdef __cplusplus
+extern "C" {
+# endif
+
+void LLVMKotlinAddTargetLibraryInfoWrapperPass(LLVMPassManagerRef passManagerRef, const char* targetTriple);
+
+void LLVMAddObjCARCContractPass(LLVMPassManagerRef passManagerRef);
+
+void LLVMKotlinInitializeTargets();
+
+void LLVMSetNoTailCall(LLVMValueRef Call);
+
+# ifdef __cplusplus
+}
+# endif
