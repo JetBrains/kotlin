20d91f3851023554b8793d2215d912c8b980e7fa
Vadim Mishenev <vad-mishenev@yandex.ru>
2022-03-28 18:26:29 +0300

    [KDM] Bug resolving in ultralightmethod
    See https://github.com/Kotlin/dokka/pull/2400
    Original commit: https://github.com/Kotlin/dokka/commit/57133de2fef1be68d9f05e6c262c31288de6cbcd

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/translators/DefaultPsiToDocumentableTranslatorTest.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/translators/DefaultPsiToDocumentableTranslatorTest.kt
@@ -1,13 +1,17 @@
 package translators
 
-import org.jetbrains.dokka.model.doc.Text
-import org.jetbrains.dokka.model.firstMemberOfType
+import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
 import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.model.Annotations
+import org.jetbrains.dokka.model.TypeConstructor
+import org.jetbrains.dokka.model.doc.Text
+import org.jetbrains.dokka.model.firstMemberOfType
+import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import utils.assertNotNull
 
 class DefaultPsiToDocumentableTranslatorTest : BaseAbstractTest() {
     val configuration = dokkaConfiguration {
@@ -189,6 +193,67 @@ class DefaultPsiToDocumentableTranslatorTest : BaseAbstractTest() {
                 assertEquals(
                     Annotations.Annotation(DRI("sample", "PackageAnnotation"), emptyMap()),
                     module.packages.single().extra[Annotations]?.directAnnotations?.values?.single()?.single()
+                )
+            }
+        }
+    }
+
+    class OnlyPsiPlugin : DokkaPlugin() {
+        private val dokkaBase by lazy { plugin<DokkaBase>() }
+
+        @Suppress("unused")
+        val psiOverrideDescriptorTranslator by extending {
+            (dokkaBase.psiToDocumentableTranslator
+                    override dokkaBase.descriptorToDocumentableTranslator)
+        }
+    }
+
+    // for Kotlin classes from DefaultPsiToDocumentableTranslator
+    @Test
+    fun `should resolve ultralight class`() {
+        val configurationWithNoJVM = dokkaConfiguration {
+            sourceSets {
+                sourceSet {
+                    sourceRoots = listOf("src/main/java")
+                }
+            }
+        }
+
+        testInline(
+            """
+            |/src/main/java/example/Test.kt
+            |package example
+            |
+            |open class KotlinSubClass {
+            |    fun kotlinSubclassFunction(bar: String): String {
+            |       return "KotlinSubClass"
+            |    }
+            |}
+            |
+            |/src/main/java/example/JavaLeafClass.java
+            |package example;
+            |
+            |public class JavaLeafClass extends KotlinSubClass {
+            |    public String javaLeafClassFunction(String baz) {
+            |        return "JavaLeafClass";
+            |    }
+            |}
+        """.trimMargin(),
+            configurationWithNoJVM,
+            pluginOverrides = listOf(OnlyPsiPlugin()) // suppress a descriptor translator because of psi and descriptor translators work in parallel
+        ) {
+            documentablesMergingStage = { module ->
+                val kotlinSubclassFunction =
+                    module.packages.single().classlikes.find { it.name == "JavaLeafClass" }?.functions?.find { it.name == "kotlinSubclassFunction" }
+                        .assertNotNull("kotlinSubclassFunction ")
+
+                assertEquals(
+                    "String",
+                    (kotlinSubclassFunction.type as? TypeConstructor)?.dri?.classNames
+                )
+                assertEquals(
+                    "String",
+                    (kotlinSubclassFunction.parameters.firstOrNull()?.type as? TypeConstructor)?.dri?.classNames
                 )
             }
         }
