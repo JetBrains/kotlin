1d482814c3b98214be20eefa51940ddb5aaf5a9f
Nikolay Lunyak <lunyak.kolya@mail.ru>
2022-04-02 00:59:02 +0300

    [FIR JS] Add the dynamic type unsupported checker

---
+++ compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/JvmTypeCheckers.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.fir.analysis.jvm.checkers
+
+import org.jetbrains.kotlin.fir.analysis.checkers.declaration.FirDynamicUnsupportedChecker
+import org.jetbrains.kotlin.fir.analysis.checkers.type.*
+
+object JvmTypeCheckers : TypeCheckers() {
+    override val typeRefCheckers: Set<FirTypeRefChecker> = setOf(
+        FirDynamicUnsupportedChecker,
+    )
+}
