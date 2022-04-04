1a8c38e40fa6c15b9c2922f002ff71d1aa7ec854
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-04-04 16:50:36 +0200

    [analysis api, tests] generate standalone tests

--- analysis/analysis-api-standalone/tests/org/jetbrains/kotlin/analysis/api/standalone/fir/test/StandaloneModeConfigurator.kt
+++ analysis/analysis-api-standalone/tests/org/jetbrains/kotlin/analysis/api/standalone/fir/test/StandaloneModeConfigurator.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.analysis.api.standalone
+package org.jetbrains.kotlin.analysis.api.standalone.fir.test
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
