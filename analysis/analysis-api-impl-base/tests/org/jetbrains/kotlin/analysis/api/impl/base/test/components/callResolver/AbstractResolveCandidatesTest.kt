82b3119874c49812e87dc29647c20074116028f4
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-04-04 16:40:47 +0200

    [analysis api, tests] move resolve call tests to separate package

--- analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/components/callResolver/AbstractResolveCandidatesTest.kt
+++ analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/components/callResolver/AbstractResolveCandidatesTest.kt
@@ -1,13 +1,15 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.analysis.api.impl.base.test.components
+package org.jetbrains.kotlin.analysis.api.impl.base.test.components.callResolver
 
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.analysis.api.calls.KtCallCandidateInfo
+import org.jetbrains.kotlin.analysis.api.impl.base.test.components.compareCalls
+import org.jetbrains.kotlin.analysis.api.impl.base.test.components.stringRepresentation
 import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedSingleModuleTest
 import org.jetbrains.kotlin.analysis.test.framework.services.expressionMarkerProvider
 import org.jetbrains.kotlin.psi.*
