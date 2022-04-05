9ca4f7673f28c1d4a4826eb177efdd868c064132
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2022-04-05 11:26:43 +0300

    FIR: suppress CONFLICTING_OVERLOADS diagnostic properly

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/declaration/FirConflictsChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/declaration/FirConflictsChecker.kt
@@ -13,6 +13,7 @@ import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.diagnostics.reportOn
+import org.jetbrains.kotlin.fir.analysis.diagnostics.withSuppressedDiagnostics
 import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.declarations.impl.FirOuterClassTypeParameterRef
 import org.jetbrains.kotlin.fir.resolve.getContainingDeclaration
@@ -227,21 +228,23 @@ object FirConflictsChecker : FirBasicDeclarationChecker() {
             inspector.declarationConflictingSymbols.forEach { (conflictingDeclaration, symbols) ->
                 val source = conflictingDeclaration.source
                 if (source != null && symbols.isNotEmpty()) {
-                    when (conflictingDeclaration) {
-                        is FirSimpleFunction,
-                        is FirConstructor -> {
-                            reporter.reportOn(source, FirErrors.CONFLICTING_OVERLOADS, symbols, context)
-                        }
-                        else -> {
-                            val factory = if (conflictingDeclaration is FirClassLikeDeclaration &&
-                                conflictingDeclaration.getContainingDeclaration(context.session) == null &&
-                                symbols.any { it is FirClassLikeSymbol<*> }
-                            ) {
-                                FirErrors.PACKAGE_OR_CLASSIFIER_REDECLARATION
-                            } else {
-                                FirErrors.REDECLARATION
+                    withSuppressedDiagnostics(conflictingDeclaration, context) { ctx ->
+                        when (conflictingDeclaration) {
+                            is FirSimpleFunction,
+                            is FirConstructor -> {
+                                reporter.reportOn(source, FirErrors.CONFLICTING_OVERLOADS, symbols, ctx)
                             }
-                            reporter.reportOn(source, factory, symbols, context)
+                            else -> {
+                                val factory = if (conflictingDeclaration is FirClassLikeDeclaration &&
+                                    conflictingDeclaration.getContainingDeclaration(ctx.session) == null &&
+                                    symbols.any { it is FirClassLikeSymbol<*> }
+                                ) {
+                                    FirErrors.PACKAGE_OR_CLASSIFIER_REDECLARATION
+                                } else {
+                                    FirErrors.REDECLARATION
+                                }
+                                reporter.reportOn(source, factory, symbols, ctx)
+                            }
                         }
                     }
                 }
