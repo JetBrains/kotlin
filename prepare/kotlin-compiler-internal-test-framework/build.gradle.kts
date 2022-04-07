eaf94b3ad89a8773e2b8750b43ec7b34271a7e67
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-04-07 15:18:42 +0400

    [Test] Don't pack compiler itself to internal-compiler-test-framework.jar

--- prepare/kotlin-compiler-internal-test-framework/build.gradle.kts
+++ prepare/kotlin-compiler-internal-test-framework/build.gradle.kts
@@ -2,19 +2,13 @@ plugins {
     java
 }
 
-val compilerModules: Array<String> by rootProject.extra
-
 dependencies {
-    compilerModules.forEach {
-        embedded(project(it)) { isTransitive = false }
-    }
     embedded(projectTests(":compiler:tests-common-jvm6")) { isTransitive = false }
     embedded(projectTests(":compiler:test-infrastructure")) { isTransitive = false }
     embedded(projectTests(":compiler:test-infrastructure-utils")) { isTransitive = false }
     embedded(projectTests(":compiler:tests-compiler-utils")) { isTransitive = false }
     embedded(projectTests(":compiler:tests-common-new")) { isTransitive = false }
-    embedded(protobufFull())
-    embedded(kotlinBuiltins())
+    embedded(intellijJavaRt()) { isTransitive = false }
 }
 
 publish()
