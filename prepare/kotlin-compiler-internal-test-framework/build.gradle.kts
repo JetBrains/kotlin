d98375c459bf2f3250482f13fb692b13c1c4abef
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-04-07 15:19:08 +0400

    [Test] Pack test generator to internal-compiler-test-framework.jar

--- prepare/kotlin-compiler-internal-test-framework/build.gradle.kts
+++ prepare/kotlin-compiler-internal-test-framework/build.gradle.kts
@@ -9,6 +9,8 @@ dependencies {
     embedded(projectTests(":compiler:tests-compiler-utils")) { isTransitive = false }
     embedded(projectTests(":compiler:tests-common-new")) { isTransitive = false }
     embedded(intellijJavaRt()) { isTransitive = false }
+    embedded(project(":generators")) { isTransitive = false }
+    embedded(projectTests(":generators:test-generator")) { isTransitive = false }
 }
 
 publish()
