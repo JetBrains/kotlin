c82b1d3e338fb23c06cb3317a02d1db8a331f082
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-04-08 14:51:41 +0200

    KT-51884 Skip XCFramework and FatFramework tasks on non-mac machines.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/AppleFrameworkNonMacIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/AppleFrameworkNonMacIT.kt
@@ -14,19 +14,34 @@ import org.junit.BeforeClass
 import org.junit.Test
 import kotlin.test.assertTrue
 
-class CocoaPodsNonMacIT : BaseGradleIT() {
+class AppleFrameworkNonMacIT : BaseGradleIT() {
 
     override val defaultGradleVersion: GradleVersionRequired
         get() = GradleVersionRequired.FOR_MPP_SUPPORT
 
     @Test
-    fun testImport() {
+    fun `check success podImport`() {
         transformProjectWithPluginsDsl(
             "native-cocoapods-tests"
         ).build("podImport", "-Pkotlin.native.cocoapods.generate.wrapper=true") {
             assertSuccessful()
             assertTrue { output.contains("Kotlin Cocoapods Plugin is fully supported on mac machines only. Gradle tasks that can not run on non-mac hosts will be skipped.") }
         }
+    }
+
+    @Test
+    fun `check XCFramework and FatFramework tasks are SKIPPED`() {
+        transformProjectWithPluginsDsl("appleXCFramework")
+            .build("assembleSharedDebugXCFramework") {
+                assertSuccessful()
+                assertTasksSkipped(":shared:linkDebugFrameworkIosArm64")
+                assertTasksSkipped(":shared:linkDebugFrameworkIosX64")
+                assertTasksSkipped(":shared:linkDebugFrameworkWatchosArm32")
+                assertTasksSkipped(":shared:linkDebugFrameworkWatchosArm64")
+                assertTasksSkipped(":shared:linkDebugFrameworkWatchosX64")
+                assertTasksSkipped(":shared:assembleDebugWatchosFatFrameworkForSharedXCFramework")
+                assertTasksSkipped(":shared:assembleSharedDebugXCFramework")
+            }
     }
 
     companion object {
