2206eb4b70224dea8df74458fd3b6572d3834c92
DMozhevitin <RayZieger@yandex.ru>
2022-04-10 16:54:19 +0300

    Transform string template with prefix to 'buildLiteral' call

--- compiler/fir/analysis-tests/testData/resolve/customStringLiterals.kt
+++ compiler/fir/analysis-tests/testData/resolve/customStringLiterals.kt
@@ -1,5 +1,17 @@
+object id
+
+fun id.buildLiteral(body: LiteralBuilder.() -> Unit): Int {
+    return LiteralBuilder().apply(body).build()
+}
+
+class LiteralBuilder {
+    fun appendString(s: String) { }
+    fun appendObject(obj: Any) { }
+    fun build(): Int = 0
+}
+
 fun test() {
     val a = 1
     val b = 2
-    pref"string template with prefix and args: $a, ${b}"
+    val x: Int = id"str $a ${b}"
 }
