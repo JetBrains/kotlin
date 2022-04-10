aa1886549082b9d35127463f7411503463c5366b
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-04-10 13:02:28 +0300

    fixup! [FIR JS] Treat named arguments as positional for dynamics

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/FirArgumentsToParametersMapper.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/FirArgumentsToParametersMapper.kt
@@ -159,19 +159,20 @@ private class FirCallArgumentsProcessor(
     }
 
     private fun processNonLambdaArgument(argument: FirExpression, isLastArgument: Boolean) {
-        // process position argument
-        if (argument !is FirNamedArgumentExpression) {
-            if (processPositionArgument(argument, isLastArgument)) {
-                state = State.VARARG_POSITION
+        when {
+            // process position argument
+            argument !is FirNamedArgumentExpression -> {
+                if (processPositionArgument(argument, isLastArgument)) {
+                    state = State.VARARG_POSITION
+                }
             }
-        }
-        // process named argument
-        else {
-            if (function.origin == FirDeclarationOrigin.DynamicScope) {
+            // process named argument
+            function.origin == FirDeclarationOrigin.DynamicScope -> {
                 if (processPositionArgument(argument.expression, isLastArgument)) {
                     state = State.VARARG_POSITION
                 }
-            } else {
+            }
+            else -> {
                 if (state == State.VARARG_POSITION) {
                     completeVarargPositionArguments()
                 }
