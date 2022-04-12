6e8e1469fb2c5d8e3e8813bab9a58778191d2065
Igor Chevdar <igor.chevdar@jetbrains.com>
2022-04-12 10:54:33 +0500

    [klibs] Fixed a bug in the signature-to-descriptor searcher
     #KT-51927 Fixed

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/DescriptorByIdSignatureFinderImpl.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/DescriptorByIdSignatureFinderImpl.kt
@@ -151,7 +151,7 @@ class DescriptorByIdSignatureFinderImpl(
         }
         val candidates = acc
 
-        return candidates.singleOrNull() ?: findDescriptorByHash(candidates, signature.id)
+        return findDescriptorByHash(candidates, signature.id)
     }
 
     private fun findDescriptorByHash(candidates: Collection<DeclarationDescriptor>, id: Long?): DeclarationDescriptor? =
