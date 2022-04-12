1b297c519003cc23284ba94fcd9a0d98e558b297
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2022-04-12 07:03:44 +0300

    Package export and documentation summary

--- libraries/stdlib/jdk8/java9/module-info.java
+++ libraries/stdlib/jdk8/java9/module-info.java
@@ -3,6 +3,7 @@ module kotlin.stdlib.jdk8 {
     requires transitive kotlin.stdlib;
     requires kotlin.stdlib.jdk7;
 
+    exports kotlin.jvm.optionals;
     exports kotlin.jvm.jdk8;
     exports kotlin.collections.jdk8;
     exports kotlin.streams.jdk8;
