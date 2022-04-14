6e2b792dee8c1e588d482edf8739f62362c660d9
Anže Sodja <asodja@gradle.com>
2022-04-14 12:22:43 +0200

    Add plus assign test for comparison

--- compiler/fir/analysis-tests/testData/resolve/assignOverload.kt
+++ compiler/fir/analysis-tests/testData/resolve/assignOverload.kt
@@ -4,8 +4,22 @@ operator fun Any.assign(other: String) {
     result = other
 }
 
+operator fun Any.plusAssign(x: String) {}
+//operator fun Foo.plusAssign(x: Foo) {}
+//operator fun Foo.plusAssign(x: String) {}
+
+class Foo {
+}
+
+
 fun box(): String {
     val x = 10
     x = "OK"
+    return result
+}
+
+fun test_2(): String {
+    val x = 10
+    x += "OK"
     return result
 }
