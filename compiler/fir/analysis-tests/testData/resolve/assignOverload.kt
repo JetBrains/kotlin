758e2214990593bf98a9e11e75d7149cf1707d00
Anže Sodja <asodja@gradle.com>
2022-04-14 14:09:27 +0200

    Add some more tests

--- compiler/fir/analysis-tests/testData/resolve/assignOverload.kt
+++ compiler/fir/analysis-tests/testData/resolve/assignOverload.kt
@@ -4,12 +4,15 @@ operator fun Any.assign(other: String) {
     result = other
 }
 
+operator fun String.assign(x: Int) {}
+
 operator fun Any.plusAssign(x: String) {}
+
+operator fun String.plusAssign(x: String) {}
 //operator fun Foo.plusAssign(x: Foo) {}
 //operator fun Foo.plusAssign(x: String) {}
 
 data class Foo(val x: String)
-
 
 fun test_local_variable(): String {
     val x = 10
@@ -29,8 +32,26 @@ fun test_object_property(): String {
     return result
 }
 
+fun test_object_property_with_different_type(): String {
+    val x = Foo("Hello")
+    x.x = 5
+    return result
+}
+
+fun test_assignment_type_mismatch(): String {
+    val x = Foo("Hello")
+    x.x = <!ASSIGNMENT_TYPE_MISMATCH!>5L<!>
+    return result
+}
+
 fun test_object_property_plus_assign(): String {
     val x = Foo("Hello")
     x.x += "OK"
+    return result
+}
+
+fun test_plus_assign_assignment_type_mismatch(): String {
+    val x = Foo("Hello")
+    x.<!VAL_REASSIGNMENT!>x<!> += 5L
     return result
 }
