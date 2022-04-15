43b6e56a823ebb6b0242fdf7c2f7db3810d67631
Anton Bannykh <anton.bannykh@jetbrains.com>
2022-04-15 21:01:43 +0300

    JS IR: materialize Unit in lambdas
    ^KT-52010 fixed
    
    (cherry picked from commit 281e381223eeb5957fac93436413639617976636)

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrBoxJsTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrBoxJsTestGenerated.java
@@ -3350,6 +3350,12 @@ public class IrBoxJsTestGenerated extends AbstractIrBoxJsTest {
             }
 
             @Test
+            @TestMetadata("lambdaReturnValue.kt")
+            public void testLambdaReturnValue() throws Exception {
+                runTest("js/js.translator/testData/box/expression/function/lambdaReturnValue.kt");
+            }
+
+            @Test
             @TestMetadata("localExtFunction.kt")
             public void testLocalExtFunction() throws Exception {
                 runTest("js/js.translator/testData/box/expression/function/localExtFunction.kt");
@@ -9464,6 +9470,12 @@ public class IrBoxJsTestGenerated extends AbstractIrBoxJsTest {
         @TestMetadata("kt2470.kt")
         public void testKt2470() throws Exception {
             runTest("js/js.translator/testData/box/regression/kt2470.kt");
+        }
+
+        @Test
+        @TestMetadata("kt52010.kt")
+        public void testKt52010() throws Exception {
+            runTest("js/js.translator/testData/box/regression/kt52010.kt");
         }
 
         @Test
