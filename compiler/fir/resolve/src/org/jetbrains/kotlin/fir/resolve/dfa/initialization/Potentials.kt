5fd81bc390646433081739bb5d7acd105ea875a9
Marat Dinmukhametov <maratdin7@gmail.com>
2022-04-16 01:18:32 +0300

    [WIP] View changes for potentials and some renaming

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/initialization/Potentials.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/initialization/Potentials.kt
@@ -5,105 +5,130 @@
 
 package org.jetbrains.kotlin.fir.resolve.dfa.initialization
 
-import org.jetbrains.kotlin.fir.FirElement
-import org.jetbrains.kotlin.fir.declarations.FirClass
-import org.jetbrains.kotlin.fir.declarations.FirFunction
-import org.jetbrains.kotlin.fir.declarations.FirProperty
-import org.jetbrains.kotlin.fir.references.FirThisReference
+import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.resolve.dfa.initialization.Effect.*
+import org.jetbrains.kotlin.fir.resolve.dfa.initialization.Potential.*
 
 
-sealed class Potential(val firElement: FirElement, val length: Int = 0) {
+sealed class Potential(val length: Int = 0) {
 
-    sealed class Root(firElement: FirElement, length: Int = 0) : Potential(firElement, length) {
+    sealed class Root(length: Int = 0) : Potential(length) {
 
-        fun effectsOf(firElement: FirElement): List<Effect> = TODO()
-        fun potentialsOf(firElement: FirElement): List<Potential> = TODO()
+        fun effectsOf(clazz: FirClass, firDeclaration: FirDeclaration): List<Effect> = TODO()
+        fun potentialsOf(clazz: FirClass, firDeclaration: FirDeclaration): List<Potential> = TODO()
 
-        class This(firElement: FirThisReference) : Root(firElement)
-        class Warm(firElement: FirElement, val clazz: FirClass, val outer: Potential) : Root(firElement, outer.length + 1)
-        class Cold(firElement: FirElement) : Root(firElement)
+        data class This(val clazz: FirClass) : Root()
+        data class Warm(val clazz: FirClass, val outer: Potential) : Root(outer.length + 1)
+        data class Cold(val firDeclaration: FirDeclaration) : Root()
     }
 
-    class MethodPotential(firElement: FirElement, val potential: Potential, val method: FirFunction) :
-        Potential(firElement, potential.length + 1)
+    data class MethodPotential(val potential: Potential, val method: FirFunction) :
+        Potential(potential.length + 1)
 
-    class FieldPotential(firElement: FirElement, val potential: Potential, val field: FirProperty) :
-        Potential(firElement, potential.length + 1)
+    data class FieldPotential(val potential: Potential, val field: FirProperty) :
+        Potential(potential.length + 1)
 
-    class OuterPotential(firElement: FirElement, val potential: Potential, val outerClass: FirClass) :
-        Potential(firElement, potential.length + 1)
+    data class OuterPotential(val potential: Potential, val outerClass: FirClass) :
+        Potential(potential.length + 1)
 
-    class FunPotential(firElement: FirElement, val potential: Potential, val todo: Any) : Potential(firElement, potential.length + 1)
+    data class FunPotential(
+        val effectsAndPotentials: EffectsAndPotentials,
+        val anonymousFunction: FirAnonymousFunction
+    ) : Potential(effectsAndPotentials.maxLength())
 }
 
-fun select(potentials: List<Potential>, firElement: FirElement, field: FirProperty): EffectsAndPotentials {
+fun select(potentials: List<Potential>, field: FirProperty): EffectsAndPotentials {
 
-    fun select(potential: Potential, firElement: FirElement, field: FirProperty): EffectsAndPotentials = when {
+    fun select(potential: Potential, field: FirProperty): EffectsAndPotentials = when {
         field.initializer != null -> EffectsAndPotentials()
-        potential is Potential.Root.Cold -> EffectsAndPotentials(Effect.Propagate(potential))
+        potential is Root.Cold -> EffectsAndPotentials(Promote(potential))
         potential.length < 2 -> EffectsAndPotentials(
-            Effect.FieldAccess(potential, field),
-            Potential.FieldPotential(firElement, potential, field)
+            FieldAccess(potential, field),
+            FieldPotential(potential, field)
         )
-        else -> EffectsAndPotentials(Effect.Propagate(potential))
+        else -> EffectsAndPotentials(Promote(potential))
     }
 
-    return potentials.fold(EffectsAndPotentials()) { sum, pot -> sum.addEffectsAndPotentials(select(pot, firElement, field)) }
+    return potentials.fold(EffectsAndPotentials()) { sum, pot -> sum.addEffectsAndPotentials(select(pot, field)) }
 }
 
-fun call(potentials: List<Potential>, firElement: FirElement, function: FirFunction): EffectsAndPotentials {
+fun call(potentials: List<Potential>, function: FirFunction): EffectsAndPotentials {
 
-    fun call(potential: Potential, firElement: FirElement, function: FirFunction): EffectsAndPotentials = when {
-        potential is Potential.Root.Cold -> EffectsAndPotentials(Effect.Propagate(potential))
+    fun call(potential: Potential, function: FirFunction): EffectsAndPotentials = when {
+        potential is Root.Cold -> EffectsAndPotentials(Promote(potential))
         potential.length < 2 -> EffectsAndPotentials(
-            Effect.MethodAccess(potential, function),
-            Potential.MethodPotential(firElement, potential, function)
+            MethodAccess(potential, function),
+            MethodPotential(potential, function)
         )
-        else -> EffectsAndPotentials(Effect.Propagate(potential))
+        else -> EffectsAndPotentials(Promote(potential))
     }
 
-    return potentials.fold(EffectsAndPotentials()) { sum, pot -> sum.addEffectsAndPotentials(call(pot, firElement, function)) }
+    return potentials.fold(EffectsAndPotentials()) { sum, pot -> sum.addEffectsAndPotentials(call(pot, function)) }
 }
 
-fun outerSelection(potentials: List<Potential>, firElement: FirElement, clazz: FirClass): EffectsAndPotentials {
+fun outerSelection(potentials: List<Potential>, clazz: FirClass): EffectsAndPotentials {
 
     fun outerSelection(potential: Potential, clazz: FirClass): EffectsAndPotentials = when {
-        potential is Potential.Root.Cold -> EffectsAndPotentials(Effect.Propagate(potential))
-        potential.length < 2 -> EffectsAndPotentials(Potential.OuterPotential(firElement, potential, clazz))
-        else -> EffectsAndPotentials(Effect.Propagate(potential))
+        potential is Root.Cold -> EffectsAndPotentials(Promote(potential))
+        potential.length < 2 -> EffectsAndPotentials(OuterPotential(potential, clazz))
+        else -> EffectsAndPotentials(Promote(potential))
     }
 
     return potentials.fold(EffectsAndPotentials()) { sum, pot -> sum.addEffectsAndPotentials(outerSelection(pot, clazz)) }
 }
 
-fun propagate(potentials: List<Potential>): List<Effect> = potentials.map(Effect::Propagate)
+fun promote(potentials: List<Potential>): List<Effect> = potentials.map(Effect::Promote)
 
 fun init(
     clazz: FirClass,
     fieldsPots: List<List<Potential>>,
-    prefix: FirElement,
     potentials: List<Potential> = listOf()
 ): EffectsAndPotentials {
-    val propagateEffects = fieldsPots.flatMap(::propagate)
-    val prefixPotentials = potentials.map { pot -> Potential.Root.Warm(prefix, clazz, pot) }
-    val initEffects = prefixPotentials.map { warm -> Effect.Init(warm, clazz) }
+    val propagateEffects = fieldsPots.flatMap(::promote)
+    val prefixPotentials = potentials.map { pot -> Root.Warm(clazz, pot) }
+    val initEffects = prefixPotentials.map { warm -> Init(warm, clazz) }
 
     return EffectsAndPotentials(propagateEffects + initEffects, prefixPotentials)
 }
 
 fun List<Potential>.viewChange(root: Potential): List<Potential> = map { pot -> viewChange(pot, root) }
 
-fun viewChange(potential: Potential, root: Potential): Potential =
-    when (potential) {
-        is Potential.Root.This -> root
-        is Potential.Root.Cold -> potential
-        is Potential.Root.Warm -> {
-            if (potential.outer is Potential.Root.Cold) return TODO()
-            val viewedPot = viewChange(potential.outer, root)
-            Potential.Root.Warm(potential.firElement, potential.clazz, viewedPot)
-        }
-        is Potential.FieldPotential -> TODO()
-        is Potential.FunPotential -> TODO()
-        is Potential.MethodPotential -> TODO()
-        is Potential.OuterPotential -> TODO()
+fun viewChange(potential: Potential, root: Potential): Potential {
+
+    fun asPotSimpleRule(
+        pot: Potential,
+        potentialConstructor: (Potential) -> Potential
+    ): Potential {
+        val viewedPot = viewChange(pot, root)
+        return potentialConstructor(viewedPot)
     }
+
+    return when (potential) {
+        is Root.This -> root                                              // As-Pot-This
+        is Root.Cold -> potential                                         // As-Pot-Cold
+        is Root.Warm -> potential.run {
+            when { // ???
+                outer is Root.Cold -> potential
+                outer.length < 2 -> Root.Warm(clazz, Root.Cold(clazz)) // ???
+                else -> {
+                    val viewedPot = viewChange(outer, root)
+                    Root.Warm(clazz, viewedPot)
+                }
+            }
+        }
+        is FieldPotential -> {                                            // As-Pot-Acc
+            val (pot, field) = potential
+            val viewedPot = viewChange(pot, root)
+            FieldPotential(viewedPot, field)
+        }
+        is MethodPotential -> {                                           // As-Pot-Inv
+            val (pot, method) = potential
+            val viewedPot = viewChange(pot, root)
+            MethodPotential(viewedPot, method)
+        }
+        is OuterPotential -> {                                            // As-Pot-Out
+            asPotSimpleRule(potential.potential) { pot -> OuterPotential(pot, potential.outerClass) }
+        }
+        is FunPotential -> TODO()
+    }
+}
