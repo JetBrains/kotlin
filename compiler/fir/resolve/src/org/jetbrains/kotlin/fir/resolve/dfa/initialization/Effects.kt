04003ac643202b9b2560d5b8cf0b34723d103541
Marat Dinmukhametov <maratdin7@gmail.com>
2022-04-16 01:18:31 +0300

    [WIP] View changes for effects

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/initialization/Effects.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/initialization/Effects.kt
@@ -6,15 +6,35 @@
 package org.jetbrains.kotlin.fir.resolve.dfa.initialization
 
 import org.jetbrains.kotlin.fir.declarations.FirClass
-import org.jetbrains.kotlin.fir.declarations.FirField
 import org.jetbrains.kotlin.fir.declarations.FirFunction
 import org.jetbrains.kotlin.fir.declarations.FirProperty
+import org.jetbrains.kotlin.fir.resolve.dfa.initialization.Effect.*
+import org.jetbrains.kotlin.fir.resolve.dfa.initialization.Potential.*
 
 sealed class Effect {
-    data class Propagate(val potential: Potential) : Effect()
+    data class Promote(val potential: Potential) : Effect()
     data class FieldAccess(val potential: Potential, val field: FirProperty) : Effect()
     data class MethodAccess(val potential: Potential, var method: FirFunction) : Effect()
-    data class Init(val potential: Potential, val clazz: FirClass) : Effect()
+    data class Init(val potential: Root.Warm, val clazz: FirClass) : Effect()
 }
 
-fun List<Effect>.viewChange(effect: Effect): List<Effect> = TODO()
+fun viewChange(effect: Effect, root: Potential): Effect {
+    return when (effect) {
+        is Promote -> {
+            val viewedPot = viewChange(effect.potential, root)
+            Promote(viewedPot)
+        }
+        is FieldAccess -> {
+            val viewedPot = viewChange(effect.potential, root)
+            FieldAccess(viewedPot, effect.field)
+        }
+        is MethodAccess -> {
+            val viewedPot = viewChange(effect.potential, root)
+            MethodAccess(viewedPot, effect.method)
+        }
+        is Init -> {
+            val viewedPot = viewChange(effect.potential, root) as Root.Warm
+            Init(viewedPot, effect.clazz)
+        }
+    }
+}
