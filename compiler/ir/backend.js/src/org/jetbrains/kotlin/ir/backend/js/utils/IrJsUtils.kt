a3ed332dbe8e10a7df033d2cbab81b04858cc52d
Anton Bannykh <anton.bannykh@jetbrains.com>
2022-04-15 10:52:56 +0000

    rrr/1.6.20/ilgonmic/kt-51973 [JS IR] Add non overridden property and method insode exported class
    [JS IR] Add method into exported interface in test
    
    [JS IR] Add interface properties cases to all file export test
    
    [JS IR] Fix usage of isExported inside IrJsUtils
    
    
    Co-authored-by: Anton Bannykh <Anton.Bannykh@jetbrains.com>
    
    Merge-request: KT-MR-6090
    Merged-by: Ilya Goncharov <Ilya.Goncharov@jetbrains.com>
    
    ^KT-51973 fixed

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/IrJsUtils.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/IrJsUtils.kt
@@ -7,20 +7,19 @@ package org.jetbrains.kotlin.ir.backend.js.utils
 
 import org.jetbrains.kotlin.descriptors.isClass
 import org.jetbrains.kotlin.descriptors.isInterface
+import org.jetbrains.kotlin.ir.backend.js.JsIrBackendContext
+import org.jetbrains.kotlin.ir.backend.js.export.isExported
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationWithVisibility
 import org.jetbrains.kotlin.ir.util.parentClassOrNull
 
-fun IrDeclaration.isExportedMember() =
+fun IrDeclaration.isExportedMember(context: JsIrBackendContext) =
     (this is IrDeclarationWithVisibility && visibility.isPublicAPI) &&
-            parentClassOrNull.let { it is IrClass && it.isJsExport() }
+            parentClassOrNull?.isExported(context) == true
 
-fun IrDeclaration?.isExportedClass() =
-    this is IrClass && kind.isClass && isJsExport()
+fun IrDeclaration?.isExportedClass(context: JsIrBackendContext) =
+    this is IrClass && kind.isClass && isExported(context)
 
-fun IrDeclaration?.isExportedInterface() =
-    this is IrClass && kind.isInterface && isJsExport()
-
-fun IrDeclaration.isExportedInterfaceMember() =
-    parentClassOrNull.isExportedInterface()
+fun IrDeclaration?.isExportedInterface(context: JsIrBackendContext) =
+    this is IrClass && kind.isInterface && isExported(context)
