f56999d93f0570e822116227b45f0c6fb3eac328
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-04-25 15:22:54 +0300

    [KDM] Revert shadow plugin update due to incompatibility with tests
    Original commit: https://github.com/Kotlin/dokka/commit/b1acc183368da3352a8b61614f0b943ae865b18b

--- libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+    implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
     implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.9.0")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
