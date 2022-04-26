1cb4f023b4d4abf23cbb6f4112e6866d74ceecc4
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2022-04-26 09:13:25 +0300

    Fir2Ir: simplify getCachedIrTypeParameter
    I could not figure out the reason for the special case in that function.
    Tests do not show anything useful.

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -7176,6 +7176,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         }
 
         @Test
+        @TestMetadata("genericSetter.kt")
+        public void testGenericSetter() throws Exception {
+            runTest("compiler/testData/codegen/box/compileKotlinAgainstKotlin/genericSetter.kt");
+        }
+
+        @Test
         @TestMetadata("importCompanion.kt")
         public void testImportCompanion() throws Exception {
             runTest("compiler/testData/codegen/box/compileKotlinAgainstKotlin/importCompanion.kt");
