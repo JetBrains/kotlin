8b2a6da5e7777c8f3c129b7358195da280c8311d
Daniel Santiago Rivera <danysantiago@google.com>
2022-05-03 07:27:51 -0700

    Support Context Receivers in Compose
    The context receivers parameters are treated like 'this' parameters, similar to the extension and dispatch receiver parameters, they take part in the 'changed' calculations and are required to participate in the slot.
    
    Context parameters will appear in the function descriptor after the extension receiver but before any actual declared parameter. They also appear in the IrFunction.valueParameters list and thus the composer and other parameter index calculations have been adjusted to account for them.
    
    Context parameters, even though always provided, take a bit in the defaults bitmask, at the call site Kotlin will produce a bitmask that is shifted by the amount of context params, therefore since Compose handles default param expressions it must also account for context params offset.
    
    Bug: 218707899 ( https://issuetracker.google.com/issues/218707899 )
    Test: ContextReceiverTests and ContextReceiversTransformTests
    Change-Id: I97ab380277f523def53624f1ef858b271221d6bd ( https://android-review.git.corp.google.com/q/I97ab380277f523def53624f1ef858b271221d6bd )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
@@ -218,21 +218,39 @@ fun bitsForSlot(bits: Int, slot: Int): Int {
 fun defaultsParamIndex(index: Int): Int = index / BITS_PER_INT
 fun defaultsBitIndex(index: Int): Int = index.rem(BITS_PER_INT)
 
+/**
+ * The number of implicit ('this') parameters the function has.
+ *
+ * Note that extension and dispatch receiver params will not show up in [IrFunction.valueParameters]
+ * but context receiver parameter ([IrFunction.contextReceiverParametersCount]) will.
+ */
 val IrFunction.thisParamCount
-    get() = (
-        if (dispatchReceiverParameter != null) 1 else 0
-        ) + (
-        if (extensionReceiverParameter != null) 1 else 0
-        )
+    get() = contextReceiverParametersCount +
+        (if (dispatchReceiverParameter != null) 1 else 0) +
+        (if (extensionReceiverParameter != null) 1 else 0)
 
+/**
+ * Calculates the number of 'changed' params needed based on the function's parameters.
+ *
+ * @param realValueParams The number of params defined by the user, those that are not implicit
+ * (no extension or context receivers) or synthetic (no %composer, %changed or %defaults).
+ * @param thisParams The number of implicit params, i.e. [IrFunction.thisParamCount]
+ */
 fun changedParamCount(realValueParams: Int, thisParams: Int): Int {
-    if (realValueParams == 0) return 1
     val totalParams = realValueParams + thisParams
+    if (totalParams == 0) return 1 // There is always at least 1 changed param
     return ceil(
         totalParams.toDouble() / SLOTS_PER_INT.toDouble()
     ).toInt()
 }
 
+/**
+ * Calculates the number of 'changed' params needed based on the function's total amount of
+ * parameters.
+ *
+ * @param totalParamsIncludingThisParams The total number of parameter including implicit and
+ * synthetic ones.
+ */
 fun changedParamCountFromTotal(totalParamsIncludingThisParams: Int): Int {
     var realParams = totalParamsIncludingThisParams
     realParams-- // composer param
@@ -245,20 +263,24 @@ fun changedParamCountFromTotal(totalParamsIncludingThisParams: Int): Int {
     return changedParams
 }
 
-fun defaultParamCount(realValueParams: Int): Int {
+/**
+ * Calculates the number of 'defaults' params needed based on the function's parameters.
+ *
+ * @param valueParams The numbers of params, usually the size of [IrFunction.valueParameters].
+ * Which includes context receivers params, but not extension param nor synthetic params.
+ */
+fun defaultParamCount(valueParams: Int): Int {
     return ceil(
-        realValueParams.toDouble() / BITS_PER_INT.toDouble()
+        valueParams.toDouble() / BITS_PER_INT.toDouble()
     ).toInt()
 }
 
 fun composeSyntheticParamCount(
     realValueParams: Int,
     thisParams: Int = 0,
-    hasDefaults: Boolean = false
 ): Int {
     return 1 + // composer param
-        changedParamCount(realValueParams, thisParams) +
-        if (hasDefaults) defaultParamCount(realValueParams) else 0
+        changedParamCount(realValueParams, thisParams)
 }
 
 interface IrChangedBitMaskValue {
@@ -1143,7 +1165,9 @@ class ComposableFunctionBodyTransformer(
             // NOTE: the unstable mask is indexed by valueParameter index, which is different
             // than the slotIndex but that is OKAY because we only care about defaults, which
             // also use the value parameter index.
-            val realParams = declaration.valueParameters.take(scope.realValueParamCount)
+            val realParams = declaration.valueParameters.take(
+                declaration.contextReceiverParametersCount + scope.realValueParamCount
+            )
             val unstableMask = realParams.map {
                 stabilityOf((it.varargElementType ?: it.type)).knownUnstable()
             }.toBooleanArray()
@@ -1613,8 +1637,10 @@ class ComposableFunctionBodyTransformer(
             )
         }
 
-        val parameterCount = function.symbol.descriptor.valueParameters.size
-        val changedIndex = numRealValueParameters + 1
+        val parameterCount = function.valueParameters.size
+        val contextParameterCount = function.contextReceiverParametersCount
+        val composerIndex = contextParameterCount + numRealValueParameters
+        val changedIndex = composerIndex + 1
         val defaultIndex = changedIndex + changedParamCount(
             numRealValueParameters,
             function.thisParamCount
@@ -1623,7 +1649,10 @@ class ComposableFunctionBodyTransformer(
         if (defaultParam == null) {
             require(parameterCount == defaultIndex) // param count is 1-based, index is 0-based
         } else {
-            require(parameterCount == defaultIndex + defaultParamCount(numRealValueParameters))
+            require(
+                parameterCount == defaultIndex +
+                    defaultParamCount(contextParameterCount + numRealValueParameters)
+            )
         }
 
         val lambda = IrFunctionImpl(
@@ -1687,7 +1716,7 @@ class ComposableFunctionBodyTransformer(
 
                         // new composer
                         putValueArgument(
-                            numRealValueParameters,
+                            composerIndex,
                             irGet(fn.valueParameters[0])
                         )
 
@@ -2708,6 +2737,7 @@ class ComposableFunctionBodyTransformer(
 
         val ownerFn = expression.symbol.owner
         val numValueParams = ownerFn.valueParameters.size
+        val numContextParams = ownerFn.contextReceiverParametersCount
         val numDefaults: Int
         val numChanged: Int
         val numRealValueParams: Int
@@ -2722,26 +2752,43 @@ class ComposableFunctionBodyTransformer(
             // be defaulted though, so we can calculate the number of real parameters based on
             // the total number of parameters
             numDefaults = 0
-            numChanged = changedParamCountFromTotal(numValueParams + ownerFn.thisParamCount)
+            numChanged = changedParamCountFromTotal(
+                // Subtracting context params from total since they are included in thisParams
+                numValueParams - numContextParams + ownerFn.thisParamCount
+            )
             numRealValueParams = numValueParams -
+                numContextParams -
                 1 - // composer param
                 numChanged
         } else {
-            numRealValueParams = ownerFn.valueParameters.indexOfLast {
+            // Context receiver params are value parameters and will precede real params, calculate
+            // the amount of real params by finding the index off the last real param (if any) and
+            // offsetting it by the amount of context receiver params.
+            val indexOfLastRealParam = ownerFn.valueParameters.indexOfLast {
                 !it.name.asString().startsWith('$')
-            } + 1
-            numDefaults = if (hasDefaults) defaultParamCount(numRealValueParams) else 0
+            }
+            numRealValueParams = if (indexOfLastRealParam != -1) {
+                (indexOfLastRealParam + 1) - numContextParams
+            } else {
+                0
+            }
+            numDefaults = if (hasDefaults) {
+                defaultParamCount(numContextParams + numRealValueParams)
+            } else {
+                0
+            }
             numChanged = changedParamCount(numRealValueParams, ownerFn.thisParamCount)
         }
 
         require(
+            numContextParams +
             numRealValueParams +
                 1 + // composer param
                 numChanged +
                 numDefaults == numValueParams
         )
 
-        val composerIndex = numRealValueParams
+        val composerIndex = numContextParams + numRealValueParams
         val changedArgIndex = composerIndex + 1
         val defaultArgIndex = changedArgIndex + numChanged
         val defaultArgs = (defaultArgIndex until numValueParams).map {
@@ -2756,9 +2803,10 @@ class ComposableFunctionBodyTransformer(
             }
         }
 
+        val contextMeta = mutableListOf<ParamMeta>()
         val paramMeta = mutableListOf<ParamMeta>()
 
-        for (index in 0 until numRealValueParams) {
+        for (index in 0 until numContextParams + numRealValueParams) {
             val arg = expression.getValueArgument(index)
             if (arg == null) {
                 val param = expression.symbol.owner.valueParameters[index]
@@ -2772,20 +2820,25 @@ class ComposableFunctionBodyTransformer(
                     continue
                 }
             }
-            val bitIndex = defaultsBitIndex(index)
-            val maskValue = if (hasDefaultArgs) defaultMasks[defaultsParamIndex(index)] else 0
-            val meta = paramMetaOf(arg, isProvided = maskValue and (0b1 shl bitIndex) == 0)
-
-            paramMeta.add(meta)
+            if (index < numContextParams) {
+                val meta = paramMetaOf(arg, isProvided = true)
+                contextMeta.add(meta)
+            } else {
+                val bitIndex = defaultsBitIndex(index)
+                val maskValue = if (hasDefaultArgs) defaultMasks[defaultsParamIndex(index)] else 0
+                val meta = paramMetaOf(arg, isProvided = maskValue and (0b1 shl bitIndex) == 0)
+                paramMeta.add(meta)
+            }
         }
 
         val extensionMeta = expression.extensionReceiver?.let { paramMetaOf(it, isProvided = true) }
         val dispatchMeta = expression.dispatchReceiver?.let { paramMetaOf(it, isProvided = true) }
 
         val changedParams = buildChangedParamsForCall(
-            paramMeta,
-            extensionMeta,
-            dispatchMeta
+            contextParams = contextMeta,
+            valueParams = paramMeta,
+            extensionParam = extensionMeta,
+            dispatchParam = dispatchMeta
         )
 
         changedParams.forEachIndexed { i, param ->
@@ -3082,13 +3135,17 @@ class ComposableFunctionBodyTransformer(
     }
 
     private fun buildChangedParamsForCall(
+        contextParams: List<ParamMeta>,
         valueParams: List<ParamMeta>,
         extensionParam: ParamMeta?,
         dispatchParam: ParamMeta?
     ): List<IrExpression> {
-        val allParams = listOfNotNull(extensionParam) + valueParams + listOfNotNull(dispatchParam)
+        val allParams = listOfNotNull(extensionParam) +
+            contextParams +
+            valueParams +
+            listOfNotNull(dispatchParam)
         // passing in 0 for thisParams since they should be included in the params list
-        val changedCount = changedParamCount(valueParams.size, allParams.size - valueParams.size)
+        val changedCount = changedParamCount(allParams.size, 0)
         val result = mutableListOf<IrExpression>()
         for (i in 0 until changedCount) {
             val start = i * SLOTS_PER_INT
@@ -3516,7 +3573,7 @@ class ComposableFunctionBodyTransformer(
             var realValueParamCount: Int = 0
                 private set
 
-            // slotCount will include the dispatchReceiver and extensionReceivers
+            // slotCount will include the dispatchReceiver, extensionReceivers and context receivers
             var slotCount: Int = 0
                 private set
 
@@ -3666,35 +3723,38 @@ class ComposableFunctionBodyTransformer(
                     }
                 }
                 slotCount = realValueParamCount
+                slotCount += function.contextReceiverParametersCount
                 if (function.extensionReceiverParameter != null) slotCount++
                 if (function.dispatchReceiverParameter != null) {
                     slotCount++
                 } else if (function.origin == IrDeclarationOrigin.LOCAL_FUNCTION_FOR_LAMBDA) {
                     slotCount++
                 }
-                changedParameter = if (composerParameter != null)
+                changedParameter = if (composerParameter != null) {
                     transformer.IrChangedBitMaskValueImpl(
                         changedParams,
                         slotCount
                     )
-                else
+                } else {
                     null
-                defaultParameter = if (defaultParams.isNotEmpty())
+                }
+                defaultParameter = if (defaultParams.isNotEmpty()) {
                     transformer.IrDefaultBitMaskValueImpl(
                         defaultParams,
-                        realValueParamCount
+                        function.contextReceiverParametersCount + realValueParamCount,
                     )
-                else
+                } else {
                     null
+                }
             }
 
             val isComposable = composerParameter != null
 
-            val allTrackedParams = listOfNotNull(
-                function.extensionReceiverParameter
-            ) + function.valueParameters.take(realValueParamCount) + listOfNotNull(
-                function.dispatchReceiverParameter
-            )
+            val allTrackedParams = listOfNotNull(function.extensionReceiverParameter) +
+                function.valueParameters.take(
+                    function.contextReceiverParametersCount + realValueParamCount
+                ) +
+                listOfNotNull(function.dispatchReceiverParameter)
 
             fun defaultIndexForSlotIndex(index: Int): Int {
                 return if (function.extensionReceiverParameter != null) index - 1 else index
@@ -4016,6 +4076,7 @@ class ComposableFunctionBodyTransformer(
 
         init {
             val actual = params.size
+            // passing in 0 for thisParams because slot count includes them
             val expected = changedParamCount(count, 0)
             require(actual == expected) {
                 "Function with $count params had $actual changed params but expected $expected"
