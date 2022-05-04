660620f1570dec5467abf3ec9fa4db8f3c4677d6
Jinseong Jeon <jsjeon@google.com>
2022-05-03 23:24:47 -0700

    AA: add JDK_HOME as dependent SDK module

--- analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtModuleByCompilerConfiguration.kt
+++ analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtModuleByCompilerConfiguration.kt
@@ -6,12 +6,13 @@
 package org.jetbrains.kotlin.analysis.project.structure.impl
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.ProjectScope
-import org.jetbrains.kotlin.analysis.project.structure.KtLibraryModule
-import org.jetbrains.kotlin.analysis.project.structure.KtLibrarySourceModule
-import org.jetbrains.kotlin.analysis.project.structure.KtModule
-import org.jetbrains.kotlin.analysis.project.structure.KtSourceModule
+import com.intellij.util.io.URLUtil
+import org.jetbrains.kotlin.analysis.api.impl.base.util.LibraryUtils
+import org.jetbrains.kotlin.analysis.project.structure.*
 import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
 import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
 import org.jetbrains.kotlin.cli.jvm.config.jvmModularRoots
@@ -22,6 +23,7 @@ import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
 import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatformAnalyzerServices
 import java.nio.file.Path
+import java.nio.file.Paths
 
 internal abstract class BaseKtModuleByCompilerConfiguration(
     private val compilerConfig: CompilerConfiguration,
@@ -51,7 +53,7 @@ internal class KtSourceModuleByCompilerConfiguration(
     project: Project,
     ktFiles: List<KtFile>,
 ) : BaseKtModuleByCompilerConfiguration(compilerConfig, project), KtSourceModule {
-    override val directRegularDependencies: List<KtLibraryModule> by lazy {
+    override val directRegularDependencies: List<KtBinaryModule> by lazy {
         val libraryRoots = compilerConfig.jvmModularRoots + compilerConfig.jvmClasspathRoots
         val libraryRootsByType = libraryRoots.groupBy { it.isDirectory }
         buildList {
@@ -68,6 +70,15 @@ internal class KtSourceModuleByCompilerConfiguration(
                     val root = it.toPath()
                     add(KtLibraryModuleByCompilerConfiguration(compilerConfig, project, root))
                 }
+            }
+            compilerConfig.get(JVMConfigurationKeys.JDK_HOME)?.let { jdkHome ->
+                val vfm = VirtualFileManager.getInstance()
+                val jdkHomePath = jdkHome.toPath()
+                val jdkHomeVirtualFile = vfm.findFileByNioPath(jdkHomePath)
+                val binaryRoots = LibraryUtils.findClassesFromJdkHome(jdkHomePath).map {
+                    Paths.get(URLUtil.extractPath(it))
+                }
+                add(KtSdkModuleByCompilerConfiguration(compilerConfig, project, "JDK", jdkHomeVirtualFile, binaryRoots))
             }
         }
     }
@@ -95,6 +106,21 @@ internal class KtLibraryModuleByCompilerConfiguration(
     }
 
     private val binaryRoots = listOf(root)
+
+    override fun getBinaryRoots(): Collection<Path> = binaryRoots
+}
+
+internal class KtSdkModuleByCompilerConfiguration(
+    compilerConfig: CompilerConfiguration,
+    project: Project,
+    override val sdkName: String,
+    sdkHome: VirtualFile?,
+    private val binaryRoots: Collection<Path>
+) : BaseKtModuleByCompilerConfiguration(compilerConfig, project), KtSdkModule {
+    override val directRegularDependencies: List<KtModule> get() = emptyList()
+
+    override val contentScope: GlobalSearchScope =
+        GlobalSearchScope.fileScope(project, sdkHome)
 
     override fun getBinaryRoots(): Collection<Path> = binaryRoots
 }
