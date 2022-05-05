9e63fb571b1117fe3a74f8f693495c444f370470
Jinseong Jeon <jsjeon@google.com>
2022-05-05 01:33:45 -0700

    AA: migrate PSI declaration provider (from IJ)

--- analysis/decompiled/light-classes-for-decompiled/build.gradle.kts
+++ analysis/decompiled/light-classes-for-decompiled/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 
 dependencies {
     api(project(":compiler:psi"))
+    api(project(":analysis:analysis-api"))
+    api(project(":analysis:analysis-api-providers"))
+    api(project(":analysis:project-structure"))
     api(project(":analysis:decompiled:decompiler-to-psi"))
     api(project(":compiler:light-classes"))
     implementation(intellijCore())
