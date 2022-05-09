876cc472b44c0cadc66b14e57319a15ae2f611b3
Mads Ager <ager@google.com>
2022-05-09 15:08:29 +0200

    Remove unused debug info functions.
    These functions return the underlying buffer for a std::string
    which you should never do as it is only valid while the std::string
    is in scope.

--- kotlin-native/llvmDebugInfoC/src/main/cpp/DebugInfoC.cpp
+++ kotlin-native/llvmDebugInfoC/src/main/cpp/DebugInfoC.cpp
@@ -281,15 +281,6 @@ LLVMValueRef LLVMBuilderGetCurrentFunction(LLVMBuilderRef builder) {
   return llvm::wrap(llvm::unwrap(builder)->GetInsertBlock()->getParent());
 }
 
-const char* LLVMBuilderGetCurrentBbName(LLVMBuilderRef builder) {
-  return llvm::unwrap(builder)->GetInsertBlock()->getName().str().c_str();
-}
-
-
-const char *DIGetSubprogramLinkName(DISubprogramRef sp) {
-  return llvm::unwrap(sp)->getLinkageName().str().c_str();
-}
-
 int DISubprogramDescribesFunction(DISubprogramRef sp, LLVMValueRef fn) {
   return llvm::unwrap(sp)->describes(llvm::cast<llvm::Function>(llvm::unwrap(fn)));
 }
