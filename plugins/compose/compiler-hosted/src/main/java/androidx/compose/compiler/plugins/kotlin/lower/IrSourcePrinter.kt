e63e11f0987d3b470b61407a760c8bbf3287e79f
Jim Sproch <jsproch@google.com>
2022-05-13 12:09:37 -0700

    Upgrade to Kotlin 1.7.0
    Change-Id: I22c13fa3266fc4d425da35f58ec08e74984d3a51 ( https://android-review.googlesource.com/q/I22c13fa3266fc4d425da35f58ec08e74984d3a51 )
    
    Moved from: https://github.com/androidx/androidx/commit/2105f53bf51883882bcccec57479d42ea8a4cc7d

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/IrSourcePrinter.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/IrSourcePrinter.kt
@@ -1011,7 +1011,7 @@ class IrSourcePrinterVisitor(
         return "${if (value < 0) "-" else ""}0b$result"
     }
 
-    override fun <T> visitConst(expression: IrConst<T>) {
+    override fun visitConst(expression: IrConst<*>) {
         val result = when (expression.kind) {
             is IrConstKind.Null -> "${expression.value}"
             is IrConstKind.Boolean -> "${expression.value}"
@@ -1269,7 +1269,7 @@ class IrSourcePrinterVisitor(
         print("<<SPREAD>>")
     }
 
-    override fun visitVariableAccess(expression: IrValueAccessExpression) {
+    override fun visitValueAccess(expression: IrValueAccessExpression) {
         print("<<VARACCESS>>")
     }
 
