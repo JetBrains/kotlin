samkenxstream/SAMkenXCCoreJetBrains-kotlin

[kpm] Replace KotlinExternalModelContainer with new Extras implementation
This newly introduced `Extras` shall present a more generic mechanism
of attaching data of a given type to any entity.

libraries/tools/kotlin-gradle-plugin-idea/src/test/kotlin/org/jetbrains/kotlin/gradle/kpm/idea/BackwardsCompatibilityDeserializationTest.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:OptIn(InternalKotlinGradlePluginApi::class)
+@file:OptIn(InternalKotlinGradlePluginApi::class, ExternalVariantApi::class)
 
 package org.jetbrains.kotlin.gradle.kpm.idea
 
@@ -13,10 +13,7 @@ import createProxyInstance
 import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.kotlin.dsl.create
 import org.gradle.testfixtures.ProjectBuilder
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelKey
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelSerializer.Companion.serializable
 import org.jetbrains.kotlin.gradle.kpm.external.ExternalVariantApi
-import org.jetbrains.kotlin.gradle.kpm.external.external
 import org.jetbrains.kotlin.gradle.kpm.idea.testFixtures.deserialize
 import org.jetbrains.kotlin.gradle.kpm.idea.testFixtures.serialize
 import org.jetbrains.kotlin.gradle.plugin.KotlinPm20PluginWrapper
@@ -24,6 +21,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinIosX64Variant
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinLinuxX64Variant
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinPm20ProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.jvm
+import org.jetbrains.kotlin.tooling.core.keyOf
 import unwrapProxyInstance
 import java.io.File
 import java.io.Serializable
@@ -41,6 +39,9 @@ import kotlin.test.*
  * - Assert the deserialized model is healthy
  */
 class BackwardsCompatibilityDeserializationTest {
+
+    data class RetainedModel(val id: Int) : Serializable
+    data class UnretainedModel(val id: Int)
 
     @Test
     fun `test - simple project`() {
@@ -79,22 +80,19 @@ class BackwardsCompatibilityDeserializationTest {
         }
     }
 
-    @OptIn(ExternalVariantApi::class)
+    @Ignore("No serialisation for extras implemented, yet")
     @Test
-    fun `test - attaching serializable models`() {
-        data class RetainedModel(val id: Int) : Serializable
-        data class UnretainedModel(val id: Int)
-
-        val retainedModelKey = KotlinExternalModelKey<RetainedModel>(serializable())
-        val unretainedModelKey = KotlinExternalModelKey<UnretainedModel>()
+    fun `test - attaching serializable extras`() {
+        val retainedModelKey = keyOf<RetainedModel>()
+        val unretainedModelKey = keyOf<UnretainedModel>()
 
         val project = ProjectBuilder.builder().build() as ProjectInternal
         project.plugins.apply(KotlinPm20PluginWrapper::class.java)
 
         /* Setup example project */
         val kotlinExtension = project.extensions.getByType(KotlinPm20ProjectExtension::class.java)
-        kotlinExtension.main.common.external[retainedModelKey] = RetainedModel(2411)
-        kotlinExtension.main.common.external[unretainedModelKey] = UnretainedModel(510)
+        kotlinExtension.main.common.extras[retainedModelKey] = RetainedModel(2411)
+        kotlinExtension.main.common.extras[unretainedModelKey] = UnretainedModel(510)
 
         val model = project.buildIdeaKotlinProjectModel()
         val deserializedModel = deserializeModelWithBackwardsCompatibleClasses(model)
@@ -108,10 +106,10 @@ class BackwardsCompatibilityDeserializationTest {
 
         run {
             val deserializedCommonFragment = unwrapProxyInstance(deserializedCommonFragmentProxy)
-            val external = deserializedCommonFragment.serialize().deserialize<IdeaKotlinFragment>().external
-            assertEquals(1, external.ids.size)
-            assertEquals(RetainedModel(2411), external[retainedModelKey])
-            assertNull(external[unretainedModelKey])
+            val extras = deserializedCommonFragment.serialize().deserialize<IdeaKotlinFragment>().extras
+            assertEquals(1, extras.keys.size)
+            assertEquals(RetainedModel(2411), extras[retainedModelKey])
+            assertNull(extras[unretainedModelKey])
         }
     }
 }
@@ -127,7 +125,7 @@ private fun getClasspathForBackwardsCompatibilityTest(): List<File> {
 
     return compatibilityTestClasspath.split(";").map { path -> File(path) }
         .onEach { file -> if (!file.exists()) println("[WARNING] Missing $file") }
-        .flatMap { file -> if(file.isDirectory) file.listFiles().orEmpty().toList() else listOf(file) }
+        .flatMap { file -> if (file.isDirectory) file.listFiles().orEmpty().toList() else listOf(file) }
 }
 
 private fun deserializeModelWithBackwardsCompatibleClasses(model: IdeaKotlinProjectModel): Any {

libraries/tools/kotlin-gradle-plugin-kpm-android/src/main/kotlin/org/jetbrains/kotlin/gradle/android/AndroidDsl.kt
@@ -3,17 +3,16 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:OptIn(InternalKotlinGradlePluginApi::class)
 
 package org.jetbrains.kotlin.gradle.android
 
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelKey
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelSerializer
-import org.jetbrains.kotlin.gradle.kpm.idea.InternalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinGradleFragment
+import org.jetbrains.kotlin.tooling.core.keyOf
 import java.io.File
 import java.io.Serializable
 
-val androidDslKey = KotlinExternalModelKey<AndroidDsl>(KotlinExternalModelSerializer.serializable())
+internal val androidDslKey = keyOf<AndroidDsl>()
+val KotlinGradleFragment.androidDsl: AndroidDsl? get() = this.extras[androidDslKey]
 
 class AndroidDsl : Serializable {
     var compileSdk = 0

libraries/tools/kotlin-tooling-core/src/main/kotlin/org/jetbrains/kotlin/tooling/core/ExtrasUtils.kt
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.tooling.core
+
+/**
+ *  Creates a value based key for accessing any [Extras] container
+ *
+ * @param T The type of data that is stored in the extras container
+ * ```kotlin
+ * keyOf<Int>() == keyOf<Int>()
+ * keyOf<Int>() != keyOf<String>()
+ * keyOf<List<Int>>() == keyOf<List<Int>>()
+ * keyOf<List<*>>() != keyOf<List<Int>>()
+ * ```
+ *
+ * @param name This typed keys can also be distinguished with an additional name. In this case
+ * ```kotlin
+ * keyOf<Int>() != keyOf<Int>("a")
+ * keyOf<Int>("a") == keyOf<Int>("a")
+ * keyOf<Int>("b") != keyOf<Int>("a")
+ * keyOf<String>("a") != keyOf<Int>("a")
+ * ```
+ */
+inline fun <reified T : Any> keyOf(name: String? = null): Extras.Key<T> =
+    Extras.Key(reifiedTypeSignatureOf(), name)
+
+fun emptyExtras(): Extras = EmptyExtras
+
+fun extrasOf() = emptyExtras()
+
+fun extrasOf(vararg entries: Extras.Entry<*>): Extras = ImmutableExtrasImpl(entries)
+
+fun mutableExtrasOf(): MutableExtras = MutableExtrasImpl()
+
+fun mutableExtrasOf(vararg entries: Extras.Entry<*>): MutableExtras = MutableExtrasImpl(entries.toList())
+
+fun Iterable<Extras.Entry<*>>.toExtras(): Extras = ImmutableExtrasImpl(this)
+
+fun Iterable<Extras.Entry<*>>.toMutableExtras(): MutableExtras = MutableExtrasImpl(this)
+
+infix fun <T : Any> Extras.Key<T>.withValue(value: T): Extras.Entry<T> = Extras.Entry(this, value)
+
+operator fun Extras.plus(entry: Extras.Entry<*>): Extras = ImmutableExtrasImpl(this.entries + entry)
+
+operator fun Extras.plus(entries: Iterable<Extras.Entry<*>>): Extras = ImmutableExtrasImpl(this.entries + entries)
+

libraries/tools/kotlin-tooling-core/src/test/kotlin/org/jetbrains/kotlin/tooling/core/ExtrasSerializableTest.kt
@@ -1,0 +1,55 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.tooling.core
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotSame
+import kotlin.test.assertSame
+
+class ExtrasSerializableTest {
+
+    @Test
+    fun `test - serialize deserialize - MutableExtras`() {
+        val extras = mutableExtrasOf(keyOf<String>() withValue "hello")
+        val deserializedExtras = extras.serialize().deserialize<Extras>()
+        assertNotSame(extras, deserializedExtras)
+        assertEquals(extras, deserializedExtras)
+    }
+
+    @Test
+    fun `test - serialize deserialize - EmptyExtras`() {
+        assertSame(emptyExtras(), emptyExtras())
+        assertSame(emptyExtras(), emptyExtras().serialize().deserialize())
+    }
+
+    @Test
+    fun `test - serialize deserialize - ImmutableExtras`() {
+        val extras = extrasOf(keyOf<String>() withValue "hello")
+        val deserializedExtras = extras.serialize().deserialize<Extras>()
+        assertNotSame(extras, deserializedExtras)
+        assertEquals(extras, deserializedExtras)
+        assertSame(extras.javaClass, deserializedExtras.javaClass)
+    }
+
+    private fun Any.serialize(): ByteArray {
+        return ByteArrayOutputStream().use { byteArrayOutputStream ->
+            ObjectOutputStream(byteArrayOutputStream).use { objectOutputStream -> objectOutputStream.writeObject(this) }
+            byteArrayOutputStream.toByteArray()
+        }
+    }
+
+    private inline fun <reified T : Any> ByteArray.deserialize(): T {
+        return ObjectInputStream(ByteArrayInputStream(this)).use { stream ->
+            stream.readObject() as T
+        }
+    }
+}
+

libraries/tools/kotlin-tooling-core/src/test/kotlin/org/jetbrains/kotlin/tooling/core/ReifiedTypeSignatureTest.kt
@@ -1,0 +1,105 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.tooling.core
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class ReifiedTypeSignatureTest {
+
+    class Box<T>
+
+    class Outer {
+        inner class Inner
+    }
+
+    open class Recursive<T : Recursive<T>>
+
+    class AnyRecursive : Recursive<AnyRecursive>()
+
+    @Test
+    fun `test - sample 0`() {
+        assertEquals(
+            "kotlin.collections.List<kotlin.Int>", reifiedTypeSignatureOf<List<Int>>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 1`() {
+        assertEquals(
+            "kotlin.collections.List<*>", reifiedTypeSignatureOf<List<*>>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 2`() {
+        assertEquals(
+            "kotlin.String", reifiedTypeSignatureOf<String>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 3`() {
+        assertEquals(
+            "kotlin.collections.Map<kotlin.String, kotlin.collections.List<kotlin.Pair<kotlin.Int, kotlin.String>>>",
+            reifiedTypeSignatureOf<Map<String, List<Pair<Int, String>>>>().signature
+        )
+    }
+
+
+    @Test
+    fun `test - sample 5 - inner class`() {
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Outer.Inner",
+            reifiedTypeSignatureOf<Outer.Inner>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 6 - recursive types`() {
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Recursive<*>",
+            reifiedTypeSignatureOf<Recursive<*>>().signature
+        )
+
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.AnyRecursive",
+            reifiedTypeSignatureOf<AnyRecursive>().signature
+        )
+
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest" +
+                    ".Recursive<org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.AnyRecursive>",
+            reifiedTypeSignatureOf<Recursive<AnyRecursive>>().signature
+        )
+    }
+
+    @Test
+    fun `test sample 7 - nullable`() {
+        assertEquals("kotlin.collections.List<*>?", reifiedTypeSignatureOf<List<*>?>().signature)
+        assertEquals("kotlin.collections.List<kotlin.Int?>", reifiedTypeSignatureOf<List<Int?>>().signature)
+    }
+
+    @Test
+    fun `test sample 8 - variance`() {
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Box<out kotlin.Int>",
+            reifiedTypeSignatureOf<Box<out Int>>().signature
+        )
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Box<in kotlin.Int>",
+            reifiedTypeSignatureOf<Box<in Int>>().signature
+        )
+    }
+
+    @Suppress("RemoveExplicitTypeArguments", "TYPE_INFERENCE_ONLY_INPUT_TYPES_WARNING")
+    @Test
+    fun `test - equals`() {
+        assertEquals(reifiedTypeSignatureOf<List<Int>>(), reifiedTypeSignatureOf<List<Int>>())
+        assertNotEquals(reifiedTypeSignatureOf<List<Int?>>(), reifiedTypeSignatureOf<List<Int>>())
+    }
+}

JetBrains/kotlin

libraries/tools/kotlin-gradle-plugin-idea/src/test/kotlin/org/jetbrains/kotlin/gradle/kpm/idea/BackwardsCompatibilityDeserializationTest.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:OptIn(InternalKotlinGradlePluginApi::class)
+@file:OptIn(InternalKotlinGradlePluginApi::class, ExternalVariantApi::class)
 
 package org.jetbrains.kotlin.gradle.kpm.idea
 
@@ -13,10 +13,7 @@ import createProxyInstance
 import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.kotlin.dsl.create
 import org.gradle.testfixtures.ProjectBuilder
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelKey
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelSerializer.Companion.serializable
 import org.jetbrains.kotlin.gradle.kpm.external.ExternalVariantApi
-import org.jetbrains.kotlin.gradle.kpm.external.external
 import org.jetbrains.kotlin.gradle.kpm.idea.testFixtures.deserialize
 import org.jetbrains.kotlin.gradle.kpm.idea.testFixtures.serialize
 import org.jetbrains.kotlin.gradle.plugin.KotlinPm20PluginWrapper
@@ -24,6 +21,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinIosX64Variant
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinLinuxX64Variant
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinPm20ProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.jvm
+import org.jetbrains.kotlin.tooling.core.extrasKeyOf
 import unwrapProxyInstance
 import java.io.File
 import java.io.Serializable
@@ -41,6 +39,9 @@ import kotlin.test.*
  * - Assert the deserialized model is healthy
  */
 class BackwardsCompatibilityDeserializationTest {
+
+    data class RetainedModel(val id: Int) : Serializable
+    data class UnretainedModel(val id: Int)
 
     @Test
     fun `test - simple project`() {
@@ -79,22 +80,19 @@ class BackwardsCompatibilityDeserializationTest {
         }
     }
 
-    @OptIn(ExternalVariantApi::class)
+    @Ignore("No serialisation for extras implemented, yet")
     @Test
-    fun `test - attaching serializable models`() {
-        data class RetainedModel(val id: Int) : Serializable
-        data class UnretainedModel(val id: Int)
-
-        val retainedModelKey = KotlinExternalModelKey<RetainedModel>(serializable())
-        val unretainedModelKey = KotlinExternalModelKey<UnretainedModel>()
+    fun `test - attaching serializable extras`() {
+        val retainedModelKey = extrasKeyOf<RetainedModel>()
+        val unretainedModelKey = extrasKeyOf<UnretainedModel>()
 
         val project = ProjectBuilder.builder().build() as ProjectInternal
         project.plugins.apply(KotlinPm20PluginWrapper::class.java)
 
         /* Setup example project */
         val kotlinExtension = project.extensions.getByType(KotlinPm20ProjectExtension::class.java)
-        kotlinExtension.main.common.external[retainedModelKey] = RetainedModel(2411)
-        kotlinExtension.main.common.external[unretainedModelKey] = UnretainedModel(510)
+        kotlinExtension.main.common.extras[retainedModelKey] = RetainedModel(2411)
+        kotlinExtension.main.common.extras[unretainedModelKey] = UnretainedModel(510)
 
         val model = project.buildIdeaKotlinProjectModel()
         val deserializedModel = deserializeModelWithBackwardsCompatibleClasses(model)
@@ -108,10 +106,10 @@ class BackwardsCompatibilityDeserializationTest {
 
         run {
             val deserializedCommonFragment = unwrapProxyInstance(deserializedCommonFragmentProxy)
-            val external = deserializedCommonFragment.serialize().deserialize<IdeaKotlinFragment>().external
-            assertEquals(1, external.ids.size)
-            assertEquals(RetainedModel(2411), external[retainedModelKey])
-            assertNull(external[unretainedModelKey])
+            val extras = deserializedCommonFragment.serialize().deserialize<IdeaKotlinFragment>().extras
+            assertEquals(1, extras.keys.size)
+            assertEquals(RetainedModel(2411), extras[retainedModelKey])
+            assertNull(extras[unretainedModelKey])
         }
     }
 }
@@ -127,7 +125,7 @@ private fun getClasspathForBackwardsCompatibilityTest(): List<File> {
 
     return compatibilityTestClasspath.split(";").map { path -> File(path) }
         .onEach { file -> if (!file.exists()) println("[WARNING] Missing $file") }
-        .flatMap { file -> if(file.isDirectory) file.listFiles().orEmpty().toList() else listOf(file) }
+        .flatMap { file -> if (file.isDirectory) file.listFiles().orEmpty().toList() else listOf(file) }
 }
 
 private fun deserializeModelWithBackwardsCompatibleClasses(model: IdeaKotlinProjectModel): Any {

libraries/tools/kotlin-gradle-plugin-kpm-android/src/main/kotlin/org/jetbrains/kotlin/gradle/android/AndroidDsl.kt
@@ -3,17 +3,16 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:OptIn(InternalKotlinGradlePluginApi::class)
 
 package org.jetbrains.kotlin.gradle.android
 
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelKey
-import org.jetbrains.kotlin.gradle.kpm.KotlinExternalModelSerializer
-import org.jetbrains.kotlin.gradle.kpm.idea.InternalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinGradleFragment
+import org.jetbrains.kotlin.tooling.core.extrasKeyOf
 import java.io.File
 import java.io.Serializable
 
-val androidDslKey = KotlinExternalModelKey<AndroidDsl>(KotlinExternalModelSerializer.serializable())
+internal val androidDslKey = extrasKeyOf<AndroidDsl>()
+val KotlinGradleFragment.androidDsl: AndroidDsl? get() = this.extras[androidDslKey]
 
 class AndroidDsl : Serializable {
     var compileSdk = 0

libraries/tools/kotlin-tooling-core/src/main/kotlin/org/jetbrains/kotlin/tooling/core/ExtrasUtils.kt
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.tooling.core
+
+/**
+ *  Creates a value based key for accessing any [Extras] container
+ *
+ * @param T The type of data that is stored in the extras container
+ * ```kotlin
+ * extrasKeyOf<Int>() == extrasKeyOf<Int>()
+ * extrasKeyOf<Int>() != extrasKeyOf<String>()
+ * extrasKeyOf<List<Int>>() == extrasKeyOf<List<Int>>()
+ * extrasKeyOf<List<*>>() != extrasKeyOf<List<Int>>()
+ * ```
+ *
+ * @param name This typed keys can also be distinguished with an additional name. In this case
+ * ```kotlin
+ * extrasKeyOf<Int>() != extrasKeyOf<Int>("a")
+ * extrasKeyOf<Int>("a") == extrasKeyOf<Int>("a")
+ * extrasKeyOf<Int>("b") != extrasKeyOf<Int>("a")
+ * extrasKeyOf<String>("a") != extrasKeyOf<Int>("a")
+ * ```
+ */
+inline fun <reified T : Any> extrasKeyOf(name: String? = null): Extras.Key<T> =
+    Extras.Key(ReifiedTypeSignature(), name)
+
+fun emptyExtras(): Extras = EmptyExtras
+
+fun extrasOf() = emptyExtras()
+
+fun extrasOf(vararg entries: Extras.Entry<*>): Extras = if (entries.isEmpty()) EmptyExtras else ImmutableExtrasImpl(entries)
+
+fun mutableExtrasOf(): MutableExtras = MutableExtrasImpl()
+
+fun mutableExtrasOf(vararg entries: Extras.Entry<*>): MutableExtras = MutableExtrasImpl(entries.toList())
+
+fun Iterable<Extras.Entry<*>>.toExtras(): Extras = ImmutableExtrasImpl(this)
+
+fun Iterable<Extras.Entry<*>>.toMutableExtras(): MutableExtras = MutableExtrasImpl(this)
+
+infix fun <T : Any> Extras.Key<T>.withValue(value: T): Extras.Entry<T> = Extras.Entry(this, value)
+
+operator fun Extras.plus(entry: Extras.Entry<*>): Extras = ImmutableExtrasImpl(this.entries + entry)
+
+operator fun Extras.plus(entries: Iterable<Extras.Entry<*>>): Extras = ImmutableExtrasImpl(this.entries + entries)
+

libraries/tools/kotlin-tooling-core/src/test/kotlin/org/jetbrains/kotlin/tooling/core/ExtrasSerializableTest.kt
@@ -1,0 +1,55 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.tooling.core
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotSame
+import kotlin.test.assertSame
+
+class ExtrasSerializableTest {
+
+    @Test
+    fun `test - serialize deserialize - MutableExtras`() {
+        val extras = mutableExtrasOf(extrasKeyOf<String>() withValue "hello")
+        val deserializedExtras = extras.serialize().deserialize<Extras>()
+        assertNotSame(extras, deserializedExtras)
+        assertEquals(extras, deserializedExtras)
+    }
+
+    @Test
+    fun `test - serialize deserialize - EmptyExtras`() {
+        assertSame(emptyExtras(), emptyExtras())
+        assertSame(emptyExtras(), emptyExtras().serialize().deserialize())
+    }
+
+    @Test
+    fun `test - serialize deserialize - ImmutableExtras`() {
+        val extras = extrasOf(extrasKeyOf<String>() withValue "hello")
+        val deserializedExtras = extras.serialize().deserialize<Extras>()
+        assertNotSame(extras, deserializedExtras)
+        assertEquals(extras, deserializedExtras)
+        assertSame(extras.javaClass, deserializedExtras.javaClass)
+    }
+
+    private fun Any.serialize(): ByteArray {
+        return ByteArrayOutputStream().use { byteArrayOutputStream ->
+            ObjectOutputStream(byteArrayOutputStream).use { objectOutputStream -> objectOutputStream.writeObject(this) }
+            byteArrayOutputStream.toByteArray()
+        }
+    }
+
+    private inline fun <reified T : Any> ByteArray.deserialize(): T {
+        return ObjectInputStream(ByteArrayInputStream(this)).use { stream ->
+            stream.readObject() as T
+        }
+    }
+}
+

libraries/tools/kotlin-tooling-core/src/test/kotlin/org/jetbrains/kotlin/tooling/core/ReifiedTypeSignatureTest.kt
@@ -1,0 +1,105 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.tooling.core
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class ReifiedTypeSignatureTest {
+
+    class Box<T>
+
+    class Outer {
+        inner class Inner
+    }
+
+    open class Recursive<T : Recursive<T>>
+
+    class AnyRecursive : Recursive<AnyRecursive>()
+
+    @Test
+    fun `test - sample 0`() {
+        assertEquals(
+            "kotlin.collections.List<kotlin.Int>", ReifiedTypeSignature<List<Int>>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 1`() {
+        assertEquals(
+            "kotlin.collections.List<*>", ReifiedTypeSignature<List<*>>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 2`() {
+        assertEquals(
+            "kotlin.String", ReifiedTypeSignature<String>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 3`() {
+        assertEquals(
+            "kotlin.collections.Map<kotlin.String, kotlin.collections.List<kotlin.Pair<kotlin.Int, kotlin.String>>>",
+            ReifiedTypeSignature<Map<String, List<Pair<Int, String>>>>().signature
+        )
+    }
+
+
+    @Test
+    fun `test - sample 5 - inner class`() {
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Outer.Inner",
+            ReifiedTypeSignature<Outer.Inner>().signature
+        )
+    }
+
+    @Test
+    fun `test - sample 6 - recursive types`() {
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Recursive<*>",
+            ReifiedTypeSignature<Recursive<*>>().signature
+        )
+
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.AnyRecursive",
+            ReifiedTypeSignature<AnyRecursive>().signature
+        )
+
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest" +
+                    ".Recursive<org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.AnyRecursive>",
+            ReifiedTypeSignature<Recursive<AnyRecursive>>().signature
+        )
+    }
+
+    @Test
+    fun `test sample 7 - nullable`() {
+        assertEquals("kotlin.collections.List<*>?", ReifiedTypeSignature<List<*>?>().signature)
+        assertEquals("kotlin.collections.List<kotlin.Int?>", ReifiedTypeSignature<List<Int?>>().signature)
+    }
+
+    @Test
+    fun `test sample 8 - variance`() {
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Box<out kotlin.Int>",
+            ReifiedTypeSignature<Box<out Int>>().signature
+        )
+        assertEquals(
+            "org.jetbrains.kotlin.tooling.core.ReifiedTypeSignatureTest.Box<in kotlin.Int>",
+            ReifiedTypeSignature<Box<in Int>>().signature
+        )
+    }
+
+    @Suppress("RemoveExplicitTypeArguments", "TYPE_INFERENCE_ONLY_INPUT_TYPES_WARNING")
+    @Test
+    fun `test - equals`() {
+        assertEquals(ReifiedTypeSignature<List<Int>>(), ReifiedTypeSignature<List<Int>>())
+        assertNotEquals(ReifiedTypeSignature<List<Int?>>(), ReifiedTypeSignature<List<Int>>())
+    }
+}