2476d1bbb6b53792d736268d5646df300f18e917
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2022-05-23 16:10:26 +0300

    [IR][tests] Extend test for IR linkage issues related to functions

--- compiler/testData/klibABI/removeFunction/lib1/l1.kt
+++ compiler/testData/klibABI/removeFunction/lib1/l1.kt
@@ -1,5 +1,7 @@
-
-
 fun foo(): String = "FAIL1"
+fun exp_foo(): String = "FAIL2"
 
-fun exp_foo() = "FAIL2"
+class A {
+    fun foo(): String = "FAIL3"
+    fun exp_foo(): String = "FAIL4"
+}
--- compiler/testData/klibABI/removeFunction/main/m.kt
+++ compiler/testData/klibABI/removeFunction/main/m.kt
@@ -2,16 +2,29 @@ fun test1(): String {
     try {
         return qux(true)
     } catch(e: Throwable) {
-        if (e.isLinkageError("/exp_foo")) return "O"
+        if (e.isLinkageError("/exp_foo")) return "OK"
     }
 
-    return "FAIL2"
+    return "FAIL5"
 }
 
 fun test2(): String = qux(false)
 
+fun test3(): String {
+    try {
+        return qux2(true)
+    } catch(e: Throwable) {
+        if (e.isLinkageError("/A.exp_foo")) return "OK"
+    }
+
+    return "FAIL6"
+}
+
+fun test4(): String = qux2(false)
+
 fun box(): String {
-    return test1() + test2()
+    val result = test1() + test2() + test3() + test4()
+    return if (result == "OKOKOKOK") "OK" else result
 }
 
 private fun Throwable.isLinkageError(symbolName: String): Boolean =
