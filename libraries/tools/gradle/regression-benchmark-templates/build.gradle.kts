262fb138ba9283a7af727cbf5406a0ef0020221e
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-05-23 17:09:15 +0200

    Update kotlinx-dataframe to 0.8.0-rc-8 version

--- libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
+++ libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
@@ -7,6 +7,5 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
     implementation("org.slf4j:slf4j-nop:1.7.32")
-    // Dev version contains the fix for this bug: https://github.com/Kotlin/dataframe/issues/101
-    implementation("org.jetbrains.kotlinx:dataframe:0.8.0-dev-952")
+    implementation("org.jetbrains.kotlinx:dataframe:0.8.0-rc-8")
 }
