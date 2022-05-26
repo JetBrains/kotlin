2af7fba9a82cb4bb836e02f6a100733235b2d0f7
Jinseong Jeon <jsjeon@google.com>
2022-05-26 11:49:03 -0700

    AA: introduce KtModule builders

---
+++ analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtLibrarySourceModuleImpl.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.project.structure.impl
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.analysis.project.structure.KtLibraryModule
+import org.jetbrains.kotlin.analysis.project.structure.KtLibrarySourceModule
+import org.jetbrains.kotlin.analysis.project.structure.KtModule
+import org.jetbrains.kotlin.platform.TargetPlatform
+import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
+
+internal class KtLibrarySourceModuleImpl(
+    override val directRegularDependencies: List<KtModule>,
+    override val directRefinementDependencies: List<KtModule>,
+    override val directFriendDependencies: List<KtModule>,
+    override val contentScope: GlobalSearchScope,
+    override val platform: TargetPlatform,
+    override val project: Project,
+    override val libraryName: String,
+    override val binaryLibrary: KtLibraryModule,
+) : KtLibrarySourceModule, KtModuleWithPlatform {
+    override val analyzerServices: PlatformDependentAnalyzerServices = super.analyzerServices
+}
