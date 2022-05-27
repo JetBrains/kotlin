314d9b9477e17b57e2df3731d3e98865fcf20439
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-05-27 16:47:42 +0200

    [analysis] move DecompiledPsiDeclarationProvider to symbol light classes module to avoid circular dependencies

--- analysis/decompiled/light-classes-for-decompiled/build.gradle.kts
+++ analysis/decompiled/light-classes-for-decompiled/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 
 dependencies {
     api(project(":compiler:psi"))
-    api(project(":analysis:analysis-api"))
-    api(project(":analysis:analysis-api-providers"))
-    api(project(":analysis:project-structure"))
     api(project(":analysis:decompiled:decompiler-to-psi"))
     api(project(":compiler:light-classes"))
     implementation(intellijCore())
