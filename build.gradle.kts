6bbd0075600832a93b9b6068e150c97dec773ae4
Nikita Bobko <nikita.bobko@jetbrains.com>
2022-05-27 07:27:53 +0200

    Refactoring: "suck in" `kotlin-reflect` and `kotlin-daemon-client` into `compilerModulesForJps`
    This commit places all dependencies of JPS plugin into a single place --
    `compilerModulesForJps`. I will need this small refactoring for the next
    commits.
    
    This commit doesn't change any logic because
    `prepare/ide-plugin-dependencies/kotlin-jps-plugin-classpath/build.gradle.kts`
    is the only `compilerModulesForJps` user right now.

--- build.gradle.kts
+++ build.gradle.kts
@@ -244,6 +244,8 @@ extra["compilerModules"] =
 
 
 extra["compilerModulesForJps"] = listOf(
+    ":kotlin-daemon-client",
+    ":kotlin-reflect",
     ":kotlin-build-common",
     ":kotlin-util-io",
     ":kotlin-util-klib",
--- prepare/ide-plugin-dependencies/kotlin-jps-plugin-classpath/build.gradle.kts
+++ prepare/ide-plugin-dependencies/kotlin-jps-plugin-classpath/build.gradle.kts
@@ -2,7 +2,7 @@ idePluginDependency {
     @Suppress("UNCHECKED_CAST")
     val compilerComponents = rootProject.extra["compilerModulesForJps"] as List<String>
 
-    val otherProjects = listOf(":kotlin-daemon-client", ":jps:jps-plugin", ":jps:jps-common", ":kotlin-reflect")
+    val otherProjects = listOf(":jps:jps-plugin", ":jps:jps-common")
 
     publishProjectJars(compilerComponents + otherProjects, libraryDependencies = listOf(protobufFull()))
 }
