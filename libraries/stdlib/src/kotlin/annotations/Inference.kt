de3f5497ab6c30270de4876c47289191b286aeed
Victor Petukhov <victor.petukhov@jetbrains.com>
2022-05-31 14:17:18 +0200

    [Stdlib] Deprecate BuilderInference annotation
    ^KT-52400 Fixed

--- libraries/stdlib/src/kotlin/annotations/Inference.kt
+++ libraries/stdlib/src/kotlin/annotations/Inference.kt
@@ -34,6 +34,8 @@ import kotlin.experimental.ExperimentalTypeInference
 @Retention(AnnotationRetention.BINARY)
 @SinceKotlin("1.3")
 @ExperimentalTypeInference
+@Deprecated("BuilderInference annotation may not be used anymore. The builder inference is enabled automatically for builder calls if needed")
+@DeprecatedSinceKotlin(warningSince = "1.7", errorSince = "1.9", hiddenSince = "1.10")
 public annotation class BuilderInference
 
 
