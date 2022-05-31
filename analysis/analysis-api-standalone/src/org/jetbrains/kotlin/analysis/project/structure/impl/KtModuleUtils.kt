4cfefbd92157ef1069434e3fd8d66e1b1b9e7a18
Jinseong Jeon <jsjeon@google.com>
2022-05-31 10:40:48 -0700

    AA: introduce ProjectStructureProvider builder

--- analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtModuleUtils.kt
+++ analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtModuleUtils.kt
@@ -5,16 +5,38 @@
 
 package org.jetbrains.kotlin.analysis.project.structure.impl
 
+import com.google.common.io.Files
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.ProjectScope
+import com.intellij.util.io.URLUtil
+import org.jetbrains.kotlin.analysis.api.impl.base.util.LibraryUtils
+import org.jetbrains.kotlin.analysis.project.structure.ProjectStructureProvider
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSdkModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildProjectStructureProvider
 import org.jetbrains.kotlin.analyzer.common.CommonPlatformAnalyzerServices
+import org.jetbrains.kotlin.cli.jvm.compiler.TopDownAnalyzerFacadeForJVM
+import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
+import org.jetbrains.kotlin.cli.jvm.config.jvmModularRoots
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.JVMConfigurationKeys
+import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.js.resolve.JsPlatformAnalyzerServices
 import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.platform.isCommon
 import org.jetbrains.kotlin.platform.js.isJs
+import org.jetbrains.kotlin.platform.jvm.JdkPlatform
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.platform.konan.isNative
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
 import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatformAnalyzerServices
 import org.jetbrains.kotlin.resolve.konan.platform.NativePlatformAnalyzerServices
+import java.nio.file.Paths
 
 internal fun TargetPlatform.getAnalyzerServices(): PlatformDependentAnalyzerServices {
     return when {
@@ -26,3 +48,73 @@ internal fun TargetPlatform.getAnalyzerServices(): PlatformDependentAnalyzerServ
     }
 }
 
+internal fun buildKtModuleProviderByCompilerConfiguration(
+    compilerConfig: CompilerConfiguration,
+    project: Project,
+    ktFiles: List<KtFile>,
+): ProjectStructureProvider = buildProjectStructureProvider {
+    addModule(
+        buildKtSourceModule {
+            val platform = TargetPlatform(setOf(JdkPlatform(JvmTarget.DEFAULT)))
+            val moduleName = compilerConfig.get(CommonConfigurationKeys.MODULE_NAME) ?: "<no module name provided>"
+
+            val libraryRoots = compilerConfig.jvmModularRoots + compilerConfig.jvmClasspathRoots
+            val libraryRootsByType = libraryRoots.groupBy { it.isDirectory }
+            libraryRootsByType[true]?.let { directories ->
+                directories.forEach {
+                    // E.g., project/app/build/intermediates/javac/debug/classes
+                    val root = it.toPath()
+                    addRegularDependency(
+                        buildKtLibraryModule {
+                            contentScope = ProjectScope.getLibrariesScope(project)
+                            this.platform = platform
+                            this.project = project
+                            binaryRoots = listOf(root)
+                            libraryName = "$moduleName-${root.toString().replace("/", "-")}"
+                        }
+                    )
+                }
+            }
+            libraryRootsByType[false]?.let { jars ->
+                jars.forEach {
+                    // E.g., project/libs/libA/a.jar
+                    val root = it.toPath()
+                    addRegularDependency(
+                        buildKtLibraryModule {
+                            contentScope = ProjectScope.getLibrariesScope(project)
+                            this.platform = platform
+                            this.project = project
+                            binaryRoots = listOf(root)
+                            libraryName = Files.getNameWithoutExtension(root.toString())
+                            isBuiltinsContainingStdlib =
+                                libraryName.startsWith("kotlin-stdlib") &&
+                                        !libraryName.contains("common") && !libraryName.contains("jdk")
+                        }
+                    )
+                }
+            }
+            compilerConfig.get(JVMConfigurationKeys.JDK_HOME)?.let { jdkHome ->
+                val vfm = VirtualFileManager.getInstance()
+                val jdkHomePath = jdkHome.toPath()
+                val jdkHomeVirtualFile = vfm.findFileByNioPath(jdkHomePath)
+                val binaryRoots = LibraryUtils.findClassesFromJdkHome(jdkHomePath).map {
+                    Paths.get(URLUtil.extractPath(it))
+                }
+                addRegularDependency(
+                    buildKtSdkModule {
+                        contentScope = GlobalSearchScope.fileScope(project, jdkHomeVirtualFile)
+                        this.platform = platform
+                        this.project = project
+                        this.binaryRoots = binaryRoots
+                        sdkName = "JDK for $moduleName"
+                    }
+                )
+            }
+
+            contentScope = TopDownAnalyzerFacadeForJVM.newModuleSearchScope(project, ktFiles)
+            this.platform = platform
+            this.project = project
+            this.moduleName = moduleName
+        }
+    )
+}
