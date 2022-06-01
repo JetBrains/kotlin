9b56a01d7407460b5adae2140e8af723392d7788
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-06-01 18:05:33 +0300

    [Plugins] Include backend parts of noarg and parcelize into jars for IDE

--- prepare/ide-plugin-dependencies/noarg-compiler-plugin-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/noarg-compiler-plugin-for-ide/build.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     kotlin("jvm")
 }
 
-publishJarsForIde(listOf(":kotlin-noarg-compiler-plugin.k1"))
+publishJarsForIde(listOf(":kotlin-noarg-compiler-plugin.k1", ":kotlin-noarg-compiler-plugin.backend"))
--- prepare/ide-plugin-dependencies/parcelize-compiler-plugin-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/parcelize-compiler-plugin-for-ide/build.gradle.kts
@@ -6,6 +6,7 @@ publishJarsForIde(
     listOf(
         ":plugins:parcelize:parcelize-compiler:parcelize.common",
         ":plugins:parcelize:parcelize-compiler:parcelize.k1",
+        ":plugins:parcelize:parcelize-compiler:parcelize.backend",
         ":plugins:parcelize:parcelize-runtime"
     )
 )
