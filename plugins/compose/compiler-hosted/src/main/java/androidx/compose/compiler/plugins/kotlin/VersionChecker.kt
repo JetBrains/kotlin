5e7c761531c8ebd95b4064e2834a575427cde68b
Nick Anthony <nickanthony@google.com>
2022-06-06 15:32:43 -0400

    Separate Compose Compiler Version and move Compiler to 1.2.0
    Test: gw verifyDependencyVersions checkApi bOS
    Change-Id: Ibdaa222fec0de0a4c51ad42e1aadddc993e74c4c ( https://android-review.git.corp.google.com/q/Ibdaa222fec0de0a4c51ad42e1aadddc993e74c4c )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -92,7 +92,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.2.0-rc01"
+        const val compilerVersion: String = "1.2.0"
         private val minimumRuntimeVersion: String
             get() = versionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
