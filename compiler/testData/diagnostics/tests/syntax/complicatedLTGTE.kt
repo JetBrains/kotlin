3ba11711d2022757f3d266288275330295e6a70b
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-06-08 16:52:00 +0300

    Adjust test data after reverting fix for KT-35811
    (see previous commit)
    
    ^KT-52684 Fixed

--- compiler/testData/diagnostics/tests/syntax/complicatedLTGTE.kt
+++ compiler/testData/diagnostics/tests/syntax/complicatedLTGTE.kt
@@ -1,8 +1,9 @@
+// FIR_IDENTICAL
 // SKIP_TXT
 // ISSUE: KT-52684
 
 fun test(x: Int, y: Int) {
-    if (<!ASSIGNMENT_IN_EXPRESSION_CONTEXT!><!UNRESOLVED_REFERENCE!>x<!> < (<!SYNTAX!>if<!><!SYNTAX!><!> <!SYNTAX!>(<!><!UNRESOLVED_REFERENCE!><!SYNTAX!><!>y<!> >= 115<!>) 1 else 2<!SYNTAX!>))<!> {
+    if (x < (if (y >= 115) 1 else 2)) {
         Unit
     }
 }
