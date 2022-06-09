c79e38f32618802dea77f400d8636b707857b7a7
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-06-09 16:14:10 +0300

    [FIR] Report a warning in FE 1.0 for annotations in where clauses

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/DeclarationsChecker.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/DeclarationsChecker.kt
@@ -537,6 +537,12 @@ class DeclarationsChecker(
             checkSupertypesForConsistency(typeParameterDescriptor, typeParameter)
             checkOnlyOneTypeParameterBound(typeParameterDescriptor, typeParameter, typeParameterListOwner)
         }
+
+        for (constraint in constraints) {
+            constraint.annotationEntries.forEach {
+                trace.report(ANNOTATION_IN_WHERE_CLAUSE.on(it))
+            }
+        }
     }
 
     private fun checkConstructorInInterface(klass: KtClass) {
