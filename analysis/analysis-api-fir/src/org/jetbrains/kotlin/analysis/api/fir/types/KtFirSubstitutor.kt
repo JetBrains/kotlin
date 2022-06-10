26dc362555b78ad4175c34f19d5d322a8852790e
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-06-10 12:13:39 +0200

    [Analysis API fir] remove now unneeded ReadOnlyWeakRef

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/types/KtFirSubstitutor.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/types/KtFirSubstitutor.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.analysis.api.fir.types
 
 import org.jetbrains.kotlin.analysis.api.KtAnalysisApiInternals
 import org.jetbrains.kotlin.analysis.api.fir.KtSymbolByFirBuilder
-import org.jetbrains.kotlin.analysis.api.fir.utils.weakRef
 import org.jetbrains.kotlin.analysis.api.impl.base.KtMapBackedSubstitutor
 import org.jetbrains.kotlin.analysis.api.symbols.KtTypeParameterSymbol
 import org.jetbrains.kotlin.analysis.api.lifetime.KtLifetimeToken
@@ -18,37 +17,35 @@ import org.jetbrains.kotlin.fir.resolve.substitution.ConeSubstitutor
 import org.jetbrains.kotlin.fir.resolve.substitution.ConeSubstitutorByMap
 
 internal abstract class AbstractKtFirSubstitutor<T : ConeSubstitutor>(
-    private val _substitutor: T,
-    builder: KtSymbolByFirBuilder,
+    val substitutor: T,
+    protected val builder: KtSymbolByFirBuilder,
     override val token: KtLifetimeToken,
 ) : KtSubstitutor {
-    protected val builderRef by weakRef(builder)
-    val substitutor: T get() = withValidityAssertion { _substitutor }
 
     override fun substituteOrNull(type: KtType): KtType? = withValidityAssertion {
         require(type is KtFirType)
-        substitutor.substituteOrNull(type.coneType)?.type?.let { builderRef.typeBuilder.buildKtType(it) }
+        substitutor.substituteOrNull(type.coneType)?.type?.let { builder.typeBuilder.buildKtType(it) }
     }
 }
 
 internal class KtFirGenericSubstitutor(
-    _substitutor: ConeSubstitutor,
+    substitutor: ConeSubstitutor,
     builder: KtSymbolByFirBuilder,
     token: KtLifetimeToken
-) : AbstractKtFirSubstitutor<ConeSubstitutor>(_substitutor, builder, token)
+) : AbstractKtFirSubstitutor<ConeSubstitutor>(substitutor, builder, token)
 
 @OptIn(KtAnalysisApiInternals::class)
 internal class KtFirMapBackedSubstitutor(
-    _substitutor: ConeSubstitutorByMap,
+    substitutor: ConeSubstitutorByMap,
     builder: KtSymbolByFirBuilder,
     token: KtLifetimeToken
-) : AbstractKtFirSubstitutor<ConeSubstitutorByMap>(_substitutor, builder, token), KtMapBackedSubstitutor {
+) : AbstractKtFirSubstitutor<ConeSubstitutorByMap>(substitutor, builder, token), KtMapBackedSubstitutor {
     override fun getAsMap(): Map<KtTypeParameterSymbol, KtType> = withValidityAssertion {
         val result = mutableMapOf<KtTypeParameterSymbol, KtType>()
         for ((typeParameter, type) in substitutor.substitution) {
-            val typeParameterSymbol = builderRef.classifierBuilder.buildTypeParameterSymbolByLookupTag(typeParameter.toLookupTag())
+            val typeParameterSymbol = builder.classifierBuilder.buildTypeParameterSymbolByLookupTag(typeParameter.toLookupTag())
             if (typeParameterSymbol != null) {
-                result[typeParameterSymbol] = builderRef.typeBuilder.buildKtType(type)
+                result[typeParameterSymbol] = builder.typeBuilder.buildKtType(type)
             }
         }
 
