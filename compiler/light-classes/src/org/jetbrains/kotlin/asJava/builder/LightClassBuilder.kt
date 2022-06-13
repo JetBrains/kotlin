8a75aef8f52cbaa97989131bd06bb790399636c5
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-06-13 21:36:52 +0200

    [light classes] add registry key to forbid access to backend
    ^KT-48773

--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/builder/LightClassBuilder.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/builder/LightClassBuilder.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.asJava.builder
@@ -19,6 +8,7 @@ package org.jetbrains.kotlin.asJava.builder
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.impl.compiled.ClsFileImpl
 import com.intellij.psi.impl.java.stubs.PsiJavaFileStub
@@ -44,6 +34,10 @@ fun buildLightClass(
     val project = files.first().project
 
     try {
+        if (Registry.`is`("kotlin.ultra.light.classes.error.on.old.backend", false)) {
+            error("Access to backend detected")
+        }
+
         val classBuilderFactory = KotlinLightClassBuilderFactory(createJavaFileStub(packageFqName, files))
         val state = GenerationState.Builder(
             project,
