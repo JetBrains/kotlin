4511c7faa9a98a6831da2fe0388350d9f42cb93c
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-06-14 14:32:55 +0200

    [Gradle] Update testHmppTasksAreNotIncludedInGradleConfigurationCache
    Generate PSM and K/Native tasks support Configuration Cache now

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/HierarchicalMppIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/HierarchicalMppIT.kt
@@ -731,16 +731,13 @@ class HierarchicalMppIT : KGPBaseTest() {
             val configCacheIncompatibleTasks = listOf(
                 ":generateProjectStructureMetadata",
                 ":transformCommonMainDependenciesMetadata",
-                ":cinteropFooLinuxX64",
-                ":compileKotlinLinuxX64",
-                ":linkDebugSharedLinuxX64",
             )
 
             build("clean", "assemble", buildOptions = options) {
                 assertTasksExecuted(configCacheIncompatibleTasks)
                 configCacheIncompatibleTasks.forEach { task ->
                     assertOutputContains(
-                        """Task `:$task` of type `.+`: .+(at execution time is unsupported)|(not supported with the configuration cache)"""
+                        """Task `$task` of type `.+`: .+(at execution time is unsupported)|(not supported with the configuration cache)"""
                             .toRegex()
                     )
                 }
