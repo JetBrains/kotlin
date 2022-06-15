6bfd5aea468be44c6b31dfa17805249e2a5e080a
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-06-15 17:27:10 +0200

    [Analysis API] add testcases, add comments to tests

--- analysis/analysis-api/testData/components/scopeProvider/typeScope/innerClassTypeParamsSubstitution.kt
+++ analysis/analysis-api/testData/components/scopeProvider/typeScope/innerClassTypeParamsSubstitution.kt
@@ -1,9 +1,13 @@
 class Outer<O> {
-  inner class A<X> {
+  inner class A<X>: Super<O> {
       fun <Y> foo(x: X, y: Y): Map<X, Map<Y, O>>
 
       val map: Map<X, O>
    }
+}
+
+abstract class Super<S> {
+    abstract fun fromSuper(): S
 }
 
 fun foo(o: Outer<String>) {
