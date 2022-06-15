0eb4551e94f1290264c8c966f58cbb7fbb43392e
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-06-15 10:56:44 +0200

    [kpm] kgp-idea: Update testedVersion to "1.7.20-dev-2127"
    ^KT-52568 Verification Pending

--- libraries/tools/kotlin-gradle-plugin-idea-for-compatibility-tests/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-idea-for-compatibility-tests/build.gradle.kts
@@ -4,7 +4,7 @@
  * Version of kotlin-gradle-plugin-idea module that should be resolved for compatibility tests
  * This version can be treated as 'minimal guaranteed backwards compatible version' of the module.
  */
-val testedVersion = "1.7.20-dev-1435"
+val testedVersion = "1.7.20-dev-2127"
 
 val isSnapshotTest = properties.contains("kgp-idea.snapshot_test")
 val resolvedTestedVersion = if (isSnapshotTest) properties["defaultSnapshotVersion"].toString() else testedVersion
