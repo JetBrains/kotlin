ba797fa1b4a325e7babe0e5d7c7323d849c65329
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-06-16 17:13:03 +0200

    [stubs] fix cls stubs for declarations with context receivers
    ^KTIJ-21243 fixed

---
+++ analysis/decompiled/decompiler-to-file-stubs/testData/clsFileStubBuilder/ContextReceiversCallableMembers/ContextReceiversCallableMembers.kt
@@ -0,0 +1,24 @@
+// !LANGUAGE: +ContextReceivers
+
+private open class ContextReceiversCallableMembers {
+    context(A, B)
+    private fun Int.function(): Int = valueA + valueB
+
+    context(A, B)
+    private val Int.property: Int get() = valueA + valueB
+
+    context(A, B)
+    private var Int.propertyWithSetter: Int
+        get() = valueA + valueB
+        set(v) { println(valueA + valueB) }
+}
+
+class A {
+    val valueA: Int = 10
+}
+
+class B {
+    val valueB: Int = 11
+}
+
+
