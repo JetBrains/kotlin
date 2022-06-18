e0bea47c5446a2eb60f45787d5ae51d2d130d9e1
Marat Dinmukhametov <maratdin7@gmail.com>
2022-06-19 00:09:05 +0300

    Correction of errors in tests

--- compiler/fir/analysis-tests/testData/resolve/extendedCheckers/SafeInitializationChecker/defaultArgument.kt
+++ compiler/fir/analysis-tests/testData/resolve/extendedCheckers/SafeInitializationChecker/defaultArgument.kt
@@ -2,4 +2,4 @@ class A {
     val def = Def("Hello")
 }
 
-data class Def(<!ACCESS_TO_UNINITIALIZED_VALUE!>val a: String = run { y }<!>,  <!ACCESS_TO_UNINITIALIZED_VALUE!>val y: String = "Tmp"<!>)
+data class Def(val a: String = run { y },  <!ACCESS_TO_UNINITIALIZED_VALUE!>val y: String = "Tmp"<!>)
--- compiler/fir/analysis-tests/testData/resolve/extendedCheckers/SafeInitializationChecker/delegation.kt
+++ compiler/fir/analysis-tests/testData/resolve/extendedCheckers/SafeInitializationChecker/delegation.kt
@@ -1,5 +1,7 @@
+// WITH_STDLIB
+
 class Hello {
     val message: String by lazy { name }
-    <!UNREACHABLE_CODE!>val len = message.length<!>
+    val len = message.length
     <!ACCESS_TO_UNINITIALIZED_VALUE!>val name = "Alice"<!>
 }
