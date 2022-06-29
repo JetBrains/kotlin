6eee30a709735a9c515e115d444f2d9d56498a88
Nikita Nazarov <nikita.nazarov@jetbrains.com>
2022-06-29 18:45:34 +0300

    Add tests on variable spilling with debug mode enabled
    #KT-48678 Fixed

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
@@ -13101,6 +13101,16 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
                     runTest("compiler/testData/codegen/box/coroutines/varSpilling/cleanup/when.kt");
                 }
             }
+
+            @Nested
+            @TestMetadata("compiler/testData/codegen/box/coroutines/varSpilling/debugMode")
+            @TestDataPath("$PROJECT_ROOT")
+            public class DebugMode {
+                @Test
+                public void testAllFilesPresentInDebugMode() throws Exception {
+                    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/coroutines/varSpilling/debugMode"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM, true);
+                }
+            }
         }
     }
 
