298c99e975b8aa2b2d1c294cf067a0b39b87c56e
mvicsokolova <maria.sokolova@jetbrains.com>
2022-06-30 13:56:22 +0200

    Revert renaming the kotlinx-atomicfu-runtime module

--- plugins/atomicfu/atomicfu-compiler/build.gradle.kts
+++ plugins/atomicfu/atomicfu-compiler/build.gradle.kts
@@ -76,9 +76,9 @@ dependencies {
 
     atomicfuClasspath("org.jetbrains.kotlinx:atomicfu-js:0.16.3") { isTransitive = false }
 
-    atomicfuRuntimeForTests(project(":atomicfu-runtime"))  { isTransitive = false }
+    atomicfuRuntimeForTests(project(":kotlinx-atomicfu-runtime"))  { isTransitive = false }
 
-    embedded(project(":atomicfu-runtime")) {
+    embedded(project(":kotlinx-atomicfu-runtime")) {
         attributes {
             attribute(KotlinPlatformType.attribute, KotlinPlatformType.js)
             attribute(KotlinJsCompilerAttribute.jsCompilerAttribute, KotlinJsCompilerAttribute.ir)
