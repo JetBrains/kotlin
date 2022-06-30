5aba84cec6b428d5cc4740f850d306a9cc60e6a3
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-06-30 17:31:19 +0200

    [KDM] Bump essential dependencies
    See https://github.com/Kotlin/dokka/pull/2534
    Original commit: https://github.com/Kotlin/dokka/commit/fd92fc0d261f50139ab80d47251316c4f7882dea

--- libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
     implementation("com.github.jengelman.gradle.plugins:shadow:2.0.4")
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.9.0")
+    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.10.1")
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
 }
