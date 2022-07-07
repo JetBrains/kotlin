62eeb294082067f3b8cf54a99722937a26be4b04
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-07-08 01:16:04 +0300

    Add failing SAM tests ^KT-49847 In progress

---
+++ kotlin-native/backend.native/tests/filecheck/kt49847_sam.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+fun interface Foo {
+    fun bar(x: Int): Any
+}
+
+fun baz(x: Any): Int = x.hashCode()
+
+// CHECK: define void @"kfun:#main(){}"()
+// CHECK-NOT: kfun:kotlin#<Int-box>(kotlin.Int){}kotlin.Any
+// CHECK-NOT: kfun:kotlin#<Int-unbox>(kotlin.Any){}kotlin.Int
+// CHECK: ret void
+fun main() {
+    val foo: Foo = Foo(::baz)
+    if( foo.bar(42) == 42 )
+        println("passed")
+}
