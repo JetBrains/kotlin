62f798e273ebe148b477802f0a4143f12c1fee81
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-07-07 19:47:01 +0300

    Add two tests

---
+++ kotlin-native/backend.native/tests/filecheck/kt49847_class.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+class C {
+    fun foo(x: Int) = x
+}
+
+// CHECK: define void @"kfun:#main(){}"()
+// CHECK-NOT: kfun:kotlin#<Int-box>(kotlin.Int){}kotlin.Any
+// CHECK-NOT: kfun:kotlin#<Int-unbox>(kotlin.Any){}kotlin.Int
+// CHECK: ret void
+fun main() {
+    val c = C()
+    val fooref = c::foo
+    if( fooref(42) == 42)
+        println("ok")
+}
---
+++ kotlin-native/backend.native/tests/filecheck/kt49847_generic.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+class C<T> {
+    fun foo(x: T) = x
+}
+
+// CHECK: define void @"kfun:#main(){}"()
+// CHECK-NOT: kfun:kotlin#<Int-box>(kotlin.Int){}kotlin.Any
+// CHECK-NOT: kfun:kotlin#<Int-unbox>(kotlin.Any){}kotlin.Int
+// CHECK: ret void
+fun main() {
+    val c = C<Int>()
+    val fooref = c::foo
+    if( fooref(42) == 42)
+        println("ok")
+}
