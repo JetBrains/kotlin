d1eec4cc373d01d4aa630f15815ab4ac21ebfb5f
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-07-07 19:12:37 +0200

    [light classes] move PsiElementWithOrigin to light-classes-base module
    ^KT-53097

---
+++ compiler/light-classes-base/src/org/jetbrains/kotlin/asJava/elements/PsiElementWithOrigin.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.asJava.elements
+
+import com.intellij.psi.PsiElement
+
+interface PsiElementWithOrigin<out T> : PsiElement where T : PsiElement {
+    val origin: T?
+}
