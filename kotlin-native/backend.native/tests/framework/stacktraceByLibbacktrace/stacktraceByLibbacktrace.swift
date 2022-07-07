8f5907c62f30ad189d39bfd3deb4e8dd18aa84e0
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-07-07 17:16:54 +0300

    [K/N] Add bridge check to the framework stacktrace tests

--- kotlin-native/backend.native/tests/framework/stacktraceByLibbacktrace/stacktraceByLibbacktrace.swift
+++ kotlin-native/backend.native/tests/framework/stacktraceByLibbacktrace/stacktraceByLibbacktrace.swift
@@ -9,6 +9,7 @@ func testStackTrace() throws {
     try assertTrue(trace[2].contains("[inlined]"))
     try assertTrue(trace[3].contains("stacktraceByLibbacktrace.kt:11"))
     try assertTrue(trace[4].contains("stacktraceByLibbacktrace.kt:16"))
+    try assertTrue(trace[5].contains("<compiler-generated>"))
     try assertTrue(trace[6].contains("stacktraceByLibbacktrace.swift:5"))
     try assertTrue(trace[7].contains("main.swift:126"))
 }
