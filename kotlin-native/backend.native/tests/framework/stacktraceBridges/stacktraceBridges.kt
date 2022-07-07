2bb69994ae3a06799d6c022bcda45f202cdbc6be
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-07-07 17:24:59 +0300

    [K/N] Add regression test for objc2kotlin and kotlin2objc names

---
+++ kotlin-native/backend.native/tests/framework/stacktraceBridges/stacktraceBridges.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+fun thrower() : Nothing {
+    throw Exception()
+}
+
+fun getStackTrace() : List<String> {
+    try {
+       thrower()
+    }  catch (e: Exception) {
+        return e.getStackTrace().toList()
+    }
+    return emptyList()
+}
+
+open class Foo {
+    open fun foo(): List<String> { return emptyList() }
+
+    companion object {
+        var trace: List<String> = emptyList()
+
+        init {
+            trace = getStackTrace()
+        }
+    }
+}
+
+private class Bar : Foo() {
+    override fun foo(): List<String> = getStackTrace()
+}
+
+object Object {
+    var trace: List<String> = emptyList()
+
+    init {
+        trace = getStackTrace()
+    }
+}
+
+enum class E {
+    A {
+        init {
+            trace = getStackTrace()
+        }
+    };
+
+    var trace: List<String> = emptyList()
+}
+
+fun createBar(): Foo = Bar()
+
+fun use(foo: Foo): List<String> {
+    return foo.foo()
+}
