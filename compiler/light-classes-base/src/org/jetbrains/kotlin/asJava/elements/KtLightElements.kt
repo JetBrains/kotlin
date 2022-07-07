409389471b41c74064eca4f26031809f4a4651af
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-07-07 19:39:08 +0200

    [light classes] move KtLightElements to light-classes-base module
    ^KT-53097

--- compiler/light-classes-base/src/org/jetbrains/kotlin/asJava/elements/KtLightElements.kt
+++ compiler/light-classes-base/src/org/jetbrains/kotlin/asJava/elements/KtLightElements.kt
@@ -5,11 +5,14 @@
 
 package org.jetbrains.kotlin.asJava.elements
 
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiField
-import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.*
+import com.intellij.psi.impl.PsiVariableEx
+import org.jetbrains.kotlin.asJava.builder.LightMemberOrigin
+import org.jetbrains.kotlin.asJava.classes.KtLightClass
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtParameter
+import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOriginKind
 
 interface KtLightElement<out T : KtElement, out D : PsiElement> : PsiElement {
     val kotlinOrigin: T?
@@ -25,6 +28,26 @@ interface KtLightElement<out T : KtElement, out D : PsiElement> : PsiElement {
 
 interface KtLightDeclaration<out T : KtDeclaration, out D : PsiElement> : KtLightElement<T, D>, PsiNamedElement
 
+interface KtLightMember<out D : PsiMember> : PsiMember, KtLightDeclaration<KtDeclaration, D>, PsiNameIdentifierOwner, PsiDocCommentOwner {
+    val lightMemberOrigin: LightMemberOrigin?
+
+    override fun getContainingClass(): KtLightClass
+}
+
+interface KtLightField : PsiField, KtLightMember<PsiField>, PsiVariableEx
+
+interface KtLightParameter : PsiParameter, KtLightDeclaration<KtParameter, PsiParameter> {
+    val method: KtLightMethod
+}
+
 interface KtLightFieldForSourceDeclarationSupport : PsiField {
     val kotlinOrigin: KtDeclaration?
+}
+
+interface KtLightMethod : PsiAnnotationMethod, KtLightMember<PsiMethod> {
+    val isDelegated: Boolean
+        get() = lightMemberOrigin?.originKind == JvmDeclarationOriginKind.DELEGATION
+                || lightMemberOrigin?.originKind == JvmDeclarationOriginKind.CLASS_MEMBER_DELEGATION_TO_DEFAULT_IMPL
+
+    val isMangled: Boolean
 }
