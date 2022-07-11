ffcc86c1c3af6934b5eb22c7517bd19b3eb1d894
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-07-11 17:51:08 +0300

    Polish genericFunctionReferenceSignature.kt

--- compiler/testData/codegen/box/reflection/genericSignature/genericFunctionReferenceSignature.kt
+++ compiler/testData/codegen/box/reflection/genericSignature/genericFunctionReferenceSignature.kt
@@ -5,8 +5,8 @@ package test
 fun <T> foo(x: T) = x
 
 fun box(): String {
-    val bar: Function1<Int, Int> = ::foo
-    val returnType = (bar as kotlin.reflect.KFunction<*>).returnType
+    val bar: kotlin.reflect.KFunction1<Int, Int> = ::foo
+    val returnType = bar.returnType
     if (returnType.toString() != "T") return returnType.toString()
     return "OK"
 }
