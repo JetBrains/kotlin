a0c55a9ab854db55ff4d7f812779d3117cf7f6cb
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-07-11 22:07:47 +0200

    [light classes] move method indices to light-classes-base module
    ^KT-53097

---
+++ compiler/light-classes-base/src/org/jetbrains/kotlin/asJava/classes/methodIndices.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.asJava.classes
+
+const val METHOD_INDEX_FOR_GETTER = 1
+const val METHOD_INDEX_FOR_SETTER = 2
+const val METHOD_INDEX_FOR_DEFAULT_CTOR = 3
+const val METHOD_INDEX_FOR_NO_ARG_OVERLOAD_CTOR = 4
+const val METHOD_INDEX_FOR_NON_ORIGIN_METHOD = 5
+const val METHOD_INDEX_FOR_SCRIPT_MAIN = 6
+const val METHOD_INDEX_BASE = 7
--- compiler/light-classes/src/org/jetbrains/kotlin/asJava/classes/ultraLightMethods.kt
+++ compiler/light-classes/src/org/jetbrains/kotlin/asJava/classes/ultraLightMethods.kt
@@ -34,14 +34,6 @@ import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOriginKind
 import org.jetbrains.kotlin.types.RawType
 import org.jetbrains.kotlin.utils.addToStdlib.safeAs
 
-const val METHOD_INDEX_FOR_GETTER = 1
-const val METHOD_INDEX_FOR_SETTER = 2
-const val METHOD_INDEX_FOR_DEFAULT_CTOR = 3
-const val METHOD_INDEX_FOR_NO_ARG_OVERLOAD_CTOR = 4
-const val METHOD_INDEX_FOR_NON_ORIGIN_METHOD = 5
-const val METHOD_INDEX_FOR_SCRIPT_MAIN = 6
-const val METHOD_INDEX_BASE = 7
-
 private class KtUltraLightMethodModifierList(
     support: KtUltraLightSupport,
     owner: KtUltraLightMethod,
