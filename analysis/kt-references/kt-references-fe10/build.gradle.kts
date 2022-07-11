248811e729b6ad7c7341c5c5e4fdbafdeb5c8cfd
Dmitry Gridin <dmitry.gridin@jetbrains.com>
2022-07-11 18:10:28 +0200

    [light classes] replace light class dependency from analysis.kt-references.kt-references-fe10 to light-classes-base
    ^KT-53097

--- analysis/kt-references/kt-references-fe10/build.gradle.kts
+++ analysis/kt-references/kt-references-fe10/build.gradle.kts
@@ -7,7 +7,8 @@ dependencies {
     implementation(project(":analysis:kt-references"))
 
     implementation(project(":compiler:psi"))
-    implementation(project(":compiler:light-classes"))
+    implementation(project(":compiler:light-classes-base"))
+    implementation(project(":compiler:frontend.java"))
     implementation(intellijCore())
 
     compileOnly(commonDependency("com.google.guava:guava"))
