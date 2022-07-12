746e73e66994b60e377a486b2eba9d43a438b0a2
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-07-12 12:10:00 +0200

    Update Duckduckgo build regression benchmark
    Compare current vs 1.7.10 release

--- libraries/tools/gradle/regression-benchmarks/benchmarkScripts/duckduckgo.benchmark.kts
+++ libraries/tools/gradle/regression-benchmarks/benchmarkScripts/duckduckgo.benchmark.kts
@@ -4,10 +4,16 @@
 @file:BenchmarkProject(
     name = "duckduckgo",
     gitUrl = "https://github.com/duckduckgo/Android.git",
-    gitCommitSha = "9c808cfda9877fbee5bbc8a542f6c80ede48e95f"
+    gitCommitSha = "648b0aae2dd54c4da4176eb91b3a05ea44118fa5"
 )
 
 import java.io.File
+
+val stableReleasePatch = {
+    "duckduckgo-kotlin-1.7.10.patch" to File("benchmarkScripts/files/duckduckgo-kotlin-1.7.10.patch")
+        .readText()
+        .byteInputStream()
+}
 
 val currentReleasePatch = {
     "duckduckgo-kotlin-current.patch" to File("benchmarkScripts/files/duckduckgo-kotlin-current.patch")
@@ -22,37 +28,40 @@ runAllBenchmarks(
             title = "Clean build"
             useGradleArgs("--no-build-cache")
 
+            warmups = 1
+            iterations = 1
+
             runTasks(":app:assemblePlayDebug")
             runCleanupTasks("clean")
         }
 
-        scenario {
-            title = "Incremental build with ABI change in common ViewModelFactory"
-            useGradleArgs("--no-build-cache")
-
-            runTasks(":app:assemblePlayDebug")
-            applyAbiChangeTo("common/src/main/java/com/duckduckgo/app/global/VpnViewModelFactory.kt")
-        }
-
-        scenario {
-            title = "Incremetal build with ABI change in Kapt component"
-            useGradleArgs("--no-build-cache")
-
-            runTasks(":app:assemblePlayDebug")
-            applyAbiChangeTo("vpn/src/main/java/com/duckduckgo/mobile/android/vpn/di/VpnModule.kt")
-        }
-
-        scenario {
-            title = "Incremental build with change in Android common string resource"
-            useGradleArgs("--no-build-cache")
-
-            runTasks(":app:assemblePlayDebug")
-
-            applyAndroidResourceValueChange("common-ui/src/main/res/values/strings.xml")
-        }
+//        scenario {
+//            title = "Incremental build with ABI change in common ViewModelFactory"
+//            useGradleArgs("--no-build-cache")
+//
+//            runTasks(":app:assemblePlayDebug")
+//            applyAbiChangeTo("common/src/main/java/com/duckduckgo/app/global/VpnViewModelFactory.kt")
+//        }
+//
+//        scenario {
+//            title = "Incremetal build with ABI change in Kapt component"
+//            useGradleArgs("--no-build-cache")
+//
+//            runTasks(":app:assemblePlayDebug")
+//            applyAbiChangeTo("vpn/src/main/java/com/duckduckgo/mobile/android/vpn/di/VpnModule.kt")
+//        }
+//
+//        scenario {
+//            title = "Incremental build with change in Android common string resource"
+//            useGradleArgs("--no-build-cache")
+//
+//            runTasks(":app:assemblePlayDebug")
+//
+//            applyAndroidResourceValueChange("common-ui/src/main/res/values/strings.xml")
+//        }
     },
     mapOf(
-        "1.6.20" to null,
-        "1.7.0" to currentReleasePatch
+        "1.7.10" to stableReleasePatch,
+        "1.7.20" to currentReleasePatch
     )
 )
