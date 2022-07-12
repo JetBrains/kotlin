74faeb74a784ddb5a1d3d10f4e5ff36a4e33cade
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-07-12 16:08:38 +0200

    ~ tmp15 - module descriptors

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirSessionFactory.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirSessionFactory.kt
@@ -329,4 +329,16 @@ object FirSessionFactory : FirAbstractSessionFactory() {
             moduleData.bindSession(this)
         }
     }
+
+    fun createModuleDataForBuiltins(
+        parentModuleName: Name,
+        platform: TargetPlatform,
+        analyzerServices: PlatformDependentAnalyzerServices
+    ): FirModuleData {
+        return DependencyListForCliModule.createDependencyModuleData(
+            Name.special("<builtins of ${parentModuleName.asString()}"),
+            platform,
+            analyzerServices,
+        )
+    }
 }
