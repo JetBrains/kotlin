1b495e5bca504a9d052fe05720f1edf8f4b4642f
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-07-13 12:13:41 +0200

    [KDM] Update references and documentation to Dokka 1.7.10
    See https://github.com/Kotlin/dokka/pull/2566
    Original commit: https://github.com/Kotlin/dokka/commit/7cbdcc9ae62d03af5efbab8f5ce2d815f456513a

--- libraries/tools/kotlin-documentation-model/analyzer/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") apply false
     id("java")
-    id("org.jetbrains.dokka") version "1.6.20"
+    id("org.jetbrains.dokka") version "1.7.10"
     id("io.github.gradle-nexus.publish-plugin")
 }
 
