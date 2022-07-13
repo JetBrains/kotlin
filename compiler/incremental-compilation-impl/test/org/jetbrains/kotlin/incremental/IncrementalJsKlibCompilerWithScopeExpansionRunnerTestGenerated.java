ce911d9ef771754a9fa22302e3957bdfc9f583ed
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2022-07-13 18:28:17 +0200

    [IC] Add test for lookups flushing
    #KT-53168

--- compiler/incremental-compilation-impl/test/org/jetbrains/kotlin/incremental/IncrementalJsKlibCompilerWithScopeExpansionRunnerTestGenerated.java
+++ compiler/incremental-compilation-impl/test/org/jetbrains/kotlin/incremental/IncrementalJsKlibCompilerWithScopeExpansionRunnerTestGenerated.java
@@ -596,6 +596,11 @@ public class IncrementalJsKlibCompilerWithScopeExpansionRunnerTestGenerated exte
             runTest("jps/jps-plugin/testData/incremental/pureKotlin/secondaryConstructorInlined/");
         }
 
+        @TestMetadata("sequentualAddingAndDeletingOfPropertyAndUsage")
+        public void testSequentualAddingAndDeletingOfPropertyAndUsage() throws Exception {
+            runTest("jps/jps-plugin/testData/incremental/pureKotlin/sequentualAddingAndDeletingOfPropertyAndUsage/");
+        }
+
         @TestMetadata("serializedSubClassAndChangedInterfaces")
         public void testSerializedSubClassAndChangedInterfaces() throws Exception {
             runTest("jps/jps-plugin/testData/incremental/pureKotlin/serializedSubClassAndChangedInterfaces/");
