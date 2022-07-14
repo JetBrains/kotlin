8cc9406b1fa7d4cd2c9ead6330920ec4755ebe11
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-07-14 13:59:56 +0200

    [Gradle] Use `objectFactory.fileCollection` instead of lazy
    ^KT-43293

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/KotlinNativeTasks.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/tasks/KotlinNativeTasks.kt
@@ -166,14 +166,14 @@ abstract class AbstractKotlinNativeCompile<
     }
 
     @get:Classpath
-    override val libraries: ConfigurableFileCollection by lazy {
+    override val libraries: ConfigurableFileCollection = objectFactory.fileCollection().from({
         // Avoid resolving these dependencies during task graph construction when we can't build the target:
         if (konanTarget.enabledOnCurrentHost)
             objectFactory.fileCollection().from(
                 compilation.compileDependencyFiles.filterOutPublishableInteropLibs(project)
             )
         else objectFactory.fileCollection()
-    }
+    })
 
     @get:Classpath
     protected val friendModule: FileCollection = project.files({ compilation.friendPaths })
