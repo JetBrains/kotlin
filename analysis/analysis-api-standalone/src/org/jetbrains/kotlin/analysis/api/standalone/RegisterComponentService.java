accc93ad45caf0ee2ff76b748d84d6a7680b3d21
Jinseong Jeon <jsjeon@google.com>
2022-07-27 21:29:06 -0700

    AA: register LLFir(Library|Builtin)SessionFactory

--- analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/api/standalone/RegisterComponentService.java
+++ analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/api/standalone/RegisterComponentService.java
@@ -7,8 +7,16 @@ package org.jetbrains.kotlin.analysis.api.standalone;
 
 import com.intellij.mock.MockProject;
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirResolveSessionService;
+import org.jetbrains.kotlin.analysis.low.level.api.fir.project.structure.LLFirLibrarySessionFactory;
 
 class RegisterComponentService {
+    static void registerLLFirLibrarySessionFactory(MockProject project) {
+        project.registerService(
+                LLFirLibrarySessionFactory.class,
+                new LLFirLibrarySessionFactory(project)
+        );
+    }
+
     static void registerLLFirResolveSessionService(MockProject project) {
         project.registerService(
                 LLFirResolveSessionService.class,
