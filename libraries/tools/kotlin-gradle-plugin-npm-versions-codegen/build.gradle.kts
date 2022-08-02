922ffa80b0bcefa0a2bde4be8b5822021bfedf9e
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2022-08-02 23:22:54 +0200

    Remove kotlinx-serialization-core:1.0.0-RC usage

--- libraries/tools/kotlin-gradle-plugin-npm-versions-codegen/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-npm-versions-codegen/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:1.4.0")
     implementation(commonDependency("com.google.code.gson:gson"))
     implementation("org.apache.velocity:velocity:1.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.0")
 }
 
 val generateNpmVersions by generator(
