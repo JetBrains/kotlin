54c0ec031d6ba7ffe9b07ff5f6612026444941a3
mvicsokolova <maria.sokolova@jetbrains.com>
2022-08-02 17:51:42 +0200

    Enable JS/JVM IR transformation by atomicfu-compiler-plugin according to the flag set in the project.

--- libraries/tools/atomicfu/src/common/kotlin/org/jetbrains/kotlinx/atomicfu/gradle/AtomicfuKotlinGradleSubplugin.kt
+++ libraries/tools/atomicfu/src/common/kotlin/org/jetbrains/kotlinx/atomicfu/gradle/AtomicfuKotlinGradleSubplugin.kt
@@ -16,8 +16,12 @@
 
 package org.jetbrains.kotlinx.atomicfu.gradle
 
+import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.plugin.*
+
+private const val ENABLE_JS_IR_TRANSFORMATION = "kotlinx.atomicfu.enableJsIrTransformation"
+private const val ENABLE_JVM_IR_TRANSFORMATION = "kotlinx.atomicfu.enableJvmIrTransformation"
 
 class AtomicfuKotlinGradleSubplugin :
     KotlinCompilerPluginSupportPlugin {
@@ -25,7 +29,11 @@ class AtomicfuKotlinGradleSubplugin :
         const val ATOMICFU_ARTIFACT_NAME = "atomicfu"
     }
 
-    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean = true
+    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean {
+        val project = kotlinCompilation.target.project
+        return (project.getBooleanProperty(ENABLE_JS_IR_TRANSFORMATION) && kotlinCompilation.target.isJs()) ||
+                (project.getBooleanProperty(ENABLE_JVM_IR_TRANSFORMATION) && (kotlinCompilation.target.isJvm()))
+    }
 
     override fun applyToCompilation(
         kotlinCompilation: KotlinCompilation<*>
@@ -36,4 +44,17 @@ class AtomicfuKotlinGradleSubplugin :
         JetBrainsSubpluginArtifact(ATOMICFU_ARTIFACT_NAME)
 
     override fun getCompilerPluginId() = "org.jetbrains.kotlinx.atomicfu"
+
+    private fun KotlinTarget.isJs() = platformType == KotlinPlatformType.js
+
+    private fun KotlinTarget.isJvm() = platformType == KotlinPlatformType.jvm || platformType == KotlinPlatformType.androidJvm
+
+    private fun Project.getBooleanProperty(name: String) =
+        rootProject.findProperty(name)?.toString()?.toBooleanStrict() ?: false
+
+    private fun String.toBooleanStrict(): Boolean = when (this) {
+        "true" -> true
+        "false" -> false
+        else -> throw IllegalArgumentException("The string doesn't represent a boolean value: $this")
+    }
 }
