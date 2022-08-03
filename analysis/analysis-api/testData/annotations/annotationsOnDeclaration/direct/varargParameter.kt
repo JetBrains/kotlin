1e9fb0549649fa3eaa238b756b46b28497e8a15f
Jinseong Jeon <jsjeon@google.com>
2022-08-03 15:01:55 -0700

    AA FIR: handle (aliased)? annotation w/ vararg parameter

---
+++ analysis/analysis-api/testData/annotations/annotationsOnDeclaration/direct/varargParameter.kt
@@ -0,0 +1,4 @@
+annotation class A(val a: Int, vararg val cs: KClass<*>)
+
+@A(a = 1, Int::class, String::class)
+fun fo<caret>o(): Int = 42
--- compiler/testData/asJava/lightClasses/SpecialAnnotationsOnAnnotationClass.fir.java
+++ compiler/testData/asJava/lightClasses/SpecialAnnotationsOnAnnotationClass.fir.java
@@ -2,7 +2,7 @@
 @kotlin.annotation.MustBeDocumented()
 @kotlin.annotation.Repeatable()
 @kotlin.annotation.Retention(value = kotlin.annotation.AnnotationRetention.SOURCE)
-@kotlin.annotation.Target(allowedTargets = kotlin.annotation.AnnotationTarget.TYPE_PARAMETER)
+@kotlin.annotation.Target(allowedTargets = {kotlin.annotation.AnnotationTarget.TYPE_PARAMETER, kotlin.annotation.AnnotationTarget.TYPE})
 public abstract @interface Anno /* Anno*/ {
   public abstract int i();//  i()
 
