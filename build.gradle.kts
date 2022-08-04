3dce733cadc647dc0ca918dcfdc87518e494d0e5
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-08-04 17:03:32 +0900

    [Pill] Prevent 'intellij' directory indexing in cooperative dev mode

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,8 @@ pill {
     excludedDirs(
         "out",
         "buildSrc/build",
-        "buildSrc/prepare-deps/intellij-sdk/build"
+        "buildSrc/prepare-deps/intellij-sdk/build",
+        "intellij"
     )
 }
 
