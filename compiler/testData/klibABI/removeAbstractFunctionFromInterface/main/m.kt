c79a2d979dbf34aceb468f8154bb06a882704b15
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2022-08-05 17:02:15 +0200

    [IR][tests] Update tests to reflect changes in partial linkage error messages

--- compiler/testData/klibABI/removeAbstractFunctionFromInterface/main/m.kt
+++ compiler/testData/klibABI/removeAbstractFunctionFromInterface/main/m.kt
@@ -4,16 +4,23 @@ import lib2.B
 fun box(): String {
     val a: A = B()
 
-    try {
+    return try {
         val answer: Int = a.foo() // <-- should throw linkage error here
         println(answer)
+        "FAIL"
     } catch (e: Throwable) {
-        if (e.isLinkageError("lib2.B.foo")) return "OK"
+        e.checkLinkageError("foo", "B")
     }
-
-    return "FAIL"
 }
 
-private fun Throwable.isLinkageError(symbolName: String): Boolean =
-    this::class.simpleName == "IrLinkageError"
-            && message == "Abstract function $symbolName is not implemented in non-abstract class ${symbolName.substringBeforeLast(".")}"
+private fun Throwable.checkLinkageError(symbolName: String, className: String): String {
+    if (this::class.simpleName != "IrLinkageError") return "Unexpected throwable: ${this::class}"
+
+    val expectedMessage = "Abstract function $symbolName is not implemented in non-abstract class $className"
+    val actualMessage = message.orEmpty()
+
+    return if (expectedMessage == actualMessage)
+        "OK"
+    else
+        "EXPECTED: $expectedMessage, ACTUAL: $actualMessage"
+}
--- compiler/testData/klibABI/removeInlinedClass/main/m.kt
+++ compiler/testData/klibABI/removeInlinedClass/main/m.kt
@@ -1,61 +1,82 @@
-fun box(): String {
-    try {
+fun test1(): String {
+    return try {
         fooVariableType()
-        return "FAIL1"
+        "FAIL1"
     } catch (e: Throwable) {
-        if (!e.isUnlinkedTypeOfExpression()) return "FAIL2"
+        e.checkLinkageError("val foo can not be read")
     }
-
-    try {
-        barVariableType()
-        return "FAIL3"
-    } catch (e: Throwable) {
-        if (!e.isUnlinkedTypeOfExpression()) return "FAIL4"
-    }
-
-    try {
-        fooInstance()
-        return "FAIL5"
-    } catch(e: Throwable) {
-        if (!e.isUnlinkedSymbolLinkageError("/Foo.<init>")) return "FAIL6"
-    }
-
-    try {
-        barInstance()
-        return "FAIL7"
-    } catch(e: Throwable) {
-        if (!e.isUnlinkedTypeInSignature("/Bar.<init>")) return "FAIL8"
-    }
-
-    try {
-        fooInstance2()
-        return "FAIL9"
-    } catch(e: Throwable) {
-        if (!e.isUnlinkedSymbolLinkageError("/Foo.<init>")) return "FAIL10"
-    }
-
-    try {
-        barInstance2()
-        return "FAIL11"
-    } catch(e: Throwable) {
-        if (!e.isUnlinkedTypeInSignature("/Bar.<init>")) return "FAIL12"
-    }
-
-    try {
-        fooAnonymousObject()
-        return "FAIL13"
-    } catch(e: Throwable) {
-        if (!e.isUnlinkedTypeOfExpression()) return "FAIL14"
-    }
-
-    return "OK"
 }
 
-private fun Throwable.isUnlinkedTypeOfExpression(): Boolean =
-    this::class.simpleName == "IrLinkageError" && message == "Unlinked type of IR expression"
+fun test2(): String {
+    return try {
+        barVariableType()
+        "FAIL2"
+    } catch (e: Throwable) {
+        e.checkLinkageError("val bar can not be read")
+    }
+}
 
-private fun Throwable.isUnlinkedSymbolLinkageError(symbolName: String): Boolean =
-    this::class.simpleName == "IrLinkageError" && message?.startsWith("Unlinked IR symbol $symbolName|") == true
+fun test3(): String {
+    return try {
+        fooInstance()
+        "FAIL3"
+    } catch (e: Throwable) {
+        e.checkLinkageError("constructor Foo.<init> can not be called")
+    }
+}
 
-private fun Throwable.isUnlinkedTypeInSignature(symbolName: String): Boolean =
-    this::class.simpleName == "IrLinkageError" && message?.startsWith("Unlinked type in signature of IR symbol $symbolName|") == true
+fun test4(): String {
+    return try {
+        barInstance()
+        "FAIL4"
+    } catch (e: Throwable) {
+        e.checkLinkageError("constructor Bar.<init> can not be called")
+    }
+}
+
+fun test5(): String {
+    return try {
+        fooInstance2()
+        "FAIL5"
+    } catch (e: Throwable) {
+        e.checkLinkageError("reference to constructor Foo.<init> can not be evaluated")
+    }
+}
+
+fun test6(): String {
+    return try {
+        barInstance2()
+        "FAIL6"
+    } catch (e: Throwable) {
+        e.checkLinkageError("reference to constructor Bar.<init> can not be evaluated")
+    }
+}
+
+fun test7(): String {
+    return try {
+        fooAnonymousObject()
+        "FAIL7"
+    } catch (e: Throwable) {
+        e.checkLinkageError("val foo can not be read")
+    }
+}
+
+fun box(): String = checkResults(test1(), test2(), test3(), test4(), test5(), test6(), test7())
+
+private fun Throwable.checkLinkageError(prefix: String): String {
+    if (this::class.simpleName != "IrLinkageError") return "Unexpected throwable: ${this::class}"
+
+    val expectedMessagePrefix = "$prefix because it uses unlinked symbols"
+    val actualMessage = message.orEmpty()
+
+    return if (actualMessage.startsWith(expectedMessagePrefix))
+        "OK"
+    else
+        "EXPECTED: $expectedMessagePrefix, ACTUAL: $actualMessage"
+}
+
+private fun checkResults(vararg results: String): String = when {
+    results.isEmpty() -> "no results to check"
+    results.all { it == "OK" } -> "OK"
+    else -> results.joinToString("\n")
+}
