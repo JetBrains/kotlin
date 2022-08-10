2d1ca37325eb34330d3afff7abfccda054582194
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-08-10 16:58:37 +0200

    [minor] missing option rename fir -> k2

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/BuildOptions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/BuildOptions.kt
@@ -125,7 +125,7 @@ data class BuildOptions(
         }
 
         if (useFir) {
-            arguments.add("-Pkotlin.useFir=true")
+            arguments.add("-Pkotlin.useK2=true")
         }
 
         if (usePreciseJavaTracking != null) {
