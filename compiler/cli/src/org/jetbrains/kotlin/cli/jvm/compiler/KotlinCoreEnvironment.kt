70a2bbab8539f3ad132b9f436b9cded7436b8e1b
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2022-08-12 23:06:54 +0200

    Core. Fix data-race in ApplicationEnvironment initialization
    getOrCreateApplicationEnvironment is main entrypoint to create
    ApplicationEnvironment in production and compiler tests
    It is subject to be called concurrently, that's why
    APPLICATION_LOCK exists
    
    ApplicationEnvironment itself hosts Application from idea-core
    It is actually singleton, that is subject to be disposed, once all
    operations referencing it completes
    
    To properly dispose ApplicationEnvironment when there is no references
    left, we maintain reference counter aka ourProjectCount
    
    Originally, there was data-race caused by the fact, that ourProjectCount
    was updated after publication of application
    
    Linear, data-race occurs in following order
    T1: getOrCreateApplicationEnvironment returns application
    T2: Disposes its reference to application, causing ourProjectCount to
    reach zero, and disposing application that is already available to T1
    T1: Updates counter, but its application already disposed

--- compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jvm/compiler/KotlinCoreEnvironment.kt
@@ -436,9 +436,6 @@ class KotlinCoreEnvironment private constructor(
             val projectEnv = ProjectEnvironment(parentDisposable, appEnv, configuration)
             val environment = KotlinCoreEnvironment(projectEnv, configuration, configFiles)
 
-            synchronized(APPLICATION_LOCK) {
-                ourProjectCount++
-            }
             return environment
         }
 
@@ -446,15 +443,7 @@ class KotlinCoreEnvironment private constructor(
         fun createForProduction(
             projectEnvironment: ProjectEnvironment, configuration: CompilerConfiguration, configFiles: EnvironmentConfigFiles
         ): KotlinCoreEnvironment {
-            val environment = KotlinCoreEnvironment(projectEnvironment, configuration, configFiles)
-
-            if (projectEnvironment.environment == applicationEnvironment) {
-                // accounting for core environment disposing
-                synchronized(APPLICATION_LOCK) {
-                    ourProjectCount++
-                }
-            }
-            return environment
+            return KotlinCoreEnvironment(projectEnvironment, configuration, configFiles)
         }
 
         @TestOnly
@@ -499,14 +488,7 @@ class KotlinCoreEnvironment private constructor(
         ): KotlinCoreApplicationEnvironment {
             synchronized(APPLICATION_LOCK) {
                 if (ourApplicationEnvironment == null) {
-                    val disposable = if (unitTestMode) {
-                        parentDisposable
-                    } else {
-                        // TODO this is a memory leak in the compiler, as this Disposable is not registered and thus is never disposed
-                        // but using parentDisposable as disposable in compiler, causes access to application extension points after
-                        // they was disposed, this needs to be fixed
-                        Disposer.newDisposable("Disposable for the KotlinCoreApplicationEnvironment")
-                    }
+                    val disposable = Disposer.newDisposable("Disposable for the KotlinCoreApplicationEnvironment")
                     ourApplicationEnvironment = createApplicationEnvironment(disposable, configuration, unitTestMode)
                     ourProjectCount = 0
                     Disposer.register(disposable, Disposable {
@@ -515,18 +497,21 @@ class KotlinCoreEnvironment private constructor(
                         }
                     })
                 }
-                // Disposing of the environment is unsafe in production then parallel builds are enabled, but turning it off universally
-                // breaks a lot of tests, therefore it is disabled for production and enabled for tests
-                if (CompilerSystemProperties.KOTLIN_COMPILER_ENVIRONMENT_KEEPALIVE_PROPERTY.value.toBooleanLenient() != true) {
-                    // JPS may run many instances of the compiler in parallel (there's an option for compiling independent modules in parallel in IntelliJ)
-                    // All projects share the same ApplicationEnvironment, and when the last project is disposed, the ApplicationEnvironment is disposed as well
-                    Disposer.register(parentDisposable, Disposable {
-                        synchronized(APPLICATION_LOCK) {
-                            if (--ourProjectCount <= 0) {
-                                disposeApplicationEnvironment()
+                try {
+                    // Do not use this property unless you sure need it, causes Application to MEMORY LEAK
+                    // Only valid use-case is when Application should be cached to avoid
+                    // initialization costs
+                    if (CompilerSystemProperties.KOTLIN_COMPILER_ENVIRONMENT_KEEPALIVE_PROPERTY.value.toBooleanLenient() != true) {
+                        Disposer.register(parentDisposable, Disposable {
+                            synchronized(APPLICATION_LOCK) {
+                                if (--ourProjectCount <= 0) {
+                                    disposeApplicationEnvironment()
+                                }
                             }
-                        }
-                    })
+                        })
+                    }
+                } finally {
+                    ourProjectCount++
                 }
 
                 return ourApplicationEnvironment!!
