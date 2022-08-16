4b4684419d64c7818a37e821655da2a23f044345
Jim Sproch <jsproch@google.com>
2022-08-16 14:50:57 -0700

    Fix VersionChecker map comment for correctness/clarity
    Change-Id: Ic6b5d3d1caa7e6d9f7c070e113eed74a0a5c44c0 ( https://android-review.git.corp.google.com/q/Ic6b5d3d1caa7e6d9f7c070e113eed74a0a5c44c0 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -26,10 +26,11 @@ class VersionChecker(val context: IrPluginContext) {
 
     companion object {
         /**
-         * A table of runtime version ints to version strings. This should be
-         * updated every time ComposeVersion.kt is updated.
+         * A table of runtime version ints to version strings for compose-runtime.
+         * This should be updated every time a new version of the Compose Runtime is released.
+         * Typically updated via update_versions_for_release.py
          */
-        private val versionTable = mapOf(
+        private val runtimeVersionToMavenVersionTable = mapOf(
             1600 to "0.1.0-dev16",
             1700 to "1.0.0-alpha06",
             1800 to "1.0.0-alpha07",
@@ -103,7 +104,7 @@ class VersionChecker(val context: IrPluginContext) {
          */
         const val compilerVersion: String = "1.3.1"
         private val minimumRuntimeVersion: String
-            get() = versionTable[minimumRuntimeVersionInt] ?: "unknown"
+            get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
 
     fun check() {
@@ -137,7 +138,7 @@ class VersionChecker(val context: IrPluginContext) {
         }
         val versionInt = versionExpr.value as Int
         if (versionInt < minimumRuntimeVersionInt) {
-            outdatedRuntime(versionTable[versionInt] ?: "<unknown>")
+            outdatedRuntime(runtimeVersionToMavenVersionTable[versionInt] ?: "<unknown>")
         }
         // success. We are compatible with this runtime version!
     }
