540138e91c97240cb0abe823dc49f9c6f593684c
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2022-08-18 19:33:53 +0200

    [KDM] Add deprecation details block
    See https://github.com/Kotlin/dokka/pull/2622
    Original commit: https://github.com/Kotlin/dokka/commit/50a3323322265ff3b5dab1d861a25bbb1167812a

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/utils.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/main/kotlin/transformers/documentables/utils.kt
@@ -5,16 +5,26 @@ import org.jetbrains.dokka.model.Documentable
 import org.jetbrains.dokka.model.ExceptionInSupertypes
 import org.jetbrains.dokka.model.properties.WithExtraProperties
 
-fun <T> T.isDeprecated() where T : WithExtraProperties<out Documentable> =
-    deprecatedAnnotation != null
+val <T : WithExtraProperties<out Documentable>> T.isException: Boolean
+    get() = extra[ExceptionInSupertypes] != null
+
 
 val <T> T.deprecatedAnnotation where T : WithExtraProperties<out Documentable>
     get() = extra[Annotations]?.let { annotations ->
         annotations.directAnnotations.values.flatten().firstOrNull {
-            it.dri.toString() == "kotlin/Deprecated///PointingToDeclaration/" ||
-                    it.dri.toString() == "java.lang/Deprecated///PointingToDeclaration/"
+            it.isDeprecated()
         }
     }
 
-val <T : WithExtraProperties<out Documentable>> T.isException: Boolean
-    get() = extra[ExceptionInSupertypes] != null
+/**
+ * @return true if [T] has [kotlin.Deprecated] or [java.lang.Deprecated]
+ *         annotation for **any** source set
+ */
+fun <T> T.isDeprecated() where T : WithExtraProperties<out Documentable> = deprecatedAnnotation != null
+
+/**
+ * @return true for [kotlin.Deprecated] and [java.lang.Deprecated]
+ */
+fun Annotations.Annotation.isDeprecated() =
+    (this.dri.packageName == "kotlin" && this.dri.classNames == "Deprecated") ||
+            (this.dri.packageName == "java.lang" && this.dri.classNames == "Deprecated")
