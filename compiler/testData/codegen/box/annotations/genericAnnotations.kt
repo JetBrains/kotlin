2908a1467354464fd809f8b7f31092462cc5d8cf
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-08-22 16:46:30 +0200

    FIR JS: temporarily mute remaining codegen tests

--- compiler/testData/codegen/box/annotations/genericAnnotations.kt
+++ compiler/testData/codegen/box/annotations/genericAnnotations.kt
@@ -1,3 +1,4 @@
+// IGNORE_BACKEND_K2: JS_IR
 // MODULE: lib
 // FILE: l1.kt
 package ann
--- compiler/testData/codegen/box/delegation/withDefaultsMultipleFilesOrder.kt
+++ compiler/testData/codegen/box/delegation/withDefaultsMultipleFilesOrder.kt
@@ -1,3 +1,4 @@
+// IGNORE_BACKEND_K2: JS_IR
 // FILE: lib.kt
 interface ResolutionScope {
     fun getContributedDescriptors(s: String = "OK"): String
