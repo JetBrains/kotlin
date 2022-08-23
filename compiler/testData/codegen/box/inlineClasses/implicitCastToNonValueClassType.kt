e1ebf404f8c512c15a88f98423ffb8d73a2f6504
Xin Wang <scaventz@outlook.com>
2022-08-23 22:47:39 +0800

    Inline: Fix operand stack type verify error
    This is a proposal to fix KT-49364.
    Operand stack type verification happens before `checkcast`
    is executed. When we implicitly cast an inline class
    to a non-inline type, if type of stack value is not subtype
    of the target, then coercing is necessary.

---
+++ compiler/testData/codegen/box/inlineClasses/implicitCastToNonValueClassType.kt
@@ -0,0 +1,19 @@
+// TARGET_BACKEND: JVM_IR
+// WITH_STDLIB
+
+fun box(): String {
+    try {
+        val a:ULong = 1u
+        a as Int
+        func(a)
+    } catch (e: ClassCastException) {
+        return "OK"
+    }
+    return "FAIL"
+}
+
+fun func(para: Int) {}
+
+// CHECK_BYTECODE_TEXT
+// 1 CHECKCAST java/lang/Integer
+// 1 L2I
