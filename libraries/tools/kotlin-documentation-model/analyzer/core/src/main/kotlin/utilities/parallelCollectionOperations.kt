bfdf3c374d80eb255a2e93e136529811e51bf3e7
Vadim Mishenev <vad-mishenev@yandex.ru>
2022-08-29 13:27:43 +0300

    [KDM] Display inherited extensions
    See https://github.com/Kotlin/dokka/pull/2625
    Original commit: https://github.com/Kotlin/dokka/commit/e68eea63f2affbacf69af041252bad4444fc812f

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/utilities/parallelCollectionOperations.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/utilities/parallelCollectionOperations.kt
@@ -1,8 +1,6 @@
 package org.jetbrains.dokka.utilities
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 
 suspend inline fun <A, B> Iterable<A>.parallelMap(crossinline f: suspend (A) -> B): List<B> = coroutineScope {
     map { async { f(it) } }.awaitAll()
@@ -13,5 +11,5 @@ suspend inline fun <A, B> Iterable<A>.parallelMapNotNull(crossinline f: suspend 
 }
 
 suspend inline fun <A> Iterable<A>.parallelForEach(crossinline f: suspend (A) -> Unit): Unit = coroutineScope {
-    map { async { f(it) } }.awaitAll()
+    forEach { launch { f(it) } }
 }
