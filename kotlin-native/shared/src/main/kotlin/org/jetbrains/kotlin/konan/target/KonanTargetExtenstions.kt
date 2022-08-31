e64b057131d228a4ef5fcace25c19252021f2d81
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2022-08-31 16:53:39 +0200

    Native: make KonanTarget.supportsMimallocAllocator() exhaustive
    To prevent forgetting adding a target there again.

--- kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
+++ kotlin-native/shared/src/main/kotlin/org/jetbrains/kotlin/konan/target/KonanTargetExtenstions.kt
@@ -57,7 +57,12 @@ fun KonanTarget.supportsMimallocAllocator(): Boolean =
         is KonanTarget.IOS_ARM64 -> true
         is KonanTarget.IOS_X64 -> true
         is KonanTarget.IOS_SIMULATOR_ARM64 -> true
-        else -> false // watchOS/tvOS/android_x86/android_arm32 aren't tested; linux_mips32/linux_mipsel32 need linking with libatomic.
+        is KonanTarget.WATCHOS_ARM32, is KonanTarget.WATCHOS_ARM64,
+        is KonanTarget.WATCHOS_SIMULATOR_ARM64, is KonanTarget.WATCHOS_X64, is KonanTarget.WATCHOS_X86,
+        is KonanTarget.TVOS_ARM64, is KonanTarget.TVOS_SIMULATOR_ARM64, is KonanTarget.TVOS_X64,
+        is KonanTarget.ANDROID_X86, is KonanTarget.ANDROID_ARM32 -> false // aren't tested.
+        is KonanTarget.LINUX_MIPS32, is KonanTarget.LINUX_MIPSEL32 -> false // need linking with libatomic.
+        is KonanTarget.WASM32, is KonanTarget.ZEPHYR -> false // likely not supported
     }
 
 fun KonanTarget.supportsLibBacktrace(): Boolean =
