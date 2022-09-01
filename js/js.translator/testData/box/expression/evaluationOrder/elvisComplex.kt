f07163125b2f0cd0c67486c77d0444a0d8b7c8d2
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2022-09-01 19:29:17 +0200

    [JS Legacy] For Unit Elvis don't forget to generate code for the RHS
    ^KT-53780 Fixed

--- js/js.translator/testData/box/expression/evaluationOrder/elvisComplex.kt
+++ js/js.translator/testData/box/expression/evaluationOrder/elvisComplex.kt
@@ -10,6 +10,15 @@ val global_b = A(2)
 
 fun nullFun(value: Boolean): Any? = if (value) null else global_a
 
+fun padding(right: Int, left: Int) {
+    ++sideEffect
+}
+
+fun setSideEffect(newValue: Int): Int {
+    sideEffect = newValue
+    return sideEffect
+}
+
 fun box(): String {
 
     nullFun(false) ?: global_b
@@ -18,6 +27,9 @@ fun box(): String {
 
     assertEquals(global_b, nullFun(true) ?: try { ++sideEffect; global_b } finally {}, "true, global_b")
     assertEquals(1, sideEffect, "true, global_b side effect")
+
+    nullFun(true) ?: padding(left = setSideEffect(10), right = setSideEffect(42))
+    assertEquals(43, sideEffect)
 
     return "OK"
 }
