db74665f134a72acf080211c0312d552051d8ca8
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-09-08 12:24:00 +0300

    [K/N] Update Xcode version to 14-RC

--- kotlin-native/tools/scripts/update_xcode.sh
+++ kotlin-native/tools/scripts/update_xcode.sh
@@ -2,7 +2,7 @@
 set -e
 
 # "brew install coreutils" for grealpath.
-KONAN_TOOLCHAIN_VERSION=xcode_13_1
+KONAN_TOOLCHAIN_VERSION=xcode_14_rc
 SDKS="macosx iphoneos iphonesimulator appletvos appletvsimulator watchos watchsimulator"
 TARBALL_macosx=target-sysroot-$KONAN_TOOLCHAIN_VERSION-macosx
 TARBALL_iphoneos=target-sysroot-$KONAN_TOOLCHAIN_VERSION-iphoneos
