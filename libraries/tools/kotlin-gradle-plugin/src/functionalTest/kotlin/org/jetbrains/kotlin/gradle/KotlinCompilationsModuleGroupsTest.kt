5f085f0448391808254764c47d36bb7b0b712498
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-08 11:53:16 +0200

    [Gradle][MPP] Move KotlinCompilationsModuleGroupsTest into functionalTest
    ^KT-52726 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/KotlinCompilationsModuleGroupsTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/KotlinCompilationsModuleGroupsTest.kt
@@ -1,27 +1,24 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.gradle.plugin.mpp.internal
+package org.jetbrains.kotlin.gradle
 
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.dsl.multiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.mpp.internal.KotlinCompilationsModuleGroups
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.KotlinCompilationData
-import org.jetbrains.kotlin.gradle.plugin.sources.MockKotlinCompilation
-import org.jetbrains.kotlin.gradle.plugin.sources.MockKotlinSourceSet
-import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class KotlinCompilationsModuleGroupsTest {
-    private lateinit var instance: KotlinCompilationsModuleGroups
+    private val project = buildProjectWithMPP()
+    private val kotlin = project.multiplatformExtension
+    private val instance: KotlinCompilationsModuleGroups = KotlinCompilationsModuleGroups()
 
-    @Before
-    fun before() {
-        instance = KotlinCompilationsModuleGroups()
-    }
-
-    private fun compilation(name: String) = MockKotlinCompilation(name, MockKotlinSourceSet(name))
+    private fun compilation(name: String): AbstractKotlinCompilation<*> =
+        kotlin.jvm().compilations.maybeCreate(name)
 
     @Test
     fun testIdentityAsModuleLeaderForNewCompilation() {
