769a2ca9b19dc1c8b1c80470151bbe66b2ac667d
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-09-09 11:31:34 +0200

    K1: report ENUM_DECLARING_CLASS_DEPRECATED on enum entry properly
    #KT-53807 Fixed

--- compiler/testData/diagnostics/tests/enum/declaringClass.fir.kt
+++ compiler/testData/diagnostics/tests/enum/declaringClass.fir.kt
@@ -4,6 +4,12 @@
 
 import java.util.*
 
+enum class SomeEnum { A }
+
+fun bar() {
+    SomeEnum.A.declaringClass
+}
+
 fun <E : Enum<E>> foo(values: Array<E>) {
     EnumSet.noneOf(values.first().declaringClass)
     EnumSet.noneOf(values.first().getDeclaringClass())
--- compiler/testData/diagnostics/tests/enum/declaringClass.kt
+++ compiler/testData/diagnostics/tests/enum/declaringClass.kt
@@ -4,6 +4,12 @@
 
 import java.util.*
 
+enum class SomeEnum { A }
+
+fun bar() {
+    SomeEnum.A.<!ENUM_DECLARING_CLASS_DEPRECATED_WARNING!>declaringClass<!>
+}
+
 fun <E : Enum<E>> foo(values: Array<E>) {
     EnumSet.noneOf(values.first().<!ENUM_DECLARING_CLASS_DEPRECATED_WARNING!>declaringClass<!>)
     EnumSet.noneOf(values.first().<!UNRESOLVED_REFERENCE!>getDeclaringClass<!>())
