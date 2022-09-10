38dd68f1f46010954bc955118d7e1b15b4d10267
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-09-10 16:09:47 +0200

    [WASM] Enable passing tests

--- compiler/testData/codegen/box/defaultArguments/implementedByFake2.kt
+++ compiler/testData/codegen/box/defaultArguments/implementedByFake2.kt
@@ -1,8 +1,3 @@
-// IGNORE_BACKEND: WASM
-// WASM_MUTE_REASON: FAKE_OVERRIDE_ISSUES
-// On wasm this will produce conflicting return types, A.g will return Any but we will try to interpret it as String.
-// Before wasm native strings this worked by chance because we added unbox intrinsic for strings.
-
 interface I<T> {
     val prop: T
 
