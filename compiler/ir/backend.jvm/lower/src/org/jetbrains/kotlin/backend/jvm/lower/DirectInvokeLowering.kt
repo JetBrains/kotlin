4d513687920e0c3d872b4accec6058080b7f1ad3
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-09-12 16:27:40 +0200

    ~ DirectInvokeLowering

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/DirectInvokeLowering.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/DirectInvokeLowering.kt
@@ -122,13 +122,17 @@ private class DirectInvokeLowering(private val context: JvmBackendContext) : Fil
         if (irFun.dispatchReceiverParameter != null) {
             dispatchReceiver = irFunRef.dispatchReceiver ?: irInvokeCall.getValueArgument(invokeArgIndex++)
         }
-        if (irFun.extensionReceiverParameter != null) {
-            extensionReceiver = irFunRef.extensionReceiver ?: irInvokeCall.getValueArgument(invokeArgIndex++)
+
+        var irFunIndex = 0
+        if (irFunRef.extensionReceiver != null) {
+            putValueArgument(irFunIndex++, irFunRef.extensionReceiver)
         }
-        if (invokeArgIndex + valueArgumentsCount != irInvokeCall.valueArgumentsCount) {
+
+        if ((valueArgumentsCount - irFunIndex) != (irInvokeCall.valueArgumentsCount - invokeArgIndex)) {
             throw AssertionError("Mismatching value arguments: $invokeArgIndex arguments used for receivers\n${irInvokeCall.dump()}")
         }
-        for (i in 0 until valueArgumentsCount) {
+
+        for (i in irFunIndex until valueArgumentsCount) {
             putValueArgument(i, irInvokeCall.getValueArgument(invokeArgIndex++))
         }
     }
