2580a5c2337a21151f5eaeaba385fb7d4e6a241e
Jinseong Jeon <jsjeon@google.com>
2022-09-12 14:17:30 -0700

    AA: migrate PackagePartProviderFactory to general API providers

--- analysis/analysis-api-providers/src/org/jetbrains/kotlin/analysis/providers/PackagePartProviderFactory.kt
+++ analysis/analysis-api-providers/src/org/jetbrains/kotlin/analysis/providers/PackagePartProviderFactory.kt
@@ -1,23 +1,23 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.analysis.low.level.api.fir.api.services
+package org.jetbrains.kotlin.analysis.providers
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.load.kotlin.PackagePartProvider
 
-abstract class PackagePartProviderFactory {
+public abstract class PackagePartProviderFactory {
     /**
      * Create a [PackagePartProvider] for a given scope. [PackagePartProvider] is responsible for searching sub packages in a library.
      */
-    abstract fun createPackagePartProviderForLibrary(scope: GlobalSearchScope): PackagePartProvider
+    public abstract fun createPackagePartProvider(scope: GlobalSearchScope): PackagePartProvider
 }
 
 /**
  * Create a [PackagePartProvider] for a given scope. [PackagePartProvider] is responsible for searching sub packages in a library.
  */
-internal fun Project.createPackagePartProviderForLibrary(scope: GlobalSearchScope): PackagePartProvider =
-    getService(PackagePartProviderFactory::class.java).createPackagePartProviderForLibrary(scope)
+public fun Project.createPackagePartProvider(scope: GlobalSearchScope): PackagePartProvider =
+    getService(PackagePartProviderFactory::class.java).createPackagePartProvider(scope)
