8c221c0b73178210f4226484177124a655188d9c
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-09-13 17:55:32 +0300

    [Assign plugin] Add experimentality warning

--- plugins/assign-plugin/assign-plugin.cli/build.gradle.kts
+++ plugins/assign-plugin/assign-plugin.cli/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     api(project(":kotlin-assignment-compiler-plugin.common"))
     api(project(":kotlin-assignment-compiler-plugin.k1"))
     api(project(":kotlin-assignment-compiler-plugin.k2"))
+    compileOnly(project(":compiler:cli"))
     compileOnly(project(":compiler:util"))
     compileOnly(project(":compiler:plugin-api"))
     compileOnly(project(":compiler:fir:entrypoint"))
