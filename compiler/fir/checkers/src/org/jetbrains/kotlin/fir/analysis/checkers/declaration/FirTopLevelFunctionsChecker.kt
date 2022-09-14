e0170441f978f50aeae32a7449a26b15feb87410
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-09-14 10:09:21 +0300

    [FIR JS] Stop reporting NON_MEMBER_FUNCTION_NO_BODY for js natives

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/declaration/FirTopLevelFunctionsChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/declaration/FirTopLevelFunctionsChecker.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.fir.analysis.checkers.declaration
 import org.jetbrains.kotlin.KtFakeSourceElementKind
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.reportOn
+import org.jetbrains.kotlin.fir.analysis.checkers.FirPlatformDiagnosticSuppressor
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
 import org.jetbrains.kotlin.fir.analysis.checkers.hasModifier
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
@@ -18,7 +19,9 @@ import org.jetbrains.kotlin.fir.declarations.utils.isExternal
 import org.jetbrains.kotlin.lexer.KtTokens
 
 // See old FE's [DeclarationsChecker]
-object FirTopLevelFunctionsChecker : FirSimpleFunctionChecker() {
+abstract class FirTopLevelFunctionsChecker : FirSimpleFunctionChecker() {
+    abstract val suppressor: FirPlatformDiagnosticSuppressor
+
     override fun check(declaration: FirSimpleFunction, context: CheckerContext, reporter: DiagnosticReporter) {
         // Only report on top level callable declarations
         if (context.containingDeclarations.size > 1) return
@@ -29,7 +32,7 @@ object FirTopLevelFunctionsChecker : FirSimpleFunctionChecker() {
         // So, our source of truth should be the full modifier list retrieved from the source.
         if (declaration.hasModifier(KtTokens.ABSTRACT_KEYWORD)) return
         if (declaration.isExternal) return
-        if (!declaration.hasBody && !declaration.isExpect) {
+        if (!declaration.hasBody && !declaration.isExpect && suppressor.shouldReportNoBody(declaration, context)) {
             reporter.reportOn(source, FirErrors.NON_MEMBER_FUNCTION_NO_BODY, declaration.symbol, context)
         }
 
