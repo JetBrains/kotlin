60d6ea2fa788dd4ba4e2b2c6061255eaf5678a76
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-09-14 17:08:25 +0300

    [FIR JS] Support DYNAMIC_RECEIVER_NOT_ALLOWED

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnosticsImpl.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnosticsImpl.kt
@@ -1673,6 +1673,11 @@ internal class DynamicUpperBoundImpl(
     override val token: KtLifetimeToken,
 ) : KtFirDiagnostic.DynamicUpperBound(), KtAbstractFirDiagnostic<KtTypeReference>
 
+internal class DynamicReceiverNotAllowedImpl(
+    override val firDiagnostic: KtPsiDiagnostic,
+    override val token: KtLifetimeToken,
+) : KtFirDiagnostic.DynamicReceiverNotAllowed(), KtAbstractFirDiagnostic<KtElement>
+
 internal class IncompatibleTypesImpl(
     override val typeA: KtType,
     override val typeB: KtType,
