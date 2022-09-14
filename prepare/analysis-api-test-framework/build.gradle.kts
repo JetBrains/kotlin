51cece47419dfcbaac1ebcc4b39d2984038fe24d
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-09-14 12:44:42 +0200

    Publish Analysis API test framework

---
+++ prepare/analysis-api-test-framework/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+val testModules = listOf(
+    ":analysis:analysis-api-fir",
+    ":analysis:low-level-api-fir",
+    ":analysis:analysis-test-framework",
+    ":analysis:analysis-api-impl-barebone",
+    ":analysis:analysis-api-impl-base",
+    ":analysis:analysis-api-standalone",
+//    ":plugins:fir-plugin-prototype",
+)
+
+dependencies {
+    testModules.forEach {
+        embedded(projectTests(it)) { isTransitive = false }
+    }
+}
+
+publish()
+runtimeJar()
+sourcesJar {
+    from {
+        testModules.map { project(it).testSourceSet.allSource }
+    }
+}
