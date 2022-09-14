18e4d9e16c8fb780fe308c559d3b85c05dcf790a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2022-09-14 16:09:40 +0200

    Deprecate modules of the empty artifacts to discourage their usage
    #KT-51907

--- libraries/stdlib/jdk7/java9/module-info.java
+++ libraries/stdlib/jdk7/java9/module-info.java
@@ -1,4 +1,8 @@
+/**
+ * @deprecated This module is empty and therefore deprecated, please use <code>kotlin.stdlib</code> instead.
+ */
 @SuppressWarnings("module") // suppress warnings about terminal digit and exporting packages to not-yet-built kotlin-stdlib-jdk8
+@Deprecated
 module kotlin.stdlib.jdk7 {
     requires transitive kotlin.stdlib;
 }
