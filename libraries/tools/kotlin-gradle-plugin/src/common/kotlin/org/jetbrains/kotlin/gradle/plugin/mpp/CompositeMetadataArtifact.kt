7a1f4304d0c8d18428a6105cfce8898e525ecbe4
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-14 11:36:14 +0200

    [Gradle][MPP] Implement CompositeMetadataArtifact
    ^KT-48135 WIP

---
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/CompositeMetadataArtifact.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.plugin.mpp
+
+import java.io.Closeable
+import java.io.File
+
+internal interface CompositeMetadataArtifact {
+
+    interface Library {
+        val artifactHandle: ArtifactHandle
+        val sourceSet: SourceSet
+        val archiveExtension: String
+
+        /**
+         * The proposed file-output path.
+         * This can actually include several path parts if the [Library] requires additional scoping (e.g. [CInteropMetadataLibrary]s
+         * need to be put into another folder)
+         */
+        val relativeFile: File
+
+        /**
+         * Copies the content of this [Library] directly into the given [file].
+         * The [file] will be overwritten when it already exists.
+         * Parent directories will be created if necessary.
+         */
+        fun copyTo(file: File): Boolean
+
+        /**
+         * Copies the content of this [Library] into the [directory] appending the [relativeFile] to it.
+         * @see copyTo
+         */
+        fun copyIntoDirectory(directory: File) = copyTo(directory.resolve(relativeFile))
+    }
+
+    /**
+     * Represents a Kotlin Metadata library produced by compiling the contained [sourceSet]
+     */
+    interface MetadataLibrary : Library
+
+    /**
+     * Represents a CInterop Metadata library, produced by the commonizer and attached to the [sourceSet]
+     */
+    interface CInteropMetadataLibrary : Library {
+        val cinteropLibraryName: String
+    }
+
+    /**
+     * Represents a SourceSet packaged into the [CompositeMetadataArtifact]
+     */
+    interface SourceSet {
+        val artifactHandle: ArtifactHandle
+        val sourceSetName: String
+        val metadataLibrary: MetadataLibrary?
+        val cinteropMetadataLibraries: List<CInteropMetadataLibrary>
+    }
+
+    interface ArtifactHandle : Closeable {
+        val moduleDependencyIdentifier: ModuleDependencyIdentifier
+        val moduleDependencyVersion: String
+        val sourceSets: List<SourceSet>
+        fun getSourceSet(name: String): SourceSet
+        fun findSourceSet(name: String): SourceSet?
+    }
+
+    val moduleDependencyIdentifier: ModuleDependencyIdentifier
+    val moduleDependencyVersion: String
+
+    fun open(): ArtifactHandle
+
+    fun <T> read(action: (artifactHandle: ArtifactHandle) -> T): T {
+        return open().use(action)
+    }
+}
