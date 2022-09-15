3a5a6e5587648404c2ac8b9b5db20404eb74bced
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-09-15 14:21:56 +0300

    [FE] Add test for KT-54055

--- compiler/testData/diagnostics/tests/controlFlowAnalysis/uninitializedCompanionOfEnum_after.kt
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/uninitializedCompanionOfEnum_after.kt
@@ -1,5 +1,5 @@
 // LANGUAGE: +ProhibitAccessToEnumCompanionMembersInEnumConstructorCall
-// ISSUE: KT-49110
+// ISSUE: KT-49110, KT-54055
 
 enum class SomeEnum(val x: Int) {
     A(<!UNINITIALIZED_ENUM_COMPANION!>companionFun()<!>.length),// UNINITIALIZED_ENUM_COMPANION
@@ -35,5 +35,30 @@ enum class OtherEnum(val x: Int) {
 
 fun OtherEnum.Companion.extensionFun(): String = companionFun()
 val OtherEnum.Companion.extensionProp: String
+    get() = companionProp
+
+enum class EnumWithLambda(val lambda: () -> Unit) {
+    M({
+      <!UNINITIALIZED_ENUM_COMPANION!>companionFun()<!>.length
+      <!UNINITIALIZED_ENUM_COMPANION!>companionProp<!>.length
+
+      <!UNINITIALIZED_ENUM_COMPANION!>EnumWithLambda<!>.companionFun().length
+      <!UNINITIALIZED_ENUM_COMPANION!>EnumWithLambda<!>.companionProp.length
+
+      <!UNINITIALIZED_ENUM_COMPANION!>extensionFun()<!>.length
+      <!UNINITIALIZED_ENUM_COMPANION!>extensionProp<!>.length
+
+      <!UNINITIALIZED_ENUM_COMPANION!>EnumWithLambda<!>.extensionFun().length
+      <!UNINITIALIZED_ENUM_COMPANION!>EnumWithLambda<!>.extensionProp.length
+      });
+
+    companion object {
+        val companionProp = "someString"
+        fun companionFun(): String = "someString"
+    }
+}
+
+fun EnumWithLambda.Companion.extensionFun(): String = companionFun()
+val EnumWithLambda.Companion.extensionProp: String
     get() = companionProp
 
