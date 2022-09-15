842f94443ad4a2f556a4337a057d00b3f1f5db02
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-09-15 19:30:22 +0200

    [WASM] Kotlin gradle plugin support Apple ARM for binaryen and D8 extensions

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/d8/D8Platform.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/d8/D8Platform.kt
@@ -11,20 +11,37 @@ internal object D8Platform {
     const val LINUX = "linux"
     const val DARWIN = "mac"
 
-    val name: String = run {
-        val name = property("os.name").toLowerCase()
-        when {
-            name.contains("windows") -> WIN
-            name.contains("mac") -> DARWIN
-            name.contains("linux") -> LINUX
-            name.contains("freebsd") -> LINUX
-            else -> throw IllegalArgumentException("Unsupported OS: $name")
+    val name: String
+        get() {
+            val osName = property("os.name").toLowerCase()
+            return when {
+                osName.contains("windows") -> WIN
+                osName.contains("mac") -> DARWIN
+                osName.contains("linux") -> LINUX
+                osName.contains("freebsd") -> LINUX
+                else -> throw IllegalArgumentException("Unsupported OS: $osName")
+            }
         }
-    }
 
+    const val ARM64 = "arm64"
     const val X64 = "64"
     const val X86 = "86"
 
     val architecture: String
-        get() = if (property("os.arch").contains("64")) X64 else X86
+        get() {
+            val arch = property("os.arch")
+            return when {
+                arch == "aarch64" -> ARM64
+                arch.contains("64") -> X64
+                else -> X86
+            }
+        }
+
+    val platform: String
+        get() = when (val architecture = D8Platform.architecture) {
+            ARM64 -> "$name-$ARM64"
+            X64 -> name + X64
+            X86 -> name + X86
+            else -> error("Unexpected platform architecture $architecture")
+        }
 }
