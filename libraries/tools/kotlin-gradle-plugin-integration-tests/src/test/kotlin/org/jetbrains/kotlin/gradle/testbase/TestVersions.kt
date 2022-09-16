2ba7c7b8a99626a28c0058cd1af628b588cf06a8
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-16 12:39:52 +0200

    [Gradle][MPP] MPP/AGP compatibility: Bump maxSupportedVersion to 7.3
    ^KT-54071 Verification Pending

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -40,6 +40,7 @@ interface TestVersions {
         const val AGP_70 = "7.0.4"
         const val AGP_71 = "7.1.3"
         const val AGP_72 = "7.2.1"
+        const val AGP_73 = "7.3.0"
 
         const val MIN_SUPPORTED = AGP_36 // KotlinAndroidPlugin.MINIMAL_SUPPORTED_AGP_VERSION
         const val MAX_SUPPORTED = AGP_70 // Update once Gradle MAX_SUPPORTED version will be bumped to 7.2+
@@ -58,6 +59,7 @@ interface TestVersions {
         AGP_70(AGP.AGP_70, GradleVersion.version(Gradle.G_7_0), GradleVersion.version(Gradle.G_7_4), JavaVersion.VERSION_11),
         AGP_71(AGP.AGP_71, GradleVersion.version(Gradle.G_7_2), GradleVersion.version(Gradle.G_7_4), JavaVersion.VERSION_11),
         AGP_72(AGP.AGP_72, GradleVersion.version(Gradle.G_7_3), GradleVersion.version(Gradle.G_7_4), JavaVersion.VERSION_11),
+        AGP_73(AGP.AGP_73, GradleVersion.version(Gradle.G_7_4), GradleVersion.version(Gradle.G_7_5), JavaVersion.VERSION_11),
         ;
     }
 }
