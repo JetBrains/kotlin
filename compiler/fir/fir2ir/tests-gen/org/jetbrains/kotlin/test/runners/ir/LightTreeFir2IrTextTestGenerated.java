bd742bb95cf6591c55056d2fb06b00be86cc7cd4
Nikita Nazarov <nikita.nazarov@jetbrains.com>
2022-09-19 03:46:42 +0300

    Account for context class receivers when creating the ExpressionReceiver
    ^KT-54084 Fixed

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/LightTreeFir2IrTextTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/LightTreeFir2IrTextTestGenerated.java
@@ -794,6 +794,12 @@ public class LightTreeFir2IrTextTestGenerated extends AbstractLightTreeFir2IrTex
             }
 
             @Test
+            @TestMetadata("contextReceiverMethod.kt")
+            public void testContextReceiverMethod() throws Exception {
+                runTest("compiler/testData/ir/irText/declarations/contextReceivers/contextReceiverMethod.kt");
+            }
+
+            @Test
             @TestMetadata("contextualFunctionConversion.kt")
             public void testContextualFunctionConversion() throws Exception {
                 runTest("compiler/testData/ir/irText/declarations/contextReceivers/contextualFunctionConversion.kt");
