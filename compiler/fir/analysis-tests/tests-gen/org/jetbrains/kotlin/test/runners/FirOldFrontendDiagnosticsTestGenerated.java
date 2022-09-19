9f7c465eb066bd81a37ddf9379fe8fd0f420f804
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-09-19 13:39:17 +0200

    FE: add test for KT-53197

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
@@ -23023,6 +23023,12 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirDiagnosti
             }
 
             @Test
+            @TestMetadata("InternalPotentialOverride.kt")
+            public void testInternalPotentialOverride() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/override/InternalPotentialOverride.kt");
+            }
+
+            @Test
             @TestMetadata("InvisiblePotentialOverride.kt")
             public void testInvisiblePotentialOverride() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/override/InvisiblePotentialOverride.kt");
