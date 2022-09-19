d9c5f100dbb626fbfb2d89ee12d170fef49514bb
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-09-19 13:04:16 +0000

    [K/JS] Use intrinsics instead of new functions in IR backend.

--- libraries/stdlib/api/js/kotlin.js.kt
+++ libraries/stdlib/api/js/kotlin.js.kt
@@ -17,7 +17,7 @@ public external fun eval(expr: kotlin.String): dynamic
 
 public external fun js(code: kotlin.String): dynamic
 
-/*∆*/ public fun jsTypeOf(value_hack: kotlin.Any?): kotlin.String
+/*∆*/ public external fun jsTypeOf(a: kotlin.Any?): kotlin.String
 
 public fun json(vararg pairs: kotlin.Pair<kotlin.String, kotlin.Any?>): kotlin.js.Json
 
