e0eca45912e53eed3cbc563a80e1e31a17a54c77
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2022-09-19 20:11:31 +0200

    [WASM] Add w3c bindings

---
+++ libraries/stdlib/wasm/src/org.w3c/org.w3c.files.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+// NOTE: THIS FILE IS AUTO-GENERATED, DO NOT EDIT!
+// See github.com/kotlin/dukat for details
+
+package org.w3c.files
+
+import kotlin.js.*
+import org.khronos.webgl.*
+import org.w3c.dom.*
+import org.w3c.dom.events.*
+import org.w3c.xhr.*
+
+/**
+ * Exposes the JavaScript [Blob](https://developer.mozilla.org/en/docs/Web/API/Blob) to Kotlin
+ */
+public external open class Blob(blobParts: Array<Dynamic?> = definedExternally, options: BlobPropertyBag = definedExternally) : MediaProvider, ImageBitmapSource {
+    open val size: Number
+    open val type: String
+    open val isClosed: Boolean
+    fun slice(start: Int = definedExternally, end: Int = definedExternally, contentType: String = definedExternally): Blob
+    fun close()
+}
+
+public external interface BlobPropertyBag {
+    var type: String? /* = "" */
+        get() = definedExternally
+        set(value) = definedExternally
+}
+
+@Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+@kotlin.internal.InlineOnly
+public inline fun BlobPropertyBag(type: String? = ""): BlobPropertyBag {
+    val o = js("({})")
+    o["type"] = type
+    return o as BlobPropertyBag
+}
+
+/**
+ * Exposes the JavaScript [File](https://developer.mozilla.org/en/docs/Web/API/File) to Kotlin
+ */
+public external open class File(fileBits: Array<Dynamic?>, fileName: String, options: FilePropertyBag = definedExternally) : Blob {
+    open val name: String
+    open val lastModified: Int
+}
+
+public external interface FilePropertyBag : BlobPropertyBag {
+    var lastModified: Int?
+        get() = definedExternally
+        set(value) = definedExternally
+}
+
+@Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+@kotlin.internal.InlineOnly
+public inline fun FilePropertyBag(lastModified: Int? = undefined, type: String? = ""): FilePropertyBag {
+    val o = js("({})")
+    o["lastModified"] = lastModified
+    o["type"] = type
+    return o as FilePropertyBag
+}
+
+/**
+ * Exposes the JavaScript [FileList](https://developer.mozilla.org/en/docs/Web/API/FileList) to Kotlin
+ */
+public external abstract class FileList : ItemArrayLike<File> {
+    override fun item(index: Int): File?
+}
+
+@Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+@kotlin.internal.InlineOnly
+public inline operator fun FileList.get(index: Int): File? = asDynamic().getAny(index)
+
+/**
+ * Exposes the JavaScript [FileReader](https://developer.mozilla.org/en/docs/Web/API/FileReader) to Kotlin
+ */
+public external open class FileReader : EventTarget {
+    open val readyState: Short
+    open val result: Dynamic?
+    open val error: Dynamic?
+    var onloadstart: ((ProgressEvent) -> Dynamic?)?
+    var onprogress: ((ProgressEvent) -> Dynamic?)?
+    var onload: ((Event) -> Dynamic?)?
+    var onabort: ((Event) -> Dynamic?)?
+    var onerror: ((Event) -> Dynamic?)?
+    var onloadend: ((Event) -> Dynamic?)?
+    fun readAsArrayBuffer(blob: Blob)
+    fun readAsBinaryString(blob: Blob)
+    fun readAsText(blob: Blob, label: String = definedExternally)
+    fun readAsDataURL(blob: Blob)
+    fun abort()
+
+    companion object {
+        val EMPTY: Short
+        val LOADING: Short
+        val DONE: Short
+    }
+}
+
+/**
+ * Exposes the JavaScript [FileReaderSync](https://developer.mozilla.org/en/docs/Web/API/FileReaderSync) to Kotlin
+ */
+public external open class FileReaderSync {
+    fun readAsArrayBuffer(blob: Blob): ArrayBuffer
+    fun readAsBinaryString(blob: Blob): String
+    fun readAsText(blob: Blob, label: String = definedExternally): String
+    fun readAsDataURL(blob: Blob): String
+}
