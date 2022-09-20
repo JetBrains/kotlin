de43877161a4d5bfd6e6874289052e07e3f08fae
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-09-20 11:23:21 +0200

    K2: add more tests about false overrides
    Related to KT-53197

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/Fir2IrTextTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/Fir2IrTextTestGenerated.java
@@ -2597,6 +2597,12 @@ public class Fir2IrTextTestGenerated extends AbstractFir2IrTextTest {
         }
 
         @Test
+        @TestMetadata("internalPotentialFakeOverride.kt")
+        public void testInternalPotentialFakeOverride() throws Exception {
+            runTest("compiler/testData/ir/irText/firProblems/internalPotentialFakeOverride.kt");
+        }
+
+        @Test
         @TestMetadata("internalPotentialOverride.kt")
         public void testInternalPotentialOverride() throws Exception {
             runTest("compiler/testData/ir/irText/firProblems/internalPotentialOverride.kt");
