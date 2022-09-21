5ed3c201601a2ad6a9fcc067853620fc6cac2744
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2022-09-21 17:49:27 +0200

    [Gradle, JS] Compilation not depends on dukat

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/tasks/KotlinPackageJsonTask.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/tasks/KotlinPackageJsonTask.kt
@@ -132,10 +132,6 @@ abstract class KotlinPackageJsonTask : DefaultTask() {
 
             nodeJs.rootPackageJsonTaskProvider!!.configure { it.mustRunAfter(packageJsonTask) }
 
-            compilation.compileKotlinTaskProvider.dependsOn(npmInstallTask!!)
-            compilation.compileKotlinTaskProvider.dependsOn(nodeJs.storeYarnLockTaskProvider!!)
-            compilation.compileKotlinTaskProvider.dependsOn(packageJsonTask)
-
             return packageJsonTask
         }
     }
