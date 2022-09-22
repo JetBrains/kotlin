7f94de1e8ee2bdc13c51ab4610aee904ae2acef8
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-09-22 10:47:01 +0200

    !fixup Move ResolvedDependencyGraph to `.gradle.internal` package

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/cacheKlibUtils.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/cacheKlibUtils.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.gradle.utils
 import org.gradle.api.artifacts.result.ResolvedArtifactResult
 import org.gradle.api.artifacts.result.ResolvedDependencyResult
 import org.gradle.api.logging.Logger
+import org.jetbrains.kotlin.gradle.internal.ResolvedDependencyGraph
 import org.jetbrains.kotlin.library.resolveSingleFileKlib
 import org.jetbrains.kotlin.library.uniqueName
 import java.io.File
