6a52ecdc52c85d08b711c8b24fa9ef14cbe07e29
Artem Kobzar <artem.kobzar@jetbrains.com>
2022-09-22 17:17:18 +0200

    fix: renaming with es mode.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/Merger.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/Merger.kt
@@ -120,16 +120,7 @@ class Merger(
     private fun <T : JsNode> Map<JsName, JsName>.rename(rootNode: T): T {
         rootNode.accept(object : RecursiveJsVisitor() {
             override fun visitElement(node: JsNode) {
-                // We need it to not rename methods and fields inside class body
-                // Because if they are in clash with something, it means overriding
-                if (node is JsClass) {
-                    node.constructor?.accept(this)
-                    node.members.forEach {
-                        it.acceptChildren(this)
-                    }
-                } else {
-                    super.visitElement(node)
-                }
+                super.visitElement(node)
                 if (node is HasName) {
                     node.name = node.name?.let { rename(it) }
                 }
