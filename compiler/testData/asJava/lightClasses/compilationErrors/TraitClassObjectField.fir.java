a203ddfcef3d9bfb87538c5c3b0973c7a62cb1cd
Jinseong Jeon <jsjeon@google.com>
2022-09-22 23:03:49 -0700

    SLC: implement PsiVariable#computeConstantValue for light field
    (cherry picked from commit fc1eb0259044dc60793b44b44e6acd1ed419e378)

--- compiler/testData/asJava/lightClasses/compilationErrors/TraitClassObjectField.fir.java
+++ compiler/testData/asJava/lightClasses/compilationErrors/TraitClassObjectField.fir.java
@@ -3,12 +3,12 @@ public abstract interface TraitClassObjectField /* TraitClassObjectField*/ {
   public static final TraitClassObjectField.Companion Companion;
 
   @org.jetbrains.annotations.Nullable()
-  public static final java.lang.String x = "" /* initializer type: java.lang.String */;
+  public static final java.lang.String x = "" /* initializer type: java.lang.String */ /* constant value  */;
 
 
 public static final class Companion /* TraitClassObjectField.Companion*/ {
   @org.jetbrains.annotations.Nullable()
-  private final java.lang.String x = "" /* initializer type: java.lang.String */;
+  private final java.lang.String x = "" /* initializer type: java.lang.String */ /* constant value  */;
 
   @org.jetbrains.annotations.Nullable()
   private final java.lang.String y;
