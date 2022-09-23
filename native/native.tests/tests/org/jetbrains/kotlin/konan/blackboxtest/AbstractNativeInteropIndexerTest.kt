d300870a6164baaf15c3c190dfe192abe033b8f5
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-09-23 15:28:24 +0200

    Add test for KT-39120

--- native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/AbstractNativeInteropIndexerTest.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/AbstractNativeInteropIndexerTest.kt
@@ -24,36 +24,49 @@ import org.jetbrains.kotlin.konan.blackboxtest.support.runner.TestRunCheck
 import org.jetbrains.kotlin.konan.blackboxtest.support.runner.TestRunChecks
 
 @Tag("cinterop")
+abstract class AbstractNativeInteropIndexerFModulesTest : AbstractNativeInteropIndexerTest() {
+    override val fmodules = true
+}
+
+@Tag("cinterop")
+abstract class AbstractNativeInteropIndexerNoFModulesTest : AbstractNativeInteropIndexerTest() {
+    override val fmodules = false
+}
+
+@Tag("cinterop")
 abstract class AbstractNativeInteropIndexerTest : AbstractNativeSimpleTest() {
     abstract val fmodules: Boolean
 
-    protected fun runTest(@TestDataFile testPath: String): Unit {
-        val homeDirectory = KtTestUtil.getHomeDirectory()
-        val testPathFull = File(homeDirectory).resolve(testPath)
-        val testData = testPathFull.parentFile.parentFile
-        val testScriptFile = testData.resolve("cinterop_contents.sh").canonicalFile
-        val fmodulesArgs = if (fmodules) "-compiler-option -fmodules" else ""
+    protected fun runTest(@TestDataFile testPath: String) {
+        val testPathFull = File(KtTestUtil.getHomeDirectory()).resolve(testPath)
         val tempDir = testRunSettings.get<BaseDirs>().testBuildDir.resolve(testPath).apply { mkdirs() }
+
+        val dummyCompilerCall = LoggedData.CompilerCall(
+            parameters = LoggedData.CompilerParameters(home = testRunSettings.get(), compilerArgs = arrayOf(), sourceModules = listOf()),
+            exitCode = ExitCode.OK,
+            compilerOutput = "",
+            compilerOutputHasErrors = false,
+            duration = 0.seconds
+        )
         val testRun = TestRun(
             executable = TestExecutable(
-                executableFile = testScriptFile,
+                executableFile = testPathFull.parentFile.parentFile.resolve("cinterop_contents.sh").canonicalFile,
                 testNames = listOf(),
-                loggedCompilerCall = LoggedData.CompilerCall(
-                    parameters = LoggedData.CompilerParameters(home = testRunSettings.get(), compilerArgs = arrayOf(), sourceModules = listOf()),
-                    exitCode = ExitCode.OK,
-                    compilerOutput = "",
-                    compilerOutputHasErrors = false,
-                    duration = 0.seconds
-                )
+                loggedCompilerCall = dummyCompilerCall
             ),
             displayName = testPath,
             testCaseId = TestCaseId.Named(testPath),
-            runParameters = listOf(TestRunParameter.WithFreeCommandLineArguments(listOf(
-                testPathFull.canonicalPath,
-                tempDir.canonicalPath,
-                fmodulesArgs))),
+            runParameters = listOf(
+                TestRunParameter.WithFreeCommandLineArguments(
+                    listOf(
+                        testPathFull.canonicalPath,
+                        tempDir.canonicalPath,
+                        if (fmodules) "-compiler-option -fmodules" else ""
+                    )
+                )
+            ),
             checks = TestRunChecks(
-                executionTimeoutCheck = TestRunCheck.ExecutionTimeout.ShouldNotExceed(timeout = 10.seconds),
+                executionTimeoutCheck = TestRunCheck.ExecutionTimeout.ShouldNotExceed(timeout = 30.seconds),
                 exitCodeCheck = TestRunCheck.ExitCode.Expected(0),
                 outputDataFile = TestRunCheck.OutputDataFile(testPathFull.resolve("contents.gold.txt"))
             )
