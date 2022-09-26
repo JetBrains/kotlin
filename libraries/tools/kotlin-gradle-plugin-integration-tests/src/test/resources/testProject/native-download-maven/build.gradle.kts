82d28b6dbf17977fea00a75633aa516a257c71e1
Pavel Punegov <pavel.punegov@jetbrains.com>
2022-09-26 17:59:27 +0200

    [K/N][gradle][test] Tests KGP ability to download artifacts from maven

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/native-download-maven/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin("multiplatform").version("<pluginMarkerVersion>")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    val nativeMain by sourceSets.creating {
+        dependsOn(sourceSets["commonMain"])
+    }
+
+    targets.withType(KotlinNativeTarget::class.java).all {
+        compilations["main"].defaultSourceSet.dependsOn(nativeMain)
+    }
+
+    <SingleNativeTarget>("host") {
+        binaries {
+            executable()
+        }
+    }
+}
