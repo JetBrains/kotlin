f356aeb8e7aedb69151307bec2e3503947ef9049
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2022-09-26 17:59:38 +0200

    Bump Kotlin/Native version to 1.8.20-dev-204

--- build.gradle.kts
+++ build.gradle.kts
@@ -127,7 +127,7 @@ rootProject.apply {
 IdeVersionConfigurator.setCurrentIde(project)
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    extra["versions.kotlin-native"] = "1.8.0-dev-3049"
+    extra["versions.kotlin-native"] = "1.8.20-dev-204"
 }
 
 val irCompilerModules = arrayOf(
