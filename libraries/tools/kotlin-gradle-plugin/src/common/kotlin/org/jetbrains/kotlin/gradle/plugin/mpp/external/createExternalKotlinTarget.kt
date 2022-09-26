b6f3e810675e08a115e28a5c90444447c37c150a
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-26 16:19:31 +0200

    [Gradle] Initial 'externalTargetApi' w/ compilable Android prototype
    ^KT-54766 Verification Pending

---
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/external/createExternalKotlinTarget.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:OptIn(ExternalKotlinTargetApi::class)
+
+package org.jetbrains.kotlin.gradle.plugin.mpp.external
+
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.ExternalKotlinTargetApi
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.tasks.locateOrRegisterTask
+import org.jetbrains.kotlin.gradle.utils.lowerCamelCaseName
+
+@ExternalKotlinTargetApi
+fun <T : DecoratedExternalKotlinTarget> KotlinMultiplatformExtension.createExternalKotlinTarget(
+    descriptor: ExternalKotlinTargetDescriptor<T>
+): T {
+    val defaultConfiguration = project.configurations.maybeCreate(lowerCamelCaseName(descriptor.targetName, "default"))
+    val apiElementsConfiguration = project.configurations.maybeCreate(lowerCamelCaseName(descriptor.targetName, "apiElements"))
+    val runtimeElementsConfiguration = project.configurations.maybeCreate(lowerCamelCaseName(descriptor.targetName, "runtimeElements"))
+    val artifactsTaskLocator = ExternalKotlinTarget.ArtifactsTaskLocator { target ->
+        target.project.locateOrRegisterTask<Jar>(lowerCamelCaseName(descriptor.targetName, "jar"))
+    }
+
+    val target = ExternalKotlinTarget(
+        project = project,
+        targetName = descriptor.targetName,
+        platformType = descriptor.platformType,
+        defaultConfiguration = defaultConfiguration,
+        apiElementsConfiguration = apiElementsConfiguration,
+        runtimeElementsConfiguration = runtimeElementsConfiguration,
+        publishable = true,
+        kotlinComponents = emptySet(),
+        artifactsTaskLocator = artifactsTaskLocator
+    )
+
+    val decorated = descriptor.decoratedExternalTargetFactory.create(target)
+    target.onCreated()
+    descriptor.configure?.invoke(decorated)
+    targets.add(decorated)
+    return decorated
+}
+
+@ExternalKotlinTargetApi
+fun <T : DecoratedExternalKotlinTarget> KotlinMultiplatformExtension.createExternalKotlinTarget(
+    descriptor: ExternalKotlinTargetDescriptorBuilder<T>.() -> Unit
+): T {
+    return createExternalKotlinTarget(ExternalKotlinTargetDescriptor(descriptor))
+}
