b7eeaa0f9783287b8495b790cd1cbad97bfc93ee
Anton Lakotka <anton.lakotka@jetbrains.com>
2022-09-26 09:58:47 +0200

    !fixup Rename granularMetadata package to metadataTransformation

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/metadataTransformation/ProjectStructureMetadataExtractor.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/metadataTransformation/ProjectStructureMetadataExtractor.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.gradle.plugin.mpp.granularMetadata
+package org.jetbrains.kotlin.gradle.plugin.mpp.metadataTransformation
 
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier
 import org.gradle.api.artifacts.result.ResolvedArtifactResult
