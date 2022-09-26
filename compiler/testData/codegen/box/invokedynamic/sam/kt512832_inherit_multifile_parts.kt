1b8a60b5a5e2844fa526ff5ea69a8704cddb43ff
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2022-09-26 21:30:56 +0200

    KT-51282 Add tests

---
+++ compiler/testData/codegen/box/invokedynamic/sam/kt512832_inherit_multifile_parts.kt
@@ -0,0 +1,41 @@
+// INHERIT_MULTIFILE_PARTS
+// SAM_CONVERSIONS: INDY
+// TARGET_BACKEND: JVM_IR
+// JVM_TARGET: 1.8
+// WITH_STDLIB
+
+// FILE: Foo.java
+package org.example.foo;
+public interface Foo {
+    String foo(String s);
+}
+
+// FILE: bar1.kt
+
+@file:JvmMultifileClass
+@file:JvmName("Bar")
+
+package org.example.bar
+
+fun doFoo(s: String): String = s.toUpperCase()
+
+// FILE: bar2.kt
+
+@file:JvmMultifileClass
+@file:JvmName("Bar")
+
+package org.example.bar
+
+const val unused = 1
+
+// FILE: Baz.kt
+
+package org.example.baz
+
+import org.example.foo.Foo
+import org.example.bar.doFoo
+
+fun box(): String {
+    val foo = Foo(::doFoo)
+    return foo.foo("ok")
+}
