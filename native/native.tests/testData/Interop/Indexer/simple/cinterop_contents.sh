e84151afc9b95f149a993fa6f997d0ac7850c95d
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-09-26 18:08:27 +0200

    Use full path for test runs

--- native/native.tests/testData/Interop/Indexer/simple/cinterop_contents.sh
+++ native/native.tests/testData/Interop/Indexer/simple/cinterop_contents.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # exit when any command fails
 set -e
-TESTPATH=$1
-WORK_DIR=$2
-FMODULES_ARG="$3 $4"
+HOME=$1
+TESTPATH=$2
+WORK_DIR=$3
+FMODULES_ARG="$4 $5"
 
-TESTDATA=$TESTPATH/../..
+KONANHOME=$HOME/kotlin-native/dist
+FULLTESTPATH=$HOME/$TESTPATH
+TESTDATA=$FULLTESTPATH/../..
 KLIB=klib.klib
 cd $WORK_DIR
-cinterop -o $KLIB -def $TESTPATH/pod1.def -compiler-option -I$TESTDATA/include $FMODULES_ARG
-klib contents $KLIB
+$KONANHOME/bin/cinterop -o $KLIB -def $FULLTESTPATH/pod1.def -compiler-option -I$TESTDATA/include $FMODULES_ARG
+$KONANHOME/bin/klib contents $KLIB
 exit 0
