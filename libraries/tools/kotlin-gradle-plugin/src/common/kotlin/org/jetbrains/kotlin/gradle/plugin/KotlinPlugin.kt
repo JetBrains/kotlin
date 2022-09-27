a9df804addc387bfb4e11f1c161543ba919d6dd8
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-27 14:21:40 +0200

    [Gradle][Minor] Move KotlinAndroidPlugin into separate source file
    KT-54312

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinPlugin.kt
@@ -9,8 +9,6 @@ import com.android.build.gradle.*
 import org.gradle.api.*
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
 import org.jetbrains.kotlin.gradle.dsl.*
-import org.jetbrains.kotlin.gradle.internal.customizeKotlinDependencies
-import org.jetbrains.kotlin.gradle.model.builder.KotlinModelBuilder
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.Companion.kotlinPropertiesProvider
 import org.jetbrains.kotlin.gradle.plugin.mpp.*
 import org.jetbrains.kotlin.gradle.scripting.internal.ScriptingGradleSubplugin
@@ -76,79 +74,6 @@ internal open class KotlinPlugin(
         // For new IC this task is not needed
         if (!project.kotlinPropertiesProvider.useClasspathSnapshot) {
             super.configureClassInspectionForIC(project)
-        }
-    }
-}
-
-internal open class KotlinAndroidPlugin(
-    private val registry: ToolingModelBuilderRegistry
-) : Plugin<Project> {
-
-    override fun apply(project: Project) {
-        checkGradleCompatibility()
-
-        project.dynamicallyApplyWhenAndroidPluginIsApplied(
-            {
-                project.objects.newInstance(
-                    KotlinAndroidTarget::class.java,
-                    "",
-                    project
-                ).also {
-                    (project.kotlinExtension as KotlinAndroidProjectExtension).target = it
-                }
-            }
-        ) { androidTarget ->
-            applyUserDefinedAttributes(androidTarget)
-            customizeKotlinDependencies(project)
-            registry.register(KotlinModelBuilder(project.getKotlinPluginVersion(), androidTarget))
-            project.whenEvaluated { project.components.addAll(androidTarget.components) }
-        }
-    }
-
-    companion object {
-        private val minimalSupportedAgpVersion = AndroidGradlePluginVersion(4, 1, 3)
-        fun androidTargetHandler(): AndroidProjectHandler {
-            val tasksProvider = KotlinTasksProvider()
-            val androidGradlePluginVersion = AndroidGradlePluginVersion.currentOrNull
-
-            if (androidGradlePluginVersion != null) {
-                if (androidGradlePluginVersion < minimalSupportedAgpVersion) {
-                    throw IllegalStateException(
-                        "Kotlin: Unsupported version of com.android.tools.build:gradle plugin: " +
-                                "version $minimalSupportedAgpVersion or higher should be used with kotlin-android plugin"
-                    )
-                }
-            }
-
-            return AndroidProjectHandler(KotlinConfigurationTools(tasksProvider))
-        }
-
-        internal fun Project.dynamicallyApplyWhenAndroidPluginIsApplied(
-            kotlinAndroidTargetProvider: () -> KotlinAndroidTarget,
-            additionalConfiguration: (KotlinAndroidTarget) -> Unit = {}
-        ) {
-            var wasConfigured = false
-
-            androidPluginIds.forEach { pluginId ->
-                plugins.withId(pluginId) {
-                    wasConfigured = true
-                    val target = kotlinAndroidTargetProvider()
-                    androidTargetHandler().configureTarget(target)
-                    additionalConfiguration(target)
-                }
-            }
-
-            afterEvaluate {
-                if (!wasConfigured) {
-                    throw GradleException(
-                        """
-                        |'kotlin-android' plugin requires one of the Android Gradle plugins.
-                        |Please apply one of the following plugins to '${project.path}' project:
-                        |${androidPluginIds.joinToString(prefix = "- ", separator = "\n\t- ")}
-                        """.trimMargin()
-                    )
-                }
-            }
         }
     }
 }
