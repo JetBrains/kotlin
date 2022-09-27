57208add907b83fc5b2880a70c8673474bf7683b
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-09-27 15:02:04 +0200

    [Gradle][Minor] Move JsCompilationDetails into separate source file
    KT-54180

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/CompilationDetails.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/CompilationDetails.kt
@@ -12,7 +12,6 @@ import org.jetbrains.kotlin.gradle.plugin.*
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.*
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.*
 import org.jetbrains.kotlin.gradle.plugin.sources.*
-import org.jetbrains.kotlin.gradle.targets.js.KotlinJsTarget
 import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrCompilation
 import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrTarget
 import org.jetbrains.kotlin.gradle.utils.*
@@ -55,72 +54,6 @@ interface CompilationDetailsWithRuntime<T : KotlinCommonOptions> : CompilationDe
 internal val CompilationDetails<*>.associateCompilationsClosure: Iterable<CompilationDetails<*>>
     get() = closure { it.associateCompilations }
 
-
-internal open class JsCompilationDetails(
-    target: KotlinTarget,
-    compilationPurpose: String,
-    defaultSourceSet: KotlinSourceSet,
-) : DefaultCompilationDetailsWithRuntime<KotlinJsOptions, CompilerJsOptions>(
-    target,
-    compilationPurpose,
-    defaultSourceSet,
-    {
-        object : HasCompilerOptions<CompilerJsOptions> {
-            override val options: CompilerJsOptions =
-                target.project.objects.newInstance(CompilerJsOptionsDefault::class.java)
-        }
-    },
-    {
-        object : KotlinJsOptions {
-            override val options: CompilerJsOptions
-                get() = compilerOptions.options
-        }
-    }
-) {
-
-    internal abstract class JsCompilationDependenciesHolder @Inject constructor(
-        val target: KotlinTarget,
-        val compilationPurpose: String
-    ) : HasKotlinDependencies {
-        override val apiConfigurationName: String
-            get() = disambiguateNameInPlatform(API)
-
-        override val implementationConfigurationName: String
-            get() = disambiguateNameInPlatform(IMPLEMENTATION)
-
-        override val compileOnlyConfigurationName: String
-            get() = disambiguateNameInPlatform(COMPILE_ONLY)
-
-        override val runtimeOnlyConfigurationName: String
-            get() = disambiguateNameInPlatform(RUNTIME_ONLY)
-
-        protected open val disambiguationClassifierInPlatform: String?
-            get() = when (target) {
-                is KotlinJsTarget -> target.disambiguationClassifierInPlatform
-                is KotlinJsIrTarget -> target.disambiguationClassifierInPlatform
-                else -> error("Unexpected target type of $target")
-            }
-
-        private fun disambiguateNameInPlatform(simpleName: String): String {
-            return lowerCamelCaseName(
-                disambiguationClassifierInPlatform,
-                compilationPurpose.takeIf { it != KotlinCompilation.MAIN_COMPILATION_NAME },
-                "compilation",
-                simpleName
-            )
-        }
-
-        override fun dependencies(configure: KotlinDependencyHandler.() -> Unit): Unit =
-            DefaultKotlinDependencyHandler(this, target.project).run(configure)
-
-        override fun dependencies(configure: Action<KotlinDependencyHandler>) =
-            dependencies { configure.execute(this) }
-    }
-
-    override val kotlinDependenciesHolder: HasKotlinDependencies
-        get() = target.project.objects.newInstance(JsCompilationDependenciesHolder::class.java, target, compilationPurpose)
-
-}
 
 internal class JsIrCompilationDetails(
     target: KotlinTarget, compilationPurpose: String, defaultSourceSet: KotlinSourceSet
