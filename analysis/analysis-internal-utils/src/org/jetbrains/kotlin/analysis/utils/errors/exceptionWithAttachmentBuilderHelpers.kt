f441cbe27b772441a36f3ca304486ed424ae79b5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-09-28 19:24:33 +0200

    [Analysis] do not fail when reporting exception with invalid psi as attachment

--- analysis/analysis-internal-utils/src/org/jetbrains/kotlin/analysis/utils/errors/exceptionWithAttachmentBuilderHelpers.kt
+++ analysis/analysis-internal-utils/src/org/jetbrains/kotlin/analysis/utils/errors/exceptionWithAttachmentBuilderHelpers.kt
@@ -6,13 +6,15 @@
 package org.jetbrains.kotlin.analysis.utils.errors
 
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiInvalidElementAccessException
 import org.jetbrains.kotlin.analysis.utils.printer.getElementTextInContext
 import org.jetbrains.kotlin.psi.KtElement
 
 public fun ExceptionAttachmentBuilder.withPsiEntry(name: String, psi: PsiElement?) {
     withEntry(name, psi) { psiElement ->
-        when (psiElement) {
-            is KtElement -> psiElement.getElementTextInContext()
+        when {
+            !psiElement.isValid -> "INVALID PSI ${PsiInvalidElementAccessException.findOutInvalidationReason(psiElement)}"
+            psiElement is KtElement -> psiElement.getElementTextInContext()
             else -> psiElement.text
         }
     }
