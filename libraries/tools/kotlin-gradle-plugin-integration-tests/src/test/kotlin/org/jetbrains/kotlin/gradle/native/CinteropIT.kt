0208dc15b2fb2663cde90f5b6e8aa24d0aec72df
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-09-28 15:49:48 +0200

    [KT-53191] Fix header dirs inputs for CInteropProcess task.

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/CinteropIT.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.native
+
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+
+@MppGradlePluginTests
+class CinteropIT : KGPBaseTest() {
+
+    @GradleTest
+    fun `rerun cinterop after a header changing`(gradleVersion: GradleVersion) {
+        nativeProject("cinterop-kt-53191", gradleVersion = gradleVersion) {
+            val headerFile = projectPath.resolve("native_lib/nlib.h").toFile()
+
+            build(":compileKotlinLinux") {
+                assertTasksExecuted(":cinteropNlibLinux")
+            }
+            build(":compileKotlinLinux") {
+                assertTasksUpToDate(":cinteropNlibLinux", ":compileKotlinLinux")
+            }
+
+            headerFile.writeText("void foo();")
+            buildAndFail(":compileKotlinLinux") {
+                assertTasksExecuted(":cinteropNlibLinux")
+                assertOutputContains("src/linuxMain/kotlin/org/sample/Platform.kt:2:10 Unresolved reference: sample")
+            }
+
+            headerFile.writeText("void sample(int i);")
+            build(":compileKotlinLinux") {
+                assertTasksExecuted(":cinteropNlibLinux")
+            }
+        }
+    }
+}
