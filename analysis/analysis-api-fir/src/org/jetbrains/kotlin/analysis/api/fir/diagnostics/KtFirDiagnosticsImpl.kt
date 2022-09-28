5f11da8800723fbd38134627c48a4d459f7b9095
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-09-28 13:38:18 +0000

    [FIR] KT-46483: Forbid annotations in where Clauses
    Merge-request: KT-MR-7208
    Merged-by: Nikolay Lunyak <lunyak.kolya@mail.ru>

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnosticsImpl.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnosticsImpl.kt
@@ -827,6 +827,11 @@ internal class WrongExtensionFunctionTypeWarningImpl(
     override val token: KtLifetimeToken,
 ) : KtFirDiagnostic.WrongExtensionFunctionTypeWarning(), KtAbstractFirDiagnostic<KtAnnotationEntry>
 
+internal class AnnotationInWhereClauseErrorImpl(
+    override val firDiagnostic: KtPsiDiagnostic,
+    override val token: KtLifetimeToken,
+) : KtFirDiagnostic.AnnotationInWhereClauseError(), KtAbstractFirDiagnostic<KtAnnotationEntry>
+
 internal class OptInUsageImpl(
     override val optInMarkerFqName: FqName,
     override val message: String,
