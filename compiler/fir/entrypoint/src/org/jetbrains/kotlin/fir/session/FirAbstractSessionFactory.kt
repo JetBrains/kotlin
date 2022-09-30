281c79e21e5268b6600fab7ffd929b7275b61843
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2022-09-30 22:25:04 +0200

    [FIR] Inline createModuleDataForBuiltins method since it doesn't look useful

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirAbstractSessionFactory.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/FirAbstractSessionFactory.kt
@@ -24,8 +24,6 @@ import org.jetbrains.kotlin.fir.scopes.FirKotlinScopeProvider
 import org.jetbrains.kotlin.incremental.components.EnumWhenTracker
 import org.jetbrains.kotlin.incremental.components.LookupTracker
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.platform.TargetPlatform
-import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
 
 @OptIn(PrivateSessionConstructor::class, SessionConfiguration::class)
 abstract class FirAbstractSessionFactory {
@@ -51,11 +49,12 @@ abstract class FirAbstractSessionFactory {
             val kotlinScopeProvider = createKotlinScopeProvider.invoke()
             register(FirKotlinScopeProvider::class, kotlinScopeProvider)
 
-            val builtinsModuleData = createModuleDataForBuiltins(
-                mainModuleName,
+            val builtinsModuleData = DependencyListForCliModule.createDependencyModuleData(
+                Name.special("<builtins of ${mainModuleName.identifier}"),
                 moduleDataProvider.platform,
-                moduleDataProvider.analyzerServices
-            ).also { it.bindSession(this@session) }
+                moduleDataProvider.analyzerServices,
+            )
+            builtinsModuleData.bindSession(this@session)
 
             val providers = createProviders(this, builtinsModuleData, kotlinScopeProvider)
 
@@ -63,18 +62,6 @@ abstract class FirAbstractSessionFactory {
             register(FirSymbolProvider::class, symbolProvider)
             register(FirProvider::class, FirLibrarySessionProvider(symbolProvider))
         }
-    }
-
-    private fun createModuleDataForBuiltins(
-        parentModuleName: Name,
-        platform: TargetPlatform,
-        analyzerServices: PlatformDependentAnalyzerServices
-    ): FirModuleData {
-        return DependencyListForCliModule.createDependencyModuleData(
-            Name.special("<builtins of ${parentModuleName.identifier}"),
-            platform,
-            analyzerServices,
-        )
     }
 
     protected fun createModuleBasedSession(
