ce4b87b32a917f3f8b1eba91b9f9bd023307d3a5
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-10-04 18:49:28 +0200

    [Gradle] KotlinCompilationImplFactory.PostConfigure: Use DecoratedKotlinCompilation
    to clear up dedicated usage
    
    KT-54312

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/compilationImpl/factory/DefaultKotlinCompilationPostConfigure.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/compilationImpl/factory/DefaultKotlinCompilationPostConfigure.kt
@@ -5,11 +5,12 @@
 
 package org.jetbrains.kotlin.gradle.plugin.mpp.compilationImpl.factory
 
+import org.jetbrains.kotlin.gradle.plugin.mpp.DecoratedKotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.mpp.InternalKotlinCompilation
 import org.jetbrains.kotlin.gradle.plugin.sources.internal
 
 internal object DefaultKotlinCompilationPostConfigure : KotlinCompilationImplFactory.PostConfigure {
-    override fun configure(compilation: InternalKotlinCompilation<*>) {
+    override fun configure(compilation: DecoratedKotlinCompilation<*>) {
         /* Ensure proper tracking of compilations in the source sets */
         compilation.allKotlinSourceSets.forAll { sourceSet ->
             sourceSet.internal.compilations.add(compilation)
