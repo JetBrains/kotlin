39ddb4f4eac44401f15e85b898106b219f369ac8
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-10-04 14:37:55 +0300

    [Assign plugin] Add assign plugin to the list of plugins for compiler dist

--- prepare/compiler/build.gradle.kts
+++ prepare/compiler/build.gradle.kts
@@ -118,7 +118,8 @@ val distCompilerPluginProjects = listOf(
     ":kotlin-noarg-compiler-plugin",
     ":kotlin-sam-with-receiver-compiler-plugin",
     ":kotlinx-serialization-compiler-plugin",
-    ":kotlin-lombok-compiler-plugin"
+    ":kotlin-lombok-compiler-plugin",
+    ":kotlin-assignment-compiler-plugin"
 )
 
 val distSourcesProjects = listOfNotNull(
