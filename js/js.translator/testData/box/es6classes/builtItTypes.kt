5db1b5639a538a72fc1eee892e9e589d947ffbcf
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2022-10-05 13:52:54 +0200

    ~ WIP support of decorators

--- js/js.translator/testData/box/es6classes/builtItTypes.kt
+++ js/js.translator/testData/box/es6classes/builtItTypes.kt
@@ -1,11 +1,57 @@
 // ES6_MODE
 // EXPECTED_REACHABLE_NODES: 1331
+
+
+open class LitElement
+
+annotation class JsDecorator
+
+@JsDecorator
+@JsName("customElement")
+external annotation class external (val tagName: String)
+
+@JsDecorator
+@JsName("property")
+external annotation class Property(/*val options: PropertyDeclaration? = null*/)
+annotation class JsStatic
+annotation class JsStringLiterl
+
+@JsStringLiterl
+fun html(s: String) {}
+// Array<String>, vararg others: Any?
+
+@JsStringLiterl
+fun css(s: String): String = s
+
+@CustomElement("simple-greeting")
+class SimpleGreeting : LitElement() {
+    @Property()
+    val name = "Somebody"
+
+    fun render() {
+        return html("<p>Hello, $name! $name</p>")
+        html(["<p>Hello, ","!</p>"], name, name)
+    }
+
+    companion object {
+         @JsStatic
+        val styles = css("p { color: blue }")
+
+        // SimpleGreeting.styles = css("p { color: blue }")
+    }
+}
+
+// SimpleGreeting.Companion = ...
 fun box(): String {
-    val s = String()
-    val ints = Array<Int>(2) { i -> (i + 2) * 2 }
+    return "OK"
+}
 
-    assertEquals(4, ints[0])
-    assertEquals(6, ints[1])
+// FILE: mylit.js
 
-    return "OK" + s
+function customElement() {
+    console.log("customElement: ", arguments);
+}
+
+function property() {
+    console.log("property: ", arguments);
 }
