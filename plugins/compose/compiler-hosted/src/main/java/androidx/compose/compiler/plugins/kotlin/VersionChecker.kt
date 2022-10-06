900ca53a55d65a6e9f4023767afb094d21430ee0
Nick Anthony <nickanthony@google.com>
2022-10-06 14:48:15 +0000

    Update Compose Compiler to 1.3.3
    Bumping to the next bug fix, assuming we're only adding bug fixes for now.
    
    Change-Id: Ifc6c2d0aaa7d40fe26ab5294456a18ce168aee80 ( https://android-review.git.corp.google.com/q/Ifc6c2d0aaa7d40fe26ab5294456a18ce168aee80 )
    Test: gw bOS

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -106,7 +106,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.3.2"
+        const val compilerVersion: String = "1.3.3"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
