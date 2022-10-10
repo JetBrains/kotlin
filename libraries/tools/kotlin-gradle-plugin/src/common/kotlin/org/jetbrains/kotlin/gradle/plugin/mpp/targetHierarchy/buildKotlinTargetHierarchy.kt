cdca1a10ca759db386fcfd0f7b643302c1e410c5
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2022-10-10 20:36:10 +0200

    [Gradle][MPP] Refine naming for KotlinTargetHierarchyBuilder
    ^KT-53570 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/targetHierarchy/buildKotlinTargetHierarchy.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/targetHierarchy/buildKotlinTargetHierarchy.kt
@@ -35,7 +35,7 @@ private class KotlinTargetHierarchyBuilderImplContext(private val compilation: K
     fun build(node: KotlinTargetHierarchy.Node): KotlinTargetHierarchy? {
         return builtValues.getOrPut(node) {
             val builder = getOrCreateBuilder(node)
-            if (!builder.includes(compilation)) return@getOrPut null
+            if (compilation !in builder) return@getOrPut null
 
             /*
             Keep the hierarchy 'deduplicated'.
@@ -66,21 +66,21 @@ private class KotlinTargetHierarchyBuilderImpl(
     private var includePredicate: ((KotlinCompilation<*>) -> Boolean) = { false }
     private var excludePredicate: ((KotlinCompilation<*>) -> Boolean) = { false }
 
-    override fun includeCompilationIf(predicate: (KotlinCompilation<*>) -> Boolean) {
+    override fun addCompilations(predicate: (KotlinCompilation<*>) -> Boolean) {
         val previousIncludePredicate = this.includePredicate
         val previousExcludePredicate = this.excludePredicate
         this.includePredicate = { previousIncludePredicate(it) || predicate(it) }
         this.excludePredicate = { previousExcludePredicate(it) && !predicate(it) }
     }
 
-    override fun excludeCompilationIf(predicate: (KotlinCompilation<*>) -> Boolean) {
+    override fun removeCompilations(predicate: (KotlinCompilation<*>) -> Boolean) {
         val previousIncludePredicate = this.includePredicate
         val previousExcludePredicate = this.excludePredicate
         this.includePredicate = { previousIncludePredicate(it) && !predicate(it) }
         this.excludePredicate = { previousExcludePredicate(it) || predicate(it) }
     }
 
-    fun includes(compilation: KotlinCompilation<*>): Boolean {
+    operator fun contains(compilation: KotlinCompilation<*>): Boolean {
         /* Return eagerly, when compilation is explicitly excluded */
         if (excludePredicate(compilation)) return false
 
@@ -88,10 +88,10 @@ private class KotlinTargetHierarchyBuilderImpl(
         if (includePredicate(compilation)) return true
 
         /* Find any child that includes this compilation */
-        return childrenClosure.any { child -> child.includes(compilation) }
+        return childrenClosure.any { child -> compilation in child }
     }
 
-    private inline fun includeTargetIf(crossinline predicate: (KotlinTarget) -> Boolean) = includeCompilationIf { predicate(it.target) }
+    private inline fun addTargets(crossinline predicate: (KotlinTarget) -> Boolean) = addCompilations { predicate(it.target) }
 
     override fun group(name: String, build: KotlinTargetHierarchyBuilder.() -> Unit) {
         val node = KotlinTargetHierarchy.Node.Group(name)
@@ -100,135 +100,135 @@ private class KotlinTargetHierarchyBuilderImpl(
         checkCyclicHierarchy()
     }
 
-    override fun anyNative() = includeTargetIf { it is KotlinNativeTarget }
+    override fun anyNative() = addTargets { it is KotlinNativeTarget }
 
-    override fun anyApple() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family.isAppleFamily }
+    override fun anyApple() = addTargets { it is KotlinNativeTarget && it.konanTarget.family.isAppleFamily }
 
-    override fun anyIos() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.IOS }
+    override fun anyIos() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.IOS }
 
-    override fun anyWatchos() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.WATCHOS }
+    override fun anyWatchos() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.WATCHOS }
 
-    override fun anyMacos() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.OSX }
+    override fun anyMacos() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.OSX }
 
-    override fun anyTvos() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.TVOS }
+    override fun anyTvos() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.TVOS }
 
-    override fun anyMingw() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.MINGW }
+    override fun anyMingw() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.MINGW }
 
-    override fun anyLinux() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.LINUX }
+    override fun anyLinux() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.LINUX }
 
-    override fun anyAndroidNative() = includeTargetIf { it is KotlinNativeTarget && it.konanTarget.family == Family.ANDROID }
+    override fun anyAndroidNative() = addTargets { it is KotlinNativeTarget && it.konanTarget.family == Family.ANDROID }
 
-    override fun anyJs() = includeTargetIf { it is KotlinJsTargetDsl }
+    override fun anyJs() = addTargets { it is KotlinJsTargetDsl }
 
-    override fun jvm() = includeTargetIf { it is KotlinJvmTarget }
+    override fun jvm() = addTargets { it is KotlinJvmTarget }
 
-    override fun android() = includeTargetIf { it is KotlinAndroidTarget }
+    override fun android() = addTargets { it is KotlinAndroidTarget }
 
-    override fun androidNativeX64() = includeTargetIf {
+    override fun androidNativeX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.ANDROID_X64
     }
 
-    override fun androidNativeX86() = includeTargetIf {
+    override fun androidNativeX86() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.ANDROID_X86
     }
 
-    override fun androidNativeArm32() = includeTargetIf {
+    override fun androidNativeArm32() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.ANDROID_X86
     }
 
-    override fun androidNativeArm64() = includeTargetIf {
+    override fun androidNativeArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.ANDROID_ARM64
     }
 
-    override fun iosArm32() = includeTargetIf {
+    override fun iosArm32() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.IOS_ARM32
     }
 
-    override fun iosArm64() = includeTargetIf {
+    override fun iosArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.IOS_ARM64
     }
 
-    override fun iosX64() = includeTargetIf {
+    override fun iosX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.IOS_X64
     }
 
-    override fun iosSimulatorArm64() = includeTargetIf {
+    override fun iosSimulatorArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.IOS_SIMULATOR_ARM64
     }
 
-    override fun watchosArm32() = includeTargetIf {
+    override fun watchosArm32() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WATCHOS_ARM32
     }
 
-    override fun watchosArm64() = includeTargetIf {
+    override fun watchosArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WATCHOS_ARM64
     }
 
-    override fun watchosX86() = includeTargetIf {
+    override fun watchosX86() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WATCHOS_X86
     }
 
-    override fun watchosX64() = includeTargetIf {
+    override fun watchosX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WATCHOS_X64
     }
 
-    override fun watchosSimulatorArm64() = includeTargetIf {
+    override fun watchosSimulatorArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WATCHOS_SIMULATOR_ARM64
     }
 
-    override fun watchosDeviceArm64() = includeTargetIf {
+    override fun watchosDeviceArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WATCHOS_DEVICE_ARM64
     }
 
-    override fun tvosArm64() = includeTargetIf {
+    override fun tvosArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.TVOS_ARM64
     }
 
-    override fun tvosX64() = includeTargetIf {
+    override fun tvosX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.TVOS_X64
     }
 
-    override fun tvosSimulatorArm64() = includeTargetIf {
+    override fun tvosSimulatorArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.TVOS_SIMULATOR_ARM64
     }
 
-    override fun linuxX64() = includeTargetIf {
+    override fun linuxX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.LINUX_X64
     }
 
-    override fun mingwX86() = includeTargetIf {
+    override fun mingwX86() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.MINGW_X86
     }
 
-    override fun mingwX64() = includeTargetIf {
+    override fun mingwX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.MINGW_X64
     }
 
-    override fun macosX64() = includeTargetIf {
+    override fun macosX64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.MACOS_X64
     }
 
-    override fun macosArm64() = includeTargetIf {
+    override fun macosArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.MACOS_ARM64
     }
 
-    override fun linuxArm64() = includeTargetIf {
+    override fun linuxArm64() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.LINUX_ARM64
     }
 
-    override fun linuxArm32Hfp() = includeTargetIf {
+    override fun linuxArm32Hfp() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.LINUX_ARM32_HFP
     }
 
-    override fun linuxMips32() = includeTargetIf {
+    override fun linuxMips32() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.LINUX_MIPS32
     }
 
-    override fun linuxMipsel32() = includeTargetIf {
+    override fun linuxMipsel32() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.LINUX_MIPSEL32
     }
 
-    override fun wasm32() = includeTargetIf {
+    override fun wasm32() = addTargets {
         it is KotlinNativeTarget && it.konanTarget == KonanTarget.WASM32
     }
 
