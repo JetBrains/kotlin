c579abd359acb95da9e507a0b777e81a7d105cf3
pyos <pyos@google.com>
2022-10-12 10:15:11 +0200

    FIR: support > 1 most specific members in type intersection scopes

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/impl/AbstractFirUseSiteMemberScope.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/impl/AbstractFirUseSiteMemberScope.kt
@@ -130,7 +130,7 @@ abstract class AbstractFirUseSiteMemberScope(
              *
              * TODO: is it enough to check only one function?
              */
-            mostSpecific
+            keySymbol
         } else {
             chosenSymbol
         }
