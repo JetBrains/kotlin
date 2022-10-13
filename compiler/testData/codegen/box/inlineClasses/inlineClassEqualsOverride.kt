b5a23e3ef7f41d3aa97c5e9396940262492c9656
vladislav.grechko <vladislav.grechko@gmail.com>
2022-10-13 18:54:34 +0200

    Minor: improve usability of tests for custom equals in inline classes

--- compiler/testData/codegen/box/inlineClasses/inlineClassEqualsOverride.kt
+++ compiler/testData/codegen/box/inlineClasses/inlineClassEqualsOverride.kt
@@ -33,8 +33,39 @@ value class IC4(val value: Int) {
     override fun equals(other: Any?) = TODO()
 }
 
-fun box() =
-    if (IC1(1.0) == IC1(1.05) && IC1(1.0) != IC1(1.2)
-        && IC2(5) == IC2(6) && IC2(5) != IC2(7)
-        && IC3(5) == IC3(5) && IC3(5) != IC3(6)
-        && IC1(1.0) != Any()) "OK" else "Fail"
+fun box() = when {
+    IC1(1.0) != IC1(1.05) -> "Fail 1.1"
+    (IC1(1.0) as Any) != IC1(1.05) -> "Fail 1.2"
+    IC1(1.0) != (IC1(1.05) as Any) -> "Fail 1.3"
+    (IC1(1.0) as Any) != (IC1(1.05) as Any) -> "Fail 1.4"
+
+    IC1(1.0) == IC1(1.2) -> "Fail 2.1"
+    (IC1(1.0) as Any) == IC1(1.2) -> "Fail 2.2"
+    IC1(1.0) == (IC1(1.2) as Any) -> "Fail 2.3"
+    (IC1(1.0) as Any) == (IC1(1.2) as Any) -> "Fail 2.4"
+
+    IC2(5) != IC2(6) -> "Fail 3.1"
+    (IC2(5) as Any) != IC2(6) -> "Fail 3.2"
+    IC2(5) != (IC2(6) as Any) -> "Fail 3.3"
+    (IC2(5) as Any) != (IC2(6) as Any) -> "Fail 3.4"
+
+    IC2(5) == IC2(7) -> "Fail 4.1"
+    (IC2(5) as Any) == IC2(7) -> "Fail 4.2"
+    IC2(5) == (IC2(7) as Any) -> "Fail 4.3"
+    (IC2(5) as Any) == (IC2(7) as Any) -> "Fail 4.4"
+
+    IC3(5) != IC3(5) -> "Fail 5.1"
+    (IC3(5) as Any) != IC3(5) -> "Fail 5.2"
+    IC3(5) != (IC3(5) as Any) -> "Fail 5.3"
+    (IC3(5) as Any) != (IC3(5) as Any) -> "Fail 5.4"
+
+    IC3(5) == IC3(6) -> "Fail 6.1"
+    (IC3(5) as Any) == IC3(6) -> "Fail 6.2"
+    IC3(5) == (IC3(6) as Any) -> "Fail 6.3"
+    (IC3(5) as Any) == (IC3(6) as Any) -> "Fail 6.4"
+
+    IC1(1.0) == Any() -> "Fail 7.1"
+    (IC1(1.0) as Any) == Any() -> "Fail 7.2"
+
+    else -> "OK"
+}
