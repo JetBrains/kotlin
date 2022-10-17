53678cc6d80e500cd035ab84d00bc48fa6849757
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2022-10-17 18:08:20 +0200

    Use one tuCache per index

--- kotlin-native/Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/defFileDependencies.kt
+++ kotlin-native/Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/defFileDependencies.kt
@@ -7,9 +7,7 @@ import org.jetbrains.kotlin.native.interop.gen.jvm.KotlinPlatform
 import org.jetbrains.kotlin.native.interop.gen.jvm.buildNativeLibrary
 import org.jetbrains.kotlin.native.interop.gen.jvm.prepareTool
 import org.jetbrains.kotlin.native.interop.indexer.NativeLibraryHeaders
-import org.jetbrains.kotlin.native.interop.indexer.TUCache
 import org.jetbrains.kotlin.native.interop.indexer.getHeaderPaths
-import org.jetbrains.kotlin.native.interop.indexer.use
 import org.jetbrains.kotlin.native.interop.tool.CInteropArguments
 import java.io.File
 import java.util.stream.Collectors
@@ -49,17 +47,15 @@ private fun makeDependencyAssignerForTarget(target: String, defFiles: List<File>
     val tool = prepareTool(target, KotlinPlatform.NATIVE, runFromDaemon)
     val cinteropArguments = CInteropArguments()
     cinteropArguments.argParser.parse(arrayOf())
-    return TUCache().use { tuCache ->
-        val libraries = defFiles.parallelStream().map {
-            it to buildNativeLibrary(
-                    tool,
-                    DefFile(it, tool.substitutions),
-                    cinteropArguments,
-                    ImportsImpl(emptyMap())
-            ).getHeaderPaths(tuCache)
-        }.toList().toMap()
-        SingleTargetDependencyAssigner(libraries)
-    }
+    val libraries = defFiles.parallelStream().map {
+        it to buildNativeLibrary(
+                tool,
+                DefFile(it, tool.substitutions),
+                cinteropArguments,
+                ImportsImpl(emptyMap())
+        ).getHeaderPaths()
+    }.toList().toMap()
+    return SingleTargetDependencyAssigner(libraries)
 }
 
 private fun defFileDependencies(dependencyAssigner: DependencyAssigner) {
