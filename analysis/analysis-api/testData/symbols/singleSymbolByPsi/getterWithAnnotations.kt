3c43416042699fb48b3e59bb32108b43450c07b0
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2022-10-17 15:28:02 +0200

    [AA] add more tests on property accessors
    ^KT-54311

---
+++ analysis/analysis-api/testData/symbols/singleSymbolByPsi/getterWithAnnotations.kt
@@ -0,0 +1,21 @@
+annotation class PropertyAnnotation
+annotation class FieldAnnotation
+annotation class GetAnnotation
+annotation class SetAnnotation
+annotation class SetparamAnnotation
+annotation class ExplicitGetAnnotation
+annotation class ExplicitSetAnnotation
+annotation class ExplicitSetparamAnnotation
+
+@property:PropertyAnnotation
+@field:FieldAnnotation
+@get:GetAnnotation
+@set:SetAnnotation
+@setparam:SetparamAnnotation
+var x: Int = 0
+    @ExplicitGetAnnotation
+    ge<caret>t() = field
+    @ExplicitSetAnnotation
+    set(@ExplicitSetparamAnnotation value) {
+        field = value
+    }
---
+++ analysis/analysis-api/testData/symbols/singleSymbolByPsi/setterWithAnnotations.kt
@@ -0,0 +1,21 @@
+annotation class PropertyAnnotation
+annotation class FieldAnnotation
+annotation class GetAnnotation
+annotation class SetAnnotation
+annotation class SetparamAnnotation
+annotation class ExplicitGetAnnotation
+annotation class ExplicitSetAnnotation
+annotation class ExplicitSetparamAnnotation
+
+@property:PropertyAnnotation
+@field:FieldAnnotation
+@get:GetAnnotation
+@set:SetAnnotation
+@setparam:SetparamAnnotation
+var x: Int = 0
+    @ExplicitGetAnnotation
+    get() = field
+    @ExplicitSetAnnotation
+    s<caret>et(@ExplicitSetparamAnnotation value) {
+        field = value
+    }
