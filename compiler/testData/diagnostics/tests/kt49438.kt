e6ce0084157c9a4ce89fc08d8d3ba46eec629bfe
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-10-19 13:58:50 +0300

    [FIR] Ignore leaked internal types because of KT-54568

--- compiler/testData/diagnostics/tests/kt49438.kt
+++ compiler/testData/diagnostics/tests/kt49438.kt
@@ -1,2 +1,3 @@
+// IGNORE_LEAKED_INTERNAL_TYPES: KT-54568
 fun <K> foo(x: K) {}
 val x = foo<(<!UNRESOLVED_REFERENCE!>unresolved<!>) -> Float> { <!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>it<!>.<!DEBUG_INFO_MISSING_UNRESOLVED!>toFloat<!>() }
