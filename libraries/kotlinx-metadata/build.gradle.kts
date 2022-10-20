dd985d8209c0fcda5c8c70a237e9f5971de388d5
Leonid Startsev <leonid.startsev@jetbrains.com>
2022-10-20 17:23:10 +0200

    Add Binary Compatibility Validator to kotlinx-metadata projects
    Initiate API dumps

--- libraries/kotlinx-metadata/build.gradle.kts
+++ libraries/kotlinx-metadata/build.gradle.kts
@@ -3,6 +3,7 @@ description = "Kotlin metadata manipulation library"
 plugins {
     kotlin("jvm")
     id("jps-compatible")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.11.0"
 }
 
 sourceSets {
