b04b76bc2c7b7311b4936f816f2803d97a51ddb5
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-10-20 18:46:48 +0200

    FIR: Allow to skip specifying type arguments for members from raw type
    ^KT-54666 Fixed
    ^KT-54526 Related

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/DiagnosisCompilerTestFE10TestdataTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/DiagnosisCompilerTestFE10TestdataTestGenerated.java
@@ -24176,6 +24176,12 @@ public class DiagnosisCompilerTestFE10TestdataTestGenerated extends AbstractDiag
                 }
 
                 @Test
+                @TestMetadata("noTypeArgumentsForRawScopedMembers.kt")
+                public void testNoTypeArgumentsForRawScopedMembers() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/platformTypes/rawTypes/noTypeArgumentsForRawScopedMembers.kt");
+                }
+
+                @Test
                 @TestMetadata("nonGenericRawMember.kt")
                 public void testNonGenericRawMember() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/platformTypes/rawTypes/nonGenericRawMember.kt");
--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/TypeArgumentMapping.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/TypeArgumentMapping.kt
@@ -5,11 +5,17 @@
 
 package org.jetbrains.kotlin.fir.resolve.calls
 
+import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclarationOrigin
 import org.jetbrains.kotlin.fir.declarations.FirTypeParameterRefsOwner
+import org.jetbrains.kotlin.fir.types.ConeTypeIntersector
 import org.jetbrains.kotlin.fir.types.FirTypeProjection
 import org.jetbrains.kotlin.fir.types.builder.buildPlaceholderProjection
+import org.jetbrains.kotlin.fir.types.builder.buildTypeProjectionWithVariance
+import org.jetbrains.kotlin.fir.types.isRaw
+import org.jetbrains.kotlin.fir.types.toFirResolvedTypeRef
+import org.jetbrains.kotlin.types.Variance
 
 sealed class TypeArgumentMapping {
     abstract operator fun get(typeParameterIndex: Int): FirTypeProjection
@@ -28,12 +34,16 @@ sealed class TypeArgumentMapping {
 internal object MapTypeArguments : ResolutionStage() {
     override suspend fun check(candidate: Candidate, callInfo: CallInfo, sink: CheckerSink, context: ResolutionContext) {
         val typeArguments = callInfo.typeArguments
+        val owner = candidate.symbol.fir as FirTypeParameterRefsOwner
+
         if (typeArguments.isEmpty()) {
-            candidate.typeArgumentMapping = TypeArgumentMapping.NoExplicitArguments
+            if (owner is FirCallableDeclaration && owner.dispatchReceiverType?.isRaw() == true) {
+                candidate.typeArgumentMapping = computeDefaultMappingForRawTypeMember(owner, context)
+            } else {
+                candidate.typeArgumentMapping = TypeArgumentMapping.NoExplicitArguments
+            }
             return
         }
-
-        val owner = candidate.symbol.fir as FirTypeParameterRefsOwner
 
         if (
             typeArguments.size == owner.typeParameters.size ||
@@ -45,6 +55,28 @@ internal object MapTypeArguments : ResolutionStage() {
             candidate.typeArgumentMapping = TypeArgumentMapping.Mapped(emptyList())
             sink.yieldDiagnostic(InapplicableCandidate)
         }
+    }
+
+    private fun computeDefaultMappingForRawTypeMember(
+        owner: FirTypeParameterRefsOwner,
+        context: ResolutionContext
+    ): TypeArgumentMapping.Mapped {
+        // There might be some minor inconsistencies where in K2, there might be a raw type, while in K1, there was a regular flexible type
+        // And in that case for K2 we would start a regular inference process leads to TYPE_INFERENCE_NO_INFORMATION_FOR_PARAMETER because raw scopes
+        // don't leave type variables there (see KT-54526)
+        // Also, it might be a separate feature of K2, because even in cases where both compilers had a raw type, it's convenient not to
+        // require explicit type arguments for the places where it doesn't make sense
+        // (See `generic1.foo(w)` call in testData/diagnostics/tests/platformTypes/rawTypes/noTypeArgumentsForRawScopedMembers.fir.kt)
+        val resultArguments = owner.typeParameters.map { typeParameterRef ->
+            buildTypeProjectionWithVariance {
+                typeRef =
+                    ConeTypeIntersector.intersectTypes(
+                        context.typeContext, typeParameterRef.symbol.resolvedBounds.map { it.type }
+                    ).toFirResolvedTypeRef()
+                variance = Variance.INVARIANT
+            }
+        }
+        return TypeArgumentMapping.Mapped(resultArguments)
     }
 }
 
