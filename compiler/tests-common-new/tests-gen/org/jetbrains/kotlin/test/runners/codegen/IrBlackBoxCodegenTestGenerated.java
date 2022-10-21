c0789b520704f9c9e4186e882ce49b4940f56d1a
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2022-10-21 16:08:56 +0200

    PSI/FIR->IR translators: return to 1.7.10 behavior for field references
    In 1.7.20 we used the nearest Java-based receiver for such field
    references in backend. Now we are using use-site receiver again,
    it can lead to accidental usage of derived class property backing field.
    
    This is effectively a revert of KT-49507 fix, see commits:
    - fa914f20
    - b0a6508d
    
    #KT-54393 Fixed
    #KT-49507 Planned
    #KT-52338 Planned

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -17802,6 +17802,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         }
 
         @Test
+        @TestMetadata("javaFieldAndKotlinPrivateProperty.kt")
+        public void testJavaFieldAndKotlinPrivateProperty() throws Exception {
+            runTest("compiler/testData/codegen/box/fieldRename/javaFieldAndKotlinPrivateProperty.kt");
+        }
+
+        @Test
         @TestMetadata("javaFieldAndKotlinProperty.kt")
         public void testJavaFieldAndKotlinProperty() throws Exception {
             runTest("compiler/testData/codegen/box/fieldRename/javaFieldAndKotlinProperty.kt");
