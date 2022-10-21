53f384007258b819f64c218c62ed9717ab9bb300
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-10-21 14:50:56 +0200

    Drop compileOnly dependency on :kotlin-gradle-compiler-types in compiler
    This dependency was only required for compiler options Gradle DSL
    generation, but was leaking into runtime not-accessible classes.
    
    ^KT-54602 Fixed

--- compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/GradleOption.kt
+++ compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/GradleOption.kt
@@ -20,13 +20,31 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KVisibility
 
 /**
+ * @param value should be one of [DefaultValue] constants
  * @param gradleInputType should be one of [GradleInputTypes] constants
  */
 @Retention(AnnotationRetention.RUNTIME)
 annotation class GradleOption(
-    val value: KClass<out DefaultValues>,
+    val value: Int,
     val gradleInputType: String
 )
+
+// Enum class here is not possible due to bug in K2 compiler:
+// https://youtrack.jetbrains.com/issue/KT-54079
+object DefaultValue {
+    const val BOOLEAN_FALSE_DEFAULT = 0
+    const val BOOLEAN_TRUE_DEFAULT = 1
+    const val STRING_NULL_DEFAULT = 2
+    const val EMPTY_STRING_LIST_DEFAULT = 3
+    const val LANGUAGE_VERSIONS = 4
+    const val API_VERSIONS = 5
+    const val JVM_TARGET_VERSIONS = 6
+    const val JS_ECMA_VERSIONS = 7
+    const val JS_MODULE_KINDS = 8
+    const val JS_SOURCE_MAP_CONTENT_MODES = 9
+    const val JS_MAIN = 10
+    const val JS_SOURCE_MAP_NAMES_POLICY = 11
+}
 
 // Enum class here is not possible due to bug in K2 compiler:
 // https://youtrack.jetbrains.com/issue/KT-54079
