0b8cfe7ab235b242fc524b9319c18157821c03e9
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-10-24 22:23:51 +0200

    [Analysis API] render extra debug information for nested symbols if renders for outers

--- analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/expressionTypeProvider/AbstractExpectedExpressionTypeTest.kt
+++ analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/expressionTypeProvider/AbstractExpectedExpressionTypeTest.kt
@@ -22,7 +22,7 @@ abstract class AbstractExpectedExpressionTypeTest : AbstractAnalysisApiSingleFil
         val actualExpectedTypeText: String? = executeOnPooledThreadInReadAction {
             analyseForTest(expressionAtCaret) {
                 val expectedType = expressionAtCaret.getExpectedType() ?: return@analyseForTest null
-                DebugSymbolRenderer.renderType(expectedType)
+                DebugSymbolRenderer().renderType(expectedType)
             }
         }
 
