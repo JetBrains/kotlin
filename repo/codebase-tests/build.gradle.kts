ea645daf527eecce9b8759a276c8efc4806f3fc4
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2022-10-25 00:07:42 +0200

    KTI-955. Add tests for CODEOWNERS file
    - Check list of possible owners
    - Check no unknown owners
    - Check no unowned files in repo
    - Check no unmatched patterns

--- repo/codebase-tests/build.gradle.kts
+++ repo/codebase-tests/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.12.7")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.7")
     testImplementation("com.fasterxml.woodstox:woodstox-core:6.2.4")
+
+    testImplementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
 }
 
 sourceSets {
