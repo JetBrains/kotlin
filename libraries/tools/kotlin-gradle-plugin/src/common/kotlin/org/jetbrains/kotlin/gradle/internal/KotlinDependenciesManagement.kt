f41a20dfdc64762de86541c4257bb48f124a071e
Yahor Berdnikau <yahor.bernikau@jetbrains.com>
2022-10-26 14:32:49 +0200

    More fine-grained substitution for stdlib-jdk7{,8} dependencies
    Before if one of the configurations has 'kotlin-stdlib:1.8+' dependency
    - all other configurations will also apply substitution. But this may
    break build/tools that want to use specific set of dependencies to run
    itself. For example, Android Lint still uses older stdlib versions in
    classpath.
    
    Now substituion only applies to configuration (if it is resolvable) and
    its resolvable children.
    
    ^KT-54653 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/KotlinDependenciesManagement.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/KotlinDependenciesManagement.kt
@@ -42,7 +42,7 @@ internal fun customizeKotlinDependencies(project: Project) {
     )
 
     if (propertiesProvider.stdlibJdkVariantsSubstitution) {
-        project.configurations.configureStdlibSubstitution(project.dependencies)
+        project.configurations.configureStdlibSubstitution()
     }
 
     excludeStdlibAndKotlinTestCommonFromPlatformCompilations(project)
