a5d56e18772febdd632872584c668fb233189a5c
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2022-10-28 12:33:22 +0200

    [FIR] DebugSymbolRenderer: fix typo
    ^KT-54417

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/symbols/DebugSymbolRenderer.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/symbols/DebugSymbolRenderer.kt
@@ -107,14 +107,14 @@ public class DebugSymbolRenderer(
     }
 
     context(KtAnalysisSession)
-    private fun PrettyPrinter.renderSymbolInternals(symbol: KtSymbol, customIgnoredPropertynames: List<String> = emptyList()) {
+    private fun PrettyPrinter.renderSymbolInternals(symbol: KtSymbol, customIgnoredPropertyNames: List<String> = emptyList()) {
         renderSymbolHeader(symbol)
         val apiClass = getSymbolApiClass(symbol)
         withIndent {
             apiClass.members
                 .asSequence()
                 .filterIsInstance<KProperty<*>>()
-                .filter { it.name !in ignoredPropertyNames && it.name !in customIgnoredPropertynames }
+                .filter { it.name !in ignoredPropertyNames && it.name !in customIgnoredPropertyNames }
                 .sortedBy { it.name }
                 .forEach { member ->
                     renderProperty(
