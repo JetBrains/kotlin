47041503bcbb1e405834d389e5177f3503e4b1fa
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-10-28 10:36:50 +0200

    [Analysis API] update testdata after renderer rework
    The new testdata seems to be more correct or just different
    
    ^KTIJ-23268

--- analysis/analysis-api/testData/components/typeInfoProvider/isDenotable/localTypes.descriptors.kt
+++
@@ -1,7 +0,0 @@
-fun test() {
-    class A
-    @Denotable("A") A()
-    @Denotable("kotlin.collections.List<A>") listOf(A())
-    @Nondenotable("`<no name provided>`") object {}
-    @Nondenotable("kotlin.collections.List<`<no name provided>`>") listOf(object {})
-}
