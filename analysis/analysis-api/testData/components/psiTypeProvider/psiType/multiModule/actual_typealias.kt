f39e37cd9fe35dbc71a982f7aad02943ff0775bf
Jinseong Jeon <jsjeon@google.com>
2022-10-28 23:10:49 -0700

    AA: introduce MPP tests for PsiTypeProvider

---
+++ analysis/analysis-api/testData/components/psiTypeProvider/psiType/multiModule/actual_typealias.kt
@@ -0,0 +1,16 @@
+// !LANGUAGE: +MultiPlatformProjects
+// MODULE: commonMain
+// FILE: foo.kt
+
+expect class Foo
+
+// MODULE: androidMain(commonMain)
+// FILE: foo.android.kt
+
+actual typealias Foo = Int
+
+// FILE: main.kt
+
+fun b<caret>ox(): Foo {
+    return 42
+}
