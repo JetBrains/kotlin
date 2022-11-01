4d41ac09df286f09c5e924ca3cb2063268e5e943
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-11-01 19:23:56 +0900

    [Analysis API] Treat inapplicable declarations safely (KTIJ-23458)
    Make 'getClassOrObjectSymbol()' and 'getNamedClassOrObjectSymbol()'
    return 'null' for inapplicable PSI declarations.

--- analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/containingDeclarationProvider/AbstractContainingDeclarationProviderByMemberScopeTest.kt
+++ analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/containingDeclarationProvider/AbstractContainingDeclarationProviderByMemberScopeTest.kt
@@ -24,7 +24,7 @@ abstract class AbstractContainingDeclarationProviderByMemberScopeTest : Abstract
         val declaration = testServices.expressionMarkerProvider.getElementOfTypeAtCaret<KtClassOrObject>(ktFile)
 
         val memberToContainingClass = analyseForTest(declaration) {
-            val symbol = declaration.getClassOrObjectSymbol()
+            val symbol = declaration.getClassOrObjectSymbol()!!
             renderScopeWithParentDeclarations(symbol.getMemberScope())
         }
 
