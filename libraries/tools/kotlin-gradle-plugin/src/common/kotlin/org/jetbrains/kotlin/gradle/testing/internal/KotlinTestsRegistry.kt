e784e18f48b54cb0a18eff0bdaccdd05535c3bdf
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2022-11-03 21:08:03 +0100

    [Gradle, MPP] Rerun test tasks whose failures were suppressed
    #KT-54506 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/testing/internal/KotlinTestsRegistry.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/testing/internal/KotlinTestsRegistry.kt
@@ -12,6 +12,7 @@ import org.gradle.api.plugins.JavaBasePlugin
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.testing.AbstractTestTask
 import org.gradle.language.base.plugins.LifecycleBasePlugin
+import org.jetbrains.kotlin.gradle.logging.kotlinInfo
 import org.jetbrains.kotlin.gradle.tasks.dependsOn
 import org.jetbrains.kotlin.gradle.tasks.locateOrRegisterTask
 import org.jetbrains.kotlin.gradle.tasks.registerTask
@@ -37,9 +38,20 @@ class KotlinTestsRegistry(val project: Project, val allTestsTaskName: String = "
     ) {
         project.tasks.named(LifecycleBasePlugin.CHECK_TASK_NAME).dependsOn(taskHolder)
         project.cleanAllTestTask.configure { it.dependsOn(cleanTaskName(taskHolder.name)) }
+        val testReportService = TestReportService.registerIfAbsent(project)
         aggregate.configure {
             it.dependsOn(taskHolder.name)
             it.registerTestTask(taskHolder.get())
+        }
+        taskHolder.configure { task ->
+            task.usesService(testReportService)
+            task.outputs.upToDateWhen {
+                val hasFailedPreviously = testReportService.get().hasTestTaskFailedPreviously(it.path)
+                if (hasFailedPreviously) {
+                    task.logger.kotlinInfo("Marking $it as not up-to-date because the task has failed previously")
+                }
+                !hasFailedPreviously
+            }
         }
 
         taskHolder.configure { task ->
@@ -67,7 +79,7 @@ class KotlinTestsRegistry(val project: Project, val allTestsTaskName: String = "
         if (existed != null) return existed
 
         val reportName = name
-
+        val testReportService = TestReportService.registerIfAbsent(project)
         val aggregate: TaskProvider<KotlinTestReport> = project.registerTask(name) { aggregate ->
             aggregate.description = description
             aggregate.group = JavaBasePlugin.VERIFICATION_GROUP
@@ -79,6 +91,8 @@ class KotlinTestsRegistry(val project: Project, val allTestsTaskName: String = "
             if (isIdeaActive) {
                 aggregate.extensions.extraProperties.set("idea.internal.test", true)
             }
+            aggregate.usesService(testReportService)
+            aggregate.testReportServiceProvider.value(testReportService).finalizeValueOnRead()
 
             project.gradle.taskGraph.whenReady { graph ->
                 aggregate.maybeOverrideReporting(graph)
