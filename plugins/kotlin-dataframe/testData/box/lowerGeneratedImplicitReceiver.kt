4e7e031abc40e88f745a9468a8c1c4f8020ce35f
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-11-04 14:19:12 +0200

    [KDF] fix inlining
    Moved from https://github.com/Kotlin/dataframe/commit/ec6663b4f67f79b198c73d7e932b611546bc3e2f

--- plugins/kotlin-dataframe/testData/box/lowerGeneratedImplicitReceiver.kt
+++ plugins/kotlin-dataframe/testData/box/lowerGeneratedImplicitReceiver.kt
@@ -4,14 +4,11 @@ import org.jetbrains.kotlinx.dataframe.*
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.annotations.*
 
-//@DataSchema
-//interface Cars {
-//    val year: Int
-//}
-
 interface Cars
 
-fun `Name is evaluated to age`(df: DataFrame<Cars>) {
+fun box(): String {
+    val df = dataFrameOf("a")(1).cast<Cars>()
     val df1 = df.add("age") { 2022 }
     val col = df1.age
+    return if (col[0] == 2022) "OK" else col[0].toString()
 }
