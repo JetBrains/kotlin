d39a0ee8dbeb499d7ef40f13faee985fc53769d4
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2022-11-04 17:03:11 +0100

    [AA K2] implement symbol pointers for value parameters
    ^KT-54311

---
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/pointers/KtFirValueParameterSymbolPointer.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.fir.symbols.pointers
+
+import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
+import org.jetbrains.kotlin.analysis.api.fir.KtFirAnalysisSession
+import org.jetbrains.kotlin.analysis.api.fir.utils.firSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.*
+import org.jetbrains.kotlin.analysis.api.symbols.pointers.KtSymbolPointer
+import org.jetbrains.kotlin.fir.declarations.FirFunction
+import org.jetbrains.kotlin.name.Name
+
+internal class KtFirValueParameterSymbolPointer(
+    private val ownerPointer: KtSymbolPointer<KtFunctionLikeSymbol>,
+    private val name: Name,
+    private val index: Int,
+) : KtSymbolPointer<KtValueParameterSymbol>() {
+    @Deprecated("Consider using org.jetbrains.kotlin.analysis.api.KtAnalysisSession.restoreSymbol")
+    override fun restoreSymbol(analysisSession: KtAnalysisSession): KtValueParameterSymbol? {
+        require(analysisSession is KtFirAnalysisSession)
+        val ownerSymbol = with(analysisSession) {
+            ownerPointer.restoreSymbol() ?: return null
+        }
+
+        val function = ownerSymbol.firSymbol.fir as? FirFunction ?: return null
+        val firValueParameterSymbol = function.valueParameters.getOrNull(index)?.takeIf { it.name == name } ?: return null
+        return analysisSession.firSymbolBuilder.variableLikeBuilder.buildValueParameterSymbol(firValueParameterSymbol.symbol)
+    }
+}
---
+++ analysis/analysis-api/testData/symbols/symbolByReference/setterValueParameter.kt
@@ -0,0 +1,6 @@
+// DO_NOT_CHECK_NON_PSI_SYMBOL_RESTORE
+
+var i: Int
+    set(value) {
+        valu<caret>e
+    }
