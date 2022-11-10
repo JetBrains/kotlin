c3144410b5ac2c38dc88861b1538cb0ecaf0db4f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-11-10 14:29:41 +0100

    [FIR] do not ignore nested type aliases in FirNestedClassifierScope

--- compiler/testData/diagnostics/tests/typealias/innerTypeAliasConstructor.fir.kt
+++ compiler/testData/diagnostics/tests/typealias/innerTypeAliasConstructor.fir.kt
@@ -10,14 +10,14 @@ class C<T> {
 
 // C<...>.P[<...>]() syntax doesn't work due to the way qualified expressions are resolved now.
 // This restriction can be removed later.
-val test0 = C<Int>.<!UNRESOLVED_REFERENCE!>P<!>(1, 1)
-val test1 = C<Int>.<!UNRESOLVED_REFERENCE!>P1<!><String>("", 1)
-val test2 = C<Int>.<!UNRESOLVED_REFERENCE!>P2<!><String>(1, "")
-val test3 = C<Int>.<!UNRESOLVED_REFERENCE!>P1<!>("", 1)
-val test4 = C<Int>.<!UNRESOLVED_REFERENCE!>P2<!>(1, "")
+val test0 = C<Int>.P(<!ARGUMENT_TYPE_MISMATCH!>1<!>, <!ARGUMENT_TYPE_MISMATCH!>1<!>)
+val test1 = C<Int>.P1<String>("", <!ARGUMENT_TYPE_MISMATCH!>1<!>)
+val test2 = C<Int>.P2<String>(<!ARGUMENT_TYPE_MISMATCH!>1<!>, "")
+val test3 = C<Int>.P1("", <!ARGUMENT_TYPE_MISMATCH!>1<!>)
+val test4 = C<Int>.P2(<!ARGUMENT_TYPE_MISMATCH!>1<!>, "")
 
 // C.P() syntax could work if we add captured type parameters as type variables in a constraint system for corresponding call.
 // However, this should be consistent with inner classes capturing type parameters.
-val test5 = C.<!UNRESOLVED_REFERENCE!>P<!>(1, 1)
-val test6 = C.<!UNRESOLVED_REFERENCE!>P1<!>("", 1)
-val test7 = C.<!UNRESOLVED_REFERENCE!>P2<!>(1, "")
+val test5 = C.P(<!ARGUMENT_TYPE_MISMATCH!>1<!>, <!ARGUMENT_TYPE_MISMATCH!>1<!>)
+val test6 = C.P1("", <!ARGUMENT_TYPE_MISMATCH!>1<!>)
+val test7 = C.P2(<!ARGUMENT_TYPE_MISMATCH!>1<!>, "")
