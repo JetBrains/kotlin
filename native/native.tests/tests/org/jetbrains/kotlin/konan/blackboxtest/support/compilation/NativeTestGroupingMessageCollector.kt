00da8c7d4e9b6006b2cfbcb6d3db1e08b6059933
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2022-11-11 15:02:48 +0100

    [Native][tests] Update test warnings filter to reflect changes in p.l. warnings

--- native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/support/compilation/NativeTestGroupingMessageCollector.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/support/compilation/NativeTestGroupingMessageCollector.kt
@@ -112,7 +112,7 @@ internal class NativeTestGroupingMessageCollector(
     }
 
     private fun isPartialLinkageWarning(message: String): Boolean =
-        partialLinkageEnabled && message.matches(PARTIAL_LINKAGE_WARNING_REGEX)
+        partialLinkageEnabled && PARTIAL_LINKAGE_WARNING_REGEXES.any(message::matches)
 
 
     private fun isK2Experimental(message: String): Boolean = message.startsWith(K2_NATIVE_EXPERIMENTAL_WARNING_PREFIX)
@@ -124,7 +124,14 @@ internal class NativeTestGroupingMessageCollector(
         private const val UNSAFE_COMPILER_ARGS_WARNING_PREFIX = "ATTENTION!\nThis build uses unsafe internal compiler arguments:\n\n"
         private const val LIBRARY_INCLUDED_MORE_THAN_ONCE_WARNING_PREFIX = "library included more than once: "
         private const val K2_NATIVE_EXPERIMENTAL_WARNING_PREFIX = "Language version 2.0 is experimental"
-        private val PARTIAL_LINKAGE_WARNING_REGEX = Regex(".+ uses unlinked symbols(:.*)?")
+
+        private val PARTIAL_LINKAGE_WARNING_REGEXES = listOf(
+            Regex(".*No .+ found for symbol .+"),
+            Regex(".+ uses unlinked .+ symbol .+"),
+            Regex(".+ lacks enclosing class$"),
+            Regex("^Abstract .+ is not implemented in non-abstract .+"),
+            Regex(".+ is .+ while .+ is expected$")
+        )
 
         private fun parseLanguageFeatureArg(arg: String): String? =
             substringAfter(arg, "-XXLanguage:-") ?: substringAfter(arg, "-XXLanguage:+")
