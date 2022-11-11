475ff817ecc303406ee9e5edbc379bb500bdd6d3
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2022-11-11 15:53:47 +0200

    [KDF] support add with dsl
    Moved from https://github.com/Kotlin/dataframe/commit/9e658d3bfae47689b894948be9cf90dd632b8048

--- plugins/kotlin-dataframe/testData/diagnostics/injectAccessorsDsl.kt
+++ plugins/kotlin-dataframe/testData/diagnostics/injectAccessorsDsl.kt
@@ -5,6 +5,8 @@ import org.jetbrains.kotlinx.dataframe.annotations.*
 fun `Dsl is evaluated to `(df: DataFrame<*>) {
     val df1 = df.add {
         "col1" from { 5 }
+        expr { 5 } into "col2"
     }
     df1.col1
+    df1.col2
 }
