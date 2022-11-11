fcba1f215a05c6f332ba91ab12d8b10f47a154ea
Jinseong Jeon <jsjeon@google.com>
2022-11-10 22:50:08 -0800

    AA: handle erroneous type in nested type argument

--- core/descriptors/src/org/jetbrains/kotlin/types/checker/ClassicTypeSystemContext.kt
+++ core/descriptors/src/org/jetbrains/kotlin/types/checker/ClassicTypeSystemContext.kt
@@ -362,7 +362,7 @@ interface ClassicTypeSystemContext : TypeSystemInferenceExtensionContext, TypeSy
      *  - type for type parameter
      *  - captured type
      *
-     * Such types can contains error types in our arguments, but type constructor isn't errorTypeConstructor
+     * Such types can contain error types in our arguments, but type constructor isn't errorTypeConstructor
      */
     override fun SimpleTypeMarker.isSingleClassifierType(): Boolean {
         require(this is SimpleType, this::errorMessage)
