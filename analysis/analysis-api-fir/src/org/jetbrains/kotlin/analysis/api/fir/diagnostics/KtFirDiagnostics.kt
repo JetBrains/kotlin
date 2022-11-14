e1e57773cfc34c094c481d14d0df7bd18e0e4726
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-11-14 15:35:07 +0200

    [FIR JS] Support DEBUG_INFO_DYNAMIC

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnostics.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KtFirDiagnostics.kt
@@ -3168,6 +3168,10 @@ sealed class KtFirDiagnostic<PSI : PsiElement> : KtDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = JavaSamInterfaceConstructorReference::class
     }
 
+    abstract class Dynamic : KtFirDiagnostic<KtElement>() {
+        override val diagnosticClass get() = Dynamic::class
+    }
+
     abstract class Syntax : KtFirDiagnostic<PsiElement>() {
         override val diagnosticClass get() = Syntax::class
     }
