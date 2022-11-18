1e85f2f24f22319e4276e6854d01e5df8fb03eb1
Steven Schäfer <stevenschaefer@google.com>
2022-11-18 15:45:19 +0100

    Minor: Fold constants in ComposableFunctionBodyTransformer
    FIR evaluates constants after IrGenerationExtensions, so we should
    not produce code with explicit calls to `.inv()` on constants.
    
    Test: ./gradlew :compose:compiler:c-h:i-t:tDUT
    Change-Id: I6315cf9cd109d0b82d8dbc419a62e29372c2536e ( https://android-review.git.corp.google.com/q/I6315cf9cd109d0b82d8dbc419a62e29372c2536e )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
@@ -4544,7 +4544,7 @@ class ComposableFunctionBodyTransformer(
                 temp,
                 irAnd(
                     irGet(temp),
-                    irInv(irConst(ParamState.Mask.bitsForSlot(slot)))
+                    irConst(ParamState.Mask.bitsForSlot(slot).inv())
                 )
             )
         }
