dbc0e34d8c83da638847ed5d0bacac2e5c096970
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2022-11-21 16:01:04 +0100

    [PL] Remove p.l. declarations when K/N static caches are enabled

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/unlinked/PartialLinkageUtils.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/unlinked/PartialLinkageUtils.kt
@@ -5,14 +5,8 @@
 
 package org.jetbrains.kotlin.backend.common.serialization.unlinked
 
-import org.jetbrains.kotlin.ir.IrStatement
-import org.jetbrains.kotlin.ir.declarations.IrAnonymousInitializer
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.expressions.IrCall
-import org.jetbrains.kotlin.ir.expressions.IrComposite
 import org.jetbrains.kotlin.ir.util.IdSignature
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
 internal object PartialLinkageUtils {
     val UNKNOWN_NAME = Name.identifier("<unknown name>")
@@ -27,16 +21,5 @@ internal object PartialLinkageUtils {
         is IdSignature.AccessorSignature -> accessorSignature.guessName(nameSegmentsToPickUp)
 
         else -> null
-    }
-}
-
-fun IrClass.isPartiallyLinked(): Boolean = origin is PartiallyLinkedDeclarationOrigin
-        || declarations.firstIsInstanceOrNull<IrAnonymousInitializer>()?.body?.statements?.any(IrStatement::isPartialLinkageRuntimeError) == true
-
-fun IrStatement.isPartialLinkageRuntimeError(): Boolean {
-    return PartiallyLinkedStatementOrigin.PARTIAL_LINKAGE_RUNTIME_ERROR == when (this) {
-        is IrCall -> origin
-        is IrComposite -> origin
-        else -> return false
     }
 }
