64fb66a3d65111865b8a2f055d603529e05c924a
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2022-11-23 18:28:42 +0100

    [PL][tests] Unmute & restore function visibility tests

--- compiler/testData/klibABI/changeFunctionVisibility/lib2/l2.kt
+++ compiler/testData/klibABI/changeFunctionVisibility/lib2/l2.kt
@@ -2,25 +2,30 @@ class ContainerImpl : Container() {
     // Just to check that accessing from within the class hierarchy has the same effect as accessing from the outside:
     fun publicToProtectedFunctionAccess() = publicToProtectedFunction()
     fun publicToInternalFunctionAccess() = publicToInternalFunction()
+    fun publicToInternalPAFunctionAccess() = publicToInternalPAFunction()
     fun publicToPrivateFunctionAccess() = publicToPrivateFunction()
 
     // As far as protected members can't be accessed outside of the class hierarchy, we need special accessors.
     fun protectedToPublicFunctionAccess() = protectedToPublicFunction()
     fun protectedToInternalFunctionAccess() = protectedToInternalFunction()
+    fun protectedToInternalPAFunctionAccess() = protectedToInternalPAFunction()
     fun protectedToPrivateFunctionAccess() = protectedToPrivateFunction()
 
     // Overridden functions with changed visibility:
     override fun publicToProtectedOverriddenFunction() = "ContainerImpl.publicToProtectedOverriddenFunction"
     override fun publicToInternalOverriddenFunction() = "ContainerImpl.publicToInternalOverriddenFunction"
+    override fun publicToInternalPAOverriddenFunction() = "ContainerImpl.publicToInternalPAOverriddenFunction"
     override fun publicToPrivateOverriddenFunction() = "ContainerImpl.publicToPrivateOverriddenFunction"
 
     override fun protectedToPublicOverriddenFunction() = "ContainerImpl.protectedToPublicOverriddenFunction"
     override fun protectedToInternalOverriddenFunction() = "ContainerImpl.protectedToInternalOverriddenFunction"
+    override fun protectedToInternalPAOverriddenFunction() = "ContainerImpl.protectedToInternalPAOverriddenFunction"
     override fun protectedToPrivateOverriddenFunction() = "ContainerImpl.protectedToPrivateOverriddenFunction"
 
     // As far as protected members can't be accessed outside of the class hierarchy, we need special accessors.
     fun protectedToPublicOverriddenFunctionAccess() = protectedToPublicOverriddenFunction()
     fun protectedToInternalOverriddenFunctionAccess() = protectedToInternalOverriddenFunction()
+    fun protectedToInternalPAOverriddenFunctionAccess() = protectedToInternalPAOverriddenFunction()
     fun protectedToPrivateOverriddenFunctionAccess() = protectedToPrivateOverriddenFunction()
 
     // Functions that accedentally start to override/conflict with functions added to Container since version v2:
@@ -30,6 +35,8 @@ class ContainerImpl : Container() {
     protected fun newOpenProtectedFunction() = "ContainerImpl.newOpenProtectedFunction"
     internal fun newInternalFunction() = "ContainerImpl.newInternalFunction"
     internal fun newOpenInternalFunction() = "ContainerImpl.newOpenInternalFunction"
+    @PublishedApi internal fun newInternalPAFunction() = "ContainerImpl.newInternalPAFunction"
+    @PublishedApi internal fun newOpenInternalPAFunction() = "ContainerImpl.newOpenInternalPAFunction"
     private fun newPrivateFunction() = "ContainerImpl.newPrivateFunction"
 
     // As far as protected/private members can't be accessed outside of the class hierarchy, and internal can't be accessed
@@ -38,5 +45,7 @@ class ContainerImpl : Container() {
     fun newOpenProtectedFunctionAccess() = newOpenProtectedFunction()
     fun newInternalFunctionAccess() = newInternalFunction()
     fun newOpenInternalFunctionAccess() = newOpenInternalFunction()
+    fun newInternalPAFunctionAccess() = newInternalPAFunction()
+    fun newOpenInternalPAFunctionAccess() = newOpenInternalPAFunction()
     fun newPrivateFunctionAccess() = newPrivateFunction()
 }
--- compiler/testData/klibABI/changeFunctionVisibility/main/m.kt
+++ compiler/testData/klibABI/changeFunctionVisibility/main/m.kt
@@ -1,33 +1,65 @@
 import abitestutils.abiTest
+import abitestutils.TestBuilder
+import abitestutils.TestMode.NATIVE_CACHE_STATIC_EVERYWHERE
 
 fun box() = abiTest {
     val c = Container()
     val ci = ContainerImpl()
-    expectSuccess("publicToInternalTopLevelFunction.v2") { publicToInternalTopLevelFunction() }
-    expectFailure(prefixed("function publicToPrivateTopLevelFunction can not be called")) { publicToPrivateTopLevelFunction() }
-    expectSuccess("Container.publicToProtectedFunction.v2") { c.publicToProtectedFunction() }
-    expectSuccess("Container.publicToProtectedFunction.v2") { ci.publicToProtectedFunction() }
-    expectFailure(prefixed("function publicToInternalFunction can not be called")) { c.publicToInternalFunction() }
-    expectFailure(prefixed("function publicToInternalFunction can not be called")) { ci.publicToInternalFunction() }
-    expectFailure(prefixed("function publicToPrivateFunction can not be called")) { c.publicToPrivateFunction() }
-    expectFailure(prefixed("function publicToPrivateFunction can not be called")) { ci.publicToPrivateFunction() }
-    expectSuccess("Container.publicToProtectedFunction.v2") { ci.publicToProtectedFunctionAccess() }
-    expectFailure(prefixed("function publicToInternalFunction can not be called")) { ci.publicToInternalFunctionAccess() }
-    expectFailure(prefixed("function publicToPrivateFunction can not be called")) { ci.publicToPrivateFunctionAccess() }
-    expectSuccess("Container.protectedToPublicFunction.v2") { ci.protectedToPublicFunctionAccess() }
-    expectFailure(prefixed("function protectedToInternalFunction can not be called")) { ci.protectedToInternalFunctionAccess() }
-    expectFailure(prefixed("function protectedToPrivateFunction can not be called")) { ci.protectedToPrivateFunctionAccess() }
-    expectSuccess("ContainerImpl.publicToProtectedOverriddenFunction") { ci.publicToProtectedOverriddenFunction() }
-    expectSuccess("ContainerImpl.publicToInternalOverriddenFunction") { ci.publicToInternalOverriddenFunction() }
-    expectSuccess("ContainerImpl.publicToPrivateOverriddenFunction") { ci.publicToPrivateOverriddenFunction() }
-    expectSuccess("ContainerImpl.protectedToPublicOverriddenFunction") { ci.protectedToPublicOverriddenFunctionAccess() }
-    expectSuccess("ContainerImpl.protectedToInternalOverriddenFunction") { ci.protectedToInternalOverriddenFunctionAccess() }
-    expectSuccess("ContainerImpl.protectedToPrivateOverriddenFunction") { ci.protectedToPrivateOverriddenFunctionAccess() }
-    expectSuccess("ContainerImpl.newPublicFunction") { ci.newPublicFunction() }
-    expectSuccess("ContainerImpl.newOpenPublicFunction") { ci.newOpenPublicFunction() }
-    expectSuccess("ContainerImpl.newProtectedFunction") { ci.newProtectedFunctionAccess() }
-    expectSuccess("ContainerImpl.newOpenProtectedFunction") { ci.newOpenProtectedFunctionAccess() }
-    expectSuccess("ContainerImpl.newInternalFunction") { ci.newInternalFunctionAccess() }
-    expectSuccess("ContainerImpl.newOpenInternalFunction") { ci.newOpenInternalFunctionAccess() }
-    expectSuccess("ContainerImpl.newPrivateFunction") { ci.newPrivateFunctionAccess() }
+
+    success("publicToInternalTopLevelFunction.v2") { publicToInternalTopLevelFunction() } // Signature remains the same.
+    success("publicToInternalPATopLevelFunction.v2") { publicToInternalPATopLevelFunction() } // Signature remains the same.
+    unlinkedSymbol("/publicToPrivateTopLevelFunction") { publicToPrivateTopLevelFunction() } // Signature changed.
+
+    success("Container.publicToProtectedFunction.v2") { c.publicToProtectedFunction() } // Signature remains the same.
+    success("Container.publicToProtectedFunction.v2") { ci.publicToProtectedFunction() } // Signature remains the same.
+    // TODO: KT-54469, Container.publicToInternalFunction() should fail because it is accessed from another module.
+    success("Container.publicToInternalFunction.v2") { c.publicToInternalFunction() } // Signature remains the same.
+    unlinkedSymbol("/ContainerImpl.publicToInternalFunction") { ci.publicToInternalFunction() } // FOs are not generated for internal members from other module.
+    // TODO: KT-54469, Container.publicToInternalPAFunction() should fail because it is accessed from another module.
+    success("Container.publicToInternalPAFunction.v2") { c.publicToInternalPAFunction() } // Signature remains the same.
+    unlinkedSymbol("/ContainerImpl.publicToInternalPAFunction") { ci.publicToInternalPAFunction() } // FOs are not generated for internal members from other module.
+    inaccessible("publicToPrivateFunction") { c.publicToPrivateFunction() } // Inaccessible from other module though signature remains the same.
+    unlinkedSymbol("/ContainerImpl.publicToPrivateFunction") { ci.publicToPrivateFunction() } // FOs are not generated for private members.
+
+    success("Container.publicToProtectedFunction.v2") { ci.publicToProtectedFunctionAccess() } // Signature remains the same.
+    unlinkedSymbol("/ContainerImpl.publicToInternalFunction") { ci.publicToInternalFunctionAccess() } // FOs are not generated for internal members from other module.
+    unlinkedSymbol("/ContainerImpl.publicToInternalPAFunction") { ci.publicToInternalPAFunctionAccess() } // FOs are not generated for internal members from other module.
+    unlinkedSymbol("/ContainerImpl.publicToPrivateFunction") { ci.publicToPrivateFunctionAccess() } // FOs are not generated for private members.
+    success("Container.protectedToPublicFunction.v2") { ci.protectedToPublicFunctionAccess() } // Signature remains the same.
+    unlinkedSymbol("/ContainerImpl.protectedToInternalFunction") { ci.protectedToInternalFunctionAccess() } // FOs are not generated for internal members from other module.
+    unlinkedSymbol("/ContainerImpl.protectedToInternalPAFunction") { ci.protectedToInternalPAFunctionAccess() } // FOs are not generated for internal members from other module.
+    unlinkedSymbol("/ContainerImpl.protectedToPrivateFunction") { ci.protectedToPrivateFunctionAccess() } // FOs are not generated for private members.
+
+    success("ContainerImpl.publicToProtectedOverriddenFunction") { ci.publicToProtectedOverriddenFunction() }
+    success("ContainerImpl.publicToInternalOverriddenFunction") { ci.publicToInternalOverriddenFunction() }
+    success("ContainerImpl.publicToInternalPAOverriddenFunction") { ci.publicToInternalPAOverriddenFunction() }
+    success("ContainerImpl.publicToPrivateOverriddenFunction") { ci.publicToPrivateOverriddenFunction() }
+    success("ContainerImpl.protectedToPublicOverriddenFunction") { ci.protectedToPublicOverriddenFunctionAccess() }
+    success("ContainerImpl.protectedToInternalOverriddenFunction") { ci.protectedToInternalOverriddenFunctionAccess() }
+    success("ContainerImpl.protectedToInternalPAOverriddenFunction") { ci.protectedToInternalPAOverriddenFunctionAccess() }
+    success("ContainerImpl.protectedToPrivateOverriddenFunction") { ci.protectedToPrivateOverriddenFunctionAccess() }
+
+    success("ContainerImpl.newPublicFunction") { ci.newPublicFunction() }
+    success("ContainerImpl.newOpenPublicFunction") { ci.newOpenPublicFunction() }
+    success("ContainerImpl.newProtectedFunction") { ci.newProtectedFunctionAccess() }
+    success("ContainerImpl.newOpenProtectedFunction") { ci.newOpenProtectedFunctionAccess() }
+    success("ContainerImpl.newInternalFunction") { ci.newInternalFunctionAccess() }
+    success("ContainerImpl.newOpenInternalFunction") { ci.newOpenInternalFunctionAccess() }
+    success("ContainerImpl.newInternalPAFunction") { ci.newInternalPAFunctionAccess() }
+    success("ContainerImpl.newOpenInternalPAFunction") { ci.newOpenInternalPAFunctionAccess() }
+    success("ContainerImpl.newPrivateFunction") { ci.newPrivateFunctionAccess() }
 }
+
+// Shortcuts:
+private inline fun TestBuilder.success(expectedOutcome: String, noinline block: () -> String) =
+    expectSuccess(expectedOutcome, block)
+
+private inline fun TestBuilder.unlinkedSymbol(signature: String, noinline block: () -> Unit) {
+    val functionName = signature.removePrefix("/").substringAfterLast(".")
+    expectFailure(skipHashes("Function $functionName can not be called: No function found for symbol $signature"), block)
+}
+
+private inline fun TestBuilder.inaccessible(functionName: String, noinline block: () -> Unit) = expectFailure(
+    skipHashes("Function $functionName can not be called: Private function $functionName declared in module <lib1> can not be accessed from module <main>"),
+    block
+)
