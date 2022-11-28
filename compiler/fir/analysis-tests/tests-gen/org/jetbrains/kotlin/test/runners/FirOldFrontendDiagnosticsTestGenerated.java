bf06033303cdfae5fa307cbfcea65318ba9187c7
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-11-28 18:02:19 +0100

    K2: Resolve non-exhaustive when expressions independently
    ^KT-55169 Fixed
    ^KT-55175 Related

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
@@ -34811,6 +34811,12 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirDiagnosti
             }
 
             @Test
+            @TestMetadata("nonExhaustiveDependentContext.kt")
+            public void testNonExhaustiveDependentContext() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/when/nonExhaustiveDependentContext.kt");
+            }
+
+            @Test
             @TestMetadata("NonExhaustivePlatformEnum.kt")
             public void testNonExhaustivePlatformEnum() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/when/NonExhaustivePlatformEnum.kt");
