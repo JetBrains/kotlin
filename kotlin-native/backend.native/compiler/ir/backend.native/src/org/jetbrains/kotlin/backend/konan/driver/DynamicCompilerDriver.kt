72f6d18d6965c236afbb4d943704c6d78c49a08a
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2022-11-29 16:23:03 +0200

    [K/N] Support CompilerOutputKind.LIBRARY in the dynamic driver

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/DynamicCompilerDriver.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/DynamicCompilerDriver.kt
@@ -5,9 +5,12 @@
 
 package org.jetbrains.kotlin.backend.konan.driver
 
+import kotlinx.cinterop.usingJvmCInteropCallbacks
 import org.jetbrains.kotlin.backend.konan.KonanConfig
+import org.jetbrains.kotlin.backend.konan.driver.phases.*
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.konan.target.CompilerOutputKind
+import org.jetbrains.kotlin.konan.util.usingNativeMemoryAllocator
 
 /**
  * Dynamic driver does not "know" upfront which phases will be executed.
@@ -20,15 +23,43 @@ internal class DynamicCompilerDriver : CompilerDriver() {
                 false
     }
 
-    override fun run(config: KonanConfig, environment: KotlinCoreEnvironment) = when (config.produce) {
-        CompilerOutputKind.PROGRAM -> error("Dynamic compiler driver does not support `program` output yet.")
-        CompilerOutputKind.DYNAMIC -> error("Dynamic compiler driver does not support `dynamic` output yet.")
-        CompilerOutputKind.STATIC -> error("Dynamic compiler driver does not support `static` output yet.")
-        CompilerOutputKind.FRAMEWORK -> error("Dynamic compiler driver does not support `framework` output yet.")
-        CompilerOutputKind.LIBRARY -> error("Dynamic compiler driver does not support `library` output yet.")
-        CompilerOutputKind.BITCODE -> error("Dynamic compiler driver does not support `bitcode` output yet.")
-        CompilerOutputKind.DYNAMIC_CACHE -> error("Dynamic compiler driver does not support `dynamic_cache` output yet.")
-        CompilerOutputKind.STATIC_CACHE -> error("Dynamic compiler driver does not support `static_cache` output yet.")
-        CompilerOutputKind.PRELIMINARY_CACHE -> TODO()
+    override fun run(config: KonanConfig, environment: KotlinCoreEnvironment) {
+        usingNativeMemoryAllocator {
+            usingJvmCInteropCallbacks {
+                PhaseEngine.startTopLevel(config) { engine ->
+                    when (config.produce) {
+                        CompilerOutputKind.PROGRAM -> error("Dynamic compiler driver does not support `program` output yet.")
+                        CompilerOutputKind.DYNAMIC -> error("Dynamic compiler driver does not support `dynamic` output yet.")
+                        CompilerOutputKind.STATIC -> error("Dynamic compiler driver does not support `static` output yet.")
+                        CompilerOutputKind.FRAMEWORK -> error("Dynamic compiler driver does not support `framework` output yet.")
+                        CompilerOutputKind.LIBRARY -> produceKlib(engine, config, environment)
+                        CompilerOutputKind.BITCODE -> error("Dynamic compiler driver does not support `bitcode` output yet.")
+                        CompilerOutputKind.DYNAMIC_CACHE -> error("Dynamic compiler driver does not support `dynamic_cache` output yet.")
+                        CompilerOutputKind.STATIC_CACHE -> error("Dynamic compiler driver does not support `static_cache` output yet.")
+                        CompilerOutputKind.PRELIMINARY_CACHE -> TODO()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun produceKlib(engine: PhaseEngine<PhaseContext>, config: KonanConfig, environment: KotlinCoreEnvironment) {
+        val frontendOutput = engine.useContext(FrontendContextImpl(config)) { it.runFrontend(environment) }
+        if (frontendOutput is FrontendPhaseOutput.ShouldNotGenerateCode) {
+            return
+        }
+        require(frontendOutput is FrontendPhaseOutput.Full)
+        val psiToIrOutput = if (config.metadataKlib) {
+            null
+        } else {
+            val psiToIrContext = PsiToIrContextImpl(config, frontendOutput.moduleDescriptor, frontendOutput.bindingContext)
+            val psiToIrOutput = engine.useContext(psiToIrContext) { psiToIrEngine ->
+                val output = psiToIrEngine.runPsiToIr(frontendOutput, isProducingLibrary = true)
+                output
+            }
+            psiToIrOutput
+        }
+        val serializerOutput = engine.runSerializer(frontendOutput.moduleDescriptor, psiToIrOutput)
+        engine.writeKlib(serializerOutput)
     }
 }
---
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/Serializer.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.konan.driver.phases
+
+import org.jetbrains.kotlin.backend.common.serialization.CompatibilityMode
+import org.jetbrains.kotlin.backend.common.serialization.metadata.KlibMetadataMonolithicSerializer
+import org.jetbrains.kotlin.backend.konan.driver.PhaseContext
+import org.jetbrains.kotlin.backend.konan.driver.PhaseEngine
+import org.jetbrains.kotlin.backend.konan.serialization.KonanIrModuleSerializer
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
+import org.jetbrains.kotlin.config.languageVersionSettings
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.ir.util.IrMessageLogger
+import org.jetbrains.kotlin.konan.library.KonanLibrary
+import org.jetbrains.kotlin.library.SerializedIrModule
+import org.jetbrains.kotlin.library.SerializedMetadata
+
+internal data class SerializerInput(
+        val moduleDescriptor: ModuleDescriptor,
+        val psiToIrResult: PsiToIrOutput?,
+)
+
+data class SerializerOutput(
+        val serializedMetadata: SerializedMetadata?,
+        val serializedIr: SerializedIrModule?,
+        val dataFlowGraph: ByteArray?,
+        val neededLibraries: List<KonanLibrary>
+)
+
+internal val SerializerPhase = createSimpleNamedCompilerPhase<PhaseContext, SerializerInput, SerializerOutput>(
+        "Serializer", "IR serialzer",
+        outputIfNotEnabled = { _, _, _, _ -> SerializerOutput(null, null, null, emptyList()) }
+) { context: PhaseContext, input: SerializerInput ->
+    val config = context.config
+    val expectActualLinker = config.configuration.get(CommonConfigurationKeys.EXPECT_ACTUAL_LINKER) ?: false
+    val messageLogger = config.configuration.get(IrMessageLogger.IR_MESSAGE_LOGGER) ?: IrMessageLogger.None
+    val relativePathBase = config.configuration.get(CommonConfigurationKeys.KLIB_RELATIVE_PATH_BASES) ?: emptyList()
+    val normalizeAbsolutePaths = config.configuration.get(CommonConfigurationKeys.KLIB_NORMALIZE_ABSOLUTE_PATH) ?: false
+
+    val serializedIr = if (input.psiToIrResult != null) {
+        val ir = input.psiToIrResult.irModule
+        KonanIrModuleSerializer(
+                messageLogger, ir.irBuiltins, input.psiToIrResult.expectDescriptorToSymbol,
+                skipExpects = !expectActualLinker,
+                compatibilityMode = CompatibilityMode.CURRENT,
+                normalizeAbsolutePaths = normalizeAbsolutePaths,
+                sourceBaseDirs = relativePathBase,
+        ).serializedIrModule(ir)
+    } else null
+
+    val serializer = KlibMetadataMonolithicSerializer(
+            config.configuration.languageVersionSettings,
+            config.configuration.get(CommonConfigurationKeys.METADATA_VERSION)!!,
+            config.project,
+            exportKDoc = context.shouldExportKDoc(),
+            !expectActualLinker, includeOnlyModuleContent = true)
+    val serializedMetadata = serializer.serializeModule(input.moduleDescriptor)
+    val neededLibraries = config.librariesWithDependencies(input.moduleDescriptor)
+    SerializerOutput(serializedMetadata, serializedIr, null, neededLibraries)
+}
+
+internal fun <T : PhaseContext> PhaseEngine<T>.runSerializer(
+        moduleDescriptor: ModuleDescriptor,
+        psiToIrResult: PsiToIrOutput?,
+): SerializerOutput {
+    val input = SerializerInput(moduleDescriptor, psiToIrResult)
+    return this.runPhase(SerializerPhase, input)
+}
