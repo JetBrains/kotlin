a0152a492ce3d8378d912f61f709f3f8faab54ab
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2022-11-29 16:01:07 +0100

    Fix logs for JPS
    Move fir-build.log files with gradle-fir-build.log because gradle build with FIR is correct, but not optimal. JPS with K2 is now tested with fir-build.log files
    
    #KT-54991 In Progress

--- jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/AbstractIncrementalK2LightTreeJvmJpsTest.kt
+++ jps/jps-plugin/jps-tests/test/org/jetbrains/kotlin/jps/build/AbstractIncrementalK2LightTreeJvmJpsTest.kt
@@ -26,5 +26,5 @@ abstract class AbstractIncrementalK2LightTreeJvmJpsTest(
     }
 
     override val buildLogFinder: BuildLogFinder
-        get() = BuildLogFinder(isJpsBuild = true/*, isFirEnabled = true*/) //TODO: add flags
+        get() = BuildLogFinder(isJpsBuild = true, isFirEnabled = true)
 }
