fca8585aff29f9ea15d01cf12579d10009777269
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-11-30 14:19:46 +0200

    [FIR] Don't report redundant INAPPLICABLE_CANDIDATE
    Consider these examples of `INAPPLICABLE_CANDIDATE`
    as redundant, because there are
    other kinds of errors reported already

--- compiler/testData/diagnostics/testsWithStdLib/inference/kt42620.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/inference/kt42620.fir.kt
@@ -6,10 +6,10 @@ fun main1() = when {
     else -> Foo::<!UNRESOLVED_REFERENCE!>plus<!>
 }
 
-fun main2() = <!INAPPLICABLE_CANDIDATE!>if (true) Foo::<!UNRESOLVED_REFERENCE!>minus<!> else Foo::<!UNRESOLVED_REFERENCE!>times<!><!>
+fun main2() = if (true) Foo::<!UNRESOLVED_REFERENCE!>minus<!> else Foo::<!UNRESOLVED_REFERENCE!>times<!>
 
-fun main3() = <!INAPPLICABLE_CANDIDATE!>if (true) { Foo::<!UNRESOLVED_REFERENCE!>minus<!> } else { Foo::<!UNRESOLVED_REFERENCE!>times<!> }<!>
+fun main3() = if (true) { Foo::<!UNRESOLVED_REFERENCE!>minus<!> } else { Foo::<!UNRESOLVED_REFERENCE!>times<!> }
 
-fun main4() = <!INAPPLICABLE_CANDIDATE!>try { Foo::<!UNRESOLVED_REFERENCE!>minus<!> } finally { Foo::<!UNRESOLVED_REFERENCE!>times<!> }<!>
+fun main4() = try { Foo::<!UNRESOLVED_REFERENCE!>minus<!> } finally { Foo::<!UNRESOLVED_REFERENCE!>times<!> }
 
-fun main5() = Foo::<!UNRESOLVED_REFERENCE!>minus<!> <!INAPPLICABLE_CANDIDATE!>?:<!> Foo::<!UNRESOLVED_REFERENCE!>times<!>
+fun main5() = Foo::<!UNRESOLVED_REFERENCE!>minus<!> ?: Foo::<!UNRESOLVED_REFERENCE!>times<!>
