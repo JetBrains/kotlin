836ec13fca5100ec7a7ee3ea65ee33fa23801baa
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-11-30 14:19:46 +0200

    [FIR] Stop reporting redundant errors
    Consider these examples of `INAPPLICABLE_CANDIDATE`
    as redundant, because there are
    other kinds of errors reported already

--- compiler/testData/diagnostics/tests/callableReference/generic/genericExtensionFunction.fir.kt
+++ compiler/testData/diagnostics/tests/callableReference/generic/genericExtensionFunction.fir.kt
@@ -22,8 +22,8 @@ fun test1() {
 fun <T> test2() {
     bar<Wrapper, Int, String>(Wrapper::fooReturnString).checkType { _<Tripple<Wrapper, Int, String>>() }
     bar<Wrapper, T, String>(Wrapper::fooReturnString).checkType { _<Tripple<Wrapper, T, String>>() }
-    <!INAPPLICABLE_CANDIDATE!>bar<!><Wrapper, T, T>(Wrapper::<!UNRESOLVED_REFERENCE!>fooReturnString<!>)
-    <!INAPPLICABLE_CANDIDATE!>bar<!><Wrapper, Int, Int>(Wrapper::<!UNRESOLVED_REFERENCE!>fooReturnString<!>)
+    bar<Wrapper, T, T>(Wrapper::<!UNRESOLVED_REFERENCE!>fooReturnString<!>)
+    bar<Wrapper, Int, Int>(Wrapper::<!UNRESOLVED_REFERENCE!>fooReturnString<!>)
 
     bar<Wrapper, Int, T>(Wrapper::fooTakeInt).checkType { _<Tripple<Wrapper, Int, T>>() }
     bar<Wrapper, Int, String>(Wrapper::fooTakeInt).checkType { _<Tripple<Wrapper, Int, String>>() }
