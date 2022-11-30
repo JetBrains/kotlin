0f533a8a2f080710b9978056145b494de78c5440
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2022-11-30 16:46:49 +0100

    [Gradle] kgp-idea: Implement isIdeaProjectLevel and KlibExtra
    KT-55189

---
+++ libraries/tools/kotlin-gradle-plugin-idea/src/main/kotlin/org/jetbrains/kotlin/gradle/idea/tcs/extras/isIdeaProjectLevel.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.idea.tcs.extras
+
+import org.jetbrains.kotlin.gradle.idea.tcs.IdeaKotlinBinaryDependency
+import org.jetbrains.kotlin.tooling.core.extrasKeyOf
+import org.jetbrains.kotlin.tooling.core.getValue
+import org.jetbrains.kotlin.tooling.core.setValue
+
+val isIdeaProjectLevelKey = extrasKeyOf<Boolean>("IdeaKotlinBinaryDependency.isIdeaProjectLevel")
+
+/**
+ * Marks any binary dependency as Global in the sense of "this library is considered
+ * the same, no matter in which Gradle project / IntelliJ module it will be used
+ */
+var IdeaKotlinBinaryDependency.isIdeaProjectLevel by isIdeaProjectLevelKey
