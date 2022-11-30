3b8c69b707a156a32c9d466385e9ac42e2ea52e1
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2022-11-30 14:19:46 +0200

    [FIR] Don't report redundant INAPPLICABLE_CANDIDATE
    Consider these examples of `INAPPLICABLE_CANDIDATE`
    as redundant, because there are
    other kinds of errors reported already

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/callableReferences/fromBasicDiagnosticTests/resolveCallableReferencesAfterAllSimpleArguments.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/callableReferences/fromBasicDiagnosticTests/resolveCallableReferencesAfterAllSimpleArguments.kt
@@ -7,6 +7,6 @@ fun <T> bar(f: (T) -> Unit, e: T) {}
 fun <T> baz(e: T, f: (T) -> Unit) {}
 
 fun test(a: A, b: B) {
-    <!INAPPLICABLE_CANDIDATE!>baz<!>(a, ::<!UNRESOLVED_REFERENCE!>fooB<!>)
-    <!INAPPLICABLE_CANDIDATE!>bar<!>(::<!UNRESOLVED_REFERENCE!>fooB<!>, a)
+    baz(a, ::<!UNRESOLVED_REFERENCE!>fooB<!>)
+    bar(::<!UNRESOLVED_REFERENCE!>fooB<!>, a)
 }
