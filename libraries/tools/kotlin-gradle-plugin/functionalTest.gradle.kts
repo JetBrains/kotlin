9c54569a309ac2ed8b98574d78d6a404253dfde0
Andrey Uskov <andrey.uskov@jetbrains.com>
2022-12-01 14:16:43 +0400

    Implemented tracking of api usage in KGP
    Added task that tracks presence of unrecommended invocations in
    generated bytecode. Currently added task 'forbidenApiCheck' which will
    fail on adding new invocations of File.getCaonicalFile.

--- libraries/tools/kotlin-gradle-plugin/functionalTest.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin/functionalTest.gradle.kts
@@ -36,6 +36,34 @@ val functionalTest by tasks.register<Test>("functionalTest") {
     }
 }
 
+val forbidenApiCheck by tasks.register<org.jetbrains.gradle.plugins.tools.InspectForForbidenAPIUsage>("forbidenApiCheck") {
+    forbiddenMethods.set(listOf("java.io.File.getCanonicalFile", "java.io.File.getCanonicalPath"))
+    ignoreClasses.set(listOf(
+        "org.jetbrains.kotlin.gradle.tasks.KotlinNativeTasksKt",
+    "org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeCompilerArgBuilderKt",
+        "org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest",
+        "org.jetbrains.kotlin.gradle.tasks.CInteropProcess",
+        "org.jetbrains.kotlin.gradle.targets.js.yarn.YarnEntryRegistry",
+        "org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig",
+        "org.jetbrains.kotlin.gradle.targets.js.ir.KotlinBrowserJsIr\$configureRun\$2\$runTask\$1\$3",
+        "org.jetbrains.kotlin.gradle.targets.js.ir.KotlinBrowserJsIr\$configureRun\$2\$runTask\$1\$4",
+        "org.jetbrains.kotlin.gradle.targets.js.binaryen.BinaryenExec",
+        "org.jetbrains.kotlin.gradle.targets.js.d8.D8Exec",
+        "org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink",
+        "org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsExec",
+        "org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependencyKt",
+        "org.jetbrains.kotlin.gradle.targets.js.npm.NpmProjectModules",
+        "org.jetbrains.kotlin.gradle.targets.js.testing.karma.KotlinKarma\$useCoverage\$3",
+        "org.jetbrains.kotlin.gradle.targets.js.subtargets.KotlinBrowserJs\$configureRun\$1\$runTask\$1\$3",
+        "org.jetbrains.kotlin.gradle.targets.js.subtargets.KotlinBrowserJs\$configureRun\$1\$runTask\$1\$4",
+        "org.jetbrains.kotlin.gradle.targets.js.subtargets.KotlinJsSubTarget\$configure\$1\$1"
+        ))
+    sourceSets.filter { !it.name.contains("test", false) }.forEach { sourceSet ->
+        validationClasspath += sourceSet.output
+    }
+}
+
 tasks.named("check") {
     dependsOn(functionalTest)
+    dependsOn(forbidenApiCheck)
 }
