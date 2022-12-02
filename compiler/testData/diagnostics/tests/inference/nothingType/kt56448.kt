2bafcddf7a43e5f3bf4071604a34931cda42988a
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-12-02 19:28:18 +0100

    K2: Avoid using Nothing? as inference result in the majority of cases
    Namely, do not choose `Nothing?` result type when fixing a variable
    that has other constraints besides the ones that came from
    the relevant type parameter's upper bounds.
    
    See more details in KT-55691.
    
    In K1, the case from specialCallWithMaterializeAndExpectedType.kt
    was working (inferred to String?) just because the branches
    were analyzed independently with `String?` expected type.
    
    This change became necessary after the previous commit when we united
    inference subsystems for if/when branches (see motivation there).
    
    NB: For K1, the behavior is left the same, but the code
    was refactored a bit.
    
    ^KT-55691 Fixed
    ^KT-56448 Fixed

---
+++ compiler/testData/diagnostics/tests/inference/nothingType/kt56448.kt
@@ -0,0 +1,43 @@
+// SKIP_TXT
+// WITH_STDLIB
+// ISSUE: KT-56448
+// FILE: SomeJavaClass.java
+
+public class SomeJavaClass {
+    public static String getString() {
+        return "";
+    }
+}
+
+// FILE: main.kt
+import kotlin.reflect.KProperty1
+
+interface XdEntity
+class XdIssue : XdEntity {
+    val isRemoved: Boolean = true
+    var votes: Int = 0
+}
+
+fun getNullableIssue(): XdIssue? = null
+
+fun <T : XdEntity> CharSequence.toXd(): T = null!!
+
+fun XdIssue.duplicatesRootSearch(): XdIssue = this
+fun <T : XdEntity, R : Comparable<*>?> T.getOldValue(property: KProperty1<T, R>): R? = null
+
+internal fun updateVotesForDuplicates(issue: XdIssue) {
+    val toRecount = HashSet<XdIssue>()
+    var oldDup = getNullableIssue()
+    if (oldDup != null) {
+        oldDup = try {
+            <!DEBUG_INFO_EXPRESSION_TYPE("XdIssue")!>SomeJavaClass.getString().toXd()<!>
+        } catch (_: Exception) {
+            null
+        }
+    }
+    val newDup = getNullableIssue()
+
+    if (oldDup != null && !<!DEBUG_INFO_SMARTCAST!>oldDup<!>.isRemoved) {
+        toRecount.add(<!DEBUG_INFO_SMARTCAST!>oldDup<!>.duplicatesRootSearch())
+    }
+}
---
+++ compiler/testData/diagnostics/tests/inference/nothingType/selectWithNull.kt
@@ -0,0 +1,13 @@
+// SKIP_TXT
+// ISSUE: KT-55691
+
+fun <T> materialize(): T = TODO()
+fun <S> select(x: S, y: S): S = x
+
+fun main() {
+    // materialize's type argument is inferred to `Nothing?` in K1 and `String?` in K2
+    val x: String? = select(<!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing?")!><!IMPLICIT_NOTHING_TYPE_ARGUMENT_AGAINST_NOT_NOTHING_EXPECTED_TYPE!>materialize<!>()<!>, null)
+
+    // materialize's type argument is inferred to `Nothing?` both in K1 and K2
+    select(<!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Nothing?")!>materialize()<!>, null)
+}
