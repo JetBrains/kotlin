2b637db0e2dcaa1695e5690b19a054f5919e9a5e
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2022-12-06 18:56:11 +0100

    [klib] Add documentation comments for IdSignatureComputer

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/signature/IdSignatureComputer.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/signature/IdSignatureComputer.kt
@@ -12,9 +12,30 @@ import org.jetbrains.kotlin.ir.declarations.IrField
 import org.jetbrains.kotlin.ir.symbols.IrFileSymbol
 import org.jetbrains.kotlin.ir.util.IdSignature
 
+/**
+ * Something capable of computing an [IdSignature] for an [IrDeclaration].
+ */
 interface IdSignatureComputer {
+
+    /**
+     * Computes a signature of [declaration].
+     *
+     * @param declaration The declaration to compute the signature for.
+     * @return The signature of the [declaration], or `null` if the declaration cannot have a signature.
+     */
     fun computeSignature(declaration: IrDeclaration): IdSignature?
 
+    /**
+     * Informs the signature computer that all signature computations for top-level private declarations within [block] will use
+     * the [file]'s signature (a signature for a top-level private declaration should always contain a signature of the file this
+     * declaration is declared in).
+     *
+     * @param file A symbol of the file for declarations in which signatures will be computed in [block], or `null` if the declarations
+     * won't have a file associated (like some compiler generated declarations).
+     * @param block A block within which signatures computed for private declarations will include [file]'s signature.
+     * @return Whatever [block] returns.
+     * @see [IdSignature.FileSignature]
+     */
     fun <R> inFile(file: IrFileSymbol?, block: () -> R): R
 }
 
