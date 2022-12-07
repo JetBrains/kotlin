201450db1207731207f2ff517a0201468694d611
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-12-07 15:43:48 +0100

    K2: Unwrap fake overrides before using them as DFA identifiers
    It's necessary because even for stable a.b.c.d we can't guarantee that
    this reference will always point to the same symbol because
    different capture type instantiations generate different scopes
    with different resulting symbol instances.

--- compiler/fir/semantics/src/org/jetbrains/kotlin/fir/resolve/dfa/util.kt
+++ compiler/fir/semantics/src/org/jetbrains/kotlin/fir/resolve/dfa/util.kt
@@ -13,9 +13,11 @@ import org.jetbrains.kotlin.fir.references.FirResolvedNamedReference
 import org.jetbrains.kotlin.fir.references.FirThisReference
 import org.jetbrains.kotlin.fir.references.impl.FirSimpleNamedReference
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirSyntheticPropertySymbol
 import org.jetbrains.kotlin.fir.types.*
+import org.jetbrains.kotlin.fir.unwrapFakeOverrides
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
@@ -75,8 +77,8 @@ val FirExpression.coneType: ConeKotlinType
 @DfaInternals
 val FirElement.symbol: FirBasedSymbol<*>?
     get() = when (this) {
-        is FirResolvable -> symbol
-        is FirDeclaration -> symbol
+        is FirResolvable -> symbol.unwrapFakeOverridesIfNecessary()
+        is FirDeclaration -> symbol.unwrapFakeOverridesIfNecessary()
         is FirWhenSubjectExpression -> whenRef.value.subject?.symbol
         is FirSafeCallExpression -> selector.symbol
         is FirSmartCastExpression -> originalExpression.symbol
@@ -85,6 +87,11 @@ val FirElement.symbol: FirBasedSymbol<*>?
         (this as? FirExpression)?.unwrapSmartcastExpression() is FirThisReceiverExpression ||
                 (it !is FirFunctionSymbol<*> && it !is FirSyntheticPropertySymbol)
     }
+
+private fun FirBasedSymbol<*>?.unwrapFakeOverridesIfNecessary(): FirBasedSymbol<*>? {
+    if (this !is FirCallableSymbol) return this
+    return this.unwrapFakeOverrides()
+}
 
 @DfaInternals
 internal val FirResolvable.symbol: FirBasedSymbol<*>?
