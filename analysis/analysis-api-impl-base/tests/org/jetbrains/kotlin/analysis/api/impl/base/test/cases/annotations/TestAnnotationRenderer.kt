e533c1dc0357a2fc9023ca07444021c570112dc4
Justin Paupore <jpaupore@google.com>
2022-12-08 11:40:38 -0800

    [AA] Add support for printing meta-annotations in annotation tests.
    This functionality will be used in follow-on commits.

--- analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/annotations/TestAnnotationRenderer.kt
+++ analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/annotations/TestAnnotationRenderer.kt
@@ -9,14 +9,43 @@ import org.jetbrains.kotlin.analysis.api.KtAnalysisSession
 import org.jetbrains.kotlin.analysis.api.annotations.KtAnnotationsList
 import org.jetbrains.kotlin.analysis.api.symbols.DebugSymbolRenderer
 import org.jetbrains.kotlin.analysis.test.framework.utils.indented
+import org.jetbrains.kotlin.name.ClassId
 
 object TestAnnotationRenderer {
     context (KtAnalysisSession)
     fun renderAnnotations(annotations: KtAnnotationsList) = buildString {
-        appendLine("annotations: [")
+        renderAnnotationsRecursive(annotations, currentMetaAnnotations = null, indent = 0)
+    }
+
+    context(KtAnalysisSession)
+    fun renderAnnotationsWithMeta(annotations: KtAnnotationsList) = buildString {
+        renderAnnotationsRecursive(annotations, currentMetaAnnotations = setOf(), indent = 0)
+    }
+
+    context(KtAnalysisSession)
+    private fun StringBuilder.renderAnnotationsRecursive(
+        annotations: KtAnnotationsList,
+        currentMetaAnnotations: Set<ClassId>?,
+        indent: Int
+    ) {
+        appendLine("annotations: [".indented(indent))
         for (annotation in annotations.annotations) {
-            appendLine(DebugSymbolRenderer().renderAnnotationApplication(annotation).indented(indent = 2))
+            appendLine(DebugSymbolRenderer().renderAnnotationApplication(annotation).indented(indent = indent + 2))
+            if (currentMetaAnnotations != null) {
+                val classId = annotation.classId ?: continue
+                if (classId in currentMetaAnnotations) {
+                    appendLine("<recursive meta-annotation ${classId}>".indented(indent + 4))
+                    continue
+                }
+
+                val metaAnnotations = getClassOrObjectSymbolByClassId(classId)?.annotationsList
+                if (metaAnnotations != null) {
+                    renderAnnotationsRecursive(metaAnnotations, currentMetaAnnotations + classId, indent = indent + 4)
+                } else {
+                    appendLine("<unknown meta-annotation ${classId}>".indented(indent + 4))
+                }
+            }
         }
-        appendLine("]")
+        appendLine("]".indented(indent))
     }
 }
