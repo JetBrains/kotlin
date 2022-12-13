03bd93f3c123b4defc299e4fd219644649148174
Denis.Zharkov <denis.zharkov@jetbrains.com>
2022-12-13 18:35:28 +0100

    ~ no hashMaps

--- compiler/testData/diagnostics/testsWithStdLib/kt8050.kt
+++ compiler/testData/diagnostics/testsWithStdLib/kt8050.kt
@@ -1,11 +1,9 @@
-private class X
+class X
 
 private operator fun X?.plus(p: Int) = X()
 
 class C {
-    private val map = hashMapOf<String, X>()
-
-    fun f() {
-        map<!NO_SET_METHOD!>[""]<!> += 1
+    fun foo(x: X) {
+        x.plus(1)sda
     }
 }
