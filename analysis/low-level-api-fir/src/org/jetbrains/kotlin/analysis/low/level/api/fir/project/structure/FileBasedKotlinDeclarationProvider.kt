e9af55fdee15ce53d5caa2a92728c236a5fa011c
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2022-12-14 01:24:39 +0900

    [LL API] Add PSI-based declaration provider for files outside roots
    Indices are not available in the IDE for files outside content roots,
    so a new provider acts as a single-file replacement.
    
    ^KTIJ-23937 Fixed

---
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/project/structure/FileBasedKotlinDeclarationProvider.kt
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.low.level.api.fir.project.structure
+
+import org.jetbrains.kotlin.analysis.providers.KotlinDeclarationProvider
+import org.jetbrains.kotlin.name.*
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.utils.addIfNotNull
+
+internal class FileBasedKotlinDeclarationProvider(private val kotlinFile: KtFile) : KotlinDeclarationProvider() {
+    override fun getClassLikeDeclarationByClassId(classId: ClassId): KtClassLikeDeclaration? {
+        if (classId.isLocal) {
+            return null
+        }
+
+        if (kotlinFile.packageFqName != classId.packageFqName) {
+            return null
+        }
+
+        var current: KtElement = kotlinFile
+
+        nextSegment@ for (segment in classId.relativeClassName.pathSegments()) {
+            val container = current as? KtDeclarationContainer ?: return null
+            for (child in container.declarations) {
+                if (child is KtClassLikeDeclaration && child.nameAsName == segment) {
+                    current = child
+                    continue@nextSegment
+                }
+            }
+
+            return null
+        }
+
+        return current as? KtClassLikeDeclaration
+    }
+
+    override fun getAllClassesByClassId(classId: ClassId): Collection<KtClassOrObject> {
+        return listOfNotNull(getClassLikeDeclarationByClassId(classId) as? KtClassOrObject)
+    }
+
+    override fun getAllTypeAliasesByClassId(classId: ClassId): Collection<KtTypeAlias> {
+        val name = classId.relativeClassName.pathSegments().singleOrNull() ?: return emptyList()
+        return getTopLevelDeclarations(classId.packageFqName, name)
+    }
+
+    override fun getTopLevelKotlinClassLikeDeclarationNamesInPackage(packageFqName: FqName): Set<Name> {
+        return getTopLevelDeclarationNames<KtClassLikeDeclaration>(packageFqName)
+    }
+
+    override fun getTopLevelProperties(callableId: CallableId): Collection<KtProperty> {
+        return getTopLevelCallables(callableId)
+    }
+
+    override fun getTopLevelFunctions(callableId: CallableId): Collection<KtNamedFunction> {
+        return getTopLevelCallables(callableId)
+    }
+
+    override fun getTopLevelCallableFiles(callableId: CallableId): Collection<KtFile> {
+        return buildSet {
+            getTopLevelProperties(callableId).mapTo(this) { it.containingKtFile }
+            getTopLevelFunctions(callableId).mapTo(this) { it.containingKtFile }
+        }
+    }
+
+    override fun getTopLevelCallableNamesInPackage(packageFqName: FqName): Set<Name> {
+        return getTopLevelDeclarationNames<KtCallableDeclaration>(packageFqName)
+    }
+
+    override fun findFilesForFacadeByPackage(packageFqName: FqName): Collection<KtFile> {
+        if (kotlinFile.packageFqName != packageFqName) {
+            return emptyList()
+        }
+
+        return listOf(kotlinFile)
+    }
+
+    override fun findFilesForFacade(facadeFqName: FqName): Collection<KtFile> {
+        for (declaration in kotlinFile.declarations) {
+            if (declaration !is KtClassLikeDeclaration) {
+                return listOf(kotlinFile)
+            }
+        }
+
+        return emptyList()
+    }
+
+    private inline fun <reified T : KtCallableDeclaration> getTopLevelCallables(callableId: CallableId): Collection<T> {
+        require(callableId.classId == null)
+        return getTopLevelDeclarations(callableId.packageName, callableId.callableName)
+    }
+
+    private inline fun <reified T : KtNamedDeclaration> getTopLevelDeclarations(packageFqName: FqName, name: Name): Collection<T> {
+        if (kotlinFile.packageFqName != packageFqName) {
+            return emptyList()
+        }
+
+        return buildList {
+            for (declaration in kotlinFile.declarations) {
+                if (declaration is T && declaration.nameAsName == name) {
+                    add(declaration)
+                }
+            }
+        }
+    }
+
+    private inline fun <reified T : KtNamedDeclaration> getTopLevelDeclarationNames(packageFqName: FqName): Set<Name> {
+        if (kotlinFile.packageFqName != packageFqName) {
+            return emptySet()
+        }
+
+        return buildSet {
+            for (declaration in kotlinFile.declarations) {
+                if (declaration is T) {
+                    addIfNotNull(declaration.nameAsName)
+                }
+            }
+        }
+    }
+}
