05438b0c203d667075ac15ebbecedeba295af6b5
Andrey Uskov <andrey.uskov@jetbrains.com>
2022-12-15 22:43:44 +0300

    Build reports: add lines count and LPS

--- build-common/src/org/jetbrains/kotlin/build/report/metrics/BuildPerformanceMetric.kt
+++ build-common/src/org/jetbrains/kotlin/build/report/metrics/BuildPerformanceMetric.kt
@@ -16,6 +16,10 @@ enum class BuildPerformanceMetric(val parent: BuildPerformanceMetric? = null, va
     BUNDLE_SIZE(readableString = "Total size of the final bundle", type = SizeMetricType.BYTES),
 
     COMPILE_ITERATION(parent = null, "Total compiler iteration", type = SizeMetricType.NUMBER),
+        ANALYZED_LINES_NUMBER(parent = COMPILE_ITERATION, "Number of lines analyzed", type = SizeMetricType.NUMBER),
+        CODE_GENERATED_LINES_NUMBER(parent = COMPILE_ITERATION, "Number of lines for code generation", type = SizeMetricType.NUMBER),
+        ANALYSIS_LPS(parent = COMPILE_ITERATION, "Analysis lines per second", type = SizeMetricType.NUMBER),
+        CODE_GENERATION_LPS(parent = COMPILE_ITERATION, "Code generation lines per second", type = SizeMetricType.NUMBER),
 
     // Metrics for the `kotlin.incremental.useClasspathSnapshot` feature
     CLASSPATH_ENTRY_SNAPSHOT_TRANSFORM_EXECUTION_COUNT(parent = null, "Number of times 'ClasspathEntrySnapshotTransform' ran", type = SizeMetricType.NUMBER),
