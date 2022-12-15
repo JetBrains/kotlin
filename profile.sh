2b2315105a9288413cb44a9a8507ecccfb53c0ae
Simon.Ogorodnik <simon.ogorodnik@jetbrains.com>
2022-12-15 10:38:30 +0100

    ++ setup

--- profile.sh
+++ profile.sh
@@ -4,15 +4,19 @@
 # Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
 #
 
-export JDK_11_0=/home/user/myjdk/amazon-corretto-11.0.99.8.1-linux-x64
-#JAVA_HOME=$JDK_11_0
-#PATH=$JAVA_HOME/bin:$PATH
+export JDK_11_0=/home/user/myjdk/amazon-corretto-11.0.97.8.1-linux-x64
+JAVA_HOME=$JDK_11_0
+PATH=$JAVA_HOME/bin:$PATH
 
 #./gradlew :compiler:fir:modularized-tests:test --tests "org.jetbrains.kotlin.fir.FullPipelineModularizedTest" \
 #-Pfir.bench.prefix=/home/user/Workspace/testData/kotlin-mt \
 #-Pfir.bench.jps.dir=/home/user/Workspace/testData/kotlin-mt/test-project-model-dump \
 #-Pfir.bench.passes=1 \
 #-Pfir.bench.use.build.file=true
+
+
+#-Dfir.bench.filter=.*idea/build.* \
+
 java -version
 java -cp `cat tmp/testClasspath.txt` \
 -XX:-TieredCompilation \
@@ -25,6 +29,7 @@ java -cp `cat tmp/testClasspath.txt` \
 -Dfir.bench.prefix=/home/user/Workspace/testData/kotlin-mt \
 -Dfir.bench.jps.dir=/home/user/Workspace/testData/kotlin-mt/test-project-model-dump \
 -Dfir.bench.passes=2 \
+-Dfir.bench.threads=1 \
 -Dfir.bench.dump=false \
 -Dfir.bench.use.build.file=true \
 -Didea.ignore.disabled.plugins=true \
