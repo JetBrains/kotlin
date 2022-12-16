98835211968cd844c32c982c325e0057f92fcd9f
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2022-12-16 13:58:47 +0100

    ~ ClassId equals

--- core/compiler.common/src/org/jetbrains/kotlin/name/ClassId.java
+++ core/compiler.common/src/org/jetbrains/kotlin/name/ClassId.java
@@ -155,7 +155,7 @@ public final class ClassId {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof ClassId)) return false;
 
         ClassId id = (ClassId) o;
         if (cachedHashCode != 0 && id.cachedHashCode != 0 && cachedHashCode != id.cachedHashCode) return false;
