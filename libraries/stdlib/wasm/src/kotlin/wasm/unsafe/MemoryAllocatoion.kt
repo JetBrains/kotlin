fead9ccd014f56c02eede3c756b832a8f1d5caf0
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2022-12-20 22:14:10 +0100

    [Wasm][Stdlib] Add public APIs for linear memory access
    Needed for interop with APIs, like WASI, that use linear memory.

---
+++ libraries/stdlib/wasm/src/kotlin/wasm/unsafe/MemoryAllocatoion.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.wasm.unsafe
+
+import kotlin.wasm.internal.WasmOp
+import kotlin.wasm.internal.implementedAsIntrinsic
+import kotlin.wasm.internal.unsafeGetScratchRawMemory
+
+/**
+ * WebAssembly linear memory allocator.
+ */
+public interface MemoryAllocator {
+    /**
+     * Allocate a block of linear memory of given [size] in bytes.
+     *
+     * @return an address of allocated memory. It is guaranteed to be a multiple of 8.
+     */
+    fun allocate(size: Int): Ptr
+}
+
+/**
+ * Run a [block] of code, providing it a temporary [MemoryAllocator] as an argument, and return its result.
+ *
+ * Free all memory allocated with provided allocator after running the [block].
+ *
+ * This function is intened to facilitate the exchange of values with outside world through linear memory.
+ * For example:
+ *
+ *    val buffer_size = ...
+ *    withScopedMemoryAllocator { allocator ->
+ *        val buffer_address = allocator.allocate(buffer_size)
+ *        importedWasmFunctionThatWritesToBuffer(buffer_address, buffer_size)
+ *        return readDataFromBufferIntoManagedKotlinMemory(buffer_address, buffer_size)
+ *    }
+ *
+ * WARNING! Addresses leaked outside of [block] scope become invalid and can be overridden.
+ *
+ * WARNING! Nested call [withScopedMemoryAllocator] will throw [IllegalStateException] will be trown.
+ *          Standard library may use this allocator to facilitaty JS interop. For example to copy String.
+ *
+ * WARNING! Accessing allocator outside of the [block] scope will throw [IllegalStateException].
+ */
+public inline fun <T> withScopedMemoryAllocator(
+    block: (allocator: MemoryAllocator) -> T
+): T {
+    check(!inScopedMemoryAllocatorBlock) { "Calls to withScopedMemoryAllocator can't be nested" }
+    inScopedMemoryAllocatorBlock = true
+    val allocator = ScopedMemoryAllocator()
+    val result = try {
+        block(allocator)
+    } finally {
+        inScopedMemoryAllocatorBlock = false
+        allocator.destroy()
+    }
+    return result
+}
+
+
+@PublishedApi
+internal var inScopedMemoryAllocatorBlock: Boolean = false
+
+@PublishedApi
+internal class ScopedMemoryAllocator : MemoryAllocator {
+    private var destroyed = false
+    private var availableAddress: ULong = unsafeGetScratchRawMemory(0/*unused*/).toULong()
+
+    override fun allocate(size: Int): Ptr {
+        check(!destroyed) { "ScopedMemoryAllocator is destroyed when out of scope" }
+
+        // Pad available address to align it to 8
+        val align = 8uL
+        val result = (availableAddress + align - 1uL) and (align - 1uL).inv()
+        check(result % 8uL == 0uL)
+
+        availableAddress = result + size.toULong()
+
+        if (availableAddress > UInt.MAX_VALUE.toULong()) {
+            error("Out of linear memory. All available address space (4gb) is used.")
+        }
+
+        val currentMaxSize = wasmMemorySize().toULong() * WASM_PAGE_SIZE_IN_BYTES.toULong()
+        if (availableAddress >= currentMaxSize) {
+
+            val numPagesToGrow =
+                (availableAddress - currentMaxSize) / WASM_PAGE_SIZE_IN_BYTES.toULong() + 2uL
+
+            if (wasmMemoryGrow(numPagesToGrow.toInt()) == -1) {
+                error("Out of linear memory. memory.grow returned -1")
+            }
+        }
+
+        check(availableAddress < wasmMemorySize().toULong() * WASM_PAGE_SIZE_IN_BYTES.toULong())
+
+        return result.toInt()
+    }
+
+    fun destroy() {
+        destroyed = true
+    }
+}
+
+private const val WASM_PAGE_SIZE_IN_BYTES = 65_536  // 64 KiB
+
+/**
+ * Current linear memory size in pages
+ */
+@WasmOp(WasmOp.MEMORY_SIZE)
+internal fun wasmMemorySize(): Int =
+    implementedAsIntrinsic
+
+/**
+ * Grow memory by a given delta (in pages).
+ * Return the previous size, or -1 if enough memory cannot be allocated.
+ */
+@WasmOp(WasmOp.MEMORY_GROW)
+internal fun wasmMemoryGrow(delta: Int): Int =
+    implementedAsIntrinsic
---
+++ libraries/stdlib/wasm/test/unsafe/MemoryAllocationTest.kt
@@ -0,0 +1,125 @@
+package test.wasm.unsafe
+
+import kotlin.wasm.unsafe.*
+import kotlin.test.*
+
+@OptIn(UnsafeWasmApi::class)
+class MemoryAllocationTest {
+    val pageSize = 65_536
+
+    @Test
+    fun testWasmMemorySizeGrow() {
+        val s1 = wasmMemorySize()
+        val grow_res = wasmMemoryGrow(10)
+        val s2 = wasmMemorySize()
+        assertNotEquals(grow_res, -1)
+        assertEquals(grow_res, s1)
+        assertEquals(s2 - s1, 10)
+    }
+
+    @Test
+    fun testScopedAllocator() {
+        val sizes = listOf<Int>(1, 1, 2, 3, 8, 10, 305, 12_747, 31_999)
+        val allocations = mutableListOf<Int>()
+        withScopedMemoryAllocator { a ->
+            for (size in sizes) {
+                allocations += a.allocate(size)
+            }
+        }
+
+        val allocations2 = mutableListOf<Int>()
+        withScopedMemoryAllocator { a ->
+            for (size in sizes) {
+                allocations2 += a.allocate(size)
+            }
+        }
+
+        // Test that we run withScopedMemoryAllocator body
+        assertEquals(allocations.size, sizes.size)
+
+        // Memory should be reusing in different scopes
+        // NOTE: This is current impl detail and can be changed
+        assertEquals(allocations, allocations2)
+
+        // Allocations are aligned
+        assertTrue(allocations.all { it % 8 == 0 })
+
+        // Allocations are different
+        assertTrue(allocations.distinct().size == allocations.size)
+
+        // Allocations do not intersect
+        for (i1 in 0 until sizes.size) {
+            for (i2 in 0 until sizes.size) {
+                if (i1 == i2) {
+                    break
+                }
+                val a1 = allocations[i1]
+                val a2 = allocations[i2]
+                val size2 = sizes[i2]
+                assertTrue(a1 !in (a2 ..< a2 + size2))
+            }
+        }
+
+        // Allocations past current memory size should grow memory
+        val memSizes = mutableListOf<Int>(wasmMemorySize())
+        withScopedMemoryAllocator { a ->
+            var allocatedAddress = a.allocate(pageSize)
+            var allocationSize = pageSize
+
+            repeat(10) {
+                var currPagesUsed = (allocatedAddress + allocationSize + 1) / pageSize
+                var currPagesAvailable = wasmMemorySize()
+                assertTrue(currPagesAvailable > currPagesUsed)
+                // Allocate 10 pages past max page
+                allocationSize = (currPagesAvailable - currPagesUsed + 10) * pageSize
+                allocatedAddress = a.allocate(allocationSize)
+                memSizes += wasmMemorySize()
+            }
+        }
+        assertTrue(memSizes.size == 11)
+        assertEquals(memSizes.distinct(), memSizes)
+        assertEquals(memSizes.sorted(), memSizes)
+    }
+
+    @Test
+    fun testScopedAllocatorThrows() {
+        assertFailsWith<IllegalStateException> {
+            withScopedMemoryAllocator {
+                withScopedMemoryAllocator {
+                }
+            }
+        }
+
+        assertFailsWith<IllegalStateException> {
+            var leakedAllocator: MemoryAllocator? = null
+            withScopedMemoryAllocator { allocator ->
+                leakedAllocator = allocator
+            }
+            leakedAllocator?.allocate(10)
+        }
+
+        assertFailsWith<IllegalStateException> {
+            var leakedAllocator: MemoryAllocator? = null
+            try {
+                withScopedMemoryAllocator { allocator ->
+                    leakedAllocator = allocator
+                    throw Error()
+                }
+            } catch (e: Throwable) {}
+            leakedAllocator?.allocate(10)
+        }
+
+        assertFailsWith<IllegalStateException> {
+            fun foo(): MemoryAllocator {
+                withScopedMemoryAllocator { allocator ->
+                    return allocator  // non-local return
+                }
+            }
+            foo().allocate(10)
+        }
+    }
+
+    private fun Int.alignTo(align: Int) =
+        (this + align - 1) and (align - 1).inv()
+
+}
