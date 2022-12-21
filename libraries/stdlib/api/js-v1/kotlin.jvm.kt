d1426acf452e952c9b5213d0fbea20544da6bd45
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2022-12-21 23:04:07 +0100

    Deprecate Synchronized annotation in common and JS #KT-55344

--- libraries/stdlib/api/js-v1/kotlin.jvm.kt
+++ libraries/stdlib/api/js-v1/kotlin.jvm.kt
@@ -1,5 +1,7 @@
 @kotlin.annotation.Target(allowedTargets = {AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER})
 @kotlin.annotation.Retention(value = AnnotationRetention.SOURCE)
+@kotlin.Deprecated(message = "Synchronizing methods on a class instance is not supported on platforms other than JVM. If you need to annotate a common method as JVM-synchronized, introduce your own optional-expectation annotation and actualize it with a typealias to kotlin.jvm.Synchronized.")
+@kotlin.DeprecatedSinceKotlin(warningSince = "1.8")
 public final annotation class Synchronized : kotlin.Annotation {
     public constructor Synchronized()
 }
