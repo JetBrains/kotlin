13f0421d50c63c19c8cf6e11acb03a1e480be6c8
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2022-12-28 09:45:09 +0100

    ~ Add test of loading/storing random values

--- libraries/stdlib/wasm/test/unsafe/MemoryAccessTest.kt
+++ libraries/stdlib/wasm/test/unsafe/MemoryAccessTest.kt
@@ -5,8 +5,64 @@ import kotlin.test.*
 
 @OptIn(UnsafeWasmMemoryApi::class)
 class MemoryAccessTestTest {
+    fun <T> testLoadStore(values: List<T>, typeSize: Int, store: (Pointer, T) -> Unit, load: (Pointer) -> T) {
+        withScopedMemoryAllocator { a ->
+            // Memory layout: [Long1][T][Long2]
+            val ptrToLong1 = a.allocate(24)
+            val ptrToT = ptrToLong1 + 8
+            val ptrToLong2 = ptrToT + typeSize
+
+            for (x in values) {
+                val prevLong = ptrToLong1.loadLong()
+                val nextLong = ptrToLong2.loadLong()
+
+                store(ptrToT, x)
+                assertEquals(load(ptrToT), x)
+
+                assertEquals(ptrToLong1.loadLong(), prevLong)
+                assertEquals(ptrToLong2.loadLong(), nextLong)
+            }
+        }
+    }
+
     @Test
-    fun testScopedAllocator() {
+    fun testByte() {
+        val bytes = listOf<Byte>(0, Byte.MIN_VALUE, Byte.MAX_VALUE, -91, -21, -47, -72, -42, 118, 120, 125, 21, -43)
+        testLoadStore(bytes, 1, { p, v -> p.storeByte(v) }, { it.loadByte() })
+    }
+
+    @Test
+    fun testShort() {
+        val shorts = listOf<Short>(
+            0, Short.MIN_VALUE, Short.MAX_VALUE, 26350, 17667, 5437, 1381,
+            21183, 26042, -25961, -22913, 9128, -10684
+        )
+        testLoadStore(shorts, 2, { p, v -> p.storeShort(v) }, { it.loadShort() })
+    }
+
+    @Test
+    fun testInt() {
+        val ints = listOf<Int>(
+            0, Int.MIN_VALUE, Int.MAX_VALUE,
+            1348618689, -299556943, -394977414, -621300994, 1034622853, -1010496662,
+            -2102993550, 199131417, 407819728, -1093382545
+        )
+        testLoadStore(ints, 4, { p, v -> p.storeInt(v) }, { it.loadInt() })
+    }
+
+    @Test
+    fun testLong() {
+        val longs = listOf<Long>(
+            0, Long.MIN_VALUE, Long.MAX_VALUE,
+            6964777768087685094, -3965399897925814666, 6876207943944046195, 7675081221595661767,
+            -3388229176969119769, 4265730675328983821, -4893379785828386453, -7516879919690485136,
+            8512965883914804069, 6155050932825287650
+        )
+        testLoadStore(longs, 8, { p, v -> p.storeLong(v) }, { it.loadLong() })
+    }
+
+    @Test
+    fun testAccessingWithDifferentTypes() {
         withScopedMemoryAllocator { a ->
             val size = 16
             val pointer = a.allocate(size)
