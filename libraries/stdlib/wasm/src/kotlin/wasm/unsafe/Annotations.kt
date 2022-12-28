e8d9af5fde0cc2496be9f0125d07a54693f2253e
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2022-12-28 07:40:53 +0100

    ~ Document annotation

--- libraries/stdlib/wasm/src/kotlin/wasm/unsafe/Annotations.kt
+++ libraries/stdlib/wasm/src/kotlin/wasm/unsafe/Annotations.kt
@@ -7,5 +7,12 @@ package kotlin.wasm.unsafe
 
 import kotlin.annotation.AnnotationTarget.*
 
-@RequiresOptIn("Unsafe WebAssembly linear memory APIs")
+/**
+ * This annotation marks APIs for working with unmanaged WebAssembly linear memory.
+ *
+ * Any usage of a declaration annotated with `@UnsafeWasmMemoryApi` must be accepted either by
+ * annotating that usage with the [OptIn] annotation, e.g. `@OptIn(UnsafeWasmMemoryApi::class)`,
+ * or by using the compiler argument `-opt-in=kotlin.wasm.unsafe.UnsafeWasmMemoryApi`.
+ */
+@RequiresOptIn("Unsafe APIs to access to WebAssembly linear memory")
 public annotation class UnsafeWasmMemoryApi
