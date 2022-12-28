e7745c1ff6970943200c6904f7c808968d6f28d9
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2022-12-28 15:20:03 +0100

    Report CONCURRENT_HASH_MAP_CONTAINS_OPERATOR_ERROR for class hierarchies
    * Previously it wasn't reported in FIR for ConcurrentHashMap inheritors
      because the receiver id hasn't matched CHM.contains id
    * Fixed by unwrapping origin of the call in case of fake overrides
    
    ^KT-55606 fixed

--- compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/expression/FirJvmInconsistentOperatorFromJavaCallChecker.kt
+++ compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/expression/FirJvmInconsistentOperatorFromJavaCallChecker.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.containingClassLookupTag
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCallOrigin
+import org.jetbrains.kotlin.fir.originalOrSelf
 import org.jetbrains.kotlin.fir.references.toResolvedCallableSymbol
 import org.jetbrains.kotlin.fir.resolve.toFirRegularClassSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
@@ -27,6 +28,12 @@ import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
+/**
+ * This checker detects if a call by operator 'contains' convention to a Java method violates the expected contract:
+ * * "key in map" commonly resolves to stdlib extension that calls Map.containsKey(),
+ * but there's a member in ConcurrentHashMap with acceptable signature that delegates to `containsValue` instead,
+ * leading to an unexpected result. See KT-18053
+ */
 object FirJvmInconsistentOperatorFromJavaCallChecker : FirFunctionCallChecker() {
     private val CONCURRENT_HASH_MAP_CALLABLE_ID = CallableId(
         ClassId.fromString("java/util/concurrent/ConcurrentHashMap"),
@@ -45,8 +52,9 @@ object FirJvmInconsistentOperatorFromJavaCallChecker : FirFunctionCallChecker() 
         callableSymbol.check(expression.calleeReference.source, context, reporter)
     }
 
-    fun FirNamedFunctionSymbol.check(source: KtSourceElement?, context: CheckerContext, reporter: DiagnosticReporter): Boolean {
-        if (callableId == CONCURRENT_HASH_MAP_CALLABLE_ID) {
+    private fun FirNamedFunctionSymbol.check(source: KtSourceElement?, context: CheckerContext, reporter: DiagnosticReporter): Boolean {
+        // Unwrap SubstitutionOverride origin if necessary
+        if (originalOrSelf().callableId == CONCURRENT_HASH_MAP_CALLABLE_ID) {
             reporter.reportOn(source, FirJvmErrors.CONCURRENT_HASH_MAP_CONTAINS_OPERATOR, context)
             return true
         }
