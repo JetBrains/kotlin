e7e2ffa383a9dfd23b9b304b8fd2041d8d5a29ba
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-12-29 15:49:19 +0100

    [LL FIR] lazy resolve imports under a separate lock

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/file/builder/LLFirLockProvider.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/file/builder/LLFirLockProvider.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.file.builder
 
+import com.intellij.util.containers.ContainerUtil
 import org.jetbrains.kotlin.analysis.low.level.api.fir.util.lockWithPCECheck
 import org.jetbrains.kotlin.fir.declarations.FirFile
 import java.util.concurrent.locks.ReentrantLock
@@ -17,12 +18,21 @@ internal class LLFirLockProvider {
     //We temporarily disable multi-locks to fix deadlocks problem
     private val globalLock = ReentrantLock()
 
+    private val locksForImports = ContainerUtil.createConcurrentSoftMap<FirFile, ReentrantLock>()
+
     inline fun <R> withLock(
         @Suppress("UNUSED_PARAMETER") key: FirFile,
         lockingIntervalMs: Long = DEFAULT_LOCKING_INTERVAL,
         action: () -> R
     ): R {
         return globalLock.lockWithPCECheck(lockingIntervalMs) { action() }
+    }
+
+    inline fun <R> withLocksForImportResolution(
+        file: FirFile,
+        action: () -> R
+    ): R {
+        return locksForImports.getOrPut(file) { ReentrantLock() }.lockWithPCECheck(DEFAULT_LOCKING_INTERVAL) { action() }
     }
 }
 
