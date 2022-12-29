256396235bc3bb7e319008712fec0c0910adc6e5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-12-29 12:12:40 +0100

    [LL FIR] rework lazy resolve, so it never resolves local declaration
    No local declaration should be available until it's fully resolved

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/contrainingFileUtils.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/contrainingFileUtils.kt
@@ -5,11 +5,13 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.util
 
-import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.llFirResolvableSession
 import org.jetbrains.kotlin.fir.FirElementWithResolvePhase
 import org.jetbrains.kotlin.fir.FirFileAnnotationsContainer
 import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.psi
 import org.jetbrains.kotlin.fir.resolve.providers.firProvider
+import org.jetbrains.kotlin.psi.KtFile
 
 fun FirElementWithResolvePhase.getContainingFile(): FirFile? {
     val provider = moduleData.session.firProvider
@@ -21,7 +23,14 @@ fun FirElementWithResolvePhase.getContainingFile(): FirFile? {
         is FirValueParameter -> containingFunctionSymbol.fir.getContainingFile()
         is FirCallableDeclaration -> provider.getFirCallableContainerFile(symbol)
         is FirClassLikeDeclaration -> provider.getFirClassifierContainerFile(symbol)
-        is FirDanglingModifierList -> null
+        is FirDanglingModifierList -> {
+            val ktFile = psi?.containingFile as? KtFile
+                ?: error("File for dangling modifier list cannot be null")
+            val moduleComponents = llFirResolvableSession?.moduleComponents
+                ?: error("LLFirResolvableModuleSession for dangling modifier list cannot be null")
+            moduleComponents.cache.getCachedFirFile(ktFile)
+                ?: error("Fir file for dandling modifier list cannot be null")
+        }
         else -> errorWithFirSpecificEntries("Unsupported declaration ${this::class.java}", fir = this)
     }
 }
