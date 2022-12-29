74734d966ae4197714d4ac5ea0a2b3f1820c0292
Egor Kulikov <egor.kulikov@jetbrains.com>
2022-12-29 14:58:08 +0100

    [FIR] Make annotations arguments lazy in RawFirBuilder
    Add more tests with annotations with arguments

--- compiler/fir/raw-fir/psi2fir/testData/rawBuilder/declarations/annotation.kt
+++ compiler/fir/raw-fir/psi2fir/testData/rawBuilder/declarations/annotation.kt
@@ -8,8 +8,6 @@
     AnnotationTarget.TYPE
 ) annotation class base
 
-@base annotation class derived
-
 @base class correct(@base val x: Int) {
     @base constructor(): this(0)
 }
@@ -26,3 +24,33 @@
 }
 
 @base val z = 0
+
+@base val x: Map<@base Int, List<@base Int>> = mapOf()
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.LOCAL_VARIABLE,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPE
+) @base annotation class derived(val x: Int): base
+
+@derived(1) class correctDerived(@derived(1) val x: Int) {
+    @base constructor(): this(0)
+}
+
+@derived(1) enum class MyDerived {
+    @derived(1) FIRST,
+    @derived(1) SECOND
+}
+
+@derived(1) fun fooDerived(@derived(1) y: @derived(1) Int): Int {
+    @derived(1) fun bar(@derived(1) z: @derived(1) Int) = z + 1
+    @derived(1) val local = bar(y)
+    return local
+}
+
+@derived(1) val zDerived = 0
+
+@derived(1) val xDerived: Map<@derived(1) Int, List<@derived(1) Int>> = mapOf()
