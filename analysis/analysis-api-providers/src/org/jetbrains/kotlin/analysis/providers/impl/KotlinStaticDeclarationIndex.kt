669afdd463b2468343070278b54239267c1a23c4
Jinseong Jeon <jsjeon@google.com>
2023-01-02 23:14:24 -0800

    AA: place KotlinStaticDeclarationIndex in its own file with less visibility

---
+++ analysis/analysis-api-providers/src/org/jetbrains/kotlin/analysis/providers/impl/KotlinStaticDeclarationIndex.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.providers.impl
+
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.*
+
+internal class KotlinStaticDeclarationIndex {
+    internal val facadeFileMap: MutableMap<FqName, MutableSet<KtFile>> = mutableMapOf()
+    internal val classMap: MutableMap<FqName, MutableSet<KtClassOrObject>> = mutableMapOf()
+    internal val typeAliasMap: MutableMap<FqName, MutableSet<KtTypeAlias>> = mutableMapOf()
+    internal val topLevelFunctionMap: MutableMap<FqName, MutableSet<KtNamedFunction>> = mutableMapOf()
+    internal val topLevelPropertyMap: MutableMap<FqName, MutableSet<KtProperty>> = mutableMapOf()
+}
