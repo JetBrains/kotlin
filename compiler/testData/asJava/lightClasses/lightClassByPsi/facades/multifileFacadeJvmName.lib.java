511e05adecd52013af359810fde6a4484854c727
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2023-01-03 11:10:17 +0100

    Fix test data
    Test data had to be adjusted together with a main change
    
    #KTIJ-912

--- compiler/testData/asJava/lightClasses/lightClassByPsi/facades/multifileFacadeJvmName.lib.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/facades/multifileFacadeJvmName.lib.java
@@ -1,3 +1,7 @@
 public final class multifileFacade /* multifileFacade*/ {
   public static final int foo();//  foo()
 }
+
+final class multifileFacade__MultifileFacadeJvmNameKt /* multifileFacade__MultifileFacadeJvmNameKt*/ {
+  public static final int foo();//  foo()
+}
