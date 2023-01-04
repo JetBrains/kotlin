0cc973ae5c5744a3886b512197d4d007ead7e3b9
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-01-04 17:23:03 +0100

    ~ K2: Avoid using Nothing? as inference result in the majority of cases

--- compiler/testData/diagnostics/tests/inference/nothingVsParameterBound.kt
+++ compiler/testData/diagnostics/tests/inference/nothingVsParameterBound.kt
@@ -4,8 +4,12 @@
 class Out<out T : CharSequence?>(val t: T)
 
 fun foo() {
+    // We have two constraints here:
+    // Nothing? <: T (from argument `null` type)
+    // T <: CharSequence?
+    // And we fix T to `Nothing?`, because it's still more preferrable than constraint from the upper bound
     val x1 = Out(null)
-    bar(x1)
+    bar(<!DEBUG_INFO_EXPRESSION_TYPE("Out<kotlin.Nothing?>")!>x1<!>)
 }
 
 fun bar(w: Out<String?>) {}
