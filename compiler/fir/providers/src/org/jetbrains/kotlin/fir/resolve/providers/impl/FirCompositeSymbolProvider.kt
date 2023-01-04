d23fd80d0f30855eab0c6af97ce857dc8dbdbd06
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-01-04 13:38:51 +0100

    K2: Add caches to FirCompositeSymbolProvider
    The cache structure is the same as at FirDependenciesSymbolProviderImpl

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/resolve/providers/impl/FirCompositeSymbolProvider.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/resolve/providers/impl/FirCompositeSymbolProvider.kt
@@ -7,20 +7,30 @@ package org.jetbrains.kotlin.fir.resolve.providers.impl
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.NoMutableState
+import org.jetbrains.kotlin.fir.caches.createCache
+import org.jetbrains.kotlin.fir.caches.firCachesFactory
+import org.jetbrains.kotlin.fir.caches.getValue
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProviderInternals
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassLikeSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
+import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 
 @NoMutableState
 class FirCompositeSymbolProvider(session: FirSession, val providers: List<FirSymbolProvider>) : FirSymbolProvider(session) {
+    private val classCache = session.firCachesFactory.createCache(::computeClass)
+    private val topLevelCallableCache = session.firCachesFactory.createCache(::computeTopLevelCallables)
+    private val topLevelFunctionCache = session.firCachesFactory.createCache(::computeTopLevelFunctions)
+    private val topLevelPropertyCache = session.firCachesFactory.createCache(::computeTopLevelProperties)
+    private val packageCache = session.firCachesFactory.createCache(::computePackage)
+
     override fun getTopLevelCallableSymbols(packageFqName: FqName, name: Name): List<FirCallableSymbol<*>> {
-        return providers.flatMap { it.getTopLevelCallableSymbols(packageFqName, name) }
+        return topLevelCallableCache.getValue(CallableId(packageFqName, name))
     }
 
     @FirSymbolProviderInternals
@@ -30,23 +40,40 @@ class FirCompositeSymbolProvider(session: FirSession, val providers: List<FirSym
 
     @FirSymbolProviderInternals
     override fun getTopLevelFunctionSymbolsTo(destination: MutableList<FirNamedFunctionSymbol>, packageFqName: FqName, name: Name) {
-        providers.forEach {
-            it.getTopLevelFunctionSymbolsTo(destination, packageFqName, name)
-        }
+        destination += topLevelFunctionCache.getValue(CallableId(packageFqName, name))
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelPropertySymbolsTo(destination: MutableList<FirPropertySymbol>, packageFqName: FqName, name: Name) {
-        providers.forEach {
-            it.getTopLevelPropertySymbolsTo(destination, packageFqName, name)
-        }
+        destination += topLevelPropertyCache.getValue(CallableId(packageFqName, name))
     }
 
     override fun getPackage(fqName: FqName): FqName? {
-        return providers.firstNotNullOfOrNull { it.getPackage(fqName) }
+        return packageCache.getValue(fqName)
     }
 
     override fun getClassLikeSymbolByClassId(classId: ClassId): FirClassLikeSymbol<*>? {
-        return providers.firstNotNullOfOrNull { it.getClassLikeSymbolByClassId(classId) }
+        return classCache.getValue(classId)
     }
+
+    @OptIn(FirSymbolProviderInternals::class)
+    private fun computeTopLevelCallables(callableId: CallableId): List<FirCallableSymbol<*>> = buildList {
+        providers.forEach { it.getTopLevelCallableSymbolsTo(this, callableId.packageName, callableId.callableName) }
+    }
+
+    @OptIn(FirSymbolProviderInternals::class)
+    private fun computeTopLevelFunctions(callableId: CallableId): List<FirNamedFunctionSymbol> = buildList {
+        providers.forEach { it.getTopLevelFunctionSymbolsTo(this, callableId.packageName, callableId.callableName) }
+    }
+
+    @OptIn(FirSymbolProviderInternals::class)
+    private fun computeTopLevelProperties(callableId: CallableId): List<FirPropertySymbol> = buildList {
+        providers.forEach { it.getTopLevelPropertySymbolsTo(this, callableId.packageName, callableId.callableName) }
+    }
+
+    private fun computePackage(it: FqName): FqName? =
+        providers.firstNotNullOfOrNull { provider -> provider.getPackage(it) }
+
+    private fun computeClass(classId: ClassId): FirClassLikeSymbol<*>? =
+        providers.firstNotNullOfOrNull { provider -> provider.getClassLikeSymbolByClassId(classId) }
 }
