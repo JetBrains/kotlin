a5c64111e9ae79c284ade005dbf8da3d0a408a86
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-01-04 15:40:28 +0100

    2/4 Refactoring: move KlibMetadataVersion from util-klib-metadata to util-klib
    - This commit relies on the previous "preparation refactoring" commit
    - This commit is needed to add Klib metadata version check in the next
      commits KT-55808

--- compiler/util-klib/build.gradle.kts
+++ compiler/util-klib/build.gradle.kts
@@ -8,6 +8,9 @@ description = "Common klib reader and writer"
 dependencies {
     api(kotlinStdlib())
     api(project(":kotlin-util-io"))
+    // HACK compileOnly to avoid creating invalid dependencies in kotlin-util-klib maven artifact
+    // (unfortunatelly, we use this hack everywhere in the Kotlin repo :( Something should be done with this problem globally)
+    compileOnly(project(":core:metadata.buildSrc"))
     testImplementation(commonDependency("junit:junit"))
 }
 
