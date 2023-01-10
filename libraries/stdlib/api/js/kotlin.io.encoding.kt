b31b9d1cbf2e0da7f91e32cec5559beab4ffa00f
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2023-01-10 18:45:57 +0200

    Introduce @ExperimentalBaseEncodingApi for binary-to-text encoding API

--- libraries/stdlib/api/js/kotlin.io.encoding.kt
+++ libraries/stdlib/api/js/kotlin.io.encoding.kt
@@ -23,3 +23,12 @@ public open class Base64 {
         public final val UrlSafe: kotlin.io.encoding.Base64 { get; }
     }
 }
+
+@kotlin.RequiresOptIn(level = Level.ERROR)
+@kotlin.annotation.Retention(value = AnnotationRetention.BINARY)
+@kotlin.annotation.Target(allowedTargets = {AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.PROPERTY, AnnotationTarget.FIELD, AnnotationTarget.LOCAL_VARIABLE, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER, AnnotationTarget.TYPEALIAS})
+@kotlin.annotation.MustBeDocumented
+@kotlin.SinceKotlin(version = "1.8")
+public final annotation class ExperimentalBaseEncodingApi : kotlin.Annotation {
+    public constructor ExperimentalBaseEncodingApi()
+}
