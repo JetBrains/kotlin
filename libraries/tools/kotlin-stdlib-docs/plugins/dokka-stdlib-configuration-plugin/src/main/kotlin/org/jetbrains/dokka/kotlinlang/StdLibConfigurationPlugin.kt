36f2b550a5b46b04a6b3daafb734e38c751c242e
vmishenev <vad-mishenev@yandex.ru>
2023-01-11 03:18:24 +0200

    Disable `ignoreCommonBuiltIns` in kotlin.test

--- libraries/tools/kotlin-stdlib-docs/plugins/dokka-stdlib-configuration-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/StdLibConfigurationPlugin.kt
+++ libraries/tools/kotlin-stdlib-docs/plugins/dokka-stdlib-configuration-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/StdLibConfigurationPlugin.kt
@@ -4,6 +4,7 @@ import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.jetbrains.dokka.analysis.DokkaAnalysisConfiguration
 import org.jetbrains.dokka.analysis.KotlinAnalysis
+import org.jetbrains.dokka.plugability.configuration
 
 class StdLibConfigurationPlugin : DokkaPlugin() {
     private val dokkaBase by lazy { plugin<DokkaBase>() }
@@ -11,10 +12,11 @@ class StdLibConfigurationPlugin : DokkaPlugin() {
     @Suppress("unused")
     val stdLibKotlinAnalysis by extending {
         dokkaBase.kotlinAnalysis providing { ctx ->
+            val ignoreCommonBuiltIns = configuration<StdLibConfigurationPlugin, StdLibAnalysisConfiguration>(ctx)?.ignoreCommonBuiltIns ?: false
             KotlinAnalysis(
                 sourceSets = ctx.configuration.sourceSets,
                 logger = ctx.logger,
-                analysisConfiguration = DokkaAnalysisConfiguration(ignoreCommonBuiltIns = true)
+                analysisConfiguration = DokkaAnalysisConfiguration(ignoreCommonBuiltIns = ignoreCommonBuiltIns)
             )
         } override dokkaBase.defaultKotlinAnalysis
     }
