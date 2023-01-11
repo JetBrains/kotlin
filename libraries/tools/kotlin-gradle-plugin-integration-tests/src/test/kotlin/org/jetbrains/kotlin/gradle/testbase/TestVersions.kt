cd6f57eb86b64172ef4f874b24a744607c66bbc9
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-01-11 15:11:14 +0100

    Add AGP 8.0 to test versions

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -43,7 +43,8 @@ interface TestVersions {
         const val AGP_71 = "7.1.3"
         const val AGP_72 = "7.2.1"
         const val AGP_73 = "7.3.0"
-        const val AGP_74 = "7.4.0-beta05"
+        const val AGP_74 = "7.4.0-rc03"
+        const val AGP_80 = "8.0.0-alpha11"
 
         const val MIN_SUPPORTED = AGP_41 // KotlinAndroidPlugin.MINIMAL_SUPPORTED_AGP_VERSION
         const val MAX_SUPPORTED = AGP_72 // Update once Gradle MAX_SUPPORTED version will be bumped to 7.2+
@@ -61,7 +62,8 @@ interface TestVersions {
         AGP_71(AGP.AGP_71, GradleVersion.version(Gradle.G_7_2), GradleVersion.version(Gradle.G_7_4), JavaVersion.VERSION_11),
         AGP_72(AGP.AGP_72, GradleVersion.version(Gradle.G_7_3), GradleVersion.version(Gradle.G_7_4), JavaVersion.VERSION_11),
         AGP_73(AGP.AGP_73, GradleVersion.version(Gradle.G_7_4), GradleVersion.version(Gradle.G_7_5), JavaVersion.VERSION_11),
-        AGP_74(AGP.AGP_74, GradleVersion.version(Gradle.G_7_5), GradleVersion.version(Gradle.G_7_5), JavaVersion.VERSION_11),
+        AGP_74(AGP.AGP_74, GradleVersion.version(Gradle.G_7_5), GradleVersion.version(Gradle.G_8_0), JavaVersion.VERSION_11),
+        AGP_80(AGP.AGP_80, GradleVersion.version(Gradle.G_8_0), GradleVersion.version(Gradle.G_8_0), JavaVersion.VERSION_17),
         ;
     }
 }
