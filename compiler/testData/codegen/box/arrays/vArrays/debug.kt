24303cf941c5602cbfc9eb0feb537aae9aa15900
vladislav.grechko <vladislav.grechko@jetbrains.com>
2023-01-11 11:48:46 +0100

    some progress

---
+++ compiler/testData/codegen/box/arrays/vArrays/debug.kt
@@ -0,0 +1,11 @@
+// ENABLE_JVM_IR_INLINER
+// WITH_STDLIB
+// TARGET_BACKEND: JVM_IR
+
+
+inline fun <T> foo(p: Array<T>) = p.iterator().next().toString()
+
+fun box(): String {
+
+    return "OK1"
+}
