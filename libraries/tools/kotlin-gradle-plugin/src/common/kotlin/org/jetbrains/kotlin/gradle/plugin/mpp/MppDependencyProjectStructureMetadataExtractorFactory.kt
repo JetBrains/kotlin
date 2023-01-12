bbb6c01a0ae81f5713c37381e61fd224908fa072
Anton Lakotka <anton.lakotka@jetbrains.com>
2023-01-12 10:13:13 +0100

    [Gradle] Add MppDependency[PSM]Factory
    Since ProjectMppDependency[PMS]Extractor is decoupled
    from Project it is possible to create MppDependency[PSM]Extractor just
    from `ResolvedArtifactResult` using its ComponentIdentifier and file.
    
    ^KT-49933

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/MppDependencyProjectStructureMetadataExtractorFactory.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/MppDependencyProjectStructureMetadataExtractorFactory.kt
@@ -8,10 +8,60 @@ package org.jetbrains.kotlin.gradle.plugin.mpp
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier
+import org.gradle.api.artifacts.result.ResolvedArtifactResult
 import org.gradle.api.artifacts.result.ResolvedComponentResult
 import org.gradle.api.artifacts.result.ResolvedDependencyResult
+import org.gradle.api.provider.Provider
+import org.jetbrains.kotlin.gradle.dsl.multiplatformExtensionOrNull
+import org.jetbrains.kotlin.gradle.plugin.addExtension
+import org.jetbrains.kotlin.gradle.plugin.findExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.toSingleKpmModuleIdentifier
 import org.jetbrains.kotlin.project.model.KpmModuleIdentifier
+
+class MppDependencyProjectStructureMetadataExtractorFactory(
+    private val projectStructureMetadataByProjectPath: Map<String, Provider<KotlinProjectStructureMetadata?>>
+) {
+    fun create(
+        metadataArtifact: ResolvedArtifactResult
+    ): MppDependencyProjectStructureMetadataExtractor {
+        val moduleId = metadataArtifact.variant.owner
+
+        if (moduleId is ProjectComponentIdentifier && moduleId.build.isCurrentBuild) {
+            val psm = projectStructureMetadataByProjectPath[moduleId.projectPath]
+                ?: error("Project structure metadata not found for project ${moduleId.projectPath}")
+            return ProjectMppDependencyProjectStructureMetadataExtractor(
+                moduleIdentifier = metadataArtifact.variant.toSingleKpmModuleIdentifier(),
+                projectPath = moduleId.projectPath,
+                projectStructureMetadataProvider = psm::get
+            )
+        }
+
+        return JarMppDependencyProjectStructureMetadataExtractor(metadataArtifact.file)
+    }
+
+    companion object {
+        private val extensionName = MppDependencyProjectStructureMetadataExtractorFactory::class.java.simpleName
+        fun getOrCreate(project: Project): MppDependencyProjectStructureMetadataExtractorFactory {
+            val existing = project.findExtension<MppDependencyProjectStructureMetadataExtractorFactory>(extensionName)
+            if (existing != null) return existing
+
+            val projectStructureMetadataByProjectPath = collectProjectStructureMetadataFromAllProjects(project)
+            val newFactory = MppDependencyProjectStructureMetadataExtractorFactory(projectStructureMetadataByProjectPath)
+            project.addExtension(extensionName, newFactory)
+            return newFactory
+        }
+
+        /**
+         * Collect Kotlin Project StructureMetadata only for TCS model.
+         * TODO: Add support for KPM and auxiliary modules
+         */
+        private fun collectProjectStructureMetadataFromAllProjects(project: Project): Map<String, Provider<KotlinProjectStructureMetadata?>> {
+            return project.rootProject.allprojects.associateBy { it.path }.mapValues { (_, subProject) ->
+                project.provider { subProject.multiplatformExtensionOrNull?.kotlinProjectStructureMetadata }
+            }
+        }
+    }
+}
 
 internal fun MppDependencyProjectStructureMetadataExtractor.Factory.create(
     project: Project,
