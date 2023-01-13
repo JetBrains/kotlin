74dd60925ca2dc478831093195aef6642db488e5
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2023-01-13 14:13:51 +0100

    [Wasm] External inheritance checker
    Check that non-external types can't extend external type

---
+++ js/js.frontend/src/org/jetbrains/kotlin/wasm/resolve/diagnostics/DefaultErrorMessagesWasm.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.wasm.resolve.diagnostics
+
+import org.jetbrains.kotlin.diagnostics.rendering.DefaultErrorMessages
+import org.jetbrains.kotlin.diagnostics.rendering.DiagnosticFactoryToRendererMap
+import org.jetbrains.kotlin.diagnostics.rendering.Renderers
+
+private val DIAGNOSTIC_FACTORY_TO_RENDERER by lazy {
+    with(DiagnosticFactoryToRendererMap("Wasm")) {
+        put(
+            ErrorsWasm.NON_EXTERNAL_TYPE_EXTENDS_EXTERNAL_TYPE,
+            "Non-external type extends external type {0}",
+            Renderers.RENDER_TYPE
+        )
+        this
+    }
+}
+
+class DefaultErrorMessagesWasm : DefaultErrorMessages.Extension {
+    override fun getMap(): DiagnosticFactoryToRendererMap = DIAGNOSTIC_FACTORY_TO_RENDERER
+}
