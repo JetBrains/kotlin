db17ee2d11af2bbb4e2987582a79effaac5232dc
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2023-01-13 14:18:26 +0100

    [Wasm] Remove unnecessary K/JS checks

--- js/js.frontend/src/org/jetbrains/kotlin/wasm/resolve/WasmPlatformConfigurator.kt
+++ js/js.frontend/src/org/jetbrains/kotlin/wasm/resolve/WasmPlatformConfigurator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -10,30 +10,22 @@ import org.jetbrains.kotlin.container.useImpl
 import org.jetbrains.kotlin.container.useInstance
 import org.jetbrains.kotlin.js.analyze.JsNativeDiagnosticSuppressor
 import org.jetbrains.kotlin.js.naming.NameSuggestion
-import org.jetbrains.kotlin.js.resolve.*
-import org.jetbrains.kotlin.js.resolve.NativeGetterChecker
-import org.jetbrains.kotlin.js.resolve.NativeInvokeChecker
-import org.jetbrains.kotlin.js.resolve.NativeSetterChecker
+import org.jetbrains.kotlin.js.resolve.ExtensionFunctionToExternalIsInlinable
 import org.jetbrains.kotlin.js.resolve.diagnostics.*
 import org.jetbrains.kotlin.resolve.PlatformConfiguratorBase
 import org.jetbrains.kotlin.resolve.calls.checkers.LateinitIntrinsicApplicabilityChecker
 import org.jetbrains.kotlin.resolve.checkers.ExpectedActualDeclarationChecker
-import org.jetbrains.kotlin.types.DynamicTypesAllowed
 
 object WasmPlatformConfigurator : PlatformConfiguratorBase(
-    DynamicTypesAllowed(),
     additionalDeclarationCheckers = listOf(
-        NativeInvokeChecker(), NativeGetterChecker(), NativeSetterChecker(),
         JsNameChecker, JsModuleChecker, JsExternalFileChecker,
-        JsExternalChecker, JsInheritanceChecker, JsMultipleInheritanceChecker,
+        JsExternalChecker,
         JsRuntimeAnnotationChecker,
-        JsDynamicDeclarationChecker,
         JsExportAnnotationChecker,
-        JsExportDeclarationChecker
+        JsExportDeclarationChecker,
     ),
     additionalCallCheckers = listOf(
         JsModuleCallChecker,
-        JsDynamicCallChecker,
         JsDefinedExternallyCallChecker,
         LateinitIntrinsicApplicabilityChecker(isWarningInPre19 = true)
     ),
@@ -41,11 +33,8 @@ object WasmPlatformConfigurator : PlatformConfiguratorBase(
     override fun configureModuleComponents(container: StorageComponentContainer) {
         container.useInstance(NameSuggestion())
         container.useImpl<JsCallChecker>()
-        container.useImpl<JsTypeSpecificityComparator>()
         container.useImpl<JsNameClashChecker>()
-        container.useImpl<JsIdentifierChecker>()
         container.useImpl<JsNameCharsChecker>()
-        container.useImpl<JsBuiltinNameClashChecker>()
         container.useInstance(JsModuleClassLiteralChecker)
         container.useImpl<JsReflectionAPICallChecker>()
         container.useImpl<JsNativeRttiChecker>()
