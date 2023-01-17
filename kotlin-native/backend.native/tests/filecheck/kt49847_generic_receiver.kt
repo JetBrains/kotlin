JetBrains/kotlin

commit 4a2a77d9b95a08763fd687b05fc4732ef294ebc8

[K/N] Make classes generated from λs that use generic parameters generic
Previously, a function reference that used generic parameters from its
outer scope was lowered into a top-level non-generic subclass of
`FunctionN`, with `FunctionN` type arguments referencing type parameters
not present in the scope anymore. This sometimes resulted in generating
malformed mangled names.

From now on the generated subclass of `FunctionN` is generic. The needed
type arguments are passed upon instantiation, when the relevant generic
parameters are present in the scope.

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/FunctionReferenceLowering.kt
@@ -10,7 +10,6 @@ import org.jetbrains.kotlin.backend.common.IrElementTransformerVoidWithContext
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
 import org.jetbrains.kotlin.backend.common.pop
 import org.jetbrains.kotlin.backend.common.push
-import org.jetbrains.kotlin.backend.konan.Context
 import org.jetbrains.kotlin.backend.konan.NativeGenerationState
 import org.jetbrains.kotlin.backend.konan.descriptors.synthesizedName
 import org.jetbrains.kotlin.backend.konan.llvm.computeFullName
@@ -29,6 +28,37 @@ import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 
+/**
+ * Transforms a function reference into a subclass of `kotlin.native.internal.KFunctionImpl` and `kotlin.FunctionN`,
+ * or `kotlin.native.internal.KSuspendFunctionImpl` and `kotlin.KSuspendFunctionN` (for suspend functions/lambdas),
+ * or `Any` (for simple lamdbas), or a custom superclass (in case of SAM conversion).
+ *
+ * For example, `::bar$lambda$0<BarTP>` in the following code:
+ * ```kotlin
+ * fun <FooTP> foo(v: FooTP, l: (FooTP) -> String): String {
+ *     return l(v)
+ * }
+ *
+ * private fun <T> bar$lambda$0(t: T): String { /* ... */ }
+ *
+ * fun <BarTP> bar(v: BarTP): String {
+ *     return foo(v, ::bar$lambda$0<BarTP>)
+ * }
+ * ```
+ *
+ * is lowered into:
+ * ```kotlin
+ * private class bar$lambda$0$FUNCTION_REFERENCE$0<T> : kotlin.native.internal.KFunctionImpl<String>, kotlin.Function1<T, String> {
+ *     override fun invoke(p1: T): String {
+ *         return bar$lambda$0<T>(p1)
+ *     }
+ * }
+ *
+ * fun <BarTP> bar(v: BarTP): String {
+ *     return foo(v, bar$lambda$0$FUNCTION_REFERENCE$0<BarTP>())
+ * }
+ * ```
+ */
 internal class FunctionReferenceLowering(val generationState: NativeGenerationState) : FileLoweringPass {
     private object DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL : IrDeclarationOriginImpl("FUNCTION_REFERENCE_IMPL")
 
@@ -173,18 +203,6 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
         private val boundFunctionParameters = functionReference.getArgumentsWithIr().map { it.first }
         private val unboundFunctionParameters = functionParameters - boundFunctionParameters
 
-        private val typeArgumentsMap = referencedFunction.typeParameters.associate { typeParam ->
-            typeParam.symbol to functionReference.getTypeArgument(typeParam.index)!!
-        }
-        private val functionParameterAndReturnTypes = (functionReference.type as IrSimpleType).arguments.map {
-            when (it) {
-                is IrTypeProjection -> it.type
-                else -> context.irBuiltIns.anyNType
-            }
-        }
-        private val functionParameterTypes = functionParameterAndReturnTypes.dropLast(1)
-        private val functionReturnType = functionParameterAndReturnTypes.last()
-
         private val isLambda = functionReference.origin.isLambda
         private val isKFunction = functionReference.type.isKFunction()
         private val isKSuspendFunction = functionReference.type.isKSuspendFunction()
@@ -201,11 +219,20 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
             visibility = DescriptorVisibilities.PRIVATE
         }.apply {
             parent = this@FunctionReferenceBuilder.parent
+            copyTypeParametersFrom(referencedFunction)
             createParameterDeclarations()
 
             // copy the generated name for IrClass, partially solves KT-47194
             generationState.copyLocalClassName(functionReference, this)
         }
+
+        private val typeArguments = functionReferenceClass.typeParameters.map { typeParam ->
+            typeParam.symbol to functionReference.getTypeArgument(typeParam.index)!!
+        }
+
+        private val typeParameterRemapper = IrTypeParameterRemapper(referencedFunction.typeParameters.zip(functionReferenceClass.typeParameters).toMap())
+        private val functionParameterTypes = unboundFunctionParameters.map { typeParameterRemapper.remapType(it.type) }
+        private val functionReturnType = typeParameterRemapper.remapType(referencedFunction.returnType)
 
         private val functionReferenceThis = functionReferenceClass.thisReceiver!!
 
@@ -219,8 +246,6 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
                 isFinal = true
             }
         }
-
-        private fun IrClass.getInvokeFunction() = simpleFunctions().single { it.name.asString() == "invoke" }
 
         private val kFunctionImplSymbol = symbols.kFunctionImpl
         private val kFunctionImplConstructorSymbol = kFunctionImplSymbol.constructors.single()
@@ -241,14 +266,15 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
                 transformedSuperMethod = samSuperClass.functions.single { it.owner.modality == Modality.ABSTRACT }.owner
             } else {
                 val numberOfParameters = unboundFunctionParameters.size
+                val functionParameterAndReturnTypes = functionParameterTypes + functionReturnType
                 if (isKSuspendFunction) {
                     val suspendFunctionClass = symbols.kSuspendFunctionN(numberOfParameters).owner
                     superTypes += suspendFunctionClass.typeWith(functionParameterAndReturnTypes)
-                    transformedSuperMethod = suspendFunctionClass.getInvokeFunction()
+                    transformedSuperMethod = suspendFunctionClass.invokeFun!!
                 } else {
                     val functionClass = (if (isKFunction) symbols.kFunctionN(numberOfParameters) else symbols.functionN(numberOfParameters)).owner
                     superTypes += functionClass.typeWith(functionParameterAndReturnTypes)
-                    transformedSuperMethod = functionClass.getInvokeFunction()
+                    transformedSuperMethod = functionClass.invokeFun!!
                 }
             }
             val originalSuperMethod = context.mapping.functionWithContinuationsToSuspendFunctions[transformedSuperMethod] ?: transformedSuperMethod
@@ -314,6 +340,8 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
                     ignoredParentSymbols = listOf(transformedSuperMethod.symbol)
             )
 
+            functionReferenceClass.remapTypes(typeParameterRemapper)
+
             return functionReferenceClass
         }
 
@@ -323,6 +351,7 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
             origin = DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL
             isPrimary = true
         }.apply {
+            val typeArgumentsMap = typeArguments.toMap()
             valueParameters += boundFunctionParameters.mapIndexed { index, parameter ->
                 parameter.copyTo(this, DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL, index,
                         type = parameter.type.substitute(typeArgumentsMap))
@@ -347,10 +376,11 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
             val clazz = buildClass()
             val constructor = buildConstructor()
             val arguments = functionReference.getArgumentsWithIr()
+            val typeArguments = typeArguments.map { it.second }
             val expression = if (arguments.isEmpty()) {
-                irBuilder.irConstantObject(clazz, emptyMap())
+                irBuilder.irConstantObject(clazz, emptyMap(), typeArguments)
             } else {
-                irBuilder.irCall(constructor).apply {
+                irBuilder.irCallConstructor(constructor.symbol, typeArguments).apply {
                     arguments.forEachIndexed { index, argument ->
                         putValueArgument(index, argument.second)
                     }
@@ -450,7 +480,7 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
                             assert(unboundIndex == valueParameters.size) { "Not all arguments of <invoke> are used" }
 
                             referencedFunction.typeParameters.forEach { typeParam ->
-                                putTypeArgument(typeParam.index, functionReference.getTypeArgument(typeParam.index)!!)
+                                putTypeArgument(typeParam.index, functionReferenceClass.typeParameters[typeParam.index].defaultType)
                             }
                         }
                 )

compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirBlackBoxCodegenTestGenerated.java
@@ -6198,6 +6198,12 @@ public class FirBlackBoxCodegenTestGenerated extends AbstractFirBlackBoxCodegenT
         }
 
         @Test
+        @TestMetadata("closureCapturingGenericParam.kt")
+        public void testClosureCapturingGenericParam() throws Exception {
+            runTest("compiler/testData/codegen/box/closures/closureCapturingGenericParam.kt");
+        }
+
+        @Test
         @TestMetadata("closureInsideConstrucor.kt")
         public void testClosureInsideConstrucor() throws Exception {
             runTest("compiler/testData/codegen/box/closures/closureInsideConstrucor.kt");

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrTypeParameterRemapper.kt
@@ -13,11 +13,12 @@ import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
 import org.jetbrains.kotlin.ir.types.impl.IrTypeAbbreviationImpl
 import org.jetbrains.kotlin.ir.types.impl.makeTypeProjection
 
-/* After moving an IrElement, some type parameter references within it may become out of scope.
-   This remapper restores validity by redirecting those references to new type parameters.
+/**
+ * After moving an [org.jetbrains.kotlin.ir.IrElement], some type parameter references within it may become out of scope.
+ * This remapper restores validity by redirecting those references to new type parameters.
  */
 class IrTypeParameterRemapper(
-    val typeParameterMap: Map<IrTypeParameter, IrTypeParameter>
+    private val typeParameterMap: Map<IrTypeParameter, IrTypeParameter>
 ) : TypeRemapper {
     override fun enterScope(irTypeParametersContainer: IrTypeParametersContainer) {}
     override fun leaveScope() {}

compiler/testData/codegen/box/closures/closureCapturingGenericParam.kt
@@ -1,0 +1,20 @@
+interface IntConvertible {
+    fun toInt(): Int
+}
+
+fun <FooTP> foo(init: Int, v: FooTP, l: Int.(FooTP) -> Int) = init.l(v)
+
+fun <BarTP : IntConvertible> computeSum(array: Array<BarTP>) = foo(0, array) {
+    var res = this
+    for (element in it) res += element.toInt()
+    res
+}
+
+class N(val v: Int) : IntConvertible {
+    override fun toInt() = v
+}
+
+fun box(): String {
+    if (computeSum(arrayOf(N(2), N(14))) != 16) return "Fail"
+    return "OK"
+}

compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/BlackBoxCodegenTestGenerated.java
@@ -6042,6 +6042,12 @@ public class BlackBoxCodegenTestGenerated extends AbstractBlackBoxCodegenTest {
         }
 
         @Test
+        @TestMetadata("closureCapturingGenericParam.kt")
+        public void testClosureCapturingGenericParam() throws Exception {
+            runTest("compiler/testData/codegen/box/closures/closureCapturingGenericParam.kt");
+        }
+
+        @Test
         @TestMetadata("closureInsideConstrucor.kt")
         public void testClosureInsideConstrucor() throws Exception {
             runTest("compiler/testData/codegen/box/closures/closureInsideConstrucor.kt");

compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -6198,6 +6198,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         }
 
         @Test
+        @TestMetadata("closureCapturingGenericParam.kt")
+        public void testClosureCapturingGenericParam() throws Exception {
+            runTest("compiler/testData/codegen/box/closures/closureCapturingGenericParam.kt");
+        }
+
+        @Test
         @TestMetadata("closureInsideConstrucor.kt")
         public void testClosureInsideConstrucor() throws Exception {
             runTest("compiler/testData/codegen/box/closures/closureInsideConstrucor.kt");

compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -5277,6 +5277,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
             runTest("compiler/testData/codegen/box/closures/capturedLocalGenericFun.kt");
         }
 
+        @TestMetadata("closureCapturingGenericParam.kt")
+        public void testClosureCapturingGenericParam() throws Exception {
+            runTest("compiler/testData/codegen/box/closures/closureCapturingGenericParam.kt");
+        }
+
         @TestMetadata("closureInsideConstrucor.kt")
         public void testClosureInsideConstrucor() throws Exception {
             runTest("compiler/testData/codegen/box/closures/closureInsideConstrucor.kt");

js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/JsCodegenBoxTestGenerated.java
@@ -4548,6 +4548,12 @@ public class JsCodegenBoxTestGenerated extends AbstractJsCodegenBoxTest {
         }
 
         @Test
+        @TestMetadata("closureCapturingGenericParam.kt")
+        public void testClosureCapturingGenericParam() throws Exception {
+            runTest("compiler/testData/codegen/box/closures/closureCapturingGenericParam.kt");
+        }
+
+        @Test
         @TestMetadata("closureInsideConstrucor.kt")
         public void testClosureInsideConstrucor() throws Exception {
             runTest("compiler/testData/codegen/box/closures/closureInsideConstrucor.kt");

commit 150b8061e9ae1a68e48278bd6d384d41f462a55e

[K/N] Fix FunctionReferenceLowering for bound refs to generic functions
4a2a77d9b95a08763fd687b05fc4732ef294ebc8 introduced a regression
where for bound references to functions generic over their receiver
the generated class was also generic even if the function reference
itself only contained concrete types. This is fixed here.

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/FunctionReferenceLowering.kt
@@ -5,11 +5,8 @@
 
 package org.jetbrains.kotlin.backend.konan.lower
 
-import org.jetbrains.kotlin.backend.common.FileLoweringPass
-import org.jetbrains.kotlin.backend.common.IrElementTransformerVoidWithContext
+import org.jetbrains.kotlin.backend.common.*
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
-import org.jetbrains.kotlin.backend.common.pop
-import org.jetbrains.kotlin.backend.common.push
 import org.jetbrains.kotlin.backend.konan.NativeGenerationState
 import org.jetbrains.kotlin.backend.konan.descriptors.synthesizedName
 import org.jetbrains.kotlin.backend.konan.llvm.computeFullName
@@ -22,6 +19,7 @@ import org.jetbrains.kotlin.ir.builders.declarations.*
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.expressions.impl.IrInstanceInitializerCallImpl
+import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
 import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
@@ -211,6 +209,32 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
 
         private val functionReferenceTarget = adaptedReferenceOriginalTarget ?: referencedFunction
 
+        /**
+         * The first element of a pair is a type parameter of [referencedFunction], the second element is its argument in
+         * [functionReference], to be passed upon instantiation of the function reference class.
+         */
+        private val allTypeParametersAndArguments: List<Pair<IrTypeParameterSymbol, IrType>> =
+                referencedFunction.typeParameters.map { typeParam ->
+                    typeParam.symbol to functionReference.getTypeArgument(typeParam.index)!!
+                }
+
+        /**
+         * @see allTypeParametersAndArguments
+         */
+        private val allTypeParametersAndArgumentsMap: Map<IrTypeParameterSymbol, IrType> = allTypeParametersAndArguments.toMap()
+
+        /**
+         * The type arguments of [functionReference] that are not concrete types,
+         * but are themselves type parameters coming from an enclosing scope.
+         *
+         * The first element of a pair is a type parameter of [referencedFunction], the second element is its argument in
+         * [functionReference].
+         *
+         * [functionReferenceClass] is only generic over these type parameters.
+         */
+        private val typeParametersAndArgumentsFromEnclosingScope: List<Pair<IrTypeParameterSymbol, IrType>> =
+                allTypeParametersAndArguments.filter { it.second.isTypeParameter() }
+
         private val functionReferenceClass = irFactory.buildClass {
             startOffset = this@FunctionReferenceBuilder.startOffset
             endOffset = this@FunctionReferenceBuilder.endOffset
@@ -219,22 +243,42 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
             visibility = DescriptorVisibilities.PRIVATE
         }.apply {
             parent = this@FunctionReferenceBuilder.parent
-            copyTypeParametersFrom(referencedFunction)
+
+            // The function reference class only needs to be generic over type parameters coming from an enclosing scope.
+            copyTypeParameters(typeParametersAndArgumentsFromEnclosingScope.map { it.first.owner })
             createParameterDeclarations()
 
             // copy the generated name for IrClass, partially solves KT-47194
             generationState.copyLocalClassName(functionReference, this)
         }
 
-        private val typeArguments = functionReferenceClass.typeParameters.map { typeParam ->
-            typeParam.symbol to functionReference.getTypeArgument(typeParam.index)!!
-        }
+        /**
+         * Remaps type parameters of [referencedFunction] to type parameters of [functionReferenceClass].
+         *
+         * Note: this is not a 1-to-1 mapping. Some type parameters of [referencedFunction] may be bound to a concrete type, in this case
+         * there will be no corresponding type parameter in [functionReferenceClass].
+         */
+        private val typeParameterRemapper = IrTypeParameterRemapper(
+                typeParametersAndArgumentsFromEnclosingScope.map { it.first.owner }.zip(functionReferenceClass.typeParameters).toMap()
+        )
 
-        private val typeParameterRemapper = IrTypeParameterRemapper(referencedFunction.typeParameters.zip(functionReferenceClass.typeParameters).toMap())
         private val functionParameterTypes = unboundFunctionParameters.map { typeParameterRemapper.remapType(it.type) }
         private val functionReturnType = typeParameterRemapper.remapType(referencedFunction.returnType)
 
         private val functionReferenceThis = functionReferenceClass.thisReceiver!!
+
+        /**
+         * Substitutes a bound value parameter's [type] with a new type according to the following rules:
+         *
+         * - If [type] is a type parameter from an enclosing scope (i.e. for which we don't know the concrete type), replace it with
+         *   the corresponding [functionReferenceClass]'s type parameter.
+         * - If this value parameter's type is a type parameter for which we know the concrete type, replace it with
+         *   the concrete type. For example, consider the `5::foo` function reference where `foo` is declared as `fun <T> T.foo()`. Here,
+         *   `T` will be replaced with `Int`.
+         * - Otherwise, just return [type] itself.
+         */
+        private fun substituteBoundValueParameterType(type: IrType): IrType =
+                typeParameterRemapper.remapType(type).substitute(allTypeParametersAndArgumentsMap)
 
         private val argumentToPropertiesMap = boundFunctionParameters.associateWith {
             functionReferenceClass.addField {
@@ -242,7 +286,7 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
                 endOffset = this@FunctionReferenceBuilder.endOffset
                 origin = DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL
                 name = it.name
-                type = it.type
+                type = substituteBoundValueParameterType(it.type)
                 isFinal = true
             }
         }
@@ -253,6 +297,17 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
         private val kSuspendFunctionImplConstructorSymbol = kSuspendFunctionImplSymbol.constructors.single()
 
         private fun buildClass(): IrClass {
+            if (unboundFunctionParameters.size != (functionReference.type as IrSimpleType).arguments.size - 1) {
+                compilationException(
+                        "The number of unbound value parameters of the function reference should match the number of type arguments " +
+                                "of the K[Suspend]FunctionN superclass minus one.\n\n" +
+                                "Unbound function parameters:\n" +
+                                unboundFunctionParameters.joinToString(separator = "\n", transform = IrElement::render) +
+                                "\n\nFunction reference type: " +
+                                functionReference.type.render(),
+                        functionReference
+                )
+            }
             val superClass = when {
                 isKSuspendFunction -> kSuspendFunctionImplSymbol.typeWith(functionReturnType)
                 isLambda -> irBuiltIns.anyType
@@ -351,10 +406,13 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
             origin = DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL
             isPrimary = true
         }.apply {
-            val typeArgumentsMap = typeArguments.toMap()
             valueParameters += boundFunctionParameters.mapIndexed { index, parameter ->
-                parameter.copyTo(this, DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL, index,
-                        type = parameter.type.substitute(typeArgumentsMap))
+                parameter.copyTo(
+                        this,
+                        DECLARATION_ORIGIN_FUNCTION_REFERENCE_IMPL,
+                        index,
+                        type = substituteBoundValueParameterType(parameter.type)
+                )
             }
 
             body = context.createIrBuilder(symbol, startOffset, endOffset).irBlockBody {
@@ -376,7 +434,7 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
             val clazz = buildClass()
             val constructor = buildConstructor()
             val arguments = functionReference.getArgumentsWithIr()
-            val typeArguments = typeArguments.map { it.second }
+            val typeArguments = typeParametersAndArgumentsFromEnclosingScope.map { it.second }
             val expression = if (arguments.isEmpty()) {
                 irBuilder.irConstantObject(clazz, emptyMap(), typeArguments)
             } else {
@@ -480,7 +538,7 @@ internal class FunctionReferenceLowering(val generationState: NativeGenerationSt
                             assert(unboundIndex == valueParameters.size) { "Not all arguments of <invoke> are used" }
 
                             referencedFunction.typeParameters.forEach { typeParam ->
-                                putTypeArgument(typeParam.index, functionReferenceClass.typeParameters[typeParam.index].defaultType)
+                                putTypeArgument(typeParam.index, substituteBoundValueParameterType(typeParam.defaultType))
                             }
                         }
                 )

kotlin-native/backend.native/tests/filecheck/kt49847_generic_receiver.kt
@@ -3,11 +3,26 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-// CHECK-LABEL: define internal void @"kfun:$foo$FUNCTION_REFERENCE$0.<init>#internal"
-// CHECK-SAME: i32
-
 fun <T> T.foo() { println(this) }
 
-fun main() {
-    println(5::foo)
+// CHECK-LABEL: define void @"kfun:#bar(0:0){0\C2\A7<kotlin.Any?>}"
+// CHECK-SAME: (%struct.ObjHeader* [[x:%[0-9]+]])
+fun <BarTP> bar(x: BarTP) {
+    // CHECK: call void @"kfun:$foo$FUNCTION_REFERENCE$0.<init>#internal"(%struct.ObjHeader* {{%[0-9]+}}, %struct.ObjHeader* [[x]])
+    println(x::foo)
 }
+
+// CHECK-LABEL: define void @"kfun:#main(){}"
+fun main() {
+    // CHECK: call void @"kfun:$foo$FUNCTION_REFERENCE$1.<init>#internal"(%struct.ObjHeader* {{%[0-9]+}}, i32 5)
+    println(5::foo)
+
+    bar("hello")
+    bar(42)
+}
+
+// CHECK-LABEL: define internal void @"kfun:$foo$FUNCTION_REFERENCE$0.<init>#internal"
+// CHECK-SAME: (%struct.ObjHeader* {{%[0-9]+}}, %struct.ObjHeader* {{%[0-9]+}})
+
+// CHECK-LABEL: define internal void @"kfun:$foo$FUNCTION_REFERENCE$1.<init>#internal"
+// CHECK-SAME: (%struct.ObjHeader* {{%[0-9]+}}, i32 {{%[0-9]+}})