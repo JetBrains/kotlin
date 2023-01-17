b7c8b6bf27f47b8d304da75f05db5880c48d9133
vladislav.grechko <vladislav.grechko@jetbrains.com>
2023-01-17 18:48:49 +0100

    Improve vArray reified type argument

--- compiler/testData/codegen/box/arrays/vArrays/vArrayReifiedTypeArgument.kt
+++ compiler/testData/codegen/box/arrays/vArrays/vArrayReifiedTypeArgument.kt
@@ -30,10 +30,27 @@ fun box(): String {
     if (!testVArray(0.toDouble(), 1.toDouble())) return "Fail 7"
     if (!testVArray('a', 'b')) return "Fail 8"
 
-    if (!testVArray("a", "b")) return "Fail 9"
-    if (!testVArray(IcInt(0), IcInt(1))) return "Fail 10"
-    if (!testVArray(0, IcInt(1))) return "Fail 11"
-    if (!testVArray(0, "a")) return "Fail 12"
+    if (!testVArray(0.toUByte(), 1.toUByte())) return "Fail 9"
+    if (!testVArray(0.toUShort(), 1.toUShort())) return "Fail 10"
+    if (!testVArray(0.toUInt(), 1.toUInt())) return "Fail 11"
+    if (!testVArray(0.toULong(), 0.toULong())) return "Fail 12"
+
+    if (!testVArray("a", "b")) return "Fail 13"
+    if (!testVArray(IcInt(0), IcInt(1))) return "Fail 14"
+    if (!testVArray(0, IcInt(1))) return "Fail 15"
+    if (!testVArray(0, "a")) return "Fail 16"
 
     return "OK"
 }
+
+// CHECK_BYTECODE_TEXT
+// 1 NEWARRAY T_BOOLEAN
+// 2 NEWARRAY T_BYTE
+// 2 NEWARRAY T_SHORT
+// 3 NEWARRAY T_INT
+// 2 NEWARRAY T_LONG
+// 1 NEWARRAY T_FLOAT
+// 1 NEWARRAY T_DOUBLE
+// 1 NEWARRAY T_CHAR
+// 1 ANEWARRAY java/lang/String
+// 3 ANEWARRAY java/lang/Object
