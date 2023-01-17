adc89ba1fb768b73fde5b45db85a997be6d5a999
vladislav.grechko <vladislav.grechko@jetbrains.com>
2023-01-17 19:24:30 +0100

    Improve vArray creation test

--- compiler/testData/codegen/box/arrays/vArrays/vArrayCreation.kt
+++ compiler/testData/codegen/box/arrays/vArrays/vArrayCreation.kt
@@ -13,32 +13,45 @@ value class ICChar(val x: Char)
 @JvmInline
 value class ICICChar(val x: ICChar)
 
+inline fun <reified T> testVArray(initVal: T): Boolean {
+    val array = VArray(2) { initVal }
+    return array[0] == initVal && array[1] == initVal
+}
+
 fun box(): String {
-    val aInt = VArray(2) { it + 1 }
-    if (aInt[1] != 2) return "Fail 1"
 
-    val aStr = VArray(2) { "a" }
-    if (aStr[1] != "a") return "Fail 2"
+    if (!testVArray(true)) return "Fail 1"
+    if (!testVArray(1.toByte())) return "Fail 2"
+    if (!testVArray(2.toShort())) return "Fail 3"
+    if (!testVArray(3)) return "Fail 4"
+    if (!testVArray(4.toLong())) return "Fail 5"
+    if (!testVArray(5.toFloat())) return "Fail 6"
+    if (!testVArray(6.toDouble())) return "Fail 7"
+    if (!testVArray('a')) return "Fail 8"
 
-    val aUByte = VArray<UByte>(2) { UByte.MAX_VALUE }
-    if (aUByte[1] != UByte.MAX_VALUE) return "Fail 3"
+    if (!testVArray(1.toUByte())) return "Fail 9"
+    if (!testVArray(2.toUShort())) return "Fail 10"
+    if (!testVArray(3.toUInt())) return "Fail 11"
+    if (!testVArray(4.toULong())) return "Fail 12"
 
-    val aULongNullable = VArray<ULong?>(2) { null }
-    if (aULongNullable[1] != null) return "Fail 4"
-
-    val aICDoubleNullable = VArray<ICDoubleNullable>(2) { ICDoubleNullable(42.0) }
-    if (aICDoubleNullable[1].x != 42.0) return "Fail 5"
-
-    val aICChar = VArray(2) { ICICChar(ICChar('a')) }
-    if (aICChar[1].x.x != 'a') return "Fail 6"
+    if (!testVArray("a")) return "Fail 13"
+    if (!testVArray<ULong?>(null)) return "Fail 14"
+    if (!testVArray<ICDoubleNullable>(ICDoubleNullable(42.0))) return "Fail 15"
+    if (!testVArray(ICICChar(ICChar('a')))) return "Fail 16"
 
     return "OK"
 }
 
 // CHECK_BYTECODE_TEXT
-// 1 LOCALVARIABLE aInt \[I
-// 1 LOCALVARIABLE aStr \[Ljava/lang/String;
-// 1 LOCALVARIABLE aUByte \[B
-// 1 LOCALVARIABLE aULongNullable \[Lkotlin/ULong;
-// 1 LOCALVARIABLE aICDoubleNullable \[Ljava/lang/Double;
-// 1 LOCALVARIABLE aICChar \[C
+// 1 NEWARRAY T_BOOLEAN
+// 2 NEWARRAY T_BYTE
+// 2 NEWARRAY T_SHORT
+// 2 NEWARRAY T_INT
+// 2 NEWARRAY T_LONG
+// 1 NEWARRAY T_FLOAT
+// 1 NEWARRAY T_DOUBLE
+// 2 NEWARRAY T_CHAR
+// 1 ANEWARRAY java/lang/String
+// 1 ANEWARRAY kotlin/ULong
+// 1 ANEWARRAY java/lang/Double
+// 1 ANEWARRAY java/lang/Object
