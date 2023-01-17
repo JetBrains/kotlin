b1f51c4cd3dd9aea56a3bf28a81780f469595209
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2023-01-17 13:58:33 +0200

    [MPP] Report deprecation warnings for HMPP-related Gradle properties
    KT-55891
    
    (cherry picked from commit ec5f56584cb9c61058b96834ba788d462644f5cf)

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppDeprecatedPropertiesIt.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.mpp
+
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+import kotlin.io.path.appendText
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+@MppGradlePluginTests
+class MppDeprecatedPropertiesIt : KGPBaseTest() {
+    @GradleTest
+    fun testDeprecations(gradleVersion: GradleVersion) {
+        project("mppDeprecatedProperties", gradleVersion) {
+            checkDeprecations(isDeprecationExpected = false)
+
+            this.gradleProperties.appendText(
+                defaultFlags.entries.joinToString(
+                    prefix = System.lineSeparator(),
+                    postfix = System.lineSeparator(),
+                    separator = System.lineSeparator(),
+                ) { (prop, value) -> "$prop=$value" }
+            )
+
+            checkDeprecations(isDeprecationExpected = true)
+        }
+    }
+
+    private fun TestProject.checkDeprecations(isDeprecationExpected: Boolean) {
+        build {
+            val assert: (Boolean, String) -> Unit = if (isDeprecationExpected) ::assertTrue else ::assertFalse
+            val warnings = output.lines().filter { it.startsWith("w:") }.toSet()
+
+            defaultFlags.keys.forEach { flag ->
+                assert(
+                    warnings.any { warning -> Regex(".*$flag.*is deprecated.*").matches(warning) },
+                    "A deprecation warning for the '$flag' should have been reported",
+                )
+            }
+        }
+    }
+
+    private val defaultFlags: Map<String, String>
+        get() = mapOf(
+            "kotlin.mpp.enableGranularSourceSetsMetadata" to "true",
+            "kotlin.mpp.enableCompatibilityMetadataVariant" to "false",
+            "kotlin.internal.mpp.hierarchicalStructureByDefault" to "true",
+            "kotlin.mpp.hierarchicalStructureSupport" to "true",
+            "kotlin.native.enableDependencyPropagation" to "false",
+        )
+}
