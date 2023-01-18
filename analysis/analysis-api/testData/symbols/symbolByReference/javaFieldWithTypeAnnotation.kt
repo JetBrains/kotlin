a49b72b05ed6801dcd49d7365719bb7134eacf1b
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-01-18 13:00:35 +0100

    [AA] update testData
    ^KTIJ-23547
    ^KTIJ-24141

--- analysis/analysis-api/testData/symbols/symbolByReference/javaFieldWithTypeAnnotation.kt
+++ analysis/analysis-api/testData/symbols/symbolByReference/javaFieldWithTypeAnnotation.kt
@@ -1,8 +1,6 @@
 // DO_NOT_CHECK_SYMBOL_RESTORE_K1
+// WITH_STDLIB
 // FILE: main.kt
-@Target(AnnotationTarget.TYPE)
-annotation class Anno1
-
 fun some() {
     val jClass = JavaClass()
     jClass.<caret>field;
@@ -11,4 +9,13 @@ fun some() {
 // FILE: JavaClass.java
 public class JavaClass {
     public @Anno1 String field = 1;
+}
+
+// FILE: Anno1.java
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE_USE)
+public @interface Anno1 {
+
 }
