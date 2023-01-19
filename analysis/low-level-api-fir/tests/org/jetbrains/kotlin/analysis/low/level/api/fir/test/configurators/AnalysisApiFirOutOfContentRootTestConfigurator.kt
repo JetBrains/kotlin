bcf8827eb46c63abac40b19170d5f8a0adde4c13
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-01-19 18:37:54 +0900

    [LL API] Make 'KtNotUnderContentRootModuleForTest' depend on built-ins

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/test/configurators/AnalysisApiFirOutOfContentRootTestConfigurator.kt
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/test/configurators/AnalysisApiFirOutOfContentRootTestConfigurator.kt
@@ -6,13 +6,23 @@
 package org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.analysis.api.standalone.base.project.structure.KtModuleWithFiles
+import org.jetbrains.kotlin.analysis.low.level.api.fir.project.structure.LLFirBuiltinsSessionFactory
+import org.jetbrains.kotlin.analysis.project.structure.KtBuiltinsModule
+import org.jetbrains.kotlin.analysis.project.structure.KtModule
+import org.jetbrains.kotlin.analysis.project.structure.KtNotUnderContentRootModule
 import org.jetbrains.kotlin.analysis.test.framework.project.structure.KtModuleFactory
-import org.jetbrains.kotlin.analysis.test.framework.project.structure.KtOutOfContentRootModuleFactory
+import org.jetbrains.kotlin.analysis.test.framework.project.structure.TestModuleStructureFactory
 import org.jetbrains.kotlin.analysis.test.framework.utils.SkipTestException
+import org.jetbrains.kotlin.platform.TargetPlatform
+import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.directives.model.DirectiveApplicability
 import org.jetbrains.kotlin.test.directives.model.SimpleDirectivesContainer
+import org.jetbrains.kotlin.test.getAnalyzerServices
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.TestServices
 
@@ -46,3 +56,41 @@ object AnalysisApiFirOutOfContentRootTestConfigurator : AnalysisApiFirSourceLike
 }
 
 private class SkipWhenOutOfContentRootException : SkipTestException()
+
+private class KtOutOfContentRootModuleFactory : KtModuleFactory {
+    override fun createModule(testModule: TestModule, testServices: TestServices, project: Project): KtModuleWithFiles {
+        val psiFiles = TestModuleStructureFactory.createSourcePsiFiles(testModule, testServices, project)
+        val platform = testModule.targetPlatform
+        val module = KtNotUnderContentRootModuleForTest(testModule.name, psiFiles.single(), platform)
+        return KtModuleWithFiles(module, psiFiles)
+    }
+}
+
+private class KtNotUnderContentRootModuleForTest(
+    override val name: String,
+    override val file: PsiFile,
+    override val platform: TargetPlatform
+) : KtNotUnderContentRootModule {
+    override val directRegularDependencies: List<KtModule> by lazy {
+        val builtinsModule = LLFirBuiltinsSessionFactory.getInstance(project).getBuiltinsSession(platform).ktModule as KtBuiltinsModule
+        listOf(builtinsModule)
+    }
+
+    override val directRefinementDependencies: List<KtModule>
+        get() = emptyList()
+
+    override val directFriendDependencies: List<KtModule>
+        get() = emptyList()
+
+    override val contentScope: GlobalSearchScope
+        get() = GlobalSearchScope.fileScope(file)
+
+    override val analyzerServices: PlatformDependentAnalyzerServices
+        get() = platform.getAnalyzerServices()
+
+    override val project: Project
+        get() = file.project
+
+    override val moduleDescription: String
+        get() = "Not under content root \"${name}\" for ${file.virtualFile.path}"
+}
