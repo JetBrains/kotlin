74ed78e81ad23d53c5dd4a4befee60d86112dad5
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-01-23 13:31:55 +0100

    [Gradle] Add MppCompositeBuildIT with host specific targets
    ... to cover KT-56111
    
    (cherry picked from commit 24db9f36809c39cdc013ba76737d8dfeb9104b22)

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/mpp-composite-build/sample2-withHostSpecificTargets/consumerBuild/consumerA/build.gradle.kts
@@ -0,0 +1,16 @@
+@file:Suppress("OPT_IN_USAGE")
+
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    targetHierarchy.default()
+    jvm()
+    iosX64()
+    iosArm64()
+
+    sourceSets.commonMain.get().dependencies {
+        implementation("org.jetbrains.sample:producerA:1.0.0-SNAPSHOT")
+    }
+}
---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/mpp-composite-build/sample2-withHostSpecificTargets/producerBuild/producerA/src/iosX64Main/kotlin/ProducerAIosX64.kt
@@ -0,0 +1,1 @@
+object ProducerAIosX64
