d796355f556d011b16ae9c9339ae82ffd3df4cec
Anton Lakotka <anton.lakotka@jetbrains.com>
2023-01-25 21:00:55 +0100

    [Gradle] Deprecate case when commonMain depends on another source set
    ^KT-55824 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/internal/deprecationDiagnostics.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/internal/deprecationDiagnostics.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.gradle.plugin.mpp.internal
 
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.dsl.multiplatformExtensionOrNull
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.Companion.kotlinPropertiesProvider
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.PropertyNames.KOTLIN_MPP_ENABLE_COMPATIBILITY_METADATA_VARIANT
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.PropertyNames.KOTLIN_MPP_ENABLE_GRANULAR_SOURCE_SETS_METADATA
@@ -14,30 +15,35 @@ import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.PropertyNames.KOTLI
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.PropertyNames.KOTLIN_MPP_HIERARCHICAL_STRUCTURE_SUPPORT
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider.PropertyNames.KOTLIN_NATIVE_DEPENDENCY_PROPAGATION
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.gradle.plugin.whenEvaluated
 import org.jetbrains.kotlin.gradle.utils.SingleWarningPerBuild
-import org.jetbrains.kotlin.gradle.utils.runProjectConfigurationHealthCheck
 import org.jetbrains.kotlin.gradle.utils.runProjectConfigurationHealthCheckWhenEvaluated
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.tooling.core.UnsafeApi
 
-internal fun checkAndReportDeprecatedNativeTargets(project: Project) {
+internal fun runDeprecationDiagnostics(project: Project) {
+    checkAndReportDeprecatedMppProperties(project)
+    handleHierarchicalStructureFlagsMigration(project)
     project.runProjectConfigurationHealthCheckWhenEvaluated {
-        val targets = project.extensions.getByType(KotlinMultiplatformExtension::class.java).targets
-        val usedDeprecatedTargets = targets.filter { it is KotlinNativeTarget && it.konanTarget in KonanTarget.deprecatedTargets }
-        if (usedDeprecatedTargets.isEmpty()) return@runProjectConfigurationHealthCheckWhenEvaluated
-        SingleWarningPerBuild.show(
-            project,
-            "w: The following deprecated kotlin native targets were used in the project: ${usedDeprecatedTargets.joinToString { it.targetName }}"
-        )
+        checkAndReportDeprecatedNativeTargets(project)
+        checkAndReportDeprecatedSourceSetsLayouts(project)
     }
+}
+
+private fun checkAndReportDeprecatedNativeTargets(project: Project) {
+    val targets = project.extensions.getByType(KotlinMultiplatformExtension::class.java).targets
+    val usedDeprecatedTargets = targets.filter { it is KotlinNativeTarget && it.konanTarget in KonanTarget.deprecatedTargets }
+    if (usedDeprecatedTargets.isEmpty()) return
+    SingleWarningPerBuild.show(
+        project,
+        "w: The following deprecated kotlin native targets were used in the project: ${usedDeprecatedTargets.joinToString { it.targetName }}"
+    )
 }
 
 /**
  * Declared properties have to be captured during plugin application phase before the HMPP migration util sets them.
  * Warnings have to be reported only for successfully evaluated projects without errors.
  */
-internal fun checkAndReportDeprecatedMppProperties(project: Project) {
+private fun checkAndReportDeprecatedMppProperties(project: Project) {
     val projectProperties = project.kotlinPropertiesProvider
     if (projectProperties.ignoreHmppDeprecationWarnings == true) return
 
@@ -51,6 +57,20 @@ internal fun checkAndReportDeprecatedMppProperties(project: Project) {
 
     warnings.forEach { message ->
         SingleWarningPerBuild.show(project, message)
+    }
+}
+
+private fun checkAndReportDeprecatedSourceSetsLayouts(project: Project) {
+    project.reportCommonMainDependsOnOtherSourceSets()
+}
+
+private fun Project.reportCommonMainDependsOnOtherSourceSets() {
+    val commonMain = multiplatformExtensionOrNull?.sourceSets?.getByName("commonMain") ?: return
+    if (commonMain.dependsOn.isNotEmpty()) {
+        SingleWarningPerBuild.show(
+            project,
+            "w: 'commonMain' source set can't depend on other source sets."
+        )
     }
 }
 
