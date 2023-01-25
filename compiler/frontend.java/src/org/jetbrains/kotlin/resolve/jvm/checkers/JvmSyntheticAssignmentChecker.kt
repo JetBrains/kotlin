3eedb7c87d325d6fd10e395cbb9e42cf66514505
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2023-01-25 17:56:13 +0100

    K1: report also TYPE_MISMATCH_WARNING in JvmSyntheticAssignmentChecker
    #KT-56061 Fixed
    
    (cherry picked from commit cab034049777cd1779b6c38b0a21e5439427ac56)

--- compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/checkers/JvmSyntheticAssignmentChecker.kt
+++ compiler/frontend.java/src/org/jetbrains/kotlin/resolve/jvm/checkers/JvmSyntheticAssignmentChecker.kt
@@ -6,11 +6,14 @@
 package org.jetbrains.kotlin.resolve.jvm.checkers
 
 import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
+import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.psi.KtBinaryExpression
 import org.jetbrains.kotlin.resolve.calls.checkers.AssignmentChecker
 import org.jetbrains.kotlin.resolve.calls.checkers.CallCheckerContext
+import org.jetbrains.kotlin.resolve.calls.checkers.isAssignmentCorrectWithDataFlowInfo
 import org.jetbrains.kotlin.resolve.calls.tower.isSynthesized
 import org.jetbrains.kotlin.resolve.calls.util.getResolvedCall
+import org.jetbrains.kotlin.resolve.calls.util.getType
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.ErrorsJvm
 import org.jetbrains.kotlin.synthetic.SyntheticJavaPropertyDescriptor
 import org.jetbrains.kotlin.types.IndexedParametersSubstitution
@@ -20,6 +23,9 @@ import org.jetbrains.kotlin.types.Variance
 import org.jetbrains.kotlin.types.typeUtil.isNothing
 
 object JvmSyntheticAssignmentChecker : AssignmentChecker {
+
+    private val TYPE_MISMATCH_ERRORS = setOf(Errors.TYPE_MISMATCH, Errors.CONSTANT_EXPECTED_TYPE_MISMATCH, Errors.NULL_FOR_NONNULL_TYPE)
+
     override fun check(assignmentExpression: KtBinaryExpression, context: CallCheckerContext) {
         val left = assignmentExpression.left ?: return
         val resolvedCall = left.getResolvedCall(context.trace.bindingContext) ?: return
@@ -43,8 +49,16 @@ object JvmSyntheticAssignmentChecker : AssignmentChecker {
                 substitutionParameters.toTypedArray(), substitutionArguments.toTypedArray(), approximateContravariantCapturedTypes = true
             )
         )
-        val substitutedPropertyType = substitutor.substitute(propertyType.unwrap(), Variance.IN_VARIANCE)
-        if (substitutedPropertyType == null || !substitutedPropertyType.isNothing()) return
-        context.trace.report(ErrorsJvm.SYNTHETIC_SETTER_PROJECTED_OUT.on(left, resultingDescriptor))
+        val substitutedPropertyType = substitutor.substitute(propertyType.unwrap(), Variance.IN_VARIANCE) ?: return
+        if (substitutedPropertyType.isNothing()) {
+            context.trace.report(ErrorsJvm.SYNTHETIC_SETTER_PROJECTED_OUT.on(left, resultingDescriptor))
+            return
+        }
+        val rValue = assignmentExpression.right ?: return
+        val rValueType = rValue.getType(context.trace.bindingContext) ?: return
+        if (isAssignmentCorrectWithDataFlowInfo(substitutedPropertyType, rValue, rValueType, context)) return
+        if (context.trace.bindingContext.diagnostics.forElement(rValue).none { it.factory in TYPE_MISMATCH_ERRORS }) {
+            context.trace.report(Errors.TYPE_MISMATCH_WARNING.on(rValue, substitutedPropertyType, rValueType))
+        }
     }
 }
--- compiler/testData/diagnostics/tests/syntheticSet.kt
+++ compiler/testData/diagnostics/tests/syntheticSet.kt
@@ -29,8 +29,13 @@ fun foo(container: Container<*>, wrapper: Wrapper<String>) {
     <!SYNTHETIC_SETTER_PROJECTED_OUT!>container.wrapper<!> = wrapper
     container.setWrapper(<!TYPE_MISMATCH!>wrapper<!>)
 
-    container.simple = "123"
+    container.simple = <!TYPE_MISMATCH_WARNING!>"123"<!>
     container.setSimple(<!TYPE_MISMATCH!>"123"<!>)
+    container.simple = null
+    container.setSimple(null)
+    container.simple = <!TYPE_MISMATCH_WARNING!>container.simple<!>
+    container.setSimple(<!TYPE_MISMATCH!>container.getSimple()<!>)
+    container<!UNREACHABLE_CODE!>.simple =<!> null!!
 }
 
 fun bar(container: Container<String>, wrapper: Wrapper<String>) {
@@ -47,32 +52,62 @@ fun baz(container: Container<Any>, wrapper: Wrapper<String>) {
     container.setSimple("123")
 }
 
-fun gau(container: Container<String>, wrapper: Wrapper<Any>) {
+fun gau(container: Container<String>, wrapper: Wrapper<Any>, arg: Any) {
     container.wrapper = <!TYPE_MISMATCH!>wrapper<!>
     container.simple = <!CONSTANT_EXPECTED_TYPE_MISMATCH!>456<!>
+    container.simple = <!TYPE_MISMATCH!>arg<!>
+    container.simple = <!TYPE_MISMATCH!>x<!>
+    container.simple = <!TYPE_MISMATCH!>O<!>
+    if (arg is String) {
+        container.simple = <!DEBUG_INFO_SMARTCAST!>arg<!>
+    }
 }
 
 fun dif(container: Container<String>, wrapper: Wrapper<Int>) {
     container.wrapper = <!TYPE_MISMATCH!>wrapper<!>
 }
 
+object O
+
 fun out(container: Container<out Any>, wrapper: Wrapper<String>) {
     <!SYNTHETIC_SETTER_PROJECTED_OUT!>container.wrapper<!> = wrapper
     container.setWrapper(<!TYPE_MISMATCH!>wrapper<!>)
-    container.simple = "123"
-    container.setSimple(<!TYPE_MISMATCH!>"123"<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>x<!>
+    container.setSimple(<!TYPE_MISMATCH!>x<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>O<!>
+    container.setSimple(<!TYPE_MISMATCH!>O<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>456<!>
+    container.setSimple(<!CONSTANT_EXPECTED_TYPE_MISMATCH!>456<!>)
 }
 
-fun inn(container: Container<in String>, wrapper: Wrapper<Any>) {
+val x = 456
+
+fun inn(container: Container<in String>, wrapper: Wrapper<Any>, arg: Any) {
     <!SYNTHETIC_SETTER_PROJECTED_OUT!>container.wrapper<!> = wrapper
     container.setWrapper(<!TYPE_MISMATCH!>wrapper<!>)
-    container.simple = 456
+    container.simple = <!TYPE_MISMATCH_WARNING!>x<!>
+    container.setSimple(<!TYPE_MISMATCH!>x<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>O<!>
+    container.setSimple(<!TYPE_MISMATCH!>O<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>456<!>
     container.setSimple(<!CONSTANT_EXPECTED_TYPE_MISMATCH!>456<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>arg<!>
+    container.setSimple(<!TYPE_MISMATCH!>arg<!>)
+    if (arg is String) {
+        container.simple = arg
+        container.setSimple(<!DEBUG_INFO_SMARTCAST!>arg<!>)
+    }
 }
 
 fun <T> generic(container: Container<out T>, wrapper: Wrapper<out T>, arg: T) {
     <!SYNTHETIC_SETTER_PROJECTED_OUT!>container.wrapper<!> = wrapper
     container.setWrapper(<!TYPE_MISMATCH!>wrapper<!>)
-    container.simple = arg
+    container.simple = <!TYPE_MISMATCH!>x<!>
+    container.setSimple(<!TYPE_MISMATCH!>x<!>)
+    container.simple = <!TYPE_MISMATCH!>O<!>
+    container.setSimple(<!TYPE_MISMATCH!>O<!>)
+    container.simple = <!CONSTANT_EXPECTED_TYPE_MISMATCH!>456<!>
+    container.setSimple(<!CONSTANT_EXPECTED_TYPE_MISMATCH!>456<!>)
+    container.simple = <!TYPE_MISMATCH_WARNING!>arg<!>
     container.setSimple(<!TYPE_MISMATCH!>arg<!>)
 }
