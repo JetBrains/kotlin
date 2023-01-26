796677c7c7270bb1bafae32edb9e75caaed8ac49
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-01-26 21:49:02 +0100

    Also clean newly added files.
    Some patches could add new file and after hard reset they are not
    deleted. This leads to patch apply failure.
    
    Updated JGit otherwise clean command was not working properly.

--- libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
+++ libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-common")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
     implementation("org.slf4j:slf4j-nop:1.7.32")
     implementation("org.jetbrains.kotlinx:dataframe:0.8.1")
 }
