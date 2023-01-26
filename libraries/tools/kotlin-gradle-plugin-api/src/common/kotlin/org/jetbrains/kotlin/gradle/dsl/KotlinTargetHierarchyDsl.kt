c3c0d0a79b71a3b50127ad7f5a7fd28485495529
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-01-26 10:10:58 +0100

    [Gradle][Minor] KotlinTargetHierarchyDsl: Fix doc pointing to old function
    KT-56204
    
    (cherry picked from commit 294014a85d22de46e8e9260d470607f0115153f9)

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinTargetHierarchyDsl.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/dsl/KotlinTargetHierarchyDsl.kt
@@ -24,7 +24,7 @@ interface KotlinTargetHierarchyDsl {
      *
      * ```kotlin
      * kotlin {
-     *     targets.hierarchy.default() // <- position of this call is not relevant!
+     *     targetHierarchy.default() // <- position of this call is not relevant!
      *
      *     iosX64()
      *     iosArm64()
@@ -66,7 +66,7 @@ interface KotlinTargetHierarchyDsl {
      *
      * ```kotlin
      * kotlin {
-     *     targets.hierarchy.default { target ->
+     *     targetHierarchy.default { target ->
      *         group("native") { // <- we can re-declare already existing groups and connect children to it!
      *             group("unixLike") {
      *                 withLinux()
