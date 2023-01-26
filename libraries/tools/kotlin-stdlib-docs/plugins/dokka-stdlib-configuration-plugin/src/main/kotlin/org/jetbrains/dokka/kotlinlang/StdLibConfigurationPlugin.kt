35ffde167f1e1117f046adadeb171475c07da97b
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-01-26 22:25:33 +0100

    Update Dokka to 1.8.0-dev-195
    - Build dokka plugins with Kotlin 1.8 (required to compile with Dokka 1.8)

--- libraries/tools/kotlin-stdlib-docs/plugins/dokka-stdlib-configuration-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/StdLibConfigurationPlugin.kt
+++ libraries/tools/kotlin-stdlib-docs/plugins/dokka-stdlib-configuration-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/StdLibConfigurationPlugin.kt
@@ -1,9 +1,14 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
 package org.jetbrains.dokka.kotlinlang
 
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.jetbrains.dokka.analysis.DokkaAnalysisConfiguration
-import org.jetbrains.dokka.analysis.KotlinAnalysis
+import org.jetbrains.dokka.analysis.ProjectKotlinAnalysis
 import org.jetbrains.dokka.plugability.configuration
 
 class StdLibConfigurationPlugin : DokkaPlugin() {
@@ -13,7 +18,7 @@ class StdLibConfigurationPlugin : DokkaPlugin() {
     val stdLibKotlinAnalysis by extending {
         dokkaBase.kotlinAnalysis providing { ctx ->
             val ignoreCommonBuiltIns = configuration<StdLibConfigurationPlugin, StdLibAnalysisConfiguration>(ctx)?.ignoreCommonBuiltIns ?: false
-            KotlinAnalysis(
+            ProjectKotlinAnalysis(
                 sourceSets = ctx.configuration.sourceSets,
                 logger = ctx.logger,
                 analysisConfiguration = DokkaAnalysisConfiguration(ignoreCommonBuiltIns = ignoreCommonBuiltIns)
