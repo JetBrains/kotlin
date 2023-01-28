e021655001905a78759d30ab0d621c7088fe5a2b
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-01-28 11:26:21 +0100

    [LC] AbstractCompilerLightClassTest: add an additional test extension
    ^KT-56046

--- compiler/tests-common/tests/org/jetbrains/kotlin/asJava/AbstractCompilerLightClassTest.kt
+++ compiler/tests-common/tests/org/jetbrains/kotlin/asJava/AbstractCompilerLightClassTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -26,7 +26,9 @@ abstract class AbstractCompilerLightClassTest : KotlinMultiFileTestWithJava<Kotl
 
     override fun doMultiFileTest(wholeFile: File, files: List<TestFile>) {
         val environment = createEnvironment(wholeFile, files)
-        val expectedFile = KotlinTestUtils.replaceExtension(wholeFile, "java")
+        val expectedFile = KotlinTestUtils.replaceExtension(wholeFile, "descriptors.java").takeIf(File::exists)
+            ?: KotlinTestUtils.replaceExtension(wholeFile, "java")
+
         val allowFrontendExceptions = InTextDirectivesUtils.isDirectiveDefined(wholeFile.readText(), "// ALLOW_FRONTEND_EXCEPTION")
 
         val actual = LightClassTestCommon.getActualLightClassText(
