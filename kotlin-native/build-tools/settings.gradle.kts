1b7ec16bc3b07fc951db556a313fbbe8893928e0
Pavel Punegov <pavel.punegov@jetbrains.com>
2023-02-01 16:57:45 +0100

    [K/N] Apply cache-redirector

--- kotlin-native/build-tools/settings.gradle.kts
+++ kotlin-native/build-tools/settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "build-tools"
 
 pluginManagement {
+    apply(from = "../../repo/scripts/cache-redirector.settings.gradle.kts")
     apply(from = "../../repo/scripts/kotlin-bootstrap.settings.gradle.kts")
     repositories {
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-dependencies")
