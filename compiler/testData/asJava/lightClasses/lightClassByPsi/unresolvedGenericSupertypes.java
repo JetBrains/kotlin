42cb69f3c538f2110f052faaecab13c5fe2cc427
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2023-02-13 14:47:45 +0000

    [SLC] Support generics and ignore whitespace in KtSuperTypeList.findEntry

---
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/unresolvedGenericSupertypes.java
@@ -0,0 +1,7 @@
+public final class C1 /* C1*/ extends Super<T, U> implements Intf1<A, B>, Intf2<X, Y<Z>> {
+  public  C1();//  .ctor()
+}
+
+public final class C2 /* C2*/ extends com.example.Super<T, U> implements com.example.Intf1<A, B>, com.example.Intf2<X, Y<Z>> {
+  public  C2();//  .ctor()
+}
