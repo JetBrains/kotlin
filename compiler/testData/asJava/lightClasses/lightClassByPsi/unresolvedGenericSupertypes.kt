5f31a2c6d8e1db320c540fc5441027a0435a2ecd
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-02-13 17:28:35 +0100

    Revert "[SLC] Support generics and ignore whitespace in KtSuperTypeList.findEntry"
    This reverts commit e77f5c7764ce3573b739e6b0aeec49be25512b99.

--- compiler/testData/asJava/lightClasses/lightClassByPsi/unresolvedGenericSupertypes.kt
+++
@@ -1,9 +0,0 @@
-public class C1: Intf1 < A , B >, Super < T , U >(42),
-    Intf2 < X ,
-            Y < Z >>
-
-public class C2: com . example . Intf1 < A , B >, com . example . Super < T , U >(42),
-    com . example . Intf2 < X ,
-            Y < Z >>
-
-// COMPILATION_ERRORS
