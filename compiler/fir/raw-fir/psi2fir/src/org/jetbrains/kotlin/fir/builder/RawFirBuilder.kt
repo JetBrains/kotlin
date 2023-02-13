8e3022452e76bacacdf27ab76a2f2415d21970d6
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2023-02-13 18:08:00 +0200

    [FIR] Unify diagnostic message in FIR dump for syntax error between PSI and LT

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -1969,7 +1969,7 @@ open class RawFirBuilder(
                 }
                 null -> FirErrorTypeRefBuilder().apply {
                     this.source = source
-                    diagnostic = ConeSimpleDiagnostic("Unwrapped type is null", DiagnosticKind.Syntax)
+                    diagnostic = ConeSimpleDiagnostic("Incomplete code", DiagnosticKind.Syntax)
                 }
                 else -> throw AssertionError("Unexpected type element: ${unwrappedElement.text}")
             }
