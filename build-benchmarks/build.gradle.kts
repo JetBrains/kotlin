dffe0b0baf82b9ed330b931b0b17d58e9e56ee6d
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-02-13 23:26:08 +0100

    Update scenarious due to rebase

--- build-benchmarks/build.gradle.kts
+++ build-benchmarks/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 buildscript {
-    val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.7.10"
+    val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.8.10"
     val kotlinRepo = "https://buildserver.labs.intellij.net/guestAuth/app/rest/builds/buildType:(id:Kotlin_KotlinDev_CompilerDistAndMavenArtifacts),number:$kotlinVersion,branch:default:any/artifacts/content/maven"
     extra["kotlinRepo"] = kotlinRepo
 
