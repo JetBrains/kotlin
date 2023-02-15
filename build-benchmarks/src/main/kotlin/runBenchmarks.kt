3d10cc5e9c12edd6580957a4aedf7a823813a97c
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-02-15 16:02:22 +0100

    Add benchmarks with K2

--- build-benchmarks/src/main/kotlin/runBenchmarks.kt
+++ build-benchmarks/src/main/kotlin/runBenchmarks.kt
@@ -3,4 +3,5 @@ import org.jetbrains.kotlin.build.benchmarks.*
 fun main() {
     // expected working dir is %KOTLIN_PROJECT_PATH%/build-benchmarks/
     mainImpl(artifactTransformBenchmarks(), "../.")
+    mainImpl(artifactTransformWithK2Benchmarks(), "../.")
 }
