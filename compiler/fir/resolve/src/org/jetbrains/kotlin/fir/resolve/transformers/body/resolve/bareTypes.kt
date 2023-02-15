f6675734103a142a215962c62dcf31b0441aff56
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-02-15 16:10:09 +0100

    [FIR] computeRepresentativeTypeForBareType: add missing lazy resolve
    ^KT-55750

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/bareTypes.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/bareTypes.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.fir.resolve.transformers.body.resolve
 
 import org.jetbrains.kotlin.fir.declarations.FirRegularClass
+import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.declarations.FirTypeAlias
 import org.jetbrains.kotlin.fir.declarations.FirTypeParameter
 import org.jetbrains.kotlin.fir.declarations.utils.expandedConeType
@@ -14,6 +15,7 @@ import org.jetbrains.kotlin.fir.resolve.defaultType
 import org.jetbrains.kotlin.fir.resolve.fullyExpandedType
 import org.jetbrains.kotlin.fir.resolve.toSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirTypeParameterSymbol
+import org.jetbrains.kotlin.fir.symbols.lazyResolveToPhase
 import org.jetbrains.kotlin.fir.types.*
 import org.jetbrains.kotlin.types.AbstractTypeChecker
 
@@ -57,6 +59,8 @@ fun BodyResolveComponents.computeRepresentativeTypeForBareType(type: ConeClassLi
 }
 
 private fun canBeUsedAsBareType(firTypeAlias: FirTypeAlias): Boolean {
+    firTypeAlias.lazyResolveToPhase(FirResolvePhase.TYPES)
+
     val typeAliasParameters = firTypeAlias.typeParameters.toSet()
     val usedTypeParameters = mutableSetOf<FirTypeParameter>()
 
