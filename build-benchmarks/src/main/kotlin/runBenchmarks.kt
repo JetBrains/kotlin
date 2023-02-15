7dcaabc2e749dba5023fd670871b0d37f9add19f
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-02-15 16:01:53 +0100

    Do not run benchmarks with old IC approaches

--- build-benchmarks/src/main/kotlin/runBenchmarks.kt
+++ build-benchmarks/src/main/kotlin/runBenchmarks.kt
@@ -2,7 +2,5 @@ import org.jetbrains.kotlin.build.benchmarks.*
 
 fun main() {
     // expected working dir is %KOTLIN_PROJECT_PATH%/build-benchmarks/
-    mainImpl(historyFilesBenchmarks(), "../.")
-    mainImpl(abiSnapshotBenchmarks(), "../.")
     mainImpl(artifactTransformBenchmarks(), "../.")
 }
