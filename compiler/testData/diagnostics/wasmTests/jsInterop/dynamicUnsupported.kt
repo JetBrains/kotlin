68238514eef6ebb4c98c89d9af82eaae5dab0571
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2023-02-16 15:57:06 +0100

    [Wasm] Diagnostic test: dynamic type is unsupported
    Validates the fix for KT-56711

---
+++ compiler/testData/diagnostics/wasmTests/jsInterop/dynamicUnsupported.kt
@@ -0,0 +1,8 @@
+val foo: <!UNSUPPORTED!>dynamic<!> = 1
+
+fun foo(x: <!UNSUPPORTED!>dynamic<!>): <!UNSUPPORTED!>dynamic<!> {
+    class C {
+        val foo: <!UNSUPPORTED!>dynamic<!> = 1
+    }
+    return x + C().foo
+}
