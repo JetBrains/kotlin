7026ca9952a210f14133b6e87cdc853a6f12252b
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-02-17 13:17:45 +0100

    [Gradle] KT-56729: Implement 'commonize-kt-56729-consume-library-with-two-roots' test
    ... to cover KT-56729

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/commonize-kt-56729-consume-library-with-two-roots/consumer/build.gradle.kts
@@ -0,0 +1,30 @@
+@file:Suppress("OPT_IN_USAGE")
+
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin("multiplatform")
+}
+
+repositories {
+    maven(project(":producer").buildDir.resolve("repo"))
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    linuxArm64()
+    linuxX64()
+
+    targetHierarchy.default()
+
+    sourceSets.commonMain.get().dependencies {
+        implementation("org.jetbrains.sample:producer:1.0.0-SNAPSHOT")
+    }
+
+    targets.withType<KotlinNativeTarget>().all {
+        compilations.getByName("main").cinterops.create("consumer-c-library") {
+            header("src/consumer-c-library.h")
+        }
+    }
+}
