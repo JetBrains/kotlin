6c205ed3345a3afb2aca15aaf12f0da2dca0dd61
Anton Lakotka <anton.lakotka@jetbrains.com>
2023-02-23 12:24:54 +0100

    [Gradle] Avoid configuration resolution in MPP projects
    Add `getResolvedArtifactsCompat` for compatibility with gradle <7.4
    However configurations are still resolved in these gradle versions
    
    ^KT-51940 Verification Pending

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-51940-hmpp-resolves-configurations-during-configuration/build.gradle.kts
@@ -0,0 +1,30 @@
+import java.util.concurrent.atomic.AtomicBoolean
+
+plugins {
+    kotlin("multiplatform")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    targetHierarchy.default()
+
+    jvm()
+    linuxX64()
+    linuxArm64()
+}
+
+// Setup configuration resolution hook
+// Report configurations that is going to be resolved before the task graph is ready
+val isResolutionAllowed = AtomicBoolean(false)
+project.gradle.taskGraph.whenReady { isResolutionAllowed.set(true) }
+
+configurations.all {
+    incoming.beforeResolve {
+        if (isResolutionAllowed.get()) return@beforeResolve
+        println("!!!Configuration Resolved: $name")
+    }
+}
---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-51940-hmpp-resolves-configurations-during-configuration/src/commonMain/kotlin/common.kt
@@ -0,0 +1,3 @@
+package lib
+
+fun commonMain() = "common"
