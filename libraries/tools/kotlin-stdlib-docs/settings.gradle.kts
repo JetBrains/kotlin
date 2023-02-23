0389c3526299debe45bf1f4ab0e773c7dd6af7f1
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-02-23 03:47:05 +0100

    docs build: allow to specify custom dokka repository
    - centralize repository management in settings.gradle.kts
    - allow to specify dokka repository with a gradle property
    - automatically add mavenLocal for -local dokka versions
    - specify kotlin version for building plugins in one place

---
+++ libraries/tools/kotlin-stdlib-docs/settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    val dokkaVersion = providers.gradleProperty("dokka_version").get()
+    val dokkaRepository = providers.gradleProperty("dokka_repository").getOrElse("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
+    plugins {
+        id("org.jetbrains.dokka") version(dokkaVersion)
+    }
+
+    repositories {
+        gradlePluginPortal()
+        maven(url = dokkaRepository)
+        if ("-local" in dokkaVersion) {
+            mavenLocal()
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    val dokkaVersion = providers.gradleProperty("dokka_version").get()
+    val dokkaRepository = providers.gradleProperty("dokka_repository").getOrElse("https://maven.pkg.jetbrains.space/kotlin/p/dokka/dev")
+    repositories {
+        mavenCentral()
+        maven(url = dokkaRepository)
+        if ("-local" in dokkaVersion) {
+            mavenLocal()
+        }
+    }
+}
+
+rootProject.name = "kotlin-stdlib-docs"
+
+include("kotlin_big")
+include("plugins")
+include("plugins:dokka-samples-transformer-plugin")
+include("plugins:dokka-stdlib-configuration-plugin")
+include("plugins:dokka-version-filter-plugin")
