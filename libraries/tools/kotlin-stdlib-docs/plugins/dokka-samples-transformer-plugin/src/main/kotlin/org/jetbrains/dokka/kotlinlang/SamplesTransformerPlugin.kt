405cecc24a3eb2ac1f9649215d3f1296619f509c
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-02-24 22:36:32 +0100

    Update Dokka to 1.8.20-dev-213
    (cherry picked from commit 36cb94c158e236fc8d81dd92720f043b47e85d91)

--- libraries/tools/kotlin-stdlib-docs/plugins/dokka-samples-transformer-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/SamplesTransformerPlugin.kt
+++ libraries/tools/kotlin-stdlib-docs/plugins/dokka-samples-transformer-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/SamplesTransformerPlugin.kt
@@ -3,6 +3,8 @@ package org.jetbrains.dokka.kotlinlang
 import org.jetbrains.dokka.CoreExtensions
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.plugability.DokkaPlugin
+import org.jetbrains.dokka.plugability.DokkaPluginApiPreview
+import org.jetbrains.dokka.plugability.PluginApiPreviewAcknowledgement
 
 class SamplesTransformerPlugin : DokkaPlugin() {
     private val dokkaBase by lazy { plugin<DokkaBase>() }
@@ -13,4 +15,7 @@ class SamplesTransformerPlugin : DokkaPlugin() {
             before(dokkaBase.pageMerger)
         }
     }
+
+    @DokkaPluginApiPreview
+    override fun pluginApiPreviewAcknowledgement(): PluginApiPreviewAcknowledgement = PluginApiPreviewAcknowledgement
 }
