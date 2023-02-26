c97f536a7f27a39799dffe800970f2cc20c45bf4
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-02-26 18:36:37 +0100

    chore: use SmartList for single and zero element collections.

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/CollectionUtils.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/CollectionUtils.kt
@@ -6,42 +6,62 @@
 package org.jetbrains.kotlin.ir.util
 
 import org.jetbrains.kotlin.utils.SmartList
+import kotlin.math.min
 
 val EMPTY_LIST = SmartList<Nothing>()
 
 inline fun <T, R> Collection<T>.map(transform: (T) -> R): List<R> {
     if (isEmpty()) return EMPTY_LIST
+    if (size == 1) return SmartList(transform(first()))
     return mapTo(ArrayList(size), transform)
 }
 
 inline fun <T, R : Any> Collection<T>.mapNotNull(transform: (T) -> R?): List<R> {
     if (isEmpty()) return EMPTY_LIST
+    if (size == 1) return transform(first())?.let { SmartList(it) } ?: EMPTY_LIST
     return mapNotNullTo(ArrayList(size), transform)
 }
 
 inline fun <T, R> Collection<T>.mapIndexed(transform: (index: Int, T) -> R): List<R> {
     if (isEmpty()) return EMPTY_LIST
+    if (size == 1) return SmartList(transform(0, first()))
     return mapIndexedTo(ArrayList<R>(size), transform)
 }
 
 inline fun <T, R> Collection<T>.flatMap(transform: (T) -> Iterable<R>): List<R> {
-    if (isEmpty()) return EMPTY_LIST
-    return flatMapTo(ArrayList<R>(), transform)
+    val result = flatMapTo(ArrayList<R>(), transform)
+    return when (result.size) {
+        0 -> EMPTY_LIST
+        1 -> SmartList(result.first())
+        else -> result
+    }
 }
 
 inline fun <T> Collection<T>.filter(predicate: (T) -> Boolean): List<T> {
-    if (isEmpty()) return EMPTY_LIST
-    return filterTo(ArrayList(), predicate)
+    val result = filterTo(ArrayList(), predicate)
+    return when (result.size) {
+        0 -> EMPTY_LIST
+        1 -> SmartList(result.first())
+        else -> result
+    }
 }
 
 inline fun <T> Collection<T>.filterNot(predicate: (T) -> Boolean): List<T> {
-    if (isEmpty()) return EMPTY_LIST
-    return filterNotTo(ArrayList(), predicate)
+    val result = filterNotTo(SmartList(), predicate)
+    return when (result.size) {
+        0 -> EMPTY_LIST
+        1 -> SmartList(result.first())
+        else -> result
+    }
 }
 
 inline fun <reified T> Collection<*>.filterIsInstance(): List<T> {
-    if (isEmpty()) return EMPTY_LIST
-    return filterIsInstanceTo(ArrayList<T>())
+    val result = filterIsInstanceTo(ArrayList<T>())
+    return when (result.size) {
+        0 -> EMPTY_LIST
+        1 -> SmartList(result.first())
+        else -> result
+    }
 }
 
 operator fun <T> List<T>.plus(elements: List<T>): List<T> {
@@ -49,11 +69,16 @@ operator fun <T> List<T>.plus(elements: List<T>): List<T> {
     val result = ArrayList<T>(this.size + elements.size)
     result.addAll(this)
     result.addAll(elements)
-    return result
+    return when (result.size) {
+        0 -> EMPTY_LIST
+        1 -> SmartList(result.first())
+        else -> result
+    }
 }
 
 infix fun <T, R> Collection<T>.zip(other: Collection<R>): List<Pair<T, R>> {
     if (isEmpty() || other.isEmpty()) return EMPTY_LIST
+    if (min(size, other.size) == 1) return SmartList(first() to other.first())
     return zip(other) { t1, t2 -> t1 to t2 }
 }
 
