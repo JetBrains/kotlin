cd7b4eb632a7232a4010582d944a199d353907a0
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-02-28 16:28:34 +0100

    [Gradle] Multiplatform: Bump maxSupportedVersion for AGP to 8.0
    ^KT-56654 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinMultiplatformAndroidGradlePluginCompatibilityHealthCheck.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinMultiplatformAndroidGradlePluginCompatibilityHealthCheck.kt
@@ -18,7 +18,7 @@ internal object KotlinMultiplatformAndroidGradlePluginCompatibilityHealthCheck {
 
     val compatibleAndroidGradlePluginVersionRange = AndroidGradlePluginVersionRange(
         minSupportedVersion = AndroidGradlePluginVersionRange.Version(4, 1),
-        maxSupportedVersion = AndroidGradlePluginVersionRange.Version(7, 4)
+        maxSupportedVersion = AndroidGradlePluginVersionRange.Version(8, 0)
     )
 
     /**
