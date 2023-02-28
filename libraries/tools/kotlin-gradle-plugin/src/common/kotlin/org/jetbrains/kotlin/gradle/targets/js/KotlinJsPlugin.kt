9b84a2180a74bc68810ecb3307b3bead739e9cfd
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2023-02-28 21:40:17 +0200

    [MPP] Disable KGP import for the standalone kotlin-js Gradle plugin
    KTIJ-24745

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/KotlinJsPlugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/KotlinJsPlugin.kt
@@ -33,6 +33,8 @@ open class KotlinJsPlugin(
 
         checkGradleCompatibility()
 
+        project.disableKgpImport()
+
         val kotlinExtension = project.kotlinExtension as KotlinJsProjectExtension
         customizeKotlinDependencies(project)
 
