da8a16f988c380ec9147bb0d38f9681532f06c6c
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-03-01 15:45:35 +0100

    KT-56747: Disable failed test for K2/Native

--- compiler/testData/codegen/box/regressions/kt56747.kt
+++ compiler/testData/codegen/box/regressions/kt56747.kt
@@ -1,3 +1,6 @@
+// KT-56747: Wrong return type for `lambda: (Any) -> Any` which returns Unit
+// IGNORE_BACKEND_K2: NATIVE
+
 // this test was minimized from compiler/testData/codegen/box/coroutines/kt15016.kt
 fun mapUnit(transform: (Any) -> Any) = transform(Unit)
 
