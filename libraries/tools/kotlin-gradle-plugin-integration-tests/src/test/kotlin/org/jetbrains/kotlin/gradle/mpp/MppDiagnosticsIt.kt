626b48ea287ff4f0ab39c627aa564c75843fa462
Pavel Kargashinsky <pavel.kargashinsky@jetbrains.com>
2023-03-07 13:35:12 +0100

    [Gradle][Tests][MPP] Bump AGP and Gradle version in MPP related tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppDiagnosticsIt.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppDiagnosticsIt.kt
@@ -14,6 +14,10 @@ import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @MppGradlePluginTests
+@GradleTestVersions(
+    maxVersion = TestVersions.Gradle.NEXT_RELEASE,
+    additionalVersions = [TestVersions.Gradle.MAX_SUPPORTED]
+)
 class MppDiagnosticsIt : KGPBaseTest() {
     @GradleTest
     fun testDeprecatedProperties(gradleVersion: GradleVersion) {
