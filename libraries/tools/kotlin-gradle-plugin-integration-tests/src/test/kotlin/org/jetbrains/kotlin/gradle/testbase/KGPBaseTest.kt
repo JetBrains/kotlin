31eb012b6b0bb2ff0fd940234a884634c32765aa
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2023-03-15 17:16:31 +0100

    [tests] Minor: add support of DevKit in Gradle IT
    This should provide some nicities in IDEA like folders for navigating
    to testData

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/KGPBaseTest.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/KGPBaseTest.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.gradle.testbase
 
+import com.intellij.testFramework.TestDataPath
 import org.jetbrains.kotlin.test.WithMuteInDatabase
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.TestInstance
@@ -13,10 +14,20 @@ import java.nio.file.Path
 
 /**
  * Base class for all Kotlin Gradle plugin integration tests.
+ *
+ * @[TestDataPath] helps DevKit IDE Plugin to find "related testData".
+ * It provides various IDE-assistance (e.g. "NavigateToTestData"-actions and gutter icons)
+ * For the test named 'testFoo' the "related testData" will be checked at:
+ *      '<argument-of-@TestDataPath>/foo'
+ *
+ * If the test uses a name that doesn't correspond to the testdata, you can add
+ *      @org.jetbrains.kotlin.test.TestMetadata("<path relative to resources/testProject">)
+ *
  */
 @Tag("JUnit5")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @WithMuteInDatabase
+@TestDataPath("\$CONTENT_ROOT/resources/testProject")
 abstract class KGPBaseTest {
     open val defaultBuildOptions = BuildOptions()
 
