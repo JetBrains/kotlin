fd71ebd64c347954fa9d28946d719e9bd98f1b17
Mads Ager <ager@google.com>
2023-03-16 12:49:36 +0100

    [K2] Type parameter metadata handling.

--- libraries/tools/kotlinp/testData/TypeParameters.kt
+++ libraries/tools/kotlinp/testData/TypeParameters.kt
@@ -1,7 +1,7 @@
-// IGNORE K2
-
 class A<T> {
     fun <T> a(t: T) {}
+
+    fun <U> f(m: Map.Entry<T, U>) {}
 
     inner class B<U, V : U> {
         fun <T, U> b(t: T, u: U & Any, v: V) where T : Comparable<T>, T : Cloneable {}
