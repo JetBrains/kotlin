df1ee532205585ba88f90d6038f0148bb0d774c8
aSemy <897017+aSemy@users.noreply.github.com>
2023-03-16 20:18:16 +0100

    [KDM] Fix test runtime Java versions
    See https://github.com/Kotlin/dokka/pull/2918
    
    Fixes https://github.com/Kotlin/dokka/issues/2917
    
    Co-authored-by: Goooler <wangzongler@gmail.com>
    Original commit: https://github.com/Kotlin/dokka/commit/18d01bf269a88c0fba0ae860a1fda644d31a37c2

--- libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/org/jetbrains/conventions/base-java.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/org/jetbrains/conventions/base-java.gradle.kts
@@ -8,15 +8,39 @@ package org.jetbrains.conventions
  */
 
 plugins {
+    id("org.jetbrains.conventions.base")
     java
 }
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
+        languageVersion.set(dokkaBuild.mainJavaVersion)
     }
 }
 
 java {
     withSourcesJar()
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+
+    javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(dokkaBuild.testJavaLauncherVersion)
+    })
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
+    // TODO Upgrade all subprojects to use JUnit Jupiter https://github.com/Kotlin/dokka/issues/2924
+    //      Replace these dependencies with `testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")`
+    //      See https://junit.org/junit5/docs/current/user-guide/#running-tests-build-gradle-engines-configure
+    // Ideally convention plugins should only provide sensible defaults that can be overridden by subprojects.
+    // If a convention plugin defines dependencies, these cannot be easily overridden by subprojects, and so
+    // this should be avoided. However, for now , both JUnit 4 and 5 must be supported, and since these are test
+    // runtime-only dependencies they are not going to have a significant impact subprojects.
+    // These dependencies should be revisited in #2924, and (for example) moved to each subproject (which is more
+    // repetitive, but more declarative and clear), or some other solution.
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 }
