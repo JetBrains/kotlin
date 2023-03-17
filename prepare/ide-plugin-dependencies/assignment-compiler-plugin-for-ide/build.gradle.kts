62b643c3e357b3fc0ce77bf486cd841ec2508a95
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2023-03-17 15:22:25 +0100

    KT-57468 Publish IDE jars properly (include K2/CLI artefacts)

--- prepare/ide-plugin-dependencies/assignment-compiler-plugin-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/assignment-compiler-plugin-for-ide/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
 
 publishJarsForIde(
     listOf(
+        ":kotlin-assignment-compiler-plugin.cli",
         ":kotlin-assignment-compiler-plugin.common",
-        ":kotlin-assignment-compiler-plugin.k1"
+        ":kotlin-assignment-compiler-plugin.k1",
+        ":kotlin-assignment-compiler-plugin.k2"
     )
 )
