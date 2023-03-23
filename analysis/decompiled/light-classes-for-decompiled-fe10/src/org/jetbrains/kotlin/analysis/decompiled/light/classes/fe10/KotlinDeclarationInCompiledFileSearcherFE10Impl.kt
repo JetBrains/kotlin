fa5c3f6ef20bb5e5989265b6135f893d1d21ba2f
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-03-23 11:42:25 +0100

    [psi] don't allow loading text from decompiled code
    use stubs when possible, when stub doesn't contain anything
    decompiled text won't contain it either,
    no sense to load

--- analysis/decompiled/light-classes-for-decompiled-fe10/src/org/jetbrains/kotlin/analysis/decompiled/light/classes/fe10/KotlinDeclarationInCompiledFileSearcherFE10Impl.kt
+++ analysis/decompiled/light-classes-for-decompiled-fe10/src/org/jetbrains/kotlin/analysis/decompiled/light/classes/fe10/KotlinDeclarationInCompiledFileSearcherFE10Impl.kt
@@ -19,18 +19,12 @@ import org.jetbrains.kotlin.psi.KtDeclaration
 class KotlinDeclarationInCompiledFileSearcherFE10Impl : KotlinDeclarationInCompiledFileSearcher() {
     override fun findDeclarationInCompiledFile(file: KtClsFile, member: PsiMember, signature: MemberSignature): KtDeclaration? {
         val relativeClassName = member.relativeClassName()
-        val key = ClassNameAndSignature(relativeClassName, signature)
 
         val memberName = member.name
-        if (memberName != null && !file.isContentsLoaded && file.hasDeclarationWithKey(BySignatureIndexer, key)) {
-            findByStubs(file, relativeClassName, member, memberName)?.let { return it }
+        if (memberName != null && !file.isContentsLoaded) {
+            return findByStubs(file, relativeClassName, member, memberName)
         }
 
-        val declaration = file.getDeclaration(BySignatureIndexer, key) ?: return null
-        return if (member is PsiMethod && member.isConstructor && declaration is KtClassOrObject) {
-            declaration.primaryConstructor ?: declaration
-        } else {
-            declaration
-        }
+        error("Should be unreachable")
     }
 }
