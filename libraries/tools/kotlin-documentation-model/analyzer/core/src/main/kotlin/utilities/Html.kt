30396c38c62a632202bef06abd540e491701511e
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2023-03-27 03:01:34 -0700

    [KDM] Mark utilities.* API as Dokka-internal
    See https://github.com/Kotlin/dokka/pull/2937
    
    * Deprecate unused declaration, remove inline declaration (as it's binary compatible), opt-in into internal API at project level
    * Opt-in into DokkaInternalApi where applicable
    * Get rid of intermediate test-utils module
    Original commit: https://github.com/Kotlin/dokka/commit/9af0b307a14253a21d11d340a78dd9061abee359

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/utilities/Html.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/utilities/Html.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.dokka.utilities
 
+import org.jetbrains.dokka.*
 import java.net.URLEncoder
 
 
@@ -7,9 +8,12 @@ import java.net.URLEncoder
  * Replaces symbols reserved in HTML with their respective entities.
  * Replaces & with &amp;, < with &lt; and > with &gt;
  */
+@InternalDokkaApi
 fun String.htmlEscape(): String = replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;")
 
+@InternalDokkaApi
 fun String.urlEncoded(): String = URLEncoder.encode(this, "UTF-8")
 
+@InternalDokkaApi
 fun String.formatToEndWithHtml() =
     if (endsWith(".html") || contains(Regex("\\.html#"))) this else "$this.html"
