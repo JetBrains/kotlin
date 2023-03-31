6a0e6b11bdae91ab3b8a6a1701a4a3c9738136dd
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-03-31 14:23:14 +0200

    [K/N] Add kotlin::RWSpinLock ^KT-56233
    Trivially constructible and destructible std::shared_mutex
    implementation with additional guarantee that try_* methods fail only if
    the mutex is locked.

--- kotlin-native/runtime/src/main/cpp/MutexTest.cpp
+++ kotlin-native/runtime/src/main/cpp/MutexTest.cpp
@@ -6,7 +6,9 @@
 #include "Mutex.hpp"
 
 #include <mutex>
+#include <shared_mutex>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "ScopedThread.hpp"
@@ -47,4 +49,179 @@ TYPED_TEST(MutexTest, SmokeDetachedThread) {
     EXPECT_EQ(protectedCounter, 2);
 }
 
+TEST(RWSpinLockTest, Lock) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    mutex.lock();
+    mutex.unlock();
+    // Check that unlock unlocked.
+    mutex.lock();
+    mutex.unlock();
+}
 
+TEST(RWSpinLockTest, TryLock) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    EXPECT_TRUE(mutex.try_lock());
+    EXPECT_FALSE(mutex.try_lock());
+    mutex.unlock();
+    mutex.lock();
+    EXPECT_FALSE(mutex.try_lock());
+    mutex.unlock();
+}
+
+TEST(RWSpinLockTest, LockShared) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    mutex.lock_shared();
+    mutex.lock_shared();
+    mutex.unlock_shared();
+    mutex.lock_shared();
+    mutex.unlock_shared();
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, TryLockShared) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    EXPECT_TRUE(mutex.try_lock_shared());
+    EXPECT_TRUE(mutex.try_lock_shared());
+    mutex.unlock_shared();
+    EXPECT_TRUE(mutex.try_lock_shared());
+    mutex.unlock_shared();
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, LockSharedWhileLocked) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    bool done = false;
+    mutex.lock();
+    ScopedThread thread([&] {
+        mutex.lock_shared();
+        EXPECT_TRUE(done);
+        mutex.unlock_shared();
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    done = true;
+    mutex.unlock();
+}
+
+TEST(RWSpinLockTest, TryLockSharedWhileLocked) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    mutex.lock();
+    EXPECT_FALSE(mutex.try_lock_shared());
+    mutex.unlock();
+}
+
+TEST(RWSpinLockTest, LockWhileLockShared) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    bool done = false;
+    mutex.lock_shared();
+    ScopedThread thread([&] {
+        mutex.lock();
+        EXPECT_TRUE(done);
+        mutex.unlock();
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    // Shared lock is not where one does mutability. But it's okay here.
+    done = true;
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, TryLockWhileLockShared) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    mutex.lock_shared();
+    EXPECT_FALSE(mutex.try_lock());
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, LockSharedWhileLockSharedWithPendingLock) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    int state = 0;
+    mutex.lock_shared();
+    ScopedThread thread_lock([&] {
+        mutex.lock();
+        EXPECT_THAT(state, 1);
+        state = 2;
+        mutex.unlock();
+    });
+    // Wait for thread_lock to say that it wants to lock the mutex.
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    ScopedThread thread_lock_shared([&] {
+        mutex.lock_shared();
+        EXPECT_THAT(state, 2);
+        mutex.unlock_shared();
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    // Shared lock is not where one does mutability. But it's okay here.
+    state = 1;
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, TryLockSharedWhileLockSharedWithPendingLock) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    bool done = false;
+    mutex.lock_shared();
+    ScopedThread thread_lock([&] {
+        mutex.lock();
+        EXPECT_TRUE(done);
+        mutex.unlock();
+    });
+    // Wait for thread_lock to say that it wants to lock the mutex.
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    EXPECT_FALSE(mutex.try_lock_shared());
+    // Shared lock is not where one does mutability. But it's okay here.
+    done = true;
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, LockSharedWhileLockSharedWithFailedPendingLock) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    mutex.lock_shared();
+    EXPECT_FALSE(mutex.try_lock());
+    mutex.lock_shared();
+    mutex.unlock_shared();
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, TryLockSharedWhileLockSharedWithFailedPendingLock) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    mutex.lock_shared();
+    EXPECT_FALSE(mutex.try_lock());
+    EXPECT_TRUE(mutex.try_lock_shared());
+    mutex.unlock_shared();
+    mutex.unlock_shared();
+}
+
+TEST(RWSpinLockTest, Counter) {
+    RWSpinLock<MutexThreadStateHandling::kIgnore> mutex;
+    // tsan will help catch if the lock breaks memory ordering.
+    uint64_t counter = 0;
+    constexpr uint64_t boundary = 10000;
+    std::atomic<bool> canStart = false;
+    std::vector<ScopedThread> writers;
+    for (int i = 0; i < 2; ++i) {
+        writers.emplace_back([&] {
+            while (!canStart.load(std::memory_order_acquire)) {
+            }
+            while (true) {
+                std::unique_lock guard(mutex);
+                if (counter == boundary) return;
+                counter += 1;
+            }
+        });
+    }
+    std::vector<ScopedThread> readers;
+    for (int i = 0; i < kDefaultThreadCount; ++i) {
+        readers.emplace_back([&] {
+            while (!canStart.load(std::memory_order_acquire)) {
+            }
+            while (true) {
+                std::shared_lock guard(mutex);
+                EXPECT_THAT(counter, testing::Ge(uint64_t(0)));
+                EXPECT_THAT(counter, testing::Le(boundary));
+                if (counter == boundary) return;
+            }
+        });
+    }
+    canStart.store(true, std::memory_order_release);
+    readers.clear();
+    writers.clear();
+    EXPECT_THAT(counter, boundary);
+}
