18f80b1ab552c656309b67fb477d284547e5f070
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-03-31 17:14:17 +0300

    [FIR] KT-57665: Ensure the current K2 behavior differs from K1

---
+++ compiler/testData/diagnostics/testsWithJsStdLib/dynamicUnresolved.fir.kt
@@ -0,0 +1,15 @@
+// ISSUE: KT-57665
+// DIAGNOSTICS: -UNUSED_PARAMETER
+
+fun <T> T.foo(a: String, b: (T) -> Unit) {
+    this.asDynamic().goo(jso {
+        this.<!UNRESOLVED_REFERENCE!>asjhasdas<!>
+    })
+}
+
+fun <T : Any> jso(): T =
+    js("({})")
+
+fun <T : Any> jso(
+    block: T.() -> Unit,
+): T = jso<T>().apply(block)
