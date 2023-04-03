031b0555596c178a481f71db5c3d49dcbc79b730
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2023-04-03 09:24:21 +0000

    Pin API and language version to 1.8 for Kotlin facet serialization used by IJ
    Facet serialization is located in Kotlin repo, and it uses new(1.9.*) stdlib in compile dependencies. But when it is used by IJ it has previous runtime stdlib dependency (looks like 1.8.0). So here is workaround, that specifies apiVersion and languageVersion, that does not produce such problematic outputs.
    
    #KTIJ-24903 Fixed
    
    Merge-request: KT-MR-9333
    Merged-by: Aleksei Cherepanov <aleksei.cherepanov@jetbrains.com>
    (cherry picked from commit 5c039e25fc270aebd0d9a7d1c553b7c1268c2290)

--- jps/jps-platform-api-signatures/build.gradle.kts
+++ jps/jps-platform-api-signatures/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
     kotlin("jvm")
     id("jps-compatible")
@@ -9,4 +12,9 @@ dependencies {
 
 sourceSets {
     "main" { projectDefault() }
+}
+
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    compilerOptions.apiVersion.value(KotlinVersion.KOTLIN_1_8).finalizeValueOnRead()
+    compilerOptions.languageVersion.value(KotlinVersion.KOTLIN_1_8).finalizeValueOnRead()
 }
