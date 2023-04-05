2bba06052557e29acfe60d1c9fd13e6b795d0548
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-04-05 17:12:30 +0200

    [cls] contract: write contracts information to cls
    KTIJ-24665
    this information would be used to create resolved FirElements from stubs,
    so no ProtoBuf would be kept in memory

--- analysis/decompiled/decompiler-to-file-stubs/tests/org/jetbrains/kotlin/analysis/decompiler/stub/files/AbstractAdditionalStubInfoTest.kt
+++ analysis/decompiled/decompiler-to-file-stubs/tests/org/jetbrains/kotlin/analysis/decompiler/stub/files/AbstractAdditionalStubInfoTest.kt
@@ -13,7 +13,6 @@ import org.jetbrains.kotlin.psi.KtProjectionKind
 import org.jetbrains.kotlin.psi.stubs.impl.*
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import java.nio.file.Paths
-import kotlin.reflect.KClass
 
 abstract class AbstractAdditionalStubInfoTest : AbstractDecompiledClassTest() {
     fun runTest(testDirectory: String) {
@@ -33,6 +32,18 @@ abstract class AbstractAdditionalStubInfoTest : AbstractDecompiledClassTest() {
                 if (upperBound != null) {
                     builder.append("    ft: ")
                     appendFlexibleTypeInfo(builder, upperBound)
+                }
+            }
+            is KotlinFunctionStubImpl -> {
+                val contract = stub.contract
+                if (contract != null) {
+                    builder.append("\n" + "  ".repeat(level))
+                        .append(
+                            contract.joinToString("\n" + "  ".repeat(level), "effect: ") { effect ->
+                                effect.effectType.name + "; " + effect.conclusion.toString() + "; " +
+                                        (effect.invocationKind?.name ?: "no invocation kind") + "; " +
+                                        effect.arguments?.joinToString(", ", "args: [", "]")
+                            })
                 }
             }
             is KotlinPropertyStubImpl -> {
--- analysis/decompiled/decompiler-to-file-stubs/tests/org/jetbrains/kotlin/analysis/decompiler/stub/files/AdditionalStubInfoTestGenerated.java
+++ analysis/decompiled/decompiler-to-file-stubs/tests/org/jetbrains/kotlin/analysis/decompiler/stub/files/AdditionalStubInfoTestGenerated.java
@@ -31,6 +31,12 @@ public class AdditionalStubInfoTestGenerated extends AbstractAdditionalStubInfoT
     }
 
     @Test
+    @TestMetadata("Contracts")
+    public void testContracts() throws Exception {
+        runTest("analysis/decompiled/decompiler-to-file-stubs/testData/additionalClsStubInfo/Contracts/");
+    }
+
+    @Test
     @TestMetadata("AnnotationValues")
     public void testAnnotationValues() throws Exception {
         runTest("analysis/decompiled/decompiler-to-file-stubs/testData/additionalClsStubInfo/AnnotationValues/");
