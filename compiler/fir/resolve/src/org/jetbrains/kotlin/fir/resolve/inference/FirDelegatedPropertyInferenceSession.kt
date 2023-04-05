f48ece6d4f6f4d54640a47ec1876ee65a8df6b1f
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-04-05 18:25:35 +0300

    [FIR] Replace `_` with `#` in type variables
    During debugging in IntelliJ it may be hard
    to read `__T`, because it's written with a
    non-monospace font. `##T` looks clearer.

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/FirDelegatedPropertyInferenceSession.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/FirDelegatedPropertyInferenceSession.kt
@@ -243,7 +243,7 @@ class FirDelegatedPropertyInferenceSession(
             val variable = variableWithConstraints.typeVariable as ConeTypeVariable
 
             val syntheticVariable = syntheticTypeVariableByTypeVariable.getOrPut(variable) {
-                ConeTypeVariable("_" + variable.typeConstructor.name).also {
+                ConeTypeVariable("#" + variable.typeConstructor.name, variable.typeConstructor.originalTypeParameter).also {
                     currentConstraintSystem.registerVariable(it)
                 }
             }
