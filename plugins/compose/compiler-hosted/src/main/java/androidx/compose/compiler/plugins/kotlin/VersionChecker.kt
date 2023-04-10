a185c3861f444a778ef4bfed52debfd07d835c8d
Jim S <jsproch@google.com>
2023-04-10 15:04:51 -0700

    Bump Compose Compiler to 1.4.5 for stable release after Kotlin upgrade.
    Change-Id: If78b9276f0ef5955273189db98c19cd2c9d0b1ba ( https://android-review.git.corp.google.com/q/If78b9276f0ef5955273189db98c19cd2c9d0b1ba )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -124,7 +124,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.5.0-alpha03"
+        const val compilerVersion: String = "1.4.5"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
