e7225d29ffb99f696dac71dac4a801397dcd9b32
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2023-04-12 23:45:42 +0300

    [K/N] Mark unit testing API with ExperimentalNativeApi
    As a part of efforts to stabilize Native stdlib #KT-55765.

--- kotlin-native/runtime/src/main/kotlin/kotlin/native/internal/test/TestRunner.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/native/internal/test/TestRunner.kt
@@ -5,12 +5,14 @@
 
 package kotlin.native.internal.test
 
+import kotlin.experimental.ExperimentalNativeApi
 import kotlin.IllegalArgumentException
 import kotlin.text.StringBuilder
 import kotlin.time.TimeSource
 import kotlin.time.measureTime
 
 @OptIn(kotlin.time.ExperimentalTime::class)
+@ExperimentalNativeApi
 internal class TestRunner(val suites: List<TestSuite>, args: Array<String>) {
     private val filters = mutableListOf<(TestCase) -> Boolean>()
     private val listeners = mutableSetOf<TestListener>()
