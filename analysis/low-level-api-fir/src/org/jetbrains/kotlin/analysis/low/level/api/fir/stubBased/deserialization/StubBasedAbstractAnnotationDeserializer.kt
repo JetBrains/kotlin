f3d9f1c8f30ebbba6fe53cdfff67e7aec275c996
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-04-13 14:21:42 +0200

    deserialize annotations

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/stubBased/deserialization/StubBasedAbstractAnnotationDeserializer.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/stubBased/deserialization/StubBasedAbstractAnnotationDeserializer.kt
@@ -7,28 +7,22 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.stubBased.deserializatio
 
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.declarations.FirValueParameter
-import org.jetbrains.kotlin.fir.expressions.FirAnnotation
-import org.jetbrains.kotlin.fir.expressions.FirAnnotationArgumentMapping
-import org.jetbrains.kotlin.fir.expressions.FirConstExpression
-import org.jetbrains.kotlin.fir.expressions.FirExpression
-import org.jetbrains.kotlin.fir.expressions.builder.buildAnnotation
-import org.jetbrains.kotlin.fir.expressions.builder.buildAnnotationArgumentMapping
-import org.jetbrains.kotlin.fir.expressions.builder.buildConstExpression
-import org.jetbrains.kotlin.fir.resolve.ScopeSession
-import org.jetbrains.kotlin.fir.resolve.defaultType
-import org.jetbrains.kotlin.fir.resolve.scope
+import org.jetbrains.kotlin.fir.declarations.FirRegularClass
+import org.jetbrains.kotlin.fir.declarations.collectEnumEntries
+import org.jetbrains.kotlin.fir.expressions.*
+import org.jetbrains.kotlin.fir.expressions.builder.*
+import org.jetbrains.kotlin.fir.references.builder.buildFromMissingDependenciesNamedReference
+import org.jetbrains.kotlin.fir.references.builder.buildResolvedNamedReference
 import org.jetbrains.kotlin.fir.resolve.toSymbol
-import org.jetbrains.kotlin.fir.scopes.FakeOverrideTypeCalculator
-import org.jetbrains.kotlin.fir.scopes.getDeclaredConstructors
-import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
 import org.jetbrains.kotlin.fir.symbols.lazyDeclarationResolver
 import org.jetbrains.kotlin.fir.types.*
 import org.jetbrains.kotlin.fir.types.builder.buildResolvedTypeRef
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.StandardClassIds
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
+import org.jetbrains.kotlin.psi.stubs.impl.*
 import org.jetbrains.kotlin.types.ConstantValueKind
 
 abstract class StubBasedAbstractAnnotationDeserializer(
@@ -140,13 +134,28 @@ abstract class StubBasedAbstractAnnotationDeserializer(
         val userType =
             ktAnnotation.getStubOrPsiChild(KtStubElementTypes.CONSTRUCTOR_CALLEE)?.getStubOrPsiChild(KtStubElementTypes.TYPE_REFERENCE)
                 ?.getStubOrPsiChild(KtStubElementTypes.USER_TYPE)!!
-        val classId = userType.classId()
+        return deserializeAnnotation(
+            userType.classId(),
+            (ktAnnotation.stub as? KotlinAnnotationEntryStubImpl)?.valueArguments,
+            useSiteTarget
+        )
+    }
+
+    private fun deserializeAnnotation(
+        classId: ClassId,
+        valueArguments: Map<Name, ConstantValue<*>>?,
+        useSiteTarget: AnnotationUseSiteTarget? = null
+    ): FirAnnotation {
         return buildAnnotation {
             annotationTypeRef = buildResolvedTypeRef {
                 type = classId.toLookupTag().constructClassType(emptyArray(), isNullable = false)
             }
             session.lazyDeclarationResolver.disableLazyResolveContractChecksInside {
-                this.argumentMapping = createArgumentMapping(ktAnnotation, classId)
+                this.argumentMapping = buildAnnotationArgumentMapping {
+                    valueArguments?.forEach { (name, constantValue) ->
+                        mapping[name] = resolveValue(constantValue)
+                    }
+                }
             }
             useSiteTarget?.let {
                 this.useSiteTarget = it
@@ -154,125 +163,74 @@ abstract class StubBasedAbstractAnnotationDeserializer(
         }
     }
 
-    private fun createArgumentMapping(
-        ktAnnotation: KtAnnotationEntry,
-        classId: ClassId
-    ): FirAnnotationArgumentMapping {
-        return buildAnnotationArgumentMapping build@{
-            if (ktAnnotation.valueArguments.isEmpty()) return@build
-            // Used only for annotation parameters of array types
-            // Avoid triggering it in other cases, since it's quite expensive
-            val parameterByName: Map<Name, FirValueParameter>? by lazy(LazyThreadSafetyMode.NONE) {
-                val lookupTag = classId.toLookupTag()
-                val symbol = lookupTag.toSymbol(session)
-                val firAnnotationClass = (symbol as? FirRegularClassSymbol)?.fir ?: return@lazy null
-
-                val classScope =
-                    firAnnotationClass.defaultType()
-                        .scope(session, ScopeSession(), FakeOverrideTypeCalculator.DoNothing, requiredPhase = null)
-                        ?: error("Null scope for $classId")
-
-                val constructor =
-                    classScope.getDeclaredConstructors().singleOrNull()?.fir ?: error("No single constructor found for $classId")
-
-                constructor.valueParameters.associateBy { it.name }
-            }
-
-            ktAnnotation.valueArguments.mapNotNull {
-                val name = it.getArgumentName()?.asName ?: error("Default value should be present")
-                val value = resolveValue(it.getArgumentExpression()) { parameterByName?.get(name)?.returnTypeRef?.coneType }
-                name to value
-            }.toMap(mapping)
-        }
-    }
-
     private fun resolveValue(
-        value: KtExpression?, expectedType: () -> ConeKotlinType?
+        value: ConstantValue<*>
     ): FirExpression {
-//        val isUnsigned = Flags.IS_UNSIGNED.get(value.flags)
-//
-//        return when (value.type) {
-//            BYTE -> {
-//                val kind = if (isUnsigned) ConstantValueKind.UnsignedByte else ConstantValueKind.Byte
-//                const(kind, value.intValue.toByte(), session.builtinTypes.byteType)
-//            }
-//
-//            SHORT -> {
-//                val kind = if (isUnsigned) ConstantValueKind.UnsignedShort else ConstantValueKind.Short
-//                const(kind, value.intValue.toShort(), session.builtinTypes.shortType)
-//            }
-//
-//            INT -> {
-//                val kind = if (isUnsigned) ConstantValueKind.UnsignedInt else ConstantValueKind.Int
-//                const(kind, value.intValue.toInt(), session.builtinTypes.intType)
-//            }
-//
-//            LONG -> {
-//                val kind = if (isUnsigned) ConstantValueKind.UnsignedLong else ConstantValueKind.Long
-//                const(kind, value.intValue, session.builtinTypes.longType)
-//            }
-//
-//            CHAR -> const(ConstantValueKind.Char, value.intValue.toInt().toChar(), session.builtinTypes.charType)
-//            FLOAT -> const(ConstantValueKind.Float, value.floatValue, session.builtinTypes.floatType)
-//            DOUBLE -> const(ConstantValueKind.Double, value.doubleValue, session.builtinTypes.doubleType)
-//            BOOLEAN -> const(ConstantValueKind.Boolean, (value.intValue != 0L), session.builtinTypes.booleanType)
-//            STRING -> const(ConstantValueKind.String, nameResolver.getString(value.stringValue), session.builtinTypes.stringType)
-//            ANNOTATION -> deserializeAnnotation(value.annotation, nameResolver)
-//            CLASS -> buildGetClassCall {
-//                val classId = nameResolver.getClassId(value.classId)
-//                val lookupTag = classId.toLookupTag()
-//                val referencedType = lookupTag.constructType(emptyArray(), isNullable = false)
-//                val resolvedTypeRef = buildResolvedTypeRef {
-//                    type = StandardClassIds.KClass.constructClassLikeType(arrayOf(referencedType), false)
+        return when (value) {
+            is EnumValue -> value.enumClassId.toEnumEntryReferenceExpression(value.enumEntryName)
+            is KClassValue -> buildGetClassCall {
+                val lookupTag = value.value.classId.toLookupTag()
+                val referencedType = lookupTag.constructType(emptyArray(), isNullable = false)
+                val resolvedTypeRef = buildResolvedTypeRef {
+                    type = StandardClassIds.KClass.constructClassLikeType(arrayOf(referencedType), false)
+                }
+                argumentList = buildUnaryArgumentList(
+                    buildClassReferenceExpression {
+                        classTypeRef = buildResolvedTypeRef { type = referencedType }
+                        typeRef = resolvedTypeRef
+                    }
+                )
+            }
+            is ArrayValue -> buildArrayOfCall {
+                argumentList = buildArgumentList {
+                    value.value.mapTo(arguments) { resolveValue(it) }
+                }
+//                typeRef = buildResolvedTypeRef {
+//                    type = expectedArrayElementType.createArrayType() //todo
 //                }
-//                argumentList = buildUnaryArgumentList(
-//                    buildClassReferenceExpression {
-//                        classTypeRef = buildResolvedTypeRef { type = referencedType }
-//                        typeRef = resolvedTypeRef
-//                    }
-//                )
-//                typeRef = resolvedTypeRef
-//            }
-//            ENUM -> buildFunctionCall {
-//                val classId = nameResolver.getClassId(value.classId)
-//                val entryName = nameResolver.getName(value.enumValueId)
-//
-//                val enumLookupTag = classId.toLookupTag()
-//                val enumSymbol = enumLookupTag.toSymbol(session)
-//                val firClass = enumSymbol?.fir as? FirRegularClass
-//                val enumEntries = firClass?.collectEnumEntries() ?: emptyList()
-//                val enumEntrySymbol = enumEntries.find { it.name == entryName }
-//                calleeReference = enumEntrySymbol?.let {
-//                    buildResolvedNamedReference {
-//                        name = entryName
-//                        resolvedSymbol = it.symbol
-//                    }
-//                } ?: buildErrorNamedReference {
-//                    diagnostic =
-//                        ConeSimpleDiagnostic("Strange deserialized enum value: $classId.$entryName", DiagnosticKind.DeserializationError)
-//                }
-//                if (enumEntrySymbol != null) {
-//                    typeRef = enumEntrySymbol.returnTypeRef
-//                }
-//            }
-//            ARRAY -> {
-//                val expectedArrayElementType = expectedType()?.arrayElementType() ?: session.builtinTypes.anyType.type
-//                buildArrayOfCall {
-//                    argumentList = buildArgumentList {
-//                        value.arrayElementList.mapTo(arguments) { resolveValue(it) { expectedArrayElementType } }
-//                    }
-//                    typeRef = buildResolvedTypeRef {
-//                        type = expectedArrayElementType.createArrayType()
-//                    }
-//                }
-//            }
-//
-//            else -> error("Unsupported annotation argument type: ${value.type} (expected $expectedType)")
-//        }
-        error("Not yet implemented $value $expectedType")
+            }
+            //todo
+            is AnnotationValue -> deserializeAnnotation(value.annoClassId, value.args.map { Name.identifier("value") to it }.toMap())
+            is BooleanValue -> const(ConstantValueKind.Boolean, value.value, session.builtinTypes.booleanType)
+            is ByteValue -> const(ConstantValueKind.Byte, value.value, session.builtinTypes.byteType)
+            is CharValue -> const(ConstantValueKind.Char, value.value, session.builtinTypes.charType)
+            is ShortValue -> const(ConstantValueKind.Short, value.value, session.builtinTypes.shortType)
+            is LongValue -> const(ConstantValueKind.Long, value.value, session.builtinTypes.longType)
+            is FloatValue -> const(ConstantValueKind.Float, value.value, session.builtinTypes.floatType)
+            is DoubleValue -> const(ConstantValueKind.Double, value.value, session.builtinTypes.doubleType)
+            is UByteValue -> const(ConstantValueKind.UnsignedByte, value.value, session.builtinTypes.byteType)
+            is UShortValue -> const(ConstantValueKind.UnsignedShort, value.value, session.builtinTypes.shortType)
+            is UIntValue -> const(ConstantValueKind.UnsignedInt, value.value, session.builtinTypes.intType)
+            is ULongValue -> const(ConstantValueKind.UnsignedLong, value.value, session.builtinTypes.longType)
+            is IntValue -> const(ConstantValueKind.Int, value.value, session.builtinTypes.intType)
+            NullValue -> const(ConstantValueKind.Null, null, session.builtinTypes.nullableAnyType)
+            is StringValue -> const(ConstantValueKind.String, value.value, session.builtinTypes.stringType)
+            else -> error("Unexpected value $value")
+        }
     }
 
     private fun <T> const(kind: ConstantValueKind<T>, value: T, typeRef: FirResolvedTypeRef): FirConstExpression<T> {
         return buildConstExpression(null, kind, value, setType = true).apply { this.replaceTypeRef(typeRef) }
+    }
+
+    private fun ClassId.toEnumEntryReferenceExpression(entryName: Name): FirExpression {
+        return buildPropertyAccessExpression {
+            val enumLookupTag = toLookupTag()
+            val enumSymbol = enumLookupTag.toSymbol(session)
+            val firClass = enumSymbol?.fir as? FirRegularClass
+            val enumEntries = firClass?.collectEnumEntries() ?: emptyList()
+            val enumEntrySymbol = enumEntries.find { it.name == entryName }
+            calleeReference = enumEntrySymbol?.let {
+                buildResolvedNamedReference {
+                    name = entryName
+                    resolvedSymbol = it.symbol
+                }
+            } ?: buildFromMissingDependenciesNamedReference {
+                name = entryName
+            }
+            if (enumEntrySymbol != null) {
+                typeRef = enumEntrySymbol.returnTypeRef
+            }
+        }
     }
 }
