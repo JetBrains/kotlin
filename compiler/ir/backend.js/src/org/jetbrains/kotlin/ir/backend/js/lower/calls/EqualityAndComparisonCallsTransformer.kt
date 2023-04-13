09bccd272d5a532237b271d92c0fed75d356bbcc
Artem Kobzar <artem.kobzar@jetbrains.com>
2023-04-13 08:39:03 +0000

    [K/JS] Prevent Char boxing on value or reference equals call

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/calls/EqualityAndComparisonCallsTransformer.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/calls/EqualityAndComparisonCallsTransformer.kt
@@ -5,7 +5,6 @@
 
 package org.jetbrains.kotlin.ir.backend.js.lower.calls
 
-import org.jetbrains.kotlin.descriptors.InlineClassRepresentation
 import org.jetbrains.kotlin.ir.backend.js.JsIrBackendContext
 import org.jetbrains.kotlin.ir.backend.js.ir.JsIrBuilder
 import org.jetbrains.kotlin.ir.backend.js.utils.isEqualsInheritedFromAny
@@ -28,7 +27,7 @@ class EqualityAndComparisonCallsTransformer(context: JsIrBackendContext) : Calls
 
     init {
         symbolToTransformer.run {
-            add(irBuiltIns.eqeqeqSymbol, intrinsics.jsEqeqeq)
+            add(irBuiltIns.eqeqeqSymbol, ::transformEqeqeqOperator)
             add(irBuiltIns.eqeqSymbol, ::transformEqeqOperator)
             // ieee754equals can only be applied in between statically known Floats, Doubles, null or undefined
             add(irBuiltIns.ieee754equalsFunByOperandType, ::chooseEqualityOperatorForPrimitiveTypes)
@@ -72,6 +71,21 @@ class EqualityAndComparisonCallsTransformer(context: JsIrBackendContext) : Calls
             Name.identifier("equals") -> transformEqualsMethodCall(call as IrCall)
             else -> call
         }
+    }
+
+    private fun transformEqeqeqOperator(call: IrFunctionAccessExpression): IrExpression {
+        val lhs = call.getValueArgument(0)!!
+        val rhs = call.getValueArgument(1)!!
+
+        return if (lhs.isCharBoxing() && rhs.isCharBoxing()) {
+            optimizeInlineClassEquality(call, lhs, rhs)
+        } else {
+            irCall(call, intrinsics.jsEqeqeq)
+        }
+    }
+
+    private fun IrExpression.isCharBoxing(): Boolean {
+        return this is IrCall && symbol == intrinsics.jsBoxIntrinsic && getValueArgument(0)!!.type.isChar()
     }
 
     private fun transformEqeqOperator(call: IrFunctionAccessExpression): IrExpression {
@@ -186,7 +200,7 @@ class EqualityAndComparisonCallsTransformer(context: JsIrBackendContext) : Calls
         isBuiltin() && this != PrimitiveType.FLOATING_POINT_NUMBER
 
     private fun IrType.isDefaultEqualsMethod() =
-        findEqualsMethod()?.origin === IrDeclarationOrigin.GENERATED_SINGLE_FIELD_VALUE_CLASS_MEMBER
+        isChar() || findEqualsMethod()?.origin === IrDeclarationOrigin.GENERATED_SINGLE_FIELD_VALUE_CLASS_MEMBER
 
     private fun IrExpression.isBoxIntrinsic() =
         this is IrCall && symbol == icUtils.boxIntrinsic
@@ -204,7 +218,7 @@ class EqualityAndComparisonCallsTransformer(context: JsIrBackendContext) : Calls
             call.putValueArgument(0, lhsUnboxed)
             call.putValueArgument(1, rhsUnboxed)
 
-            if (lhsUnboxed.type.getLowestUnderlyingType().getPrimitiveType().canBeUsedWithJsEq()) {
+            if (lhsUnboxed.type.isChar() || lhsUnboxed.type.getLowestUnderlyingType().getPrimitiveType().canBeUsedWithJsEq()) {
                 return chooseEqualityOperatorForPrimitiveTypes(call)
             }
         }
