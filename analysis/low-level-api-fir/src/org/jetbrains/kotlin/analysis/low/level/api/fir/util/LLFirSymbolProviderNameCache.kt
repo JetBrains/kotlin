9dcc9d0d102e4979f287144202628bae61539ba5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-04-17 16:58:37 +0200

    [LL FIR] introduce LLFirCompositeSymbolProviderNameCache
    ^KT-57930

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/LLFirSymbolProviderNameCache.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/LLFirSymbolProviderNameCache.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.caches.createCache
 import org.jetbrains.kotlin.fir.caches.firCachesFactory
 import org.jetbrains.kotlin.fir.caches.getValue
+import org.jetbrains.kotlin.fir.resolve.providers.flatMapToNullableSet
 import org.jetbrains.kotlin.fir.resolve.providers.mayHaveTopLevelClassifier
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
@@ -56,5 +57,44 @@ internal abstract class LLFirSymbolProviderNameCacheBase(
         if (name.isSpecial) return true
         val names = getTopLevelCallableNamesInPackage(packageFqName) ?: return true
         return name in names
+    }
+}
+
+
+internal object LLFirEmptySymbolProviderNameCache : LLFirSymbolProviderNameCache() {
+    override fun getTopLevelClassifierNamesInPackage(packageFqName: FqName): Set<String>? = emptySet()
+    override fun getTopLevelCallableNamesInPackage(packageFqName: FqName): Set<Name>? = emptySet()
+    override fun mayHaveTopLevelClassifier(classId: ClassId, mayHaveFunctionClass: Boolean): Boolean = false
+    override fun mayHaveTopLevelCallable(packageFqName: FqName, name: Name): Boolean = false
+}
+
+internal class LLFirCompositeSymbolProviderNameCache
+private constructor(
+    private val caches: List<LLFirSymbolProviderNameCache>
+) : LLFirSymbolProviderNameCache() {
+    override fun getTopLevelClassifierNamesInPackage(packageFqName: FqName): Set<String>? {
+        return caches.flatMapToNullableSet { it.getTopLevelClassifierNamesInPackage(packageFqName) }
+    }
+
+    override fun getTopLevelCallableNamesInPackage(packageFqName: FqName): Set<Name>? {
+        return caches.flatMapToNullableSet { it.getTopLevelCallableNamesInPackage(packageFqName) }
+    }
+
+    override fun mayHaveTopLevelClassifier(classId: ClassId, mayHaveFunctionClass: Boolean): Boolean {
+        return caches.any { it.mayHaveTopLevelClassifier(classId, mayHaveFunctionClass) }
+    }
+
+    override fun mayHaveTopLevelCallable(packageFqName: FqName, name: Name): Boolean {
+        return caches.any { it.mayHaveTopLevelCallable(packageFqName, name) }
+    }
+
+    companion object {
+        fun create(caches: List<LLFirSymbolProviderNameCache>): LLFirSymbolProviderNameCache {
+            return when (caches.size) {
+                0 -> LLFirEmptySymbolProviderNameCache
+                1 -> caches.single()
+                else -> LLFirCompositeSymbolProviderNameCache(caches)
+            }
+        }
     }
 }
