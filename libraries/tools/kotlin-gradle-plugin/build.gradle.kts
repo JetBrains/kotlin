9f003328cfaa8ece0e1a708d7659a08d1f21c842
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-04-18 13:58:13 +0200

    [Gradle] Setup initial api validation for ExternalTargetApi
    KT-55524

--- libraries/tools/kotlin-gradle-plugin/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin/build.gradle.kts
@@ -5,6 +5,7 @@ import org.jetbrains.kotlin.pill.PillExtension
 plugins {
     id("gradle-plugin-common-configuration")
     id("jps-compatible")
+    id("org.jetbrains.kotlinx.binary-compatibility-validator")
 }
 
 repositories {
@@ -24,6 +25,12 @@ kotlin.sourceSets.all {
     languageSettings.optIn("org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi")
     languageSettings.optIn("org.jetbrains.kotlin.gradle.ExternalKotlinTargetApi")
     languageSettings.optIn("org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi")
+}
+
+apiValidation {
+    publicMarkers.add("org.jetbrains.kotlin.gradle.ExternalKotlinTargetApi")
+    nonPublicMarkers.add("org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi")
+    additionalSourceSets.add("common")
 }
 
 dependencies {
