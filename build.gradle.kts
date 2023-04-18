425c7c738ee9828050dc5a0e4024732f8f39432c
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-04-18 13:04:05 +0200

    Update kotlin binary compatibility validator to v0.13.1
    KT-55524

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ plugins {
     id("jps-compatible")
     id("org.jetbrains.gradle.plugin.idea-ext")
     id("org.gradle.crypto.checksum") version "1.4.0"
-    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.0" apply false
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.1" apply false
     signing
     id("org.jetbrains.kotlin.jvm") apply false
     id("org.jetbrains.kotlin.plugin.serialization") apply false
