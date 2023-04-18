f29e505bd3c63b261d0d8f085aa9b273106245fb
Leonid Startsev <leonid.startsev@jetbrains.com>
2023-04-18 14:54:23 +0200

    Use correct check in FirSerializationPluginClassChecker to determine if the given type is a enum class.
    Otherwise, findTypeSerializerOrContextUnchecked works incorrectly when analyzing code
    compiled with Kotlin 1.7.20 and serialization 1.4.1.
    
    #KT-57704 Fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/K2KotlinxSerializationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/K2KotlinxSerializationIT.kt
@@ -8,6 +8,10 @@ package org.jetbrains.kotlin.gradle
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Path
+import kotlin.io.path.listDirectoryEntries
+import kotlin.test.assertTrue
 
 @OtherGradlePluginTests
 class K2KotlinxSerializationIT : KGPBaseTest() {
@@ -29,6 +33,25 @@ class K2KotlinxSerializationIT : KGPBaseTest() {
         project("kotlinxSerializationK2AgainstK1", gradleVersion) {
             build(":app:run") {
                 assertTasksExecuted(":app:run")
+            }
+        }
+    }
+
+    @DisplayName("Compile code with kotlinx.serialization K2 against old K1 library without enum factory support (KT-57704).")
+    @GradleTest
+    fun `test kotlinx serialization K2 against K1 library`(gradleVersion: GradleVersion, @TempDir tempDir: Path) {
+        project("kotlinxSerializationK2AgainstK1Lib/lib", gradleVersion, localRepoDir = tempDir) {
+            build(":publish") {
+                assertTasksExecuted(":publish")
+                val publishedLib = tempDir.resolve("com/example/serialization_lib/serialization_lib/1.0")
+                assertDirectoryExists(publishedLib)
+                assertTrue(publishedLib.listDirectoryEntries().isNotEmpty())
+            }
+        }
+
+        project("kotlinxSerializationK2AgainstK1Lib/app", gradleVersion, localRepoDir = tempDir) {
+            build(":run") {
+                assertTasksExecuted(":run")
             }
         }
     }
---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kotlinxSerializationK2AgainstK1Lib/app/build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+    `maven-publish`
+    application
+}
+
+repositories {
+    mavenLocal()
+    maven("<localRepo>")
+    mavenCentral()
+}
+
+group = "com.example.serialization_app"
+version = "1.0"
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("com.example.serialization_lib:serialization_lib:1.0")
+}
+
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.compilerOptions {
+    languageVersion.set(KotlinVersion.KOTLIN_2_0)
+}
+
+application {
+    mainClass.set("com.example.serialization_app.MainKt")
+}
