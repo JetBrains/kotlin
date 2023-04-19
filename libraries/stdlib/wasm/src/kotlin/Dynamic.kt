740d67bd64b3dc4d674217ee63537e958d31d6b0
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2023-04-19 15:27:35 +0200

    [Wasm] Revert removal of some Dynamic functions
    Deprecate them with ERROR instead to improve compiler messages

--- libraries/stdlib/wasm/src/kotlin/Dynamic.kt
+++ libraries/stdlib/wasm/src/kotlin/Dynamic.kt
@@ -13,6 +13,26 @@ package kotlin.js
 @Deprecated("Use JsAny instead", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("JsAny"))
 public external interface Dynamic : JsAny
 
+/**
+ * Reinterprets this value as a value of the Dynamic type.
+ */
+@Deprecated("If value is a subtype of JsAny, use JsAny instead. Otherwise, use toJsHandle", level = DeprecationLevel.ERROR)
+fun Any.asDynamic(): JsAny = this.toJsHandle()
+
+/**
+ * Reinterprets this value as a value of the Dynamic type.
+ */
+@Deprecated("Use toJsString instead", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("toJsString"))
+@kotlin.internal.InlineOnly
+fun String.asDynamic(): JsString = this.toJsString()
+
+/**
+ * Reinterprets boolean value as a value of the specified type [T] without any actual type checking.
+ */
+@Deprecated("Use toJsBoolean instead", level = DeprecationLevel.ERROR, replaceWith = ReplaceWith("toJsBoolean"))
+@Suppress("UNCHECKED_CAST")
+fun <T> Boolean.unsafeCast(): T = this.toJsBoolean() as T
+
 private fun jsThrow(e: JsAny) {
     js("throw e;")
 }
