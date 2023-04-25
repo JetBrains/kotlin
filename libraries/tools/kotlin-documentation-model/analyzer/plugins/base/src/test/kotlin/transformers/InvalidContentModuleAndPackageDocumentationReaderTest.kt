4cd8bba0a7d40acb34691e6fb3ae322960a9fcb1
Lukas Knoch-Girstmair <strangesource@users.noreply.github.com>
2023-04-25 23:04:33 +0200

    [KDM] Replace invalid documentation links
    See https://github.com/Kotlin/dokka/pull/2973
    Original commit: https://github.com/Kotlin/dokka/commit/79aaba062ebcb9e22599263b0f2a9dee3be688d1

--- libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/transformers/InvalidContentModuleAndPackageDocumentationReaderTest.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/transformers/InvalidContentModuleAndPackageDocumentationReaderTest.kt
@@ -75,7 +75,7 @@ class InvalidContentModuleAndPackageDocumentationReaderTest : AbstractContextMod
             runCatching { readerA[dModule(name = "moduleA", sourceSets = setOf(sourceSetA))] }.exceptionOrNull()
         assertEquals(
             "Unexpected classifier: \"Invalid\", expected either \"Module\" or \"Package\". \n" +
-                    "For more information consult the specification: https://kotlinlang.org/docs/reference/dokka-module-and-package-docs.html",
+                    "For more information consult the specification: https://kotlinlang.org/docs/dokka-module-and-package-docs.html",
             exception?.message
         )
     }
