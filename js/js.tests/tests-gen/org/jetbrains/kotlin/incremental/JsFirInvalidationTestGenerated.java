81b591ed21c4f3d50f59efa0a0d15b0ea371550e
Alexander Korepanov <alexander.korepanov@jetbrains.com>
2023-04-25 14:53:34 +0200

    [JS IR IC] Interface default implementations affect IC hash
    Adding or removing a method or property with
    a default implementation to an interface should
    invalidate all children: we must regenerate JS code for them
    
    ^KT-56237 Fixed

--- js/js.tests/tests-gen/org/jetbrains/kotlin/incremental/JsFirInvalidationTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/incremental/JsFirInvalidationTestGenerated.java
@@ -320,6 +320,12 @@ public class JsFirInvalidationTestGenerated extends AbstractJsFirInvalidationTes
     }
 
     @Test
+    @TestMetadata("localObjectsLeakThroughInterface")
+    public void testLocalObjectsLeakThroughInterface() throws Exception {
+        runTest("js/js.translator/testData/incremental/invalidation/localObjectsLeakThroughInterface/");
+    }
+
+    @Test
     @TestMetadata("mainModuleInvalidation")
     public void testMainModuleInvalidation() throws Exception {
         runTest("js/js.translator/testData/incremental/invalidation/mainModuleInvalidation/");
@@ -371,6 +377,12 @@ public class JsFirInvalidationTestGenerated extends AbstractJsFirInvalidationTes
     @TestMetadata("privateInlineFunction1")
     public void testPrivateInlineFunction1() throws Exception {
         runTest("js/js.translator/testData/incremental/invalidation/privateInlineFunction1/");
+    }
+
+    @Test
+    @TestMetadata("privateObjectsLeakThroughSealedInterface")
+    public void testPrivateObjectsLeakThroughSealedInterface() throws Exception {
+        runTest("js/js.translator/testData/incremental/invalidation/privateObjectsLeakThroughSealedInterface/");
     }
 
     @Test
