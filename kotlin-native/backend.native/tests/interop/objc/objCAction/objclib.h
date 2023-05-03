860d0e32f776a4b2c32e9986a6e6d54dc6bf65b0
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2023-05-03 16:03:29 +0200

    [K/N] Add tests on ObjCAction and ObjCOutlet. ^KT-58441

---
+++ kotlin-native/backend.native/tests/interop/objc/objCAction/objclib.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+void performSelector0(id target, NSString* selectorName);
+void performSelectorInNewThread0(id target, NSString* selectorName);
+void performSelector1(id target, NSString* selectorName, id arg1);
+void performSelectorInNewThread1(id target, NSString* selectorName, id arg1);
+void performSelector2(id target, NSString* selectorName, id arg1, id arg2);
+void performSelectorInNewThread2(id target, NSString* selectorName, id arg1, id arg2);
+void setProperty(id target, NSString* propertyName, id value);
