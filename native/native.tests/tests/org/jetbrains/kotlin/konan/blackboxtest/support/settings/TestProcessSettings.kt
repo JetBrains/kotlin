68ffa46187e7b15e1506e0befef2994e8c246d7a
Pavel Punegov <pavel.punegov@jetbrains.com>
2023-05-04 08:50:14 +0000

    [K/N][test] Increase default test timeout to 30 seconds
    Sometimes on slower hosts longer running test may time out.
    
    Merge-request: KT-MR-9935
    Merged-by: Pavel Punegov <Pavel.Punegov@jetbrains.com>

--- native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/support/settings/TestProcessSettings.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/support/settings/TestProcessSettings.kt
@@ -181,7 +181,7 @@ internal class BaseDirs(val testBuildDir: File)
  */
 internal class Timeouts(val executionTimeout: Duration) {
     companion object {
-        val DEFAULT_EXECUTION_TIMEOUT: Duration get() = 15.seconds
+        val DEFAULT_EXECUTION_TIMEOUT: Duration get() = 30.seconds
     }
 }
 
