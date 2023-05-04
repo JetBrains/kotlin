5e3bf71b0778dc0809ed1a7ee2c898ea7c3860d5
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-05-04 11:56:24 +0200

    [Gradle] Kotlin2JsCompile: convert enhancedFreeCompilerArgs to ArrayList
    ... using .toList():
    Using ListProperty.get() can return List implementations
    that do not play well with CompilerArguments.
    In this particular case RegularImmutableList from guava was returned
    which later failed in the copyBeanTo function
    
    ^KT-58427 Verification Pending
    
    (cherry picked from commit ca8af7786edf01485abb68489d38dbe27e252ba0)

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/tasks/Kotlin2JsCompile.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/tasks/Kotlin2JsCompile.kt
@@ -167,7 +167,7 @@ abstract class Kotlin2JsCompile @Inject constructor(
 
             // Overriding freeArgs from compilerOptions with enhanced one + additional one set on execution phase
             // containing additional arguments based on the js compilation configuration
-            args.freeArgs = executionTimeFreeCompilerArgs ?: enhancedFreeCompilerArgs.get()
+            args.freeArgs = executionTimeFreeCompilerArgs ?: enhancedFreeCompilerArgs.get().toList()
         }
 
         pluginClasspath { args ->
