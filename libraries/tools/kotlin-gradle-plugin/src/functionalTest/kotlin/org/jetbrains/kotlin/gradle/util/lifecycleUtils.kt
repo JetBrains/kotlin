b53bf4b148a91f24404abf04e577997fec568865
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-05-04 10:03:10 +0200

    [Gradle][Minor] runLifecycleAwareTest: Prefer re-throwing AssertionError
    ^KT-58255 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/lifecycleUtils.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/lifecycleUtils.kt
@@ -8,8 +8,17 @@ package org.jetbrains.kotlin.gradle.util
 import org.gradle.api.Project
 import org.gradle.api.internal.project.ProjectInternal
 import org.jetbrains.kotlin.gradle.plugin.kotlinPluginLifecycle
+import org.jetbrains.kotlin.tooling.core.withLinearClosure
+import java.lang.AssertionError
 
 fun Project.runLifecycleAwareTest(block: suspend Project.() -> Unit) {
     kotlinPluginLifecycle.launch { block() }
-    (this as ProjectInternal).evaluate()
+    try {
+        (this as ProjectInternal).evaluate()
+    } catch (t: Throwable) {
+        /* Prefer throwing AssertionError directly, if possible */
+        val allCauses = t.withLinearClosure { it.cause }
+        allCauses.find { it is AssertionError }?.let { throw it }
+        throw t
+    }
 }
