2980f8d9dce873fa24b75d2e18f3d62cfd4bc31e
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-05-08 20:12:26 +0200

    TESTS WIP

--- compiler/testData/diagnostics/tests/constructorConsistency/openProperty.fir.kt
+++ compiler/testData/diagnostics/tests/constructorConsistency/openProperty.fir.kt
@@ -1,7 +1,7 @@
-open class Base {
-    open var x: Int
+abstract class Base {
+    abstract var x: Int
 
-    open var y: Int
+    abstract var y: Int
 
     constructor() {
         x = 42
@@ -9,6 +9,5 @@ open class Base {
         val temp = this.x
         this.x = y
         y = temp
-
     }
 }
