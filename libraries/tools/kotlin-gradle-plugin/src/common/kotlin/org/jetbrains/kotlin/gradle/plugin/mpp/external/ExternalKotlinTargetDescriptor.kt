f588b06ca5777bbafa8e151ec4b053ce6db92a1c
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-05-10 15:22:54 +0200

    [Gradle] Remove .configure duplication in ExternalKotlinTargetDescriptorBuilder
    KT-58601

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/external/ExternalKotlinTargetDescriptor.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/external/ExternalKotlinTargetDescriptor.kt
@@ -48,13 +48,13 @@ interface ExternalKotlinTargetDescriptor<T : DecoratedExternalKotlinTarget> {
  */
 @ExternalKotlinTargetApi
 fun <T : DecoratedExternalKotlinTarget> ExternalKotlinTargetDescriptor(
-    configure: ExternalKotlinTargetDescriptorBuilder<T>.() -> Unit
+    configure: ExternalKotlinTargetDescriptorBuilder<T>.() -> Unit,
 ): ExternalKotlinTargetDescriptor<T> {
     return ExternalKotlinTargetDescriptorBuilder<T>().also(configure).build()
 }
 
 /**
- * Mutable version of [ExternalKotlinTargetDescriptor]
+ * Builder for [ExternalKotlinTargetDescriptor]
  * The following properties have to be specified:
  * - [targetName]
  * - [platformType]
@@ -84,12 +84,7 @@ class ExternalKotlinTargetDescriptorBuilder<T : DecoratedExternalKotlinTarget> i
     val runtimeElementsPublished: ExternalKotlinTargetConfigurationDescriptorBuilder<T> =
         ExternalKotlinTargetConfigurationDescriptorBuilder()
 
-    /**
-     * Generic configuration that will be invoked when building the target.
-     * This configuration is called right after creating the instance and before
-     * publishing the target to all subscribers of `kotlin.targets.all {}`
-     */
-    var configure: ((T) -> Unit)? = null
+    private var configure: ((T) -> Unit)? = null
 
     /**
      * Generic configuration that will be invoked when building the target.
@@ -102,14 +97,7 @@ class ExternalKotlinTargetDescriptorBuilder<T : DecoratedExternalKotlinTarget> i
         else this.configure = { configure(it); action(it) }
     }
 
-    /**
-     * Main entrance of configuring the ide import:
-     * The [IdeMultiplatformImport] instance passed to this function shall
-     * not be captured and used outside of this block.
-     *
-     * The [IdeMultiplatformImport] instance shall not be retrieved any other way than using this function.
-     */
-    var configureIdeImport: (IdeMultiplatformImport.() -> Unit)? = null
+    internal var configureIdeImport: (IdeMultiplatformImport.() -> Unit)? = null
 
     /**
      * Main entrance of configuring the ide import:
