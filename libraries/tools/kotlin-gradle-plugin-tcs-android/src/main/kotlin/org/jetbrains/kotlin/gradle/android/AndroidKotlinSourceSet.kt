cbf21faa249c496779d83a590ac2fe8759acc2d2
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-05-10 15:25:38 +0200

    [Gradle] Remove old ExternalTargetApiExtensions
    KT-58601
    
    (cherry picked from commit d9f572bc0b50695238c04d62abd411083ce7ef26)

--- libraries/tools/kotlin-gradle-plugin-tcs-android/src/main/kotlin/org/jetbrains/kotlin/gradle/android/AndroidKotlinSourceSet.kt
+++ libraries/tools/kotlin-gradle-plugin-tcs-android/src/main/kotlin/org/jetbrains/kotlin/gradle/android/AndroidKotlinSourceSet.kt
@@ -6,7 +6,6 @@
 package org.jetbrains.kotlin.gradle.android
 
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import org.jetbrains.kotlin.gradle.plugin.mpp.external.extras
 import org.jetbrains.kotlin.tooling.core.extrasKeyOf
 
 class AndroidKotlinSourceSet {
