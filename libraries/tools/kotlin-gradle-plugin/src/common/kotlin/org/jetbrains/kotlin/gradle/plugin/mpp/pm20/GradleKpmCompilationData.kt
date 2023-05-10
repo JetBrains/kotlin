e2c2e2be10e732433e216c422f2eb02f178d9827
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-05-10 23:27:14 +0200

    [Gradle] Move pm20 interfaces from plugin/api into plugin codebase
    ^KT-58619 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/GradleKpmCompilationData.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/pm20/GradleKpmCompilationData.kt
@@ -3,14 +3,12 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:Suppress("TYPEALIAS_EXPANSION_DEPRECATION")
-
 package org.jetbrains.kotlin.gradle.plugin.mpp.pm20
 
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.file.SourceDirectorySet
-import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptionsDeprecated
+import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
 import org.jetbrains.kotlin.gradle.plugin.HasCompilerOptions
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilationOutput
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
@@ -20,7 +18,7 @@ import org.jetbrains.kotlin.project.model.LanguageSettings
 Previously used to find a common representation for KPM and TCS.
 Not in use anymore. Can be removed/inlined into KPM later.
  */
-interface GradleKpmCompilationData<T : KotlinCommonOptionsDeprecated> {
+interface GradleKpmCompilationData<T : KotlinCommonOptions> {
     val project: Project
     val owner: Any?
 
