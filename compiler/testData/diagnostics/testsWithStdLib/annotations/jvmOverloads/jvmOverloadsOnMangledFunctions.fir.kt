21ee2dfb8b95b537c5d4a1e23c7cebe219cca2e4
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2023-05-11 04:09:14 +0200

    [FIR, FE1.0] Add MFVC test for @JvmOverloads
    #KT-1179

--- compiler/testData/diagnostics/testsWithStdLib/annotations/jvmOverloads/jvmOverloadsOnMangledFunctions.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/jvmOverloads/jvmOverloadsOnMangledFunctions.fir.kt
@@ -1,7 +1,8 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
-// !LANGUAGE: +InlineClasses, -JvmInlineValueClasses
+// !LANGUAGE: +ValueClasses
 
-inline class Z(val x: Int)
+@JvmInline
+value class Z(val x: Int)
 
 @JvmOverloads
 fun testTopLevelFunction1(z: Z, x: Int = 0) {}
@@ -27,4 +28,34 @@ class C {
 
     @JvmOverloads
     fun testMemberFunction3(x: Int = 0): Z = Z(x)
+}
+
+
+@JvmInline
+value class ZZ(val x: Int, val y: Int)
+
+@JvmOverloads
+fun testTopLevelFunction1(ZZ: ZZ, x: Int = 0) {}
+
+@JvmOverloads
+fun testTopLevelFunction2(x: Int, ZZ: ZZ = ZZ(0, 1)) {}
+
+@JvmOverloads
+fun testTopLevelFunction3ZZ(x: Int = 0): ZZ = ZZ(x, x + 1)
+
+class C1 {
+    @JvmOverloads
+    constructor(i: Int, ZZ: ZZ = ZZ(0, 1))
+
+    @JvmOverloads
+    constructor(s: String, ZZ: ZZ, i: Int = 0)
+
+    @JvmOverloads
+    fun testMemberFunction1(ZZ: ZZ, x: Int = 0) {}
+
+    @JvmOverloads
+    fun testMemberFunction2(x: Int, ZZ: ZZ = ZZ(0, 1)) {}
+
+    @JvmOverloads
+    fun testMemberFunction3(x: Int = 0): ZZ = ZZ(x, x + 1)
 }
