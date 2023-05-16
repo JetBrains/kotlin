9ffd66050ecc8b500d9eab47ec01552cb820d9c5
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-05-16 10:47:44 +0200

    fixup! [FIR] fix false negative MUST_BE_INITIALIZED for deferred init + custom setter case

--- compiler/testData/ir/irText/classes/initVar.kt
+++ compiler/testData/ir/irText/classes/initVar.kt
@@ -16,24 +16,3 @@ class TestInitVarWithCustomSetter {
     var x = 0
         set(value) { field = value }
 }
-
-class TestInitVarWithCustomSetterWithExplicitCtor {
-    var x: Int
-        set(value) { field = value }
-
-    init {
-        x = 0
-    }
-
-    constructor()
-}
-
-class TestInitVarWithCustomSetterInCtor {
-    var x: Int set(value) {
-        field = value
-    }
-
-    constructor() {
-        x = 42
-    }
-}
