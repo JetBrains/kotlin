75fe25cc4207c680ab8ad6a54aa10da007e1eba0
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-05-16 17:11:47 +0200

    [Test] Refactor DeferredInit testData to make it more readable
    1. Fix weird indentation in ValDeferredInit*
    2. Use meaningful names in ValDeferredInit*
    3. Drop leading indentation in VarDeferredInit*. Not everyone has a wide
       monitor
    4. Add some NOTICABLE comment that VarDeferredInit* test should be read
       as table

--- compiler/testData/diagnostics/tests/backingField/ValDeferredInitInOpenClassOpenValError.kt
+++ compiler/testData/diagnostics/tests/backingField/ValDeferredInitInOpenClassOpenValError.kt
@@ -1,55 +1,48 @@
 // FIR_IDENTICAL
 // DIAGNOSTICS: -DEBUG_INFO_LEAKING_THIS
 // LANGUAGE:+ProhibitOpenValDeferredInitialization
-// a = final + not initialized in place + deferred init
-// e = final + not initialized in place
-// c = final + initialized in place
-
-// b = open + not initialized in place + deferred init
-// f = open + not initialized in place
-// d = open + initialized in place
 open class Foo {
     // no getter
-                                                             val a0: Int
-                       <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>val e0: Int<!>
-                                                             val c0: Int = 1
-            <!MUST_BE_INITIALIZED_OR_FINAL_OR_ABSTRACT!>open val b0: Int<!>
-                  <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>open val f0: Int<!>
-                                                        open val d0: Int = 1
+    val final_notInitializedInPlace_deferredInit0: Int
+    <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>val final_notInitializedInPlace0: Int<!>
+    val final_initializedInPlace0: Int = 1
+    <!MUST_BE_INITIALIZED_OR_FINAL_OR_ABSTRACT!>open val open_notInitializedInPlace_deferredInit0: Int<!>
+    <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>open val open_notInitializedInPlace0: Int<!>
+    open val open_initializedInPlace0: Int = 1
 
     // getter with field
-                                                    val a1: Int; get() = field
-                             <!MUST_BE_INITIALIZED!>val e1: Int<!>; get() = field
-                                                    val c1: Int = 1; get() = field
-            <!MUST_BE_INITIALIZED_OR_BE_FINAL!>open val b1: Int<!>; get() = field
-                        <!MUST_BE_INITIALIZED!>open val f1: Int<!>; get() = field
-                                               open val d1: Int = 1; get() = field
+    val final_notInitializedInPlace_deferredInit1: Int; get() = field
+    <!MUST_BE_INITIALIZED!>val final_notInitializedInPlace1: Int<!>; get() = field
+    val final_initializedInPlace1: Int = 1; get() = field
+    <!MUST_BE_INITIALIZED_OR_BE_FINAL!>open val open_notInitializedInPlace_deferredinit1: Int<!>; get() = field
+    <!MUST_BE_INITIALIZED!>open val open_notInitializedInPlace1: Int<!>; get() = field
+    open val open_initializedInPlace1: Int = 1; get() = field
 
     // getter with empty body
-                                                             val a2: Int; get
-                       <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>val e2: Int<!>; get
-                                                             val c2: Int = 1; get
-            <!MUST_BE_INITIALIZED_OR_FINAL_OR_ABSTRACT!>open val b2: Int<!>; get
-                  <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>open val f2: Int<!>; get
-                                                        open val d2: Int = 1; get
+    val final_notInitializedInPlace_deferredInit2: Int; get
+    <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>val final_notInitializedInPlace2: Int<!>; get
+    val final_initializedInPlace2: Int = 1; get
+    <!MUST_BE_INITIALIZED_OR_FINAL_OR_ABSTRACT!>open val open_notInitializedInPlace_deferredinit2: Int<!>; get
+    <!MUST_BE_INITIALIZED_OR_BE_ABSTRACT!>open val open_notInitializedInPlace2: Int<!>; get
+    open val open_initializedInPlace2: Int = 1; get
 
     // getter no field
-         val a3: Int; get() = 1
-         val e3: Int; get() = 1
-         val c3: Int = <!PROPERTY_INITIALIZER_NO_BACKING_FIELD!>1<!>; get() = 1
-    open val b3: Int; get() = 1
-    open val f3: Int; get() = 1
-    open val d3: Int = <!PROPERTY_INITIALIZER_NO_BACKING_FIELD!>1<!>; get() = 1
+    val final_notInitializedInPlace_deferredInit3: Int; get() = 1
+    val final_notInitializedInPlace3: Int; get() = 1
+    val final_initializedInPlace3: Int = <!PROPERTY_INITIALIZER_NO_BACKING_FIELD!>1<!>; get() = 1
+    open val open_notInitializedInPlace_deferredinit3: Int; get() = 1
+    open val open_notInitializedInPlace3: Int; get() = 1
+    open val open_initializedInPlace3: Int = <!PROPERTY_INITIALIZER_NO_BACKING_FIELD!>1<!>; get() = 1
 
     init {
-        a0 = 1
-        a1 = 1
-        a2 = 1
-        <!VAL_REASSIGNMENT!>a3<!> = 1
+        final_notInitializedInPlace_deferredInit0 = 1
+        final_notInitializedInPlace_deferredInit1 = 1
+        final_notInitializedInPlace_deferredInit2 = 1
+        <!VAL_REASSIGNMENT!>final_notInitializedInPlace_deferredInit3<!> = 1
 
-        b0 = 1
-        b1 = 1
-        b2 = 1
-        <!VAL_REASSIGNMENT!>b3<!> = 1
+        open_notInitializedInPlace_deferredInit0 = 1
+        open_notInitializedInPlace_deferredinit1 = 1
+        open_notInitializedInPlace_deferredinit2 = 1
+        <!VAL_REASSIGNMENT!>open_notInitializedInPlace_deferredinit3<!> = 1
     }
 }
