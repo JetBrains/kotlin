d1a37526266a7f3a502746b2fa09d017526cc7c7
Igor Chevdar <igor.chevdar@jetbrains.com>
2023-05-19 13:07:51 +0300

    [K/N][tests] Fixed lldb tests on arrays

--- kotlin-native/runtime/src/main/cpp/Types.h
+++ kotlin-native/runtime/src/main/cpp/Types.h
@@ -72,7 +72,7 @@ extern const TypeInfo* theWorkerBoundReferenceTypeInfo;
 extern const TypeInfo* theCleanerImplTypeInfo;
 extern const TypeInfo* theRegularWeakReferenceImplTypeInfo;
 
-KBoolean IsInstance(const ObjHeader* obj, const TypeInfo* type_info) RUNTIME_PURE;
+KBoolean IsInstance(const ObjHeader* obj, const TypeInfo* type_info) RUNTIME_PURE RUNTIME_USED; // Used in konan_lldb.py
 KBoolean IsSubtype(const TypeInfo* obj_type_info, const TypeInfo* type_info) RUNTIME_PURE;
 KBoolean IsSubclassFast(const TypeInfo* obj_type_info, int32_t lo, int32_t hi) RUNTIME_PURE;
 void CheckCast(const ObjHeader* obj, const TypeInfo* type_info);
