6f3544cdda08a904b6e611966b3496184356edce
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2023-05-23 15:14:14 +0200

    ~ Good K1 Unmute

--- compiler/testData/codegen/box/multiplatform/defaultArguments/withTypeParameter.kt
+++ compiler/testData/codegen/box/multiplatform/defaultArguments/withTypeParameter.kt
@@ -1,6 +1,5 @@
 // !LANGUAGE: +MultiPlatformProjects
-// IGNORE_BACKEND: WASM
-// IGNORE_BACKEND_K2: JVM_IR, JS_IR, NATIVE
+// IGNORE_BACKEND_K2: JVM_IR, JS_IR, NATIVE, WASM
 // FIR status: outdated code (expect/actual in the same module)
 // WITH_STDLIB
 // MODULE: lib
