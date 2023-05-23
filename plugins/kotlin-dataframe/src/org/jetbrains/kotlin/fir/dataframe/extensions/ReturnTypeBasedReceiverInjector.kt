c4f968e1cd6c010ac82b84b367f6159d02d111e5
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2023-05-23 21:56:58 +0300

    [KDF] prototype ReturnTypeBasedReceiverInjector
    Moved from https://github.com/Kotlin/dataframe/commit/4a21c8223401b0892b5277b8fadda810635c15c7

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlin/fir/dataframe/extensions/ReturnTypeBasedReceiverInjector.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlin/fir/dataframe/extensions/ReturnTypeBasedReceiverInjector.kt
@@ -1,12 +1,31 @@
 package org.jetbrains.kotlin.fir.dataframe.extensions
 
 import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.dataframe.Names
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.fir.extensions.FirExpressionResolutionExtension
+import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
+import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.fir.types.classId
+import org.jetbrains.kotlin.fir.types.coneTypeSafe
+import org.jetbrains.kotlin.fir.types.toSymbol
 
 class ReturnTypeBasedReceiverInjector(session: FirSession) : FirExpressionResolutionExtension(session) {
     override fun addNewImplicitReceivers(functionCall: FirFunctionCall): List<ConeKotlinType> {
-        TODO("Not yet implemented")
+        val token = generatedTokenOrNull(functionCall) ?: return emptyList()
+        val symbol = token.toSymbol(session)!! as FirRegularClassSymbol
+        return symbol.declarationSymbols.filterIsInstance<FirPropertySymbol>().map { it.resolvedReturnType }
+    }
+
+    private fun generatedTokenOrNull(call: FirFunctionCall): ConeClassLikeType? {
+        val callReturnType = call.typeRef.coneTypeSafe<ConeClassLikeType>() ?: return null
+        if (callReturnType.classId != Names.DF_CLASS_ID) return null
+        val rootMarker = callReturnType.typeArguments[0]
+        if (rootMarker !is ConeClassLikeType) {
+            return null
+        }
+        return rootMarker.takeIf { it.classId?.shortClassName?.asString()?.startsWith("Token") == true }
     }
 }
