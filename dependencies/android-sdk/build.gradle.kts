02489dcd75a0d47fd13997e6849e23f312e84be0
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2023-05-23 11:25:28 +0200

    Build: Add android sdk to `implicitDependencies`
     #KTI-72

--- dependencies/android-sdk/build.gradle.kts
+++ dependencies/android-sdk/build.gradle.kts
@@ -25,6 +25,24 @@ repositories {
     }
 }
 
+val buildToolsVersion = "r29.0.3"
+val platformToolsVersion = "r28.0.1"
+val sdkToolsVersion = "4333796" /*26.1.1*/
+
+dependencies {
+    implicitDependencies("google:build-tools:$buildToolsVersion:linux@zip")
+    implicitDependencies("google:build-tools:$buildToolsVersion:windows@zip")
+    implicitDependencies("google:build-tools:$buildToolsVersion:macosx@zip")
+
+    implicitDependencies("google:platform-tools:$platformToolsVersion:linux@zip")
+    implicitDependencies("google:platform-tools:$platformToolsVersion:windows@zip")
+    implicitDependencies("google:platform-tools:$platformToolsVersion:darwin@zip")
+
+    implicitDependencies("google:sdk-tools-linux:$sdkToolsVersion@zip")
+    implicitDependencies("google:sdk-tools-windows:$sdkToolsVersion@zip")
+    implicitDependencies("google:sdk-tools-darwin:$sdkToolsVersion@zip")
+}
+
 val androidSdk by configurations.creating
 val androidJar by configurations.creating
 val androidPlatform by configurations.creating
@@ -128,10 +146,9 @@ fun unzipSdkTask(
 
 unzipSdkTask("platform", "26_r02", "platforms/android-26", "", androidPlatform, 1, prepareTask = preparePlatform)
 unzipSdkTask("android_m2repository", "r44", "extras/android", "")
-unzipSdkTask("platform-tools", "r28.0.1", "", toolsOsDarwin)
-unzipSdkTask("sdk-tools-$toolsOsDarwin", "4333796"/*26.1.1*/, "", "")
-unzipSdkTask("build-tools", "r28.0.3", "build-tools/28.0.3", toolsOs, buildTools, 1)
-unzipSdkTask("build-tools", "r29.0.3", "build-tools/29.0.3", toolsOs, buildTools, 1)
+unzipSdkTask("platform-tools", platformToolsVersion, "", toolsOsDarwin)
+unzipSdkTask("sdk-tools-$toolsOsDarwin", sdkToolsVersion, "", "")
+unzipSdkTask("build-tools", buildToolsVersion, "build-tools/29.0.3", toolsOs, buildTools, 1)
 unzipSdkTask("emulator-$toolsOsDarwin", "5264690", "", "", prepareTask = prepareEmulator)
 unzipSdkTask("armeabi-v7a", "19", "system-images/android-19/default","r05", prepareTask = prepareEmulator)
 if (!kotlinBuildProperties.isTeamcityBuild) {
