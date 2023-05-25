03a795def1df23bfe31ca0e6f781f5e5e7e48429
James Groom <OSSYoshiRulz+GitHub@gmail.com>
2023-05-25 22:46:22 +1000

    Uncomment `binarySearchBy` overload taking a `Comparator`

--- libraries/stdlib/src/kotlin/collections/Collections.kt
+++ libraries/stdlib/src/kotlin/collections/Collections.kt
@@ -403,9 +403,23 @@ public inline fun <T, K : Comparable<K>> List<T>.binarySearchBy(
 ): Int =
     binarySearch(fromIndex, toIndex) { compareValues(selector(it), key) }
 
-// do not introduce this overload --- too rare
-//public fun <T, K> List<T>.binarySearchBy(key: K, comparator: Comparator<K>, fromIndex: Int = 0, toIndex: Int = size(), selector: (T) -> K): Int =
-//        binarySearch(fromIndex, toIndex) { comparator.compare(selector(it), key) }
+/**
+ * Searches this list or its range for an element having the key returned by the specified [selector] function
+ * equal to the provided [key] value using the binary search algorithm.
+ * The list is expected to be sorted into ascending order according to the ordering of keys of its elements as given by the specified [comparator].
+ * otherwise the result is undefined.
+ *
+ * If the list contains multiple elements with the specified [key], there is no guarantee which one will be found.
+ *
+ * @return the index of the element with the specified [key], if it is contained in the list within the specified range;
+ * otherwise, the inverted insertion point `(-insertion point - 1)`.
+ * The insertion point is defined as the index at which the element should be inserted,
+ * so that the list (or the specified subrange of list) still remains sorted.
+ * @sample samples.collections.Collections.Lists.binarySearchByKey
+ * @sample samples.collections.Collections.Lists.binarySearchWithComparator
+ */
+public fun <T, K> List<T>.binarySearchBy(key: K, comparator: Comparator<K>, fromIndex: Int = 0, toIndex: Int = size, selector: (T) -> K): Int =
+    binarySearch(fromIndex, toIndex) { comparator.compare(selector(it), key) }
 
 
 /**
