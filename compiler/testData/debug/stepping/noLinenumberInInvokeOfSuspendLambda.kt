a41d36edba7cfc00087b1fcd93bc6986a7d407a7
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2023-05-30 11:31:34 +0200

    JS IR: Minor. Disable test

--- compiler/testData/debug/stepping/noLinenumberInInvokeOfSuspendLambda.kt
+++ compiler/testData/debug/stepping/noLinenumberInInvokeOfSuspendLambda.kt
@@ -1,4 +1,6 @@
 // WITH_STDLIB
+// TODO: Enable the test on JS BE, but now it is too flaky there.
+// TARGET_BACKEND: JVM
 // FILE: test.kt
 
 import kotlin.coroutines.*
@@ -21,61 +23,34 @@ fun box() {
 }
 
 // EXPECTATIONS JVM JVM_IR
-// test.kt:15 box
-// test.kt:7 builder
+// test.kt:17 box
+// test.kt:9 builder
 // Continuation.kt:66 <init>
-// test.kt:7 builder
+// test.kt:9 builder
 // Continuation.kt:68 getContext
 // test.kt:-1 <init>
 // test.kt:-1 create
-// test.kt:12 empty
-// test.kt:16 invokeSuspend
-// test.kt:17 invokeSuspend
-// test.kt:7 builder
-// Continuation.kt:66 <init>
-// test.kt:7 builder
-// Continuation.kt:68 getContext
-// test.kt:-1 <init>
-// test.kt:-1 create
-// test.kt:12 empty
+// test.kt:14 empty
 // test.kt:18 invokeSuspend
 // test.kt:19 invokeSuspend
-// Continuation.kt:71 resumeWith
-// test.kt:8 resumeWith
-// test.kt:9 resumeWith
-// Continuation.kt:71 resumeWith
-// test.kt:10 builder
+// test.kt:9 builder
+// Continuation.kt:66 <init>
+// test.kt:9 builder
+// Continuation.kt:68 getContext
+// test.kt:-1 <init>
+// test.kt:-1 create
+// test.kt:14 empty
 // test.kt:20 invokeSuspend
+// test.kt:21 invokeSuspend
 // Continuation.kt:71 resumeWith
-// test.kt:8 resumeWith
-// test.kt:9 resumeWith
+// test.kt:10 resumeWith
+// test.kt:11 resumeWith
 // Continuation.kt:71 resumeWith
-// test.kt:10 builder
-// test.kt:21 box
-
-// EXPECTATIONS JS_IR
-// test.kt:15 box
-// test.kt:15 box$slambda
-// test.kt:15 box
-// test.kt:82 <init>
-// test.kt:7 builder
-// test.kt:82 <get-context>
-// test.kt:82 <get-context>
-// test.kt:16 doResume
-// test.kt:12 empty
-// test.kt:17 doResume
-// test.kt:17 box$slambda$slambda
-// test.kt:17 doResume
-// test.kt:82 <init>
-// test.kt:7 builder
-// test.kt:82 <get-context>
-// test.kt:82 <get-context>
-// test.kt:18 doResume
-// test.kt:12 empty
-// test.kt:19 doResume
-// test.kt:82 resumeWith
-// test.kt:10 builder
-// test.kt:20 doResume
-// test.kt:82 resumeWith
-// test.kt:10 builder
-// test.kt:21 box
+// test.kt:12 builder
+// test.kt:22 invokeSuspend
+// Continuation.kt:71 resumeWith
+// test.kt:10 resumeWith
+// test.kt:11 resumeWith
+// Continuation.kt:71 resumeWith
+// test.kt:12 builder
+// test.kt:23 box
