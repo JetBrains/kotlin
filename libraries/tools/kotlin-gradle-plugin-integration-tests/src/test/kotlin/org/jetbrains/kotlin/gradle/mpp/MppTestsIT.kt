98da9619a3e0f12eef5a7530056772df13d1b74c
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-06-08 13:30:16 +0000

    [Gradle, JS] Use workaround to not link provider with task
    ^KTIJ-25757 fixed
    
    (cherry picked from commit b783a87c25402206253812c1d5b3dc581368feb7)

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppTestsIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppTestsIT.kt
@@ -50,4 +50,42 @@ class MppTestsIT : KGPBaseTest() {
             }
         }
     }
+
+    // Android Studio has build service which checks all output files of all tasks in task graph
+    // https://cs.android.com/android-studio/platform/tools/base/+/0185d5af71ba51c64681731f99f319bfcaeb0174:build-system/gradle-core/src/main/java/com/android/build/gradle/internal/attribution/BuildAnalyzerConfiguratorService.kt;l=78-84
+    @DisplayName("KTIJ-25757: MPP is compatible with getting all task output files before execution (Android Studio case)")
+    @GradleTest
+    fun testKtij25757AllTaskOutputFilesBeforeExecution(gradleVersion: GradleVersion) {
+        project(
+            "new-mpp-lib-with-tests",
+            gradleVersion
+        ) {
+            buildGradle.modify {
+                it + "\n" +
+                        """
+                        kotlin {
+                            js {
+                                binaries.executable()
+                                browser {
+                                }
+                            }
+                        }
+                        
+                        gradle.taskGraph.whenReady { taskGraph ->
+                            taskGraph.allTasks.forEach { task ->
+                                // to not execute, just check configuration
+                                task.enabled = false
+                                task.outputs.files.forEach { outputFile ->
+                                    println(outputFile.path)
+                                }
+                            }
+                        }
+                        """.trimIndent()
+
+            }
+
+            build(":assemble") {
+            }
+        }
+    }
 }
