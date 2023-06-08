4c9b94330d737d19a36f3755438f351aa572033d
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-06-08 13:18:06 +0000

    [Gradle, Wasm] Use invariant path separator for wasm karma
    ^KT-58931 fixed

---
+++ libraries/tools/kotlin-gradle-plugin/src/test/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/karma/KotlinKarmaTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.targets.js.testing.karma
+
+import org.junit.Test
+import java.nio.file.Files.createTempDirectory
+import kotlin.test.assertEquals
+
+class KotlinKarmaTest {
+    @Test
+    fun checkLoadWasm() {
+        val npmProjectDir = createTempDirectory("tmp")
+        val executableFile = npmProjectDir.resolve("kotlin/main.mjs")
+
+        val loadWasm = createLoadWasm(npmProjectDir.toFile(), executableFile.toFile())
+
+        assertEquals(
+            "static/load.js",
+            loadWasm.relativeTo(npmProjectDir.toFile()).invariantSeparatorsPath
+        )
+
+        assertEquals(
+            """
+            import exports from "../kotlin/main.mjs";
+            
+            exports.startUnitTests();
+            
+            window.__karma__.loaded();
+            """.trimIndent(),
+            loadWasm.readText().trimIndent()
+        )
+    }
+}
