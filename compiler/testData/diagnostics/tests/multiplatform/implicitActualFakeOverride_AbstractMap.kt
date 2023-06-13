e5048435222b932d26a3c59b89a3ec414d8e4bc1
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-06-13 13:10:32 +0200

    [FE] Cover "implicit actualization"/"actual fake-overrides" KT-58544 with regression tests
    Review: https://jetbrains.team/p/kt/reviews/11039/timeline
    (cherry picked from commit ab8913dee8067d7f8254a65c202daddc286bf06f)

---
+++ compiler/testData/diagnostics/tests/multiplatform/implicitActualFakeOverride_AbstractMap.kt
@@ -0,0 +1,15 @@
+// WITH_STDLIB
+// MODULE: m1-common
+// FILE: common.kt
+
+// K2: false positve INCOMPATIBLE_MATCHING: KT-60155
+public expect abstract class AbstractMutableMap<K, V> : MutableMap<K, V> {
+    override val values: MutableCollection<V>
+}
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: jvm.kt
+
+import java.util.AbstractMap
+
+public actual abstract class AbstractMutableMap<K, V>() : MutableMap<K, V>, AbstractMap<K, V>()
