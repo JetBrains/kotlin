6379d40514bd4c1ccd6114b994cfdcdc92831425
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2023-06-13 17:41:58 +0200

    force logs

--- kotlin-native/runtime/src/gc/cms/cpp/ConcurrentMarkAndSweepTest.cpp
+++ kotlin-native/runtime/src/gc/cms/cpp/ConcurrentMarkAndSweepTest.cpp
@@ -1151,7 +1151,11 @@ INSTANTIATE_TEST_SUITE_P(,
             ParallelismOptions{kDefaultThreadCount * 3, true, kDefaultThreadCount},
 
             ParallelismOptions{kDefaultThreadCount / 2, true, kDefaultThreadCount},
-            ParallelismOptions{kDefaultThreadCount / 2 * 3, true, kDefaultThreadCount}
+            ParallelismOptions{kDefaultThreadCount / 2 * 3, true, kDefaultThreadCount},
+            ParallelismOptions{5, true, kDefaultThreadCount},
+            ParallelismOptions{6, true, kDefaultThreadCount},
+            ParallelismOptions{7, true, kDefaultThreadCount},
+            ParallelismOptions{8, true, kDefaultThreadCount}
     ),
     [] (const testing::TestParamInfo<ParallelismOptions>& paramInfo) {
         using namespace std::string_literals;
