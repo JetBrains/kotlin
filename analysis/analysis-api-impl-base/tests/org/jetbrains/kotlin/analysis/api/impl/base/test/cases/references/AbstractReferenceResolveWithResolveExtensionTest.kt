96bea42333d91900484d4b7e1ac577c18cc118c4
Justin Paupore <jpaupore@google.com>
2023-06-14 23:49:03 -0700

    [AA] Allow specifying REx file contents in testdata.
    Tests can now specify the code generated by a resolve extension from
    within the test's testdata. Module-level directives control whether
    resolve extensions are enabled for that module, as well as package names
    and source shadowing regexes. File-level directives allow a `// FILE:`
    block within the testdata to be converted into a KtResolveExtensionFile
    and removed from the module as a whole. (This requires a new
    `ModuleStructureTransformer`, because we need to be able to entirely
    remove the files in question.)
    
    Any test can add support for these directives by calling
    `KtResolveExtensionTestSupport.configure` from within their
    `configureTest` stanza. This allows this functionality to be used in
    conjuction with any test base class.
    
    ^KT-59329

--- analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/references/AbstractReferenceResolveWithResolveExtensionTest.kt
+++ analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/references/AbstractReferenceResolveWithResolveExtensionTest.kt
@@ -5,74 +5,12 @@
 
 package org.jetbrains.kotlin.analysis.api.impl.base.test.cases.references
 
-import com.intellij.openapi.module.Module
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.kotlin.analysis.api.impl.base.test.util.KtResolveExtensionFileForTests
-import org.jetbrains.kotlin.analysis.api.impl.base.test.util.KtResolveExtensionProviderForTestPreAnalysisHandler
-import org.jetbrains.kotlin.analysis.api.resolve.extensions.KtResolveExtensionFile
-import org.jetbrains.kotlin.analysis.api.resolve.extensions.KtResolveExtensionProvider
-import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.test.bind
+import org.jetbrains.kotlin.analysis.api.impl.base.test.cases.resolve.extensions.KtResolveExtensionTestSupport
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 
 abstract class AbstractReferenceResolveWithResolveExtensionTest : AbstractReferenceResolveTest() {
-    abstract fun createResolveExtensionProvider(
-        files: List<KtResolveExtensionFile>,
-        packages: Set<FqName>,
-        shadowedScope: GlobalSearchScope,
-    ): KtResolveExtensionProvider
-
     override fun configureTest(builder: TestConfigurationBuilder) {
         super.configureTest(builder)
-        val provider = createResolveExtensionProvider(
-            files = listOf(
-                KtResolveExtensionFileForTests(
-                    "extension1.kt",
-                    packageName = FqName("generated"),
-                    topLevelClassifiersNames = setOf("GeneratedClass1"),
-                    topLevelCallableNames = setOf(
-                        "generatedTopLevelFunction1",
-                        "generatedTopLevelExtensionFunction1",
-                        "generatedOverloadedExtensionFunction",
-                    ),
-                    fileText = """|package generated
-                       |
-                       |class GeneratedClass1 {
-                       |   fun generatedClassMember1(): Int
-                       |}
-                       |
-                       |fun generatedTopLevelFunction1(): GeneratedClass2
-                       |
-                       |fun String.generatedTopLevelExtensionFunction1(boolean: Boolean): Int
-                       |
-                       |fun Any.generatedOverloadedExtensionFunction(): Int
-                    """.trimMargin()
-                ),
-                KtResolveExtensionFileForTests(
-                    "extension2.kt",
-                    packageName = FqName("generated"),
-                    topLevelClassifiersNames = setOf("GeneratedClass2"),
-                    topLevelCallableNames = setOf(),
-                    fileText = """|package generated
-                       |
-                       |class GeneratedClass2 {
-                       |   fun generatedClassMember2(): GeneratedClass1
-                       |}
-                    """.trimMargin(),
-                )
-            ),
-            packages = setOf(FqName("generated")),
-            shadowedScope = object : GlobalSearchScope() {
-                override fun contains(file: VirtualFile): Boolean = ".hidden." in file.name
-
-                override fun isSearchInModuleContent(aModule: Module): Boolean = false
-
-                override fun isSearchInLibraries(): Boolean = false
-            }
-        )
-        with(builder) {
-            usePreAnalysisHandlers(::KtResolveExtensionProviderForTestPreAnalysisHandler.bind(listOf(provider)))
-        }
+        KtResolveExtensionTestSupport.configure(builder)
     }
 }
