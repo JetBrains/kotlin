de78f52bc93c0f83dfd37343bfa8ebbf56ec3ca2
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2023-06-19 13:51:53 +0200

    Bump Kotlin/Native version to 1.9.20-dev-4192

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,7 +102,7 @@ IdeVersionConfigurator.setCurrentIde(project)
 
 if (!project.hasProperty("versions.kotlin-native")) {
     // BEWARE! Bumping this version doesn't take an immediate effect on TeamCity: KTI-1107
-    extra["versions.kotlin-native"] = "1.9.20-dev-2332"
+    extra["versions.kotlin-native"] = "1.9.20-dev-4192"
 }
 
 val irCompilerModules = arrayOf(
