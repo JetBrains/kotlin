048ecdf9810a0070a4b9b7055cdc5fc5691e9c10
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-06-23 14:57:00 +0200

    [Analysis API] add regression tests for KTIJ-24163
    ^KTIJ-24114 obsolete

--- analysis/analysis-api-standalone/tests-gen/org/jetbrains/kotlin/analysis/api/standalone/fir/test/cases/generated/cases/components/callResolver/FirStandaloneNormalAnalysisSourceModuleResolveCallTestGenerated.java
+++ analysis/analysis-api-standalone/tests-gen/org/jetbrains/kotlin/analysis/api/standalone/fir/test/cases/generated/cases/components/callResolver/FirStandaloneNormalAnalysisSourceModuleResolveCallTestGenerated.java
@@ -972,6 +972,12 @@ public class FirStandaloneNormalAnalysisSourceModuleResolveCallTestGenerated ext
         }
 
         @Test
+        @TestMetadata("assignementInExpression.kt")
+        public void testAssignementInExpression() throws Exception {
+            runTest("analysis/analysis-api/testData/components/callResolver/resolveCall/invalidCode/assignementInExpression.kt");
+        }
+
+        @Test
         @TestMetadata("getterAssignment.kt")
         public void testGetterAssignment() throws Exception {
             runTest("analysis/analysis-api/testData/components/callResolver/resolveCall/invalidCode/getterAssignment.kt");
