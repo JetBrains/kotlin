a55f3c55837404014eff25ea283d2dffd00f1853
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2023-06-26 16:45:49 +0300

    [FIR LT] Set source on FirExplicitSuperReference
    #KT-55835

--- compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/converter/LightTreeRawFirExpressionBuilder.kt
+++ compiler/fir/raw-fir/light-tree2fir/src/org/jetbrains/kotlin/fir/lightTree/converter/LightTreeRawFirExpressionBuilder.kt
@@ -1445,10 +1445,12 @@ class LightTreeRawFirExpressionBuilder(
         }
 
         return buildPropertyAccessExpression {
-            source = superExpression.toFirSourceElement()
+            val sourceElement = superExpression.toFirSourceElement()
+            source = sourceElement
             calleeReference = buildExplicitSuperReference {
                 labelName = label
                 this.superTypeRef = superTypeRef
+                source = sourceElement.fakeElement(KtFakeSourceElementKind.ReferenceInAtomicQualifiedAccess)
             }
         }
     }
