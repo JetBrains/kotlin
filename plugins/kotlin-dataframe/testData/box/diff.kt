a1e156bdc9caee994e01c7a8f2dc724493cf5d05
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2023-06-27 15:34:40 +0300

    [KDF] rework transformations to avoid loops in a tree
    Moved from https://github.com/Kotlin/dataframe/commit/aac8453bd27bf2360f1f06715cc52f8aa21d5f6a

---
+++ plugins/kotlin-dataframe/testData/box/diff.kt
@@ -0,0 +1,52 @@
+@file:Suppress("warnings")
+
+package org.jetbrains.kotlinx.dataframe
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.time.temporal.Temporal
+import kotlin.experimental.ExperimentalTypeInference
+import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
+import org.jetbrains.kotlinx.dataframe.annotations.DisableInterpretation
+import org.jetbrains.kotlinx.dataframe.api.cast
+import org.jetbrains.kotlinx.dataframe.io.*
+import org.jetbrains.kotlinx.dataframe.api.*
+
+//fun <T, V : Temporal> DataRow<T>.diff(unit: ChronoUnit, expression: RowExpression<T, V>): Long? = prev()?.let { p -> unit.between(expression(this, this), expression(p, p)) }
+fun <T, V : Temporal> DataRow<T>.diff(unit: ChronoUnit, expression: RowExpression<T, V>): Long? = prev()?.let { p -> unit.between(expression(p, p), expression(this, this)) }
+
+/**
+char,level,race,charclass,zone,guild,timestamp
+59425,1,Orc,Rogue,Orgrimmar,165,01/01/08 00:02:04
+65494,9,Orc,Hunter,Durotar,-1,01/01/08 00:02:04
+*/
+@DataSchema
+interface ActivePlayer {
+    val char: Int
+    val level: Int
+    val race: String
+    val charclass: String
+    val zone: String
+    val guild: Int
+    val timestamp: String
+}
+
+fun box(): String {
+    val df = @DisableInterpretation DataFrame.readDelimStr("""
+        char,level,race,charclass,zone,guild,timestamp
+        59425,1,Orc,Rogue,Orgrimmar,165,01/01/08 00:02:04
+        65494,9,Orc,Hunter,Durotar,-1,01/01/08 00:02:04
+    """.trimIndent())
+    val df = df.cast<ActivePlayer>()
+
+    val format = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm:ss")
+    val df1 = df
+        .convert { timestamp }.with { LocalDateTime.parse(it, format) }
+        .sortBy { char and timestamp }
+        .add("tsDiff") { diff(ChronoUnit.MINUTES) { timestamp }?.let { it > 20  } ?: true }
+//        .add("charDiff") { diff { char }?.let { it != 0 } ?: true }
+
+    df1.print()
+    return "OK"
+}
