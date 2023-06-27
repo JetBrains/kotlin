01fddfc4bc429c2bcc1f563d6be37cb8479c2129
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2023-06-27 14:07:30 +0200

    Update test data

--- compiler/testData/asJava/lightClasses/lightClassByFqName/defaultMethodInKotlinWithSettingAllCompatibility.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/defaultMethodInKotlinWithSettingAllCompatibility.java
@@ -4,6 +4,8 @@ public abstract interface KtInterface /* KtInterface*/ {
   public abstract void withoutBody();//  withoutBody()
 
   public static final class DefaultImpls /* KtInterface.DefaultImpls*/ {
+    private static int getProp(@org.jetbrains.annotations.NotNull() KtInterface);//  getProp(KtInterface)
+
     private static void privateFun(@org.jetbrains.annotations.NotNull() KtInterface);//  privateFun(KtInterface)
 
     public static void defaultFun(@org.jetbrains.annotations.NotNull() KtInterface);//  defaultFun(KtInterface)
