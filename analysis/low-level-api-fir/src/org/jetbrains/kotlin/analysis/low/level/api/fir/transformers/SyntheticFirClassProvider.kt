988c6acf5757f4728f23ca0ad49c94bc0b984c3e
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-06-28 17:13:17 +0900

    fixup! [LL API] Replace whole-session invalidation with granular FIR guards
    Pass through on-air analysis only for the target declaration.

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/SyntheticFirClassProvider.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/SyntheticFirClassProvider.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.transformers
 
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.FirDesignationWithFile
 import org.jetbrains.kotlin.fir.FirElement
+import org.jetbrains.kotlin.fir.FirElementWithResolveState
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.FirClassLikeDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirFile
@@ -24,8 +25,8 @@ internal interface SyntheticFirClassProvider {
     }
 }
 
-internal val FirSession.isOnAirAnalysis: Boolean
-    get() = onAirProviderForThread.get()?.session == this
+internal val FirSession.onAirAnalysisTarget: FirElementWithResolveState?
+    get() = onAirProviderForThread.get()?.target
 
 /**
  * Injects a designation-based class file provider to 'LLFirProvider'.
@@ -47,6 +48,7 @@ internal fun withOnAirDesignation(designation: FirDesignationWithFile, block: ()
 
 private class OnAirSyntheticFirClassProvider private constructor(
     private val firFile: FirFile,
+    val target: FirElementWithResolveState,
     private val classes: Map<ClassId, FirClassLikeDeclaration>
 ) : SyntheticFirClassProvider {
     val session: FirSession
@@ -76,7 +78,7 @@ private class OnAirSyntheticFirClassProvider private constructor(
             }
 
             nodeInfoCollector.visitElement(firElement)
-            return OnAirSyntheticFirClassProvider(firFile, nodeInfoCollector.classes)
+            return OnAirSyntheticFirClassProvider(firFile, designation.target, nodeInfoCollector.classes)
         }
     }
 }
