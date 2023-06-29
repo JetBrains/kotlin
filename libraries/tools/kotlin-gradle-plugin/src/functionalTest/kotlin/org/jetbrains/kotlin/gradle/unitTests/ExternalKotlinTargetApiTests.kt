24debfd9b743692a6565055172c5cd9a4de6cebe
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-06-29 14:35:40 +0200

    [Gradle] ExternalTargetApi: Implement ExternalKotlinTargetSourcesJarUtils
    This utils are intended to be used external target authors exclusively
    and shall help to create sources.jar files using regular
    Kotlin Multiplatform conventions.
    
    ^KT-58109 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/ExternalKotlinTargetApiTests.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/ExternalKotlinTargetApiTests.kt
@@ -17,39 +17,24 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
 import org.jetbrains.kotlin.gradle.plugin.hierarchy.KotlinSourceSetTreeClassifier
 import org.jetbrains.kotlin.gradle.plugin.hierarchy.orNull
 import org.jetbrains.kotlin.gradle.plugin.launchInStage
-import org.jetbrains.kotlin.gradle.plugin.mpp.external.*
 import org.jetbrains.kotlin.gradle.plugin.mpp.external.ExternalKotlinCompilationDescriptor.CompilationFactory
-import org.jetbrains.kotlin.gradle.plugin.mpp.external.ExternalKotlinTargetDescriptor.TargetFactory
-import org.jetbrains.kotlin.gradle.util.buildProjectWithMPP
-import org.jetbrains.kotlin.gradle.util.runLifecycleAwareTest
+import org.jetbrains.kotlin.gradle.plugin.mpp.external.createCompilation
+import org.jetbrains.kotlin.gradle.plugin.mpp.external.createExternalKotlinTarget
+import org.jetbrains.kotlin.gradle.util.*
 import org.jetbrains.kotlin.gradle.utils.property
 import org.jetbrains.kotlin.gradle.utils.toMap
 import kotlin.test.*
 
 class ExternalKotlinTargetApiTests {
 
-    class FakeTarget(delegate: Delegate) : DecoratedExternalKotlinTarget(delegate)
-    class FakeCompilation(delegate: Delegate) : DecoratedExternalKotlinCompilation(delegate)
-
     val project = buildProjectWithMPP()
     val kotlin = project.multiplatformExtension
 
-    private fun ExternalKotlinTargetDescriptorBuilder<FakeTarget>.defaults() {
-        targetName = "fake"
-        platformType = KotlinPlatformType.jvm
-        targetFactory = TargetFactory(::FakeTarget)
-    }
-
-    private fun ExternalKotlinCompilationDescriptorBuilder<FakeCompilation>.defaults() {
-        compilationName = "fake"
-        compilationFactory = CompilationFactory(::FakeCompilation)
-        defaultSourceSet = kotlin.sourceSets.maybeCreate("fake")
-    }
 
     @Test
     fun `test - sourceSetClassifier - default`() = buildProjectWithMPP().runLifecycleAwareTest {
         val target = kotlin.createExternalKotlinTarget<FakeTarget> { defaults() }
-        val compilation = target.createCompilation<FakeCompilation> { defaults() }
+        val compilation = target.createCompilation<FakeCompilation> { defaults(kotlin) }
 
         assertEquals(KotlinSourceSetTree("fake"), KotlinSourceSetTree.orNull(compilation))
     }
@@ -58,7 +43,7 @@ class ExternalKotlinTargetApiTests {
     fun `test - sourceSetClassifier - custom name`() = buildProjectWithMPP().runLifecycleAwareTest {
         val target = kotlin.createExternalKotlinTarget<FakeTarget> { defaults() }
         val compilation = target.createCompilation<FakeCompilation> {
-            defaults()
+            defaults(kotlin)
             sourceSetTreeClassifierV2 = KotlinSourceSetTreeClassifier.Name("mySourceSetTree")
         }
 
@@ -73,7 +58,7 @@ class ExternalKotlinTargetApiTests {
         val target = kotlin.createExternalKotlinTarget<FakeTarget> { defaults() }
 
         val mainCompilation = target.createCompilation<FakeCompilation> {
-            defaults()
+            defaults(kotlin)
             sourceSetTreeClassifierV2 = KotlinSourceSetTreeClassifier.Property(myProperty)
         }
 
