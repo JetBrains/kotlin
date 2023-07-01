JetBrains/kotlin

commit 60f09f6512ea0b1cc1411a731bb29279b97c4bf5

K2: Adjust renaming on import for current package with K1 rules
Namely, once a file in a package `foo` has import `foo.bar as baz`,
auto imported `bar` from the package name becomes inivisible in the file

^KT-54854 Fixed

compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/impl/FirPackageMemberScope.kt
@@ -23,17 +23,23 @@ import org.jetbrains.kotlin.utils.SmartList
 class FirPackageMemberScope(
     val fqName: FqName,
     val session: FirSession,
-    private val symbolProvider: FirSymbolProvider = session.symbolProvider
+    private val symbolProvider: FirSymbolProvider = session.symbolProvider,
+    excludedImportNames: Collection<FqName>? = null,
 ) : FirScope() {
     private val classifierCache: MutableMap<Name, FirClassifierSymbol<*>?> = mutableMapOf()
     private val functionCache: MutableMap<Name, List<FirNamedFunctionSymbol>> = mutableMapOf()
     private val propertyCache: MutableMap<Name, List<FirPropertySymbol>> = mutableMapOf()
+
+    private val excludedNames by lazy {
+        excludedImportNames?.mapNotNullTo(mutableSetOf()) { if (it.parent() == fqName) it.shortName() else null }.orEmpty()
+    }
 
     override fun processClassifiersByNameWithSubstitution(
         name: Name,
         processor: (FirClassifierSymbol<*>, ConeSubstitutor) -> Unit
     ) {
         if (name.asString().isEmpty()) return
+        if (name in excludedNames) return
 
         val symbol = classifierCache.getOrPut(name) {
             val unambiguousFqName = ClassId(fqName, name)
@@ -46,6 +52,8 @@ class FirPackageMemberScope(
     }
 
     override fun processFunctionsByName(name: Name, processor: (FirNamedFunctionSymbol) -> Unit) {
+        if (name in excludedNames) return
+
         val symbols = functionCache.getOrPut(name) {
             symbolProvider.getTopLevelFunctionSymbols(fqName, name)
         }
@@ -55,6 +63,8 @@ class FirPackageMemberScope(
     }
 
     override fun processPropertiesByName(name: Name, processor: (FirVariableSymbol<*>) -> Unit) {
+        if (name in excludedNames) return
+
         val symbols = propertyCache.getOrPut(name) {
             symbolProvider.getTopLevelPropertySymbols(fqName, name)
         }

compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/ImportingScopes.kt
@@ -61,7 +61,7 @@ private fun doCreateImportingScopes(
             FirDefaultSimpleImportingScope(session, scopeSession, priority = DefaultImportPriority.HIGH)
         },
         scopeSession.getOrBuild(file.packageFqName, PACKAGE_MEMBER) {
-            FirPackageMemberScope(file.packageFqName, session)
+            FirPackageMemberScope(file.packageFqName, session, excludedImportNames = excludedImportNames)
         },
         // TODO: explicit simple importing scope should have highest priority (higher than inner scopes added in process)
         FirExplicitSimpleImportingScope(file.imports, session, scopeSession)

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/DiagnosticCompilerTestFE10TestdataTestGenerated.java
@@ -14203,6 +14203,12 @@ public class DiagnosticCompilerTestFE10TestdataTestGenerated extends AbstractDia
             }
 
             @Test
+            @TestMetadata("ImportFromCurrentWithDifferentNameComplex.kt")
+            public void testImportFromCurrentWithDifferentNameComplex() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/imports/ImportFromCurrentWithDifferentNameComplex.kt");
+            }
+
+            @Test
             @TestMetadata("ImportFromObject.kt")
             public void testImportFromObject() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/imports/ImportFromObject.kt");

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
@@ -14203,6 +14203,12 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
             }
 
             @Test
+            @TestMetadata("ImportFromCurrentWithDifferentNameComplex.kt")
+            public void testImportFromCurrentWithDifferentNameComplex() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/imports/ImportFromCurrentWithDifferentNameComplex.kt");
+            }
+
+            @Test
             @TestMetadata("ImportFromObject.kt")
             public void testImportFromObject() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/imports/ImportFromObject.kt");

compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -14203,6 +14203,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
             }
 
             @Test
+            @TestMetadata("ImportFromCurrentWithDifferentNameComplex.kt")
+            public void testImportFromCurrentWithDifferentNameComplex() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/imports/ImportFromCurrentWithDifferentNameComplex.kt");
+            }
+
+            @Test
             @TestMetadata("ImportFromObject.kt")
             public void testImportFromObject() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/imports/ImportFromObject.kt");

compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
@@ -14209,6 +14209,12 @@ public class FirPsiOldFrontendDiagnosticsTestGenerated extends AbstractFirPsiDia
             }
 
             @Test
+            @TestMetadata("ImportFromCurrentWithDifferentNameComplex.kt")
+            public void testImportFromCurrentWithDifferentNameComplex() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/imports/ImportFromCurrentWithDifferentNameComplex.kt");
+            }
+
+            @Test
             @TestMetadata("ImportFromObject.kt")
             public void testImportFromObject() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/imports/ImportFromObject.kt");

compiler/testData/diagnostics/tests/imports/ImportFromCurrentWithDifferentName.fir.kt
@@ -1,8 +1,0 @@
-package a
-
-import a.A as ER
-
-interface A {
-    val a: A
-    val b: ER
-}

compiler/testData/diagnostics/tests/imports/ImportFromCurrentWithDifferentName.kt
@@ -1,3 +1,4 @@
+// FIR_IDENTICAL
 package a
 
 import a.A as ER

commit f37dc279740ec8d3fda71859e832d81a4b121504

K2: Fix incorrect UNRESOLVED_REFERENCE after aliased import in other file
The problem is actually caused by 60f09f6512ea0b1cc1411a731bb29279b97c4bf5
It happened because we've been caching package scope using FqName key,
so once one file contains aliased import it would work like other file
contains it, too.

So, the fix is adding excludedNames to the key, too.

^KT-59789 Fixed

compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/impl/FirPackageMemberScope.kt
@@ -24,15 +24,11 @@ class FirPackageMemberScope(
     val fqName: FqName,
     val session: FirSession,
     private val symbolProvider: FirSymbolProvider = session.symbolProvider,
-    excludedImportNames: Collection<FqName>? = null,
+    private val excludedNames: Set<Name> = emptySet(),
 ) : FirScope() {
     private val classifierCache: MutableMap<Name, FirClassifierSymbol<*>?> = mutableMapOf()
     private val functionCache: MutableMap<Name, List<FirNamedFunctionSymbol>> = mutableMapOf()
     private val propertyCache: MutableMap<Name, List<FirPropertySymbol>> = mutableMapOf()
-
-    private val excludedNames by lazy {
-        excludedImportNames?.mapNotNullTo(mutableSetOf()) { if (it.parent() == fqName) it.shortName() else null }.orEmpty()
-    }
 
     override fun processClassifiersByNameWithSubstitution(
         name: Name,

compiler/fir/resolve/src/org/jetbrains/kotlin/fir/scopes/ImportingScopes.kt
@@ -18,7 +18,6 @@ import org.jetbrains.kotlin.name.FqName
 private val ALL_IMPORTS = scopeSessionKey<FirFile, ListStorageFirScope>()
 private val DEFAULT_STAR_IMPORT = scopeSessionKey<DefaultStarImportKey, FirDefaultStarImportingScope>()
 private val DEFAULT_SIMPLE_IMPORT = scopeSessionKey<DefaultImportPriority, FirDefaultSimpleImportingScope>()
-
 private data class DefaultStarImportKey(val priority: DefaultImportPriority, val excludedImportNames: Set<FqName>)
 
 fun createImportingScopes(
@@ -42,6 +41,12 @@ private fun doCreateImportingScopes(
     file.lazyResolveToPhase(FirResolvePhase.IMPORTS)
     val excludedImportNames =
         file.imports.filter { it.aliasName != null }.mapNotNullTo(hashSetOf()) { it.importedFqName }.ifEmpty { emptySet() }
+
+    val excludedNamesInPackage =
+        excludedImportNames.mapNotNullTo(mutableSetOf()) {
+            if (it.parent() == file.packageFqName) it.shortName() else null
+        }
+
     return listOf(
         scopeSession.getOrBuild(DefaultStarImportKey(DefaultImportPriority.LOW, excludedImportNames), DEFAULT_STAR_IMPORT) {
             FirDefaultStarImportingScope(session, scopeSession, DefaultImportPriority.LOW, excludedImportNames)
@@ -60,8 +65,14 @@ private fun doCreateImportingScopes(
         scopeSession.getOrBuild(DefaultImportPriority.HIGH, DEFAULT_SIMPLE_IMPORT) {
             FirDefaultSimpleImportingScope(session, scopeSession, priority = DefaultImportPriority.HIGH)
         },
-        scopeSession.getOrBuild(file.packageFqName, PACKAGE_MEMBER) {
-            FirPackageMemberScope(file.packageFqName, session, excludedImportNames = excludedImportNames)
+        when {
+            excludedNamesInPackage.isEmpty() ->
+                // Supposed to be the most common branch, so we cache it
+                scopeSession.getOrBuild(file.packageFqName, PACKAGE_MEMBER) {
+                    FirPackageMemberScope(file.packageFqName, session, excludedNames = emptySet())
+                }
+            else ->
+                FirPackageMemberScope(file.packageFqName, session, excludedNames = excludedNamesInPackage)
         },
         // TODO: explicit simple importing scope should have highest priority (higher than inner scopes added in process)
         FirExplicitSimpleImportingScope(file.imports, session, scopeSession)

compiler/testData/diagnostics/tests/imports/renamedImportInDifferentFile.fir.kt
@@ -1,22 +1,0 @@
-// ISSUE: KT-59789
-// FILE: a/A.java
-package a;
-public interface A {}
-
-// FILE: first.kt
-package b
-import b.DependencyAnalyzerDependency as Dependency
-
-fun foo(d: Dependency) {}
-
-// FILE: main.kt
-package b
-
-import a.A
-interface DependencyAnalyzerDependency : A {
-    val parent: <!UNRESOLVED_REFERENCE!>DependencyAnalyzerDependency<!>? // Should be resolved
-}
-
-fun bar(d: <!UNRESOLVED_REFERENCE!>DependencyAnalyzerDependency<!>) {
-    foo(d)
-}

compiler/testData/diagnostics/tests/imports/renamedImportInDifferentFile.kt
@@ -1,3 +1,4 @@
+// FIR_IDENTICAL
 // ISSUE: KT-59789
 // FILE: a/A.java
 package a;