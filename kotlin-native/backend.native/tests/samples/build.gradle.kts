7cb0875bd84e441b480cccd56e88af48714cfd7d
Pavel Punegov <pavel.punegov@jetbrains.com>
2023-07-05 15:50:12 +0200

    [K/N][test] Add ExperimentalNativeApi opt-in in the samples

--- kotlin-native/backend.native/tests/samples/build.gradle.kts
+++ kotlin-native/backend.native/tests/samples/build.gradle.kts
@@ -23,7 +23,8 @@ allprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile>().configureEach {
         compilerOptions.freeCompilerArgs.addAll(
                 "-XXLanguage:+ImplicitSignedToUnsignedIntegerConversion",
-                "-opt-in=kotlinx.cinterop.ExperimentalForeignApi"
+                "-opt-in=kotlinx.cinterop.ExperimentalForeignApi",
+                "-opt-in=kotlin.experimental.ExperimentalNativeApi"
         )
     }
 }
