f3793cedbb3c303668c02e1b41383f3d3ea9bf5f
Sergei Kharitontcev-Beglov <khbminus@gmail.com>
2023-07-09 16:55:31 +0200

    [Wasm] Fixed synthetic marking

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/dce/utils.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/dce/utils.kt
@@ -6,47 +6,37 @@
 package org.jetbrains.kotlin.ir.backend.js.dce
 
 import org.jetbrains.kotlin.ir.IrElement
-import org.jetbrains.kotlin.ir.backend.js.lower.PrimaryConstructorLowering
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.util.dumpKotlinLike
-import org.jetbrains.kotlin.ir.util.file
-import org.jetbrains.kotlin.ir.util.fileEntry
 import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.name.SpecialNames
 import java.io.File
 
-private fun IrDeclaration.hasAnonymousParent(): Boolean =
-    parent.let {
-        require(parent != this) { println(this) }
-        when {
-            it is IrFile -> false
-            it !is IrDeclaration -> false
-            it is IrDeclarationWithName && (it.name == SpecialNames.NO_NAME_PROVIDED || it.name == SpecialNames.ANONYMOUS) -> true
-            else -> it.hasAnonymousParent()
-        }
-    }
 
 internal fun IrDeclaration.fqNameForDceDump(): String {
     // TODO: sanitize names
     val fqn = (this as? IrDeclarationWithName)?.fqNameWhenAvailable?.asString() ?: "<unknown>"
     val signature = when (this is IrFunction) {
-        true -> this.valueParameters.joinToString(prefix = "(", postfix = ")") { it.type.dumpKotlinLike() } +
-                (this.extensionReceiverParameter?.type?.dumpKotlinLike()?.let { " (For: $it)" } ?: "")
+        true -> "${
+            this.valueParameters.joinToString(
+                prefix = "(",
+                postfix = ")"
+            ) { it.type.dumpKotlinLike() }
+        }: ${this.returnType.dumpKotlinLike()}"
         else -> ""
     }
-    val synthetic = when (this.origin == PrimaryConstructorLowering.SYNTHETIC_PRIMARY_CONSTRUCTOR) {
+    val synthetic = when (this.origin.isSynthetic) {
         true -> "[synthetic]"
         else -> ""
     }
 
-    val location = when (this.hasAnonymousParent()) {
-        true -> " (From ${this.file.path}:${fileEntry.getLineNumber(this.startOffset)}:${fileEntry.getColumnNumber(this.startOffset)})"
+    val receiver = when (this is IrFunction && this.extensionReceiverParameter != null) {
+        true -> "(For: ${this.extensionReceiverParameter!!.type.dumpKotlinLike()})"
         else -> ""
     }
-
-    return (fqn + signature + synthetic + location)
+    return (fqn + signature + synthetic + receiver)
 }
 
 private data class IrDeclarationDumpInfo(val fqName: String, val type: String, val size: Int)
