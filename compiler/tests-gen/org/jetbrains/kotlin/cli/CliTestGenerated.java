41d7cb576250ae63b6e80be66a08151e9b38c3c8
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-07-10 18:19:53 +0200

    ++ more CLI test data changes

--- compiler/tests-gen/org/jetbrains/kotlin/cli/CliTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/cli/CliTestGenerated.java
@@ -645,11 +645,6 @@ public class CliTestGenerated extends AbstractCliTest {
             runTest("compiler/testData/cli/jvm/internalArgEmptyFeatureName.args");
         }
 
-        @TestMetadata("internalArgEnableLanguageFeature.args")
-        public void testInternalArgEnableLanguageFeature() throws Exception {
-            runTest("compiler/testData/cli/jvm/internalArgEnableLanguageFeature.args");
-        }
-
         @TestMetadata("internalArgMissingModificator.args")
         public void testInternalArgMissingModificator() throws Exception {
             runTest("compiler/testData/cli/jvm/internalArgMissingModificator.args");
@@ -1075,21 +1070,6 @@ public class CliTestGenerated extends AbstractCliTest {
             runTest("compiler/testData/cli/jvm/resultInReturnTypeSupportedByDefault15.args");
         }
 
-        @TestMetadata("resultInReturnTypeUnsupportedByDefault14.args")
-        public void testResultInReturnTypeUnsupportedByDefault14() throws Exception {
-            runTest("compiler/testData/cli/jvm/resultInReturnTypeUnsupportedByDefault14.args");
-        }
-
-        @TestMetadata("resultInReturnTypeWithEnabledInlineClasses.args")
-        public void testResultInReturnTypeWithEnabledInlineClasses() throws Exception {
-            runTest("compiler/testData/cli/jvm/resultInReturnTypeWithEnabledInlineClasses.args");
-        }
-
-        @TestMetadata("resultInReturnTypeWithEnabledInlineClassesXX.args")
-        public void testResultInReturnTypeWithEnabledInlineClassesXX() throws Exception {
-            runTest("compiler/testData/cli/jvm/resultInReturnTypeWithEnabledInlineClassesXX.args");
-        }
-
         @TestMetadata("returnAsWhenKey.args")
         public void testReturnAsWhenKey() throws Exception {
             runTest("compiler/testData/cli/jvm/returnAsWhenKey.args");
@@ -1193,11 +1173,6 @@ public class CliTestGenerated extends AbstractCliTest {
         @TestMetadata("useMixedNamedArgumentsFlag.args")
         public void testUseMixedNamedArgumentsFlag() throws Exception {
             runTest("compiler/testData/cli/jvm/useMixedNamedArgumentsFlag.args");
-        }
-
-        @TestMetadata("variableInWhenSubject.args")
-        public void testVariableInWhenSubject() throws Exception {
-            runTest("compiler/testData/cli/jvm/variableInWhenSubject.args");
         }
 
         @TestMetadata("warningJdkWithNoJdk.args")
