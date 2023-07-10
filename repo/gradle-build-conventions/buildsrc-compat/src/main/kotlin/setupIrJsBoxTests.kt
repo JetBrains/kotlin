a1289f0daf5f77baa11eca3b631e2970956b91b6
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2023-07-10 22:34:34 +0200

    fixup! js.tests: change paths to kotlin-stdlib-js.klib

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/setupIrJsBoxTests.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/setupIrJsBoxTests.kt
@@ -13,6 +13,7 @@ fun Test.useJsIrBoxTests(
 ) {
     setupV8()
     dependsOn(":kotlin-stdlib:jsJar")
+    dependsOn(":kotlin-stdlib:jsJarForTests") // TODO: think how to remove dependency on the artifact in this place
     dependsOn(":kotlin-test:kotlin-test-js-ir:compileKotlinJs")
     dependsOn(":kotlin-stdlib-js-ir-minimal-for-test:compileKotlinJs")
 
