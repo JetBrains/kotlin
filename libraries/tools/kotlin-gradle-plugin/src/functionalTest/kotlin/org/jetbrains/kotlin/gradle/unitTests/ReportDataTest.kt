f28099f3e9daca6d5e8d6612e5948ca387ba023f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-07-18 14:24:09 +0200

    ~ Migrate some Gradle tests to Kotlin 2.0

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/ReportDataTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/ReportDataTest.kt
@@ -45,7 +45,7 @@ class ReportDataTest {
         assertNotNull(statisticData)
         assertTrue(statisticData.getTags().contains(StatTag.KOTLIN_DEBUG))
         assertTrue(statisticData.getTags().contains(StatTag.NON_INCREMENTAL))
-        assertTrue(statisticData.getTags().contains(StatTag.KOTLIN_1))
+        assertTrue(statisticData.getTags().contains(StatTag.KOTLIN_2))
     }
 
     private fun taskRecord(buildMetrics: BuildMetrics<GradleBuildTime, GradleBuildPerformanceMetric>) = TaskRecord(
