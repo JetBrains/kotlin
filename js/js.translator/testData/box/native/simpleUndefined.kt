6be70a045055b9fb83ada877e17415785b24936a
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2023-07-20 17:30:08 +0200

    [KT-59294] Fix for the WRONG_JS_INTEROP_TYPE error in K1WasmJsTranslatorTestGenerated::simpleUndefined

--- js/js.translator/testData/box/native/simpleUndefined.kt
+++ js/js.translator/testData/box/native/simpleUndefined.kt
@@ -1,7 +1,8 @@
 // EXPECTED_REACHABLE_NODES: 1281
 package foo
 
-external val c: Any? = definedExternally
+external interface MyAny
+external val c: MyAny? = definedExternally
 
 fun box(): String {
     if (c != null) return "fail1"
