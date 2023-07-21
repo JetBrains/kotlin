30342fcd8c25e24fc0acf7ef357e89ba1d2e5c8d
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-07-21 13:39:27 +0200

    [KDM] Add tests for parsing data objects
    See https://github.com/Kotlin/dokka/pull/3076
    Original commit: https://github.com/Kotlin/dokka/commit/3f50846197ffce7e5dcc1c80e360ce6d499d67a6

---
+++ libraries/tools/kotlin-documentation-model/analyzer/plugins/base/src/test/kotlin/model/ObjectTest.kt
@@ -0,0 +1,39 @@
+package model
+
+import org.jetbrains.dokka.model.AdditionalModifiers
+import org.jetbrains.dokka.model.DObject
+import org.jetbrains.dokka.model.ExtraModifiers
+import org.junit.jupiter.api.Test
+import utils.AbstractModelTest
+
+class ObjectTest : AbstractModelTest("/src/main/kotlin/objects/Test.kt", "objects") {
+
+    @Test
+    fun emptyObject() {
+        inlineModelTest(
+            """
+            |object Obj {}
+            """.trimIndent()
+        ) {
+            with((this / "objects" / "Obj").cast<DObject>()) {
+                name equals "Obj"
+                children counts 3
+            }
+        }
+    }
+
+    @Test
+    fun `data object class`() {
+        inlineModelTest(
+            """
+            |data object KotlinDataObject {}
+            """.trimIndent()
+        ) {
+            with((this / "objects" / "KotlinDataObject").cast<DObject>()) {
+                name equals "KotlinDataObject"
+                extra[AdditionalModifiers]?.content?.values?.single()
+                    ?.single() equals ExtraModifiers.KotlinOnlyModifiers.Data
+            }
+        }
+    }
+}
