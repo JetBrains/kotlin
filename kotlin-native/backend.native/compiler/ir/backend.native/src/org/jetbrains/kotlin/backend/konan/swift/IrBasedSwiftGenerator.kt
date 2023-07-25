2a378c1cad7babb66d2dbf8d7f4b1cc76359ce3d
Gleb Lukianets <gleb.lukianets@jetbrains.com>
2023-07-25 08:06:10 +0000

    [SwiftInterop]: KT-60151: Map trivial types to swift
    Map binary compatible types between kotlin and swift as-is.
    Tests will follow when the infrastructure catches up.
    
    
    Merge-request: KT-MR-11174
    Merged-by: Gleb Lukianets <Gleb.Lukianets@jetbrains.com>

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/swift/IrBasedSwiftGenerator.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/swift/IrBasedSwiftGenerator.kt
@@ -5,15 +5,15 @@
 
 package org.jetbrains.kotlin.backend.konan.swift
 
-import io.outfoxx.swiftpoet.CodeBlock
-import io.outfoxx.swiftpoet.FileSpec
-import io.outfoxx.swiftpoet.FunctionSpec
-import io.outfoxx.swiftpoet.Modifier
+import io.outfoxx.swiftpoet.*
 import org.jetbrains.kotlin.backend.konan.llvm.KonanBinaryInterface
+import org.jetbrains.kotlin.builtins.PrimitiveType
+import org.jetbrains.kotlin.builtins.UnsignedType
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrFunction
-import org.jetbrains.kotlin.ir.types.isUnit
+import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.util.allParameters
+import org.jetbrains.kotlin.ir.util.explicitParameters
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 
@@ -28,8 +28,9 @@ import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 class IrBasedSwiftGenerator(private val moduleName: String) : IrElementVisitorVoid {
 
     private val initRuntimeIfNeededSpec = FunctionSpec.abstractBuilder("initRuntimeIfNeeded")
+            // FIXME: _silgen_name only work for as long as swiftcc matches ccc. Switch to c brindging instead.
             .addAttribute("_silgen_name", "\"Kotlin_initRuntimeIfNeeded\"")
-            .addModifiers(Modifier.FILEPRIVATE)
+            .addModifiers(Modifier.PRIVATE)
             .build()
 
     val functions = mutableListOf<FunctionSpec>(initRuntimeIfNeededSpec)
@@ -49,28 +50,78 @@ class IrBasedSwiftGenerator(private val moduleName: String) : IrElementVisitorVo
         if (!isSupported(declaration)) {
             return
         }
+
         val name = declaration.name.identifier
         val symbolName = with(KonanBinaryInterface) { declaration.symbolName }
-        val silgenFunctionSpec = FunctionSpec.abstractBuilder("${name}_bridge")
+
+        val returnTypeSpec = mapType(declaration.returnType) ?: return
+        val parametersSpec = declaration.explicitParameters.map {
+            ParameterSpec.builder(it.name.asString(), mapType(it.type) ?: return).build()
+        }
+
+        val forwardDeclarationSpec = FunctionSpec.abstractBuilder("${name}_bridge")
+                // FIXME: _silgen_name only work for as long as swiftcc matches ccc. Switch to c brindging instead.
                 .addAttribute("_silgen_name", "\"${symbolName}\"")
-                .addModifiers(Modifier.FILEPRIVATE)
+                .addModifiers(Modifier.PRIVATE)
+                .returns(returnTypeSpec)
+                .apply {
+                    for (parameter in parametersSpec) {
+                        addParameter(parameter)
+                    }
+                }
                 .build()
-        functions += silgenFunctionSpec
-        val swiftFunctionSpec = FunctionSpec.builder(name)
-                .addCode(CodeBlock.builder().addStatement("${initRuntimeIfNeededSpec.name}()").build())
-                .addCode(CodeBlock.builder().addStatement("${silgenFunctionSpec.name}()").build())
+
+        val shimFunctionSpec = FunctionSpec.builder(name)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(returnTypeSpec)
+                .apply {
+                    for (parameter in parametersSpec) {
+                        addParameter(parameter)
+                    }
+                }
+                .addStatement("${initRuntimeIfNeededSpec.name}()")
+                .addStatement("return ${forwardDeclarationSpec.name}(${parametersSpec.map { "${it.parameterName}: ${it.parameterName}" }.joinToString()})")
                 .build()
-        functions += swiftFunctionSpec
+
+        functions += forwardDeclarationSpec
+        functions += shimFunctionSpec
     }
 
     private fun isSupported(declaration: IrFunction): Boolean {
-        // For now, we support only () -> Unit functions.
-        if (!declaration.returnType.isUnit()) {
-            return false
+        // No Kotlin-exclusive stuff
+        return declaration.visibility.isPublicAPI
+                && declaration.extensionReceiverParameter == null
+                && declaration.dispatchReceiverParameter == null
+                && declaration.contextReceiverParametersCount == 0
+                && !declaration.isExpect
+                && !declaration.isInline
+    }
+
+    private fun mapType(declaration: IrType): TypeName? {
+        val swiftPrimitiveTypeName: String? = declaration.getPrimitiveType().takeUnless { declaration.isNullable() }?.let {
+            when (it) {
+                PrimitiveType.BYTE -> "Int8"
+                PrimitiveType.BOOLEAN -> "Bool"
+                PrimitiveType.CHAR -> null
+                PrimitiveType.SHORT -> "Int16"
+                PrimitiveType.INT -> "Int32"
+                PrimitiveType.LONG -> "Int64"
+                PrimitiveType.FLOAT -> "Float"
+                PrimitiveType.DOUBLE -> "Double"
+            }
+        } ?: declaration.getUnsignedType().takeUnless { declaration.isNullable() }?.let {
+            when (it) {
+                UnsignedType.UBYTE -> "UInt8"
+                UnsignedType.USHORT -> "UInt16"
+                UnsignedType.UINT -> "UInt32"
+                UnsignedType.ULONG -> "UInt64"
+            }
+        } ?: if (declaration.isUnit()) "Void" else null
+
+        if (swiftPrimitiveTypeName == null) {
+            println("Failed to bridge ${declaration.classFqName}")
         }
-        if (declaration.allParameters.isNotEmpty()) {
-            return false
-        }
-        return true
+
+        return swiftPrimitiveTypeName?.let { DeclaredTypeName.typeName("Swift.$it") }
     }
 }
