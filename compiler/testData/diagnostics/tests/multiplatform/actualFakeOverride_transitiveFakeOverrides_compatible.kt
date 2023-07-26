2d1a7fd0f6163bf8896af0672d44637c76c3914e
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-07-26 14:17:22 +0200

    fixup! [FE] Convert specific diagnostic for actual function with default arguments into a common incompatibility

--- compiler/testData/diagnostics/tests/multiplatform/actualFakeOverride_transitiveFakeOverrides_compatible.kt
+++ compiler/testData/diagnostics/tests/multiplatform/actualFakeOverride_transitiveFakeOverrides_compatible.kt
@@ -1,34 +1,18 @@
 // FIR_IDENTICAL
 // MODULE: m1-common
 // FILE: common.kt
-interface Shared {
-    fun foo(param: Int = 2) {}
+expect class Foo {
+    fun foo(param: Int)
 }
-
-expect class Foo : Shared
 
 // MODULE: m2-jvm()()(m1-common)
 // FILE: jvm.kt
-actual class Foo : Shared, A {
-    override fun foo(param: Int) {}
-}
+actual class Foo : A
 
-interface A : B, C {
+interface A : B {
     override fun foo(param: Int) {}
 }
 
 interface B {
-    fun foo(param: Int) {}
-}
-
-interface C : D, E {
-    override fun foo(param: Int) {}
-}
-
-interface D {
-    fun foo(param: Int) {}
-}
-
-interface E {
     fun foo(param: Int) {}
 }
