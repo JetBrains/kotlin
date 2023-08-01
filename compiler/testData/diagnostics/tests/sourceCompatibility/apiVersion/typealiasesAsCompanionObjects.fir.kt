c392b0c477574a0e8351d74703899dc7c019af8e
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-08-01 12:46:28 +0300

    [FIR] Expand typealiases when resolving to companions
    ^KT-59901 Fixed

--- compiler/testData/diagnostics/tests/sourceCompatibility/apiVersion/typealiasesAsCompanionObjects.fir.kt
+++ compiler/testData/diagnostics/tests/sourceCompatibility/apiVersion/typealiasesAsCompanionObjects.fir.kt
@@ -11,5 +11,5 @@ typealias CA = C
 
 val test1 = CA
 val test2 = CA.<!UNRESOLVED_REFERENCE!>Companion<!>
-val test3 = CA.x
+val test3 = <!API_NOT_AVAILABLE!>CA<!>.x
 val test4 = CA.<!UNRESOLVED_REFERENCE!>Companion<!>.x
