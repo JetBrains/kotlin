94deedde771e6674432d17a272d37d5e7c75686f
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-08-02 20:09:33 +0900

    [LL API] Add more flexible 'preferBodyPredicate'
    The previous API required to pass the exact declaration element to
    the 'ContextCollector'. Now it's possible to get a 'BODY' context for
    the target ancestor.

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirBodyLazyResolver.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirBodyLazyResolver.kt
@@ -172,8 +172,9 @@ private class LLFirBodyTargetResolver(
 
         firCodeFragment.codeFragmentContext = if (contextKtFile != null) {
             val contextFirFile = resolveSession.getOrBuildFirFile(contextKtFile)
+            val sessionHolder = transformer.components
 
-            val elementContext = ContextCollector.process(contextFirFile, transformer.components, contextPsiElement, preferBody = true)
+            val elementContext = ContextCollector.process(contextFirFile, sessionHolder, contextPsiElement) { it === contextPsiElement }
                 ?: errorWithAttachment("Cannot find enclosing context for ${contextPsiElement::class}") {
                     withPsiEntry("contextPsiElement", contextPsiElement)
                 }
