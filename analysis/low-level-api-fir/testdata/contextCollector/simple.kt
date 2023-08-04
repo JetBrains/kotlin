6c26aeeb97601d6248992eff862d9be36d5d48b2
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-08-04 22:13:31 +0900

    [LL API] Check declaration resolution status in 'ContextCollector' tests

--- analysis/low-level-api-fir/testdata/contextCollector/simple.kt
+++ analysis/low-level-api-fir/testdata/contextCollector/simple.kt
@@ -8,3 +8,12 @@ class Foo {
         <expr>print(x)</expr>
     }
 }
+
+class Unrelated {
+    val unrelatedMember: Boolean
+        get() = true
+}
+
+fun unrelatedFunction(): Int {
+    return "foo".length
+}
