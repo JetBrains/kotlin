3aa635a766d1cb2671ebe1c7f36ac986447db185
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-08-07 18:14:08 +0200

    K2: Fix compilation after making Candidate::symbol a var
    See the previous commits
    
    Explicit cast became necessary as the smart cast stopped working there

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/CallableReferenceResolution.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/CallableReferenceResolution.kt
@@ -48,7 +48,7 @@ internal object CheckCallableReferenceExpectedType : CheckerStage() {
             else -> null
         }
 
-        val fir: FirCallableDeclaration = candidate.symbol.fir
+        val fir: FirCallableDeclaration = candidate.symbol.fir as FirCallableDeclaration
 
         val (rawResultingType, callableReferenceAdaptation) = buildReflectionType(fir, resultingReceiverType, candidate, context)
         val resultingType = candidate.substitutor.substituteOrSelf(rawResultingType)
