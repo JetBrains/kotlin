6b8463a873455400f121cf541ef472c0b55718d8
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-08-08 14:02:21 +0200

    Add kdoc for ConstraintSystemCompletionMode.PARTIAL

--- compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/inference/components/ConstraintSystemCompletionMode.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/inference/components/ConstraintSystemCompletionMode.kt
@@ -7,6 +7,17 @@ package org.jetbrains.kotlin.resolve.calls.inference.components
 
 enum class ConstraintSystemCompletionMode {
     FULL,
+
+    /**
+     * This mode allows us to infer variables in calls, which have enough type-info to be completed right-away
+     * It can also trigger analysis of some postponed arguments
+     * We can't treat it as a plain optimization, because it affects the overload resolution in some cases
+     * e.g:
+     * ```kotlin
+     * val x: Int = 1
+     * x.plus(run { x }) // Here, to select plus overload we need to analyze lambda
+     * ```
+     */
     PARTIAL,
     UNTIL_FIRST_LAMBDA
 }
