dc4091b5f69f7438240bf69ad8641eb6a4680be3
Anton Lakotka <anton.lakotka@jetbrains.com>
2023-08-08 02:31:53 +0200

    [Gradle] Draft implementation of Configurations for Commonized CInterops
    Introduce *CommonizedCInterop & *CommonizedCInteropElements configurations
    and fix CInteropCommonizerTask to consume dependencies / expose artifacts
    via these configurations.
    
    (cherry picked from commit b37ebb47d9a7121e1bbc5ec33c7b7a46d8851471)

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/internal/CInteropConfigurations.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/native/internal/CInteropConfigurations.kt
@@ -9,17 +9,21 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.attributes.*
 import org.gradle.api.tasks.TaskProvider
+import org.jetbrains.kotlin.commonizer.CommonizerOutputFileLayout
+import org.jetbrains.kotlin.commonizer.SharedCommonizerTarget
+import org.jetbrains.kotlin.gradle.plugin.*
 import org.jetbrains.kotlin.gradle.plugin.KotlinNativeTargetConfigurator.NativeArtifactFormat
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginLifecycle.Stage.AfterFinaliseDsl
-import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 import org.jetbrains.kotlin.gradle.plugin.categoryByName
 import org.jetbrains.kotlin.gradle.plugin.internal.artifactTypeAttribute
 import org.jetbrains.kotlin.gradle.plugin.launchInStage
 import org.jetbrains.kotlin.gradle.plugin.mpp.*
 import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.copyAttributes
-import org.jetbrains.kotlin.gradle.plugin.usesPlatformOf
+import org.jetbrains.kotlin.gradle.plugin.sources.internal
 import org.jetbrains.kotlin.gradle.targets.native.internal.CInteropKlibLibraryElements.cinteropKlibLibraryElements
 import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
+import org.jetbrains.kotlin.gradle.utils.markConsumable
+import org.jetbrains.kotlin.gradle.utils.markResolvable
 
 internal fun createCInteropApiElementsKlibArtifact(
     target: KotlinNativeTarget,
@@ -37,6 +41,23 @@ internal fun createCInteropApiElementsKlibArtifact(
     }
 }
 
+internal suspend fun Project.createCommonizedCInteropApiElementsKlibArtifact(
+    interopTask: CInteropCommonizerTask
+) {
+    for (commonizerGroup in interopTask.allInteropGroups.await()) {
+        for (sharedCommonizerTargets in commonizerGroup.targets) {
+            val configuration = locateOrCreateCommonizedCInteropApiElementsConfiguration(sharedCommonizerTargets)
+            val artifactPath = CommonizerOutputFileLayout.resolveCommonizedDirectory(interopTask.outputDirectory(commonizerGroup), sharedCommonizerTargets)
+            project.artifacts.add(configuration.name, artifactPath) { artifact ->
+                artifact.extension = "klib"
+                artifact.type = "klib"
+                artifact.classifier = "cinterop-" + sharedCommonizerTargets.dashedIdentityString()
+                artifact.builtBy(interopTask)
+            }
+        }
+    }
+}
+
 internal fun Project.locateOrCreateCInteropDependencyConfiguration(
     compilation: KotlinNativeCompilation,
 ): Configuration {
@@ -48,8 +69,7 @@ internal fun Project.locateOrCreateCInteropDependencyConfiguration(
     return configurations.create(compilation.cInteropDependencyConfigurationName).apply {
         extendsFrom(compileOnlyConfiguration, implementationConfiguration)
         isVisible = false
-        isCanBeResolved = true
-        isCanBeConsumed = false
+        markResolvable()
 
         /* Deferring attributes to wait for compilation.attributes to be configured  by user*/
         launchInStage(AfterFinaliseDsl) {
@@ -63,8 +83,45 @@ internal fun Project.locateOrCreateCInteropDependencyConfiguration(
     }
 }
 
+private fun Configuration.applyAttributesForCommonizerTarget(commonizerTarget: SharedCommonizerTarget) {
+    attributes.attribute(KotlinPlatformType.attribute, KotlinPlatformType.native)
+    // TODO: [KotlinNativeTarget.konanTargetAttribute] is a public attribute. It should be better to introduce dedicated private attribute
+    // TODO: for commonized target
+    attributes.attribute(KotlinNativeTarget.konanTargetAttribute, commonizerTarget.dashedIdentityString())
+}
+
+internal suspend fun Project.locateOrCreateCommonizedCInteropDependencyConfiguration(
+    sourceSet: KotlinSourceSet,
+): Configuration? {
+    val commonizerTarget = sourceSet.commonizerTarget.await() ?: return null
+    if (commonizerTarget !is SharedCommonizerTarget) return null
+
+    configurations.findByName(commonizerTarget.commonizedCInteropDependencyConfigurationName)?.let { return it }
+
+    val configuration = configurations.create(commonizerTarget.commonizedCInteropDependencyConfigurationName).apply {
+        isVisible = false
+        markResolvable()
+
+        /* Deferring attributes to wait for compilation.attributes to be configured  by user*/
+        launchInStage(AfterFinaliseDsl) {
+            // Extends from Metadata Configuration associated with given source set to ensure matching
+            extendsFrom(sourceSet.internal.resolvableMetadataConfiguration)
+            applyAttributesForCommonizerTarget(commonizerTarget)
+            attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, cinteropKlibLibraryElements())
+            attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, KotlinUsages.KOTLIN_CINTEROP))
+            attributes.attribute(Category.CATEGORY_ATTRIBUTE, project.categoryByName(Category.LIBRARY))
+            description = "Commonized CInterop dependencies for targets: '$commonizerTarget'."
+        }
+    }
+
+    return configuration
+}
+
 internal val KotlinNativeCompilation.cInteropDependencyConfigurationName: String
     get() = compilation.disambiguateName("CInterop")
+
+internal val SharedCommonizerTarget.commonizedCInteropDependencyConfigurationName: String
+    get() = dashedIdentityString() + "CInterop"
 
 internal fun Project.locateOrCreateCInteropApiElementsConfiguration(target: KotlinTarget): Configuration {
     val configurationName = cInteropApiElementsConfigurationName(target)
@@ -86,8 +143,35 @@ internal fun Project.locateOrCreateCInteropApiElementsConfiguration(target: Kotl
     }
 }
 
+internal fun Project.locateOrCreateCommonizedCInteropApiElementsConfiguration(commonizerTarget: SharedCommonizerTarget): Configuration {
+    val configurationName = commonizedCInteropApiElementsConfigurationName(commonizerTarget)
+    configurations.findByName(configurationName)?.let { return it }
+
+    return configurations.create(configurationName).apply {
+        markConsumable()
+
+        /* Deferring attributes to wait for target.attributes to be configured by user */
+        launchInStage(AfterFinaliseDsl) {
+            applyAttributesForCommonizerTarget(commonizerTarget)
+
+            attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, cinteropKlibLibraryElements())
+            attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, KotlinUsages.KOTLIN_CINTEROP))
+            attributes.attribute(Category.CATEGORY_ATTRIBUTE, project.categoryByName(Category.LIBRARY))
+            attributes.attribute(artifactTypeAttribute, NativeArtifactFormat.KLIB)
+        }
+    }
+}
+
 private fun cInteropApiElementsConfigurationName(target: KotlinTarget): String {
     return target.name + "CInteropApiElements"
+}
+
+/**
+ * Same as [org.jetbrains.kotlin.commonizer.identityString] but target segments separated with dash
+ */
+private fun SharedCommonizerTarget.dashedIdentityString() = targets.map { it.name }.sorted().joinToString("-")
+private fun commonizedCInteropApiElementsConfigurationName(commonizerTarget: SharedCommonizerTarget): String {
+    return commonizerTarget.dashedIdentityString() + "CInteropApiElements"
 }
 
 internal object CInteropKlibLibraryElements {
