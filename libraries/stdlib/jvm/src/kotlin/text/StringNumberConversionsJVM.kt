ff693f4b62083484f0b4497d310e30b7aacf2be9
Mimimishkin <68619081+Mimimishkin@users.noreply.github.com>
2023-08-09 01:02:57 +0300

    Add commas to number check
    The current implementation does not provide that in some languages ​​floating-point numbers are written not with a dot, but with a comma

--- libraries/stdlib/jvm/src/kotlin/text/StringNumberConversionsJVM.kt
+++ libraries/stdlib/jvm/src/kotlin/text/StringNumberConversionsJVM.kt
@@ -248,11 +248,11 @@ private object ScreenFloatValueRegEx {
         val HexDigits = "(\\p{XDigit}+)"
         val Exp = "[eE][+-]?$Digits"
 
-        val HexString = "(0[xX]$HexDigits(\\.)?)|" + // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
-                "(0[xX]$HexDigits?(\\.)$HexDigits)"  // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
+        val HexString = "(0[xX]$HexDigits(\\.|,)?)|" + // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
+                "(0[xX]$HexDigits?(\\.|,)$HexDigits)"  // 0[xX] HexDigits_opt . HexDigits BinaryExponent FloatTypeSuffix_opt
 
-        val Number = "($Digits(\\.)?($Digits?)($Exp)?)|" +  // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
-                "(\\.($Digits)($Exp)?)|" +                  // . Digits ExponentPart_opt FloatTypeSuffix_opt
+        val Number = "($Digits(\\.|,)?($Digits?)($Exp)?)|" +  // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
+                "((\\.|,)($Digits)($Exp)?)|" +                  // . Digits ExponentPart_opt FloatTypeSuffix_opt
                 "(($HexString)[pP][+-]?$Digits)"            // HexSignificand BinaryExponent
 
         val fpRegex = "[\\x00-\\x20]*[+-]?(NaN|Infinity|(($Number)[fFdD]?))[\\x00-\\x20]*"
