1b5209d85a8f89975d63e109b562d1e3806dc138
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-08-09 11:37:05 +0200

    [stdlib-mpp] Publish patched/custom kpsm to list 'commonMain' included into 'nativeApiElements'
    Native/Shared Source Sets still requires stdlib:commonMain.klib as
    dependency for IDE analysis.
    
    See: KT-56278
    ^KT-61034 Verification Pending

--- libraries/stdlib/build.gradle.kts
+++ libraries/stdlib/build.gradle.kts
@@ -477,6 +477,36 @@ dependencies {
     }
 }
 
+/*
+We are using a custom 'kotlin-project-structure-metadata' to ensure 'nativeApiElements' lists 'commonMain' as source set
+ */
+tasks.named("generateProjectStructureMetadata") {
+    val outputFile = file("build/kotlinProjectStructureMetadata/kotlin-project-structure-metadata.json")
+    val outputTestFile = file("kotlin-project-structure-metadata.json.beforePatch")
+    val patchedFile = file("kotlin-project-structure-metadata.json")
+
+    inputs.file(patchedFile)
+    inputs.file(outputTestFile)
+
+    doLast {
+        /*
+        Check that the generated 'outputFile' by default matches our expectations stored in the .beforePatch file
+        This will fail if the kotlin-project-structure-metadata.json file would change unnoticed (w/o updating our patched file)
+         */
+        run {
+            val outputFileText = outputFile.readText().trim()
+            val expectedFileContent = outputTestFile.readText().trim()
+            if (outputFileText != expectedFileContent)
+                error(
+                    "${outputFile.path} file content does not match expected content\n\n" +
+                            "expected:\n\n$expectedFileContent\n\nactual:\n\n$outputFileText"
+                )
+        }
+
+        patchedFile.copyTo(outputFile, overwrite = true)
+    }
+}
+
 tasks {
     val metadataJar by existing(Jar::class) {
         archiveAppendix.set("metadata")
