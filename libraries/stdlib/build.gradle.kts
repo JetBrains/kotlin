7b1e08a47ea4b928d3314c1ddad8fe5c29af12ef
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-08-09 20:44:22 +0200

    fixup! [stdlib-mpp] Add version constraint from kotlin-stdlib to kotlin-stdlib-jdk*

--- libraries/stdlib/build.gradle.kts
+++ libraries/stdlib/build.gradle.kts
@@ -461,8 +461,7 @@ kotlin {
 
 dependencies {
     val jvmMainImplementation by configurations.getting
-    val jvmApiElements by configurations.getting
-    val jvmRuntimeElements by configurations.getting
+    val jvmMainApi by configurations.getting
     val commonMainMetadataElementsWithClassifier by configurations.creating
     val metadataApiElements by configurations.getting
     val nativeApiElements by configurations.creating
@@ -474,11 +473,8 @@ dependencies {
         metadataApiElements(project(":kotlin-stdlib-common"))
         nativeApiElements(project(":kotlin-stdlib-common"))
 
-        jvmApiElements(project(":kotlin-stdlib-jdk7"))
-        jvmApiElements(project(":kotlin-stdlib-jdk8"))
-
-        jvmRuntimeElements(project(":kotlin-stdlib-jdk7"))
-        jvmRuntimeElements(project(":kotlin-stdlib-jdk8"))
+        jvmMainApi(project(":kotlin-stdlib-jdk7"))
+        jvmMainApi(project(":kotlin-stdlib-jdk8"))
 
         // to avoid split package and duplicate classes on classpath after moving them from these artifacts in 1.8.0
         jvmMainImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0")
