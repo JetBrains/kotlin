178848ac989dc21ba4195811a01d0a17c76f5c3e
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-08-11 19:23:40 +0200

    Revert "[FIR] Union CFG edges from postponed lambdas only if outer call was analyzed independently"
    This reverts commit fc57f48c8fc5845d2495d71da43a6651ebe001c7.

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
@@ -159,7 +159,7 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
                     if (!transformedCallee.isAcceptableResolvedQualifiedAccess()) {
                         return qualifiedAccessExpression
                     }
-                    callCompleter.completeCall(transformedCallee, data)
+                    callCompleter.completeCall(transformedCallee, data).result
                 } else {
                     transformedCallee
                 }
@@ -432,10 +432,10 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
                 callResolver.resolveCallAndSelectCandidate(withTransformedArguments)
             }
 
-            val completeInference = callCompleter.completeCall(resultExpression, data)
+            val (completeInference, callCompleted) = callCompleter.completeCall(resultExpression, data)
             val result = completeInference.transformToIntegerOperatorCallOrApproximateItIfNeeded(data)
             if (!resolvingAugmentedAssignment) {
-                dataFlowAnalyzer.exitFunctionCall(result, data.forceFullCompletion)
+                dataFlowAnalyzer.exitFunctionCall(result, callCompleted && data.forceFullCompletion)
             }
 
             addReceiversFromExtensions(result)
@@ -947,11 +947,11 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
             .transformAnnotations(transformer, ResolutionMode.ContextIndependent)
             .replaceArgumentList(checkNotNullCall.argumentList.transform(transformer, ResolutionMode.ContextDependent))
 
-        val result = callCompleter.completeCall(
+        val (result, callCompleted) = callCompleter.completeCall(
             components.syntheticCallGenerator.generateCalleeForCheckNotNullCall(checkNotNullCall, resolutionContext), data
         )
 
-        dataFlowAnalyzer.exitCheckNotNullCall(result, data.forceFullCompletion)
+        dataFlowAnalyzer.exitCheckNotNullCall(result, callCompleted && data.forceFullCompletion)
         return result
     }
 
@@ -1299,8 +1299,8 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
 
         // it seems that we may leave this code as is
         // without adding `context.withTowerDataContext(context.getTowerDataContextForConstructorResolution())`
-        val result = callCompleter.completeCall(resolvedCall, ResolutionMode.ContextIndependent)
-        dataFlowAnalyzer.exitDelegatedConstructorCall(result, data.forceFullCompletion)
+        val (result, callCompleted) = callCompleter.completeCall(resolvedCall, ResolutionMode.ContextIndependent)
+        dataFlowAnalyzer.exitDelegatedConstructorCall(result, callCompleted && data.forceFullCompletion)
         return result
     }
 
