31e96eb7de0ed0cf1fcaab01dbd4ec973fe474b7
Oddbjørn Kvalsund <oddbjorn.kvalsund@gmail.com>
2023-08-11 12:45:54 +0200

    Update KotlinCompileMojoBase.java
    kotlin-maven-plugin: Simplify log message when there are no sources to compile
    
    This new message is now identical to the Java compiler plugins message for the same scenario.
    
    (cherry picked from commit 0fc5086d1118669a48e57ed70174c630bafbf9c4)

--- libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/KotlinCompileMojoBase.java
+++ libraries/tools/kotlin-maven-plugin/src/main/java/org/jetbrains/kotlin/maven/KotlinCompileMojoBase.java
@@ -203,7 +203,7 @@ public abstract class KotlinCompileMojoBase<A extends CommonCompilerArguments> e
                 " (JRE " + System.getProperty("java.runtime.version") + ")");
 
         if (!hasKotlinFilesInSources()) {
-            getLog().info("No sources found skipping Kotlin compile");
+            getLog().info("No sources to compile");
             return;
         }
 
