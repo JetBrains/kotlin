952a381d8dd3a9f1ea15cea2865fb3353ec3d2f3
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-08-11 12:21:58 +0200

    [FE] Skip delegation descriptors during default params matching
    ^KT-61163 Fixed
    Review: https://jetbrains.team/p/kt/reviews/11599/timeline
    
    They can be safely skiped because we check overridden descriptors
    anyway.
    
    IMO, it's a bug that delegated descriptors are copied with default
    params. But it's much harder problem, and my IMO can easily be wrong for
    some weird reason.
    
    (cherry picked from commit 577d0db3f15007e06379bd27aa96c8938a791e58)

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
@@ -22777,6 +22777,18 @@ public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
             }
 
             @Test
+            @TestMetadata("defaultParams_inheritanceByDelegation_negative.kt")
+            public void testDefaultParams_inheritanceByDelegation_negative() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/multiplatform/defaultParams_inheritanceByDelegation_negative.kt");
+            }
+
+            @Test
+            @TestMetadata("defaultParams_inheritanceByDelegation_positive.kt")
+            public void testDefaultParams_inheritanceByDelegation_positive() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/multiplatform/defaultParams_inheritanceByDelegation_positive.kt");
+            }
+
+            @Test
             @TestMetadata("expectAbstractToString.kt")
             public void testExpectAbstractToString() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/multiplatform/expectAbstractToString.kt");
