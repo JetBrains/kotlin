1df08f942df59d834ad253abb5e28f1ccb6be1e2
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2023-08-14 19:35:12 +0200

    Create FUS Gradle plugin
    fus-statistics-gradle-plugin can be used by any other Gradle plugins to
    collect additional metrics for FUS.
    
    KT-59627

---
+++ libraries/tools/gradle/fus-statistics-gradle-plugin/ReadMe.md
@@ -0,0 +1,4 @@
+## Description
+
+Contains a plugin for FUS statistics. fus-statistics-gradle-plugin can be used by other Gradle plugins to
+collect additional metrics for FUS.  
---
+++ libraries/tools/gradle/fus-statistics-gradle-plugin/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("gradle-plugin-common-configuration")
+}
+
+
+dependencies {
+    commonApi(project(":kotlin-gradle-plugin-api"))
+    commonApi(project(":kotlin-gradle-plugin"))
+    commonCompileOnly(gradleKotlinDsl())
+}
+
+
+gradlePlugin {
+    plugins {
+        create("fus-statistics-gradle-plugin") {
+            id = "org.jetbrains.kotlin.fus-statistics-gradle-plugin"
+            displayName = "FusStatisticsPlugin"
+            description = displayName
+            implementationClass = "org.jetbrains.kotlin.gradle.fus.FusStatisticsPlugin"
+        }
+    }
+}
