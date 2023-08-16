f90b624d3568765ad8a73189971c949aa9e7f324
vmishenev <vad-mishenev@yandex.ru>
2023-08-16 22:11:55 +0300

    Update Dokka to 1.9.10-dev
    - Use analysis plugin configuration instead of `StdLibAnalysisConfigurationPlugin`
    - Change SamplesTransformer plugin after Dokka refactoring
    
    (cherry picked from commit 5c0c8ed2d508d2daf57067b9cb993b88c106bfd1)

--- libraries/tools/kotlin-stdlib-docs/plugins/dokka-samples-transformer-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/SamplesTransformerPlugin.kt
+++ libraries/tools/kotlin-stdlib-docs/plugins/dokka-samples-transformer-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/SamplesTransformerPlugin.kt
@@ -1,19 +1,23 @@
 package org.jetbrains.dokka.kotlinlang
 
-import org.jetbrains.dokka.CoreExtensions
-import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.InternalDokkaApi
+import org.jetbrains.dokka.analysis.kotlin.descriptors.compiler.CompilerDescriptorAnalysisPlugin
+import org.jetbrains.dokka.analysis.kotlin.internal.InternalKotlinAnalysisPlugin
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.jetbrains.dokka.plugability.DokkaPluginApiPreview
 import org.jetbrains.dokka.plugability.PluginApiPreviewAcknowledgement
+import org.jetbrains.dokka.plugability.querySingle
 
 class SamplesTransformerPlugin : DokkaPlugin() {
-    private val dokkaBase by lazy { plugin<DokkaBase>() }
+    @OptIn(InternalDokkaApi::class)
+    private val dokkaKotlinAnalysisPlugin by lazy { plugin<InternalKotlinAnalysisPlugin>() }
+    @OptIn(InternalDokkaApi::class)
+    private val dokkaDescriptorAnalysisPlugin by lazy { plugin<CompilerDescriptorAnalysisPlugin>() }
 
+    @OptIn(InternalDokkaApi::class)
     @Suppress("unused")
     val kotlinWebsiteSamplesTransformer by extending {
-        CoreExtensions.pageTransformer providing ::KotlinWebsiteSamplesTransformer override dokkaBase.defaultSamplesTransformer order {
-            before(dokkaBase.pageMerger)
-        }
+        dokkaKotlinAnalysisPlugin.sampleProviderFactory providing ::KotlinWebsiteSampleProviderFactory override dokkaDescriptorAnalysisPlugin.kotlinSampleProviderFactory
     }
 
     @DokkaPluginApiPreview
--- libraries/tools/kotlin-stdlib-docs/plugins/dokka-stdlib-configuration-plugin/src/main/kotlin/org/jetbrains/dokka/kotlinlang/StdLibAnalysisConfiguration.kt
+++
@@ -1,5 +0,0 @@
-package org.jetbrains.dokka.kotlinlang
-
-import org.jetbrains.dokka.plugability.ConfigurableBlock
-
-data class StdLibAnalysisConfiguration(val ignoreCommonBuiltIns: Boolean) : ConfigurableBlock
