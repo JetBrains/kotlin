7a1dc1f89be1109bd680d1084590e5d2aeb9165e
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-08-16 18:14:35 +0200

    [Build] Do not add ij dependencies in `testApiJUnit5`
    This is not needed since the dependencies are already declared as API dependencies in the relevant projects.
    #KTI-1349 In Progress

--- compiler/tests-common/build.gradle.kts
+++ compiler/tests-common/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     testApi(project(":kotlin-test:kotlin-test-jvm"))
     testApi(project(":kotlin-scripting-compiler-impl"))
     testApi(projectTests(":compiler:test-infrastructure-utils"))
-    testApi(libs.junit4)
+    testApi(libs.junit4) // for ComparisonFailure
     testApi(commonDependency("com.android.tools:r8"))
     testApi(project(":analysis:analysis-internal-utils"))
     testCompileOnly(commonDependency("org.jetbrains.kotlin:kotlin-reflect")) { isTransitive = false }
@@ -73,7 +73,7 @@ dependencies {
 
     testApi(jpsModel()) { isTransitive = false }
     testApi(jpsModelImpl()) { isTransitive = false }
-    testApi(intellijJavaRt())
+    testApi(intellijJavaRt()) // for FileComparisonFailure
 
     testImplementation(libs.guava)
     testImplementation(commonDependency("org.jetbrains.intellij.deps:trove4j"))
