b1965e746f9b04a924919324cc01bd6f2652ac07
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-08-21 19:20:41 +0200

    [Gradle] Move project isolation test to separate test suite
    ^KT-59826 In Progress

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ProjectIsolationIT.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle
+
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.junit.jupiter.api.DisplayName
+
+class ProjectIsolationIT : KGPBaseTest() {
+
+    override val defaultBuildOptions: BuildOptions
+        get() = super.defaultBuildOptions.copy(configurationCache = true, projectIsolation = true)
+
+    @DisplayName("Should be compatible with project isolation")
+    @JvmGradlePluginTests
+    @GradleTestVersions(
+        minVersion = TestVersions.Gradle.G_7_1,
+        maxVersion = TestVersions.Gradle.G_7_6
+    )
+    @GradleTest
+    fun testProjectIsolationInJvmSimple(gradleVersion: GradleVersion) {
+        project(
+            projectName = "instantExecution",
+            gradleVersion = gradleVersion,
+        ) {
+            build(":main-project:compileKotlin")
+        }
+    }
+}
