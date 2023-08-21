fbd9d65d38ef396566cc948d0e9c3942e4594fa9
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-08-21 21:54:41 +0200

    [Build] Move native-platform declaration to version catalog
    #KT-55271 In Progress

--- compiler/daemon/daemon-client/build.gradle.kts
+++ compiler/daemon/daemon-client/build.gradle.kts
@@ -8,28 +8,12 @@ plugins {
     id("jps-compatible")
 }
 
-val nativePlatformVariants = listOf(
-    "windows-amd64",
-    "windows-i386",
-    "osx-amd64",
-    "osx-i386",
-    "linux-amd64",
-    "linux-i386",
-    "freebsd-amd64-libcpp",
-    "freebsd-amd64-libstdcpp",
-    "freebsd-i386-libcpp",
-    "freebsd-i386-libstdcpp"
-)
-
 dependencies {
     compileOnly(project(":daemon-common"))
-    compileOnly(commonDependency("net.rubygrapefruit", "native-platform"))
+    compileOnly(libs.native.platform)
 
     embedded(project(":daemon-common")) { isTransitive = false }
-    embedded(commonDependency("net.rubygrapefruit", "native-platform"))
-    nativePlatformVariants.forEach {
-        embedded(commonDependency("net.rubygrapefruit", "native-platform", "-$it"))
-    }
+    embedded(libs.bundles.native.platform.all)
 }
 
 tasks.withType<KotlinCompilationTask<*>> {
