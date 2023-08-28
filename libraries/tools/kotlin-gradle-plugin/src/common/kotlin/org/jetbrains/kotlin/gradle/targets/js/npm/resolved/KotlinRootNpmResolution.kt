72b3f5ebe30222bddb8b8cfbfebbb8430dfca82d
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-08-28 12:36:35 +0000

    [Gradle, JS] Distinguish creating prepared resolution and writing package.json
    ^KT-61326 fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/resolved/KotlinRootNpmResolution.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/npm/resolved/KotlinRootNpmResolution.kt
@@ -16,7 +16,7 @@ import java.io.Serializable
 class KotlinRootNpmResolution(
     val projects: Map<String, KotlinProjectNpmResolution>,
     val rootProjectName: String,
-    val rootProjectVersion: String
+    val rootProjectVersion: String,
 ) : Serializable {
     operator fun get(project: String) = projects[project] ?: KotlinProjectNpmResolution.empty()
 
@@ -27,12 +27,20 @@ class KotlinRootNpmResolution(
         logger: Logger,
         npmEnvironment: NpmEnvironment,
         yarnEnvironment: YarnEnvironment,
-        npmResolutionManager: KotlinNpmResolutionManager
+        npmResolutionManager: KotlinNpmResolutionManager,
     ): Installation {
         synchronized(projects) {
             npmResolutionManager.parameters.gradleNodeModulesProvider.get().close()
 
-            val projectResolutions: List<PreparedKotlinCompilationNpmResolution> = projects.values.flatMap { it.npmProjects }.map { it.close() }
+            val projectResolutions: List<PreparedKotlinCompilationNpmResolution> = projects.values
+                .flatMap { it.npmProjects }
+                .map {
+                    it.close(
+                        npmResolutionManager,
+                        logger
+                    )
+                }
+
             npmEnvironment.packageManager.prepareRootProject(
                 npmEnvironment,
                 rootProjectName,
