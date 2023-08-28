adceae83a558860031295185a002c42bb5065928
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2023-08-28 14:11:34 +0200

    try: enable mark checks

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
@@ -227,7 +227,7 @@ class KonanConfig(val project: Project, val configuration: CompilerConfiguration
     }
 
     val gcCheckMarkCorrectness: Boolean
-        get() = configuration.get(BinaryOptions.gcCheckMarkCorrectness) ?: false
+        get() = configuration.get(BinaryOptions.gcCheckMarkCorrectness) ?: true
 
     val irVerificationMode: IrVerificationMode
         get() = configuration.getNotNull(KonanConfigKeys.VERIFY_IR)
