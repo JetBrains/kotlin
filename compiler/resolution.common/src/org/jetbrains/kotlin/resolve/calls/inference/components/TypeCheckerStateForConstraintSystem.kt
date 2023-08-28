5bbc0049f574ccf5fe86554c71e265c18f219f45
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-08-28 17:36:38 +0200

    ~????

--- compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/inference/components/TypeCheckerStateForConstraintSystem.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/inference/components/TypeCheckerStateForConstraintSystem.kt
@@ -143,8 +143,8 @@ abstract class TypeCheckerStateForConstraintSystem(
     // extract type variable only from type like Captured(out T)
     private fun extractTypeVariableForSubtype(subType: KotlinTypeMarker, superType: KotlinTypeMarker): KotlinTypeMarker? =
         with(extensionTypeContext) {
-            // see testData/diagnostics/tests/inference/builderInference/stubTypes/capturedTypes.kt
-            if (isK2) return null
+//            // see testData/diagnostics/tests/inference/builderInference/stubTypes/capturedTypes.kt
+//            if (isK2) return null
             val typeMarker = subType.asSimpleType()?.asCapturedType() ?: return null
 
             val projection = typeMarker.typeConstructorProjection()
