d39333eeb736050dacbb6c6295595ea09c9575a0
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-08-29 18:38:01 +0200

    [repo] Update jgit to the latest version
    ^KT-61190 In Progress

--- libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
+++ libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-common")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.4.0.202211300538-r")
+    implementation(libs.jgit)
     implementation("org.slf4j:slf4j-nop:1.7.32")
     implementation("org.jetbrains.kotlinx:dataframe:0.8.1")
 }
