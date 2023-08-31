06a1bab5ae2b7142b035921612ca3391cd245997
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-08-31 16:02:13 +0200

    [KDM] Update copyright
    See https://github.com/Kotlin/dokka/pull/3149
    
    * Add the default copyright profile and enable IDEA project settings
    * Add the license notice file
    Original commit: https://github.com/Kotlin/dokka/commit/6a181a7a2b03ec263788d137610e86937a57d434

--- libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/plugability/LazyEvaluated.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/core/src/main/kotlin/plugability/LazyEvaluated.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package org.jetbrains.dokka.plugability
 
 internal class LazyEvaluated<T : Any> private constructor(private val recipe: ((DokkaContext) -> T)? = null, private var value: T? = null) {
