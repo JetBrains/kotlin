GlebSolovev/kmm-research

[Docs] add wasm source sets for wasm-js and wasm-wasi

libraries/tools/kotlin-stdlib-docs/build.gradle.kts
@@ -168,6 +168,35 @@ fun createStdLibVersionedDocTask(version: String, isLatest: Boolean) =
                     suppress.set(true)
                 }
             }
+            register("wasm-js") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-Js")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_stdlib_dir/native-wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/internal")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/stubs")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/js/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/js/internal")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/js/src")
+            }
+            register("wasm-wasi") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-Wasi")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_stdlib_dir/native-wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/internal")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/stubs")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/wasi/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/wasi/src")
+            }
             configureEach {
                 documentedVisibilities.set(setOf(DokkaConfiguration.Visibility.PUBLIC, DokkaConfiguration.Visibility.PROTECTED))
                 skipDeprecated.set(false)
@@ -249,7 +278,7 @@ fun createKotlinTestVersionedDocTask(version: String, isLatest: Boolean) =
 
         val moduleDirName = "kotlin-test"
         with(pluginsMapConfiguration) {
-            put("org.jetbrains.dokka.base.DokkaBase", """{ "templatesDir": "$templatesDir" }""")
+            put("org.jetbrains.dokka.base.DokkaBase", """{ "mergeImplicitExpectActualDeclarations": "true", "templatesDir": "$templatesDir" }""")
             put("org.jetbrains.dokka.versioning.VersioningPlugin", """{ "version": "$version" }""")
         }
         if (isLatest) {
@@ -344,6 +373,24 @@ fun createKotlinTestVersionedDocTask(version: String, isLatest: Boolean) =
                 displayName.set("Native")
                 dependsOn("common")
                 sourceRoots.from("$kotlin_native_root/runtime/src/main/kotlin/kotlin/test")
+            }
+            register("wasm-js") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-Js")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/src/main")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/js/src/main")
+            }
+            register("wasm-wasi") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-Wasi")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/src/main")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/wasi/src/main")
             }
             configureEach {
                 skipDeprecated.set(false)

JetBrains/kotlin

(cherry picked from commit 516383b99581994671472dd145af782ea88639ad)

libraries/tools/kotlin-stdlib-docs/build.gradle.kts
@@ -168,6 +168,35 @@ fun createStdLibVersionedDocTask(version: String, isLatest: Boolean) =
                     suppress.set(true)
                 }
             }
+            register("wasm-js") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-JS")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_stdlib_dir/native-wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/internal")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/stubs")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/js/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/js/internal")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/js/src")
+            }
+            register("wasm-wasi") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-WASI")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_stdlib_dir/native-wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/src")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/internal")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/stubs")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/wasi/builtins")
+                sourceRoots.from("$kotlin_stdlib_dir/wasm/wasi/src")
+            }
             configureEach {
                 documentedVisibilities.set(setOf(DokkaConfiguration.Visibility.PUBLIC, DokkaConfiguration.Visibility.PROTECTED))
                 skipDeprecated.set(false)
@@ -249,7 +278,7 @@ fun createKotlinTestVersionedDocTask(version: String, isLatest: Boolean) =
 
         val moduleDirName = "kotlin-test"
         with(pluginsMapConfiguration) {
-            put("org.jetbrains.dokka.base.DokkaBase", """{ "templatesDir": "$templatesDir" }""")
+            put("org.jetbrains.dokka.base.DokkaBase", """{ "mergeImplicitExpectActualDeclarations": "true", "templatesDir": "$templatesDir" }""")
             put("org.jetbrains.dokka.versioning.VersioningPlugin", """{ "version": "$version" }""")
         }
         if (isLatest) {
@@ -344,6 +373,24 @@ fun createKotlinTestVersionedDocTask(version: String, isLatest: Boolean) =
                 displayName.set("Native")
                 dependsOn("common")
                 sourceRoots.from("$kotlin_native_root/runtime/src/main/kotlin/kotlin/test")
+            }
+            register("wasm-js") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-JS")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/src/main")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/js/src/main")
+            }
+            register("wasm-wasi") {
+                platform.set(Platform.wasm)
+                noJdkLink.set(true)
+
+                displayName.set("Wasm-WASI")
+                dependsOn("common")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/src/main")
+                sourceRoots.from("$kotlin_root/libraries/kotlin.test/wasm/wasi/src/main")
             }
             configureEach {
                 skipDeprecated.set(false)