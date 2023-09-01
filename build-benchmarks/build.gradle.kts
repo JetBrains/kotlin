1612d609591cce4cf9a82dbe40dc4800f5108965
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-09-01 19:16:37 +0200

    Update benchmark scenarios after rebase

--- build-benchmarks/build.gradle.kts
+++ build-benchmarks/build.gradle.kts
@@ -4,44 +4,13 @@ version = "1.0-SNAPSHOT"
 plugins {
     java
     application
+    kotlin("jvm") version "1.9.10"
 }
-
-buildscript {
-    val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "1.8.10"
-    val kotlinRepo = "https://buildserver.labs.intellij.net/guestAuth/app/rest/builds/buildType:(id:Kotlin_KotlinDev_CompilerDistAndMavenArtifacts),number:$kotlinVersion,branch:default:any/artifacts/content/maven"
-    extra["kotlinRepo"] = kotlinRepo
-
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        maven {
-            url = uri(kotlinRepo)
-        }
-    }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-    }
-}
-
-tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-    targetCompatibility = JavaVersion.VERSION_1_8.toString()
-}
-
-apply {
-    plugin("kotlin")
-}
-
-val kotlinRepo: String by extra
 
 repositories {
     maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
     mavenLocal()
     mavenCentral()
-    maven {
-        url = uri(kotlinRepo)
-    }
 }
 
 dependencies {
@@ -49,16 +18,20 @@ dependencies {
     implementation(kotlin("stdlib"))
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 application {
-    mainClassName = "RunBenchmarksKt"
+    mainClass = "RunBenchmarksKt"
 }
 
 tasks.register("runGavra0", JavaExec::class) {
     classpath = sourceSets.main.get().runtimeClasspath
-    main = "RunGavra0BenchmarksKt"
+    mainClass = "RunGavra0BenchmarksKt"
 }
 
 tasks.register("runAbiSnapshot", JavaExec::class) {
     classpath = sourceSets.main.get().runtimeClasspath
-    main = "RunAbiSnapshotsBenchmarksKt"
+    mainClass = "RunAbiSnapshotsBenchmarksKt"
 }
