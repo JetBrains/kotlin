9c24ab4c4ddb9b5809226401608230939fdac6b4
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2023-09-01 14:13:04 +0200

    [Gradle] Implement KotlinExtensionPoint to allow extensible designs inside the KotlinGradlePlugin
    KT-61634

---
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/extensionPointUtils.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.util
+
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.plugin.KotlinExtensionPoint
+import org.jetbrains.kotlin.gradle.plugin.KotlinExtensionPointInternal
+import org.jetbrains.kotlin.tooling.core.UnsafeApi
+
+/**
+ * Completely overwrites the currently registered extensions on this [KotlinExtensionPoint] in this project.
+ */
+@OptIn(UnsafeApi::class)
+fun <T> KotlinExtensionPoint<T>.set(project: Project, extensions: List<T>) {
+    (this as KotlinExtensionPointInternal<T>)
+    set(project, extensions)
+}
