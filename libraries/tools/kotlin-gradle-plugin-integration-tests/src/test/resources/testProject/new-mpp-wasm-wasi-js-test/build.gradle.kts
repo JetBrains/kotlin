f3ea86a5c1f78bc90b9c15ec8fdc952a9d0d6b62
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-09-01 11:53:04 +0000

    [Gradle, wasm] Hack for mixed mode with js, wasmJs and wasmWasi
    ^KT-61623 fixed
    
    (cherry picked from commit 0f40b2adceb977df25e926e9a8ef79c6505549cd)

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/new-mpp-wasm-wasi-js-test/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform").version("<pluginMarkerVersion>").apply(false)
+}
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "20.2.0"
+
+    tasks.named<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockCopyTask>("kotlinStoreYarnLock") {
+        //A little hacky way to make yarn results
+        inputFile.fileValue(projectDir.resolve("yarnLockStub"))
+    }
+}
+
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.d8.D8RootPlugin> {
+    // Test that we can set the version and it is a String.
+    // But use the default version since update this place every time anyway.
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.d8.D8RootExtension>().version = (version as String)
+}
+
+allprojects.forEach {
+    it.tasks.withType<org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask>().configureEach {
+        args.add("--ignore-engines")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
