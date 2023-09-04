0701da6c59d524939ee55fe9021ada20b78ee45d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-09-04 16:01:03 +0200

    [stub] simplify ClassId creation
    We can create nested ClassId just from the parent stub instead of calculation by tree.
    This commit simplified the complexity of ClassId building to liner.
    Example:
    ```kotlin
    package one
    class A {
      class B {
        class C {
          class D
        }
      }
    }
    ```
    Previously, we had to create ClassId by traverse parents for each class.
    Now we will visit each class only once.
    It should improve performance and memory consumption during indexing.
    
    ^KTIJ-26848

--- compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtTypeAliasElementType.kt
+++ compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtTypeAliasElementType.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.elements
@@ -21,11 +10,8 @@ import com.intellij.psi.stubs.StubElement
 import com.intellij.psi.stubs.StubInputStream
 import com.intellij.psi.stubs.StubOutputStream
 import com.intellij.util.io.StringRef
-import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtTypeAlias
 import org.jetbrains.kotlin.psi.psiUtil.safeFqNameForLazyResolve
-import org.jetbrains.kotlin.psi.stubs.KotlinClassStub
-import org.jetbrains.kotlin.psi.stubs.KotlinFileStub
 import org.jetbrains.kotlin.psi.stubs.KotlinTypeAliasStub
 import org.jetbrains.kotlin.psi.stubs.StubUtils
 import org.jetbrains.kotlin.psi.stubs.impl.KotlinTypeAliasStubImpl
@@ -33,24 +19,12 @@ import org.jetbrains.kotlin.psi.stubs.impl.KotlinTypeAliasStubImpl
 class KtTypeAliasElementType(debugName: String) :
     KtStubElementType<KotlinTypeAliasStub, KtTypeAlias>(debugName, KtTypeAlias::class.java, KotlinTypeAliasStub::class.java) {
 
-    override fun createStub(psi: KtTypeAlias, parentStub: StubElement<*>?): KotlinTypeAliasStub {
+    override fun createStub(psi: KtTypeAlias, parentStub: StubElement<*>): KotlinTypeAliasStub {
         val name = StringRef.fromString(psi.name)
         val fqName = StringRef.fromString(psi.safeFqNameForLazyResolve()?.asString())
-        val classId = parentStub?.let { createNestedClassId(it, psi) }
+        val classId = StubUtils.createNestedClassId(parentStub, psi)
         val isTopLevel = psi.isTopLevel()
         return KotlinTypeAliasStubImpl(parentStub, name, fqName, classId, isTopLevel)
-    }
-
-    private fun createNestedClassId(parentStub: StubElement<*>, typeAlias: KtTypeAlias): ClassId? {
-        val typeAliasName = typeAlias.nameAsName ?: return null
-        return when {
-            parentStub.stubType == KtStubElementTypes.CLASS_BODY -> {
-                val parentClass = parentStub.parentStub as? KotlinClassStub
-                parentClass?.getClassId()?.createNestedClassId(typeAliasName)
-            }
-            parentStub is KotlinFileStub -> ClassId(parentStub.getPackageFqName(), typeAliasName)
-            else -> null
-        }
     }
 
     override fun serialize(stub: KotlinTypeAliasStub, dataStream: StubOutputStream) {
