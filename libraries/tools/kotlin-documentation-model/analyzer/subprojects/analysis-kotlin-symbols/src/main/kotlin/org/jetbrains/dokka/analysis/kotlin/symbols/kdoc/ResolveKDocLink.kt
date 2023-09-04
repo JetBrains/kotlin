18c6ba0fa908863edd09717a44ade26700612cb7
Vadim Mishenev <vad-mishenev@yandex.ru>
2023-09-04 17:18:55 +0300

    [KDM] [K2] Fix functional type and improve logging for unresolved link
    See https://github.com/Kotlin/dokka/pull/3157
    
    * [K2] Fix functional type and improve logging for unresolved link
    
    For example
    `typealias CompletionHandler = (cause: Throwable?) -> Unit`
    has a functional type with no type arguments in K2. In K1 we have a usual generic type
    Original commit: https://github.com/Kotlin/dokka/commit/e9e8fbf59efc5eefdaef2a432dbdef299a89fde1

--- libraries/tools/kotlin-documentation-model/analyzer/subprojects/analysis-kotlin-symbols/src/main/kotlin/org/jetbrains/dokka/analysis/kotlin/symbols/kdoc/ResolveKDocLink.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/subprojects/analysis-kotlin-symbols/src/main/kotlin/org/jetbrains/dokka/analysis/kotlin/symbols/kdoc/ResolveKDocLink.kt
@@ -15,15 +15,24 @@ import org.jetbrains.kotlin.kdoc.psi.impl.KDocLink
 import org.jetbrains.kotlin.kdoc.psi.impl.KDocName
 import org.jetbrains.kotlin.psi.KtPsiFactory
 
-internal fun DRI?.logIfNotResolved(link: String, logger: DokkaLogger): DRI? {
-    if(this == null)
-    logger.warn("Couldn't resolve link for $link")
-    return this
+/**
+ * Util to print a message about unresolved [link]
+ */
+internal fun DokkaLogger.logUnresolvedLink(link: String, location: String?) {
+    warn("Couldn't resolve link for $link" + if (location != null) " in $location" else "")
+}
+
+internal inline fun DRI?.ifUnresolved(action: () -> Unit): DRI? = this ?: run {
+    action()
+    null
 }
 
 /**
- * It resolves KDoc link via creating PSI.
+ * Resolves KDoc link via creating PSI.
+ * If the [link] is ambiguous, i.e. leads to more than one declaration,
+ * it returns deterministically any declaration.
  *
+ * @return [DRI] or null if the [link] is unresolved
  */
 internal fun KtAnalysisSession.resolveKDocTextLink(link: String, context: PsiElement? = null): DRI? {
     val psiFactory = context?.let { KtPsiFactory.contextual(it) } ?: KtPsiFactory(this.useSiteModule.project)
@@ -38,9 +47,15 @@ internal fun KtAnalysisSession.resolveKDocTextLink(link: String, context: PsiEle
     return kDocLink?.let { resolveKDocLink(it) }
 }
 
+/**
+ * If the [link] is ambiguous, i.e. leads to more than one declaration,
+ * it returns deterministically any declaration.
+ *
+ * @return [DRI] or null if the [link] is unresolved
+ */
 internal fun KtAnalysisSession.resolveKDocLink(link: KDocLink): DRI? {
     val lastNameSegment = link.children.filterIsInstance<KDocName>().lastOrNull()
     val linkedSymbol = lastNameSegment?.mainReference?.resolveToSymbols()?.firstOrNull()
-    return if (linkedSymbol == null) null // logger.warn("Couldn't resolve link for $link")
+    return if (linkedSymbol == null) null
     else getDRIFromSymbol(linkedSymbol)
 }
