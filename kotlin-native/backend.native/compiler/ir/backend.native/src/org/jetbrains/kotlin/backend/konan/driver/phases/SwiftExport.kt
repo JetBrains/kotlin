4c57f081b9dafe529db7bc1e5e01f66f374c1e35
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2023-09-07 15:27:34 +0300

    [Swift Export] Extract embedded swift to output directory from klib

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/SwiftExport.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/SwiftExport.kt
@@ -11,8 +11,11 @@ import org.jetbrains.kotlin.backend.konan.swift.IrBasedSwiftGenerator
 import org.jetbrains.kotlin.backend.konan.swift.SwiftCode
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.konan.file.use
+import org.jetbrains.kotlin.konan.library.KonanLibrary
 import java.io.File
 import java.io.FileWriter
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
 
 internal data class SwiftGenerationInput(val module: IrModuleFragment)
 
@@ -59,5 +62,28 @@ internal val WriteSwiftGenerationOutputPhase = createSimpleNamedCompilerPhase<Ph
         FileWriter(it).use { writer ->
             input.cCodeProducer().renderLines().forEach { writer.write(it) }
         }
+    }
+}
+
+internal data class ExtractEmbeddedSwiftExtensionsInput(
+        val library: KonanLibrary,
+        val swiftOutputDirectory: File,
+        val objcOutputDirectory: File
+)
+
+/**
+ * A hack to simplify testing of Swift Export. Probably a proper solution would be to access files from klib directly.
+ */
+internal val ExtractEmbeddedSwiftExtensionsPhase = createSimpleNamedCompilerPhase<PhaseContext, ExtractEmbeddedSwiftExtensionsInput>(
+        name = "ExtractEmbeddedSwiftExtensions",
+        description = "Extract Swift and ObjC sources to the output directory",
+) { _, input ->
+    input.library.swiftSourcesPaths.forEach { swiftSrc ->
+        val swiftFile = File(swiftSrc)
+        Files.copy(swiftFile.toPath(), File(input.swiftOutputDirectory, swiftFile.name).toPath(), StandardCopyOption.REPLACE_EXISTING)
+    }
+    input.library.objcHeadersPaths.forEach { objcHeader ->
+        val objcFile = File(objcHeader)
+        Files.copy(objcFile.toPath(), File(input.objcOutputDirectory, objcFile.name).toPath(), StandardCopyOption.REPLACE_EXISTING)
     }
 }
