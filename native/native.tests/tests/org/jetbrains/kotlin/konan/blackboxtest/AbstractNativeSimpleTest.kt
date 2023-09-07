e8115820ddaa227a327a8b51e9d08d519b039016
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-09-07 15:12:02 +0200

    LV 2.0: mute CompilerOutputTest.testReleaseCompilerAgainstPreReleaseLibrary
    Related to KT-61773

--- native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/AbstractNativeSimpleTest.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/AbstractNativeSimpleTest.kt
@@ -12,11 +12,24 @@ import org.jetbrains.kotlin.konan.blackboxtest.support.runner.TestExecutable
 import org.jetbrains.kotlin.konan.blackboxtest.support.runner.TestRunners.createProperTestRunner
 import org.jetbrains.kotlin.konan.blackboxtest.support.settings.SimpleTestRunSettings
 import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.fail
 
 @ExtendWith(NativeSimpleTestSupport::class)
 abstract class AbstractNativeSimpleTest {
     internal lateinit var testRunSettings: SimpleTestRunSettings
     internal lateinit var testRunProvider: SimpleTestRunProvider
+
+    fun muteForK2(isK2: Boolean, test: () -> Unit) {
+        if (!isK2) {
+            return test()
+        }
+        try {
+            test()
+        } catch (e: Throwable) {
+            return
+        }
+        fail("Looks like this test can be unmuted. Remove the call to `muteForK2`.")
+    }
 
     internal fun runExecutableAndVerify(testCase: TestCase, executable: TestExecutable) {
         val testRun = testRunProvider.getTestRun(testCase, executable)
--- native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/CompilerOutputTest.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/blackboxtest/CompilerOutputTest.kt
@@ -14,6 +14,7 @@ import org.jetbrains.kotlin.konan.blackboxtest.support.compilation.LibraryCompil
 import org.jetbrains.kotlin.konan.blackboxtest.support.compilation.TestCompilationArtifact
 import org.jetbrains.kotlin.konan.blackboxtest.support.compilation.TestCompilationResult
 import org.jetbrains.kotlin.konan.blackboxtest.support.compilation.TestCompilationResult.Companion.assertSuccess
+import org.jetbrains.kotlin.konan.blackboxtest.support.settings.PipelineType
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -21,8 +22,10 @@ import java.io.File
 @TestDataPath("\$PROJECT_ROOT")
 @EnforcedProperty(ClassLevelProperty.COMPILER_OUTPUT_INTERCEPTOR, "NONE")
 class CompilerOutputTest : AbstractNativeSimpleTest() {
+
+    // TODO: unmute after fix of KT-61773
     @Test
-    fun testReleaseCompilerAgainstPreReleaseLibrary() {
+    fun testReleaseCompilerAgainstPreReleaseLibrary() = muteForK2(isK2 = testRunSettings.get<PipelineType>() == PipelineType.K2) {
         // We intentionally use JS testdata, because the compilers should behave the same way in such a test.
         // To be refactored later, after CompileKotlinAgainstCustomBinariesTest.testReleaseCompilerAgainstPreReleaseLibraryJs is fixed.
         val rootDir = File("compiler/testData/compileKotlinAgainstCustomBinaries/releaseCompilerAgainstPreReleaseLibraryJs")
