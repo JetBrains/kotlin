9f6f3752884dec6d2e483dcbf1b8624dc435fbf7
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-09-08 17:20:18 +0200

    ~ reasonable

--- compiler/testData/diagnostics/testsWithStdLib/builderInference/upperBoundViolation_noReporting.kt
+++ compiler/testData/diagnostics/testsWithStdLib/builderInference/upperBoundViolation_noReporting.kt
@@ -4,8 +4,8 @@
 fun <T : Number> printGenericNumber(t: T) = println("Number is $t")
 
 fun main() {
-    buildList { // inferred into MutableList<String>
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>buildList<!> { // inferred into MutableList<String>
         add("Boom")
-        printGenericNumber(this[0])
+        printGenericNumber(<!ARGUMENT_TYPE_MISMATCH!>this[0]<!>)
     }
 }
