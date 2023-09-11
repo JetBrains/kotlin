847f979494fa05df01acc61e1c87d36bae5ae9cb
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2023-09-11 18:51:33 +0300

    [Swift Export] KT-60697: export Kotlin.Any type as an ObjC type
    Since we are going to keep runtime peer-object model from ObjCExport,
    we can reuse KotlinBase from Objective-C export for Swift Export.
    It gives us many things for free, including integration with memory
    manager.

--- native/native.tests/testData/SwiftExport/bridged_types/main.kt
+++ native/native.tests/testData/SwiftExport/bridged_types/main.kt
@@ -1,1 +1,12 @@
 fun identity(obj: Any): Any = obj
+
+fun produceAny(): Any {
+    return Any()
+}
+fun equals(any1: Any, any2: Any): Boolean {
+    return any1 == any2
+}
+
+fun refEquals(any1: Any, any2: Any): Boolean {
+    return any1 === any2
+}
