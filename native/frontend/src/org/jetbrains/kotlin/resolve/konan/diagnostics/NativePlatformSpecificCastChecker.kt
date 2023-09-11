97a60b6ed4c81fec290f973a88a0af45e22672d1
Timofey Solonin <timofey.solonin@jetbrains.com>
2023-09-11 11:18:02 +0200

    fixup! Don't emit CAST_NEVER_SUCCEEDS when casting to a forward declaration type

--- native/frontend/src/org/jetbrains/kotlin/resolve/konan/diagnostics/NativePlatformSpecificCastChecker.kt
+++ native/frontend/src/org/jetbrains/kotlin/resolve/konan/diagnostics/NativePlatformSpecificCastChecker.kt
@@ -8,8 +8,6 @@ package org.jetbrains.kotlin.resolve.konan.diagnostics
 import org.jetbrains.kotlin.builtins.PlatformSpecificCastChecker
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.konan.getForwardDeclarationKindOrNull
-import org.jetbrains.kotlin.resolve.descriptorUtil.classId
-import org.jetbrains.kotlin.resolve.descriptorUtil.getAllSuperClassifiers
 import org.jetbrains.kotlin.types.KotlinType
 
 object NativePlatformSpecificCastChecker : PlatformSpecificCastChecker {
@@ -18,23 +16,13 @@ object NativePlatformSpecificCastChecker : PlatformSpecificCastChecker {
     }
 
     /**
-     * Here, we check only that a type has a supertype with a name that could result in a valid cast, to suppress a CAST_NEVER_SUCCEEDS
-     * warning. The cast is further checked in NativeForwardDeclarationRttiChecker.
+     * Here, we only check that we are casting to a forward declaration to suppress a CAST_NEVER_SUCCEEDS warning. The cast is further
+     * checked in NativeForwardDeclarationRttiChecker.
      */
     private fun isCastToAForwardDeclaration(realType: KotlinType, forwardDeclarationType: KotlinType): Boolean {
         val forwardDeclarationClassDescriptor = forwardDeclarationType.constructor.declarationDescriptor
         if (forwardDeclarationClassDescriptor !is ClassDescriptor) return false
-
-        val isACastToAForwardDeclarationType = forwardDeclarationClassDescriptor.getForwardDeclarationKindOrNull() != null
-        if (!isACastToAForwardDeclarationType) return false
-
-        val forwardDeclarationClassName = forwardDeclarationClassDescriptor.classId?.relativeClassName ?: return false
-        val isAnySuperClassifierNamedAsTheForwardDeclaration = realType.constructor.declarationDescriptor
-            ?.getAllSuperClassifiers()?.any { superClassifier ->
-                superClassifier.classId?.relativeClassName == forwardDeclarationClassName
-            } ?: false
-
-        return isAnySuperClassifierNamedAsTheForwardDeclaration
+        return forwardDeclarationClassDescriptor.getForwardDeclarationKindOrNull() != null
     }
 
 }
