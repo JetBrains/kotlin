140840da026b842ee718e4a51087e1c6bf6b07b2
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2023-09-12 10:25:27 +0200

    [K/N] OncePerThreadAction helper template

--- kotlin-native/runtime/src/mm/cpp/SafePoint.hpp
+++ kotlin-native/runtime/src/mm/cpp/SafePoint.hpp
@@ -8,6 +8,7 @@
 #include <atomic>
 #include <utility>
 
+#include "ThreadData.hpp"
 #include "Utils.hpp"
 
 namespace kotlin::mm {
@@ -35,6 +36,78 @@ private:
 
 void safePoint(std::memory_order fastPathOrder = std::memory_order_relaxed) noexcept;
 void safePoint(ThreadData& threadData, std::memory_order fastPathOrder = std::memory_order_relaxed) noexcept;
+
+/**
+ * Helps in implementation and execution of actions which should be performed by each thread exactly once.
+ * e.g. GC barriers enablement.
+ *
+ * Ensures that either the mutator thread performs the action itself, or the requester thread thread does it for them
+ * (in case mutator thread is not reachable e.g. in the native code at the moment).
+ */
+template<typename Impl, typename SafePointActivator = mm::SafePointActivator>
+class OncePerThreadAction {
+public:
+    class ThreadData final : private Pinned {
+        friend OncePerThreadAction;
+    public:
+        explicit ThreadData(OncePerThreadAction& owner, mm::ThreadData& base) noexcept : owner_(owner), base_(base) {}
+        void onSafePoint() noexcept {
+            CallsCheckerIgnoreGuard guard;
+
+            if (owner_.actionRequested_.load()) {
+                if (actionPerformed_.load(std::memory_order_acquire)) return; // TODO to acquire barriers state?
+                std::unique_lock lock(mutex_);
+                if (actionPerformed_.load(std::memory_order_relaxed)) return; // TODO order?
+                Impl::action(base_);
+                actionPerformed_.store(true, std::memory_order_release);
+            }
+        }
+    private:
+        OncePerThreadAction& owner_;
+        mm::ThreadData& base_;
+        std::atomic<bool> actionPerformed_ = false;
+        std::mutex mutex_;
+    };
+
+    void ensurePerformed(mm::ThreadRegistry::Iterable& threads) {
+        RuntimeAssert(actionRequested_ == false, "no pending action expected");
+
+        // reset action performed flag
+        for (auto& thread: threads) {
+            OncePerThreadAction::ThreadData& utilityData = Impl::getUtilityData(thread);
+            std::unique_lock lock(utilityData.mutex_);
+            utilityData.actionPerformed_.store(false, std::memory_order_relaxed); // TODO order?
+        }
+
+        AutoReset scopedRequestGuard(&actionRequested_, true);
+        SafePointActivator safePointActivator;
+
+        while (!std::all_of(threads.begin(), threads.end(), [=](mm::ThreadData& thread) {
+            OncePerThreadAction::ThreadData& utilityData = Impl::getUtilityData(thread);
+
+            if (utilityData.actionPerformed_.load(std::memory_order_acquire)) return true;
+
+            std::unique_lock lock(utilityData.mutex_, std::try_to_lock);
+            if (!lock) {
+                // The thread must be performing the action. Skip them for now.
+                return false;
+            }
+
+
+            if (thread.suspensionData().suspendedOrNative()) {
+                Impl::action(thread);
+                utilityData.actionPerformed_.store(true, std::memory_order_release);
+                return true;
+            }
+
+            // let a runnable thread perform the action themself
+            return false;
+        })) { std::this_thread::yield(); }
+    }
+
+private:
+    std::atomic<bool> actionRequested_ = false;
+};
 
 namespace test_support {
 
--- kotlin-native/runtime/src/mm/cpp/SafePointTest.cpp
+++ kotlin-native/runtime/src/mm/cpp/SafePointTest.cpp
@@ -146,3 +146,114 @@ TEST_F(SafePointActionTest, SafePointWithExplicitThread) {
         mm::test_support::setSafePointAction(nullptr);
     });
 }
+
+class OncePerThreadActionTest : public ::testing::Test {
+public:
+    struct FakeSafePointActivator : private MoveOnly {
+        FakeSafePointActivator() noexcept {
+            EXPECT_FALSE(mm::test_support::safePointsAreActive());
+            mm::test_support::setSafePointAction([](mm::ThreadData& thread) {
+                if (instance_->actionReady_) {
+                    TestAction::getUtilityData(thread).onSafePoint();
+                }
+            });
+        }
+        ~FakeSafePointActivator() {
+            EXPECT_TRUE(mm::test_support::safePointsAreActive());
+            mm::test_support::setSafePointAction(nullptr);
+        }
+    };
+
+    struct TestAction : public mm::OncePerThreadAction<TestAction, FakeSafePointActivator> {
+        static OncePerThreadAction::ThreadData& getUtilityData(mm::ThreadData& threadData) {
+            return instance_->actionUtilData_.find(&threadData)->second;
+        }
+        static void action(mm::ThreadData& threadData) noexcept {
+            instance_->mockAction_.Call(threadData);
+        }
+    };
+
+    OncePerThreadActionTest() {
+        EXPECT_THAT(instance_, nullptr);
+        instance_ = this;
+    }
+
+    ~OncePerThreadActionTest() {
+        EXPECT_THAT(instance_, this);
+        instance_ = nullptr;
+    }
+
+    void registerThread(mm::ThreadData& thread) {
+        auto [_, inserted] = actionUtilData_.try_emplace(&thread, testAction_, thread);
+        EXPECT_TRUE(inserted);
+    }
+
+    void setActionReady() {
+        actionReady_ = true;
+    }
+
+    auto& testAction() { return testAction_; }
+    auto& mockAction() { return mockAction_; }
+
+private:
+    static OncePerThreadActionTest* instance_;
+    TestAction testAction_;
+    std::unordered_map<mm::ThreadData*, TestAction::ThreadData> actionUtilData_;
+    testing::StrictMock<testing::MockFunction<void(mm::ThreadData&)>> mockAction_;
+    std::atomic<bool> actionReady_ = false;
+};
+
+// static
+OncePerThreadActionTest* OncePerThreadActionTest::instance_ = nullptr;
+
+TEST_F(OncePerThreadActionTest, StressOncePerThreadAction) {
+    constexpr auto kIterations = 10;
+
+    std::atomic<size_t> initialized = 0;
+    std::mutex initializationMutex_;
+
+    std::atomic<bool> canStop = false;
+
+    std::vector<ScopedThread> threads;
+    for (int i = 0; i < kDefaultThreadCount; ++i) {
+        threads.emplace_back([&]() noexcept {
+            ScopedMemoryInit memory;
+            mm::ThreadData& threadData = *memory.memoryState()->GetThreadData();
+            {
+                std::unique_lock initLock(initializationMutex_);
+                registerThread(threadData);
+            }
+            initialized += 1;
+
+            while (!canStop.load(std::memory_order_relaxed)) {
+                std::this_thread::yield();
+
+                {
+                    kotlin::ThreadStateGuard guard(kotlin::ThreadState::kNative);
+                    std::this_thread::yield();
+                }
+
+                mm::safePoint();
+            }
+        });
+    }
+
+    while (initialized.load(std::memory_order_relaxed) < threads.size()) {
+        std::this_thread::yield();
+    }
+    std::unique_lock initLock(initializationMutex_);
+    setActionReady();
+
+    for (int i = 0; i < kIterations; ++i) {
+        auto threadRegistry = mm::ThreadRegistry::Instance().LockForIter();
+        for (auto& thread: threadRegistry) {
+            EXPECT_CALL(mockAction(), Call(testing::Ref(thread))).Times(1);
+        }
+        testAction().ensurePerformed(threadRegistry);
+        testing::Mock::VerifyAndClearExpectations(&mockAction());
+
+        std::this_thread::yield();
+    }
+
+    canStop = true;
+}
