4cc94cd4d8d627a79b337a4eeaf11e7c77bbe43d
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-09-13 12:45:36 +0200

    WIP! Wed 13 Sep 12:45:36 CEST 2023 missing_actual

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/mpp/FirExpectActualResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/mpp/FirExpectActualResolver.kt
@@ -86,8 +86,9 @@ object FirExpectActualResolver {
                     }
                 }
                 is FirClassLikeSymbol<*> -> {
-                    val expectClassSymbol = useSiteSession.dependenciesSymbolProvider
-                        .getClassLikeSymbolByClassId(actualSymbol.classId) as? FirRegularClassSymbol ?: return null
+                    val classLikeSymbolByClassId = useSiteSession.dependenciesSymbolProvider
+                        .getClassLikeSymbolByClassId(actualSymbol.classId)
+                    val expectClassSymbol = classLikeSymbolByClassId as? FirRegularClassSymbol ?: return null
                     val compatibility = AbstractExpectActualCompatibilityChecker.getClassifiersCompatibility(
                         expectClassSymbol,
                         actualSymbol,
