d45e4a2e63cc81af198ecc18a44948525ec1b8f5
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-09-14 20:26:14 +0200

    WIP: effectiveModality
    WIP: add LV check later
    
    Introduce LV vs don't introduce LV:
    
    introduce LV:
    - K1 IDE and K2 project: IDE doesn't show false positive or false negative
    - K2 IDE and K1 project: There are cases when IDE shows red but in reality it's green
    
    don't introduce LV:
    - K1 IDE and K2 project: There are cases when IDE shows red but in reality it's green
    - K2 IDE and K1 project: There are cases when IDE shows green but in reality it's red

---
+++ compiler/testData/diagnostics/tests/multiplatform/widerVisibility_expectIsEffectivelyFinal.kt
@@ -0,0 +1,18 @@
+// FIR_IDENTICAL
+// MODULE: m1-common
+// FILE: common.kt
+
+open class Base {
+    internal open fun foo() {}
+}
+expect class Foo : Base {
+    override fun foo()
+}
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: jvm.kt
+
+actual class Foo : Base() {
+    public actual override fun foo() {
+    }
+}
