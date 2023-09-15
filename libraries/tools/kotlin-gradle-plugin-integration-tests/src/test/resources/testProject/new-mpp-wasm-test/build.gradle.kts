c915cc54c7e3f665c6c07d155fa542fdb206218d
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-09-15 14:57:48 +0200

    [Wasm] Use Node.js with "canary" v8 in gradle mpp tests
    It's required due to switching to the latest wasm opcodes.
    
    #KT-59720

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/new-mpp-wasm-test/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/new-mpp-wasm-test/build.gradle.kts
@@ -8,13 +8,21 @@ repositories {
 }
 
 with(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin.apply(rootProject)) {
-    nodeVersion = "20.2.0"
+    // canary nodejs that supports recent Wasm GC changes
+    nodeVersion = "21.0.0-v8-canary202309167e82ab1fa2"
+    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
 }
 
 with(org.jetbrains.kotlin.gradle.targets.js.d8.D8RootPlugin.apply(rootProject)) {
     // Test that we can set the version and it is a String.
     // But use the default version since update this place every time anyway.
     version = (version as String)
+}
+
+allprojects.forEach {
+    it.tasks.withType<org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask>().configureEach {
+        args.add("--ignore-engines")
+    }
 }
 
 tasks.named<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockCopyTask>("kotlinStoreYarnLock") {
