64767bb5bd724cd06c9e44d8aedc51729eb5fcb0
Bryan Logan <bryanlogan@users.noreply.github.com>
2023-09-18 11:00:57 -0500

    [ABI Validation] Fix and test cases
    Fixes https://github.com/Kotlin/binary-compatibility-validator/issues/135 
    Pull request https://github.com/Kotlin/binary-compatibility-validator/pull/136
    
    Moved from https://github.com/Kotlin/binary-compatibility-validator/commit/0dc8cd990042113dba7d552a5debbc77546736df

---
+++ libraries/tools/abi-validation/src/functionalTest/resources/examples/classes/BuildCon.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2016-2020 JetBrains s.r.o.
+ * Use of this source code is governed by the Apache 2.0 License that can be found in the LICENSE.txt file.
+ */
+
+package com.company
+
+public class BuildCon {
+    public val p1 = 1
+
+    public fun f1() = p1
+}
