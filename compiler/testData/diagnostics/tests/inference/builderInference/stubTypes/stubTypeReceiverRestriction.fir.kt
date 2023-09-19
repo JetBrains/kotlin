816de676b54ed32cb1d0ebc143f93a69a41e3d8d
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-09-19 14:31:03 +0200

    ~ red-to-red code

--- compiler/testData/diagnostics/tests/inference/builderInference/stubTypes/stubTypeReceiverRestriction.fir.kt
+++ compiler/testData/diagnostics/tests/inference/builderInference/stubTypes/stubTypeReceiverRestriction.fir.kt
@@ -27,7 +27,7 @@ fun test2() {
 
 fun test3() {
     operator fun <T> T.getValue(thisRef: Any?, prop: Any?): T = this
-    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>a<!> {
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>a<!> {
         val v by this.get(0)
         v.extension()
         use(v::extension)
@@ -40,7 +40,7 @@ class Box<TIn>(val t: TIn)
 fun test4() {
     operator fun <T> T.provideDelegate(thisRef: Any?, prop: Any?): Box<T> = Box(this)
     operator fun <T> Box<T>.getValue(thisRef: Any?, prop: Any?): T = this.t
-    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>a<!> {
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>a<!> {
         val v by this.get(0)
         v.extension()
         use(v::extension)
@@ -55,7 +55,7 @@ fun test5() {
     operator fun <T> T.invoke(): T = this
     <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>b<!> {
         extension()
-        this().extension()
+        <!CANNOT_INFER_PARAMETER_TYPE!>this<!><!NO_VALUE_FOR_PARAMETER!>()<!>.<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>extension<!>()
         use(::extension)
     }
 }
@@ -63,10 +63,10 @@ fun test5() {
 val <T> T.genericLambda: T.((T) -> Unit) -> Unit get() = {}
 
 fun test6() {
-    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>b<!> {
-        extension()
-        genericLambda { }
-        genericLambda { it.extension() }
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>b<!> {
+        <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>extension<!>()
+        <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>genericLambda<!> { }
+        <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>genericLambda<!> { it.extension() }
         use(::extension)
     }
 }
