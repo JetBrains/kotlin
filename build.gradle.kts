53f994263f4280264ac4fb5055dac320b6f6e995
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-09-20 17:44:27 +0000

    Bump Kotlin/Native version in KGP to 1.9.30-dev-3695
    Merge-request: KT-MR-12303
    Merged-by: Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -99,7 +99,7 @@ IdeVersionConfigurator.setCurrentIde(project)
 
 if (!project.hasProperty("versions.kotlin-native")) {
     // BEWARE! Bumping this version doesn't take an immediate effect on TeamCity: KTI-1107
-    extra["versions.kotlin-native"] = "1.9.30-dev-3194"
+    extra["versions.kotlin-native"] = "1.9.30-dev-3695"
 }
 
 val irCompilerModules = arrayOf(
