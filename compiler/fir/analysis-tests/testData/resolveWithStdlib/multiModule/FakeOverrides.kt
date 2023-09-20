5ed979b70df661ca647b11f71af305e2d6e8512b
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-09-20 12:31:02 +0200

    K2: Implement a checker that disallows to have different member scopes for `expect open` and its `actual`
    ^KT-61031 Fixed
    Review: https://jetbrains.team/p/kt/reviews/12147/timeline

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/multiModule/FakeOverrides.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/multiModule/FakeOverrides.kt
@@ -17,10 +17,10 @@ open class C : B() {
 // MODULE: m1-jvm(m1-common)
 // FILE: jvm.kt
 
-actual open class A<T> {
+actual open <!ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_MEMBERS_AS_NON_FINAL_EXPECT_CLASSIFIER!>class A<!><T> {
     actual open fun foo(arg: T) {}
-    open fun bar(arg: T): T = arg
-    open fun baz(arg: T): T = arg
+    open fun <!NON_ACTUAL_MEMBER_DECLARED_IN_EXPECT_NON_FINAL_CLASSIFIER_ACTUALIZATION!>bar<!>(arg: T): T = arg
+    open fun <!NON_ACTUAL_MEMBER_DECLARED_IN_EXPECT_NON_FINAL_CLASSIFIER_ACTUALIZATION!>baz<!>(arg: T): T = arg
 }
 class D : C() {
     fun test() {
