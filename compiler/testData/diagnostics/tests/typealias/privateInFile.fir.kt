1115ff1a51847f538afb9c751224b0fe01427a90
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-10-04 15:58:36 +0300

    [FIR] Store the first file in `data.state.classifierContainerFileMap`
    Note that this doesn't affect
    multi-module configurations
    
    ^KT-59927

--- compiler/testData/diagnostics/tests/typealias/privateInFile.fir.kt
+++ compiler/testData/diagnostics/tests/typealias/privateInFile.fir.kt
@@ -1,22 +1,22 @@
 // FILE: file1.kt
-private class C {
+private class <!PACKAGE_OR_CLASSIFIER_REDECLARATION!>C<!> {
     companion object
 }
 
-private typealias TA = C
+private typealias <!PACKAGE_OR_CLASSIFIER_REDECLARATION!>TA<!> = C
 
-private val test1: <!INVISIBLE_REFERENCE!>C<!> = <!INVISIBLE_REFERENCE!>C<!>()
-private val test1co: <!INVISIBLE_REFERENCE!>C<!>.Companion = <!INVISIBLE_REFERENCE!>C<!>
-
-private val test2: <!INVISIBLE_REFERENCE!>TA<!> = <!INVISIBLE_REFERENCE!>TA<!>()
-private val test2co = <!INVISIBLE_REFERENCE!>TA<!>
-
-// FILE: file2.kt
 private val test1: C = C()
 private val test1co: C.Companion = C
 
 private val test2: TA = TA()
 private val test2co = TA
 
-private class <!PACKAGE_OR_CLASSIFIER_REDECLARATION!>C<!>
-private typealias <!PACKAGE_OR_CLASSIFIER_REDECLARATION!>TA<!> = Int
+// FILE: file2.kt
+private val test1: <!INVISIBLE_REFERENCE!>C<!> = <!INVISIBLE_REFERENCE!>C<!>()
+private val test1co: <!INVISIBLE_REFERENCE!>C<!>.Companion = <!INVISIBLE_REFERENCE!>C<!>
+
+private val test2: <!INVISIBLE_REFERENCE!>TA<!> = <!INVISIBLE_REFERENCE!>TA<!>()
+private val test2co = <!INVISIBLE_REFERENCE!>TA<!>
+
+private class C
+private typealias TA = Int
