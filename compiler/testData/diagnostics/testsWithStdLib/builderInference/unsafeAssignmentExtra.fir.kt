cfa06a96d33c2fb418f6ed92487853f90b2fd7ea
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-10-04 18:27:27 +0200

    ~ reasonable

--- compiler/testData/diagnostics/testsWithStdLib/builderInference/unsafeAssignmentExtra.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/builderInference/unsafeAssignmentExtra.fir.kt
@@ -26,18 +26,18 @@ fun main(arg: Any, condition: Boolean) {
     val value = myBuilder {
         b[0] = 123
         a = 45
-        a<!OVERLOAD_RESOLUTION_AMBIGUITY!>++<!>
+        a<!NONE_APPLICABLE!>++<!>
         bar(::a)
         if (<!USELESS_IS_CHECK!>a is Int<!>) {
             a = 67
-            a<!OVERLOAD_RESOLUTION_AMBIGUITY!>--<!>
+            a<!NONE_APPLICABLE!>--<!>
             bar(::a)
         }
         when (condition) {
             true -> a = 87
             false -> a = 65
         }
-        val x by <!DELEGATE_SPECIAL_FUNCTION_AMBIGUITY!>a<!>
+        val x by <!DELEGATE_SPECIAL_FUNCTION_NONE_APPLICABLE!>a<!>
 
         change {
             a = 99
@@ -62,8 +62,8 @@ fun main(arg: Any, condition: Boolean) {
     // See KT-54664
     val value3 = myBuilder {
         accept("")
-        a = 45
-        bar(::a)
+        a = <!ASSIGNMENT_TYPE_MISMATCH!>45<!>
+        bar(::<!UNRESOLVED_REFERENCE!>a<!>)
     }
 
     fun baz(t: Int) {}
@@ -72,6 +72,6 @@ fun main(arg: Any, condition: Boolean) {
         accept("")
         a = 45
         b[0] = 123
-        baz(a)
+        baz(<!ARGUMENT_TYPE_MISMATCH!>a<!>)
     }
 }
