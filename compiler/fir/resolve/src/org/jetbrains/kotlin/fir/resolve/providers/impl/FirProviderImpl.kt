21cb5cf83a8f06cb5dc2fe1f81520be39fcad986
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-10-04 15:58:36 +0300

    [FIR] Store the first file in `data.state.classifierContainerFileMap`
    Note that this doesn't affect
    multi-module configurations
    
    ^KT-59927

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/providers/impl/FirProviderImpl.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/providers/impl/FirProviderImpl.kt
@@ -121,15 +121,8 @@ class FirProviderImpl(val session: FirSession, val kotlinScopeProvider: FirKotli
         override fun visitElement(element: FirElement, data: FirRecorderData) {}
 
         override fun visitRegularClass(regularClass: FirRegularClass, data: FirRecorderData) {
+            visitClassifier(regularClass, data)
             val classId = regularClass.symbol.classId
-            val prevFile = data.state.classifierContainerFileMap.put(classId, data.file)
-            val storedClassifier = data.state.classifierMap.getOrPut(classId) { regularClass }
-
-            if (storedClassifier != regularClass) {
-                data.nameConflictsTracker?.registerClassifierRedeclaration(
-                    classId, regularClass.symbol, data.file, storedClassifier.symbol, prevFile,
-                )
-            }
 
             if (!classId.isNestedClass && !classId.isLocal) {
                 data.state.classesInPackage.getOrPut(classId.packageFqName, ::mutableSetOf).add(classId.shortClassName)
@@ -140,17 +133,24 @@ class FirProviderImpl(val session: FirSession, val kotlinScopeProvider: FirKotli
         }
 
         override fun visitTypeAlias(typeAlias: FirTypeAlias, data: FirRecorderData) {
+            visitClassifier(typeAlias, data)
             val classId = typeAlias.symbol.classId
-            val prevFile = data.state.classifierContainerFileMap.put(classId, data.file)
-            val storedClassifier = data.state.classifierMap.getOrPut(classId) { typeAlias }
+            data.state.classifierInPackage.getOrPut(classId.packageFqName, ::mutableSetOf).add(classId.shortClassName)
+        }
 
-            if (storedClassifier != typeAlias) {
+        private fun visitClassifier(classLike: FirClassLikeDeclaration, data: FirRecorderData) {
+            val classId = classLike.symbol.classId
+
+            if (classId !in data.state.classifierMap) {
+                data.state.classifierMap[classId] = classLike
+                data.state.classifierContainerFileMap[classId] = data.file
+            } else {
                 data.nameConflictsTracker?.registerClassifierRedeclaration(
-                    classId, typeAlias.symbol, data.file, storedClassifier.symbol, prevFile,
+                    classId, classLike.symbol, data.file,
+                    data.state.classifierMap.getValue(classId).symbol,
+                    data.state.classifierContainerFileMap.getValue(classId),
                 )
             }
-
-            data.state.classifierInPackage.getOrPut(classId.packageFqName, ::mutableSetOf).add(classId.shortClassName)
         }
 
         override fun visitPropertyAccessor(
