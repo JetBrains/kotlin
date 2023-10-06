2c7fcf5ce91337be37fd63dce16f73cc7251d9c6
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-10-06 13:38:16 +0200

    ~ red-to-red after rebase

--- compiler/testData/diagnostics/tests/inference/builderInference/kt47986Enabled.kt
+++ compiler/testData/diagnostics/tests/inference/builderInference/kt47986Enabled.kt
@@ -7,7 +7,7 @@ fun <K> buildFoo(builderAction: Foo<K>.() -> Unit): Foo<K> = Foo()
 fun <K> Foo<K>.bar(x: Int = 1) {}
 
 fun main() {
-    val x = <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>buildFoo<!> {
-        bar()
+    val x = <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>buildFoo<!> {
+        <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>bar<!>()
     }
 }
