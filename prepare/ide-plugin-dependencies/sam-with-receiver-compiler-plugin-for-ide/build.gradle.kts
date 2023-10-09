1ec6a29d1b6553a9c3ad90232f0ba37f2a05474f
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-10-09 22:51:57 +0200

    [build] fix project names in `sam-with-receiver-compiler-plugin-for-ide`
    They usually use one `:`

--- prepare/ide-plugin-dependencies/sam-with-receiver-compiler-plugin-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/sam-with-receiver-compiler-plugin-for-ide/build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
 
 publishJarsForIde(
     listOf(
-        "::kotlin-sam-with-receiver-compiler-plugin.cli",
-        "::kotlin-sam-with-receiver-compiler-plugin.common",
-        "::kotlin-sam-with-receiver-compiler-plugin.k1",
-        "::kotlin-sam-with-receiver-compiler-plugin.k2",
+        ":kotlin-sam-with-receiver-compiler-plugin.cli",
+        ":kotlin-sam-with-receiver-compiler-plugin.common",
+        ":kotlin-sam-with-receiver-compiler-plugin.k1",
+        ":kotlin-sam-with-receiver-compiler-plugin.k2",
     )
 )
