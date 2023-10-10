bc2eb546b43dd19f967600042b0194b1a5837f3c
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2023-10-10 12:37:19 +0300

    [FIR] Report conflicts for data class generated members
    ^KT-59880

--- compiler/testData/diagnostics/tests/dataClasses/conflictingCopyOverloads.fir.kt
+++ compiler/testData/diagnostics/tests/dataClasses/conflictingCopyOverloads.fir.kt
@@ -1,6 +1,6 @@
 // !DIAGNOSTICS: -UNUSED_PARAMETER
 
-data class A(val x: Int, val y: String) {
+data class <!CONFLICTING_OVERLOADS!>A(val x: Int, val y: String)<!> {
     <!CONFLICTING_OVERLOADS!>fun copy(x: Int, y: String)<!> = x
     <!CONFLICTING_OVERLOADS!>fun copy(x: Int, y: String)<!> = A(x, y)
 }
--- compiler/testData/diagnostics/tests/dataClasses/dataClassExplicitlyOverridingCopyWithDefaults.fir.kt
+++ compiler/testData/diagnostics/tests/dataClasses/dataClassExplicitlyOverridingCopyWithDefaults.fir.kt
@@ -4,6 +4,6 @@ interface WithCopy<T> {
     fun copy(str: T): WithCopy<T>
 }
 
-<!DATA_CLASS_OVERRIDE_DEFAULT_VALUES!>data<!> class Test(val str: String) : WithCopy<String> {
-    override fun copy(str: String = <!DEFAULT_VALUE_NOT_ALLOWED_IN_OVERRIDE!>this.str<!>) = Test(str)
+<!DATA_CLASS_OVERRIDE_DEFAULT_VALUES!>data<!> class <!CONFLICTING_OVERLOADS!>Test(val str: String)<!> : WithCopy<String> {
+    <!CONFLICTING_OVERLOADS!>override fun copy(str: String = <!DEFAULT_VALUE_NOT_ALLOWED_IN_OVERRIDE!>this.str<!>)<!> = Test(str)
 }
