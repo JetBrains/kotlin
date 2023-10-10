32a87836c2902ea5d57792b2d907e7ba0f22abac
Roman Efremov <roman.efremov@jetbrains.com>
2023-10-10 17:37:45 +0200

    [Test] Improve test of SUPERTYPE_INITIALIZED_IN_EXPECTED_CLASS diagnostic
    Add case where explicit primary constructor is present in expect,
    so that SUPERTYPE_INITIALIZED_WITHOUT_PRIMARY_CONSTRUCTOR is not
    reported.
    
    ^KT-59979

--- compiler/testData/diagnostics/tests/multiplatform/headerClass/superClass.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/headerClass/superClass.fir.kt
@@ -9,14 +9,20 @@ expect class Foo : I, C, J
 
 <!INCOMPATIBLE_MATCHING{JVM}, SUPERTYPE_INITIALIZED_WITHOUT_PRIMARY_CONSTRUCTOR, SUPERTYPE_INITIALIZED_WITHOUT_PRIMARY_CONSTRUCTOR!>expect class Bar : C()<!>
 
+expect class WithExplicitPrimaryConstructor() : C()
+
 // MODULE: m2-jvm()()(m1-common)
 // FILE: jvm.kt
 actual class Foo : I, C(), J
 
 actual class <!ACTUAL_WITHOUT_EXPECT!>Bar<!>
 
+actual class WithExplicitPrimaryConstructor : C()
+
 // MODULE: m3-js()()(m1-common)
 // FILE: js.kt
 actual class Foo : I, J, C()
 
 actual class Bar : C()
+
+actual class WithExplicitPrimaryConstructor : C()
