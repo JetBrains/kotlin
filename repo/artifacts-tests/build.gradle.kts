1dc76f0b94e10177efe79d04a9bdc522103d9acc
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-10-12 14:59:58 +0200

    [Build] Fix configuration cache issue with  :repo:artifacts-tests:test
    ^KTI-1473 In Progress

--- repo/artifacts-tests/build.gradle.kts
+++ repo/artifacts-tests/build.gradle.kts
@@ -16,10 +16,11 @@ dependencies {
 projectTest(jUnitMode = JUnitMode.JUnit5) {
     workingDir = rootDir
     useJUnitPlatform { }
+    val kotlinVersion = version
     doFirst {
         val defaultMavenLocal = Paths.get(System.getProperty("user.home"), ".m2", "repository").toAbsolutePath()
         val mavenLocal = System.getProperty("maven.repo.local") ?: defaultMavenLocal
         systemProperty("maven.repo.local", mavenLocal)
-        systemProperty("kotlin.version", version)
+        systemProperty("kotlin.version", kotlinVersion)
     }
 }
