6ca30dac5077dc4fde0d50bd0f103f8a5fc3a2da
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2023-10-12 11:38:02 +0200

    [BIR] Setup basic module structure

---
+++ compiler/ir/bir/tree/tree-generator/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+    application
+}
+
+val runtimeOnly by configurations
+val compileOnly by configurations
+runtimeOnly.extendsFrom(compileOnly)
+
+dependencies {
+    implementation(project(":generators"))
+    implementation(project(":generators:tree-generator-common"))
+    implementation(project(":compiler:util"))
+    implementation("com.squareup:kotlinpoet:1.11.0")
+
+    compileOnly(intellijCore())
+    compileOnly(commonDependency("org.jetbrains.intellij.deps:trove4j"))
+
+    runtimeOnly(commonDependency("org.jetbrains.intellij.deps:jdom"))
+}
+
+application {
+    mainClass.set("org.jetbrains.kotlin.bir.generator.MainKt")
+}
+
+sourceSets {
+    "main" {
+        projectDefault()
+    }
+    "test" {}
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>> {
+    kotlinOptions {
+        allWarningsAsErrors = false
+        languageVersion = "2.0"
+    }
+}
