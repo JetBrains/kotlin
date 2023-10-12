4595309453147011af2137bed10320732fd693f5
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2023-10-12 15:13:20 +0200

    [BIR] Autogenerate BirMutableAnnotationContainer

--- compiler/ir/bir/tree/src/org/jetbrains/kotlin/bir/declarations/BirAnnotationContainer.kt
+++ compiler/ir/bir/tree/src/org/jetbrains/kotlin/bir/declarations/BirAnnotationContainer.kt
@@ -1,18 +1,12 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.bir.declarations
 
 import org.jetbrains.kotlin.bir.expressions.BirConstructorCall
-import org.jetbrains.kotlin.ir.declarations.IrAnnotationContainer
-import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
 
 interface BirAnnotationContainer {
     val annotations: List<BirConstructorCall>
-}
-
-interface BirMutableAnnotationContainer : BirAnnotationContainer {
-    override var annotations: List<BirConstructorCall>
 }
