3784782268457e4ee2ac323df929dc6d721c5db3
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-10-13 13:57:28 +0200

    [K2] ReturnType expect-actual incompatibility: convert from strong to weak
    It should have been WeakIncompatible from the beginning because it's not
    possible to overload by return type in Kotlin
    
    This commit is a step forward to fix KT-62591
    
    Unfortunately, the test cannot demonstrate the problem because of
    another bug in K2 KT-59887

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/DiagnosticTestGenerated.java
@@ -23465,6 +23465,12 @@ public class DiagnosticTestGenerated extends AbstractDiagnosticTest {
             }
 
             @Test
+            @TestMetadata("returnTypeVsGenericsUpperBoundIncompatibility.kt")
+            public void testReturnTypeVsGenericsUpperBoundIncompatibility() throws Exception {
+                runTest("compiler/testData/diagnostics/tests/multiplatform/returnTypeVsGenericsUpperBoundIncompatibility.kt");
+            }
+
+            @Test
             @TestMetadata("sealedClassWithPrivateConstructor.kt")
             public void testSealedClassWithPrivateConstructor() throws Exception {
                 runTest("compiler/testData/diagnostics/tests/multiplatform/sealedClassWithPrivateConstructor.kt");
