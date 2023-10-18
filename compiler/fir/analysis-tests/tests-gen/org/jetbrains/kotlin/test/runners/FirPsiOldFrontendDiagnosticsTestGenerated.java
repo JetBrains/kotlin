1400d53822d52c570864d1fa231c01ca8a88a20d
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-10-18 18:04:06 +0200

    ~ synthetic calls fix

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
@@ -16896,6 +16896,12 @@ public class FirPsiOldFrontendDiagnosticsTestGenerated extends AbstractFirPsiDia
                 }
 
                 @Test
+                @TestMetadata("whenWithPropertyHavingSomePCLACallsWithinInitializer.kt")
+                public void testWhenWithPropertyHavingSomePCLACallsWithinInitializer() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/builderInference/whenWithPropertyHavingSomePCLACallsWithinInitializer.kt");
+                }
+
+                @Test
                 @TestMetadata("withoutAnnotationDisabledFeature.kt")
                 public void testWithoutAnnotationDisabledFeature() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/builderInference/withoutAnnotationDisabledFeature.kt");
