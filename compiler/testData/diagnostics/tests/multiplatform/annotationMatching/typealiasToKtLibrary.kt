d700cd13ac5c6d38e6b4f28f8ed0ca538351b4b2
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-10-20 13:53:51 +0200

    [FE] 1.9.20 exclusive commit. Fix testData after previous two commits
    This is 1.9.20 exclusive commit. testData in 1.9.20 and master diverged
    so much that I need special commits for the release branch

--- compiler/testData/diagnostics/tests/multiplatform/annotationMatching/typealiasToKtLibrary.kt
+++ compiler/testData/diagnostics/tests/multiplatform/annotationMatching/typealiasToKtLibrary.kt
@@ -40,4 +40,4 @@ actual typealias <!ACTUAL_ANNOTATIONS_NOT_MATCH_EXPECT!>MyDeprecatedNotMatch<!> 
 
 actual typealias MyDeprecatedMatch = kotlin.Deprecated
 
-actual typealias <!ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_MEMBERS_AS_NON_FINAL_EXPECT_CLASSIFIER_WARNING, ACTUAL_CLASSIFIER_MUST_HAVE_THE_SAME_SUPERTYPES_AS_NON_FINAL_EXPECT_CLASSIFIER_WARNING!>MyAbstractIterator<!><T> = AbstractIterator<T>
+actual typealias MyAbstractIterator<T> = AbstractIterator<T>
