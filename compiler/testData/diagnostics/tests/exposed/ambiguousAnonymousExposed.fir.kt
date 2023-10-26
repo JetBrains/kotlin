f82c3c3bf70146b10c4af1ec18238e5694efeca8
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2023-10-26 11:20:02 +0200

    K2: add test confirming finished migration in KT-33917
    Related to KT-62918, KT-60086

--- compiler/testData/diagnostics/tests/exposed/ambiguousAnonymousExposed.fir.kt
+++ compiler/testData/diagnostics/tests/exposed/ambiguousAnonymousExposed.fir.kt
@@ -12,3 +12,8 @@ fun fooFoo(): My<I1> = My(object : I1, I2 {})
 <!AMBIGUOUS_ANONYMOUS_TYPE_INFERRED!>internal fun bar()<!> = My(object : I1, I2 {})
 
 private fun baz() = My(object : I1, I2 {})
+
+// See also KT-33917
+<!AMBIGUOUS_ANONYMOUS_TYPE_INFERRED!>private <!NOTHING_TO_INLINE!>inline<!> fun bazBaz()<!> = My(object : I1, I2 {})
+
+private <!NOTHING_TO_INLINE!>inline<!> fun bazBazBaz() = My(object : I1 {})
--- compiler/testData/diagnostics/tests/exposed/ambiguousAnonymousExposed.kt
+++ compiler/testData/diagnostics/tests/exposed/ambiguousAnonymousExposed.kt
@@ -12,3 +12,8 @@ fun fooFoo(): My<I1> = My(object : I1, I2 {})
 internal fun <!EXPOSED_FUNCTION_RETURN_TYPE!>bar<!>() = My(object : I1, I2 {})
 
 private fun baz() = My(object : I1, I2 {})
+
+// See also KT-33917
+private <!NOTHING_TO_INLINE!>inline<!> fun <!PRIVATE_INLINE_FUNCTIONS_RETURNING_ANONYMOUS_OBJECTS!>bazBaz<!>() = My(object : I1, I2 {})
+
+private <!NOTHING_TO_INLINE!>inline<!> fun <!PRIVATE_INLINE_FUNCTIONS_RETURNING_ANONYMOUS_OBJECTS!>bazBazBaz<!>() = My(object : I1 {})
