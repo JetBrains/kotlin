f566f5d47580bdc825ff04fa83012beb4f31a3eb
Alexander Korepanov <alexander.korepanov@jetbrains.com>
2023-10-30 21:25:39 +0100

    Add test

--- js/js.tests/test/org/jetbrains/kotlin/incremental/AbstractInvalidationTest.kt
+++ js/js.tests/test/org/jetbrains/kotlin/incremental/AbstractInvalidationTest.kt
@@ -131,6 +131,7 @@ abstract class AbstractInvalidationTest(
         copy.put(JSConfigurationKeys.PROPERTY_LAZY_INITIALIZATION, true)
         copy.put(JSConfigurationKeys.SOURCE_MAP, true)
         copy.put(JSConfigurationKeys.USE_ES6_CLASSES, targetBackend == TargetBackend.JS_IR_ES6)
+        copy.put(JSConfigurationKeys.COMPILE_SUSPEND_AS_JS_GENERATOR, true)
 
         copy.languageVersionSettings = with(LanguageVersionSettingsBuilder()) {
             language.forEach {
