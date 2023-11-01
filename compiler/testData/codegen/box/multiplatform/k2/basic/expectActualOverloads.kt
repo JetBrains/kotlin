5f71c2c76caa595ce7ff16398ca55a4a21a6789b
Nikita Bobko <nikita.bobko@jetbrains.com>
2023-11-01 14:11:22 +0100

    fixup! [IR] Workaround expectActualOverloads test failure

--- compiler/testData/codegen/box/multiplatform/k2/basic/expectActualOverloads.kt
+++ compiler/testData/codegen/box/multiplatform/k2/basic/expectActualOverloads.kt
@@ -1,6 +1,9 @@
 // IGNORE_BACKEND_K1: JS, JS_IR, JS_IR_ES6, NATIVE, WASM
 // !LANGUAGE: +MultiPlatformProjects
 
+// Ignore until KT-62926 is fixed
+// IGNORE_BACKEND_K2: ANY
+
 // MODULE: common
 // TARGET_PLATFORM: Common
 // FILE: commonMain.kt
@@ -11,13 +14,11 @@ expect fun foo(s: S): S
 
 expect fun foo(i: Int): Int
 
-// Uncomment once KT-62926 is fixed
-//expect val Int.k: Int
+expect val Int.k: Int
 
 expect val String.k: String
 
-// Uncomment once KT-62926 is fixed
-//expect var Int.l: Int
+expect var Int.l: Int
 
 expect var String.l: String
 
@@ -32,15 +33,13 @@ actual fun foo(i: Int) = i
 
 actual fun foo(s: String) = s
 
-// Uncomment once KT-62926 is fixed
-//actual val Int.k: Int get() = 42
+actual val Int.k: Int get() = 42
 
 actual val String.k: String get() = this
 
-// Uncomment once KT-62926 is fixed
-//actual var Int.l: Int
-//    get() = 48
-//    set(value) {}
+actual var Int.l: Int
+    get() = 48
+    set(value) {}
 
 actual var String.l: String
     get() = this
