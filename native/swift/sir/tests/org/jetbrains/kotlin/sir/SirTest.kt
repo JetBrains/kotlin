06bc2ab78ba2509ad502c7ee22d4df335b1bf507
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2023-11-08 15:43:43 +0200

    [Swift Export] initial infrastructure for Swift IR
    New modules 'sir', 'sir-passes', and 'sir-analysis-api' were added under
    'native:swift' for creating and manipulating Swift IR elements.
    
    * `sir` contains the implementation of Swift IR.
    * `sir-analysis-api` contains translation from Analysis API to SIR.
    * `sir-passses` contains transformation passes.
    
    This groundwork will allow to progress with the Swift IR.

---
+++ native/swift/sir/tests/org/jetbrains/kotlin/sir/SirTest.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.sir
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class SirTest {
+
+    // TODO: Just a fake test to validate that everything is working. Feel free to delete.
+    @Test
+    fun exampleTest() {
+        val fakeElement = produceSwiftElement()
+        assertTrue(fakeElement is SirElement)
+    }
+
+    private fun produceSwiftElement(): Any {
+        return object : SirElement {}
+    }
+}
