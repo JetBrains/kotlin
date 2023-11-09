0196928af7da6bf64d520ddef53aad78c64250ca
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-11-09 22:20:47 +0100

    [BTA] Add debug logging about the used compilation strategy
    ^KT-61860 In Progress

--- compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/CompilationServiceImpl.kt
+++ compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/CompilationServiceImpl.kt
@@ -123,6 +123,7 @@ internal object CompilationServiceImpl : CompilationService {
         sources: List<File>,
         arguments: List<String>,
     ): CompilationResult {
+        loggerAdapter.kotlinLogger.debug("Compiling using the in-process strategy")
         val compiler = K2JVMCompiler()
         val parsedArguments = compiler.createArguments()
         parseCommandLineArguments(arguments, parsedArguments)
@@ -182,6 +183,7 @@ internal object CompilationServiceImpl : CompilationService {
         sources: List<File>,
         arguments: List<String>,
     ): CompilationResult {
+        loggerAdapter.kotlinLogger.debug("Compiling using the daemon strategy")
         val compilerId = CompilerId.makeCompilerId(getCurrentClasspath())
         val sessionIsAliveFlagFile = buildIdToSessionFlagFile.computeIfAbsent(projectId) {
             createSessionIsAliveFlagFile()
