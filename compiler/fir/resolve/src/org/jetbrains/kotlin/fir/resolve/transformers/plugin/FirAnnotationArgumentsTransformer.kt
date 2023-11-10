28895a26138585727b4b3216c54ff7ae91280f7d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-11-10 17:41:44 +0100

    [FIR] FirAnnotationArgumentsTransformer: fix scope problem with type parameters
    We should wrap outer declaration to avoid FirTowerDataMode.CLASS_HEADER_ANNOTATIONS
    
    ^KT-63396 Fixed

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/plugin/FirAnnotationArgumentsTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/plugin/FirAnnotationArgumentsTransformer.kt
@@ -214,10 +214,9 @@ private class FirDeclarationsResolveTransformerForAnnotationArguments(
         simpleFunction: FirSimpleFunction,
         data: ResolutionMode
     ): FirSimpleFunction {
-        doTransformTypeParameters(simpleFunction)
-
         context.withSimpleFunction(simpleFunction, session) {
             simpleFunction
+                .transformTypeParameters(transformer, data)
                 .transformReturnTypeRef(transformer, data)
                 .transformReceiverParameter(transformer, data)
                 .transformValueParameters(transformer, data)
@@ -229,10 +228,9 @@ private class FirDeclarationsResolveTransformerForAnnotationArguments(
 
     override fun transformConstructor(constructor: FirConstructor, data: ResolutionMode): FirConstructor {
         val containingClass = context.containerIfAny as? FirRegularClass
-        doTransformTypeParameters(constructor)
-
         context.withConstructor(constructor) {
             constructor
+                .transformTypeParameters(transformer, data)
                 .transformAnnotations(transformer, data)
                 .transformReceiverParameter(transformer, data)
                 .transformReturnTypeRef(transformer, data)
@@ -262,10 +260,10 @@ private class FirDeclarationsResolveTransformerForAnnotationArguments(
 
     override fun transformProperty(property: FirProperty, data: ResolutionMode): FirProperty {
         property.transformReceiverParameter(transformer, ResolutionMode.ContextIndependent)
-        doTransformTypeParameters(property)
 
         context.withProperty(property) {
             property
+                .transformTypeParameters(transformer, data)
                 .transformAnnotations(transformer, data)
                 .transformReceiverParameter(transformer, data)
                 .transformReturnTypeRef(transformer, data)
