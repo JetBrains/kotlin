7f89b5c1e2404a6a5c2d69138e50fd39b0e765c2
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-11-10 01:05:49 +0100

    [BT] Add smoke tests for cross-module incremental JVM compilation
    ^KT-61860 In Progress

---
+++ compiler/build-tools/kotlin-build-tools-api-tests/src/testCommon/kotlin/compilation/SimpleJvmCrossModuleICTests.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.buildtools.api.tests.compilation
+
+import org.jetbrains.kotlin.buildtools.api.SourcesChanges
+import org.jetbrains.kotlin.buildtools.api.tests.buildToolsVersion
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.runner.BuildRunnerProvider
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.runner.LogLevel
+import org.jetbrains.kotlin.buildtools.api.tests.compilation.runner.prepareModule
+import org.jetbrains.kotlin.tooling.core.KotlinToolingVersion
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
+import kotlin.io.path.exists
+import kotlin.io.path.writeText
+
+@DisplayName("Smoke tests for cross-module incremental compilation via the build tools API")
+class SimpleJvmCrossModuleICTests : IncrementalBaseCompilationTest() {
+    @CompilationTest
+    fun compilationAvoidance(buildRunnerProvider: BuildRunnerProvider) {
+        maybeSkip(buildRunnerProvider)
+        val module1 = prepareModule("jvm-module1", workingDirectory)
+        val module2 = prepareModule("jvm-module2", workingDirectory)
+
+        buildRunnerProvider(project).use { runner ->
+            module1.compileIncrementally(runner, SourcesChanges.Unknown)
+            module2.compileIncrementally(runner, SourcesChanges.Unknown, dependencies = setOf(module1))
+        }
+
+        val barKt = module1.sourcesDirectory.resolve("bar.kt")
+        // change the form of the bar method
+        barKt.writeText(
+            """
+            class Bar {
+                fun bar() {
+                    foo()
+                }
+            }
+            """.trimIndent()
+        )
+
+        buildRunnerProvider(project).use { runner ->
+            module1.compileIncrementally(
+                runner,
+                SourcesChanges.Known(
+                    modifiedFiles = listOf(barKt.toFile()),
+                    removedFiles = emptyList()
+                )
+            ) { logs ->
+                if (buildToolsVersion >= KotlinToolingVersion(2, 0, 0, "Beta1")) {
+                    // TODO: improve compiled files check
+                    assertTrue(logs.getValue(LogLevel.DEBUG).contains("compile iteration: jvm-module1/src/bar.kt"))
+                }
+            }
+            module2.compileIncrementally(
+                runner,
+                SourcesChanges.Known(modifiedFiles = emptyList(), removedFiles = emptyList()),
+                dependencies = setOf(module1)
+            ) { logs ->
+                // check that nothing is deleted
+                assertTrue(module2.outputDirectory.resolve("AKt.class").exists())
+                assertTrue(module2.outputDirectory.resolve("BKt.class").exists())
+                if (buildToolsVersion >= KotlinToolingVersion(2, 0, 0, "Beta1")) {
+                    // TODO: improve compiled files check
+                    assertTrue(logs.getValue(LogLevel.DEBUG).none { it.startsWith("compile iteration") })
+                }
+            }
+        }
+    }
+}
