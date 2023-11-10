5768438446d050d2c6f4632c155380184bc353c5
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2023-11-10 11:46:54 +0100

    [KDM] Restructure the project to utilize included builds
    See https://github.com/Kotlin/dokka/pull/3174
    
    * Refactor and simplify artifact publishing
    * Update Gradle to 8.4
    * Refactor and simplify convention plugins and build scripts
    
    Fixes https://github.com/Kotlin/dokka/issues/3132
    
    ---------
    
    Co-authored-by: Adam <897017+aSemy@users.noreply.github.com>
    Co-authored-by: Oleg Yukhnevich <whyoleg@gmail.com>
    Original commit: https://github.com/Kotlin/dokka/commit/8e5c63d035ef44a269b8c43430f43f5c8eebfb63

---
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/dokkabuild.kotlin-jvm.gradle.kts
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    id("dokkabuild.java")
+    kotlin("jvm")
+}
+
+val rootProjectsWithoutDependencyOnDokkaCore = listOf("dokka-integration-tests")
+
+kotlin {
+    explicitApi()
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+        languageVersion.set(dokkaBuild.kotlinLanguageLevel)
+        apiVersion.set(dokkaBuild.kotlinLanguageLevel)
+
+        // These projects know nothing about the `@InternalDokkaApi` annotation, so the Kotlin compiler
+        // will complain about an unresolved opt-in requirement marker and fail the build if it's not excluded.
+        if (rootProject.name !in rootProjectsWithoutDependencyOnDokkaCore) {
+            optIn.addAll(
+                "kotlin.RequiresOptIn",
+                "org.jetbrains.dokka.InternalDokkaApi"
+            )
+        }
+
+        freeCompilerArgs.addAll(
+            // need 1.4 support, otherwise there might be problems
+            // with Gradle 6.x (it's bundling Kotlin 1.4)
+            "-Xsuppress-version-warnings",
+            "-Xjsr305=strict",
+            "-Xskip-metadata-version-check",
+        )
+    }
+}
--- libraries/tools/kotlin-documentation-model/analyzer/settings.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/settings.gradle.kts
@@ -58,50 +58,40 @@ plugins {
     `gradle-enterprise`
 }
 
+includeBuild("dokka-integration-tests")
+includeBuild("dokka-runners/runner-gradle-plugin-classic")
+includeBuild("dokka-runners/runner-maven-plugin")
+includeBuild("dokka-runners/runner-cli")
+
 include(
-    ":core",
-    ":core:test-api",
-    ":core:content-matcher-test-utils",
-
-    ":subprojects",
-
-    ":subprojects:analysis-java-psi",
-    ":subprojects:analysis-kotlin-api",
-    ":subprojects:analysis-kotlin-descriptors",
-    ":subprojects:analysis-kotlin-descriptors:compiler",
-    ":subprojects:analysis-kotlin-descriptors:ide",
-    ":subprojects:analysis-kotlin-symbols",
-    ":subprojects:analysis-markdown-jb",
-
-    ":runners:gradle-plugin",
-    ":runners:cli",
-    ":runners:maven-plugin",
-
-    ":plugins:base",
-    ":plugins:base:frontend",
-    ":plugins:base:base-test-utils",
-    ":plugins:all-modules-page",
-    ":plugins:templating",
-    ":plugins:versioning",
-    ":plugins:android-documentation",
-
-    ":plugins:mathjax",
-    ":plugins:gfm",
-    ":plugins:gfm:gfm-template-processing",
-    ":plugins:jekyll",
-    ":plugins:jekyll:jekyll-template-processing",
-    ":plugins:kotlin-as-java",
-    ":plugins:javadoc",
-
-    ":integration-tests",
-    ":integration-tests:gradle",
-    ":integration-tests:cli",
-    ":integration-tests:maven",
-
     ":docs-developer",
-)
 
-includeBuild("dokka-runners/dokkatoo")
+    ":dokka-subprojects",
+    ":dokka-subprojects:analysis-java-psi",
+    ":dokka-subprojects:analysis-kotlin-api",
+    ":dokka-subprojects:analysis-kotlin-descriptors",
+    ":dokka-subprojects:analysis-kotlin-descriptors-compiler",
+    ":dokka-subprojects:analysis-kotlin-descriptors-ide",
+    ":dokka-subprojects:analysis-kotlin-symbols",
+    ":dokka-subprojects:analysis-markdown-jb",
+    ":dokka-subprojects:core",
+    ":dokka-subprojects:core-content-matcher-test-utils",
+    ":dokka-subprojects:core-test-api",
+    ":dokka-subprojects:plugin-all-modules-page",
+    ":dokka-subprojects:plugin-android-documentation",
+    ":dokka-subprojects:plugin-base",
+    ":dokka-subprojects:plugin-base-frontend",
+    ":dokka-subprojects:plugin-base-test-utils",
+    ":dokka-subprojects:plugin-gfm",
+    ":dokka-subprojects:plugin-gfm-template-processing",
+    ":dokka-subprojects:plugin-javadoc",
+    ":dokka-subprojects:plugin-jekyll",
+    ":dokka-subprojects:plugin-jekyll-template-processing",
+    ":dokka-subprojects:plugin-kotlin-as-java",
+    ":dokka-subprojects:plugin-mathjax",
+    ":dokka-subprojects:plugin-templating",
+    ":dokka-subprojects:plugin-versioning",
+)
 
 val isCiBuild = System.getenv("GITHUB_ACTIONS") != null || System.getenv("TEAMCITY_VERSION") != null
 
@@ -113,5 +103,11 @@ gradleEnterprise {
     }
 }
 
+// This hack is required for included build support.
+// The name of the published artifact is `dokka-core`, but the module is named `core`.
+// For some reason, dependency substitution doesn't work in this case. Maybe we fall under one of the unsupported
+// cases: https://docs.gradle.org/current/userguide/composite_builds.html#included_build_substitution_limitations.
+// Should no longer be a problem once Dokka's artifacts are relocated, see #3245.
+project(":dokka-subprojects:core").name = "dokka-core"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
