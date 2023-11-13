d9d5ed607eecab39e825c554ae447eea96dcf381
Jim S <jsproch@google.com>
2023-11-13 10:13:32 -0800

    Compose Compiler to 1.5.5 to catch next release train
    Change-Id: I03296b8d99d75e82a81c7edf68e1198b107bf153 ( https://android-review.git.corp.google.com/q/I03296b8d99d75e82a81c7edf68e1198b107bf153 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -142,7 +142,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.5.4"
+        const val compilerVersion: String = "1.5.5"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
