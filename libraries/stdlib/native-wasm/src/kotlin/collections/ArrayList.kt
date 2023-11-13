90ba14f17e6eff13442cd60d4b04bfd7f4326367
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2023-11-13 20:47:52 +0200

    Remove KDoc for K/N ArrayList primary constructor
    The documentation is provided for ArrayList class but describes its
    primary constructor. Thus, in documentation website it appears
    as ArrayList documentation. When no KDoc is provided, website generator
    takes it from other platforms.

--- libraries/stdlib/native-wasm/src/kotlin/collections/ArrayList.kt
+++ libraries/stdlib/native-wasm/src/kotlin/collections/ArrayList.kt
@@ -5,18 +5,6 @@
 
 package kotlin.collections
 
-/**
- * Creates a new empty [ArrayList] with the specified initial capacity.
- *
- * Capacity is the maximum number of elements the list is able to store in current backing storage.
- * When the list gets full and a new element can't be added, its capacity is expanded,
- * which usually leads to creation of a bigger backing storage.
- *
- * @param initialCapacity the initial capacity of the created list.
- *   Note that the argument is just a hint for the implementation and can be ignored.
- *
- * @throws IllegalArgumentException if [initialCapacity] is negative.
- */
 actual class ArrayList<E> actual constructor(initialCapacity: Int) : MutableList<E>, RandomAccess, AbstractMutableList<E>() {
     private var backing = arrayOfUninitializedElements<E>(initialCapacity)
     private var length = 0
