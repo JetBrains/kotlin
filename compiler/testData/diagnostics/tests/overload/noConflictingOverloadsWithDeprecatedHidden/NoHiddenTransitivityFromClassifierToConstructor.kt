d0958068c92ecd2c38d95338fe9deb30fa250487
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-14 06:49:16 +0200

    [tests] Add code examples from exploration of KT-22004

---
+++ compiler/testData/diagnostics/tests/overload/noConflictingOverloadsWithDeprecatedHidden/NoHiddenTransitivityFromClassifierToConstructor.kt
@@ -0,0 +1,19 @@
+// FIR_IDENTICAL
+
+@Deprecated(message = "", level = DeprecationLevel.HIDDEN)
+class <!CONFLICTING_OVERLOADS!>KlassA<!>
+<!CONFLICTING_OVERLOADS!>fun KlassA()<!> {}
+
+@Deprecated(message = "", level = DeprecationLevel.HIDDEN)
+class KlassB<!CONFLICTING_OVERLOADS!>()<!>
+<!CONFLICTING_OVERLOADS!>fun KlassB()<!> {}
+
+@Deprecated(message = "", level = DeprecationLevel.HIDDEN)
+class KlassC <!CONFLICTING_OVERLOADS!>constructor()<!>
+<!CONFLICTING_OVERLOADS!>fun KlassC()<!> {}
+
+@Deprecated(message = "", level = DeprecationLevel.HIDDEN)
+class KlassD {
+    <!CONFLICTING_OVERLOADS!>constructor()<!>
+}
+<!CONFLICTING_OVERLOADS!>fun KlassD()<!> {}
