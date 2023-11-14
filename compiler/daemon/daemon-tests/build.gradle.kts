b8d1398bdb3fecf3a169b5c4a3220a338981c80f
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2023-11-14 16:20:13 +0100

    [Daemon] Use JUnit Vintage engine to run JUnit4 tests

--- compiler/daemon/daemon-tests/build.gradle.kts
+++ compiler/daemon/daemon-tests/build.gradle.kts
@@ -11,8 +11,11 @@ dependencies {
     testImplementation(project(":kotlin-daemon"))
     testImplementation(project(":kotlin-daemon-client"))
     testImplementation(libs.junit4)
+    testImplementation(libs.junit.jupiter.api)
     testImplementation(projectTests(":compiler:tests-common"))
     testImplementation(intellijCore())
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.vintage.engine)
 }
 
 sourceSets {
@@ -20,9 +23,11 @@ sourceSets {
     "test" { projectDefault() }
 }
 
-projectTest(parallel = true) {
+projectTest(parallel = true, jUnitMode = JUnitMode.JUnit5) {
     dependsOn(":dist")
     workingDir = rootDir
+
+    useJUnitPlatform()
 
     val testClassesDirs = testSourceSet.output.classesDirs
     doFirst {
