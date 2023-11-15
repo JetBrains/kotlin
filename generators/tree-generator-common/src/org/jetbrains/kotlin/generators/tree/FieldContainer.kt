bf1f6de83fd691c0ef90162978d6dca4b8f57ec4
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2023-11-15 14:44:30 +0100

    Fix stackoverflow

--- generators/tree-generator-common/src/org/jetbrains/kotlin/generators/tree/FieldContainer.kt
+++ generators/tree-generator-common/src/org/jetbrains/kotlin/generators/tree/FieldContainer.kt
@@ -5,7 +5,7 @@
 
 package org.jetbrains.kotlin.generators.tree
 
-interface FieldContainer<out Field : AbstractField<Field>> {
+interface FieldContainer<out Field : AbstractField<*>> {
     val allFields: List<Field>
     operator fun get(fieldName: String): Field?
 
