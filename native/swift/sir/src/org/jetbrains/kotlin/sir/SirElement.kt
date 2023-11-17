3f10d93a81650af8342dc3b6c2f5af60953a7e93
Artem Olkov <artem.olkov@jetbrains.com>
2023-11-17 10:41:09 +0100

    KT-63270: add SwiftPrinter capable of printing functions

--- native/swift/sir/src/org/jetbrains/kotlin/sir/SirElement.kt
+++ native/swift/sir/src/org/jetbrains/kotlin/sir/SirElement.kt
@@ -9,13 +9,61 @@ package org.jetbrains.kotlin.sir
  * A marker interface that denotes Swift IR elements.
  */
 interface SirElement {
+    fun <Data, Result> accept(visitor: SirVisitor<Data, Result>, data: Data): Result
 }
 
-class SirModule : SirElement {
-    val declarations: MutableList<SirDeclaration> =
-        mutableListOf()
+interface SirDeclaration : SirElement
+
+interface SirVisitor<Data, Result> {
+    fun acceptModule(module: SirModule, data: Data): Result
+    fun acceptForeignFunction(function: SirForeignFunction, data: Data): Result
+    fun acceptSwiftFunction(function: SirFunction, data: Data): Result
+    fun acceptType(type: SirType, data: Data): Result
+    fun acceptParameter(param: SirParameter, data: Data): Result
 }
 
-interface SirDeclaration
+data class SirModule(
+    val declarations: MutableList<SirDeclaration> = mutableListOf()
+) : SirElement {
+    override fun <Data, Result> accept(
+        visitor: SirVisitor<Data, Result>,
+        data: Data
+    ): Result = visitor.acceptModule(this, data)
+}
 
-class SirForeignFunction(val fqName: List<String>) : SirDeclaration
+data class SirType(val name: String) : SirDeclaration {
+    override fun <Data, Result> accept(
+        visitor: SirVisitor<Data, Result>,
+        data: Data
+    ): Result = visitor.acceptType(this, data)
+
+}
+data class SirForeignFunction(
+    val fqName: List<String>
+) : SirDeclaration {
+    override fun <Data, Result> accept(
+        visitor: SirVisitor<Data, Result>,
+        data: Data
+    ): Result = visitor.acceptForeignFunction(this, data)
+}
+
+data class SirFunction(
+    val name: String,
+    val arguments: MutableList<SirParameter> = mutableListOf(),
+    val returnType: SirType
+) : SirDeclaration {
+    override fun <Data, Result> accept(
+        visitor: SirVisitor<Data, Result>,
+        data: Data
+    ): Result = visitor.acceptSwiftFunction(this, data)
+}
+
+data class SirParameter(
+    val name: String,
+    val type: SirType
+) : SirElement {
+    override fun <Data, Result> accept(
+        visitor: SirVisitor<Data, Result>,
+        data: Data
+    ): Result = visitor.acceptParameter(this, data)
+}
