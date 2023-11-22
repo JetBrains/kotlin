be4bc81b1b07aa91912ee6e874ef8658332a3ce8
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2023-11-22 17:46:36 +0100

    [FIR] FirFileSymbol: add stable toString
    It will be used in tests in the next commit
    
    ^KT-63042

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/symbols/impl/FirFileSymbol.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/symbols/impl/FirFileSymbol.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,4 +8,6 @@ package org.jetbrains.kotlin.fir.symbols.impl
 import org.jetbrains.kotlin.fir.declarations.FirFile
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 
-class FirFileSymbol : FirBasedSymbol<FirFile>()
+class FirFileSymbol : FirBasedSymbol<FirFile>() {
+    override fun toString(): String = "${this::class.simpleName} ${fir.name}"
+}
