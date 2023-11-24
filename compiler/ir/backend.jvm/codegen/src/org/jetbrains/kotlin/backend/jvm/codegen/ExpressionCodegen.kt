a611cc874699cd785993a0e89cc35d44f3792c5f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-11-24 12:50:17 +0100

    [JVM] Simplify SMAP generation for IR inliner mode

--- compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
+++ compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/ExpressionCodegen.kt
@@ -487,7 +487,7 @@ class ExpressionCodegen(
             inlineCall.markLineNumber(startOffset = true)
             mv.nop()
 
-            lineNumberMapper.buildSmapFor(inlinedBlock, inlinedBlock.buildOrGetClassSMAP(info), info)
+            lineNumberMapper.buildSmapFor(inlinedBlock)
 
             if (inlineCall.usesDefaultArguments()) {
                 // $default function has first LN pointing to original callee
@@ -537,41 +537,6 @@ class ExpressionCodegen(
                 lineNumberMapper.afterIrInline(inlinedBlock)
             }
         }
-    }
-
-    private fun IrDeclaration.getClassWithDeclaredFunction(): IrClass? {
-        val parent = this.parentClassOrNull ?: return null
-        if (!parent.isInterface || (this is IrFunction && this.hasJvmDefault())) return parent
-        return parent.declarations.singleOrNull { it.origin == JvmLoweredDeclarationOrigin.DEFAULT_IMPLS } as IrClass
-    }
-
-    private fun IrInlinedFunctionBlock.buildOrGetClassSMAP(data: BlockInfo): SMAP {
-        if (this.isLambdaInlining()) {
-            return context.typeToCachedSMAP[(this.inlinedElement as IrAttributeContainer).localClassType]!!
-        }
-
-        val callee = this.inlineDeclaration
-        val calleeFromActualClass = callee.getClassWithDeclaredFunction()!!.declarations
-            .asSequence()
-            .filterIsInstance<IrSimpleFunction>()
-            .filter { it.attributeOwnerId == callee } // original callee could be transformed after lowerings, so we must get correct one
-            .filter {
-                if (inlineCall.usesDefaultArguments()) it.origin == IrDeclarationOrigin.FUNCTION_FOR_DEFAULT_PARAMETER
-                else it.origin != IrDeclarationOrigin.FUNCTION_FOR_DEFAULT_PARAMETER
-            }
-            .filter { it.origin != JvmLoweredDeclarationOrigin.FOR_INLINE_STATE_MACHINE_TEMPLATE } // filter functions with $$forInline postfix
-            .single()
-
-        val nodeAndSmap = calleeFromActualClass.let { actualCallee ->
-            val callToActualCallee = IrCallImpl.fromSymbolOwner(
-                inlineCall.startOffset, inlineCall.endOffset, inlineCall.type, actualCallee.symbol
-            )
-            val callable = methodSignatureMapper.mapToCallableMethod(callToActualCallee, null)
-            val callGenerator = getOrCreateCallGenerator(callToActualCallee, data, callable.signature)
-            (callGenerator as IrInlineCodegen).compileInline()
-        }
-
-        return nodeAndSmap.classSMAP
     }
 
     private fun visitStatementContainer(container: IrStatementContainer, data: BlockInfo): PromisedValue {
--- compiler/testData/codegen/boxInline/nonLocalReturns/tryFinally/chained/nestedLambda.kt
+++ compiler/testData/codegen/boxInline/nonLocalReturns/tryFinally/chained/nestedLambda.kt
@@ -1,4 +1,4 @@
-// IGNORE_INLINER: IR
+
 
 // FILE: 1.kt
 
