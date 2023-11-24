25c22a7ed1d7d457f984ee869611180c5a68b4bb
Oleg Yukhnevich <whyoleg@gmail.com>
2023-11-24 11:52:22 +0200

    [KDM] Add `All Types` page generation
    See https://github.com/Kotlin/dokka/pull/3267
    
    * Hide All Types page under a system property
    * Show documentation if it exists selecting most relevant
    * Take the minimum SinceKotlin version if they differ
    * Extract internal configuration properties to one place
    Original commit: https://github.com/Kotlin/dokka/commit/17ad866ff2c85cdebc87fbca648432dd758b1ee1

---
+++ libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/plugin-base/src/main/kotlin/org/jetbrains/dokka/base/DokkaBaseInternalConfiguration.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package org.jetbrains.dokka.base
+
+// revisit in scope of https://github.com/Kotlin/dokka/issues/2776
+internal object DokkaBaseInternalConfiguration {
+    const val SHOULD_DISPLAY_ALL_TYPES_PAGE_SYS_PROP = "dokka.shouldDisplayAllTypesPage"
+    const val SHOULD_DISPLAY_SINCE_KOTLIN_SYS_PROP = "dokka.shouldDisplaySinceKotlin"
+
+    var allTypesPageEnabled: Boolean = false
+        private set
+    var sinceKotlinRenderingEnabled: Boolean = false
+        private set
+
+    init {
+        reinitialize()
+    }
+
+    // should be private, internal is only for usage in tests
+    internal fun reinitialize() {
+        allTypesPageEnabled = getBooleanProperty(SHOULD_DISPLAY_ALL_TYPES_PAGE_SYS_PROP)
+        sinceKotlinRenderingEnabled = getBooleanProperty(SHOULD_DISPLAY_SINCE_KOTLIN_SYS_PROP)
+    }
+
+    private fun getBooleanProperty(propertyName: String): Boolean {
+        return System.getProperty(propertyName) in setOf("1", "true")
+    }
+}
