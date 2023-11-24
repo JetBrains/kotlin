a57f9c46c5ad7d5ab32c64f10c322c5273ac6a8e
Gleb Lukianets <gleb.lukianets@jetbrains.com>
2023-11-24 16:15:09 +0100

    Regen + copy builders

---
+++ native/swift/sir/gen/org/jetbrains/kotlin/sir/builder/SirFunctionBuilder.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+// This file was generated automatically. See native/swift/sir/tree-generator/Readme.md.
+// DO NOT MODIFY IT MANUALLY.
+
+@file:Suppress("DuplicatedCode", "unused")
+
+package org.jetbrains.kotlin.sir.builder
+
+import kotlin.contracts.*
+import org.jetbrains.kotlin.sir.*
+import org.jetbrains.kotlin.sir.impl.SirFunctionImpl
+
+@SirBuilderDsl
+class SirFunctionBuilder {
+    var origin: SirOrigin = SirOrigin.Unknown
+    var visibility: SirVisibility = SirVisibility.PUBLIC
+    lateinit var name: String
+    val parameters: MutableList<SirParameter> = mutableListOf()
+    lateinit var returnType: SirType
+
+    fun build(): SirFunction {
+        return SirFunctionImpl(
+            origin,
+            visibility,
+            name,
+            parameters,
+            returnType,
+        )
+    }
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun buildFunction(init: SirFunctionBuilder.() -> Unit): SirFunction {
+    contract {
+        callsInPlace(init, InvocationKind.EXACTLY_ONCE)
+    }
+    return SirFunctionBuilder().apply(init).build()
+}
+
+@OptIn(ExperimentalContracts::class)
+inline fun buildFunctionCopy(original: SirFunction, init: SirFunctionBuilder.() -> Unit): SirFunction {
+    contract {
+        callsInPlace(init, InvocationKind.EXACTLY_ONCE)
+    }
+    val copyBuilder = SirFunctionBuilder()
+    copyBuilder.origin = original.origin
+    copyBuilder.visibility = original.visibility
+    copyBuilder.name = original.name
+    copyBuilder.parameters.addAll(original.parameters)
+    copyBuilder.returnType = original.returnType
+    return copyBuilder.apply(init).build()
+}
--- native/swift/sir/tree-generator/src/org/jetbrains/kotlin/sir/tree/generator/BuilderConfigurator.kt
+++ native/swift/sir/tree-generator/src/org/jetbrains/kotlin/sir/tree/generator/BuilderConfigurator.kt
@@ -5,8 +5,10 @@
 
 package org.jetbrains.kotlin.sir.tree.generator
 
+import org.jetbrains.kotlin.generators.tree.*
 import org.jetbrains.kotlin.sir.tree.generator.config.AbstractSwiftIrTreeBuilderConfigurator
 import org.jetbrains.kotlin.sir.tree.generator.model.Element
+import org.jetbrains.kotlin.sir.tree.generator.model.Implementation
 
 class BuilderConfigurator(elements: List<Element>) : AbstractSwiftIrTreeBuilderConfigurator(elements) {
 
@@ -21,5 +23,23 @@ class BuilderConfigurator(elements: List<Element>) : AbstractSwiftIrTreeBuilderC
         configureFieldInAllLeafBuilders("visibility") {
             default(it, "SirVisibility.PUBLIC")
         }
+
+        configureAllLeafBuilders {
+            withCopy()
+        }
     }
 }
+
+//AbstractBuilderConfigurator<Element, Implementation, Field, Field>
+private fun <Element, Implementation, BuilderField, ElementField> AbstractBuilderConfigurator<Element, Implementation, BuilderField, ElementField>.configureAllLeafBuilders(
+    builderPredicate: (LeafBuilder<BuilderField, Element, Implementation>) -> Boolean = { true },
+    init: AbstractBuilderConfigurator<Element, Implementation, BuilderField, ElementField>.LeafBuilderConfigurationContext.() -> Unit,
+) where Element : AbstractElement<Element, ElementField, Implementation>,
+        Implementation : AbstractImplementation<Implementation, Element, BuilderField>,
+        BuilderField : AbstractField<*>,
+        BuilderField : AbstractFieldWithDefaultValue<*>,
+        ElementField : AbstractField<ElementField> = elements
+    .flatMap { it.allImplementations }
+    .mapNotNull { it.builder }
+    .filter(builderPredicate)
+    .forEach { LeafBuilderConfigurationContext(it).init() }
