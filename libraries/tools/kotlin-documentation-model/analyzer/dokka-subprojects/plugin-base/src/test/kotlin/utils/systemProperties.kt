fed9bccebe9341c162a3be76487f6e49936afe33
Oleg Yukhnevich <whyoleg@gmail.com>
2023-11-24 11:52:22 +0200

    [KDM] Add `All Types` page generation
    See https://github.com/Kotlin/dokka/pull/3267
    
    * Hide All Types page under a system property
    * Show documentation if it exists selecting most relevant
    * Take the minimum SinceKotlin version if they differ
    * Extract internal configuration properties to one place
    Original commit: https://github.com/Kotlin/dokka/commit/17ad866ff2c85cdebc87fbca648432dd758b1ee1

---
+++ libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/plugin-base/src/test/kotlin/utils/systemProperties.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package utils
+
+import org.jetbrains.dokka.base.DokkaBaseInternalConfiguration
+import org.jetbrains.dokka.base.DokkaBaseInternalConfiguration.SHOULD_DISPLAY_ALL_TYPES_PAGE_SYS_PROP
+import org.jetbrains.dokka.base.DokkaBaseInternalConfiguration.SHOULD_DISPLAY_SINCE_KOTLIN_SYS_PROP
+
+internal fun withAllTypesPage(block: () -> Unit): Unit =
+    DokkaBaseInternalConfiguration.withProperty(SHOULD_DISPLAY_ALL_TYPES_PAGE_SYS_PROP, "true", block)
+
+internal fun withSinceKotlin(block: () -> Unit): Unit =
+    DokkaBaseInternalConfiguration.withProperty(SHOULD_DISPLAY_SINCE_KOTLIN_SYS_PROP, "true", block)
+
+internal fun DokkaBaseInternalConfiguration.withProperty(propertyName: String, value: String, block: () -> Unit) {
+    setProperty(propertyName, value)
+    try {
+        block()
+    } finally {
+        clearProperty(propertyName)
+    }
+}
+
+internal fun DokkaBaseInternalConfiguration.setProperty(propertyName: String, value: String) {
+    System.setProperty(propertyName, value)
+    reinitialize()
+}
+
+internal fun DokkaBaseInternalConfiguration.clearProperty(propertyName: String) {
+    System.clearProperty(propertyName)
+    reinitialize()
+}
