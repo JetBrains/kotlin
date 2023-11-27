66e1e2ce4fc871cfbff7904acebda3405cff475e
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-11-27 16:31:09 +0100

    ~ refine postponed receiver definition

--- native/native.tests/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestNoPLGenerated.java
+++ native/native.tests/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestNoPLGenerated.java
@@ -1966,6 +1966,12 @@ public class FirNativeCodegenBoxTestNoPLGenerated extends AbstractNativeCodegenB
                 KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/builderInference"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.NATIVE, true);
             }
 
+            @Test
+            @TestMetadata("receiverUsesOuterTVButReturnTypeIsProper.kt")
+            public void testReceiverUsesOuterTVButReturnTypeIsProper() throws Exception {
+                runTest("compiler/testData/codegen/box/builderInference/receiverUsesOuterTVButReturnTypeIsProper.kt");
+            }
+
             @Nested
             @TestMetadata("compiler/testData/codegen/box/builderInference/issues")
             @TestDataPath("$PROJECT_ROOT")
