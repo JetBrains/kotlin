f24380f915141da9ed4c67e8b25f12bd4ac91f16
Andrei Shikov <ashikov@google.com>
2023-11-28 17:31:46 +0000

    Fix memoization of adapted function references and inline arguments
    Adapted function references (function references with inexact signature match, e.g. w/ default parameters) generate a block around function reference with adapter lambda. The later Kotlin lowerings expect this IR shape to be preserved, so Compose needs to account for it and wrap whole block instead.
    
    This change also fixes memoization of function references for inline function parameters, making sure we correctly inline function references.
    
    Fixes: 312738720 ( https://issuetracker.google.com/issues/312738720 )
    Test: Updated compiler test
    Change-Id: If4858efc32854c03393e00d71f8248037aab26d4 ( https://android-review.googlesource.com/q/If4858efc32854c03393e00d71f8248037aab26d4 )
    
    Moved from: https://github.com/androidx/androidx/commit/2c45d16b1455ab3c5f05ea3914baf37b158807ea

--- plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/LambdaMemoizationTransformTests.kt
+++ plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/LambdaMemoizationTransformTests.kt
@@ -652,4 +652,28 @@ class LambdaMemoizationTransformTests(useFir: Boolean) : AbstractIrTransformTest
             }
         """
     )
+
+    @Test
+    fun testAdaptedFunctionRef() = verifyGoldenComposeIrTransform(
+        """
+            import androidx.compose.runtime.Composable
+
+            class ScrollState {
+                fun test(index: Int, default: Int = 0): Int = 0
+                fun testExact(index: Int): Int = 0
+            }
+            fun scrollState(): ScrollState = TODO()
+
+            @Composable fun rememberFooInline() = fooInline(scrollState()::test)
+            @Composable fun rememberFoo() = foo(scrollState()::test)
+            @Composable fun rememberFooExactInline() = fooInline(scrollState()::testExact)
+            @Composable fun rememberFooExact() = foo(scrollState()::testExact)
+
+            @Composable
+            inline fun fooInline(block: (Int) -> Int) = block(0)
+
+            @Composable
+            fun foo(block: (Int) -> Int) = block(0)
+        """,
+    )
 }
