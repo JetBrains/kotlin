a0d4dd7c54467f6a18dbcc45b23542de8084eb3f
Jim S <jsproch@google.com>
2023-11-28 10:45:46 -0800

    Rebase androidx to Kotlin 1.9.21
    Change-Id: I1c91989c32b4f85ee8af3c7ece59019b83937e64 ( https://android-review.git.corp.google.com/q/I1c91989c32b4f85ee8af3c7ece59019b83937e64 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposePlugin.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposePlugin.kt
@@ -266,7 +266,7 @@ class ComposePluginRegistrar : org.jetbrains.kotlin.compiler.plugin.ComponentReg
     companion object {
         fun checkCompilerVersion(configuration: CompilerConfiguration): Boolean {
             try {
-                val KOTLIN_VERSION_EXPECTATION = "1.9.20"
+                val KOTLIN_VERSION_EXPECTATION = "1.9.21"
                 KotlinCompilerVersion.getVersion()?.let { version ->
                     val msgCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY)
                     val suppressKotlinVersionCheck = configuration.get(
