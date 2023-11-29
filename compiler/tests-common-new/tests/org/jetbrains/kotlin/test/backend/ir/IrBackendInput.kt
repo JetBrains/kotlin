59e0971afdfac09b4a7603e7e14f9788f3e036c5
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-11-29 16:38:58 +0100

    [JVM_IR] Don't use `irPluginContext` at all when compiling code fragment
    This is an extended version of the fix in
    57d912a5d4ddc66e7827f9e0d15910351b155c41. We don't want to use any
    plugins in code fragment compilation, but we still get them through
    `project` because they are registered in IDEA. So we just fully
    exclude `irPluginContext`.
    
    #KT-63695 Fixed

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/ir/IrBackendInput.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/ir/IrBackendInput.kt
@@ -147,7 +147,7 @@ sealed class IrBackendInput : ResultingArtifact.BackendInput<IrBackendInput>() {
             get() = backendInput.irModuleFragment
 
         override val irPluginContext: IrPluginContext
-            get() = backendInput.pluginContext
+            get() = backendInput.pluginContext!!
 
         override val diagnosticReporter: BaseDiagnosticsCollector
             get() = state.diagnosticReporter as BaseDiagnosticsCollector
