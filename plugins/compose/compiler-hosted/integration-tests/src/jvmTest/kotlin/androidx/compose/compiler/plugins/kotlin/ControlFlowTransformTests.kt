e1cc53d9ecd599e00f57d1b0f4f711337f6b053f
Andrei Shikov <ashikov@google.com>
2023-11-29 20:24:30 +0000

    Realize groups when exiting inline function call
    Disables coalescing groups inside inlined function body, as the inlined function might be called multiple times (e.g. forEach).
    
    Test: updated compiler and runtime tests
    Fixes: 312242547 ( https://issuetracker.google.com/issues/312242547 )
    
    Change-Id: I8a3906eadb951657c350e5ecf5b7597c4611e881 ( https://android-review.git.corp.google.com/q/I8a3906eadb951657c350e5ecf5b7597c4611e881 )

--- plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/ControlFlowTransformTests.kt
+++ plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/ControlFlowTransformTests.kt
@@ -2376,4 +2376,37 @@ class ControlFlowTransformTests(useFir: Boolean) : AbstractControlFlowTransformT
             }
         """.trimIndent()
     )
+
+    @Test
+    fun testGroupsInLoops() = verifyGoldenComposeIrTransform(
+        """
+            import androidx.compose.runtime.*
+
+            @Composable
+            private fun KeyContent1(items: List<Int>) {
+                items.forEach { item ->
+                    if (item > -1) {
+                        key(item) {
+                            remember {
+                                item
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Composable
+            private fun KeyContent2(items: List<Int>) {
+                for (item in items) {
+                    if (item > -1) {
+                        key(item) {
+                            remember {
+                                item
+                            }
+                        }
+                    }
+                }
+            }
+        """
+    )
 }
