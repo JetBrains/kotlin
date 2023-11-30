f3cc0f75656b35a53ad3baaa73d98d26a6006599
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-30 22:04:11 +0100

    [tests] Add test data for KT-52838

---
+++ compiler/testData/codegen/box/builderInference/issues/kt52838b.kt
@@ -0,0 +1,28 @@
+// ISSUE: KT-52838
+
+// IGNORE_LIGHT_ANALYSIS
+// IGNORE_BACKEND: ANY
+// REASON: red code (see corresponding diagnostic test)
+
+fun box(): String {
+    build {
+        this as DerivedBuildee<*>
+        getTypeVariable()
+        getTypeVariable()
+    }
+    return "OK"
+}
+
+
+
+
+open class Buildee<TV> {
+    fun getTypeVariable(): TV = storage
+    private var storage: TV = Any() as TV
+}
+
+class DerivedBuildee<TA>: Buildee<TA>()
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return DerivedBuildee<PTV>().apply(instructions)
+}
