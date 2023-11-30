f6a3f999c375c7a6b9cde36610bc2eabd9e9f145
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-30 22:03:57 +0100

    [tests] Add test data for KT-52757

---
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt52757.kt
@@ -0,0 +1,29 @@
+// ISSUE: KT-52757
+// FIR_IDENTICAL
+// CHECK_TYPE_WITH_EXACT
+
+fun test() {
+    val buildee = build {
+        typeVariableConsumer = ::consumeTargetType
+        typeVariableConsumer = {}
+    }
+    // exact type equality check — turns unexpected compile-time behavior into red code
+    // considered to be non-user-reproducible code for the purposes of these tests
+    checkExactType<Buildee<TargetType>>(buildee)
+}
+
+
+
+
+class TargetType
+
+fun consumeTargetType(value: TargetType) {}
+
+class Buildee<TV> {
+    var typeVariableConsumer: (TV) -> Unit = { storage = it }
+    private var storage: TV = null!!
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
