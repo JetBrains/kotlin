2a257091fbf537f544e70d2053b05bd3432a9f88
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-30 22:03:02 +0100

    [tests] Add test data for KT-49160

---
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt49160a.fir.kt
@@ -0,0 +1,35 @@
+// ISSUE: KT-49160
+// CHECK_TYPE_WITH_EXACT
+
+fun test() {
+    val outerBuildee = <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>build<!> outerBuild@ {
+        class LocalClass {
+            fun localClassMember() {
+                val innerBuildee = build innerBuild@ {
+                    this@outerBuild.setTypeVariable(TargetType())
+                    this@innerBuild.setTypeVariable(TargetType())
+                }
+                // exact type equality check — turns unexpected compile-time behavior into red code
+                // considered to be non-user-reproducible code for the purposes of these tests
+                checkExactType<Buildee<TargetType>>(innerBuildee)
+            }
+        }
+    }
+    // exact type equality check — turns unexpected compile-time behavior into red code
+    // considered to be non-user-reproducible code for the purposes of these tests
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>checkExactType<!><<!CANNOT_INFER_PARAMETER_TYPE!>Buildee<TargetType><!>>(outerBuildee)
+}
+
+
+
+
+class TargetType
+
+class Buildee<TV> {
+    fun setTypeVariable(value: TV) { storage = value }
+    private var storage: TV = null!!
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
---
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt49160c.kt
@@ -0,0 +1,33 @@
+// ISSUE: KT-49160
+// CHECK_TYPE_WITH_EXACT
+
+fun test() {
+    val outerBuildee = <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>build<!> outerBuild@ {
+        fun localFunction() {
+            val innerBuildee = build innerBuild@ {
+                this@outerBuild.setTypeVariable(TargetType())
+                this@innerBuild.setTypeVariable(TargetType())
+            }
+            // exact type equality check — turns unexpected compile-time behavior into red code
+            // considered to be non-user-reproducible code for the purposes of these tests
+            checkExactType<Buildee<TargetType>>(innerBuildee)
+        }
+    }
+    // exact type equality check — turns unexpected compile-time behavior into red code
+    // considered to be non-user-reproducible code for the purposes of these tests
+    checkExactType<Buildee<TargetType>>(<!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>outerBuildee<!>)
+}
+
+
+
+
+class TargetType
+
+class Buildee<TV> {
+    fun setTypeVariable(value: TV) { storage = value }
+    private var storage: TV = null!!
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
