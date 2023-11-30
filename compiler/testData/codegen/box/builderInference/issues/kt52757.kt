8b702df14c7e43d80c6c34b3ae9db42973f878de
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-30 22:03:57 +0100

    [tests] Add test data for KT-52757

---
+++ compiler/testData/codegen/box/builderInference/issues/kt52757.kt
@@ -0,0 +1,30 @@
+// ISSUE: KT-52757
+
+// IGNORE_LIGHT_ANALYSIS
+// IGNORE_BACKEND_K1: ANY
+// REASON: compile-time failure in K1/JVM (java.lang.AssertionError: Could not load module <Error module> @ org.jetbrains.kotlin.backend.common.serialization.KotlinIrLinker.resolveModuleDeserializer)
+// REASON: compile-time failure in K1/Native, K1/WASM, K1/JS (java.lang.NullPointerException @ org.jetbrains.kotlin.backend.common.serialization.mangle.descriptor.DescriptorExportCheckerVisitor.isExported)
+
+fun box(): String {
+    build {
+        typeVariableConsumer = ::consumeTargetType
+        typeVariableConsumer = {}
+    }
+    return "OK"
+}
+
+
+
+
+class TargetType
+
+fun consumeTargetType(value: TargetType) {}
+
+class Buildee<TV> {
+    var typeVariableConsumer: (TV) -> Unit = { storage = it }
+    private var storage: TV = TargetType() as TV
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
