50e58c00ea6892ebfdd041d0b471ac6fed23768a
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-11-30 22:03:44 +0100

    [tests] Add test data for KT-50827

---
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt50827.kt
@@ -0,0 +1,30 @@
+// ISSUE: KT-50827
+// CHECK_TYPE_WITH_EXACT
+
+fun test() {
+    val box = ClassWithBoundedTypeParameter(
+        build {
+            setTypeVariable(TargetType())
+        }
+    )
+    val buildee = box.buildee
+    // exact type equality check — turns unexpected compile-time behavior into red code
+    // considered to be non-user-reproducible code for the purposes of these tests
+    checkExactType<Buildee<TargetType>>(<!TYPE_MISMATCH("Buildee<TargetType>; Buildee<Any>"), TYPE_MISMATCH("Buildee<Any>; Buildee<TargetType>")!>buildee<!>)
+}
+
+
+
+
+class TargetType
+
+class ClassWithBoundedTypeParameter<T: Any>(val buildee: Buildee<T>)
+
+class Buildee<TV> {
+    fun setTypeVariable(value: TV) { storage = value }
+    private var storage: TV = null!!
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
