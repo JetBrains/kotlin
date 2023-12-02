205d56854533b3c8823b6f66763ce4bcf03a519c
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-12-02 15:25:37 +0100

    [tests] Add test data for KT-54400

---
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt54400a.kt
@@ -0,0 +1,25 @@
+// ISSUE: KT-54400
+// CHECK_TYPE_WITH_EXACT
+
+fun test() {
+    val buildee = <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>build<!> {
+        typeVariableMutableProperty = <!TYPE_MISMATCH("TypeVariable(PTV); String")!>""<!>
+    }
+    // exact type equality check — turns unexpected compile-time behavior into red code
+    // considered to be non-user-reproducible code for the purposes of these tests
+    checkExactType<Buildee<String>>(<!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE!>buildee<!>)
+}
+
+
+
+
+class Buildee<TV> {
+    var typeVariableMutableProperty: TV
+        get() = storage
+        set(value) { storage = value }
+    private var storage: TV = null!!
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
