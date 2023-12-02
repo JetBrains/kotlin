4671a9612af5c5fdadbdffb89c2393f2c32059c2
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2023-12-02 15:28:59 +0100

    [tests] Add test data for KT-61310

---
+++ compiler/testData/codegen/box/builderInference/issues/kt61310.kt
@@ -0,0 +1,31 @@
+// ISSUE: KT-61310
+
+// IGNORE_LIGHT_ANALYSIS
+// IGNORE_BACKEND_K2: ANY
+// REASON: red code (see corresponding diagnostic test)
+
+fun box(): String {
+    build {
+        if (true) {
+            setTypeVariable(GenericBox())
+        } else {
+            setTypeVariable(GenericBox<TargetType>())
+        }
+    }
+    return "OK"
+}
+
+
+
+
+class TargetType
+class GenericBox<T>
+
+class Buildee<TV> {
+    fun setTypeVariable(value: TV) { storage = value }
+    private var storage: TV = GenericBox<TargetType>() as TV
+}
+
+fun <PTV> build(instructions: Buildee<PTV>.() -> Unit): Buildee<PTV> {
+    return Buildee<PTV>().apply(instructions)
+}
