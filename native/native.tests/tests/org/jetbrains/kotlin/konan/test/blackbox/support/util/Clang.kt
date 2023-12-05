13618fc03b232fe48a94b1765ba8e699f7421469
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2023-12-05 10:21:17 +0200

    [Native][tests] Extract Clang invocation to a separate function

---
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/util/Clang.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.konan.test.blackbox.support.util
+
+import org.jetbrains.kotlin.konan.test.blackbox.AbstractNativeSimpleTest
+import org.jetbrains.kotlin.konan.test.blackbox.support.settings.configurables
+import java.io.File
+
+// FIXME: absoluteTargetToolchain might not work correctly with KONAN_USE_INTERNAL_SERVER because
+// :kotlin-native:dependencies:update is not a dependency of :native:native.tests:test where this test runs
+internal fun AbstractNativeSimpleTest.compileWithClang(
+    sourceFiles: List<File>,
+    outputFile: File,
+    frameworkDirectories: List<File>
+) {
+    val process = ProcessBuilder(
+        "${testRunSettings.configurables.absoluteTargetToolchain}/bin/clang",
+        *sourceFiles.map { it.absolutePath }.toTypedArray(),
+        "-isysroot", testRunSettings.configurables.absoluteTargetSysRoot,
+        "-target", testRunSettings.configurables.targetTriple.toString(),
+        "-g", "-fmodules",
+        *frameworkDirectories.flatMap { listOf("-F", it.absolutePath) }.toTypedArray(),
+        "-o", outputFile.absolutePath
+    ).redirectErrorStream(true).start()
+    val clangOutput = process.inputStream.readBytes()
+
+    check(
+        process.waitFor() == 0
+    ) { clangOutput.decodeToString() }
+}
