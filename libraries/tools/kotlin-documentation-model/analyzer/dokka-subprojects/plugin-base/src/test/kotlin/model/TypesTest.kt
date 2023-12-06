dbf2081269921ad2aeb61b95d46b99242d4ee853
Vadim Mishenev <vad-mishenev@yandex.ru>
2023-12-06 23:48:36 +0300

    [KDM] [K2] Fix typealiased type that leads to functional type
    See https://github.com/Kotlin/dokka/pull/3396
    
    * [K2] Fix typealiased type to function
    
    * Add check on emptiness
    Original commit: https://github.com/Kotlin/dokka/commit/3d8be6c8b01a7f4032d814ab362593b4c38eb1ad

---
+++ libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/plugin-base/src/test/kotlin/model/TypesTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package model
+
+import org.jetbrains.dokka.base.signatures.KotlinSignatureUtils.driOrNull
+import org.jetbrains.dokka.links.DRI
+import org.jetbrains.dokka.model.*
+import utils.AbstractModelTest
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class TypesTest : AbstractModelTest("/src/main/kotlin/classes/Test.kt", "types") {
+
+    @Test
+    fun `type with typealias to functional type with parameter`() {
+        inlineModelTest(
+            """
+            |typealias HttpExceptionCallback<T> = String.(T) -> String
+            |fun <T> exception(callback: HttpExceptionCallback<T>){}"""
+        ) {
+            with((this / "types" / "HttpExceptionCallback").cast<DTypeAlias>()) {
+                name equals "HttpExceptionCallback"
+                assertTrue(type is GenericTypeConstructor)
+                (type as GenericTypeConstructor).projections counts 1
+                underlyingType.values.first().driOrNull equals DRI("kotlin", "Function2")
+            }
+            with((this / "types" / "exception").cast<DFunction>()) {
+                name equals "exception"
+                val parameterType = parameters.first().type
+                assertTrue(parameterType is TypeAliased)
+                with(parameterType) {
+                    assertTrue(typeAlias is GenericTypeConstructor)
+                    (typeAlias as GenericTypeConstructor).projections counts 1
+                    assertTrue(inner is FunctionalTypeConstructor)
+                    (inner as FunctionalTypeConstructor).dri equals DRI("kotlin", "Function2")
+                    (inner as FunctionalTypeConstructor).projections counts 3
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `type with typealias to functional type`() {
+        inlineModelTest(
+            """
+            |typealias CompletionHandler = (cause: Throwable?) -> Unit
+            |fun exception(callback: CompletionHandler){}"""
+        ) {
+            with((this / "types" / "CompletionHandler").cast<DTypeAlias>()) {
+                assertTrue(type is GenericTypeConstructor)
+                (type as GenericTypeConstructor).projections counts 0
+                name equals "CompletionHandler"
+                underlyingType.values.first().driOrNull equals DRI("kotlin", "Function1")
+            }
+            with((this / "types" / "exception").cast<DFunction>()) {
+                name equals "exception"
+                val parameterType = parameters.first().type
+                assertTrue(parameterType is TypeAliased)
+                with(parameterType) {
+                    assertTrue(typeAlias is GenericTypeConstructor)
+                    assertTrue(inner is FunctionalTypeConstructor)
+                    (inner as FunctionalTypeConstructor).dri equals DRI("kotlin", "Function1")
+                    (inner as FunctionalTypeConstructor).projections counts 2
+                }
+            }
+        }
+    }
+}
