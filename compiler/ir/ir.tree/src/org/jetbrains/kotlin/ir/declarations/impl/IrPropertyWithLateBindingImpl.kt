9845ca801eefea40e09a82e715aaa03714a8d42f
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2023-12-07 17:59:26 +0100

    [IR] Mark IrPropertyWithLateBindingImpl with an opt-in annotation
    KT-59318

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/impl/IrPropertyWithLateBindingImpl.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/impl/IrPropertyWithLateBindingImpl.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.ir.declarations.impl
 
 import org.jetbrains.kotlin.descriptors.DescriptorVisibility
 import org.jetbrains.kotlin.descriptors.Modality
+import org.jetbrains.kotlin.ir.IrImplementationDetail
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.descriptors.toIrBasedDescriptor
@@ -15,7 +16,7 @@ import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
 
-class IrPropertyWithLateBindingImpl(
+class IrPropertyWithLateBindingImpl @IrImplementationDetail constructor(
     override val startOffset: Int,
     override val endOffset: Int,
     override var origin: IrDeclarationOrigin,
