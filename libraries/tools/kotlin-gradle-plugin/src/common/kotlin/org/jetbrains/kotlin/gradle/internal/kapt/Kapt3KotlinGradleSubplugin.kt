8028e2a42d29834e35479419163efad47571e997
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-12-09 22:13:02 +0100

    [Gradle] Suppress package directory mismatch in Kapt3KotlinGradleSubplugin.kt
    ^KT-56904 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/Kapt3KotlinGradleSubplugin.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/Kapt3KotlinGradleSubplugin.kt
@@ -3,6 +3,8 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("PackageDirectoryMismatch")
+
 package org.jetbrains.kotlin.gradle.internal
 
 import com.android.build.gradle.BaseExtension
