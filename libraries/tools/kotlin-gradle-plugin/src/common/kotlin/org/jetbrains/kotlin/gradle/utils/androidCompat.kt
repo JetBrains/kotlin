56c7309fadcd96d0e5b5f25ac906a3f7b6f7c171
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2023-12-09 22:12:26 +0100

    [Gradle] Suppress Android deprecations in Kapt3KotlinGradleSubplugin.kt
    ^KT-56904 In Progress

---
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/androidCompat.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("DEPRECATION")
+
+package org.jetbrains.kotlin.gradle.utils
+
+internal typealias DeprecatedAndroidSourceSet = com.android.build.gradle.api.AndroidSourceSet
+internal typealias DeprecatedAndroidBaseVariant = com.android.build.gradle.api.BaseVariant
+internal typealias DeprecatedAndroidSourceKind = com.android.build.gradle.api.SourceKind
