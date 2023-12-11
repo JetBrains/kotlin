ee3f8963a6ef410d048d4ff787b0e995aeb76094
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2023-12-11 14:21:02 +0100

    [BIR] Cleanup: remove excessive copyright comments

--- compiler/ir/bir/backend/src/org/jetbrains/kotlin/bir/backend/BirLoweringPhase.kt
+++ compiler/ir/bir/backend/src/org/jetbrains/kotlin/bir/backend/BirLoweringPhase.kt
@@ -3,18 +3,9 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.bir.backend/*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
-
+package org.jetbrains.kotlin.bir.backend
 import org.jetbrains.kotlin.bir.*
 import org.jetbrains.kotlin.bir.declarations.BirModuleFragment
-
-/*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
 
 context(BirBackendContext)
 abstract class BirLoweringPhase {
