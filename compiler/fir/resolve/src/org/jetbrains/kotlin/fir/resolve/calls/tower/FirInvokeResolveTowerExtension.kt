7b0c4e5bf68a3cf1497192e768c6d445996aee10
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2023-12-12 16:48:00 +0100

    [FIR] Adapt assertion after #KT-62959

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/tower/FirInvokeResolveTowerExtension.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/tower/FirInvokeResolveTowerExtension.kt
@@ -20,8 +20,11 @@ import org.jetbrains.kotlin.fir.symbols.impl.*
 import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.coneTypeUnsafe
 import org.jetbrains.kotlin.fir.types.isExtensionFunctionType
+import org.jetbrains.kotlin.fir.types.typeApproximator
 import org.jetbrains.kotlin.resolve.calls.tasks.ExplicitReceiverKind
 import org.jetbrains.kotlin.resolve.calls.tower.CandidateApplicability
+import org.jetbrains.kotlin.types.AbstractTypeChecker
+import org.jetbrains.kotlin.types.TypeApproximatorConfiguration
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
 internal class FirInvokeResolveTowerExtension(
@@ -221,7 +224,16 @@ internal class FirInvokeResolveTowerExtension(
             }
         } else {
             if (useImplicitReceiverAsBuiltinInvokeArgument) {
-                require(explicitReceiver.type.fullyExpandedType(context.session).isExtensionFunctionType)
+                if (AbstractTypeChecker.RUN_SLOW_ASSERTIONS) {
+                    val session = context.session
+                    val fullyExpandedType = explicitReceiver.type.fullyExpandedType(session)
+                    require(
+                        (session.typeApproximator.approximateToSuperType(
+                            fullyExpandedType,
+                            TypeApproximatorConfiguration.FinalApproximationAfterResolutionAndInference
+                        ) ?: fullyExpandedType).isExtensionFunctionType
+                    )
+                }
                 manager.enqueueResolverTask {
                     task.runResolverForBuiltinInvokeExtensionWithImplicitArgument(
                         invokeFunctionInfo, explicitReceiver,
