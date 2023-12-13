1a1b2deda63ebdfaa334dd3257f274971a3ee58e
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-12-13 15:08:19 +0100

    ~ test data green-to-green

--- compiler/testData/diagnostics/tests/builderInference/issues/kt57834.fir.kt
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt57834.fir.kt
@@ -4,7 +4,7 @@
 fun test() {
     val buildee = build {
         setTypeVariable(TargetType())
-        <!OVERLOAD_RESOLUTION_AMBIGUITY!>consume<!>(getTypeVariable())
+        consume(getTypeVariable())
     }
     // exact type equality check — turns unexpected compile-time behavior into red code
     // considered to be non-user-reproducible code for the purposes of these tests
