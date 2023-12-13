2fcf1202a4917c4ea2392a588124e4a31135751e
Artem Olkov <artem.olkov@jetbrains.com>
2023-12-13 15:54:56 +0100

    KT-64160: support runnning swift-export compiler plugin from CLI

--- plugins/swift-export/swift-export.cli/build.gradle.kts
+++ plugins/swift-export/swift-export.cli/build.gradle.kts
@@ -17,6 +17,13 @@ dependencies {
     embedded(project(":analysis:analysis-api-impl-barebone")) { isTransitive = false }
     embedded(project(":analysis:analysis-api-standalone:analysis-api-standalone-base")) { isTransitive = false }
     embedded(project(":analysis:analysis-api-standalone:analysis-api-fir-standalone-base")) { isTransitive = false }
+    embedded(project(":analysis:analysis-api-providers")) { isTransitive = false }
+    embedded(project(":analysis:decompiled:decompiler-to-file-stubs")) { isTransitive = false }
+    embedded(project(":analysis:decompiled:decompiler-to-psi")) { isTransitive = false }
+    embedded(project(":analysis:kt-references")) { isTransitive = false }
+    embedded(project(":analysis:decompiled:decompiler-to-stubs")) { isTransitive = false }
+    embedded(project(":analysis:decompiled:decompiler-to-file-stubs")) { isTransitive = false }
+
     embedded(project(":analysis:analysis-internal-utils")) { isTransitive = false }
     embedded(project(":analysis:low-level-api-fir")) { isTransitive = false }
     embedded(project(":analysis:symbol-light-classes")) { isTransitive = false }
