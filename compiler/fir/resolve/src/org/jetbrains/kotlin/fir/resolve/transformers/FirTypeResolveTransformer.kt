d574d859cbec1567add57c02c7e4fefa47a421be
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2023-12-13 14:15:13 +0100

    [FIR] Clean up unused dummy code

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirTypeResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirTypeResolveTransformer.kt
@@ -573,20 +573,3 @@ open class FirTypeResolveTransformer(
     private fun annotationShouldBeMovedToField(allowedTargets: Set<AnnotationUseSiteTarget>): Boolean =
         (FIELD in allowedTargets || PROPERTY_DELEGATE_FIELD in allowedTargets) && PROPERTY !in allowedTargets
 }
-
-annotation class NoTarget
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-annotation class Param
-
-@Target(AnnotationTarget.PROPERTY)
-annotation class Prop
-
-@Target(AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
-annotation class Both
-
-data class Foo(
-    @NoTarget @Param @Prop @Both val p1: Int,
-    @param:NoTarget @param:Both val p2: String,
-    @property:NoTarget @property:Both val p3: Boolean,
-)
