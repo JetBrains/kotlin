cf57b44a1ea6e5d28f19ee789b0ab3abaa95576c
Anna Kozlova <anna.kozlova@jetbrains.com>
2023-12-13 10:36:58 +0100

    [LC] avoid icon calculation in compiler
    move calculation to the IDE icon provider
    
    ^KT-64282 fixed

--- compiler/psi/src/org/jetbrains/kotlin/idea/KotlinIconProviderService.java
+++ compiler/psi/src/org/jetbrains/kotlin/idea/KotlinIconProviderService.java
@@ -5,19 +5,13 @@
 
 package org.jetbrains.kotlin.idea;
 
-import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.psi.PsiModifierListOwner;
-import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public abstract class KotlinIconProviderService {
     public abstract Icon getFileIcon();
     public abstract Icon getBuiltInFileIcon();
-
-    public abstract Icon getLightVariableIcon(@NotNull PsiModifierListOwner element, int flags);
 
     public static class CompilerKotlinFileIconProviderService extends KotlinIconProviderService {
         @Override
@@ -27,11 +21,6 @@ public abstract class KotlinIconProviderService {
 
         @Override
         public Icon getBuiltInFileIcon() {
-            return null;
-        }
-
-        @Override
-        public Icon getLightVariableIcon(@NotNull PsiModifierListOwner element, int flags) {
             return null;
         }
     }
