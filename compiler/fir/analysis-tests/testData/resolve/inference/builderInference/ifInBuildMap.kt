b9019d3de115006a4cfc0d82327ba0c1f57e7f43
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-12-13 15:08:19 +0100

    K2: Adjust test data after PCLA implementation [red-to-green]
    ^KT-59791 In Progress

--- compiler/fir/analysis-tests/testData/resolve/inference/builderInference/ifInBuildMap.kt
+++ compiler/fir/analysis-tests/testData/resolve/inference/builderInference/ifInBuildMap.kt
@@ -2,11 +2,11 @@
 // ISSUE: KT-51143
 
 fun main() {
-    <!NEW_INFERENCE_ERROR!>buildMap {
+    buildMap {
         if (true) {
             println("test")
         } else {
             put("foo", "bar")
         }
-    }<!>
+    }
 }
