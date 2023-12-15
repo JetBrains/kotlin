a3cd69f19506afd804f1e9259e904904113b6f12
Jim S <jsproch@google.com>
2023-12-15 09:11:47 -0800

    1.5.7 Compose Compiler release for bug fixes.
    Change-Id: I5d80396b21dc00a0ec547a5b013b420a3e95b243 ( https://android-review.git.corp.google.com/q/I5d80396b21dc00a0ec547a5b013b420a3e95b243 )

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -142,7 +142,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.5.6"
+        const val compilerVersion: String = "1.5.7"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
