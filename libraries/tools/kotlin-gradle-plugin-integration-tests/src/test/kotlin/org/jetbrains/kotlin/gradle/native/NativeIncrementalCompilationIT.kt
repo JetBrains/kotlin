9c7aac2ec0b44a6af740032f4df31c5a762b47fb
Igor Chevdar <igor.chevdar@jetbrains.com>
2023-12-18 10:59:32 +0200

    [gradle] Use more fine grained directory for K/N incremental compilation
    Otherwise, two or more link tasks being executed in parallel will use the same directory
    for IC caches which might lead to races during compilation
    
     #KT-63471 Fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeIncrementalCompilationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeIncrementalCompilationIT.kt
@@ -40,9 +40,10 @@ class NativeIncrementalCompilationIT : KGPBaseTest() {
                 "-Xbackend-threads=4"
             )
 
+            val icCacheDir = projectPath.resolve("build").resolve("kotlin-native-ic-cache").resolve("debugExecutable")
             val incrementalCacheArgs = arrayOf(
                 "-Xenable-incremental-compilation",
-                "-Xic-cache-dir=${projectPath.resolve("build").resolve("kotlin-native-ic-cache").toFile().canonicalPath}"
+                "-Xic-cache-dir=${icCacheDir.toFile().canonicalPath}"
             )
 
             val withoutIncrementalCacheBuildOptions = defaultBuildOptions.copy(
