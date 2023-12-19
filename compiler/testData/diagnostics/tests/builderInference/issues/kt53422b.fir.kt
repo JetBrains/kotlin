de9d3009d6d66c7afb0ff1302c4a3f46afa6c739
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-12-19 18:43:54 +0100

    ~ test data on multiLambdaRestriction

--- compiler/testData/diagnostics/tests/builderInference/issues/kt53422b.fir.kt
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt53422b.fir.kt
@@ -3,17 +3,17 @@
 
 fun test() {
     val buildee = stepByStepBuild(
-        <!BUILDER_INFERENCE_MULTI_LAMBDA_RESTRICTION!>{
+        {
             it.<!UNRESOLVED_REFERENCE!>concreteTypeMemberProperty<!>
             TargetType()
-        }<!>,
-        <!BUILDER_INFERENCE_MULTI_LAMBDA_RESTRICTION!>{
+        },
+        {
             consumeTargetTypeBase(it)
-        }<!>
+        }
     )
     // exact type equality check — turns unexpected compile-time behavior into red code
     // considered to be non-user-reproducible code for the purposes of these tests
-    checkExactType<Buildee<TargetType>>(<!ARGUMENT_TYPE_MISMATCH("Buildee<TargetType>; Buildee<TargetTypeBase>")!>buildee<!>)
+    checkExactType<Buildee<TargetType>>(buildee)
 }
 
 
