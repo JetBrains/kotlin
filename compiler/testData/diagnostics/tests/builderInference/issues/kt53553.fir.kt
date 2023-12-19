acf30cbc5666d0af24b8bda23bbd4a797b9fa70c
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-12-19 18:43:54 +0100

    ~ test data on multiLambdaRestriction

--- compiler/testData/diagnostics/tests/builderInference/issues/kt53553.fir.kt
+++ compiler/testData/diagnostics/tests/builderInference/issues/kt53553.fir.kt
@@ -2,17 +2,17 @@
 // CHECK_TYPE_WITH_EXACT
 
 fun test() {
-    val buildee = parallelBuild(
-        <!BUILDER_INFERENCE_MULTI_LAMBDA_RESTRICTION!>{
+    val buildee = <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>parallelBuild<!>(
+        {
             consumeTargetTypeBase(it)
-        }<!>,
-        <!BUILDER_INFERENCE_MULTI_LAMBDA_RESTRICTION!>{
+        },
+        {
             consumeTargetType(it)
-        }<!>
+        }
     )
     // exact type equality check — turns unexpected compile-time behavior into red code
     // considered to be non-user-reproducible code for the purposes of these tests
-    checkExactType<Buildee<TargetType>>(<!ARGUMENT_TYPE_MISMATCH("Buildee<TargetType>; Buildee<TargetTypeBase>")!>buildee<!>)
+    <!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>checkExactType<!><<!CANNOT_INFER_PARAMETER_TYPE!>Buildee<TargetType><!>>(buildee)
 }
 
 
