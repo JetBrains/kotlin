5017abbd2caa9257256f5d68501998077e28d03d
Denis.Zharkov <denis.zharkov@jetbrains.com>
2023-12-19 17:13:35 +0100

    ~ new tests

---
+++ compiler/testData/diagnostics/tests/inference/builderInference/fixingTVForLambadaWithShallowDependencyToOuter2.kt
@@ -0,0 +1,24 @@
+class Controller<T> {
+    fun yield(t: T): Boolean = true
+    fun get(): T = TODO()
+}
+
+fun <S> generate(g: suspend Controller<S>.() -> Unit): S = TODO()
+
+fun bar(x: Any) {}
+fun <R> myWith(r: R, b: (R) -> Unit) {}
+
+fun main() {
+    generate {
+        // S <: Any
+        bar(get())
+
+        // S <: R
+        // Any <: R
+        myWith(get()) {
+            it.<!DEBUG_INFO_ELEMENT_WITH_ERROR_TYPE, DEBUG_INFO_UNRESOLVED_WITH_TARGET, UNRESOLVED_REFERENCE!>length<!>
+        }
+
+        yield("")
+    }.length
+}
