9fa1d8e7056bd2ada8020261d0ad1238ebfc7b17
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2023-12-20 14:00:42 +0100

    [BIR] Allow skipping index keys in sequence in getElementsWithIndex

--- compiler/ir/bir/src/org.jetbrains.kotlin.bir/compilation.kt
+++ compiler/ir/bir/src/org.jetbrains.kotlin.bir/compilation.kt
@@ -20,6 +20,7 @@ import org.jetbrains.kotlin.bir.lazy.BirLazyElementBase
 import org.jetbrains.kotlin.bir.types.BirType
 import org.jetbrains.kotlin.bir.util.Bir2IrConverter
 import org.jetbrains.kotlin.bir.util.Ir2BirConverter
+import org.jetbrains.kotlin.bir.util.collectAllElementsInTree
 import org.jetbrains.kotlin.bir.util.countAllElementsInTree
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrAttributeContainer
