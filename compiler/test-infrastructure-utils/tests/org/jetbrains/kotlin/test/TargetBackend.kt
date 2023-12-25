71f2a2df8e56119838dbe5f83a0b7f950a8a4a07
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-12-25 11:44:59 +0100

    [Tests] Drop `JVM_WITH_OLD_EVALUATOR` and `JVM_WITH_IR_EVALUATOR` targets
    These targets were used in plugin, but not anymore.

--- compiler/test-infrastructure-utils/tests/org/jetbrains/kotlin/test/TargetBackend.kt
+++ compiler/test-infrastructure-utils/tests/org/jetbrains/kotlin/test/TargetBackend.kt
@@ -23,9 +23,7 @@ enum class TargetBackend(
     ANDROID(false, JVM),
     ANDROID_IR(true, JVM_IR),
     NATIVE(true),
-    JVM_WITH_OLD_EVALUATOR(false),
     JVM_IR_WITH_OLD_EVALUATOR(true),
-    JVM_WITH_IR_EVALUATOR(false),
     JVM_IR_WITH_IR_EVALUATOR(true);
 
     val compatibleWith get() = compatibleWithTargetBackend ?: ANY
