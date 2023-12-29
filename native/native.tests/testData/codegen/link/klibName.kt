5728b347333f7a2cf6a081c30c0b0b82c7f89f5c
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-12-29 19:29:51 +0100

    [K/N][Tests] Ignore test `klibName` on LInux with caches
    ^KT-61259

--- native/native.tests/testData/codegen/link/klibName.kt
+++ native/native.tests/testData/codegen/link/klibName.kt
@@ -1,3 +1,9 @@
+// KT-64622: [K/N] `ar` utility fails with symbol `@` in module/klib name on Linux with static cache
+// IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE && target=linux_x64
+// IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE && target=linux_arm64
+// IGNORE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE && target=linux_x64
+// IGNORE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE && target=linux_arm64
+
 // MODULE: lib@name
 // FILE: lib.kt
 
