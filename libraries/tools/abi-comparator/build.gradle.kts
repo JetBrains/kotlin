3cf9a6f687312d209355b74c657cc9094ad4190d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-01-02 14:46:46 +0200

    [Build] Fix JPS import of :libraries:tools:abi-comparator

--- libraries/tools/abi-comparator/build.gradle.kts
+++ libraries/tools/abi-comparator/build.gradle.kts
@@ -7,11 +7,9 @@ dependencies {
     implementation(commonDependency("org.jetbrains.intellij.deps:asm-all"))
     implementation("org.apache.commons:commons-text:1.10.0")
 
-    implementation(project(":kotlin-stdlib-jdk8"))
     implementation(project(":tools:kotlinp"))
     implementation(project(":kotlinx-metadata-jvm"))
     implementation(project(":kotlinx-metadata"))
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
-
