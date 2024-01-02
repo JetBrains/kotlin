54970db37aec5f5ad7cb7c428d4980def44365d1
gala377 <rfg@google.com>
2024-01-02 11:59:35 +0100

    Add test generation for FIR in parcelize plugin for bytecode listing.
    All the files under testData have been autogenerated by the test
    generator and will be used as golden files from now on to diff against
    in the future tests.
    
    #KT-64454 Fixed
    
    (cherry picked from commit 555f5ce93f9e8830e8b7a6dfe64f39482234dc19)

---
+++ plugins/parcelize/parcelize-compiler/tests/org/jetbrains/kotlin/parcelize/test/runners/AbstractFirParcelizeBytecodeListingTest.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.parcelize.test.runners
+
+import org.jetbrains.kotlin.test.runners.codegen.AbstractFirPsiAsmLikeInstructionListingTest
+import org.jetbrains.kotlin.parcelize.test.services.ParcelizeEnvironmentConfigurator
+import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.directives.AsmLikeInstructionListingDirectives.FIR_DIFFERENCE
+
+
+open class AbstractFirParcelizeBytecodeListingTest : AbstractFirPsiAsmLikeInstructionListingTest() {
+    override fun configure(builder: TestConfigurationBuilder) {
+        super.configure(builder)
+        builder.configureParcelizeSpecific()
+    }
+}
+
+private fun TestConfigurationBuilder.configureParcelizeSpecific() {
+    defaultDirectives {
+        +FIR_DIFFERENCE
+    }
+    useConfigurators(::ParcelizeEnvironmentConfigurator)
+}
