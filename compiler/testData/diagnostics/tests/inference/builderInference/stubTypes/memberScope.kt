90feeab07640facc208a8f8f30e455f0a791a360
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-01-05 14:28:25 +0100

    K2: Temporary mute controversial K2 IDE test after PCLA implementation
    ^KT-59791 In Progress

--- compiler/testData/diagnostics/tests/inference/builderInference/stubTypes/memberScope.kt
+++ compiler/testData/diagnostics/tests/inference/builderInference/stubTypes/memberScope.kt
@@ -1,6 +1,7 @@
 // !LANGUAGE: +UnrestrictedBuilderInference
 // !DIAGNOSTICS: -DEPRECATION -OPT_IN_IS_NOT_ENABLED
 // WITH_STDLIB
+// MUTE_LL_FIR: KT-64741
 
 // FILE: main.kt
 import kotlin.experimental.ExperimentalTypeInference
