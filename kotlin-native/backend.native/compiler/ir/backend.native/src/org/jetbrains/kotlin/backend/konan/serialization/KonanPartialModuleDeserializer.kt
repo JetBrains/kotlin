1b521d564fcb0271e6722fb58d73227e9cf03a7f
Johan Bay <jobay@google.com>
2024-01-09 15:41:34 +0100

    [K/N] serialize field names to caches
    ^KT-65443

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanPartialModuleDeserializer.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanPartialModuleDeserializer.kt
@@ -241,7 +241,7 @@ internal class KonanPartialModuleDeserializer(
                         val nameAndType = BinaryNameAndType.decode(outerProtoClass.thisReceiver.nameType)
 
                         SerializedClassFieldInfo(
-                                name = INVALID_INDEX,
+                                name = "",
                                 binaryType = INVALID_INDEX,
                                 nameAndType.typeIndex,
                                 flags = 0,
@@ -258,7 +258,7 @@ internal class KonanPartialModuleDeserializer(
                         val primitiveBinaryType = field.type.computePrimitiveBinaryTypeOrNull()
 
                         SerializedClassFieldInfo(
-                                nameAndType.nameIndex,
+                                field.name,
                                 primitiveBinaryType?.ordinal ?: INVALID_INDEX,
                                 if (with(KonanManglerIr) { (classifier as? IrClassSymbol)?.owner?.isExported(compatibleMode) } == false)
                                     INVALID_INDEX
@@ -478,7 +478,7 @@ internal class KonanPartialModuleDeserializer(
                     require(it.alignment == field.alignment) { "Mismatched align information for outer this" }
                 }
             } else {
-                val name = fileDeserializationState.fileReader.string(field.name)
+                val name = field.name
                 val type = when {
                     field.type != INVALID_INDEX -> declarationDeserializer.deserializeIrType(field.type)
                     field.binaryType == INVALID_INDEX -> builtIns.anyNType
