18ace5c10408b6cd118ea39fd1b53ee7d6277885
strangepleasures <pavel.mikhailovskii@jetbrains.com>
2024-01-10 22:07:07 +0100

    Properly exclude Gradle common dependencies in Kapt 4

--- plugins/kapt4/build.gradle.kts
+++ plugins/kapt4/build.gradle.kts
@@ -5,6 +5,15 @@ plugins {
     id("jps-compatible")
 }
 
+fun ModuleDependency.excludeGradleCommonDependencies() {
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-common")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-reflect")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-script-runtime")
+}
+
 dependencies {
     compileOnly(project(":compiler:frontend.java"))
     compileOnly(project(":compiler:plugin-api"))
@@ -12,7 +21,7 @@ dependencies {
     compileOnly(project(":kotlin-annotation-processing-base"))
     compileOnly(project(":analysis:analysis-api-standalone"))
 
-    embedded(project(":kotlin-annotation-processing-compiler")) { isTransitive = false }
+    embedded(project(":kotlin-annotation-processing-compiler")) { excludeGradleCommonDependencies() }
 
     // The following list of dependencies to be embedded is: analysis-api-standalone plus all its transitive dependencies used from this
     // module, excluding the Kotlin compiler and standard libraries. We don't bundle compiler and libraries because they are published
@@ -20,16 +29,17 @@ dependencies {
     // This list is error-prone because if the analysis API module structure changes, the kotlin-annotation-processing artifact might have
     // references to non-existing symbols and it won't work. Integration tests are supposed to check that though.
     // TODO (KT-61030): simplify this somehow and ensure an integration test checks it.
-    embedded(project(":analysis:analysis-api-standalone")) { isTransitive = false }
-    embedded(project(":analysis:analysis-api")) { isTransitive = false }
-    embedded(project(":analysis:analysis-api-fir")) { isTransitive = false }
-    embedded(project(":analysis:analysis-api-impl-base")) { isTransitive = false }
-    embedded(project(":analysis:analysis-api-impl-barebone")) { isTransitive = false }
-    embedded(project(":analysis:analysis-api-standalone:analysis-api-standalone-base")) { isTransitive = false }
-    embedded(project(":analysis:analysis-api-standalone:analysis-api-fir-standalone-base")) { isTransitive = false }
-    embedded(project(":analysis:analysis-internal-utils")) { isTransitive = false }
-    embedded(project(":analysis:low-level-api-fir")) { isTransitive = false }
-    embedded(project(":analysis:symbol-light-classes")) { isTransitive = false }
+    embedded(project(":analysis:analysis-api-standalone")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-api")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-api-fir")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-api-impl-base")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-api-impl-barebone")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-api-standalone:analysis-api-standalone-base")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-api-standalone:analysis-api-fir-standalone-base")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:analysis-internal-utils")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:low-level-api-fir")) { excludeGradleCommonDependencies() }
+    embedded(project(":analysis:symbol-light-classes")) { excludeGradleCommonDependencies() }
+    embedded(intellijCore())
 
     testApi(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter.api)
