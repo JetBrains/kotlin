198c76fc97c513376a45a743fe14c4d36a0afb6e
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-01-14 22:28:12 +0100

    [Gradle] Add documentation for HasKotlinDependencies interface
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/HasKotlinDependencies.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/HasKotlinDependencies.kt
@@ -192,13 +192,64 @@ interface KotlinDependencyHandler {
     ): Dependency
 }
 
+/**
+ * Contains all the configurable Kotlin dependencies for a Kotlin DSL entity, like an instance of `KotlinSourceSet`.
+ */
 interface HasKotlinDependencies {
+
+    /**
+     * Configures all dependencies for this entity.
+     */
     fun dependencies(configure: KotlinDependencyHandler.() -> Unit)
+
+    /**
+     * Configures all dependencies for this entity.
+     */
     fun dependencies(configure: Action<KotlinDependencyHandler>)
 
+    /**
+     * The name of the Gradle [Configuration]
+     * that contains [`api`](https://kotlinlang.org/docs/gradle-configure-project.html#dependency-types) dependencies.
+     *
+     * The Gradle `api` configuration should be used to declare dependencies which are exported by the project API.
+     *
+     * This configuration is not meant to be resolved.
+     */
     val apiConfigurationName: String
+
+    /**
+     * The name of the Gradle [Configuration]
+     * that contains [`implementation`](https://kotlinlang.org/docs/gradle-configure-project.html#dependency-types)
+     * dependencies.
+     *
+     * The Gradle `implementation` configuration should be used to declare dependencies which are internal to the component (internal APIs).
+     *
+     * This configuration is not meant to be resolved.
+     */
     val implementationConfigurationName: String
+
+    /**
+     * The name of the Gradle [Configuration]
+     * containing [`compileOnly`](https://kotlinlang.org/docs/gradle-configure-project.html#dependency-types)
+     * dependencies.
+     *
+     * The Gradle `compileOnly` configuration should be used to declare dependencies that participate in compilation,
+     * but who need to be added explicitly by consumers at runtime.
+     *
+     * This configuration is not meant to be resolved.
+     */
     val compileOnlyConfigurationName: String
+
+    /**
+     * The name of the Gradle [Configuration]
+     * containing [`runtimeOnly`](https://kotlinlang.org/docs/gradle-configure-project.html#dependency-types)
+     * dependencies.
+     *
+     * The Gradle `runtimeOnly` configuration should be used to declare dependencies that don't participate in compilation,
+     * but who are added at runtime.
+     *
+     * This configuration is not meant to be resolved.
+     */
     val runtimeOnlyConfigurationName: String
 }
 
