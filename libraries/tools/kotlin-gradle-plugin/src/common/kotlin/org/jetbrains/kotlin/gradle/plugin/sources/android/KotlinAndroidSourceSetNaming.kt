5ed28a6a79696089ef3cdf3f1fff4f4068d337f5
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-01-15 10:39:59 +0100

    [Gradle] Fix warnings in KotlinAndroidSourceSetNaming
    ^KT-56904 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/sources/android/KotlinAndroidSourceSetNaming.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/sources/android/KotlinAndroidSourceSetNaming.kt
@@ -5,9 +5,9 @@
 
 package org.jetbrains.kotlin.gradle.plugin.sources.android
 
-import com.android.build.gradle.api.BaseVariant
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
+import org.jetbrains.kotlin.gradle.utils.*
 
 internal interface KotlinAndroidSourceSetNaming {
 
@@ -25,5 +25,8 @@ internal interface KotlinAndroidSourceSetNaming {
      * Returns `null`, if this naming schema does not know about it. In this case, the
      * 'default' defaultSourceSetName will be constructed by the compilation.
      */
-    fun defaultKotlinSourceSetName(target: KotlinAndroidTarget, variant: BaseVariant): String? = null
+    fun defaultKotlinSourceSetName(
+        target: KotlinAndroidTarget,
+        @Suppress("TYPEALIAS_EXPANSION_DEPRECATION") variant: DeprecatedAndroidBaseVariant
+    ): String? = null
 }
