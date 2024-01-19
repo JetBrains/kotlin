20e84125b37ca312133cb4ef4cdb38e390553a11
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-01-19 18:22:23 +0100

    Remove suppress

--- compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/generators/StatementGenerator.kt
+++ compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/psi2ir/generators/StatementGenerator.kt
@@ -301,9 +301,8 @@ internal class StatementGenerator(
 
             1 -> {
                 val first = entries.first()
-                @Suppress("CANNOT_CHECK_FOR_ERASED")
-                if (first is IrConst<String> && first.kind == IrConstKind.String)
-                    IrConstImpl.string(startOffset, endOffset, first.type, first.value)
+                if (first is IrConst<*> && first.kind == IrConstKind.String)
+                    IrConstImpl.string(startOffset, endOffset, first.type, first.value as String)
                 else
                     IrStringConcatenationImpl(startOffset, endOffset, resultType, listOf(first))
             }
