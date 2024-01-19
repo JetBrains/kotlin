3036b408ffaa396419ce78517a83f4d9908b6f45
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-01-19 21:14:54 +0100

    [Gradle] Fix warnings in MultiplatformLayoutV2DefaultManifestLocationConfigurator
    ^KT-56904 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/sources/android/configurator/MultiplatformLayoutV2DefaultManifestLocationConfigurator.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/sources/android/configurator/MultiplatformLayoutV2DefaultManifestLocationConfigurator.kt
@@ -5,16 +5,20 @@
 
 package org.jetbrains.kotlin.gradle.plugin.sources.android.configurator
 
-import com.android.build.gradle.api.AndroidSourceSet
 import org.gradle.api.logging.Logging
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
+import org.jetbrains.kotlin.gradle.utils.*
 
 internal object MultiplatformLayoutV2DefaultManifestLocationConfigurator : KotlinAndroidSourceSetConfigurator {
     private const val DEFAULT_FILE_NAME = "AndroidManifest.xml"
     private val logger = Logging.getLogger(javaClass)
 
-    override fun configure(target: KotlinAndroidTarget, kotlinSourceSet: KotlinSourceSet, androidSourceSet: AndroidSourceSet) {
+    override fun configure(
+        target: KotlinAndroidTarget,
+        kotlinSourceSet: KotlinSourceSet,
+        @Suppress("TYPEALIAS_EXPANSION_DEPRECATION") androidSourceSet: DeprecatedAndroidSourceSet
+    ) {
         val defaultManifestLocation = target.project.file("src/${androidSourceSet.name}/$DEFAULT_FILE_NAME")
         if (androidSourceSet.manifest.srcFile != defaultManifestLocation) {
             logger.debug(
