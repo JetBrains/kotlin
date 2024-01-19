be848cc432f13d91793914855451c019594ba8d3
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-01-19 21:03:21 +0100

    [Gradle] Fix warnings in GradleConventionAddKotlinSourcesToAndroidSourceSetConfigurator
    ^KT-56904 In Progress

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/sources/android/configurator/GradleConventionAddKotlinSourcesToAndroidSourceSetConfigurator.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/sources/android/configurator/GradleConventionAddKotlinSourcesToAndroidSourceSetConfigurator.kt
@@ -5,18 +5,22 @@
 
 package org.jetbrains.kotlin.gradle.plugin.sources.android.configurator
 
-import com.android.build.gradle.api.AndroidSourceSet
-import org.jetbrains.kotlin.gradle.plugin.*
+import org.jetbrains.kotlin.gradle.plugin.KOTLIN_DSL_NAME
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.internal.compatibilityConventionRegistrar
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
+import org.jetbrains.kotlin.gradle.utils.*
 
 /*
 Used by AGP com.android.build.gradle.internal.utils.syncAgpAndKgpSources to automatically pick up sources
 from this KotlinSourceSet. This mechanism is deprecated.
 */
-@Suppress("DEPRECATION")
 internal object GradleConventionAddKotlinSourcesToAndroidSourceSetConfigurator : KotlinAndroidSourceSetConfigurator {
-    override fun configure(target: KotlinAndroidTarget, kotlinSourceSet: KotlinSourceSet, androidSourceSet: AndroidSourceSet) {
+    override fun configure(
+        target: KotlinAndroidTarget,
+        kotlinSourceSet: KotlinSourceSet,
+        @Suppress("TYPEALIAS_EXPANSION_DEPRECATION") androidSourceSet: DeprecatedAndroidSourceSet
+    ) {
         target.project.compatibilityConventionRegistrar.addConvention(androidSourceSet, KOTLIN_DSL_NAME, kotlinSourceSet)
     }
 }
