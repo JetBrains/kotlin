37b55cde4f9df66dce4954a540e416810d733780
Adam <152864218+adam-enko@users.noreply.github.com>
2024-01-22 12:13:04 +0100

    [KDM] Configure JetBrains Gradle Enterprise & Build Cache
    See https://github.com/Kotlin/dokka/pull/3446
    
    * [Gradle] configure JetBrains Gradle Enterprise & Build Cache
    
    ^KT-64916 Fixed
    Original commit: https://github.com/Kotlin/dokka/commit/ba5dd18d9ac2fed9529f4c3dae475b106119d221

---
+++ libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/build.gradle.kts
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+plugins {
+    `kotlin-dsl`
+}
+
+description = "Conventions for use in settings.gradle.kts scripts"
+
+dependencies {
+    implementation(libs.gradlePlugin.gradle.enterprise)
+    implementation(libs.gradlePlugin.gradle.customUserData)
+}
---
+++ libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/settings.gradle.kts
@@ -0,0 +1,29 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode.PREFER_SETTINGS
+
+/*
+ * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+rootProject.name = "build-settings-logic"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(PREFER_SETTINGS)
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
