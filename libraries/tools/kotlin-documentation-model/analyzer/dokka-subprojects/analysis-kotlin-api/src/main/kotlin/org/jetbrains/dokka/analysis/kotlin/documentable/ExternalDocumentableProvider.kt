7cb654e6d53bcabf10812ba16c0743bb10295e38
Ignat Beresnev <ignat.beresnev@jetbrains.com>
2024-01-24 17:57:54 +0100

    [KDM] Update the copyright year to 2024
    See https://github.com/Kotlin/dokka/pull/3454
    Original commit: https://github.com/Kotlin/dokka/commit/d4d457d357627fd8100e4f0d47868020485996ec

--- libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/analysis-kotlin-api/src/main/kotlin/org/jetbrains/dokka/analysis/kotlin/documentable/ExternalDocumentableProvider.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/analysis-kotlin-api/src/main/kotlin/org/jetbrains/dokka/analysis/kotlin/documentable/ExternalDocumentableProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package org.jetbrains.dokka.analysis.kotlin.documentable
