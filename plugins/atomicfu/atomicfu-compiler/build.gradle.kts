7702374dbd9a14113701dd28196c139519be9a34
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-01-29 19:31:49 +0100

    [K/N][tests] In atomicfu make check task run all backends ^KT-65392
    - test task will run JVM and JS backend tests; called by CI
    - nativeTest will run Native backend tests; called by CI
    - check will run all backends tests; called locally;

--- plugins/atomicfu/atomicfu-compiler/build.gradle.kts
+++ plugins/atomicfu/atomicfu-compiler/build.gradle.kts
@@ -175,12 +175,6 @@ projectTest(jUnitMode = JUnitMode.JUnit5) {
     dependsOn(":dist")
     dependsOn(atomicfuJsIrRuntimeForTests)
 
-    // Depend on the test task that launches Native tests so that it will also run together with tests
-    // for all other targets if K/N is enabled
-    if (kotlinBuildProperties.isKotlinNativeEnabled) {
-        dependsOn(nativeTest)
-    }
-
     val localAtomicfuJsIrRuntimeForTests: FileCollection = atomicfuJsIrRuntimeForTests
     val localAtomicfuJsClasspath: FileCollection = atomicfuJsClasspath
     val localAtomicfuJvmClasspath: FileCollection = atomicfuJvmClasspath
@@ -203,3 +197,11 @@ val nativeTest = nativeTest(
     customTestDependencies = listOf(atomicfuNativeKlib),
     compilerPluginDependencies = listOf(atomicfuCompilerPluginForTests)
 )
+
+tasks.named("check") {
+    // Depend on the test task that launches Native tests so that it will also run together with tests
+    // for all other targets if K/N is enabled
+    if (kotlinBuildProperties.isKotlinNativeEnabled) {
+        dependsOn(nativeTest)
+    }
+}
