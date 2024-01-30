f25840f0755b70b287cb49c036b82ceb63c3b425
Pavel Kirpichenkov <pavel.kirpichenkov@jetbrains.com>
2024-01-30 15:28:30 +0200

    [AA] Remove dependency on raw status from KtFirMultiplatformInfoProvider
    Not all effectively actual declarations have isActual set in FIR status

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KtFirMultiplatformInfoProvider.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KtFirMultiplatformInfoProvider.kt
@@ -11,9 +11,6 @@ import org.jetbrains.kotlin.analysis.api.fir.symbols.KtFirSymbol
 import org.jetbrains.kotlin.analysis.api.lifetime.KtLifetimeToken
 import org.jetbrains.kotlin.analysis.api.symbols.KtDeclarationSymbol
 import org.jetbrains.kotlin.fir.declarations.expectForActual
-import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirTypeAliasSymbol
 import org.jetbrains.kotlin.resolve.multiplatform.ExpectActualMatchingCompatibility
 
 internal class KtFirMultiplatformInfoProvider(
@@ -23,13 +20,6 @@ internal class KtFirMultiplatformInfoProvider(
     override fun getExpectForActual(actual: KtDeclarationSymbol): List<KtDeclarationSymbol> {
         require(actual is KtFirSymbol<*>)
         val firSymbol = actual.firSymbol
-        val status = when (firSymbol) {
-            is FirCallableSymbol -> firSymbol.rawStatus
-            is FirClassSymbol -> firSymbol.rawStatus
-            is FirTypeAliasSymbol -> firSymbol.rawStatus
-            else -> null
-        }
-        if (status?.isActual != true) return emptyList()
 
         return firSymbol.expectForActual?.get(ExpectActualMatchingCompatibility.MatchedSuccessfully)
             ?.map { analysisSession.firSymbolBuilder.buildSymbol(it) as KtDeclarationSymbol }.orEmpty()
