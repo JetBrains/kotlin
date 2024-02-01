3b264aa25e3238f115b6eb7283bb5c09e92303b8
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-02-01 14:45:22 +0200

    [KDF] remove default arguments overloads needed for plugin
    Moved from https://github.com/Kotlin/dataframe/commit/93b495472a4fbe30a062c5f21f24abc18ed4f69a

--- plugins/kotlin-dataframe/testData/box/unhandledIntrisic.kt
+++ plugins/kotlin-dataframe/testData/box/unhandledIntrisic.kt
@@ -1,9 +1,10 @@
 package org.jetbrains.kotlinx.dataframe
 
 import org.jetbrains.kotlinx.dataframe.api.*
+import org.jetbrains.kotlinx.dataframe.io.*
 
 fun main() {
-    val df = DataFrame.readCSVDefault("https://raw.githubusercontent.com/Kotlin/dataframe/master/data/jetbrains_repositories.csv")
+    val df = DataFrame.readCSV("https://raw.githubusercontent.com/Kotlin/dataframe/master/data/jetbrains_repositories.csv")
     val df1 = df
         .add("topicsList") { topics.removeSurrounding("[", "]").split(", ").filter { it.isNotEmpty() } }
 }
