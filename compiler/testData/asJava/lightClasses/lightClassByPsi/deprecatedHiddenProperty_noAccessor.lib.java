0533c7391046d0bccd7353879c7339abf141b3dd
Jinseong Jeon <jsjeon@google.com>
2024-02-04 22:42:36 -0800

    SLC: add tests about deprecated-hidden property/accessor
    ^KT-65393
    ^KTIJ-27244

---
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/deprecatedHiddenProperty_noAccessor.lib.java
@@ -0,0 +1,25 @@
+public final class Test /* test.pkg.Test*/ {
+  @org.jetbrains.annotations.NotNull()
+  private java.lang.String pNew_noAccessor;
+
+  @org.jetbrains.annotations.NotNull()
+  private java.lang.String pOld_noAccessor_deprecatedOnGetter;
+
+  @org.jetbrains.annotations.NotNull()
+  private java.lang.String pOld_noAccessor_deprecatedOnProperty;
+
+  @org.jetbrains.annotations.NotNull()
+  private java.lang.String pOld_noAccessor_deprecatedOnSetter;
+
+  @org.jetbrains.annotations.NotNull()
+  public final java.lang.String getPNew_noAccessor();//  getPNew_noAccessor()
+
+  @org.jetbrains.annotations.NotNull()
+  public final java.lang.String getPOld_noAccessor_deprecatedOnSetter();//  getPOld_noAccessor_deprecatedOnSetter()
+
+  public  Test();//  .ctor()
+
+  public final void setPNew_noAccessor(@org.jetbrains.annotations.NotNull() java.lang.String);//  setPNew_noAccessor(java.lang.String)
+
+  public final void setPOld_noAccessor_deprecatedOnGetter(@org.jetbrains.annotations.NotNull() java.lang.String);//  setPOld_noAccessor_deprecatedOnGetter(java.lang.String)
+}
