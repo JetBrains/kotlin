0861619b27cd4506c220980159d3d434266abf7f
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-02-07 21:06:11 +0100

    [JPS] Use JDK 17 for the tests generation
    ^KTI-1416 In Progress

--- jps/jps-plugin/build.gradle.kts
+++ jps/jps-plugin/build.gradle.kts
@@ -13,7 +13,7 @@ val compilerModules: Array<String> by rootProject.extra
 val generateTests by generator("org.jetbrains.kotlin.jps.GenerateJpsPluginTestsKt") {
     javaLauncher.set(
         javaToolchains.launcherFor {
-            languageVersion.set(JavaLanguageVersion.of(11))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
     )
 }
