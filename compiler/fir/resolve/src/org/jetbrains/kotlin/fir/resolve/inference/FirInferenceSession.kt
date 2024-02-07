3cea719b1236947455e7d89f8a74f0f532e04a83
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-02-07 15:51:10 +0100

    Minor. Reorder some methods in FirInferenceSession
    Put them in the order in which they are assumed to be called

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/FirInferenceSession.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/FirInferenceSession.kt
@@ -16,6 +16,27 @@ import org.jetbrains.kotlin.resolve.calls.inference.model.ConstraintStorage
 import org.jetbrains.kotlin.resolve.calls.inference.model.NewConstraintSystemImpl
 
 abstract class FirInferenceSession {
+    open fun baseConstraintStorageForCandidate(candidate: Candidate): ConstraintStorage? = null
+
+    open fun customCompletionModeInsteadOfFull(
+        call: FirResolvable,
+    ): ConstraintSystemCompletionMode? = null
+
+    abstract fun <T> processPartiallyResolvedCall(
+        call: T,
+        resolutionMode: ResolutionMode,
+        completionMode: ConstraintSystemCompletionMode
+    ) where T : FirResolvable, T : FirStatement
+
+    open fun runLambdaCompletion(candidate: Candidate, forOverloadByLambdaReturnType: Boolean, block: () -> Unit): ConstraintStorage? {
+        block()
+        return null
+    }
+
+    open fun <T> runCallableReferenceResolution(candidate: Candidate, block: () -> T): T = block()
+
+    open fun addSubtypeConstraintIfCompatible(lowerType: ConeKotlinType, upperType: ConeKotlinType, element: FirElement) {}
+
     companion object {
         val DEFAULT: FirInferenceSession = object : FirInferenceSession() {
             override fun <T> processPartiallyResolvedCall(
@@ -37,25 +58,4 @@ abstract class FirInferenceSession {
             }
         }
     }
-
-    open fun runLambdaCompletion(candidate: Candidate, forOverloadByLambdaReturnType: Boolean, block: () -> Unit): ConstraintStorage? {
-        block()
-        return null
-    }
-
-    open fun <T> runCallableReferenceResolution(candidate: Candidate, block: () -> T): T = block()
-
-    open fun customCompletionModeInsteadOfFull(
-        call: FirResolvable,
-    ): ConstraintSystemCompletionMode? = null
-
-    abstract fun <T> processPartiallyResolvedCall(
-        call: T,
-        resolutionMode: ResolutionMode,
-        completionMode: ConstraintSystemCompletionMode
-    ) where T : FirResolvable, T : FirStatement
-
-    open fun baseConstraintStorageForCandidate(candidate: Candidate): ConstraintStorage? = null
-
-    open fun addSubtypeConstraintIfCompatible(lowerType: ConeKotlinType, upperType: ConeKotlinType, element: FirElement) {}
 }
