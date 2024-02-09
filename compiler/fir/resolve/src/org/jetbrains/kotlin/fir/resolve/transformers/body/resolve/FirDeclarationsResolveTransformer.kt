4aa10fe22c461c28be085dc25b503eb0c7cd76cb
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-02-09 13:29:19 +0200

    [FIR] Prevent `null` sources for functions' implicit return types
    This change in
    `FirDeclarationsResolveTransformer`
    is needed to prevent `source must not
    be null` for implicit function
    return types. See:
    `FirPsiOldFrontendDiagnosticsTestGenerated.testArrayOfNothing`.

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirDeclarationsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirDeclarationsResolveTransformer.kt
@@ -846,8 +846,9 @@ open class FirDeclarationsResolveTransformer(
             val simpleFunction = function as? FirSimpleFunction
             val returnExpression = (body?.statements?.singleOrNull() as? FirReturnExpression)?.result
             val expressionType = returnExpression?.resolvedType
+            val newSource = result.returnTypeRef.source ?: returnExpression?.source?.fakeElement(KtFakeSourceElementKind.ImplicitTypeRef)
             val returnTypeRef = expressionType
-                ?.toFirResolvedTypeRef(result.returnTypeRef.source)
+                ?.toFirResolvedTypeRef(newSource)
                 ?.approximateDeclarationType(
                     session,
                     simpleFunction?.visibilityForApproximation(),
@@ -855,7 +856,7 @@ open class FirDeclarationsResolveTransformer(
                     isInlineFunction = simpleFunction?.isInline == true
                 )
                 ?: buildErrorTypeRef {
-                    source = result.returnTypeRef.source
+                    source = newSource
                     diagnostic = ConeSimpleDiagnostic("empty body", DiagnosticKind.Other)
                 }
             result.transformReturnTypeRef(transformer, withExpectedType(returnTypeRef))
