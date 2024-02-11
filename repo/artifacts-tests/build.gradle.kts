a9d0e8574ec14dfd8ec5679c77511543d75809f9
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2024-02-11 23:39:08 +0100

    Make artifacts-tests:test ConfigurationCache compatible
    Required for KTI-1553

--- repo/artifacts-tests/build.gradle.kts
+++ repo/artifacts-tests/build.gradle.kts
@@ -15,19 +15,19 @@ val defaultSnapshotVersion: String by extra
 findProperty("deployVersion")?.let {
     assert(findProperty("build.number") != null) { "`build.number` parameter is expected to be explicitly set with the `deployVersion`" }
 }
-val buildNumber by extra(findProperty("build.number")?.toString() ?: defaultSnapshotVersion)
-val kotlinVersion by extra(
-    findProperty("deployVersion")?.toString()?.let { deploySnapshotStr ->
-        if (deploySnapshotStr != "default.snapshot") deploySnapshotStr else defaultSnapshotVersion
-    } ?: buildNumber
-)
 
 projectTest(jUnitMode = JUnitMode.JUnit5) {
     workingDir = rootDir
     useJUnitPlatform { }
+    val buildNumber by extra(findProperty("build.number")?.toString() ?: defaultSnapshotVersion)
+    val kotlinVersion by extra(
+        findProperty("deployVersion")?.toString()?.let { deploySnapshotStr ->
+            if (deploySnapshotStr != "default.snapshot") deploySnapshotStr else defaultSnapshotVersion
+        } ?: buildNumber
+    )
+    val defaultMavenLocal: String = rootProject.projectDir.resolve("build/repo").absolutePath
+    val mavenLocal = System.getProperty("maven.repo.local") ?: defaultMavenLocal
     doFirst {
-        val defaultMavenLocal = rootProject.projectDir.resolve("build/repo").absolutePath
-        val mavenLocal = System.getProperty("maven.repo.local") ?: defaultMavenLocal
         systemProperty("maven.repo.local", mavenLocal)
         systemProperty("kotlin.version", kotlinVersion)
     }
