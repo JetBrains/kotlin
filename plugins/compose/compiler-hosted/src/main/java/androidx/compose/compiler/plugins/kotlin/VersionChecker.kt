d9fa1567db30de83110ef6016dd14a8281238703
Jim S <jsproch@google.com>
2024-02-13 11:24:58 -0800

    Catch 1.5.10 Compose Compiler release train
    Change-Id: I86d37768457e9d904788c0571cb86d1028280d77 ( https://android-review.googlesource.com/q/I86d37768457e9d904788c0571cb86d1028280d77 )
    
    Moved from: https://github.com/androidx/androidx/commit/af1802072d653931bb4d6e1e07c122ffad0bebe6

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -144,7 +144,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.5.9"
+        const val compilerVersion: String = "1.5.10"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
