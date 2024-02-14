05cbe66ee009040bb60cb81de47764d413f1e42d
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-02-14 11:47:58 +0100

    [FIR][tests] Unify `IntArrayAsVararg.kt` test between K1 and K2
    Rename `main` function to `test` to avoid running JVM-specific mangling
    and having different IR signature dumps for K1 and K2.
    
    ^KT-65588

--- compiler/testData/ir/irText/firProblems/IntArrayAsVararg.kt
+++ compiler/testData/ir/irText/firProblems/IntArrayAsVararg.kt
@@ -2,10 +2,7 @@
 // WITH_STDLIB
 // ISSUE: KT-60312
 
-// SEPARATE_SIGNATURE_DUMP_FOR_K2
-// ^ The main function has a different mangled name when it's computed from its K1 descriptor in K/JVM.
-
-fun main() {
+fun test() {
     consumeVarargs(1, 2)
     consumeVarargs(arr = intArrayOf(41, 42))
 }
