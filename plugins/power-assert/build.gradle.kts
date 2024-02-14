6d634ac87fd33fca7abc0b4d4dbed76df159f43b
Brian Norman <brian.norman@jetbrains.com>
2024-02-14 07:50:43 -0600

    [PowerAssert] Mark project tests as using JUnit 5

--- plugins/power-assert/build.gradle.kts
+++ plugins/power-assert/build.gradle.kts
@@ -41,7 +41,7 @@ sourcesJar()
 javadocJar()
 testsJar()
 
-projectTest(parallel = true) {
+projectTest(jUnitMode = JUnitMode.JUnit5) {
     dependsOn(":dist")
     workingDir = rootDir
     useJUnitPlatform()
