e7e31097906d74dd495e47f7a4356be47f459ab3
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-02-15 19:05:01 +0100

    Add an IT for publication of multiplatform resources in Android target
    ^KT-65540

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/resources/MultiplatformResourcesPublicationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/resources/MultiplatformResourcesPublicationIT.kt
@@ -5,15 +5,60 @@ import java.nio.file.Paths
 import java.nio.file.StandardCopyOption
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
-import org.jetbrains.kotlin.gradle.util.capitalize
-import org.jetbrains.kotlin.incremental.testingUtils.assertEqualDirectories
+import org.jetbrains.kotlin.incremental.testingUtils.assertEqualDirectoriesIgnoringDotFiles
 import org.junit.jupiter.api.DisplayName
 import java.nio.file.Path
 import java.util.zip.ZipFile
+import kotlin.io.path.name
 
 @MppGradlePluginTests
+@AndroidTestVersions(minVersion = TestVersions.AGP.AGP_73)
 @DisplayName("Test multiplatform resources publication")
 class MultiplatformResourcesPublicationIT : KGPBaseTest() {
+
+    @DisplayName("Multiplatform resources publication for Android target with release build type")
+    @GradleAndroidTest
+    fun testAndroidReleaseResourcesPublication(
+        gradleVersion: GradleVersion,
+        androidVersion: String,
+        providedJdk: JdkVersions.ProvidedJdk,
+    ) {
+        project(
+            "multiplatformResources/publication",
+            gradleVersion,
+            buildJdk = providedJdk.location,
+        ) {
+            buildWithAGPVersion(
+                ":publishAndroidReleasePublicationToMavenRepository",
+                androidVersion,
+                defaultBuildOptions,
+            )
+            val publishedAarPath = "build/repo/test/publication-android/1.0/publication-android-1.0.aar"
+            val classesInAar = projectPath.resolve("classesInAar")
+            val classesJar = "classes.jar"
+            unzip(
+                projectPath.resolve(publishedAarPath),
+                classesInAar,
+                filesStartingWith = classesJar
+            )
+            compareEmbeddedResources(
+                inputZip = classesInAar.resolve(classesJar),
+                reference = reference("androidMain")
+            )
+
+            val assetsInAar = projectPath.resolve("assetsInAar")
+            unzip(
+                projectPath.resolve(publishedAarPath),
+                assetsInAar,
+                filesStartingWith = "assets"
+            )
+            assertEqualDirectoriesIgnoringDotFiles(
+                assetsInAar.toFile(),
+                reference("androidFonts").toFile(),
+                forgiveOtherExtraFiles = false,
+            )
+        }
+    }
 
     @DisplayName("Multiplatform resources publication for jvm target")
     @GradleAndroidTest
@@ -28,7 +73,7 @@ class MultiplatformResourcesPublicationIT : KGPBaseTest() {
             providedJdk,
             publicationTask = ":publishJvmPublicationToMavenRepository",
             publishedArchive = "build/repo/test/publication-jvm/1.0/publication-jvm-1.0.jar",
-            reference = "jvm",
+            referenceName = "jvm",
         )
     }
 
@@ -38,7 +83,7 @@ class MultiplatformResourcesPublicationIT : KGPBaseTest() {
         providedJdk: JdkVersions.ProvidedJdk,
         publicationTask: String,
         publishedArchive: String,
-        reference: String,
+        referenceName: String,
     ) {
         project(
             "multiplatformResources/publication",
@@ -48,61 +93,36 @@ class MultiplatformResourcesPublicationIT : KGPBaseTest() {
             buildWithAGPVersion(
                 publicationTask,
                 androidVersion,
+                defaultBuildOptions,
             )
             compareEmbeddedResources(
-                publishedArchive,
-                reference
+                projectPath.resolve(publishedArchive),
+                reference(referenceName)
             )
         }
     }
 
-    private fun TestProject.buildWithAGPVersion(
-        task: String,
-        androidVersion: String,
-    ) {
-        build(
-            task,
-            buildOptions = defaultBuildOptions.copy(androidVersion = androidVersion)
-        )
-    }
+    private fun TestProject.reference(
+        named: String
+    ): Path = projectPath.resolve("reference/$named")
 
     private fun TestProject.compareEmbeddedResources(
-        inputZip: String,
-        reference: String,
+        inputZip: Path,
+        reference: Path,
     ) {
-        val publishedResources = projectPath.resolve("published/${reference}")
+        val publishedResources = projectPath.resolve("published/${reference.name}")
         unzip(
             inputZip = inputZip,
             outputDir = publishedResources,
             filesStartingWith = "embed",
         )
-        val referenceResources = projectPath.resolve("reference/$reference")
         assertDirectoryExists(publishedResources)
-        assertDirectoryExists(referenceResources)
-        assertEqualDirectories(
+        assertDirectoryExists(reference)
+        assertEqualDirectoriesIgnoringDotFiles(
             publishedResources.toFile(),
-            referenceResources.toFile(),
-            forgiveExtraFiles = false,
-            filter = { !it.name.startsWith(".") }
+            reference.toFile(),
+            forgiveOtherExtraFiles = false,
         )
-    }
-
-    private fun TestProject.unzip(
-        inputZip: String,
-        outputDir: Path,
-        filesStartingWith: String,
-    ) {
-        ZipFile(projectPath.resolve(inputZip).toFile()).use {
-            it.entries().asSequence().filter { it.name.startsWith(filesStartingWith) && !it.isDirectory }.forEach { entry ->
-                val outputFile = outputDir.resolve(Paths.get(entry.name))
-                if (!outputFile.parent.toFile().exists())
-                    Files.createDirectories(outputFile.parent)
-
-                it.getInputStream(entry).use { input ->
-                    Files.copy(input, outputFile, StandardCopyOption.REPLACE_EXISTING)
-                }
-            }
-        }
     }
 
 }
