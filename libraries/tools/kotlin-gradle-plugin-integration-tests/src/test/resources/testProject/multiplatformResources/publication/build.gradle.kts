b7631a7e1056e374d72db2dc93beeef9af7af7aa
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-02-15 14:09:21 +0100

    Add an IT for publication of multiplatform resources in Android target
    ^KT-65540

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/multiplatformResources/publication/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/multiplatformResources/publication/build.gradle.kts
@@ -1,14 +1,25 @@
 @file:OptIn(InternalKotlinGradlePluginApi::class)
 
+import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.resources.KotlinTargetResourcesPublication
+import java.io.File
 
 group = "test"
 version = "1.0"
 
 plugins {
     kotlin("multiplatform")
+    id("com.android.library")
     `maven-publish`
+}
+
+repositories {
+    google()
+    mavenCentral()
+    mavenLocal()
 }
 
 kotlin {
@@ -17,24 +28,57 @@ kotlin {
     ) as KotlinTargetResourcesPublication
 
     listOf(
+        androidTarget {
+            publishAllLibraryVariants()
+            compilations.all {
+                kotlinOptions.jvmTarget = "1.8"
+            }
+        },
         jvm(),
     ).forEach { target ->
+        val fontsFilter = if (target is KotlinAndroidTarget) listOf("fonts/*") else emptyList()
+        val relativeResourcePlacement = provider { File("embed/inside") }
+        val sourceSetPathProvider: (KotlinSourceSet) -> (Provider<File>) = { sourceSet ->
+            project.provider { project.file("src/${sourceSet.name}/multiplatformResources") }
+        }
+
         publication.publishResourcesAsKotlinComponent(
-            target,
-            { sourceSet ->
+            target = target,
+            resourcePathForSourceSet = { sourceSet ->
                 KotlinTargetResourcesPublication.ResourceRoot(
-                    absolutePath = project.provider { project.file("src/${sourceSet.name}/multiplatformResources") },
+                    absolutePath = sourceSetPathProvider(sourceSet),
                     includes = emptyList(),
-                    excludes = emptyList()
+                    excludes = fontsFilter,
                 )
             },
-            provider { File("embed/inside") }
+            relativeResourcePlacement = relativeResourcePlacement,
         )
+        if (target is KotlinAndroidTarget) {
+            publication.publishInAndroidAssets(
+                target = target,
+                resourcePathForSourceSet = { sourceSet ->
+                    KotlinTargetResourcesPublication.ResourceRoot(
+                        absolutePath = sourceSetPathProvider(sourceSet),
+                        includes = fontsFilter,
+                        excludes = emptyList(),
+                    )
+                },
+                relativeResourcePlacement = relativeResourcePlacement,
+            )
+        }
     }
 }
 
 publishing {
     repositories {
         maven("${buildDir}/repo")
+    }
+}
+
+android {
+    namespace = "test"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
     }
 }
