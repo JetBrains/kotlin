8dd66c081070ec027de34844e332294590a50c7d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-02-15 22:37:43 +0100

    [LC] add test on unit type actualization
    ^KT-65843

---
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/UnitAsTypeAlias.java
@@ -0,0 +1,15 @@
+public final class Usage /* mypack.Usage*/ {
+  @org.jetbrains.annotations.NotNull()
+  public final kotlin.Unit unitTypeAlias();//  unitTypeAlias()
+
+  @org.jetbrains.annotations.Nullable()
+  public final kotlin.Unit nullableUnitTypeAlias();//  nullableUnitTypeAlias()
+
+  @org.jetbrains.annotations.Nullable()
+  public final kotlin.Unit nullableUnitTypeAliasAsNullable();//  nullableUnitTypeAliasAsNullable()
+
+  @org.jetbrains.annotations.Nullable()
+  public final kotlin.Unit unitTypeAliasAsNullable();//  unitTypeAliasAsNullable()
+
+  public  Usage();//  .ctor()
+}
---
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/compilationErrors/UnitAsTypeAliasActualization.java
@@ -0,0 +1,17 @@
+public final class Usage /* mypack.Usage*/ {
+  @org.jetbrains.annotations.NotNull()
+  private final kotlin.Unit prop;
+
+  @org.jetbrains.annotations.NotNull()
+  public final kotlin.Unit foo();//  foo()
+
+  @org.jetbrains.annotations.NotNull()
+  public final kotlin.Unit getProp();//  getProp()
+
+  @org.jetbrains.annotations.Nullable()
+  public final kotlin.Unit getNullableProperty();//  getNullableProperty()
+
+  public  Usage();//  .ctor()
+
+  public final void usage(@org.jetbrains.annotations.NotNull() kotlin.Unit);//  usage(kotlin.Unit)
+}
