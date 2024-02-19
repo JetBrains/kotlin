fe4ead751f11c1ecad2fcd68a25be5a52feb8ad3
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-02-19 15:24:18 +0100

    [LL FIR] LLFirTargetResolver: do not resolve file annotation container before each declaration
    We don't have to resolve `FirFileAnnotationsContainer` before each
    declaration.
    This became possible after KT-65345 as now `FirDesignation` has `FirFile`
    in its path so, `PersistenceContextCollector#collectContext` resolves
    it as well as we don't have problems with unresolved annotations
    during diagnostics processing
    
    ^KT-65876

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirTargetResolver.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirTargetResolver.kt
@@ -13,7 +13,6 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.util.checkPhase
 import org.jetbrains.kotlin.analysis.low.level.api.fir.util.errorWithFirSpecificEntries
 import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.FirElementWithResolveState
-import org.jetbrains.kotlin.fir.FirFileAnnotationsContainer
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirConstructor
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
@@ -135,8 +134,7 @@ internal abstract class LLFirTargetResolver(
      * @see skipDependencyTargetResolutionStep
      */
     private fun resolveDependencies(target: FirElementWithResolveState) {
-        if (skipDependencyTargetResolutionStep || target is FirFileAnnotationsContainer) return
-        resolveTarget.firFile?.annotationsContainer?.lazyResolveToPhase(resolverPhase)
+        if (skipDependencyTargetResolutionStep) return
 
         val originalDeclaration = (target as? FirCallableDeclaration)?.originalIfFakeOverrideOrDelegated()
         when {
@@ -160,6 +158,9 @@ internal abstract class LLFirTargetResolver(
             target is FirField && target.origin == FirDeclarationOrigin.Synthetic.DelegateField || target is FirConstructor -> {
                 containingClass(target).lazyResolveToPhase(resolverPhase)
             }
+
+            // Effectively, file annotation container is a part of FirFile, so we have to resolve it before
+            target is FirFile -> target.annotationsContainer?.lazyResolveToPhase(resolverPhase)
         }
     }
 
