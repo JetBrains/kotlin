0e68b8868e31f3cce0e839595080f40bff27bb7c
Troels Bjerre Lund <troels@google.com>
2024-02-20 09:39:27 +0100

    Update to LLVM-16 for linux
    This required disabling the Konan_cxa_demangler dummy, which is used to
    reduce binary size by avoid linking in the default demangler and its
    dependencies. TODO: find out how to make this work again.

--- kotlin-native/libllvmext/src/main/include/CAPIExtensions.h
+++ kotlin-native/libllvmext/src/main/include/CAPIExtensions.h
@@ -5,8 +5,6 @@
 #ifndef LIBLLVMEXT_EXTENSIONS_H
 #define LIBLLVMEXT_EXTENSIONS_H
 
-#endif //LIBLLVMEXT_EXTENSIONS_H
-
 #include <llvm-c/Core.h>
 #include <llvm-c/Target.h>
 
@@ -15,17 +13,11 @@
 extern "C" {
 # endif
 
-void LLVMKotlinAddTargetLibraryInfoWrapperPass(LLVMPassManagerRef passManagerRef, const char* targetTriple);
-
-void LLVMAddObjCARCContractPass(LLVMPassManagerRef passManagerRef);
-
 void LLVMKotlinInitializeTargets();
 
 void LLVMSetNoTailCall(LLVMValueRef Call);
 
 int LLVMInlineCall(LLVMValueRef call);
-
-void LLVMAddThreadSanitizerPass(LLVMPassManagerRef PM);
 
 /// Control LLVM -time-passes flag.
 void LLVMSetTimePasses(int enabled);
@@ -39,3 +31,5 @@ void LLVMClearAllTimers();
 # ifdef __cplusplus
 }
 # endif
+
+#endif //LIBLLVMEXT_EXTENSIONS_H
