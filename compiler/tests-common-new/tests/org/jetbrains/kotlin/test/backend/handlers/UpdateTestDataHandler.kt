6c030923f042a2d12b7743ccfa7a3ec6b7569497
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2024-02-22 14:40:47 +0100

    [Tests] Support for multiple failures in UpdateTestDataHandler

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/handlers/UpdateTestDataHandler.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/handlers/UpdateTestDataHandler.kt
@@ -9,10 +9,10 @@ import com.intellij.rt.execution.junit.FileComparisonFailure
 import org.jetbrains.kotlin.test.WrappedException
 import org.jetbrains.kotlin.test.model.AfterAnalysisChecker
 import org.jetbrains.kotlin.test.services.TestServices
+import org.junit.runners.model.MultipleFailureException
+import org.opentest4j.MultipleFailuresError
 import java.io.File
-import kotlin.collections.mapNotNull
 import kotlin.io.writeText
-import kotlin.text.endsWith
 
 /**
  * Does nothing normally.
@@ -34,9 +34,27 @@ class UpdateTestDataHandler(
 ) : AfterAnalysisChecker(testServices) {
     private val enabled = false
 
+    private fun Throwable.unwrap(): Sequence<FileComparisonFailure> = sequence {
+        when (this@unwrap) {
+            is MultipleFailuresError -> {
+                for (failure in failures) {
+                    yieldAll(failure.unwrap())
+                }
+            }
+            is MultipleFailureException -> {
+                for (failure in failures) {
+                    yieldAll(failure.unwrap())
+                }
+            }
+            is WrappedException -> yieldAll(cause.unwrap())
+            is FileComparisonFailure -> yield(this@unwrap)
+            else -> {}
+        }
+    }
+
     override fun suppressIfNeeded(failedAssertions: List<WrappedException>): List<WrappedException> {
         if (enabled || System.getProperty("kotlin.test.update.test.data") == "true") {
-            for (failure in failedAssertions.mapNotNull { it.cause as? FileComparisonFailure }) {
+            for (failure in failedAssertions.asSequence().flatMap { it.unwrap() }) {
                 File(failure.filePath).writeText(failure.actual)
             }
         }
