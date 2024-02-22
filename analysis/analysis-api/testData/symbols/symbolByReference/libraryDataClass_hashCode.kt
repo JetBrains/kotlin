10ebf7df0954909298f8896e6236c2315aff837d
Roman Golyshev <roman.golyshev@jetbrains.com>
2024-02-22 17:48:02 +0100

    KT-64808 [stubs] Add extra tests for synthethic declarations from data and value classes
    It's not related to the stubs format changes, but there were no tests
    for that in symbols resolve tests

---
+++ analysis/analysis-api/testData/symbols/symbolByReference/libraryDataClass_hashCode.kt
@@ -0,0 +1,13 @@
+// IGNORE_FE10
+// MODULE: lib
+// MODULE_KIND: LibraryBinary
+// FILE: Lib.kt
+package one
+
+data class DataClass(val name: String)
+
+// MODULE: main(lib)
+// FILE: usage.kt
+fun usage(instance: one.DataClass) {
+    instance.<caret>hashCode()
+}
