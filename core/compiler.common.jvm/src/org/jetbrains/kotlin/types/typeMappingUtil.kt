94e5653eb7bec217eac931edda84a9def96ef59e
Jinseong Jeon <jsjeon@google.com>
2024-02-22 14:30:21 -0800

    Migrate utils to check/alter TypeMappingMode according to suppress wildcard annotations
    ^KT-61734

---
+++ core/compiler.common.jvm/src/org/jetbrains/kotlin/types/typeMappingUtil.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:JvmName("TypeMappingUtil")
+
+package org.jetbrains.kotlin.types
+
+import org.jetbrains.kotlin.load.kotlin.TypeMappingMode
+import org.jetbrains.kotlin.name.JvmStandardClassIds
+import org.jetbrains.kotlin.name.JvmStandardClassIds.JVM_SUPPRESS_WILDCARDS_ANNOTATION_FQ_NAME
+import org.jetbrains.kotlin.types.model.KotlinTypeMarker
+
+fun TypeMappingMode.updateArgumentModeFromAnnotations(
+    type: KotlinTypeMarker, typeSystem: TypeSystemCommonBackendContext
+): TypeMappingMode {
+    type.suppressWildcardsMode(typeSystem)?.let {
+        return TypeMappingMode.createWithConstantDeclarationSiteWildcardsMode(
+            skipDeclarationSiteWildcards = it,
+            isForAnnotationParameter = isForAnnotationParameter,
+            needInlineClassWrapping = needInlineClassWrapping,
+            mapTypeAliases = mapTypeAliases
+        )
+    }
+
+    if (with(typeSystem) { type.hasAnnotation(JvmStandardClassIds.JVM_WILDCARD_ANNOTATION_FQ_NAME) }) {
+        return TypeMappingMode.createWithConstantDeclarationSiteWildcardsMode(
+            skipDeclarationSiteWildcards = false,
+            isForAnnotationParameter = isForAnnotationParameter,
+            fallbackMode = this,
+            needInlineClassWrapping = needInlineClassWrapping,
+            mapTypeAliases = mapTypeAliases
+        )
+    }
+
+    return this
+}
+
+fun KotlinTypeMarker.suppressWildcardsMode(typeSystem: TypeSystemCommonBackendContext): Boolean? =
+    with(typeSystem) {
+        if (hasAnnotation(JVM_SUPPRESS_WILDCARDS_ANNOTATION_FQ_NAME))
+            getAnnotationFirstArgumentValue(JVM_SUPPRESS_WILDCARDS_ANNOTATION_FQ_NAME) as? Boolean ?: true
+        else null
+    }
