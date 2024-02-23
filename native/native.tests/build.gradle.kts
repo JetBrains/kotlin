3c097ee5e04b8777fc9e7e7606a9ed8c88dae3de
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-02-23 13:12:59 +0100

    Revert "Revert "[K/N][Tests] Migrate driver test llvm_variant_dev""
    This reverts commit dc4f12f29c5ea6ebf5326c29599ff44ba87c2631.

--- native/native.tests/build.gradle.kts
+++ native/native.tests/build.gradle.kts
@@ -56,6 +56,7 @@ val debuggerTest = nativeTest("debuggerTest", "debugger")
 val cachesTest = nativeTest("cachesTest", "caches")
 val klibTest = nativeTest("klibTest", "klib")
 val standaloneTest = nativeTest("standaloneTest", "standalone")
+val driverTest = nativeTest("driverTest", "driver")
 
 val testTags = findProperty("kotlin.native.tests.tags")?.toString()
 // Note: arbitrary JUnit tag expressions can be used in this property.
