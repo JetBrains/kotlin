69a7e4f5f206b90818c62e58eeca47bf12d7f8e6
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-02-26 14:58:23 +0100

    [FIR] Improve source selection for NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER

--- compiler/testData/diagnostics/tests/inference/reportNotEnoughTypeInformationErrorsOnBlockExpressions.fir.kt
+++ compiler/testData/diagnostics/tests/inference/reportNotEnoughTypeInformationErrorsOnBlockExpressions.fir.kt
@@ -14,7 +14,7 @@ fun poll17(flag: Boolean): Any? {
 }
 
 fun poll26(flag: Boolean): Any? {
-    val inv = when (flag) { true -> ::<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>Foo7<!> false -> foo7() else -> ::Foo7 }
+    val inv = when (flag) { true -> ::<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>Foo7<!> false -> foo7() else -> ::<!NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>Foo7<!> }
     return inv
 }
 
