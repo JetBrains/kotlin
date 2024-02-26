fde58e4dececb89a0827e716bad4c61fbcdb5092
Vadim Mishenev <vad-mishenev@yandex.ru>
2024-02-26 13:42:59 +0200

    [KDM] [K2] Fix missing `actual` keyword for constructors
    See https://github.com/Kotlin/dokka/pull/3500
    Original commit: https://github.com/Kotlin/dokka/commit/2a94f8b56ee9a5607c6127ed637f9f9b775088f1

--- libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/plugin-base/src/test/kotlin/model/ConstructorsTest.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/plugin-base/src/test/kotlin/model/ConstructorsTest.kt
@@ -161,4 +161,123 @@ class ConstructorsTest : AbstractModelTest("/src/main/kotlin/constructors/Test.k
         }
     }
 
+    @Test
+    fun `should have KMP documentation for @constructor tag`() {
+        val expectedDescriptionTag = Description(
+            CustomDocTag(
+                listOf(
+                    P(
+                        listOf(
+                            Text("some doc"),
+                        )
+                    )
+                ),
+                params = emptyMap(),
+                name = MARKDOWN_ELEMENT_FILE_NAME
+            )
+        )
+        val configuration = dokkaConfiguration {
+            sourceSets {
+                val common = sourceSet {
+                    name = "common"
+                    displayName = "common"
+                    analysisPlatform = "common"
+                    sourceRoots = listOf("src/main/kotlin/common/Test.kt")
+                }
+
+                sourceSet {
+                    name = "jvm"
+                    displayName = "jvm"
+                    analysisPlatform = "jvm"
+                    sourceRoots = listOf("src/main/kotlin/jvm/Test.kt")
+                    dependentSourceSets = setOf(common.value.sourceSetID)
+                }
+            }
+        }
+
+        testInline(
+            """
+            |/src/main/kotlin/common/Test.kt
+            |package multiplatform
+            |
+            |expect class A()
+            |
+            |/src/main/kotlin/jvm/Test.kt
+            |package multiplatform
+            |/**
+            ||* @constructor some doc
+            |*/
+            |actual class A()
+        """.trimMargin(),
+            configuration
+        ) {
+            documentablesMergingStage = {
+                val classlike = it.packages.flatMap { it.classlikes }.first() as DClass
+                classlike.name equals "A"
+                val actualConstructor = classlike.constructors.first { it.sourceSets.single().displayName == "jvm" }
+                actualConstructor.documentation.values.single() equals DocumentationNode(listOf(expectedDescriptionTag))
+
+                val expectConstructor = classlike.constructors.first { it.sourceSets.single().displayName == "common" }
+                expectConstructor.documentation.isEmpty() equals true
+            }
+
+        }
+    }
+
+    @Test
+    fun `should have actual constructor`() {
+        val configuration = dokkaConfiguration {
+            sourceSets {
+                val common = sourceSet {
+                    name = "common"
+                    displayName = "common"
+                    analysisPlatform = "common"
+                    sourceRoots = listOf("src/main/kotlin/common/Test.kt")
+                }
+
+                sourceSet {
+                    name = "jvm"
+                    displayName = "jvm"
+                    analysisPlatform = "jvm"
+                    sourceRoots = listOf("src/main/kotlin/jvm/Test.kt")
+                    dependentSourceSets = setOf(common.value.sourceSetID)
+                }
+            }
+        }
+
+        testInline(
+            """
+            |/src/main/kotlin/common/Test.kt
+            |package multiplatform
+            |
+            |expect class A()
+            |
+            |/src/main/kotlin/jvm/Test.kt
+            |package multiplatform
+            |actual class A{
+            |    actual constructor(){}
+            |}
+        """.trimMargin(),
+            configuration
+        ) {
+            preMergeDocumentablesTransformationStage = {
+                val actualClasslike = it.first { it.sourceSets.single().displayName == "common" }.packages.flatMap { it.classlikes }.first() as DClass
+                actualClasslike.name equals "A"
+                val actualConstructor = actualClasslike.constructors.first { it.sourceSets.single().displayName == "common" }
+                actualConstructor.isExpectActual equals true
+
+                val expectClasslike = it.first { it.sourceSets.single().displayName == "jvm" }.packages.flatMap { it.classlikes }.first() as DClass
+                expectClasslike.name equals "A"
+                val expectConstructor = expectClasslike.constructors.first { it.sourceSets.single().displayName == "jvm" }
+                expectConstructor.isExpectActual equals true
+            }
+            documentablesMergingStage = {
+                val classlike = it.packages.flatMap { it.classlikes }.first() as DClass
+                classlike.name equals "A"
+                val constructor = classlike.constructors.single()
+                constructor.isExpectActual equals true
+                constructor.sourceSets counts 2
+            }
+        }
+    }
 }
