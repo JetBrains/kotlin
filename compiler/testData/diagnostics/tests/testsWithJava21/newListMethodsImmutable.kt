d870d9dcc6b5ab07cea5b975d05b1e01e43940ce
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-02-26 10:40:22 +0100

    K1/K2: add tests for KT-65441 current behavior

--- compiler/testData/diagnostics/tests/testsWithJava21/newListMethodsImmutable.kt
+++ compiler/testData/diagnostics/tests/testsWithJava21/newListMethodsImmutable.kt
@@ -1,10 +1,14 @@
-// ISSUE: KT-64640
+// ISSUE: KT-64640, KT-65441
 
 fun bar(x: List<String>) {
     x.<!UNRESOLVED_REFERENCE!>addFirst<!>("")
     x.<!UNRESOLVED_REFERENCE!>addLast<!>("")
     x.<!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>removeFirst<!>()
     x.<!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>removeLast<!>()
+    x.<!DEPRECATION, JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE!>getFirst<!>()
+    x.<!DEPRECATION, JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE!>getLast<!>()
+    x.<!DEPRECATION!>first<!>
+    x.<!DEPRECATION!>last<!>
 }
 
 // Additional test for other SequenceCollection inheritor
@@ -13,11 +17,19 @@ fun baz(x: ArrayDeque<String>, y: LinkedHashSet<String>) {
     x.addLast("")
     x.removeFirst()
     x.removeLast()
+    x.<!DEPRECATION!>getFirst<!>()
+    x.<!DEPRECATION!>getLast<!>()
+    x.<!DEPRECATION!>first<!>
+    x.<!DEPRECATION!>last<!>
 
     y.addFirst("")
     y.addLast("")
     y.removeFirst()
     y.removeLast()
+    y.getFirst()
+    y.getLast()
+    y.first
+    y.last
 }
 
 // Test for collections with (add/remove)(First/Last) methods which are not covered by autotests
@@ -26,14 +38,26 @@ fun foo(x: java.util.SequencedCollection<String>, y: java.util.SequencedSet<Stri
     x.addLast("")
     x.removeFirst()
     x.removeLast()
+    x.getFirst()
+    x.getLast()
+    x.first
+    x.last
 
     y.addFirst("")
     y.addLast("")
     y.removeFirst()
     y.removeLast()
+    y.getFirst()
+    y.getLast()
+    y.first
+    y.last
 
     z.addFirst("")
     z.addLast("")
     z.removeFirst()
     z.removeLast()
+    z.getFirst()
+    z.getLast()
+    z.first
+    z.last
 }
