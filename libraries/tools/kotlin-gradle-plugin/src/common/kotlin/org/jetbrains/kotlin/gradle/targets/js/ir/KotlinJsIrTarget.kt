a8266eb20e6cb2a8728a9ea87b41e1b0a58b0096
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-02-27 10:42:30 +0100

    Simplify set up of the resources variant in js and native targets
    ^KT-65540

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/ir/KotlinJsIrTarget.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/ir/KotlinJsIrTarget.kt
@@ -89,16 +89,11 @@ constructor(
             )
         )
 
-        if (project.kotlinPropertiesProvider.mppResourcesPublication) {
-            project.multiplatformExtensionOrNull?.let {
-                usageContexts.add(
-                    it.setUpResourcesVariant(
-                        this,
-                        mainCompilation
-                    )
-                )
-            }
-        }
+        usageContexts.addIfNotNull(
+            setUpResourcesVariant(
+                mainCompilation
+            )
+        )
 
         val result = createKotlinVariant(componentName, mainCompilation, usageContexts)
 
