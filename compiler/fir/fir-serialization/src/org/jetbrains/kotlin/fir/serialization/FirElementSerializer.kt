b4d036ce5816c85c1730821c04c03d1bf19da5ac
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-02-28 15:00:34 +0100

    [FIR] Remove unused code

--- compiler/fir/fir-serialization/src/org/jetbrains/kotlin/fir/serialization/FirElementSerializer.kt
+++ compiler/fir/fir-serialization/src/org/jetbrains/kotlin/fir/serialization/FirElementSerializer.kt
@@ -1212,14 +1212,6 @@ class FirElementSerializer private constructor(
         return proto
     }
 
-    private operator fun FirArgumentList.get(name: Name): FirExpression? {
-        // TODO: constant evaluation
-        return arguments.filterIsInstance<FirNamedArgumentExpression>().find {
-            it.name == name
-        }?.expression
-    }
-
-
     private fun normalizeVisibility(declaration: FirMemberDeclaration): Visibility {
         return declaration.visibility.normalize()
     }
