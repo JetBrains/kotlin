2118c4a3946f6594a3342fdaa2413115e2fd817d
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-02-29 16:02:16 +0100

    [PowerAssert] Reproduce KT-66208

--- plugins/power-assert/testData/codegen/cast/InstanceEquals.kt
+++ plugins/power-assert/testData/codegen/cast/InstanceEquals.kt
@@ -1,3 +1,21 @@
 fun box() = expectThrowableMessage {
     assert(null is String)
+} + "\n\n" + expectThrowableMessage {
+    // Test that we don't just search for `is` in the expression.
+    assert(!(" is " is String))
+} + "\n\n" + expectThrowableMessage {
+    // Test multiline case
+    assert(!(
+        " is "
+
+                is
+
+                String
+    ))
+} + "\n\n" + expectThrowableMessage {
+    // Test that we don't assume whitespaces around the operator
+    assert(null/*is*/is/*is*/String)
+} + "\n\n" + expectThrowableMessage {
+    // Test nested `is`
+    assert(!((null is String) is Boolean))
 }
