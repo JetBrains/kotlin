7d22825176b0f516db8b9f03f7704d96eaee5dc4
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-02-29 16:02:16 +0100

    [PowerAssert] Reproduce KT-66208

---
+++ plugins/power-assert/testData/codegen/operator/ExcleqOperator.kt
@@ -0,0 +1,21 @@
+fun box() = expectThrowableMessage {
+    assert(1 != 1)
+} + "\n\n" + expectThrowableMessage {
+    // Test that we don't just search for `!=` in the expression.
+    assert(" != " != " != ")
+} + "\n\n" + expectThrowableMessage {
+    // Test multiline case
+    assert(
+        " != "
+
+                !=
+
+                " != "
+    )
+} + "\n\n" + expectThrowableMessage {
+    // Test that we don't assume whitespaces around the infix operator
+    assert(1/*!=*/!=/*!=*/1)
+} + "\n\n" + expectThrowableMessage {
+    // Test nested `!=`
+    assert((1 != 1) != false)
+}
