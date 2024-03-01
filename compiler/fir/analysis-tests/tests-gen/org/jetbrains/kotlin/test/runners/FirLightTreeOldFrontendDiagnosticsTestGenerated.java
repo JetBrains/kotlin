cee922e1e58dc6a8ad6a1d231332dcc57b7fab62
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-03-01 14:39:56 +0200

    [FIR] Ensure KT-50293 is no longer a problem
    PCLA-related changes led to this change in
    behavior. In K1 an explicit cast was needed
    to prevent ORA, but now in K2 this code
    is green without casts.
    
    ^KT-50293 Obsolete

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsTestGenerated.java
@@ -17652,6 +17652,12 @@ public class FirLightTreeOldFrontendDiagnosticsTestGenerated extends AbstractFir
         }
 
         @Test
+        @TestMetadata("kt49829_noCast.kt")
+        public void testKt49829_noCast() {
+          runTest("compiler/testData/diagnostics/tests/inference/builderInference/kt49829_noCast.kt");
+        }
+
+        @Test
         @TestMetadata("kt50498.kt")
         public void testKt50498() {
           runTest("compiler/testData/diagnostics/tests/inference/builderInference/kt50498.kt");
