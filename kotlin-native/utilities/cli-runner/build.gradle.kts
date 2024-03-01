ab62632a85b7cf10be496a43ad4cec4632559899
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-03-02 00:15:09 +0100

    [K/N] Move files, env interop generation to kotlin-native/common/

--- kotlin-native/utilities/cli-runner/build.gradle.kts
+++ kotlin-native/utilities/cli-runner/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
     kotlin("jvm")
 }
@@ -10,6 +12,16 @@ dependencies {
     implementation(project(":kotlin-native:klib"))
     implementation(project(":kotlin-native:endorsedLibraries:kotlinx.cli", configuration = "jvmRuntimeElements"))
     implementation(project(":native:kotlin-native-utils"))
+    implementation(project(":kotlin-native:common", configuration = "filesInteropStubs"))
+    implementation(project(":kotlin-native:common", configuration = "envInteropStubs"))
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions.optIn.addAll(
+            listOf(
+                    "kotlinx.cinterop.ExperimentalForeignApi",
+            )
+    )
 }
 
 sourceSets {
