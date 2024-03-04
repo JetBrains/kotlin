bc40d52c6dd00305b7cb25da62bdf150c8e39e50
Pavel Shishkin <pavel.shishkin@jetbrains.com>
2024-03-04 19:55:06 +0100

    compose-compiler: prevent composable objc export
    exporting protected composable functions for ObjC will lead to signature
    mismatches due to Composable functions transformation
    
    preventing composable functions with Composable annotation or
    CompositionLocal type from being exported to ObjC by marking their
    declarations with HiddenFromObjC annotation
    
    Test: tested against Compose Multiplatform testsuite
    
    Change-Id: I536af931334161c10689c9393b43d45d395c393c ( https://android-review.googlesource.com/q/I536af931334161c10689c9393b43d45d395c393c )
    Signed-off-by: Pavel Shishkin <pavel.shishkin@jetbrains.com>
    
    Moved from: https://github.com/androidx/androidx/commit/b5b1395a5ffd2a5ac841839532764f2ee4087c90

---
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/hiddenfromobjc/AddHiddenFromObjCLowering.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.compiler.plugins.kotlin.lower.hiddenfromobjc
+
+import androidx.compose.compiler.plugins.kotlin.ModuleMetrics
+import androidx.compose.compiler.plugins.kotlin.analysis.StabilityInferencer
+import androidx.compose.compiler.plugins.kotlin.lower.AbstractComposeLowering
+import androidx.compose.compiler.plugins.kotlin.lower.ComposableSymbolRemapper
+import androidx.compose.compiler.plugins.kotlin.lower.containsComposableAnnotation
+import androidx.compose.compiler.plugins.kotlin.lower.needsComposableRemapping
+import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.declarations.IrDeclaration
+import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.declarations.IrProperty
+import org.jetbrains.kotlin.ir.expressions.impl.IrConstructorCallImpl
+import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
+import org.jetbrains.kotlin.ir.types.defaultType
+import org.jetbrains.kotlin.ir.util.constructors
+import org.jetbrains.kotlin.ir.util.isLocal
+import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
+import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.platform.konan.isNative
+
+/**
+ *  AddHiddenFromObjCLowering looks for functions and properties with @Composable types and
+ *  adds the `kotlin.native.HiddenFromObjC` annotation to them.
+ *  @see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.native/-hidden-from-obj-c/
+ */
+class AddHiddenFromObjCLowering(
+    private val pluginContext: IrPluginContext,
+    symbolRemapper: ComposableSymbolRemapper,
+    metrics: ModuleMetrics,
+    private val hideFromObjCDeclarationsSet: HideFromObjCDeclarationsSet,
+    stabilityInferencer: StabilityInferencer
+) : AbstractComposeLowering(pluginContext, symbolRemapper, metrics, stabilityInferencer) {
+
+    private val hiddenFromObjCAnnotation: IrClassSymbol by lazy {
+        getTopLevelClass(ClassId.fromString("kotlin/native/HiddenFromObjC"))
+    }
+
+    override fun lower(module: IrModuleFragment) {
+        require(context.platform.isNative()) {
+            "AddHiddenFromObjCLowering is expected to run only for k/native. " +
+                "The platform - ${context.platform}"
+        }
+        module.transformChildrenVoid(this)
+    }
+
+    override fun visitFunction(declaration: IrFunction): IrStatement {
+        val f = super.visitFunction(declaration) as IrFunction
+        if (f.isLocal ||
+            !(f.visibility == DescriptorVisibilities.PUBLIC ||
+                f.visibility == DescriptorVisibilities.PROTECTED))
+            return f
+
+        if (f.hasComposableAnnotation() || f.needsComposableRemapping()) {
+            f.addHiddenFromObjCAnnotation()
+            hideFromObjCDeclarationsSet.add(f)
+        }
+
+        return f
+    }
+
+    override fun visitProperty(declaration: IrProperty): IrStatement {
+        val p = super.visitProperty(declaration) as IrProperty
+        if (p.isLocal || p.visibility != DescriptorVisibilities.PUBLIC) return p
+
+        val shouldAdd = p.getter?.hasComposableAnnotation() ?: false ||
+            p.getter?.needsComposableRemapping() ?: false ||
+            p.backingField?.type.containsComposableAnnotation()
+
+        if (shouldAdd) {
+            p.addHiddenFromObjCAnnotation()
+            hideFromObjCDeclarationsSet.add(p)
+        }
+
+        return p
+    }
+
+    private fun IrDeclaration.addHiddenFromObjCAnnotation() {
+        val annotation = IrConstructorCallImpl.fromSymbolOwner(
+            type = hiddenFromObjCAnnotation.defaultType,
+            constructorSymbol = hiddenFromObjCAnnotation.constructors.first()
+        )
+        pluginContext.metadataDeclarationRegistrar.addMetadataVisibleAnnotationsToElement(this, annotation)
+    }
+}
