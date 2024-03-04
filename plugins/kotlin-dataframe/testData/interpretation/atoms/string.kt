a1c0af57184f7d9f570aebdfdb7d61fef9a3472b
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-03-04 14:22:44 +0200

    [KDF] remove code generation that was needed to glue library and plugin together
    The idea was to verify that compile time evaluation and runtime evaluation of the same expressions give the same results.  To run the same code twice, i decided to use code generation. It also was a source of truth about mapping between dataframe function arguments and compile time FIR value that are expected.
    Code generation pipeline has proven to be a bit hard to maintain, so approach needs to be changed.
    
    Moved from https://github.com/Kotlin/dataframe/commit/f11b354de374b927e192e08c17630ab90fd56409

--- plugins/kotlin-dataframe/testData/interpretation/atoms/string.kt
+++
@@ -1,6 +0,0 @@
-import org.jetbrains.kotlinx.dataframe.plugin.testing.*
-import org.jetbrains.kotlinx.dataframe.plugin.testing.atoms.*
-
-fun injectionScope() {
-    test(id = "string_1", call = kotlinPrimitive("42"))
-}
