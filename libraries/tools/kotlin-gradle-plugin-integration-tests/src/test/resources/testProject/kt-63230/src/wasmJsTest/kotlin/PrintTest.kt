71a2adcf33eee49f46087f8144e412ca9d704e00
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2024-03-05 00:51:10 +0100

    [Gradle, JS] Fix reading logs from browser
    ^KT-63230 fixed

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-63230/src/wasmJsTest/kotlin/PrintTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+
+class PrintTest {
+    @Test
+    fun test() {
+        println("====\n")
+        println(null)
+        println("\n")
+        println("====\n")
+    }
+}
