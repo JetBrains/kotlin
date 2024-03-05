c41874212d29c71fa3d125e619e7529d65ec356e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-03-05 09:15:30 +0100

    K2: add a BB test confirming work of KT-63588

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenWithIrInlinerTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenWithIrInlinerTestGenerated.java
@@ -21526,6 +21526,12 @@ public class IrBlackBoxCodegenWithIrInlinerTestGenerated extends AbstractIrBlack
     }
 
     @Test
+    @TestMetadata("overriddenPropertyInComplexHierarchy.kt")
+    public void testOverriddenPropertyInComplexHierarchy() {
+      runTest("compiler/testData/codegen/box/fir/overriddenPropertyInComplexHierarchy.kt");
+    }
+
+    @Test
     @TestMetadata("publicMethodCallViaPackagePrivateCommon.kt")
     public void testPublicMethodCallViaPackagePrivateCommon() {
       runTest("compiler/testData/codegen/box/fir/publicMethodCallViaPackagePrivateCommon.kt");
