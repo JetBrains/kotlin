28c80a15810a5c42fe4f2f12b9ec0dc542e43e4f
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-03-05 16:22:56 +0100

    [LL FIR] StubBasedFirMemberDeserializer: add missed deprecation provider
    ^KT-60996 Fixed

--- compiler/testData/diagnostics/testsWithStdLib/typealias/exceptionTypeAliasesInvisibleWithApiVersion1_0.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/typealias/exceptionTypeAliasesInvisibleWithApiVersion1_0.fir.kt
@@ -1,8 +1,5 @@
 // !LANGUAGE: +TypeAliases
 // !API_VERSION: 1.0
-// IGNORE_DIAGNOSTIC_API
-// IGNORE_REVERSED_RESOLVE
-// ^KT-60996
 // FILE: test.kt
 val fooException = <!API_NOT_AVAILABLE!>Exception<!>("foo")
 val barException = kotlin.<!API_NOT_AVAILABLE!>Exception<!>("bar")
