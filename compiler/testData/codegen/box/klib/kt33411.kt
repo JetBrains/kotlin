d6a040954eef23edad07a983bddbe76cc83e459a
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2024-03-05 12:13:41 +0100

    fixup! [Tests] Move passing test for KT-33411 to better place

--- compiler/testData/codegen/box/klib/kt33411.kt
+++ compiler/testData/codegen/box/klib/kt33411.kt
@@ -1,17 +1,13 @@
-// KT-33411: Overload resolution ambiguity:
-// public fun getOK(): String defined in root package
-// public fun getOK(): String defined in root package
-// IGNORE_BACKEND_K1: JVM, JVM_IR
-
 // MODULE: m1
 // FILE: m1.kt
-fun getOK() = "OK"
+fun f() {}
+fun getO() = "O"
 
 // MODULE: m2
 // FILE: m2.kt
-fun getOK() = "OK"
+fun f() {}
+fun getK() = "K"
 
 // MODULE: main(m1)(m2)
 // FILE: main.kt
-
-fun box() = getOK()
+fun box() = getO() + getK()
