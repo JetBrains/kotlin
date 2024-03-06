47546ece2751f7bbcf512743b0b4aa50a8aa56a5
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2024-03-06 18:29:21 +0100

    [tests] Adjust MppDiagnosticsIt after removing deprecated K/N targets (8/8)
    Tests were using deprecated targets to produce ERROR-diagnostics.
    Replace it with a different diagnostic
    (CommonMainOrTestWithDependsOnEdges)
    
    ^KT-64517

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/errorsFailOnlyRelevantProjects/brokenProjectA/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/errorsFailOnlyRelevantProjects/brokenProjectA/build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
 }
 
 kotlin {
-    // Using deprecated target to provoke ERROR-diagnostic. If you came here because you removed
-    // that target entirely, just replace it with any other construction from KGP that provokes
-    // ERROR-diagnostic
-    @Suppress("DEPRECATION_ERROR")
-    linuxMips32()
+    linuxX64()
+    jvm()
+
+    sourceSets {
+        val myCustomSourceSet by creating
+        commonMain.get().dependsOn(myCustomSourceSet)
+    }
 }
