b0b60f99d027a60e537f99efdb6cec2f0e9d1019
Adam <152864218+adam-enko@users.noreply.github.com>
2024-03-12 17:30:08 +0100

    [KDM] consistently apply Gradle Build Cache config in all includedBuild projects
    See https://github.com/Kotlin/dokka/pull/3522
    
    * consistently apply Gradle Build Cache config in all includedBuild projects
    
    * hardcode Build Scan URL (unrelated change, but tidying up while we're here)
    
    The build scan URL is configurable because it used to control whether someone could opt in/out of build scans, but since then an explicit option has been added, so it makes sense to hardcode the build scan url.
    
    * add warning about build-cache script plugin requirement
    Original commit: https://github.com/Kotlin/dokka/commit/43b2201a3894c460286df8e2589b180d19a615fb

--- libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/settings.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/settings.gradle.kts
@@ -4,10 +4,6 @@
 
 import org.gradle.api.initialization.resolve.RepositoriesMode.PREFER_SETTINGS
 
-/*
- * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
 rootProject.name = "build-settings-logic"
 
 pluginManagement {
@@ -35,3 +31,10 @@ dependencyResolutionManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
+
+// We have to make sure the build-cache config is consistent in the settings.gradle.kts
+// files of all projects. The natural way to share logic is with a convention plugin,
+// but we can't apply a convention plugin in build-settings-logic to itself, unless we
+// do it with a dumb hack:
+apply(from = "src/main/kotlin/dokkasettings.build-cache.settings.gradle.kts")
+// We could publish build-settings-logic to a Maven repo? But this is quicker and easier.
