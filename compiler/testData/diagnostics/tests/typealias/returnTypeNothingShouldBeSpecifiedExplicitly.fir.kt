dc634c037f486294cec7ba3bb8dd365dee70a73d
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-03-14 15:27:38 +0200

    [FIR] Fix FirImplicitNothingReturnTypeChecker

--- compiler/testData/diagnostics/tests/typealias/returnTypeNothingShouldBeSpecifiedExplicitly.fir.kt
+++
@@ -1,9 +0,0 @@
-typealias N = Nothing
-
-fun testFun(): N = null!!
-val testVal: N = null!!
-val testValWithGetter: N get() = null!!
-
-fun testFunN(): Nothing = null!!
-val testValN: Nothing = null!!
-val testValWithGetterN: Nothing get() = null!!
