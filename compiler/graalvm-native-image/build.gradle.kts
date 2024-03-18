53d830d22a556dbd04858ac94bc422d8a1d2fd06
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-03-18 09:40:50 +0100

    [Native Image] Add GraalVM gradle plugin to build Native Image
    ^KT-66666

---
+++ compiler/graalvm-native-image/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("org.graalvm.buildtools.native") version "0.10.1"
+    kotlin("jvm")
+}
+
+graalvmNative {
+    toolchainDetection = true
+
+    agent {
+        defaultMode = "standard"
+    }
+
+    binaries {
+        val main by getting {
+            imageName = "kotlinc-jvm"
+            mainClass = "org.jetbrains.kotlin.cli.jvm.K2JVMCompiler"
+
+            buildArgs(
+                "-H:+UnlockExperimentalVMOptions",
+                "-H:+AllowJRTFileSystem",
+                "-H:+AddAllCharsets",
+                "--strict-image-heap",
+                "-march=native"
+            )
+
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+                vendor.set(JvmVendorSpec.matching("Oracle Corporation"))
+            })
+        }
+    }
+}
+
+afterEvaluate {
+    dependencies {
+        "nativeImageClasspath".invoke(project(":kotlin-compiler"))
+    }
+}
