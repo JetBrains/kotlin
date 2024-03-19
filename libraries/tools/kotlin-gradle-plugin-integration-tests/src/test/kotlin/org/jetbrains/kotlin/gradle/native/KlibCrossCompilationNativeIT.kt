dfeff9cb503c3f3532585074d59dbcf0f202121f
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2024-03-19 18:34:08 +0100

    [kgp, native, tests] Add Gradle IT on klibs crosscompilation
    So far, no changes have been done on Gradle side, so it is expected
    that the relevant targets will be disabled for cross-compilation

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/KlibCrossCompilationNativeIT.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.native
+
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.test.KotlinTestUtils
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.condition.OS
+
+@DisplayName("Tests for checking klib cross-compilation in KGP")
+@NativeGradlePluginTests
+class KlibCrossCompilationNativeIT : KGPBaseTest() {
+
+    @GradleTest
+    @TestMetadata("klibCrossCompilationDefaultSettings")
+    @OsCondition(supportedOn = [OS.LINUX, OS.WINDOWS], enabledOnCI = [OS.LINUX, OS.WINDOWS])
+    fun compileIosTargetOnNonDarwinHostWithDefaultSettings(gradleVersion: GradleVersion) {
+        nativeProject("klibCrossCompilationDefaultSettings", gradleVersion) {
+            build(":compileKotlinIosArm64") {
+                KotlinTestUtils.assertEqualsToFile(projectPath.resolve("diagnostics.txt"), extractProjectsAndTheirDiagnostics())
+                assertTasksSkipped(":compileKotlinIosArm64")
+            }
+        }
+    }
+}
