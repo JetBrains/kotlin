2ea4876a0b80a326e1df0f9279f4b2c8f22fa4a3
Artem Daugel-Dauge <artem.daugel-dauge@jetbrains.com>
2024-03-20 22:48:26 +0100

    [Native] Update set of targets in the Xcode update scripts
    ^KT-64298

--- kotlin-native/platformLibs/src/platform/tvos/set_depends.sh
+++ kotlin-native/platformLibs/src/platform/tvos/set_depends.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../../../../dist/bin/run_konan defFileDependencies -target tvos_arm64 -target tvos_x64 *.def
+../../../../dist/bin/run_konan defFileDependencies -target tvos_arm64 -target tvos_x64 -target tvos_simulator_arm64 *.def
--- kotlin-native/platformLibs/src/platform/watchos/set_depends.sh
+++ kotlin-native/platformLibs/src/platform/watchos/set_depends.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ../../../../dist/bin/run_konan defFileDependencies \
   -target watchos_arm32 \
-  -target watchos_x86 \
   -target watchos_arm64 \
   -target watchos_x64 \
   -target watchos_simulator_arm64 \
