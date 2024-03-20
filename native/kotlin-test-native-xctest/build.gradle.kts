7ae88d62ce07a6b157d3284e5f7a0cad465b22e4
Pavel Punegov <pavel.punegov@jetbrains.com>
2024-03-20 16:46:50 +0100

    [K/N][xctest] Make multiplatform plugin initialize targets
    Check for mac makes KGP issue warnings that no targets initialized.
    There is no need to have this check.

--- native/kotlin-test-native-xctest/build.gradle.kts
+++ native/kotlin-test-native-xctest/build.gradle.kts
@@ -4,7 +4,6 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinUsages
 import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
 import org.jetbrains.kotlin.konan.target.*
 import java.io.ByteArrayOutputStream
-import java.nio.file.Paths
 
 description = "XCTest wrapper of Native kotlin.test"
 
@@ -97,37 +96,35 @@ fun registerCopyFrameworkTask(target: KonanTarget): TaskProvider<Sync> =
 
 val nativeTargets = mutableListOf<KotlinNativeTarget>()
 
-if (HostManager.hostIsMac) {
-    kotlin {
-        with(nativeTargets) {
-            add(macosX64())
-            add(macosArm64())
-            add(iosX64())
-            add(iosArm64())
-            add(iosSimulatorArm64())
+kotlin {
+    with(nativeTargets) {
+        add(macosX64())
+        add(macosArm64())
+        add(iosX64())
+        add(iosArm64())
+        add(iosSimulatorArm64())
 
-            forEach {
-                val copyTask = registerCopyFrameworkTask(it.konanTarget)
-                it.compilations.all {
-                    cinterops {
-                        register("XCTest") {
-                            compilerOpts("-iframework", copyTask.map { it.destinationDir }.get().absolutePath)
-                            // cinterop task should depend on the framework copy task
-                            tasks.named(interopProcessingTaskName).configure {
-                                dependsOn(copyTask)
-                            }
+        forEach {
+            val copyTask = registerCopyFrameworkTask(it.konanTarget)
+            it.compilations.all {
+                cinterops {
+                    register("XCTest") {
+                        compilerOpts("-iframework", copyTask.map { it.destinationDir }.get().absolutePath)
+                        // cinterop task should depend on the framework copy task
+                        tasks.named(interopProcessingTaskName).configure {
+                            dependsOn(copyTask)
                         }
                     }
                 }
             }
         }
-        sourceSets.all {
-            languageSettings.apply {
-                // Oh, yeah! So much experimental, so wow!
-                optIn("kotlinx.cinterop.BetaInteropApi")
-                optIn("kotlinx.cinterop.ExperimentalForeignApi")
-                optIn("kotlin.experimental.ExperimentalNativeApi")
-            }
+    }
+    sourceSets.all {
+        languageSettings.apply {
+            // Oh, yeah! So much experimental, so wow!
+            optIn("kotlinx.cinterop.BetaInteropApi")
+            optIn("kotlinx.cinterop.ExperimentalForeignApi")
+            optIn("kotlin.experimental.ExperimentalNativeApi")
         }
     }
 }
