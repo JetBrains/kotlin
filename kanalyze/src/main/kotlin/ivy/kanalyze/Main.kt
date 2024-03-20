7cbf78b4fec6dcdb73e80c600115fdbdef7272eb
iliyangermanov <iliyan.germanov971@gmail.com>
2024-03-20 21:48:36 +0200

    Extract functions

--- kanalyze/src/main/kotlin/ivy/kanalyze/Main.kt
+++ kanalyze/src/main/kotlin/ivy/kanalyze/Main.kt
@@ -4,11 +4,15 @@
  */
 package ivy.kanalyze
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.PsiRecursiveElementVisitor
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
 fun main() {
     println("Hello")
@@ -23,11 +27,37 @@ fun main() {
             fun main() {
               println("Hello, world!")
             }
+            
+            fun test() = 42
+            
+            fun okay() {
+                main()
+            }
         """.trimIndent()
     val file = PsiFileFactory.getInstance(environment.project)
         .createFileFromText("MyFile.kt", KotlinLanguage.INSTANCE, code)
+        ?: error("[IVY] File is null! Failed to parse file!")
 
-    file.children.forEach {
-        println(it.text)
+    extractFunctions(file).forEach {
+        println(it.name)
     }
+}
+
+fun extractFunctions(psiFile: PsiFile): List<KtNamedFunction> {
+    // List to hold all found functions
+    val functions = mutableListOf<KtNamedFunction>()
+
+    // Recursively visit all children and sub-children of the file
+    psiFile.acceptChildren(object : PsiRecursiveElementVisitor() {
+        override fun visitElement(element: PsiElement) {
+            // If the element is a KtNamedFunction, add it to the list
+            if (element is KtNamedFunction) {
+                functions.add(element)
+            }
+            // Continue recursively visiting children
+            super.visitElement(element)
+        }
+    })
+
+    return functions
 }
