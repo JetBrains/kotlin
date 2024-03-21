e4f4ad44965c5c339cdec557b8e6b1672715e031
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-03-21 12:06:51 +0100

    [Gradle] Change project to rootProject in commonize task provider
    In commonize task provider we should use `rootProject`
    for configuring Kotlin Native toolchain.
    Subproject's properties could differ from root project's properties,
    but `commonizeNativeDistributionTask` task is connected with root.
    
    ^KT-66750 Fixed

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/KotlinNativeToolchainIT.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.native
+
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("This test class contains base scenarios for testing Kotlin Native toolchain feature")
+@NativeGradlePluginTests
+class KotlinNativeToolchainIT : KGPBaseTest() {
+
+
+    @DisplayName(
+        "KT-66750: check that disabled native toolchain flag in subproject does not affect root project"
+    )
+    @GradleTest
+    fun checkCommonizeNativeDistributionWithPlatform(gradleVersion: GradleVersion) {
+        nativeProject("commonize-kt-66750-with-subproject", gradleVersion) {
+            build(":commonizeNativeDistribution")
+        }
+    }
+
+}
---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/commonize-kt-66750-with-subproject/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    kotlin("multiplatform") apply false
+}
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
