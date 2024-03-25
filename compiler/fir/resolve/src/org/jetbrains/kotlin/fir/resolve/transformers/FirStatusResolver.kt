58d730fd1d85d426f4ea07115986665657cec97a
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-03-25 13:58:03 +0100

    [FIR] Cleanup: Drop redundant when branch
    else branch covers this redundant branch

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirStatusResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirStatusResolver.kt
@@ -250,7 +250,6 @@ class FirStatusResolver(
                 isLocal -> Visibilities.Local
                 else -> resolveVisibility(declaration, containingClass, containingProperty, overriddenStatuses)
             }
-            Visibilities.Private -> Visibilities.Private
             else -> status.visibility
         }
 
