6bd824e539a2e6146b9e68a5e9715aec01c58613
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2024-03-27 19:28:51 +0100

    [Infrastructure] Limit tiered compilation 2
    #KTI-1609

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
@@ -217,7 +217,7 @@ fun Project.projectTest(
             "-XX:ReservedCodeCacheSize=${reservedCodeCacheSizeMb}m",
             "-XX:MaxMetaspaceSize=${maxMetaspaceSizeMb}m",
             "-Djna.nosys=true",
-            "-XX:TieredStopAtLevel=1",
+            "-XX:TieredStopAtLevel=2",
         )
 
         val nativeMemoryTracking = project.providers.gradleProperty("kotlin.build.test.process.NativeMemoryTracking")
