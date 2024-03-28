2be1edf9242f69b950a15d49ea35c77eed7535cc
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-03-28 16:05:50 +0100

    Reapply "Partially revert "[IR] Hide constructors of implementation classes behind factory functions""
    This reverts commit ebe775bb895c9982e5fe5de6dda9bed665206d48.
    
    Apparently, the compatibility with compiler plugins used in
    user projects still needs to be preserved. Restore it here.

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/impl/IrCompositeImpl.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/expressions/impl/IrCompositeImpl.kt
@@ -10,47 +10,25 @@ import org.jetbrains.kotlin.ir.declarations.IrAttributeContainer
 import org.jetbrains.kotlin.ir.expressions.IrComposite
 import org.jetbrains.kotlin.ir.expressions.IrStatementOrigin
 import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.util.IrElementConstructorIndicator
 
-class IrCompositeImpl internal constructor(
-    @Suppress("UNUSED_PARAMETER")
-    constructorIndicator: IrElementConstructorIndicator?,
+class IrCompositeImpl(
     override val startOffset: Int,
     override val endOffset: Int,
     override var type: IrType,
-    override var origin: IrStatementOrigin?,
+    override var origin: IrStatementOrigin? = null,
 ) : IrComposite() {
     override val statements: MutableList<IrStatement> = ArrayList(2)
 
     override var attributeOwnerId: IrAttributeContainer = this
     override var originalBeforeInline: IrAttributeContainer? = null
-}
 
-fun IrCompositeImpl(
-    startOffset: Int,
-    endOffset: Int,
-    type: IrType,
-    origin: IrStatementOrigin? = null,
-) = IrCompositeImpl(
-    constructorIndicator = null,
-    startOffset = startOffset,
-    endOffset = endOffset,
-    type = type,
-    origin = origin,
-)
-
-fun IrCompositeImpl(
-    startOffset: Int,
-    endOffset: Int,
-    type: IrType,
-    origin: IrStatementOrigin?,
-    statements: List<IrStatement>,
-) = IrCompositeImpl(
-    constructorIndicator = null,
-    startOffset = startOffset,
-    endOffset = endOffset,
-    type = type,
-    origin = origin,
-).apply {
-    this.statements.addAll(statements)
+    constructor(
+        startOffset: Int,
+        endOffset: Int,
+        type: IrType,
+        origin: IrStatementOrigin?,
+        statements: List<IrStatement>,
+    ) : this(startOffset, endOffset, type, origin) {
+        this.statements.addAll(statements)
+    }
 }
