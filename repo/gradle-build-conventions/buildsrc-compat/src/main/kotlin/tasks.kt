4a4f72d679b5b9be01e30856cc5d7c55a5f74088
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2024-03-28 10:53:08 +0100

    Build: Set -XX:CICompilerCount=2 for test processes
     #KTI-1609

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
@@ -216,6 +216,7 @@ fun Project.projectTest(
             "-XX:+UseCodeCacheFlushing",
             "-XX:ReservedCodeCacheSize=${reservedCodeCacheSizeMb}m",
             "-XX:MaxMetaspaceSize=${maxMetaspaceSizeMb}m",
+            "-XX:CICompilerCount=2",
             "-Djna.nosys=true"
         )
 
