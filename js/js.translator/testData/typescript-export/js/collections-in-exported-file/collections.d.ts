fef4db14d090dd45fffb3d55df104e0d75b9dba9
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-03-28 15:22:18 +0000

    [K/JS] Export ability to convert JS collections into Kotlin collections
    fixup!: use build instead of the custom internal method
    
    fixup!: rework the solution with the hidden companion object + extension methods for convesion of the JS collections into Kotlin ones
    
    fixup!: set SinceKotlin(2.0) for the collections interop
    
    fixup!: version of the compiler that will contain the collection interop + add comment with the reason of collectionsWithoutExportLogic.kt exists
    
    fixup: add new companions inside the API test files
    
    [K/JS] Export ability to convert JS collections into Kotlin collections
    
    
    Merge-request: KT-MR-14738
    Merged-by: Artem Kobzar <Artem.Kobzar@jetbrains.com>

--- js/js.translator/testData/typescript-export/js/collections-in-exported-file/collections.d.ts
+++ js/js.translator/testData/typescript-export/js/collections-in-exported-file/collections.d.ts
@@ -7,36 +7,54 @@ declare namespace JS_TESTS {
                 readonly "kotlin.collections.KtList": unique symbol;
             };
         }
+        const KtList: {
+            fromJsArray<E>(array: ReadonlyArray<E>): kotlin.collections.KtList<E>;
+        };
         interface KtMap<K, V> {
             asJsReadonlyMapView(): ReadonlyMap<K, V>;
             readonly __doNotUseOrImplementIt: {
                 readonly "kotlin.collections.KtMap": unique symbol;
             };
         }
+        const KtMap: {
+            fromJsMap<K, V>(map: ReadonlyMap<K, V>): kotlin.collections.KtMap<K, V>;
+        };
         interface KtMutableList<E> extends kotlin.collections.KtList<E>/*, kotlin.collections.MutableCollection<E> */ {
             asJsArrayView(): Array<E>;
             readonly __doNotUseOrImplementIt: {
                 readonly "kotlin.collections.KtMutableList": unique symbol;
             } & kotlin.collections.KtList<E>["__doNotUseOrImplementIt"];
         }
+        const KtMutableList: {
+            fromJsArray<E>(array: ReadonlyArray<E>): kotlin.collections.KtMutableList<E>;
+        };
         interface KtSet<E> /* extends kotlin.collections.Collection<E> */ {
             asJsReadonlySetView(): ReadonlySet<E>;
             readonly __doNotUseOrImplementIt: {
                 readonly "kotlin.collections.KtSet": unique symbol;
             };
         }
+        const KtSet: {
+            fromJsSet<E>(set: ReadonlySet<E>): kotlin.collections.KtSet<E>;
+        };
         interface KtMutableSet<E> extends kotlin.collections.KtSet<E>/*, kotlin.collections.MutableCollection<E> */ {
             asJsSetView(): Set<E>;
             readonly __doNotUseOrImplementIt: {
                 readonly "kotlin.collections.KtMutableSet": unique symbol;
             } & kotlin.collections.KtSet<E>["__doNotUseOrImplementIt"];
         }
+        const KtMutableSet: {
+            fromJsSet<E>(set: ReadonlySet<E>): kotlin.collections.KtMutableSet<E>;
+        };
         interface KtMutableMap<K, V> extends kotlin.collections.KtMap<K, V> {
             asJsMapView(): Map<K, V>;
             readonly __doNotUseOrImplementIt: {
                 readonly "kotlin.collections.KtMutableMap": unique symbol;
             } & kotlin.collections.KtMap<K, V>["__doNotUseOrImplementIt"];
         }
+        const KtMutableMap: {
+            fromJsMap<K, V>(map: ReadonlyMap<K, V>): kotlin.collections.KtMutableMap<K, V>;
+        };
     }
     function provideList(): kotlin.collections.KtList<number>;
     function provideMutableList(): kotlin.collections.KtMutableList<number>;
