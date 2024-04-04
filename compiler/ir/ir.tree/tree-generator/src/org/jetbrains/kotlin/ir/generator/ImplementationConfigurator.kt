ecabd37710a1e3301bbe927375a50f0dcb555c22
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-04-04 13:41:24 +0200

    [IR] Autogenerate IrConstImpl

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/ImplementationConfigurator.kt
@@ -365,6 +365,74 @@ object ImplementationConfigurator : AbstractIrTreeImplementationConfigurator() {
                 println("companion object")
             }
         }
+
+        impl(const) {
+            additionalImports(
+                ArbitraryImportable("org.jetbrains.kotlin.builtins", "PrimitiveType"),
+                ArbitraryImportable("org.jetbrains.kotlin.ir.expressions", "IrConstKind"),
+                ArbitraryImportable("org.jetbrains.kotlin.ir.types", "getPrimitiveType"),
+                ArbitraryImportable("org.jetbrains.kotlin.ir.types", "makeNullable"),
+                ArbitraryImportable("org.jetbrains.kotlin.ir.types", "isMarkedNullable"),
+            )
+            implementation.generationCallback = {
+                println()
+                print()
+                println("""
+                    companion object {
+                        fun string(startOffset: Int, endOffset: Int, type: IrType, value: String): IrConstImpl<String> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.String, value)
+                
+                        fun int(startOffset: Int, endOffset: Int, type: IrType, value: Int): IrConstImpl<Int> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Int, value)
+                
+                        fun constNull(startOffset: Int, endOffset: Int, type: IrType): IrConstImpl<Nothing?> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Null, null)
+                
+                        fun boolean(startOffset: Int, endOffset: Int, type: IrType, value: Boolean): IrConstImpl<Boolean> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Boolean, value)
+                
+                        fun constTrue(startOffset: Int, endOffset: Int, type: IrType): IrConstImpl<Boolean> =
+                            boolean(startOffset, endOffset, type, true)
+                
+                        fun constFalse(startOffset: Int, endOffset: Int, type: IrType): IrConstImpl<Boolean> =
+                            boolean(startOffset, endOffset, type, false)
+                
+                        fun long(startOffset: Int, endOffset: Int, type: IrType, value: Long): IrConstImpl<Long> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Long, value)
+                
+                        fun float(startOffset: Int, endOffset: Int, type: IrType, value: Float): IrConstImpl<Float> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Float, value)
+                
+                        fun double(startOffset: Int, endOffset: Int, type: IrType, value: Double): IrConstImpl<Double> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Double, value)
+                
+                        fun char(startOffset: Int, endOffset: Int, type: IrType, value: Char): IrConstImpl<Char> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Char, value)
+                
+                        fun byte(startOffset: Int, endOffset: Int, type: IrType, value: Byte): IrConstImpl<Byte> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Byte, value)
+                
+                        fun short(startOffset: Int, endOffset: Int, type: IrType, value: Short): IrConstImpl<Short> =
+                            IrConstImpl(startOffset, endOffset, type, IrConstKind.Short, value)
+                
+                        fun defaultValueForType(startOffset: Int, endOffset: Int, type: IrType): IrConstImpl<*> {
+                            if (type.isMarkedNullable()) return constNull(startOffset, endOffset, type)
+                            return when (type.getPrimitiveType()) {
+                                PrimitiveType.BOOLEAN -> boolean(startOffset, endOffset, type, false)
+                                PrimitiveType.CHAR -> char(startOffset, endOffset, type, 0.toChar())
+                                PrimitiveType.BYTE -> byte(startOffset, endOffset, type, 0)
+                                PrimitiveType.SHORT -> short(startOffset, endOffset, type, 0)
+                                PrimitiveType.INT -> int(startOffset, endOffset, type, 0)
+                                PrimitiveType.FLOAT -> float(startOffset, endOffset, type, 0.0F)
+                                PrimitiveType.LONG -> long(startOffset, endOffset, type, 0)
+                                PrimitiveType.DOUBLE -> double(startOffset, endOffset, type, 0.0)
+                                else -> constNull(startOffset, endOffset, type.makeNullable())
+                            }
+                        }
+                    }
+                """.replaceIndent(currentIndent))
+            }
+        }
     }
 
     private fun ImplementationContext.configureDeclarationWithLateBindinig(symbolType: ClassRef<*>) {
@@ -418,10 +486,6 @@ object ImplementationConfigurator : AbstractIrTreeImplementationConfigurator() {
 
         for (element in model.elements) {
             for (implementation in element.implementations) {
-                if (element == IrTree.const) {
-                    implementation.doPrint = false
-                }
-
                 if (element.category == Element.Category.Expression) {
                     implementation.isConstructorPublic = false
                 }
