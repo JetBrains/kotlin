1c9bf64c5f73f5b55ce2235a2b7630bd996fb214
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-04-04 16:55:59 +0200

    [Analysis API Standalone] KtSourceModuleImpl: add implementation for stableModuleName
    In the Standalone mode,
    we assume that all module names are stable
    
    ^KT-66713 Fixed

--- analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtSourceModuleImpl.kt
+++ analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/impl/KtSourceModuleImpl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -26,6 +26,7 @@ internal class KtSourceModuleImpl(
     override val languageVersionSettings: LanguageVersionSettings,
     internal val sourceRoots: List<PsiFileSystemItem>,
 ) : KtSourceModule, KtModuleWithPlatform {
+    override val stableModuleName: String? get() = moduleName
     override val transitiveDependsOnDependencies: List<KtModule> by lazy { computeTransitiveDependsOnDependencies(directDependsOnDependencies) }
     override val analyzerServices: PlatformDependentAnalyzerServices = super.analyzerServices
 }
