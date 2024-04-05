89613927eadc6145549bbb5f90056d601950c69d
Anastasia.Nekrasova <anastasia.nekrasova@jetbrains.com>
2024-04-06 01:18:32 +0300

    K2: investigate early extension receiver type check
    fixed tests
    
    #KT-55414

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/properties/backingField/propertyTypeNarrowing.reversed.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/properties/backingField/propertyTypeNarrowing.reversed.kt
@@ -2,14 +2,14 @@ class A {
     val it: Number
         field = 4
 
-    fun test() = it <!UNRESOLVED_REFERENCE!>+<!> 3
+    fun test() = it <!NONE_APPLICABLE!>+<!> 3
 
     val p = 5
         get() = field
 }
 
 fun test() {
-    val c = A().it <!UNRESOLVED_REFERENCE!>+<!> 1
+    val c = A().it <!NONE_APPLICABLE!>+<!> 1
     val d = test()
     val b = A().p + 2
 }
