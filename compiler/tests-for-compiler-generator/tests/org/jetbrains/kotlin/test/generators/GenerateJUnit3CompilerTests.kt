f2e964bd2eb6b1356cee20926b3148fad8b328f7
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-04-08 13:12:13 +0200

    Tests: add CLI tests for non-trivial source files in args
    Tests how we process mixed and non-source files then passed in args.
    Note that for non-script tests, the reported error about missing
    support of LightTree for scripts is a false positive, described in
    #KT-65451
    The same error reported for the script case is reported in #KT-60127
    
    (cherry picked from commit 5f50b6a6c3aad28039008ecf32e1f8a744ccef0d)

--- compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/GenerateJUnit3CompilerTests.kt
+++ compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/GenerateJUnit3CompilerTests.kt
@@ -233,6 +233,7 @@ fun generateJUnit3CompilerTests(args: Array<String>, mainClassName: String?) {
                 model("cli/jvm/readingConfigFromEnvironment", extension = "args", testMethod = "doJvmTest", recursive = false)
                 model("cli/jvm/plugins", extension = "args", testMethod = "doJvmTest", recursive = false)
                 model("cli/jvm/hmpp", extension = "args", testMethod = "doJvmTest", recursive = false)
+                model("cli/jvm/sourceFilesAndDirectories", extension = "args", testMethod = "doJvmTest", recursive = false)
                 model("cli/jvm", extension = "args", testMethod = "doJvmTest", recursive = false)
                 model("cli/js", extension = "args", testMethod = "doJsTest", recursive = false)
                 model("cli/js-dce", extension = "args", testMethod = "doJsDceTest", recursive = false)
