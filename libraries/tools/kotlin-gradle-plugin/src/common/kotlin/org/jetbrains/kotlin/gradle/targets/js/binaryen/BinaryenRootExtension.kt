300dc293dd1db1230ddd6f820c7692fceb01e047
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-04-08 13:42:18 +0000

    [K/Wasm] Update binaryen version

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/binaryen/BinaryenRootExtension.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/binaryen/BinaryenRootExtension.kt
@@ -24,7 +24,7 @@ open class BinaryenRootExtension(
 
     override var installationDir by Property(gradleHome.resolve("binaryen"))
     override var downloadBaseUrl: String? by Property("https://github.com/WebAssembly/binaryen/releases/download")
-    override var version: String by Property("116")
+    override var version: String by Property("117")
     override var download: Boolean by Property(true)
     override var command: String by Property("wasm-opt")
 
