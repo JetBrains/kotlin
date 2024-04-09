f73561f00a3f47107398e9108d4bccdb947220ca
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-04-09 13:47:21 +0200

    K2: allow Java annotations to be used on expressions
    #KT-67014 Fixed
    
    (cherry picked from commit 48432d14da8d2634710142302d9e301ed869bd69)

--- compiler/testData/diagnostics/tests/annotations/AnnotatedStatement.fir.kt
+++ compiler/testData/diagnostics/tests/annotations/AnnotatedStatement.fir.kt
@@ -8,20 +8,20 @@ public @interface JavaAnn {}
 annotation class KotlinAnn
 
 fun foo(list: MutableList<Int>) {
-    <!WRONG_ANNOTATION_TARGET!>@JavaAnn<!> <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
+    @JavaAnn <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
     when { else -> {} }
 
     @JavaAnn @KotlinAnn
     while (true) { break }
 
-    <!WRONG_ANNOTATION_TARGET!>@JavaAnn<!> <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
+    @JavaAnn <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
     if (true) {}
 
     var x = 1
 
-    <!WRONG_ANNOTATION_TARGET!>@JavaAnn<!> <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
+    @JavaAnn <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
     x += 2
 
-    <!WRONG_ANNOTATION_TARGET!>@JavaAnn<!> <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
+    @JavaAnn <!WRONG_ANNOTATION_TARGET!>@KotlinAnn<!>
     list += 2
 }
