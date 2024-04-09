d55f2862af45a25282d0450a57c75a227fc291b6
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-04-09 15:26:15 +0200

    fixup!: ignore tests only on CI and allow them to be run locally

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinWasmGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinWasmGradlePluginIT.kt
@@ -13,6 +13,7 @@ import java.nio.file.Files
 import kotlin.io.path.writeText
 import kotlin.test.assertTrue
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.condition.OS
 
 @MppGradlePluginTests
 class KotlinWasmGradlePluginIT : KGPBaseTest() {
@@ -226,7 +227,10 @@ class KotlinWasmGradlePluginIT : KGPBaseTest() {
     @DisplayName("Browser print works with null type")
     @GradleTest
     @GradleTestVersions(minVersion = TestVersions.Gradle.G_7_6)
-    @Disabled("The CI can't run the test, so, ignore it until we add headless Chrome browser to our CI")
+    @OsCondition(
+        supportedOn = [OS.LINUX, OS.MAC, OS.WINDOWS],
+        enabledOnCI = []
+    ) // The CI can't run the test, so, ignore it until we add headless Chrome browser to our CI
     fun testBrowserNullPrint(gradleVersion: GradleVersion) {
         project("kt-63230", gradleVersion) {
             build("check", "-Pkotlin.tests.individualTaskReports=true") {
