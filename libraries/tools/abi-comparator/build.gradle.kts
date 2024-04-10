24e57272b7ecd781530e5d6de72dbc9a0d1d3835
aleksandr.shalygin <aleksandr.shalygin@jetbrains.com>
2024-04-10 15:52:37 +0200

    AbiComparator: add CLI tooling

--- libraries/tools/abi-comparator/build.gradle.kts
+++ libraries/tools/abi-comparator/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("jps-compatible")
+    application
 }
 
 dependencies {
@@ -12,4 +13,19 @@ dependencies {
     implementation(project(":kotlin-metadata"))
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+runtimeJar {
+    manifest.attributes["Main-Class"] = "org.jetbrains.kotlin.abicmp.AbiComparatorMain"
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+             configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+         })
+}
+
+application {
+    mainClass = "org.jetbrains.kotlin.abicmp.AbiComparatorMain"
 }
