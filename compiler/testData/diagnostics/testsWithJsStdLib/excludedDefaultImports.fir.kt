97e937016f2401632745174b5b201043f1e745e2
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-04-10 16:51:29 +0200

    [Tests] Reproduce #KT-67315

---
+++ compiler/testData/diagnostics/testsWithJsStdLib/excludedDefaultImports.fir.kt
@@ -0,0 +1,12 @@
+// DIAGNOSTICS: -UNUSED_PARAMETER
+
+fun foo(
+    p: Promise<*>,
+    d: Date,
+    c: Console,
+    r: RegExp,
+    rm: RegExpMatch,
+    j: Json,
+) {
+    json()
+}
--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirLightTreeJsOldFrontendDiagnosticsTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirLightTreeJsOldFrontendDiagnosticsTestGenerated.java
@@ -38,6 +38,12 @@ public class FirLightTreeJsOldFrontendDiagnosticsTestGenerated extends AbstractF
   }
 
   @Test
+  @TestMetadata("excludedDefaultImports.kt")
+  public void testExcludedDefaultImports() {
+    runTest("compiler/testData/diagnostics/testsWithJsStdLib/excludedDefaultImports.kt");
+  }
+
+  @Test
   @TestMetadata("externalInterfaceClassLiteral.kt")
   public void testExternalInterfaceClassLiteral() {
     runTest("compiler/testData/diagnostics/testsWithJsStdLib/externalInterfaceClassLiteral.kt");
