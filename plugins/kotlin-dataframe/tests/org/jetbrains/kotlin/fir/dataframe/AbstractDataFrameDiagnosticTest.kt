fbea007ebf0a446d163dc9b079b7266d8b371c63
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-04-10 14:35:19 +0300

    [KDF] refactor test runner to use default setup from compiler
    Moved from https://github.com/Kotlin/dataframe/commit/8b17f31c56b6a44a045c3146c25a65685bab784e

--- plugins/kotlin-dataframe/tests/org/jetbrains/kotlin/fir/dataframe/AbstractDataFrameDiagnosticTest.kt
+++ plugins/kotlin-dataframe/tests/org/jetbrains/kotlin/fir/dataframe/AbstractDataFrameDiagnosticTest.kt
@@ -7,15 +7,19 @@ package org.jetbrains.kotlin.fir.dataframe
 
 import org.jetbrains.kotlin.fir.dataframe.services.DataFramePluginAnnotationsProvider
 import org.jetbrains.kotlin.fir.dataframe.services.ExperimentalExtensionRegistrarConfigurator
+import org.jetbrains.kotlin.fir.dataframe.services.TemporaryDirectoryManagerImplFixed
 import org.jetbrains.kotlin.test.FirParser
+import org.jetbrains.kotlin.test.TestJdkKind
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
+import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
 import org.jetbrains.kotlin.test.frontend.fir.DisableLazyResolveChecksAfterAnalysisChecker
 import org.jetbrains.kotlin.test.initIdeaConfiguration
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerTest
 import org.jetbrains.kotlin.test.runners.baseFirDiagnosticTestConfiguration
 import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
 import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
+import org.jetbrains.kotlin.test.services.TemporaryDirectoryManager
 import org.junit.jupiter.api.BeforeAll
 
 abstract class AbstractDataFrameDiagnosticTest : AbstractKotlinCompilerTest() {
@@ -40,11 +44,13 @@ abstract class AbstractDataFrameDiagnosticTest : AbstractKotlinCompilerTest() {
 //            ::FirResolveContractViolationErrorHandler,
 //        )
 //    }
+        useAdditionalService<TemporaryDirectoryManager>(::TemporaryDirectoryManagerImplFixed)
 
         defaultDirectives {
             +FirDiagnosticsDirectives.ENABLE_PLUGIN_PHASES
             +FirDiagnosticsDirectives.FIR_DUMP
             FirDiagnosticsDirectives.FIR_PARSER with FirParser.LightTree
+            JvmEnvironmentConfigurationDirectives.JDK_KIND with TestJdkKind.FULL_JDK
         }
 
         useConfigurators(
