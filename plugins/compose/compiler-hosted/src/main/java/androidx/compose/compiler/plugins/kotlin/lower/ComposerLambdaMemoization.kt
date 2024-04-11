e49af4abaecb6b8290e2fa6db54b95eb24ca95f5
Pavel Shishkin <pavel.shishkin@jetbrains.com>
2024-04-11 15:20:14 +0200

    Make compose compiler support k/wasm target
    Since k/wasm has a few things common with k/js,
    it often requires the same checks and transformations as k/js.
    
    Test: tested against Compose Multiplatform testsuite
    
    Change-Id: Ia5d85d0af2e986fa764637672fe5ff19ed5e18a5 ( https://android-review.googlesource.com/q/Ia5d85d0af2e986fa764637672fe5ff19ed5e18a5 )
    Signed-off-by: Pavel Shishkin <pavel.shishkin@jetbrains.com>
    
    Moved from: https://github.com/androidx/androidx/commit/b905531cd106721234bb5c4ebd1a0ad4983a174b

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposerLambdaMemoization.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposerLambdaMemoization.kt
@@ -101,6 +101,7 @@ import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 import org.jetbrains.kotlin.load.kotlin.PackagePartClassUtils
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.isJs
+import org.jetbrains.kotlin.platform.isWasm
 import org.jetbrains.kotlin.platform.jvm.isJvm
 
 private class CaptureCollector {
@@ -872,11 +873,11 @@ class ComposerLambdaMemoization(
         val function = expression.function
         val argumentCount = function.valueParameters.size
 
-        val isJs = context.platform.isJs()
-        if (argumentCount > MAX_RESTART_ARGUMENT_COUNT && isJs) {
+        val isJsOrWasm = context.platform.isJs() || context.platform.isWasm()
+        if (argumentCount > MAX_RESTART_ARGUMENT_COUNT && isJsOrWasm) {
             error(
                 "only $MAX_RESTART_ARGUMENT_COUNT parameters " +
-                    "in @Composable lambda are supported on JS"
+                    "in @Composable lambda are supported on K/JS or K/Wasm"
             )
         }
 
@@ -955,7 +956,7 @@ class ComposerLambdaMemoization(
     ): IrExpression {
         // Kotlin/JS doesn't have an optimization for non-capturing lambdas
         // https://youtrack.jetbrains.com/issue/KT-49923
-        val skipNonCapturingLambdas = !context.platform.isJs()
+        val skipNonCapturingLambdas = !context.platform.isJs() && !context.platform.isWasm()
 
         // If the function doesn't capture, Kotlin's default optimization is sufficient
         if (captures.isEmpty() && skipNonCapturingLambdas) {
