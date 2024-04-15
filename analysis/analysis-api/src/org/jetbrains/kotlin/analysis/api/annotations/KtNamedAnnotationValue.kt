40fa85cae6d7c4fbee2e80f4f55f60ef94e9862d
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2024-04-15 23:01:13 +0900

    [Analysis API] Remove 'data' modifier from 'KtNamedAnnotationValue'
    As the class evolves, the 'data' attribute might make binary
    compatibility preserving more challenging.

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/annotations/KtNamedAnnotationValue.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/annotations/KtNamedAnnotationValue.kt
@@ -6,8 +6,32 @@
 package org.jetbrains.kotlin.analysis.api.annotations
 
 import org.jetbrains.kotlin.name.Name
+import java.util.Objects
 
 /**
  * Name-Value pair which is used as annotation argument.
  */
-public data class KtNamedAnnotationValue(val name: Name, val expression: KtAnnotationValue)
+public class KtNamedAnnotationValue(
+    public val name: Name,
+    public val expression: KtAnnotationValue
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as KtNamedAnnotationValue
+
+        if (name != other.name) return false
+        if (expression != other.expression) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(name, expression)
+    }
+
+    override fun toString(): String {
+        return "KtNamedAnnotationValue(name=$name, expression=$expression)"
+    }
+}
