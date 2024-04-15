6c0b27cbb7f4b6599164d8306102ff8ade5bd428
Sonya Valchuk <pyos@google.com>
2024-04-15 08:37:21 +0000

    FIR CFA: check that labeled edges only appear in `finally`

--- compiler/tests-compiler-utils/tests/org/jetbrains/kotlin/fir/FirCfgConsistencyChecker.kt
+++ compiler/tests-compiler-utils/tests/org/jetbrains/kotlin/fir/FirCfgConsistencyChecker.kt
@@ -35,6 +35,11 @@ class FirCfgConsistencyChecker(private val assertions: Assertions) : FirVisitorV
                 }
             }
             for (from in node.previousNodes) {
+                if (node !is FinallyBlockEnterNode && from !is FinallyBlockExitNode) {
+                    val label = node.edgeFrom(from).label
+                    val correctedLabel = if (label == PostponedPath) NormalPath else label
+                    assertions.assertEquals(correctedLabel, NormalPath) { "edge from $from to $node cannot be labeled" }
+                }
                 assertions.assertContainsElements(from.followingNodes, node)
             }
             assertions.assertFalse(node.followingNodes.isEmpty() && node.previousNodes.isEmpty()) { "Unconnected CFG node: $node" }
