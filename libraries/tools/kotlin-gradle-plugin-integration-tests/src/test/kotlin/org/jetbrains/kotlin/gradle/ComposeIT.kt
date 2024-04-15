44a100e51bec12432e68a63a26f8cbed7cac428a
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-04-15 22:56:45 +0200

    [Gradle, Compose] Add the test for Gradle build cache relocation
    ^KT-67441 Verification Pending

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ComposeIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ComposeIT.kt
@@ -9,7 +9,10 @@ import org.gradle.api.logging.LogLevel
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Path
 import kotlin.io.path.appendText
+import kotlin.io.path.writeText
 
 @DisplayName("Compose compiler Gradle plugin")
 class ComposeIT : KGPBaseTest() {
@@ -93,6 +96,77 @@ class ComposeIT : KGPBaseTest() {
             build("assembleDebug") {
                 assertOutputContains("Detected Android Gradle Plugin compose compiler configuration")
             }
+        }
+    }
+
+    @DisplayName("Should not break build cache relocation")
+    @AndroidGradlePluginTests
+    @GradleAndroidTest
+    fun testAndroidBuildCacheRelocation(
+        gradleVersion: GradleVersion,
+        agpVersion: String,
+        providedJdk: JdkVersions.ProvidedJdk,
+        @TempDir localCacheDir: Path,
+    ) {
+        val project1 = androidComposeAppProjectWithLocalCacheEnabled(
+            gradleVersion,
+            agpVersion,
+            providedJdk,
+            localCacheDir
+        )
+
+        val project2 = androidComposeAppProjectWithLocalCacheEnabled(
+            gradleVersion,
+            agpVersion,
+            providedJdk,
+            localCacheDir
+        )
+
+        project1.build("assembleDebug") {
+            assertTasksExecuted(":compileDebugKotlin")
+        }
+
+        project2.build("assembleDebug") {
+            assertTasksFromCache(":compileDebugKotlin")
+        }
+    }
+
+    private fun androidComposeAppProjectWithLocalCacheEnabled(
+        gradleVersion: GradleVersion,
+        agpVersion: String,
+        providedJdk: JdkVersions.ProvidedJdk,
+        localCacheDir: Path,
+    ): TestProject {
+        return project(
+            projectName = "AndroidSimpleComposeApp",
+            gradleVersion = gradleVersion,
+            buildJdk = providedJdk.location,
+            buildOptions = defaultBuildOptions.copy(
+                androidVersion = agpVersion,
+                buildCacheEnabled = true,
+            )
+        ) {
+            projectPath.resolve("stability-configuration.conf").writeText(
+                """
+                |// Consider LocalDateTime stable
+                |java.time.LocalDateTime
+                |// Consider kotlin collections stable
+                |kotlin.collections.*
+                """.trimMargin()
+            )
+            buildGradleKts.appendText(
+                """
+                |
+                |composeCompiler {
+                |    suppressKotlinVersionCompatibilityCheck.set("${buildOptions.kotlinVersion}")
+                |    metricsDestination.set(project.layout.buildDirectory.dir("metrics"))
+                |    reportsDestination.set(project.layout.buildDirectory.dir("reports"))
+                |    stabilityConfigurationFile.set(project.layout.projectDirectory.file("stability-configuration.conf"))
+                |}
+                """.trimMargin()
+            )
+
+            enableLocalBuildCache(localCacheDir)
         }
     }
 }
