7f2de412af72fddb3957ea59a85c0a8bf4f1540f
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-04-16 18:49:37 +0200

    [LL] Weakly reference `LLFirSession` from its validity tracker
    - We prefer a weak reference over a soft reference here so that the
      reference is cleared as early as possible if caches are already
      cleared.
    
    ^KT-65759 fixed

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirSession.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirSession.kt
@@ -16,6 +16,7 @@ import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.PrivateSessionConstructor
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
+import java.lang.ref.WeakReference
 import java.util.concurrent.atomic.AtomicLong
 
 /**
@@ -62,21 +63,7 @@ abstract class LLFirSession(
     /**
      * Creates a [ModificationTracker] which tracks the validity of this session via [isValid].
      */
-    fun createValidityTracker(): ModificationTracker = ValidityModificationTracker()
-
-    private inner class ValidityModificationTracker : ModificationTracker {
-        private var count = AtomicLong()
-
-        override fun getModificationCount(): Long {
-            if (isValid) return 0
-
-            // When the session is invalid, we cannot simply return a static modification count of 1. For example, consider situations where
-            // a cached value was created with an already invalid session (so it remembers the modification count of 1). Then, if we return
-            // a static modification count of 1, the modification count never changes and the cached value misses that the session has been
-            // invalidated. Hence, `count` is incremented on each modification count access.
-            return count.incrementAndGet()
-        }
-    }
+    fun createValidityTracker(): ModificationTracker = LLFirSessionValidityModificationTracker(WeakReference(this))
 
     private val lazyDisposable: Lazy<Disposable> = lazy {
         val disposable = Disposer.newDisposable()
@@ -110,6 +97,26 @@ abstract class LLFirSession(
 
     override fun toString(): String {
         return "${this::class.simpleName} for ${ktModule.moduleDescription}"
+    }
+}
+
+/**
+ * The validity tracker must not strongly reference the session to avoid leaking it, as the validity tracker may survive it.
+ *
+ * Similarly, by convention, the [LLFirSession] doesn't keep a strong reference to the validity tracker, to avoid overly complicated
+ * reference cycles (from the developer's perspective).
+ */
+private class LLFirSessionValidityModificationTracker(private val sessionRef: WeakReference<LLFirSession>) : ModificationTracker {
+    private val count = AtomicLong()
+
+    override fun getModificationCount(): Long {
+        if (sessionRef.get()?.isValid == true) return 0
+
+        // When the session is invalid, we cannot simply return a static modification count of 1. For example, consider situations where
+        // a cached value was created with an already invalid session (so it remembers the modification count of 1). Then, if we return
+        // a static modification count of 1, the modification count never changes and the cached value misses that the session has been
+        // invalidated. Hence, `count` is incremented on each modification count access.
+        return count.incrementAndGet()
     }
 }
 
