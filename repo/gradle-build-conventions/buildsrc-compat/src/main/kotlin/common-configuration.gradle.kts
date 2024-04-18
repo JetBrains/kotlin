a0f06985c955af243bd5a23f5bd8b2cd676245d1
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-04-18 14:27:52 +0200

    [Build] Update Gradle to 8.7

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
@@ -15,16 +15,12 @@ val gsonVersion = rootProject.extra["versions.gson"] as String
 dependencies {
     constraints {
         configurations.all {
-            if (isCanBeResolved && !isCanBeConsumed) {
-                allDependencies.configureEach {
-                    if (group == "com.google.code.gson" && name == "gson" && this@all.isCanBeDeclared) {
-                        this@constraints.add(this@all.name, "com.google.code.gson:gson") {
-                            version {
-                                require(gsonVersion)
-                            }
-                            because("Force using same gson version because of https://github.com/google/gson/pull/1991")
-                        }
+            if (isCanBeDeclared) {
+                this@constraints.add(this@all.name, "com.google.code.gson:gson") {
+                    version {
+                        require(gsonVersion)
                     }
+                    because("Force using same gson version because of https://github.com/google/gson/pull/1991")
                 }
             }
         }
