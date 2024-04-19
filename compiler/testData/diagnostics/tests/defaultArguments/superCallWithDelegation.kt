10ec1243a94315e2d69304c3884a8acdbfe7e268
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-04-19 15:09:42 +0200

    FirSuperCallWithDefaultsChecker: unwrap delegated/override function
    #KT-67593 Fixed
    
    (cherry picked from commit 44225e2449bd2840ec378173fc624a8fccaf1c26)

--- compiler/testData/diagnostics/tests/defaultArguments/superCallWithDelegation.kt
+++ compiler/testData/diagnostics/tests/defaultArguments/superCallWithDelegation.kt
@@ -1,3 +1,4 @@
+// FIR_IDENTICAL
 // ISSUE: KT-67593
 
 interface Foo {
@@ -9,5 +10,49 @@ open class FooFoo(val f: Foo) : Foo by f
 class Final(f: Foo) : FooFoo(f) {
     override fun bar(x: Int, y: String?): String {
         return super.<!SUPER_CALL_WITH_DEFAULT_PARAMETERS!>bar<!>(x)
+    }
+}
+
+interface Generic<T> {
+    fun bar(x: Int = 0)
+}
+
+open class Impl(val g: Generic<String>) : Generic<String> by g
+
+class Final2(g: Generic<String>) : Impl(g) {
+    override fun bar(x: Int) {
+        return super.<!SUPER_CALL_WITH_DEFAULT_PARAMETERS!>bar<!>()
+    }
+}
+
+open class GenericClass<T> {
+    open fun bar(x: Int = 0) {}
+}
+
+class FinalClass : GenericClass<String>() {
+    override fun bar(x: Int) {
+        return super.<!SUPER_CALL_WITH_DEFAULT_PARAMETERS!>bar<!>()
+    }
+}
+
+open class A {
+    open fun bar(x: Int = 0) {}
+
+    open fun baz(x: Int) {}
+}
+
+interface B {
+    fun bar(x: Int)
+
+    fun baz(x: Int = 0)
+}
+
+class AB : A(), B {
+    override fun bar(x: Int) {
+        return super.<!SUPER_CALL_WITH_DEFAULT_PARAMETERS!>bar<!>()
+    }
+
+    override fun baz(x: Int) {
+        return super.baz<!NO_VALUE_FOR_PARAMETER!>()<!>
     }
 }
