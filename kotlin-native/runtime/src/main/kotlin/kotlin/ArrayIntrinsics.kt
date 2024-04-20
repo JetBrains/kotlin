1dde03f7c6bd3f9179ccdf6fb9c86496725b28e3
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-04-20 04:43:31 +0200

    [stdlib] Move common compilable builtin classes into common source set
    - Split emptyArray() into expect/actual
    - Cherry-pick JVM ArrayIntrinsics.kt back to builtins
    - Extract Native array creation functions to a separate file
    
    - Remove duplicated classes from Native
    2 existing shared annotations have been introduced to Native as a result:
    - internal kotlin.internal.PlatformDependent
    - public kotlin.ContextFunctionTypeParams
    
    KT-65526

--- kotlin-native/runtime/src/main/kotlin/kotlin/ArrayIntrinsics.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/ArrayIntrinsics.kt
@@ -1,94 +1,76 @@
 /*
- * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:Suppress("NOTHING_TO_INLINE")
-
 package kotlin
 
-import kotlin.wasm.internal.enumValueOfIntrinsic
-import kotlin.wasm.internal.enumValuesIntrinsic
-
-@Suppress("ACTUAL_ANNOTATIONS_NOT_MATCH_EXPECT") // missing @PureReifiable on T
-public actual inline fun <T> emptyArray(): Array<T> = arrayOf()
-
-/**
- * Returns a string representation of the object. Can be called with a null receiver, in which case
- * it returns the string "null".
- */
-public fun Any?.toString(): String = this?.toString() ?: "null"
-
-/**
- * Concatenates this string with the string representation of the given [other] object. If either the receiver
- * or the [other] object are null, they are represented as the string "null".
- */
-public operator fun String?.plus(other: Any?): String = (this ?: "null") + other.toString()
+import kotlin.native.internal.*
 
 /**
  * Returns an array of objects of the given type with the given [size], initialized with null values.
  *
  * @throws RuntimeException if the specified [size] is negative.
  */
-@Suppress("UNCHECKED_CAST")
-public inline fun <T> arrayOfNulls(size: Int): Array<T?> = Array<Any?>(size) as Array<T?>
+public inline fun <T> arrayOfNulls(size: Int): Array<T?> =
+        @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
+        arrayOfUninitializedElements<T?>(size)
 
 /**
  * Returns an array containing the specified elements.
  */
-@Suppress("UNCHECKED_CAST")
-public inline fun <T> arrayOf(vararg elements: T): Array<T> = elements as Array<T>
+@TypedIntrinsic(IntrinsicType.IDENTITY)
+@PointsTo(0x00, 0x01) // ret -> elements
+public external inline fun <T> arrayOf(vararg elements: T): Array<T>
+
+@GCUnsafeCall("Kotlin_emptyArray")
+@Suppress("ACTUAL_ANNOTATIONS_NOT_MATCH_EXPECT") // missing @PureReifiable on T
+public actual external fun <T> emptyArray(): Array<T>
 
 /**
  * Returns an array containing the specified [Double] numbers.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun doubleArrayOf(vararg elements: Double): DoubleArray = elements
 
 /**
  * Returns an array containing the specified [Float] numbers.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun floatArrayOf(vararg elements: Float): FloatArray = elements
 
 /**
  * Returns an array containing the specified [Long] numbers.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun longArrayOf(vararg elements: Long): LongArray = elements
 
 /**
  * Returns an array containing the specified [Int] numbers.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun intArrayOf(vararg elements: Int): IntArray = elements
 
 /**
  * Returns an array containing the specified characters.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun charArrayOf(vararg elements: Char): CharArray = elements
 
 /**
  * Returns an array containing the specified [Short] numbers.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun shortArrayOf(vararg elements: Short): ShortArray = elements
 
 /**
  * Returns an array containing the specified [Byte] numbers.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun byteArrayOf(vararg elements: Byte): ByteArray = elements
 
 /**
  * Returns an array containing the specified boolean values.
  */
+@Suppress("NOTHING_TO_INLINE")
 public inline fun booleanArrayOf(vararg elements: Boolean): BooleanArray = elements
-
-/**
- * Returns an array containing enum T entries.
- */
-@SinceKotlin("1.1")
-public inline fun <reified T : Enum<T>> enumValues(): Array<T> =
-    enumValuesIntrinsic()
-
-/**
- * Returns an enum entry with specified name.
- */
-@SinceKotlin("1.1")
-public inline fun <reified T : Enum<T>> enumValueOf(name: String): T =
-    enumValueOfIntrinsic(name)
