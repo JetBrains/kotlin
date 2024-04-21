d7949280a5f27959d26fc0448495e654969bd91a
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-04-21 10:01:01 +0300

    Fix js-ir-minimal-for-test build

--- libraries/stdlib/js-ir-minimal-for-test/build.gradle.kts
+++ libraries/stdlib/js-ir-minimal-for-test/build.gradle.kts
@@ -66,6 +66,8 @@ val commonMainSources by task<Sync> {
                 "libraries/stdlib/src/kotlin/time/**",
                 "libraries/stdlib/src/kotlin/util/KotlinVersion.kt",
                 "libraries/stdlib/src/kotlin/util/Tuples.kt",
+                "libraries/stdlib/src/kotlin/util/UUID.kt",
+                "libraries/stdlib/src/kotlin/util/UUIDImpl.kt",
                 "libraries/stdlib/src/kotlin/enums/**"
             )
         )
@@ -118,6 +120,7 @@ val jsMainSources by task<Sync> {
             "kotlinx/dom/**",
             "kotlinx/browser/**",
             "kotlin/enums/**",
+            "kotlin/UUIDJs.kt",
         )
     }
     from {
