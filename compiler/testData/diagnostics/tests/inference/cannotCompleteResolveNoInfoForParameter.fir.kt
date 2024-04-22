c667b99d150daa18a949171b3374d25d9b593e8f
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-04-22 16:35:03 +0200

    [FIR] Properly handle vararg remapping even if ConeInapplicableCandidateError
    ^KT-67609 Fixed
    
    Related tests:
    - compiler/testData/diagnostics/tests/inference/cannotCompleteResolveWithFunctionLiterals.kt
    - compiler/testData/diagnostics/tests/inference/builderInference/invalidateKeys.kt
    - compiler/testData/diagnostics/tests/inference/builderInference/regresssions/exponentialErrorsInCSInitial.kt
    - compiler/testData/diagnostics/tests/inference/builderInference/stubTypes/stubTypeReceiverRestriction.kt
    - compiler/testData/diagnostics/tests/inference/builderInference/stubTypes/stubTypeReceiverRestrictionDisabled.kt
    - compiler/testData/diagnostics/tests/inference/cannotCompleteResolveNoInfoForParameter.kt
    - compiler/testData/diagnostics/tests/inference/cannotCompleteResolveNoInfoForParameter.kt
    - compiler/testData/diagnostics/tests/inference/kt46515.kt
    - compiler/testData/diagnostics/tests/inference/recursiveLocalFuns/selfCall.kt
    - compiler/testData/diagnostics/tests/varargs
    - compiler/fir/analysis-tests/testData/resolve/arguments/vararg.kt
    - compiler/testData/diagnostics/tests/checkArguments/SpreadVarargs.kt
    - compiler/testData/diagnostics/tests/resolve/overloadConflicts/varargsInDifferentPositions.kt
    - compiler/testData/diagnostics/tests/resolve/overloadConflicts/varargsMixed.kt

--- compiler/testData/diagnostics/tests/inference/cannotCompleteResolveNoInfoForParameter.fir.kt
+++ compiler/testData/diagnostics/tests/inference/cannotCompleteResolveNoInfoForParameter.fir.kt
@@ -1,10 +1,16 @@
 package f
 
+class In<in K>
+
 fun <T> f(i: Int, c: Collection<T>): List<T> {throw Exception()}
 fun <T> f(a: Any, l: List<T>): Collection<T> {throw Exception()}
 
+fun <T, K> g(i: Int, c: Collection<T>, vararg x: In<K>): List<T> {throw Exception()}
+fun <T, K> g(a: Any, l: List<T>, vararg x: In<K>): Collection<T> {throw Exception()}
+
 fun <T> test(l: List<T>) {
     <!OVERLOAD_RESOLUTION_AMBIGUITY!>f<!>(1, <!CANNOT_INFER_PARAMETER_TYPE!>emptyList<!>())
+    <!NONE_APPLICABLE!>g<!>(1, <!CANNOT_INFER_PARAMETER_TYPE!>emptyList<!>(), In<String>(), In<Int>())
 }
 
 fun <T> emptyList(): List<T> {throw Exception()}
