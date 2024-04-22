16c51f74ef8c84d86e9f45d51d983e57b11d4b56
Troels Bjerre Lund <troels@google.com>
2024-04-22 18:09:39 +0200

    [LLVM 16] Remove llvmext functions that are now available in LLVM

--- kotlin-native/libllvmext/src/main/include/OpaquePointerAPI.h
+++ kotlin-native/libllvmext/src/main/include/OpaquePointerAPI.h
@@ -9,24 +9,6 @@
 extern "C" {
 # endif
 
-/* These are the opaque pointer functions that are missing in llvm-11, which
-must be removed upon upgrading LLVM version.
-
-Copied from llvm release/14.x.
-*/
-
-// LLVMConstGEP2 and LLVMConstInBoundsGEP2 are already forward declared in Core.h
-LLVMValueRef LLVMConstGEP2(LLVMTypeRef Ty, LLVMValueRef ConstantVal,
-                           LLVMValueRef *ConstantIndices, unsigned NumIndices);
-
-LLVMValueRef LLVMConstInBoundsGEP2(LLVMTypeRef Ty, LLVMValueRef ConstantVal,
-                                   LLVMValueRef *ConstantIndices,
-                                   unsigned NumIndices);
-
-LLVMValueRef LLVMAddAlias2(LLVMModuleRef M, LLVMTypeRef ValueTy,
-                           unsigned AddrSpace, LLVMValueRef Aliasee,
-                           const char *Name);
-
 unsigned LLVMGetProgramAddressSpace(LLVMModuleRef moduleRef);
 
 # ifdef __cplusplus
