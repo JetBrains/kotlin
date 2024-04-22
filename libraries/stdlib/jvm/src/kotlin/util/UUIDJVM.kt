e60594f3ac8676f71526381ab6fe85e97e35afd9
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-04-22 13:12:21 +0300

    Add JVM-only convenience functions

--- libraries/stdlib/jvm/src/kotlin/util/UUIDJVM.kt
+++ libraries/stdlib/jvm/src/kotlin/util/UUIDJVM.kt
@@ -5,7 +5,9 @@
 
 package kotlin
 
+import java.nio.*
 import java.security.SecureRandom
+import kotlin.internal.InlineOnly
 
 private val secureRandom by lazy { SecureRandom() }
 
@@ -14,3 +16,48 @@ internal actual fun secureRandomUUID(): UUID {
     secureRandom.nextBytes(randomBytes)
     return uuidFromRandomBytes(randomBytes)
 }
+
+/** Converts this [java.util.UUID] value to [kotlin.UUID] value. */
+@InlineOnly
+public inline fun java.util.UUID.toKotlinUUID(): UUID =
+    UUID.fromLongs(mostSignificantBits, leastSignificantBits)
+
+/** Converts this [kotlin.UUID] value to [java.util.UUID] value. */
+@InlineOnly
+public inline fun UUID.toJavaUUID(): java.util.UUID =
+    java.util.UUID(mostSignificantBits, leastSignificantBits)
+
+// TODO: ByteBuffer could be configured to have a LITTLE_ENDIAN byte order.
+/**
+ * Reads the next 16 bytes at this buffer's current position, composing them into a UUID value according
+ * to the current byte order, and then increments the position by 16.
+ *
+ * The returned UUID is equivalent to:
+ * ```kotlin
+ * val bytes = ByteArray(16)
+ * byteBuffer.get(bytes)
+ * return UUID.fromByteArray(bytes)
+ * ```
+ *
+ * @throws BufferUnderflowException If there are fewer than 16 bytes remaining in this buffer.
+ */
+@InlineOnly
+public inline fun ByteBuffer.getUUID(): UUID =
+    UUID.fromLongs(getLong(), getLong())
+
+// TODO: ByteBuffer could be configured to have a LITTLE_ENDIAN byte order
+/**
+ * Writes 16 bytes containing the given UUID value, in the current byte order, into this buffer at the current position,
+ * and then increments the position by 16.
+ *
+ * This function is equivalent to:
+ * ```kotlin
+ * byteBuffer.put(uuid.toByteArray())
+ * ```
+ *
+ * @throws BufferOverflowException If there is insufficient space in this buffer for 16 bytes.
+ * @throws ReadOnlyBufferException If this buffer is read-only.
+ */
+@InlineOnly
+public inline fun ByteBuffer.putUUID(uuid: UUID): ByteBuffer =
+    putLong(uuid.mostSignificantBits).putLong(uuid.leastSignificantBits)
