6924dd3fe59d34a9ea1a223770378f7feef3e19c
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-04-22 18:21:40 +0200

    [JS][tests] Test for failed KLIB resolve due to ABI version
    ^KT-52602

---
+++ js/js.tests/test/org/jetbrains/kotlin/js/testOld/klib/JsKlibResolverTest.kt
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.js.testOld.klib
+
+import org.jetbrains.kotlin.cli.common.ExitCode
+import org.jetbrains.kotlin.cli.js.K2JsIrCompiler
+import org.jetbrains.kotlin.library.KLIB_PROPERTY_ABI_VERSION
+import org.jetbrains.kotlin.library.KotlinAbiVersion
+import org.jetbrains.kotlin.test.TestCaseWithTmpdir
+import org.jetbrains.kotlin.test.services.StandardLibrariesPathProviderForKotlinProject
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
+import java.util.*
+
+class JsKlibResolverTest : TestCaseWithTmpdir() {
+    fun testWarningAboutRejectedLibraryIsNotSuppressed() {
+        val testDataDir = File("compiler/testData/klib/resolve/mismatched-abi-version")
+
+        val lib1V1 = createKlibDir("lib1", 1)
+
+        compileKlib(
+            sourceFile = testDataDir.resolve("lib1.kt"),
+            dependency = null,
+            outputFile = lib1V1
+        ).assertSuccess() // Should compile successfully.
+
+        compileKlib(
+            sourceFile = testDataDir.resolve("lib2.kt"),
+            dependency = lib1V1,
+            outputFile = createKlibDir("lib2", 1)
+        ).assertSuccess() // Should compile successfully.
+
+        // Now patch lib1:
+        val lib1V2 = createKlibDir("lib1", 2)
+        lib1V1.copyRecursively(lib1V2)
+        patchManifest(lib1V2) { key, value ->
+            if (key == KLIB_PROPERTY_ABI_VERSION)
+                KotlinAbiVersion.CURRENT.copy(major = KotlinAbiVersion.CURRENT.major + 1).toString()
+            else
+                value
+        }
+
+        val result = compileKlib(
+            sourceFile = testDataDir.resolve("lib2.kt"),
+            dependency = lib1V2,
+            outputFile = createKlibDir("lib2", 2)
+        )
+
+        result.assertFailure() // Should not compile successfully.
+
+        val lines = result.output.lineSequence()
+            .filter(String::isNotBlank)
+            .map { it.replace(tmpdir.absolutePath, "<path>") }
+            .toList()
+
+        lines.assertHasLineWithPrefix("error: KLIB resolver: Could not find \"<path>/v2/lib1\"")
+        //lines.assertHasLineWithPrefix("warning: KLIB resolver: Skipping '<path>/v2/lib1'. Incompatible ABI version")
+    }
+
+    private fun createKlibDir(name: String, version: Int): File =
+        tmpdir.resolve("v$version").resolve(name).apply(File::mkdirs)
+
+    private fun patchManifest(klib: File, transform: (String, String) -> String) {
+        val manifestFile = klib.resolve("default/manifest")
+        assertTrue(manifestFile.isFile)
+
+        Properties().apply {
+            manifestFile.inputStream().use { load(it) }
+            entries.forEach { entry -> entry.setValue(transform(entry.key.toString(), entry.value.toString())) }
+            manifestFile.outputStream().use { store(it, null) }
+        }
+    }
+
+    private fun List<String>.assertHasLineWithPrefix(prefix: String) {
+        if (none { it.startsWith(prefix) }) {
+            fail(
+                """
+                    No line starting with prefix found: $prefix
+                    There are $size lines:
+                    ${joinToString("\n")}
+                """.trimIndent()
+            )
+        }
+    }
+
+    private fun compileKlib(sourceFile: File, dependency: File?, outputFile: File): CompilationResult {
+        val libraries = listOfNotNull(
+            StandardLibrariesPathProviderForKotlinProject.fullJsStdlib(),
+            dependency
+        ).joinToString(File.pathSeparator) { it.absolutePath }
+
+        val args = arrayOf(
+            "-Xir-produce-klib-dir",
+            "-libraries", libraries,
+            "-ir-output-dir", outputFile.absolutePath,
+            "-ir-output-name", outputFile.nameWithoutExtension,
+            sourceFile.absolutePath
+        )
+
+        val compilerXmlOutput = ByteArrayOutputStream()
+        val exitCode = PrintStream(compilerXmlOutput).use { printStream ->
+            K2JsIrCompiler().execFullPathsInMessages(printStream, args)
+        }
+
+        return CompilationResult(exitCode, compilerXmlOutput.toString())
+    }
+
+    private data class CompilationResult(val exitCode: ExitCode, val output: String) {
+        fun assertSuccess() = JUnit4Assertions.assertTrue(exitCode == ExitCode.OK) {
+            buildString {
+                appendLine("Expected exit code: ${ExitCode.OK}, Actual: $exitCode")
+                appendLine("Compiler output:")
+                appendLine(output)
+            }
+        }
+
+        fun assertFailure() = JUnit4Assertions.assertTrue(exitCode != ExitCode.OK) {
+            buildString {
+                appendLine("Expected exit code: any but ${ExitCode.OK}, Actual: $exitCode")
+                appendLine("Compiler output:")
+                appendLine(output)
+            }
+        }
+    }
+}
