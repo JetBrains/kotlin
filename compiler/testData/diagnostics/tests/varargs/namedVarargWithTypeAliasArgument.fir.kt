acac9f041831ca79411b11e7ab04bfcc88b07982
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-04-22 11:33:22 +0200

    [FIR] Expand typealiases in FirNamedVarargChecker
    #KT-67625 Fixed

--- compiler/testData/diagnostics/tests/varargs/namedVarargWithTypeAliasArgument.fir.kt
+++
@@ -1,8 +0,0 @@
-typealias ReadonlyArray<T> = Array<T>
-
-fun printAll(vararg values: Any?) {}
-
-fun main() {
-    var a: ReadonlyArray<String> = arrayOf("a")
-    printAll(values = <!ASSIGNING_SINGLE_ELEMENT_TO_VARARG_IN_NAMED_FORM_FUNCTION_ERROR!>a<!>)
-}
