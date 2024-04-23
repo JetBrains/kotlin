fb3ffb4557a65f188ad95b5b8a519434667c6ee1
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-04-23 14:08:10 +0200

    [Analysis API] ValidityAwareCachedValue: cleanup code
    ^KT-67655

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/utils/ValidityAwareCachedValue.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/utils/ValidityAwareCachedValue.kt
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.analysis.api.fir.utils
 
 import org.jetbrains.kotlin.analysis.api.lifetime.KtLifetimeOwner
-import org.jetbrains.kotlin.analysis.api.lifetime.assertIsValidAndAccessible
+import org.jetbrains.kotlin.analysis.api.lifetime.withValidityAssertion
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -14,16 +14,15 @@ import kotlin.reflect.KProperty
  * Lazy value that guaranties safe publication and checks validity on every access
  */
 @JvmInline
-value class ValidityAwareCachedValue<T>(
+internal value class ValidityAwareCachedValue<T>(
     private val lazyValue: Lazy<T>,
-) : ReadOnlyProperty<Any, T> {
-
-    override fun getValue(thisRef: Any, property: KProperty<*>): T {
-        require(thisRef is KtLifetimeOwner)
-        thisRef.token.assertIsValidAndAccessible()
-        return lazyValue.value
+) : ReadOnlyProperty<KtLifetimeOwner, T> {
+    override fun getValue(thisRef: KtLifetimeOwner, property: KProperty<*>): T {
+        return thisRef.withValidityAssertion { lazyValue.value }
     }
 }
 
 @Suppress("UnusedReceiverParameter") // we need to have the KtLifetimeOwner as receiver to make sure it's called only for KtLifetimeOwner
-internal fun <T> KtLifetimeOwner.cached(init: () -> T) = ValidityAwareCachedValue(lazy(LazyThreadSafetyMode.PUBLICATION, init))
+internal fun <T> KtLifetimeOwner.cached(init: () -> T): ValidityAwareCachedValue<T> {
+    return ValidityAwareCachedValue(lazy(LazyThreadSafetyMode.PUBLICATION, init))
+}
