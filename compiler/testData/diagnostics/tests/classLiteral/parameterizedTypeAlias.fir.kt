6c78baa3c8099ab79919884808cdf869912cd41b
Alevtina.Gamzikova <alevtina.gamzikova@jetbrains.com>
2024-04-24 22:40:13 +0200

    [Test] KT-59553 Add additional testcases

--- compiler/testData/diagnostics/tests/classLiteral/parameterizedTypeAlias.fir.kt
+++ compiler/testData/diagnostics/tests/classLiteral/parameterizedTypeAlias.fir.kt
@@ -14,7 +14,50 @@ typealias SimpleArrayAlias<T> = Array<T>
 typealias SpecificArrayAlias = Array<Int>
 typealias UnusedArrayAlias<T> = Array<Int>
 
+typealias Mixed<T> = Inv<MyPair<T, T>>
+typealias Mixed2<T> = MyPair<Inv<T>, Inv<T>>
+
+class UpperBound<A, T : A>
+typealias UpperBoundTypeAlias<A, T> = UpperBound<A, T>
+typealias UpperBoundTypeAliasUnused<A, T> = UpperBound<Int, <!UPPER_BOUND_VIOLATED!>Long<!>>
+
+class UpperBoundOutIn<out A, in T : A>
+typealias UpperBoundOutInTypealias<A, T> = UpperBoundOutIn<A, T>
+
 fun test() {
+    Mixed::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed<String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed<in String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed<out String>::class<!>
+
+    Mixed2::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed2<String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed2<in String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed2<out String>::class<!>
+
+    UpperBound::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<Int, Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<in Int, out Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<out Int, out Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<in Int, in Long>::class<!>
+
+    UpperBoundTypeAlias::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<Int, Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<in Int, out Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<out Int, in Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<out Int, out Long>::class<!>
+
+
+    UpperBoundOutInTypealias::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundOutInTypealias<Int, Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundOutInTypealias<in Int, out Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundOutInTypealias<in Int, in Long>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundOutInTypealias<out Int, out Long>::class<!>
+
+    UpperBoundTypeAliasUnused::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAliasUnused<String, String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAliasUnused<in String, out String>::class<!>
+
     Inv::class
     <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Inv<String>::class<!>
 
--- compiler/testData/diagnostics/tests/classLiteral/parameterizedTypeAlias.kt
+++ compiler/testData/diagnostics/tests/classLiteral/parameterizedTypeAlias.kt
@@ -14,7 +14,50 @@ typealias SimpleArrayAlias<T> = Array<T>
 typealias SpecificArrayAlias = Array<Int>
 typealias UnusedArrayAlias<T> = Array<Int>
 
+typealias Mixed<T> = Inv<MyPair<T, T>>
+typealias Mixed2<T> = MyPair<Inv<T>, Inv<T>>
+
+class UpperBound<A, T : A>
+typealias UpperBoundTypeAlias<A, T> = UpperBound<A, T>
+typealias UpperBoundTypeAliasUnused<A, T> = UpperBound<Int, <!UPPER_BOUND_VIOLATED!>Long<!>>
+
+class UpperBoundOutIn<out A, in T : A>
+typealias UpperBoundOutInTypealias<A, T> = UpperBoundOutIn<A, T>
+
 fun test() {
+    <!WRONG_NUMBER_OF_TYPE_ARGUMENTS!>Mixed<!>::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed<String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed<in String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed<out String>::class<!>
+
+    <!WRONG_NUMBER_OF_TYPE_ARGUMENTS!>Mixed2<!>::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed2<String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed2<in String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Mixed2<out String>::class<!>
+
+    UpperBound::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<Int, <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<in Int, out <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<out Int, out <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBound<in Int, in <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+
+    UpperBoundTypeAlias::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<Int, <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<in Int, out <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<out Int, in <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAlias<out Int, out <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+
+
+    UpperBoundOutInTypealias::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundOutInTypealias<Int, <!UPPER_BOUND_VIOLATED!>Long<!>>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!><!CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION, CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION!>UpperBoundOutInTypealias<in Int, out <!UPPER_BOUND_VIOLATED!>Long<!>><!>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!><!CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION!>UpperBoundOutInTypealias<in Int, in <!UPPER_BOUND_VIOLATED!>Long<!>><!>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!><!CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION!>UpperBoundOutInTypealias<out Int, out <!UPPER_BOUND_VIOLATED!>Long<!>><!>::class<!>
+
+    <!WRONG_NUMBER_OF_TYPE_ARGUMENTS!>UpperBoundTypeAliasUnused<!>::class
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAliasUnused<String, String>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>UpperBoundTypeAliasUnused<in String, out String>::class<!>
+
     Inv::class
     <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Inv<String>::class<!>
 
