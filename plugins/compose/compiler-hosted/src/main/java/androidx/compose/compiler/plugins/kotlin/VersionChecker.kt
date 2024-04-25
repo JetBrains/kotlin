3810dd7fc4c8ab94d0363ae1f288575d5cc6f71f
Andrei Shikov <ashikov@google.com>
2024-04-25 18:21:52 +0100

    Prepare for Compose compiler 1.5.13 release
    Change-Id: I76a27678061438d9507f9610b1e8ccc5b6d2e1d8 ( https://android-review.googlesource.com/q/I76a27678061438d9507f9610b1e8ccc5b6d2e1d8 )
    
    Moved from: https://github.com/androidx/androidx/commit/4655e42cbaddfcba6db63a5d9a806c6085e8dfa7

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -150,7 +150,7 @@ class VersionChecker(val context: IrPluginContext) {
          * The maven version string of this compiler. This string should be updated before/after every
          * release.
          */
-        const val compilerVersion: String = "1.5.12"
+        const val compilerVersion: String = "1.5.13"
         private val minimumRuntimeVersion: String
             get() = runtimeVersionToMavenVersionTable[minimumRuntimeVersionInt] ?: "unknown"
     }
