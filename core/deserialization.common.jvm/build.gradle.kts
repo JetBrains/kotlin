67ef15c1d89e8962c7b6ef661622a190a42ef06b
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-04-25 16:56:53 +0200

    Cleanup: replace DATA_CLASS_COPY_VISIBILITY_WILL_BE_CHANGED_WARNING suppress with -Xconsistent-data-class-copy-visibility No.2
    The bootstrap has happened. Now the compiler flag is available.
    I accidentally missed this todo in the first commit

--- core/deserialization.common.jvm/build.gradle.kts
+++ core/deserialization.common.jvm/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
     kotlin("jvm")
     id("jps-compatible")
@@ -9,6 +11,10 @@ dependencies {
     api(project(":core:metadata.jvm"))
     api(project(":core:deserialization.common"))
     implementation(project(":core:compiler.common.jvm"))
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions.freeCompilerArgs.add("-Xconsistent-data-class-copy-visibility")
 }
 
 sourceSets {
