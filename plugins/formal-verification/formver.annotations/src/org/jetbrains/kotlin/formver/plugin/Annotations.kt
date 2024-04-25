319c32d543f31f584f76a07494ba18319f4219c1
Cactus (Anton) Golov <anton.golov@jetbrains.com>
2024-04-25 09:46:06 +0200

    Add annotations for Unique and Borrowed (#206)

--- plugins/formal-verification/formver.annotations/src/org/jetbrains/kotlin/formver/plugin/Annotations.kt
+++ plugins/formal-verification/formver.annotations/src/org/jetbrains/kotlin/formver/plugin/Annotations.kt
@@ -9,3 +9,9 @@ annotation class NeverConvert
 annotation class NeverVerify
 annotation class AlwaysVerify
 annotation class DumpExpEmbeddings
+
+// We annotate the function to indicate that the return value is unique
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY)
+annotation class Unique
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Borrowed
