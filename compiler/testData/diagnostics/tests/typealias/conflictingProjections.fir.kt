8fcf11bcfd2e60e6863df66383e99324f2a5a343
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-04-25 10:02:45 +0300

    [FIR] FirProjectionRelationChecker: Add false changes

--- compiler/testData/diagnostics/tests/typealias/conflictingProjections.fir.kt
+++ compiler/testData/diagnostics/tests/typealias/conflictingProjections.fir.kt
@@ -9,8 +9,8 @@ typealias OutAlias<T> = Out<T>
 typealias TestOutForIn<T> = In<<!CONFLICTING_PROJECTION!>out<!> T>
 typealias TestInForOut<T> = Out<<!CONFLICTING_PROJECTION!>in<!> T>
 
-typealias TestOutForInWithinAlias<T> = InAlias<out T>
-typealias TestInForOutWithinAlias<T> = OutAlias<in T>
+typealias TestOutForInWithinAlias<T> = InAlias<<!CONFLICTING_PROJECTION!>out<!> T>
+typealias TestInForOutWithinAlias<T> = OutAlias<<!CONFLICTING_PROJECTION!>in<!> T>
 
-fun <T> testOutForInWithinResolvedType(x: InAlias<<!CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION!>out<!> T>) {}
-fun <T> testInForOutWithinResolvedType(x: OutAlias<<!CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION!>in<!> T>) {}
+fun <T> testOutForInWithinResolvedType(x: InAlias<<!CONFLICTING_PROJECTION!>out<!> T>) {}
+fun <T> testInForOutWithinResolvedType(x: OutAlias<<!CONFLICTING_PROJECTION!>in<!> T>) {}
