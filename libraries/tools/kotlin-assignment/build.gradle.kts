638d4bcb3763ef93e36d76cbb7978d5567635245
Alexander.Danilov <alexander.danilov@jetbrains.com>
2024-04-26 12:54:07 +0200

    Add red code examples for checking reports on CI

--- libraries/tools/kotlin-assignment/build.gradle.kts
+++ libraries/tools/kotlin-assignment/build.gradle.kts
@@ -22,3 +22,37 @@ gradlePlugin {
         }
     }
 }
+
+if (!kotlinBuildProperties.isInJpsBuildIdeaSync) {
+    testing {
+        suites {
+            val test by getting(JvmTestSuite::class) {
+                useJUnitJupiter(libs.versions.junit5)
+            }
+
+            register<JvmTestSuite>("functionalTest") {
+                dependencies {
+                    implementation(project())
+                    implementation(gradleKotlinDsl())
+                    implementation(platform(libs.junit.bom))
+                    implementation(libs.junit.jupiter.api)
+                    implementation(project(":kotlin-test"))
+
+                    runtimeOnly(libs.junit.jupiter.engine)
+                }
+
+                targets {
+                    all {
+                        testTask.configure {
+                            shouldRunAfter(test)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    tasks.named("check") {
+        dependsOn(testing.suites.named("functionalTest"))
+    }
+}
--- libraries/tools/kotlin-build-tools-enum-compat/build.gradle.kts
+++ libraries/tools/kotlin-build-tools-enum-compat/build.gradle.kts
@@ -15,3 +15,37 @@ dependencies {
 tasks.withType<KotlinJvmCompile>().configureEach {
     compilerOptions.freeCompilerArgs.add("-Xallow-kotlin-package")
 }
+
+if (!kotlinBuildProperties.isInJpsBuildIdeaSync) {
+    testing {
+        suites {
+            val test by getting(JvmTestSuite::class) {
+                useJUnitJupiter(libs.versions.junit5)
+            }
+
+            register<JvmTestSuite>("functionalTest") {
+                dependencies {
+                    implementation(project())
+                    implementation(gradleKotlinDsl())
+                    implementation(platform(libs.junit.bom))
+                    implementation(libs.junit.jupiter.api)
+                    implementation(project(":kotlin-test"))
+
+                    runtimeOnly(libs.junit.jupiter.engine)
+                }
+
+                targets {
+                    all {
+                        testTask.configure {
+                            shouldRunAfter(test)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    tasks.named("check") {
+        dependsOn(testing.suites.named("functionalTest"))
+    }
+}
