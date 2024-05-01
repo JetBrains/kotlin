7d91eba0600bd492d2d66e45261bfae732bee73f
Juan Rodriguez <juanrdz@google.com>
2024-05-01 22:55:42 +0000

    Compose beta01 version bump Test: ./gradlew updateApi
    Change-Id: Ic857cfe0e6e4bd37f351a5c1b2dce63897c85cb3 ( https://android-review.googlesource.com/q/Ic857cfe0e6e4bd37f351a5c1b2dce63897c85cb3 )
    
    Moved from: https://github.com/androidx/androidx/commit/717f4fd6d888b8de5f7d21b5432fb6ca824e55f9

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -145,6 +145,7 @@ class VersionChecker(val context: IrPluginContext, private val messageCollector:
             12500 to "1.7.0-alpha06",
             12600 to "1.7.0-alpha07",
             12700 to "1.7.0-alpha08",
+            12800 to "1.7.0-beta01",
         )
 
         /**
