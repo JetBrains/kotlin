9414e9b31b77be49c640aea44bf3c6537952a20b
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-05-01 18:02:39 +0200

    [KLIB Resolver] Test to check that `dependency_version_?` are set by the compiler
    ^KT-65834

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/KlibResolverTest.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/KlibResolverTest.kt
@@ -18,6 +18,7 @@ import org.jetbrains.kotlin.library.KLIB_PROPERTY_DEPENDENCY_VERSION
 import org.jetbrains.kotlin.library.KLIB_PROPERTY_LIBRARY_VERSION
 import org.jetbrains.kotlin.library.SearchPathResolver
 import org.jetbrains.kotlin.test.services.JUnit5Assertions
+import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertTrue
 import org.jetbrains.kotlin.test.utils.*
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.parallel.Execution
@@ -156,8 +157,69 @@ class KlibResolverTest : AbstractNativeSimpleTest() {
                 "libb" -> {
                     // pretend it depends on liba v2.0
                     patchManifestAsMap(JUnit5Assertions, klib.klibFile) { properties ->
-                        properties.entries.removeIf { it.key.startsWith(KLIB_PROPERTY_DEPENDENCY_VERSION) }
-                        properties[KLIB_PROPERTY_DEPENDENCY_VERSION + "_liba"] = "2.0"
+                        // first, check:
+                        val dependencyVersionForLibaPropertyName = KLIB_PROPERTY_DEPENDENCY_VERSION + "_liba"
+
+                        val dependencyVersionPropertyNames: Set<String> =
+                            properties.keys.filter { it.startsWith(KLIB_PROPERTY_DEPENDENCY_VERSION) }.toSet()
+                        val otherDependencyVersionPropertyNames = dependencyVersionPropertyNames - dependencyVersionForLibaPropertyName
+
+                        assertTrue(dependencyVersionForLibaPropertyName in dependencyVersionPropertyNames) {
+                            "Property $dependencyVersionForLibaPropertyName is expected in manifest"
+                        }
+                        assertTrue(otherDependencyVersionPropertyNames.isEmpty()) {
+                            "Unexpected properties in manifest: ${otherDependencyVersionPropertyNames.joinToString()}"
+                        }
+
+                        // then, patch:
+                        properties.remove(dependencyVersionForLibaPropertyName)
+                        properties[dependencyVersionForLibaPropertyName] = "2.0"
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testDependencyVersionsAreNotAdded() {
+        val modules = createModules(
+            Module("liba"),
+            Module("libb", "liba"),
+        )
+
+        // Control compilation -- should finish successfully.
+        modules.compileModules(
+            produceUnpackedKlibs = true,
+            useLibraryNamesInCliArguments = false
+        )
+
+        // Compilation with patched manifest -- should finish successfully too.
+        modules.compileModules(
+            produceUnpackedKlibs = true,
+            useLibraryNamesInCliArguments = false,
+        ) { module, klib ->
+            when (module.name) {
+                "liba" -> {
+                    // set the library version = 1.0
+                    patchManifestAsMap(JUnit5Assertions, klib.klibFile) { properties ->
+                        properties[KLIB_PROPERTY_LIBRARY_VERSION] = "1.0"
+                    }
+                }
+                "libb" -> {
+                    // check that dependency version is set
+                    patchManifestAsMap(JUnit5Assertions, klib.klibFile) { properties ->
+                        val dependencyVersionForLibaPropertyName = KLIB_PROPERTY_DEPENDENCY_VERSION + "_liba"
+
+                        val dependencyVersionPropertyNames: Set<String> =
+                            properties.keys.filter { it.startsWith(KLIB_PROPERTY_DEPENDENCY_VERSION) }.toSet()
+                        val otherDependencyVersionPropertyNames = dependencyVersionPropertyNames - dependencyVersionForLibaPropertyName
+
+                        assertTrue(dependencyVersionForLibaPropertyName in dependencyVersionPropertyNames) {
+                            "Property $dependencyVersionForLibaPropertyName is expected in manifest"
+                        }
+                        assertTrue(otherDependencyVersionPropertyNames.isEmpty()) {
+                            "Unexpected properties in manifest: ${otherDependencyVersionPropertyNames.joinToString()}"
+                        }
                     }
                 }
             }
