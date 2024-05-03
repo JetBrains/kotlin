7317a96ab191a5bdf39afd4f151138bb0e73c73c
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-05-03 12:41:12 +0200

    [Tests] Reproduce #KT-58347

--- js/js.tests/tests-gen/org/jetbrains/kotlin/incremental/JsFirInvalidationPerModuleTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/incremental/JsFirInvalidationPerModuleTestGenerated.java
@@ -577,6 +577,12 @@ public class JsFirInvalidationPerModuleTestGenerated extends AbstractJsFirInvali
   }
 
   @Test
+  @TestMetadata("unstableIrReproducer")
+  public void testUnstableIrReproducer() {
+    runTest("js/js.translator/testData/incremental/invalidation/unstableIrReproducer/");
+  }
+
+  @Test
   @TestMetadata("updateExports")
   public void testUpdateExports() {
     runTest("js/js.translator/testData/incremental/invalidation/updateExports/");
