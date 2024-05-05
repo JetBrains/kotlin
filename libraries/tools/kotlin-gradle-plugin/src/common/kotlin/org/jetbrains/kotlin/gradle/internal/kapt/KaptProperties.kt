ef31b6f1123ac142b38398348a20eaef4f15932f
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-05-05 22:38:04 +0200

    [Gradle] Flatten Kapt properties
    Remove not needed intermediate objects.
    
    ^KT-64627 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/KaptProperties.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/KaptProperties.kt
@@ -11,65 +11,57 @@ import org.jetbrains.kotlin.gradle.internal.properties.PropertiesBuildService.*
 import org.jetbrains.kotlin.gradle.internal.properties.propertiesService
 
 internal object KaptProperties {
-    object BooleanProperties {
-        val KAPT_VERBOSE = BooleanGradleProperty("kapt.verbose", false)
-        val KAPT_INCREMENTAL_APT = BooleanGradleProperty(
-            "kapt.incremental.apt",
-            // Currently doesn't match the default value of KaptFlag.INCREMENTAL_APT,
-            // but it's fine (see https://github.com/JetBrains/kotlin/pull/3942#discussion_r532578690).
-            true
-        )
-        val KAPT_INFO_AS_WARNINGS = BooleanGradleProperty("kapt.info.as.warnings", false)
-        val KAPT_INCLUDE_COMPILE_CLASSPATH = BooleanGradleProperty("kapt.include.compile.classpath", true)
-        val KAPT_KEEP_KDOC_COMMENTS_IN_STUBS = BooleanGradleProperty("kapt.keep.kdoc.comments.in.stubs", true)
-        val KAPT_USE_K2 = BooleanGradleProperty("kapt.use.k2", false)
-        val KAPT_DONT_WARN_ANNOTATION_PROCESSOR_DEPENDENCIES = BooleanGradleProperty(
-            "kapt.dont.warn.annotationProcessor.dependencies",
-            false
-        )
-    }
-
-    object StringProperties {
-        val CLASSLOADERS_CACHE_DISABLE_FOR_PROCESSORS = StringGradleProperty("kapt.classloaders.cache.disableForProcessors", "")
-    }
-
-    object IntProperties {
-        val CLASSLOADERS_CACHE_SIZE = IntGradleProperty("kapt.classloaders.cache.size", 0)
-    }
+    private val KAPT_VERBOSE = BooleanGradleProperty("kapt.verbose", false)
+    private val KAPT_INCREMENTAL_APT = BooleanGradleProperty(
+        "kapt.incremental.apt",
+        // Currently doesn't match the default value of KaptFlag.INCREMENTAL_APT,
+        // but it's fine (see https://github.com/JetBrains/kotlin/pull/3942#discussion_r532578690).
+        true
+    )
+    private val KAPT_INFO_AS_WARNINGS = BooleanGradleProperty("kapt.info.as.warnings", false)
+    private val KAPT_INCLUDE_COMPILE_CLASSPATH = BooleanGradleProperty("kapt.include.compile.classpath", true)
+    private val KAPT_KEEP_KDOC_COMMENTS_IN_STUBS = BooleanGradleProperty("kapt.keep.kdoc.comments.in.stubs", true)
+    private val KAPT_USE_K2 = BooleanGradleProperty("kapt.use.k2", false)
+    private val KAPT_DONT_WARN_ANNOTATION_PROCESSOR_DEPENDENCIES = BooleanGradleProperty(
+        "kapt.dont.warn.annotationProcessor.dependencies",
+        false
+    )
+    private val CLASSLOADERS_CACHE_DISABLE_FOR_PROCESSORS = StringGradleProperty("kapt.classloaders.cache.disableForProcessors", "")
+    private val CLASSLOADERS_CACHE_SIZE = IntGradleProperty("kapt.classloaders.cache.size", 0)
 
     fun isKaptVerbose(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_VERBOSE, project)
+        it.property(KAPT_VERBOSE, project)
     }
 
     fun isIncrementalKapt(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_INCREMENTAL_APT, project)
+        it.property(KAPT_INCREMENTAL_APT, project)
     }
 
     fun isInfoAsWarnings(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_INFO_AS_WARNINGS, project)
+        it.property(KAPT_INFO_AS_WARNINGS, project)
     }
 
     fun isIncludeCompileClasspath(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_INCLUDE_COMPILE_CLASSPATH, project)
+        it.property(KAPT_INCLUDE_COMPILE_CLASSPATH, project)
     }
 
     fun isKaptKeepKdocCommentsInStubs(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_KEEP_KDOC_COMMENTS_IN_STUBS, project)
+        it.property(KAPT_KEEP_KDOC_COMMENTS_IN_STUBS, project)
     }
 
     fun isUseK2(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_USE_K2, project)
+        it.property(KAPT_USE_K2, project)
     }
 
     fun isKaptDontWarnAnnotationProcessorDependencies(project: Project): Provider<Boolean> = project.propertiesService.flatMap {
-        it.property(BooleanProperties.KAPT_DONT_WARN_ANNOTATION_PROCESSOR_DEPENDENCIES, project)
+        it.property(KAPT_DONT_WARN_ANNOTATION_PROCESSOR_DEPENDENCIES, project)
     }
 
     fun getClassloadersCacheDisableForProcessors(project: Project): Provider<String> = project.propertiesService.flatMap {
-        it.property(StringProperties.CLASSLOADERS_CACHE_DISABLE_FOR_PROCESSORS, project)
+        it.property(CLASSLOADERS_CACHE_DISABLE_FOR_PROCESSORS, project)
     }
 
     fun getClassloadersCacheSize(project: Project): Provider<Int> = project.propertiesService.flatMap {
-        it.property(IntProperties.CLASSLOADERS_CACHE_SIZE, project)
+        it.property(CLASSLOADERS_CACHE_SIZE, project)
     }
 }
