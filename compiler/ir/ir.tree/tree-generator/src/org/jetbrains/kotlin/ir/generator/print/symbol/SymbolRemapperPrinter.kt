1e86f92ceba9dd59054b3535eca90b05afc755ea
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-05-06 17:50:23 +0200

    [IR] Remove unused method from SymbolRemapper
    Compose doesn't use this method since
    ae26a9f67d4470f5ab8becb0233ed0628dd06084

--- compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/print/symbol/SymbolRemapperPrinter.kt
+++ compiler/ir/ir.tree/tree-generator/src/org/jetbrains/kotlin/ir/generator/print/symbol/SymbolRemapperPrinter.kt
@@ -175,17 +175,6 @@ internal class SymbolRemapperInterfacePrinter(
         printBlock {
             println("val EMPTY: ${symbolRemapperType.render()} = ${emptySymbolRemapperType.simpleName}()")
         }
-        println()
-        println("// This method is left for compatibility with Compose. Do not use it, it will be removed soon.")
-        printFunctionDeclaration(
-            name = "getReferencedClassOrNull",
-            parameters = listOf(FunctionParameter("symbol", classSymbolType.copy(nullable = true))),
-            returnType = classSymbolType.copy(nullable = true),
-        )
-        println(" =")
-        withIndent {
-            println("symbol?.let { getReferencedClass(it) }")
-        }
     }
 }
 
