4c630535697ff313f33b6a45c97e7dc2836cc0ce
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-05-07 18:43:33 +0200

    [common backend] Merge LoggingContext & ErrorReportingContext interfaces
    These interfaces were always implemented together by
    CommonBackendContext and PhaseContext

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/LoggingContext.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/LoggingContext.kt
@@ -5,9 +5,19 @@
 
 package org.jetbrains.kotlin.backend.common
 
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
+import org.jetbrains.kotlin.ir.declarations.IrDeclaration
+import org.jetbrains.kotlin.ir.declarations.IrFile
+import org.jetbrains.kotlin.ir.util.getPackageFragment
 
-interface LoggingContext : ErrorReportingContext {
+interface LoggingContext {
+
+    val messageCollector: MessageCollector
+
     var inVerbosePhase: Boolean
 
     fun log(message: () -> String) {
@@ -17,3 +27,36 @@ interface LoggingContext : ErrorReportingContext {
         }
     }
 }
+
+fun LoggingContext.report(severity: CompilerMessageSeverity, element: IrElement?, irFile: IrFile?, message: String) {
+    val location = if (element != null && irFile != null) element.getCompilerMessageLocation(irFile) else null
+    messageCollector.report(severity, message, location)
+}
+
+fun LoggingContext.reportWarning(message: String, irFile: IrFile?, irElement: IrElement) {
+    report(CompilerMessageSeverity.WARNING, irElement, irFile, message)
+}
+
+fun LoggingContext.reportCompilationWarning(message: String) {
+    report(CompilerMessageSeverity.WARNING, null, null, message)
+}
+
+fun IrElement.getCompilerMessageLocation(containingFile: IrFile): CompilerMessageLocation? =
+    createCompilerMessageLocation(containingFile, this.startOffset, this.endOffset)
+
+fun IrBuilderWithScope.getCompilerMessageLocation(): CompilerMessageLocation? {
+    val declaration = this.scope.scopeOwnerSymbol.owner as? IrDeclaration ?: return null
+    val file = declaration.getPackageFragment() as? IrFile ?: return null
+    return createCompilerMessageLocation(file, startOffset, endOffset)
+}
+
+private fun createCompilerMessageLocation(containingFile: IrFile, startOffset: Int, endOffset: Int): CompilerMessageLocation? {
+    val sourceRangeInfo = containingFile.fileEntry.getSourceRangeInfo(startOffset, endOffset)
+    return CompilerMessageLocation.create(
+        path = sourceRangeInfo.filePath,
+        line = sourceRangeInfo.startLineNumber + 1,
+        column = sourceRangeInfo.startColumnNumber + 1,
+        lineContent = null // TODO: retrieve the line content.
+    )
+}
+
