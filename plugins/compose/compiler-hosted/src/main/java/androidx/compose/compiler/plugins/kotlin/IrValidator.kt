ff37be4e68f5000c9547d608b96edf142f0dbed3
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-05-09 17:28:52 +0200

    [compose] Use IrValidator from Kotlin instead of the copy-pasted one

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/IrValidator.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/IrValidator.kt
@@ -17,60 +17,25 @@
 package androidx.compose.compiler.plugins.kotlin
 
 import androidx.compose.compiler.plugins.kotlin.lower.dumpSrc
-import org.jetbrains.kotlin.backend.common.CheckIrElementVisitor
-import org.jetbrains.kotlin.backend.common.IrValidatorConfig
-import org.jetbrains.kotlin.backend.common.ScopeValidator
-import org.jetbrains.kotlin.backend.common.checkDeclarationParents
+import org.jetbrains.kotlin.backend.common.IrValidationError
+import org.jetbrains.kotlin.backend.common.performBasicIrValidation
 import org.jetbrains.kotlin.config.IrVerificationMode
 import org.jetbrains.kotlin.ir.IrBuiltIns
-import org.jetbrains.kotlin.ir.IrElement
-import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.declarations.name
 import org.jetbrains.kotlin.ir.util.render
-import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
-import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
-import org.jetbrains.kotlin.ir.visitors.acceptVoid
 
 fun validateIr(fragment: IrModuleFragment, irBuiltIns: IrBuiltIns, mode: IrVerificationMode) {
     if (mode == IrVerificationMode.NONE) return
-    val validatorConfig = IrValidatorConfig(
-        abortOnError = mode == IrVerificationMode.ERROR,
-        ensureAllNodesAreDifferent = true,
-        checkTypes = false, // This should be enabled, the fact this doesn't work is a Compose bug.
-        checkDescriptors = false,
+    performBasicIrValidation(
+        fragment,
+        irBuiltIns,
         checkProperties = true,
-        checkScopes = false
-    )
-    fragment.accept(IrValidator(irBuiltIns, validatorConfig), null)
-    fragment.checkDeclarationParents()
-}
-
-// TODO: Replace with IrValidator validator when upstream is compatible with PluginContext
-class IrValidator(val irBuiltIns: IrBuiltIns, val config: IrValidatorConfig) :
-    IrElementVisitorVoid {
-
-    var currentFile: IrFile? = null
-
-    override fun visitFile(declaration: IrFile) {
-        currentFile = declaration
-        super.visitFile(declaration)
-        if (config.checkScopes) {
-            ScopeValidator(this::error).check(declaration)
-        }
-    }
-
-    private fun error(element: IrElement, message: String) {
-        throw Error(
+        checkTypes = false, // This should be enabled, the fact this doesn't work is a Compose bug.
+    ) { file, element, message ->
+        throw IrValidationError(
             "Validation error ($message) for ${element.dumpSrc()}...  ${element.render()} in" +
-                " ${currentFile?.name ?: "???"}"
+                    " ${file?.name ?: "???"}"
         )
-    }
-
-    private val elementChecker = CheckIrElementVisitor(irBuiltIns, this::error, config)
-
-    override fun visitElement(element: IrElement) {
-        element.acceptVoid(elementChecker)
-        element.acceptChildrenVoid(this)
     }
 }
