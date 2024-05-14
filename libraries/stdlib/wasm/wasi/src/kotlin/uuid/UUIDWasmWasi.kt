cb4d364ae1d721c2fe4a7abaa2d463c32447a289
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-05-15 00:40:17 +0300

    fixup! Implement WasmWasi random UUID creation
    Load bytes instead of longs and use existing uuidFromRandomBytes.
    Simplifies implementation.

--- libraries/stdlib/wasm/wasi/src/kotlin/uuid/UUIDWasmWasi.kt
+++ libraries/stdlib/wasm/wasi/src/kotlin/uuid/UUIDWasmWasi.kt
@@ -15,20 +15,12 @@ internal actual fun secureRandomUUID(): UUID {
         var memory = allocator.allocate(UUID.SIZE_BYTES)
         val ret = wasiRawRandomGet(memory.address.toInt(), UUID.SIZE_BYTES)
         return if (ret == 0) {
-            val msb = memory.loadLong()
-            memory += Long.SIZE_BYTES
-            val lsb = memory.loadLong()
-            uuidFromRandomLongs(msb, lsb)
+            val randomBytes = ByteArray(UUID.SIZE_BYTES) {
+                memory.loadByte().also { memory += 1 }
+            }
+            uuidFromRandomBytes(randomBytes)
         } else {
             throw WasiError(WasiErrorCode.entries[ret])
         }
     }
-}
-
-internal fun uuidFromRandomLongs(msb: Long, lsb: Long): UUID {
-    var _msb = msb and 0xf000L.inv()        /* clear version        */
-    _msb = _msb or 0x4000                   /* set to version 4     */
-    var _lsb = lsb and 0x3fffffffffffffff   /* clear variant        */
-    _lsb = _lsb or 0x7fffffffffffffff.inv() /* set to IETF variant  */
-    return UUID.fromLongs(_msb, _lsb)
 }
