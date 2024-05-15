ef0dc957c0055ff5c4b79ea860d949ad03526e69
Juan Rodriguez <juanrdz@google.com>
2024-05-15 20:03:29 +0000

    Compose version bump to 1.8.0-alpha01
    Change-Id: I5eed5ae4fe1be50300483964c7e0d73fb637d4b2 ( https://android-review.googlesource.com/q/I5eed5ae4fe1be50300483964c7e0d73fb637d4b2 )
    
    Moved from: https://github.com/androidx/androidx/commit/010336b8bcc141a05c1a5b83ceedad736e259b1f

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/VersionChecker.kt
@@ -147,6 +147,7 @@ class VersionChecker(val context: IrPluginContext, private val messageCollector:
             12700 to "1.7.0-alpha08",
             12800 to "1.7.0-beta01",
             12900 to "1.7.0-beta02",
+            13000 to "1.8.0-alpha01",
         )
 
         /**
