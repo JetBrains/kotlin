8c3a176fc25afdadc1dd9c771c3df83a1b99bf2b
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2024-05-15 16:16:16 +0200

    [FIR] Transform children of `FirErrorTypeRefImpl` while transforming `partiallyResolvedTypeRef`
    ^KT-67503 Fixed

--- compiler/testData/diagnostics/tests/exceptions/transformErrorTypeChildrenWhileTransformingPartiallyResolvedType.fir.kt
+++
@@ -1,9 +0,0 @@
-// ISSUE: KT-67503
-
-@Target(AnnotationTarget.TYPE)
-annotation class Ann
-
-class Inv<T>
-
-fun <T> foo(x: Inv<@Ann <!SYNTAX!>*<!>>) {}
-fun <T> foo2(x: Inv<@Ann<!SYNTAX!><!> >) {}
--- compiler/testData/diagnostics/tests/exceptions/transformErrorTypeChildrenWhileTransformingPartiallyResolvedType.kt
+++ compiler/testData/diagnostics/tests/exceptions/transformErrorTypeChildrenWhileTransformingPartiallyResolvedType.kt
@@ -1,3 +1,4 @@
+// FIR_IDENTICAL
 // ISSUE: KT-67503
 
 @Target(AnnotationTarget.TYPE)
