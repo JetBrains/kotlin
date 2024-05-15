6f5dc64a88f68dfa6e1a63ec5d2c0bc42a3a3986
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-05-15 19:12:13 +0200

    [stdlib] Remaining actual builtins for JS
    Co-authored-by: Ivan Kochurkin <ivan.kochurkin@jetbrains.com>

--- libraries/stdlib/jvm/builtins/Collections.kt
+++ libraries/stdlib/jvm/builtins/Collections.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -12,11 +12,11 @@ import kotlin.internal.PlatformDependent
  * be iterated over.
  * @param T the type of element being iterated over. The iterator is covariant in its element type.
  */
-public interface Iterable<out T> {
+public actual interface Iterable<out T> {
     /**
      * Returns an iterator over the elements of this object.
      */
-    public operator fun iterator(): Iterator<T>
+    public actual operator fun iterator(): Iterator<T>
 }
 
 /**
@@ -24,11 +24,11 @@ public interface Iterable<out T> {
  * be iterated over and that supports removing elements during iteration.
  * @param T the type of element being iterated over. The mutable iterator is invariant in its element type.
  */
-public interface MutableIterable<out T> : Iterable<T> {
+public actual interface MutableIterable<out T> : Iterable<T> {
     /**
      * Returns an iterator over the elements of this sequence that supports removing elements during iteration.
      */
-    override fun iterator(): MutableIterator<T>
+    actual override fun iterator(): MutableIterator<T>
 }
 
 /**
@@ -36,30 +36,30 @@ public interface MutableIterable<out T> : Iterable<T> {
  * read/write access is supported through the [MutableCollection] interface.
  * @param E the type of elements contained in the collection. The collection is covariant in its element type.
  */
-public interface Collection<out E> : Iterable<E> {
+public actual interface Collection<out E> : Iterable<E> {
     // Query Operations
     /**
      * Returns the size of the collection.
      */
-    public val size: Int
+    public actual val size: Int
 
     /**
      * Returns `true` if the collection is empty (contains no elements), `false` otherwise.
      */
-    public fun isEmpty(): Boolean
+    public actual fun isEmpty(): Boolean
 
     /**
      * Checks if the specified element is contained in this collection.
      */
-    public operator fun contains(element: @UnsafeVariance E): Boolean
+    public actual operator fun contains(element: @UnsafeVariance E): Boolean
 
-    override fun iterator(): Iterator<E>
+    actual override fun iterator(): Iterator<E>
 
     // Bulk Operations
     /**
      * Checks if all elements in the specified collection are contained in this collection.
      */
-    public fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
+    public actual fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
 }
 
 /**
@@ -67,9 +67,9 @@ public interface Collection<out E> : Iterable<E> {
  *
  * @param E the type of elements contained in the collection. The mutable collection is invariant in its element type.
  */
-public interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
+public actual interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
     // Query Operations
-    override fun iterator(): MutableIterator<E>
+    actual override fun iterator(): MutableIterator<E>
 
     // Modification Operations
     /**
@@ -78,7 +78,7 @@ public interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
      * @return `true` if the element has been added, `false` if the collection does not support duplicates
      * and the element is already contained in the collection.
      */
-    public fun add(element: E): Boolean
+    public actual fun add(element: E): Boolean
 
     /**
      * Removes a single instance of the specified element from this
@@ -86,7 +86,7 @@ public interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
      *
      * @return `true` if the element has been successfully removed; `false` if it was not present in the collection.
      */
-    public fun remove(element: E): Boolean
+    public actual fun remove(element: E): Boolean
 
     // Bulk Modification Operations
     /**
@@ -94,26 +94,26 @@ public interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
      *
      * @return `true` if any of the specified elements was added to the collection, `false` if the collection was not modified.
      */
-    public fun addAll(elements: Collection<E>): Boolean
+    public actual fun addAll(elements: Collection<E>): Boolean
 
     /**
      * Removes all of this collection's elements that are also contained in the specified collection.
      *
      * @return `true` if any of the specified elements was removed from the collection, `false` if the collection was not modified.
      */
-    public fun removeAll(elements: Collection<E>): Boolean
+    public actual fun removeAll(elements: Collection<E>): Boolean
 
     /**
      * Retains only the elements in this collection that are contained in the specified collection.
      *
      * @return `true` if any element was removed from the collection, `false` if the collection was not modified.
      */
-    public fun retainAll(elements: Collection<E>): Boolean
+    public actual fun retainAll(elements: Collection<E>): Boolean
 
     /**
      * Removes all elements from this collection.
      */
-    public fun clear(): Unit
+    public actual fun clear(): Unit
 }
 
 /**
@@ -121,46 +121,46 @@ public interface MutableCollection<E> : Collection<E>, MutableIterable<E> {
  * read/write access is supported through the [MutableList] interface.
  * @param E the type of elements contained in the list. The list is covariant in its element type.
  */
-public interface List<out E> : Collection<E> {
+public actual interface List<out E> : Collection<E> {
     // Query Operations
 
-    override val size: Int
-    override fun isEmpty(): Boolean
-    override fun contains(element: @UnsafeVariance E): Boolean
-    override fun iterator(): Iterator<E>
+    actual override val size: Int
+    actual override fun isEmpty(): Boolean
+    actual override fun contains(element: @UnsafeVariance E): Boolean
+    actual override fun iterator(): Iterator<E>
 
     // Bulk Operations
-    override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
+    actual override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
 
     // Positional Access Operations
     /**
      * Returns the element at the specified index in the list.
      */
-    public operator fun get(index: Int): E
+    public actual operator fun get(index: Int): E
 
     // Search Operations
     /**
      * Returns the index of the first occurrence of the specified element in the list, or -1 if the specified
      * element is not contained in the list.
      */
-    public fun indexOf(element: @UnsafeVariance E): Int
+    public actual fun indexOf(element: @UnsafeVariance E): Int
 
     /**
      * Returns the index of the last occurrence of the specified element in the list, or -1 if the specified
      * element is not contained in the list.
      */
-    public fun lastIndexOf(element: @UnsafeVariance E): Int
+    public actual fun lastIndexOf(element: @UnsafeVariance E): Int
 
     // List Iterators
     /**
      * Returns a list iterator over the elements in this list (in proper sequence).
      */
-    public fun listIterator(): ListIterator<E>
+    public actual fun listIterator(): ListIterator<E>
 
     /**
      * Returns a list iterator over the elements in this list (in proper sequence), starting at the specified [index].
      */
-    public fun listIterator(index: Int): ListIterator<E>
+    public actual fun listIterator(index: Int): ListIterator<E>
 
     // View
     /**
@@ -169,23 +169,23 @@ public interface List<out E> : Collection<E> {
      *
      * Structural changes in the base list make the behavior of the view undefined.
      */
-    public fun subList(fromIndex: Int, toIndex: Int): List<E>
+    public actual fun subList(fromIndex: Int, toIndex: Int): List<E>
 }
 
 /**
  * A generic ordered collection of elements that supports adding and removing elements.
  * @param E the type of elements contained in the list. The mutable list is invariant in its element type.
  */
-public interface MutableList<E> : List<E>, MutableCollection<E> {
+public actual interface MutableList<E> : List<E>, MutableCollection<E> {
     // Modification Operations
     /**
      * Adds the specified element to the end of this list.
      *
      * @return `true` because the list is always modified as the result of this operation.
      */
-    override fun add(element: E): Boolean
+    actual override fun add(element: E): Boolean
 
-    override fun remove(element: E): Boolean
+    actual override fun remove(element: E): Boolean
 
     // Bulk Modification Operations
     /**
@@ -202,11 +202,11 @@ public interface MutableList<E> : List<E>, MutableCollection<E> {
      *
      * @return `true` if the list was changed as the result of the operation.
      */
-    public fun addAll(index: Int, elements: Collection<E>): Boolean
+    public actual fun addAll(index: Int, elements: Collection<E>): Boolean
 
-    override fun removeAll(elements: Collection<E>): Boolean
-    override fun retainAll(elements: Collection<E>): Boolean
-    override fun clear(): Unit
+    actual override fun removeAll(elements: Collection<E>): Boolean
+    actual override fun retainAll(elements: Collection<E>): Boolean
+    actual override fun clear(): Unit
 
     // Positional Access Operations
     /**
@@ -214,27 +214,27 @@ public interface MutableList<E> : List<E>, MutableCollection<E> {
      *
      * @return the element previously at the specified position.
      */
-    public operator fun set(index: Int, element: E): E
+    public actual operator fun set(index: Int, element: E): E
 
     /**
      * Inserts an element into the list at the specified [index].
      */
-    public fun add(index: Int, element: E): Unit
+    public actual fun add(index: Int, element: E): Unit
 
     /**
      * Removes an element at the specified [index] from the list.
      *
      * @return the element that has been removed.
      */
-    public fun removeAt(index: Int): E
+    public actual fun removeAt(index: Int): E
 
     // List Iterators
-    override fun listIterator(): MutableListIterator<E>
+    actual override fun listIterator(): MutableListIterator<E>
 
-    override fun listIterator(index: Int): MutableListIterator<E>
+    actual override fun listIterator(index: Int): MutableListIterator<E>
 
     // View
-    override fun subList(fromIndex: Int, toIndex: Int): MutableList<E>
+    actual override fun subList(fromIndex: Int, toIndex: Int): MutableList<E>
 }
 
 /**
@@ -243,16 +243,16 @@ public interface MutableList<E> : List<E>, MutableCollection<E> {
  * read/write access is supported through the [MutableSet] interface.
  * @param E the type of elements contained in the set. The set is covariant in its element type.
  */
-public interface Set<out E> : Collection<E> {
+public actual interface Set<out E> : Collection<E> {
     // Query Operations
 
-    override val size: Int
-    override fun isEmpty(): Boolean
-    override fun contains(element: @UnsafeVariance E): Boolean
-    override fun iterator(): Iterator<E>
+    actual override val size: Int
+    actual override fun isEmpty(): Boolean
+    actual override fun contains(element: @UnsafeVariance E): Boolean
+    actual override fun iterator(): Iterator<E>
 
     // Bulk Operations
-    override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
+    actual override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean
 }
 
 /**
@@ -260,9 +260,9 @@ public interface Set<out E> : Collection<E> {
  * adding and removing elements.
  * @param E the type of elements contained in the set. The mutable set is invariant in its element type.
  */
-public interface MutableSet<E> : Set<E>, MutableCollection<E> {
+public actual interface MutableSet<E> : Set<E>, MutableCollection<E> {
     // Query Operations
-    override fun iterator(): MutableIterator<E>
+    actual override fun iterator(): MutableIterator<E>
 
     // Modification Operations
 
@@ -271,16 +271,16 @@ public interface MutableSet<E> : Set<E>, MutableCollection<E> {
      *
      * @return `true` if the element has been added, `false` if the element is already contained in the set.
      */
-    override fun add(element: E): Boolean
+    actual override fun add(element: E): Boolean
 
-    override fun remove(element: E): Boolean
+    actual override fun remove(element: E): Boolean
 
     // Bulk Modification Operations
 
-    override fun addAll(elements: Collection<E>): Boolean
-    override fun removeAll(elements: Collection<E>): Boolean
-    override fun retainAll(elements: Collection<E>): Boolean
-    override fun clear(): Unit
+    actual override fun addAll(elements: Collection<E>): Boolean
+    actual override fun removeAll(elements: Collection<E>): Boolean
+    actual override fun retainAll(elements: Collection<E>): Boolean
+    actual override fun clear(): Unit
 }
 
 /**
@@ -292,32 +292,32 @@ public interface MutableSet<E> : Set<E>, MutableCollection<E> {
  *          can accept key as a parameter (of [containsKey] for example) and return it in [keys] set.
  * @param V the type of map values. The map is covariant in its value type.
  */
-public interface Map<K, out V> {
+public actual interface Map<K, out V> {
     // Query Operations
     /**
      * Returns the number of key/value pairs in the map.
      */
-    public val size: Int
+    public actual val size: Int
 
     /**
      * Returns `true` if the map is empty (contains no elements), `false` otherwise.
      */
-    public fun isEmpty(): Boolean
+    public actual fun isEmpty(): Boolean
 
     /**
      * Returns `true` if the map contains the specified [key].
      */
-    public fun containsKey(key: K): Boolean
+    public actual fun containsKey(key: K): Boolean
 
     /**
      * Returns `true` if the map maps one or more keys to the specified [value].
      */
-    public fun containsValue(value: @UnsafeVariance V): Boolean
+    public actual fun containsValue(value: @UnsafeVariance V): Boolean
 
     /**
      * Returns the value corresponding to the given [key], or `null` if such a key is not present in the map.
      */
-    public operator fun get(key: K): V?
+    public actual operator fun get(key: K): V?
 
     /**
      * Returns the value corresponding to the given [key], or [defaultValue] if such a key is not present in the map.
@@ -326,7 +326,7 @@ public interface Map<K, out V> {
      */
     @SinceKotlin("1.1")
     @PlatformDependent
-    public fun getOrDefault(key: K, defaultValue: @UnsafeVariance V): V {
+    public actual fun getOrDefault(key: K, defaultValue: @UnsafeVariance V): V {
         // See default implementation in JDK sources
         throw NotImplementedError()
     }
@@ -335,31 +335,31 @@ public interface Map<K, out V> {
     /**
      * Returns a read-only [Set] of all keys in this map.
      */
-    public val keys: Set<K>
+    public actual val keys: Set<K>
 
     /**
      * Returns a read-only [Collection] of all values in this map. Note that this collection may contain duplicate values.
      */
-    public val values: Collection<V>
+    public actual val values: Collection<V>
 
     /**
      * Returns a read-only [Set] of all key/value pairs in this map.
      */
-    public val entries: Set<Map.Entry<K, V>>
+    public actual val entries: Set<Map.Entry<K, V>>
 
     /**
      * Represents a key/value pair held by a [Map].
      */
-    public interface Entry<out K, out V> {
+    public actual interface Entry<out K, out V> {
         /**
          * Returns the key of this key/value pair.
          */
-        public val key: K
+        public actual val key: K
 
         /**
          * Returns the value of this key/value pair.
          */
-        public val value: V
+        public actual val value: V
     }
 }
 
@@ -369,21 +369,21 @@ public interface Map<K, out V> {
  * @param K the type of map keys. The map is invariant in its key type.
  * @param V the type of map values. The mutable map is invariant in its value type.
  */
-public interface MutableMap<K, V> : Map<K, V> {
+public actual interface MutableMap<K, V> : Map<K, V> {
     // Modification Operations
     /**
      * Associates the specified [value] with the specified [key] in the map.
      *
      * @return the previous value associated with the key, or `null` if the key was not present in the map.
      */
-    public fun put(key: K, value: V): V?
+    public actual fun put(key: K, value: V): V?
 
     /**
      * Removes the specified key and its corresponding value from this map.
      *
      * @return the previous value associated with the key, or `null` if the key was not present in the map.
      */
-    public fun remove(key: K): V?
+    public actual fun remove(key: K): V?
 
     /**
      * Removes the entry for the specified key only if it is mapped to the specified value.
@@ -392,7 +392,7 @@ public interface MutableMap<K, V> : Map<K, V> {
      */
     @SinceKotlin("1.1")
     @PlatformDependent
-    public fun remove(key: K, value: V): Boolean {
+    public actual fun remove(key: K, value: V): Boolean {
         // See default implementation in JDK sources
         return true
     }
@@ -401,38 +401,38 @@ public interface MutableMap<K, V> : Map<K, V> {
     /**
      * Updates this map with key/value pairs from the specified map [from].
      */
-    public fun putAll(from: Map<out K, V>): Unit
+    public actual fun putAll(from: Map<out K, V>): Unit
 
     /**
      * Removes all elements from this map.
      */
-    public fun clear(): Unit
+    public actual fun clear(): Unit
 
     // Views
     /**
      * Returns a [MutableSet] of all keys in this map.
      */
-    override val keys: MutableSet<K>
+    actual override val keys: MutableSet<K>
 
     /**
      * Returns a [MutableCollection] of all values in this map. Note that this collection may contain duplicate values.
      */
-    override val values: MutableCollection<V>
+    actual override val values: MutableCollection<V>
 
     /**
      * Returns a [MutableSet] of all key/value pairs in this map.
      */
-    override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
+    actual override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
 
     /**
      * Represents a key/value pair held by a [MutableMap].
      */
-    public interface MutableEntry<K, V> : Map.Entry<K, V> {
+    public actual interface MutableEntry<K, V> : Map.Entry<K, V> {
         /**
          * Changes the value associated with the key of this entry.
          *
          * @return the previous value corresponding to the key.
          */
-        public fun setValue(newValue: V): V
+        public actual fun setValue(newValue: V): V
     }
 }
