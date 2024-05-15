4969e4bbdf4613067e207699ef2c1f52c4594c26
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-05-15 19:10:56 +0200

    [stdlib] Remaining expect builtins including top-level callables
    - Annotate expects with ActualizeByJvmBuiltinProvider
    - Remove enum and collection members from expect declarations that are available only on JVM
    
    Co-authored-by: Ivan Kochurkin <ivan.kochurkin@jetbrains.com>

--- libraries/stdlib/src/kotlin/Number.kt
+++ libraries/stdlib/src/kotlin/Number.kt
@@ -1,25 +1,17 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package kotlin
 
+import kotlin.internal.ActualizeByJvmBuiltinProvider
+
 /**
  * Superclass for all platform classes representing numeric values.
  */
-public abstract class Number {
+@ActualizeByJvmBuiltinProvider
+public expect abstract class Number {
     /**
      * Returns the value of this number as a [Double], which may involve rounding.
      */
@@ -45,9 +37,9 @@ public abstract class Number {
      */
     @Deprecated("Direct conversion to Char is deprecated. Use toInt().toChar() or Char constructor instead.\nIf you override toChar() function in your Number inheritor, it's recommended to gradually deprecate the overriding function and then remove it.\nSee https://youtrack.jetbrains.com/issue/KT-46465 for details about the migration", ReplaceWith("this.toInt().toChar()"))
     @DeprecatedSinceKotlin(warningSince = "1.9", errorSince = "2.3")
-    public open fun toChar(): Char {
+    public open fun toChar(): Char /*{
         return toInt().toChar()
-    }
+    }*/
 
     /**
      * Returns the value of this number as a [Short], which may involve rounding or truncation.
--- libraries/stdlib/src/kotlin/Throwable.kt
+++ libraries/stdlib/src/kotlin/Throwable.kt
@@ -1,20 +1,11 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package kotlin
+
+import kotlin.internal.ActualizeByJvmBuiltinProvider
 
 /**
  * The base class for all errors and exceptions. Only instances of this class can be thrown or caught.
@@ -22,10 +13,13 @@ package kotlin
  * @param message the detail message string.
  * @param cause the cause of this throwable.
  */
-public open class Throwable(open val message: String?, open val cause: Throwable?) {
-    constructor(message: String?) : this(message, null)
+@ActualizeByJvmBuiltinProvider
+public expect open class Throwable {
+    public open val message: String?
+    public open val cause: Throwable?
 
-    constructor(cause: Throwable?) : this(cause?.toString(), cause)
-
-    constructor() : this(null, null)
+    public constructor()
+    public constructor(message: String?)
+    public constructor(cause: Throwable?)
+    public constructor(message: String?, cause: Throwable?)
 }
