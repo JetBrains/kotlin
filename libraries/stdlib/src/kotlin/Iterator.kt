0e4b1dd526a94866c83d2f810dcf7c756c674d37
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-05-15 19:10:56 +0200

    [stdlib] Remaining expect builtins including top-level callables
    - Annotate expects with ActualizeByJvmBuiltinProvider
    - Remove enum and collection members from expect declarations that are available only on JVM
    
    KT-65526
    
    Co-authored-by: Ivan Kochurkin <ivan.kochurkin@jetbrains.com>

--- libraries/stdlib/src/kotlin/Iterator.kt
+++ libraries/stdlib/src/kotlin/Iterator.kt
@@ -1,26 +1,18 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package kotlin.collections
+
+import kotlin.internal.ActualizeByJvmBuiltinProvider
 
 /**
  * An iterator over a collection or another entity that can be represented as a sequence of elements.
  * Allows to sequentially access the elements.
  */
-public interface Iterator<out T> {
+@ActualizeByJvmBuiltinProvider
+public expect interface Iterator<out T> {
     /**
      * Returns the next element in the iteration.
      *
@@ -38,7 +30,8 @@ public interface Iterator<out T> {
  * An iterator over a mutable collection. Provides the ability to remove elements while iterating.
  * @see MutableCollection.iterator
  */
-public interface MutableIterator<out T> : Iterator<T> {
+@ActualizeByJvmBuiltinProvider
+public expect interface MutableIterator<out T> : Iterator<T> {
     /**
      * Removes from the underlying collection the last element returned by this iterator.
      *
@@ -52,7 +45,8 @@ public interface MutableIterator<out T> : Iterator<T> {
  * An iterator over a collection that supports indexed access.
  * @see List.listIterator
  */
-public interface ListIterator<out T> : Iterator<T> {
+@ActualizeByJvmBuiltinProvider
+public expect interface ListIterator<out T> : Iterator<T> {
     // Query Operations
     override fun next(): T
     override fun hasNext(): Boolean
@@ -89,7 +83,8 @@ public interface ListIterator<out T> : Iterator<T> {
  * to add, modify and remove elements while iterating.
  * @see MutableList.listIterator
  */
-public interface MutableListIterator<T> : ListIterator<T>, MutableIterator<T> {
+@ActualizeByJvmBuiltinProvider
+public expect interface MutableListIterator<T> : ListIterator<T>, MutableIterator<T> {
     // Query Operations
     override fun next(): T
     override fun hasNext(): Boolean
