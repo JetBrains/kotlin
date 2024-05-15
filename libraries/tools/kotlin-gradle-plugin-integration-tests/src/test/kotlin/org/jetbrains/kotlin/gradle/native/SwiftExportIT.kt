8bd4af5222a84efe1b15a706e3a46e539379b7d2
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2024-05-15 16:39:13 +0200

    KT-68231 Add swift export multiple source sets integration test

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/SwiftExportIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/SwiftExportIT.kt
@@ -14,6 +14,7 @@ import java.nio.file.Path
 import kotlin.io.path.appendText
 import kotlin.io.path.createFile
 import kotlin.io.path.exists
+import kotlin.io.path.readText
 
 @OsCondition(supportedOn = [OS.MAC], enabledOnCI = [OS.MAC])
 @DisplayName("Tests for Swift Export")
@@ -60,6 +61,48 @@ class SwiftExportIT : KGPBaseTest() {
                 assertDirectoryInProjectExists("shared/build/iosArm64DebugSPMPackage")
                 assertDirectoryInProjectExists("shared/build/iosArm64DebugSwiftExport")
             }
+        }
+    }
+
+    @DisplayName("Swift Export with multiple source sets")
+    @OptIn(EnvironmentalVariablesOverride::class)
+    @GradleTest
+    fun testMultipleSourceSetsWithSwiftExportEnabled(
+        gradleVersion: GradleVersion,
+        @TempDir testBuildDir: Path,
+    ) {
+        nativeProject(
+            "simpleSwiftExport",
+            gradleVersion,
+        ) {
+            projectPath.enableSwiftExport()
+
+            val environmentVariables = EnvironmentalVariables(
+                "CONFIGURATION" to "Debug",
+                "SDK_NAME" to "iphoneos123",
+                "ARCHS" to "arm64",
+                "ONLY_ACTIVE_ARCH" to "YES",
+                "TARGET_BUILD_DIR" to testBuildDir.toString(),
+                "FRAMEWORKS_FOLDER_PATH" to "build/xcode-derived",
+                "PLATFORM_NAME" to "iphoneos",
+                "BUILT_PRODUCTS_DIR" to projectPath.resolve("shared/build/builtProductsDir").toString(),
+            )
+
+            build(":shared:embedAndSignAppleFrameworkForXcode", environmentVariables = environmentVariables) {
+                assertTasksExecuted(":shared:iosArm64DebugCopySPMIntermediates")
+                assertDirectoryInProjectExists("shared/build/iosArm64DebugSwiftExport")
+            }
+
+            val swiftFile = projectPath
+                .resolve("shared/build/iosArm64DebugSwiftExport/swiftIntermediates/KotlinAPI.swift")
+                .readText()
+
+            assert(swiftFile.contains("iosBar()")) { "Swift file doesn't contain iosBar() from iosMain source set" }
+            assert(swiftFile.contains("iosFoo()")) { "Swift file doesn't contain iosFoo() from iosMain source set" }
+
+            assert(swiftFile.contains("bar()")) { "Swift file doesn't contain bar() from commonMain source set" }
+            assert(swiftFile.contains("foo()")) { "Swift file doesn't contain foo() from commonMain source set" }
+            assert(swiftFile.contains("foobar(")) { "Swift file doesn't contain foobar( from commonMain source set" }
         }
     }
 }
