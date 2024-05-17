4d3cac90a58b796b3d8ed12f651d9e0db49dabbe
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-05-17 15:32:31 +0300

    [KDF] refactor logic around hardcoded "Token" prefix
    Moved from https://github.com/Kotlin/dataframe/commit/1c7f510760afbf7ec0c34d051a0c028cbee7b596

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlin/fir/dataframe/extensions/ReturnTypeBasedReceiverInjector.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlin/fir/dataframe/extensions/ReturnTypeBasedReceiverInjector.kt
@@ -2,36 +2,40 @@ package org.jetbrains.kotlin.fir.dataframe.extensions
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.dataframe.Names
+import org.jetbrains.kotlin.fir.dataframe.callShapeData
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.fir.extensions.FirExpressionResolutionExtension
 import org.jetbrains.kotlin.fir.scopes.collectAllProperties
 import org.jetbrains.kotlin.fir.scopes.impl.declaredMemberScope
+import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
 import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
 import org.jetbrains.kotlin.fir.types.classId
 import org.jetbrains.kotlin.fir.types.resolvedType
-import org.jetbrains.kotlin.fir.types.toSymbol
+import org.jetbrains.kotlin.fir.types.toRegularClassSymbol
 
 class ReturnTypeBasedReceiverInjector(session: FirSession) : FirExpressionResolutionExtension(session) {
     override fun addNewImplicitReceivers(functionCall: FirFunctionCall): List<ConeKotlinType> {
-        val token = generatedTokenOrNull(functionCall) ?: return emptyList()
-        val symbol = token.toSymbol(session)!! as FirRegularClassSymbol
+        val symbol = generatedTokenOrNull(functionCall) ?: return emptyList()
         return symbol.declaredMemberScope(session, FirResolvePhase.DECLARATIONS).collectAllProperties()
             .filterIsInstance<FirPropertySymbol>()
             .filter { it.resolvedReturnType.classId?.shortClassName?.asString()?.startsWith("Scope") ?: false }
             .map { it.resolvedReturnType }
     }
 
-    private fun generatedTokenOrNull(call: FirFunctionCall): ConeClassLikeType? {
+    @OptIn(SymbolInternals::class)
+    private fun generatedTokenOrNull(call: FirFunctionCall): FirRegularClassSymbol? {
         val callReturnType = call.resolvedType
         if (callReturnType.classId != Names.DF_CLASS_ID) return null
         val rootMarker = callReturnType.typeArguments[0]
         if (rootMarker !is ConeClassLikeType) {
             return null
         }
-        return rootMarker.takeIf { it.classId?.shortClassName?.asString()?.startsWith("Token") == true }
+
+        val symbol = rootMarker.toRegularClassSymbol(session)
+        return symbol.takeIf { it?.fir?.callShapeData != null }
     }
 }
