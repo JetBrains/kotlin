77ba284131e1cd6deb8c52fa24fb3965b8ae02e9
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-05-17 20:12:58 +0200

    Add test showing the state for KT-68193/KT-67804 and LinkedList
    Note that everything works as expected for JDK21, but no warnings/errors
    reported for older JDKs, thus it might be unexpected unwarned breaking
    change once users finally update their JDK to 21 or later.
    
    ^KT-68193 In Progress
    ^KT-67804 In Progress

---
+++ compiler/testData/diagnostics/tests/testsWithJava21/newListMethodsNullabilityLinkedList21.kt
@@ -0,0 +1,36 @@
+// LANGUAGE_VERSION: 2.1
+// ALLOW_DANGEROUS_LANGUAGE_VERSION_TESTING
+// DIAGNOSTICS: -JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE -ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE
+// JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE accidentally reported (K1 only) on x.addFirst/addLast/removeFirst/removeLast
+// ISSUE: KT-68193
+// ISSUE: KT-67804
+import java.util.LinkedList
+
+fun <E1> addFirstLast(sl: LinkedList<String>, el: LinkedList<E1>, ev: E1) {
+
+    sl.addFirst(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>)
+    sl.addFirst("")
+    sl.addLast(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>)
+    sl.addLast("")
+
+    el.addFirst(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>)
+    el.addFirst(ev)
+    el.addLast(<!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>)
+    el.addLast(ev)
+}
+
+fun removeFirstLastLinkedListString(s: LinkedList<String>) {
+    var x1 = s.removeFirst()
+    x1 = <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>
+
+    var x2 = s.removeLast()
+    x1 = <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>
+}
+
+fun <E> removeFirstLastArrayListE(s: LinkedList<E>) {
+    var x1 = s.removeFirst()
+    x1 = <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>
+
+    var x2 = s.removeLast()
+    x1 = <!NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS!>null<!>
+}
