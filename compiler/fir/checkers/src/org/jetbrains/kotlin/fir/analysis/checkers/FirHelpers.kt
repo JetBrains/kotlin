5c96a827b7e43c18997ebd0ec94e8428607e92d1
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-05-20 14:27:18 +0300

    [FIR] Move `FirProjectionRelationChecker.checkTypeRef`
    It will be used in a different place in the next commit.
    
    ^KT-67764

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirHelpers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirHelpers.kt
@@ -46,6 +46,7 @@ import org.jetbrains.kotlin.resolve.AnnotationTargetList
 import org.jetbrains.kotlin.resolve.AnnotationTargetLists
 import org.jetbrains.kotlin.types.AbstractTypeChecker
 import org.jetbrains.kotlin.types.TypeApproximatorConfiguration
+import org.jetbrains.kotlin.types.Variance
 import org.jetbrains.kotlin.types.model.KotlinTypeMarker
 import org.jetbrains.kotlin.types.model.TypeCheckerProviderContext
 import org.jetbrains.kotlin.util.ImplementationStatus
@@ -852,3 +853,62 @@ fun isExplicitTypeArgumentSource(source: KtSourceElement?): Boolean =
     source != null && source.kind !is KtFakeSourceElementKind.ImplicitTypeArgument
 
 val FirTypeProjection.isExplicit: Boolean get() = isExplicitTypeArgumentSource(source)
+
+fun checkTypeRef(
+    typeRef: FirTypeRef,
+    context: CheckerContext,
+    reporter: DiagnosticReporter
+) {
+    if (typeRef.source?.kind?.shouldSkipErrorTypeReporting != false) return
+    val type = typeRef.coneTypeSafe<ConeClassLikeType>()
+    val fullyExpandedType = type?.fullyExpandedType(context.session) ?: return
+    val declaration = fullyExpandedType.toSymbol(context.session) as? FirRegularClassSymbol ?: return
+    val typeParameters = declaration.typeParameterSymbols
+    val typeArguments = type.typeArguments
+
+    val size = minOf(typeParameters.size, typeArguments.size)
+
+    val typeRefAndSourcesForArguments = extractArgumentsTypeRefAndSource(typeRef) ?: return
+    for (it in 0 until size) {
+        val proto = typeParameters[it]
+        val actual = typeArguments[it]
+        val fullyExpandedProjection = fullyExpandedType.typeArguments[it]
+
+        val protoVariance = proto.variance
+
+        val projectionRelation = if (fullyExpandedProjection is ConeKotlinTypeConflictingProjection ||
+            actual is ConeKotlinTypeProjectionIn && protoVariance == Variance.OUT_VARIANCE ||
+            actual is ConeKotlinTypeProjectionOut && protoVariance == Variance.IN_VARIANCE
+        ) {
+            ProjectionRelation.Conflicting
+        } else if (actual is ConeKotlinTypeProjectionIn && protoVariance == Variance.IN_VARIANCE ||
+            actual is ConeKotlinTypeProjectionOut && protoVariance == Variance.OUT_VARIANCE
+        ) {
+            ProjectionRelation.Redundant
+        } else {
+            ProjectionRelation.None
+        }
+
+        val argTypeRefSource = typeRefAndSourcesForArguments.getOrNull(it) ?: continue
+
+        if (projectionRelation != ProjectionRelation.None && typeRef.source?.kind !is KtFakeSourceElementKind) {
+            reporter.reportOn(
+                argTypeRefSource.source ?: argTypeRefSource.typeRef?.source,
+                if (projectionRelation == ProjectionRelation.Conflicting)
+                    if (type != fullyExpandedType) FirErrors.CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION else FirErrors.CONFLICTING_PROJECTION
+                else
+                    FirErrors.REDUNDANT_PROJECTION,
+                fullyExpandedType,
+                context
+            )
+        }
+
+        argTypeRefSource.typeRef?.let { checkTypeRef(it, context, reporter) }
+    }
+}
+
+private enum class ProjectionRelation {
+    Conflicting,
+    Redundant,
+    None
+}
--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/declaration/FirProjectionRelationChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/declaration/FirProjectionRelationChecker.kt
@@ -5,18 +5,11 @@
 
 package org.jetbrains.kotlin.fir.analysis.checkers.declaration
 
-import org.jetbrains.kotlin.KtFakeSourceElementKind
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
-import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
+import org.jetbrains.kotlin.fir.analysis.checkers.checkTypeRef
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
-import org.jetbrains.kotlin.fir.analysis.checkers.extractArgumentsTypeRefAndSource
-import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.declarations.*
-import org.jetbrains.kotlin.fir.resolve.fullyExpandedType
-import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
-import org.jetbrains.kotlin.fir.types.*
-import org.jetbrains.kotlin.types.Variance
 
 object FirProjectionRelationChecker : FirBasicDeclarationChecker(MppCheckerKind.Common) {
     override fun check(declaration: FirDeclaration, context: CheckerContext, reporter: DiagnosticReporter) {
@@ -38,64 +31,5 @@ object FirProjectionRelationChecker : FirBasicDeclarationChecker(MppCheckerKind.
                 checkTypeRef(declaration.expandedTypeRef, context, reporter)
             else -> {}
         }
-    }
-
-    private fun checkTypeRef(
-        typeRef: FirTypeRef,
-        context: CheckerContext,
-        reporter: DiagnosticReporter
-    ) {
-        if (typeRef.source?.kind?.shouldSkipErrorTypeReporting != false) return
-        val type = typeRef.coneTypeSafe<ConeClassLikeType>()
-        val fullyExpandedType = type?.fullyExpandedType(context.session) ?: return
-        val declaration = fullyExpandedType.toSymbol(context.session) as? FirRegularClassSymbol ?: return
-        val typeParameters = declaration.typeParameterSymbols
-        val typeArguments = type.typeArguments
-
-        val size = minOf(typeParameters.size, typeArguments.size)
-
-        val typeRefAndSourcesForArguments = extractArgumentsTypeRefAndSource(typeRef) ?: return
-        for (it in 0 until size) {
-            val proto = typeParameters[it]
-            val actual = typeArguments[it]
-            val fullyExpandedProjection = fullyExpandedType.typeArguments[it]
-
-            val protoVariance = proto.variance
-
-            val projectionRelation = if (fullyExpandedProjection is ConeKotlinTypeConflictingProjection ||
-                actual is ConeKotlinTypeProjectionIn && protoVariance == Variance.OUT_VARIANCE ||
-                actual is ConeKotlinTypeProjectionOut && protoVariance == Variance.IN_VARIANCE
-            ) {
-                ProjectionRelation.Conflicting
-            } else if (actual is ConeKotlinTypeProjectionIn && protoVariance == Variance.IN_VARIANCE ||
-                actual is ConeKotlinTypeProjectionOut && protoVariance == Variance.OUT_VARIANCE
-            ) {
-                ProjectionRelation.Redundant
-            } else {
-                ProjectionRelation.None
-            }
-
-            val argTypeRefSource = typeRefAndSourcesForArguments.getOrNull(it) ?: continue
-
-            if (projectionRelation != ProjectionRelation.None && typeRef.source?.kind !is KtFakeSourceElementKind) {
-                reporter.reportOn(
-                    argTypeRefSource.source ?: argTypeRefSource.typeRef?.source,
-                    if (projectionRelation == ProjectionRelation.Conflicting)
-                        if (type != fullyExpandedType) FirErrors.CONFLICTING_PROJECTION_IN_TYPEALIAS_EXPANSION else FirErrors.CONFLICTING_PROJECTION
-                    else
-                        FirErrors.REDUNDANT_PROJECTION,
-                    fullyExpandedType,
-                    context
-                )
-            }
-
-            argTypeRefSource.typeRef?.let { checkTypeRef(it, context, reporter) }
-        }
-    }
-
-    private enum class ProjectionRelation {
-        Conflicting,
-        Redundant,
-        None
     }
 }
