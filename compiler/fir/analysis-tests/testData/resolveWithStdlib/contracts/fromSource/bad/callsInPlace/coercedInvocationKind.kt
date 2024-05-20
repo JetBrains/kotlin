fcc20cfd9d73689c98d4227bcdae5d9fa2f76b23
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2024-05-20 20:39:12 +0200

    [FIR][Tests] Fix suggesting non-existent `InvocationKind`
    ^KT-68291 Fixed

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/contracts/fromSource/bad/callsInPlace/coercedInvocationKind.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/contracts/fromSource/bad/callsInPlace/coercedInvocationKind.kt
@@ -6,21 +6,21 @@ import kotlin.contracts.*
 @OptIn(ExperimentalContracts::class)
 fun zero(block: () -> Unit) {
     contract {
-        <!WRONG_INVOCATION_KIND("block: () -> Unit; EXACTLY_ONCE; ZERO")!>callsInPlace(block, InvocationKind.EXACTLY_ONCE)<!>
+        <!WRONG_INVOCATION_KIND("block: () -> Unit; EXACTLY_ONCE; AT_MOST_ONCE")!>callsInPlace(block, InvocationKind.EXACTLY_ONCE)<!>
     }
 }
 
 @OptIn(ExperimentalContracts::class)
 fun zero2(block: () -> Unit) {
     contract {
-        <!WRONG_INVOCATION_KIND("block: () -> Unit; AT_LEAST_ONCE; ZERO")!>callsInPlace(block, InvocationKind.AT_LEAST_ONCE)<!>
+        <!WRONG_INVOCATION_KIND("block: () -> Unit; AT_LEAST_ONCE; AT_MOST_ONCE")!>callsInPlace(block, InvocationKind.AT_LEAST_ONCE)<!>
     }
 }
 
 @OptIn(ExperimentalContracts::class)
 fun multiple(block: () -> Unit) {
     contract {
-        <!WRONG_INVOCATION_KIND("block: () -> Unit; EXACTLY_ONCE; MORE_THAN_ONCE")!>callsInPlace(block, InvocationKind.EXACTLY_ONCE)<!>
+        <!WRONG_INVOCATION_KIND("block: () -> Unit; EXACTLY_ONCE; AT_LEAST_ONCE")!>callsInPlace(block, InvocationKind.EXACTLY_ONCE)<!>
     }
 
     block()
@@ -30,7 +30,7 @@ fun multiple(block: () -> Unit) {
 @OptIn(ExperimentalContracts::class)
 fun multiple2(block: () -> Unit) {
     contract {
-        <!WRONG_INVOCATION_KIND("block: () -> Unit; AT_MOST_ONCE; MORE_THAN_ONCE")!>callsInPlace(block, InvocationKind.AT_MOST_ONCE)<!>
+        <!WRONG_INVOCATION_KIND("block: () -> Unit; AT_MOST_ONCE; AT_LEAST_ONCE")!>callsInPlace(block, InvocationKind.AT_MOST_ONCE)<!>
     }
 
     block()
