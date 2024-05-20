aa6ed41795c00da4ebaf45e5b2e9b7239f81cb51
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-05-20 12:58:17 +0200

    [tree generators] Simplify field inheritance part 1
    Including removal of custom AbstractField.equals. It was rather confusing
    because one, AbstractField is a mutable type, and two, it had been used
    inconsistently (Set<AbstractField> vs Map<String, AbstractField>).
    
    #KT-67398
    #KT-67400 Related

--- compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/printer/ImplementationPrinter.kt
+++ compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/printer/ImplementationPrinter.kt
@@ -317,8 +317,9 @@ internal class ImplementationPrinter(
                     }
                 }
 
-                for (overridenType in field.overriddenTypes) {
-                    generateReplace(field, overridenType) {
+                val additionalOverriddenTypes = field.overriddenFields.map { it.typeRef.copy(nullable = false) }.toSet() - field.typeRef.copy(false)
+                for (overriddenType in additionalOverriddenTypes) {
+                    generateReplace(field, overriddenType) {
                         println("require($newValue is ${field.typeRef.render()})")
                         println("replace$capitalizedFieldName($newValue)")
                     }
