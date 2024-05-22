96006b769b172fb295945e168b44e8946ff5ec76
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-05-22 10:42:12 +0300

    [FIR] Properly handle captured type parameters in callable reference resolution
    ^KT-68350 Fixed

--- compiler/testData/diagnostics/tests/callableReference/generic/localClassWithCapturedTypeParameters.fir.kt
+++
@@ -1,9 +0,0 @@
-// WITH_STDLIB
-// ISSUE: KT-68350
-
-fun <T, K> test() {
-    class InnerClass(var x: Int, val y: T)
-
-    val list: List<InnerClass> = listOf()
-    list.<!CANNOT_INFER_PARAMETER_TYPE!>sortedBy<!>(InnerClass::<!INAPPLICABLE_CANDIDATE!>x<!>)
-}
