34d9405b8b15fbedda595ce6679536446fa7e718
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2024-05-23 14:43:42 +0200

    Revert "[LLVM 16][tests] Mute stackConcurrentPush test failing due to KT-67578"
    This reverts commit 133192d7ef2f79f8f0fc5e43baa6b6464195476b.

--- kotlin-native/runtime/test/concurrent/AtomicStressTest.kt
+++ kotlin-native/runtime/test/concurrent/AtomicStressTest.kt
@@ -148,8 +148,6 @@ class AtomicStressTest {
     }
 
     @Test fun stackConcurrentPush() {
-        if (Platform.osFamily == OsFamily.WINDOWS) return // https://youtrack.jetbrains.com/issue/KT-67578
-
         val stack = LockFreeStack<Int>()
         val writers = ThreadPool.execute {
             stack.push(it)
