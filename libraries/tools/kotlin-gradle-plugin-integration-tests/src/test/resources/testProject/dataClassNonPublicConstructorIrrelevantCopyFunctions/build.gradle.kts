c5fe73c367de85b90034f11b28c0557b7f7aef26
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-05-27 15:37:07 +0200

    2/2 [FIR] FirDataClassCopyUsageWillBecomeInaccessibleChecker & FirStatusResolver: better "isDataClassCopyFun" check
    ^KT-68528 Fixed
    ^KT-68526 Fixed
    Root ticket: KT-11914
    
    origin has proved to be falsy and problematic:
    - origin is different in diagnostic tests and in real Gradle projects.
      See: 3ad94dab8dc6e4391033d2f6fde380ea4a4d150d
    - origin becomes SubstitutionOverride when generic is used KT-68526
    
    And I can't understand whether it's a data class `copy` function just by
    its "shape" (parameter and receiver types) because of generics. That's
    why I have to serialize this info into metadata (previous commit) so I
    can use it later (this commit)
    
    == Test infra
    
    I use full blown Gradle test for
    dataClassNonPublicConstructorIrrelevantCopyFunctions because `origin`
    isn't the same in diagnostic tests and real Gradle projects (see:
    3ad94dab8dc6e4391033d2f6fde380ea4a4d150d).
    
    Yes, I no longer rely on the `origin` but anyway I want to test this
    example in a real project.
    
    I don't use BTA infra instead of a Gradle test because: KT-68555

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/dataClassNonPublicConstructorIrrelevantCopyFunctions/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":lib"))
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions.progressiveMode.set(true)
+    compilerOptions.languageVersion.set(KotlinVersion.KOTLIN_2_0)
+    compilerOptions.freeCompilerArgs.add("-XXLanguage:+ErrorAboutDataClassCopyVisibilityChange")
+    compilerOptions.freeCompilerArgs.add("-XXLanguage:+DataClassCopyRespectsConstructorVisibility")
+}
