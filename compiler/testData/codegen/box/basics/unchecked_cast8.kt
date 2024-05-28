b1452330df43c5900f90f31bc32cc6914a557044
Igor Chevdar <igor.chevdar@jetbrains.com>
2024-05-28 15:02:52 +0300

    [K/N][tests] More tests on generic boundary casts

--- compiler/testData/codegen/box/basics/unchecked_cast8.kt
+++ compiler/testData/codegen/box/basics/unchecked_cast8.kt
@@ -18,6 +18,17 @@ open class Derived<T> : Base<T>() where T : I1, T: I2 {
     }
 }
 
+open class Base2<in T : I1> {
+    open fun foo(x: T): I1 = x
+}
+
+open class Derived2<in T> : Base2<T>() where T : I1, T: I2 {
+    override fun foo(x: T): I1 {
+        println(x.z)
+        return x
+    }
+}
+
 class C : I1
 
 class D : I1, I2 {
@@ -28,8 +39,12 @@ fun box(): String {
     try {
         val b = Derived<D>()
         val i1 = (b as Base<C>).foo(C())
-        return "FAIL: $i1"
-    } catch (e: ClassCastException) {
-        return "OK"
-    }
+        return "FAIL 1: $i1"
+    } catch (e: ClassCastException) {}
+    try {
+        val b = Derived2<D>()
+        val i1 = (b as Base2<C>).foo(C())
+        return "FAIL 2: $i1"
+    } catch (e: ClassCastException) {}
+    return "OK"
 }
--- compiler/testData/codegen/box/bridges/test23.kt
+++ compiler/testData/codegen/box/bridges/test23.kt
@@ -10,12 +10,24 @@ class Derived<T : Foo> : Base<T>() {
     override fun bar(x: T) { }
 }
 
+abstract class Base2<in T> {
+    abstract fun bar(x: T)
+}
+
+class Derived2<in T : Foo> : Base2<T>() {
+    override fun bar(x: T) { }
+}
+
 fun box(): String {
     val d = Derived<Foo>()
     try {
         val x = (d as Base<Any>).bar(Any())
-        return "FAIL: $x"
-    } catch (e: ClassCastException) {
-        return "OK"
-    }
+        return "FAIL 1: $x"
+    } catch (e: ClassCastException) {}
+    val d2 = Derived2<Foo>()
+    try {
+        val x = (d2 as Base2<Any>).bar(Any())
+        return "FAIL 2: $x"
+    } catch (e: ClassCastException) {}
+    return "OK"
 }
