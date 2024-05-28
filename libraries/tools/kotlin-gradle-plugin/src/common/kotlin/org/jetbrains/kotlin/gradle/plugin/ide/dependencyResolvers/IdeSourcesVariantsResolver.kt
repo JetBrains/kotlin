8867c79ae0aef4e66ee80c59a1b9259181da9fc0
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-05-28 14:50:40 +0200

    [Gradle] Remove *dependencySources sources configurations
    Replace them with detachable variants that are not visible for
    in default configuration container so that users can resolve
    configurations without any failures.
    
    ^KT-68447 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/ide/dependencyResolvers/IdeSourcesVariantsResolver.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/ide/dependencyResolvers/IdeSourcesVariantsResolver.kt
@@ -5,34 +5,57 @@
 
 package org.jetbrains.kotlin.gradle.plugin.ide.dependencyResolvers
 
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier
 import org.gradle.api.artifacts.result.ResolvedVariantResult
+import org.jetbrains.kotlin.gradle.dsl.metadataTarget
+import org.jetbrains.kotlin.gradle.dsl.multiplatformExtensionOrNull
 import org.jetbrains.kotlin.gradle.idea.tcs.*
 import org.jetbrains.kotlin.gradle.idea.tcs.extras.sourcesClasspath
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.ide.IdeAdditionalArtifactResolver
 import org.jetbrains.kotlin.gradle.plugin.ide.IdeaKotlinBinaryCapability
+import org.jetbrains.kotlin.gradle.plugin.mpp.configureSourcesPublicationAttributes
 import org.jetbrains.kotlin.gradle.plugin.sources.internal
 
 internal object IdeSourcesVariantsResolver : IdeAdditionalArtifactResolver {
     override fun resolve(sourceSet: KotlinSourceSet, dependencies: Set<IdeaKotlinDependency>) {
         val project = sourceSet.project
+        val multiplatformExtension = project.multiplatformExtensionOrNull ?: return // resolve sources only for KMP projects
+
+        val sourceSetTarget = sourceSet.internal
+            .compilations
+            .map { it.target }
+            .toSet()
+            .singleOrNull() ?: multiplatformExtension.metadataTarget
 
         val binaryDependenciesByCoordinates = dependencies
             .filterIsInstance<IdeaKotlinResolvedBinaryDependency>()
             .filter { dependency -> dependency.isKotlinCompileBinaryType }
             .groupBy { Coordinates(it.coordinates) }
 
-        val dependencySourceConfiguration = project.configurations
-            .findByName(sourceSet.internal.dependencySourcesConfigurationName) ?: return
+        val gradleDependencies = binaryDependenciesByCoordinates
+            .keys
+            .mapNotNull { coordinates -> coordinates?.toGradleDependency(project) }
 
-        dependencySourceConfiguration.incoming.artifactView { it.isLenient = true }.artifacts.forEach { artifactDependency ->
-            val coordinates = Coordinates(artifactDependency.variant)
-            val binaryDependencies = binaryDependenciesByCoordinates[coordinates] ?: return@forEach
-            binaryDependencies.forEach { dependency ->
-                dependency.sourcesClasspath.add(artifactDependency.file)
+        val dependencySourceConfiguration = project.configurations
+            .detachedConfiguration(*gradleDependencies.toTypedArray())
+            .apply { configureSourcesPublicationAttributes(sourceSetTarget) }
+
+        dependencySourceConfiguration.incoming
+            // not every dependency can be resolved into sources, and it is OK for IDE import
+            .artifactView { it.isLenient = true }
+            .artifacts
+            .forEach { artifactDependency ->
+                // now match originally requested dependencies with resolved sources artifacts
+                val coordinates = Coordinates(artifactDependency.variant)
+                val binaryDependencies = binaryDependenciesByCoordinates[coordinates] ?: return@forEach
+                binaryDependencies.forEach { dependency ->
+                    dependency.sourcesClasspath.add(artifactDependency.file)
+                }
             }
-        }
     }
 }
 
@@ -41,7 +64,33 @@ private data class Coordinates(
     val module: String,
     val version: String?,
     val capabilities: Set<IdeaKotlinBinaryCapability>
-)
+) {
+    val gradleDependencyNotationString
+        get() = buildString {
+            append("$group:$module")
+            if (version != null) append(":$version")
+        }
+}
+
+private fun Coordinates.toGradleDependency(project: Project): Dependency {
+    val dependency = project.dependencies.create(gradleDependencyNotationString)
+    dependency as ExternalModuleDependency // this is safe since we create the dependency from "string" notation.
+    if (capabilities.isNotEmpty()) {
+        dependency.capabilities { handler ->
+            capabilities.forEach {
+                handler.requireCapability(it.gradleDependencyNotationString)
+            }
+        }
+    }
+
+    return dependency
+}
+
+private val IdeaKotlinBinaryCapability.gradleDependencyNotationString
+    get() = buildString {
+        append("$group:$name")
+        if (version != null) append(":$version")
+    }
 
 private fun Coordinates(coordinates: IdeaKotlinBinaryCoordinates?): Coordinates? {
     if (coordinates == null) return null
