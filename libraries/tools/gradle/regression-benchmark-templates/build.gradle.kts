1b4864f0f20cad1b6175a2b0e37306985c378717
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-05-29 14:05:49 +0200

    [Build] Add a workaround for KTIJ-30065
    IDEA during the import creates detached configurations that contain a subset of dependencies that causes resolution of actually unused artifacts that fail the dependency verification. More details in the related ticket.
    ^KTI-1807 Fixed

--- libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
+++ libraries/tools/gradle/regression-benchmark-templates/build.gradle.kts
@@ -8,4 +8,7 @@ dependencies {
     implementation(libs.jgit)
     implementation("org.slf4j:slf4j-nop:1.7.32")
     implementation("org.jetbrains.kotlinx:dataframe:0.8.1")
+    implicitDependencies("org.jetbrains.kotlinx:dataframe:0.8.1") {
+        because("workaround for KTIJ-30065, remove after its resolution")
+    }
 }
