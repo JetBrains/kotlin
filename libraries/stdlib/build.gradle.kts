591bd3c17b2c4c914ecca0d940bdf892bf302547
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-05-30 04:30:50 +0200

    [temporary] allow compilation warnings

--- libraries/stdlib/build.gradle.kts
+++ libraries/stdlib/build.gradle.kts
@@ -48,6 +48,7 @@ fun KotlinCommonCompilerOptions.mainCompilationOptions() {
     apiVersion = KotlinVersion.KOTLIN_2_0
     freeCompilerArgs.add("-Xstdlib-compilation")
     freeCompilerArgs.add("-Xdont-warn-on-error-suppression")
+    allWarningsAsErrors = false
 }
 
 val configurationBuiltins = resolvingConfiguration("builtins") {
