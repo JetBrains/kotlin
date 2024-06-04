d175a5f189ceeb069e09ee1d213887a281bceef8
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-06-04 20:19:13 +0200

    [Daemon] Introduce `DaemonGcAutoConfigurationProblemsListener`
    ^KT-68297 In Progress
    
    (cherry picked from commit 6e349be4651ea2a044001259cf184c53ccdb8ceb)

--- compiler/daemon/daemon-client/src/main/kotlin/daemonProblemReportingOutputListener.kt
+++ compiler/daemon/daemon-client/src/main/kotlin/daemonProblemReportingOutputListener.kt
@@ -45,3 +45,36 @@ internal class DaemonLastOutputLinesListener : DaemonProblemReportingOutputListe
         private const val OUTPUT_INDENT = 4
     }
 }
+
+internal class DaemonGcAutoConfigurationProblemsListener(
+    private val gcAutoConfiguration: KotlinCompilerClient.GcAutoConfiguration,
+    private val startupAttempt: Int,
+) :
+    DaemonProblemReportingOutputListener {
+    private var hasGcSelectionProblems = false
+
+    override fun onOutputLine(line: String) {
+        if (!gcAutoConfiguration.shouldAutoConfigureGc) return
+        if (line.contains("Multiple garbage collectors selected") || line.contains("Conflicting collector combinations in option list")) {
+            hasGcSelectionProblems = true
+            gcAutoConfiguration.shouldAutoConfigureGc = false
+        }
+    }
+
+    override fun retrieveProblems(): List<String> {
+        val shouldReport = when {
+            hasGcSelectionProblems -> true
+            gcAutoConfiguration.shouldAutoConfigureGc && startupAttempt >= 1 -> {
+                gcAutoConfiguration.shouldAutoConfigureGc = false
+                true
+            }
+            else -> false
+        }
+        if (!shouldReport) return emptyList()
+        return listOf(
+            "Problems may have occurred during auto-selection of GC. The preferred GC is ${gcAutoConfiguration.preferredGc} GC.",
+            "If the problems persist, try adding the JVM option to the Kotlin daemon JVM arguments: -XX:-Use${gcAutoConfiguration.preferredGc}GC.",
+            "GC auto-selection logic is disabled temporary for the next daemon startup.",
+        )
+    }
+}
