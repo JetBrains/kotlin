74d0154d01935de9f2e4baffbb3464981b451734
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-06-05 17:31:51 +0200

    [TEST] Fix privateConstructorWithDefaults test for Wasm
    ^KT-68828 Fixed
    
    `fun box` is expected to have `String` return type.
    Previous return type was `CharSequence`.

--- compiler/testData/codegen/box/multiplatform/k2/privateConstructorWithDefaults.kt
+++ compiler/testData/codegen/box/multiplatform/k2/privateConstructorWithDefaults.kt
@@ -1,16 +1,14 @@
-// IGNORE_BACKEND: WASM
-//   Ignore wasm because KT-68828
 // LANGUAGE: +MultiPlatformProjects
 // ISSUE: KT-68798
 
 // MODULE: common
 // FILE: common.kt
 
-expect open class Frame private constructor(disposableHandle: CharSequence = "OK")
+expect open class Frame private constructor(disposableHandle: String = "OK")
 
 // MODULE: platform()()(common)
 // FILE: platform.kt
-actual open class Frame actual constructor(val disposableHandle: CharSequence) {
+actual open class Frame actual constructor(val disposableHandle: String) {
     class Break : Frame()
 }
 
