dc57321189d80bcbe5ebc34fa759caecbe9139ce
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-06-10 16:54:43 +0200

    [TEST] 1/2 Reproduce KT-68636

--- compiler/testData/diagnostics/tests/dataClassNonPublicConstructor/deprecationPhase1_optIn.kt
+++ compiler/testData/diagnostics/tests/dataClassNonPublicConstructor/deprecationPhase1_optIn.kt
@@ -6,3 +6,10 @@ data class Data private constructor(val x: Int)
 fun usage(data: Data) {
     data.copy()
 }
+
+@ConsistentCopyVisibility
+data class DontFallbackToPrivateToThis<out T> private constructor(val t: T) {
+    private fun private(other: DontFallbackToPrivateToThis<T>) {
+        other.copy()
+    }
+}
