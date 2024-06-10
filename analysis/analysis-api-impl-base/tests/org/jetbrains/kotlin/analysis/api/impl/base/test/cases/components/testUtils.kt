d1a0ca992790cd9822ab4ae605c5db36e050eb4a
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-06-10 15:19:29 +0200

    [AA] Skip "Base" classes when rendering frontend-independent class names
    - `analysis-api-impl-base` may contain `KaBase*` classes which contain
      common implementations for FE10 and FIR.
    
    ^KT-68462

--- analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/testUtils.kt
+++ analysis/analysis-api-impl-base/tests/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/testUtils.kt
@@ -324,7 +324,7 @@ internal fun KaSession.renderScopeWithParentDeclarations(scope: KaScope): String
 
 internal fun renderFrontendIndependentKClassNameOf(instanceOfClassToRender: Any): String {
     var classToRender: Class<*> = instanceOfClassToRender::class.java
-    while (classToRender.simpleName.let { it.contains("Fir") || it.contains("Fe10") } == true) {
+    while (classToRender.simpleName.let { it.contains("Fir") || it.contains("Fe10") || it.contains("Base") } == true) {
         classToRender = classToRender.superclass
     }
 
