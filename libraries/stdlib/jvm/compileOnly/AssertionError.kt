5863eb8406a73b67abb46a2020e5837a8de8c4ae
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-06-11 17:44:47 +0200

    [stdlib] JDK 7 Exception type stubs
    for matching constructors of expect ConcurrentModificationException and
    AssertionError available since JDK7
    
    KT-68731

---
+++ libraries/stdlib/jvm/compileOnly/AssertionError.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package java.lang
+
+public open class AssertionError : Error {
+    public constructor()
+    public constructor(message: Any?)
+
+    public constructor(message: String?, cause: Throwable?)
+}
