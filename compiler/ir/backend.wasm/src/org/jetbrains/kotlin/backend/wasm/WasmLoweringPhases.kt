737a828f4a7aa05244d9f29caa500e5f3dcb8a5b
Osama Ahmad <osama199915@gmail.com>
2024-06-12 15:44:43 +0300

    Delete FieldInitializersLowering because field initialization is in linkWasmCompiledFragments

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
@@ -619,13 +619,6 @@ private val whenBranchOptimiserLoweringPhase = makeIrModulePhase(
     description = "[Optimization] Remove unreachable code in when's, it needed because dead paths could have an invalid IR after the inliner",
 )
 
-private val fieldInitializersLoweringPhase = makeIrModulePhase(
-    ::FieldInitializersLowering,
-    name = "FieldInitializersLowering",
-    description = "Move field initializers to start function",
-    prerequisite = setOf(purifyObjectInstanceGettersLoweringPhase)
-)
-
 val constEvaluationPhase = makeIrModulePhase(
     { context ->
         val configuration = IrInterpreterConfiguration(
@@ -765,7 +758,6 @@ val loweringList = listOf(
 
     objectUsageLoweringPhase,
     purifyObjectInstanceGettersLoweringPhase,
-//    fieldInitializersLoweringPhase,
 
     explicitlyCastExternalTypesPhase,
     typeOperatorLoweringPhase,
