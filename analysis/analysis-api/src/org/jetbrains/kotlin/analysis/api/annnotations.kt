2868321d1297bee9d9dce60fd99b87d15b657d27
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-06-12 17:39:21 +0200

    [Analysis API] introduce more opt-in annotations
    `KaAnalysisApiInternals` should be replaced with `KaImplementationDetail`.
    `KaAnalysisNonPublicApi` should be replaced with `KaNonPublicApi`.
    This is not a part of this commit as it requires much more changes
    
    ^KT-68883

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/annnotations.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/annnotations.kt
@@ -10,6 +10,19 @@ package org.jetbrains.kotlin.analysis.api
     AnnotationTarget.PROPERTY,
     AnnotationTarget.CONSTRUCTOR,
     AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPEALIAS,
+)
+@RequiresOptIn("Internal API which should not be used outside the Analysis API implementation modules as it does not have any compatibility guarantees")
+public annotation class KaImplementationDetail
+
+/**
+ * TODO(KT-68883): replace with [KaImplementationDetail]
+ */
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
     AnnotationTarget.TYPEALIAS
 )
 @RequiresOptIn("Internal Analysis API component which should not be used outside the Analysis API implementation modules as it does not have any compatibility guarantees")
@@ -18,9 +31,42 @@ public annotation class KaAnalysisApiInternals
 @Suppress("OPT_IN_MARKER_CAN_ONLY_BE_USED_AS_ANNOTATION_OR_ARGUMENT_IN_OPT_IN")
 public typealias KtAnalysisApiInternals = KaAnalysisApiInternals
 
+/**
+ * TODO(KT-68883): replace with [KaNonPublicApi]
+ */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION, AnnotationTarget.TYPEALIAS)
-@RequiresOptIn("Internal Analysis API component which is used from other modules in the Kotlin project and is not supposed to be used anywhere else since it has no compatibility guarantees")
+@RequiresOptIn("Internal API which is used in projects developed by JetBrains")
 public annotation class KaAnalysisNonPublicApi
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPEALIAS,
+)
+@RequiresOptIn("Internal API which is used in projects developed by JetBrains")
+public annotation class KaNonPublicApi
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPEALIAS,
+)
+@RequiresOptIn("Internal API which is used only from the IntelliJ Kotlin plugin. Such an API should not be used in other places since it has no compatibility guarantees")
+public annotation class KaIdeApi
+
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.CONSTRUCTOR,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPEALIAS,
+)
+@RequiresOptIn("Experimental API with no compatibility guarantees")
+public annotation class KaExperimentalApi
 
 @Suppress("OPT_IN_MARKER_CAN_ONLY_BE_USED_AS_ANNOTATION_OR_ARGUMENT_IN_OPT_IN")
 public typealias KtAnalysisNonPublicApi = KaAnalysisNonPublicApi
