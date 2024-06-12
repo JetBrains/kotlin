ff3dea98abdb93b1499ce1e0ee5bdac6dfe98086
Troels Bjerre Lund <troels@google.com>
2024-06-12 16:38:09 +0200

    Workaround LLDB symbol resolution bug
    After LLVM-14, the debug functions from the native runtime end up in a
    second .text section, hitting a bug in LLDB where symbol resolution
    fails with multiple sections with the same name.
    
    This fix moves the annotations keeping the functions alive to a separate
    const array, thereby allowing clang to put the debug functions in the
    normal .text section.

--- kotlin-native/runtime/src/main/cpp/KDebug.h
+++ kotlin-native/runtime/src/main/cpp/KDebug.h
@@ -32,55 +32,42 @@ extern "C" {
 // the new function please do not forget to add new functions into the debug operations list.
 
 // Get memory buffer where debugger can put data in Konan app process.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 char* Konan_DebugBuffer();
 
 // Same, but runtime-specific.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 char* Konan_DebugBufferWithObject(KRef obj);
 
 // Get size of memory buffer where debugger can put data in Konan app process.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugBufferSize();
 
 // Same, but runtime-specific.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugBufferSizeWithObject(KRef obj);
 
 // Put string representation of an object to the provided buffer.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugObjectToUtf8Array(KRef obj, char* buffer, int32_t bufferSize);
 
 // Print to console string representation of an object.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugPrint(KRef obj);
 
 // Returns 1 if obj refers to an array, string or binary blob and 0 otherwise.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugIsArray(KRef obj);
 
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugIsInstance(KRef obj, const TypeInfo* typeInfo);
 
 // Returns number of fields in an objects, or elements in an array.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugGetFieldCount(KRef obj);
 
 // Compute type of field or an array element at the index, or 0, if incorrect,
 // see Konan_RuntimeType.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 int32_t Konan_DebugGetFieldType(KRef obj, int32_t index);
 
 // Compute address of field or an array element at the index, or null, if incorrect.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 void* Konan_DebugGetFieldAddress(KRef obj, int32_t index);
 
 // Compute address of field or an array element at the index, or null, if incorrect.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 const char* Konan_DebugGetFieldName(KRef obj, int32_t index);
 
 // Returns name of type.
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 const char* Konan_DebugGetTypeName(KRef obj);
 
 /**
@@ -88,7 +75,6 @@ const char* Konan_DebugGetTypeName(KRef obj);
  * Important for cases where multiple K/N runtimes coexist in the same address space and debugger
  * doesn't know which debug operation to use on particular instance.
  */
-RUNTIME_USED RUNTIME_WEAK RUNTIME_RETAIN
 void* Konan_DebugGetOperation(KRef obj, /* Konan_DebugOperation */ int32_t operation);
 
 #ifdef __cplusplus
