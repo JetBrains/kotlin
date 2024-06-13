c7d9e7cfc4f1670955d0797c3e28391de02bac26
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-06-13 10:55:59 +0200

    ~ fix usage of decls source from common module

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/impl/FirTypeIntersectionScopeContext.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/scopes/impl/FirTypeIntersectionScopeContext.kt
@@ -362,7 +362,7 @@ class FirTypeIntersectionScopeContext(
             newDispatchReceiverType = dispatchReceiverType,
             deferredReturnTypeCalculation = deferredReturnTypeCalculation,
             newReturnType = if (!forClassUseSiteScope && deferredReturnTypeCalculation == null) intersectReturnTypes(mostSpecific) else null,
-            newSource = dispatchReceiverType.toSymbol(session)?.source,
+            newSource = dispatchReceiverType.toSymbol(session)?.source ?: keyFir.source,
         ).apply {
             originalForIntersectionOverrideAttr = keyFir
         }
