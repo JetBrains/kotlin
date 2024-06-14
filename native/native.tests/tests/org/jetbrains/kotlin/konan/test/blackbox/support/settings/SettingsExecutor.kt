3376e71bc50ff40b7901bb4da03f1db64976534a
Pavel Punegov <pavel.punegov@jetbrains.com>
2024-06-14 17:49:21 +0200

    [K/N][test] No-op executor if CompileOnly option is specified
    Some tests don't use TestRunners object to get runner backed
    with the appropriate executor suitable for current options.
    In case of CompileOnly specified NoopTestRunner is used, but some tests
    use `Settings.executor` directly and fail to get executor.
    For this case, add NoOpExecutor to skip tests execution.

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/settings/SettingsExecutor.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/settings/SettingsExecutor.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.konan.target.AppleConfigurables
 import org.jetbrains.kotlin.konan.target.ConfigurablesWithEmulator
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.konan.target.isSimulator
+import org.jetbrains.kotlin.konan.test.blackbox.support.ClassLevelProperty
 import org.jetbrains.kotlin.native.executors.*
 import org.jetbrains.kotlin.test.services.JUnit5Assertions
 import java.util.concurrent.ConcurrentHashMap
@@ -19,26 +20,27 @@ val Settings.executor: Executor
     get() = with(get<KotlinNativeTargets>()) {
         executorCache.computeIfAbsent(testTarget) {
             val configurables = configurables
-            if (get<XCTestRunner>().isEnabled) {
-                // Forcibly run tests with XCTest
-                check(configurables is AppleConfigurables) {
-                    "Running tests with XCTest is not supported on non-Apple $configurables"
+            when {
+                get<ForcedNoopTestRunner>().value -> {
+                    NoOpExecutor("${ClassLevelProperty.COMPILE_ONLY.name} option was specified to skip execution")
                 }
-                when (configurables.target) {
-                    hostTarget -> XCTestHostExecutor(configurables)
-                    is KonanTarget.IOS_ARM64 -> FirebaseCloudXCTestExecutor(configurables)
-                    is KonanTarget.IOS_X64, KonanTarget.IOS_SIMULATOR_ARM64 -> XCTestSimulatorExecutor(configurables)
-                    else -> JUnit5Assertions.fail { "Running tests for $testTarget on $hostTarget with XCTest is not supported yet." }
+                get<XCTestRunner>().isEnabled -> {
+                    // Forcibly run tests with XCTest
+                    check(configurables is AppleConfigurables) {
+                        "Running tests with XCTest is not supported on non-Apple $configurables"
+                    }
+                    when (configurables.target) {
+                        hostTarget -> XCTestHostExecutor(configurables)
+                        is KonanTarget.IOS_ARM64 -> FirebaseCloudXCTestExecutor(configurables)
+                        is KonanTarget.IOS_X64, KonanTarget.IOS_SIMULATOR_ARM64 -> XCTestSimulatorExecutor(configurables)
+                        else -> JUnit5Assertions.fail { "Running tests for $testTarget on $hostTarget with XCTest is not supported yet." }
+                    }
                 }
-            } else {
-                when {
-                    configurables.target == hostTarget -> HostExecutor()
-                    configurables is ConfigurablesWithEmulator -> EmulatorExecutor(configurables)
-                    configurables is AppleConfigurables && configurables.targetTriple.isSimulator ->
-                        XcodeSimulatorExecutor(configurables)
-                    configurables is AppleConfigurables && RosettaExecutor.availableFor(configurables) -> RosettaExecutor(configurables)
-                    else -> JUnit5Assertions.fail { "Running tests for $testTarget on $hostTarget is not supported yet." }
-                }
+                configurables.target == hostTarget -> HostExecutor()
+                configurables is ConfigurablesWithEmulator -> EmulatorExecutor(configurables)
+                configurables is AppleConfigurables && configurables.targetTriple.isSimulator -> XcodeSimulatorExecutor(configurables)
+                configurables is AppleConfigurables && RosettaExecutor.availableFor(configurables) -> RosettaExecutor(configurables)
+                else -> JUnit5Assertions.fail { "Running tests for $testTarget on $hostTarget is not supported yet." }
             }
         }
     }
