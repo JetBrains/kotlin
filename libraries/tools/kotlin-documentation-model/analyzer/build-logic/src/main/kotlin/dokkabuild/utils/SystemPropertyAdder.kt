65d59b278f7af3efd2bd777276b95699ceb9509b
Adam <152864218+adam-enko@users.noreply.github.com>
2024-06-19 11:08:13 +0200

    [KDM] Update Maven Integration Tests to use JVM Test Suites
    See https://github.com/Kotlin/dokka/pull/3581
    
    Part of KT-64200
    
    * update BioJava test task
    - Re-introduce shared integration-test convention plugin, to de-duplicate common IT config.
    - Add util for adding system properties & registering the values as appropriate Gradle task inputs.
      (This required replacing env-vars with system props.)
    * fail test if Android SDK missing
    * bump jgit
    * fix devMavenRepositories caching
    * fix ANDROID_HOME in AndroidIT
    * move Dokka CLI JAR check into test (simplifies the Gradle config)
    * fix ENABLE_DEBUG env var
    * Refactor `fun MutableList<CommandLineArgumentProvider>.systemProperty()` to be more consistent with the other systemProperty utils, and so the intention is clearer.
    * move test suite dependency `implementation(project())` to convention
    * disable explicitApi in integration tests by default
    * modify DevMavenPublishExtension#configureTask to configure Test task
    Original commit: https://github.com/Kotlin/dokka/commit/880ac6527e1208cc867d3ad1f3b78072aac61936

---
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/dokkabuild/utils/SystemPropertyAdder.kt
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+package dokkabuild.utils
+
+import org.gradle.api.file.Directory
+import org.gradle.api.file.FileCollection
+import org.gradle.api.file.RegularFile
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskInputFilePropertyBuilder
+import org.gradle.api.tasks.TaskInputPropertyBuilder
+import org.gradle.api.tasks.testing.Test
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.findByType
+import org.gradle.process.CommandLineArgumentProvider
+import javax.inject.Inject
+
+
+/**
+ * Utility for adding a System Property command line arguments to this [Test] task,
+ * and correctly registering the values as task inputs (for Gradle up-to-date checks).
+ */
+// https://github.com/gradle/gradle/issues/11534
+// https://github.com/gradle/gradle/issues/12247
+val Test.systemProperty: SystemPropertyAdder
+    get() {
+        val spa = extensions.findByType<SystemPropertyAdder>()
+            ?: extensions.create<SystemPropertyAdder>("SystemPropertyAdder", this)
+        return spa
+    }
+
+
+abstract class SystemPropertyAdder @Inject internal constructor(
+    private val task: Test,
+) {
+    fun inputDirectory(
+        key: String,
+        value: Directory,
+    ): TaskInputFilePropertyBuilder {
+        task.jvmArgumentProviders.add(
+            SystemPropertyArgumentProvider(key, value) {
+                it.asFile.invariantSeparatorsPath
+            }
+        )
+        return task.inputs.dir(value)
+            .withPropertyName("SystemProperty input directory $key")
+    }
+
+    fun inputFile(
+        key: String,
+        file: RegularFile,
+    ): TaskInputFilePropertyBuilder {
+        task.jvmArgumentProviders.add(
+            SystemPropertyArgumentProvider(key, file) {
+                it.asFile.invariantSeparatorsPath
+            }
+        )
+        return task.inputs.file(file)
+            .withPropertyName("SystemProperty input file $key")
+    }
+
+    fun inputFile(
+        key: String,
+        file: Provider<out RegularFile>,
+    ): TaskInputFilePropertyBuilder {
+        task.jvmArgumentProviders.add(
+            SystemPropertyArgumentProvider(key, file) {
+                it.orNull?.asFile?.invariantSeparatorsPath
+            }
+        )
+        return task.inputs.file(file)
+            .withPropertyName("SystemProperty input file $key")
+    }
+
+    fun inputFiles(
+        key: String,
+        files: Provider<out FileCollection>,
+    ): TaskInputFilePropertyBuilder {
+        task.jvmArgumentProviders.add(
+            SystemPropertyArgumentProvider(key, files) { it.orNull?.asPath }
+        )
+        return task.inputs.files(files)
+            .withPropertyName("SystemProperty input files $key")
+    }
+
+    fun inputProperty(
+        key: String,
+        value: Provider<out String>,
+    ): TaskInputPropertyBuilder {
+        task.jvmArgumentProviders.add(
+            SystemPropertyArgumentProvider(key, value) { it.orNull }
+        )
+        return task.inputs.property("SystemProperty input property $key", value)
+    }
+
+    @JvmName("inputBooleanProperty")
+    fun inputProperty(
+        key: String,
+        value: Provider<out Boolean>,
+    ): TaskInputPropertyBuilder = inputProperty(key, value.map { it.toString() })
+
+    /**
+     * Add a System Property (in the format `-D$key=$value`).
+     *
+     * [value] will be treated as if it were annotated with [org.gradle.api.tasks.Internal]
+     * and will _not_ be registered as a Gradle [org.gradle.api.Task] input.
+     * (Which is beneficial in cases where [value] is optional, or is not reproducible,
+     * because such a property might disrupt task caching.)
+     *
+     * If you want to register the property as a Task input that can be normalized, use one of
+     * the typed inputs above.
+     *
+     * @see org.gradle.api.tasks.Internal
+     */
+    fun internalProperty(
+        key: String,
+        value: Provider<out String>,
+    ) {
+        task.jvmArgumentProviders.add(
+            SystemPropertyArgumentProvider(
+                key = key,
+                value = value,
+                transformer = { it.orNull },
+            )
+        )
+    }
+}
+
+/**
+ * Provide a Java system property.
+ *
+ * [value] is not registered as a Gradle Task input.
+ * The value must be registered as a task input, using the [SystemPropertyAdder] utils.
+ */
+private class SystemPropertyArgumentProvider<T : Any>(
+    @get:Input
+    val key: String,
+    private val value: T,
+    private val transformer: (value: T) -> String?,
+) : CommandLineArgumentProvider {
+    override fun asArguments(): Iterable<String> {
+        val value = transformer(value) ?: return emptyList()
+        return listOf("-D$key=$value")
+    }
+}
