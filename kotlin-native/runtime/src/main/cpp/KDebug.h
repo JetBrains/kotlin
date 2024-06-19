27bc3345e54c8ce08ec71fc84ed956dcd52f3ec3
Troels Bjerre Lund <troels@google.com>
2024-06-19 14:00:59 +0200

    [LLVM 16] Workaround for old ld.gold and lldb bugs
    Using the retain attribute hits a bug in the currently bundled version
    of ld.gold, resulting in two separate sections being created, one for
    symbols with retain and one for symbols without. Both these sections are
    named .text. This isn't technically incorrect ELF, but it in turn hits a
    bug in LLDB, where symbols in the second .text are incorrectly resolved,
    with offsets relative to the first .text section. The upshot of all this
    is that adding retain will make LLDB unable to lookup and call the
    Konan_DebugXX functions required for the konan_lldb.py extension.
    
    This fix removes the retain annotation from the debug functions, thus
    moving them to the first .text section, where they get resolved
    correctly in old lldb version. To keep the symbols alive, a dummy array
    of pointers to those functions is annotated with retain.
    
    Both of the bugs are fixed upstream in their respective projects, so
    this workaround is only required until, e.g., the linker gets upgraded
    or changed.
    
    ^KT-69206 Fixed

--- kotlin-native/runtime/src/main/cpp/KDebug.h
+++ kotlin-native/runtime/src/main/cpp/KDebug.h
@@ -32,55 +32,55 @@ extern "C" {
 // the new function please do not forget to add new functions into the debug operations list.
 
 // Get memory buffer where debugger can put data in Konan app process.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 char* Konan_DebugBuffer();
 
 // Same, but runtime-specific.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 char* Konan_DebugBufferWithObject(KRef obj);
 
 // Get size of memory buffer where debugger can put data in Konan app process.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugBufferSize();
 
 // Same, but runtime-specific.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugBufferSizeWithObject(KRef obj);
 
 // Put string representation of an object to the provided buffer.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugObjectToUtf8Array(KRef obj, char* buffer, int32_t bufferSize);
 
 // Print to console string representation of an object.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugPrint(KRef obj);
 
 // Returns 1 if obj refers to an array, string or binary blob and 0 otherwise.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugIsArray(KRef obj);
 
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugIsInstance(KRef obj, const TypeInfo* typeInfo);
 
 // Returns number of fields in an objects, or elements in an array.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugGetFieldCount(KRef obj);
 
 // Compute type of field or an array element at the index, or 0, if incorrect,
 // see Konan_RuntimeType.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 int32_t Konan_DebugGetFieldType(KRef obj, int32_t index);
 
 // Compute address of field or an array element at the index, or null, if incorrect.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 void* Konan_DebugGetFieldAddress(KRef obj, int32_t index);
 
 // Compute address of field or an array element at the index, or null, if incorrect.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 const char* Konan_DebugGetFieldName(KRef obj, int32_t index);
 
 // Returns name of type.
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 const char* Konan_DebugGetTypeName(KRef obj);
 
 /**
@@ -88,7 +88,7 @@ const char* Konan_DebugGetTypeName(KRef obj);
  * Important for cases where multiple K/N runtimes coexist in the same address space and debugger
  * doesn't know which debug operation to use on particular instance.
  */
-RUNTIME_EXPORT RUNTIME_WEAK
+RUNTIME_WEAK
 void* Konan_DebugGetOperation(KRef obj, /* Konan_DebugOperation */ int32_t operation);
 
 #ifdef __cplusplus
