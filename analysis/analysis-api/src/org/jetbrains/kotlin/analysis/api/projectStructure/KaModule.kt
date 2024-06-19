b4e4895666fc93517f5a5b2a2875163358868329
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-06-19 08:01:02 +0200

    [Analysis API] properly implement `KtBuiltinsModule.contentScope`
    so we can analyze it as a binary module
    
    KT-68689

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/projectStructure/KaModule.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/projectStructure/KaModule.kt
@@ -180,24 +180,16 @@ public interface KaLibrarySourceModule : KaModule {
 
 /**
  * A module which contains kotlin [builtins](https://kotlinlang.org/spec/built-in-types-and-their-semantics.html) for a specific platform.
- * Kotlin builtins usually reside in the compiler, so [contentScope] is empty.
  */
 @KaPlatformInterface
-public class KaBuiltinsModule(
-    override val targetPlatform: TargetPlatform,
-    override val project: Project
-) : KaModule {
+public interface KaBuiltinsModule : KaModule {
     override val directRegularDependencies: List<KaModule> get() = emptyList()
     override val directDependsOnDependencies: List<KaModule> get() = emptyList()
     override val transitiveDependsOnDependencies: List<KaModule> get() = emptyList()
     override val directFriendDependencies: List<KaModule> get() = emptyList()
-    override val contentScope: GlobalSearchScope get() = GlobalSearchScope.EMPTY_SCOPE
 
     @KaExperimentalApi
     override val moduleDescription: String get() = "Builtins for $targetPlatform"
-
-    override fun equals(other: Any?): Boolean = other is KaBuiltinsModule && this.targetPlatform == other.targetPlatform
-    override fun hashCode(): Int = targetPlatform.hashCode()
 }
 
 /**
