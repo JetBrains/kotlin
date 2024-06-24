1658dac0e35e548dddb877818427f2adfe2180db
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-06-24 18:19:56 +0200

    [K/N] Don't run synthetic accessor tests with thread state checker
    For the same reason as with compiler caches: we don't produce
    binaries in these tests => nothing to do for the thread state checker.

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/NativeTestSupport.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/NativeTestSupport.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -104,6 +104,7 @@ class KlibSyntheticAccessorTestSupport : BeforeEachCallback {
         }
 
         Assumptions.assumeTrue(settings.get<CacheMode>() == CacheMode.WithoutCache)
+        Assumptions.assumeTrue(settings.get<ThreadStateChecker>() == ThreadStateChecker.DISABLED)
 
         // Inject the required properties to test instance.
         with(settings.get<NativeTestInstances<AbstractNativeKlibSyntheticAccessorTest>>().enclosingTestInstance) {
