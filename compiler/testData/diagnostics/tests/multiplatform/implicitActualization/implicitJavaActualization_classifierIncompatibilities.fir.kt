88738854f8c00406938eabba806416cc6006277d
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-06-24 14:13:01 +0200

    [FIR] Don't report CLASSIFIER_REDECLARATION for Java vs expect class annotated @ImplicitlyActualizedByJvmDeclaration
    KT-67202
    Review: https://jetbrains.team/p/kt/reviews/16828/timeline

--- compiler/testData/diagnostics/tests/multiplatform/implicitActualization/implicitJavaActualization_classifierIncompatibilities.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/implicitActualization/implicitJavaActualization_classifierIncompatibilities.fir.kt
@@ -8,19 +8,19 @@ import kotlin.jvm.ImplicitlyActualizedByJvmDeclaration
 
 interface I
 
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class <!CLASSIFIER_REDECLARATION!>A<!><!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect value class <!CLASSIFIER_REDECLARATION!>B<!>(val x: Int)<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect fun interface <!CLASSIFIER_REDECLARATION!>C1<!> { fun foo() }<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect fun interface <!CLASSIFIER_REDECLARATION!>C2<!> { fun foo() }<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class <!CLASSIFIER_REDECLARATION!>D1<!> : I<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class <!CLASSIFIER_REDECLARATION!>D2<!> : I<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect enum class <!CLASSIFIER_REDECLARATION!>E1<!> { ONE, TWO }<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect enum class <!CLASSIFIER_REDECLARATION!>E2<!> { ONE, TWO }<!>
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class <!CLASSIFIER_REDECLARATION!>Outer<!> {
-    class <!CLASSIFIER_REDECLARATION!>F1<!>
-    inner class <!CLASSIFIER_REDECLARATION!>F2<!>
-    inner class <!CLASSIFIER_REDECLARATION!>F3<!>
-    class <!CLASSIFIER_REDECLARATION!>F4<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class A<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect value class B(val x: Int)<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect fun interface C1 { fun foo() }<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect fun interface C2 { fun foo() }<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class D1 : I<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class D2 : I<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect enum class E1 { ONE, TWO }<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect enum class E2 { ONE, TWO }<!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>@ImplicitlyActualizedByJvmDeclaration expect class Outer {
+    class F1
+    inner class F2
+    inner class F3
+    class F4
 }<!>
 
 // MODULE: m2-jvm()()(m1-common)
