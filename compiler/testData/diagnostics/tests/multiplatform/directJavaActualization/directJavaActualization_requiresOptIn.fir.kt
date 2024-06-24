270eb445128c5fd65d83dfcb87ec5118324447f2
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-06-24 14:13:01 +0200

    [FIR] Don't report CLASSIFIER_REDECLARATION for expect classes conflicting with Java classes
    KT-67202
    Review: https://jetbrains.team/p/kt/reviews/16828/timeline

--- compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_requiresOptIn.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_requiresOptIn.fir.kt
@@ -3,7 +3,7 @@
 // MODULE: m1-common
 // FILE: common.kt
 
-<!NO_ACTUAL_FOR_EXPECT{JVM}!>expect annotation class <!CLASSIFIER_REDECLARATION!>Foo<!><!>
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>expect annotation class Foo<!>
 
 // MODULE: m2-jvm()()(m1-common)
 // FILE: Foo.java
