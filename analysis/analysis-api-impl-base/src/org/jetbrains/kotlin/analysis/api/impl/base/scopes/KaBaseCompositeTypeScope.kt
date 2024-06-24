c52ac2fe249b482d3d11294a3803326d40bb0287
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-06-24 20:24:11 +0200

    [Analysis API] base: rename KaCompositeTypeScope -> KaBaseCompositeTypeScope
    ^KT-67996

--- analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/scopes/KaBaseCompositeTypeScope.kt
+++ analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/scopes/KaBaseCompositeTypeScope.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -18,7 +18,7 @@ import org.jetbrains.kotlin.analysis.api.symbols.KaConstructorSymbol
 import org.jetbrains.kotlin.name.Name
 
 @KaAnalysisApiInternals
-class KaCompositeTypeScope(
+class KaBaseCompositeTypeScope(
     val subScopes: List<KaTypeScope>,
     override val token: KaLifetimeToken
 ) : KaTypeScope {
