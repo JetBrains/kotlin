23365d298be4d9e4815d91154d1114a42a571514
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-06-25 17:24:41 +0300

    [FIR] Ensure `FirProjectionRelationChecker` handles error type arguments

--- compiler/testData/diagnostics/tests/exceptions/unresolvedReferenceDuringProjectionRelationChecking.kt
+++ compiler/testData/diagnostics/tests/exceptions/unresolvedReferenceDuringProjectionRelationChecking.kt
@@ -2,3 +2,8 @@
 // ISSUE: KT-69407
 
 fun <T> <!UNRESOLVED_REFERENCE!>UnresolvedClass<!><out T>.test() { }
+
+class Box<T>
+typealias TA<K> = Box<K>
+
+fun checkList(list: TA<out <!UNRESOLVED_REFERENCE!>UnresolvedClass<!>>) {}
