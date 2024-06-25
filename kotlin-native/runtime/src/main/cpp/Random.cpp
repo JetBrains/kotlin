6f2cc34015dc678990ff070c5acd93b78b4c03d9
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-06-25 14:12:08 +0300

    [K/N] Add correct markers to throwReadingRandomBytesFailed()

--- kotlin-native/runtime/src/main/cpp/Random.cpp
+++ kotlin-native/runtime/src/main/cpp/Random.cpp
@@ -3,6 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
 
@@ -27,7 +28,7 @@
 namespace {
 
 #if KONAN_LINUX || KONAN_WINDOWS
-void throwReadingRandomBytesFailed(const char* format, ...) {
+RUNTIME_NORETURN __attribute__((format(printf, 1, 2))) void throwReadingRandomBytesFailed(const char* format, ...) {
     va_list args;
     va_start(args, format);
     std::array<char, 128> buffer;
@@ -61,7 +62,7 @@ void Kotlin_Uuid_getRandomBytes(KRef byteArray, KInt size) {
         if (ret >= 0) {
             count += ret;
         } else if (errno != EINTR) { // repeat if interrupted
-            throwReadingRandomBytesFailed("getrandom returned a negative value: %ld, errno: %d", ret, errno);
+            throwReadingRandomBytesFailed("getrandom returned a negative value: %ld, error: %s", ret, strerror(errno));
         }
     }
 #elif KONAN_WINDOWS
