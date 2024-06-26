01dcea34bbb6f3eb33112d42f251208aa2df1064
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-06-26 10:46:46 +0200

    [FIR] Cleanup. Fix LightTreePositioningStrategies.CONTEXT_KEYWORD super type
    I mistakenly used BaseDeclarationNameStrategy, because I copy-pasted
    near standing declaration.
    
    If CONTEXT_KEYWORD can't be found the default positional strategy should
    be used, whetever it is
    
    Move the declaration, to make DECLARATION_NAME and DECLARATION_NAME_ONLY
    stand together

--- compiler/frontend.common-psi/src/org/jetbrains/kotlin/diagnostics/LightTreePositioningStrategies.kt
+++ compiler/frontend.common-psi/src/org/jetbrains/kotlin/diagnostics/LightTreePositioningStrategies.kt
@@ -254,17 +254,6 @@ object LightTreePositioningStrategies {
         }
     }
 
-    val CONTEXT_KEYWORD: LightTreePositioningStrategy = object : BaseDeclarationNameStrategy() {
-        override fun mark(
-            node: LighterASTNode,
-            startOffset: Int,
-            endOffset: Int,
-            tree: FlyweightCapableTreeStructure<LighterASTNode>
-        ): List<TextRange> = tree.findDescendantByType(node, KtNodeTypes.CONTEXT_RECEIVER_LIST)?.getChildren(tree)?.firstOrNull()
-            ?.let { markElement(it, startOffset, endOffset, tree, node) }
-            ?: super.mark(node, startOffset, endOffset, tree)
-    }
-
     val DECLARATION_NAME_ONLY: LightTreePositioningStrategy = BaseDeclarationNameStrategy()
 
     val ACTUAL_DECLARATION_NAME: LightTreePositioningStrategy = object : LightTreePositioningStrategy() {
@@ -280,6 +269,17 @@ object LightTreePositioningStrategies {
             }
             return DEFAULT.mark(node, startOffset, endOffset, tree)
         }
+    }
+
+    val CONTEXT_KEYWORD: LightTreePositioningStrategy = object : LightTreePositioningStrategy() {
+        override fun mark(
+            node: LighterASTNode,
+            startOffset: Int,
+            endOffset: Int,
+            tree: FlyweightCapableTreeStructure<LighterASTNode>
+        ): List<TextRange> = tree.findDescendantByType(node, KtNodeTypes.CONTEXT_RECEIVER_LIST)?.getChildren(tree)?.firstOrNull()
+            ?.let { markElement(it, startOffset, endOffset, tree, node) }
+            ?: super.mark(node, startOffset, endOffset, tree)
     }
 
     val DECLARATION_SIGNATURE: LightTreePositioningStrategy = object : LightTreePositioningStrategy() {
