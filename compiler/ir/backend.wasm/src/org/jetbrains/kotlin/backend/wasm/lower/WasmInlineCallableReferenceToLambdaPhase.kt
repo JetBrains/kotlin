afa444e6a3f07c019bf69ecace542bb53f27b6ed
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-06-26 16:43:32 +0200

    Fix signature restriction for inlined function

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmInlineCallableReferenceToLambdaPhase.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmInlineCallableReferenceToLambdaPhase.kt
@@ -6,8 +6,15 @@
 package org.jetbrains.kotlin.backend.wasm.lower
 
 import org.jetbrains.kotlin.backend.wasm.WasmBackendContext
+import org.jetbrains.kotlin.ir.IrStatement
+import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.inline.CommonInlineCallableReferenceToLambdaPhase
 
 internal class WasmInlineCallableReferenceToLambdaPhase(context: WasmBackendContext) : CommonInlineCallableReferenceToLambdaPhase(
     context, WasmInlineFunctionResolver(context)
-)
+) {
+    override fun visitFunction(declaration: IrFunction, data: IrDeclarationParent?): IrStatement =
+        context.irFactory.stageController.restrictTo(declaration) {
+            super.visitFunction(declaration, data)
+        }
+}
