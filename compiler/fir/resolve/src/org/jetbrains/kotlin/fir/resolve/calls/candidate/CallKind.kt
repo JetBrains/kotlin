35ddbb121902506aa4ae8c66db329e53641cba5a
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-06-27 10:33:59 +0300

    [FIR] Initialize empty argument mapping for variable access candidates

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/candidate/CallKind.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/candidate/CallKind.kt
@@ -14,6 +14,7 @@ sealed class CallKind(vararg resolutionSequence: ResolutionStage) {
         DiscriminateSyntheticProperties,
         CheckExplicitReceiverConsistency,
         NoTypeArguments,
+        InitializeEmptyArgumentMap,
         CreateFreshTypeVariableSubstitutorStage,
         CollectTypeVariableUsagesInfo,
         CheckDispatchReceiver,
