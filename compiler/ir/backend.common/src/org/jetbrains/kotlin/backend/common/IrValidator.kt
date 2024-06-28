99fe3c970a8468e9410cca4ebfa4dbc07b541a98
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-06-28 14:28:39 +0200

    IR Inliner: Add KDocs for InlineFunctionUseSiteChecker.isPermitted
    ^KT-69172
    ^KT-69173

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/IrValidator.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/IrValidator.kt
@@ -34,6 +34,19 @@ internal class IrValidatorConfig(
 )
 
 fun interface InlineFunctionUseSiteChecker {
+    /**
+     * Check if the given use site of the inline function is permitted at the current phase of IR validation.
+     *
+     * Example 1: Check use sites after inlining all private functions.
+     *   It is permitted to have only use sites of non-private functions in the whole IR tree. So, for a use site
+     *   of a private inline function we should return `false` if it is met in the IR. For any other use site
+     *   we should return `true` (== permitted).
+     *
+     * Example 2: Check use sites after inlining all functions.
+     *   Normally, no use sites of inline functions should remain in the whole IR tree. So, if we met one we shall
+     *   return `false` (== not permitted). However, there are a few exceptions that are temporarily permitted.
+     *   For example, `inline external` intrinsics in Native (KT-66734).
+     */
     fun isPermitted(inlineFunctionUseSite: IrMemberAccessExpression<IrFunctionSymbol>): Boolean
 }
 
