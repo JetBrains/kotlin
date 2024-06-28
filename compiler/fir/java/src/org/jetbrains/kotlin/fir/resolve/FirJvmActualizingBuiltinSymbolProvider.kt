004fe73ba49004443f43fad066fc6b2d0489509c
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2024-06-28 22:36:51 +0200

    [FIR] Fill `expectForActual` map for builtin `actual` declarations in `FirJvmActualizingBuiltinSymbolProvider`
    ^KT-69997 Fixed

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/resolve/FirJvmActualizingBuiltinSymbolProvider.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/resolve/FirJvmActualizingBuiltinSymbolProvider.kt
@@ -6,18 +6,25 @@
 package org.jetbrains.kotlin.fir.resolve
 
 import org.jetbrains.kotlin.fir.analysis.checkers.hasAnnotationOrInsideAnnotatedClass
+import org.jetbrains.kotlin.fir.declarations.expectForActual
+import org.jetbrains.kotlin.fir.declarations.hasAnnotation
+import org.jetbrains.kotlin.fir.declarations.utils.isExpect
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolNamesProvider
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProviderInternals
 import org.jetbrains.kotlin.fir.resolve.providers.impl.FirBuiltinsSymbolProvider
+import org.jetbrains.kotlin.fir.symbols.FirBasedSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
+import org.jetbrains.kotlin.fir.types.ConeClassLikeType
+import org.jetbrains.kotlin.fir.types.classId
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.name.StandardClassIds
+import org.jetbrains.kotlin.resolve.multiplatform.ExpectActualMatchingCompatibility
 
 /**
  * It's initialized on the top of module-based providers for platform source sets.
@@ -32,6 +39,8 @@ import org.jetbrains.kotlin.name.StandardClassIds
  * to symbols from common source set
  *
  * Those actuals can be treated as declarations in a virtual file
+ *
+ * Fill the `expectForActual` map on successful matches here because `FirExpectActualMatcherTransformer` handles actuals only from sources
  */
 class FirJvmActualizingBuiltinSymbolProvider(
     val builtinsSymbolProvider: FirBuiltinsSymbolProvider,
@@ -48,8 +57,15 @@ class FirJvmActualizingBuiltinSymbolProvider(
                 continue
             }
 
-            // If there are multiple declarations with the same name, they will be reported as redeclarations by a checker
-            return builtinsSymbolProvider.getClassLikeSymbolByClassId(classId) as FirRegularClassSymbol
+            require(classSymbol.isExpect)
+
+            // Assume an `actual` always exists for the found `expect`
+            // Otherwise it's something wrong with stdlib sources
+            return builtinsSymbolProvider.getClassLikeSymbolByClassId(classId)!!.also {
+                it.fir.expectForActual = mapOf<ExpectActualMatchingCompatibility, List<FirBasedSymbol<*>>>(
+                    ExpectActualMatchingCompatibility.MatchedSuccessfully to listOf(classSymbol)
+                )
+            }
         }
 
         return null
@@ -59,14 +75,62 @@ class FirJvmActualizingBuiltinSymbolProvider(
 
     @FirSymbolProviderInternals
     override fun getTopLevelCallableSymbolsTo(destination: MutableList<FirCallableSymbol<*>>, packageFqName: FqName, name: Name) {
+        getTopLevelSymbolsTo(destination) { provider, localDestination ->
+            provider.getTopLevelCallableSymbolsTo(localDestination, packageFqName, name)
+        }
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelFunctionSymbolsTo(destination: MutableList<FirNamedFunctionSymbol>, packageFqName: FqName, name: Name) {
+        getTopLevelSymbolsTo(destination) { provider, localDestination ->
+            provider.getTopLevelFunctionSymbolsTo(localDestination, packageFqName, name)
+        }
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelPropertySymbolsTo(destination: MutableList<FirPropertySymbol>, packageFqName: FqName, name: Name) {
+        getTopLevelSymbolsTo(destination) { provider, localDestination ->
+            provider.getTopLevelPropertySymbolsTo(localDestination, packageFqName, name)
+        }
+    }
+
+    @FirSymbolProviderInternals
+    private fun <T : FirCallableSymbol<*>> getTopLevelSymbolsTo(
+        destination: MutableList<T>,
+        getFunction: (FirSymbolProvider, MutableList<T>) -> Unit,
+    ) {
+        val tempDestination = mutableListOf<T>()
+        for (symbolProvider in refinedSourceSymbolProviders) {
+            tempDestination.clear()
+            getFunction(symbolProvider, tempDestination)
+
+            // Make sure there is a single `expect` with `@ActualizeByJvmBuiltinProvider` annotation
+            // To match it possible to match it with a corresponding `actual`
+            val expectSymbol = tempDestination.singleOrNull {
+                it.hasAnnotation(StandardClassIds.Annotations.ActualizeByJvmBuiltinProvider, symbolProvider.session)
+            } ?: continue
+            require(expectSymbol.isExpect)
+
+            val expectDeclarationReceiverType = (expectSymbol.resolvedReceiverTypeRef?.coneType as? ConeClassLikeType)?.classId
+
+            tempDestination.clear()
+            getFunction(builtinsSymbolProvider, tempDestination)
+
+            // Typically, there is only one `actual` that matches the found `expect`
+            // The single case with multiple actuals is `toString` with different receivers (String?, BigDecimal, BigInteger)
+            // Fortunately, they can be distinguished just by the receiver type
+            val actualSymbol = tempDestination.single {
+                (it.resolvedReceiverTypeRef?.coneType as? ConeClassLikeType)?.classId == expectDeclarationReceiverType
+            }
+
+            actualSymbol.fir.expectForActual = mapOf<ExpectActualMatchingCompatibility, List<FirBasedSymbol<*>>>(
+                ExpectActualMatchingCompatibility.MatchedSuccessfully to listOf(expectSymbol)
+            )
+
+            destination.add(actualSymbol)
+
+            return
+        }
     }
 
     override fun getPackage(fqName: FqName): FqName? = builtinsSymbolProvider.getPackage(fqName)
