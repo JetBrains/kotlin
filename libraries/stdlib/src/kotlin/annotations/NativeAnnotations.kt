d2a568d62ced2113b614bcfe2ed34eeafaa097ae
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-07-01 14:10:16 +0200

    fixup! [K/N] Remove legacy MM from stdlib ^KT-59121

--- libraries/stdlib/src/kotlin/annotations/NativeAnnotations.kt
+++ libraries/stdlib/src/kotlin/annotations/NativeAnnotations.kt
@@ -49,7 +49,8 @@ public expect annotation class CName(val externName: String = "", val shortName:
     AnnotationTarget.TYPEALIAS,
 )
 @Retention(AnnotationRetention.BINARY)
-public annotation class FreezingIsDeprecated
+@OptionalExpectation
+public expect annotation class FreezingIsDeprecated
 
 /**
  * Instructs the Kotlin compiler to use a custom Objective-C and/or Swift name for this class, property, parameter or function.
