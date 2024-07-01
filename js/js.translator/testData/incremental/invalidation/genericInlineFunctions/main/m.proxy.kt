250eedd1f97373c608b73add49804eaa61a02e20
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-07-01 21:10:26 +0200

    [Wasm] Fix invalidation genericInlineFunctions test for Wasm

--- js/js.translator/testData/incremental/invalidation/genericInlineFunctions/main/m.proxy.kt
+++ js/js.translator/testData/incremental/invalidation/genericInlineFunctions/main/m.proxy.kt
@@ -1,6 +1,13 @@
 fun isEqual(l: Any?, r: Any?) = if (l == r) true else null
 
 fun box(stepId: Int, isWasm: Boolean): String {
+    if (isWasm) {
+        when (stepId) {
+            6, 7, 14, 22 -> return "OK" // Wasm prohibits unsafe cast to invalid T
+            else -> Unit
+        }
+    }
+
     when (stepId) {
         in 0..5 -> isEqual(foo_proxy("test"), 123) ?: return "Fail"
         in 8..13 -> isEqual(foo_proxy("test"), 123) ?: return "Fail"
