52c80f37fbc3c43ee80ac57a5c07bab27d4548a5
Artem Olkov <artem.olkov@jetbrains.com>
2024-07-02 16:38:59 +0000

    [Swift Export] Add task to launch tests tagged by swiftexport
    Small cleanup, we have made the tag but never used it.
    With this change it is now possible to launch only relevant to Swift Export tests from native.test:test task
    
    
    Merge-request: KT-MR-16854
    Merged-by: Artem Olkov <artem.olkov@jetbrains.com>

--- native/native.tests/build.gradle.kts
+++ native/native.tests/build.gradle.kts
@@ -58,6 +58,7 @@ val cachesTest = nativeTest("cachesTest", "caches")
 val klibTest = nativeTest("klibTest", "klib")
 val standaloneTest = nativeTest("standaloneTest", "standalone")
 val gcTest = nativeTest("gcTest", "gc")
+val swiftExportTest = nativeTest("swiftExportTest", "swiftexport")
 
 val testTags = findProperty("kotlin.native.tests.tags")?.toString()
 // Note: arbitrary JUnit tag expressions can be used in this property.
