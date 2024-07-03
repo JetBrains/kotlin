be692d9f48be262b466c9f6de380edf8c2742d57
Osama Ahmad <osama199915@gmail.com>
2024-07-03 17:10:55 +0300

    [Wasm] Not change return type of the changeFunctionSignature test for Wasm

--- js/js.translator/testData/incremental/invalidationWithPL/changeFunctionSignature/main/m.kt
+++ js/js.translator/testData/incremental/invalidationWithPL/changeFunctionSignature/main/m.kt
@@ -3,7 +3,13 @@ fun box(stepId: Int, isWasm: Boolean): String {
     if (stepId == 4) {
         // this is known issue, that return type doesn not affect IdSignature
         // https://youtrack.jetbrains.com/issue/KT-51707
-        if (x != 41) return "Fail; got $x"
+        if (isWasm) {
+            // The Wasm version doesn't change return type because
+            //  the validation phase won't let the test pass.
+            if (x != 5) return "Fail; got $x"
+        } else {
+            if (x != 41) return "Fail; got $x"
+        }
     } else {
         if (stepId != x) return "Fail; got $x"
     }
