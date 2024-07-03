56df4796ebab5c8fea1dd376fd45961a0073b8f5
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-07-03 21:42:28 +0200

    FastJarFS: support big jars (>2Gb)
    Implement "floating mapping" approach that allows overcoming the
    inherent limitation of the JDK mmap implementation
    (https://bugs.openjdk.org/browse/JDK-7147951)
    
    #KT-69434 in progress

--- compiler/cli/cli-base/src/org/jetbrains/kotlin/cli/jvm/compiler/jarfs/ZipImplementation.kt
+++ compiler/cli/cli-base/src/org/jetbrains/kotlin/cli/jvm/compiler/jarfs/ZipImplementation.kt
@@ -5,17 +5,14 @@
 
 package org.jetbrains.kotlin.cli.jvm.compiler.jarfs
 
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.nio.MappedByteBuffer
 import java.util.zip.Inflater
 
 
 class ZipEntryDescription(
     val relativePath: CharSequence,
-    val compressedSize: Int,
-    val uncompressedSize: Int,
-    val offsetInFile: Int,
+    val compressedSize: Long,
+    val uncompressedSize: Long,
+    val offsetInFile: Long,
     val compressionKind: CompressionKind,
     val fileNameSize: Int,
 ) {
@@ -24,7 +21,7 @@ class ZipEntryDescription(
         PLAIN, DEFLATE
     }
 
-    val isDirectory get() = uncompressedSize == 0
+    val isDirectory get() = uncompressedSize == 0L
 }
 
 private const val END_OF_CENTRAL_DIR_SIZE = 22
@@ -32,117 +29,116 @@ private const val END_OF_CENTRAL_DIR_ZIP64_SIZE = 56
 private const val LOCAL_FILE_HEADER_EXTRA_OFFSET = 28
 private const val LOCAL_FILE_HEADER_SIZE = LOCAL_FILE_HEADER_EXTRA_OFFSET + 2
 
-fun MappedByteBuffer.contentsToByteArray(
+internal fun LargeDynamicMappedBuffer.contentsToByteArray(
     zipEntryDescription: ZipEntryDescription
-): ByteArray {
-    order(ByteOrder.LITTLE_ENDIAN)
-    val extraSize =
-        getUnsignedShort(zipEntryDescription.offsetInFile + LOCAL_FILE_HEADER_EXTRA_OFFSET)
+): ByteArray =
+    withMappedRangeFrom(zipEntryDescription.offsetInFile) {
+        val extraSize = getUnsignedShort((LOCAL_FILE_HEADER_EXTRA_OFFSET).toInt())
+        val startPos = LOCAL_FILE_HEADER_SIZE + zipEntryDescription.fileNameSize + extraSize
 
-    position(
-        zipEntryDescription.offsetInFile + LOCAL_FILE_HEADER_SIZE + zipEntryDescription.fileNameSize + extraSize
-    )
-    val compressed = ByteArray(zipEntryDescription.compressedSize + 1)
-    get(compressed, 0, zipEntryDescription.compressedSize)
-
-    return when (zipEntryDescription.compressionKind) {
-        ZipEntryDescription.CompressionKind.DEFLATE -> {
-            val inflater = Inflater(true)
-            inflater.setInput(compressed, 0, zipEntryDescription.compressedSize)
-
-            val result = ByteArray(zipEntryDescription.uncompressedSize)
-
-            inflater.inflate(result)
-            inflater.end()
-
-            result
+        require(zipEntryDescription.compressedSize - startPos < Int.MAX_VALUE && zipEntryDescription.uncompressedSize <= Int.MAX_VALUE) {
+            "Reading files bigger than Int.MAX_VALUE - $startPos is not supported yet"
         }
 
-        ZipEntryDescription.CompressionKind.PLAIN -> compressed.copyOf(zipEntryDescription.compressedSize)
-    }
-}
+        val compressed = getBytes(startPos, zipEntryDescription.compressedSize.toInt())
 
-fun MappedByteBuffer.parseCentralDirectory(): List<ZipEntryDescription> {
-    order(ByteOrder.LITTLE_ENDIAN)
+        when (zipEntryDescription.compressionKind) {
+            ZipEntryDescription.CompressionKind.DEFLATE -> {
+                val inflater = Inflater(true)
+                inflater.setInput(compressed, 0, zipEntryDescription.compressedSize.toInt())
+
+                val result = ByteArray(zipEntryDescription.uncompressedSize.toInt())
+
+                inflater.inflate(result)
+                inflater.end()
+
+                result
+            }
+
+            ZipEntryDescription.CompressionKind.PLAIN -> compressed.copyOf(zipEntryDescription.compressedSize.toInt())
+        }
+    }
+
+internal fun LargeDynamicMappedBuffer.parseCentralDirectory(): List<ZipEntryDescription> {
 
     val (entriesNumber, offsetOfCentralDirectory) = parseCentralDirectoryRecordsNumberAndOffset()
 
-    var currentOffset = offsetOfCentralDirectory
+    var currentStart = offsetOfCentralDirectory
 
     val result = mutableListOf<ZipEntryDescription>()
     for (i in 0 until entriesNumber) {
-        val headerConst = getInt(currentOffset)
-        require(headerConst == 0x02014b50) {
-            "$i: $headerConst"
+        withMappedRangeFrom(currentStart) {
+            val headerConst = getInt(0)
+            require(headerConst == 0x02014b50) {
+                "$i: $headerConst"
+            }
+
+            val versionNeededToExtract =
+                getShort(6).toInt()
+
+            val compressionMethod = getShort(10).toInt()
+
+            val fileNameLength = getUnsignedShort(28)
+            val extraLength = getUnsignedShort(30)
+
+            val compressedSize = getIntAsLong(20)
+            val uncompressedSize = getIntAsLong(24)
+            val fileCommentLength = getUnsignedShort(32)
+
+            val offsetOfFileData = getIntAsLong(42)
+
+            val bytesForName = getBytes(46, fileNameLength)
+
+            val name =
+                if (bytesForName.all { it >= 0 })
+                    ByteArrayCharSequence(bytesForName)
+                else
+                    String(bytesForName, Charsets.UTF_8)
+            currentStart += 46 + fileNameLength + extraLength + fileCommentLength
+
+            // We support version needed to extract 1.0, 2.0 and 4.5. However, there are zip
+            // files in the eco-system with entries with invalid version to extract
+            // of 0. Therefore, we just check that the version is between 0 and 20 and additionally for version 4.5.
+            require(versionNeededToExtract in 0..20 || versionNeededToExtract == 45) {
+                "Unexpected versionNeededToExtract ($versionNeededToExtract) at $name"
+            }
+
+            val compressionKind = when (compressionMethod) {
+                0 -> ZipEntryDescription.CompressionKind.PLAIN
+                8 -> ZipEntryDescription.CompressionKind.DEFLATE
+                else -> error("Unexpected compression method ($compressionMethod) at $name")
+            }
+
+            result += ZipEntryDescription(
+                name, compressedSize, uncompressedSize, offsetOfFileData, compressionKind,
+                fileNameLength
+            )
         }
-
-        val versionNeededToExtract =
-            getShort(currentOffset + 6).toInt()
-
-        val compressionMethod = getShort(currentOffset + 10).toInt()
-
-        val compressedSize = getInt(currentOffset + 20)
-        val uncompressedSize = getInt(currentOffset + 24)
-        val fileNameLength = getUnsignedShort(currentOffset + 28)
-        val extraLength = getUnsignedShort(currentOffset + 30)
-        val fileCommentLength = getUnsignedShort(currentOffset + 32)
-
-        val offsetOfFileData = getInt(currentOffset + 42)
-
-        val bytesForName = ByteArray(fileNameLength)
-
-        position(currentOffset + 46)
-        get(bytesForName)
-
-        val name =
-            if (bytesForName.all { it >= 0 })
-                ByteArrayCharSequence(bytesForName)
-            else
-                String(bytesForName, Charsets.UTF_8)
-
-        currentOffset += 46 + fileNameLength + extraLength + fileCommentLength
-
-        // We support version needed to extract 1.0, 2.0 and 4.5. However, there are zip
-        // files in the eco-system with entries with invalid version to extract
-        // of 0. Therefore, we just check that the version is between 0 and 20 and additionally for version 4.5.
-        require(versionNeededToExtract in 0..20 || versionNeededToExtract == 45) {
-            "Unexpected versionNeededToExtract ($versionNeededToExtract) at $name"
-        }
-
-        val compressionKind = when (compressionMethod) {
-            0 -> ZipEntryDescription.CompressionKind.PLAIN
-            8 -> ZipEntryDescription.CompressionKind.DEFLATE
-            else -> error("Unexpected compression method ($compressionMethod) at $name")
-        }
-
-        result += ZipEntryDescription(
-            name, compressedSize, uncompressedSize, offsetOfFileData, compressionKind,
-            fileNameLength
-        )
     }
 
     return result
 }
 
-private fun MappedByteBuffer.parseCentralDirectoryRecordsNumberAndOffset(): Pair<Int, Int> {
-    var endOfCentralDirectoryOffset = capacity() - END_OF_CENTRAL_DIR_SIZE
-    while (endOfCentralDirectoryOffset >= 0) {
-        // header of "End of central directory" (see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT)
-        if (getInt(endOfCentralDirectoryOffset) == 0x06054b50) break
-        endOfCentralDirectoryOffset--
+private fun LargeDynamicMappedBuffer.parseCentralDirectoryRecordsNumberAndOffset(): Pair<Long, Long> =
+    withMappedTail {
+        var endOfCentralDirectoryOffset = endOffset() - END_OF_CENTRAL_DIR_SIZE
+        while (endOfCentralDirectoryOffset >= 0) {
+            // header of "End of central directory" (see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT)
+            if (getInt(endOfCentralDirectoryOffset) == 0x06054b50) break
+            endOfCentralDirectoryOffset--
+        }
+
+        val entriesNumber = getUnsignedShort(endOfCentralDirectoryOffset + 10)
+        val offsetOfCentralDirectory = getInt(endOfCentralDirectoryOffset + 16)
+        // Offset of start of central directory, relative to start of archive (or -1 for ZIP64)
+        // (see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT)
+        if (entriesNumber == 0xffff || offsetOfCentralDirectory == -1) parseZip64CentralDirectoryRecordsNumberAndOffset()
+        else Pair(entriesNumber.toLong(), offsetOfCentralDirectory.toUInt().toLong())
     }
 
-    val entriesNumber = getUnsignedShort(endOfCentralDirectoryOffset + 10)
-    val offsetOfCentralDirectory = getInt(endOfCentralDirectoryOffset + 16)
-    // Offset of start of central directory, relative to start of archive (or -1 for ZIP64)
-    // (see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT)
-    if (entriesNumber == 0xffff || offsetOfCentralDirectory == -1) return parseZip64CentralDirectoryRecordsNumberAndOffset()
 
-    return Pair(entriesNumber, offsetOfCentralDirectory)
-}
-
-private fun MappedByteBuffer.parseZip64CentralDirectoryRecordsNumberAndOffset(): Pair<Int, Int> {
-    var endOfCentralDirectoryOffset = capacity() - END_OF_CENTRAL_DIR_ZIP64_SIZE
+private fun LargeDynamicMappedBuffer.Mapping.parseZip64CentralDirectoryRecordsNumberAndOffset(): Pair<Long, Long> {
+    var endOfCentralDirectoryOffset = endOffset() - END_OF_CENTRAL_DIR_ZIP64_SIZE
     while (endOfCentralDirectoryOffset >= 0) {
         // header of "End of central directory" (see https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT)
         if (getInt(endOfCentralDirectoryOffset) == 0x06064b50) break
@@ -152,15 +148,13 @@ private fun MappedByteBuffer.parseZip64CentralDirectoryRecordsNumberAndOffset():
     val entriesNumber = getLong(endOfCentralDirectoryOffset + 32)
     val offsetOfCentralDirectory = getLong(endOfCentralDirectoryOffset + 48)
 
-    require(entriesNumber <= Int.MAX_VALUE) {
-        "Jar $entriesNumber entries number equal or more than ${Int.MAX_VALUE} is not supported by FastJarFS"
-    }
-
-    require(offsetOfCentralDirectory <= Int.MAX_VALUE) {
-        "Jar $offsetOfCentralDirectory offset equal or more than ${Int.MAX_VALUE} is not supported by FastJarFS"
-    }
-
-    return Pair(entriesNumber.toInt(), offsetOfCentralDirectory.toInt())
+    return Pair(entriesNumber, offsetOfCentralDirectory)
 }
 
-private fun ByteBuffer.getUnsignedShort(offset: Int): Int = java.lang.Short.toUnsignedInt(getShort(offset))
+private fun LargeDynamicMappedBuffer.Mapping.getUnsignedShort(offset: Int): Int = java.lang.Short.toUnsignedInt(getShort(offset))
+
+internal fun LargeDynamicMappedBuffer.Mapping.getIntAsLong(offset: Int): Long {
+    val i = getInt(offset)
+    return if (i == -1) -1L else i.toUInt().toLong()
+}
+
--- compiler/tests/org/jetbrains/kotlin/jvm/compiler/FastJarFSTest.kt
+++ compiler/tests/org/jetbrains/kotlin/jvm/compiler/FastJarFSTest.kt
@@ -11,12 +11,10 @@ import junit.framework.TestCase
 import org.jetbrains.kotlin.cli.jvm.compiler.jarfs.FastJarFileSystem
 import org.jetbrains.kotlin.test.KotlinTestUtils
 import org.junit.Assert
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
-import java.io.PrintStream
+import java.io.*
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
+import kotlin.random.Random
 
 
 class FastJarFSTest : TestCase() {
@@ -102,6 +100,99 @@ class FastJarFSTest : TestCase() {
         // Asserting that core jar FS still behaves the same way as the "emulation" implemented in FastJarFS
         Assert.assertTrue(errFromCoreJarFs.contains("WARN: zip file is empty"))
     }
+
+    fun testInterleaveSmallAndBigJarEntriesInOrder() {
+        // tests the zip file larger than Int.MAX_VALUE, so it doesn't fit into MappedByteBuffer, but smaller than UInt.MAX__VALUE,
+        // so no ZIP64 format is triggered
+        val fs = fs ?: return
+        val tmpDir = KotlinTestUtils.tmpDirForTest(this)
+        val jarFile = File(tmpDir, "tmp.jar")
+
+        val chunkSize = 1024 * 1024
+        val repetitions = (Int.MAX_VALUE.toLong() + 1) / chunkSize
+
+        ZipOutputStream(FileOutputStream(jarFile)).use { out ->
+            out.addEntry("small1.txt") { append("smallOne") }
+            out.addRandomEntry("big.txt", chunkSize, repetitions)
+            out.addEntry("small2.txt") { append("smallTwo") }
+        }
+
+        assert(jarFile.length() > Int.MAX_VALUE)
+        assertEquals("smallOne", String(fs.findFileByPath(jarFile.absolutePath + "!/small1.txt")!!.contentsToByteArray()))
+        assertEquals(repetitions * chunkSize, fs.findFileByPath(jarFile.absolutePath + "!/big.txt")!!.length)
+        assertEquals("smallTwo", String(fs.findFileByPath(jarFile.absolutePath + "!/small2.txt")!!.contentsToByteArray()))
+    }
+
+    fun testInterleaveSmallAndBigJarEntriesOutOfOrder() {
+        // similar to testInterleaveSmallAndBigJarEntriesInOrder, but with out of order files reading, promting more remappings
+        val fs = fs ?: return
+        val tmpDir = KotlinTestUtils.tmpDirForTest(this)
+        val jarFile = File(tmpDir, "tmp.jar")
+
+        val chunkSize = 1024 * 1024
+        val repetitions = (Int.MAX_VALUE.toLong() + 1) / chunkSize
+
+        ZipOutputStream(FileOutputStream(jarFile)).use { out ->
+            out.addEntry("small1.txt") { append("smallOne") }
+            out.addRandomEntry("big.txt", chunkSize, repetitions)
+            out.addEntry("small2.txt") { append("smallTwo") }
+        }
+
+        assert(jarFile.length() > Int.MAX_VALUE)
+        assertEquals("smallTwo", String(fs.findFileByPath(jarFile.absolutePath + "!/small2.txt")!!.contentsToByteArray()))
+        assertEquals(repetitions * chunkSize, fs.findFileByPath(jarFile.absolutePath + "!/big.txt")!!.length)
+        assertEquals("smallOne", String(fs.findFileByPath(jarFile.absolutePath + "!/small1.txt")!!.contentsToByteArray()))
+    }
+
+    fun skip_testBigJarDirectory() {
+        // it will be nice to have a test that checks logic of ZIP directory reading on the directories that doesn't fit to MappedByteBuffer
+        // but this tests fails with OOM on such zip file generation.
+        // TODO: find out how to implement such tests
+        val fs = fs ?: return
+        val tmpDir = KotlinTestUtils.tmpDirForTest(this)
+        val jarFile = File(tmpDir, "tmp.jar")
+        val out = ZipOutputStream(FileOutputStream(jarFile))
+
+        val chunkSize = 1024 * 4
+        val characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        val chunk = generateSequence { characters[Random.nextInt(0, characters.length)] }.take(chunkSize).joinToString(separator = "")
+        val entriesNumber = (Int.MAX_VALUE.toLong() + 1) / chunkSize
+        for (i in 0..entriesNumber) {
+            out.putNextEntry(ZipEntry("$i$chunk"))
+            out.writer().apply {
+                append(i.toString())
+                flush()
+            }
+            out.closeEntry()
+        }
+        out.putNextEntry(ZipEntry("000"))
+        out.writer().apply {
+            append("!")
+            flush()
+        }
+        out.close()
+
+        fs.findFileByPath(jarFile.absolutePath + "!/000")
+    }
+}
+
+private fun ZipOutputStream.addEntry(name: String, body: OutputStreamWriter.() -> Unit) {
+    putNextEntry(ZipEntry(name))
+    writer().apply {
+        body()
+        flush()
+    }
+    closeEntry()
+}
+
+private fun ZipOutputStream.addRandomEntry(name: String, chunkSize: Int, repetitions: Long) {
+    putNextEntry(ZipEntry(name))
+    val chunk = Random.nextBytes(ByteArray(chunkSize), 0, chunkSize)
+    for (i in 0 until repetitions) {
+        write(chunk)
+    }
+    flush()
+    closeEntry()
 }
 
 private fun captureErr(body: () -> Unit): String {
