9c00919277ea289c9067c52bbb6f7b62286eec95
Alevtina.Gamzikova <alevtina.gamzikova@jetbrains.com>
2024-07-08 09:33:55 +0000

    [Test] KT-58310 Add additional testcases

--- compiler/testData/diagnostics/tests/resolve/lambdaAgainstTypeVariableWithConstraintAfter.fir.kt
+++ compiler/testData/diagnostics/tests/resolve/lambdaAgainstTypeVariableWithConstraintAfter.fir.kt
@@ -3,29 +3,149 @@
 
 class Inv<T>
 
-fun <T> foo(box: Inv<T>, value: T) {}
-fun <T> foo(box: Inv<T>, value: () -> T) {}
-fun <T> bar(value: T, box: Inv<T>) {}
-fun <T> bar(value: () -> T, box: Inv<T>) {}
-fun <T> Inv<T>.qux(value: T) {}
-fun <T> Inv<T>.qux(value: () -> T) {}
+fun <T> topLevelOverload(value: T, box: Inv<T>) {}
+fun <T> topLevelOverload(value: () -> T, box: Inv<T>) {}
 
-fun test(box: Inv<String>) {
-    foo(box) { "hello" }
-    bar({ "hello" }, box)
-    box.qux { "hello" }
+fun <T> topLevelOverload1(box: Inv<T>, value: T) {}
+fun <T> topLevelOverload1(box: Inv<T>, value: () -> T) {}
+
+fun <T: CharSequence> topLevelOverload2(box: Inv<T>, value: T) {}
+fun <T: CharSequence> topLevelOverload2(box: Inv<T>, value: () -> T) {}
+
+fun <T> topLevelOverload3(box: Inv<T>, value: T) {}
+fun <T> topLevelOverload3(box: Inv<T>, value: (param: T) -> Unit) {}
+
+fun <T> Inv<T>.extensionOverload(value: T) {}
+fun <T> Inv<T>.extensionOverload(value: () -> T) {}
+
+open class FunHolder {
+    fun <T> classMemberOverload(value: T, box: Inv<T>) {}
+    fun <T> classMemberOverload(value: () -> T, box: Inv<T>) {}
+
+    fun <T> classMemberOverload1(box: Inv<T>, value: T) {}
+    fun <T> classMemberOverload1(box: Inv<T>, value: () -> T) {}
+
+    fun <T: CharSequence> classMemberOverload2(box: Inv<T>, value: T) {}
+    fun <T: CharSequence> classMemberOverload2(box: Inv<T>, value: () -> T) {}
+
+    fun <T> classMemberOverload3(box: Inv<T>, value: T) {}
+    fun <T> classMemberOverload3(box: Inv<T>, value: (param: T) -> Unit) {}
 }
 
-fun testAny(box: Inv<Any>) {
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>foo<!>(box) { "hello" }
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>bar<!>({ "hello" }, box)
-    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>qux<!> { "hello" }
+class SubFunHolder: FunHolder() {
+    fun <T> classMemberOverload(value: (param: T) -> Unit, box: Inv<T>) {}
+    fun <T> classMemberOverload1(box: Inv<T>, value: (param: T) -> Unit) {}
+    fun <T: CharSequence> classMemberOverload2(box: Inv<T>, value: (param: T) -> Unit) {}
+    fun <T> classMemberOverload3(box: Inv<T>, value: () -> T) {}
 }
 
-fun testFunctionType(box: Inv<() -> Any?>) {
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>foo<!>(box) { "hello" }
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>bar<!>({ "hello" }, box)
-    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>qux<!> { "hello" }
+class ExtensionHolder {
+    fun <T> mixedOverload(box: Inv<T>, value: T) {}
+}
+
+fun <T> ExtensionHolder.extensionOverload(value: T, box: Inv<T>) {}
+fun <T> ExtensionHolder.extensionOverload(value: () -> T, box: Inv<T>) {}
+
+fun <T> ExtensionHolder.extensionOverload1(box: Inv<T>, value: T) {}
+fun <T> ExtensionHolder.extensionOverload1(box: Inv<T>, value: () -> T) {}
+
+fun <T: CharSequence> ExtensionHolder.extensionOverload2(box: Inv<T>, value: T) {}
+fun <T: CharSequence> ExtensionHolder.extensionOverload2(box: Inv<T>, value: () -> T) {}
+
+fun <T> ExtensionHolder.extensionOverload3(box: Inv<T>, value: T) {}
+fun <T> ExtensionHolder.extensionOverload3(box: Inv<T>, value: (param: T) -> Unit) {}
+
+fun <T> ExtensionHolder.mixedOverload(box: Inv<T>, value: () -> T) {}
+
+fun testWithString(box: Inv<String>) {
+    topLevelOverload({ "hello" }, (box))
+    topLevelOverload1(box) { "hello" }
+    topLevelOverload2(box) { "hello" }
+    topLevelOverload2(box) { <!ARGUMENT_TYPE_MISMATCH!>1<!> }
+    topLevelOverload3(box) { param: String -> }
+
+    FunHolder().classMemberOverload({ "hello" }, (box))
+    FunHolder().classMemberOverload1(box) { "hello" }
+    FunHolder().classMemberOverload2(box) { "hello" }
+    FunHolder().classMemberOverload3(box) { param: String -> }
+
+    SubFunHolder().classMemberOverload ({ param: String -> }, (box))
+    SubFunHolder().classMemberOverload1(box) { param: String -> }
+    SubFunHolder().classMemberOverload2(box) { param: String -> }
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { "hello" }
+
+    ExtensionHolder().extensionOverload({ "hello" }, (box))
+    ExtensionHolder().extensionOverload1(box) { "hello" }
+    ExtensionHolder().extensionOverload2(box) { "hello" }
+    ExtensionHolder().extensionOverload3(box) { param: String -> }
+    ExtensionHolder().mixedOverload(box) { "hello" }
+
+    box.extensionOverload { "hello" }
+}
+
+fun testWithAny(box: Inv<Any>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { "hello" }
+
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload<!>({ "hello" }, (box))
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload1<!>(box) { "hello" }
+
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { "hello" }
+
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!>({ "hello" }, (box))
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload1<!>(box) { "hello" }
+
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun testWithStringSuper(box: Inv<CharSequence>) {
+    topLevelOverload({ "hello" }, (box))
+    topLevelOverload1(box) { "hello" }
+    topLevelOverload2(box) { "hello" }
+    topLevelOverload3(box) { param: CharSequence -> }
+
+    FunHolder().classMemberOverload({ "hello" }, (box))
+    FunHolder().classMemberOverload1(box) { "hello" }
+    FunHolder().classMemberOverload2(box) { "hello" }
+    FunHolder().classMemberOverload3(box) { param: CharSequence -> }
+
+    SubFunHolder().classMemberOverload({ param: CharSequence -> }, (box))
+    SubFunHolder().classMemberOverload1(box) { param: CharSequence -> }
+    SubFunHolder().classMemberOverload2(box) { param: CharSequence -> }
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { "hello" }
+
+    ExtensionHolder().extensionOverload({ "hello" }, (box))
+    ExtensionHolder().extensionOverload1(box) { "hello" }
+    ExtensionHolder().extensionOverload2(box) { "hello" }
+    ExtensionHolder().extensionOverload3(box) { param: CharSequence -> }
+    ExtensionHolder().mixedOverload(box) { "hello" }
+
+    box.extensionOverload { "hello" }
+}
+
+fun testWithCallableString(box: Inv<() -> String>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { { "hello" } }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload3<!>(box) { { param: CharSequence -> } }
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun testWithCallableAny(box: Inv<() -> Any?>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { { "hello" } }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload3<!>(box) { { param: CharSequence -> } }
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun interface SAM<T> {
+    fun single(a: Inv<T>, value: T): Unit
+}
+
+fun testSam() {
+    val k = SAM { a: Inv<String>, value -> topLevelOverload1(a, value) }
+    k.single(Inv<String>(), "")
+    val k2 = SAM { a: Inv<()->String>, value -> <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(a, {value}) }
+    k2.single(Inv<()->String>()) { "hello" }
 }
 
 // Check error when there is only one candidate
--- compiler/testData/diagnostics/tests/resolve/lambdaAgainstTypeVariableWithConstraintAfter.kt
+++ compiler/testData/diagnostics/tests/resolve/lambdaAgainstTypeVariableWithConstraintAfter.kt
@@ -3,29 +3,149 @@
 
 class Inv<T>
 
-fun <T> foo(box: Inv<T>, value: T) {}
-fun <T> foo(box: Inv<T>, value: () -> T) {}
-fun <T> bar(value: T, box: Inv<T>) {}
-fun <T> bar(value: () -> T, box: Inv<T>) {}
-fun <T> Inv<T>.qux(value: T) {}
-fun <T> Inv<T>.qux(value: () -> T) {}
+fun <T> topLevelOverload(value: T, box: Inv<T>) {}
+fun <T> topLevelOverload(value: () -> T, box: Inv<T>) {}
 
-fun test(box: Inv<String>) {
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>foo<!>(box) { "hello" }
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>bar<!>({ "hello" }, box)
-    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>qux<!> { "hello" }
+fun <T> topLevelOverload1(box: Inv<T>, value: T) {}
+fun <T> topLevelOverload1(box: Inv<T>, value: () -> T) {}
+
+fun <T: CharSequence> topLevelOverload2(box: Inv<T>, value: T) {}
+fun <T: CharSequence> topLevelOverload2(box: Inv<T>, value: () -> T) {}
+
+fun <T> topLevelOverload3(box: Inv<T>, value: T) {}
+fun <T> topLevelOverload3(box: Inv<T>, value: (param: T) -> Unit) {}
+
+fun <T> Inv<T>.extensionOverload(value: T) {}
+fun <T> Inv<T>.extensionOverload(value: () -> T) {}
+
+open class FunHolder {
+    fun <T> classMemberOverload(value: T, box: Inv<T>) {}
+    fun <T> classMemberOverload(value: () -> T, box: Inv<T>) {}
+
+    fun <T> classMemberOverload1(box: Inv<T>, value: T) {}
+    fun <T> classMemberOverload1(box: Inv<T>, value: () -> T) {}
+
+    fun <T: CharSequence> classMemberOverload2(box: Inv<T>, value: T) {}
+    fun <T: CharSequence> classMemberOverload2(box: Inv<T>, value: () -> T) {}
+
+    fun <T> classMemberOverload3(box: Inv<T>, value: T) {}
+    fun <T> classMemberOverload3(box: Inv<T>, value: (param: T) -> Unit) {}
 }
 
-fun testAny(box: Inv<Any>) {
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>foo<!>(box) { "hello" }
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>bar<!>({ "hello" }, box)
-    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>qux<!> { "hello" }
+class SubFunHolder: FunHolder() {
+    fun <T> classMemberOverload(value: (param: T) -> Unit, box: Inv<T>) {}
+    fun <T> classMemberOverload1(box: Inv<T>, value: (param: T) -> Unit) {}
+    fun <T: CharSequence> classMemberOverload2(box: Inv<T>, value: (param: T) -> Unit) {}
+    fun <T> classMemberOverload3(box: Inv<T>, value: () -> T) {}
 }
 
-fun testFunctionType(box: Inv<() -> Any?>) {
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>foo<!>(box) { "hello" }
-    <!OVERLOAD_RESOLUTION_AMBIGUITY!>bar<!>({ "hello" }, box)
-    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>qux<!> { "hello" }
+class ExtensionHolder {
+    fun <T> mixedOverload(box: Inv<T>, value: T) {}
+}
+
+fun <T> ExtensionHolder.extensionOverload(value: T, box: Inv<T>) {}
+fun <T> ExtensionHolder.extensionOverload(value: () -> T, box: Inv<T>) {}
+
+fun <T> ExtensionHolder.extensionOverload1(box: Inv<T>, value: T) {}
+fun <T> ExtensionHolder.extensionOverload1(box: Inv<T>, value: () -> T) {}
+
+fun <T: CharSequence> ExtensionHolder.extensionOverload2(box: Inv<T>, value: T) {}
+fun <T: CharSequence> ExtensionHolder.extensionOverload2(box: Inv<T>, value: () -> T) {}
+
+fun <T> ExtensionHolder.extensionOverload3(box: Inv<T>, value: T) {}
+fun <T> ExtensionHolder.extensionOverload3(box: Inv<T>, value: (param: T) -> Unit) {}
+
+fun <T> ExtensionHolder.mixedOverload(box: Inv<T>, value: () -> T) {}
+
+fun testWithString(box: Inv<String>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { "hello" }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload2<!>(box) { "hello" }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload2<!>(box) { 1 }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload3<!>(box) { param: String -> }
+
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload<!>({ "hello" }, (box))
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload1<!>(box) { "hello" }
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload2<!>(box) { "hello" }
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { param: String -> }
+
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload<!> ({ param: String -> }, (box))
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload1<!>(box) { param: String -> }
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload2<!>(box) { param: String -> }
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { "hello" }
+
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!>({ "hello" }, (box))
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload1<!>(box) { "hello" }
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload2<!>(box) { "hello" }
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload3<!>(box) { param: String -> }
+    ExtensionHolder().mixedOverload(box) <!TYPE_MISMATCH!>{ <!TYPE_MISMATCH!>"hello"<!> }<!>
+
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun testWithAny(box: Inv<Any>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { "hello" }
+
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload<!>({ "hello" }, (box))
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload1<!>(box) { "hello" }
+
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { "hello" }
+
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!>({ "hello" }, (box))
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload1<!>(box) { "hello" }
+
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun testWithStringSuper(box: Inv<CharSequence>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { "hello" }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload2<!>(box) { "hello" }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload3<!>(box) { param: CharSequence -> }
+
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload<!>({ "hello" }, (box))
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload1<!>(box) { "hello" }
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload2<!>(box) { "hello" }
+    FunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { param: CharSequence -> }
+
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload<!>({ param: CharSequence -> }, (box))
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload1<!>(box) { param: CharSequence -> }
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload2<!>(box) { param: CharSequence -> }
+    SubFunHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>classMemberOverload3<!>(box) { "hello" }
+
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!>({ "hello" }, (box))
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload1<!>(box) { "hello" }
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload2<!>(box) { "hello" }
+    ExtensionHolder().<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload3<!>(box) { param: CharSequence -> }
+    ExtensionHolder().mixedOverload(box) <!TYPE_MISMATCH!>{ <!TYPE_MISMATCH!>"hello"<!> }<!>
+
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun testWithCallableString(box: Inv<() -> String>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { { "hello" } }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload3<!>(box) { { param: CharSequence -> } }
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun testWithCallableAny(box: Inv<() -> Any?>) {
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload<!>({ "hello" }, (box))
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(box) { { "hello" } }
+    <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload3<!>(box) { { param: CharSequence -> } }
+    box.<!OVERLOAD_RESOLUTION_AMBIGUITY!>extensionOverload<!> { "hello" }
+}
+
+fun interface SAM<T> {
+    fun single(a: Inv<T>, value: T): Unit
+}
+
+fun testSam() {
+    val k = SAM { a: Inv<String>, value -> topLevelOverload1(a, value) }
+    k.single(Inv<String>(), "")
+    val k2 = SAM { a: Inv<()->String>, value -> <!OVERLOAD_RESOLUTION_AMBIGUITY!>topLevelOverload1<!>(a, {value}) }
+    k2.single(Inv<()->String>()) { "hello" }
 }
 
 // Check error when there is only one candidate
