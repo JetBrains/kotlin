d6511456e092202dd74f1246552a4296d2caaef6
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2024-07-08 15:18:51 +0200

    Build: Refactor android sdk dependencies declaration
     #KT-65049
     #KTI-1812

--- dependencies/android-sdk/build.gradle.kts
+++ dependencies/android-sdk/build.gradle.kts
@@ -28,50 +28,16 @@ val platformToolsVersion = "r28.0.1"
 val sdkToolsVersion = "4333796" /*26.1.1*/
 val emulatorVersion = "5264690"
 
-val buildTools_30_0_3_artifactId = mapOf(
-    "windows" to "91936d4ee3ccc839f0addd53c9ebf087b1e39251.build-tools",
-    "macosx" to "f6d24b187cc6bd534c6c37604205171784ac5621.build-tools"
-).withDefault { "build-tools" }
-
 dependencies {
-    implicitDependencies("google:platform:23_r03@zip")
-    implicitDependencies("google:platform:26_r02@zip")
-    implicitDependencies("google:platform:33_r02@zip")
-    implicitDependencies("google:platform:34-ext7_r02@zip")
-
-    implicitDependencies("google:build-tools:r29.0.3:linux@zip")
-    implicitDependencies("google:build-tools:r29.0.3:windows@zip")
-    implicitDependencies("google:build-tools:r29.0.3:macosx@zip")
-
-    implicitDependencies("google:${buildTools_30_0_3_artifactId.getValue("linux")}:r30.0.3:linux@zip")
-    implicitDependencies("google:${buildTools_30_0_3_artifactId.getValue("windows")}:r30.0.3:windows@zip")
-    implicitDependencies("google:${buildTools_30_0_3_artifactId.getValue("macosx")}:r30.0.3:macosx@zip")
-
-    implicitDependencies("google:build-tools:r33.0.1:linux@zip")
-    implicitDependencies("google:build-tools:r33.0.1:windows@zip")
-    implicitDependencies("google:build-tools:r33.0.1:macosx@zip")
-
-    implicitDependencies("google:build-tools:r34:linux@zip")
-    implicitDependencies("google:build-tools:r34:windows@zip")
-    implicitDependencies("google:build-tools:r34:macosx@zip")
-
-    implicitDependencies("google:platform-tools:$platformToolsVersion:linux@zip")
-    implicitDependencies("google:platform-tools:$platformToolsVersion:windows@zip")
-    implicitDependencies("google:platform-tools:$platformToolsVersion:darwin@zip")
-
-    implicitDependencies("google:sdk-tools-linux:$sdkToolsVersion@zip")
-    implicitDependencies("google:sdk-tools-windows:$sdkToolsVersion@zip")
-    implicitDependencies("google:sdk-tools-darwin:$sdkToolsVersion@zip")
-
-    implicitDependencies("google:emulator-linux:$emulatorVersion@zip")
-    implicitDependencies("google:emulator-windows:$emulatorVersion@zip")
-    implicitDependencies("google:emulator-darwin:$emulatorVersion@zip")
+    listOf("linux", "windows", "darwin").forEach {
+        implicitDependencies("google:platform-tools:$platformToolsVersion:$it@zip")
+        implicitDependencies("google:sdk-tools-$it:$sdkToolsVersion@zip")
+        implicitDependencies("google:emulator-$it:$emulatorVersion@zip")
+    }
 }
 
 val androidSdk by configurations.creating
 val androidJar by configurations.creating
-val androidPlatform by configurations.creating
-val buildTools by configurations.creating
 val androidEmulator by configurations.creating
 
 val sdkDestDirName = "androidSdk"
@@ -167,27 +133,73 @@ fun unzipSdkTask(
     return unzipTask
 }
 
-unzipSdkTask("android", "22_r02", "platforms/android-22", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "23_r03", "platforms/android-23", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "24_r02", "platforms/android-24", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "26_r02", "platforms/android-26", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "27_r03", "platforms/android-27", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "28_r06", "platforms/android-28", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "30_r03", "platforms/android-30", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "31_r01", "platforms/android-31", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "33_r02", "platforms/android-33", "", androidPlatform, 1, prepareTask = preparePlatform)
-unzipSdkTask("platform", "34-ext7_r02", "platforms/android-34", "", androidPlatform, 1, prepareTask = preparePlatform)
+fun androidPlatform(version: String): TaskProvider<Task> {
+    val artifactId = if (version.startsWith("22_")) "android" else "platform"
+    return unzipSdkTask(
+        sdkName = artifactId,
+        sdkVer = version,
+        destinationSubdir = "platforms/android-${version.substringBefore("_").substringBefore("-")}",
+        coordinatesSuffix = "",
+        additionalConfig = configurations.implicitDependencies.get(),
+        dirLevelsToSkipOnUnzip = 1,
+        prepareTask = preparePlatform
+    )
+}
 
-unzipSdkTask("build-tools", "r29.0.3", "build-tools/29.0.3", toolsOs, buildTools, 1)
-unzipSdkTask(buildTools_30_0_3_artifactId.getValue(toolsOs), "r30.0.3", "build-tools/30.0.3", toolsOs, buildTools, 1)
-unzipSdkTask("build-tools", "r33.0.1", "build-tools/33.0.1", toolsOs, buildTools, 1)
-unzipSdkTask("build-tools", "r34", "build-tools/34.0.0", toolsOs, buildTools, 1)
+fun androidBuildTools(version: String): TaskProvider<Task> {
+    val revision = if (version == "34.0.0") "r34" else "r$version"
+
+    @Suppress("LocalVariableName")
+    val buildTools_30_0_3_artifactId = mapOf(
+        "windows" to "91936d4ee3ccc839f0addd53c9ebf087b1e39251.build-tools",
+        "macosx" to "f6d24b187cc6bd534c6c37604205171784ac5621.build-tools"
+    ).withDefault { "build-tools" }
+
+    listOf("linux", "windows", "macosx").forEach {
+        dependencies {
+            val artifactId = if (version == "30.0.3")
+                buildTools_30_0_3_artifactId.getValue(it)
+            else
+                "build-tools"
+            implicitDependencies("google:$artifactId:$revision:$it@zip")
+        }
+    }
+
+    val artifactId = if (version == "30.0.3")
+        buildTools_30_0_3_artifactId.getValue(toolsOs)
+    else
+        "build-tools"
+
+    return unzipSdkTask(
+        sdkName = artifactId,
+        sdkVer = revision,
+        destinationSubdir = "build-tools/$version",
+        coordinatesSuffix = toolsOs,
+        dirLevelsToSkipOnUnzip = 1
+    )
+}
+
+androidPlatform("22_r02")
+androidPlatform("23_r03")
+androidPlatform("24_r02")
+androidPlatform("26_r02")
+androidPlatform("27_r03")
+androidPlatform("28_r06")
+androidPlatform("30_r03")
+androidPlatform("31_r01")
+androidPlatform("33_r02")
+androidPlatform("34-ext7_r02")
+
+androidBuildTools("29.0.3")
+androidBuildTools("30.0.3")
+androidBuildTools("33.0.1")
+androidBuildTools("34.0.0")
 
 unzipSdkTask("android_m2repository", "r44", "extras/android", "")
 unzipSdkTask("platform-tools", platformToolsVersion, "", toolsOsDarwin)
 unzipSdkTask("sdk-tools-$toolsOsDarwin", sdkToolsVersion, "", "")
 unzipSdkTask("emulator-$toolsOsDarwin", emulatorVersion, "", "", prepareTask = prepareEmulator)
-unzipSdkTask("armeabi-v7a", "19", "system-images/android-19/default","r05", prepareTask = prepareEmulator)
+unzipSdkTask("armeabi-v7a", "19", "system-images/android-19/default", "r05", prepareTask = prepareEmulator)
 unzipSdkTask("x86", "19", "system-images/android-19/default", "r06", prepareTask = prepareEmulator)
 
 val clean by task<Delete> {
