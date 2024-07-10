88bdac6f92ee82ea78954fd4cbdc321a6d0f7a3e
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-07-10 16:56:31 +0300

    fixup! [FIR] Remove duplicate diagnostics in metadata by making checkers `DSFAPFO`
    Make `FirJvmConflictsChecker` an expect checker.

--- compiler/testData/diagnostics/tests/multiplatform/java/implicitJavaActualization_multipleActuals.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/java/implicitJavaActualization_multipleActuals.fir.kt
@@ -1,7 +1,7 @@
 // MODULE: m1-common
 // FILE: common.kt
 
-<!EXPECT_ACTUAL_INCOMPATIBILITY{JVM}!>expect class <!CLASSIFIER_REDECLARATION, CLASSIFIER_REDECLARATION{METADATA}!>Foo<!>(i: Int) {
+<!EXPECT_ACTUAL_INCOMPATIBILITY{JVM}!>expect class <!CLASSIFIER_REDECLARATION!>Foo<!>(i: Int) {
     fun foo()
 }<!>
 
