31d5c14085299231a217bc5eb1923a052b77ce70
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-07-10 16:38:50 +0300

    fixup! [FIR] Remove duplicate diagnostics in metadata by making checkers `DSFAPFO`
    Make `FirTailrecFunctionChecker` an expect checker.

--- compiler/testData/diagnostics/tests/multiplatform/expectTailrec.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/expectTailrec.fir.kt
@@ -1,13 +1,13 @@
 // MODULE: m1-common
 // FILE: common.kt
 
-expect <!EXPECTED_TAILREC_FUNCTION, EXPECTED_TAILREC_FUNCTION{METADATA}, NO_TAIL_CALLS_FOUND, NO_TAIL_CALLS_FOUND{METADATA}!>tailrec<!> fun foo(p: Int): Int
+expect <!EXPECTED_TAILREC_FUNCTION, EXPECTED_TAILREC_FUNCTION{METADATA}, NO_TAIL_CALLS_FOUND!>tailrec<!> fun foo(p: Int): Int
 expect fun bar(p: Int): Int
 
 expect <!WRONG_MODIFIER_TARGET!>tailrec<!> val notReport: String
 
 expect class A {
-    <!EXPECTED_TAILREC_FUNCTION, EXPECTED_TAILREC_FUNCTION{METADATA}, NO_TAIL_CALLS_FOUND, NO_TAIL_CALLS_FOUND{METADATA}!>tailrec<!> fun foo(p: Int): Int
+    <!EXPECTED_TAILREC_FUNCTION, EXPECTED_TAILREC_FUNCTION{METADATA}, NO_TAIL_CALLS_FOUND!>tailrec<!> fun foo(p: Int): Int
     fun bar(p: Int): Int
 }
 
--- compiler/testData/diagnostics/tests/multiplatform/expectTailrec_oldLanguageVersion.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/expectTailrec_oldLanguageVersion.fir.kt
@@ -2,7 +2,7 @@
 // MODULE: m1-common
 // FILE: common.kt
 
-expect <!NO_TAIL_CALLS_FOUND, NO_TAIL_CALLS_FOUND{METADATA}!>tailrec<!> fun foo(p: Int): Int
+expect <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun foo(p: Int): Int
 
 // MODULE: m1-jvm()()(m1-common)
 // FILE: jvm.kt
