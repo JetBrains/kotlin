3514a5c743923f5e42e7ebbfa2a278c6a4b2cbc3
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-07-12 11:58:22 +0200

    Kapt: use IR instead of PSI to compute line information
    This will help in implementing Kapt3 over K2 (KT-69860), where PSI is
    not always available.
    
    Line information is used to report errors and to sort declarations in
    kapt. Ordering of some declarations was changed, but it's fine because
    those are mostly synthetic declarations (data class-generated members,
    bridges, etc.), which are unlikely to affect any real-world annotation
    processors.
    
     #KT-69861 Fixed

--- plugins/kapt3/kapt3-compiler/src/org/jetbrains/kotlin/kapt3/stubs/KaptLineMappingCollector.kt
+++ plugins/kapt3/kapt3-compiler/src/org/jetbrains/kotlin/kapt3/stubs/KaptLineMappingCollector.kt
@@ -17,9 +17,21 @@
 package org.jetbrains.kotlin.kapt3.stubs
 
 import com.sun.tools.javac.tree.JCTree
+import org.jetbrains.kotlin.backend.jvm.JvmLoweredDeclarationOrigin
+import org.jetbrains.kotlin.backend.jvm.extensions.JvmIrDeclarationOrigin
+import org.jetbrains.kotlin.backend.jvm.ir.erasedUpperBound
+import org.jetbrains.kotlin.backend.jvm.ir.fileParent
+import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
+import org.jetbrains.kotlin.ir.declarations.*
+import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
+import org.jetbrains.kotlin.ir.util.fields
+import org.jetbrains.kotlin.ir.util.functions
+import org.jetbrains.kotlin.ir.util.parentAsClass
 import org.jetbrains.kotlin.kapt3.KaptContextForStubGeneration
 import org.jetbrains.kotlin.kapt3.base.stubs.KotlinPosition
 import org.jetbrains.kotlin.kapt3.base.stubs.getJavacSignature
+import org.jetbrains.kotlin.load.java.JvmAbi
+import org.jetbrains.kotlin.name.SpecialNames
 import org.jetbrains.org.objectweb.asm.tree.ClassNode
 import org.jetbrains.org.objectweb.asm.tree.FieldNode
 import org.jetbrains.org.objectweb.asm.tree.MethodNode
@@ -46,7 +58,54 @@ class KaptLineMappingCollector(private val kaptContext: KaptContextForStubGenera
     fun getPosition(clazz: ClassNode, field: FieldNode): KotlinPosition? = lineInfo[clazz.name + "#" + field.name]
 
     private fun register(asmNode: Any, fqName: String) {
-        val psiElement = kaptContext.origins[asmNode]?.element ?: return
-        register(fqName, psiElement)
+        val origin = kaptContext.origins[asmNode]
+        val ir = (origin as? JvmIrDeclarationOrigin)?.declaration ?: return
+        val offset = computeOffset(ir)
+
+        lineInfo[fqName] = KotlinPosition(ir.fileParent.fileEntry.name, isRelativePath = false, offset)
+    }
+
+    private fun computeOffset(ir: IrDeclaration): Int {
+        if (ir.origin == JvmLoweredDeclarationOrigin.SYNTHETIC_METHOD_FOR_PROPERTY_OR_TYPEALIAS_ANNOTATIONS) {
+            // `$annotations` functions have -1 offsets in the IR, but we'd like to keep them closer to the accessors of the corresponding
+            // property to maintain the order that was used when line information was computed by PSI.
+            val getter = ir.findPropertyGetterOrFieldBySyntheticAnnotationsMethod()
+            if (getter != null) return getter.startOffset
+        }
+
+        // All other functions with -1 offsets are supposed to be synthesized by the compiler and it makes sense for them to be at the end.
+        if (ir.startOffset == UNDEFINED_OFFSET) return Int.MAX_VALUE
+
+        // Some declarations are synthesized by the compiler, but their offsets are not -1: enum entries, delegated members.
+        // Let's place them at the end as well, as other synthesized declarations.
+
+        if (ir is IrFunction && ir.name == SpecialNames.ENUM_GET_ENTRIES && ir.origin == IrDeclarationOrigin.ENUM_CLASS_SPECIAL_MEMBER)
+            return Int.MAX_VALUE
+
+        if (ir.origin == IrDeclarationOrigin.DELEGATED_MEMBER)
+            return Int.MAX_VALUE
+
+        return ir.startOffset
+    }
+
+    @OptIn(UnsafeDuringIrConstructionAPI::class)
+    private fun IrDeclaration.findPropertyGetterOrFieldBySyntheticAnnotationsMethod(): IrDeclaration? {
+        if (this !is IrSimpleFunction) return null
+        for (declaration in parentAsClass.functions + parentAsClass.fields) {
+            val property = when (declaration) {
+                is IrSimpleFunction -> declaration.correspondingPropertySymbol?.owner
+                is IrField -> declaration.correspondingPropertySymbol?.owner
+                else -> null
+            } ?: continue
+            val expectedAnnotationsMethodName =
+                JvmAbi.getSyntheticMethodNameForAnnotatedProperty(JvmAbi.getterName(property.name.asString()))
+            val extensionReceiver = (declaration as? IrSimpleFunction)?.extensionReceiverParameter
+            if (expectedAnnotationsMethodName == name.asString() &&
+                extensionReceiver?.type?.erasedUpperBound == valueParameters.firstOrNull()?.type?.erasedUpperBound
+            ) {
+                return declaration
+            }
+        }
+        return null
     }
 }
--- plugins/kapt3/kapt3-compiler/src/org/jetbrains/kotlin/kapt3/stubs/KaptLineMappingCollectorBase.kt
+++ plugins/kapt3/kapt3-compiler/src/org/jetbrains/kotlin/kapt3/stubs/KaptLineMappingCollectorBase.kt
@@ -5,49 +5,14 @@
 
 package org.jetbrains.kotlin.kapt3.stubs
 
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.kapt3.base.stubs.KaptStubLineInformation
-import org.jetbrains.kotlin.kapt3.base.stubs.KotlinPosition
 import org.jetbrains.kotlin.kapt3.base.stubs.LineInfoMap
 import java.io.ByteArrayOutputStream
-import java.io.File
 import java.io.ObjectOutputStream
 
 abstract class KaptLineMappingCollectorBase {
     protected val lineInfo: LineInfoMap = mutableMapOf()
     protected val signatureInfo = mutableMapOf<String, String>()
-    private val filePaths = mutableMapOf<PsiFile, Pair<String, Boolean>>()
-
-    protected fun register(fqName: String, psiElement: PsiElement) {
-        val containingVirtualFile = psiElement.containingFile.virtualFile
-        if (containingVirtualFile == null || FileDocumentManager.getInstance().getDocument(containingVirtualFile) == null) {
-            return
-        }
-
-        val textRange = psiElement.textRange ?: return
-
-        val (path, isRelative) = getFilePathRelativePreferred(psiElement.containingFile)
-        lineInfo[fqName] = KotlinPosition(path, isRelative, textRange.startOffset)
-    }
-
-    private fun getFilePathRelativePreferred(file: PsiFile): Pair<String, Boolean> {
-        return filePaths.getOrPut(file) {
-            val absolutePath = file.virtualFile.canonicalPath ?: file.virtualFile.path
-            val absoluteFile = File(absolutePath)
-            val baseFile = file.project.basePath?.let { File(it) }
-
-            if (absoluteFile.exists() && baseFile != null && baseFile.exists()) {
-                val relativePath = absoluteFile.relativeToOrNull(baseFile)?.path
-                if (relativePath != null) {
-                    return@getOrPut Pair(relativePath, true)
-                }
-            }
-
-            return@getOrPut Pair(absolutePath, false)
-        }
-    }
 
     fun serialize(): ByteArray {
         val os = ByteArrayOutputStream()
