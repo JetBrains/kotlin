8790fd27da7cbbf15c073a131a2a21a15a7668e2
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-07-13 19:36:03 +0200

    IR Inliner: Add the generated synthetic accessor dump files for Native
    ^KT-69462

--- compiler/testData/klib/syntheticAccessors/outerThis/crossModuleLeak/leakingOuterPrivateVarThroughInnerClass.kt
+++ compiler/testData/klib/syntheticAccessors/outerThis/crossModuleLeak/leakingOuterPrivateVarThroughInnerClass.kt
@@ -1,3 +1,7 @@
+// IGNORE_BACKEND: NATIVE
+// ^^^ Muted, because the IR node for accessing "outher this" in Native (IrGetField of `$this$0` field) is different from
+//     the corresponding IR node in JS (IrGetValue of `<this>` value parameter). To be unmuted along with the fix of KT-67172.
+
 // MODULE: lib
 // FILE: Outer.kt
 class Outer {
