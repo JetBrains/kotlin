05ab986ca4d098195660d1fa50ea641f0b3b81d9
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-07-15 22:36:19 +0200

    fixup! Add static checks to swift-export-embeddable

--- native/swift/swift-export-embeddable/build.gradle.kts
+++ native/swift/swift-export-embeddable/build.gradle.kts
@@ -52,29 +52,18 @@ dependencies {
         "org.checkerframework" to "checker-qual",
         "com.google.errorprone" to "error_prone_annotations",
     )
-    val inheritedDependencies = projectsToInheritDependenciesFrom.incoming.resolutionResult.allComponents.filter {
-        val moduleVersion = it.moduleVersion ?: return@filter false
-        dependenciesToInherit[moduleVersion.group] == moduleVersion.name
-    }.map { it.moduleVersion!! }
-
-    val validateAllDependenciesWereInheritedCorrectly by tasks.registering {
-        doLast {
-            if (inheritedDependencies.size != dependenciesToInherit.size) {
-                error("Actual inherited dependencies: $inheritedDependencies")
-            }
-        }
-    }
+    val validateAllDependenciesWereInheritedCorrectly = inheritAndValidateExternalDependencies(
+        sourceConfiguration = projectsToInheritDependenciesFrom,
+        targetConfiguration = configurations.getByName("runtimeOnly"),
+        dependenciesToInherit = dependenciesToInherit,
+    )
     validateSwiftExportEmbeddable.configure { dependsOn(validateAllDependenciesWereInheritedCorrectly) }
 
-    inheritedDependencies.forEach {
-        runtimeOnly(group = it.group, name = it.name, version = it.version)
-    }
     runtimeOnly(kotlinStdlib())
     runtimeOnly(project(":kotlin-compiler-embeddable"))
 }
 
-fun validateSwiftExportEmbeddable(swiftExportEmbeddableJarTask: TaskProvider<out org.gradle.jvm.tasks.Jar>) {
-    val swiftExportEmbeddableJar = files(swiftExportEmbeddableJarTask)
+fun registerSwiftExportEmbeddableValidationTasks(swiftExportEmbeddableJarTask: TaskProvider<out org.gradle.jvm.tasks.Jar>) {
     val runtimeClasspathCopy = configurations.runtimeClasspath.get().copyRecursive()
     runtimeClasspathCopy.dependencies.addAll(
         listOf(
@@ -86,87 +75,19 @@ fun validateSwiftExportEmbeddable(swiftExportEmbeddableJarTask: TaskProvider<out
             dependencies.create(project(":kotlin-assignment-compiler-plugin.embeddable")),
         )
     )
-    val swiftExportActionRuntimeClasspath = files(runtimeClasspathCopy)
-    val proguardedSwiftExportEmbeddableJar = layout.buildDirectory.file("proguard/output.jar")
 
-    /**
-     * This task reproduces SwiftExportAction classpath and runs ProGuard on the swift-export-embeddable. ProGuard will fail the task if
-     * swift-export-embeddable references classes that are not present in libraryjars.
-     */
     // FIXME: Publish the ProGuarded version of swift-export-embeddable - KT-69180
-    val validateSwiftExportEmbeddableHasProperDependenciesInTheClasspath by tasks.registering(CacheableProguardTask::class) {
-        outputs.cacheIf { false }
-        outputs.upToDateWhen { false }
-
-        dependsOn(swiftExportActionRuntimeClasspath)
-        dependsOn(swiftExportEmbeddableJar)
-        javaLauncher.set(project.getToolchainLauncherFor(JdkMajorVersion.JDK_1_8))
-
-        configuration("swift-export-embeddable.pro")
-
-        injars(swiftExportEmbeddableJar)
-        outjars(proguardedSwiftExportEmbeddableJar)
-
-        libraryjars(swiftExportActionRuntimeClasspath)
-        libraryjars(
-            files(
-                javaLauncher.map {
-                    firstFromJavaHomeThatExists(
-                        "jre/lib/rt.jar",
-                        "../Classes/classes.jar",
-                        jdkHome = it.metadata.installationPath.asFile
-                    )!!
-                },
-                javaLauncher.map {
-                    firstFromJavaHomeThatExists(
-                        "jre/lib/jsse.jar",
-                        "../Classes/jsse.jar",
-                        jdkHome = it.metadata.installationPath.asFile
-                    )!!
-                },
-                javaLauncher.map {
-                    Jvm.forHome(it.metadata.installationPath.asFile).toolsJar!!
-                }
-            )
-        )
-    }
-
+    val validateSwiftExportEmbeddableHasProperDependenciesInTheClasspath = validateEmbeddedJarClasspathUsingProguard(
+        swiftExportEmbeddableJarTask,
+        files(runtimeClasspathCopy),
+        proguardConfiguration = project.layout.projectDirectory.file("swift-export-embeddable.pro")
+    )
     validateSwiftExportEmbeddable.configure { dependsOn(validateSwiftExportEmbeddableHasProperDependenciesInTheClasspath) }
 
-    fun String.isImplementationClassFile() = endsWith(".class") && !endsWith("module-info.class")
-    fun File.forEachClassFileInAJar(action: (ZipEntry) -> (Unit)) = ZipFile(this).use { zip ->
-        zip.entries().asSequence().filter {
-            it.name.isImplementationClassFile()
-        }.forEach { action(it) }
-    }
-
-    /**
-     * This task makes sure there are no duplicates in the runtime classpath; i.e. runtimeOnly dependencies are correctly specified and
-     * there is no collision with embedded classes
-     */
-    val validateNoDuplicatesInRuntimeClasspath by tasks.registering {
-        dependsOn(swiftExportActionRuntimeClasspath)
-        dependsOn(swiftExportEmbeddableJar)
-
-        doLast {
-            val duplicates = mutableMapOf<String, MutableList<File>>()
-            (swiftExportActionRuntimeClasspath + swiftExportEmbeddableJar).forEach { jar ->
-                jar.forEachClassFileInAJar { entry ->
-                    duplicates.getOrPut(entry.name, ::mutableListOf).add(jar)
-                }
-            }
-            val duplicateClassfiles = duplicates.filter { it.value.size > 1 }
-            if (duplicateClassfiles.isNotEmpty()) {
-                error(
-                    """
-                        |Duplicates in runtime classpath:
-                        |${duplicateClassfiles.map { "${it.key}: ${it.value}" }.joinToString("\n")}
-                    """.trimMargin()
-                )
-            }
-        }
-    }
-
+    val validateNoDuplicatesInRuntimeClasspath = validateEmbeddedJarRuntimeClasspathHasNoDuplicates(
+        swiftExportEmbeddableJarTask,
+        files(runtimeClasspathCopy),
+    )
     validateSwiftExportEmbeddable.configure { dependsOn(validateNoDuplicatesInRuntimeClasspath) }
 }
 
@@ -175,6 +96,6 @@ sourceSets {
     "test" {}
 }
 
-validateSwiftExportEmbeddable(runtimeJar(rewriteDefaultJarDepsToShadedCompiler()))
+registerSwiftExportEmbeddableValidationTasks(runtimeJar(rewriteDefaultJarDepsToShadedCompiler()))
 sourcesJar { includeEmptyDirs = false; eachFile { exclude() } } // empty Jar, no public sources
 javadocJar { includeEmptyDirs = false; eachFile { exclude() } } // empty Jar, no public javadocs
