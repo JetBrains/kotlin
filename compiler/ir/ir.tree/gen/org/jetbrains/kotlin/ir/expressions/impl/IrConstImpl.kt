64e66ba466c72c33ae039f9f8ca4d284c33a974d
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-07-15 13:51:12 +0200

    [IR] Autogenerate IrConstImpl
    #KT-65773 Fixed

--- compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/expressions/impl/IrConstImpl.kt
+++ compiler/ir/ir.tree/gen/org/jetbrains/kotlin/ir/expressions/impl/IrConstImpl.kt
@@ -3,21 +3,21 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+// This file was generated automatically. See compiler/ir/ir.tree/tree-generator/ReadMe.md.
+// DO NOT MODIFY IT MANUALLY.
+
+@file:Suppress("DuplicatedCode")
+
 package org.jetbrains.kotlin.ir.expressions.impl
 
-import org.jetbrains.kotlin.builtins.PrimitiveType
 import org.jetbrains.kotlin.ir.declarations.IrAttributeContainer
 import org.jetbrains.kotlin.ir.expressions.IrConst
 import org.jetbrains.kotlin.ir.expressions.IrConstKind
 import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.types.getPrimitiveType
-import org.jetbrains.kotlin.ir.types.isMarkedNullable
-import org.jetbrains.kotlin.ir.types.makeNullable
 import org.jetbrains.kotlin.ir.util.IrElementConstructorIndicator
 
 class IrConstImpl<T> internal constructor(
-    @Suppress("UNUSED_PARAMETER")
-    constructorIndicator: IrElementConstructorIndicator?,
+    @Suppress("UNUSED_PARAMETER") constructorIndicator: IrElementConstructorIndicator?,
     override val startOffset: Int,
     override val endOffset: Int,
     override var type: IrType,
@@ -25,6 +25,7 @@ class IrConstImpl<T> internal constructor(
     override var value: T,
 ) : IrConst<T>() {
     override var attributeOwnerId: IrAttributeContainer = this
+
     override var originalBeforeInline: IrAttributeContainer? = null
 
     companion object {
