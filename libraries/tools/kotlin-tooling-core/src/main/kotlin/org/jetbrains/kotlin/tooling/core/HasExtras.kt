78be32c41a355c0e2a687842fb53e8bc806628f5
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-07-16 22:48:45 +0200

    [Gradle] Document Has(Mutable)Extras interfaces
    ^KT-58858 In Progress

--- libraries/tools/kotlin-tooling-core/src/main/kotlin/org/jetbrains/kotlin/tooling/core/HasExtras.kt
+++ libraries/tools/kotlin-tooling-core/src/main/kotlin/org/jetbrains/kotlin/tooling/core/HasExtras.kt
@@ -6,10 +6,24 @@
 package org.jetbrains.kotlin.tooling.core
 
 
+/**
+ * Represents a Kotlin DSL entity providing some extra immutable metadata for tooling.
+ */
 interface HasExtras {
+
+    /**
+     * Immutable metadata for tooling.
+     */
     val extras: Extras
 }
 
+/**
+ * Represents a Kotlin DSL entity providing some extra mutable metadata for tooling.
+ */
 interface HasMutableExtras : HasExtras {
+
+    /**
+     * Mutable metadata for tooling.
+     */
     override val extras: MutableExtras
 }
