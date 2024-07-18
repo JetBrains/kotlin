9eb076940eca1957cd0769fc73fc9a959a1f87e5
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-07-18 13:37:46 +0200

    Fix JS CLI help command and add node_modules and .gradle to exclusions of the new wasm.debug.browser module

--- repo/codebase-tests/tests/org/jetbrains/kotlin/code/CodeConformanceTest.kt
+++ repo/codebase-tests/tests/org/jetbrains/kotlin/code/CodeConformanceTest.kt
@@ -147,6 +147,8 @@ class CodeConformanceTest : TestCase() {
                 "repo/gradle-build-conventions/generators/build/generated-sources",
                 "repo/gradle-build-conventions/compiler-tests-convention/build/generated-sources",
                 "repo/gradle-build-conventions/android-sdk-provisioner/build/generated-sources",
+                "wasm/wasm.debug.browsers/node_modules",
+                "wasm/wasm.debug.browsers/.gradle",
                 ".gradle/expanded",
             )
         )
