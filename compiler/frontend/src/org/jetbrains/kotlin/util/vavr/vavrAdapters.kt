a893ba6468e4816ecc93bc55ba8921087467c8cf
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-07-22 12:34:15 +0200

    Drop io.vavr.control.Option.getOrNull which now exists as a member

--- compiler/frontend/src/org/jetbrains/kotlin/util/vavr/vavrAdapters.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/util/vavr/vavrAdapters.kt
@@ -6,7 +6,6 @@
 package org.jetbrains.kotlin.util.vavr
 
 import io.vavr.Tuple2
-import io.vavr.control.Option
 
 typealias ImmutableMap<K, V> = io.vavr.collection.Map<K, V>
 typealias ImmutableHashMap<K, V> = io.vavr.collection.HashMap<K, V>
@@ -17,5 +16,4 @@ typealias ImmutableLinkedHashSet<E> = io.vavr.collection.LinkedHashSet<E>
 operator fun <T> Tuple2<T, *>.component1(): T = _1()
 operator fun <T> Tuple2<*, T>.component2(): T = _2()
 
-fun <T> Option<T>.getOrNull(): T? = getOrElse(null as T?)
 fun <K, V> ImmutableMap<K, V>.getOrNull(k: K): V? = get(k)?.getOrElse(null as V?)
--- compiler/resolution.common.jvm/src/org/jetbrains/kotlin/util/vavr/vavrAdapters.kt
+++ compiler/resolution.common.jvm/src/org/jetbrains/kotlin/util/vavr/vavrAdapters.kt
@@ -6,7 +6,6 @@
 package org.jetbrains.kotlin.util.vavr
 
 import io.vavr.Tuple2
-import io.vavr.control.Option
 
 internal typealias ImmutableMap<K, V> = io.vavr.collection.Map<K, V>
 internal typealias ImmutableHashMap<K, V> = io.vavr.collection.HashMap<K, V>
@@ -17,5 +16,4 @@ internal typealias ImmutableLinkedHashSet<E> = io.vavr.collection.LinkedHashSet<
 internal operator fun <T> Tuple2<T, *>.component1(): T = _1()
 internal operator fun <T> Tuple2<*, T>.component2(): T = _2()
 
-internal fun <T> Option<T>.getOrNull(): T? = getOrElse(null as T?)
 internal fun <K, V> ImmutableMap<K, V>.getOrNull(k: K): V? = get(k)?.getOrElse(null as V?)
