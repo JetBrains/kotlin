8a64ec67752eab31800d7f39c426806a4988914b
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-07-24 12:19:18 +0200

    [JS] Add tests for reproducing KT-69928

--- js/js.translator/testData/typescript-export/js/collections/collections__main.ts
+++ js/js.translator/testData/typescript-export/js/collections/collections__main.ts
@@ -129,6 +129,12 @@ function testImmutableSet() {
     const setReadonlyView = set.asJsReadonlySetView();
 
     assert(setReadonlyView.has(1), "Problem with accessing element of readonly view")
+    assert(joinIterator(setReadonlyView.keys()) == "123", "Unexpected keys() in set readonly view")
+    assert(joinIterator(setReadonlyView.values()) == "123", "Unexpected values() in set readonly view")
+    assert(joinIterator(setReadonlyView.entries()) == "1,12,23,3", "Unexpected entries() in set readonly view")
+    assert(Array.from(setReadonlyView.keys()).toString() == "", "Unexpected array created from keys() in set readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(setReadonlyView.values()).toString() == "", "Unexpected array created from values() in set readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(setReadonlyView.entries()).toString() == "", "Unexpected array created from entries() in set readonly view") // FIXME(KT-69928): The array should not be empty
     assert(joinSetOrMap(setReadonlyView) == "123", "Problem with readonly view iterator")
     assert(consumeSet(set), "Problem with consumption of a Kotlin set")
     assertThrow(() => { (setReadonlyView as Set<number>).add(4) }, "Set readonly view have ability to mutate the set by 'add'")
@@ -143,6 +149,12 @@ function testMutableSet() {
     const mutableSetReadonlyView = mutableSet.asJsReadonlySetView()
 
     assert(mutableSetReadonlyView.has(4), "Problem with accessing element of mutable set readonly view")
+    assert(joinIterator(mutableSetReadonlyView.keys()) == "456", "Unexpected keys() in mutable set readonly view")
+    assert(joinIterator(mutableSetReadonlyView.values()) == "456", "Unexpected values() in mutable set readonly view")
+    assert(joinIterator(mutableSetReadonlyView.entries()) == "4,45,56,6", "Unexpected entries() in mutable set readonly view")
+    assert(Array.from(mutableSetReadonlyView.keys()).toString() == "", "Unexpected array created from keys() in mutable set readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableSetReadonlyView.values()).toString() == "", "Unexpected array created from values() in mutable set readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableSetReadonlyView.entries()).toString() == "", "Unexpected array created from entries() in mutable set readonly view") // FIXME(KT-69928): The array should not be empty
     assert(joinSetOrMap(mutableSetReadonlyView) == "456", "Problem with mutable set readonly view iterator")
     assert(!consumeSet(mutableSet), "Problem with consumption of a Kotlin mutable set as a set")
     assert(consumeMutableSet(mutableSet), "Problem with consumption of a Kotlin mutable set as a mutable set")
@@ -156,6 +168,12 @@ function testMutableSet() {
     mutableSetView.delete(7)
 
     assert(mutableSetView.has(4), "Problem with accessing element of mutable set view")
+    assert(joinIterator(mutableSetView.keys()) == "456", "Unexpected keys() in mutable set view")
+    assert(joinIterator(mutableSetView.values()) == "456", "Unexpected values() in mutable set view")
+    assert(joinIterator(mutableSetView.entries()) == "4,45,56,6", "Unexpected entries() in mutable set view")
+    assert(Array.from(mutableSetView.keys()).toString() == "", "Unexpected array created from keys() in mutable set view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableSetView.values()).toString() == "", "Unexpected array created from values() in mutable set view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableSetView.entries()).toString() == "", "Unexpected array created from entries() in mutable set view") // FIXME(KT-69928): The array should not be empty
     assert(joinSetOrMap(mutableSetView) == "456", "Problem with mutable set view")
     assert(consumeMutableSet(mutableSet), "Problem with consumption of a Kotlin mutable set as a mutable set")
     assert(joinSetOrMap(mutableSetView) == "4567", "Problem with mutable set view after original set is mutated")
@@ -178,6 +196,12 @@ function testImmutableMap() {
 
     assert(mapReadonlyView.has("a"), "Problem with accessing element in map readonly view")
     assert(mapReadonlyView.get("a") == 1, "Problem with accessing element in map readonly view")
+    assert(joinIterator(mapReadonlyView.keys()) == "123", "Unexpected keys() in map readonly view") // FIXME(KT-69928): This should be "abc"
+    assert(joinIterator(mapReadonlyView.values()) == "123", "Unexpected values() in map readonly view")
+    assert(joinIterator(mapReadonlyView.entries()) == "a,1b,2c,3", "Unexpected entries() in map readonly view")
+    assert(Array.from(mapReadonlyView.keys()).toString() == "", "Unexpected array created from keys() in map readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mapReadonlyView.values()).toString() == "", "Unexpected array created from values() in map readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mapReadonlyView.entries()).toString() == "", "Unexpected array created from entries() in map readonly view") // FIXME(KT-69928): The array should not be empty
     assert(joinSetOrMap(mapReadonlyView) == "[a:1][b:2][c:3]", "Problem with map readonly view iterator")
     assert(consumeMap(map), "Problem with consumption of a Kotlin map")
     assertThrow(() => { (mapReadonlyView as Map<string, number>).set("d", 4) }, "Map readonly view have ability to mutate the map by 'set'")
@@ -193,6 +217,12 @@ function testMutableMap() {
 
     assert(mutableMapReadonlyView.has("d"), "Problem with accessing element in mutable map readonly view")
     assert(mutableMapReadonlyView.get("d") == 4, "Problem with accessing element in mutable map readonly view")
+    assert(joinIterator(mutableMapReadonlyView.keys()) == "456", "Unexpected keys() in mutable map readonly view") // FIXME(KT-69928): This should be "def"
+    assert(joinIterator(mutableMapReadonlyView.values()) == "456", "Unexpected values() in mutable map readonly view")
+    assert(joinIterator(mutableMapReadonlyView.entries()) == "d,4e,5f,6", "Unexpected entries() in mutable map readonly view")
+    assert(Array.from(mutableMapReadonlyView.keys()).toString() == "", "Unexpected array created from keys() in mutable map readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableMapReadonlyView.values()).toString() == "", "Unexpected array created from values() in mutable map readonly view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableMapReadonlyView.entries()).toString() == "", "Unexpected array created from entries() in mutable map readonly view") // FIXME(KT-69928): The array should not be empty
     assert(joinSetOrMap(mutableMapReadonlyView) == "[d:4][e:5][f:6]", "Problem with mutable map readonly view")
     assert(!consumeMap(mutableMap), "Problem with consumption of a Kotlin mutable map as a map")
     assert(consumeMutableMap(mutableMap), "Problem with consumption of a Kotlin mutable map as a mutable map")
@@ -207,6 +237,12 @@ function testMutableMap() {
 
     assert(mutableMapView.has("d"), "Problem with accessing element in mutable map view")
     assert(mutableMapView.get("d") == 4, "Problem with accessing element in mutable map view")
+    assert(joinIterator(mutableMapView.keys()) == "456", "Unexpected keys() in mutable map view") // FIXME(KT-69928): This should be "abc"
+    assert(joinIterator(mutableMapView.values()) == "456", "Unexpected values() in mutable map view")
+    assert(joinIterator(mutableMapView.entries()) == "d,4e,5f,6", "Unexpected entries() in mutable map view")
+    assert(Array.from(mutableMapView.keys()).toString() == "", "Unexpected array created from keys() in mutable map view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableMapView.values()).toString() == "", "Unexpected array created from values() in mutable map view") // FIXME(KT-69928): The array should not be empty
+    assert(Array.from(mutableMapView.entries()).toString() == "", "Unexpected array created from entries() in mutable map view") // FIXME(KT-69928): The array should not be empty
     assert(joinSetOrMap(mutableMapView) == "[d:4][e:5][f:6]", "Problem with mutable map view")
     assert(consumeMutableMap(mutableMap), "Problem with consumption of a Kotlin mutable map as a mutable map")
     assert(joinSetOrMap(mutableMapView) == "[d:4][e:5][f:6][g:7]", "Problem with mutable map view after original map is mutated")
@@ -236,5 +272,15 @@ function joinSetOrMap(setOrMap: ReadonlySet<number> | ReadonlyMap<string, number
         });
     }
 
+    return result
+}
+
+function joinIterator<T>(iterator: Iterator<T>): string {
+    let result = ""
+    let value = iterator.next()
+    while (!value.done) {
+        result += value.value
+        value = iterator.next()
+    }
     return result
 }
