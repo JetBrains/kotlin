JetBrains/kotlin

commit 4166b60c159facc7b69888c94c0afbe28f77fa85

[K/N] Remove redundant fields from SingleObjectPage (KT-58866)
Merge-request: KT-MR-16979
Merged-by: Alexey Glushko <aleksei.glushko@jetbrains.com>

kotlin-native/runtime/src/alloc/custom/cpp/SingleObjectPage.cpp
@@ -8,6 +8,7 @@
 #include <atomic>
 #include <cstdint>
 
+#include "AllocatorImpl.hpp"
 #include "CustomLogging.hpp"
 #include "GCApi.hpp"
 #include "NextFitPage.hpp"
@@ -17,39 +18,46 @@ namespace kotlin::alloc {
 SingleObjectPage* SingleObjectPage::Create(uint64_t cellCount) noexcept {
     CustomAllocInfo("SingleObjectPage::Create(%" PRIu64 ")", cellCount);
     RuntimeAssert(cellCount > NextFitPage::maxBlockSize(), "blockSize too small for SingleObjectPage");
-    uint64_t size = sizeof(SingleObjectPage) + cellCount * sizeof(uint64_t);
-    return new (SafeAlloc(size)) SingleObjectPage(size);
+    auto objectSize = AllocationSize::cells(cellCount);
+    return new (SafeAlloc(pageSize(objectSize).inBytes())) SingleObjectPage(objectSize);
 }
 
-SingleObjectPage::SingleObjectPage(size_t size) noexcept : size_(size) {}
+SingleObjectPage::SingleObjectPage(AllocationSize objectSize) noexcept {
+    auto& heap = mm::GlobalData::Instance().allocator().impl().heap();
+    heap.allocatedSizeTracker().recordDifference(static_cast<ptrdiff_t>(objectSize.inBytes()), false);
+}
 
 void SingleObjectPage::Destroy() noexcept {
-    Free(this, size_);
+    auto* object = reinterpret_cast<kotlin::alloc::HeapObjHeader*>(data_)->object();
+    auto objectSize = AllocationSize::bytesAtLeast(CustomAllocator::GetAllocatedHeapSize(object));
+
+    auto& heap = mm::GlobalData::Instance().allocator().impl().heap();
+    heap.allocatedSizeTracker().recordDifference(-static_cast<ptrdiff_t>(objectSize.inBytes()), false);
+
+    Free(this, pageSize(objectSize).inBytes());
 }
 
 uint8_t* SingleObjectPage::Data() noexcept {
     return data_;
 }
 
-uint8_t* SingleObjectPage::TryAllocate() noexcept {
-    if (isAllocated_) return nullptr;
-
-    allocatedSizeTracker_.onPageOverflow(size_);
-
-    isAllocated_ = true;
+uint8_t* SingleObjectPage::Allocate() noexcept {
     return Data();
 }
 
-bool SingleObjectPage::Sweep(GCSweepScope& sweepHandle, FinalizerQueue& finalizerQueue) noexcept {
-    CustomAllocDebug("SingleObjectPage@%p::Sweep()", this);
+bool SingleObjectPage::SweepAndDestroy(GCSweepScope& sweepHandle, FinalizerQueue& finalizerQueue) noexcept {
+    CustomAllocDebug("SingleObjectPage@%p::SweepAndDestroy()", this);
     if (SweepObject(Data(), finalizerQueue, sweepHandle)) {
         return true;
     }
 
-    allocatedSizeTracker_.afterSweep(0);
+    Destroy();
 
-    isAllocated_ = false;
     return false;
+}
+
+AllocationSize SingleObjectPage::pageSize(AllocationSize objectSize) noexcept {
+    return objectSize + AllocationSize::bytesAtLeast(sizeof(SingleObjectPage));
 }
 
 std::vector<uint8_t*> SingleObjectPage::GetAllocatedBlocks() noexcept {

kotlin-native/runtime/src/alloc/custom/cpp/SingleObjectPageTest.cpp
@@ -26,7 +26,7 @@ void mark(void* obj) {
 
 SingleObjectPage* alloc(uint64_t blockSize) {
     SingleObjectPage* page = SingleObjectPage::Create(blockSize);
-    uint8_t* ptr = page->TryAllocate();
+    uint8_t* ptr = page->Allocate();
     EXPECT_TRUE(ptr[0] == 0 && memcmp(ptr, ptr + 1, blockSize * 8 - 1) == 0);
     reinterpret_cast<uint64_t*>(ptr)[1] = reinterpret_cast<uint64_t>(&fakeType);
     return page;
@@ -38,8 +38,7 @@ TEST(CustomAllocTest, SingleObjectPageSweepEmptyPage) {
     auto gcHandle = kotlin::gc::GCHandle::createFakeForTests();
     auto gcScope = gcHandle.sweep();
     kotlin::alloc::FinalizerQueue finalizerQueue;
-    EXPECT_FALSE(page->Sweep(gcScope, finalizerQueue));
-    page->Destroy();
+    EXPECT_FALSE(page->SweepAndDestroy(gcScope, finalizerQueue));
 }
 
 TEST(CustomAllocTest, SingleObjectPageSweepFullPage) {
@@ -50,8 +49,7 @@ TEST(CustomAllocTest, SingleObjectPageSweepFullPage) {
     auto gcHandle = kotlin::gc::GCHandle::createFakeForTests();
     auto gcScope = gcHandle.sweep();
     kotlin::alloc::FinalizerQueue finalizerQueue;
-    EXPECT_TRUE(page->Sweep(gcScope, finalizerQueue));
-    page->Destroy();
+    EXPECT_TRUE(page->SweepAndDestroy(gcScope, finalizerQueue));
 }
 
 #undef MIN_BLOCK_SIZE

kotlin-native/runtime/src/alloc/custom/cpp/AnyPage.hpp
@@ -29,6 +29,14 @@ protected:
     // Intentionally non-virtual. `AnyPage` should not be used in any context other than base class clause.
     // Please use concrete implementations instead.
     ~AnyPage() = default;
+};
+
+template<typename Page>
+class alignas(kPageAlignment) MultiObjectPage : public AnyPage<Page> {
+protected:
+    // Intentionally non-virtual. `MultiObjectPage` should not be used in any context other than base class clause.
+    // Please use concrete implementations instead.
+    ~MultiObjectPage() = default;
 
     AllocatedSizeTracker::Page allocatedSizeTracker_{};
 };

kotlin-native/runtime/src/alloc/custom/cpp/CustomAllocator.cpp
@@ -125,7 +125,7 @@ uint8_t* CustomAllocator::Allocate(AllocationSize size) noexcept {
 
 uint8_t* CustomAllocator::AllocateInSingleObjectPage(uint64_t cellCount) noexcept {
     CustomAllocDebug("CustomAllocator::AllocateInSingleObjectPage(%" PRIu64 ")", cellCount);
-    uint8_t* block = heap_.GetSingleObjectPage(cellCount, finalizerQueue_)->TryAllocate();
+    uint8_t* block = heap_.GetSingleObjectPage(cellCount, finalizerQueue_)->Allocate();
     return block;
 }
 

kotlin-native/runtime/src/alloc/custom/cpp/CustomAllocatorTest.cpp
@@ -53,25 +53,29 @@ TEST(CustomAllocTest, SmallAllocSameFixedBlockPage) {
 }
 
 TEST(CustomAllocTest, FixedBlockPageThreshold) {
+    std::list<TypeInfo> types;
     Heap heap;
     CustomAllocator ca(heap);
     const int FROM = FixedBlockPage::MAX_BLOCK_SIZE - 10;
     const int TO = FixedBlockPage::MAX_BLOCK_SIZE + 10;
     for (int blocks = FROM; blocks <= TO; ++blocks) {
-        TypeInfo fakeType = {.typeInfo_ = &fakeType, .instanceSize_ = 8 * blocks, .flags_ = 0};
-        ca.CreateObject(&fakeType);
+        auto& type = types.emplace_back();
+        type = {.typeInfo_ = &type, .instanceSize_ = 8 * blocks, .flags_ = 0};
+        ca.CreateObject(&type);
     }
 }
 
 TEST(CustomAllocTest, NextFitPageThreshold) {
+    std::list<TypeInfo> types;
     Heap heap;
     CustomAllocator ca(heap);
     const int MAX_BLOCK_SIZE = NextFitPage::maxBlockSize();
     const int FROM = MAX_BLOCK_SIZE - 10;
     const int TO = MAX_BLOCK_SIZE + 10;
     for (int blocks = FROM; blocks <= TO; ++blocks) {
-        TypeInfo fakeType = {.typeInfo_ = &fakeType, .instanceSize_ = 8 * blocks, .flags_ = 0};
-        ca.CreateObject(&fakeType);
+        auto& type = types.emplace_back();
+        type = {.typeInfo_ = &type, .instanceSize_ = 8 * blocks, .flags_ = 0};
+        ca.CreateObject(&type);
     }
 }
 

kotlin-native/runtime/src/alloc/custom/cpp/ExtraObjectPage.hpp
@@ -20,7 +20,7 @@
 
 namespace kotlin::alloc {
 
-class alignas(kPageAlignment) ExtraObjectPage : public AnyPage<ExtraObjectPage> {
+class alignas(kPageAlignment) ExtraObjectPage : public MultiObjectPage<ExtraObjectPage> {
 public:
     static inline constexpr const size_t SIZE = 64 * KiB;
 

kotlin-native/runtime/src/alloc/custom/cpp/FixedBlockPage.hpp
@@ -31,7 +31,7 @@ struct alignas(8) FixedBlockCell {
     };
 };
 
-class alignas(kPageAlignment) FixedBlockPage : public AnyPage<FixedBlockPage> {
+class alignas(kPageAlignment) FixedBlockPage : public MultiObjectPage<FixedBlockPage> {
 public:
     static inline constexpr const size_t SIZE = 256 * KiB;
 

kotlin-native/runtime/src/alloc/custom/cpp/Heap.cpp
@@ -42,7 +42,7 @@ FinalizerQueue Heap::Sweep(gc::GCHandle gcHandle) noexcept {
             fixedBlockPages_[blockSize].Sweep(sweepHandle, finalizerQueue);
         }
         nextFitPages_.Sweep(sweepHandle, finalizerQueue);
-        singleObjectPages_.SweepAndFree(sweepHandle, finalizerQueue);
+        singleObjectPages_.Sweep(sweepHandle, finalizerQueue);
     }
     CustomAllocDebug("Heap: before extra sweep FinalizerQueue size == %zu", finalizerQueue.size());
     {
@@ -117,11 +117,11 @@ std::vector<ObjHeader*> Heap::GetAllocatedObjects() noexcept {
 
 void Heap::ClearForTests() noexcept {
     for (int blockSize = 0; blockSize <= FixedBlockPage::MAX_BLOCK_SIZE; ++blockSize) {
-        fixedBlockPages_[blockSize].ClearForTests();
+        fixedBlockPages_[blockSize].Clear();
     }
-    nextFitPages_.ClearForTests();
-    singleObjectPages_.ClearForTests();
-    extraObjectPages_.ClearForTests();
+    nextFitPages_.Clear();
+    singleObjectPages_.Clear();
+    extraObjectPages_.Clear();
 }
 
 } // namespace kotlin::alloc

commit 0d1d8e02775ef48248a2e4b3b70595e670d44dbf

[K/N] Notify scheduler on SingleObjectPage allocation
    Fixes the regression introduced in 4166b60c159facc7b69888c94c0afbe28f77fa85.


Merge-request: KOTLIN-MR-1097
Merged-by: Aleksej Glushko <aleksei.glushko@jetbrains.com>

kotlin-native/runtime/src/alloc/custom/cpp/AllocatedSizeTracker.cpp
@@ -10,6 +10,10 @@
 
 using namespace kotlin;
 
+namespace {
+void (*schedulerNotificationTestHook)(std::size_t) = nullptr;
+}
+
 void alloc::AllocatedSizeTracker::Page::onPageOverflow(std::size_t allocatedBytes) noexcept {
     RuntimeAssert(allocatedBytes >= allocatedBytesLastRecorded_,
                   "A page can't overflow with less allocated bytes (%zu) than there were after the last sweep (%zu)",
@@ -17,20 +21,31 @@ void alloc::AllocatedSizeTracker::Page::onPageOverflow(std::size_t allocatedByte
     auto allocatedSinceLastSweep = allocatedBytes - allocatedBytesLastRecorded_;
     allocatedBytesLastRecorded_ = allocatedBytes;
     auto& heap = mm::GlobalData::Instance().allocator().impl().heap();
-    heap.allocatedSizeTracker().recordDifference(static_cast<std::ptrdiff_t>(allocatedSinceLastSweep), true);
+    heap.allocatedSizeTracker().recordDifferenceAndNotifyScheduler(static_cast<std::ptrdiff_t>(allocatedSinceLastSweep));
 }
 
 void alloc::AllocatedSizeTracker::Page::afterSweep(std::size_t allocatedBytes) noexcept {
     auto diffBytes = static_cast<std::ptrdiff_t>(allocatedBytes) - static_cast<std::ptrdiff_t>(allocatedBytesLastRecorded_);
     allocatedBytesLastRecorded_ = allocatedBytes;
     auto& heap = mm::GlobalData::Instance().allocator().impl().heap();
-    heap.allocatedSizeTracker().recordDifference(diffBytes, false);
+    heap.allocatedSizeTracker().recordDifference(diffBytes);
 }
 
-void alloc::AllocatedSizeTracker::Heap::recordDifference(std::ptrdiff_t diffBytes, bool notifyScheduler) noexcept {
+std::size_t alloc::AllocatedSizeTracker::Heap::recordDifference(std::ptrdiff_t diffBytes) noexcept {
     auto prevRecord = allocatedBytes_.fetch_add(diffBytes, std::memory_order_relaxed);
     RuntimeAssert(diffBytes >= 0 || prevRecord >= -diffBytes, "Negative overflow: %td+(%td) must be >= 0", prevRecord, diffBytes);
-    if (notifyScheduler) {
-        OnMemoryAllocation(prevRecord + diffBytes);
+    return prevRecord + diffBytes;
+}
+
+void alloc::AllocatedSizeTracker::Heap::recordDifferenceAndNotifyScheduler(std::ptrdiff_t diffBytes) noexcept {
+    auto nowAllocated = recordDifference(diffBytes);
+
+    if (schedulerNotificationTestHook) {
+        schedulerNotificationTestHook(nowAllocated);
     }
+    OnMemoryAllocation(nowAllocated);
+}
+
+void alloc::test_support::setSchedulerNotificationHook(void (*hook)(std::size_t)) noexcept {
+    schedulerNotificationTestHook = hook;
 }

kotlin-native/runtime/src/alloc/custom/cpp/AllocatedSizeTracker.hpp
@@ -21,10 +21,16 @@ struct AllocatedSizeTracker {
 
     class Heap {
     public:
-        void recordDifference(std::ptrdiff_t diffBytes, bool notifyScheduler) noexcept;
+        /** Returns the tracker value after the update. */
+        std::size_t recordDifference(std::ptrdiff_t diffBytes) noexcept;
+        void recordDifferenceAndNotifyScheduler(std::ptrdiff_t diffBytes) noexcept;
     private:
         std::atomic<std::ptrdiff_t> allocatedBytes_ = 0;
     };
 };
+
+namespace test_support {
+void setSchedulerNotificationHook(void (*hook)(std::size_t)) noexcept;
+}
 
 }

kotlin-native/runtime/src/alloc/custom/cpp/CustomAllocatorTestSupport.cpp
@@ -1,0 +1,26 @@
+/*
+ * Copyright 2022-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+#include "CustomAllocatorTestSupport.hpp"
+
+namespace {
+
+testing::StrictMock<testing::MockFunction<void(std::size_t)>>* mock = nullptr;
+
+void hookImpl(std::size_t allocatedBytes) {
+    mock->Call(allocatedBytes);
+}
+
+}
+
+kotlin::alloc::test_support::WithSchedulerNotificationHook::WithSchedulerNotificationHook() {
+    mock = &schedulerNotificationHook_;
+    setSchedulerNotificationHook(hookImpl);
+}
+
+kotlin::alloc::test_support::WithSchedulerNotificationHook::~WithSchedulerNotificationHook() {
+    setSchedulerNotificationHook(nullptr);
+    mock = nullptr;
+}

kotlin-native/runtime/src/alloc/custom/cpp/CustomAllocatorTestSupport.hpp
@@ -1,0 +1,24 @@
+/*
+ * Copyright 2022-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+#pragma once
+
+#include "gmock/gmock.h"
+
+#include "AllocatedSizeTracker.hpp"
+
+namespace kotlin::alloc::test_support {
+
+class WithSchedulerNotificationHook {
+public:
+    WithSchedulerNotificationHook();
+    ~WithSchedulerNotificationHook();
+
+    auto& hook() { return schedulerNotificationHook_; }
+private:
+    testing::StrictMock<testing::MockFunction<void(std::size_t)>> schedulerNotificationHook_;
+};
+
+}

kotlin-native/runtime/src/alloc/custom/cpp/SingleObjectPage.cpp
@@ -24,7 +24,7 @@ SingleObjectPage* SingleObjectPage::Create(uint64_t cellCount) noexcept {
 
 SingleObjectPage::SingleObjectPage(AllocationSize objectSize) noexcept {
     auto& heap = mm::GlobalData::Instance().allocator().impl().heap();
-    heap.allocatedSizeTracker().recordDifference(static_cast<ptrdiff_t>(objectSize.inBytes()), false);
+    heap.allocatedSizeTracker().recordDifferenceAndNotifyScheduler(static_cast<ptrdiff_t>(objectSize.inBytes()));
 }
 
 void SingleObjectPage::Destroy() noexcept {
@@ -32,7 +32,7 @@ void SingleObjectPage::Destroy() noexcept {
     auto objectSize = AllocationSize::bytesAtLeast(CustomAllocator::GetAllocatedHeapSize(object));
 
     auto& heap = mm::GlobalData::Instance().allocator().impl().heap();
-    heap.allocatedSizeTracker().recordDifference(-static_cast<ptrdiff_t>(objectSize.inBytes()), false);
+    heap.allocatedSizeTracker().recordDifference(-static_cast<ptrdiff_t>(objectSize.inBytes()));
 
     Free(this, pageSize(objectSize).inBytes());
 }

kotlin-native/runtime/src/alloc/custom/cpp/FixedBlockPageTest.cpp
@@ -9,11 +9,15 @@
 #include <unordered_set>
 #include <vector>
 
+#include "CustomAllocatorTestSupport.hpp"
+#include "gtest/gtest.h"
+
 #include "Cell.hpp"
 #include "ExtraObjectPage.hpp"
-#include "gtest/gtest.h"
 #include "FixedBlockPage.hpp"
 #include "TypeInfo.h"
+
+using testing::_;
 
 namespace {
 
@@ -163,6 +167,19 @@ TEST(CustomAllocTest, FixedBlockPageRandomExercise) {
         EXPECT_EQ(live.size(), BLOCK_COUNT);
         EXPECT_FALSE(page->Sweep(gcScope, finalizerQueue));
         page->Destroy();
+    }
+}
+
+TEST(CustomAllocTest, FixedBlockPageSchedulerNotification) {
+    for (uint32_t size = 2; size <= FixedBlockPage::MAX_BLOCK_SIZE; ++size) {
+        kotlin::alloc::test_support::WithSchedulerNotificationHook hookHandle;
+        EXPECT_CALL(hookHandle.hook(), Call(_));
+
+        FixedBlockPage* page = FixedBlockPage::Create(size);
+        while (alloc(page, size)) {}
+        page->Destroy();
+
+        testing::Mock::VerifyAndClearExpectations(&hookHandle.hook());
     }
 }
 

kotlin-native/runtime/src/alloc/custom/cpp/NextFitPageTest.cpp
@@ -6,12 +6,16 @@
 #include <cstdint>
 #include <random>
 
+#include "CustomAllocatorTestSupport.hpp"
+#include "gtest/gtest.h"
+
 #include "Cell.hpp"
 #include "ExtraObjectPage.hpp"
 #include "FixedBlockPage.hpp"
-#include "gtest/gtest.h"
 #include "NextFitPage.hpp"
 #include "TypeInfo.h"
+
+using testing::_;
 
 namespace {
 
@@ -134,6 +138,18 @@ TEST(CustomAllocTest, NextFitPageSweepCoallesce) {
     EXPECT_FALSE(page->Sweep(gcScope, finalizerQueue));
     EXPECT_TRUE(alloc(page, (NextFitPage::cellCount()-1) - 1));
     page->Destroy();
+}
+
+
+TEST(CustomAllocTest, NextFitPageSchedulerNotification) {
+    kotlin::alloc::test_support::WithSchedulerNotificationHook hookHandle;
+    EXPECT_CALL(hookHandle.hook(), Call(_));
+
+    NextFitPage * page = NextFitPage ::Create(MIN_BLOCK_SIZE);
+    while (alloc(page, MIN_BLOCK_SIZE)) {}
+    page->Destroy();
+
+    testing::Mock::VerifyAndClearExpectations(&hookHandle.hook());
 }
 
 #undef MIN_BLOCK_SIZE

kotlin-native/runtime/src/alloc/custom/cpp/SingleObjectPageTest.cpp
@@ -6,11 +6,15 @@
 #include <cstdint>
 #include <random>
 
-#include "ExtraObjectPage.hpp"
+#include "CustomAllocatorTestSupport.hpp"
 #include "gtest/gtest.h"
+
+#include "ExtraObjectPage.hpp"
 #include "NextFitPage.hpp"
 #include "SingleObjectPage.hpp"
 #include "TypeInfo.h"
+
+using testing::_;
 
 namespace {
 
@@ -50,6 +54,13 @@ TEST(CustomAllocTest, SingleObjectPageSweepFullPage) {
     auto gcScope = gcHandle.sweep();
     kotlin::alloc::FinalizerQueue finalizerQueue;
     EXPECT_TRUE(page->SweepAndDestroy(gcScope, finalizerQueue));
+}
+
+TEST(CustomAllocTest, SingleObjectPageSchedulerNotification) {
+    kotlin::alloc::test_support::WithSchedulerNotificationHook hookHandle;
+    EXPECT_CALL(hookHandle.hook(), Call(_));
+    alloc(MIN_BLOCK_SIZE);
+    testing::Mock::VerifyAndClearExpectations(&hookHandle.hook());
 }
 
 #undef MIN_BLOCK_SIZE