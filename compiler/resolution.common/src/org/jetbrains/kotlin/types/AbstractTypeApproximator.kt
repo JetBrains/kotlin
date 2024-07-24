daf8723ca1db3f654c1c1e3c8a0838ae7d22abb5
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-07-24 18:14:38 +0200

    Minor. Turn local function into private one in AbstractTypeApproximator

--- compiler/resolution.common/src/org/jetbrains/kotlin/types/AbstractTypeApproximator.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/types/AbstractTypeApproximator.kt
@@ -360,41 +360,6 @@ abstract class AbstractTypeApproximator(
         toSuper: Boolean,
         depth: Int,
     ): KotlinTypeMarker? {
-        fun KotlinTypeMarker.replaceRecursionWithStarProjection(capturedType: CapturedTypeMarker): KotlinTypeMarker {
-            // This replacement is important for resolving the code like below in K2.
-            //     fun bar(y: FieldOrRef<*>) = y.field
-            //     interface FieldOrRef<FF : AbstractField<FF>> { val field: FF }
-            //     abstract class AbstractField<out F : AbstractField<F>>
-            // During resolving the value parameter y type, K1 also builds a type for a star projection *.
-            // See fun TypeParameterDescriptor.starProjectionType(): KotlinType and fun buildStarProjectionTypeByTypeParameters.
-            // Thanks to it, K1 builds the star projection type as AbstractField<*> and no other approximation is needed.
-            //
-            // In turn, K2 never makes such a thing (K2 star projection has no associated type).
-            // Instead, it resolves y.field as CapturedType(*) C (see usage one line below),
-            // and the constructor of this captured type has a star projection and a supertype of `AbstractField<C>`.
-            //
-            // Without this replacement, the type approximator currently cannot handle such a situation properly
-            // and builds AbstractField<AbstractField<AbstractField<Any?>>>.
-            // The check it == type here is intended to find a recursion inside a captured type.
-            // A similar replacement for baseSubType looks unnecessary, no hits in the tests.
-
-            fun TypeArgumentMarker.unwrapForComparison(): KotlinTypeMarker? {
-                if (this.isStarProjection()) return null
-                return getType().lowerBoundIfFlexible().originalIfDefinitelyNotNullable()
-            }
-
-            return if (isK2 && getArguments().any { it.unwrapForComparison() == capturedType }) {
-                replaceArguments {
-                    when {
-                        it.unwrapForComparison() != capturedType -> it
-                        // It's possible to use the stub here, because K2 star projection is an object and
-                        // in fact this parameter is never used
-                        else -> createStarProjection(TypeParameterMarkerStubForK2StarProjection)
-                    }
-                }
-            } else this
-        }
-
         val supertypes = capturedType.typeConstructor().supertypes()
         val baseSuperType = when (supertypes.size) {
             0 -> nullableAnyType() // Let C = in Int, then superType for C and C? is Any?
@@ -465,6 +430,41 @@ abstract class AbstractTypeApproximator(
                 else -> it
             }
         }
+    }
+
+    private fun KotlinTypeMarker.replaceRecursionWithStarProjection(capturedType: CapturedTypeMarker): KotlinTypeMarker {
+        // This replacement is important for resolving the code like below in K2.
+        //     fun bar(y: FieldOrRef<*>) = y.field
+        //     interface FieldOrRef<FF : AbstractField<FF>> { val field: FF }
+        //     abstract class AbstractField<out F : AbstractField<F>>
+        // During resolving the value parameter y type, K1 also builds a type for a star projection *.
+        // See fun TypeParameterDescriptor.starProjectionType(): KotlinType and fun buildStarProjectionTypeByTypeParameters.
+        // Thanks to it, K1 builds the star projection type as AbstractField<*> and no other approximation is needed.
+        //
+        // In turn, K2 never makes such a thing (K2 star projection has no associated type).
+        // Instead, it resolves y.field as CapturedType(*) C (see usage one line below),
+        // and the constructor of this captured type has a star projection and a supertype of `AbstractField<C>`.
+        //
+        // Without this replacement, the type approximator currently cannot handle such a situation properly
+        // and builds AbstractField<AbstractField<AbstractField<Any?>>>.
+        // The check it == type here is intended to find a recursion inside a captured type.
+        // A similar replacement for baseSubType looks unnecessary, no hits in the tests.
+
+        fun TypeArgumentMarker.unwrapForComparison(): KotlinTypeMarker? {
+            if (this.isStarProjection()) return null
+            return getType().lowerBoundIfFlexible().originalIfDefinitelyNotNullable()
+        }
+
+        return if (isK2 && getArguments().any { it.unwrapForComparison() == capturedType }) {
+            replaceArguments {
+                when {
+                    it.unwrapForComparison() != capturedType -> it
+                    // It's possible to use the stub here, because K2 star projection is an object and
+                    // in fact this parameter is never used
+                    else -> createStarProjection(TypeParameterMarkerStubForK2StarProjection)
+                }
+            }
+        } else this
     }
 
     private object TypeParameterMarkerStubForK2StarProjection : TypeParameterMarker
