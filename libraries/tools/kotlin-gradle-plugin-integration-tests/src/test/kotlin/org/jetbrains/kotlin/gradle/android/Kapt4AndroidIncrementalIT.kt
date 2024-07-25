9cf7b982dd3640479fee1f12b7aa13bd62f2bd9d
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2024-07-25 15:44:24 +0200

    Run K2 Kapt with IC support
     #KT-69860

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/Kapt4AndroidIncrementalIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/Kapt4AndroidIncrementalIT.kt
@@ -7,11 +7,9 @@ package org.jetbrains.kotlin.gradle.android
 
 import org.jetbrains.kotlin.gradle.forceKapt4
 import org.jetbrains.kotlin.gradle.testbase.TestProject
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("android with kapt4 incremental build tests")
-@Disabled("KT-63102 Incremental compilation doesn't work in 2.0")
 class Kapt4AndroidIncrementalIT : Kapt3AndroidIncrementalIT() {
     override val defaultBuildOptions = super.defaultBuildOptions.copyEnsuringK2()
 
@@ -21,7 +19,6 @@ class Kapt4AndroidIncrementalIT : Kapt3AndroidIncrementalIT() {
 }
 
 @DisplayName("android with kapt4 incremental build tests with precise compilation outputs backup")
-@Disabled("KT-63102 Incremental compilation doesn't work in 2.0")
 class Kapt4AndroidIncrementalWithoutPreciseBackupIT : Kapt3AndroidIncrementalWithoutPreciseBackupIT() {
     override val defaultBuildOptions = super.defaultBuildOptions.copyEnsuringK2()
 
--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/KaptGenerateStubsTask.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/KaptGenerateStubsTask.kt
@@ -158,7 +158,4 @@ abstract class KaptGenerateStubsTask @Inject constructor(
             args.freeArgs += (scriptSources.asFileTree.files + javaSources.files + sources.asFileTree.files).map { it.absolutePath }
         }
     }
-
-    override fun isIncrementalCompilationEnabled(): Boolean =
-        super.isIncrementalCompilationEnabled() && !useK2Kapt.get() && ("-Xuse-k2-kapt" !in compilerOptions.freeCompilerArgs.get())
 }
