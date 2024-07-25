8ac0dcef950da8a257481666ec1e6a357bed6518
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-07-25 14:25:46 +0300

    Convert `FqName` and `FqNameUnsafe` to Kotlin files.

--- core/compiler.common/src/org/jetbrains/kotlin/name/FqName.kt
+++ core/compiler.common/src/org/jetbrains/kotlin/name/FqName.kt
@@ -1,135 +1,105 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.name
 
-package org.jetbrains.kotlin.name;
-
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.utils.StringsKt;
-
-import java.util.List;
-
-public final class FqName {
-
-    @NotNull
-    public static FqName fromSegments(@NotNull List<String> names) {
-        return new FqName(StringsKt.join(names, "."));
-    }
-
-    public static final FqName ROOT = new FqName("");
-
-    @NotNull
-    private final FqNameUnsafe fqName;
+class FqName {
+    private val fqName: FqNameUnsafe
 
     // cache
-    private transient FqName parent;
+    @Transient
+    private var parent: FqName? = null
 
-    public FqName(@NotNull String fqName) {
-        this.fqName = new FqNameUnsafe(fqName, this);
+    constructor(fqName: String) {
+        this.fqName = FqNameUnsafe(fqName, this)
     }
 
-    public FqName(@NotNull FqNameUnsafe fqName) {
-        this.fqName = fqName;
+    constructor(fqName: FqNameUnsafe) {
+        this.fqName = fqName
     }
 
-    private FqName(@NotNull FqNameUnsafe fqName, FqName parent) {
-        this.fqName = fqName;
-        this.parent = parent;
+    private constructor(fqName: FqNameUnsafe, parent: FqName) {
+        this.fqName = fqName
+        this.parent = parent
     }
 
-    @NotNull
-    public String asString() {
-        return fqName.asString();
+    fun asString(): String {
+        return fqName.asString()
     }
 
-    @NotNull
-    public FqNameUnsafe toUnsafe() {
-        return fqName;
+    fun toUnsafe(): FqNameUnsafe {
+        return fqName
     }
 
-    public boolean isRoot() {
-        return fqName.isRoot();
-    }
+    val isRoot: Boolean
+        get() = fqName.isRoot
 
-    @NotNull
-    public FqName parent() {
-        if (parent != null) {
-            return parent;
+    fun parent(): FqName {
+        parent?.let {
+            return it
         }
 
-        if (isRoot()) {
-            throw new IllegalStateException("root");
+        check(!isRoot) { "root" }
+
+        return FqName(fqName.parent()).also {
+            parent = it
+        }
+    }
+
+    fun child(name: Name): FqName {
+        return FqName(fqName.child(name), this)
+    }
+
+    fun shortName(): Name {
+        return fqName.shortName()
+    }
+
+    fun shortNameOrSpecial(): Name {
+        return fqName.shortNameOrSpecial()
+    }
+
+    fun pathSegments(): List<Name> {
+        return fqName.pathSegments()
+    }
+
+    fun startsWith(segment: Name): Boolean {
+        return fqName.startsWith(segment)
+    }
+
+    fun startsWith(other: FqName): Boolean {
+        return fqName.startsWith(other.fqName)
+    }
+
+    override fun toString(): String {
+        return fqName.toString()
+    }
+
+    override fun equals(o: Any?): Boolean {
+        if (this === o) return true
+        if (o !is FqName) return false
+
+        if (fqName != o.fqName) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return fqName.hashCode()
+    }
+
+    companion object {
+        @JvmStatic
+        fun fromSegments(names: List<String?>): FqName {
+            return FqName(names.joinToString("."))
         }
 
-        parent = new FqName(fqName.parent());
+        @JvmField
+        val ROOT: FqName = FqName("")
 
-        return parent;
-    }
-
-    @NotNull
-    public FqName child(@NotNull Name name) {
-        return new FqName(fqName.child(name), this);
-    }
-
-    @NotNull
-    public Name shortName() {
-        return fqName.shortName();
-    }
-
-    @NotNull
-    public Name shortNameOrSpecial() {
-        return fqName.shortNameOrSpecial();
-    }
-
-    @NotNull
-    public List<Name> pathSegments() {
-        return fqName.pathSegments();
-    }
-
-    public boolean startsWith(@NotNull Name segment) {
-        return fqName.startsWith(segment);
-    }
-
-    public boolean startsWith(@NotNull FqName other) {
-        return fqName.startsWith(other.fqName);
-    }
-
-    @NotNull
-    public static FqName topLevel(@NotNull Name shortName) {
-        return new FqName(FqNameUnsafe.topLevel(shortName));
-    }
-
-    @Override
-    public String toString() {
-        return fqName.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FqName)) return false;
-
-        FqName otherFqName = (FqName) o;
-
-        if (!fqName.equals(otherFqName.fqName)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return fqName.hashCode();
+        @JvmStatic
+        fun topLevel(shortName: Name): FqName {
+            return FqName(FqNameUnsafe.topLevel(shortName))
+        }
     }
 }
