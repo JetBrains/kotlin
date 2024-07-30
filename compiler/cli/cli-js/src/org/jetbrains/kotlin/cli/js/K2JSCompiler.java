cfe82ac21d08f4056ce94575fa138512382d922e
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-07-30 17:54:59 +0200

    [JS] Move K2JSCompiler#calculateSourceMapSourceRoot to K2JsIrCompiler
    This is required for dropping K2JSCompiler entirely (KT-70219).

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JSCompiler.java
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JSCompiler.java
@@ -1,12 +1,11 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.cli.js;
 
 import com.intellij.openapi.Disposable;
-import com.intellij.util.ExceptionUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.cli.common.CLICompiler;
@@ -14,7 +13,6 @@ import org.jetbrains.kotlin.cli.common.CommonCompilerPerformanceManager;
 import org.jetbrains.kotlin.cli.common.ExitCode;
 import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments;
 import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArgumentsKt;
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity;
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector;
 import org.jetbrains.kotlin.config.CommonConfigurationKeys;
 import org.jetbrains.kotlin.config.CompilerConfiguration;
@@ -23,9 +21,7 @@ import org.jetbrains.kotlin.metadata.deserialization.BinaryVersion;
 import org.jetbrains.kotlin.utils.JsMetadataVersion;
 import org.jetbrains.kotlin.utils.KotlinPaths;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.List;
 
 import static org.jetbrains.kotlin.cli.common.ExitCode.COMPILATION_ERROR;
 import static org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.ERROR;
@@ -82,57 +78,6 @@ public class K2JSCompiler extends CLICompiler<K2JSCompilerArguments> {
         if (K2JSCompilerArgumentsKt.isIrBackendEnabled(arguments)) {
             getIrCompiler().setupPlatformSpecificArgumentsAndServices(configuration, arguments, services);
         }
-    }
-
-    @NotNull
-    static String calculateSourceMapSourceRoot(
-            @NotNull MessageCollector messageCollector,
-            @NotNull K2JSCompilerArguments arguments
-    ) {
-        File commonPath = null;
-        List<File> pathToRoot = new ArrayList<>();
-        Map<File, Integer> pathToRootIndexes = new HashMap<>();
-
-        try {
-            for (String path : arguments.getFreeArgs()) {
-                File file = new File(path).getCanonicalFile();
-                if (commonPath == null) {
-                    commonPath = file;
-
-                    while (file != null) {
-                        pathToRoot.add(file);
-                        file = file.getParentFile();
-                    }
-                    Collections.reverse(pathToRoot);
-
-                    for (int i = 0; i < pathToRoot.size(); ++i) {
-                        pathToRootIndexes.put(pathToRoot.get(i), i);
-                    }
-                }
-                else {
-                    while (file != null) {
-                        Integer existingIndex = pathToRootIndexes.get(file);
-                        if (existingIndex != null) {
-                            existingIndex = Math.min(existingIndex, pathToRoot.size() - 1);
-                            pathToRoot.subList(existingIndex + 1, pathToRoot.size()).clear();
-                            commonPath = pathToRoot.get(pathToRoot.size() - 1);
-                            break;
-                        }
-                        file = file.getParentFile();
-                    }
-                    if (file == null) {
-                        break;
-                    }
-                }
-            }
-        }
-        catch (IOException e) {
-            String text = ExceptionUtil.getThrowableText(e);
-            messageCollector.report(CompilerMessageSeverity.ERROR, "IO error occurred calculating source root:\n" + text, null);
-            return ".";
-        }
-
-        return commonPath != null ? commonPath.getPath() : ".";
     }
 
     @NotNull
--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JsIrCompiler.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2JsIrCompiler.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.cli.js
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.util.ExceptionUtil
 import org.jetbrains.kotlin.analyzer.CompilationErrorException
 import org.jetbrains.kotlin.backend.common.CompilationException
 import org.jetbrains.kotlin.backend.common.phaser.PhaseConfig
@@ -70,6 +71,8 @@ import org.jetbrains.kotlin.utils.join
 import org.jetbrains.kotlin.wasm.config.WasmConfigurationKeys
 import java.io.File
 import java.io.IOException
+import java.util.*
+import kotlin.math.min
 
 private val K2JSCompilerArguments.granularity: JsGenerationGranularity
     get() = when {
@@ -790,7 +793,7 @@ class K2JsIrCompiler : CLICompiler<K2JSCompilerArguments>() {
 
             var sourceMapSourceRoots = arguments.sourceMapBaseDirs
             if (sourceMapSourceRoots == null && StringUtil.isNotEmpty(arguments.sourceMapPrefix)) {
-                sourceMapSourceRoots = K2JSCompiler.calculateSourceMapSourceRoot(messageCollector, arguments)
+                sourceMapSourceRoots = calculateSourceMapSourceRoot(messageCollector, arguments)
             }
 
             if (sourceMapSourceRoots != null) {
@@ -936,6 +939,54 @@ class K2JsIrCompiler : CLICompiler<K2JSCompilerArguments>() {
                 .dropLastWhile { it.isEmpty() }
                 .toTypedArray()
                 .filterNot { it.isEmpty() }
+        }
+
+        private fun calculateSourceMapSourceRoot(
+            messageCollector: MessageCollector,
+            arguments: K2JSCompilerArguments,
+        ): String {
+            var commonPath: File? = null
+            val pathToRoot = mutableListOf<File>()
+            val pathToRootIndexes = hashMapOf<File, Int>()
+
+            try {
+                for (path in arguments.freeArgs) {
+                    var file: File? = File(path).canonicalFile
+                    if (commonPath == null) {
+                        commonPath = file
+
+                        while (file != null) {
+                            pathToRoot.add(file)
+                            file = file.parentFile
+                        }
+                        pathToRoot.reverse()
+
+                        for (i in pathToRoot.indices) {
+                            pathToRootIndexes[pathToRoot[i]] = i
+                        }
+                    } else {
+                        while (file != null) {
+                            var existingIndex = pathToRootIndexes[file]
+                            if (existingIndex != null) {
+                                existingIndex = min(existingIndex, pathToRoot.size - 1)
+                                pathToRoot.subList(existingIndex + 1, pathToRoot.size).clear()
+                                commonPath = pathToRoot[pathToRoot.size - 1]
+                                break
+                            }
+                            file = file.parentFile
+                        }
+                        if (file == null) {
+                            break
+                        }
+                    }
+                }
+            } catch (e: IOException) {
+                val text = ExceptionUtil.getThrowableText(e)
+                messageCollector.report(ERROR, "IO error occurred calculating source root:\n$text", location = null)
+                return "."
+            }
+
+            return commonPath?.path ?: "."
         }
     }
 }
