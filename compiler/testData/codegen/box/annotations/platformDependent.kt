9781d9a58cc890c17a4daf719ee033e1bf9cef25
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-08-01 11:51:37 +0300

    [Test] Adjust test for `@PlatformDependent` according to new stdlib layout
    Now all stdlibs contain `@PlatformDependent` annotation, so declaring it
      separately in test causing clash in `STATIC_EVERYWHERE` mode
    
    ^KT-56076
    ^KT-57858

--- compiler/testData/codegen/box/annotations/platformDependent.kt
+++ compiler/testData/codegen/box/annotations/platformDependent.kt
@@ -1,13 +1,6 @@
 // IGNORE_BACKEND_K1: JS, JS_IR, JS_IR_ES6, WASM, NATIVE
 // ISSUE: KT-57858
 
-// FILE: Sub.kt
-
-package kotlin.internal
-
-@Target(AnnotationTarget.FUNCTION)
-annotation class PlatformDependent
-
 // FILE: Main.kt
 
 @file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
