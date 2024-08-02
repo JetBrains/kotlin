c02eb2c38181ff30b0f9b1001e7490c9fc547be4
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-08-02 22:18:13 +0200

    [stdlib] Move CoroutineImpl to the common JS & Wasm source set
    This will help avoid maintaining two copies of CoroutineImpl.

--- libraries/stdlib/js/src/kotlin/coroutines/intrinsics/IntrinsicsJs.kt
+++ libraries/stdlib/js/src/kotlin/coroutines/intrinsics/IntrinsicsJs.kt
@@ -208,7 +208,7 @@ private inline fun <T> createCoroutineFromSuspendFunction(
 ): Continuation<Unit> {
     return object : CoroutineImpl(completion as Continuation<Any?>) {
         override fun doResume(): Any? {
-            if (exception != null) throw exception
+            exception?.let { throw it }
             return block()
         }
     }
