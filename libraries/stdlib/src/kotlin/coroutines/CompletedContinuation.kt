307a1de23dc443e5ce43fed7cb99f0dac7ada926
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-08-05 17:33:07 +0200

    [stdlib] Commonize CompletedContinuation
    This object was copied verbatim in each platform source set.
    There's no need in this.

---
+++ libraries/stdlib/src/kotlin/coroutines/CompletedContinuation.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.coroutines
+
+internal object CompletedContinuation : Continuation<Any?> {
+    override val context: CoroutineContext
+        get() = error("This continuation is already complete")
+
+    override fun resumeWith(result: Result<Any?>) {
+        error("This continuation is already complete")
+    }
+
+    override fun toString(): String = "This continuation is already complete"
+}
