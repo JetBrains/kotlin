bbc09d92b40f32094adac0e0d0dec9f543530338
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-08-05 18:38:02 +0200

    [K/N][tests] Fix local execution of CompilerOutputTest.

--- native/native.tests/build.gradle.kts
+++ native/native.tests/build.gradle.kts
@@ -60,7 +60,16 @@ val swiftExportTest = nativeTest("swiftExportTest", "swiftexport")
 val testTags = findProperty("kotlin.native.tests.tags")?.toString()
 // Note: arbitrary JUnit tag expressions can be used in this property.
 // See https://junit.org/junit5/docs/current/user-guide/#running-tests-tag-expressions
-val test by nativeTest("test", testTags, requirePlatformLibs = true) {
+val test by nativeTest(
+    "test",
+    testTags,
+    requirePlatformLibs = true,
+    defineJDKEnvVariables = listOf(
+        JdkMajorVersion.JDK_1_8,  // required in CompilerOutputTest via AbstractCliTest.getNormalizedCompilerOutput
+        JdkMajorVersion.JDK_11_0, // required in CompilerOutputTest via AbstractCliTest.getNormalizedCompilerOutput
+        JdkMajorVersion.JDK_17_0, // required in CompilerOutputTest via AbstractCliTest.getNormalizedCompilerOutput
+    )
+) {
     options {
         // See [org.jetbrains.kotlin.konan.test.KlibCrossCompilationIdentityTest.FULL_CROSS_DIST_ENABLED_PROPERTY]
         // See also kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/nativeFullCrossDist.kt
