62673385a115d1ad7682697b2e4286ed53d77a6a
Maria Sokolova <82594708+mvicsokolova@users.noreply.github.com>
2024-08-06 11:59:04 +0200

    [atomicfu-plugin] Update kotlinx-atomicfu dependency to 0.25.0

--- plugins/atomicfu/atomicfu-compiler/testData/box/atomics_basic/LoopTest.kt
+++ plugins/atomicfu/atomicfu-compiler/testData/box/atomics_basic/LoopTest.kt
@@ -1,3 +1,6 @@
+// TODO(KT-65977): reenable these tests with caches
+//IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE
+//IGNORE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE
 import kotlinx.atomicfu.*
 import kotlin.test.*
 
