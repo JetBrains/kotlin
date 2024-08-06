7194fb271118685b252d36c2a97f4223091a5bd1
Maria Sokolova <82594708+mvicsokolova@users.noreply.github.com>
2024-08-06 11:59:04 +0200

    [atomicfu-plugin] Update kotlinx-atomicfu dependency to 0.25.0

--- plugins/atomicfu/atomicfu-compiler/testData/box/locks/SynchronizedObjectTest.kt
+++ plugins/atomicfu/atomicfu-compiler/testData/box/locks/SynchronizedObjectTest.kt
@@ -1,3 +1,6 @@
+// TODO(KT-65977): reenable these tests with caches
+//IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE
+//IGNORE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE
 import kotlinx.atomicfu.locks.*
 import kotlin.test.*
 
