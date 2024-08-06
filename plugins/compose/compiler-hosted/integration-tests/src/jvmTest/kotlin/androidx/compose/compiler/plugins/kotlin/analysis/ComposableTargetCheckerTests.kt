2b9c9ec261c2d7c4e7503d3e3a90c68c33b2216b
Chuck Jazdzewski <chuckj@google.com>
2024-08-06 16:42:03 -0700

    [Compose] Fix infinite recursion in target analysis
    The guard to prevent infinite recursion relies on the InfererenceNode
    type correctly implementing `equals()` and `hashCode()` which was not
    implemented for `PsiElementNode` in the K1 analysis.
    
    Fixes: 325004814

--- plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/analysis/ComposableTargetCheckerTests.kt
+++ plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/analysis/ComposableTargetCheckerTests.kt
@@ -469,4 +469,32 @@ class ComposableTargetCheckerTests : AbstractComposeDiagnosticsTest(useFir = fal
           }
         }
         """)
+
+    @Test
+    fun testIndirectRecursiveCall() = check(
+        """
+             import androidx.compose.runtime.*
+
+             @Composable @ComposableTarget("N") fun N() { }
+
+             @Composable
+             fun T() {
+               A(10) { N() }
+             }
+
+             @Composable
+             fun A(level: Int, content: @Composable () -> Unit) {
+                if (level > 0) B(level - 1) { content() }
+             }
+
+             @Composable
+             fun B(level: Int, content: @Composable () -> Unit) {
+               A(level) {
+                 content()
+                 B(level - 1) { content() }
+               }
+               N()
+             }
+        """
+    )
 }
