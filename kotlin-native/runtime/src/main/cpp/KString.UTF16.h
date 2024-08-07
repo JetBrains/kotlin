1a61db385321954c0d84182ec92e7328354ff4b5
Sonya Valchuk <pyos@google.com>
2024-08-07 21:27:42 +0000

    [Native] Use Latin-1 encoding where possible
    1. For constant strings
    2. When converting from an ASCII C or Objective-C string;
    3. When replacing with a Latin-1 character in a Latin-1 string
     (also, if the character to be replaced is not Latin-1, we know
     there's nothing to replace, so that's nice)

--- kotlin-native/runtime/src/main/cpp/KString.UTF16.h
+++ kotlin-native/runtime/src/main/cpp/KString.UTF16.h
@@ -41,9 +41,10 @@ struct UTF16String {
     UTF16StringIterator begin() const { return {data_}; }
     UTF16StringIterator end() const { return {data_ + size_}; }
     UTF16StringIterator at(const KChar* ptr) const { return {ptr}; }
-    size_t sizeInBytes() const { return size_ * sizeof(KChar); }
     size_t sizeInUnits() const { return size_; }
     size_t sizeInChars() const { return size_; }
+
+    static bool canEncode(KChar c) { return true; }
 
     static OBJ_GETTER(createUninitialized, size_t sizeInUnits) {
         RETURN_RESULT_OF(CreateUninitializedUtf16String, sizeInUnits);
