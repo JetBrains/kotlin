c1501a7e878df02cca41bbc7aa22681cef3a3534
Brian Norman <brian.norman@jetbrains.com>
2024-08-08 09:52:02 -0700

    [CFG] Fix var initialization check of nested lambdas and trys
    In deeply nested cases of a try-finally within a lambda, local var
    initialization checks can report false positives due to the recent
    capture-by-value changes meant for local val initialization checks.
    
    ^KT-70133 Fixed

---
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/nestedTryFinallyLambda.fir.kt
@@ -0,0 +1,42 @@
+fun invokeLater(x: () -> Unit) {
+    x()
+}
+
+fun nestedFinallyAndLambda1() {
+    var x: String
+    invokeLater {
+        x = ""
+        invokeLater {
+            try {
+            } finally {
+                <!UNINITIALIZED_VARIABLE!>x<!>.length
+            }
+        }
+    }
+}
+
+fun nestedFinallyAndLambda2() {
+    var x: String
+    invokeLater {
+        x = ""
+        try {
+        } finally {
+            invokeLater {
+                x.length
+            }
+        }
+    }
+}
+
+fun nestedFinallyAndLambda3() {
+    var x: String
+    try {
+    } finally {
+        invokeLater {
+            x = ""
+            invokeLater {
+                x.length
+            }
+        }
+    }
+}
