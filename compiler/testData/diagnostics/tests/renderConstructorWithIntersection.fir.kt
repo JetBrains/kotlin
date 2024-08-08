266df891fea82d9f889b91cfea6502c5afc651bd
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-08-08 15:37:40 +0300

    [FIR] Reproduce ^KT-70194

---
+++ compiler/testData/diagnostics/tests/renderConstructorWithIntersection.fir.kt
@@ -0,0 +1,22 @@
+// ISSUE: KT-70194
+// RENDER_DIAGNOSTICS_FULL_TEXT
+
+// FILE: Flexier.java
+
+public class Flexier {
+    public static <T> T flexify(T it) {
+        return it;
+    }
+}
+
+// FILE: Main.kt
+
+interface A
+interface B
+
+object O1 : A, B
+object O2 : A, B
+
+fun test() {
+    val a: Int = <!INITIALIZER_TYPE_MISMATCH, TYPE_MISMATCH!>Flexier.flexify(if (true) O1 else O2)<!>
+}
