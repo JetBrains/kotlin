d8158a21ed0d9db93d1ebac76500c1b8f7677f1b
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-08-09 14:35:45 +0200

    [Gradle] Make Gradle 7.1 plugin variant as main one
    ^KT-70430 In Progress

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/gradle-plugin-dependency-configuration.gradle.kts
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/gradle-plugin-dependency-configuration.gradle.kts
@@ -20,13 +20,6 @@ extensions.extraProperties["kotlin.stdlib.default.dependency"] = "false"
 val commonSourceSet = createGradleCommonSourceSet()
 reconfigureMainSourcesSetForGradlePlugin(commonSourceSet)
 
-// Used for Gradle 7.1+ versions
-createGradlePluginVariant(
-    GradlePluginVariant.GRADLE_71,
-    commonSourceSet = commonSourceSet,
-    isGradlePlugin = false
-)
-
 // Used for Gradle 7.4+ versions
 createGradlePluginVariant(
     GradlePluginVariant.GRADLE_74,
