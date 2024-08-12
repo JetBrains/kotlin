b6c7ecb3c84fca2ab03b46bf53fa49e24930caef
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-08-12 22:21:26 +0200

    Kapt: mute testAndroidDaggerIC for new K2 kapt
     #KT-69860
     #KT-70637

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/Kapt3AndroidIncrementalIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/android/Kapt3AndroidIncrementalIT.kt
@@ -78,7 +78,7 @@ open class Kapt3AndroidIncrementalIT : Kapt3BaseIT() {
 
     @DisplayName("incremental compilation works with dagger")
     @GradleAndroidTest
-    fun testAndroidDaggerIC(
+    open fun testAndroidDaggerIC(
         gradleVersion: GradleVersion,
         agpVersion: String,
         jdkVersion: JdkVersions.ProvidedJdk,
