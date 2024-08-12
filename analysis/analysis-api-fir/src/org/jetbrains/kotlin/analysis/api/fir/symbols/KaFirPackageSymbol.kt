8d3983c6cedeacd7183ce8c18a83a01148b7dde7
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-08-12 21:01:14 +0200

    [Analysis API FIR] KaFirPackageSymbol: cleanup implementation
    ^KT-70165

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirPackageSymbol.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirPackageSymbol.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -35,33 +35,19 @@ internal class KaFirPackageSymbol(
     override val origin: KaSymbolOrigin
         get() = withValidityAssertion { KaSymbolOrigin.SOURCE } // TODO
 
-    override fun createPointer(): KaSymbolPointer<KaPackageSymbol> = KaFirPackageSymbolPointer(fqName)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as KaFirPackageSymbol
-
-        if (fqName != other.fqName) return false
-        if (token != other.token) return false
-
-        return true
+    override fun createPointer(): KaSymbolPointer<KaPackageSymbol> = withValidityAssertion {
+        KaFirPackageSymbolPointer(fqName)
     }
 
-    override fun hashCode(): Int {
-        var result = fqName.hashCode()
-        result = 31 * result + token.hashCode()
-        return result
-    }
+    override fun equals(other: Any?): Boolean = this === other || other is KaFirPackageSymbol && other.fqName == fqName
+    override fun hashCode(): Int = fqName.hashCode()
 }
 
-class KtPackage(
+private class KtPackage(
     manager: PsiManager,
     private val fqName: FqName,
     private val scope: GlobalSearchScope,
 ) : PsiPackageImpl(manager, fqName.asString().replace('/', '.')) {
     override fun copy() = KtPackage(manager, fqName, scope)
-
     override fun isValid(): Boolean = project.createPackageProvider(scope).doesKotlinOnlyPackageExist(fqName)
 }
