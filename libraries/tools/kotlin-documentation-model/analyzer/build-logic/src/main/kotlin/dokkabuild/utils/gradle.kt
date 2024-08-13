44ece71ef9fa43cf4294b13e98b4538caff622ff
Adam <152864218+adam-enko@users.noreply.github.com>
2024-08-13 11:57:28 +0200

    [KDM] Merge Dokkatoo into Dokka Gradle Plugin
    See https://github.com/Kotlin/dokka/pull/3695
    
    Initial Dokkatoo import
    
    - merge DGP-classic into dokka-gradle-plugin
    - add flag to toggle between DPG-classic and Dokkatoo
    - Update Dokkatoo code to be Java-8 compatible
    - update dokkatoo package name
    - stop using `org.jetbrains.dokka.gradle.utils.sourceLink_` - weird class-not-found compilation issue
    - update TaskPathCollector workaround
    - fix LogHtmlPublicationLinkTask - use Http instead of Https connection
    - Specify language/api as Kotlin 1.4 for dokka-gradle-plugin (to be compatible with Gradle 7)
    - update api dump
    - update formatting, add copyright header
    - remove duplicated 'Isolation' from WorkerIsolation subtypes, & fix test
    - renaming 'Dokkatoo' to 'Dokka'...
    - fix process isolation worker jvm args
    - add 'min supported Gradle' warning
    - lazily fetch DokkaGradlePluginMode from `project.extra.properties`
    - fixing KotlinNativeDistributionAccessor...
    - fix DGP group/version
    - add copyright header
    - Change BCV to official BCV
    - fix `toLowerCase()` deprecation
    - remove `dokka-gradle-plugin` from `libs.versions.toml` (it's no longer used)
    - remove duplicated plugin website/vcsUrl, move tags into specific Dokka plugin
    - exclude Gradle's embedded-dependencies from DGP
    - remove extraneous leftover groups
    Original commit: https://github.com/Kotlin/dokka/commit/a3d5d43fa383141fae41bb88a1900e94e68c3fd7

--- libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/dokkabuild/utils/gradle.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/dokkabuild/utils/gradle.kt
@@ -4,11 +4,18 @@
 
 package dokkabuild.utils
 
+import org.gradle.api.Action
+import org.gradle.api.NamedDomainObjectProvider
+import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ModuleDependency
 import org.gradle.api.attributes.*
 import org.gradle.api.attributes.java.TargetJvmEnvironment
+import org.gradle.api.component.AdhocComponentWithVariants
 import org.gradle.api.model.ObjectFactory
-import org.gradle.kotlin.dsl.named
+import org.gradle.api.tasks.SourceSet
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.kotlin.dsl.*
 
 
 /**
@@ -86,4 +93,65 @@ internal fun AttributeContainer.jvmJar(objects: ObjectFactory) {
     attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
     attribute(TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE, objects.named(TargetJvmEnvironment.STANDARD_JVM))
     attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.JAR))
+}
+
+/**
+ * Pretty-print the Java name.
+ *
+ * For Java 8 and below the version is prefixed with `1.`.
+ */
+fun JavaLanguageVersion.formattedName(): String =
+    if (asInt() <= 8) "1.${asInt()}" else asInt().toString()
+
+/**
+ * Disable publishing of test fixtures (which causes warnings when publishing).
+ *
+ * https://docs.gradle.org/current/userguide/java_testing.html#publishing_test_fixtures
+ */
+fun Project.skipTestFixturesPublications() {
+    val javaComponent = components["java"] as AdhocComponentWithVariants
+    javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+    javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
+}
+
+/**
+ * Exclude embedded Gradle dependencies from the given [SourceSet] configurations.
+ *
+ * The excluded dependencies are embedded into Gradle, they so should be excluded to prevent
+ * classpath ordering issues.
+ */
+// After extensive manual testing, it appears that these exclusions have no effect in dokka-gradle-plugin.
+//
+// DGP has no direct runtime dependencies on any of the conflicting dependencies, and Gradle has
+// constraints that force the transitive dependencies to match the embedded versions.
+// Unfortunately this means there is no way of testing if this config works, or can be safely removed.
+// It was originally added because in previous versions DGP did have a direct dependency on
+// kotlin-stdlib, but the buildscript has been re-written to correct this, meaning there are no
+// longer conflicting dependencies.
+//
+// These exclusions have been kept due to an abundance of caution.
+//
+// See also:
+// - https://youtrack.jetbrains.com/issue/KT-41142
+// - https://github.com/JetBrains/kotlin/blob/2f41a05651e4709fcb6984bbac769af8e8f63935/libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/SimpleKotlinGradleIT.kt#L180
+// - https://github.com/Kotlin/dokka/pull/2570
+fun Project.excludeGradleEmbeddedDependencies(sourceSet: NamedDomainObjectProvider<SourceSet>) {
+    val excludeAction = Action<Configuration> {
+        dependencies
+            .withType<ModuleDependency>()
+            .configureEach {
+                exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
+                exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+                exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+                exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-common")
+                exclude(group = "org.jetbrains.kotlin", module = "kotlin-reflect")
+                exclude(group = "org.jetbrains.kotlin", module = "kotlin-script-runtime")
+            }
+    }
+
+    sourceSet.configure {
+        configurations.named(implementationConfigurationName, excludeAction)
+        configurations.named(apiConfigurationName, excludeAction)
+        configurations.named(runtimeOnlyConfigurationName, excludeAction)
+    }
 }
