32945890d6f911704114b9ecbc6cbf592eb97ab2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-08-15 16:51:08 +0200

    [LL FIR] simplify AbstractFirClassByPsiClassProviderTest
    to avoid retrieving KtTestModule by hand
    
    KT-65618

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirClassByPsiClassProviderTest.kt
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirClassByPsiClassProviderTest.kt
@@ -7,18 +7,17 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir
 
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiJavaFile
-import com.intellij.psi.util.childrenOfType
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaSourceModule
 import org.jetbrains.kotlin.analysis.low.level.api.fir.providers.firClassByPsiClassProvider
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirSourceTestConfigurator
 import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedTest
-import org.jetbrains.kotlin.analysis.test.framework.projectStructure.ktTestModuleStructure
+import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtTestModule
 import org.jetbrains.kotlin.analysis.test.framework.services.expressionMarkerProvider
 import org.jetbrains.kotlin.fir.render
+import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.assertions
-import org.jetbrains.kotlin.test.services.moduleStructure
 import kotlin.collections.any
 import kotlin.collections.flatMap
 import kotlin.collections.single
@@ -28,25 +27,24 @@ import kotlin.text.endsWith
 abstract class AbstractFirClassByPsiClassProviderTest : AbstractAnalysisApiBasedTest() {
     override val configurator = AnalysisApiFirSourceTestConfigurator(analyseInDependentSession = false)
 
-    override fun doTest(testServices: TestServices) {
-        val useSiteModule = testServices.moduleStructure.modules.single()
-        val useSiteKtModule = testServices.ktTestModuleStructure.getKtTestModule(useSiteModule).ktModule
-        val psiClassUnderCaret = when (useSiteKtModule) {
+    override fun doTestByMainModuleAndOptionalMainFile(mainFile: KtFile?, mainModule: KtTestModule, testServices: TestServices) {
+        val mainKtModule = mainModule.ktModule
+        val psiClassUnderCaret = when (mainKtModule) {
             is KaSourceModule -> {
                 testServices.expressionMarkerProvider.getElementsOfTypeAtCarets<PsiClass>(testServices).single().first
             }
             is KaLibraryModule -> {
-                testServices.ktTestModuleStructure.getKtTestModule(useSiteModule).files
+                mainModule.files
                     .filterIsInstance<PsiJavaFile>()
                     .flatMap { it.classes.toList() }
                     .flatMap { it.withAllNestedClasses() }
                     .single { it.annotations.any { it.qualifiedName?.endsWith("Caret") == true } }
             }
             else -> {
-                error("Unexpected module kind ${useSiteKtModule::class.simpleName}")
+                error("Unexpected module kind ${mainKtModule::class.simpleName}")
             }
         }
-        val resolveSession = LLFirResolveSessionService.getInstance(useSiteKtModule.project).getFirResolveSessionNoCaching(useSiteKtModule)
+        val resolveSession = LLFirResolveSessionService.getInstance(mainKtModule.project).getFirResolveSessionNoCaching(mainKtModule)
         val firClass = resolveSession.useSiteFirSession.firClassByPsiClassProvider.getFirClass(psiClassUnderCaret)!!
         val rendered = firClass.fir.render()
         testServices.assertions.assertEqualsToTestDataFileSibling(rendered)
