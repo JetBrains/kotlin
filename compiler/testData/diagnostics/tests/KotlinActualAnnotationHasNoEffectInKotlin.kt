84b9bdddef16fed0e2f2370490486318f4aa58af
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-08-16 11:55:46 +0200

    1/n [TESTS] Add test for KotlinActual annotation illegal usage in Kotlin
    KT-67202
    Extract to a separate commit, because too much of the generated code

---
+++ compiler/testData/diagnostics/tests/KotlinActualAnnotationHasNoEffectInKotlin.kt
@@ -0,0 +1,19 @@
+// FIR_IDENTICAL
+// WITH_STDLIB
+
+@KotlinActual
+fun foo() {
+    val a: KotlinActual = null!!
+    val b: (KotlinActual) -> Unit = { x -> }
+    val c: ((KotlinActual) -> Unit, (KotlinActual) -> Unit) -> ((KotlinActual) -> Unit) = { x, y -> { } }
+    val d: (() -> KotlinActual, () -> KotlinActual) -> (() -> KotlinActual) = { x, y -> { null!! } }
+
+    val e = KotlinActual::class
+    val f = ::<!CALLABLE_REFERENCE_TO_ANNOTATION_CONSTRUCTOR!>KotlinActual<!>
+    val g = KotlinActual()
+}
+
+typealias Duh = KotlinActual
+
+@[KotlinActual]
+fun bar() {}
