6ff40f644c547ad15cd63411b7a1b9620f70bb4f
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2024-08-19 17:15:06 +0200

    Fix :compiler:tests-for-compiler-generator according dependency-analysis plugin

--- compiler/tests-for-compiler-generator/build.gradle.kts
+++ compiler/tests-for-compiler-generator/build.gradle.kts
@@ -7,18 +7,21 @@ dependencies {
     testImplementation(kotlinStdlib())
 
     testApi(platform(libs.junit.bom))
-    testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
     testImplementation(projectTests(":compiler:tests-common"))
-    testImplementation(projectTests(":compiler:test-infrastructure"))
     testImplementation(projectTests(":compiler:tests-common-new"))
     testImplementation(projectTests(":compiler"))
     testImplementation(projectTests(":compiler:fir:raw-fir:psi2fir"))
     testImplementation(projectTests(":compiler:fir:raw-fir:light-tree2fir"))
-    testImplementation(projectTests(":compiler:fir:fir2ir"))
     testImplementation(projectTests(":compiler:fir:analysis-tests:legacy-fir-tests"))
-    testImplementation(projectTests(":js:js.tests"))
     testImplementation(projectTests(":generators:test-generator"))
+    testImplementation(projectTests(":compiler:fir:analysis-tests"))
+
+    testImplementation(project(":compiler:backend"))
+    testImplementation(project(":compiler:backend.common.jvm"))
+    testImplementation(project(":compiler:test-infrastructure-utils"))
+    testImplementation(project(":compiler:tests-compiler-utils"))
+    testImplementation(project(":libraries:tools:abi-comparator"))
 }
 
 sourceSets {
