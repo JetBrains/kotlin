22a6ebed35881eefaea71338488a0d843fec11cd
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-08-19 14:52:10 +0300

    [KDF] Support more parameters in read* functions
    Moved from https://github.com/Kotlin/dataframe/commit/874abff8e51b4cfa64edcf2147c31db2530ef825

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/read.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/read.kt
@@ -7,11 +7,17 @@ import org.jetbrains.kotlinx.dataframe.plugin.impl.AbstractInterpreter
 import org.jetbrains.kotlinx.dataframe.plugin.impl.Arguments
 import org.jetbrains.kotlinx.dataframe.plugin.impl.Present
 import org.jetbrains.kotlinx.dataframe.api.schema
+import org.jetbrains.kotlinx.dataframe.io.JSON
+import org.jetbrains.kotlinx.dataframe.io.JSON.TypeClashTactic.ARRAY_AND_VALUE_COLUMNS
+import org.jetbrains.kotlinx.dataframe.io.NameRepairStrategy
+import org.jetbrains.kotlinx.dataframe.io.StringColumns
 import org.jetbrains.kotlinx.dataframe.io.read
 import org.jetbrains.kotlinx.dataframe.io.readCSV
 import org.jetbrains.kotlinx.dataframe.io.readDelimStr
+import org.jetbrains.kotlinx.dataframe.io.readExcel
 import org.jetbrains.kotlinx.dataframe.io.readJson
 import org.jetbrains.kotlinx.dataframe.io.readJsonStr
+import org.jetbrains.kotlinx.dataframe.plugin.impl.AbstractSchemaModificationInterpreter
 import org.jetbrains.kotlinx.dataframe.plugin.impl.PluginDataFrameSchema
 import org.jetbrains.kotlinx.dataframe.plugin.impl.data.IoSchema
 import org.jetbrains.kotlinx.dataframe.plugin.impl.data.deserializeToPluginDataFrameSchema
@@ -30,13 +36,17 @@ internal class Read0 : AbstractInterpreter<PluginDataFrameSchema>() {
 
 internal class ReadCSV0 : AbstractInterpreter<PluginDataFrameSchema>() {
     val Arguments.fileOrUrl: String by arg()
+    val Arguments.delimiter: Char by arg(defaultValue = Present(','))
+    val Arguments.skipLines: Int by arg(defaultValue = Present(0))
+    val Arguments.readLines: Int? by arg(defaultValue = Present(null))
+    val Arguments.duplicate: Boolean by arg(defaultValue = Present(true))
 
     override fun Arguments.interpret(): PluginDataFrameSchema {
         val file = resolveFile(resolutionPath, fileOrUrl)
         val df = if (file != null && file.exists()) {
-            DataFrame.readCSV(file)
+            DataFrame.readCSV(file, delimiter, skipLines = skipLines, readLines = readLines, duplicate = duplicate)
         } else {
-            DataFrame.readCSV(fileOrUrl)
+            DataFrame.readCSV(fileOrUrl, delimiter, skipLines = skipLines, readLines = readLines, duplicate = duplicate)
         }
         return df.schema().toPluginDataFrameSchema()
     }
@@ -91,17 +101,42 @@ private fun resolveFile(resolutionPath: String?, path: String): File? {
 internal class ReadDelimStr : AbstractInterpreter<PluginDataFrameSchema>() {
     val Arguments.text: String by arg()
     val Arguments.delimiter: Char by arg(defaultValue = Present(','))
+    val Arguments.skipLines: Int by arg(defaultValue = Present(0))
+    val Arguments.readLines: Int? by arg(defaultValue = Present(null))
 
     override fun Arguments.interpret(): PluginDataFrameSchema {
-        return DataFrame.readDelimStr(text, delimiter).schema().toPluginDataFrameSchema()
+        return DataFrame.readDelimStr(text, delimiter, skipLines = skipLines, readLines = readLines).schema().toPluginDataFrameSchema()
     }
 }
 
 internal class ReadJsonStr : AbstractInterpreter<PluginDataFrameSchema>() {
     val Arguments.text: String by arg()
+    val Arguments.typeClashTactic: JSON.TypeClashTactic by arg(defaultValue = Present(ARRAY_AND_VALUE_COLUMNS))
 
     override fun Arguments.interpret(): PluginDataFrameSchema {
-        return DataFrame.readJsonStr(text).schema().toPluginDataFrameSchema()
+        return DataFrame.readJsonStr(text, typeClashTactic = typeClashTactic).schema().toPluginDataFrameSchema()
     }
 }
 
+internal class ReadExcel : AbstractSchemaModificationInterpreter() {
+    val Arguments.fileOrUrl: String by arg()
+    val Arguments.sheetName: String? by arg(defaultValue = Present(null))
+    val Arguments.skipRows: Int by arg(defaultValue = Present(0))
+    val Arguments.columns: String? by arg(defaultValue = Present(null))
+    val Arguments.stringColumns: StringColumns? by arg(defaultValue = Present(null))
+    val Arguments.rowsCount: Int? by arg(defaultValue = Present(null))
+    val Arguments.nameRepairStrategy: NameRepairStrategy by arg(defaultValue = Present(NameRepairStrategy.CHECK_UNIQUE))
+
+    override fun Arguments.interpret(): PluginDataFrameSchema {
+        val df = DataFrame.readExcel(fileOrUrl, sheetName, skipRows, columns, stringColumns, rowsCount, nameRepairStrategy)
+        return df.schema().toPluginDataFrameSchema()
+    }
+}
+
+internal class StringColumnsConstructor : AbstractInterpreter<StringColumns>() {
+    val Arguments.range: String by arg()
+
+    override fun Arguments.interpret(): StringColumns {
+        return StringColumns(range)
+    }
+}
