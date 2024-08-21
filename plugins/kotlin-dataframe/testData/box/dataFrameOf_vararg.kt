841de5d0cf0757543b416b461a3a7214dc5b51f0
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-08-21 15:58:38 +0300

    [KDF] Fix NPE in guess type for values of functional types
    From my quick research, reflection doesn't know anything about these values. They don't have invoke methods, nor any supertypes. So for now i decided to simply fix NPE by falling back to generic Function type for such columns. It will then at least work together with compiler plugin
    
    Moved from https://github.com/Kotlin/dataframe/commit/10935d2e80d1e233f2891a71decba2c7af3276a1

--- plugins/kotlin-dataframe/testData/box/dataFrameOf_vararg.kt
+++ plugins/kotlin-dataframe/testData/box/dataFrameOf_vararg.kt
@@ -6,5 +6,8 @@ import org.jetbrains.kotlinx.dataframe.io.*
 fun box(): String {
     val df = dataFrameOf("a")(1, 2, 3)
     val i: Int = df.a[0]
+
+    val df1 = dataFrameOf("a", "b")({ 1 }, 2)
+    val i1: Int = df1.a[0].invoke()
     return "OK"
 }
