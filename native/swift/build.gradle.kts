232286c11c9f39bc07dffdad934b4d9de6c08cdb
Artem Olkov <artem.olkov@jetbrains.com>
2024-08-21 13:12:11 +0000

    Swift Export: add Test All IDEA configuration
    Merge-request: KT-MR-17575
    Merged-by: Artem Olkov <artem.olkov@jetbrains.com>

--- native/swift/build.gradle.kts
+++ native/swift/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
 
 tasks.register("sirAllTests") {
     dependsOn(
+        ":native:native.tests:swiftExportTest",
+        ":native:swift:swift-export-standalone:test",
         ":native:swift:sir-compiler-bridge:test",
-        ":native:swift:sir-printer:test"
+        ":native:swift:sir-printer:test",
     )
 }
