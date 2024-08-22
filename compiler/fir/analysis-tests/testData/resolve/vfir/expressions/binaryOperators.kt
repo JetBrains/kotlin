3fb8ccba2653a06637ccbfd88b42f40aef7b0edf
Tomas Husak <tomas.husak@jetbrains.com>
2024-08-22 15:26:55 +0200

    fixup! update tests

--- compiler/fir/analysis-tests/testData/resolve/vfir/expressions/binaryOperators.kt
+++ compiler/fir/analysis-tests/testData/resolve/vfir/expressions/binaryOperators.kt
@@ -1,7 +1,20 @@
 fun main() {
-    // We fold just this case
-    val stringLiterals = "A" + "B" + "C"
-    val intLiterals = 1 + 2 + 3
-    var a = "A"
-    val stringVariables = a + a + a
+    val case1 = "1" + "2"
+    val case2 = "1" + "2" + "3"
+    val case3 = ("1" + "2") + "3"
+    val case4 = "1" + ("2" + "3")
+    val case5 = "1" + case1
+    val case6 = "1" + "2" + case1
+    val case6a = ("1" + "2") + case1
+    val case7 = case1 + "1" + "2"
+    val case7a = case1 + ("1" + "2")
+    val case8 = "1" + case1 + "2"
+    val case9 = 1 + 2 + 3
+    val case10 = case1 + case1 + case1
+    val case11 = "1" + "2" + "3" + "4" + "5" + "6"
+    val case12 = "1" <!UNRESOLVED_REFERENCE!>-<!> "2"
+    val case13 = <!EXPRESSION_EXPECTED!>while(true) {}<!> + "2" + "3"
+    val case14 = "1" + "2" + f("1" + "2")
 }
+
+fun f(item: String): String = item
