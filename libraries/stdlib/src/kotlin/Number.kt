f7e0eaf4f560078ef8e83bfaeea96d3fba0997ac
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2024-08-22 13:33:27 +0200

    [stdlib] Make common Number constructible KT-65526

--- libraries/stdlib/src/kotlin/Number.kt
+++ libraries/stdlib/src/kotlin/Number.kt
@@ -11,7 +11,7 @@ import kotlin.internal.ActualizeByJvmBuiltinProvider
  * Superclass for all platform classes representing numeric values.
  */
 @ActualizeByJvmBuiltinProvider
-public expect abstract class Number {
+public expect abstract class Number() {
     /**
      * Returns the value of this number as a [Double], which may involve rounding.
      */
