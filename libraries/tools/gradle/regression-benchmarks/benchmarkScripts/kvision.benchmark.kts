bf779141f3e0f36e59d9e7b5f3296c52a54c4260
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-08-25 22:51:45 +0200

    [Gradle] Run Kvision benchmark against Kotlin 2.0.20 release
    ^KT-70307 In Progress

--- libraries/tools/gradle/regression-benchmarks/benchmarkScripts/kvision.benchmark.kts
+++ libraries/tools/gradle/regression-benchmarks/benchmarkScripts/kvision.benchmark.kts
@@ -10,7 +10,7 @@
     name = "kvision",
     gitUrl = "https://github.com/rjaros/kvision.git",
     gitCommitSha = "8f2b3c96dcc9ad594995367ad138a37328244bb9",
-    stableKotlinVersion = "2.0.0",
+    stableKotlinVersion = "2.0.20",
 )
 
 import java.io.File
