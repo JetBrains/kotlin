0cba321bf43fc2c639ecb5e04fee14584600bc8d
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-08-28 16:18:32 +0200

    [TEST] Add tests for Java direct actualization
    KT-67202

---
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_simple.fir.kt
@@ -0,0 +1,16 @@
+// WITH_KOTLIN_JVM_ANNOTATIONS
+// MODULE: m1-common
+// FILE: common.kt
+
+<!NO_ACTUAL_FOR_EXPECT{JVM}!>expect class <!CLASSIFIER_REDECLARATION!>Foo<!>() {
+    fun foo()
+}<!>
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: Foo.java
+
+@kotlin.annotations.jvm.KotlinActual public class Foo {
+    @kotlin.annotations.jvm.KotlinActual public Foo() {}
+    @kotlin.annotations.jvm.KotlinActual public void foo() {
+    }
+}
