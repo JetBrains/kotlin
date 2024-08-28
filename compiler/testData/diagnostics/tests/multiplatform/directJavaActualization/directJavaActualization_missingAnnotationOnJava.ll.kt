c934d9d64f8ba88ee71025f9b5aaf62231af4501
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-08-28 16:18:32 +0200

    [TEST] Add tests for Java direct actualization
    KT-67202

---
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_missingAnnotationOnJava.ll.kt
@@ -0,0 +1,15 @@
+// WITH_KOTLIN_JVM_ANNOTATIONS
+// MODULE: m1-common
+// FILE: common.kt
+
+expect class Foo() {
+    fun foo()
+}
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: Foo.java
+
+public class Foo {
+    public void foo() {
+    }
+}
--- wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxTestGenerated.java
+++ wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxTestGenerated.java
@@ -27979,6 +27979,12 @@ public class FirWasmJsCodegenBoxTestGenerated extends AbstractFirWasmJsCodegenBo
       }
 
       @Test
+      @TestMetadata("directJavaActualization.kt")
+      public void testDirectJavaActualization() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/directJavaActualization.kt");
+      }
+
+      @Test
       @TestMetadata("expectNonExpectOverloads.kt")
       public void testExpectNonExpectOverloads() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectNonExpectOverloads.kt");
