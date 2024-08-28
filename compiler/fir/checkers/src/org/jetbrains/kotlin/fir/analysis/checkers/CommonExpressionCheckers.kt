694f0b9c4bb940e758ea0c7d78a7aeeb09704cd8
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-08-28 16:18:32 +0200

    2/2 [FIR] Report all KotlinActual annotation usages in Kotlin.
    KT-67202
    Implement FirKotlinActualAnnotationHasNoEffectInKotlinExpressionChecker and
    FirKotlinActualAnnotationHasNoEffectInKotlinTypeChecker

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonExpressionCheckers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonExpressionCheckers.kt
@@ -17,6 +17,7 @@ object CommonExpressionCheckers : ExpressionCheckers() {
 
     override val basicExpressionCheckers: Set<FirBasicExpressionChecker>
         get() = setOf(
+            FirKotlinActualAnnotationHasNoEffectInKotlinExpressionChecker,
             FirUnderscoreChecker,
             FirExpressionAnnotationChecker,
             FirDeprecationChecker,
