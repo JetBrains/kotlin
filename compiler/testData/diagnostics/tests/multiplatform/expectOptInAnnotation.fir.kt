4b6b58c5dabc9654cb727fca05703d579d7be552
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-08-28 16:18:32 +0200

    [FIR, IR] Improve EXPECT_ACTUAL_OPT_IN_ANNOTATION reporting
    KT-67202
    
    Architecturally, EXPECT_ACTUAL_OPT_IN_ANNOTATION is reported more
    correctly now.
    
    After this commit:
    - Illegal @RequiresOptIn on expect declarations is reported by a separate
      FirRequiresOptInOnExpectChecker
    - Illegal @RequiresOptIn on actual declarations is reported by
      AbstractExpectActualChecker
    
    AbstractExpectActualChecker is shared infrastructure between FIR and IR.
    Which means that the diagnostic will be repoted in IR as well (needed
    for direct Java actualization)
    
    Previously the diagnostic was reported from FirExpectActualDeclarationChecker.
    FirExpectActualDeclarationChecker checker is MppCheckerKind.Platform
    checker. But it's preferred to use MppCheckerKind.Common when possible
    (the newly introduced FirRequiresOptInOnExpectChecker is
    MppCheckerKind.Common checker)
    
    Downsides:
    - On actual declarations, the diagnostic with clear name
      EXPECT_ACTUAL_OPT_IN_ANNOTATION turns into a generic
      ACTUAL_WITHOUT_EXPECT (but it's applicable for all things reported
      from AbstractExpectActualChecker KT-62631)

--- compiler/testData/diagnostics/tests/multiplatform/expectOptInAnnotation.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/expectOptInAnnotation.fir.kt
@@ -3,10 +3,10 @@
 // FILE: common.kt
 @file:OptIn(ExperimentalMultiplatform::class)
 
-expect annotation class ActualOnly
+<!EXPECT_ACTUAL_INCOMPATIBILITY{JVM}!>expect annotation class ActualOnly<!>
 
-@RequiresOptIn
-<!EXPECT_ACTUAL_OPT_IN_ANNOTATION, EXPECT_ACTUAL_OPT_IN_ANNOTATION{METADATA}!>expect<!> annotation class Both
+<!EXPECT_ACTUAL_INCOMPATIBILITY{JVM}!>@RequiresOptIn
+<!EXPECT_ACTUAL_OPT_IN_ANNOTATION!>expect<!> annotation class Both<!>
 
 @RequiresOptIn
 @OptionalExpectation
@@ -15,10 +15,10 @@ expect annotation class MyOptIn
 // MODULE: m1-jvm()()(m1-common)
 // FILE: jvm.kt
 @RequiresOptIn
-<!EXPECT_ACTUAL_OPT_IN_ANNOTATION!>actual<!> annotation class ActualOnly
+actual annotation class <!ACTUAL_WITHOUT_EXPECT!>ActualOnly<!>
 
 @RequiresOptIn
-<!EXPECT_ACTUAL_OPT_IN_ANNOTATION!>actual<!> annotation class Both
+actual annotation class <!ACTUAL_WITHOUT_EXPECT!>Both<!>
 
 @RequiresOptIn
 actual annotation class MyOptIn
