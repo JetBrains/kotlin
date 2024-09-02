b1daaacdd04f18e88156f46ca75397334dfe36e5
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-09-02 16:27:20 +0200

    [TEST] Add more tests for direct Java actualization
    KT-67202

---
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_actualMemberWithoutExpect_sharedActual.kt
@@ -0,0 +1,20 @@
+// WITH_KOTLIN_JVM_ANNOTATIONS
+// LANGUAGE:+DirectJavaActualization
+// WITH_STDLIB
+
+// MODULE: m1-common
+// FILE: common.kt
+expect class <!IMPLICIT_JVM_ACTUALIZATION{JVM}!>Foo<!>
+
+expect class Bar {
+    fun typealiasOnly()
+}
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: Foo.java
+@kotlin.annotations.jvm.KotlinActual public class Foo {
+    @kotlin.annotations.jvm.KotlinActual public void typealiasOnly() {}
+}
+
+// FILE: jvm.kt
+actual typealias Bar = Foo
--- compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_actualWithoutExpect.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_actualWithoutExpect.fir.kt
@@ -2,14 +2,16 @@
 // LANGUAGE:+DirectJavaActualization
 // MODULE: m1-common
 // FILE: common.kt
+open class Base() {
+    open fun fakeOverride() {}
+}
 
-<!JAVA_DIRECT_ACTUAL_WITHOUT_EXPECT{JVM}, JAVA_DIRECT_ACTUAL_WITHOUT_EXPECT{JVM}!>expect<!> class Foo
+<!JAVA_DIRECT_ACTUAL_WITHOUT_EXPECT{JVM}, JAVA_DIRECT_ACTUAL_WITHOUT_EXPECT{JVM}, JAVA_DIRECT_ACTUAL_WITHOUT_EXPECT{JVM}!>expect<!> class Foo : Base
 
 // MODULE: m2-jvm()()(m1-common)
 // FILE: Foo.java
-
-@kotlin.annotations.jvm.KotlinActual public class Foo {
+@kotlin.annotations.jvm.KotlinActual public class Foo extends Base {
     @kotlin.annotations.jvm.KotlinActual public Foo() {}
-    @kotlin.annotations.jvm.KotlinActual public void foo() {
-    }
+    @kotlin.annotations.jvm.KotlinActual public void foo() {}
+    @kotlin.annotations.jvm.KotlinActual @Override void fakeOverride() {}
 }
