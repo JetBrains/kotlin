f139f908011c7a1d02b27ae26b07903570442aff
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-09-02 14:26:17 +0200

    fixup! [IR] 2/2 Implement FirDirectJavaActualDeclarationExtractor

--- compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_javaInDifferentModule.kt
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_javaInDifferentModule.kt
@@ -1,4 +1,5 @@
 // WITH_KOTLIN_JVM_ANNOTATIONS
+// LANGUAGE:+DirectJavaActualization
 // MODULE: m1-common
 // FILE: common.kt
 expect class <!NO_ACTUAL_FOR_EXPECT{JVM}!>Foo<!> { // "direct actual" in different compilation unit is not permitted
