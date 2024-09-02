ec4161925220debcddc3ec16e2116b1dfed8888a
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-09-02 16:27:20 +0200

    [TEST] Add more tests for direct Java actualization
    KT-67202

--- compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_actualWithoutExpect.ll.kt
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_actualWithoutExpect.ll.kt
@@ -2,14 +2,16 @@
 // LANGUAGE:+DirectJavaActualization
 // MODULE: m1-common
 // FILE: common.kt
+open class Base() {
+    open fun fakeOverride() {}
+}
 
-expect class Foo
+expect class Foo : Base
 
 // MODULE: m2-jvm()()(m1-common)
 // FILE: Foo.java
-
-@kotlin.annotations.jvm.KotlinActual public class Foo {
+@kotlin.annotations.jvm.KotlinActual public class Foo extends Base {
     @kotlin.annotations.jvm.KotlinActual public Foo() {}
-    @kotlin.annotations.jvm.KotlinActual public void foo() {
-    }
+    @kotlin.annotations.jvm.KotlinActual public void foo() {}
+    @kotlin.annotations.jvm.KotlinActual @Override void fakeOverride() {}
 }
--- compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_simple.fir.kt
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_simple.fir.kt
@@ -2,16 +2,34 @@
 // LANGUAGE:+DirectJavaActualization
 // MODULE: m1-common
 // FILE: common.kt
+open class Base() {
+    open fun fakeOverrideInExpect() {}
+}
 
-expect class Foo() {
+expect open class Foo() : Base {
     fun foo()
+    open fun fakeOverrideInActual()
+
+    class Nested
+    inner class Inner
 }
 
 // MODULE: m2-jvm()()(m1-common)
 // FILE: Foo.java
-
-@kotlin.annotations.jvm.KotlinActual public class Foo {
+@kotlin.annotations.jvm.KotlinActual public class Foo extends Base implements JavaBase {
     @kotlin.annotations.jvm.KotlinActual public Foo() {}
-    @kotlin.annotations.jvm.KotlinActual public void foo() {
-    }
+    @kotlin.annotations.jvm.KotlinActual public void foo() {}
+
+    @Override
+    public void fakeOverrideInExpect() {}
+
+    public void additionalMember() {}
+
+    @kotlin.annotations.jvm.KotlinActual public static class Nested {}
+    @kotlin.annotations.jvm.KotlinActual public class Inner {}
+}
+
+// FILE: JavaBase.java
+public interface JavaBase {
+    default void fakeOverrideInActual() {}
 }
