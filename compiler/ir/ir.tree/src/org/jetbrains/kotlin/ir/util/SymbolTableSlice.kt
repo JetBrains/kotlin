ae1d6363214bf3866b6e704bee2e56f89d040132
Julien Dramaix <dramaix@google.com>
2024-09-03 07:30:22 +0000

    Check if the provided signature is public before adding a symbol to the SymbolTable.
    This is needed for Kotlin/Closure that needs to temporary populate the SymbolTable in order to be able to deserialize IR.
    IR symbol created by K2 does not have signature.

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/SymbolTableSlice.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/SymbolTableSlice.kt
@@ -91,7 +91,7 @@ abstract class SymbolTableSlice<Key, SymbolOwner, Symbol>(val lock: IrLock)
         return owner
     }
 
-    class Flat<Key, SymbolOwner, Symbol>(lock: IrLock, val symbolFilter: (Symbol) -> Boolean = { true }) : SymbolTableSlice<Key, SymbolOwner, Symbol>(lock)
+    class Flat<Key, SymbolOwner, Symbol>(lock: IrLock, val symbolFilter: (Key, Symbol) -> Boolean = { _,_ -> true }) : SymbolTableSlice<Key, SymbolOwner, Symbol>(lock)
             where SymbolOwner : IrSymbolOwner, Symbol : IrBindableSymbol<*, SymbolOwner> {
         private val signatureToSymbol = hashMapOf<Key, Symbol>()
 
@@ -100,7 +100,7 @@ abstract class SymbolTableSlice<Key, SymbolOwner, Symbol>(val lock: IrLock)
         }
 
         override fun set(key: Key, symbol: Symbol) {
-            if (symbolFilter(symbol)) {
+            if (symbolFilter(key, symbol)) {
                 signatureToSymbol[key] = symbol
             }
         }
