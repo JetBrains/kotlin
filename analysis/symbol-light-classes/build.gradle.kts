218eae255ad32a372edef4e4e2159c42d54325ce
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-09-06 12:22:58 +0300

    [Build] Add dependency on plugin annotation build task to LC tests
    These tests enable `:plugins:fir-plugin-prototype` plugin, which requires
      klib with annotations produced by
      `:plugins:fir-plugin-prototype:plugin-annotations:distAnnotations` task
      to be built

--- analysis/symbol-light-classes/build.gradle.kts
+++ analysis/symbol-light-classes/build.gradle.kts
@@ -36,6 +36,7 @@ sourceSets {
 
 projectTest(jUnitMode = JUnitMode.JUnit5) {
     dependsOn(":dist")
+    dependsOn(":plugins:fir-plugin-prototype:plugin-annotations:distAnnotations")
     workingDir = rootDir
     useJUnitPlatform()
 }
