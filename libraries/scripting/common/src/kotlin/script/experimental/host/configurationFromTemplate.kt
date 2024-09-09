96b29819e53a00ecbc4d6a48b88f98946ee21d5e
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2024-09-09 12:36:05 +0200

    Enhance scriptConfigInstance to handle constructor accessibility
    ^KT-71352 Fixed

--- libraries/scripting/common/src/kotlin/script/experimental/host/configurationFromTemplate.kt
+++ libraries/scripting/common/src/kotlin/script/experimental/host/configurationFromTemplate.kt
@@ -199,6 +199,12 @@ private inline fun <reified T : Annotation> KClass<*>.findAnnotation(): T? =
 private inline fun <reified T : PropertiesCollection> scriptConfigInstance(kclass: KClass<out T>): T? =
     kclass.objectInstance ?: run {
         val noArgsConstructor = kclass.java.constructors.singleOrNull { it.parameters.isEmpty() }
-        noArgsConstructor?.let { it.newInstance() as T }
+        noArgsConstructor?.let {
+            try {
+                it.isAccessible = true
+            } catch (_: RuntimeException) {
+            }
+            it.newInstance() as T
+        }
     }
 
