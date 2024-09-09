ca156e7b170192bfe94b5bc9963f80240b968ab9
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-09-09 14:54:30 +0300

    [Gradle] Bump NPM dependencies
    ^KT-71184 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/NpmVersions.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/NpmVersions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -13,18 +13,18 @@ import java.io.Serializable
 // DO NOT MODIFY DIRECTLY! Use org.jetbrains.kotlin.generators.gradle.targets.js.MainKt
 class NpmVersions : Serializable {
     val webpackDevServer = NpmPackageVersion("webpack-dev-server", "4.15.2")
-    val webpack = NpmPackageVersion("webpack", "5.93.0")
+    val webpack = NpmPackageVersion("webpack", "5.94.0")
     val webpackCli = NpmPackageVersion("webpack-cli", "5.1.4")
     val sourceMapLoader = NpmPackageVersion("source-map-loader", "5.0.0")
     val sourceMapSupport = NpmPackageVersion("source-map-support", "0.5.21")
     val cssLoader = NpmPackageVersion("css-loader", "7.1.2")
     val styleLoader = NpmPackageVersion("style-loader", "4.0.0")
-    val sassLoader = NpmPackageVersion("sass-loader", "14.2.1")
-    val sass = NpmPackageVersion("sass", "1.77.8")
+    val sassLoader = NpmPackageVersion("sass-loader", "16.0.1")
+    val sass = NpmPackageVersion("sass", "1.78.0")
     val toStringLoader = NpmPackageVersion("to-string-loader", "1.2.0")
-    val miniCssExtractPlugin = NpmPackageVersion("mini-css-extract-plugin", "2.9.0")
-    val mocha = NpmPackageVersion("mocha", "10.7.0")
-    val karma = NpmPackageVersion("karma", "6.4.3")
+    val miniCssExtractPlugin = NpmPackageVersion("mini-css-extract-plugin", "2.9.1")
+    val mocha = NpmPackageVersion("mocha", "10.7.3")
+    val karma = NpmPackageVersion("karma", "6.4.4")
     val karmaChromeLauncher = NpmPackageVersion("karma-chrome-launcher", "3.2.0")
     val karmaPhantomjsLauncher = NpmPackageVersion("karma-phantomjs-launcher", "1.0.4")
     val karmaFirefoxLauncher = NpmPackageVersion("karma-firefox-launcher", "2.1.3")
