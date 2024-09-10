4e7ec12892494d2a2709f10a83d1b3f9c4c3aad3
Anastasia Pikalova <anastasia.nekrasova@jetbrains.com>
2024-09-10 23:11:19 +0200

    Make @SubclassOptInRequired accept multiple experimental markers
    [4/5] Update existing tests
    
    ^KT-70562 in progress

--- compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/NotApiMarkerAsArgument.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/NotApiMarkerAsArgument.kt
@@ -1,4 +1,10 @@
 annotation class DummyAnnotation
 
+@RequiresOptIn
+annotation class NotDummyAnnotation
+
 <!OPT_IN_ARGUMENT_IS_NOT_MARKER!>@SubclassOptInRequired(DummyAnnotation::class)<!>
-open class IncorrectSubclassOptInArgumentMarker
+open class IncorrectSubclassOptInArgumentMarkerA
+
+<!OPT_IN_ARGUMENT_IS_NOT_MARKER("DummyAnnotation")!>@SubclassOptInRequired(DummyAnnotation::class, NotDummyAnnotation::class)<!>
+open class IncorrectSubclassOptInArgumentMarkerB
