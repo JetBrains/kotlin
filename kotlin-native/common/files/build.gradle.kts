2f9ad11e9e69019150e12ceb69ee347b6ad4a314
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-09-11 17:25:18 +0200

    [K/N][build] Split :kotlin-native:common into 2 projects
    `:kotlin-native:common` had 2 independently compiled "modules":
    `env` and `files`. Move these "modules" into 2 separate projects,
    as is convention.
    
    ^KT-71270

---
+++ kotlin-native/common/files/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.konan.target.TargetWithSanitizer
+
+plugins {
+    id("compile-to-bitcode")
+    kotlin("jvm")
+    id("native-interop-plugin")
+}
+
+bitcode {
+    hostTarget {
+        module("files") {
+            srcRoot.set(layout.projectDirectory.dir("src"))
+            headersDirs.from(srcRoot.dir("headers"))
+            sourceSets {
+                main {}
+            }
+        }
+    }
+}
+
+kotlinNativeInterop {
+    create("files") {
+        pkg("org.jetbrains.kotlin.backend.konan.files")
+        linker("clang++")
+        linkOutputs(bitcode.hostTarget.module("files").get().sourceSets.main.get().task.get())
+        headers(layout.projectDirectory.files("src/headers/Files.h"))
+    }
+}
+
+configurations.apiElements.configure {
+    extendsFrom(kotlinNativeInterop["files"].configuration)
+}
+
+configurations.runtimeElements.configure {
+    extendsFrom(kotlinNativeInterop["files"].configuration)
+}
+
+val nativeLibs by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = false
+    attributes {
+        attribute(ArtifactTypeDefinition.ARTIFACT_TYPE_ATTRIBUTE, ArtifactTypeDefinition.DIRECTORY_TYPE)
+        attribute(TargetWithSanitizer.TARGET_ATTRIBUTE, TargetWithSanitizer.host)
+    }
+}
+
+artifacts {
+    add(nativeLibs.name, layout.buildDirectory.dir("nativelibs/${TargetWithSanitizer.host}")) {
+        builtBy(kotlinNativeInterop["files"].genTask)
+    }
+}
