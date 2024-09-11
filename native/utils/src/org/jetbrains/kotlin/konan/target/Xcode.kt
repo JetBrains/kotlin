28de843da3258a729102f983a2a065589beabd80
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-09-11 11:34:59 +0200

    Bump supported Xcode in KGP to 16.0
    ^KT-71377

--- native/utils/src/org/jetbrains/kotlin/konan/target/Xcode.kt
+++ native/utils/src/org/jetbrains/kotlin/konan/target/Xcode.kt
@@ -42,7 +42,7 @@ data class XcodeVersion(val major: Int, val minor: Int) : Comparable<XcodeVersio
             )
         }
 
-        val maxTested = XcodeVersion(15, 3)
+        val maxTested = XcodeVersion(16, 0)
     }
 }
 
