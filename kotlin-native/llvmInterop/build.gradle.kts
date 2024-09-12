8a2df5d60033ef5f57a635448f7607094b4145fb
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-09-12 17:04:03 +0200

    [K/N][build] Move kotlin library compilation outside of interop plugin
    ^KT-71270

--- kotlin-native/llvmInterop/build.gradle.kts
+++ kotlin-native/llvmInterop/build.gradle.kts
@@ -28,12 +28,15 @@ kotlinNativeInterop {
     }
 }
 
-configurations.apiElements.configure {
-    extendsFrom(kotlinNativeInterop["llvm"].configuration)
+dependencies {
+    implementation(kotlinStdlib())
+    api(project(":kotlin-native:Interop:Runtime"))
 }
 
-configurations.runtimeElements.configure {
-    extendsFrom(kotlinNativeInterop["llvm"].configuration)
+sourceSets {
+    "main" {
+        kotlin.srcDir(kotlinNativeInterop["llvm"].genTask.map { layout.buildDirectory.dir("nativeInteropStubs/llvm/kotlin") })
+    }
 }
 
 val nativeLibs by configurations.creating {
