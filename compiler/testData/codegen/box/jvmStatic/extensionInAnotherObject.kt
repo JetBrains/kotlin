JetBrains/kotlin

commit 9924d8e8ceb96968e0ef3ba0790453565dc9cd65

[IR] Eagerly initialize dispatch/extensionReceiverParameter
See the next commit for details.

KT-68003 Related
KT-69734 Related

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/DeclarationStubGenerator.kt
@@ -23,6 +23,7 @@ import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.declarations.lazy.*
+import org.jetbrains.kotlin.ir.declarations.lazy.IrLazyFunctionBase
 import org.jetbrains.kotlin.ir.linkage.IrProvider
 import org.jetbrains.kotlin.ir.symbols.IrFieldSymbol
 import org.jetbrains.kotlin.ir.symbols.IrSymbol
@@ -201,7 +202,10 @@ abstract class DeclarationStubGenerator(
                 isFakeOverride = (origin == IrDeclarationOrigin.FAKE_OVERRIDE),
                 isOperator = descriptor.isOperator, isInfix = descriptor.isInfix,
                 stubGenerator = this, typeTranslator = typeTranslator
-            ).generateParentDeclaration()
+            ).generateParentDeclaration().also {
+                it.dispatchReceiverParameter = it.createReceiverParameter(descriptor.dispatchReceiverParameter)
+                it.extensionReceiverParameter = it.createReceiverParameter(descriptor.extensionReceiverParameter)
+            }
         }
     }
 
@@ -221,7 +225,10 @@ abstract class DeclarationStubGenerator(
                 descriptor.name, descriptor.visibility,
                 descriptor.isInline, descriptor.isEffectivelyExternal(), descriptor.isPrimary, descriptor.isExpect,
                 this, typeTranslator
-            ).generateParentDeclaration()
+            ).generateParentDeclaration().also {
+                it.dispatchReceiverParameter = it.createReceiverParameter(descriptor.dispatchReceiverParameter)
+                it.extensionReceiverParameter = it.createReceiverParameter(descriptor.extensionReceiverParameter)
+            }
         }
     }
 
@@ -239,6 +246,12 @@ abstract class DeclarationStubGenerator(
             }
         }.generateParentDeclaration()
     }
+
+    private fun IrLazyFunctionBase.createReceiverParameter(parameter: ReceiverParameterDescriptor?): IrValueParameter? =
+        if (stubGenerator.extensions.isStaticFunction(descriptor)) null
+        else typeTranslator.buildWithScope(this) {
+            parameter?.generateReceiverParameterStub()?.also { it.parent = this@createReceiverParameter }
+        }
 
     // in IR Generator enums also have special handling, but here we have not enough data for it
     // probably, that is not a problem, because you can't add new enum value to external module

compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/Fir2IrLazyDeclarationsGenerator.kt
@@ -7,14 +7,17 @@ package org.jetbrains.kotlin.fir.backend.generators
 
 import org.jetbrains.kotlin.fir.backend.*
 import org.jetbrains.kotlin.fir.backend.utils.convertWithOffsets
+import org.jetbrains.kotlin.fir.backend.utils.declareThisReceiverParameter
 import org.jetbrains.kotlin.fir.backend.utils.irOrigin
 import org.jetbrains.kotlin.fir.containingClassLookupTag
 import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.isSubstitutionOrIntersectionOverride
 import org.jetbrains.kotlin.fir.lazy.*
+import org.jetbrains.kotlin.fir.lazy.Fir2IrLazyConstructor
 import org.jetbrains.kotlin.fir.unwrapUseSiteSubstitutionOverrides
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.symbols.*
+import org.jetbrains.kotlin.ir.util.parentClassOrNull
 
 class Fir2IrLazyDeclarationsGenerator(private val c: Fir2IrComponents) : Fir2IrComponents by c {
     internal fun createIrLazyFunction(
@@ -29,10 +32,33 @@ class Fir2IrLazyDeclarationsGenerator(private val c: Fir2IrComponents) : Fir2IrC
             Fir2IrLazySimpleFunction(
                 c, startOffset, endOffset, declarationOrigin,
                 fir, firContainingClass, symbol, lazyParent, isFakeOverride
-            ).apply {
-                prepareTypeParameters()
-            }
+            )
         }
+
+        irFunction.prepareTypeParameters()
+
+        declarationStorage.enterScope(symbol)
+
+        irFunction.extensionReceiverParameter = fir.receiverParameter?.let {
+            irFunction.declareThisReceiverParameter(
+                c,
+                thisType = it.typeRef.toIrType(typeConverter),
+                thisOrigin = irFunction.origin,
+                explicitReceiver = it,
+            )
+        }
+
+        val containingClass = lazyParent as? IrClass
+        if (containingClass != null && irFunction.shouldHaveDispatchReceiver(containingClass)) {
+            val thisType = Fir2IrCallableDeclarationsGenerator.computeDispatchReceiverType(irFunction, fir, containingClass, c)
+            irFunction.dispatchReceiverParameter = irFunction.declareThisReceiverParameter(
+                c,
+                thisType = thisType ?: error("No dispatch receiver receiver for function"),
+                thisOrigin = irFunction.origin
+            )
+        }
+
+        declarationStorage.leaveScope(symbol)
         return irFunction
     }
 
@@ -61,8 +87,25 @@ class Fir2IrLazyDeclarationsGenerator(private val c: Fir2IrComponents) : Fir2IrC
         symbol: IrConstructorSymbol,
         declarationOrigin: IrDeclarationOrigin,
         lazyParent: IrDeclarationParent,
-    ): Fir2IrLazyConstructor = fir.convertWithOffsets { startOffset, endOffset ->
-        Fir2IrLazyConstructor(c, startOffset, endOffset, declarationOrigin, fir, symbol, lazyParent)
+    ): Fir2IrLazyConstructor {
+        val irConstructor = fir.convertWithOffsets { startOffset, endOffset ->
+            Fir2IrLazyConstructor(c, startOffset, endOffset, declarationOrigin, fir, symbol, lazyParent)
+        }
+
+        declarationStorage.enterScope(symbol)
+
+        val containingClass = lazyParent as? IrClass
+        val outerClass = containingClass?.parentClassOrNull
+        if (containingClass?.isInner == true && outerClass != null) {
+            irConstructor.dispatchReceiverParameter = irConstructor.declareThisReceiverParameter(
+                c,
+                thisType = outerClass.thisReceiver!!.type,
+                thisOrigin = irConstructor.origin
+            )
+        }
+
+        declarationStorage.leaveScope(symbol)
+        return irConstructor
     }
 
     fun createIrLazyClass(

compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/AbstractFir2IrLazyFunction.kt
@@ -9,12 +9,8 @@ import org.jetbrains.kotlin.descriptors.DescriptorVisibility
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.fir.backend.Fir2IrComponents
-import org.jetbrains.kotlin.fir.backend.utils.declareThisReceiverParameter
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
-import org.jetbrains.kotlin.fir.declarations.FirReceiverParameter
-import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.declarations.utils.*
-import org.jetbrains.kotlin.fir.symbols.lazyResolveToPhase
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.declarations.lazy.AbstractIrLazyFunction
@@ -24,7 +20,6 @@ import org.jetbrains.kotlin.ir.expressions.IrBody
 import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
 import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
-import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.isFacadeClass
 import org.jetbrains.kotlin.ir.util.isObject
 import org.jetbrains.kotlin.utils.addToStdlib.shouldNotBeCalled
@@ -108,16 +103,9 @@ abstract class AbstractFir2IrLazyFunction<F : FirCallableDeclaration>(
         get() = null
         set(_) = error("We should never need to store metadata of external declarations.")
 
-    protected fun shouldHaveDispatchReceiver(containingClass: IrClass): Boolean {
+    internal fun shouldHaveDispatchReceiver(containingClass: IrClass): Boolean {
         return !fir.isStatic && !containingClass.isFacadeClass &&
                 (!containingClass.isObject || containingClass.isCompanion || !extensions.isTrueStatic(fir, session))
-    }
-
-    protected fun createThisReceiverParameter(thisType: IrType, explicitReceiver: FirReceiverParameter? = null): IrValueParameter {
-        declarationStorage.enterScope(this.symbol)
-        return declareThisReceiverParameter(c, thisType, origin, explicitReceiver = explicitReceiver).apply {
-            declarationStorage.leaveScope(this@AbstractFir2IrLazyFunction.symbol)
-        }
     }
 
     override val factory: IrFactory

compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/Fir2IrLazyConstructor.kt
@@ -83,20 +83,7 @@ class Fir2IrLazyConstructor(
         fir.returnTypeRef.toIrType(typeConverter)
     }
 
-    override var dispatchReceiverParameter: IrValueParameter? by lazyVar(lock) {
-        val containingClass = parent as? IrClass
-        val outerClass = containingClass?.parentClassOrNull
-        if (containingClass?.isInner == true && outerClass != null) {
-            declarationStorage.enterScope(this.symbol)
-            declareThisReceiverParameter(
-                c,
-                thisType = outerClass.thisReceiver!!.type,
-                thisOrigin = origin
-            ).apply {
-                declarationStorage.leaveScope(this@Fir2IrLazyConstructor.symbol)
-            }
-        } else null
-    }
+    override var dispatchReceiverParameter: IrValueParameter? = null
 
     override var extensionReceiverParameter: IrValueParameter? = null
 

compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/Fir2IrLazyProperty.kt
@@ -12,6 +12,7 @@ import org.jetbrains.kotlin.descriptors.isAnnotationClass
 import org.jetbrains.kotlin.fir.backend.*
 import org.jetbrains.kotlin.fir.backend.utils.ConversionTypeOrigin
 import org.jetbrains.kotlin.fir.backend.utils.asCompileTimeIrInitializer
+import org.jetbrains.kotlin.fir.backend.utils.declareThisReceiverParameter
 import org.jetbrains.kotlin.fir.backend.utils.toIrConst
 import org.jetbrains.kotlin.fir.containingClassLookupTag
 import org.jetbrains.kotlin.fir.declarations.*
@@ -32,6 +33,7 @@ import org.jetbrains.kotlin.ir.expressions.IrExpressionBody
 import org.jetbrains.kotlin.ir.symbols.IrPropertySymbol
 import org.jetbrains.kotlin.ir.symbols.UnsafeDuringIrConstructionAPI
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.util.isFacadeClass
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.name.NameUtils
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
@@ -214,8 +216,8 @@ class Fir2IrLazyProperty(
             parent = this@Fir2IrLazyProperty.parent,
             isFakeOverride = isFakeOverride,
             correspondingPropertySymbol = this.symbol
-        ).apply {
-            classifiersGenerator.setTypeParameters(this, this@Fir2IrLazyProperty.fir, ConversionTypeOrigin.DEFAULT)
+        ).also {
+            initializeAccessor(it, ConversionTypeOrigin.DEFAULT)
         }
     }
 
@@ -238,9 +240,35 @@ class Fir2IrLazyProperty(
             parent = this@Fir2IrLazyProperty.parent,
             isFakeOverride = isFakeOverride,
             correspondingPropertySymbol = this.symbol
-        ).apply {
-            classifiersGenerator.setTypeParameters(this, this@Fir2IrLazyProperty.fir, ConversionTypeOrigin.SETTER)
+        ).also {
+            initializeAccessor(it, ConversionTypeOrigin.SETTER)
         }
+    }
+
+    private fun initializeAccessor(accessor: Fir2IrLazyPropertyAccessor, typeOrigin: ConversionTypeOrigin) {
+        declarationStorage.enterScope(accessor.symbol)
+
+        accessor.classifiersGenerator.setTypeParameters(accessor, fir, typeOrigin)
+
+        val containingClass = (parent as? IrClass)?.takeUnless { it.isFacadeClass }
+        if (containingClass != null && accessor.shouldHaveDispatchReceiver(containingClass)) {
+            accessor.dispatchReceiverParameter = accessor.declareThisReceiverParameter(
+                c,
+                thisType = containingClass.thisReceiver?.type ?: error("No this receiver for containing class"),
+                thisOrigin = accessor.origin,
+            )
+        }
+
+        accessor.extensionReceiverParameter = fir.receiverParameter?.let {
+            accessor.declareThisReceiverParameter(
+                c,
+                thisType = it.typeRef.toIrType(typeConverter, typeOrigin),
+                thisOrigin = accessor.origin,
+                explicitReceiver = it
+            )
+        }
+
+        declarationStorage.leaveScope(accessor.symbol)
     }
 
     override var overriddenSymbols: List<IrPropertySymbol> by symbolsMappingForLazyClasses.lazyMappedPropertyListVar(lock) lazy@{

compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/Fir2IrLazyPropertyAccessor.kt
@@ -66,19 +66,6 @@ class Fir2IrLazyPropertyAccessor(
         if (isSetter) builtins.unitType else firParentProperty.returnTypeRef.toIrType(typeConverter, conversionTypeContext)
     }
 
-    override var dispatchReceiverParameter: IrValueParameter? by lazyVar(lock) {
-        val containingClass = (parent as? IrClass)?.takeUnless { it.isFacadeClass }
-        if (containingClass != null && shouldHaveDispatchReceiver(containingClass)) {
-            createThisReceiverParameter(thisType = containingClass.thisReceiver?.type ?: error("No this receiver for containing class"))
-        } else null
-    }
-
-    override var extensionReceiverParameter: IrValueParameter? by lazyVar(lock) {
-        firParentProperty.receiverParameter?.let {
-            createThisReceiverParameter(it.typeRef.toIrType(typeConverter, conversionTypeContext), it)
-        }
-    }
-
     override var contextReceiverParametersCount: Int = fir.contextReceiversForFunctionOrContainingProperty().size
 
     override var valueParameters: List<IrValueParameter> by lazyVar(lock) {

compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/lazy/Fir2IrLazySimpleFunction.kt
@@ -50,25 +50,6 @@ class Fir2IrLazySimpleFunction(
         fir.symbol.resolvedReturnTypeRef.toIrType(typeConverter)
     }
 
-    override var dispatchReceiverParameter: IrValueParameter? by lazyVar(lock) {
-        val containingClass = parent as? IrClass
-        if (containingClass != null && shouldHaveDispatchReceiver(containingClass)) {
-            val thisType = Fir2IrCallableDeclarationsGenerator.computeDispatchReceiverType(
-                this,
-                fir,
-                containingClass,
-                c
-            )
-            createThisReceiverParameter(thisType ?: error("No dispatch receiver receiver for function"))
-        } else null
-    }
-
-    override var extensionReceiverParameter: IrValueParameter? by lazyVar(lock) {
-        fir.receiverParameter?.let {
-            createThisReceiverParameter(it.typeRef.toIrType(typeConverter), it)
-        }
-    }
-
     override var contextReceiverParametersCount: Int = fir.contextReceiversForFunctionOrContainingProperty().size
 
     override var valueParameters: List<IrValueParameter> by lazyVar(lock) {

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/declarations/lazy/AbstractIrLazyFunction.kt
@@ -7,10 +7,15 @@ package org.jetbrains.kotlin.ir.declarations.lazy
 
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.declarations.IrValueParameter
 import org.jetbrains.kotlin.ir.util.deserializedIr
 
 abstract class AbstractIrLazyFunction : IrSimpleFunction(), IrLazyFunctionBase {
     abstract val isDeserializationEnabled: Boolean
+
+    override var dispatchReceiverParameter: IrValueParameter? = null
+
+    override var extensionReceiverParameter: IrValueParameter? = null
 
     fun tryLoadIr(): Boolean {
         if (!isInline || isFakeOverride) return false

commit d2d29d86f59e1346473605e3b27723ba7d079a91

[IR K1] Fix lack of extension receiver on external @JvmStatic function
Fixes a bug introduced in 9924d8e8, where the part distinguishing between
dispatch and extension receivers was omitted.

#KT-71551 Fixed

compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/DeclarationStubGenerator.kt
@@ -204,8 +204,8 @@ abstract class DeclarationStubGenerator(
                 isOperator = descriptor.isOperator, isInfix = descriptor.isInfix,
                 stubGenerator = this, typeTranslator = typeTranslator
             ).generateParentDeclaration().also {
-                it.dispatchReceiverParameter = it.createReceiverParameter(descriptor.dispatchReceiverParameter)
-                it.extensionReceiverParameter = it.createReceiverParameter(descriptor.extensionReceiverParameter)
+                it.dispatchReceiverParameter = it.createReceiverParameter(descriptor.dispatchReceiverParameter, true)
+                it.extensionReceiverParameter = it.createReceiverParameter(descriptor.extensionReceiverParameter, false)
                 it.valueParameters = it.createValueParameters()
             }
         }
@@ -228,8 +228,8 @@ abstract class DeclarationStubGenerator(
                 descriptor.isInline, descriptor.isEffectivelyExternal(), descriptor.isPrimary, descriptor.isExpect,
                 this, typeTranslator
             ).generateParentDeclaration().also {
-                it.dispatchReceiverParameter = it.createReceiverParameter(descriptor.dispatchReceiverParameter)
-                it.extensionReceiverParameter = it.createReceiverParameter(descriptor.extensionReceiverParameter)
+                it.dispatchReceiverParameter = it.createReceiverParameter(descriptor.dispatchReceiverParameter, true)
+                it.extensionReceiverParameter = it.createReceiverParameter(descriptor.extensionReceiverParameter, false)
                 it.valueParameters = it.createValueParameters()
             }
         }
@@ -274,8 +274,11 @@ abstract class DeclarationStubGenerator(
         }.generateParentDeclaration()
     }
 
-    private fun IrLazyFunctionBase.createReceiverParameter(parameter: ReceiverParameterDescriptor?): IrValueParameter? =
-        if (stubGenerator.extensions.isStaticFunction(descriptor)) null
+    private fun IrLazyFunctionBase.createReceiverParameter(
+        parameter: ReceiverParameterDescriptor?,
+        functionDispatchReceiver: Boolean,
+    ): IrValueParameter? =
+        if (functionDispatchReceiver && stubGenerator.extensions.isStaticFunction(descriptor)) null
         else typeTranslator.buildWithScope(this) {
             parameter?.generateReceiverParameterStub()?.also { it.parent = this@createReceiverParameter }
         }

compiler/tests-gen/org/jetbrains/kotlin/codegen/LightAnalysisModeTestGenerated.java
@@ -31273,6 +31273,11 @@ public class LightAnalysisModeTestGenerated extends AbstractLightAnalysisModeTes
       runTest("compiler/testData/codegen/box/jvmStatic/explicitObject.kt");
     }
 
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirBlackBoxCodegenBasedTestGenerated.java
@@ -37029,6 +37029,12 @@ public class LLFirBlackBoxCodegenBasedTestGenerated extends AbstractLLFirBlackBo
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirReversedBlackBoxCodegenBasedTestGenerated.java
@@ -37029,6 +37029,12 @@ public class LLFirReversedBlackBoxCodegenBasedTestGenerated extends AbstractLLFi
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirLightTreeBlackBoxCodegenTestGenerated.java
@@ -36796,6 +36796,12 @@ public class FirLightTreeBlackBoxCodegenTestGenerated extends AbstractFirLightTr
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirPsiBlackBoxCodegenTestGenerated.java
@@ -36796,6 +36796,12 @@ public class FirPsiBlackBoxCodegenTestGenerated extends AbstractFirPsiBlackBoxCo
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt
@@ -1,0 +1,18 @@
+// TARGET_BACKEND: JVM
+// WITH_STDLIB
+// ISSUE: KT-71551
+
+// MODULE: lib
+// FILE: lib.kt
+package test
+
+object O {
+    @JvmStatic
+    public fun String.id(): String = this
+}
+
+// MODULE: main(lib)
+// FILE: box.kt
+import test.O.id
+
+fun box(): String = "OK".id()

compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/JvmAbiConsistencyTestBoxGenerated.java
@@ -36796,6 +36796,12 @@ public class JvmAbiConsistencyTestBoxGenerated extends AbstractJvmAbiConsistency
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -36796,6 +36796,12 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenWithIrInlinerTestGenerated.java
@@ -36796,6 +36796,12 @@ public class IrBlackBoxCodegenWithIrInlinerTestGenerated extends AbstractIrBlack
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");

compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/inlineScopes/FirBlackBoxCodegenTestWithInlineScopesGenerated.java
@@ -36796,6 +36796,12 @@ public class FirBlackBoxCodegenTestWithInlineScopesGenerated extends AbstractFir
     }
 
     @Test
+    @TestMetadata("extensionInAnotherObject.kt")
+    public void testExtensionInAnotherObject() {
+      runTest("compiler/testData/codegen/box/jvmStatic/extensionInAnotherObject.kt");
+    }
+
+    @Test
     @TestMetadata("extensionPropertyGetter.kt")
     public void testExtensionPropertyGetter() {
       runTest("compiler/testData/codegen/box/jvmStatic/extensionPropertyGetter.kt");