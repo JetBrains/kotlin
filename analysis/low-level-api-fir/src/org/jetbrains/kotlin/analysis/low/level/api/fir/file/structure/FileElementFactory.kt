8029e97cf33098267db04ee5789a4829c3f56e55
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2024-09-18 17:26:43 +0200

    fix resolve
    #KTIJ-28972

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/file/structure/FileElementFactory.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/file/structure/FileElementFactory.kt
@@ -5,62 +5,21 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.file.structure
 
-import org.jetbrains.kotlin.KtFakeSourceElementKind
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirModuleResolveComponents
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.collectDesignation
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.targets.LLFirClassWithSpecificMembersResolveTarget
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.targets.resolve
-import org.jetbrains.kotlin.analysis.low.level.api.fir.diagnostics.isImplicitConstructor
 import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.symbols.lazyResolveToPhaseWithCallableMembers
 
 internal object FileElementFactory {
     fun createFileStructureElement(
         firDeclaration: FirDeclaration,
         firFile: FirFile,
         moduleComponents: LLFirModuleResolveComponents,
-    ): FileStructureElement = when {
-        firDeclaration is FirRegularClass -> {
-            lazyResolveClassWithGeneratedMembers(firDeclaration)
+    ): FileStructureElement = when (firDeclaration) {
+        is FirRegularClass -> {
+            firDeclaration.lazyResolveToPhaseWithCallableMembers(FirResolvePhase.BODY_RESOLVE)
             ClassDeclarationStructureElement(firFile, firDeclaration, moduleComponents)
         }
-
-        firDeclaration is FirScript -> RootScriptStructureElement(firFile, firDeclaration, moduleComponents)
+        is FirScript -> RootScriptStructureElement(firFile, firDeclaration, moduleComponents)
         else -> DeclarationStructureElement(firFile, firDeclaration, moduleComponents)
-    }
-
-    private fun lazyResolveClassWithGeneratedMembers(firClass: FirRegularClass) {
-        val classMembersToResolve = buildList {
-            for (member in firClass.declarations) {
-                when {
-                    member is FirSimpleFunction && member.source?.kind == KtFakeSourceElementKind.DataClassGeneratedMembers -> {
-                        add(member)
-                    }
-
-                    member.source?.kind == KtFakeSourceElementKind.EnumGeneratedDeclaration -> {
-                        add(member)
-                    }
-
-                    member.isImplicitConstructor -> {
-                        add(member)
-                    }
-
-                    member is FirField && member.source?.kind == KtFakeSourceElementKind.ClassDelegationField -> {
-                        add(member)
-                    }
-
-                    member is FirDanglingModifierList -> {
-                        add(member)
-                    }
-                }
-            }
-        }
-
-        val firClassDesignation = firClass.collectDesignation()
-        val designationWithMembers = LLFirClassWithSpecificMembersResolveTarget(
-            firClassDesignation,
-            classMembersToResolve,
-        )
-
-        designationWithMembers.resolve(FirResolvePhase.BODY_RESOLVE)
     }
 }
