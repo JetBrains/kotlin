277a178f4e1dfae951283b6bbf5f00567ea0d982
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-09-18 17:22:35 -0400

    PoC: specify category

--- libraries/tools/kotlin-stdlib-docs/kotlin_big/build.gradle.kts
+++ libraries/tools/kotlin-stdlib-docs/kotlin_big/build.gradle.kts
@@ -35,6 +35,7 @@ modules.forEach { module ->
     if (module == "kotlin-test-js" || module == "kotlin-stdlib-js") {
         library.attributes {
             attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class, "kotlin-runtime"))
+            attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category::class, "library"))
             attribute(Attribute.of("org.jetbrains.kotlin.js.compiler", String::class.java), "ir")
         }
     }
