8f0d6099b4906fef130fb8b3edc70d62620f0eb6
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-09-19 02:41:37 +0200

    [LL FIR] FirElementBuilder: add tests on `where` for classes

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/SourceGetOrBuildFirTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/SourceGetOrBuildFirTestGenerated.java
@@ -76,6 +76,12 @@ public class SourceGetOrBuildFirTestGenerated extends AbstractSourceGetOrBuildFi
     }
 
     @Test
+    @TestMetadata("annotationInsideWhereClass.kt")
+    public void testAnnotationInsideWhereClass() {
+      runTest("analysis/low-level-api-fir/testData/getOrBuildFir/annotations/annotationInsideWhereClass.kt");
+    }
+
+    @Test
     @TestMetadata("annotationInsideWhereFunction.kt")
     public void testAnnotationInsideWhereFunction() {
       runTest("analysis/low-level-api-fir/testData/getOrBuildFir/annotations/annotationInsideWhereFunction.kt");
@@ -1341,6 +1347,12 @@ public class SourceGetOrBuildFirTestGenerated extends AbstractSourceGetOrBuildFi
     @TestMetadata("unresolvedTypeConsturctorResolvedTypeArgument.kt")
     public void testUnresolvedTypeConsturctorResolvedTypeArgument() {
       runTest("analysis/low-level-api-fir/testData/getOrBuildFir/types/unresolvedTypeConsturctorResolvedTypeArgument.kt");
+    }
+
+    @Test
+    @TestMetadata("whereClass.kt")
+    public void testWhereClass() {
+      runTest("analysis/low-level-api-fir/testData/getOrBuildFir/types/whereClass.kt");
     }
 
     @Test
