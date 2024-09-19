013c89826c6a7ea0a1873e37c78c52c90dcf5f56
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-09-19 16:22:07 +0200

    [IR] Expect-actual matcher: don't pull out static members
    ^KT-71577 Fixed
    
    Similar to FirExpectActualMatchingContextImpl#collectAllMembers,
    IrExpectActualMatchingContext#collectAllMembers should collect all non
    static members

--- compiler/ir/ir.actualization/src/main/kotlin/org/jetbrains/kotlin/backend/common/actualizer/IrExpectActualMatchingContext.kt
+++ compiler/ir/ir.actualization/src/main/kotlin/org/jetbrains/kotlin/backend/common/actualizer/IrExpectActualMatchingContext.kt
@@ -254,7 +254,7 @@ internal abstract class IrExpectActualMatchingContext(
      *   has no sense in IR context
      */
     override fun RegularClassSymbolMarker.collectAllMembers(isActualDeclaration: Boolean): List<DeclarationSymbolMarker> {
-        return asIr().declarations.filterNot { it is IrAnonymousInitializer }.map { it.symbol }
+        return asIr().declarations.filter { it !is IrAnonymousInitializer && !it.isStaticFun() }.map { it.symbol }
     }
 
     override fun RegularClassSymbolMarker.getMembersForExpectClass(name: Name): List<DeclarationSymbolMarker> {
@@ -610,3 +610,5 @@ internal abstract class IrExpectActualMatchingContext(
         }
     }
 }
+
+private fun IrDeclaration.isStaticFun(): Boolean = this is IrSimpleFunction && isStatic
