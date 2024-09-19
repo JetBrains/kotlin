21055d41471a67cafabdf983f12230113a4f9e3d
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-09-19 14:50:31 +0200

    [TEST] Add more Kotlin-to-Java direct actualization tests
    directJavaActualization_companionObject.kt and
    directJavaActualization_excessiveKotlinActualAnnotationOnJavaStatic.kt
    are inspired by KT-71577

---
+++ compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_companionObject.fir.kt
@@ -0,0 +1,14 @@
+// WITH_KOTLIN_JVM_ANNOTATIONS
+// LANGUAGE:+DirectJavaActualization
+// WITH_STDLIB
+
+// MODULE: m1-common
+// FILE: common.kt
+<!EXPECT_ACTUAL_INCOMPATIBILITY{JVM}!>expect<!> class Foo {
+    companion <!NO_ACTUAL_FOR_EXPECT{JVM}!>object<!>
+}
+
+// MODULE: m2-jvm()()(m1-common)
+// FILE: Foo.java
+@kotlin.annotations.jvm.KotlinActual public class Foo {
+}
