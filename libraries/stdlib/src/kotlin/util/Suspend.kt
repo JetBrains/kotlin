9f3c95e2945346e2396de77ad72e71eca3654668
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2024-09-22 20:01:14 +0000

    Document 'suspend' functional builder
    #KT-71570 Fixed
    
    
    Merge-request: KT-MR-18017
    Merged-by: Vsevolod Tolstopyatov <qwwdfsad@gmail.com>

--- libraries/stdlib/src/kotlin/util/Suspend.kt
+++ libraries/stdlib/src/kotlin/util/Suspend.kt
@@ -1,12 +1,26 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package kotlin
-import kotlin.internal.RequireKotlin
-import kotlin.internal.RequireKotlinVersionKind
 
+
+/**
+ * A functional type builder that ensures the given [block] has a `suspend` modifier and can be
+ * used as a suspend function.
+ *
+ * By default, functional type declarations are not inferred as `suspend` and cannot be used as such:
+ *
+ * ```
+ * suspend fun yield() {}
+ *
+ * // Inferred type: '() -> Unit'
+ * val regularBlock = { yield() } // Does not compile, `yield` requires to be in the suspend context
+ * // Inferred type: 'suspend () -> Unit'
+ * val suspendBlock = suspend { yield() } // Compiles as expected
+ * ```
+ */
 @kotlin.internal.InlineOnly
 @SinceKotlin("1.2")
 @Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
