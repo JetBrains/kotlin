5e5c1deb174ff872a81af8796b652139569ef288
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-09-23 01:23:17 +0300

    [K/N] Advance kotlin.native.Vector128 deprecation level to error #KT-71628

--- kotlin-native/runtime/src/main/kotlin/kotlin/native/simd.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/native/simd.kt
@@ -5,26 +5,24 @@
 package kotlin.native
 
 import kotlin.native.internal.GCUnsafeCall
-import kotlin.native.internal.TypedIntrinsic
-import kotlin.native.internal.IntrinsicType
 import kotlinx.cinterop.ExperimentalForeignApi
 
 
 @Deprecated("Use kotlinx.cinterop.Vector128 instead.", ReplaceWith("kotlinx.cinterop.Vector128"))
-@DeprecatedSinceKotlin(warningSince = "1.9")
+@DeprecatedSinceKotlin(warningSince = "1.9", errorSince = "2.1")
 @ExperimentalForeignApi
 public typealias Vector128 = kotlinx.cinterop.Vector128
 
-@Suppress("DEPRECATION")
+@Suppress("DEPRECATION_ERROR")
 @Deprecated("Use kotlinx.cinterop.vectorOf instead.", ReplaceWith("kotlinx.cinterop.vectorOf(f0, f1, f2, f3)"))
-@DeprecatedSinceKotlin(warningSince = "1.9")
+@DeprecatedSinceKotlin(warningSince = "1.9", errorSince = "2.1")
 @ExperimentalForeignApi
 @GCUnsafeCall("Kotlin_Interop_Vector4f_of")
 public external fun vectorOf(f0: Float, f1: Float, f2: Float, f3: Float): Vector128
 
-@Suppress("DEPRECATION")
+@Suppress("DEPRECATION_ERROR")
 @Deprecated("Use kotlinx.cinterop.vectorOf instead.", ReplaceWith("kotlinx.cinterop.vectorOf(f0, f1, f2, f3)"))
-@DeprecatedSinceKotlin(warningSince = "1.9")
+@DeprecatedSinceKotlin(warningSince = "1.9", errorSince = "2.1")
 @ExperimentalForeignApi
 @GCUnsafeCall("Kotlin_Interop_Vector4i32_of")
 public external fun vectorOf(f0: Int, f1: Int, f2: Int, f3: Int): Vector128
