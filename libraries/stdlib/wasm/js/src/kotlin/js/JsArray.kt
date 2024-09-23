2c587b547764959d4d5755f76b86d747ed9b5eca
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2024-09-23 17:28:22 +0200

    [Wasm][stdlib] Add extension methods for JS array conversion
    ^KT-71686 Fixed

--- libraries/stdlib/wasm/js/src/kotlin/js/JsArray.kt
+++ libraries/stdlib/wasm/js/src/kotlin/js/JsArray.kt
@@ -26,3 +26,33 @@ private fun <T : JsAny?> jsArrayGet(array: JsArray<T>, index: Int): T? =
 private fun <T : JsAny?> jsArraySet(array: JsArray<T>, index: Int, value: T) {
     js("array[index] = value")
 }
+
+/** Returns a new [Array] containing all the elements of this [JsArray]. */
+public fun <T : JsAny?> JsArray<T>.toArray(): Array<T> {
+    @Suppress("UNCHECKED_CAST", "TYPE_PARAMETER_AS_REIFIED")
+    return Array(this.length) { this[it] as T }
+}
+
+/** Returns a new [JsArray] containing all the elements of this [Array]. */
+public fun <T : JsAny?> Array<T>.toJsArray(): JsArray<T> {
+    val destination = JsArray<T>()
+    for (i in this.indices) {
+        destination[i] = this[i]
+    }
+    return destination
+}
+
+/** Returns a new [List] containing all the elements of this [JsArray]. */
+public fun <T : JsAny?> JsArray<T>.toList(): List<T> {
+    @Suppress("UNCHECKED_CAST")
+    return List(length) { this[it] as T }
+}
+
+/** Returns a new [JsArray] containing all the elements of this [List]. */
+public fun <T : JsAny?> List<T>.toJsArray(): JsArray<T> {
+    val destination = JsArray<T>()
+    for (i in this.indices) {
+        destination[i] = this[i]
+    }
+    return destination
+}
