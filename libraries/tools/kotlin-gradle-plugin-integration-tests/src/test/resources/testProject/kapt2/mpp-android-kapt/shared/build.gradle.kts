f141dc02a7cc3520fad97b16d8fbe6516d16c8f1
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-09-25 13:49:44 +0200

    [Kapt IT] Bump dagger version in mpp-android-kapt test data project

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kapt2/mpp-android-kapt/shared/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kapt2/mpp-android-kapt/shared/build.gradle.kts
@@ -18,14 +18,14 @@ kotlin {
     sourceSets {
         val androidMain by getting {
             dependencies {
-                implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-                implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
-                implementation("com.google.dagger:hilt-android:2.4.7")
+                implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+                implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+                implementation("com.google.dagger:hilt-android:2.51.1")
                 configurations.getByName("kapt").dependencies.add(
                     org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency(
                         "com.google.dagger",
                         "hilt-compiler",
-                        "2.47"
+                        "2.52"
                     )
                 )
             }
