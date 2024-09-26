75665655cb6f6a0d266321874fea82fbe834e33f
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-09-26 09:34:28 +0200

    [Native][tests] Add ExperimentalForeignApi opt-in for failing test
    ^KT-71333

--- compiler/testData/diagnostics/nativeTests/specialBackendChecks/objCInterop/t17.kt
+++ compiler/testData/diagnostics/nativeTests/specialBackendChecks/objCInterop/t17.kt
@@ -1,9 +1,17 @@
 // FIR_IDENTICAL
 // WITH_PLATFORM_LIBS
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.darwin.*
 import platform.Foundation.*
 
 class Foo : NSObject(), NSPortDelegateProtocol {
+    @OptIn(ExperimentalForeignApi::class)
+    // ^^^ Added opt-in because of the difference in NSPortDelegateProtocol.handlePortMessage() signature
+    //     on different platforms. On some targets, the signature uses experimental C-interop API.
+    //
+    //     Examples:
+    //     macos_arm64:         public open fun handlePortMessage(message: platform.Foundation.NSPortMessage)
+    //     ios_simulator_arm64: public open fun handlePortMessage(message: objcnames.classes.NSPortMessage)
     fun foo() {
         super.handlePortMessage(TODO())
     }
