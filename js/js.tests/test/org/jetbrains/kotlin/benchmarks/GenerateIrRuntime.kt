d1907254d2be08f991bffa6defc77737bee80911
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-09-27 19:13:32 +0200

    [Metadata] Use `MetadataVersion` instead of pure `String`

--- js/js.tests/test/org/jetbrains/kotlin/benchmarks/GenerateIrRuntime.kt
+++ js/js.tests/test/org/jetbrains/kotlin/benchmarks/GenerateIrRuntime.kt
@@ -252,7 +252,7 @@ class GenerateIrRuntime {
     fun runMonolithicDiskWriting() {
         val compilerVersion = KotlinCompilerVersion.getVersion()
         val abiVersion = KotlinAbiVersion.CURRENT
-        val metadataVersion = KLIB_LEGACY_METADATA_VERSION.toString()
+        val metadataVersion = KLIB_LEGACY_METADATA_VERSION
 
         val versions = KotlinLibraryVersioning(compilerVersion, abiVersion, metadataVersion)
         val file = createTempFile(directory = workingDir.toPath()).toFile()
@@ -272,7 +272,7 @@ class GenerateIrRuntime {
     fun runPerFileDiskWriting() {
         val compilerVersion = KotlinCompilerVersion.getVersion()
         val abiVersion = KotlinAbiVersion.CURRENT
-        val metadataVersion = KLIB_LEGACY_METADATA_VERSION.toString()
+        val metadataVersion = KLIB_LEGACY_METADATA_VERSION
 
         val versions = KotlinLibraryVersioning(compilerVersion, abiVersion, metadataVersion)
         val file = createTempFile(directory = workingDir.toPath()).toFile()
