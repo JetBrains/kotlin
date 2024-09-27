JetBrains/kotlin

commit 2bbc1e7ef75c9874ce9438155ee1364a3a785781

[IR] Expect-actual matcher: don't pull out static members
^KT-71577 Fixed

Similar to FirExpectActualMatchingContextImpl#collectAllMembers,
IrExpectActualMatchingContext#collectAllMembers should collect all non
static members

compiler/ir/ir.actualization/src/main/kotlin/org/jetbrains/kotlin/backend/common/actualizer/IrExpectActualMatchingContext.kt
@@ -254,7 +254,7 @@ internal abstract class IrExpectActualMatchingContext(
      *   has no sense in IR context
      */
     override fun RegularClassSymbolMarker.collectAllMembers(isActualDeclaration: Boolean): List<DeclarationSymbolMarker> {
-        return asIr().declarations.filterNot { it is IrAnonymousInitializer }.map { it.symbol }
+        return asIr().declarations.filter { it !is IrAnonymousInitializer && !it.isStaticFun() }.map { it.symbol }
     }
 
     override fun RegularClassSymbolMarker.getMembersForExpectClass(name: Name): List<DeclarationSymbolMarker> {
@@ -610,3 +610,5 @@ internal abstract class IrExpectActualMatchingContext(
         }
     }
 }
+
+private fun IrDeclaration.isStaticFun(): Boolean = this is IrSimpleFunction && isStatic

compiler/testData/diagnostics/tests/multiplatform/directJavaActualization/directJavaActualization_javaStatics.ll.kt
@@ -3,8 +3,8 @@
 
 // MODULE: m1-common
 // FILE: common.kt
-<!EXPECT_ACTUAL_INCOMPATIBILITY{JVM}, JAVA_DIRECT_ACTUAL_WITHOUT_EXPECT{JVM}!>expect<!> class Foo() {
-    fun <!NO_ACTUAL_FOR_EXPECT{JVM}!>foo<!>()
+expect class Foo() {
+    fun foo()
 }
 
 // MODULE: m2-jvm()()(m1-common)

commit b2ca2799a38d5bc2bdda7935db74565d22c9602a

[TEST] expect-actual. Reproduce partial linkage error in Wasm, JS, native
Reproduce KT-71817
JVM is not affected
Caused by: 2bbc1e7ef75c9874ce9438155ee1364a3a785781

    There are linkage errors reported by the partial linkage engine
    org.jetbrains.kotlin.analyzer.CompilationErrorException: There are linkage errors reported by the partial linkage engine
            at org.jetbrains.kotlin.backend.common.linkage.issues.KotlinIrLinkerIssue.raiseIssue(KotlinIrLinkerIssues.kt:29)
            at org.jetbrains.kotlin.backend.common.linkage.partial.PartialLinkageSupportForLinkerImpl.generateStubsAndPatchUsagesInternal(PartialLinkageSupportForLinkerImpl.kt:88)
            at org.jetbrains.kotlin.backend.common.linkage.partial.PartialLinkageSupportForLinkerImpl.generateStubsAndPatchUsages(PartialLinkageSupportForLinkerImpl.kt:62)
            at org.jetbrains.kotlin.backend.common.serialization.KotlinIrLinker.postProcess(KotlinIrLinker.kt:223)
            at org.jetbrains.kotlin.ir.backend.js.KlibKt.getIrModuleInfoForKlib(klib.kt:274)
            at org.jetbrains.kotlin.ir.backend.js.KlibKt.loadIr(klib.kt:213)
            at org.jetbrains.kotlin.ir.backend.js.KlibKt.loadIr$default(klib.kt:168)

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirBlackBoxCodegenBasedTestGenerated.java
@@ -39304,6 +39304,12 @@ public class LLFirBlackBoxCodegenBasedTestGenerated extends AbstractLLFirBlackBo
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectAnnotationAsTypeAlias.kt")
       public void testExpectAnnotationAsTypeAlias() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectAnnotationAsTypeAlias.kt");

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirReversedBlackBoxCodegenBasedTestGenerated.java
@@ -39304,6 +39304,12 @@ public class LLFirReversedBlackBoxCodegenBasedTestGenerated extends AbstractLLFi
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectAnnotationAsTypeAlias.kt")
       public void testExpectAnnotationAsTypeAlias() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectAnnotationAsTypeAlias.kt");

compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirLightTreeBlackBoxCodegenTestGenerated.java
@@ -39065,6 +39065,12 @@ public class FirLightTreeBlackBoxCodegenTestGenerated extends AbstractFirLightTr
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectAnnotationAsTypeAlias.kt")
       public void testExpectAnnotationAsTypeAlias() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectAnnotationAsTypeAlias.kt");

compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirPsiBlackBoxCodegenTestGenerated.java
@@ -39065,6 +39065,12 @@ public class FirPsiBlackBoxCodegenTestGenerated extends AbstractFirPsiBlackBoxCo
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectAnnotationAsTypeAlias.kt")
       public void testExpectAnnotationAsTypeAlias() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectAnnotationAsTypeAlias.kt");

compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt
@@ -1,0 +1,28 @@
+// IGNORE_BACKEND: WASM, JS, NATIVE, JS_IR, JS_IR_ES6
+// LANGUAGE: +MultiPlatformProjects
+
+// MODULE: common
+// TARGET_PLATFORM: Common
+// FILE: common.kt
+
+expect enum class E {
+    O, K;
+
+    fun values(): Int
+}
+
+fun common(): String {
+    return if (E.O.values() == 42) E.valueOf("O").name + E.valueOf("K").name else "NOT OK"
+}
+
+// MODULE: platform()()(common)
+// FILE: platform.kt
+
+actual enum class E {
+    O, K;
+
+    // Not to be confused with `static fun values`
+    actual fun values(): Int = 42
+}
+
+fun box(): String = common()

js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenBoxTestGenerated.java
@@ -28121,6 +28121,12 @@ public class FirJsCodegenBoxTestGenerated extends AbstractFirJsCodegenBoxTest {
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectNonExpectOverloads.kt")
       public void testExpectNonExpectOverloads() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectNonExpectOverloads.kt");

js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsES6CodegenBoxTestGenerated.java
@@ -28454,6 +28454,12 @@ public class FirJsES6CodegenBoxTestGenerated extends AbstractFirJsES6CodegenBoxT
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectNonExpectOverloads.kt")
       public void testExpectNonExpectOverloads() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectNonExpectOverloads.kt");

native/native.tests/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestGenerated.java
@@ -31038,6 +31038,12 @@ public class FirNativeCodegenBoxTestGenerated extends AbstractNativeCodegenBoxTe
         }
 
         @Test
+        @TestMetadata("enumStaticMethods.kt")
+        public void testEnumStaticMethods() {
+          runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+        }
+
+        @Test
         @TestMetadata("expectNonExpectOverloads.kt")
         public void testExpectNonExpectOverloads() {
           runTest("compiler/testData/codegen/box/multiplatform/k2/expectNonExpectOverloads.kt");

native/native.tests/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestNoPLGenerated.java
@@ -31728,6 +31728,12 @@ public class FirNativeCodegenBoxTestNoPLGenerated extends AbstractNativeCodegenB
         }
 
         @Test
+        @TestMetadata("enumStaticMethods.kt")
+        public void testEnumStaticMethods() {
+          runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+        }
+
+        @Test
         @TestMetadata("expectNonExpectOverloads.kt")
         public void testExpectNonExpectOverloads() {
           runTest("compiler/testData/codegen/box/multiplatform/k2/expectNonExpectOverloads.kt");

wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxTestGenerated.java
@@ -27953,6 +27953,12 @@ public class FirWasmJsCodegenBoxTestGenerated extends AbstractFirWasmJsCodegenBo
       }
 
       @Test
+      @TestMetadata("enumStaticMethods.kt")
+      public void testEnumStaticMethods() {
+        runTest("compiler/testData/codegen/box/multiplatform/k2/enumStaticMethods.kt");
+      }
+
+      @Test
       @TestMetadata("expectNonExpectOverloads.kt")
       public void testExpectNonExpectOverloads() {
         runTest("compiler/testData/codegen/box/multiplatform/k2/expectNonExpectOverloads.kt");