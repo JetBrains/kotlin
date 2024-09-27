a1d349d0ae4ce3f7bd10e025a88bcff51d67ef7d
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-09-27 17:50:19 +0200

    [Metadata] Add a TODO note for later to drop special K2 check

--- core/metadata.jvm/src/org/jetbrains/kotlin/metadata/jvm/deserialization/JvmMetadataVersion.kt
+++ core/metadata.jvm/src/org/jetbrains/kotlin/metadata/jvm/deserialization/JvmMetadataVersion.kt
@@ -29,6 +29,7 @@ class JvmMetadataVersion(versionArray: IntArray, val isStrictSemantics: Boolean)
     }
 
     fun isCompatible(metadataVersionFromLanguageVersion: JvmMetadataVersion): Boolean {
+        // TODO delete after K1 is dropped 
         // Special case for bootstrap: 1.8 can read 2.0
         if (major == 2 && minor == 0 && INSTANCE.major == 1 && INSTANCE.minor == 8) return true
         val limitVersion = metadataVersionFromLanguageVersion.lastSupportedVersionWithThisLanguageVersion(isStrictSemantics)
