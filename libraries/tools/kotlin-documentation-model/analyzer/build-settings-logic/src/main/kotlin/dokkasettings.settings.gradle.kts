f7d85f6303ec28fbdd5e741f0ecfcec71aa7579b
Oleg Yukhnevich <whyoleg@gmail.com>
2024-09-30 15:09:05 +0300

    [KDM] Refactor `build-settings-logic` to expose single plugin
    See https://github.com/Kotlin/dokka/pull/3834
    
    * `dokkasettings` plugin should be applied to all projects in `settings.gradle.kts`
    * content of `dokkasettings` is copied to `build-settings-logic` itself
    * remove usage of user/password for build cache and use `develocity` extension
    * move foojay toolchains there to share it
    Original commit: https://github.com/Kotlin/dokka/commit/1ba220d7aa3b03a39059bf7419faf9f1063196c3

--- libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/src/main/kotlin/dokkasettings.settings.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/src/main/kotlin/dokkasettings.settings.gradle.kts
@@ -2,55 +2,20 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import org.gradle.api.initialization.resolve.RepositoriesMode.PREFER_SETTINGS
+// Based on https://github.com/JetBrains/kotlin/blob/c20f644ee4cd8d28b39b12ea5304b68c5639e531/repo/gradle-settings-conventions/develocity/src/main/kotlin/develocity.settings.gradle.kts
+// Because Dokka uses Composite Builds, Build Cache must be configured consistently on:
+// - the root settings.gradle.kts,
+// - and the settings.gradle.kts of any projects added with `pluginManagement { includedBuild("...") }`
+// The Content of this file should be kept in sync with the content at the end of:
+//   `build-settings-logic/settings.gradle.kts`
+// useful links:
+// - develocity: https://docs.gradle.com/develocity/gradle-plugin/
+// - build cache: https://docs.gradle.org/8.4/userguide/build_cache.html#sec:build_cache_composite
 
-rootProject.name = "build-settings-logic"
-
-pluginManagement {
-    repositories {
-        maven("https://cache-redirector.jetbrains.com/repo.maven.apache.org/maven2") {
-            name = "MavenCentral-JBCache"
-        }
-        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2") {
-            name = "GradlePluginPortal-JBCache"
-        }
-    }
-}
-
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode = PREFER_SETTINGS
-    repositories {
-        maven("https://cache-redirector.jetbrains.com/repo.maven.apache.org/maven2") {
-            name = "MavenCentral-JBCache"
-        }
-        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2") {
-            name = "GradlePluginPortal-JBCache"
-        }
-    }
-
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
-
-// We have to make sure the build-cache config is consistent in the settings.gradle.kts
-// files of all projects. The natural way to share logic is with a convention plugin,
-// but we can't apply a convention plugin in build-settings-logic to itself, so we just copy it.
-// We could publish build-settings-logic to a Maven repo? But this is quicker and easier.
-// The following content should be kept in sync with the content of:
-//   `src/main/kotlin/dokkasettings.settings.gradle.kts`
-// The only difference with the script above is explicitly specified versions
-
-//region copy of src/main/kotlin/dokkasettings.settings.gradle.kts
-
-// version should be kept in sync with `gradle/libs.versions.toml`
 plugins {
-    id("com.gradle.develocity") version "3.17.6"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0.2" apply false
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.gradle.develocity")
+    id("com.gradle.common-custom-user-data-gradle-plugin") apply false
+    id("org.gradle.toolchains.foojay-resolver-convention")
 }
 
 //region properties
@@ -71,12 +36,9 @@ val buildScanEnabled: Provider<Boolean> =
     dokkaProperty("build.scan.enabled", String::toBoolean)
         .orElse(buildingOnCi)
 
-val BUILD_SCAN_USERNAME_DEFAULT = "<default>"
-
 /** Optionally override the default name attached to a Build Scan. */
 val buildScanUsername: Provider<String> =
     dokkaProperty("build.scan.username")
-        .orElse(BUILD_SCAN_USERNAME_DEFAULT)
         .map(String::trim)
 
 develocity {
@@ -100,6 +62,12 @@ develocity {
         }
 
         val overriddenName = buildScanUsername.orNull
+        // we need to assign properties' values to local variables to avoid issues with Gradle Configuration Cache.
+        // if we don't do it, by using those properties in `username { ... }` we catch `this` in closure
+        // and Gradle Configuration Cache doesn't allow to catch implicit `this` object
+        val buildingOnTeamCity = buildingOnTeamCity
+        val buildingOnGitHub = buildingOnGitHub
+        val buildingOnCi = buildingOnCi
         obfuscation {
             ipAddresses { _ -> listOf("0.0.0.0") }
             hostname { _ -> "concealed" }
@@ -109,8 +77,7 @@ develocity {
                     buildingOnGitHub -> "GitHub"
                     buildingOnCi -> "CI"
                     !overriddenName.isNullOrBlank() -> overriddenName
-                    overriddenName == BUILD_SCAN_USERNAME_DEFAULT -> originalUsername
-                    else -> "unknown"
+                    else -> originalUsername
                 }
             }
         }
@@ -139,6 +106,4 @@ buildCache {
         isPush = buildCachePushEnabled.get()
     }
 }
-//endregion
-
 //endregion
