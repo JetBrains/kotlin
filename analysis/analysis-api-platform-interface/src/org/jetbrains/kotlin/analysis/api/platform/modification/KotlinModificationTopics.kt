smileding/kotlin-2.0.0

Provide topics for scripts sessions invalidations
^KT-71540

analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/modification/KotlinModificationTopics.kt
@@ -104,6 +104,9 @@ public object KotlinModificationTopics {
 
     public val CODE_FRAGMENT_CONTEXT_MODIFICATION: Topic<KotlinCodeFragmentContextModificationListener> =
         Topic(KotlinCodeFragmentContextModificationListener::class.java, Topic.BroadcastDirection.TO_CHILDREN, true)
+
+    public val SCRIPTS_STATE_MODIFICATION: Topic<KotlinScriptsStateModificationListener> =
+        Topic(KotlinScriptsStateModificationListener::class.java, Topic.BroadcastDirection.TO_CHILDREN, true)
 }
 
 /**

JetBrains/kotlin

analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/modification/KotlinModificationTopics.kt
@@ -104,6 +104,9 @@ public object KotlinModificationTopics {
 
     public val CODE_FRAGMENT_CONTEXT_MODIFICATION: Topic<KotlinCodeFragmentContextModificationListener> =
         Topic(KotlinCodeFragmentContextModificationListener::class.java, Topic.BroadcastDirection.TO_CHILDREN, true)
+
+    public val GLOBAL_SCRIPT_MODULE_STATE_MODIFICATION: Topic<KotlinGlobalScriptModuleStateModificationListener> =
+        Topic(KotlinGlobalScriptModuleStateModificationListener::class.java, Topic.BroadcastDirection.TO_CHILDREN, true)
 }
 
 /**