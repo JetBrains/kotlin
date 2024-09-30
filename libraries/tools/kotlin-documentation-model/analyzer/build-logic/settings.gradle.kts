a84dfdd45da3bbc9aba91b4ba0192c042e74beec
Oleg Yukhnevich <whyoleg@gmail.com>
2024-09-30 15:09:05 +0300

    [KDM] Refactor `build-settings-logic` to expose single plugin
    See https://github.com/Kotlin/dokka/pull/3834
    
    * `dokkasettings` plugin should be applied to all projects in `settings.gradle.kts`
    * content of `dokkasettings` is copied to `build-settings-logic` itself
    * remove usage of user/password for build cache and use `develocity` extension
    * move foojay toolchains there to share it
    Original commit: https://github.com/Kotlin/dokka/commit/1ba220d7aa3b03a39059bf7419faf9f1063196c3

--- libraries/tools/kotlin-documentation-model/analyzer/build-logic/settings.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/settings.gradle.kts
@@ -38,6 +38,5 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-    id("dokkasettings.build-cache")
+    id("dokkasettings")
 }
