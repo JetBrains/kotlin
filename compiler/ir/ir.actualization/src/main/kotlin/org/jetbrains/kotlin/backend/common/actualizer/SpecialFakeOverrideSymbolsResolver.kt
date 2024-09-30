8f93e4b0d5e2737318c22a4735b3392fb929bffd
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2024-09-30 15:24:28 +0200

    [Ir.Actualizer] Fix property to field actualization
    In before, the link to temporary field inside f/o property
    was left in tree. In fact, this doesn't break anything in
    normal compilation, because of the backend ability to work
    with slightly incorrect IR.
    
    But it can become a problem after some stdlib code changes,
    or when -Xserialize-ir is enabled.
    
    ^KT-71826 Fixed

--- compiler/ir/ir.actualization/src/main/kotlin/org/jetbrains/kotlin/backend/common/actualizer/SpecialFakeOverrideSymbolsResolver.kt
+++ compiler/ir/ir.actualization/src/main/kotlin/org/jetbrains/kotlin/backend/common/actualizer/SpecialFakeOverrideSymbolsResolver.kt
@@ -19,6 +19,7 @@ import org.jetbrains.kotlin.ir.util.SymbolRemapper
 import org.jetbrains.kotlin.ir.util.isGetter
 import org.jetbrains.kotlin.ir.util.isSetter
 import org.jetbrains.kotlin.ir.util.render
+import org.jetbrains.kotlin.ir.util.resolveFakeOverride
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.IrElementVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
@@ -242,7 +243,7 @@ class SpecialFakeOverrideSymbolsActualizedByFieldsTransformer(
         val originalAccessorSymbol = fakeOverrideAccessorSymbol.originalSymbol
         val actualizedClassSymbol = fakeOverrideAccessorSymbol.containingClassSymbol
 
-        val actualFieldSymbol = expectActualMap.propertyAccessorsActualizedByFields[originalAccessorSymbol]?.owner?.backingField?.symbol
+        val actualFieldSymbol = expectActualMap.propertyAccessorsActualizedByFields[originalAccessorSymbol]?.owner?.resolveFakeOverride()?.backingField?.symbol
             ?: error("No override for $originalAccessorSymbol in $actualizedClassSymbol")
 
         return when {
