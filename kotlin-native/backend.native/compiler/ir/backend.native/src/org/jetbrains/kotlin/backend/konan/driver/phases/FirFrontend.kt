81f34b5eaf8921f50311dd4ee74614fc8ec8ed35
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2024-10-01 11:10:59 +0200

    [K/N] assert the FE invoked matches the language version.
    Except for the case of empty sources for technical debt reasons.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/FirFrontend.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/driver/phases/FirFrontend.kt
@@ -10,8 +10,10 @@ import org.jetbrains.kotlin.backend.konan.driver.PhaseContext
 import org.jetbrains.kotlin.backend.konan.driver.PhaseEngine
 import org.jetbrains.kotlin.backend.konan.firFrontendWithPsi
 import org.jetbrains.kotlin.backend.konan.firFrontendWithLightTree
+import org.jetbrains.kotlin.cli.common.config.kotlinSourceRoots
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
+import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.fir.pipeline.FirResult
 
 sealed class FirOutput {
@@ -32,5 +34,11 @@ internal val FIRPhase = createSimpleNamedCompilerPhase(
 }
 
 internal fun <T : PhaseContext> PhaseEngine<T>.runFirFrontend(environment: KotlinCoreEnvironment): FirOutput {
+    val languageVersion = environment.configuration.languageVersionSettings.languageVersion
+    val kotlinSourceRoots = environment.configuration.kotlinSourceRoots
+    if (!languageVersion.usesK2 && kotlinSourceRoots.isNotEmpty()) {
+        throw Error("Attempt to run K1 from unsupported LV=${languageVersion}")
+    }
+
     return this.runPhase(FIRPhase, environment)
 }
