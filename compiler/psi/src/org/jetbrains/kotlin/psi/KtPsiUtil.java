06720eac12ac6a423ed2e5cfac38e12d728725f0
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-10-03 13:12:13 +0200

    [PSI] KtPsiUtil: do not iterate through directories
    Fix for KT-71565 hid this problem as now we use stubs to calculate
    `isLocal` where it is possible. But the main contributor to
    this performance problem is `getStubOrPsiParent` on `PsiDirectoryImpl`
    as we iterate the entire directory tree up to the root.
    
    ^KT-71973 Fixed

--- compiler/psi/src/org/jetbrains/kotlin/psi/KtPsiUtil.java
+++ compiler/psi/src/org/jetbrains/kotlin/psi/KtPsiUtil.java
@@ -828,6 +828,11 @@ public class KtPsiUtil {
         PsiElement current = PsiTreeUtil.getStubOrPsiParent(declaration);
         boolean isNonLocalCallable = isNonLocalCallable(declaration);
         while (current != null) {
+            // No enclosing declaration found. There no sense to iterate through directories
+            if (current instanceof PsiFile) {
+                return null;
+            }
+
             PsiElement parent = PsiTreeUtil.getStubOrPsiParent(current);
             if (parent instanceof KtScript) return null;
             if (current instanceof KtAnonymousInitializer) {
