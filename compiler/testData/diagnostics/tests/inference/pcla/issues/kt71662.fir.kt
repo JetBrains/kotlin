3aa2930d2d7e0d76661d12e0860a826ec550cbea
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-10-04 15:52:28 +0200

    PCLA: Allow semi-fixing to constraints containing non-outer-CS variables
    Looks like the limitation was introduced initially just to play safe,
    but it seems there's no much sense to have it as it prevents quite
    natural things from working well.
    
    ^KT-71662 Fixed

--- compiler/testData/diagnostics/tests/inference/pcla/issues/kt71662.fir.kt
+++ compiler/testData/diagnostics/tests/inference/pcla/issues/kt71662.fir.kt
@@ -2,7 +2,7 @@
 fun reproduce() {
     val x = pcla { otvOwner ->
         otvOwner.constrain(GenericKlass(TypeArgument))
-        <!BUILDER_INFERENCE_STUB_RECEIVER!>otvOwner.provide()<!>.fix()
+        otvOwner.provide().fix()
     }
 
     <!DEBUG_INFO_EXPRESSION_TYPE("GenericKlass<TypeArgument>")!>x<!>
