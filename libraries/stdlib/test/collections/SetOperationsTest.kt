32f7404b2aa191d8cfa8f5956b52404012d6c0e6
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-10-07 11:51:13 -0400

    fixup! KT-71822 Correctly intersect with identity set

--- libraries/stdlib/test/collections/SetOperationsTest.kt
+++ libraries/stdlib/test/collections/SetOperationsTest.kt
@@ -33,8 +33,66 @@ class SetOperationsTest {
     @Test fun intersect() {
         assertTrue(listOf(1, 3).intersect(listOf(5)).none())
         assertEquals(listOf(5), listOf(1, 3, 5).intersect(listOf(5)).toList())
-        assertEquals(listOf(1, 3, 5), listOf(1, 3, 5).intersect(listOf(1, 3, 5)).toList())
+        assertEquals(listOf(1, 3, 5), listOf(1, 3, 5).intersect(listOf(5, 3, 1)).toList())
         assertTrue(listOf<Int>().intersect(listOf(1)).none())
+    }
+
+    @Test fun intersectIntArray() {
+        assertTrue(intArrayOf(1, 3).intersect(listOf(5)).none())
+        assertEquals(setOf(5), intArrayOf(1, 3, 5).intersect(listOf(5)))
+        assertEquals(listOf(1, 3, 5), intArrayOf(1, 3, 5).intersect(listOf(5, 3, 1)).toList())
+        assertTrue(intArrayOf().intersect(listOf(1)).none())
+    }
+
+    @Test fun intersectLongArray() {
+        assertTrue(longArrayOf(1L, 3L).intersect(listOf(5L)).none())
+        assertEquals(setOf(5L), longArrayOf(1L, 3L, 5L).intersect(listOf(5L)))
+        assertEquals(listOf(1L, 3L, 5L), longArrayOf(1L, 3L, 5L).intersect(listOf(5L, 3L, 1L)).toList())
+        assertTrue(longArrayOf().intersect(listOf(1L)).none())
+    }
+
+    @Test fun intersectShortArray() {
+        assertTrue(shortArrayOf(1, 3).intersect(listOf(5)).none())
+        assertEquals(setOf(5.toShort()), shortArrayOf(1, 3, 5).intersect(listOf(5)))
+        assertEquals(listOf(1, 3, 5).map(Int::toShort), shortArrayOf(1, 3, 5).intersect(listOf(5, 3, 1)).toList())
+        assertTrue(shortArrayOf().intersect(listOf(1)).none())
+    }
+
+    @Test fun intersectByteArray() {
+        assertTrue(byteArrayOf(1, 3).intersect(listOf(5)).none())
+        assertEquals(setOf(5.toByte()), byteArrayOf(1, 3, 5).intersect(listOf(5)))
+        assertEquals(listOf(1, 3, 5).map(Int::toByte), byteArrayOf(1, 3, 5).intersect(listOf(5, 3, 1)).toList())
+        assertTrue(byteArrayOf().intersect(listOf(1)).none())
+    }
+
+    @Test fun intersectCharArray() {
+        assertTrue(charArrayOf('a', 'c').intersect(listOf('b')).none())
+        assertEquals(setOf('e'), charArrayOf('a', 'c', 'e').intersect(listOf('e')))
+        assertEquals(listOf('a', 'c', 'e'), charArrayOf('a', 'c', 'e').intersect(listOf('e', 'c', 'a')).toList())
+        assertTrue(charArrayOf().intersect(listOf('x')).none())
+    }
+
+    @Test fun intersectFloatArray() {
+        assertTrue(floatArrayOf(1f, 3f).intersect(listOf(5f)).none())
+        assertEquals(setOf(5f), floatArrayOf(1f, 3f, 5f).intersect(listOf(5f)))
+        assertEquals(listOf(1f, 3f, 5f), floatArrayOf(1f, 3f, 5f).intersect(listOf(5f, 3f, 1f)).toList())
+        assertTrue(floatArrayOf().intersect(listOf(1f)).none())
+    }
+
+    @Test fun intersectDoubleArray() {
+        assertTrue(doubleArrayOf(1.0, 3.0).intersect(listOf(5.0)).none())
+        assertEquals(setOf(5.0), doubleArrayOf(1.0, 3.0, 5.0).intersect(listOf(5.0)))
+        assertEquals(listOf(1.0, 3.0, 5.0), doubleArrayOf(1.0, 3.0, 5.0).intersect(listOf(5.0, 3.0, 1.0)).toList())
+        assertTrue(doubleArrayOf().intersect(listOf(1.0)).none())
+    }
+
+    @Test fun intersectArray() {
+        data class O(val x: Int)
+
+        assertTrue(arrayOf(O(1), O(3)).intersect(listOf(O(5))).none())
+        assertEquals(setOf(O(5)), arrayOf(O(1), O(3), O(5)).intersect(listOf(O(5))))
+        assertEquals(listOf(O(1), O(3), O(5)), arrayOf(O(1), O(3), O(5)).intersect(listOf(O(5), O(3), O(1))).toList())
+        assertTrue(arrayOf<O>().intersect(listOf(1)).none())
     }
 
     @Test fun intersectWithIdentitySet() {
@@ -47,6 +105,18 @@ class SetOperationsTest {
         assertEquals(setOf(b, c), listOf(a, b, c).intersect(IdentitySet(b, c)))
         assertEquals(setOf(b, c), listOf(c, b, a).intersect(IdentitySet(b, c)))
         assertEquals(setOf(b), listOf(b, c).intersect(IdentitySet(b, a)))
+    }
+
+    @Test fun intersectArrayWithIdentitySet() {
+        data class Obj(val x: Int)
+
+        val a = Obj(1)
+        val b = Obj(2)
+        val c = Obj(1)
+
+        assertEquals(setOf(b, c), arrayOf(a, b, c).intersect(IdentitySet(b, c)))
+        assertEquals(setOf(b, c), arrayOf(c, b, a).intersect(IdentitySet(b, c)))
+        assertEquals(setOf(b), arrayOf(b, c).intersect(IdentitySet(b, a)))
     }
 
     fun testPlus(doPlus: (Set<String>) -> Set<String>) {
