6c032e5e8edf0e282af5cce5a25b1a839fa159c8
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2024-10-09 13:50:17 +0300

    [KDF] Add empty test for debugging purposes
    Moved from https://github.com/Kotlin/dataframe/commit/883d45146d5711815175b09f585e79ef308166e0

---
+++ plugins/kotlin-dataframe/testData/box/playground.kt
@@ -0,0 +1,9 @@
+import org.jetbrains.kotlinx.dataframe.*
+import org.jetbrains.kotlinx.dataframe.annotations.*
+import org.jetbrains.kotlinx.dataframe.api.*
+import org.jetbrains.kotlinx.dataframe.io.*
+
+fun box(): String {
+
+    return "OK"
+}
