af1be4d0a311b8d97988c248db2dc29a9f655c30
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-10-09 18:08:03 +0200

    [K/N] Migrate NativeGenerationState.calledFromExportedInlineFunctions to IR attribute
    as isCalledFromExportedInlineFunction
    
    KT-69082

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/NativeIrAttributes.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/NativeIrAttributes.kt
@@ -6,6 +6,10 @@
 package org.jetbrains.kotlin.backend.konan
 
 import org.jetbrains.kotlin.ir.declarations.IrAttributeContainer
+import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.irAttribute
+import org.jetbrains.kotlin.ir.irFlag
 
 var IrAttributeContainer.localClassName: String? by irAttribute(followAttributeOwner = true)
+
+var IrFunction.isCalledFromExportedInlineFunction: Boolean by irFlag(followAttributeOwner = false)
