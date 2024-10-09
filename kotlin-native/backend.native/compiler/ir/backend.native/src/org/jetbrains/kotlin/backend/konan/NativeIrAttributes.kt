9eef29e155cb1a7a380d9b46ac62ea54cfdf6315
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-10-09 18:11:04 +0200

    [K/N] Migrate NativeGenerationState.constructedFromExportedInlineFunctions to IR attribute
    as isConstructedFromExportedInlineFunctions
    
    KT-69082

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/NativeIrAttributes.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/NativeIrAttributes.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.backend.konan
 
 import org.jetbrains.kotlin.ir.declarations.IrAttributeContainer
+import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.irAttribute
 import org.jetbrains.kotlin.ir.irFlag
@@ -13,3 +14,5 @@ import org.jetbrains.kotlin.ir.irFlag
 var IrAttributeContainer.localClassName: String? by irAttribute(followAttributeOwner = true)
 
 var IrFunction.isCalledFromExportedInlineFunction: Boolean by irFlag(followAttributeOwner = false)
+
+var IrClass.isConstructedFromExportedInlineFunctions: Boolean by irFlag(followAttributeOwner = false)
--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/CacheInfoBuilder.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/CacheInfoBuilder.kt
@@ -13,6 +13,7 @@ import org.jetbrains.kotlin.backend.konan.KonanFqNames
 import org.jetbrains.kotlin.ir.objcinterop.isExternalObjCClass
 import org.jetbrains.kotlin.backend.konan.isFunctionInterfaceFile
 import org.jetbrains.kotlin.backend.konan.isCalledFromExportedInlineFunction
+import org.jetbrains.kotlin.backend.konan.isConstructedFromExportedInlineFunctions
 import org.jetbrains.kotlin.backend.konan.serialization.KonanManglerIr
 import org.jetbrains.kotlin.backend.konan.serialization.KonanPartialModuleDeserializer
 import org.jetbrains.kotlin.ir.IrElement
@@ -96,9 +97,7 @@ internal class CacheInfoBuilder(
             private fun processFunction(function: IrFunction) {
                 if (generationState.context.irLinker.getCachedDeclarationModuleDeserializer(function) == null) {
                     function.isCalledFromExportedInlineFunction = true
-                    (function as? IrConstructor)?.constructedClass?.let {
-                        generationState.constructedFromExportedInlineFunctions.add(it)
-                    }
+                    (function as? IrConstructor)?.constructedClass?.isConstructedFromExportedInlineFunctions = true
                     if (function.isInline && !function.isExported) {
                         // An exported inline function calls a non-exported inline function:
                         // should track its callees as well as it won't be handled by the main visitor.
