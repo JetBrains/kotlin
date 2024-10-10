c29a437a1bc2465df187ecff888505a0d6620437
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-10-10 11:58:16 +0200

    [Gradle] Add regression test for KT-71549

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/DependencyLockingIT.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle
+
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.jupiter.api.DisplayName
+
+class DependencyLockingIT : KGPBaseTest() {
+    @JvmGradlePluginTests
+    @DisplayName("KT-71549: dependency locking does not cause build failure")
+    @TestMetadata("jvm-with-dependency-locking")
+    @GradleTest
+    fun testJvmDependencyLocking(gradleVersion: GradleVersion) {
+        project("jvm-with-dependency-locking", gradleVersion) {
+            build("compileKotlin", "--write-locks")
+            build("compileKotlin")
+        }
+    }
+}
