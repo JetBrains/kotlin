48fe7ab01d948dec0d9c89cdebf269f70857b72c
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-10-10 22:16:24 +0200

    [Gradle] Remove the rest of intellij dependencies
    ^KT-70252 In Progress
    ^KTI-1825 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/stringUtils.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/stringUtils.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.gradle.utils
 
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.capitalizeAsciiOnly
 import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
+import kotlin.text.toUpperCase
 
 internal fun lowerCamelCaseName(vararg nameParts: String?): String {
     val nonEmptyParts = nameParts.mapNotNull { it?.takeIf(String::isNotEmpty) }
@@ -57,3 +58,58 @@ internal fun Appendable.appendLine(): Appendable =
     append('\n')
 
 internal fun String.removingTrailingNewline(): String = this.dropLastWhile { it == '\n' }
+
+// copied from IJ
+@OptIn(ExperimentalStdlibApi::class)
+internal fun escapeStringCharacters(
+    str: String,
+    length: Int = str.length,
+    buffer: StringBuilder = StringBuilder(length),
+    additionalChars: String? = "\"",
+    escapeSlash: Boolean = true,
+    escapeUnicode: Boolean = true,
+): String {
+    var prev = 0.toChar()
+    for (idx in 0 until length) {
+        val ch = str.get(idx)
+        when (ch) {
+            '\b' -> buffer.append("\\b")
+            '\t' -> buffer.append("\\t")
+            '\n' -> buffer.append("\\n")
+            '\u000c' -> buffer.append("\\f")
+            '\r' -> buffer.append("\\r")
+            else -> if (escapeSlash && ch == '\\') {
+                buffer.append("\\\\")
+            } else if (additionalChars != null && additionalChars.indexOf(ch) > -1 && (escapeSlash || prev != '\\')) {
+                buffer.append("\\").append(ch)
+            } else if (escapeUnicode && !isPrintableUnicode(ch)) {
+                val hexCode: CharSequence = Integer.toHexString(ch.code).toUpperCase()
+                buffer.append("\\u")
+                var paddingCount = 4 - hexCode.length
+                while (paddingCount-- > 0) {
+                    buffer.append(0)
+                }
+                buffer.append(hexCode)
+            } else {
+                buffer.append(ch)
+            }
+        }
+        prev = ch
+    }
+    return buffer.toString()
+}
+
+// copied from IJ
+private fun isPrintableUnicode(c: Char): Boolean {
+    val t = Character.getType(c).toByte()
+    val block = Character.UnicodeBlock.of(c)
+    return t != Character.UNASSIGNED &&
+            t != Character.LINE_SEPARATOR &&
+            t != Character.PARAGRAPH_SEPARATOR &&
+            t != Character.CONTROL &&
+            t != Character.FORMAT &&
+            t != Character.PRIVATE_USE &&
+            t != Character.SURROGATE &&
+            block != Character.UnicodeBlock.VARIATION_SELECTORS &&
+            block != Character.UnicodeBlock.VARIATION_SELECTORS_SUPPLEMENT
+}
