faccf14fd377cd1dd732f7f3936a886b0b6bf050
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2024-10-10 17:44:06 +0200

    fixup! [PSI2FIR] always recreate annotation for copy method parameters
    More tests

---
+++ analysis/low-level-api-fir/testData/lazyResolve/withTestCompilerPluginEnabled/dataClassProperty.kt
@@ -0,0 +1,15 @@
+// WITH_FIR_TEST_COMPILER_PLUGIN
+// SKIP_WHEN_OUT_OF_CONTENT_ROOT
+// MEMBER_NAME_FILTER: p1
+package pack
+
+annotation class Anno(val value: Int)
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class PropertyAnno(val s: String)
+
+@Anno(0)
+data cl<caret>ass MyData @Anno(3) constructor(
+    @Anno(1) @PropertyAnno("str") val p1: String,
+    @param:Anno(2) val p2: Int,
+)
--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/FirSourceLazyDeclarationResolveTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/FirSourceLazyDeclarationResolveTestGenerated.java
@@ -1069,6 +1069,30 @@ public class FirSourceLazyDeclarationResolveTestGenerated extends AbstractFirSou
       }
 
       @Test
+      @TestMetadata("dataClassComponent1.kt")
+      public void testDataClassComponent1() {
+        runTest("analysis/low-level-api-fir/testData/lazyResolve/classes/dataClass/dataClassComponent1.kt");
+      }
+
+      @Test
+      @TestMetadata("dataClassConstructor.kt")
+      public void testDataClassConstructor() {
+        runTest("analysis/low-level-api-fir/testData/lazyResolve/classes/dataClass/dataClassConstructor.kt");
+      }
+
+      @Test
+      @TestMetadata("dataClassCopy.kt")
+      public void testDataClassCopy() {
+        runTest("analysis/low-level-api-fir/testData/lazyResolve/classes/dataClass/dataClassCopy.kt");
+      }
+
+      @Test
+      @TestMetadata("dataClassProperty.kt")
+      public void testDataClassProperty() {
+        runTest("analysis/low-level-api-fir/testData/lazyResolve/classes/dataClass/dataClassProperty.kt");
+      }
+
+      @Test
       @TestMetadata("parameterTypeCollision.kt")
       public void testParameterTypeCollision() {
         runTest("analysis/low-level-api-fir/testData/lazyResolve/classes/dataClass/parameterTypeCollision.kt");
@@ -1702,9 +1726,33 @@ public class FirSourceLazyDeclarationResolveTestGenerated extends AbstractFirSou
     }
 
     @Test
+    @TestMetadata("dataClassComponent1.kt")
+    public void testDataClassComponent1() {
+      runTest("analysis/low-level-api-fir/testData/lazyResolve/withTestCompilerPluginEnabled/dataClassComponent1.kt");
+    }
+
+    @Test
+    @TestMetadata("dataClassConstructor.kt")
+    public void testDataClassConstructor() {
+      runTest("analysis/low-level-api-fir/testData/lazyResolve/withTestCompilerPluginEnabled/dataClassConstructor.kt");
+    }
+
+    @Test
+    @TestMetadata("dataClassCopy.kt")
+    public void testDataClassCopy() {
+      runTest("analysis/low-level-api-fir/testData/lazyResolve/withTestCompilerPluginEnabled/dataClassCopy.kt");
+    }
+
+    @Test
     @TestMetadata("dataClassCopyAnnotation.kt")
     public void testDataClassCopyAnnotation() {
       runTest("analysis/low-level-api-fir/testData/lazyResolve/withTestCompilerPluginEnabled/dataClassCopyAnnotation.kt");
+    }
+
+    @Test
+    @TestMetadata("dataClassProperty.kt")
+    public void testDataClassProperty() {
+      runTest("analysis/low-level-api-fir/testData/lazyResolve/withTestCompilerPluginEnabled/dataClassProperty.kt");
     }
 
     @Test
