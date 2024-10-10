313f7bbc664fe7f63bc368fb0973fe78adf35bca
Kirill Rakhman <Kirill.Rakhman@jetbrains.com>
2024-10-10 09:12:32 +0000

    fixup! [FIR] Describe how to enable an `UNSUPPORTED_FEATURE`

--- generators/tests/org/jetbrains/kotlin/generators/arguments/GenerateCompilerArgumentsCopyTest.kt
+++ generators/tests/org/jetbrains/kotlin/generators/arguments/GenerateCompilerArgumentsCopyTest.kt
@@ -14,7 +14,7 @@ import kotlin.test.assertContentEquals
 class GenerateCompilerArgumentsCopyTest : TestCase() {
     fun testCompilerArgumentsCopyFunctionsAreUpToDate() {
         generateCompilerArgumentsCopy(::getPrinterForTests)
-        generateConfigureCommonLanguageFeatures(::getPrinterToFile)
+        generateConfigureCommonLanguageFeatures(::getPrinterForTests)
     }
 
     fun testCopyDoesNotCopyTransientFields() {
