5240c3d220ab9d3fe86c8e6d463504735c7222f3
Stanislav Ruban <stanislav.ruban@jetbrains.com>
2024-10-10 10:10:46 +0300

    [tests] Add & reorganize test data for root package entities in non-root packages
    #KT-69985
    #KT-72173
    #KT-69986
    #KT-72170
    #KT-72171

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGenerated.java
@@ -15885,12 +15885,6 @@ public class FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGene
       }
 
       @Test
-      @TestMetadata("ImportFromRootPackage.kt")
-      public void testImportFromRootPackage() {
-        runTest("compiler/testData/diagnostics/tests/imports/ImportFromRootPackage.kt");
-      }
-
-      @Test
       @TestMetadata("importFunctionWithAllUnderImport.kt")
       public void testImportFunctionWithAllUnderImport() {
         runTest("compiler/testData/diagnostics/tests/imports/importFunctionWithAllUnderImport.kt");
@@ -16041,12 +16035,6 @@ public class FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGene
       }
 
       @Test
-      @TestMetadata("noImportFromRootPackage.kt")
-      public void testNoImportFromRootPackage() {
-        runTest("compiler/testData/diagnostics/tests/imports/noImportFromRootPackage.kt");
-      }
-
-      @Test
       @TestMetadata("OperatorRenameOnImport.kt")
       public void testOperatorRenameOnImport() {
         runTest("compiler/testData/diagnostics/tests/imports/OperatorRenameOnImport.kt");
@@ -16104,6 +16092,30 @@ public class FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGene
       @TestMetadata("renamedImportInDifferentFile.kt")
       public void testRenamedImportInDifferentFile() {
         runTest("compiler/testData/diagnostics/tests/imports/renamedImportInDifferentFile.kt");
+      }
+
+      @Test
+      @TestMetadata("RootPackage.kt")
+      public void testRootPackage() {
+        runTest("compiler/testData/diagnostics/tests/imports/RootPackage.kt");
+      }
+
+      @Test
+      @TestMetadata("RootPackageImportsA.kt")
+      public void testRootPackageImportsA() {
+        runTest("compiler/testData/diagnostics/tests/imports/RootPackageImportsA.kt");
+      }
+
+      @Test
+      @TestMetadata("RootPackageImportsB.kt")
+      public void testRootPackageImportsB() {
+        runTest("compiler/testData/diagnostics/tests/imports/RootPackageImportsB.kt");
+      }
+
+      @Test
+      @TestMetadata("RootPackageNoImports.kt")
+      public void testRootPackageNoImports() {
+        runTest("compiler/testData/diagnostics/tests/imports/RootPackageNoImports.kt");
       }
 
       @Test
