70c3ed26d27880f7dc1912b734e6851d66caa36b
Artem Olkov <artem.olkov@jetbrains.com>
2024-10-10 17:58:22 +0200

    fixup! KT-72102: Add test infra for Swift export in IDE

--- native/swift/swift-export-ide/tests/org/jetbrains/kotlin/swiftexport/ide/AbstractSymbolToSirTest.kt
+++ native/swift/swift-export-ide/tests/org/jetbrains/kotlin/swiftexport/ide/AbstractSymbolToSirTest.kt
@@ -5,45 +5,38 @@
 
 package org.jetbrains.kotlin.swiftexport.ide
 
-import org.jetbrains.kotlin.analysis.api.KaSession
-import org.jetbrains.kotlin.analysis.api.impl.base.test.cases.symbols.AbstractSymbolTest
-import org.jetbrains.kotlin.analysis.api.impl.base.test.cases.symbols.SymbolsData
-import org.jetbrains.kotlin.analysis.api.symbols.KaDeclarationSymbol
-import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
-import org.jetbrains.kotlin.analysis.api.symbols.KaValueParameterSymbol
+import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedTest
+import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtTestModule
 import org.jetbrains.kotlin.analysis.test.framework.services.expressionMarkerProvider
 import org.jetbrains.kotlin.psi.KtDeclaration
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.sir.SirDeclaration
+import org.jetbrains.kotlin.sir.SirModule
 import org.jetbrains.kotlin.sir.SirMutableDeclarationContainer
 import org.jetbrains.kotlin.sir.util.addChild
-import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
 import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.assertions
 import org.jetbrains.sir.printer.SirAsSwiftSourcesPrinter
 
-abstract class AbstractSymbolToSirTest : AbstractSymbolTest() {
-    override fun KaSession.collectSymbols(ktFile: KtFile, testServices: TestServices): SymbolsData {
-        return SymbolsData(
-            listOf(
-                testServices.expressionMarkerProvider.getElementOfTypeAtCaret<KtDeclaration>(ktFile).symbol
-            ),
-            symbolsForPrettyRendering = emptyList(),
-        )
-    }
-
-    override fun KaSession.renderSymbolForComparison(symbol: KaSymbol, directives: RegisteredDirectives): String {
-        if (symbol is KaValueParameterSymbol) return ""
-        require(symbol is KaDeclarationSymbol)
-        return withSirSession {
-            SirAsSwiftSourcesPrinter.print(
-                module = symbol.containingModule.sirModule().also {
-                    val resultedDeclaration = symbol.sirDeclaration()
-                    val parent = resultedDeclaration.parent as? SirMutableDeclarationContainer
-                        ?: error("top level declaration can contain only module or extension to package as a parent")
-                    parent.addChild { resultedDeclaration }
-                },
-                stableDeclarationsOrder = true,
-                renderDocComments = false,
-            )
+abstract class AbstractSymbolToSirTest : AbstractAnalysisApiBasedTest() {
+    override fun doTestByMainFile(mainFile: KtFile, mainModule: KtTestModule, testServices: TestServices) = analyseForTest(mainFile) {
+        val kaDeclaration = testServices
+            .expressionMarkerProvider.getElementOfTypeAtCaret<KtDeclaration>(mainFile).symbol
+        val actual: String = withSirSession {
+            kaDeclaration
+                .sirDeclaration()
+                .print(into = kaDeclaration.containingModule.sirModule())
         }
+        testServices.assertions.assertEqualsToTestDataFileSibling(actual)
     }
 }
+
+private fun SirDeclaration.print(into: SirModule): String = SirAsSwiftSourcesPrinter.print(
+    module = into.also {
+        val parent = parent as? SirMutableDeclarationContainer
+            ?: error("top level declaration can contain only module or extension to package as a parent")
+        parent.addChild { this }
+    },
+    stableDeclarationsOrder = true,
+    renderDocComments = false,
+)
