c87fb973f63df119b11dcbdccf5f15dd79bb75ff
Denis Lochmelis <denis.lochmelis@jetbrains.com>
2024-10-13 15:37:03 +0200

    Simplify build file

--- native/litmuskt/repo-tests/build.gradle.kts
+++ native/litmuskt/repo-tests/build.gradle.kts
@@ -5,7 +5,6 @@ import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
     kotlin("jvm")
-    id("jps-compatible")
 }
 
 repositories {
@@ -53,70 +52,17 @@ dependencies {
     litmusktTestsuiteNativeKlib("org.jetbrains.litmuskt:litmuskt-testsuite:0.1")
     litmusktRepoUtilsNativeKlib(project(":litmuskt:repo-utils"))
 
-    compileOnly(intellijCore())
-    compileOnly(libs.intellij.asm)
-
-    compileOnly(project(":compiler:plugin-api"))
-    compileOnly(project(":compiler:cli-common"))
-    compileOnly(project(":compiler:frontend"))
-    compileOnly(project(":compiler:backend"))
-    compileOnly(project(":compiler:ir.backend.common"))
-    compileOnly(project(":compiler:backend.jvm"))
-    compileOnly(project(":compiler:ir.tree"))
-
-    compileOnly(kotlinStdlib())
-
-    testApi(projectTests(":compiler:tests-common"))
-    testApi(projectTests(":compiler:test-infrastructure"))
-    testApi(projectTests(":compiler:test-infrastructure-utils"))
-    testApi(projectTests(":compiler:tests-compiler-utils"))
-    testApi(projectTests(":compiler:tests-common-new"))
-    testImplementation(projectTests(":generators:test-generator"))
-    testImplementation(libs.junit4)
-    testApi(kotlinTest())
-
-    // Dependencies for Kotlin/Native test infra:
-    if (!kotlinBuildProperties.isInIdeaSync) {
-        testImplementation(projectTests(":native:native.tests"))
-    }
-    testImplementation(project(":native:kotlin-native-utils"))
-    testImplementation(commonDependency("org.jetbrains.teamcity:serviceMessages"))
-
-    // todo: remove unnecessary dependencies
-    testImplementation(project(":kotlin-compiler-runner-unshaded"))
-
-    testImplementation(commonDependency("commons-lang:commons-lang"))
-    testImplementation(projectTests(":compiler:tests-common"))
-    testImplementation(projectTests(":compiler:tests-common-new"))
-    testImplementation(projectTests(":compiler:test-infrastructure"))
-
-    testApi(platform(libs.junit.bom))
+    testImplementation(platform(libs.junit.bom))
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
 
-    testRuntimeOnly(kotlinStdlib())
-    testRuntimeOnly(project(":compiler:backend-common"))
+    testImplementation(projectTests(":native:native.tests"))
 }
 
 sourceSets {
     "main" { projectDefault() }
     "test" { projectDefault() }
 }
-
-testsJar()
-useD8Plugin()
-
-projectTest(jUnitMode = JUnitMode.JUnit5) {
-    useJUnitPlatform {
-        // Exclude all tests with the "litmuskt-native" tag. They should be launched by another test task.
-        excludeTags("litmuskt-native")
-    }
-    workingDir = rootDir
-    dependsOn(":dist")
-}
-
-publish()
-standardPublicJars()
 
 val nativeTest = nativeTest(
     taskName = "nativeTest",
@@ -125,11 +71,3 @@ val nativeTest = nativeTest(
     customTestDependencies = listOf(litmusktCoreNativeKlib, litmusktTestsuiteNativeKlib, litmusktRepoUtilsNativeKlib),
     allowParallelExecution = false,
 )
-
-tasks.named("check") {
-    // Depend on the test task that launches Native tests so that it will also run together with tests
-    // for all other targets if K/N is enabled
-    if (kotlinBuildProperties.isKotlinNativeEnabled) {
-        dependsOn(nativeTest)
-    }
-}
--- native/native.tests/tests/org/jetbrains/kotlin/generators/tests/GenerateNativeTests.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/generators/tests/GenerateNativeTests.kt
@@ -450,7 +450,7 @@ fun main() {
         }
 
         // LitmusKt tests.
-        testGroup("native/litmuskt/repo-tests/test", "native/litmuskt/repo-tests/testData") {
+        testGroup("native/litmuskt/repo-tests/test", "native/litmuskt/repo-tests/tests-gen") {
             testClass<AbstractNativeBlackBoxTest>(
                 suiteTestClassName = "LitmusKtTestsGenerated",
                 annotations = listOf(
