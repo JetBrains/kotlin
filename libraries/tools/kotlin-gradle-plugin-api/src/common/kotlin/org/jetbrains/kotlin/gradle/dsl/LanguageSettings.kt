cc6385a0d6e29a713b6a52e53228373f5b5532f3
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-10-14 23:07:15 +0200

    [Gradle] Update KDoc for LanguageSettingsBuilder
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/dsl/LanguageSettings.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/dsl/LanguageSettings.kt
@@ -10,8 +10,8 @@ package org.jetbrains.kotlin.project.model
 /**
  * Represents most common Kotlin compilation settings for an entity.
  *
- * **Note**: This interface is soft-deprecated.
- * Instead, better to use existing `compilerOptions` DSL.
+ * **Note**: This interface will be deprecated in the future.
+ * Instead, it's better to use the existing `compilerOptions` DSL.
  *
  * See also [Compiler options DSL documentation](https://kotlinlang.org/docs/gradle-compiler-options.html).
  */
