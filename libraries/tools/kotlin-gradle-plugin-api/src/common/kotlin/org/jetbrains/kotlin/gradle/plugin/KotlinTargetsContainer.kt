e2af40090f856ba3228e0ac1de696357bd377948
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-10-14 22:57:54 +0200

    [Gradle] Suppress KDoc for KotlinTargetsContainerWithPresets
    It only contains one deprecated property and does not make sense to
    document it now.
    
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTargetsContainer.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTargetsContainer.kt
@@ -24,7 +24,7 @@ interface KotlinTargetsContainer {
 }
 
 /**
- * @suppress TODO: KT-58858 add documentation
+ * @suppress
  */
 @OptIn(InternalKotlinGradlePluginApi::class)
 @DeprecatedTargetPresetApi
