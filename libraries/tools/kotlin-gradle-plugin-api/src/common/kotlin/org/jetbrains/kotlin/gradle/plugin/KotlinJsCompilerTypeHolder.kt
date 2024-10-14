16e53a4e65f8ef830044efff1b10999b8cef9195
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-10-14 22:17:22 +0200

    [Gradle] Add KDoc for KotlinJsCompilerTypeHolder
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinJsCompilerTypeHolder.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinJsCompilerTypeHolder.kt
@@ -8,23 +8,41 @@
 package org.jetbrains.kotlin.gradle.plugin
 
 /**
- * @suppress TODO: KT-58858 add documentation
+ * Represents a Kotlin DSL entity holding information about possible modes for Kotlin/JS compiler.
  */
 interface KotlinJsCompilerTypeHolder {
+
+    /**
+     * @suppress
+     */
     @Deprecated("Because only IR compiler is left, no more necessary to know about compiler type in properties")
     val compilerTypeFromProperties: KotlinJsCompilerType?
 
+    /**
+     * Default type of the Kotlin/JS compiler to be used.
+     */
     val defaultJsCompilerType: KotlinJsCompilerType
         get() = KotlinJsCompilerType.IR
 
+    /**
+     * @suppress
+     */
     // Necessary to get rid of KotlinJsCompilerType import in build script
     @Deprecated("Legacy compiler is deprecated. Migrate your project to the new IR-based compiler", level = DeprecationLevel.HIDDEN)
     val LEGACY: KotlinJsCompilerType
         get() = KotlinJsCompilerType.LEGACY
 
+    /**
+     * Represents the IR (Intermediate Representation) backend mode of the Kotlin compiler.
+     *
+     * @see KotlinJsCompilerType.IR
+     */
     val IR: KotlinJsCompilerType
         get() = KotlinJsCompilerType.IR
 
+    /**
+     * @suppress
+     */
     @Deprecated("Legacy compiler is deprecated. Migrate your project to the new IR-based compiler", level = DeprecationLevel.HIDDEN)
     val BOTH: KotlinJsCompilerType
         get() = KotlinJsCompilerType.BOTH
