bd5e13453c6a1672aad5003223317ac3e7ffade0
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-10-14 22:05:01 +0200

    [Gradle] Don't require KDoc for KotlinGradlePluginExtensionPoint
    At this moment, this interface is not really usable without going into
    KGP internals.
    
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinGradlePluginExtensionPoint.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinGradlePluginExtensionPoint.kt
@@ -9,7 +9,7 @@ import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 /**
- * @suppress TODO: KT-58858 add documentation
+ * @suppress This API is not usable for external users as they could not access an instance of [KotlinGradlePluginExtensionPoint]
  * ### Extension Point for the Kotlin Gradle Plugin
  *
  * Note: Not stable for implementation: Custom implementations of this interface will not be supported by the Kotlin Gradle Plugin!
