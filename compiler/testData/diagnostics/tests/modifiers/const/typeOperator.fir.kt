f36f2f75aee2b4e307e8c2514595b79afe8faba9
Alexander Udalov <alexander.udalov@jetbrains.com>
2024-10-15 17:45:54 +0200

    FIR: check if argument of FirTypeOperatorCall is const
     #KT-71776 Fixed
     #KT-72302 Fixed
    
    Co-authored-by: Ivan Kylchik <ivan.kylchik@jetbrains.com>

--- compiler/testData/diagnostics/tests/modifiers/const/typeOperator.fir.kt
+++ compiler/testData/diagnostics/tests/modifiers/const/typeOperator.fir.kt
@@ -6,8 +6,11 @@ const val asOperator1 = 1 as Int
 const val asOperator2 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER!>1 <!CAST_NEVER_SUCCEEDS!>as<!> String<!>
 <!TYPE_CANT_BE_USED_FOR_CONST_VAL!>const<!> val asOperator3 = "1" <!CAST_NEVER_SUCCEEDS!>as?<!> Int
 <!TYPE_CANT_BE_USED_FOR_CONST_VAL!>const<!> val asOperator4 = "1" <!CAST_NEVER_SUCCEEDS!>as?<!> MyClass
+const val asOperator5 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER!>f() as String<!>
 
 const val isOperator1 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>1 is Int<!>
 const val isOperator2 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>1 is String<!>
 const val isOperator3 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>"1" !is Int<!>
 const val isOperator4 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>"1" !is MyClass<!>
+
+fun f(): String = ""
