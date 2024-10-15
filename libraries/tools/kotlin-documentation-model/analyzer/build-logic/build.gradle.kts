396dfbfc57ad2c232f854c09d39490a7855505cf
Adam <152864218+adam-enko@users.noreply.github.com>
2024-10-15 14:52:07 +0200

    [KDM] Enable Develocity test retry on CI
    See https://github.com/Kotlin/dokka/pull/3858
    
    Enabling test retry will allow Develocity to identify flaky tests in a single build and across multiple builds.
    Original commit: https://github.com/Kotlin/dokka/commit/b037efb7d384c96b4a1fcab5c3c52e8d183f8a8f

--- libraries/tools/kotlin-documentation-model/analyzer/build-logic/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/build.gradle.kts
@@ -23,4 +23,6 @@ dependencies {
     // workaround for accessing version-catalog in convention plugins
     // https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+
+    implementation(libs.gradlePlugin.gradle.develocity)
 }
