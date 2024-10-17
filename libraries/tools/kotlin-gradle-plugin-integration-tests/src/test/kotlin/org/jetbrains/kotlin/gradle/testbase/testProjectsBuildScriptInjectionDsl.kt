2f734d2d4397f2b52e73276cc5829f30bfa471fc
Anton Lakotka <anton.lakotka@jetbrains.com>
2024-10-17 13:24:58 +0200

    [Gradle] Check when Android library is published from kotlin managed
    components.
    
    This can lead into errors and mistakes. Android requires users to
    explicitly configure publication on AGP side.
    For example BuildTypeAttribute may leak into publication.
    
    So in this diagnostic we will warn users that doing publications
    incorrectly.
    
    ^KT-70380 Verification Pending

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/testProjectsBuildScriptInjectionDsl.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.testbase
+
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.util.GradleVersion
+
+fun KGPBaseTest.kotlinAndroidLibraryProject(
+    gradleVersion: GradleVersion,
+    agpVersion: String,
+    jdkVersion: JdkVersions.ProvidedJdk,
+): TestProject {
+    return project(
+        "base-kotlin-android-library",
+        gradleVersion,
+        buildOptions = defaultBuildOptions.copy(androidVersion = agpVersion),
+        buildJdk = jdkVersion.location,
+    ) {
+        buildScriptInjection { applyDefaultAndroidLibraryConfiguration() }
+    }
+}
+
+fun GradleBuildScriptInjectionContext.applyMavenPublishPlugin(): PublishingExtension {
+    project.plugins.apply("maven-publish")
+    publishing.repositories.apply {
+        maven { maven ->
+            maven.setUrl(project.layout.projectDirectory.dir("repo"))
+        }
+    }
+    return publishing
+}
+
+fun GradleBuildScriptInjectionContext.applyDefaultAndroidLibraryConfiguration() {
+    androidLibrary.apply {
+        compileSdk = 31
+        defaultConfig {
+            minSdk = 31
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+        namespace = "org.jetbrains.kotlin.sample"
+    }
+
+    java.apply {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
