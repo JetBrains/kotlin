941da90c7fde83c0a84ae895c8d14f3f07d208f3
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-10-22 10:24:08 +0200

    Reproduce KT-72238 and KT-72272

---
+++ compiler/testData/diagnostics/tests/inference/pcla/issues/kt72238.kt
@@ -0,0 +1,14 @@
+// WITH_STDLIB
+// ISSUE: KT-72238
+
+fun main() {
+    buildList {
+        var result: MutableList<Int>? = null
+        for (i in 0..10) {
+            result?.add(i) ?: run { result = mutableListOf(i) }
+
+        }
+
+        add("a")
+    }
+}
