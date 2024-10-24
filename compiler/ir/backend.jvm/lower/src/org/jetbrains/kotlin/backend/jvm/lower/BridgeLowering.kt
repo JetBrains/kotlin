733120a6b3f306b82a77e2c705d40b9b3115add7
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-10-24 18:27:37 +0200

    [IR] Migrate backend.jvm to new IR parameter API A22

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/BridgeLowering.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/BridgeLowering.kt
@@ -488,7 +488,7 @@ internal class BridgeLowering(val context: JvmBackendContext) : ClassLoweringPas
             }
 
             if (MethodSignatureMapper.shouldBoxSingleValueParameterForSpecialCaseOfRemove(this)) {
-                valueParameters.last().let {
+                parameters.last().let {
                     it.type = it.type.makeNullable()
                 }
             }
