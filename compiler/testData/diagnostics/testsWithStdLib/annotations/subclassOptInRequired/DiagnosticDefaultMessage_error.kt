6594ada5e652b39a4b00de2a88c6f217c73675d5
Anastasiia Pikalova <anastasia.nekrasova@jetbrains.com>
2024-11-05 10:52:32 +0100

    Add tests to reproduce the issue KT-72338

--- compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/DiagnosticDefaultMessage_error.kt
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/DiagnosticDefaultMessage_error.kt
@@ -13,3 +13,5 @@ open class OpenKlassInheritor: <!OPT_IN_TO_INHERITANCE_ERROR("ApiMarker; This cl
 open class OpenApiKlassInheritor: <!OPT_IN_USAGE_ERROR("ApiMarker; This declaration needs opt-in. Its usage must be marked with '@ApiMarker' or '@OptIn(ApiMarker::class)'")!>OpenApiKlass<!>()
 
 fun check(klass: <!OPT_IN_USAGE_ERROR("ApiMarker; This declaration needs opt-in. Its usage must be marked with '@ApiMarker' or '@OptIn(ApiMarker::class)'")!>OpenApiKlass<!>){}
+
+class FinalImplA: <!OPT_IN_TO_INHERITANCE_ERROR("ApiMarker; This class or interface requires opt-in to be implemented. Its usage must be marked with '@ApiMarker', '@OptIn(ApiMarker::class)' or '@SubclassOptInRequired(ApiMarker::class)'")!>OpenKlass<!>()
