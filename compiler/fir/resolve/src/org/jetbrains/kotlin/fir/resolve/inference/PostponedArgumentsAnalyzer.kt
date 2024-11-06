224e6d42ae87bdefd8a6f46e8a5a6e6132c12784
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-11-06 17:45:04 +0100

    ~ no expected type for overload resolution by return type

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/PostponedArgumentsAnalyzer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/PostponedArgumentsAnalyzer.kt
@@ -162,6 +162,7 @@ class PostponedArgumentsAnalyzer(
         val lambdaReturnType = lambda.returnType
 
         val expectedTypeForReturnArguments = when {
+            forOverloadByLambdaReturnType -> null
             c.canBeProper(lambdaReturnType) -> substitute(lambdaReturnType)
 
             // For Unit-coercion
