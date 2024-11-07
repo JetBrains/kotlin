5d91ba531f92386a009c16e41f1f40ab9a9f71da
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2024-11-07 14:41:29 +0100

    update test projects after removing intellij deps

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/commonize-kt-46248-singleNativeTargetPropagation/p1/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/commonize-kt-46248-singleNativeTargetPropagation/p1/build.gradle.kts
@@ -1,5 +1,5 @@
-import org.jetbrains.kotlin.com.intellij.openapi.util.SystemInfo.*
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.konan.target.HostManager
 
 operator fun KotlinSourceSet.invoke(builder: SourceSetHierarchyBuilder.() -> Unit): KotlinSourceSet {
     SourceSetHierarchyBuilder(this).builder()
@@ -37,14 +37,14 @@ fun registerListDependenciesTask(sourceSet: KotlinSourceSet) {
 
 kotlin {
     val nativePlatform = when {
-        isMac -> macosX64("nativePlatform")
-        isLinux -> linuxX64("nativePlatform")
-        isWindows -> mingwX64("nativePlatform")
+        HostManager.hostIsMac -> macosX64("nativePlatform")
+        HostManager.hostIsLinux -> linuxX64("nativePlatform")
+        HostManager.hostIsMingw -> mingwX64("nativePlatform")
         else -> throw IllegalStateException("Unsupported host")
     }
 
     val unsupportedNativePlatform = when {
-        isMac -> mingwX64("unsupportedNativePlatform")
+        HostManager.hostIsMac -> mingwX64("unsupportedNativePlatform")
         else -> macosX64("unsupportedNativePlatform")
     }
 
