e108263f2b79ffd8b8c8c0104567738d85017705
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2024-11-07 20:03:39 +0100

    [FIR] Remove code that checks for `isInner` and `origin` in `collectCandidates`
    It will help to avoid filtering out of inner type aliases with noninner RHS:
    `inner typealias TA = List<T>` where `T` is an outer type parameter.
    
    `isInner` check is not actual because `ConstructorFilter` on
    a level above actually does the same check.
    
    The check on `FirDeclarationOrigin.SamConstructor` was introduced by 907ebb36d02a763d54c18fb18e532aa1dda0061e,
    but the related test remains green if the check is removed.

--- compiler/testData/diagnostics/tests/typealias/innerTypeAliasConstructor.fir.kt
+++ compiler/testData/diagnostics/tests/typealias/innerTypeAliasConstructor.fir.kt
@@ -11,11 +11,11 @@ class C<T> {
 
 // C<...>.P[<...>]() syntax doesn't work due to the way qualified expressions are resolved now.
 // This restriction can be removed later.
-val test0 = C<Int>.P(1, 1)
-val test1 = C<Int>.P1<String>("", 1)
-val test2 = C<Int>.P2<String>(1, "")
-val test3 = C<Int>.P1("", 1)
-val test4 = C<Int>.P2(1, "")
+val test0 = C<Int>().P(1, 1)
+val test1 = C<Int>().P1<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>("", 1)
+val test2 = C<Int>().P2<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>(1, "")
+val test3 = C<Int>().P1("", 1)
+val test4 = C<Int>().P2(1, "")
 
 // C.P() syntax could work if we add captured type parameters as type variables in a constraint system for corresponding call.
 // However, this should be consistent with inner classes capturing type parameters.
