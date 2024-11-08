8528c1b2e881aaedb36ce7918a57dae3cf1e3197
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-11-08 19:35:01 +0100

    [JS] Remove unused properties from JS AST
    KT-70223

--- js/js.ast/build.gradle.kts
+++ js/js.ast/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    api(project(":core:descriptors"))
+    api(kotlinStdlib())
     compileOnly(intellijCore())
     compileOnly(commonDependency("org.jetbrains.intellij.deps.fastutil:intellij-deps-fastutil"))
 }
