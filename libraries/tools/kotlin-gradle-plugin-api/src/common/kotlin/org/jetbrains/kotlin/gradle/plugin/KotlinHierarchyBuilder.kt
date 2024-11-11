f0e6d56a5c3197886778286d0d197a57f7569731
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-11-11 10:53:12 +0100

    [Gradle] Add KDoc for KotlinHierarchyBuilder
    ^KT-58858 In Progress

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinHierarchyBuilder.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinHierarchyBuilder.kt
@@ -4,18 +4,28 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.konan.target.REMOVED_TARGET_MESSAGE
 
 /**
- * @suppress TODO: KT-58858 add documentation
+ * Provides a DSL to define the structure of [KotlinSourceSets][KotlinSourceSet] inside the [KotlinSourceSetTree].
+ *
+ * For example, to build a custom [KotlinSourceSetTree] using a DSL call to `kotlin.applyHierarchyTemplate { .. }`
+ * in a multiplatform project.
  */
 @KotlinTargetsDsl
 @ExperimentalKotlinGradlePluginApi
 interface KotlinHierarchyBuilder {
 
+    /**
+     * The root node in the hierarchy DSL structure.
+     */
     @KotlinTargetsDsl
     @ExperimentalKotlinGradlePluginApi
     interface Root : KotlinHierarchyBuilder {
+
         /**
-         * Defines the trees that the described hierarchy is applied to.
-         * ### Example 1: Only apply a hierarchy for the "main" and "test" [KotlinSourceSetTree]
+         * Defines the [KotlinSourceSetTrees][KotlinSourceSetTree] that the described hierarchy is applied to.
+         *
+         * Examples of DSL:
+         *
+         * 1. Only apply a hierarchy for the "main" and "test" [KotlinSourceSetTree]:
          *
          * ```kotlin
          * applyHierarchyTemplate {
@@ -29,7 +39,7 @@ interface KotlinHierarchyBuilder {
          * }
          *```
          *
-         * Will create the following trees given an iosX64(), iosArm64() and jvm() target:
+         * When given the `iosX64()`, `iosArm64()`, and `jvm()` targets, this DSL creates the following trees:
          * ```
          *             "main"                      "test"
          *           commonMain                 commonTest
@@ -43,8 +53,8 @@ interface KotlinHierarchyBuilder {
          * iosX64Main  iosArm64Main   iosX64Test  iosArm64Test
          * ```
          *
-         * ### Example 2:
-         * Using a different hierarchy for "main" and "test"
+         * 2. Use a different hierarchy for "main" and "test" [KotlinSourceSetTree]:
+         *
          *```kotlin
          * applyHierarchyTemplate {
          *    sourceSetTrees(SourceSetTree.main)  // ! <- only applied to the "main" tree
@@ -65,7 +75,7 @@ interface KotlinHierarchyBuilder {
          * }
          * ```
          *
-         * Will create the following trees given an iosX64(), iosArm64() and jvm() target:
+         * When given the `iosX64()`, `iosArm64()`, and `jvm()` targets, this DSL creates the following trees:
          * ```
          *             "main"                            "test"
          *           commonMain                        commonTest
@@ -82,25 +92,33 @@ interface KotlinHierarchyBuilder {
         fun sourceSetTrees(vararg tree: KotlinSourceSetTree)
 
         /**
-         * Will add the given [tree]s into for this descriptor.
+         * Adds the given [trees][tree] into this descriptor.
+         *
          * @see sourceSetTrees
          */
         fun withSourceSetTree(vararg tree: KotlinSourceSetTree)
 
         /**
-         * Will remove the given [tree]s from this descriptor
+         * Removes the given [trees][tree] from this descriptor.
          * @see sourceSetTrees
          */
         fun excludeSourceSetTree(vararg tree: KotlinSourceSetTree)
     }
 
-    /* Declaring groups */
+    /**
+     * Creates a group of [KotlinSourceSets][KotlinSourceSet] with the given [name] and structure provided via the [build] block.
+     *
+     * @see common
+     */
+    fun group(name: String, build: KotlinHierarchyBuilder.() -> Unit = {})
 
     /**
-     * Shortcut for `group("common") { }`:
-     * Most hierarchies should attach their nodes/groups to 'common'
+     * Creates a group with the name "common". It's a shortcut for `group("common") { }`.
      *
-     * e.g.
+     * Most hierarchies attach their nodes/groups to "common".
+     *
+     * The following example applies the shown hierarchy to the "main" compilations and creates a `nativeMain` source set
+     * that depends on the usual 'commonMain' source set:
      * ```
      * common {
      *     group("native") {
@@ -109,86 +127,314 @@ interface KotlinHierarchyBuilder {
      *     }
      * }
      * ```
-     * applying the shown hierarchy to the main compilations will create a 'nativeMain' source set which will
-     * depend on the usual 'commonMain'
-     *
      */
     fun common(build: KotlinHierarchyBuilder.() -> Unit) = group("common", build)
-    fun group(name: String, build: KotlinHierarchyBuilder.() -> Unit = {})
 
-    /* low-level APIs */
+    /**
+     * Allows including only those [KotlinCompilations][KotlinCompilation] for which the [predicate] returns `true`.
+     *
+     * This is a low-level API. Try to avoid using it.
+     */
     fun withCompilations(predicate: (KotlinCompilation<*>) -> Boolean)
 
+    /**
+     * Allows including only those [KotlinCompilations][KotlinCompilation] for which the [predicate] returns `true`.
+     *
+     * This is a low-level API. Try to avoid using it.
+     */
     fun excludeCompilations(predicate: (KotlinCompilation<*>) -> Boolean)
 
+    /**
+     * @suppress
+     */
     @Deprecated("Use 'excludeCompilations' instead", ReplaceWith("excludeCompilations(predicate)"))
     fun withoutCompilations(predicate: (KotlinCompilation<*>) -> Boolean) = excludeCompilations(predicate)
 
+    /**
+     * @suppress
+     */
     @Deprecated("Use plain 'withoutCompilations(!predicate) instead'", ReplaceWith("withoutCompilations { !predicate(it) }"))
     fun filterCompilations(predicate: (KotlinCompilation<*>) -> Boolean) = excludeCompilations { !predicate(it) }
 
-    /* Convenient groups */
+    /**
+     * Only includes targets for Kotlin/Native in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withNative()
+
+    /**
+     * Only includes Kotlin's Apple targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withApple()
+
+    /**
+     * Only includes Kotlin's Apple/iOS targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withIos()
+
+    /**
+     * Only includes Kotlin's Apple/watchOS targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withWatchos()
+
+    /**
+     * Only includes Kotlin's Apple/macOS targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withMacos()
+
+    /**
+     * Only includes Kotlin's Apple/tvOS targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withTvos()
+
+    /**
+     * Only includes Kotlin's MinGW targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withMingw()
+
+    /**
+     * Only includes Kotlin's Linux targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withLinux()
+
+    /**
+     * Only includes Kotlin's Android/Native targets in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withAndroidNative()
 
-    /* Actual targets */
+    /**
+     * Only includes targets for Kotlin/JS in this [group].
+     */
     fun withJs()
+
+    /**
+     * @suppress
+     */
     @Deprecated("Renamed to 'withWasmJs''", replaceWith = ReplaceWith("withWasmJs()"))
     fun withWasm()
+
+    /**
+     * Only includes Kotlin's Wasm/JS targets in this [group].
+     */
     fun withWasmJs()
+
+    /**
+     * Only includes Kotlin's Wasm/WASI targets in this [group].
+     */
     fun withWasmWasi()
+
+    /**
+     * Only includes targets for Kotlin/JVM in this [group].
+     */
     fun withJvm()
 
+    /**
+     * @suppress
+     */
     @Deprecated("Renamed to 'withAndroidTarget''", replaceWith = ReplaceWith("withAndroidTarget()"))
     fun withAndroid()
+
+    /**
+     * Only includes Kotlin's Android targets in this [group].
+     */
     fun withAndroidTarget()
+
+    /**
+     * Only includes Kotlin's Android/androidNativeX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withAndroidNativeX64()
+
+    /**
+     * Only includes Kotlin's Android/androidNativeX86 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withAndroidNativeX86()
+
+    /**
+     * Only includes Kotlin's Android/androidNativeArm32 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withAndroidNativeArm32()
+
+    /**
+     * Only includes Kotlin's Android/androidNativeArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withAndroidNativeArm64()
 
+    /**
+     * Only includes Kotlin's Apple/iosArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withIosArm64()
+
+    /**
+     * Only includes Kotlin's Apple/iosX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withIosX64()
+
+    /**
+     * Only includes Kotlin's Apple/iosSimulatorArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withIosSimulatorArm64()
+
+    /**
+     * Only includes Kotlin's Apple/watchosArm32 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withWatchosArm32()
+
+    /**
+     * Only includes Kotlin's Apple/watchosArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withWatchosArm64()
+
+    /**
+     * Only includes Kotlin's Apple/watchosX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withWatchosX64()
+
+    /**
+     * Only includes Kotlin's Apple/watchosSimulatorArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withWatchosSimulatorArm64()
+
+    /**
+     * Only includes Kotlin's Apple/watchosDeviceArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withWatchosDeviceArm64()
+
+    /**
+     * Only includes Kotlin's Apple/tvosArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withTvosArm64()
+
+    /**
+     * Only includes Kotlin's Apple/tvosX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withTvosX64()
+
+    /**
+     * Only includes Kotlin's Apple/tvosSimulatorArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withTvosSimulatorArm64()
+
+    /**
+     * Only includes Kotlin's linuxX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withLinuxX64()
+
+    /**
+     * Only includes Kotlin's mingwX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withMingwX64()
+
+    /**
+     * Only includes Kotlin's Apple/macosX64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withMacosX64()
+
+    /**
+     * Only includes Kotlin's Apple/macosArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withMacosArm64()
+
+    /**
+     * Only includes Kotlin's linuxArm64 target in this [group].
+     *
+     * For more information, see [Native targets overview](https://kotlinlang.org/docs/native-target-support.html).
+     */
     fun withLinuxArm64()
 
+    /**
+     * @suppress
+     */
     @Deprecated(message = REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withIosArm32()
 
+    /**
+     * @suppress
+     */
     @Deprecated(REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withWatchosX86()
 
+    /**
+     * @suppress
+     */
     @Deprecated(REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withMingwX86()
 
+    /**
+     * @suppress
+     */
     @Deprecated(REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withLinuxArm32Hfp()
 
+    /**
+     * @suppress
+     */
     @Deprecated(REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withLinuxMips32()
 
+    /**
+     * @suppress
+     */
     @Deprecated(REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withLinuxMipsel32()
 
+    /**
+     * @suppress
+     */
     @Deprecated(REMOVED_TARGET_MESSAGE, level = DeprecationLevel.ERROR)
     fun withWasm32()
 }
