8b9054b929b2ed58bfbf0f3d3211aa5ee4c19211
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-11-13 16:37:07 +0100

    [IR] Migrate backend.jvm to new IR parameter API A35

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/MfvcNodeFactory.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/MfvcNodeFactory.kt
@@ -327,7 +327,7 @@ fun collectPropertiesOrFieldsAfterLowering(irClass: IrClass, context: JvmBackend
         }
 
         fun handleAccessor(element: IrSimpleFunction) {
-            if (element.extensionReceiverParameter == null && element.contextReceiverParametersCount == 0) {
+            if (element.parameters.none { it.kind == IrParameterKind.ExtensionReceiver || it.kind == IrParameterKind.Context }) {
                 element.correspondingPropertySymbol?.owner?.let { add(Property(it)) }
             }
         }
