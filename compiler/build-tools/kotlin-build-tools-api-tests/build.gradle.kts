4d0e90e446d2a0af3a5d89217637af5383258450
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2024-11-19 15:45:15 +0100

    projectTest(...) is not idempotent, so it cannot be called more than one for a task.

--- compiler/build-tools/kotlin-build-tools-api-tests/build.gradle.kts
+++ compiler/build-tools/kotlin-build-tools-api-tests/build.gradle.kts
@@ -94,6 +94,7 @@ testing {
                 targets.all {
                     projectTest(taskName = testTask.name, jUnitMode = JUnitMode.JUnit5) {
                         ensureExecutedAgainstExpectedBuildToolsImplVersion(implVersion)
+                        systemProperty("kotlin.build-tools-api.log.level", "DEBUG")
                     }
                 }
             }
@@ -116,8 +117,10 @@ testing {
             }
 
             targets.all {
-                projectTest(taskName = testTask.name, jUnitMode = JUnitMode.JUnit5) {
-                    systemProperty("kotlin.build-tools-api.log.level", "DEBUG")
+                if (!testTask.name.startsWith("testCompatibility")) {
+                    projectTest(taskName = testTask.name, jUnitMode = JUnitMode.JUnit5) {
+                        systemProperty("kotlin.build-tools-api.log.level", "DEBUG")
+                    }
                 }
             }
         }
