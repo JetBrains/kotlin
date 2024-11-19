14ac9895e409a8313c1057e28c745fc68a982fce
Denis.Zharkov <denis.zharkov@jetbrains.com>
2024-11-19 15:40:42 +0100

    FIR: Refine contracts for postponed atoms properties
    NB: !isPostponedBecauseOfAmbiguity meant both
    `state == State.NOT_RESOLVED_YET` and `state == State.RESOLVED`,
    but I believe the main idea of the affected if's is that we _should_
    consider input types as trivial for freshly introduced callable
    references.
    
    As for `state == State.RESOLVED`, it doesn't really matter what
    we would return for that state, because the atom is completed, so
    these properties shouldn't even be used.
    I think we might've had an assertion here, but on the other
    hand it seems too much.
    
    ^KT-74021 Related

--- compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/model/PostponedResolvedAtomMarker.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/model/PostponedResolvedAtomMarker.kt
@@ -8,7 +8,23 @@ package org.jetbrains.kotlin.resolve.calls.model
 import org.jetbrains.kotlin.types.model.KotlinTypeMarker
 
 interface PostponedResolvedAtomMarker {
+    /**
+     * Generally, it's a collection of types that need to be "proper" to start the analysis of the atom (unless PCLA).
+     * Used mostly to define if the atom is ready and to define the order among other atoms.
+     *
+     * Usually, it's just a list of receiver/value parameter types, but might be an expected type variable.
+     * (see [org.jetbrains.kotlin.fir.resolve.calls.ConeLambdaWithTypeVariableAsExpectedTypeAtom])
+     */
     val inputTypes: Collection<KotlinTypeMarker>
+
+    /**
+     * Type that might be refined after analysis of the given atom, i.e., some new type variable constraints found.
+     * Currently, used to define dependencies between variables.
+     * (see TypeVariableDependencyInformationProvider.computePostponeArgumentsEdges)
+     *
+     * Usually, it's a return type of lambda/reference.
+     * Might be `null` if the return type is unknown or irrelevant.
+     */
     val outputType: KotlinTypeMarker?
     val expectedType: KotlinTypeMarker?
     val analyzed: Boolean
