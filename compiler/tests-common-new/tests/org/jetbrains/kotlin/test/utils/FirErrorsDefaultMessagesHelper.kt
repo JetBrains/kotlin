410f35c4d5a01de19a5e4674b6f8f1b7471b85f6
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2024-11-22 17:02:17 +0100

    [FIR] Test for British spelling in diagnostic messages

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/utils/FirErrorsDefaultMessagesHelper.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/utils/FirErrorsDefaultMessagesHelper.kt
@@ -27,7 +27,7 @@ fun KtDiagnosticFactoryToRendererMap.verifyMessages(objectWithErrors: Any) {
     }
 
     if (errors.isNotEmpty()) {
-        Assert.fail(errors.joinToString("\n", prefix = "\n"))
+        Assert.fail(errors.joinToString("\n\n", prefix = "\n"))
     }
 }
 
@@ -58,22 +58,40 @@ fun KtDiagnosticFactoryToRendererMap.verifyMessageForFactory(factory: AbstractKt
         else -> 0
     }
 
-    for (parameter in messageParameterRegex.findAll(renderer.message)) {
+    val message = renderer.message
+
+    for (parameter in messageParameterRegex.findAll(message)) {
         val index = parameter.value.substring(1, 2).toInt()
         if (index >= parameterCount) {
             add("Message for ${property.name} references wrong parameter {$index}")
         }
     }
 
-    if (parameterCount > 0 && renderer.message.contains("(?<!')'(?!')".toRegex())) {
+    if (parameterCount > 0 && message.contains("(?<!')'(?!')".toRegex())) {
         add("Renderer for ${property.name} has parameters and contains a single quote. Text inside single quotes is not formatted in MessageFormat. Use double quotes instead.")
     }
 
-    if (parameterCount == 0 && renderer.message.contains("(?<!')''(?!')".toRegex())) {
+    if (parameterCount == 0 && message.contains("(?<!')''(?!')".toRegex())) {
         add("Renderer for ${property.name} has no parameters and contains double quote. Single quotes should be used.")
     }
 
-    if (property.name !in lastCharExclusions && !renderer.message.last().toString().matches(lastCharRegex)) {
+    if (property.name !in lastCharExclusions && !message.last().toString().matches(lastCharRegex)) {
         add("Renderer for ${property.name} should end with a full stop. If this error is a false positive, add the name of the diagnostic to the list of exclusions.")
     }
+
+    fun MutableList<String>.checkRule(regex: Regex, hasProblem: String, exclusions: Set<String> = emptySet()) {
+        if (property.name !in exclusions && message.contains(regex)) {
+            val updatedMessage = message.replace(regex) { matchResult -> "[[${matchResult.value}]]" }
+            add(
+                "Message of ${property.name} $hasProblem: $updatedMessage\n"
+                        + "If this error is a false positive, add the name of the diagnostic to the list of exclusions."
+            )
+        }
+    }
+
+    checkRule(
+        """\b(colour|favour|realise|analyse|centre|defence|offence|licence|cancelled|metre|tonne|cheque|catalogue|neighbour|grey|programme)\b""".toRegex(
+            RegexOption.IGNORE_CASE),
+        "uses British spelling. Use American spelling instead"
+    )
 }
