684ad0f9e65b4debaa9f0cf7edb4b8cfea2fa53a
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-11-26 13:25:31 +0100

    [IR] Use parameter kind in isInlineParameter()
    KT-73220

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrInlineUtils.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/IrInlineUtils.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.ir.util
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationParent
 import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.IrParameterKind
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
 import org.jetbrains.kotlin.ir.expressions.*
 import org.jetbrains.kotlin.ir.irAttribute
@@ -40,7 +41,11 @@ val IrContainerExpression.innerInlinedBlockOrThis: IrContainerExpression
     get() = (this as? IrReturnableBlock)?.statements?.singleOrNull() as? IrInlinedFunctionBlock ?: this
 
 fun IrValueParameter.isInlineParameter() =
-    indexInOldValueParameters >= 0 && !isNoinline && !type.isNullable() && (type.isFunction() || type.isSuspendFunction()) && parent.isInlineFunction()
+    kind == IrParameterKind.Regular
+            && !isNoinline
+            && !type.isNullable()
+            && (type.isFunction() || type.isSuspendFunction())
+            && parent.isInlineFunction()
 
 private fun IrDeclarationParent.isInlineFunction(): Boolean {
     if (this !is IrFunction) return false
