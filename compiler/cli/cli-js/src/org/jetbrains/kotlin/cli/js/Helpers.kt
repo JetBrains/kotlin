ed88d5189410f418e6d145aa016822b9578e13ac
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-11-27 20:14:39 +0100

    [Wasm] K2JSCompiler refactoring

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/Helpers.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/Helpers.kt
@@ -5,8 +5,64 @@
 
 package org.jetbrains.kotlin.cli.js
 
+import com.intellij.util.ExceptionUtil
+import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.ERROR
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.psi.KtFile
+import java.io.File
+import java.io.IOException
+import kotlin.math.min
 
 fun checkKotlinPackageUsageForPsi(configuration: CompilerConfiguration, files: Collection<KtFile>) =
     org.jetbrains.kotlin.cli.common.checkKotlinPackageUsageForPsi(configuration, files)
+
+
+internal fun calculateSourceMapSourceRoot(
+    messageCollector: MessageCollector,
+    arguments: K2JSCompilerArguments,
+): String {
+    var commonPath: File? = null
+    val pathToRoot = mutableListOf<File>()
+    val pathToRootIndexes = hashMapOf<File, Int>()
+
+    try {
+        for (path in arguments.freeArgs) {
+            var file: File? = File(path).canonicalFile
+            if (commonPath == null) {
+                commonPath = file
+
+                while (file != null) {
+                    pathToRoot.add(file)
+                    file = file.parentFile
+                }
+                pathToRoot.reverse()
+
+                for (i in pathToRoot.indices) {
+                    pathToRootIndexes[pathToRoot[i]] = i
+                }
+            } else {
+                while (file != null) {
+                    var existingIndex = pathToRootIndexes[file]
+                    if (existingIndex != null) {
+                        existingIndex = min(existingIndex, pathToRoot.size - 1)
+                        pathToRoot.subList(existingIndex + 1, pathToRoot.size).clear()
+                        commonPath = pathToRoot[pathToRoot.size - 1]
+                        break
+                    }
+                    file = file.parentFile
+                }
+                if (file == null) {
+                    break
+                }
+            }
+        }
+    } catch (e: IOException) {
+        val text = ExceptionUtil.getThrowableText(e)
+        messageCollector.report(ERROR, "IO error occurred calculating source root:\n$text", location = null)
+        return "."
+    }
+
+    return commonPath?.path ?: "."
+}
