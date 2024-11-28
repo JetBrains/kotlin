e8c98e3bd98247c85c90f276a266c031469a826a
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-11-28 13:47:47 +0100

    KT-49155 Fix compatibility with gradle-test-retry and gradle-test-distribution plugins

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/jvm/tasks/KotlinJvmTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/jvm/tasks/KotlinJvmTest.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.gradle.targets.jvm.tasks
 
 import org.gradle.api.internal.tasks.testing.*
+import org.gradle.api.internal.tasks.testing.detection.DefaultTestExecuter
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
@@ -17,12 +18,18 @@ abstract class KotlinJvmTest : Test() {
     @Optional
     var targetName: String? = null
 
-    override fun createTestExecuter(): TestExecuter<JvmTestExecutionSpec> =
-        if (targetName != null) Executor(
-            super.createTestExecuter(),
-            targetName!!,
-        )
-        else super.createTestExecuter()
+    override fun createTestExecuter(): TestExecuter<JvmTestExecutionSpec> {
+        val originalExecutor = super.createTestExecuter()
+
+        // If the executor is not an instance of DefaultTestExecutor it means that it was created before
+        // and set to `Test.testExecuter` by Gradle plugins like test-distribution and test-retry.
+        // We can return the executor "as is" in this case as it is already wrapped with our Executor.
+        return if (targetName != null && originalExecutor is DefaultTestExecuter) {
+            Executor(originalExecutor, targetName!!)
+        } else {
+            originalExecutor
+        }
+    }
 
     class Executor(
         private val delegate: TestExecuter<JvmTestExecutionSpec>,
