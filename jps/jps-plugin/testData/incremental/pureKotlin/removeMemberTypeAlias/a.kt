11f625cc800c98e21450fb5116991760ecca99f5
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2024-11-28 12:12:05 +0100

    [FIR] Revert `inner` modifier from typealias in incremental test-data
    It was advised by aleksei.cherepanov@jetbrains.com some time ago,
    and this modifier is not needed because RHS doesn't have outer type parameters
    (top-level type aliases with inner RHS are supported).
    
    Also, it simplifies nested typealias bug-fixing
    because `inner` typealias is not going to be supported in the nearest future.

--- jps/jps-plugin/testData/incremental/pureKotlin/removeMemberTypeAlias/a.kt
+++ jps/jps-plugin/testData/incremental/pureKotlin/removeMemberTypeAlias/a.kt
@@ -3,11 +3,11 @@ package a
 class Outer {
     inner class B(x: String)
 
-    @Suppress("TOPLEVEL_TYPEALIASES_ONLY", "WRONG_MODIFIER_TARGET")
-    inner typealias A1 = B
+    @Suppress("TOPLEVEL_TYPEALIASES_ONLY")
+    typealias A1 = B
 
-    @Suppress("TOPLEVEL_TYPEALIASES_ONLY", "WRONG_MODIFIER_TARGET")
-    private inner typealias A2 = B
+    @Suppress("TOPLEVEL_TYPEALIASES_ONLY")
+    private typealias A2 = B
 
     fun A1(x: Any) = x
     fun A2(x: Any) = x
