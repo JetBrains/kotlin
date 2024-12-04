7f4f762e9db9494b88744e81a18c12939c82d69e
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-12-04 10:42:48 +0100

    [AA] Expand/edit documentation in the `resolution` package
    ^KT-65850

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/resolution/KaCallCandidateInfo.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/resolution/KaCallCandidateInfo.kt
@@ -9,31 +9,52 @@ import org.jetbrains.kotlin.analysis.api.diagnostics.KaDiagnostic
 import org.jetbrains.kotlin.analysis.api.lifetime.KaLifetimeOwner
 
 /**
- * A candidate considered for a call. I.e., one of the overload candidates in scope at the call site.
+ * [KaCallCandidateInfo] represents one of the candidates considered during [overload resolution](https://kotlinlang.org/spec/overload-resolution.html)
+ * of a call.
+ *
+ * In contrast to [KaErrorCallInfo.candidateCalls], retrieving a call candidate represented by [KaCallCandidateInfo] doesn't imply that the
+ * call is erroneous or ambiguous. Rather, resolving all call candidates is helpful when analyzing all possible options at the call site.
+ *
+ * @see org.jetbrains.kotlin.analysis.api.components.KaResolver.resolveToCallCandidates
  */
 public sealed interface KaCallCandidateInfo : KaLifetimeOwner {
+    /**
+     * The [KaCall] representing the call candidate.
+     */
     public val candidate: KaCall
 
     /**
-     * Returns true if the [candidate] is in the final set of candidates that the call is actually resolved to. There can be multiple
+     * Whether the [candidate] is in the final set of candidates considered during the call's resolution. There can be multiple best
      * candidates if the call is ambiguous.
      */
     public val isInBestCandidates: Boolean
 }
 
 /**
- * A candidate that is applicable for a call. A candidate is applicable if the call's arguments are complete and are assignable to the
- * candidate's parameters, AND the call's type arguments are complete and fit all the constraints of the candidate's type parameters.
+ * A [call candidate][KaCallCandidateInfo] that is applicable for a call.
+ *
+ * A candidate is applicable if:
+ *
+ * - Its arguments are complete and assignable to its parameters.
+ * - Its type arguments are complete and fit all the constraints of its type parameters.
+ *
+ * In other words, given the arguments and type arguments, the candidate *could* be called at that specific call site.
  */
 public interface KaApplicableCallCandidateInfo : KaCallCandidateInfo
 
 /**
- * A candidate that is NOT applicable for a call. A candidate is inapplicable if a call argument is missing or is not assignable to the
- * candidate's parameters, OR a call type argument is missing or does not fit the constraints of the candidate's type parameters.
+ * A [call candidate][KaCallCandidateInfo] that is *not* applicable for a call.
+ *
+ * A candidate is inapplicable if either:
+ *
+ * - An argument is missing or is not assignable to the corresponding parameter.
+ * - A type argument is missing or does not fit the constraints of the corresponding type parameter.
+ *
+ * In other words, given the arguments and type arguments, the candidate *could not* be called at that specific call site.
  */
 public interface KaInapplicableCallCandidateInfo : KaCallCandidateInfo {
     /**
-     * The reason the [candidate] was not applicable for the call (e.g., argument type mismatch, or no value for parameter).
+     * The reason for the [candidate]'s missing applicability (e.g. argument type mismatch or no value for a parameter).
      */
     public val diagnostic: KaDiagnostic
 }
