81f7f171e4e7945261ee6ad8ff07ee6bb4bc908f
Artem Olkov <artem.olkov@jetbrains.com>
2024-12-05 16:59:02 +0100

    KT-73655: fix C bridge generator for blocks with arguments ^KT-73655 fixed

--- native/swift/sir-compiler-bridge/testData/functional_types/expected.h
+++ native/swift/sir-compiler-bridge/testData/functional_types/expected.h
@@ -1,10 +1,12 @@
 #include <Foundation/Foundation.h>
 #include <stdint.h>
 
+int32_t root_foo_consuming_closure_returning_closure__TypesOfArguments__function__(void (^(^arg1)(void))(void));
+
+int32_t root_foo_consuming_closure_with_param__TypesOfArguments__function_MyAnotherClass__(uintptr_t (^arg1)(uintptr_t));
+
 int32_t root_foo_consuming_simple_closure__TypesOfArguments__function__(void (^arg1)(void));
 
 void (^(^root_foo_returning_closure_returning_closure__TypesOfArguments__Int32__(int32_t arg1))(void))(void);
 
 void (^root_foo_returning_simple_closure__TypesOfArguments__Int32__(int32_t arg1))(void);
-
-int32_t root_root_foo_consuming_closure_returning_closure__TypesOfArguments__function__(void (^(^arg1)(void))(void));
