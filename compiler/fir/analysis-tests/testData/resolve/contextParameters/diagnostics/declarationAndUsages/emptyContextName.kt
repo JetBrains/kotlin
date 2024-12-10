17976f28b8a593fda35d5070a3b58e6ea741c7a9
Aleksandra.Arsenteva <aleksandra.arsenteva@jetbrains.com>
2024-12-10 12:26:03 +0100

    [Tests] KQA-1566. Add checks with backticked name

--- compiler/fir/analysis-tests/testData/resolve/contextParameters/diagnostics/declarationAndUsages/emptyContextName.kt
+++ compiler/fir/analysis-tests/testData/resolve/contextParameters/diagnostics/declarationAndUsages/emptyContextName.kt
@@ -1,5 +1,6 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // LANGUAGE: +ContextParameters
+// ISSUE: KT-73797
 
 class A {
     fun foo(a: String): String { return a }
@@ -15,46 +16,40 @@ fun <T> implicit(): T = ctx
 context(_: A)
 fun test1() {
     implicit<A>().foo("")
+    <!UNRESOLVED_REFERENCE!>_<!>.foo("")
 }
 
 context(`_`: A)
 fun test2() {
     implicit<A>().foo("")
+    <!UNRESOLVED_REFERENCE!>`_`<!>.foo("")
 }
 
-context(_: A, b: B, `_`: Int)
+context(_: A, b: B, _: Int)
 fun test3() {
     implicit<A>().foo("")
     b.bar("")
     implicit<Int>().inc()
 }
 
-context(_ : A, `_`: B)
-fun test4() {
-    <!UNRESOLVED_REFERENCE!>_<!>.foo("")
-    <!UNRESOLVED_REFERENCE!>_<!>.bar("")
-}
-
 context(_: A)
 val property1: String
-    get() = implicit<A>().foo("")
+    get() {
+        <!UNRESOLVED_REFERENCE!>_<!>.foo("")
+        return implicit<A>().foo("")
+    }
 
 context(`_`: A)
 val property2: String
-    get() = implicit<A>().foo("")
+    get() {
+        <!UNRESOLVED_REFERENCE!>`_`<!>.foo("")
+        return implicit<A>().foo("")
+    }
 
-context(_: A, b: B, `_`: Int)
+context(_: A, b: B, _: Int)
 val property3: String
     get() {
         b.bar("")
         implicit<Int>().inc()
         return implicit<A>().foo("")
-    }
-
-context(_ : A, `_`: B)
-val property4: String
-    get() {
-        <!UNRESOLVED_REFERENCE!>_<!>.foo("")
-        <!UNRESOLVED_REFERENCE!>_<!>.bar("")
-        return ""
     }
