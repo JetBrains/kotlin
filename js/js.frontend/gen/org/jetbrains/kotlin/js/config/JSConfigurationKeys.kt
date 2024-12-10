ea32ad2855c264ca87c761ab51cfb506976e64eb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2024-12-10 12:20:21 +0200

    [JS] Declare new configuration keys for the phased CLI
    Also adjust throwing exception on `null` value for some existing keys,
    as it makes their usage during configuration update more convenient
    
    ^KT-73320

--- js/js.frontend/gen/org/jetbrains/kotlin/js/config/JSConfigurationKeys.kt
+++ js/js.frontend/gen/org/jetbrains/kotlin/js/config/JSConfigurationKeys.kt
@@ -13,6 +13,8 @@ package org.jetbrains.kotlin.js.config
  */
 
 import java.io.File
+import org.jetbrains.kotlin.backend.js.JsGenerationGranularity
+import org.jetbrains.kotlin.backend.js.TsCompilationStrategy
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.CompilerConfigurationKey
 import org.jetbrains.kotlin.incremental.js.IncrementalDataProvider
@@ -23,10 +25,19 @@ import org.jetbrains.kotlin.serialization.js.ModuleKind
 
 object JSConfigurationKeys {
     @JvmField
+    val WASM_COMPILATION = CompilerConfigurationKey.create<Boolean>("compile to WASM")
+
+    @JvmField
+    val OUTPUT_NAME = CompilerConfigurationKey.create<String>("Name of output KLib file")
+
+    @JvmField
     val TRANSITIVE_LIBRARIES = CompilerConfigurationKey.create<List<String>>("library files for transitive dependencies")
 
     @JvmField
     val LIBRARIES = CompilerConfigurationKey.create<List<String>>("library file paths")
+
+    @JvmField
+    val FRIEND_LIBRARIES = CompilerConfigurationKey.create<List<String>>("friend library file paths")
 
     @JvmField
     val SOURCE_MAP = CompilerConfigurationKey.create<Boolean>("generate source map")
@@ -60,6 +71,9 @@ object JSConfigurationKeys {
 
     @JvmField
     val MODULE_KIND = CompilerConfigurationKey.create<ModuleKind>("module kind")
+
+    @JvmField
+    val JS_INCREMENTAL_COMPILATION_ENABLED = CompilerConfigurationKey.create<Boolean>("incremental compilation enabled")
 
     @JvmField
     val INCREMENTAL_DATA_PROVIDER = CompilerConfigurationKey.create<IncrementalDataProvider>("incremental data provider")
@@ -133,7 +147,63 @@ object JSConfigurationKeys {
     @JvmField
     val USE_ES6_CLASSES = CompilerConfigurationKey.create<Boolean>("perform ES6 class usage")
 
+    @JvmField
+    val INCLUDES = CompilerConfigurationKey.create<String>("List of KLibs for this linking phase")
+
+    @JvmField
+    val PRODUCE_KLIB_FILE = CompilerConfigurationKey.create<Boolean>("Need to produce KLib file or not")
+
+    @JvmField
+    val PRODUCE_KLIB_DIR = CompilerConfigurationKey.create<Boolean>("Need to produce unpacked KLib dir or not")
+
+    @JvmField
+    val PER_MODULE_OUTPUT_NAME = CompilerConfigurationKey.create<String>("Custom output name to the split .js files")
+
+    @JvmField
+    val KEEP = CompilerConfigurationKey.create<List<String>>("list of fully qualified names not to be eliminated by DCE")
+
+    @JvmField
+    val DCE = CompilerConfigurationKey.create<Boolean>("Perform experimental dead code elimination")
+
+    @JvmField
+    val DCE_RUNTIME_DIAGNOSTIC = CompilerConfigurationKey.create<String>("Enable runtime diagnostics instead of removing declarations when performing DCE")
+
+    @JvmField
+    val SAFE_EXTERNAL_BOOLEAN = CompilerConfigurationKey.create<Boolean>("Wrap access to external 'Boolean' properties with an explicit conversion to 'Boolean'")
+
+    @JvmField
+    val SAFE_EXTERNAL_BOOLEAN_DIAGNOSTIC = CompilerConfigurationKey.create<String>("Enable runtime diagnostics when accessing external 'Boolean' properties")
+
+    @JvmField
+    val MINIMIZED_MEMBER_NAMES = CompilerConfigurationKey.create<Boolean>("Minimize the names of members")
+
+    @JvmField
+    val GRANULARITY = CompilerConfigurationKey.create<JsGenerationGranularity>("Granularity of JS files generation")
+
+    @JvmField
+    val TS_COMPILATION_STRATEGY = CompilerConfigurationKey.create<TsCompilationStrategy>("TS compilation strategy")
+
+    @JvmField
+    val CALL_MAIN_MODE = CompilerConfigurationKey.create<String>("Specify whether the 'main' function should be called upon execution.")
+
+    @JvmField
+    val IC_CACHE_DIRECTORY = CompilerConfigurationKey.create<String>("Directory for the IC cache")
+
+    @JvmField
+    val IC_CACHE_READ_ONLY = CompilerConfigurationKey.create<Boolean>("IC caches are read-only")
+
+    @JvmField
+    val PRESERVE_IC_ORDER = CompilerConfigurationKey.create<Boolean>("Preserve IC order")
+
 }
+
+var CompilerConfiguration.wasmCompilation: Boolean
+    get() = getBoolean(JSConfigurationKeys.WASM_COMPILATION)
+    set(value) { put(JSConfigurationKeys.WASM_COMPILATION, value) }
+
+var CompilerConfiguration.outputName: String?
+    get() = get(JSConfigurationKeys.OUTPUT_NAME)
+    set(value) { put(JSConfigurationKeys.OUTPUT_NAME, requireNotNull(value) { "nullable values are not allowed" }) }
 
 var CompilerConfiguration.transitiveLibraries: List<String>
     get() = getList(JSConfigurationKeys.TRANSITIVE_LIBRARIES)
@@ -142,6 +212,10 @@ var CompilerConfiguration.transitiveLibraries: List<String>
 var CompilerConfiguration.libraries: List<String>
     get() = getList(JSConfigurationKeys.LIBRARIES)
     set(value) { put(JSConfigurationKeys.LIBRARIES, value) }
+
+var CompilerConfiguration.friendLibraries: List<String>
+    get() = getList(JSConfigurationKeys.FRIEND_LIBRARIES)
+    set(value) { put(JSConfigurationKeys.FRIEND_LIBRARIES, value) }
 
 var CompilerConfiguration.sourceMap: Boolean
     get() = getBoolean(JSConfigurationKeys.SOURCE_MAP)
@@ -187,17 +261,21 @@ var CompilerConfiguration.moduleKind: ModuleKind?
     get() = get(JSConfigurationKeys.MODULE_KIND)
     set(value) { put(JSConfigurationKeys.MODULE_KIND, requireNotNull(value) { "nullable values are not allowed" }) }
 
+var CompilerConfiguration.jsIncrementalCompilationEnabled: Boolean
+    get() = getBoolean(JSConfigurationKeys.JS_INCREMENTAL_COMPILATION_ENABLED)
+    set(value) { put(JSConfigurationKeys.JS_INCREMENTAL_COMPILATION_ENABLED, value) }
+
 var CompilerConfiguration.incrementalDataProvider: IncrementalDataProvider?
     get() = get(JSConfigurationKeys.INCREMENTAL_DATA_PROVIDER)
-    set(value) { put(JSConfigurationKeys.INCREMENTAL_DATA_PROVIDER, requireNotNull(value) { "nullable values are not allowed" }) }
+    set(value) { putIfNotNull(JSConfigurationKeys.INCREMENTAL_DATA_PROVIDER, value) }
 
 var CompilerConfiguration.incrementalResultsConsumer: IncrementalResultsConsumer?
     get() = get(JSConfigurationKeys.INCREMENTAL_RESULTS_CONSUMER)
-    set(value) { put(JSConfigurationKeys.INCREMENTAL_RESULTS_CONSUMER, requireNotNull(value) { "nullable values are not allowed" }) }
+    set(value) { putIfNotNull(JSConfigurationKeys.INCREMENTAL_RESULTS_CONSUMER, value) }
 
 var CompilerConfiguration.incrementalNextRoundChecker: IncrementalNextRoundChecker?
     get() = get(JSConfigurationKeys.INCREMENTAL_NEXT_ROUND_CHECKER)
-    set(value) { put(JSConfigurationKeys.INCREMENTAL_NEXT_ROUND_CHECKER, requireNotNull(value) { "nullable values are not allowed" }) }
+    set(value) { putIfNotNull(JSConfigurationKeys.INCREMENTAL_NEXT_ROUND_CHECKER, value) }
 
 var CompilerConfiguration.friendPathsDisabled: Boolean
     get() = getBoolean(JSConfigurationKeys.FRIEND_PATHS_DISABLED)
@@ -253,7 +331,7 @@ var CompilerConfiguration.printReachabilityInfo: Boolean
 
 var CompilerConfiguration.dumpReachabilityInfoToFile: String?
     get() = get(JSConfigurationKeys.DUMP_REACHABILITY_INFO_TO_FILE)
-    set(value) { put(JSConfigurationKeys.DUMP_REACHABILITY_INFO_TO_FILE, requireNotNull(value) { "nullable values are not allowed" }) }
+    set(value) { putIfNotNull(JSConfigurationKeys.DUMP_REACHABILITY_INFO_TO_FILE, value) }
 
 var CompilerConfiguration.fakeOverrideValidator: Boolean
     get() = getBoolean(JSConfigurationKeys.FAKE_OVERRIDE_VALIDATOR)
@@ -282,4 +360,68 @@ var CompilerConfiguration.optimizeGeneratedJs: Boolean
 var CompilerConfiguration.useEs6Classes: Boolean
     get() = getBoolean(JSConfigurationKeys.USE_ES6_CLASSES)
     set(value) { put(JSConfigurationKeys.USE_ES6_CLASSES, value) }
+
+var CompilerConfiguration.includes: String?
+    get() = get(JSConfigurationKeys.INCLUDES)
+    set(value) { put(JSConfigurationKeys.INCLUDES, requireNotNull(value) { "nullable values are not allowed" }) }
+
+var CompilerConfiguration.produceKlibFile: Boolean
+    get() = getBoolean(JSConfigurationKeys.PRODUCE_KLIB_FILE)
+    set(value) { put(JSConfigurationKeys.PRODUCE_KLIB_FILE, value) }
+
+var CompilerConfiguration.produceKlibDir: Boolean
+    get() = getBoolean(JSConfigurationKeys.PRODUCE_KLIB_DIR)
+    set(value) { put(JSConfigurationKeys.PRODUCE_KLIB_DIR, value) }
+
+var CompilerConfiguration.perModuleOutputName: String?
+    get() = get(JSConfigurationKeys.PER_MODULE_OUTPUT_NAME)
+    set(value) { putIfNotNull(JSConfigurationKeys.PER_MODULE_OUTPUT_NAME, value) }
+
+var CompilerConfiguration.keep: List<String>
+    get() = getList(JSConfigurationKeys.KEEP)
+    set(value) { put(JSConfigurationKeys.KEEP, value) }
+
+var CompilerConfiguration.dce: Boolean
+    get() = getBoolean(JSConfigurationKeys.DCE)
+    set(value) { put(JSConfigurationKeys.DCE, value) }
+
+var CompilerConfiguration.dceRuntimeDiagnostic: String?
+    get() = get(JSConfigurationKeys.DCE_RUNTIME_DIAGNOSTIC)
+    set(value) { put(JSConfigurationKeys.DCE_RUNTIME_DIAGNOSTIC, requireNotNull(value) { "nullable values are not allowed" }) }
+
+var CompilerConfiguration.safeExternalBoolean: Boolean
+    get() = getBoolean(JSConfigurationKeys.SAFE_EXTERNAL_BOOLEAN)
+    set(value) { put(JSConfigurationKeys.SAFE_EXTERNAL_BOOLEAN, value) }
+
+var CompilerConfiguration.safeExternalBooleanDiagnostic: String?
+    get() = get(JSConfigurationKeys.SAFE_EXTERNAL_BOOLEAN_DIAGNOSTIC)
+    set(value) { put(JSConfigurationKeys.SAFE_EXTERNAL_BOOLEAN_DIAGNOSTIC, requireNotNull(value) { "nullable values are not allowed" }) }
+
+var CompilerConfiguration.minimizedMemberNames: Boolean
+    get() = getBoolean(JSConfigurationKeys.MINIMIZED_MEMBER_NAMES)
+    set(value) { put(JSConfigurationKeys.MINIMIZED_MEMBER_NAMES, value) }
+
+var CompilerConfiguration.granularity: JsGenerationGranularity?
+    get() = get(JSConfigurationKeys.GRANULARITY)
+    set(value) { put(JSConfigurationKeys.GRANULARITY, requireNotNull(value) { "nullable values are not allowed" }) }
+
+var CompilerConfiguration.tsCompilationStrategy: TsCompilationStrategy?
+    get() = get(JSConfigurationKeys.TS_COMPILATION_STRATEGY)
+    set(value) { put(JSConfigurationKeys.TS_COMPILATION_STRATEGY, requireNotNull(value) { "nullable values are not allowed" }) }
+
+var CompilerConfiguration.callMainMode: String?
+    get() = get(JSConfigurationKeys.CALL_MAIN_MODE)
+    set(value) { put(JSConfigurationKeys.CALL_MAIN_MODE, requireNotNull(value) { "nullable values are not allowed" }) }
+
+var CompilerConfiguration.icCacheDirectory: String?
+    get() = get(JSConfigurationKeys.IC_CACHE_DIRECTORY)
+    set(value) { putIfNotNull(JSConfigurationKeys.IC_CACHE_DIRECTORY, value) }
+
+var CompilerConfiguration.icCacheReadOnly: Boolean
+    get() = getBoolean(JSConfigurationKeys.IC_CACHE_READ_ONLY)
+    set(value) { put(JSConfigurationKeys.IC_CACHE_READ_ONLY, value) }
+
+var CompilerConfiguration.preserveIcOrder: Boolean
+    get() = getBoolean(JSConfigurationKeys.PRESERVE_IC_ORDER)
+    set(value) { put(JSConfigurationKeys.PRESERVE_IC_ORDER, value) }
 
