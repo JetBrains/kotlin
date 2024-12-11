177e38eeeb168fd1625221ce4cb6ab046dd812ac
vladislavgrecko <vladislav.grechko@jetbrains.com>
2024-12-11 17:15:55 +0100

    [JVM] Use `internal` visibility for $default-stubs of internal function
    At the moment, we extend its visibility to public in IR. It is not a big
    deal, but we still mangle those functions, and it breaks the
    debugger-related hacks when we're looking up for mangled function
    bytecode (see SourceCompilerForInline#getMethodNode).
    
    Yet `internal` still will be `public` at JVM level, so it's safe.
    
    ^KT-73538: Fixed

--- compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/ir/JvmIrUtils.kt
+++ compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/ir/JvmIrUtils.kt
@@ -106,7 +106,11 @@ fun IrClass.hasJvmDefaultWithCompatibilityAnnotation(): Boolean = hasAnnotation(
 fun IrFunction.hasPlatformDependent(): Boolean = propertyIfAccessor.hasAnnotation(PLATFORM_DEPENDENT_ANNOTATION_FQ_NAME)
 
 fun IrFunction.getJvmVisibilityOfDefaultArgumentStub() =
-    if (DescriptorVisibilities.isPrivate(visibility) || isInlineOnly()) JavaDescriptorVisibilities.PACKAGE_VISIBILITY else DescriptorVisibilities.PUBLIC
+    when {
+        DescriptorVisibilities.isPrivate(visibility) || isInlineOnly() -> JavaDescriptorVisibilities.PACKAGE_VISIBILITY
+        visibility == DescriptorVisibilities.INTERNAL -> DescriptorVisibilities.INTERNAL
+        else -> DescriptorVisibilities.PUBLIC
+    }
 
 fun IrDeclaration.isInCurrentModule(): Boolean =
     getPackageFragment() is IrFile
