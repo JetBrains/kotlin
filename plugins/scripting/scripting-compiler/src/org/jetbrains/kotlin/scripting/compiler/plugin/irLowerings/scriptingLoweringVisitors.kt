770d4f6b22765cf3b893a740fb834236a978b5af
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2024-12-12 11:19:10 +0100

    fixup! IR Scripting: move script lowering to the scripting plugin

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/irLowerings/scriptingLoweringVisitors.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/irLowerings/scriptingLoweringVisitors.kt
@@ -7,6 +7,7 @@
 
 package org.jetbrains.kotlin.scripting.compiler.plugin.irLowerings
 
+import org.jetbrains.kotlin.backend.common.extensions.ExperimentalAPIForScriptingPlugin
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.lower.ClosureAnnotator
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
@@ -529,6 +530,7 @@ internal fun patchDeclarationsDispatchReceiver(statements: List<IrStatement>, co
     }
 }
 
+@OptIn(ExperimentalAPIForScriptingPlugin::class)
 internal fun Collection<IrClass>.collectCapturersByReceivers(
     context: IrPluginContext,
     parentDeclaration: IrDeclaration,
