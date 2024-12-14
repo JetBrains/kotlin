f697ea552f5db03c56e5759f41b4e6026e260add
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2024-12-14 01:34:25 +0100

    [Test] IR Deserialization: Filter "conflicting" declaration flags
    There are many cases when declaration flags to the same declaration
    don't match in the frontend-generated IR and in the deserialized IR.
    
    Examples:
    1. 'external' flag for a declaration reference used in
       `IrDeclarationReference`. See commit
       12d28b57e0671fd86fde51fe6f58d2bdc1f91fe6 for more details.
    2. 'fake_override' flag which is missing in the frontend-generated IR
       in IR calls of `kotlin.reflect.KFunction<N>.invoke()` and
       `kotlin.reflect.KSuspendFunction<N>.invoke()` functions.
    3. 'delegated' flag for fake override of delegated property.
    
    ^KT-73171

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/handlers/SerializedIrDumpHandler.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/handlers/SerializedIrDumpHandler.kt
@@ -5,10 +5,16 @@
 
 package org.jetbrains.kotlin.test.backend.handlers
 
+import org.jetbrains.kotlin.ir.declarations.IrClass
+import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrPossiblyExternalDeclaration
+import org.jetbrains.kotlin.ir.declarations.IrProperty
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.expressions.IrDeclarationReference
 import org.jetbrains.kotlin.ir.util.DumpIrTreeOptions
 import org.jetbrains.kotlin.ir.util.dumpTreesFromLineNumber
+import org.jetbrains.kotlin.ir.util.isKFunction
+import org.jetbrains.kotlin.ir.util.isKSuspendFunction
 import org.jetbrains.kotlin.test.backend.ir.IrBackendInput
 import org.jetbrains.kotlin.test.directives.KlibBasedCompilerTestDirectives
 import org.jetbrains.kotlin.test.directives.KlibBasedCompilerTestDirectives.SKIP_IR_DESERIALIZATION_CHECKS
@@ -18,6 +24,8 @@ import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.moduleStructure
 import org.jetbrains.kotlin.test.services.temporaryDirectoryManager
 import org.jetbrains.kotlin.test.utils.MultiModuleInfoDumper
+import org.jetbrains.kotlin.util.OperatorNameConventions.INVOKE
+import org.jetbrains.kotlin.utils.addToStdlib.applyIf
 import java.io.File
 
 /**
@@ -53,13 +61,13 @@ class SerializedIrDumpHandler(
             normalizeNames = true,
             stableOrder = true,
             stableOrderOfOverriddenSymbols = true, // We need to print overridden symbols always in stable order.
-            declarationFlagsFilter = DumpIrTreeOptions.FlagsFilter.KEEP_ALL_FLAGS,
             // External declarations origin differs between frontend-generated and deserialized IR,
             // which prevents us from running irText tests against deserialized IR,
             // since it uses the same golden data as when we run them against frontend-generated IR.
             renderOriginForExternalDeclarations = false,
             printTypeAbbreviations = false,
             printExpectDeclarations = false, // Expect declarations do not survive during serialization to KLIB.
+            declarationFlagsFilter = FlagsFilterImpl(isAfterDeserialization),
             printSourceRetentionAnnotations = false, // KT-69965: Don't dump annotations having source retention
 
             /**
@@ -103,14 +111,122 @@ class SerializedIrDumpHandler(
 
         private val PRE_SERIALIZATION_DUMP_SANITIZER = IrDumpSanitizer.composite(
             FilterOutSealedSubclasses,
-            RemoveExternalFlagInRenderedSymbolOfIrDeclarationReference,
             RewriteImplicitSetterParameterName,
         )
 
         private val POST_DESERIALIZATION_DUMP_SANITIZER = IrDumpSanitizer.composite(
-            RemoveExternalFlagInRenderedSymbolOfIrDeclarationReference,
             RewriteImplicitSetterParameterName,
         )
+    }
+}
+
+private class FlagsFilterImpl(private val isAfterDeserialization: Boolean) : DumpIrTreeOptions.FlagsFilter {
+    override fun filterFlags(declaration: IrDeclaration, isReference: Boolean, flags: List<String>): List<String> = flags
+        .removeExternalFlagInDeclarationReferences(isReference)
+        .removeFakeOverrideFlagInKotlinReflectKFunctionCalls(declaration, isReference)
+        .removeDelegatedFlagFromPropertyFakeOverrides(declaration, isReference)
+
+    /**
+     * The text of every rendered [IrDeclarationReference] expression contains some details about the referenced symbol and
+     * its "owner" declaration. As a part of this text, there are declaration flags, one of which can be the "external" flag.
+     * This flag is rendered if the declaration is marked as "external" (see [IrPossiblyExternalDeclaration.isExternal]).
+     *
+     * However, there is a difference in how "external"-ness is propagated to declarations in Fir2LazyIr and in
+     * the IR during the serialization/deserialization. This makes it difficult to compare this flag 1-to-1 in the IR
+     * that was before serialization and the deserialized IR.
+     *
+     * We know that there are two strict rules that allow correctly handling "external" flag in the compiler:
+     * 1.Language permits `external` keyword only for top-level declarations. So, any top-level declaration must have this flag
+     *   set in metadata and subsequently in IR proto.
+     * 2.Whenever the compiler needs to check "external"-ness of a declaration, it should check the effective "external"-ness
+     *   (i.e., taking into account containing declarations, if there are any).
+     *
+     * Given this, it makes sense to filter out any mentioning of this flag in IR expressions that may refer to symbols
+     * bound to LazyIr/Fir2LazyIr declarations.
+     */
+    private fun List<String>.removeExternalFlagInDeclarationReferences(isReference: Boolean): List<String> =
+        applyIf(isReference) { this - "external" }
+
+    /**
+     * Remove 'fake_override' flag which is missing in the frontend-generated IR in IR calls of `kotlin.reflect.KFunction<N>.invoke()`
+     * and `kotlin.reflect.KSuspendFunction<N>.invoke()` functions.
+     */
+    private fun List<String>.removeFakeOverrideFlagInKotlinReflectKFunctionCalls(
+        declaration: IrDeclaration,
+        isReference: Boolean,
+    ): List<String> {
+        if (!isAfterDeserialization || !isReference) return this
+
+        if ((declaration as? IrSimpleFunction)?.name != INVOKE) return this
+
+        val parentClass = (declaration.parent as? IrClass) ?: return this
+        if (!parentClass.symbol.isKFunction() && !parentClass.symbol.isKSuspendFunction()) return this
+
+        return this - "fake_override"
+    }
+
+    /**
+     * Remove 'delegated' flag from property fake overrides and overridden symbols.
+     *
+     * Due to the differences in the fake overrides generation algorithm between Fir2Ir and regular IR,
+     * it could happen that the attributes of the same fake override are computed differently.
+     * As a result, the generated fake override could get different flags.
+     *
+     * Example 1:
+     * ```
+     * // module 1
+     * open class A {
+     *     open val x = 1
+     * }
+     *
+     * open class B: A() {
+     *     override val x by lazy { 2 }
+     * }
+     *
+     * // module 2 (depends on module 1)
+     * open class C: B() {
+     *     // frontend-generated IR:
+     *     // PROPERTY name:x visibility:public modality:OPEN [val]
+     *     //   overridden:
+     *     //     public open x: kotlin.Int [val] declared in B
+     *     //
+     *     // deserialized IR:
+     *     // PROPERTY name:x visibility:public modality:OPEN [val]
+     *     //   overridden:
+     *     //     public open x: kotlin.Int [delegated,val] declared in B
+     *     override val x = 3
+     * }
+     * ```
+     *
+     * Example 2:
+     * ```
+     * // module 1
+     * open class B {
+     *     val a by lazy { "b" }
+     * }
+     *
+     * // module 2 (depends on module 1)
+     * class C : B() {
+     *     // frontend-generated IR:
+     *     // PROPERTY FAKE_OVERRIDE name:a visibility:public modality:FINAL [fake_override,val]
+     *     //   overridden:
+     *     //     public final a: kotlin.String [val] declared in B
+     *     //
+     *     // deserialized IR:
+     *     // PROPERTY FAKE_OVERRIDE name:a visibility:public modality:FINAL [delegated,fake_override,val]
+     *     //   overridden:
+     *     //     public final a: kotlin.String [delegated,val] declared in B
+     *     val a = "c"
+     * }
+     * ```
+     */
+    private fun List<String>.removeDelegatedFlagFromPropertyFakeOverrides(
+        declaration: IrDeclaration,
+        isReference: Boolean,
+    ): List<String> {
+        if (declaration !is IrProperty) return this
+
+        return applyIf(isReference || declaration.isFakeOverride) { this - "delegated" }
     }
 }
 
@@ -149,49 +265,6 @@ private object FilterOutSealedSubclasses : IrDumpSanitizer {
     }
 
     private const val SEALED_SUBCLASSES_CLAUSE = "sealedSubclasses:"
-}
-
-/**
- * The text of every rendered [IrDeclarationReference] expression contains some details about the referenced symbol and
- * its "owner" declaration. As a part of this text, there are declaration flags, one of which can be the "external" flag.
- * This flag is rendered if the declaration is marked as "external" (see [IrPossiblyExternalDeclaration.isExternal]).
- *
- * However, there is a difference in how K1 and K2 compilers propagate "external"-ness to declarations in metadata, and
- * how such declarations are later represented in the form of LazyIr and Fir2LazyIr. This makes it difficult to compare
- * this flag 1-to-1 in the IR that was before serialization and the deserialized IR.
- *
- * We know that there are two strict rules that allow correctly handling "external" flag in the compiler:
- * 1.Language permits `external` keyword only for top-level declarations. So, any top-level declaration must have this flag
- *   written in metadata and subsequently in IR proto.
- * 2.Whenever the compiler needs to check "external"-ness of a declaration, it should check the effective "external"-ness
- *   (i.e., taking into account containing declarations, if any).
- *
- * Given this, it makes sense to filter out any mentioning of this flag in IR expressions that may refer to symbols
- * bound to LazyIr/Fir2LazyIr declarations.
- */
-private object RemoveExternalFlagInRenderedSymbolOfIrDeclarationReference : IrDumpSanitizer {
-    override fun sanitize(testData: String) = testData.lineSequence().map { line ->
-        IR_DECLARATION_REFERENCE_WITH_RENDERED_SYMBOL_REGEX.matchEntire(line)?.transformGroup(groupIndex = 2) { groupValue ->
-            groupValue.removeSurrounding("[", "] ")
-                .split(",")
-                .filter { it != "external" }
-                .takeIf(List<*>::isNotEmpty)
-                ?.joinToString(",", prefix = "[", postfix = "] ")
-                .orEmpty()
-        } ?: line
-    }.joinToString("\n")
-
-    private val IR_DECLARATION_REFERENCE_PREFIXES = listOf(
-        "CALL",                         // IrCall
-        "CONSTRUCTOR_CALL",             // IrConstructorCall
-        "DELEGATING_CONSTRUCTOR_CALL",  // IrDelegatingConstructorCall
-        "PROPERTY_REFERENCE",           // IrPropertyReference
-        "FUNCTION_REFERENCE",           // IrFunctionReference
-        "CLASS_REFERENCE",              // IrClassReference
-        "GET_OBJECT",                   // IrGetObjectValue
-    ).joinToString("|", prefix = "(", postfix = ")")
-
-    private val IR_DECLARATION_REFERENCE_WITH_RENDERED_SYMBOL_REGEX = Regex(".* $IR_DECLARATION_REFERENCE_PREFIXES .*(\\[[a-z,]+] ).*")
 }
 
 /**
