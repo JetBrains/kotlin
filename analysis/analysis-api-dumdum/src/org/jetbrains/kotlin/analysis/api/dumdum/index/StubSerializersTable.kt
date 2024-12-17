03ac7f9e12d784298ac22d4f865ea943df0f604a
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2024-12-17 09:44:12 +0100

    [dumdum] ugly hack to force IElementTypes registration on STATIC

--- analysis/analysis-api-dumdum/src/org/jetbrains/kotlin/analysis/api/dumdum/index/StubSerializersTable.kt
+++ analysis/analysis-api-dumdum/src/org/jetbrains/kotlin/analysis/api/dumdum/index/StubSerializersTable.kt
@@ -1,19 +1,29 @@
 package org.jetbrains.kotlin.analysis.api.dumdum.index
 
+import com.intellij.psi.impl.java.stubs.JavaStubElementTypes
 import com.intellij.psi.stubs.ObjectStubSerializer
 import com.intellij.psi.stubs.PsiFileStubImpl
 import com.intellij.psi.stubs.StubSerializer
 import com.intellij.psi.tree.IElementType
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
 data class StubSerializersTable(val map: Map<String, ObjectStubSerializer<*, *>>) {
     fun getSerializer(serializerName: String): ObjectStubSerializer<*, *> =
-        map[serializerName]!!
+        requireNotNull(map[serializerName]) {
+            "no stub serializer for $serializerName\n $map"
+        }
 }
 
-fun stubSerializersTable(): StubSerializersTable =
-    StubSerializersTable(
+fun stubSerializersTable(): StubSerializersTable {
+    KtStubElementTypes.CLASS;
+    JavaStubElementTypes.CLASS;
+    return StubSerializersTable(
         IElementType
             .enumerate { true }
             .filterIsInstance<StubSerializer<*>>()
-            .associateByTo(hashMapOf(PsiFileStubImpl.TYPE.externalId to PsiFileStubImpl.TYPE)) { it.externalId }
+            .associateByTo(
+                destination = hashMapOf(PsiFileStubImpl.TYPE.externalId to PsiFileStubImpl.TYPE),
+                keySelector = StubSerializer<*>::getExternalId
+            )
     )
+}
