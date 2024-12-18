75bcbb24d290812fc7dddec3a56c7a15732cfa1c
Timofey Solonin <timofey.solonin@jetbrains.com>
2024-12-18 21:07:47 +0100

    Test stale metadata compilations
    These compilations shouldn't exist as fragments in the resulting Uklib
    
    ^KT-74005

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/uklibs/UklibConsumptionGranularMetadataTransformationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/uklibs/UklibConsumptionGranularMetadataTransformationIT.kt
@@ -266,7 +266,7 @@ class UklibConsumptionGranularMetadataTransformationIT : KGPBaseTest() {
                     }
                 }
             }
-        }.publish(publisherConfiguration)
+        }.publish(publisherConfiguration = publisherConfiguration)
     }
 
 }
