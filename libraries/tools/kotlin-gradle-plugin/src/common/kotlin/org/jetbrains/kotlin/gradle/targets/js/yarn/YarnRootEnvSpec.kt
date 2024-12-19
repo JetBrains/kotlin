f75e81a125a7fe9cd11259c4167100bc8d8d4e27
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-12-19 18:05:37 +0100

    [Wasm] Separate Yarn to js and wasm

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnRootEnvSpec.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnRootEnvSpec.kt
@@ -5,106 +5,13 @@
 
 package org.jetbrains.kotlin.gradle.targets.js.yarn
 
-import org.gradle.api.provider.ListProperty
-import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
-import org.jetbrains.kotlin.gradle.targets.js.EnvSpec
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.Platform
-import org.jetbrains.kotlin.gradle.tasks.internal.CleanableStore
-import org.jetbrains.kotlin.gradle.utils.getFile
+import org.jetbrains.kotlin.gradle.targets.web.yarn.BaseYarnRootEnvSpec
 
 /**
  * Spec for Yarn - package manager to install NPM dependencies
  */
-abstract class YarnRootEnvSpec : EnvSpec<YarnEnv>() {
-
-    /**
-     * Specify a platform information with name and architecture
-     */
-    internal abstract val platform: Property<Platform>
-
-    /**
-     * Specify whether to not run install without custom package scripts.
-     * It is useful for security
-     *
-     * Default: true
-     */
-    abstract val ignoreScripts: Property<Boolean>
-
-    /**
-     * Specify a behaviour if yarn.lock file was changed
-     *
-     * Default: FAIL
-     */
-    abstract val yarnLockMismatchReport: Property<YarnLockMismatchReport>
-
-    /**
-     * Specify whether to fail a build if new yarn.lock file was generated during the build
-     *
-     * Default: false
-     */
-    abstract val reportNewYarnLock: Property<Boolean>
-
-    /**
-     * Specify whether to replace already existing yarn.lock file with newly generated yarn.lock file
-     *
-     * Default: false
-     */
-    abstract val yarnLockAutoReplace: Property<Boolean>
-
-    /**
-     * Specify replacements of versions of installed NPM dependencies
-     *
-     * Details: https://classic.yarnpkg.com/lang/en/docs/selective-version-resolutions/
-     */
-    abstract val resolutions: ListProperty<YarnResolution>
-
-    final override val env: Provider<YarnEnv> = produceEnv()
-
-    override val executable: Provider<String> = env.map { it.executable }
-
-    final override fun produceEnv(): Provider<YarnEnv> {
-        return platform.map { platformValue ->
-            val cleanableStore = CleanableStore[installationDirectory.getFile().path]
-
-            val isWindows = platformValue.isWindows()
-
-            val home = cleanableStore["yarn-v${version.get()}"].use()
-
-            val downloadValue = download.get()
-            fun getExecutable(
-                command: String,
-                customCommand: String,
-                windowsExtension: String,
-            ): String {
-                val finalCommand =
-                    if (isWindows && customCommand == command) "$command.$windowsExtension" else customCommand
-                return if (downloadValue)
-                    home
-                        .resolve("bin/yarn.js").absolutePath
-                else
-                    finalCommand
-            }
-
-            YarnEnv(
-                download = downloadValue,
-                downloadBaseUrl = downloadBaseUrl.orNull,
-                allowInsecureProtocol = allowInsecureProtocol.get(),
-                cleanableStore = cleanableStore,
-                dir = home,
-                executable = getExecutable("yarn", command.get(), "cmd"),
-                ivyDependency = "com.yarnpkg:yarn:${version.get()}@tar.gz",
-                ignoreScripts = ignoreScripts.get(),
-                yarnLockMismatchReport = yarnLockMismatchReport.get(),
-                reportNewYarnLock = reportNewYarnLock.get(),
-                yarnLockAutoReplace = yarnLockAutoReplace.get(),
-                yarnResolutions = resolutions.get()
-            )
-        }
-    }
-
+abstract class YarnRootEnvSpec : BaseYarnRootEnvSpec() {
     companion object {
         const val YARN: String = "kotlinYarnSpec"
     }
-
 }
