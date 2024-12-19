2ec6b6f90934ea1b3323852fdc28a7f227fd597c
Tomas Husak <tomas.husak@jetbrains.com>
2024-12-19 14:43:08 +0100

    [FIR] Remove lightTree exclusion dir from tests

--- compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/TieredConfigurationHelpers.kt
+++ compiler/tests-for-compiler-generator/tests/org/jetbrains/kotlin/test/generators/TieredConfigurationHelpers.kt
@@ -38,7 +38,6 @@ fun configureTierModelsForDeclaredAs(
     vararg tiers: TestTierLabel,
     relativeRootPaths: List<String>,
     excludeDirs: List<String>,
-    excludeDirsRecursively: List<String> = emptyList(),
     extension: String? = "kt",
     pattern: String = if (extension == null) """^([^\.]+)$""" else "^(.+)\\.$extension\$",
     excludedPattern: String? = null,
@@ -53,7 +52,6 @@ fun configureTierModelsForDeclaredAs(
             extension = extension,
             pattern = pattern,
             excludedPattern = excludedPattern,
-            excludeDirsRecursively = excludeDirsRecursively,
         )
     }
 }
@@ -64,7 +62,6 @@ fun configureTierModelsForDeclaredAs(
 fun configureTierModelsForK1AlongsideDiagnosticTestsStating(
     vararg tiers: TestTierLabel,
     allowKts: Boolean,
-    excludeDirsRecursively: List<String> = emptyList(),
 ): TestGroup.TestClass.() -> Unit =
     configureTierModelsForDeclaredAs(
         *tiers,
@@ -83,5 +80,4 @@ fun configureTierModelsForK1AlongsideDiagnosticTestsStating(
             else -> "^(.*)\\.(kt|nkt)$"
         },
         excludedPattern = CUSTOM_TEST_DATA_EXTENSION_PATTERN,
-        excludeDirsRecursively = excludeDirsRecursively,
     )
