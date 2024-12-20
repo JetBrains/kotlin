10b3969bf806798f185cb251e0df61025b39c0dc
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2024-12-20 20:30:39 +0100

    [Tests] Reproduce KT-73765 (Prohibit nested type aliases with inner RHS...
    ...if it captures outer type parameters)

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeDiagnosticsWithLatestLanguageVersionTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeDiagnosticsWithLatestLanguageVersionTestGenerated.java
@@ -4292,6 +4292,12 @@ public class FirLightTreeDiagnosticsWithLatestLanguageVersionTestGenerated exten
       }
 
       @Test
+      @TestMetadata("innerRHSandTypeArguments.kt")
+      public void testInnerRHSandTypeArguments() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/nestedTypeAliases/innerRHSandTypeArguments.kt");
+      }
+
+      @Test
       @TestMetadata("kmp.kt")
       public void testKmp() {
         runTest("compiler/fir/analysis-tests/testData/resolve/nestedTypeAliases/kmp.kt");
--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/TieredFrontendJvmLightTreeTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/TieredFrontendJvmLightTreeTestGenerated.java
@@ -29510,6 +29510,12 @@ public class TieredFrontendJvmLightTreeTestGenerated extends AbstractTieredFront
       }
 
       @Test
+      @TestMetadata("innerRHSandTypeArguments.kt")
+      public void testInnerRHSandTypeArguments() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/nestedTypeAliases/innerRHSandTypeArguments.kt");
+      }
+
+      @Test
       @TestMetadata("kmp.kt")
       public void testKmp() {
         runTest("compiler/fir/analysis-tests/testData/resolve/nestedTypeAliases/kmp.kt");
