9df698ee9f3693d2002b269ab944d0f5dcb75481
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2024-12-20 17:46:39 +0100

    [Wasm] Streamline a visitor method
    Visitors are not supposed to override two similar methods
    at the same time.

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/ir2wasm/BodyGenerator.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/ir2wasm/BodyGenerator.kt
@@ -951,21 +951,17 @@ class BodyGenerator(
     }
 
     override fun visitInlinedFunctionBlock(inlinedBlock: IrInlinedFunctionBlock) {
-        body.buildNop(inlinedBlock.getSourceLocation())
-
-        functionContext.stepIntoInlinedFunction(inlinedBlock.inlineFunctionSymbol, inlinedBlock.fileEntry)
-        super.visitInlinedFunctionBlock(inlinedBlock)
-        functionContext.stepOutLastInlinedFunction()
-    }
-
-    override fun visitInlinedFunctionBlock(inlinedBlock: IrInlinedFunctionBlock, data: Nothing?) {
         val inlineFunction = inlinedBlock.inlineFunctionSymbol?.owner
         val correspondingProperty = (inlineFunction as? IrSimpleFunction)?.correspondingPropertySymbol
         val owner = correspondingProperty?.owner ?: inlineFunction
         val name = owner?.fqNameWhenAvailable?.asString() ?: owner?.name?.asString() ?: "UNKNOWN"
 
         body.commentGroupStart { "Inlined call of `$name`" }
-        super.visitInlinedFunctionBlock(inlinedBlock, data)
+        body.buildNop(inlinedBlock.getSourceLocation())
+
+        functionContext.stepIntoInlinedFunction(inlinedBlock.inlineFunctionSymbol, inlinedBlock.fileEntry)
+        super.visitInlinedFunctionBlock(inlinedBlock)
+        functionContext.stepOutLastInlinedFunction()
     }
 
     override fun visitReturnableBlock(expression: IrReturnableBlock) {
