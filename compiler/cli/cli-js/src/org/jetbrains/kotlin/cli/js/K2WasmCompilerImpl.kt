52917b8bcf247cee8d5ddd27ad17891213f29fc1
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-12-22 07:03:57 +0100

    [to be squashed] fix compiler arguments test + rollback unnecessary changes

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2WasmCompilerImpl.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/js/K2WasmCompilerImpl.kt
@@ -77,12 +77,7 @@ internal class K2WasmCompilerImpl(
         moduleKind: ModuleKind?,
     ): ExitCode {
         configuration.phaseConfig = createPhaseConfig(arguments).also {
-            if (arguments.listPhases) it.list(
-                getWasmPhases(
-                    configuration,
-                    isIncremental = false,
-                )
-            )
+            if (arguments.listPhases) it.list(getWasmPhases(configuration, isIncremental = false))
         }
 
 
