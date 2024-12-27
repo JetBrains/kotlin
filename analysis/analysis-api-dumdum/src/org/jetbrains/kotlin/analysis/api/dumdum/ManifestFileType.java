4727a41fac49cf6a8d9a117a5b351b4c2df4c203
Andrey Zaytsev <andrey.zaytsev@jetbrains.com>
2024-12-28 00:42:08 +0100

    [dumdum] copypaste ManifestFileType

---
+++ analysis/analysis-api-dumdum/src/org/jetbrains/kotlin/analysis/api/dumdum/ManifestFileType.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2007-2009, Osmorc Development Team
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list
+ *       of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this
+ *       list of conditions and the following disclaimer in the documentation and/or other
+ *       materials provided with the distribution.
+ *     * Neither the name of 'Osmorc Development Team' nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without specific
+ *       prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.jetbrains.kotlin.analysis.api.dumdum;
+
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * @author Robert F. Beeger (robert@beeger.net)
+ */
+public final class ManifestFileType extends LanguageFileType {
+  public static final ManifestFileType INSTANCE = new ManifestFileType();
+
+  private ManifestFileType() {
+    super(ManifestLanguage.INSTANCE);
+  }
+
+  @Override
+  public @NotNull @NonNls String getName() {
+    return "Manifest";
+  }
+
+  @Override
+  public @NotNull String getDescription() {
+    return "Manifest";
+  }
+
+  @Override
+  public @NotNull @NonNls String getDefaultExtension() {
+    return "MF";
+  }
+
+  @Override
+  public Icon getIcon() {
+    return null;
+  }
+}
