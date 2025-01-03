f5ec128b0e3ba0b2b56a165d6a0af94512492d35
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-01-03 11:30:37 +0200

    fixup! [Test] Rename `DependencyProvider` service to `ArtifactsProvider`

--- compiler/test-infrastructure/tests/org/jetbrains/kotlin/test/services/TestServices.kt
+++ compiler/test-infrastructure/tests/org/jetbrains/kotlin/test/services/TestServices.kt
@@ -55,6 +55,6 @@ class TestServices : ComponentArrayOwner<TestService, TestService>(){
     }
 }
 
-fun TestServices.registerDependencyProvider(artifactsProvider: ArtifactsProvider) {
+fun TestServices.registerArtifactsProvider(artifactsProvider: ArtifactsProvider) {
     register(ArtifactsProvider::class, artifactsProvider)
 }
