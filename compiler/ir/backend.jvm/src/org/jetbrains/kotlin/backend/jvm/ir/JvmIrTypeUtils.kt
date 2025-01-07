JetBrains/kotlin

commit 4e4585c04d12fc0de11daef694a315010fdc8689

[IR] Merge type erasure utilities from different backends

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/ir/JvmIrTypeUtils.kt
@@ -19,88 +19,8 @@ import org.jetbrains.kotlin.ir.symbols.IrScriptSymbol
 import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
 import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
-import org.jetbrains.kotlin.ir.types.impl.IrStarProjectionImpl
 import org.jetbrains.kotlin.ir.types.impl.makeTypeProjection
 import org.jetbrains.kotlin.ir.util.*
-import org.jetbrains.kotlin.ir.util.isNullable
-
-/**
- * Perform as much type erasure as is significant for JVM signature generation.
- * Class types are kept as is, while type parameters are replaced with their
- * erased upper bounds, keeping the nullability information.
- *
- * For example, a type parameter `T?` where `T : Any`, `T : Comparable<T>` is
- * erased to `Any?`.
- *
- * Type arguments to the erased upper bound are replaced by `*`, since
- * recursive erasure could loop. For example, a type parameter
- * `T : Comparable<T>` is replaced by `Comparable<*>`.
- */
-fun IrType.eraseTypeParameters(): IrType = when (this) {
-    is IrSimpleType ->
-        when (val owner = classifier.owner) {
-            is IrScript -> {
-                assert(arguments.isEmpty()) { "Script can't be generic: " + owner.render() }
-                IrSimpleTypeImpl(classifier, nullability, emptyList(), annotations)
-            }
-            is IrClass -> IrSimpleTypeImpl(classifier, nullability, arguments.map { it.eraseTypeParameters() }, annotations)
-            is IrTypeParameter -> owner.erasedType(isNullable())
-            else -> error("Unknown IrSimpleType classifier kind: $owner")
-        }
-    is IrErrorType ->
-        this
-    else -> error("Unknown IrType kind: $this")
-}
-
-fun IrType.eraseIfTypeParameter(): IrType {
-    val typeParameter = (this as? IrSimpleType)?.classifier?.owner as? IrTypeParameter ?: return this
-    return typeParameter.erasedType(isNullable())
-}
-
-private fun IrTypeParameter.erasedType(isNullable: Boolean): IrType {
-    val upperBound = erasedUpperBound
-    return IrSimpleTypeImpl(
-        upperBound.symbol,
-        isNullable,
-        // Should not affect JVM signature, but may result in an invalid type object
-        List(upperBound.typeParameters.size) { IrStarProjectionImpl },
-        annotations
-    )
-}
-
-private fun IrTypeArgument.eraseTypeParameters(): IrTypeArgument = when (this) {
-    is IrStarProjection -> this
-    is IrTypeProjection -> makeTypeProjection(type.eraseTypeParameters(), variance)
-}
-
-/**
- * Computes the erased class for this type parameter according to the java erasure rules.
- */
-val IrTypeParameter.erasedUpperBound: IrClass
-    get() {
-        // Pick the (necessarily unique) non-interface upper bound if it exists
-        for (type in superTypes) {
-            val irClass = type.classOrNull?.owner ?: continue
-            if (!irClass.isJvmInterface) return irClass
-        }
-
-        // Otherwise, choose either the first IrClass supertype or recurse.
-        // In the first case, all supertypes are interface types and the choice was arbitrary.
-        // In the second case, there is only a single supertype.
-        return superTypes.first().erasedUpperBound
-    }
-
-val IrType.erasedUpperBound: IrClass
-    get() = when (this) {
-        is IrSimpleType -> when (val classifier = classifier.owner) {
-            is IrClass -> classifier
-            is IrTypeParameter -> classifier.erasedUpperBound
-            is IrScript -> classifier.targetClass?.owner ?: error(render())
-            else -> error(render())
-        }
-        is IrErrorType -> symbol.owner
-        else -> error(render())
-    }
 
 /**
  * Get the default null/0 value for the type.

compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/TypeTranformer.kt
@@ -1,34 +1,0 @@
-/*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
- */
-
-package org.jetbrains.kotlin.ir.backend.js.utils
-
-import org.jetbrains.kotlin.ir.IrBuiltIns
-import org.jetbrains.kotlin.ir.declarations.IrClass
-import org.jetbrains.kotlin.ir.declarations.IrTypeParameter
-import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
-import org.jetbrains.kotlin.ir.symbols.IrTypeParameterSymbol
-import org.jetbrains.kotlin.ir.types.*
-import org.jetbrains.kotlin.ir.util.defaultType
-
-
-// Return null if upper bound is Any
-private val IrTypeParameter.erasedUpperBound: IrClass?
-    get() {
-        // Pick the (necessarily unique) non-interface upper bound if it exists
-        for (type in superTypes) {
-            return type.erasedUpperBound ?: continue
-        }
-
-        return null
-    }
-
-val IrType.erasedUpperBound: IrClass?
-    get() =
-        when (val classifier = classifierOrNull) {
-            is IrClassSymbol -> classifier.owner
-            is IrTypeParameterSymbol -> classifier.owner.erasedUpperBound
-            else -> throw IllegalStateException()
-        }

compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/AnnotationCodegen.kt
@@ -20,7 +20,7 @@ import org.jetbrains.annotations.NotNull
 import org.jetbrains.annotations.Nullable
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.JvmLoweredDeclarationOrigin
-import org.jetbrains.kotlin.backend.jvm.ir.erasedUpperBound
+import org.jetbrains.kotlin.ir.util.erasedUpperBound
 import org.jetbrains.kotlin.backend.jvm.ir.isInlineClassType
 import org.jetbrains.kotlin.backend.jvm.ir.isOptionalAnnotationClass
 import org.jetbrains.kotlin.backend.jvm.ir.isWithFlexibleNullability

compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/CoroutineCodegen.kt
@@ -7,7 +7,7 @@ package org.jetbrains.kotlin.backend.jvm.codegen
 
 import org.jetbrains.kotlin.backend.jvm.InlineClassAbi
 import org.jetbrains.kotlin.backend.jvm.JvmLoweredDeclarationOrigin
-import org.jetbrains.kotlin.backend.jvm.ir.erasedUpperBound
+import org.jetbrains.kotlin.ir.util.erasedUpperBound
 import org.jetbrains.kotlin.backend.jvm.ir.hasContinuation
 import org.jetbrains.kotlin.backend.jvm.ir.isReadOfCrossinline
 import org.jetbrains.kotlin.backend.jvm.ir.suspendFunctionOriginal

compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/PromisedValue.kt
@@ -7,7 +7,7 @@ package org.jetbrains.kotlin.backend.jvm.codegen
 
 import org.jetbrains.kotlin.backend.jvm.InlineClassAbi
 import org.jetbrains.kotlin.backend.jvm.inlineClassFieldName
-import org.jetbrains.kotlin.backend.jvm.ir.eraseIfTypeParameter
+import org.jetbrains.kotlin.ir.util.eraseIfTypeParameter
 import org.jetbrains.kotlin.backend.jvm.mapping.IrTypeMapper
 import org.jetbrains.kotlin.codegen.AsmUtil
 import org.jetbrains.kotlin.codegen.StackValue

compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/FunctionNVarargBridgeLowering.kt
@@ -13,7 +13,7 @@ import org.jetbrains.kotlin.backend.common.lower.irIfThen
 import org.jetbrains.kotlin.backend.common.phaser.PhaseDescription
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.ir.createJvmIrBuilder
-import org.jetbrains.kotlin.backend.jvm.ir.erasedUpperBound
+import org.jetbrains.kotlin.ir.util.erasedUpperBound
 import org.jetbrains.kotlin.backend.jvm.ir.irArray
 import org.jetbrains.kotlin.backend.jvm.needsMfvcFlattening
 import org.jetbrains.kotlin.builtins.StandardNames

compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmInlineClassLowering.kt
@@ -10,7 +10,7 @@ import org.jetbrains.kotlin.backend.common.lower.irBlockBody
 import org.jetbrains.kotlin.backend.common.lower.loops.ForLoopsLowering
 import org.jetbrains.kotlin.backend.common.phaser.PhaseDescription
 import org.jetbrains.kotlin.backend.jvm.*
-import org.jetbrains.kotlin.backend.jvm.ir.erasedUpperBound
+import org.jetbrains.kotlin.ir.util.erasedUpperBound
 import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.config.ApiVersion
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities

compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/JvmMultiFieldValueClassLowering.kt
@@ -16,7 +16,7 @@ import org.jetbrains.kotlin.backend.jvm.MemoizedMultiFieldValueClassReplacements
 import org.jetbrains.kotlin.backend.jvm.MemoizedMultiFieldValueClassReplacements.RemappedParameter.MultiFieldValueClassMapping
 import org.jetbrains.kotlin.backend.jvm.MemoizedMultiFieldValueClassReplacements.RemappedParameter.RegularMapping
 import org.jetbrains.kotlin.backend.jvm.ir.createJvmIrBuilder
-import org.jetbrains.kotlin.backend.jvm.ir.erasedUpperBound
+import org.jetbrains.kotlin.ir.util.erasedUpperBound
 import org.jetbrains.kotlin.backend.jvm.lower.BlockOrBody.Block
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
 import org.jetbrains.kotlin.descriptors.Modality

commit f094c9e309be3b587dda0e224e885334bb38cd0b

Fix user-projects build broken in 4e4585c04
A property used there is temporarily restored, but marked as deprecated

compiler/ir/backend.jvm/src/org/jetbrains/kotlin/backend/jvm/ir/JvmIrTypeUtils.kt
@@ -5,6 +5,8 @@
 
 package org.jetbrains.kotlin.backend.jvm.ir
 
+import org.jetbrains.kotlin.CompilerVersionOfApiDeprecation
+import org.jetbrains.kotlin.DeprecatedForRemovalCompilerApi
 import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.unboxInlineClass
 import org.jetbrains.kotlin.descriptors.ClassKind
@@ -21,6 +23,7 @@ import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.types.impl.IrSimpleTypeImpl
 import org.jetbrains.kotlin.ir.types.impl.makeTypeProjection
 import org.jetbrains.kotlin.ir.util.*
+import org.jetbrains.kotlin.ir.util.erasedUpperBound
 
 /**
  * Get the default null/0 value for the type.
@@ -104,3 +107,21 @@ val IrTypeParameter.representativeUpperBound: IrType
             irClass.kind != ClassKind.INTERFACE && irClass.kind != ClassKind.ANNOTATION_CLASS
         } ?: superTypes.first()
     }
+
+@DeprecatedForRemovalCompilerApi(
+    CompilerVersionOfApiDeprecation._2_1_20,
+    "Moved to different package",
+    "org.jetbrains.kotlin.ir.util.erasedUpperBound"
+)
+// generic parameter to deprioritize in overload resolution against moved one if both are star-imported
+val <T : IrTypeParameter> T.erasedUpperBound
+    get() = /*org.jetbrains.kotlin.ir.util.*/erasedUpperBound
+
+@DeprecatedForRemovalCompilerApi(
+    CompilerVersionOfApiDeprecation._2_1_20,
+    "Moved to different package",
+    "org.jetbrains.kotlin.ir.util.erasedUpperBound"
+)
+// generic parameter to deprioritize in overload resolution against moved one if both are star-imported
+val <T : IrType> T.erasedUpperBound: IrClass
+    get() = /*org.jetbrains.kotlin.ir.util.*/erasedUpperBound