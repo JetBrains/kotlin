4b3a3c33b810ad6fb284284b3d65a77772cc2915
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-01-08 14:44:04 +0200

    [AA] Lazily compute the effective visibility in FIR stub-based deserializer
    See previous commits for explanation
    
    ^KT-74040

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/stubBased/deserialization/StubBasedClassDeserialization.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/stubBased/deserialization/StubBasedClassDeserialization.kt
@@ -22,9 +22,11 @@ import org.jetbrains.kotlin.fir.declarations.builder.buildRegularClass
 import org.jetbrains.kotlin.fir.declarations.builder.buildSimpleFunction
 import org.jetbrains.kotlin.fir.declarations.comparators.FirMemberDeclarationComparator
 import org.jetbrains.kotlin.fir.declarations.impl.FirResolvedDeclarationStatusImpl
+import org.jetbrains.kotlin.fir.declarations.impl.FirResolvedDeclarationStatusWithLazyEffectiveVisibility
 import org.jetbrains.kotlin.fir.declarations.utils.*
 import org.jetbrains.kotlin.fir.deserialization.addCloneForArrayIfNeeded
 import org.jetbrains.kotlin.fir.deserialization.deserializationExtension
+import org.jetbrains.kotlin.fir.deserialization.toLazyEffectiveVisibility
 import org.jetbrains.kotlin.fir.resolve.transformers.setLazyPublishedVisibility
 import org.jetbrains.kotlin.fir.scopes.FirScopeProvider
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
@@ -126,10 +128,10 @@ internal fun deserializeClassToSymbol(
     }
     val modality = classOrObject.modality
     val visibility = classOrObject.visibility
-    val status = FirResolvedDeclarationStatusImpl(
+    val status = FirResolvedDeclarationStatusWithLazyEffectiveVisibility(
         visibility,
         modality,
-        visibility.toEffectiveVisibility(parentContext?.outerClassSymbol, forClass = true)
+        visibility.toLazyEffectiveVisibility(parentContext?.outerClassSymbol, session, forClass = true)
     ).apply {
         isExpect = classOrObject.hasModifier(KtTokens.EXPECT_KEYWORD)
         isActual = false
