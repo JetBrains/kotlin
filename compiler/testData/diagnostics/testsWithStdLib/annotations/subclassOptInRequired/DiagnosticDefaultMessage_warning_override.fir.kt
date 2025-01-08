ce8ef69791e9d5e75e76f99b32587891242a7a96
anastasiia <anastasia.nekrasova@jetbrains.com>
2025-01-08 13:09:45 +0100

    OPT_IN_USAGE, OPT_IN_OVERRIDE and OPT_IN_TO_INHERITANCE diagnostic messages are inconsistent
    Added FIR tests files
    
    ^KT-72339

---
+++ compiler/testData/diagnostics/testsWithStdLib/annotations/subclassOptInRequired/DiagnosticDefaultMessage_warning_override.fir.kt
@@ -0,0 +1,56 @@
+// RUN_PIPELINE_TILL: BACKEND
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+annotation class MarkerA
+
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+annotation class MarkerB
+
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+annotation class MarkerC
+
+@RequiresOptIn(level = RequiresOptIn.Level.WARNING)
+annotation class MarkerD
+
+interface OverrideOptInA {
+    @MarkerA
+    fun overrideOptIn() {
+    }
+}
+
+interface OverrideOptInB {
+    @MarkerB
+    fun overrideOptIn() {
+    }
+}
+
+interface OverrideOptInC {
+    @MarkerC
+    fun overrideOptIn() {
+    }
+}
+
+
+interface OverrideOptInD {
+    @MarkerD
+    fun overrideOptIn() {
+    }
+}
+
+
+class OverrideOptInImplA : OverrideOptInA {
+    override fun <!OPT_IN_OVERRIDE("MarkerA; Base declaration of supertype 'OverrideOptInA' needs opt-in. The declaration override should be annotated with '@MarkerA' or '@OptIn(MarkerA::class)'")!>overrideOptIn<!>(){}
+}
+
+class OverrideOptInImplB : OverrideOptInB {
+    override fun <!OPT_IN_OVERRIDE("MarkerB; Base declaration of supertype 'OverrideOptInB' needs opt-in. The declaration override should be annotated with '@MarkerB' or '@OptIn(MarkerB::class)'")!>overrideOptIn<!>(){}
+}
+
+class OverrideOptInImplC : OverrideOptInC {
+    override fun <!OPT_IN_OVERRIDE("MarkerC; Base declaration of supertype 'OverrideOptInC' needs opt-in. The declaration override should be annotated with '@MarkerC' or '@OptIn(MarkerC::class)'")!>overrideOptIn<!>(){}
+}
+
+
+class OverrideOptInImplD : OverrideOptInD {
+    override fun <!OPT_IN_OVERRIDE("MarkerD; Base declaration of supertype 'OverrideOptInD' needs opt-in. The declaration override should be annotated with '@MarkerD' or '@OptIn(MarkerD::class)'")!>overrideOptIn<!>(){}
+}
+
