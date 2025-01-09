141c3127badc053db1ea0fc9ccc07719460d3279
Brian Norman <brian.norman@jetbrains.com>
2025-01-09 13:58:43 -0600

    [Test] Update Gradle integration test dependency versions
    In order to support running Gradle integration tests against Kotlin
    language version 2.2, Gradle plugins and libraries used within the
    builds must support Kotlin metadata version 2.2. This requires updating
    some plugin and library versions as some only supported up to Kotlin
    version 2.1.
    
    ^KT-71792

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -105,9 +105,9 @@ interface TestVersions {
         const val GOOGLE_DAGGER = "2.24"
         const val GRADLE_ENTERPRISE_PLUGIN_VERSION = "3.13.4"
         const val GRADLE_DEVELOCITY_PLUGIN_VERSION = "3.18"
-        const val KOTLINX_ATOMICFU = "0.25.0"
-        const val KOTLINX_KOVER = "0.9.0-RC"
-        const val KOTLINX_BINARY_COMPATIBILITY_VALIDATOR = "0.16.3"
+        const val KOTLINX_ATOMICFU = "0.27.0"
+        const val KOTLINX_KOVER = "0.9.0"
+        const val KOTLINX_BINARY_COMPATIBILITY_VALIDATOR = "0.17.0"
         const val DOKKA = "1.8.10"
         // TODO KT-70336 update Dokka version to a stable version when 2.0.0 is released 
         const val DOKKA_V2 = "2.0.20-dev-360"
