bf7d3b08271d8b85f296e5e20af986c40e7d512b
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-01-10 11:55:21 +0200

    [FIR] Reproduce ^KT-73945

---
+++ compiler/testData/diagnostics/tests/duplicateRedundantOpen.fir.kt
@@ -0,0 +1,9 @@
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-73945
+// WITH_EXTRA_CHECKERS
+
+interface InterfaceFoo {
+    <!REDUNDANT_MODALITY_MODIFIER, REDUNDANT_OPEN_IN_INTERFACE!>open<!> val a: Int
+}
+
+/* GENERATED_FIR_TAGS: interfaceDeclaration, propertyDeclaration */
