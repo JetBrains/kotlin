5a50dcfbd37a883e8b516be370b2bdf2e1195f9f
Brian Norman <brian.norman@jetbrains.com>
2025-01-10 05:44:01 -0600

    [Test] Update `UnstableSmartcastOnDelegatedProperties` tests (Part 15)
    Update tests related to the `UnstableSmartcastOnDelegatedProperties`
    language feature (see KT-57417). Now that this language feature is
    enabled by default in Kotlin 2.2, test data within `.latestLV.kt` files
    becomes the expected result.
    
    ^KT-71792

--- compiler/testData/diagnostics/tests/smartCasts/varnotnull/classDelegationDeprecated.fir.kt
+++ compiler/testData/diagnostics/tests/smartCasts/varnotnull/classDelegationDeprecated.fir.kt
@@ -1,5 +1,4 @@
-// RUN_PIPELINE_TILL: BACKEND
-// LATEST_LV_DIFFERENCE
+// RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-57417
 
 interface HasProperty {
@@ -9,5 +8,5 @@ interface HasProperty {
 class Test(delegate: HasProperty) : HasProperty by delegate
 
 fun test(a: Test) {
-    if (a.property != null) <!DEPRECATED_SMARTCAST_ON_DELEGATED_PROPERTY!>a.property<!> + 1
+    if (a.property != null) <!SMARTCAST_IMPOSSIBLE!>a.property<!> + 1
 }
