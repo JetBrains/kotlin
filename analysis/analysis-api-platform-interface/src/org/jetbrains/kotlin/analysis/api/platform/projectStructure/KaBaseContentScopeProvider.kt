3e26fa2e57848678d1ef9ad0e27f3bad8d24868e
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-01-15 18:32:30 +0100

    [Analysis API] Implement `KaContentScopeProvider` service
    ^KT-73290

---
+++ analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/projectStructure/KaBaseContentScopeProvider.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.platform.projectStructure
+
+import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
+
+public class KaBaseContentScopeProvider : KaContentScopeProvider {
+    override fun getRefinedContentScope(module: KaModule): GlobalSearchScope {
+        var resultingScope = module.contentScope
+
+        val refiners = KotlinContentScopeRefiner.getRefiners(module.project)
+
+        if (refiners.isEmpty()) return resultingScope
+
+        refiners.flatMap {
+            it.getEnlargementScopes(module).filter { scope ->
+                !GlobalSearchScope.isEmptyScope(scope)
+            }
+        }.forEach {
+            resultingScope = resultingScope.union(it)
+        }
+
+        refiners.flatMap {
+            it.getRestrictionScopes(module).filter { scope ->
+                !GlobalSearchScope.isEmptyScope(scope)
+            }
+        }.forEach {
+            resultingScope = resultingScope.intersectWith(GlobalSearchScope.notScope(it))
+        }
+
+        return resultingScope
+    }
+}
