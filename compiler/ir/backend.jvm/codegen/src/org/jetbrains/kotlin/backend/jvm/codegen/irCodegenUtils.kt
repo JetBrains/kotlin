6c6d459005611327a25f90a2b2f2eb019042d00f
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2025-01-17 13:42:53 +0100

    [Tests] Add missing JVM modifiers to inner classes
    #KT-74568

--- compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/irCodegenUtils.kt
+++ compiler/ir/backend.jvm/codegen/src/org/jetbrains/kotlin/backend/jvm/codegen/irCodegenUtils.kt
@@ -113,9 +113,9 @@ fun IrClass.calculateInnerClassAccessFlags(context: JvmBackendContext): Int {
         else -> getVisibilityAccessFlag()
     }
     return visibility or
-            if (origin.isSynthetic) Opcodes.ACC_SYNTHETIC else 0 or
-                    innerAccessFlagsForModalityAndKind() or
-                    if (isInner) 0 else Opcodes.ACC_STATIC
+            (if (origin.isSynthetic) Opcodes.ACC_SYNTHETIC else 0) or
+            innerAccessFlagsForModalityAndKind() or
+            (if (isInner) 0 else Opcodes.ACC_STATIC)
 }
 
 private fun IrClass.innerAccessFlagsForModalityAndKind(): Int {
