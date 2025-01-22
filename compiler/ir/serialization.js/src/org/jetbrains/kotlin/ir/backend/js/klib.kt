7357b77f8ea69e59c61bac7d6daadd1735bc2cb7
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-01-22 10:42:51 +0100

    [KLib] Reuse existing symbols during serialization for inlined IR
    #KT-64812

--- compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/klib.kt
+++ compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/klib.kt
@@ -599,6 +599,7 @@ fun serializeModuleIntoKlib(
                     normalizeAbsolutePaths = normalizeAbsolutePaths,
                     sourceBaseDirs = sourceBaseDirs,
                     shouldCheckSignaturesOnUniqueness = shouldCheckSignaturesOnUniqueness,
+                    reuseExistingSignaturesForSymbols = languageVersionSettings.supportsFeature(LanguageFeature.IrInlinerBeforeKlibSerialization),
                 ),
                 irDiagnosticReporter,
                 irBuiltins,
