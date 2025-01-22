3469f25cdebd9f8b3710c19c4a4fccfadbc1e305
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-01-22 19:10:07 +0100

    FE: Adjust test data not to use unintended lambda type conversion
    See more details at KTLC-276

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirBlackBoxCodegenBasedTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirBlackBoxCodegenBasedTestGenerated.java
@@ -21137,6 +21137,12 @@ public class LLFirBlackBoxCodegenBasedTestGenerated extends AbstractLLFirBlackBo
     }
 
     @Test
+    @TestMetadata("selectingLambdas.kt")
+    public void testSelectingLambdas() {
+      runTest("compiler/testData/codegen/box/extensionFunctions/selectingLambdas.kt");
+    }
+
+    @Test
     @TestMetadata("shared.kt")
     public void testShared() {
       runTest("compiler/testData/codegen/box/extensionFunctions/shared.kt");
@@ -22630,12 +22636,6 @@ public class LLFirBlackBoxCodegenBasedTestGenerated extends AbstractLLFirBlackBo
     @TestMetadata("SamWithReceiverMavenProjectImportHandler.kt")
     public void testSamWithReceiverMavenProjectImportHandler() {
       runTest("compiler/testData/codegen/box/fir/SamWithReceiverMavenProjectImportHandler.kt");
-    }
-
-    @Test
-    @TestMetadata("selectingLambdas.kt")
-    public void testSelectingLambdas() {
-      runTest("compiler/testData/codegen/box/fir/selectingLambdas.kt");
     }
 
     @Test
