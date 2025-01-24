b2c0c053a79c6aef5650f757ce8d0233e28aab26
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-01-24 19:54:08 +0100

    fixup! [IR] Enable inlining of public function on the first phase

--- compiler/ir/ir.inline/build.gradle.kts
+++ compiler/ir/ir.inline/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     compileOnly(project(":compiler:ir.tree"))
     compileOnly(project(":compiler:ir.backend.common"))
     compileOnly(project(":compiler:ir.backend.native"))
+    compileOnly(project(":compiler:ir.serialization.native"))
     compileOnly(project(":compiler:ir.serialization.common"))
 }
 
