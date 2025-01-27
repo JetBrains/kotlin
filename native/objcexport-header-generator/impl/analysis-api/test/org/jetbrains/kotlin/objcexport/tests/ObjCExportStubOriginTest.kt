fa17887436715b7d3245d9c5fc7b99d48226aaa9
Elijah Semyonov <elijah.semyonov@jetbrains.com>
2025-01-27 14:17:11 +0100

    [ObjCExport] Add `origin` to some ObjC translated members
    Copy `origin` from translated Objective-C @interface for Kotlin `object`
    and `companion object` to generated Objective-C accessor members. This
    allows the IDE to avoid post-processing phase where context for
    deriving it is partially omitted. ^KT-74700 fixed.

---
+++ native/objcexport-header-generator/impl/analysis-api/test/org/jetbrains/kotlin/objcexport/tests/ObjCExportStubOriginTest.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.objcexport.tests
+
+import org.jetbrains.kotlin.analysis.api.symbols.KaClassKind
+import org.jetbrains.kotlin.backend.konan.objcexport.*
+import org.jetbrains.kotlin.objcexport.*
+import org.jetbrains.kotlin.objcexport.testUtils.InlineSourceCodeAnalysis
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+
+class ObjCExportStubOriginTest(
+    private val inlineSourceCodeAnalysis: InlineSourceCodeAnalysis,
+) {
+    // Tests for scenarios covered in KT-74700
+    @Test
+    fun `test - generated accessors have same origin as interface for object`() {
+        inlineSourceCodeAnalysis.createObjCExportFile(
+            """
+                object SomeObject {
+                    const val x = 42
+                }
+            """.trimIndent()
+        ) { file ->
+            val classSymbols = file.classifierSymbols
+            val objectSymbol = classSymbols.single()
+            val objectStub = translateToObjCExportStub(objectSymbol)
+            assertIs<ObjCInterface>(objectStub, "Object stub should be an interface")
+
+            val sharedProperty = objectStub.members.firstOrNull {
+                it is ObjCProperty && it.name == ObjCPropertyNames.objectPropertyName
+            }
+            assertNotNull(sharedProperty, "Object stub should have a `shared` property")
+
+            val namedAccessor = objectStub.members.firstOrNull {
+                it is ObjCMethod && it.name == "someObject"
+            }
+            assertNotNull(namedAccessor, "Object stub should have a named accessor")
+
+            assertIs<ObjCExportStubOrigin.Source>(
+                objectStub.origin,
+                "Object stub should have a source origin"
+            )
+
+            assertEquals(
+                objectStub.origin,
+                sharedProperty.origin,
+                "Object class and its shared property should have the same origin"
+            )
+
+            assertEquals(
+                objectStub.origin,
+                namedAccessor.origin,
+                "Object class and its named accessor should have the same origin"
+            )
+        }
+    }
+
+    @Test
+    fun `test - generated accessors have same origin as interface for companion object`() {
+        inlineSourceCodeAnalysis.createObjCExportFile(
+            """
+                class SomeClass {
+                    companion object {
+                        val x = 42
+                    }
+                }
+            """.trimIndent()
+        ) { file ->
+            val classSymbols = file.classifierSymbols
+            val companionObjectSymbol = classSymbols.firstOrNull {
+                it.classKind == KaClassKind.COMPANION_OBJECT
+            }
+            assertNotNull(companionObjectSymbol, "Companion object symbol should be present")
+
+            val classSymbol = classSymbols.firstOrNull {
+                it.classKind == KaClassKind.CLASS
+            }
+            assertNotNull(classSymbol, "Class symbol should be present")
+
+            val companionObjectStub = translateToObjCExportStub(companionObjectSymbol)
+            assertIs<ObjCInterface>(companionObjectStub, "Object stub should be an interface")
+
+            val classStub = translateToObjCExportStub(classSymbol)
+            assertIs<ObjCInterface>(classStub, "Class stub should be an interface")
+
+            val sharedProperty = companionObjectStub.members.firstOrNull {
+                it is ObjCProperty && it.name == ObjCPropertyNames.objectPropertyName
+            }
+            assertNotNull(sharedProperty, "Companion object stub should have a `shared` property")
+
+            val companionProperty = classStub.members.firstOrNull {
+                it is ObjCProperty && it.name == ObjCPropertyNames.companionObjectPropertyName
+            }
+            assertNotNull(companionProperty, "Class stub should have a `companion` property")
+
+            val origin = companionObjectStub.origin
+            assertIs<ObjCExportStubOrigin.Source>(
+                origin,
+                "Companion object stub should have a source origin"
+            )
+
+            assertEquals(
+                origin,
+                sharedProperty.origin,
+                "Shared property should have the same origin as its class"
+            )
+
+            assertEquals(
+                origin,
+                companionProperty.origin,
+                "Class companion property should has the same origin as its companion class"
+            )
+        }
+    }
+}
