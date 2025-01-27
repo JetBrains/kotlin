8b48207f5673efba80e2bd0e98c3bcb1cb7d7b18
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-01-27 13:15:05 +0100

    [KDF] Junie added support for DataFrame.renameToCamelCase()
    Moved from https://github.com/Kotlin/dataframe/commit/20585b59283aa343e9c2cd6e739ed7a709c35416

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/loadInterpreter.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/loadInterpreter.kt
@@ -102,6 +102,7 @@ import org.jetbrains.kotlinx.dataframe.plugin.impl.api.TrimMargin
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.Update0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.UpdateWith0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.ValueCounts
+import org.jetbrains.kotlinx.dataframe.plugin.impl.api.RenameToCamelCase
 import org.jetbrains.kotlinx.dataframe.plugin.utils.Names
 
 internal fun FirFunctionCall.loadInterpreter(session: FirSession): Interpreter<*>? {
@@ -251,6 +252,7 @@ internal inline fun <reified T> String.load(): T {
         "Aggregate" -> Aggregate()
         "DataFrameOf3" -> DataFrameOf3()
         "ValueCounts" -> ValueCounts()
+        "RenameToCamelCase" -> RenameToCamelCase()
         else -> error("$this")
     } as T
 }
