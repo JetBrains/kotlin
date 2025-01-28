0dc2193a8f6af064c09b3037be0e355cb6287b2f
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2025-01-28 20:52:11 +0100

    fixup! K2: soften requirements for supertype arguments accessibility

--- compiler/testData/diagnostics/tests/multimodule/SupertypesWithInaccessibleTypeArguments.fir.kt
+++ compiler/testData/diagnostics/tests/multimodule/SupertypesWithInaccessibleTypeArguments.fir.kt
@@ -16,12 +16,22 @@ interface BoxedConcreteType : Box<InaccessibleConcreteSuperType>
 interface BoxedGenericType : Box<InaccessibleGenericSuperType<Nothing>>
 interface BoxedGenericTypeWithGeneric<T> : Box<T>
 interface ConcreteSuperType : InaccessibleConcreteSuperType
+interface VeryConcreteSuperType : InaccessibleConcreteSuperType
 
 // MODULE: end(middle)
 // FILE: end.kt
 
+// Type argument of 2nd level supertype is inaccessible
 interface BoxedConcreteTypeImplementation : BoxedConcreteType
 
+// Type argument of 2nd level supertype is inaccessible
 interface BoxedGenericTypeImplementation : BoxedGenericType
 
+// Supertype of type argument of direct supertype is inaccessible
 <!MISSING_DEPENDENCY_SUPERCLASS_IN_TYPE_ARGUMENT!>interface BoxedGenericTypeWithGenericImplementation<!> : BoxedGenericTypeWithGeneric<ConcreteSuperType>
+
+// Type argument of supertype of type argument of direct supertype is inaccessible
+interface BoxedGenericTypeWithBoxedImplementation : BoxedGenericTypeWithGeneric<BoxedConcreteType>
+
+// 2nd level supertype of type argument of direct supertype is inaccessible
+<!MISSING_DEPENDENCY_SUPERCLASS_IN_TYPE_ARGUMENT!>interface VeryBoxedGenericTypeWithGenericImplementation<!> : BoxedGenericTypeWithGeneric<VeryConcreteSuperType>
