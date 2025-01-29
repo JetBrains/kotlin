97074a1ffa2a118db57b45e0abfbff98e60b6b84
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-01-29 14:43:14 +0200

    [Test] Make the DISABLE_JAVA_FACADE more granular
    Previously it was global, now it could be applied per module

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/ir/JvmIrBackendFacade.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/ir/JvmIrBackendFacade.kt
@@ -49,7 +49,7 @@ abstract class AbstractJvmIrBackendFacade(testServices: TestServices) : IrBacken
         // Currently there's a ton of diagnostic tests with incorrect Java code:
         // strictly speaking, compiling it with javac is not required for testing
         // Kotlin code
-        if (DISABLE_JAVA_FACADE !in testServices.moduleStructure.allDirectives) {
+        if (DISABLE_JAVA_FACADE !in module.directives) {
             val configuration = testServices.compilerConfigurationProvider.getCompilerConfiguration(module)
             javaCompilerFacade.compileJavaFiles(module, configuration, state.factory)
         }
