d80977aae36304dd4ef8d491ada2de60fc87f0e0
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-01-31 12:02:25 +0200

    [Swift export] Add unit tests for collection types

---
+++ native/swift/sir-light-classes/test/org/jetbrains/kotlin/sir/providers/CollectionTranslationTests.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.sir.providers
+
+import org.jetbrains.kotlin.export.test.InlineSourceCodeAnalysis
+import org.jetbrains.kotlin.sir.SirArrayType
+import org.jetbrains.kotlin.sir.SirDictionaryType
+import org.jetbrains.kotlin.sir.SirNominalType
+import org.jetbrains.kotlin.sir.providers.support.translate
+import org.jetbrains.kotlin.sir.providers.support.variableNamed
+import org.jetbrains.kotlin.sir.util.SirSwiftModule
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CollectionTranslationTests(
+    private val inlineSourceCodeAnalysis: InlineSourceCodeAnalysis,
+) {
+    @Test
+    fun `list property is translated to Swift array`() {
+        val file = inlineSourceCodeAnalysis.createKtFile(
+            """
+                val list: List<String> = listOf("a", "b", "c")
+            """.trimIndent()
+        )
+        translate(file) {
+            val list = it.variableNamed("list")!!
+            assertEquals(SirArrayType(SirNominalType(SirSwiftModule.string)), list.type)
+        }
+    }
+
+    @Test
+    fun `map property is translated to Swift dictionary`() {
+        val file = inlineSourceCodeAnalysis.createKtFile(
+            """
+                val map: Map<String, List<String>> = listOf("a", "b", "c")
+            """.trimIndent()
+        )
+        translate(file) {
+            val map = it.variableNamed("map")!!
+            val keyType = SirNominalType(SirSwiftModule.string)
+            val valueType = SirArrayType(SirNominalType(SirSwiftModule.string))
+            assertEquals(SirDictionaryType(keyType, valueType), map.type)
+        }
+    }
+}
--- native/swift/sir-light-classes/test/org/jetbrains/kotlin/sir/providers/support/SirTestExtensions.kt
+++ native/swift/sir-light-classes/test/org/jetbrains/kotlin/sir/providers/support/SirTestExtensions.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.sir.providers.support
 import org.jetbrains.kotlin.sir.SirClass
 import org.jetbrains.kotlin.sir.SirDeclaration
 import org.jetbrains.kotlin.sir.SirNominalType
+import org.jetbrains.kotlin.sir.SirVariable
 
 val SirClass.superClassDeclaration: SirClass?
     get() = (superClass as? SirNominalType)?.typeDeclaration as? SirClass
@@ -15,4 +16,9 @@ val SirClass.superClassDeclaration: SirClass?
 fun List<SirDeclaration>.classNamed(className: String): SirClass? {
     return filterIsInstance<SirClass>()
         .firstOrNull { it.name == className }
+}
+
+fun List<SirDeclaration>.variableNamed(propertyName: String): SirVariable? {
+    return filterIsInstance<SirVariable>()
+        .firstOrNull { it.name == propertyName }
 }
