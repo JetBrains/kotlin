27254636b51563de792b460ad50f27cf2278be59
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2025-01-31 15:35:17 +0100

    fixup! [Wasm] Move D8 to wasm package

---
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/d8/D8Exec.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.targets.js.d8
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+@OptIn(ExperimentalWasmDsl::class)
+typealias D8Exec = org.jetbrains.kotlin.gradle.targets.wasm.d8.D8Exec
