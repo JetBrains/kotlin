5d47eb572d23eaf0de8367d798a1915dce469985
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-03 14:57:53 +0100

    FE: Reflect diagnostic test data change related to KT-74912
    Those changes appeared with a fix for KT-67869 in previous commits
    
    Their nature is the following:
    - Now top-level lambdas are resolved within a synthetic call
    context
    - In some cases, we don't have `expectedTypeMismatchIsReportedInChecker`
    for the `ResolutionMode.WithExpectedType` set,
    so we just in case stick to leaving the original diagnostic
    which is generally `ArgumentTypeMismatch`
    
    See FirSyntheticCallGenerator.adaptForTrivialTypeMismatchToBeReportedInChecker
    
    Many of them would likely be fixed with KT-74545 when we set
    expectedTypeMismatchIsReportedInChecker=true for the variable initializer
    
    ^KT-74912 Related

--- compiler/testData/diagnostics/tests/functionAsExpression/NonSuspendAnonFuncAsInitializerOfSuspendFunctionType.fir.kt
+++ compiler/testData/diagnostics/tests/functionAsExpression/NonSuspendAnonFuncAsInitializerOfSuspendFunctionType.fir.kt
@@ -1,4 +1,4 @@
 // RUN_PIPELINE_TILL: FRONTEND
 fun test () {
-    val local: suspend () -> Unit = <!INITIALIZER_TYPE_MISMATCH("SuspendFunction0<Unit>; Function0<Unit>")!>fun () {}<!>;
+    val local: suspend () -> Unit = <!ARGUMENT_TYPE_MISMATCH!>fun () {}<!>;
 }
--- compiler/testData/diagnostics/tests/inference/lambdaInValInitializerWithAnonymousFunctions.fir.kt
+++ compiler/testData/diagnostics/tests/inference/lambdaInValInitializerWithAnonymousFunctions.fir.kt
@@ -1,10 +1,10 @@
 // RUN_PIPELINE_TILL: FRONTEND
 typealias SuspendFn = suspend () -> Unit
 
-val test1f: suspend () -> Unit = <!INITIALIZER_TYPE_MISMATCH!>fun () {}<!>
-val test2f: suspend Any.() -> Unit = <!INITIALIZER_TYPE_MISMATCH!>fun Any.() {}<!>
+val test1f: suspend () -> Unit = <!ARGUMENT_TYPE_MISMATCH!>fun () {}<!>
+val test2f: suspend Any.() -> Unit = <!ARGUMENT_TYPE_MISMATCH!>fun Any.() {}<!>
 
 // This is a bug in the old inference and should be fixed in new inference
 // see "Fix anonymous function literals handling in type checker" for more details
-val test3f: suspend Any.(Int) -> Int = <!INITIALIZER_TYPE_MISMATCH!>fun (k: Int) = k + 1<!>
-val test4f: SuspendFn = <!INITIALIZER_TYPE_MISMATCH!>fun Any.() {}<!>
+val test3f: suspend Any.(Int) -> Int = <!ARGUMENT_TYPE_MISMATCH!>fun (k: Int) = k + 1<!>
+val test4f: SuspendFn = <!ARGUMENT_TYPE_MISMATCH!>fun Any.() {}<!>
