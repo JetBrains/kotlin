cdcd92bbdeb544e1eb872085b4c0ae2a4ac27cf2
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-02-03 10:12:09 +0200

    [Test] Don't suggest kt47450.kt test to be promoted to backend stage
    This test non-deterministically fails/passes the backend with K1 frontend
    (sometimes it throws StackOverflowException), so automatic tier promotion
    is not applicable to it.

--- compiler/testData/ir/irText/expressions/kt47450.kt
+++ compiler/testData/ir/irText/expressions/kt47450.kt
@@ -1,6 +1,7 @@
 // FIR_IDENTICAL
 // SKIP_KT_DUMP
 // RUN_PIPELINE_TILL: FIR2IR
+// DISABLE_NEXT_PHASE_SUGGESTION: K1 backend flackyly fails with stack overflow error
 
 fun test(z: Int): String {
     return "" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
