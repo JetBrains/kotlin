e5c0ae378ec2dfab128d8a4b3cdd7d07c5eb2f04
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-02-03 12:35:35 +0100

    fixup! [Analysis API] Implement `KaContentScopeProvider` service
    * Properly register `KaContentScopeProvider` service
    * Mark it as `KaEngineService`
    * Refine KDoc

--- analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/projectStructure/KaContentScopeProvider.kt
+++ analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/projectStructure/KaContentScopeProvider.kt
@@ -8,11 +8,12 @@ package org.jetbrains.kotlin.analysis.api.platform.projectStructure
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.analysis.api.platform.KaEngineService
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 
-public interface KaContentScopeProvider {
+public interface KaContentScopeProvider : KaEngineService {
     /**
-     * Calculates a content scope for the given [KaModule] using [KotlinContentScopeRefiner]s
+     * Calculates a [KaModule.contentScope] from [KaModule.baseContentScope] for the given [KaModule] using [KotlinContentScopeRefiner]s.
      */
     public fun getRefinedContentScope(module: KaModule): GlobalSearchScope
 
