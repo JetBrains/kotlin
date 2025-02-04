dd5a10657b40982e7bd33fc378b1319a6c81fac8
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-04 18:45:51 +0100

    FE: Preserve context parameter names for anonymous functions
    Otherwise, UNRESOLVED_REFERENCE is reported on `a` usages.
    Without this change, the test was red even in the original state
    because we start resolving `val f = context(a: String) fun () = a`
    as it belongs to the call.

--- compiler/testData/codegen/box/contextParameters/contextualAnonymousFunction.kt
+++ compiler/testData/codegen/box/contextParameters/contextualAnonymousFunction.kt
@@ -1,7 +1,12 @@
 // IGNORE_BACKEND_K1: ANY
 // LANGUAGE: +ContextParameters
 
+fun foo(f: Any): String = (f as Function1<String, String>).invoke("OK")
+
 fun box(): String {
+    val x = foo(context(a: String) fun () = a)
+    if (x != "OK") return "fail"
+
     val f = context(a: String) fun () = a
 
     return with("OK") {
