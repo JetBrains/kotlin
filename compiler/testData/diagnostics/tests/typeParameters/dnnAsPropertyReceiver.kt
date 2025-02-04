c8b58b33ecf567da746176ab6e6ee3b8e425c3aa
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-02-04 13:18:56 +0100

    K2: unwrap DNN types in FirPropertyTypeParametersChecker
    #KT-74929 Fixed
    
    (cherry picked from commit d60eb00df3d876f9a5ffd58818947ef0c22122e9)

--- compiler/testData/diagnostics/tests/typeParameters/dnnAsPropertyReceiver.kt
+++ compiler/testData/diagnostics/tests/typeParameters/dnnAsPropertyReceiver.kt
@@ -1,8 +1,11 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// FIR_IDENTICAL
+// RUN_PIPELINE_TILL: BACKEND
 // ISSUE: KT-74929
 
 val <T : Number?> (T & Any).prop : T get() = this
 
 interface Base {
     val <T : Number?> (T & Any).prop : T
+
+    val <T : Number?> List<T & Any>.prop : T
 }
