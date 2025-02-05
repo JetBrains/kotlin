9b8d213178ba43e9cf643e29429ea7dc83be6322
Edoardo Luppi <lp.edoardo@gmail.com>
2025-02-05 18:54:24 +0100

    refactor: remove unnecessary blank lines

--- libraries/stdlib/js/src/kotlin/collectionJs.kt
+++ libraries/stdlib/js/src/kotlin/collectionJs.kt
@@ -98,7 +98,6 @@ internal actual inline fun <K, V> buildMapInternal(capacity: Int, builderAction:
     return LinkedHashMap<K, V>(capacity).apply(builderAction).build()
 }
 
-
 /**
  * Fills the list with the provided [value].
  *
@@ -193,7 +192,6 @@ internal actual inline fun <K, V> Map<K, V>.toSingletonMapOrSelf(): Map<K, V> = 
 @Suppress("NOTHING_TO_INLINE")
 internal actual inline fun <K, V> Map<out K, V>.toSingletonMap(): Map<K, V> = this.toMutableMap()
 
-
 @Suppress("NOTHING_TO_INLINE")
 internal actual inline fun <T> Array<out T>.copyToArrayOfAny(isVarargs: Boolean): Array<out Any?> =
     if (isVarargs)
@@ -201,8 +199,6 @@ internal actual inline fun <T> Array<out T>.copyToArrayOfAny(isVarargs: Boolean)
         this
     else
         this.copyOf()
-
-
 
 @PublishedApi
 internal actual fun checkIndexOverflow(index: Int): Int {
@@ -219,7 +215,6 @@ internal actual fun checkCountOverflow(count: Int): Int {
     }
     return count
 }
-
 
 /**
  * JS map and set implementations do not make use of capacities or load factors.
