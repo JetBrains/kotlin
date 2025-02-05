0ab6de39c1b21384e5e25b6f3671a64779729626
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-02-05 11:07:17 +0100

    fixup! Kapt: remove "kapt3" and "kapt4" from package names

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/KaptWithoutKotlincTask.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/internal/kapt/KaptWithoutKotlincTask.kt
@@ -289,9 +289,11 @@ private class KaptExecution @Inject constructor(
             try {
                 Class.forName("org.jetbrains.kotlin.kapt.base.$simpleName", true, this)
             } catch (_: ClassNotFoundException) { // in case we have an old plugin version on the classpath
-                Class.forName("org.jetbrains.kotlin.kapt3.base.$simpleName", true, this)
-            } catch (_: ClassNotFoundException) {
-                Class.forName("org.jetbrains.kotlin.base.kapt3.$simpleName", true, this)
+                try {
+                    Class.forName("org.jetbrains.kotlin.kapt3.base.$simpleName", true, this)
+                } catch (_: ClassNotFoundException) {
+                    Class.forName("org.jetbrains.kotlin.base.kapt3.$simpleName", true, this)
+                }
             }
 
         private var classLoadersCache: ClassLoadersCache? = null
