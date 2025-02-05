72a07ecf47b387142754820c24c2af22e51c6b14
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2025-02-05 18:04:47 +0100

    [Gradle] Add ToolingDiagnosticGroup unit tests
    ^KT-73849

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/diagnosticsTests/ToolingDiagnosticGroupEqualTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/diagnosticsTests/ToolingDiagnosticGroupEqualTest.kt
@@ -1,2 +1,78 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("FunctionName")
+
 package org.jetbrains.kotlin.gradle.unitTests.diagnosticsTests
 
+import org.jetbrains.kotlin.gradle.plugin.diagnostics.DiagnosticGroups
+import kotlin.test.*
+
+class ToolingDiagnosticGroupEqualTest {
+
+    @Test
+    fun `kgp groups with different categories are not equal`() {
+        val defaultGroup = DiagnosticGroups.KGP.Default
+        val misconfigurationGroup = DiagnosticGroups.KGP.Misconfiguration
+        val deprecationGroup = DiagnosticGroups.KGP.Deprecation
+        val experimentalGroup = DiagnosticGroups.KGP.Experimental
+
+        assert(defaultGroup != misconfigurationGroup) { "Default group should not be equal to Misconfiguration group" }
+        assert(defaultGroup != deprecationGroup) { "Default group should not be equal to Deprecation group" }
+        assert(defaultGroup != experimentalGroup) { "Default group should not be equal to Experimental group" }
+
+        assert(misconfigurationGroup != deprecationGroup) { "Misconfiguration group should not be equal to Deprecation group" }
+        assert(misconfigurationGroup != experimentalGroup) { "Misconfiguration group should not be equal to Experimental group" }
+
+        assert(deprecationGroup != experimentalGroup) { "Deprecation group should not be equal to Experimental group" }
+    }
+
+    @Test
+    fun `kgp groups should be equal to themselves`() {
+        val groups = listOf(
+            DiagnosticGroups.KGP.Default to DiagnosticGroups.KGP.Default,
+            DiagnosticGroups.KGP.Misconfiguration to DiagnosticGroups.KGP.Misconfiguration,
+            DiagnosticGroups.KGP.Deprecation to DiagnosticGroups.KGP.Deprecation,
+            DiagnosticGroups.KGP.Experimental to DiagnosticGroups.KGP.Experimental
+        )
+
+        groups.forEach { (group1, group2) ->
+            assert(group1 == group2) { "${group1.groupPath} group should be equal to itself" }
+        }
+    }
+
+    @Test
+    fun `kgp groups should be consistent with hashCode`() {
+        val groups = listOf(
+            DiagnosticGroups.KGP.Default to DiagnosticGroups.KGP.Default,
+            DiagnosticGroups.KGP.Misconfiguration to DiagnosticGroups.KGP.Misconfiguration,
+            DiagnosticGroups.KGP.Deprecation to DiagnosticGroups.KGP.Deprecation,
+            DiagnosticGroups.KGP.Experimental to DiagnosticGroups.KGP.Experimental
+        )
+
+        val distinctGroups = listOf(
+            DiagnosticGroups.KGP.Default to DiagnosticGroups.KGP.Misconfiguration,
+            DiagnosticGroups.KGP.Default to DiagnosticGroups.KGP.Deprecation,
+            DiagnosticGroups.KGP.Default to DiagnosticGroups.KGP.Experimental,
+            DiagnosticGroups.KGP.Misconfiguration to DiagnosticGroups.KGP.Deprecation,
+            DiagnosticGroups.KGP.Misconfiguration to DiagnosticGroups.KGP.Experimental,
+            DiagnosticGroups.KGP.Deprecation to DiagnosticGroups.KGP.Experimental
+        )
+
+        // Same groups should have same hashCode
+        groups.forEach { (group1, group2) ->
+            assert(group1.hashCode() == group2.hashCode()) {
+                "${group1.groupPath} group should have same hashCode with itself"
+            }
+        }
+
+        // Different groups should have different hashCodes
+        distinctGroups.forEach { (group1, group2) ->
+            assert(group1.hashCode() != group2.hashCode()) {
+                "${group1.groupPath} and ${group2.groupPath} groups should have different hashCodes"
+            }
+        }
+    }
+}
--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/diagnosticsTests/ToolingDiagnosticGroupTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/diagnosticsTests/ToolingDiagnosticGroupTest.kt
@@ -1,4 +1,76 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("FunctionName")
+
 package org.jetbrains.kotlin.gradle.unitTests.diagnosticsTests
 
+import org.jetbrains.kotlin.gradle.plugin.diagnostics.DiagnosticGroups
+import kotlin.test.*
+
 class ToolingDiagnosticGroupTest {
+
+    @Test
+    fun `kgp groups should have correct group paths`() {
+        val expectedPaths = mapOf(
+            DiagnosticGroups.KGP.Default to "kotlin:kgp",
+            DiagnosticGroups.KGP.Deprecation to "kotlin:kgp:deprecation",
+            DiagnosticGroups.KGP.Misconfiguration to "kotlin:kgp:misconfiguration",
+            DiagnosticGroups.KGP.Experimental to "kotlin:kgp:experimental"
+        )
+
+        expectedPaths.forEach { (group, expectedPath) ->
+            assert(group.groupPath == expectedPath) {
+                "Group ${group.groupPath} should have path $expectedPath"
+            }
+        }
+    }
+
+    @Test
+    fun `kgp groups should have correct display names`() {
+        val expectedDisplayNames = mapOf(
+            DiagnosticGroups.KGP.Default to "Kotlin Gradle Plugin",
+            DiagnosticGroups.KGP.Deprecation to "Kotlin Gradle Plugin Deprecation",
+            DiagnosticGroups.KGP.Misconfiguration to "Kotlin Gradle Plugin Misconfiguration",
+            DiagnosticGroups.KGP.Experimental to "Kotlin Gradle Plugin Experimental Feature"
+        )
+
+        expectedDisplayNames.forEach { (group, expectedDisplayName) ->
+            assert(group.displayName == expectedDisplayName) {
+                "Group ${group.groupPath} should have display name $expectedDisplayName"
+            }
+        }
+    }
+
+    @Test
+    fun `kgp groups should have parent references`() {
+        val expectedParents = mapOf(
+            DiagnosticGroups.KGP.Default to DiagnosticGroups.Kotlin,
+            DiagnosticGroups.KGP.Deprecation to DiagnosticGroups.Kotlin,
+            DiagnosticGroups.KGP.Misconfiguration to DiagnosticGroups.Kotlin,
+            DiagnosticGroups.KGP.Experimental to DiagnosticGroups.Kotlin
+        )
+
+        expectedParents.forEach { (group, expectedParent) ->
+            assert(group.parent == expectedParent) {
+                "Group ${group.groupPath} should have parent ${expectedParent.groupPath}"
+            }
+        }
+    }
+
+    @Test
+    fun `kgp groups should have unique group ids`() {
+        val groupIds = listOf(
+            DiagnosticGroups.KGP.Default.groupId,
+            DiagnosticGroups.KGP.Deprecation.groupId,
+            DiagnosticGroups.KGP.Misconfiguration.groupId,
+            DiagnosticGroups.KGP.Experimental.groupId
+        )
+
+        assert(groupIds.toSet().size == groupIds.size) {
+            "All KGP diagnostic groups should have unique group IDs"
+        }
+    }
 }
