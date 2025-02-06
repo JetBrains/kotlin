d29d303ec9742dc6916a71c75c337c7a52a6d9ac
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-06 14:48:39 +0100

    Reproduce KT-67869
    NB: LANGUAGE directives are commented there until the language feature
    is introduced

---
+++ compiler/fir/analysis-tests/testData/resolve/inference/lambdasReturnsShareCSFeatureDisabled.kt
@@ -0,0 +1,24 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// SKIP_FIR_DUMP
+// ISSUE: KT-67869
+//// LANGUAGE: -ResolveTopLevelLambdasAsSyntheticCallArgument
+
+fun expectAny(a: Any) {}
+
+var b: Boolean = false
+
+fun <E> myEmptyList(): List<E> = TODO()
+
+fun main() {
+    expectAny(x@{
+        if (b) return@x myEmptyList()
+
+        myEmptyList<String>()
+    })
+}
+
+val x: Any = x@{
+    if (b) return@x <!CANNOT_INFER_PARAMETER_TYPE, NEW_INFERENCE_NO_INFORMATION_FOR_PARAMETER!>myEmptyList<!>()
+
+    myEmptyList<String>()
+}
