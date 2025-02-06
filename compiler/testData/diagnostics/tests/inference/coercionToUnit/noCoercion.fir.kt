2253b3f32ef82791488d18e68ae73a0fb6e49ea3
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-02-06 17:56:52 +0100

    Reapply "FE: Reflect diagnostic test data change after the fix for KT-67869"
    This reverts commit 9d50a6bba1f6ce9aa26bfdfb09089869a9a0351a.

--- compiler/testData/diagnostics/tests/inference/coercionToUnit/noCoercion.fir.kt
+++ compiler/testData/diagnostics/tests/inference/coercionToUnit/noCoercion.fir.kt
@@ -10,9 +10,9 @@ fun noCoercionLastExpressionUsedAsReturnArgument() {
 }
 
 fun noCoercionBlockHasExplicitType() {
-    val b: () -> Int = <!INITIALIZER_TYPE_MISMATCH!>{
-        if (true) 42
-    }<!>
+    val b: () -> Int = {
+        <!RETURN_TYPE_MISMATCH!>if (true) 42<!>
+    }
 }
 
 fun noCoercionBlockHasExplicitReturn() {
