JetBrains/kotlin

commit f949c1508d8a80f3cbe058bc4206ae8a5b3493b1

IR Scripting: force script plugin application in IDE's eval expr.
Since the script lowering was moved to the scripting plugin, expression
evaluation in IDE is broken for Kotlin scripts, because the IR compiler
plugin extensions are disabled in this scenario.
This fix introduces the flag to enforce plugin extension application in
this scenario, and enables it for the script lowering extension.

#KT-74004 fixed

compiler/ir/backend.jvm/entrypoint/src/org/jetbrains/kotlin/backend/jvm/JvmIrCodegenFactory.kt
@@ -121,6 +121,13 @@ class JvmIrCodegenFactory(
         val allBuiltins: List<IrFile>,
     )
 
+    /**
+     * If the extension is marked with this marker, it is applied even on expression evaluation in the IDE
+     *
+     * see [convertToIr] for implementation details
+     */
+    interface IrGeneratorExtensionMarkerForExpressionEvaluation
+
     fun convertAndGenerate(files: Collection<KtFile>, state: GenerationState, bindingContext: BindingContext) {
         ProgressIndicatorAndCompilationCanceledStatus.checkCanceled()
         val backendInput = convertToIr(state, files, bindingContext)
@@ -217,21 +224,22 @@ class JvmIrCodegenFactory(
             irLinker,
             messageCollector,
             diagnosticReporter
-        ).takeIf { !ideCodegenSettings.doNotLoadDependencyModuleHeaders }
-        if (pluginExtensions.isNotEmpty() && pluginContext != null) {
-            for (extension in pluginExtensions) {
-                if (psi2irContext.configuration.generateBodies ||
-                    @OptIn(FirIncompatiblePluginAPI::class) extension.shouldAlsoBeAppliedInKaptStubGenerationMode
-                ) {
-                    psi2ir.addPostprocessingStep { module ->
-                        val old = stubGenerator.unboundSymbolGeneration
-                        try {
-                            stubGenerator.unboundSymbolGeneration = true
-                            extension.generate(module, pluginContext)
-                        } finally {
-                            stubGenerator.unboundSymbolGeneration = old
-                        }
-                    }
+        )
+        val skipRegularPlugins = ideCodegenSettings.doNotLoadDependencyModuleHeaders
+        for (extension in pluginExtensions) {
+            if (skipRegularPlugins && extension !is IrGeneratorExtensionMarkerForExpressionEvaluation) continue
+
+            if (!psi2irContext.configuration.generateBodies &&
+                !@OptIn(FirIncompatiblePluginAPI::class) extension.shouldAlsoBeAppliedInKaptStubGenerationMode
+            ) continue
+
+            psi2ir.addPostprocessingStep { module ->
+                val old = stubGenerator.unboundSymbolGeneration
+                try {
+                    stubGenerator.unboundSymbolGeneration = true
+                    extension.generate(module, pluginContext)
+                } finally {
+                    stubGenerator.unboundSymbolGeneration = old
                 }
             }
         }

plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/extensions/ScriptLoweringExtension.kt
@@ -7,12 +7,13 @@ package org.jetbrains.kotlin.scripting.compiler.plugin.extensions
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.backend.jvm.JvmIrCodegenFactory.IrGeneratorExtensionMarkerForExpressionEvaluation
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.scripting.compiler.plugin.irLowerings.JvmSymbolsForScripting
 import org.jetbrains.kotlin.scripting.compiler.plugin.irLowerings.ScriptsToClassesLowering
 
-class ScriptLoweringExtension : IrGenerationExtension {
+class ScriptLoweringExtension : IrGenerationExtension, IrGeneratorExtensionMarkerForExpressionEvaluation {
     override fun generate(
         moduleFragment: IrModuleFragment,
         pluginContext: IrPluginContext,

commit faf77ecf43951b04720214c81a8df7fc20a44ff3

[JVM] Filter out regular IR extensions if IDE settings tell us so
This fixes evaluate-expression regression introduced in f949c150

^KT-74827: Fixed

compiler/ir/backend.jvm/entrypoint/src/org/jetbrains/kotlin/backend/jvm/JvmIrCodegenFactory.kt
@@ -144,6 +144,11 @@ class JvmIrCodegenFactory(
         )
     }
 
+    private val Project.filteredExtensions: List<IrGenerationExtension>
+        get() = IrGenerationExtension.getInstances(this)
+            .filter { !ideCodegenSettings.doNotLoadDependencyModuleHeaders || it is IrGeneratorExtensionMarkerForExpressionEvaluation }
+
+
     @OptIn(ObsoleteDescriptorBasedAPI::class)
     fun convertToIr(
         project: Project,
@@ -194,8 +199,6 @@ class JvmIrCodegenFactory(
             (psi2irContext.irBuiltIns as? IrBuiltInsOverDescriptors)?.let { symbolTable.bindSymbolFinder(it.symbolFinder) }
         }
 
-        val pluginExtensions = IrGenerationExtension.getInstances(project)
-
         val stubGenerator =
             DeclarationStubGeneratorImpl(
                 psi2irContext.moduleDescriptor, symbolTable, psi2irContext.irBuiltIns,
@@ -227,10 +230,7 @@ class JvmIrCodegenFactory(
             messageCollector,
             diagnosticReporter
         )
-        val skipRegularPlugins = ideCodegenSettings.doNotLoadDependencyModuleHeaders
-        for (extension in pluginExtensions) {
-            if (skipRegularPlugins && extension !is IrGeneratorExtensionMarkerForExpressionEvaluation) continue
-
+        for (extension in project.filteredExtensions) {
             if (!psi2irContext.configuration.generateBodies &&
                 !@OptIn(FirIncompatiblePluginAPI::class) extension.shouldAlsoBeAppliedInKaptStubGenerationMode
             ) continue
@@ -340,7 +340,7 @@ class JvmIrCodegenFactory(
         if (evaluatorFragmentInfoForPsi2Ir != null) {
             context.evaluatorData = JvmEvaluatorData(mutableMapOf(), evaluatorFragmentInfoForPsi2Ir.methodIR)
         }
-        val generationExtensions = IrGenerationExtension.getInstances(state.project)
+        val generationExtensions = state.project.filteredExtensions
             .mapNotNull { it.getPlatformIntrinsicExtension(context) as? JvmIrIntrinsicExtension }
         val intrinsics by lazy { IrIntrinsicMethods(irBuiltIns, context.ir.symbols) }
         context.getIntrinsic = { symbol: IrFunctionSymbol ->