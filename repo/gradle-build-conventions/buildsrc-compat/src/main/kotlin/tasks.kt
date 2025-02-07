148dfd970e7c830fd9f383065c64c45480676444
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-02-07 23:03:52 +0100

    JUnit4 Mute agent

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
@@ -158,6 +158,10 @@ fun Project.projectTest(
         project.dependencies {
             "testImplementation"(project(":compiler:tests-mutes:mutes-junit5"))
         }
+    } else {
+        project.dependencies {
+            "testImplementation"(project(":compiler:tests-mutes:mutes-junit4"))
+        }
     }
     val shouldInstrument = project.providers.gradleProperty("kotlin.test.instrumentation.disable")
         .orNull?.toBoolean() != true
@@ -169,6 +173,12 @@ fun Project.projectTest(
         inputs.dir(File(rootDir, "build/ideaHomeForTests")).withPathSensitivity(PathSensitivity.RELATIVE)
 
         muteWithDatabase()
+        if (jUnitMode == JUnitMode.JUnit4) {
+            jvmArgs(
+                "-javaagent:${classpath.find { it.name.contains("junit-foundation") }?.absolutePath ?:
+                error("junit-foundation not found in ${classpath.joinToString("\n")}")}"
+            )
+        }
 
         doFirst {
             if (jUnitMode == JUnitMode.JUnit5) return@doFirst
