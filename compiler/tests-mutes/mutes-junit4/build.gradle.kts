4b53faa3e73ef1eb333c9db593f033a80b535ceb
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-02-07 23:03:52 +0100

    JUnit4 Mute agent

--- compiler/tests-mutes/mutes-junit4/build.gradle.kts
+++ compiler/tests-mutes/mutes-junit4/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 dependencies {
     api(libs.junit4)
     implementation(project(":compiler:tests-mutes"))
+    api("com.nordstrom.tools:junit-foundation:17.1.1")
 }
 
 sourceSets {
     "main" { projectDefault() }
-    "test" {}
 }
