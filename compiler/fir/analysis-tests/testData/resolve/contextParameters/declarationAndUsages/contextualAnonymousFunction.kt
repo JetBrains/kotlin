2a38fc4ff40f07f3bc414091fef6a5af05e8ef03
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-02-10 13:28:43 +0100

    [Tests] Reproduce #KT-75009

--- compiler/fir/analysis-tests/testData/resolve/contextParameters/declarationAndUsages/contextualAnonymousFunction.kt
+++ compiler/fir/analysis-tests/testData/resolve/contextParameters/declarationAndUsages/contextualAnonymousFunction.kt
@@ -1,13 +1,17 @@
-// RUN_PIPELINE_TILL: BACKEND
+// RUN_PIPELINE_TILL: FRONTEND
 // LANGUAGE: +ContextParameters
 
 annotation class Ann
 class A
 
-val t = context(a: A) fun () { }
-val t2 = @Ann context(a: A) fun () { }
+fun runWithA(block: context(String) () -> Unit) {
+}
+
+val t = context(a: A) fun () { a }
+val t2 = @Ann context(a: A) fun () { a }
 
 fun foo() {
-    val t = context(a: A) fun () { }
-    val t2 = @Ann context(a: A) fun () { }
+    val t = context(a: A) fun () { a }
+    val t2 = @Ann context(a: A) fun () { a }
+    runWithA(context(a: String) fun () { <!UNRESOLVED_REFERENCE!>a<!> })
 }
