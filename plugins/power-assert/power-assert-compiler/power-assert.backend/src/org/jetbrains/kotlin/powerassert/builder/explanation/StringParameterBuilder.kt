smileding/kotlin-2.0.0

Add PowerAssert annotations and support in compiler-plugin

plugins/power-assert/power-assert-compiler/power-assert.backend/src/org/jetbrains/kotlin/powerassert/builder/explanation/StringParameterBuilder.kt
@@ -1,0 +1,72 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.powerassert.builder.explanation
+
+import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
+import org.jetbrains.kotlin.ir.builders.irCall
+import org.jetbrains.kotlin.ir.builders.irConcat
+import org.jetbrains.kotlin.ir.builders.irString
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.expressions.*
+import org.jetbrains.kotlin.ir.types.classOrNull
+import org.jetbrains.kotlin.ir.util.functions
+import org.jetbrains.kotlin.powerassert.diagram.IrTemporaryVariable
+
+class StringParameterBuilder(
+    private val factory: ExplanationFactory,
+    private val function: IrSimpleFunction,
+    private val messageArgument: IrExpression?,
+    private val delegate: CallDiagramParameterBuilder,
+) : ParameterBuilder {
+
+    override fun build(
+        builder: IrBuilderWithScope,
+        dispatchReceiver: List<IrTemporaryVariable>?,
+        extensionReceiver: List<IrTemporaryVariable>?,
+        valueParameters: Map<String, List<IrTemporaryVariable>>,
+    ): IrExpression {
+        val prefix = messageArgument?.let { builder.buildMessagePrefix(it, function.valueParameters.last()) }
+
+        val diagram = delegate.build(builder, dispatchReceiver, extensionReceiver, valueParameters)
+        val message = with(factory) { builder.irDefaultMessage(diagram) }
+
+        return builder.irConcat().apply {
+            if (prefix != null) addArgument(prefix)
+            addArgument(builder.irString("\n"))
+            addArgument(message)
+        }
+    }
+
+    private fun IrBuilderWithScope.buildMessagePrefix(
+        messageArgument: IrExpression?,
+        messageParameter: IrValueParameter,
+    ): IrExpression? {
+        return when (messageArgument) {
+            null -> null
+            is IrConst -> messageArgument
+            is IrStringConcatenation -> messageArgument
+            is IrGetValue -> {
+                if (messageArgument.type == context.irBuiltIns.stringType) {
+                    return messageArgument
+                } else {
+                    val invoke = messageParameter.type.classOrNull!!.functions
+                        .filter { !it.owner.isFakeOverride } // TODO best way to find single access method?
+                        .single()
+                    irCall(invoke).apply { dispatchReceiver = messageArgument }
+                }
+            }
+            // Kotlin Lambda or SAMs conversion lambda
+            is IrFunctionExpression, is IrTypeOperatorCall -> {
+                val invoke = messageParameter.type.classOrNull!!.functions
+                    .filter { !it.owner.isFakeOverride } // TODO best way to find single access method?
+                    .single()
+                irCall(invoke).apply { dispatchReceiver = messageArgument }
+            }
+            else -> null
+        }
+    }
+}

plugins/power-assert/power-assert-compiler/tests-gen/org/jetbrains/kotlin/powerassert/FirLightTreeBlackBoxCodegenTestForPowerAssertGenerated.java
@@ -68,6 +68,40 @@ public class FirLightTreeBlackBoxCodegenTestForPowerAssertGenerated extends Abst
   }
 
   @Nested
+  @TestMetadata("plugins/power-assert/power-assert-compiler/testData/codegen/annotated")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Annotated {
+    @Test
+    public void testAllFilesPresentInAnnotated() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("plugins/power-assert/power-assert-compiler/testData/codegen/annotated"), Pattern.compile("^(.+)\\.kt$"), Pattern.compile("^(.+)\\.fir\\.kts?$"), TargetBackend.JVM_IR, true);
+    }
+
+    @Test
+    @TestMetadata("describe.kt")
+    public void testDescribe() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/describe.kt");
+    }
+
+    @Test
+    @TestMetadata("IgnoreParameter.kt")
+    public void testIgnoreParameter() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/IgnoreParameter.kt");
+    }
+
+    @Test
+    @TestMetadata("LocalVariables.kt")
+    public void testLocalVariables() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/LocalVariables.kt");
+    }
+
+    @Test
+    @TestMetadata("modules.kt")
+    public void testModules() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/modules.kt");
+    }
+  }
+
+  @Nested
   @TestMetadata("plugins/power-assert/power-assert-compiler/testData/codegen/arithmetic")
   @TestDataPath("$PROJECT_ROOT")
   public class Arithmetic {
@@ -692,6 +726,12 @@ public class FirLightTreeBlackBoxCodegenTestForPowerAssertGenerated extends Abst
     @Test
     public void testAllFilesPresentInParameters() {
       KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("plugins/power-assert/power-assert-compiler/testData/codegen/parameters"), Pattern.compile("^(.+)\\.kt$"), Pattern.compile("^(.+)\\.fir\\.kts?$"), TargetBackend.JVM_IR, true);
+    }
+
+    @Test
+    @TestMetadata("FluentBuilder.kt")
+    public void testFluentBuilder() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/parameters/FluentBuilder.kt");
     }
 
     @Test

JetBrains/kotlin

plugins/power-assert/power-assert-compiler/power-assert.backend/src/org/jetbrains/kotlin/powerassert/builder/explanation/StringParameterBuilder.kt
@@ -1,0 +1,70 @@
+/*
+ * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.powerassert.builder.explanation
+
+import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
+import org.jetbrains.kotlin.ir.builders.irCall
+import org.jetbrains.kotlin.ir.builders.irConcat
+import org.jetbrains.kotlin.ir.builders.irString
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.expressions.*
+import org.jetbrains.kotlin.ir.types.classOrNull
+import org.jetbrains.kotlin.ir.util.functions
+import org.jetbrains.kotlin.powerassert.diagram.IrTemporaryVariable
+
+class StringParameterBuilder(
+    private val factory: ExplanationFactory,
+    private val function: IrSimpleFunction,
+    private val messageArgument: IrExpression?,
+    private val delegate: CallDiagramParameterBuilder,
+) : ParameterBuilder {
+
+    override fun build(
+        builder: IrBuilderWithScope,
+        argumentVariables: Map<IrValueParameter, List<IrTemporaryVariable>>,
+    ): IrExpression {
+        val prefix = messageArgument?.let { builder.buildMessagePrefix(it, function.valueParameters.last()) }
+
+        val diagram = delegate.build(builder, argumentVariables)
+        val message = with(factory) { builder.irDefaultMessage(diagram) }
+
+        return builder.irConcat().apply {
+            if (prefix != null) addArgument(prefix)
+            addArgument(builder.irString("\n"))
+            addArgument(message)
+        }
+    }
+
+    private fun IrBuilderWithScope.buildMessagePrefix(
+        messageArgument: IrExpression?,
+        messageParameter: IrValueParameter,
+    ): IrExpression? {
+        return when (messageArgument) {
+            null -> null
+            is IrConst -> messageArgument
+            is IrStringConcatenation -> messageArgument
+            is IrGetValue -> {
+                if (messageArgument.type == context.irBuiltIns.stringType) {
+                    return messageArgument
+                } else {
+                    val invoke = messageParameter.type.classOrNull!!.functions
+                        .filter { !it.owner.isFakeOverride } // TODO best way to find single access method?
+                        .single()
+                    irCall(invoke).apply { dispatchReceiver = messageArgument }
+                }
+            }
+            // Kotlin Lambda or SAMs conversion lambda
+            is IrFunctionExpression, is IrTypeOperatorCall -> {
+                val invoke = messageParameter.type.classOrNull!!.functions
+                    .filter { !it.owner.isFakeOverride } // TODO best way to find single access method?
+                    .single()
+                irCall(invoke).apply { dispatchReceiver = messageArgument }
+            }
+            else -> null
+        }
+    }
+}

plugins/power-assert/power-assert-compiler/tests-gen/org/jetbrains/kotlin/powerassert/FirLightTreeBlackBoxCodegenTestForPowerAssertGenerated.java
@@ -74,6 +74,40 @@ public class FirLightTreeBlackBoxCodegenTestForPowerAssertGenerated extends Abst
   }
 
   @Nested
+  @TestMetadata("plugins/power-assert/power-assert-compiler/testData/codegen/annotated")
+  @TestDataPath("$PROJECT_ROOT")
+  public class Annotated {
+    @Test
+    public void testAllFilesPresentInAnnotated() {
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("plugins/power-assert/power-assert-compiler/testData/codegen/annotated"), Pattern.compile("^(.+)\\.kt$"), Pattern.compile("^(.+)\\.fir\\.kts?$"), TargetBackend.JVM_IR, true);
+    }
+
+    @Test
+    @TestMetadata("Explain.kt")
+    public void testExplain() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/Explain.kt");
+    }
+
+    @Test
+    @TestMetadata("IgnoreParameter.kt")
+    public void testIgnoreParameter() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/IgnoreParameter.kt");
+    }
+
+    @Test
+    @TestMetadata("LocalVariables.kt")
+    public void testLocalVariables() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/LocalVariables.kt");
+    }
+
+    @Test
+    @TestMetadata("modules.kt")
+    public void testModules() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/annotated/modules.kt");
+    }
+  }
+
+  @Nested
   @TestMetadata("plugins/power-assert/power-assert-compiler/testData/codegen/arithmetic")
   @TestDataPath("$PROJECT_ROOT")
   public class Arithmetic {
@@ -740,6 +774,12 @@ public class FirLightTreeBlackBoxCodegenTestForPowerAssertGenerated extends Abst
     @TestMetadata("ContextReceivers.kt")
     public void testContextReceivers() {
       runTest("plugins/power-assert/power-assert-compiler/testData/codegen/parameters/ContextReceivers.kt");
+    }
+
+    @Test
+    @TestMetadata("FluentBuilder.kt")
+    public void testFluentBuilder() {
+      runTest("plugins/power-assert/power-assert-compiler/testData/codegen/parameters/FluentBuilder.kt");
     }
 
     @Test