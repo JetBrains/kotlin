fd01e6c5d51af4962da1fe4e6a88b3b45209b51c
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-02-10 20:22:05 +0100

    [SLC] support context parameters
    ^KT-74868 Fixed

--- compiler/testData/asJava/lightClasses/lightClassByFqName/facades/contextParameters.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/facades/contextParameters.java
@@ -1,9 +1,9 @@
 public final class ContextParametersKt /* one.ContextParametersKt*/ {
-  public static final boolean getPropertyContext();//  getPropertyContext()
+  public static final boolean getPropertyContext(int);//  getPropertyContext(int)
 
-  public static final int getPropertyContextReceiver(boolean);//  getPropertyContextReceiver(boolean)
+  public static final int getPropertyContextReceiver(int, @org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String, boolean);//  getPropertyContextReceiver(int, @org.jetbrains.annotations.NotNull() java.lang.String, boolean)
 
-  public static final void contextAndReceiverAndValue(boolean, long);//  contextAndReceiverAndValue(boolean, long)
+  public static final void contextAndReceiverAndValue(int, @org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String, boolean, long);//  contextAndReceiverAndValue(int, @org.jetbrains.annotations.NotNull() java.lang.String, boolean, long)
 
-  public static final void unnamedContext();//  unnamedContext()
+  public static final void unnamedContext(int);//  unnamedContext(int)
 }
