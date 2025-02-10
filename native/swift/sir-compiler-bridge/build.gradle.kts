df52c940998116fb8b15527d1be249234800bcf3
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-02-10 11:22:02 +0200

    [Swift export] sir-compiler-bridges now depend on analysis-api
    For historical reasons, sir-compiler-bridges was abstracted from
    Analysis API details. While it was a good thing in the beginning of the
    project, now this abstraction makes things worse: One have add more and
    more "Kotlin modeling" code instead of directly accessing Kotlin origin
    which was used to create a SIR node.
    
    So let's fix that and simplify our lives.
    
    ^KT-75079

--- native/swift/sir-compiler-bridge/build.gradle.kts
+++ native/swift/sir-compiler-bridge/build.gradle.kts
@@ -13,7 +13,12 @@ dependencies {
     compileOnly(kotlinStdlib())
 
     api(project(":native:swift:sir"))
+    // SIR providers give access to Kotlin origin of SIR declaration.
     implementation(project(":native:swift:sir-providers"))
+    // Allow direct access to Analysis API in bridges to avoid modeling Kotlin code all over again.
+    implementation(project(":analysis:analysis-api"))
+    // Analysis API depends on some API from the compiler core (like FqName).
+    implementation(project(":core:compiler.common"))
 
     testApi(platform(libs.junit.bom))
     testRuntimeOnly(libs.junit.jupiter.engine)
