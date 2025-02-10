b836d0323a32997c3f0d48b708fd37a375a4ac3d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-02-10 19:44:08 +0100

    [SLC] drop light classes for members with value classes as context parameters
    ^KT-74868

--- compiler/testData/asJava/lightClasses/lightClassByFqName/facades/valueClassAsContextParameter.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/facades/valueClassAsContextParameter.java
@@ -1,5 +1,2 @@
 public final class ValueClassAsContextParameterKt /* one.ValueClassAsContextParameterKt*/ {
-  public static final int getPropertyContextReceiver(boolean);//  getPropertyContextReceiver(boolean)
-
-  public static final void contextAndReceiverAndValue(boolean, long);//  contextAndReceiverAndValue(boolean, long)
 }
--- compiler/testData/asJava/lightClasses/lightClassByFqName/valueClassAsContextParameter.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/valueClassAsContextParameter.java
@@ -1,7 +1,3 @@
 public final class MyClass /* one.MyClass*/ {
   public  MyClass();//  .ctor()
-
-  public final int getPropertyContextReceiver(boolean);//  getPropertyContextReceiver(boolean)
-
-  public final void contextAndReceiverAndValue(boolean, long);//  contextAndReceiverAndValue(boolean, long)
 }
