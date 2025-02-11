a8bd9cda698a8f49d788e3a8e5aa3eb3a94647b6
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-02-11 11:37:19 +0100

    [Tests] Reproduce #KT-74979

--- compiler/testData/psi/contextParameters/anonymousFunction.kt
+++ compiler/testData/psi/contextParameters/anonymousFunction.kt
@@ -2,3 +2,25 @@ val t = context(a: A) fun () { }
 val t = context(a: A) fun <A> () { }
 val t = @Ann context(a: A) fun () { }
 val t = context(a)
+
+fun test(a: Boolean = context(a: Boolean) fun (): Boolean { return a }(true)){}
+
+class A(val k: Boolean = context(a: Boolean) fun (): Boolean { return a }(true))
+
+fun f() {
+    when {
+        context(a: Boolean) fun(): Boolean { return a }(true) -> 1
+        else -> 2
+    }
+
+    when(val x = context(a: Boolean) fun(): Boolean { return a }(true)) {
+        true -> 1
+        else -> 2
+    }
+
+    if(x > context(a: Boolean) fun (): Boolean { return a }(true)) { }
+
+    for (i in context(a: Boolean) fun (): IntRange { return 1..10 }(true)){ }
+}
+
+val String.t get() = fun1@ context(a: String) fun String.() { this@fun1 + a + this@t }
