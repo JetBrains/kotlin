738dc7482bdbca44b2bc99e50089ac9444012958
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-02-12 11:20:51 +0100

    Move TestApplicationUtil to tests-compiler-utils

--- compiler/tests-compiler-utils/tests/org/jetbrains/kotlin/test/testFramework/TestApplicationUtil.kt
+++ compiler/tests-compiler-utils/tests/org/jetbrains/kotlin/test/testFramework/TestApplicationUtil.kt
@@ -1,12 +1,12 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.test.testFramework
 
-import com.intellij.openapi.application.Application
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import com.intellij.openapi.application.Application
 import org.jetbrains.kotlin.utils.rethrow
 
 fun resetApplicationToNull(old: Application?) {
