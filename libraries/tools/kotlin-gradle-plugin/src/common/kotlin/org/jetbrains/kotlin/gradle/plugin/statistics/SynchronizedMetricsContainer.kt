0648250ea7563068b17f35a4db9f95fa85e0a416
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2025-02-12 13:28:18 +0100

    Add synchronization for reporting fus metrics
    ^KT-75026: Verification Pending

---
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/statistics/SynchronizedMetricsContainer.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.gradle.plugin.statistics
+
+import org.jetbrains.kotlin.statistics.metrics.*
+import java.io.Serializable
+
+class SynchronizedMetricsContainer : NonSynchronizedMetricsContainer(), Serializable {
+
+    private val metricsLock = Object()
+
+    override fun report(metric: BooleanMetrics, value: Boolean, subprojectName: String?, weight: Long?): Boolean {
+        synchronized(metricsLock) {
+            super.report(metric, value, subprojectName, weight)
+        }
+        return true
+    }
+
+    override fun report(metric: NumericalMetrics, value: Long, subprojectName: String?, weight: Long?): Boolean {
+        synchronized(metricsLock) {
+            super.report(metric, value, subprojectName, weight)
+        }
+        return true
+    }
+
+    override fun report(metric: StringMetrics, value: String, subprojectName: String?, weight: Long?): Boolean {
+        synchronized(metricsLock) {
+            super.report(metric, value, subprojectName, weight)
+        }
+        return true
+    }
+
+    override fun readFromMetricConsumer(metricConsumer: NonSynchronizedMetricsContainer) {
+        synchronized(metricsLock) {
+            super.readFromMetricConsumer(metricConsumer)
+        }
+    }
+
+    override fun sendToConsumer(metricConsumer: StatisticsValuesConsumer) {
+        synchronized(metricsLock) {
+            super.sendToConsumer(metricConsumer)
+        }
+    }
+}
