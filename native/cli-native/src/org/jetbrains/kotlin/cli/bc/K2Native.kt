4606a58d45630700bc3c7569233f3f43d2d1508d
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-02-12 14:20:45 +0100

    [CLI] Unify logic about metadata version parsing between common and Native
    #KT-56062 Fixed

--- native/cli-native/src/org/jetbrains/kotlin/cli/bc/K2Native.kt
+++ native/cli-native/src/org/jetbrains/kotlin/cli/bc/K2Native.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.cli.common.*
 import org.jetbrains.kotlin.cli.common.arguments.K2NativeCompilerArguments
 import org.jetbrains.kotlin.cli.common.arguments.parseCommandLineArguments
 import org.jetbrains.kotlin.cli.common.arguments.parseCustomKotlinAbiVersion
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.ERROR
 import org.jetbrains.kotlin.cli.common.messages.MessageRenderer
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
@@ -24,7 +25,6 @@ import org.jetbrains.kotlin.config.*
 import org.jetbrains.kotlin.ir.linkage.partial.partialLinkageConfig
 import org.jetbrains.kotlin.ir.linkage.partial.setupPartialLinkageConfig
 import org.jetbrains.kotlin.konan.KonanPendingCompilationError
-import org.jetbrains.kotlin.library.KLIB_LEGACY_METADATA_VERSION
 import org.jetbrains.kotlin.metadata.deserialization.BinaryVersion
 import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
 import org.jetbrains.kotlin.platform.TargetPlatform
@@ -97,12 +97,6 @@ class K2Native : CLICompiler<K2NativeCompilerArguments>() {
                 configuration, EnvironmentConfigFiles.NATIVE_CONFIG_FILES)
 
         configuration.phaseConfig = createPhaseConfig(arguments)
-
-        /* Set default version of metadata version */
-        val metadataVersionString = arguments.metadataVersion
-        if (metadataVersionString == null) {
-            configuration.put(CommonConfigurationKeys.METADATA_VERSION, KLIB_LEGACY_METADATA_VERSION)
-        }
 
         arguments.relativePathBases?.let {
             configuration.put(KlibConfigurationKeys.KLIB_RELATIVE_PATH_BASES, it.toList())
