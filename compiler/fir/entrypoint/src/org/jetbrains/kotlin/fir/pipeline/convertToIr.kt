574551f14763ca9a00478f46347ed50a6100366f
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2025-02-12 18:18:47 +0100

    [IR] Change warning to error in IR validation after compiler plugins
    ^KT-71138 Fixed

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/pipeline/convertToIr.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/pipeline/convertToIr.kt
@@ -436,8 +436,7 @@ private fun IrPluginContext.runMandatoryIrValidation(
     fir2IrConfiguration: Fir2IrConfiguration,
 ) {
     if (!fir2IrConfiguration.validateIrAfterPlugins) return
-    // TODO(KT-71138): Replace with IrVerificationMode.ERROR in Kotlin 2.2
-    validateIr(fir2IrConfiguration.messageCollector, IrVerificationMode.WARNING) {
+    validateIr(fir2IrConfiguration.messageCollector, IrVerificationMode.ERROR) {
         customMessagePrefix = if (extension == null) {
             "The frontend generated invalid IR. This is a compiler bug, please report it to https://kotl.in/issue."
         } else {
