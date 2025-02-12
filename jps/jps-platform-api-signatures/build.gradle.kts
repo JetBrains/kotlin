07d30352dea47a333657a20ced63aa2b91c2e03d
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-02-12 16:07:34 +0200

    fixup! Deprecate language versions 1.8 and 1.9
    According to `5c039e25`, these languages were pinned because otherwise
    IntelliJ would supply an outdated stdlib. This doesn't sound pretty,
    they should probably be removed and the stdlib version must be synched
    some other way.

--- jps/jps-platform-api-signatures/build.gradle.kts
+++ jps/jps-platform-api-signatures/build.gradle.kts
@@ -17,6 +17,8 @@ sourceSets {
 }
 
 tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    @Suppress("DEPRECATION")
     compilerOptions.apiVersion.value(KotlinVersion.KOTLIN_1_8).finalizeValueOnRead()
+    @Suppress("DEPRECATION")
     compilerOptions.languageVersion.value(KotlinVersion.KOTLIN_1_8).finalizeValueOnRead()
 }
