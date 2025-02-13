2fe27cfdb3e90964c8e9b3aa451996fba73a891a
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-02-13 14:22:26 +0200

    [Swift export] extend SirChildrenProvider with an simpler function
    Sequence<KaDeclarationSymbol> is more flexible receiver parameter as it
    allows filtering declarations.
    Keep the old declaration for the convenience.

--- native/swift/sir-providers/src/org/jetbrains/kotlin/sir/providers/impl/SirDeclarationChildrenProviderImpl.kt
+++ native/swift/sir-providers/src/org/jetbrains/kotlin/sir/providers/impl/SirDeclarationChildrenProviderImpl.kt
@@ -6,24 +6,22 @@
 package org.jetbrains.kotlin.sir.providers.impl
 
 import org.jetbrains.kotlin.analysis.api.KaSession
-import org.jetbrains.kotlin.analysis.api.scopes.KaScope
+import org.jetbrains.kotlin.analysis.api.symbols.KaDeclarationSymbol
 import org.jetbrains.kotlin.sir.SirDeclaration
 import org.jetbrains.kotlin.sir.SirVisibility
 import org.jetbrains.kotlin.sir.providers.SirChildrenProvider
 import org.jetbrains.kotlin.sir.providers.SirSession
 
 public class SirDeclarationChildrenProviderImpl(private val sirSession: SirSession) : SirChildrenProvider {
+    override fun Sequence<KaDeclarationSymbol>.extractDeclarations(kaSession: KaSession): Sequence<SirDeclaration> = with(sirSession) {
+        filter { isAccessible(it, kaSession) }
+            .flatMap { it.toSir().allDeclarations }
+            .flatMap { listOf(it) + it.trampolineDeclarations() }
+    }
 
-    override fun KaScope.extractDeclarations(ktAnalysisSession: KaSession): Sequence<SirDeclaration> =
-        declarations
-            .filter {
-                with(sirSession) {
-                    when (it.sirVisibility(ktAnalysisSession)) {
-                        null, SirVisibility.PRIVATE, SirVisibility.FILEPRIVATE, SirVisibility.INTERNAL -> false
-                        SirVisibility.PUBLIC, SirVisibility.PACKAGE -> true
-                    }
-                }
-            }
-            .flatMap { with(sirSession) { it.toSir().allDeclarations } }
-            .flatMap { with(sirSession) { listOf(it) + it.trampolineDeclarations() } }
+    private fun SirSession.isAccessible(symbol: KaDeclarationSymbol, kaSession: KaSession): Boolean =
+        when (symbol.sirVisibility(kaSession)) {
+            null, SirVisibility.PRIVATE, SirVisibility.FILEPRIVATE, SirVisibility.INTERNAL -> false
+            SirVisibility.PUBLIC, SirVisibility.PACKAGE -> true
+        }
 }
