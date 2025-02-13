fc1a43b3c9dab30862b34a7b7cf17762a6571429
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-02-13 18:10:37 +0100

    TODO

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/KlibIcDeserializedContainerSource.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/KlibIcDeserializedContainerSource.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -13,7 +13,7 @@ import org.jetbrains.kotlin.serialization.deserialization.descriptors.Deserializ
 
 class KlibIcDeserializedContainerSource(packageFqName: FqName) : DeserializedContainerSource {
     override val presentableString: String = "Package '$packageFqName'"
-    override val incompatibility: IncompatibleVersionErrorData<*>? get() = null
+    override val incompatibility: IncompatibleVersionErrorData<*>? get() = null // TODO should be handed?
     override val isPreReleaseInvisible: Boolean get() = false
     override val abiStability: DeserializedContainerAbiStability get() = DeserializedContainerAbiStability.STABLE
     override fun getContainingFile(): SourceFile = SourceFile.NO_SOURCE_FILE
