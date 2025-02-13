7145d8d735af4187c36ea5e8e2cff8cf184c45ab
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-02-13 17:20:47 +0100

    [Tests] Reproduce #KT-75150

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticLightTreeTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticLightTreeTestGenerated.java
@@ -51479,6 +51479,12 @@ public class PhasedJvmDiagnosticLightTreeTestGenerated extends AbstractPhasedJvm
         }
 
         @Test
+        @TestMetadata("genericContextAmbiguity.kt")
+        public void testGenericContextAmbiguity() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/contextParameters/declarationAndUsages/genericContextAmbiguity.kt");
+        }
+
+        @Test
         @TestMetadata("inFunctionDeclaration.kt")
         public void testInFunctionDeclaration() {
           runTest("compiler/fir/analysis-tests/testData/resolve/contextParameters/declarationAndUsages/inFunctionDeclaration.kt");
