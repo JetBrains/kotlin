8293f0661e8008208d6c67bac9ef5170c3523a4a
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-02-14 17:22:24 +0200

    fixup! [FIR] Unify `subject` and `subjectVariable` in when expressions
    Update `FirCastOperatorsChecker` so that it only ignores `USELESS_IS_CHECK`
    in some very specific situations related to the last branch in `when`s.

--- compiler/testData/diagnostics/testsWithJsStdLib/dynamicTypes/dynamicCastTarget.fir.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/dynamicTypes/dynamicCastTarget.fir.kt
@@ -14,9 +14,9 @@ fun test(d: Any, dl: Collection<dynamic>) {
     <!USELESS_IS_CHECK!>d !is <!DYNAMIC_NOT_ALLOWED!>dynamic?<!><!>
 
     when (d) {
-        is <!DYNAMIC_NOT_ALLOWED!>dynamic<!> -> {}
-        is <!DUPLICATE_BRANCH_CONDITION_IN_WHEN, DYNAMIC_NOT_ALLOWED!>dynamic?<!> -> {}
-        !is <!DYNAMIC_NOT_ALLOWED!>dynamic<!> -> {}
+        <!USELESS_IS_CHECK!>is <!DYNAMIC_NOT_ALLOWED!>dynamic<!><!> -> {}
+        <!USELESS_IS_CHECK!>is <!DUPLICATE_BRANCH_CONDITION_IN_WHEN, DYNAMIC_NOT_ALLOWED!>dynamic?<!><!> -> {}
+        <!USELESS_IS_CHECK!>!is <!DYNAMIC_NOT_ALLOWED!>dynamic<!><!> -> {}
         !is <!DUPLICATE_BRANCH_CONDITION_IN_WHEN, DYNAMIC_NOT_ALLOWED!>dynamic?<!> -> {}
     }
 
@@ -24,6 +24,6 @@ fun test(d: Any, dl: Collection<dynamic>) {
     <!USELESS_IS_CHECK!>dl is List<dynamic><!>
 
     when (dl) {
-        is List<dynamic> -> {}
+        <!USELESS_IS_CHECK!>is List<dynamic><!> -> {}
     }
 }
