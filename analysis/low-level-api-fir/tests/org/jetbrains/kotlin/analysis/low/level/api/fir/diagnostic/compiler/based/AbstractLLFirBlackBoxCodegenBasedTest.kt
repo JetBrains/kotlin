a7bf83bf8b13491e735207de3659ec5dc1dcbcc4
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-02-18 20:10:05 +0100

    [LL] rename AbstractLLFirBlackBoxCodegenBasedTestBase -> AbstractLLBlackBoxTestBase
    ^KT-73386

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/AbstractLLFirBlackBoxCodegenBasedTest.kt
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/AbstractLLFirBlackBoxCodegenBasedTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -12,7 +12,7 @@ import org.jetbrains.kotlin.test.model.AfterAnalysisChecker
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.utils.bind
 
-abstract class AbstractLLFirBlackBoxCodegenBasedTest : AbstractLLFirBlackBoxCodegenBasedTestBase() {
+abstract class AbstractLLFirBlackBoxCodegenBasedTest : AbstractLLBlackBoxTestBase() {
     override fun facade(): Constructor<LowLevelFirFrontendFacade> {
         return ::LowLevelFirFrontendFacade.bind(LLFirAnalyzerFacadeFactoryWithoutPreresolve)
     }
