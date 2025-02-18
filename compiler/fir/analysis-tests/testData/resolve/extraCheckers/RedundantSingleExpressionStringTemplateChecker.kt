b74eb9bbbdf5c0f3846553ebd4641a1482e17c4d
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-02-18 20:21:23 +0100

    K2: reproduce and fix NPE in string template extra checker
    #KT-75289 Fixed

--- compiler/fir/analysis-tests/testData/resolve/extraCheckers/RedundantSingleExpressionStringTemplateChecker.kt
+++ compiler/fir/analysis-tests/testData/resolve/extraCheckers/RedundantSingleExpressionStringTemplateChecker.kt
@@ -1,4 +1,6 @@
 // RUN_PIPELINE_TILL: BACKEND
+// WITH_STDLIB
+
 val x = "Hello"
 
 val y = "$<!REDUNDANT_SINGLE_EXPRESSION_STRING_TEMPLATE!>x<!>"
@@ -13,4 +15,22 @@ fun toString(x: String) = "IC$x"
 
 data class ProductGroup(val short_name: String, val parent: ProductGroup?) {
     val name: String = if (parent == null) short_name else "${parent.name} $short_name"
+}
+
+// ISSUE: KT-75289
+
+private fun checkStartAndEnd(
+    len: Int,
+    start: Int,
+    end: Int
+) {
+    if (start < 0 || end > len) {
+        throw ArrayIndexOutOfBoundsException(
+            "start < 0 || end > len."
+                    + " start=" + start + ", end=" + end + ", len=" + len
+        )
+    }
+    if (start > end) {
+        throw IllegalArgumentException("start > end: $start > $end")
+    }
 }
