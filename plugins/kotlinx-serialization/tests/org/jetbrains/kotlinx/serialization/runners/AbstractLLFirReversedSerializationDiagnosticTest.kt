0f59804c81a90758999937cd5fe3e0cf565c2f72
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-02-18 19:34:44 +0100

    [LL] rename AbstractLLFirPreresolvedReversedDiagnosticCompilerTestDataTest -> AbstractLLReversedDiagnosticsTest
    ^KT-73386

--- plugins/kotlinx-serialization/tests/org/jetbrains/kotlinx/serialization/runners/AbstractLLFirReversedSerializationDiagnosticTest.kt
+++ plugins/kotlinx-serialization/tests/org/jetbrains/kotlinx/serialization/runners/AbstractLLFirReversedSerializationDiagnosticTest.kt
@@ -1,15 +1,14 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlinx.serialization.runners
 
-import org.jetbrains.kotlin.analysis.low.level.api.fir.diagnostic.compiler.based.AbstractLLFirPreresolvedReversedDiagnosticCompilerTestDataTest
+import org.jetbrains.kotlin.analysis.low.level.api.fir.diagnostic.compiler.based.AbstractLLReversedDiagnosticsTest
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
-import org.jetbrains.kotlinx.serialization.runners.configureSerializationFirPsiDiagnosticTest
 
-abstract class AbstractLLFirReversedSerializationDiagnosticTest : AbstractLLFirPreresolvedReversedDiagnosticCompilerTestDataTest() {
+abstract class AbstractLLFirReversedSerializationDiagnosticTest : AbstractLLReversedDiagnosticsTest() {
     override fun configure(builder: TestConfigurationBuilder) {
         super.configure(builder)
         builder.configureSerializationFirPsiDiagnosticTest()
