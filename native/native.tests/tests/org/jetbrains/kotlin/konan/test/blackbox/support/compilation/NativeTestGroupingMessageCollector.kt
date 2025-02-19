f85b6217f798973660e8b90bd09b6d7fba44fc18
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-02-19 13:20:48 +0200

    fixup! Deprecate language versions 1.8 and 1.9
    bad check

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/compilation/NativeTestGroupingMessageCollector.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/compilation/NativeTestGroupingMessageCollector.kt
@@ -74,7 +74,6 @@ internal class NativeTestGroupingMessageCollector(
                     || isKlibResolver(message)
                     || isContextReceiversWarning(message)
                     || isMimallocWarning(message)
-                    || isK1LanguageVersionWarning(message)
                 -> {
                 // These warnings are known and should not be reported as errors.
                 severity
@@ -124,8 +123,6 @@ internal class NativeTestGroupingMessageCollector(
 
     private fun isMimallocWarning(message: String): Boolean = message.startsWith(MIMALLOC_WARNING_PREFIX)
 
-    private fun isK1LanguageVersionWarning(message: String): Boolean = message.matches(K1_LANGUAGE_VERSIONS_WARNING_REGEX)
-
     override fun hasErrors() = hasWarningsWithRaisedSeverity || super.hasErrors()
 
     companion object {
@@ -137,7 +134,6 @@ internal class NativeTestGroupingMessageCollector(
         private const val CONTEXT_RECEIVERS_WARNING_PREFIX = "Experimental context receivers are superseded by context parameters"
         private const val MIMALLOC_WARNING_PREFIX = "Usage of mimalloc in Kotlin/Native compiler is deprecated. Please remove -Xallocator=mimalloc compiler flag."
 
-        private val K1_LANGUAGE_VERSIONS_WARNING_REGEX = Regex("Language version 1.[0-9.]+ is deprecated and its support will be removed in a future version of Kotlin")
         private val PARTIAL_LINKAGE_WARNING_REGEX = Regex("^<[^<>]+>( @ (?:(?!: ).)+)?: .*")
 
         private fun parseLanguageFeatureArg(arg: String): String? =
