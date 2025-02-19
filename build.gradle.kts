464781ae3af6215f90f9df0a1891c511176ae30b
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2025-02-19 10:10:15 +0100

    Bump Kotlin/Native version in KGP to 2.2.0-dev-6046

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,7 +128,7 @@ if (!project.hasProperty("versions.kotlin-native")) {
     extra["versions.kotlin-native"] = if (kotlinBuildProperties.isKotlinNativeEnabled) {
         kotlinBuildProperties.defaultSnapshotVersion
     } else {
-        "2.2.0-dev-2907"
+        "2.2.0-dev-6046"
     }
 }
 
