9363094c544e2a02a01d107a27d97cc9c3c0b146
andrei.kislitsyn <kisandy@yandex.ru>
2025-02-20 15:30:56 +0400

    [KDF] update move in writerside and kdf plugin
    Moved from https://github.com/Kotlin/dataframe/commit/42d2453fd21131344794175aa8fe3fafd191142e

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/loadInterpreter.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/loadInterpreter.kt
@@ -105,9 +105,9 @@ import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MergeInto0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.Move0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveAfter0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveInto0
-import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveToLeft0
-import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveToLeft1
-import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveToRight0
+import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveToStart0
+import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveToStart1
+import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveToEnd0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveUnder0
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.MoveUnder1
 import org.jetbrains.kotlinx.dataframe.plugin.impl.api.PairConstructor
@@ -290,9 +290,9 @@ internal inline fun <reified T> String.load(): T {
         "MoveUnder0" -> MoveUnder0()
         "MoveUnder1" -> MoveUnder1()
         "MoveInto0" -> MoveInto0()
-        "MoveToLeft0" -> MoveToLeft0()
-        "MoveToLeft1" -> MoveToLeft1()
-        "MoveToRight0" -> MoveToRight0()
+        "MoveToStart0" -> MoveToStart0()
+        "MoveToStart1" -> MoveToStart1()
+        "MoveToEnd0" -> MoveToEnd0()
         "MoveAfter0" -> MoveAfter0()
         "GroupByAdd" -> GroupByAdd()
         "Merge0" -> Merge0()
