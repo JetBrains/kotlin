fa87bacce2711b0836e47c16b6b09c55dac779fd
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-02-21 20:22:23 +0100

    [SLC] SymbolLightRepeatableAnnotationContainerMethod: add simple `isVarArgs`
    There are no var args
    
    ^KT-75391

--- analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/methods/SymbolLightRepeatableAnnotationContainerMethod.kt
+++ analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/methods/SymbolLightRepeatableAnnotationContainerMethod.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -45,6 +45,7 @@ internal class SymbolLightRepeatableAnnotationContainerMethod(
 
     override fun getParameterList(): PsiParameterList = _parameterList
 
+    override fun isVarArgs(): Boolean = false
     override fun isConstructor(): Boolean = false
 
     private val _returnType by lazyPub {
