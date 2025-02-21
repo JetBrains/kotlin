d174d5de15c3d6f946dc2adcf33e68d2963f6b9d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-02-21 19:42:27 +0100

    [SLC] SymbolLightClassForClassLike: simplify `name`
    ^KT-75391

--- analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/classes/SymbolLightClassForClassLike.kt
+++ analysis/symbol-light-classes/src/org/jetbrains/kotlin/light/classes/symbol/classes/SymbolLightClassForClassLike.kt
@@ -128,9 +128,7 @@ internal abstract class SymbolLightClassForClassLike<SType : KaClassSymbol> prot
 
     override fun hashCode(): Int = classOrObjectDeclaration.hashCode()
 
-    override fun getName(): String? = classOrObjectDeclaration?.name ?: withClassSymbol {
-        it.name?.asString()
-    }
+    override fun getName(): String? = withClassSymbol { it.name?.asString() }
 
     override fun hasModifierProperty(@NonNls name: String): Boolean = modifierList?.hasModifierProperty(name) ?: false
 
