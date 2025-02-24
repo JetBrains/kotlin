2d478cdfcd21b7e46ad965f98af62d8be9b64ea6
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-02-24 21:28:04 +0100

    [Metadata] Support proper metadata compatibility check inside decompiler
    #KTIJ-32951 Fixed

--- analysis/decompiled/decompiler-native/src/org/jetbrains/kotlin/analysis/decompiler/konan/KotlinNativeMetadataDecompiler.kt
+++ analysis/decompiled/decompiler-native/src/org/jetbrains/kotlin/analysis/decompiler/konan/KotlinNativeMetadataDecompiler.kt
@@ -5,7 +5,6 @@
 package org.jetbrains.kotlin.analysis.decompiler.konan
 
 import com.intellij.openapi.vfs.VirtualFile
-import org.jetbrains.kotlin.library.KLIB_LEGACY_METADATA_VERSION
 import org.jetbrains.kotlin.library.metadata.KlibMetadataSerializerProtocol
 import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
 import org.jetbrains.kotlin.psi.stubs.KotlinStubVersions
@@ -15,8 +14,6 @@ class KotlinNativeMetadataDecompiler : Fe10KlibMetadataDecompiler<MetadataVersio
     KlibMetaFileType,
     { KlibMetadataSerializerProtocol },
     DynamicTypeDeserializer,
-    { KLIB_LEGACY_METADATA_VERSION },
-    { MetadataVersion.INVALID_VERSION },
     KotlinStubVersions.KOTLIN_NATIVE_STUB_VERSION,
 ) {
     override fun doReadFile(file: VirtualFile): FileWithMetadata? {
