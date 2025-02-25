rizzzav/kotlinforked

Replace HashMaps with when in ClassReference
Fix KT-71832

libraries/stdlib/jvm/runtime/kotlin/jvm/internal/ClassReference.kt
@@ -106,61 +106,146 @@ public class ClassReference(override val jClass: Class<*>) : KClass<Any>, ClassB
                 Function20::class.java, Function21::class.java, Function22::class.java
             ).mapIndexed { i, clazz -> clazz to i }.toMap()
 
-        private val primitiveFqNames = HashMap<String, String>().apply {
-            put("boolean", "kotlin.Boolean")
-            put("char", "kotlin.Char")
-            put("byte", "kotlin.Byte")
-            put("short", "kotlin.Short")
-            put("int", "kotlin.Int")
-            put("float", "kotlin.Float")
-            put("long", "kotlin.Long")
-            put("double", "kotlin.Double")
-        }
-
-        private val primitiveWrapperFqNames = HashMap<String, String>().apply {
-            put("java.lang.Boolean", "kotlin.Boolean")
-            put("java.lang.Character", "kotlin.Char")
-            put("java.lang.Byte", "kotlin.Byte")
-            put("java.lang.Short", "kotlin.Short")
-            put("java.lang.Integer", "kotlin.Int")
-            put("java.lang.Float", "kotlin.Float")
-            put("java.lang.Long", "kotlin.Long")
-            put("java.lang.Double", "kotlin.Double")
-        }
-
         // See JavaToKotlinClassMap.
-        private val classFqNames = HashMap<String, String>().apply {
-            put("java.lang.Object", "kotlin.Any")
-            put("java.lang.String", "kotlin.String")
-            put("java.lang.CharSequence", "kotlin.CharSequence")
-            put("java.lang.Throwable", "kotlin.Throwable")
-            put("java.lang.Cloneable", "kotlin.Cloneable")
-            put("java.lang.Number", "kotlin.Number")
-            put("java.lang.Comparable", "kotlin.Comparable")
-            put("java.lang.Enum", "kotlin.Enum")
-            put("java.lang.annotation.Annotation", "kotlin.Annotation")
-            put("java.lang.Iterable", "kotlin.collections.Iterable")
-            put("java.util.Iterator", "kotlin.collections.Iterator")
-            put("java.util.Collection", "kotlin.collections.Collection")
-            put("java.util.List", "kotlin.collections.List")
-            put("java.util.Set", "kotlin.collections.Set")
-            put("java.util.ListIterator", "kotlin.collections.ListIterator")
-            put("java.util.Map", "kotlin.collections.Map")
-            put("java.util.Map\$Entry", "kotlin.collections.Map.Entry")
-            put("kotlin.jvm.internal.StringCompanionObject", "kotlin.String.Companion")
-            put("kotlin.jvm.internal.EnumCompanionObject", "kotlin.Enum.Companion")
-
-            putAll(primitiveFqNames)
-            putAll(primitiveWrapperFqNames)
-            primitiveFqNames.values.associateTo(this) { kotlinName ->
-                "kotlin.jvm.internal.${kotlinName.substringAfterLast('.')}CompanionObject" to "$kotlinName.Companion"
-            }
-            for ((klass, arity) in FUNCTION_CLASSES) {
-                put(klass.name, "kotlin.Function$arity")
-            }
+        private fun classFqNameOf(type: String) = when(type) {
+            "boolean" -> "kotlin.Boolean"
+            "byte" -> "kotlin.Byte"
+            "char" -> "kotlin.Char"
+            "double" -> "kotlin.Double"
+            "float" -> "kotlin.Float"
+            "int" -> "kotlin.Int"
+            "long" -> "kotlin.Long"
+            "short" -> "kotlin.Short"
+            "java.lang.annotation.Annotation" -> "kotlin.Annotation"
+            "java.lang.Boolean" -> "kotlin.Boolean"
+            "java.lang.Byte" -> "kotlin.Byte"
+            "java.lang.Character" -> "kotlin.Char"
+            "java.lang.CharSequence" -> "kotlin.CharSequence"
+            "java.lang.Cloneable" -> "kotlin.Cloneable"
+            "java.lang.Comparable" -> "kotlin.Comparable"
+            "java.lang.Double" -> "kotlin.Double"
+            "java.lang.Enum" -> "kotlin.Enum"
+            "java.lang.Float" -> "kotlin.Float"
+            "java.lang.Integer" -> "kotlin.Int"
+            "java.lang.Iterable" -> "kotlin.collections.Iterable"
+            "java.lang.Long" -> "kotlin.Long"
+            "java.lang.Number" -> "kotlin.Number"
+            "java.lang.Object" -> "kotlin.Any"
+            "java.lang.Short" -> "kotlin.Short"
+            "java.lang.String" -> "kotlin.String"
+            "java.lang.Throwable" -> "kotlin.Throwable"
+            "java.util.Collection" -> "kotlin.collections.Collection"
+            "java.util.Iterator" -> "kotlin.collections.Iterator"
+            "java.util.ListIterator" -> "kotlin.collections.ListIterator"
+            "java.util.List" -> "kotlin.collections.List"
+            "java.util.Map\$Entry" -> "kotlin.collections.Map.Entry"
+            "java.util.Map" -> "kotlin.collections.Map"
+            "java.util.Set" -> "kotlin.collections.Set"
+            "kotlin.jvm.functions.Function0" -> "kotlin.Function0"
+            "kotlin.jvm.functions.Function1" -> "kotlin.Function1"
+            "kotlin.jvm.functions.Function2" -> "kotlin.Function2"
+            "kotlin.jvm.functions.Function3" -> "kotlin.Function3"
+            "kotlin.jvm.functions.Function4" -> "kotlin.Function4"
+            "kotlin.jvm.functions.Function5" -> "kotlin.Function5"
+            "kotlin.jvm.functions.Function6" -> "kotlin.Function6"
+            "kotlin.jvm.functions.Function7" -> "kotlin.Function7"
+            "kotlin.jvm.functions.Function8" -> "kotlin.Function8"
+            "kotlin.jvm.functions.Function9" -> "kotlin.Function9"
+            "kotlin.jvm.functions.Function10" -> "kotlin.Function10"
+            "kotlin.jvm.functions.Function11" -> "kotlin.Function11"
+            "kotlin.jvm.functions.Function12" -> "kotlin.Function12"
+            "kotlin.jvm.functions.Function13" -> "kotlin.Function13"
+            "kotlin.jvm.functions.Function14" -> "kotlin.Function14"
+            "kotlin.jvm.functions.Function15" -> "kotlin.Function15"
+            "kotlin.jvm.functions.Function16" -> "kotlin.Function16"
+            "kotlin.jvm.functions.Function17" -> "kotlin.Function17"
+            "kotlin.jvm.functions.Function18" -> "kotlin.Function18"
+            "kotlin.jvm.functions.Function19" -> "kotlin.Function19"
+            "kotlin.jvm.functions.Function20" -> "kotlin.Function20"
+            "kotlin.jvm.functions.Function21" -> "kotlin.Function21"
+            "kotlin.jvm.functions.Function22" -> "kotlin.Function22"
+            "kotlin.jvm.internal.BooleanCompanionObject" -> "kotlin.Boolean.Companion"
+            "kotlin.jvm.internal.ByteCompanionObject" -> "kotlin.Byte.Companion"
+            "kotlin.jvm.internal.CharCompanionObject" -> "kotlin.Char.Companion"
+            "kotlin.jvm.internal.DoubleCompanionObject" -> "kotlin.Double.Companion"
+            "kotlin.jvm.internal.EnumCompanionObject" -> "kotlin.Enum.Companion"
+            "kotlin.jvm.internal.FloatCompanionObject" -> "kotlin.Float.Companion"
+            "kotlin.jvm.internal.IntCompanionObject" -> "kotlin.Int.Companion"
+            "kotlin.jvm.internal.LongCompanionObject" -> "kotlin.Long.Companion"
+            "kotlin.jvm.internal.ShortCompanionObject" -> "kotlin.Short.Companion"
+            "kotlin.jvm.internal.StringCompanionObject" -> "kotlin.String.Companion"
+            else -> null
         }
 
-        private val simpleNames = classFqNames.mapValues { (_, fqName) -> fqName.substringAfterLast('.') }
+        private fun simpleNameOf(type: String) = when(type) {
+            "boolean" -> "Boolean"
+            "byte" -> "Byte"
+            "char" -> "Char"
+            "double" -> "Double"
+            "float" -> "Float"
+            "int" -> "Int"
+            "long" -> "Long"
+            "short" -> "Short"
+            "java.lang.annotation.Annotation" -> "Annotation"
+            "java.lang.Boolean" -> "Boolean"
+            "java.lang.Byte" -> "Byte"
+            "java.lang.Character" -> "Char"
+            "java.lang.CharSequence" -> "CharSequence"
+            "java.lang.Cloneable" -> "Cloneable"
+            "java.lang.Comparable" -> "Comparable"
+            "java.lang.Double" -> "Double"
+            "java.lang.Enum" -> "Enum"
+            "java.lang.Float" -> "Float"
+            "java.lang.Integer" -> "Int"
+            "java.lang.Iterable" -> "Iterable"
+            "java.lang.Long" -> "Long"
+            "java.lang.Number" -> "Number"
+            "java.lang.Object" -> "Any"
+            "java.lang.Short" -> "Short"
+            "java.lang.String" -> "String"
+            "java.lang.Throwable" -> "Throwable"
+            "java.util.Collection" -> "Collection"
+            "java.util.Iterator" -> "Iterator"
+            "java.util.ListIterator" -> "ListIterator"
+            "java.util.List" -> "List"
+            "java.util.Map\$Entry" -> "Entry"
+            "java.util.Map" -> "Map"
+            "java.util.Set" -> "Set"
+            "kotlin.jvm.functions.Function0" -> "Function0"
+            "kotlin.jvm.functions.Function1" -> "Function1"
+            "kotlin.jvm.functions.Function2" -> "Function2"
+            "kotlin.jvm.functions.Function3" -> "Function3"
+            "kotlin.jvm.functions.Function4" -> "Function4"
+            "kotlin.jvm.functions.Function5" -> "Function5"
+            "kotlin.jvm.functions.Function6" -> "Function6"
+            "kotlin.jvm.functions.Function7" -> "Function7"
+            "kotlin.jvm.functions.Function8" -> "Function8"
+            "kotlin.jvm.functions.Function9" -> "Function9"
+            "kotlin.jvm.functions.Function10" -> "Function10"
+            "kotlin.jvm.functions.Function11" -> "Function11"
+            "kotlin.jvm.functions.Function12" -> "Function12"
+            "kotlin.jvm.functions.Function13" -> "Function13"
+            "kotlin.jvm.functions.Function14" -> "Function14"
+            "kotlin.jvm.functions.Function15" -> "Function15"
+            "kotlin.jvm.functions.Function16" -> "Function16"
+            "kotlin.jvm.functions.Function17" -> "Function17"
+            "kotlin.jvm.functions.Function18" -> "Function18"
+            "kotlin.jvm.functions.Function19" -> "Function19"
+            "kotlin.jvm.functions.Function20" -> "Function20"
+            "kotlin.jvm.functions.Function21" -> "Function21"
+            "kotlin.jvm.functions.Function22" -> "Function22"
+            "kotlin.jvm.internal.BooleanCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.ByteCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.CharCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.DoubleCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.EnumCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.FloatCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.IntCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.LongCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.ShortCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.StringCompanionObject" -> "Companion"
+            else -> null
+        }
 
         public fun getClassSimpleName(jClass: Class<*>): String? = when {
             jClass.isAnonymousClass -> null
@@ -173,11 +258,11 @@ public class ClassReference(override val jClass: Class<*>) : KClass<Any>, ClassB
             jClass.isArray -> {
                 val componentType = jClass.componentType
                 when {
-                    componentType.isPrimitive -> simpleNames[componentType.name]?.plus("Array")
+                    componentType.isPrimitive -> simpleNameOf(componentType.name)?.plus("Array")
                     else -> null
                 } ?: "Array"
             }
-            else -> simpleNames[jClass.name] ?: jClass.simpleName
+            else -> simpleNameOf(jClass.name) ?: jClass.simpleName
         }
 
         public fun getClassQualifiedName(jClass: Class<*>): String? = when {
@@ -186,11 +271,11 @@ public class ClassReference(override val jClass: Class<*>) : KClass<Any>, ClassB
             jClass.isArray -> {
                 val componentType = jClass.componentType
                 when {
-                    componentType.isPrimitive -> classFqNames[componentType.name]?.plus("Array")
+                    componentType.isPrimitive -> classFqNameOf(componentType.name)?.plus("Array")
                     else -> null
                 } ?: "kotlin.Array"
             }
-            else -> classFqNames[jClass.name] ?: jClass.canonicalName
+            else -> classFqNameOf(jClass.name) ?: jClass.canonicalName
         }
 
         public fun isInstance(value: Any?, jClass: Class<*>): Boolean {

JetBrains/kotlin

 #KT-71832 Fixed

libraries/stdlib/jvm/runtime/kotlin/jvm/internal/ClassReference.kt
@@ -106,61 +106,146 @@ public class ClassReference(override val jClass: Class<*>) : KClass<Any>, ClassB
                 Function20::class.java, Function21::class.java, Function22::class.java
             ).mapIndexed { i, clazz -> clazz to i }.toMap()
 
-        private val primitiveFqNames = HashMap<String, String>().apply {
-            put("boolean", "kotlin.Boolean")
-            put("char", "kotlin.Char")
-            put("byte", "kotlin.Byte")
-            put("short", "kotlin.Short")
-            put("int", "kotlin.Int")
-            put("float", "kotlin.Float")
-            put("long", "kotlin.Long")
-            put("double", "kotlin.Double")
-        }
-
-        private val primitiveWrapperFqNames = HashMap<String, String>().apply {
-            put("java.lang.Boolean", "kotlin.Boolean")
-            put("java.lang.Character", "kotlin.Char")
-            put("java.lang.Byte", "kotlin.Byte")
-            put("java.lang.Short", "kotlin.Short")
-            put("java.lang.Integer", "kotlin.Int")
-            put("java.lang.Float", "kotlin.Float")
-            put("java.lang.Long", "kotlin.Long")
-            put("java.lang.Double", "kotlin.Double")
-        }
-
         // See JavaToKotlinClassMap.
-        private val classFqNames = HashMap<String, String>().apply {
-            put("java.lang.Object", "kotlin.Any")
-            put("java.lang.String", "kotlin.String")
-            put("java.lang.CharSequence", "kotlin.CharSequence")
-            put("java.lang.Throwable", "kotlin.Throwable")
-            put("java.lang.Cloneable", "kotlin.Cloneable")
-            put("java.lang.Number", "kotlin.Number")
-            put("java.lang.Comparable", "kotlin.Comparable")
-            put("java.lang.Enum", "kotlin.Enum")
-            put("java.lang.annotation.Annotation", "kotlin.Annotation")
-            put("java.lang.Iterable", "kotlin.collections.Iterable")
-            put("java.util.Iterator", "kotlin.collections.Iterator")
-            put("java.util.Collection", "kotlin.collections.Collection")
-            put("java.util.List", "kotlin.collections.List")
-            put("java.util.Set", "kotlin.collections.Set")
-            put("java.util.ListIterator", "kotlin.collections.ListIterator")
-            put("java.util.Map", "kotlin.collections.Map")
-            put("java.util.Map\$Entry", "kotlin.collections.Map.Entry")
-            put("kotlin.jvm.internal.StringCompanionObject", "kotlin.String.Companion")
-            put("kotlin.jvm.internal.EnumCompanionObject", "kotlin.Enum.Companion")
-
-            putAll(primitiveFqNames)
-            putAll(primitiveWrapperFqNames)
-            primitiveFqNames.values.associateTo(this) { kotlinName ->
-                "kotlin.jvm.internal.${kotlinName.substringAfterLast('.')}CompanionObject" to "$kotlinName.Companion"
-            }
-            for ((klass, arity) in FUNCTION_CLASSES) {
-                put(klass.name, "kotlin.Function$arity")
-            }
+        private fun classFqNameOf(type: String) = when (type) {
+            "boolean" -> "kotlin.Boolean"
+            "byte" -> "kotlin.Byte"
+            "char" -> "kotlin.Char"
+            "double" -> "kotlin.Double"
+            "float" -> "kotlin.Float"
+            "int" -> "kotlin.Int"
+            "long" -> "kotlin.Long"
+            "short" -> "kotlin.Short"
+            "java.lang.annotation.Annotation" -> "kotlin.Annotation"
+            "java.lang.Boolean" -> "kotlin.Boolean"
+            "java.lang.Byte" -> "kotlin.Byte"
+            "java.lang.Character" -> "kotlin.Char"
+            "java.lang.CharSequence" -> "kotlin.CharSequence"
+            "java.lang.Cloneable" -> "kotlin.Cloneable"
+            "java.lang.Comparable" -> "kotlin.Comparable"
+            "java.lang.Double" -> "kotlin.Double"
+            "java.lang.Enum" -> "kotlin.Enum"
+            "java.lang.Float" -> "kotlin.Float"
+            "java.lang.Integer" -> "kotlin.Int"
+            "java.lang.Iterable" -> "kotlin.collections.Iterable"
+            "java.lang.Long" -> "kotlin.Long"
+            "java.lang.Number" -> "kotlin.Number"
+            "java.lang.Object" -> "kotlin.Any"
+            "java.lang.Short" -> "kotlin.Short"
+            "java.lang.String" -> "kotlin.String"
+            "java.lang.Throwable" -> "kotlin.Throwable"
+            "java.util.Collection" -> "kotlin.collections.Collection"
+            "java.util.Iterator" -> "kotlin.collections.Iterator"
+            "java.util.ListIterator" -> "kotlin.collections.ListIterator"
+            "java.util.List" -> "kotlin.collections.List"
+            "java.util.Map\$Entry" -> "kotlin.collections.Map.Entry"
+            "java.util.Map" -> "kotlin.collections.Map"
+            "java.util.Set" -> "kotlin.collections.Set"
+            "kotlin.jvm.functions.Function0" -> "kotlin.Function0"
+            "kotlin.jvm.functions.Function1" -> "kotlin.Function1"
+            "kotlin.jvm.functions.Function2" -> "kotlin.Function2"
+            "kotlin.jvm.functions.Function3" -> "kotlin.Function3"
+            "kotlin.jvm.functions.Function4" -> "kotlin.Function4"
+            "kotlin.jvm.functions.Function5" -> "kotlin.Function5"
+            "kotlin.jvm.functions.Function6" -> "kotlin.Function6"
+            "kotlin.jvm.functions.Function7" -> "kotlin.Function7"
+            "kotlin.jvm.functions.Function8" -> "kotlin.Function8"
+            "kotlin.jvm.functions.Function9" -> "kotlin.Function9"
+            "kotlin.jvm.functions.Function10" -> "kotlin.Function10"
+            "kotlin.jvm.functions.Function11" -> "kotlin.Function11"
+            "kotlin.jvm.functions.Function12" -> "kotlin.Function12"
+            "kotlin.jvm.functions.Function13" -> "kotlin.Function13"
+            "kotlin.jvm.functions.Function14" -> "kotlin.Function14"
+            "kotlin.jvm.functions.Function15" -> "kotlin.Function15"
+            "kotlin.jvm.functions.Function16" -> "kotlin.Function16"
+            "kotlin.jvm.functions.Function17" -> "kotlin.Function17"
+            "kotlin.jvm.functions.Function18" -> "kotlin.Function18"
+            "kotlin.jvm.functions.Function19" -> "kotlin.Function19"
+            "kotlin.jvm.functions.Function20" -> "kotlin.Function20"
+            "kotlin.jvm.functions.Function21" -> "kotlin.Function21"
+            "kotlin.jvm.functions.Function22" -> "kotlin.Function22"
+            "kotlin.jvm.internal.BooleanCompanionObject" -> "kotlin.Boolean.Companion"
+            "kotlin.jvm.internal.ByteCompanionObject" -> "kotlin.Byte.Companion"
+            "kotlin.jvm.internal.CharCompanionObject" -> "kotlin.Char.Companion"
+            "kotlin.jvm.internal.DoubleCompanionObject" -> "kotlin.Double.Companion"
+            "kotlin.jvm.internal.EnumCompanionObject" -> "kotlin.Enum.Companion"
+            "kotlin.jvm.internal.FloatCompanionObject" -> "kotlin.Float.Companion"
+            "kotlin.jvm.internal.IntCompanionObject" -> "kotlin.Int.Companion"
+            "kotlin.jvm.internal.LongCompanionObject" -> "kotlin.Long.Companion"
+            "kotlin.jvm.internal.ShortCompanionObject" -> "kotlin.Short.Companion"
+            "kotlin.jvm.internal.StringCompanionObject" -> "kotlin.String.Companion"
+            else -> null
         }
 
-        private val simpleNames = classFqNames.mapValues { (_, fqName) -> fqName.substringAfterLast('.') }
+        private fun simpleNameOf(type: String) = when (type) {
+            "boolean" -> "Boolean"
+            "byte" -> "Byte"
+            "char" -> "Char"
+            "double" -> "Double"
+            "float" -> "Float"
+            "int" -> "Int"
+            "long" -> "Long"
+            "short" -> "Short"
+            "java.lang.annotation.Annotation" -> "Annotation"
+            "java.lang.Boolean" -> "Boolean"
+            "java.lang.Byte" -> "Byte"
+            "java.lang.Character" -> "Char"
+            "java.lang.CharSequence" -> "CharSequence"
+            "java.lang.Cloneable" -> "Cloneable"
+            "java.lang.Comparable" -> "Comparable"
+            "java.lang.Double" -> "Double"
+            "java.lang.Enum" -> "Enum"
+            "java.lang.Float" -> "Float"
+            "java.lang.Integer" -> "Int"
+            "java.lang.Iterable" -> "Iterable"
+            "java.lang.Long" -> "Long"
+            "java.lang.Number" -> "Number"
+            "java.lang.Object" -> "Any"
+            "java.lang.Short" -> "Short"
+            "java.lang.String" -> "String"
+            "java.lang.Throwable" -> "Throwable"
+            "java.util.Collection" -> "Collection"
+            "java.util.Iterator" -> "Iterator"
+            "java.util.ListIterator" -> "ListIterator"
+            "java.util.List" -> "List"
+            "java.util.Map\$Entry" -> "Entry"
+            "java.util.Map" -> "Map"
+            "java.util.Set" -> "Set"
+            "kotlin.jvm.functions.Function0" -> "Function0"
+            "kotlin.jvm.functions.Function1" -> "Function1"
+            "kotlin.jvm.functions.Function2" -> "Function2"
+            "kotlin.jvm.functions.Function3" -> "Function3"
+            "kotlin.jvm.functions.Function4" -> "Function4"
+            "kotlin.jvm.functions.Function5" -> "Function5"
+            "kotlin.jvm.functions.Function6" -> "Function6"
+            "kotlin.jvm.functions.Function7" -> "Function7"
+            "kotlin.jvm.functions.Function8" -> "Function8"
+            "kotlin.jvm.functions.Function9" -> "Function9"
+            "kotlin.jvm.functions.Function10" -> "Function10"
+            "kotlin.jvm.functions.Function11" -> "Function11"
+            "kotlin.jvm.functions.Function12" -> "Function12"
+            "kotlin.jvm.functions.Function13" -> "Function13"
+            "kotlin.jvm.functions.Function14" -> "Function14"
+            "kotlin.jvm.functions.Function15" -> "Function15"
+            "kotlin.jvm.functions.Function16" -> "Function16"
+            "kotlin.jvm.functions.Function17" -> "Function17"
+            "kotlin.jvm.functions.Function18" -> "Function18"
+            "kotlin.jvm.functions.Function19" -> "Function19"
+            "kotlin.jvm.functions.Function20" -> "Function20"
+            "kotlin.jvm.functions.Function21" -> "Function21"
+            "kotlin.jvm.functions.Function22" -> "Function22"
+            "kotlin.jvm.internal.BooleanCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.ByteCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.CharCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.DoubleCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.EnumCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.FloatCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.IntCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.LongCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.ShortCompanionObject" -> "Companion"
+            "kotlin.jvm.internal.StringCompanionObject" -> "Companion"
+            else -> null
+        }
 
         public fun getClassSimpleName(jClass: Class<*>): String? = when {
             jClass.isAnonymousClass -> null
@@ -173,11 +258,11 @@ public class ClassReference(override val jClass: Class<*>) : KClass<Any>, ClassB
             jClass.isArray -> {
                 val componentType = jClass.componentType
                 when {
-                    componentType.isPrimitive -> simpleNames[componentType.name]?.plus("Array")
+                    componentType.isPrimitive -> simpleNameOf(componentType.name)?.plus("Array")
                     else -> null
                 } ?: "Array"
             }
-            else -> simpleNames[jClass.name] ?: jClass.simpleName
+            else -> simpleNameOf(jClass.name) ?: jClass.simpleName
         }
 
         public fun getClassQualifiedName(jClass: Class<*>): String? = when {
@@ -186,11 +271,11 @@ public class ClassReference(override val jClass: Class<*>) : KClass<Any>, ClassB
             jClass.isArray -> {
                 val componentType = jClass.componentType
                 when {
-                    componentType.isPrimitive -> classFqNames[componentType.name]?.plus("Array")
+                    componentType.isPrimitive -> classFqNameOf(componentType.name)?.plus("Array")
                     else -> null
                 } ?: "kotlin.Array"
             }
-            else -> classFqNames[jClass.name] ?: jClass.canonicalName
+            else -> classFqNameOf(jClass.name) ?: jClass.canonicalName
         }
 
         public fun isInstance(value: Any?, jClass: Class<*>): Boolean {