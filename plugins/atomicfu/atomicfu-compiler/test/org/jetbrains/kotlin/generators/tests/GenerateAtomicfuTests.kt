ddf2eae4597f45dcd1d8d188c0e14bf03fe91a90
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-02-26 00:14:30 +0100

    [atomicfu-K/N] Gather generation of Atomicfu tests, part 2

--- plugins/atomicfu/atomicfu-compiler/test/org/jetbrains/kotlin/generators/tests/GenerateAtomicfuTests.kt
+++ plugins/atomicfu/atomicfu-compiler/test/org/jetbrains/kotlin/generators/tests/GenerateAtomicfuTests.kt
@@ -6,14 +6,30 @@
 package org.jetbrains.kotlin.generators.tests
 
 import org.jetbrains.kotlin.generators.generateTestGroupSuiteWithJUnit5
+import org.jetbrains.kotlin.generators.impl.generateTestGroupSuite
 import org.jetbrains.kotlin.generators.model.annotation
-import org.jetbrains.kotlin.konan.test.blackbox.support.group.FirPipeline
+import org.jetbrains.kotlin.konan.test.blackbox.AbstractNativeCodegenBoxTest
 import org.jetbrains.kotlin.konan.test.blackbox.support.group.UseExtTestCaseGroupProvider
+import org.jetbrains.kotlin.test.TargetBackend
+import org.jetbrains.kotlinx.atomicfu.incremental.AbstractIncrementalK2JVMWithAtomicfuRunnerTest
+import org.jetbrains.kotlinx.atomicfu.runners.AbstractAtomicfuFirCheckerTest
+import org.jetbrains.kotlinx.atomicfu.runners.AbstractAtomicfuJsFirTest
+import org.jetbrains.kotlinx.atomicfu.runners.AbstractAtomicfuJsIrTest
+import org.jetbrains.kotlinx.atomicfu.runners.AbstractAtomicfuJvmFirLightTreeTest
+import org.jetbrains.kotlinx.atomicfu.runners.AbstractAtomicfuJvmIrTest
 import org.jetbrains.kotlinx.atomicfu.runners.AbstractAtomicfuNativeIrTextTest
 import org.junit.jupiter.api.Tag
 
-fun main() {
+fun main(args: Array<String>) {
     System.setProperty("java.awt.headless", "true")
+
+    generateTestGroupSuite(args) {
+        testGroup("plugins/atomicfu/atomicfu-compiler/tests-gen", "plugins/atomicfu/atomicfu-compiler/testData/") {
+            testClass<AbstractIncrementalK2JVMWithAtomicfuRunnerTest> {
+                model("projects/", extension = null, recursive = false, targetBackend = TargetBackend.JVM_IR)
+            }
+        }
+    }
 
     generateTestGroupSuiteWithJUnit5 {
         testGroup("plugins/atomicfu/atomicfu-compiler/tests-gen", "plugins/atomicfu/atomicfu-compiler/testData/box") {
@@ -24,15 +40,57 @@ fun main() {
                 model()
             }
         }
+
+        // Atomicfu compiler plugin native tests.
+        testGroup("plugins/atomicfu/atomicfu-compiler/tests-gen", "plugins/atomicfu/atomicfu-compiler/testData/box") {
+            testClass<AbstractNativeCodegenBoxTest>(
+                suiteTestClassName = "AtomicfuNativeTestGenerated",
+                annotations = listOf(*atomicfuNative(), *frontendClassic(), provider<UseExtTestCaseGroupProvider>())
+            ) {
+                model(targetBackend = TargetBackend.NATIVE)
+            }
+            testClass<AbstractNativeCodegenBoxTest>(
+                suiteTestClassName = "AtomicfuNativeFirTestGenerated",
+                annotations = listOf(*atomicfuNative(), *frontendFir(), provider<UseExtTestCaseGroupProvider>())
+            ) {
+                model(targetBackend = TargetBackend.NATIVE)
+            }
+        }
+
+        testGroup(
+            "plugins/atomicfu/atomicfu-compiler/tests-gen",
+            "plugins/atomicfu/atomicfu-compiler/testData",
+            testRunnerMethodName = "runTest0"
+        ) {
+            testClass<AbstractAtomicfuJsIrTest> {
+                model("box/")
+            }
+
+            testClass<AbstractAtomicfuJsFirTest> {
+                model("box/")
+            }
+        }
+
+        testGroup(
+            "plugins/atomicfu/atomicfu-compiler/tests-gen",
+            "plugins/atomicfu/atomicfu-compiler/testData",
+            testRunnerMethodName = "runTest0"
+        ) {
+            testClass<AbstractAtomicfuFirCheckerTest> {
+                model("diagnostics/")
+            }
+
+            testClass<AbstractAtomicfuJvmIrTest> {
+                model("box/")
+            }
+
+            testClass<AbstractAtomicfuJvmFirLightTreeTest> {
+                model("box/")
+            }
+        }
     }
 }
 
-
-fun frontendFir() = arrayOf(
-    annotation(FirPipeline::class.java)
-)
-
 private fun atomicfuNative() = arrayOf(
     annotation(Tag::class.java, "atomicfu-native"),
-//    annotation(EnforcedHostTarget::class.java), // TODO(KT-65977): Make atomicfu tests run on all targets.
 )
