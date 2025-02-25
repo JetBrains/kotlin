853520e019aa8c967015040fe3b4191f2d0181e5
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-02-25 20:35:28 +0100

    Remove @Required

--- compiler/arguments/src/main/kotlin/org/jetbrains/kotlin/arguments/ValueType.kt
+++ compiler/arguments/src/main/kotlin/org/jetbrains/kotlin/arguments/ValueType.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.kotlin.arguments
 
-import kotlinx.serialization.Required
 import kotlinx.serialization.Serializable
 import kotlin.Boolean
 
@@ -18,22 +17,22 @@ sealed interface KotlinArgumentValueType<T : Any> {
 
     @Serializable
     class BooleanType(
-        @Required override val name: String = Boolean::class.simpleName!!,
-        @Required override val isNullable: Boolean = true,
-        @Required override val defaultValue: Boolean? = null,
+        override val name: String = Boolean::class.simpleName!!,
+        override val isNullable: Boolean = true,
+        override val defaultValue: Boolean? = null,
     ) : KotlinArgumentValueType<Boolean>
 
     @Serializable
     class KotlinVersionType(
-        @Required override val name: String = KotlinVersion::class.simpleName!!,
-        @Required override val isNullable: Boolean = true,
-        @Required override val defaultValue: KotlinVersion? = KotlinVersion.KOTLIN_2_0,
+        override val name: String = KotlinVersion::class.simpleName!!,
+        override val isNullable: Boolean = true,
+        override val defaultValue: KotlinVersion? = KotlinVersion.KOTLIN_2_0,
     ) : KotlinArgumentValueType<KotlinVersion>
 
     @Serializable
     class KotlinJvmTargetType(
-        @Required override val name: String = JvmTarget::class.simpleName!!,
-        @Required override val isNullable: Boolean = true,
-        @Required override val defaultValue: JvmTarget? = JvmTarget.JvmTarget_1_8,
+        override val name: String = JvmTarget::class.simpleName!!,
+        override val isNullable: Boolean = true,
+        override val defaultValue: JvmTarget? = JvmTarget.JvmTarget_1_8,
     ) : KotlinArgumentValueType<JvmTarget>
 }
