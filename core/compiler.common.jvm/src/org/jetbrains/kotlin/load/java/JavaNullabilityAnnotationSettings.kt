7d6b21d0b41ff9543c1efd07aca139690f9c6c1b
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2025-02-26 15:13:58 +0900

    fix test

--- core/compiler.common.jvm/src/org/jetbrains/kotlin/load/java/JavaNullabilityAnnotationSettings.kt
+++ core/compiler.common.jvm/src/org/jetbrains/kotlin/load/java/JavaNullabilityAnnotationSettings.kt
@@ -91,7 +91,7 @@ fun getDefaultReportLevelForAnnotation(annotationFqName: FqName) =
 fun getReportLevelForAnnotation(
     annotation: FqName,
     configuredReportLevels: NullabilityAnnotationStates<ReportLevel>,
-    configuredKotlinVersion: KotlinVersion = KotlinVersion(1, 7, 20)
+    configuredKotlinVersion: KotlinVersion = KotlinVersion(1, 7, 20),
 ): ReportLevel {
     configuredReportLevels[annotation]?.let { return it }
 
