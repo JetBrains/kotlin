d69369d725969c937a301d96d69942fc82a23dd6
Timofey Solonin <timofey.solonin@jetbrains.com>
2025-03-03 22:21:54 +0100

    Modernize tests about configuration avoidance
    ^KT-75659

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ConfigurationAvoidanceIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ConfigurationAvoidanceIT.kt
@@ -5,12 +5,20 @@
 
 package org.jetbrains.kotlin.gradle
 
+import org.gradle.api.attributes.Attribute
+import org.gradle.api.internal.artifacts.configurations.ConfigurationInternal
+import org.gradle.jvm.tasks.Jar
 import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.plugin.extraProperties
 import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.gradle.uklibs.include
 import org.jetbrains.kotlin.test.TestMetadata
 import org.junit.jupiter.api.DisplayName
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.io.path.appendText
-import kotlin.io.path.exists
+import kotlin.test.assertEquals
 
 @DisplayName("Tasks configuration avoidance")
 class ConfigurationAvoidanceIT : KGPBaseTest() {
@@ -20,9 +28,35 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
     @GradleTest
     fun testUnrelatedTaskNotConfigured(gradleVersion: GradleVersion) {
         project("simpleProject", gradleVersion) {
-            createTaskWithExpensiveConfiguration()
-
-            build("compileKotlin")
+            val compilationConfiguredTasks = configuredTasks().buildAndReturn("compileKotlin")
+            if (gradleVersion < GradleVersion.version(TestVersions.Gradle.G_8_0)) {
+                assertEquals(
+                    mapOf(
+                        ":" to setOf(
+                            "checkKotlinGradlePluginConfigurationErrors",
+                            "clean",
+                            "compileDeployJava",
+                            "compileJava",
+                            "compileKotlin",
+                            "compileTestJava",
+                            "jar"
+                        )
+                    ),
+                    compilationConfiguredTasks,
+                )
+            } else {
+                assertEquals(
+                    mapOf(
+                        ":" to setOf(
+                            "checkKotlinGradlePluginConfigurationErrors",
+                            "clean",
+                            "compileJava",
+                            "compileKotlin",
+                        )
+                    ),
+                    compilationConfiguredTasks,
+                )
+            }
         }
     }
 
@@ -31,18 +65,10 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
     @GradleTest
     fun testJvmConfigurationAvoidance(gradleVersion: GradleVersion) {
         project("simpleProject", gradleVersion) {
-            buildGradle.appendText(
-                """
-                |
-                |tasks.configureEach {
-                |    if (name != "help" && name != "clean") {
-                |        throw new GradleException("Configuration avoidance failure for ${'$'}name!")
-                |    }
-                |}
-                """.trimMargin()
+            assertEquals(
+                mapOf(":" to setOf("clean", "help")),
+                configuredTasks().buildAndReturn("--dry-run"),
             )
-
-            build("--dry-run")
         }
     }
 
@@ -51,18 +77,10 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
     @GradleTest
     fun testKaptConfigurationAvoidance(gradleVersion: GradleVersion) {
         project("kapt2/simple", gradleVersion) {
-            buildGradle.appendText(
-                """
-                |
-                |tasks.configureEach {
-                |    if (name != "help" && name != "clean") {
-                |        throw new GradleException("Configuration avoidance failure for ${'$'}name!")
-                |    }
-                |}
-                """.trimMargin()
+            assertEquals(
+                mapOf(":" to setOf("clean", "help")),
+                configuredTasks().buildAndReturn("--dry-run"),
             )
-
-            build("--dry-run")
         }
     }
 
@@ -80,42 +98,17 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
             buildOptions = defaultBuildOptions.copy(androidVersion = agpVersion),
             buildJdk = providedJdk.location
         ) {
-
             gradleProperties.appendText("android.defaults.buildfeatures.aidl=true")
-            listOf("Android", "Test").forEach { subproject ->
-                subProject(subproject)
-                    .buildGradle
-                    .append(
-                        //language=Groovy
-                        """
-                        
-                        android {
-                            applicationVariants.all {
-                                it.getAidlCompileProvider().configure {
-                                    throw new RuntimeException("Task should not be configured.")
-                                }
-                            }
-                        }
-                        """.trimIndent()
-                    )
-            }
 
-            subProject("Lib")
-                .buildGradle
-                .append(
-                    //language=Groovy
-                    """
-                    
-                    android {
-                        libraryVariants.all {
-                            it.getAidlCompileProvider().configure {
-                                throw new RuntimeException("Task should not be configured.")
-                            }
-                        }
-                    }
-                    """.trimIndent()
-                )
-
+            assertEquals(
+                mapOf(
+                    ":" to setOf("help"),
+                    ":Android" to setOf("clean"),
+                    ":Lib" to setOf("clean"),
+                    ":Test" to setOf("clean"),
+                ),
+                configuredTasks().buildAndReturn("--dry-run"),
+            )
             build("help")
         }
     }
@@ -125,9 +118,10 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
     @GradleTest
     fun jsNoTasksConfigured(gradleVersion: GradleVersion) {
         project("kotlin-js-plugin-project", gradleVersion) {
-            createTaskWithExpensiveConfiguration()
-
-            build("help")
+            assertEquals(
+                mapOf(":" to setOf("clean", "help")),
+                configuredTasks().buildAndReturn("help"),
+            )
         }
     }
 
@@ -136,46 +130,73 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
     @GradleTest
     fun mppNoTasksConfigured(gradleVersion: GradleVersion) {
         project("new-mpp-lib-and-app/sample-app", gradleVersion) {
-            createTaskWithExpensiveConfiguration()
-
-            build("help")
+            assertEquals(
+                mapOf(":" to setOf("clean", "help")),
+                configuredTasks().buildAndReturn("help"),
+            )
         }
     }
 
-    private fun TestProject.createTaskWithExpensiveConfiguration(
-        expensivelyConfiguredTaskName: String = "expensivelyConfiguredTask"
-    ): String {
-        @Suppress("GroovyAssignabilityCheck")
-        if (buildGradle.exists()) {
-            buildGradle.appendText(
-                //language=Groovy
-                """
-                    
-                tasks.register("$expensivelyConfiguredTaskName") {
-                    throw new GradleException("Should not configure expensive task!")
+    @MppGradlePluginTests
+    @DisplayName("Check configuredTasks dumping utility dumps expected set of tasks")
+    @GradleTest
+    fun configuredTasksSanityCheck(gradleVersion: GradleVersion) {
+        val a = project("empty", gradleVersion) {
+            buildScriptInjection {
+                project.tasks.register("unrelated")
+                project.tasks.register("transitiveInA")
+                project.tasks.register("direct") {
+                    it.dependsOn("transitiveInA")
                 }
-                """.trimIndent()
-            )
-        } else {
-            buildGradleKts.appendText(
-                //language=kotlin
-                """
-                |
-                |tasks.register("$expensivelyConfiguredTaskName") {
-                |   throw GradleException("Should not configure expensive task!")
-                |}
-                """.trimMargin()
-            )
+            }
+        }
+        val b = project("empty", gradleVersion) {
+            buildScriptInjection {
+                project.tasks.register("unrelated")
+                project.tasks.register("transitiveInB")
+                project.tasks.register("direct") {
+                    it.dependsOn("transitiveInB")
+                }
+            }
         }
 
-        return expensivelyConfiguredTaskName
+        val configuredTasks = project("empty", gradleVersion) {
+            settingsBuildScriptInjection {
+                settings.rootProject.name = "root"
+            }
+            include(a, "projectA")
+            include(b, "projectB")
+        }.configuredTasks().buildAndReturn("direct")
+        assertEquals(
+            mapOf(
+                ":projectA" to setOf("direct", "transitiveInA"),
+                ":projectB" to setOf("direct", "transitiveInB"),
+            ),
+            configuredTasks,
+        )
+    }
+
+    private fun TestProject.configuredTasks() = buildScriptReturn {
+        val configuredTasks = ConcurrentHashMap<String, MutableSet<String>>()
+        project.gradle.allprojects { project ->
+            project.tasks.configureEach { task ->
+                configuredTasks.getOrPut(
+                    project.path, { Collections.newSetFromMap(ConcurrentHashMap()) }
+                ).add(task.name)
+            }
+        }
+        return@buildScriptReturn configuredTasks
     }
 
     @JvmGradlePluginTests
     @DisplayName("JVM early configuration resolution")
     @GradleTest
     fun testEarlyConfigurationsResolutionKotlin(gradleVersion: GradleVersion) {
-        testEarlyConfigurationsResolution("kotlinProject", gradleVersion, kts = false)
+        val eagerlyResolvedConfigurations = testEarlyConfigurationsResolution("kotlinProject", gradleVersion)
+        assertEquals(
+            mapOf(),
+            eagerlyResolvedConfigurations,
+        )
     }
 
     @JsGradlePluginTests
@@ -183,42 +204,122 @@ class ConfigurationAvoidanceIT : KGPBaseTest() {
     @GradleTest
     @TestMetadata("kotlin-js-browser-project")
     fun testEarlyConfigurationsResolutionKotlinJs(gradleVersion: GradleVersion) {
-        testEarlyConfigurationsResolution("kotlin-js-browser-project", gradleVersion, kts = true)
+        val eagerlyResolvedConfigurations = testEarlyConfigurationsResolution("kotlin-js-browser-project", gradleVersion)
+        assertEquals(
+            mapOf(
+                ":base" to setOf(
+                    "testNpmAggregated",
+                    "npmAggregated",
+                ),
+                ":app" to setOf(
+                    "testNpmAggregated",
+                    "npmAggregated",
+                ),
+                ":lib" to setOf(
+                    "testNpmAggregated",
+                    "npmAggregated",
+                )
+            ),
+            eagerlyResolvedConfigurations,
+        )
+    }
+
+    @MppGradlePluginTests
+    @DisplayName("Check eagerlyResolvedConfigurations dumping utility dumps expected set of configurations")
+    @GradleTest
+    fun eagerlyResolvedConfigurationsSanityCheck(gradleVersion: GradleVersion) {
+        val producer = project("empty", gradleVersion) {
+            buildScriptInjection {
+                project.configurations.create("neverResolvedInProducer") { it.isCanBeConsumed = false }
+                val extendsForDependencies = project.configurations.create("dependenciesExtendedTransitivelyEagerlyInProducer") {
+                    it.isCanBeConsumed = false
+                }
+                val attribute = Attribute.of("attribute", String::class.java)
+                project.configurations.create("consumableEagerlyInProducer") {
+                    it.extendsFrom(extendsForDependencies)
+                    it.isCanBeResolved = false
+                    it.attributes.attribute(attribute, "value")
+                }
+            }
+        }
+
+        val consumer = project("empty", gradleVersion) {
+            buildScriptInjection {
+                val attribute = Attribute.of("attribute", String::class.java)
+                project.configurations.create("resolvedEagerlyInConsumer") {
+                    it.isCanBeConsumed = false
+                    it.attributes.attribute(attribute, "value")
+                    it.dependencies.add(project.dependencies.project(mapOf("path" to ":producer")))
+                }.resolve()
+
+                val resolvedByTaskInput = project.configurations.create("resolvedByTaskInputConsumer") { it.isCanBeConsumed = false }
+                val resolvedAtExecution = project.configurations.create("resolvedAtExecutionInConsumer") { it.isCanBeConsumed = false }
+                project.tasks.register("execute") {
+                    it.inputs.files(resolvedByTaskInput)
+                    it.doLast {
+                        resolvedAtExecution.resolve()
+                    }
+                }
+            }
+        }
+
+        val resolvedConfigurations = project("empty", gradleVersion) {
+            settingsBuildScriptInjection {
+                settings.rootProject.name = "root"
+            }
+            include(producer, "producer")
+            include(consumer, "consumer")
+        }.eagerlyResolvedConfigurations().buildAndReturn(":consumer:execute")
+        assertEquals(
+            mapOf(
+                ":consumer" to setOf("resolvedEagerlyInConsumer"),
+            ),
+            resolvedConfigurations,
+        )
+    }
+
+    private fun TestProject.eagerlyResolvedConfigurations(): ReturnFromBuildScriptAfterExecution<EagerlyResolvedConfigurationsReturn> {
+        val eagerlyResolvedConfigurationsProperty = "eagerlyResolvedConfigurations"
+        buildScriptInjection {
+            val eagerlyResolvedConfigurations = EagerlyResolvedConfigurationsReturn()
+            project.extraProperties.set(eagerlyResolvedConfigurationsProperty, eagerlyResolvedConfigurations)
+
+            val configurationTime = AtomicBoolean(true)
+            project.gradle.taskGraph.whenReady {
+                configurationTime.set(false)
+            }
+
+            project.gradle.allprojects { project ->
+                project.configurations.all { configuration ->
+                    configuration.incoming.beforeResolve {
+                        if (configurationTime.get()) {
+                            eagerlyResolvedConfigurations.getOrPut(
+                                project.path, { Collections.newSetFromMap(ConcurrentHashMap()) }
+                            ).add(configuration.name)
+                        }
+                    }
+                }
+            }
+        }
+
+        return buildScriptReturn {
+            @Suppress("UNCHECKED_CAST")
+            project.extraProperties.get(eagerlyResolvedConfigurationsProperty) as EagerlyResolvedConfigurationsReturn
+        }
     }
 
     private fun testEarlyConfigurationsResolution(
         projectName: String,
         gradleVersion: GradleVersion,
-        kts: Boolean,
         buildOptions: BuildOptions = defaultBuildOptions
-    ) = project(projectName, gradleVersion, buildOptions = buildOptions) {
-        (if (kts) buildGradleKts else buildGradle).appendText(
-            //language=Gradle
-            """${'\n'}
-            // KT-45834 start
-            ${if (kts) "var" else "def"} ready = false
-            gradle.taskGraph.whenReady {
-                println("Task Graph Ready")
-                ready = true
-            }
-
-            allprojects {
-                configurations.forEach { configuration ->
-                    configuration.incoming.beforeResolve {
-                        println("Resolving ${'$'}configuration")
-                        if (!ready) {
-                            throw ${if (kts) "" else "new"} GradleException("${'$'}configuration is being resolved at configuration time")
-                        }
-                    }
-                }
-            }
-            // KT-45834 end
-            """.trimIndent()
-        )
-
-        build(
-            "assemble",
-            "-m"
-        )
-    }
+    ) = project(
+        projectName,
+        gradleVersion,
+        buildOptions = buildOptions
+    ).eagerlyResolvedConfigurations().buildAndReturn(
+        "assemble",
+        "-m",
+    )
 }
+
+private typealias EagerlyResolvedConfigurationsReturn = ConcurrentHashMap<String, MutableSet<String>>
