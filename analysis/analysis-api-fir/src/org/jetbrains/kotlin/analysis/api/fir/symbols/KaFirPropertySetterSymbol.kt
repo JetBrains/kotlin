deb83df7ef74d7b9d2077a14f519fc2173aaa9a8
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-03-03 14:07:04 +0100

    [Analysis API FIR] provide correct isInline flag for property accessors
    With explicit `inline` modifier
    
    ^KT-75586 Fixed

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirPropertySetterSymbol.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirPropertySetterSymbol.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -81,7 +81,7 @@ internal class KaFirPropertySetterSymbol(
 
     override val isInline: Boolean
         get() = withValidityAssertion {
-            owningKaProperty.backingPsi?.hasModifier(KtTokens.INLINE_KEYWORD) ?: firSymbol.isInline
+            backingPsi?.hasModifier(KtTokens.INLINE_KEYWORD) == true || owningKaProperty.backingPsi?.hasModifier(KtTokens.INLINE_KEYWORD) ?: firSymbol.isInline
         }
 
     override val isOverride: Boolean
--- analysis/analysis-api/testData/symbols/singleSymbolByPsi/inlineSetter.kt
+++ analysis/analysis-api/testData/symbols/singleSymbolByPsi/inlineSetter.kt
@@ -1,4 +1,4 @@
-// DO_NOT_CHECK_NON_PSI_SYMBOL_RESTORE
+// DO_NOT_CHECK_NON_PSI_SYMBOL_RESTORE_K1
 
 var <caret>foo: Int
     get() = 0
