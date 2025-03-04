6768217ce3fed74c2728b096462966e67c0eb56b
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-03-04 19:23:29 +0100

    [IR] Refactor IrTypeParameterRemapper into two classes
    Allow passing arbitrary context to its methods.

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/RemapTypes.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/RemapTypes.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.ir.util
 
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.types.IrType
+import org.jetbrains.kotlin.ir.visitors.IrTypeTransformer
 import org.jetbrains.kotlin.ir.visitors.IrTypeTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.acceptVoid
 
@@ -21,5 +22,24 @@ fun IrElement.remapTypes(typeRemapper: TypeRemapper) {
                 return type?.let { typeRemapper.remapType(it) } as Type
             }
         }
+    )
+}
+
+/**
+ * Modifies the node in place, applying [typeRemapper] to all its [IrType]-containing fields.
+ */
+fun <D> IrElement.remapTypes(typeRemapper: TypeRemapperWithData<D>, data: D) {
+    accept(
+        object : IrTypeTransformer<Unit, D>() {
+            override fun visitElement(element: IrElement, data: D) {
+                element.acceptChildren(this, data)
+            }
+
+            override fun <Type : IrType?> transformTypeRecursively(container: IrElement, type: Type, data: D): Type {
+                @Suppress("UNCHECKED_CAST")
+                return type?.let { typeRemapper.remapType(it, data) } as Type
+            }
+        },
+        data,
     )
 }
--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/TypeRemapper.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/util/TypeRemapper.kt
@@ -8,10 +8,28 @@ package org.jetbrains.kotlin.ir.util
 import org.jetbrains.kotlin.ir.declarations.IrTypeParametersContainer
 import org.jetbrains.kotlin.ir.types.IrType
 
-interface TypeRemapper {
+interface TypeRemapperWithData<in D> {
+    fun enterScope(irTypeParametersContainer: IrTypeParametersContainer, data: D)
+    fun remapType(type: IrType, data: D): IrType
+    fun leaveScope(data: D)
+}
+
+interface TypeRemapper : TypeRemapperWithData<Nothing?> {
     fun enterScope(irTypeParametersContainer: IrTypeParametersContainer)
+
+    override fun enterScope(irTypeParametersContainer: IrTypeParametersContainer, data: Nothing?) {
+        enterScope(irTypeParametersContainer)
+    }
+
     fun remapType(type: IrType): IrType
+
+    override fun remapType(type: IrType, data: Nothing?): IrType = remapType(type)
+
     fun leaveScope()
+
+    override fun leaveScope(data: Nothing?) {
+        leaveScope()
+    }
 }
 
 inline fun <T> TypeRemapper.withinScope(irTypeParametersContainer: IrTypeParametersContainer, fn: () -> T): T {
