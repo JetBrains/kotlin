7f0c62c3e308c20402f53e9e056bc9696a133d7c
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-03-05 15:31:03 +0100

    [Metadata] Prepare the logic to deduce KLib metadata version from LV
    #KT-74417

--- compiler/frontend.common/src/org/jetbrains/kotlin/util/klibMetadataHelpers.kt
+++ compiler/frontend.common/src/org/jetbrains/kotlin/util/klibMetadataHelpers.kt
@@ -6,11 +6,29 @@
 package org.jetbrains.kotlin.util
 
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.LanguageVersion
+import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.config.metadataVersion
 import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
+import java.util.*
 
 private val KLIB_LEGACY_METADATA_VERSION = MetadataVersion(1, 4, 1)
 
-fun CompilerConfiguration.klibMetadataVersionOrDefault(): MetadataVersion {
-    return this.metadataVersion as? MetadataVersion ?: KLIB_LEGACY_METADATA_VERSION
+private val LANGUAGE_TO_KLIB_METADATA_VERSION = EnumMap<LanguageVersion, MetadataVersion>(LanguageVersion::class.java).apply {
+    LanguageVersion.entries.forEach { this[it] = KLIB_LEGACY_METADATA_VERSION }
+    // TODO KT-74417 Uncomment in version 2.3 to bump metadata version
+    this[LanguageVersion.KOTLIN_2_3] = KLIB_LEGACY_METADATA_VERSION // MetadataVersion(2, 3, 0)
+
+    check(size == LanguageVersion.entries.size) {
+        "Please add mappings from the missing LanguageVersion instances to the corresponding MetadataVersion " +
+                "in `LANGUAGE_TO_KLIB_METADATA_VERSION`"
+    }
+}
+
+private fun LanguageVersion.toKlibMetadataVersion(): MetadataVersion = LANGUAGE_TO_KLIB_METADATA_VERSION.getValue(this)
+
+fun CompilerConfiguration.klibMetadataVersionOrDefault(
+    languageVersion: LanguageVersion = languageVersionSettings.languageVersion
+): MetadataVersion {
+    return this.metadataVersion as? MetadataVersion ?: languageVersion.toKlibMetadataVersion()
 }
