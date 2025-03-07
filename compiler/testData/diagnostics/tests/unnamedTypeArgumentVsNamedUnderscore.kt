f2023670b214987e3bae31c77c51c252b66b2461
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-03-07 17:07:07 +0200

    [FIR] Add tests for unnamed local variables
    `unnamedTypeArgumentVsNamedUnderscore.fir.kt` behaves differently with
    PSI and LT: with LT it loses the diagnostics.
    
    ^KT-74809

---
+++ compiler/testData/diagnostics/tests/unnamedTypeArgumentVsNamedUnderscore.kt
@@ -0,0 +1,11 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-74809
+// WITH_STDLIB
+
+typealias `_` = Int
+
+fun main() {
+    val x: MutableList<String> = mutableListOf<_>()
+    val y: MutableList<String> = <!TYPE_MISMATCH, TYPE_MISMATCH, TYPE_MISMATCH!>mutableListOf<`_`>()<!>
+    val z: MutableList<Int> = mutableListOf<`_`>()
+}
