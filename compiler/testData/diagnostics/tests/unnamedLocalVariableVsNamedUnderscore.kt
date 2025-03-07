b1db4e945ce493210033cac59af2e6f039cc4dc0
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-03-07 17:07:07 +0200

    [FIR] Add tests for unnamed local variables
    `unnamedTypeArgumentVsNamedUnderscore.fir.kt` behaves differently with
    PSI and LT: with LT it loses the diagnostics.

---
+++ compiler/testData/diagnostics/tests/unnamedLocalVariableVsNamedUnderscore.kt
@@ -0,0 +1,21 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-74809
+
+fun println(a: Any?) {}
+
+fun f() {
+    val `_` = "one"
+    val <!UNDERSCORE_IS_RESERVED!>_<!> = "two"
+    println(<!UNDERSCORE_USAGE_WITHOUT_BACKTICKS!>_<!>)
+    println(`_`)
+}
+
+fun g() {
+    "".let { `_` -> println(`_`) }
+    "".let { _ -> println(<!UNRESOLVED_REFERENCE!>_<!>) }
+}
+
+fun h() {
+    val <!UNDERSCORE_IS_RESERVED!>_<!> = "three"
+    println(<!UNRESOLVED_REFERENCE!>`_`<!>)
+}
