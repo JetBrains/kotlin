f953823d2024feea25328fac922c264246627c4a
Anton Lakotka <anton.lakotka@jetbrains.com>
2025-03-07 14:14:19 +0100

    [Gradle] Register pom rewriter as soon as publication is created
    So users can add their rewriters after.
    This is regression fix that was introduced in 2.1.0
    
    ^KT-75512 Verification Pending

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppDslPomIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/MppDslPomIT.kt
@@ -4,9 +4,13 @@
  */
 package org.jetbrains.kotlin.gradle.mpp
 
+import groovy.util.Node
+import groovy.util.NodeList
+import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
 import org.jetbrains.kotlin.test.TestMetadata
+import org.jetbrains.kotlin.util.capitalizeDecapitalize.toLowerCaseAsciiOnly
 import kotlin.io.path.pathString
 
 @MppGradlePluginTests
@@ -79,6 +83,82 @@ class MppDslPomIT : KGPBaseTest() {
 
             // Also check that the flag for keeping POMs intact works:
             doTestPomRewriting(mppProjectDependency = false, keepPomIntact = true)
+        }
+    }
+
+    @GradleTest
+    fun `KT-75512 user withXml should be invoked after KMP POM rewriter`(
+        gradleVersion: GradleVersion
+    ) {
+        fun GradleProjectBuildScriptInjectionContext.configureKmpProject() {
+            applyMavenPublishPlugin()
+            project.version = "1.0.0"
+            project.group = "sample"
+
+            kotlinMultiplatform.apply {
+                jvm()
+                linuxX64()
+            }
+        }
+
+        project("base-kotlin-multiplatform-library", gradleVersion) {
+            includeOtherProjectAsSubmodule("base-kotlin-multiplatform-library", newSubmoduleName = "utils") {
+                buildScriptInjection {
+                    project.group = "sample"
+                    configureKmpProject()
+                }
+            }
+
+            buildScriptInjection {
+                configureKmpProject()
+                kotlinMultiplatform.apply {
+                    sourceSets.commonMain.dependencies {
+                        api(project(":utils"))
+                    }
+                }
+                publishing.publications.configureEach { publication ->
+                    publication as MavenPublication
+
+                    // Append artifact ID string i.e.
+                    // <dependency>
+                    //  <artifactId>OLD-$MyArtifactId</artifactId>
+                    //  ...
+                    //  </dependency>
+                    // If pom rewriter did its job, it should contain target-specific artifact ID
+                    publication.pom.withXml { xmlProvider ->
+                        val root = xmlProvider.asNode()
+                        val dependenciesNode = (root.get("dependencies") as NodeList).filterIsInstance<Node>().single()
+                        val dependencyNodes = (dependenciesNode.get("dependency") as? NodeList).orEmpty().filterIsInstance<Node>()
+                        fun Node.updateChildNodeByName(name: String, map: (String) -> String) {
+                            val childNode = (get(name) as NodeList).single() as Node
+                            val newValue = map(childNode.value().toString())
+                            childNode.setValue(newValue)
+                        }
+                        for (dependencyNode in dependencyNodes) {
+                            dependencyNode.updateChildNodeByName("artifactId") { "$it-MyArtifactId" }
+                        }
+                    }
+                }
+            }
+
+            val tasks = arrayOf(
+                ":generatePomFileForJvmPublication",
+                ":generatePomFileForLinuxX64Publication"
+            )
+
+            fun assertTargetPublicationPomIsCorrect(targetName: String) {
+                assertFileInProjectContains(
+                    "build/publications/$targetName/pom-default.xml",
+                    "<artifactId>utils-${targetName.toLowerCaseAsciiOnly()}-MyArtifactId</artifactId>"
+                )
+            }
+
+            build(*tasks) {
+                println(projectPath)
+                assertTasksExecuted(*tasks)
+                assertTargetPublicationPomIsCorrect("jvm")
+                assertTargetPublicationPomIsCorrect("linuxX64")
+            }
         }
     }
 }
