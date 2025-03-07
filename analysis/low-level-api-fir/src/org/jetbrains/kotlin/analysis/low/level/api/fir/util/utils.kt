2bc0bfb4b9dffac171c7d5f3062816d017989f33
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-03-07 17:54:41 +0100

    [LL] ValueWithPostCompute: reuse lock interval settings from LLFirLockProvider
    ^KT-75790

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/utils.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/util/utils.kt
@@ -1,11 +1,12 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.util
 
 import com.intellij.openapi.progress.ProgressManager
+import org.jetbrains.kotlin.analysis.low.level.api.fir.file.builder.LLFirLockProvider
 import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.diagnostics.FirDiagnosticHolder
@@ -18,10 +19,10 @@ import org.jetbrains.kotlin.psi.psiUtil.isObjectLiteral
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.Lock
 
-internal inline fun <T> Lock.lockWithPCECheck(lockingIntervalMs: Long, action: () -> T): T {
+internal inline fun <T> Lock.lockWithPCECheck(action: () -> T): T {
     while (true) {
         checkCanceled()
-        if (tryLock(lockingIntervalMs, TimeUnit.MILLISECONDS)) {
+        if (tryLock(LLFirLockProvider.lockingInterval, TimeUnit.MILLISECONDS)) {
             try {
                 checkCanceled()
                 return action()
