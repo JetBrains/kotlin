05703eb2e5f8b491f0787daf389aac18afe6e0e9
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-03-07 15:11:29 +0100

    [Analysis API] Register `KaResolveExtensionToContentScopeRefinerBridge` in `FirStandaloneServiceRegistrar`

--- analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/projectStructure/KaResolveExtensionToContentScopeRefinerBridge.kt
+++ analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/projectStructure/KaResolveExtensionToContentScopeRefinerBridge.kt
@@ -10,7 +10,14 @@ import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinContent
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.analysis.api.resolve.extensions.KaResolveExtensionProvider
 
-internal class KaResolveExtensionToContentScopeRefinerBridge : KotlinContentScopeRefiner {
+class KaResolveExtensionToContentScopeRefinerBridge : KotlinContentScopeRefiner {
+    override fun getEnlargementScopes(module: KaModule): List<GlobalSearchScope> =
+        if (KaResolveExtensionProvider.provideExtensionsFor(module).isNotEmpty()) {
+            listOf(KaBaseResolveExtensionGeneratedFilesScope(listOf(module)))
+        } else {
+            emptyList()
+        }
+
     override fun getRestrictionScopes(module: KaModule): List<GlobalSearchScope> =
         KaResolveExtensionProvider.provideExtensionsFor(module).map { resolveExtension ->
             GlobalSearchScope.notScope(resolveExtension.getShadowedScope())
