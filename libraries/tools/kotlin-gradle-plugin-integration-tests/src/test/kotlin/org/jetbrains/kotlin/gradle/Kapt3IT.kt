0cff9a6374f58c5ac6db00f99a778482fb62d5b4
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-03-07 13:14:17 +0100

    [Gradle] make KAPT ITs CC-compatible
    ^KT-67291 In Progress

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kapt3IT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/Kapt3IT.kt
@@ -1138,7 +1138,6 @@ open class Kapt3IT : Kapt3BaseIT() {
 
     @DisplayName("Works with JPMS on JDK 9+")
     @GradleTest
-    @BrokenOnMacosTest
     fun testJpmsModule(gradleVersion: GradleVersion) {
         project(
             "jpms-module".withPrefix,
