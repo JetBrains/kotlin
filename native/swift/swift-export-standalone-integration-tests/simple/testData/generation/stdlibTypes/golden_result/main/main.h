66494c7f376fc4bb76dfb5f7aa9124937f288002
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-03-10 17:16:13 +0200

    [Swift export] Update stdlibTypes test golden data

---
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/stdlibTypes/golden_result/main/main.h
@@ -0,0 +1,4 @@
+#include <Foundation/Foundation.h>
+#include <stdint.h>
+
+uintptr_t generation_stdlibTypes_stdlibTypes_returnsByteArray();
---
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/stdlibTypes/golden_result/stdlib/stdlib.h
@@ -0,0 +1,4 @@
+#include <Foundation/Foundation.h>
+#include <stdint.h>
+
+int32_t kotlin_ByteArray_size_get(uintptr_t self);
