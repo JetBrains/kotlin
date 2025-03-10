8ebcd4452d43d911a6e1f8fb28b2eee53d9ca197
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2025-03-10 02:21:55 +0200

    [KDF] Update test configuration

--- plugins/kotlin-dataframe/tests/org/jetbrains/kotlin/fir/dataframe/AbstractDataFrameBlackBoxCodegenTest.kt
+++ plugins/kotlin-dataframe/tests/org/jetbrains/kotlin/fir/dataframe/AbstractDataFrameBlackBoxCodegenTest.kt
@@ -8,76 +8,53 @@ package org.jetbrains.kotlin.fir.dataframe
 import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.fir.dataframe.services.DataFramePluginAnnotationsProvider
 import org.jetbrains.kotlin.fir.dataframe.services.ExperimentalExtensionRegistrarConfigurator
-import org.jetbrains.kotlin.fir.dataframe.services.TemporaryDirectoryManagerImplFixed
-import org.jetbrains.kotlin.fir.dataframe.services.classpath.classpathFromClassloader
 import org.jetbrains.kotlin.test.TestJdkKind
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.configuration.enableLazyResolvePhaseChecking
-import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.IGNORE_DEXING
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
 import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
-import org.jetbrains.kotlin.test.initIdeaConfiguration
 import org.jetbrains.kotlin.test.model.TestFile
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.runners.codegen.AbstractFirLightTreeBlackBoxCodegenTest
 import org.jetbrains.kotlin.test.services.AdditionalSourceProvider
-import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
-import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
 import org.jetbrains.kotlin.test.services.RuntimeClasspathProvider
-import org.jetbrains.kotlin.test.services.TemporaryDirectoryManager
 import org.jetbrains.kotlin.test.services.TestModuleStructure
 import org.jetbrains.kotlin.test.services.TestServices
-import org.junit.jupiter.api.Assumptions
-import org.junit.jupiter.api.BeforeAll
 import java.io.File
+import kotlin.script.experimental.jvm.util.classpathFromClassloader
 
 open class AbstractDataFrameBlackBoxCodegenTest : AbstractFirLightTreeBlackBoxCodegenTest() {
-    companion object {
-        @BeforeAll
-        @JvmStatic
-        fun setUp() {
-            initIdeaConfiguration()
-        }
-    }
-
     override fun configure(builder: TestConfigurationBuilder) {
         super.configure(builder)
         builder.defaultDirectives {
-            JvmEnvironmentConfigurationDirectives.JDK_KIND with TestJdkKind.FULL_JDK_11
-            JvmEnvironmentConfigurationDirectives.JVM_TARGET with JvmTarget.JVM_11
+            JvmEnvironmentConfigurationDirectives.JDK_KIND with TestJdkKind.FULL_JDK
             +JvmEnvironmentConfigurationDirectives.WITH_REFLECT
-            +IGNORE_DEXING
         }
         builder.forTestsMatching("*/csDsl/*") {
             builder.useAdditionalSourceProviders(::SelectionDslUtilsSourceProvider)
         }
         builder.enableLazyResolvePhaseChecking()
-        builder.useAdditionalService<TemporaryDirectoryManager>(::TemporaryDirectoryManagerImplFixed)
         builder.useConfigurators(::DataFramePluginAnnotationsProvider)
         builder.useConfigurators(::ExperimentalExtensionRegistrarConfigurator)
-        builder.useCustomRuntimeClasspathProviders(::MyClasspathProvider)
+        builder.useCustomRuntimeClasspathProviders(::DataFrameClasspathProvider)
         builder.useAdditionalSourceProviders(::TestUtilsSourceProvider)
     }
 
-    class MyClasspathProvider(testServices: TestServices) : RuntimeClasspathProvider(testServices) {
+    class DataFrameClasspathProvider(testServices: TestServices) : RuntimeClasspathProvider(testServices) {
         override fun runtimeClassPaths(module: TestModule): List<File> {
             return (classpathFromClassloader(javaClass.classLoader) ?: error("no classpath"))
         }
     }
 
-    override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
-        return EnvironmentBasedStandardLibrariesPathProvider
-    }
-
     class TestUtilsSourceProvider(testServices: TestServices) : AdditionalSourceProvider(testServices) {
         companion object {
-            const val COMMON_SOURCE_PATH = "testData/testUtils.kt"
+            const val COMMON_SOURCE_PATH = "plugins/kotlin-dataframe/testData/testUtils.kt"
         }
 
         override fun produceAdditionalFiles(
             globalDirectives: RegisteredDirectives,
             module: TestModule,
-            testModuleStructure: TestModuleStructure
+            testModuleStructure: TestModuleStructure,
         ): List<TestFile> {
             return listOf(File(COMMON_SOURCE_PATH).toTestFile())
         }
@@ -85,13 +62,13 @@ open class AbstractDataFrameBlackBoxCodegenTest : AbstractFirLightTreeBlackBoxCo
 
     class SelectionDslUtilsSourceProvider(testServices: TestServices) : AdditionalSourceProvider(testServices) {
         companion object {
-            const val SELECTION_DSL_UTILS = "testData/selectionDslTestUtils.kt"
+            const val SELECTION_DSL_UTILS = "plugins/kotlin-dataframe/testData/selectionDslTestUtils.kt"
         }
 
         override fun produceAdditionalFiles(
             globalDirectives: RegisteredDirectives,
             module: TestModule,
-            testModuleStructure: TestModuleStructure
+            testModuleStructure: TestModuleStructure,
         ): List<TestFile> {
             return listOf(File(SELECTION_DSL_UTILS).toTestFile())
         }
--- plugins/kotlin-dataframe/tests/org/jetbrains/kotlin/fir/dataframe/AbstractDataFrameDiagnosticTest.kt
+++ plugins/kotlin-dataframe/tests/org/jetbrains/kotlin/fir/dataframe/AbstractDataFrameDiagnosticTest.kt
@@ -6,10 +6,9 @@
 package org.jetbrains.kotlin.fir.dataframe
 
 import org.jetbrains.kotlin.config.JvmTarget
-import org.jetbrains.kotlin.fir.dataframe.services.Directives
 import org.jetbrains.kotlin.fir.dataframe.services.DataFramePluginAnnotationsProvider
+import org.jetbrains.kotlin.fir.dataframe.services.Directives
 import org.jetbrains.kotlin.fir.dataframe.services.ExperimentalExtensionRegistrarConfigurator
-import org.jetbrains.kotlin.fir.dataframe.services.TemporaryDirectoryManagerImplFixed
 import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.TestJdkKind
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
@@ -17,44 +16,16 @@ import org.jetbrains.kotlin.test.configuration.baseFirDiagnosticTestConfiguratio
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
 import org.jetbrains.kotlin.test.frontend.fir.DisableLazyResolveChecksAfterAnalysisChecker
-import org.jetbrains.kotlin.test.initIdeaConfiguration
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerTest
-import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
-import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
-import org.jetbrains.kotlin.test.services.TemporaryDirectoryManager
-import org.junit.jupiter.api.Assumptions
-import org.junit.jupiter.api.BeforeAll
 
 abstract class AbstractDataFrameDiagnosticTest : AbstractKotlinCompilerTest() {
-
-    companion object {
-        @BeforeAll
-        @JvmStatic
-        fun setUp() {
-            initIdeaConfiguration()
-        }
-    }
-
-    override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
-        return EnvironmentBasedStandardLibrariesPathProvider
-    }
-
     override fun configure(builder: TestConfigurationBuilder) {
         builder.baseFirDiagnosticTestConfiguration()
-// disabled because checker it too strict and fails even when shouldn't
-//    firHandlersStep {
-//        useHandlers(
-//            ::FirResolveContractViolationErrorHandler,
-//        )
-//    }
-        builder.useAdditionalService<TemporaryDirectoryManager>(::TemporaryDirectoryManagerImplFixed)
-
         builder.defaultDirectives {
             +FirDiagnosticsDirectives.ENABLE_PLUGIN_PHASES
             +FirDiagnosticsDirectives.FIR_DUMP
             FirDiagnosticsDirectives.FIR_PARSER with FirParser.LightTree
-            JvmEnvironmentConfigurationDirectives.JDK_KIND with TestJdkKind.FULL_JDK_11
-            JvmEnvironmentConfigurationDirectives.JVM_TARGET with JvmTarget.JVM_11
+            JvmEnvironmentConfigurationDirectives.JDK_KIND with TestJdkKind.FULL_JDK
         }
 
         builder.useDirectives(Directives)
@@ -65,11 +36,5 @@ abstract class AbstractDataFrameDiagnosticTest : AbstractKotlinCompilerTest() {
         builder.useAfterAnalysisCheckers(
             ::DisableLazyResolveChecksAfterAnalysisChecker,
         )
-    }
-
-    override fun runTest(filePath: String) {
-        val muted = setOf("HistoryItem.kt")
-        Assumptions.assumeFalse(muted.any { filePath.contains(it) })
-        super.runTest(filePath)
     }
 }
