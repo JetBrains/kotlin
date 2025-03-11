29f65eb1556e5eb9f3aaf52ef028a5555d3969a3
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-03-11 19:39:02 +0100

    [stubs] add tests to aggregator
    ^KT-71787

--- analysis/build.gradle.kts
+++ analysis/build.gradle.kts
@@ -15,6 +15,7 @@ tasks.register("analysisAllTests") {
         ":analysis:analysis-api-standalone:test",
         ":analysis:low-level-api-fir:test",
         ":analysis:low-level-api-fir:tests-jdk11:test",
+        ":analysis:stubs:test",
         ":analysis:symbol-light-classes:test",
         ":analysis:analysis-tools:analysis-api-kdoc-coverage-test:test"
     )
