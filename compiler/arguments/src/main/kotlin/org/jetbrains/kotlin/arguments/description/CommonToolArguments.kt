f6576202de204c080dcd3a6a2d27677cd5663575
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-03-12 11:48:32 +0200

    [Args] Introduce an utilities for default boolean values

--- compiler/arguments/src/main/kotlin/org/jetbrains/kotlin/arguments/description/CommonToolArguments.kt
+++ compiler/arguments/src/main/kotlin/org/jetbrains/kotlin/arguments/description/CommonToolArguments.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.arguments.description
 import org.jetbrains.kotlin.arguments.KotlinReleaseVersion
 import org.jetbrains.kotlin.arguments.asReleaseDependent
 import org.jetbrains.kotlin.arguments.compilerArgumentsLevel
+import org.jetbrains.kotlin.arguments.defaultFalse
 import org.jetbrains.kotlin.arguments.types.BooleanType
 
 /*
@@ -21,11 +22,7 @@ val actualCommonToolsArguments by compilerArgumentsLevel(Levels.commonToolArgume
         shortName = "h"
         description = "Print a synopsis of standard options.".asReleaseDependent()
 
-        valueType = BooleanType(
-            isNullable = false.asReleaseDependent(),
-            defaultValue = false.asReleaseDependent()
-        )
-
+        valueType = BooleanType.defaultFalse
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v1_4_0,
             stabilizedVersion = KotlinReleaseVersion.v1_4_0
@@ -36,11 +33,7 @@ val actualCommonToolsArguments by compilerArgumentsLevel(Levels.commonToolArgume
         name = "X"
         description = "Print a synopsis of advanced options.".asReleaseDependent()
 
-        valueType = BooleanType(
-            isNullable = false.asReleaseDependent(),
-            defaultValue = false.asReleaseDependent()
-        )
-
+        valueType = BooleanType.defaultFalse
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v1_9_20
         )
@@ -50,11 +43,7 @@ val actualCommonToolsArguments by compilerArgumentsLevel(Levels.commonToolArgume
         name = "version"
         description = "Display the compiler version.".asReleaseDependent()
 
-        valueType = BooleanType(
-            isNullable = false.asReleaseDependent(),
-            defaultValue = false.asReleaseDependent()
-        )
-
+        valueType = BooleanType.defaultFalse
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v1_9_20
         )
@@ -64,11 +53,7 @@ val actualCommonToolsArguments by compilerArgumentsLevel(Levels.commonToolArgume
         name = "verbose"
         description = "Enable verbose logging output.".asReleaseDependent()
 
-        valueType = BooleanType(
-            isNullable = false.asReleaseDependent(),
-            defaultValue = false.asReleaseDependent()
-        )
-
+        valueType = BooleanType.defaultFalse
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v1_9_20
         )
@@ -78,11 +63,7 @@ val actualCommonToolsArguments by compilerArgumentsLevel(Levels.commonToolArgume
         name = "nowarn"
         description = "Don't generate any warnings.".asReleaseDependent()
 
-        valueType = BooleanType(
-            isNullable = false.asReleaseDependent(),
-            defaultValue = false.asReleaseDependent()
-        )
-
+        valueType = BooleanType.defaultFalse
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v1_9_20
         )
@@ -92,11 +73,7 @@ val actualCommonToolsArguments by compilerArgumentsLevel(Levels.commonToolArgume
         name = "Werror"
         description = "Report an error if there are any warnings.".asReleaseDependent()
 
-        valueType = BooleanType(
-            isNullable = false.asReleaseDependent(),
-            defaultValue = false.asReleaseDependent()
-        )
-
+        valueType = BooleanType.defaultFalse
         lifecycle(
             introducedVersion = KotlinReleaseVersion.v1_9_20
         )
