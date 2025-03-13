46ac99a47a03d150f6dcf047c888bd1a4c4c0ec5
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-03-13 10:45:35 +0100

    KLIB writer: Drop unused `IrMemoryIntArrayWriter` and `IrMemoryLongArrayWriter`

--- compiler/util-klib/src/org/jetbrains/kotlin/library/impl/IrFileWriters.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/impl/IrFileWriters.kt
@@ -74,22 +74,6 @@ class IrMemoryStringWriter(private val data: List<String>) : IrMemoryWriter() {
     }
 }
 
-class IrMemoryIntArrayWriter(private val data: List<Int>) : IrMemoryWriter() {
-    override fun writeData(dataOutput: DataOutput) {
-        dataOutput.writeInt(data.size)
-
-        data.forEach { dataOutput.writeInt(it) }
-    }
-}
-
-class IrMemoryLongArrayWriter(private val data: List<Long>) : IrMemoryWriter() {
-    override fun writeData(dataOutput: DataOutput) {
-        dataOutput.writeInt(data.size)
-
-        data.forEach { dataOutput.writeLong(it) }
-    }
-}
-
 class IrByteArrayWriter(private val data: List<ByteArray>) : IrFileWriter() {
     override fun writeData(dataOutput: DataOutput) {
         dataOutput.writeInt(data.size)
