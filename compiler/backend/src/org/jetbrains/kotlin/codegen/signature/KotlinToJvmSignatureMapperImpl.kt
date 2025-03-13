9e2499fab47f6dca3080f478e8ec31abaa6410bf
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-03-13 11:21:43 +0100

    JVM: move KotlinToJvmSignatureMapper to descriptors.jvm
    It doesn't need to depend on ASM, according to its only usage. This will
    simplify removing dependency of 'backend' on 'frontend.java'.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/signature/KotlinToJvmSignatureMapperImpl.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/signature/KotlinToJvmSignatureMapperImpl.kt
@@ -9,7 +9,9 @@ import org.jetbrains.kotlin.codegen.state.KotlinTypeMapper
 import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
-import org.jetbrains.kotlin.resolve.jvm.jvmSignature.KotlinToJvmSignatureMapper
+import org.jetbrains.kotlin.resolve.jvm.KotlinToJvmSignatureMapper
+import org.jetbrains.kotlin.resolve.jvm.KotlinToJvmSignatureMapper.MethodSignature
+import org.jetbrains.org.objectweb.asm.commons.Method
 
 class KotlinToJvmSignatureMapperImpl : KotlinToJvmSignatureMapper {
     private val typeMapper = KotlinTypeMapper(
@@ -18,5 +20,15 @@ class KotlinToJvmSignatureMapperImpl : KotlinToJvmSignatureMapper {
         useOldInlineClassesManglingScheme = false
     )
 
-    override fun mapToJvmMethodSignature(function: FunctionDescriptor) = typeMapper.mapAsmMethod(function)
+    override fun mapToJvmMethodSignature(function: FunctionDescriptor): MethodSignature =
+        MethodSignatureImpl(typeMapper.mapAsmMethod(function))
+
+    override fun erasedSignaturesEqualIgnoringReturnTypes(subFunction: MethodSignature, superFunction: MethodSignature): Boolean =
+        subFunction.parametersDescriptor() == superFunction.parametersDescriptor()
+
+    private fun MethodSignature.parametersDescriptor(): String = (this as MethodSignatureImpl).method.let { method ->
+        method.descriptor.substring(1, method.descriptor.lastIndexOf(")"))
+    }
+
+    private class MethodSignatureImpl(val method: Method) : MethodSignature
 }
