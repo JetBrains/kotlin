37a6bb83721999559115f0dbd78060d2a54909da
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-03-14 13:53:19 +0200

    [CLI] Extract some utilities from `CommonToolArguments.kt`

--- compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/CommonToolArguments.kt
+++ compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/CommonToolArguments.kt
@@ -107,25 +107,3 @@ abstract class CommonToolArguments : Freezable(), Serializable {
             field = value
         }
 }
-
-/**
- * An argument which should be passed to Kotlin compiler to enable [this] compiler option
- */
-val KProperty1<out CommonCompilerArguments, *>.argumentAnnotation: Argument
-    get() {
-        val javaField = javaField ?: error("Java field should be present for $this")
-        return javaField.getAnnotation(Argument::class.java)
-    }
-
-/**
- * An argument which should be passed to Kotlin compiler to enable [this] compiler option
- */
-val KProperty1<out CommonCompilerArguments, *>.cliArgument: String
-    get() = argumentAnnotation.value
-
-/**
- * Returns a string of the form "argument=value" where "argument" is the [Argument.value] of this compiler argument.
- */
-fun KProperty1<out CommonCompilerArguments, *>.cliArgument(value: String): String {
-    return "$cliArgument=$value"
-}
