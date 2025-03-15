JetBrains/kotlin

commit 40d931f366b4dfc08c9793807399c3e94c6df0b6

[JVM] Migrate CollectionStubMethodLowering to the new parameters API
#KT-73360

compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/CollectionStubMethodLowering.kt
@@ -12,8 +12,6 @@ import org.jetbrains.kotlin.backend.jvm.JvmBackendContext
 import org.jetbrains.kotlin.backend.jvm.caches.StubsForCollectionClass
 import org.jetbrains.kotlin.backend.jvm.ir.isJvmInterface
 import org.jetbrains.kotlin.backend.jvm.overridesWithoutStubs
-import org.jetbrains.kotlin.builtins.StandardNames
-import org.jetbrains.kotlin.config.AnalysisFlag
 import org.jetbrains.kotlin.config.AnalysisFlags
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.builders.declarations.buildFun
@@ -57,7 +55,7 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
     )
 
     private val IrSimpleFunction.nameAndArity
-        get() = NameAndArity(name, typeParameters.size, valueParameters.size)
+        get() = NameAndArity(name, typeParameters.size, nonDispatchParameters.size)
 
     override fun lower(irClass: IrClass) {
         if (irClass.isInterface) {
@@ -120,14 +118,14 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
                     if (context.config.languageVersionSettings.getFlag(AnalysisFlags.stdlibCompilation)
                         && irClass.classId == StandardClassIds.AbstractMap
                         && irClass.typeParameters.size == 2
-                        && stub.valueParameters.size == 2
-                        && stub.valueParameters[0].type.classifierOrNull == irClass.typeParameters[0].symbol
-                        && stub.valueParameters[1].type.classifierOrNull == irClass.typeParameters[1].symbol
+                        && stub.hasShape(regularParameters = 2)
+                        && stub.parameters[0].type.classifierOrNull == irClass.typeParameters[0].symbol
+                        && stub.parameters[1].type.classifierOrNull == irClass.typeParameters[1].symbol
                     ) {
                         continue
                     }
                     irClass.declarations.add(stub.apply {
-                        valueParameters = valueParameters.map {
+                        parameters = parameters.map {
                             it.copyWithCustomTypeSubstitution(this) { context.irBuiltIns.anyNType }
                         }
                     })
@@ -170,11 +168,7 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
         }.apply {
             // NB stub method for 'remove(int)' doesn't override any built-in Kotlin declaration
             parent = removeAtStub.parent
-            dispatchReceiverParameter = removeAtStub.dispatchReceiverParameter?.copyWithCustomTypeSubstitution(this) { it }
-            extensionReceiverParameter = null
-            valueParameters = removeAtStub.valueParameters.map { stubParameter ->
-                stubParameter.copyWithCustomTypeSubstitution(this) { it }
-            }
+            parameters = removeAtStub.parameters.map { stubParameter -> stubParameter.copyWithCustomTypeSubstitution(this) { it } }
             body = createThrowingStubBody(context, this)
         }
     }
@@ -198,9 +192,7 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
             // Add the abstract function symbol to stub function for bridge lowering
             overriddenSymbols = listOf(function.symbol)
             parent = irClass
-            dispatchReceiverParameter = function.dispatchReceiverParameter?.copyWithSubstitution(this, substitutionMap)
-            extensionReceiverParameter = function.extensionReceiverParameter?.copyWithSubstitution(this, substitutionMap)
-            valueParameters = function.valueParameters.map { it.copyWithSubstitution(this, substitutionMap) }
+            parameters = function.parameters.map { it.copyWithSubstitution(this, substitutionMap) }
             body = createThrowingStubBody(context, this)
         }
     }
@@ -227,7 +219,7 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
 
         if (superFun.name != overridingFun.name) return false
         if (superFun.typeParameters.size != overridingFun.typeParameters.size) return false
-        if (superFun.valueParameters.size != overridingFun.valueParameters.size) return false
+        if (superFun.getShapeOfParameters() != overridingFun.getShapeOfParameters()) return false
         if (!superFun.isSuspend && overridingFun.isSuspend) return false
 
         val typeChecker = createTypeCheckerState(superFun, overridingFun)
@@ -237,7 +229,7 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
         // but we keep it here for the sake of consistency.
         if (!areTypeParametersEquivalent(overridingFun, superFun, typeChecker)) return false
 
-        if (!areValueParametersEquivalent(overridingFun, superFun, typeChecker)) return false
+        if (!areParametersEquivalent(overridingFun, superFun, typeChecker)) return false
         if (!isReturnTypeOverrideCompliant(overridingFun, superFun, typeChecker)) return false
 
         return true
@@ -265,12 +257,12 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
                     }
             }
 
-    private fun areValueParametersEquivalent(
+    private fun areParametersEquivalent(
         overrideFun: IrSimpleFunction,
         parentFun: IrSimpleFunction,
         typeChecker: TypeCheckerState
     ): Boolean =
-        overrideFun.valueParameters.zip(parentFun.valueParameters)
+        overrideFun.nonDispatchParameters.zip(parentFun.nonDispatchParameters)
             .all { (valueParameter1, valueParameter2) ->
                 AbstractTypeChecker.equalTypes(typeChecker, valueParameter1.type, valueParameter2.type)
             }
@@ -295,6 +287,7 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
     ): IrValueParameter {
         val parameter = this
         return buildValueParameter(target) {
+            kind = parameter.kind
             origin = IrDeclarationOrigin.IR_BUILTINS_STUB
             name = parameter.name
             type = substituteType(parameter.type)
@@ -411,6 +404,6 @@ fun createThrowingStubBody(context: JvmBackendContext, function: IrSimpleFunctio
         // Function body consist only of throwing UnsupportedOperationException statement
         +irCall(context.symbols.throwUnsupportedOperationException)
             .apply {
-                putValueArgument(0, irString("Operation is not supported for read-only collection"))
+                arguments[0] = irString("Operation is not supported for read-only collection")
             }
     }

commit 6338f8bff2c6d5b79079035d8f54c583cd8d6722

[JVM] Fix `CollectionStubMethodLowering` after moving to new param API
Filtering out remove(key, value) method was broken by 40d931f3.

compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/CollectionStubMethodLowering.kt
@@ -118,9 +118,9 @@ internal class CollectionStubMethodLowering(val context: JvmBackendContext) : Cl
                     if (context.config.languageVersionSettings.getFlag(AnalysisFlags.stdlibCompilation)
                         && irClass.classId == StandardClassIds.AbstractMap
                         && irClass.typeParameters.size == 2
-                        && stub.hasShape(regularParameters = 2)
-                        && stub.parameters[0].type.classifierOrNull == irClass.typeParameters[0].symbol
-                        && stub.parameters[1].type.classifierOrNull == irClass.typeParameters[1].symbol
+                        && stub.hasShape(dispatchReceiver = true, regularParameters = 2)
+                        && stub.parameters[1].type.classifierOrNull == irClass.typeParameters[0].symbol
+                        && stub.parameters[2].type.classifierOrNull == irClass.typeParameters[1].symbol
                     ) {
                         continue
                     }