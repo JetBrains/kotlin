70ab806e3f2b7c6f41637876d69ca979326f548c
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2025-03-17 14:56:01 +0200

    [KDF] Fix test

--- plugins/kotlin-dataframe/testData/box/extractDataSchemaWithTypeParameter.kt
+++ plugins/kotlin-dataframe/testData/box/extractDataSchemaWithTypeParameter.kt
@@ -8,7 +8,7 @@ data class Record(val id: String, val b: Int)
 
 fun box(): String {
     val df = dataFrameOf(Record("1", 1), Record("2", 123), Record("3", 321))
-    val df1 = listOf("1", "2", "3").toDataFrame().join(df) { value match right.id }
+    val df1 = listOf("1", "2", "3").toDataFrame { "value" from { it } }.join(df) { value match right.id }
     val col: DataColumn<String> = df1.value
     return "OK"
 }
