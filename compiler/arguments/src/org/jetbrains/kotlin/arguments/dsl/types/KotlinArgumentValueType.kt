d4d19acf7d2953b386300357b6e3fc0acf0ede06
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-03-18 18:52:41 +0100

    [Args] Add KotlinVersionType type
    ^KT-75812 In Progress

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinArgumentValueType.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinArgumentValueType.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.arguments.dsl.types
 
 import kotlinx.serialization.Serializable
+import org.jetbrains.kotlin.arguments.dsl.base.KotlinReleaseVersion
 import org.jetbrains.kotlin.arguments.dsl.base.ReleaseDependent
 
 sealed interface KotlinArgumentValueType<T : Any> {
@@ -27,3 +28,19 @@ class BooleanType(
         return value?.toString()
     }
 }
+
+@Serializable
+class KotlinVersionType(
+    override val isNullable: ReleaseDependent<Boolean> = ReleaseDependent(true),
+    override val defaultValue: ReleaseDependent<KotlinVersion?> = ReleaseDependent(
+        KotlinVersion.v2_0,
+        KotlinReleaseVersion.v1_4_0..KotlinReleaseVersion.v1_9_20 to KotlinVersion.v1_9
+    )
+) : KotlinArgumentValueType<KotlinVersion> {
+    override fun stringRepresentation(value: KotlinVersion?): String? {
+        return value?.versionName?.valueOrNullStringLiteral
+    }
+}
+
+private val String?.valueOrNullStringLiteral: String
+    get() = "\"${this}\""
