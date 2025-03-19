91a269d8c4896b0e8ded75a295e31de9e904b58e
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2025-03-19 10:27:24 +0100

    Revert "[K/N] Set enableLocalObjects=false by default due to KT-75861"
    This reverts commit a6e23f97fb435d592b23dabaf1146b145e7cc30f.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
@@ -291,7 +291,7 @@ class KonanConfig(val project: Project, val configuration: CompilerConfiguration
     }
 
     val enableLocalObjects: Boolean by lazy {
-        configuration.get(BinaryOptions.enableLocalObjects) == true
+        configuration.get(BinaryOptions.enableLocalObjects) != false
     }
 
     val enableDebugTransparentStepping: Boolean
