a2fea2b13a0f41a688e9fb661476c5e0842c3b32
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-03-19 10:52:35 +0100

    [Tests] Add more tests for EXTENSION_SHADOWED_BY_MEMBER
    #KT-75169

--- compiler/testData/diagnostics/tests/extensionShadowedByMember.kt
+++ compiler/testData/diagnostics/tests/extensionShadowedByMember.kt
@@ -1,15 +1,16 @@
 // RUN_PIPELINE_TILL: BACKEND
 // FIR_IDENTICAL
-// ISSUE: KT-54483
-// RENDER_DIAGNOSTICS_FULL_TEXT
+// ISSUE: KT-54483, KT-75169
 // WITH_STDLIB
 
 abstract class Cache {
-    abstract fun <T> getTyped(key: Any, klass: Class<T>): T
-
-    fun get(key: Any): Int = 10
+    fun get(): Int = 10
+    fun get2(): Int = 10
+    fun <T> get3(): Int = 10
+    fun <T : CharSequence> get4(): Int = 10
 }
 
-inline fun <reified T> Cache.<!EXTENSION_SHADOWED_BY_MEMBER!>get<!>(key: Any) = getTyped(key, T::class.java)
-
-fun Cache.getString(key: Any) = get<String>(key)
+inline fun <reified T> Cache.<!EXTENSION_SHADOWED_BY_MEMBER!>get<!>() = 10
+fun <T> Cache.<!EXTENSION_SHADOWED_BY_MEMBER!>get2<!>() = 10
+fun <T, R> Cache.<!EXTENSION_SHADOWED_BY_MEMBER!>get3<!>() = 10
+fun <T : List<String>> Cache.<!EXTENSION_SHADOWED_BY_MEMBER!>get4<!>() = 10
