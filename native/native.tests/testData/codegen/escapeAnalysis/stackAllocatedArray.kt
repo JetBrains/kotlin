d6b291703c8eaa0da275825e243ba33249236086
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2025-03-19 11:34:03 +0100

    [K/N][tests] Restore, adjust and add tests for KT-69731
    Previous commits revert KT-69731 and follow-ups.
    
    This commit improves the associated test coverage:
    * It restores some of the reverted tests
    * It adjusts those and other tests that became failing after the revert
    * It adds a few new tests
    
    The affected tests are expected to help in the next attempt to fix
    KT-69731.
    
    (cherry picked from commit 96f142cf7d357f0f17a75a785234944b3f4dd4d5)

--- native/native.tests/testData/codegen/escapeAnalysis/stackAllocatedArray.kt
+++ native/native.tests/testData/codegen/escapeAnalysis/stackAllocatedArray.kt
@@ -43,25 +43,9 @@ fun stackArray2(x: Int): Int {
     return array[5].x
 }
 
-fun localArray(x: Int): Int {
-    val c1 = C(x + 42)
-    val c2 = C(x + 117)
-    val c3 = C(x - 1)
-    val allC = arrayOf(c1, c2, c3)
-    val array = Array(x) { allC[it % 3] }
-
-    assertTrue(c1.isStack())
-    assertTrue(c2.isStack())
-    assertTrue(c3.isStack())
-    assertTrue(allC.isStack())
-    assertFalse(array.isStack())
-    return array[5].x
-}
-
 fun box(): String {
     assertEquals(stackArray1(43), 42)
     assertEquals(stackArray2(43), 42)
-    assertEquals(localArray(43), 42)
 
     return "OK"
 }
