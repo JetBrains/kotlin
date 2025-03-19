rizzzav/kotlinforked

WIP - Unified symbol providers

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirJsSessionFactory.kt
@@ -14,6 +14,7 @@ import org.jetbrains.kotlin.analysis.api.projectStructure.KaSourceModule
 import org.jetbrains.kotlin.analysis.low.level.api.fir.projectStructure.LLLibrarySymbolProviderFactory
 import org.jetbrains.kotlin.analysis.low.level.api.fir.providers.LLFirModuleWithDependenciesSymbolProvider
 import org.jetbrains.kotlin.fir.SessionConfiguration
+import org.jetbrains.kotlin.fir.resolve.providers.DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.firProvider
 import org.jetbrains.kotlin.fir.session.FirJsSessionFactory.registerJsComponents
@@ -24,18 +25,18 @@ internal class LLFirJsSessionFactory(project: Project) : LLFirAbstractSessionFac
         return doCreateSourcesSession(module) { context ->
             registerJsComponents(moduleKind = null)
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        context.firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    context.firProvider.symbolProvider,
+                    context.switchableExtensionDeclarationsSymbolProvider,
+                    context.syntheticFunctionInterfaceProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -43,16 +44,16 @@ internal class LLFirJsSessionFactory(project: Project) : LLFirAbstractSessionFac
         return doCreateLibrarySession(module) { context ->
             registerJsComponents(moduleKind = null)
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOf(
-                        context.firProvider.symbolProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOf(
+                    context.firProvider.symbolProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -66,18 +67,18 @@ internal class LLFirJsSessionFactory(project: Project) : LLFirAbstractSessionFac
         return doCreateDanglingFileSession(module, contextSession) {
             registerJsComponents(moduleKind = null)
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        firProvider.symbolProvider,
-                        switchableExtensionDeclarationsSymbolProvider,
-                        syntheticFunctionInterfaceProvider,
-                    ),
-                    dependencyProvider,
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    firProvider.symbolProvider,
+                    switchableExtensionDeclarationsSymbolProvider,
+                    syntheticFunctionInterfaceProvider,
+                ),
+                computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirJvmSessionFactory.kt
@@ -22,6 +22,7 @@ import org.jetbrains.kotlin.fir.backend.jvm.FirJvmTypeMapper
 import org.jetbrains.kotlin.fir.deserialization.SingleModuleDataProvider
 import org.jetbrains.kotlin.fir.java.JavaSymbolProvider
 import org.jetbrains.kotlin.fir.java.deserialization.OptionalAnnotationClassesProvider
+import org.jetbrains.kotlin.fir.resolve.providers.DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.firProvider
 import org.jetbrains.kotlin.fir.resolve.scopes.wrapScopeWithJvmMapped
@@ -40,19 +41,19 @@ internal class LLFirJvmSessionFactory(project: Project) : LLFirAbstractSessionFa
             val javaSymbolProvider = LLFirJavaSymbolProvider(this, context.contentScope)
             register(JavaSymbolProvider::class, javaSymbolProvider)
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        context.firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        javaSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    context.firProvider.symbolProvider,
+                    context.switchableExtensionDeclarationsSymbolProvider,
+                    javaSymbolProvider,
+                    context.syntheticFunctionInterfaceProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
 
             register(FirJvmTypeMapper::class, FirJvmTypeMapper(this))
         }
@@ -62,17 +63,18 @@ internal class LLFirJvmSessionFactory(project: Project) : LLFirAbstractSessionFa
         return doCreateLibrarySession(module) { context ->
             registerJavaComponents(JavaModuleResolver.getInstance(project))
             val javaSymbolProvider = LLFirJavaSymbolProvider(this, context.contentScope)
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOf(
-                        context.firProvider.symbolProvider,
-                        javaSymbolProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOf(
+                    context.firProvider.symbolProvider,
+                    javaSymbolProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
             register(JavaSymbolProvider::class, javaSymbolProvider)
             register(FirJvmTypeMapper::class, FirJvmTypeMapper(this))
         }
@@ -94,19 +96,20 @@ internal class LLFirJvmSessionFactory(project: Project) : LLFirAbstractSessionFa
                 register(JavaSymbolProvider::class, contextJavaSymbolProvider)
             }
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        firProvider.symbolProvider,
-                        switchableExtensionDeclarationsSymbolProvider,
-                        syntheticFunctionInterfaceProvider,
-                        contextJavaSymbolProvider
-                    ),
-                    dependencyProvider
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                // We don't need to register `contextJavaSymbolProvider` here. As a context session symbol provider, it is already included
+                // in the dependency providers.
+                providers = listOfNotNull(
+                    firProvider.symbolProvider,
+                    switchableExtensionDeclarationsSymbolProvider,
+                    syntheticFunctionInterfaceProvider,
+                ),
+                computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
 
             register(FirJvmTypeMapper::class, FirJvmTypeMapper(this))
         }

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirNativeSessionFactory.kt
@@ -18,6 +18,7 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.providers.LLFirModuleWith
 import org.jetbrains.kotlin.fir.SessionConfiguration
 import org.jetbrains.kotlin.fir.deserialization.SingleModuleDataProvider
 import org.jetbrains.kotlin.fir.java.deserialization.OptionalAnnotationClassesProvider
+import org.jetbrains.kotlin.fir.resolve.providers.DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.scopes.FirOverrideChecker
 import org.jetbrains.kotlin.fir.scopes.impl.FirStandardOverrideChecker
@@ -31,18 +32,18 @@ internal class LLFirNativeSessionFactory(project: Project) : LLFirAbstractSessio
         return doCreateSourcesSession(module) { context ->
             registerNativeComponents()
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        context.firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    context.firProvider.symbolProvider,
+                    context.switchableExtensionDeclarationsSymbolProvider,
+                    context.syntheticFunctionInterfaceProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -52,16 +53,17 @@ internal class LLFirNativeSessionFactory(project: Project) : LLFirAbstractSessio
             // Resolvable library session for decompiled libraries can miss annotation arguments
             // necessary for correct work of the native overload checker
             register(FirOverrideChecker::class, FirStandardOverrideChecker(this))
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOf(
-                        context.firProvider.symbolProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOf(
+                    context.firProvider.symbolProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -75,18 +77,18 @@ internal class LLFirNativeSessionFactory(project: Project) : LLFirAbstractSessio
         return doCreateDanglingFileSession(module, contextSession) {
             registerNativeComponents()
 
-            register(
-                FirSymbolProvider::class,
-                LLFirModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        firProvider.symbolProvider,
-                        switchableExtensionDeclarationsSymbolProvider,
-                        syntheticFunctionInterfaceProvider,
-                    ),
-                    dependencyProvider,
-                )
+            val symbolProvider = LLFirModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    firProvider.symbolProvider,
+                    switchableExtensionDeclarationsSymbolProvider,
+                    syntheticFunctionInterfaceProvider,
+                ),
+                computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 

JetBrains/kotlin

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirJsSessionFactory.kt
@@ -14,6 +14,7 @@ import org.jetbrains.kotlin.analysis.api.projectStructure.KaSourceModule
 import org.jetbrains.kotlin.analysis.low.level.api.fir.symbolProviders.factories.LLLibrarySymbolProviderFactory
 import org.jetbrains.kotlin.analysis.low.level.api.fir.symbolProviders.LLModuleWithDependenciesSymbolProvider
 import org.jetbrains.kotlin.fir.SessionConfiguration
+import org.jetbrains.kotlin.fir.resolve.providers.DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.firProvider
 import org.jetbrains.kotlin.fir.session.FirJsSessionFactory.registerJsComponents
@@ -24,18 +25,18 @@ internal class LLFirJsSessionFactory(project: Project) : LLFirAbstractSessionFac
         return doCreateSourcesSession(module) { context ->
             registerJsComponents(moduleKind = null)
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        context.firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    context.firProvider.symbolProvider,
+                    context.switchableExtensionDeclarationsSymbolProvider,
+                    context.syntheticFunctionInterfaceProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -43,16 +44,16 @@ internal class LLFirJsSessionFactory(project: Project) : LLFirAbstractSessionFac
         return doCreateLibrarySession(module) { context ->
             registerJsComponents(moduleKind = null)
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOf(
-                        context.firProvider.symbolProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOf(
+                    context.firProvider.symbolProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -66,18 +67,18 @@ internal class LLFirJsSessionFactory(project: Project) : LLFirAbstractSessionFac
         return doCreateDanglingFileSession(module, contextSession) { context ->
             registerJsComponents(moduleKind = null)
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    firProvider.symbolProvider,
+                    switchableExtensionDeclarationsSymbolProvider,
+                    syntheticFunctionInterfaceProvider,
+                ),
+                computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirJvmSessionFactory.kt
@@ -22,6 +22,7 @@ import org.jetbrains.kotlin.fir.backend.jvm.FirJvmTypeMapper
 import org.jetbrains.kotlin.fir.deserialization.SingleModuleDataProvider
 import org.jetbrains.kotlin.fir.java.JavaSymbolProvider
 import org.jetbrains.kotlin.fir.java.deserialization.OptionalAnnotationClassesProvider
+import org.jetbrains.kotlin.fir.resolve.providers.DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.firProvider
 import org.jetbrains.kotlin.fir.resolve.scopes.wrapScopeWithJvmMapped
@@ -40,19 +41,19 @@ internal class LLFirJvmSessionFactory(project: Project) : LLFirAbstractSessionFa
             val javaSymbolProvider = LLFirJavaSymbolProvider(this, context.contentScope)
             register(JavaSymbolProvider::class, javaSymbolProvider)
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        context.firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        javaSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    context.firProvider.symbolProvider,
+                    context.switchableExtensionDeclarationsSymbolProvider,
+                    javaSymbolProvider,
+                    context.syntheticFunctionInterfaceProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
 
             register(FirJvmTypeMapper::class, FirJvmTypeMapper(this))
         }
@@ -62,17 +63,18 @@ internal class LLFirJvmSessionFactory(project: Project) : LLFirAbstractSessionFa
         return doCreateLibrarySession(module) { context ->
             registerJavaComponents(JavaModuleResolver.getInstance(project))
             val javaSymbolProvider = LLFirJavaSymbolProvider(this, context.contentScope)
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOf(
-                        context.firProvider.symbolProvider,
-                        javaSymbolProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOf(
+                    context.firProvider.symbolProvider,
+                    javaSymbolProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
             register(JavaSymbolProvider::class, javaSymbolProvider)
             register(FirJvmTypeMapper::class, FirJvmTypeMapper(this))
         }
@@ -94,19 +96,20 @@ internal class LLFirJvmSessionFactory(project: Project) : LLFirAbstractSessionFa
                 register(JavaSymbolProvider::class, contextJavaSymbolProvider)
             }
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                        contextJavaSymbolProvider
-                    ),
-                    context.dependencyProvider
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                // We don't need to register `contextJavaSymbolProvider` here. As a context session symbol provider, it is already included
+                // in the dependency providers.
+                providers = listOfNotNull(
+                    firProvider.symbolProvider,
+                    switchableExtensionDeclarationsSymbolProvider,
+                    syntheticFunctionInterfaceProvider,
+                ),
+                computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
 
             register(FirJvmTypeMapper::class, FirJvmTypeMapper(this))
         }

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirNativeSessionFactory.kt
@@ -18,6 +18,7 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.symbolProviders.LLModuleW
 import org.jetbrains.kotlin.fir.SessionConfiguration
 import org.jetbrains.kotlin.fir.deserialization.SingleModuleDataProvider
 import org.jetbrains.kotlin.fir.java.deserialization.OptionalAnnotationClassesProvider
+import org.jetbrains.kotlin.fir.resolve.providers.DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY
 import org.jetbrains.kotlin.fir.resolve.providers.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.providers.firProvider
 import org.jetbrains.kotlin.fir.scopes.FirOverrideChecker
@@ -32,18 +33,18 @@ internal class LLFirNativeSessionFactory(project: Project) : LLFirAbstractSessio
         return doCreateSourcesSession(module) { context ->
             registerNativeComponents()
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        context.firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    context.firProvider.symbolProvider,
+                    context.switchableExtensionDeclarationsSymbolProvider,
+                    context.syntheticFunctionInterfaceProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -53,16 +54,17 @@ internal class LLFirNativeSessionFactory(project: Project) : LLFirAbstractSessio
             // Resolvable library session for decompiled libraries can miss annotation arguments
             // necessary for correct work of the native overload checker
             register(FirOverrideChecker::class, FirStandardOverrideChecker(this))
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOf(
-                        context.firProvider.symbolProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOf(
+                    context.firProvider.symbolProvider,
+                ),
+                context.computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 
@@ -76,18 +78,18 @@ internal class LLFirNativeSessionFactory(project: Project) : LLFirAbstractSessio
         return doCreateDanglingFileSession(module, contextSession) { context ->
             registerNativeComponents()
 
-            register(
-                FirSymbolProvider::class,
-                LLModuleWithDependenciesSymbolProvider(
-                    this,
-                    providers = listOfNotNull(
-                        firProvider.symbolProvider,
-                        context.switchableExtensionDeclarationsSymbolProvider,
-                        context.syntheticFunctionInterfaceProvider,
-                    ),
-                    context.dependencyProvider,
-                )
+            val symbolProvider = LLModuleWithDependenciesSymbolProvider(
+                this,
+                providers = listOfNotNull(
+                    firProvider.symbolProvider,
+                    switchableExtensionDeclarationsSymbolProvider,
+                    syntheticFunctionInterfaceProvider,
+                ),
+                computeDependencyProviders,
             )
+
+            register(FirSymbolProvider::class, symbolProvider)
+            register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, symbolProvider.dependenciesSymbolProvider)
         }
     }
 