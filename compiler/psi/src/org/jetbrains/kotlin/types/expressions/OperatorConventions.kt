e3c5d53e14940a614b8893213f300e46026590e1
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2025-03-25 18:48:27 +0100

    [Parser] Run j2k on `kotlin/types/expressions/OperatorConventions`

--- compiler/psi/src/org/jetbrains/kotlin/types/expressions/OperatorConventions.kt
+++ compiler/psi/src/org/jetbrains/kotlin/types/expressions/OperatorConventions.kt
@@ -13,148 +13,175 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.types.expressions
 
-package org.jetbrains.kotlin.types.expressions;
+import com.google.common.collect.ImmutableBiMap
+import com.google.common.collect.ImmutableSet
+import org.jetbrains.kotlin.lexer.KtSingleValueToken
+import org.jetbrains.kotlin.lexer.KtToken
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.SpecialNames.ITERATOR
+import org.jetbrains.kotlin.util.OperatorNameConventions.AND
+import org.jetbrains.kotlin.util.OperatorNameConventions.COMPARE_TO
+import org.jetbrains.kotlin.util.OperatorNameConventions.COMPONENT_REGEX
+import org.jetbrains.kotlin.util.OperatorNameConventions.CONTAINS
+import org.jetbrains.kotlin.util.OperatorNameConventions.DEC
+import org.jetbrains.kotlin.util.OperatorNameConventions.DIV
+import org.jetbrains.kotlin.util.OperatorNameConventions.DIV_ASSIGN
+import org.jetbrains.kotlin.util.OperatorNameConventions.EQUALS
+import org.jetbrains.kotlin.util.OperatorNameConventions.GET
+import org.jetbrains.kotlin.util.OperatorNameConventions.GET_VALUE
+import org.jetbrains.kotlin.util.OperatorNameConventions.HAS_NEXT
+import org.jetbrains.kotlin.util.OperatorNameConventions.INC
+import org.jetbrains.kotlin.util.OperatorNameConventions.INVOKE
+import org.jetbrains.kotlin.util.OperatorNameConventions.MINUS
+import org.jetbrains.kotlin.util.OperatorNameConventions.MINUS_ASSIGN
+import org.jetbrains.kotlin.util.OperatorNameConventions.NEXT
+import org.jetbrains.kotlin.util.OperatorNameConventions.NOT
+import org.jetbrains.kotlin.util.OperatorNameConventions.OR
+import org.jetbrains.kotlin.util.OperatorNameConventions.PLUS
+import org.jetbrains.kotlin.util.OperatorNameConventions.PLUS_ASSIGN
+import org.jetbrains.kotlin.util.OperatorNameConventions.RANGE_TO
+import org.jetbrains.kotlin.util.OperatorNameConventions.RANGE_UNTIL
+import org.jetbrains.kotlin.util.OperatorNameConventions.REM
+import org.jetbrains.kotlin.util.OperatorNameConventions.REM_ASSIGN
+import org.jetbrains.kotlin.util.OperatorNameConventions.SET
+import org.jetbrains.kotlin.util.OperatorNameConventions.SET_VALUE
+import org.jetbrains.kotlin.util.OperatorNameConventions.TIMES
+import org.jetbrains.kotlin.util.OperatorNameConventions.TIMES_ASSIGN
+import org.jetbrains.kotlin.util.OperatorNameConventions.UNARY_MINUS
+import org.jetbrains.kotlin.util.OperatorNameConventions.UNARY_PLUS
 
-import com.google.common.collect.ImmutableBiMap;
-import com.google.common.collect.ImmutableSet;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.lexer.KtSingleValueToken;
-import org.jetbrains.kotlin.lexer.KtToken;
-import org.jetbrains.kotlin.lexer.KtTokens;
-import org.jetbrains.kotlin.name.Name;
-
-import static org.jetbrains.kotlin.util.OperatorNameConventions.*;
-
-public class OperatorConventions {
-
-    private OperatorConventions() {}
-
+object OperatorConventions {
     // Names for primitive type conversion properties
-    public static final Name DOUBLE = Name.identifier("toDouble");
-    public static final Name FLOAT = Name.identifier("toFloat");
-    public static final Name LONG = Name.identifier("toLong");
-    public static final Name INT = Name.identifier("toInt");
-    public static final Name CHAR = Name.identifier("toChar");
-    public static final Name SHORT = Name.identifier("toShort");
-    public static final Name BYTE = Name.identifier("toByte");
+    val DOUBLE: Name = Name.identifier("toDouble")
+    val FLOAT: Name = Name.identifier("toFloat")
+    val LONG: Name = Name.identifier("toLong")
+    val INT: Name = Name.identifier("toInt")
+    val CHAR: Name = Name.identifier("toChar")
+    val SHORT: Name = Name.identifier("toShort")
+    val BYTE: Name = Name.identifier("toByte")
 
 
-    public static final ImmutableSet<Name> NUMBER_CONVERSIONS = ImmutableSet.of(
-            DOUBLE, FLOAT, LONG, INT, SHORT, BYTE, CHAR
-    );
+    val NUMBER_CONVERSIONS: ImmutableSet<Name> = ImmutableSet.of<Name>(
+        DOUBLE, FLOAT, LONG, INT, SHORT, BYTE, CHAR
+    )
 
     // If you add new unary, binary or assignment operators, add it to OperatorConventionNames as well
+    @JvmField
+    val UNARY_OPERATION_NAMES: ImmutableBiMap<KtSingleValueToken, Name> = ImmutableBiMap.builder<KtSingleValueToken, Name>()
+        .put(KtTokens.PLUSPLUS, INC)
+        .put(KtTokens.MINUSMINUS, DEC)
+        .put(KtTokens.PLUS, UNARY_PLUS)
+        .put(KtTokens.MINUS, UNARY_MINUS)
+        .put(KtTokens.EXCL, NOT)
+        .build()
 
-    public static final ImmutableBiMap<KtSingleValueToken, Name> UNARY_OPERATION_NAMES = ImmutableBiMap.<KtSingleValueToken, Name>builder()
-            .put(KtTokens.PLUSPLUS, INC)
-            .put(KtTokens.MINUSMINUS, DEC)
-            .put(KtTokens.PLUS, UNARY_PLUS)
-            .put(KtTokens.MINUS, UNARY_MINUS)
-            .put(KtTokens.EXCL, NOT)
-            .build();
+    @JvmField
+    val BINARY_OPERATION_NAMES: ImmutableBiMap<KtSingleValueToken, Name> = ImmutableBiMap.builder<KtSingleValueToken, Name>()
+        .put(KtTokens.MUL, TIMES)
+        .put(KtTokens.PLUS, PLUS)
+        .put(KtTokens.MINUS, MINUS)
+        .put(KtTokens.DIV, DIV)
+        .put(KtTokens.PERC, REM)
+        .put(KtTokens.RANGE, RANGE_TO)
+        .put(KtTokens.RANGE_UNTIL, RANGE_UNTIL)
+        .build()
 
-    public static final ImmutableBiMap<KtSingleValueToken, Name> BINARY_OPERATION_NAMES = ImmutableBiMap.<KtSingleValueToken, Name>builder()
-            .put(KtTokens.MUL, TIMES)
-            .put(KtTokens.PLUS, PLUS)
-            .put(KtTokens.MINUS, MINUS)
-            .put(KtTokens.DIV, DIV)
-            .put(KtTokens.PERC, REM)
-            .put(KtTokens.RANGE, RANGE_TO)
-            .put(KtTokens.RANGE_UNTIL, RANGE_UNTIL)
-            .build();
+    val NOT_OVERLOADABLE: ImmutableSet<KtSingleValueToken> =
+        ImmutableSet.of(KtTokens.ANDAND, KtTokens.OROR, KtTokens.ELVIS, KtTokens.EQEQEQ, KtTokens.EXCLEQEQEQ)
 
-    public static final ImmutableSet<KtSingleValueToken> NOT_OVERLOADABLE =
-            ImmutableSet.of(KtTokens.ANDAND, KtTokens.OROR, KtTokens.ELVIS, KtTokens.EQEQEQ, KtTokens.EXCLEQEQEQ);
-    
-    public static final ImmutableSet<KtSingleValueToken> INCREMENT_OPERATIONS =
-            ImmutableSet.of(KtTokens.PLUSPLUS, KtTokens.MINUSMINUS);
+    val INCREMENT_OPERATIONS: ImmutableSet<KtSingleValueToken> =
+        ImmutableSet.of(KtTokens.PLUSPLUS, KtTokens.MINUSMINUS)
 
-    public static final ImmutableSet<KtSingleValueToken> COMPARISON_OPERATIONS =
-            ImmutableSet.of(KtTokens.LT, KtTokens.GT, KtTokens.LTEQ, KtTokens.GTEQ);
+    @JvmField
+    val COMPARISON_OPERATIONS: ImmutableSet<KtSingleValueToken> =
+        ImmutableSet.of(KtTokens.LT, KtTokens.GT, KtTokens.LTEQ, KtTokens.GTEQ)
 
-    public static final ImmutableSet<KtSingleValueToken> EQUALS_OPERATIONS =
-            ImmutableSet.of(KtTokens.EQEQ, KtTokens.EXCLEQ);
+    @JvmField
+    val EQUALS_OPERATIONS: ImmutableSet<KtSingleValueToken> = ImmutableSet.of(KtTokens.EQEQ, KtTokens.EXCLEQ)
 
-    public static final ImmutableSet<KtSingleValueToken> IDENTITY_EQUALS_OPERATIONS =
-            ImmutableSet.of(KtTokens.EQEQEQ, KtTokens.EXCLEQEQEQ);
+    @JvmField
+    val IDENTITY_EQUALS_OPERATIONS: ImmutableSet<KtSingleValueToken> =
+        ImmutableSet.of<KtSingleValueToken>(KtTokens.EQEQEQ, KtTokens.EXCLEQEQEQ)
 
-    public static final ImmutableSet<KtSingleValueToken> IN_OPERATIONS =
-            ImmutableSet.of(KtTokens.IN_KEYWORD, KtTokens.NOT_IN);
+    @JvmField
+    val IN_OPERATIONS: ImmutableSet<KtSingleValueToken> = ImmutableSet.of(KtTokens.IN_KEYWORD, KtTokens.NOT_IN)
 
-    public static final ImmutableBiMap<KtSingleValueToken, Name> ASSIGNMENT_OPERATIONS = ImmutableBiMap.<KtSingleValueToken, Name>builder()
-            .put(KtTokens.MULTEQ, TIMES_ASSIGN)
-            .put(KtTokens.DIVEQ, DIV_ASSIGN)
-            .put(KtTokens.PERCEQ, REM_ASSIGN)
-            .put(KtTokens.PLUSEQ, PLUS_ASSIGN)
-            .put(KtTokens.MINUSEQ, MINUS_ASSIGN)
-            .build();
+    @JvmField
+    val ASSIGNMENT_OPERATIONS: ImmutableBiMap<KtSingleValueToken, Name> = ImmutableBiMap.builder<KtSingleValueToken, Name>()
+        .put(KtTokens.MULTEQ, TIMES_ASSIGN)
+        .put(KtTokens.DIVEQ, DIV_ASSIGN)
+        .put(KtTokens.PERCEQ, REM_ASSIGN)
+        .put(KtTokens.PLUSEQ, PLUS_ASSIGN)
+        .put(KtTokens.MINUSEQ, MINUS_ASSIGN)
+        .build()
 
-    public static final ImmutableBiMap<KtSingleValueToken, KtSingleValueToken> ASSIGNMENT_OPERATION_COUNTERPARTS = ImmutableBiMap.<KtSingleValueToken, KtSingleValueToken>builder()
+    @JvmField
+    val ASSIGNMENT_OPERATION_COUNTERPARTS: ImmutableBiMap<KtSingleValueToken, KtSingleValueToken> =
+        ImmutableBiMap.builder<KtSingleValueToken, KtSingleValueToken>()
             .put(KtTokens.MULTEQ, KtTokens.MUL)
             .put(KtTokens.DIVEQ, KtTokens.DIV)
             .put(KtTokens.PERCEQ, KtTokens.PERC)
             .put(KtTokens.PLUSEQ, KtTokens.PLUS)
             .put(KtTokens.MINUSEQ, KtTokens.MINUS)
-            .build();
+            .build()
 
-    public static final Name ASSIGN_METHOD = Name.identifier("assign");
+    val ASSIGN_METHOD: Name = Name.identifier("assign")
 
-    public static final ImmutableBiMap<KtSingleValueToken, Name> BOOLEAN_OPERATIONS = ImmutableBiMap.<KtSingleValueToken, Name>builder()
-             .put(KtTokens.ANDAND, AND)
-             .put(KtTokens.OROR, OR)
-             .build();
+    @JvmField
+    val BOOLEAN_OPERATIONS: ImmutableBiMap<KtSingleValueToken, Name> = ImmutableBiMap.builder<KtSingleValueToken, Name>()
+        .put(KtTokens.ANDAND, AND)
+        .put(KtTokens.OROR, OR)
+        .build()
 
-    public static final ImmutableSet<Name> CONVENTION_NAMES = ImmutableSet.<Name>builder()
-            .add(GET, SET, INVOKE, CONTAINS, ITERATOR, NEXT, HAS_NEXT, EQUALS, COMPARE_TO, GET_VALUE, SET_VALUE)
-            .addAll(UNARY_OPERATION_NAMES.values())
-            .addAll(BINARY_OPERATION_NAMES.values())
-            .addAll(ASSIGNMENT_OPERATIONS.values())
-            .build();
+    val CONVENTION_NAMES: ImmutableSet<Name> = ImmutableSet.builder<Name>()
+        .add(GET, SET, INVOKE, CONTAINS, ITERATOR, NEXT, HAS_NEXT, EQUALS, COMPARE_TO, GET_VALUE, SET_VALUE)
+        .addAll(UNARY_OPERATION_NAMES.values)
+        .addAll(BINARY_OPERATION_NAMES.values)
+        .addAll(ASSIGNMENT_OPERATIONS.values)
+        .build()
 
-    @Nullable
-    public static Name getNameForOperationSymbol(@NotNull KtToken token) {
-        return getNameForOperationSymbol(token, true, true);
+    @JvmStatic
+    fun getNameForOperationSymbol(token: KtToken): Name? {
+        return getNameForOperationSymbol(token, unaryOperations = true, binaryOperations = true)
     }
 
-    @Nullable
-    public static Name getNameForOperationSymbol(@NotNull KtToken token, boolean unaryOperations, boolean binaryOperations) {
-        Name name;
+    fun getNameForOperationSymbol(token: KtToken, unaryOperations: Boolean, binaryOperations: Boolean): Name? {
+        var name: Name?
 
         if (binaryOperations) {
-            name = BINARY_OPERATION_NAMES.get(token);
-            if (name != null) return name;
+            name = BINARY_OPERATION_NAMES.get(token)
+            if (name != null) return name
         }
 
         if (unaryOperations) {
-            name = UNARY_OPERATION_NAMES.get(token);
-            if (name != null) return name;
+            name = UNARY_OPERATION_NAMES.get(token)
+            if (name != null) return name
         }
 
-        name = ASSIGNMENT_OPERATIONS.get(token);
-        if (name != null) return name;
-        if (COMPARISON_OPERATIONS.contains(token)) return COMPARE_TO;
-        if (EQUALS_OPERATIONS.contains(token)) return EQUALS;
-        if (IN_OPERATIONS.contains(token)) return CONTAINS;
-        return null;
+        name = ASSIGNMENT_OPERATIONS.get(token)
+        if (name != null) return name
+        if (COMPARISON_OPERATIONS.contains(token)) return COMPARE_TO
+        if (EQUALS_OPERATIONS.contains(token)) return EQUALS
+        if (IN_OPERATIONS.contains(token)) return CONTAINS
+        return null
     }
 
-    @Nullable
-    public static KtToken getOperationSymbolForName(@NotNull Name name) {
-        if (!isConventionName(name)) return null;
-
-        KtToken token;
-        token = BINARY_OPERATION_NAMES.inverse().get(name);
-        if (token != null) return token;
-        token = UNARY_OPERATION_NAMES.inverse().get(name);
-        if (token != null) return token;
-        token = ASSIGNMENT_OPERATIONS.inverse().get(name);
-        if (token != null) return token;
-        return null;
+    fun getOperationSymbolForName(name: Name): KtToken? {
+        if (!isConventionName(name)) return null
+        var token = BINARY_OPERATION_NAMES.inverse().get(name)
+        if (token != null) return token
+        token = UNARY_OPERATION_NAMES.inverse().get(name)
+        if (token != null) return token
+        token = ASSIGNMENT_OPERATIONS.inverse().get(name)
+        if (token != null) return token
+        return null
     }
 
-    public static boolean isConventionName(@NotNull Name name) {
-        return CONVENTION_NAMES.contains(name) || COMPONENT_REGEX.matches(name.asString());
+    @JvmStatic
+    fun isConventionName(name: Name): Boolean {
+        return CONVENTION_NAMES.contains(name) || COMPONENT_REGEX.matches(name.asString())
     }
 }
