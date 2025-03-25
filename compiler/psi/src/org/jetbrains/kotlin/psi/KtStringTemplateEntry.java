2c77e622978d4d877d4ad7bdce471835b4ae20fd
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-03-25 23:49:13 +0100

    [psi] KtStringTemplateEntry: compute expression by stubs if possible
    `PsiRawFirBuilder` uses `KtStringTemplateEntryWithExpression.getExpressions`
    which delegates to `KtStringTemplateEntry.getExpression` when it is
    possible. So, effectively this change allows building annotation
    arguments for syntactically valid code directly on top of stubs
    
    ^KT-71787

--- compiler/psi/src/org/jetbrains/kotlin/psi/KtStringTemplateEntry.java
+++ compiler/psi/src/org/jetbrains/kotlin/psi/KtStringTemplateEntry.java
@@ -1,26 +1,20 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
+import com.intellij.psi.stubs.StubElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.psi.stubs.KotlinPlaceHolderStub;
 import org.jetbrains.kotlin.psi.stubs.KotlinPlaceHolderWithTextStub;
+
+import java.util.List;
 
 public abstract class KtStringTemplateEntry extends KtElementImplStub<KotlinPlaceHolderWithTextStub<? extends KtStringTemplateEntry>> {
     public static final KtStringTemplateEntry[] EMPTY_ARRAY = new KtStringTemplateEntry[0];
@@ -36,8 +30,26 @@ public abstract class KtStringTemplateEntry extends KtElementImplStub<KotlinPlac
         super(stub, elementType);
     }
 
+    /**
+     * Retrieves the child expression if available.
+     *
+     * @return the child {@link KtExpression} or null if no expression is found
+     */
     @Nullable
     public KtExpression getExpression() {
+        // not green stub is used on purpose as it should be more performant
+        // to search directly via ast if possible
+        KotlinPlaceHolderStub<?> stub = getStub();
+        if (stub != null) {
+            List<StubElement> childrenStubs = stub.getChildrenStubs();
+            for (StubElement<?> element : childrenStubs) {
+                PsiElement psiElement = element.getPsi();
+                if (psiElement instanceof KtExpression) {
+                    return (KtExpression) psiElement;
+                }
+            }
+        }
+
         return findChildByClass(KtExpression.class);
     }
 
