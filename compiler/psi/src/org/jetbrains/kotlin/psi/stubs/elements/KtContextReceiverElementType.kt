f4434c2b0f7703661103380fe7b701f93086c7f7
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-03-25 23:26:43 +0100

    [psi] KotlinContextReceiverStubImpl: prefer `StringRef`
    ^KT-71787

--- compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtContextReceiverElementType.kt
+++ compiler/psi/src/org/jetbrains/kotlin/psi/stubs/elements/KtContextReceiverElementType.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.psi.stubs.elements
 import com.intellij.psi.stubs.StubElement
 import com.intellij.psi.stubs.StubInputStream
 import com.intellij.psi.stubs.StubOutputStream
+import com.intellij.util.io.StringRef
 import org.jetbrains.kotlin.psi.KtContextReceiver
 import org.jetbrains.kotlin.psi.stubs.KotlinContextReceiverStub
 import org.jetbrains.kotlin.psi.stubs.impl.KotlinContextReceiverStubImpl
@@ -20,11 +21,11 @@ class KtContextReceiverElementType(debugName: String) : KtStubElementType<Kotlin
     override fun createStub(
         element: KtContextReceiver,
         parentStub: StubElement<*>?
-    ): KotlinContextReceiverStub = KotlinContextReceiverStubImpl(parentStub, this, element.labelName())
+    ): KotlinContextReceiverStub = KotlinContextReceiverStubImpl(parentStub, this, StringRef.fromString(element.labelName()))
 
     override fun serialize(stub: KotlinContextReceiverStub, dataStream: StubOutputStream) =
         dataStream.writeName(stub.getLabel())
 
     override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>?) =
-        KotlinContextReceiverStubImpl(parentStub, this, dataStream.readNameString())
+        KotlinContextReceiverStubImpl(parentStub, this, dataStream.readName())
 }
--- compiler/psi/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinContextReceiverStubImpl.kt
+++ compiler/psi/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinContextReceiverStubImpl.kt
@@ -1,12 +1,13 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.stubs.StubElement
 import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.StubElement
+import com.intellij.util.io.StringRef
 import org.jetbrains.kotlin.psi.KtContextReceiver
 import org.jetbrains.kotlin.psi.stubs.KotlinContextReceiverStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtContextReceiverElementType
@@ -14,7 +15,7 @@ import org.jetbrains.kotlin.psi.stubs.elements.KtContextReceiverElementType
 class KotlinContextReceiverStubImpl(
     parent: StubElement<out PsiElement>?,
     elementType: KtContextReceiverElementType,
-    private val label: String?,
+    private val label: StringRef?,
 ) : KotlinStubBaseImpl<KtContextReceiver>(parent, elementType), KotlinContextReceiverStub {
-    override fun getLabel() = label
+    override fun getLabel(): String? = label?.string
 }
