94088d67015feddeedc1e0a12fa49a2a4bc88848
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-03-25 18:24:22 +0100

    Reflection: use kotlin-metadata-jvm for KClass.sealedSubclasses
     #KT-75505

--- compiler/testData/codegen/boxModernJdk/testsWithJava17/sealed/sealedJavaInterfaceViaKotlinReflection.kt
+++ compiler/testData/codegen/boxModernJdk/testsWithJava17/sealed/sealedJavaInterfaceViaKotlinReflection.kt
@@ -14,7 +14,7 @@ fun box(): String {
     val klass = Base::class
     if (!klass.isSealed) return "Error: Base is not sealed"
     if (klass.isAbstract) return "Error: Base is abstract"
-    return klass.sealedSubclasses.asReversed()
+    return klass.sealedSubclasses
         .joinToString("") { it.simpleName ?: "_No name provided_" }
         .takeIf { it.isNotBlank() }
         ?: "_No sealed subclasses found_"
