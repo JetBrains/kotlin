db570f10043029f9762752b02c14ea0de3634598
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2025-03-27 16:16:56 +0100

    fixup! Stdlib: Add @JvmExposeBoxed and @BoxingConstructorMarker annotations

--- libraries/stdlib/jvm/runtime/kotlin/jvm/annotations/JvmPlatformAnnotations.kt
+++ libraries/stdlib/jvm/runtime/kotlin/jvm/annotations/JvmPlatformAnnotations.kt
@@ -194,19 +194,24 @@ public actual annotation class JvmInline
 public actual annotation class JvmRecord
 
 /**
- * Instructs the compiler
- * to expose the API related to the annotated [kotlin.jvm.JvmInline] value classes as its boxed variant for effective usage from Java.
+ * This annotation instructs the compiler to expose the API of functions with inline classes
+ * (and the classes containing them, including inline classes themselves)
+ * as their boxed variant for effective usage from Java.
  *
- * Particularly, it performs the following transformations:
+ * It performs the following transformations:
  *
- * - For functions and constructors that take or return inline classes,
- * a wrapper declaration for Java is created where inline classes are boxed.
- * - Functions declared in the marked class can be called normally from Java.
- * - Constructor available from Java is added.
+ * - For annotated functions and constructors that take or return inline classes,
+ *   an unmangled wrapper function is created where inline classes are boxed.
+ *   The wrapper is thus visible and callable from Java.
  *
- * These additions preserve backwards compatibility (both binary and source), so existing inline classes can be marked safely.
+ * - If class is annotated, the annotation implicitly propagates to its methods, forcing
+ *   the compiler to generate wrappers for them.
  *
- * @property jvmName - optional wrapper name. Only applicable to functions, getters and setters.
+ * - A constructor is made available from Java.
+ *
+ * These changes maintain backward compatibility, allowing existing API to be safely marked.
+ *
+ * @property jvmName optional wrapper name. Only applicable to functions, getters and setters.
  */
 @Retention(AnnotationRetention.BINARY)
 @MustBeDocumented
