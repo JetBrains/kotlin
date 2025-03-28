d954861c1df2af36d3c423e899371491134f721a
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-03-28 17:18:37 +0400

    [AA] Add a test for an obsolete issue
    Fixed by KT-76339.
    
    ^KT-76297 Obsolete

--- analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/typeProvider/FirIdeNormalAnalysisSourceModuleTypeReferenceTestGenerated.java
+++ analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/typeProvider/FirIdeNormalAnalysisSourceModuleTypeReferenceTestGenerated.java
@@ -106,6 +106,12 @@ public class FirIdeNormalAnalysisSourceModuleTypeReferenceTestGenerated extends 
   }
 
   @Test
+  @TestMetadata("danglingAnnotationInsideEnumEntry.kt")
+  public void testDanglingAnnotationInsideEnumEntry() {
+    runTest("analysis/analysis-api/testData/components/typeProvider/typeReference/danglingAnnotationInsideEnumEntry.kt");
+  }
+
+  @Test
   @TestMetadata("functionReceiver.kt")
   public void testFunctionReceiver() {
     runTest("analysis/analysis-api/testData/components/typeProvider/typeReference/functionReceiver.kt");
