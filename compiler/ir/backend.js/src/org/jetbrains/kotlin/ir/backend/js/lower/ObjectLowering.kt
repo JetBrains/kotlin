3977ba3ffabfbfb4dde5b46d162e81d9570f68cd
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-04-02 20:34:17 +0200

    [JS] Migrate enumClassToInitEntryInstancesFun to IR attribute

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/ObjectLowering.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/ObjectLowering.kt
@@ -16,6 +16,7 @@ import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
 import org.jetbrains.kotlin.ir.backend.js.JsCommonBackendContext
 import org.jetbrains.kotlin.ir.backend.js.JsIrBackendContext
 import org.jetbrains.kotlin.ir.backend.js.JsLoweredDeclarationOrigin
+import org.jetbrains.kotlin.ir.backend.js.initEntryInstancesFun
 import org.jetbrains.kotlin.ir.backend.js.ir.JsIrBuilder
 import org.jetbrains.kotlin.ir.backend.js.objectGetInstanceFunction
 import org.jetbrains.kotlin.ir.backend.js.objectInstanceField
@@ -38,10 +39,6 @@ import org.jetbrains.kotlin.utils.addToStdlib.safeAs
  * Creates lazy object instance generator functions.
  */
 class ObjectDeclarationLowering(val context: JsCommonBackendContext) : DeclarationTransformer {
-    /**
-     * If the object being lowered is nested inside an enum class, we want to also initialize the enum entries when initializing the object.
-     */
-    private var IrClass.initEntryInstancesFun: IrSimpleFunction? by context.mapping.enumClassToInitEntryInstancesFun
     override fun transformFlat(declaration: IrDeclaration): List<IrDeclaration>? {
         if (declaration !is IrClass || declaration.kind != ClassKind.OBJECT || declaration.isEffectivelyExternal())
             return null
