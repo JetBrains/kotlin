03a4ff61ddd671f1cb6c49f47aeb89f80f9948a2
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-04-03 14:33:01 +0300

    [FIR] Don't report `CONFLICTING_UPPER_BOUNDS` for `Nothing` bounds
    The diagnostic was introduced in 160edaea78c2c6f718176c5828f5673dd75807a9
    because the context behind it was lost :( Unfortunately, the original
    ticket suggesting not to flag such bounds (^KT-57274) was marked as
    fixed in K2 without having added a corresponding diagnostic test to the
    compiler, which would otherwise let us restore the context and prevent
    the unnecessary change.
    
    ^KT-70352
    ^KT-76209 Fixed

--- compiler/testData/diagnostics/tests/nothingBound.kt
+++ compiler/testData/diagnostics/tests/nothingBound.kt
@@ -1,4 +1,6 @@
-// RUN_PIPELINE_TILL: FRONTEND
-// ISSUE: KT-70352
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-57274, KT-70352, KT-76209
 
+// CONFLICTING_UPPER_BOUND should not be reported as having
+// Nothing bounds is useful for some scenarios.
 fun <<!CONFLICTING_UPPER_BOUNDS!>T<!>: <!FINAL_UPPER_BOUND!>Nothing<!>> f() {}
