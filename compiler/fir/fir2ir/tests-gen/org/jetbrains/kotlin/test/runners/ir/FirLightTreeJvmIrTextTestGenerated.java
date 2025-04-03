a7240a251f95c1ea020ffc2fbb9460657e30d2e9
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-04-03 11:25:29 +0200

    [IR.Actualizer] Deep copy annotation calls to lenient stubs
    #KT-75300

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/FirLightTreeJvmIrTextTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/ir/FirLightTreeJvmIrTextTestGenerated.java
@@ -4557,6 +4557,12 @@ public class FirLightTreeJvmIrTextTestGenerated extends AbstractFirLightTreeJvmI
     }
 
     @Test
+    @TestMetadata("annotations.kt")
+    public void testAnnotations() {
+      runTest("compiler/testData/ir/irText/lenientMode/annotations.kt");
+    }
+
+    @Test
     @TestMetadata("lenientModeStubs.kt")
     public void testLenientModeStubs() {
       runTest("compiler/testData/ir/irText/lenientMode/lenientModeStubs.kt");
