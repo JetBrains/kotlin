5321fddae0781458be4c7a1863fe5d133e06d6a5
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-04-04 13:45:42 +0200

    [Inline] Remove any private inlined symbols from the copy of inline function
    A copy of the inlined function is stored separately in Klib.
    None of the private symbols should appear inside this copy.
    
    #KT-76181

--- compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/InlineFunctionSerializationPreProcessing.kt
+++ compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/InlineFunctionSerializationPreProcessing.kt
@@ -9,15 +9,14 @@ import org.jetbrains.kotlin.backend.common.FileLoweringPass
 import org.jetbrains.kotlin.backend.common.LoweringContext
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.declarations.IrParameterKind
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.expressions.IrInlinedFunctionBlock
 import org.jetbrains.kotlin.ir.types.extractTypeParameters
 import org.jetbrains.kotlin.ir.util.erasedTopLevelCopy
 import org.jetbrains.kotlin.ir.util.file
 import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 
-@Suppress("unused")
 class InlineFunctionSerializationPreProcessing(private val context: LoweringContext) : IrVisitorVoid(), FileLoweringPass {
     override fun lower(irFile: IrFile) {
         irFile.accept(this, null)
@@ -29,12 +28,12 @@ class InlineFunctionSerializationPreProcessing(private val context: LoweringCont
 
     override fun visitSimpleFunction(declaration: IrSimpleFunction) {
         if (!declaration.isInline || declaration.body == null || declaration.symbol.isConsideredAsPrivateForInlining()) return
-        declaration.erasedTopLevelCopy = declaration.copyAndEraseTypeParameters().convertToTopLevel()
+        declaration.erasedTopLevelCopy = declaration.copyAndEraseTypeParameters().convertToTopLevel().erasePrivateSymbols()
     }
 
     private fun IrSimpleFunction.copyAndEraseTypeParameters(): IrSimpleFunction {
         val typeArguments = extractTypeParameters(this)
-            .associate { it.symbol to (if (it.isReified) null else context.irBuiltIns.anyNType) }
+            .associate { it.symbol to (if (it.isReified) null else context.irBuiltIns.anyNType) } // TODO clean up in KT-76454
         return InlineFunctionBodyPreprocessor(typeArguments, CallInlinerStrategy.DEFAULT)
             .preprocess(this) as IrSimpleFunction
     }
@@ -44,5 +43,25 @@ class InlineFunctionSerializationPreProcessing(private val context: LoweringCont
         parent = file
 
         return this
+    }
+
+    private fun IrSimpleFunction.erasePrivateSymbols(): IrSimpleFunction {
+        object : IrVisitorVoid() {
+            override fun visitElement(element: IrElement) {
+                element.acceptChildrenVoid(this)
+            }
+
+            override fun visitInlinedFunctionBlock(inlinedBlock: IrInlinedFunctionBlock) {
+                inlinedBlock.acceptChildrenVoid(this)
+                if (inlinedBlock.isEffectivelyPrivate()) {
+                    inlinedBlock.inlinedFunctionSymbol = null
+                }
+            }
+        }.visitElement(this)
+        return this
+    }
+
+    private fun IrInlinedFunctionBlock.isEffectivelyPrivate(): Boolean {
+        return inlinedFunctionSymbol?.isConsideredAsPrivateForInlining() == true
     }
 }
