13cc839e351a56c4f56bf333aec7787b4226a16c
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2025-04-04 11:17:05 +0200

    [Gradle] Fix failing testCompileSharedNativeSourceSetWithOKIODependency

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-54995-compileSharedNative-with-okio/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-54995-compileSharedNative-with-okio/build.gradle.kts
@@ -15,7 +15,7 @@ kotlin {
     sourceSets {
         val commonMain = getByName("commonMain") {
             dependencies {
-                implementation("com.squareup.okio:okio:3.2.0")
+                implementation("com.squareup.okio:okio:3.10.2")
             }
         }
 
