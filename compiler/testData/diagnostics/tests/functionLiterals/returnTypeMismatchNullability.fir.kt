52ddc7eff0afa40dc4cb28446c23c58b2aad2675
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-04-07 10:54:47 +0200

    [FIR] Fix nullability of type mismatch types
    #KT-76192 Fixed

--- compiler/testData/diagnostics/tests/functionLiterals/returnTypeMismatchNullability.fir.kt
+++ compiler/testData/diagnostics/tests/functionLiterals/returnTypeMismatchNullability.fir.kt
@@ -2,5 +2,5 @@
 // WITH_STDLIB
 // RENDER_DIAGNOSTICS_MESSAGES
 fun test(a: List<Int?>) {
-    val b: List<Int> = a.map { <!RETURN_TYPE_MISMATCH("Int; Int")!>it?.let { c -> c }<!> }
+    val b: List<Int> = a.map { <!RETURN_TYPE_MISMATCH("Int; Int?")!>it?.let { c -> c }<!> }
 }
