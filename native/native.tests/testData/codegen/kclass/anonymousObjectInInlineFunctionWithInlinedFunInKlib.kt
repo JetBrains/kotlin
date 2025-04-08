a8c3a1861826fd04233dbbce5c1d92263254f36d
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-08 19:40:07 +0200

    [Tests] Split test anonymousObjectInInlineFunction.kt

--- native/native.tests/testData/codegen/kclass/anonymousObjectInInlineFunctionWithInlinedFunInKlib.kt
+++ native/native.tests/testData/codegen/kclass/anonymousObjectInInlineFunctionWithInlinedFunInKlib.kt
@@ -2,7 +2,12 @@
  * Copyright 2010-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the LICENSE file.
  */
-// LANGUAGE: -IrInlinerBeforeKlibSerialization
+// LANGUAGE: +IrInlinerBeforeKlibSerialization
+// IGNORE_BACKEND_K1: NATIVE
+
+// IGNORE_BACKEND: NATIVE
+// ^^^ KT-72441, KT-76547: Remove `IGNORE_BACKEND: NATIVE` after KT-72441.
+// Note: values of `expectedToStringName` are different from ones in `anonymousObjectInInlineFunction.kt`, due to inlining
 
 package codegen.kclass.kclass0
 import kotlin.test.*
@@ -26,7 +31,7 @@ fun box(): String {
         hasFooPrivate::class,
         expectedQualifiedName = null,
         expectedSimpleName = null, // KT-64460: simpleName is explicitly prohibited in NATIVE backend
-        expectedToStringName = "class codegen.kclass.kclass0.box\$\$inlined\$getHasFooPrivate\$1",
+        expectedToStringName = "class codegen.kclass.kclass0.box\$hasFooPrivate\$1",
         expectedInstance = hasFooPrivate,
         expectedNotInstance = Any()
     )
@@ -35,7 +40,7 @@ fun box(): String {
         hasFooPublic::class,
         expectedQualifiedName = null,
         expectedSimpleName = null, // KT-64460: simpleName is explicitly prohibited in NATIVE backend
-        expectedToStringName = "class codegen.kclass.kclass0.box\$\$inlined\$getHasFooPublic\$1",
+        expectedToStringName = "class codegen.kclass.kclass0.box\$hasFooPublic\$1",
         expectedInstance = hasFooPublic,
         expectedNotInstance = Any()
     )
