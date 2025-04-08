059201bc2d7cfcb2cf07456e4a27cbfffe26ed7b
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-08 19:40:41 +0200

    [Native][Tests] Split LLDB tests
    ^KT-76547

--- native/native.tests/testData/lldb/kt42208WithPassingLambdaToAnotherFunctionWithInlinedFunInKlib.kt
+++ native/native.tests/testData/lldb/kt42208WithPassingLambdaToAnotherFunctionWithInlinedFunInKlib.kt
@@ -1,8 +1,8 @@
 // KIND: STANDALONE_LLDB
 // FIR_IDENTICAL
-// FREE_COMPILER_ARGS: -XXLanguage:-IrInlinerBeforeKlibSerialization
-
-
+// FREE_COMPILER_ARGS: -XXLanguage:+IrInlinerBeforeKlibSerialization
+// IGNORE_NATIVE_K2: optimizationMode=DEBUG
+// ^^^ KT-76547: WARNING:  Unable to resolve breakpoint to any actual locations.
 
 // FILE: kt42208-1.kt
 fun main() {
--- native/native.tests/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirLldbTestGenerated.java
+++ native/native.tests/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirLldbTestGenerated.java
@@ -111,15 +111,45 @@ public class FirLldbTestGenerated extends AbstractNativeBlackBoxTest {
   }
 
   @Test
+  @TestMetadata("kt42208WithInlinedFunInKlib.kt")
+  public void testKt42208WithInlinedFunInKlib() {
+    runTest("native/native.tests/testData/lldb/kt42208WithInlinedFunInKlib.kt");
+  }
+
+  @Test
   @TestMetadata("kt42208WithPassingLambdaToAnotherFunction.kt")
   public void testKt42208WithPassingLambdaToAnotherFunction() {
     runTest("native/native.tests/testData/lldb/kt42208WithPassingLambdaToAnotherFunction.kt");
   }
 
   @Test
+  @TestMetadata("kt42208WithPassingLambdaToAnotherFunctionWithInlinedFunInKlib.kt")
+  public void testKt42208WithPassingLambdaToAnotherFunctionWithInlinedFunInKlib() {
+    runTest("native/native.tests/testData/lldb/kt42208WithPassingLambdaToAnotherFunctionWithInlinedFunInKlib.kt");
+  }
+
+  @Test
   @TestMetadata("kt42208WithVariable.kt")
   public void testKt42208WithVariable() {
     runTest("native/native.tests/testData/lldb/kt42208WithVariable.kt");
+  }
+
+  @Test
+  @TestMetadata("kt42208WithVariableAndNesting.kt")
+  public void testKt42208WithVariableAndNesting() {
+    runTest("native/native.tests/testData/lldb/kt42208WithVariableAndNesting.kt");
+  }
+
+  @Test
+  @TestMetadata("kt42208WithVariableAndNestingWithInlinedFunInKlib.kt")
+  public void testKt42208WithVariableAndNestingWithInlinedFunInKlib() {
+    runTest("native/native.tests/testData/lldb/kt42208WithVariableAndNestingWithInlinedFunInKlib.kt");
+  }
+
+  @Test
+  @TestMetadata("kt42208WithVariableWithInlinedFunInKlib.kt")
+  public void testKt42208WithVariableWithInlinedFunInKlib() {
+    runTest("native/native.tests/testData/lldb/kt42208WithVariableWithInlinedFunInKlib.kt");
   }
 
   @Test
