d270f5885d15522d7602eef1dfbe28a0ae5ffd94
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-08 19:40:41 +0200

    [Tests] Disable 1st stage inliner in some LLDB tests
    ^KT-76547

--- native/native.tests/testData/lldb/kt42208WithPassingLambdaToAnotherFunction.kt
+++ native/native.tests/testData/lldb/kt42208WithPassingLambdaToAnotherFunction.kt
@@ -1,5 +1,12 @@
 // KIND: STANDALONE_LLDB
 // FIR_IDENTICAL
+// LANGUAGE: -IrInlinerBeforeKlibSerialization
+// ^^^ KT-76547: With pre-serialization inliner, there's an error:
+// Failed(reason=Exit code is 1 while 0 was expected.)
+// Failed(reason=Tested process output has not passed validation: The number of responses do not match the number of commands.
+// - Commands (5): [b kt42208-2.kt:13, r, bt, c, q]
+// - Responses (3): [b kt42208-2.kt:13, r, bt] ==> expected: <true> but was: <false>)
+
 // FILE: kt42208-1.kt
 fun main() {
     val a = foo()
