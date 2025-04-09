aaea22f185130136524283bbe90aee0c16c4c2c3
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-04-09 13:18:26 +0200

    Use the nearest BuildCache node for Mac iCRI agents
    ^KTI-1970 fixed

--- repo/gradle-settings-conventions/develocity/src/main/kotlin/develocity.settings.gradle.kts
+++ repo/gradle-settings-conventions/develocity/src/main/kotlin/develocity.settings.gradle.kts
@@ -51,12 +51,19 @@ buildCache {
         }
     }
     if (develocity.server.isPresent) {
-        remote(develocity.buildCache) {
-            isPush = buildProperties.pushToBuildCache
-            val remoteBuildCacheUrl = buildProperties.buildCacheUrl?.trim()
-            isEnabled = remoteBuildCacheUrl != "" // explicit "" disables it
-            if (!remoteBuildCacheUrl.isNullOrEmpty()) {
-                server = remoteBuildCacheUrl.removeSuffix("/cache/")
+        if (System.getenv("TC_K8S_CLOUD_PROFILE_ID") == "kotlindev-kotlin-k8s") {
+            remote(develocity.buildCache) {
+                isPush = buildProperties.pushToBuildCache
+                server = "https://kotlin-cache.eqx.k8s.intellij.net"
+            }
+        } else {
+            remote(develocity.buildCache) {
+                isPush = buildProperties.pushToBuildCache
+                val remoteBuildCacheUrl = buildProperties.buildCacheUrl?.trim()
+                isEnabled = remoteBuildCacheUrl != "" // explicit "" disables it
+                if (!remoteBuildCacheUrl.isNullOrEmpty()) {
+                    server = remoteBuildCacheUrl.removeSuffix("/cache/")
+                }
             }
         }
     }
