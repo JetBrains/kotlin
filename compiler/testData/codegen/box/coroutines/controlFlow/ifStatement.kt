91f45675b04b1fd4ead1002ed01df303971ff4d8
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2025-04-10 00:28:06 +0200

    modified example

--- compiler/testData/codegen/box/coroutines/controlFlow/ifStatement.kt
+++ compiler/testData/codegen/box/coroutines/controlFlow/ifStatement.kt
@@ -4,75 +4,43 @@ import helpers.*
 import kotlin.coroutines.*
 import kotlin.coroutines.intrinsics.*
 
+var data = ""
+fun log(s: String) {
+    data += s + ";"
+}
 
 class Controller {
     var result = ""
 
     suspend fun <T> suspendWithResult(value: T): T = suspendCoroutineUninterceptedOrReturn { c ->
+        log("3")
         c.resume(value)
+        // TODO what if with exception?
+        // TODO resume >1 times
+        log("4")
         COROUTINE_SUSPENDED
     }
 }
 
 fun builder(c: suspend Controller.() -> Unit): String {
     val controller = Controller()
-    c.startCoroutine(controller, EmptyContinuation)
+    c.startCoroutine(controller, Continuation(EmptyCoroutineContext) {})
     return controller.result
 }
 
 fun box(): String {
     var value = builder {
+        log("0")
         if (suspendWithResult(true)) {
+            log("1")
             result = "OK"
         }
+        log("2")
     }
+    log("5")
+
     if (value != "OK") return "fail: suspend as if condition: $value"
+    if (data != "0;3;1;2;4;5;") return "fail: $data"
 
-    value = builder {
-        for (x in listOf(true, false)) {
-            if (x) {
-                result += suspendWithResult("O")
-            }
-            else {
-                result += "K"
-            }
-        }
-    }
-    if (value != "OK") return "fail: suspend in then branch: $value"
-
-    value = builder {
-        for (x in listOf(true, false)) {
-            if (x) {
-                result += "O"
-            }
-            else {
-                result += suspendWithResult("K")
-            }
-        }
-    }
-    if (value != "OK") return "fail: suspend in else branch: $value"
-
-    value = builder {
-        for (x in listOf(true, false)) {
-            if (x) {
-                result += suspendWithResult("O")
-            }
-            else {
-                result += suspendWithResult("K")
-            }
-        }
-    }
-    if (value != "OK") return "fail: suspend in both branches: $value"
-
-    value = builder {
-        for (x in listOf(true, false)) {
-            if (x) {
-                result += suspendWithResult("O")
-            }
-            result += ";"
-        }
-    }
-    if (value != "O;;") return "fail: suspend in then branch without else: $value"
-
-    return "OK"
+    return value
 }
