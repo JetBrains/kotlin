f1e3a5d856b30f295e404796858a07db62f70903
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-10 20:48:29 +0200

    [Tests] Ignore deserialization test in fakeOverrideGenericBaseWithIrInliner.kt
    ^KT-72441
    ^KT-72777

--- compiler/testData/codegen/boxInline/anonymousObject/fakeOverrideGenericBaseWithIrInliner.kt
+++ compiler/testData/codegen/boxInline/anonymousObject/fakeOverrideGenericBaseWithIrInliner.kt
@@ -2,6 +2,8 @@
 // IGNORE_BACKEND_K1: JS_IR, JS_IR_ES6
 
 // NO_CHECK_LAMBDA_INLINING
+// IGNORE_IR_DESERIALIZATION_TEST: NATIVE, JS_IR
+// ^^^ KT-76547 before serialization: `lib.C<<unbound IrTypeParameterSymbolImpl>>`, after deserialization: `lib.C<T of lib.C>`
 
 // MODULE: lib
 // FILE: lib.kt
