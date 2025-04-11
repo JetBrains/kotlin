3bbb0531f06d22f7560f2efbe215e31d02785cf6
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-11 12:00:51 +0200

    fixup! [Tests] Simplify diagnostic tests, so test pipeline would not crash in deserialization during public inliner

--- compiler/testData/diagnostics/irInliner/syntheticAccessors/privateMember/crossModulePrivateLeak/leakingPrivateInnerClassThroughInternalInlineFun.kt
+++ compiler/testData/diagnostics/irInliner/syntheticAccessors/privateMember/crossModulePrivateLeak/leakingPrivateInnerClassThroughInternalInlineFun.kt
@@ -2,8 +2,6 @@
 // FIR_IDENTICAL
 // DIAGNOSTICS: -NOTHING_TO_INLINE
 
-// MODULE: lib
-// FILE: A.kt
 class A {
     private inner class Inner {
         fun foo() = "OK"
@@ -11,10 +9,4 @@ class A {
 
     private inline fun privateFun() = Inner().foo()
     internal inline fun internalInlineFun() = <!IR_PRIVATE_TYPE_USED_IN_NON_PRIVATE_INLINE_FUNCTION_CASCADING_ERROR!>privateFun()<!>
-}
-
-// MODULE: main()(lib)
-// FILE: main.kt
-fun box(): String {
-    return "OK" // Callsite of `A().internalInlineFun()` is omitted, to avoid test pipeline crash in public inliner after error diagnostic in dependent module
 }
