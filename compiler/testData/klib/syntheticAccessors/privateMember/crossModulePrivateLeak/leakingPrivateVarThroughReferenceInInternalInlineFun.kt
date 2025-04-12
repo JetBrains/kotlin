c5c645c914c85a88efd9b290003657214355cfec
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-12 21:00:56 +0200

    [Tests] Ignore two failed tests for synthetic accessors
    ^KT-76547
    ^KT-76712

--- compiler/testData/klib/syntheticAccessors/privateMember/crossModulePrivateLeak/leakingPrivateVarThroughReferenceInInternalInlineFun.kt
+++ compiler/testData/klib/syntheticAccessors/privateMember/crossModulePrivateLeak/leakingPrivateVarThroughReferenceInInternalInlineFun.kt
@@ -1,6 +1,11 @@
 // DONT_TARGET_EXACT_BACKEND: JS_IR
-// ^^^ Muted because accessor for function/constructor/property references are not generated for JS and first stage. To be fixed in KT-69797.
+// ^^^ KT-69797: Muted because accessor for function/constructor/property references are not generated for JS and first stage. To be fixed in KT-69797.
 // Can be replaced with ignore after KT-69941
+
+// IGNORE_BACKEND: NATIVE
+// IGNORE_KLIB_SYNTHETIC_ACCESSORS_CHECKS: NATIVE
+// ^^^ KT-76712, KT-76547: error: <missing declarations>: No function found for symbol '/<unknown name>|?'
+//                         error: there are linkage errors reported by the partial linkage engine
 
 // KT-72862: Undefined symbols
 // IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE
