5e37509dd36e4cc0eebd0576ba974e4382f2e80a
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-04-14 16:53:28 +0200

    fixup! [Tests][JS] Avoid crash in JsIrIniningFacade

--- js/js.tests/test/org/jetbrains/kotlin/js/test/converters/JsIrInliningFacade.kt
+++ js/js.tests/test/org/jetbrains/kotlin/js/test/converters/JsIrInliningFacade.kt
@@ -30,8 +30,7 @@ class JsIrInliningFacade(
     testServices: TestServices,
 ) : IrInliningFacade<IrBackendInput>(testServices, BackendKinds.IrBackend, BackendKinds.IrBackend) {
     override fun shouldTransform(module: TestModule): Boolean {
-        return module.languageVersionSettings.supportsFeature(LanguageFeature.IrInlinerBeforeKlibSerialization) &&
-                module.languageVersionSettings.languageVersion.usesK2
+        return module.languageVersionSettings.supportsFeature(LanguageFeature.IrInlinerBeforeKlibSerialization)
     }
 
     override fun transform(module: TestModule, inputArtifact: IrBackendInput): IrBackendInput {
--- native/native.tests/klib-ir-inliner/tests/org/jetbrains/kotlin/konan/test/converters/NativeInliningFacade.kt
+++ native/native.tests/klib-ir-inliner/tests/org/jetbrains/kotlin/konan/test/converters/NativeInliningFacade.kt
@@ -25,8 +25,7 @@ class NativeInliningFacade(
     testServices: TestServices,
 ) : IrInliningFacade<IrBackendInput>(testServices, BackendKinds.IrBackend, BackendKinds.IrBackend) {
     override fun shouldTransform(module: TestModule): Boolean {
-        return module.languageVersionSettings.supportsFeature(LanguageFeature.IrInlinerBeforeKlibSerialization) &&
-                module.languageVersionSettings.languageVersion.usesK2
+        return module.languageVersionSettings.supportsFeature(LanguageFeature.IrInlinerBeforeKlibSerialization)
     }
 
     override fun transform(module: TestModule, inputArtifact: IrBackendInput): IrBackendInput {
