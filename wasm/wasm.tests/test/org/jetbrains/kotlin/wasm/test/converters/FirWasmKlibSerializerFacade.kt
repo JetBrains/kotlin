d73f8bfc455ce173f9fea902054ed343e0b3839d
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-04-15 23:48:12 +0200

    Minor, Wasm. Use named parameters in function calls with many arguments

--- wasm/wasm.tests/test/org/jetbrains/kotlin/wasm/test/converters/FirWasmKlibSerializerFacade.kt
+++ wasm/wasm.tests/test/org/jetbrains/kotlin/wasm/test/converters/FirWasmKlibSerializerFacade.kt
@@ -62,14 +62,14 @@ class FirWasmKlibSerializerFacade(
 
         if (firstTimeCompilation) {
             serializeModuleIntoKlib(
-                configuration[CommonConfigurationKeys.MODULE_NAME]!!,
-                configuration,
-                diagnosticReporter,
-                inputArtifact.metadataSerializer,
+                moduleName = configuration[CommonConfigurationKeys.MODULE_NAME]!!,
+                configuration = configuration,
+                diagnosticReporter = diagnosticReporter,
+                metadataSerializer = inputArtifact.metadataSerializer,
                 klibPath = outputFile.path,
-                libraries.map { it.library },
-                inputArtifact.irModuleFragment,
-                inputArtifact.irPluginContext.irBuiltIns,
+                dependencies = libraries.map { it.library },
+                moduleFragment = inputArtifact.irModuleFragment,
+                irBuiltIns = inputArtifact.irPluginContext.irBuiltIns,
                 cleanFiles = inputArtifact.icData,
                 nopack = true,
                 containsErrorCode = inputArtifact.hasErrors,
