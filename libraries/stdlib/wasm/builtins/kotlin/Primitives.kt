9b3ed059087cbe2bc89ad5fed73b368c5b521d7f
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2025-04-15 17:02:57 +0200

    [Wasm] Implement Float rem operation aligned with JVM
    KT-76775

--- libraries/stdlib/wasm/builtins/kotlin/Primitives.kt
+++ libraries/stdlib/wasm/builtins/kotlin/Primitives.kt
@@ -2167,7 +2167,7 @@ public actual class Float private constructor(private val value: Float) : Number
     @SinceKotlin("1.1")
     @kotlin.internal.IntrinsicConstEvaluation
     public actual operator fun rem(other: Float): Float =
-        wasm_f32_copysign(this - (wasm_f32_truncate(this / other) * other), this)
+        kotlin.math.fdlibm.__ieee754_fmodf(this, other)
 
     /**
      * Calculates the remainder of truncating division of this value (dividend) by the other value (divisor).
