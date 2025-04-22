36bedfee4b29f00576c2f26b7c1918705f58353d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-04-22 21:03:07 +0200

    [FIR] ConeAttributeRenderer: stabilize attributes order
    This helps to prevent flaky behavior as there is no guarantee
    on their order
    
    ^KT-76947

--- compiler/fir/cones/src/org/jetbrains/kotlin/fir/renderer/ConeAttributeRenderer.kt
+++ compiler/fir/cones/src/org/jetbrains/kotlin/fir/renderer/ConeAttributeRenderer.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -12,12 +12,20 @@ abstract class ConeAttributeRenderer {
     abstract fun render(attributes: Iterable<ConeAttribute<*>>): String
 
     object ToString : ConeAttributeRenderer() {
-        override fun render(attributes: Iterable<ConeAttribute<*>>): String =
-            attributes.joinToString(separator = " ", postfix = " ") { it.toString() }
+        override fun render(attributes: Iterable<ConeAttribute<*>>): String {
+            return attributes.sortedBy { it.key.qualifiedName }.joinToString(separator = " ", postfix = " ")
+        }
     }
 
     object ForReadability : ConeAttributeRenderer() {
-        override fun render(attributes: Iterable<ConeAttribute<*>>): String =
-            attributes.mapNotNull { it.renderForReadability() }.ifNotEmpty { joinToString(separator = " ", postfix = " ") } ?: ""
+        override fun render(attributes: Iterable<ConeAttribute<*>>): String {
+            return attributes.mapNotNull { attribute -> attribute.renderForReadability()?.let { attribute to it } }
+                .sortedBy { (attribute, _) -> attribute.key.qualifiedName }
+                .ifNotEmpty {
+                    joinToString(separator = " ", postfix = " ") { (_, output) ->
+                        output
+                    }
+                } ?: ""
+        }
     }
 }
