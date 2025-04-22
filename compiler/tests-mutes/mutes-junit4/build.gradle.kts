457f003f26d43f45f78aabd6131def7260840509
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-04-23 00:24:52 +0200

    junit-foundation 17.2.2
    ^KTI-2274 Fixed

--- compiler/tests-mutes/mutes-junit4/build.gradle.kts
+++ compiler/tests-mutes/mutes-junit4/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     api(libs.junit4)
     implementation(project(":compiler:tests-mutes"))
-    api("com.nordstrom.tools:junit-foundation:17.1.1")
+    api("com.nordstrom.tools:junit-foundation:17.2.2")
 }
 
 sourceSets {
