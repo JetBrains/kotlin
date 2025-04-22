4c8f0f917eebf1b282a08ad037ed2d8d8dc5f86c
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-04-22 14:49:33 +0300

    [CLI] Convert `PlainTextMessageRenderer` from java to kotlin
    This allows to get rid of `else` branch in switch by `CompilerMessageSeverity`
    to avoid errors like KT-76829 in future

--- compiler/cli/src/org/jetbrains/kotlin/cli/common/messages/PlainTextMessageRenderer.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/common/messages/PlainTextMessageRenderer.kt
@@ -13,192 +13,165 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.cli.common.messages
 
-package org.jetbrains.kotlin.cli.common.messages;
+import org.fusesource.jansi.Ansi
+import org.fusesource.jansi.AnsiConsole
+import org.fusesource.jansi.internal.CLibrary
+import org.jetbrains.kotlin.cli.common.CompilerSystemProperties
+import org.jetbrains.kotlin.cli.common.isWindows
+import org.jetbrains.kotlin.util.capitalizeDecapitalize.decapitalizeAsciiOnly
+import java.util.*
 
-import kotlin.text.StringsKt;
-import org.fusesource.jansi.Ansi;
-import org.fusesource.jansi.AnsiConsole;
-import org.fusesource.jansi.internal.CLibrary;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.cli.common.CompilerSystemProperties;
-import org.jetbrains.kotlin.cli.common.PropertiesKt;
-import org.jetbrains.kotlin.util.capitalizeDecapitalize.CapitalizeDecapitalizeKt;
-
-import java.util.EnumSet;
-import java.util.Set;
-
-import static org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity.*;
-
-public abstract class PlainTextMessageRenderer implements MessageRenderer {
-    private static final boolean COLOR_ENABLED;
-
-    static {
-        boolean isStderrATty = false;
-        // TODO: investigate why ANSI escape codes on Windows only work in REPL for some reason
-        String kotlinColorsEnabled = CompilerSystemProperties.KOTLIN_COLORS_ENABLED_PROPERTY.getValue();
-        if (!PropertiesKt.isWindows() && "true".equals(kotlinColorsEnabled)) {
-            try {
-                isStderrATty = CLibrary.isatty(CLibrary.STDERR_FILENO) != 0;
-            }
-            catch (UnsatisfiedLinkError ignored) {
-            }
-        }
-        COLOR_ENABLED = isStderrATty || "always".equals(kotlinColorsEnabled);
+// This constructor can be used in a compilation server to still be able to generate colored output, even if stderr is not a TTY.
+abstract class PlainTextMessageRenderer @JvmOverloads constructor(private val colorEnabled: Boolean = COLOR_ENABLED) : MessageRenderer {
+    override fun renderPreamble(): String {
+        return ""
     }
 
-    private static final String LINE_SEPARATOR = System.lineSeparator();
+    override fun render(severity: CompilerMessageSeverity, message: String, location: CompilerMessageSourceLocation?): String {
+        val result = StringBuilder()
 
-    private static final Set<CompilerMessageSeverity> IMPORTANT_MESSAGE_SEVERITIES = EnumSet.of(EXCEPTION, ERROR, STRONG_WARNING, WARNING);
+        val line = location?.line ?: -1
+        val column = location?.column ?: -1
+        val lineEnd = location?.lineEnd ?: -1
+        val columnEnd = location?.columnEnd ?: -1
+        val lineContent = location?.lineContent
 
-    private final boolean colorEnabled;
-
-    public PlainTextMessageRenderer() {
-        this(COLOR_ENABLED);
-    }
-
-    // This constructor can be used in a compilation server to still be able to generate colored output, even if stderr is not a TTY.
-    @SuppressWarnings("WeakerAccess")
-    public PlainTextMessageRenderer(boolean colorEnabled) {
-        this.colorEnabled = colorEnabled;
-    }
-
-    @Override
-    public String renderPreamble() {
-        return "";
-    }
-
-    @Override
-    public String render(@NotNull CompilerMessageSeverity severity, @NotNull String message, @Nullable CompilerMessageSourceLocation location) {
-        StringBuilder result = new StringBuilder();
-
-        int line = location != null ? location.getLine() : -1;
-        int column = location != null ? location.getColumn() : -1;
-        int lineEnd = location != null ? location.getLineEnd() : -1;
-        int columnEnd = location != null ? location.getColumnEnd() : -1;
-        String lineContent = location != null ? location.getLineContent() : null;
-
-        String path = location != null ? getPath(location) : null;
+        val path = if (location != null) getPath(location) else null
         if (path != null) {
-            result.append(path);
-            result.append(":");
+            result.append(path)
+            result.append(":")
             if (line > 0) {
-                result.append(line).append(":");
+                result.append(line).append(":")
                 if (column > 0) {
-                    result.append(column).append(":");
+                    result.append(column).append(":")
                 }
             }
-            result.append(" ");
+            result.append(" ")
         }
 
         if (this.colorEnabled) {
-            Ansi ansi = Ansi.ansi()
-                    .bold()
-                    .fg(severityColor(severity))
-                    .a(severity.getPresentableName())
-                    .a(": ")
-                    .reset();
+            val ansi: Ansi = Ansi.ansi()
+                .bold()
+                .fg(severityColor(severity))
+                .a(severity.presentableName)
+                .a(": ")
+                .reset()
 
             if (IMPORTANT_MESSAGE_SEVERITIES.contains(severity)) {
-                ansi.bold();
+                ansi.bold()
             }
 
             // Only make the first line of the message bold. Otherwise long overload ambiguity errors or exceptions are hard to read
-            String decapitalized = decapitalizeIfNeeded(message);
-            int firstNewline = decapitalized.indexOf(LINE_SEPARATOR);
+            val decapitalized: String = decapitalizeIfNeeded(message)
+            val firstNewline: Int = decapitalized.indexOf(LINE_SEPARATOR)
             if (firstNewline < 0) {
-                result.append(ansi.a(decapitalized).reset());
-            }
-            else {
-                result.append(ansi.a(decapitalized.substring(0, firstNewline)).reset().a(decapitalized.substring(firstNewline)));
-            }
-        }
-        else {
-            result.append(severity.getPresentableName());
-            result.append(": ");
-            result.append(decapitalizeIfNeeded(message));
-        }
-
-        if (lineContent != null && 1 <= column && column <= lineContent.length() + 1) {
-            result.append(LINE_SEPARATOR);
-            result.append(lineContent);
-            result.append(LINE_SEPARATOR);
-            result.append(StringsKt.repeat(" ", column - 1));
-            if (lineEnd > line) {
-                result.append(StringsKt.repeat("^", lineContent.length() - column + 1));
-            } else if (lineEnd == line && columnEnd > column) {
-                result.append(StringsKt.repeat("^", columnEnd - column));
+                result.append(ansi.a(decapitalized).reset())
             } else {
-                result.append("^");
+                result.append(ansi.a(decapitalized.substring(0, firstNewline)).reset().a(decapitalized.substring(firstNewline)))
+            }
+        } else {
+            result.append(severity.presentableName)
+            result.append(": ")
+            result.append(decapitalizeIfNeeded(message))
+        }
+
+        if (lineContent != null && 1 <= column && column <= lineContent.length + 1) {
+            result.append(LINE_SEPARATOR)
+            result.append(lineContent)
+            result.append(LINE_SEPARATOR)
+            result.append(" ".repeat(column - 1))
+            if (lineEnd > line) {
+                result.append("^".repeat(lineContent.length - column + 1))
+            } else if (lineEnd == line && columnEnd > column) {
+                result.append("^".repeat(columnEnd - column))
+            } else {
+                result.append("^")
             }
         }
 
-        return result.toString();
+        return result.toString()
     }
 
-    @NotNull
-    private static String decapitalizeIfNeeded(@NotNull String message) {
-        // TODO: invent something more clever
-        // An ad-hoc heuristic to prevent decapitalization of some names
-        if (message.startsWith("Java") || message.startsWith("Kotlin")) {
-            return message;
-        }
+    protected abstract fun getPath(location: CompilerMessageSourceLocation): String?
 
-        // For abbreviations and capitalized text
-        if (message.length() >= 2 && Character.isUpperCase(message.charAt(0)) && Character.isUpperCase(message.charAt(1))) {
-            return message;
-        }
-
-        return CapitalizeDecapitalizeKt.decapitalizeAsciiOnly(message);
+    override fun renderUsage(usage: String): String {
+        return usage
     }
 
-    @NotNull
-    private static Ansi.Color severityColor(@NotNull CompilerMessageSeverity severity) {
-        switch (severity) {
-            case EXCEPTION:
-                return Ansi.Color.RED;
-            case ERROR:
-                return Ansi.Color.RED;
-            case STRONG_WARNING:
-                return Ansi.Color.YELLOW;
-            case WARNING:
-                return Ansi.Color.YELLOW;
-            case FIXED_WARNING:
-                return Ansi.Color.YELLOW;
-            case INFO:
-                return Ansi.Color.BLUE;
-            case LOGGING:
-                return Ansi.Color.BLUE;
-            case OUTPUT:
-                return Ansi.Color.BLUE;
-            default:
-                throw new UnsupportedOperationException("Unknown severity: " + severity);
-        }
+    override fun renderConclusion(): String {
+        return ""
     }
 
-    @Nullable
-    protected abstract String getPath(@NotNull CompilerMessageSourceLocation location);
-
-    @Override
-    public String renderUsage(@NotNull String usage) {
-        return usage;
-    }
-
-    @Override
-    public String renderConclusion() {
-        return "";
-    }
-
-    public void enableColorsIfNeeded() {
+    fun enableColorsIfNeeded() {
         if (colorEnabled) {
-            AnsiConsole.systemInstall();
+            AnsiConsole.systemInstall()
         }
     }
 
-    public void disableColorsIfNeeded() {
+    fun disableColorsIfNeeded() {
         if (colorEnabled) {
-            AnsiConsole.systemUninstall();
+            AnsiConsole.systemUninstall()
+        }
+    }
+
+    companion object {
+        private val COLOR_ENABLED: Boolean
+
+        init {
+            var isStderrATty = false
+            // TODO: investigate why ANSI escape codes on Windows only work in REPL for some reason
+            val kotlinColorsEnabled = CompilerSystemProperties.KOTLIN_COLORS_ENABLED_PROPERTY.value
+            if (!isWindows && "true" == kotlinColorsEnabled) {
+                try {
+                    isStderrATty = CLibrary.isatty(CLibrary.STDERR_FILENO) != 0
+                } catch (_: UnsatisfiedLinkError) {
+                }
+            }
+            COLOR_ENABLED = isStderrATty || "always" == kotlinColorsEnabled
+        }
+
+        private val LINE_SEPARATOR: String = System.lineSeparator()
+
+        private val IMPORTANT_MESSAGE_SEVERITIES: Set<CompilerMessageSeverity> = EnumSet.of(
+            CompilerMessageSeverity.EXCEPTION,
+            CompilerMessageSeverity.ERROR,
+            CompilerMessageSeverity.STRONG_WARNING,
+            CompilerMessageSeverity.WARNING
+        )
+
+        private fun decapitalizeIfNeeded(message: String): String {
+            // TODO: invent something more clever
+            // An ad-hoc heuristic to prevent decapitalization of some names
+            if (message.startsWith("Java") || message.startsWith("Kotlin")) {
+                return message
+            }
+
+            // For abbreviations and capitalized text
+            if (message.length >= 2 && Character.isUpperCase(message[0]) && Character.isUpperCase(message[1])) {
+                return message
+            }
+
+            return message.decapitalizeAsciiOnly()
+        }
+
+        private fun severityColor(severity: CompilerMessageSeverity): Ansi.Color {
+            return when (severity) {
+                CompilerMessageSeverity.EXCEPTION,
+                CompilerMessageSeverity.ERROR
+                    -> Ansi.Color.RED
+
+                CompilerMessageSeverity.STRONG_WARNING,
+                CompilerMessageSeverity.WARNING,
+                CompilerMessageSeverity.FIXED_WARNING
+                    -> Ansi.Color.YELLOW
+
+                CompilerMessageSeverity.INFO,
+                CompilerMessageSeverity.LOGGING,
+                CompilerMessageSeverity.OUTPUT
+                    -> Ansi.Color.BLUE
+            }
         }
     }
 }
