a6cb1fa99a55361d3b037380645e8b5b6c063a4c
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-04-22 10:40:26 +0300

    [FIR2IR] Insert implicit nullability casts to elements of vararg expressions
    ^KT-76615 Fixed

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
@@ -12,6 +12,7 @@ import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.descriptors.isObject
 import org.jetbrains.kotlin.diagnostics.findChildByType
 import org.jetbrains.kotlin.fir.*
+import org.jetbrains.kotlin.fir.backend.Fir2IrImplicitCastInserter.NoConversionsExpected
 import org.jetbrains.kotlin.fir.backend.generators.ClassMemberGenerator
 import org.jetbrains.kotlin.fir.backend.generators.OperatorExpressionGenerator
 import org.jetbrains.kotlin.fir.backend.utils.*
@@ -588,8 +589,20 @@ class Fir2IrVisitor(
                 varargArgumentsExpression.coneElementTypeOrNull?.toIrType(c)
                     ?: error("Vararg expression has incorrect type"),
                 varargArgumentsExpression.arguments.mapNotNull {
-                    if (isGetClassOfUnresolvedTypeInAnnotation(it)) null
-                    else it.convertToIrVarargElement()
+                    if (isGetClassOfUnresolvedTypeInAnnotation(it)) return@mapNotNull null
+                    val irVarargElement = it.convertToIrVarargElement()
+                    if (irVarargElement is IrExpression) {
+                        with(implicitCastInserter) {
+                            @OptIn(NoConversionsExpected::class)
+                            irVarargElement.insertSpecialCast(
+                                it,
+                                it.resolvedType,
+                                varargArgumentsExpression.coneElementTypeOrNull ?: it.resolvedType.lowerBoundIfFlexible()
+                            )
+                        }
+                    } else {
+                        irVarargElement
+                    }
                 }
             )
         }
