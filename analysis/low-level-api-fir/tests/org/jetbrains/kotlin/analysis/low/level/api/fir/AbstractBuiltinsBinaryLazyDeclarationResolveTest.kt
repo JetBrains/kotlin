boost-mw-poc/jetbrains_kotlin

[LL] Rename `checkSession` to `checkResolutionFacade`
Follow-up to the rename of `LLFirResolveSession` to `LLResolutionFacade`

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractBuiltinsBinaryLazyDeclarationResolveTest.kt
@@ -14,7 +14,7 @@ import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirective
 import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
 
 abstract class AbstractBuiltinsBinaryLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
-    override fun checkSession(llResolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade) {
         requireIsInstance<KaBuiltinsModule>(llResolutionFacade.useSiteKtModule)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirLazyDeclarationResolveByReferenceTest.kt
@@ -22,7 +22,7 @@ import org.jetbrains.kotlin.test.services.TestServices
  * may be unacceptable in some cases.
  */
 abstract class AbstractFirLazyDeclarationResolveByReferenceTest : AbstractFirLazyDeclarationResolveOverAllPhasesTest() {
-    override fun checkSession(llResolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade) {
         require(llResolutionFacade.isSourceSession)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirLazyDeclarationResolveOverAllPhasesTest.kt
@@ -29,7 +29,7 @@ import org.jetbrains.kotlin.test.services.moduleStructure
  * This test iterates over all [FirResolvePhase] for the selected declaration and dump output after each phase
  */
 abstract class AbstractFirLazyDeclarationResolveOverAllPhasesTest : AbstractFirLazyDeclarationResolveTestCase() {
-    abstract fun checkSession(llResolutionFacade: LLResolutionFacade)
+    abstract fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade)
 
     protected open val outputExtension: String get() = ".txt"
 
@@ -57,7 +57,7 @@ abstract class AbstractFirLazyDeclarationResolveOverAllPhasesTest : AbstractFirL
         val renderer = lazyResolveRenderer(resultBuilder)
 
         withResolutionFacade(ktFile) { resolutionFacade ->
-            checkSession(resolutionFacade)
+            checkResolutionFacade(resolutionFacade)
             val allKtFiles = testServices.ktTestModuleStructure.allMainKtFiles
 
             val preresolvedElementCarets = testServices.expressionMarkerProvider.getBottommostElementsOfTypeAtCarets<KtDeclaration>(
@@ -113,7 +113,7 @@ abstract class AbstractFirLazyDeclarationResolveOverAllPhasesTest : AbstractFirL
         clearCaches(ktFile.project)
 
         withResolutionFacade(ktFile) { resolutionFacade ->
-            checkSession(resolutionFacade)
+            checkResolutionFacade(resolutionFacade)
             val firFile = resolutionFacade.getOrBuildFirFile(ktFile)
             firFile.lazyResolveToPhaseRecursively(FirResolvePhase.BODY_RESOLVE)
             if (resultBuilder.isNotEmpty()) {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirLazyDeclarationResolveTest.kt
@@ -20,7 +20,7 @@ import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirective
 import org.jetbrains.kotlin.test.services.TestServices
 
 abstract class AbstractFirLazyDeclarationResolveTest : AbstractFirLazyDeclarationResolveOverAllPhasesTest() {
-    override fun checkSession(llResolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade) {
         require(llResolutionFacade.isSourceSession)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibSourcesLazyDeclarationResolveTest.kt
@@ -11,7 +11,7 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.Analys
 import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
 
 abstract class AbstractStdLibSourcesLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
-    override fun checkSession(llResolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade) {
         requireIsInstance<KaLibrarySourceModule>(llResolutionFacade.useSiteKtModule)
     }
 

JetBrains/kotlin

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractBuiltinsBinaryLazyDeclarationResolveTest.kt
@@ -14,7 +14,7 @@ import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirective
 import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
 
 abstract class AbstractBuiltinsBinaryLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
-    override fun checkSession(resolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(resolutionFacade: LLResolutionFacade) {
         requireIsInstance<KaBuiltinsModule>(resolutionFacade.useSiteKtModule)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirLazyDeclarationResolveByReferenceTest.kt
@@ -22,7 +22,7 @@ import org.jetbrains.kotlin.test.services.TestServices
  * may be unacceptable in some cases.
  */
 abstract class AbstractFirLazyDeclarationResolveByReferenceTest : AbstractFirLazyDeclarationResolveOverAllPhasesTest() {
-    override fun checkSession(resolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(resolutionFacade: LLResolutionFacade) {
         require(resolutionFacade.isSourceSession)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirLazyDeclarationResolveOverAllPhasesTest.kt
@@ -29,7 +29,7 @@ import org.jetbrains.kotlin.test.services.moduleStructure
  * This test iterates over all [FirResolvePhase] for the selected declaration and dump output after each phase
  */
 abstract class AbstractFirLazyDeclarationResolveOverAllPhasesTest : AbstractFirLazyDeclarationResolveTestCase() {
-    abstract fun checkSession(resolutionFacade: LLResolutionFacade)
+    abstract fun checkResolutionFacade(resolutionFacade: LLResolutionFacade)
 
     protected open val outputExtension: String get() = ".txt"
 
@@ -57,7 +57,7 @@ abstract class AbstractFirLazyDeclarationResolveOverAllPhasesTest : AbstractFirL
         val renderer = lazyResolveRenderer(resultBuilder)
 
         withResolutionFacade(ktFile) { resolutionFacade ->
-            checkSession(resolutionFacade)
+            checkResolutionFacade(resolutionFacade)
             val allKtFiles = testServices.ktTestModuleStructure.allMainKtFiles
 
             val preresolvedElementCarets = testServices.expressionMarkerProvider.getBottommostElementsOfTypeAtCarets<KtDeclaration>(
@@ -113,7 +113,7 @@ abstract class AbstractFirLazyDeclarationResolveOverAllPhasesTest : AbstractFirL
         clearCaches(ktFile.project)
 
         withResolutionFacade(ktFile) { resolutionFacade ->
-            checkSession(resolutionFacade)
+            checkResolutionFacade(resolutionFacade)
             val firFile = resolutionFacade.getOrBuildFirFile(ktFile)
             firFile.lazyResolveToPhaseRecursively(FirResolvePhase.BODY_RESOLVE)
             if (resultBuilder.isNotEmpty()) {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractFirLazyDeclarationResolveTest.kt
@@ -20,7 +20,7 @@ import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirective
 import org.jetbrains.kotlin.test.services.TestServices
 
 abstract class AbstractFirLazyDeclarationResolveTest : AbstractFirLazyDeclarationResolveOverAllPhasesTest() {
-    override fun checkSession(resolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(resolutionFacade: LLResolutionFacade) {
         require(resolutionFacade.isSourceSession)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibSourcesLazyDeclarationResolveTest.kt
@@ -11,7 +11,7 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.Analys
 import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
 
 abstract class AbstractStdLibSourcesLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
-    override fun checkSession(resolutionFacade: LLResolutionFacade) {
+    override fun checkResolutionFacade(resolutionFacade: LLResolutionFacade) {
         requireIsInstance<KaLibrarySourceModule>(resolutionFacade.useSiteKtModule)
     }
 