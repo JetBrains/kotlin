boost-mw-poc/jetbrains_kotlin

[LL, AA FIR] Rename `LLResolutionFacade.useSiteKtModule` to `useSiteModule`
In Analysis API, we only have `KaModule`s, which can be
abbreviated to just "module" here.

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/KaFirSession.kt
@@ -86,7 +86,7 @@ private constructor(
     }
 
     @Suppress("AnalysisApiMissingLifetimeCheck")
-    override val useSiteModule: KaModule get() = llResolutionFacade.useSiteKtModule
+    override val useSiteModule: KaModule get() = llResolutionFacade.useSiteModule
 
     internal val firSession: LLFirSession get() = llResolutionFacade.useSiteFirSession
     internal val targetPlatform: TargetPlatform get() = firSession.moduleData.platform
@@ -123,7 +123,7 @@ private constructor(
             token: KaLifetimeToken,
         ): KaFirSession {
             token.assertIsValid()
-            val useSiteModule = llResolutionFacade.useSiteKtModule
+            val useSiteModule = llResolutionFacade.useSiteModule
             val useSiteSession = llResolutionFacade.useSiteFirSession
 
             val extensionTools = buildList {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractBuiltinsBinaryLazyDeclarationResolveTest.kt
@@ -15,7 +15,7 @@ import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
 
 abstract class AbstractBuiltinsBinaryLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
     override fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade) {
-        requireIsInstance<KaBuiltinsModule>(llResolutionFacade.useSiteKtModule)
+        requireIsInstance<KaBuiltinsModule>(llResolutionFacade.useSiteModule)
     }
 
     override fun configureTest(builder: TestConfigurationBuilder) {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibSourcesLazyDeclarationResolveTest.kt
@@ -12,7 +12,7 @@ import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
 
 abstract class AbstractStdLibSourcesLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
     override fun checkResolutionFacade(llResolutionFacade: LLResolutionFacade) {
-        requireIsInstance<KaLibrarySourceModule>(llResolutionFacade.useSiteKtModule)
+        requireIsInstance<KaLibrarySourceModule>(llResolutionFacade.useSiteModule)
     }
 
     override val configurator get() = AnalysisApiFirStdlibSourceTestConfigurator

JetBrains/kotlin

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/KaFirSession.kt
@@ -86,7 +86,7 @@ private constructor(
     }
 
     @Suppress("AnalysisApiMissingLifetimeCheck")
-    override val useSiteModule: KaModule get() = resolutionFacade.useSiteKtModule
+    override val useSiteModule: KaModule get() = resolutionFacade.useSiteModule
 
     internal val firSession: LLFirSession get() = resolutionFacade.useSiteFirSession
     internal val targetPlatform: TargetPlatform get() = firSession.moduleData.platform
@@ -123,7 +123,7 @@ private constructor(
             token: KaLifetimeToken,
         ): KaFirSession {
             token.assertIsValid()
-            val useSiteModule = resolutionFacade.useSiteKtModule
+            val useSiteModule = resolutionFacade.useSiteModule
             val useSiteSession = resolutionFacade.useSiteFirSession
 
             val extensionTools = buildList {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractBuiltinsBinaryLazyDeclarationResolveTest.kt
@@ -15,7 +15,7 @@ import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
 
 abstract class AbstractBuiltinsBinaryLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
     override fun checkResolutionFacade(resolutionFacade: LLResolutionFacade) {
-        requireIsInstance<KaBuiltinsModule>(resolutionFacade.useSiteKtModule)
+        requireIsInstance<KaBuiltinsModule>(resolutionFacade.useSiteModule)
     }
 
     override fun configureTest(builder: TestConfigurationBuilder) {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibSourcesLazyDeclarationResolveTest.kt
@@ -12,7 +12,7 @@ import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
 
 abstract class AbstractStdLibSourcesLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
     override fun checkResolutionFacade(resolutionFacade: LLResolutionFacade) {
-        requireIsInstance<KaLibrarySourceModule>(resolutionFacade.useSiteKtModule)
+        requireIsInstance<KaLibrarySourceModule>(resolutionFacade.useSiteModule)
     }
 
     override val configurator get() = AnalysisApiFirStdlibSourceTestConfigurator