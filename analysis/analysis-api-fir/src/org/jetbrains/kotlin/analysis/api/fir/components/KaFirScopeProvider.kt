boost-mw-poc/jetbrains_kotlin

[LL, AA FIR] Adapt calling code to rename of `LLResolutionFacade`

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirScopeProvider.kt
@@ -272,7 +272,7 @@ internal class KaFirScopeProvider(
 
     override val KtFile.importingScopeContext: KaScopeContext
         get() = withValidityAssertion {
-            val firFile = getOrBuildFirFile(firResolveSession)
+            val firFile = getOrBuildFirFile(llResolutionFacade)
             val firFileSession = firFile.moduleData.session
             val firImportingScopes = createImportingScopes(
                 firFile,
@@ -296,7 +296,7 @@ internal class KaFirScopeProvider(
         val correctedPosition = parentKDoc?.owner ?: position
 
         val context = ContextCollector.process(
-            fakeFile.getOrBuildFirFile(firResolveSession),
+            fakeFile.getOrBuildFirFile(llResolutionFacade),
             SessionHolderImpl(analysisSession.firSession, getScopeSession()),
             correctedPosition,
         )
@@ -406,7 +406,7 @@ internal class KaFirScopeProvider(
     }
 
     private fun getFirTypeScope(type: KaFirType): FirTypeScope? = type.coneType.scope(
-        firResolveSession.useSiteFirSession,
+        llResolutionFacade.useSiteFirSession,
         getScopeSession(),
         CallableCopyTypeCalculator.CalculateDeferredForceLazyResolution,
         requiredMembersPhase = FirResolvePhase.STATUS,
@@ -414,7 +414,7 @@ internal class KaFirScopeProvider(
 
     private fun getFirSyntheticPropertiesScope(coneType: ConeKotlinType, typeScope: FirTypeScope): FirSyntheticPropertiesScope? =
         FirSyntheticPropertiesScope.createIfSyntheticNamesProviderIsDefined(
-            firResolveSession.useSiteFirSession,
+            llResolutionFacade.useSiteFirSession,
             coneType,
             typeScope
         )

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/KaFirArrayAccessReference.kt
@@ -20,7 +20,7 @@ internal class KaFirArrayAccessReference(
     expression: KtArrayAccessExpression,
 ) : KtArrayAccessReference(expression), KaFirReference {
     override fun KaFirSession.computeSymbols(): Collection<KaSymbol> {
-        val fir = element.getOrBuildFir(firResolveSession) ?: return emptyList()
+        val fir = element.getOrBuildFir(llResolutionFacade) ?: return emptyList()
         return when (fir) {
             is FirFunctionCall -> fir.getCandidateSymbols().map { it.fir.buildSymbol(firSymbolBuilder) }
             is FirResolvedNamedReference -> listOf(fir.resolvedSymbol.buildSymbol(firSymbolBuilder))

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirPsiJavaClassSymbol.kt
@@ -123,7 +123,7 @@ internal class KaFirPsiJavaClassSymbol(
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     override val lazyFirSymbol: Lazy<FirRegularClassSymbol> = lazyPub {
         val module = analysisSession.getModule(backingPsi)
-        val provider = analysisSession.firResolveSession.getSessionFor(module).firClassByPsiClassProvider
+        val provider = analysisSession.llResolutionFacade.getSessionFor(module).firClassByPsiClassProvider
         provider.getFirClass(backingPsi)
     }
 

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirSymbolProvider.kt
@@ -198,7 +198,7 @@ internal class KaFirSymbolProvider(
         firSymbolBuilder.createPackageSymbolIfOneExists(fqName)
     }
 
-    private val backingRootPackageSymbol by lazy { KaFirPackageSymbol(FqName.ROOT, firResolveSession.project, token) }
+    private val backingRootPackageSymbol by lazy { KaFirPackageSymbol(FqName.ROOT, llResolutionFacade.project, token) }
 
     override val rootPackageSymbol: KaPackageSymbol
         get() = withValidityAssertion { backingRootPackageSymbol }

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/LLFirResolveSessionService.kt
@@ -20,7 +20,7 @@ import org.jetbrains.kotlin.utils.exceptions.errorWithAttachment
 class LLFirResolveSessionService(project: Project) {
     private val cache = LLFirSessionCache.getInstance(project)
 
-    fun getFirResolveSession(module: KaModule): LLResolutionFacade {
+    fun getLLResolutionFacade(module: KaModule): LLResolutionFacade {
         return create(module, cache::getSession)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractByQualifiedNameLazyDeclarationResolveTest.kt
@@ -29,8 +29,8 @@ abstract class AbstractByQualifiedNameLazyDeclarationResolveTest : AbstractFirLa
     override fun doTestByMainModuleAndOptionalMainFile(mainFile: KtFile?, mainModule: KtTestModule, testServices: TestServices) {
         val psiFile = mainFile ?: mainModule.psiFiles.first()
         val classId = testServices.moduleStructure.allDirectives.singleValue(Directives.CLASS_ID).let(ClassId::fromString)
-        val resolveSession = LLFirResolveSessionService.getInstance(psiFile.project).getFirResolveSession(mainModule.ktModule)
-        val classDeclaration = findRegularClass(classId, mainModule.ktModule, resolveSession).findPsi(mainModule.ktModule.contentScope) as KtClassOrObject
+        val resolutionFacade = LLFirResolveSessionService.getInstance(psiFile.project).getLLResolutionFacade(mainModule.ktModule)
+        val classDeclaration = findRegularClass(classId, mainModule.ktModule, resolutionFacade).findPsi(mainModule.ktModule.contentScope) as KtClassOrObject
         val file = classDeclaration.containingFile as KtFile
 
         doLazyResolveTest(file, testServices, outputRenderingMode) { firSession ->
@@ -44,8 +44,8 @@ abstract class AbstractByQualifiedNameLazyDeclarationResolveTest : AbstractFirLa
 
     open val outputRenderingMode: OutputRenderingMode = OutputRenderingMode.USE_SITE_AND_DESIGNATION_FILES
 
-    private fun findRegularClass(classId: ClassId, module: KaModule, firSession: LLResolutionFacade): FirRegularClassSymbol {
-        return firSession.getSessionFor(module).getRegularClassSymbolByClassId(classId) ?: error("'$classId' is not found")
+    private fun findRegularClass(classId: ClassId, module: KaModule, llResolutionFacade: LLResolutionFacade): FirRegularClassSymbol {
+        return llResolutionFacade.getSessionFor(module).getRegularClassSymbolByClassId(classId) ?: error("'$classId' is not found")
     }
 
     private object Directives : SimpleDirectivesContainer() {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractContextCollectorTest.kt
@@ -6,7 +6,7 @@
 package org.jetbrains.kotlin.analysis.low.level.api.fir
 
 import org.jetbrains.kotlin.analysis.api.projectStructure.copyOrigin
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getFirResolveSession
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getLLResolutionFacade
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFirFile
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirCustomScriptDefinitionTestConfigurator
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirSourceTestConfigurator
@@ -60,11 +60,11 @@ abstract class AbstractContextCollectorTest : AbstractAnalysisApiBasedTest() {
         testPrefixes: List<String>,
         useBodyElement: Boolean
     ) {
-        val resolveSession = mainModule.ktModule.getFirResolveSession(mainFile.project)
-        val session = resolveSession.useSiteFirSession
+        val resolutionFacade = mainModule.ktModule.getLLResolutionFacade(mainFile.project)
+        val session = resolutionFacade.useSiteFirSession
         val sessionHolder = SessionHolderImpl(session, session.getScopeSession())
 
-        val firFile = mainFile.getOrBuildFirFile(resolveSession)
+        val firFile = mainFile.getOrBuildFirFile(resolutionFacade)
 
         val targetElement = testServices.expressionMarkerProvider
             .getBottommostSelectedElementOfType(mainFile, KtElement::class)

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibSourcesLazyDeclarationResolveTest.kt
@@ -11,8 +11,8 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.Analys
 import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
 
 abstract class AbstractStdLibSourcesLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
-    override fun checkSession(firSession: LLResolutionFacade) {
-        requireIsInstance<KaLibrarySourceModule>(firSession.useSiteKtModule)
+    override fun checkSession(llResolutionFacade: LLResolutionFacade) {
+        requireIsInstance<KaLibrarySourceModule>(llResolutionFacade.useSiteKtModule)
     }
 
     override val configurator get() = AnalysisApiFirStdlibSourceTestConfigurator

JetBrains/kotlin

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirScopeProvider.kt
@@ -272,7 +272,7 @@ internal class KaFirScopeProvider(
 
     override val KtFile.importingScopeContext: KaScopeContext
         get() = withValidityAssertion {
-            val firFile = getOrBuildFirFile(firResolveSession)
+            val firFile = getOrBuildFirFile(resolutionFacade)
             val firFileSession = firFile.moduleData.session
             val firImportingScopes = createImportingScopes(
                 firFile,
@@ -296,7 +296,7 @@ internal class KaFirScopeProvider(
         val correctedPosition = parentKDoc?.owner ?: position
 
         val context = ContextCollector.process(
-            fakeFile.getOrBuildFirFile(firResolveSession),
+            fakeFile.getOrBuildFirFile(resolutionFacade),
             SessionHolderImpl(analysisSession.firSession, getScopeSession()),
             correctedPosition,
         )
@@ -406,7 +406,7 @@ internal class KaFirScopeProvider(
     }
 
     private fun getFirTypeScope(type: KaFirType): FirTypeScope? = type.coneType.scope(
-        firResolveSession.useSiteFirSession,
+        resolutionFacade.useSiteFirSession,
         getScopeSession(),
         CallableCopyTypeCalculator.CalculateDeferredForceLazyResolution,
         requiredMembersPhase = FirResolvePhase.STATUS,
@@ -414,7 +414,7 @@ internal class KaFirScopeProvider(
 
     private fun getFirSyntheticPropertiesScope(coneType: ConeKotlinType, typeScope: FirTypeScope): FirSyntheticPropertiesScope? =
         FirSyntheticPropertiesScope.createIfSyntheticNamesProviderIsDefined(
-            firResolveSession.useSiteFirSession,
+            resolutionFacade.useSiteFirSession,
             coneType,
             typeScope
         )

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/KaFirArrayAccessReference.kt
@@ -20,7 +20,7 @@ internal class KaFirArrayAccessReference(
     expression: KtArrayAccessExpression,
 ) : KtArrayAccessReference(expression), KaFirReference {
     override fun KaFirSession.computeSymbols(): Collection<KaSymbol> {
-        val fir = element.getOrBuildFir(firResolveSession) ?: return emptyList()
+        val fir = element.getOrBuildFir(resolutionFacade) ?: return emptyList()
         return when (fir) {
             is FirFunctionCall -> fir.getCandidateSymbols().map { it.fir.buildSymbol(firSymbolBuilder) }
             is FirResolvedNamedReference -> listOf(fir.resolvedSymbol.buildSymbol(firSymbolBuilder))

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirPsiJavaClassSymbol.kt
@@ -123,7 +123,7 @@ internal class KaFirPsiJavaClassSymbol(
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     override val lazyFirSymbol: Lazy<FirRegularClassSymbol> = lazyPub {
         val module = analysisSession.getModule(backingPsi)
-        val provider = analysisSession.firResolveSession.getSessionFor(module).firClassByPsiClassProvider
+        val provider = analysisSession.resolutionFacade.getSessionFor(module).firClassByPsiClassProvider
         provider.getFirClass(backingPsi)
     }
 

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirSymbolProvider.kt
@@ -198,7 +198,7 @@ internal class KaFirSymbolProvider(
         firSymbolBuilder.createPackageSymbolIfOneExists(fqName)
     }
 
-    private val backingRootPackageSymbol by lazy { KaFirPackageSymbol(FqName.ROOT, firResolveSession.project, token) }
+    private val backingRootPackageSymbol by lazy { KaFirPackageSymbol(FqName.ROOT, resolutionFacade.project, token) }
 
     override val rootPackageSymbol: KaPackageSymbol
         get() = withValidityAssertion { backingRootPackageSymbol }

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/LLFirResolveSessionService.kt
@@ -20,7 +20,7 @@ import org.jetbrains.kotlin.utils.exceptions.errorWithAttachment
 class LLFirResolveSessionService(project: Project) {
     private val cache = LLFirSessionCache.getInstance(project)
 
-    fun getFirResolveSession(module: KaModule): LLResolutionFacade {
+    fun getResolutionFacade(module: KaModule): LLResolutionFacade {
         return create(module, cache::getSession)
     }
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractByQualifiedNameLazyDeclarationResolveTest.kt
@@ -29,8 +29,8 @@ abstract class AbstractByQualifiedNameLazyDeclarationResolveTest : AbstractFirLa
     override fun doTestByMainModuleAndOptionalMainFile(mainFile: KtFile?, mainModule: KtTestModule, testServices: TestServices) {
         val psiFile = mainFile ?: mainModule.psiFiles.first()
         val classId = testServices.moduleStructure.allDirectives.singleValue(Directives.CLASS_ID).let(ClassId::fromString)
-        val resolveSession = LLFirResolveSessionService.getInstance(psiFile.project).getFirResolveSession(mainModule.ktModule)
-        val classDeclaration = findRegularClass(classId, mainModule.ktModule, resolveSession).findPsi(mainModule.ktModule.contentScope) as KtClassOrObject
+        val resolutionFacade = LLFirResolveSessionService.getInstance(psiFile.project).getResolutionFacade(mainModule.ktModule)
+        val classDeclaration = findRegularClass(classId, mainModule.ktModule, resolutionFacade).findPsi(mainModule.ktModule.contentScope) as KtClassOrObject
         val file = classDeclaration.containingFile as KtFile
 
         doLazyResolveTest(file, testServices, outputRenderingMode) { firSession ->
@@ -44,8 +44,8 @@ abstract class AbstractByQualifiedNameLazyDeclarationResolveTest : AbstractFirLa
 
     open val outputRenderingMode: OutputRenderingMode = OutputRenderingMode.USE_SITE_AND_DESIGNATION_FILES
 
-    private fun findRegularClass(classId: ClassId, module: KaModule, firSession: LLResolutionFacade): FirRegularClassSymbol {
-        return firSession.getSessionFor(module).getRegularClassSymbolByClassId(classId) ?: error("'$classId' is not found")
+    private fun findRegularClass(classId: ClassId, module: KaModule, resolutionFacade: LLResolutionFacade): FirRegularClassSymbol {
+        return resolutionFacade.getSessionFor(module).getRegularClassSymbolByClassId(classId) ?: error("'$classId' is not found")
     }
 
     private object Directives : SimpleDirectivesContainer() {

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractContextCollectorTest.kt
@@ -6,7 +6,7 @@
 package org.jetbrains.kotlin.analysis.low.level.api.fir
 
 import org.jetbrains.kotlin.analysis.api.projectStructure.copyOrigin
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getFirResolveSession
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getResolutionFacade
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFirFile
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirCustomScriptDefinitionTestConfigurator
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirSourceTestConfigurator
@@ -60,11 +60,11 @@ abstract class AbstractContextCollectorTest : AbstractAnalysisApiBasedTest() {
         testPrefixes: List<String>,
         useBodyElement: Boolean
     ) {
-        val resolveSession = mainModule.ktModule.getFirResolveSession(mainFile.project)
-        val session = resolveSession.useSiteFirSession
+        val resolutionFacade = mainModule.ktModule.getResolutionFacade(mainFile.project)
+        val session = resolutionFacade.useSiteFirSession
         val sessionHolder = SessionHolderImpl(session, session.getScopeSession())
 
-        val firFile = mainFile.getOrBuildFirFile(resolveSession)
+        val firFile = mainFile.getOrBuildFirFile(resolutionFacade)
 
         val targetElement = testServices.expressionMarkerProvider
             .getBottommostSelectedElementOfType(mainFile, KtElement::class)

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibSourcesLazyDeclarationResolveTest.kt
@@ -11,8 +11,8 @@ import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.Analys
 import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
 
 abstract class AbstractStdLibSourcesLazyDeclarationResolveTest : AbstractByQualifiedNameLazyDeclarationResolveTest() {
-    override fun checkSession(firSession: LLResolutionFacade) {
-        requireIsInstance<KaLibrarySourceModule>(firSession.useSiteKtModule)
+    override fun checkSession(resolutionFacade: LLResolutionFacade) {
+        requireIsInstance<KaLibrarySourceModule>(resolutionFacade.useSiteKtModule)
     }
 
     override val configurator get() = AnalysisApiFirStdlibSourceTestConfigurator