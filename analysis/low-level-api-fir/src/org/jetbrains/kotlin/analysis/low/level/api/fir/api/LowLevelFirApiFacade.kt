boost-mw-poc/jetbrains_kotlin

[LL] Rename `LLFirResolveSessionService` to `LLResolutionFacadeService`
Follow-up to the rename of `LLFirResolveSession` to `LLResolutionFacade`

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/api/LowLevelFirApiFacade.kt
@@ -6,7 +6,7 @@
 package org.jetbrains.kotlin.analysis.low.level.api.fir.api
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirResolveSessionService
+import org.jetbrains.kotlin.analysis.low.level.api.fir.LLResolutionFacadeService
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.diagnostics.KtPsiDiagnostic
 import org.jetbrains.kotlin.fir.FirElement
@@ -21,7 +21,7 @@ import org.jetbrains.kotlin.psi.KtFile
  * Returns [LLResolutionFacade] which corresponds to containing module
  */
 fun KaModule.getLLResolutionFacade(project: Project): LLResolutionFacade =
-    LLFirResolveSessionService.getInstance(project).getLLResolutionFacade(this)
+    LLResolutionFacadeService.getInstance(project).getLLResolutionFacade(this)
 
 
 /**

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractByQualifiedNameLazyDeclarationResolveTest.kt
@@ -29,7 +29,7 @@ abstract class AbstractByQualifiedNameLazyDeclarationResolveTest : AbstractFirLa
     override fun doTestByMainModuleAndOptionalMainFile(mainFile: KtFile?, mainModule: KtTestModule, testServices: TestServices) {
         val psiFile = mainFile ?: mainModule.psiFiles.first()
         val classId = testServices.moduleStructure.allDirectives.singleValue(Directives.CLASS_ID).let(ClassId::fromString)
-        val resolutionFacade = LLFirResolveSessionService.getInstance(psiFile.project).getLLResolutionFacade(mainModule.ktModule)
+        val resolutionFacade = LLResolutionFacadeService.getInstance(psiFile.project).getLLResolutionFacade(mainModule.ktModule)
         val classDeclaration = findRegularClass(classId, mainModule.ktModule, resolutionFacade).findPsi(mainModule.ktModule.contentScope) as KtClassOrObject
         val file = classDeclaration.containingFile as KtFile
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibBasedGetOrBuildFirTest.kt
@@ -33,7 +33,7 @@ abstract class AbstractStdLibBasedGetOrBuildFirTest : AbstractAnalysisApiBasedTe
                 ktReferences.first().resolveToSymbol()?.psi as KtDeclaration
             }
 
-        val resolutionFacade = LLFirResolveSessionService.getInstance(project).getLLResolutionFacade(mainModule.ktModule)
+        val resolutionFacade = LLResolutionFacadeService.getInstance(project).getLLResolutionFacade(mainModule.ktModule)
         val fir = declaration.resolveToFirSymbol(resolutionFacade).fir
         testServices.assertions.assertEqualsToTestOutputFile(renderActualFir(fir, declaration, testServices.firRenderingOptions))
     }

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/compiler/based/AbstractLLCompilerBasedTest.kt
@@ -9,7 +9,7 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.compiler.based
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
-import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirResolveSessionService
+import org.jetbrains.kotlin.analysis.low.level.api.fir.LLResolutionFacadeService
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirTestSuppressor
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.DiagnosticCheckerFilter
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFirFile
@@ -139,7 +139,7 @@ abstract class AbstractLLCompilerBasedTest : AbstractKotlinCompilerTest() {
             val ktModule = ktTestModule.ktModule as KtModuleByCompilerConfiguration
 
             val project = ktModule.project
-            val resolutionFacade = LLFirResolveSessionService.getInstance(project).getLLResolutionFacade(ktModule as KaModule)
+            val resolutionFacade = LLResolutionFacadeService.getInstance(project).getLLResolutionFacade(ktModule as KaModule)
 
             val allFirFiles = module.files.filter { it.isKtFile }.zip(
                 ktModule.psiFiles

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/firTestUtils.kt
@@ -49,7 +49,7 @@ internal inline fun <R> withResolveSession(context: KtElement, action: (LLResolu
 }
 
 internal inline fun <R> withResolveSession(module: KaModule, action: (LLResolutionFacade) -> R): R {
-    val resolutionFacade = LLFirResolveSessionService.getInstance(module.project).getLLResolutionFacade(module)
+    val resolutionFacade = LLResolutionFacadeService.getInstance(module.project).getLLResolutionFacade(module)
     return action(resolutionFacade)
 }
 

JetBrains/kotlin

analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/api/LowLevelFirApiFacade.kt
@@ -6,7 +6,7 @@
 package org.jetbrains.kotlin.analysis.low.level.api.fir.api
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirResolveSessionService
+import org.jetbrains.kotlin.analysis.low.level.api.fir.LLResolutionFacadeService
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.diagnostics.KtPsiDiagnostic
 import org.jetbrains.kotlin.fir.FirElement
@@ -21,7 +21,7 @@ import org.jetbrains.kotlin.psi.KtFile
  * Returns [LLResolutionFacade] which corresponds to containing module
  */
 fun KaModule.getResolutionFacade(project: Project): LLResolutionFacade =
-    LLFirResolveSessionService.getInstance(project).getResolutionFacade(this)
+    LLResolutionFacadeService.getInstance(project).getResolutionFacade(this)
 
 
 /**

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractByQualifiedNameLazyDeclarationResolveTest.kt
@@ -29,7 +29,7 @@ abstract class AbstractByQualifiedNameLazyDeclarationResolveTest : AbstractFirLa
     override fun doTestByMainModuleAndOptionalMainFile(mainFile: KtFile?, mainModule: KtTestModule, testServices: TestServices) {
         val psiFile = mainFile ?: mainModule.psiFiles.first()
         val classId = testServices.moduleStructure.allDirectives.singleValue(Directives.CLASS_ID).let(ClassId::fromString)
-        val resolutionFacade = LLFirResolveSessionService.getInstance(psiFile.project).getResolutionFacade(mainModule.ktModule)
+        val resolutionFacade = LLResolutionFacadeService.getInstance(psiFile.project).getResolutionFacade(mainModule.ktModule)
         val classDeclaration = findRegularClass(classId, mainModule.ktModule, resolutionFacade).findPsi(mainModule.ktModule.contentScope) as KtClassOrObject
         val file = classDeclaration.containingFile as KtFile
 

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibBasedGetOrBuildFirTest.kt
@@ -33,7 +33,7 @@ abstract class AbstractStdLibBasedGetOrBuildFirTest : AbstractAnalysisApiBasedTe
                 ktReferences.first().resolveToSymbol()?.psi as KtDeclaration
             }
 
-        val resolutionFacade = LLFirResolveSessionService.getInstance(project).getResolutionFacade(mainModule.ktModule)
+        val resolutionFacade = LLResolutionFacadeService.getInstance(project).getResolutionFacade(mainModule.ktModule)
         val fir = declaration.resolveToFirSymbol(resolutionFacade).fir
         testServices.assertions.assertEqualsToTestOutputFile(renderActualFir(fir, declaration, testServices.firRenderingOptions))
     }

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/compiler/based/AbstractLLCompilerBasedTest.kt
@@ -9,7 +9,7 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.compiler.based
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
-import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirResolveSessionService
+import org.jetbrains.kotlin.analysis.low.level.api.fir.LLResolutionFacadeService
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirTestSuppressor
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.DiagnosticCheckerFilter
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFirFile
@@ -139,7 +139,7 @@ abstract class AbstractLLCompilerBasedTest : AbstractKotlinCompilerTest() {
             val ktModule = ktTestModule.ktModule as KtModuleByCompilerConfiguration
 
             val project = ktModule.project
-            val resolutionFacade = LLFirResolveSessionService.getInstance(project).getResolutionFacade(ktModule as KaModule)
+            val resolutionFacade = LLResolutionFacadeService.getInstance(project).getResolutionFacade(ktModule as KaModule)
 
             val allFirFiles = module.files.filter { it.isKtFile }.zip(
                 ktModule.psiFiles

analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/firTestUtils.kt
@@ -49,7 +49,7 @@ internal inline fun <R> withResolveSession(context: KtElement, action: (LLResolu
 }
 
 internal inline fun <R> withResolveSession(module: KaModule, action: (LLResolutionFacade) -> R): R {
-    val resolutionFacade = LLFirResolveSessionService.getInstance(module.project).getResolutionFacade(module)
+    val resolutionFacade = LLResolutionFacadeService.getInstance(module.project).getResolutionFacade(module)
     return action(resolutionFacade)
 }
 