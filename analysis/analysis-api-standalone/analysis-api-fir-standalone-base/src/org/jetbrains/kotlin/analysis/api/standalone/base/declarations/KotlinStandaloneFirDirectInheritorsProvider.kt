3b0093b3c209d636f79e815c3b91c5a1251b2811
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-04-23 14:06:15 +0200

    [LL] Avoid querying `KotlinDirectInheritorsProvider` with dangling classes
    - The problem with KT-76952 was that `LLSealedInheritorsProvider` passed
      a class from a dangling file to the IDE's
      `KotlinDirectInheritorsProvider`, which the provider wasn't able to
      handle. See the issue for a detailed analysis.
    - `KotlinDirectInheritorsProvider` usually searches class inheritors in
      indices, which don't cover dangling files. To allow straight-forward
      implementations of the direct inheritors provider, we now avoid
      querying the provider with classes from dangling files.
    - `LLSealedInheritorsProvider` already passed the context module's scope
      to `KotlinDirectInheritorsProvider` instead of the dangling module's
      scope, but this didn't match up with the `ktClass`, which was still
      from the dangling file. `KotlinStandaloneFirDirectInheritorsProvider`
      actually mitigated this problem on its own by grabbing the base FIR
      class to compare with from the original source module. The same
      provider on the IDE side didn't implement the same mitigation. It just
      accidentally worked because of dangling elements being analyzable in
      a session of their context module (see KT-71135) and comparing
      inheritor candidate supertypes also by class ID instead of only by
      symbol.
    - Instead of putting the burden of handling dangling classes on the
      Analysis API platform implementation, we can put the burden on the use
      site. In this case, the only current usage is from
      `LLSealedInheritorsProvider`. And in general, it's easier to require
      special handling in LL FIR compared to all Analysis API platforms.
    - A known downside of the current approach is that dangling files cannot
      visibly change the hierarchy of sealed classes by adding or removing
      inheritors. But this hasn't been supported so far and can be
      implemented separately (see KT-77147). And we can actually handle this
      on the LL FIR side, since searching inheritors in a dangling file
      doesn't require platform-side index access, only file traversal. This
      is another point in favor of putting the dangling file handling on the
      LL FIR side.
    
    ^KT-76952 fixed

--- analysis/analysis-api-standalone/analysis-api-fir-standalone-base/src/org/jetbrains/kotlin/analysis/api/standalone/base/declarations/KotlinStandaloneFirDirectInheritorsProvider.kt
+++ analysis/analysis-api-standalone/analysis-api-fir-standalone-base/src/org/jetbrains/kotlin/analysis/api/standalone/base/declarations/KotlinStandaloneFirDirectInheritorsProvider.kt
@@ -11,7 +11,6 @@ import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.fir.utils.isSubclassOf
 import org.jetbrains.kotlin.analysis.low.level.api.fir.LLFirInternals
 import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSessionCache
-import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinProjectStructureProvider
 import org.jetbrains.kotlin.analysis.api.platform.declarations.KotlinDeclarationProviderFactory
@@ -43,30 +42,16 @@ internal class KotlinStandaloneFirDirectInheritorsProvider(private val project: 
         includeLocalInheritors: Boolean,
     ): Iterable<KtClassOrObject> {
         val classId = ktClass.getClassId() ?: return emptyList()
+        val baseModule = KotlinProjectStructureProvider.getModule(project, ktClass, useSiteModule = null)
+        val baseFirClass = classId.toFirSymbol(baseModule)?.fir as? FirClass ?: return emptyList()
 
-        val aliases = mutableSetOf(classId.shortClassName)
-        calculateAliases(classId.shortClassName, aliases)
+        val baseClassNames = mutableSetOf(classId.shortClassName)
+        calculateAliases(classId.shortClassName, baseClassNames)
 
-        val possibleInheritors = aliases.flatMap { standaloneDeclarationProviderFactory.getDirectInheritorCandidates(it) }
-
+        val possibleInheritors = baseClassNames.flatMap { standaloneDeclarationProviderFactory.getDirectInheritorCandidates(it) }
         if (possibleInheritors.isEmpty()) {
             return emptyList()
         }
-
-        // The index provides candidates from an original module, not dangling files. If we resolve the supertypes of a candidate in the
-        // context of its session, we will resolve to FIR classes from non-dangling, original modules. If `ktClass` is inside a dangling
-        // file, the FIR class for `ktClass` will come from the dangling module. So we'd compare the original FIR class for the supertype
-        // with the dangling FIR class for `ktClass`, resulting in a mismatch. To avoid such incompatible comparisons, we need to resolve
-        // `ktClass` to the original FIR class.
-        //
-        // Note that this means we don't support providing inheritors based on the dangling file yet, for example if an inheritor was added
-        // or removed only in the dangling file.
-        val baseKtModule = when (val ktModule = KotlinProjectStructureProvider.getModule(project, ktClass, useSiteModule = null)) {
-            is KaDanglingFileModule -> ktModule.contextModule
-            else -> ktModule
-        }
-
-        val baseFirClass = ktClass.toFirSymbol(classId, baseKtModule)?.fir as? FirClass ?: return emptyList()
         return possibleInheritors.filter { isValidInheritor(it, baseFirClass, scope, includeLocalInheritors) }
     }
 
@@ -96,8 +81,8 @@ internal class KotlinStandaloneFirDirectInheritorsProvider(private val project: 
         }
 
         val candidateClassId = candidate.getClassId() ?: return false
-        val candidateKtModule = KotlinProjectStructureProvider.getModule(project, candidate, useSiteModule = null)
-        val candidateFirSymbol = candidate.toFirSymbol(candidateClassId, candidateKtModule) ?: return false
+        val candidateModule = KotlinProjectStructureProvider.getModule(project, candidate, useSiteModule = null)
+        val candidateFirSymbol = candidateClassId.toFirSymbol(candidateModule) ?: return false
         val candidateFirClass = candidateFirSymbol.fir as? FirClass ?: return false
 
         // `KotlinDirectInheritorsProvider`'s interface guarantees that `getDirectKotlinInheritors` is only called from lazy resolution to
@@ -105,11 +90,11 @@ internal class KotlinStandaloneFirDirectInheritorsProvider(private val project: 
         return isSubclassOf(candidateFirClass, baseFirClass, candidateFirClass.moduleData.session, allowIndirectSubtyping = false)
     }
 
-    private fun KtClassOrObject.toFirSymbol(classId: ClassId, ktModule: KaModule): FirClassLikeSymbol<*>? {
+    private fun ClassId.toFirSymbol(module: KaModule): FirClassLikeSymbol<*>? {
         // Using a resolve session/source-preferred session will cause class stubs from binary libraries to be AST-loaded in IDE mode tests,
         // which results in an exception since we don't have a decompiler for them. See KT-64898, KT-64899, and KT-64900. If not for these
         // issues, we would be able to use `analyze` instead of custom session logic.
-        val session = LLFirSessionCache.getInstance(project).getSession(ktModule, preferBinary = true)
-        return session.symbolProvider.getClassLikeSymbolByClassId(classId)
+        val session = LLFirSessionCache.getInstance(project).getSession(module, preferBinary = true)
+        return session.symbolProvider.getClassLikeSymbolByClassId(this)
     }
 }
--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/providers/LLSealedInheritorsProvider.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/providers/LLSealedInheritorsProvider.kt
@@ -11,6 +11,8 @@ import org.jetbrains.kotlin.analysis.api.platform.declarations.KotlinDirectInher
 import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinModuleDependentsProvider
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaDanglingFileModule
 import org.jetbrains.kotlin.analysis.low.level.api.fir.projectStructure.llFirModuleData
+import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSessionCache
+import org.jetbrains.kotlin.analysis.low.level.api.fir.symbolProviders.symbolProvider
 import org.jetbrains.kotlin.fir.declarations.FirClass
 import org.jetbrains.kotlin.fir.declarations.FirRegularClass
 import org.jetbrains.kotlin.fir.declarations.SealedClassInheritorsProvider
@@ -70,22 +72,29 @@ internal class LLSealedInheritorsProvider(private val project: Project) : Sealed
      *    See KT-65591.
      */
     private fun searchInheritors(firClass: FirClass): List<ClassId> {
-        val ktClass = firClass.psi as? KtClass ?: return emptyList()
-
-        val ktModule = when (val classKtModule = firClass.llFirModuleData.ktModule) {
-            is KaDanglingFileModule -> classKtModule.contextModule
-            else -> classKtModule
+        val (targetModule, targetFirClass) = when (val classModule = firClass.llFirModuleData.ktModule) {
+            is KaDanglingFileModule -> {
+                // Since we are searching for inheritors in the `contextModule`'s scope, we need to search for inheritors of the *original*
+                // FIR class, not the dangling FIR class.
+                val contextModule = classModule.contextModule
+                val contextSession = LLFirSessionCache.getInstance(project).getSession(contextModule, preferBinary = true)
+                val originalFirSymbol = contextSession.symbolProvider.getClassLikeSymbolByClassId(firClass.classId)
+                val originalFirClass = originalFirSymbol?.fir as? FirClass ?: return emptyList()
+                contextModule to originalFirClass
+            }
+            else -> classModule to firClass
         }
+        val targetKtClass = targetFirClass.psi as? KtClass ?: return emptyList()
 
         // `FirClass.isExpect` does not depend on the `STATUS` phase because it's already set during FIR building.
-        val scope = if (firClass.isExpect) {
-            val refinementDependents = KotlinModuleDependentsProvider.getInstance(project).getRefinementDependents(ktModule)
-            GlobalSearchScope.union(refinementDependents.map { it.contentScope } + ktModule.contentScope)
+        val scope = if (targetFirClass.isExpect) {
+            val refinementDependents = KotlinModuleDependentsProvider.getInstance(project).getRefinementDependents(targetModule)
+            GlobalSearchScope.union(refinementDependents.map { it.contentScope } + targetModule.contentScope)
         } else {
-            ktModule.contentScope
+            targetModule.contentScope
         }
 
-        return searchInScope(ktClass, firClass.classId, scope)
+        return searchInScope(targetKtClass, targetFirClass.classId, scope)
     }
 
     private fun searchInScope(ktClass: KtClass, classId: ClassId, scope: GlobalSearchScope): List<ClassId> =
