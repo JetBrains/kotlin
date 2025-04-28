33d3cfbab838e153b672c9dccb5f7090e1be7b34
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-04-28 16:39:32 +0200

    [Tests] Enable remaining synthetic accessor tests of callable references
    They were fixed by KT-72734
    
    KT-69797

--- compiler/testData/klib/syntheticAccessors/privateMember/crossFilePrivateLeak/leakingPrivateMethodThroughReference.kt
+++ compiler/testData/klib/syntheticAccessors/privateMember/crossFilePrivateLeak/leakingPrivateMethodThroughReference.kt
@@ -1,12 +1,8 @@
-// IGNORE_BACKEND: ANY
-// ^^^ Muted because accessor for function/constructor/property references are not generated. To be fixed in KT-69797.
-// IGNORE_KLIB_SYNTHETIC_ACCESSORS_CHECKS: ANY
-// ^^^ error: public-API inline function cannot access non-public-API function.
-
 // FILE: A.kt
 class A {
     private fun privateMethod() = "OK"
 
+    @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
     public inline fun publicInlineFunction() = ::privateMethod
 }
 
