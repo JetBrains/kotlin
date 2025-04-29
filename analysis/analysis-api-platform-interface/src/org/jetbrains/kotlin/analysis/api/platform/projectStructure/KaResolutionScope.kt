89fd0fe729077125da4c1707214961b0c7f55bc3
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-04-29 15:49:42 +0200

    [AA] Cleanup & update KDoc in `KaResolutionScope`
    ^KT-62474

--- analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/projectStructure/KaResolutionScope.kt
+++ analysis/analysis-api-platform-interface/src/org/jetbrains/kotlin/analysis/api/platform/projectStructure/KaResolutionScope.kt
@@ -11,23 +11,21 @@ import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 
 /**
- * A scope used for resolving files inside [useSiteModule].
- * This resulting scope is a union of [useSiteModule]'s content scope and content scopes of modules [useSiteModule] depends on.
+ * The resolution scope for a [KaModule]. It determines the PSI elements that can be resolved in the context of an analysis session.
+ *
+ * A resolution scope is mainly built from its use-site module's [KaModule.contentScope] and its dependencies' content scopes.
  */
 public abstract class KaResolutionScope : GlobalSearchScope() {
     /**
-     * Checks whether a [PsiElement] is contained in the resolution scope,
-     * i.e., the containing file is present in the resolution scope.
+     * Checks whether a [PsiElement] is contained in the resolution scope, i.e., the containing file is present in the resolution scope.
      *
-     * In contrast to [GlobalSearchScope.contains],
-     * this method provides an additional support for [PsiElement]s contained
-     * in dangling files that do not have backing virtual files.
+     * In contrast to [GlobalSearchScope.contains], this method provides additional support for [PsiElement]s contained in dangling files
+     * that do not have backing virtual files.
      */
     public abstract fun contains(element: PsiElement): Boolean
 
     public companion object {
-        public fun forModule(useSiteModule: KaModule): KaResolutionScope {
-            return KaResolutionScopeProvider.getInstance(useSiteModule.project).getResolutionScope(useSiteModule)
-        }
+        public fun forModule(useSiteModule: KaModule): KaResolutionScope =
+            KaResolutionScopeProvider.getInstance(useSiteModule.project).getResolutionScope(useSiteModule)
     }
 }
