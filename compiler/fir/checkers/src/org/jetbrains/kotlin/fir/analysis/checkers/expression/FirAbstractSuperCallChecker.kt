cbf8a587794ce3354a649d040dc5fbe7c9eba829
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-04-29 12:57:19 +0300

    [FIR] Report `ABSTRACT_SUPER_CALL` regardless of the `classKind`
    These checks were probably added by mistake when the checker was first
    implemented.
    
    ^KT-76365 Fixed

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirAbstractSuperCallChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/expression/FirAbstractSuperCallChecker.kt
@@ -7,23 +7,18 @@ package org.jetbrains.kotlin.fir.analysis.checkers.expression
 
 import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.descriptors.ClassKind
-import org.jetbrains.kotlin.descriptors.isClass
-import org.jetbrains.kotlin.descriptors.isObject
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.analysis.checkers.MppCheckerKind
 import org.jetbrains.kotlin.fir.analysis.checkers.classKind
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
-import org.jetbrains.kotlin.fir.analysis.checkers.context.findClosest
 import org.jetbrains.kotlin.fir.analysis.checkers.explicitReceiverIsNotSuperReference
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.containingClassLookupTag
 import org.jetbrains.kotlin.fir.declarations.utils.isAbstract
 import org.jetbrains.kotlin.fir.expressions.FirQualifiedAccessExpression
 import org.jetbrains.kotlin.fir.expressions.toResolvedCallableSymbol
-import org.jetbrains.kotlin.fir.resolve.toRegularClassSymbol
 import org.jetbrains.kotlin.fir.resolve.toSymbol
-import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirIntersectionCallableSymbol
 
 object FirAbstractSuperCallChecker : FirQualifiedAccessExpressionChecker(MppCheckerKind.Common) {
@@ -32,29 +27,19 @@ object FirAbstractSuperCallChecker : FirQualifiedAccessExpressionChecker(MppChec
         // require the receiver to be the super reference
         if (expression.explicitReceiverIsNotSuperReference()) return
 
-        val closestClass = context.findClosest<FirClassSymbol<*>>() ?: return
+        val declarationSymbol = expression.toResolvedCallableSymbol() ?: return
 
-        if (closestClass.classKind.isClass || closestClass.classKind.isObject) {
-            // handles all the FirSimpleFunction/FirProperty/etc.
-            val declarationSymbol = expression.toResolvedCallableSymbol() ?: return
-
-            val containingClassSymbol = declarationSymbol.containingClassLookupTag()?.toRegularClassSymbol(context.session) ?: return
-
-            if (containingClassSymbol.isAbstract) {
-                if (declarationSymbol.isAbstract) {
+        if (declarationSymbol.isAbstract) {
+            reporter.reportOn(expression.calleeReference.source, FirErrors.ABSTRACT_SUPER_CALL)
+        } else if (declarationSymbol is FirIntersectionCallableSymbol) {
+            val symbolFromBaseClass = declarationSymbol.intersections.firstOrNull {
+                it.containingClassLookupTag()?.toSymbol(context.session)?.classKind != ClassKind.INTERFACE
+            }
+            if (symbolFromBaseClass?.isAbstract == true) {
+                if (context.languageVersionSettings.supportsFeature(LanguageFeature.ForbidSuperDelegationToAbstractFakeOverride)) {
                     reporter.reportOn(expression.calleeReference.source, FirErrors.ABSTRACT_SUPER_CALL)
-                }
-                if (declarationSymbol is FirIntersectionCallableSymbol) {
-                    val symbolFromBaseClass = declarationSymbol.intersections.firstOrNull {
-                        it.containingClassLookupTag()?.toSymbol(context.session)?.classKind != ClassKind.INTERFACE
-                    }
-                    if (symbolFromBaseClass?.isAbstract == true) {
-                        if (context.languageVersionSettings.supportsFeature(LanguageFeature.ForbidSuperDelegationToAbstractFakeOverride)) {
-                            reporter.reportOn(expression.calleeReference.source, FirErrors.ABSTRACT_SUPER_CALL)
-                        } else {
-                            reporter.reportOn(expression.calleeReference.source, FirErrors.ABSTRACT_SUPER_CALL_WARNING)
-                        }
-                    }
+                } else {
+                    reporter.reportOn(expression.calleeReference.source, FirErrors.ABSTRACT_SUPER_CALL_WARNING)
                 }
             }
         }
