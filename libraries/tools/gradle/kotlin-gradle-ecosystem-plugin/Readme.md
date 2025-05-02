a0064fe11f5ef9abe192cc3054867fead2d11e9a
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-02 16:56:36 +0200

    [Gradle] Add 'org.jetbrains.kotlin.ecosystem' settings plugin
    This plugin should provide a project-wide Kotlin configuration and
    specifically support for Declarative Gradle.
    
    ^KT-77107 In Progress

---
+++ libraries/tools/gradle/kotlin-gradle-ecosystem-plugin/Readme.md
@@ -0,0 +1,4 @@
+# Description
+
+Contains 'org.jetbrains.kotlin.ecosystem' Gradle settings plugin. This plugin provides project-wide 
+Kotlin configuration.
---
+++ libraries/tools/gradle/kotlin-gradle-ecosystem-plugin/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("gradle-plugin-common-configuration")
+}
+
+gradlePlugin {
+    plugins {
+        create("kotlin-ecosystem-plugin") {
+            id = "org.jetbrains.kotlin.ecosystem"
+            displayName = "Kotlin Ecosystem plugin"
+            description = "Gradle settings plugin providing project wide Kotlin configuration"
+            implementationClass = "org.jetbrains.kotlin.gradle.ecosystem.KotlinEcosystemPlugin"
+        }
+    }
+}
