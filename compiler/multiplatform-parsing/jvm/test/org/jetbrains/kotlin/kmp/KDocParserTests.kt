2a9341e63c11ac47a14df1a648dd697a0356db42
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2025-05-03 16:30:42 +0200

    [KMP Parser] Print single-line syntax element values in dumps for clarity

--- compiler/multiplatform-parsing/jvm/test/org/jetbrains/kotlin/kmp/KDocParserTests.kt
+++ compiler/multiplatform-parsing/jvm/test/org/jetbrains/kotlin/kmp/KDocParserTests.kt
@@ -70,38 +70,38 @@ class KDocParserTests {
     private fun testParseNodesDump(parser: AbstractTestParser<*>) {
         assertEquals(
             """KDoc [7:1..10:4)
-  KDOC_START [7:1..4)
+  KDOC_START `/**` [7:1..4)
   WHITE_SPACE [7:4..8:2)
   KDOC_SECTION [8:2..9:23)
-    KDOC_LEADING_ASTERISK [8:2..3)
-    KDOC_TEXT [8:3..4)
-    KDOC_TAG [8:4..32)
-      KDOC_TAG_NAME [8:4..10)
-      WHITE_SPACE [8:10..11)
-      KDOC_MARKDOWN_LINK [8:11..16)
-        LBRACKET [8:11..12)
-        KDOC_NAME [8:12..15)
-          KDOC_NAME [8:12..13)
-            IDENTIFIER [8:12..13)
-          DOT [8:13..14)
-          IDENTIFIER [8:14..15)
-        RBRACKET [8:15..16)
-      WHITE_SPACE [8:16..17)
-      KDOC_TEXT [8:17..32)
+    KDOC_LEADING_ASTERISK `*` [8:2..3)
+    KDOC_TEXT ` ` [8:3..4)
+    KDOC_TAG `@param [C.x] Some parameter.` [8:4..32)
+      KDOC_TAG_NAME `@param` [8:4..10)
+      WHITE_SPACE ` ` [8:10..11)
+      KDOC_MARKDOWN_LINK `[C.x]` [8:11..16)
+        LBRACKET `[` [8:11..12)
+        KDOC_NAME `C.x` [8:12..15)
+          KDOC_NAME `C` [8:12..13)
+            IDENTIFIER `C` [8:12..13)
+          DOT `.` [8:13..14)
+          IDENTIFIER `x` [8:14..15)
+        RBRACKET `]` [8:15..16)
+      WHITE_SPACE ` ` [8:16..17)
+      KDOC_TEXT `Some parameter.` [8:17..32)
     WHITE_SPACE [8:32..9:2)
-    KDOC_LEADING_ASTERISK [9:2..3)
-    KDOC_TEXT [9:3..4)
-    KDOC_TAG [9:4..23)
-      KDOC_TAG_NAME [9:4..11)
-      WHITE_SPACE [9:11..12)
-      KDOC_MARKDOWN_LINK [9:12..23)
-        LBRACKET [9:12..13)
-        KDOC_NAME [9:13..22)
-          IDENTIFIER [9:13..22)
-        RBRACKET [9:22..23)
+    KDOC_LEADING_ASTERISK `*` [9:2..3)
+    KDOC_TEXT ` ` [9:3..4)
+    KDOC_TAG `@return [Exception]` [9:4..23)
+      KDOC_TAG_NAME `@return` [9:4..11)
+      WHITE_SPACE ` ` [9:11..12)
+      KDOC_MARKDOWN_LINK `[Exception]` [9:12..23)
+        LBRACKET `[` [9:12..13)
+        KDOC_NAME `Exception` [9:13..22)
+          IDENTIFIER `Exception` [9:13..22)
+        RBRACKET `]` [9:22..23)
   WHITE_SPACE [9:23..10:2)
-  KDOC_END [10:2..4)""",
-            parser.parseKDocOnlyNodes("kotlinCodeSample.kt", kotlinCodeSample).dump(kotlinCodeSample.toSourceLinesMapping())
+  KDOC_END `*/` [10:2..4)""",
+            parser.parseKDocOnlyNodes("kotlinCodeSample.kt", kotlinCodeSample).dump(kotlinCodeSample.toSourceLinesMapping(), kotlinCodeSample)
         )
     }
 
@@ -150,8 +150,8 @@ class KDocParserTests {
 
         fun failWithDifferentParseTrees() {
             assertEquals(
-                oldKDocTrees.dump(sourceLinesMapping),
-                newKDocTrees.dump(sourceLinesMapping),
+                oldKDocTrees.dump(sourceLinesMapping, kotlinCodeSample),
+                newKDocTrees.dump(sourceLinesMapping, kotlinCodeSample),
                 path?.let { "Different parse tree nodes on file: $it" }
             )
         }
