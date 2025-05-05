1d3d204d64844e8a47a0af33ce32ec83522eb3ec
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-05 15:31:17 +0200

    [Args] Move ReturnValueCheckerModeType to the proper package
    ^KT-75812 In Progress

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinArgumentValueType.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinArgumentValueType.kt
@@ -9,8 +9,9 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.kotlin.arguments.dsl.base.KotlinReleaseVersion
 import org.jetbrains.kotlin.arguments.dsl.base.ReleaseDependent
 import org.jetbrains.kotlin.config.ExplicitApiMode
+import org.jetbrains.kotlin.config.ReturnValueCheckerMode
 
-interface KotlinArgumentValueType<T : Any> {
+sealed interface KotlinArgumentValueType<T : Any> {
     val isNullable: ReleaseDependent<Boolean>
     val defaultValue: ReleaseDependent<T?>
 
@@ -92,6 +93,16 @@ class ExplicitApiModeType(
     override val defaultValue: ReleaseDependent<ExplicitApiMode?> = ReleaseDependent(ExplicitApiMode.DISABLED),
 ) : KotlinArgumentValueType<ExplicitApiMode> {
     override fun stringRepresentation(value: ExplicitApiMode?): String {
+        return value?.state.valueOrNullStringLiteral
+    }
+}
+
+@Serializable
+class ReturnValueCheckerModeType(
+    override val isNullable: ReleaseDependent<Boolean> = ReleaseDependent(false),
+    override val defaultValue: ReleaseDependent<ReturnValueCheckerMode?> = ReleaseDependent(ReturnValueCheckerMode.DISABLED),
+) : KotlinArgumentValueType<ReturnValueCheckerMode> {
+    override fun stringRepresentation(value: ReturnValueCheckerMode?): String {
         return value?.state.valueOrNullStringLiteral
     }
 }
