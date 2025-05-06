18a9dbd26f25109980ee5e71557879c289cc62de
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-05-06 10:00:32 +0200

    ~ Proguard?

--- prepare/klib-abi-reader-embeddable/build.gradle.kts
+++ prepare/klib-abi-reader-embeddable/build.gradle.kts
@@ -1,21 +1,102 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.internal.jvm.Jvm
+
 description = "Kotlin KLIB abi reader embeddable"
 
 plugins {
     kotlin("jvm")
+    id("jps-compatible")
+}
+
+val jarBaseName = the<BasePluginExtension>().archivesName
+
+val localPackagesToRelocate = listOf<String>()
+
+val proguardLibraryJars by configurations.creating {
+    attributes {
+        attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.JAVA_RUNTIME))
+        attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.JAR))
+    }
+}
+
+val embedded by configurations
+
+val relocatedJarContents by configurations.creating {
+    extendsFrom(embedded)
 }
 
 dependencies {
+    runtimeOnly(kotlinStdlib())
+    /*embedded(intellijCore()) {
+        isTransitive = true
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }*/
     embedded(project(":kotlin-util-klib-abi")) {
         isTransitive = true
         exclude("org.jetbrains.kotlin", "kotlin-stdlib")
     }
-}
 
-sourceSets {
-    "main" {}
-    "test" { projectDefault() }
+    proguardLibraryJars(kotlinStdlib())
+    proguardLibraryJars(commonDependency("org.jetbrains.kotlin:kotlin-reflect")) { isTransitive = false }
+    //proguardLibraryJars(project(":kotlin-compiler"))
+
+    relocatedJarContents(mainSourceSet.output)
 }
 
 publish()
 
-runtimeJar()
+noDefaultJar()
+
+val relocatedJar by task<ShadowJar> {
+    configurations = listOf(relocatedJarContents)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    destinationDirectory.set(layout.buildDirectory.dir("libs"))
+    archiveClassifier.set("before-proguard")
+
+    // don't add this files to resources classpath to avoid IDE exceptions on kotlin project
+    from("jar-resources")
+
+    if (kotlinBuildProperties.relocation) {
+        (packagesToRelocate + localPackagesToRelocate).forEach {
+            relocate(it, "$kotlinEmbeddableRootPackage.$it")
+        }
+    }
+}
+
+val proguard by task<CacheableProguardTask> {
+    dependsOn(relocatedJar)
+    configuration("klib-abi-reader.pro")
+
+    injars(mapOf("filter" to "!META-INF/versions/**,!kotlinx/coroutines/debug/**"), relocatedJar.get().outputs.files)
+
+    outjars(layout.buildDirectory.file(jarBaseName.map { "libs/$it-$version-after-proguard.jar" }))
+
+    javaLauncher.set(project.getToolchainLauncherFor(JdkMajorVersion.JDK_1_8))
+
+    libraryjars(mapOf("filter" to "!META-INF/versions/**"), proguardLibraryJars)
+    libraryjars(
+        files(
+            javaLauncher.map {
+                firstFromJavaHomeThatExists(
+                    "jre/lib/rt.jar",
+                    "../Classes/classes.jar",
+                    jdkHome = it.metadata.installationPath.asFile
+                )!!
+            },
+            javaLauncher.map {
+                Jvm.forHome(it.metadata.installationPath.asFile).toolsJar!!
+            }
+        )
+    )
+}
+
+val resultJar by task<Jar> {
+    val pack = if (kotlinBuildProperties.proguard) proguard else relocatedJar
+    dependsOn(pack)
+    setupPublicJar(jarBaseName)
+    from {
+        zipTree(pack.map { it.singleOutputFile(layout) })
+    }
+}
+
+setPublishableArtifact(resultJar)
