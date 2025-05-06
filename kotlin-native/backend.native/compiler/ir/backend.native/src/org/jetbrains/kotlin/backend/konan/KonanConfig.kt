Tencent-TDS/KuiklyBase-kotlin

[K/N] Ignore caches built by the wrong distribution
^KT-72122

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
@@ -14,6 +14,8 @@ import org.jetbrains.kotlin.backend.konan.objcexport.ObjCEntryPoints
 import org.jetbrains.kotlin.backend.konan.objcexport.readObjCEntryPoints
 import org.jetbrains.kotlin.backend.konan.serialization.KonanUserVisibleIrModulesSupport
 import org.jetbrains.kotlin.backend.konan.serialization.PartialCacheInfo
+import org.jetbrains.kotlin.backend.konan.util.compilerFingerprint
+import org.jetbrains.kotlin.backend.konan.util.runtimeFingerprint
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
@@ -600,7 +602,9 @@ class KonanConfig(val project: Project, val configuration: CompilerConfiguration
             autoCacheDirectory = autoCacheDirectory,
             incrementalCacheDirectory = incrementalCacheDirectory,
             target = target,
-            produce = produce
+            produce = produce,
+            compilerFingerprint = distribution.compilerFingerprint,
+            targetFingerprint = distribution.runtimeFingerprint(target),
     )
 
     internal val cachedLibraries: CachedLibraries

JetBrains/kotlin

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanConfig.kt
@@ -14,6 +14,8 @@ import org.jetbrains.kotlin.backend.konan.objcexport.ObjCEntryPoints
 import org.jetbrains.kotlin.backend.konan.objcexport.readObjCEntryPoints
 import org.jetbrains.kotlin.backend.konan.serialization.KonanUserVisibleIrModulesSupport
 import org.jetbrains.kotlin.backend.konan.serialization.PartialCacheInfo
+import org.jetbrains.kotlin.backend.konan.util.compilerFingerprint
+import org.jetbrains.kotlin.backend.konan.util.runtimeFingerprint
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.config.CommonConfigurationKeys
 import org.jetbrains.kotlin.config.CompilerConfiguration
@@ -609,7 +611,9 @@ class KonanConfig(val project: Project, val configuration: CompilerConfiguration
             autoCacheDirectory = autoCacheDirectory,
             incrementalCacheDirectory = incrementalCacheDirectory,
             target = target,
-            produce = produce
+            produce = produce,
+            compilerFingerprint = distribution.compilerFingerprint,
+            runtimeFingerprint = distribution.runtimeFingerprint(target),
     )
 
     internal val cachedLibraries: CachedLibraries