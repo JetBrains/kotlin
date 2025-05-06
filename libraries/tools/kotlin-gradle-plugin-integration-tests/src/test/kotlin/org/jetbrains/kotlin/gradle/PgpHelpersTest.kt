09749f0cae226c44c81c10a6755412b397f39d8b
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2025-05-06 16:22:57 +0200

    fixup! KT-77034: Generate and upload PGP keys tasks

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/PgpHelpersTest.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/PgpHelpersTest.kt
@@ -12,18 +12,18 @@ import io.ktor.server.engine.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.util.pipeline.PipelineInterceptor
-import org.gradle.testkit.runner.BuildResult
+import io.ktor.util.pipeline.*
+import org.gradle.kotlin.dsl.kotlin
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.testbase.*
 import org.jetbrains.kotlin.gradle.util.awaitInitialization
 import org.jetbrains.kotlin.gradle.util.getEmptyPort
 import org.junit.jupiter.api.DisplayName
-import java.nio.file.Path
-import kotlin.collections.plusAssign
+import java.security.Security
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.readText
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 @OtherGradlePluginTests
 @DisplayName("PGP signing helper tasks")
@@ -32,10 +32,7 @@ class PgpHelpersTest : KGPBaseTest() {
     @GradleTest
     @DisplayName("Should complain about missing name parameter")
     internal fun shouldComplainAboutMissingName(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpers",
-            gradleVersion = gradleVersion,
-        ) {
+        projectWithJvmPlugin(gradleVersion) {
             buildAndFail("generatePgpKeys", "--password=abc") {
                 assertOutputContains("You must provide a value for the '--name' command line option, e.g. --name \"Jane Doe <janedoe@example.com>\"")
             }
@@ -45,10 +42,7 @@ class PgpHelpersTest : KGPBaseTest() {
     @GradleTest
     @DisplayName("Should complain about missing password parameter")
     internal fun shouldComplainAboutMissingPassword(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpers",
-            gradleVersion = gradleVersion,
-        ) {
+        projectWithJvmPlugin(gradleVersion) {
             buildAndFail("generatePgpKeys", "--name='Jane Doe <janedoe@example.com>'") {
                 assertOutputContains("You must provide a value for either the '--password' command line option or the 'signing.password' Gradle property")
             }
@@ -58,12 +52,9 @@ class PgpHelpersTest : KGPBaseTest() {
     @GradleTest
     @DisplayName("Should read password supplied through CLI option")
     internal fun passwordSuppliedThroughOption(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpers",
-            gradleVersion = gradleVersion,
-        ) {
+        projectWithJvmPlugin(gradleVersion) {
             build("generatePgpKeys", "--name='Jane Doe <janedoe@example.com>'", "--password=abc") {
-                assertPgpKeysWereGenerated(projectPath)
+                assertPgpKeysWereGenerated()
             }
         }
     }
@@ -71,12 +62,9 @@ class PgpHelpersTest : KGPBaseTest() {
     @GradleTest
     @DisplayName("Should read password supplied through project property")
     internal fun passwordSuppliedThroughGradleProperty(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpers",
-            gradleVersion = gradleVersion,
-        ) {
+        projectWithJvmPlugin(gradleVersion) {
             build("generatePgpKeys", "--name='Jane Doe <janedoe@example.com>'", "-Psigning.password=abc") {
-                assertPgpKeysWereGenerated(projectPath)
+                assertPgpKeysWereGenerated()
             }
         }
     }
@@ -84,24 +72,28 @@ class PgpHelpersTest : KGPBaseTest() {
     @GradleTest
     @DisplayName("Should not leak BouncyCastle dependency")
     internal fun shouldNotLeakBouncyCastleDependency(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpersClassloaderIsolation",
-            gradleVersion = gradleVersion,
-        ) {
-            build("generatePgpKeys", "--name='Jane Doe <janedoe@example.com>'", "-Psigning.password=abc") {
-                assertOutputContains("PGPContentSignerBuilder class not found")
-                assertOutputContains("Security provider is: null")
-            }
+        projectWithJvmPlugin(gradleVersion) {
+            val classesLeaked = buildScriptReturn {
+                try {
+                    this::class.java.classLoader.loadClass("org.bouncycastle.openpgp.operator.PGPContentSignerBuilder")
+                    return@buildScriptReturn true
+                } catch (_: ClassNotFoundException) {
+                    // ignore
+                }
+                if (Security.getProvider("BC") != null) {
+                    return@buildScriptReturn true
+                }
+                false
+            }.buildAndReturn("generatePgpKeys", "--name='Jane Doe <janedoe@example.com>'", "-Psigning.password=abc")
+
+            assertFalse(classesLeaked)
         }
     }
 
     @GradleTest
     @DisplayName("Should upload public key to server")
     internal fun uploadPublicKeyToServer(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpers",
-            gradleVersion = gradleVersion,
-        ) {
+        projectWithJvmPlugin(gradleVersion) {
             build(
                 "generatePgpKeys",
                 "--name='Jane Doe <janedoe@example.com>'",
@@ -109,11 +101,13 @@ class PgpHelpersTest : KGPBaseTest() {
             )
             val parameters = mutableListOf<Parameters>()
 
-            runWithKtorService({
-                                   val formParameters: Parameters = call.receiveParameters()
-                                   parameters += formParameters
-                                   call.respond(HttpStatusCode.OK)
-                               }) { port ->
+            runWithKtorService(
+                {
+                    val formParameters: Parameters = call.receiveParameters()
+                    parameters += formParameters
+                    call.respond(HttpStatusCode.OK)
+                }
+            ) { port ->
                 build(
                     "uploadPublicPgpKey",
                     "--keyServer",
@@ -130,18 +124,17 @@ class PgpHelpersTest : KGPBaseTest() {
     @GradleTest
     @DisplayName("Should fail task when upload public key to server fails")
     internal fun failedUploadPublicKeyToServer(gradleVersion: GradleVersion) {
-        project(
-            projectName = "signingHelpers",
-            gradleVersion = gradleVersion,
-        ) {
+        projectWithJvmPlugin(gradleVersion) {
             build(
                 "generatePgpKeys",
                 "--name='Jane Doe <janedoe@example.com>'",
                 "-Psigning.password=abc",
             )
-            runWithKtorService({
-                                   call.respond(HttpStatusCode.BadRequest, "Some reason")
-                               }) { port ->
+            runWithKtorService(
+                {
+                    call.respond(HttpStatusCode.BadRequest, "Some reason")
+                }
+            ) { port ->
                 buildAndFail(
                     "uploadPublicPgpKey",
                     "--keyServer",
@@ -153,39 +146,41 @@ class PgpHelpersTest : KGPBaseTest() {
         }
     }
 
-    private fun BuildResult.assertJdkHomeIsUsingJdk(
-        javaexecPath: String,
-    ) = assertOutputContains("[KOTLIN] Kotlin compilation 'jdkHome' argument: $javaexecPath")
-
-
-    private fun BuildResult.assertPgpKeysWereGenerated(projectPath: Path) {
+    private fun TestProject.assertPgpKeysWereGenerated() {
         val expectedFileNames = setOf("secret.gpg", "secret.asc", "public.gpg", "public.asc", "example.properties")
         val actualFileNames = projectPath.resolve("build/pgp").listDirectoryEntries().map { it.fileName.toString() }.toSet()
         assertEquals(expectedFileNames, actualFileNames)
     }
 
-    companion object {
-        private fun runWithKtorService(
-            addEndpointAction: PipelineInterceptor<Unit, ApplicationCall>,
-            action: (Int) -> Unit,
-        ) {
-            var server: ApplicationEngine? = null
-            try {
-                val port = getEmptyPort().localPort
-                server = embeddedServer(CIO, host = "localhost", port = port)
-                {
-                    routing {
-                        get("/isReady") {
-                            call.respond(HttpStatusCode.OK)
-                        }
-                        post("/pks/add", addEndpointAction)
+    private fun runWithKtorService(
+        addEndpointAction: PipelineInterceptor<Unit, ApplicationCall>,
+        action: (Int) -> Unit,
+    ) {
+        var server: ApplicationEngine? = null
+        try {
+            val port = getEmptyPort().localPort
+            server = embeddedServer(CIO, host = "localhost", port = port)
+            {
+                routing {
+                    get("/isReady") {
+                        call.respond(HttpStatusCode.OK)
                     }
-                }.start()
-                awaitInitialization(port)
-                action(port)
-            } finally {
-                server?.stop(1000, 1000)
+                    post("/pks/add", addEndpointAction)
+                }
+            }.start()
+            awaitInitialization(port)
+            action(port)
+        } finally {
+            server?.stop(1000, 1000)
+        }
+    }
+
+    private fun projectWithJvmPlugin(gradleVersion: GradleVersion, test: TestProject.() -> Unit) {
+        project("empty", gradleVersion) {
+            plugins {
+                kotlin("jvm")
             }
+            test()
         }
     }
 }
