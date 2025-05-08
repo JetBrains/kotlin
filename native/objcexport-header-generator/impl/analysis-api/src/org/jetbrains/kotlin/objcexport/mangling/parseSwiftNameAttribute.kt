e8b61b3e1490e279b8d33b27caec4bb12e8c8c51
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-05-08 15:31:46 +0200

    [ObjCExport] Drop property and method names values limits
    Fix ^KT-77434
    
    (cherry picked from commit fe540eadd92a9759416006c1ff6611cf4d0c5a3d)

--- native/objcexport-header-generator/impl/analysis-api/src/org/jetbrains/kotlin/objcexport/mangling/parseSwiftNameAttribute.kt
+++ native/objcexport-header-generator/impl/analysis-api/src/org/jetbrains/kotlin/objcexport/mangling/parseSwiftNameAttribute.kt
@@ -1,26 +1,33 @@
 package org.jetbrains.kotlin.objcexport.mangling
 
+
 private val swiftNameRegex = """^swift_name\("([^"]+)"\)$""".toRegex()
-private val methodNameAndParametersRegex = """^([a-zA-Z0-9]+)\((.*)\)$""".toRegex()
-private val parametersRegex = Regex("[a-zA-Z0-9_]+:")
 
 internal fun parseSwiftPropertyNameAttribute(attribute: String): ObjCMemberDetails {
-  val swiftNameMatch = swiftNameRegex.find(attribute)
-  if (swiftNameMatch != null) {
-    val propertyName = swiftNameMatch.groupValues[1]
-    return ObjCMemberDetails(propertyName, emptyList())
-  } else error("Invalid swift_name property attribute: $attribute")
+    val swiftNameMatch = swiftNameRegex.find(attribute)
+    if (swiftNameMatch != null) {
+        val propertyName = swiftNameMatch.groupValues[1]
+        return ObjCMemberDetails(propertyName, emptyList())
+    } else error("Invalid swift_name property attribute: $attribute")
 }
 
-internal fun parseSwiftMethodNameAttribute(attribute: String, isConstructor: Boolean = false): ObjCMemberDetails {
+internal fun parseSwiftMethodNameAttribute(
+    attribute: String,
+    isConstructor: Boolean = false
+): ObjCMemberDetails {
     val swiftNameMatch = swiftNameRegex.find(attribute)
     if (swiftNameMatch != null) {
         val swiftName = swiftNameMatch.groupValues[1]
-        val methodAndParametersMatch = methodNameAndParametersRegex.find(swiftName)
-        if (methodAndParametersMatch != null) {
-            val methodName = methodAndParametersMatch.groupValues[1]
-            val parameters = methodAndParametersMatch.groupValues[2]
-            return ObjCMemberDetails(methodName, splitParameters(parameters), isConstructor)
+
+        val methodName = swiftName.extractMethodName()
+        val parameterNames = parseSwiftNameParameters(swiftName)
+
+        if (!methodName.isNullOrEmpty()) {
+            return ObjCMemberDetails(
+                name = methodName,
+                parameters = parameterNames,
+                isConstructor = isConstructor
+            )
         } else error("Invalid name and parameters of swift_name attribute: $attribute")
     } else error("Invalid swift_name method attribute: $attribute")
 }
@@ -29,11 +36,33 @@ internal data class ObjCMemberDetails(
     val name: String,
     val parameters: List<String>,
     val isConstructor: Boolean = false,
-    val postfix: String = "",
+    val postfix: String = ""
 )
 
-private fun splitParameters(parameters: String): List<String> {
-    return parametersRegex.findAll(parameters)
-        .map { it.value }
-        .toList()
+/**
+ * foo(a:b:) -> [a:, b:]
+ */
+internal fun parseSwiftNameParameters(swiftNameValue: String): List<String> {
+    val functionPattern = Regex("""\w+\((.*?)\)""")
+    val match = functionPattern.matchEntire(swiftNameValue.trim())
+
+    return when {
+        match != null -> {
+            val params = match.groupValues[1]
+            if (params.isBlank()) emptyList()
+            else {
+                params.split(':')
+                    .filter { it.isNotEmpty() }
+                    .map { param -> "$param:" }
+            }
+        }
+        else -> emptyList()
+    }
+}
+
+/**
+ * `foo(bar) -> foo
+ */
+internal fun String?.extractMethodName(): String? {
+    return this?.substringBefore('(')
 }
