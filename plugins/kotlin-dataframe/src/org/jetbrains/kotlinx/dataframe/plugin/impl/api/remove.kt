3f20af52a6453161b3ac5acab2c6fb48e8081643
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2025-05-08 12:26:47 +0300

    [KDF] Replace custom removeImpl with DataFrame.remove

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/remove.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/remove.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.kotlinx.dataframe.plugin.impl.api
 
-import org.jetbrains.kotlinx.dataframe.plugin.extensions.KotlinTypeFacade
+import org.jetbrains.kotlinx.dataframe.api.remove
 import org.jetbrains.kotlinx.dataframe.plugin.impl.*
 
 class Remove0 : AbstractSchemaModificationInterpreter() {
@@ -8,31 +8,6 @@ class Remove0 : AbstractSchemaModificationInterpreter() {
     val Arguments.columns: ColumnsResolver by arg()
 
     override fun Arguments.interpret(): PluginDataFrameSchema {
-        val removeResult = removeImpl(receiver.columns(), columns.resolve(receiver).mapTo(mutableSetOf()) { it.path.path })
-        return PluginDataFrameSchema(removeResult.updatedColumns)
+        return receiver.asDataFrame().remove { columns }.toPluginDataFrameSchema()
     }
-}
-
-class RemoveResult(val updatedColumns: List<SimpleCol>, val removedColumns: List<SimpleCol>)
-
-fun KotlinTypeFacade.removeImpl(schema: List<SimpleCol>, paths: Set<List<String>>): RemoveResult {
-    val removed = mutableListOf<SimpleCol>()
-    fun remove(schema: List<SimpleCol>, p: List<String>): List<SimpleCol> {
-        return schema.flatMap {
-            if (p + it.name() in paths) {
-                removed.add(it)
-                emptyList()
-            } else {
-                if (it is SimpleColumnGroup) {
-                    val columns = remove(it.columns(), p + it.name())
-                    if (columns.isEmpty()) emptyList() else listOf(SimpleColumnGroup(it.name(), columns))
-                } else {
-                    listOf(it)
-                }
-            }
-        }
-    }
-
-    val updatedColumns = remove(schema, emptyList())
-    return RemoveResult(updatedColumns, removed)
 }
