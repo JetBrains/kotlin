69f7281a8fd14c68c7c16d26372aa354ad8e1497
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2025-05-08 15:15:53 +0300

    [KDF] Add an option to disable top level extensions generator for REPL integration
    ^KT-77437 Fixed
    
    Jupyter integration of DataFrame already generates top level properties
    for DataSchemas, so one of them should be disabled

--- plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/FirDataFrameComponentRegistrar.kt
+++ plugins/kotlin-dataframe/src/org/jetbrains/kotlinx/dataframe/plugin/FirDataFrameComponentRegistrar.kt
@@ -6,22 +6,31 @@
 package org.jetbrains.kotlinx.dataframe.plugin
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
+import org.jetbrains.kotlin.compiler.plugin.CliOption
+import org.jetbrains.kotlin.compiler.plugin.CliOptionProcessingException
+import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.extensions.FirExtensionApiInternals
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
+import org.jetbrains.kotlinx.dataframe.plugin.DataFrameConfigurationKeys.DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES
 import org.jetbrains.kotlinx.dataframe.plugin.extensions.*
 
 class FirDataFrameExtensionRegistrar(
     val isTest: Boolean,
     val dumpSchemas: Boolean,
+    val disableTopLevelExtensionsGenerator: Boolean = false,
 ) : FirExtensionRegistrar() {
     @OptIn(FirExtensionApiInternals::class)
     override fun ExtensionRegistrarContext.configurePlugin() {
-        +::TopLevelExtensionsGenerator
+        if (!disableTopLevelExtensionsGenerator) {
+            +::TopLevelExtensionsGenerator
+        }
         +::ReturnTypeBasedReceiverInjector
         +{ it: FirSession ->
             FunctionCallTransformer(it, isTest)
@@ -40,10 +49,41 @@ class FirDataFrameExtensionRegistrar(
 class FirDataFrameComponentRegistrar : CompilerPluginRegistrar() {
     override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
         FirExtensionRegistrarAdapter.registerExtension(
-            FirDataFrameExtensionRegistrar(isTest = false, dumpSchemas = true)
+            FirDataFrameExtensionRegistrar(
+                isTest = false,
+                dumpSchemas = true,
+                configuration.get(DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES) == true
+            )
         )
         IrGenerationExtension.registerExtension(IrBodyFiller())
     }
 
     override val supportsK2: Boolean = true
+}
+
+object DataFrameConfigurationKeys {
+    val DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES: CompilerConfigurationKey<Boolean> =
+        CompilerConfigurationKey.create("Disable generation of extension properties for @DataSchema annotated classes or interfaces")
+}
+
+class DataFrameCommandLineProcessor : CommandLineProcessor {
+    companion object {
+        val DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES_OPTION = CliOption(
+            "disableTopLevelExtensionProperties",
+            "true/false",
+            "Disable generation of extension properties for @DataSchema annotated classes or interfaces",
+            required = false, allowMultipleOccurrences = false
+        )
+    }
+
+    override val pluginId: String = "org.jetbrains.kotlin.dataframe"
+
+    override val pluginOptions: Collection<AbstractCliOption> = listOf(DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES_OPTION)
+
+    override fun processOption(option: AbstractCliOption, value: String, configuration: CompilerConfiguration) {
+        return when (option) {
+            DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES_OPTION -> configuration.put(DISABLE_TOP_LEVEL_EXTENSION_PROPERTIES, value == "true")
+            else -> throw CliOptionProcessingException("Unknown option: ${option.optionName}")
+        }
+    }
 }
