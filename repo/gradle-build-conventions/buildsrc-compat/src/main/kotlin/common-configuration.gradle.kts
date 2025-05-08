701defff780cc35f6053b44271220e3358423f44
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-08 21:24:52 +0200

    [Repo] Apply Kotlin bootstrap using Gradle enforcedPlatform approach
    Gradle 'enforcedPlatform' applies strict version selection to the
    provided dependencies which ensures current bootstrap version is used.
    Kotlin BOM and Kotlin Gradle plugins BOMs are used as a source of which
    dependencies should have version constraints. Kotlin BOM is a part of
    the maven build, which means 'mvnPublish' task is also now required for
    the local bootstrap.
    
    Such approach has allowed to drop existing hacks for bootstrap such as
    substituting sub-project classpath by using the specific root project
    resolved classpath. And it also makes 'kotlin.git' project more
    compatible with isolated projects.
    
    Due to https://github.com/gradle/gradle/issues/26876 issue for some
    projects that are present in the 'kotlinCompilerClasspath' or
    'kotlinBuildToolsApiClasspath' configurations I had to do dependency
    substituion with a special project that exposes current project
    as bootstrap jars.
    
    (cherry picked from commit 74a64878eeca29800191f88b38e37b75b32b289e)

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
@@ -28,22 +28,6 @@ project.configureTests()
 // therefore it is disabled by default
 // buildDir = File(commonBuildDir, project.name)
 
-afterEvaluate {
-    run configureCompilerClasspath@{
-        val bootstrapCompilerClasspath by rootProject.buildscript.configurations
-        configurations.findByName("kotlinCompilerClasspath")?.let {
-            dependencies.add(it.name, files(bootstrapCompilerClasspath))
-        }
-        val bootstrapBuildToolsApiClasspath by rootProject.buildscript.configurations
-        configurations.findByName("kotlinBuildToolsApiClasspath")?.let {
-            it.dependencies.clear() // it's different from `bootstrapCompilerClasspath` as this configuration does not use "default dependencies"
-            dependencies.add(it.name, files(bootstrapBuildToolsApiClasspath))
-        }
-
-        configurations.findByName("kotlinCompilerPluginClasspath")
-            ?.exclude("org.jetbrains.kotlin", "kotlin-scripting-compiler-embeddable")
-    }
-}
 
 fun Project.addLocalRepository() {
     repositories {
