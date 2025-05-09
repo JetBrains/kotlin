5ed59249e6da4c9585d079b4549f91d6e0290141
Azat Abdullin <azat.abdullin@jetbrains.com>
2025-05-09 14:53:24 +0200

    KT-77435 Update more test data

--- compiler/testData/checkLocalVariablesTable/itInReturnedLambda.kt
+++ compiler/testData/checkLocalVariablesTable/itInReturnedLambda.kt
@@ -8,6 +8,6 @@ fun foo() {
     }
 }
 
-// METHOD : ItInReturnedLambdaKt$foo$bar$1.invoke(I)V
-// VARIABLE : NAME=this TYPE=LItInReturnedLambdaKt$foo$bar$1; INDEX=0
+// METHOD : ItInReturnedLambdaKt$foo$bar$1$kotlin_Function1$0.invoke(I)V
+// VARIABLE : NAME=this TYPE=LItInReturnedLambdaKt$foo$bar$1$kotlin_Function1$0; INDEX=0
 // VARIABLE : NAME=it TYPE=I INDEX=1
--- compiler/testData/writeFlags/inline/lostInnerClass2.kt
+++ compiler/testData/writeFlags/inline/lostInnerClass2.kt
@@ -12,9 +12,9 @@ interface Introspector {
 }
 
 // TESTED_OBJECT_KIND: innerClass
-// TESTED_OBJECTS: Introspector$SchemaRetriever$inSchema$1, SchemaRetriever
+// TESTED_OBJECTS: Introspector$SchemaRetriever$inSchema$1$kotlin_Function0$0, SchemaRetriever
 // FLAGS: ACC_FINAL, ACC_PUBLIC, ACC_STATIC
 
 // TESTED_OBJECT_KIND: innerClass
-// TESTED_OBJECTS: Introspector$SchemaRetriever$inSchema$1, DefaultImpls
+// TESTED_OBJECTS: Introspector$SchemaRetriever$inSchema$1$kotlin_Function0$0, DefaultImpls
 // ABSENT: true
