f20f5274bff0dd0b956fa0297362910c23437f29
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2025-05-09 21:38:45 +0200

    Refactor JVM classpath root management to use NIO paths
    Replaced usages of `jvmClasspathRoots` with `jvmClasspathNioRoots` across the Kotlin compiler codebase for more consistent and flexible handling of NIO paths. Adjusted related logic, interfaces, and data flows to accommodate the new methods, enhancing path handling and modular dependency definitions. Also cleaned up redundant code and ensured compatibility with virtual file systems.

--- compiler/tests-common-new/tests/org/jetbrains/kotlin/test/services/configuration/JvmEnvironmentConfigurator.kt
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/services/configuration/JvmEnvironmentConfigurator.kt
@@ -289,7 +289,7 @@ open class JvmEnvironmentConfigurator(testServices: TestServices) : EnvironmentC
             if (javaModuleInfoFiles.isNotEmpty()) {
                 configuration.addJavaBinaryRootsByJavaModules(configurationKind, javaModuleInfoFiles)
             } else {
-                val jvmClasspathRoots = configuration.jvmClasspathRoots.map { it.absolutePath }
+                val jvmClasspathRoots = configuration.jvmClasspathNioRoots().map { it.toAbsolutePath().toString() }.toList()
                 val additionalClassPath = testServices.additionalClassPathForJavaCompilationOrAnalysis?.classPath.orEmpty()
                 configuration.addJvmClasspathRoot(
                     compileJavaFilesLibraryToJar(
@@ -338,7 +338,7 @@ open class JvmEnvironmentConfigurator(testServices: TestServices) : EnvironmentC
         configurationKind: ConfigurationKind,
         javaModuleInfoFiles: List<TestFile>
     ) {
-        val classPath = jvmClasspathRoots.map(File::getAbsolutePath)
+        val classPath = jvmClasspathNioRoots().map { it.toAbsolutePath().toString() }.toList()
         val modulePath = buildList {
             addAll(jvmModularRoots.map(File::getAbsolutePath))
             if (configurationKind.withRuntime) {
