053e140f44da59260d5d52357eb370b01e2d5ba4
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-09 13:51:41 +0200

    [Args] Fill JvmTarget type values
    ^KT-75812 In Progress

--- compiler/arguments/test/org/jetbrains/kotlin/arguments/dsl/types/JvmTargetConsistency.kt
+++ compiler/arguments/test/org/jetbrains/kotlin/arguments/dsl/types/JvmTargetConsistency.kt
@@ -5,15 +5,48 @@
 
 package org.jetbrains.kotlin.arguments.dsl.types
 
+import org.jetbrains.kotlin.config.JvmTarget as CompilerJvmTarget
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
 
 class JvmTargetConsistency {
     @Test
     fun supportedVersionsDescriptionIsConsistent() {
-        val lastSupportedTarget = org.jetbrains.kotlin.config.JvmTarget.entries.last()
-        assertTrue("Please update the `JvmTarget.SUPPORTED_VERSIONS_DESCRIPTION`") {
+        val lastSupportedTarget = CompilerJvmTarget.entries.last()
+        assertTrue("Please update the max supported target in the `JvmTarget.SUPPORTED_VERSIONS_DESCRIPTION`") {
             JvmTarget.SUPPORTED_VERSIONS_DESCRIPTION.endsWith(lastSupportedTarget.description)
         }
+
+        val firstSupportedTarget = CompilerJvmTarget.supportedValues().first()
+        assertTrue("Please update the min supported target in the `JvmTarget.SUPPORTED_VERSIONS_DESCRIPTION`") {
+            JvmTarget.SUPPORTED_VERSIONS_DESCRIPTION.startsWith(firstSupportedTarget.description)
+        }
+    }
+
+    @Test
+    fun allSupportedKotlinVersionsArePresent() {
+        CompilerJvmTarget.entries.forEach { jvmTarget ->
+            assertTrue(
+                actual = jvmTarget.toDslJvmTargetOrNull() != null,
+                message = "Jvm target '$jvmTarget' is not present in 'JvmTarget' DSL type"
+            )
+        }
+    }
+
+    @Test
+    fun versionIsRemoved() {
+        (CompilerJvmTarget.entries - CompilerJvmTarget.supportedValues())
+            .forEach { jvmTarget ->
+                jvmTarget.toDslJvmTargetOrNull()?.let {
+                    assertTrue(
+                        actual = it.releaseVersionsMetadata.removedVersion != null,
+                        message = "Jvm target '$jvmTarget' is not supported, while $it 'removedVersion' is 'null'!"
+                    )
+                }
+            }
+    }
+
+    private fun CompilerJvmTarget.toDslJvmTargetOrNull() = JvmTarget.entries.find { target ->
+        target.targetName == description
     }
 }
