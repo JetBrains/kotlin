e87c7874df03241c7364385a68815ff9dc7eb7bd
Alevtina.Gamzikova <alevtina.gamzikova@jetbrains.com>
2025-05-09 14:13:20 +0200

    [Tests] Add testcases for KQA-2040

---
+++ compiler/testData/codegen/boxWasmJsInterop/contextParametersJSCodeJSFun.kt
@@ -0,0 +1,33 @@
+// LANGUAGE: +ContextParameters
+// TARGET_BACKEND: WASM
+// WITH_STDLIB
+
+external interface Context {
+    fun action(): String
+}
+
+@JsFun("() => ({ action: () => 'OK' })")
+external fun get(): Context
+
+context(c: Context)
+fun test(): String = c.action()
+
+fun usage1(c: Context): String =
+    context(c) { test() }
+
+fun usage2(c: Context): String =
+    with(c) { test() }
+
+context(c: Context)
+fun usage3(): String = test()
+
+fun box(): String {
+    val ctx = get()
+
+    val r1 = usage1(ctx)
+    val r2 = usage2(ctx)
+    val r3 = context(ctx) { usage3() }
+
+    return if (r1 == "OK" && r2 == "OK" && r3 == "OK") "OK"
+    else "FAIL"
+}
