0edca821cec1fbcf2fb0589ea5cdd52d57b734a6
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-12 21:11:06 +0200

    [Args] Add KDoc for KotlinReleaseVersionLifecycle
    ^KT-75812 In Progress

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/base/KotlinReleaseVersionLifecycle.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/base/KotlinReleaseVersionLifecycle.kt
@@ -7,10 +7,28 @@ package org.jetbrains.kotlin.arguments.dsl.base
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Types/values containing [KotlinReleaseVersionLifecycle] metadata should implement this interface.
+ */
 interface WithKotlinReleaseVersionsMetadata {
+
+    /**
+     * Kotlin release version lifecycle metadata.
+     */
     val releaseVersionsMetadata: KotlinReleaseVersionLifecycle
 }
 
+/**
+ * Metadata containing information about type or value lifecycle in Kotlin releases.
+ *
+ * Types/values containing such metadata should implement [WithKotlinReleaseVersionsMetadata] interface.
+ *
+ * @param introducedVersion in which Kotlin release, this type/value was introduced. Mandatory property.
+ * @param stabilizedVersion in which Kotlin release, this type/value was promoted to stable.
+ * If the value of this property is `null` - the type/value should be considered experimental.
+ * @param deprecatedVersion in which Kotlin release, this type/value was deprecated.
+ * @param removedVersion in which Kotlin release, this type/value was removed.
+ */
 @Serializable
 data class KotlinReleaseVersionLifecycle(
     val introducedVersion: KotlinReleaseVersion,
