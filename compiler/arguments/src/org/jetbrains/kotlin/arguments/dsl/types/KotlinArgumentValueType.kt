4c0ab96410476f36f984f999fd71bcc63e9a0189
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-12 20:40:46 +0200

    [Args] Fix primitive argument value types are not serializable
    ^KT-75812 In Progress

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinArgumentValueType.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinArgumentValueType.kt
@@ -9,6 +9,7 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.kotlin.arguments.dsl.base.KotlinReleaseVersion
 import org.jetbrains.kotlin.arguments.dsl.base.ReleaseDependent
 
+@Serializable
 sealed interface KotlinArgumentValueType<T : Any> {
     val isNullable: ReleaseDependent<Boolean>
     val defaultValue: ReleaseDependent<T?>
