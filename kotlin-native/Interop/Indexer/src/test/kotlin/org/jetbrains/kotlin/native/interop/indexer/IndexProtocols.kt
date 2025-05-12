6f62c2a43afa50e9f8bf53941d63600ae7c75026
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-05-12 14:52:09 +0200

    wip: KotlinArray compiles

--- kotlin-native/Interop/Indexer/src/test/kotlin/org/jetbrains/kotlin/native/interop/indexer/IndexProtocols.kt
+++ kotlin-native/Interop/Indexer/src/test/kotlin/org/jetbrains/kotlin/native/interop/indexer/IndexProtocols.kt
@@ -14,8 +14,15 @@ class IndexProtocols : IndexerTests() {
 
     private val moduleName = "Foo"
     private var files: TempFiles = TempFiles(moduleName)
-    private val appleSdkPath = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.4.sdk"
-    private val appleFrameworkPath = "$appleSdkPath/System/Library/Frameworks"
+
+    companion object {
+        private val appleSdkPath = "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX15.4.sdk"
+        private val appleFrameworkPath = "$appleSdkPath/System/Library/Frameworks"
+
+        private val xcodeDir = "/Applications/Xcode.app/Contents/Developer"
+        private val clangVersion = "17.0.0"
+        private val clangResourceDir = "$xcodeDir/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/$clangVersion/include"
+    }
 
     @Before
     fun before() {
@@ -24,13 +31,13 @@ class IndexProtocols : IndexerTests() {
 
     @Test
     fun `basic protocol`() {
-        val arrayHeader = files.file("Foo.h", """
+        val fooHeader = files.file("Foo.h", """
             @protocol Foo
             - (void)doSomething;
             @end
         """.trimIndent())
 
-        val indexerResult = compileAndIndex(listOf(arrayHeader), files)
+        val indexerResult = compileAndIndex(listOf(fooHeader), files)
         val foo = indexerResult.index.objCProtocols.first()
 
         assertEquals(1, indexerResult.index.objCProtocols.size)
@@ -41,7 +48,7 @@ class IndexProtocols : IndexerTests() {
     @Test
     fun `kotlin array`() {
 
-        val arrayHeader = files.file("Foo.h", """
+        val arrayHeader = files.file("KotlinArray.h", """
 
             #import <Foundation/NSArray.h>
             #import <Foundation/NSDictionary.h>
@@ -50,7 +57,12 @@ class IndexProtocols : IndexerTests() {
             #import <Foundation/NSSet.h>
             #import <Foundation/NSString.h>
             #import <Foundation/NSValue.h>
-            #include <Base.h>
+            
+            @protocol KotlinIterator
+            @required
+            - (BOOL)hasNext __attribute__((swift_name("hasNext()")));
+            - (id _Nullable)next __attribute__((swift_name("next()")));
+            @end
                      
             @interface KotlinArray<T> : Base
             + (instancetype)arrayWithSize:(int32_t)size init:(T _Nullable (^)(Int *))init __attribute__((swift_name("init(size:init:)")));
@@ -63,26 +75,19 @@ class IndexProtocols : IndexerTests() {
             @end
         """.trimIndent())
 
-        val base = files.file("Base.h", """
-            @interface Base
-            @end
-        """.trimIndent())
 
-        val indexerResult = compileAndIndex(
+        val base = files.file("Base.h", kotlinCinteropTypes.trimIndent())
+
+        val index = compileAndIndex(
                 listOf(base, arrayHeader),
                 files,
                 "-isysroot", appleSdkPath,
                 "-F", appleFrameworkPath,
-                "-fmodules",
-                "-fcxx-modules"
-        )
+        ).index
 
-        assertEquals(1, indexerResult.index.objCProtocols.size)
+        val kotlinArray = index.objCClasses.filter { it.name == "KotlinArray" }.first()
 
-        val prot = indexerResult.index.objCProtocols.first()
-        prot.methods.forEach {
-            println(it)
-        }
+        println(kotlinArray)
     }
 
     private fun compileAndIndex(
@@ -105,9 +110,11 @@ class IndexProtocols : IndexerTests() {
             IncludeInfo(it.absolutePath, moduleName)
         }
 
+
         val compilation = compilation(
                 includeInfos,
                 "-I${files.directory}",
+                "-I$clangResourceDir",
                 *args
         )
 
@@ -140,3 +147,115 @@ class IndexProtocols : IndexerTests() {
 class HeaderExclusionPolicyImpl : HeaderExclusionPolicy {
     override fun excludeAll(headerId: HeaderId): Boolean = false
 }
+
+private const val kotlinCinteropTypes = """
+    
+    #ifndef CINTEROP_SHIMS_H
+    #define CINTEROP_SHIMS_H
+    
+    #import <Foundation/NSArray.h>
+    #import <Foundation/NSDictionary.h>
+    #import <Foundation/NSError.h>
+    #import <Foundation/NSObject.h>
+    #import <Foundation/NSSet.h>
+    #import <Foundation/NSString.h>
+    #import <Foundation/NSValue.h>
+    
+    __attribute__((swift_name("KotlinBase")))
+    @interface Base : NSObject
+    - (instancetype)init __attribute__((unavailable));
+    + (instancetype)new __attribute__((unavailable));
+    + (void)initialize __attribute__((objc_requires_super));
+    @end
+    @interface Base (BaseCopying) <NSCopying>
+    @end
+    __attribute__((swift_name("KotlinMutableSet")))
+    @interface MutableSet<ObjectType> : NSMutableSet<ObjectType>
+    @end
+    __attribute__((swift_name("KotlinMutableDictionary")))
+    @interface MutableDictionary<KeyType, ObjectType> : NSMutableDictionary<KeyType, ObjectType>
+    @end
+    @interface NSError (NSErrorKotlinException)
+    @property (readonly) id _Nullable kotlinException;
+    @end
+    __attribute__((swift_name("KotlinNumber")))
+    @interface Number : NSNumber
+    - (instancetype)initWithChar:(char)value __attribute__((unavailable));
+    - (instancetype)initWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
+    - (instancetype)initWithShort:(short)value __attribute__((unavailable));
+    - (instancetype)initWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
+    - (instancetype)initWithInt:(int)value __attribute__((unavailable));
+    - (instancetype)initWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
+    - (instancetype)initWithLong:(long)value __attribute__((unavailable));
+    - (instancetype)initWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
+    - (instancetype)initWithLongLong:(long long)value __attribute__((unavailable));
+    - (instancetype)initWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
+    - (instancetype)initWithFloat:(float)value __attribute__((unavailable));
+    - (instancetype)initWithDouble:(double)value __attribute__((unavailable));
+    - (instancetype)initWithBool:(BOOL)value __attribute__((unavailable));
+    - (instancetype)initWithInteger:(NSInteger)value __attribute__((unavailable));
+    - (instancetype)initWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
+    + (instancetype)numberWithChar:(char)value __attribute__((unavailable));
+    + (instancetype)numberWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
+    + (instancetype)numberWithShort:(short)value __attribute__((unavailable));
+    + (instancetype)numberWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
+    + (instancetype)numberWithInt:(int)value __attribute__((unavailable));
+    + (instancetype)numberWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
+    + (instancetype)numberWithLong:(long)value __attribute__((unavailable));
+    + (instancetype)numberWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
+    + (instancetype)numberWithLongLong:(long long)value __attribute__((unavailable));
+    + (instancetype)numberWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
+    + (instancetype)numberWithFloat:(float)value __attribute__((unavailable));
+    + (instancetype)numberWithDouble:(double)value __attribute__((unavailable));
+    + (instancetype)numberWithBool:(BOOL)value __attribute__((unavailable));
+    + (instancetype)numberWithInteger:(NSInteger)value __attribute__((unavailable));
+    + (instancetype)numberWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
+    @end
+    __attribute__((swift_name("KotlinUByte")))
+    @interface UByte : Number
+    - (instancetype)initWithUnsignedChar:(unsigned char)value;
+    + (instancetype)numberWithUnsignedChar:(unsigned char)value;
+    @end
+    __attribute__((swift_name("KotlinShort")))
+    @interface Short : Number
+    - (instancetype)initWithShort:(short)value;
+    + (instancetype)numberWithShort:(short)value;
+    @end
+    __attribute__((swift_name("KotlinUShort")))
+    @interface UShort : Number
+    - (instancetype)initWithUnsignedShort:(unsigned short)value;
+    + (instancetype)numberWithUnsignedShort:(unsigned short)value;
+    @end
+    __attribute__((swift_name("KotlinInt")))
+    @interface Int : Number
+    - (instancetype)initWithInt:(int)value;
+    + (instancetype)numberWithInt:(int)value;
+    @end
+    __attribute__((swift_name("KotlinUInt")))
+    @interface UInt : Number
+    - (instancetype)initWithUnsignedInt:(unsigned int)value;
+    + (instancetype)numberWithUnsignedInt:(unsigned int)value;
+    @end
+    __attribute__((swift_name("KotlinLong")))
+    @interface Long : Number
+    - (instancetype)initWithLongLong:(long long)value;
+    + (instancetype)numberWithLongLong:(long long)value;
+    @end
+    __attribute__((swift_name("KotlinULong")))
+    @interface ULong : Number
+    - (instancetype)initWithUnsignedLongLong:(unsigned long long)value;
+    + (instancetype)numberWithUnsignedLongLong:(unsigned long long)value;
+    @end
+    __attribute__((swift_name("KotlinFloat")))
+    @interface Float : Number
+    - (instancetype)initWithFloat:(float)value;
+    + (instancetype)numberWithFloat:(float)value;
+    @end
+    __attribute__((swift_name("KotlinDouble")))
+    @interface Double : Number
+    - (instancetype)initWithDouble:(double)value;
+    + (instancetype)numberWithDouble:(double)value;
+    @end
+
+    #endif // CINTEROP_SHIMS_H
+"""
