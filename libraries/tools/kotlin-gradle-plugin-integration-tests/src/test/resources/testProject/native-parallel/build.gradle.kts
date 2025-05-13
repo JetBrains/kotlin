2ad46aa78f2d7572352ad9d5fae34b2259505197
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-05-13 11:56:26 +0200

    [Gradle] explicitly disable isolated projects for tests in `KmpIncrementalCompilationSetExpansionIT` and fix some Kapt tests
    ^KT-67291 In Progress

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/native-parallel/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/native-parallel/build.gradle.kts
@@ -2,10 +2,3 @@ import java.util.concurrent.CountDownLatch
 plugins {
     id("org.jetbrains.kotlin.multiplatform").apply(false)
 }
-
-allprojects {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-    }
-}
