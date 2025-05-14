dc741fb1a869b0c23ef683e9ab68c42f892f96f0
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-05-14 17:39:55 +0200

    [test] Migrate JS diagnostic tests to the new CLI-based test facades
    ^KT-77190 Fixed

--- compiler/testData/diagnostics/testsWithJsStdLibAndBackendCompilation/jsCode/warning.fir.kt
+++ compiler/testData/diagnostics/testsWithJsStdLibAndBackendCompilation/jsCode/warning.fir.kt
@@ -1,6 +1,7 @@
 // FIR_DIFFERENCE
 // The diagnostic cannot be implemented with the FIR frontend checker because it requires constant evaluation over FIR.
 // The diagnostic is implemented as a klib check over IR.
+// IGNORE_FIR_DIAGNOSTICS
 
 fun main(): Unit {
     js(<!JSCODE_WARNING!>"var a = 08;"<!>)
