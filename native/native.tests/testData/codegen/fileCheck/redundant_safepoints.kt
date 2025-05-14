1b1dac1bbeb5804980bce7146894cc97f4948d3b
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-05-14 15:45:36 +0300

    [Native] Fix FileCheck test with external calls checker
    Previously we checked callee mention, not its call-site. It is not
    correct in case of external calls checker which inserts the following:
    ```
    call void @Kotlin_mm_checkStateAtExternalFunctionCall(..., ptr @Kotlin_mm_safePointFunctionPrologue)
    ```

--- native/native.tests/testData/codegen/fileCheck/redundant_safepoints.kt
+++ native/native.tests/testData/codegen/fileCheck/redundant_safepoints.kt
@@ -20,13 +20,14 @@ fun g() = f()
 // CHECK-LABEL: define {{(noundef )?}}ptr @"kfun:#h(kotlin.Boolean){}kotlin.Any"
 @Retain
 fun h(cond: Boolean): Any {
-    // CHECK-SMALLBINARY: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // We have to check actual _call_ to a function, not just callee mention.
+    // CHECK-SMALLBINARY: {{call .*Kotlin_mm_safePointFunctionPrologue\(\)}}
     // CHECK-BIGBINARY-OPT: _ZN12_GLOBAL__N_115safePointActionE
 
     // CHECK-SMALLBINARY-NOT: _ZN12_GLOBAL__N_115safePointActionE
-    // CHECK-BIGBINARY-OPT-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-OPT-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue\(\)}}
 
-    // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue\(\)}}
     // CHECK-BIGBINARY-OPT-NOT: _ZN12_GLOBAL__N_115safePointActionE
     if (cond) {
         return listOf(C(), C())
@@ -39,13 +40,13 @@ fun h(cond: Boolean): Any {
 // CHECK-LABEL: define {{(noundef )?}}{{(nonnull )?}}ptr @"kfun:#box(){}kotlin.String"
 @Retain
 fun box(): String {
-    // CHECK-SMALLBINARY: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-SMALLBINARY: {{call .*Kotlin_mm_safePointFunctionPrologue\(\)}}
     // CHECK-BIGBINARY-OPT: _ZN12_GLOBAL__N_115safePointActionE
 
     // CHECK-SMALLBINARY-NOT: _ZN12_GLOBAL__N_115safePointActionE
-    // CHECK-BIGBINARY-OPT-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-BIGBINARY-OPT-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue\(\)}}
 
-    // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue}}
+    // CHECK-SMALLBINARY-NOT: {{call .*Kotlin_mm_safePointFunctionPrologue\(\)}}
     // CHECK-BIGBINARY-OPT-NOT: _ZN12_GLOBAL__N_115safePointActionE
     println(g())
     println(h(true))
