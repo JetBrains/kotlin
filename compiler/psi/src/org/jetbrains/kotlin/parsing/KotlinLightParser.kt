3b0e5d88542f0f2fe779a9e42761aef67e043696
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-05-14 15:14:16 +0300

    [LT] Support parsing scripts in LT mode
    ^KT-64477 Fixed

--- compiler/psi/src/org/jetbrains/kotlin/parsing/KotlinLightParser.kt
+++ compiler/psi/src/org/jetbrains/kotlin/parsing/KotlinLightParser.kt
@@ -11,22 +11,28 @@ import com.intellij.lang.impl.PsiBuilderImpl
 import com.intellij.openapi.util.Ref
 import com.intellij.psi.TokenType
 import com.intellij.util.diff.FlyweightCapableTreeStructure
+import org.jetbrains.kotlin.KtSourceFile
 import org.jetbrains.kotlin.lexer.KotlinLexer
 
 object KotlinLightParser {
     fun buildLightTree(
         code: CharSequence,
+        sourceFile: KtSourceFile?,
         errorListener: LightTreeParsingErrorListener?,
     ): FlyweightCapableTreeStructure<LighterASTNode> {
         val builder = PsiBuilderFactory.getInstance().createBuilder(KotlinParserDefinition(), KotlinLexer(), code)
-        return parse(builder).also {
+        return parse(builder, sourceFile).also {
             if (errorListener != null) reportErrors(it.root, it, errorListener)
         }
     }
 
-    private fun parse(builder: PsiBuilder): FlyweightCapableTreeStructure<LighterASTNode> {
+    private fun parse(builder: PsiBuilder, sourceFile: KtSourceFile?): FlyweightCapableTreeStructure<LighterASTNode> {
         val ktParsing = KotlinParsing.createForTopLevelNonLazy(SemanticWhitespaceAwarePsiBuilderImpl(builder))
-        ktParsing.parseFile()
+        val extension = sourceFile?.name?.substringAfterLast('.') ?: "kt"
+        when (extension) {
+            "kts" -> ktParsing.parseScript()
+            else -> ktParsing.parseFile()
+        }
 
         return builder.lightTree
     }
