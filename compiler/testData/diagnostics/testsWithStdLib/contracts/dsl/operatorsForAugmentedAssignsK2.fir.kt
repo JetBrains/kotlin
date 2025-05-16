39496e14f6594c9a3aa9eb7a41388a3601eab940
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2025-05-17 00:31:25 +0900

    [FIR] Deduplicate implicit values on snapshot creation
    Before, clones of implicit values were created independently. However,
    the compiler expects to see identical values in 'ImplicitValueStorage',
    so modifications in one map can reflect others.
    
    ^KT-72357

--- compiler/testData/diagnostics/testsWithStdLib/contracts/dsl/operatorsForAugmentedAssignsK2.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/contracts/dsl/operatorsForAugmentedAssignsK2.fir.kt
@@ -7,85 +7,8 @@ import kotlin.contracts.*
 
 class A(var v: Int = 0)
 
-// plus
-
-operator fun Boolean.plus(x: Boolean): Boolean {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { returns() implies (x) }
-    return x
-}
-
-fun test_plus(x: Any) {
-    if (true + (x is String)) {
-        x.length
-    }
-}
-
-// minus
-
-operator fun Boolean.minus(x: Boolean): Boolean {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { returns() implies (x) }
-    return x
-}
-
-fun test_minus(x: Any) {
-    if (true - (x is Int)) {
-        x.toChar()
-    }
-}
-
-// *Assign
-
-operator fun A.plusAssign(body: () -> Int) {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { callsInPlace(body, InvocationKind.EXACTLY_ONCE) }
-    v += body()
-}
-
-operator fun A.minusAssign(body: () -> Int) {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { callsInPlace(body, InvocationKind.EXACTLY_ONCE) }
-    v -= body()
-}
-
-operator fun A.timesAssign(body: () -> Int) {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { callsInPlace(body, InvocationKind.EXACTLY_ONCE) }
-    v *= body()
-}
-
-operator fun A.divAssign(body: () -> Int) {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { callsInPlace(body, InvocationKind.EXACTLY_ONCE) }
-    v /= body()
-}
-
-operator fun A.remAssign(body: () -> Int) {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { callsInPlace(body, InvocationKind.EXACTLY_ONCE) }
-    v %= body()
-}
-
-fun test_xAssign() {
-    var a = A()
-    val plus: Boolean; a += { plus = true; 1 }
-    val minus: Boolean; a -= { minus = true; 1 }
-    val times: Boolean; a *= { times = true; 1 }
-    val div: Boolean; a /= { div = true; 1 }
-    val rem: Boolean; a %= { rem = true; 1 }
-}
-
-// indexed access
-
-operator fun A.get(i: Int?): Int {
-    <!CONTRACT_NOT_ALLOWED!>contract<!> { returns() implies (i != null) }
-    return v
-}
-
 operator fun A?.set(i: Int, vnew: Int) {
     <!CONTRACT_NOT_ALLOWED!>contract<!> { returns() implies (this@set != null) }
     this!!
     v = vnew
-}
-
-fun test_indexed(a1: A, i: Int?, a2: A?) {
-    if (a1[i] == 0) {
-        i + 1
-    }
-    a2[0] = 1
-    a2.v
 }
