1e1e22c7e3f3bdb5f2bf271d15ad0ddda3d2fb34
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2025-05-16 14:51:05 +0200

    Implement `isFunctionInlining` in non K/JVM-specific way
    ^KT-76761 Fixed

--- native/native.tests/testData/codegen/kclass/anonymousObjectInInlineFunction.kt
+++ native/native.tests/testData/codegen/kclass/anonymousObjectInInlineFunction.kt
@@ -2,7 +2,6 @@
  * Copyright 2010-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the LICENSE file.
  */
-// LANGUAGE: -IrInlinerBeforeKlibSerialization
 
 package codegen.kclass.kclass0
 import kotlin.test.*
