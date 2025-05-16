39cdde896ccaaf9a7c4cc383186796a6242c5f83
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-16 10:12:45 +0200

    [Args] Add KDoc for KotlinCompilerArgumentsLevel
    ^KT-75812 In Progress

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/base/KotlinCompilerArgumentsLevel.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/base/KotlinCompilerArgumentsLevel.kt
@@ -8,6 +8,19 @@ package org.jetbrains.kotlin.arguments.dsl.base
 import kotlinx.serialization.Serializable
 import kotlin.properties.ReadOnlyProperty
 
+/**
+ * A Kotlin compiler arguments level.
+ *
+ * A level represents a collection of compiler arguments and may contain nested levels forming a tree-like structure of arguments.
+ * A nested level includes arguments for the parent levels, but not from a sibling level.
+ *
+ * @param name the name of the level
+ * @param arguments a set of [KotlinCompilerArguments][KotlinCompilerArgument] this level contains
+ * @param nestedLevels nested levels of compiler arguments
+ *
+ * Usually compiler argument level should either be defined via top level builder [KotlinCompilerArgumentsBuilder.topLevel]
+ * or via special standalone builder DSL - [compilerArgumentsLevel].
+ */
 @Serializable
 data class KotlinCompilerArgumentsLevel(
     val name: String,
@@ -15,6 +28,13 @@ data class KotlinCompilerArgumentsLevel(
     val nestedLevels: Set<KotlinCompilerArgumentsLevel>
 ) {
 
+    /**
+     * Merge all arguments and nested levels from [another] level into this one.
+     *
+     * The [another] [KotlinCompilerArgumentsLevel.name] should be equal to this one!
+     * Nested level names may or not be equal to the nested levels of this one.
+     * If they are equal, they will be merged as well.
+     */
     internal fun mergeWith(another: KotlinCompilerArgumentsLevel): KotlinCompilerArgumentsLevel {
         require(name == another.name) {
             "Names for compiler arguments level should be the same! We are trying to merge $name with ${another.name}"
@@ -40,12 +60,18 @@ data class KotlinCompilerArgumentsLevel(
     }
 }
 
+/**
+ * DSL builder for [KotlinCompilerArgumentsLevel].
+ */
 @KotlinArgumentsDslMarker
 internal class KotlinCompilerArgumentsLevelBuilder(
     val name: String
 ) {
     private val arguments = mutableSetOf<KotlinCompilerArgument>()
 
+    /**
+     * Define a new [KotlinCompilerArgument].
+     */
     fun compilerArgument(
         config: KotlinCompilerArgumentBuilder.() -> Unit
     ) {
@@ -54,6 +80,9 @@ internal class KotlinCompilerArgumentsLevelBuilder(
         arguments.add(argumentBuilder.build())
     }
 
+    /**
+     * Add additional [KotlinCompilerArguments][KotlinCompilerArgument] into this level.
+     */
     fun addCompilerArguments(
         vararg compilerArguments: KotlinCompilerArgument
     ) {
@@ -62,6 +91,9 @@ internal class KotlinCompilerArgumentsLevelBuilder(
 
     private val nestedLevels = mutableSetOf<KotlinCompilerArgumentsLevel>()
 
+    /**
+     * Define a new nested compiler arguments level.
+     */
     fun subLevel(
         name: String,
         mergeWith: Set<KotlinCompilerArgumentsLevel> = emptySet(),
@@ -76,6 +108,9 @@ internal class KotlinCompilerArgumentsLevelBuilder(
         )
     }
 
+    /**
+     * Build a new instance of [KotlinCompilerArgumentsLevel].
+     */
     fun build(): KotlinCompilerArgumentsLevel = KotlinCompilerArgumentsLevel(
         name,
         arguments,
@@ -83,6 +118,24 @@ internal class KotlinCompilerArgumentsLevelBuilder(
     )
 }
 
+/**
+ * Allows creating separated from the main DSL compiler argument level definition, which later could be added into the main DSL.
+ *
+ * Usage example:
+ * ```
+ * val deprecatedCommonCompilerArguments by compilerArgumentsLevel {
+ *    name = "commonCompilerArguments"
+ *
+ *    compilerArgument { ... }
+ *    compilerArgument { ... }
+ * }
+ * ```
+ *
+ * Such standalone compiler argument level could be added into the main definition via [KotlinCompilerArgumentsLevel.mergeWith]
+ * method.
+ *
+ * @see KotlinCompilerArgumentsLevelBuilder
+ */
 internal fun compilerArgumentsLevel(
     name: String,
     config: KotlinCompilerArgumentsLevelBuilder.() -> Unit
