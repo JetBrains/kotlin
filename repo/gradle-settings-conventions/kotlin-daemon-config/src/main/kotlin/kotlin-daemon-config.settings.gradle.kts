6f1bfe9fa11be40ff96ca01b8a55dfc5dde67f47
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-20 19:20:24 +0200

    [Repo] Fix Kotlin daemon memory settings for main project
    Using '.afterProject` which is triggered after project evaluation is
    too late - property is already queried by KGP and memoized.
    
    As approach does not use KGP directly - moving it to '.beforeProject'
    solves the issue. KGP sees extra property available and does not try to
    query Gradle properties.
    
    Signed-off-by: Yahor Berdnikau <yahor.berdnikau@jetbrains.com>

--- repo/gradle-settings-conventions/kotlin-daemon-config/src/main/kotlin/kotlin-daemon-config.settings.gradle.kts
+++ repo/gradle-settings-conventions/kotlin-daemon-config/src/main/kotlin/kotlin-daemon-config.settings.gradle.kts
@@ -1,4 +1,4 @@
-gradle.afterProject {
+gradle.beforeProject {
     // We could not use KGP symbols directly as the bootstrap repo is added
     // after transitive dependencies of this project should be resolved.
     // And the 'compileOnly' approach doesn't work because of the Gradle classload isolation.
