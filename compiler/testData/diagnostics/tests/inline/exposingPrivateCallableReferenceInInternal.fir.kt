9fb0b7790c2e678753dfee5b27ee0d029fb60508
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-05-21 13:16:21 +0200

    [Tests] Reproduce #KT-77095

---
+++ compiler/testData/diagnostics/tests/inline/exposingPrivateCallableReferenceInInternal.fir.kt
@@ -0,0 +1,52 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// DIAGNOSTICS: -NOTHING_TO_INLINE
+// LANGUAGE: -ForbidExposingLessVisibleTypesInInline
+
+private fun f() {}
+private val p = 1
+
+private class PC {
+    fun f() {}
+    val p = 1
+}
+
+internal inline fun test() {
+    ::f
+    ::p
+
+    PC::<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING, PRIVATE_CLASS_MEMBER_FROM_INLINE!>f<!>
+    PC::<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING, PRIVATE_CLASS_MEMBER_FROM_INLINE!>p<!>
+
+    val o = object {
+        private fun f() {}
+        private val p = 1
+
+        fun inAnonymousObject() {
+            ::f
+            ::p
+        }
+    }
+}
+
+class C {
+    private fun f() {}
+    private val p = 1
+
+    protected fun protectedF() {}
+    protected val protectedP = 1
+
+    protected class PC {
+        fun f() {}
+        val p = 1
+    }
+
+    internal inline fun test() {
+        ::f
+        ::p
+        ::protectedF
+        ::protectedP
+
+        PC::<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING!>f<!>
+        PC::<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING!>p<!>
+    }
+}
---
+++ compiler/testData/diagnostics/tests/inline/exposingPrivateCallableReferenceInInternalAfter.kt
@@ -0,0 +1,52 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// DIAGNOSTICS: -NOTHING_TO_INLINE
+// LANGUAGE: +ForbidExposingLessVisibleTypesInInline
+
+private fun f() {}
+private val p = 1
+
+private class PC {
+    fun f() {}
+    val p = 1
+}
+
+internal inline fun test() {
+    ::f
+    ::p
+
+    PC::<!PRIVATE_CLASS_MEMBER_FROM_INLINE!>f<!>
+    PC::<!PRIVATE_CLASS_MEMBER_FROM_INLINE!>p<!>
+
+    val o = object {
+        private fun f() {}
+        private val p = 1
+
+        fun inAnonymousObject() {
+            ::f
+            ::p
+        }
+    }
+}
+
+class C {
+    private fun f() {}
+    private val p = 1
+
+    protected fun protectedF() {}
+    protected val protectedP = 1
+
+    protected class PC {
+        fun f() {}
+        val p = 1
+    }
+
+    internal inline fun test() {
+        ::f
+        ::p
+        ::protectedF
+        ::protectedP
+
+        PC::f
+        PC::p
+    }
+}
