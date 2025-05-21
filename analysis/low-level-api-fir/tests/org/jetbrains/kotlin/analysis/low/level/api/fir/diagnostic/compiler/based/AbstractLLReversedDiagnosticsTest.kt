47fb3add7b6784af7861406814938bb30c788028
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-05-21 11:43:25 +0200

    Tests: don't collect fixation logs in reversed tests as they have different order

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/AbstractLLReversedDiagnosticsTest.kt
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/AbstractLLReversedDiagnosticsTest.kt
@@ -39,6 +39,7 @@ abstract class AbstractLLReversedDiagnosticsTest : AbstractLLCompilerBasedTest()
             baseFirDiagnosticTestConfiguration(
                 frontendFacade = ::LowLevelFirFrontendFacade.bind(LLFirAnalyzerFacadeFactoryWithPreresolveInReversedOrder),
                 testDataConsistencyHandler = ::ReversedFirIdenticalChecker,
+                fixationLogsCollectionForbidden = true,
             )
 
             useAfterAnalysisCheckers(::LLFirOnlyReversedTestSuppressor)
