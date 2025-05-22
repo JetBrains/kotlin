90da84029d52f4a0d298085345785db47a36ead4
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-05-22 16:27:37 +0200

    wip: add Indexer to objc-export

--- native/objcexport-header-generator/build.gradle.kts
+++ native/objcexport-header-generator/build.gradle.kts
@@ -1,7 +1,26 @@
 @file:Suppress("HasPlatformType")
 
+import org.gradle.kotlin.dsl.api
+import org.gradle.kotlin.dsl.check
+import org.gradle.kotlin.dsl.creating
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getValue
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.libs
+import org.gradle.kotlin.dsl.project
+import org.gradle.kotlin.dsl.test
+import org.gradle.kotlin.dsl.testApi
+import org.gradle.kotlin.dsl.testImplementation
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.nativeDistribution.asProperties
+import org.jetbrains.kotlin.nativeDistribution.llvmDistributionSource
+import org.jetbrains.kotlin.nativeDistribution.nativeProtoDistribution
+import kotlin.apply
+
 plugins {
     kotlin("jvm")
+    id("native-dependencies")
 }
 
 sourceSets {
@@ -16,6 +35,8 @@ dependencies {
     api(project(":native:base"))
 
     testImplementation(project(":native:external-projects-test-utils"))
+    testImplementation(project(":kotlin-native:Interop:Indexer"))
+
     testImplementation(gradleTestKit())
 
     testApi(libs.junit.jupiter.api)
@@ -63,3 +84,29 @@ tasks.check.configure {
     dependsOn(":native:objcexport-header-generator-k1:check")
     dependsOn(":native:objcexport-header-generator-analysis-api:check")
 }
+
+tasks {
+    // Copy-pasted from Indexer build.gradle.kts.
+    withType<Test>().configureEach {
+        dependsOn(nativeDependencies.llvmDependency)
+        jvmArgumentProviders.add(objects.newInstance<TestArgumentProvider>().apply {
+            nativeLibraries.from(testCppRuntime)
+        })
+        val libclangPath = "${nativeDependencies.llvmPath}/" + if (org.jetbrains.kotlin.konan.target.HostManager.hostIsMingw) {
+            "bin/libclang.dll"
+        } else {
+            "lib/${System.mapLibraryName("clang")}"
+        }
+        systemProperty("kotlin.native.llvm.libclang", libclangPath)
+        systemProperty("kotlin.native.interop.stubgenerator.temp", layout.buildDirectory.dir("stubGeneratorTestTemp").get().asFile)
+
+        // Set the konan.home property because we run the cinterop tool not from a distribution jar
+        // so it will not be able to determine this path by itself.
+        systemProperty("konan.home", nativeProtoDistribution.root.asFile) // at most target description is required in the distribution.
+        systemProperty("kotlin.native.propertyOverrides", llvmDistributionSource.asProperties.entries.joinToString(separator = ";") {
+            "${it.key}=${it.value}"
+        })
+        environment["LIBCLANG_DISABLE_CRASH_RECOVERY"] = "1"
+    }
+}
+
