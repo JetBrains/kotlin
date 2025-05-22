1dae2d89be11b31aa96f13c2b1efcebd365e2c03
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-05-22 13:48:50 +0200

    [Gradle] explicitly disable isolated projects or fix tests in `MPPBuildReproducibilityIT`, `MppCompilerPluginsIT`, `MppConsumeFromNonKotlinIT`, `MppDiagnosticsIt`, `MppKlibIT` and `NativeIncrementalCompilationIT`
    ^KT-67291 In Progress

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/native-incremental-multi-project/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/native-incremental-multi-project/build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     id("org.jetbrains.kotlin.multiplatform")
 }
 
-allprojects {
-    repositories {
-        mavenCentral()
-        mavenLocal()
-    }
-}
-
 kotlin {
     <SingleNativeTarget>("host")
 }
