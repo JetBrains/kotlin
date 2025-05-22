901bc272fad764121731034806fb4a54fa4078c4
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-05-22 21:38:38 +0200

    [Inline] Copy inline functions from other modules on 1st stage

---
+++ compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/CopyInlineFunctionsToOurModuleLowering.kt
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.ir.inline
+
+import org.jetbrains.kotlin.backend.common.ModuleLoweringPass
+import org.jetbrains.kotlin.backend.common.PreSerializationLoweringContext
+import org.jetbrains.kotlin.backend.common.ir.Symbols
+import org.jetbrains.kotlin.backend.common.serialization.NonLinkingIrInlineFunctionDeserializer
+import org.jetbrains.kotlin.backend.common.serialization.isDeserializedFromOtherModule
+import org.jetbrains.kotlin.backend.common.serialization.signature.PublicIdSignatureComputer
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.descriptors.Modality
+import org.jetbrains.kotlin.ir.IrElement
+import org.jetbrains.kotlin.ir.declarations.IrFile
+import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
+import org.jetbrains.kotlin.ir.declarations.lazy.IrLazyDeclarationBase
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.irFlag
+import org.jetbrains.kotlin.ir.util.*
+import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
+import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
+import org.jetbrains.kotlin.ir.visitors.acceptVoid
+import org.jetbrains.kotlin.name.Name
+
+class CopyInlineFunctionsToOurModuleLowering(
+    private val context: PreSerializationLoweringContext,
+    irMangler: KotlinMangler.IrMangler,
+) : IrVisitorVoid(), ModuleLoweringPass {
+    private val externalInlineFunctionDeserializer = NonLinkingIrInlineFunctionDeserializer(
+        irBuiltIns = context.irBuiltIns,
+        signatureComputer = PublicIdSignatureComputer(irMangler),
+    )
+
+    private lateinit var currentFile: IrFile
+    private val copiedInlineFunctionsInCurrentFile = mutableListOf<IrSimpleFunction>()
+    private val originalToCopiedInlineFunctions = mutableMapOf<IrSimpleFunction, IrSimpleFunction>()
+
+    override fun lower(irModule: IrModuleFragment) {
+        for (file in irModule.files) {
+            currentFile = file
+            file.acceptVoid(this)
+            file.declarations += copiedInlineFunctionsInCurrentFile
+            copiedInlineFunctionsInCurrentFile.clear()
+        }
+    }
+
+    override fun visitElement(element: IrElement) {
+        element.acceptChildrenVoid(this)
+    }
+
+    override fun visitCall(expression: IrCall) {
+        val symbol = expression.symbol
+        if (!symbol.isBound) {
+            return super.visitCall(expression)
+        }
+        val originalRealFun = symbol.owner.resolveFakeOverrideOrSelf() as? IrSimpleFunction
+            ?: return super.visitCall(expression)
+
+        var targetFunction = originalToCopiedInlineFunctions[originalRealFun]
+        if (targetFunction == null) {
+            if (!shouldCopyFunction(originalRealFun)) {
+                return super.visitCall(expression)
+            }
+
+            targetFunction = originalRealFun
+            if (targetFunction.body == null) {
+                targetFunction = externalInlineFunctionDeserializer.deserializeInlineFunction(targetFunction) as IrSimpleFunction?
+                    ?: return super.visitCall(expression)
+            }
+            if (targetFunction.body == null) {
+                return super.visitCall(expression)
+            }
+
+            val oldSignature = targetFunction.symbol.signature!!.nearestPublicSig() as IdSignature.CommonSignature
+            val newName = createNameForCopiedFun(oldSignature.declarationFqName)
+            val newSignature = oldSignature.let {
+                IdSignature.CommonSignature(it.packageFqName, newName, it.id, it.mask, it.description)
+            }
+
+            fun remapSignatureInsideCopiedFun(oldSig: IdSignature?): IdSignature? = when (oldSig) {
+                null -> null
+                is IdSignature.CommonSignature -> {
+                    if (oldSig == targetFunction!!.symbol.signature) newSignature else oldSig
+                }
+                is IdSignature.CompositeSignature -> IdSignature.CompositeSignature(
+                    remapSignatureInsideCopiedFun(oldSig.container)!!,
+                    remapSignatureInsideCopiedFun(oldSig.inner)!!,
+                )
+                else -> oldSig
+            }
+
+            val symbolRemapper = DeepCopySymbolRemapper(signatureRemapper = ::remapSignatureInsideCopiedFun)
+            targetFunction.acceptVoid(symbolRemapper)
+            val deepCopy = DeepCopyIrTreeWithSymbols(symbolRemapper, DeepCopyTypeRemapper(symbolRemapper))
+            val functionCopy = targetFunction.transform(deepCopy, null) as IrSimpleFunction
+            functionCopy.patchDeclarationParents(currentFile)
+
+            functionCopy.name = Name.identifier(newName)
+            functionCopy.correspondingPropertySymbol = null
+            functionCopy.visibility = DescriptorVisibilities.PUBLIC
+            functionCopy.modality = Modality.FINAL
+
+            functionCopy.isInlineCopy = true
+            copiedInlineFunctionsInCurrentFile += functionCopy
+            originalToCopiedInlineFunctions[originalRealFun] = functionCopy
+            targetFunction = functionCopy
+
+            targetFunction.body?.acceptVoid(this)
+            targetFunction.parameters.forEach { param ->
+                if (expression.arguments[param.indexInParameters] == null) {
+                    param.defaultValue?.acceptVoid(this)
+                }
+            }
+        }
+
+        expression.symbol = targetFunction.symbol
+        super.visitCall(expression)
+    }
+
+    private fun createNameForCopiedFun(oldName: String): String {
+        val copyPrefix = "copy-of-inline-"
+        val originalName: String
+        var index = 0
+        if (oldName.startsWith(copyPrefix)) {
+            val prefixEnd = oldName.indexOf(':', copyPrefix.length)
+            index = oldName.substring(copyPrefix.length, prefixEnd).toInt()
+            originalName = oldName.substring(prefixEnd)
+        } else {
+            originalName = oldName.replace('.', '-')
+        }
+        return "$copyPrefix$index:$originalName"
+    }
+
+    private fun shouldCopyFunction(originalRealFun: IrSimpleFunction): Boolean {
+        if (!originalRealFun.isInline || originalRealFun.isInlineCopy) {
+            return false
+        }
+        if (Symbols.isTypeOfIntrinsic(originalRealFun.symbol)
+            || originalRealFun.symbol == context.symbols.coroutineContextGetter
+            || originalRealFun.name.asString() == "suspendCoroutineUninterceptedOrReturn"
+        ) {
+            return false
+        }
+        if (originalRealFun !is IrLazyDeclarationBase && !originalRealFun.isDeserializedFromOtherModule) {
+            // Function from the same module
+            return false
+        }
+
+        return true
+    }
+}
+
+private var IrFunction.isInlineCopy by irFlag(copyByDefault = true)
--- compiler/testData/codegen/box/aaa/adhoc.kt
+++ compiler/testData/codegen/box/aaa/adhoc.kt
@@ -1,32 +1,35 @@
-// WITH_STDLIB
-// WITH_REFLECT
-// TARGET_BACKEND: JVM
-// TARGET_BACKEND: NATIVE, JS_IR
-
-// MODULE: lib
-// FILE: lib.kt
+// FILE: A.kt
 package test;
 
 import kotlin.reflect.*
 
 class Pair<A, B>(val x: A, val y: B)
 
-inline fun <reified T1> typeOfX(x: T1) = typeOf<T1>()
-inline fun <reified T2, T3> typeOfPair(x: T2, y: T3) = typeOfX(Pair(x, y))
-inline fun <T4, T5> typeOfPair2(x: T4, y: T5) = typeOfPair(Pair(x, y), y)
+private inline fun <reified T1> typeOfX(x: T1) = typeOf<T1>()
+private inline fun <reified T2, T3> typeOfPair(x: T2, y: T3) = typeOfX(Pair(x, y))
+internal inline fun <T4, T5> typeOfPair2(x: T4, y: T5) = typeOfPair(Pair(x, y), y)
 
-inline fun <reified T> typeOfWrapped() = typeOf<T>()
-inline fun <reified T2, T3> typeOfPair3() = typeOfWrapped<Pair<T2, T3>>()
-
-// MODULE: main(lib)
-// FILE: main.kt
+// FILE: B.kt
 import test.*
 
 fun box() : String {
-    //if (typeOfPair2_local("1", 1) != "OK") return "FAIL 1"
-    //if (typeOfPair2("1", 1).toString() != "test.Pair<test.Pair<T4, T5>, T3>") return "FAIL 3: ${typeOfPair2("1", 1)}"
-    if (typeOfPair3<String, Int>().toString() != "test.Pair<kotlin.String, T3>") return "FAIL 4: ${typeOfPair3<String, Int>()}"
+    if (typeOfPair2("1", 1).toString() != "test.Pair<test.Pair<T4, T5>, T3>") return "FAIL 3: ${typeOfPair2("1", 1)}"
     return "OK"
 }
 
-//inline fun <T4, T5> typeOfPair2_local(x: T4, y: T5) = "OK"
+// FILE: A.kt
+class A
+
+// FILE: B.kt
+private fun A.privateExtension() = "OK"
+private inline fun A.privateInlineExtension1() = privateExtension()
+internal inline fun A.internalInlineExtension() = privateInlineExtension1() + "1"
+
+// FILE: main.kt
+internal fun topLevelFun() = A().internalInlineExtension() + "2"
+
+fun box(): String {
+    val result = topLevelFun()
+    if (result != "OK12") return result
+    return "OK"
+}
