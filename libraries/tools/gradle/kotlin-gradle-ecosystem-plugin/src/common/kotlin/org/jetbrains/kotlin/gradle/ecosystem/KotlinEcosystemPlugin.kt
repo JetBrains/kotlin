5b2cc0f6f2e18aa78da93061bb153ebf308f15ae
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-23 15:49:36 +0200

    [Gradle] Add tests validating dcl property behavior
    ^KT-77107 In Progress

--- libraries/tools/gradle/kotlin-gradle-ecosystem-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/ecosystem/KotlinEcosystemPlugin.kt
+++ libraries/tools/gradle/kotlin-gradle-ecosystem-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/ecosystem/KotlinEcosystemPlugin.kt
@@ -9,16 +9,17 @@ import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
 import org.gradle.api.logging.Logging
 import org.gradle.util.GradleVersion
-import org.jetbrains.kotlin.ecosystem.internal.declarative.KotlinDclStatus
-import org.jetbrains.kotlin.ecosystem.internal.declarative.KotlinDeclarativePlugin
-import org.jetbrains.kotlin.ecosystem.internal.declarative.dclStatus
+import org.jetbrains.kotlin.gradle.ecosystem.internal.declarative.DCL_STATUS_GRADLE_PROPERTY_NAME
+import org.jetbrains.kotlin.gradle.ecosystem.internal.declarative.KotlinDclStatus
+import org.jetbrains.kotlin.gradle.ecosystem.internal.declarative.KotlinDeclarativePlugin
+import org.jetbrains.kotlin.gradle.ecosystem.internal.declarative.dclStatus
 
 class KotlinEcosystemPlugin : Plugin<Settings> {
 
     private val logger = Logging.getLogger("KotlinEcosystemPlugin")
 
     override fun apply(settings: Settings) {
-        logger.warn("Kotlin Ecosystem plugin is experimental!")
+        logger.warn(EXPERIMENTAL_WARNING_MESSAGE)
 
         settings.applyDeclarativeGradleIfEnabled()
     }
@@ -29,13 +30,22 @@ class KotlinEcosystemPlugin : Plugin<Settings> {
             if (GradleVersion.current() == KotlinDeclarativePlugin.SUPPORTED_GRADLE_VERSION) {
                 settings.plugins.apply(KotlinDeclarativePlugin::class.java)
             } else {
-                logger.warn(
-                    "Kotlin Declarative Gradle support is disabled due to unsupported Gradle version: ${GradleVersion.current()}. " +
-                            "It only works with ${KotlinDeclarativePlugin.SUPPORTED_GRADLE_VERSION} Gradle release."
-                )
+                logger.warn(buildDclUnsupportedMessage(GradleVersion.current()))
             }
         } else {
-            logger.info("Kotlin Declarative Gradle is disabled")
+            logger.info(DCL_DISABLED_MESSAGE)
         }
+    }
+
+    companion object {
+        internal const val EXPERIMENTAL_WARNING_MESSAGE = "The Kotlin Ecosystem plugin ('org.jetbrains.kotlin.ecosystem') is experimental" +
+                " and may change in future releases!"
+        internal const val DCL_DISABLED_MESSAGE =
+            "Kotlin Declarative Gradle is disabled by the property '$DCL_STATUS_GRADLE_PROPERTY_NAME'."
+
+        internal fun buildDclUnsupportedMessage(
+            currentGradleVersion: GradleVersion
+        ) = "Support for Kotlin Declarative Gradle is only available with Gradle version " +
+                "${KotlinDeclarativePlugin.SUPPORTED_GRADLE_VERSION}. The current Gradle version is $currentGradleVersion."
     }
 }
