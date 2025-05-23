605402a924c703a3baff073a454b58dca1704b33
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-05-23 18:25:10 +0200

    ~ Some cleanup

--- compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/EmbedExternalInlinedFunctionLowering.kt
+++ compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/EmbedExternalInlinedFunctionLowering.kt
@@ -9,31 +9,16 @@ import org.jetbrains.kotlin.backend.common.LoweringContext
 import org.jetbrains.kotlin.backend.common.ModuleLoweringPass
 import org.jetbrains.kotlin.backend.common.serialization.NonLinkingIrInlineFunctionDeserializer
 import org.jetbrains.kotlin.backend.common.serialization.signature.PublicIdSignatureComputer
-import org.jetbrains.kotlin.ir.declarations.IrConstructor
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
-import org.jetbrains.kotlin.ir.declarations.impl.IrFileImpl
 import org.jetbrains.kotlin.ir.expressions.IrCall
-import org.jetbrains.kotlin.ir.expressions.IrConstructorCall
-import org.jetbrains.kotlin.ir.expressions.IrFunctionAccessExpression
-import org.jetbrains.kotlin.ir.symbols.IrConstructorSymbol
-import org.jetbrains.kotlin.ir.symbols.IrFunctionSymbol
 import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
-import org.jetbrains.kotlin.ir.symbols.impl.IrConstructorSymbolImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrFileSymbolImpl
-import org.jetbrains.kotlin.ir.symbols.impl.IrSimpleFunctionSymbolImpl
-import org.jetbrains.kotlin.ir.util.IdSignature
-import org.jetbrains.kotlin.ir.util.KotlinMangler
-import org.jetbrains.kotlin.ir.util.NaiveSourceBasedFileEntryImpl
-import org.jetbrains.kotlin.ir.util.deepCopyWithSymbols
-import org.jetbrains.kotlin.ir.util.erasedTopLevelCopy
-import org.jetbrains.kotlin.ir.util.render
+import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
-import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.ir.util.erasedTopLevelCopy
 import org.jetbrains.kotlin.ir.visitors.acceptVoid
 
 class EmbedExternalInlinedFunctionLowering(
@@ -46,70 +31,49 @@ class EmbedExternalInlinedFunctionLowering(
         signatureComputer = PublicIdSignatureComputer(irMangler)
     )
 
-    private lateinit var module: IrModuleFragment
-    private lateinit var externalInlinerFunctionsFile: IrFile
-    private val mappedInlineFunctions = mutableMapOf<IrFunctionSymbol, IrFunction>()
+    private lateinit var currentFile: IrFile
+    private val copiedInlineFunctionsInCurrentFile = mutableListOf<IrSimpleFunction>()
+    private val copiedInlineFunctions = mutableMapOf<IrSimpleFunctionSymbol, IrSimpleFunction>()
 
     override fun lower(irModule: IrModuleFragment) {
-        module = irModule
-        externalInlinerFunctionsFile = IrFileImpl(
-            NaiveSourceBasedFileEntryImpl("External inliner functions file"),
-            IrFileSymbolImpl(),
-            FqName("External inliner functions file"),
-            irModule,
-        )
-        irModule.files.add(0, externalInlinerFunctionsFile)
-
-        irModule.acceptChildrenVoid(this)
+        for (file in irModule.files) {
+            currentFile = file
+            file.acceptVoid(this)
+            file.declarations += copiedInlineFunctionsInCurrentFile
+            copiedInlineFunctionsInCurrentFile.clear()
+        }
     }
 
-    override fun visitFunctionAccess(expression: IrFunctionAccessExpression) {
-        var actualCallee = mappedInlineFunctions[expression.symbol]
-        if (actualCallee == null) {
-            actualCallee = (expression.symbol.owner as? IrSimpleFunction)?.erasedTopLevelCopy
+    override fun visitElement(element: IrElement) {
+        element.acceptChildrenVoid(this)
+    }
 
+    override fun visitCall(expression: IrCall) {
+        var actualCallee = copiedInlineFunctions[expression.symbol]
+        if (actualCallee == null) {
+            actualCallee = if (expression.symbol.isBound) expression.symbol.owner.erasedTopLevelCopy else null
             if (actualCallee == null) {
-                actualCallee = inlineFunctionResolver.getFunctionDeclarationToInline(expression)
-                    ?: return
+                actualCallee = inlineFunctionResolver.getFunctionDeclarationToInline(expression) as IrSimpleFunction?
+                    ?: return super.visitCall(expression)
                 if (actualCallee.body != null) {
                     // Function from the same module
-                    return
+                    return super.visitCall(expression)
                 }
                 val body = externalInlineFunctionDeserializer.deserializeInlineFunction(actualCallee)
-                    ?: return
+                    ?: return super.visitCall(expression)
 
-                val functionCopySignature = IdSignature.CompositeSignature(
-                    IdSignature.CommonSignature("externalInlineFunctionCopy", "abcd", null, 0L, null),
-                    actualCallee.symbol.signature!!
-                )
-                val functionCopySymbol = when(actualCallee) {
-                    is IrSimpleFunction -> IrSimpleFunctionSymbolImpl(signature = functionCopySignature)
-                    is IrConstructor -> IrConstructorSymbolImpl(signature = functionCopySignature)
-                }
+                val functionCopy = actualCallee.deepCopyWithSymbols(currentFile)
+                functionCopy.correspondingPropertySymbol = null
+                functionCopy.visibility = DescriptorVisibilities.PUBLIC
 
-                // todo: use functionCopySymbol
-                val functionCopy = actualCallee.deepCopyWithSymbols(externalInlinerFunctionsFile)
-                (functionCopy as? IrSimpleFunction)?.correspondingPropertySymbol = null
-                functionCopy.parent = externalInlinerFunctionsFile
-
-                externalInlinerFunctionsFile.declarations += functionCopy
-                mappedInlineFunctions[expression.symbol] = functionCopy
+                copiedInlineFunctionsInCurrentFile += functionCopy
+                copiedInlineFunctions[expression.symbol] = functionCopy
 
                 body.acceptChildrenVoid(this)
-                /*actualCallee.parameters.forEachIndexed { index, param ->
-                    if (expression.arguments[index] == null) {
-                        // Default values can recursively reference [callee] - transform only needed.
-                        param.defaultValue?.acceptVoid(this)
-                    }
-                }*/
             }
         }
 
-        when (expression) {
-            is IrCall -> expression.symbol = actualCallee as IrSimpleFunctionSymbol
-            is IrConstructorCall -> expression.symbol = actualCallee as IrConstructorSymbol
-            else -> TODO(expression.render())
-        }
-        expression.acceptChildrenVoid(this)
+        expression.symbol = actualCallee.symbol
+        super.visitCall(expression)
     }
 }
