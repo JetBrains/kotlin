ead410fbab147ab4472842abde11128b3d9afc3f
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-05-23 12:21:25 +0200

    fixup! [ObjCExport] Fix ObjCName annotation translation

--- native/objcexport-header-generator/testData/headers/propertiesAnnotatedWithObjCName/!propertiesAnnotatedWithObjCName.h
+++ native/objcexport-header-generator/testData/headers/propertiesAnnotatedWithObjCName/!propertiesAnnotatedWithObjCName.h
@@ -19,6 +19,16 @@ NS_ASSUME_NONNULL_BEGIN
 #endif
 
 __attribute__((objc_subclassing_restricted))
+@interface Foo : Base
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+
+@property(readonly) int32_t prop __attribute__((swift_name("prop")));
+@property(readonly) int32_t objCName __attribute__((swift_name("objCName")));
+@property(readonly) int32_t objCNameWithSwiftName __attribute__((swift_name("swiftNameWithObjCName")));
+@end
+
+__attribute__((objc_subclassing_restricted))
 @interface FooKt : Base
 + (int32_t)prop:(int32_t)receiver __attribute__((swift_name("prop(_:)")));
 + (int32_t)objCName:(int32_t)receiver __attribute__((swift_name("objCName(_:)")));
