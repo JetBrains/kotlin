082d24f37e6c7f5af581e86bf55119768ea6a161
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-05-26 17:39:51 +0200

    [Gradle] Use the default heap size for commonization
    Commoner parts were optimized, and now it should run fine with the
    default 4g heap size. Plus existed approach was overriding any
    user-provided heap size.
    
    ^KT-68256 Verification Pending

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/compilerRunner/GradleCliCommonizer.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/compilerRunner/GradleCliCommonizer.kt
@@ -85,7 +85,7 @@ private fun ObjectFactory.kotlinNativeCommonizerToolSpec(
     mainClass = property("org.jetbrains.kotlin.commonizer.cli.CommonizerCLI"),
     daemonEntryPoint = property("main"),
     classpath = toolClasspath,
-    jvmArgs = listProperty<String>().value(toolJvmArgs).also { it.add("-Xmx4g") },
+    jvmArgs = listProperty<String>().value(toolJvmArgs),
     shouldPassArgumentsViaArgFile = property<Boolean>().value(true),
     collectNativeCompilerMetrics = property<Boolean>().value(false),
 ).disableC2().enableAssertions()
