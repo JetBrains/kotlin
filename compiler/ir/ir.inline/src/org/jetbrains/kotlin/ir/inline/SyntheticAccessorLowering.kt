da41afeff1cf2d4b91165d08d604835d800ce835
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-05-27 22:17:57 +0200

    [Inline] Include private inline functions in accessor generator

--- compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/SyntheticAccessorLowering.kt
+++ compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/SyntheticAccessorLowering.kt
@@ -157,10 +157,7 @@ class SyntheticAccessorLowering(private val context: LoweringContext, isExecuted
         val generatedAccessors = currentFile::generatedAccessors.getOrSetIfNull(::GeneratedAccessors)
 
         override fun visitFunction(declaration: IrFunction, data: TransformerData?): IrStatement {
-            val newData = data ?: runIf(declaration.isInline && !declaration.symbol.isConsideredAsPrivateForInlining()) {
-                // By the time this lowering is executed, there must be no private inline functions; however,
-                // there are exceptions, for example, `suspendCoroutineUninterceptedOrReturn` which are somewhat magical.
-                // If we encounter one, ignore it.
+            val newData = data ?: runIf(declaration.isPubliclyAccessibleInline) {
                 TransformerData(declaration)
             }
 
