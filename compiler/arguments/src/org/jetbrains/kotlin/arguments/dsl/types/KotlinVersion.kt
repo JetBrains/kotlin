1bd0dea8e3109b12b749c4bb52091a057ac94974
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-05-27 15:48:41 +0300

    fixup! [Build] Switch the default language version to 2.3
    Stabilize 2.3.

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinVersion.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KotlinVersion.kt
@@ -130,6 +130,7 @@ enum class KotlinVersion(
         versionName = "2.3",
         releaseVersionsMetadata = KotlinReleaseVersionLifecycle(
             introducedVersion = KotlinReleaseVersion.v2_2_0,
+            stabilizedVersion = KotlinReleaseVersion.v2_3_0,
         )
     ),
     v2_4(
