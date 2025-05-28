6799a4675a64b82b5e2ffcdef89761578b27afa7
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2025-05-28 16:47:51 +0900

    [PSI] Introduce module structure for PSI splitting
    The PSI module is a monolith – it contains both the API layer, its
    implementation, stubs and the parser. The goal is to extract all non-API
    parts to separate modules.
    
    ^KT-76614

---
+++ compiler/psi/psi-api/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    id("jps-compatible")
+}
+
+repositories {
+    maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
+}
+
+dependencies {
+    api(project(":core:compiler.common"))
+    api(project(":compiler:util"))
+    api(project(":compiler:frontend.common"))
+    api(project(":kotlin-script-runtime"))
+
+    compileOnly(intellijCore())
+    compileOnly(libs.guava)
+    compileOnly(libs.intellij.fastutil)
+}
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
