b219ba72c6565f46daa078427d77ae60b3a15971
Andrei Shikov <ashikov@google.com>
2025-05-29 01:51:14 +0100

    Emit parameter names in Compose source information
    Required to support indy-desugared lambda parameters in Layout inspector.
    
    Bug: 408492167
    Test: Updated compiler tests

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposePlugin.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/ComposePlugin.kt
@@ -740,6 +740,13 @@ class ComposePluginRegistrar : CompilerPluginRegistrar() {
                 ?: emptySet()
             stableTypeMatchers.addAll(testingMatchers)
 
+            val jvmLambdaScheme = configuration.get(JVMConfigurationKeys.LAMBDAS)
+                ?: if (configuration.languageVersionSettings.supportsFeature(LanguageFeature.LightweightLambdas)) {
+                    JvmClosureGenerationScheme.INDY
+                } else {
+                    JvmClosureGenerationScheme.CLASS
+                }
+
             return ComposeIrGenerationExtension(
                 liveLiteralsEnabled = liveLiteralsEnabled,
                 liveLiteralsV2Enabled = liveLiteralsV2Enabled,
@@ -756,6 +763,7 @@ class ComposePluginRegistrar : CompilerPluginRegistrar() {
                 featureFlags = featureFlags,
                 skipIfRuntimeNotFound = skipIrLoweringIfRuntimeNotFound,
                 messageCollector = configuration.messageCollector,
+                indyJvmLambdasEnabled = jvmLambdaScheme == JvmClosureGenerationScheme.INDY,
             )
         }
     }
