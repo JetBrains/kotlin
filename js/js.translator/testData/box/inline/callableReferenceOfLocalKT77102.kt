af2d3ca005547a1aa536e11379b7886a077f40fb
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-05-29 22:13:41 +0200

    ~ Other possibly fixed tests

--- js/js.translator/testData/box/inline/callableReferenceOfLocalKT77102.kt
+++ js/js.translator/testData/box/inline/callableReferenceOfLocalKT77102.kt
@@ -1,6 +1,4 @@
 // LANGUAGE: +IrInlinerBeforeKlibSerialization
-// IGNORE_BACKEND: JS_IR, JS_IR_ES6
-// ^^^ KT-77102: Function 'CHECK_NOT_NULL' can not be called: Expression uses unlinked type parameter symbol '[ /updateStatus|{}updateStatus[0] <- Local[<BF>] ]:3:4:5' declared in file callableReferenceOfLocalKT77102.kt
 
 inline fun <T> inlineGenericTestFunction(f: () -> T) = f()
 
