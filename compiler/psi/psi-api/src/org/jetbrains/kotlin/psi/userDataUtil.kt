f835d00ab9bceb073a9285d99aa0c59a486e7490
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2025-05-29 16:29:40 +0900

    [PSI] Move out utility classes not used by the PSI API module
    ^KT-76614

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/userDataUtil.kt
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/userDataUtil.kt
@@ -18,33 +18,10 @@ package org.jetbrains.kotlin.psi
 
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.UserDataHolder
-import com.intellij.psi.PsiElement
 import kotlin.reflect.KProperty
 
 class UserDataProperty<in R : UserDataHolder, T : Any>(val key: Key<T>) {
     operator fun getValue(thisRef: R, desc: KProperty<*>) = thisRef.getUserData(key)
 
     operator fun setValue(thisRef: R, desc: KProperty<*>, value: T?) = thisRef.putUserData(key, value)
-}
-
-class NotNullableUserDataProperty<in R : UserDataHolder, T : Any>(val key: Key<T>, val defaultValue: T) {
-    operator fun getValue(thisRef: R, desc: KProperty<*>) = thisRef.getUserData(key) ?: defaultValue
-
-    operator fun setValue(thisRef: R, desc: KProperty<*>, value: T) {
-        thisRef.putUserData(key, if (value != defaultValue) value else null)
-    }
-}
-
-class CopyablePsiUserDataProperty<in R : PsiElement, T : Any>(val key: Key<T>) {
-    operator fun getValue(thisRef: R, property: KProperty<*>) = thisRef.getCopyableUserData(key)
-
-    operator fun setValue(thisRef: R, property: KProperty<*>, value: T?) = thisRef.putCopyableUserData(key, value)
-}
-
-class NotNullablePsiCopyableUserDataProperty<in R : PsiElement, T : Any>(val key: Key<T>, val defaultValue: T) {
-    operator fun getValue(thisRef: R, property: KProperty<*>) = thisRef.getCopyableUserData(key) ?: defaultValue
-
-    operator fun setValue(thisRef: R, property: KProperty<*>, value: T) {
-        thisRef.putCopyableUserData(key, if (value != defaultValue) value else null)
-    }
 }
