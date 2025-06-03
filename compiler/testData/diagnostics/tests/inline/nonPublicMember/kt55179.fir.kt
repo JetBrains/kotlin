boost-mw-poc/jetbrains_kotlin

[FIR] Check exposed types in resolved qualifiers
#KT-77709 Fixed

compiler/testData/diagnostics/tests/inline/nonPublicMember/kt55179.fir.kt
@@ -15,7 +15,7 @@ private class Foo {
 
 internal <!NOTHING_TO_INLINE!>inline<!> fun foo() {
     <!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING, LESS_VISIBLE_TYPE_IN_INLINE_ACCESSED_SIGNATURE_WARNING, PRIVATE_CLASS_MEMBER_FROM_INLINE!>Foo<!>()
-    Foo.<!PRIVATE_CLASS_MEMBER_FROM_INLINE!>Companion<!>
-    Foo.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING, LESS_VISIBLE_TYPE_IN_INLINE_ACCESSED_SIGNATURE_WARNING, PRIVATE_CLASS_MEMBER_FROM_INLINE!>buildFoo<!>()
-    Foo.Companion.Nested.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING!>bar<!>()
+    Foo.<!LESS_VISIBLE_TYPE_ACCESS_IN_INLINE_WARNING, PRIVATE_CLASS_MEMBER_FROM_INLINE!>Companion<!>
+    <!LESS_VISIBLE_TYPE_ACCESS_IN_INLINE_WARNING!>Foo<!>.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING, LESS_VISIBLE_TYPE_IN_INLINE_ACCESSED_SIGNATURE_WARNING, PRIVATE_CLASS_MEMBER_FROM_INLINE!>buildFoo<!>()
+    Foo.Companion.<!LESS_VISIBLE_TYPE_ACCESS_IN_INLINE_WARNING!>Nested<!>.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_WARNING!>bar<!>()
 }

JetBrains/kotlin

compiler/testData/diagnostics/tests/inline/nonPublicMember/kt55179.fir.kt
@@ -16,7 +16,7 @@ private class Foo {
 
 internal <!NOTHING_TO_INLINE!>inline<!> fun foo() {
     <!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_ERROR, LESS_VISIBLE_TYPE_IN_INLINE_ACCESSED_SIGNATURE_ERROR, PRIVATE_CLASS_MEMBER_FROM_INLINE!>Foo<!>()
-    Foo.<!PRIVATE_CLASS_MEMBER_FROM_INLINE!>Companion<!>
-    Foo.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_ERROR, LESS_VISIBLE_TYPE_IN_INLINE_ACCESSED_SIGNATURE_ERROR, PRIVATE_CLASS_MEMBER_FROM_INLINE!>buildFoo<!>()
-    Foo.Companion.Nested.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_ERROR!>bar<!>()
+    Foo.<!LESS_VISIBLE_TYPE_ACCESS_IN_INLINE_ERROR, PRIVATE_CLASS_MEMBER_FROM_INLINE!>Companion<!>
+    <!LESS_VISIBLE_TYPE_ACCESS_IN_INLINE_ERROR!>Foo<!>.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_ERROR, LESS_VISIBLE_TYPE_IN_INLINE_ACCESSED_SIGNATURE_ERROR, PRIVATE_CLASS_MEMBER_FROM_INLINE!>buildFoo<!>()
+    Foo.Companion.<!LESS_VISIBLE_TYPE_ACCESS_IN_INLINE_ERROR!>Nested<!>.<!LESS_VISIBLE_CONTAINING_CLASS_IN_INLINE_ERROR!>bar<!>()
 }