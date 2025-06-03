ab7206f61bab25acfa2abdd7cd8469d3e8bd4176
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-06-03 20:05:56 +0200

    [Analysis API] Add caching of built-in files in `BuiltinsVirtualFileProviderBaseImpl`

--- analysis/decompiled/decompiler-to-psi/src/org/jetbrains/kotlin/analysis/decompiler/psi/BuiltinsVirtualFileProvider.kt
+++ analysis/decompiled/decompiler-to-psi/src/org/jetbrains/kotlin/analysis/decompiler/psi/BuiltinsVirtualFileProvider.kt
@@ -39,19 +39,22 @@ abstract class BuiltinsVirtualFileProviderBaseImpl : BuiltinsVirtualFileProvider
     }
 
     override fun createBuiltinsScope(project: Project): GlobalSearchScope {
-        val builtInFiles = getBuiltinVirtualFiles()
-        return GlobalSearchScope.filesScope(project, builtInFiles)
+        return GlobalSearchScope.filesScope(project, builtinVirtualFilesCached)
     }
 
     protected abstract fun findVirtualFile(url: URL): VirtualFile?
 
-    override fun getBuiltinVirtualFiles(): Set<VirtualFile> = builtInUrls.mapNotNullTo(mutableSetOf()) { url ->
-        val file = findVirtualFile(url)
-        if (file == null) {
-            logger<BuiltinsVirtualFileProvider>().warn("VirtualFile not found for builtin $url")
+    private val builtinVirtualFilesCached: Set<VirtualFile> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        builtInUrls.mapNotNullTo(mutableSetOf()) { url ->
+            val file = findVirtualFile(url)
+            if (file == null) {
+                logger<BuiltinsVirtualFileProvider>().warn("VirtualFile not found for builtin $url")
+            }
+            file
         }
-        file
     }
+
+    override fun getBuiltinVirtualFiles(): Set<VirtualFile> = builtinVirtualFilesCached
 }
 
 class BuiltinsVirtualFileProviderCliImpl : BuiltinsVirtualFileProviderBaseImpl() {
