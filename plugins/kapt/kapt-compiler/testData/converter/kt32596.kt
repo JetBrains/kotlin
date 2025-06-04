7e90848cdf34520d8895e9504fba6fb31d7f3e23
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-06-04 10:07:08 +0200

    [Tests] Disable IR visibility checks in `kt32596.kt`
    ^KT-76723

--- plugins/kapt/kapt-compiler/testData/converter/kt32596.kt
+++ plugins/kapt/kapt-compiler/testData/converter/kt32596.kt
@@ -1,4 +1,6 @@
 // CORRECT_ERROR_TYPES
+// DISABLE_IR_VISIBILITY_CHECKS: ANY
+// ^^^ `@file:Suppress("UNRESOLVED_REFERENCE")` causes the visibility checker to crash when checking annotation visibility
 
 @file:Suppress("UNRESOLVED_REFERENCE", "ANNOTATION_ARGUMENT_MUST_BE_CONST")
 import kotlin.reflect.KClass
