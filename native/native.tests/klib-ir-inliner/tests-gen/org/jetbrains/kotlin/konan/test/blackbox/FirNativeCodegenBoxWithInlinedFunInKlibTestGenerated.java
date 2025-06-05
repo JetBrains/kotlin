bf8ae4bda991ee8788b547916ceb28737023568e
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-06-05 12:57:50 +0200

    Tests: add more tests for reified types in catch
     #KT-54363

--- native/native.tests/klib-ir-inliner/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxWithInlinedFunInKlibTestGenerated.java
+++ native/native.tests/klib-ir-inliner/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxWithInlinedFunInKlibTestGenerated.java
@@ -43480,6 +43480,30 @@ public class FirNativeCodegenBoxWithInlinedFunInKlibTestGenerated extends Abstra
         }
 
         @Test
+        @TestMetadata("nestedReifiedCatch.kt")
+        public void testNestedReifiedCatch() {
+          runTest("compiler/testData/codegen/box/reified/catchParameter/nestedReifiedCatch.kt");
+        }
+
+        @Test
+        @TestMetadata("nullableExceptionType.kt")
+        public void testNullableExceptionType() {
+          runTest("compiler/testData/codegen/box/reified/catchParameter/nullableExceptionType.kt");
+        }
+
+        @Test
+        @TestMetadata("nullableNothing.kt")
+        public void testNullableNothing() {
+          runTest("compiler/testData/codegen/box/reified/catchParameter/nullableNothing.kt");
+        }
+
+        @Test
+        @TestMetadata("reificationClash.kt")
+        public void testReificationClash() {
+          runTest("compiler/testData/codegen/box/reified/catchParameter/reificationClash.kt");
+        }
+
+        @Test
         @TestMetadata("tryCatchReifiedType.kt")
         public void testTryCatchReifiedType() {
           runTest("compiler/testData/codegen/box/reified/catchParameter/tryCatchReifiedType.kt");
