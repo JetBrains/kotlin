b01bdade1efa729634734f955e694e21e36d70fc
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-06-05 23:38:52 +0200

    [Native] Don't finish initialization phase after cacheBuilder has already done it.
    ^KT-76477

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanDriver.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/KonanDriver.kt
@@ -25,8 +25,8 @@ import org.jetbrains.kotlin.konan.target.CompilerOutputKind
 import org.jetbrains.kotlin.konan.target.KonanTarget
 import org.jetbrains.kotlin.library.uniqueName
 import org.jetbrains.kotlin.protobuf.ExtensionRegistryLite
-import org.jetbrains.kotlin.util.PhaseType
 import org.jetbrains.kotlin.util.PerformanceManager
+import org.jetbrains.kotlin.util.PotentiallyIncorrectPhaseTimeMeasurement
 import java.util.*
 
 private val softDeprecatedTargets = setOf(
@@ -129,7 +129,9 @@ class KonanDriver(
         performanceManager?.apply {
             targetDescription = "${konanConfig.moduleId}-${konanConfig.produce}"
             addSourcesStats(sourcesFiles.size, environment.countLinesOfCode(sourcesFiles))
-            notifyPhaseFinished(PhaseType.Initialization)
+            // there's a chance `cacheBuilder.build()` already finishes this phase at the beginning of spawned `runKonanDriver()`
+            @OptIn(PotentiallyIncorrectPhaseTimeMeasurement::class)
+            notifyCurrentPhaseFinishedIfNeeded()
         }
 
         NativeCompilerDriver(performanceManager).run(konanConfig, environment)
