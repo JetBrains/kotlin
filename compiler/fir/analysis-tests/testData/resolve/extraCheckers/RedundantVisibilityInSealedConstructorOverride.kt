88e1adfb864ae0343ab7fd91f84b2d241666019e
Empa <itsempa@users.noreply.github.com>
2025-06-10 10:42:22 +0200

    [FIR]: Fix wrong implicit visibility on Sealed classes.
    ^KT-76629 Fixed

--- compiler/fir/analysis-tests/testData/resolve/extraCheckers/RedundantVisibilityInSealedConstructorOverride.kt
+++ compiler/fir/analysis-tests/testData/resolve/extraCheckers/RedundantVisibilityInSealedConstructorOverride.kt
@@ -1,23 +1,24 @@
 // FIR_IDENTICAL
 // RUN_PIPELINE_TILL: FRONTEND
+// KT-76629
 
 sealed class A(val x: Int) {
-    protected constructor(s: String) : this(s.length)
+    <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(s: String) : this(s.length)
 
     class A1 : A(100)
     class A2 : A("String")
 }
 
 
-sealed class B protected constructor(val x: Int) {
-    protected constructor(s: String) : this(s.length)
+sealed class B <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(val x: Int) {
+    <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(s: String) : this(s.length)
 
     class B1 : B(100)
     class B2 : B("String")
 }
 
-sealed class C <!REDUNDANT_VISIBILITY_MODIFIER!>private<!> constructor(val x: Int) {
-    protected constructor(s: String) : this(s.length)
+sealed class C private constructor(val x: Int) {
+    <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(s: String) : this(s.length)
 
     class C1 : C(100)
     class C2 : C("String")
@@ -26,16 +27,16 @@ sealed class C <!REDUNDANT_VISIBILITY_MODIFIER!>private<!> constructor(val x: In
 sealed class C3 : <!INVISIBLE_REFERENCE!>C<!>(200)
 
 sealed class D {
-    protected constructor(x: Int)
-    protected constructor(s: String) : this(s.length)
+    <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(x: Int)
+    <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(s: String) : this(s.length)
 
     class D1 : D(100)
     class D2 : D("String")
 }
 
 sealed class E() {
-    <!REDUNDANT_VISIBILITY_MODIFIER!>private<!> constructor(x: Int) : this()
-    protected constructor(x: Byte) : this()
+    private constructor(x: Int) : this()
+    <!REDUNDANT_VISIBILITY_MODIFIER!>protected<!> constructor(x: Byte) : this()
     <!NON_PRIVATE_OR_PROTECTED_CONSTRUCTOR_IN_SEALED!>internal constructor(x: Short) : this()<!>
     <!NON_PRIVATE_OR_PROTECTED_CONSTRUCTOR_IN_SEALED!>public constructor(x: Long) : this()<!>
     constructor(x: Double) : this()
