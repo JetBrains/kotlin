a5ee4af4b099a52b21d78e4a3e05e488da90b0b7
Igor Chevdar <igor.chevdar@jetbrains.com>
2025-06-10 14:46:18 +0300

    fixup! [tests] Added a test for #KT-77448

--- compiler/testData/klib/partial-linkage/interop-changedFunction/main/m.kt
+++ compiler/testData/klib/partial-linkage/interop-changedFunction/main/m.kt
@@ -6,10 +6,11 @@ fun box() = abiTest {
     expectFailure(
         linkage(
             "Function 'foo' can not be called: No function found for symbol 'lib/foo|foo(kotlin.Int){}[100]'." +
-                    " Looks like a cinterop-generated library issue. It might be that one of the dependencies" +
-                    " was compiled with a different version of K/N compiler/cinterop tool than the version used to compile this binary." +
+                    " This looks like a cinterop-generated library issue. It could happen if there is a cinterop dependency" +
+                    " generated with a different version of the Kotlin/Native compiler than the version used to compile this binary." +
                     " Please check that the project configuration is correct and has consistent versions of all required dependencies." +
-                    " It's worth trying to recompile cinterop dependencies with the same K/N version used for compiling this binary." +
+                    " It's worth trying to regenerate cinterop dependencies with the same version of" +
+                    " the Kotlin/Native compiler that was used for compiling this binary." +
                     " See https://youtrack.jetbrains.com/issue/KT-78062 for more details."
         )
     ) {
