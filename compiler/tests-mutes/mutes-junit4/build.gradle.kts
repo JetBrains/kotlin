61e3c4676d1eb887a041685bd4475758d36e1149
Bogdan Mukvich <bogdan.mukvich@jetbrains.com>
2025-06-11 15:20:11 +0200

    Update commons-beanutils dependency
    KTI-2390

--- compiler/tests-mutes/mutes-junit4/build.gradle.kts
+++ compiler/tests-mutes/mutes-junit4/build.gradle.kts
@@ -6,7 +6,9 @@ plugins {
 dependencies {
     api(libs.junit4)
     implementation(project(":compiler:tests-mutes"))
-    api("com.nordstrom.tools:junit-foundation:17.2.2")
+    api("com.nordstrom.tools:junit-foundation:17.2.4")
+    // override beanutils dependency due to CVE-2025-48734
+    api(libs.apache.commons.beanutils)
 }
 
 sourceSets {
