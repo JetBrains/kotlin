8ff6ef4c7c4ee58d4caa014628c757ad491f69d4
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-06-11 16:41:21 +0200

    fixup! [K/N] Use correct source file of inlined local declarations

--- native/native.tests/testData/lldb/kt42208WithInlinedFunInKlib.kt
+++ native/native.tests/testData/lldb/kt42208WithInlinedFunInKlib.kt
@@ -1,8 +1,8 @@
 // KIND: STANDALONE_LLDB
 // FIR_IDENTICAL
 // FREE_COMPILER_ARGS: -XXLanguage:+IrInlinerBeforeKlibSerialization
-// IGNORE_NATIVE_K2: optimizationMode=DEBUG
-// ^^^ KT-76763: lost attributeOwnerId leads to misdetecting the origin of inlined function, thus corruption of fileEntry in debug info for `invoke` function.
+
+
 // FILE: kt42208-1.kt
 fun main() {
     foo()()
