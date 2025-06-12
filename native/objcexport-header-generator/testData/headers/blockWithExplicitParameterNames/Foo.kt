0d97eb2839d14dbcd6e55f17c1d2fa36fe89893b
Yijie Jiang <edisongz123@gmail.com>
2025-06-12 15:26:13 +0800

    [ObjCExport] Unify testData function body

--- native/objcexport-header-generator/testData/headers/blockWithExplicitParameterNames/Foo.kt
+++ native/objcexport-header-generator/testData/headers/blockWithExplicitParameterNames/Foo.kt
@@ -1,8 +1,8 @@
 class Foo {
-    fun bar0(cb: (result: Int) -> Unit) {}
-    fun bar1(cb: (result: @ParameterName("a") Int) -> Unit) {}
-    fun bar2(cb: (a: Int, b: Int, c: String) -> Unit) {}
-    fun bar3(cb: (int: Int, int_: Int, double: Double) -> Unit) {}
+    fun bar0(cb: (result: Int) -> Unit) = Unit
+    fun bar1(cb: (result: @ParameterName("a") Int) -> Unit) = Unit
+    fun bar2(cb: (a: Int, b: Int, c: String) -> Unit) = Unit
+    fun bar3(cb: (int: Int, int_: Int, double: Double) -> Unit) = Unit
 }
 
-fun Foo.bar4(cb: (int: Int, message: String) -> Unit)
+fun Foo.bar4(cb: (int: Int, message: String) -> Unit) = Unit
