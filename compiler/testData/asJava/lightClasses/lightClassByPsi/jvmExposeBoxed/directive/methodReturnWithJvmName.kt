0999149cbe020b0748d69db70628183986464699
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-06-13 17:49:31 +0200

    [LC] cleanup test data
    Drop misleading comment
    
    ^KT-78065

--- compiler/testData/asJava/lightClasses/lightClassByPsi/jvmExposeBoxed/directive/methodReturnWithJvmName.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/jvmExposeBoxed/directive/methodReturnWithJvmName.kt
@@ -6,7 +6,6 @@
 value class StringWrapper(val s: String)
 
 class Foo {
-    // Because of @JvmName, no exposed method can be generated, since it is unmangled
     @JvmName("foo")
     fun thenamedoesnotmatter(): StringWrapper = StringWrapper("OK")
 }
