46e5470b3e3436d66dc7fe50b16da8eeed2e9071
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-06-13 18:32:15 +0400

    [AA] Add tests for redundant smart casts
    KT-77674

--- analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/dataFlowInfoProvider/FirIdeNormalAnalysisSourceModuleHLSmartCastInfoTestGenerated.java
+++ analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/dataFlowInfoProvider/FirIdeNormalAnalysisSourceModuleHLSmartCastInfoTestGenerated.java
@@ -76,6 +76,18 @@ public class FirIdeNormalAnalysisSourceModuleHLSmartCastInfoTestGenerated extend
   }
 
   @Test
+  @TestMetadata("redundantSmartCast.kt")
+  public void testRedundantSmartCast() {
+    runTest("analysis/analysis-api/testData/components/dataFlowInfoProvider/smartCastInfo/redundantSmartCast.kt");
+  }
+
+  @Test
+  @TestMetadata("redundantSmartCastImplicitReceiver.kt")
+  public void testRedundantSmartCastImplicitReceiver() {
+    runTest("analysis/analysis-api/testData/components/dataFlowInfoProvider/smartCastInfo/redundantSmartCastImplicitReceiver.kt");
+  }
+
+  @Test
   @TestMetadata("smartcastAsReceiver_stable.kt")
   public void testSmartcastAsReceiver_stable() {
     runTest("analysis/analysis-api/testData/components/dataFlowInfoProvider/smartCastInfo/smartcastAsReceiver_stable.kt");
