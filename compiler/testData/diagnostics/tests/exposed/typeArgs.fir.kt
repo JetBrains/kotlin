4e4e77bb384f68a15b3084d88ed60e2c020ab140
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-06-16 14:54:02 +0300

    [Test] Adjust tests for `ReportExposedTypeForInternalTypeParameterBounds`
    ^KT-76136

--- compiler/testData/diagnostics/tests/exposed/typeArgs.fir.kt
+++ compiler/testData/diagnostics/tests/exposed/typeArgs.fir.kt
@@ -1,6 +1,5 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // RENDER_DIAGNOSTICS_FULL_TEXT
-// LATEST_LV_DIFFERENCE
 
 // FILE: case1.kt
 internal open class My
@@ -29,7 +28,7 @@ internal interface Inter {
 
 class Wrapper<T>(val it: T)
 
-fun <T: <!EXPOSED_TYPE_PARAMETER_BOUND_DEPRECATION_WARNING!>Inter?<!>> public(a: T & Any) = Wrapper(a)
+fun <T: <!EXPOSED_TYPE_PARAMETER_BOUND!>Inter?<!>> public(a: T & Any) = Wrapper(a)
 
 fun <!EXPOSED_FUNCTION_RETURN_TYPE!>other<!>() = public(object : Inter {})
 
