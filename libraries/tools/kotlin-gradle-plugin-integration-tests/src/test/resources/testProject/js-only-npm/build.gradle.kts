581438900c94dfed1a58817e38659d124be4274b
Alex Sokol / y9san9 <y9san9@gmail.com>
2025-06-16 08:43:10 +0200

    [Wasm, JS] Small fixes inside test of offline mode

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/js-only-npm/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+group = "com.example"
+version = "1.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        binaries.library()
+        nodejs()
+    }
+    sourceSets {
+        jsMain {
+            dependencies {
+                implementation(npm("decamelize", "6.0.0"))
+            }
+        }
+    }
+}
