b7f86054a825d7d3ad2338cdc1424e0fabeeea96
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-06-16 14:54:02 +0300

    [Test] Adjust tests for `ReportExposedTypeForInternalTypeParameterBounds`
    ^KT-76136

--- compiler/testData/diagnostics/tests/exposed/implictlyExposedAndSuppressed.kt
+++ compiler/testData/diagnostics/tests/exposed/implictlyExposedAndSuppressed.kt
@@ -1,7 +1,6 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // RENDER_DIAGNOSTICS_FULL_TEXT
 // LANGUAGE: -ReportExposedTypeForMoreCasesOfTypeParameterBounds, -ForbidInferOfInvisibleTypeAsReifiedVarargOrReturnType
-// LATEST_LV_DIFFERENCE
 
 // MODULE: a
 
@@ -15,12 +14,6 @@ fun <T: Inter?> public(a: T & Any) = Wrapper(a)
 
 @Suppress("EXPOSED_FUNCTION_RETURN_TYPE")
 fun other() = public(object : Inter {})
-
-// MODULE: b(a)
-
-fun test() {
-    other().it.foo() // ok in K1, invisible reference in K2
-}
 
 /* GENERATED_FIR_TAGS: anonymousObjectExpression, capturedType, classDeclaration, dnnType, functionDeclaration,
 integerLiteral, interfaceDeclaration, nullableType, outProjection, primaryConstructor, propertyDeclaration,
