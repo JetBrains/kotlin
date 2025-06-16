f43ec90ac437916ae9ccc308db359d277b9a7a84
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-06-16 15:59:25 +0300

    [Test] Adjust tests for `UnstableSmartcastOnDelegatedProperties`
    ^KT-76136

--- compiler/testData/diagnostics/tests/smartCasts/varnotnull/classDelegationDeprecated.fir.kt
+++ compiler/testData/diagnostics/tests/smartCasts/varnotnull/classDelegationDeprecated.fir.kt
@@ -1,5 +1,4 @@
-// RUN_PIPELINE_TILL: BACKEND
-// LATEST_LV_DIFFERENCE
+// RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-57417
 
 interface HasProperty {
@@ -9,7 +8,7 @@ interface HasProperty {
 class Test(delegate: HasProperty) : HasProperty by delegate
 
 fun test(a: Test) {
-    if (a.property != null) <!DEPRECATED_SMARTCAST_ON_DELEGATED_PROPERTY!>a.property<!> + 1
+    if (a.property != null) <!SMARTCAST_IMPOSSIBLE!>a.property<!> + 1
 }
 
 /* GENERATED_FIR_TAGS: additiveExpression, classDeclaration, equalityExpression, functionDeclaration, ifExpression,
