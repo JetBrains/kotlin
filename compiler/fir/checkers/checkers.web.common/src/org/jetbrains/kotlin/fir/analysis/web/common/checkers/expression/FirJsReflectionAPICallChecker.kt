22fd258433d5e95e96e68d7378b32259de6d153b
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-06-18 14:45:37 +0200

    [WASM][Tests] Add FirJsReflectionAPICallChecker
    ^KT-71533

--- compiler/fir/checkers/checkers.web.common/src/org/jetbrains/kotlin/fir/analysis/web/common/checkers/expression/FirJsReflectionAPICallChecker.kt
+++ compiler/fir/checkers/checkers.web.common/src/org/jetbrains/kotlin/fir/analysis/web/common/checkers/expression/FirJsReflectionAPICallChecker.kt
@@ -1,9 +1,9 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.fir.analysis.js.checkers.expression
+package org.jetbrains.kotlin.fir.analysis.web.common.checkers.expression
 
 import org.jetbrains.kotlin.KtSourceElement
 import org.jetbrains.kotlin.builtins.StandardNames
