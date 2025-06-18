3e4de9a61cd33f9ca3adb960c87ae04b7c9e1425
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2025-06-18 13:10:34 +0200

    Build: Use cache-redirector for Npm & Yarn

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/jsCacheRedirector.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/jsCacheRedirector.kt
@@ -6,13 +6,14 @@ import com.github.gradle.node.NodeExtension
 import com.github.gradle.node.NodePlugin
 import com.github.gradle.node.npm.task.NpmTask
 import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 
-const val NPM_REGISTRY = "https://redirector.kotlinlang.org/npm/kotlin-dependencies"
+const val NPM_REGISTRY = "https://cache-redirector.jetbrains.com/registry.npmjs.org"
 const val NODE_DIST = "https://cache-redirector.jetbrains.com/nodejs.org/dist"
 const val YARN_DIST = "https://cache-redirector.jetbrains.com/github.com/yarnpkg/yarn/releases/download"
 
@@ -25,24 +26,38 @@ fun Project.configureJsCacheRedirector() {
 
     project.allprojects {
         pluginManager.withPlugin("com.github.node-gradle.node") {
+            val npmRcFile = layout.projectDirectory.file(".npmrc")
             val npmSetRegistry = tasks.register<NpmTask>("npmSetRegistry") {
                 group = NodePlugin.NPM_GROUP
                 args.addAll(
                     listOf("config", "set", "registry", NPM_REGISTRY, "--location=project")
                 )
-                outputs.file(project.layout.projectDirectory.file(".npmrc"))
+                outputs.file(npmRcFile)
             }
+
             tasks.named("npmInstall").configure {
                 dependsOn(npmSetRegistry)
+            }
+
+            tasks.register<Delete>("cleanNpmRc") {
+                delete = setOf(npmRcFile)
+            }
+
+            tasks.named("clean").configure {
+                dependsOn("cleanNpmRc")
             }
         }
 
         plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+            val npmRcFile = layout.buildDirectory.file("js/.npmrc")
             tasks.named("kotlinNpmInstall").configure {
                 doFirst {
-                    layout.buildDirectory.file("js/.npmrc")
-                        .get().asFile.writeText("registry=$NPM_REGISTRY")
+                    npmRcFile.get().asFile.writeText("registry=$NPM_REGISTRY")
                 }
+            }
+
+            tasks.register<Delete>("cleanNpmRc") {
+                delete = setOf(npmRcFile)
             }
         }
     }
