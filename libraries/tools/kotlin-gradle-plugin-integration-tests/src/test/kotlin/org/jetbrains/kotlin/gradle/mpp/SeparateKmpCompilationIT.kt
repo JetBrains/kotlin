203bcad638b6f9065e03da5f9ae1dd16e3cc755d
Timofey Solonin <timofey.solonin@jetbrains.com>
2025-06-20 15:57:43 +0200

    [Gradle] Add test utility for testing FUS

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/SeparateKmpCompilationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/mpp/SeparateKmpCompilationIT.kt
@@ -100,7 +100,6 @@ class SeparateKmpCompilationIT : KGPBaseTest() {
                 ":compileKotlinJvm",
                 ":compileKotlinJs",
                 configurationCache = BuildOptions.ConfigurationCacheValue.DISABLED, // otherwise we would access GMT task outputs before the task execution
-                buildAction = ReturnFromBuildScriptAfterExecution.build
             )
             for ((targetName, particularCompileArgs) in compileArgs) {
                 val fragmentDependencies = particularCompileArgs.fragmentDependencies
@@ -399,7 +398,6 @@ class SeparateKmpCompilationIT : KGPBaseTest() {
             }.buildAndReturn(
                 ":compileKotlinLinuxArm64",
                 configurationCache = BuildOptions.ConfigurationCacheValue.DISABLED, // otherwise we would access GMT task outputs before the task execution
-                buildAction = ReturnFromBuildScriptAfterExecution.build
             )
             val fragmentDependencies = compileArgs.fragmentDependencies
             assert(fragmentDependencies != null) { "Fragment dependencies are not set" }
