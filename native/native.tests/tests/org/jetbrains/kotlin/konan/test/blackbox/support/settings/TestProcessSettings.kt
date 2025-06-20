d13bbd51376859d42f5088f4a7f5fe3fc9acc445
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-06-20 15:26:58 +0200

    [Native] Load trove4j for older compiler versions in KLIB compat tests
    ^KT-71957

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/settings/TestProcessSettings.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/settings/TestProcessSettings.kt
@@ -360,8 +360,12 @@ internal class CustomNativeCompiler(private val lazyNativeHome: Lazy<KotlinNativ
     val nativeHome: KotlinNativeHome get() = lazyNativeHome.value
 
     val lazyClassloader: Lazy<URLClassLoader> = lazy {
-        val nativeClassPath = setOf(
-            nativeHome.dir.resolve("konan/lib/trove4j.jar"), // to be removed after bumping `kotlin.internal.native.test.compat.customCompilerVersion` to 2.2.0+
+        val nativeClassPath = setOfNotNull(
+            nativeHome.dir.resolve("konan/lib/trove4j.jar").takeIf {
+                // This artifact was removed in Kotlin/Native 2.2.0-Beta1.
+                // But it is still available in older compiler versions, where we need to load it.
+                it.exists()
+            },
             nativeHome.dir.resolve("konan/lib/kotlin-native-compiler-embeddable.jar")
         )
             .map { it.toURI().toURL() }
