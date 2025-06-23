74f7bc92d4baa020d44acbfa7b943f692642afb3
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-06-24 00:59:30 +0200

    [Tests] Adjust testdata for diagnostics/wasmTests/jsInterop/jsExportPromiseUnit.kt
    ^KT-71533

--- compiler/testData/diagnostics/wasmTests/jsInterop/jsExportPromiseUnit.kt
+++ compiler/testData/diagnostics/wasmTests/jsInterop/jsExportPromiseUnit.kt
@@ -1,18 +1,13 @@
 // ISSUE: KT-57192
-// FIR_IDENTICAL
 // OPT_IN: kotlin.js.ExperimentalJsExport
-// Promise<Unit> wrongly raised:
-// K1: NON_EXPORTABLE_TYPE
-// K1,K2: UPPER_BOUND_VIOLATED
-// IGNORE_BACKEND: WASM
 
 import kotlin.js.Promise
 
 @JsExport
 fun fooJsNumber(p: Promise<JsNumber>): Promise<JsNumber>? = p
 
-@JsExport
-fun fooUnitReturn(): Promise<Unit>? = null
+<!NON_EXPORTABLE_TYPE!>@JsExport
+fun fooUnitReturn(): Promise<<!UPPER_BOUND_VIOLATED!>Unit<!>>?<!> = null
 
 @JsExport
-fun fooUnitArgument(p: Promise<Unit>) = p.hashCode()
+fun fooUnitArgument(<!NON_EXPORTABLE_TYPE!>p: Promise<<!UPPER_BOUND_VIOLATED!>Unit<!>><!>) = p.hashCode()
