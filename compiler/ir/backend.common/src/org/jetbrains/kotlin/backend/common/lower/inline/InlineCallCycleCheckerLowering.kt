b6a9db43d6ed2b8ec0c1ecb4c24e9f2d94be2a65
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2025-06-24 10:01:49 +0200

    fixup! [Inline] Add inline call cycle detection to the common prefix

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/inline/InlineCallCycleCheckerLowering.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/inline/InlineCallCycleCheckerLowering.kt
@@ -16,15 +16,18 @@ import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.KtDiagnosticReporterWithImplicitIrBasedContext
 import org.jetbrains.kotlin.ir.declarations.IrFunction
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.declarations.IrValueParameter
+import org.jetbrains.kotlin.ir.expressions.IrBody
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.impl.IrErrorCallExpressionImpl
 import org.jetbrains.kotlin.ir.util.file
 import org.jetbrains.kotlin.ir.util.render
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
-import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
-import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
-import org.jetbrains.kotlin.ir.visitors.acceptVoid
+import org.jetbrains.kotlin.ir.visitors.IrVisitor
+import org.jetbrains.kotlin.utils.filterIsInstanceAnd
+
+typealias CallNode = Pair<IrFunction, IrElement>
 
 @PhaseDescription("InlineCallCycleCheckerLowering")
 class InlineCallCycleCheckerLowering<Context : PreSerializationLoweringContext>(val context: Context) : ModuleLoweringPass {
@@ -35,67 +38,95 @@ class InlineCallCycleCheckerLowering<Context : PreSerializationLoweringContext>(
         )
 
         val callsInInlineCycle = mutableSetOf<IrCall>()
-        irModule.acceptVoid(IrInlineCallCycleChecker(irDiagnosticReporter, callsInInlineCycle))
+        val callGraph = mutableMapOf<CallNode, MutableSet<Pair<IrCall, CallNode>>>()
+
+        irModule.accept(IrInlineCallGraphBuilder(callGraph), listOf())
+        traverseCallGraph(callGraph, irDiagnosticReporter, callsInInlineCycle)
         irModule.accept(IrInlineCallCycleRemover(callsInInlineCycle), null)
+    }
+
+    private fun traverseCallGraph(
+        callGraph: MutableMap<CallNode, MutableSet<Pair<IrCall, CallNode>>>,
+        diagnosticReporter: IrDiagnosticReporter,
+        callsInInlineCycle: MutableSet<IrCall>,
+    ) {
+        val visited = mutableSetOf<CallNode>()
+        val completed = mutableSetOf<CallNode>()
+        val inlineCallsStack = mutableListOf<Pair<IrCall?, CallNode>>()
+
+        fun reportInlineCallCycle(element: IrCall?, callee: IrFunction) {
+            if (element == null) return
+            callsInInlineCycle.add(element)
+            diagnosticReporter.at(element, callee.file).report(CommonBackendErrors.INLINE_CALL_CYCLE, callee.name)
+        }
+
+        fun CallNode.dfs(call: IrCall?) {
+            if (visited.contains(this)) {
+                if (!completed.contains(this)) {
+                    reportInlineCallCycle(call, this.first)
+                    inlineCallsStack.takeLastWhile { (_, callNode) -> callNode != this }.forEach { (call, callNode) ->
+                        reportInlineCallCycle(call, callNode.first)
+                    }
+                }
+                return
+            }
+
+            inlineCallsStack += Pair(call, this)
+            visited += this
+
+            callGraph[this]?.forEach { (call, node) -> node.dfs(call) }
+
+            inlineCallsStack.removeLast()
+            completed += this
+
+        }
+
+        callGraph.keys.forEach { it.dfs(null) }
     }
 }
 
-class IrInlineCallCycleChecker(
-    private val diagnosticReporter: IrDiagnosticReporter,
-    private val callsInInlineCycle: MutableSet<IrCall>,
-) : IrVisitorVoid() {
-    private val inlineCallsAndDeclarationsStack = mutableListOf<Any>()
-    private val inlineDeclarationsBeingProcessed = mutableSetOf<IrFunction>()
-
-    override fun visitElement(element: IrElement) {
-        element.acceptChildrenVoid(this)
+class IrInlineCallGraphBuilder(
+    private val callGraph: MutableMap<CallNode, MutableSet<Pair<IrCall, CallNode>>>,
+) : IrVisitor<Unit, List<IrElement>>() {
+    override fun visitElement(element: IrElement, data: List<IrElement>) {
+        element.acceptChildren(this, data)
     }
 
-    override fun visitFunction(declaration: IrFunction) {
-        declaration.body?.acceptChildrenVoid(this)
+    override fun visitFunction(declaration: IrFunction, data: List<IrElement>) {
+        super.visitFunction(declaration, data + declaration)
     }
 
-    override fun visitCall(expression: IrCall) {
-        if (expression in callsInInlineCycle) return
+    override fun visitBody(body: IrBody, data: List<IrElement>) {
+        super.visitBody(body, data + body)
+    }
+
+    override fun visitValueParameter(declaration: IrValueParameter, data: List<IrElement>) {
+        super.visitValueParameter(declaration, data + declaration)
+    }
+
+    override fun visitCall(expression: IrCall, data: List<IrElement>) {
+        val inlineFunParents = data.filterIsInstanceAnd<IrFunction> { it.isInline }
+
         val callee = expression.symbol.owner
-        if (!callee.isInline || !canEnterInlineDeclaration(expression, callee)) return
+        if (!callee.isInline) return super.visitCall(expression, data)
 
-        enterInlineDeclaration(expression, callee)
-        visitFunction(callee)
-        exitInlineDeclaration()
+        inlineFunParents.forEach { addEdges(it, callee, expression, data) }
 
-        return super.visitCall(expression)
+        return super.visitCall(expression, data)
     }
 
+    private fun addEdges(caller: IrFunction, callee: IrFunction, call: IrCall, data: List<IrElement>) {
+        val callLocationIndex = data.indexOf(caller) + 1
 
-    private fun canEnterInlineDeclaration(element: IrCall, callee: IrFunction): Boolean {
-        if (callee in inlineDeclarationsBeingProcessed) {
-            reportInlineCallCycle(element, callee)
-            for ((call, callee) in inlineCallsAndDeclarationsStack.dropWhile { it != callee }.zipWithNext()) {
-                if (call is IrCall && callee is IrFunction) {
-                    reportInlineCallCycle(call, callee)
-                }
-            }
-            return false
+        data.getOrNull(callLocationIndex)?.let { callLocation ->
+            val parametersUsingDefaultValues =
+                callee.parameters.filter { it.defaultValue != null && call.arguments[it] == null }
+
+            val callNodes =
+                (parametersUsingDefaultValues + callee.body).filterNotNull().map { Pair(call, Pair(callee, it)) }
+
+            callGraph.getOrPut(Pair(caller, callLocation)) { mutableSetOf() }.addAll(callNodes)
         }
-        return true
-    }
-
-    private fun reportInlineCallCycle(element: IrCall, callee: IrFunction) {
-        callsInInlineCycle.add(element)
-        diagnosticReporter.at(element, callee.file).report(CommonBackendErrors.INLINE_CALL_CYCLE, callee.name)
-
-    }
-
-    private fun enterInlineDeclaration(call: IrCall, callee: IrFunction) {
-        inlineCallsAndDeclarationsStack.add(call)
-        inlineCallsAndDeclarationsStack.add(callee)
-        inlineDeclarationsBeingProcessed.add(callee)
-    }
-
-    private fun exitInlineDeclaration() {
-        inlineDeclarationsBeingProcessed.remove(inlineCallsAndDeclarationsStack.removeLast())
-        inlineCallsAndDeclarationsStack.removeLast()
     }
 }
 
