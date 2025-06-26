a208c03f9fff911f1bde4c933026dfb915a4761a
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-06-26 19:55:31 +0200

    [stubs] inherit KotlinPropertyAccessorStub from KotlinDeclarationWithBodyStub
    It adds `mayHaveContract` into it
    
    ^KT-76490

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtPropertyAccessorElementType.java
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtPropertyAccessorElementType.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.elements;
@@ -22,6 +11,7 @@ import com.intellij.psi.stubs.StubOutputStream;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.psi.KtPropertyAccessor;
+import org.jetbrains.kotlin.psi.psiUtil.KtPsiUtilKt;
 import org.jetbrains.kotlin.psi.stubs.KotlinPropertyAccessorStub;
 import org.jetbrains.kotlin.psi.stubs.impl.KotlinPropertyAccessorStubImpl;
 
@@ -34,7 +24,13 @@ public class KtPropertyAccessorElementType extends KtStubElementType<KotlinPrope
 
     @Override
     public KotlinPropertyAccessorStub createStub(@NotNull KtPropertyAccessor psi, StubElement parentStub) {
-        return new KotlinPropertyAccessorStubImpl(parentStub, psi.isGetter(), psi.hasBody(), psi.hasBlockBody());
+        return new KotlinPropertyAccessorStubImpl(
+                parentStub,
+                psi.isGetter(),
+                psi.hasBody(),
+                psi.hasBlockBody(),
+                KtPsiUtilKt.isContractPresentPsiCheck(psi)
+        );
     }
 
     @Override
@@ -42,6 +38,7 @@ public class KtPropertyAccessorElementType extends KtStubElementType<KotlinPrope
         dataStream.writeBoolean(stub.isGetter());
         dataStream.writeBoolean(stub.hasBody());
         dataStream.writeBoolean(stub.hasBlockBody());
+        dataStream.writeBoolean(stub.mayHaveContract());
     }
 
     @NotNull
@@ -50,6 +47,7 @@ public class KtPropertyAccessorElementType extends KtStubElementType<KotlinPrope
         boolean isGetter = dataStream.readBoolean();
         boolean hasBody = dataStream.readBoolean();
         boolean hasBlockBody = dataStream.readBoolean();
-        return new KotlinPropertyAccessorStubImpl(parentStub, isGetter, hasBody, hasBlockBody);
+        boolean mayHaveContract = dataStream.readBoolean();
+        return new KotlinPropertyAccessorStubImpl(parentStub, isGetter, hasBody, hasBlockBody, mayHaveContract);
     }
 }
