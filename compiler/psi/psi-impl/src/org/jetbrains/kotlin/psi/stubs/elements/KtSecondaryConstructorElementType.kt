41eb4efed4c8c167a3da089b4ca375a4f0a0edbd
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-06-26 20:05:39 +0200

    [stubs] inherit KotlinConstructorStub from KotlinDeclarationWithBodyStub
    It adds `mayHaveContract` and `hasBlockBody` into it
    
    ^KT-76490

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtSecondaryConstructorElementType.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtSecondaryConstructorElementType.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 package org.jetbrains.kotlin.psi.stubs.elements
@@ -18,11 +18,16 @@ class KtSecondaryConstructorElementType(debugName: String) :
         hasBody: Boolean,
         isDelegatedCallToThis: Boolean,
         isExplicitDelegationCall: Boolean,
-    ): KotlinConstructorStub<KtSecondaryConstructor> {
-        return KotlinConstructorStubImpl(
-            parentStub, KtStubElementTypes.SECONDARY_CONSTRUCTOR, nameRef, hasBody, isDelegatedCallToThis, isExplicitDelegationCall
-        )
-    }
+        mayHaveContract: Boolean,
+    ): KotlinConstructorStub<KtSecondaryConstructor> = KotlinConstructorStubImpl(
+        parent = parentStub,
+        elementType = KtStubElementTypes.SECONDARY_CONSTRUCTOR,
+        containingClassName = nameRef,
+        hasBody = hasBody,
+        isDelegatedCallToThis = isDelegatedCallToThis,
+        isExplicitDelegationCall = isExplicitDelegationCall,
+        mayHaveContract = mayHaveContract,
+    )
 
     override fun isDelegatedCallToThis(constructor: KtSecondaryConstructor) = constructor.getDelegationCallOrNull()?.isCallToThis ?: true
 
