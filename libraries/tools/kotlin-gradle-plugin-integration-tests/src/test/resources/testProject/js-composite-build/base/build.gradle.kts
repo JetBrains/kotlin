358adbdd03991c40f997b9f5f522b878bc3e7b87
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2025-06-27 19:49:26 +0200

    [Wasm, JS] Use mutex and limit network concurrency to prevent Yarn network problems
    ^KT-78504 fixed
    
    (cherry picked from commit 0113ffbeaff048a4817296b8cb3be7a8feebca36)

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/js-composite-build/base/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/js-composite-build/base/build.gradle.kts
@@ -18,20 +18,6 @@ tasks.named("browserTest") {
     enabled = false
 }
 
-rootProject.tasks
-    .withType(org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask::class.java)
-    .named("kotlinNpmInstall")
-    .configure {
-        args.addAll(
-            listOf(
-                "--network-concurrency",
-                "1",
-                "--mutex",
-                "network"
-            )
-        )
-    }
-
 dependencies {
     implementation(kotlin("stdlib-js"))
     implementation(npm("decamelize", "1.1.1"))
--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnBasics.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/yarn/YarnBasics.kt
@@ -30,7 +30,17 @@ abstract class YarnBasics : NpmApiExecution<YarnEnvironment> {
                 )
                 .plus(
                     if (yarn.ignoreScripts) "--ignore-scripts" else ""
-                ).filter { it.isNotEmpty() }
+                )
+                .plus(
+                    // It is necessary for different Yarn processes to not interfere with each other
+                    listOf(
+                        "--network-concurrency",
+                        "1",
+                        "--mutex",
+                        "network",
+                    )
+                )
+                .filter { it.isNotEmpty() }
 
             val nodeExecutable = nodeJs.nodeExecutable
             if (!yarn.ignoreScripts) {
