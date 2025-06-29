17bb80f786145c968d5112b129701622bbc6879a
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-06-29 19:06:14 +0200

    [Test] Extend test kt47450.kt and prevent K1 from testing it.
    ^KT-78587 Fixed

--- compiler/testData/ir/irText/expressions/kt47450.kt
+++ compiler/testData/ir/irText/expressions/kt47450.kt
@@ -2,9 +2,22 @@
 // SKIP_KT_DUMP
 // RUN_PIPELINE_TILL: FIR2IR
 // DISABLE_NEXT_PHASE_SUGGESTION: Backend flackyly fails with stack overflow error for both K1 and K2
+// LANGUAGE: +EnhanceNullabilityOfPrimitiveArrays
+// ^^^ To avoid flaky testing on K1, a random K2.0 language feature is used
 
 fun test(z: Int): String {
     return "" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
             "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
+            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
+            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
+            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
+            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
+            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
+            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
+            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
+            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
+            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
+            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
             z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z
+// Note: Adding 2 more lines results in SOE in IrFileValidator on CI during FirLightTreeJvmIrTextTestGenerated
 }
