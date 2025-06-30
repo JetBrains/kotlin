807b37a48a4ae0bbef93e82fbd40520d2a5d99a5
Nikita Nazarov <nikitanazarov@google.com>
2025-06-30 13:56:56 +0000

    Enable unbound symbol generation
    b/428225169

--- compiler/cli/src/org/jetbrains/kotlin/cli/jklib/K2JKlibCompiler.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/jklib/K2JKlibCompiler.kt
@@ -489,7 +489,9 @@ class K2JKlibCompiler : CLICompiler<K2JKlibCompilerArguments>() {
                 mainModule, symbolTable, irBuiltIns,
                 DescriptorByIdSignatureFinderImpl(mainModule, mangler),
                 JvmGeneratorExtensionsImpl(configuration)
-            )
+            ).apply {
+                unboundSymbolGeneration = true
+            }
         val linker = JKlibLinker(
             module = mainModule,
             messageCollector = messageCollector,
