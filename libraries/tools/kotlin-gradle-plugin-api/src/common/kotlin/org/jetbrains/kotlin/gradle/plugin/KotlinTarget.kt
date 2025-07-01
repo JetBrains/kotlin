14a9091e4764b03a5c029bdc9eeee7aab707d388
Adam Semenenko <adam.semenenko@jetbrains.com>
2025-07-01 20:37:55 +0200

    [Gradle] Remove unused `Suppress("TYPEALIAS_EXPANSION_DEPRECATION")`

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTarget.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinTarget.kt
@@ -3,8 +3,6 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:Suppress("TYPEALIAS_EXPANSION_DEPRECATION")
-
 package org.jetbrains.kotlin.gradle.plugin
 
 import org.gradle.api.Action
