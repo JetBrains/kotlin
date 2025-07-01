f12d419b19a368e637c6987ac1ebfc605cb3a96b
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-07-01 12:15:22 +0300

    KT-78725: Use GC binary option in native tests
    A simple example showing that binary options are available in tests.
    At some point at the future we should remove all duplicates and use
    a single source of truth.

--- native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/compilation/TestCompilation.kt
+++ native/native.tests/tests/org/jetbrains/kotlin/konan/test/blackbox/support/compilation/TestCompilation.kt
@@ -92,7 +92,7 @@ abstract class BasicCompilation<A : TestCompilationArtifact>(
 
         threadStateChecker.compilerFlag?.let { compilerFlag -> add(compilerFlag) }
         sanitizer.compilerFlag?.let { compilerFlag -> add(compilerFlag) }
-        gcType.compilerFlag?.let { compilerFlag -> add(compilerFlag) }
+        gcType.gc?.let { gc -> add("-Xbinary=gc=${gc.shortcut ?: gc.name}") }
         gcScheduler.compilerFlag?.let { compilerFlag -> add(compilerFlag) }
         allocator.compilerFlag?.let { compilerFlag -> add(compilerFlag) }
 
