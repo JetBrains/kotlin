7dbd05c7c686e17be4decfb31e1ac05d3432bce4
Andrii Rublov <andrii.rublov@jetbrains.com>
2025-07-02 21:52:27 +0200

    [K/JS]: WIP KT-78742

--- js/js.parser/build.gradle.kts
+++ js/js.parser/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
 }
 
 dependencies {
+    implementation("com.caoccao.javet:swc4j:1.6.0") // Must Have
+    implementation("com.caoccao.javet:swc4j-linux-arm64:1.6.0")
+    implementation("com.caoccao.javet:swc4j-linux-x86_64:1.6.0")
+    implementation("com.caoccao.javet:swc4j-macos-arm64:1.6.0")
+    implementation("com.caoccao.javet:swc4j-macos-x86_64:1.6.0")
+    implementation("com.caoccao.javet:swc4j-windows-arm64:1.6.0")
+    implementation("com.caoccao.javet:swc4j-windows-x86_64:1.6.0")
+
     api(kotlinStdlib())
     api(project(":js:js.ast"))
     compileOnly(intellijCore())
