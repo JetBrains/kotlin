3b96c62379bc513094e080f223fba749e46a2bb3
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-07-04 17:50:02 +0400

    [AA] KaTypeMappingMode.RETURN_TYPE_FUNCTIONS: convert Unit to void
    ^KT-63464 Fixed

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirJavaInteroperabilityComponent.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirJavaInteroperabilityComponent.kt
@@ -122,6 +122,12 @@ internal class KaFirJavaInteroperabilityComponent(
 
         if (!rootModuleSession.moduleData.platform.has<JvmPlatform>() && !allowNonJvmPlatforms) return null
 
+        if (mode == KaTypeMappingMode.RETURN_TYPE_FUNCTIONS && !isAnnotationMethod && coneType.isUnit) {
+            // Here we approximate the JVM backend logic from `MethodSignatureMapper#hasVoidReturnType`.
+            // But we do it only in the special type mapping mode `RETURN_TYPE_FUNCTIONS` because it is not applicable in other cases.
+            return PsiTypes.voidType()
+        }
+
         val mappingMode = mode.toTypeMappingMode(this, isAnnotationMethod, suppressWildcards)
         val typeElement = coneType.simplifyType(rootModuleSession, useSitePosition).asPsiTypeElement(
             mode = mappingMode,
