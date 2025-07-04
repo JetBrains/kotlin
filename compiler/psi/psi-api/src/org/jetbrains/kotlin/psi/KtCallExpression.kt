bc7e7722a32d3f36b0d1d0e83276c083a71a63d5
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-07-04 16:55:00 +0200

    [psi] convert KtCallExpression to Kotlin
    ^KT-77097

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtCallExpression.kt
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtCallExpression.kt
@@ -1,56 +1,27 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.psi
 
-package org.jetbrains.kotlin.psi;
+import com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.KtNodeTypes
 
-import com.google.common.collect.Lists;
-import com.intellij.lang.ASTNode;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.KtNodeTypes;
-
-import java.util.Collections;
-import java.util.List;
-
-public class KtCallExpression extends KtExpressionImpl implements KtCallElement, KtReferenceExpression {
-    public KtCallExpression(@NotNull ASTNode node) {
-        super(node);
+open class KtCallExpression(node: ASTNode) : KtExpressionImpl(node), KtCallElement, KtReferenceExpression {
+    override fun <R, D> accept(visitor: KtVisitor<R, D>, data: D): R {
+        return visitor.visitCallExpression(this, data)
     }
 
-    @Override
-    public <R, D> R accept(@NotNull KtVisitor<R, D> visitor, D data) {
-        return visitor.visitCallExpression(this, data);
+    override fun getCalleeExpression(): KtExpression? {
+        return findChildByClass(KtExpression::class.java)
     }
 
-    @Override
-    @Nullable
-    public KtExpression getCalleeExpression() {
-        return findChildByClass(KtExpression.class);
+    override fun getValueArgumentList(): KtValueArgumentList? {
+        return findChildByType(KtNodeTypes.VALUE_ARGUMENT_LIST)
     }
 
-    @Override
-    @Nullable
-    public KtValueArgumentList getValueArgumentList() {
-        return (KtValueArgumentList) findChildByType(KtNodeTypes.VALUE_ARGUMENT_LIST);
-    }
-
-    @Override
-    @Nullable
-    public KtTypeArgumentList getTypeArgumentList() {
-        return (KtTypeArgumentList) findChildByType(KtNodeTypes.TYPE_ARGUMENT_LIST);
+    override fun getTypeArgumentList(): KtTypeArgumentList? {
+        return findChildByType(KtNodeTypes.TYPE_ARGUMENT_LIST)
     }
 
     /**
@@ -58,31 +29,18 @@ public class KtCallExpression extends KtExpressionImpl implements KtCallElement,
      * The returned value is a list for better handling of commonly made mistake of a function taking a lambda and returning another function.
      * Most of users can simply ignore lists of more than one element.
      */
-    @Override
-    @NotNull
-    public List<KtLambdaArgument> getLambdaArguments() {
-        return findChildrenByType(KtNodeTypes.LAMBDA_ARGUMENT);
+    override fun getLambdaArguments(): List<KtLambdaArgument> {
+        return findChildrenByType(KtNodeTypes.LAMBDA_ARGUMENT)
     }
 
-    @Override
-    @NotNull
-    public List<KtValueArgument> getValueArguments() {
-        KtValueArgumentList list = getValueArgumentList();
-        List<KtValueArgument> valueArgumentsInParentheses = list != null ? list.getArguments() : Collections.emptyList();
-        List<KtLambdaArgument> functionLiteralArguments = getLambdaArguments();
-        if (functionLiteralArguments.isEmpty()) {
-            return valueArgumentsInParentheses;
+    override fun getValueArguments(): List<KtValueArgument> {
+        val valueArgumentsInParentheses = valueArgumentList?.arguments.orEmpty()
+        val functionLiteralArguments = lambdaArguments.ifEmpty {
+            return valueArgumentsInParentheses
         }
-        List<KtValueArgument> allValueArguments = Lists.newArrayList();
-        allValueArguments.addAll(valueArgumentsInParentheses);
-        allValueArguments.addAll(functionLiteralArguments);
-        return allValueArguments;
+
+        return valueArgumentsInParentheses + functionLiteralArguments
     }
 
-    @Override
-    @NotNull
-    public List<KtTypeProjection> getTypeArguments() {
-        KtTypeArgumentList list = getTypeArgumentList();
-        return list != null ? list.getArguments() : Collections.emptyList();
-    }
+    override fun getTypeArguments(): List<KtTypeProjection> = typeArgumentList?.arguments.orEmpty()
 }
