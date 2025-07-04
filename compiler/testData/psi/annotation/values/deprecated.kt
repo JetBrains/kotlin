76a225f0951021b70d6f0a0792f7416d51f9b82a
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-07-04 17:28:30 +0200

    [psi] more tests on qualifier calls inside annotations
    ^KT-77097

--- compiler/testData/psi/annotation/values/deprecated.kt
+++ compiler/testData/psi/annotation/values/deprecated.kt
@@ -1,2 +1,7 @@
+// FILE: Simple.kt
 @Deprecated("Deprecated", ReplaceWith("NewClass", "foo.bar.baz.NewClass"), DeprecationLevel.HIDDEN)
-class Another
+class Simple
+
+// FILE: Qualified.kt
+@kotlin.Deprecated("Deprecated", kotlin.ReplaceWith("NewClass", "foo.bar.baz.NewClass"), level = kotlin.DeprecationLevel.HIDDEN)
+class Qualified
