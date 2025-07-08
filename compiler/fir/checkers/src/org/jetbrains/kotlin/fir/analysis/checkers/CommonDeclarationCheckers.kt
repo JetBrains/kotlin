c69258bc7636a288cebbd667a50316626fd4ccfb
Brian Norman <brian.norman@jetbrains.com>
2025-07-08 10:22:59 -0500

    [FIR] Move FirUnusedExpressionChecker to the default checkers list
    ^KT-77726 Fixed

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/CommonDeclarationCheckers.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.fir.analysis.cfa.FirCallsEffectAnalyzer
 import org.jetbrains.kotlin.fir.analysis.cfa.FirPropertyInitializationAnalyzer
 import org.jetbrains.kotlin.fir.analysis.checkers.cfa.FirControlFlowChecker
 import org.jetbrains.kotlin.fir.analysis.checkers.declaration.*
+import org.jetbrains.kotlin.fir.analysis.checkers.extra.FirUnusedExpressionChecker
 import org.jetbrains.kotlin.fir.analysis.checkers.syntax.*
 
 object CommonDeclarationCheckers : DeclarationCheckers() {
@@ -34,6 +35,7 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
         FirOptionalExpectationDeclarationChecker,
         FirMissingDependencySupertypeInDeclarationsChecker,
         FirContextParametersDeclarationChecker,
+        FirUnusedExpressionChecker,
         FirUnusedReturnValueChecker,
         FirReturnValueAnnotationsChecker,
     )
