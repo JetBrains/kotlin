8ddee553ac57daa0006d08e956a1a07ca4f5a478
Florian Freitag <florian.freitag@jetbrains.com>
2025-07-09 15:02:25 +0200

    [IR] Add evaluation of unsigned integers to IrInterpreter
    KT-51065

--- compiler/testData/codegen/bytecodeText/unsignedTypes/unsignedIntToString_jvm18.kt
+++ compiler/testData/codegen/bytecodeText/unsignedTypes/unsignedIntToString_jvm18.kt
@@ -15,4 +15,4 @@ fun box(): String {
 }
 
 // 0 kotlin/UInt.toString
-// 3 INVOKESTATIC java/lang/Integer.toUnsignedString \(I\)Ljava/lang/String;
+// 0 INVOKESTATIC java/lang/Integer.toUnsignedString \(I\)Ljava/lang/String;
