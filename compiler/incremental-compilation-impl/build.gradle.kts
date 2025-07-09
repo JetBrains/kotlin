a6c7915e6a030c664f207e0628d0b5301b74c165
Evgenii Mazhukin <evgenii.mazhukin@jetbrains.com>
2025-07-09 16:28:10 +0200

    Revert "fixup! wip for investigation - not really mergeable"
    This reverts commit b7d844c001aea88c9b5e346cb1ac59dd285ef7c4.

--- compiler/incremental-compilation-impl/build.gradle.kts
+++ compiler/incremental-compilation-impl/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     api(project(":daemon-common"))
     api(project(":compiler:build-tools:kotlin-build-statistics"))
     api(project(":compiler:build-tools:kotlin-build-tools-api"))
+
+    api(protobufFull())
+
     compileOnly(intellijCore())
 
     testImplementation(libs.junit4)
