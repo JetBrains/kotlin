9df102ecb34aed1a93ffb8fd762b0850ef8561c4
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-07-09 17:44:00 +0200

    KLIB compat tests, JS: Add a test runner for custom first phase
    ^KT-78188

--- js/js.tests/klib-compatibility/build.gradle.kts
+++ js/js.tests/klib-compatibility/build.gradle.kts
@@ -2,7 +2,19 @@ import org.jetbrains.kotlin.gradle.targets.js.KotlinJsCompilerAttribute
 
 plugins {
     kotlin("jvm")
+    alias(libs.plugins.gradle.node)
     id("d8-configuration")
+}
+
+val cacheRedirectorEnabled = findProperty("cacheRedirectorEnabled")?.toString()?.toBoolean() == true
+
+node {
+    download.set(true)
+    version.set(nodejsVersion)
+    nodeProjectDir.set(layout.buildDirectory.dir("node"))
+    if (cacheRedirectorEnabled) {
+        distBaseUrl.set("https://cache-redirector.jetbrains.com/nodejs.org/dist")
+    }
 }
 
 dependencies {
@@ -51,16 +63,26 @@ fun Test.setUpJsBoxTests() {
         setupV8()
     }
     dependsOn(":dist")
-
+    systemProperty("kotlin.js.test.root.out.dir", "${node.nodeProjectDir.get().asFile}/")
     workingDir = rootDir
 }
 
-projectTest(jUnitMode = JUnitMode.JUnit5) {
+fun Test.setUpCustomCompiler() {
     dependsOn(downloadCustomCompilerArtifacts)
     systemProperty("kotlin.internal.js.test.compat.customCompilerArtifactsDir", customCompilerArtifactsDir.get().asFile.absolutePath)
     systemProperty("kotlin.internal.js.test.compat.customCompilerVersion", customCompilerVersion)
-
-    setUpJsBoxTests()
-    useJUnitPlatform()
 }
 
+projectTest(
+    taskName = "testCustomFirstPhase",
+    jUnitMode = JUnitMode.JUnit5,
+) {
+    setUpJsBoxTests()
+    setUpCustomCompiler()
+    useJUnitPlatform { includeTags("custom-first-phase") }
+}
+
+@Suppress("unused")
+val generateTests by generator("org.jetbrains.kotlin.generators.tests.GenerateJsKlibCompatibilityTestsKt") {
+    dependsOn(":compiler:generateTestData")
+}
