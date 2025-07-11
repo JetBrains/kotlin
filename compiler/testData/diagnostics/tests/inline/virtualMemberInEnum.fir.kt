447b8d2cb795d477d94ff3b44f15b2997edb6567
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-07-11 14:14:58 +0200

    [FIR] FirStatusResolver: do not inherit inline modifier
    ^KT-79099 Fixed

--- compiler/testData/diagnostics/tests/inline/virtualMemberInEnum.fir.kt
+++ compiler/testData/diagnostics/tests/inline/virtualMemberInEnum.fir.kt
@@ -1,7 +1,7 @@
 // RUN_PIPELINE_TILL: FRONTEND
 enum class Some {
     A {
-        override <!OVERRIDE_BY_INLINE!>fun foo(s: () -> String): String<!> {
+        override fun foo(s: () -> String): String {
             return s() + s()
         }
     };
