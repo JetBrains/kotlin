ebe1be8db89c4bdb1b18d6b5b483501067eb7a0d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-07-14 10:58:59 +0300

    [Build] Migrate :analysis:analysis-api-fe10 to test fixtures

--- analysis/analysis-api-fe10/build.gradle.kts
+++ analysis/analysis-api-fe10/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 plugins {
     kotlin("jvm")
     id("jps-compatible")
+    id("java-test-fixtures")
 }
 
 dependencies {
@@ -20,26 +21,24 @@ dependencies {
     implementation(project(":compiler:backend.jvm"))
     implementation(project(":compiler:backend.jvm.entrypoint"))
 
-    testApi(platform(libs.junit.bom))
-    testImplementation(libs.junit.jupiter.api)
+    testFixturesApi(platform(libs.junit.bom))
+    testFixturesApi(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(project(":analysis:analysis-api-platform-interface"))
-    testImplementation(project(":analysis:analysis-api-standalone:analysis-api-standalone-base"))
-    testImplementation(testFixtures(project(":compiler:tests-common")))
-    testApi(testFixtures(project(":compiler:test-infrastructure-utils")))
-    testApi(testFixtures(project(":compiler:test-infrastructure")))
-    testImplementation(testFixtures(project(":compiler:tests-common-new")))
-    testImplementation(projectTests(":analysis:analysis-api-impl-base"))
-    testImplementation(projectTests(":analysis:analysis-test-framework"))
+    testFixturesImplementation(project(":analysis:analysis-api-platform-interface"))
+    testFixturesImplementation(project(":analysis:analysis-api-standalone:analysis-api-standalone-base"))
+    testFixturesImplementation(testFixtures(project(":compiler:tests-common")))
+    testFixturesApi(testFixtures(project(":compiler:test-infrastructure-utils")))
+    testFixturesApi(testFixtures(project(":compiler:test-infrastructure")))
+    testFixturesImplementation(testFixtures(project(":compiler:tests-common-new")))
+    testFixturesApi(projectTests(":analysis:analysis-api-impl-base"))
+    testFixturesApi(projectTests(":analysis:analysis-test-framework"))
 
 }
 
 sourceSets {
     "main" { projectDefault() }
-    "test" {
-        projectDefault()
-        generatedTestDir()
-    }
+    "test" { generatedTestDir() }
+    "testFixtures" { projectDefault() }
 }
 
 tasks.withType<KotlinJvmCompile>().configureEach {
