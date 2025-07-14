f95a62862704e57a4214513612b84ebd928234a6
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-07-14 10:58:52 +0300

    [Build] Migrate :compiler:fir:analysis-tests to test fixtures

--- plugins/plugin-sandbox/build.gradle.kts
+++ plugins/plugin-sandbox/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     testFixturesApi(testFixtures(project(":compiler:tests-common-new")))
     testFixturesApi(projectTests(":compiler:test-infrastructure"))
     testFixturesApi(projectTests(":compiler:test-infrastructure-utils"))
-    testFixturesApi(projectTests(":compiler:fir:analysis-tests"))
+    testFixturesApi(testFixtures(project(":compiler:fir:analysis-tests")))
     testFixturesApi(testFixtures(project(":js:js.tests")))
     testFixturesApi(project(":compiler:fir:checkers"))
     testFixturesApi(project(":compiler:fir:checkers:checkers.jvm"))
