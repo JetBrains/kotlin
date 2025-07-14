e128eaab3f8d0e87313501897d4ed85eb3c9ec6f
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-07-14 10:58:48 +0300

    [Build] Migrate :plugins:sam-with-receiver to test fixtures

--- generators/build.gradle.kts
+++ generators/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     testApi(testFixtures(project(":kotlin-noarg-compiler-plugin")))
     testApi(testFixtures(project(":kotlin-lombok-compiler-plugin")))
     testApi(testFixtures(project(":kotlin-power-assert-compiler-plugin")))
-    testApi(projectTests(":kotlin-sam-with-receiver-compiler-plugin"))
+    testApi(testFixtures(project(":kotlin-sam-with-receiver-compiler-plugin")))
     testApi(testFixtures(project(":kotlin-assignment-compiler-plugin")))
     testApi(testFixtures(project(":kotlinx-serialization-compiler-plugin")))
     testApi(testFixtures(project(":kotlin-atomicfu-compiler-plugin")))
