8389b95a9efb70c06ef236e7af1e60b8f3f63089
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-07-14 11:43:41 +0300

    [Test] Adjust tests for `PreciseSimplificationToFlexibleLowerConstraint`

--- compiler/testData/diagnostics/tests/platformTypes/methodCall/singleton.fir.kt
+++ compiler/testData/diagnostics/tests/platformTypes/methodCall/singleton.fir.kt
@@ -1,13 +1,12 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 // DIAGNOSTICS: -UNUSED_PARAMETER -UNUSED_VARIABLE
 // LANGUAGE: -JavaTypeParameterDefaultRepresentationWithDNN
-// LATEST_LV_DIFFERENCE
 
 interface Foo
 
 fun test() {
     var nullable: Foo? = null
-    val foo: Collection<Foo> = <!INITIALIZER_TYPE_MISMATCH!>java.util.Collections.singleton(nullable)<!>
+    val foo: Collection<Foo> = java.util.Collections.singleton(nullable)
     val foo1: Collection<Foo> = java.util.Collections.singleton(nullable!!)
 }
 
