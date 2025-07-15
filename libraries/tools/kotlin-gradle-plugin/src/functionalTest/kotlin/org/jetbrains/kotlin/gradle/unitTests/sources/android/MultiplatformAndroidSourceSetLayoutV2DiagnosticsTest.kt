c9cec0c4c1f15b95e859479efb3e1bad0a8dd86d
Timofey Solonin <timofey.solonin@jetbrains.com>
2025-07-15 18:30:38 +0200

    Create directories in AGP source set checker tests before applying KGP
    ^KT-78993

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/sources/android/MultiplatformAndroidSourceSetLayoutV2DiagnosticsTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/sources/android/MultiplatformAndroidSourceSetLayoutV2DiagnosticsTest.kt
@@ -12,14 +12,9 @@ import org.gradle.api.Project
 import org.gradle.api.internal.project.ProjectInternal
 import org.jetbrains.kotlin.gradle.dsl.multiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.PropertiesProvider
-import org.jetbrains.kotlin.gradle.plugin.diagnostics.kotlinToolingDiagnosticsCollector
-import org.jetbrains.kotlin.gradle.plugin.sources.android.findAndroidSourceSet
-import org.jetbrains.kotlin.gradle.plugin.sources.android.multiplatformAndroidSourceSetLayoutV2
 import org.jetbrains.kotlin.gradle.util.*
-import org.jetbrains.kotlin.util.assertThrows
 import org.junit.Test
 import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class MultiplatformAndroidSourceSetLayoutV2DiagnosticsTest {
 
@@ -36,45 +31,29 @@ class MultiplatformAndroidSourceSetLayoutV2DiagnosticsTest {
         }
     }
 
-    private fun Project.checkCreatedSourceSets() {
-        /* Invoke checkers on all source sets */
-        project.multiplatformExtension.sourceSets.forEach { kotlinSourceSet ->
-            val androidSourceSet = project.findAndroidSourceSet(kotlinSourceSet) ?: return@forEach
-            multiplatformAndroidSourceSetLayoutV2.checker.checkCreatedSourceSet(
-                diagnosticsCollector = project.kotlinToolingDiagnosticsCollector,
-                target = project.multiplatformExtension.androidTarget(),
-                layout = multiplatformAndroidSourceSetLayoutV2,
-                kotlinSourceSet = kotlinSourceSet,
-                androidSourceSet = androidSourceSet
-            )
-        }
-    }
-
     @Test
     fun `test - KT-53709 - androidTest_kotlin in use`() {
-        val project = buildMinimalAndroidMultiplatformProject()
-
-        /* Ensure that the problematic androidTest/kotlin source dir is 'in use' */
-        val androidTestKotlinSourceDir = project.file("src/androidTest/kotlin")
-        androidTestKotlinSourceDir.mkdirs()
+        val project = buildMinimalAndroidMultiplatformProject {
+            /* Ensure that the problematic androidTest/kotlin source dir is 'in use' */
+            val androidTestKotlinSourceDir = project.file("src/androidTest/kotlin")
+            androidTestKotlinSourceDir.mkdirs()
+        }
         project.evaluate()
 
-        /* Invoke checkers on all source sets */
-        project.checkCreatedSourceSets()
         project.checkDiagnostics("kt53709AndroidTest_kotlinInUse")
     }
 
     @Test
     fun `test - android style source dir usage checker`() {
-        val project = buildMinimalAndroidMultiplatformProject()
-        val androidStyleMain = project.file("src/main/kotlin")
-        val androidStyleUnitTest = project.file("src/test/kotlin")
+        val project = buildMinimalAndroidMultiplatformProject {
+            val androidStyleMain = project.file("src/main/kotlin")
+            val androidStyleUnitTest = project.file("src/test/kotlin")
+            androidStyleMain.mkdirs()
+            androidStyleUnitTest.mkdirs()
+        }
 
-        androidStyleMain.mkdirs()
-        androidStyleUnitTest.mkdirs()
         project.evaluate()
 
-        project.checkCreatedSourceSets()
         project.checkDiagnostics("androidStyleSourceDirUsage")
     }
 
@@ -82,27 +61,26 @@ class MultiplatformAndroidSourceSetLayoutV2DiagnosticsTest {
     fun `test - nowarn flag - android style source dir usage checker`() {
         val project = buildMinimalAndroidMultiplatformProject {
             propertiesExtension.set(PropertiesProvider.PropertyNames.KOTLIN_MPP_ANDROID_SOURCE_SET_LAYOUT_ANDROID_STYLE_NO_WARN, "true")
-        }
-        val androidStyleMain = project.file("src/main/kotlin")
-        val androidStyleUnitTest = project.file("src/test/kotlin")
-        val androidStyleInstrumentedTest = project.file("src/androidTest/kotlin")
+            val androidStyleMain = project.file("src/main/kotlin")
+            val androidStyleUnitTest = project.file("src/test/kotlin")
+            val androidStyleInstrumentedTest = project.file("src/androidTest/kotlin")
 
-        androidStyleMain.mkdirs()
-        androidStyleUnitTest.mkdirs()
-        androidStyleInstrumentedTest.mkdirs()
+            androidStyleMain.mkdirs()
+            androidStyleUnitTest.mkdirs()
+            androidStyleInstrumentedTest.mkdirs()
+        }
         project.evaluate()
 
-        project.checkCreatedSourceSets()
         project.checkDiagnostics("androidStyleSourceDirUsageNoWarn")
     }
 
     @Test
     fun `test - v1 style source dir usage checker`() {
-        val project = buildMinimalAndroidMultiplatformProject()
-        val v1StyleInstrumentedTest = project.file("src/androidAndroidTest/kotlin")
-        v1StyleInstrumentedTest.mkdirs()
+        val project = buildMinimalAndroidMultiplatformProject {
+            val v1StyleInstrumentedTest = project.file("src/androidAndroidTest/kotlin")
+            v1StyleInstrumentedTest.mkdirs()
+        }
         project.evaluate()
-        project.checkCreatedSourceSets()
         project.checkDiagnostics("v1LayoutStyleSourceDirUsage")
     }
 
