1ad43365d9a29e689ae3d9b500e9881c40912e4a
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2025-07-16 14:26:49 +0200

    [ABI Validation] Replace using of compiler-embeddable with klib-abi-reader
    Fixes #KT-79283

--- libraries/tools/abi-validation/abi-tools/build.gradle.kts
+++ libraries/tools/abi-validation/abi-tools/build.gradle.kts
@@ -33,11 +33,14 @@ dependencies {
     api(project(":libraries:tools:abi-validation:abi-tools-api"))
 
     implementation(project(":kotlin-metadata-jvm"))
-    implementation(project(":kotlin-compiler-embeddable"))
+    implementation(project(":kotlin-klib-abi-reader"))
 
+    implementation(libs.intellij.asm)
     implementation(libs.diff.utils)
 
     testImplementation(kotlinTest("junit"))
     testImplementation(libs.junit4)
+    // using `KonanTarget` class
+    testImplementation(project(":native:kotlin-native-utils"))
 }
 
