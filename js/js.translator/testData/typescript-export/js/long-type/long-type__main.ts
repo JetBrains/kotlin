881415402e52618252f4b918388409d5909ee034
Artem Kobzar <artem.kobzar@jetbrains.com>
2025-07-17 09:24:51 +0000

    [K/JS] Allow export Long if -Xes-long-as-bigint is used ^KT-79222 Fixed
    Merge-request: KT-MR-22567
    Merged-by: Artem Kobzar <Artem.Kobzar@jetbrains.com>

---
+++ js/js.translator/testData/typescript-export/js/long-type/long-type__main.ts
@@ -0,0 +1,17 @@
+import foo = JS_TESTS.foo;
+
+function assert(condition: boolean) {
+    if (!condition) {
+        throw "Assertion failed";
+    }
+}
+
+assert(foo._long === 1n);
+// TODO: Add BigInt64Array
+// assert(foo._long_array instanceof BigInt64Array);
+assert(foo._array_long instanceof Array);
+assert(foo._n_long === 1n);
+
+function box(): string {
+    return "OK";
+}
