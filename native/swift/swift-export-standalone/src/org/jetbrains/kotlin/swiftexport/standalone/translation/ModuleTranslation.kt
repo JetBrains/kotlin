fbab4a28a871746c5a698944055459b9c5dbcf03
Gleb Lukianets <gleb.lukianets@jetbrains.com>
2025-07-18 15:10:03 +0200

    [Swift Export] KT-79227: Fix trampoline declarations conflicting at module scope

--- native/swift/swift-export-standalone/src/org/jetbrains/kotlin/swiftexport/standalone/translation/ModuleTranslation.kt
+++ native/swift/swift-export-standalone/src/org/jetbrains/kotlin/swiftexport/standalone/translation/ModuleTranslation.kt
@@ -10,6 +10,7 @@ import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
 import org.jetbrains.kotlin.analysis.api.symbols.KaClassLikeSymbol
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.sir.*
+import org.jetbrains.kotlin.sir.builder.buildModule
 import org.jetbrains.kotlin.sir.providers.SirAndKaSession
 import org.jetbrains.kotlin.sir.providers.SirSession
 import org.jetbrains.kotlin.sir.providers.impl.SirKaClassReferenceHandler
@@ -18,8 +19,10 @@ import org.jetbrains.kotlin.sir.providers.utils.KotlinRuntimeModule
 import org.jetbrains.kotlin.sir.providers.utils.updateImport
 import org.jetbrains.kotlin.sir.providers.withSessions
 import org.jetbrains.kotlin.sir.util.allParameters
+import org.jetbrains.kotlin.sir.util.conflictsWith
 import org.jetbrains.kotlin.sir.util.returnType
 import org.jetbrains.kotlin.swiftexport.standalone.InputModule
+import org.jetbrains.kotlin.swiftexport.standalone.SwiftExportLogger
 import org.jetbrains.kotlin.swiftexport.standalone.SwiftExportModule
 import org.jetbrains.kotlin.swiftexport.standalone.builders.KaModules
 import org.jetbrains.kotlin.swiftexport.standalone.builders.buildSirSession
@@ -152,6 +155,10 @@ private fun SirSession.createTranslationResult(
     // It might not be the case, but precise tracking seems like an overkill at the moment.
     sirModule.updateImport(SirImport(config.runtimeSupportModuleName))
     sirModule.updateImport(SirImport(config.runtimeModuleName))
+
+    // Conflicts may have arisen from the package flattening process
+    sirModule.removeConflicts(config.logger)
+
     val bridgeModuleName = "${moduleConfig.bridgeModuleName}_${sirModule.name}"
 
     val printer = SirPrinter(
@@ -238,3 +245,34 @@ private fun SirAndKaSession.deepTouch(
         (it - this).forEach { it.kaSymbolOrNull<KaClassLikeSymbol>()?.let(symbolHandler) }
     }
 }
+
+private fun SirMutableDeclarationContainer.removeConflicts(logger: SwiftExportLogger?) {
+    val trashBin = buildModule { name = "removedDueToConflicts" }
+    val iterator = this.declarations.listIterator()
+    while (iterator.hasNext()) {
+        val decl = iterator.next()
+        declarations
+            .indexOfFirst { decl.conflictsWith(it) } // We assume that `conflictsWith` is transitive
+            .takeIf { it in 0..<iterator.previousIndex() }
+            ?.let { i ->
+                val expelled = decl.takeIf { decl.priority < declarations[i].priority } ?: declarations[i].also { declarations[i] = decl }
+                iterator.remove()
+                expelled.parent = trashBin
+                logger?.report(
+                    SwiftExportLogger.Severity.Warning,
+                    "Exported declaration $expelled was removed from export due to conflicts")
+            }
+    }
+}
+
+private val SirDeclaration.priority: Int get() = when (this) {
+        is SirVariable -> 10
+        is SirFunction -> 20
+        is SirNamedDeclaration -> 30
+        else -> 0
+    }.let {
+        if (this.origin is SirOrigin.Trampoline)
+            it - 50
+        else
+            it
+    }
