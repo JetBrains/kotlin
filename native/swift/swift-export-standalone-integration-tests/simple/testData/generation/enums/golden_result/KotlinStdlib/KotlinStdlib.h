2621c9918064a5c505607db90ae0c258c9eb99e1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-07-18 10:59:16 +0200

    Swift export tests: extend enums test case
    Related to KT-79163, KT-79167

--- native/swift/swift-export-standalone-integration-tests/simple/testData/generation/enums/golden_result/KotlinStdlib/KotlinStdlib.h
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/enums/golden_result/KotlinStdlib/KotlinStdlib.h
@@ -3,6 +3,14 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+void * _Nullable kotlin_Array_get__TypesOfArguments__Swift_Int32__(void * self, int32_t index);
+
+void * kotlin_Array_iterator(void * self);
+
+void kotlin_Array_set__TypesOfArguments__Swift_Int32_Swift_Optional_KotlinRuntime_KotlinBase___(void * self, int32_t index, void * _Nullable value);
+
+int32_t kotlin_Array_size_get(void * self);
+
 void * kotlin_Enum_Companion_get();
 
 int32_t kotlin_Enum_compareTo__TypesOfArguments__ExportedKotlinPackages_kotlin_Enum__(void * self, void * other);
@@ -16,5 +24,55 @@ NSString * kotlin_Enum_name_get(void * self);
 int32_t kotlin_Enum_ordinal_get(void * self);
 
 NSString * kotlin_Enum_toString(void * self);
+
+_Bool kotlin_collections_Collection_contains__TypesOfArguments__Swift_Optional_KotlinRuntime_KotlinBase___(void * self, void * _Nullable element);
+
+_Bool kotlin_collections_Collection_containsAll__TypesOfArguments__anyU20ExportedKotlinPackages_kotlin_collections_Collection__(void * self, void * elements);
+
+_Bool kotlin_collections_Collection_isEmpty(void * self);
+
+void * kotlin_collections_Collection_iterator(void * self);
+
+int32_t kotlin_collections_Collection_size_get(void * self);
+
+void * kotlin_collections_Iterable_iterator(void * self);
+
+_Bool kotlin_collections_Iterator_hasNext(void * self);
+
+void * _Nullable kotlin_collections_Iterator_next(void * self);
+
+_Bool kotlin_collections_ListIterator_hasNext(void * self);
+
+_Bool kotlin_collections_ListIterator_hasPrevious(void * self);
+
+void * _Nullable kotlin_collections_ListIterator_next(void * self);
+
+int32_t kotlin_collections_ListIterator_nextIndex(void * self);
+
+void * _Nullable kotlin_collections_ListIterator_previous(void * self);
+
+int32_t kotlin_collections_ListIterator_previousIndex(void * self);
+
+_Bool kotlin_collections_List_contains__TypesOfArguments__Swift_Optional_KotlinRuntime_KotlinBase___(void * self, void * _Nullable element);
+
+_Bool kotlin_collections_List_containsAll__TypesOfArguments__anyU20ExportedKotlinPackages_kotlin_collections_Collection__(void * self, void * elements);
+
+void * _Nullable kotlin_collections_List_get__TypesOfArguments__Swift_Int32__(void * self, int32_t index);
+
+int32_t kotlin_collections_List_indexOf__TypesOfArguments__Swift_Optional_KotlinRuntime_KotlinBase___(void * self, void * _Nullable element);
+
+_Bool kotlin_collections_List_isEmpty(void * self);
+
+void * kotlin_collections_List_iterator(void * self);
+
+int32_t kotlin_collections_List_lastIndexOf__TypesOfArguments__Swift_Optional_KotlinRuntime_KotlinBase___(void * self, void * _Nullable element);
+
+void * kotlin_collections_List_listIterator(void * self);
+
+void * kotlin_collections_List_listIterator__TypesOfArguments__Swift_Int32__(void * self, int32_t index);
+
+int32_t kotlin_collections_List_size_get(void * self);
+
+NSArray<id> * kotlin_collections_List_subList__TypesOfArguments__Swift_Int32_Swift_Int32__(void * self, int32_t fromIndex, int32_t toIndex);
 
 NS_ASSUME_NONNULL_END
