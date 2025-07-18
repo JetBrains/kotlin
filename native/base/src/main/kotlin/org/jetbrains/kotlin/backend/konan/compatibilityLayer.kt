6a151cbd2e57d26e62a75d33d241bd758734eba7
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-07-18 14:33:08 +0300

    fixup! KT-78725: Binaries options refactoring

---
+++ native/base/src/main/kotlin/org/jetbrains/kotlin/backend/konan/compatibilityLayer.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.konan
+
+import org.jetbrains.kotlin.config.nativeBinaryOptions.UnitSuspendFunctionObjCExport
+
+// Used in intellij monorepo, kept for compatibility
+typealias UnitSuspendFunctionObjCExport = UnitSuspendFunctionObjCExport
