b02292481b70a5def8d71e70de9dd18fc6cdf8bd
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-07-22 16:36:33 +0200

    Swift export: make SirEnumCase a kind of SirNamedDeclaration
    KT-79999 in progress
    Related to KT-79167

--- native/swift/sir/src/org/jetbrains/kotlin/sir/util/Debug.kt
+++ native/swift/sir/src/org/jetbrains/kotlin/sir/util/Debug.kt
@@ -20,6 +20,7 @@ val SirElement.debugString: String
             is SirClass -> "class ${swiftFqName}${listOfNotNull(superClass?.render, renderProtocols.takeIf { it.isNotEmpty() }).joinToString().prefixIfNotEmpty(": ")}"
             is SirProtocol -> "protocol ${swiftFqName}${renderProtocols.prefixIfNotEmpty(": ")}"
             is SirEnum -> "enum ${swiftFqName}" // TODO: Render protocols
+            is SirEnumCase -> "case ${swiftFqName}"
             is SirStruct -> "struct ${swiftFqName}" // TODO: Render protocols
             is SirTypealias -> "typealias ${swiftFqName} = ${type.render}"
             is SirModule -> "module ${swiftFqNameOrNull ?: "?"}"
