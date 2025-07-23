4520ddd9d148005412889308a0e63539e9430f0b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-07-23 13:58:45 +0200

    fixup! Swift export tests: introduce SwiftExportWithResultValidationTest

--- native/swift/swift-export-standalone-integration-tests/src/org/jetbrains/kotlin/swiftexport/standalone/test/AbstractSwiftExportWithResultValidationTest.kt
+++ native/swift/swift-export-standalone-integration-tests/src/org/jetbrains/kotlin/swiftexport/standalone/test/AbstractSwiftExportWithResultValidationTest.kt
@@ -6,33 +6,25 @@
 package org.jetbrains.kotlin.swiftexport.standalone.test
 
 import com.intellij.testFramework.TestDataFile
+import org.jetbrains.kotlin.konan.test.blackbox.support.settings.KotlinNativeTargets
 import org.jetbrains.kotlin.konan.test.blackbox.support.util.getAbsoluteFile
 import org.jetbrains.kotlin.test.directives.model.SimpleDirectivesContainer
+import org.junit.jupiter.api.Assumptions
 import kotlin.io.path.div
 
 abstract class AbstractSwiftExportWithResultValidationTest : AbstractSwiftExportTest(), SwiftExportValidator {
     private object Directives : SimpleDirectivesContainer() {
         // TODO: better refactor to new test infrastructure
-        val IGNORE_SWIFT_EXPORT_WITH_ONLY_VALIDATION by stringDirective("Disables swift export with result validation test")
+        val APPLE_ONLY_VALIDATION by stringDirective("Ignore swift export with result validation test for non-apple platforms")
     }
 
     protected fun runTest(@TestDataFile testDir: String) {
         val testPathFull = getAbsoluteFile(testDir)
         val mainFile = (testPathFull.toPath() / "${testPathFull.name}.kt").toFile()
-        val isDisabled = "// ${Directives.IGNORE_SWIFT_EXPORT_WITH_ONLY_VALIDATION.name}" in mainFile.readLines()
-        try {
-            val (swiftExportOutputs) = runConvertToSwift(testDir)
-            val testPathFull = getAbsoluteFile(testDir)
+        val onlyApple = "// ${Directives.APPLE_ONLY_VALIDATION.name}" in mainFile.readLines()
+        Assumptions.assumeTrue(!onlyApple || testRunSettings.get<KotlinNativeTargets>().hostTarget.family.isAppleFamily)
 
-            validateSwiftExportOutput(testPathFull, swiftExportOutputs)
-            if (isDisabled) {
-                error(
-                    "Test contains ${Directives.IGNORE_SWIFT_EXPORT_WITH_ONLY_VALIDATION} directive but no errors was reported." +
-                            " Please remove directive"
-                )
-            }
-        } catch (t: Throwable) {
-            if (!isDisabled) throw t
-        }
+        val (swiftExportOutputs) = runConvertToSwift(testDir)
+        validateSwiftExportOutput(testPathFull, swiftExportOutputs)
     }
 }
