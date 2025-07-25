ea05117e7c8b0f0da2df81045cc847d772f2678c
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-07-25 16:36:58 +0200

    [stubs] KtFileStubBuilder: covert to Kotlin
    ^KT-79555

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtFileStubBuilder.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtFileStubBuilder.kt
@@ -2,36 +2,44 @@
  * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.psi.stubs.elements
 
-package org.jetbrains.kotlin.psi.stubs.elements;
+import com.intellij.psi.PsiFile
+import com.intellij.psi.stubs.DefaultStubBuilder
+import com.intellij.psi.stubs.StubElement
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.psiUtil.JvmFileClassUtil
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinFileStubImpl
 
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.stubs.DefaultStubBuilder;
-import com.intellij.psi.stubs.StubElement;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.psi.KtFile;
-import org.jetbrains.kotlin.psi.psiUtil.JvmFileClassInfo;
-import org.jetbrains.kotlin.psi.psiUtil.JvmFileClassUtil;
-import org.jetbrains.kotlin.psi.stubs.impl.KotlinFileStubImpl;
-
-public class KtFileStubBuilder extends DefaultStubBuilder {
-    @NotNull
-    @Override
-    protected StubElement createStubForFile(@NotNull PsiFile file) {
-        if (!(file instanceof KtFile)) {
-            return super.createStubForFile(file);
+class KtFileStubBuilder : DefaultStubBuilder() {
+    override fun createStubForFile(file: PsiFile): StubElement<*> {
+        if (file !is KtFile) {
+            return super.createStubForFile(file)
         }
 
-        KtFile ktFile = (KtFile) file;
-        String packageFqName = ktFile.getPackageFqName().asString();
-        boolean isScript = ktFile.isScript();
-        if (ktFile.hasTopLevelCallables()) {
-            JvmFileClassInfo fileClassInfo = JvmFileClassUtil.getFileClassInfoNoResolve(ktFile);
-            String facadeFqNameRef = fileClassInfo.getFacadeClassFqName().asString();
-            String partSimpleName = fileClassInfo.getFileClassFqName().shortName().asString();
-            return new KotlinFileStubImpl(ktFile, packageFqName, isScript, facadeFqNameRef, partSimpleName, null);
+        val packageFqName = file.packageFqName.asString()
+        val isScript = file.isScript()
+        if (file.hasTopLevelCallables()) {
+            val fileClassInfo = JvmFileClassUtil.getFileClassInfoNoResolve(file)
+            val facadeFqNameRef = fileClassInfo.facadeClassFqName.asString()
+            val partSimpleName = fileClassInfo.fileClassFqName.shortName().asString()
+            return KotlinFileStubImpl(
+                ktFile = file,
+                packageName = packageFqName,
+                isScript = isScript,
+                facadeFqNameString = facadeFqNameRef,
+                partSimpleName = partSimpleName,
+                facadePartSimpleNames = null,
+            )
         }
 
-        return new KotlinFileStubImpl(ktFile, packageFqName, isScript, null, null, null);
+        return KotlinFileStubImpl(
+            ktFile = file,
+            packageName = packageFqName,
+            isScript = isScript,
+            facadeFqNameString = null,
+            partSimpleName = null,
+            facadePartSimpleNames = null,
+        )
     }
 }
