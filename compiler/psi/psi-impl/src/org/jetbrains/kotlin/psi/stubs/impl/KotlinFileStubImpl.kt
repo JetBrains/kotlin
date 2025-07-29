60642a0c5d90232ea525c6f8bb5c289e5702005b
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-07-29 15:37:38 +0200

    [stubs] KotlinFileStubImpl: cleanup code
    ^KT-79555

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinFileStubImpl.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinFileStubImpl.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.impl
@@ -36,18 +25,12 @@ class KotlinFileStubImpl(
     val partSimpleName: String?,
     val facadePartSimpleNames: List<String>?,
 ) : PsiFileStubImpl<KtFile>(ktFile), KotlinFileStub {
-
-    constructor(ktFile: KtFile?, packageName: String, isScript: Boolean) : this(
-        ktFile,
-        packageName,
-        isScript,
-        facadeFqNameString = null,
-        partSimpleName = null,
-        facadePartSimpleNames = null
-    )
-
     private fun String.relativeToPackage() = getPackageFqName().child(Name.identifier(this))
 
+    /**
+     * Is used from [org.jetbrains.kotlin.psi.stubs.elements.StubIndexService] implementation on the plugin side
+     */
+    @Suppress("unused")
     val partFqName: FqName?
         get() = partSimpleName?.relativeToPackage()
 
