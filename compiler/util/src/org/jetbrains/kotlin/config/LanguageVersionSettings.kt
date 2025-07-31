5ca694e8c7929021a127a845f35a22f584d90ce7
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2025-07-31 14:34:45 +0200

    Contracts: poison binaries with AllowContractsOnPropertyAccessors
    because enabling the feature affects declarations in the dependent
    modules, e.g., as in #KT-79506

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -427,7 +427,7 @@ enum class LanguageFeature(
 
     AllowCheckForErasedTypesInContracts(KOTLIN_2_3, "KT-45683"),
     AllowContractsOnSomeOperators(KOTLIN_2_3, "KT-32313"),
-    AllowContractsOnPropertyAccessors(KOTLIN_2_3, "KT-27090"),
+    AllowContractsOnPropertyAccessors(KOTLIN_2_3, forcesPreReleaseBinaries = true, issue = "KT-27090"),
     ConditionImpliesReturnsContracts(KOTLIN_2_3, "KT-8889"),
     HoldsInContracts(KOTLIN_2_3, "KT-32993"),
 
