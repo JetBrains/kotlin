82155a77deae8a20f78b9fc806a90a1913af3948
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2025-08-04 15:57:20 +0200

    fixup! [Gradle] Add .xcodeproj configuration checker tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/apple/XcodeProjectConfigurationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/apple/XcodeProjectConfigurationIT.kt
@@ -8,17 +8,20 @@ package org.jetbrains.kotlin.gradle.apple
 import org.gradle.kotlin.dsl.kotlin
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.plugin.diagnostics.KotlinToolingDiagnostics
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XcodeTargetsConfigurationTask.Companion.TASK_NAME
 import org.jetbrains.kotlin.gradle.testbase.*
 import org.jetbrains.kotlin.gradle.uklibs.applyMultiplatform
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.condition.OS
 
 @OsCondition(supportedOn = [OS.MAC], enabledOnCI = [OS.MAC])
-@DisplayName("Tests for Xcode project configuration checker")
+@DisplayName("Tests for Xcode project configuration checker task")
 @NativeGradlePluginTests
 class XcodeProjectConfigurationIT : KGPBaseTest() {
 
-    @DisplayName("Checker fails when .xcodeproj has no application targets")
+    private val xcodeCheckTask = ":$TASK_NAME"
+
+    @DisplayName("Checker reports warning when .xcodeproj has no application targets")
     @GradleTest
     fun testXcodeprojHasNoTargets(
         gradleVersion: GradleVersion,
@@ -31,12 +34,10 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
             buildScriptInjection {
                 project.applyMultiplatform {
                     iosArm64()
-                    macosArm64()
-                    tvosArm64()
-                    watchosArm64()
                 }
             }
-            buildAndFail(":build") {
+            build(xcodeCheckTask) {
+                assertTasksExecuted(xcodeCheckTask)
                 assertHasDiagnostic(KotlinToolingDiagnostics.NoApplicationTargetFoundDiagnostic)
                 assertNoDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
             }
@@ -45,7 +46,7 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
 
     @DisplayName("Checker passes when all Kotlin targets are in .xcodeproj")
     @GradleTest
-    fun testTargetsMatchesXcodeprojTargets(
+    fun testTargetsMatchXcodeprojTargets(
         gradleVersion: GradleVersion,
     ) {
         project("empty", gradleVersion) {
@@ -61,14 +62,15 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
                     watchosArm64()
                 }
             }
-            build(":tasks") { // Using :tasks is faster than :build for a configuration-only check
+            build(xcodeCheckTask) {
+                assertTasksExecuted(xcodeCheckTask)
                 assertNoDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
                 assertNoDiagnostic(KotlinToolingDiagnostics.NoApplicationTargetFoundDiagnostic)
             }
         }
     }
 
-    @DisplayName("Checker fails when some Kotlin targets are missing from .xcodeproj")
+    @DisplayName("Checker reports warning when some Kotlin targets are missing from .xcodeproj")
     @GradleTest
     fun testPartialMismatch(
         gradleVersion: GradleVersion,
@@ -86,41 +88,19 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
                     watchosArm64()
                 }
             }
-            buildAndFail(":build") {
+            build(xcodeCheckTask) {
+                assertTasksExecuted(xcodeCheckTask)
                 assertHasDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
-                assertOutputContains("'tvosArm64' (expected SDK: 'appletvos')")
-                assertOutputContains("'watchosArm64' (expected SDK: 'watchos')")
-                assertOutputDoesNotContain("'iosArm64'")
-                assertOutputDoesNotContain("'macosArm64'")
+                assertNoDiagnostic(KotlinToolingDiagnostics.NoApplicationTargetFoundDiagnostic)
+                assertOutputContains("'tvos_arm64' (expected SDK: 'appletvos')")
+                assertOutputContains("'watchos_arm64' (expected SDK: 'watchos')")
+                assertOutputDoesNotContain("'ios_arm64'")
+                assertOutputDoesNotContain("'macos_arm64'")
             }
         }
     }
 
-    @DisplayName("Checker fails for simulator-only mismatch")
-    @GradleTest
-    fun testSimulatorVsDeviceMismatch(
-        gradleVersion: GradleVersion,
-    ) {
-        project("empty", gradleVersion) {
-            embedDirectoryFromTestData("xcodeProjectConfiguration/iosApp_ios", "iosApp")
-            plugins {
-                kotlin("multiplatform")
-            }
-            buildScriptInjection {
-                project.applyMultiplatform {
-                    iosArm64() // device
-                    iosSimulatorArm64() // simulator
-                }
-            }
-            buildAndFail(":build") {
-                assertHasDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
-                assertOutputContains("'iosSimulatorArm64' (expected SDK: 'iphonesimulator')")
-                assertOutputDoesNotContain("'iosArm64'")
-            }
-        }
-    }
-
-    @DisplayName("Checker is skipped when no Apple targets are defined")
+    @DisplayName("Task is not registered when no Apple targets are defined")
     @GradleTest
     fun testNoKotlinAppleTargets(
         gradleVersion: GradleVersion,
@@ -136,19 +116,19 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
                 }
             }
             build(":tasks") {
+                assertTasksAreNotInTaskGraph(xcodeCheckTask)
                 assertNoDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
                 assertNoDiagnostic(KotlinToolingDiagnostics.NoApplicationTargetFoundDiagnostic)
             }
         }
     }
 
-    @DisplayName("Checker is skipped when .xcodeproj directory is missing")
+    @DisplayName("Task is skipped when .xcodeproj directory is missing")
     @GradleTest
     fun testXcodeProjectNotFound(
         gradleVersion: GradleVersion,
     ) {
         project("empty", gradleVersion) {
-            // No embedDirectoryFromTestData call
             plugins {
                 kotlin("multiplatform")
             }
@@ -158,15 +138,16 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
                 }
             }
             build(":tasks") {
+                assertTasksAreNotInTaskGraph(xcodeCheckTask)
                 assertNoDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
                 assertNoDiagnostic(KotlinToolingDiagnostics.NoApplicationTargetFoundDiagnostic)
             }
         }
     }
 
-    @DisplayName("Checker handles malformed .pbxproj file gracefully")
+    @DisplayName("Task handles malformed xcodeproj gracefully")
     @GradleTest
-    fun testMalformedPbxprojFile(
+    fun testMalformedXcodeproj(
         gradleVersion: GradleVersion,
     ) {
         project("empty", gradleVersion) {
@@ -179,8 +160,8 @@ class XcodeProjectConfigurationIT : KGPBaseTest() {
                     iosArm64()
                 }
             }
-            build(":tasks") {
-                assertOutputContains("Failed to execute 'plutil' on")
+            build(xcodeCheckTask) {
+                assertTasksSkipped(xcodeCheckTask)
                 assertNoDiagnostic(KotlinToolingDiagnostics.MissingXcodeTargetDiagnostic)
                 assertNoDiagnostic(KotlinToolingDiagnostics.NoApplicationTargetFoundDiagnostic)
             }
