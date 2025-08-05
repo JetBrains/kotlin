db30d64c2432dbed7ae588700db945dcffa3b266
Leonid Startsev <leonid.startsev@jetbrains.com>
2025-08-05 15:54:23 +0200

    [FIR/ReturnValueChecker] Report functions that override ExplicitlyIgnorable status with MustUse
    Using metadata flag for this feature is a more correct approach than looking for explicit annotation.
    
    #KT-77504
    #KT-79920

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/diagnostics/FirErrorsDefaultMessages.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/diagnostics/FirErrorsDefaultMessages.kt
@@ -2475,7 +2475,7 @@ object FirErrorsDefaultMessages : BaseDiagnosticRendererFactory() {
         )
         map.put(
             OVERRIDING_IGNORABLE_WITH_MUST_USE,
-            "Method ''{0}'' is located in a scope marked as ''@MustUseReturnValue'', but effectively declared as ''@IgnorableReturnValue'' in the parent type ''{1}''. " +
+            "Method ''{0}'' is located in a scope marked as ''@MustUseReturnValue'', but marked as ''@IgnorableReturnValue'' in parent type ''{1}'' or its supertypes. " +
                     "Overriding ignorable methods with must-use methods is a semantically incorrect change. Consider marking ''{0}'' as ''@IgnorableReturnValue''.",
             DECLARATION_NAME,
             DECLARATION_FQ_NAME,
