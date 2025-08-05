a2bbdd3c82824ce8c669ebd5ef3efc5a8c98dae5
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-08-05 21:41:12 +0200

    Wasm, Tests: Add missing `additionalServices` to WasmBackendFacade
    Through `WasmBackendFacade.additionalServices` the wrapped facades
    can inform the test infrastructure about the required services
    to allow initializing and loading them on time.
    
    ^KT-78188

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/converters/WasmBackendFacade.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/converters/WasmBackendFacade.kt
@@ -5,21 +5,29 @@
 
 package org.jetbrains.kotlin.wasm.test.converters
 
+import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
 import org.jetbrains.kotlin.test.model.AbstractTestFacade
 import org.jetbrains.kotlin.test.model.ArtifactKinds
 import org.jetbrains.kotlin.test.model.BinaryArtifacts
 import org.jetbrains.kotlin.test.model.TestModule
+import org.jetbrains.kotlin.test.services.ServiceRegistrationData
 import org.jetbrains.kotlin.test.services.TestServices
 
 class WasmBackendFacade(
     private val testServices: TestServices
 ) : AbstractTestFacade<BinaryArtifacts.KLib, BinaryArtifacts.Wasm>() {
-    override val inputKind = ArtifactKinds.KLib
-    override val outputKind = ArtifactKinds.Wasm
+    override val inputKind: ArtifactKinds.KLib get() = ArtifactKinds.KLib
+    override val outputKind: ArtifactKinds.Wasm get() = ArtifactKinds.Wasm
 
     private val deserializerFacade = WasmDeserializerFacade(testServices)
 
     private val loweringFacade = WasmLoweringFacade(testServices)
+
+    override val additionalServices: List<ServiceRegistrationData>
+        get() = deserializerFacade.additionalServices + loweringFacade.additionalServices
+
+    override val directiveContainers: List<DirectivesContainer>
+        get() = deserializerFacade.directiveContainers + loweringFacade.directiveContainers
 
     override fun shouldTransform(module: TestModule): Boolean {
         return deserializerFacade.shouldTransform(module) && loweringFacade.shouldTransform(module)
