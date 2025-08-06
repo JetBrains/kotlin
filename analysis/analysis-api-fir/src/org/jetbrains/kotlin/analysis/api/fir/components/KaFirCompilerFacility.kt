JetBrains/kotlin

commit f2f3aea11f45dbf3f2c28d0617a26729c914daa7

KT-78890 Workaround hotfix for evaluation failures on the Kotlin project
This workaround will allow to evaluation if some IR compiler plugin
fails during code fragment compilation. The original problem comes
somewhere from the serialization compiler plugin.

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirCompilerFacility.kt
@@ -166,11 +166,29 @@ internal class KaFirCompilerFacility(
         target: KaCompilerTarget,
         allowedErrorFilter: (KaDiagnostic) -> Boolean
     ): KaCompilationResult = withPsiValidityAssertion(file) {
-        try {
-            return compileUnsafe(file, configuration, target as KaCompilerTarget.Jvm, allowedErrorFilter)
-        } catch (e: Throwable) {
-            rethrowIntellijPlatformExceptionIfNeeded(e)
-            throw KaCodeCompilationException(e)
+        compileWithRetry(file, configuration, target, allowedErrorFilter)
+    }
+
+    private fun compileWithRetry(
+        file: KtFile,
+        configuration: CompilerConfiguration,
+        target: KaCompilerTarget,
+        allowedErrorFilter: (KaDiagnostic) -> Boolean,
+    ): KaCompilationResult {
+        val disabledIrExtensions = mutableListOf<Class<out IrGenerationExtension>>()
+        while (true) {
+            try {
+                return compileUnsafe(file, configuration, target as KaCompilerTarget.Jvm, allowedErrorFilter, disabledIrExtensions)
+            } catch (e: IrGenerationExtensionException) {
+                val extensionClass = e.extensionClass
+                if (disabledIrExtensions.contains(extensionClass)) {
+                    throw KaCodeCompilationException(e) // very strange
+                }
+                disabledIrExtensions.add(extensionClass)
+            } catch (e: Throwable) {
+                rethrowIntellijPlatformExceptionIfNeeded(e)
+                throw KaCodeCompilationException(e)
+            }
         }
     }
 
@@ -178,7 +196,8 @@ internal class KaFirCompilerFacility(
         mainFile: KtFile,
         configuration: CompilerConfiguration,
         target: KaCompilerTarget.Jvm,
-        allowedErrorFilter: (KaDiagnostic) -> Boolean
+        allowedErrorFilter: (KaDiagnostic) -> Boolean,
+        disabledIrExtensions: List<Class<out IrGenerationExtension>>,
     ): KaCompilationResult {
         val syntaxErrors = SyntaxErrorReportingVisitor(analysisSession.firSession) { it.asKaDiagnostic() }
             .also(mainFile::accept).diagnostics
@@ -255,6 +274,7 @@ internal class KaFirCompilerFacility(
                 generateClassFilter,
                 KaFirDelegatingCompiledCodeProvider(registeredCodeProviders),
                 contextDeclarationCache,
+                disabledIrExtensions,
             )
 
             when (result) {
@@ -870,6 +890,7 @@ internal class KaFirCompilerFacility(
         generateClassFilter: GenerationState.GenerateClassFilter,
         compiledCodeProvider: CompiledCodeProvider,
         contextDeclarationCache: CodeFragmentContextDeclarationCache?,
+        disabledIrExtensions: List<Class<out IrGenerationExtension>>,
     ): KaCompilationResult {
         val session = resolutionFacade.sessionProvider.getResolvableSession(module)
         val configuration = baseConfiguration.copy().apply {
@@ -894,13 +915,15 @@ internal class KaFirCompilerFacility(
 
         val commonMemberStorage = contextDeclarationCache?.customCommonMemberStorage ?: Fir2IrCommonMemberStorage()
 
+        val irGeneratorExtensions = getIrGenerationExtensions(module).filter { !disabledIrExtensions.contains(it.javaClass) }
+
         val fir2IrResult = runFir2Ir(
             session = session,
             firFiles = chunk.files.map { it.firFile },
             fir2IrExtensions = fir2IrExtensions,
             diagnosticReporter = diagnosticReporter,
             effectiveConfiguration = configuration,
-            irGeneratorExtensions = getIrGenerationExtensions(module),
+            irGeneratorExtensions = irGeneratorExtensions,
             commonMemberStorage = commonMemberStorage
         )
 

compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/pipeline/convertToIr.kt
@@ -43,6 +43,8 @@ import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.ir.visitors.acceptVoid
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.utils.addToStdlib.runIf
+import org.jetbrains.kotlin.utils.exceptions.rethrowIntellijPlatformExceptionIfNeeded
+import kotlin.reflect.KClass
 
 data class FirResult(val outputs: List<ModuleCompilerAnalyzedOutput>)
 
@@ -488,6 +490,9 @@ private fun IrPluginContext.runMandatoryIrValidation(
     }
 }
 
+class IrGenerationExtensionException(cause: Throwable, val extensionClass: Class<out IrGenerationExtension>) :
+    RuntimeException(cause.message, cause)
+
 fun IrPluginContext.applyIrGenerationExtensions(
     fir2IrConfiguration: Fir2IrConfiguration,
     irModuleFragment: IrModuleFragment,
@@ -495,7 +500,12 @@ fun IrPluginContext.applyIrGenerationExtensions(
 ) {
     runMandatoryIrValidation(null, irModuleFragment, fir2IrConfiguration)
     for (extension in irGenerationExtensions) {
-        extension.generate(irModuleFragment, this)
-        runMandatoryIrValidation(extension, irModuleFragment, fir2IrConfiguration)
+        try {
+            extension.generate(irModuleFragment, this)
+            runMandatoryIrValidation(extension, irModuleFragment, fir2IrConfiguration)
+        } catch (e: Throwable) {
+            rethrowIntellijPlatformExceptionIfNeeded(e)
+            throw IrGenerationExtensionException(e, extension::class.java)
+        }
     }
 }

commit c7ef10524895501c08105c4124ea763aff337f85

[AA] Collect kotlin evaluator muted exceptions to report later
Relates to KT-78890 workaround. Muting exceptions were introduced in
f2f3aea11f45dbf3f2c28d0617a26729c914daa7

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirCompilerFacility.kt
@@ -176,14 +176,31 @@ internal class KaFirCompilerFacility(
         allowedErrorFilter: (KaDiagnostic) -> Boolean,
     ): KaCompilationResult {
         val disabledIrExtensions = mutableListOf<Class<out IrGenerationExtension>>()
+        val mutedExceptions = mutableListOf<Throwable>()
         while (true) {
             try {
-                return compileUnsafe(file, configuration, target as KaCompilerTarget.Jvm, allowedErrorFilter, disabledIrExtensions)
+                val result = compileUnsafe(file, configuration, target as KaCompilerTarget.Jvm, allowedErrorFilter, disabledIrExtensions)
+                if (mutedExceptions.isNotEmpty()) {
+                    return when (result) {
+                        is KaCompilationResult.Failure -> KaCompilationResult.Failure(
+                            errors = result.errors,
+                            mutedExceptions = mutedExceptions,
+                        )
+                        is KaCompilationResult.Success -> KaCompilationResult.Success(
+                            output = result.output,
+                            capturedValues = result.capturedValues,
+                            canBeCached = result.canBeCached,
+                            mutedExceptions = mutedExceptions,
+                        )
+                    }
+                }
+                return result
             } catch (e: IrGenerationExtensionException) {
                 val extensionClass = e.extensionClass
                 if (disabledIrExtensions.contains(extensionClass)) {
                     throw KaCodeCompilationException(e) // very strange
                 }
+                mutedExceptions.add(e)
                 disabledIrExtensions.add(extensionClass)
             } catch (e: Throwable) {
                 rethrowIntellijPlatformExceptionIfNeeded(e)

analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/components/KaCompilerFacility.kt
@@ -27,7 +27,10 @@ import java.io.File
  * [analysis block][org.jetbrains.kotlin.analysis.api.analyze].
  */
 @KaExperimentalApi
-public sealed class KaCompilationResult {
+public sealed class KaCompilationResult(
+    /** A list of exceptions that were thrown during compilation but workaround somehow */
+    public val mutedExceptions: List<Throwable>,
+) {
     /**
      * A successful compilation result.
      *
@@ -53,7 +56,8 @@ public sealed class KaCompilationResult {
         public val output: List<KaCompiledFile>,
         public val capturedValues: List<CodeFragmentCapturedValue>,
         public var canBeCached: Boolean,
-    ) : KaCompilationResult()
+        mutedExceptions: List<Throwable> = emptyList(),
+    ) : KaCompilationResult(mutedExceptions)
 
     /**
      * A failed compilation result.
@@ -61,7 +65,10 @@ public sealed class KaCompilationResult {
      * @property errors Non-recoverable errors which occurred either during code analysis or during code generation.
      */
     @KaExperimentalApi
-    public class Failure(public val errors: List<KaDiagnostic>) : KaCompilationResult()
+    public class Failure(
+        public val errors: List<KaDiagnostic>,
+        mutedExceptions: List<Throwable> = emptyList(),
+    ) : KaCompilationResult(mutedExceptions)
 }
 
 @KaExperimentalApi