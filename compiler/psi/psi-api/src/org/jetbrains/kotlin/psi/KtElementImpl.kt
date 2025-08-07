bac77c55ba0f8285da16d93cdebeda6a62eeff1b
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-07 14:54:51 +0200

    [psi] convert KtElementImpl to Kotlin
    ^KT-79798

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtElementImpl.kt
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtElementImpl.kt
@@ -1,111 +1,63 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.psi
 
-package org.jetbrains.kotlin.psi;
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
+import com.intellij.lang.Language
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiReference
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KotlinReferenceProvidersService.Companion.getReferencesFromProviders
+import org.jetbrains.kotlin.psi.psiUtil.parentSubstitute
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
-import com.intellij.lang.ASTNode;
-import com.intellij.lang.Language;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReference;
-import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.idea.KotlinLanguage;
-import org.jetbrains.kotlin.psi.psiUtil.KtPsiUtilKt;
+open class KtElementImpl(node: ASTNode) : ASTWrapperPsiElement(node), KtElement {
+    override fun getLanguage(): Language = KotlinLanguage.INSTANCE
 
-public class KtElementImpl extends ASTWrapperPsiElement implements KtElement {
-    public KtElementImpl(@NotNull ASTNode node) {
-        super(node);
-    }
+    override fun toString(): String = node.elementType.toString()
 
-    @NotNull
-    @Override
-    public Language getLanguage() {
-        return KotlinLanguage.INSTANCE;
-    }
-
-    @Override
-    public String toString() {
-        return getNode().getElementType().toString();
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public final void accept(@NotNull PsiElementVisitor visitor) {
-        if (visitor instanceof KtVisitor) {
-            accept((KtVisitor) visitor, null);
-        }
-        else {
-            visitor.visitElement(this);
+    final override fun accept(visitor: PsiElementVisitor) {
+        if (visitor is KtVisitorVoid) {
+            accept(visitor, null)
+        } else {
+            visitor.visitElement(this)
         }
     }
 
-    @NotNull
-    @Override
-    public KtFile getContainingKtFile() {
-        PsiFile file = getContainingFile();
-        if(!(file instanceof KtFile))  {
-            String fileString = (file != null && file.isValid()) ? (" " + file.getText()) : "";
-            throw new IllegalStateException("KtElement not inside KtFile: " + file + fileString +
-                                            " for element " + this + " of type " + this.getClass() + " node = " + getNode());
+    override fun getContainingKtFile(): KtFile {
+        val file = containingFile
+        if (file !is KtFile) {
+            val fileString = if (file != null && file.isValid) (" " + file.text) else ""
+            throw IllegalStateException(
+                "KtElement not inside KtFile: " + file + fileString +
+                        " for element " + this + " of type " + this.javaClass + " node = " + node
+            )
         }
-        return (KtFile) file;
+        return file
     }
 
-    @Override
-    public <D> void acceptChildren(@NotNull KtVisitor<Void, D> visitor, D data) {
-        KtPsiUtil.visitChildren(this, visitor, data);
+    override fun <D> acceptChildren(visitor: KtVisitor<Void, D>, data: D) {
+        KtPsiUtil.visitChildren(this, visitor, data)
     }
 
-    @Override
-    public <R, D> R accept(@NotNull KtVisitor<R, D> visitor, D data) {
-        return visitor.visitKtElement(this, data);
+    override fun <R, D> accept(visitor: KtVisitor<R, D>, data: D): R {
+        return visitor.visitKtElement(this, data)
     }
 
-    @Override
-    public void delete() throws IncorrectOperationException {
-        KtElementUtilsKt.deleteSemicolon(this);
-        super.delete();
+    override fun delete() {
+        this.deleteSemicolon()
+        super.delete()
     }
 
-    @Override
-    @SuppressWarnings("deprecation")
-    public PsiReference getReference() {
-        PsiReference[] references = getReferences();
-        if (references.length == 1) return references[0];
-        else return null;
+    override fun getReference(): PsiReference? = references.singleOrNull()
+
+    override fun getReferences(): Array<PsiReference> {
+        return getReferencesFromProviders(this)
     }
 
-    @NotNull
-    @Override
-    public PsiReference[] getReferences() {
-        return KotlinReferenceProvidersService.getReferencesFromProviders(this);
-    }
-
-    @NotNull
-    @Override
-    public KtElement getPsiOrParent() {
-        return this;
-    }
-
-    @Override
-    public PsiElement getParent() {
-        PsiElement substitute = KtPsiUtilKt.getParentSubstitute(this);
-        return substitute != null ? substitute : super.getParent();
-    }
+    override fun getPsiOrParent(): KtElement = this
+    override fun getParent(): PsiElement = parentSubstitute ?: super.getParent()
 }
