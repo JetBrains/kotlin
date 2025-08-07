fdd4f3b28a2848d36592ca562f46adc7973e2360
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-07 14:42:41 +0200

    [psi] small cleanups
    ^KT-79798

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtElement.kt
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtElement.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi
@@ -29,12 +18,9 @@ interface KtElement : NavigatablePsiElement, KtPureElement {
     override fun getReference(): PsiReference?
 }
 
-fun KtElement.getModificationStamp(): Long {
-    return when (this) {
-        is PsiFile -> this.modificationStamp
-        is KtDeclarationStub<*> -> this.modificationStamp
-        is KtSuperTypeList -> this.modificationStamp
-        is KtScriptInitializer -> this.getModificationStamp()
-        else -> (parent as KtElement).getModificationStamp()
-    }
+fun KtElement.getModificationStamp(): Long = when (this) {
+    is PsiFile -> this.modificationStamp
+    is KtDeclarationStub<*> -> this.modificationStamp
+    is KtSuperTypeList -> this.modificationStamp
+    else -> (parent as KtElement).getModificationStamp()
 }
