6778885a6122ebc3fcd366493968bb2f479be279
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-08-08 15:55:06 +0200

    [test] Add a reproducer for KT-79916
    (cherry picked from commit ddb99a7b571e5f3527499c905ec7c2058b9bc1d5)

--- wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxTestGenerated.java
+++ wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxTestGenerated.java
@@ -31712,6 +31712,12 @@ public class FirWasmJsCodegenBoxTestGenerated extends AbstractFirWasmJsCodegenBo
     }
 
     @Test
+    @TestMetadata("kt79977_withIncrementalCompilation.kt")
+    public void testKt79977_withIncrementalCompilation() {
+      runTest("compiler/testData/codegen/box/primitiveTypes/kt79977_withIncrementalCompilation.kt");
+    }
+
+    @Test
     @TestMetadata("kt828.kt")
     public void testKt828() {
       runTest("compiler/testData/codegen/box/primitiveTypes/kt828.kt");
