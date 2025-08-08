5cc4f6ed7addb179c6a33c635320423255cc25cc
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2025-08-08 22:58:06 +0200

    [Wasm] Run wasm tests in JavaScriptCore when it's available
    Automatic download for JSC should be done separately within KT-80028.
    Before that, JSC could be installed using `jsvu`[1].
    To provide the path to the JSC executable, use the system property `javascript.engine.path.JavaScriptCore.local`.
    
    [1]: https://github.com/GoogleChromeLabs/jsvu
    
    #KT-80018

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/handlers/WasmBoxRunner.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/handlers/WasmBoxRunner.kt
@@ -20,7 +20,7 @@ import java.io.File
 class WasmBoxRunner(
     testServices: TestServices
 ) : AbstractWasmArtifactsCollector(testServices) {
-    private val vmsToCheck: List<WasmVM> = listOf(WasmVM.V8, WasmVM.SpiderMonkey, WasmVM.JavaScriptCore)
+    private val vmsToCheck: List<WasmVM> = listOfNotNull(WasmVM.V8, WasmVM.SpiderMonkey, WasmVM.JavaScriptCoreOrNull)
 
     override fun processAfterAllModules(someAssertionWasFailed: Boolean) {
         if (!someAssertionWasFailed) {
