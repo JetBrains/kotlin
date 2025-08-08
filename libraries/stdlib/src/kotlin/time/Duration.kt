ced3eb6b65fd8a4661ab99d1a9cbc8d97d74e049
Dmitry Nekrasov <dmitry.nekrasov@jetbrains.com>
2025-08-08 17:50:59 +0400

    Add test case for large `Duration` values and optimize microsecond handling

--- libraries/stdlib/src/kotlin/time/Duration.kt
+++ libraries/stdlib/src/kotlin/time/Duration.kt
@@ -1151,7 +1151,6 @@ private inline fun parseDefaultStringFormat(
     }
 
     var totalMillis = 0L
-    var totalMicros = 0L
     var totalNanos = 0L
     var prevUnit: DurationUnit? = null
     var isFirstComponent = true
@@ -1191,11 +1190,11 @@ private inline fun parseDefaultStringFormat(
         when (unit) {
             DurationUnit.MICROSECONDS -> {
                 totalMillis += integralValue / MICROS_IN_MILLIS
-                totalMicros = integralValue % MICROS_IN_MILLIS
+                totalNanos += (integralValue % MICROS_IN_MILLIS) * NANOS_IN_MICROS
             }
             DurationUnit.NANOSECONDS -> {
-                totalMicros += integralValue / NANOS_IN_MICROS
-                totalNanos += integralValue % NANOS_IN_MICROS
+                totalMillis += integralValue / NANOS_IN_MILLIS
+                totalNanos += integralValue % NANOS_IN_MILLIS
             }
             else -> {
                 val multiplier = unit.millisMultiplier
@@ -1210,9 +1209,7 @@ private inline fun parseDefaultStringFormat(
         }
     }
 
-    return totalMillis.toDuration(DurationUnit.MILLISECONDS) +
-            totalMicros.toDuration(DurationUnit.MICROSECONDS) +
-            totalNanos.toDuration(DurationUnit.NANOSECONDS)
+    return totalMillis.toDuration(DurationUnit.MILLISECONDS) + totalNanos.toDuration(DurationUnit.NANOSECONDS)
 }
 
 @kotlin.internal.InlineOnly
