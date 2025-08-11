ecfe6a7c3101f57fe5e3a831aa63c9e1ffa55bf7
Andrei Shikov <ashikov@google.com>
2025-08-11 17:56:47 +0100

    Add JS tests for CMP-8741

--- plugins/compose/compiler-hosted/testData/js/testValueClassJs.kt
+++ plugins/compose/compiler-hosted/testData/js/testValueClassJs.kt
@@ -2,16 +2,22 @@
 
 // MODULE: lib
 // FILE: Test.kt
+import androidx.compose.runtime.Composable
+
 value class Test private constructor(val i: Int) {
     companion object {
         fun default() = Test(0)
     }
 }
 
+@Composable fun Content(test: Test = Test.default()) {}
+
 // MODULE: main(lib)
 // FILE: main.kt
 import androidx.compose.runtime.Composable
 
-@Composable fun Content(test: Test = Test.default()) {}
+@Composable fun Test() {
+    Content()
+}
 
 fun box() = "OK"
