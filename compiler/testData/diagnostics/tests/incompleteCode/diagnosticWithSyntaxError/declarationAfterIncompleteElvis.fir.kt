341807f43f416be8a62eb1b89d234d0c3a244509
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-08-12 15:19:23 +0200

    Fixup

--- compiler/testData/diagnostics/tests/incompleteCode/diagnosticWithSyntaxError/declarationAfterIncompleteElvis.fir.kt
+++ compiler/testData/diagnostics/tests/incompleteCode/diagnosticWithSyntaxError/declarationAfterIncompleteElvis.fir.kt
@@ -7,7 +7,7 @@ fun foo(x: Any?) {
     x ?:<!SYNTAX!><!>
     fun bar() = 2
 
-    val res: String.() -> Int = null ?:
+    val res: String.() -> Int = <!USELESS_ELVIS_LEFT_IS_NULL!>null ?:<!>
     fun String.() = 3
 }
 
@@ -18,7 +18,7 @@ class A {
     val y = null ?:<!SYNTAX!><!>
     fun baz() = 5
 
-    val q = null ?:
+    val q = <!USELESS_ELVIS_LEFT_IS_NULL!>null ?:<!>
     fun String.() = 6
 }
 
