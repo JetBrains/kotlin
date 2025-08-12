e39a4e8de7137477ec82fbf66747eb2d2fd5c19e
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-08-12 12:46:28 +0200

    JVM: consider extension receiver first in PropertyReferenceDelegationLowering
    The order was incorrectly switched in 232d5e372d.
    
     #KT-79979 Fixed

--- compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirLightTreeSerializeCompileKotlinAgainstInlineKotlinTestGenerated.java
+++ compiler/fir/fir2ir/tests-gen/org/jetbrains/kotlin/test/runners/codegen/FirLightTreeSerializeCompileKotlinAgainstInlineKotlinTestGenerated.java
@@ -18465,6 +18465,12 @@ public class FirLightTreeSerializeCompileKotlinAgainstInlineKotlinTestGenerated 
         }
 
         @Test
+        @TestMetadata("kt79979_memberExtension.kt")
+        public void testKt79979_memberExtension() {
+          runTest("compiler/testData/codegen/box/delegatedProperty/delegateToAnother/kt79979_memberExtension.kt");
+        }
+
+        @Test
         @TestMetadata("mutable.kt")
         public void testMutable() {
           runTest("compiler/testData/codegen/box/delegatedProperty/delegateToAnother/mutable.kt");
