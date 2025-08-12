4c406ef0dfe8c8a34a5e650d8b24cfa1269d1f92
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-08-12 14:44:59 +0200

    [Tests] Shorten test kt47450 once again to avoid SOE in IR validator
    Now that the IR validation is also executed from a different place,
    the initial stack is probably longer than before and causes
    StackOverflowException in validator.

--- compiler/testData/ir/irText/expressions/kt47450.kt
+++ compiler/testData/ir/irText/expressions/kt47450.kt
@@ -15,9 +15,7 @@ fun test(z: Int): String {
             z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
             "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
             z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
-            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
-            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z +
-            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" +
-            z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z
-// Note: Adding 2 more lines results in SOE in IrFileValidator on CI during FirLightTreeJvmIrTextTestGenerated
+            "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1" + z + "1"
+// Note: We have to watch out for too many expressions though. In the current implementation it may result in SOE in IrValidator,
+// (e.g. in FirLightTreeJvmIrTextTestGenerated), but what is tricky, it may fail on CI only, presumably because of different JDK version.
 }
