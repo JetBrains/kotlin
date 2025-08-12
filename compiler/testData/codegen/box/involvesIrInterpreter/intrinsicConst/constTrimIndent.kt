17ebd1e06d639872a87c1e44b87399f5789c7337
Florian Freitag <florian.freitag@jetbrains.com>
2025-08-12 18:56:47 +0200

    [FIR] Add trim functions to constant evaluation

--- compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/constTrimIndent.kt
+++ compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/constTrimIndent.kt
@@ -1,15 +1,14 @@
 // LANGUAGE: +IntrinsicConstEvaluation
-// IGNORE_BACKEND_K1: JVM_IR, JS_IR, JS_IR_ES6, NATIVE, WASM
 // WITH_STDLIB
 fun <T> T.id() = this
 
-const val trimIndent = "123".<!EVALUATED{IR}("123")!>trimIndent()<!>
+const val trimIndent = "123".<!EVALUATED("123")!>trimIndent()<!>
 const val complexTrimIndent =
     """
             ABC
             123
             456
-        """.<!EVALUATED{IR}("ABC\n123\n456")!>trimIndent()<!>
+        """.<!EVALUATED("ABC\n123\n456")!>trimIndent()<!>
 
 // STOP_EVALUATION_CHECKS
 fun box(): String {
