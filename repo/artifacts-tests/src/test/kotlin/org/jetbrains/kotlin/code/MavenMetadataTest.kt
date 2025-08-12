103a6a3a1c584f55b3a1325a7f0ee2dd3f29c320
Fedor Tolstonozhenko <fedor.tolstonozhenko@jetbrains.com>
2025-08-12 12:17:28 +0200

    Debug info 3

--- repo/artifacts-tests/src/test/kotlin/org/jetbrains/kotlin/code/MavenMetadataTest.kt
+++ repo/artifacts-tests/src/test/kotlin/org/jetbrains/kotlin/code/MavenMetadataTest.kt
@@ -5,8 +5,6 @@
 
 package org.jetbrains.kotlin.code
 
-import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertEqualsToFile
-import org.jetbrains.kotlin.test.services.JUnit5Assertions.isTeamCityBuild
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import java.util.stream.Stream
@@ -14,29 +12,29 @@ import kotlin.io.path.absolutePathString
 import kotlin.io.path.exists
 import kotlin.streams.asStream
 import kotlin.test.assertTrue
-import kotlin.test.fail
 
 class MavenMetadataTest {
     @TestFactory
     fun generateMavenMetadataTests(): Stream<DynamicTest> {
-        return findActualArtifacts(".pom", mustContainKotlinVersion = true).map { actual ->
+        findActualArtifacts(".pom", mustContainKotlinVersion = true).map { actual ->
             val expectedPomPath = actual.toExpectedPath()
             println("Expected ${expectedPomPath.absolutePathString()} exists – ${expectedPomPath.exists()}")
 
-            DynamicTest.dynamicTest(expectedPomPath.fileName.toString()) {
-                if ("${expectedPomPath.parent.fileName}" !in excludedProjects) {
-                    if ("${expectedPomPath.parent.fileName}" !in nativeBundles) {
-                        val regex = Regex(
-                            """(<groupId>org.jetbrains.kotlin\S*</groupId>\s*\S*\s*)<version>$kotlinVersion</version>"""
-                        )
-                        val actualString = actual.toFile().readText().replace(regex, "\$1<version>ArtifactsTest.version</version>")
-                        assertEqualsToFile(expectedPomPath, actualString)
-                    }
-                } else {
-                    if (isTeamCityBuild) fail("Excluded project in actual artifacts: $actual")
-                }
-            }
+//            DynamicTest.dynamicTest(expectedPomPath.fileName.toString()) {
+//                if ("${expectedPomPath.parent.fileName}" !in excludedProjects) {
+//                    if ("${expectedPomPath.parent.fileName}" !in nativeBundles) {
+//                        val regex = Regex(
+//                            """(<groupId>org.jetbrains.kotlin\S*</groupId>\s*\S*\s*)<version>$kotlinVersion</version>"""
+//                        )
+//                        val actualString = actual.toFile().readText().replace(regex, "\$1<version>ArtifactsTest.version</version>")
+//                        assertEqualsToFile(expectedPomPath, actualString)
+//                    }
+//                } else {
+//                    if (isTeamCityBuild) fail("Excluded project in actual artifacts: $actual")
+//                }
+//            }
         }.asStream()
+        return emptySequence<DynamicTest>().asStream()
     }
 
     @TestFactory
