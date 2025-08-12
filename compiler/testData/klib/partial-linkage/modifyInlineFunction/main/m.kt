b13f763e8ce330c59625c5e98c6bca266be1f714
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-08-12 16:32:45 +0200

    fixup! [PL][Tests] Add tests for ABI compatible changes of inline functions

--- compiler/testData/klib/partial-linkage/modifyInlineFunction/main/m.kt
+++ compiler/testData/klib/partial-linkage/modifyInlineFunction/main/m.kt
@@ -3,41 +3,41 @@ import abitestutils.abiTest
 fun box() = abiTest {
     expectSuccess("inlineFunction.v2") { inlineFunction() }
     expectSuccess("inlineFunctionWithParam.v2: default.v2") { inlineFunctionWithParam() }
-    expectSuccess("receiver.inlineExtensionFunction.v2 with context c") { with("c") { "receiver".inlineExtensionFunction() } }
+    expectSuccess("v2Receiver.inlineExtensionFunction.v2 with context v2Context") { with("v2Context") { "v2Receiver".inlineExtensionFunction() } }
     expectSuccess("inlineClassFunction.v2") { C().inlineClassFunction() }
     expectSuccess("inlineClassFunctionWithParam.v2: default.v2") { C().inlineClassFunctionWithParam() }
-    expectSuccess("receiver.inlineClassExtensionFunction.v2 with context c") { C().run { with("c") { "receiver".inlineClassExtensionFunction() } } }
+    expectSuccess("v2ClassReceiver.inlineClassExtensionFunction.v2 with context v2ClassContext") { C().run { with("v2ClassContext") { "v2ClassReceiver".inlineClassExtensionFunction() } } }
 
 
     expectSuccess("inlineFunction.v1") { directCall1() }
     expectSuccess("inlineFunctionWithParam.v1: default.v1") { directCall2() }
-    expectSuccess("inlineFunctionWithParam.v1: custom") { directCall3() }
-    expectSuccess("receiver.inlineExtensionFunction.v1 with context c") { directCall4() }
+    expectSuccess("inlineFunctionWithParam.v1: directCustomParam") { directCall3() }
+    expectSuccess("directReceiver.inlineExtensionFunction.v1 with context directContext") { directCall4() }
     expectSuccess("inlineClassFunction.v1") { directCall5() }
     expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { directCall6() }
-    expectSuccess("inlineClassFunctionWithParam.v1: custom") { directCall7() }
-    expectSuccess("receiver.inlineClassExtensionFunction.v1 with context c") { directCall8() }
+    expectSuccess("inlineClassFunctionWithParam.v1: directClassCustomParam") { directCall7() }
+    expectSuccess("directClassReceiver.inlineClassExtensionFunction.v1 with context directClassContext") { directCall8() }
 
-    expectSuccess("inlineFunction.v1") { inlineCall1() }
-    expectSuccess("inlineFunctionWithParam.v1: default.v1") { inlineCall2() }
-    expectSuccess("inlineFunctionWithParam.v1: custom") { inlineCall3() }
-    expectSuccess("receiver.inlineExtensionFunction.v1 with context c") { inlineCall4() }
-    expectSuccess("inlineClassFunction.v1") { inlineCall5() }
-    expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { inlineCall6() }
-    expectSuccess("inlineClassFunctionWithParam.v1: custom") { inlineCall7() }
-    expectSuccess("receiver.inlineClassExtensionFunction.v1 with context c") { inlineCall8() }
+    expectSuccess("inlineFunction.v1") { directCall9() }
+    expectSuccess("inlineFunctionWithParam.v1: default.v1") { directCall10() }
+    expectSuccess("inlineFunctionWithParam.v1: inlineCustomParam") { directCall11() }
+    expectSuccess("inlineReceiver.inlineExtensionFunction.v1 with context inlineContext") { directCall12() }
+    expectSuccess("inlineClassFunction.v1") { directCall13() }
+    expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { directCall14() }
+    expectSuccess("inlineClassFunctionWithParam.v1: inlineClassCustomParam") { directCall15() }
+    expectSuccess("inlineClassReceiver.inlineClassExtensionFunction.v1 with context inlineClassContext") { directCall16() }
 
-    expectSuccess("inlineFunction.v1") { lambdaCall1() }
-    expectSuccess("inlineFunctionWithParam.v1: default.v1") { lambdaCall2() }
-    expectSuccess("inlineFunctionWithParam.v1: custom") { lambdaCall3() }
-    expectSuccess("receiver.inlineExtensionFunction.v1 with context c") { lambdaCall4() }
-    expectSuccess("inlineClassFunction.v1") { lambdaCall5() }
-    expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { lambdaCall6() }
-    expectSuccess("inlineClassFunctionWithParam.v1: custom") { lambdaCall7() }
-    expectSuccess("receiver.inlineClassExtensionFunction.v1 with context c") { lambdaCall8() }
+    expectSuccess("inlineFunction.v1") { directCall17() }
+    expectSuccess("inlineFunctionWithParam.v1: default.v1") { directCall18() }
+    expectSuccess("inlineFunctionWithParam.v1: lambdaCustomParam") { directCall19() }
+    expectSuccess("lambdaReceiver.inlineExtensionFunction.v1 with context lambdaContext") { directCall20() }
+    expectSuccess("inlineClassFunction.v1") { directCall21() }
+    expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { directCall22() }
+    expectSuccess("inlineClassFunctionWithParam.v1: lambdaClassCustomParam") { directCall23() }
+    expectSuccess("lambdaClassReceiver.inlineClassExtensionFunction.v1 with context lambdaClassContext") { directCall24() }
 
-    expectSuccess("inlineFunction.v1") { defaultParamFunction1() }
-    expectSuccess("inlineFunctionWithParam.v1: default.v1") { defaultParamFunction2() }
-    expectSuccess("inlineClassFunction.v1") { defaultParamFunction3() }
-    expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { defaultParamFunction4() }
+    expectSuccess("inlineFunction.v1") { directCall25() }
+    expectSuccess("inlineFunctionWithParam.v1: default.v1") { directCall26() }
+    expectSuccess("inlineClassFunction.v1") { directCall27() }
+    expectSuccess("inlineClassFunctionWithParam.v1: default.v1") { directCall28() }
 }
