9a6f09c9e68d14962e13112a38c9071a50d6de61
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-08-13 15:05:14 +0200

    [LL] Package delegation symbol provider: Fix `hasPackage` for parent packages
    - The previous `providersByPackage` only contained "non-empty" packages
      with classes or callables, but not their "empty" parent packages. We
      cannot use such a map for deciding `hasPackage`, since the function
      should also allow parent packages.
    - The fix ensures that parent packages are also added to the map.
    - The implementation is optimized for memory without sacrificing
      performance:
      - `String` keys are much more light-weight than `FqName`, especially
        when parents remain cached inside `FqName`. Getting a string from an
        `FqName` during querying does not perform any work, as the
        `FqNameUnsafe` simply stores the string.
      - Even when `ArrayList`s are appropriately sized, they consume more
        memory than a plain array due to an additional object layer. Arrays
        are a good fit when a collection is created once and only needs to
        be queried. In the future, we could consider using immutable arrays
        here (but I didn't want to add a new library without proper
        investigation).
      - Using a `HashMap` explicitly avoids the memory overhead of linked
        hash maps.
      - These three measures led to real memory improvements in local
        testing. The memory usage is significant enough to warrant such
        optimization. In my tests, I executed Find Usages on
        `PsiElement.getText` in the `ultimate` repository, with 6GB max heap
        and taking a memory snapshot when ~9000 elements had been found.
        Here, ~250 symbol providers consumed around 4MB of memory with the
        optimized solution. That's comparable to or even exceeds the memory
        consumption of other combined symbol providers.
    - With parent packages included, there is a slight disadvantage:
      Providers will now be queried for parent packages which don't actually
      contain any symbols. However, this is a rare case: In my tests with
      `PsiElement.getText` Find Usages, 0.6% of the class-like requests and
      0.08% of callable requests were "unnecessary" (allowed by the "empty"
      parent package). For such cases, it is fine to do the additional
      iteration.
      - It was considered to use a second `providersByPackageWithParents`,
        but this consumed too much memory. Doing a separate package set
        check is also not ideal, since the package set of
        `LLCombinedPackageDelegationSymbolProvider` is currently `null` in
        real-world usage and would require the additional allocation. And
        given it's such a rare case, doing a pre-check on a set for *all*
        requests will likely drag down performance instead of improving it.
    
    ^KT-58325

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/symbolProviders/combined/LLCombinedPackageDelegationSymbolProvider.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/symbolProviders/combined/LLCombinedPackageDelegationSymbolProvider.kt
@@ -18,6 +18,8 @@ import org.jetbrains.kotlin.fir.symbols.impl.FirPropertySymbol
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.parentOrNull
+import org.jetbrains.kotlin.utils.newHashMapWithExpectedSize
 
 /**
  * [LLCombinedPackageDelegationSymbolProvider] combines multiple [FirSymbolProvider]s by delegating to the appropriate individual providers
@@ -57,46 +59,52 @@ import org.jetbrains.kotlin.name.Name
  * package delegation requires that all individual symbol providers can compute package sets, which currently isn't the case for source
  * symbol providers. But [LLCombinedKotlinSymbolProvider] might also be better in cases where multiple index accesses would be performed in
  * individual symbol providers, whereas [LLCombinedKotlinSymbolProvider] can perform a single index access.
+ *
+ * @param providersByPackage For the implementation of [hasPackage], [providersByPackage] must contain a transitive closure of all parent
+ *  packages. For example, if the map contains 'foo.bar.baz', it must also contain 'foo.bar' and 'foo'. The entry for 'foo.bar' must contain
+ *  all providers that have any package matching the prefix 'foo.bar*'.
+ *
+ *  [providersByPackage] has [String] keys instead of [FqName] keys to conserve memory. Furthermore, it uses arrays for the same purpose.
  */
 internal class LLCombinedPackageDelegationSymbolProvider private constructor(
     session: FirSession,
     override val providers: List<FirSymbolProvider>,
-    private val providersByPackage: Map<FqName, List<FirSymbolProvider>>
+    private val providersByPackage: Map<String, Array<FirSymbolProvider>>
 ) : LLCombinedSymbolProvider<FirSymbolProvider>(session) {
     override val symbolNamesProvider: FirSymbolNamesProvider = FirCompositeCachedSymbolNamesProvider.fromSymbolProviders(session, providers)
 
     override fun getClassLikeSymbolByClassId(classId: ClassId): FirClassLikeSymbol<*>? {
-        val relevantProviders = providersByPackage[classId.packageFqName] ?: return null
+        val relevantProviders = providersByPackage[classId.packageFqName.asString()] ?: return null
 
         return relevantProviders.firstNotNullOfOrNull { it.getClassLikeSymbolByClassId(classId) }
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelCallableSymbolsTo(destination: MutableList<FirCallableSymbol<*>>, packageFqName: FqName, name: Name) {
-        val relevantProviders = providersByPackage[packageFqName] ?: return
+        val relevantProviders = providersByPackage[packageFqName.asString()] ?: return
 
         relevantProviders.forEach { it.getTopLevelCallableSymbolsTo(destination, packageFqName, name) }
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelFunctionSymbolsTo(destination: MutableList<FirNamedFunctionSymbol>, packageFqName: FqName, name: Name) {
-        val relevantProviders = providersByPackage[packageFqName] ?: return
+        val relevantProviders = providersByPackage[packageFqName.asString()] ?: return
 
         relevantProviders.forEach { it.getTopLevelFunctionSymbolsTo(destination, packageFqName, name) }
     }
 
     @FirSymbolProviderInternals
     override fun getTopLevelPropertySymbolsTo(destination: MutableList<FirPropertySymbol>, packageFqName: FqName, name: Name) {
-        val relevantProviders = providersByPackage[packageFqName] ?: return
+        val relevantProviders = providersByPackage[packageFqName.asString()] ?: return
 
         relevantProviders.forEach { it.getTopLevelPropertySymbolsTo(destination, packageFqName, name) }
     }
 
     override fun hasPackage(fqName: FqName): Boolean {
-        val relevantProviders = providersByPackage[fqName] ?: return false
+        val relevantProviders = providersByPackage[fqName.asString()] ?: return false
 
-        // We still have to query individual providers since the package sets from symbol names providers
-        // may contain false positives, so the `fqName` being in `providersByPackage` doesn't prove that the package exists.
+        // We still have to query individual providers since the package sets from symbol names providers may contain false positives, so
+        // the `fqName` being in `providersByPackage` doesn't prove that the package exists.
         return relevantProviders.any { it.hasPackage(fqName) }
     }
 
@@ -117,15 +125,40 @@ internal class LLCombinedPackageDelegationSymbolProvider private constructor(
          * Builds the "package to providers" map. If any package set is `null`, the resulting map will be `null` as well, and we'll need to
          * fall back to querying all providers individually.
          */
-        private fun buildPackageToProvidersMap(providers: List<FirSymbolProvider>): Map<FqName, List<FirSymbolProvider>>? =
-            buildMap<FqName, MutableList<FirSymbolProvider>> {
+        private fun buildPackageToProvidersMap(providers: List<FirSymbolProvider>): Map<String, Array<FirSymbolProvider>>? {
+            val providerListsByPackage = buildMap {
                 providers.forEach { provider ->
                     val packageNames = provider.symbolNamesProvider.getPackageNames() ?: return null
                     packageNames.forEach { packageName ->
-                        // We start with an array list of capacity 1 since most packages will only be associated with a single provider.
-                        getOrPut(FqName(packageName)) { ArrayList(1) }.add(provider)
+                        // We only use the `FqName` here for convenience. It won't be stored.
+                        FqName(packageName).forEachFqName { fqName ->
+                            val list = getOrPut(fqName.asString()) { mutableListOf() }
+
+                            // Parent package names may overlap (e.g. 'foo.bar' and 'foo.baz' have the same parent 'foo'), so we have to be
+                            // careful not to add the same provider multiple times. Since we're iterating provider by provider, the current
+                            // provider will always be last in the list if it has been added, so we just need to check the last element.
+                            if (list.lastOrNull() != provider) {
+                                list.add(provider)
+                            }
+                        }
                     }
                 }
             }
+
+            // Avoid linked hash maps to conserve memory.
+            return newHashMapWithExpectedSize<String, Array<FirSymbolProvider>>(providerListsByPackage.size).apply {
+                providerListsByPackage.forEach { (packageName, providers) ->
+                    this[packageName] = providers.toTypedArray()
+                }
+            }
+        }
+
+        private inline fun FqName.forEachFqName(f: (FqName) -> Unit) {
+            var current: FqName? = this
+            while (current != null) {
+                f(current)
+                current = current.parentOrNull()
+            }
+        }
     }
 }
