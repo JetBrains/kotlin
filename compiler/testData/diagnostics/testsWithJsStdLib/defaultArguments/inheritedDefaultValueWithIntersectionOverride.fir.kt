0ba45fa8961a5e7c37c70354752bda8b10b73c11
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-08-13 12:10:44 +0200

    [Tests] Run tests in `diagnostics/testsWithJsStdLib/defaultArguments` till FIR2IR
    Due to KT-49722, inherited default parameters on intersection override
    with inline function are not reported as `NOT_YET_SUPPORTED_IN_INLINE`.
    Currently, without inlining functions before klib serialization,
    these tests don't make the compiler crash.
    This happens because they are run until `KLIB`.
    They would make the compiler crash if running until `BACKEND`.
    With `IrInlinerBeforeKlibSerialization` enabled,
    these tests fail when running until KLIB for the same reason.
    So it is necessary to run the pipeline until FIR2IR to keep them green
    when `IrInlinerBeforeKlibSerialization` is enabled.
    
    ^KT-79800 Fixed

--- compiler/testData/diagnostics/testsWithJsStdLib/defaultArguments/inheritedDefaultValueWithIntersectionOverride.fir.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/defaultArguments/inheritedDefaultValueWithIntersectionOverride.fir.kt
@@ -1,5 +1,6 @@
-// RUN_PIPELINE_TILL: KLIB
+// RUN_PIPELINE_TILL: FIR2IR
 // ISSUE: KT-49722
+// LANGUAGE: +IrInlinerBeforeKlibSerialization
 
 interface I {
     fun h(x: Int = 3, block: () -> String): Any
