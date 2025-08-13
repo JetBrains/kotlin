7aa103c0dc0bf70eef126bb17036b14dcf6480cb
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-08-13 14:01:12 +0300

    [FIR] Reproduce ^KT-80109

---
+++ compiler/testData/diagnostics/tests/falseAlwaysTrueIsCheck.kt
@@ -0,0 +1,12 @@
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-80109
+// RENDER_DIAGNOSTICS_MESSAGES
+
+fun main() {
+    val x: Int? = 42
+
+    if (x is <!INCOMPATIBLE_TYPES!>Byte?<!>) {}
+}
+
+/* GENERATED_FIR_TAGS: functionDeclaration, ifExpression, integerLiteral, isExpression, localProperty, nullableType,
+propertyDeclaration */
