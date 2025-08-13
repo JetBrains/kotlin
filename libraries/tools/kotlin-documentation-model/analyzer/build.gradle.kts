21cc6994c6df0811a3f060d6b2702b3ca2c2c735
Adam <152864218+adam-enko@users.noreply.github.com>
2025-08-13 18:42:46 +0200

    [KDM] Fix DGPv2 versioning-multimodule-example
    See https://github.com/Kotlin/dokka/pull/3968
    
    * Fix DGPv2 versioning-multimodule-example
    
    * rm Gradle wrapper files
    
    * tidy readme
    
    * mark previousDocVersions dir as generated
    
    * update expected data
    
    * exclude previousDocVersions dirs from git metrics
    
    * mark v1 previousDocVersions dir as generated
    Original commit: https://github.com/Kotlin/dokka/commit/244e6a542648fd44651a12a331df4fc435c58d45

--- libraries/tools/kotlin-documentation-model/analyzer/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build.gradle.kts
@@ -125,6 +125,9 @@ idea {
                 "dokka-integration-tests/gradle/projects/it-android/expectedData",
                 "dokka-integration-tests/gradle/projects/it-android-compose/expectedData",
                 "dokka-integration-tests/gradle/projects/it-kotlin-multiplatform/expectedData",
+
+                "examples/gradle/dokka-versioning-multimodule-example/previousDocVersions/",
+                "examples/gradle-v2/versioning-multimodule-example/docs/previousDocVersions/",
             )
         )
     }
