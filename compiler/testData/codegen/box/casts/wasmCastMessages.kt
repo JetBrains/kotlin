b1d4f65b1fa3f6c759b24e1a4aeb0f8a748bfa7b
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2025-08-14 17:48:02 +0400

    [Wasm] Fix cast messages test to support FQN

--- compiler/testData/codegen/box/casts/wasmCastMessages.kt
+++ compiler/testData/codegen/box/casts/wasmCastMessages.kt
@@ -1,5 +1,6 @@
 // WITH_STDLIB
 // TARGET_BACKEND: WASM
+// WASM_ALLOW_FQNAME_IN_KCLASS
 
 import kotlin.js.Promise
 import kotlin.reflect.KProperty1
@@ -52,7 +53,7 @@ interface IFACE_CS : IFACE, CharSequence
 fun <T : IFACE>wrongResultOfT(): T = Any() as T
 
 fun tryCastGeneric(): String? {
-    val expected = "Cannot cast instance of Any to IFACE: incompatible types"
+    val expected = "Cannot cast instance of kotlin.Any to IFACE: incompatible types"
     try {
         wrongResultOfT()
     } catch (cce: ClassCastException) {
@@ -65,7 +66,7 @@ fun tryCastGeneric(): String? {
 fun <T> wrongBound2(): T where T : IFACE, T : CharSequence = "txt" as Any as T
 
 fun tryCastGenericDoubleBound(): String? {
-    val expected = "Cannot cast instance of String to IFACE: incompatible types"
+    val expected = "Cannot cast instance of kotlin.String to IFACE: incompatible types"
     try {
         wrongBound2<IFACE_CS>()
     } catch (cce: ClassCastException) {
@@ -78,7 +79,7 @@ fun tryCastGenericDoubleBound(): String? {
 fun <T> wrongArray(): Array<T> = intArrayOf(1) as Any as Array<T>
 
 fun tryCastGenericArray(): String? {
-    val expected = "Cannot cast instance of IntArray to Array: incompatible types"
+    val expected = "Cannot cast instance of kotlin.IntArray to kotlin.Array: incompatible types"
     try {
         wrongArray<String>()
     } catch (cce: ClassCastException) {
@@ -104,35 +105,35 @@ open class Base: I
 class Derived: Base()
 
 fun box(): String {
-    tryCast<String>(42, "Cannot cast instance of Int to String: incompatible types")?.let { return it }
-    tryCast<String?>(42, "Cannot cast instance of Int to String?: incompatible types")?.let { return it }
-    tryCast<String>(null, "Cannot cast null to String: target type is non-nullable")?.let { return it }
-    tryCastToNNothing(42, "Expected null (Nothing?), got an instance of Int")?.let { return it }
-    tryCast<CharSequence>(42, "Cannot cast instance of Int to CharSequence: incompatible types")?.let { return it }
-    tryCast<String>(Promise.resolve(null), "Cannot cast instance of Promise to String: incompatible types")?.let { return it }
-    tryCast<Promise<*>>(42, "Cannot cast instance of Int to Promise: incompatible types")?.let { return it }
+    tryCast<String>(42, "Cannot cast instance of kotlin.Int to kotlin.String: incompatible types")?.let { return it }
+    tryCast<String?>(42, "Cannot cast instance of kotlin.Int to kotlin.String?: incompatible types")?.let { return it }
+    tryCast<String>(null, "Cannot cast null to kotlin.String: target type is non-nullable")?.let { return it }
+    tryCastToNNothing(42, "Expected null (Nothing?), got an instance of kotlin.Int")?.let { return it }
+    tryCast<CharSequence>(42, "Cannot cast instance of kotlin.Int to kotlin.CharSequence: incompatible types")?.let { return it }
+    tryCast<String>(Promise.resolve(null), "Cannot cast instance of Promise to kotlin.String: incompatible types")?.let { return it }
+    tryCast<Promise<*>>(42, "Cannot cast instance of kotlin.Int to Promise: incompatible types")?.let { return it }
     tryCastToJsAny(null)?.let { return it }
-    tryCastToNothing(42, "Cannot cast instance of Int to Nothing: incompatible types")?.let { return it }
-    tryCastToNothing(null, "Cannot cast null to Nothing: target type is non-nullable")?.let { return it }
+    tryCastToNothing(42, "Cannot cast instance of kotlin.Int to kotlin.Nothing: incompatible types")?.let { return it }
+    tryCastToNothing(null, "Cannot cast null to kotlin.Nothing: target type is non-nullable")?.let { return it }
     tryCastGeneric()?.let { return it }
     tryCastGenericDoubleBound()?.let { return it }
     tryCastGenericArray()?.let { return it }
-    tryCast<UInt>(42, "Cannot cast instance of Int to UInt: incompatible types")?.let { return it }
-    tryCast<Int>(E.A, "Cannot cast instance of E to Int: incompatible types")?.let { return it }
-    tryCast<Unit>(42, "Cannot cast instance of Int to Unit: incompatible types")?.let { return it }
+    tryCast<UInt>(42, "Cannot cast instance of kotlin.Int to kotlin.UInt: incompatible types")?.let { return it }
+    tryCast<Int>(E.A, "Cannot cast instance of E to kotlin.Int: incompatible types")?.let { return it }
+    tryCast<Unit>(42, "Cannot cast instance of kotlin.Int to kotlin.Unit: incompatible types")?.let { return it }
     val f = {}
-    tryCast<(String) -> Int>(f, "Cannot cast instance of box\$lambda to Function1: incompatible types")?.let { return it }
-    tryCast<Array<String>>(intArrayOf(1), "Cannot cast instance of IntArray to Array: incompatible types")?.let { return it }
+    tryCast<(String) -> Int>(f, "Cannot cast instance of box\$lambda to kotlin.Function1: incompatible types")?.let { return it }
+    tryCast<Array<String>>(intArrayOf(1), "Cannot cast instance of kotlin.IntArray to kotlin.Array: incompatible types")?.let { return it }
     val sf: suspend () -> Unit = suspend { }
-    tryCast<(String) -> Int>(sf, "Cannot cast instance of box\$slambda to Function1: incompatible types")?.let { return it }
-    tryCast<IntArray>(arrayOf(1), "Cannot cast instance of Array to IntArray: incompatible types")?.let { return it }
-    tryCast<Array<Int>>(intArrayOf(1), "Cannot cast instance of IntArray to Array: incompatible types")?.let { return it }
+    tryCast<(String) -> Int>(sf, "Cannot cast instance of box\$slambda to kotlin.Function1: incompatible types")?.let { return it }
+    tryCast<IntArray>(arrayOf(1), "Cannot cast instance of kotlin.Array to kotlin.IntArray: incompatible types")?.let { return it }
+    tryCast<Array<Int>>(intArrayOf(1), "Cannot cast instance of kotlin.IntArray to kotlin.Array: incompatible types")?.let { return it }
     val u: Any = 1u
-    tryCast<Int>(u, "Cannot cast instance of UInt to Int: incompatible types")?.let { return it }
+    tryCast<Int>(u, "Cannot cast instance of kotlin.UInt to kotlin.Int: incompatible types")?.let { return it }
     tryCast<B>(A(), "Cannot cast instance of A to B: incompatible types")?.let { return it }
-    tryCast<IntArray>(enumValues<E>(), "Cannot cast instance of Array to IntArray: incompatible types")?.let { return it }
+    tryCast<IntArray>(enumValues<E>(), "Cannot cast instance of kotlin.Array to kotlin.IntArray: incompatible types")?.let { return it }
     val propRef = String::length
-    tryCast<() -> Int>(propRef, "Cannot cast instance of KProperty1Impl to Function0: incompatible types")?.let { return it }
+    tryCast<() -> Int>(propRef, "Cannot cast instance of kotlin.wasm.internal.KProperty1Impl to kotlin.Function0: incompatible types")?.let { return it }
     tryCast<Derived>(Base(), "Cannot cast instance of Base to Derived: incompatible types")?.let { return it }
     return "OK"
 }
