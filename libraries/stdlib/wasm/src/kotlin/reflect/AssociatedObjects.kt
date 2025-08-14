120d6a13f1d850fe44d03da060a775288532d89c
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-08-14 09:22:04 +0200

    [stdlib] Commonize AssociatedObjects in commonNonJvmMain
    #KT-80130 Fixed

--- libraries/stdlib/wasm/src/kotlin/reflect/AssociatedObjects.kt
+++ libraries/stdlib/wasm/src/kotlin/reflect/AssociatedObjects.kt
@@ -9,29 +9,6 @@ import kotlin.wasm.internal.wasmTypeId
 import kotlin.wasm.internal.findAssociatedObject
 
 /**
- * The experimental marker for associated objects API.
- *
- * Any usage of a declaration annotated with `@ExperimentalAssociatedObjects` must be accepted either by
- * annotating that usage with the [OptIn] annotation, e.g. `@OptIn(ExperimentalAssociatedObjects::class)`,
- * or by using the compiler argument `-opt-in=kotlin.reflect.ExperimentalAssociatedObjects`.
- */
-@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
-@Retention(value = AnnotationRetention.BINARY)
-public annotation class ExperimentalAssociatedObjects
-
-/**
- * Makes the annotated annotation class an associated object key.
- *
- * An associated object key annotation should have single [KClass] parameter.
- * When applied to a class with reference to an object declaration as an argument, it binds
- * the object to the class, making this binding discoverable at runtime using [findAssociatedObject].
- */
-@ExperimentalAssociatedObjects
-@Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.ANNOTATION_CLASS)
-public annotation class AssociatedObjectKey
-
-/**
  * If [T] is an @[AssociatedObjectKey]-annotated annotation class and [this] class is annotated with @[T] (`S::class`),
  * returns object `S`.
  *
@@ -39,5 +16,5 @@ public annotation class AssociatedObjectKey
  */
 @ExperimentalAssociatedObjects
 @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
-public inline fun <reified T : Annotation> KClass<*>.findAssociatedObject(): Any? =
+public actual inline fun <reified T : Annotation> KClass<*>.findAssociatedObject(): Any? =
     findAssociatedObject(this, wasmTypeId<T>())
