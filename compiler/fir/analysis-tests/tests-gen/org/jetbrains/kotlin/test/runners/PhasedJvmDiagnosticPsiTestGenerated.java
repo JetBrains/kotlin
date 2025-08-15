6c4caae74cf1fbbd7c0e92f7c93fef623c163dbd
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-08-15 14:20:09 +0200

    [Tests] Reproduce #KT-79430

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticPsiTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticPsiTestGenerated.java
@@ -636,6 +636,12 @@ public class PhasedJvmDiagnosticPsiTestGenerated extends AbstractPhasedJvmDiagno
     }
 
     @Test
+    @TestMetadata("extensionShadowedByMemberOverride.kt")
+    public void testExtensionShadowedByMemberOverride() {
+      runTest("compiler/testData/diagnostics/tests/extensionShadowedByMemberOverride.kt");
+    }
+
+    @Test
     @TestMetadata("extensionShadowedByMemberWithMpp.kt")
     public void testExtensionShadowedByMemberWithMpp() {
       runTest("compiler/testData/diagnostics/tests/extensionShadowedByMemberWithMpp.kt");
