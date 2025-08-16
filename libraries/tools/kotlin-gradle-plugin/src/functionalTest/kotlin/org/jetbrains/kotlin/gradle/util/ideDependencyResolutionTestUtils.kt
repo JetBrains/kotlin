4331e7d077f76f84afad8e56558d05b5fadd30d7
Yury Kabargin <yury.kabargin@jetbrains.com>
2025-08-16 23:28:44 +0200

    Reapply "Enable use of deploy version for Kotlin Native in TeamCity"
    This reverts commit efc646176e22cfa8f22a5f51aa96e4d5482c0218.

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/ideDependencyResolutionTestUtils.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/ideDependencyResolutionTestUtils.kt
@@ -5,7 +5,16 @@
 
 package org.jetbrains.kotlin.gradle.util
 
+import org.gradle.kotlin.dsl.maven
 import org.jetbrains.kotlin.gradle.idea.testFixtures.tcs.binaryCoordinates
+import org.jetbrains.kotlin.gradle.internal.properties.nativeProperties
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginLifecycle
+import org.jetbrains.kotlin.gradle.plugin.await
+import org.jetbrains.kotlin.gradle.plugin.kotlinPluginLifecycle
+import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeCompilation
+import org.jetbrains.kotlin.gradle.targets.native.internal.retrievePlatformDependencies
 
 /**
  * Legacy -jdk8 and -jdk7 dependencies:
@@ -21,3 +30,58 @@ fun legacyStdlibJdkDependencies(version: String = "1.8.0") = listOf(
     binaryCoordinates("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"),
     binaryCoordinates("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"),
 )
+
+/**
+ * Download and unpack Kotlin Native distribution
+ *
+ * This is a workaround for the issue where Kotlin Native isn't unpacked into ~/.konan dir when used from local Maven repo: KT-77580
+ * as opposed to CDN where it happens in the project configuration phase.
+ * It should be run before tests that check dependencies in ~/.konan.
+ * The project in this function is created only to trigger downloading and unpacking Kotlin Native
+ * and isn't supposed to be used otherwise.
+ */
+fun provisionKotlinNativeDistribution() {
+    val project = buildProjectWithMPP {
+        if (project.nativeProperties.actualNativeHomeDirectory.get().exists()) return@buildProjectWithMPP
+
+        // For CI
+        repositories.mavenLocal()
+        // For local runs
+        repositories.maven("https://packages.jetbrains.team/maven/p/kt/bootstrap")
+        // Remove after KTI-1994
+        listOf(
+            "https://download.jetbrains.com/kotlin/native/builds/releases",
+            "https://download.jetbrains.com/kotlin/native/builds/dev",
+        ).forEach { repoUrl ->
+            repositories.ivy {
+                it.url = project.uri(repoUrl)
+                it.patternLayout {
+                    it.artifact("[revision]/[classifier]/[artifact]-[classifier]-[revision].[ext]")
+                }
+                it.metadataSources {
+                    it.artifact()
+                }
+            }
+        }
+
+        kotlin {
+            iosArm64()
+            iosX64()
+            linuxX64()
+            linuxArm64()
+            mingwX64()
+            kotlinPluginLifecycle.launch {
+                KotlinPluginLifecycle.Stage.ReadyForExecution.await()
+                val downloadDependencies = project.files()
+                targets.filter { it.platformType != KotlinPlatformType.common }.forEach {
+                    downloadDependencies.from(
+                        (it.compilations.getByName(KotlinCompilation.MAIN_COMPILATION_NAME) as AbstractKotlinNativeCompilation)
+                            .retrievePlatformDependencies()
+                    )
+                }
+                downloadDependencies.files
+            }
+        }
+    }
+    project.evaluate()
+}
