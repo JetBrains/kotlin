f83714cf868e507dd30004ca3b15d5082630eec9
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-08-18 19:02:14 +0200

    [Inline] Add missing originalOfErasedTopLevelCopy write
    KT-75794

--- compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/InlineFunctionSerializationPreProcessing.kt
+++ compiler/ir/ir.inline/src/org/jetbrains/kotlin/ir/inline/InlineFunctionSerializationPreProcessing.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.ir.expressions.IrInlinedFunctionBlock
 import org.jetbrains.kotlin.ir.types.extractTypeParameters
 import org.jetbrains.kotlin.ir.util.erasedTopLevelCopy
 import org.jetbrains.kotlin.ir.util.file
+import org.jetbrains.kotlin.ir.util.originalOfErasedTopLevelCopy
 import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 
@@ -29,7 +30,9 @@ class InlineFunctionSerializationPreProcessing(private val context: LoweringCont
 
     override fun visitSimpleFunction(declaration: IrSimpleFunction) {
         if (!declaration.isInline || declaration.body == null || declaration.symbol.isConsideredAsPrivateForInlining()) return
-        declaration.erasedTopLevelCopy = declaration.copyAndEraseTypeParameters().convertToPrivateTopLevel().erasePrivateSymbols()
+        val preprocessed = declaration.copyAndEraseTypeParameters().convertToPrivateTopLevel().erasePrivateSymbols()
+        declaration.erasedTopLevelCopy = preprocessed
+        preprocessed.originalOfErasedTopLevelCopy = declaration
     }
 
     private fun IrSimpleFunction.copyAndEraseTypeParameters(): IrSimpleFunction {
