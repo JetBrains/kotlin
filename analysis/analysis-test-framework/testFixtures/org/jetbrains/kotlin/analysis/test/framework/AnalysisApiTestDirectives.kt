4889b1b06827b6478147bb820782b2cea3fbb729
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-08-18 15:16:04 +0200

    [AA] Minor: Fix typos in `AnalysisApiTestDirectives`

--- analysis/analysis-test-framework/testFixtures/org/jetbrains/kotlin/analysis/test/framework/AnalysisApiTestDirectives.kt
+++ analysis/analysis-test-framework/testFixtures/org/jetbrains/kotlin/analysis/test/framework/AnalysisApiTestDirectives.kt
@@ -17,9 +17,9 @@ object AnalysisApiTestDirectives : SimpleDirectivesContainer() {
     )
 
     val DISABLE_DEPENDED_MODE by directive("Analysis in dependent mode should not be run in this test")
-    val IGNORE_FE10 by directive("FE10 Analysis API implementation test should mot be run")
-    val IGNORE_FIR by directive("FIR Analysis API implementation test should mot be run")
-    val IGNORE_STANDALONE by directive("Standalone implementation test should mot be run")
+    val IGNORE_FE10 by directive("FE10 Analysis API implementation test should not be run")
+    val IGNORE_FIR by directive("FIR Analysis API implementation test should not be run")
+    val IGNORE_STANDALONE by directive("Standalone implementation test should not be run")
 
     /**
      * @see org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedTest.findMainFile
