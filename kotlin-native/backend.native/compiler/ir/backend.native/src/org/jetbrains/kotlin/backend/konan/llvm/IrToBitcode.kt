9fb82cbb081a599671d92f40f7444919fa0019d4
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2025-08-20 14:12:34 +0200

    Revert "noinline initializers"
    This reverts commit 3bba1b0ea5d41a7d5678507b683b5b41458ecd0f.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
@@ -436,12 +436,7 @@ internal class CodeGeneratorVisitor(
     val ctorFunctionSignature = LlvmFunctionSignature(LlvmRetType(llvm.voidType, isObjectType = false))
     val kNodeInitType = llvm.runtime.initNodeType
     val kMemoryStateType = llvm.runtime.memoryStateType
-    val kInitFuncType = LlvmFunctionSignature(
-        LlvmRetType(llvm.voidType, isObjectType = false),
-        listOf(LlvmParamType(llvm.int32Type),
-        LlvmParamType(pointerType(kMemoryStateType))),
-        functionAttributes = listOf(LlvmFunctionAttribute.NoInline)
-    )
+    val kInitFuncType = LlvmFunctionSignature(LlvmRetType(llvm.voidType, isObjectType = false), listOf(LlvmParamType(llvm.int32Type), LlvmParamType(pointerType(kMemoryStateType))))
 
     //-------------------------------------------------------------------------//
 
