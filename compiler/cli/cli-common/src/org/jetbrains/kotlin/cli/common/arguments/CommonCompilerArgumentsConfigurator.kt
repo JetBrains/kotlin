bdc68884ed78fea0f9fcf7324a11efe0d44307a6
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-08-20 16:58:10 +0200

    [Args] Add error for wrong IR inliner configuration
    ^KT-79717

--- compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArgumentsConfigurator.kt
+++ compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArgumentsConfigurator.kt
@@ -132,6 +132,16 @@ open class CommonCompilerArgumentsConfigurator {
             put(LanguageFeature.DisableCompatibilityModeForNewInference, LanguageFeature.State.ENABLED)
         }
 
+        val isCrossModuleInlinerEnabled = this[LanguageFeature.IrCrossModuleInlinerBeforeKlibSerialization] == LanguageFeature.State.ENABLED
+        val isIntraModuleInlinerEnabled = this[LanguageFeature.IrIntraModuleInlinerBeforeKlibSerialization] == LanguageFeature.State.ENABLED
+        if (isCrossModuleInlinerEnabled && !isIntraModuleInlinerEnabled) {
+            collector.report(
+                CompilerMessageSeverity.ERROR,
+                "-XXLanguage:+IrCrossModuleInlinerBeforeKlibSerialization requires -XXLanguage:+IrIntraModuleInlinerBeforeKlibSerialization. " +
+                        "Enable the intra-module inliner as well to avoid inconsistent configuration."
+            )
+        }
+
         if (featuresThatForcePreReleaseBinaries.isNotEmpty()) {
             collector.report(
                 CompilerMessageSeverity.STRONG_WARNING,
