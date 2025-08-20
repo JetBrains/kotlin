8082c0f2a6ce85bf2fbc165244d1d774fe5f66f6
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-08-20 11:38:02 +0200

    [Args] Introduce a new argument type: `KlibIrInlinerMode`
    ^KT-79717

---
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/dsl/types/KlibIrInlinerMode.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.arguments.dsl.types
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.kotlin.arguments.dsl.base.KotlinReleaseVersion
+import org.jetbrains.kotlin.arguments.dsl.base.KotlinReleaseVersionLifecycle
+import org.jetbrains.kotlin.arguments.dsl.base.WithKotlinReleaseVersionsMetadata
+import org.jetbrains.kotlin.arguments.serialization.json.KotlinKlibIrInlinerModeAsNameSerializer
+
+@Serializable(KotlinKlibIrInlinerModeAsNameSerializer::class)
+enum class KlibIrInlinerMode(
+    val modeState: String,
+    override val releaseVersionsMetadata: KotlinReleaseVersionLifecycle,
+) : WithKotlinReleaseVersionsMetadata {
+    intraModule(
+        modeState = "intra-module",
+        releaseVersionsMetadata = KotlinReleaseVersionLifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_3_0,
+        ),
+    ),
+    full(
+        modeState = "full",
+        releaseVersionsMetadata = KotlinReleaseVersionLifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_3_0,
+        ),
+    ),
+    disabled(
+        modeState = "disabled",
+        releaseVersionsMetadata = KotlinReleaseVersionLifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_3_0,
+        ),
+    ),
+}
