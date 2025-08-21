42288f123b8f9fa46154a2cf06a315da9f27480e
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2025-08-21 16:24:22 +0200

    Update KaType.render kdoc
    #KTIJ-35058

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/components/KaRenderer.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/components/KaRenderer.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.analysis.api.KaContextParameterApi
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.renderer.declarations.KaDeclarationRenderer
+import org.jetbrains.kotlin.analysis.api.renderer.declarations.KaRendererTypeApproximator
 import org.jetbrains.kotlin.analysis.api.renderer.declarations.impl.KaDeclarationRendererForSource
 import org.jetbrains.kotlin.analysis.api.renderer.types.KaTypeRenderer
 import org.jetbrains.kotlin.analysis.api.renderer.types.impl.KaTypeRendererForSource
@@ -31,8 +32,10 @@ public interface KaRenderer : KaSessionComponent {
     /**
      * Renders the given [KaType] into a string. The particular rendering strategy is defined by the [renderer].
      *
-     * If [position] is not invariant, the rendered type is approximated. Specifically, a denotable subtype is used for
-     * [Variance.IN_VARIANCE], and a denotable supertype is used for [Variance.OUT_VARIANCE].
+     * [position] controls the way the given type is approximated using [KaTypeRenderer.typeApproximator].
+     *
+     * Specifically, when [KaRendererTypeApproximator.TO_DENOTABLE] is used, no approximation is performed for [Variance.INVARIANT],
+     * a denotable subtype is rendered for [Variance.IN_VARIANCE], and a denotable supertype is rendered for [Variance.OUT_VARIANCE].
      */
     @KaExperimentalApi
     public fun KaType.render(renderer: KaTypeRenderer = KaTypeRendererForSource.WITH_QUALIFIED_NAMES, position: Variance): String
