febb3f5949bd36a9af26334ec3ca29174f80dc99
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2025-08-21 11:31:14 +0200

    fixup! llvm-split

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/OptimizationPipeline.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/OptimizationPipeline.kt
@@ -294,7 +294,7 @@ class ModuleOptimizationPipeline(config: LlvmPipelineConfig, performanceManager:
         LlvmOptimizationPipeline(config, performanceManager, logger) {
     override val pipelineName = "llvm-default"
     override val passes = listOf(config.modulePasses ?: "default<$optimizationFlag>")
-    override val threads = 4
+    override val threads = 12
 }
 
 class LTOOptimizationPipeline(config: LlvmPipelineConfig, performanceManager: PerformanceManager?, logger: LoggingContext? = null) :
