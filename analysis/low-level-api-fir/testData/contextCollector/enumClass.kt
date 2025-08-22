594cb689f408e49c4ec76a0e38f7b715414c2a55
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-22 19:43:02 +0200

    [LL] add context collector tests on classes
    ^KT-79653

---
+++ analysis/low-level-api-fir/testData/contextCollector/enumClass.kt
@@ -0,0 +1,22 @@
+package foo
+
+<expr>enum class MyEnumClass {
+    ENTRY1,
+    ENTRY2,
+    ENTRY3;
+
+    fun foo() {
+
+    }
+
+    val bar: Int = 42
+
+    class NestedClass {
+        class Nested
+    }
+
+    object NestedObject
+    inner class InnerClass {
+        val value = 10
+    }
+}</expr>
