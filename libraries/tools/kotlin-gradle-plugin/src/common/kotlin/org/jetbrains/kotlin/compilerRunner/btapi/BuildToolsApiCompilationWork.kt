de09a1d5635edc4a2984abe0a479716ca33912c2
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2025-08-22 11:27:59 +0200

    [BTA+KGP] Fixing daemon tests WIP

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/compilerRunner/btapi/BuildToolsApiCompilationWork.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/compilerRunner/btapi/BuildToolsApiCompilationWork.kt
@@ -5,6 +5,7 @@
 
 package org.jetbrains.kotlin.compilerRunner.btapi
 
+import com.android.build.gradle.internal.cxx.os.exe
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.provider.ListProperty
@@ -23,16 +24,22 @@ import org.jetbrains.kotlin.buildtools.api.jvm.JvmSnapshotBasedIncrementalCompil
 import org.jetbrains.kotlin.buildtools.api.jvm.JvmSnapshotBasedIncrementalCompilationOptions.Companion.PRECISE_JAVA_TRACKING
 import org.jetbrains.kotlin.buildtools.api.jvm.JvmSnapshotBasedIncrementalCompilationOptions.Companion.ROOT_PROJECT_DIR
 import org.jetbrains.kotlin.buildtools.api.jvm.JvmSnapshotBasedIncrementalCompilationOptions.Companion.USE_FIR_RUNNER
+import org.jetbrains.kotlin.buildtools.api.jvm.operations.JvmCompilationOperation
 import org.jetbrains.kotlin.buildtools.api.jvm.operations.JvmCompilationOperation.Companion.COMPILER_ARGUMENTS_LOG_LEVEL
 import org.jetbrains.kotlin.buildtools.api.jvm.operations.JvmCompilationOperation.Companion.INCREMENTAL_COMPILATION
 import org.jetbrains.kotlin.buildtools.api.jvm.operations.JvmCompilationOperation.Companion.KOTLINSCRIPT_EXTENSIONS
 import org.jetbrains.kotlin.cli.common.ExitCode
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
 import org.jetbrains.kotlin.cli.common.arguments.parseCommandLineArguments
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.compilerRunner.GradleKotlinCompilerWorkArguments
 import org.jetbrains.kotlin.compilerRunner.asFinishLogMessage
 import org.jetbrains.kotlin.gradle.internal.ClassLoadersCachingBuildService
 import org.jetbrains.kotlin.gradle.internal.ParentClassLoaderProvider
+import org.jetbrains.kotlin.gradle.logging.GradleErrorMessageCollector
+import org.jetbrains.kotlin.gradle.logging.GradlePrintingMessageCollector
+import org.jetbrains.kotlin.gradle.logging.kotlinDebug
+import org.jetbrains.kotlin.gradle.logging.logCompilerArgumentsMessage
 import org.jetbrains.kotlin.gradle.plugin.BuildFinishedListenerService
 import org.jetbrains.kotlin.gradle.plugin.internal.BuildIdService
 import org.jetbrains.kotlin.gradle.plugin.internal.state.TaskExecutionResults
@@ -41,7 +48,9 @@ import org.jetbrains.kotlin.gradle.report.TaskExecutionInfo
 import org.jetbrains.kotlin.gradle.report.TaskExecutionResult
 import org.jetbrains.kotlin.gradle.tasks.*
 import org.jetbrains.kotlin.gradle.utils.destinationAsFile
+import org.jetbrains.kotlin.gradle.utils.stackTraceAsString
 import org.jetbrains.kotlin.incremental.ClasspathChanges
+import org.jetbrains.kotlin.util.removeSuffixIfPresent
 import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.ObjectInputStream
@@ -78,8 +87,7 @@ internal abstract class BuildToolsApiCompilationWork @Inject constructor(
 
     private val log: KotlinLogger = getTaskLogger(taskPath, LOGGER_PREFIX, BuildToolsApiCompilationWork::class.java.simpleName, true)
 
-    private fun performCompilation(): CompilationResult {
-        val executionStrategy = workArguments.compilerExecutionSettings.strategy
+    private fun performCompilation(executionStrategy: KotlinCompilerExecutionStrategy): CompilationResult {
         try {
             val classLoader = parameters.classLoadersCachingService.get()
                 .getClassLoader(workArguments.compilerFullClasspath, SharedApiClassesClassLoaderProvider)
@@ -89,14 +97,7 @@ internal abstract class BuildToolsApiCompilationWork @Inject constructor(
             parameters.buildFinishedListenerService.get().onCloseOnceByKey(buildId.toString()) {
                 build.close()
             }
-            val executionConfig = when (executionStrategy) {
-                KotlinCompilerExecutionStrategy.DAEMON -> compilationService.createDaemonExecutionPolicy().apply {
-                    this[ExecutionPolicy.WithDaemon.Companion.JVM_ARGUMENTS] =
-                        workArguments.compilerExecutionSettings.daemonJvmArgs ?: emptyList()
-                }
-                KotlinCompilerExecutionStrategy.IN_PROCESS -> compilationService.createInProcessExecutionPolicy()
-                else -> error("The \"$executionStrategy\" execution strategy is not supported by the Build Tools API")
-            }
+
             val args = parseCommandLineArguments<K2JVMCompilerArguments>(workArguments.compilerArgs.toList())
             val jvmCompilationOperation = compilationService.jvm.createJvmCompilationOperation(
                 args.freeArgs.mapNotNull {
@@ -139,7 +140,7 @@ internal abstract class BuildToolsApiCompilationWork @Inject constructor(
 
                 when (classpathChanges) {
                     is ClasspathChanges.ClasspathSnapshotEnabled.IncrementalRun.NoChanges -> {
-                        classpathSnapshotsOptions[JvmSnapshotBasedIncrementalCompilationOptions.Companion.ASSURED_NO_CLASSPATH_SNAPSHOT_CHANGES] =
+                        classpathSnapshotsOptions[JvmSnapshotBasedIncrementalCompilationOptions.ASSURED_NO_CLASSPATH_SNAPSHOT_CHANGES] =
                             true
                     }
                     is ClasspathChanges.ClasspathSnapshotEnabled.NotAvailableForNonIncrementalRun -> {
@@ -148,25 +149,37 @@ internal abstract class BuildToolsApiCompilationWork @Inject constructor(
                     else -> {}
                 }
             }
-            return build.executeOperation(jvmCompilationOperation, executionConfig, log).also {
-                if (metrics is BuildMetricsReporterImpl) {
-                    @Suppress("DEPRECATION_ERROR")
-                    val key = BuildOperation.createCustomOption<ByteArray>("XX_KGP_METRICS_COLLECTOR_OUT")
-                    try {
-                        ByteArrayInputStream(jvmCompilationOperation[key]).use {
-                            @Suppress("UNCHECKED_CAST")
-                            val metricsFromBta =
-                                ObjectInputStream(it).readObject() as BuildMetricsReporterImpl<GradleBuildTime, GradleBuildPerformanceMetric>
-                            metrics.addMetrics(metricsFromBta.getMetrics())
-                        }
-                    } catch (_: Exception) {
+            val executionConfig = when (executionStrategy) {
+                KotlinCompilerExecutionStrategy.DAEMON -> compilationService.createDaemonExecutionPolicy().apply {
+                    val arguments = workArguments.compilerExecutionSettings.daemonJvmArgs ?: emptyList()
+                    this[ExecutionPolicy.WithDaemon.JVM_ARGUMENTS] = arguments
+                    if (log.isDebugEnabled) {
+                        log.debug("Kotlin compile daemon JVM options: ${arguments.joinToString(" ")}")
                     }
                 }
+                KotlinCompilerExecutionStrategy.IN_PROCESS -> compilationService.createInProcessExecutionPolicy()
+                else -> error("The \"$executionStrategy\" execution strategy is not supported by the Build Tools API")
             }
+            return build.executeOperation(jvmCompilationOperation, executionConfig, log)
+                .also { extractMetrics(jvmCompilationOperation) }
         } catch (e: Throwable) {
             wrapAndRethrowCompilationException(executionStrategy, e)
-        } finally {
-            log.info(executionStrategy.asFinishLogMessage)
+        }
+    }
+
+    private fun extractMetrics(jvmCompilationOperation: JvmCompilationOperation) {
+        if (metrics is BuildMetricsReporterImpl) {
+            @Suppress("DEPRECATION_ERROR")
+            val key = BuildOperation.createCustomOption<ByteArray>("XX_KGP_METRICS_COLLECTOR_OUT")
+            try {
+                ByteArrayInputStream(jvmCompilationOperation[key]).use {
+                    @Suppress("UNCHECKED_CAST")
+                    val metricsFromBta =
+                        ObjectInputStream(it).readObject() as BuildMetricsReporterImpl<GradleBuildTime, GradleBuildPerformanceMetric>
+                    metrics.addMetrics(metricsFromBta.getMetrics())
+                }
+            } catch (_: Exception) {
+            }
         }
     }
 
@@ -182,23 +195,84 @@ internal abstract class BuildToolsApiCompilationWork @Inject constructor(
         null
     }
 
-    override fun execute() {
-        metrics.addTimeMetric(GradleBuildPerformanceMetric.START_WORKER_EXECUTION)
-        metrics.startMeasure(GradleBuildTime.RUN_COMPILATION_IN_WORKER)
-        val backup = initializeBackup()
-        val executionStrategy = workArguments.compilerExecutionSettings.strategy
+    private fun compileWithDaemonOrFallback(
+        executionStrategy: KotlinCompilerExecutionStrategy,
+        backup: TaskOutputsBackup?,
+    ): CompilationResult {
+        with(log) {
+            kotlinDebug { "Kotlin compiler class: ${workArguments.compilerClassName}" }
+            kotlinDebug {
+                val compilerClasspath = workArguments.compilerFullClasspath.joinToString(File.pathSeparator) { it.normalize().absolutePath }
+                "Kotlin compiler classpath: $compilerClasspath"
+            }
+            logCompilerArgumentsMessage(workArguments.compilerArgumentsLogLevel) {
+                "${workArguments.taskPath} Kotlin compiler args: ${workArguments.compilerArgs.joinToString(" ")}"
+            }
+        }
         try {
-            val result = performCompilation()
+            val result = performCompilation(executionStrategy)
             if (result == CompilationResult.COMPILATION_OOM_ERROR || result == CompilationResult.COMPILATION_ERROR) {
                 backup?.restoreOutputs()
             }
             throwExceptionIfCompilationFailed(result.asExitCode, executionStrategy)
+            log.info(executionStrategy.asFinishLogMessage)
+            return result
         } catch (e: FailedCompilationException) {
             backup?.tryRestoringOnRecoverableException(e) { restoreAction ->
                 log.info(DEFAULT_BACKUP_RESTORE_MESSAGE)
                 restoreAction()
             }
             throw e
+        }
+    }
+
+    override fun execute() {
+        metrics.addTimeMetric(GradleBuildPerformanceMetric.START_WORKER_EXECUTION)
+        metrics.startMeasure(GradleBuildTime.RUN_COMPILATION_IN_WORKER)
+        val backup = initializeBackup()
+        try {
+            compileWithDaemonOrFallback(workArguments.compilerExecutionSettings.strategy, backup)
+        } catch (e: FailedCompilationException) {
+            if (workArguments.compilerExecutionSettings.strategy == KotlinCompilerExecutionStrategy.DAEMON) {
+                val gradlePrintingMessageCollector = GradlePrintingMessageCollector(log, workArguments.allWarningsAsErrors)
+                val gradleMessageCollector = GradleErrorMessageCollector(
+                    log,
+                    gradlePrintingMessageCollector,
+                    kotlinPluginVersion = workArguments.kotlinPluginVersion
+                )
+                gradleMessageCollector.report(
+                    severity = CompilerMessageSeverity.EXCEPTION,
+                    message = "Daemon compilation failed: ${e.message}\n${e.stackTraceToString()}"
+                )
+                val recommendation = """
+                        Try ./gradlew --stop if this issue persists
+                        If it does not look related to your configuration, please file an issue with logs to https://kotl.in/issue
+                    """.trimIndent()
+                if (!workArguments.compilerExecutionSettings.useDaemonFallbackStrategy) {
+                    throw RuntimeException(
+                        """
+                            |Failed to compile with Kotlin daemon.
+                            |Fallback strategy (compiling without Kotlin daemon) is turned off.
+                            |$recommendation
+                            """.trimMargin(),
+                        e
+                    )
+                }
+                val failDetails = e.stackTraceAsString().removeSuffixIfPresent("\n")
+                log.warn(
+                    """
+                        |Failed to compile with Kotlin daemon: $failDetails
+                        |Using fallback strategy: Compile without Kotlin daemon
+                        |$recommendation
+                        """.trimMargin()
+                )
+                compileWithDaemonOrFallback(
+                    KotlinCompilerExecutionStrategy.IN_PROCESS,
+                    backup
+                )
+            } else {
+                throw e
+            }
         } finally {
             val taskInfo = TaskExecutionInfo(
                 kotlinLanguageVersion = workArguments.kotlinLanguageVersion,
@@ -213,6 +287,7 @@ internal abstract class BuildToolsApiCompilationWork @Inject constructor(
             backup?.deleteSnapshot()
         }
     }
+
 
     // temporary adapter property
     private val CompilationResult.asExitCode
