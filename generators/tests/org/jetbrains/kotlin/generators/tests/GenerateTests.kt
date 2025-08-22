d02d2f3a377cb9f79b5fb8dab4ecb111f0fb050c
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-08-22 17:25:39 +0300

    [Build] Extract test generator for :plugins:plugins-interactions-testing (2 of 2)

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -5,23 +5,16 @@
 
 package org.jetbrains.kotlin.generators.tests
 
-import org.jetbrains.kotlin.compiler.plugins.AbstractPluginInteractionFirBlackBoxCodegenTest
 import org.jetbrains.kotlin.fir.dataframe.AbstractCompilerFacilityTestForDataFrame
 import org.jetbrains.kotlin.fir.dataframe.AbstractDataFrameBlackBoxCodegenTest
 import org.jetbrains.kotlin.fir.dataframe.AbstractDataFrameDiagnosticTest
 import org.jetbrains.kotlin.generators.generateTestGroupSuiteWithJUnit5
-import org.jetbrains.kotlin.generators.util.TestGeneratorUtil
 
 
 fun main(args: Array<String>) {
     System.setProperty("java.awt.headless", "true")
 
     generateTestGroupSuiteWithJUnit5 {
-        testGroup("plugins/plugins-interactions-testing/tests-gen", "plugins/plugins-interactions-testing/testData") {
-            testClass<AbstractPluginInteractionFirBlackBoxCodegenTest> {
-                model("box", excludedPattern = TestGeneratorUtil.KT_OR_KTS_WITH_FIR_PREFIX)
-            }
-        }
 
         testGroup("plugins/kotlin-dataframe/tests-gen", "plugins/kotlin-dataframe/testData") {
             testClass<AbstractDataFrameDiagnosticTest> {
