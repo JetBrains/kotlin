f3e3d34b27cc6a342b4b753fcc467318c4f057c0
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-08-25 14:08:04 +0300

    [Args] Add `-XXLanguage` as a regular CLI flag
    ^KT-80348

--- compiler/cli/cli-common/gen/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArgumentsCopyGenerated.kt
+++ compiler/cli/cli-common/gen/org/jetbrains/kotlin/cli/common/arguments/CommonCompilerArgumentsCopyGenerated.kt
@@ -53,6 +53,7 @@ fun copyCommonCompilerArguments(from: CommonCompilerArguments, to: CommonCompile
     to.languageVersion = from.languageVersion
     to.lenientMode = from.lenientMode
     to.listPhases = from.listPhases
+    to.manuallyConfiguredFeatures = from.manuallyConfiguredFeatures?.copyOf()
     to.metadataKlib = from.metadataKlib
     to.metadataVersion = from.metadataVersion
     to.multiDollarInterpolation = from.multiDollarInterpolation
