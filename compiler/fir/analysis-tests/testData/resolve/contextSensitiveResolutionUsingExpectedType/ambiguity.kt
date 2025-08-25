2745497fbfba65c9b691eca992a657a9f0999266
Alejandro Serrano Mena <alejandro.serrano.mena@jetbrains.com>
2025-08-25 16:34:26 +0200

    fixup! Tests without 'when'

--- compiler/fir/analysis-tests/testData/resolve/contextSensitiveResolutionUsingExpectedType/ambiguity.kt
+++ compiler/fir/analysis-tests/testData/resolve/contextSensitiveResolutionUsingExpectedType/ambiguity.kt
@@ -33,6 +33,51 @@ fun topLevelExplicit(s: Sealed): Int = when (s) {
     else -> 5
 }
 
-/* GENERATED_FIR_TAGS: classDeclaration, data, equalityExpression, functionDeclaration, integerLiteral, isExpression,
-nestedClass, objectDeclaration, primaryConstructor, propertyDeclaration, sealed, smartcast, whenExpression,
+fun cast1wrong(s: Sealed): Int {
+    s <!CAST_NEVER_SUCCEEDS!>as<!> <!CONTEXT_SENSITIVE_RESOLUTION_AMBIGUITY!>A<!>
+    return 1
+}
+
+fun cast2wrong(s: Sealed): Int {
+    s <!CAST_NEVER_SUCCEEDS!>as<!> <!CONTEXT_SENSITIVE_RESOLUTION_AMBIGUITY!>B<!>
+    return 2
+}
+
+fun cast1sealed(s: Sealed): Int {
+    s as Sealed.A
+    return 1
+}
+
+fun cast2sealed(s: Sealed): Int {
+    s as Sealed.B
+    return 2
+}
+
+fun cast1topLevel(s: Sealed): Int {
+    s <!CAST_NEVER_SUCCEEDS!>as<!> foo.A
+    return 1
+}
+
+fun cast2topLevel(s: Sealed): Int {
+    s <!CAST_NEVER_SUCCEEDS!>as<!> foo.B
+    return 2
+}
+
+fun cast3ok(s: Sealed): Int {
+    s as C
+    return 3
+}
+
+fun cast4ok(s: Sealed): Int {
+    s as D
+    return 4
+}
+
+fun equality1wrong(s: Sealed): Boolean = s == <!CONTEXT_SENSITIVE_RESOLUTION_AMBIGUITY!>A<!>
+fun equality1sealed(s: Sealed): Boolean = s == Sealed.A
+fun equality1topLevel(s: Sealed): Boolean = s == foo.A
+fun equality2ok(s: Sealed): Boolean = s == Sealed.C
+
+/* GENERATED_FIR_TAGS: asExpression, classDeclaration, data, equalityExpression, functionDeclaration, integerLiteral,
+isExpression, nestedClass, objectDeclaration, primaryConstructor, propertyDeclaration, sealed, smartcast, whenExpression,
 whenWithSubject */
