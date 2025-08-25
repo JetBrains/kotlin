002bc2f1aca54c367c768061742c2930bbe67d91
grigorii.solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2025-08-25 12:35:06 +0300

    [Tests] Differentiate collection literals in RHS and call arguments

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticLightTreeTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/PhasedJvmDiagnosticLightTreeTestGenerated.java
@@ -54008,9 +54008,21 @@ public class PhasedJvmDiagnosticLightTreeTestGenerated extends AbstractPhasedJvm
         }
 
         @Test
+        @TestMetadata("genericCollectionAssignments.kt")
+        public void testGenericCollectionAssignments() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/genericCollectionAssignments.kt");
+        }
+
+        @Test
         @TestMetadata("genericCollectionWithNonGenericElementType.kt")
         public void testGenericCollectionWithNonGenericElementType() {
           runTest("compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/genericCollectionWithNonGenericElementType.kt");
+        }
+
+        @Test
+        @TestMetadata("genericCollectionWithNonGenericElementTypeAssignments.kt")
+        public void testGenericCollectionWithNonGenericElementTypeAssignments() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/genericCollectionWithNonGenericElementTypeAssignments.kt");
         }
 
         @Test
@@ -54020,9 +54032,21 @@ public class PhasedJvmDiagnosticLightTreeTestGenerated extends AbstractPhasedJvm
         }
 
         @Test
+        @TestMetadata("nonGenericCollectionAssignments.kt")
+        public void testNonGenericCollectionAssignments() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/nonGenericCollectionAssignments.kt");
+        }
+
+        @Test
         @TestMetadata("nonGenericCollectionWithGenericElementType.kt")
         public void testNonGenericCollectionWithGenericElementType() {
           runTest("compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/nonGenericCollectionWithGenericElementType.kt");
+        }
+
+        @Test
+        @TestMetadata("nonGenericCollectionWithGenericElementTypeAssignments.kt")
+        public void testNonGenericCollectionWithGenericElementTypeAssignments() {
+          runTest("compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/nonGenericCollectionWithGenericElementTypeAssignments.kt");
         }
       }
     }
