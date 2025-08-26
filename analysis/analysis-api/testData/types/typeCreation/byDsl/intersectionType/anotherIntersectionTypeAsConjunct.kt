11592cc11231129da236503ec4758a89616fe571
Oleg Makeev <oleg.makeev@jetbrains.com>
2025-08-26 17:45:16 +0200

    [Analysis API] Implement DSL test data for `KaTypeCreator.intersectionType`
    ^KT-65912
    ^KT-66566
    ^KT-66043

---
+++ analysis/analysis-api/testData/types/typeCreation/byDsl/intersectionType/anotherIntersectionTypeAsConjunct.kt
@@ -0,0 +1,6 @@
+fun foo(xx: Int, yy: String, zz: Boolean, ff: Nothing) {
+    x<caret_1>x.toString()
+    y<caret_2>y.toString()
+    z<caret_3>z.toString()
+    f<caret_4>f.toString()
+}
---
+++ analysis/analysis-api/testData/types/typeCreation/byDsl/intersectionType/withAnnotations.kt
@@ -0,0 +1,8 @@
+annotation class MyAnno1
+annotation class MyAnno2
+annotation class MyAnno3
+
+
+fun foo(xx: Any) {
+    x<caret_type>x.toString()
+}
