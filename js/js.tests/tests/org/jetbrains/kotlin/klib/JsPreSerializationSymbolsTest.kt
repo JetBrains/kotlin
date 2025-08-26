118c36b1f5ebae3bee3a2b4f79e3d59305f7fc9f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-08-26 12:15:44 +0200

    [IR] Create a validation test to check pre-serialization symbols
    #KT-79801

---
+++ js/js.tests/tests/org/jetbrains/kotlin/klib/JsPreSerializationSymbolsTest.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.klib
+
+import org.jetbrains.kotlin.js.test.converters.Fir2IrCliWebFacade
+import org.jetbrains.kotlin.js.test.converters.FirCliWebFacade
+import org.jetbrains.kotlin.platform.js.JsPlatforms
+import org.jetbrains.kotlin.test.TargetBackend
+import org.jetbrains.kotlin.test.backend.ir.IrPreSerializationJsSymbolValidationHandler
+import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.klib.AbstractPreSerializationSymbolsTest
+import org.jetbrains.kotlin.test.services.configuration.JsEnvironmentConfigurator
+
+@Suppress("JUnitTestCaseWithNoTests")
+class JsPreSerializationSymbolsTest : AbstractPreSerializationSymbolsTest(
+    TargetBackend.JS_IR,
+    JsPlatforms.defaultJsPlatform,
+    ::FirCliWebFacade,
+    ::Fir2IrCliWebFacade,
+    ::IrPreSerializationJsSymbolValidationHandler
+) {
+    override fun TestConfigurationBuilder.applyConfigurators() {
+        useConfigurators(::JsEnvironmentConfigurator)
+    }
+}
