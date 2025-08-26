166cb4c2b57caec261dfcdc3864117bbd4f417ef
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-08-26 10:24:15 +0200

    K2: use visibility instead of classId.isLocal in FirClassLikeDeclaration.isLocal
    #KT-78895 Fixed

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/utils/FirStatusUtils.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/declarations/utils/FirStatusUtils.kt
@@ -60,7 +60,7 @@ inline val FirMemberDeclaration.isFromEnumClass: Boolean get() = status.isFromEn
 inline val FirMemberDeclaration.isFun: Boolean get() = status.isFun
 inline val FirMemberDeclaration.hasStableParameterNames: Boolean get() = status.hasStableParameterNames
 
-inline val FirClassLikeDeclaration.isLocal: Boolean get() = symbol.classId.isLocal
+inline val FirClassLikeDeclaration.isLocal: Boolean get() = visibility == Visibilities.Local
 
 fun FirDeclaration?.isLocalClassOrAnonymousObject(): Boolean = ((this as? FirRegularClass)?.isLocal == true) || this is FirAnonymousObject
 fun FirBasedSymbol<*>?.isLocalClassOrAnonymousObject(): Boolean = this?.fir.isLocalClassOrAnonymousObject()
