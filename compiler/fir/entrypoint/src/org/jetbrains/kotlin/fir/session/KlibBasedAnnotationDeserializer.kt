7b52948edef955dac5ae3e4fdfa25733225c1f90
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2025-08-26 20:10:04 +0200

    [Klib] Support reading common annotations in KLIB compilers
    ^KT-64237

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/KlibBasedAnnotationDeserializer.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/session/KlibBasedAnnotationDeserializer.kt
@@ -5,9 +5,142 @@
 
 package org.jetbrains.kotlin.fir.session
 
+import org.jetbrains.kotlin.config.LanguageFeature
+import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.deserialization.AbstractAnnotationDeserializerWithTypeAnnotations
+import org.jetbrains.kotlin.fir.expressions.FirAnnotation
+import org.jetbrains.kotlin.fir.languageVersionSettings
 import org.jetbrains.kotlin.library.metadata.KlibMetadataSerializerProtocol
+import org.jetbrains.kotlin.metadata.ProtoBuf
+import org.jetbrains.kotlin.metadata.deserialization.Flags
+import org.jetbrains.kotlin.metadata.deserialization.NameResolver
+import org.jetbrains.kotlin.metadata.deserialization.TypeTable
+import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.protobuf.MessageLite
+import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedContainerSource
 
-class KlibBasedAnnotationDeserializer(session: FirSession) :
-    AbstractAnnotationDeserializerWithTypeAnnotations(session, KlibMetadataSerializerProtocol)
+class KlibBasedAnnotationDeserializer(val session: FirSession) :
+    AbstractAnnotationDeserializerWithTypeAnnotations(session, KlibMetadataSerializerProtocol) {
+
+    override fun loadClassAnnotations(classProto: ProtoBuf.Class, nameResolver: NameResolver): List<FirAnnotation> =
+        loadAnnotationsFromMetadata(classProto.flags, classProto.annotationList, nameResolver)
+            ?: super.loadClassAnnotations(classProto, nameResolver)
+
+    override fun loadFunctionAnnotations(
+        containerSource: DeserializedContainerSource?,
+        functionProto: ProtoBuf.Function,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(functionProto.flags, functionProto.annotationList, nameResolver)
+        ?: super.loadFunctionAnnotations(containerSource, functionProto, nameResolver, typeTable)
+
+    override fun loadPropertyAnnotations(
+        containerSource: DeserializedContainerSource?,
+        propertyProto: ProtoBuf.Property,
+        containingClassProto: ProtoBuf.Class?,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(
+        propertyProto.flags,
+        propertyProto.annotationList,
+        nameResolver,
+        AnnotationUseSiteTarget.PROPERTY
+    ) ?: super.loadPropertyAnnotations(containerSource, propertyProto, containingClassProto, nameResolver, typeTable)
+
+    override fun loadConstructorAnnotations(
+        containerSource: DeserializedContainerSource?,
+        constructorProto: ProtoBuf.Constructor,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(constructorProto.flags, constructorProto.annotationList, nameResolver)
+        ?: super.loadConstructorAnnotations(containerSource, constructorProto, nameResolver, typeTable)
+
+    override fun loadValueParameterAnnotations(
+        containerSource: DeserializedContainerSource?,
+        callableProto: MessageLite,
+        valueParameterProto: ProtoBuf.ValueParameter,
+        classProto: ProtoBuf.Class?,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+        kind: CallableKind,
+        parameterIndex: Int,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(valueParameterProto.flags, valueParameterProto.annotationList, nameResolver)
+        ?: super.loadValueParameterAnnotations(
+            containerSource,
+            callableProto,
+            valueParameterProto,
+            classProto,
+            nameResolver,
+            typeTable,
+            kind,
+            parameterIndex
+        )
+
+    override fun loadEnumEntryAnnotations(
+        classId: ClassId,
+        enumEntryProto: ProtoBuf.EnumEntry,
+        nameResolver: NameResolver,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(flags = null, enumEntryProto.annotationList, nameResolver)
+        ?: super.loadEnumEntryAnnotations(classId, enumEntryProto, nameResolver)
+
+    override fun loadPropertyGetterAnnotations(
+        containerSource: DeserializedContainerSource?,
+        propertyProto: ProtoBuf.Property,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+        getterFlags: Int,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(getterFlags, propertyProto.getterAnnotationList, nameResolver)
+        ?: super.loadPropertyGetterAnnotations(containerSource, propertyProto, nameResolver, typeTable, getterFlags)
+
+    override fun loadPropertySetterAnnotations(
+        containerSource: DeserializedContainerSource?,
+        propertyProto: ProtoBuf.Property,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+        setterFlags: Int,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(setterFlags, propertyProto.setterAnnotationList, nameResolver)
+        ?: super.loadPropertySetterAnnotations(containerSource, propertyProto, nameResolver, typeTable, setterFlags)
+
+    override fun loadPropertyBackingFieldAnnotations(
+        containerSource: DeserializedContainerSource?,
+        propertyProto: ProtoBuf.Property,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(
+        propertyProto.flags, propertyProto.backingFieldAnnotationList, nameResolver, AnnotationUseSiteTarget.FIELD
+    ) ?: super.loadPropertyBackingFieldAnnotations(containerSource, propertyProto, nameResolver, typeTable)
+
+    override fun loadPropertyDelegatedFieldAnnotations(
+        containerSource: DeserializedContainerSource?,
+        propertyProto: ProtoBuf.Property,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+    ): List<FirAnnotation> = loadAnnotationsFromMetadata(
+        propertyProto.flags, propertyProto.delegateFieldAnnotationList, nameResolver, AnnotationUseSiteTarget.PROPERTY_DELEGATE_FIELD
+    ) ?: super.loadPropertyDelegatedFieldAnnotations(containerSource, propertyProto, nameResolver, typeTable)
+
+    override fun loadExtensionReceiverParameterAnnotations(
+        containerSource: DeserializedContainerSource?,
+        callableProto: MessageLite,
+        nameResolver: NameResolver,
+        typeTable: TypeTable,
+        kind: CallableKind,
+    ): List<FirAnnotation> = when (callableProto) {
+        is ProtoBuf.Function -> loadAnnotationsFromMetadata(flags = null, callableProto.extensionReceiverAnnotationList, nameResolver)
+        is ProtoBuf.Property -> loadAnnotationsFromMetadata(flags = null, callableProto.extensionReceiverAnnotationList, nameResolver)
+        else -> null
+    } ?: super.loadExtensionReceiverParameterAnnotations(containerSource, callableProto, nameResolver, typeTable, kind)
+
+    private fun loadAnnotationsFromMetadata(
+        flags: Int?,
+        annotations: List<ProtoBuf.Annotation>,
+        nameResolver: NameResolver,
+        useSiteTarget: AnnotationUseSiteTarget? = null,
+    ): List<FirAnnotation>? = when {
+        flags != null && !Flags.HAS_ANNOTATIONS.get(flags) -> emptyList()
+        session.languageVersionSettings.supportsFeature(LanguageFeature.KlibAnnotationsInCommonMetadata) && annotations.isNotEmpty() ->
+            annotations.map { deserializeAnnotation(it, nameResolver, useSiteTarget) }
+        else -> null
+    }
+}
