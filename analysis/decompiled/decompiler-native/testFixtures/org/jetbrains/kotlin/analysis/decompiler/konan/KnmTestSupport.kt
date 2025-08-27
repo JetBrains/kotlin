2e03db7950409ad8ed3e52d3fbba46bd8f1fdeba
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-27 17:43:44 +0200

    [decompiler] rename `KotlinNativeMetadataDecompiler` -> `K1KotlinNativeMetadataDecompiler`
    ^KT-80350

--- analysis/decompiled/decompiler-native/testFixtures/org/jetbrains/kotlin/analysis/decompiler/konan/KnmTestSupport.kt
+++ analysis/decompiled/decompiler-native/testFixtures/org/jetbrains/kotlin/analysis/decompiler/konan/KnmTestSupport.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -35,7 +35,7 @@ object Fe10KnmTestSupport : KnmTestSupport {
         get() = Directives.KNM_FE10_IGNORE
 
     override fun createDecompiler(): KlibMetadataDecompiler<*> {
-        return KotlinNativeMetadataDecompiler()
+        return K1KotlinNativeMetadataDecompiler()
     }
 
     override fun compileCommonMetadata(
