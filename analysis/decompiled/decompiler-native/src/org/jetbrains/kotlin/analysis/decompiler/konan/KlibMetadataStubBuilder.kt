6de3df9db74e1a34eefaf496eacd3fe33970d391
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-27 18:44:52 +0200

    [decompiler] merge K2KlibMetadataStubBuilder to KlibMetadataStubBuilder
    There is no need to have K2 prefix
    
    ^KT-80350

--- analysis/decompiled/decompiler-native/src/org/jetbrains/kotlin/analysis/decompiler/konan/KlibMetadataStubBuilder.kt
+++ analysis/decompiled/decompiler-native/src/org/jetbrains/kotlin/analysis/decompiler/konan/KlibMetadataStubBuilder.kt
@@ -8,20 +8,24 @@ import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.compiled.ClsStubBuilder
-import com.intellij.psi.impl.compiled.ClassFileStubBuilder
 import com.intellij.psi.stubs.PsiFileStub
 import com.intellij.util.indexing.FileContent
 import org.jetbrains.kotlin.analysis.decompiler.psi.text.createIncompatibleMetadataVersionDecompiledText
-import org.jetbrains.kotlin.analysis.decompiler.stub.createIncompatibleAbiVersionFileStub
+import org.jetbrains.kotlin.analysis.decompiler.stub.*
+import org.jetbrains.kotlin.analysis.decompiler.stub.file.AnnotationLoaderForStubBuilderImpl
+import org.jetbrains.kotlin.metadata.deserialization.TypeTable
+import org.jetbrains.kotlin.serialization.SerializerExtensionProtocol
+import org.jetbrains.kotlin.serialization.deserialization.ProtoBasedClassDataFinder
+import org.jetbrains.kotlin.serialization.deserialization.ProtoContainer.Package
+import org.jetbrains.kotlin.serialization.deserialization.getClassId
 
-abstract class KlibMetadataStubBuilder(
+class KlibMetadataStubBuilder(
     private val version: Int,
     private val fileType: FileType,
-    protected val readFile: (VirtualFile) -> FileWithMetadata?
+    private val serializerProtocol: () -> SerializerExtensionProtocol,
+    private val readFile: (VirtualFile) -> FileWithMetadata?,
 ) : ClsStubBuilder() {
-    override fun getStubVersion() = ClassFileStubBuilder.STUB_VERSION + version
-
-    protected abstract fun buildMetadataFileStub(fileWithMetadata: FileWithMetadata.Compatible, fileContent: FileContent): PsiFileStub<*>
+    override fun getStubVersion(): Int = version
 
     override fun buildFileStub(content: FileContent): PsiFileStub<*>? {
         val virtualFile = content.file
@@ -34,7 +38,40 @@ abstract class KlibMetadataStubBuilder(
                 createIncompatibleMetadataVersionDecompiledText(fileWithMetadata.version)
             )
 
-            is FileWithMetadata.Compatible -> buildMetadataFileStub(fileWithMetadata, content)
+            is FileWithMetadata.Compatible -> {
+                val packageProto = fileWithMetadata.proto.`package`
+                val packageFqName = fileWithMetadata.packageFqName
+                val nameResolver = fileWithMetadata.nameResolver
+                val mainClassDataFinder = ProtoBasedClassDataFinder(fileWithMetadata.proto, nameResolver, fileWithMetadata.version)
+                val protocol = serializerProtocol()
+                val components = ClsStubBuilderComponents(
+                    classDataFinder = NearFileClassDataFinder.wrapIfNeeded(mainClassDataFinder, content.file, readFile),
+                    annotationLoader = AnnotationLoaderForStubBuilderImpl(protocol),
+                    virtualFileForDebug = content.file,
+                    serializationProtocol = protocol,
+                )
+
+                val context = components.createContext(nameResolver, packageFqName, TypeTable(packageProto.typeTable))
+                val fileStub = createFileStub(packageFqName, isScript = false)
+                createPackageDeclarationsStubs(
+                    parentStub = fileStub, outerContext = context,
+                    protoContainer = Package(packageFqName, context.nameResolver, context.typeTable, source = null),
+                    packageProto = packageProto,
+                )
+
+                for (classProto in fileWithMetadata.classesToDecompile) {
+                    createClassStub(
+                        parent = fileStub,
+                        classProto = classProto,
+                        nameResolver = nameResolver,
+                        classId = nameResolver.getClassId(classProto.fqName),
+                        source = null,
+                        context = context,
+                    )
+                }
+
+                fileStub
+            }
         }
     }
 }
