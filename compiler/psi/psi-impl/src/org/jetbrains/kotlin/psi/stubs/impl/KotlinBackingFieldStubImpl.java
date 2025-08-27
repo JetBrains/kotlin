fidelity-contributions/jetbrains-kotlin

[stubs] introduce KotlinStubElement with copy method
to avoid hacks with serialization and deserialization

^KT-80276 Fixed

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinBackingFieldStubImpl.java
@@ -6,13 +6,17 @@
 package org.jetbrains.kotlin.psi.stubs.impl;
 
 import com.intellij.psi.stubs.StubElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.psi.KtBackingField;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.stubs.KotlinBackingFieldStub;
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
 
 public class KotlinBackingFieldStubImpl extends KotlinStubBaseImpl<KtBackingField>
         implements KotlinBackingFieldStub {
     private final boolean hasInitializer;
+
     public KotlinBackingFieldStubImpl(StubElement<?> parent, boolean hasInitializer) {
         super(parent, KtStubElementTypes.BACKING_FIELD);
         this.hasInitializer = hasInitializer;
@@ -21,5 +25,14 @@ public class KotlinBackingFieldStubImpl extends KotlinStubBaseImpl<KtBackingFiel
     @Override
     public boolean getHasInitializer() {
         return hasInitializer;
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinBackingFieldStubImpl copy(@Nullable StubElement<?> newParent) {
+        return new KotlinBackingFieldStubImpl(
+                newParent,
+                hasInitializer
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinImportAliasStubImpl.kt
@@ -1,20 +1,27 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
+import org.jetbrains.kotlin.psi.KtImplementationDetail
 import org.jetbrains.kotlin.psi.KtImportAlias
 import org.jetbrains.kotlin.psi.stubs.KotlinImportAliasStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
+@OptIn(KtImplementationDetail::class)
 class KotlinImportAliasStubImpl(
-    parent: StubElement<out PsiElement>?,
-    private val name: StringRef?
+    parent: StubElement<*>?,
+    private val name: StringRef?,
 ) : KotlinStubBaseImpl<KtImportAlias>(parent, KtStubElementTypes.IMPORT_ALIAS), KotlinImportAliasStub {
     override fun getName(): String? = StringRef.toString(name)
+
+    @KtImplementationDetail
+    override fun copy(newParent: StubElement<*>?): KotlinImportAliasStubImpl = KotlinImportAliasStubImpl(
+        parent = newParent,
+        name = name
+    )
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinModifierListStubImpl.java
@@ -7,17 +7,19 @@ package org.jetbrains.kotlin.psi.stubs.impl;
 
 import com.intellij.psi.stubs.StubElement;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.lexer.KtModifierKeywordToken;
 import org.jetbrains.kotlin.psi.KtDeclarationModifierList;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.stubs.KotlinModifierListStub;
-import org.jetbrains.kotlin.psi.stubs.elements.KtModifierListElementType;
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
 
 public class KotlinModifierListStubImpl extends KotlinStubBaseImpl<KtDeclarationModifierList> implements KotlinModifierListStub {
 
     private final long mask;
 
-    public KotlinModifierListStubImpl(StubElement parent, long mask, @NotNull KtModifierListElementType<?> elementType) {
-        super(parent, elementType);
+    public KotlinModifierListStubImpl(@Nullable StubElement<?> parent, long mask) {
+        super(parent, KtStubElementTypes.MODIFIER_LIST);
         this.mask = mask;
     }
 
@@ -43,5 +45,14 @@ public class KotlinModifierListStubImpl extends KotlinStubBaseImpl<KtDeclaration
 
     public boolean hasAnyModifier() {
         return mask != 0;
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinModifierListStubImpl copy(@Nullable StubElement<?> newParent) {
+        return new KotlinModifierListStubImpl(
+                newParent,
+                mask
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinNameReferenceExpressionStubImpl.java
@@ -1,26 +1,15 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.impl;
 
-import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.psi.stubs.StubElement;
 import com.intellij.util.io.StringRef;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression;
 import org.jetbrains.kotlin.psi.stubs.KotlinNameReferenceExpressionStub;
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
@@ -30,12 +19,6 @@ public class KotlinNameReferenceExpressionStubImpl extends KotlinStubBaseImpl<Kt
     @NotNull
     private final StringRef referencedName;
     private final boolean myClassRef;
-
-    public KotlinNameReferenceExpressionStubImpl(StubElement parent, @NotNull StringRef referencedName) {
-        super(parent, KtStubElementTypes.REFERENCE_EXPRESSION);
-        this.referencedName = referencedName;
-        myClassRef = false;
-    }
 
     public KotlinNameReferenceExpressionStubImpl(
             @Nullable StubElement<?> parent,
@@ -55,5 +38,15 @@ public class KotlinNameReferenceExpressionStubImpl extends KotlinStubBaseImpl<Kt
     @Override
     public String getReferencedName() {
         return referencedName.getString();
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinNameReferenceExpressionStubImpl copy(@Nullable StubElement<?> newParent) {
+        return new KotlinNameReferenceExpressionStubImpl(
+                newParent,
+                referencedName,
+                myClassRef
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinPropertyAccessorStubImpl.java
@@ -6,6 +6,9 @@
 package org.jetbrains.kotlin.psi.stubs.impl;
 
 import com.intellij.psi.stubs.StubElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.KtPropertyAccessor;
 import org.jetbrains.kotlin.psi.stubs.KotlinPropertyAccessorStub;
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
@@ -17,7 +20,7 @@ public class KotlinPropertyAccessorStubImpl extends KotlinStubBaseImpl<KtPropert
     private final boolean mayHaveContract;
 
     public KotlinPropertyAccessorStubImpl(
-            StubElement parent,
+            @Nullable StubElement<?> parent,
             boolean isGetter,
             boolean hasBody,
             boolean hasNoExpressionBody,
@@ -48,5 +51,17 @@ public class KotlinPropertyAccessorStubImpl extends KotlinStubBaseImpl<KtPropert
     @Override
     public boolean getMayHaveContract() {
         return mayHaveContract;
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinPropertyAccessorStubImpl copy(@Nullable StubElement<?> newParent) {
+        return new KotlinPropertyAccessorStubImpl(
+                newParent,
+                isGetter,
+                hasBody,
+                hasNoExpressionBody,
+                mayHaveContract
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinPropertyStubImpl.kt
@@ -5,17 +5,18 @@
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
 import org.jetbrains.kotlin.constant.ConstantValue
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtImplementationDetail
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.stubs.KotlinPropertyStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
+@OptIn(KtImplementationDetail::class)
 class KotlinPropertyStubImpl(
-    parent: StubElement<out PsiElement>?,
+    parent: StubElement<*>?,
     private val name: StringRef?,
     override val isVar: Boolean,
     override val isTopLevel: Boolean,
@@ -39,5 +40,22 @@ class KotlinPropertyStubImpl(
         }
     }
 
-    override fun getName() = StringRef.toString(name)
+    override fun getName(): String? = StringRef.toString(name)
+
+    @KtImplementationDetail
+    override fun copy(newParent: StubElement<*>?): KotlinPropertyStubImpl = KotlinPropertyStubImpl(
+        parent = newParent,
+        name = name,
+        isVar = isVar,
+        isTopLevel = isTopLevel,
+        hasDelegate = hasDelegate,
+        hasDelegateExpression = hasDelegateExpression,
+        hasInitializer = hasInitializer,
+        isExtension = isExtension,
+        hasReturnTypeRef = hasReturnTypeRef,
+        fqName = fqName,
+        constantInitializer = constantInitializer,
+        origin = origin,
+        hasBackingField = hasBackingField,
+    )
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinTypeParameterStubImpl.kt
@@ -5,19 +5,27 @@
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtImplementationDetail
 import org.jetbrains.kotlin.psi.KtTypeParameter
 import org.jetbrains.kotlin.psi.stubs.KotlinTypeParameterStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
+@OptIn(KtImplementationDetail::class)
 class KotlinTypeParameterStubImpl(
-    parent: StubElement<out PsiElement>?,
+    parent: StubElement<*>?,
     private val name: StringRef?,
 ) : KotlinStubBaseImpl<KtTypeParameter>(parent, KtStubElementTypes.TYPE_PARAMETER), KotlinTypeParameterStub {
-    override fun getName() = StringRef.toString(name)
+    override fun getName(): String? = StringRef.toString(name)
+
     // type parameters don't have FqNames
     override val fqName: FqName? get() = null
+
+    @KtImplementationDetail
+    override fun copy(newParent: StubElement<*>?): KotlinTypeParameterStubImpl = KotlinTypeParameterStubImpl(
+        parent = newParent,
+        name = name,
+    )
 }

JetBrains/kotlin

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinBackingFieldStubImpl.java
@@ -6,13 +6,17 @@
 package org.jetbrains.kotlin.psi.stubs.impl;
 
 import com.intellij.psi.stubs.StubElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.psi.KtBackingField;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.stubs.KotlinBackingFieldStub;
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
 
 public class KotlinBackingFieldStubImpl extends KotlinStubBaseImpl<KtBackingField>
         implements KotlinBackingFieldStub {
     private final boolean hasInitializer;
+
     public KotlinBackingFieldStubImpl(StubElement<?> parent, boolean hasInitializer) {
         super(parent, KtStubElementTypes.BACKING_FIELD);
         this.hasInitializer = hasInitializer;
@@ -21,5 +25,14 @@ public class KotlinBackingFieldStubImpl extends KotlinStubBaseImpl<KtBackingFiel
     @Override
     public boolean getHasInitializer() {
         return hasInitializer;
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinBackingFieldStubImpl copyInto(@Nullable StubElement<?> newParent) {
+        return new KotlinBackingFieldStubImpl(
+                newParent,
+                hasInitializer
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinImportAliasStubImpl.kt
@@ -1,20 +1,27 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
+import org.jetbrains.kotlin.psi.KtImplementationDetail
 import org.jetbrains.kotlin.psi.KtImportAlias
 import org.jetbrains.kotlin.psi.stubs.KotlinImportAliasStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
+@OptIn(KtImplementationDetail::class)
 class KotlinImportAliasStubImpl(
-    parent: StubElement<out PsiElement>?,
-    private val name: StringRef?
+    parent: StubElement<*>?,
+    private val name: StringRef?,
 ) : KotlinStubBaseImpl<KtImportAlias>(parent, KtStubElementTypes.IMPORT_ALIAS), KotlinImportAliasStub {
     override fun getName(): String? = StringRef.toString(name)
+
+    @KtImplementationDetail
+    override fun copyInto(newParent: StubElement<*>?): KotlinImportAliasStubImpl = KotlinImportAliasStubImpl(
+        parent = newParent,
+        name = name
+    )
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinModifierListStubImpl.java
@@ -7,17 +7,19 @@ package org.jetbrains.kotlin.psi.stubs.impl;
 
 import com.intellij.psi.stubs.StubElement;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.kotlin.lexer.KtModifierKeywordToken;
 import org.jetbrains.kotlin.psi.KtDeclarationModifierList;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.stubs.KotlinModifierListStub;
-import org.jetbrains.kotlin.psi.stubs.elements.KtModifierListElementType;
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
 
 public class KotlinModifierListStubImpl extends KotlinStubBaseImpl<KtDeclarationModifierList> implements KotlinModifierListStub {
 
     private final long mask;
 
-    public KotlinModifierListStubImpl(StubElement parent, long mask, @NotNull KtModifierListElementType<?> elementType) {
-        super(parent, elementType);
+    public KotlinModifierListStubImpl(@Nullable StubElement<?> parent, long mask) {
+        super(parent, KtStubElementTypes.MODIFIER_LIST);
         this.mask = mask;
     }
 
@@ -43,5 +45,14 @@ public class KotlinModifierListStubImpl extends KotlinStubBaseImpl<KtDeclaration
 
     public boolean hasAnyModifier() {
         return mask != 0;
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinModifierListStubImpl copyInto(@Nullable StubElement<?> newParent) {
+        return new KotlinModifierListStubImpl(
+                newParent,
+                mask
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinNameReferenceExpressionStubImpl.java
@@ -1,26 +1,15 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi.stubs.impl;
 
-import com.intellij.psi.stubs.IStubElementType;
 import com.intellij.psi.stubs.StubElement;
 import com.intellij.util.io.StringRef;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression;
 import org.jetbrains.kotlin.psi.stubs.KotlinNameReferenceExpressionStub;
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
@@ -30,12 +19,6 @@ public class KotlinNameReferenceExpressionStubImpl extends KotlinStubBaseImpl<Kt
     @NotNull
     private final StringRef referencedName;
     private final boolean myClassRef;
-
-    public KotlinNameReferenceExpressionStubImpl(StubElement parent, @NotNull StringRef referencedName) {
-        super(parent, KtStubElementTypes.REFERENCE_EXPRESSION);
-        this.referencedName = referencedName;
-        myClassRef = false;
-    }
 
     public KotlinNameReferenceExpressionStubImpl(
             @Nullable StubElement<?> parent,
@@ -55,5 +38,15 @@ public class KotlinNameReferenceExpressionStubImpl extends KotlinStubBaseImpl<Kt
     @Override
     public String getReferencedName() {
         return referencedName.getString();
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinNameReferenceExpressionStubImpl copyInto(@Nullable StubElement<?> newParent) {
+        return new KotlinNameReferenceExpressionStubImpl(
+                newParent,
+                referencedName,
+                myClassRef
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinPropertyAccessorStubImpl.java
@@ -6,6 +6,9 @@
 package org.jetbrains.kotlin.psi.stubs.impl;
 
 import com.intellij.psi.stubs.StubElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.kotlin.psi.KtImplementationDetail;
 import org.jetbrains.kotlin.psi.KtPropertyAccessor;
 import org.jetbrains.kotlin.psi.stubs.KotlinPropertyAccessorStub;
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes;
@@ -17,7 +20,7 @@ public class KotlinPropertyAccessorStubImpl extends KotlinStubBaseImpl<KtPropert
     private final boolean mayHaveContract;
 
     public KotlinPropertyAccessorStubImpl(
-            StubElement parent,
+            @Nullable StubElement<?> parent,
             boolean isGetter,
             boolean hasBody,
             boolean hasNoExpressionBody,
@@ -48,5 +51,17 @@ public class KotlinPropertyAccessorStubImpl extends KotlinStubBaseImpl<KtPropert
     @Override
     public boolean getMayHaveContract() {
         return mayHaveContract;
+    }
+
+    @Override
+    @KtImplementationDetail
+    public @NotNull KotlinPropertyAccessorStubImpl copyInto(@Nullable StubElement<?> newParent) {
+        return new KotlinPropertyAccessorStubImpl(
+                newParent,
+                isGetter,
+                hasBody,
+                hasNoExpressionBody,
+                mayHaveContract
+        );
     }
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinPropertyStubImpl.kt
@@ -5,17 +5,18 @@
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
 import org.jetbrains.kotlin.constant.ConstantValue
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtImplementationDetail
 import org.jetbrains.kotlin.psi.KtProperty
 import org.jetbrains.kotlin.psi.stubs.KotlinPropertyStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
+@OptIn(KtImplementationDetail::class)
 class KotlinPropertyStubImpl(
-    parent: StubElement<out PsiElement>?,
+    parent: StubElement<*>?,
     private val name: StringRef?,
     override val isVar: Boolean,
     override val isTopLevel: Boolean,
@@ -39,5 +40,22 @@ class KotlinPropertyStubImpl(
         }
     }
 
-    override fun getName() = StringRef.toString(name)
+    override fun getName(): String? = StringRef.toString(name)
+
+    @KtImplementationDetail
+    override fun copyInto(newParent: StubElement<*>?): KotlinPropertyStubImpl = KotlinPropertyStubImpl(
+        parent = newParent,
+        name = name,
+        isVar = isVar,
+        isTopLevel = isTopLevel,
+        hasDelegate = hasDelegate,
+        hasDelegateExpression = hasDelegateExpression,
+        hasInitializer = hasInitializer,
+        isExtension = isExtension,
+        hasReturnTypeRef = hasReturnTypeRef,
+        fqName = fqName,
+        constantInitializer = constantInitializer,
+        origin = origin,
+        hasBackingField = hasBackingField,
+    )
 }

compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/impl/KotlinTypeParameterStubImpl.kt
@@ -5,19 +5,27 @@
 
 package org.jetbrains.kotlin.psi.stubs.impl
 
-import com.intellij.psi.PsiElement
 import com.intellij.psi.stubs.StubElement
 import com.intellij.util.io.StringRef
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtImplementationDetail
 import org.jetbrains.kotlin.psi.KtTypeParameter
 import org.jetbrains.kotlin.psi.stubs.KotlinTypeParameterStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
+@OptIn(KtImplementationDetail::class)
 class KotlinTypeParameterStubImpl(
-    parent: StubElement<out PsiElement>?,
+    parent: StubElement<*>?,
     private val name: StringRef?,
 ) : KotlinStubBaseImpl<KtTypeParameter>(parent, KtStubElementTypes.TYPE_PARAMETER), KotlinTypeParameterStub {
-    override fun getName() = StringRef.toString(name)
+    override fun getName(): String? = StringRef.toString(name)
+
     // type parameters don't have FqNames
     override val fqName: FqName? get() = null
+
+    @KtImplementationDetail
+    override fun copyInto(newParent: StubElement<*>?): KotlinTypeParameterStubImpl = KotlinTypeParameterStubImpl(
+        parent = newParent,
+        name = name,
+    )
 }