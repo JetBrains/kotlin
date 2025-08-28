9507e3f2fda88b255f49cafe5d2f172d0cf68889
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-08-28 16:07:07 +0200

    [decompiler] KlibMetadataStubBuilder: drop custom buildFileStub
    ^KT-80350

--- analysis/decompiled/decompiler-native/src/org/jetbrains/kotlin/analysis/decompiler/konan/KlibMetadataStubBuilder.kt
+++ analysis/decompiled/decompiler-native/src/org/jetbrains/kotlin/analysis/decompiler/konan/KlibMetadataStubBuilder.kt
@@ -5,25 +5,16 @@
 package org.jetbrains.kotlin.analysis.decompiler.konan
 
 import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.stubs.PsiFileStub
-import com.intellij.util.indexing.FileContent
-import org.jetbrains.kotlin.analysis.decompiler.psi.text.createIncompatibleMetadataVersionDecompiledText
-import org.jetbrains.kotlin.analysis.decompiler.stub.*
-import org.jetbrains.kotlin.analysis.decompiler.stub.file.AnnotationLoaderForStubBuilderImpl
 import org.jetbrains.kotlin.analysis.decompiler.stub.file.KotlinMetadataStubBuilder
 import org.jetbrains.kotlin.library.metadata.KlibMetadataProtoBuf
 import org.jetbrains.kotlin.library.metadata.KlibMetadataSerializerProtocol
 import org.jetbrains.kotlin.metadata.ProtoBuf
 import org.jetbrains.kotlin.metadata.deserialization.BinaryVersion
 import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
-import org.jetbrains.kotlin.metadata.deserialization.TypeTable
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.stubs.KotlinStubVersions
-import org.jetbrains.kotlin.serialization.deserialization.ProtoBasedClassDataFinder
-import org.jetbrains.kotlin.serialization.deserialization.ProtoContainer.Package
-import org.jetbrains.kotlin.serialization.deserialization.getClassId
+import org.jetbrains.kotlin.serialization.deserialization.ClassDataFinder
 
 internal object KlibMetadataStubBuilder : KotlinMetadataStubBuilder() {
     override fun getStubVersion(): Int = KotlinStubVersions.KLIB_STUB_VERSION
@@ -44,52 +35,8 @@ internal object KlibMetadataStubBuilder : KotlinMetadataStubBuilder() {
         return KlibFileWithMetadata(fragment, version)
     }
 
-    override fun buildFileStub(content: FileContent): PsiFileStub<*>? {
-        val virtualFile = content.file
-        assert(FileTypeRegistry.getInstance().isFileOfType(virtualFile, fileType)) { "Unexpected file type ${virtualFile.fileType}" }
-
-        val fileWithMetadata = readFileSafely(virtualFile) ?: return null
-
-        return when (fileWithMetadata) {
-            is FileWithMetadata.Incompatible -> createIncompatibleAbiVersionFileStub(
-                createIncompatibleMetadataVersionDecompiledText(fileWithMetadata.version)
-            )
-
-            is FileWithMetadata.Compatible -> {
-                val packageProto = fileWithMetadata.proto.`package`
-                val packageFqName = fileWithMetadata.packageFqName
-                val nameResolver = fileWithMetadata.nameResolver
-                val mainClassDataFinder = ProtoBasedClassDataFinder(fileWithMetadata.proto, nameResolver, fileWithMetadata.version)
-                val protocol = fileWithMetadata.serializerProtocol
-                val components = ClsStubBuilderComponents(
-                    classDataFinder = NearFileClassDataFinder.wrapIfNeeded(mainClassDataFinder, content.file) { readFileSafely(it) },
-                    annotationLoader = AnnotationLoaderForStubBuilderImpl(protocol),
-                    virtualFileForDebug = content.file,
-                    serializationProtocol = protocol,
-                )
-
-                val context = components.createContext(nameResolver, packageFqName, TypeTable(packageProto.typeTable))
-                val fileStub = createFileStub(packageFqName, isScript = false)
-                createPackageDeclarationsStubs(
-                    parentStub = fileStub, outerContext = context,
-                    protoContainer = Package(packageFqName, context.nameResolver, context.typeTable, source = null),
-                    packageProto = packageProto,
-                )
-
-                for (classProto in fileWithMetadata.classesToDecompile) {
-                    createClassStub(
-                        parent = fileStub,
-                        classProto = classProto,
-                        nameResolver = nameResolver,
-                        classId = nameResolver.getClassId(classProto.fqName),
-                        source = null,
-                        context = context,
-                    )
-                }
-
-                fileStub
-            }
-        }
+    override fun classDataFinder(original: ClassDataFinder, file: VirtualFile): ClassDataFinder {
+        return NearFileClassDataFinder.wrapIfNeeded(original, file) { readFileSafely(it) }
     }
 
     class KlibFileWithMetadata(
