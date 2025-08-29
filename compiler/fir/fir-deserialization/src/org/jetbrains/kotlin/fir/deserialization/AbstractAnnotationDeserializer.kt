bf031f50a760cc54bf376e762b1d2da5d29ed91e
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2025-08-29 15:27:51 +0200

    [Serialization] Add `AnnotationDeserializer` interface

--- compiler/fir/fir-deserialization/src/org/jetbrains/kotlin/fir/deserialization/AbstractAnnotationDeserializer.kt
+++ compiler/fir/fir-deserialization/src/org/jetbrains/kotlin/fir/deserialization/AbstractAnnotationDeserializer.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.fir.deserialization
 
 import org.jetbrains.kotlin.descriptors.annotations.AnnotationUseSiteTarget
 import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.deserialization.AnnotationDeserializer.CallableKind
 import org.jetbrains.kotlin.fir.expressions.FirAnnotation
 import org.jetbrains.kotlin.fir.expressions.FirExpression
 import org.jetbrains.kotlin.fir.types.FirTypeRef
@@ -22,29 +23,22 @@ import org.jetbrains.kotlin.serialization.deserialization.descriptors.Deserializ
 abstract class AbstractAnnotationDeserializer(
     private val session: FirSession,
     protected val protocol: SerializerExtensionProtocol
-) {
-    open fun inheritAnnotationInfo(parent: AbstractAnnotationDeserializer) {
+) : AnnotationDeserializer {
+    override fun inheritAnnotationInfo(parent: AnnotationDeserializer) {
     }
 
-    enum class CallableKind {
-        PROPERTY,
-        PROPERTY_GETTER,
-        PROPERTY_SETTER,
-        OTHERS
-    }
-
-    open fun loadClassAnnotations(classProto: ProtoBuf.Class, nameResolver: NameResolver): List<FirAnnotation> {
+    override fun loadClassAnnotations(classProto: ProtoBuf.Class, nameResolver: NameResolver): List<FirAnnotation> {
         if (!Flags.HAS_ANNOTATIONS.get(classProto.flags)) return emptyList()
         val annotations = classProto.getExtension(protocol.classAnnotation).orEmpty()
         return annotations.map { deserializeAnnotation(session, it, nameResolver) }
     }
 
-    fun loadTypeAliasAnnotations(aliasProto: ProtoBuf.TypeAlias, nameResolver: NameResolver): List<FirAnnotation> {
+    override fun loadTypeAliasAnnotations(aliasProto: ProtoBuf.TypeAlias, nameResolver: NameResolver): List<FirAnnotation> {
         if (!Flags.HAS_ANNOTATIONS.get(aliasProto.flags)) return emptyList()
         return aliasProto.annotationList.map { deserializeAnnotation(session, it, nameResolver) }
     }
 
-    open fun loadFunctionAnnotations(
+    override fun loadFunctionAnnotations(
         containerSource: DeserializedContainerSource?,
         functionProto: ProtoBuf.Function,
         nameResolver: NameResolver,
@@ -55,7 +49,7 @@ abstract class AbstractAnnotationDeserializer(
         return annotations.map { deserializeAnnotation(session, it, nameResolver) }
     }
 
-    open fun loadPropertyAnnotations(
+    override fun loadPropertyAnnotations(
         containerSource: DeserializedContainerSource?,
         propertyProto: ProtoBuf.Property,
         containingClassProto: ProtoBuf.Class?,
@@ -68,7 +62,7 @@ abstract class AbstractAnnotationDeserializer(
         )
     }
 
-    open fun loadPropertyBackingFieldAnnotations(
+    override fun loadPropertyBackingFieldAnnotations(
         containerSource: DeserializedContainerSource?,
         propertyProto: ProtoBuf.Property,
         nameResolver: NameResolver,
@@ -80,7 +74,7 @@ abstract class AbstractAnnotationDeserializer(
         )
     }
 
-    open fun loadPropertyDelegatedFieldAnnotations(
+    override fun loadPropertyDelegatedFieldAnnotations(
         containerSource: DeserializedContainerSource?,
         propertyProto: ProtoBuf.Property,
         nameResolver: NameResolver,
@@ -92,7 +86,7 @@ abstract class AbstractAnnotationDeserializer(
         )
     }
 
-    open fun loadPropertyGetterAnnotations(
+    override fun loadPropertyGetterAnnotations(
         containerSource: DeserializedContainerSource?,
         propertyProto: ProtoBuf.Property,
         nameResolver: NameResolver,
@@ -105,7 +99,7 @@ abstract class AbstractAnnotationDeserializer(
         )
     }
 
-    open fun loadPropertySetterAnnotations(
+    override fun loadPropertySetterAnnotations(
         containerSource: DeserializedContainerSource?,
         propertyProto: ProtoBuf.Property,
         nameResolver: NameResolver,
@@ -118,7 +112,7 @@ abstract class AbstractAnnotationDeserializer(
         )
     }
 
-    open fun loadConstructorAnnotations(
+    override fun loadConstructorAnnotations(
         containerSource: DeserializedContainerSource?,
         constructorProto: ProtoBuf.Constructor,
         nameResolver: NameResolver,
@@ -127,7 +121,7 @@ abstract class AbstractAnnotationDeserializer(
         return constructorProto.loadAnnotations(session, protocol.constructorAnnotation, constructorProto.flags, nameResolver)
     }
 
-    open fun loadValueParameterAnnotations(
+    override fun loadValueParameterAnnotations(
         containerSource: DeserializedContainerSource?,
         callableProto: MessageLite,
         valueParameterProto: ProtoBuf.ValueParameter,
@@ -140,7 +134,7 @@ abstract class AbstractAnnotationDeserializer(
         return valueParameterProto.loadAnnotations(session, protocol.parameterAnnotation, valueParameterProto.flags, nameResolver)
     }
 
-    open fun loadEnumEntryAnnotations(
+    override fun loadEnumEntryAnnotations(
         classId: ClassId,
         enumEntryProto: ProtoBuf.EnumEntry,
         nameResolver: NameResolver,
@@ -148,7 +142,7 @@ abstract class AbstractAnnotationDeserializer(
         return enumEntryProto.loadAnnotations(session, protocol.enumEntryAnnotation, -1, nameResolver)
     }
 
-    open fun loadExtensionReceiverParameterAnnotations(
+    override fun loadExtensionReceiverParameterAnnotations(
         containerSource: DeserializedContainerSource?,
         callableProto: MessageLite,
         nameResolver: NameResolver,
@@ -166,7 +160,7 @@ abstract class AbstractAnnotationDeserializer(
         }
     }
 
-    open fun loadAnnotationPropertyDefaultValue(
+    override fun loadAnnotationPropertyDefaultValue(
         containerSource: DeserializedContainerSource?,
         propertyProto: ProtoBuf.Property,
         expectedPropertyType: FirTypeRef,
@@ -176,8 +170,6 @@ abstract class AbstractAnnotationDeserializer(
         return null
     }
 
-    abstract fun loadTypeAnnotations(typeProto: ProtoBuf.Type, nameResolver: NameResolver): List<FirAnnotation>
-
-    open fun loadTypeParameterAnnotations(typeParameterProto: ProtoBuf.TypeParameter, nameResolver: NameResolver): List<FirAnnotation> =
+    override fun loadTypeParameterAnnotations(typeParameterProto: ProtoBuf.TypeParameter, nameResolver: NameResolver): List<FirAnnotation> =
         emptyList<FirAnnotation>()
 }
