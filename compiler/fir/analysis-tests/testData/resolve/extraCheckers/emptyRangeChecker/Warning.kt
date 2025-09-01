67d102e2cb207ba21916564ebb9c052a54f14959
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-09-01 11:00:38 +0200

    K2: support non-integer types in EmptyRangeChecker
    #KT-80538 Fixed

--- compiler/fir/analysis-tests/testData/resolve/extraCheckers/emptyRangeChecker/Warning.kt
+++ compiler/fir/analysis-tests/testData/resolve/extraCheckers/emptyRangeChecker/Warning.kt
@@ -8,6 +8,12 @@ fun foo() {
 
     val v = 1
     if (v in <!EMPTY_RANGE!>10..1<!>) { }
+
+    if (' ' in <!EMPTY_RANGE!>'b'..'a'<!>) { }
+
+    if (0.0F in <!EMPTY_RANGE!>0.02F..0.01F<!>) { }
+
+    if (0.0 in <!EMPTY_RANGE!>0.02..0.01<!>) { }
 }
 
 fun backward() {
@@ -17,6 +23,8 @@ fun backward() {
 
     val v = 1
     if (v in <!EMPTY_RANGE!>0 downTo 6<!>) { }
+
+    if (' ' in <!EMPTY_RANGE!>'a' downTo 'b'<!>) { }
 }
 
 fun until() {
@@ -26,12 +34,20 @@ fun until() {
 
     val v = 1
     if (v in <!EMPTY_RANGE!>-5 until -5<!>) { }
+
+    if (' ' in <!EMPTY_RANGE!>'7' until '7'<!>) { }
 }
 
 fun rangeUntil() {
     for (i in <!EMPTY_RANGE!>0..<-1<!>) {}
 
     for (i in <!EMPTY_RANGE!>2..<2<!>) {}
+
+    if (' ' in <!EMPTY_RANGE!>'7' ..< '7'<!>) { }
+
+    if (0.0F in <!EMPTY_RANGE!>0.0F ..< 0.0F<!>) { }
+
+    if (0.0 in <!EMPTY_RANGE!>0.0 ..< 0.0<!>) { }
 }
 
 /* GENERATED_FIR_TAGS: forLoop, functionDeclaration, ifExpression, integerLiteral, localProperty, propertyDeclaration,
