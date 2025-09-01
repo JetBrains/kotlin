9bccf16dd0c08ac358629e26e0f4b3d0975ddf0a
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-09-01 16:52:18 +0200

    DO_NOT_MERGE: Make cross-module IR inliner enabled by default

--- core/language.version-settings/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ core/language.version-settings/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -505,6 +505,7 @@ enum class LanguageFeature(
     DiscriminateNothingAsNullabilityConstraintInInference(KOTLIN_2_4, "KT-81948"),
     ResolveEqualsRhsInDependentContextWithCompletion(KOTLIN_2_4, "KT-81763"),
     IrIntraModuleInlinerBeforeKlibSerialization(KOTLIN_2_4, sinceApiVersion = ApiVersion.KOTLIN_2_3, issue = "KT-79717"),
+    IrCrossModuleInlinerBeforeKlibSerialization(KOTLIN_2_4, sinceApiVersion = ApiVersion.KOTLIN_2_3, issue = "KT-79717"),
     ImprovedExhaustivenessCheckForSubjectVariable24(KOTLIN_2_4, issue = "KT-83903"),
     DontMakeExplicitNullableJavaTypeArgumentsFlexible(KOTLIN_2_4, "KTLC-284"),
     ExplicitBackingFields(sinceVersion = KOTLIN_2_4, issue = "KT-14663"),
@@ -650,7 +651,6 @@ enum class LanguageFeature(
         }
     },
     ProperFieldAccessGenerationForFieldAccessShadowedByKotlinProperty(sinceVersion = null, "KT-56386"),
-    IrCrossModuleInlinerBeforeKlibSerialization(sinceVersion = null, sinceApiVersion = ApiVersion.KOTLIN_2_3, forcesPreReleaseBinaries = true, issue = "KT-79717"),
     UnnamedLocalVariables(sinceVersion = null, forcesPreReleaseBinaries = false, issue = "KT-74809", enabledInLatestLVTests = true),
     ContextSensitiveResolutionUsingExpectedType(sinceVersion = null, issue = "KT-16768", enabledInLatestLVTests = true),
     DisableWarningsForValueBasedJavaClasses(sinceVersion = null, "KT-70722"),
