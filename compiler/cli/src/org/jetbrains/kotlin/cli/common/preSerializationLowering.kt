9c6875fb022a9421c1a0178eb77c59f5de181ff1
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-09-02 00:02:46 +0200

    [IR] Stop pre-serialization after errors
    Should diagnosticReporter get an error from a pre-serialization lowering, next lowerings are skipped.
    PhaseEngine<T>.runPreSerializationLoweringPhases() is the only place
    to execute pre-serialization IR lowerings in all pipelines, so it's enough to insert such a logic there.
    
    ^KT-78392 Fixed

--- compiler/cli/src/org/jetbrains/kotlin/cli/common/preSerializationLowering.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/common/preSerializationLowering.kt
@@ -8,21 +8,26 @@ package org.jetbrains.kotlin.cli.common
 import org.jetbrains.kotlin.backend.common.PreSerializationLoweringContext
 import org.jetbrains.kotlin.backend.common.phaser.PhaseEngine
 import org.jetbrains.kotlin.config.phaser.NamedCompilerPhase
+import org.jetbrains.kotlin.diagnostics.impl.BaseDiagnosticsCollector
 import org.jetbrains.kotlin.fir.pipeline.Fir2IrActualizedResult
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.util.PhaseType
 import org.jetbrains.kotlin.util.tryMeasureDynamicPhaseTime
+import org.jetbrains.kotlin.utils.addToStdlib.applyIf
 
 fun <T : PreSerializationLoweringContext> PhaseEngine<T>.runPreSerializationLoweringPhases(
     lowerings: List<NamedCompilerPhase<T, IrModuleFragment, IrModuleFragment>>,
     irModuleFragment: IrModuleFragment,
 ): IrModuleFragment {
+    val diagnosticReporter = context.diagnosticReporter as? BaseDiagnosticsCollector
     return lowerings.fold(irModuleFragment) { module, lowering ->
         context.configuration.perfManager.tryMeasureDynamicPhaseTime(lowering.name, PhaseType.IrPreLowering) {
-            runPhase(
-                lowering,
-                module,
-            )
+            module.applyIf(diagnosticReporter?.hasErrors != true) {
+                runPhase(
+                    lowering,
+                    module,
+                )
+            }
         }
     }
 }
