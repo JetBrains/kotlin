08e3b0d0144a9899541487882c883aa551f2666b
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-09-02 09:47:40 +0200

    improve testShared

--- compiler/testData/codegen/boxWasmJsInterop/testShared.kt
+++ compiler/testData/codegen/boxWasmJsInterop/testShared.kt
@@ -1,15 +1,23 @@
 // TARGET_BACKEND: WASM
 // USE_SHARED_OBJECTS
-// DISABLE_WASM_EXCEPTION_HANDLING
+// NODISABLE_WASM_EXCEPTION_HANDLING
 
 // FILE: shared.kt
 
-class MyClass(val a: Int, val b: Int) : Function2<Int, Int, Int> {
-    override fun invoke(p1: Int, p2: Int): Int = p1 - a + p2 * b
+abstract class MyClassBase {
+    abstract fun foo(): Int
 }
 
-var a = MyClass(1, 2)
+class MyClass(val a: Int, val b: Int) : Function2<Int, Int, Int>, MyClassBase() {
+    override fun invoke(p1: Int, p2: Int): Int = p1 - a + p2 * b
+
+    override fun foo() = 5
+}
+
+var myObj = MyClass(1, 2)
 
 fun box(): String {
-    return if (a.a == 1 && MyClass(1, 2).invoke(3, 4) == 10) "OK" else "Fail"
+    val myObjAsBase: MyClassBase = myObj
+    val myObjAsFunc: Function2<Int, Int, Int> = myObj
+    return if (myObj.a == 1 && myObjAsFunc.invoke(3, 4) == 10 && myObjAsBase.foo() == 5) "OK" else "Fail"
 }
