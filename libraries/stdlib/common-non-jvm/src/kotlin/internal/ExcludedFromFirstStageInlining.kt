5bbd134d23c5300517280e7211a82ca914b58b32
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-09-02 15:27:53 +0200

    [stdlib] Introduce a new annotation `ExcludedFromFirstStageInlining`
    ^KT-80565

---
+++ libraries/stdlib/common-non-jvm/src/kotlin/internal/ExcludedFromFirstStageInlining.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.internal
+
+/**
+ * Specifies that the annotated declaration must be excluded from the inlining performed before klib serialization.
+ */
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.SOURCE)
+@SinceKotlin("2.3")
+internal annotation class ExcludedFromFirstStageInlining
