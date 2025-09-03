1f3d974a384a794ffaf4e3f1ee3eac8b6d4d392b
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-09-04 00:39:20 +0200

    [Tests][IC] Adjust testdata for +IrIntraModuleInlinerBeforeKlibSerialization
    ^KT-80692

--- js/js.tests/testFixtures/org/jetbrains/kotlin/incremental/JsAbstractInvalidationTest.kt
+++ js/js.tests/testFixtures/org/jetbrains/kotlin/incremental/JsAbstractInvalidationTest.kt
@@ -25,6 +25,7 @@ import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.CompilationOutputs
 import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.dtsExtension
 import org.jetbrains.kotlin.ir.backend.js.transformers.irToJs.jsExtension
 import org.jetbrains.kotlin.js.config.JSConfigurationKeys
+import org.jetbrains.kotlin.js.engine.ScriptExecutionException
 import org.jetbrains.kotlin.js.testOld.V8JsTestChecker
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.serialization.js.ModuleKind
@@ -200,6 +201,8 @@ abstract class JsAbstractInvalidationTest(
                 throw ComparisonFailure("Mismatched box out at step $stepId", e.expected, e.actual)
             } catch (e: IllegalStateException) {
                 throw IllegalStateException("Something goes wrong (bad JS code?) at step $stepId\n${e.message}")
+            } catch (e: ScriptExecutionException) {
+                throw IllegalStateException("Something goes wrong (bad JS script?) at step $stepId\n${e.message}")
             }
         }
 
