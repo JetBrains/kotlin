4c96c9a0de5dab232c107b995cdf3f649a3243d1
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2025-09-03 10:01:45 +0300

    [Native] Disable Swift export coroutines test for macos_x64
    At the moment, our test infra allows only macos_arm64 target due
    to metadata verification.

--- native/swift/swift-export-standalone-integration-tests/coroutines/testData/execution/coroutines/coroutines.kt
+++ native/swift/swift-export-standalone-integration-tests/coroutines/testData/execution/coroutines/coroutines.kt
@@ -1,6 +1,7 @@
 // IGNORE_NATIVE: targetFamily=IOS
 // IGNORE_NATIVE: targetFamily=TVOS
 // IGNORE_NATIVE: targetFamily=WATCHOS
+// IGNORE_NATIVE: target=macos_x64
 // KIND: STANDALONE
 // MODULE: Main
 // FILE: coroutines.kt
--- native/swift/swift-export-standalone-integration-tests/coroutines/testData/generation/coroutines/coroutines.kt
+++ native/swift/swift-export-standalone-integration-tests/coroutines/testData/generation/coroutines/coroutines.kt
@@ -1,6 +1,7 @@
 // IGNORE_NATIVE: targetFamily=IOS
 // IGNORE_NATIVE: targetFamily=TVOS
 // IGNORE_NATIVE: targetFamily=WATCHOS
+// IGNORE_NATIVE: target=macos_x64
 // KIND: STANDALONE
 // APPLE_ONLY_VALIDATION
 // MODULE: main
