91c3d74aa909a8774506319e963b05d87e57f61a
misosvec <misosvec01@gmail.com>
2025-09-03 17:46:09 +0200

    removed annoying netty logging

--- compiler/daemon/remote-daemon/build.gradle.kts
+++ compiler/daemon/remote-daemon/build.gradle.kts
@@ -81,7 +81,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.9.1")
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.9.1")
     implementation("io.ktor:ktor-server-netty-jvm:3.2.3")
-    implementation("ch.qos.logback:logback-classic:1.5.18")
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.9.1")
     implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.9.1")
 
