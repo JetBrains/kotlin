c2393e32247c60f2a24018719caa4fd46620b0f4
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2025-09-04 11:59:05 +0300

    [Tests] KQA-2443: type erasure in function with one reified type parameter

---
+++ compiler/testData/codegen/box/typeErasure/funWithReifiedTypeParameterContextParameterAndExtensionReceiver.kt
@@ -0,0 +1,13 @@
+// DUMP_IR_OF_PREPROCESSED_INLINE_FUNCTIONS
+// WITH_STDLIB
+// LANGUAGE: +ContextParameters
+import kotlin.reflect.typeOf
+
+context(c: Int)
+inline fun <reified T : CharSequence> Int.foo(a: T) = typeOf<T>()
+
+fun box(): String = with(42) {
+    if (foo("0123456789") != typeOf<String>())
+            return "FAIL"
+    return "OK"
+}
