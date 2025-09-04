39037cbdfd423f567a8a2bed5cc6015d1df53e4b
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-09-04 18:53:34 +0200

    [Swift Export] Factor out getting declarations into the KLIB reader

--- libraries/tools/analysis-api-based-klib-reader/src/org/jetbrains/kotlin/analysis/api/klib/reader/KlibScope.kt
+++ libraries/tools/analysis-api-based-klib-reader/src/org/jetbrains/kotlin/analysis/api/klib/reader/KlibScope.kt
@@ -1,12 +1,11 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.swiftexport.standalone.klib
+package org.jetbrains.kotlin.analysis.api.klib.reader
 
 import org.jetbrains.kotlin.analysis.api.KaSession
-import org.jetbrains.kotlin.analysis.api.klib.reader.*
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
 import org.jetbrains.kotlin.analysis.api.symbols.KaCallableSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaClassifierSymbol
@@ -14,14 +13,14 @@ import org.jetbrains.kotlin.analysis.api.symbols.KaDeclarationSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.markers.KaNamedSymbol
 
 context(ka: KaSession)
-internal fun KaLibraryModule.getAllDeclarations(): Sequence<KaDeclarationSymbol> = sequence {
+public fun KaLibraryModule.getAllDeclarations(): Sequence<KaDeclarationSymbol> = sequence {
     val addresses = addresses.asSequence()
     yieldAll(addresses.getAllCallables())
     yieldAll(addresses.getAllClassifiers())
 }
 
 context(ka: KaSession)
-internal fun KaLibraryModule.getAllClassifiers(): Sequence<KaDeclarationSymbol> = sequence {
+public fun KaLibraryModule.getAllClassifiers(): Sequence<KaDeclarationSymbol> = sequence {
     val addresses = addresses.asSequence()
     yieldAll(addresses.getAllClassifiers())
 }
