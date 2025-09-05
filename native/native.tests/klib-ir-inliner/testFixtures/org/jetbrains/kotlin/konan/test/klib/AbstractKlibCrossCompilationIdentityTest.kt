81502985ae0a2f5b21e121ffc180c3f4dd467e17
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-09-05 10:04:13 +0200

    IR inliner: Update test data due to enabling IR inliner in stdlib
    ^KT-80619

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/klib/AbstractKlibCrossCompilationIdentityTest.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/klib/AbstractKlibCrossCompilationIdentityTest.kt
@@ -47,6 +47,7 @@ import org.jetbrains.kotlin.utils.bind
 import org.junit.jupiter.api.Assumptions.assumeTrue
 import org.junit.jupiter.api.Tag
 import java.io.File
+import java.nio.file.Paths
 
 /**
  * This test works in the following way:
@@ -169,7 +170,7 @@ private class NativeKlibCrossCompilationIdentityHandler(testServices: TestServic
         val klibFile = info.outputFile
 
         metadataDumper[module] += klibFile.dumpMetadata(kotlinNativeClassLoader, printSignatures = false, signatureVersion = null)
-        irDumper[module] += klibFile.dumpIr(kotlinNativeClassLoader)
+        irDumper[module] += klibFile.dumpIrAndSanitizePathsInFileEntries(kotlinNativeClassLoader)
         manifestDumper[module] += readManifestAndSanitize(klibFile, singleTargetInManifestToBeReplacedByTheAlias = null)
     }
 
@@ -212,3 +213,28 @@ private class NativeKlibCrossCompilationIdentityHandler(testServices: TestServic
     }
 }
 
+private fun File.dumpIrAndSanitizePathsInFileEntries(
+    kotlinNativeClassLoader: ClassLoader,
+): String {
+    val rawIrDump = dumpIr(kotlinNativeClassLoader)
+
+    // There can be absolute paths in IR file entries in `IrInlinedFunctionBlock`s coming from dependency libraries (stdlib, kotlin-test).
+    // Let's truncate them to the last 5 path segments to make tests more stable.
+    val sanitizedIrDump = rawIrDump.lineSequence().map { line ->
+        val prefixStartIndex = line.indexOf(INLINED_FUNCTION_BLOCK_FILE_ENTRY_PREFIX)
+        if (prefixStartIndex < 0) return@map line
+
+        val prefixEndIndex = prefixStartIndex + INLINED_FUNCTION_BLOCK_FILE_ENTRY_PREFIX.length
+
+        val prefix = line.substring(0, prefixEndIndex)
+        val maybeAbsolutePath = line.substring(prefixEndIndex)
+
+        val relativePath = Paths.get(maybeAbsolutePath).toList().takeLast(5).joinToString(File.separator)
+
+        prefix + relativePath
+    }.joinToString("\n")
+
+    return sanitizedIrDump
+}
+
+private const val INLINED_FUNCTION_BLOCK_FILE_ENTRY_PREFIX = "inlinedFunctionFileEntry: FILE_ENTRY path:"
