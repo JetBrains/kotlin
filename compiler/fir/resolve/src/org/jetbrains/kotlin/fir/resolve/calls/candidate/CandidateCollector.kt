2e6c5ec3b8216870894127ca88843c8609cff8f2
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2025-09-05 13:54:12 +0200

    [FIR] Introduce `forwardedDiagnostics` to `CandidateCollector`
    It will be used for diagnostics that are dropped during tower resolution
    but should be preserved for reporting useful cases.
    
    For instance, a case when declaration order affects resolution.

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/candidate/CandidateCollector.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/candidate/CandidateCollector.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.fir.resolve.calls.candidate
 
 import org.jetbrains.kotlin.fir.resolve.BodyResolveComponents
 import org.jetbrains.kotlin.fir.resolve.calls.ResolutionContext
+import org.jetbrains.kotlin.fir.resolve.calls.ResolutionDiagnostic
 import org.jetbrains.kotlin.fir.resolve.calls.ResolutionResultOverridesOtherToPreserveCompatibility
 import org.jetbrains.kotlin.fir.resolve.calls.stages.ResolutionStageRunner
 import org.jetbrains.kotlin.fir.resolve.calls.tower.TowerGroup
@@ -29,11 +30,14 @@ open class CandidateCollector(
 
     private var bestGroup = TowerGroup.Last
 
+    private val forwardedDiagnostics: MutableList<ResolutionDiagnostic> = mutableListOf()
+
     fun newDataSet() {
         groupNumbers.clear()
         candidates.clear()
         currentApplicability = CandidateApplicability.HIDDEN
         bestGroup = TowerGroup.Last
+        forwardedDiagnostics.clear()
     }
 
     open fun consumeCandidate(group: TowerGroup, candidate: Candidate, context: ResolutionContext): CandidateApplicability {
@@ -73,6 +77,12 @@ open class CandidateCollector(
 
         return applicability
     }
+
+    fun addForwardedDiagnostic(diagnostic: ResolutionDiagnostic) {
+        forwardedDiagnostics.add(diagnostic)
+    }
+
+    fun forwardedDiagnostics(): List<ResolutionDiagnostic> = forwardedDiagnostics
 
     fun bestCandidates(): List<Candidate> = candidates
 
