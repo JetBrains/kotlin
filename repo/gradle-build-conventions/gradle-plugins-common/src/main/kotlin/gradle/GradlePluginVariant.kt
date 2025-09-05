7b1439aff15c56f3548df5b7205edd03f5fac4a8
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-09-05 22:43:31 +0200

    [Gradle] Compile against Gradle API 9.0.0
    ^KT-77457 Fixed

--- repo/gradle-build-conventions/gradle-plugins-common/src/main/kotlin/gradle/GradlePluginVariant.kt
+++ repo/gradle-build-conventions/gradle-plugins-common/src/main/kotlin/gradle/GradlePluginVariant.kt
@@ -36,7 +36,7 @@ enum class GradlePluginVariant(
     ;
 
     companion object {
-        const val GRADLE_COMMON_COMPILE_API_VERSION = "8.14"
+        const val GRADLE_COMMON_COMPILE_API_VERSION = "9.0.0"
 
         val MIDDLE_GRADLE_VARIANT_FOR_TESTS = GradlePluginVariant.values().run { this[size / 2] }
         val MAXIMUM_SUPPORTED_GRADLE_VARIANT = GradlePluginVariant.values().last()
