98164208dcea5caaf349ba9923bf3e1211085436
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-09-05 09:31:41 -0400

    fixup! KT-79192 Hide InputStream.readBytes(Int)

--- compiler/testData/diagnostics/testsWithStdLib/deprecated/deprecationOnReadBytes.kt
+++ compiler/testData/diagnostics/testsWithStdLib/deprecated/deprecationOnReadBytes.kt
@@ -5,7 +5,7 @@ import java.io.InputStream
 fun InputStream.test() {
     readBytes()
 
-    <!DEPRECATION_ERROR!>readBytes<!>(1)
+    readBytes(<!TOO_MANY_ARGUMENTS!>1<!>)
 }
 
 /* GENERATED_FIR_TAGS: funWithExtensionReceiver, functionDeclaration, integerLiteral */
