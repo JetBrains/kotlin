4785b8759525b6474954c562153d825fac5b77e1
misosvec <misosvec01@gmail.com>
2025-09-05 16:50:01 +0200

    reogranized cache paths

--- compiler/daemon/remote-daemon/src/main/kotlin/common/Constants.kt
+++ compiler/daemon/remote-daemon/src/main/kotlin/common/Constants.kt
@@ -11,23 +11,32 @@ import java.nio.file.Paths
 fun getServerEnv(): String = System.getenv("SERVER_ENV") ?: "localhost"
 fun isServerEnvK8s(): Boolean = (System.getenv("SERVER_ENV") ?: "").contains("K8S")
 
-val K8S_STORAGE_PATH: Path = Paths.get("/storage")
+const val CACHE_FOLDER_NAME = "cache"
+const val ARTIFACTS_FOLDER_NAME = "artifacts"
+const val TMP_FOLDER_NAME = "tmp"
+const val STORAGE_FOLDER_NAME = "storage"
+const val WORKSPACE_FOLDER_NAME = "workspace"
+
+val K8S_STORAGE_PATH: Path = Paths.get("/$STORAGE_FOLDER_NAME")
 
 val SERVER_CACHE_DIR: Path =
-    if (isServerEnvK8s()) K8S_STORAGE_PATH.resolve("cache")
-    else Paths.get("src", "main", "kotlin", "server", "cache")
+    if (isServerEnvK8s()) K8S_STORAGE_PATH.resolve(CACHE_FOLDER_NAME)
+    else Paths.get(STORAGE_FOLDER_NAME, CACHE_FOLDER_NAME)
 
-val SERVER_ARTIFACTS_CACHE_DIR: Path = SERVER_CACHE_DIR.resolve("artifacts")
+val SERVER_ARTIFACTS_CACHE_DIR: Path = SERVER_CACHE_DIR.resolve(ARTIFACTS_FOLDER_NAME)
 
-val SERVER_TMP_CACHE_DIR: Path = SERVER_CACHE_DIR.resolve("tmp")
+val SERVER_TMP_CACHE_DIR: Path = SERVER_CACHE_DIR.resolve(TMP_FOLDER_NAME)
 
 val SERVER_COMPILATION_WORKSPACE_DIR: Path =
-    if (isServerEnvK8s()) K8S_STORAGE_PATH.resolve("workspace")
-    else Paths.get("src", "main", "kotlin", "server", "workspace")
+    if (isServerEnvK8s()) K8S_STORAGE_PATH.resolve(WORKSPACE_FOLDER_NAME)
+    else Paths.get(STORAGE_FOLDER_NAME, WORKSPACE_FOLDER_NAME)
 
 
-val CLIENT_COMPILED_DIR: Path = Paths.get("src", "main", "kotlin", "client", "compiled")
-val CLIENT_TMP_DIR: Path = Paths.get("src", "main", "kotlin", "client", "tmp")
+val CLIENT_STORAGE_FOLDER_NAME = "client-storage"
+val COMPILED_FOLDER_NAME = "compiled"
+
+val CLIENT_COMPILED_DIR: Path = Paths.get(CLIENT_STORAGE_FOLDER_NAME, COMPILED_FOLDER_NAME)
+val CLIENT_TMP_DIR: Path = Paths.get(CLIENT_STORAGE_FOLDER_NAME, TMP_FOLDER_NAME)
 
 // TODO it would be appropriate to configure also different paths in k8s environment, do not forget to udpate Dockerfile
 val AUTH_FILE: Path = Paths.get("src", "main", "kotlin", "server", "auth", "auth.json")
