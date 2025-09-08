7624887356ae08184868abcfed5eae545f68652a
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-09-08 11:34:33 +0200

    [Wasm] rename Wasm instructions to standard names any.convert_extern/extern.convert_any

--- wasm/wasm.ir/src/org/jetbrains/kotlin/wasm/ir/Operators.kt
+++ wasm/wasm.ir/src/org/jetbrains/kotlin/wasm/ir/Operators.kt
@@ -402,8 +402,8 @@ enum class WasmOp(
     BR_ON_CAST("br_on_cast", 0xFB_18, listOf(CONST_U8, LABEL_IDX, HEAP_TYPE, HEAP_TYPE)),
     BR_ON_CAST_FAIL("br_on_cast_fail", 0xFB_19, listOf(CONST_U8, LABEL_IDX, HEAP_TYPE, HEAP_TYPE)),
 
-    EXTERN_INTERNALIZE("extern.internalize", 0xFB_1A), // externref -> anyref
-    EXTERN_EXTERNALIZE("extern.externalize", 0xFB_1B), // anyref -> externref
+    EXTERN_INTERNALIZE("any.convert_extern", 0xFB_1A), // externref -> anyref
+    EXTERN_EXTERNALIZE("extern.convert_any", 0xFB_1B), // anyref -> externref
 
     // ============================================================
     // Exception handling
