f90b861d42eacf848de7283ce9b54eea8af9966f
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-09-08 17:16:08 +0200

    [stubs] AbstractStubsTest: add serialization validation
    to unsure that the serialize/deserialize function is implemented correctly
    
    ^KT-80758

--- analysis/stubs/testFixtures/org/jetbrains/kotlin/analysis/stubs/AbstractStubsTest.kt
+++ analysis/stubs/testFixtures/org/jetbrains/kotlin/analysis/stubs/AbstractStubsTest.kt
@@ -5,10 +5,11 @@
 
 package org.jetbrains.kotlin.analysis.stubs
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.StubBasedPsiElement
-import com.intellij.psi.stubs.PsiFileStub
-import com.intellij.psi.stubs.StubElement
-import com.intellij.psi.stubs.StubTreeLoader
+import com.intellij.psi.stubs.*
+import com.intellij.util.io.AbstractStringEnumerator
+import com.intellij.util.io.UnsyncByteArrayOutputStream
 import org.jetbrains.kotlin.analysis.decompiler.stub.file.ClsClassFinder
 import org.jetbrains.kotlin.analysis.decompiler.stub.files.serializeToString
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirBinaryTestConfigurator
@@ -24,6 +25,8 @@ import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.test.services.AssertionsService
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.assertions
+import org.jetbrains.kotlin.utils.addToStdlib.shouldNotBeCalled
+import kotlin.test.assertEquals
 
 abstract class AbstractStubsTest : AbstractAnalysisApiBasedTest() {
     override fun doTestByMainModuleAndOptionalMainFile(mainFile: KtFile?, mainModule: KtTestModule, testServices: TestServices) {
@@ -38,12 +41,12 @@ abstract class AbstractStubsTest : AbstractAnalysisApiBasedTest() {
 
             val singleElement = filesAndStubs.singleOrNull()
             if (singleElement != null) {
-                renderStub(singleElement.second)
+                printStub(singleElement.second)
             } else {
                 printCollection(filesAndStubs, separator = "\n\n") { element ->
                     appendLine("${element.first.name}:")
                     withIndent {
-                        renderStub(element.second)
+                        printStub(element.second)
                     }
                 }
             }
@@ -52,8 +55,29 @@ abstract class AbstractStubsTest : AbstractAnalysisApiBasedTest() {
         testServices.assertions.assertEqualsToTestOutputFile(actual, extension = ".stubs.txt")
 
         for ((_, stub) in filesAndStubs) {
-            checkPsiElementTypeConsistency(testServices.assertions, stub ?: continue)
+            val fileStub = stub ?: continue
+            checkPsiElementTypeConsistency(testServices.assertions, fileStub)
+            validateSerializers(testServices.assertions, fileStub)
         }
+    }
+
+    /**
+     * Validates that the stub tree can be serialized and deserialized correctly.
+     *
+     * This means that a stub serializer wrote the same number of bytes to the output stream as it read during deserialization.
+     */
+    private fun validateSerializers(assertions: AssertionsService, fileStub: PsiFileStub<*>) {
+        val deserializedStub = serializeAndDeserializeStub(
+            originalStub = fileStub,
+            deserializedParentStub = null,
+            buffer = UnsyncByteArrayOutputStream(),
+            storage = StringEnumerator(),
+        )
+
+        assertions.assertEquals(
+            expected = renderStub(fileStub),
+            actual = renderStub(deserializedStub),
+        ) { "The deserialized stub must be the same" }
     }
 
     private fun checkPsiElementTypeConsistency(assertions: AssertionsService, stubElement: StubElement<*>) {
@@ -62,8 +86,9 @@ abstract class AbstractStubsTest : AbstractAnalysisApiBasedTest() {
             assertions.assertEquals(
                 stubElement.stubType,
                 psi.elementType,
-                { "Expected the PSI of `$stubElement` to have the same element type. Instead got: `${psi.elementType}`." },
-            )
+            ) {
+                "Expected the PSI of `$stubElement` to have the same element type. Instead got: `${psi.elementType}`."
+            }
         }
 
         stubElement.childrenStubs.forEach {
@@ -71,10 +96,13 @@ abstract class AbstractStubsTest : AbstractAnalysisApiBasedTest() {
         }
     }
 
-    private fun PrettyPrinter.renderStub(stub: PsiFileStub<*>?) {
-        val treeStr = stub?.serializeToString()
-        append(treeStr)
+    context(printer: PrettyPrinter)
+    private fun printStub(stub: PsiFileStub<*>?) {
+        val stubRepresentation = renderStub(stub)
+        printer.append(stubRepresentation)
     }
+
+    protected open fun renderStub(stub: PsiFileStub<*>?): String = stub?.serializeToString().toString()
 
     abstract fun computeStub(file: KtFile): PsiFileStub<*>?
 }
@@ -107,4 +135,63 @@ abstract class AbstractDecompiledStubsTest : AbstractStubsTest() {
     }
 
     override fun computeStub(file: KtFile): PsiFileStub<*> = file.calcStubTree().root
+}
+
+private fun <P : PsiElement, S : StubElement<P>> serializeAndDeserializeStub(
+    originalStub: S,
+    deserializedParentStub: StubElement<*>?,
+    buffer: UnsyncByteArrayOutputStream,
+    storage: AbstractStringEnumerator,
+): S {
+    buffer.reset()
+
+    val serializer = if (originalStub is PsiFileStub<*>) {
+        originalStub.type
+    } else {
+        originalStub.stubType
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    serializer as ObjectStubSerializer<StubElement<*>, StubElement<*>>
+
+    serializer.serialize(originalStub, StubOutputStream(buffer, storage))
+
+    val stubInputStream = StubInputStream(buffer.toInputStream(), storage)
+    val deserializedStub = serializer.deserialize(stubInputStream, deserializedParentStub)
+    assertEquals(-1, stubInputStream.read(), "The deserializer has to read the same amount of bytes as the serializer wrote")
+    assertEquals(originalStub::class, deserializedStub::class, "The stub class must be the same")
+    assertEquals(originalStub.stubType, deserializedStub.stubType, "The stub type must be the same")
+
+    for (originalChild in originalStub.childrenStubs) {
+        serializeAndDeserializeStub(
+            originalStub = originalChild,
+            deserializedParentStub = deserializedStub,
+            buffer = buffer,
+            storage = storage,
+        )
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return deserializedStub as S
+}
+
+private class StringEnumerator : AbstractStringEnumerator {
+    private val values = HashMap<String, Int>()
+    private val strings = mutableListOf<String>()
+
+    override fun enumerate(value: String?): Int {
+        if (value == null) return 0
+
+        return values.getOrPut(value) {
+            strings += value
+            values.size + 1
+        }
+    }
+
+    override fun valueOf(idx: Int): String? = if (idx == 0) null else strings[idx - 1]
+
+    override fun markCorrupted(): Unit = shouldNotBeCalled()
+    override fun close(): Unit = shouldNotBeCalled()
+    override fun isDirty(): Boolean = shouldNotBeCalled()
+    override fun force(): Unit = shouldNotBeCalled()
 }
