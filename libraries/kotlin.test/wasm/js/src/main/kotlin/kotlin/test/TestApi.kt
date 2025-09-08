064be2d868252d5bd20e5a15a250cbb6d25fd93d
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-09-08 20:25:16 +0200

    [TMP] temporary remove test ExternalFrameworkAdapter

--- libraries/kotlin.test/wasm/js/src/main/kotlin/kotlin/test/TestApi.kt
+++ libraries/kotlin.test/wasm/js/src/main/kotlin/kotlin/test/TestApi.kt
@@ -20,25 +20,13 @@ internal fun setAdapter(adapter: FrameworkAdapter) {
 
 internal var currentAdapter: FrameworkAdapter? = null
 
-@JsName("kotlinTest")
-internal external val kotlinTestNamespace: ExternalKotlinTestNamespace?
-
-internal external interface ExternalKotlinTestNamespace : JsAny {
-    public val adapterTransformer: ((ExternalFrameworkAdapter) -> ExternalFrameworkAdapter)?
-}
-
 internal actual fun adapter(): FrameworkAdapter {
     currentAdapter?.let { return it }
 
     val adapter: FrameworkAdapter
     if (isJasmine()) {
         val jasmineLikeAdapter = JasmineLikeAdapter()
-        val transformer = kotlinTestNamespace?.adapterTransformer
-        if (transformer != null) {
-            adapter = transformer(jasmineLikeAdapter.externalize()).internalize()
-        } else {
-            adapter = jasmineLikeAdapter
-        }
+        adapter = jasmineLikeAdapter
     } else {
         adapter = TeamcityAdapterWithPromiseSupport()
     }
