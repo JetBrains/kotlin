cc13629363b63ca728d0b0387f063ea4177bfd26
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-09-08 11:05:06 +0200

    IJ SDK: Adjust kotlinx.serialization tests
    Runtime version was updated to 1.7.3 implicitly via the newer IJ SDK

--- plugins/kotlinx-serialization/testData/boxIr/uuidSerializer.kt
+++ plugins/kotlinx-serialization/testData/boxIr/uuidSerializer.kt
@@ -2,31 +2,6 @@
 // API_VERSION: LATEST
 // OPT_IN: kotlin.uuid.ExperimentalUuidApi
 
-// FILE: serializer.kt
-
-package kotlinx.serialization.internal
-
-import kotlin.uuid.*
-import kotlin.uuid.Uuid
-import kotlinx.serialization.*
-import kotlinx.serialization.encoding.*
-import kotlinx.serialization.descriptors.*
-
-// TODO: delete when serialization runtime is updated to 1.8.0
-object UuidSerializer: KSerializer<Uuid> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("kotlin.uuid.Uuid", PrimitiveKind.STRING)
-
-    override fun serialize(encoder: Encoder, value: Uuid) {
-        encoder.encodeString(value.toString())
-    }
-
-    override fun deserialize(decoder: Decoder): Uuid {
-        return Uuid.parse(decoder.decodeString())
-    }
-}
-
-// FILE: test.kt
-
 package a
 
 import kotlin.uuid.*
