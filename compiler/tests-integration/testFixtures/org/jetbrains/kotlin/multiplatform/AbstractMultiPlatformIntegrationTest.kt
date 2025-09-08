47cd55c6434fa1316709682dd6a2f98da1a31634
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-09-08 14:40:16 +0300

    [Test] Add `-Xtarget-platform` argument to metadata CLI tests
    KT-78443

--- compiler/tests-integration/testFixtures/org/jetbrains/kotlin/multiplatform/AbstractMultiPlatformIntegrationTest.kt
+++ compiler/tests-integration/testFixtures/org/jetbrains/kotlin/multiplatform/AbstractMultiPlatformIntegrationTest.kt
@@ -44,7 +44,7 @@ abstract class AbstractMultiPlatformIntegrationTest : KtUsefulTestCase() {
 
         val result = buildString {
             appendLine("-- Common --")
-            appendLine(KotlinMetadataCompiler().compile(commonSrc, null, "-d", commonDest, *optionalStdlibCommon))
+            appendLine(KotlinMetadataCompiler().compile(commonSrc, null, "-d", commonDest, *optionalStdlibCommon, "-Xtarget-platform=JVM,JS,WASM_JS,WASM_WASI,Native"))
 
             if (jvmSrc != null) {
                 appendLine()
