3f36824c4b1949a95353c2389b372ce6f92d63af
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-09-08 19:14:08 +0200

    [stubs] extract AbstractCompiledStubsTest to separate file
    ^KT-80798

---
+++ analysis/stubs/testFixtures/org/jetbrains/kotlin/analysis/stubs/AbstractCompiledStubsTest.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.stubs
+
+import com.intellij.psi.stubs.PsiFileStub
+import com.intellij.psi.stubs.StubTreeLoader
+import org.jetbrains.kotlin.analysis.decompiler.psi.file.KtDecompiledFile
+import org.jetbrains.kotlin.analysis.decompiler.stub.file.ClsClassFinder
+import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirBinaryTestConfigurator
+import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtLibraryBinaryDecompiledTestModuleFactory
+import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtTestModuleFactory
+import org.jetbrains.kotlin.analysis.test.framework.test.configurators.AnalysisApiTestConfigurator
+import org.jetbrains.kotlin.analysis.utils.errors.requireIsInstance
+import org.jetbrains.kotlin.psi.KtFile
+
+/**
+ * This test is supposed to validate the compiled stubs output.
+ *
+ * It takes a compiled file as a binary data and creates stubs for it.
+ *
+ * @see AbstractDecompiledStubsTest
+ */
+abstract class AbstractCompiledStubsTest : AbstractStubsTest() {
+    override val configurator: AnalysisApiTestConfigurator = CompiledStubsTestConfigurator()
+
+    override fun computeStub(file: KtFile): PsiFileStub<*>? = ClsClassFinder.allowMultifileClassPart {
+        requireIsInstance<KtDecompiledFile>(file)
+
+        // The tree loader is called to build a stub tree for a binary file directly
+        StubTreeLoader.getInstance()
+            .build(/* project = */ null, /* vFile = */ file.virtualFile, /* psiFile = */ null)
+            ?.root
+            ?.let { it as PsiFileStub<*> }
+    }
+
+    internal open class CompiledStubsTestConfigurator : AnalysisApiFirBinaryTestConfigurator() {
+        override val testModuleFactory: KtTestModuleFactory get() = KtLibraryBinaryDecompiledTestModuleFactory
+        override val testPrefixes: List<String> get() = listOf("compiled")
+    }
+}
