da54184e3cf43f560e69248bc0fb75fd1413b063
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2025-09-09 17:12:52 +0200

    [KGP][BTA] Switch KGP to new BTA API
    # Conflicts:
    #	compiler/build-tools/kotlin-build-tools-api/src/main/kotlin/org/jetbrains/kotlin/buildtools/api/jvm/operations/JvmCompilationOperation.kt
    #	compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/jvm/operations/JvmCompilationOperationImpl.kt
    
    wip: add support for enableUnsafeIncrementalCompilationForMultiplatform
    
    # Conflicts:
    #	compiler/build-tools/kotlin-build-tools-api/src/main/kotlin/org/jetbrains/kotlin/buildtools/api/jvm/JvmSnapshotBasedIncrementalCompilationConfiguration.kt
    #	compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/icAdapters.kt
    #	compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/jvm/JvmSnapshotBasedIncrementalCompilationConfigurationImpl.kt
    
    wip: add support for enableMonotonousIncrementalCompileSetExpansion
    
    wip: [KGP][BTA] Fixing daemon tests
    
    wip: measure RUN_COMPILATION as compilation operation execution time

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
@@ -90,6 +90,7 @@ val kotlinGradlePluginAndItsRequired = arrayOf(
     ":compiler:build-tools:kotlin-build-statistics",
     ":compiler:build-tools:kotlin-build-tools-api",
     ":compiler:build-tools:kotlin-build-tools-impl",
+    ":compiler:build-tools:kotlin-build-tools-compat",
     ":libraries:tools:gradle:fus-statistics-gradle-plugin",
     ":kotlin-util-klib-metadata",
     ":libraries:tools:abi-validation:abi-tools-api",
