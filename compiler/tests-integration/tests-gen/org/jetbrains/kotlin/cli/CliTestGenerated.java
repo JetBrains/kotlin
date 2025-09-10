f9de75191cf20e1a232a189cd3770c6e4f071922
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2025-09-10 16:44:52 +0300

    [Tests] KT-79718: add tests with explicit API mode disabled

--- compiler/tests-integration/tests-gen/org/jetbrains/kotlin/cli/CliTestGenerated.java
+++ compiler/tests-integration/tests-gen/org/jetbrains/kotlin/cli/CliTestGenerated.java
@@ -2134,6 +2134,16 @@ public class CliTestGenerated extends AbstractCliTest {
       runTest("compiler/testData/cli/js/perFileWithWrongModuleKind.args");
     }
 
+    @TestMetadata("publicSyntheticAccessorGenerationWithExplicitAPIModeDisabled.args")
+    public void testPublicSyntheticAccessorGenerationWithExplicitAPIModeDisabled() {
+      runTest("compiler/testData/cli/js/publicSyntheticAccessorGenerationWithExplicitAPIModeDisabled.args");
+    }
+
+    @TestMetadata("publicSyntheticAccessorGenerationWithoutExplicitAPIFlag.args")
+    public void testPublicSyntheticAccessorGenerationWithoutExplicitAPIFlag() {
+      runTest("compiler/testData/cli/js/publicSyntheticAccessorGenerationWithoutExplicitAPIFlag.args");
+    }
+
     @TestMetadata("reifiedIntersectionType.args")
     public void testReifiedIntersectionType() {
       runTest("compiler/testData/cli/js/reifiedIntersectionType.args");
