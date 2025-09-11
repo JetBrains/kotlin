93929a39fd9596b724cf4dbcbf20f98e74749a4e
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-09-11 23:24:33 +0200

    kotlin-compiler-testdata-for-ide: drop redundant test data
    They are not used by the IntelliJ Kotlin plugin for a long time

--- prepare/ide-plugin-dependencies/kotlin-compiler-testdata-for-ide/build.gradle.kts
+++ prepare/ide-plugin-dependencies/kotlin-compiler-testdata-for-ide/build.gradle.kts
@@ -9,13 +9,6 @@ idePluginDependency {
         listOf(
             "compiler/testData/asJava/lightClasses",
             "compiler/testData/loadJava/compiledKotlin",
-            "compiler/fir/analysis-tests/testData/resolve",
-            "compiler/fir/analysis-tests/testData/resolveWithStdlib",
-            "compiler/testData/diagnostics/tests",
-            "compiler/testData/diagnostics/helpers",
-            "compiler/tests-spec/testData",
-            "compiler/testData/diagnostics/testsWithStdLib",
-            "compiler/fir/raw-fir/psi2fir/testData",
         ).forEach {
             from(rootDir.resolve(it)) {
                 into(it)
