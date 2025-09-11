311fd262afa4c9fe00362bc1cbf7a5276b6068fb
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-09-11 15:29:58 +0200

    [JS stdlib] Comment out unused PrimitiveClasses.longArrayClass property
    It became unused in 15c6b275d7b6ca34657c03ebe9c16535aa4dd36a.

--- libraries/stdlib/js/src/kotlin/reflect/primitives.kt
+++ libraries/stdlib/js/src/kotlin/reflect/primitives.kt
@@ -66,8 +66,11 @@ internal object PrimitiveClasses {
     @JsName("intArrayClass")
     val intArrayClass = PrimitiveKClassImpl(js("Int32Array").unsafeCast<JsClass<IntArray>>(), "IntArray", { it is IntArray })
 
-    @JsName("longArrayClass")
-    val longArrayClass = PrimitiveKClassImpl(js("Array").unsafeCast<JsClass<LongArray>>(), "LongArray", { it is LongArray })
+    // NOTE: The PrimitiveKClassImpl instance for LongArray is initialized in either kotlin.js.internal.longAsBigInt or
+    // kotlin.js.internal.boxedLong package, depending on whether compiling Long as BigInt is enabled.
+    // TODO(KT-70480): Uncomment this after we drop all ES editions except the latest.
+//    @JsName("longArrayClass")
+//    val longArrayClass = PrimitiveKClassImpl(js("BigInt64Array").unsafeCast<JsClass<LongArray>>(), "LongArray", { it is LongArray })
 
     @JsName("floatArrayClass")
     val floatArrayClass = PrimitiveKClassImpl(js("Float32Array").unsafeCast<JsClass<FloatArray>>(), "FloatArray", { it is FloatArray })
