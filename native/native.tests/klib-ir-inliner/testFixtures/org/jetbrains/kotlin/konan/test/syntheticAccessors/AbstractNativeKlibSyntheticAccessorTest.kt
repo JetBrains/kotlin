bf1c32a58c61a10048286557fb5828d73beb493f
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-09-12 15:44:28 +0200

    [Tests] Move commonConfigurationForDumpSyntheticAccessorsTest to `tests-common-new`
    ^KT-78964

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/syntheticAccessors/AbstractNativeKlibSyntheticAccessorTest.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/syntheticAccessors/AbstractNativeKlibSyntheticAccessorTest.kt
@@ -5,7 +5,6 @@
 
 package org.jetbrains.kotlin.konan.test.syntheticAccessors
 
-import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.konan.test.Fir2IrNativeResultsConverter
 import org.jetbrains.kotlin.konan.test.NativeKlibSerializerFacade
 import org.jetbrains.kotlin.konan.test.converters.NativeDeserializerFacade
@@ -14,34 +13,16 @@ import org.jetbrains.kotlin.platform.konan.NativePlatforms
 import org.jetbrains.kotlin.test.Constructor
 import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.TargetBackend
-import org.jetbrains.kotlin.test.backend.BlackBoxCodegenSuppressor
-import org.jetbrains.kotlin.test.backend.handlers.NoIrCompilationErrorsHandler
-import org.jetbrains.kotlin.test.backend.handlers.SyntheticAccessorsDumpHandler
 import org.jetbrains.kotlin.test.backend.ir.IrBackendInput
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
-import org.jetbrains.kotlin.test.builders.deserializedIrHandlersStep
-import org.jetbrains.kotlin.test.builders.firHandlersStep
-import org.jetbrains.kotlin.test.builders.irHandlersStep
-import org.jetbrains.kotlin.test.builders.loweredIrHandlersStep
-import org.jetbrains.kotlin.test.configuration.commonFirHandlersForCodegenTest
-import org.jetbrains.kotlin.test.directives.ConfigurationDirectives
-import org.jetbrains.kotlin.test.directives.DiagnosticsDirectives
-import org.jetbrains.kotlin.test.directives.DiagnosticsDirectives.DIAGNOSTICS
+import org.jetbrains.kotlin.test.configuration.commonConfigurationForDumpSyntheticAccessorsTest
 import org.jetbrains.kotlin.test.directives.KlibBasedCompilerTestDirectives
-import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives.LANGUAGE
 import org.jetbrains.kotlin.test.directives.configureFirParser
-import org.jetbrains.kotlin.test.directives.model.ValueDirective
 import org.jetbrains.kotlin.test.frontend.fir.FirFrontendFacade
-import org.jetbrains.kotlin.test.frontend.fir.FirMetaInfoDiffSuppressor
 import org.jetbrains.kotlin.test.frontend.fir.FirOutputArtifact
-import org.jetbrains.kotlin.test.frontend.fir.handlers.*
 import org.jetbrains.kotlin.test.model.*
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerWithTargetBackendTest
-import org.jetbrains.kotlin.test.services.LibraryProvider
 import org.jetbrains.kotlin.test.services.configuration.NativeEnvironmentConfigurator
-import org.jetbrains.kotlin.test.services.sourceProviders.AdditionalDiagnosticsSourceFilesProvider
-import org.jetbrains.kotlin.test.services.sourceProviders.CoroutineHelpersSourceFilesProvider
-import org.jetbrains.kotlin.utils.bind
 
 // Base class for IR dump synthetic accessors test, configured with FIR frontend, in Native-specific way.
 open class AbstractNativeKlibSyntheticAccessorTest : AbstractKotlinCompilerWithTargetBackendTest(TargetBackend.NATIVE) {
@@ -76,62 +57,4 @@ open class AbstractNativeKlibSyntheticAccessorTest : AbstractKotlinCompilerWithT
         )
         configureFirParser(parser)
     }
-}
-
-// it's a code duplication with same function in AbstractFirJsKlibSyntheticAccessorTest.kt
-// consider extracting these functions into one in some common backend module
-private fun TestConfigurationBuilder.commonConfigurationForDumpSyntheticAccessorsTest(
-    targetFrontend: FrontendKind<FirOutputArtifact>,
-    frontendFacade: Constructor<FrontendFacade<FirOutputArtifact>>,
-    frontendToIrConverter: Constructor<Frontend2BackendConverter<FirOutputArtifact, IrBackendInput>>,
-    irInliningFacade: Constructor<IrPreSerializationLoweringFacade<IrBackendInput>>,
-    serializerFacade: Constructor<BackendFacade<IrBackendInput, BinaryArtifacts.KLib>>,
-    deserializerFacade: Constructor<DeserializerFacade<BinaryArtifacts.KLib, IrBackendInput>>,
-    customIgnoreDirective: ValueDirective<TargetBackend>? = null,
-) {
-    globalDefaults {
-        frontend = targetFrontend
-        dependencyKind = DependencyKind.Binary
-    }
-    defaultDirectives {
-        DIAGNOSTICS with listOf("-NOTHING_TO_INLINE", "-ERROR_SUPPRESSION", "-UNCHECKED_CAST")
-        LANGUAGE with listOf(
-            "+${LanguageFeature.IrIntraModuleInlinerBeforeKlibSerialization.name}",
-            "+${LanguageFeature.IrCrossModuleInlinerBeforeKlibSerialization.name}"
-        )
-        +DiagnosticsDirectives.REPORT_ONLY_EXPLICITLY_DEFINED_DEBUG_INFO
-        +ConfigurationDirectives.WITH_STDLIB
-    }
-    useAdditionalService(::LibraryProvider)
-    useAdditionalSourceProviders(
-        ::CoroutineHelpersSourceFilesProvider,
-        ::AdditionalDiagnosticsSourceFilesProvider,
-    )
-    useAfterAnalysisCheckers(
-        ::BlackBoxCodegenSuppressor.bind(customIgnoreDirective, null),
-        ::FirMetaInfoDiffSuppressor,
-    )
-    facadeStep(frontendFacade)
-    firHandlersStep {
-        commonFirHandlersForCodegenTest()
-        useHandlers(
-            ::FirDumpHandler,
-            ::FirCfgDumpHandler,
-            ::FirCfgConsistencyHandler,
-            ::FirResolvedTypesVerifier,
-        )
-    }
-    facadeStep(frontendToIrConverter)
-    irHandlersStep {
-        useHandlers(::NoIrCompilationErrorsHandler)
-    }
-    facadeStep(irInliningFacade)
-    loweredIrHandlersStep {
-        useHandlers(::NoIrCompilationErrorsHandler)
-    }
-
-    enableMetaInfoHandler()
-    facadeStep(serializerFacade)
-    facadeStep(deserializerFacade)
-    deserializedIrHandlersStep { useHandlers(::SyntheticAccessorsDumpHandler) }
 }
