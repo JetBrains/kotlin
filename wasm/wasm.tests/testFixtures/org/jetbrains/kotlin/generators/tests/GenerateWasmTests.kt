de8748e6f3afb00ecf3a9b70cb027f8bc74196d6
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-09-12 14:35:00 +0200

    [Tests][Wasm] Add diagnostic tests with IR Inliner
    ^KT-78964

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/generators/tests/GenerateWasmTests.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/generators/tests/GenerateWasmTests.kt
@@ -81,6 +81,14 @@ fun main(args: Array<String>) {
             testClass<AbstractDiagnosticsFirWasmKlibTest> {
                 model("wasmDiagnosticsKlibTests", excludedPattern = TestGeneratorUtil.KT_OR_KTS_WITH_FIR_PREFIX)
             }
+
+            testClass<AbstractDiagnosticsWasmJsWithIrInlinerTestBase> {
+                model("irInliner", excludedPattern = TestGeneratorUtil.KT_OR_KTS_WITH_FIR_PREFIX)
+            }
+
+            testClass<AbstractDiagnosticsWasmWasiWithIrInlinerTestBase> {
+                model("irInliner", excludedPattern = TestGeneratorUtil.KT_OR_KTS_WITH_FIR_PREFIX)
+            }
         }
 
         testGroup(testsRoot, "js/js.translator/testData/box", testRunnerMethodName = "runTest0") {
--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/diagnostics/AbstractDiagnosticsFirWasmTest.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/diagnostics/AbstractDiagnosticsFirWasmTest.kt
@@ -5,14 +5,17 @@
 
 package org.jetbrains.kotlin.wasm.test.diagnostics
 
+import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.platform.wasm.WasmPlatforms
 import org.jetbrains.kotlin.test.Constructor
 import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.TargetBackend
-import org.jetbrains.kotlin.test.backend.BlackBoxCodegenSuppressor
+import org.jetbrains.kotlin.test.backend.ir.IrDiagnosticsHandler
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.builders.firHandlersStep
+import org.jetbrains.kotlin.test.builders.irHandlersStep
+import org.jetbrains.kotlin.test.builders.loweredIrHandlersStep
 import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives
 import org.jetbrains.kotlin.test.directives.configureFirParser
 import org.jetbrains.kotlin.test.frontend.fir.FirFrontendFacade
@@ -21,6 +24,9 @@ import org.jetbrains.kotlin.test.model.DependencyKind
 import org.jetbrains.kotlin.test.model.FrontendKinds
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerTest
 import org.jetbrains.kotlin.test.configuration.configurationForClassicAndFirTestsAlongside
+import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives.LANGUAGE
+import org.jetbrains.kotlin.test.frontend.fir.Fir2IrResultsConverter
+import org.jetbrains.kotlin.test.model.ResultingArtifact
 import org.jetbrains.kotlin.test.services.AbstractEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.LibraryProvider
 import org.jetbrains.kotlin.test.services.configuration.CommonEnvironmentConfigurator
@@ -28,6 +34,7 @@ import org.jetbrains.kotlin.test.services.configuration.WasmEnvironmentConfigura
 import org.jetbrains.kotlin.test.services.configuration.WasmEnvironmentConfiguratorWasi
 import org.jetbrains.kotlin.test.services.sourceProviders.AdditionalDiagnosticsSourceFilesProvider
 import org.jetbrains.kotlin.test.services.sourceProviders.CoroutineHelpersSourceFilesProvider
+import org.jetbrains.kotlin.wasm.test.converters.WasmPreSerializationLoweringFacade
 
 abstract class AbstractFirWasmDiagnosticTestBase(
     val parser: FirParser,
@@ -90,3 +97,36 @@ abstract class AbstractDiagnosticsFirWasmWasiTest : AbstractFirWasmDiagnosticTes
     WasmPlatforms.wasmWasi,
     ::WasmEnvironmentConfiguratorWasi
 )
+
+abstract class AbstractDiagnosticsWasmJsWithIrInlinerTestBase : AbstractDiagnosticsFirWasmTest() {
+    override fun configure(builder: TestConfigurationBuilder) = with(builder) {
+        super.configure(builder)
+        commonConfigurationForWasmDiagnosticTestAfterFir2IR()
+    }
+}
+
+abstract class AbstractDiagnosticsWasmWasiWithIrInlinerTestBase : AbstractDiagnosticsFirWasmWasiTest() {
+    override fun configure(builder: TestConfigurationBuilder) = with(builder) {
+        super.configure(builder)
+        commonConfigurationForWasmDiagnosticTestAfterFir2IR()
+    }
+}
+
+fun <FO : ResultingArtifact.FrontendOutput<FO>> TestConfigurationBuilder.commonConfigurationForWasmDiagnosticTestAfterFir2IR() {
+    defaultDirectives {
+        LANGUAGE with listOf(
+            "+${LanguageFeature.IrIntraModuleInlinerBeforeKlibSerialization.name}",
+            "+${LanguageFeature.IrCrossModuleInlinerBeforeKlibSerialization.name}"
+        )
+    }
+
+    facadeStep(::Fir2IrResultsConverter) // TODO Change for ::Fir2IrCliWebFacade in scope of KT-74671
+    irHandlersStep {
+        useHandlers(::IrDiagnosticsHandler)
+    }
+
+    facadeStep(::WasmPreSerializationLoweringFacade)
+    loweredIrHandlersStep {
+        useHandlers(::IrDiagnosticsHandler)
+    }
+}
