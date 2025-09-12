597d3493da62cbd70c3e5a0f9bd0e2fc7851c92d
Brian Norman <brian.norman@jetbrains.com>
2025-09-12 10:03:32 -0500

    [WIP] fix destructuring and anonymous object approximation

--- plugins/scripting/scripting-tests/testData/diagnostics/repl/unsafe_cast_in_loop.kts
+++ plugins/scripting/scripting-tests/testData/diagnostics/repl/unsafe_cast_in_loop.kts
@@ -4,6 +4,6 @@ var num: Int? = 3
 val numNotNull = num!!
 for (x in 1..2) {
     val num2 = num
-    println(num <!UNSAFE_OPERATOR_CALL!>+<!> 1)
+    println(<!SMARTCAST_IMPOSSIBLE!>num<!> + 1)
     num = null
 }
