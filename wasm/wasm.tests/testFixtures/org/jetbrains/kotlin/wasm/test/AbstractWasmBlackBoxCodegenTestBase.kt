2e3f8fefa9f604c20f82dcddd3ad14eac3de5211
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-09-15 19:14:51 +0200

    [Tests][Wasm] Extract commonConfigurationForWasmTest()
    Just split out part of `commonConfigurationForWasmBlackBoxCodegenTest()`,
    till IR serialization, so it can be used also in shorter pipelines
    
    ^KT-78964

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/AbstractWasmBlackBoxCodegenTestBase.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/AbstractWasmBlackBoxCodegenTestBase.kt
@@ -27,7 +27,6 @@ import org.jetbrains.kotlin.test.model.*
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerWithTargetBackendTest
 import org.jetbrains.kotlin.test.services.AdditionalSourceProvider
 import org.jetbrains.kotlin.test.services.LibraryProvider
-import org.jetbrains.kotlin.test.services.configuration.WasmEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.configuration.WasmFirstStageEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.configuration.WasmSecondStageEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.sourceProviders.AdditionalDiagnosticsSourceFilesProvider
@@ -59,67 +58,22 @@ abstract class AbstractWasmBlackBoxCodegenTestBase<R : ResultingArtifact.Fronten
         customIgnoreDirective: ValueDirective<TargetBackend>? = null,
         additionalIgnoreDirectives: List<ValueDirective<TargetBackend>>? = null,
     ) {
-        globalDefaults {
-            frontend = targetFrontend
-            targetPlatform = this@AbstractWasmBlackBoxCodegenTestBase.targetPlatform
-            dependencyKind = DependencyKind.Binary
-        }
-
-        val pathToRootOutputDir = System.getProperty("kotlin.wasm.test.root.out.dir") ?: error("'kotlin.wasm.test.root.out.dir' is not set")
-        defaultDirectives {
-            +DiagnosticsDirectives.REPORT_ONLY_EXPLICITLY_DEFINED_DEBUG_INFO
-            WasmEnvironmentConfigurationDirectives.PATH_TO_ROOT_OUTPUT_DIR with pathToRootOutputDir
-            WasmEnvironmentConfigurationDirectives.PATH_TO_TEST_DIR with pathToTestDir
-            WasmEnvironmentConfigurationDirectives.TEST_GROUP_OUTPUT_DIR_PREFIX with testGroupOutputDirPrefix
-            LANGUAGE with "+JsAllowImplementingFunctionInterface"
-        }
-
+        commonConfigurationForWasmFirstStageTest(
+            targetFrontend,
+            targetPlatform,
+            wasmTarget,
+            pathToTestDir,
+            testGroupOutputDirPrefix,
+            frontendFacade,
+            frontendToBackendConverter,
+            backendFacade,
+            additionalSourceProvider,
+            customIgnoreDirective,
+            additionalIgnoreDirectives,
+        )
         useConfigurators(
-            ::WasmFirstStageEnvironmentConfigurator.bind(wasmTarget),
             ::WasmSecondStageEnvironmentConfigurator.bind(wasmTarget),
         )
-
-        useAdditionalSourceProviders(
-            ::WasmAdditionalSourceProvider,
-            ::CoroutineHelpersSourceFilesProvider,
-            ::AdditionalDiagnosticsSourceFilesProvider,
-        )
-
-        additionalSourceProvider?.let {
-            useAdditionalSourceProviders(it)
-        }
-
-        useAdditionalService(::LibraryProvider)
-
-        useAfterAnalysisCheckers(
-            ::BlackBoxCodegenSuppressor.bind(customIgnoreDirective, additionalIgnoreDirectives),
-        )
-
-        facadeStep(frontendFacade)
-
-        classicFrontendHandlersStep {
-            commonClassicFrontendHandlersForCodegenTest()
-            useHandlers(::ClassicDiagnosticsHandler)
-        }
-
-        firHandlersStep {
-            useHandlers(::FirDiagnosticsHandler)
-        }
-
-        facadeStep(frontendToBackendConverter)
-        irHandlersStep {
-            useHandlers(::NoIrCompilationErrorsHandler)
-        }
-
-        facadeStep(::WasmPreSerializationLoweringFacade)
-        loweredIrHandlersStep {
-            useHandlers(::NoIrCompilationErrorsHandler)
-        }
-
-        loweredIrHandlersStep()
-
-        facadeStep(backendFacade)
-        klibArtifactsHandlersStep()
         facadeStep(afterBackendFacade)
 
         wasmArtifactsHandlersStep {
@@ -161,4 +115,80 @@ abstract class AbstractWasmBlackBoxCodegenTestBase<R : ResultingArtifact.Fronten
             }
         }
     }
+}
+
+fun <R : ResultingArtifact.FrontendOutput<R>, I : ResultingArtifact.BackendInput<I>, A : ResultingArtifact.Binary<A>>
+        TestConfigurationBuilder.commonConfigurationForWasmFirstStageTest(
+    targetFrontend: FrontendKind<R>,
+    targetPlatform: TargetPlatform,
+    wasmTarget: WasmTarget,
+    pathToTestDir: String,
+    testGroupOutputDirPrefix: String,
+    frontendFacade: Constructor<FrontendFacade<R>>,
+    frontendToBackendConverter: Constructor<Frontend2BackendConverter<R, I>>,
+    backendFacade: Constructor<BackendFacade<I, A>>,
+    additionalSourceProvider: Constructor<AdditionalSourceProvider>?,
+    customIgnoreDirective: ValueDirective<TargetBackend>? = null,
+    additionalIgnoreDirectives: List<ValueDirective<TargetBackend>>? = null,
+) {
+    globalDefaults {
+        frontend = targetFrontend
+        this.targetPlatform = targetPlatform
+        dependencyKind = DependencyKind.Binary
+    }
+
+    val pathToRootOutputDir = System.getProperty("kotlin.wasm.test.root.out.dir") ?: error("'kotlin.wasm.test.root.out.dir' is not set")
+    defaultDirectives {
+        +DiagnosticsDirectives.REPORT_ONLY_EXPLICITLY_DEFINED_DEBUG_INFO
+        WasmEnvironmentConfigurationDirectives.PATH_TO_ROOT_OUTPUT_DIR with pathToRootOutputDir
+        WasmEnvironmentConfigurationDirectives.PATH_TO_TEST_DIR with pathToTestDir
+        WasmEnvironmentConfigurationDirectives.TEST_GROUP_OUTPUT_DIR_PREFIX with testGroupOutputDirPrefix
+        LANGUAGE with "+JsAllowImplementingFunctionInterface"
+    }
+
+    useConfigurators(
+        ::WasmFirstStageEnvironmentConfigurator.bind(wasmTarget),
+    )
+
+    useAdditionalSourceProviders(
+        ::WasmAdditionalSourceProvider,
+        ::CoroutineHelpersSourceFilesProvider,
+        ::AdditionalDiagnosticsSourceFilesProvider,
+    )
+
+    additionalSourceProvider?.let {
+        useAdditionalSourceProviders(it)
+    }
+
+    useAdditionalService(::LibraryProvider)
+
+    useAfterAnalysisCheckers(
+        ::BlackBoxCodegenSuppressor.bind(customIgnoreDirective, additionalIgnoreDirectives),
+    )
+
+    facadeStep(frontendFacade)
+
+    classicFrontendHandlersStep {
+        commonClassicFrontendHandlersForCodegenTest()
+        useHandlers(::ClassicDiagnosticsHandler)
+    }
+
+    firHandlersStep {
+        useHandlers(::FirDiagnosticsHandler)
+    }
+
+    facadeStep(frontendToBackendConverter)
+    irHandlersStep {
+        useHandlers(::NoIrCompilationErrorsHandler)
+    }
+
+    facadeStep(::WasmPreSerializationLoweringFacade)
+    loweredIrHandlersStep {
+        useHandlers(::NoIrCompilationErrorsHandler)
+    }
+
+    loweredIrHandlersStep()
+
+    facadeStep(backendFacade)
+    klibArtifactsHandlersStep()
 }
