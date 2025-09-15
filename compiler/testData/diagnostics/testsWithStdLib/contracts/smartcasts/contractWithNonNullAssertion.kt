af3205a71456d7fc4f738455cb644a263fd2de3f
YuliaKorolenko <korollenkoyullia@gmail.com>
2025-09-15 15:54:11 +0200

    KT-26856 Smartcasts with non-null assertion for a contract function
    [Test] Create test, which checks contract with non-null assertion

---
+++ compiler/testData/diagnostics/testsWithStdLib/contracts/smartcasts/contractWithNonNullAssertion.kt
@@ -0,0 +1,49 @@
+// RUN_PIPELINE_TILL: BACKEND
+// OPT_IN: kotlin.contracts.ExperimentalContracts
+// ISSUE: KT-26856
+
+import kotlin.contracts.*
+
+fun funWithReturnTrueContract(value: Int?): Boolean? {
+    contract {
+        returns(true) implies (value != null)
+    }
+    return value != null
+}
+
+fun funWithReturnFalseContract(value: Int?): Boolean {
+    contract {
+        returns(false) implies (value != null)
+    }
+    return value != null
+}
+
+fun funWithReturnsNotNullContract(value: Int?): Boolean? {
+    contract {
+        returnsNotNull() implies (value != null)
+    }
+
+    return value != null
+}
+
+
+fun case_1(value: Int?) {
+    if (funWithReturnTrueContract(value)!!) {
+        value<!UNSAFE_CALL!>.<!>inv()
+    }
+}
+
+fun case_2(value: Int?) {
+    if (!funWithReturnFalseContract(value)<!UNNECESSARY_NOT_NULL_ASSERTION!>!!<!>) {
+        value<!UNSAFE_CALL!>.<!>inv()
+    }
+}
+
+fun case_3(value: Int?) {
+    if (funWithReturnsNotNullContract(value)!!) {
+        value<!UNSAFE_CALL!>.<!>inv()
+    }
+}
+
+/* GENERATED_FIR_TAGS: contractConditionalEffect, contracts, checkNotNullCall, functionDeclaration,
+ ifExpression, nullableType, smartcast */
