3b6e74cb910136885df014e2755eb6f97ea8aaaf
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2025-09-15 13:40:15 +0200

    [Wasm, JS] Promote deprecation of ExperimentalWasmDsl to error level
    ^KT-81005 fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/dsl/ExperimentalWasmDsl.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/dsl/ExperimentalWasmDsl.kt
@@ -9,6 +9,7 @@ package org.jetbrains.kotlin.gradle.targets.js.dsl
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY)
 @Deprecated(
     "Moved to kotlin-gradle-plugin-annotations module. Scheduled for removal in Kotlin 2.4.",
-    replaceWith = ReplaceWith("org.jetbrains.kotlin.gradle.ExperimentalWasmDsl")
+    replaceWith = ReplaceWith("org.jetbrains.kotlin.gradle.ExperimentalWasmDsl"),
+    level = DeprecationLevel.ERROR
 )
 annotation class ExperimentalWasmDsl
