9885e48dc5b50b83bab2d5ceeeff1b34c3221cbd
Adam <152864218+adam-enko@users.noreply.github.com>
2025-09-16 07:29:55 +0200

    [KDM] Update Gradle settings plugins
    See https://github.com/Kotlin/dokka/pull/4233
    
    * Update Gradle settings plugins
    
    - Update develocity, common-custom-user-data-gradle-plugin, foojay-resolver-convention.
      (I didn't update foojay-resolver-convention to the latest version, 1.0.0, because it requires Java 17.)
    - Add a task to ensure consistency between version catalog and build-settings-logic.
      (Because the develocity version was out-of-sync)
    
    * add description
    
    * reuse 'actualVersion' val
    
    * downgrade develocity to 3.19
    Original commit: https://github.com/Kotlin/dokka/commit/b679d1b49977882be715fcf7de58f129cb2e5dac

--- libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/build.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-settings-logic/build.gradle.kts
@@ -17,3 +17,75 @@ dependencies {
     implementation(libs.gradlePlugin.gradle.customUserData)
     implementation(libs.gradlePlugin.gradle.foojayToolchains)
 }
+
+
+//region check consistent develocity version
+
+val checkBuildSettingsLogicPluginConsistency by tasks.registering {
+    description = "Check consistency of plugin versions in settings.gradle.kts with version catalog."
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+
+    val customUserDataVersion = libs.versions.gradlePlugin.gradle.customUserData
+    val develocityVersion = libs.versions.gradlePlugin.gradle.develocity
+    val foojayToolchainsVersion = libs.versions.gradlePlugin.gradle.foojayToolchains
+
+    inputs.property("customUserDataVersionCatalogVersion", customUserDataVersion)
+    inputs.property("develocityVersionCatalogVersion", develocityVersion)
+    inputs.property("foojayToolchainsVersionCatalogVersion", foojayToolchainsVersion)
+
+    val settingsGradleKtsInput = layout.projectDirectory.file("settings.gradle.kts")
+    inputs.file(settingsGradleKtsInput)
+        .withPropertyName("settingsGradleKtsInput")
+        .normalizeLineEndings()
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+
+    val projectName = project.displayName
+
+    doLast {
+        val pluginVersionsMap =
+            settingsGradleKtsInput.asFile.useLines { lines ->
+                lines
+                    .dropWhile { it != "plugins {" }
+                    .takeWhile { it != "}" }
+                    .filter { it.startsWith("    id(") }
+                    .associate {
+                        val id = it.substringAfter("id(\"")
+                            .substringBefore("\")")
+                        val version = it.substringAfter("version \"")
+                            .substringBefore("\"")
+                        id to version
+                    }
+            }
+
+        val result = buildString {
+            fun checkVersion(id: String, version: String) {
+                val actualVersion = pluginVersionsMap[id]
+                when {
+                    actualVersion == null ->
+                        appendLine("plugin $id: missing")
+
+                    actualVersion != version ->
+                        appendLine("plugin $id: expected $version, actual $actualVersion")
+                }
+            }
+
+            checkVersion("com.gradle.develocity", develocityVersion.get())
+            checkVersion("com.gradle.common-custom-user-data-gradle-plugin", customUserDataVersion.get())
+            checkVersion("org.gradle.toolchains.foojay-resolver-convention", foojayToolchainsVersion.get())
+        }
+
+        check(result.isEmpty()) {
+            """
+            $projectName - inconsistent plugin versions:
+            ${result.prependIndent()}
+            """.trimIndent()
+        }
+    }
+}
+
+tasks.compileKotlin {
+    // pick a task that always runs to depend on the check, to make sure it always runs
+    dependsOn(checkBuildSettingsLogicPluginConsistency)
+}
+
+//endregion
