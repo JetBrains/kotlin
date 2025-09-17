f12cc1f3184b72aadcb5f7f187fc86cd7ccd46fe
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-09-17 19:17:54 +0200

    FE: Support assigning lambdas to receiver/context function types
    It's been controversially working since 2.0, but was broken
    once ResolveTopLevelLambdasAsSyntheticCallArgument became enabled
    by default.
    
    See KT-81115 and related issues for details.
    
    ^KT-81115 Fixed

--- compiler/fir/analysis-tests/testData/resolve/inference/lambdasWithExplicitParametersViaReceiversFeatureEnabled.kt
+++ compiler/fir/analysis-tests/testData/resolve/inference/lambdasWithExplicitParametersViaReceiversFeatureEnabled.kt
@@ -3,13 +3,13 @@
 // ISSUE: KT-81115
 
 val l1: String.() -> String = fun(x: String) = ""
-val l2: String.() -> String = <!INITIALIZER_TYPE_MISMATCH!>{ x: String -> "" }<!>
+val l2: String.() -> String = { x: String -> "" }
 
 val l3: String.() -> String = fun(x) = x
-val l4: String.() -> String = <!INITIALIZER_TYPE_MISMATCH!>{ <!CANNOT_INFER_VALUE_PARAMETER_TYPE!>x<!> -> x }<!>
+val l4: String.() -> String = { x -> x }
 
 val l5: String.(Int) -> String = fun(x: String, y) = ""
-val l6: String.(Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>{ x: String, <!CANNOT_INFER_VALUE_PARAMETER_TYPE!>y<!> -> "" }<!>
+val l6: String.(Int) -> String = { x: String, y -> "" }
 
 val w1: String.(Int, Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>fun(x: String, y) = ""<!>
 val w2: String.(Int, Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>{ x: String, y -> "" }<!>
--- compiler/fir/analysis-tests/testData/resolve/inference/lambdasWithExplicitParametersWithContexts.kt
+++ compiler/fir/analysis-tests/testData/resolve/inference/lambdasWithExplicitParametersWithContexts.kt
@@ -3,10 +3,10 @@
 // ISSUE: KT-81115
 
 val l1: context(String) (Int) -> String = fun(x: String, y) = ""
-val l2: context(String) (Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>{ x: String, <!CANNOT_INFER_VALUE_PARAMETER_TYPE!>y<!> -> "" }<!>
+val l2: context(String) (Int) -> String = { x: String, y -> "" }
 
 val l3: context(String) Double.(Int) -> String = fun(x: String, y, z: Int) = ""
-val l4: context(String) Double.(Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>{ x: String, <!CANNOT_INFER_VALUE_PARAMETER_TYPE!>y<!>, z: Int -> "" }<!>
+val l4: context(String) Double.(Int) -> String = { x: String, y, z: Int -> "" }
 
 val w1: context(String) Double.(Int, Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>fun(x: String, y, z: Int) = ""<!>
 val w2: context(String) Double.(Int, Int) -> String = <!INITIALIZER_TYPE_MISMATCH!>{ x: String, y, z: Int -> "" }<!>
