bf70bc0d63e989c7d7ada807377a649ef4970ab1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-09-18 11:01:35 +0200

    Swift export: add some simple tests for KT-66838
    (cherry picked from commit f4fdb44b78e2757996a487fb1bb8e1856e2026aa)

--- native/swift/swift-export-standalone-integration-tests/simple/tests-gen/org/jetbrains/kotlin/swiftexport/standalone/test/SwiftExportWithBinaryCompilationTest.java
+++ native/swift/swift-export-standalone-integration-tests/simple/tests-gen/org/jetbrains/kotlin/swiftexport/standalone/test/SwiftExportWithBinaryCompilationTest.java
@@ -230,6 +230,12 @@ public class SwiftExportWithBinaryCompilationTest extends AbstractSwiftExportWit
   }
 
   @Test
+  @TestMetadata("vararg")
+  public void testVararg() {
+    runTest("native/swift/swift-export-standalone-integration-tests/simple/testData/generation/vararg/");
+  }
+
+  @Test
   @TestMetadata("variables")
   public void testVariables() {
     runTest("native/swift/swift-export-standalone-integration-tests/simple/testData/generation/variables/");
