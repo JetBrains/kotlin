2886f89a34f7bc0eb711652432a1804f11ce328c
grigorii.solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2025-09-19 13:06:33 +0300

    [FIR] Treat the `of` overload with an error in return type as suitable

---
+++ compiler/fir/analysis-tests/testData/resolve/collectionLiterals/userTypes/noExplicitReturnTypeRecursion.kt
@@ -0,0 +1,13 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-79330
+// LANGUAGE: +CollectionLiterals
+
+class MyList {
+    companion object {
+        operator fun of(vararg lst: MyList) = id(<!TYPECHECKER_HAS_RUN_INTO_RECURSIVE_PROBLEM!>[MyList()]<!>)
+    }
+}
+
+fun id(x: MyList): MyList = x
+
+/* GENERATED_FIR_TAGS: classDeclaration, companionObject, functionDeclaration, objectDeclaration, operator, vararg */
