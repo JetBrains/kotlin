fbe4ecc84a2391648983d7b8f3cc6c89be09d936
grigorii.solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2025-09-19 14:10:11 +0300

    [Tests] Add diagnostics tests with empty arguments in different calls
    Reproduces KT-79116.

---
+++ compiler/testData/diagnostics/tests/incompleteCode/emptyArgumentsInArrayAccess.kt
@@ -0,0 +1,29 @@
+// FIR_IDENTICAL
+// RUN_PIPELINE_TILL: FRONTEND
+// DIAGNOSTICS: -UNUSED_PARAMETER
+// ISSUE: KT-79116
+
+class Get {
+    operator fun get(a: Int, b: Int, c: Int) = 0
+}
+
+fun test() {
+    val a = 0
+    val b = 0
+    val c = 0
+    val getter = Get()
+    getter[a, <!SYNTAX!>,<!>]
+    getter[a, <!SYNTAX!>,<!><!SYNTAX!><!> ,]
+    <!NO_VALUE_FOR_PARAMETER!>getter[a, <!SYNTAX!>,<!> c]<!>
+    getter[a, b, <!SYNTAX!>,<!>]
+    getter[<!SYNTAX!>,<!><!SYNTAX!><!> ,]
+    getter[<!SYNTAX!>,<!>]
+    getter[<!SYNTAX!>,<!><!SYNTAX!><!> , <!SYNTAX!>,<!>]
+    getter[<!SYNTAX!>,<!><!SYNTAX!><!> , <!SYNTAX!>,<!><!SYNTAX!><!> ,]
+    <!NO_VALUE_FOR_PARAMETER, NO_VALUE_FOR_PARAMETER!>getter[<!SYNTAX!>,<!> b,]<!>
+    <!NO_VALUE_FOR_PARAMETER!>getter[<!SYNTAX!>,<!> b, c]<!>
+    <!NO_VALUE_FOR_PARAMETER!>getter[<!SYNTAX!>,<!> b, c,]<!>
+}
+
+/* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, integerLiteral, localProperty, operator,
+propertyDeclaration */
---
+++ compiler/testData/diagnostics/tests/incompleteCode/emptyArgumentsInArrayLiterals.kt
@@ -0,0 +1,17 @@
+// FIR_IDENTICAL
+// RUN_PIPELINE_TILL: FRONTEND
+// DIAGNOSTICS: -UNUSED_PARAMETER
+// ISSUE: KT-79116
+
+@Repeatable
+annotation class Anno(val arr: IntArray)
+
+@Anno([<!SYNTAX!>,<!>])
+@Anno([<!SYNTAX!>,<!><!SYNTAX!><!>,])
+@Anno([<!SYNTAX!>,<!>2])
+@Anno([1, <!SYNTAX!>,<!>])
+@Anno([1, <!SYNTAX!>,<!> 3])
+fun target() { }
+
+/* GENERATED_FIR_TAGS: annotationDeclaration, collectionLiteral, functionDeclaration, integerLiteral, outProjection,
+primaryConstructor, propertyDeclaration, vararg */
