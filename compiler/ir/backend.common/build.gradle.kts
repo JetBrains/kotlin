ee51e244365cda4e4a4ef5a64af05f7c7a46d0f2
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-09-22 12:18:47 +0200

    [Gradle] Don't expose `:compiler:frontend` from `ir.backend.common`
    ^KT-74763

--- compiler/ir/backend.common/build.gradle.kts
+++ compiler/ir/backend.common/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 dependencies {
     api(project(":compiler:util"))
-    api(project(":compiler:frontend"))
     api(project(":compiler:backend-common"))
     api(project(":compiler:ir.tree"))
     api(project(":compiler:ir.interpreter"))
     api(project(":compiler:ir.serialization.common"))
     api(project(":compiler:ir.validation"))
+    compileOnly(project(":compiler:frontend")) // this dependency is needed because of `IrPluginContext` exposing K1 frontend as deprecated.
     compileOnly(intellijCore())
 
     testImplementation(kotlinTest("junit"))
