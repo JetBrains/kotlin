a9f57e643a6fc4850df30b8ee2069e76a46603cd
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2025-09-25 15:43:44 +0300

    [Tests] KT-72356: cases with expect-actual pair, fake overriden property
    Also cases with annotation with default value, annotation on the second paremeter and .java file
    
    fixup! fixup! [Tests] KT-72356: cases with expect-actual pair, fake overriden property
    
    (cherry picked from commit fdddb902a9e9906b397e96350b6ed85a64c6d7f4)

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/IrJsCodegenBoxTestGenerated.java
@@ -35888,6 +35888,12 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
     @Tag("legacy-frontend")
     public class Serialization {
       @Test
+      @TestMetadata("actualAnnotationOnFakeOverride.kt")
+      public void testActualAnnotationOnFakeOverride() {
+        runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/actualAnnotationOnFakeOverride.kt");
+      }
+
+      @Test
       public void testAllFilesPresentInSerialization() {
         KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/codegen/box/involvesIrInterpreter/serialization"), Pattern.compile("^(.+)\\.kt$"), null, true);
       }
@@ -35905,9 +35911,27 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
       }
 
       @Test
+      @TestMetadata("annotationOnFakeOverrideProperty.kt")
+      public void testAnnotationOnFakeOverrideProperty() {
+        runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationOnFakeOverrideProperty.kt");
+      }
+
+      @Test
+      @TestMetadata("annotationOnFakeOverrideWithJava.kt")
+      public void testAnnotationOnFakeOverrideWithJava() {
+        runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationOnFakeOverrideWithJava.kt");
+      }
+
+      @Test
       @TestMetadata("annotationOnParameterOnFakeOverride.kt")
       public void testAnnotationOnParameterOnFakeOverride() {
         runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationOnParameterOnFakeOverride.kt");
+      }
+
+      @Test
+      @TestMetadata("annotationOnSecondParameterOnFakeOverride.kt")
+      public void testAnnotationOnSecondParameterOnFakeOverride() {
+        runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationOnSecondParameterOnFakeOverride.kt");
       }
 
       @Test
@@ -35926,6 +35950,12 @@ public class IrJsCodegenBoxTestGenerated extends AbstractIrJsCodegenBoxTest {
       @TestMetadata("annotationWithArray.kt")
       public void testAnnotationWithArray() {
         runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationWithArray.kt");
+      }
+
+      @Test
+      @TestMetadata("annotationWithDefaultValueOnFakeOverride.kt")
+      public void testAnnotationWithDefaultValueOnFakeOverride() {
+        runTest("compiler/testData/codegen/box/involvesIrInterpreter/serialization/annotationWithDefaultValueOnFakeOverride.kt");
       }
 
       @Test
