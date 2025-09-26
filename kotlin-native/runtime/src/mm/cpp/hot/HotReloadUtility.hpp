3fdd4088175e1a931815a69716dd5e65ecb124cd
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2025-09-27 00:01:08 +0200

    refactor HotReloadUtility: enhance log handling, add type utilities, and update logging logic

--- kotlin-native/runtime/src/mm/cpp/hot/HotReloadUtility.hpp
+++ kotlin-native/runtime/src/mm/cpp/hot/HotReloadUtility.hpp
@@ -5,7 +5,6 @@
 #ifndef HOTRELOADUTILITY_HPP
 #define HOTRELOADUTILITY_HPP
 
-#include <cstdlib>
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -15,12 +14,51 @@
 
 namespace kotlin::hot::utility {
 
-enum class LogLevel { DEBUG, INFO, WARN, ERROR };
+static constexpr const char* kTypeNames[] = {
+        "__Invalid",
+        "kotlin.Any",
+        "kotlin.Char",
+        "kotlin.Short",
+        "kotlin.Int",
+        "kotlin.Long",
+        "kotlin.Float",
+        "kotlin.Double",
+        "kotlin.native.internal.NativePtr",
+        "kotlin.Boolean",
+        "kotlinx.cinterop.Vector128"};
 
-static bool IsLogEnabled = false;
+static constexpr int kRuntimeTypeSize[] = {
+        -1, // INVALID
+        sizeof(ObjHeader*), // OBJECT
+        1, // INT8
+        2, // INT16
+        4, // INT32
+        8, // INT64
+        4, // FLOAT32
+        8, // FLOAT64
+        sizeof(void*), // NATIVE_PTR
+        1, // BOOLEAN
+        16 // VECTOR128
+};
 
-static void log(const std::string& message, const LogLevel level = LogLevel::INFO) {
-    if (!IsLogEnabled) return;
+/**
+ * Defines logging levels for the hot reload system using bit flags.
+ * Multiple log levels can be combined using bitwise operations.
+ * Example: DEBUG + INFO = 6 (00110 in binary)
+ */
+enum class LogLevel : uint8_t {
+    NONE = 0, ///< Disable all logging
+    DEBUG = 1 << 1, ///< Debug level logging (2)
+    INFO = 1 << 2, ///< Information level logging (4)
+    WARN = 1 << 3, ///< Warning level logging (8)
+    ERR = 1 << 4 ///< Error level logging (16)
+};
+
+static auto CurrentLogLevel = LogLevel::NONE;
+
+inline void log(const std::string& message, const LogLevel level = LogLevel::INFO) {
+    // if (!(static_cast<uint8_t>(CurrentLogLevel) & static_cast<uint8_t>(level)))
+    //     return;
 
     auto now = std::chrono::system_clock::now();
     auto timestamp = std::chrono::system_clock::to_time_t(now);
@@ -38,8 +76,11 @@ static void log(const std::string& message, const LogLevel level = LogLevel::INF
         case LogLevel::WARN:
             levelStr = "WARN";
             break;
-        case LogLevel::ERROR:
+        case LogLevel::ERR:
             levelStr = "ERROR";
+            break;
+        default:
+            levelStr = "UNKNW";
             break;
     }
 
@@ -54,19 +95,61 @@ static void log(const std::string& message, const LogLevel level = LogLevel::INF
     }
 }
 
-static void InitializeHotReloadLogs() {
-    if (const char* isLogEnabledEnv = std::getenv(ENV_LOG_PARAM); isLogEnabledEnv != nullptr) {
-        const auto isLogEnabled = std::strtol(isLogEnabledEnv, nullptr, 10) == 1L;
-        IsLogEnabled = isLogEnabled;
-    }
+inline void initializeHotReloadLogs() {
+    const char* logEnabledEnv = std::getenv(ENV_LOG_PARAM);
+    if (logEnabledEnv == nullptr) return;
+    const auto level = static_cast<LogLevel>(strtol(logEnabledEnv, nullptr, 10));
+    CurrentLogLevel = level;
 }
 
 /// Assuming the className is a fully-qualified-name
-inline std::string KotlinClassNameAsTypeInfoName(const std::string& className) {
+inline std::string kotlinClassNameAsTypeInfoName(const std::string& className) {
     return "kclass:" + className;
 }
 
+std::string field2String(const char* fieldName, const uint8_t* fieldValue, const Konan_RuntimeType fieldType) {
+    std::stringstream ss;
+    ss << fieldName << ":" << kTypeNames[fieldType] << " = ";
+
+    switch (fieldType) {
+        case RT_INVALID:
+            ss << "???";
+            break;
+        case RT_OBJECT:
+            ss << "ObjHeader*";
+            break;
+        case RT_INT8:
+            ss << *(reinterpret_cast<const int8_t*>(fieldValue));
+            break;
+        case RT_INT16:
+            ss << *(reinterpret_cast<const int16_t*>(fieldValue));
+            break;
+        case RT_INT32:
+            ss << *(reinterpret_cast<const int32_t*>(fieldValue));
+            break;
+        case RT_INT64:
+            ss << *(reinterpret_cast<const int64_t*>(fieldValue));
+            break;
+        case RT_FLOAT32:
+            ss << *(reinterpret_cast<const float*>(fieldValue));
+            break;
+        case RT_FLOAT64:
+            ss << *(reinterpret_cast<const double*>(fieldValue));
+            break;
+        case RT_NATIVE_PTR:
+            ss << *(reinterpret_cast<const uintptr_t*>(fieldValue));
+            break;
+        case RT_BOOLEAN:
+            ss << *(reinterpret_cast<const bool*>(fieldValue));
+            break;
+        case RT_VECTOR128:
+            ss << "vec128";
+            break;
+    }
+
+    return ss.str();
 }
 
+}; // namespace kotlin::hot::utility
 
-#endif //HOTRELOADUTILITY_HPP
+#endif // HOTRELOADUTILITY_HPP
