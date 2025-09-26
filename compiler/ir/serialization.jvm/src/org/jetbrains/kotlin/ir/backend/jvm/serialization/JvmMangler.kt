142f3fcb77426d4634dd94fdbc4998c45f62cba3
Nikita Nazarov <nikitanazarov@google.com>
2025-09-26 11:36:24 +0000

    Fix b/447548985

--- compiler/ir/serialization.jvm/src/org/jetbrains/kotlin/ir/backend/jvm/serialization/JvmMangler.kt
+++ compiler/ir/serialization.jvm/src/org/jetbrains/kotlin/ir/backend/jvm/serialization/JvmMangler.kt
@@ -15,12 +15,17 @@ import org.jetbrains.kotlin.backend.common.serialization.mangle.descriptor.Descr
 import org.jetbrains.kotlin.backend.common.serialization.mangle.ir.IrBasedKotlinManglerImpl
 import org.jetbrains.kotlin.backend.common.serialization.mangle.ir.IrExportCheckerVisitor
 import org.jetbrains.kotlin.backend.common.serialization.mangle.ir.IrMangleComputer
+import org.jetbrains.kotlin.builtins.KotlinBuiltIns
+import org.jetbrains.kotlin.builtins.PrimitiveType
 import org.jetbrains.kotlin.descriptors.CallableDescriptor
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.descriptors.PropertyGetterDescriptor
+import org.jetbrains.kotlin.descriptors.annotations.CompositeAnnotations
 import org.jetbrains.kotlin.descriptors.containingPackage
 import org.jetbrains.kotlin.idea.MainFunctionDetector
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
@@ -33,20 +38,180 @@ import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.getPackageFragment
 import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.ir.util.parentClassOrNull
+import org.jetbrains.kotlin.load.java.JSPECIFY_NULL_MARKED_ANNOTATION_FQ_NAME
 import org.jetbrains.kotlin.load.java.JvmAnnotationNames
 import org.jetbrains.kotlin.load.java.descriptors.JavaCallableMemberDescriptor
 import org.jetbrains.kotlin.load.java.descriptors.JavaClassDescriptor
 import org.jetbrains.kotlin.load.java.lazy.descriptors.isJavaField
+import org.jetbrains.kotlin.load.java.typeEnhancement.ENHANCED_NULLABILITY_ANNOTATIONS
 import org.jetbrains.kotlin.load.java.typeEnhancement.hasEnhancedNullability
-import org.jetbrains.kotlin.load.kotlin.computeJvmDescriptor
+import org.jetbrains.kotlin.load.kotlin.JvmType
+import org.jetbrains.kotlin.load.kotlin.JvmTypeFactory
+import org.jetbrains.kotlin.load.kotlin.TypeMappingConfiguration
+import org.jetbrains.kotlin.load.kotlin.TypeMappingMode
+import org.jetbrains.kotlin.load.kotlin.mapType
 import org.jetbrains.kotlin.load.kotlin.signature
 import org.jetbrains.kotlin.load.kotlin.signatures
 import org.jetbrains.kotlin.resolve.DescriptorUtils
+import org.jetbrains.kotlin.resolve.jvm.JvmClassName
+import org.jetbrains.kotlin.resolve.jvm.JvmPrimitiveType
 import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.SimpleType
+import org.jetbrains.kotlin.types.TypeUtils
 import org.jetbrains.kotlin.types.checker.SimpleClassicTypeSystemContext
+import org.jetbrains.kotlin.types.typeUtil.replaceAnnotations
 
 fun String.isKotlinPackage(): Boolean {
     return this == "kotlin" || startsWith("kotlin.")
+}
+
+private object JvmTypeFactoryImpl : JvmTypeFactory<JvmType> {
+    private val BOOLEAN = JvmType.Primitive(JvmPrimitiveType.BOOLEAN)
+    private val CHAR = JvmType.Primitive(JvmPrimitiveType.CHAR)
+    private val BYTE = JvmType.Primitive(JvmPrimitiveType.BYTE)
+    private val SHORT = JvmType.Primitive(JvmPrimitiveType.SHORT)
+    private val INT = JvmType.Primitive(JvmPrimitiveType.INT)
+    private val FLOAT = JvmType.Primitive(JvmPrimitiveType.FLOAT)
+    private val LONG = JvmType.Primitive(JvmPrimitiveType.LONG)
+    private val DOUBLE = JvmType.Primitive(JvmPrimitiveType.DOUBLE)
+
+    override fun boxType(possiblyPrimitiveType: JvmType) =
+        when {
+            possiblyPrimitiveType is JvmType.Primitive && possiblyPrimitiveType.jvmPrimitiveType != null ->
+                createObjectType(
+                    JvmClassName.byFqNameWithoutInnerClasses(possiblyPrimitiveType.jvmPrimitiveType!!.wrapperFqName).internalName
+                )
+            else -> possiblyPrimitiveType
+        }
+
+    override fun createFromString(representation: String): JvmType {
+        assert(representation.isNotEmpty()) { "empty string as JvmType" }
+        val firstChar = representation[0]
+
+        JvmPrimitiveType.values().firstOrNull { it.desc[0] == firstChar }?.let {
+            return JvmType.Primitive(it)
+        }
+
+        return when (firstChar) {
+            'V' -> JvmType.Primitive(null)
+            '[' -> JvmType.Array(createFromString(representation.substring(1)))
+            else -> {
+                assert(firstChar == 'L' && representation.endsWith(';')) {
+                    "Type that is not primitive nor array should be Object, but '$representation' was found"
+                }
+
+                JvmType.Object(representation.substring(1, representation.length - 1))
+            }
+        }
+    }
+
+    override fun createPrimitiveType(primitiveType: PrimitiveType): JvmType =
+        when (primitiveType) {
+            PrimitiveType.BOOLEAN -> BOOLEAN
+            PrimitiveType.CHAR -> CHAR
+            PrimitiveType.BYTE -> BYTE
+            PrimitiveType.SHORT -> SHORT
+            PrimitiveType.INT -> INT
+            PrimitiveType.FLOAT -> FLOAT
+            PrimitiveType.LONG -> LONG
+            PrimitiveType.DOUBLE -> DOUBLE
+        }
+
+    override fun createObjectType(internalName: String): JvmType.Object =
+        JvmType.Object(internalName)
+
+    override fun toString(type: JvmType): String =
+        when (type) {
+            is JvmType.Array -> "[" + toString(type.elementType)
+            is JvmType.Primitive -> type.jvmPrimitiveType?.desc ?: "V"
+            is JvmType.Object -> "L" + type.internalName + ";"
+        }
+
+    override val javaLangClassType: JvmType
+        get() = createObjectType("java/lang/Class")
+
+}
+
+internal object TypeMappingConfigurationImpl : TypeMappingConfiguration<JvmType> {
+    override fun commonSupertype(types: Collection<KotlinType>): KotlinType {
+        throw AssertionError("There should be no intersection type in existing descriptors, but found: " + types.joinToString())
+    }
+
+    override fun getPredefinedTypeForClass(classDescriptor: ClassDescriptor): JvmType? = null
+    override fun getPredefinedInternalNameForClass(classDescriptor: ClassDescriptor): String? = null
+
+    override fun preprocessType(kotlinType: KotlinType): KotlinType? {
+        return null
+//        type.
+    }
+
+    override fun processErrorType(kotlinType: KotlinType, descriptor: ClassDescriptor) {
+        // DO nothing
+    }
+}
+
+fun StringBuilder.appendErasedType(type: KotlinType) {
+    append(type.mapToJvmType())
+}
+
+fun KotlinType.mapToJvmType(): JvmType =
+    mapType(this, JvmTypeFactoryImpl, TypeMappingMode.DEFAULT, TypeMappingConfigurationImpl, descriptorTypeWriter = null)
+
+fun hasVoidReturnType(descriptor: CallableDescriptor): Boolean {
+    if (descriptor is ConstructorDescriptor) return true
+    return KotlinBuiltIns.isUnit(descriptor.returnType!!) && !TypeUtils.isNullableType(descriptor.returnType!!)
+            && descriptor !is PropertyGetterDescriptor
+}
+
+fun FunctionDescriptor.computeJvmDescriptor(withReturnType: Boolean = true, withName: Boolean = true): String = buildString {
+    if (withName) {
+        append(if (this@computeJvmDescriptor is ConstructorDescriptor) "<init>" else name.asString())
+    }
+
+    append("(")
+
+    extensionReceiverParameter?.let {
+        appendErasedType(it.type)
+    }
+
+    for (parameter in valueParameters) {
+        appendErasedType(parameter.type)
+    }
+
+    append(")")
+
+    if (withReturnType) {
+        if (hasVoidReturnType(this@computeJvmDescriptor)) {
+            append("V")
+        } else {
+            val returnType = returnType!!
+            // Workaround for b/447548985
+            // Given a class annotated with `@NullMarked`, there will be such difference between K1 and K2 signatures for
+            // functions containing primitive types in arguments/return value:
+            // ```
+            // kotlinjavainterop/NullMarkedClass.getNonNullInteger()I // K2
+            // kotlinjavainterop/NullMarkedClass.getNonNullInteger()Ljava/lang/Integer; // K1
+            // ```
+            // The reason is that in K1 the return type is annotated with `@EnhancedNullability`, but in K2 it isn't.
+            // The workaround forces adding `@EnhancedNullability` for such types so that they are always boxed
+            // when computing function's signature.
+            if (returnType is SimpleType &&
+                KotlinBuiltIns.isPrimitiveType(returnType) &&
+                !returnType.hasEnhancedNullability() &&
+                containingDeclaration.annotations.hasAnnotation(JSPECIFY_NULL_MARKED_ANNOTATION_FQ_NAME)
+            ) {
+                val newType = returnType.replaceAnnotations(
+                    CompositeAnnotations(
+                        returnType.annotations,
+                        ENHANCED_NULLABILITY_ANNOTATIONS
+                    )
+                )
+                appendErasedType(newType)
+            } else {
+                appendErasedType(returnType)
+            }
+        }
+    }
 }
 
 fun CallableDescriptor.computeJvmSignature(): String? = signatures {
