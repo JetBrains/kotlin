6f87d17887a5e28f067f5e241571b21df3bde03d
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2025-09-27 18:50:35 +0200

    refactor HotReload: remove unused `forceReloadOf` method and clean up comments

--- kotlin-native/runtime/src/main/kotlin/kotlin/native/runtime/HotReload.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/native/runtime/HotReload.kt
@@ -11,7 +11,6 @@ import kotlin.reflect.KClass
 
 @NativeRuntimeApi
 public object HotReload {
-
     /**
      * Perform hot-code reload in a stop-the-world fashion if there is an upcoming reloading request.
      * Note that this function is invoked implicitly in safe-points, and it should be used for test-only.
@@ -19,18 +18,4 @@ public object HotReload {
     @GCUnsafeCall("Kotlin_native_internal_HotReload_perform")
     @Escapes.Nothing
     public external fun performIfNeeded()
-
-    /**
-     * Forces the runtime to reload a new type into the place of an existing type. This is used to replace the old
-     * type with a new one, potentially reflecting changes in the structure or attributes of the class.
-     *
-     * @param oldType The existing class type that needs to be replaced.
-     * @param newType The new class type that will replace the old one.
-     *
-     * @param T1 The type parameter representing the old class type.
-     * @param T2 The type parameter representing the new class type.
-     */
-    @GCUnsafeCall("Kotlin_native_internal_HotReload_forceReloadOf")
-    @Escapes.Nothing
-    public external fun <T1 : Any, T2: Any> forceReloadOf(oldType: KClass<T1>, newType: KClass<T2>)
 }
