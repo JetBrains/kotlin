48a5dfa1dbbd49f45f0156d754863c36754d7b20
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2025-09-29 16:30:31 +0200

    [Maven] Add test for KT-81218
    (cherry picked from commit bb923b30a4d828f7c6d90d667f37d523f998fa55)

--- libraries/tools/kotlin-maven-plugin-test/src/test/java/org.jetbrains.kotlin.maven/IncrementalCompilationIT.java
+++ libraries/tools/kotlin-maven-plugin-test/src/test/java/org.jetbrains.kotlin.maven/IncrementalCompilationIT.java
@@ -1,15 +1,34 @@
 package org.jetbrains.kotlin.maven;
 
 import org.jetbrains.annotations.NotNull;
+import org.junit.Assume;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.io.File;
 
+@RunWith(Parameterized.class)
 public class IncrementalCompilationIT extends MavenITBase {
+    private final MavenProject.ExecutionStrategy executionStrategy;
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][] {
+                {MavenProject.ExecutionStrategy.IN_PROCESS},
+                {MavenProject.ExecutionStrategy.DAEMON},
+        };
+    }
+
+    public IncrementalCompilationIT(MavenProject.ExecutionStrategy executionStrategy) {
+        Assume.assumeFalse("KT-81218", executionStrategy == MavenProject.ExecutionStrategy.IN_PROCESS);
+        this.executionStrategy = executionStrategy;
+    }
+
     @Test
     public void testSimpleCompile() throws Exception {
         MavenProject project = new MavenProject("kotlinSimple");
-        project.exec("package", "-X")
+        project.exec(executionStrategy, "package", "-X")
                .succeeded()
                .filesExist(kotlinSimpleOutputPaths())
                .compiledKotlin("src/main/kotlin/A.kt", "src/main/kotlin/useA.kt", "src/main/kotlin/Dummy.kt");
@@ -30,9 +49,9 @@ public class IncrementalCompilationIT extends MavenITBase {
     @Test
     public void testNoChanges() throws Exception {
         MavenProject project = new MavenProject("kotlinSimple");
-        project.exec("package");
+        project.exec(executionStrategy, "package");
 
-        project.exec("package", "-X")
+        project.exec(executionStrategy, "package", "-X")
                .succeeded()
                .filesExist(kotlinSimpleOutputPaths())
                .compiledKotlin();
@@ -41,19 +60,19 @@ public class IncrementalCompilationIT extends MavenITBase {
     @Test
     public void testCompileError() throws Exception {
         MavenProject project = new MavenProject("kotlinSimple");
-        project.exec("package");
+        project.exec(executionStrategy, "package");
 
         File aKt = project.file("src/main/kotlin/A.kt");
         String original = "class A";
         String replacement = "private class A";
         MavenTestUtils.replaceFirstInFile(aKt, original, replacement);
 
-        project.exec("package")
+        project.exec(executionStrategy, "package")
                .failed()
                .contains("Cannot access 'class A : Any': it is private in file");
 
         MavenTestUtils.replaceFirstInFile(aKt, replacement, original);
-        project.exec("package", "-X")
+        project.exec(executionStrategy, "package", "-X")
                .succeeded()
                .filesExist(kotlinSimpleOutputPaths())
                .compiledKotlin("src/main/kotlin/A.kt", "src/main/kotlin/useA.kt");
@@ -63,12 +82,12 @@ public class IncrementalCompilationIT extends MavenITBase {
     @Test
     public void testFunctionVisibilityChanged() throws Exception {
         MavenProject project = new MavenProject("kotlinSimple");
-        project.exec("package");
+        project.exec(executionStrategy, "package");
 
         File aKt = project.file("src/main/kotlin/A.kt");
         MavenTestUtils.replaceFirstInFile(aKt, "fun foo", "internal fun foo");
 
-        project.exec("package", "-X")
+        project.exec(executionStrategy, "package", "-X")
                .succeeded()
                .filesExist(kotlinSimpleOutputPaths())
                .compiledKotlin("src/main/kotlin/A.kt", "src/main/kotlin/useA.kt");
@@ -79,12 +98,12 @@ public class IncrementalCompilationIT extends MavenITBase {
     @Test
     public void testJavaChanged() throws Exception {
         MavenProject project = new MavenProject("kotlinSimple");
-        project.exec("package");
+        project.exec(executionStrategy, "package");
 
         File aKt = project.file("src/main/java/JavaUtil.java");
         MavenTestUtils.replaceFirstInFile(aKt, "CONST = 0", "CONST = 1");
 
-        project.exec("package", "-X")
+        project.exec(executionStrategy, "package", "-X")
                 .succeeded()
                 .filesExist(kotlinSimpleOutputPaths())
                 .compiledKotlin("src/main/kotlin/A.kt");
