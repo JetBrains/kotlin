30368a3d2988ad02e16117cf79f6834155f47456
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-09-29 10:28:20 +0200

    [IR] Reproduce KT-81241
    #KT-81241
    
    (cherry picked from commit 1e603fbee0bdbbcdf157b4291db8c7e6a51f7f2a)

--- wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxWithInlinedFunInKlibTestGenerated.java
+++ wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxWithInlinedFunInKlibTestGenerated.java
@@ -3810,6 +3810,18 @@ public class FirWasmJsCodegenBoxWithInlinedFunInKlibTestGenerated extends Abstra
         runTest("compiler/testData/codegen/box/bridges/typeParameterInExtensionReceiver.kt");
       }
 
+      @Test
+      @TestMetadata("varargBridge1.kt")
+      public void testVarargBridge1() {
+        runTest("compiler/testData/codegen/box/bridges/varargBridge1.kt");
+      }
+
+      @Test
+      @TestMetadata("varargBridge2.kt")
+      public void testVarargBridge2() {
+        runTest("compiler/testData/codegen/box/bridges/varargBridge2.kt");
+      }
+
       @Nested
       @TestMetadata("compiler/testData/codegen/box/bridges/declarationDifferentSignature")
       @TestDataPath("$PROJECT_ROOT")
