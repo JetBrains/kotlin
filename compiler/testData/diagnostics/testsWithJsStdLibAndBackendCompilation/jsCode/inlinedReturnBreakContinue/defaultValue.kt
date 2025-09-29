ca59ec624bd2039f5adb7fbccc6eb474316153e5
Brian Norman <brian.norman@jetbrains.com>
2025-09-29 08:47:11 -0500

    fixup! [FIR] Move FirUnusedExpressionChecker to the default checkers list

--- compiler/testData/diagnostics/testsWithJsStdLibAndBackendCompilation/jsCode/inlinedReturnBreakContinue/defaultValue.kt
+++ compiler/testData/diagnostics/testsWithJsStdLibAndBackendCompilation/jsCode/inlinedReturnBreakContinue/defaultValue.kt
@@ -1,5 +1,5 @@
 // FIR_IDENTICAL
-// DIAGNOSTICS: -NOTHING_TO_INLINE
+// DIAGNOSTICS: -NOTHING_TO_INLINE -UNUSED_EXPRESSION
 // LANGUAGE: +IrIntraModuleInlinerBeforeKlibSerialization +IrCrossModuleInlinerBeforeKlibSerialization
 inline fun foo1(bar: () -> Unit = { 1 }) = js(<!JS_CODE_CAPTURES_INLINABLE_FUNCTION_WARNING!>"bar()"<!>)
 
