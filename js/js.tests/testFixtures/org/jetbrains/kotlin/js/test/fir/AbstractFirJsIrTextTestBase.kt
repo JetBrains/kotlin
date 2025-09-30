e2e845269048011650d8e4872693eef3257a2e1d
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-09-30 12:16:23 +0200

    [test] Merge AbstractJsIrTextTestBase and AbstractFirJsIrTextTestBase
    The latter was the only inheritor of the former.

--- js/js.tests/testFixtures/org/jetbrains/kotlin/js/test/fir/AbstractFirJsIrTextTestBase.kt
+++ js/js.tests/testFixtures/org/jetbrains/kotlin/js/test/fir/AbstractFirJsIrTextTestBase.kt
@@ -1,18 +1,16 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.js.test.fir
 
 import org.jetbrains.kotlin.config.LanguageFeature
-import org.jetbrains.kotlin.js.test.converters.Fir2IrCliWebFacade
-import org.jetbrains.kotlin.js.test.converters.FirCliWebFacade
-import org.jetbrains.kotlin.js.test.converters.FirKlibSerializerCliWebFacade
-import org.jetbrains.kotlin.js.test.converters.JsIrDeserializerFacade
-import org.jetbrains.kotlin.js.test.ir.AbstractJsIrTextTestBase
+import org.jetbrains.kotlin.js.test.converters.*
+import org.jetbrains.kotlin.platform.js.JsPlatforms
 import org.jetbrains.kotlin.test.Constructor
 import org.jetbrains.kotlin.test.FirParser
+import org.jetbrains.kotlin.test.TargetBackend
 import org.jetbrains.kotlin.test.backend.handlers.AbstractKlibAbiDumpBeforeInliningSavingHandler
 import org.jetbrains.kotlin.test.backend.handlers.FirJsKlibAbiDumpBeforeInliningSavingHandler
 import org.jetbrains.kotlin.test.backend.ir.IrBackendInput
@@ -22,13 +20,20 @@ import org.jetbrains.kotlin.test.configuration.additionalK2ConfigurationForIrTex
 import org.jetbrains.kotlin.test.directives.KlibAbiConsistencyDirectives.CHECK_SAME_ABI_AFTER_INLINING
 import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives.LANGUAGE
 import org.jetbrains.kotlin.test.frontend.fir.FirOutputArtifact
-import org.jetbrains.kotlin.test.model.Frontend2BackendConverter
-import org.jetbrains.kotlin.test.model.FrontendFacade
-import org.jetbrains.kotlin.test.model.FrontendKind
-import org.jetbrains.kotlin.test.model.FrontendKinds
+import org.jetbrains.kotlin.test.model.*
 import org.jetbrains.kotlin.test.runners.codegen.FirPsiCodegenTest
+import org.jetbrains.kotlin.test.runners.ir.AbstractNonJvmIrTextTest
+import org.jetbrains.kotlin.test.services.LibraryProvider
+import org.jetbrains.kotlin.test.services.configuration.CommonEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.configuration.JsFirstStageEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.configuration.JsSecondStageEnvironmentConfigurator
 
-abstract class AbstractFirJsIrTextTestBase(private val parser: FirParser) : AbstractJsIrTextTestBase<FirOutputArtifact>() {
+abstract class AbstractFirJsIrTextTestBase(
+    private val parser: FirParser
+) : AbstractNonJvmIrTextTest<FirOutputArtifact>(JsPlatforms.defaultJsPlatform, TargetBackend.JS_IR) {
+
+    override val preSerializerFacade: Constructor<IrPreSerializationLoweringFacade<IrBackendInput>>
+        get() = ::JsIrPreSerializationLoweringFacade
 
     override val frontend: FrontendKind<*>
         get() = FrontendKinds.FIR
@@ -60,6 +65,16 @@ abstract class AbstractFirJsIrTextTestBase(private val parser: FirParser) : Abst
                 )
             }
         }
+    }
+
+    final override fun TestConfigurationBuilder.applyConfigurators() {
+        useConfigurators(
+            ::CommonEnvironmentConfigurator,
+            ::JsFirstStageEnvironmentConfigurator,
+            ::JsSecondStageEnvironmentConfigurator,
+        )
+
+        useAdditionalService(::LibraryProvider)
     }
 }
 
