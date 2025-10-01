ea20d2ca76ebf61fa1adb45d823f6a908c27d55b
Wojciech Kaliciński <wojciech.kalicinski@jetbrains.com>
2025-10-01 14:06:28 +0200

    Fix update-verification-metadata.sh on Linux

--- scripts/update-verification-metadata.sh
+++ scripts/update-verification-metadata.sh
@@ -3,6 +3,6 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' -e '/<components>/,/<\/components>/d' gradle/verification-metadata.xml
 else
-  sed -i -e '' 'r/<components>/,/<\/components>/d' gradle/verification-metadata.xml
+  sed -i -e '/<components>/,/<\/components>/d' gradle/verification-metadata.xml
 fi
 ./gradlew -i --write-verification-metadata sha256,md5 -Pkotlin.native.enabled=true resolveDependencies
