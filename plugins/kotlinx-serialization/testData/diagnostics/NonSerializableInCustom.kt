40e18ee5da80faf98500db11b009dc98afcada6c
Leonid Startsev <leonid.startsev@jetbrains.com>
2025-10-03 12:53:46 +0200

    [kx.serialization] Prepare test for cases where class is not serializable,
    but is used as a type argument for a type/property serializable with custom serializer.
    
    Apparently, this case was not properly covered by K2 diagnostic.
    It also emulates well the situation where the class may have private serializer
    which is later removed.
    
    #KT-81348
    
    (cherry picked from commit 468fa4316b0e540e0e0e21428edadbf33c24ad1a)

---
+++ plugins/kotlinx-serialization/testData/diagnostics/NonSerializableInCustom.kt
@@ -0,0 +1,36 @@
+// WITH_STDLIB
+
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+
+class NonSerializable
+
+class Box<T>
+
+class BoxSerializer<T>(val element: KSerializer<T>): KSerializer<Box<T>> {
+    override val descriptor: SerialDescriptor
+        get() = TODO()
+
+    override fun serialize(encoder: Encoder, value: Box<T>) = TODO()
+
+    override fun deserialize(decoder: Decoder): Box<T> = TODO()
+}
+
+class BoxSerializerArgless(): KSerializer<Box<Any>> {
+    override val descriptor: SerialDescriptor
+        get() = TODO()
+
+    override fun serialize(encoder: Encoder, value: Box<Any>) = TODO()
+
+    override fun deserialize(decoder: Decoder): Box<Any> = TODO()
+}
+
+@Serializable
+class TestCase(@Serializable(BoxSerializer::class) val box: Box<<!SERIALIZER_NOT_FOUND!>NonSerializable<!>>)
+
+@Serializable
+class TestCase2(@Serializable(BoxSerializerArgless::class) val box: Box<<!SERIALIZER_NOT_FOUND!>NonSerializable<!>>)
+
+@Serializable
+class TestCase3(@Serializable(BoxSerializer::class) val box: Box<List<<!SERIALIZER_NOT_FOUND!>NonSerializable<!>>>)
