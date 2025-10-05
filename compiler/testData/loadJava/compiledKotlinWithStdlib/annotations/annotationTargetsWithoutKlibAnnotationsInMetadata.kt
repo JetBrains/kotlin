adb39f8d2afacaac9a0f3fc314bc5e23bc65c79b
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2025-10-05 17:06:15 +0200

    [Test] Test reading/writing annotations to common metadata in KLIB compilers
    ^KT-64237
    
    (cherry picked from commit 077919a11c1d6de0fba883bb8f8e668a56f6e18d)

---
+++ compiler/testData/loadJava/compiledKotlinWithStdlib/annotations/annotationTargetsWithoutKlibAnnotationsInMetadata.kt
@@ -0,0 +1,66 @@
+// TARGET_BACKEND: JS_IR, WASM
+// PLATFORM_DEPENDANT_METADATA
+// LANGUAGE: -KlibAnnotationsInMetadata
+
+package test
+
+import kotlin.annotation.AnnotationTarget.*
+
+@Target(
+    CLASS,
+    TYPE_PARAMETER,
+    PROPERTY,
+    FIELD,
+    LOCAL_VARIABLE,
+    VALUE_PARAMETER,
+    CONSTRUCTOR,
+    FUNCTION,
+    PROPERTY_GETTER,
+    PROPERTY_SETTER,
+    TYPE,
+    TYPEALIAS,
+)
+
+@Repeatable
+annotation class A(val s: String)
+
+@A("class-1")
+@A("class-2")
+class C<@A("class-type-param") T> @A("primary-ctor") constructor(
+    @property:A("ctor-property") @param:A("ctor-param") val p: Int
+) {
+    @A("secondary-ctor") constructor() : this(0)
+
+    @A("property")
+    @field:A("field")
+    @get:A("getter")
+    @set:A("setter")
+    @setparam:A("setparam-2")
+    var q: Int = 1
+        set(@A("setparam-1") value) {}
+
+    @A("fun")
+    fun <@A("fun-type-param") T> f(@A("fun-param-1") @A("fun-param-2") r: Any): @A("return-type") Unit {
+        @A("local-delegated-property-in-class")
+        val ldp: Int by lazy { 1 }
+    }
+
+    @A("nested-class") class Nested
+}
+
+@A("typealias")
+typealias Z = String
+
+fun topLevel() {
+    @A("local-delegated-property-in-file")
+    val ldp: Int by lazy { 2 }
+}
+
+fun @receiver:A("fun-receiver") Any.ff() {}
+
+@delegate:A("delegate")
+val @receiver:A("property-receiver") Any.pp: Int by lazy { 3 }
+
+enum class E {
+    @A("enum-entry") ENTRY,
+}
