907585ecc5c8f1b8be5403b421164a938590cf6e
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2025-10-06 17:48:52 +0200

    [LL] Execute session invalidation in a non-cancellable section
    - To avoid cache consistency issues, we have to ensure that session
      invalidation cannot be interrupted. A non-cancellable section avoids
      *most* process canceled exceptions, including the
      `ReadAction.CannotReadException` that was observed (see the comments
      of KT-78994).
    - There are edge cases where process canceled exceptions can still
      occur, but the logic of session invalidation is limited, so they might
      never occur. To make sure, we can keep the control flow error logging
      in `performInvalidation` for some more time.
    
    ^KT-80911 fixed
    ^KT-78994
    
    (cherry picked from commit 60453bc71325e8b7f58a6f5ddcbd4b7ee53782b8)

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirSessionCacheStorageInvalidator.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirSessionCacheStorageInvalidator.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.analysis.low.level.api.fir.sessions
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.analysis.api.platform.analysisMessageBus
 import org.jetbrains.kotlin.analysis.api.platform.modification.KotlinCodeFragmentContextModificationEvent
@@ -208,7 +209,7 @@ class LLFirSessionCacheStorageInvalidator(
     }
 
     /**
-     * Ensures that the invalidated [block] does not run concurrently.
+     * Ensures that the invalidated [block] does not run concurrently and cannot be canceled.
      *
      * Invalidation caused by a listener only happens in a write action. However, `KaFirCacheCleaner` can perform immediate invalidation
      * from outside one if there are no ongoing analyses. There might happen a situation when both an invalidation listener and the
@@ -216,14 +217,23 @@ class LLFirSessionCacheStorageInvalidator(
      *
      * The synchronization protects concurrent-unsafe cleanup in [SessionStorage].
      */
-    private inline fun performInvalidation(block: () -> Unit) {
+    private inline fun performInvalidation(crossinline block: () -> Unit) {
         synchronized(this) {
             // Any exception from session invalidation is an error that should be logged (even control flow exceptions). Since an exception
             // interrupts session invalidation, session caches might be left in an inconsistent state. See KT-78994.
             //
             // This is temporary code to gather more diagnostic data.
             try {
-                block()
+                // Cancellation exceptions can bring session caches into an inconsistent state (see KT-78994). Conceptually, session
+                // invalidation is a critical operation that must not be interrupted. Historically, this was not an issue because session
+                // invalidation was performed only in write actions. However, with low-memory cache cleanup, session invalidation can be
+                // executed in a read action.
+                //
+                // Note that this does not prevent all cancellation exceptions, for example, when awaiting on an external cancellable
+                // resource. However, it covers all known cases of such exceptions occurring during session invalidation.
+                ProgressManager.getInstance().executeNonCancelableSection {
+                    block()
+                }
             } catch (t: Throwable) {
                 LOG.error("Exception from LL FIR session invalidation!", t)
                 throw t
