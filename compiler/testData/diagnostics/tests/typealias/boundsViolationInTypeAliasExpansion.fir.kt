02bc34d5921e2c0bbbded6fec264350d226b374f
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-10-07 17:48:49 +0300

    [FIR] Commit false negative `UPPER_BOUNDS_VIOLATED`
    These are caused by moving the reporting to resolution, and they are
    a sign of a bug in somewhere in the code.

--- compiler/testData/diagnostics/tests/typealias/boundsViolationInTypeAliasExpansion.fir.kt
+++ compiler/testData/diagnostics/tests/typealias/boundsViolationInTypeAliasExpansion.fir.kt
@@ -19,7 +19,7 @@ val test5 = NA<Int>()
 val test6 = NA<<!UPPER_BOUND_VIOLATED!>Any<!>>()
 val test7 = NL<Int>()
 val test8 = MMMM<Int>()
-val test9dwd = <!UPPER_BOUND_VIOLATED!>NL<Any>()<!>
+val test9dwd = NL<Any>()
 
 fun test9(x: TC<Number, Collection<Number>>) {}
 fun test10(x: TC<Number, Collection<Int>>) {}
--- compiler/testData/diagnostics/testsWithStdLib/typealias/upperBoundViolated.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/typealias/upperBoundViolated.fir.kt
@@ -1,10 +1,10 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 class NumColl<T : Collection<Number>>
 typealias NumList<T2> = NumColl<List<T2>>
 typealias AliasOfNumList<A3> = NumList<A3>
 
 val falseUpperBoundViolation = AliasOfNumList<Int>() // Shouldn't be error
-val missedUpperBoundViolation = <!UPPER_BOUND_VIOLATED!>NumList<Any>()<!>  // Should be error
+val missedUpperBoundViolation = NumList<Any>()  // Should be error
 
 /* GENERATED_FIR_TAGS: classDeclaration, nullableType, propertyDeclaration, typeAliasDeclaration,
 typeAliasDeclarationWithTypeParameter, typeConstraint, typeParameter */
