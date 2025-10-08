d437cf1c238f401c1c0ce97467df76ed814b14bc
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-10-08 13:16:36 +0200

    [BTA] add some docs for the CRI toolchain and operations
    ^KT-80338 In progress

--- compiler/build-tools/kotlin-build-tools-api/src/main/kotlin/org/jetbrains/kotlin/buildtools/api/cri/CriToolchain.kt
+++ compiler/build-tools/kotlin-build-tools-api/src/main/kotlin/org/jetbrains/kotlin/buildtools/api/cri/CriToolchain.kt
@@ -3,31 +3,53 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-@file:OptIn(ExperimentalBuildToolsApi::class)
-
 package org.jetbrains.kotlin.buildtools.api.cri
 
-import org.jetbrains.kotlin.buildtools.api.BuildOperation
 import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
 import org.jetbrains.kotlin.buildtools.api.KotlinToolchains
 import org.jetbrains.kotlin.buildtools.api.getToolchain
 
-public interface CriLookupDataDeserializationOperation : BuildOperation<Collection<LookupEntry>>
-public interface CriFileIdToPathDataDeserializationOperation : BuildOperation<Collection<FileIdToPathEntry>>
-public interface CriSubtypeDataDeserializationOperation : BuildOperation<Collection<SubtypeEntry>>
-
+/**
+ * Allows creating operations that can be used for working with Compiler Reference Index generation.
+ *
+ * This interface is not intended to be implemented by the API consumers.
+ *
+ * Obtain an instance of this interface from [org.jetbrains.kotlin.buildtools.api.KotlinToolchains.cri].
+ *
+ * An example of the basic usage is:
+ *  ```
+ *   val lookupData: ByteArray = ...
+ *   val toolchain = KotlinToolchains.loadImplementation(ClassLoader.getSystemClassLoader())
+ *   val operation = toolchain.cri.createCriLookupDataDeserializationOperation(lookupData))
+ *   val lookups = toolchain.createBuildSession().use { it.executeOperation(operation) }
+ *  ```
+ *
+ * @since 2.3.20
+ */
+@ExperimentalBuildToolsApi
 public interface CriToolchain : KotlinToolchains.Toolchain {
     /**
-     * Example:
-     *  ```
-     *   val lookupData: ByteArray = ...
-     *   val toolchain = KotlinToolchain.loadImplementation(ClassLoader.getSystemClassLoader())
-     *   val operation = toolchain.cri.createCriLookupDataDeserializationOperation(lookupData)
-     *   val lookups = toolchain.createBuildSession().use { it.executeOperation(operation) }
-     *  ```
+     * Creates a build operation for deserializing [ByteArray] containing serialized lookup data into [LookupEntry]s.
+     *
+     * @param data serialized lookup data
+     * @see org.jetbrains.kotlin.buildtools.api.KotlinToolchains.BuildSession.executeOperation
      */
     public fun createCriLookupDataDeserializationOperation(data: ByteArray): CriLookupDataDeserializationOperation
+
+    /**
+     * Creates a build operation for deserializing [ByteArray] containing serialized fileIdToPath data into [FileIdToPathEntry]s.
+     *
+     * @param data serialized fileIdToPath data
+     * @see org.jetbrains.kotlin.buildtools.api.KotlinToolchains.BuildSession.executeOperation
+     */
     public fun createCriFileIdToPathDataDeserializationOperation(data: ByteArray): CriFileIdToPathDataDeserializationOperation
+
+    /**
+     * Creates a build operation for deserializing [ByteArray] containing serialized subtype data into [SubtypeEntry]s.
+     *
+     * @param data serialized subtype data
+     * @see org.jetbrains.kotlin.buildtools.api.KotlinToolchains.BuildSession.executeOperation
+     */
     public fun createCriSubtypeDataDeserializationOperation(data: ByteArray): CriSubtypeDataDeserializationOperation
 
     public companion object {
