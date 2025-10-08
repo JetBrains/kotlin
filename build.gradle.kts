JetBrains/kotlin

commit 12c6943eb36fcf9892fa2446fb43e24c600486d7

Enable use of deploy version for Kotlin Native in TeamCity
With this, versions.kotlin-native is set to the version of artifacts
in current build by default.
The tests that used the fallback version of Kotlin Native are switched
to work with Kotlin Native from local repo set up by TeamCity.
There are tests that rely on Kotlin Native being unpacked
in the project configuration stage. This is broken when using Maven repo
so a workaround is applied.

This is reapplication of commit b08685cd32b27f10ec449e9dce3c4c2bc0fcbbc5
with additional minor changes.

KTI-2342
KTI-2238

build.gradle.kts
@@ -114,8 +114,9 @@ rootProject.apply {
 IdeVersionConfigurator.setCurrentIde(project)
 
 if (!project.hasProperty("versions.kotlin-native")) {
-    // BEWARE! Bumping this version doesn't take an immediate effect on TeamCity: KTI-1107
-    extra["versions.kotlin-native"] = if (kotlinBuildProperties.isKotlinNativeEnabled) {
+    extra["versions.kotlin-native"] = if (kotlinBuildProperties.isTeamcityBuild) {
+        kotlinVersion
+    } else if (kotlinBuildProperties.isKotlinNativeEnabled) {
         kotlinBuildProperties.defaultSnapshotVersion
     } else {
         "2.3.0-dev-5665"

libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/buildProject.kt
@@ -53,7 +53,6 @@ fun buildProject(
     .apply(projectBuilder)
     .build()
     .also {
-        disableDownloadingKonanFromMavenCentral(it)
         it.enableDependencyVerification(false)
     }
     .apply(configureProject)

libraries/tools/kotlin-compose-compiler/src/functionalTest/kotlin/org/jetbrains/kotlin/compose/compiler/gradle/testUtils/functionalTestDsl.kt
@@ -24,7 +24,6 @@ fun buildProject(
     .build()
     .also {
         it.enableDependencyVerification(false)
-        disableDownloadingKonanFromMavenCentral(it)
     }
     .apply(configureProject)
     .let { it as ProjectInternal }
@@ -53,11 +52,6 @@ fun buildProjectWithMPP(
 
 inline val ExtensionAware.extraProperties: ExtraPropertiesExtension
     get() = extensions.extraProperties
-
-// TODO(Dmitrii Krasnov): we can remove this, when downloading konan from maven local will be possible KT-63198
-internal fun disableDownloadingKonanFromMavenCentral(project: Project) {
-    project.extraProperties.set("kotlin.native.distribution.downloadFromMaven", "false")
-}
 
 fun Project.applyKotlinJvmPlugin() {
     project.plugins.apply(KotlinPluginWrapper::class.java)

libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/NativeWithConfigurationCacheIT.kt
@@ -31,7 +31,7 @@ class NativeWithConfigurationCacheIT : KGPBaseTest() {
                 // We need to download compiler on the first build, that is why we are setting custom konan home dir without any compiler inside
                 konanDataDir = workingDir.resolve(".konan"),
                 nativeOptions = defaultBuildOptions.nativeOptions.copy(
-                    version = null,
+                    version = "2.1.21", // an arbitrary release version
                     distributionDownloadFromMaven = false, // please, remove the whole test, when this flag will be removed
                 ),
             ),

libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/dependencyResolutionTests/tcs/IdeNativePlatformDependencyResolverTest.kt
@@ -13,13 +13,20 @@ import org.jetbrains.kotlin.gradle.idea.testFixtures.tcs.binaryCoordinates
 import org.jetbrains.kotlin.gradle.internal.properties.nativeProperties
 import org.jetbrains.kotlin.gradle.plugin.ide.dependencyResolvers.IdeNativePlatformDependencyResolver
 import org.jetbrains.kotlin.gradle.util.buildProjectWithMPP
+import org.jetbrains.kotlin.gradle.util.provisionKotlinNativeDistribution
 import org.jetbrains.kotlin.konan.target.HostManager
 import org.jetbrains.kotlin.konan.target.KonanTarget.LINUX_X64
 import org.jetbrains.kotlin.konan.target.KonanTarget.MACOS_ARM64
 import org.junit.Assume
+import org.junit.Before
 import kotlin.test.Test
 
 class IdeNativePlatformDependencyResolverTest {
+    // workaround for tests that don't unpack Kotlin Native when using local repo: KT-77580
+    @Before
+    fun setUp() {
+        provisionKotlinNativeDistribution()
+    }
 
     @Test
     fun `test - posix on linux`() {

libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/dependencyResolutionTests/tcs/IdeNativeStdlibResolverTest.kt
@@ -13,9 +13,16 @@ import org.jetbrains.kotlin.gradle.idea.testFixtures.tcs.binaryCoordinates
 import org.jetbrains.kotlin.gradle.internal.properties.nativeProperties
 import org.jetbrains.kotlin.gradle.plugin.ide.dependencyResolvers.IdeNativeStdlibDependencyResolver
 import org.jetbrains.kotlin.gradle.util.buildProjectWithMPP
+import org.jetbrains.kotlin.gradle.util.provisionKotlinNativeDistribution
+import org.junit.Before
 import org.junit.Test
 
 class IdeNativeStdlibResolverTest {
+    // workaround for tests that don't unpack Kotlin Native when using local repo: KT-77580
+    @Before
+    fun setUp() {
+        provisionKotlinNativeDistribution()
+    }
 
 
     @Test

libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/dependencyResolutionTests/tcs/IdeSourcesAndDocumentationResolutionTest.kt
@@ -23,11 +23,18 @@ import org.jetbrains.kotlin.gradle.util.applyMultiplatformPlugin
 import org.jetbrains.kotlin.gradle.util.buildProject
 import org.jetbrains.kotlin.gradle.util.enableDefaultStdlibDependency
 import org.jetbrains.kotlin.gradle.util.enableDependencyVerification
+import org.jetbrains.kotlin.gradle.util.provisionKotlinNativeDistribution
+import org.junit.Before
 import kotlin.test.Test
 import kotlin.test.fail
 import kotlin.text.Regex.Companion.escape
 
 class IdeSourcesAndDocumentationResolutionTest {
+    // workaround for tests that don't unpack Kotlin Native when using local repo: KT-77580
+    @Before
+    fun setUp() {
+        provisionKotlinNativeDistribution()
+    }
 
     @Test
     fun `test - MVIKotlin`() {

libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/dependencyResolutionTests/tcs/IdeStdlibResolutionTest.kt
@@ -25,10 +25,17 @@ import org.jetbrains.kotlin.gradle.util.buildProject
 import org.jetbrains.kotlin.gradle.util.configureDefaults
 import org.jetbrains.kotlin.gradle.util.enableDefaultStdlibDependency
 import org.jetbrains.kotlin.gradle.util.enableDependencyVerification
+import org.jetbrains.kotlin.gradle.util.provisionKotlinNativeDistribution
 import org.jetbrains.kotlin.gradle.utils.androidExtension
+import org.junit.Before
 import org.junit.Test
 
 class IdeStdlibResolutionTest {
+    // workaround for tests that don't unpack Kotlin Native when using local repo: KT-77580
+    @Before
+    fun setUp() {
+        provisionKotlinNativeDistribution()
+    }
 
     @Test
     fun `test single jvm target`() {

commit 9959b9cdb6490dc151e82b8a3dd95eb4b402694d

Disable downloading K/N from Maven for local KGP functional test runs
Broken in 12c6943eb36fcf9892fa2446fb43e24c600486d7.
Kotlin Gradle Plugin functional test runs in Teamcity switched to using
Kotlin/Native distribution from artifacts stored in local Maven
repository, but local runs still use the version hardcoded in
root project's build script, which is only available in CDN.
This fix disables download from Maven (and enforces download from CDN)
conditionally depending on the value of a property which is set for
local runs.

KTI-2238

build.gradle.kts
@@ -127,6 +127,7 @@ if (!project.hasProperty("versions.kotlin-native")) {
     } else if (kotlinBuildProperties.isKotlinNativeEnabled) {
         kotlinBuildProperties.defaultSnapshotVersion
     } else {
+        System.setProperty("kotlin.native.distribution.disableDownloadFromMavenForTests", "true")
         "2.3.0-dev-5665"
     }
 }

libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/util/buildProject.kt
@@ -41,6 +41,9 @@ fun buildProject(
     .also {
         it.enableDependencyVerification(false)
         it.setFunctionalTestMode()
+        if (System.getProperty("kotlin.native.distribution.disableDownloadFromMavenForTests", "false").toBoolean()) {
+            it.extensions.extraProperties.set("kotlin.native.distribution.downloadFromMaven", "false")
+        }
     }
     .apply(configureProject)
     .let { it as ProjectInternal }