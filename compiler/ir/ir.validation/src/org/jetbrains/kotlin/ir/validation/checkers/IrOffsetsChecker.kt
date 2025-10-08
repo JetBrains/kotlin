8590d20c1b7f1f6f20e9ac33ff1d99a33c03227d
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-10-08 14:21:14 +0200

    fixup! [IR] Add offsets validation

--- compiler/ir/ir.validation/src/org/jetbrains/kotlin/ir/validation/checkers/IrOffsetsChecker.kt
+++ compiler/ir/ir.validation/src/org/jetbrains/kotlin/ir/validation/checkers/IrOffsetsChecker.kt
@@ -10,8 +10,23 @@ import org.jetbrains.kotlin.ir.validation.checkers.context.CheckerContext
 
 object IrOffsetsChecker : IrElementChecker<IrElement>(IrElement::class) {
     override fun check(element: IrElement, context: CheckerContext) {
-        if (element.startOffset > element.endOffset) {
-            context.error(element, "Element has invalid offsets: startOffset=${element.startOffset}, endOffset=${element.endOffset}")
+        when {
+            element.startOffset < -2 || element.endOffset < -2 -> context.error(
+                element,
+                "Element has invalid offsets. Offsets must be >= 0, UNDEFINED_OFFSET (-1) or SYNTHETIC_OFFSET (-2). " +
+                        "Actual: startOffset=${element.startOffset}, endOffset=${element.endOffset}"
+            )
+            (element.startOffset < 0 || element.endOffset < 0) && element.startOffset != element.endOffset -> context.error(
+                element,
+                "Element has invalid offsets. UNDEFINED_OFFSET (-1) and SYNTHETIC_OFFSET (-2) " +
+                        "can only appear simultaneously in both startOffset and endOffset. " +
+                        "Actual: startOffset=${element.startOffset}, endOffset=${element.endOffset}"
+            )
+            element.startOffset > element.endOffset -> context.error(
+                element,
+                "Element has invalid offsets. startOffset must not be greater than endOffset " +
+                        "Actual: startOffset=${element.startOffset}, endOffset=${element.endOffset}"
+            )
         }
     }
 }
