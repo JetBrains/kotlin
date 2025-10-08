b4b272f2a7c9caa85f1cd761d30fa4bc30997aa1
Anton Lakotka <anton.lakotka@jetbrains.com>
2025-10-09 01:27:47 +0200

    [Gradle] Don't publish unresolvable dependencies in compatibility variants
    when uklib is published we produce compatibility variants for
    standard KMP consumers. and since we have lenient resolution
    so uklibs can resolve some dependencies that regular KMP can't resolve.
    So we must filterout these dependencies in order to allow such consumption
    
    ^KT-81511
    ^KT-81575

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/gradleUtils.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/gradleUtils.kt
@@ -6,15 +6,20 @@
 package org.jetbrains.kotlin.gradle.utils
 
 import org.gradle.api.Action
+import org.gradle.api.Named
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.artifacts.component.BuildIdentifier
 import org.gradle.api.artifacts.component.ComponentIdentifier
 import org.gradle.api.artifacts.component.ProjectComponentIdentifier
+import org.gradle.api.attributes.Attribute
+import org.gradle.api.attributes.AttributeContainer
 import org.gradle.api.invocation.Gradle
 import org.gradle.api.provider.Provider
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
 import org.gradle.api.services.BuildServiceSpec
+import org.gradle.util.GradleVersion
 import kotlin.reflect.KClass
 
 internal val Project.compositeBuildRootGradle: Gradle get() = generateSequence(project.gradle) { it.parent }.last()
@@ -46,3 +51,31 @@ internal val ComponentIdentifier.buildOrNull: BuildIdentifier?
  */
 internal val ComponentIdentifier.projectPathOrNull: String?
     get() = (this as? ProjectComponentIdentifier)?.projectPath
+
+internal val ProjectDependency.projectPathCompat: String
+    get() = if (GradleVersion.current().baseVersion >= GradleVersion.version("8.11")) {
+        path
+    } else {
+        // FIXME: replace with propper Compatibility service [VariantImplementationFactories]
+        val dependencyProject = this.javaClass.getMethod("getDependencyProject").invoke(this) as Project
+        dependencyProject.path
+    }
+
+/**
+ * Getting value from [AttributeContainer.getAttribute] that came from external places
+ * e.g. from resolution result, or from configurations of other gradle plugins
+ * can return null if passed attribute instance differs from the ones that are stored in the container.
+ *
+ * This method should provide safe way of accessing these attributes by using underlying [Attribute.name]
+ * as a key.
+ */
+internal fun <T : Named> AttributeContainer.getAttributeSafely(attribute: Attribute<T>): String? {
+    val attributeKeyByName = keySet().associateBy { it.name }
+    val attributeKey = attributeKeyByName[attribute.name] ?: return null
+    val value = getAttribute(attributeKey)
+    return when (value) {
+        is String -> value
+        is Named -> value.name
+        else -> null
+    }
+}
