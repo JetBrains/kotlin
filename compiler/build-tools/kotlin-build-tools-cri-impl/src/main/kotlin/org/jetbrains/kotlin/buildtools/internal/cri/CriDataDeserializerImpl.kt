af68dff6b58353ed111cbbdc55f752d80dcc9354
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-10-09 11:48:30 +0200

    [BTA] move the CRI implementation to the `org.jetbrains.kotlin.buildtools.internal.cri` package
    ^KT-80338 In progress

--- compiler/build-tools/kotlin-build-tools-cri-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/cri/CriDataDeserializerImpl.kt
+++ compiler/build-tools/kotlin-build-tools-cri-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/cri/CriDataDeserializerImpl.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.buildtools.cri.internal
+package org.jetbrains.kotlin.buildtools.internal.cri
 
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromByteArray
