3675af3c7690e15e72c9713f7d508a59dc9bdf7e
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2025-10-09 17:29:45 +0300

    fixup! [Language] Add feature to forbid conflicting bridges and overrides

--- compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -482,7 +482,7 @@ enum class LanguageFeature(
     // 2.5
 
     ErrorAboutDataClassCopyVisibilityChange(KOTLIN_2_5, enabledInProgressiveMode = true, "KT-11914"), // KT-11914. Deprecation phase 2
-    ForbidBridgesConflictingWithInheritedMethodsInJvmCode(KOTLIN_2_5, enabledInProgressiveMode = true, "KT-13712"),
+    ForbidBridgesConflictingWithInheritedMethodsInJvmCode(KOTLIN_2_5, enabledInProgressiveMode = true, "KTLC-371"),
 
     // End of 2.* language features --------------------------------------------------
 
