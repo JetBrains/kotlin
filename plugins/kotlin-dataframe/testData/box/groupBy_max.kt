22b91956c269ecd110fdd40d36f2b3c743185031
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-10-10 14:21:02 +0200

    [KDF] streamlined statistics tests

--- plugins/kotlin-dataframe/testData/box/groupBy_max.kt
+++ plugins/kotlin-dataframe/testData/box/groupBy_max.kt
@@ -2,20 +2,23 @@ import org.jetbrains.kotlinx.dataframe.*
 import org.jetbrains.kotlinx.dataframe.annotations.*
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.io.*
+import java.math.BigInteger
 
 fun box(): String {
     // multiple columns
-    val personsDf = dataFrameOf("name", "age", "city", "weight", "height", "yearsToRetirement")(
-        "Alice", 15, "London", 99.5, "1.85", 50,
-        "Bob", 20, "Paris", 140.0, "1.35", 45,
-        "Charlie", 100, "Dubai", 75.0, "1.95", 0,
-        "Rose", 1, "Moscow", 45.33, "0.79", 64,
-        "Dylan", 35, "London", 23.4, "1.83", 30,
-        "Eve", 40, "Paris", 56.72, "1.85", 25,
-        "Frank", 55, "Dubai", 78.9, "1.35", 10,
-        "Grace", 29, "Moscow", 67.8, "1.65", 36,
-        "Hank", 60, "Paris", 80.22, "1.75", 5,
-        "Isla", 22, "London", 75.1, "1.85", 43,
+    val personsDf = dataFrameOf(
+        "name", "age", "city", "weight", "height", "yearsToRetirement", "bigNumber",
+    )(
+        "Alice", 15, "London", 99.5, "1.85", 50f, BigInteger.valueOf(23L),
+        "Bob", 20, "Paris", 140.0, "1.35", 45f, BigInteger.valueOf(12L),
+        "Charlie", 100, "Dubai", 75.0, "1.95", 0f, BigInteger.valueOf(68798L),
+        "Rose", 1, "Moscow", 45.33, "0.79", 64f, BigInteger.valueOf(46556L),
+        "Dylan", 35, "London", 23.4, "1.83", 30f, BigInteger.valueOf(235L),
+        "Eve", 40, "Paris", 56.72, "1.85", 25f, BigInteger.valueOf(-23534L),
+        "Frank", 55, "Dubai", 78.9, "1.35", 10f, BigInteger.valueOf(235L),
+        "Grace", 29, "Moscow", 67.8, "1.65", 36f, BigInteger.valueOf(0L),
+        "Hank", 60, "Paris", 80.22, "1.75", 5f, BigInteger.valueOf(-4L),
+        "Isla", 22, "London", 75.1, "1.85", 43f, BigInteger.valueOf(2345L),
     )
 
     // scenario #0: all numerical columns
@@ -37,6 +40,12 @@ fun box(): String {
         df.compareSchemas()
     }
 
+    // scenario #1.2: multiple columns via max
+    personsDf.groupBy { city }.max { age and age }.let { df ->
+        val max111: Int? = df.max[0]
+        df.compareSchemas()
+    }
+
     // scenario #2: particular column with new name - schema changes
     // TODO: not supported scenario
     // val res2 = personsDf.groupBy { city }.max("age", name = "newAge")
@@ -49,8 +58,7 @@ fun box(): String {
     }
 
     // scenario #2.2: two columns with new name - schema changes but via columnSelector
-    // TODO: handle multiple columns https://github.com/Kotlin/dataframe/issues/1090
-    personsDf.groupBy { city }.max("newAge") { age and yearsToRetirement }.let { df ->
+    personsDf.groupBy { city }.max("newAge") { age and age }.let { df ->
         val max221: Int? = df.newAge[0]
         df.compareSchemas()
     }
--- plugins/kotlin-dataframe/testData/box/groupBy_percentile.kt
+++ plugins/kotlin-dataframe/testData/box/groupBy_percentile.kt
@@ -2,29 +2,22 @@ import org.jetbrains.kotlinx.dataframe.*
 import org.jetbrains.kotlinx.dataframe.annotations.*
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.io.*
+import java.math.BigInteger
 
 fun box(): String {
     val personsDf = dataFrameOf(
-        "name",
-        "age",
-        "city",
-        "weight",
-        "height",
-        "yearsToRetirement",
-        "workExperienceYears",
-        "dependentsCount",
-        "annualIncome"
+        "name", "age", "city", "weight", "height", "yearsToRetirement", "workExperienceYears", "dependentsCount", "annualIncome", "bigNumber",
     )(
-        "Alice", 15, "London", 99.5, "1.85", 50, 0.toShort(), 0.toByte(), 0L,
-        "Bob", 20, "Paris", 140.0, "1.35", 45, 2.toShort(), 0.toByte(), 12000L,
-        "Charlie", 100, "Dubai", 75.0, "1.95", 0, 70.toShort(), 0.toByte(), 0L,
-        "Rose", 1, "Moscow", 45.33, "0.79", 64, 0.toShort(), 2.toByte(), 0L,
-        "Dylan", 35, "London", 23.4, "1.83", 30, 15.toShort(), 1.toByte(), 90000L,
-        "Eve", 40, "Paris", 56.72, "1.85", 25, 18.toShort(), 3.toByte(), 125000L,
-        "Frank", 55, "Dubai", 78.9, "1.35", 10, 35.toShort(), 2.toByte(), 145000L,
-        "Grace", 29, "Moscow", 67.8, "1.65", 36, 5.toShort(), 1.toByte(), 70000L,
-        "Hank", 60, "Paris", 80.22, "1.75", 5, 40.toShort(), 4.toByte(), 200000L,
-        "Isla", 22, "London", 75.1, "1.85", 43, 1.toShort(), 0.toByte(), 30000L,
+        "Alice", 15, "London", 99.5, "1.85", 50f, 0.toShort(), 0.toByte(), 0L, BigInteger.valueOf(23L),
+        "Bob", 20, "Paris", 140.0, "1.35", 45f, 2.toShort(), 0.toByte(), 12000L, BigInteger.valueOf(12L),
+        "Charlie", 100, "Dubai", 75.0, "1.95", 0f, 70.toShort(), 0.toByte(), 0L, BigInteger.valueOf(68798L),
+        "Rose", 1, "Moscow", 45.33, "0.79", 64f, 0.toShort(), 2.toByte(), 0L, BigInteger.valueOf(46556L),
+        "Dylan", 35, "London", 23.4, "1.83", 30f, 15.toShort(), 1.toByte(), 90000L, BigInteger.valueOf(235L),
+        "Eve", 40, "Paris", 56.72, "1.85", 25f, 18.toShort(), 3.toByte(), 125000L, BigInteger.valueOf(-23534L),
+        "Frank", 55, "Dubai", 78.9, "1.35", 10f, 35.toShort(), 2.toByte(), 145000L, BigInteger.valueOf(235L),
+        "Grace", 29, "Moscow", 67.8, "1.65", 36f, 5.toShort(), 1.toByte(), 70000L, BigInteger.valueOf(0L),
+        "Hank", 60, "Paris", 80.22, "1.75", 5f, 40.toShort(), 4.toByte(), 200000L, BigInteger.valueOf(-4L),
+        "Isla", 22, "London", 75.1, "1.85", 43f, 1.toShort(), 0.toByte(), 30000L, BigInteger.valueOf(2345L),
     )
 
     // scenario #0: all intraComparable columns
@@ -50,6 +43,13 @@ fun box(): String {
         val percentile111: Double? = df.age[0]
     }
 
+    // scenario #1.2: multiple columns via percentile
+    personsDf.groupBy { city }.percentile(0.5) { age and age }.let { df ->
+        df.compareSchemas()
+
+        val percentile111: Double? = df.percentile[0]
+    }
+
     // scenario #2: particular column with new name - schema changes
     // TODO: not supported scenario
     // val res2 = personsDf.groupBy { city }.percentile("age", name = "newAge")
@@ -63,8 +63,7 @@ fun box(): String {
     }
 
     // scenario #2.2: two columns with new name - schema changes but via columnSelector
-    // TODO: handle multiple columns https://github.com/Kotlin/dataframe/issues/1090
-    personsDf.groupBy { city }.percentile(0.5, "newAge") { age and yearsToRetirement }.let { df ->
+    personsDf.groupBy { city }.percentile(0.5, "newAge") { age and age }.let { df ->
         df.compareSchemas()
 
         val percentile221: Double? = df.newAge[0]
