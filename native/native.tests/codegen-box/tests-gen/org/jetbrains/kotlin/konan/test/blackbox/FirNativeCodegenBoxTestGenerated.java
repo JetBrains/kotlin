73d18ecffaf8639afed4ff3a78032f03c3c83691
Igor Chevdar <igor.chevdar@jetbrains.com>
2025-10-10 22:05:04 +0300

    [box-tests] Added a reproducer for #KT-81617

--- native/native.tests/codegen-box/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestGenerated.java
+++ native/native.tests/codegen-box/tests-gen/org/jetbrains/kotlin/konan/test/blackbox/FirNativeCodegenBoxTestGenerated.java
@@ -7184,6 +7184,12 @@ public class FirNativeCodegenBoxTestGenerated extends AbstractNativeCodegenBoxTe
       }
 
       @Test
+      @TestMetadata("kt81617.kt")
+      public void testKt81617() {
+        runTest("compiler/testData/codegen/box/casts/kt81617.kt");
+      }
+
+      @Test
       @TestMetadata("lambdaToUnitCast.kt")
       public void testLambdaToUnitCast() {
         runTest("compiler/testData/codegen/box/casts/lambdaToUnitCast.kt");
