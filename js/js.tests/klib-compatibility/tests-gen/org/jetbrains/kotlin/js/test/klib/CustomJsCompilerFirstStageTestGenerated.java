657c764de4aecc6da0e98659e38f367fe08b8e7a
Florian Freitag <florian.freitag.external@jetbrains.com>
2025-10-10 08:49:05 +0200

    [FIR] Refactor trimIndent and trimMargin tests into one
    This is a preperation for future work where more functions from the
    trim family will be supported
    
    ^KT-80804

--- js/js.tests/klib-compatibility/tests-gen/org/jetbrains/kotlin/js/test/klib/CustomJsCompilerFirstStageTestGenerated.java
+++ js/js.tests/klib-compatibility/tests-gen/org/jetbrains/kotlin/js/test/klib/CustomJsCompilerFirstStageTestGenerated.java
@@ -36224,15 +36224,9 @@ public class CustomJsCompilerFirstStageTestGenerated extends AbstractCustomJsCom
         }
 
         @Test
-        @TestMetadata("constTrimIndent.kt")
-        public void testConstTrimIndent() {
-          runTest("compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/constTrimIndent.kt");
-        }
-
-        @Test
-        @TestMetadata("constTrimMargin.kt")
-        public void testConstTrimMargin() {
-          runTest("compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/constTrimMargin.kt");
+        @TestMetadata("constTrim.kt")
+        public void testConstTrim() {
+          runTest("compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/constTrim.kt");
         }
 
         @Test
