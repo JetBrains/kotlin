8722236879059f143cb6393ee4134984239fb984
Igor Chevdar <igor.chevdar@jetbrains.com>
2025-10-10 22:05:04 +0300

    [box-tests] Added a reproducer for #KT-81617

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenBoxTestGenerated.java
@@ -7077,6 +7077,12 @@ public class FirJsCodegenBoxTestGenerated extends AbstractFirJsCodegenBoxTest {
     }
 
     @Test
+    @TestMetadata("kt81617.kt")
+    public void testKt81617() {
+      runTest("compiler/testData/codegen/box/casts/kt81617.kt");
+    }
+
+    @Test
     @TestMetadata("lambdaToUnitCast.kt")
     public void testLambdaToUnitCast() {
       runTest("compiler/testData/codegen/box/casts/lambdaToUnitCast.kt");
