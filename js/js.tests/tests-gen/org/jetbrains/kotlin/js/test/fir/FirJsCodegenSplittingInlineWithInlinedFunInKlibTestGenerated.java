daa641c3e922fa73a04e3c8d3a09431294d9a8ba
Florian Freitag <florian.freitag.external@jetbrains.com>
2025-10-10 08:49:53 +0200

    [FIR][IR] Add char pseudo constructor to constant evaluation
    KT-80804

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenSplittingInlineWithInlinedFunInKlibTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/fir/FirJsCodegenSplittingInlineWithInlinedFunInKlibTestGenerated.java
@@ -36618,6 +36618,12 @@ public class FirJsCodegenSplittingInlineWithInlinedFunInKlibTestGenerated extend
         }
 
         @Test
+        @TestMetadata("charPseudoConstructor.kt")
+        public void testCharPseudoConstructor() {
+          runTest("compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/charPseudoConstructor.kt");
+        }
+
+        @Test
         @TestMetadata("constTrim.kt")
         public void testConstTrim() {
           runTest("compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/constTrim.kt");
