e57c40986f9f041762b917dea74d93a91388f6e2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-10-13 14:17:51 +0200

    K2: file NOT_YET_SUPPORTED_IN_INLINE for inherited default parameters
    KT-49722 in progress

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirInlineCheckerPlatformSpecificComponent.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirInlineCheckerPlatformSpecificComponent.kt
@@ -6,14 +6,17 @@
 package org.jetbrains.kotlin.fir.analysis.checkers
 
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
+import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
 import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirSimpleFunction
 import org.jetbrains.kotlin.fir.declarations.FirValueParameter
 import org.jetbrains.kotlin.fir.symbols.impl.FirCallableSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
 
 abstract class FirInlineCheckerPlatformSpecificComponent : FirSessionComponent {
     context(context: CheckerContext, reporter: DiagnosticReporter)
@@ -30,7 +33,24 @@ abstract class FirInlineCheckerPlatformSpecificComponent : FirSessionComponent {
         function: FirSimpleFunction,
         overriddenSymbols: List<FirCallableSymbol<FirCallableDeclaration>>,
     ) {
+        val paramsWithDefaults = overriddenSymbols.flatMap {
+            if (it !is FirFunctionSymbol<*>) return@flatMap emptyList()
+            it.valueParameterSymbols.mapIndexedNotNull { idx, param ->
+                idx.takeIf { param.hasDefaultValue }
+            }
+        }.toSet()
+        function.valueParameters.forEachIndexed { idx, param ->
+            if (param.defaultValue == null && paramsWithDefaults.contains(idx)) {
+                reporter.reportOn(
+                    param.source,
+                    FirErrors.NOT_YET_SUPPORTED_IN_INLINE,
+                    "Functional parameters with inherited default values"
+                )
+            }
+        }
     }
+
+    companion object Default : FirInlineCheckerPlatformSpecificComponent()
 }
 
-val FirSession.inlineCheckerExtension: FirInlineCheckerPlatformSpecificComponent? by FirSession.nullableSessionComponentAccessor<FirInlineCheckerPlatformSpecificComponent>()
+val FirSession.inlineCheckerExtension: FirInlineCheckerPlatformSpecificComponent by FirSession.sessionComponentAccessor<FirInlineCheckerPlatformSpecificComponent>()
--- compiler/testData/diagnostics/testsWithJsStdLib/defaultArguments/inheritedDefaultValue.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/defaultArguments/inheritedDefaultValue.kt
@@ -1,4 +1,4 @@
-// RUN_PIPELINE_TILL: FIR2IR
+// RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-49722
 // LANGUAGE: +IrIntraModuleInlinerBeforeKlibSerialization +IrCrossModuleInlinerBeforeKlibSerialization
 
@@ -8,7 +8,7 @@ interface I {
 
 open class A : I {
     // [NOT_YET_SUPPORTED_IN_INLINE] Functional parameters with inherited default values are not yet supported in inline functions.
-    final override inline <!OVERRIDE_BY_INLINE!>fun h(x: Int, block: () -> String)<!> = block()
+    final override inline <!OVERRIDE_BY_INLINE!>fun h(<!NOT_YET_SUPPORTED_IN_INLINE!>x: Int<!>, block: () -> String)<!> = block()
 }
 
 fun box() = A().h { "OK" }
