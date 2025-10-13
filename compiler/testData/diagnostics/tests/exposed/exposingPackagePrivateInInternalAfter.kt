59c794532f5cacbd2ede886fce1d403281a344f5
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-10-13 11:01:22 +0200

    [FIR] Put warning about exposing package private from internal under experimental LF
    Before this change, the LF `ForbidExposingPackagePrivateInInternal`
    would control whether we report an exposed visibility error or warning.
    Now, it controls whether we report a warning or nothing.
    Note this only affects exposed visibility diagnostics, not type
    exposure in inline (see compiler/testData/diagnostics/tests/inline
    /exposingPackagePrivateTypeInInternal).
    
    #KT-81657 Fixed

--- compiler/testData/diagnostics/tests/exposed/exposingPackagePrivateInInternalAfter.kt
+++ compiler/testData/diagnostics/tests/exposed/exposingPackagePrivateInInternalAfter.kt
@@ -1,10 +1,9 @@
-// FIR_IDENTICAL
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 // LANGUAGE: +ForbidExposingPackagePrivateInInternal
 // FILE: Foo.java
 class Foo {}
 
 // FILE: test.kt
-internal fun <T : <!EXPOSED_TYPE_PARAMETER_BOUND!>Foo<!>> <!EXPOSED_RECEIVER_TYPE!>Foo<!>.<!EXPOSED_FUNCTION_RETURN_TYPE!>bar<!>(<!EXPOSED_PARAMETER_TYPE!>f: Foo<!>): Foo = Foo()
+internal fun <T : Foo> Foo.bar(f: Foo): Foo = Foo()
 
 /* GENERATED_FIR_TAGS: funWithExtensionReceiver, functionDeclaration, javaType */
