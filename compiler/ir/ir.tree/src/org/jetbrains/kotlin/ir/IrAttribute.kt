c9b9764fb3b4656eb097d9500b1f6699e5004fc4
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-10-14 19:33:23 +0200

    [IR] Drop IR attribute migration utils
    The mass migration from maps to attributes in the compiler is finished,
    those utilities are not useful anymore.
    
    KT-69082

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/IrAttribute.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/IrAttribute.kt
@@ -67,32 +67,6 @@ operator fun <E : IrElement, T : Any> E.set(attribute: IrAttribute<E, T>, value:
  * }
  * ```
  *
- * ##### Migration note:
- * There is also a [MutableMap] wrapper around [IrAttribute] to ease migration from maps in
- * the form of `MutableMap<IrElement, T>` to attributes - you can replace such maps with
- * `irAttribute().asMap()` in-place, and the usual map-based syntax will continue to work.
- * Example:
- * ```kotlin
- * val binaryNames = mutableMapOf<IrDeclaration, String>()
- * // can be replaced with:
- * val binaryNames by irAttribute<IrDeclaration, String>().asMap()
- * ```
- * `binaryNames[element]` will then behave the same way as `element.binaryNames`.
- *
- * Similarly, `MutableSet<IrElement>` may be implemented by `irFlag()`:
- * ```kotlin
- * val functionsWithSpecialBridges = hashSet<IrFunction>()
- * // can be replaced with:
- * val functionsWithSpecialBridges by irFlag<IrFunction>().asSet()
- * ```
- * This is useful for marker sets, e.g. where later you would check `someFunction in functionsWithSpecialBridges`.
- *
- * However, a proper migration to the extension property syntax is eventually expected, and this
- * functionality is to be removed.
- * Note that collection operations, like iterating over all entries of the map, are not supported
- * and will throw at runtime. If there is a need to use them, such a map should not be converted to
- * an attribute.
- *
  * @param E restricts the type of [IrElement] on which this attribute can be stored.
  * @param T the type of the data stored in the attribute.
  */
@@ -152,11 +126,6 @@ class IrAttribute<E : IrElement, T : Any> internal constructor(
                 val attribute = attributeDelegate.provideDelegate(thisRef, property)
                 return Flag(attribute)
             }
-
-            fun asSet() = PropertyDelegateProvider { thisRef: Any?, property: KProperty<*> ->
-                val attribute = this@Delegate.provideDelegate(thisRef, property)
-                DummyDelegate(IrAttributeMapWrapper.FlagSetWrapper(attribute))
-            }
         }
     }
 
@@ -173,154 +142,5 @@ class IrAttribute<E : IrElement, T : Any> internal constructor(
 
         operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): IrAttribute<E, T> =
             create(thisRef, property.name)
-
-
-        fun asMap() = PropertyDelegateProvider { thisRef: Any?, property: KProperty<*> ->
-            val attribute = this@Delegate.provideDelegate(thisRef, property)
-            DummyDelegate(IrAttributeMapWrapper(attribute))
-        }
     }
 }
-
-/**
- * A helper for migration from `MutableMap<IrElement, T>` to `IrAttribute`.
- * See [IrAttribute]
- */
-@Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-class IrAttributeMapWrapper<E : IrElement, T : Any> internal constructor(
-    val attribute: IrAttribute<E, T>,
-) : AbstractMutableMap<E, T>() {
-    override operator fun get(element: E): T? {
-        return element[attribute]
-    }
-
-    override fun containsKey(element: E): Boolean {
-        return element[attribute] != null
-    }
-
-    override fun put(element: E, value: T): T? {
-        return element.set(attribute, value)
-    }
-
-    override fun remove(element: E): T? {
-        return element.set(attribute, null)
-    }
-
-    override fun computeIfAbsent(element: E, mappingFunction: Function<in E, out T>): T {
-        element[attribute]?.let {
-            return it
-        }
-        val newValue = mappingFunction.apply(element)
-        element[attribute] = newValue
-        return newValue
-    }
-
-    override val keys: MutableSet<E> = KeyCollection()
-
-    @Deprecated(
-        "Not implemented in IrAttribute, will throw at runtime." +
-                "If you need this Map functionality, please use regular MutableMap.",
-        level = DeprecationLevel.ERROR
-    )
-    override val entries: MutableSet<MutableMap.MutableEntry<E, T>> get() = unsupportedMapOperation()
-
-    @Deprecated(
-        "Not implemented in IrAttribute, will throw at runtime." +
-                "If you need this Map functionality, please use regular MutableMap.",
-        level = DeprecationLevel.ERROR
-    )
-    override val size: Int get() = unsupportedMapOperation()
-
-    @Deprecated(
-        "Not implemented in IrAttribute, will throw at runtime." +
-                "If you need this Map functionality, please use regular MutableMap.",
-        level = DeprecationLevel.ERROR
-    )
-    override fun clear() = unsupportedMapOperation()
-
-    override fun equals(other: Any?): Boolean = other is IrAttributeMapWrapper<*, *> && attribute == other.attribute
-
-    override fun hashCode(): Int = attribute.hashCode()
-
-    override fun toString(): String = attribute.toString()
-
-
-    private inner class KeyCollection : AbstractMutableSet<E>() {
-        override fun contains(element: E): Boolean {
-            return element[attribute] != null
-        }
-
-        @Deprecated(
-            "Not implemented in IrAttribute, will throw at runtime." +
-                    "If you need this Map functionality, please use regular MutableMap.",
-            level = DeprecationLevel.ERROR
-        )
-        override val size: Int
-            get() = unsupportedMapOperation()
-
-        @Deprecated(
-            "Not implemented in IrAttribute, will throw at runtime." +
-                    "If you need this Map functionality, please use regular MutableMap.",
-            level = DeprecationLevel.ERROR
-        )
-        override fun add(element: E): Boolean = unsupportedMapOperation()
-
-        @Deprecated(
-            "Not implemented in IrAttribute, will throw at runtime." +
-                    "If you need this Map functionality, please use regular MutableMap.",
-            level = DeprecationLevel.ERROR
-        )
-        override fun iterator(): MutableIterator<E> = unsupportedMapOperation()
-    }
-
-    /**
-     * A helper for migration from `MutableSet<IrElement>` to `IrAttribute`.
-     * See [IrAttribute]
-     */
-    class FlagSetWrapper<E : IrElement> internal constructor(
-        private val flag: IrAttribute.Flag<E>,
-    ) : AbstractMutableSet<E>() {
-        override fun contains(element: E): Boolean {
-            return flag.get(element)
-        }
-
-        override fun add(element: E): Boolean {
-            val wasSet = flag.get(element)
-            flag.set(element, true)
-            return !wasSet
-        }
-
-        override fun remove(element: E): Boolean {
-            val wasSet = flag.get(element)
-            flag.set(element, false)
-            return wasSet
-        }
-
-        @Deprecated(
-            "Not implemented in IrAttribute, will throw at runtime." +
-                    "If you need this Map functionality, please use regular MutableMap.",
-            level = DeprecationLevel.ERROR
-        )
-        override val size: Int get() = unsupportedMapOperation()
-
-        @Deprecated(
-            "Not implemented in IrAttribute, will throw at runtime." +
-                    "If you need this Map functionality, please use regular MutableMap.",
-            level = DeprecationLevel.ERROR
-        )
-        override fun iterator(): MutableIterator<E> = unsupportedMapOperation()
-
-        @Deprecated(
-            "Not implemented in IrAttribute, will throw at runtime." +
-                    "If you need this Map functionality, please use regular MutableMap.",
-            level = DeprecationLevel.ERROR
-        )
-        override fun clear() = unsupportedMapOperation()
-    }
-
-    companion object {
-        private fun unsupportedMapOperation(): Nothing =
-            throw UnsupportedOperationException("This map-based operation is unsupported by IR attribute")
-    }
-}
-
