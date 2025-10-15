2668f5495fc342aa13c573227fa4ce9a70760b99
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-15 13:44:29 +0400

    [SLC] WIP remove delegation from test

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/collectionAbstractClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/collectionAbstractClass.java
@@ -1,19 +1,3 @@
 public abstract class CCollection /* test.CCollection*/<Elem>  implements java.util.Collection<Elem>, kotlin.collections.Collection<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
-  @java.lang.Override()
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
-
-  @java.lang.Override()
-  public boolean contains(Elem);//  contains(Elem)
-
-  @java.lang.Override()
-  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
-
-  @java.lang.Override()
-  public boolean isEmpty();//  isEmpty()
-
-  @java.lang.Override()
-  public int getSize();//  getSize()
-
   public  CCollection();//  .ctor()
 }
