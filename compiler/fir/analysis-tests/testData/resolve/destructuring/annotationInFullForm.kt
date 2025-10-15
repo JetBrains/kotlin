352f3985749e07de59da8135dbe38aa2a933eb6f
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-10-15 11:54:24 +0300

    Fix `testAnnotationInFullForm` muted in `c149067d` and `19ad3f92`
    ^KTI-2720

--- compiler/fir/analysis-tests/testData/resolve/destructuring/annotationInFullForm.kt
+++ compiler/fir/analysis-tests/testData/resolve/destructuring/annotationInFullForm.kt
@@ -7,7 +7,7 @@ annotation class Ann
 data class Person(val fullName: String)
 
 fun annSimpleNegative(person: Person) {
-    <!WRAPPED_LHS_IN_ASSIGNMENT_ERROR!>(<!EXPRESSION_EXPECTED{PSI}, VARIABLE_EXPECTED!>@Ann val <!VARIABLE_WITH_NO_TYPE_NO_INITIALIZER{PSI}!>fullName<!><!>)<!> = person
+    <!WRAPPED_LHS_IN_ASSIGNMENT_WARNING!>(<!EXPRESSION_EXPECTED{PSI}, VARIABLE_EXPECTED!>@Ann val <!VARIABLE_WITH_NO_TYPE_NO_INITIALIZER{PSI}!>fullName<!><!>)<!> = person
 }
 
 /* GENERATED_FIR_TAGS: annotationDeclaration, classDeclaration, data, functionDeclaration, primaryConstructor,
