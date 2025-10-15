2de220eaee289f3d1788638177d65c0924009610
Anton Lakotka <anton.lakotka@jetbrains.com>
2025-10-15 21:43:53 +0200

    [Gradle] Add test to cover Gradle Daemon ClassLoader issue
    I had to use trick to reproduce it.
    Because Gradle Test Kit doesn't allow to stop daemon explicitly.
    However running gradle daemon against different project causes it
    to drop classloader caches for the previous project.
    
    For safety I compare Classloader hashcodes for first and second runs
    to make sure they're different.
    
    ^KT-81060 Verification Pending
    
    (cherry picked from commit 7f950050a91743cb98ddd1746dd16a37dffcf2d7)

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-81060_agp_configuration_cache_class_not_found/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("multiplatform") apply false
+    id("com.android.kotlin.multiplatform.library") apply false
+    id("com.android.application") apply false
+}
---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kt-81060_agp_configuration_cache_class_not_found/composeApp/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.application")
+}
+
+kotlin {
+    androidTarget {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":library"))
+        }
+    }
+}
+
+android {
+    namespace = "org.bug.replication"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "org.bug.replication"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
