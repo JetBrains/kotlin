a9a20cdb89ce62aa4b365ad34fbf35b37c20081d
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-10-15 12:19:40 +0200

    Remove test tasks from projects without test sources

--- analysis/decompiled/decompiler-native/build.gradle.kts
+++ analysis/decompiled/decompiler-native/build.gradle.kts
@@ -2,21 +2,10 @@ plugins {
     kotlin("jvm")
     id("jps-compatible")
     id("java-test-fixtures")
-    id("project-tests-convention")
 }
 
 sourceSets {
     "main" { projectDefault() }
-    "test" { generatedTestDir() }
-    "testFixtures" { projectDefault() }
-}
-
-projectTests {
-    testTask(jUnitMode = JUnitMode.JUnit4) {
-        workingDir = rootDir
-    }
-
-    withJvmStdlibAndReflect()
 }
 
 dependencies {
@@ -34,12 +23,9 @@ dependencies {
 
     testFixturesApi(platform(libs.junit.bom))
     testFixturesApi(libs.junit4)
-    testCompileOnly(libs.junit.jupiter.api) // the annotations are misused and have no effect
     testFixturesApi(testFixtures(project(":compiler:tests-common")))
     testFixturesApi(testFixtures(project(":compiler:tests-common-new")))
     testFixturesApi(testFixtures(project(":analysis:decompiled:decompiler-to-file-stubs")))
 }
 
 optInToK1Deprecation()
-
-testsJar()
