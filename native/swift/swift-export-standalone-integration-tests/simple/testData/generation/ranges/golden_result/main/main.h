62251786de5f4740a7c9d945a56154e994751f75
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-10-16 10:09:08 +0200

    Swift export: add some new test(s) for custom types (ranges in particular)
    KT-81591 in progress

---
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/ranges/golden_result/main/main.h
@@ -0,0 +1,16 @@
+#include <Foundation/Foundation.h>
+#include <stdint.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+void * generation_ranges_ranges_accept__TypesOfArguments__ExportedKotlinPackages_kotlin_ranges_IntRange__(void * range);
+
+void * generation_ranges_ranges_bar();
+
+void * generation_ranges_ranges_baz();
+
+void * generation_ranges_ranges_foo();
+
+void * generation_ranges_ranges_unsupported();
+
+NS_ASSUME_NONNULL_END
--- native/swift/swift-export-standalone-integration-tests/simple/tests-gen/org/jetbrains/kotlin/swiftexport/standalone/test/SwiftExportWithResultValidationTest.java
+++ native/swift/swift-export-standalone-integration-tests/simple/tests-gen/org/jetbrains/kotlin/swiftexport/standalone/test/SwiftExportWithResultValidationTest.java
@@ -194,6 +194,12 @@ public class SwiftExportWithResultValidationTest extends AbstractSwiftExportWith
   }
 
   @Test
+  @TestMetadata("ranges")
+  public void testRanges() {
+    runTest("native/swift/swift-export-standalone-integration-tests/simple/testData/generation/ranges/");
+  }
+
+  @Test
   @TestMetadata("set")
   public void testSet() {
     runTest("native/swift/swift-export-standalone-integration-tests/simple/testData/generation/set/");
