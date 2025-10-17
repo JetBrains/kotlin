8c9ba440fe8cda556acc1ec460d9b71a810a7145
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2025-10-17 10:25:14 +0200

    [Tests] Convert MPP test from context receivers to context parameters
    #KT-72994

--- compiler/testData/diagnostics/tests/multiplatform/actualClassifierMustHasTheSameMembersAsNonFinalExpectClassifierChecker/changeClassTypeParameterNames.kt
+++ compiler/testData/diagnostics/tests/multiplatform/actualClassifierMustHasTheSameMembersAsNonFinalExpectClassifierChecker/changeClassTypeParameterNames.kt
@@ -1,14 +1,13 @@
 // RUN_PIPELINE_TILL: BACKEND
-// DIAGNOSTICS: -CONTEXT_RECEIVERS_DEPRECATED
 // FIR_IDENTICAL
-// LANGUAGE: +ContextReceivers
+// LANGUAGE: +ContextParameters
 // MODULE: m1-common
 // FILE: common.kt
 
 open class Base<T> {
     open fun returnType(): T = null!!
     open fun parameterType(t: T) {}
-    context(T)
+    context(_: T)
     open fun contextReceiverType() {}
     open fun T.extensionReceiverType() {}
 }
@@ -21,7 +20,7 @@ expect open class Foo<E> : Base<E>
 actual open class Foo<R> : Base<R>() {
     override fun returnType(): R = null!!
     override fun parameterType(t: R) {}
-    context(R)
+    context(_: R)
     override fun contextReceiverType() {}
     override fun R.extensionReceiverType() {}
 }
