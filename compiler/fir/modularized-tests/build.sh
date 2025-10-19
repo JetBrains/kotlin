b0f92544d2004753a806fdcac22bf9b83c90b733
Simon Ogorodnik <simon.ogorodnik@gmail.com>
2025-10-19 16:50:03 +0200

    wip

--- compiler/fir/modularized-tests/build.sh
+++ compiler/fir/modularized-tests/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ~/Downloads/graalvm-jdk-21.0.5+9.1/Contents/Home/bin/native-image \
 --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -H:+AddAllCharsets \
--jar owo-2.1.255-SNAPSHOT.jar
+-jar owo-2.3.255-SNAPSHOT.jar
--- compiler/fir/modularized-tests/run.sh
+++ compiler/fir/modularized-tests/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-/Users/Simon.Ogorodnik/Workspace/KotlinRepo/main/compiler/fir/modularized-tests/build/libs/owo-2.1.255-SNAPSHOT \
+/Users/Simon.Ogorodnik/Workspace/KotlinRepo/work2/compiler/fir/modularized-tests/build/libs/owo-2.3.255-SNAPSHOT \
 -DcacheRedirectorEnabled=true \
 -Dfir.bench.dump=false \
--Dfir.bench.jps.dir=/Users/Simon.Ogorodnik/TestWorkspace/mt-kotlin2/test-project-model-dump \
+-Dfir.bench.jps.dir=/Users/Simon.Ogorodnik/TestWorkspace/okhttp-bench/modelDump \
 -Dfir.bench.passes=1 \
--Dfir.bench.prefix=/Users/Simon.Ogorodnik/TestWorkspace/mt-kotlin2 \
--Didea.home.path=/Users/Simon.Ogorodnik/Workspace/KotlinRepo/main/build/ideaHomeForTests \
+-Dfir.bench.prefix=/ \
+-Didea.home.path=/Users/Simon.Ogorodnik/Workspace/KotlinRepo/work2/build/ideaHomeForTests \
 -Didea.ignore.disabled.plugins=true \
 -Didea.is.unit.test=true \
 -Didea.use.native.fs.for.win=false \
 -Djava.awt.headless=true \
 -Djna.nosys=true \
--Djps.kotlin.home=/Users/Simon.Ogorodnik/Workspace/KotlinRepo/main/dist/kotlinc \
+-Djps.kotlin.home=/Users/Simon.Ogorodnik/Workspace/KotlinRepo/work2/dist/kotlinc \
 -Xmx8192m \
 -Xms8192m
