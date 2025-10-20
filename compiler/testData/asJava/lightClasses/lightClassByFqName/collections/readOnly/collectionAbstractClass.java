2a3e02aa4203cda86124bbb4bd9b5b250693af88
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-20 20:13:34 +0400

    [SLC] WIP don't render abstract

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/collectionAbstractClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/collectionAbstractClass.java
@@ -1,27 +1,27 @@
 public abstract class CCollection /* test.CCollection*/<Elem>  implements java.util.Collection<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
   public  CCollection();//  .ctor()
 
-  public abstract <T> T[] toArray(T[]);// <T>  toArray(T[])
-
-  public abstract boolean add(Elem);//  add(Elem)
-
-  public abstract boolean addAll(java.util.Collection<? extends Elem>);//  addAll(java.util.Collection<? extends Elem>)
-
-  public abstract boolean equals(java.lang.Object);//  equals(java.lang.Object)
-
-  public abstract boolean remove(java.lang.Object);//  remove(java.lang.Object)
-
-  public abstract boolean removeAll(java.util.Collection<?>);//  removeAll(java.util.Collection<?>)
-
-  public abstract boolean retainAll(java.util.Collection<?>);//  retainAll(java.util.Collection<?>)
+  public <T> T[] toArray(T[]);// <T>  toArray(T[])
 
   public abstract int getSize();//  getSize()
 
-  public abstract int hashCode();//  hashCode()
+  public boolean add(Elem);//  add(Elem)
 
-  public abstract java.lang.Object[] toArray();//  toArray()
+  public boolean addAll(java.util.Collection<? extends Elem>);//  addAll(java.util.Collection<? extends Elem>)
 
-  public abstract void clear();//  clear()
+  public boolean equals(java.lang.Object);//  equals(java.lang.Object)
 
-  public final abstract int size();//  size()
+  public boolean remove(java.lang.Object);//  remove(java.lang.Object)
+
+  public boolean removeAll(java.util.Collection<?>);//  removeAll(java.util.Collection<?>)
+
+  public boolean retainAll(java.util.Collection<?>);//  retainAll(java.util.Collection<?>)
+
+  public final int size();//  size()
+
+  public int hashCode();//  hashCode()
+
+  public java.lang.Object[] toArray();//  toArray()
+
+  public void clear();//  clear()
 }
