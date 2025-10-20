e812460635c5535202fd75ba902b954b9a465c1e
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-20 21:04:05 +0400

    [SLC] WIP iterableAbstractClass
    TODO difference in source and lib

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/iterableAbstractClass.kmp.lib.java
+++
@@ -1,6 +0,0 @@
-public abstract class CIterable /* test.CIterable*/<Elem>  implements java.lang.Iterable<Elem>, kotlin.collections.Iterable<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
-
-  public  CIterable();//  .ctor()
-}
