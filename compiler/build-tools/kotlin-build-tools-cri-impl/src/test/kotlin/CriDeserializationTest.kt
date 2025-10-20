45517650f3925766cf5df5d08368e0c96dc1ad5a
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-10-20 19:52:37 +0200

    [BTA] update 'LookupEntry' CRI schema to use fqName hashCode instead of artificial complex hash. No migration needed as no CRI data is currently being produced.
    ^KT-81886 In progress

--- compiler/build-tools/kotlin-build-tools-cri-impl/src/test/kotlin/CriDeserializationTest.kt
+++ compiler/build-tools/kotlin-build-tools-cri-impl/src/test/kotlin/CriDeserializationTest.kt
@@ -31,9 +31,9 @@ class CriDeserializationTest {
     @Test
     fun testLookupsSerialization() {
         val lookups = listOf(
-            LookupEntryImpl(0L, emptyList()),
-            LookupEntryImpl(1L, listOf(0)),
-            LookupEntryImpl(2L, listOf(1, 2)),
+            LookupEntryImpl(0, emptyList()),
+            LookupEntryImpl(1, listOf(0)),
+            LookupEntryImpl(2, listOf(1, 2)),
         )
         val serializedLookups = ProtoBuf.encodeToByteArray(lookups)
         val deserializer = CriDataDeserializerImpl()
@@ -49,7 +49,7 @@ class CriDeserializationTest {
 
             // serial name 'org.jetbrains.kotlin.buildtools.internal.cri.LookupEntryImpl'
             message LookupEntryImpl {
-              optional int64 key = 1;
+              optional int32 fqNameHashCode = 1;
               repeated int32 fileIds = 2;
             }
 
