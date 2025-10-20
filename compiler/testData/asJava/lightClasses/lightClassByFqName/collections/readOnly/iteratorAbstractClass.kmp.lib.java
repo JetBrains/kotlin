f1869ce4d674c78cd41dcc7e8c16294a4f8fd229
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-20 21:22:30 +0400

    [SLC] WIP iteratorAbstractClass

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/iteratorAbstractClass.kmp.lib.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/iteratorAbstractClass.kmp.lib.java
@@ -1,7 +1,3 @@
-public abstract class CIterator /* test.CIterator*/<Elem>  implements java.util.Iterator<Elem>, kotlin.collections.Iterator<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
+public abstract class CIterator /* test.CIterator*/<Elem>  implements java.util.Iterator<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
   public  CIterator();//  .ctor()
-
-  public Elem next();//  next()
-
-  public boolean hasNext();//  hasNext()
 }
