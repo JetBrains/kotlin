871ec9c7f255387afc52dcbd12880423b811b411
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-20 20:49:43 +0400

    [SLC] WIP mutableCollectionClass.java

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableCollectionClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableCollectionClass.java
@@ -3,17 +3,7 @@ public abstract class CMutableCollection /* test.CMutableCollection*/<Elem>  imp
 
   public <T> T[] toArray(T[]);// <T>  toArray(T[])
 
-  public abstract boolean remove(Elem);//  remove(Elem)
-
   public abstract int getSize();//  getSize()
-
-  public boolean contains(java.lang.Object);//  contains(java.lang.Object)
-
-  public boolean containsAll(java.util.Collection<?>);//  containsAll(java.util.Collection<?>)
-
-  public boolean isEmpty();//  isEmpty()
-
-  public final boolean remove(java.lang.Object);//  remove(java.lang.Object)
 
   public final int size();//  size()
 
