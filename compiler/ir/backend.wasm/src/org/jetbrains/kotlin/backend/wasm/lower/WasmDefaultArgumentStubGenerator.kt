9e8b8b3e13375177295eb577a16e1d8ff9e12bf8
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-10-20 16:25:48 +0200

    [WASM] Create WASM variants for some lowerings to represent prerequisites
    #KT-67461
    #KT-67463

---
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmDefaultArgumentStubGenerator.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.wasm.lower
+
+import org.jetbrains.kotlin.backend.common.lower.DefaultArgumentStubGenerator
+import org.jetbrains.kotlin.backend.common.lower.MaskedDefaultArgumentFunctionFactory
+import org.jetbrains.kotlin.backend.wasm.WasmBackendContext
+
+internal class WasmDefaultArgumentStubGenerator(
+    context: WasmBackendContext,
+) : DefaultArgumentStubGenerator<WasmBackendContext>(
+    context,
+    MaskedDefaultArgumentFunctionFactory(context, copyOriginalFunctionLocation = false),
+    skipExternalMethods = true
+)
---
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/WasmDefaultParameterPatchOverridenSymbolsLowering.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.backend.wasm.lower
+
+import org.jetbrains.kotlin.backend.common.lower.DefaultParameterPatchOverridenSymbolsLowering
+import org.jetbrains.kotlin.backend.common.phaser.PhasePrerequisites
+import org.jetbrains.kotlin.backend.wasm.WasmBackendContext
+
+@PhasePrerequisites(WasmDefaultArgumentStubGenerator::class)
+internal class WasmDefaultParameterPatchOverridenSymbolsLowering(
+    context: WasmBackendContext
+) : DefaultParameterPatchOverridenSymbolsLowering(context)
