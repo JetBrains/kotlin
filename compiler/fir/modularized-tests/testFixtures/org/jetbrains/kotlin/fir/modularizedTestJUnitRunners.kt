02eb8ecf70647b280c2faaff723e3b7455bb5671
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2025-10-21 18:52:07 +0200

    MT tests: refactor tests with testFixture plugin properly

--- compiler/fir/modularized-tests/testFixtures/org/jetbrains/kotlin/fir/modularizedTestJUnitRunners.kt
+++ compiler/fir/modularized-tests/testFixtures/org/jetbrains/kotlin/fir/modularizedTestJUnitRunners.kt
@@ -9,10 +9,8 @@ import org.jetbrains.kotlin.cli.common.ExitCode
 import org.jetbrains.kotlin.test.util.JUnit4Assertions.assertEquals
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
-
 
 abstract class AbstractModularizedJUnit5Test<T : AbstractModularizedTest>(protected val test: T) {
     @BeforeEach
@@ -26,36 +24,7 @@ abstract class AbstractModularizedJUnit5Test<T : AbstractModularizedTest>(protec
     }
 }
 
-class FullPipelineModularizedTest : AbstractModularizedJUnit5Test<FullPipelineModularizedTestPure>(
-    FullPipelineModularizedTestPure(modularizedTestConfigFromSystemProperties())
-) {
-    @Test
-    fun testTotalKotlin() = test.testTotalKotlin()
-}
-
-class FE1FullPipelineModularizedTest : AbstractModularizedJUnit5Test<FE1FullPipelineModularizedTestPure>(
-    FE1FullPipelineModularizedTestPure(modularizedTestConfigFromSystemProperties())
-) {
-    @Test
-    fun testTotalKotlin() = test.testTotalKotlin()
-}
-
-class FirResolveModularizedTotalKotlinTest : AbstractModularizedJUnit5Test<FirResolveModularizedTotalKotlinTestPure>(
-    @Test
-    FirResolveModularizedTotalKotlinTestPure(modularizedTestConfigFromSystemProperties())
-) {
-    fun testTotalKotlin() = test.testTotalKotlin()
-}
-
-class NonFirResolveModularizedTotalKotlinTest : AbstractModularizedJUnit5Test<NonFirResolveModularizedTotalKotlinTestPure>(
-    NonFirResolveModularizedTotalKotlinTestPure(modularizedTestConfigFromSystemProperties())
-) {
-    @Test
-    fun testTotalKotlin() = test.testTotalKotlin()
-}
-
 // base for generated tests
-@Suppress("unused")
 abstract class AbstractIsolatedFulPipelineTestRunner {
 
     @TempDir
