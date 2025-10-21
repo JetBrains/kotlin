8a42b7222a2386995c0ef3f167bb427c06160fd9
Timofey Solonin <timofey.solonin@jetbrains.com>
2025-10-21 23:11:12 +0200

    Separate packages checkout from generating def files and running xcodebuild

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/apple/swiftimport/SwiftImportExtension.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/apple/swiftimport/SwiftImportExtension.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.kotlin.gradle.plugin.mpp.apple.swiftimport
 
-import org.gradle.api.DomainObjectCollection
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
@@ -10,17 +9,47 @@ import javax.inject.Inject
 abstract class SwiftImportExtension @Inject constructor(
     private val objects: ObjectFactory,
 ) {
-    // FIXME: Maybe tests with against RECOMMENDED_ version?
-    val iosDeploymentVersion: Property<String> = objects.property(String::class.java).convention(
-        "15.0"
-    )
-    abstract val spmDependencies: DomainObjectSet<SwiftPMDependency>
+    // FIXME: Maybe tests against CI RECOMMENDED_ version to keep up to date?
+    val iosDeploymentVersion: Property<String> = objects.property(String::class.java)
+        .convention("15.0")
+    val macosDeploymentVersion: Property<String> = objects.property(String::class.java)
+        .convention("10.15")
+    val watchosDeploymentVersion: Property<String> = objects.property(String::class.java)
+        .convention("15.0")
+    val tvosDeploymentVersion: Property<String> = objects.property(String::class.java)
+        .convention("7.0")
+
+    internal abstract val spmDependencies: DomainObjectSet<SwiftPMDependency>
+
+    fun `package`(
+        repository: String,
+        fromVersion: String,
+        products: List<String>,
+        cinteropClangModules: List<String> = products
+    ) {
+        // FIXME: check if this is actually correct
+        val packageName = repository.split("/").last().split(".git").first()
+        spmDependencies.add(
+            SwiftPMDependency(
+                repository = repository,
+                fromVersion = fromVersion,
+                packageName = packageName,
+                products = products,
+                cinteropClangModules = cinteropClangModules
+            )
+        )
+    }
+
+    companion object {
+        const val EXTENSION_NAME = "swiftPMDependencies"
+    }
 }
 
-data class SwiftPMDependency(
+internal data class SwiftPMDependency(
     val repository: String,
-    val version: String,
+    val fromVersion: String,
+    // FIXME: This can actually be inferred from the repository URL
     val packageName: String,
     val products: List<String>,
-    val cinteropTargets: List<String>,
+    val cinteropClangModules: List<String>,
 ) : Serializable
