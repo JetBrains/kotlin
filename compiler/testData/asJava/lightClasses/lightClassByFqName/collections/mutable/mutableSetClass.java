bc745c9ee36c5251cb55b6dc2d5031d1b60e602a
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-22 21:27:41 +0400

    [SLC] WIP mutableSetClass

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableSetClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableSetClass.java
@@ -1,39 +1,11 @@
-public abstract class CMutableSet /* test.CMutableSet*/<Elem>  implements java.util.Set<Elem>, kotlin.collections.MutableSet<Elem>, kotlin.jvm.internal.markers.KMutableSet {
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  public boolean add(Elem);//  add(Elem)
-
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  public boolean addAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  addAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
-
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  public boolean remove(Elem);//  remove(Elem)
-
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  public boolean removeAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  removeAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
-
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  public boolean retainAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  retainAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
-
-  @java.lang.Override()
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
-
-  @java.lang.Override()
-  public boolean contains(Elem);//  contains(Elem)
-
-  @java.lang.Override()
-  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
-
-  @java.lang.Override()
-  public boolean isEmpty();//  isEmpty()
-
-  @java.lang.Override()
-  public void clear();//  clear()
-
+public abstract class CMutableSet /* test.CMutableSet*/<Elem>  implements java.util.Set<Elem>, kotlin.jvm.internal.markers.KMutableSet {
   public  CMutableSet();//  .ctor()
+
+  public <T> T[] toArray(T[]);// <T>  toArray(T[])
+
+  public abstract int getSize();//  getSize()
+
+  public final int size();//  size()
+
+  public java.lang.Object[] toArray();//  toArray()
 }
