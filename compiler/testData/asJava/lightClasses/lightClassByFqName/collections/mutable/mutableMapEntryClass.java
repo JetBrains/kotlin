6909772e9a516fd3abe9584c70f1ca4ba9313e0a
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-22 21:24:21 +0400

    [SLC] WIP mutableMapEntryClass

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableMapEntryClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableMapEntryClass.java
@@ -1,13 +1,3 @@
-public abstract class CMutableMapEntry /* test.CMutableMapEntry*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.collections.MutableMap.MutableEntry<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap$Entry {
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  public VElem setValue(VElem);//  setValue(VElem)
-
-  @java.lang.Override()
-  public KElem getKey();//  getKey()
-
-  @java.lang.Override()
-  public VElem getValue();//  getValue()
-
+public abstract class CMutableMapEntry /* test.CMutableMapEntry*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap$Entry {
   public  CMutableMapEntry();//  .ctor()
 }
