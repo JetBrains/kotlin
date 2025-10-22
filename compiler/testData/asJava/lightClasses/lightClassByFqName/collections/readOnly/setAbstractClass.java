a4c8ff3bb70aa9af756dd677a6a18c24464fa523
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-22 20:16:40 +0400

    [SLC] WIP setAbstractClass

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/setAbstractClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/setAbstractClass.java
@@ -1,19 +1,23 @@
-public abstract class CSet /* test.CSet*/<Elem>  implements java.util.Set<Elem>, kotlin.collections.Set<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
-  @java.lang.Override()
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
-
-  @java.lang.Override()
-  public boolean contains(Elem);//  contains(Elem)
-
-  @java.lang.Override()
-  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
-
-  @java.lang.Override()
-  public boolean isEmpty();//  isEmpty()
-
-  @java.lang.Override()
-  public int getSize();//  getSize()
-
+public abstract class CSet /* test.CSet*/<Elem>  implements java.util.Set<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
   public  CSet();//  .ctor()
+
+  public <T> T[] toArray(T[]);// <T>  toArray(T[])
+
+  public abstract int getSize();//  getSize()
+
+  public boolean add(Elem);//  add(Elem)
+
+  public boolean addAll(java.util.Collection<? extends Elem>);//  addAll(java.util.Collection<? extends Elem>)
+
+  public boolean remove(java.lang.Object);//  remove(java.lang.Object)
+
+  public boolean removeAll(java.util.Collection<?>);//  removeAll(java.util.Collection<?>)
+
+  public boolean retainAll(java.util.Collection<?>);//  retainAll(java.util.Collection<?>)
+
+  public final int size();//  size()
+
+  public java.lang.Object[] toArray();//  toArray()
+
+  public void clear();//  clear()
 }
