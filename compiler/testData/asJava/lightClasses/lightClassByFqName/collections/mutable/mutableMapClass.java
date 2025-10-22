305aec0af9e97c6de1490e4ccba43fbafd7a3824
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-22 21:21:14 +0400

    [SLC] WIP mutableMapClass

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableMapClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/mutable/mutableMapClass.java
@@ -1,47 +1,19 @@
-public abstract class CMutableMap /* test.CMutableMap*/<KElem, VElem>  implements java.util.Map<KElem, VElem>, kotlin.collections.MutableMap<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap {
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  @org.jetbrains.annotations.Nullable()
-  public @org.jetbrains.annotations.Nullable() VElem put(KElem, VElem);//  put(KElem, VElem)
-
-  @java.lang.Override()
-  @kotlin.IgnorableReturnValue()
-  @org.jetbrains.annotations.Nullable()
-  public @org.jetbrains.annotations.Nullable() VElem remove(KElem);//  remove(KElem)
-
-  @java.lang.Override()
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Collection<VElem> getValues();//  getValues()
-
-  @java.lang.Override()
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Set<@org.jetbrains.annotations.NotNull() java.util.Map.Entry<KElem, VElem>> getEntries();//  getEntries()
-
-  @java.lang.Override()
-  @org.jetbrains.annotations.NotNull()
-  public @org.jetbrains.annotations.NotNull() java.util.Set<KElem> getKeys();//  getKeys()
-
-  @java.lang.Override()
-  @org.jetbrains.annotations.Nullable()
-  public @org.jetbrains.annotations.Nullable() VElem get(KElem);//  get(KElem)
-
-  @java.lang.Override()
-  public boolean containsKey(KElem);//  containsKey(KElem)
-
-  @java.lang.Override()
-  public boolean containsValue(VElem);//  containsValue(VElem)
-
-  @java.lang.Override()
-  public boolean isEmpty();//  isEmpty()
-
-  @java.lang.Override()
-  public int getSize();//  getSize()
-
-  @java.lang.Override()
-  public void clear();//  clear()
-
-  @java.lang.Override()
-  public void putAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Map<? extends KElem, ? extends VElem>);//  putAll(@org.jetbrains.annotations.NotNull() java.util.Map<? extends KElem, ? extends VElem>)
-
+public abstract class CMutableMap /* test.CMutableMap*/<KElem, VElem>  implements java.util.Map<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap {
   public  CMutableMap();//  .ctor()
+
+  public abstract int getSize();//  getSize()
+
+  public abstract java.util.Collection<VElem> getValues();//  getValues()
+
+  public abstract java.util.Set<KElem> getKeys();//  getKeys()
+
+  public abstract java.util.Set<java.util.Map.Entry<KElem, VElem>> getEntries();//  getEntries()
+
+  public final int size();//  size()
+
+  public final java.util.Collection<VElem> values();//  values()
+
+  public final java.util.Set<KElem> keySet();//  keySet()
+
+  public final java.util.Set<java.util.Map.Entry<KElem, VElem>> entrySet();//  entrySet()
 }
