eab7499da95380ec2e4d9dfd87fc5166e25d199c
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-23 23:49:45 +0400

    [SLC] WIP delegation support

--- compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/collectionAbstractClassOfA.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/collections/readOnly/collectionAbstractClassOfA.java
@@ -1,13 +1,7 @@
-public abstract class ACollection /* test.ACollection*/ implements java.util.Collection<@org.jetbrains.annotations.NotNull() test.A>, kotlin.collections.Collection<@org.jetbrains.annotations.NotNull() test.A>, kotlin.jvm.internal.markers.KMappedMarker {
+public abstract class ACollection /* test.ACollection*/ implements java.util.Collection<@org.jetbrains.annotations.NotNull() test.A>, kotlin.jvm.internal.markers.KMappedMarker {
   @java.lang.Override()
   @org.jetbrains.annotations.NotNull()
   public @org.jetbrains.annotations.NotNull() java.util.Iterator<@org.jetbrains.annotations.NotNull() test.A> iterator();//  iterator()
-
-  @java.lang.Override()
-  public boolean contains(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() test.A);//  contains(@org.jetbrains.annotations.NotNull() test.A)
-
-  @java.lang.Override()
-  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() test.A>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() test.A>)
 
   @java.lang.Override()
   public boolean isEmpty();//  isEmpty()
@@ -16,4 +10,26 @@ public abstract class ACollection /* test.ACollection*/ implements java.util.Col
   public int getSize();//  getSize()
 
   public  ACollection();//  .ctor()
+
+  public <T> T[] toArray(T[]);// <T>  toArray(T[])
+
+  public boolean add(@org.jetbrains.annotations.NotNull() test.A);//  add(@org.jetbrains.annotations.NotNull() test.A)
+
+  public boolean addAll(java.util.Collection<? extends @org.jetbrains.annotations.NotNull() test.A>);//  addAll(java.util.Collection<? extends @org.jetbrains.annotations.NotNull() test.A>)
+
+  public boolean contains(java.lang.Object);//  contains(java.lang.Object)
+
+  public boolean containsAll(java.util.Collection<?>);//  containsAll(java.util.Collection<?>)
+
+  public boolean remove(java.lang.Object);//  remove(java.lang.Object)
+
+  public boolean removeAll(java.util.Collection<?>);//  removeAll(java.util.Collection<?>)
+
+  public boolean retainAll(java.util.Collection<?>);//  retainAll(java.util.Collection<?>)
+
+  public final int size();//  size()
+
+  public java.lang.Object[] toArray();//  toArray()
+
+  public void clear();//  clear()
 }
