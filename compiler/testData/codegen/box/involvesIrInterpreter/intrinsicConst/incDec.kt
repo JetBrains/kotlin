f2e4b35669ccf8d48c6fa7aa61383b51ff708c35
Florian Freitag <florian.freitag.external@jetbrains.com>
2025-10-23 16:30:11 +0200

    fixup! [FIR] Add dec and inc methods for builtins to const evaluation

--- compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/incDec.kt
+++ compiler/testData/codegen/box/involvesIrInterpreter/intrinsicConst/incDec.kt
@@ -25,25 +25,25 @@ const val byteVal4 = Byte.MIN_VALUE.<!EVALUATED("127")!>dec()<!>
 
 // STOP_EVALUATION_CHECKS
 fun box(): String {
-    if (longVal1 != 3L)                 return "Fail longVal1"
-    if (longVal2 != 1L)                 return "Fail longVal2"
-    if (longVal3 != Long.MIN_VALUE)     return "Fail longVal3"
-    if (longVal4 != Long.MAX_VALUE)     return "Fail longVal4"
+    if (longVal1.id() != 3L)                    return "Fail longVal1"
+    if (longVal2.id()!= 1L)                     return "Fail longVal2"
+    if (longVal3.id() != Long.MIN_VALUE)        return "Fail longVal3"
+    if (longVal4.id() != Long.MAX_VALUE)        return "Fail longVal4"
 
-    if (intVal1 != 3)                   return "Fail intVal1"
-    if (intVal2 != 1)                   return "Fail intVal2"
-    if (intVal3 != Int.MIN_VALUE)       return "Fail intVal3"
-    if (intVal4 != Int.MAX_VALUE)       return "Fail intVal4"
+    if (intVal1.id() != 3)                      return "Fail intVal1"
+    if (intVal2.id() != 1)                      return "Fail intVal2"
+    if (intVal3.id() != Int.MIN_VALUE)          return "Fail intVal3"
+    if (intVal4.id() != Int.MAX_VALUE)          return "Fail intVal4"
 
-    if (shortVal1 != 3.toShort())       return "Fail shortVal1"
-    if (shortVal2 != 1.toShort())       return "Fail shortVal2"
-    if (shortVal3 != Short.MIN_VALUE)   return "Fail shortVal3"
-    if (shortVal4 != Short.MAX_VALUE)   return "Fail shortVal4"
+    if (shortVal1.id() != 3.toShort())          return "Fail shortVal1"
+    if (shortVal2.id() != 1.toShort())          return "Fail shortVal2"
+    if (shortVal3.id() != Short.MIN_VALUE)      return "Fail shortVal3"
+    if (shortVal4.id() != Short.MAX_VALUE)      return "Fail shortVal4"
 
-    if (byteVal1 != 3.toByte())         return "Fail byteVal1"
-    if (byteVal2 != 1.toByte())         return "Fail byteVal2"
-    if (byteVal3 != Byte.MIN_VALUE)     return "Fail byteVal3"
-    if (byteVal4 != Byte.MAX_VALUE)     return "Fail byteVal4"
+    if (byteVal1.id() != 3.toByte())            return "Fail byteVal1"
+    if (byteVal2.id() != 1.toByte())            return "Fail byteVal2"
+    if (byteVal3.id() != Byte.MIN_VALUE)        return "Fail byteVal3"
+    if (byteVal4.id() != Byte.MAX_VALUE)        return "Fail byteVal4"
 
     return "OK"
 }
