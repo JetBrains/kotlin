9615f7eb8008d6b084012360e716b7f2ba4e240b
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-24 18:09:14 +0400

    [SLC] WIP mapAbstractClass delegation

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapAbstractClass.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapAbstractClass.kt
@@ -1,6 +1,6 @@
-// test.CMap
 // WITH_STDLIB
-
 package test
 
 abstract class CMap<KElem, VElem> : Map<KElem, VElem>
+
+abstract class CMap2<KElem, VElem> : Map<KElem, VElem> by emptyMap<KElem, VElem>()
