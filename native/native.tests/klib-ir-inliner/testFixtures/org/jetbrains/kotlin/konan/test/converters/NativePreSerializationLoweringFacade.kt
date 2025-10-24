415374a79e660c1c3f5a542344319c556a29cf6a
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-10-24 13:10:42 +0200

    [IR] Move logic of runPreSerializationLoweringPhases to backend.common module

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/converters/NativePreSerializationLoweringFacade.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/converters/NativePreSerializationLoweringFacade.kt
@@ -7,7 +7,7 @@ package org.jetbrains.kotlin.konan.test.converters
 
 import org.jetbrains.kotlin.backend.common.phaser.PhaseEngine
 import org.jetbrains.kotlin.backend.konan.NativePreSerializationLoweringContext
-import org.jetbrains.kotlin.cli.common.runPreSerializationLoweringPhases
+import org.jetbrains.kotlin.backend.common.runPreSerializationLoweringPhases
 import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.config.messageCollector
