75e3a44704be412d3bd459b40c3457fadc8d032a
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-10-24 12:16:27 +0200

    KLIB API: Write theIR through `KlibIrWriterImpl` (in the compiler)
    The new `KlibIrWriterImpl` class is the replacement for
    the obsolete KLIB writing API.
    
    In this commit, `KlibIrWriterImpl` is integrated into the standard
    compiler endpoints: `buildKotlinLibrary()` and `buildLibrary()`.
    
    ^KT-81411

--- compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibraryLayout.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibraryLayout.kt
@@ -17,16 +17,6 @@
 package org.jetbrains.kotlin.library
 
 import org.jetbrains.kotlin.konan.file.File
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_BODIES_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_DEBUG_INFO_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_DECLARATIONS_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_FILES_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_FILE_ENTRIES_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_FOLDER_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_INLINABLE_FUNCTIONS_FOLDER_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_SIGNATURES_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_STRINGS_FILE_NAME
-import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_TYPES_FILE_NAME
 
 const val KLIB_MANIFEST_FILE_NAME = "manifest"
 const val KLIB_RESOURCES_FOLDER_NAME = "resources"
@@ -47,36 +37,4 @@ interface KotlinLibraryLayout {
         get() = File(componentDir, KLIB_RESOURCES_FOLDER_NAME)
     val pre_1_4_manifest: File
         get() = File(libFile, KLIB_MANIFEST_FILE_NAME)
-}
-
-interface IrKotlinLibraryLayout : KotlinLibraryLayout {
-    val mainIr: IrDirectory
-        get() = IrDirectory(File(componentDir, KLIB_IR_FOLDER_NAME))
-
-    // This directory is similar to the main "ir" directory but contains only specially prepared copies of public inline functions,
-    // instead of the entire IR.
-    // Those may be read and inlined on the first stage of compilation, without the need to read the main, much bigger,
-    // "ir" directory (see KT-75794).
-    val inlineableFunsIr: IrDirectory
-        get() = IrDirectory(File(componentDir, KLIB_IR_INLINABLE_FUNCTIONS_FOLDER_NAME))
-
-    class IrDirectory(val dir: File) {
-        val irDeclarations
-            get() = File(dir, KLIB_IR_DECLARATIONS_FILE_NAME)
-        val irTypes
-            get() = File(dir, KLIB_IR_TYPES_FILE_NAME)
-        val irSignatures
-            get() = File(dir, KLIB_IR_SIGNATURES_FILE_NAME)
-        val irStrings
-            get() = File(dir, KLIB_IR_STRINGS_FILE_NAME)
-        val irBodies
-            get() = File(dir, KLIB_IR_BODIES_FILE_NAME)
-        val irFiles
-            get() = File(dir, KLIB_IR_FILES_FILE_NAME)
-        val irDebugInfo
-            get() = File(dir, KLIB_IR_DEBUG_INFO_FILE_NAME)
-        // Please check `hasFileEntriesTable` before getter invocation, otherwise it may crash in override getter
-        val irFileEntries
-            get() = File(dir, KLIB_IR_FILE_ENTRIES_FILE_NAME)
-    }
 }
