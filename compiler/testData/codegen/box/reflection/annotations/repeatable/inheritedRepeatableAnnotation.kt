31eb44645ea4685681d1cb7f4def9995ea4eb558
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-10-27 18:29:36 +0100

    add (failing) test for repeatable inherited annotations

---
+++ compiler/testData/codegen/box/reflection/annotations/repeatable/inheritedRepeatableAnnotation.kt
@@ -0,0 +1,57 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FULL_JDK
+
+import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.findAnnotations
+import kotlin.reflect.full.hasAnnotation
+
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class NotInheritedAnno
+
+@java.lang.annotation.Inherited
+@Repeatable
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Anno(val value: String = "myDefaultValue")
+
+@NotInheritedAnno
+@Anno
+open class BaseClass
+
+@Anno("1")
+@Anno("2")
+open class MiddleClass: BaseClass()
+
+@Anno("3")
+class ChildClass1: MiddleClass()
+
+class ChildClass2: MiddleClass()
+
+private fun test(klass: KClass<*>, vararg expectedValues: String) {
+    val expected = expectedValues.map { Anno(it) }.toSet()
+
+    assertEquals(expected, klass.annotations.toSet(), "Failed annotations for $klass")
+    assertEquals(expected, klass.findAnnotations<Anno>().toSet(), "Failed findAnnotations() for $klass")
+
+    val found = klass.findAnnotation<Anno>() // any of expected annotations may be returned
+    assertNotNull(found, "Failed findAnnotation() for $klass: not found")
+    assertContains(expected, found, "Failed findAnnotation() for $klass: invalid result")
+
+    assertTrue(klass.hasAnnotation<Anno>(), "Failed hasAnnotation for $klass")
+}
+
+fun box(): String {
+    test(MiddleClass::class, "1", "2")
+    test(ChildClass1::class, "3")
+    test(ChildClass2::class, "1", "2")
+
+    return "OK"
+}
