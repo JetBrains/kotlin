b4c7af1cd8f54d0f113ae43a2d7fef507ab602bb
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-27 17:25:12 +0400

    [SLC] WIP setAbstractClass

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/setAbstractClass.kmp.lib.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/setAbstractClass.kmp.lib.java
@@ -1,3 +1,26 @@
 public abstract class CSet /* test.CSet*/<Elem>  implements java.util.Set<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
   public  CSet();//  .ctor()
 }
+
+public abstract class CSet2 /* test.CSet2*/<Elem>  implements java.util.Set<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
+  private final int size;
+
+  @kotlin.SinceKotlin()
+  @kotlin.js.ExperimentalJsCollectionsApi()
+  @kotlin.js.ExperimentalJsExport()
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() kotlin.js.collections.JsReadonlySet<Elem> asJsReadonlySetView();//  asJsReadonlySetView()
+
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
+
+  public  CSet2();//  .ctor()
+
+  public boolean contains(Elem);//  contains(Elem)
+
+  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
+
+  public boolean isEmpty();//  isEmpty()
+
+  public int getSize();//  getSize()
+}
