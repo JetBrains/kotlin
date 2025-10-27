fa4d93beace3b7a3831db2e03531391d4a777455
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-27 17:13:30 +0400

    [SLC] WIP mapEntryAbstractClass

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.kmp.lib.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.kmp.lib.java
@@ -1,3 +1,15 @@
 public abstract class CMapEntry /* test.CMapEntry*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMappedMarker {
   public  CMapEntry();//  .ctor()
 }
+
+public abstract class CMapEntry2 /* test.CMapEntry2*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMappedMarker {
+  private final KElem key;
+
+  private final VElem value;
+
+  public  CMapEntry2();//  .ctor()
+
+  public KElem getKey();//  getKey()
+
+  public VElem getValue();//  getValue()
+}
