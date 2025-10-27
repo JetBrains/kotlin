ce21fc677660b060dfe4891f28cc51bbf11ec8c9
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-27 17:57:24 +0400

    [SLC] WIP setInterface

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/setInterface.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/setInterface.java
@@ -1,2 +1,2 @@
-public abstract interface ISet /* test.ISet*/<Elem>  extends java.util.Set<Elem>, kotlin.collections.Set<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
+public abstract interface ISet /* test.ISet*/<Elem>  extends java.util.Set<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
 }
