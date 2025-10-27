7fbaab00f8ff1babda1ce3537a13d95ea0303317
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-27 17:13:30 +0400

    [SLC] WIP mapEntryAbstractClass

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.java
@@ -3,3 +3,15 @@ public abstract class CMapEntry /* test.CMapEntry*/<KElem, VElem>  implements ja
 
   public VElem setValue(VElem);//  setValue(VElem)
 }
+
+public abstract class CMapEntry2 /* test.CMapEntry2*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMappedMarker {
+  @java.lang.Override()
+  public KElem getKey();//  getKey()
+
+  @java.lang.Override()
+  public VElem getValue();//  getValue()
+
+  public  CMapEntry2();//  .ctor()
+
+  public VElem setValue(VElem);//  setValue(VElem)
+}
--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.lib.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.lib.java
@@ -1,13 +1,15 @@
 public abstract class CMapEntry /* test.CMapEntry*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMappedMarker {
   public  CMapEntry();//  .ctor()
+
+  public VElem setValue(VElem);//  setValue(VElem)
 }
 
 public abstract class CMapEntry2 /* test.CMapEntry2*/<KElem, VElem>  implements java.util.Map.Entry<KElem, VElem>, kotlin.jvm.internal.markers.KMappedMarker {
-  @java.lang.Override()
+  public  CMapEntry2();//  .ctor()
+
   public KElem getKey();//  getKey()
 
-  @java.lang.Override()
   public VElem getValue();//  getValue()
 
-  public  CMapEntry2();//  .ctor()
+  public VElem setValue(VElem);//  setValue(VElem)
 }
