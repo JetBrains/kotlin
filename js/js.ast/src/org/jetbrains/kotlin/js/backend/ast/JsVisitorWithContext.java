5f6955d7fdffbd27eaa16c55df8cf8a117a61a69
Artem Kobzar <artem.kobzar@jetbrains.com>
2025-10-27 14:35:00 +0100

    fixup! Add lost endVisit for JsYield

--- js/js.ast/src/org/jetbrains/kotlin/js/backend/ast/JsVisitorWithContext.java
+++ js/js.ast/src/org/jetbrains/kotlin/js/backend/ast/JsVisitorWithContext.java
@@ -101,6 +101,9 @@ public abstract class JsVisitorWithContext {
     public void endVisit(@NotNull JsContinue x, @NotNull JsContext ctx) {
     }
 
+    public void endVisit(@NotNull JsYield x, @NotNull JsContext ctx) {
+    }
+
     public void endVisit(@NotNull JsYieldStar x, @NotNull JsContext ctx) {
     }
 
