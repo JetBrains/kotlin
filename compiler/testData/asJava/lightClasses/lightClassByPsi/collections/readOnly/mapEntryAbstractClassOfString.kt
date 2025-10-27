94fd092777d44c5702ffe4253f4d658d26b04fdb
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-27 17:18:55 +0400

    [SLC] WIP mapEntryAbstractClassOfString

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClassOfString.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClassOfString.kt
@@ -1,6 +1,6 @@
-// test.SMapEntry
 // WITH_STDLIB
-
 package test
 
-abstract class SMapEntry<VElem> : Map.Entry<String, VElem> by emptyMap<String, VElem>().entries.first()
+abstract class SMapEntry<VElem> : Map.Entry<String, VElem>
+
+abstract class SMapEntry2<VElem> : Map.Entry<String, VElem> by emptyMap<String, VElem>().entries.first()
