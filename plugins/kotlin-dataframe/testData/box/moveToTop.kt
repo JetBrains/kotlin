542dd273a9741500e2b193da478c97fa97ffe09d
Jolan Rensen <jolan.rensen@jetbrains.com>
2025-10-28 11:30:04 +0100

    [KDF] added explicit omission of newColumnName arg in move toTop. #1508

--- plugins/kotlin-dataframe/testData/box/moveToTop.kt
+++ plugins/kotlin-dataframe/testData/box/moveToTop.kt
@@ -8,5 +8,8 @@ fun box(): String {
     df.a
     df.c.b
     df.checkCompileTimeSchemaEqualsRuntime()
+
+    val df2 = dataFrameOf("a", "b")(1, 2).group { a and b }.into("c").move { c.a }.toTop { "newName" }
+    require(df2.compileTimeSchema().columns.isEmpty()) { "move toTop with lambda did not result in an empty compile time schema." }
     return "OK"
 }
