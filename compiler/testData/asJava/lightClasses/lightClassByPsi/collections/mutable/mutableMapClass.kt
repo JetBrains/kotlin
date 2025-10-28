39ef9d26848a1c435d30ab3513c79b70271d498c
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-28 22:52:03 +0400

    [SLC] WIP mutableMapClass

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableMapClass.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableMapClass.kt
@@ -1,6 +1,6 @@
-// test.CMutableMap
 // WITH_STDLIB
-
 package test
 
 abstract class CMutableMap<KElem, VElem> : MutableMap<KElem, VElem>
+
+abstract class CMutableMap2<KElem, VElem> : MutableMap<KElem, VElem> by mutableMapOf<KElem, VElem>()
