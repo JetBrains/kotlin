c1493f56f8ad2dd66d236257717a252c0f7f56dc
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-28 14:13:01 +0400

    [SLC] WIP mutableCollectionClass

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableCollectionClass.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableCollectionClass.java
@@ -9,3 +9,44 @@ public abstract class CMutableCollection /* test.CMutableCollection*/<Elem>  imp
 
   public java.lang.Object[] toArray();//  toArray()
 }
+
+public abstract class CMutableCollection2 /* test.CMutableCollection2*/<Elem>  implements java.util.Collection<Elem>, kotlin.jvm.internal.markers.KMutableCollection {
+  @java.lang.Override()
+  @kotlin.IgnorableReturnValue()
+  public boolean add(Elem);//  add(Elem)
+
+  @java.lang.Override()
+  @kotlin.IgnorableReturnValue()
+  public boolean addAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  addAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
+
+  @java.lang.Override()
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
+
+  @java.lang.Override()
+  public boolean isEmpty();//  isEmpty()
+
+  @java.lang.Override()
+  public int getSize();//  getSize()
+
+  @java.lang.Override()
+  public void clear();//  clear()
+
+  public  CMutableCollection2();//  .ctor()
+
+  public <T> T[] toArray(T[]);// <T>  toArray(T[])
+
+  public boolean contains(java.lang.Object);//  contains(java.lang.Object)
+
+  public boolean containsAll(java.util.Collection<?>);//  containsAll(java.util.Collection<?>)
+
+  public boolean remove(java.lang.Object);//  remove(java.lang.Object)
+
+  public boolean removeAll(java.util.Collection<?>);//  removeAll(java.util.Collection<?>)
+
+  public boolean retainAll(java.util.Collection<?>);//  retainAll(java.util.Collection<?>)
+
+  public final int size();//  size()
+
+  public java.lang.Object[] toArray();//  toArray()
+}
--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableCollectionClass.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableCollectionClass.kt
@@ -1,6 +1,6 @@
-// test.CMutableCollection
 // WITH_STDLIB
-
 package test
 
 abstract class CMutableCollection<Elem> : MutableCollection<Elem>
+
+abstract class CMutableCollection2<Elem> : MutableCollection<Elem> by mutableListOf<Elem>()
