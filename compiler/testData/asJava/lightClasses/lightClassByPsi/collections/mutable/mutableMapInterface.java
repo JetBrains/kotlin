f78c04286a23fa9e2eb4add6dfa47fe490acd444
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-28 23:17:53 +0400

    [SLC] WIP mutableMap

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableMapInterface.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableMapInterface.java
@@ -1,2 +1,2 @@
-public abstract interface IMutableMap /* test.IMutableMap*/<KElem, VElem>  extends java.util.Map<KElem, VElem>, kotlin.collections.MutableMap<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap {
+public abstract interface IMutableMap /* test.IMutableMap*/<KElem, VElem>  extends java.util.Map<KElem, VElem>, kotlin.jvm.internal.markers.KMutableMap {
 }
