2325d898d711d0ac2c47172546885fc314bf84cf
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-28 23:02:44 +0400

    [SLC] WIP mutableMapEntry

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableMapEntryOfStringToV.kmp.lib.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableMapEntryOfStringToV.kmp.lib.java
@@ -1,4 +1,8 @@
-public abstract class SMutableMapEntry /* test.SMutableMapEntry*/<VElem>  implements java.util.Map.Entry<@org.jetbrains.annotations.NotNull() java.lang.String, VElem>, kotlin.collections.MutableMap.MutableEntry<@org.jetbrains.annotations.NotNull() java.lang.String, VElem>, kotlin.jvm.internal.markers.KMutableMap$Entry {
+public abstract class SMutableMapEntry /* test.SMutableMapEntry*/<VElem>  implements java.util.Map.Entry<@org.jetbrains.annotations.NotNull() java.lang.String, VElem>, kotlin.jvm.internal.markers.KMutableMap$Entry {
+  public  SMutableMapEntry();//  .ctor()
+}
+
+public abstract class SMutableMapEntry2 /* test.SMutableMapEntry2*/<VElem>  implements java.util.Map.Entry<@org.jetbrains.annotations.NotNull() java.lang.String, VElem>, kotlin.jvm.internal.markers.KMutableMap$Entry {
   @org.jetbrains.annotations.NotNull()
   private final @org.jetbrains.annotations.NotNull() java.lang.String key;
 
@@ -10,7 +14,7 @@ public abstract class SMutableMapEntry /* test.SMutableMapEntry*/<VElem>  implem
   @org.jetbrains.annotations.NotNull()
   public @org.jetbrains.annotations.NotNull() java.lang.String getKey();//  getKey()
 
-  public  SMutableMapEntry();//  .ctor()
+  public  SMutableMapEntry2();//  .ctor()
 
   public VElem getValue();//  getValue()
 }
