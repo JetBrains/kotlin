2cd2384b0a7d79adac7f398367fb489d9c15b356
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-28 17:28:57 +0400

    [SLC] WIP mutableListClass
    TODO bug with "size" delegated property

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableListClass.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/mutable/mutableListClass.kt
@@ -1,6 +1,6 @@
-// test.CMutableList
 // WITH_STDLIB
-
 package test
 
 abstract class CMutableList<Elem> : MutableList<Elem>
+
+abstract class CMutableList2<Elem> : MutableList<Elem> by mutableListOf<Elem>()
