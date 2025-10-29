fca6196d5fe892ca587d927f331cbd0a609cef91
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2025-10-29 16:23:57 +0200

    fixup! [Tests] KT-82017: inline function overrides method with default value

--- compiler/testData/codegen/box/inline/inlineFunctionOverridesAbstractMethodWithDefaultValueWithIrInlinerBeforeKlibSerializationEnabled.kt
+++ compiler/testData/codegen/box/inline/inlineFunctionOverridesAbstractMethodWithDefaultValueWithIrInlinerBeforeKlibSerializationEnabled.kt
@@ -1,5 +1,6 @@
 // ISSUE: KT-82017
 // LANGUAGE: +IrIntraModuleInlinerBeforeKlibSerialization +IrCrossModuleInlinerBeforeKlibSerialization
+// IGNORE_IR_DESERIALIZATION_TEST: ANY
 // IGNORE_BACKEND: ANY
 // ^^^ KT-82017 Incomplete expression: call to FUN name:foo visibility:public modality:OPEN <> (<this>:<root>.A, a:kotlin.Int) returnType:kotlin.Int [inline] has no argument at index 1
 
