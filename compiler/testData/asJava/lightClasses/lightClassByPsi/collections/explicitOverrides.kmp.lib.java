2567b1386fa0ae22d15c0a3b9bb6b3f0adb71f91
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-10-30 15:45:52 +0400

    [SLC] WIP explicitOverrides

---
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/explicitOverrides.kmp.lib.java
@@ -0,0 +1,22 @@
+public abstract class CCollection /* test.CCollection*/<Elem>  implements java.util.Collection<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
+  public  CCollection();//  .ctor()
+
+  public boolean contains(Elem);//  contains(Elem)
+}
+
+public abstract class CCollection2 /* test.CCollection2*/<Elem>  extends test.CCollection<Elem> {
+  public  CCollection2();//  .ctor()
+
+  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<? extends Elem>)
+
+  public int getSize();//  getSize()
+}
+
+public abstract class CCollection3 /* test.CCollection3*/<Elem>  extends test.CCollection2<Elem> {
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() java.util.Iterator<Elem> iterator();//  iterator()
+
+  public  CCollection3();//  .ctor()
+
+  public boolean isEmpty();//  isEmpty()
+}
