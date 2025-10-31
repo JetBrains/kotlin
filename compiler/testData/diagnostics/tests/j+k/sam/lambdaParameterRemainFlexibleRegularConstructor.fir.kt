87e242cf3e299e910132aeff0aeb99d266068a48
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-10-31 14:59:55 +0100

    FE: Adjust test data after postponing DontMakeExplicitJavaTypeArgumentsFlexible
    And PreciseSimplificationToFlexibleLowerConstraint, too.
    
    Unfortunately, some issues have to be reopened
    
    ^KT-81988 Fixed
    ^KT-79864 Reopen
    ^KT-67825 Reopen

--- compiler/testData/diagnostics/tests/j+k/sam/lambdaParameterRemainFlexibleRegularConstructor.fir.kt
+++ compiler/testData/diagnostics/tests/j+k/sam/lambdaParameterRemainFlexibleRegularConstructor.fir.kt
@@ -15,7 +15,7 @@ public class J<T> {
 // FILE: main.kt
 fun main() {
     J<String?> { x ->
-        <!NULLABILITY_MISMATCH_BASED_ON_EXPLICIT_TYPE_ARGUMENTS_FOR_JAVA("String?; String;  This will become an error in language version 2.3. See https://youtrack.jetbrains.com/issue/KTLC-284.")!>x<!>.length // Should not be unsafe call
+        <!NULLABILITY_MISMATCH_BASED_ON_EXPLICIT_TYPE_ARGUMENTS_FOR_JAVA("String?; String;  This will become an error in a future release. See https://youtrack.jetbrains.com/issue/KTLC-284.")!>x<!>.length // Should not be unsafe call
     }
 }
 
