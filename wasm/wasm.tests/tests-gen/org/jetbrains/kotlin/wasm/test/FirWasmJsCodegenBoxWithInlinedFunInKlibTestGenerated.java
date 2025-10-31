fab3123752847d4cc6a6fd743d546411e1a40e03
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2025-10-31 15:02:01 +0100

    [Tests] Reproduce KT-81952
    #KT-81952
    
    (cherry picked from commit 6adcb090119ea9cbf8bc64b2e03b665518ca1e53)

--- wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxWithInlinedFunInKlibTestGenerated.java
+++ wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenBoxWithInlinedFunInKlibTestGenerated.java
@@ -66094,6 +66094,12 @@ public class FirWasmJsCodegenBoxWithInlinedFunInKlibTestGenerated extends Abstra
         }
 
         @Test
+        @TestMetadata("genericVararg.kt")
+        public void testGenericVararg() {
+          runTest("compiler/testData/codegen/boxInline/callableReference/adaptedReferences/genericVararg.kt");
+        }
+
+        @Test
         @TestMetadata("inlineBound.kt")
         public void testInlineBound() {
           runTest("compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineBound.kt");
