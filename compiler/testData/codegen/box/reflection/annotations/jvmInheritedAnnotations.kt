2a1f24eaa613bb17aa5a8324de87f33d4197d15e
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-11-03 16:26:21 +0100

    [Reflection/JVM] add JvmInherited as typealias to Java's @Inherited
    ^KT-22265 Fixed

---
+++ compiler/testData/codegen/box/reflection/annotations/jvmInheritedAnnotations.kt
@@ -0,0 +1,32 @@
+// TARGET_BACKEND: JVM
+// API_VERSION: 2.4
+// WITH_REFLECT
+// FULL_JDK
+
+package test
+
+import kotlin.jvm.JvmInherited
+import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.findAnnotations
+import kotlin.reflect.full.declaredFunctions
+
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+@JvmInherited
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Anno(val value: String = "myDefaultValue")
+
+@Anno
+open class BaseClass
+
+class ChildClass: BaseClass()
+
+fun box(): String {
+    assertNotNull(Anno::class.java.getAnnotation(java.lang.annotation.Inherited::class.java))
+    assertTrue(ChildClass::class.annotations.single() is Anno)
+
+    return "OK"
+}
---
+++ libraries/stdlib/jdk8/src/kotlin/annotations/JvmInherited.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+@file:JvmPackageName("kotlin.jvm.jdk8")
+
+package kotlin.jvm
+
+import java.lang.annotation.Inherited
+
+/**
+ * Makes the annotation class "inherited" in Java and Kotlin. An annotation declared on a class will be visible on sub-classes
+ * unless overridden by another instance of the same annotation class.
+ */
+@SinceKotlin("2.4")
+public typealias JvmInherited = Inherited
