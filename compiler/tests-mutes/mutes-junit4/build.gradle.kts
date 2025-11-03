0b7e0a91e3b4d0c407236c9e8f105e2a495af0a8
Bogdan Mukvich <bogdan.mukvich@jetbrains.com>
2025-11-03 15:07:31 +0100

    Update logback dependency to 1.5.20
    KTI-2774

--- compiler/tests-mutes/mutes-junit4/build.gradle.kts
+++ compiler/tests-mutes/mutes-junit4/build.gradle.kts
@@ -10,6 +10,8 @@ dependencies {
     api("com.nordstrom.tools:junit-foundation:17.2.4")
     // override beanutils dependency due to CVE-2025-48734
     api(libs.apache.commons.beanutils)
+    // override logback dependency due to CVE-2025-11226
+    api(libs.logback.classic)
 
     constraints {
         api(libs.apache.commons.lang)
