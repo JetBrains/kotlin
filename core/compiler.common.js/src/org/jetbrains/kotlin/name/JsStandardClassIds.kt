61fa065eced79e6ec2e58080de5143080f2fc09c
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2025-11-04 20:27:58 +0300

    Update JsStandardClassIds.kt

--- core/compiler.common.js/src/org/jetbrains/kotlin/name/JsStandardClassIds.kt
+++ core/compiler.common.js/src/org/jetbrains/kotlin/name/JsStandardClassIds.kt
@@ -24,85 +24,61 @@ object JsStandardClassIds {
     @JvmField
     val Date = "Date".jsId()
 
+    @file:JvmField
     object Annotations {
-        @JvmField
         val JsQualifier = "JsQualifier".jsId()
 
-        @JvmField
         val JsModule = "JsModule".jsId()
 
-        @JvmField
         val JsNonModule = "JsNonModule".jsId()
 
-        @JvmField
         val JsNative = "native".jsId()
 
-        @JvmField
         val JsLibrary = "library".jsId()
 
-        @JvmField
         val JsNativeInvoke = "nativeInvoke".jsId()
 
-        @JvmField
         val JsNativeGetter = "nativeGetter".jsId()
 
-        @JvmField
         val JsNativeSetter = "nativeSetter".jsId()
 
-        @JvmField
         val JsName = "JsName".jsId()
 
-        @JvmField
         val JsExport = "JsExport".jsId()
 
-        @JvmField
         val JsImplicitExport = "JsImplicitExport".jsId()
 
-        @JvmField
         val JsNoDispatchReceiver = "JsNoDispatchReceiver".jsId()
 
-        @JvmField
         val JsStatic = "JsStatic".jsId()
 
-        @JvmField
         val JsExternalInheritorsOnly = "JsExternalInheritorsOnly".jsId()
 
-        @JvmField
         val JsExternalArgument = "JsExternalArgument".jsId()
 
-        @JvmField
         val JsExportIgnore = JsExport.createNestedClassId(Name.identifier("Ignore"))
 
-        @JvmField
         val JsExportDefault = JsExport.createNestedClassId(Name.identifier("Default"))
 
-        @JvmField
         val JsFun = "JsFun".id()
 
-        @JvmField
         val JsOutlinedFunction = "JsOutlinedFunction".jsId()
 
-        @JvmField
         val JsGenerator = "JsGenerator".jsId()
 
-        @JvmField
         val DoNotIntrinsify = "DoNotIntrinsify".jsId()
 
-        @JvmField
         val annotationsRequiringExternal = setOf(JsModule, JsQualifier)
 
-        @JvmField
         val nativeAnnotations = setOf(JsNative, JsNativeInvoke, JsNativeGetter, JsNativeSetter)
 
-        @JvmField
         val JsNoLifting = "JsNoLifting".jsId()
     }
 
+    @file:JvmField
     object Callables {
-        @JvmField
         val JsCode = "js".callableId(BASE_JS_PACKAGE)
 
-        @JvmField
         val JsDefinedExternally = "definedExternally".callableId(BASE_JS_PACKAGE)
     }
 }
