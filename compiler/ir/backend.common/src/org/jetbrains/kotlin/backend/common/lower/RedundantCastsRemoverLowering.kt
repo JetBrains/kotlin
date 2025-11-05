952702c4722379be1bd698029fb5bbd881bc128d
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2025-11-05 13:28:31 +0100

    [IR] Convert RedundantCastsRemoverLowering to BodyLoweringPass

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/RedundantCastsRemoverLowering.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/RedundantCastsRemoverLowering.kt
@@ -5,9 +5,10 @@
 
 package org.jetbrains.kotlin.backend.common.lower
 
-import org.jetbrains.kotlin.backend.common.FileLoweringPass
+import org.jetbrains.kotlin.backend.common.BodyLoweringPass
 import org.jetbrains.kotlin.backend.common.LoweringContext
-import org.jetbrains.kotlin.ir.declarations.IrFile
+import org.jetbrains.kotlin.ir.declarations.IrDeclaration
+import org.jetbrains.kotlin.ir.expressions.IrBody
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrTypeOperator
 import org.jetbrains.kotlin.ir.expressions.IrTypeOperatorCall
@@ -15,12 +16,15 @@ import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
 import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 
 /**
- * Removes redundant casts like
- * - `TYPE_OP type=kotlin.Int origin=IMPLICIT_CAST typeOperand=kotlin.Int`
+ * Removes redundant casts like the following, which can appear after function inlining
+ * ```
+ * TYPE_OP type=kotlin.Int origin=IMPLICIT_CAST typeOperand=kotlin.Int
+ *   GET_VAR 'val arg: kotlin.Int declared in <root>.box' type=kotlin.Int origin=null
+ * ```
  */
-class RedundantCastsRemoverLowering(val context: LoweringContext) : FileLoweringPass {
-    override fun lower(irFile: IrFile) {
-        irFile.transformChildrenVoid(Transformer())
+class RedundantCastsRemoverLowering(val context: LoweringContext) : BodyLoweringPass {
+    override fun lower(irBody: IrBody, container: IrDeclaration) {
+        irBody.transformChildrenVoid(Transformer())
     }
 
     private class Transformer : IrElementTransformerVoid() {
