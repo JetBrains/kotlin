JetBrains/kotlin

commit 17fa9d947036da7d6c43d42dd45314229edb83a4

FIR: introduce FirCallableDeclaration/FirMemberDeclaration.isLocal
KT-80673 in progress

compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirDeclarationsResolveTransformer.kt
@@ -1580,7 +1580,7 @@ open class FirDeclarationsResolveTransformer(
 
             val newTypeRef: FirResolvedTypeRef = resultType?.let {
                 val expectedType = it.toExpectedTypeRef(fallbackSource = variable.source)
-                expectedType.approximateDeclarationType(session, variable.visibilityForApproximation(), variable.isLocal)
+                expectedType.approximateDeclarationType(session, variable.visibilityForApproximation(), variable.isLocalVariableOrParameter)
             } ?: buildErrorTypeRef {
                 diagnostic = ConeLocalVariableNoTypeOrInitializer(variable)
                 source = variable.source
@@ -1592,7 +1592,7 @@ open class FirDeclarationsResolveTransformer(
         }
     }
 
-    private val FirVariable.isLocal: Boolean
+    private val FirVariable.isLocalVariableOrParameter: Boolean
         get() = when (symbol) {
             is FirValueParameterSymbol -> true
             is FirLocalPropertySymbol -> true

commit 8a342257afc8fe0d2dc2d6afd68f03ace56243dc

K2: fix isLocal usages in FirDeclarationsResolveTransformer broken by 17fa9d94
Before 17fa9d94, FirDeclarationsResolveTransformer contained a
FirVariable.isLocal extension that was used a number of times.
In 17fa9d94, a member FirCallableDeclaration.isLocal was introduced;
this member supersedes the extension. The extension was renamed,
but not all usages were renamed with it. In this commit,
I renamed all remaining usages.

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLDiagnosticsFirTestGenerated.java
@@ -201,6 +201,12 @@ public class LLDiagnosticsFirTestGenerated extends AbstractLLDiagnosticsTest {
     }
 
     @Test
+    @TestMetadata("delegatedSuperTypeByExpression.kt")
+    public void testDelegatedSuperTypeByExpression() {
+      runTest("compiler/fir/analysis-tests/testData/resolve/delegatedSuperTypeByExpression.kt");
+    }
+
+    @Test
     @TestMetadata("delegatingConstructorCall.kt")
     public void testDelegatingConstructorCall() {
       runTest("compiler/fir/analysis-tests/testData/resolve/delegatingConstructorCall.kt");

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLPartialDiagnosticsFirTestGenerated.java
@@ -201,6 +201,12 @@ public class LLPartialDiagnosticsFirTestGenerated extends AbstractLLPartialDiagn
     }
 
     @Test
+    @TestMetadata("delegatedSuperTypeByExpression.kt")
+    public void testDelegatedSuperTypeByExpression() {
+      runTest("compiler/fir/analysis-tests/testData/resolve/delegatedSuperTypeByExpression.kt");
+    }
+
+    @Test
     @TestMetadata("delegatingConstructorCall.kt")
     public void testDelegatingConstructorCall() {
       runTest("compiler/fir/analysis-tests/testData/resolve/delegatingConstructorCall.kt");

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedDiagnosticsFirTestGenerated.java
@@ -201,6 +201,12 @@ public class LLReversedDiagnosticsFirTestGenerated extends AbstractLLReversedDia
     }
 
     @Test
+    @TestMetadata("delegatedSuperTypeByExpression.kt")
+    public void testDelegatedSuperTypeByExpression() {
+      runTest("compiler/fir/analysis-tests/testData/resolve/delegatedSuperTypeByExpression.kt");
+    }
+
+    @Test
     @TestMetadata("delegatingConstructorCall.kt")
     public void testDelegatingConstructorCall() {
       runTest("compiler/fir/analysis-tests/testData/resolve/delegatingConstructorCall.kt");

compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirDeclarationsResolveTransformer.kt
@@ -378,7 +378,7 @@ open class FirDeclarationsResolveTransformer(
         // First, resolve delegate expression in dependent context withing existing (possibly Default) inference session
         val delegateExpression =
             // Resolve delegate expression; after that, delegate will contain either expr.provideDelegate or expr
-            if (property.isLocal) {
+            if (property.isLocalVariableOrParameter) {
                 transformDelegateExpression(delegateContainer)
             } else {
                 // TODO: the [skipCleanup] hack should be reverted on fixing KT-79107
@@ -498,7 +498,7 @@ open class FirDeclarationsResolveTransformer(
                     typeRef.approximateDeclarationType(
                         session,
                         property.visibilityForApproximation(),
-                        property.isLocal
+                        property.isLocalVariableOrParameter
                     )
                 )
 
@@ -642,7 +642,7 @@ open class FirDeclarationsResolveTransformer(
     }
 
     private fun transformLocalVariable(variable: FirProperty): FirProperty = whileAnalysing(session, variable) {
-        assert(variable.isLocal)
+        assert(variable.isLocalVariableOrParameter)
         val delegate = variable.delegate
 
         val hadExplicitType = variable.returnTypeRef !is FirImplicitTypeRef

compiler/fir/analysis-tests/testData/resolve/delegatedSuperTypeByExpression.kt
@@ -1,0 +1,14 @@
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-80673
+
+interface I
+
+class C : I by when {
+    else -> {
+        val config = object : I {}
+        config
+    }
+}
+
+/* GENERATED_FIR_TAGS: anonymousObjectExpression, classDeclaration, inheritanceDelegation, interfaceDeclaration,
+localProperty, propertyDeclaration, whenExpression */

compiler/fir/analysis-tests/testData/resolve/delegates/delegatedPropertyInsideLocalClass.kt
@@ -3,7 +3,7 @@
 
 fun owner() {
     class Local(param: Any) {
-        val x: Any by <!UNRESOLVED_REFERENCE!>param<!>
+        val x: Any <!DELEGATE_SPECIAL_FUNCTION_MISSING!>by<!> param
     }
 }
 