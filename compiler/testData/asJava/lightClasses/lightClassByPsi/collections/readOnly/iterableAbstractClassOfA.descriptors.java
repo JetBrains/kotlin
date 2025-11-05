a211c7d16388992ad85f25bb9ebc0255f1b75401
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-11-05 20:18:09 +0400

    Remove duplicate testdata

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/iterableAbstractClassOfA.descriptors.java
+++
@@ -1,6 +0,0 @@
-public abstract class AIterable /* test.AIterable*/ implements java.lang.Iterable<test.A>, kotlin.collections.Iterable<test.A>, kotlin.jvm.internal.markers.KMappedMarker {
-  @org.jetbrains.annotations.NotNull()
-  public java.util.Iterator<test.A> iterator();//  iterator()
-
-  public  AIterable();//  .ctor()
-}
