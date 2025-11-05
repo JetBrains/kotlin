1446cbd3aaa2fe65950eb8163d9b47f8fad11fa5
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-11-05 15:52:37 +0100

    Swift export: add execution test for ranges

---
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/execution/ranges/ranges.swift
@@ -0,0 +1,31 @@
+import Ranges
+import KotlinRuntime
+import Testing
+
+@Test
+func testSimple() throws {
+    let d = distance(some: 5 ... 10)
+    let m = materialize()
+    let s = simple(some: 5 ... 10)
+    #expect(d == 5)
+    #expect(m.contains(8))
+    #expect(s.contains(4))
+    #expect(s.contains(11))
+    #expect(!s.contains(0))
+}
+
+@Test
+func nullable() throws {
+    // Currently crashes with SIGSEGV
+    //let n1 = nullable(some: -10 ... -5)
+    //let n2 = nullable(some: nil)
+    //#expect(n1 == -11 ... -4)
+    //#expect(n2 == nil)
+}
+
+@Test
+func testTotal() throws {
+    // Currently crashes with CCE
+    //let t = total(list: [1 ... 5, 0 ... 4, 2 ... 7])
+    //#expect(t == 0 ... 7)
+}
