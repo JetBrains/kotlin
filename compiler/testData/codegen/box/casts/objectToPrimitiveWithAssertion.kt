3ec96b6cba4aba76a6919e5f371f752c11a9f3c5
Igor Chevdar <igor.chevdar@jetbrains.com>
2025-11-06 15:49:49 +0200

    [K/N] Disabled generic safe casts for now
    They are not ready to be turned on by default yet.
    
     #KT-81712 Fixed

--- compiler/testData/codegen/box/casts/objectToPrimitiveWithAssertion.kt
+++ compiler/testData/codegen/box/casts/objectToPrimitiveWithAssertion.kt
@@ -1,6 +1,4 @@
 // IGNORE_BACKEND: JS_IR, JS_IR_ES6, WASM_JS, WASM_WASI
-// IGNORE_NATIVE: optimizationMode=DEBUG
-// IGNORE_NATIVE: optimizationMode=NO
 @Suppress("UNCHECKED_CAST")
 fun <T> f() = 1L as T
 
