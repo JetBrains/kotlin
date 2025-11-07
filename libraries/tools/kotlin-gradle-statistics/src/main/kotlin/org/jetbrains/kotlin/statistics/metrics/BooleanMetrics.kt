16efbafe050b6654cb9317665bce583babe10155
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-11-07 14:45:38 +0100

    [Gradle] Track 'webMain' sources usage
    ^KT-79482 In Progress

--- libraries/tools/kotlin-gradle-statistics/src/main/kotlin/org/jetbrains/kotlin/statistics/metrics/BooleanMetrics.kt
+++ libraries/tools/kotlin-gradle-statistics/src/main/kotlin/org/jetbrains/kotlin/statistics/metrics/BooleanMetrics.kt
@@ -47,6 +47,7 @@ enum class BooleanMetrics(val type: BooleanOverridePolicy, val anonymization: Bo
     KOTLIN_PROGRESSIVE_MODE(OVERRIDE, SAFE),
     KOTLIN_KTS_USED(OR, SAFE),
     KOTLIN_GENERATED_SOURCES_USED(OR, SAFE),
+    KOTLIN_WEB_MAIN_SOURCES_USED(OR, SAFE),
 
     // Disabled explicitly by the user
     KOTLIN_CROSS_COMPILATION_DISABLED(OR, SAFE),
@@ -126,6 +127,6 @@ enum class BooleanMetrics(val type: BooleanOverridePolicy, val anonymization: Bo
     ;
 
     companion object {
-        const val VERSION = 18
+        const val VERSION = 21
     }
 }
