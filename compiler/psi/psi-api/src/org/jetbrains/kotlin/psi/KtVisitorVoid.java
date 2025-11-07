a33ea0c424a4c86bc1be6d0551fe479631d08856
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-11-07 18:26:17 +0100

    [psi] introduce KtContextParameterList
    This is a list supposed to replace `KtContextReceiverList`
    completely after K1 drop. Its name in the API better aligns
    with the context parameters feature.
    The change aims to be as much compatible as possible
    to simplify transition from context receivers to
    context parameters
    
    ^KT-82258

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtVisitorVoid.java
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtVisitorVoid.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi;
@@ -149,8 +138,17 @@ public class KtVisitorVoid extends KtVisitor<Void, Void> {
         super.visitSuperTypeEntry(specifier, null);
     }
 
+    /**
+     * Must not be overriden at the same time as {@link #visitContextParameterList(KtContextParameterList)}.
+     *
+     * @see KtVisitor#visitContextReceiverList(KtContextReceiverList, Object)
+     */
     public void visitContextReceiverList(@NotNull KtContextReceiverList contextReceiverList) {
         super.visitContextReceiverList(contextReceiverList, null);
+    }
+
+    public void visitContextParameterList(@NotNull KtContextParameterList contextParameterList) {
+        super.visitContextParameterList(contextParameterList, null);
     }
 
     public void visitContextReceiver(@NotNull KtContextReceiver contextReceiver) {
@@ -649,6 +647,12 @@ public class KtVisitorVoid extends KtVisitor<Void, Void> {
     @Override
     public final Void visitContextReceiverList(@NotNull KtContextReceiverList contextReceiverList, Void data) {
         visitContextReceiverList(contextReceiverList);
+        return null;
+    }
+
+    @Override
+    public Void visitContextParameterList(@NotNull KtContextParameterList contextParameterList, Void data) {
+        visitContextParameterList(contextParameterList);
         return null;
     }
 
