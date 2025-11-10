f1fc53cc1a5abd8a4c0659a08595ba5d35505622
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-11-10 12:07:44 +0100

    Regenerate tests

--- wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenSingleModuleBoxTestGenerated.java
+++ wasm/wasm.tests/tests-gen/org/jetbrains/kotlin/wasm/test/FirWasmJsCodegenSingleModuleBoxTestGenerated.java
@@ -55844,6 +55844,12 @@ public class FirWasmJsCodegenSingleModuleBoxTestGenerated extends AbstractFirWas
       }
 
       @Test
+      @TestMetadata("functionSupertype.kt")
+      public void testFunctionSupertype() {
+        runTest("compiler/testData/codegen/box/reflection/supertypes/functionSupertype.kt");
+      }
+
+      @Test
       @TestMetadata("genericSubstitution.kt")
       public void testGenericSubstitution() {
         runTest("compiler/testData/codegen/box/reflection/supertypes/genericSubstitution.kt");
@@ -56373,6 +56379,12 @@ public class FirWasmJsCodegenSingleModuleBoxTestGenerated extends AbstractFirWas
       @TestMetadata("declarationSiteVariance.kt")
       public void testDeclarationSiteVariance() {
         runTest("compiler/testData/codegen/box/reflection/typeParameters/declarationSiteVariance.kt");
+      }
+
+      @Test
+      @TestMetadata("functionClasses.kt")
+      public void testFunctionClasses() {
+        runTest("compiler/testData/codegen/box/reflection/typeParameters/functionClasses.kt");
       }
 
       @Test
