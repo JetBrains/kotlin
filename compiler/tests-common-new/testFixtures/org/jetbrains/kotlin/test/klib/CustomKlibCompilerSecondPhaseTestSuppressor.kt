79086a36724c90f8fbdeae67f36c9926e9902663
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-11-11 11:23:41 +0100

    [Tests][JS] Improve ignore directives for custom second phase
    ^KT-81955

--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/klib/CustomKlibCompilerSecondPhaseTestSuppressor.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/klib/CustomKlibCompilerSecondPhaseTestSuppressor.kt
@@ -9,9 +9,11 @@ import org.jetbrains.kotlin.test.WrappedException
 import org.jetbrains.kotlin.test.backend.handlers.JsBinaryArtifactHandler
 import org.jetbrains.kotlin.test.backend.handlers.NoFirCompilationErrorsHandler
 import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
+import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
 import org.jetbrains.kotlin.test.directives.model.StringDirective
-import org.jetbrains.kotlin.test.klib.CustomKlibCompilerTestDirectives.IGNORE_KLIB_1ST_PHASE_ERRORS_WITH_CUSTOM_SECOND_PHASE
+import org.jetbrains.kotlin.test.klib.CustomKlibCompilerTestDirectives.IGNORE_KLIB_FRONTEND_ERRORS_WITH_CUSTOM_SECOND_PHASE
 import org.jetbrains.kotlin.test.klib.CustomKlibCompilerTestDirectives.IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_SECOND_PHASE
+import org.jetbrains.kotlin.test.klib.CustomKlibCompilerTestDirectives.IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_SECOND_PHASE
 import org.jetbrains.kotlin.test.model.AfterAnalysisChecker
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.moduleStructure
@@ -31,22 +33,13 @@ class CustomKlibCompilerSecondPhaseTestSuppressor(
 
     override fun suppressIfNeeded(failedAssertions: List<WrappedException>): List<WrappedException> {
         if (failedAssertions.isEmpty()) {
-            val directives = testServices.moduleStructure.modules.first().directives
-            if (customCompilerVersion in directives[IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_SECOND_PHASE]) {
-                return listOf(
-                    AssertionError(
-                        "Looks like this test can be unmuted. Remove $customCompilerVersion from the $IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_SECOND_PHASE directive"
-                    ).wrap()
-                )
+            return buildList {
+                with(testServices.moduleStructure.modules.first().directives) {
+                    addAll(createUnmutingErrorIfNeeded(IGNORE_KLIB_FRONTEND_ERRORS_WITH_CUSTOM_SECOND_PHASE))
+                    addAll(createUnmutingErrorIfNeeded(IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_SECOND_PHASE))
+                    addAll(createUnmutingErrorIfNeeded(IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_SECOND_PHASE))
+                }
             }
-            if (customCompilerVersion in directives[IGNORE_KLIB_1ST_PHASE_ERRORS_WITH_CUSTOM_SECOND_PHASE]) {
-                return listOf(
-                    AssertionError(
-                        "Looks like this test can be unmuted. Remove $customCompilerVersion from the $IGNORE_KLIB_1ST_PHASE_ERRORS_WITH_CUSTOM_SECOND_PHASE directive"
-                    ).wrap()
-                )
-            }
-            return emptyList()
         }
 
         val newFailedAssertions = failedAssertions.flatMap { wrappedException ->
@@ -55,7 +48,7 @@ class CustomKlibCompilerSecondPhaseTestSuppressor(
                     is NoFirCompilationErrorsHandler -> emptyList() // Some tests cannot be compiled with previous LV. These are just ignored
                     is JsBinaryArtifactHandler -> processException(  // Execution error
                         wrappedException,
-                        IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_SECOND_PHASE
+                        IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_SECOND_PHASE
                     )
                     else -> listOf(wrappedException)
                 }
@@ -64,7 +57,7 @@ class CustomKlibCompilerSecondPhaseTestSuppressor(
                         wrappedException,
                         IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_SECOND_PHASE
                     )
-                    else -> processException(wrappedException, IGNORE_KLIB_1ST_PHASE_ERRORS_WITH_CUSTOM_SECOND_PHASE)
+                    else -> processException(wrappedException, IGNORE_KLIB_FRONTEND_ERRORS_WITH_CUSTOM_SECOND_PHASE)
                 }
                 else -> error("Yet unsupported wrapped exception type: ${wrappedException::class.qualifiedName} ")
             }
@@ -76,6 +69,17 @@ class CustomKlibCompilerSecondPhaseTestSuppressor(
         } else {
             return newFailedAssertions
         }
+    }
+
+    context(directives: RegisteredDirectives)
+    private fun createUnmutingErrorIfNeeded(stringDirective: StringDirective): List<WrappedException> {
+        return if (customCompilerVersion in directives[stringDirective])
+            listOf(
+                AssertionError(
+                    "Looks like this test can be unmuted. Remove $customCompilerVersion from the $stringDirective directive"
+                ).wrap()
+            )
+        else emptyList()
     }
 
     private fun processException(wrappedException: WrappedException, ignoreDirective: StringDirective): List<WrappedException> {
