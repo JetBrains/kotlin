2769444d51a25f88d6c701a887ed7b063d7404ed
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-11-11 15:38:47 +0200

    FE/fixation: take non-ILT into account in `HAS_PROPER_NON_NOTHING_LOWER_CONSTRAINT`
    Note that this change also makes the yellow `intersect.fir.kt` green.
    
    ^KT-77939

--- compiler/testData/diagnostics/tests/inference/pcla/stubTypes/intersect.fir.kt
+++ compiler/testData/diagnostics/tests/inference/pcla/stubTypes/intersect.fir.kt
@@ -32,7 +32,7 @@ fun <E> intersect(vararg x: In<E>): E = null as E
 fun test() {
     val ret = build {
         emit("1")
-        <!INFERRED_TYPE_VARIABLE_INTO_EMPTY_INTERSECTION_WARNING!>intersect<!>(getIn(), getIn())
+        intersect(getIn(), getIn())
         intersect(getIn(), Test.foo(getIn()))
         intersect(Test.foo(getIn()), Test.foo(getIn()))
         intersect(Test.foo(getIn()), getIn())
