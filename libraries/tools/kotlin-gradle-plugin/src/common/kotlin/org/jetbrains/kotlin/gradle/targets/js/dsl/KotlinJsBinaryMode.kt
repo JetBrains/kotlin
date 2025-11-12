6e307c434be2a79e79f2bb5e002ac98c97076d64
Adam Semenenko <adam.semenenko@jetbrains.com>
2025-11-12 11:37:22 +0100

    [Gradle] document KotlinJsBinaryMode
    ^KT-76299

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/dsl/KotlinJsBinaryMode.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/dsl/KotlinJsBinaryMode.kt
@@ -5,7 +5,46 @@
 
 package org.jetbrains.kotlin.gradle.targets.js.dsl
 
+import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsBinaryMode.DEVELOPMENT
+import org.jetbrains.kotlin.gradle.targets.js.dsl.KotlinJsBinaryMode.PRODUCTION
+
+/**
+ * The mode that describes how WasmJS and JS
+ * [binaries][org.jetbrains.kotlin.gradle.targets.js.ir.JsBinary]
+ * are built.
+ *
+ * All JS and WasmJS binaries will have a [PRODUCTION] and [DEVELOPMENT] variants.
+ *
+ * The mode affects how KGP builds the binary.
+ *
+ * #### Kotlin compilation.
+ * - For [PRODUCTION], enable code minimisation, Dead Code Elimination.
+ * - For [DEVELOPMENT], enable incremental compilation, and include debugging information.
+ *
+ * #### Bundling
+ *
+ * For example, sets Webpack [mode](https://webpack.js.org/configuration/mode/).
+ *
+ * @see org.jetbrains.kotlin.gradle.targets.js.ir.JsBinary.mode
+ */
 enum class KotlinJsBinaryMode {
+    /**
+     * Production mode.
+     *
+     * When KGP builds a binary with this mode, it enables options
+     * to optimise for size and performance.
+     *
+     * The build times may increase.
+     */
     PRODUCTION,
+
+    /**
+     * Development mode.
+     *
+     * When KGP builds a binary with this mode, it enables options
+     * to optimise for improving local development experience.
+     *
+     * Use for local development only.
+     */
     DEVELOPMENT
 }
