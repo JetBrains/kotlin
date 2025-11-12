94bf00bad8f2e77601618dc5581b6185cb331786
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-11-12 23:09:39 +0400

    [SLC] Add testdata for mapped Kotlin collection classes
    KT-22594
    KT-36991
    KT-60480
    KT-70426
    KT-70428

---
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/collectionInterfaceOfString.kmp.lib.java
@@ -0,0 +1,38 @@
+public abstract class CCollection /* test.CCollection*/ implements test.ICollection {
+  public  CCollection();//  .ctor()
+}
+
+public abstract class CCollection2 /* test.CCollection2*/ implements test.ICollection {
+  private final int size;
+
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() java.util.Iterator<@org.jetbrains.annotations.NotNull() java.lang.String> iterator();//  iterator()
+
+  public  CCollection2(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() test.ICollection);//  .ctor(@org.jetbrains.annotations.NotNull() test.ICollection)
+
+  public boolean contains(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String);//  contains(@org.jetbrains.annotations.NotNull() java.lang.String)
+
+  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>)
+
+  public boolean isEmpty();//  isEmpty()
+
+  public int getSize();//  getSize()
+}
+
+public class CCollection3 /* test.CCollection3*/ implements test.ICollection {
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() java.util.Iterator<@org.jetbrains.annotations.NotNull() java.lang.String> iterator();//  iterator()
+
+  public  CCollection3();//  .ctor()
+
+  public boolean contains(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String);//  contains(@org.jetbrains.annotations.NotNull() java.lang.String)
+
+  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>)
+
+  public boolean isEmpty();//  isEmpty()
+
+  public int getSize();//  getSize()
+}
+
+public abstract interface ICollection /* test.ICollection*/ extends java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>, kotlin.collections.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>, kotlin.jvm.internal.markers.KMappedMarker {
+}
---
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/mapEntryAbstractClass.kt
@@ -0,0 +1,14 @@
+// WITH_STDLIB
+package test
+
+abstract class CMapEntry<KElem, VElem> : Map.Entry<KElem, VElem>
+
+abstract class CMapEntry2<KElem, VElem> : Map.Entry<KElem, VElem> by emptyMap<KElem, VElem>().entries.first()
+
+open class CMapEntry3<KElem, VElem> : Map.Entry<KElem, VElem> {
+    override val key: KElem
+        get() = TODO("Not yet implemented")
+    override val value: VElem
+        get() = TODO("Not yet implemented")
+}
+// LIGHT_ELEMENTS_NO_DECLARATION: CMapEntry.class[setValue], CMapEntry2.class[setValue], CMapEntry3.class[setValue]
