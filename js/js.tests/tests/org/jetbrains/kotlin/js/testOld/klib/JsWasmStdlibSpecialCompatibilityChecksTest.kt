0256284187234d2dc476ce8123ec9fe1b48129a3
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-11-12 15:25:57 +0100

    [JS, Wasm] Move stdlib/kotlin-test version tests to the test source set
    These tests happen to exist in a 'fixture' source set. As a result,
    they were not executed at the CI server.
    
    Now, they are moved to the actual 'test' source set, which would make
    them run at the CI server.
    
    ^KT-82208

---
+++ js/js.tests/tests/org/jetbrains/kotlin/js/testOld/klib/JsWasmStdlibSpecialCompatibilityChecksTest.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.js.testOld.klib
+
+import org.jetbrains.kotlin.cli.common.messages.MessageCollectorImpl
+import kotlin.collections.any
+
+@Suppress("JUnitTestCaseWithNoTests")
+class JsWasmStdlibSpecialCompatibilityChecksTest : LibrarySpecialCompatibilityChecksTest() {
+    override val originalLibraryPath: String
+        get() = System.getProperty("kotlin.js.full.stdlib.path")
+
+    override fun MessageCollectorImpl.hasJsOldLibraryError(specificVersions: Pair<TestVersion, TestVersion>?): Boolean {
+        val stdlibMessagePart = "Kotlin/JS standard library has an older version" + specificVersions?.first?.let { " ($it)" }.orEmpty()
+        val compilerMessagePart = "than the compiler" + specificVersions?.second?.let { " ($it)" }.orEmpty()
+
+        return messages.any { stdlibMessagePart in it.message && compilerMessagePart in it.message }
+    }
+
+    override fun MessageCollectorImpl.hasJsTooNewLibraryError(specificVersions: Pair<TestVersion, TestVersion>?): Boolean {
+        val stdlibMessagePart =
+            "The Kotlin/JS standard library has a more recent version" + specificVersions?.first?.let { " ($it)" }.orEmpty()
+        val compilerMessagePart = "The compiler version is " + specificVersions?.second?.toString().orEmpty()
+
+        return messages.any { stdlibMessagePart in it.message && compilerMessagePart in it.message }
+    }
+
+    override fun MessageCollectorImpl.hasWasmError(specificVersions: Pair<TestVersion, TestVersion>?): Boolean {
+        val stdlibMessagePart = "The version of the Kotlin/Wasm standard library" + specificVersions?.first?.let { " ($it)" }.orEmpty()
+        val compilerMessagePart = "differs from the version of the compiler" + specificVersions?.second?.let { " ($it)" }.orEmpty()
+
+        return messages.any { stdlibMessagePart in it.message && compilerMessagePart in it.message }
+    }
+}
