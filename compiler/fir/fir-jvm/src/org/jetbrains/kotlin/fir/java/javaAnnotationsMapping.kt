d3a923fb1683a925c8518e565d055bb9ee65f05a
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-11-13 08:36:02 +0100

    [FIR] Java: cover case with binary annotation which resolves to source
    Even Java resolve might in exceptional cases return a light
    class for a source Kotlin class. `JavaClassImpl` forbids
    usages of light classes, so this API cannot be used as is
    in this case.
    Since normally such situations are impossible (this setup
    should lead to a runtime exception due to a missed library),
    it should be fine to just ignore such cases instead of trying
    to work with source declarations
    
    ^KT-82076 Fixed

--- compiler/fir/fir-jvm/src/org/jetbrains/kotlin/fir/java/javaAnnotationsMapping.kt
+++ compiler/fir/fir-jvm/src/org/jetbrains/kotlin/fir/java/javaAnnotationsMapping.kt
@@ -289,7 +289,11 @@ private fun fillAnnotationArgumentMapping(
         // Binary session dependencies work differently in the CLI and the Analysis API mode,
         // so some common mechanism has to be used to eliminate the difference.
         // This approach works both for Java and Kotlin annotations, as in the end all of them are just .class files.
-        val binaryClass = javaAnnotation.resolve()?.originalClsJavaClass
+        val javaClass = javaAnnotation.resolve()?.originalClsJavaClass
+
+        // It is still possible to get a source light class if there is no binary class on the classpath.
+        // Usually, this absence leads to a runtime exception, so there is no need to try to analyze source Kotlin code via light classes
+        val binaryClass = javaClass?.takeIf { it.lightClassOriginKind == null }
         binaryClass?.annotationParametersMapping(session, source)
     } else {
         // For source sessions symbol provider-based search is used as it is more precise
