09ef8efb530949f3a237037af1d00db39f06946d
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2025-11-14 03:22:23 +0900

    [PSI] Move 'getAssignmentLhsIfUnwrappable()' out of the PSI API
    This eliminates usage of 'FlyweightCapableTreeStructure' and
    'LighterASTNode' in the Kotlin PSI API.

--- compiler/psi/psi-frontend-utils/src/org/jetbrains/kotlin/resolve/source/sourceElementUtils.kt
+++ compiler/psi/psi-frontend-utils/src/org/jetbrains/kotlin/resolve/source/sourceElementUtils.kt
@@ -5,9 +5,14 @@
 
 package org.jetbrains.kotlin.resolve.source
 
+import com.intellij.lang.LighterASTNode
+import com.intellij.util.diff.FlyweightCapableTreeStructure
+import org.jetbrains.kotlin.KtNodeTypes.PREFIX_EXPRESSION
 import org.jetbrains.kotlin.KtSourceElement
 import org.jetbrains.kotlin.psi
+import org.jetbrains.kotlin.psi.psiUtil.UNWRAPPABLE_TOKEN_TYPES
 import org.jetbrains.kotlin.psi.psiUtil.getAssignmentLhsIfUnwrappable
+import org.jetbrains.kotlin.util.getChildren
 
 /**
  * This function should only be called for a source element corresponding to
@@ -23,3 +28,17 @@ fun KtSourceElement?.hasUnwrappableAsAssignmentLhs(): Boolean {
 
     return node != null
 }
+
+/**
+ * This function should only be called for a source element corresponding to
+ * an assignment/assignment operator call/increment or a decrement operator.
+ */
+fun LighterASTNode.getAssignmentLhsIfUnwrappable(tree: FlyweightCapableTreeStructure<LighterASTNode>): LighterASTNode? =
+    when {
+        // In `++(x)` the LHS source `(x)` is the last child
+        tokenType == PREFIX_EXPRESSION -> getChildren(tree).lastOrNull()
+        // In `(x)++` or `(x) = ...` the LHS source is the first child
+        else -> getChildren(tree).firstOrNull()
+    }.takeIf {
+        it?.tokenType in UNWRAPPABLE_TOKEN_TYPES
+    }
