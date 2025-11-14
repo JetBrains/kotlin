4c4a32ec192af7bfffa5ffe18f10fb10583e8acd
Ilya Plisko <ilya.plisko@jetbrains.com>
2025-11-14 15:37:12 +0100

    KT-79227: Fixed Problem with No Golden Data Being Generated
    The problem is fixed by creating a new module for the test case
    
    ^KT-79227 fixed

---
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/inheritance/golden_result/override/override.h
@@ -0,0 +1,20 @@
+#include <Foundation/Foundation.h>
+#include <stdint.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+void Base_g__TypesOfArguments__anyU20override_P__(void * self, void * x);
+
+void P_f(void * self);
+
+void Sub_g__TypesOfArguments__anyU20override_P__(void * self, void * x);
+
+void * __root___Base_init_allocate();
+
+void __root___Base_init_initialize__TypesOfArguments__Swift_UnsafeMutableRawPointer__(void * __kt);
+
+void * __root___Sub_init_allocate();
+
+void __root___Sub_init_initialize__TypesOfArguments__Swift_UnsafeMutableRawPointer__(void * __kt);
+
+NS_ASSUME_NONNULL_END
