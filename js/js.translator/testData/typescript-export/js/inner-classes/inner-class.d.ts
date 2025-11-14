9e232af88d12dd06df345da2cea234855f2f3337
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-11-14 16:12:01 +0100

    [JS] Test exporting open inner classes to TypeScript

--- js/js.translator/testData/typescript-export/js/inner-classes/inner-class.d.ts
+++ js/js.translator/testData/typescript-export/js/inner-classes/inner-class.d.ts
@@ -8,6 +8,11 @@ declare namespace JS_TESTS {
             get Inner(): {
                 new(a: string): TestInner.Inner;
             } & typeof TestInner.Inner;
+            get OpenInnerWithPublicConstructor(): {
+                new(a: string): TestInner.OpenInnerWithPublicConstructor;
+            } & typeof TestInner.OpenInnerWithPublicConstructor;
+            get OpenInnerWithProtectedConstructor(): {
+            } & typeof TestInner.OpenInnerWithProtectedConstructor;
         }
         namespace TestInner {
             /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
@@ -38,6 +43,30 @@ declare namespace JS_TESTS {
                     namespace $metadata$ {
                         const constructor: abstract new () => SecondLayerInner;
                     }
+                }
+            }
+            class OpenInnerWithPublicConstructor {
+                protected constructor($outer: foo.TestInner, a: string);
+                get a(): string;
+                static fromNumber(a: number): foo.TestInner.OpenInnerWithPublicConstructor;
+                get concat(): string;
+            }
+            namespace OpenInnerWithPublicConstructor {
+                /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+                namespace $metadata$ {
+                    const constructor: abstract new () => OpenInnerWithPublicConstructor;
+                }
+            }
+            class OpenInnerWithProtectedConstructor {
+                protected constructor($outer: foo.TestInner, a: string);
+                get a(): string;
+                protected static fromNumber(a: number): foo.TestInner.OpenInnerWithProtectedConstructor;
+                get concat(): string;
+            }
+            namespace OpenInnerWithProtectedConstructor {
+                /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+                namespace $metadata$ {
+                    const constructor: abstract new () => OpenInnerWithProtectedConstructor;
                 }
             }
         }
