ae0510c08c80cb29b1dc9e2dbbd0607feadcedf4
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-11-14 15:06:44 +0200

    [Build] Delete pill

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,6 @@ plugins {
     id("org.jetbrains.gradle.plugin.idea-ext") version "1.0.1" // this version should be in sync with repo/buildsrc-compat/build.gradle.kts
     id("build-time-report")
     id("java-instrumentation")
-    id("jps")
     id("modularized-test-configurations")
     id("resolve-dependencies")
     id("org.gradle.crypto.checksum") version "1.4.0"
