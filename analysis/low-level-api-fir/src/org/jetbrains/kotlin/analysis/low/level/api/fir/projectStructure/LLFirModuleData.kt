c4a9b38876221ccf89171c891221e9d19e91f2f4
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-11-17 23:28:19 +0100

    [LL] forbid redeclaration for library sources
    Library sources may have equivalent declarations in the same module,
    so they have to be filtered via `ConeCallConflictResolver`.
    In particular, `ConeEquivalentCallConflictResolver` is supposed to
    deduplicate `expect` and `actual` declarations.
    The issue is covered by
    `StdLibSourcesLazyDeclarationResolveTestGenerated.testWrappedInt` test.
    The flaky behavior of this test was caused by the non-stable order of
    `kotlin.toString` expect and actual declarations inside the stdlib
    sources.
    As a result, in one case
    `ConeEquivalentCallConflictResolver.filterOutEquivalentCalls` was
    enough to drop `expect` declarations since it was in the first position,
    and we also had another declaration from the builtins module.
    If the `actual` variant was the first one, then this condition wasn't
    reached, and the result contained both `expect` and `actual` since
    they weren't treated as equivalent.
    13021e43d42d60f6e85fcb4e25109647ced9e998 commit was the place where
    the order of `expect` and `actual` became a factor
    
    ^KT-76487 Fixed

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/projectStructure/LLFirModuleData.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/projectStructure/LLFirModuleData.kt
@@ -1,10 +1,11 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir.projectStructure
 
+import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibrarySourceModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSession
 import org.jetbrains.kotlin.analysis.low.level.api.fir.sessions.LLFirSessionCache
@@ -65,6 +66,13 @@ open class LLFirModuleData internal constructor(val ktModule: KaModule) : FirMod
 
     override val stableModuleName: String?
         get() = ktModule.stableModuleName
+
+    /**
+     * Library sources have [FirSession.Kind.Source] kind, but should be treated as a binary dependency since we don't expect
+     * redeclararions there.
+     */
+    override val areRedeclarationsEquivalent: Boolean
+        get() = super.areRedeclarationsEquivalent || ktModule is KaLibrarySourceModule
 
     override fun equals(other: Any?): Boolean = this === other || other is LLFirModuleData && ktModule == other.ktModule
     override fun hashCode(): Int = ktModule.hashCode()
--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/overloads/ConeEquivalentCallConflictResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/overloads/ConeEquivalentCallConflictResolver.kt
@@ -7,11 +7,7 @@ package org.jetbrains.kotlin.fir.resolve.calls.overloads
 
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.containingClassLookupTag
-import org.jetbrains.kotlin.fir.declarations.FirCallableDeclaration
-import org.jetbrains.kotlin.fir.declarations.FirFunction
-import org.jetbrains.kotlin.fir.declarations.FirProperty
-import org.jetbrains.kotlin.fir.declarations.FirNamedFunction
-import org.jetbrains.kotlin.fir.declarations.FirVariable
+import org.jetbrains.kotlin.fir.declarations.*
 import org.jetbrains.kotlin.fir.declarations.utils.isExpect
 import org.jetbrains.kotlin.fir.resolve.calls.candidate.Candidate
 import org.jetbrains.kotlin.fir.scopes.impl.FirStandardOverrideChecker
@@ -117,7 +113,7 @@ class ConeEquivalentCallConflictResolver(private val session: FirSession) : Cone
             // Furthermore, the call resolver also has to filter out duplicate `emptyArray()` candidates coming from the *same* stdlib, as
             // the function is defined both as a builtin in `kotlin.kotlin_builtins` and a regular top-level function in
             // `ArrayIntrinsicsKt.class`. See KT-78882.
-            if (first.moduleData == second.moduleData && first.moduleData.session.kind == FirSession.Kind.Source) return false
+            if (first.moduleData == second.moduleData && !first.moduleData.areRedeclarationsEquivalent) return false
 
             if (first is FirVariable != second is FirVariable) {
                 return false
