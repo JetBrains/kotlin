5544739d68c85f52038072322a8016e761f7e66c
Roman Venediktov <roman.venediktov@jetbrains.com>
2025-11-17 09:40:56 +0100

    IE checker

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/IeChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/IeChecker.kt
@@ -12,6 +12,8 @@ import org.jetbrains.kotlin.diagnostics.reportOn
 import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
 import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirFunctionCallChecker
 import org.jetbrains.kotlin.fir.analysis.diagnostics.FirErrors
+import org.jetbrains.kotlin.fir.declarations.FirFunction
+import org.jetbrains.kotlin.fir.declarations.utils.isFun
 import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.fir.expressions.toResolvedCallableSymbol
 import org.jetbrains.kotlin.fir.render
@@ -21,6 +23,7 @@ import org.jetbrains.kotlin.fir.types.FirTypeProjectionWithVariance
 import org.jetbrains.kotlin.fir.types.coneType
 import org.jetbrains.kotlin.fir.types.equalTypes
 import org.jetbrains.kotlin.fir.types.forEachType
+import org.jetbrains.kotlin.fir.types.isSomeFunctionType
 import kotlin.reflect.full.memberProperties
 
 class IEReporter(
@@ -49,6 +52,11 @@ class IEReporter(
 data class IEData(
     val type: String? = null,
     val call: String? = null,
+    val isReified: Boolean? = null,
+    val oneLevelRedundant: Boolean? = null,
+    val oneLevelRedundantIncludingLambdas: Boolean? = null,
+    val upperBound: Boolean? = null,
+    val upperBoundedByTp: Boolean? = null,
 )
 
 object FirMyChecker : FirFunctionCallChecker(MppCheckerKind.Common) {
@@ -57,16 +65,43 @@ object FirMyChecker : FirFunctionCallChecker(MppCheckerKind.Common) {
     override fun check(expression: FirFunctionCall) {
         val report = IEReporter(expression.source, context, reporter, FirErrors.IE_DIAGNOSTIC)
         val symbol = expression.toResolvedCallableSymbol() ?: return
+        val usedParameters = buildSet {
+            symbol.fir.returnTypeRef.coneType.forEachType { type ->
+                if (type is ConeTypeParameterType) {
+                    add(type.lookupTag.typeParameterSymbol)
+                }
+            }
+        }
+        val usedInLambdas = buildSet {
+            (symbol.fir as? FirFunction)?.let { function ->
+                function.valueParameters.lastOrNull()?.let { lastParam ->
+                    if (lastParam.returnTypeRef.coneType.isSomeFunctionType(context.session)) {
+                        lastParam.returnTypeRef.coneType.forEachType { type ->
+                            if (type is ConeTypeParameterType) {
+                                add(type.lookupTag.typeParameterSymbol)
+                            }
+                        }
+                    }
+                }
+            }
+        }
         symbol.typeParameterSymbols.zip(expression.typeArguments).forEach { (param, type) ->
             if (!type.isExplicit) return@forEach
-            if (param.resolvedBounds.size != 1) return@forEach
-            val bound = param.resolvedBounds.first()
-            type as? FirTypeProjectionWithVariance ?: return@forEach
-            if (!bound.coneType.equalTypes(type.typeRef.coneType, context.session)) return@forEach
+            val bound = if (param.resolvedBounds.size == 1) param.resolvedBounds.first().coneType else null
+            if (type !is FirTypeProjectionWithVariance) return@forEach
+            val upperBound = bound?.equalTypes(type.typeRef.coneType, context.session) ?: false
+            val upperBoundedByTp = bound is ConeTypeParameterType
+            val oneLevelRedundant = param !in usedParameters
+            val oneLevelRedundantIncludingLambdas = param !in usedInLambdas && param !in usedParameters
             report(
                 IEData(
                     type = type.render(),
-                    call = symbol.name.toString()
+                    call = symbol.name.toString(),
+                    isReified = param.isReified,
+                    oneLevelRedundant = oneLevelRedundant,
+                    oneLevelRedundantIncludingLambdas = oneLevelRedundantIncludingLambdas,
+                    upperBound = upperBound,
+                    upperBoundedByTp = upperBoundedByTp,
                 )
             )
         }
