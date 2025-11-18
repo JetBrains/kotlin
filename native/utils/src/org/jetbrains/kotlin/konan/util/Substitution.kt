be0c573249b5de597369b4631351738ed85b3904
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-11-18 23:20:43 +0100

    Native: Allow substitution only for the limited set of properties
    We do not want property substitution to be possible for all manifest
    file properties, because this can be the source of hardly debuggable
    problems. Instead, we would like to permit substitution only for
    a limited set of properties where it is known to be used in practice
    and is known to be safe.
    
    ^KT-81411

--- native/utils/src/org/jetbrains/kotlin/konan/util/Substitution.kt
+++ native/utils/src/org/jetbrains/kotlin/konan/util/Substitution.kt
@@ -4,32 +4,47 @@ import org.jetbrains.kotlin.konan.target.KonanTarget
 import java.util.*
 
 fun Properties.substituteFor(target: KonanTarget): Properties {
+    val tagetSuffix = target.visibleName
+    val archSuffix = target.architecture.visibleName
+    val familySuffix = target.family.visibleName
+
+    val substitutionMap: MutableMap</* base property name */ String, Substitutions> = hashMapOf()
+
     val result = Properties()
-    result.putAll(this)
-    substitute(result, defaultTargetSubstitutions(target))
-    return result
-}
 
-private fun defaultTargetSubstitutions(target: KonanTarget) =
-        mapOf<String, String>(
-            "target" to target.visibleName,
-            "arch" to target.architecture.visibleName,
-            "family" to target.family.visibleName)
+    for (propertyName in stringPropertyNames()) {
+        val propertyValue = getProperty(propertyName)
+        result[propertyName] = propertyValue
 
-// Performs substitution similar to:
-//  foo = ${foo} ${foo.${arch}} ${foo.${os}}
-private fun substitute(properties: Properties, substitutions: Map<String, String>) {
-    val propertyNames = properties.stringPropertyNames().toList()
-    for (substitution in substitutions.values) {
-        for (key in propertyNames) {
-            val suffix = ".$substitution"
-            if (key.endsWith(suffix)) {
-                val baseKey = key.removeSuffix(suffix)
-                val oldValue = properties.getProperty(baseKey, "")
-                val appendedValue = properties.getProperty(key, "")
-                val newValue = if (oldValue != "") "$oldValue $appendedValue" else appendedValue
-                properties.setProperty(baseKey, newValue)
+        val dotIndex = propertyName.indexOf('.')
+        if (dotIndex != -1) {
+            val basePropertyName = propertyName.take(dotIndex)
+            if (basePropertyName in DefFileProperty.substitutablePropertyNames) {
+                fun getSubstitution() = substitutionMap.getOrPut(basePropertyName) { Substitutions() }
+
+                when (propertyName.substring(dotIndex + 1)) {
+                    tagetSuffix -> getSubstitution().targetValue = propertyValue
+                    archSuffix -> getSubstitution().archValue = propertyValue
+                    familySuffix -> getSubstitution().familyValue = propertyValue
+                }
             }
         }
     }
+
+    for ((basePropertyName, substitutions) in substitutionMap) {
+        result[basePropertyName] = listOfNotNull(
+            result.getProperty(basePropertyName),
+            substitutions.targetValue,
+            substitutions.archValue,
+            substitutions.familyValue
+        ).joinToString(" ")
+    }
+
+    return result
+}
+
+private class Substitutions {
+    var targetValue: String? = null
+    var archValue: String? = null
+    var familyValue: String? = null
 }
