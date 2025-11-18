f8f7fe1d921a6f68d2d285d39be60462a9bc7e19
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-11-18 18:45:32 +0100

    [references] KtDestructuringDeclarationReference: simplify resolve
    There is no need to call resolution at all since the result anyway
    is expected to be the destructuring declaration entry itself
    
    ^KT-82394

--- analysis/kt-references/src/org/jetbrains/kotlin/idea/references/KtDestructuringDeclarationReference.kt
+++ analysis/kt-references/src/org/jetbrains/kotlin/idea/references/KtDestructuringDeclarationReference.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -7,11 +7,9 @@ package org.jetbrains.kotlin.idea.references
 
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.util.IncorrectOperationException
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtDestructuringDeclaration
 import org.jetbrains.kotlin.psi.KtDestructuringDeclarationEntry
-import org.jetbrains.kotlin.idea.references.KtMultiReference
 
 abstract class KtDestructuringDeclarationReference(
     element: KtDestructuringDeclarationEntry
@@ -19,9 +17,7 @@ abstract class KtDestructuringDeclarationReference(
 
     override fun getRangeInElement() = TextRange(0, element.textLength)
 
-    override fun resolve() = multiResolve(false).asSequence()
-        .map { it.element }
-        .firstOrNull { it is KtDestructuringDeclarationEntry }
+    override fun resolve(): PsiElement? = element
 
     override val resolvesByNames: Collection<Name>
         get() {
