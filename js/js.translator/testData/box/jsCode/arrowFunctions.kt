60b3cd539ac22c67b781e3a2fe42c0f22d98efae
Andrii Rublov <andrii.rublov@jetbrains.com>
2025-11-19 16:38:45 +0100

    [K/JS]: KT-21626: Add recompilation in arrow functions test

--- js/js.translator/testData/box/jsCode/arrowFunctions.kt
+++ js/js.translator/testData/box/jsCode/arrowFunctions.kt
@@ -1,11 +1,14 @@
-package foo
+// FILE: a.kt
+
+val one = js("() => 1")
+val sum = js("(a, b) => a + b")
+val id = js("a => a")
+val block = js("(a) => { return 'Hello, ' + a; }")
+
+// FILE: b.kt
+// RECOMPILE
 
 fun box(): String {
-    val one = js("() => 1")
-    val sum = js("(a, b) => a + b")
-    val id = js("a => a")
-    val block = js("(a) => { return 'Hello, ' + a; }")
-
     assertEquals(1, one())
     assertEquals(3, sum(1, 2))
     assertEquals(4, id(4))
