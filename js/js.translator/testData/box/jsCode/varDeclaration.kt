9924d163903536202439e716f2e5a16e17eabee6
Andrii Rublov <andrii.rublov@jetbrains.com>
2025-11-19 16:39:46 +0100

    [K/JS]: KT-21626: Add recompilation in variable declaration test

--- js/js.translator/testData/box/jsCode/varDeclaration.kt
+++ js/js.translator/testData/box/jsCode/varDeclaration.kt
@@ -1,16 +1,23 @@
-package foo
-
-fun box(): String {
+// FILE: a.kt
+inline fun withDeclared(action: () -> Unit) {
     js("""
         var a = 123;
         const b = 123;
         let c = 123;
         c = a + b
     """)
+    action()
+}
 
-    assertEquals(123, js("a"))
-    assertEquals(123, js("b"))
-    assertEquals(246, js("c"))
+// FILE: b.kt
+// RECOMPILE
+
+fun box(): String {
+    withDeclared {
+        assertEquals(123, js("a"))
+        assertEquals(123, js("b"))
+        assertEquals(246, js("c"))
+    }
 
     return "OK"
 }
