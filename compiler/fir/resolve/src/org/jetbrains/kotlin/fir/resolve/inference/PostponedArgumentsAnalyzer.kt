e21c7d17cbea41256a2fbf9d9ab1057f8299863e
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-11-19 14:39:08 +0100

    FE: Remove unused default value at applyResultsOfAnalyzedLambdaToCandidateSystem

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/PostponedArgumentsAnalyzer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/PostponedArgumentsAnalyzer.kt
@@ -339,7 +339,7 @@ class PostponedArgumentsAnalyzer(
         lambda: ConeResolvedLambdaAtom,
         candidate: Candidate,
         results: ReturnArgumentsAnalysisResult,
-        substituteAlreadyFixedVariables: (ConeKotlinType) -> ConeKotlinType = c.createSubstituteFunctorForLambdaAnalysis(),
+        substituteAlreadyFixedVariables: (ConeKotlinType) -> ConeKotlinType,
     ) {
         val (returnAtoms, additionalConstraintStorage) = results
         val returnArguments = returnAtoms.map { it.expression }
@@ -465,12 +465,6 @@ class PostponedArgumentsAnalyzer(
                 )
             }
         }
-    }
-
-    private fun PostponedArgumentsAnalyzerContext.createSubstituteFunctorForLambdaAnalysis(): (ConeKotlinType) -> ConeKotlinType {
-        val stubsForPostponedVariables = bindingStubsForPostponedVariables()
-        val currentSubstitutor = buildCurrentSubstitutor(stubsForPostponedVariables.mapKeys { it.key.freshTypeConstructor(this) })
-        return { currentSubstitutor.safeSubstitute(this, it).asCone() }
     }
 }
 
