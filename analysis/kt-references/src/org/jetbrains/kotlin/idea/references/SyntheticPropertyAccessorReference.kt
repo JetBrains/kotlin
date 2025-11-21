65cc409523484d0735e9f17f196d9fb47b536ce3
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-11-21 16:42:10 +0100

    [references] SyntheticPropertyAccessorReference: resolvesByNames should support synthetic properties
    ^KT-82615

--- analysis/kt-references/src/org/jetbrains/kotlin/idea/references/SyntheticPropertyAccessorReference.kt
+++ analysis/kt-references/src/org/jetbrains/kotlin/idea/references/SyntheticPropertyAccessorReference.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -8,11 +8,12 @@ package org.jetbrains.kotlin.idea.references
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
+import org.jetbrains.kotlin.asJava.canHaveSyntheticGetter
+import org.jetbrains.kotlin.asJava.canHaveSyntheticSetter
+import org.jetbrains.kotlin.load.java.JvmAbi
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.resolve.references.ReferenceAccess
-import org.jetbrains.kotlin.asJava.canHaveSyntheticSetter
-import org.jetbrains.kotlin.asJava.canHaveSyntheticGetter
 
 abstract class SyntheticPropertyAccessorReference(
     expression: KtNameReferenceExpression,
@@ -37,5 +38,13 @@ abstract class SyntheticPropertyAccessorReference(
     override fun canRename() = true
 
     override val resolvesByNames: Collection<Name>
-        get() = listOf(element.getReferencedNameAsName())
+        get() {
+            val name = element.getReferencedNameAsName()
+            val nameAsString = name.asString()
+            return listOfNotNull(
+                name,
+                JvmAbi.getterName(nameAsString).takeIf { it != nameAsString }?.let(Name::identifier),
+                JvmAbi.setterName(nameAsString).takeIf { it != nameAsString }?.let(Name::identifier),
+            )
+        }
 }
