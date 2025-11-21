e7dbb375932dcdff5a28e4ffcd65f9ad2d9bc0e4
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-11-21 16:36:54 +0100

    ~ add test data for throwable vs non-throwable vs runnable SAM interfaces

---
+++ compiler/fir/analysis-tests/testData/resolve/callResolution/orblrt/throwableJavaSam.kt
@@ -0,0 +1,45 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// FIR_DUMP
+// FULL_JDK
+// WITH_STDLIB
+// ISSUE: KT-7052
+
+// FILE: MyComputable.java
+public interface MyComputable<V> {
+    V compute();
+}
+
+// FILE: MyThrowableComputable.java
+public interface MyThrowableComputable<T, E extends Throwable> {
+    T compute() throws E;
+}
+
+// FILE: MyJavaClass.java
+public class MyJavaClass {
+    public static void runSomething(Runnable computable) {
+    }
+
+    public static <T> T runSomething(MyComputable<T> computable) {
+        return null;
+    }
+
+    public static <T, E extends Throwable> T runSomething(MyThrowableComputable<T, E> computable) throws E {
+        return null;
+    }
+
+    public static <T> T runSomething2(MyComputable<T> computable) {
+        return null;
+    }
+
+    public static <T, E extends Throwable> T runSomething2(MyThrowableComputable<T, E> computable) throws E {
+        return null;
+    }
+}
+
+// FILE: main.kt
+fun main() {
+    MyJavaClass.<!OVERLOAD_RESOLUTION_AMBIGUITY!>runSomething<!> { "" }
+    MyJavaClass.<!OVERLOAD_RESOLUTION_AMBIGUITY!>runSomething2<!> { "" }
+}
+
+/* GENERATED_FIR_TAGS: functionDeclaration, lambdaLiteral, stringLiteral */
