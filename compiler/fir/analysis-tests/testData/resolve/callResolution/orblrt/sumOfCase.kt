10dbfb1659378101f63771c81e5586fb82ee29f7
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-11-21 14:24:22 +0100

    ~ fix sumOf

--- compiler/fir/analysis-tests/testData/resolve/callResolution/orblrt/sumOfCase.kt
+++ compiler/fir/analysis-tests/testData/resolve/callResolution/orblrt/sumOfCase.kt
@@ -1,4 +1,4 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 // WITH_STDLIB
 
 @JvmName("sumInt")
@@ -6,12 +6,12 @@ fun <T1> Iterable<T1>.mySumOf(selector: (T1) -> Int): Int = 0
 fun <T2> Iterable<T2>.mySumOf(selector: (T2) -> Long): Long = 0
 
 fun main(x: List<String>) {
-    val y1 = x.<!OVERLOAD_RESOLUTION_AMBIGUITY!>mySumOf<!> { 0 }
+    val y1 = x.mySumOf { 0 }
     val y2 = x.mySumOf { 0L }
     val y3 = x.mySumOf { it.length }
     val y4 = x.mySumOf { it.length.toLong() }
 
-    <!DEBUG_INFO_EXPRESSION_TYPE("ERROR CLASS: Ambiguity: mySumOf, [/mySumOf, /mySumOf]")!>y1<!>
+    <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Int")!>y1<!>
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Long")!>y2<!>
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Int")!>y3<!>
     <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.Long")!>y4<!>
