5fd437ecf342674bf07c0f9c5dbfad06b4c5af31
Andrey Mogilev <andrey.mogilev@jetbrains.com>
2025-11-24 21:50:38 +0100

    fixup! [JVM] generate bridges for abstract overrides in interfaces
    adds a property to interfacesBridges.kt test

--- compiler/testData/codegen/bytecodeListing/interfaceBridges.kt
+++ compiler/testData/codegen/bytecodeListing/interfaceBridges.kt
@@ -1,6 +1,7 @@
 // JVM_DEFAULT_MODE: disable
 
 interface I1<T: Any> {
+    var property: T
     fun foo(p: T)
     fun bar(): Any
 }
@@ -11,6 +12,7 @@ interface I2<T: Number> : I1<T>
 // bridges are expected to be generated as default interface methods
 // JVM_DEFAULT_MODE does not affect bridges generation
 interface I3<T: Number> : I2<T> {
+    abstract override var property: T
     override fun foo(p: T)
     override fun bar(): Number
 }
@@ -20,11 +22,15 @@ open abstract class C1<T: Int> : I3<T>
 
 // bridges are expected both for Number & Object signatures
 open abstract class C2<T: Int> : C1<T>() {
+    abstract override var property: T
     abstract override fun foo(p: T)
     abstract override fun bar(): Int
 }
 
 open class C3<T: Int> : C2<T>() {
+    @Suppress("UNCHECKED_CAST")
+    override var property: T = 0 as T
+
     override fun foo(p: T) {}
     override fun bar(): Int = 42
 }
