39d28364407e15db69039c3ddbd284a4379bbdb7
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-11-26 14:33:59 +0200

    ~~ JVM diagnostic tests ~~

--- compiler/testData/diagnostics/tests/visibility/exposedPrivate/publicInInternalClass.kt
+++ compiler/testData/diagnostics/tests/visibility/exposedPrivate/publicInInternalClass.kt
@@ -1,3 +1,4 @@
+// RUN_PIPELINE_TILL: FRONTEND
 // DIAGNOSTICS: -NOTHING_TO_INLINE
 
 private class C
@@ -6,6 +7,8 @@ private inline fun privateFun() = C()
 
 internal open class A {
     public inline fun test() {
-        <!PRIVATE_TYPE_USED_IN_NON_PRIVATE_INLINE_FUNCTION_WARNING!>privateFun()<!>
+        privateFun()
     }
 }
+
+/* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, inline */
