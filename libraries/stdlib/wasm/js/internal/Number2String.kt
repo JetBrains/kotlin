257265f950f50e47ce1a916229f7eefb4da2d4e5
Aleksandr Shefer <aleksandr.shefer@jetbrains.com>
2025-11-27 09:19:42 +0100

    [Wasm] Use Number.prototype.toString instead of String for Integer to String conversions (KT-82718)

--- libraries/stdlib/wasm/js/internal/Number2String.kt
+++ libraries/stdlib/wasm/js/internal/Number2String.kt
@@ -2,17 +2,18 @@
  * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+@file:OptIn(ExperimentalWasmJsInterop::class)
 
 package kotlin.wasm.internal
 
-@JsName("String")
-internal actual external fun itoa32(inputValue: Int): String
+internal actual fun itoa32(inputValue: Int): String =
+    js("Function.prototype.call.bind(Number.prototype.toString)(inputValue)")
 
-@JsName("String")
-internal actual external fun utoa32(inputValue: UInt): String
+internal actual fun utoa32(inputValue: UInt): String =
+    js("Function.prototype.call.bind(Number.prototype.toString)(inputValue)")
 
-@JsName("String")
-internal actual external fun itoa64(inputValue: Long): String
+internal actual fun itoa64(inputValue: Long): String =
+    js("Function.prototype.call.bind(BigInt.prototype.toString)(inputValue)")
 
-@JsName("String")
-internal actual external fun utoa64(inputValue: ULong): String
+internal actual fun utoa64(inputValue: ULong): String =
+    js("Function.prototype.call.bind(BigInt.prototype.toString)(inputValue)")
