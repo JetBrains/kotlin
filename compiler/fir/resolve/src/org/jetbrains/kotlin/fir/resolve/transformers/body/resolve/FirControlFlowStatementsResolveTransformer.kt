704b580e1f7ee8af3e1cd0926c3551fdcc91286f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-11-27 13:48:16 +0100

    K2: during Elvis resolve, don't use expected type from equality operator
    Before this commit, while resolving things like 'a == (b ?: c)'
    we used Any? as Elvis expected type, that could provoke strange
    corner case results (see comments in KT-81763).
    As such a constraint in fact produces no significant information,
    in this commit we disabled such kind of constraints in Elvis,
    thus returning to K1 behavior in ILT-related cases like
    'someLong == (nullableLong ?: 0)'.
    
    #KT-81763 Fixed

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirControlFlowStatementsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirControlFlowStatementsResolveTransformer.kt
@@ -234,16 +234,18 @@ class FirControlFlowStatementsResolveTransformer(transformer: FirAbstractBodyRes
         )
         dataFlowAnalyzer.exitElvisLhs(elvisExpression)
 
+        val resolutionModeForRhs = withExpectedType(
+            data.expectedTypeUnlessFromEquality,
+            lastStatementInBlock = (data as? ResolutionMode.WithExpectedType)?.lastStatementInBlock == true
+        )
         elvisExpression.transformRhs(
             transformer,
-            withExpectedType(
-                data.expectedType,
-                lastStatementInBlock = (data as? ResolutionMode.WithExpectedType)?.lastStatementInBlock == true
-            )
+            resolutionModeForRhs
         )
 
         val result = callCompleter.completeCall(
-            syntheticCallGenerator.generateCalleeForElvisExpression(elvisExpression, resolutionContext, data), data
+            syntheticCallGenerator.generateCalleeForElvisExpression(elvisExpression, resolutionContext, data),
+            if (data is ResolutionMode.WithExpectedType) resolutionModeForRhs else data
         )
 
         var isLhsNotNull = false
@@ -300,7 +302,7 @@ class FirControlFlowStatementsResolveTransformer(transformer: FirAbstractBodyRes
     private fun computeResolutionModeForElvisLHS(
         data: ResolutionMode,
     ): ResolutionMode {
-        val expectedType = data.expectedType
+        val expectedType = data.expectedTypeUnlessFromEquality
         val lastStatementInBlock = (data as? ResolutionMode.WithExpectedType)?.lastStatementInBlock == true
 
         val isObsoleteCompilerMode =
