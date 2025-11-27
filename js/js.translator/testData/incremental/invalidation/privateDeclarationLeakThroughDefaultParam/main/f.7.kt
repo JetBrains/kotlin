726dff805ba21588a53425a660fc53158e674707
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-11-27 14:50:49 +0200

    [LV Update] Remove js invalidation test with leaked private declarations
    ^KT-81590

--- js/js.translator/testData/incremental/invalidation/privateDeclarationLeakThroughDefaultParam/main/f.7.kt
+++
@@ -1,5 +0,0 @@
-private inline fun prop() = "7"
-
-internal inline fun foo(lambda: () -> String = ::prop): String {
-    return lambda()
-}
--- js/js.translator/testData/incremental/invalidation/privateDeclarationLeakThroughDefaultParam/main/f.8.kt
+++
@@ -1,9 +0,0 @@
-private object Obj {
-    val x = "8"
-
-    override fun toString() = x
-}
-
-internal inline fun foo(lambda: () -> String = { "$Obj" } ): String {
-    return lambda()
-}
