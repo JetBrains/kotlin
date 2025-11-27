4e904d89486e202a7e17217c885f0195eebd80ad
Charles Zhang <charles.zhang@jetbrains.com>
2025-11-27 15:13:33 +0100

    [wasm]: Relax js(...) call constraints somewhat when lowering.
    We have many constraints on how js(...) can be used when targeting the
    wasm backend, in particular that functions using it must only call
    js(...) and do nothing else. The lowering phase assumed this, which
    was mostly fine because there are also syntactic validation checks to
    make sure uses of js(...) conformed to these restrictions.
    
    However, function annotations such as @Composable can actually do
    arbitrary code transformations which break the constraints that the
    lowering pass assumes, even if the original function passes the
    validation steps pre-transformation. In order to not have to maintain
    a whitelist or blacklist of allowable code annotations, this change
    attempts to relax the restrictions just enough to allow code
    transformations which preserve the semantics of the js call (which
    should be most well-behaving annotations) to work correctly.
    
    In particular, the lowering pass now supports multiple calls to js in
    unrestricted positions in a function which may do other things as
    well, for example in conditional branches or other control flow
    constructs that a code transformation may wrap around the original
    code. However, the restrictions which remain create slightly
    unintuitive semantics which makes it unsuitable to expose these
    relaxed semantics to user code, so none of the user-code validation
    checks have been lifted. In particular, the remaining restrictions
    which remain on the lowering side due to implementation reasons are:
    
    * Only function parameters can be referenced in js code.
      ** Closing-over local variables, while possible, essentially force a
         shared-variables or closure conversion type analysis to occur,
         and for assignment to work, heap allocation of value cells is
         entailed.
    * The return type of all calls to js(...) is taken directly from the
      return type of the entire function.
      ** While it may make more sense to try and figure out the correct
      expected return type from the calling context, the type system
      doesn't work that way.
    
    Since js(...) calls are opaque to code transformation annotations,
    it's not really possible that code annotations which seek to preserve
    the semantics of the js call will change the expected return type or
    introduce locals that the js call needs to reference. For user code,
    the return type quirk is going to be too unexpected.
    
    No functional difference should otherwise be observed.
    
    Fixes user program in ^KT-79411.

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/JsCodeCallsLowering.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/lower/JsCodeCallsLowering.kt
@@ -18,6 +18,8 @@ import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.util.copyTo
 import org.jetbrains.kotlin.ir.util.copyTypeParametersFrom
 import org.jetbrains.kotlin.ir.util.transformDeclarationsFlat
+import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
 
 /**
  * Lower calls to `js(code)` into `@JsFun(code) external` functions.
@@ -36,64 +38,102 @@ class JsCodeCallsLowering(val context: WasmBackendContext) : FileLoweringPass {
         }
     }
 
+    // Wrap a piece of javascript code in a function definition.
+    private fun wrapJsCode(jsCode: String, function: IrSimpleFunction, forValue: Boolean): String {
+        return buildString {
+            append('(')
+            append(function.parameters.joinToString { it.name.identifier })
+            append(") => ")
+            if (!forValue) append("{ ")
+            append(jsCode)
+            if (!forValue) append(" }")
+        }
+    }
+
+    private inner class Transformer(val function: IrSimpleFunction, val externalFuns: MutableList<IrDeclaration>) : IrElementTransformerVoid() {
+        private var lastDeclaration: IrDeclaration? = null
+        override fun visitBody(body: IrBody): IrBody = body
+
+        // Replace a call to 'js' with a call to a JS external
+        // fun. The arguments of the external function are the same as
+        // the function parameters of the original call, with any
+        // default arguments made explicit. The type is also taken
+        // from the function return type. This is a kludge as it is
+        // not obvious how to infer the `correct` type.
+        override fun visitCall(expression: IrCall): IrExpression {
+            if (expression.symbol != jsRelatedSymbols.jsCode)
+                return super.visitCall(expression)
+            val jsCode = (expression.arguments[0] as IrConst).value as String
+            // TODO: We could generate slightly more optimal code if
+            // we could tell if this call is used for value or not.
+            val jsFunCode = wrapJsCode(jsCode, function, true)
+            val externalFun = context.irFactory.stageController.restrictTo(lastDeclaration?: function) {
+                createExternalJsFunction(
+                    context,
+                    function.name,
+                    "_js_code_${externalFuns.size}",
+                    function.returnType,
+                    jsCode = jsFunCode,
+                )
+            }
+            lastDeclaration = externalFun
+            externalFun.copyTypeParametersFrom(function)
+            externalFun.parameters = function.parameters.map { it.copyTo(externalFun, defaultValue = null) }
+            externalFuns.add(externalFun)
+            return context.createIrBuilder(function.symbol, expression.startOffset, expression.endOffset).run {
+                val expression = irCall(externalFun.symbol)
+                function.parameters.forEachIndexed { index, parameter ->
+                    expression.arguments[index] = irGet(parameter)
+                }
+                function.typeParameters.forEachIndexed { index, typeParameter ->
+                    expression.typeArguments[index] = typeParameter.defaultType
+                }
+                super.visitCall(expression)
+            }
+        }
+    }
+
+    // If function has no default arguments and the body is a single
+    // statement/expression to a call to js(...), lower the call by
+    // just changing the function's annotation to that of an external
+    // js function. Otherwise, create an external entry point stubs
+    // for every call to js(...) in the function, where we capture
+    // only the function parameters of the original function. (For
+    // now, we only support JS code access to function parameters,
+    // with the call type matching the return type of the function.)
     private fun transformFunction(function: IrSimpleFunction): List<IrDeclaration>? {
         val body = function.body ?: return null
 
-        val statement = when (body) {
+        fun generalCase(): List<IrDeclaration>? {
+            val declarations = mutableListOf<IrDeclaration>()
+            body.transformChildrenVoid(Transformer(function, declarations))
+            if (declarations.isEmpty())
+                return null
+            declarations.add(function)
+            return declarations
+        }
+
+        val singleExpression = when (body) {
             is IrExpressionBody -> body.expression
             is IrBlockBody -> body.statements.singleOrNull()
             else -> null
         }
 
-        val isSingleExpressionJsCode: Boolean
-        val jsCode: String
-        when (statement) {
-            is IrReturn -> {
-                jsCode = statement.value.getJsCode() ?: return null
-                isSingleExpressionJsCode = true
-            }
-            is IrCall -> {
-                jsCode = statement.getJsCode() ?: return null
-                isSingleExpressionJsCode = false
-            }
-            else -> return null
-        }
-        val valueParameters = function.parameters
-        val jsFunCode = buildString {
-            append('(')
-            append(valueParameters.joinToString { it.name.identifier })
-            append(") => ")
-            if (!isSingleExpressionJsCode) append("{ ")
-            append(jsCode)
-            if (!isSingleExpressionJsCode) append(" }")
-        }
-        if (function.parameters.any { it.defaultValue != null }) {
-            // Create a separate external function without default arguments
-            // and delegate calls to it.
-            val externalFun = context.irFactory.stageController.restrictTo(function) {
-                createExternalJsFunction(
-                    context,
-                    function.name,
-                    "_js_code",
-                    function.returnType,
-                    jsCode = jsFunCode,
-                )
-            }
-            externalFun.copyTypeParametersFrom(function)
-            externalFun.parameters = function.parameters.map { it.copyTo(externalFun, defaultValue = null) }
-            function.body = context.createIrBuilder(function.symbol).irBlockBody {
-                val call = irCall(externalFun.symbol)
-                function.parameters.forEachIndexed { index, parameter ->
-                    call.arguments[index] = irGet(parameter)
-                }
-                function.typeParameters.forEachIndexed { index, typeParameter ->
-                    call.typeArguments[index] = typeParameter.defaultType
-                }
-                +irReturn(call)
-            }
-            return listOf(function, externalFun)
+        if (singleExpression == null || function.parameters.any { it.defaultValue != null })
+            return generalCase()
+
+        val (jsCode: String?, forValue: Boolean) = when (singleExpression) {
+            is IrReturn -> singleExpression.value.getJsCode() to true
+            is IrCall -> singleExpression.getJsCode() to false
+            else -> null to false
         }
 
+        if (jsCode == null)
+            return generalCase()
+
+        // At this point, the function is eligible to be marked
+        // external directly.
+        val jsFunCode = wrapJsCode(jsCode, function, forValue)
         val builder = context.createIrBuilder(function.symbol)
         function.annotations += builder.irCallConstructor(jsRelatedSymbols.jsFunConstructor, typeArguments = emptyList()).also {
             it.arguments[0] = builder.irString(jsFunCode)
