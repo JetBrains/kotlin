5df5b4e6bf016ccd3d681767793405738b39c7bc
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2025-11-28 14:06:57 +0100

    Rework node argument updating

--- native/hair/compiler/src/commonMain/kotlin/hair/ir/NodeCloner.kt
+++ native/hair/compiler/src/commonMain/kotlin/hair/ir/NodeCloner.kt
@@ -2,22 +2,15 @@ package hair.ir
 
 import hair.ir.nodes.Node
 import hair.ir.nodes.NodeBuilder
-import hair.ir.nodes.SimpleArgsUpdater
 import hair.ir.nodes.*
-import hair.opt.NormalizationImpl
 import hair.utils.forEachInWorklist
-import hair.utils.withWorklist
 
 context(_: NodeBuilder, _: ArgsUpdater)
 fun Session.cloneNodes(originals: Sequence<Node>, replacementProvider: (Node) -> Node?): Map<Node, Node> {
     val destinationSession = this
     val clones = mutableMapOf<Node, Node>()
     val replacements = mutableMapOf<Node, Node>()
-    val shallowCloner = ShallowNodeCloner(object : NodeBuilder {
-        override val session = destinationSession
-        override fun normalize(node: Node): Node = node
-        override fun <N : Node> register(node: N): N = node
-    })
+    val shallowCloner = ShallowNodeCloner(RawNodeBuilder(this))
     fun cloneShallow(original: Node): Node = clones.getOrPut(original) {
         val replacement = replacementProvider(original)
         require(replacement == null || replacement.session == destinationSession)
@@ -43,7 +36,7 @@ fun Session.cloneNodes(originals: Sequence<Node>, replacementProvider: (Node) ->
 
     val normalization = normalization
     forEachInWorklist(clones.values) {
-        val registered = it.register()
+        val registered = it// TODO.register()
         val normal = normalization.normalize(registered)
         if (normal != it) {
             it.replaceValueUses(normal)
