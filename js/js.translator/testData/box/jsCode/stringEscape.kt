cb421bf49fd1315270b0745a8fa90080f76508c3
Andrii Rublov <andrii.rublov@jetbrains.com>
2025-12-01 20:13:39 +0100

    [K/JS]: Fix ordering of `assertEquals` arguments in `stringEscape` test

--- js/js.translator/testData/box/jsCode/stringEscape.kt
+++ js/js.translator/testData/box/jsCode/stringEscape.kt
@@ -1,15 +1,16 @@
 package foo
+
 fun box(): String {
-    assertEquals(js("'\\u1234'"), "\u1234")
-    assertEquals(js("'\\xFA'"), "\u00FA")
-    assertEquals(js("'\\b\\f\\n\\r\\t\\v\\\\'"), "\b\u000C\n\r\t\u000B\\")
-    assertEquals(js("'a\\\nb'"), "ab")
-    assertEquals(js("'ab\\0\\01\\012\\123\\377c'"), "ab\u0000\u0001\u000A\u0053\u00FFc")
-    assertEquals(js("'\\401'"), " 1")
-    assertEquals(js("'\\0'"), "\u0000")
-    assertEquals(js("'\\a'"), "a")
-    assertEquals(js(""" '"' """), "\"")
-    assertEquals(js(""" "'" """), "'")
-    assertEquals(js("'`'"), "`")
+    assertEquals("\u1234", js("'\\u1234'"))
+    assertEquals("\u00FA", js("'\\xFA'"))
+    assertEquals("\b\u000C\n\r\t\u000B\\", js("'\\b\\f\\n\\r\\t\\v\\\\'"))
+    assertEquals("ab", js("'a\\\nb'"))
+    assertEquals("ab\u0000\u0001\u000A\u0053\u00FFc", js("'ab\\0\\01\\012\\123\\377c'"))
+    assertEquals(" 1", js("'\\401'"))
+    assertEquals("\u0000", js("'\\0'"))
+    assertEquals("a", js("'\\a'"))
+    assertEquals("\"", js(""" '"' """))
+    assertEquals("'", js(""" "'" """))
+    assertEquals("`", js("'`'"))
     return "OK"
 }
