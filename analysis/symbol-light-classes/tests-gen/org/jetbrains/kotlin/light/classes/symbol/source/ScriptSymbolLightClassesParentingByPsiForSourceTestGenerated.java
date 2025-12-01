02fb4d36190b778c5d88681abada419dc387dc67
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-12-01 17:56:11 +0400

    fixup! [SLC] Add testdata for mapped Kotlin collection classes
    WIP value classes

--- analysis/symbol-light-classes/tests-gen/org/jetbrains/kotlin/light/classes/symbol/source/ScriptSymbolLightClassesParentingByPsiForSourceTestGenerated.java
+++ analysis/symbol-light-classes/tests-gen/org/jetbrains/kotlin/light/classes/symbol/source/ScriptSymbolLightClassesParentingByPsiForSourceTestGenerated.java
@@ -52,6 +52,16 @@ public class ScriptSymbolLightClassesParentingByPsiForSourceTestGenerated extend
         KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly"), Pattern.compile("^(.+)\\.(kts)$"), null, true);
       }
     }
+
+    @Nested
+    @TestMetadata("compiler/testData/asJava/lightClasses/lightClassByPsi/collections/valueClasses")
+    @TestDataPath("$PROJECT_ROOT")
+    public class ValueClasses {
+      @Test
+      public void testAllFilesPresentInValueClasses() {
+        KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/asJava/lightClasses/lightClassByPsi/collections/valueClasses"), Pattern.compile("^(.+)\\.(kts)$"), null, true);
+      }
+    }
   }
 
   @Nested
