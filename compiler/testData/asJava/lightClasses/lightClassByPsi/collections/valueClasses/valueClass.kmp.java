c8e4d383b20c8dfad482e626e9bfa5a6684f00f0
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-12-01 18:11:33 +0400

    fixup! [SLC] Support Kotlin collection classes mapped to Java classes
    WIP value classes

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/valueClasses/valueClass.kmp.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/valueClasses/valueClass.kmp.java
@@ -3,29 +3,35 @@ public abstract class SCollection /* test.SCollection*/ implements java.util.Col
 }
 
 public abstract class SCollection2 /* test.SCollection2*/ implements java.util.Collection<@org.jetbrains.annotations.NotNull() kotlin.UInt>, kotlin.jvm.internal.markers.KMappedMarker {
-  private final int size;
-
+  @java.lang.Override()
   @org.jetbrains.annotations.NotNull()
   public @org.jetbrains.annotations.NotNull() java.util.Iterator<@org.jetbrains.annotations.NotNull() kotlin.UInt> iterator();//  iterator()
 
-  public  SCollection2();//  .ctor()
-
+  @java.lang.Override()
   public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() kotlin.UInt>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() kotlin.UInt>)
 
+  @java.lang.Override()
   public boolean isEmpty();//  isEmpty()
 
+  @java.lang.Override()
   public int getSize();//  getSize()
+
+  public  SCollection2();//  .ctor()
 }
 
 public class SCollection3 /* test.SCollection3*/ implements java.util.Collection<@org.jetbrains.annotations.NotNull() kotlin.UInt>, kotlin.jvm.internal.markers.KMappedMarker {
+  @java.lang.Override()
   @org.jetbrains.annotations.NotNull()
   public @org.jetbrains.annotations.NotNull() java.util.Iterator<@org.jetbrains.annotations.NotNull() kotlin.UInt> iterator();//  iterator()
 
-  public  SCollection3();//  .ctor()
-
+  @java.lang.Override()
   public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() kotlin.UInt>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() kotlin.UInt>)
 
+  @java.lang.Override()
   public boolean isEmpty();//  isEmpty()
 
+  @java.lang.Override()
   public int getSize();//  getSize()
+
+  public  SCollection3();//  .ctor()
 }
