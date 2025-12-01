7a83ca21822aea52319df028824e344cfc5febd1
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-12-01 22:32:32 +0100

    Klib API: Drop `KotlinLibrary` internal implementation classes
    ^KT-81411

--- compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibraryUtils.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibraryUtils.kt
@@ -1,27 +1,7 @@
 package org.jetbrains.kotlin.library
 
-import org.jetbrains.kotlin.konan.file.File
-import org.jetbrains.kotlin.konan.file.unzipTo
-import org.jetbrains.kotlin.library.impl.zippedKotlinLibraryChecks
-
 const val KLIB_FILE_EXTENSION = "klib"
 const val KLIB_FILE_EXTENSION_WITH_DOT = ".$KLIB_FILE_EXTENSION"
-
-fun File.unpackZippedKonanLibraryTo(newDir: File) {
-
-    // First, run validity checks for the given KLIB file.
-    zippedKotlinLibraryChecks(this)
-
-    if (newDir.exists) {
-        if (newDir.isDirectory)
-            newDir.deleteRecursively()
-        else
-            newDir.delete()
-    }
-
-    this.unzipTo(newDir)
-    check(newDir.exists) { "Could not unpack $this as $newDir." }
-}
 
 val List<String>.toUnresolvedLibraries
     get() = this.map {
