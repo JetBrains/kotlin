cf3378a9d8d9283ed08dde517ba29823447fc6c3
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-12-01 17:56:11 +0400

    fixup! [SLC] Add testdata for mapped Kotlin collection classes
    WIP value classes

---
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/valueClasses/implicitJvmExposeBoxed.descriptors.java
@@ -0,0 +1,33 @@
+public abstract class SCollection /* test.SCollection*/ implements java.util.Collection<kotlin.UInt>, kotlin.collections.Collection<kotlin.UInt>, kotlin.jvm.internal.markers.KMappedMarker {
+  public  SCollection();//  .ctor()
+}
+
+public abstract class SCollection2 /* test.SCollection2*/ implements java.util.Collection<kotlin.UInt>, kotlin.collections.Collection<kotlin.UInt>, kotlin.jvm.internal.markers.KMappedMarker {
+  @org.jetbrains.annotations.NotNull()
+  public java.util.Iterator<kotlin.UInt> iterator();//  iterator()
+
+  public  SCollection2();//  .ctor()
+
+  public boolean contains-WZ4Q5Ns(int);//  contains-WZ4Q5Ns(int)
+
+  public boolean containsAll();//  containsAll()
+
+  public boolean isEmpty();//  isEmpty()
+
+  public int getSize();//  getSize()
+}
+
+public class SCollection3 /* test.SCollection3*/ implements java.util.Collection<kotlin.UInt>, kotlin.collections.Collection<kotlin.UInt>, kotlin.jvm.internal.markers.KMappedMarker {
+  @org.jetbrains.annotations.NotNull()
+  public java.util.Iterator<kotlin.UInt> iterator();//  iterator()
+
+  public  SCollection3();//  .ctor()
+
+  public boolean contains(int);//  contains(int)
+
+  public boolean containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection);//  containsAll(java.util.Collection)
+
+  public boolean isEmpty();//  isEmpty()
+
+  public int getSize();//  getSize()
+}
