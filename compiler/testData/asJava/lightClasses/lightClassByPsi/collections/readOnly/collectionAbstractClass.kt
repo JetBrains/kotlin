70c9f55ec84cfb54cb87050333b95ae7863f1f5a
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-12-02 16:47:10 +0400

    [SLC] WIP debug

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/collectionAbstractClass.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/readOnly/collectionAbstractClass.kt
@@ -3,26 +3,26 @@ package test
 
 abstract class CCollection<Elem> : Collection<Elem>
 
-abstract class CCollection2<Elem> : Collection<Elem> by emptyList()
-
-open class CCollection3<Elem> : Collection<Elem> {
-    override fun contains(element: Elem): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun containsAll(elements: Collection<Elem>): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun isEmpty(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun iterator(): Iterator<Elem> {
-        TODO("Not yet implemented")
-    }
-
-    override val size: Int
-        get() = TODO("Not yet implemented")
-}
+//abstract class CCollection2<Elem> : Collection<Elem> by emptyList()
+//
+//open class CCollection3<Elem> : Collection<Elem> {
+//    override fun contains(element: Elem): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun containsAll(elements: Collection<Elem>): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun isEmpty(): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun iterator(): Iterator<Elem> {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override val size: Int
+//        get() = TODO("Not yet implemented")
+//}
 // LIGHT_ELEMENTS_NO_DECLARATION: CCollection.class[add;addAll;clear;getSize;iterator;remove;removeAll;removeIf;retainAll;size;toArray;toArray], CCollection2.class[add;addAll;clear;remove;removeAll;removeIf;retainAll;size;toArray;toArray], CCollection3.class[add;addAll;clear;remove;removeAll;removeIf;retainAll;size;toArray;toArray]
