d58ca7d1349e78889d28d497bf992cf4aa56df11
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-12-02 19:57:21 +0100

    [Tests] Make IGNORE_ compatibility directives to respect targets
    ^KT-78188

--- compiler/testData/codegen/box/when/whenWithoutBranches.kt
+++ compiler/testData/codegen/box/when/whenWithoutBranches.kt
@@ -1,4 +1,6 @@
 // ISSUE: KT-82844
+// IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_FIRST_STAGE: ANY:2.0,2.1,2.2,2.3.0
+// ^^^ KT-82844 is fixed in 2.3.20-Beta1
 
 fun box(): String {
     var a = 1
--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/klib/utils.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/klib/utils.kt
@@ -6,22 +6,55 @@
 package org.jetbrains.kotlin.test.klib
 
 import org.jetbrains.kotlin.config.LanguageVersion
-import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
 import org.jetbrains.kotlin.test.directives.model.StringDirective
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.moduleStructure
+import org.jetbrains.kotlin.test.services.targetPlatform
 
 /*
  * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+const val VERSION_AND_TARGET_SEPARATOR = ':'
+const val TARGETS_SEPARATOR = ','
+const val VERSIONS_SEPARATOR = ','
 
-context(directives: RegisteredDirectives, customCompilerVersion: LanguageVersion)
-internal fun createUnmutingErrorIfNeeded(stringDirective: StringDirective): List<Throwable> {
-    return if (directives[stringDirective].any { it.startsWith(customCompilerVersion.versionString) })
+internal fun TestServices.createUnmutingErrorIfNeeded(stringDirective: StringDirective, defaultLanguageVersion: LanguageVersion): List<Throwable> {
+    return if (versionAndTargetAreIgnored(stringDirective, defaultLanguageVersion))
         listOf(
             AssertionError(
-                "Looks like this test can be unmuted. Remove $customCompilerVersion from the $stringDirective directive"
+                "Looks like this test can be unmuted. Remove $defaultLanguageVersion from the $stringDirective directive"
             )
         )
     else emptyList()
+}
+
+/**
+ * Check whether defaultLanguageVersion and `target platform name` match to one of values of ignore directive in formats:
+ * - `<VERSION>`, for ex., `1.9.20` or `2.0` or `2.2.21` or `*`
+ * - `<TARGETPLATFORM_LIST>:<VERSION>`, for ex., `JS:2.0` or `JS,Native:*` or `JS,Wasm:2.2.20` or `ANY:1.9` or `ANY:2.0,2.1`
+ */
+internal fun TestServices.versionAndTargetAreIgnored(directive: StringDirective, defaultLanguageVersion: LanguageVersion): Boolean {
+    val firstModule = moduleStructure.modules.first()
+    val platformName = firstModule.targetPlatform(this).componentPlatforms.single().platformName
+    val versionString = defaultLanguageVersion.versionString
+
+    for (maybeTuple in firstModule.directives[directive]) {
+        val parts = maybeTuple.split(VERSION_AND_TARGET_SEPARATOR)
+        // Check for a matching version or ANY version
+        val lastPart = parts.last()
+        if (lastPart == "*" || lastPart.split(VERSIONS_SEPARATOR).any {
+                it == versionString || it.substringAfter(versionString).matches(Regex("\\.\\d+"))
+            }) {
+            when (parts.size) {
+                1 -> return true // no platform specification means any platform
+                2 -> // Check for a matching platform or ANY platform
+                    if (parts[0] == "ANY" || parts[0].split(TARGETS_SEPARATOR).map{ it.uppercase() }.contains(platformName.uppercase()))
+                        return true
+                else -> error("Cannot parse `$maybeTuple`. See KDoc for `TestServices.versionAndTargetAreIgnored()`")
+            }
+        }
+    }
+    return false
 }
