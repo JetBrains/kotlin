25784f0b05b3a6fcb75859447afbddc6dc87f998
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-12-03 13:43:50 +0100

    [JS] Refactor `NameTable` a bit, add KDocs

--- js/js.ast/src/org/jetbrains/kotlin/js/util/NameTable.kt
+++ js/js.ast/src/org/jetbrains/kotlin/js/util/NameTable.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.kotlin.js.util
 
 import org.jetbrains.kotlin.js.common.makeValidES5Identifier
+import org.jetbrains.kotlin.utils.addToStdlib.joinToWithBuffer
 
 abstract class NameScope {
     abstract fun isReserved(name: String): Boolean
@@ -15,23 +16,46 @@ abstract class NameScope {
     }
 }
 
-class NameTable<T>(
-    val parent: NameScope = EmptyScope,
-    val reserved: MutableSet<String> = hashSetOf(),
-) : NameScope() {
-    val names = mutableMapOf<T, String>()
+/**
+ * A map from [T] values to strings with an additional property that all the associated strings are made unique by appending a number
+ * for disambiguation, if needed.
+ */
+class NameTable<T>(private val parent: NameScope = EmptyScope) : NameScope() {
+    private val reserved = hashSetOf<String>()
+    private val names = hashMapOf<T, String>()
 
     private val suggestedNameLastIdx = mutableMapOf<String, Int>()
+
+    operator fun get(name: T): String? = names[name]
+
+    fun dump(renderKey: (T) -> String): String = buildString {
+        appendLine("Names:")
+        names.entries.joinToWithBuffer(this, separator = "\n") { (declaration, name) ->
+            append("--- ")
+            append(renderKey(declaration))
+            append(" => ")
+            append(name)
+        }
+    }
 
     override fun isReserved(name: String): Boolean {
         return parent.isReserved(name) || name in reserved
     }
 
+    /**
+     * Associates [name] with [declaration] as is, without disambiguation.
+     */
     fun declareStableName(declaration: T, name: String) {
         names[declaration] = name
         reserved.add(name)
     }
 
+    /**
+     * Associates a name with [declaration] using [suggestedName] as the basis and optionally appending a number to it, if there already
+     * is such a string in the map.
+     *
+     * @return The associated name.
+     */
     fun declareFreshName(declaration: T, suggestedName: String): String {
         val freshName = findFreshName(makeValidES5Identifier(suggestedName))
         declareStableName(declaration, freshName)
