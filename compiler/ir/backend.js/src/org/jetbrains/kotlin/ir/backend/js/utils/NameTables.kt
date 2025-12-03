a49895301160ee0559e64cfe18dad779cb2ee164
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2025-12-03 13:32:43 +0100

    [JS] Move the `NameTable` util to the `:js:js.ast: module
    …so that we could use it from Analysis API-based TypeScript export

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/NameTables.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/utils/NameTables.kt
@@ -18,60 +18,11 @@ import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.ir.visitors.IrVisitorVoid
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 import org.jetbrains.kotlin.js.common.makeValidES5Identifier
+import org.jetbrains.kotlin.js.util.NameTable
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.utils.addToStdlib.ifNotEmpty
 import java.util.*
 import kotlin.math.abs
-
-abstract class NameScope {
-    abstract fun isReserved(name: String): Boolean
-
-    object EmptyScope : NameScope() {
-        override fun isReserved(name: String): Boolean = false
-    }
-}
-
-class NameTable<T>(
-    val parent: NameScope = EmptyScope,
-    val reserved: MutableSet<String> = hashSetOf(),
-) : NameScope() {
-    val names = mutableMapOf<T, String>()
-
-    private val suggestedNameLastIdx = mutableMapOf<String, Int>()
-
-    override fun isReserved(name: String): Boolean {
-        return parent.isReserved(name) || name in reserved
-    }
-
-    fun declareStableName(declaration: T, name: String) {
-        names[declaration] = name
-        reserved.add(name)
-    }
-
-    fun declareFreshName(declaration: T, suggestedName: String): String {
-        val freshName = findFreshName(makeValidES5Identifier(suggestedName))
-        declareStableName(declaration, freshName)
-        return freshName
-    }
-
-    private fun findFreshName(suggestedName: String): String {
-        if (!isReserved(suggestedName))
-            return suggestedName
-
-        var i = suggestedNameLastIdx[suggestedName] ?: 0
-
-        fun freshName() =
-            suggestedName + "_" + i
-
-        while (isReserved(freshName())) {
-            i++
-        }
-
-        suggestedNameLastIdx[suggestedName] = i
-
-        return freshName()
-    }
-}
 
 fun NameTable<IrDeclaration>.dump(): String =
     "Names: \n" + names.toList().joinToString("\n") { (declaration, name) ->
