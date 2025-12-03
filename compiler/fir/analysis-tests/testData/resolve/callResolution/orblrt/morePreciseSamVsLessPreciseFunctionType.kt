0d95e4e1850bc95f35431e894423d4f8fa4e30a1
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-12-03 18:31:49 +0100

    ~ add some test data
    see com.intellij.platform.backend.observation.TrackingUtil#trackActivity 
    and usages
    
    also com.intellij.platform.backend.documentation.DocumentationResult.Companion.asyncDocumentation
    and usages

---
+++ compiler/fir/analysis-tests/testData/resolve/callResolution/orblrt/morePreciseSamVsLessPreciseFunctionType.kt
@@ -0,0 +1,27 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// FULL_JDK
+import java.util.function.Consumer
+
+interface StringCollector : MutableList<String>
+
+fun interface KConsumer<T> {
+    fun accept(t: T)
+}
+
+fun foo(x: StringCollector.() -> Unit) {}
+fun foo(x: Consumer<MutableList<String>>) {}
+
+fun bar(x: StringCollector.() -> Unit) {}
+fun bar(x: KConsumer<MutableList<String>>) {}
+
+fun baz(x: StringCollector.() -> Unit) {}
+fun baz(x: KConsumer<StringCollector>) {}
+
+fun main() {
+    foo { <!UNRESOLVED_REFERENCE!>add<!>("1") }
+    bar { <!UNRESOLVED_REFERENCE!>add<!>("1") }
+    baz { add("1") }
+}
+
+/* GENERATED_FIR_TAGS: funInterface, functionDeclaration, functionalType, interfaceDeclaration, lambdaLiteral,
+nullableType, samConversion, stringLiteral, typeParameter, typeWithExtension */
