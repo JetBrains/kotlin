32ac8b61ad4f7762206f4193a78058694ebff0eb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-03 10:34:12 +0200

    [Test] Reproduce KT-82841

--- analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedDiagnosticsFe10TestGenerated.java
+++ analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedDiagnosticsFe10TestGenerated.java
@@ -40474,6 +40474,12 @@ public class LLReversedDiagnosticsFe10TestGenerated extends AbstractLLReversedDi
       }
 
       @Test
+      @TestMetadata("nonExhaustiveByNegation.kt")
+      public void testNonExhaustiveByNegation() {
+        runTest("compiler/testData/diagnostics/tests/sealed/nonExhaustiveByNegation.kt");
+      }
+
+      @Test
       @TestMetadata("NonExhaustiveWhen.kt")
       public void testNonExhaustiveWhen() {
         runTest("compiler/testData/diagnostics/tests/sealed/NonExhaustiveWhen.kt");
