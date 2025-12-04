e34d6fc25303f0d3b015232f33f1535df370b057
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2025-12-04 18:10:02 +0100

    ~ wip - refine on anns via fir

--- libraries/examples/scripting/jvm-maven-deps/script/src/org/jetbrains/kotlin/script/examples/jvm/resolve/maven/scriptDef.kt
+++ libraries/examples/scripting/jvm-maven-deps/script/src/org/jetbrains/kotlin/script/examples/jvm/resolve/maven/scriptDef.kt
@@ -33,8 +33,8 @@ object ScriptWithMavenDepsConfiguration : ScriptCompilationConfiguration(
         refineConfiguration {
             @Suppress("DEPRECATION")
             onAnnotations(DependsOn::class, Repository::class, handler = ::configureMavenDepsOnAnnotations)
-            onAst(handler = ::configureMavenDepsOnAst)
-            onFir(handler = ::configureMavenDepsOnFir)
+//            onAst(handler = ::configureMavenDepsOnAst)
+//            onFir(handler = ::configureMavenDepsOnFir)
         }
     }
 ) {
