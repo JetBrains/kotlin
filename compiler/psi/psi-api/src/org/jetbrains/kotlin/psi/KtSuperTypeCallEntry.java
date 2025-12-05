3512fef84ec1544e9882c7d3b7d179d5e276526f
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-12-05 16:06:56 +0100

    [stubs] drop custom KtValueArgumentListElementType
    Since the special logic for annotations was dropped in the previous
    commit, there is no since to explicitly forbid stubs for elements
    inside already supported calls. Effectively, this allows properly
    supporting arguments list access inside `KtSuperTypeCallEntry`
    
    ^KT-82792

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtSuperTypeCallEntry.java
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtSuperTypeCallEntry.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.psi;
@@ -19,7 +8,6 @@ package org.jetbrains.kotlin.psi;
 import com.intellij.lang.ASTNode;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.kotlin.KtNodeTypes;
 import org.jetbrains.kotlin.KtStubBasedElementTypes;
 import org.jetbrains.kotlin.psi.stubs.KotlinPlaceHolderStub;
 
@@ -49,8 +37,9 @@ public class KtSuperTypeCallEntry extends KtSuperTypeListEntry implements KtCall
 
     @Override
     @Nullable
+    @SuppressWarnings("deprecation") // KT-78356
     public KtValueArgumentList getValueArgumentList() {
-        return (KtValueArgumentList) findChildByType(KtNodeTypes.VALUE_ARGUMENT_LIST);
+        return getStubOrPsiChild(KtStubBasedElementTypes.VALUE_ARGUMENT_LIST);
     }
 
     @Override
