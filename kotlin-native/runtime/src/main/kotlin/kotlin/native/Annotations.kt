e70985519e70d0497f7f15946438e3458a9ee5a6
Stefan Haustein <stefan.haustein@gmail.com>
2025-12-05 16:40:22 +0100

    Add swiftName documentation

--- kotlin-native/runtime/src/main/kotlin/kotlin/native/Annotations.kt
+++ kotlin-native/runtime/src/main/kotlin/kotlin/native/Annotations.kt
@@ -120,8 +120,9 @@ public actual annotation class ObjCName(actual val name: String = "", actual val
 /**
  * Instructs the Kotlin compiler to generate a NS_ENUM typedef for the annotated enum class. The name of the generated type will
  * be the name of the enum type with "NSEnum" appended. This name can be overridden with the "name" parameter, which is treated
- * as an exact name. The enum literals will be prefixed with the type name, as they live in a global namespace.
- * Swift naming will automatically remove these disambiguation prefixes. The NSEnum values are accessible via the "nsEnum" property.
+ * as an exact name. Additionally, a separate name for Swift can be specified using the swiftName parameter.
+ * The enum literals will be prefixed with the type name, as they live in a global namespace.
+ * Swift naming will remove these disambiguation prefixes. The NSEnum values are accessible via the "nsEnum" property.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
