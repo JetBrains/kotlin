97fe4b5fb9568a55f74fbed90a41d33e45221e04
Artem Kobzar <artem.kobzar@jetbrains.com>
2025-12-05 18:23:58 +0100

    [K/JS, K/Wasm] Split jsTypes to multiple tests with and without Wasm specific

--- compiler/testData/codegen/boxWasmJsInterop/jsTypes.kt
+++ compiler/testData/codegen/boxWasmJsInterop/jsTypes.kt
@@ -1,15 +1,11 @@
-// IGNORE_BACKEND: JS_IR
-// ^^ In non ES6 Long is not represented as BigInt
+// IGNORE_BACKEND: JS_IR, JS_IR_ES6
+// ^^ There is a frontend diagnostic thrown that it's impossible to infer type of .get after toJsReference
 // WITH_STDLIB
 
 fun assertTrue(x: Boolean): Unit = check(x)
 fun assertNull(x: Any?): Unit = check(x === null)
 
 fun jsRepresentation(x: JsAny): String = js("(typeof x) + ':' + String(x)")
-
-@Suppress("INCOMPATIBLE_TYPES", "IMPOSSIBLE_IS_CHECK_ERROR")
-fun castToKotlinString(jsAny: JsAny?): String? =
-    if (jsAny is String) jsAny as String else null
 
 fun box(): String {
     // JsNumber
@@ -48,7 +44,6 @@ fun box(): String {
     assertTrue(jsBoolTrue == true.toJsBoolean())
     assertTrue(jsBoolTrue != false.toJsBoolean())
     assertTrue(jsRepresentation(jsBoolTrue) == "boolean:true")
-    assertNull(castToKotlinString(jsStr1))
 
 
     // JsArray
