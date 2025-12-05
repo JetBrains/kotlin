970e9be2c9ecbcf0e4ba5cdaa7dca0953c6177e2
Nikita Bobko <nikita.bobko@jetbrains.com>
2025-12-05 19:19:59 +0100

    [Test] 3/3 Annotate kotlin-reflect failing tests with SKIP_NEW_KOTLIN_REFLECT_COMPAT_CHECK
    In scope of: OSIP-51

--- compiler/testData/ir/irText/firProblems/TypeParameterInClashingAccessor.kt
+++ compiler/testData/ir/irText/firProblems/TypeParameterInClashingAccessor.kt
@@ -2,6 +2,9 @@
 // IGNORE_BACKEND_K1: JVM_IR
 // ISSUE: KT-59038
 
+// KT-81029
+// SKIP_NEW_KOTLIN_REFLECT_COMPATIBILITY_CHECK
+
 // FILE: C.java
 abstract class C extends B {
     protected C(A a) {
--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/backend/handlers/JvmNewKotlinReflectCompatibilityCheck.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/backend/handlers/JvmNewKotlinReflectCompatibilityCheck.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.test.backend.handlers
 
 import org.jetbrains.kotlin.test.TestJdkKind
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.KOTLIN_REFLECT_DUMP_MISMATCH
+import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.SKIP_NEW_KOTLIN_REFLECT_COMPATIBILITY_CHECK
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.DISABLE_JAVA_FACADE
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
 import org.jetbrains.kotlin.test.directives.model.singleOrZeroValue
@@ -38,6 +39,7 @@ class JvmNewKotlinReflectCompatibilityCheck(testServices: TestServices) : JvmBin
     override fun processModule(module: TestModule, info: BinaryArtifacts.Jvm) {
         // Running the test is impossible if there are errors in Java code
         if (DISABLE_JAVA_FACADE in module.directives) return
+        if (SKIP_NEW_KOTLIN_REFLECT_COMPATIBILITY_CHECK in module.directives) return
         when (module.directives.singleOrZeroValue(JvmEnvironmentConfigurationDirectives.JDK_KIND)) {
             TestJdkKind.MOCK_JDK, TestJdkKind.MODIFIED_MOCK_JDK, TestJdkKind.FULL_JDK, null -> {}
             // Classes for newer JDK can't be loaded into the current old Java runtime (Java 8)
