9f79397da167f62c08c7909abcd0b0e081ec0b37
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2025-12-05 18:00:58 +0100

    fixup! [K/JVM] Copy all attributes in JvmUpgradeCallableReferences

--- plugins/atomicfu/atomicfu-compiler/src/org/jetbrains/kotlinx/atomicfu/compiler/backend/native/NativeAtomicfuIrBuilder.kt
+++ plugins/atomicfu/atomicfu-compiler/src/org/jetbrains/kotlinx/atomicfu/compiler/backend/native/NativeAtomicfuIrBuilder.kt
@@ -133,7 +133,7 @@ class NativeAtomicfuIrBuilder(
             else -> error("kotlin.native.internal/getAndAddField intrinsic is not supported for values of type ${valueType.dumpKotlinLike()}")
         }
 
-    private fun addAndGetField(propertyRef: IrExpression, valueType: IrType, delta: IrConst?): IrCall =
+    private fun addAndGetField(propertyRef: IrExpression, valueType: IrType, delta: IrExpression?): IrCall =
         getAndAddField(propertyRef, valueType, delta).plus(delta?.deepCopyWithoutPatchingParents())
 
     private fun getAndIncrementField(propertyRef: IrExpression, valueType: IrType): IrCall {
