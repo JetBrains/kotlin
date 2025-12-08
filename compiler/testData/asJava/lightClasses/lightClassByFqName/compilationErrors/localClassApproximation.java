a8616130dfc7906379451d567e9015154f9f56fa
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-08 13:20:51 +0200

    ~~ AA tests ~~

--- compiler/testData/asJava/lightClasses/lightClassByFqName/compilationErrors/localClassApproximation.java
+++ compiler/testData/asJava/lightClasses/lightClassByFqName/compilationErrors/localClassApproximation.java
@@ -1,15 +1,15 @@
 public final class B /* p.B*/ {
   @org.jetbrains.annotations.NotNull()
-  private final @org.jetbrains.annotations.NotNull() p.Self<A> x;
+  private final @org.jetbrains.annotations.NotNull() p.Self<?> x;
 
   @org.jetbrains.annotations.NotNull()
-  private final @org.jetbrains.annotations.NotNull() p.Self<A> y;
+  private final @org.jetbrains.annotations.NotNull() p.Self<?> y;
 
   @org.jetbrains.annotations.NotNull()
-  public final @org.jetbrains.annotations.NotNull() p.Self<? extends p.Self<?>> getY();//  getY()
+  public final @org.jetbrains.annotations.NotNull() p.Self<?> getX();//  getX()
 
   @org.jetbrains.annotations.NotNull()
-  public final @org.jetbrains.annotations.NotNull() p.Self<A> getX();//  getX()
+  public final @org.jetbrains.annotations.NotNull() p.Self<?> getY();//  getY()
 
   public  B();//  .ctor()
 }
