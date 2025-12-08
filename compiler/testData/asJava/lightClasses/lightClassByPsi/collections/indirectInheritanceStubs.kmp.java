95765acfbe11369eb78c62981df6bf96cca5c860
Alexey Belkov <alexey.belkov@jetbrains.com>
2025-12-08 17:09:34 +0400

    fixup! [SLC] Support Kotlin collection classes mapped to Java classes
    Rework applicability check of stub generation

--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/indirectInheritanceStubs.kmp.java
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/indirectInheritanceStubs.kmp.java
@@ -6,5 +6,44 @@ public abstract class CCollection2 /* test.CCollection2*/<Elem>  extends test.CC
   public  CCollection2();//  .ctor()
 }
 
+public abstract class CCollection3 /* test.CCollection3*/ extends test.Foo implements java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>, kotlin.jvm.internal.markers.KMappedMarker {
+  public  CCollection3();//  .ctor()
+}
+
+public abstract class CCollection4 /* test.CCollection4*/ extends test.Foo implements test.ICollection<@org.jetbrains.annotations.NotNull() java.lang.String> {
+  public  CCollection4();//  .ctor()
+}
+
+public abstract class CCollection5 /* test.CCollection5*/ extends test.CCollection4 {
+  public  CCollection5();//  .ctor()
+}
+
+public final class CCollection6 /* test.CCollection6*/ implements java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>, kotlin.jvm.internal.markers.KMappedMarker {
+  @org.jetbrains.annotations.NotNull()
+  public static final @org.jetbrains.annotations.NotNull() test.CCollection6 INSTANCE;
+
+  @java.lang.Override()
+  @org.jetbrains.annotations.NotNull()
+  public @org.jetbrains.annotations.NotNull() java.util.Iterator<@org.jetbrains.annotations.NotNull() java.lang.String> iterator();//  iterator()
+
+  @java.lang.Override()
+  public boolean contains(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String);//  contains(@org.jetbrains.annotations.NotNull() java.lang.String)
+
+  @java.lang.Override()
+  public boolean containsAll(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>);//  containsAll(@org.jetbrains.annotations.NotNull() java.util.Collection<@org.jetbrains.annotations.NotNull() java.lang.String>)
+
+  @java.lang.Override()
+  public boolean isEmpty();//  isEmpty()
+
+  @java.lang.Override()
+  public int getSize();//  getSize()
+
+  private  CCollection6();//  .ctor()
+}
+
+public abstract class Foo /* test.Foo*/ {
+  public  Foo();//  .ctor()
+}
+
 public abstract interface ICollection /* test.ICollection*/<Elem>  extends java.util.Collection<Elem>, kotlin.jvm.internal.markers.KMappedMarker {
 }
--- compiler/testData/asJava/lightClasses/lightClassByPsi/collections/indirectInheritanceStubs.kt
+++ compiler/testData/asJava/lightClasses/lightClassByPsi/collections/indirectInheritanceStubs.kt
@@ -6,4 +6,34 @@ interface ICollection<Elem> : Collection<Elem>
 abstract class CCollection<Elem> : ICollection<Elem>
 
 abstract class CCollection2<Elem> : CCollection<Elem>()
-// LIGHT_ELEMENTS_NO_DECLARATION: CCollection.class[add;addAll;clear;getSize;iterator;remove;removeAll;removeIf;retainAll;size;toArray;toArray]
+
+abstract class Foo
+
+abstract class CCollection3 : Foo(), Collection<String>
+
+abstract class CCollection4 : Foo(), ICollection<String>
+
+abstract class CCollection5 : CCollection4()
+
+object CCollection6 : Collection<String> {
+    override fun contains(element: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsAll(elements: Collection<String>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): Iterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override val size: Int
+        get() = TODO("Not yet implemented")
+}
+
+// LIGHT_ELEMENTS_NO_DECLARATION: CCollection.class[add;addAll;clear;getSize;iterator;remove;removeAll;removeIf;retainAll;size;toArray;toArray], CCollection3.class[add;addAll;clear;contains;contains;getSize;iterator;remove;removeAll;removeIf;retainAll;size;toArray;toArray], CCollection4.class[add;addAll;clear;contains;contains;getSize;iterator;remove;removeAll;removeIf;retainAll;size;toArray;toArray], CCollection6.class[add;addAll;clear;remove;removeAll;removeIf;retainAll;size;toArray;toArray]
