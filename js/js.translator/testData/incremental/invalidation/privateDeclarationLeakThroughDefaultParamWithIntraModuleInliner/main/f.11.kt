1316ddaec54d61ab9594ad20b0cb13c23a6fb837
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2025-12-08 11:19:43 +0100

    [Tests][IC] Drop steps failing due to suppressed private-type exposure check
    This test was fully muted on both WASM and JS because several steps were
    failing. The failures are unrelated to incremental compilation; they
    happen because the
    `ForbidExposureOfPrivateTypesInNonPrivateInlineFunctionsInKlibs` feature
    is suppressed, which causes linkage errors when `box` in `m.kt`accesses
    private symbols from other files.
    
    KT-80696 Fixed

--- js/js.translator/testData/incremental/invalidation/privateDeclarationLeakThroughDefaultParamWithIntraModuleInliner/main/f.11.kt
+++
@@ -1,9 +0,0 @@
-private object Obj {
-    val x = "11"
-}
-
-private inline fun bar() = Obj.x
-
-internal inline fun foo(lambda: () -> String = ::bar): String {
-    return lambda()
-}
--- js/js.translator/testData/incremental/invalidation/privateDeclarationLeakThroughDefaultParamWithIntraModuleInliner/main/f.13.kt
+++
@@ -1,9 +0,0 @@
-private object Obj {
-    inline val x get() = "13"
-}
-
-private inline fun bar() = Obj.x
-
-internal inline fun foo(lambda: () -> String = ::bar): String {
-    return lambda()
-}
