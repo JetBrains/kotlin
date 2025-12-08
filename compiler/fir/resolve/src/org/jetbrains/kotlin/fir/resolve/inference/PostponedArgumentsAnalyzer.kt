0d82b9748babe86c1a02b145ba89103f1045c6d3
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-12-08 14:37:43 +0100

    ~ generic Unit

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/PostponedArgumentsAnalyzer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/inference/PostponedArgumentsAnalyzer.kt
@@ -279,7 +279,7 @@ class PostponedArgumentsAnalyzer(
             c.canBeProper(lambdaReturnType) -> substitute(lambdaReturnType)
 
             // For Unit-coercion
-            !lambdaReturnType.isMarkedNullable && c.hasUpperOrEqualUnitConstraint(lambdaReturnType) -> unitType
+            lambdaReturnType.willEventuallyBecomeUnit(c) -> unitType
 
             // Supplying the expected type for lambda effectively makes it being resolved in the FULL completion mode.
             // For non-PCLA lambdas using expected types with non-fixed type variables would lead to illegal state: calls inside return
@@ -319,6 +319,8 @@ class PostponedArgumentsAnalyzer(
         return results
     }
 
+    private fun ConeKotlinType.willEventuallyBecomeUnit(c: PostponedArgumentsAnalyzerContext): Boolean =
+        !isMarkedNullable && c.hasUpperOrEqualUnitConstraint(this)
 
     fun applyResultsOfAnalyzedLambdaToCandidateSystem(
         c: PostponedArgumentsAnalyzerContext,
@@ -348,7 +350,8 @@ class PostponedArgumentsAnalyzer(
         }
         val isExpectedReturnTypeUnit = returnTypeRef.coneType.isUnitOrFlexibleUnit
         val isLastExpressionCoercedToUnit =
-            isExpectedReturnTypeUnit || lambda.anonymousFunction.lambdaWithExplicitEmptyReturns(returnArguments)
+            isExpectedReturnTypeUnit || returnTypeRef.coneType.willEventuallyBecomeUnit(c)
+                    || lambda.anonymousFunction.lambdaWithExplicitEmptyReturns(returnArguments)
 
         for (atom in returnAtoms) {
             val expression = atom.expression
