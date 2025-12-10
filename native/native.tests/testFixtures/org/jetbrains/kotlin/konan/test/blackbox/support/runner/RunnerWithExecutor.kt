d1ae2bb4535c340ecaa9cb918108649c990ff668
Aleksei.Glushko <aleksei.glushko@jetbrains.com>
2025-12-10 14:21:23 +0100

    [K/N] Detect minidumps in workdir
        In case of a timeout termination, the test executor can miss the
        notification that a minidump was written because it closes the stderr
        stream prematurely.
        If this is the case, try to identify a single minidump file in the
        current working directory instead of relying only on stderr output.

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/runner/RunnerWithExecutor.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/runner/RunnerWithExecutor.kt
@@ -61,10 +61,17 @@ internal open class RunnerWithExecutor(
         val response = executor.execute(request)
 
         if (response.exitCode != 0) {
-            val stderrString = stderr.toString("UTF-8")
-            val regex = "^Minidump written to \"(.*)\"$".toRegex(RegexOption.MULTILINE)
-            val matchResult = regex.find(stderrString)
-            val minidumpFileName = matchResult?.groups?.get(1)?.value
+            fun findMinidumpByStderr(): String? {
+                val stderrString = stderr.toString("UTF-8")
+                val regex = "^Minidump written to \"(.*)\"$".toRegex(RegexOption.MULTILINE)
+                val matchResult = regex.find(stderrString)
+                return matchResult?.groups?.get(1)?.value
+            }
+
+            fun findMinidumpInWorkingDirectory(): String? =
+                workingDirectory.listFiles { it.extension == "dmp" }?.singleOrNull()?.path
+
+            val minidumpFileName = findMinidumpByStderr() ?: findMinidumpInWorkingDirectory()
             if (minidumpFileName != null) {
                 val minidumpAnalyzerStdout = ByteArrayOutputStream()
                 val minidumpAnalyzerStderr = ByteArrayOutputStream()
