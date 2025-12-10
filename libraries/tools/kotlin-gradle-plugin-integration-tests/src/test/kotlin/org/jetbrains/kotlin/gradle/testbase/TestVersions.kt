ffbcf3f90fd5f10c3d92b4e86c3d3217fb5fbce9
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2025-12-10 16:50:33 +0100

    [Gradle] Bump stable kotlin for IT
    ^KT-78763

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -45,7 +45,7 @@ interface TestVersions {
     }
 
     object Kotlin {
-        const val STABLE_RELEASE = "2.2.10"
+        const val STABLE_RELEASE = "2.2.21"
 
         // Copied from KOTLIN_VERSION.kt file
         val CURRENT
