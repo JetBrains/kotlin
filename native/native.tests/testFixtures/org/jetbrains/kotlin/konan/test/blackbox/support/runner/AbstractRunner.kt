f653694ed9a81f4f43a5a8db4a15ebe0d25213a6
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-12-10 19:18:07 +0100

    [K\N][Tests] Preserve expected/actual data in assertation errors
    The goal is to throw a proper AssertationError that can be picked up
    by a host and presented nicely, primary to enable our "Run test and
    apply diffs" action in native tests the same way as in others.

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/runner/AbstractRunner.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/runner/AbstractRunner.kt
@@ -6,7 +6,8 @@
 package org.jetbrains.kotlin.konan.test.blackbox.support.runner
 
 import org.jetbrains.kotlin.konan.test.blackbox.support.LoggedData
-import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertTrue
+import org.opentest4j.AssertionFailedError
+import org.opentest4j.MultipleFailuresError
 import org.opentest4j.TestAbortedException
 
 internal abstract class AbstractRunner<R> : Runner<R> {
@@ -39,7 +40,22 @@ internal abstract class AbstractResultHandler<R>(protected val runResult: RunRes
     abstract fun getLoggedRun(): LoggedData
     abstract fun handle(): R
 
-    protected inline fun verifyExpectation(shouldBeTrue: Boolean, crossinline errorMessage: () -> String) {
-        assertTrue(shouldBeTrue) { getLoggedRun().withErrorMessage(errorMessage()) }
+    protected fun verifyExpectation(shouldBeTrue: Boolean, failedResults: List<TestRunCheck.Result.Failed>, errorMessage: () -> String) {
+        if (!shouldBeTrue) {
+            val message = getLoggedRun().withErrorMessage(errorMessage())
+            if (failedResults.size < 2) {
+                val missmatch = failedResults.singleOrNull() as? TestRunCheck.Result.FailedWithMissmatch
+                if (missmatch != null) {
+                    throw AssertionFailedError(message, missmatch.expected, missmatch.actual)
+                } else {
+                    throw AssertionError(message)
+                }
+            } else {
+                val subErrors = failedResults
+                    .filterIsInstance<TestRunCheck.Result.FailedWithMissmatch>()
+                    .map { AssertionFailedError("", it.expected, it.actual) }
+                throw MultipleFailuresError(message, subErrors)
+            }
+        }
     }
 }
