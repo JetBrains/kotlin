c79fb0e90f5eee49ac64f7406992fc2d7b387a50
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-12-10 21:26:20 +0100

    [Tests] Move UnsupportedFeaturesTestConfigurator to tests-common-new
    ^KT-82469

--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/services/configuration/UnsupportedFeaturesTestConfigurator.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/services/configuration/UnsupportedFeaturesTestConfigurator.kt
@@ -3,7 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.js.test.services.configuration
+package org.jetbrains.kotlin.test.services.configuration
 
 import org.jetbrains.kotlin.config.LanguageFeature
 import org.jetbrains.kotlin.config.LanguageVersion
