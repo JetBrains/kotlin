e86239cb6f0c0e04fa56c8e68233a176ffa75faf
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-12-11 00:08:49 +0100

    [Tests][Native] Implement klib compatibility tests with common codegen tests
    ^KT-82469
    ^KT-82471

---
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/handlers/NativeRunner.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.konan.test.handlers
+
+import org.jetbrains.kotlin.native.executors.ExecuteRequest
+import org.jetbrains.kotlin.native.executors.HostExecutor
+import org.jetbrains.kotlin.test.backend.handlers.NativeBinaryArtifactHandler
+import org.jetbrains.kotlin.test.model.BinaryArtifacts
+import org.jetbrains.kotlin.test.model.TestModule
+import org.jetbrains.kotlin.test.services.ModuleStructureExtractor
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.configuration.NativeEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.moduleStructure
+import java.io.ByteArrayOutputStream
+
+class NativeRunner(testServices: TestServices) : NativeBinaryArtifactHandler(testServices) {
+    private var artifact: BinaryArtifacts.Native? = null
+    override fun processModule(module: TestModule, info: BinaryArtifacts.Native) {
+        if (NativeEnvironmentConfigurator.isMainModule(module, testServices.moduleStructure))
+            artifact = info
+    }
+
+    override fun processAfterAllModules(someAssertionWasFailed: Boolean) {
+        val stdout = ByteArrayOutputStream()
+        val stderr = ByteArrayOutputStream()
+        val executable = artifact?.executable
+        val executableAbsolutePath = executable?.absolutePath ?: error("Module ${ModuleStructureExtractor.DEFAULT_MODULE_NAME} is expected to run")
+        val request = ExecuteRequest(executableAbsolutePath).apply {
+            this.workingDirectory = executable.parentFile
+            this.stdout = stdout
+            this.stderr = stderr
+        }
+        val response = HostExecutor().execute(request)
+
+        val stdoutStr = stdout.toString()
+        if (response.exitCode != 0) {
+            throw AssertionError("Native executable failed with exit code ${response.exitCode}:\nstdout: $stdoutStr\nstderr: $stderr")
+        }
+        if (!stdoutStr.contains("[  PASSED  ] 1 tests."))
+            throw AssertionError("Run failed. stdout: $stdoutStr\nstderr: $stderr")
+    }
+}
