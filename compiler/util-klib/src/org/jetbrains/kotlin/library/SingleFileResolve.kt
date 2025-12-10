0c0916bf081b96682116ba860bd5719196d94e6f
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-12-10 22:45:34 +0100

    Klib API: Drop or deprecate obsolete/unused API
    Drop:
    - org.jetbrains.kotlin.library.ToolingSingleFileKlibResolveStrategy
    
    Deprecate as HIDDEN:
    - org.jetbrains.kotlin.library.SingleFileKlibResolveStrategy
    
    ^KT-61096
    ^KT-82882

--- compiler/util-klib/src/org/jetbrains/kotlin/library/SingleFileResolve.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/SingleFileResolve.kt
@@ -11,6 +11,10 @@ import org.jetbrains.kotlin.library.loader.reportLoadingProblemsIfAny
 import org.jetbrains.kotlin.util.DummyLogger
 import org.jetbrains.kotlin.util.Logger
 
+@Deprecated(
+    "Preserved for binary compatibility with existing versions of the kotlinx-benchmarks Gradle plugin. See KT-82882.",
+    level = DeprecationLevel.HIDDEN
+)
 fun interface SingleFileKlibResolveStrategy {
     fun resolve(libraryFile: File, logger: Logger): KotlinLibrary
 }
@@ -22,13 +26,16 @@ fun interface SingleFileKlibResolveStrategy {
 fun resolveSingleFileKlib(
     libraryFile: File,
     logger: Logger = DummyLogger,
-    strategy: SingleFileKlibResolveStrategy = SingleFileKlibResolveStrategy { _, _ ->
-        val klibLoadingResult = KlibLoader { libraryPaths(libraryFile.path) }.load()
-        klibLoadingResult.reportLoadingProblemsIfAny { _, message ->
-            // N.B. A call to the @Deprecated Logger.fatal() function is intentional here.
-            // It's necessary to replicate the behavior that existed before.
-            @Suppress("DEPRECATION") logger.fatal(message)
+    @Suppress("DEPRECATION", "DEPRECATION_ERROR") strategy: SingleFileKlibResolveStrategy = object : SingleFileKlibResolveStrategy {
+        override fun resolve(libraryFile: File, logger: Logger): KotlinLibrary {
+            val klibLoadingResult = KlibLoader { libraryPaths(libraryFile.path) }.load()
+            klibLoadingResult.reportLoadingProblemsIfAny { _, message ->
+                // N.B. A call to the @Deprecated Logger.fatal() function is intentional here.
+                // It's necessary to replicate the behavior that existed before.
+                @Suppress("DEPRECATION") logger.fatal(message)
+            }
+            return klibLoadingResult.librariesStdlibFirst.single()
         }
-        klibLoadingResult.librariesStdlibFirst.single()
-    }
-): KotlinLibrary = strategy.resolve(libraryFile, logger)
+    },
+
+    ): KotlinLibrary = strategy.resolve(libraryFile, logger)
