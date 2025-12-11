7f6caf5f03abfd732b3d9455f7143624e1b03af5
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2025-12-11 16:14:54 +0100

    Develocity 4.1.1

--- repo/gradle-settings-conventions/settings.gradle.kts
+++ repo/gradle-settings-conventions/settings.gradle.kts
@@ -19,7 +19,7 @@ buildscript {
 plugins {
     // Versions here should be also synced with the versions in 'libs.versions.toml'
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("com.gradle.develocity") version("4.0.3")
+    id("com.gradle.develocity") version("4.1.1")
 }
 
 dependencyResolutionManagement {
