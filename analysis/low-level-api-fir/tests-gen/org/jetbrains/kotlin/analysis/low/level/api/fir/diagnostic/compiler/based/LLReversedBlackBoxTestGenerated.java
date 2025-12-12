eac5c65d1f3f8da1aea4ba48b00d7e76e430b6ef
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-12 14:22:51 +0200

    [LV Update] Update JS tests
    ^KT-81590

--- analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
+++ analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
@@ -28530,9 +28530,9 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
     }
 
     @Test
-    @TestMetadata("inlineFunctionOverridesAbstractMethodWithDefaultValue.kt")
-    public void testInlineFunctionOverridesAbstractMethodWithDefaultValue() {
-      runTest("compiler/testData/codegen/box/inline/inlineFunctionOverridesAbstractMethodWithDefaultValue.kt");
+    @TestMetadata("inlineFunctionOverridesAbstractMethodWithDefaultValueWithIrInlinerBeforeKlibSerializationDisabled.kt")
+    public void testInlineFunctionOverridesAbstractMethodWithDefaultValueWithIrInlinerBeforeKlibSerializationDisabled() {
+      runTest("compiler/testData/codegen/box/inline/inlineFunctionOverridesAbstractMethodWithDefaultValueWithIrInlinerBeforeKlibSerializationDisabled.kt");
     }
 
     @Test
@@ -39139,12 +39139,6 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
     @TestMetadata("lambdaWithDynamicReceiver.kt")
     public void testLambdaWithDynamicReceiver() {
       runTest("compiler/testData/codegen/box/js/lambdaWithDynamicReceiver.kt");
-    }
-
-    @Test
-    @TestMetadata("lambdaWithoutNonLocalControlflow.kt")
-    public void testLambdaWithoutNonLocalControlflow() {
-      runTest("compiler/testData/codegen/box/js/lambdaWithoutNonLocalControlflow.kt");
     }
 
     @Test
