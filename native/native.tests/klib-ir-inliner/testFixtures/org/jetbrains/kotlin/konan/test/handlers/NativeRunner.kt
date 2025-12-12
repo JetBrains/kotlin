147a6801703906d12dfeddeeef625f95986b7b1b
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2025-12-12 11:37:29 +0100

    [Tests][Native] Simplify NativeRunner
    ^KT-82469
    ^KT-82471

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/handlers/NativeRunner.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/handlers/NativeRunner.kt
@@ -5,41 +5,38 @@
 
 package org.jetbrains.kotlin.konan.test.handlers
 
-import org.jetbrains.kotlin.native.executors.ExecuteRequest
-import org.jetbrains.kotlin.native.executors.HostExecutor
+import org.jetbrains.kotlin.native.executors.RunProcessException
+import org.jetbrains.kotlin.native.executors.runProcess
 import org.jetbrains.kotlin.test.backend.handlers.NativeBinaryArtifactHandler
 import org.jetbrains.kotlin.test.model.BinaryArtifacts
 import org.jetbrains.kotlin.test.model.TestModule
-import org.jetbrains.kotlin.test.services.ModuleStructureExtractor
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.configuration.NativeEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.moduleStructure
-import java.io.ByteArrayOutputStream
+import kotlin.time.Duration.Companion.seconds
 
 class NativeRunner(testServices: TestServices) : NativeBinaryArtifactHandler(testServices) {
     private var artifact: BinaryArtifacts.Native? = null
     override fun processModule(module: TestModule, info: BinaryArtifacts.Native) {
-        if (NativeEnvironmentConfigurator.isMainModule(module, testServices.moduleStructure))
+        if (NativeEnvironmentConfigurator.isMainModule(module, testServices.moduleStructure)) {
+            if (artifact != null) error("Internal error: more that one main module in the test: $artifact and $info")
             artifact = info
+        }
     }
 
     override fun processAfterAllModules(someAssertionWasFailed: Boolean) {
-        val stdout = ByteArrayOutputStream()
-        val stderr = ByteArrayOutputStream()
         val executable = artifact?.executable
-        val executableAbsolutePath = executable?.absolutePath ?: error("Module ${ModuleStructureExtractor.DEFAULT_MODULE_NAME} is expected to run")
-        val request = ExecuteRequest(executableAbsolutePath).apply {
-            this.workingDirectory = executable.parentFile
-            this.stdout = stdout
-            this.stderr = stderr
+        val executableAbsolutePath = executable?.absolutePath ?: error("One main module is expected to be in the test.")
+        val result = try {
+            runProcess(executableAbsolutePath) {
+                this.workingDirectory = executable.parentFile
+                this.timeout = 10.seconds
+            }
+        } catch (e: RunProcessException) {
+            throw AssertionError("Native executable failed with exit code ${e.exitCode}:\nstdout: ${e.stdout}\nstderr: ${e.stderr}")
         }
-        val response = HostExecutor().execute(request)
 
-        val stdoutStr = stdout.toString()
-        if (response.exitCode != 0) {
-            throw AssertionError("Native executable failed with exit code ${response.exitCode}:\nstdout: $stdoutStr\nstderr: $stderr")
-        }
-        if (!stdoutStr.contains("[  PASSED  ] 1 tests."))
-            throw AssertionError("Run failed. stdout: $stdoutStr\nstderr: $stderr")
+        if (!result.stdout.contains("[  PASSED  ] 1 tests."))
+            throw AssertionError("Run failed, stdout and stderr: ${result.output}")
     }
 }
