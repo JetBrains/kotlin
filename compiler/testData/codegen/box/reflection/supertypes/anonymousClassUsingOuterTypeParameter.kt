484c869461e85ee5739fbb28922f6710ac0e5212
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-12-15 17:24:05 +0100

    Reflection: do not fail on error type parameters in new implementation
    When a local class uses a type parameter declared in the enclosing
    method, kotlin-reflect fails to load that type parameter. The old
    implementation (before KT-76521) worked incorrectly too, however it did
    not throw exception, but created a "error type" instead, which is wrong
    in many ways, but at least it didn't fail.
    
    The correct fix of the underlying issue (KT-47030) would be to collect
    type parameters used in the class signature from enclosing methods and
    their containing classes, but right now the goal is to make behavior as
    close to the old (K1-based) reflection implementation as possible, so we
    also create an error type here.
    
    RCA: see above.
    
     #KT-83361 Fixed

---
+++ compiler/testData/codegen/box/reflection/supertypes/anonymousClassUsingOuterTypeParameter.kt
@@ -0,0 +1,30 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+package test
+
+import kotlin.test.assertEquals
+
+interface I<A, B>
+
+fun <S, T> f() = object : I<T, S> {}
+
+fun box(): String {
+    // Ideally, we would have `test.I<T, S>`, where `T` and `S` are types obtained from f's type parameters.
+    // But currently it's not implemented: KT-47030.
+    // At the moment, this test checks that kotlin-reflect at least does not throw exception on such types.
+    assertEquals("[test.I<???, ???>, kotlin.Any]", f<Any, Any>()::class.supertypes.toString())
+
+    val i = f<Any, Any>()::class.supertypes.single { it.classifier == I::class }
+    val arg = i.arguments.first().type!!
+    if (System.getProperty("kotlin.reflect.jvm.useK1Implementation")?.toBoolean() == true) {
+        // In the descriptor-based implementation, classifier for error type is null.
+        assertEquals(null, arg.classifier)
+    } else {
+        // In the new implementation, classifier is a special object which is neither a class, nor a type parameter, nor a type alias,
+        // and its `toString` returns something meaningful (although it's still not a requirement and it could've returned `???` as well,
+        // the main idea is that it should not throw exception).
+        assertEquals("[Error type parameter 0]", arg.classifier!!.toString())
+    }
+
+    return "OK"
+}
