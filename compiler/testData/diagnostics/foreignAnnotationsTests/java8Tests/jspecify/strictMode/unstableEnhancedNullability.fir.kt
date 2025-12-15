90be73985fe5c0a469f667c309054f123a100ae8
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-12-15 14:20:50 +0100

    K2: reproduce KT-83031
    (cherry picked from commit 98a8301f1402c173be2160ec379aa3bf42d826e6)

---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/jspecify/strictMode/unstableEnhancedNullability.fir.kt
@@ -0,0 +1,32 @@
+// JSPECIFY_STATE: strict
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-83031
+// FIR_DUMP
+
+// FILE: P.java
+import org.jspecify.annotations.*;
+
+public class P {
+    public static @NonNull String @NonNull [] f() {
+        return new String[0];
+    }
+}
+
+// FILE: C.kt
+class C {
+    fun g() {
+        r() <!USELESS_CAST!>as String<!>
+        for (p in P.f()) {
+            // Should be @NonNull String
+            <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.String")!>p<!>.length     // <------ null check or not?
+        }
+    }
+
+    fun r(): String = ""
+}
+
+// FILE: B.kt
+class B {
+    val c = arrayOf("")
+    val t = c.size
+}
