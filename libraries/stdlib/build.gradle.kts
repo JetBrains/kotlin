a9a7c0a0a3364fedc1c2ab38e7dc39eb7bec29a9
Leonid Startsev <leonid.startsev@jetbrains.com>
2025-12-18 12:17:29 +0100

    [Stdlib] Enable RVC for Jdk8 source set.
    Add @IgnorableReturnValue where appropriate
    
    #KT-80786 Fixed

--- libraries/stdlib/build.gradle.kts
+++ libraries/stdlib/build.gradle.kts
@@ -202,6 +202,7 @@ kotlin {
                             )
                         )
                         mainCompilationOptions()
+                        addReturnValueCheckerInfo()
                     }
                 }
             }
