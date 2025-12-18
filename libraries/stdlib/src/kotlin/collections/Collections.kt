7d404bbe34edc1284b0257533b3756e919de33d5
Natasha Murashkina <murfel@users.noreply.github.com>
2025-12-18 13:26:25 +0000

    add my own suggestions

--- libraries/stdlib/src/kotlin/collections/Collections.kt
+++ libraries/stdlib/src/kotlin/collections/Collections.kt
@@ -314,16 +314,16 @@ internal fun <T> List<T>.optimizeReadOnlyList() = when (size) {
 
 /**
  * Searches this list or its range for the provided [element] using the binary search algorithm.
- * The list is expected to be sorted into ascending order according to the Comparable natural ordering of its elements,
- * otherwise the result is undefined.
+ * The list is expected to be sorted in a non-descending order according to the Comparable natural ordering of its elements,
+ * otherwise the result is unspecified.
  *
- * If the list contains multiple elements equal to the specified [element], there is no guarantee which one will be found.
+ * If the list contains multiple elements equal to [element], there is no guarantee which one will be found.
  *
  * `null` value is considered to be less than any non-null value.
  *
  * @return the index of the element, if it is contained in the list within the specified range;
- * otherwise, the inverted insertion point `(-insertion point - 1)`.
- * The insertion point is defined as the index at which the element should be inserted,
+ * otherwise, the inverted insertion index `(-insertion index - 1)`.
+ * The insertion index is defined as the index at which the element should be inserted,
  * so that the list (or the specified subrange of the list) still remains sorted.
  * Thus, if the returned index is `>= 0`, then the value is present;
  * otherwise the value could be inserted at `index.inv()`
