4d4838f3838c6c3886702a8665eb78e3ee2055f0
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-12-18 20:10:06 +0100

    [K\N][Tests] Extract an abstract class from LLDBSessionSpec
    In preparation for a stepping test implementation in the next commit.
    
    KT-78937

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/util/LLDBSessionSpec.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/util/LLDBSessionSpec.kt
@@ -11,60 +11,60 @@ import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertTrue
 import org.jetbrains.kotlin.test.services.JUnit5Assertions.fail
 import java.io.File
 
-private class Step(val command: String, val body: List<String>) {
-    companion object {
-        fun parse(block: String, expectedPrefix: String): Step {
-            val lines = block.lines()
-
-            val commandWithPrefix = lines.first()
-            assertTrue(commandWithPrefix.startsWith(expectedPrefix)) {
-                "The command should start with $expectedPrefix. Got: $commandWithPrefix"
-            }
-
-            val command = commandWithPrefix.removePrefix(expectedPrefix).trimStart()
-            val body = lines.drop(1).filterNot(String::isBlank)
-
-            return Step(command, body)
-        }
-    }
-}
-
-internal class LLDBSessionSpec private constructor(private val expectedSteps: List<Step>) {
-    fun generateCLIArguments(prettyPrinters: File): List<String> = buildList {
+abstract class LLDBSessionSpec {
+    open fun generateCLIArguments(prettyPrinters: File): List<String> = buildList {
+        // Don't pickup ~/.lldbinit
+        this += "--no-lldbinit"
         this += "-b"
         this += "-o"
         this += "command script import ${prettyPrinters.absolutePath}"
+    }
+
+    abstract fun checkLLDBOutput(output: String, nativeTargets: KotlinNativeTargets): Boolean
+
+    protected fun sanityCheckLLDBOutput(output: String) {
+        assertFalse(PYTHON_EXCEPTION_HEADER in output) {
+            "Unhandled python exception in debugger: ${output.substring(output.indexOf(PYTHON_EXCEPTION_HEADER))}"
+        }
+    }
+
+    companion object {
+        private const val PYTHON_EXCEPTION_HEADER = "Traceback (most recent call last):"
+    }
+}
+
+/**
+ * Executes a set of debugger commands defined per-test, compares raw LLDB output with the golden data.
+ */
+internal class ReplLLDBSessionSpec private constructor(private val expectedSteps: List<Step>) : LLDBSessionSpec() {
+    override fun generateCLIArguments(prettyPrinters: File): List<String> = buildList {
+        addAll(super.generateCLIArguments(prettyPrinters))
         expectedSteps.forEach { step ->
             this += "-o"
             this += step.command
         }
-        // Don't pickup ~/.lldbinit
-        this += "--no-lldbinit"
     }
 
-    fun checkLLDBOutput(output: String, nativeTargets: KotlinNativeTargets): Boolean {
-        assertFalse(PYTHON_EXCEPTION_HEADER in output) {
-            "Unhandled python exception in debugger: ${output.substring(output.indexOf(PYTHON_EXCEPTION_HEADER))}"
-        }
+    override fun checkLLDBOutput(output: String, nativeTargets: KotlinNativeTargets): Boolean {
+        sanityCheckLLDBOutput(output)
 
         val blocks = output.split(LLDB_OUTPUT_SEPARATOR).filterNot(String::isBlank)
 
         val meaningfulBlocks = if (nativeTargets.testTarget == nativeTargets.hostTarget) {
             // TODO: why are these two leading blocks only checked for the host target?
-
-            val createTargetBlock = blocks.getOrElse(0) { "" }
-            assertTrue(createTargetBlock.startsWith("(lldb) target create")) {
-                "Missing block \"target create\". Got: $createTargetBlock"
+            val expectedPrefix = listOf(
+                "(lldb) target create",
+                "(lldb) command script import",
+                "(lldb) command script import",
+            )
+            expectedPrefix.forEachIndexed { index, expected ->
+                val actual = blocks.getOrElse(index) { "" }
+                assertTrue(actual.startsWith(expected)) { "Missing block $expected. Got: $actual" }
             }
 
-            val commandScriptBlock = blocks.getOrElse(1) { "" }
-            assertTrue(commandScriptBlock.startsWith("(lldb) command script import")) {
-                "Missing block \"command script import\". Got: $commandScriptBlock"
-            }
-
-            blocks.drop(2)
+            blocks.drop(expectedPrefix.size)
         } else {
-            blocks.drop(2).dropLast(1)
+            blocks.drop(3).dropLast(1)
         }
 
         val recordedSteps = meaningfulBlocks.map { block -> Step.parse(block, LLDB_COMMAND_PREFIX) }
@@ -128,18 +128,32 @@ internal class LLDBSessionSpec private constructor(private val expectedSteps: Li
         return true
     }
 
+    private class Step(val command: String, val body: List<String>) {
+        companion object {
+            fun parse(block: String, expectedPrefix: String): Step {
+                val lines = block.lines()
+
+                val commandWithPrefix = lines.first()
+                assertTrue(commandWithPrefix.startsWith(expectedPrefix)) {
+                    "The command should start with $expectedPrefix. Got: $commandWithPrefix"
+                }
+
+                val command = commandWithPrefix.removePrefix(expectedPrefix).trimStart()
+                val body = lines.drop(1).filterNot(String::isBlank)
+
+                return Step(command, body)
+            }
+        }
+    }
+
     companion object {
         private const val LLDB_COMMAND_PREFIX = "(lldb)"
         private const val SPEC_COMMAND_PREFIX = ">"
-
         private val LLDB_OUTPUT_SEPARATOR = """(?=\(lldb\))""".toRegex()
         private val SPEC_BLOCK_SEPARATOR = "(?=^>)".toRegex(RegexOption.MULTILINE)
-
         private val LINE_WILDCARD = """\s*\[\.\.]\s*""".toRegex()
 
-        private const val PYTHON_EXCEPTION_HEADER = "Traceback (most recent call last):"
-
-        fun parse(lldbSpec: String): LLDBSessionSpec = LLDBSessionSpec(
+        fun parse(lldbSpec: String): ReplLLDBSessionSpec = ReplLLDBSessionSpec(
             lldbSpec.split(SPEC_BLOCK_SEPARATOR)
                 .filterNot(String::isBlank)
                 .map { block -> Step.parse(block, SPEC_COMMAND_PREFIX) }
