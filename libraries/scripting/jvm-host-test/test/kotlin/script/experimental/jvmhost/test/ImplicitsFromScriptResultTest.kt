55e9658543750dbefdeba0478adf2bed657350ad
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2025-12-18 11:06:48 +0100

    ~ wip - snippets/scripts locationId - compatible with updated refinement

--- libraries/scripting/jvm-host-test/test/kotlin/script/experimental/jvmhost/test/ImplicitsFromScriptResultTest.kt
+++ libraries/scripting/jvm-host-test/test/kotlin/script/experimental/jvmhost/test/ImplicitsFromScriptResultTest.kt
@@ -134,7 +134,7 @@ class CompilerHost {
 
     private class SourceCodeTestImpl(number: Int, override val text: String) : SourceCode {
         override val name: String = "Line_$number"
-        override val locationId: String = "location_$number"
+        override val locationId: String = "$name.kts"
     }
 }
 
