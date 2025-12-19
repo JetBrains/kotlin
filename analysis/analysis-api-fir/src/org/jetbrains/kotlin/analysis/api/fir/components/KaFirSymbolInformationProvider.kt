a32ca2b2c2185d38ad6f403913ad2b53d5d5a57d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2025-12-19 19:45:47 +0100

    [Analysis API] add deprecated-hidden status for unavailable methods from JDK 21
    Kotlin produces some methods (like getFirst/getLast) for compatibility
    with old JDKs, and it was fixed in KT-77143, but with >=21 version
    the hidden flag is still required.
    The same problem is applicable for java.lang.Enum
    members such as `getDeclaringClass` and `finalize`.
    All such declarations are mentioned in
    `JvmBuiltInsSignatures#HIDDEN_METHOD_SIGNATURES`
    
    ^KT-78550 Fixed

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirSymbolInformationProvider.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirSymbolInformationProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -57,7 +57,7 @@ internal class KaFirSymbolInformationProvider(
             val firSymbol = this.firSymbol
 
             // A symbol exists for compatibility reasons and should never be referenced in user code
-            val isObsoleteSymbol = firSymbol.origin == FirDeclarationOrigin.Synthetic.FakeHiddenInPreparationForNewJdk
+            val isObsoleteSymbol = (firSymbol.fir as? FirCallableDeclaration)?.hiddenEverywhereBesideSuperCallsStatus != null
                     || (firSymbol is FirSimpleSyntheticPropertySymbol && firSymbol.noJavaOrigin)
 
             if (isObsoleteSymbol) {
