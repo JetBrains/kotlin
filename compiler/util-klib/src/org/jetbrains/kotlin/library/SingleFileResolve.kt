bfbce0659800a2254024a2ae74e560af6621c878
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2025-12-22 11:44:42 +0100

    KLIB Resolver: Add test for compatibility with external legacy tools
    There is at least one known client of the KLIB resolver API:
    The Gradle plugin of the kotlinx-benchmarks project.
    
    This Gradle plugin should provide binary compatibility with a wide
    range of compiler versions, starting from 2.0.20. Which means
    that we can't drop the KLIB resolver when it's no more used
    in the compiler. Instead, we should keep it in the compiler marked
    as @Deprecated. And at the same time we have to set up some tests
    that would help to control that the KLIB resolver still works correctly.
    
    With this commit, we are adding such tests.
    
    ^KT-82882
    ^KT-83328

--- compiler/util-klib/src/org/jetbrains/kotlin/library/SingleFileResolve.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/SingleFileResolve.kt
@@ -20,8 +20,10 @@ fun interface SingleFileKlibResolveStrategy {
 }
 
 @Deprecated(
-    "Preserved for binary compatibility with existing versions of the kotlinx-benchmarks Gradle plugin. See KT-82882.",
-    level = DeprecationLevel.HIDDEN
+    "Preserved for binary compatibility with existing versions of the kotlinx-benchmarks Gradle plugin. See KT-82882." +
+            "\nPlease use KlibLoader.load() instead.",
+    replaceWith = ReplaceWith("KlibLoader.load()", imports = ["org.jetbrains.kotlin.library.loader.KlibLoader"]),
+    level = DeprecationLevel.ERROR
 )
 fun resolveSingleFileKlib(
     libraryFile: File,
--- native/native.tests/klib-ir-inliner/tests/org/jetbrains/kotlin/konan/test/KlibResolverTest.kt
+++ native/native.tests/klib-ir-inliner/tests/org/jetbrains/kotlin/konan/test/KlibResolverTest.kt
@@ -10,6 +10,8 @@ import org.jetbrains.kotlin.cli.common.arguments.CommonKlibBasedCompilerArgument
 import org.jetbrains.kotlin.cli.common.arguments.cliArgument
 import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.config.DuplicatedUniqueNameStrategy
+import org.jetbrains.kotlin.konan.file.ZipFileSystemAccessor
+import org.jetbrains.kotlin.konan.library.KLIB_INTEROP_IR_PROVIDER_IDENTIFIER
 import org.jetbrains.kotlin.konan.properties.propertyList
 import org.jetbrains.kotlin.konan.test.blackbox.*
 import org.jetbrains.kotlin.konan.test.blackbox.support.*
@@ -23,10 +25,11 @@ import org.jetbrains.kotlin.konan.test.blackbox.support.runner.TestExecutable
 import org.jetbrains.kotlin.konan.test.blackbox.support.settings.KotlinNativeHome
 import org.jetbrains.kotlin.konan.test.blackbox.support.settings.KotlinNativeTargets
 import org.jetbrains.kotlin.library.*
+import org.jetbrains.kotlin.library.impl.createKotlinLibraryComponents
 import org.jetbrains.kotlin.library.loader.KlibLoader
+import org.jetbrains.kotlin.library.metadata.resolver.impl.libraryResolver
 import org.jetbrains.kotlin.test.services.JUnit5Assertions
 import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertContainsElements
-import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertEquals
 import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertFalse
 import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertNotEquals
 import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertTrue
@@ -34,11 +37,14 @@ import org.jetbrains.kotlin.test.services.JUnit5Assertions.fail
 import org.jetbrains.kotlin.test.utils.assertCompilerOutputHasKlibResolverIncompatibleAbiMessages
 import org.jetbrains.kotlin.test.utils.patchManifestAsMap
 import org.jetbrains.kotlin.test.utils.patchManifestToBumpAbiVersion
+import org.jetbrains.kotlin.util.Logger
 import org.jetbrains.kotlin.utils.addToStdlib.runIf
 import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 import org.junit.jupiter.api.parallel.Isolated
+import org.jetbrains.kotlin.konan.file.File as KlibFile
 import java.io.File
 
 /**
@@ -436,6 +442,173 @@ class KlibResolverTest : AbstractNativeSimpleTest() {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * This is a special test needed to make sure that the external users of the KLIB resolver that cannot migrate to [KlibLoader]
+     * can still use the KLIB resolver and get the correct results.
+     *
+     * Related tickets: KT-82882, KT-83328.
+     */
+    @Nested
+    inner class LegacyKlibResolverUser {
+        @Test
+        fun `Minimal required set of dependencies written to manifest of Native libraries`() {
+            createModules(
+                Module("usesOnlyStdlib")
+            ).compileModules(
+                produceUnpackedKlibs = true,
+                useLibraryNamesInCliArguments = false,
+            ) { module, successKlib ->
+                assertEquals("usesOnlyStdlib", module.name)
+
+                patchManifestAsMap(JUnit5Assertions, successKlib.resultingArtifact.klibFile) { properties ->
+                    assertEquals("stdlib", properties[KLIB_PROPERTY_DEPENDS])
+                }
+            }
+
+            createModules(
+                Module("usesStdlibAndPosix")
+            ).also { modules ->
+                modules[0].sourceFile.appendText(
+                    """
+                        @OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+                        fun makePosixCall() {
+                            platform.posix.fopen("test.txt", "r")
+                        }
+                    """.trimIndent()
+                )
+            }.compileModules(
+                produceUnpackedKlibs = true,
+                useLibraryNamesInCliArguments = false,
+            ) { module, successKlib ->
+                assertEquals("usesStdlibAndPosix", module.name)
+
+                patchManifestAsMap(JUnit5Assertions, successKlib.resultingArtifact.klibFile) { properties ->
+                    assertEquals(
+                        setOf("stdlib", "org.jetbrains.kotlin.native.platform.posix"),
+                        properties[KLIB_PROPERTY_DEPENDS]?.split(" ")?.toSet()
+                    )
+                }
+            }
+        }
+
+        @Test
+        fun `Resolve non-Native libraries in kotlinx-benchmarks Gradle plugins`() = doTest(isForKotlinNative = false)
+
+        @Test
+        fun `Resolve Native libraries in kotlinx-benchmarks Gradle plugins`() = doTest(isForKotlinNative = true)
+
+        private fun doTest(isForKotlinNative: Boolean) {
+            val moduleToKlibMapping: MutableMap<Module, File> = hashMapOf()
+
+            /*
+             * "e" -> "d" -> "c" -> "b" -> "a"
+             *  |      |             ^      ^
+             *  |      +-------------+      |
+             *  +---------------------------+
+             */
+            createModules(
+                Module("a"),
+                Module("b", "a"),
+                Module("c", "b"),
+                Module("d", "c", "b"),
+                Module("e", "d", "a"),
+            ).compileModules(
+                produceUnpackedKlibs = true,
+                useLibraryNamesInCliArguments = false
+            ) { module, successKlib ->
+                // Remember the location of a KLIB dir.
+                val libraryLocation = File(successKlib.resultingArtifact.path)
+                moduleToKlibMapping[module] = libraryLocation
+
+                if (!isForKotlinNative) {
+                    // Simulate the absence of `depends=` property, which is in effect for Kotlin/JS and Kotlin/Wasm.
+                    patchManifestAsMap(JUnit5Assertions, successKlib.resultingArtifact.klibFile) { properties ->
+                        properties.remove(KLIB_PROPERTY_DEPENDS)
+                    }
+                }
+            }
+
+            // There should be 5 generated KLIBs in total.
+            assertEquals(5, moduleToKlibMapping.size)
+
+            val targetModule: Module = moduleToKlibMapping.keys.single { it.name == "e" }
+            val targetModuleLocation: File = moduleToKlibMapping.getValue(targetModule)
+
+            val dependencyModules: Set<Module> = moduleToKlibMapping.keys - targetModule
+            val dependencyModuleLocations: Set<File> = dependencyModules.map { moduleToKlibMapping.getValue(it) }.toSet()
+
+            val allDependencyLocations: Set<File> = dependencyModuleLocations + stdlibLocation // also add stdlib
+
+            val resolvedDependencies = resolveLibrariesInKotlinxBenchmarksGradlePlugin(
+                libraryFile = targetModuleLocation,
+                dependencyFiles = allDependencyLocations,
+                isForKotlinNative = isForKotlinNative,
+            )
+
+            if (!isForKotlinNative) {
+                // TODO (KT-83328): This needs to be fixed.
+                assertTrue(resolvedDependencies.isEmpty())
+                return
+            }
+
+            assertEquals(allDependencyLocations.size, resolvedDependencies.size)
+
+            assertEquals(
+                dependencyModules.map { it.name }.toSet() + "stdlib",
+                resolvedDependencies.map { it.uniqueName }.toSet()
+            )
+        }
+
+        private val stdlibLocation: File
+            get() = testRunSettings.get<KotlinNativeHome>().librariesDir.resolve("common/stdlib")
+
+        /**
+         * This is an emulation of `KlibResolver.createModuleDescriptor(File, Set<File>, StorageManager)` function
+         * from the kotlinx-benchmarks Gradle plugin.
+         */
+        @Suppress("DEPRECATION", "UNRESOLVED_REFERENCE", "OVERRIDE_DEPRECATION", "DEPRECATION_ERROR")
+        private fun resolveLibrariesInKotlinxBenchmarksGradlePlugin(
+            libraryFile: File,
+            dependencyFiles: Set<File>,
+            isForKotlinNative: Boolean,
+        ): Collection<KotlinLibrary> {
+            val logger = object : Logger {
+                override fun log(message: String) = Unit
+                override fun error(message: String) = kotlin.error("e: $message")
+                override fun warning(message: String) = Unit
+                override fun fatal(message: String) = kotlin.error("e: $message")
+            }
+
+            val knownIrProviders = if (isForKotlinNative) listOf(KLIB_INTEROP_IR_PROVIDER_IDENTIFIER) else emptyList()
+
+            class KotlinxBenchmarksLibraryResolverSimulation(
+                klibs: List<String>
+            ) : KotlinLibraryProperResolverWithAttributes<KotlinLibrary>(
+                repositories = emptyList(),
+                directLibs = klibs,
+                distributionKlib = null,
+                localKotlinDir = null,
+                skipCurrentDir = false,
+                logger = logger,
+                knownIrProviders = knownIrProviders
+            ) {
+                override fun libraryComponentBuilder(file: KlibFile, isDefault: Boolean): List<KotlinLibrary> =
+                    createKotlinLibraryComponents(file, isDefault, null as ZipFileSystemAccessor?)
+            }
+
+            val library = resolveSingleFileKlib(KlibFile(libraryFile.path).canonicalFile)
+
+            return KotlinxBenchmarksLibraryResolverSimulation(
+                klibs = dependencyFiles.map { it.path }
+            ).libraryResolver().resolveWithDependencies(
+                unresolvedLibraries = library.unresolvedDependencies,
+                noStdLib = !isForKotlinNative,
+                noDefaultLibs = !isForKotlinNative,
+                noEndorsedLibs = !isForKotlinNative,
+            ).getFullList()
         }
     }
 
