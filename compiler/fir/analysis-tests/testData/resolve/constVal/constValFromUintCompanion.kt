bc6c4b4bae16183334bff55c7b874669679af3f0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-12-22 12:45:17 +0100

    K2/deserialization/JVM: correct isUnsigned flag for deserialized constants to match real type
    #KT-83317 Fixed
    
    (cherry picked from commit d16c7d083374325caa812a8154c6cc93ea9c0a5c)

--- compiler/fir/analysis-tests/testData/resolve/constVal/constValFromUintCompanion.kt
+++ compiler/fir/analysis-tests/testData/resolve/constVal/constValFromUintCompanion.kt
@@ -1,5 +1,7 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 // ISSUE: KT-83317
 // WITH_STDLIB
 
 const val HEADER_BYTE_SIZE: Int = UInt.SIZE_BYTES + UInt.SIZE_BYTES
+
+/* GENERATED_FIR_TAGS: additiveExpression, const, propertyDeclaration */
