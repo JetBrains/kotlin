e27d0b00717b8d07912900338a153818482f2aac
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-12-22 12:55:26 +0100

    K2: extract ConstantValueKind.toConeType() from buildLiteralExpression
    (cherry picked from commit 6fd3fe89db5ec841aeffa31a08559d0464f34f2e)

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/builder/FirConstExpressionBuilder.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/expressions/builder/FirConstExpressionBuilder.kt
@@ -7,12 +7,13 @@ package org.jetbrains.kotlin.fir.expressions.builder
 
 import org.jetbrains.kotlin.KtSourceElement
 import org.jetbrains.kotlin.fir.StandardTypes
-import org.jetbrains.kotlin.fir.StandardTypes.UInt
 import org.jetbrains.kotlin.fir.builder.toMutableOrEmpty
 import org.jetbrains.kotlin.fir.expressions.FirAnnotation
 import org.jetbrains.kotlin.fir.expressions.FirLiteralExpression
 import org.jetbrains.kotlin.fir.expressions.impl.FirLiteralExpressionImpl
+import org.jetbrains.kotlin.fir.types.ConeKotlinType
 import org.jetbrains.kotlin.types.ConstantValueKind
+import org.jetbrains.kotlin.utils.addToStdlib.runIf
 
 fun buildLiteralExpression(
     source: KtSourceElement?,
@@ -22,29 +23,29 @@ fun buildLiteralExpression(
     setType: Boolean,
     prefix: String? = null,
 ): FirLiteralExpression {
-    val coneType = if (setType) {
-        when (kind) {
-            ConstantValueKind.Boolean -> StandardTypes.Boolean
-            ConstantValueKind.Byte -> StandardTypes.Byte
-            ConstantValueKind.Char -> StandardTypes.Char
-            ConstantValueKind.Double -> StandardTypes.Double
-            ConstantValueKind.Float -> StandardTypes.Float
-            ConstantValueKind.Int -> StandardTypes.Int
-            ConstantValueKind.Long -> StandardTypes.Long
-            ConstantValueKind.Null -> StandardTypes.NullableAny
-            ConstantValueKind.Short -> StandardTypes.Short
-            ConstantValueKind.String -> StandardTypes.String
-            ConstantValueKind.UnsignedByte -> StandardTypes.UByte
-            ConstantValueKind.UnsignedInt -> UInt
-            ConstantValueKind.UnsignedLong -> StandardTypes.ULong
-            ConstantValueKind.UnsignedShort -> StandardTypes.UShort
-            ConstantValueKind.IntegerLiteral,
-            ConstantValueKind.UnsignedIntegerLiteral,
-            ConstantValueKind.Error
-                -> null
-        }
-    } else {
-        null
+    val coneType = runIf(setType) {
+        kind.toConeType()
     }
     return FirLiteralExpressionImpl(source, coneType, annotations.toMutableOrEmpty(), kind, value, prefix)
+}
+
+fun ConstantValueKind.toConeType(): ConeKotlinType? = when (this) {
+    ConstantValueKind.Boolean -> StandardTypes.Boolean
+    ConstantValueKind.Byte -> StandardTypes.Byte
+    ConstantValueKind.Char -> StandardTypes.Char
+    ConstantValueKind.Double -> StandardTypes.Double
+    ConstantValueKind.Float -> StandardTypes.Float
+    ConstantValueKind.Int -> StandardTypes.Int
+    ConstantValueKind.Long -> StandardTypes.Long
+    ConstantValueKind.Null -> StandardTypes.NullableAny
+    ConstantValueKind.Short -> StandardTypes.Short
+    ConstantValueKind.String -> StandardTypes.String
+    ConstantValueKind.UnsignedByte -> StandardTypes.UByte
+    ConstantValueKind.UnsignedInt -> StandardTypes.UInt
+    ConstantValueKind.UnsignedLong -> StandardTypes.ULong
+    ConstantValueKind.UnsignedShort -> StandardTypes.UShort
+    ConstantValueKind.IntegerLiteral,
+    ConstantValueKind.UnsignedIntegerLiteral,
+    ConstantValueKind.Error
+        -> null
 }
