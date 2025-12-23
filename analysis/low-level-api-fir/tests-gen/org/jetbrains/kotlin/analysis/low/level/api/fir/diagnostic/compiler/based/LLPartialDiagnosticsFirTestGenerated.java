469ae392faf23183fc92a976edc4e028dcfcfeac
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2025-12-23 18:14:27 +0100

    K2: reproduce KT-83241

--- analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLPartialDiagnosticsFirTestGenerated.java
+++ analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLPartialDiagnosticsFirTestGenerated.java
@@ -4938,6 +4938,12 @@ public class LLPartialDiagnosticsFirTestGenerated extends AbstractLLPartialDiagn
       public void testSuppressOnNameBasedDestructuring() {
         runTest("compiler/fir/analysis-tests/testData/resolve/destructuring/suppressOnNameBasedDestructuring.kt");
       }
+
+      @Test
+      @TestMetadata("topLevelWithLambda.kt")
+      public void testTopLevelWithLambda() {
+        runTest("compiler/fir/analysis-tests/testData/resolve/destructuring/topLevelWithLambda.kt");
+      }
     }
 
     @Nested
