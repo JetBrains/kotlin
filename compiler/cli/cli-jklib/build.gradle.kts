d74ae1a977039610887ba553fa2c7aa6da2f0bcf
Julien Dramaix <dramaix@google.com>
2025-12-23 10:59:07 +0100

    Create specific cli and serialization modules for jklib

---
+++ compiler/cli/cli-jklib/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation(project(":compiler:ir.psi2ir"))
+    implementation(project(":compiler:cli-common"))
+    implementation(project(":compiler:cli"))
+    implementation(project(":compiler:ir.backend.common"))
+    implementation(project(":compiler:ir.serialization.jklib"))
+    implementation(project(":compiler:backend.jvm"))
+    implementation(project(":compiler:backend.jvm.entrypoint"))
+    implementation(project(":compiler:fir:fir2ir:jvm-backend"))
+
+    compileOnly(intellijCore())
+}
+
+sourceSets {
+    "main" {
+        projectDefault()
+    }
+}
+
+optInToUnsafeDuringIrConstructionAPI()
