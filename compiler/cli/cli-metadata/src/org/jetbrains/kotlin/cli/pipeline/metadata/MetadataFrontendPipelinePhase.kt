ec73dc5f6b41bc86942752f6bd5eeb763be8ed04
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-23 13:37:08 +0200

    [CLI] Extract metadata-specific code to `:compiler:cli-metadata`

--- compiler/cli/cli-metadata/src/org/jetbrains/kotlin/cli/pipeline/metadata/MetadataFrontendPipelinePhase.kt
+++ compiler/cli/cli-metadata/src/org/jetbrains/kotlin/cli/pipeline/metadata/MetadataFrontendPipelinePhase.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 @file:Suppress("DEPRECATION")
@@ -121,7 +121,7 @@ object MetadataFrontendPipelinePhase : PipelinePhase<ConfigurationPipelineArtifa
 
             val sourceScope =
                 projectEnvironment.getSearchScopeByPsiFiles(ktFiles) + projectEnvironment.getSearchScopeForProjectJavaSources()
-            val providerAndScopeForIncrementalCompilation = org.jetbrains.kotlin.cli.jvm.compiler.createContextForIncrementalCompilation(
+            val providerAndScopeForIncrementalCompilation = createContextForIncrementalCompilation(
                 projectEnvironment,
                 configuration,
                 sourceScope
