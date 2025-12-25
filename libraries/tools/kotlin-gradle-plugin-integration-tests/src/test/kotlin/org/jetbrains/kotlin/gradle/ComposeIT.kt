0946452e1cc5e56349f24fb86bdbe203e2a2d6b1
Andrei Shikov <ashikov@google.com>
2025-12-25 20:31:12 +0000

    Bump version of the ASM dependency
    Allow Compose mapping analysis to read files compiled for Java 25+.
    
    Fixes: CMP-9459
    Test: ComposeIT

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ComposeIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/ComposeIT.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.gradle
 
 import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import org.gradle.api.logging.LogLevel
-import org.gradle.api.logging.configuration.WarningMode
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
@@ -668,6 +667,12 @@ class ComposeIT : KGPBaseTest() {
                         it.shrinkResources = true
                     }
                 }
+                // Known dependency that is compiled with JDK 25
+                // Tests for CMP-9459
+                dependencies.add(
+                    "implementation",
+                    "org.bouncycastle:bcprov-jdk18on:1.83"
+                )
             }
 
             build("assembleRelease") {
