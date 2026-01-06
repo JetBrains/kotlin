28184c3ff9ff5470ceee0ea1c4a5daaf82ae3087
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-01-06 20:31:54 +0100

    [FIR] FirJvmAnnotationsPlatformSpecificSupportComponent: drop redundant StandardClassIds usages
    Since KT-78443 components are composable, so there is no need to
    duplicate the default logic inside the JVM one
    
    ^KT-83225

--- compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/FirJvmAnnotationsPlatformSpecificSupportComponent.kt
+++ compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/FirJvmAnnotationsPlatformSpecificSupportComponent.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -11,43 +11,29 @@ import org.jetbrains.kotlin.fir.expressions.FirAnnotation
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassLikeSymbol
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.JvmStandardClassIds
-import org.jetbrains.kotlin.name.StandardClassIds
 
 object FirJvmAnnotationsPlatformSpecificSupportComponent : FirAnnotationsPlatformSpecificSupportComponent() {
     override val requiredAnnotationsWithArguments: Set<ClassId> = setOf(
-        StandardClassIds.Annotations.Deprecated,
-        StandardClassIds.Annotations.Target,
         JvmStandardClassIds.Annotations.Java.Target,
         JvmStandardClassIds.Annotations.JvmName,
-        StandardClassIds.Annotations.DeprecatedSinceKotlin,
-        StandardClassIds.Annotations.SinceKotlin,
     )
 
     override val requiredAnnotations: Set<ClassId> = requiredAnnotationsWithArguments + setOf(
         JvmStandardClassIds.Annotations.Java.Deprecated,
-        StandardClassIds.Annotations.WasExperimental,
         JvmStandardClassIds.Annotations.JvmRecord,
     )
 
     override val volatileAnnotations: Set<ClassId> = setOf(
-        StandardClassIds.Annotations.Volatile,
         JvmStandardClassIds.Annotations.JvmVolatile,
     )
 
     override val deprecationAnnotationsWithOverridesPropagation: Map<ClassId, Boolean> = mapOf(
-        StandardClassIds.Annotations.Deprecated to true,
         JvmStandardClassIds.Annotations.Java.Deprecated to false,
-        StandardClassIds.Annotations.SinceKotlin to true,
     )
 
     override fun symbolContainsRepeatableAnnotation(symbol: FirClassLikeSymbol<*>, session: FirSession): Boolean {
-        if (symbol.hasAnnotationWithClassId(StandardClassIds.Annotations.Repeatable, session)) return true
-        if (symbol.hasAnnotationWithClassId(JvmStandardClassIds.Annotations.Java.Repeatable, session) ||
-            symbol.hasAnnotationWithClassId(JvmStandardClassIds.Annotations.JvmRepeatable, session)
-        ) {
-            return true
-        }
-        return false
+        return symbol.hasAnnotationWithClassId(JvmStandardClassIds.Annotations.Java.Repeatable, session) ||
+                symbol.hasAnnotationWithClassId(JvmStandardClassIds.Annotations.JvmRepeatable, session)
     }
 
     override fun extractBackingFieldAnnotationsFromProperty(
