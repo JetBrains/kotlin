cd2fa82018c81bb763812336292d024ce0f5b5e4
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-01-07 18:53:42 +0100

    [Analysis API FIR] move test data for AnalysisApiSurfaceTest to fir module
    Closer to the test
    
    ^KT-79109

--- analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/test/AnalysisApiSurfaceTest.kt
+++ analysis/analysis-api-fir/tests/org/jetbrains/kotlin/analysis/api/fir/test/AnalysisApiSurfaceTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -13,7 +13,7 @@ import org.jetbrains.kotlin.psi.KtFile
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-class AnalysisApiSurfaceTest : AbstractAnalysisApiExecutionTest("analysis/analysis-api/testData/surface") {
+class AnalysisApiSurfaceTest : AbstractAnalysisApiExecutionTest("analysis/analysis-api-fir/testData/surface") {
     override val configurator = AnalysisApiFirSourceTestConfigurator(analyseInDependentSession = false)
 
     @Test
