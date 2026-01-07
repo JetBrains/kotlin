2639bc377f47aad7d12d6d5e04244790e0243745
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-01-07 15:27:36 +0100

    ~ WIP basic proj boilerplate with parsing libs

--- compiler/java-direct/build.gradle.kts
+++ compiler/java-direct/build.gradle.kts
@@ -3,8 +3,20 @@ plugins {
     id("test-inputs-check")
 }
 
+repositories {
+    mavenCentral()
+    maven { setUrl("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
+}
+
 dependencies {
     api(project(":compiler:frontend.java"))
+
+    implementation(libs.org.jetbrains.syntax.api)
+    implementation(libs.org.jetbrains.java.syntax.jvm)
+
+    testApi(kotlinTest("junit"))
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
 sourceSets {
