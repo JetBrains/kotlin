fcc3cee184ea743226c3b9bec84e7aaa3fa92b6d
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-01-09 18:16:07 +0100

    [Analysis API FIR] KaFirCollectionLiteralReference: support non-annotation positions
    `KtCollectionLiteralReference` was updated to stabilize the order
    
    ^KT-83226

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/KaFirCollectionLiteralReference.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/references/KaFirCollectionLiteralReference.kt
@@ -1,16 +1,18 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.analysis.api.fir.references
 
 import org.jetbrains.kotlin.analysis.api.fir.KaFirSession
-import org.jetbrains.kotlin.analysis.api.fir.symbols.KaFirArrayOfSymbolProvider.arrayOf
+import org.jetbrains.kotlin.analysis.api.fir.getResolvedKtSymbolOfNameReference
+import org.jetbrains.kotlin.analysis.api.fir.symbols.KaFirArrayOfSymbolProvider
 import org.jetbrains.kotlin.analysis.api.fir.symbols.KaFirArrayOfSymbolProvider.arrayOfSymbol
 import org.jetbrains.kotlin.analysis.api.symbols.KaSymbol
-import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFirSafe
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.getOrBuildFir
 import org.jetbrains.kotlin.fir.expressions.FirCollectionLiteral
+import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 import org.jetbrains.kotlin.idea.references.KtCollectionLiteralReference
 import org.jetbrains.kotlin.psi.KtCollectionLiteralExpression
 import org.jetbrains.kotlin.psi.KtImplementationDetail
@@ -21,9 +23,14 @@ import org.jetbrains.kotlin.references.KotlinPsiReferenceProviderContributor
 internal class KaFirCollectionLiteralReference(
     expression: KtCollectionLiteralExpression,
 ) : KtCollectionLiteralReference(expression), KaFirReference {
-    override fun KaFirSession.computeSymbols(): Collection<KaSymbol> {
-        val fir = element.getOrBuildFirSafe<FirCollectionLiteral>(resolutionFacade) ?: return emptyList()
-        return listOfNotNull(arrayOfSymbol(fir) ?: arrayOfSymbol(arrayOf))
+    override fun KaFirSession.computeSymbols(): Collection<KaSymbol> = when (val fir = element.getOrBuildFir(resolutionFacade)) {
+        // Collection literals inside annotations
+        is FirCollectionLiteral -> listOfNotNull(arrayOfSymbol(fir) ?: arrayOfSymbol(KaFirArrayOfSymbolProvider.arrayOf))
+
+        // Non-annotations position
+        is FirFunctionCall -> listOfNotNull(fir.calleeReference.getResolvedKtSymbolOfNameReference(firSymbolBuilder))
+
+        else -> emptyList()
     }
 
     override fun isReferenceToImportAlias(alias: KtImportAlias): Boolean {
