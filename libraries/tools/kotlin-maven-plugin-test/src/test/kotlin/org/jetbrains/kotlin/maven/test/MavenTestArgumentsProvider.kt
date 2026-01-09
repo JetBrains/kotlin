f6959dc647cd53d03904d4e0f9a548fe2a21d11f
Anton Lakotka <anton.lakotka@jetbrains.com>
2026-01-09 13:17:11 +0100

    [Maven] Configure MavenTest to provide arguments only for maven
    For JDK it is simpler to use it directly.
    
    Configure test to use two MavenVersions
    
    ^KT-83111 Verification Pending
    ^KT-83112 Verification Pending

--- libraries/tools/kotlin-maven-plugin-test/src/test/kotlin/org/jetbrains/kotlin/maven/test/MavenTestArgumentsProvider.kt
+++ libraries/tools/kotlin-maven-plugin-test/src/test/kotlin/org/jetbrains/kotlin/maven/test/MavenTestArgumentsProvider.kt
@@ -6,21 +6,47 @@
 package org.jetbrains.kotlin.maven.test
 
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
 import org.junit.jupiter.params.support.ParameterDeclarations
 import java.util.stream.Stream
 import kotlin.streams.asStream
 
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@MavenVersions
+@ParameterizedTest(name = "{0}: {displayName}")
+@ArgumentsSource(MavenTestArgumentsProvider::class)
+annotation class MavenTest
+
 class MavenTestArgumentsProvider : ArgumentsProvider {
     override fun provideArguments(parameters: ParameterDeclarations, context: ExtensionContext): Stream<out Arguments> {
-        val jdkVersions = findAnnotationOrNull<JdkVersions>(context) ?: return Stream.empty()
-        return jdkVersions.versions.asSequence().map { Arguments.of(it) }.asStream()
+        val mavenVersionsAnnotation = findAnnotationOrNull<MavenVersions>(context)
+            ?: throw IllegalArgumentException("@MavenVersions annotation is required when maven parameter is present")
+
+        val mavenVersions = buildSet {
+            add(mavenVersionsAnnotation.min)
+            addAll(mavenVersionsAnnotation.additional.toList())
+            add(mavenVersionsAnnotation.max)
+        }
+
+        return mavenVersions.asSequence().map { mavenVersion ->
+            parameters.all.map { parameter ->
+                when (parameter.parameterType) {
+                    TestVersions.Maven::class.java -> mavenVersion
+                    else -> throw IllegalArgumentException("Unexpected test parameter: $parameter")
+                }
+            }.let { Arguments.of(*it.toTypedArray<Any>()) }
+        }.asStream()
     }
 }
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class JdkVersions(
-    val versions: Array<TestVersions.Java> = [TestVersions.Java.JDK_11],
+annotation class MavenVersions(
+    val min: String = TestVersions.Maven.MIN_SUPPORTED,
+    val max: String = TestVersions.Maven.MAX_SUPPORTED,
+    val additional: Array<String> = [],
 )
