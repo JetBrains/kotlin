4ff54834bbd7195c4b035cf4adcbcf40a5ad4c05
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-01-09 18:52:44 +0100

    [Analysis API FIR] support resolve for unsigned annotation collection literals
    `KaFirArrayOfSymbolProvider#correspondingArrayOfCallFqName` had incorrect
    `correspondingArrayOfCallFqName` that transformed unsigned arrays to
    something like `uIntArrayOf`
    
    ^KT-83226

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirArrayOfSymbolProvider.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/symbols/KaFirArrayOfSymbolProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -7,40 +7,33 @@ package org.jetbrains.kotlin.analysis.api.fir.symbols
 
 import org.jetbrains.kotlin.analysis.api.fir.KaFirSession
 import org.jetbrains.kotlin.analysis.api.symbols.KaNamedFunctionSymbol
+import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.fir.expressions.FirCollectionLiteral
 import org.jetbrains.kotlin.fir.resolve.providers.symbolProvider
-import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
+import org.jetbrains.kotlin.fir.resolve.toArrayOfFactoryName
 import org.jetbrains.kotlin.fir.symbols.impl.FirNamedFunctionSymbol
 import org.jetbrains.kotlin.fir.types.ConeClassLikeType
 import org.jetbrains.kotlin.fir.types.resolvedType
-import org.jetbrains.kotlin.name.ClassId
-import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.name.StandardClassIds
 
 internal object KaFirArrayOfSymbolProvider {
     internal fun KaFirSession.arrayOfSymbol(identifier: Name): KaNamedFunctionSymbol? {
-        val firSymbol = firSession.symbolProvider.getTopLevelCallableSymbols(kotlinPackage, identifier).firstOrNull {
+        val firSymbol = firSession.symbolProvider.getTopLevelCallableSymbols(
+            packageFqName = StandardNames.BUILT_INS_PACKAGE_FQ_NAME,
+            name = identifier,
+        ).firstOrNull {
             /* choose (for a byte array)
              * public fun byteArrayOf(vararg elements: kotlin.Byte): kotlin.ByteArray
              */
-            (it as? FirFunctionSymbol<*>)?.fir?.valueParameters?.singleOrNull()?.isVararg == true
+            (it as? FirNamedFunctionSymbol)?.fir?.valueParameters?.singleOrNull()?.isVararg == true
         } as? FirNamedFunctionSymbol ?: return null
+
         return firSymbolBuilder.functionBuilder.buildNamedFunctionSymbol(firSymbol)
     }
 
-    private val kotlinPackage = FqName("kotlin")
-    internal val arrayOf = Name.identifier("arrayOf")
-    internal val arrayTypeToArrayOfCall = run {
-        StandardClassIds.primitiveArrayTypeByElementType.values + StandardClassIds.unsignedArrayTypeByElementType.values
-    }.associateWith { it.correspondingArrayOfCallFqName() }
-
-    private fun ClassId.correspondingArrayOfCallFqName(): Name =
-        Name.identifier("${shortClassName.identifier.replaceFirstChar(Char::lowercaseChar)}Of")
-
     internal fun KaFirSession.arrayOfSymbol(collectionLiteral: FirCollectionLiteral): KaNamedFunctionSymbol? {
         val type = collectionLiteral.resolvedType as? ConeClassLikeType ?: return null
-        val call = arrayTypeToArrayOfCall[type.lookupTag.classId] ?: arrayOf
-        return arrayOfSymbol(call)
+        val factoryName = toArrayOfFactoryName(type, firSession, eagerlyReturnNonPrimitive = true) ?: return null
+        return arrayOfSymbol(factoryName)
     }
 }
