ffd8ee7f7c6ee6a95a02516c56c4dc07314cfede
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-01-12 13:48:08 +0200

    ~~ Rewrite the flow of `FirWasmJsEqualityChecker`
    This way there're no "late" applicability checks at the stage of
    selecting the proper diagnostic - all the checks happen in the beginning
    and the diagnostic-chossing code remains untouched.

--- compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirPlatformSpecificEqualityChecker.kt
+++ compiler/fir/checkers/src/org/jetbrains/kotlin/fir/analysis/checkers/FirPlatformSpecificEqualityChecker.kt
@@ -5,25 +5,40 @@
 
 package org.jetbrains.kotlin.fir.analysis.checkers
 
+import org.jetbrains.kotlin.config.LanguageFeature
+import org.jetbrains.kotlin.diagnostics.DiagnosticReporter
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.FirSessionComponent
+import org.jetbrains.kotlin.fir.analysis.checkers.context.CheckerContext
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirEqualityCompatibilityChecker
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirEqualityCompatibilityChecker.checkApplicability
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirEqualityCompatibilityChecker.ifInapplicable
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.FirEqualityCompatibilityChecker.reportInapplicabilityDiagnostic
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.isCaseMissedByAdditionalK1IncompatibleEnumsCheck
+import org.jetbrains.kotlin.fir.analysis.checkers.expression.isCaseMissedByK1Intersector
+import org.jetbrains.kotlin.fir.expressions.FirEqualityOperatorCall
+import org.jetbrains.kotlin.fir.expressions.FirSmartCastExpression
+import org.jetbrains.kotlin.fir.isEnabled
 import org.jetbrains.kotlin.fir.types.ConeKotlinType
 
 abstract class FirPlatformSpecificEqualityChecker : FirSessionComponent {
-    abstract fun shouldSuppressInapplicableEquality(
+    context(context: CheckerContext)
+    abstract fun runApplicabilityCheck(
+        expression: FirEqualityOperatorCall,
         leftType: ConeKotlinType,
         rightType: ConeKotlinType,
-        generalApplicabilityChecker: TypeOperationApplicabilityChecker,
-    ): Boolean
+        checker: FirEqualityCompatibilityChecker,
+    ): FirEqualityCompatibilityChecker.Applicability
 
     object Default : FirPlatformSpecificEqualityChecker() {
-        override fun shouldSuppressInapplicableEquality(
+        context(context: CheckerContext)
+        override fun runApplicabilityCheck(
+            expression: FirEqualityOperatorCall,
             leftType: ConeKotlinType,
             rightType: ConeKotlinType,
-            generalApplicabilityChecker: TypeOperationApplicabilityChecker
-        ): Boolean {
-            return false
-        }
+            checker: FirEqualityCompatibilityChecker,
+        ): FirEqualityCompatibilityChecker.Applicability =
+            checker.checkApplicability(expression.operation, leftType.toTypeInfo(context.session), rightType.toTypeInfo(context.session))
     }
 }
 
