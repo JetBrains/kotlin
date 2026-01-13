817600ee1b9960c83b11e51b72d36b1d0e6d5d2d
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-01-13 18:18:02 +0100

    fixup! [K\N][Tests] Extract an abstract class from LLDBSessionSpec

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/util/LLDBSessionSpec.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/util/LLDBSessionSpec.kt
@@ -18,8 +18,6 @@ abstract class LLDBSessionSpec {
         this += "-b"
         this += "-o"
         this += "command script import ${prettyPrinters.absolutePath}"
-        this += "-o"
-        this += "command script import ${File("kotlin-native/llvmDebugInfoC/src/scripts/konan_lldb_test_helper.py").absolutePath}"
     }
 
     abstract fun checkLLDBOutput(output: String, nativeTargets: KotlinNativeTargets): Boolean
