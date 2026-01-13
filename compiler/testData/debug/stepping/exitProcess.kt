5986ea47b6b82001a30594123e7db1602c267511
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-01-13 16:25:00 +0100

    [Tests] Add exitProcess.kt stepping test
    It's not only a test of the debug functionality itself, but also the
    test infra, to ensure we handle early process exit correctly.

---
+++ compiler/testData/debug/stepping/exitProcess.kt
@@ -0,0 +1,14 @@
+// IGNORE_BACKEND: JS_IR, WASM_JS
+// ^^^ Those backends don't support exitProcess()
+// WITH_STDLIB
+
+// FILE: test.kt
+fun box() {
+    println("start")
+    kotlin.system.exitProcess(0)
+    println("unreachable")
+}
+
+// EXPECTATIONS JVM_IR
+// test.kt:7 box
+// test.kt:8 box
