894dccb5f658fea744cf4b115534b09c4b40e476
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-01-15 18:20:43 +0400

    [AA] Expand test with external declarations
    In particular, an "external" enum entry or constructor inside
    a non-external class is not considered `external` by the K2 compiler
    (and is invalid code).
    
    KT-83694

--- analysis/analysis-api/testData/symbols/symbolByPsi/externalDeclarations.kt
+++ analysis/analysis-api/testData/symbols/symbolByPsi/externalDeclarations.kt
@@ -1,11 +1,18 @@
+// DO_NOT_CHECK_NON_PSI_SYMBOL_RESTORE
 // DO_NOT_CHECK_SYMBOL_RESTORE_K1
 
 external class ClassC(val paramProp: String) {
     constructor() : this("")
 
-    fun String.foo(regularParam: Int)
+    init {
 
-    class Nested {
+    }
+
+    fun String.foo(regularParam: Int) {
+        val local = 0
+    }
+
+    class Nested<T> {
         fun bar()
     }
 
@@ -16,6 +23,23 @@ external val prop: Int
 
 external enum class EnumE { EnumA, EnumB }
 
+enum class EnumNotExternal { external EnumA2 }
+
+class NotExternalC(external val paramProp2: String) {
+    external constructor() : this("")
+}
+
 external interface InterfaceI {
     fun fooI()
 }
+
+sealed external class SealedC {
+    data object SealedO : SealedC()
+}
+
+val propWithExternalGetter: Int
+    external get
+
+var propWithExternalAccessors: Int
+    external get
+    external set
