7aa9007e149d2313e0d7e3d33da53185d2926330
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-01-15 16:47:38 +0100

    [K/N] Use footprint instead of RSS for MemoryUsageInfo on Apple
    On Apple platforms, memory footprint (rather than RSS)
    is the typically used memory usage metric.
    
    Change the MemoryUsageInfo.peakResidentSetSizeBytes from
    peak RSS to peak footprint on Apple platforms.
    
    ^KT-83002

--- kotlin-native/runtime/src/main/cpp/MemoryUsageInfo.cpp
+++ kotlin-native/runtime/src/main/cpp/MemoryUsageInfo.cpp
@@ -10,12 +10,14 @@
 
 #include "Types.h"
 
+using namespace kotlin;
+
 #if KONAN_WINDOWS
 
 #include <windows.h>
 #include <psapi.h>
 
-size_t kotlin::GetPeakResidentSetSizeBytes() noexcept {
+size_t kotlin::peakResidentSetSizeBytes() noexcept {
     ::PROCESS_MEMORY_COUNTERS memoryCounters;
     auto succeeded = ::GetProcessMemoryInfo(::GetCurrentProcess(), &memoryCounters, sizeof(memoryCounters));
     if (!succeeded) {
@@ -24,40 +26,48 @@ size_t kotlin::GetPeakResidentSetSizeBytes() noexcept {
     return memoryCounters.PeakWorkingSetSize;
 }
 
-#elif KONAN_LINUX || KONAN_MACOSX || KONAN_IOS || KONAN_TVOS || KONAN_WATCHOS
+#elif KONAN_APPLE
 
-#include <sys/time.h>
-#include <sys/resource.h>
+#include <unistd.h>
 
-size_t kotlin::GetPeakResidentSetSizeBytes() noexcept {
-    ::rusage usage;
-    auto failed = ::getrusage(RUSAGE_SELF, &usage);
-#if KONAN_LINUX
-    // On Linux it's in kilobytes.
-    size_t maxrss = static_cast<size_t>(usage.ru_maxrss * 1024);
-#elif KONAN_MACOSX || KONAN_IOS || KONAN_TVOS || KONAN_WATCHOS
-    // On macOS, iOS, tvOS and watchOS it's in bytes.
-    size_t maxrss = static_cast<size_t>(usage.ru_maxrss);
+#if KONAN_MACOSX
+#include <libproc.h>
 #else
-#error "Check what units ru_maxrss is in."
+// libproc.h is not shipped on non-macOS, but the function is still defined there.
+extern "C" int proc_pid_rusage(int pid, int flavor, rusage_info_t* buffer)
+	__OSX_AVAILABLE_STARTING(__MAC_10_9, __IPHONE_7_0);
 #endif
+
+size_t kotlin::peakResidentSetSizeBytes() noexcept {
+    ::rusage_info_current info;
+    auto failed = proc_pid_rusage(getpid(), RUSAGE_INFO_CURRENT, reinterpret_cast<rusage_info_t*>(&info));
     if (failed) {
         return 0;
     }
-    return maxrss;
+    // Max footprint can't be more than size_t.
+    return static_cast<size_t>(info.ri_lifetime_max_phys_footprint);
 }
 
 #else
 
-// TODO: Support more platforms
-size_t kotlin::GetPeakResidentSetSizeBytes() noexcept {
-    return 0;
+#include <sys/time.h>
+#include <sys/resource.h>
+
+size_t kotlin::peakResidentSetSizeBytes() noexcept {
+    ::rusage usage;
+    auto failed = ::getrusage(RUSAGE_SELF, &usage);
+    if (failed) {
+        return 0;
+    }
+    // ru_maxrss is in KiB on Linux.
+    size_t maxrss = static_cast<size_t>(usage.ru_maxrss * 1024);
+    return maxrss;
 }
 
 #endif
 
 extern "C" RUNTIME_NOTHROW KLong Kotlin_MemoryUsageInfo_getPeakResidentSetSizeBytes() {
-    auto result = kotlin::GetPeakResidentSetSizeBytes();
+    auto result = peakResidentSetSizeBytes();
     // TODO: Need a common implementation for such conversions.
     if constexpr (sizeof(decltype(result)) >= sizeof(KLong)) {
         return static_cast<KLong>(std::min<decltype(result)>(result, std::numeric_limits<KLong>::max()));
