41db1c9ee1b683661898d18d28b7849730601549
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-01-15 22:56:47 +0100

    [Native][Tests] Don't use PSI in NativeLauncherAdditionalSourceProvider
    ^KT-82469
    ^KT-82471

--- native/native.tests/klib-compatibility/testFixtures/org/jetbrains/kotlin/konan/test/klib/NativeLauncherAdditionalSourceProvider.kt
+++ native/native.tests/klib-compatibility/testFixtures/org/jetbrains/kotlin/konan/test/klib/NativeLauncherAdditionalSourceProvider.kt
@@ -5,32 +5,20 @@
 
 package org.jetbrains.kotlin.konan.test.klib
 
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.util.Disposer
-import org.jetbrains.kotlin.K1Deprecation
-import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
-import org.jetbrains.kotlin.config.CommonConfigurationKeys
-import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.konan.test.blackbox.support.util.generateBoxFunctionLauncher
-import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.KtPsiFactory
-import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
 import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
 import org.jetbrains.kotlin.test.model.TestFile
 import org.jetbrains.kotlin.test.model.TestModule
-import org.jetbrains.kotlin.test.services.AdditionalSourceProvider
 import org.jetbrains.kotlin.test.services.TestModuleStructure
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.configuration.NativeEnvironmentConfigurator
+import org.jetbrains.kotlin.test.services.sourceProviders.MainFunctionForBlackBoxTestsSourceProvider
 import org.jetbrains.kotlin.test.services.temporaryDirectoryManager
 
 private const val LAUNCHER_FILE_NAME = "__launcher__.kt"
-private val BOX_FUNCTION_NAME = Name.identifier("box")
+private val BOX_FUNCTION_NAME = "box"
 
-class NativeLauncherAdditionalSourceProvider(testServices: TestServices) : AdditionalSourceProvider(testServices) {
+class NativeLauncherAdditionalSourceProvider(testServices: TestServices) : MainFunctionForBlackBoxTestsSourceProvider(testServices) {
     override fun produceAdditionalFiles(
         globalDirectives: RegisteredDirectives,
         module: TestModule,
@@ -39,60 +27,14 @@ class NativeLauncherAdditionalSourceProvider(testServices: TestServices) : Addit
         if (!NativeEnvironmentConfigurator.isMainModule(module, testModuleStructure))
             return emptyList()
 
-        val boxFunctionFqn = findBoxFunctionFqn(module)
-            ?: BOX_FUNCTION_NAME.asString() // Should metatestconfigurators allow the test to run, let the frontend raise error "No box() function found")
+        val fileWithBox = module.files.firstOrNull { containsBoxMethod(it.originalContent) } ?: return emptyList()
+        val boxFqName = detectPackage(fileWithBox)?.let { "$it.$BOX_FUNCTION_NAME" } ?: BOX_FUNCTION_NAME
+        val launcherContent = generateBoxFunctionLauncher(boxFqName)
 
-        val launcherContent = generateBoxFunctionLauncher(boxFunctionFqn)
         val tempDir = testServices.temporaryDirectoryManager.getOrCreateTempDirectory("launcher")
         val launcherFile = tempDir.resolve(LAUNCHER_FILE_NAME).also {
             it.writeText(launcherContent)
         }
-
-        return listOf(
-            TestFile(
-                relativePath = LAUNCHER_FILE_NAME,
-                originalContent = launcherContent,
-                originalFile = launcherFile,
-                startLineNumberInOriginalFile = 0,
-                isAdditional = true,
-                directives = RegisteredDirectives.Empty
-            )
-        )
-    }
-
-    private fun findBoxFunctionFqn(module: TestModule): String? {
-        val disposable = Disposer.newDisposable("Disposable for NativeLauncherAdditionalSourceProvider")
-        try {
-            val psiFactory = createPsiFactory(disposable)
-            for (file in module.files) {
-                if (!file.name.endsWith(".kt")) continue
-                runWriteAction {
-                    // Actually, no real writing happens here, however write action is needed here,
-                    // since in the end of action, `FileManagerImpl.dispose()` invokes `clearViewProviders()`, which is annotated with @RequiresWriteLock
-                    val ktFile = psiFactory.createFile(file.name, file.originalContent)
-                    if (ktFile.getChildrenOfType<KtNamedFunction>().any { function ->
-                            function.name == BOX_FUNCTION_NAME.asString() && function.valueParameters.isEmpty()
-                        }) ktFile.packageFqName.child(BOX_FUNCTION_NAME).asString()
-                    else null
-                }?.let { return it }
-            }
-            return null
-        } finally {
-            Disposer.dispose(disposable)
-        }
-    }
-
-    private fun createPsiFactory(parentDisposable: Disposable): KtPsiFactory {
-        val configuration = CompilerConfiguration()
-        configuration.put(CommonConfigurationKeys.MODULE_NAME, "native-launcher-source-provider-module")
-
-        @OptIn(K1Deprecation::class)
-        val environment = KotlinCoreEnvironment.createForProduction(
-            projectDisposable = parentDisposable,
-            configuration = configuration,
-            configFiles = EnvironmentConfigFiles.METADATA_CONFIG_FILES
-        )
-
-        return KtPsiFactory(environment.project)
+        return listOf(launcherFile.toTestFile())
     }
 }
