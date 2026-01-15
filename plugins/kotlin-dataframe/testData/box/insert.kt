d6e1aff247033b31f3f8d560aac6c7a538343a5a
Nikita Klimenko <nikita.klimenko@jetbrains.com>
2026-01-15 15:25:23 +0200

    [KDF] Make insert.kt test actually run compiled df code

--- plugins/kotlin-dataframe/testData/box/insert.kt
+++ plugins/kotlin-dataframe/testData/box/insert.kt
@@ -8,18 +8,18 @@ interface Person {
     val age: Int
 }
 
-val df = dataFrameOf("age")(1).cast<Person>()
-
-fun insert_properties() {
+fun box(): String {
+    val df = dataFrameOf("age")(1).cast<Person>()
     val df1 = df.insert("year of birth") { 2021 - age }.under("test")
     df1.age
     df1.test.`year of birth`
+    df1.compareSchemas(strict = true)
 
     val df2 = dataFrameOf("a")(11)
         .group { a }.into("group")
         .insert("b") { group.a * 111 }.under { group }
     df2.group.a
     df2.group.b
+    df2.compareSchemas(strict = true)
+    return "OK"
 }
-
-fun box() = "OK"
