7eefb6ed353d3f66b0a3e804b54a53a2c235a2fd
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-01-15 23:46:51 +0100

    [K/N][build] Download gtest via ivy repository
    Switch googletest downloading to ivy repositories
    instead of a custom `GitDownloadTask`.
    
    Using repositories is much better integrated into Gradle
    w.r.t. caching and dependency verification.
    
    Additionally, completely remove `RuntimeTestingPlugin`
    and `GitDownloadTask` as they are no longer used.
    
    ^KT-83702

--- kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/bitcode/CompileToBitcodePlugin.kt
+++ kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/bitcode/CompileToBitcodePlugin.kt
@@ -31,7 +31,6 @@ import org.jetbrains.kotlin.dependencies.NativeDependenciesPlugin
 import org.jetbrains.kotlin.konan.target.*
 import org.jetbrains.kotlin.nativeDistribution.nativeProtoDistribution
 import org.jetbrains.kotlin.resolveLlvmUtility
-import org.jetbrains.kotlin.testing.native.GoogleTestExtension
 import org.jetbrains.kotlin.utils.capitalized
 import java.io.File
 import java.time.Duration
@@ -144,6 +143,16 @@ open class CompileToBitcodeExtension @Inject constructor(val project: Project) :
 
         project.executorsClasspathConfiguration()
     }
+
+    /**
+     * GTest headers to be used in testFixtures and test source sets.
+     */
+    val googleTestHeadersNoDependency: ConfigurableFileCollection = project.objects.fileCollection()
+
+    /**
+     * Task to download GTest.
+     */
+    val googleTestHeadersDependency: Property<String> = project.objects.property(String::class)
 
     private val allTestsTasks by lazy {
         val name = project.name.capitalized
@@ -314,9 +323,6 @@ open class CompileToBitcodeExtension @Inject constructor(val project: Project) :
         abstract class SourceSets @Inject constructor(private val module: Module, private val container: ExtensiblePolymorphicDomainObjectContainer<SourceSet>) : NamedDomainObjectContainer<SourceSet> by container {
             private val project by module::project
 
-            // googleTestExtension is only used if testFixtures or tests are used.
-            private val googleTestExtension by lazy { project.extensions.getByType<GoogleTestExtension>() }
-
             /**
              * Get `main` source set if it was configured.
              */
@@ -349,14 +355,14 @@ open class CompileToBitcodeExtension @Inject constructor(val project: Project) :
              */
             fun testFixtures(action: Action<in SourceSet>): SourceSet = create(TEST_FIXTURES_SOURCE_SET_NAME) {
                 this.inputFiles.include("**/*TestSupport.cpp", "**/*TestSupport.mm")
-                this.headersDirs.from(googleTestExtension.headersDirs)
+                this.headersDirs.from(module.owner.googleTestHeadersNoDependency)
                 // TODO: Must generally depend on googletest module headers which must itself depend on sources being present.
-                dependencies.add(project.tasks.named("downloadGoogleTest"))
+                dependencies.add(project.tasks.named(module.owner.googleTestHeadersDependency.get()))
                 compileTask.configure {
                     this.group = VERIFICATION_BUILD_TASK_GROUP
 
-                    // Without this explicit statement task dependency is not created even if it is requested in RuntimeTestingPlugin
-                    dependsOn(project.tasks.named("downloadGoogleTest"))
+                    // Without this explicit statement task dependency is not created even though it's set in dependencies just above
+                    dependsOn(project.tasks.named(module.owner.googleTestHeadersDependency.get()))
                 }
                 task.configure {
                     this.group = VERIFICATION_BUILD_TASK_GROUP
@@ -375,14 +381,14 @@ open class CompileToBitcodeExtension @Inject constructor(val project: Project) :
              */
             fun test(action: Action<in SourceSet>): SourceSet = create(TEST_SOURCE_SET_NAME) {
                 this.inputFiles.include("**/*Test.cpp", "**/*Test.mm")
-                this.headersDirs.from(googleTestExtension.headersDirs)
+                this.headersDirs.from(module.owner.googleTestHeadersNoDependency)
                 // TODO: Must generally depend on googletest module headers which must itself depend on sources being present.
-                dependencies.add(project.tasks.named("downloadGoogleTest"))
+                dependencies.add(project.tasks.named(module.owner.googleTestHeadersDependency.get()))
                 compileTask.configure {
                     this.group = VERIFICATION_BUILD_TASK_GROUP
 
-                    // Without this explicit statement task dependency is not created even if it is requested in RuntimeTestingPlugin
-                    dependsOn(project.tasks.named("downloadGoogleTest"))
+                    // Without this explicit statement task dependency is not created even though it's set in dependencies just above
+                    dependsOn(project.tasks.named(module.owner.googleTestHeadersDependency.get()))
                 }
                 task.configure {
                     this.group = VERIFICATION_BUILD_TASK_GROUP
