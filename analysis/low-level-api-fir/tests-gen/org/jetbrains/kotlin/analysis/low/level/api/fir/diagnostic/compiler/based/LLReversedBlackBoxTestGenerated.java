cc4675b1100cdea638d22661eca0ea2d954c2967
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-01-15 11:35:29 +0100

    K2: add more tests for KT-82201, in particular around KT-81988
    The tests were added mostly to prove that the experimental feature
    PreciseSimplificationToFlexibleLowerConstraint cannot be easily "relaxed",
    as bugs like KT-81988 can occur even in a relaxed state.
    Under "relaxed" I mean additional checks in simplifyLowerConstraint,
    like subType.isCapturedType or subType.isTypeVariable(Parameter)Type.

--- analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
+++ analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
@@ -38244,6 +38244,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
     }
 
     @Test
+    @TestMetadata("usingNullableValueAsLowerBoundLeadsToNullableResult2.kt")
+    public void testUsingNullableValueAsLowerBoundLeadsToNullableResult2() {
+      runTest("compiler/testData/codegen/box/javaInterop/usingNullableValueAsLowerBoundLeadsToNullableResult2.kt");
+    }
+
+    @Test
     @TestMetadata("varargCall1.kt")
     public void testVarargCall1() {
       runTest("compiler/testData/codegen/box/javaInterop/varargCall1.kt");
