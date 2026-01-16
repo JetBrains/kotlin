678f830ccb7280674b2719ed8e9caf133e0e44d3
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2026-01-16 17:08:07 +0100

    [Gradle] Remove usages of isVisible/setVisible in buildsrc-compat
    ^KT-78754

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/share-kotlin-wasm-custom-formatters.gradle.kts
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/share-kotlin-wasm-custom-formatters.gradle.kts
@@ -9,14 +9,12 @@ val wasmCustomFormatters by configurations.creating {
     isCanBeResolved = false
     isCanBeConsumed = false
     isCanBeDeclared = true
-    isVisible = false
 }
 
 val wasmCustomFormattersProvider by configurations.creating {
     isCanBeResolved = false
     isCanBeConsumed = true
     isCanBeDeclared = false
-    isVisible = false
     attributes {
         attribute(Usage.USAGE_ATTRIBUTE, CUSTOM_FORMATTERS_ATTRIBUTE)
     }
@@ -26,7 +24,6 @@ val wasmCustomFormattersResolver by configurations.creating {
     isCanBeResolved = true
     isCanBeConsumed = false
     isCanBeDeclared = false
-    isVisible = false
     extendsFrom(wasmCustomFormatters)
     attributes {
         attribute(Usage.USAGE_ATTRIBUTE, CUSTOM_FORMATTERS_ATTRIBUTE)
