a00c7a1a05e362662eed37256c9a4c488087dce3
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-01-16 21:19:54 +0900

    [Analysis API] Add tests for 'KotlinPackageProvider'
    ^KT-83388

---
+++ analysis/analysis-api/testData/symbols/packages/simpleJs.kt
@@ -0,0 +1,13 @@
+// WITH_STDLIB
+// TARGET_PLATFORM: JS
+
+package foo
+
+// HAS_PACKAGE: <root>
+// NO_PACKAGE: bar
+// HAS_PACKAGE: foo
+// NO_PACKAGE: foo.bar
+// HAS_PACKAGE: kotlin
+// HAS_PACKAGE: kotlin.collections
+// NO_PACKAGE: kotlin.reflect.js.internal
+// NO_PACKAGE: java.io
