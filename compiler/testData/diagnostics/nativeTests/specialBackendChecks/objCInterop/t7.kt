4bda562b1aa78d5d04b2d320b07912f2e60f5ae2
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-01-16 20:45:05 +0100

    [Native][Tests] Add diagnostic and modify testData for special backend testT6 & testT7
    ^KT-61564

--- compiler/testData/diagnostics/nativeTests/specialBackendChecks/objCInterop/t7.kt
+++ compiler/testData/diagnostics/nativeTests/specialBackendChecks/objCInterop/t7.kt
@@ -1,6 +1,7 @@
-// RUN_PIPELINE_TILL: BACKEND
+// RUN_PIPELINE_TILL: FRONTEND
 // WITH_PLATFORM_LIBS
 import platform.darwin.*
 import platform.Foundation.*
 
-fun foo(s: Array<Any?>) = NSLog("zzz", *s)
+fun foo(s: Array<Any?>) = NSLog("zzz", <!VARIADIC_C_SPREAD_IS_SUPPORTED_ONLY_FOR_ARRAYOF!>*s<!>)
+fun bar(s: Array<Any?>) = NSLog("zzz", *arrayOf(*arrayOf(<!VARIADIC_C_SPREAD_IS_SUPPORTED_ONLY_FOR_ARRAYOF!>*s<!>)))
