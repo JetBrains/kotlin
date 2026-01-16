7014ad6c037edcb6dd46930a8fc99e9d52f54a2a
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-01-16 17:01:52 +0400

    [AA] Introduce `KtExpression.isStable` property
    ^KT-80460 Fixed

---
+++ analysis/analysis-api-impl-base/testFixtures/org/jetbrains/kotlin/analysis/api/impl/base/test/cases/components/expressionInfoProvider/AbstractIsStableTest.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.impl.base.test.cases.components.expressionInfoProvider
+
+import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedTest
+import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtTestModule
+import org.jetbrains.kotlin.analysis.test.framework.services.expressionMarkerProvider
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.test.services.TestServices
+import org.jetbrains.kotlin.test.services.assertions
+
+abstract class AbstractIsStableTest : AbstractAnalysisApiBasedTest() {
+    override fun doTestByMainFile(mainFile: KtFile, mainModule: KtTestModule, testServices: TestServices) {
+        val expression = testServices.expressionMarkerProvider.getTopmostSelectedElementOfType<KtExpression>(mainFile)
+        val actual = StringBuilder()
+
+        copyAwareAnalyzeForTest(expression) { contextExpression ->
+            actual.appendLine("expression: $contextExpression")
+            actual.appendLine("text: ${contextExpression.text}")
+            actual.appendLine("isStable: ${contextExpression.isStable}")
+        }
+
+        testServices.assertions.assertEqualsToTestOutputFile(actual.toString())
+    }
+}
