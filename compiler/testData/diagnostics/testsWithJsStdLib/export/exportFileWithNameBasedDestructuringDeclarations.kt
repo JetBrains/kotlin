12ab6bbbdfaa5bb83dff604ecf35fce580aa5b01
Artem Kobzar <artem.kobzar@jetbrains.com>
2026-01-17 22:48:57 +0100

    [K/JS] Fix triggering of NON_EXPORTABLE_TYPE diagnostic on syntax errors
    ^KT-83722 Fixed

--- compiler/testData/diagnostics/testsWithJsStdLib/export/exportFileWithNameBasedDestructuringDeclarations.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/export/exportFileWithNameBasedDestructuringDeclarations.kt
@@ -7,9 +7,9 @@ data class Person(val id: Int, val name: String?)
 
 val person = Person(42, null)
 
-<!NON_EXPORTABLE_TYPE!>val <!SYNTAX!>(id, name)<!> = person<!>
+val <!SYNTAX!>(id, name)<!> = person
 
-<!NON_EXPORTABLE_TYPE!><!SYNTAX!>(val fullId = id, var fullName = name)<!> = person<!>
+<!SYNTAX!>(val fullId = id, var fullName = name)<!> = person
 
 fun main() {
     val (id, name) = person
--- compiler/testData/diagnostics/testsWithJsStdLib/export/exportFileWithPositionBasedDestructuringDeclarations.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/export/exportFileWithPositionBasedDestructuringDeclarations.kt
@@ -7,9 +7,9 @@ data class Person(val id: Int, val name: String?)
 
 val person = Person(42, null)
 
-<!NON_EXPORTABLE_TYPE!>val <!SYNTAX!>[first, second]<!> = person<!>
+val <!SYNTAX!>[first, second]<!> = person
 
-<!NON_EXPORTABLE_TYPE!><!SYNTAX!>[val id, var name]<!> = person<!>
+<!SYNTAX!>[val id, var name]<!> = person
 
 fun main() {
     val [first, second] = person
