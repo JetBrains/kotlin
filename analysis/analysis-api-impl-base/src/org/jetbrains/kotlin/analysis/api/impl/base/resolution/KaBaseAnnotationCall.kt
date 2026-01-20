9377f4e0df389950b0c95ccf890aa67a21819e45
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-01-20 18:37:38 +0100

    [Analysis API] KaFunctionCall: rename argumentMapping -> valueArgumentMapping
    We now might have not only value arguments in the mapping but also
    explicit context arguments
    
    ^KT-83223

--- analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/resolution/KaBaseAnnotationCall.kt
+++ analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/resolution/KaBaseAnnotationCall.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -45,5 +45,5 @@ class KaBaseAnnotationCall(
         get() = withValidityAssertion { backingPartiallyAppliedSymbol.contextArguments }
 
     override val typeArgumentsMapping: Map<KaTypeParameterSymbol, KaType> get() = withValidityAssertion { emptyMap() }
-    override val argumentMapping: Map<KtExpression, KaVariableSignature<KaValueParameterSymbol>> get() = withValidityAssertion { backingArgumentMapping }
+    override val valueArgumentMapping: Map<KtExpression, KaVariableSignature<KaValueParameterSymbol>> get() = withValidityAssertion { backingArgumentMapping }
 }
