9b8883e43fdc810ff2b2cbd3c47ee0fbfb047201
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-01-20 13:15:01 +0100

    [K/N][performance] Remove unused stuff from analyzer
    ^KT-83283

--- kotlin-native/tools/benchmarks/shared/src/main/kotlin/analyzer/Utils.kt
+++ kotlin-native/tools/benchmarks/shared/src/main/kotlin/analyzer/Utils.kt
@@ -8,7 +8,3 @@ package org.jetbrains.analyzer
 expect fun readFile(fileName: String): String
 expect fun Double.format(decimalNumber: Int = 4): String
 expect fun writeToFile(fileName: String, text: String)
-expect fun assert(value: Boolean, lazyMessage: () -> Any)
-expect fun sendGetRequest(url: String, user: String? = null, password: String? = null,
-                          followLocation: Boolean = false) : String
-expect fun getDefaultPerformanceServerUrl(): String?
--- kotlin-native/tools/benchmarksAnalyzer/src/main/kotlin-native/org/jetbrains/analyzer/Utils.kt
+++ kotlin-native/tools/benchmarksAnalyzer/src/main/kotlin-native/org/jetbrains/analyzer/Utils.kt
@@ -8,7 +8,6 @@ package org.jetbrains.analyzer
 
 import platform.posix.*
 import kotlinx.cinterop.*
-import libcurl.*
 
 actual fun readFile(fileName: String): String {
     val file = fopen(fileName, "r") ?: error("Cannot read file '$fileName'")
@@ -40,73 +39,3 @@ actual fun writeToFile(fileName: String, text: String) {
         fclose(file)
     }
 }
-
-@OptIn(kotlin.experimental.ExperimentalNativeApi::class)
-actual fun assert(value: Boolean, lazyMessage: () -> Any) =
-    kotlin.assert(value, lazyMessage)
-
-class CUrl(url: String, user: String? = null, password: String? = null, followLocation: Boolean = false)  {
-    private val stableRef = StableRef.create(this)
-
-    private val curl = curl_easy_init()
-
-    init {
-        curl_easy_setopt(curl, CURLOPT_URL, url)
-        val writeData = staticCFunction(::collectResponse)
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeData)
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, stableRef.asCPointer())
-        if (followLocation) {
-            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L)
-        }
-        user ?.let {
-            curl_easy_setopt(curl, CURLOPT_USERNAME, it)
-        }
-        password ?.let {
-            curl_easy_setopt(curl, CURLOPT_PASSWORD, it)
-        }
-    }
-
-    val body = StringBuilder()
-
-    fun fetch() {
-        memScoped {
-            val res = curl_easy_perform(curl)
-            if (res != CURLE_OK)
-                error("curl_easy_perform() failed: ${curl_easy_strerror(res)?.toKString()}")
-            val http_code = alloc<LongVar>()
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, http_code.ptr)
-            if (http_code.value >= 400L) {
-                error("Error http code ${http_code.value}")
-            }
-        }
-    }
-
-    fun close() {
-        curl_easy_cleanup(curl)
-        stableRef.dispose()
-    }
-}
-
-fun CPointer<ByteVar>.toKString(length: Int): String {
-    val bytes = this.readBytes(length)
-    return bytes.toKString()
-}
-
-fun collectResponse(buffer: CPointer<ByteVar>?, size: size_t, nitems: size_t, userdata: COpaquePointer?): size_t {
-    buffer ?: return 0u
-    userdata ?. let {
-        val data = buffer.toKString((size * nitems).toInt()).trim()
-        val curl = userdata.asStableRef<CUrl>().get()
-        curl.body.append(data)
-    }
-    return size * nitems
-}
-
-actual fun sendGetRequest(url: String, user: String?, password: String?, followLocation: Boolean) : String {
-    val curl = CUrl(url, user, password, followLocation)
-    curl.fetch()
-    curl.close()
-    return curl.body.toString()
-}
-
-actual fun getDefaultPerformanceServerUrl() : String? = null
