07e162d24bba00dd11d624abc9feec909febcf98
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-01-21 21:04:48 +0100

    KLIB API: Move KLIB_DEFAULT_COMPONENT_NAME constant to `KlibConstants`
    ^KT-81411

--- compiler/util-klib/testFixtures/org/jetbrains/kotlin/library/KlibMockDSL.kt
+++ compiler/util-klib/testFixtures/org/jetbrains/kotlin/library/KlibMockDSL.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.library
 
 import org.jetbrains.kotlin.konan.properties.Properties
 import org.jetbrains.kotlin.konan.properties.saveToFile
+import org.jetbrains.kotlin.library.KlibConstants.KLIB_DEFAULT_COMPONENT_NAME
 import org.jetbrains.kotlin.library.KlibConstants.KLIB_MANIFEST_FILE_NAME
 import org.jetbrains.kotlin.library.KlibConstants.KLIB_RESOURCES_FOLDER_NAME
 import org.jetbrains.kotlin.library.KlibMockDSL.Companion.mockKlib
@@ -14,7 +15,6 @@ import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_FOLDER_NA
 import org.jetbrains.kotlin.library.components.KlibIrConstants.KLIB_IR_INLINABLE_FUNCTIONS_FOLDER_NAME
 import org.jetbrains.kotlin.library.components.KlibMetadataConstants.KLIB_METADATA_FOLDER_NAME
 import org.jetbrains.kotlin.library.impl.BuiltInsPlatform
-import org.jetbrains.kotlin.library.impl.KLIB_DEFAULT_COMPONENT_NAME
 import org.jetbrains.kotlin.library.impl.KlibIrComponentWriterImpl
 import org.jetbrains.kotlin.library.impl.KlibMetadataComponentWriterImpl
 import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
