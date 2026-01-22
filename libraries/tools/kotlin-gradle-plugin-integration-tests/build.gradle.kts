a19f98b0e4926852f5074680bad4a25fca97d91c
Andrei Tyrin <andrei.tyrin@jetbrains.com>
2026-01-22 17:17:49 +0100

    Aggregate KGP test coverage data
    Aggregate data from functional and integration tests
    Generate HTML/XML reports
    
    KT-77279

--- libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/build.gradle.kts
@@ -570,3 +570,10 @@ tasks.withType<Test>().configureEach {
 }
 
 excludeGradleEmbeddedStdlibFromTestTasksRuntimeClasspath()
+
+registerKgpTestCoverageDataVariant(
+    configurationName = "integrationTestCoverageDataElements",
+    suiteName = "integrationTest",
+    execFile = layout.buildDirectory.file("jacoco/coverage.exec"),
+    testTask = tasks.named("kgpAllParallelTests"),
+)
