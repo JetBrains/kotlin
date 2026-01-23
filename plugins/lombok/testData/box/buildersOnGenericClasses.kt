98fa6a997b623433303784af94da66a82b7d0acf
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-01-23 18:30:49 +0100

    [Lombok] Support for `@Buider` and `@SuperBuilder` on generic classes
    ^KT-83334 Fixed

--- plugins/lombok/testData/box/buildersOnGenericClasses.kt
+++ plugins/lombok/testData/box/buildersOnGenericClasses.kt
@@ -1,5 +1,4 @@
 // IGNORE_BACKEND_K1: ANY
-// IGNORE_BACKEND_K2: ANY
 // ISSUE: KT-83334
 
 // FILE: GenericBuilder.java
@@ -45,17 +44,17 @@ public class Ctor<T> {
 
 // FILE: test.kt
 fun testBuilder() {
-    val builderObj1 = GenericBuilder.builder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>().value(<!ARGUMENT_TYPE_MISMATCH!>"x"<!>).build()
+    val builderObj1 = GenericBuilder.builder<String>().value("x").build()
 
-    val builder: GenericBuilder.GenericBuilderBuilder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!> = GenericBuilder.builder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>()
-    val builder2: GenericBuilder.GenericBuilderBuilder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!> = builder.<!UNRESOLVED_REFERENCE!>value<!>("x")
-    val builderObj2: GenericBuilder<String> = builder2.<!UNRESOLVED_REFERENCE!>build<!>();
+    val builder: GenericBuilder.GenericBuilderBuilder<String> = GenericBuilder.builder<String>()
+    val builder2: GenericBuilder.GenericBuilderBuilder<String> = builder.value("x")
+    val builderObj2: GenericBuilder<String> = builder2.build();
 }
 
 fun testSuperBuilder() {
-    val superBuilder: GenericSuperBuilder.GenericSuperBuilderBuilder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String, *, *><!> = GenericSuperBuilder.builder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>();
-    val superBuilder2: GenericSuperBuilder.GenericSuperBuilderBuilder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String, *, *><!> = superBuilder.<!UNRESOLVED_REFERENCE!>value<!>("str")
-    val superBuilderObj: GenericSuperBuilder<String> = superBuilder2.<!UNRESOLVED_REFERENCE!>build<!>()
+    val superBuilder: GenericSuperBuilder.GenericSuperBuilderBuilder<String, *, *> = GenericSuperBuilder.builder<String>();
+    val superBuilder2: GenericSuperBuilder.GenericSuperBuilderBuilder<String, *, *> = superBuilder.value("str")
+    val superBuilderObj: GenericSuperBuilder<String> = superBuilder2.build()
 }
 
 fun testSuperBuilder2() {
@@ -68,16 +67,16 @@ fun testSuperBuilder2() {
     //         T extends Object declared in method <T>builder()
     // However, support it in Kotlin at well.
 
-    GenericSuperBuilder2.builder().value(<!ARGUMENT_TYPE_MISMATCH!>10<!>).value2("str").build();
+    GenericSuperBuilder2.builder().value(10).value2("str").build();
 
     val superBuilder: GenericSuperBuilder2.GenericSuperBuilder2Builder<*, *> = GenericSuperBuilder2.builder()
-    val superBuilder2: GenericSuperBuilder2.GenericSuperBuilder2Builder<*, *> = superBuilder.value(<!ARGUMENT_TYPE_MISMATCH!>10<!>)
+    val superBuilder2: GenericSuperBuilder2.GenericSuperBuilder2Builder<*, *> = superBuilder.value(10)
     val superBuilder3: GenericSuperBuilder2.GenericSuperBuilder2Builder<*, *> = superBuilder2.value2("str")
     val superBuilderObj: GenericSuperBuilder2 = superBuilder3.build()
 }
 
 fun testCtor() {
-    Ctor.builder<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><String><!>().value(<!ARGUMENT_TYPE_MISMATCH!>"x"<!>).build()
+    Ctor.builder<String>().value("x").build()
 }
 
 fun box(): String {
