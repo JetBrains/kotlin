e52365529e082ced1bb2bc5c4041c61ccec3042c
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-01-23 17:36:54 +0200

    [CLI] Convert `CompilerConfiguration` to kotlin
    Also convert `CompilerConfigurationKey` and update call-sites considering
    new `Any` bound on parameter of `CompilerConfigurationKey` and functions
    of the `CompilerConfiguration`.

--- compiler/config/src/org/jetbrains/kotlin/config/CompilerConfiguration.kt
+++ compiler/config/src/org/jetbrains/kotlin/config/CompilerConfiguration.kt
@@ -1,198 +1,138 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.config;
+package org.jetbrains.kotlin.config
 
-import com.intellij.openapi.util.Key;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.openapi.util.Key
+import java.util.*
 
-import java.util.*;
+class CompilerConfiguration {
+    companion object {
+        @JvmField
+        val EMPTY = CompilerConfiguration().apply { isReadOnly = true }
 
-@SuppressWarnings({"unchecked", "rawtypes"})
-public class CompilerConfiguration {
-    public static CompilerConfiguration EMPTY = new CompilerConfiguration();
-
-    private final Map<Key, Object> map = new LinkedHashMap<>();
-    private boolean readOnly = false;
-
-    static {
-        EMPTY.setReadOnly(true);
+        private fun <T> T.unmodifiable(): T {
+            @Suppress("UNCHECKED_CAST")
+            return when (this) {
+                is List<*> -> Collections.unmodifiableList(this)
+                is Map<*, *> -> Collections.unmodifiableMap(this)
+                is Set<*> -> Collections.unmodifiableSet(this)
+                is Collection<*> -> Collections.unmodifiableCollection(this)
+                else -> this
+            } as T
+        }
     }
 
-    @Nullable
-    public <T> T get(@NotNull CompilerConfigurationKey<T> key) {
-        T data = (T) map.get(key.ideaKey);
-        return data == null ? null : unmodifiable(data);
+    private val map: MutableMap<Key<*>, Any> = LinkedHashMap()
+    var isReadOnly = false
+
+    operator fun <T : Any> get(key: CompilerConfigurationKey<T>): T? {
+        return getValue(key)?.unmodifiable()
     }
 
-    @NotNull
-    public <T> T get(@NotNull CompilerConfigurationKey<T> key, @NotNull T defaultValue) {
-        T data = get(key);
-        return data == null ? defaultValue : data;
+    operator fun <T : Any> get(key: CompilerConfigurationKey<T>, defaultValue: T): T {
+        return getValue(key) ?: defaultValue
     }
 
-    @NotNull
-    public <T> T getNotNull(@NotNull CompilerConfigurationKey<T> key) {
-        T data = get(key);
-        assert data != null : "No value for configuration key: " + key;
-        return data;
+    fun <T : Any> getNotNull(key: CompilerConfigurationKey<T>): T {
+        return getValue(key) ?: error("No value for configuration key: $key")
     }
 
-    public boolean getBoolean(@NotNull CompilerConfigurationKey<Boolean> key) {
-        return get(key, false);
+    fun getBoolean(key: CompilerConfigurationKey<Boolean>): Boolean {
+        return get(key, defaultValue = false)
     }
 
-    @NotNull
-    public <T> List<T> getList(@NotNull CompilerConfigurationKey<List<T>> key) {
-        List<T> data = get(key);
-        return data == null ? Collections.emptyList() : data;
+    fun <T> getList(key: CompilerConfigurationKey<List<T>>): List<T> {
+        return get(key, defaultValue = emptyList())
     }
 
-    @NotNull
-    public <K, V> Map<K, V> getMap(@NotNull CompilerConfigurationKey<Map<K, V>> key) {
-        Map<K, V> data = get(key);
-        return data == null ? Collections.emptyMap() : data;
+    fun <K, V> getMap(key: CompilerConfigurationKey<Map<K, V>>): Map<K, V> {
+        return get(key, defaultValue = emptyMap())
     }
 
-    public <T> void put(@NotNull CompilerConfigurationKey<T> key, @NotNull T value) {
-        checkReadOnly();
-        map.put(key.ideaKey, value);
+    fun <T : Any> put(key: CompilerConfigurationKey<T>, value: T) {
+        checkReadOnly()
+        map[key.ideaKey] = value
     }
 
-    public <T> T putIfAbsent(@NotNull CompilerConfigurationKey<T> key, @NotNull T value) {
-        T data = get(key);
-        if (data != null) return data;
-
-        checkReadOnly();
-        put(key, value);
-        return value;
+    fun <T : Any> putIfAbsent(key: CompilerConfigurationKey<T>, value: T): T {
+        getValue(key)?.let { return it }
+        checkReadOnly()
+        put(key, value)
+        return value
     }
 
-    public <T> void putIfNotNull(@NotNull CompilerConfigurationKey<T> key, @Nullable T value) {
+    fun <T : Any> putIfNotNull(key: CompilerConfigurationKey<T>, value: T?) {
         if (value != null) {
-            put(key, value);
+            put(key, value)
         }
     }
 
-    public <T> void add(@NotNull CompilerConfigurationKey<List<T>> key, @NotNull T value) {
-        checkReadOnly();
-        Key<List<T>> ideaKey = key.ideaKey;
-        map.computeIfAbsent(ideaKey, k -> new ArrayList<T>());
-        List<T> list = (List<T>) map.get(ideaKey);
-        list.add(value);
+    fun <T> add(key: CompilerConfigurationKey<List<T>>, value: T) {
+        checkReadOnly()
+
+        @Suppress("UNCHECKED_CAST")
+        val list = map.getOrPut(key.ideaKey) { mutableListOf<T>() } as MutableList<T>
+        list += value
     }
 
-    public <K, V> void put(@NotNull CompilerConfigurationKey<Map<K, V>> configurationKey, @NotNull K key, @NotNull V value) {
-        checkReadOnly();
-        Key<Map<K, V>> ideaKey = configurationKey.ideaKey;
-        map.computeIfAbsent(ideaKey, k -> new HashMap<K, V>());
-        Map<K, V> data = (Map<K, V>) map.get(ideaKey);
-        data.put(key, value);
+    fun <K, V> put(configurationKey: CompilerConfigurationKey<Map<K, V>>, key: K, value: V) {
+        checkReadOnly()
+
+        @Suppress("UNCHECKED_CAST")
+        val map = map.getOrPut(configurationKey.ideaKey) { mutableMapOf<K, V>() } as MutableMap<K, V>
+        map[key] = value
     }
 
-    public <T> void addAll(@NotNull CompilerConfigurationKey<List<T>> key, @Nullable Collection<T> values) {
+    fun <T : Any> addAll(key: CompilerConfigurationKey<List<T>>, values: Collection<T>?) {
         if (values != null) {
-            addAll(key, getList(key).size(), values);
+            addAll(key, getList(key).size, values)
         }
     }
 
-    public <T> void addAll(@NotNull CompilerConfigurationKey<List<T>> key, int index, @NotNull Collection<T> values) {
-        checkReadOnly();
-        checkForNullElements(values);
-        Key<List<T>> ideaKey = key.ideaKey;
-        map.computeIfAbsent(ideaKey, k -> new ArrayList<T>());
-        List<T> list = (List<T>) map.get(ideaKey);
-        list.addAll(index, values);
+    fun <T : Any> addAll(key: CompilerConfigurationKey<List<T>>, index: Int, values: Collection<T>) {
+        checkReadOnly()
+
+        @Suppress("UNCHECKED_CAST")
+        val list = map.getOrPut(key.ideaKey) { mutableListOf<T>() } as MutableList<T>
+        list.addAll(index, values)
     }
 
-    public CompilerConfiguration copy() {
-        CompilerConfiguration copy = new CompilerConfiguration();
-        copy.map.putAll(map);
-        return copy;
+    fun copy(): CompilerConfiguration {
+        return CompilerConfiguration().also { it.map.putAll(map) }
     }
 
-    private void checkReadOnly() {
-        if (readOnly) {
-            throw new IllegalStateException("CompilerConfiguration is read-only");
-        }
+    private fun <T : Any> getValue(key: CompilerConfigurationKey<T>): T? {
+        @Suppress("UNCHECKED_CAST")
+        return map[key.ideaKey] as T?
     }
 
-    public void setReadOnly(boolean readOnly) {
-        if (readOnly != this.readOnly) {
-            this.readOnly = readOnly;
-        }
-    }
-
-    public boolean isReadOnly() {
-        return readOnly;
-    }
-
-    @NotNull
-    private static <T> T unmodifiable(@NotNull T object) {
-        if (object instanceof List) {
-            return (T) Collections.unmodifiableList((List) object);
-        }
-        else if (object instanceof Map) {
-            return (T) Collections.unmodifiableMap((Map) object);
-        }
-        else if (object instanceof Set) {
-            return (T) Collections.unmodifiableSet((Set) object);
-        }
-        else if (object instanceof Collection) {
-            return (T) Collections.unmodifiableCollection((Collection) object);
-        }
-        else {
-            return object;
-        }
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<Key, Object> entry : map.entrySet()) {
-            Object value = entry.getValue();
-            sb.append(entry.getKey()).append(":");
-            if (value instanceof Collection) {
-                sb.append("\n");
-                for (Object o : (Collection<?>) value) {
-                    sb.append("  ").append(o).append("\n");
+    override fun toString(): String {
+        return buildString {
+            for ((key, value) in map) {
+                append(key).append("=")
+                when (value) {
+                    is Collection<*> -> {
+                        appendLine()
+                        for (v in value) {
+                            append("  ").appendLine(v)
+                        }
+                    }
+                    is Map<*, *> -> {
+                        appendLine()
+                        for ((k, v) in value) {
+                            append("  ").append(k).append("=").appendLine(v)
+                        }
+                    }
+                    else -> append("  ").appendLine(value)
                 }
             }
-            else if (value instanceof Map) {
-                sb.append("\n");
-                for (Map.Entry<?, ?> e : ((Map<?, ?>) value).entrySet()) {
-                    sb.append("  ").append(e.getKey()).append("=").append(e.getValue()).append("\n");
-                }
-            }
-            else {
-                sb.append(" ").append(value).append("\n");
-            }
-        }
-        return sb.toString().trim();
+        }.trim()
     }
 
-    private static <T> void checkForNullElements(Collection<T> values) {
-        int index = 0;
-        for (T value : values) {
-            if (value == null) {
-                throw new IllegalArgumentException("Element " + index
-                                                   + " is null, while null values in compiler configuration are not allowed");
-            }
-            index++;
-        }
+    private fun checkReadOnly() {
+        check(!isReadOnly) { "CompilerConfiguration is read-only" }
     }
 }
