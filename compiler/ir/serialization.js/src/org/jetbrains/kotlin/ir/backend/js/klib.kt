b245b23c0926a3aa05076fcd4c326590eb7bc7fa
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2026-01-23 11:01:15 +0100

    [Web] Use declaration table in `WebKlibSerializationPipelinePhase`

--- compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/klib.kt
+++ compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/klib.kt
@@ -472,7 +472,8 @@ fun serializeModuleIntoKlib(
     jsOutputName: String?,
     builtInsPlatform: BuiltInsPlatform = BuiltInsPlatform.JS,
     wasmTarget: WasmTarget? = null,
-    performanceManager: PerformanceManager? = null
+    performanceManager: PerformanceManager? = null,
+    declarationTable: DeclarationTable.Default? = null,
 ) {
     val moduleJsExportNames = moduleFragment.collectJsExportNames()
     val incrementalResultsConsumer = configuration.get(JSConfigurationKeys.INCREMENTAL_RESULTS_CONSUMER)
@@ -490,6 +491,7 @@ fun serializeModuleIntoKlib(
                     settings = IrSerializationSettings(configuration),
                     irDiagnosticReporter,
                     irBuiltIns,
+                    declarationTable,
                 ) { JsIrFileMetadata(moduleJsExportNames[it]?.values?.toSmartList() ?: emptyList()) }
             },
             metadataSerializer = metadataSerializer,
