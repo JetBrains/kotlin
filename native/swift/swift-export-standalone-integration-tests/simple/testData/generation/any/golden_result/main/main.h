380003a1b0cfdc103b144cfabd9addb9be30951f
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-01-23 13:05:42 +0100

    [K/N][Tests] Set kind = standalone for swift export test "generation/any"
    It is the only test in this suite not marked as STANDALONE already,
    possibly by mistake. This causes a problem:
    
    REGULAR tests may be grouped together for faster test execution,
    and this involves patching packages of the test sources to avoid clashes.
    Normally it doesn't change much, but for the Swift export the package of
    the Kotlin code affects the generated Swift code.
    
    It means that when this test was run under STANDALONE mode locally,
    such as with forceStandalone=true, it would fail because package, and
    therefore the generated Swift code, would be different than in the
    REGULAR mode.
    
    KT-83803

--- native/swift/swift-export-standalone-integration-tests/simple/testData/generation/any/golden_result/main/main.h
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/any/golden_result/main/main.h
@@ -3,8 +3,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-void * _Nullable generation_any_any_bar__TypesOfArguments__Swift_Optional_anyU20KotlinRuntimeSupport__KotlinBridgeable___(void * _Nullable arg);
+void * _Nullable __root___bar__TypesOfArguments__Swift_Optional_anyU20KotlinRuntimeSupport__KotlinBridgeable___(void * _Nullable arg);
 
-void * generation_any_any_foo__TypesOfArguments__anyU20KotlinRuntimeSupport__KotlinBridgeable__(void * arg);
+void * __root___foo__TypesOfArguments__anyU20KotlinRuntimeSupport__KotlinBridgeable__(void * arg);
 
 NS_ASSUME_NONNULL_END
