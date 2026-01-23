4d5c82fb1aec834b5a75f01c8e90a7e9d8943629
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-01-23 15:33:32 +0200

    [Test] Reproduce KT-83377

---
+++ plugins/kotlinx-serialization/testData/diagnostics/directAccessOfGeneratedCompanion.kt
@@ -0,0 +1,19 @@
+// WITH_STDLIB
+// ISSUE: KT-83377
+
+import kotlinx.serialization.*
+import kotlinx.serialization.builtins.*
+
+@Serializable
+class Some(val t: String)
+
+fun testGlobal() {
+    val s: Some.Companion? = null
+}
+
+fun testLocal() {
+    @Serializable
+    class Local(val t: String)
+
+    val s: Local.Companion? = null
+}
