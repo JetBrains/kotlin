95c3430c6b3c56ee728da361c47e895b7d9bcf8f
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2026-01-26 16:43:49 +0200

    Add missed file

---
+++ compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/K2NativeKlibCompilerArgumentsConfigurator.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.cli.common.arguments
+
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.config.AnalysisFlag
+import org.jetbrains.kotlin.config.AnalysisFlags
+import org.jetbrains.kotlin.config.LanguageVersion
+
+class K2NativeKlibCompilerArgumentsConfigurator : CommonKlibBasedCompilerArgumentsConfigurator() {
+    override fun configureAnalysisFlags(
+        arguments: CommonCompilerArguments,
+        collector: MessageCollector,
+        languageVersion: LanguageVersion,
+    ): MutableMap<AnalysisFlag<*>, Any> = with(arguments) {
+        require(this is K2NativeKlibCompilerArguments)
+        super.configureAnalysisFlags(arguments, collector, languageVersion).apply {
+            val optInList = (get(AnalysisFlags.optIn) as List<*>?).orEmpty()
+            putAnalysisFlag(AnalysisFlags.optIn, optInList + listOf("kotlin.ExperimentalUnsignedTypes"))
+        }
+    }
+}
