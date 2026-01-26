2096604b9ad15c954000c282c5b79fc3accdae47
Brian Norman <brian.norman@jetbrains.com>
2026-01-26 10:10:34 -0600

    Revert "[Assign plugin] Use AbstractSimpleClassPredicateMatchingService"
    This reverts commit c5b8c4470ee0f4cdcc0ad66627dc4c18f1e55786.

--- plugins/assign-plugin/assign-plugin.k2/src/org/jetbrains/kotlin/assignment/plugin/k2/FirAssignAnnotationMatchingService.kt
+++ plugins/assign-plugin/assign-plugin.k2/src/org/jetbrains/kotlin/assignment/plugin/k2/FirAssignAnnotationMatchingService.kt
@@ -6,25 +6,47 @@
 package org.jetbrains.kotlin.assignment.plugin.k2
 
 import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.extensions.predicate.DeclarationPredicate
-import org.jetbrains.kotlin.fir.extensions.utils.AbstractSimpleClassPredicateMatchingService
+import org.jetbrains.kotlin.fir.caches.FirCache
+import org.jetbrains.kotlin.fir.caches.firCachesFactory
+import org.jetbrains.kotlin.fir.caches.getValue
+import org.jetbrains.kotlin.fir.declarations.toAnnotationClassId
+import org.jetbrains.kotlin.fir.extensions.FirExtensionSessionComponent
+import org.jetbrains.kotlin.fir.extensions.FirExtensionSessionComponent.Factory
+import org.jetbrains.kotlin.fir.resolve.fullyExpandedType
+import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
+import org.jetbrains.kotlin.fir.resolve.toRegularClassSymbol
+import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.name.FqName
 
-class FirAssignAnnotationMatchingService(
+internal class FirAssignAnnotationMatchingService(
     session: FirSession,
-    annotationClassIds: Set<FqName>,
-) : AbstractSimpleClassPredicateMatchingService(session) {
+    private val annotationClassIds: List<FqName>
+) : FirExtensionSessionComponent(session) {
+
     companion object {
         fun getFactory(annotations: List<String>): Factory {
-            return Factory { session ->
-                FirAssignAnnotationMatchingService(session, annotations.map { FqName(it) }.toSet())
-            }
+            return Factory { session -> FirAssignAnnotationMatchingService(session, annotations.map { FqName(it) }) }
         }
     }
 
-    override val predicate: DeclarationPredicate = DeclarationPredicate.create {
-        // TODO KT-83571 Reconsider meta-annotation support for assign plugin
-        annotated(annotationClassIds) or metaAnnotated(annotationClassIds, includeItself = true)
+    private val cache: FirCache<FirRegularClassSymbol, Boolean, Nothing?> = session.firCachesFactory.createCache { symbol, _ ->
+        symbol.annotated()
+    }
+
+    fun isAnnotated(symbol: FirRegularClassSymbol?): Boolean {
+        if (symbol == null) {
+            return false
+        }
+        return cache.getValue(symbol)
+    }
+
+    @OptIn(SymbolInternals::class)
+    private fun FirRegularClassSymbol.annotated(): Boolean {
+        if (this.annotations.any { it.toAnnotationClassId(session)?.asSingleFqName() in annotationClassIds }) return true
+        return resolvedSuperTypeRefs.any { superTypeRef ->
+            val symbol = superTypeRef.coneType.fullyExpandedType(session).toRegularClassSymbol(session) ?: return@any false
+            symbol.annotated()
+        }
     }
 }
 
