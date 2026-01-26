91584f9f66373edaaca2dee080b7b9085e2c6114
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-01-26 22:57:17 +0100

    JVM: fix detection of signature-polymorphic methods
    Even though all signature-polymorphic methods are annotated with
    `@java.lang.invoke.MethodHandle.PolymorphicSignature`, it was incorrect
    to use this annotation as an indicator, because the annotation class is
    package-private, and therefore is stripped in `ct.sym`. Which led to
    polymorphic calls generated incorrectly whenever `-Xjdk-release` was
    used.
    
    The proper solution is to check the method signature only according to
    JLS, in the exact same way as javac does.
    
     #KT-72880 Fixed

--- compiler/testData/diagnostics/tests/j+k/polymorphicSignature/spreadOperator.kt
+++ compiler/testData/diagnostics/tests/j+k/polymorphicSignature/spreadOperator.kt
@@ -1,15 +1,18 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // FIR_IDENTICAL
-// LANGUAGE: +PolymorphicSignature
-// FULL_JDK
+// JDK_KIND: FULL_JDK_21
 
 import java.lang.invoke.MethodHandle
+import java.lang.invoke.VarHandle
 
-fun test(mh: MethodHandle) {
+fun test(mh: MethodHandle, vh: VarHandle) {
     mh.invokeExact("1", "2")
     mh.invokeExact(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>emptyArray(), "X")
     mh.invokeExact(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>arrayOf("A", "B"), "C", <!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>arrayOf("D", "E"))
     mh.invoke(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>arrayOf("A"))
+    vh.get()
+    vh.get(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>emptyArray())
+    vh.set(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>emptyArray())
 }
 
 /* GENERATED_FIR_TAGS: flexibleType, functionDeclaration, javaFunction, stringLiteral */
---
+++ compiler/testData/diagnostics/tests/j+k/polymorphicSignature/spreadOperator_jdkRelease.fir.kt
@@ -0,0 +1,18 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// JDK_KIND: FULL_JDK_21
+// JDK_RELEASE: 11
+
+import java.lang.invoke.MethodHandle
+import java.lang.invoke.VarHandle
+
+fun test(mh: MethodHandle, vh: VarHandle) {
+    mh.invokeExact("1", "2")
+    mh.invokeExact(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>emptyArray(), "X")
+    mh.invokeExact(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>arrayOf("A", "B"), "C", <!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>arrayOf("D", "E"))
+    mh.invoke(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>arrayOf("A"))
+    vh.get()
+    vh.get(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>emptyArray())
+    vh.set(<!SPREAD_ON_SIGNATURE_POLYMORPHIC_CALL_ERROR!>*<!>emptyArray())
+}
+
+/* GENERATED_FIR_TAGS: flexibleType, functionDeclaration, javaFunction, stringLiteral */
