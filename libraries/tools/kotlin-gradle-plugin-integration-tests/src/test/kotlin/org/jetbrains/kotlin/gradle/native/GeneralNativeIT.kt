4cd6a1cd56a0ac86b0f5a2e31598225730d47633
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-01-27 18:19:16 +0100

    [Gradle] Fix test on macOS after max Gradle version bump to 9.3.0
    ^KT-82885

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/GeneralNativeIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/native/GeneralNativeIT.kt
@@ -635,7 +635,18 @@ class GeneralNativeIT : KGPBaseTest() {
 
 
             fun assertStacktrace(taskName: String, targetName: String) {
-                val testReport = projectPath.resolve("build/test-results/$taskName/TEST-org.foo.test.TestKt.xml").toFile()
+                val testReportDir = projectPath.resolve("build/test-results/$taskName")
+                assertDirectoryExists(testReportDir, "Directory $testReportDir does not exist")
+                val testReport = if (gradleVersion < GradleVersion.version(TestVersions.Gradle.G_9_3)) {
+                    testReportDir.resolve("TEST-org.foo.test.TestKt.xml")
+                } else {
+                    testReportDir.resolve("TEST-$taskName.org.foo.test.TestKt.xml")
+                }
+                assertFileExists(
+                    testReport,
+                    "Test report file $testReport does not exist, current files in directory:\n" +
+                            (testReportDir.listDirectoryEntries().joinToString(", ") ?: "empty")
+                )
                 val stacktrace = JDOMUtil.load(testReport)
                     .getChildren("testcase")
                     .single { it.getAttribute("name").value == "fail" || it.getAttribute("name").value == "fail[$targetName]" }
@@ -663,7 +674,12 @@ class GeneralNativeIT : KGPBaseTest() {
                 val testTask = "${testTarget}Test"
 
                 val expectedXmlPath = projectPath.resolve("TEST-TestKt-iOSsim.xml")
-                projectPath.resolve("TEST-TestKt-iOSsim-template.xml").copyTo(expectedXmlPath)
+                val template = if (gradleVersion < GradleVersion.version(TestVersions.Gradle.G_9_3)) {
+                    projectPath.resolve("TEST-TestKt-iOSsim-template.xml")
+                } else {
+                    projectPath.resolve("Gradle93-TEST-TestKt-iOSsim-template.xml")
+                }
+                template.copyTo(expectedXmlPath)
                 expectedXmlPath.replaceText("<target>", testTarget)
                 assertTestResults(
                     expectedXmlPath,
