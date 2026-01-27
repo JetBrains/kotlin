d593f884610eec9bf38543f95451b0567891aa72
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-01-27 14:35:53 +0900

    [Analysis API] Use module's content scope for package annotation search
    The annotation search scope previously included the content scope for
    all kinds of modules. Currently, there is no way to track dependencies
    between library JARs (see KT-61210 for library source modules, which is
    also applicable to library binaries). So, to properly resolve a library,
    its resolution scope includes fallback library dependencies – basically,
    all other libraries in the project.
    
    Now, if there are multiple versions of the same library in a project
    (JUnit 5 and 6, as in the issue), the scope will include
    'package-info.class' from both of them, unexpectedly making new package
    annotations available to older versions. Previously,
    'package-info.class' files were ignored by the
    'LLFirJavaAnnotationProvider' (only .java was supported), but that was
    fixed in KT-83467, and KT-82049 resurrected.
    
    This fix limits the annotation search scope for libraries to the library
    JAR itself. While there might be cases where a 'package-info.class' is
    shared between multiple JARs of the same library (e.g., asm-util vs.
    asm-tree are basically the same library), cross-module package sharing
    is generally discouraged in Java.
    
    Search scope for sources isn't changed in this commit in attempt to make
    the change less risky.
    
    ^KT-83867 Fixed

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirAbstractSessionFactory.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/LLFirAbstractSessionFactory.kt
@@ -376,13 +376,13 @@ internal abstract class LLFirAbstractSessionFactory(protected val project: Proje
         components.session = session
 
         val moduleData = createModuleData(session)
-        val resolutionScope = resolutionScopeProvider.getResolutionScope(module)
+        val binaryContentScope = binaryModule.contentScope
 
         return session.apply {
             registerModuleData(moduleData)
             register(FirKotlinScopeProvider::class, scopeProvider)
 
-            registerAllCommonComponents(languageVersionSettings, module, resolutionScope)
+            registerAllCommonComponents(languageVersionSettings, module, binaryContentScope)
             registerCommonComponentsAfterExtensionsAreConfigured()
 
             val firProvider = LLFirProvider(
@@ -397,10 +397,8 @@ internal abstract class LLFirAbstractSessionFactory(protected val project: Proje
 
             register(FirLazyDeclarationResolver::class, LLFirLazyDeclarationResolver())
 
-            val contentScope = binaryModule.contentScope
-
             // We need FirRegisteredPluginAnnotations during extensions' registration process
-            val annotationsResolver = project.createAnnotationResolver(contentScope)
+            val annotationsResolver = project.createAnnotationResolver(binaryContentScope)
             register(FirRegisteredPluginAnnotations::class, LLFirIdeRegisteredPluginAnnotations(this, annotationsResolver))
             register(FirPredicateBasedProvider::class, FirEmptyPredicateBasedProvider)
 
@@ -429,7 +427,7 @@ internal abstract class LLFirAbstractSessionFactory(protected val project: Proje
 
             register(DEPENDENCIES_SYMBOL_PROVIDER_QUALIFIED_KEY, dependencyProvider)
 
-            val context = LibrarySessionCreationContext(contentScope, firProvider, dependencyProvider)
+            val context = LibrarySessionCreationContext(binaryContentScope, firProvider, dependencyProvider)
             additionalSessionConfiguration(context)
 
             LLFirSessionConfigurator.configure(this)
@@ -453,11 +451,12 @@ internal abstract class LLFirAbstractSessionFactory(protected val project: Proje
         val session = LLFirLibrarySession(module, builtinsSession.builtinTypes)
 
         val moduleData = createModuleData(session)
+        val contentScope = module.contentScope
 
         return session.apply {
             val languageVersionSettings = KotlinProjectStructureProvider.getInstance(project).libraryLanguageVersionSettings
             registerModuleData(moduleData)
-            registerIdeComponents(project, languageVersionSettings, resolutionScopeProvider.getResolutionScope(module))
+            registerIdeComponents(project, languageVersionSettings, contentScope)
             register(FirLazyDeclarationResolver::class, FirDummyCompilerLazyDeclarationResolver)
             registerCommonComponents(languageVersionSettings, isMetadataCompilation = false)
             registerCommonComponentsAfterExtensionsAreConfigured()
@@ -471,7 +470,7 @@ internal abstract class LLFirAbstractSessionFactory(protected val project: Proje
 
             val symbolProvider = LLModuleWithDependenciesSymbolProvider(
                 this,
-                providers = createProjectLibraryProvidersForScope(this, module.contentScope),
+                providers = createProjectLibraryProvidersForScope(this, contentScope),
                 LLDependenciesSymbolProvider(this) {
                     // A binary library session should not have any dependencies (apart from fallback builtins), as library module
                     // dependencies only apply to *resolvable* sessions, including fallback dependencies.
@@ -718,9 +717,9 @@ internal abstract class LLFirAbstractSessionFactory(protected val project: Proje
     private fun LLFirSession.registerAllCommonComponents(
         languageVersionSettings: LanguageVersionSettings,
         module: KaModule,
-        resolutionScope: GlobalSearchScope,
+        annotationSearchScope: GlobalSearchScope,
     ) {
-        registerIdeComponents(project, languageVersionSettings, resolutionScope)
+        registerIdeComponents(project, languageVersionSettings, annotationSearchScope)
         registerCommonComponents(languageVersionSettings, isMetadataCompilation = false)
         registerResolveComponents(KtRegisteredDiagnosticFactoriesStorage())
     }
