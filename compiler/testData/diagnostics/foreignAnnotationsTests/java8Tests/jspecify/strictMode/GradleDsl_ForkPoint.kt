8f22f8a1463a09d19b5019208609600f3ea04fbb
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-01-28 11:06:34 +0100

    K2: reproduce KT-83824

---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/jspecify/strictMode/GradleDsl_ForkPoint.kt
@@ -0,0 +1,177 @@
+// ISSUE: KT-83824
+// JSPECIFY_STATE: strict
+// FIR_DUMP
+// LANGUAGE: +CheckPackageInfoNullnessAnnotations
+
+// FILE: api/action/Action.java
+
+package api.action;
+
+public interface Action<A> {
+    void execute(A t);
+}
+
+// FILE: api/package-info.java
+
+@NullMarked
+package api;
+
+import org.jspecify.annotations.*;
+
+// FILE: api/Owner.java
+
+package api;
+
+import artifacts.*;
+
+public interface Owner {
+    ConfigurationContainer getConfigurations();
+}
+
+// FILE: artifacts/package-info.java
+
+@NullMarked
+package artifacts;
+
+import org.jspecify.annotations.*;
+
+// FILE: artifacts/ConfigurationContainer.java
+
+package artifacts;
+
+import api.*;
+
+public interface ConfigurationContainer extends NamedDomainObjectContainer<Configuration> {
+
+}
+
+// FILE: artifacts/Configuration.java
+
+package artifacts;
+
+import attributes.*;
+
+public interface Configuration extends HasConfigurableAttributes<Configuration> {}
+
+// FILE: attributes/HasConfigurableAttributes.java
+
+package attributes;
+
+public interface HasConfigurableAttributes<SELF> {}
+
+// FILE: api/NamedDomainObjectContainer.java
+
+package api;
+
+public interface NamedDomainObjectContainer<NC> {
+    NamedDomainObjectProvider<NC> register(String name);
+}
+
+// FILE: api/NamedDomainObjectProvider.java
+
+package api;
+
+public interface NamedDomainObjectProvider<NP> {}
+
+// FILE: file/ConfigurableFileCollection.java
+
+package file;
+
+public interface ConfigurableFileCollection {}
+
+// FILE: dsl.kt
+
+package dsl
+
+import kotlin.reflect.*
+import api.*
+import api.action.*
+import artifacts.*
+import file.*
+
+operator fun ConfigurationContainer.invoke(configuration: Action<ConfigurationContainerScope>): ConfigurationContainer = apply {}
+
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <IT : Any, IC : NamedDomainObjectContainer<IT>> IC.invoke(configuration: Action<NamedDomainObjectContainerScope<IT>>): IC = this
+
+inline val <RT : Any, RC : NamedDomainObjectContainer<RT>> RC.registering: RegisteringDomainObjectDelegateProvider<out RC>
+    get() = RegisteringDomainObjectDelegateProvider.of(this)
+
+class RegisteringDomainObjectDelegateProvider<RP> private constructor(val delegateProvider: RP) {
+    companion object {
+        fun <RPO> of(delegateProvider: RPO) = RegisteringDomainObjectDelegateProvider(delegateProvider)
+    }
+}
+
+class ExistingDomainObjectDelegate<ED> private constructor(val delegate: ED) {
+    companion object {
+        fun <EDO> of(delegate: EDO) = ExistingDomainObjectDelegate(delegate)
+    }
+}
+
+class ConfigurationContainerScope private constructor(
+    override val delegate: ConfigurationContainer
+) : NamedDomainObjectContainerScope<Configuration>(delegate), ConfigurationContainer {
+    companion object {
+        fun of(container: ConfigurationContainer) = ConfigurationContainerScope(container)
+    }
+}
+
+open class NamedDomainObjectContainerScope<NS : Any> internal constructor(
+    override val delegate: NamedDomainObjectContainer<NS>
+) : NamedDomainObjectContainerDelegate<NS>()
+
+abstract class NamedDomainObjectContainerDelegate<ND : Any> : NamedDomainObjectContainer<ND> {
+    internal abstract val delegate: NamedDomainObjectContainer<ND>
+
+    override fun register(name: String): NamedDomainObjectProvider<ND> = delegate.register(name)
+}
+
+operator fun <PDT : Any, PDC : NamedDomainObjectContainer<PDT>> RegisteringDomainObjectDelegateProvider<PDC>.provideDelegate(
+    receiver: Any?,
+    property: KProperty<*>
+) = ExistingDomainObjectDelegate.of(delegateProvider.register(property.name))
+
+operator fun <GV> ExistingDomainObjectDelegate<out GV>.getValue(receiver: Any?, property: KProperty<*>): GV = delegate
+
+// FILE: test.kt
+
+package dsl
+
+import api.Owner
+import api.NamedDomainObjectProvider
+import artifacts.Configuration
+
+fun Owner.foo() {
+    configurations.invoke /* (Action<ConfigurationContainerScope>) */ { /* it: ConfigurationContainerScope! */
+        // class ConfigurationContainerScope : NamedDomainObjectContainerScope<Configuration>
+        // class NamedDomainObjectContainerScope<T : Any> : NamedDomainObjectContainerDelegate<T>
+        // class NamedDomainObjectContainerDelegate<T : Any> : NamedDomainObjectContainer<T>
+        val valley: NamedDomainObjectProvider<Configuration> by it.registering
+        /* it.registering<Configuration, ConfigurationContainerScope!> : RegisteringDomainObjectDelegateProvider<captured(out ConfigurationContainerScope!)>
+              .provideDelegate<Configuration, captured(out ConfigurationContainerScope!)> : ExistingDomainObjectDelegate<ConfigurationContainerScope!>
+         */
+        it.registering.provideDelegate(null, ::<!UNSUPPORTED("References to variables aren't supported yet")!>valley<!>).hashCode()
+        with(it.registering) {
+            provideDelegate(null, ::<!UNSUPPORTED!>valley<!>).hashCode()
+        }
+    }
+}
+
+// provideDelegate
+// actualType = RegisteringDomainObjectDelegatProvide<CapturedType(out RC)>
+// expectedType = RegisteringDomainObjectDelegateProvider<PDC>
+
+// RT <: Any
+// RC <: NamedDomainObjectContainer<RT>
+// ConfigurationContainerScope! <: RC
+// PDT : Any
+// PDC <: NamedDomainObjectContainer<PDT>
+// RegisteringDomainObjectDelegateProvider<CapturedType(out RC)> <: RegisteringDomainObjectDelegateProvider<PDC>
+
+// Incorporate: ConfigurationContainerScope! <: CapturedType(out TypeVariable(RC)) ==> FAIL
+
+/* GENERATED_FIR_TAGS: capturedType, classDeclaration, companionObject, flexibleType, funWithExtensionReceiver,
+functionDeclaration, getter, javaProperty, javaType, lambdaLiteral, localProperty, nullableType, objectDeclaration,
+operator, outProjection, override, primaryConstructor, propertyDeclaration, propertyDelegate,
+propertyWithExtensionReceiver, samConversion, starProjection, thisExpression, typeConstraint, typeParameter */
---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/java8Tests/jspecify/strictMode/GradleDsl_ForkPoint_Simplified.kt
@@ -0,0 +1,59 @@
+// ISSUE: KT-83824
+// JSPECIFY_STATE: strict
+// LANGUAGE: +CheckPackageInfoNullnessAnnotations
+
+// FILE: api/package-info.java
+
+@NullMarked
+package api;
+
+import org.jspecify.annotations.NullMarked;
+
+// FILE: api/JavaInvOfStringInheritor.java
+
+package api;
+
+public interface JavaInvOfStringInheritor extends JavaInv<String> {}
+
+// FILE: api/JavaInv.java
+
+package api;
+
+public interface JavaInv<NC> {}
+
+// FILE: test.kt
+
+package dsl
+
+import kotlin.reflect.*
+import api.*
+
+class Inv1<RP>
+class Inv2<ED>
+
+class MixOfJavaInvStringAndInheritor : JavaInv<String>, JavaInvOfStringInheritor
+
+/**
+ * RT <: Any
+ * RC <: JavaInv<RT>
+ * MixOfJavaInvStringAndInheritor! <: RC
+ * PDT <: Any
+ * PDC <: JavaInv<PDT>
+ * Inv1<CapturedType(out RC)> <: Inv1<PDC>
+ *
+ * PDC = CapturedType(out RC)
+ */
+
+val <RT : Any, RC : JavaInv<RT>> RC.registering: Inv1<out RC>
+    get() = TODO()
+
+operator fun <PDT : Any, PDC : JavaInv<PDT>> Inv1<PDC>.provideDelegate(
+    receiver: Any?,
+    property: KProperty<*>
+): Inv2<Collection<PDT>> = TODO()
+
+operator fun <GV> Inv2<out GV>.getValue(receiver: Any?, property: KProperty<*>): GV = TODO()
+
+fun foo(t: MixOfJavaInvStringAndInheritor) {
+    val valley123: Collection<String> by t.registering
+}
