d55d62846f218bf98afd466a919f3b7c927597a6
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2026-01-28 11:50:50 +0100

    [K/N][build] Exclude local MSVC when using MSVC from internal server
    When building the K/N distribution for Windows, native code is built
    to compiler's JNI libraries using MSVC+Clang+lld-link. To simplify the
    environment configuration, there is a possibility to download MSVC and
    Windows SDK from the internal server. It can be enabled using the env
    var `KONAN_USE_INTERNAL_SERVER=1`.
    
    When calling clang in this build mode, the build scripts added the
    downloaded directories using `-isystem` and `-L`. Clang also forwards
    the `-L` flags to `lld-link` as `-libpath:`.
    
    The problem is that clang and lld-link also try to locate local MSVC and
    Windows SDK installations by default, adding the paths from them to
    header and library search paths.
    In other words, the compilation ended up using a mix of local and
    downloaded MSVC and SDK, with the former having priority.
    
    This also led to the build being not reproducible: KT-82879.
    Despite using `KONAN_USE_INTERNAL_SERVER=1`, the dlls were linked
    against local MSVC and SDK, which could be of different versions.
    
    To address this problem, this commit passes the downloaded dependencies
    to clang with `-Xmicrosoft-visualc-tools-root` (equivalent to clang-cl's
    `/vctoolsdir`) and `-Xmicrosoft-windows-sdk-root` (equivalent to
    clang-cl's `/winsdkdir`). Additionally, since clang seemingly doesn't
    propagate those to lld-link, this commit also passes to clang explicit
    `-Wl,-vctoolsdir` and `-Wl,-winsdkdir` to address the problem with the
    linker behavior as well.
    
    Since the layout of the "Windows SDK" artifact from the internal server
    doesn't match the actual Windows SDK layout, this commit also applies
    some flag tricks to close the gap.
    
    The K/N build uses the bootstrap version of the changed code. So the
    issue will get actually fixed only after the next bootstrap update.
    
    ^KT-82879

--- native/utils/src/org/jetbrains/kotlin/konan/target/Windows.kt
+++ native/utils/src/org/jetbrains/kotlin/konan/target/Windows.kt
@@ -16,13 +16,18 @@ class MingwConfigurablesImpl(target: KonanTarget, properties: Properties, depend
     KonanPropertiesLoader(target, properties, dependenciesRoot, progressCallback = progressCallback) {
     override val windowsKit: WindowsKit by lazy {
         when (windowsSdkPartsProvider) {
-            WindowsSdkPartsProvider.InternalServer -> createCustomWindowsKitPath(Paths.get(absolute(windowsKitParts)))
+            WindowsSdkPartsProvider.InternalServer -> WindowsKit.CustomPath(
+                Paths.get(absolute(windowsKitParts)),
+                target
+            )
             WindowsSdkPartsProvider.Local -> WindowsKit.DefaultPath
         }
     }
     override val msvc: Msvc by lazy {
         when (windowsSdkPartsProvider) {
-            WindowsSdkPartsProvider.InternalServer -> createCustomMsvcPath(Paths.get(absolute(msvcParts)))
+            WindowsSdkPartsProvider.InternalServer -> Msvc.CustomPath(
+                Paths.get(absolute(msvcParts))
+            )
             WindowsSdkPartsProvider.Local -> Msvc.DefaultPath
         }
     }
@@ -50,29 +55,6 @@ class MingwConfigurablesImpl(target: KonanTarget, properties: Properties, depend
         }
 }
 
-private fun createCustomWindowsKitPath(windowsKitParts: Path): WindowsKit.CustomPath {
-    return WindowsKit.CustomPath(
-            libraryDirectories = listOf(
-                    windowsKitParts.resolve("Lib").resolve("ucrt").resolve("x64"),
-                    windowsKitParts.resolve("Lib").resolve("um").resolve("x64")
-            ),
-            includeDirectories = listOf(
-                    windowsKitParts.resolve("Include").resolve("ucrt")
-            )
-    )
-}
-
-private fun createCustomMsvcPath(msvcParts: Path): Msvc.CustomPath {
-    return Msvc.CustomPath(
-            libraryDirectories = listOf(
-                    msvcParts.resolve("lib").resolve("x64")
-            ),
-            includeDirectories = listOf(
-                    msvcParts.resolve("include")
-            )
-    )
-}
-
 sealed class Msvc {
 
     abstract fun compilerFlags(): List<String>
@@ -81,16 +63,46 @@ sealed class Msvc {
         override fun compilerFlags(): List<String> = emptyList()
     }
 
-    class CustomPath(
-            private val includeDirectories: List<Path>,
-            private val libraryDirectories: List<Path>
-    ) : Msvc() {
-        // Note that this approach doesn't exclude default VS path.
-        // TODO: A better (but harder) way would be LIB environment variable.
-        override fun compilerFlags(): List<String> =
-                includeDirectories.flatMap { listOf("-isystem", it.toAbsolutePath().toString()) } +
-                        libraryDirectories.flatMap { listOf("-L", it.toAbsolutePath().toString()) }
+    class CustomPath(private val msvcParts: Path) : Msvc() {
+        override fun compilerFlags(): List<String> = buildList {
+            /*
+            By default, Clang would try to find a local installation of MSVC and add its contents
+            to library and header search paths, e.g.
+            * https://github.com/llvm/llvm-project/blob/32cd703da578e769787a921d76b768164a4256b6/clang/lib/Driver/ToolChains/MSVC.cpp#L445-L455
+            * https://github.com/llvm/llvm-project/blob/32cd703da578e769787a921d76b768164a4256b6/clang/lib/Driver/ToolChains/MSVC.cpp#L112-L121
+            * https://github.com/llvm/llvm-project/blob/32cd703da578e769787a921d76b768164a4256b6/clang/lib/Driver/ToolChains/MSVC.cpp#L700-L705
 
+            Since the goal of `CustomPath` is to make Clang work independently of any local MSVC
+            installations, this is a problem.
+
+            The easiest way to disable that behavior is to pass `-Xmicrosoft-visualc-tools-root`.
+            The flag is undocumented and is not widely known, but it is basically equivalent to
+            `/vctoolsdir` (available when using an alternative Clang driver for Windows, `clang-cl`):
+            * https://clang.llvm.org/docs/UsersManual.html#windows-system-headers-and-library-lookup
+            * https://github.com/llvm/llvm-project/blob/8ebbcfb4e326abb71e80fad05951e2e74ae7f64a/clang/include/clang/Driver/Options.td#L8967
+
+            For Kotlin, it is easier to use the same Clang driver for all targets, hence the need
+            for the undocumented flag.
+
+            `msvcParts` is passed as the flag value. Technically, it is not an MSVC installation
+            (while the flag expects one), but the layout of the important parts matches, so this
+            also makes Clang automatically add the directories within `msvcParts` as header and
+            library search paths.
+
+            `/vctoolsversion` is not passed, but it doesn't seem to have any effect here.
+             */
+            val pathStr = msvcParts.toAbsolutePath().toString()
+            addAll(listOf("-Xmicrosoft-visualc-tools-root", pathStr))
+
+            /*
+            Apparently, the linker (`lld-link`) also has the same kind of behavior (adding local
+            MSVC paths automatically), despite having the same paths passed from the compiler driver:
+            https://github.com/llvm/llvm-project/blob/615b7eeaa94d7c2d2c782fcdc21de5b62f3c168e/lld/COFF/Driver.cpp#L665-L669
+
+            Workaround is basically the same: pass `-vctoolsdir` to the linker.
+             */
+            add("-Wl,-vctoolsdir:$pathStr")
+        }
     }
 }
 
@@ -101,16 +113,49 @@ sealed class WindowsKit {
         override fun compilerFlags(): List<String> = emptyList()
     }
 
-    class CustomPath(
-            private val includeDirectories: List<Path>,
-            private val libraryDirectories: List<Path>
-    ) : WindowsKit() {
-        // Note that this approach doesn't exclude default Windows Kit path.
-        // TODO: A better (but harder) way would be LIB environment variable.
-        override fun compilerFlags(): List<String> =
-                includeDirectories.flatMap { listOf("-isystem", it.toAbsolutePath().toString()) } +
-                        libraryDirectories.flatMap { listOf("-L", it.toAbsolutePath().toString()) }
+    class CustomPath(private val windowsKitParts: Path, val target: KonanTarget) : WindowsKit() {
+        override fun compilerFlags(): List<String> = buildList {
+            /*
+            See the comments in [MSVC.CustomPath.compilerFlags].
 
+            Additional source code references:
+            * https://github.com/llvm/llvm-project/blob/32cd703da578e769787a921d76b768164a4256b6/clang/lib/Driver/ToolChains/MSVC.cpp#L125-L134
+            * https://github.com/llvm/llvm-project/blob/32cd703da578e769787a921d76b768164a4256b6/clang/lib/Driver/ToolChains/MSVC.cpp#L707-L718
+            * https://github.com/llvm/llvm-project/blob/8ebbcfb4e326abb71e80fad05951e2e74ae7f64a/clang/include/clang/Driver/Options.td#L8970
+            * https://github.com/llvm/llvm-project/blob/615b7eeaa94d7c2d2c782fcdc21de5b62f3c168e/lld/COFF/Driver.cpp#L671-L683
+
+            clang's `-Xmicrosoft-windows-sdk-root` is equivalent to clang-cl's `/winsdkdir`.
+             */
+            val pathStr = windowsKitParts.toAbsolutePath().toString()
+            addAll(listOf("-Xmicrosoft-windows-sdk-root", pathStr))
+            add("-Wl,-winsdkdir:$pathStr")
+
+            /*
+            Unlike with MSVC, the layout of `windowsKitParts` doesn't match the layout of the expected
+            Windows SDK root. This problem is tracked in KT-83978.
+            `windowsKitParts` has:
+            * `Include/ucrt`
+            * `Lib/ucrt/$arch`
+            * `Lib/um/$arch`
+
+            Windows SDK has instead:
+            * `Include/$version/ucrt`
+            * `Lib/$version/ucrt/$arch`
+            * `Lib/$version/um/$arch`
+
+            By not passing the SDK version, this code tricks Clang into omitting the version from the paths.
+            But then it also omits `um` from the lib path:
+            https://github.com/llvm/llvm-project/blob/615b7eeaa94d7c2d2c782fcdc21de5b62f3c168e/lld/COFF/Driver.cpp#L629-L630
+
+            As a simple workaround, just pass the proper path explicitly:
+             */
+            val arch = when (target) {
+                KonanTarget.MINGW_X64 -> "x64"
+                else -> error("WindowsKit.CustomPath supports only mingw_x64 for now.")
+            }
+            val libUmPath = windowsKitParts.resolve("Lib").resolve("um").resolve(arch)
+            addAll(listOf("-L", libUmPath.toAbsolutePath().toString()))
+        }
     }
 }
 
