ced3a513de166a0f844ed7079019921e06951dc6
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-01-28 16:22:12 +0100

    fixup! [FIR] Change how annotations are rendered

--- analysis/analysis-api/testData/components/resolver/singleByPsi/variableAsFunctionWithParameterNameInNonFunctionType.kt
+++ analysis/analysis-api/testData/components/resolver/singleByPsi/variableAsFunctionWithParameterNameInNonFunctionType.kt
@@ -1,3 +1,4 @@
+// IGNORE_FE10
 // @ParameterName annotation is ignored when not used in function type
 fun call(a: @ParameterName("notMe") Int, b: @ParameterName("meNeither") String) {
     <expr>call(1, "")</expr>
