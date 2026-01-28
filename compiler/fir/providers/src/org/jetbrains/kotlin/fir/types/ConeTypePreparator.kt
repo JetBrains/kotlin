48213ce1656eab5d2211e609456f5f2f1bb52476
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-01-28 11:25:35 +0100

    Type checker: filter equivalent supertypes at a fork point more aggressively
    RCA: while checking subtyping in the type checker, it may happen
    that we find two or more matching supertypes with the same constructor
    but different type arguments, and create a forking point to be able
    to check all opportunities. However, forking points make the analysis
    more complex and less stable, and it makes no sense to create them
    for trivial cases like the test 'GradleDsl_ForkPoint_Simplified':
    after the fix of KT-83031 we find here two separate supertypes,
    JavaInv<String> and JavaInv<@EnhancedNullability String>.
    Before the fix of KT-83031 they were counted equivalent, but after the
    fix they became non-equivalent, despite the fact they are really
    equivalent from the point of view of the type checker
    (EnhancedNullability does not influence its work). To fix this situation,
    in this commit we filter out EnhancedNullability before forking, thus
    avoiding creation of a forking point and returning to pre-KT-83031 behavior.
    As a possible next step, we could investigate why the tests don't work
    correctly in case a forking point is created, as normally it shouldn't
    influence semantics (KT-83965).
    
    #KT-83824 Fixed

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/types/ConeTypePreparator.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/types/ConeTypePreparator.kt
@@ -9,6 +9,8 @@ import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.resolve.fullyExpandedType
 import org.jetbrains.kotlin.types.AbstractTypePreparator
 import org.jetbrains.kotlin.types.model.KotlinTypeMarker
+import org.jetbrains.kotlin.types.model.RigidTypeMarker
+import org.jetbrains.kotlin.types.model.projection
 
 class ConeTypePreparator(val session: FirSession) : AbstractTypePreparator() {
     private fun <T : ConeRigidType> prepareType(type: T): T {
@@ -28,5 +30,56 @@ class ConeTypePreparator(val session: FirSession) : AbstractTypePreparator() {
             is ConeFlexibleType -> type.mapTypesOrSelf(session.typeContext, dropIdentity = true) { prepareType(it) }
             is ConeRigidType -> prepareType(type)
         }
+    }
+
+    override fun clearTypeFromUnnecessaryAttributes(type: RigidTypeMarker): RigidTypeMarker {
+        return (type as ConeRigidType).dropEnhancedNullability()
+    }
+
+    private fun ConeKotlinType.dropEnhancedNullability(): ConeKotlinType {
+        when (this) {
+            is ConeFlexibleType -> {
+                val lowerBound = lowerBound.dropEnhancedNullability()
+                val upperBound = upperBound.dropEnhancedNullability()
+                if (lowerBound === this.lowerBound && upperBound === this.upperBound) return this
+                if (this is ConeRawType) return ConeRawType.create(lowerBound, upperBound)
+                return ConeFlexibleType(lowerBound, upperBound, isTrivial = this.isTrivial)
+            }
+            is ConeIntersectionType -> {
+                val intersectedTypes = intersectedTypes.map { it.dropEnhancedNullability() }
+                if (intersectedTypes.zip(this.intersectedTypes).all { (a, b) -> a === b }) return this
+                return ConeIntersectionType(
+                    intersectedTypes,
+                    upperBoundForApproximation?.dropEnhancedNullability()
+                )
+            }
+            is ConeCapturedType -> {
+                val constructor = constructor
+                val lowerType = constructor.lowerType?.dropEnhancedNullability()
+                val projection = constructor.projection.dropEnhancedNullability()
+                val newConstructor =
+                    if (lowerType === constructor.lowerType && projection === constructor.projection) constructor
+                    else ConeCapturedTypeConstructor(
+                        projection = projection, lowerType = lowerType, captureStatus = constructor.captureStatus
+                    )
+                val resultCapturedType =
+                    if (constructor === newConstructor) this
+                    else ConeCapturedType(isMarkedNullable, newConstructor, attributes)
+                return resultCapturedType.dropEnhancedNullability()
+            }
+            is ConeRigidType -> {
+                return dropEnhancedNullability()
+            }
+        }
+    }
+
+    private fun ConeRigidType.dropEnhancedNullability(): ConeRigidType {
+        return withAttributes(attributes.remove(CompilerConeAttributes.EnhancedNullability))
+            .withArguments { projection -> projection.dropEnhancedNullability() }
+    }
+
+    private fun ConeTypeProjection.dropEnhancedNullability(): ConeTypeProjection {
+        return if (this !is ConeKotlinTypeProjection) this
+        else replaceType(type.dropEnhancedNullability())
     }
 }
