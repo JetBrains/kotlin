34a73a2b06e33bc1c9504b69810cdc3a54a123f1
Igor Chevdar <igor.chevdar@jetbrains.com>
2026-01-29 20:04:05 +0200

    [K/N][codegen] Less strict requirement
    For the code
    ```
    class A(val x: Any)
    
    inline fun box(): String {
        val test = A(return "OK")
    }
    
    fun main() {
        val s = box()
        println(s)
    }
    ```
    there will be built `IrInlinedFunctionBlock` with type = String, while in reality
    it contains one statement which is a variable and thus has type Unit.
    
    For the time being (until this corner case is resolved) just make the requirement
    in codegen less strict.
    
     #KT-84000

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
@@ -2196,7 +2196,7 @@ internal class CodeGeneratorVisitor(
                 }
             }
 
-            assert(value.type.isUnit())
+            require(value is IrInlinedFunctionBlock || value.type.isUnit())
             return codegen.theUnitInstanceRef.llvm
         }
     }
