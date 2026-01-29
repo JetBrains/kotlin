dfaa88354d112e65891d791884d8ef2286418644
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-01-29 15:50:51 +0100

    [K/N][performance] Migrate to kotlinx-benchmark in objcinterop
    ^KT-83283

--- kotlin-native/performance/objcinterop/src/commonMain/kotlin/main.kt
+++ kotlin-native/performance/objcinterop/src/commonMain/kotlin/main.kt
@@ -3,36 +3,63 @@
  * that can be found in the LICENSE file.
  */
 
-
-import org.jetbrains.benchmarksLauncher.*
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.State
+import org.jetbrains.benchmarksLauncher.SkipWhenBaseOnly
 import org.jetbrains.complexNumbers.*
-import kotlinx.cli.*
 
-class ObjCInteropLauncher: Launcher() {
-    override val baseBenchmarksSet: MutableMap<String, AbstractBenchmarkEntry> = mutableMapOf(
-            "sumComplex" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { sumComplex() }),
-            "stringToObjC" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { stringToObjC() }),
-            "stringFromObjC" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { stringFromObjC() }),
-            "fft" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { fft() })
-    )
-    override val extendedBenchmarksSet: MutableMap<String, AbstractBenchmarkEntry> = mutableMapOf(
-            "generateNumbersSequence" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, {
-                @OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
-                generateNumbersSequence()
-            }),
-            "subComplex" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { subComplex() }),
-            "classInheritance" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { classInheritance() }),
-            "categoryMethods" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { categoryMethods() }),
-            "invertFft" to BenchmarkEntryWithInit.create(::ComplexNumbersBenchmark, { invertFft() })
-    )
-}
+@State(Scope.Benchmark)
+class ComplexNumbersBenchmarkHideName : SkipWhenBaseOnly() {
+    private val instance = ComplexNumbersBenchmark()
 
-fun main(args: Array<String>) {
-    val launcher = ObjCInteropLauncher()
-    BenchmarksRunner.runBenchmarks(args, { arguments: BenchmarkArguments ->
-        if (arguments is BaseBenchmarkArguments) {
-            launcher.launch(arguments.warmup, arguments.repeat, arguments.prefix,
-                    arguments.filter, arguments.filterRegex, arguments.verbose)
-        } else emptyList()
-    }, benchmarksListAction = launcher::benchmarksListAction)
+    @Benchmark
+    fun sumComplex() {
+        instance.sumComplex()
+    }
+
+    @Benchmark
+    fun stringToObjC() {
+        instance.stringToObjC()
+    }
+
+    @Benchmark
+    fun stringFromObjC() {
+        instance.stringFromObjC()
+    }
+
+    @Benchmark
+    fun fft() {
+        instance.fft()
+    }
+
+    @Benchmark
+    fun generateNumbersSequence() {
+        skipWhenBaseOnly()
+        instance.generateNumbersSequence()
+    }
+
+    @Benchmark
+    fun subComplex() {
+        skipWhenBaseOnly()
+        instance.subComplex()
+    }
+
+    @Benchmark
+    fun classInheritance() {
+        skipWhenBaseOnly()
+        instance.classInheritance()
+    }
+
+    @Benchmark
+    fun categoryMethods() {
+        skipWhenBaseOnly()
+        instance.categoryMethods()
+    }
+
+    @Benchmark
+    fun invertFft() {
+        skipWhenBaseOnly()
+        instance.invertFft()
+    }
 }
