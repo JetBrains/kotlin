2d0923c9d47463a406c8a3eef2eb500ebd15ff34
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-01-29 13:03:35 +0100

    [test] Enable using the `webMain` stdlib source set in HMPP tests
    We need to be able to reference the `webMain` source set of
    the standard library in HMPP tests. For this, we need to actually
    build the metadata KLIB for that source set, which we didn't do before.
    
    (See the next commit for an example of such a test.)

--- core/compiler.common/src/org/jetbrains/kotlin/platform/WasmPlatform.kt
+++ core/compiler.common/src/org/jetbrains/kotlin/platform/WasmPlatform.kt
@@ -1,13 +1,16 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.platform
 
-abstract class WasmPlatform(platformName: String) : SimplePlatform(platformName) {
+abstract class WasmPlatform(platformName: String) : SimplePlatform(platformName), PotentiallyWebPlatform {
     override val oldFashionedDescription: String
         get() = "Wasm "
+
+    override val isWeb: Boolean
+        get() = true
 }
 
 fun TargetPlatform?.isWasm(): Boolean = this?.singleOrNull() is WasmPlatform
