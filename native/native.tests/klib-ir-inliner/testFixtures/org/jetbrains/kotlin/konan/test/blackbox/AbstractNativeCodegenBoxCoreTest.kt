f3cbf59fca040406cbde53a7e24bcf941922e05b
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2026-01-30 12:55:27 +0100

    [Native][Tests] Rename NativeRunnerHandler to NativeBoxRunner
    ^KT-83769

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/AbstractNativeCodegenBoxCoreTest.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/AbstractNativeCodegenBoxCoreTest.kt
@@ -14,7 +14,7 @@ import org.jetbrains.kotlin.konan.test.blackbox.support.TestDirectives
 import org.jetbrains.kotlin.konan.test.configuration.commonConfigurationForNativeFirstStageUpToSerialization
 import org.jetbrains.kotlin.konan.test.converters.NativePreSerializationLoweringFacade
 import org.jetbrains.kotlin.konan.test.handlers.FileCheckHandler
-import org.jetbrains.kotlin.konan.test.handlers.NativeRunnerHandler
+import org.jetbrains.kotlin.konan.test.handlers.NativeBoxRunner
 import org.jetbrains.kotlin.konan.test.klib.NativeCompilerSecondStageFacade
 import org.jetbrains.kotlin.konan.test.klib.currentCustomNativeCompilerSettings
 import org.jetbrains.kotlin.konan.test.services.CInteropTestSkipper
@@ -80,7 +80,7 @@ abstract class AbstractNativeCodegenBoxCoreTest : AbstractNativeCoreTest() {
         // 2nd stage (klibs -> executable)
         facadeStep(::NativeCompilerSecondStageFacade.bind(currentCustomNativeCompilerSettings))
         nativeArtifactsHandlersStep {
-            useHandlers(::FileCheckHandler, ::NativeRunnerHandler)
+            useHandlers(::FileCheckHandler, ::NativeBoxRunner)
         }
 
         defaultDirectives {
