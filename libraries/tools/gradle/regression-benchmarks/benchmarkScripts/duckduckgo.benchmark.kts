d345fcd74bf495cc5faecd6b8c8a7404dca87501
Timofey Solonin <timofey.solonin@jetbrains.com>
2026-02-02 12:02:47 +0100

    fixup! Bump stable reference version to 2.3.0

--- libraries/tools/gradle/regression-benchmarks/benchmarkScripts/duckduckgo.benchmark.kts
+++ libraries/tools/gradle/regression-benchmarks/benchmarkScripts/duckduckgo.benchmark.kts
@@ -5,7 +5,7 @@
     name = "duckduckgo",
     gitUrl = "https://github.com/duckduckgo/Android.git",
     gitCommitSha = "0c100be84e7e91a6c053afd84cece44747bb64fb",
-    stableKotlinVersion = "2.2.21-RC2",
+    stableKotlinVersion = "2.3.0",
 )
 
 import java.io.File
