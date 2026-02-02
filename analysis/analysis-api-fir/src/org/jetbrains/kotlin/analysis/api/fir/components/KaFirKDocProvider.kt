8b75336a1959d2217866bee7028b3055bfaf7640
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-02-02 19:19:01 +0900

    [Analysis API] Add stub-based support in KaFirKDocProvider
    ^KT-83921

--- analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirKDocProvider.kt
+++ analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/components/KaFirKDocProvider.kt
@@ -7,7 +7,34 @@ package org.jetbrains.kotlin.analysis.api.fir.components
 
 import org.jetbrains.kotlin.analysis.api.fir.KaFirSession
 import org.jetbrains.kotlin.analysis.api.impl.base.components.KaBaseKDocProvider
+import org.jetbrains.kotlin.analysis.api.symbols.KaDeclarationSymbol
+import org.jetbrains.kotlin.analysis.api.symbols.KaSymbolOrigin
+import org.jetbrains.kotlin.analysis.low.level.api.fir.stubBased.deserialization.compiledStub
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtNonPublicApi
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.jetbrains.kotlin.psi.KtPrimaryConstructor
+import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtSecondaryConstructor
 
 internal class KaFirKDocProvider(
     override val analysisSessionProvider: () -> KaFirSession,
-) : KaBaseKDocProvider<KaFirSession>()
+) : KaBaseKDocProvider<KaFirSession>() {
+    @OptIn(KtNonPublicApi::class)
+    override fun findDeserializedKdocText(symbol: KaDeclarationSymbol): String? {
+        if (symbol.origin != KaSymbolOrigin.LIBRARY) {
+            return null
+        }
+
+        return when (val psi = symbol.psi) {
+            is KtClass -> psi.compiledStub.kdocText
+            is KtObjectDeclaration -> psi.compiledStub.kdocText
+            is KtNamedFunction -> psi.compiledStub.kdocText
+            is KtProperty -> psi.compiledStub.kdocText
+            is KtPrimaryConstructor -> psi.compiledStub.kdocText
+            is KtSecondaryConstructor -> psi.compiledStub.kdocText
+            else -> null
+        }
+    }
+}
