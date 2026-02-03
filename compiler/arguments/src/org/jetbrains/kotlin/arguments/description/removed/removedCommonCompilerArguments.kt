8ea02622ec1d736380f182de8a40a92194c78dd3
Iveta Kovalenko <iveta.kovalenko@jetbrains.com>
2026-02-03 14:51:46 +0100

    [SSoT] Deprecate valueType/Description for argumentType/Description
    ^KT-83794

--- compiler/arguments/src/org/jetbrains/kotlin/arguments/description/removed/removedCommonCompilerArguments.kt
+++ compiler/arguments/src/org/jetbrains/kotlin/arguments/description/removed/removedCommonCompilerArguments.kt
@@ -3,6 +3,8 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("DEPRECATION")
+
 package org.jetbrains.kotlin.arguments.description.removed
 
 import org.jetbrains.kotlin.arguments.description.CompilerArgumentsLevelNames
