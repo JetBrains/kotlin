bb750d0112922a63978546c8857ab9b2eb599363
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2026-02-03 10:24:46 +0100

    [Native] Merge FirFrontend.kt into NativeFrontendPhase
    It simplifies code a little bit and stores all Native frontend code
    in one place
    
    ^KT-81748

--- compiler/cli/cli-native-klib/src/main/kotlin/org/jetbrains/kotlin/native/pipeline/NativeFrontendPhase.kt
+++ compiler/cli/cli-native-klib/src/main/kotlin/org/jetbrains/kotlin/native/pipeline/NativeFrontendPhase.kt
@@ -5,13 +5,48 @@
 
 package org.jetbrains.kotlin.native.pipeline
 
+import org.jetbrains.kotlin.KtSourceFile
+import org.jetbrains.kotlin.analyzer.CompilationErrorException
+import org.jetbrains.kotlin.backend.konan.KonanConfigKeys
+import org.jetbrains.kotlin.backend.konan.driver.PhaseContext
+import org.jetbrains.kotlin.cli.common.collectSources
+import org.jetbrains.kotlin.cli.common.fileBelongsToModuleForLt
+import org.jetbrains.kotlin.cli.common.fileBelongsToModuleForPsi
+import org.jetbrains.kotlin.cli.common.fir.FirDiagnosticsCompilerResultsReporter
+import org.jetbrains.kotlin.cli.common.isCommonSourceForLt
+import org.jetbrains.kotlin.cli.common.isCommonSourceForPsi
+import org.jetbrains.kotlin.cli.common.messages.AnalyzerWithCompilerReport
+import org.jetbrains.kotlin.cli.common.prepareNativeSessions
+import org.jetbrains.kotlin.cli.common.renderDiagnosticInternalName
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.compiler.toVfsBasedProjectEnvironment
 import org.jetbrains.kotlin.cli.pipeline.CheckCompilationErrors
+import org.jetbrains.kotlin.cli.pipeline.FrontendFilesForPluginsGenerationPipelinePhase
 import org.jetbrains.kotlin.cli.pipeline.PerformanceNotifications
 import org.jetbrains.kotlin.cli.pipeline.PipelinePhase
+import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
+import org.jetbrains.kotlin.compiler.plugin.getCompilerExtensions
+import org.jetbrains.kotlin.config.CommonConfigurationKeys
+import org.jetbrains.kotlin.diagnostics.DiagnosticReporterFactory
+import org.jetbrains.kotlin.diagnostics.impl.BaseDiagnosticsCollector
+import org.jetbrains.kotlin.fir.DependencyListForCliModule
+import org.jetbrains.kotlin.fir.FirBinaryDependenciesModuleData
+import org.jetbrains.kotlin.fir.FirModuleCapabilities
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.SessionConfiguration
+import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
+import org.jetbrains.kotlin.fir.pipeline.AllModulesFrontendOutput
+import org.jetbrains.kotlin.fir.pipeline.SingleModuleFrontendOutput
+import org.jetbrains.kotlin.fir.pipeline.buildResolveAndCheckFirFromKtFiles
+import org.jetbrains.kotlin.fir.pipeline.buildResolveAndCheckFirViaLightTree
+import org.jetbrains.kotlin.fir.pipeline.runPlatformCheckers
+import org.jetbrains.kotlin.fir.render
+import org.jetbrains.kotlin.fir.resolve.ImplicitIntegerCoercionModuleCapability
+import org.jetbrains.kotlin.library.metadata.isCInteropLibrary
+import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.native.FirOutput
 import org.jetbrains.kotlin.native.NativePhaseContext
 import org.jetbrains.kotlin.native.createNativeKlibConfig
-import org.jetbrains.kotlin.native.firFrontend
 
 object NativeFrontendPhase : PipelinePhase<NativeConfigurationArtifact, NativeFrontendArtifact>(
     name = "NativeFrontendPhase",
@@ -35,5 +70,131 @@ object NativeFrontendPhase : PipelinePhase<NativeConfigurationArtifact, NativeFr
                 phaseContext = phaseContext,
             )
         }
+    }
+
+    @OptIn(SessionConfiguration::class, ExperimentalCompilerApi::class)
+    private inline fun <F> PhaseContext.firFrontend(
+        input: KotlinCoreEnvironment,
+        files: List<F>,
+        fileHasSyntaxErrors: (F) -> Boolean,
+        noinline isCommonSource: (F) -> Boolean,
+        noinline fileBelongsToModule: (F, String) -> Boolean,
+        buildResolveAndCheckFir: (FirSession, List<F>, BaseDiagnosticsCollector) -> SingleModuleFrontendOutput,
+    ): FirOutput {
+        val configuration = input.configuration
+        val messageCollector = configuration.getNotNull(CommonConfigurationKeys.MESSAGE_COLLECTOR_KEY)
+        val extensionRegistrars = configuration.getCompilerExtensions(FirExtensionRegistrar)
+        val diagnosticsReporter = DiagnosticReporterFactory.createPendingReporter()
+        val renderDiagnosticNames = configuration.renderDiagnosticInternalName
+
+        // FIR
+        val mainModuleName = Name.special("<${config.moduleId}>")
+        val syntaxErrors = files.fold(false) { errorsFound, file -> fileHasSyntaxErrors(file) or errorsFound }
+        val dependencyList = DependencyListForCliModule.build {
+            val (interopLibs, regularLibs) = config.resolvedLibraries.getFullList().partition { it.isCInteropLibrary() }
+            defaultDependenciesSet(mainModuleName) {
+                dependencies(regularLibs.map { it.libraryFile.absolutePath })
+                friendDependencies(config.friendModuleFiles.map { it.absolutePath })
+                dependsOnDependencies(config.refinesModuleFiles.map { it.absolutePath })
+            }
+            if (interopLibs.isNotEmpty()) {
+                val interopModuleData =
+                    FirBinaryDependenciesModuleData(Name.special("<regular interop dependencies of $mainModuleName>"),
+                                                    FirModuleCapabilities.create(listOf(ImplicitIntegerCoercionModuleCapability))
+                    )
+                dependencies(interopModuleData, interopLibs.map { it.libraryFile.absolutePath })
+            }
+            // TODO: !!! dependencies module data?
+        }
+        val resolvedLibraries = config.resolvedLibraries.getFullResolvedList().map { it.library }
+
+        val sessionsWithSources = prepareNativeSessions(
+            files,
+            configuration,
+            mainModuleName,
+            resolvedLibraries,
+            dependencyList,
+            extensionRegistrars,
+            metadataCompilationMode = config.metadataKlib,
+            isCommonSource = isCommonSource,
+            fileBelongsToModule = fileBelongsToModule,
+        )
+
+        val outputs = sessionsWithSources.map { (session, sources) ->
+            buildResolveAndCheckFir(session, sources, diagnosticsReporter).also {
+                if (config.configuration.getBoolean(KonanConfigKeys.PRINT_FILES)) {
+                    it.fir.forEach { file -> println(file.render()) }
+                }
+            }
+        }
+
+        outputs.runPlatformCheckers(diagnosticsReporter)
+        // This seems rudimental, but it fixes the error reporting in the case of one-stage compilation
+        FirDiagnosticsCompilerResultsReporter.reportToMessageCollector(diagnosticsReporter, messageCollector, renderDiagnosticNames)
+        return if (syntaxErrors || diagnosticsReporter.hasErrors) {
+            throw CompilationErrorException("Compilation failed: there were frontend errors")
+        } else {
+            FirOutput.Full(AllModulesFrontendOutput(outputs))
+        }
+    }
+
+    private fun PhaseContext.firFrontendWithPsi(input: KotlinCoreEnvironment): FirOutput {
+        val configuration = input.configuration
+        val messageCollector = configuration.getNotNull(CommonConfigurationKeys.MESSAGE_COLLECTOR_KEY)
+        // FIR
+
+        val ktFiles = input.getSourceFiles()
+        return firFrontend(
+            input,
+            ktFiles,
+            fileHasSyntaxErrors = {
+                AnalyzerWithCompilerReport.reportSyntaxErrors(it, messageCollector).isHasErrors
+            },
+            isCommonSource = isCommonSourceForPsi,
+            fileBelongsToModule = fileBelongsToModuleForPsi,
+            buildResolveAndCheckFir = { session, files, diagnosticsReporter ->
+                buildResolveAndCheckFirFromKtFiles(session, files, diagnosticsReporter)
+            },
+        )
+    }
+
+    private fun PhaseContext.firFrontendWithLightTree(input: KotlinCoreEnvironment): FirOutput {
+        val configuration = input.configuration
+        val messageCollector = configuration.getNotNull(CommonConfigurationKeys.MESSAGE_COLLECTOR_KEY)
+        // FIR
+
+        val groupedSources = collectSources(
+            configuration,
+            input.toVfsBasedProjectEnvironment(),
+            messageCollector
+        )
+
+        val ktSourceFiles = mutableListOf<KtSourceFile>().apply {
+            addAll(groupedSources.commonSources)
+            addAll(groupedSources.platformSources)
+        }
+
+        return firFrontend(
+            input,
+            ktSourceFiles,
+            fileHasSyntaxErrors = { false },
+            isCommonSource = { groupedSources.isCommonSourceForLt(it) },
+            fileBelongsToModule = { file, it -> groupedSources.fileBelongsToModuleForLt(file, it) },
+            buildResolveAndCheckFir = { session, files, diagnosticsReporter ->
+                buildResolveAndCheckFirViaLightTree(session, files, diagnosticsReporter, null)
+            },
+        )
+    }
+
+    private fun PhaseContext.firFrontend(input: KotlinCoreEnvironment): FirOutput {
+        var output = if (input.configuration.getBoolean(CommonConfigurationKeys.USE_LIGHT_TREE)) {
+            firFrontendWithLightTree(input)
+        } else {
+            firFrontendWithPsi(input)
+        }
+        if (output is FirOutput.Full) {
+            output = FirOutput.Full(FrontendFilesForPluginsGenerationPipelinePhase.createFilesWithGeneratedDeclarations(output.firResult))
+        }
+        return output
     }
 }
