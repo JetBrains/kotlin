77365188509a713fbb3b5ce877c95bd9f2ae74de
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2026-02-03 07:27:27 +0100

    [Native] Relocate TestProcessor
    Make TestProcessor accessible from native-klib-cli module by moving it
    in the compiler/ir/backend.native module.
    Also adjust a few minor things during refactoring, like using
    entries instead of values on enums, etc.
    
    ^KT-81748

--- compiler/ir/backend.native/build.gradle.kts
+++ compiler/ir/backend.native/build.gradle.kts
@@ -11,6 +11,9 @@ dependencies {
     implementation(project(":core:compiler.common.native"))
     implementation(project(":native:kotlin-native-utils"))
     implementation(project(":compiler:ir.serialization.native"))
+    // This dependency is required only because of PackagePartClassUtils in TestProcessor.
+    // TODO (KT-84117)
+    implementation(project(":compiler:frontend.common.jvm"))
     api(project(":native:base"))
 }
 
