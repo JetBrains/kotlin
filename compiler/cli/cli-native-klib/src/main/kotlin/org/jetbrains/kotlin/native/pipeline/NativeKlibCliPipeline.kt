c89d6b247fbf5a3f47542025a4221005a4671f08
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2026-02-04 08:28:59 +0100

    [Native] Port 40e59ecd2ecd9fbd1fe24f185defbbc14db78a43 to phased CLI
    We create a conditional `thenIf` which allows to redirect pipeline
    depending on the output of the previous phase
    
    ^KT-82482 Fixed

--- compiler/cli/cli-native-klib/src/main/kotlin/org/jetbrains/kotlin/native/pipeline/NativeKlibCliPipeline.kt
+++ compiler/cli/cli-native-klib/src/main/kotlin/org/jetbrains/kotlin/native/pipeline/NativeKlibCliPipeline.kt
@@ -6,11 +6,17 @@
 package org.jetbrains.kotlin.native.pipeline
 
 import org.jetbrains.kotlin.backend.common.phaser.then
+import org.jetbrains.kotlin.backend.common.phaser.thenIf
 import org.jetbrains.kotlin.cli.common.arguments.K2NativeCompilerArguments
 import org.jetbrains.kotlin.cli.pipeline.AbstractCliPipeline
 import org.jetbrains.kotlin.cli.pipeline.ArgumentsPipelineArtifact
 import org.jetbrains.kotlin.cli.pipeline.PipelineContext
+import org.jetbrains.kotlin.config.AnalysisFlags
+import org.jetbrains.kotlin.config.HeaderMode
+import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.config.phaser.CompilerPhase
+import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.pipeline.AllModulesFrontendOutput
 import org.jetbrains.kotlin.util.PerformanceManager
 
 class NativeKlibCliPipeline(
@@ -23,15 +29,51 @@ class NativeKlibCliPipeline(
     ): CompilerPhase<PipelineContext, ArgumentsPipelineArtifact<K2NativeCompilerArguments>, *> {
         return NativeConfigurationPhase then
                 NativeEnvironmentPhase then
-                NativeFrontendPhase then
-                if (arguments.metadataKlib) {
-                    NativeMetadataSerializationPhase then
-                    NativeKlibWritingPhase
-                } else {
-                    NativeFir2IrPhase then
-                    NativePreSerializationPhase then
-                    NativeIrSerializationPhase then
-                    NativeKlibWritingPhase
-                }
+                NativeFrontendPhase.thenIf(
+                    condition = ::skipIrGeneration,
+                    onTrue = NativeMetadataSerializationPhase,
+                    onFalse = NativeFir2IrPhase then
+                            NativePreSerializationPhase then
+                            NativeIrSerializationPhase
+                ) then
+                NativeKlibWritingPhase
+    }
+
+    /**
+     * Skips IR generation if either generating using -Xmetadata-klib or running header mode with
+     * -Xheader-mode and -Xheader-mode-type=compilation.
+     */
+    private fun skipIrGeneration(artifact: NativeFrontendArtifact): Boolean {
+        val config = artifact.phaseContext.config
+        if (config.metadataKlib) return true
+        val skipInHeaderMode = config.configuration.languageVersionSettings.getFlag(AnalysisFlags.headerMode) &&
+                config.configuration.languageVersionSettings.getFlag(AnalysisFlags.headerModeType) == HeaderMode.COMPILATION &&
+                !requireIrForHeaderCompilationMode(artifact.frontendOutput)
+        return skipInHeaderMode
+    }
+
+    @OptIn(DirectDeclarationsAccess::class)
+    private fun requireIrForHeaderCompilationMode(frontendOutput: AllModulesFrontendOutput): Boolean {
+        for (output in frontendOutput.outputs) {
+            for (file in output.fir) {
+                if (requireIrForHeaderCompilationMode(file.declarations)) return true
+            }
+        }
+        return false
+    }
+
+    @OptIn(DirectDeclarationsAccess::class)
+    private fun requireIrForHeaderCompilationMode(declarations: List<FirDeclaration>): Boolean {
+        for (declaration in declarations) {
+            if (declaration is FirFunction && declaration.status.isInline) return true
+            if (declaration is FirProperty) {
+                if (declaration.getter?.status?.isInline == true || declaration.setter?.status?.isInline == true) return true
+            }
+            if (declaration is FirClass) {
+                if (declaration.status.isValue) return true
+                if (requireIrForHeaderCompilationMode(declaration.declarations)) return true
+            }
+        }
+        return false
     }
 }
