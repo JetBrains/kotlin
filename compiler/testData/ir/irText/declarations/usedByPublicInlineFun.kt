51c49921adaabe1dfc7ffc226245e5eaf6e54307
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-02-04 17:30:45 +0100

    fixup! [Tests] Add tests for current ABI dumps of protected/internal funs used by public inline funs

--- compiler/testData/ir/irText/declarations/usedByPublicInlineFun.kt
+++ compiler/testData/ir/irText/declarations/usedByPublicInlineFun.kt
@@ -10,3 +10,14 @@ class Foo {
         internalFun()
     }
 }
+
+open class OpenFoo {
+    protected fun protectedFun() = Unit
+    internal fun internalFun() = Unit
+
+    @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE", "PROTECTED_CALL_FROM_PUBLIC_INLINE_ERROR")
+    inline fun publicInline() {
+        protectedFun()
+        internalFun()
+    }
+}
