7f504423888ea097ff8bddb119c4195cf3a9b2b7
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-02-05 15:32:46 +0100

    [psi] deprecate KtSelfType
    Support for this feature was dropped from the parser
    in 2015 (8544a5ab5f6be21d4498670270946447fed194ac)
    
    ^KT-84135 Fixed

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtVisitor.java
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -431,6 +431,10 @@ public class KtVisitor<R, D> extends PsiElementVisitor {
         return visitTypeElement(type, data);
     }
 
+    /**
+     * @deprecated This method is obsolete because {@link KtSelfType} is obsolete.
+     */
+    @Deprecated
     public R visitSelfType(@NotNull KtSelfType type, D data) {
         return visitTypeElement(type, data);
     }
