02f17d455d146e27ab39c4ff7524085776ecb496
Igor Chevdar <igor.chevdar@jetbrains.com>
2026-02-06 12:17:39 +0200

    [K/N][tests] A reproducer for #KT-84133

---
+++ native/native.tests/testData/codegen/intrinsics/initInstanceInlineClass.kt
@@ -0,0 +1,29 @@
+// MODULE: lib
+// FILE: lib.kt
+
+value class IntWrapper(val x: Int) {
+    constructor(s: String): this(s.length)
+}
+
+value class StringWrapper(val s: String)
+
+// MODULE: main(lib)
+// FILE: main.kt
+
+import kotlin.native.internal.*
+import kotlin.test.*
+
+fun createIntWrapper(): Any = IntWrapper(117)
+fun createStringWrapper(): Any = StringWrapper("fail")
+
+fun box(): String {
+    val i = createIntWrapper()
+    initInstance(i, IntWrapper(42))
+    assertEquals((i as IntWrapper).x, 42)
+    initInstance(i, IntWrapper("zzz"))
+    assertEquals((i as IntWrapper).x, 3)
+
+    val s = createStringWrapper()
+    initInstance(s, StringWrapper("OK"))
+    return (s as StringWrapper).s
+}
