92df78aa89ab660b5f9f349ded5ef933d6de22e6
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2026-02-09 18:13:45 +0100

    fixup! Fix diagnostic test setup for ProblemsReporter.Factory
    This fixup commit addresses an issue with registering the TestsProblemsReporter.Factory during project initialization in functional tests, which was causing an IsolationException.
    
    The changes modify the project initialization process to correctly set up the problems reporter, ensuring that diagnostic tests like filtersDiagnosticsWithIdSuffix can run without isolation conflicts.

--- libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/diagnosticsTests/DiagnosticFilteringTest.kt
+++ libraries/tools/kotlin-gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/gradle/unitTests/diagnosticsTests/DiagnosticFilteringTest.kt
@@ -7,6 +7,7 @@ package org.jetbrains.kotlin.gradle.unitTests.diagnosticsTests
 
 import org.jetbrains.kotlin.gradle.plugin.diagnostics.KotlinToolingDiagnostics
 import org.jetbrains.kotlin.gradle.plugin.diagnostics.reportDiagnosticOncePerBuild
+import org.jetbrains.kotlin.gradle.util.applyKotlinJvmPlugin
 import org.jetbrains.kotlin.gradle.util.assertNoDiagnostics
 import org.jetbrains.kotlin.gradle.util.buildProject
 import kotlin.test.Test
@@ -14,9 +15,12 @@ import kotlin.test.Test
 class DiagnosticFilteringTest {
     @Test
     fun filtersDiagnosticsWithIdSuffix() {
-        val project = buildProject()
+        val root = buildProject()
 
-        project.reportDiagnosticOncePerBuild(
+        root.applyKotlinJvmPlugin()
+        root.evaluate()
+
+        root.reportDiagnosticOncePerBuild(
             KotlinToolingDiagnostics.DisabledNativeTargetTaskWarning(
                 taskName = "runDebugExecutableLinuxX64",
                 targetName = "linuxX64",
@@ -25,6 +29,6 @@ class DiagnosticFilteringTest {
             )
         )
 
-        project.assertNoDiagnostics(filterDiagnosticIds = listOf(KotlinToolingDiagnostics.DisabledNativeTargetTaskWarning))
+        root.assertNoDiagnostics(filterDiagnosticIds = listOf(KotlinToolingDiagnostics.DisabledNativeTargetTaskWarning))
     }
 }
