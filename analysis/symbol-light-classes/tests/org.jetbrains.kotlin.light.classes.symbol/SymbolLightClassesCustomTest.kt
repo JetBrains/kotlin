03fde50bbb364fbe7350c460b7de7d7bac1fe237
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-02-09 23:21:39 +0100

    [SLC] add regression test on annotation argument psi
    ^KT-83766 Obsolete

--- analysis/symbol-light-classes/tests/org.jetbrains.kotlin.light.classes.symbol/SymbolLightClassesCustomTest.kt
+++ analysis/symbol-light-classes/tests/org.jetbrains.kotlin.light.classes.symbol/SymbolLightClassesCustomTest.kt
@@ -1,20 +1,24 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.light.classes.symbol
 
 import com.intellij.psi.PsiClassType
+import com.intellij.psi.PsiEnumConstant
+import com.intellij.psi.PsiLiteralExpression
+import com.intellij.psi.PsiMethod
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.AnalysisApiFirSourceTestConfigurator
 import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiExecutionTest
-import com.intellij.psi.PsiEnumConstant
+import org.jetbrains.kotlin.asJava.elements.KtLightElementBase
 import org.jetbrains.kotlin.asJava.findFacadeClass
 import org.jetbrains.kotlin.asJava.toLightClass
 import org.jetbrains.kotlin.light.classes.symbol.classes.SymbolLightClassForEnumEntry
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.assertions
 import org.junit.jupiter.api.Test
@@ -63,5 +67,21 @@ class SymbolLightClassesCustomTest : AbstractAnalysisApiExecutionTest(
         testServices.assertions.assertEquals(enumLightClass, actualBaseClass) {
             "Expected enum entry's base type to resolve to the enum class, but got $actualBaseClass"
         }
+    }
+
+    /**
+     * A regression test for KT-83766 to ensure that annotation arguments have an argument PSI element
+     */
+    @Test
+    fun annotationArgumentPsi(file: KtFile, testServices: TestServices) {
+        val topLevelClass = file.declarations.first() as KtClass
+        val topLevelLightClass = topLevelClass.toLightClass() ?: error("Light class was not found")
+        val method = topLevelLightClass.findMethodsByName("method", false).first() as PsiMethod
+        val annotation = method.annotations.first()
+        val argument = annotation.findAttributeValue("value")!! as PsiLiteralExpression
+        testServices.assertions.assertEquals("my text", argument.value)
+
+        val nameReference = (argument as KtLightElementBase).kotlinOrigin as KtNameReferenceExpression
+        testServices.assertions.assertEquals("MY_CONST", nameReference.getReferencedName())
     }
 }
