f8ffaae97993be0b9ec71d3b169419a4dc22615c
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2026-02-09 13:58:35 +0100

    [ABI Validation] Excluded protected members from sealed classes and enums
    #KT-78366 Fixed

--- libraries/tools/abi-validation/abi-tools-tests/src/compiling/kotlin/cases/protected/protectedInAbstract.kt
+++
@@ -1,8 +0,0 @@
-package cases.protected
-
-public abstract class PublicAbstractClass protected constructor() {
-    protected abstract val protectedVal: Int
-    protected abstract var protectedVar: Any?
-
-    protected abstract fun protectedFun()
-}
