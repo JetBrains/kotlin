1c53f11547e4cb013e43e86c9dab5d1126d3bbdd
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-02-10 22:27:27 +0100

    [Args] Cleanup old tests
    ^KT-79402 Verification Pending

--- compiler/arguments/test/org/jetbrains/kotlin/arguments/dsl/types/ExplicitApiConsistency.kt
+++ compiler/arguments/test/org/jetbrains/kotlin/arguments/dsl/types/ExplicitApiConsistency.kt
@@ -5,9 +5,7 @@
 
 package org.jetbrains.kotlin.arguments.dsl.types
 
-import org.jetbrains.kotlin.arguments.description.actualCommonCompilerArguments
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 import org.jetbrains.kotlin.config.ExplicitApiMode as CompilerExplicitApiMode
 import kotlin.test.assertTrue
 
@@ -21,27 +19,5 @@ class ExplicitApiConsistency {
                 message = "Missing entry $entry in DSL types"
             )
         }
-    }
-
-    @Test
-    fun explicitApiArgumentDescription() {
-        val explicitApiArg = actualCommonCompilerArguments.arguments.single { it.name == "Xexplicit-api" }
-
-        assertEquals(
-            expected = -312461075,
-            actual = explicitApiArg.argumentTypeDescription.hashCode(),
-            message = "Value description in '${explicitApiArg.name}' should be updated by moving current value to 'oldValues'."
-        )
-    }
-
-    @Test
-    fun explicitApiReturnTypesArgumentDescription() {
-        val explicitApiReturnTypesArg = actualCommonCompilerArguments.arguments.single { it.name == "XXexplicit-return-types" }
-
-        assertEquals(
-            expected = -312461075,
-            actual = explicitApiReturnTypesArg.argumentTypeDescription.hashCode(),
-            message = "Value description in '${explicitApiReturnTypesArg.name}' should be updated by moving current value to 'oldValues'."
-        )
     }
 }
