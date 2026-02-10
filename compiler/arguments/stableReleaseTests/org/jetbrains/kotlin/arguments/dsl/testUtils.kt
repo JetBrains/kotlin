6a5a06d6f59084b24210afc73a43b847f2f2e020
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-02-10 21:55:33 +0100

    [Args] Ensure current deprecated arguments consistency with previous release
    ^KT-79402

--- compiler/arguments/stableReleaseTests/org/jetbrains/kotlin/arguments/dsl/testUtils.kt
+++ compiler/arguments/stableReleaseTests/org/jetbrains/kotlin/arguments/dsl/testUtils.kt
@@ -21,7 +21,11 @@ private val temporaryExceptions = setOf(
 )
 
 internal fun Set<StableKotlinCompilerArgument>.filterNonDeprecated() = filter {
-    it.isObsolete || it.releaseVersionsMetadata.deprecatedVersion != null
+    !it.isObsolete && it.releaseVersionsMetadata.deprecatedVersion == null && it.name !in temporaryExceptions
+}
+
+internal fun Set<StableKotlinCompilerArgument>.filterDeprecated() = filter {
+    !it.isObsolete && it.releaseVersionsMetadata.deprecatedVersion != null && it.name !in temporaryExceptions
 }
 
 internal val StableKotlinReleaseVersion.asCurrent: KotlinReleaseVersion
