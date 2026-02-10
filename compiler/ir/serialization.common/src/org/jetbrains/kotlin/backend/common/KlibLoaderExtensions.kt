47e1fad84d87416c01deaec1dc3c081b8ec693ab
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-02-10 23:32:53 +0100

    Klib Loading: Don't try to canonicalize non-existing paths for friend libraries
    An attempt to transform a `java.nio.file.Path` that does not point to
    an existing file or directory to it's canonical version ends up
    with a Java IO exception. So, we need to check if a file/directory
    exists the first.

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/KlibLoaderExtensions.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/KlibLoaderExtensions.kt
@@ -17,7 +17,9 @@ import org.jetbrains.kotlin.library.loader.KlibLoaderResult
 import org.jetbrains.kotlin.library.loader.reportLoadingProblemsIfAny
 import org.jetbrains.kotlin.library.uniqueName
 import java.nio.file.InvalidPathException
+import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.io.path.exists
 
 /**
  * Checks for existence of duplicated [uniqueName]s among [KlibLoaderResult.librariesStdlibFirst].
@@ -98,11 +100,18 @@ fun KlibLoaderResult.loadFriendLibraries(friendLibraryPaths: List<String>): List
     val canonicalFriendLibraryPaths: Set<String> = friendLibraryPaths.mapNotNullTo(linkedSetOf()) { rawPath ->
         if (rawPath.isEmpty()) return@mapNotNullTo null
 
-        try {
-            Paths.get(rawPath).toRealPath().toString()
+        val validPath: Path = try {
+            Paths.get(rawPath)
         } catch (_: InvalidPathException) {
             return@mapNotNullTo null
         }
+
+        // First, check if this path exists on the file system.
+        if (!validPath.exists()) return@mapNotNullTo null
+
+        // And only then attempt to resolve it to a canonical path.
+        // Otherwise, we might end up with a Java IO exception.
+        validPath.toRealPath().toString()
     }
 
     if (canonicalFriendLibraryPaths.isEmpty()) return emptyList()
