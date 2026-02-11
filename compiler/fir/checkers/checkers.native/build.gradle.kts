62ba1d364c04fc66394df54e517ba6c801bf0147
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-02-11 12:18:16 +0100

    [FIR][Native] Adjust dependencies using task `:compiler:fir:checkers:checkers.native:projectHealth`

--- compiler/fir/checkers/checkers.native/build.gradle.kts
+++ compiler/fir/checkers/checkers.native/build.gradle.kts
@@ -5,8 +5,17 @@ plugins {
 
 dependencies {
     api(project(":compiler:fir:checkers"))
-    api(project(":compiler:fir:resolve"))
-
+    api(project(":native:base"))
+    api(project(":compiler:fir:cones"))
+    api(project(":compiler:fir:providers"))
+    api(project(":compiler:fir:tree"))
+    api(project(":compiler:frontend.common"))
+    api(project(":core:compiler.common"))
+    api(project(":core:util.runtime"))
+    api(project(":kotlin-stdlib"))
+    implementation(project(":compiler:fir:semantics"))
+    implementation(project(":compiler:frontend.common-psi"))
+    implementation(project(":compiler:util"))
     /*
      * We can't remove this dependency until we use
      *   diagnostics framework from FE 1.0
@@ -14,7 +23,7 @@ dependencies {
     implementation(project(":compiler:frontend"))
     implementation(project(":compiler:psi:psi-api"))
     implementation(project(":compiler:fir:fir-native"))
-    implementation(project(":native:base"))
+    implementation(project(":compiler:fir:resolve"))
 
     compileOnly(project(":core:compiler.common.native"))
     compileOnly(commonDependency("org.jetbrains.kotlin:kotlin-reflect")) { isTransitive = false }
