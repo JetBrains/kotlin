90201210c9d2a698203d35cbc27e57d36fd7d045
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-02-11 15:32:28 +0200

    [FIR] Reproduce the flaky IC problem with explicit backing fields
    ^KT-84213

--- compiler/build-tools/kotlin-build-tools-api-tests/src/testInputChangesTracking/kotlin/SourceChangesTrackingTest.kt
+++ compiler/build-tools/kotlin-build-tools-api-tests/src/testInputChangesTracking/kotlin/SourceChangesTrackingTest.kt
@@ -5,9 +5,15 @@
 
 package org.jetbrains.kotlin.buildtools.tests.compilation
 
+import org.jetbrains.kotlin.buildtools.api.arguments.CommonCompilerArguments.Companion.X_EXPLICIT_BACKING_FIELDS
+import org.jetbrains.kotlin.buildtools.api.arguments.ExperimentalCompilerArgument
 import org.jetbrains.kotlin.buildtools.tests.CompilerExecutionStrategyConfiguration
 import org.jetbrains.kotlin.buildtools.tests.compilation.assertions.assertCompiledSources
+import org.jetbrains.kotlin.buildtools.tests.compilation.assertions.assertLogContainsPatterns
+import org.jetbrains.kotlin.buildtools.tests.compilation.assertions.assertLogDoesNotContainPatterns
+import org.jetbrains.kotlin.buildtools.tests.compilation.model.BtaV2StrategyAgnosticCompilationTest
 import org.jetbrains.kotlin.buildtools.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
+import org.jetbrains.kotlin.buildtools.tests.compilation.model.LogLevel
 import org.jetbrains.kotlin.buildtools.tests.compilation.scenario.assertAddedOutputs
 import org.jetbrains.kotlin.buildtools.tests.compilation.scenario.assertRemovedOutputs
 import org.jetbrains.kotlin.buildtools.tests.compilation.scenario.scenario
@@ -34,6 +40,41 @@ class SourceChangesTrackingTest : BaseCompilationTest() {
             module1.compile {
                 assertCompiledSources("bar.kt")
                 assertRemovedOutputs("SecretKt.class")
+            }
+        }
+    }
+
+    @DisplayName("Explicit backing fields don't alter the compilability of a module")
+    @BtaV2StrategyAgnosticCompilationTest
+    @TestMetadata("explicit-backing-fields-incremental-1")
+    fun testExplicitBackingFieldsIncremental(strategyConfig: CompilerExecutionStrategyConfiguration) {
+        scenario(strategyConfig) {
+            val module = trackedModule(
+                moduleName = "explicit-backing-fields-incremental-1",
+                compilationConfigAction = {
+                    @OptIn(ExperimentalCompilerArgument::class)
+                    it.compilerArguments[X_EXPLICIT_BACKING_FIELDS] = true
+                },
+            )
+
+            // Ensure no `OPT_IN_USAGE_ERROR`s are produced
+            module.createPredefinedFile("A.kt", "1")
+            module.createPredefinedFile("Main.kt", "1")
+            module.compile {
+                assertCompiledSources("A.kt", "Main.kt")
+                assertAddedOutputs(
+                    "A.class", "Experimental.class",
+                    "MainKt.class", $$"MainKt$main$1.class", $$"I$DefaultImpls.class", "I.class", $$"MainKt$main$2.class",
+                )
+            }
+            module.changeFile("Main.kt") { "$it // DUMMY_CHANGE" }
+
+            module.compile {
+                expectFail()
+                assertCompiledSources("Main.kt")
+                assertLogContainsPatterns(LogLevel.DEBUG, ".*Incremental compilation completed".toRegex())
+                assertLogDoesNotContainPatterns(LogLevel.ERROR, ".*Main\\.kt:8:19 This declaration needs opt-in\\. Its usage must be marked with '@Experimental' or '@OptIn\\(Experimental::class\\)'.*".toRegex())
+                assertLogContainsPatterns(LogLevel.ERROR, ".*Main\\.kt:14:19 This declaration needs opt-in\\. Its usage must be marked with '@Experimental' or '@OptIn\\(Experimental::class\\)'.*".toRegex())
             }
         }
     }
