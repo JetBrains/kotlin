3e1110326a56cf0ebbac1b061d9c0f6bcb2de455
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2026-02-11 12:55:23 +0100

    [Wasm] Fix test with predefined NPM tooling
    ^KT-84230 fixed

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/WasmPackageManagerGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/WasmPackageManagerGradlePluginIT.kt
@@ -14,6 +14,7 @@ import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsEnvSpec
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNpmTooling
 import org.jetbrains.kotlin.gradle.targets.wasm.npm.WasmNpmExtension
 import org.jetbrains.kotlin.gradle.targets.wasm.yarn.WasmYarnRootEnvSpec
+import org.jetbrains.kotlin.gradle.targets.wasm.yarn.WasmYarnRootExtension
 import org.jetbrains.kotlin.gradle.testbase.*
 import org.jetbrains.kotlin.gradle.util.replaceText
 import org.jetbrains.kotlin.test.TestMetadata
@@ -286,6 +287,7 @@ abstract class WasmPackageManagerGradlePluginIT : KGPBaseTest() {
                         task.args(
                             listOf(project.rootProject.extensions.getByType(WasmYarnRootEnvSpec::class.java).executable.get())
                         )
+                        task.dependsOn(WasmYarnRootExtension.get(project.rootProject).yarnSetupTaskProvider)
                     } else {
                         task.executable(
                             project.rootProject.extensions.getByType(WasmNpmExtension::class.java).requireConfigured().executable
