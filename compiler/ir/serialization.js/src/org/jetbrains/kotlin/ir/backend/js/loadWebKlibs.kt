09356a4975b8f3d510400ef16cc44b96d71960ad
Francesco Protopapa <francesco.protopapa@jetbrains.com>
2026-02-11 17:24:06 +0100

    [Web] Run library compatibility checks both in test and production pipelines
    ^KT-83012

--- compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/loadWebKlibs.kt
+++ compiler/ir/serialization.js/src/org/jetbrains/kotlin/ir/backend/js/loadWebKlibs.kt
@@ -33,24 +33,14 @@ import org.jetbrains.kotlin.library.loader.KlibPlatformChecker
 fun loadWebKlibsInProductionPipeline(
     configuration: CompilerConfiguration,
     platformChecker: KlibPlatformChecker,
-): LoadedKlibs {
-    val klibs = loadWebKlibs(
-        configuration = configuration,
-        libraryPaths = configuration.libraries,
-        friendPaths = configuration.friendLibraries,
-        includedPath = configuration.includes,
-        platformChecker = platformChecker,
-        useStricterChecks = false // That's only necessary in tests. So, false.
-    )
-
-    if (!configuration.skipLibrarySpecialCompatibilityChecks) {
-        val isWasm = platformChecker is KlibPlatformChecker.Wasm
-        val checker = if (isWasm) WasmLibrarySpecialCompatibilityChecker else JsLibrarySpecialCompatibilityChecker
-        checker.check(klibs.all, configuration.messageCollector, configuration.klibAbiCompatibilityLevel)
-    }
-
-    return klibs
-}
+): LoadedKlibs = loadWebKlibs(
+    configuration = configuration,
+    libraryPaths = configuration.libraries,
+    friendPaths = configuration.friendLibraries,
+    includedPath = configuration.includes,
+    platformChecker = platformChecker,
+    useStricterChecks = false // That's only necessary in tests. So, false.
+)
 
 /**
  * This is the entry point to load Kotlin/JS or Kotlin/Wasm KLIBs in the test pipeline.
@@ -100,5 +90,11 @@ private fun loadWebKlibs(
         all = result.librariesStdlibFirst,
         friends = result.loadFriendLibraries(friendPaths),
         included = result.loadFriendLibraries(listOfNotNull(includedPath)).firstOrNull()
-    )
+    ).also { klibs ->
+        if (!configuration.skipLibrarySpecialCompatibilityChecks) {
+            val isWasm = platformChecker is KlibPlatformChecker.Wasm
+            val checker = if (isWasm) WasmLibrarySpecialCompatibilityChecker else JsLibrarySpecialCompatibilityChecker
+            checker.check(klibs.all, configuration.messageCollector, configuration.klibAbiCompatibilityLevel)
+        }
+    }
 }
