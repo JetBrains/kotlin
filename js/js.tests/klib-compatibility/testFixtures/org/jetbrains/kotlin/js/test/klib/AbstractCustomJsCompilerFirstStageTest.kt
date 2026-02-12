4a01d03643bc5641a24a4b65371e9a78389967c5
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-02-12 23:16:35 +0100

    [Tests][JS] Fix race condition by setting different output dirs for backward compatibility testing
    ^KT-78188

--- js/js.tests/klib-compatibility/testFixtures/org/jetbrains/kotlin/js/test/klib/AbstractCustomJsCompilerFirstStageTest.kt
+++ js/js.tests/klib-compatibility/testFixtures/org/jetbrains/kotlin/js/test/klib/AbstractCustomJsCompilerFirstStageTest.kt
@@ -72,7 +72,8 @@ open class AbstractCustomJsCompilerFirstStageTest(val testDataRoot: String = "co
 
         commonConfigurationForJsBackendSecondStageTest(
             pathToTestDir = testDataRoot,
-            testGroupOutputDirPrefix = "customJsCompilerFirstStageTest/",
+            testGroupOutputDirPrefix = this@AbstractCustomJsCompilerFirstStageTest::class.java.simpleName +
+                    customJsCompilerSettings.defaultLanguageVersion,
             backendFacades = JsBackendFacades.WithRecompilation
         )
 
