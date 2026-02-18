10ad3cb5984e5e5011961ef1a2cc7aed829c94aa
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-02-18 17:52:30 +0100

    [Native] Extend UsedLibrariesComputationTest with CLI-based cases
    Let's not only unit-test the logic of "user libraries" computation,
    but also have a CLI-based test that checks how it indeed works
    in the compiler.
    
    ^KT-70118
    ^KT-61096

--- native/native.tests/klib-ir-inliner/tests/org/jetbrains/kotlin/konan/test/UsedLibrariesComputationTest.kt
+++ native/native.tests/klib-ir-inliner/tests/org/jetbrains/kotlin/konan/test/UsedLibrariesComputationTest.kt
@@ -5,8 +5,13 @@
 
 package org.jetbrains.kotlin.konan.test
 
-import org.jetbrains.kotlin.konan.file.File
 import org.jetbrains.kotlin.konan.properties.Properties
+import org.jetbrains.kotlin.konan.properties.propertyList
+import org.jetbrains.kotlin.konan.test.blackbox.AbstractNativeSimpleTest
+import org.jetbrains.kotlin.konan.test.blackbox.support.compilation.TestCompilationArtifact.KLIB
+import org.jetbrains.kotlin.konan.test.blackbox.support.compilation.TestCompilationResult
+import org.jetbrains.kotlin.konan.test.klib.compileToKlibsViaCli
+import org.jetbrains.kotlin.konan.test.klib.newSourceModules
 import org.jetbrains.kotlin.library.*
 import org.jetbrains.kotlin.library.components.KlibMetadataComponent
 import org.jetbrains.kotlin.library.components.metadata
@@ -15,8 +20,80 @@ import org.jetbrains.kotlin.library.metadata.parseModuleHeader
 import org.jetbrains.kotlin.name.FqName
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.jetbrains.kotlin.konan.file.File as KlibFile
 
-class UsedLibrariesComputationTest {
+class UsedLibrariesComputationTest : AbstractNativeSimpleTest() {
+    @Test
+    fun `Used libraries are computed correctly (CLI)`() {
+        var additionalUnusedModulePath: String? = null
+        newSourceModules { addRegularModule("additional") }.compileToKlibsViaCli { _, successKlib ->
+            additionalUnusedModulePath = successKlib.resultingArtifact.klibFile.absolutePath
+        }
+        checkNotNull(additionalUnusedModulePath)
+
+        fun TestCompilationResult.Success<out KLIB>.assertDependencyNames(vararg expectedDependencyNamesInManifest: String) {
+            val expectedDependencyNames = expectedDependencyNamesInManifest.toSet()
+            val actualDependencyNames = resultingArtifact.klibFile.resolve("default").resolve("manifest").bufferedReader().use { reader ->
+                Properties().apply { load(reader) }
+            }.propertyList(KLIB_PROPERTY_DEPENDS, escapeInQuotes = true).toSet()
+
+            assertEquals(expectedDependencyNames, actualDependencyNames) {
+                "Dependency mismatch for module ${resultingArtifact.klibFile}: expected $expectedDependencyNames, got $actualDependencyNames"
+            }
+        }
+
+        newSourceModules {
+            addRegularModule("test")
+        }.compileToKlibsViaCli { _, successKlib ->
+            successKlib.assertDependencyNames(
+                /* implicit unavoidable dependency */ "stdlib",
+            )
+        }
+
+        // TODO (KT-60874): Need to clarify why used-specified libraries are forced to stay in the list of "used libraries"
+        //  even when they are not used. Note that the default value for `konanPurgeUserLibs` is always `false`.
+        newSourceModules {
+            addRegularModule("test")
+        }.compileToKlibsViaCli(extraCliArgs = listOf("-l", additionalUnusedModulePath)) { _, successKlib ->
+            successKlib.assertDependencyNames(
+                /* implicit unavoidable dependency */ "stdlib",
+                /* because it was passed explicitly via CLI, even if "test" doesn't use any API from "additional" */ "additional",
+            )
+        }
+
+        // TODO (KT-60874): Need to clarify why used-specified libraries are forced to stay in the list of "used libraries"
+        //  even when they are not used. Note that the default value for `konanPurgeUserLibs` is always `false`.
+        newSourceModules {
+            addRegularModule("test")
+        }.compileToKlibsViaCli(extraCliArgs = listOf("-l", additionalUnusedModulePath, "-Xpurge-user-libs")) { _, successKlib ->
+            successKlib.assertDependencyNames(
+                /* implicit unavoidable dependency */ "stdlib",
+                /* because it was passed explicitly via CLI, even if "test" doesn't use any API from "additional" */ "additional",
+            )
+        }
+
+        newSourceModules {
+            addRegularModule("test") {
+                sourceFileAddend("fun callAdditional() = additional.additional(-1)")
+            }
+        }.compileToKlibsViaCli(extraCliArgs = listOf("-l", additionalUnusedModulePath)) { _, successKlib ->
+            successKlib.assertDependencyNames(
+                /* implicit unavoidable dependency */ "stdlib",
+                /* because it was passed explicitly via CLI and "test" uses some API from "additional" */ "additional",
+            )
+        }
+
+        newSourceModules {
+            addRegularModule("test") {
+                sourceFileAddend("@kotlinx.cinterop.ExperimentalForeignApi fun callPosix() = platform.posix.fopen(\"test.txt\", \"r\")")
+            }
+        }.compileToKlibsViaCli { _, successKlib ->
+            successKlib.assertDependencyNames(
+                /* implicit unavoidable dependency */ "stdlib",
+                /* because "test" uses some API from posix */ "org.jetbrains.kotlin.native.platform.posix",
+            )
+        }
+    }
 
     @Test
     fun `Used libraries are computed correctly (low-level)`() {
@@ -69,11 +146,11 @@ class UsedLibrariesComputationTest {
             return null
         }
 
-        override val location: File get() = File(".")
+        override val location: KlibFile get() = KlibFile(".")
         override val attributes: KlibAttributes get() = error("Not supported")
 
         override val versions: KotlinLibraryVersioning get() = error("Not supported")
-        override val libraryFile: File get() = File(".")
+        override val libraryFile: KlibFile get() = KlibFile(".")
 
         override val manifestProperties: Properties = Properties().apply {
             setProperty(KLIB_PROPERTY_UNIQUE_NAME, libName)
