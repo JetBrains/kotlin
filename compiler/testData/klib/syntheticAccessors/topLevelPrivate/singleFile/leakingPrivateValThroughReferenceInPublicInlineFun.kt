15e37f3b3c5829f293062d69b780e7887192b153
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2026-02-18 16:59:37 +0200

    [Tests] KQA-2794: review klib/syntheticAccessors/topLevelPrivate tests with ForbidExposingLessVisibleTypesInInline enabled
    For details see the comments to KQA-2794

--- compiler/testData/klib/syntheticAccessors/topLevelPrivate/singleFile/leakingPrivateValThroughReferenceInPublicInlineFun.kt
+++
@@ -1,9 +0,0 @@
-// LANGUAGE: -ForbidExposingLessVisibleTypesInInline
-private val privateVal: String = "OK"
-
-@Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
-public inline fun publicInlineFunction() = ::privateVal
-
-fun box(): String {
-    return publicInlineFunction().invoke()
-}
