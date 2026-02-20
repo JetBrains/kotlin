e81067f39cbaf5e8fa6f67e674705396ccfe68bd
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-02-20 22:07:50 +0000

    KT-84372 Adapted copyToRestrictedReadSource expectation for new JDKs
    Starting from JDK 22, Files.copy throws ADE if the source directory is not readable.
    
    ^KT-84372 fixed
    
    
    Merge-request: KT-MR-25845
    Merged-by: Filipp Zhinkin <filipp.zhinkin@jetbrains.com>

--- libraries/stdlib/jdk7/test/PathExtensionsTest.kt
+++ libraries/stdlib/jdk7/test/PathExtensionsTest.kt
@@ -213,7 +213,12 @@ class PathExtensionsTest : AbstractPathTest() {
         val dstDirectory = root.resolve("dstDirectory")
 
         withRestrictedRead(srcDirectory, alsoReset = listOf(dstDirectory)) {
-            srcDirectory.copyTo(dstDirectory) // successfully copies restricted directory
+            try {
+                // successfully copies restricted directory before jdk22, fails with AccessDeniedException after
+                srcDirectory.copyTo(dstDirectory)
+            } catch (_: AccessDeniedException) {
+                return@withRestrictedRead
+            }
             assertFalse(dstDirectory.isReadable()) // copies access permissions
         }
     }
