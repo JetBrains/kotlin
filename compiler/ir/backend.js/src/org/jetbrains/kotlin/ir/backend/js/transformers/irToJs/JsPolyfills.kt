a2ef1ea7a1d6eea7c07290e2d8c260bba1c553b5
Artem Kobzar <artem.kobzar@jetbrains.com>
2026-02-23 17:06:50 +0100

    [K/JS] Sort polyfills to not re-trigger hot-reload for stdlib
    ^KT-68281 Fixed

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsPolyfills.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/transformers/irToJs/JsPolyfills.kt
@@ -5,15 +5,18 @@
 
 package org.jetbrains.kotlin.ir.backend.js.transformers.irToJs
 
+import org.jetbrains.kotlin.backend.common.compilationException
 import org.jetbrains.kotlin.ir.backend.js.utils.JsAnnotations
 import org.jetbrains.kotlin.ir.backend.js.utils.hasJsPolyfill
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.IrConst
+import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.util.getAnnotation
 import org.jetbrains.kotlin.js.backend.ast.JsStatement
+import java.util.TreeMap
 
 class JsPolyfills {
-    private val polyfillsPerFile = hashMapOf<IrFile, HashSet<IrDeclaration>>()
+    private val polyfillsPerFile = hashMapOf<IrFile, MutableSet<IrDeclaration>>()
 
     fun registerDeclarationNativeImplementation(file: IrFile, declaration: IrDeclaration) {
         if (!declaration.hasJsPolyfill()) return
@@ -24,19 +27,28 @@ class JsPolyfills {
 
     fun saveOnlyIntersectionOfNextDeclarationsFor(file: IrFile, declarations: Set<IrDeclaration>) {
         val polyfills = polyfillsPerFile[file] ?: return
-        polyfillsPerFile[file] = polyfills.intersect(declarations).toHashSet()
+        polyfillsPerFile[file] = polyfills.intersect(declarations) as MutableSet<IrDeclaration>
     }
 
     fun getAllPolyfillsFor(file: IrFile): List<JsStatement> =
         polyfillsPerFile[file].orEmpty().asImplementationList()
 
-    private fun Iterable<IrDeclaration>.asImplementationList() =
-        asSequence().asImplementationList()
+    private fun Iterable<IrDeclaration>.asImplementationList(): List<JsStatement> {
+        val orderedMapOfPolyfills = TreeMap<String, List<JsStatement>>()
 
-    private fun Sequence<IrDeclaration>.asImplementationList(): List<JsStatement> {
-        return map { it to it.getAnnotation(JsAnnotations.JsPolyfillFqn)!!.arguments[0]!! }
-            .distinctBy { (it.second as IrConst).value as String }
-            .flatMap { (container, polyfill) -> translateJsCodeIntoStatementList(polyfill, container).orEmpty() }
-            .toList()
+        for (declaration in this) {
+            val polyfillCodeExpression = declaration.getAnnotation(JsAnnotations.JsPolyfillFqn)?.arguments?.get(0) ?: compilationException(
+                "there is no @JsPolyfill annotation, while the declaration was added to the polyfilled declaration set",
+                declaration
+            )
+
+            val polyfillCodeString = (polyfillCodeExpression as IrConst).value as String
+
+            if (polyfillCodeString in orderedMapOfPolyfills) continue
+
+            orderedMapOfPolyfills[polyfillCodeString] = translateJsCodeIntoStatementList(polyfillCodeExpression, declaration) ?: emptyList()
+        }
+
+        return orderedMapOfPolyfills.flatMap { it.value }
     }
 }
