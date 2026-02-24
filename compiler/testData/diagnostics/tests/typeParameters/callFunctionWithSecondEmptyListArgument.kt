40136dcac72b1353691de5b84c9e24ad2345f3d3
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-02-24 16:17:07 +0200

    [FIR] Add a test from Kotlin bootstrap

---
+++ compiler/testData/diagnostics/tests/typeParameters/callFunctionWithSecondEmptyListArgument.kt
@@ -0,0 +1,23 @@
+// FIR_IDENTICAL
+// RUN_PIPELINE_TILL: BACKEND
+// WITH_STDLIB
+// DUMP_INFERENCE_LOGS: FIXATION
+
+interface CallableMemberDescriptor
+interface SimpleFunctionDescriptor : CallableMemberDescriptor
+
+public fun <D : CallableMemberDescriptor> resolveOverridesForNonStaticMembers(
+    membersFromSupertypes: Collection<D>,
+    membersFromCurrent: Collection<D>,
+): Collection<D> = membersFromSupertypes
+
+fun computeNonDeclaredFunctions(functionsFromSupertypes: Set<SimpleFunctionDescriptor>) {
+    val mergedFunctionFromSuperTypes = resolveOverridesForNonStaticMembers(functionsFromSupertypes, emptyList())
+
+    addOverriddenSpecialMethods(mergedFunctionFromSuperTypes)
+}
+
+fun addOverriddenSpecialMethods(alreadyDeclaredFunctions: Collection<SimpleFunctionDescriptor>) {}
+
+/* GENERATED_FIR_TAGS: asExpression, classDeclaration, funWithExtensionReceiver, functionDeclaration, inProjection,
+intersectionType, nullableType, primaryConstructor, propertyDeclaration, thisExpression, typeConstraint, typeParameter */
