462285ba17c0bef266d8929729cda6e91a19b002
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-02-24 16:38:29 +0200

    fixup! [FIR] Introduce `IS_SELF_SUFFICIENT_MATERIALIZE_VARIABLE`

--- compiler/testData/diagnostics/tests/typeParameters/callFunctionWithSecondEmptyListArgument.fir.kt
+++ compiler/testData/diagnostics/tests/typeParameters/callFunctionWithSecondEmptyListArgument.fir.kt
@@ -13,7 +13,7 @@ public fun <D : CallableMemberDescriptor> resolveOverridesForNonStaticMembers(
 fun computeNonDeclaredFunctions(functionsFromSupertypes: Set<SimpleFunctionDescriptor>) {
     val mergedFunctionFromSuperTypes = resolveOverridesForNonStaticMembers(functionsFromSupertypes, emptyList())
 
-    addOverriddenSpecialMethods(mergedFunctionFromSuperTypes)
+    addOverriddenSpecialMethods(<!ARGUMENT_TYPE_MISMATCH!>mergedFunctionFromSuperTypes<!>)
 }
 
 fun addOverriddenSpecialMethods(alreadyDeclaredFunctions: Collection<SimpleFunctionDescriptor>) {}
