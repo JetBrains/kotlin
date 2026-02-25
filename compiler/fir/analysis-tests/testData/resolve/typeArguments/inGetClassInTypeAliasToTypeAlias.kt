353cec3df2a7ae7ba4f79a2f99fa1497ba2c9185
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-02-25 15:22:10 +0200

    [Tests] Add extra test for KT-84380
    The test demonstrates behavior when typealias expands to class via
    another typealias.

---
+++ compiler/fir/analysis-tests/testData/resolve/typeArguments/inGetClassInTypeAliasToTypeAlias.kt
@@ -0,0 +1,30 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-84380
+// LANGUAGE: -ForbidUselessTypeArgumentsIn25
+// WITH_STDLIB
+
+typealias No = List<Int>
+typealias Yes<T> = No
+
+typealias No2 = String
+typealias Yes2<T> = No2
+
+typealias No3 = Array<String>
+typealias Yes3<T> = No3
+
+fun test() {
+    Yes::class
+    Yes2::class
+    Yes3::class
+
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS!>Yes<Int>::class<!>
+    <!CLASS_LITERAL_LHS_NOT_A_CLASS_WARNING!>Yes2<Int>::class<!>
+    Yes3<Int>::class
+
+    Yes<!WRONG_NUMBER_OF_TYPE_ARGUMENTS!><Int, String><!>::class
+    Yes2<!WRONG_NUMBER_OF_TYPE_ARGUMENTS_IN_GET_CLASS_WARNING!><Int, String><!>::class
+    Yes2<!WRONG_NUMBER_OF_TYPE_ARGUMENTS_IN_GET_CLASS_WARNING!><Int, String><!>::class
+}
+
+/* GENERATED_FIR_TAGS: classReference, functionDeclaration, nullableType, typeAliasDeclaration,
+typeAliasDeclarationWithTypeParameter, typeParameter */
