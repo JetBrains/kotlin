JetBrains/kotlin

commit 7375d523ca62b74dfd1676387b6b3c95609f6232

Reflection: use descriptor-less KProperty for local delegated properties
Local delegated properties are the simplest kind of declarations which
we can reimplement using kotlin-metadata-jvm and check that basic things
are working: they always have default accessors, no type parameters from
outer declarations, call/callBy are not supposed to work, etc.

In this change, the general hierarchy of new Km-based declarations is
introduced (KotlinKCallable, KotlinKProperty, etc), however many methods
are not implemented properly yet because it's not needed for local
delegated properties. These methods call an assertion
`checkLocalDelegatedPropertyOrAccessor` which will be removed in future
commits where the new objects are supported for other kinds of
callables.

 #KT-80710

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ReflectionFactoryImpl.java
@@ -11,6 +11,7 @@ import kotlin.reflect.*;
 import kotlin.reflect.full.KClassifiers;
 import kotlin.reflect.jvm.ReflectLambdaKt;
 import kotlin.reflect.jvm.internal.types.TypeOfImplKt;
+import kotlin.text.MatchResult;
 
 import java.lang.annotation.Annotation;
 import java.util.Collections;
@@ -72,11 +73,29 @@ public class ReflectionFactoryImpl extends ReflectionFactory {
 
     @Override
     public KProperty0 property0(PropertyReference0 p) {
-        return new DescriptorKProperty0(getOwner(p), p.getName(), p.getSignature(), p.getBoundReceiver());
+        KDeclarationContainerImpl container = getOwner(p);
+        String signature = p.getSignature();
+        if (!SystemPropertiesKt.getUseK1Implementation()) {
+            MatchResult result = KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matchEntire(signature);
+            if (result != null) {
+                List<String> values = result.getGroupValues();
+                return container.createLocalProperty(Integer.parseInt(values.get(1)), signature);
+            }
+        }
+        return new DescriptorKProperty0(container, p.getName(), signature, p.getBoundReceiver());
     }
 
     @Override
     public KMutableProperty0 mutableProperty0(MutablePropertyReference0 p) {
+        KDeclarationContainerImpl container = getOwner(p);
+        String signature = p.getSignature();
+        if (!SystemPropertiesKt.getUseK1Implementation()) {
+            MatchResult result = KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matchEntire(signature);
+            if (result != null) {
+                List<String> values = result.getGroupValues();
+                return (KMutableProperty0) container.createLocalProperty(Integer.parseInt(values.get(1)), signature);
+            }
+        }
         return new DescriptorKMutableProperty0(getOwner(p), p.getName(), p.getSignature(), p.getBoundReceiver());
     }
 

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLBlackBoxTestGenerated.java
@@ -56832,6 +56832,12 @@ public class LLBlackBoxTestGenerated extends AbstractLLBlackBoxTest {
         }
 
         @Test
+        @TestMetadata("equals.kt")
+        public void testEquals() {
+          runTest("compiler/testData/codegen/box/reflection/properties/localDelegated/equals.kt");
+        }
+
+        @Test
         @TestMetadata("inInterface.kt")
         public void testInInterface() {
           runTest("compiler/testData/codegen/box/reflection/properties/localDelegated/inInterface.kt");

analysis/low-level-api-fir/tests-gen/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLReversedBlackBoxTestGenerated.java
@@ -56832,6 +56832,12 @@ public class LLReversedBlackBoxTestGenerated extends AbstractLLReversedBlackBoxT
         }
 
         @Test
+        @TestMetadata("equals.kt")
+        public void testEquals() {
+          runTest("compiler/testData/codegen/box/reflection/properties/localDelegated/equals.kt");
+        }
+
+        @Test
         @TestMetadata("inInterface.kt")
         public void testInInterface() {
           runTest("compiler/testData/codegen/box/reflection/properties/localDelegated/inInterface.kt");

compiler/testData/codegen/box/reflection/properties/localDelegated/equals.kt
@@ -1,0 +1,60 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FILE: Delegate.kt
+import kotlin.reflect.KProperty
+
+object Delegate {
+    operator fun getValue(instance: Any?, kProperty: KProperty<*>): KProperty<*> = kProperty
+
+    operator fun setValue(instance: Any?, kProperty: KProperty<*>, value: KProperty<*>) {}
+}
+
+// FILE: pkg.kt
+import kotlin.reflect.KProperty
+
+fun getInPackage(flag: Boolean): KProperty<*> =
+    if (flag) {
+        val p by Delegate
+        p
+    } else {
+        val p by Delegate
+        p
+    }
+
+// FILE: class.kt
+import kotlin.reflect.KProperty
+
+object O {
+    fun getInClass(flag: Boolean): KProperty<*> =
+        if (flag) {
+            val p by Delegate
+            p
+        } else {
+            val p by Delegate
+            p
+        }
+}
+
+// FILE: multifile.kt
+@file:JvmMultifileClass
+@file:JvmName("Multifile")
+import kotlin.reflect.KProperty
+
+fun getInMultifileClass(flag: Boolean): KProperty<*> =
+    if (flag) {
+        val p by Delegate
+        p
+    } else {
+        val p by Delegate
+        p
+    }
+
+// FILE: box.kt
+import kotlin.test.*
+
+fun box(): String {
+    assertNotEquals(getInPackage(false), getInPackage(true))
+    assertNotEquals(O.getInClass(false), O.getInClass(true))
+    assertNotEquals(getInMultifileClass(false), getInMultifileClass(true))
+    return "OK"
+}

compiler/testData/codegen/box/reflection/properties/localDelegated/localDelegatedProperty.kt
@@ -2,6 +2,7 @@
 // WITH_REFLECT
 
 import kotlin.reflect.*
+import kotlin.reflect.jvm.javaField
 import kotlin.test.*
 
 object Delegate {
@@ -17,17 +18,29 @@ object Delegate {
     }
 }
 
-fun check(expectedName: String, p: KProperty0<*>): String? {
+fun checkIsFinal(c: KCallable<*>) {
+    assertTrue(c.isFinal)
+    assertFalse(c.isOpen)
+    assertFalse(c.isAbstract)
+}
+
+fun checkUnsupportedCall(block: () -> Unit) {
+    try {
+        block()
+        throw AssertionError("Fail: reflective call of a local delegated property should fail because it's not supported")
+    } catch (e: UnsupportedOperationException) {  /* ok */ }
+}
+
+fun check(expectedName: String, p: KProperty0<*>) {
     assertEquals(expectedName, p.name)
     assertEquals(emptyList<KParameter>(), p.parameters)
     assertEquals(emptyList<KTypeParameter>(), p.typeParameters)
     assertEquals(null, p.visibility) // "local" visibility is not representable with reflection API
     assertEquals("kotlin.collections.List<kotlin.String?>", p.returnType.toString())
-    assertTrue(p.isFinal)
-    assertFalse(p.isOpen)
-    assertFalse(p.isAbstract)
+    checkIsFinal(p)
     assertFalse(p.isLateinit)
     assertFalse(p.isConst)
+    assertEquals(null, p.javaField)
 
     // TODO: support getDelegate for local delegated properties
     assertEquals(null, (p as KProperty0<*>).getDelegate())
@@ -35,22 +48,53 @@ fun check(expectedName: String, p: KProperty0<*>): String? {
     assertEquals(emptyList<KParameter>(), p.getter.parameters)
     assertEquals("kotlin.collections.List<kotlin.String?>", p.getter.returnType.toString())
 
-    try {
-        p.call()
-        return "Fail: reflective call of a local delegated property should fail because it's not supported"
-    } catch (e: UnsupportedOperationException) {  /* ok */ }
+    checkUnsupportedCall { p.call() }
+    checkUnsupportedCall { p.callBy(emptyMap()) }
+
+    val getter = p.getter
+    checkIsFinal(getter)
+    assertEquals(p, getter.property)
+    assertEquals("<get-$expectedName>", getter.name)
+    assertEquals(emptyList(), getter.parameters)
+    assertEquals(p.returnType, getter.returnType)
+    assertEquals(emptyList(), getter.typeParameters)
+    assertEquals(null, getter.visibility)
+    assertFalse(getter.isSuspend)
+    assertFalse(getter.isInline)
+    assertFalse(getter.isExternal)
+    assertFalse(getter.isOperator)
+    assertFalse(getter.isInfix)
+    assertEquals(emptyList(), getter.annotations)
+    checkUnsupportedCall { getter.call() }
+    checkUnsupportedCall { getter.callBy(emptyMap()) }
 
     if (p is KMutableProperty0<*>) {
-        assertEquals(listOf("kotlin.collections.List<kotlin.String?>"), p.setter.parameters.map { it.type.toString() })
-        assertEquals("kotlin.Unit", p.setter.returnType.toString())
+        val setter = p.setter
+        val param = setter.parameters.single()
 
-        try {
-            p.setter.call()
-            return "Fail: reflective call of a local delegated property setter should fail because it's not supported"
-        } catch (e: UnsupportedOperationException) {  /* ok */ }
+        checkIsFinal(setter)
+        assertEquals(p, setter.property)
+        assertEquals("<set-$expectedName>", setter.name)
+        assertEquals("kotlin.Unit", setter.returnType.toString())
+        assertEquals(emptyList(), setter.typeParameters)
+        assertEquals(null, setter.visibility)
+        assertFalse(setter.isSuspend)
+        assertFalse(setter.isInline)
+        assertFalse(setter.isExternal)
+        assertFalse(setter.isOperator)
+        assertFalse(setter.isInfix)
+        assertEquals(emptyList(), setter.annotations)
+        // Passing some argument here to mitigate the effect of KT-81377.
+        checkUnsupportedCall { setter.call(listOf("")) }
+        checkUnsupportedCall { setter.callBy(mapOf(param to listOf(""))) }
+
+        assertEquals(0, param.index)
+        assertEquals(null, param.name)
+        assertEquals(KParameter.Kind.VALUE, param.kind)
+        assertFalse(param.isOptional)
+        assertFalse(param.isVararg)
+        assertEquals(emptyList(), param.annotations)
     }
-
-    return null
 }
 
 annotation class Anno
@@ -59,14 +103,16 @@ fun box(): String {
     @Anno
     val localVal by Delegate
     localVal
-
-    check("localVal", Delegate.property as KProperty0<*>)?.let { error -> return error }
+    val valProperty = Delegate.property as KProperty0<*>
+    assertFalse(valProperty is KMutableProperty<*>)
+    check("localVal", valProperty)
 
     @Anno
     var localVar by Delegate
     localVar
-
-    check("localVar", Delegate.property as KProperty0<*>)?.let { error -> return error }
+    var varProperty = Delegate.property as KProperty0<*>
+    assertTrue(varProperty is KMutableProperty<*>)
+    check("localVar", varProperty)
 
     return "OK"
 }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ConvertFromMetadata.kt
@@ -166,7 +166,7 @@ private fun KmTypeProjection.toKTypeProjection(
     else
         KTypeProjection(variance?.toKVariance(), type?.toKType(classLoader, typeParameterTable, computeJavaType))
 
-private fun KmVariance.toKVariance(): KVariance = when (this) {
+internal fun KmVariance.toKVariance(): KVariance = when (this) {
     KmVariance.IN -> KVariance.IN
     KmVariance.OUT -> KVariance.OUT
     KmVariance.INVARIANT -> KVariance.INVARIANT
@@ -178,7 +178,7 @@ private fun KmClassifier.toMutableCollectionKClass(kClassifier: KClassifier): Mu
     return getMutableCollectionKClass(classId.asSingleFqName(), kClassifier as KClass<*>)
 }
 
-private fun KmAnnotation.toAnnotation(classLoader: ClassLoader): Annotation =
+internal fun KmAnnotation.toAnnotation(classLoader: ClassLoader): Annotation =
     createAnnotationInstance(
         classLoader.loadClass(className.toClassId())
             ?: throw KotlinReflectionInternalError("Annotation class not found: $className"),
@@ -223,4 +223,13 @@ private fun KmAnnotationArgument.toAnnotationArgument(
         classLoader.loadClass(className.toClassId())
             ?: throw KotlinReflectionInternalError("Unresolved class: $className")
     is KmAnnotationArgument.LiteralValue<*> -> value
+}
+
+internal fun Visibility.toKVisibility(): KVisibility? = when (this) {
+    Visibility.INTERNAL -> KVisibility.INTERNAL
+    Visibility.PRIVATE -> KVisibility.PRIVATE
+    Visibility.PROTECTED -> KVisibility.PROTECTED
+    Visibility.PUBLIC -> KVisibility.PUBLIC
+    Visibility.PRIVATE_TO_THIS -> KVisibility.PRIVATE
+    Visibility.LOCAL -> null
 }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/DescriptorKCallable.kt
@@ -21,7 +21,7 @@ import kotlin.coroutines.Continuation
 import kotlin.reflect.*
 import kotlin.reflect.jvm.internal.types.DescriptorKType
 
-internal abstract class DescriptorKCallable<out R> : ReflectKCallable<R>, KTypeParameterOwnerImpl {
+internal abstract class DescriptorKCallable<out R> : ReflectKCallable<R> {
     abstract val descriptor: CallableMemberDescriptor
 
     private val _annotations = ReflectProperties.lazySoft { descriptor.computeAnnotations() }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/EmptyContainerForLocal.kt
@@ -20,6 +20,7 @@ import org.jetbrains.kotlin.descriptors.ConstructorDescriptor
 import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.name.Name
+import kotlin.metadata.KmProperty
 import kotlin.reflect.KCallable
 
 internal object EmptyContainerForLocal : KDeclarationContainerImpl() {
@@ -36,7 +37,9 @@ internal object EmptyContainerForLocal : KDeclarationContainerImpl() {
 
     override fun getFunctions(name: Name): Collection<FunctionDescriptor> = fail()
 
-    override fun getLocalProperty(index: Int): PropertyDescriptor? = null
+    override fun getLocalPropertyDescriptor(index: Int): PropertyDescriptor? = null
+
+    override fun getLocalPropertyMetadata(index: Int): KmProperty? = null
 
     private fun fail(): Nothing = throw KotlinReflectionInternalError(
         "Introspecting local functions, lambdas, anonymous functions, local variables and typealiases is not yet fully supported in Kotlin reflection"

commit 7ef8bccc4be91ff0e8e144123f7c74913b212a78

Reflection: add LazyKProperty to fix recent performance regression
In 7375d523ca and 98722a1688, `Reflection.property0/1` started to
perform some additional checks to determine what kind of property to
create. It was needed because the new `KCallable` implementations do not
have the abstraction layer similar to descriptors. In particular, new
`KProperty` implementations are different for Kotlin and Java
properties, and metadata of delegated properties needs to be read from a
specific place, different from class properties or top-level properties.

Those changes relied on the fact that `Reflection.property0/1` were only
called from places where full reflection definitely needed to be
initialized and was allowed to be "heavy", such as the computation of
the full reflection object for property reference in
`PropertyReference0.computeReflected`.

But that fact only became true in 2.3.20 with the fix for KT-81974.
Before that issue was fixed, every delegated property creation resulted
in an unnecessary call to `Reflection.property0/1`.

So if we ran code compiled with Kotlin 2.3.0, where
`Reflection.property0/1` is invoked for any delegated property at the
moment of its creation (static initializer of the containing class/file)
with kotlin-reflect 2.3.20-RC, we'd observe performance regressions, and
possibly other issues, caused by the unnecessary parsing of protobuf,
reading of metadata, etc, on each delegated property.

The fix is kind of similar to how `DescriptorKProperty` behaves in that
it doesn't compute anything heavy in its constructor, but it does so
only when asked for any non-trivial operation for the first time.
However, the new implementation is a bit dumber (which is good) because
it doesn't try to do anything specific to different kinds of Kotlin
properties, but just delegates everything to the lazily computed "real"
property object.

 #KT-84600 Fixed

core/reflection.jvm/src/kotlin/reflect/jvm/internal/LazyKProperty.kt
@@ -1,0 +1,59 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.reflect.jvm.internal
+
+import kotlin.reflect.*
+
+internal abstract class LazyKProperty<out V, out D : KProperty<V>>(computeProperty: () -> D) : KProperty<V> {
+    val delegate: D by lazy(LazyThreadSafetyMode.PUBLICATION, computeProperty)
+
+    override val name: String get() = delegate.name
+    override val parameters: List<KParameter> get() = delegate.parameters
+    override val returnType: KType get() = delegate.returnType
+    override val typeParameters: List<KTypeParameter> get() = delegate.typeParameters
+    override fun call(vararg args: Any?): V = delegate.call(*args)
+    override fun callBy(args: Map<KParameter, Any?>): V = delegate.callBy(args)
+    override val visibility: KVisibility? get() = delegate.visibility
+    override val isFinal: Boolean get() = delegate.isFinal
+    override val isOpen: Boolean get() = delegate.isOpen
+    override val isAbstract: Boolean get() = delegate.isAbstract
+    override val isSuspend: Boolean get() = delegate.isSuspend
+    override val isLateinit: Boolean get() = delegate.isLateinit
+    override val isConst: Boolean get() = delegate.isConst
+    override val annotations: List<Annotation> get() = delegate.annotations
+
+    override fun equals(other: Any?): Boolean = delegate == other
+    override fun hashCode(): Int = delegate.hashCode()
+    override fun toString(): String = delegate.toString()
+}
+
+internal open class LazyKProperty0<out V, out D : KProperty0<V>>(computeProperty: () -> D) :
+    LazyKProperty<V, D>(computeProperty), KProperty0<V> {
+    override val getter: KProperty0.Getter<V> get() = delegate.getter
+    override fun get(): V = delegate.get()
+    override fun getDelegate(): Any? = delegate.getDelegate()
+    override fun invoke(): V = delegate.invoke()
+}
+
+internal class LazyKMutableProperty0<V, D : KMutableProperty0<V>>(computeProperty: () -> D) :
+    LazyKProperty0<V, D>(computeProperty), KMutableProperty0<V> {
+    override val setter: KMutableProperty0.Setter<V> get() = delegate.setter
+    override fun set(value: V): Unit = delegate.set(value)
+}
+
+internal open class LazyKProperty1<T, out V, out D : KProperty1<T, V>>(computeProperty: () -> D) :
+    LazyKProperty<V, D>(computeProperty), KProperty1<T, V> {
+    override val getter: KProperty1.Getter<T, V> get() = delegate.getter
+    override fun get(receiver: T): V = delegate.get(receiver)
+    override fun getDelegate(receiver: T): Any? = delegate.getDelegate(receiver)
+    override fun invoke(receiver: T): V = delegate.invoke(receiver)
+}
+
+internal class LazyKMutableProperty1<T, V, D : KMutableProperty1<T, V>>(computeProperty: () -> D) :
+    LazyKProperty1<T, V, D>(computeProperty), KMutableProperty1<T, V> {
+    override val setter: KMutableProperty1.Setter<T, V> get() = delegate.setter
+    override fun set(receiver: T, value: V): Unit = delegate.set(receiver, value)
+}

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ReflectionFactoryImpl.java
@@ -95,15 +95,18 @@ public class ReflectionFactoryImpl extends ReflectionFactory {
         KDeclarationContainerImpl container = getOwner(p);
         String signature = p.getSignature();
         if (!SystemPropertiesKt.getUseK1Implementation()) {
-            MatchResult result = KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matchEntire(signature);
-            if (result != null) {
-                List<String> values = result.getGroupValues();
-                return container.createLocalProperty(Integer.parseInt(values.get(1)), signature);
-            }
-            if (container instanceof KPackageImpl) {
-                KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
-                return new KotlinKProperty0(container, signature, p.getBoundReceiver(), kmProperty);
-            }
+            return new LazyKProperty0(() -> {
+                MatchResult result = KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matchEntire(signature);
+                if (result != null) {
+                    List<String> values = result.getGroupValues();
+                    return container.createLocalProperty(Integer.parseInt(values.get(1)), signature);
+                }
+                if (container instanceof KPackageImpl) {
+                    KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
+                    return new KotlinKProperty0(container, signature, p.getBoundReceiver(), kmProperty);
+                }
+                return new DescriptorKProperty0(container, p.getName(), signature, p.getBoundReceiver());
+            });
         }
         return new DescriptorKProperty0(container, p.getName(), signature, p.getBoundReceiver());
     }
@@ -113,15 +116,18 @@ public class ReflectionFactoryImpl extends ReflectionFactory {
         KDeclarationContainerImpl container = getOwner(p);
         String signature = p.getSignature();
         if (!SystemPropertiesKt.getUseK1Implementation()) {
-            MatchResult result = KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matchEntire(signature);
-            if (result != null) {
-                List<String> values = result.getGroupValues();
-                return (KMutableProperty0) container.createLocalProperty(Integer.parseInt(values.get(1)), signature);
-            }
-            if (container instanceof KPackageImpl) {
-                KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
-                return new KotlinKMutableProperty0(container, signature, p.getBoundReceiver(), kmProperty);
-            }
+            return new LazyKMutableProperty0(() -> {
+                MatchResult result = KDeclarationContainerImpl.LOCAL_PROPERTY_SIGNATURE.matchEntire(signature);
+                if (result != null) {
+                    List<String> values = result.getGroupValues();
+                    return container.createLocalProperty(Integer.parseInt(values.get(1)), signature);
+                }
+                if (container instanceof KPackageImpl) {
+                    KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
+                    return new KotlinKMutableProperty0(container, signature, p.getBoundReceiver(), kmProperty);
+                }
+                return new DescriptorKMutableProperty0(container, p.getName(), signature, p.getBoundReceiver());
+            });
         }
         return new DescriptorKMutableProperty0(container, p.getName(), signature, p.getBoundReceiver());
     }
@@ -131,10 +137,13 @@ public class ReflectionFactoryImpl extends ReflectionFactory {
         KDeclarationContainerImpl container = getOwner(p);
         String signature = p.getSignature();
         if (!SystemPropertiesKt.getUseK1Implementation()) {
-            if (container instanceof KPackageImpl) {
-                KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
-                return new KotlinKProperty1(container, signature, p.getBoundReceiver(), kmProperty);
-            }
+            return new LazyKProperty1(() -> {
+                if (container instanceof KPackageImpl) {
+                    KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
+                    return new KotlinKProperty1(container, signature, p.getBoundReceiver(), kmProperty);
+                }
+                return new DescriptorKProperty1(container, p.getName(), signature, p.getBoundReceiver());
+            });
         }
         return new DescriptorKProperty1(container, p.getName(), signature, p.getBoundReceiver());
     }
@@ -144,10 +153,13 @@ public class ReflectionFactoryImpl extends ReflectionFactory {
         KDeclarationContainerImpl container = getOwner(p);
         String signature = p.getSignature();
         if (!SystemPropertiesKt.getUseK1Implementation()) {
-            if (container instanceof KPackageImpl) {
-                KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
-                return new KotlinKMutableProperty1(container, signature, p.getBoundReceiver(), kmProperty);
-            }
+            return new LazyKMutableProperty1(() -> {
+                if (container instanceof KPackageImpl) {
+                    KmProperty kmProperty = container.findPropertyMetadata(p.getName(), signature);
+                    return new KotlinKMutableProperty1(container, signature, p.getBoundReceiver(), kmProperty);
+                }
+                return new DescriptorKMutableProperty1(container, p.getName(), signature, p.getBoundReceiver());
+            });
         }
         return new DescriptorKMutableProperty1(container, p.getName(), signature, p.getBoundReceiver());
     }

core/reflection.jvm/src/kotlin/reflect/jvm/internal/util.kt
@@ -296,14 +296,16 @@ internal fun Any?.asReflectFunction(): ReflectKFunction? = when (this) {
 }
 
 internal fun Any?.asReflectProperty(): ReflectKProperty<*>? = when (this) {
+    is LazyKProperty<*, *> -> delegate.asReflectProperty()
     is ReflectKProperty<*> -> this
-    is PropertyReference -> compute() as? ReflectKProperty
+    is PropertyReference -> compute().takeUnless { it === this }?.asReflectProperty()
     else -> null
 }
 
 internal fun Any?.asReflectCallable(): ReflectKCallable<*>? = when (this) {
+    is LazyKProperty<*, *> -> delegate.asReflectCallable()
     is ReflectKCallable<*> -> this
-    is CallableReference -> compute() as? ReflectKCallable<*>
+    is CallableReference -> compute().takeUnless { it === this }?.asReflectCallable()
     else -> null
 }
 

compiler/testData/reflection/lazyInitializationForDelegatedProperty/test.kt
@@ -1,0 +1,19 @@
+import kotlin.jvm.internal.*
+
+val p0 = ""
+var mp0 = ""
+
+class A {
+    val p1 = ""
+    var mp1 = ""
+
+    val String.p2 get() = this
+    var String.mp2 get() = this; set(v) {}
+}
+
+fun main() {
+    Reflection.property0(::p0 as PropertyReference0)
+    Reflection.mutableProperty0(::mp0 as MutablePropertyReference0)
+    Reflection.property1(A::p1 as PropertyReference1)
+    Reflection.mutableProperty1(A::mp1 as MutablePropertyReference1)
+}

compiler/tests-integration/tests/org/jetbrains/kotlin/reflection/ReflectionIntegrationTest.kt
@@ -95,10 +95,28 @@ class ReflectionIntegrationTest : KtUsefulTestCase() {
         compileAndRunProgram(KtTestUtil.getTestDataPathBase() + "/reflection/concurrentAccessToPrivateFunction")
     }
 
-    private fun compileAndRunProgram(root: String) {
+    // This test checks that we don't initialize full reflection (and specifically never read any metadata or parse protobuf) in case if
+    // delegated properties are used, whose `getValue`/`setValue` operators don't try to access anything from the `KProperty` parameter.
+    // Before Kotlin 2.3.20, where KT-81974 was fixed, bytecode of delegated properties contained unnecessary calls to
+    // `Reflection.property0/1/2` methods. If these methods start to read metadata or perform anything that can hurt performance, it will
+    // introduce performance regression in the old code (compiled by Kotlin < 2.3.20) if it has the new kotlin-reflect in classpath.
+    // So, effectively this test checks that `Reflection.property0/1/2` are still fast enough to be called from the old code.
+    fun testLazyInitializationForDelegatedProperty() {
+        // Run the program and print all classes loaded by the JVM.
+        val (stdout, stderr) = compileAndRunProgram(
+            KtTestUtil.getTestDataPathBase() + "/reflection/lazyInitializationForDelegatedProperty",
+            listOf("-verbose:class"),
+        )
+        if ("JvmProtoBuf" in stdout || "JvmProtoBuf" in stderr) {
+            fail("Full reflection should not be loaded for delegated properties:\n\n$stdout\n\n$stderr\n\n")
+        }
+    }
+
+    private fun compileAndRunProgram(root: String, jvmArgs: List<String> = emptyList()): TestOutput {
         val lib = CompilerTestUtil.compileJvmLibrary(File("$root/test.kt"))
 
-        runJava(
+        return runJava(
+            *jvmArgs.toTypedArray(),
             "-ea",
             "-classpath",
             listOf(
@@ -110,17 +128,27 @@ class ReflectionIntegrationTest : KtUsefulTestCase() {
         )
     }
 
-    private fun runJava(vararg args: String) {
+    private fun runJava(vararg args: String): TestOutput {
         val javaHome = System.getProperty("java.home")
         val javaExe = File(javaHome, "bin/java.exe").takeIf(File::exists)
             ?: File(javaHome, "bin/java").takeIf(File::exists)
             ?: error("Can't find 'java' executable in $javaHome")
 
-        val process = ProcessBuilder(javaExe.absolutePath, *args).start()
-        process.waitFor(1, TimeUnit.MINUTES)
-        val stderr = process.errorStream.reader().readText()
-        val stdout = process.inputStream.reader().readText()
+        val tmpdir = KotlinTestUtils.tmpDirForTest(this)
+        val stdoutFile = tmpdir.resolve("stdout.txt")
+        val stderrFile = tmpdir.resolve("stderr.txt")
+        val process = ProcessBuilder(javaExe.absolutePath, *args)
+            .redirectOutput(stdoutFile)
+            .redirectError(stderrFile)
+            .start()
+        val finished = process.waitFor(1, TimeUnit.MINUTES)
+        if (!finished) process.destroyForcibly()
+        val stdout = stdoutFile.readText()
+        val stderr = stderrFile.readText()
         val exitCode = process.exitValue()
         assertEquals("Program exited with exit code $exitCode.\nStdout:\n$stdout\nStderr:\n$stderr", 0, exitCode)
+        return TestOutput(stdout, stderr)
     }
+
+    private data class TestOutput(val stdout: String, val stderr: String)
 }