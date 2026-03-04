f619e2ea6db0b3560e615396fd288a1c012a81a8
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2026-03-04 01:20:15 +0100

    [Wasm] Replace `wasm_memory_copy` intrinsic with a manual implementation until the next bootstrap
    KT-65030

--- libraries/stdlib/wasm/internal/kotlin/wasm/internal/WasmInstructions.kt
+++ libraries/stdlib/wasm/internal/kotlin/wasm/internal/WasmInstructions.kt
@@ -542,10 +542,22 @@ internal fun wasm_memory_grow(delta: Int): Int =
 /**
  * Copy `size` bytes from `src` to `dst` in linear memory.
  */
-@Suppress("UNUSED_PARAMETER")
-@WasmOp(WasmOp.MEMORY_COPY)
-internal fun wasm_memory_copy(dst: Int, src: Int, size: Int): Unit =
-    implementedAsIntrinsic
+//@Suppress("UNUSED_PARAMETER")
+//@WasmOp(WasmOp.MEMORY_COPY)
+//internal fun wasm_memory_copy(dst: Int, src: Int, size: Int): Unit =
+//    implementedAsIntrinsic
+
+// TODO replace this function declaration with intrinsic above after bootstrap
+private fun wasm_memory_copy(dst: Int, src: Int, size: Int) {
+    var s = src
+    var d = dst
+    val end = src + size
+    while (s < end) {
+        wasm_i32_store8(d, wasm_i32_load8(s))
+        s++
+        d++
+    }
+}
 
 @WasmOp(WasmOp.I32_CLZ)
 internal fun wasm_i32_clz(a: Int): Int =
