e1824201a1e1377445a05de9dd7848695e84661b
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-03-04 22:14:07 +0100

    [Tests][Wasm] Mute fresh regression tests in backward klib compatibility testing
    ^KT-82803
    ^KT-83728

--- compiler/testData/codegen/box/coroutines/kt82803.kt
+++ compiler/testData/codegen/box/coroutines/kt82803.kt
@@ -1,5 +1,7 @@
 // WITH_STDLIB
 // WITH_COROUTINES
+// IGNORE_KLIB_BACKEND_ERRORS_WITH_CUSTOM_FIRST_STAGE: Wasm-JS:2.3
+// ^^^ K/Wasm backend has issues KT-82803 & KT-83728 with FIR2IR v.2.3.0, fixed only in 2.4.0-Beta1. So, a test `2.3.0 frontend + current backend` expectedly fails
 
 import helpers.*
 import kotlin.coroutines.*
