c87482e72c120929097534affbae817a9f6d0ba3
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-03-05 15:57:50 +0900

    [Analysis API] Deprecate 'renderAsDataClassToString()'
    ^KT-84932

--- analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/utils/ClassRenderingUtils.kt
+++ analysis/analysis-api/src/org/jetbrains/kotlin/analysis/api/utils/ClassRenderingUtils.kt
@@ -1,20 +1,16 @@
 /*
- * Copyright 2010-2023 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
-package org.jetbrains.kotlin.analysis.utils.relfection
+package org.jetbrains.kotlin.analysis.api.utils
 
+import org.jetbrains.kotlin.analysis.utils.printer.prettyPrint
 import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.full.declaredMemberProperties
-import org.jetbrains.kotlin.analysis.utils.printer.prettyPrint
 import kotlin.reflect.jvm.isAccessible
 
-@Deprecated(
-    "Unintentionally exposed implementation detail. Do not use",
-    level = DeprecationLevel.ERROR
-)
-public fun Any.renderAsDataClassToString(): String = prettyPrint {
+internal fun Any.renderAsDataClassToString(): String = prettyPrint {
     append(this@renderAsDataClassToString::class.qualifiedName)
     append("(")
     printCollection(this@renderAsDataClassToString::class.declaredMemberProperties) { property ->
