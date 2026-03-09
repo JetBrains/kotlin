0c5ae63d9ed2fc65dd881c821c42184c980e043d
Aleksandr.Shefer <aleksandr.shefer@jetbrains.com>
2026-03-09 16:01:52 +0100

    [Wasm] change return type for suspend functions

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmBackendContext.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmBackendContext.kt
@@ -29,6 +29,7 @@ import org.jetbrains.kotlin.ir.declarations.impl.IrExternalPackageFragmentImpl
 import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
 import org.jetbrains.kotlin.ir.symbols.IrFileSymbol
 import org.jetbrains.kotlin.ir.symbols.impl.DescriptorlessExternalPackageFragmentSymbol
+import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.types.IrTypeSystemContext
 import org.jetbrains.kotlin.ir.types.IrTypeSystemContextImpl
 import org.jetbrains.kotlin.ir.util.SymbolTable
@@ -167,5 +168,9 @@ class WasmBackendContext(
                 compilationException("Invalid special size count", null)
             }
         }
+    }
+
+    override fun loweredSuspendFunctionReturnType(function: IrFunction): IrType {
+        return function.returnType
     }
 }
