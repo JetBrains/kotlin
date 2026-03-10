3aec37bf7668f0208cfa75f251b40883881af841
Anton Lakotka <anton.lakotka@jetbrains.com>
2026-03-10 10:51:33 +0100

    [Maven] Upgrade Surefire & JUnit to version 6
    - Junit6 doesn't support Java 8, but it is not necessary for Maven plugin unit testing
    - Also upgraded maven harness test library
    
    ^KT-83114

--- libraries/tools/kotlin-maven-plugin/src/test/kotlin/org/jetbrains/kotlin/maven/KotlinCompilationFailureExceptionTest.kt
+++ libraries/tools/kotlin-maven-plugin/src/test/kotlin/org/jetbrains/kotlin/maven/KotlinCompilationFailureExceptionTest.kt
@@ -1,64 +1,60 @@
 package org.jetbrains.kotlin.maven
 
 import org.apache.maven.plugin.testing.SilentLog
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation.Companion.create
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
-
+import kotlin.test.*
 
 class KotlinCompilationFailureExceptionTest {
-    @org.junit.Test
+    @Test
     fun testNoLocation() {
         val collector = MavenPluginLogMessageCollector(SilentLog())
         collector.report(CompilerMessageSeverity.ERROR, "Something went wrong", null)
 
         try {
             collector.throwKotlinCompilerException()
-            org.junit.Assert.fail()
+            fail()
         } catch (e: KotlinCompilationFailureException) {
-            org.junit.Assert.assertEquals("Something went wrong", e.getLongMessage().trim { it <= ' ' })
+            assertEquals("Something went wrong", e.getLongMessage().trim { it <= ' ' })
         }
     }
 
-    @org.junit.Test
+    @Test
     fun testLocationNoLineNoColumn() {
         val collector = MavenPluginLogMessageCollector(SilentLog())
-        collector.report(CompilerMessageSeverity.ERROR, "Error in file", CompilerMessageLocation.create("myfile.txt", -1, -1, "nothing"))
+        collector.report(CompilerMessageSeverity.ERROR, "Error in file", create("myfile.txt", -1, -1, "nothing"))
 
         try {
             collector.throwKotlinCompilerException()
-            org.junit.Assert.fail()
+            fail()
         } catch (e: KotlinCompilationFailureException) {
-            org.junit.Assert.assertEquals("myfile.txt: Error in file", e.getLongMessage().trim { it <= ' ' })
+            assertEquals("myfile.txt: Error in file", e.getLongMessage().trim { it <= ' ' })
         }
     }
 
-    @org.junit.Test
+    @Test
     fun testLocationNoColumn() {
         val collector = MavenPluginLogMessageCollector(SilentLog())
-        collector.report(
-            CompilerMessageSeverity.ERROR,
-            "Error in file",
-            CompilerMessageLocation.create("myfile.txt", 777, -1, "nothing")
-        )
+        collector.report(CompilerMessageSeverity.ERROR, "Error in file", create("myfile.txt", 777, -1, "nothing"))
 
         try {
             collector.throwKotlinCompilerException()
-            org.junit.Assert.fail()
+            fail()
         } catch (e: KotlinCompilationFailureException) {
-            org.junit.Assert.assertEquals("myfile.txt:[777] Error in file", e.getLongMessage().trim { it <= ' ' })
+            assertEquals("myfile.txt:[777] Error in file", e.getLongMessage().trim { it <= ' ' })
         }
     }
 
-    @org.junit.Test
+    @Test
     fun testLocationWithLocation() {
         val collector = MavenPluginLogMessageCollector(SilentLog())
-        collector.report(CompilerMessageSeverity.ERROR, "Error in file", CompilerMessageLocation.create("myfile.txt", 777, 9, "nothing"))
+        collector.report(CompilerMessageSeverity.ERROR, "Error in file", create("myfile.txt", 777, 9, "nothing"))
 
         try {
             collector.throwKotlinCompilerException()
-            org.junit.Assert.fail()
+            fail()
         } catch (e: KotlinCompilationFailureException) {
-            org.junit.Assert.assertEquals("myfile.txt:[777,9] Error in file", e.getLongMessage().trim { it <= ' ' })
+            assertEquals("myfile.txt:[777,9] Error in file", e.getLongMessage().trim { it <= ' ' })
         }
     }
 }
