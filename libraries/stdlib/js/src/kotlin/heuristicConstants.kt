249a5ba07178080c333a890c41caa4ab7b350edb
Edoardo Luppi <lp.edoardo@gmail.com>
2026-03-10 15:47:53 +0100

    [K/JS] Improve performance of CharArray.concatToString
    ^KT-84791 Fixed

--- libraries/stdlib/js/src/kotlin/heuristicConstants.kt
+++ libraries/stdlib/js/src/kotlin/heuristicConstants.kt
@@ -7,3 +7,12 @@ package kotlin
 
 // The discussion regarding the constant https://github.com/JetBrains/kotlin/pull/5455
 internal const val OPTIMAL_SIZE_FOR_REGULAR_LOOP = 16
+
+// The optimal size of a chunk of character codes passed to String.fromCharCode while building
+// a string from a character array. The value has been chosen through benchmarking.
+//
+// Chunking may split a surrogate pair, but it is safe because code unit ordering is preserved
+// and so the surrogate pair remains valid in the resulting string.
+//
+// See discussion at https://github.com/JetBrains/kotlin/pull/5732
+internal const val OPTIMAL_CHAR_CODE_CHUNK_SIZE = 4096
