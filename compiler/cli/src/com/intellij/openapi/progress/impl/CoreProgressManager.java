f2043f912e81daa9df4cff42af6ac8b645f78c92
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2026-03-11 16:09:39 +0100

    IJ SDK: adjust `CoreProgressManager` to avoid using `sun.misc.Unsafe`
    `CoreProgressManager` from the IJ SDK uses
    `ConcurrentLongObjectHashMap`, which in turn uses `sun.misc.Unsafe`.
    
    `sun.misc.Unsafe` is being phased out in modern JDK versions:
    https://openjdk.org/jeps/471.
    
    In the scope of KT-83133, the K/N compiler should stop using
    `sun.misc.Unsafe`, including this usage. Other Kotlin backends also have
    similar goals.
    
    `sun.misc.Unsafe` usage in `CoreProgressManager` is fixed in IJ SDK 253
    (IJPL-191435). But Kotlin is not yet updated to it (KT-81457).
    
    Luckily, because of another workaround, Kotlin has its own version of
    `CoreProgressManager`, so it can be adjusted without updating the IJ
    SDK.
    
    This commit does exactly that. Specifically, it replaces usages of
    `ConcurrentLongObjectHashMap` with
    `java.util.concurrent.ConcurrentHashMap`.
    
    The former was likely used for performance reasons, but this change
    is not expected to have any actual performance impact in Kotlin.

--- compiler/cli/src/com/intellij/openapi/progress/impl/CoreProgressManager.java
+++ compiler/cli/src/com/intellij/openapi/progress/impl/CoreProgressManager.java
@@ -23,10 +23,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.*;
 import com.intellij.openapi.wm.ex.ProgressIndicatorEx;
 import com.intellij.util.ExceptionUtil;
-import com.intellij.util.Java11Shim;
 import com.intellij.util.SystemProperties;
 import com.intellij.util.concurrency.AppExecutorUtil;
-import com.intellij.util.containers.ConcurrentLongObjectMap;
 import com.intellij.util.ui.EDT;
 import org.jetbrains.annotations.*;
 
@@ -44,11 +42,17 @@ import static com.intellij.openapi.application.ModalityKt.currentThreadContextMo
 import static com.intellij.openapi.progress.impl.ProgressManagerScopeKt.ProgressManagerScope;
 
 /**
- * This class is a simplified version of the original one, which tries to avoid loading a specific class
+ * This class is a simplified version of the original one, which applies two workarounds:
+ *
+ * 1. Try to avoid loading a specific class
  * kotlinx/coroutines/internal/intellij/IntellijCoroutine from kotlinx.coroutines fork in a CLI environment.
  *
  * After IJPL-207644 has been fixed, since 253 we can hopefully remove the workaround, via setting
  * `ide.can.use.coroutines.fork` property to false.
+ *
+ * 2. Replace `ConcurrentLongObjectMap` with `ConcurrentHashMap`: the former uses `sun.misc.Unsafe` which is
+ * being phased out in modern JDK versions: https://openjdk.org/jeps/471.
+ * The original version of this class is already fixed in 253 (IJPL-191435).
  *
  * TODO: Remove it once KT-81457 is fixed
  */
@@ -65,11 +69,11 @@ public class CoreProgressManager extends ProgressManager implements Disposable {
   // THashMap is avoided here because of tombstones overhead
   private static final Map<ProgressIndicator, Set<Thread>> threadsUnderIndicator = new HashMap<>(); // guarded by threadsUnderIndicator
   // the active indicator for the thread id
-  private static final ConcurrentLongObjectMap<ProgressIndicator> currentIndicators =
-    Java11Shim.Companion.getINSTANCE().createConcurrentLongObjectMap();
+  private static final ConcurrentMap<Long, ProgressIndicator> currentIndicators =
+    new ConcurrentHashMap<>();
   // top-level indicators for the thread id
-  private static final ConcurrentLongObjectMap<ProgressIndicator> threadTopLevelIndicators =
-    Java11Shim.Companion.getINSTANCE().createConcurrentLongObjectMap();
+  private static final ConcurrentMap<Long, ProgressIndicator> threadTopLevelIndicators =
+    new ConcurrentHashMap<>();
   // threads which are running under canceled indicator
   private static final Set<Thread> threadsUnderCanceledIndicator = new HashSet<>(); // guarded by threadsUnderIndicator
 
