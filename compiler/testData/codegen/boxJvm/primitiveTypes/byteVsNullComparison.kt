a7f4a3a58504ef2befdf1875aaadd3d36ce61497
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-03-12 17:17:33 +0100

    K2: reproduce KT-83587

---
+++ compiler/testData/codegen/boxJvm/primitiveTypes/byteVsNullComparison.kt
@@ -0,0 +1,46 @@
+// TARGET_BACKEND: JVM
+// IGNORE_BACKEND_K2: JVM_IR
+// ISSUE: KT-83587
+// JVM_TARGET: 11
+// JDK_KIND: FULL_JDK_11
+// DUMP_IR
+
+fun box(): String {
+    attempt("bool") {
+        val value = getNullAsPlatform(true)
+        value == true
+    }
+
+    attempt("short") {
+        val value = getNullAsPlatform(0.toShort())
+        value == 0.toShort()
+    }
+
+    attempt("char") {
+        val value = getNullAsPlatform(0.toChar())
+        value == 0.toChar()
+    }
+
+    attempt("long") {
+        val value = getNullAsPlatform(0L)
+        value == 0L
+    }
+
+    attempt("int") {
+        val value = getNullAsPlatform(0)
+        value == 0
+    }
+
+    attempt("byte") {
+        val value = getNullAsPlatform(0.toByte())
+        value == 0.toByte()
+    }
+    return "OK"
+}
+
+inline fun attempt(name: String, block: () -> Boolean) {
+    println(name + ": " + block())
+}
+
+// inferring the return type to get the platform type
+fun <E> getNullAsPlatform(e: E) /*: E? */ = java.util.List.of(e).toMutableList().also { it.set(0, null) }.first()
