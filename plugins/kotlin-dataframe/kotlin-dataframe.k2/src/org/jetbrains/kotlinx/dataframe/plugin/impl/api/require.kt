1461a7a33911fe9df8486f4c6256bed06fd89f12
Jolan Rensen <jolan.rensen@jetbrains.com>
2026-03-16 21:11:04 +0100

    [KDF] simplified requireColumn version using convertAsColumn

--- plugins/kotlin-dataframe/kotlin-dataframe.k2/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/require.kt
+++ plugins/kotlin-dataframe/kotlin-dataframe.k2/src/org/jetbrains/kotlinx/dataframe/plugin/impl/api/require.kt
@@ -5,10 +5,6 @@
 
 package org.jetbrains.kotlinx.dataframe.plugin.impl.api
 
-import org.jetbrains.kotlinx.dataframe.api.asColumn
-import org.jetbrains.kotlinx.dataframe.api.convert
-import org.jetbrains.kotlinx.dataframe.api.insert
-import org.jetbrains.kotlinx.dataframe.api.under
 import org.jetbrains.kotlinx.dataframe.plugin.extensions.ColumnType
 import org.jetbrains.kotlinx.dataframe.plugin.impl.*
 
@@ -27,16 +23,9 @@ class Require1 : AbstractSchemaModificationInterpreter() {
     val Arguments.typeArg1: ColumnType by arg()
 
     override fun Arguments.interpret(): PluginDataFrameSchema {
-        val columnExists = receiver.asDataFrame().containsColumn(column.path)
-        return receiver.modify {
-            if (columnExists) {
-                convert { column }.asColumn {
-                    simpleColumnOf(it.name(), typeArg1.coneType).asDataColumn()
-                }
-            } else {
-                val newColumn = column.resolve(receiver).single { it.isImpliedColumn }
-                insert(newColumn.column.asDataColumn()).under { column.path.dropLast() }
-            }
+        // if the column doesn't exist yet, convertAsColumn considers it "implied"
+        return receiver.convertAsColumn(column) {
+            simpleColumnOf(it.name(), typeArg1.coneType)
         }
     }
 }
