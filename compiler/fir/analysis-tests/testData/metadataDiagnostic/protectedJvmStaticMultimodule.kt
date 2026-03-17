6ea306ffd61c114c43d2f443bf7fdd2fd65bee10
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-03-17 14:28:57 +0200

    [Test] Reproduce KT-85021

---
+++ compiler/fir/analysis-tests/testData/metadataDiagnostic/protectedJvmStaticMultimodule.kt
@@ -0,0 +1,22 @@
+// LANGUAGE: +MultiPlatformProjects
+// ISSUE: KT-85021
+// WITH_STDLIB
+// DIAGNOSTICS: -NOTHING_TO_INLINE
+// MODULE: m1-common
+// FILE: m1.kt
+
+import kotlin.jvm.JvmStatic
+
+abstract class Parent {
+    companion object {
+        @JvmStatic
+        protected val someString = "OK"
+    }
+}
+
+// MODULE: m2-common(m1-common)
+// FILE: m2.kt
+
+class Child : Parent() {
+    protected inline fun foo() = <!SUBCLASS_CANT_CALL_COMPANION_PROTECTED_NON_STATIC!>someString<!>
+}
