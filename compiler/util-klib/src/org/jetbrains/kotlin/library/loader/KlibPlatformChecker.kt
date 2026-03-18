1d0f98ceb13c79abaa4e2aaf39d024d276f74933
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-03-18 11:00:46 +0100

    Klib API: Rewrite KlibPlatformChecker implementations to make them safer
    With the new non-inline utility function
    `firstFailedCheckOf(vararg checks: () -> PlatformCheckMismatch?)` it
    becomes not possible to make an accidental return from
    the `KlibPlatformChecker.check(library: BaseKotlinLibrary)` function
    if only the current check needs to be skipped. I.e.
    ```
    override fun check(library: BaseKotlinLibrary) = firstFailedCheckOf(
        { library.target ?: return null },
                            // ^^^ this will make a return only from
                            // the current lambda, not the whole function
        { ... },
        { ... },
    )
    ```
    
    ^KT-85057

--- compiler/util-klib/src/org/jetbrains/kotlin/library/loader/KlibPlatformChecker.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/loader/KlibPlatformChecker.kt
@@ -21,16 +21,20 @@ interface KlibPlatformChecker {
      * If [target] is not null, then additionally checks that the given target is supported in the library.
      */
     class Native(private val target: String? = null) : KlibPlatformChecker {
-        override fun check(library: BaseKotlinLibrary): PlatformCheckMismatch? {
-            val platformMismatch: PlatformCheckMismatch? = checkPlatform(
-                expectedPlatform = BuiltInsPlatform.NATIVE,
-                actualPlatform = library.builtInsPlatform
-            )
-            if (platformMismatch != null) return platformMismatch
-
-            val expectedTarget: String = target ?: return null
-            return checkTarget(BuiltInsPlatform.NATIVE, expectedTarget, actualTargets = library.nativeTargets)
-        }
+        override fun check(library: BaseKotlinLibrary) = firstFailedCheckOf(
+            {
+                checkPlatform(
+                    expectedPlatform = BuiltInsPlatform.NATIVE,
+                    actualPlatform = library.builtInsPlatform,
+                )
+            }, targetCheck@{
+                checkTarget(
+                    platform = BuiltInsPlatform.NATIVE,
+                    expectedTarget = target ?: return@targetCheck null,
+                    actualTargets = library.nativeTargets,
+                )
+            }
+        )
     }
 
     /**
@@ -40,37 +44,44 @@ interface KlibPlatformChecker {
      * - Or a metadata-aware Kotlin/Native library
      */
     class NativeMetadata(private val target: String) : KlibPlatformChecker {
-        override fun check(library: BaseKotlinLibrary): PlatformCheckMismatch? {
-            /**
-             * Common (platform-agnostic) libraries use [BuiltInsPlatform.COMMON] built-ins platform.
-             * But [BuiltInsPlatform.COMMON] is a special value that is not written to the manifest,
-             * effectively leaving such libraries without the `builtins_platform` property.
-             * So, to make this check work properly, we should skip validation of the platform when
-             * the `builtins_platform` property is missing in the manifest assuming this is a "common" platform.
-             */
-            val actualBuiltInsPlatform = library.builtInsPlatform
-            if (actualBuiltInsPlatform != null) {
-                val platformMismatch: PlatformCheckMismatch? = checkPlatform(
+        override fun check(library: BaseKotlinLibrary) = firstFailedCheckOf(
+            platformCheck@{
+                checkPlatform(
                     expectedPlatform = BuiltInsPlatform.NATIVE,
-                    actualPlatform = actualBuiltInsPlatform
+                    /**
+                     * Common (platform-agnostic) libraries use [BuiltInsPlatform.COMMON] built-ins platform.
+                     * But [BuiltInsPlatform.COMMON] is a special value that is not written to the manifest,
+                     * effectively leaving such libraries without the `builtins_platform` property.
+                     * So, to make this check work properly, we should skip validation of the platform when
+                     * the `builtins_platform` property is missing in the manifest assuming this is a "common" platform.
+                     */
+                    actualPlatform = library.builtInsPlatform ?: return@platformCheck null,
                 )
-                if (platformMismatch != null) return platformMismatch
+            }, targetCheck@{
+                val commonizerNativeTargets = library.commonizerNativeTargets
+                if (!commonizerNativeTargets.isNullOrEmpty()) {
+                    /**
+                     * This is a commonized Kotlin/Native library.
+                     * We need to check that the given target is supported in the commonized library.
+                     */
+                    checkTarget(
+                        platform = BuiltInsPlatform.NATIVE,
+                        expectedTarget = target,
+                        actualTargets = commonizerNativeTargets,
+                    )
+                } else {
+                    checkTarget(
+                        platform = BuiltInsPlatform.NATIVE,
+                        expectedTarget = target,
+                        /**
+                         * Some metadata-only Kotlin/Native libraries might not have any targets written in the manifest.
+                         * We need to skip the check if this is the case.
+                         */
+                        actualTargets = library.nativeTargets.takeIf { it.isNotEmpty() } ?: return@targetCheck null,
+                    )
+                }
             }
-
-            val commonizerNativeTargets = library.commonizerNativeTargets
-            if (!commonizerNativeTargets.isNullOrEmpty()) {
-                // This is a commonized Kotlin/Native library. We need to check that the given target is supported in the commonized library.
-                return checkTarget(BuiltInsPlatform.NATIVE, expectedTarget = target, actualTargets = commonizerNativeTargets)
-            }
-
-            val nativeTargets = library.nativeTargets
-            if (nativeTargets.isEmpty()) {
-                // This is a metadata-aware Kotlin/Native library that might not have any targets written in the manifest.
-                return null
-            }
-
-            return checkTarget(BuiltInsPlatform.NATIVE, expectedTarget = target, actualTargets = nativeTargets)
-        }
+        )
     }
 
     /**
@@ -87,24 +98,26 @@ interface KlibPlatformChecker {
      * If [target] is not null, then additionally checks that the given target is supported in the library.
      */
     class Wasm(private val target: String? = null) : KlibPlatformChecker {
-        override fun check(library: BaseKotlinLibrary): PlatformCheckMismatch? {
-            val platformMismatch: PlatformCheckMismatch? = checkPlatform(
-                expectedPlatform = BuiltInsPlatform.WASM,
-                actualPlatform = library.builtInsPlatform
-            )
-            if (platformMismatch != null) return platformMismatch
-
-            val expectedTarget: String = target ?: return null
-            val actualTargets: List<String> = library.wasmTargets
-
-            if (actualTargets.isEmpty() && library.versions.abiVersion?.isAtMost(1, 8, 0) == true) {
-                // Only Kotlin/Wasm KLIBs produced with the compiler version >= 2.0.0 have targets in the manifest (see KT-66327).
-                // In 2.0.0 (as well as in the preceding 1.9.x) we had the ABI version = 1.8.0.
-                return null
+        override fun check(library: BaseKotlinLibrary) = firstFailedCheckOf(
+            {
+                checkPlatform(
+                    expectedPlatform = BuiltInsPlatform.WASM,
+                    actualPlatform = library.builtInsPlatform
+                )
+            }, targetCheck@{
+                checkTarget(
+                    platform = BuiltInsPlatform.WASM,
+                    expectedTarget = target ?: return@targetCheck null,
+                    actualTargets = library.wasmTargets.takeUnless {
+                        /**
+                         * Only Kotlin/Wasm KLIBs produced with the compiler version >= 2.0.0 have targets in the manifest (see KT-66327).
+                         * In 2.0.0 (as well as in the preceding 1.9.x) we had the ABI version = 1.8.0.
+                         */
+                        it.isEmpty() && library.versions.abiVersion?.isAtMost(1, 8, 0) == true
+                    } ?: return@targetCheck null
+                )
             }
-
-            return checkTarget(BuiltInsPlatform.WASM, expectedTarget, actualTargets)
-        }
+        )
     }
 
     companion object {
@@ -134,5 +147,14 @@ interface KlibPlatformChecker {
                 actual = "${platform.name}[${actualTargets.joinToString()}]"
             )
         }
+
+        private fun firstFailedCheckOf(vararg checks: () -> PlatformCheckMismatch?): PlatformCheckMismatch? {
+            for (check in checks) {
+                val mismatch = check()
+                if (mismatch != null) return mismatch
+            }
+            return null
+        }
     }
 }
+
