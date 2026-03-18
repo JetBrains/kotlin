2ec1bafc29bed104b575f0b4b2f95e0f1673d454
Anton Lakotka <anton.lakotka@jetbrains.com>
2026-03-18 09:36:33 +0100

    [Maven] Update EnvironmentJdkProvider
    * for JDK 1.8 look for JDK_1_8 env variable
    
    * look at `JDK_{number}_0` environment variables as well
    Team City doesn't set JDK_17, but only JDK_17_0.

--- libraries/tools/kotlin-maven-plugin-test/src/test/kotlin/org/jetbrains/kotlin/maven/test/jdk/EnvironmentJdkProvider.kt
+++ libraries/tools/kotlin-maven-plugin-test/src/test/kotlin/org/jetbrains/kotlin/maven/test/jdk/EnvironmentJdkProvider.kt
@@ -8,8 +8,16 @@ import kotlin.io.path.isDirectory
 
 object EnvironmentJdkProvider : JdkProvider {
     override fun getJavaHome(version: TestVersions.Java): Path? {
-        val envVar = "JDK_${version.numericVersion}"
-        val envValue = System.getenv(envVar) ?: return null
+        val jdkVersion = when (version) {
+            TestVersions.Java.JDK_1_8 -> "1_8"
+            else -> version.numericVersion.toString()
+        }
+
+        val envVar1 = "JDK_${jdkVersion}"
+        val envVar2 = "JDK_${jdkVersion}_0"
+        val envValue = System.getenv(envVar1)
+            ?: System.getenv(envVar2)
+            ?: return null
         val javaHome = Path(envValue)
         return javaHome.takeIf { it.exists() && it.isDirectory() }
     }
