b8c08294bb18eba0bc88fa0860c782610be7b133
Denis Zharkov <Denis.Zharkov@jetbrains.com>
2026-03-18 09:50:46 +0000

    [stdlib] Clarify NOTE section in the kdoc for BuilderInference
    ^KT-52400 Related

--- libraries/stdlib/src/kotlin/annotations/Inference.kt
+++ libraries/stdlib/src/kotlin/annotations/Inference.kt
@@ -28,7 +28,7 @@ import kotlin.experimental.ExperimentalTypeInference
  * Here the type argument of the resulting sequence is inferred to `String` from
  * the argument of the [SequenceScope.yield] function, that is called inside the lambda passed to [sequence].
  *
- * **Note:** this annotation is deprecated and must not be used anymore. Builder inference is enabled automatically for builder calls if needed.
+ * **Note:** This annotation is deprecated and should no longer be used. The compiler automatically enables builder inference for builder calls when needed.
  */
 @Target(VALUE_PARAMETER, FUNCTION, PROPERTY)
 @Retention(AnnotationRetention.BINARY)
