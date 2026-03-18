3f74f05d8e647aade950c82be6c6b6e6127e3927
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2026-03-18 20:56:41 +0100

    Disable :native:native.tests:klib-ir-inliner:test
     #KT-85080

--- native/native.tests/klib-ir-inliner/build.gradle.kts
+++ native/native.tests/klib-ir-inliner/build.gradle.kts
@@ -44,6 +44,8 @@ projectTests {
         allowParallelExecution = true,
         requirePlatformLibs = true,
     ) {
+        enabled = false
+
         // To workaround KTI-2421, we make these tests run on JDK 11 instead of the project-default JDK 8.
         // Kotlin test infra uses reflection to access JDK internals.
         // With JDK 11, some JVM args are required to silence the warnings caused by that:
