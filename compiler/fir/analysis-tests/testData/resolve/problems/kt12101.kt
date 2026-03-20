732f310a78a3b15e5dcee262163cebe0c844750f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-03-20 12:59:33 +0100

    fixup! Add reproducer test for KT-12101

--- compiler/fir/analysis-tests/testData/resolve/problems/kt12101.kt
+++ compiler/fir/analysis-tests/testData/resolve/problems/kt12101.kt
@@ -1,13 +1,13 @@
-// RUN_PIPELINE_TILL: BACKEND
+// RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-12101
 // WITH_STDLIB
 
 // KT-12101: Confusing error message for TYPE_PARAMETER_AS_REIFIED in 'arrayOf<T>' and similar calls
 class Foo<E: Any>() {
     fun foo() {
-        arrayOf<<!TYPE_PARAMETER_AS_REIFIED!>E?<!>>()
-        Array<<!TYPE_PARAMETER_AS_REIFIED!>E?<!>>(size = 1, init = { i -> null })
-        arrayOfNulls<<!TYPE_PARAMETER_AS_REIFIED!>E<!>>(6)
+        arrayOf<<!TYPE_PARAMETER_AS_REIFIED("E")!>E?<!>>()
+        Array<<!TYPE_PARAMETER_AS_REIFIED("E")!>E?<!>>(size = 1, init = { i -> null })
+        arrayOfNulls<<!TYPE_PARAMETER_AS_REIFIED("E")!>E<!>>(6)
     }
 }
 
