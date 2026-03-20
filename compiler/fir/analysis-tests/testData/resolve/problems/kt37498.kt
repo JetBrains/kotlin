33c244273c937f7cffc199d2481b9e9958980adb
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-03-20 08:00:32 +0100

    fixup! Add reproducer test for KT-37498

--- compiler/fir/analysis-tests/testData/resolve/problems/kt37498.kt
+++ compiler/fir/analysis-tests/testData/resolve/problems/kt37498.kt
@@ -4,7 +4,7 @@
 // KT-37498: Misleading diagnostic message for incorrectly specified lambda parameter
 
 fun <R> foo(op: (R) -> R) {}
-val test = foo <!ARGUMENT_TYPE_MISMATCH!>{ _: String -> }<!>
+val test = foo <!ARGUMENT_TYPE_MISMATCH("(String) -> Unit; (String) -> String")!>{ _: String -> }<!>
 
 /* GENERATED_FIR_TAGS: functionDeclaration, functionalType, lambdaLiteral, nullableType, propertyDeclaration,
 typeParameter */
