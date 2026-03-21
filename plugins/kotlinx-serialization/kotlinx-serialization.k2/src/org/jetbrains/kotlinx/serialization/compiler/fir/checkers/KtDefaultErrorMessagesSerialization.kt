1826be152c205230592e997717e6758c45871316
xiaozhikang <xiaozhikang0916@gmail.com>
2026-03-21 15:51:38 +0800

    [Protobuf] Improve ProtoUnknownFields error messages to start with error cause

--- plugins/kotlinx-serialization/kotlinx-serialization.k2/src/org/jetbrains/kotlinx/serialization/compiler/fir/checkers/KtDefaultErrorMessagesSerialization.kt
+++ plugins/kotlinx-serialization/kotlinx-serialization.k2/src/org/jetbrains/kotlinx/serialization/compiler/fir/checkers/KtDefaultErrorMessagesSerialization.kt
@@ -236,20 +236,20 @@ object KtDefaultErrorMessagesSerialization : BaseDiagnosticRendererFactory() {
         )
         map.put(
             FirSerializationErrors.PROTO_UNKNOWN_FIELDS_MULTIPLE_ANNOTATIONS,
-            "In class ''{0}'', fields annotated with @ProtoUnknownFields: ''{1}''; only one field per class is allowed.",
+            "@ProtoUnknownFields must not be present on more than one property, specified on ''{1}'' in class ''{0}''.",
             CommonRenderers.STRING,
             CommonRenderers.STRING,
         )
         map.put(
             FirSerializationErrors.PROTO_UNKNOWN_FIELDS_WRONG_TYPE,
-            "In class ''{0}'', field ''{1}'' annotated with @ProtoUnknownFields must have type ''ProtoUnknownFieldHolder'', but has type ''{2}''.",
+            "@ProtoUnknownFields on field ''{1}'' in class ''{0}'' must have type ProtoUnknownFieldHolder, but has type ''{2}''.",
             CommonRenderers.STRING,
             CommonRenderers.STRING,
             CommonRenderers.STRING,
         )
         map.put(
             FirSerializationErrors.PROTO_UNKNOWN_FIELDS_MISSING_DEFAULT,
-            "In class ''{0}'', non-nullable field ''{1}'' annotated with @ProtoUnknownFields must have a default value ''ProtoUnknownFieldHolder.Empty''.",
+            "@ProtoUnknownFields on non-nullable field ''{1}'' in class ''{0}'' must have a default value ProtoUnknownFieldHolder.Empty.",
             CommonRenderers.STRING,
             CommonRenderers.STRING,
         )
