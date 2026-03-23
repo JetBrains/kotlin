402bc223616eb533fd416422d07ee19aed9a146f
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-03-23 18:25:02 +0100

    [Bench] Migrate benchmarks under test source set
    To make the structure more refined and to get rid of a warning on `createForTests` (it should be used only in tests)
    
    Remove unused Gradle config stuff

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -14,7 +14,8 @@ dependencies {
 }
 
 sourceSets {
-    "main" { projectDefault() }
+    "main" { none() }
+    "test" { projectDefault() }
 }
 
 optInToK1Deprecation()
@@ -39,40 +40,12 @@ benchmark {
         }
     }
     targets {
-        register("main")
+        register("test")
     }
 }
 
-tasks.withType<Zip>().matching { it.name == "mainBenchmarkJar" }.configureEach {
-    isZip64 = true
-    archiveFileName.set("benchmarks.jar")
-}
-
-val benchmarkTasks = listOf("mainBenchmark")
-tasks.withType<JavaExec>().matching { it.name in benchmarkTasks }.configureEach {
+tasks.withType<JavaExec>().matching { it.name == "testBenchmark" }.configureEach {
     dependsOn(":createIdeaHomeForTests")
     systemProperty("idea.home.path", ideaHomePathForTests().get().asFile.canonicalPath)
     systemProperty("idea.use.native.fs.for.win", false)
-}
-
-tasks.register<JavaExec>("runBenchmark") {
-    dependsOn(":createIdeaHomeForTests")
-
-    // jmhArgs example: -PjmhArgs='CommonCalls -p size=500 -f 1'
-    val jmhArgs = project.providers.gradleProperty("jvmArgs")
-    val resultFilePath = project.layout.buildDirectory.file("benchmarks/jmh-result.json")
-    val ideaHome = ideaHomePathForTests()
-
-    val benchmarkJarPath = project.layout.buildDirectory.file("benchmarks/main/jars/benchmarks.jar")
-    argumentProviders.add {
-        listOf(
-            "-Didea.home.path=${ideaHome.get().asFile.canonicalPath}",
-            benchmarkJarPath.get().asFile.toString(),
-            "-rf",
-            "json",
-            "-rff",
-            resultFilePath.get().asFile.toString(),
-        ) + jmhArgs.map { it.split("\\s".toRegex()) }.orElse(emptyList()).get()
-    }
-    mainClass.set("-jar")
 }
