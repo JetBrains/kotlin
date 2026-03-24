df7af645a0392112360e02119f26caf87f781fd3
Aleksandr.Shefer <aleksandr.shefer@jetbrains.com>
2026-03-24 12:31:34 +0100

    [Wasm] split Coroutines.kt for State Machine/Stack Switching

---
+++ libraries/stdlib/wasm/internal/kotlin/wasm/internal/CoroutinesStateMachine.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.wasm.internal
+
+import kotlin.coroutines.Continuation
+import kotlin.internal.DoNotInlineOnFirstStage
+import kotlin.internal.UsedFromCompilerGeneratedCode
+
+@PublishedApi
+@DoNotInlineOnFirstStage
+@UsedFromCompilerGeneratedCode
+internal suspend inline fun <T> suspendCoroutineUninterceptedOrReturnStateMachine(block: (Continuation<T>) -> Any?): T =
+    returnIfSuspended<T>(block(getContinuation<T>()))
