3e6b2d7d4633b0dea175a7da64345795333693ad
Aleksandr.Shefer <aleksandr.shefer@jetbrains.com>
2026-03-24 12:31:49 +0100

    [Wasm] split CoroutinesIntrinsics.kt for State Machine/Stack Switching

---
+++ libraries/stdlib/wasm/src/kotlin/coroutines/CoroutinesIntrinsicsStackSwitching.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.coroutines.intrinsics
+
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.CoroutineImplStackSwitching
+import kotlin.coroutines.WasmContinuation
+import kotlin.wasm.internal.*
+
+internal fun <T> createCoroutineUninterceptedIntrinsics0StackSwitching(
+    f: suspend () -> T,
+    completion: Continuation<T>
+): Continuation<Unit> = WasmContinuation<Unit, T>(
+    suspendFunction0ToContrefImpl(f),
+    createSimpleCoroutineFromSuspendFunctionStackSwitching(completion)
+)
+
+internal fun <R, T> createCoroutineUninterceptedIntrinsics1StackSwitching(
+    f: suspend R.() -> T,
+    receiver: R,
+    completion: Continuation<T>
+): Continuation<Unit> = WasmContinuation<Unit, T>(
+    suspendFunction1ToContrefImpl(f, receiver),
+    createSimpleCoroutineFromSuspendFunctionStackSwitching(completion)
+)
+
+@Suppress("UNCHECKED_CAST")
+internal fun <T> createSimpleCoroutineFromSuspendFunctionStackSwitching(
+    completion: Continuation<T>
+): CoroutineImplStackSwitching<Any?, T> = object : CoroutineImplStackSwitching<Any?, T>(completion) {
+    override fun doResume(): Any? {
+        if (exception != null) throw exception as Throwable
+        return result
+    }
+}
+
