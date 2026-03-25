035a8fb1c686a3598c1e4d9bb5c89a795adfd1ad
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-03-25 14:23:20 +0100

    fixup! Add reproducer test for KT-32545

--- compiler/fir/analysis-tests/testData/resolve/problems/kt32545.kt
+++ compiler/fir/analysis-tests/testData/resolve/problems/kt32545.kt
@@ -1,5 +1,6 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-32545
+// RENDER_DIAGNOSTICS_FULL_TEXT
 
 // KT-32545: TYPE_MISMATCH diagnostic: different order/presentation of errors in compiler and IDE
 fun <T> f(it: T): T = <!RETURN_TYPE_MISMATCH!>if (it is String) "" else it<!>
