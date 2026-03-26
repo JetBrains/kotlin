844548e5eacc2e064b875e809e51d40fdf378b98
Denis.Zharkov <denis.zharkov@jetbrains.com>
2026-03-26 18:52:50 +0100

    FIR: Support nested function conversions in augmented indexed assignments

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
@@ -2016,7 +2016,7 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
             // Otherwise, we might fail resolution if the get and set operator parameter types are different
             // (different SAM types or one is a SAM type and the other isn't).
             // See testData/ir/irText/expressions/callableReferences/caoWithAdaptationForSam.kt
-            val unwrappedSamIndex = (index as? FirFunctionTypeConversionExpression)?.expression ?: index
+            val unwrappedSamIndex = index.unwrapFunctionTypeConversions()
             generateTemporaryVariable(
                 session.moduleData,
                 source = unwrappedSamIndex.source?.fakeElement(fakeSourceElementKind),
@@ -2041,11 +2041,8 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
         // We need to reapply the SAM conversions here because the get call won't be completed again (where the SAM conversions could be
         // applied automatically).
         // SAM conversions will be applied automatically for the set call during completion.
-        val indicesQualifiedAccessForGet = indicesQualifiedAccess.mapIndexed { index, qualifiedAccess ->
-            val samConversion = flattenedGetCallArguments[index] as? FirFunctionTypeConversionExpression ?: return@mapIndexed qualifiedAccess
-            buildFunctionTypeConversionExpressionCopy(samConversion) {
-                expression = qualifiedAccess
-            }
+        val indicesQualifiedAccessForGet = indicesQualifiedAccess.mapIndexed { index, temporaryVariableAccess ->
+            wrapIntoFunctionConversionsIfNecessary(temporaryVariableAccess, flattenedGetCallArguments[index])
         }
 
         val getCall = buildFunctionCall {
@@ -2108,6 +2105,19 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
             resolvedSetCall
         )
     }
+
+    private fun wrapIntoFunctionConversionsIfNecessary(
+        temporaryVariableAccess: FirQualifiedAccessExpression,
+        initialGetArgument: FirExpression,
+    ): FirExpression {
+        val functionTypeConversionExpression = initialGetArgument as? FirFunctionTypeConversionExpression ?: return temporaryVariableAccess
+        return buildFunctionTypeConversionExpressionCopy(functionTypeConversionExpression) {
+            expression = wrapIntoFunctionConversionsIfNecessary(temporaryVariableAccess, functionTypeConversionExpression.expression)
+        }
+    }
+
+    private fun FirExpression.unwrapFunctionTypeConversions(): FirExpression =
+        (this as? FirFunctionTypeConversionExpression)?.expression?.unwrapFunctionTypeConversions() ?: this
 
     @OptIn(ExperimentalContracts::class)
     private val ResolutionMode.forCollectionLiteralInAnnotationResolution: Boolean
