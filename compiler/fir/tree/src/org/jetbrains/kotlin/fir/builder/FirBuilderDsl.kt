2e960e443f719a54c82a5925b9bf8d41855f9995
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-03-27 18:30:05 +0100

    Introduce utility `buildFirList` and `buildFirMap`
    For further usages with new no-builder build functions

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/builder/FirBuilderDsl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/builder/FirBuilderDsl.kt
@@ -16,3 +16,11 @@ fun <T> MutableList<T>?.toMutableOrEmpty(): MutableOrEmptyList<T> =
 @JvmName("toMutableOrEmptyForImmutable")
 fun <T> List<T>?.toMutableOrEmpty(): MutableOrEmptyList<T> =
     if (isNullOrEmpty()) MutableOrEmptyList.empty() else MutableOrEmptyList(this.toMutableList())
+
+inline fun <T> buildFirList(init: MutableList<T>.() -> Unit): MutableList<T> {
+    return mutableListOf<T>().apply(init)
+}
+
+inline fun <K, V> buildFirMap(init: MutableMap<K, V>.() -> Unit): MutableMap<K, V> {
+    return mutableMapOf<K, V>().apply(init)
+}
