247d3834a295b9498830fc0648b93d41191f6cef
Azat Abdullin <azat.abdullin@jetbrains.com>
2026-04-01 12:11:11 +0200

    [ni] buildNativeImage script

---
+++ native-image/buildNativeImage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+GRAAL_HOME=$1
+REACHABILITY_METADATA=$2
+
+
+echo 'Starting native image build of the compiler'
+
+echo '1. Building kotlin compiler embeddable'
+./gradlew -q :kotlin-compiler-embeddable:embeddable
+
+EMBEDDABLE_JAR=$(find prepare/compiler-embeddable -name 'kotlin-compiler-embeddable-2.4.255-SNAPSHOT.jar')
+STDLIB_JAR=$(find libraries/ -name 'kotlin-stdlib-2.4.255-SNAPSHOT.jar')
+
+CLASSPATH=$EMBEDDABLE_JAR:$STDLIB_JAR
+echo "Class path: $CLASSPATH"
+
+echo '2. Building native image of kotlin compiler embeddable'
+NATIVE_IMAGE_BIN=$GRAAL_HOME/bin/native-image
+
+$NATIVE_IMAGE_BIN \
+  --add-opens java.base/java.lang=ALL-UNNAMED \
+  --add-opens java.base/java.io=ALL-UNNAMED \
+  --add-opens java.base/java.nio=ALL-UNNAMED \
+  --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+  --add-opens java.desktop/javax.swing=ALL-UNNAMED \
+  -H:+AddAllCharsets \
+  -H:+UnlockExperimentalVMOptions \
+  -H:+AllowJRTFileSystem \
+  -H:ReachabilityMetadataConfigurationFiles=$REACHABILITY_METADATA \
+  -cp $CLASSPATH \
+  -o kotlincn \
+  org.jetbrains.kotlin.cli.jvm.K2JVMCompiler
