31def3f578cdc33f5428cb2d89c55cc7f4ed2cc2
Derek Xu <derekx@google.com>
2026-04-07 15:54:30 -0400

    Cherry-pick "Fix `callableInferenceNodeOf` in `ComposableTargetChecker.kt`"
    `LazySchemeStorage<Node>.getOrPut` only returns the same scheme for
    nodes whose `element` properties are structurally equal. This PR ensures
    that the nodes returned by `callableInferenceNodeOf` always have
    `element` properties that are as suitable as possible.
    
    This PR also fixes `FirCallableElementInferenceNode.kind` by making it
    return `NodeKind.Function` instead of `NodeKind.Expression`.
    
    Test: ComposableTargetCheckerTests.testDifferentWrapperTypes
    Bug: 422193018

--- plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/k2/ComposableTargetChecker.kt
+++ plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/k2/ComposableTargetChecker.kt
@@ -50,6 +50,7 @@ private open class FirElementInferenceNode(element: FirElement) : FirInferenceNo
 }
 
 private class FirCallableElementInferenceNode(val callable: FirCallableSymbol<*>, element: FirElement) : FirElementInferenceNode(element) {
+    override val kind get() = NodeKind.Function
     override val type: InferenceNodeType = InferenceCallableType(callable)
     override fun toString(): String = "${callable.name.toString()}()@${element.source?.startOffset}"
 }
@@ -85,12 +86,27 @@ private class FirParameterReferenceNode(
     override fun toString(): String = "param:$parameterIndex"
 }
 
-private fun callableInferenceNodeOf(expression: FirElement, callable: FirCallableSymbol<*>, context: CheckerContext) =
-    parameterInferenceNodeOrNull(expression, context) ?: (expression as? FirAnonymousFunction)?.let {
+@OptIn(SymbolInternals::class)
+private fun callableInferenceNodeOf(expression: FirElement, callable: FirCallableSymbol<*>, context: CheckerContext): FirInferenceNode {
+    parameterInferenceNodeOrNull(expression, context)?.let {
+        return it
+    }
+
+    (expression as? FirAnonymousFunction)?.let {
         context.session.composableTargetSessionStorage.getLambdaExpression(expression)
     }?.let {
-        inferenceNodeOf(it, context)
-    } ?: FirCallableElementInferenceNode(callable, expression)
+        return inferenceNodeOf(it, context)
+    }
+
+    (expression as? FirFunctionCall)?.let {
+        return FirCallableElementInferenceNode(callable, callable.fir)
+    }
+
+    // A node's `element` property determines which scheme it is associated with. Falling through to
+    // this case means that it is not safe for the returned node to share a scheme with any other
+    // node, so we set its `element` property to `expression`.
+    return FirCallableElementInferenceNode(callable, expression)
+}
 
 @OptIn(SymbolInternals::class)
 private fun parameterInferenceNodeOrNull(expression: FirElement, context: CheckerContext): FirInferenceNode? {
