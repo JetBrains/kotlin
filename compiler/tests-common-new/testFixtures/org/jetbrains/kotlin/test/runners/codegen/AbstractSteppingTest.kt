92e20aff33ca93f05e2c01a4c4f48fde9c6db09d
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-04-08 09:22:49 +0000

    [Tests] Simplify `AbstractSteppingTestBase` hierarchy
    ^KT-85292

--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/runners/codegen/AbstractSteppingTest.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/runners/codegen/AbstractSteppingTest.kt
@@ -5,38 +5,56 @@
 
 package org.jetbrains.kotlin.test.runners.codegen
 
-import org.jetbrains.kotlin.test.Constructor
 import org.jetbrains.kotlin.test.FirParser
+import org.jetbrains.kotlin.test.TargetBackend
+import org.jetbrains.kotlin.test.backend.BlackBoxCodegenSuppressor
+import org.jetbrains.kotlin.test.backend.handlers.SteppingDebugRunner
 import org.jetbrains.kotlin.test.backend.ir.BackendCliJvmFacade
-import org.jetbrains.kotlin.test.backend.ir.IrBackendInput
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
+import org.jetbrains.kotlin.test.builders.configureJvmArtifactsHandlersStep
+import org.jetbrains.kotlin.test.configuration.commonConfigurationForJvmTest
+import org.jetbrains.kotlin.test.configuration.commonHandlersForCodegenTest
 import org.jetbrains.kotlin.test.configuration.configureDumpHandlersForCodegenTest
+import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.REQUIRES_SEPARATE_PROCESS
+import org.jetbrains.kotlin.test.directives.DiagnosticsDirectives.REPORT_ONLY_EXPLICITLY_DEFINED_DEBUG_INFO
 import org.jetbrains.kotlin.test.directives.configureFirParser
 import org.jetbrains.kotlin.test.frontend.fir.Fir2IrCliJvmFacade
 import org.jetbrains.kotlin.test.frontend.fir.FirCliJvmFacade
-import org.jetbrains.kotlin.test.frontend.fir.FirOutputArtifact
-import org.jetbrains.kotlin.test.model.*
+import org.jetbrains.kotlin.test.model.FrontendKinds
+import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerWithTargetBackendTest
+import org.jetbrains.kotlin.test.services.sourceProviders.MainFunctionForDebugTestsSourceProvider
 
-open class AbstractFirSteppingTestBase(val parser: FirParser) : AbstractSteppingTestBase<FirOutputArtifact>(
-    FrontendKinds.FIR
-) {
-    override val frontendFacade: Constructor<FrontendFacade<FirOutputArtifact>>
-        get() = ::FirCliJvmFacade
+abstract class AbstractSteppingTestBase(
+    val parser: FirParser
+) : AbstractKotlinCompilerWithTargetBackendTest(TargetBackend.JVM_IR) {
+    override fun configure(builder: TestConfigurationBuilder) = with(builder) {
+        commonConfigurationForJvmTest(
+            FrontendKinds.FIR,
+            ::FirCliJvmFacade,
+            ::Fir2IrCliJvmFacade,
+            ::BackendCliJvmFacade,
+            additionalSourceProvider = ::MainFunctionForDebugTestsSourceProvider
+        )
 
-    override val frontendToBackendConverter: Constructor<Frontend2BackendConverter<FirOutputArtifact, IrBackendInput>>
-        get() = ::Fir2IrCliJvmFacade
+        configureFirParser(parser)
+        commonHandlersForCodegenTest()
+        configureDumpHandlersForCodegenTest()
+        configureJvmArtifactsHandlersStep {
+            useHandlers(::SteppingDebugRunner)
+        }
 
-    override val backendFacade: Constructor<BackendFacade<IrBackendInput, BinaryArtifacts.Jvm>>
-        get() = ::BackendCliJvmFacade
+        useAfterAnalysisCheckers(::BlackBoxCodegenSuppressor)
 
-    override fun configure(builder: TestConfigurationBuilder) {
-        super.configure(builder)
-        builder.configureDumpHandlersForCodegenTest()
-        builder.configureFirParser(parser)
+        defaultDirectives {
+            +REPORT_ONLY_EXPLICITLY_DEFINED_DEBUG_INFO
+            +REQUIRES_SEPARATE_PROCESS
+        }
+
+        enableMetaInfoHandler()
     }
 }
 
-open class AbstractFirLightTreeSteppingTest : AbstractFirSteppingTestBase(FirParser.LightTree)
+open class AbstractFirLightTreeSteppingTest : AbstractSteppingTestBase(FirParser.LightTree)
 
 @FirPsiCodegenTest
-open class AbstractFirPsiSteppingTest : AbstractFirSteppingTestBase(FirParser.Psi)
+open class AbstractFirPsiSteppingTest : AbstractSteppingTestBase(FirParser.Psi)
