718b56983ac101c416d5b11f1f6a356deafb2b03
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-04-09 13:52:51 +0300

    [Tests] Mute (reasonably) failing test in JKlib tests
    The member scope of kotlin.Int is different in JVM and JKlib stdlibs,
    so it's expected that the IR dumps of this class are different.

--- compiler/testData/ir/irText/stubs/constFromBuiltins.kt
+++ compiler/testData/ir/irText/stubs/constFromBuiltins.kt
@@ -1,5 +1,7 @@
 // FIR_IDENTICAL
 // DUMP_EXTERNAL_CLASS: kotlin/Int
 // TARGET_BACKEND: JVM_IR
+// IGNORE_BACKEND: JKLIB
+// ^^^^ member scope of kotlin.Int is different for JVM and JKlib stdlibs
 
 val test = Int.MIN_VALUE
