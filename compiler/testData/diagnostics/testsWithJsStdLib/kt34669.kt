428277316dbc4d34efcd3a91da546b1124fca43e
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-04-10 17:06:36 +0200

    fixup! Add reproducer test for KT-34669

--- compiler/testData/diagnostics/testsWithJsStdLib/kt34669.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/kt34669.kt
@@ -4,14 +4,14 @@
 // KT-34669: Incorrect USELESS_CAST warning when casting from dynamic type multiple times
 
 fun foo() {
-    val dyn: <!UNSUPPORTED!>dynamic<!> = 1
+    val dyn: dynamic = 1
 
     val str: String = dyn as String
     val str1: String = dyn as String // should not warn "No cast needed"
 }
 
 fun bar() {
-    val dyn: <!UNSUPPORTED!>dynamic<!> = 1
+    val dyn: dynamic = 1
 
     val int1: Int = dyn as Int
     val int2: Int = dyn as Int // should not warn "No cast needed"
