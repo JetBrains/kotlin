9261a6f0790b7504e0223e9e0a0dd242fe0cc3f7
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2026-04-13 13:39:23 +0200

    [K/N][tests] Add a reproducer for KT-85508
    This commit adds a test closely following the case reported in KT-85508.
    
    (cherry picked from commit 760716932dbeafc4279fc38a8acd2df9301ed404)

---
+++ native/native.tests/testData/codegen/cinterop/objc/kt85508.kt
@@ -0,0 +1,24 @@
+// TARGET_BACKEND: NATIVE
+// WITH_PLATFORM_LIBS
+// DISABLE_NATIVE: isAppleTarget=false
+
+// KT-85508
+// DISABLE_NATIVE: isAppleTarget=true
+@file:OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+
+import platform.Network.NW_PARAMETERS_DISABLE_PROTOCOL
+import platform.Network.nw_parameters_copy
+import platform.Network.nw_parameters_create_secure_tcp
+import platform.Network.nw_tcp_options_set_enable_keepalive
+
+fun box(): String {
+    // See KT-85508.
+    val params = nw_parameters_create_secure_tcp(
+        NW_PARAMETERS_DISABLE_PROTOCOL
+    ) { tcpOptions ->
+        nw_tcp_options_set_enable_keepalive(tcpOptions, true)
+    }
+
+    val paramsCopy = nw_parameters_copy(params) // Just a use site.
+    return if (paramsCopy != null) "OK" else "FAIL: $params"
+}
