58e46ec7667ec7f77616084280fbd5c69ebe85cf
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-04-13 08:59:55 +0200

    fixup! Add reproducer test for KT-30045

--- compiler/testData/diagnostics/testsWithJsStdLib/kt30045.kt
+++ compiler/testData/diagnostics/testsWithJsStdLib/kt30045.kt
@@ -1,12 +1,12 @@
-// RUN_PIPELINE_TILL: FRONTEND
+// RUN_PIPELINE_TILL: BACKEND
 // ISSUE: KT-30045
 // WITH_STDLIB
 
 // KT-30045: False positive USELESS_CAST on `dynamic` value used with spread operator
 fun foo() {
-    val data: <!UNSUPPORTED!>dynamic<!> = ""
+    val data: dynamic = ""
     val list: List<String> = when (data) {
-        is String -> <!UNSUPPORTED!>listOf<!>(data)
+        is String -> listOf(data)
         is Array<String> -> listOf(*data as Array<String>)
         else -> TODO()
     }
