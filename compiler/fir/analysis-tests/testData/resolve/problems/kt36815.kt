9acac352419a10d48ce1c9c375876d9152215d0c
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-04-13 09:24:59 +0200

    fixup! Add reproducer test for KT-36815

--- compiler/fir/analysis-tests/testData/resolve/problems/kt36815.kt
+++ compiler/fir/analysis-tests/testData/resolve/problems/kt36815.kt
@@ -1,5 +1,6 @@
 // RUN_PIPELINE_TILL: BACKEND
 // ISSUE: KT-36815
+// DIAGNOSTICS: -EXTENSION_FUNCTION_SHADOWED_BY_MEMBER_PROPERTY_WITH_INVOKE -EXTENSION_SHADOWED_BY_MEMBER
 
 // KT-36815: Wrong overload resolution for infix function call between member property+invoke without infix modifier and infix function extension
 
@@ -16,14 +17,14 @@ class B() {
     val barD: D = D()
 }
 
-infix fun B.<!EXTENSION_FUNCTION_SHADOWED_BY_MEMBER_PROPERTY_WITH_INVOKE!>barC<!>(i: Int) = {}  //(2)
+infix fun B.barC(i: Int) = {}  //(2)
 infix fun B.barD(i: Int) = {}  //(3)
 
 class Case() {
-    val B.<!EXTENSION_SHADOWED_BY_MEMBER!>barC<!>: D
+    val B.barC: D
         get() = D()
 
-    infix fun B.<!EXTENSION_FUNCTION_SHADOWED_BY_MEMBER_PROPERTY_WITH_INVOKE!>barC<!>(i: Int) = {}  //(4)
+    infix fun B.barC(i: Int) = {}  //(4)
     infix fun B.barD(i: Int) = {}  //(5)
     infix fun B.bar(i: Int) = {}   //(6)
 
