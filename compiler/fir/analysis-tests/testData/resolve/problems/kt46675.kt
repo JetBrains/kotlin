12be46ec112cb9a89daf325f80c1edb03903397f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-04-13 09:44:00 +0200

    fixup! Add reproducer test for KT-46675

--- compiler/fir/analysis-tests/testData/resolve/problems/kt46675.kt
+++ compiler/fir/analysis-tests/testData/resolve/problems/kt46675.kt
@@ -5,7 +5,7 @@
 // KT-46675: Cryptic "Nothing was expected" compiler error with new type inference
 interface I<T>
 fun <T> foo(bar: Iterable<I<in T>>): I<T> = TODO()
-fun <T, U : T> I<T>.baz(that: I<U>) = foo<U>(<!ARGUMENT_TYPE_MISMATCH!>listOf(this, that)<!>)
+fun <T, U : T> I<T>.baz(that: I<U>) = foo<U>(<!ARGUMENT_TYPE_MISMATCH("List<I<out T (of fun <T, U : T> I<T>.baz)>>; Iterable<I<in U (of fun <T, U : T> I<T>.baz)>>")!>listOf(this, that)<!>)
 
 /* GENERATED_FIR_TAGS: funWithExtensionReceiver, functionDeclaration, inProjection, interfaceDeclaration, nullableType,
 outProjection, thisExpression, typeConstraint, typeParameter */
