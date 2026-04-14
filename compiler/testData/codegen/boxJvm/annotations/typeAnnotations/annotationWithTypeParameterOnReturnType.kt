0ef420a9e36083f6365d977d82c77ec67ad2d674
Milosz Piekutowski <milosz.piekutowski@jetbrains.com>
2026-04-14 18:19:28 +0200

    [Tests] Disable type parameter scope checks for `annotationWithTypeParameterOnReturnType`
    Reason: KT-85775
    
    KT-69305

--- compiler/testData/codegen/boxJvm/annotations/typeAnnotations/annotationWithTypeParameterOnReturnType.kt
+++ compiler/testData/codegen/boxJvm/annotations/typeAnnotations/annotationWithTypeParameterOnReturnType.kt
@@ -1,9 +1,14 @@
 // TARGET_BACKEND: JVM_IR
 // ISSUE: KT-43553
 
+// DISABLE_IR_TYPE_PARAMETER_SCOPE_CHECKS: ANY
+// More context at line 12
+
 @Target(AnnotationTarget.TYPE)
 annotation class Qualifier<T>
 
 fun <T> func(param: String): @Qualifier<T> String = param
 
+// Type parameter <T> is leaking through return type annotation
+// type=@[Qualifier<T of <root>.func>] kotlin.String origin=null
 fun box(): String = func<String>("OK")
