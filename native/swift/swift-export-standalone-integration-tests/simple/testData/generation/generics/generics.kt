90d81cb2f07e9adf86fc19417d0c836df4f7e962
Rick Clephas <rick.clephas.external@jetbrains.com>
2026-04-14 15:22:28 +0200

    [Swift Export] Fix input type param check for receiver/context types
    ^KT-85714 Fixed
    
    (cherry picked from commit 2c3af53c52ff2638c58b1e7aeaec269720ca2880)

--- native/swift/swift-export-standalone-integration-tests/simple/testData/generation/generics/generics.kt
+++ native/swift/swift-export-standalone-integration-tests/simple/testData/generation/generics/generics.kt
@@ -108,6 +108,10 @@ fun takeBoxUpperBoundClosure(box: () -> Box<Any?>): Unit = TODO()
 
 fun produceBox(box: (Box<String>) -> Unit): Unit = TODO() // unsupported input generic
 
+fun produceExtBox(box: Box<String>.() -> Unit): Unit = TODO() // unsupported input generic
+
+fun produceCtxBox(box: context(Box<String>) () -> Unit): Unit = TODO() // unsupported input generic
+
 fun produceBoxUpperBound(box: (Box<Any?>) -> Unit): Unit = TODO()
 
 typealias BFun = (B<*>) -> Unit
