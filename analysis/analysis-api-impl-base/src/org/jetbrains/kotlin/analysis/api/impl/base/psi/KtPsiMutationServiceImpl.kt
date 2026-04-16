17a411151908f1d1d4181243d1d4967f214ecafc
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-04-16 23:53:42 +0400

    [PSI] Add KtPsiMutationService
    Introduce the service abstraction and register its default Analysis API
    implementation before moving any PSI mutation entry points behind it.
    
    KT-85052

---
+++ analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/psi/KtPsiMutationServiceImpl.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.analysis.api.impl.base.psi
+
+import org.jetbrains.kotlin.psi.KtNonPublicApi
+import org.jetbrains.kotlin.psi.KtPsiMutationService
+
+@KtNonPublicApi
+internal class KtPsiMutationServiceImpl : KtPsiMutationService
