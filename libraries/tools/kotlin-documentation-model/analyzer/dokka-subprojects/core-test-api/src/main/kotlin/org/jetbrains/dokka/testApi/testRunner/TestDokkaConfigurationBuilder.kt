3293a235c4c0496b60de9806e8a88f0e5bb84093
Ryan Nett <JNett96@gmail.com>
2026-04-16 12:32:51 -0700

    [KDM] Add option to suppress annotated declarations
    See https://github.com/Kotlin/dokka/pull/4464
    Original commit: https://github.com/Kotlin/dokka/commit/895a18a68ba8686ae3f6122abf507ce610d39931

--- libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/core-test-api/src/main/kotlin/org/jetbrains/dokka/testApi/testRunner/TestDokkaConfigurationBuilder.kt
+++ libraries/tools/kotlin-documentation-model/analyzer/dokka-subprojects/core-test-api/src/main/kotlin/org/jetbrains/dokka/testApi/testRunner/TestDokkaConfigurationBuilder.kt
@@ -111,7 +111,8 @@ public class DokkaSourceSetBuilder(
     public var analysisPlatform: String = "jvm",
     public var perPackageOptions: List<PackageOptionsImpl> = emptyList(),
     public var externalDocumentationLinks: List<ExternalDocumentationLinkImpl> = emptyList(),
-    public var sourceLinks: List<SourceLinkDefinitionImpl> = emptyList()
+    public var sourceLinks: List<SourceLinkDefinitionImpl> = emptyList(),
+    public var suppressAnnotatedWith: Set<String> = DokkaDefaults.suppressAnnotatedWith
 ) {
     @Suppress("DEPRECATION")
     public fun build(): DokkaSourceSetImpl {
@@ -137,6 +138,7 @@ public class DokkaSourceSetBuilder(
             noStdlibLink = noStdlibLink,
             noJdkLink = noJdkLink,
             suppressedFiles = suppressedFiles.map(::File).toSet(),
+            suppressAnnotatedWith = suppressAnnotatedWith,
             analysisPlatform = Platform.fromString(analysisPlatform)
         )
     }
