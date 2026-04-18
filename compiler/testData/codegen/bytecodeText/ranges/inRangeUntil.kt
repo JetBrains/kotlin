5497781153bbf37228feec30fdd3d954151ce57c
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-04-18 20:24:38 +0800

    Optimize open-end range contains lowering

--- compiler/testData/codegen/bytecodeText/ranges/inRangeUntil.kt
+++ compiler/testData/codegen/bytecodeText/ranges/inRangeUntil.kt
@@ -10,6 +10,18 @@ fun testInt(a: Int, x: Int, y: Int) = a in x..<y
 
 fun testLong(a: Long, x: Long, y: Long) = a in x..<y
 
+fun testFloat(a: Float, x: Float, y: Float) = a in x..<y
+
+fun testDouble(a: Double, x: Double, y: Double) = a in x..<y
+
+fun testFloatInDoubleRange(a: Float, x: Double, y: Double) = a in x..<y
+
+fun testDoubleLiteral(a: Double) = a in 0.0..<2.0
+
 // 0 until
+// 0 rangeUntil
+// 0 doubleRangeContains
+// 0 floatRangeContains
 // 0 INVOKEVIRTUAL
 // 0 contains
+// 0 valueOf
