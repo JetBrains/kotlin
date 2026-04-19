42a5b6a52f5a7f893bcb2b67ab70d2d58b446d11
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-04-19 19:31:26 +0200

    [Tests] Move Settings class hierarchy to `:compiler:tests-common-new`
    ^KT-84712

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/settings/SettingsContainers.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/settings/SettingsContainers.kt
@@ -6,67 +6,20 @@
 package org.jetbrains.kotlin.konan.test.blackbox.support.settings
 
 import org.jetbrains.kotlin.config.nativeBinaryOptions.BinaryOptions
-import org.jetbrains.kotlin.konan.target.Architecture
-import org.jetbrains.kotlin.konan.target.Configurables
-import org.jetbrains.kotlin.konan.target.Distribution
-import org.jetbrains.kotlin.konan.target.HostManager
-import org.jetbrains.kotlin.konan.target.KonanTarget
-import org.jetbrains.kotlin.konan.target.PlatformManager
-import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertTrue
-import org.jetbrains.kotlin.test.services.JUnit5Assertions.fail
-import org.jetbrains.kotlin.test.services.TestService
-import kotlin.reflect.KClass
+import org.jetbrains.kotlin.konan.target.*
+import org.jetbrains.kotlin.test.settings.Settings
+import org.jetbrains.kotlin.test.settings.SimpleTestClassSettings
+import org.jetbrains.kotlin.test.settings.SimpleTestRunSettings
+import org.jetbrains.kotlin.test.settings.TestClassSettings
+import org.jetbrains.kotlin.test.settings.TestProcessSettings
+import org.jetbrains.kotlin.test.settings.TestRunSettings
 
-abstract class Settings(private val parent: Settings?, settings: Iterable<Any>) {
-    private val map: Map<KClass<*>, Any> = buildMap {
-        settings.forEach {
-            val settingClass: KClass<*>
-            val setting: Any
-            if (it is Pair<*, *>) {
-                settingClass = it.first as KClass<*>
-                setting = it.second ?: error("Setting $settingClass is null")
-            } else {
-                settingClass = it::class
-                setting = it
-            }
-
-            val previous = put(settingClass, setting)
-            assertTrue(previous == null) { "Duplicated settings: $settingClass, $previous, $setting" }
-        }
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    fun <T : Any> get(clazz: KClass<out T>): T = map[clazz] as T?
-        ?: parent?.get(clazz)
-        ?: fail { "No such setting: $clazz" }
-
-    inline fun <reified T : Any> get(): T = get(T::class)
-}
-
-/**
- * The hierarchy of settings containers for Native black box tests:
- *
- * | Settings container    | Parent                 | Scope                                       |
- * | --------------------- | ---------------------- | ------------------------------------------- |
- * | [TestProcessSettings] | `null`                 | The whole Gradle test executor process      |
- * | [TestClassSettings]   | [TestProcessSettings]  | The single top-level (enclosing) test class |
- * | [TestRunSettings]     | [TestClassSettings]    | The single test run of a test function      |
- */
-class TestProcessSettings(vararg settings: Any) : Settings(parent = null, settings.asIterable())
-class TestClassSettings(parent: TestProcessSettings, settings: Iterable<Any>) : Settings(parent, settings)
-class TestRunSettings(parent: TestClassSettings, settings: Iterable<Any>) : Settings(parent, settings), TestService
-
-/**
- * The hierarchy of settings containers for simple Native tests (e.g. KLIB tests):
- *
- * | Settings container        | Parent                    | Scope                                       |
- * | ------------------------- | ------------------------- | ------------------------------------------- |
- * | [TestProcessSettings]     | `null`                    | The whole Gradle test executor process      |
- * | [SimpleTestClassSettings] | [TestProcessSettings]     | The single top-level (enclosing) test class |
- * | [SimpleTestRunSettings]   | [SimpleTestClassSettings] | The single test run of a test function      |
- */
-class SimpleTestClassSettings(parent: TestProcessSettings, settings: Iterable<Any>) : Settings(parent, settings)
-class SimpleTestRunSettings(parent: SimpleTestClassSettings, settings: Iterable<Any>) : Settings(parent, settings)
+typealias Settings = Settings
+typealias TestProcessSettings = TestProcessSettings
+typealias TestClassSettings = TestClassSettings
+typealias TestRunSettings = TestRunSettings
+typealias SimpleTestClassSettings = SimpleTestClassSettings
+typealias SimpleTestRunSettings = SimpleTestRunSettings
 
 val Settings.configurables: Configurables
     get() {
