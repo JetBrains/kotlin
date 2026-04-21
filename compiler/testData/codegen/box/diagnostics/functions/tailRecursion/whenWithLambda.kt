2f319941e994583dfdb2049aefeb55ae251cab6b
Brian Norman <brian.norman@jetbrains.com>
2026-04-21 09:18:20 -0500

    [Tests] Add recreation test for KT-82196

---
+++ compiler/testData/codegen/box/diagnostics/functions/tailRecursion/whenWithLambda.kt
@@ -0,0 +1,11 @@
+// ISSUES: KT-82196
+
+tailrec fun withWhen(counter: Int): Int {
+    return when (counter) {
+        0 -> withWhen(1)
+        1 -> counter
+        else -> run { null } ?: <!NON_TAIL_RECURSIVE_CALL!>withWhen<!>(counter - 1)
+    }
+}
+
+fun box(): String = if (withWhen(100000) == 1) "OK" else "FAIL"
