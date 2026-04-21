870c2c52aa47af24b2d933118e5faf1097b88152
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-04-21 16:05:02 +0200

    Contracts: add reproducer for substitution problem in property accessors
    #KT-84960 In progress

---
+++ compiler/testData/diagnostics/testsWithStdLib/contracts/propertyAccessors/kt84960.kt
@@ -0,0 +1,35 @@
+// RUN_PIPELINE_TILL: BACKEND
+// LANGUAGE: +AllowContractsOnPropertyAccessors
+// OPT_IN: kotlin.contracts.ExperimentalContracts, kotlin.contracts.ExperimentalExtendedContracts
+// ISSUE: KT-84960
+
+import kotlin.contracts.*
+
+fun works() {
+    val foo: Foo<Unit> = BarImpl
+    foo.foo()
+    val bar: Bar<Unit> = foo
+}
+
+fun fails() {
+    val foo: Foo<Unit> = BarImpl
+    foo.foo2
+    val bar: Bar<Unit> <!INITIALIZER_TYPE_MISMATCH!>=<!> foo // Initializer type mismatch: expected 'Bar<Unit>', actual 'Bar<T (of val <T> Foo<T>.foo2)> & Foo<Unit>'.
+}
+
+interface Foo<T>
+interface Bar<T>: Foo<T>
+
+object BarImpl: Bar<Unit>
+
+fun <T> Foo<T>.foo() {
+    contract { returns() implies (this@foo is Bar) }
+}
+
+val <T> Foo<T>.foo2: Unit get() {
+    contract { returns() implies (this@foo2 is Bar) }
+}
+
+/* GENERATED_FIR_TAGS: contractConditionalEffect, contracts, funWithExtensionReceiver, functionDeclaration, getter,
+interfaceDeclaration, intersectionType, isExpression, lambdaLiteral, localProperty, nullableType, objectDeclaration,
+propertyDeclaration, propertyWithExtensionReceiver, smartcast, thisExpression, typeParameter */
