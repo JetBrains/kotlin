631b1c5d558e631e0e02545c4c7aa2568617fcfd
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-04-23 10:23:18 +0200

    [Plugins] Replace MessageCollector usage in test-plugins
    #KT-78277

--- plugins/test-plugins/middle/src/org/jetbrains/kotlin/plugin/test/MiddleComponentRegistrar.kt
+++ plugins/test-plugins/middle/src/org/jetbrains/kotlin/plugin/test/MiddleComponentRegistrar.kt
@@ -8,11 +8,16 @@ package org.jetbrains.kotlin.plugin.test
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.report
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.compiler.plugin.registerExtension
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.messageCollector
+import org.jetbrains.kotlin.diagnostics.KtDiagnosticFactoryToRendererMap
+import org.jetbrains.kotlin.diagnostics.KtDiagnosticsContainer
+import org.jetbrains.kotlin.diagnostics.errorWithoutSource
+import org.jetbrains.kotlin.diagnostics.rendering.BaseDiagnosticRendererFactory
+import org.jetbrains.kotlin.diagnostics.rendering.BaseSourcelessDiagnosticRendererFactory
+import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 import org.jetbrains.kotlin.ir.builders.declarations.addGetter
 import org.jetbrains.kotlin.ir.builders.declarations.buildProperty
 import org.jetbrains.kotlin.ir.builders.irBlockBody
@@ -23,6 +28,24 @@ import org.jetbrains.kotlin.ir.declarations.nameWithPackage
 import org.jetbrains.kotlin.ir.util.toIrConst
 import org.jetbrains.kotlin.name.Name
 
+object MiddleDiagnostics : KtDiagnosticsContainer() {
+    val MIDDLE_ERROR by errorWithoutSource()
+
+    override fun getRendererFactory(): BaseDiagnosticRendererFactory = Messages
+
+    object Messages : BaseSourcelessDiagnosticRendererFactory() {
+        override val MAP: KtDiagnosticFactoryToRendererMap by KtDiagnosticFactoryToRendererMap("MiddleDiagnostics") { map ->
+            map.put(MIDDLE_ERROR, MESSAGE_PLACEHOLDER)
+        }
+    }
+}
+
+class FirMiddleRegistrar : FirExtensionRegistrar() {
+    override fun ExtensionRegistrarContext.configurePlugin() {
+        registerDiagnosticContainers(MiddleDiagnostics)
+    }
+}
+
 class MiddleComponentRegistrar : CompilerPluginRegistrar() {
     override val pluginId: String
         get() = "kotlin.plugin.test.middle"
@@ -31,24 +54,25 @@ class MiddleComponentRegistrar : CompilerPluginRegistrar() {
         get() = true
 
     override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
-        IrGenerationExtension.registerExtension(Extension(configuration.messageCollector))
+        IrGenerationExtension.registerExtension(Extension(configuration))
+        FirExtensionRegistrar.registerExtension(FirMiddleRegistrar())
     }
 
-    private class Extension(private val messageCollector: MessageCollector) : IrGenerationExtension {
+    private class Extension(private val configuration: CompilerConfiguration) : IrGenerationExtension {
         override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
             for (file in moduleFragment.files) {
                 val properties = file.declarations.filterIsInstance<IrProperty>()
 
                 if (properties.singleOrNull { it.name.toString() == "before_${file.nameWithPackage}" } == null) {
-                    messageCollector.report(
-                        CompilerMessageSeverity.ERROR,
+                    configuration.report(
+                        MiddleDiagnostics.MIDDLE_ERROR,
                         "Plugin 'kotlin.plugin.test.before' must run before 'kotlin.plugin.test.middle'"
                     )
                 }
 
                 if (properties.singleOrNull { it.name.toString() == "after_${file.nameWithPackage}" } != null) {
-                    messageCollector.report(
-                        CompilerMessageSeverity.ERROR,
+                    configuration.report(
+                        MiddleDiagnostics.MIDDLE_ERROR,
                         "Plugin 'kotlin.plugin.test.after' must run after 'kotlin.plugin.test.middle'"
                     )
                 }
