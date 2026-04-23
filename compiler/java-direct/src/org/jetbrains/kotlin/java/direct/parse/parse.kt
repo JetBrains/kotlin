dd08694a0ea3a91f9b138bb1c3cca3a02726e1b9
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-04-23 16:14:00 +0200

    Java Direct: implement light tree parsing with tests

---
+++ compiler/java-direct/src/org/jetbrains/kotlin/java/direct/parse/parse.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+package org.jetbrains.kotlin.java.direct.parse
+
+import com.intellij.java.syntax.JavaSyntaxDefinition
+import com.intellij.java.syntax.parser.JavaParser
+import com.intellij.platform.syntax.lexer.performLexing
+import com.intellij.platform.syntax.parser.SyntaxTreeBuilder
+import com.intellij.platform.syntax.parser.SyntaxTreeBuilderFactory
+import fleet.com.intellij.multiplatform.pom.java.LanguageLevel
+
+fun parseJavaToSyntaxTreeBuilder(
+    charSequence: CharSequence,
+    start: Int,
+): SyntaxTreeBuilder {
+    val lexer = JavaSyntaxDefinition.createLexer(LanguageLevel.HIGHEST)
+
+    val syntaxTreeBuilder = SyntaxTreeBuilderFactory.builder(
+        charSequence,
+        performLexing(charSequence, lexer, cancellationProvider = null, logger = null),
+        whitespaces = JavaSyntaxDefinition.whitespaces,
+        comments = JavaSyntaxDefinition.comments,
+    ).withStartOffset(start)
+        .withWhitespaceOrCommentBindingPolicy(JavaSyntaxDefinition.whitespaceOrCommentBindingPolicy)
+        .build()
+
+    parse(LanguageLevel.HIGHEST, syntaxTreeBuilder)
+    return syntaxTreeBuilder
+}
+
+fun parse(languageLevel: LanguageLevel, builder: SyntaxTreeBuilder) {
+    val parser = JavaParser(languageLevel)
+    parser.fileParser.parse(builder)
+}
+
+/**
+ * Convenience wrapper around [parseJavaToSyntaxTreeBuilder] + [buildJavaLightTree] for callers
+ * that just need the resulting [JavaLightTree].
+ */
+fun parseJavaToLightTree(charSequence: CharSequence, start: Int): JavaLightTree {
+    val builder = parseJavaToSyntaxTreeBuilder(charSequence, start)
+    return buildJavaLightTree(builder, charSequence)
+}
+
