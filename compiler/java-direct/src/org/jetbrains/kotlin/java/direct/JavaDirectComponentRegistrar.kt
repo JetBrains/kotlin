caf850ce5e14004214eea1bc69c8c2ddbec1e158
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-04-24 18:58:51 +0200

    Java Direct: expose as compiler plugin

---
+++ compiler/java-direct/src/org/jetbrains/kotlin/java/direct/JavaDirectComponentRegistrar.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.java.direct
+
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileSystem
+import org.jetbrains.kotlin.cli.jvm.compiler.extensions.JavaClassFinderFactory
+import org.jetbrains.kotlin.cli.jvm.config.javaSourceRoots
+import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.fir.session.environment.AbstractProjectFileSearchScope
+import org.jetbrains.kotlin.load.java.JavaAnnotationProvider
+import org.jetbrains.kotlin.load.java.JavaClassFinder
+
+class JavaDirectPluginRegistrar : CompilerPluginRegistrar() {
+    override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
+        JavaClassFinderFactory.registerExtension(JavaClassFinderOverAstFactory(configuration))
+    }
+
+    override val pluginId: String get() = PLUGIN_ID
+
+    override val supportsK2: Boolean
+        get() = true
+}
+
+class JavaClassFinderOverAstFactory(private val configuration: CompilerConfiguration) : JavaClassFinderFactory {
+    override fun createJavaClassFinder(
+        scope: AbstractProjectFileSearchScope,
+        annotationProvider: JavaAnnotationProvider?,
+        localFs: VirtualFileSystem,
+        findLocalFile: (String) -> VirtualFile?,
+        defaultFinderProvider: (() -> JavaClassFinder)?,
+    ): JavaClassFinder {
+        val roots: List<VirtualFile> = configuration.javaSourceRoots
+            .mapNotNull(findLocalFile)
+
+        // For library session (no Java sources), just use the default finder
+        if (roots.isEmpty()) {
+            return defaultFinderProvider?.invoke()
+                ?: throw IllegalStateException("No Java source roots and no default finder provider")
+        }
+
+        val sourceFinder = JavaClassFinderOverAstImpl(roots)
+
+        val binaryFinder = defaultFinderProvider?.invoke() ?: return sourceFinder
+
+        return CombinedJavaClassFinder(sourceFinder, binaryFinder)
+    }
+}
+
+private const val PLUGIN_ID = "org.jetbrains.kotlin.javaDirect"
