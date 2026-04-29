9309537bd8a511549baf361182ac8bcb727ee6bf
Denis.Zharkov <denis.zharkov@jetbrains.com>
2026-04-29 14:26:36 +0200

    FE: Add test data for KT-86042 and KT-86043

---
+++ compiler/testData/diagnostics/tests/inference/fixationOrder/inputTypesFirst.kt
@@ -0,0 +1,33 @@
+// WITH_STDLIB
+// RUN_PIPELINE_TILL: FRONTEND
+// DUMP_INFERENCE_LOGS: MARKDOWN
+// ISSUE: KT-86042
+
+interface Out<out T1, out T2>
+fun <R1, I, R2> foo(r1: R1, i: I, y: (I) -> R1, z: (I) -> R2): Out<Base, R2> = TODO()
+
+fun bar(x: Out<Base, String>) {}
+@JvmName("bar2")
+fun bar(x: Out<Base, Int>) {}
+
+interface Base
+interface Derived : Base
+
+fun test(base: Base, derived: Derived) {
+    foo(
+        derived, "",
+        { base },
+        { "" }
+    )
+
+    bar(
+        foo(
+            derived, "",
+            { <!RETURN_TYPE_MISMATCH!>base<!> },
+            { "" }
+        )
+    )
+}
+
+/* GENERATED_FIR_TAGS: functionDeclaration, functionalType, interfaceDeclaration, lambdaLiteral, nullableType, out,
+stringLiteral, typeParameter */
