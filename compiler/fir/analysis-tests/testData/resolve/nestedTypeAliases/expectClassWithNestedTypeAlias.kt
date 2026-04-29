fa3738e4f637726d34400cc059a154c99ff83e90
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-04-29 15:17:41 +0200

    [Test] Reproduce missing diagnostic for nested typealias inside expect class
    KT-85656

---
+++ compiler/fir/analysis-tests/testData/resolve/nestedTypeAliases/expectClassWithNestedTypeAlias.kt
@@ -0,0 +1,19 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-85656
+// LANGUAGE: +MultiPlatformProjects
+
+// MODULE: common
+// FILE: common.kt
+
+<!WRONG_MODIFIER_TARGET!>expect<!> typealias TopLevelExpectTA = String
+
+expect class My {
+    typealias Numbers = List<Int> // Missing diagnostic about phobition of nested typealiases in expect classes
+}
+
+// MODULE: platform()()(common)
+// FILE: platform.kt
+
+actual class <!NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS!>My<!>
+
+/* GENERATED_FIR_TAGS: actual, classDeclaration, expect, typeAliasDeclaration */
