25dfeb9aa69f1540b25945b10fb82156c611e19c
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-04-29 10:55:02 +0300

    [commonizer] Implement tests for support expect classes
    Tests checking `TypeCommonizer` view it as a pure function that maps
    a set of types into a single type, regardless the targets, and having
    a target-dependent commonizer stuff there feels wrong. The supplier will
    throw anytime it can't build a mapping between targets and types, which
    makes no sense for these tests.

--- native/commonizer/tests/org/jetbrains/kotlin/commonizer/utils/metadataCompilation.kt
+++ native/commonizer/tests/org/jetbrains/kotlin/commonizer/utils/metadataCompilation.kt
@@ -111,7 +111,7 @@ fun serializeModuleAndAllDependenciesToMetadata(
         moduleRoot.resolve(sourceFile.name).writeText(sourceFile.content)
     }
 
-    for (dependency in module.dependencies) {
+    for (dependency in module.dependencies + module.refinesDependencies) {
         if (dependency !in dependencyToMetadata) {
             val [configuration, dependencyArtifact] = serializeModuleAndAllDependenciesToMetadata(
                 dependency, disposable, dependencyToMetadata, compiledDependenciesRoot,
@@ -129,7 +129,11 @@ fun serializeModuleAndAllDependenciesToMetadata(
         regularDependencies = module.dependencies.map { dependency ->
             dependencyToMetadata[dependency]?.let { JvmClasspathRoot(File(it.destination)) }
                 ?: error("Missing dependency metadata for ${dependency.name}")
-        }
+        },
+        refinesDependencies = module.refinesDependencies.map { dependency ->
+            dependencyToMetadata[dependency]?.destination
+                ?: error("Missing dependency metadata for ${dependency.name}")
+        },
     )
 }
 
