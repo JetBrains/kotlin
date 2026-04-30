1f11cf26f6c2acea77b975494e822a7a180dff6b
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-04-30 11:42:50 +0200

    [Common Backend] Remove unused helpers
    #KT-85920

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/ErrorReportingContext.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/ErrorReportingContext.kt
@@ -6,31 +6,14 @@
 package org.jetbrains.kotlin.backend.common
 
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.IrDiagnosticReporter
 import org.jetbrains.kotlin.ir.IrElement
-import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
-import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrFile
-import org.jetbrains.kotlin.ir.util.getPackageFragment
 
 interface ErrorReportingContext {
     val messageCollector: MessageCollector
     val diagnosticReporter: IrDiagnosticReporter
-}
-
-fun ErrorReportingContext.report(severity: CompilerMessageSeverity, element: IrElement?, irFile: IrFile?, message: String) {
-    val location = if (element != null && irFile != null) element.getCompilerMessageLocation(irFile) else null
-    messageCollector.report(severity, message, location)
-}
-
-fun ErrorReportingContext.reportWarning(message: String, irFile: IrFile?, irElement: IrElement) {
-    report(CompilerMessageSeverity.WARNING, irElement, irFile, message)
-}
-
-fun ErrorReportingContext.reportCompilationWarning(message: String) {
-    report(CompilerMessageSeverity.WARNING, null, null, message)
 }
 
 fun IrElement.getCompilerMessageLocation(containingFile: IrFile): CompilerMessageLocation? =
@@ -38,12 +21,6 @@ fun IrElement.getCompilerMessageLocation(containingFile: IrFile): CompilerMessag
 
 fun IrFile.getCompilerMessageLocation(element: IrElement): CompilerMessageLocation? =
     createCompilerMessageLocation(this, element.startOffset, element.endOffset)
-
-fun IrBuilderWithScope.getCompilerMessageLocation(): CompilerMessageLocation? {
-    val declaration = this.scope.scopeOwnerSymbol.owner as? IrDeclaration ?: return null
-    val file = declaration.getPackageFragment() as? IrFile ?: return null
-    return createCompilerMessageLocation(file, startOffset, endOffset)
-}
 
 private fun createCompilerMessageLocation(containingFile: IrFile, startOffset: Int, endOffset: Int): CompilerMessageLocation? {
     val sourceRangeInfo = containingFile.fileEntry.getSourceRangeInfo(startOffset, endOffset)
