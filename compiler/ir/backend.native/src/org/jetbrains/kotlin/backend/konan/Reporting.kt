f3f1f2adfe5d095858f795b822519d691841a42a
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-04-30 16:09:03 +0200

    [Native] Use diagnostic reporter in reportCompilationError
    #KT-85920

--- compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/Reporting.kt
+++ compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/Reporting.kt
@@ -7,25 +7,23 @@ package org.jetbrains.kotlin.backend.konan
 
 import org.jetbrains.kotlin.backend.common.ErrorReportingContext
 import org.jetbrains.kotlin.backend.common.getCompilerMessageLocation
-import org.jetbrains.kotlin.backend.common.report
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.util.render
 
 fun ErrorReportingContext.reportCompilationError(message: String, compilerMessageLocation: CompilerMessageLocation?): Nothing {
-    messageCollector.report(CompilerMessageSeverity.ERROR, message, compilerMessageLocation)
+    diagnosticReporter.report(NativeBackendDiagnostics.NATIVE_BACKEND_ERROR, message, compilerMessageLocation)
     throw KonanCompilationException()
 }
 
 fun ErrorReportingContext.reportCompilationError(message: String, irFile: IrFile, irElement: IrElement): Nothing {
-    report(CompilerMessageSeverity.ERROR, irElement, irFile, message)
+    diagnosticReporter.report(NativeBackendDiagnostics.NATIVE_BACKEND_ERROR, message, irElement.getCompilerMessageLocation(irFile))
     throw KonanCompilationException()
 }
 
 fun ErrorReportingContext.reportCompilationError(message: String): Nothing {
-    report(CompilerMessageSeverity.ERROR, null, null, message)
+    diagnosticReporter.report(NativeBackendDiagnostics.NATIVE_BACKEND_ERROR, message)
     throw KonanCompilationException()
 }
 
