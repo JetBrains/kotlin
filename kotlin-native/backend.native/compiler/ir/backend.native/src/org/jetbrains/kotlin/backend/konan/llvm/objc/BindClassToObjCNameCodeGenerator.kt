723acbbd1736a3acf6c5b904834e39095f0ffc7f
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-04-30 13:22:57 +0200

    [Native] Use diagnostic reporter in BindClassToObjCNameCodeGenerator
    #KT-85920

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/objc/BindClassToObjCNameCodeGenerator.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/objc/BindClassToObjCNameCodeGenerator.kt
@@ -5,13 +5,13 @@
 
 package org.jetbrains.kotlin.backend.konan.llvm.objc
 
-import org.jetbrains.kotlin.backend.common.report
+import org.jetbrains.kotlin.backend.common.getCompilerMessageLocation
+import org.jetbrains.kotlin.backend.konan.NativeBackendDiagnostics
 import org.jetbrains.kotlin.backend.konan.ir.annotations.allBindClassToObjCName
 import org.jetbrains.kotlin.backend.konan.llvm.CodeGenerator
 import org.jetbrains.kotlin.backend.konan.llvm.objcexport.ObjCTypeAdapter.Companion.ObjCTypeAdapterForBindClassToObjCName
 import org.jetbrains.kotlin.backend.konan.llvm.objcexport.WritableTypeInfoOverrideError
 import org.jetbrains.kotlin.backend.konan.llvm.objcexport.bindObjCExportTypeAdapterTo
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.util.kotlinFqName
 
@@ -26,11 +26,10 @@ internal fun CodeGenerator.processBindClassToObjCNameAnnotations(file: IrFile) {
                 WritableTypeInfoOverrideError.Reason.NON_OVERRIDABLE -> "class cannot have ObjC class attachments"
                 WritableTypeInfoOverrideError.Reason.ALREADY_OVERRIDDEN -> "another ObjC class is already bound"
             }
-            context.report(
-                    CompilerMessageSeverity.ERROR,
-                    element = it.annotationElement,
-                    irFile = file,
-                    message = "Cannot bind ObjC class `${it.objCName}` to ${it.kotlinClass.kotlinFqName}: $reason",
+            context.diagnosticReporter.report(
+                    NativeBackendDiagnostics.NATIVE_BACKEND_ERROR,
+                    "Cannot bind ObjC class `${it.objCName}` to ${it.kotlinClass.kotlinFqName}: $reason",
+                    it.annotationElement.getCompilerMessageLocation(file)
             )
         }
     }
