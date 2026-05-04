415c7f712c35e1e1f063b73dec9937bcc5c7fb2f
Vsevolod Tolstopyatov <vsevolod.tolstopyatov@jetbrains.com>
2026-05-04 10:35:03 +0200

    ~backtest

--- libraries/tools/kotlin-maven-plugin-test/src/test/kotlin/org/jetbrains/kotlin/maven/test/MavenTestProject.kt
+++ libraries/tools/kotlin-maven-plugin-test/src/test/kotlin/org/jetbrains/kotlin/maven/test/MavenTestProject.kt
@@ -11,7 +11,7 @@ import java.nio.file.Path
 import kotlin.io.path.*
 
 private const val NESTED_MAVEN_OPTS =
-    "-Xms64m -Xmx1g -XX:+UseSerialGC -XX:TieredStopAtLevel=1 -Djava.awt.headless=true"
+    ""
 
 private val NESTED_MAVEN_CLI_ARGUMENTS = arrayOf("-B", "-ntp", "-nsu")
 
