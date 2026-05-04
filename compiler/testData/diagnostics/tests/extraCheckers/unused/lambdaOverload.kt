96f8bb5548264219ee0387844f8cb62569fee986
Brian Norman <brian.norman@jetbrains.com>
2026-05-04 08:01:48 -0500

    [Test] Add reproducer test for KT-76632

---
+++ compiler/testData/diagnostics/tests/extraCheckers/unused/lambdaOverload.kt
@@ -0,0 +1,27 @@
+// RUN_PIPELINE_TILL: BACKEND
+// WITH_STDLIB
+// ISSUE: KT-76632
+// DUMP_CFG
+
+fun test1(): Boolean {
+    var loadData = false
+    listOf("").invokeInline {
+        <!ASSIGNED_VALUE_IS_NEVER_READ!>loadData<!> = true
+        emptyList<String>()
+    }
+    return loadData
+}
+
+inline fun <T, R> Iterable<T>.invokeInline(transform: (T) -> Iterable<R>): List<R> {
+    return flatMap(transform)
+}
+
+@OptIn(kotlin.experimental.ExperimentalTypeInference::class)
+@OverloadResolutionByLambdaReturnType
+@kotlin.jvm.JvmName("invokeInlineSequence")
+inline fun <T, R> Iterable<T>.invokeInline(transform: (T) -> Sequence<R>): List<R> {
+    return flatMap(transform)
+}
+
+/* GENERATED_FIR_TAGS: assignment, equalityExpression, functionDeclaration, lambdaLiteral, localProperty,
+propertyDeclaration, stringLiteral, whenExpression */
