JetBrains/kotlin

commit 639c9b39ef3543e46732a6aa66822916f6663555

Add groups to inline functions with two or more inline parameters
Previously all inlined lambdas were wrapped in the group around a caller to preserve the correct structure. It is possible, however, to create a structure that breaks this arrangement when inline function has two or more parameters (like indicated in the regression tests). This change fixes it by wrapping each inlined lambda body with a group.

Fixes: 479646393
Test: New compiler tests

plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
@@ -624,9 +624,6 @@ class ComposableFunctionBodyTransformer(
         return inScope(scope) {
             visitFunctionInScope(declaration)
         }.also {
-            if (scope.isInlinedLambda && !scope.isComposable && scope.hasComposableCalls) {
-                encounteredCapturedComposableCall()
-            }
             metrics.recordFunction(scope.metrics)
             declaration.functionMetrics = scope.metrics
         }
@@ -635,7 +632,13 @@ class ComposableFunctionBodyTransformer(
     private fun visitFunctionInScope(declaration: IrFunction): IrStatement {
         val scope = currentFunctionScope
         // if the function isn't composable, there's nothing to do
-        if (!scope.isComposable) return super.visitFunction(declaration)
+        if (!scope.isComposable) {
+            if (scope.isInlinedLambda) {
+                return visitInlinedLambda(declaration)
+            } else {
+                return super.visitFunction(declaration)
+            }
+        }
         if (declaration.isDefaultParamStub) {
             // don't transform the body of the stub normally
             return visitComposableFunctionStub(declaration)
@@ -1277,6 +1280,54 @@ class ComposableFunctionBodyTransformer(
             }
         }
 
+        return declaration
+    }
+
+    private fun visitInlinedLambda(declaration: IrFunction): IrStatement {
+        val scope = currentFunctionScope
+        val parentScope = scope.parent
+        val outerGroupRequired = parentScope is Scope.CaptureScope && parentScope.forceInlinedLambdaGroup
+
+        if (!outerGroupRequired) {
+            val result = super.visitFunction(declaration)
+            if (scope.hasComposableCalls) {
+                encounteredCapturedComposableCall()
+            }
+            return result
+        }
+
+        val originalBody = declaration.body ?: return super.visitFunction(declaration)
+        val (body, returnVar) = originalBody.asBodyAndResultVar()
+        body.transformChildrenVoid()
+
+        scope.realizeGroup {
+            irEndReplaceGroup(scope = scope, startOffset = body.endOffset, endOffset = body.endOffset)
+        }
+
+        declaration.body = context.irFactory.createBlockBody(body.startOffset, body.endOffset) {
+            statements.add(
+                irStartReplaceGroup(
+                    body,
+                    scope,
+                    irFunctionSourceKey(),
+                    startOffset = body.startOffset,
+                    endOffset = body.startOffset
+                )
+            )
+            statements.addAll(body.statements)
+            statements.add(
+                irEndReplaceGroup(
+                    startOffset = body.endOffset,
+                    endOffset = body.endOffset,
+                    scope
+                )
+            )
+            if (returnVar != null) {
+                statements.add(
+                    irReturnVar(declaration.symbol, returnVar)
+                )
+            }
+        }
         return declaration
     }
 
@@ -2635,13 +2686,13 @@ class ComposableFunctionBodyTransformer(
     ) {
         var scope: Scope? = currentScope
         val blockScopeMarks = mutableListOf<Scope.BlockScope>()
-        var leavingInlinedLambda = false
+        var leavingInlinedComposableLambda = false
         loop@ while (scope != null) {
             when (scope) {
                 is Scope.FunctionScope -> {
                     if (scope.function == symbol.owner) {
                         scope.hasAnyEarlyReturn = true
-                        if (!leavingInlinedLambda || !rollbackGroupMarkerEnabled) {
+                        if (!rollbackGroupMarkerEnabled || !leavingInlinedComposableLambda) {
                             blockScopeMarks.fastForEach {
                                 it.markReturn(extraEndLocation)
                             }
@@ -2662,9 +2713,12 @@ class ComposableFunctionBodyTransformer(
                         }
                         break@loop
                     }
-                    if (scope.isInlinedLambda && scope.inComposableCall) {
-                        leavingInlinedLambda = true
-                        scope.hasInlineEarlyReturn = true
+                    if (scope.isInlinedLambda) {
+                        blockScopeMarks.add(scope)
+                        if (scope.inComposableCall) {
+                            scope.hasInlineEarlyReturn = true
+                            leavingInlinedComposableLambda = true
+                        }
                     }
                 }
                 is Scope.BlockScope -> {
@@ -2953,8 +3007,15 @@ class ComposableFunctionBodyTransformer(
             expression.symbol.owner.isInline || expression.symbol.owner.isInlineArrayConstructor() -> {
                 val captureScope = Scope.CaptureScope()
                 withScope(Scope.CallScope(expression, this)) {
+                    val owner = expression.symbol.owner
+
+                    // if it is a non-composable call with multiple inline lambdas, we need to force a group for each inline function.
+                    // this preserves structure in the argument body in cases when inline function hides some control flow.
+                    val inlineLambdaCount = owner.parameters.count { it.isInlineParameter() }
+                    captureScope.forceInlinedLambdaGroup = inlineLambdaCount > 1
+
                     expression.arguments.fastForEachIndexed { index, arg ->
-                        val parameter = expression.symbol.owner.parameters[index]
+                        val parameter = owner.parameters[index]
                         val transformed = if (parameter.isInlineParameter()) {
                             // if it is not a composable call but it is an inline function, then we allow
                             // composable calls to happen inside of the inlined lambdas. This means that we have
@@ -2969,7 +3030,9 @@ class ComposableFunctionBodyTransformer(
                         expression.arguments[index] = transformed
                     }
                 }
-                return if (captureScope.hasCapturedComposableCall) {
+                return if (captureScope.hasCapturedComposableCall && !captureScope.forceInlinedLambdaGroup) {
+                    // the outer group around the call is only required when the inline function body is not
+                    // wrapped with a group.
                     captureScope.realizeAllDirectChildren()
                     expression.asCoalescableGroup(captureScope)
                 } else {
@@ -4472,6 +4535,8 @@ class ComposableFunctionBodyTransformer(
         class CaptureScope : BlockScope("capture") {
             var hasCapturedComposableCall = false
                 private set
+
+            var forceInlinedLambdaGroup = false
 
             fun markCapturedComposableCall() {
                 hasCapturedComposableCall = true

plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/ControlFlowTransformTests.kt
@@ -2636,4 +2636,75 @@ class ControlFlowTransformTests(useFir: Boolean) : AbstractControlFlowTransformT
             }
         """
     )
+
+    // Regression test for b/479646393
+    @Test
+    fun testInlineSwitching() = verifyGoldenComposeIrTransform(
+        source = """
+            import androidx.compose.runtime.*
+
+            @Composable fun Test(clicked: Boolean) {
+                thenIf(
+                    condition = clicked,
+                    ifTrue = {
+                        val lambdaTrue = { }
+                    },
+                    ifFalse = {
+                        remember { mutableStateOf(value = "Mock") }
+                        val lambdaFalse = { }
+                    },
+                )
+            }
+        """,
+        extra = """
+            inline fun thenIf(
+                condition: Boolean,
+                ifFalse: () -> Unit,
+                ifTrue: () -> Unit,
+            ) = if (condition) {
+                ifTrue()
+            } else {
+                ifFalse()
+            }
+        """
+    )
+
+    // Regression test for b/479646393
+    @Test
+    fun testInlineSwitchingEarlyReturn() = verifyGoldenComposeIrTransform(
+        source = """
+            import androidx.compose.runtime.*
+
+            @Composable fun Test(clicked: Boolean) {
+                Modifier.thenIf(
+                    condition = clicked,
+                    ifTrue = {
+                       clickable { println(clicked) }
+                    },
+                    ifFalse = {
+                        remember { mutableStateOf(value = "Mock") }
+                        if (clicked) {
+                            return
+                        }
+                        clickable { println(clicked) }
+                    },
+                )
+            }
+        """,
+        extra = """
+            object Modifier
+
+            inline fun Modifier.thenIf(
+                condition: Boolean,
+                ifFalse: Modifier.() -> Modifier,
+                ifTrue: Modifier.() -> Modifier,
+            ) = if (condition) {
+                ifTrue()
+            } else {
+                ifFalse()
+            }
+            
+            fun Modifier.clickable(f: () -> Unit): Modifier = this
+        """
+    )
 }

plugins/compose/compiler-hosted/runtime-tests/src/commonTest/kotlin/androidx/compose/compiler/test/CompositionTests.kt
@@ -443,6 +443,31 @@ class CompositionTests {
             }
         }
     }
+
+    // Regression test for b/479646393
+    @Test
+    fun testInlineSwitchRemembers() = compositionTest {
+        var clicked by mutableStateOf(true)
+
+        compose {
+            Modifier.thenIf(
+                condition = clicked,
+                ifTrue = {
+                    clickable { clicked = !clicked }
+                },
+                ifFalse = {
+                    remember { mutableStateOf(value = "Mock") }
+                    clickable { clicked = !clicked }
+                },
+            )
+        }
+
+        clicked = !clicked
+        advance()
+
+        clicked = !clicked
+        advance()
+    }
 }
 
 @Composable
@@ -575,3 +600,17 @@ fun TwoLambdas(
 }
 
 private fun use(value: Any?) {}
+
+private object Modifier
+
+private inline fun Modifier.thenIf(
+    condition: Boolean,
+    ifFalse: Modifier.() -> Modifier,
+    ifTrue: Modifier.() -> Modifier,
+) = if (condition) {
+    ifTrue()
+} else {
+    ifFalse()
+}
+
+private fun Modifier.clickable(f: () -> Unit): Modifier = this

commit 50b1830e13dea9c8d31146e2980d5dc9746c83a5

Do not generate groups in inline lambdas without `@Composable` calls
https://github.com/JetBrains/kotlin/commit/639c9b39ef3543e46732a6aa66822916f6663555 introduced a regression that causes every inline lambda to generate a group in cases when function has two or more inline parameters. The groups are only required for lambdas with the `@Composable` calls inside.

Test: Compiler tests
Fixes: 509945632

plugins/compose/compiler-hosted/src/main/java/androidx/compose/compiler/plugins/kotlin/lower/ComposableFunctionBodyTransformer.kt
@@ -1297,19 +1297,24 @@ class ComposableFunctionBodyTransformer(
     private fun visitInlinedLambdaInComposableScope(declaration: IrFunction): IrStatement {
         val scope = currentFunctionScope
         val parentScope = scope.parent
-        val outerGroupRequired = parentScope is Scope.CaptureScope && parentScope.forceInlinedLambdaGroup
+        val forceInlineLambdaGroup = parentScope is Scope.CaptureScope && parentScope.forceInlinedLambdaGroup
 
-        if (!outerGroupRequired) {
-            val result = super.visitFunction(declaration)
+        if (!forceInlineLambdaGroup) {
+            val transformed = super.visitFunction(declaration)
             if (scope.hasComposableCalls) {
                 encounteredCapturedComposableCall()
             }
-            return result
+            return transformed
         }
 
         val originalBody = declaration.body ?: return super.visitFunction(declaration)
         val (body, returnVar) = originalBody.asBodyAndResultVar()
         body.transformChildrenVoid()
+
+        // Avoid transforming functions that are not referencing anything composable, as they cannot use slots.
+        if (!scope.hasComposableCalls) {
+            return super.visitFunction(declaration)
+        }
 
         scope.realizeGroup {
             irEndReplaceGroup(scope = scope, startOffset = body.endOffset, endOffset = body.endOffset)

plugins/compose/compiler-hosted/integration-tests/src/jvmTest/kotlin/androidx/compose/compiler/plugins/kotlin/ControlFlowTransformTests.kt
@@ -2707,4 +2707,35 @@ class ControlFlowTransformTests : AbstractControlFlowTransformTests() {
             fun Modifier.clickable(f: () -> Unit): Modifier = this
         """
     )
+
+    // Regression test for b/509945632
+    @Test
+    fun testInlineSwitchNoComposables() = verifyGoldenComposeIrTransform(
+        source = """
+            import androidx.compose.runtime.*
+
+            @Composable fun Test(clicked: Boolean) {
+                thenIf(
+                    condition = clicked,
+                    ifTrue = {
+                        "true"
+                    },
+                    ifFalse = {
+                        "false"
+                    },
+                )
+            }
+        """,
+        extra = """
+            inline fun thenIf(
+                condition: Boolean,
+                ifFalse: () -> Unit,
+                ifTrue: () -> Unit,
+            ) = if (condition) {
+                ifTrue()
+            } else {
+                ifFalse()
+            }
+        """
+    )
 }

plugins/compose/compiler-hosted/runtime-tests/src/commonTest/kotlin/androidx/compose/compiler/test/CompositionTests.kt
@@ -479,6 +479,23 @@ class CompositionTests {
             AnyParameter(Any())
         }
     }
+
+
+    // Regression test for b/509945632
+    @Test
+    fun testInlineFunctionWith2Lambdas() = compositionTest {
+        var elements by mutableStateOf(emptyList<String>())
+        var counter = 0
+        compose {
+            // if lambdas introduce a group, `ReceiveValueGroup` is removed after update
+            elements.associateBy({ it }, { it.length })
+            ReceiveValueGroup(counter)
+        }
+
+        elements = List(100) { "$it" }
+        counter += 10
+        advance()
+    }
 }
 
 @Composable
@@ -490,6 +507,12 @@ fun getCondition() = remember { false }
 @NonRestartableComposable
 @Composable
 fun ReceiveValue(value: Int) {
+    val string = remember { "$value" }
+    assertEquals(1, string.length)
+}
+
+@Composable
+fun ReceiveValueGroup(value: Int) {
     val string = remember { "$value" }
     assertEquals(1, string.length)
 }