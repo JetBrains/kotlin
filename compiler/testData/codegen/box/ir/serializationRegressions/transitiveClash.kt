b352b0895b84a30c5f17c3563e7559064968e067
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2026-05-06 12:46:37 +0200

    [Wasm] Ignore wrongly passing test

--- compiler/testData/codegen/box/ir/serializationRegressions/transitiveClash.kt
+++ compiler/testData/codegen/box/ir/serializationRegressions/transitiveClash.kt
@@ -1,11 +1,13 @@
 // IGNORE_BACKEND: WASM_JS, WASM_WASI
 // WASM_MUTE_REASON: SERIALIZATION_REGRESSION: Conflicting overloads: public fun foo(): String defined in pkg in file lib1.kt, public fun foo(): String defined in pkg in file main.kt (6,1)
+// TODO(KT-86166): This test fails in Wasm monolith mode, but not in single module mode.
+// With the current test infra, it's not possible to express this, so the test is hard-ignored for single module mode by excluding it during test generation.
+// Once KT-86166 is solved and provides us tools to exclude it properly, do that.
 // JS_IR error: Cross module dependency resolution failed due to signature 'pkg/foo|-1041209573719867811[0]' redefinition
 // IGNORE_BACKEND: JS_IR
 // IGNORE_BACKEND: JS_IR_ES6
 // NATIVE error caused by `foo()` clash: kotlin.AssertionError: Test failed with: 42K. Expected <OK>, actual <42K>.
 // DONT_TARGET_EXACT_BACKEND: NATIVE
-// WASM_FAILS_IN_SINGLE_MODULE_MODE
 
 // MODULE: lib1
 // FILE: lib1.kt
