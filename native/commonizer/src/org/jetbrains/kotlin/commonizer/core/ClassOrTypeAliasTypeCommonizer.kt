655e6366f7f8e3d228661bd1048bb3c1a9b64d4f
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-05-06 15:26:15 +0300

    [commonizer] Properly commonize type arguments for support expect classes
    ^KT-69909

--- native/commonizer/src/org/jetbrains/kotlin/commonizer/core/ClassOrTypeAliasTypeCommonizer.kt
+++ native/commonizer/src/org/jetbrains/kotlin/commonizer/core/ClassOrTypeAliasTypeCommonizer.kt
@@ -41,8 +41,9 @@ internal class ClassOrTypeAliasTypeCommonizer(
         val substitutedTypes = substituteTypesIfNecessary(values)
 
         if (substitutedTypes == null) {
-            classifiers.supportExpectClassSupplier?.buildSupportExpectTypeFor(expansions, isMarkedNullable)?.let {
-                return it
+            classifiers.supportExpectClassSupplier?.buildSupportExpectTypeFor(expansions)?.let {
+                val arguments = TypeArgumentListCommonizer(typeCommonizer).commonize(expansions.map { it.arguments }) ?: return@let
+                return CirClassType.createInterned(it, outerType = null, arguments, isMarkedNullable)
             }
 
             val integerCommonizationResultIfApplicable = isPlatformIntegerCommonizationEnabled.ifTrue {
