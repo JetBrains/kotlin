aef93d0d9503a3973a6165690b2f19bfcdc6062a
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2026-05-08 15:10:18 +0200

    [FIR] Extract `checkDistinctKeys` from `checkDistinctSourceElements`
    - `checkDistinctKeys` will be reused by symbol ID constraint checks.
    - We keep the explicit `toLocationDescription` out of
      `checkDistinctKeys` so that `compiler.fir.tree` doesn't have to depend
      on IntelliJ/PSI specifics.
    
    ^KT-84343

--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/frontend/fir/sourceElements.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/frontend/fir/sourceElements.kt
@@ -5,75 +5,25 @@
 
 package org.jetbrains.kotlin.test.frontend.fir
 
-import com.intellij.openapi.util.text.StringUtil
-import org.jetbrains.kotlin.KtPsiSourceElement
 import org.jetbrains.kotlin.KtSourceElement
-import org.jetbrains.kotlin.fir.FirElement
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.declarations.FirFile
-import org.jetbrains.kotlin.fir.visitors.FirVisitorVoid
-import kotlin.collections.forEach
+import org.jetbrains.kotlin.fir.utils.checkDistinctKeys
 
 /**
  * Checks that all FIR declarations in the given [files] have distinct [KtSourceElement]s. This is a requirement of FIR as Data (KT-84343)
  * and checked in various tests.
  *
- * @param lazyErrorHeadline A headline for the error message if the check fails. The parameters are the two FIR declarations that have
- *  conflicting source elements.
+ * @param lazyErrorTitle A title for the error message if the check fails. The parameters are the two FIR declarations that have conflicting
+ *  source elements.
  */
-fun checkDistinctSourceElements(files: List<FirFile>, lazyErrorHeadline: (FirDeclaration, FirDeclaration) -> String) {
-    val declarationBySourceElement = hashMapOf<KtSourceElement, FirDeclaration>()
-
-    val visitor = object : FirVisitorVoid() {
-        override fun visitElement(element: FirElement) {
-            if (element is FirDeclaration) {
-                checkDeclaration(element)
-            }
-
-            element.acceptChildren(this)
-        }
-
-        private fun checkDeclaration(declaration: FirDeclaration) {
-            val sourceElement = declaration.symbol.source ?: return
-            val previousDeclaration = declarationBySourceElement.put(sourceElement, declaration)
-
-            // We have to compare `previousDeclaration` and `declaration` with reference equality, because regular equality might defer to
-            // the source element whose uniqueness we want to check in the first place.
-            //
-            // Source element uniqueness doesn't prevent the *exact* same FIR declaration instance from appearing multiple times. It's about
-            // different FIR declaration instances sharing equal source elements accidentally.
-            if (previousDeclaration != null && previousDeclaration !== declaration) {
-                throwDuplicateSourceElementsError(lazyErrorHeadline, previousDeclaration, declaration)
-            }
-        }
-    }
-
-    files.forEach { it.accept(visitor) }
-}
-
-private fun throwDuplicateSourceElementsError(
-    lazyErrorHeadline: (FirDeclaration, FirDeclaration) -> String,
-    previousDeclaration: FirDeclaration,
-    declaration: FirDeclaration,
-): Nothing {
-    val message = buildString {
-        append(lazyErrorHeadline(previousDeclaration, declaration))
-        appendLine(":")
-
-        append("  First declaration:  ")
-        append(previousDeclaration::class.simpleName)
-        append(" with ")
-        append(previousDeclaration.source.toLocationDescription())
-        appendLine()
-
-        append("  Second declaration: ")
-        append(declaration::class.simpleName)
-        append(" with ")
-        append(declaration.source.toLocationDescription())
-        appendLine()
-    }
-
-    error(message)
+fun checkDistinctSourceElements(files: List<FirFile>, lazyErrorTitle: (FirDeclaration, FirDeclaration) -> String) {
+    checkDistinctKeys(
+        files,
+        keyExtractor = { it.symbol.source },
+        lazyErrorTitle = lazyErrorTitle,
+        formatLocation = { it.source.toLocationDescription() },
+    )
 }
 
 private fun KtSourceElement?.toLocationDescription(): String =
