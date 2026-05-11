d527f10f50db2aa6b50797754cb3f9ca8f2b6d39
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-05-11 20:31:40 +0200

    [LL] migrate AbstractStdLibBasedGetOrBuildFirTest to new resolution API
    ^KT-84925

--- analysis/low-level-api-fir/testFixtures/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibBasedGetOrBuildFirTest.kt
+++ analysis/low-level-api-fir/testFixtures/org/jetbrains/kotlin/analysis/low/level/api/fir/AbstractStdLibBasedGetOrBuildFirTest.kt
@@ -5,34 +5,39 @@
 
 package org.jetbrains.kotlin.analysis.low.level.api.fir
 
+import com.intellij.psi.util.parentOfType
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.resolveToFirSymbol
 import org.jetbrains.kotlin.analysis.low.level.api.fir.services.firRenderingOptions
 import org.jetbrains.kotlin.analysis.low.level.api.fir.test.configurators.LLSourceLikeTestConfigurator
 import org.jetbrains.kotlin.analysis.test.framework.base.AbstractAnalysisApiBasedTest
 import org.jetbrains.kotlin.analysis.test.framework.projectStructure.KtTestModule
 import org.jetbrains.kotlin.analysis.test.framework.services.expressionMarkerProvider
-import org.jetbrains.kotlin.analysis.test.framework.utils.unwrapMultiReferences
-import org.jetbrains.kotlin.idea.references.KtReference
 import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtExperimentalApi
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.resolution.KtResolvable
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.assertions
 
 abstract class AbstractStdLibBasedGetOrBuildFirTest : AbstractAnalysisApiBasedTest() {
     override val configurator = LLSourceLikeTestConfigurator()
 
+    @OptIn(KtExperimentalApi::class)
     override fun doTestByMainFile(mainFile: KtFile, mainModule: KtTestModule, testServices: TestServices) {
         val project = mainFile.project
         assert(!project.isDisposed) { "$project is disposed" }
         val caretPosition = testServices.expressionMarkerProvider.getCaret(mainFile)
-        val ktReferences = mainFile.findReferenceAt(caretPosition)?.unwrapMultiReferences().orEmpty().filterIsInstance<KtReference>()
-        if (ktReferences.size != 1) {
-            testServices.assertions.fail { "No references at caret found" }
+        val element = mainFile.findElementAt(caretPosition)?.parentOfType<KtElement>(true)
+            ?: testServices.assertions.fail { "No element at caret found" }
+
+        if (element !is KtResolvable) {
+            testServices.assertions.fail { "Element at caret is not resolvable" }
         }
-        val declaration =
-            analyzeForTest(ktReferences.first().element) {
-                ktReferences.first().resolveToSymbol()?.psi as KtDeclaration
-            }
+
+        val declaration = analyzeForTest(element) {
+            element.resolveSymbol()?.psi as KtDeclaration
+        }
 
         val resolutionFacade = LLResolutionFacadeService.getInstance(project).getResolutionFacade(mainModule.ktModule)
         val fir = declaration.resolveToFirSymbol(resolutionFacade).fir
