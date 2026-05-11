b48a5849e065ff1038f7f2a266265c910573a182
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-05-11 13:50:24 +0200

    [Wasm] Ignore transitiveClash.kt with WASM_IGNORE_FOR
    ^KT-86166 Fixed

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/generators/tests/GenerateWasmTests.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/generators/tests/GenerateWasmTests.kt
@@ -36,9 +36,6 @@ fun main(args: Array<String>) {
     // TODO: Remove excludedPattern below after fix of KT-78960 (it's simpler to exclude temporarily than to split test `boxInline/innerClasses/kt12126.kt`)
     val excludedPatternForBoxInlineTestsWithInliner = "kt12126.kt"
 
-    // TODO: Remove excludedPattern and ignore it correctly after fixing KT-86166
-    val excludePatternForSingleModuleTest = "transitiveClash.kt"
-
     generateTestGroupSuiteWithJUnit5(args) {
         testGroup(testsRoot, "compiler/testData/klib/partial-linkage") {
             testClass<AbstractWasmPartialLinkageNoICTestCase> {
@@ -136,7 +133,7 @@ fun main(args: Array<String>) {
 
         testGroup(testsRoot, "compiler/testData", testRunnerMethodName = "runTest0") {
             testClass<AbstractFirWasmJsCodegenSingleModuleBoxTest> {
-                model("codegen/box", pattern = jsTranslatorTestPattern, excludeDirs = jvmOnlyBoxTests + k1BoxTestDir, excludedPattern = excludePatternForSingleModuleTest)
+                model("codegen/box", pattern = jsTranslatorTestPattern, excludeDirs = jvmOnlyBoxTests + k1BoxTestDir)
             }
 
             testClass<AbstractFirWasmJsCodegenMultiModuleBoxTest> {
