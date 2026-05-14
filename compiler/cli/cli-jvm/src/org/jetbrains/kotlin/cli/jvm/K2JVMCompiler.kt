997b2f156bc8e44b5623b292a0b72762ef7afc40
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-05-14 12:30:16 +0300

    [CLI] Move `configureModuleChunk` to the `JvmConfigurationPipelinePhase`

--- compiler/cli/cli-jvm/src/org/jetbrains/kotlin/cli/jvm/K2JVMCompiler.kt
+++ compiler/cli/cli-jvm/src/org/jetbrains/kotlin/cli/jvm/K2JVMCompiler.kt
@@ -7,31 +7,24 @@ package org.jetbrains.kotlin.cli.jvm
 
 import com.intellij.openapi.Disposable
 import org.jetbrains.kotlin.K1Deprecation
-import org.jetbrains.kotlin.cli.CliDiagnostics
-import org.jetbrains.kotlin.cli.CliDiagnostics.COMPILER_ARGUMENTS_WARNING
 import org.jetbrains.kotlin.cli.common.CLICompiler
 import org.jetbrains.kotlin.cli.common.ExitCode
 import org.jetbrains.kotlin.cli.common.arguments.K2JVMCompilerArguments
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
-import org.jetbrains.kotlin.cli.common.modules.ModuleBuilder
 import org.jetbrains.kotlin.cli.common.modules.ModuleChunk
 import org.jetbrains.kotlin.cli.common.profiling.ProfilingCompilerPerformanceManager
-import org.jetbrains.kotlin.cli.jvm.compiler.CompileEnvironmentUtil
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
-import org.jetbrains.kotlin.cli.jvm.compiler.configureFromArgs
 import org.jetbrains.kotlin.cli.pipeline.CheckCompilationErrors.CheckDiagnosticCollector
 import org.jetbrains.kotlin.cli.pipeline.jvm.JvmCliPipeline
-import org.jetbrains.kotlin.cli.report
-import org.jetbrains.kotlin.cli.reportException
-import org.jetbrains.kotlin.config.*
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.Services
+import org.jetbrains.kotlin.config.perfManager
 import org.jetbrains.kotlin.metadata.deserialization.BinaryVersion
 import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
-import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
 import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.util.PerformanceManager
-import java.io.File
 
 class K2JVMCompiler : CLICompiler<K2JVMCompilerArguments>() {
     override val platform: TargetPlatform
@@ -97,52 +90,6 @@ class K2JVMCompiler : CLICompiler<K2JVMCompilerArguments>() {
 
     override fun createPerformanceManager(arguments: K2JVMCompilerArguments, services: Services): PerformanceManager {
         return createCustomPerformanceManagerOrNull(arguments, services) ?: defaultPerformanceManager
-    }
-}
-
-fun CompilerConfiguration.configureModuleChunk(
-    arguments: K2JVMCompilerArguments,
-    buildFile: File?
-): ModuleChunk {
-    val destination = arguments.destination?.let { File(it) }
-
-    return if (buildFile != null) {
-        fun strongWarning(message: String) {
-            this.report(COMPILER_ARGUMENTS_WARNING, message)
-        }
-
-        if (destination != null) {
-            strongWarning("The '-d' option with a directory destination is ignored because '-Xbuild-file' is specified")
-        }
-        if (arguments.javaSourceRoots.isNotEmpty()) {
-            strongWarning("The '-Xjava-source-roots' option is ignored because '-Xbuild-file' is specified")
-        }
-        if (arguments.javaPackagePrefix != null) {
-            strongWarning("The '-Xjava-package-prefix' option is ignored because '-Xbuild-file' is specified")
-        }
-        configureContentRootsFromClassPath(arguments)
-        put(JVMConfigurationKeys.MODULE_XML_FILE, buildFile)
-        CompileEnvironmentUtil.loadModuleChunk(
-            buildFile,
-            { report(CliDiagnostics.JVM_CLI_ERROR, it) },
-            { reportException(it) }
-        )
-    } else {
-        if (destination != null) {
-            if (destination.path.endsWith(".jar")) {
-                put(JVMConfigurationKeys.OUTPUT_JAR, destination)
-            } else {
-                put(JVMConfigurationKeys.OUTPUT_DIRECTORY, destination)
-            }
-        }
-
-        val module = ModuleBuilder(
-            this[CommonConfigurationKeys.MODULE_NAME] ?: JvmProtoBufUtil.DEFAULT_MODULE_NAME,
-            destination?.path ?: ".", "java-production"
-        )
-        module.configureFromArgs(arguments)
-
-        ModuleChunk(listOf(module))
     }
 }
 
