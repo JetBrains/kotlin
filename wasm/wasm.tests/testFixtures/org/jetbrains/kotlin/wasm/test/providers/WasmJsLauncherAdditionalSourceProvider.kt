ad9d089c54bf13905d08cb66da207441ff76015c
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-05-15 09:56:39 +0200

    [Tests] Unify Wasm,Native to additional source providers for test launcher
    ^KT-85850

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/providers/WasmJsLauncherAdditionalSourceProvider.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/providers/WasmJsLauncherAdditionalSourceProvider.kt
@@ -5,55 +5,17 @@
 
 package org.jetbrains.kotlin.wasm.test.providers
 
-import org.jetbrains.kotlin.test.directives.ModuleStructureDirectives.ESCAPE_MODULE_NAME
-import org.jetbrains.kotlin.test.directives.model.RegisteredDirectives
-import org.jetbrains.kotlin.test.impl.shouldIsolateTestInGroupingConfiguration
-import org.jetbrains.kotlin.test.model.TestFile
-import org.jetbrains.kotlin.test.model.TestModule
-import org.jetbrains.kotlin.test.services.BatchingPackageInserter
-import org.jetbrains.kotlin.test.services.TestModuleStructure
 import org.jetbrains.kotlin.test.services.TestServices
-import org.jetbrains.kotlin.test.services.sourceProviders.MainFunctionForBlackBoxTestsSourceProvider
-import org.jetbrains.kotlin.test.services.temporaryDirectoryManager
-import org.jetbrains.kotlin.test.services.testInfo
+import org.jetbrains.kotlin.test.services.sourceProviders.AbstractLauncherAdditionalSourceProvider
 
-private const val LAUNCHER_FILE_NAME = "__launcher__.kt"
-private val BOX_FUNCTION_NAME = "box"
-
-class WasmJsLauncherAdditionalSourceProvider(testServices: TestServices) : MainFunctionForBlackBoxTestsSourceProvider(testServices) {
-    override fun produceAdditionalFiles(
-        globalDirectives: RegisteredDirectives,
-        module: TestModule,
-        testModuleStructure: TestModuleStructure
-    ): List<TestFile> {
-        val fileWithBox = module.files.firstOrNull { containsBoxMethod(it.originalContent) } ?: return emptyList()
-        var boxFqName = detectPackage(fileWithBox)?.let { "$it.$BOX_FUNCTION_NAME" } ?: BOX_FUNCTION_NAME
-        if (ESCAPE_MODULE_NAME in globalDirectives &&
-            // Non-isolated tests will be grouped, hence their `box()` functions have to be moved to separate packages to avoid clashes.
-            // In isolated tests, packages are not altered
-            !testServices.shouldIsolateTestInGroupingConfiguration(testModuleStructure, fileGenerationPhase = true)
-        ) {
-            val additionalPackage = BatchingPackageInserter.computePackage(testServices.testInfo)
-            boxFqName = "$additionalPackage.$boxFqName"
-        }
-        val launcherContent = generateBoxFunctionLauncher(boxFqName)
-
-        val tempDir = testServices.temporaryDirectoryManager.getOrCreateTempDirectory("launcher")
-        val launcherFile = tempDir.resolve(LAUNCHER_FILE_NAME).also {
-            it.writeText(launcherContent)
-        }
-        return listOf(launcherFile.toTestFile())
-    }
-}
-
-fun generateBoxFunctionLauncher(entryPointFunctionFQN: String, expectedResult: String = "OK"): String =
-    """
-        @kotlin.contracts.ExperimentalContracts  // for tests in compiler/testData/codegen/box/contracts/
+class WasmJsLauncherAdditionalSourceProvider(testServices: TestServices) : AbstractLauncherAdditionalSourceProvider(testServices) {
+    override fun generateLauncherContent(boxFqName: String, expectedResult: String): String = """
         class Launcher {
             @kotlin.test.Test
             fun runTest() {
-                val result = $entryPointFunctionFQN()
+                val result = $boxFqName()
                 kotlin.test.assertEquals("$expectedResult", result, "Test failed with: ${'$'}result")
             }
         }
     """.trimIndent()
+}
