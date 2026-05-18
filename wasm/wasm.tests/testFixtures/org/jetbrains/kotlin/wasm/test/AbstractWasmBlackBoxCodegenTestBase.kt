93ea95333ee76d1bd005120d6e80b10d9fa00157
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-05-18 20:30:50 +0200

    fixup! [Tests][Wasm] Extract setupStepsForWasmFirstStageUpToSerialization from commonConfigurationForWasmFirstStageTest

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/AbstractWasmBlackBoxCodegenTestBase.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/AbstractWasmBlackBoxCodegenTestBase.kt
@@ -21,7 +21,11 @@ import org.jetbrains.kotlin.test.directives.DiagnosticsDirectives.DIAGNOSTICS
 import org.jetbrains.kotlin.test.directives.LanguageSettingsDirectives.LANGUAGE
 import org.jetbrains.kotlin.test.directives.WasmEnvironmentConfigurationDirectives
 import org.jetbrains.kotlin.test.directives.model.ValueDirective
+import org.jetbrains.kotlin.test.frontend.fir.handlers.FirCfgConsistencyHandler
+import org.jetbrains.kotlin.test.frontend.fir.handlers.FirCfgDumpHandler
 import org.jetbrains.kotlin.test.frontend.fir.handlers.FirDiagnosticsHandler
+import org.jetbrains.kotlin.test.frontend.fir.handlers.FirDumpHandler
+import org.jetbrains.kotlin.test.frontend.fir.handlers.FirResolvedTypesVerifier
 import org.jetbrains.kotlin.test.model.*
 import org.jetbrains.kotlin.test.runners.AbstractKotlinCompilerWithTargetBackendTest
 import org.jetbrains.kotlin.test.services.AdditionalSourceProvider
@@ -142,11 +146,27 @@ fun <R : ResultingArtifact.FrontendOutput<R>, I : ResultingArtifact.BackendInput
     )
 }
 
-fun TestConfigurationBuilder.setupStepsForWasmFirstStageUpToSerialization() {
+fun TestConfigurationBuilder.setupStepsForWasmFirstStageUpToSerialization(
+    includeBasicFirHandlers: Boolean = true,
+    includeDumpFirHandlers: Boolean = true
+) {
     facadeStep(::FirCliWebFacade)
 
     firHandlersStep {
-        useHandlers(::FirDiagnosticsHandler)
+        if (includeBasicFirHandlers) {
+            useHandlers(
+                ::FirCfgConsistencyHandler,
+                ::FirResolvedTypesVerifier,
+                ::FirDiagnosticsHandler,
+            )
+        }
+        if (includeDumpFirHandlers) {
+            useHandlers(
+                ::FirDumpHandler,
+                ::FirCfgDumpHandler,
+            )
+        }
+
     }
 
     facadeStep(::Fir2IrCliWebFacade)
