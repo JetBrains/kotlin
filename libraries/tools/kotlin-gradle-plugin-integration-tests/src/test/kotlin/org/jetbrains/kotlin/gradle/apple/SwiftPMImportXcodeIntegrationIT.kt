01908def41459dc9abc0246fcdf06223bbb2c82b
Andrei Tyrin <andrei.tyrin@jetbrains.com>
2026-05-18 17:30:19 +0200

    [tests] Add integrateEmbedAndSign task tests

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/apple/SwiftPMImportXcodeIntegrationIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/apple/SwiftPMImportXcodeIntegrationIT.kt
@@ -1266,38 +1266,6 @@ class SwiftPMImportXcodeIntegrationIT : KGPBaseTest() {
     }
 
     @GradleTest
-    fun `integrateEmbedAndSign uses root project task path without duplicate separators`(version: GradleVersion) {
-        Assumptions.assumeTrue(version >= GradleVersion.version("8.14.4"))
-
-        project("emptyxcode-no-embedandsign", version) {
-            initDefaultKmpWithLocalSPM()
-
-            val pbxFile = projectPath.resolve("iosApp/iosApp.xcodeproj/project.pbxproj")
-
-            build(
-                "integrateEmbedAndSign",
-                environmentVariables = EnvironmentalVariables(
-                    "XCODEPROJ_PATH" to "iosApp/iosApp.xcodeproj",
-                    "GRADLEW_PATH" to projectPath.resolve("gradlew").absolutePathString(),
-                    "GRADLE_PROJECT_PATH" to ":",
-                )
-            ) {
-                val pbxFileContent = pbxFile.readText()
-
-                assertContains(
-                    pbxFileContent,
-                    "./gradlew :embedAndSignAppleFrameworkForXcode -i",
-                    message = "Generated embed-and-sign phase should target the root project task path",
-                )
-                assertFalse(
-                    pbxFileContent.contains("./gradlew ::embedAndSignAppleFrameworkForXcode -i"),
-                    "Generated embed-and-sign phase should not contain duplicate separators in the task path",
-                )
-            }
-        }
-    }
-
-    @GradleTest
     fun `embedAndSignAppleFrameworkForXcode uses root gradlew for external xcode project`(version: GradleVersion) {
         project("emptyxcode", version) {
             initDefaultKmpWithLocalSPM()
