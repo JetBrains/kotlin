8bbbbb59e9f0c16b8b3909995889ce606bf13b6b
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-05-19 14:09:01 +0200

    ~ [cc] build

--- compiler/java-direct/build.gradle.kts
+++ compiler/java-direct/build.gradle.kts
@@ -57,7 +57,10 @@ javadocJar()
 
 projectTests {
     testTask(
-        jUnitMode = JUnitMode.JUnit5, defineJDKEnvVariables = listOf(
+        jUnitMode = JUnitMode.JUnit5,
+        javaLauncher = JdkMajorVersion.JDK_1_8,
+        defineJDKEnvVariables = listOf(
+            JdkMajorVersion.JDK_1_8,
             JdkMajorVersion.JDK_11_0,
             JdkMajorVersion.JDK_17_0,
             JdkMajorVersion.JDK_21_0
