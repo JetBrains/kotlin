3d43a2d55e2fe2c36690dd5406c8069c8e836907
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2026-05-19 15:53:07 +0200

    [BTA] Add test for KT-72768

---
+++ compiler/build-tools/kotlin-build-tools-api-tests/src/testInputChangesTracking/kotlin/InterfaceChangesTest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.buildtools.tests.compilation
+
+import org.jetbrains.kotlin.buildtools.tests.CompilerExecutionStrategyConfiguration
+import org.jetbrains.kotlin.buildtools.tests.compilation.assertions.assertCompiledSources
+import org.jetbrains.kotlin.buildtools.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
+import org.jetbrains.kotlin.buildtools.tests.compilation.scenario.jvmScenario
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("Interface ABI changes in incremental compilation")
+class InterfaceChangesTest : BaseCompilationTest() {
+    @DefaultStrategyAgnosticCompilationTest
+    @DisplayName("KT-72768: Adding a default method to interface should recompile anonymous implementors")
+    @TestMetadata("ic-scenarios/kt-72768")
+    fun testKt72768_addDefaultMethodRecompilesAnonymousImplementors(strategyConfig: CompilerExecutionStrategyConfiguration) {
+        jvmScenario(strategyConfig) {
+            val mod = module("ic-scenarios/kt-72768")
+
+            mod.replaceFileWithVersion("A.kt", "add-default-method")
+            mod.compile {
+                assertCompiledSources("A.kt", "B.kt")
+            }
+        }
+    }
+}
