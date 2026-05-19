554687e5177552b2dc42bd251d4afaf8b6ad804f
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2026-05-19 17:03:00 +0200

    [BTA] Add test for KT-25455

---
+++ compiler/build-tools/kotlin-build-tools-api-tests/src/testInputChangesTracking/kotlin/ClassHierarchyChangesTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.buildtools.tests.compilation
+
+import org.jetbrains.kotlin.buildtools.tests.CompilerExecutionStrategyConfiguration
+import org.jetbrains.kotlin.buildtools.tests.compilation.assertions.assertCompiledSources
+import org.jetbrains.kotlin.buildtools.tests.compilation.model.DefaultStrategyAgnosticCompilationTest
+import org.jetbrains.kotlin.buildtools.tests.compilation.scenario.jvmScenario
+import org.jetbrains.kotlin.test.TestMetadata
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("Class hierarchy changes in incremental compilation")
+class ClassHierarchyChangesTest : BaseCompilationTest() {
+    @DefaultStrategyAgnosticCompilationTest
+    @DisplayName("KT-25455: Removing a supertype should recompile indirect subclasses")
+    @TestMetadata("ic-scenarios/kt-25455/module-app")
+    fun testKt25455_removedSupertypeRecompilesIndirectSubclasses(strategyConfig: CompilerExecutionStrategyConfiguration) {
+        jvmScenario(strategyConfig) {
+            val lib = module("ic-scenarios/kt-25455/lib")
+            val app = module("ic-scenarios/kt-25455/app", dependencies = listOf(lib))
+
+            lib.replaceFileWithVersion("B.kt", "remove-superclass")
+            lib.compile { assertCompiledSources("B.kt") }
+            app.compile {
+                expectFail()
+                assertCompiledSources("C.kt", "D.kt", "main.kt")
+            }
+        }
+    }
+}
