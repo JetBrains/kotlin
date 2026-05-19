846a57a6f9af59599c042866fa5cd4b21366e0cd
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-05-19 19:32:37 +0200

    [Tests][Wasm] Ignore functionReference.kt when testing K/Wasm 2.3 compiler

--- compiler/testData/codegen/box/coroutines/tailCallOptimizations/unit/functionReference.kt
+++ compiler/testData/codegen/box/coroutines/tailCallOptimizations/unit/functionReference.kt
@@ -1,4 +1,8 @@
 // IGNORE_BACKEND: JS_IR_ES6
+// IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_SECOND_STAGE: Wasm-JS:2.3
+// ^^^ Running after compilation with Kotlin 2.3 gives:
+//     RuntimeError: illegal cast
+//        at <main>.$invokeCOROUTINE$.doResume (wasm://wasm/<main>-002e8cb2:wasm-function[3874]:0x4c55a)
 // WITH_STDLIB
 // WITH_COROUTINES
 
