d69633d9dcae3afe118420528f50b8af2e07b749
Alexander.Likhachev <Alexander.Likhachev@jetbrains.com>
2026-05-19 15:53:41 +0200

    [BTA] Add test for KT-53854

--- compiler/build-tools/kotlin-build-tools-api-tests/src/testInputChangesTracking/kotlin/InterfaceChangesTest.kt
+++ compiler/build-tools/kotlin-build-tools-api-tests/src/testInputChangesTracking/kotlin/InterfaceChangesTest.kt
@@ -27,4 +27,18 @@ class InterfaceChangesTest : BaseCompilationTest() {
             }
         }
     }
+
+    @DefaultStrategyAgnosticCompilationTest
+    @DisplayName("KT-53854: Adding a default method to interface should recompile anonymous implementors")
+    @TestMetadata("ic-scenarios/kt-53854")
+    fun testKt53854_addDefaultMethodRecompilesAnonymousImplementors(strategyConfig: CompilerExecutionStrategyConfiguration) {
+        jvmScenario(strategyConfig) {
+            val mod = module("ic-scenarios/kt-53854")
+
+            mod.replaceFileWithVersion("i.kt", "add-default-method")
+            mod.compile {
+                assertCompiledSources("i.kt", "main.kt")
+            }
+        }
+    }
 }
