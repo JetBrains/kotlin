521488ee17934027a4d4474aa38fd4f5e7f2bd8c
Adam Semenenko <adam.semenenko@jetbrains.com>
2026-05-20 20:34:05 +0200

    fixup! [Gradle] Register lock files as RequiresNpmDependenciesTasks inputs
    Test npm version changes cause Webpack task to re-run.
    
    Test all combinations of:
    - Targets: JS and WasmJS
    - npm package managers: Yarn and npm
    - Custom KGP dev npm tooling directory.
    
    Tests for KT-84724

---
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/web/KotlinWebpackUpToDateIT.kt
@@ -0,0 +1,393 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+@file:OptIn(ExperimentalWasmDsl::class)
+
+package org.jetbrains.kotlin.gradle.web
+
+import kotlinx.serialization.json.*
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.withType
+import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.npm.BaseNpmExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.LockFileMismatchReport
+import org.jetbrains.kotlin.gradle.targets.js.npm.RequiresNpmDependenciesTask
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
+import org.jetbrains.kotlin.gradle.targets.web.nodejs.CommonNodeJsRootPlugin
+import org.jetbrains.kotlin.gradle.targets.web.yarn.BaseYarnRootExtension
+import org.jetbrains.kotlin.gradle.targets.web.yarn.CommonYarnPlugin
+import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.gradle.uklibs.ProjectIncludeCopyMode
+import org.jetbrains.kotlin.gradle.uklibs.include
+import org.jetbrains.kotlin.gradle.util.setupCustomKgpNpmToolingDependenciesDir
+import org.junit.jupiter.api.Disabled
+import kotlin.io.path.*
+import kotlin.test.assertEquals
+
+@GradleTestVersions(
+    // Test does not depend on Gradle-version specifics.
+    // If these tests fail in older Gradle versions, it's probably a Gradle bug.
+    minVersion = TestVersions.Gradle.MAX_SUPPORTED
+)
+class KotlinWebpackUpToDateIT : KGPBaseTest() {
+
+    override val defaultBuildOptions: BuildOptions
+        get() = super.defaultBuildOptions.disableIsolatedProjectsBecauseOfJsAndWasmKT75899()
+
+    @GradleTest
+    @JsGradlePluginTests
+    fun `testChangingNpmDepVersion - js - yarn`(gradleVersion: GradleVersion) {
+        testChangingNpmDepVersion(gradleVersion, buildTaskPath = ":sp1:jsBrowserDevelopmentWebpack", useYarn = true)
+    }
+
+    @GradleTest
+    @MppGradlePluginTests
+    fun `testChangingNpmDepVersion - wasmjs - yarn`(gradleVersion: GradleVersion) {
+        testChangingNpmDepVersion(gradleVersion, buildTaskPath = ":sp1:wasmJsBrowserDevelopmentWebpack", useYarn = true)
+    }
+
+    @GradleTest
+    @JsGradlePluginTests
+    fun `testChangingNpmDepVersion - js - npm`(gradleVersion: GradleVersion) {
+        testChangingNpmDepVersion(gradleVersion, buildTaskPath = ":sp1:jsBrowserDevelopmentWebpack", useYarn = false)
+    }
+
+    @GradleTest
+    @MppGradlePluginTests
+    fun `testChangingNpmDepVersion - wasmjs - npm`(gradleVersion: GradleVersion) {
+        testChangingNpmDepVersion(gradleVersion, buildTaskPath = ":sp1:wasmJsBrowserDevelopmentWebpack", useYarn = false)
+    }
+
+    private fun testChangingNpmDepVersion(
+        gradleVersion: GradleVersion,
+        buildTaskPath: String,
+        useYarn: Boolean,
+    ) {
+        createTestProject(gradleVersion, useYarn = useYarn) {
+            var cowsayVersion = "1.5.0"
+
+            build(
+                buildTaskPath,
+                "-PcowsayVersion=$cowsayVersion",
+                enableBuildCacheDebug = true,
+            ) {
+                assertTasksExecuted(buildTaskPath)
+            }
+
+            build(
+                buildTaskPath,
+                "-PcowsayVersion=$cowsayVersion",
+                enableBuildCacheDebug = true,
+            ) {
+                assertTasksUpToDate(buildTaskPath)
+            }
+
+            cowsayVersion = "1.6.0"
+            build(
+                "upgradeAllLockFiles",
+                buildTaskPath,
+                "-PcowsayVersion=$cowsayVersion",
+                enableBuildCacheDebug = true,
+            ) {
+                assertTasksExecuted(buildTaskPath)
+            }
+        }
+    }
+
+    @GradleTest
+    @JsGradlePluginTests
+    @Disabled("Kotlin/JS does not have a separate KGP npm tooling dir")
+    fun `npm tooling - js - yarn`(gradleVersion: GradleVersion) {
+        testCustomNpmToolingDir(gradleVersion, buildTaskPath = ":sp1:jsBrowserDevelopmentWebpack", useYarn = true)
+    }
+
+    @GradleTest
+    @MppGradlePluginTests
+    fun `npm tooling - wasmjs - yarn`(gradleVersion: GradleVersion) {
+        testCustomNpmToolingDir(gradleVersion, buildTaskPath = ":sp1:wasmJsBrowserDevelopmentWebpack", useYarn = true)
+    }
+
+    @GradleTest
+    @JsGradlePluginTests
+    @Disabled("Kotlin/JS does not have a separate KGP npm tooling dir")
+    fun `npm tooling - js - npm`(gradleVersion: GradleVersion) {
+        testCustomNpmToolingDir(gradleVersion, buildTaskPath = ":sp1:jsBrowserDevelopmentWebpack", useYarn = false)
+    }
+
+    @GradleTest
+    @MppGradlePluginTests
+    fun `npm tooling - wasmjs - npm`(gradleVersion: GradleVersion) {
+        testCustomNpmToolingDir(gradleVersion, buildTaskPath = ":sp1:wasmJsBrowserDevelopmentWebpack", useYarn = false)
+    }
+
+    private fun testCustomNpmToolingDir(
+        gradleVersion: GradleVersion,
+        buildTaskPath: String,
+        useYarn: Boolean,
+    ) {
+        createTestProject(gradleVersion, useYarn = useYarn) {
+
+            val toolingCustomDir = projectPath.resolve("kgp-tooling-" + if (useYarn) "yarn" else "npm")
+
+            setupCustomKgpNpmToolingDependenciesDir(
+                toolingCustomDir = toolingCustomDir,
+                useYarn = useYarn,
+            )
+
+            val toolingPackageJson = toolingCustomDir.resolve("package.json")
+            val toolingPackageJsonObj: JsonObject = json.decodeFromString(toolingPackageJson.readText())
+            val toolingPackageJsonObjUpdated = buildJsonObject {
+                toolingPackageJsonObj.forEach { [key, value] ->
+                    put(key, value)
+                }
+                put("dependencies", buildJsonObject {
+                    toolingPackageJsonObj.getValue("dependencies").jsonObject.forEach { [key, value] ->
+                        put(key, value)
+                    }
+                    put("cowsay", "1.6.0")
+                })
+            }
+            toolingPackageJson.writeText(json.encodeToString(toolingPackageJsonObjUpdated))
+
+            build(":toolingInstall") {
+                assertTasksExecuted(":toolingInstall")
+            }
+
+            build(
+                "upgradeAllLockFiles",
+                buildTaskPath,
+                enableBuildCacheDebug = true,
+            ) {
+                assertTasksExecuted(buildTaskPath)
+            }
+        }
+    }
+
+    @GradleTest
+    @JsGradlePluginTests
+    fun `test file npm dependency disables caching - js - yarn`(gradleVersion: GradleVersion) {
+        testFileNpmDependencyDisablesCaching(gradleVersion, buildTaskPath = ":sp2:jsBrowserDevelopmentWebpack", useYarn = true)
+    }
+
+    @GradleTest
+    @MppGradlePluginTests
+    fun `test file npm dependency disables caching - wasmjs - yarn`(gradleVersion: GradleVersion) {
+        testFileNpmDependencyDisablesCaching(gradleVersion, buildTaskPath = ":sp2:wasmJsBrowserDevelopmentWebpack", useYarn = true)
+    }
+
+    @GradleTest
+    @JsGradlePluginTests
+    fun `test file npm dependency disables caching - js - npm`(gradleVersion: GradleVersion) {
+        testFileNpmDependencyDisablesCaching(gradleVersion, buildTaskPath = ":sp2:jsBrowserDevelopmentWebpack", useYarn = false)
+    }
+
+    @GradleTest
+    @MppGradlePluginTests
+    fun `test file npm dependency disables caching - wasmjs - npm`(gradleVersion: GradleVersion) {
+        testFileNpmDependencyDisablesCaching(gradleVersion, buildTaskPath = ":sp2:wasmJsBrowserDevelopmentWebpack", useYarn = false)
+    }
+
+    private fun testFileNpmDependencyDisablesCaching(
+        gradleVersion: GradleVersion,
+        buildTaskPath: String,
+        useYarn: Boolean,
+    ) {
+        createTestProject(gradleVersion, useYarn = useYarn) {
+            build(
+                buildTaskPath,
+                enableBuildCacheDebug = true,
+            ) {
+                assertTasksExecuted(buildTaskPath)
+
+                val lines =
+                    outputReader.useLines { lines ->
+                        lines.filter { buildTaskPath in it && "file-based NPM dependencies" in it }
+                            .distinct()
+                            .joinToString("\n")
+                            .trim()
+                    }
+
+                val expectedFileBasedDepPath = projectPath.toRealPath().absolutePathString() + "/sp2/foo-npm-dep"
+                assertEquals(
+                    """
+                        |$buildTaskPath doNotCacheIf=true, because task depends on file-based NPM dependencies: [${expectedFileBasedDepPath}]
+                        """.trimMargin(),
+                    lines,
+                )
+            }
+        }
+    }
+
+    companion object {
+        private val json = Json {
+            prettyPrint = true
+        }
+    }
+}
+
+
+/**
+ * Create a test project for [KotlinWebpackUpToDateIT].
+ *
+ * Two subprojects:
+ * - `sp1` depends on `cowsay` with a configurable version.
+ * - `sp2` depends on a file-based npm dependency.
+ */
+private fun KGPBaseTest.createTestProject(
+    gradleVersion: GradleVersion,
+    useYarn: Boolean,
+    useCustomKgpToolingDir: Boolean = false,
+    runTest: TestProject.() -> Unit,
+) {
+
+    project("empty", gradleVersion) {
+
+        //region configure root project
+        plugins {
+            kotlin("multiplatform").apply(false)
+        }
+
+        if (useCustomKgpToolingDir) {
+            setupCustomKgpNpmToolingDependenciesDir(
+                toolingCustomDir = projectPath.resolve("kgp-npm-tooling"),
+                useYarn = useYarn,
+            )
+        }
+
+        buildScriptInjection {
+            project.plugins.withType<CommonNodeJsRootPlugin>().configureEach { _ ->
+                project.extensions.configure<BaseNpmExtension> {
+                    packageLockMismatchReport.set(LockFileMismatchReport.NONE)
+                    packageLockAutoReplace.set(true)
+                }
+            }
+            project.plugins.withType<CommonYarnPlugin>().configureEach { _ ->
+                project.extensions.configure<BaseYarnRootExtension> {
+                    yarnLockMismatchReport = YarnLockMismatchReport.NONE
+                    yarnLockAutoReplace = true
+                }
+            }
+
+            project.tasks.register("upgradeAllLockFiles") { t ->
+                t.description = "helper task to run all lock file upgrade tasks"
+                if (useYarn) {
+                    t.dependsOn(project.tasks.matching { "UpgradeYarnLock" in it.name })
+                } else {
+                    t.dependsOn(project.tasks.matching { "UpgradePackageLock" in it.name })
+                }
+            }
+        }
+        //endregion
+
+        gradleProperties.appendText("\nkotlin.js.yarn=$useYarn")
+
+        subProject("sp1").apply {
+            include(this@project, this.projectName, copyMode = ProjectIncludeCopyMode.DoNotCopy)
+
+            projectPath.resolve("src/commonMain/kotlin/main.kt")
+                .apply { parent.createDirectories() }
+                .createFile()
+                .writeText(
+                    """
+                    |package com.example
+                    |
+                    |fun main() {
+                    |  println("Hello, world!")
+                    |}
+                    |""".trimMargin()
+                )
+            buildScriptInjection {
+                project.plugins.apply("org.jetbrains.kotlin.multiplatform")
+
+                kotlinMultiplatform.apply {
+                    js {
+                        browser()
+                        binaries.executable()
+                    }
+                    wasmJs {
+                        browser()
+                        binaries.executable()
+                    }
+
+                    sourceSets.commonMain.dependencies {
+                        val cowsayVersion = project.providers.gradleProperty("cowsayVersion").getOrElse("1.4.0")
+                        project.logger.lifecycle("using cowsayVersion: $cowsayVersion")
+                        implementation(npm("cowsay", cowsayVersion))
+                    }
+                }
+            }
+        }
+
+        subProject("sp2").apply {
+            include(this@project, this.projectName, copyMode = ProjectIncludeCopyMode.DoNotCopy)
+
+            val fooNpmDepName = "foo-npm-dep"
+
+            projectPath.resolve(fooNpmDepName).apply {
+                createDirectories()
+                resolve("package.json").writeText(
+                    """
+                    |{
+                    |  "name": "$fooNpmDepName",
+                    |  "version": "1.0.0",
+                    |  "main": "index.js",
+                    |  "private": true
+                    |}
+                    |""".trimMargin()
+                )
+                resolve("index.js").writeText(
+                    """
+                    |console.log('Hello from $fooNpmDepName');
+                    |""".trimMargin()
+                )
+            }
+
+            projectPath.resolve("src/commonMain/kotlin/main.kt")
+                .apply { parent.createDirectories() }
+                .createFile()
+                .writeText(
+                    """
+                    |package com.example
+                    |
+                    |fun main() {
+                    |  println("Hello, world!")
+                    |}
+                    |""".trimMargin()
+                )
+
+            buildScriptInjection {
+                project.plugins.apply("org.jetbrains.kotlin.multiplatform")
+
+                kotlinMultiplatform.apply {
+                    js {
+                        browser()
+                        binaries.executable()
+                    }
+                    wasmJs {
+                        browser()
+                        binaries.executable()
+                    }
+
+                    sourceSets.commonMain.dependencies {
+                        implementation(npm(fooNpmDepName, project.projectDir.resolve(fooNpmDepName)))
+                    }
+                }
+
+                @Suppress("INVISIBLE_REFERENCE")
+                project.tasks.withType<RequiresNpmDependenciesTask>().configureEach { task ->
+                    task.doFirst { _ ->
+                        task.logger.lifecycle(
+                            "${task.path} npmDependenciesLockFiles:\n" +
+                                    task.npmDependenciesLockFiles.files.joinToString("\n") { f -> f.invariantSeparatorsPath }
+                        )
+                    }
+                }
+            }
+        }
+
+        runTest()
+    }
+}
