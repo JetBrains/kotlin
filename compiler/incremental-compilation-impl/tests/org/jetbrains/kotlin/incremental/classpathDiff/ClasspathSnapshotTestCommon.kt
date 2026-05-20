b0775e00eeabc64d0ec85c4510a34e70892adbd7
cristiangarcia <Cristian.Garcia-Marin@jetbrains.com>
2026-05-20 14:34:35 +0200

    [Build] remove hardcoded path at incremental-compilation-impl

--- compiler/incremental-compilation-impl/tests/org/jetbrains/kotlin/incremental/classpathDiff/ClasspathSnapshotTestCommon.kt
+++ compiler/incremental-compilation-impl/tests/org/jetbrains/kotlin/incremental/classpathDiff/ClasspathSnapshotTestCommon.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.incremental.classpathDiff
 import com.google.gson.GsonBuilder
 import org.jetbrains.kotlin.buildtools.api.jvm.ClassSnapshotGranularity
 import org.jetbrains.kotlin.cli.common.isWindows
+import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.incremental.classpathDiff.ClasspathSnapshotTestCommon.ClassFileUtil.snapshot
 import org.jetbrains.kotlin.incremental.classpathDiff.ClasspathSnapshotTestCommon.CompileUtil.compile
 import org.jetbrains.kotlin.incremental.classpathDiff.ClasspathSnapshotTestCommon.CompileUtil.compileAll
@@ -133,10 +134,11 @@ abstract class ClasspathSnapshotTestCommon {
             //     org.jetbrains.kotlin.test.MockLibraryUtil.compileKotlin(
             //         srcDir.path, classesDir, extraClasspath = classpath.map { it.path }.toTypedArray())
             // However, it currently fails with UnsupportedClassVersionError, so we have to launch a new kotlinc process instead.
-            val kotlincBinary = if (isWindows) "dist/kotlinc/bin/kotlinc.bat" else "dist/kotlinc/bin/kotlinc"
-            check(File(kotlincBinary).exists()) { "'${File(kotlincBinary).absolutePath}' not found. Run ./gradlew dist first." }
+            val kotlincDir = ForTestCompileRuntime.distKotlincForTests()
+            val kotlincBinary = if (isWindows) kotlincDir.resolve("bin/kotlinc.bat") else kotlincDir.resolve("bin/kotlinc")
+            check(kotlincBinary.exists()) { "'${kotlincBinary.absolutePath}' not found. Run ./gradlew dist first." }
             val commandAndArgs = listOf(
-                kotlincBinary,
+                kotlincBinary.path,
                 srcDir.path,
                 "-d", classesDir.path,
                 "-classpath", (listOf(srcDir) + classpath).joinToString(File.pathSeparator) { it.path }
