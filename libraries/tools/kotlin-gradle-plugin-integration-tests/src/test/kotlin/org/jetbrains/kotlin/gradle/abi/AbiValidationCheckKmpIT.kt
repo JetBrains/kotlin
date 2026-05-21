13d25e2c581f55cecbe80bf9e9359af14fa00ef9
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-05-21 14:15:26 +0200

    KT-86268: Regression test added
    This is not a cherry-pick of the e1d154c4e96bf8ddf26eaafaeedf8a1cf11ff947,
    but rather a back-ported version of the test.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/abi/AbiValidationCheckKmpIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/abi/AbiValidationCheckKmpIT.kt
@@ -8,10 +8,13 @@
 package org.jetbrains.kotlin.gradle.abi
 
 import org.gradle.util.GradleVersion
+import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.abi.utils.abiValidation
 import org.jetbrains.kotlin.gradle.abi.utils.kmpProject
 import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 import org.jetbrains.kotlin.gradle.testbase.*
+import org.junit.jupiter.api.DisplayName
 
 @MppGradlePluginTests
 class AbiValidationCheckKmpIT : KGPBaseTest() {
@@ -27,6 +30,26 @@ class AbiValidationCheckKmpIT : KGPBaseTest() {
             abiValidation()
 
             // create the reference dumps to check
+            build("updateKotlinAbi")
+
+            build("check") {
+                assertTasksExecuted(":checkKotlinAbi")
+            }
+        }
+    }
+
+    @OptIn(ExperimentalBuildToolsApi::class, ExperimentalKotlinGradlePluginApi::class)
+    @GradleTest
+    @DisplayName("Test abiValidation compatibility with compilerVersion prior 2.4.0")
+    fun testCompilerVersionCompatibility(gradleVersion: GradleVersion) {
+        kmpProject(gradleVersion) {
+            buildScriptInjection {
+                kotlinMultiplatform.jvm()
+                kotlinMultiplatform.compilerVersion.set("2.2.0")
+            }
+
+            abiValidation()
+
             build("updateKotlinAbi")
 
             build("check") {
