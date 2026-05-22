c0ccbc23c5da80a49d5a1d34f27400ab99f7c7de
Jose Enrique Estremadoyro Fort <joseefort@google.com>
2026-05-21 22:54:52 -0600

    fixup! [JKLIB][TEST] enable IR Deserialization and linking phase phase in tests
    Remove JRE stubs and add full JDK by default

--- compiler/jklib.tests/testFixtures/org/jetbrains/kotlin/jklib/test/irText/JKlibJavaSourceConfigurator.kt
+++ compiler/jklib.tests/testFixtures/org/jetbrains/kotlin/jklib/test/irText/JKlibJavaSourceConfigurator.kt
@@ -6,21 +6,16 @@
 package org.jetbrains.kotlin.jklib.test.irText
 
 import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoot
-import org.jetbrains.kotlin.cli.jvm.config.configureJdkClasspathRoots
 import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.test.MockLibraryUtil
-import org.jetbrains.kotlin.test.TestJdkKind
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
 import org.jetbrains.kotlin.test.directives.model.DirectivesContainer
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.EnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.TestServices
 import org.jetbrains.kotlin.test.services.assertions
-import org.jetbrains.kotlin.test.services.configuration.JvmEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.sourceFileProvider
-import org.jetbrains.kotlin.test.util.KtTestUtil
 import java.io.File
 
 class JKlibJavaSourceConfigurator(testServices: TestServices) : EnvironmentConfigurator(testServices) {
@@ -28,28 +23,6 @@ class JKlibJavaSourceConfigurator(testServices: TestServices) : EnvironmentConfi
         get() = listOf(JvmEnvironmentConfigurationDirectives)
 
     override fun configureCompilerConfiguration(configuration: CompilerConfiguration, module: TestModule) {
-        val registeredDirectives = module.directives
-        val jdkKind = JvmEnvironmentConfigurator.extractJdkKind(registeredDirectives)
-        JvmEnvironmentConfigurator.getJdkHome(jdkKind)?.let { configuration.put(JVMConfigurationKeys.JDK_HOME, it) }
-
-        when (jdkKind) {
-            TestJdkKind.MOCK_JDK, TestJdkKind.MODIFIED_MOCK_JDK -> {
-                configuration.put(JVMConfigurationKeys.NO_JDK, true)
-                configuration.addJvmClasspathRoot(KtTestUtil.findMockJdkRtJar())
-            }
-            else -> {
-                JvmEnvironmentConfigurator.getJdkClasspathRoot(jdkKind)?.let { configuration.addJvmClasspathRoot(it) }
-            }
-        }
-
-        val withReflect = JvmEnvironmentConfigurationDirectives.WITH_REFLECT in module.directives
-
-        if (withReflect) {
-            testServices.assertions.fail { "WITH_REFLECT is not supported in JKlib tests" }
-        }
-
-        configuration.configureJdkClasspathRoots()
-
         val javaFiles = module.files.filter { it.name.endsWith(".java") }
         if (javaFiles.isEmpty()) return
 
