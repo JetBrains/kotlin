70bae61edce972f16e3cbc467c3b5b46eebaefd3
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-05-25 19:16:04 +0200

    fixup! Java Direct: run java-related tests from compiler testdata
    test infra

---
+++ compiler/java-direct/testFixtures/org/jetbrains/kotlin/java/direct/JavaDirectFacadeBuilderProvider.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.java.direct
+
+import org.jetbrains.kotlin.cli.jvm.compiler.VfsBasedProjectEnvironment
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.fir.FirModuleData
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.java.FirJavaFacade
+import org.jetbrains.kotlin.fir.session.environment.AbstractProjectEnvironment
+import org.jetbrains.kotlin.fir.session.environment.AbstractProjectFileSearchScope
+import org.jetbrains.kotlin.test.frontend.fir.JavaFacadeBuilderProvider
+import org.jetbrains.kotlin.test.services.TestServices
+
+/**
+ * Test-fixture wiring that mirrors the production CLI hook in
+ * `JvmFrontendPipelinePhase.preprocessSessions`: returns a `javaFacadeBuilder` lambda backed by
+ * `createJavaDirectSourceJavaFacadeBuilder`, so the `JavaUsingAst*TestGenerated` suites resolve
+ * Java sources through `JavaClassFinderOverAstImpl` rather than PSI.
+ */
+class JavaDirectFacadeBuilderProvider(@Suppress("UNUSED_PARAMETER") testServices: TestServices) : JavaFacadeBuilderProvider() {
+    override fun createBuilder(
+        configuration: CompilerConfiguration,
+        projectEnvironment: VfsBasedProjectEnvironment,
+    ): (AbstractProjectEnvironment, FirSession, FirModuleData, AbstractProjectFileSearchScope) -> FirJavaFacade =
+        createJavaDirectSourceJavaFacadeBuilder(configuration, projectEnvironment)
+}
