54c342d441cd83ccbf2ecab1fc19c41178284dba
Alevtina.Gamzikova <alevtina.gamzikova@jetbrains.com>
2026-05-26 15:59:36 +0000

    [Tests] Add more test data for KT-84090
    Merge-request: KT-MR-27288
    Merged-by: Alevtina Gamzikova <Alevtina.Gamzikova@jetbrains.com>

--- js/js.translator/testData/typescript-export/js/variance-annotations/variance-annotations.aa.d.ts
+++ js/js.translator/testData/typescript-export/js/variance-annotations/variance-annotations.aa.d.ts
@@ -45,5 +45,75 @@ declare namespace JS_TESTS {
                 const constructor: abstract new <T>() => UnsafeCovariant<T>;
             }
         }
+        class Base {
+            constructor(name: string);
+            get name(): string;
+        }
+        namespace Base {
+            /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+            namespace $metadata$ {
+                const constructor: abstract new () => Base;
+            }
+        }
+        class Producer<out T extends foo.Base> {
+            constructor(value: T);
+            get value(): T;
+        }
+        namespace Producer {
+            /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+            namespace $metadata$ {
+                const constructor: abstract new <T extends foo.Base>() => Producer<T>;
+            }
+        }
+        interface Consumer<in T extends foo.Base> {
+            consume(value: T): void;
+            readonly __doNotUseOrImplementIt: {
+                readonly "foo.Consumer": unique symbol;
+            };
+        }
+        class InvariantBound<T extends foo.Base> {
+            constructor(value: T);
+            get value(): T;
+            set value(value: T);
+        }
+        namespace InvariantBound {
+            /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+            namespace $metadata$ {
+                const constructor: abstract new <T extends foo.Base>() => InvariantBound<T>;
+            }
+        }
+        class Token {
+            constructor(text: string);
+            get text(): string;
+        }
+        namespace Token {
+            /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+            namespace $metadata$ {
+                const constructor: abstract new () => Token;
+            }
+        }
+        class TokenBox<out T extends foo.Token> {
+            constructor(value: T);
+            get value(): T;
+            get Entry(): {
+                new(): foo.TokenBox.Entry<T>;
+            };
+        }
+        namespace TokenBox {
+            /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+            namespace $metadata$ {
+                const constructor: abstract new <T extends foo.Token>() => TokenBox<T>;
+            }
+            class Entry<out T$TokenBox extends foo.Token> {
+                private constructor();
+                getValue(): T$TokenBox;
+            }
+            namespace Entry {
+                /** @deprecated $metadata$ is used for internal purposes, please don't use it in your code, because it can be removed at any moment */
+                namespace $metadata$ {
+                    const constructor: abstract new <T$TokenBox extends foo.Token>() => Entry<T$TokenBox>;
+                }
+            }
+        }
     }
 }
