5c52c93cf19f3b042a690c5643eb5e7905c194f2
Francesco Protopapa <fprotopapa@google.com>
2026-05-27 15:26:39 +0200

    [Parcelize] Add reproducer test for 413411326

---
+++ plugins/parcelize/parcelize-compiler/testData/box/listSimplePersistentNullable.kt
@@ -0,0 +1,31 @@
+// WITH_STDLIB
+
+@file:JvmName("TestKt")
+
+package test
+
+import kotlinx.parcelize.*
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.collections.immutable.*
+
+@Parcelize
+data class Test(val a: PersistentList<String>?) : Parcelable
+
+fun box() = parcelTest { parcel ->
+    val first = Test(null)
+    val second = Test(persistentListOf("A", "B", "C"))
+
+    first.writeToParcel(parcel, 0)
+    second.writeToParcel(parcel, 0)
+
+    val bytes = parcel.marshall()
+    parcel.unmarshall(bytes, 0, bytes.size)
+    parcel.setDataPosition(0)
+
+    val first2 = parcelableCreator<Test>().createFromParcel(parcel)
+    val second2 = parcelableCreator<Test>().createFromParcel(parcel)
+
+    assert(first == first2)
+    assert(second == second2)
+}
