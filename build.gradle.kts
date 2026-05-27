3d194200f82d44300451b5c85d9cc2d0e434121b
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-05-27 15:12:52 +0200

    [build] Re-enable commonizer-api tests
    ^KTI-2519 fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -656,7 +656,7 @@ tasks {
     testLifecycleTask("toolsTest") {
         dependsOn(":tools:kotlinp-jvm:test")
         dependsOn(":native:kotlin-klib-commonizer:test")
-//        dependsOn(":native:kotlin-klib-commonizer-api:test")
+        dependsOn(":native:kotlin-klib-commonizer-api:test")
         dependsOn(":kotlin-tooling-core:check")
         dependsOn(":kotlin-tooling-metadata:check")
         dependsOn(":compiler:build-tools:kotlin-build-tools-api:check")
