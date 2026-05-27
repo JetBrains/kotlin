1818afd81a2e841e1f42b3ac7efe8e85076dbf29
vladislavgrecko <vladislav.grechko@jetbrains.com>
2026-05-27 15:51:59 +0200

    [Reflection] Fix rendering of extension function types
    ^KT-83677: Fixed

--- compiler/testData/codegen/boxJvm/reflection/types/typeAliasedType.kt
+++ compiler/testData/codegen/boxJvm/reflection/types/typeAliasedType.kt
@@ -9,15 +9,18 @@ import kotlin.test.assertEquals
 typealias T1 = String
 typealias T2<X> = List<X>
 typealias T3<X, Y> = MutableMap<in Y, X?>
+typealias T4 = String.() -> Int
 
 fun f1(): T1 = null!!
 fun f2(): T2<Any> = null!!
 fun f3(): T3<Int, T1> = null!!
+fun f4(): T4 = null!!
 
 fun box(): String {
     assertEquals("test.T1 /* = kotlin.String */", ::f1.returnType.toString())
     assertEquals("test.T2<kotlin.Any> /* = kotlin.collections.List<kotlin.Any> */", ::f2.returnType.toString())
     assertEquals("test.T3<kotlin.Int, test.T1 /* = kotlin.String */> /* = kotlin.collections.MutableMap<in test.T1 /* = kotlin.String */, kotlin.Int?> */", ::f3.returnType.toString())
+    assertEquals("test.T4 /* = kotlin.String.() -> kotlin.Int */", ::f4.returnType.toString())
 
     return "OK"
 }
