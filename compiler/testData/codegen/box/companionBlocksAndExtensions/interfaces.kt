f8b528f59c525aa91a3e2a6866dc13953e52bbed
Andrii Rublov <andrii.rublov@jetbrains.com>
2026-05-29 22:49:34 +0200

    [Tests]: Introduce common tests for companion blocks
    Including tests for companion properties initializers

---
+++ compiler/testData/codegen/box/companionBlocksAndExtensions/interfaces.kt
@@ -0,0 +1,28 @@
+// LANGUAGE: +CompanionBlocksAndExtensions
+// IGNORE_BACKEND: WASM, WASM_JS, WASM_WASI, NATIVE
+
+interface MyInterface {
+    companion {
+        fun interfaceFun() = "InterfaceFun"
+    }
+}
+
+companion fun MyInterface.extFun() = "ExtFun"
+
+class Impl : MyInterface {
+    fun useInterfaceCompanion() = MyInterface.interfaceFun()
+}
+
+fun box(): String {
+    // Access companion block members on interface
+    if (MyInterface.interfaceFun() != "InterfaceFun") return "FAIL: interfaceFun=${MyInterface.interfaceFun()}"
+
+    // Access companion extensions on interface
+    if (MyInterface.extFun() != "ExtFun") return "FAIL: extFun=${MyInterface.extFun()}"
+
+    // Access from implementing class instance
+    val impl = Impl()
+    if (impl.useInterfaceCompanion() != "InterfaceFun") return "FAIL: useInterfaceCompanion=${impl.useInterfaceCompanion()}"
+
+    return "OK"
+}
---
+++ compiler/testData/codegen/box/companionBlocksAndExtensions/nestedClasses.kt
@@ -0,0 +1,50 @@
+// LANGUAGE: +CompanionBlocksAndExtensions
+// IGNORE_BACKEND: WASM, WASM_JS, WASM_WASI, JS_IR, JS_IR_ES6
+
+var initOrder = ""
+
+class Outer {
+    companion {
+        val outerVal: String = run {
+            initOrder += "O"
+            "OuterVal"
+        }
+
+        fun outerFun() = "OuterFun"
+    }
+
+    class Nested {
+        companion {
+            val nestedStaticVal: String = run {
+                initOrder += "N"
+                "NestedVal"
+            }
+
+            fun nestedStaticFun() = "NestedFun"
+        }
+
+        fun readNestedVal() = nestedStaticVal
+    }
+}
+
+fun box(): String {
+    // Access nested class companion independently of outer
+    if (Outer.Nested.nestedStaticVal != "NestedVal") return "FAIL: nestedStaticVal=${Outer.Nested.nestedStaticVal}"
+    if (Outer.Nested.nestedStaticFun() != "NestedFun") return "FAIL: nestedStaticFun=${Outer.Nested.nestedStaticFun()}"
+
+    // Nested init should not trigger outer init
+    if (initOrder != "N") return "FAIL: expected only nested init, got initOrder=$initOrder"
+
+    // Now access outer companion
+    if (Outer.outerVal != "OuterVal") return "FAIL: outerVal=${Outer.outerVal}"
+    if (Outer.outerFun() != "OuterFun") return "FAIL: outerFun=${Outer.outerFun()}"
+
+    // Now both should be initialized
+    if (initOrder != "NO") return "FAIL: initOrder=$initOrder"
+
+    // Instance access
+    val nested = Outer.Nested()
+    if (nested.readNestedVal() != "NestedVal") return "FAIL: nested.readNestedVal=${nested.readNestedVal()}"
+
+    return "OK"
+}
