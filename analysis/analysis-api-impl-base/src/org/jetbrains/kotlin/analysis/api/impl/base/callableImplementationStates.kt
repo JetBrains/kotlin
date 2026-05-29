239f0060017f8b9e3581c1ac3a1bc0f60358bb4f
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2026-05-29 20:56:37 +0900

    fixup! [Analysis API] Introduce 'implementationState'
    Make 'KaCallableImplementationState' implementation classes 'data'.

--- analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/callableImplementationStates.kt
+++ analysis/analysis-api-impl-base/src/org/jetbrains/kotlin/analysis/api/impl/base/callableImplementationStates.kt
@@ -10,41 +10,21 @@ package org.jetbrains.kotlin.analysis.api.impl.base
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
 import org.jetbrains.kotlin.analysis.api.components.KaCallableImplementationState
-import java.util.Objects
 
 @KaImplementationDetail
-class KaCallableExplicitImplementationStateImpl(
+data class KaCallableExplicitImplementationStateImpl(
     override val isComplete: Boolean,
 ) : KaCallableImplementationState.Explicit {
-    override fun equals(other: Any?): Boolean {
-        return this === other || (other is KaCallableImplementationState.Explicit && other.isComplete == isComplete)
-    }
-
-    override fun hashCode(): Int {
-        return isComplete.hashCode()
-    }
-
     override fun toString(): String {
         return "Explicit(isComplete=$isComplete)"
     }
 }
 
 @KaImplementationDetail
-class KaCallableInheritedImplementationStateImpl(
+data class KaCallableInheritedImplementationStateImpl(
     override val isAmbiguous: Boolean,
     override val isOverridable: Boolean,
 ) : KaCallableImplementationState.Inherited {
-    override fun equals(other: Any?): Boolean {
-        return this === other ||
-                (other is KaCallableImplementationState.Inherited &&
-                        other.isAmbiguous == isAmbiguous &&
-                        other.isOverridable == isOverridable)
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(isAmbiguous, isOverridable)
-    }
-
     override fun toString(): String {
         return "Inherited(isAmbiguous=$isAmbiguous, isOverridable=$isOverridable)"
     }
