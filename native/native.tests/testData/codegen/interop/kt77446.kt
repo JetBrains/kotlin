dc03ac9de5798bb536939c4dc142145731b63fbd
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-06-01 15:20:50 +0300

    [Tests][Native] Add compatibility ignore directives to test kt77446.kt
    ^KT-77446

--- native/native.tests/testData/codegen/interop/kt77446.kt
+++ native/native.tests/testData/codegen/interop/kt77446.kt
@@ -1,5 +1,8 @@
 // TARGET_BACKEND: NATIVE
 // DISABLE_NATIVE: isAppleTarget=false
+// IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_FIRST_STAGE: Native:1.9,2.0,2.1,2.2,2.3,2.4
+// IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_SECOND_STAGE: Native:1.9,2.0,2.1,2.2,2.3,2.4
+// ^^^KT-77446 is fixed in 2.4.20-Beta1
 
 // KT-77446: a Kotlin class subclassing an Objective-C class whose -init is declared unavailable
 // must not crash when constructed via another initializer that internally dispatches through
