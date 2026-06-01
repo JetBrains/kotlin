ewok-pulse/saber-e2e-kotlin

[IR] Refactor configuration of IR checkers
Remove compiler config keys and arguments which enable/disable specific
IR checkers:
ENABLE_IR_VISIBILITY_CHECKS
ENABLE_IR_VARARG_TYPES_CHECKS
ENABLE_IR_NESTED_OFFSETS_CHECKS

Replace them with two generic arguments that allow to disable or enable
additional IR checkers by a name or tag.
This makes it possible for users to control the set of IR checkers,
which may be handy for compiler plugin authors, and makes it much easier
for the dev team to control them in tests.

There is a side effect: previously ENABLE_IR_VISIBILITY_CHECKS and
ENABLE_IR_VARARG_TYPES_CHECKS were enabled in most tests, but not all.
(This was probably an oversight, steming from the variety of test
facades.) Now, all checkers are on in every test which enables
-Xverify-ir. This results in some failures about visibility violations.
The next commits will disable the visibility checker in those cases.

KT-80070

compiler/arguments/src/org/jetbrains/kotlin/arguments/description/removed/removedCommonCompilerArguments.kt
@@ -25,4 +25,28 @@ val removedCommonCompilerArguments by compilerArgumentsLevel(CompilerArgumentsLe
             removedVersion = KotlinReleaseVersion.v2_2_0,
         )
     }
+
+    compilerArgument {
+        name = "Xverify-ir-visibility"
+        description =
+            "Check for visibility violations in IR when validating it before running any lowerings. Only has effect if '-Xverify-ir' is not 'none'.".asReleaseDependent()
+        valueType = BooleanType.defaultFalse
+
+        lifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_0_20,
+            removedVersion = KotlinReleaseVersion.v2_4_0,
+        )
+    }
+
+    compilerArgument {
+        name = "Xverify-ir-nested-offsets"
+        description =
+            "Check that offsets of nested IR elements conform to offsets of their containers. Only has effect if '-Xverify-ir' is not 'none'.".asReleaseDependent()
+        valueType = BooleanType.defaultFalse
+
+        lifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_3_20,
+            removedVersion = KotlinReleaseVersion.v2_4_0,
+        )
+    }
 }

compiler/cli/src/org/jetbrains/kotlin/cli/common/arguments.kt
@@ -56,25 +56,20 @@ fun CompilerConfiguration.setupCommonArguments(
     } ?: IrVerificationMode.NONE
     put(CommonConfigurationKeys.VERIFY_IR, irVerificationMode)
 
-    if (arguments.verifyIrVisibility) {
-        put(CommonConfigurationKeys.ENABLE_IR_VISIBILITY_CHECKS, true)
-        if (irVerificationMode == IrVerificationMode.NONE) {
-            this.report(
-                COMPILER_ARGUMENTS_WARNING,
-                "'-Xverify-ir-visibility' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
-            )
-        }
+    if (arguments.disableIrCheckers.isNotEmpty() && irVerificationMode == IrVerificationMode.NONE) {
+        this.report(
+            COMPILER_ARGUMENTS_WARNING,
+            "'-Xdisable-ir-checkers' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
+        )
     }
-
-    if (arguments.verifyIrNestedOffsets) {
-        put(CommonConfigurationKeys.ENABLE_IR_NESTED_OFFSETS_CHECKS, true)
-        if (irVerificationMode == IrVerificationMode.NONE) {
-            this.report(
-                COMPILER_ARGUMENTS_WARNING,
-                "'-Xverify-ir-nested-offsets' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
-            )
-        }
+    put(CommonConfigurationKeys.DISABLE_IR_CHECKERS, arguments.disableIrCheckers.toList())
+    if (arguments.additionalIrCheckers.isNotEmpty() && irVerificationMode == IrVerificationMode.NONE) {
+        this.report(
+            COMPILER_ARGUMENTS_WARNING,
+            "'-Xadditional-ir-checkers' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
+        )
     }
+    put(CommonConfigurationKeys.ADDITIONAL_IR_CHECKERS, arguments.additionalIrCheckers.toList())
 
     @Suppress("DEPRECATION")
     if (arguments.useFirExperimentalCheckers) {

JetBrains/kotlin

compiler/arguments/src/org/jetbrains/kotlin/arguments/description/removed/removedCommonCompilerArguments.kt
@@ -27,4 +27,28 @@ val removedCommonCompilerArguments by compilerArgumentsLevel(CompilerArgumentsLe
             removedVersion = KotlinReleaseVersion.v2_2_0,
         )
     }
+
+    compilerArgument {
+        name = "Xverify-ir-visibility"
+        description =
+            "Check for visibility violations in IR when validating it before running any lowerings. Only has effect if '-Xverify-ir' is not 'none'.".asReleaseDependent()
+        valueType = BooleanType.defaultFalse
+
+        lifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_0_20,
+            removedVersion = KotlinReleaseVersion.v2_4_20,
+        )
+    }
+
+    compilerArgument {
+        name = "Xverify-ir-nested-offsets"
+        description =
+            "Check that offsets of nested IR elements conform to offsets of their containers. Only has effect if '-Xverify-ir' is not 'none'.".asReleaseDependent()
+        valueType = BooleanType.defaultFalse
+
+        lifecycle(
+            introducedVersion = KotlinReleaseVersion.v2_3_20,
+            removedVersion = KotlinReleaseVersion.v2_4_20,
+        )
+    }
 }

compiler/cli/src/org/jetbrains/kotlin/cli/common/arguments.kt
@@ -55,25 +55,20 @@ fun CompilerConfiguration.setupCommonArguments(
     } ?: IrVerificationMode.NONE
     put(CommonConfigurationKeys.VERIFY_IR, irVerificationMode)
 
-    if (arguments.verifyIrVisibility) {
-        put(CommonConfigurationKeys.ENABLE_IR_VISIBILITY_CHECKS, true)
-        if (irVerificationMode == IrVerificationMode.NONE) {
-            this.report(
-                COMPILER_ARGUMENTS_WARNING,
-                "'-Xverify-ir-visibility' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
-            )
-        }
+    if (arguments.disableIrCheckers.isNotEmpty() && irVerificationMode == IrVerificationMode.NONE) {
+        this.report(
+            COMPILER_ARGUMENTS_WARNING,
+            "'-Xdisable-ir-checkers' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
+        )
     }
-
-    if (arguments.verifyIrNestedOffsets) {
-        put(CommonConfigurationKeys.ENABLE_IR_NESTED_OFFSETS_CHECKS, true)
-        if (irVerificationMode == IrVerificationMode.NONE) {
-            this.report(
-                COMPILER_ARGUMENTS_WARNING,
-                "'-Xverify-ir-nested-offsets' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
-            )
-        }
+    put(CommonConfigurationKeys.DISABLE_IR_CHECKERS, arguments.disableIrCheckers.toList())
+    if (arguments.enableAdditionalIrCheckers.isNotEmpty() && irVerificationMode == IrVerificationMode.NONE) {
+        this.report(
+            COMPILER_ARGUMENTS_WARNING,
+            "'-Xadditional-ir-checkers' has no effect unless '-Xverify-ir=warning' or '-Xverify-ir=error' is specified"
+        )
     }
+    put(CommonConfigurationKeys.ADDITIONAL_IR_CHECKERS, arguments.enableAdditionalIrCheckers.toList())
 
     @Suppress("DEPRECATION")
     if (arguments.useFirExperimentalCheckers) {