51b96201993fcb82e4a60f8d089734324473b68f
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2026-06-02 12:59:00 +0200

    [FIR, IR] Rename getUnsubstitutedUnderlyingType too getUnsubstitutedUnderlyingTypeInJvm and unsubstitutedUnderlyingTypeForInlineClass to unsubstitutedUnderlyingTypeForInlineClassInJvm
    #KT-84904

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/declarations/ValueClassesUtils.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/declarations/ValueClassesUtils.kt
@@ -23,7 +23,7 @@ import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
 @OptIn(ValueClassBackendAgnosticApi::class)
-internal fun ConeKotlinType.unsubstitutedUnderlyingTypeForInlineClass(session: FirSession): ConeKotlinType? {
+internal fun ConeKotlinType.unsubstitutedUnderlyingTypeForInlineClassInJvm(session: FirSession): ConeKotlinType? {
     val symbol = this.fullyExpandedType(session).toRegularClassSymbol(session) ?: return null
     // All the usages are JVM-only, so treatFullValueClassesWithOneFieldAsBasic is effectively false
     return symbol.inlineClassRepresentation(treatFullValueClassesWithOneFieldAsBasic = false)?.underlyingType
