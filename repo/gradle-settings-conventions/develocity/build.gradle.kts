557e5b078d96d67836d2fd9e3861edc9b56280a1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-06-03 14:15:19 +0200

    Build: bump LV/AV we use in gradle-settings-convention to 2.1
    We have to get rid of language version 1.9, as it's unsupported formally
    and only partially supported inside Kotlin project actually.
    As Gradle plugin 8.11 is based on Kotlin 2.0.20, we may bump to 2.1
    as it's forward-compatible.

--- repo/gradle-settings-conventions/develocity/build.gradle.kts
+++ repo/gradle-settings-conventions/develocity/build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions {
-        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1)
+        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1)
     }
 }
 
