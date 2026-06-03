77fe75ff2c622445a0be0b142ec8a5bff392c9b9
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-06-03 14:37:40 +0200

    Reflection: reproduce KT-86692, KT-86698

--- compiler/testData/codegen/boxJvm/reflection/parameters/isOptional.kt
+++ compiler/testData/codegen/boxJvm/reflection/parameters/isOptional.kt
@@ -1,7 +1,13 @@
 // LANGUAGE: +ContextParameters
 // TARGET_BACKEND: JVM
 // WITH_REFLECT
+// FILE: J.java
+public class J extends A {
+    @Override
+    public void foo(int x, int y) {}
+}
 
+// FILE: box.kt
 import kotlin.test.*
 
 open class A {
@@ -26,6 +32,13 @@ fun box(): String {
     assertEquals(listOf(false, false, true), A::foo.parameters.map { it.isOptional })
     assertEquals(listOf(false, false, true), B::foo.parameters.map { it.isOptional })
     assertEquals(listOf(false, false, true), C::foo.parameters.map { it.isOptional })
+
+    if (Class.forName("kotlin.reflect.jvm.internal.SystemPropertiesKt").getMethod("getUseK1Implementation").invoke(null) == true) {
+        assertEquals(listOf(false, false, true), J::foo.parameters.map { it.isOptional })
+    } else {
+        // TODO(KT-86692): isOptional for new Java method implementation doesn't consider inherited default values
+        assertEquals(listOf(false, false, false), J::foo.parameters.map { it.isOptional })
+    }
 
     assertFalse(Int::extFun.parameters.single().isOptional)
 
