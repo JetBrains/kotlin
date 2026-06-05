4e3765d39d622cf52085e705c6941c019409fc53
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-06-05 09:56:54 +0200

    [FIR] Fix false negative ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS
    #KT-85972 Fixed

--- compiler/testData/diagnostics/tests/extraCheckers/ArrayEqualityCanBeReplacedWithEquals.kt
+++ compiler/testData/diagnostics/tests/extraCheckers/ArrayEqualityCanBeReplacedWithEquals.kt
@@ -30,19 +30,25 @@ fun testsFromIdea() {
 
 // Smart casts
 fun <T> eq1(a: T, b: T): Boolean =
-    if (a is IntArray && b is IntArray) a == b else false
+    if (a is IntArray && b is IntArray) a <!ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS!>==<!> b else false
 
 inline fun <reified T> eq2(a: T, b: T): Boolean =
-    if (a is IntArray && b is IntArray) a == b else false
+    if (a is IntArray && b is IntArray) a <!ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS!>==<!> b else false
 
 fun <T : Cloneable> eq3(a: T, b: T): Boolean =
-    if (a is IntArray && b is IntArray) a == b else false
+    if (a is IntArray && b is IntArray) a <!ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS!>==<!> b else false
 
 fun eq4(a: Any?, b: Any?): Boolean =
     if (a is IntArray && b is IntArray) a <!ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS!>==<!> b else false
 
 fun flexible(a: Array<String>, b: Array<String>) {
     a <!ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS!>==<!> J.makeFlexible(b)
+}
+
+class Box<T>(val item: T)
+
+fun captured(box: Box<out IntArray>, box2: Box<out IntArray>) {
+    box.item <!ARRAY_EQUALITY_OPERATOR_CAN_BE_REPLACED_WITH_CONTENT_EQUALS!>==<!> box2.item
 }
 
 /* GENERATED_FIR_TAGS: equalityExpression, functionDeclaration, ifExpression, integerLiteral, localProperty,
