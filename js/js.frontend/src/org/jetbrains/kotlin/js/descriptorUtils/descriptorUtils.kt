2180bad71332b4cba8dc967efad2ddf5d740f343
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-06-05 18:36:17 +0200

    [JS] Drop the remaining JS-specific K1 frontend checkers
    OSIP-190

--- js/js.frontend/src/org/jetbrains/kotlin/js/descriptorUtils/descriptorUtils.kt
+++ js/js.frontend/src/org/jetbrains/kotlin/js/descriptorUtils/descriptorUtils.kt
@@ -1,22 +1,14 @@
 /*
- * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.js.descriptorUtils
 
 import com.intellij.openapi.util.text.StringUtil
-import org.jetbrains.kotlin.builtins.KotlinBuiltIns
-import org.jetbrains.kotlin.descriptors.*
-import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
 import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.types.KotlinType
-
-val KotlinType.nameIfStandardType: Name?
-    get() = constructor.declarationDescriptor?.takeIf(KotlinBuiltIns::isBuiltIn)?.name
-
-@Deprecated(message = "Use getKotlinTypeFqName(Boolean) instead")
-fun KotlinType.getJetTypeFqName(printTypeArguments: Boolean): String = getKotlinTypeFqName(printTypeArguments)
 
 fun KotlinType.getKotlinTypeFqName(printTypeArguments: Boolean): String {
     val declaration = requireNotNull(constructor.declarationDescriptor) {
