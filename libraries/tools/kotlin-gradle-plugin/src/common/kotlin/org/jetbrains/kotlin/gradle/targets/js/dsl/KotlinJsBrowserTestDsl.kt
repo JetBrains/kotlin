19831726ea1b9e9b18b1994e4fa655c9637bb28e
Anton Lakotka <anton.lakotka@jetbrains.com>
2026-06-08 10:24:15 +0400

    [Gradle] Rename jsBrowser.test.bundleTask -> jsBrowser.test.defaultBundleTask
    ^KT-86260

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/dsl/KotlinJsBrowserTestDsl.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/dsl/KotlinJsBrowserTestDsl.kt
@@ -92,7 +92,7 @@ interface KotlinJsBrowserTestDsl {
      *
      * Use this task to post-process bundle output when needed.
      */
-    val bundleTask: TaskProvider<out BundleKotlinJsTestsTask>
+    val defaultBundleTask: TaskProvider<out BundleKotlinJsTestsTask>
 
     /**
      * Default location of bundled and ready to execute JS tests produced from Kotlin JS test compilation.
