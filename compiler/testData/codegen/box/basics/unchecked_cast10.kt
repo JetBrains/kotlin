bae90899e68a5591021c86db447b3c1a009bf82e
Miłosz Piekutowski <milosz.piekutowski@jetbrains.com>
2026-06-10 15:06:17 +0200

    [Tests] Mute tests that are not supposed to be run with LV<=2.4
    ^KT-86728
    ^KT-86822

--- compiler/testData/codegen/box/basics/unchecked_cast10.kt
+++ compiler/testData/codegen/box/basics/unchecked_cast10.kt
@@ -1,4 +1,6 @@
 // IGNORE_BACKEND: JS_IR, JS_IR_ES6
+// IGNORE_KLIB_RUNTIME_ERRORS_WITH_CUSTOM_SECOND_STAGE: Wasm-JS:2.3,2.4
+// KT-86822 is fixed no earlier than in 2.4.20-Beta1
 
 val any = Any()
 fun <T> foo(): T = any as T
