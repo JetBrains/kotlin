8f6fe520be7ed8182d529e115772f1eaeb4946ec
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-06-10 06:57:37 +0200

    Tests: drop 'exampleValue' check from SubpluginsIT test
    This commit is a follow-up ce92663a. As the corresponding component
    ExampleComponentRegistrar was dropped, this assertion has no more sense.
    
    Related to KT-85816

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/SubpluginsIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/SubpluginsIT.kt
@@ -26,13 +26,11 @@ class SubpluginsIT : KGPBaseTest() {
             build("compileKotlin", "build") {
                 assertTasksExecuted(":compileKotlin")
                 assertOutputContains("ExampleSubplugin loaded")
-                assertOutputContains("Project component registration: exampleValue")
             }
 
             build("compileKotlin", "build") {
                 assertTasksUpToDate(":compileKotlin")
                 assertOutputContains("ExampleSubplugin loaded")
-                assertOutputDoesNotContain("Project component registration: exampleValue")
             }
         }
     }
@@ -48,14 +46,12 @@ class SubpluginsIT : KGPBaseTest() {
             build("compileKotlin", "build") {
                 assertTasksExecuted(":compileKotlin")
                 assertOutputContains("ExampleSubplugin loaded")
-                assertOutputContains("Project component registration: exampleValue")
             }
 
             build("compileKotlin", "build") {
                 assertTasksUpToDate(":compileKotlin")
                 assertConfigurationCacheReused()
                 assertOutputDoesNotContain("ExampleSubplugin loaded")
-                assertOutputDoesNotContain("Project component registration: exampleValue")
             }
         }
     }
