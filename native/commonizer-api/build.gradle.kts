0a2dccb5d5682d05502def9e8d61d3710dbb9272
Radoslaw Panuszewski <radoslaw.panuszewski@jetbrains.com>
2026-06-10 22:33:24 +0200

    [Build] Switch `:native:commonizer-api` to `test-inputs-check-v2`
    KTI-3337

--- native/commonizer-api/build.gradle.kts
+++ native/commonizer-api/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("gradle-plugin-published-compiler-dependency-configuration")
     id("project-tests-convention")
     id("native-bootstrap-distribution-provisioner")
-    id("test-inputs-check")
+    id("test-inputs-check-v2")
 }
 
 kotlin {
@@ -40,9 +40,6 @@ projectTests {
         javaLauncher = getToolchainLauncherFor(JdkMajorVersion.JDK_21_0)
         jvmArgs("--add-opens", "java.base/java.lang=ALL-UNNAMED")
         testData(project.isolated, "testData")
-        testInputsCheck {
-            extraPermissions.add("""permission java.util.PropertyPermission "*", "read,write";""")
-        }
     }
 }
 
