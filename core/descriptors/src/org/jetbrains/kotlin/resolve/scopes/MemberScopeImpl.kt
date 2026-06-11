JetBrains/kotlin

commit bf97323301b5f4438fefc0b0deac7f02686c8dc0

Optimize data handling inside scopes

core/descriptors/src/org/jetbrains/kotlin/resolve/scopes/MemberScopeImpl.kt
@@ -16,9 +16,13 @@
 
 package org.jetbrains.kotlin.resolve.scopes
 
-import org.jetbrains.kotlin.descriptors.*
+import org.jetbrains.kotlin.descriptors.ClassifierDescriptor
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.descriptors.SimpleFunctionDescriptor
 import org.jetbrains.kotlin.incremental.components.LookupLocation
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.util.collectionUtils.filterIsInstanceMapTo
 import org.jetbrains.kotlin.utils.Printer
 import org.jetbrains.kotlin.utils.alwaysTrue
 
@@ -35,14 +39,14 @@ abstract class MemberScopeImpl : MemberScope {
                                            nameFilter: (Name) -> Boolean): Collection<DeclarationDescriptor> = emptyList()
 
     override fun getFunctionNames(): Set<Name> =
-            getContributedDescriptors(
-                    DescriptorKindFilter.FUNCTIONS, alwaysTrue()
-            ).filterIsInstance<SimpleFunctionDescriptor>().mapTo(mutableSetOf()) { it.name }
+        getContributedDescriptors(
+            DescriptorKindFilter.FUNCTIONS, alwaysTrue()
+        ).filterIsInstanceMapTo<SimpleFunctionDescriptor, Name, MutableSet<Name>>(mutableSetOf()) { it.name }
 
     override fun getVariableNames(): Set<Name> =
-            getContributedDescriptors(
-                    DescriptorKindFilter.VARIABLES, alwaysTrue()
-            ).filterIsInstance<VariableDescriptor>().mapTo(mutableSetOf()) { it.name }
+        getContributedDescriptors(
+            DescriptorKindFilter.VARIABLES, alwaysTrue()
+        ).filterIsInstanceMapTo<SimpleFunctionDescriptor, Name, MutableSet<Name>>(mutableSetOf()) { it.name }
 
     override fun getClassifierNames(): Set<Name>? = null
 

compiler/frontend.java/src/org/jetbrains/kotlin/synthetic/SamAdapterFunctionsScope.kt
@@ -44,6 +44,7 @@ import org.jetbrains.kotlin.storage.StorageManager
 import org.jetbrains.kotlin.types.*
 import org.jetbrains.kotlin.types.checker.findCorrespondingSupertype
 import org.jetbrains.kotlin.types.typeUtil.isNothing
+import org.jetbrains.kotlin.utils.addIfNotNull
 import kotlin.properties.Delegates
 
 interface SamAdapterExtensionFunctionDescriptor : FunctionDescriptor, FunctionInterfaceAdapterExtensionFunctionDescriptor {
@@ -244,14 +245,15 @@ class SamAdapterFunctionsScope(
         }
     }
 
-    private fun getAllSamConstructors(classifier: ClassifierDescriptor): List<FunctionDescriptor> {
-        return getSamAdaptersFromConstructors(classifier) + listOfNotNull(getSamConstructor(classifier))
-    }
+    private fun getAllSamConstructors(classifier: ClassifierDescriptor): List<FunctionDescriptor> =
+        getSamAdaptersFromConstructors(classifier).also {
+            it.addIfNotNull(getSamConstructor(classifier))
+        }
 
-    private fun getSamAdaptersFromConstructors(classifier: ClassifierDescriptor): List<FunctionDescriptor> {
-        if (!shouldGenerateAdditionalSamCandidate || classifier !is JavaClassDescriptor) return emptyList()
+    private fun getSamAdaptersFromConstructors(classifier: ClassifierDescriptor): MutableList<FunctionDescriptor> {
+        if (!shouldGenerateAdditionalSamCandidate || classifier !is JavaClassDescriptor) return SmartList()
 
-        return arrayListOf<FunctionDescriptor>().apply {
+        return SmartList<FunctionDescriptor>().apply {
             for (constructor in classifier.constructors) {
                 val samConstructor = getSyntheticConstructor(constructor) ?: continue
                 add(samConstructor)

compiler/frontend/src/org/jetbrains/kotlin/resolve/AllUnderImportScope.kt
@@ -24,6 +24,8 @@ import org.jetbrains.kotlin.resolve.scopes.BaseImportingScope
 import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
 import org.jetbrains.kotlin.resolve.scopes.MemberScope
 import org.jetbrains.kotlin.resolve.scopes.computeAllNames
+import org.jetbrains.kotlin.util.collectionUtils.flatMapScopes
+import org.jetbrains.kotlin.util.collectionUtils.listOfNonEmptyScopes
 import org.jetbrains.kotlin.utils.Printer
 import org.jetbrains.kotlin.utils.addToStdlib.flatMapToNullable
 
@@ -32,13 +34,13 @@ class AllUnderImportScope(
     excludedImportNames: Collection<FqName>
 ) : BaseImportingScope(null) {
 
-    private val scopes: List<MemberScope> = if (descriptor is ClassDescriptor) {
-        listOf(descriptor.staticScope, descriptor.unsubstitutedInnerClassesScope)
+    private val scopes: Array<MemberScope> = if (descriptor is ClassDescriptor) {
+        listOfNonEmptyScopes(descriptor.staticScope, descriptor.unsubstitutedInnerClassesScope).toTypedArray()
     } else {
         assert(descriptor is PackageViewDescriptor) {
             "Must be class or package view descriptor: $descriptor"
         }
-        listOf((descriptor as PackageViewDescriptor).memberScope)
+        listOfNonEmptyScopes((descriptor as PackageViewDescriptor).memberScope).toTypedArray()
     }
 
     private val excludedNames: Set<Name> = if (excludedImportNames.isEmpty()) { // optimization
@@ -49,7 +51,11 @@ class AllUnderImportScope(
         excludedImportNames.mapNotNull { if (it.parent() == fqName) it.shortName() else null }.toSet()
     }
 
-    override fun computeImportedNames(): Set<Name>? = scopes.flatMapToNullable(hashSetOf(), MemberScope::computeAllNames)
+    override fun computeImportedNames(): Set<Name>? = when (scopes.size) {
+        0 -> null
+        1 -> scopes[0].computeAllNames()
+        else -> scopes.asIterable().flatMapToNullable(hashSetOf(), MemberScope::computeAllNames)
+    }
 
     override fun getContributedDescriptors(
         kindFilter: DescriptorKindFilter,
@@ -64,23 +70,29 @@ class AllUnderImportScope(
 
         val noPackagesKindFilter = kindFilter.withoutKinds(DescriptorKindFilter.PACKAGES_MASK)
         return scopes
-            .flatMap { it.getContributedDescriptors(noPackagesKindFilter, nameFilterToUse) }
+            .flatMapScopes { it.getContributedDescriptors(noPackagesKindFilter, nameFilterToUse) }
             .filter { it !is PackageViewDescriptor } // subpackages are not imported
     }
 
     override fun getContributedClassifier(name: Name, location: LookupLocation): ClassifierDescriptor? {
         if (name in excludedNames) return null
-        return scopes.asSequence().mapNotNull { it.getContributedClassifier(name, location) }.singleOrNull()
+        var single: ClassifierDescriptor? = null
+        for (scope in scopes) {
+            val res = scope.getContributedClassifier(name, location) ?: continue
+            if (single == null) single = res
+            else return null
+        }
+        return single
     }
 
-    override fun getContributedVariables(name: Name, location: LookupLocation): List<VariableDescriptor> {
+    override fun getContributedVariables(name: Name, location: LookupLocation): Collection<VariableDescriptor> {
         if (name in excludedNames) return emptyList()
-        return scopes.flatMap { it.getContributedVariables(name, location) }
+        return scopes.flatMapScopes { it.getContributedVariables(name, location) }
     }
 
-    override fun getContributedFunctions(name: Name, location: LookupLocation): List<FunctionDescriptor> {
+    override fun getContributedFunctions(name: Name, location: LookupLocation): Collection<FunctionDescriptor> {
         if (name in excludedNames) return emptyList()
-        return scopes.flatMap { it.getContributedFunctions(name, location) }
+        return scopes.flatMapScopes { it.getContributedFunctions(name, location) }
     }
 
     override fun recordLookup(name: Name, location: LookupLocation) {
@@ -91,4 +103,5 @@ class AllUnderImportScope(
         p.println(this::class.java.simpleName)
     }
 }
+
 

compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/FileScopeFactory.kt
@@ -27,7 +27,7 @@ import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.KtImportInfo
 import org.jetbrains.kotlin.resolve.BindingTrace
 import org.jetbrains.kotlin.resolve.ImportPath
-import org.jetbrains.kotlin.resolve.*
+import org.jetbrains.kotlin.resolve.PlatformDependentAnalyzerServices
 import org.jetbrains.kotlin.resolve.TemporaryBindingTrace
 import org.jetbrains.kotlin.resolve.extensions.ExtraImportsProviderExtension
 import org.jetbrains.kotlin.resolve.scopes.*
@@ -85,20 +85,20 @@ class FileScopeFactory(
         }
 
         val explicit = createDefaultImportResolver(
-            ExplicitImportsIndexed(defaultImportsFiltered),
+            makeExplicitImportsIndexed(defaultImportsFiltered, components.storageManager),
             tempTrace,
             packageFragment = null,
             aliasImportNames = aliasImportNames
         )
         val allUnder = createDefaultImportResolver(
-            AllUnderImportsIndexed(defaultImportsFiltered),
+            makeAllUnderImportsIndexed(defaultImportsFiltered),
             tempTrace,
             packageFragment = null,
             aliasImportNames = aliasImportNames,
             excludedImports = analyzerServices.excludedImports
         )
         val lowPriority = createDefaultImportResolver(
-            AllUnderImportsIndexed(defaultLowPriorityImports.also { imports ->
+            makeAllUnderImportsIndexed(defaultLowPriorityImports.also { imports ->
                 assert(imports.all { it.isAllUnder }) { "All low priority imports must be all-under: $imports" }
             }),
             tempTrace,
@@ -142,9 +142,13 @@ class FileScopeFactory(
         val imports = file.importDirectives
         val aliasImportNames = imports.mapNotNull { if (it.aliasName != null) it.importedFqName else null }
 
-        val explicitImportResolver = createImportResolver(ExplicitImportsIndexed(imports), bindingTrace, aliasImportNames, packageFragment)
+        val explicitImportResolver =
+            createImportResolver(
+                makeExplicitImportsIndexed(imports, components.storageManager),
+                bindingTrace, aliasImportNames, packageFragment
+            )
         val allUnderImportResolver = createImportResolver(
-            AllUnderImportsIndexed(imports),
+            makeAllUnderImportsIndexed(imports),
             bindingTrace,
             aliasImportNames,
             packageFragment

compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/LazyImportScope.kt
@@ -39,6 +39,7 @@ import org.jetbrains.kotlin.resolve.deprecation.DeprecationResolver
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
 import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
 import org.jetbrains.kotlin.resolve.scopes.ImportingScope
+import org.jetbrains.kotlin.storage.LockBasedStorageManager
 import org.jetbrains.kotlin.storage.NotNullLazyValue
 import org.jetbrains.kotlin.storage.StorageManager
 import org.jetbrains.kotlin.types.expressions.OperatorConventions
@@ -47,20 +48,20 @@ import org.jetbrains.kotlin.utils.Printer
 import org.jetbrains.kotlin.utils.addToStdlib.flatMapToNullable
 import org.jetbrains.kotlin.utils.ifEmpty
 
-interface IndexedImports<I : KtImportInfo> {
-    val imports: List<I>
-    fun importsForName(name: Name): Collection<I>
+open class IndexedImports<I : KtImportInfo>(val imports: Array<I>) {
+    open fun importsForName(name: Name): Iterable<I> = imports.asIterable()
 }
 
-class AllUnderImportsIndexed<I : KtImportInfo>(allImports: Collection<I>) : IndexedImports<I> {
-    override val imports = allImports.filter { it.isAllUnder }
-    override fun importsForName(name: Name) = imports
-}
+inline fun <reified I : KtImportInfo> makeAllUnderImportsIndexed(imports: Collection<I>) : IndexedImports<I> =
+    IndexedImports(imports.filter { it.isAllUnder }.toTypedArray())
 
-class ExplicitImportsIndexed<I : KtImportInfo>(allImports: Collection<I>) : IndexedImports<I> {
-    override val imports = allImports.filter { !it.isAllUnder }
 
-    private val nameToDirectives: ListMultimap<Name, I> by lazy {
+class ExplicitImportsIndexed<I : KtImportInfo>(
+    imports: Array<I>,
+    storageManager: StorageManager
+) : IndexedImports<I>(imports) {
+
+    private val nameToDirectives: NotNullLazyValue<ListMultimap<Name, I>> = storageManager.createLazyValue {
         val builder = ImmutableListMultimap.builder<Name, I>()
 
         for (directive in imports) {
@@ -71,8 +72,14 @@ class ExplicitImportsIndexed<I : KtImportInfo>(allImports: Collection<I>) : Inde
         builder.build()
     }
 
-    override fun importsForName(name: Name) = nameToDirectives.get(name)
+    override fun importsForName(name: Name) = nameToDirectives().get(name)
 }
+
+inline fun <reified I : KtImportInfo> makeExplicitImportsIndexed(
+    imports: Collection<I>,
+    storageManager: StorageManager
+) : IndexedImports<I> =
+    ExplicitImportsIndexed(imports.filter { !it.isAllUnder }.toTypedArray(), storageManager)
 
 interface ImportForceResolver {
     fun forceResolveNonDefaultImports()
@@ -119,15 +126,18 @@ open class LazyImportResolver<I : KtImportInfo>(
     }
 
     val allNames: Set<Name>? by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        indexedImports.imports.flatMapToNullable(THashSet()) { getImportScope(it).computeImportedNames() }
+        indexedImports.imports.asIterable().flatMapToNullable(THashSet()) { getImportScope(it).computeImportedNames() }
     }
 
     fun definitelyDoesNotContainName(name: Name) = allNames?.let { name !in it } == true
 
     fun recordLookup(name: Name, location: LookupLocation) {
         if (allNames == null) return
-        indexedImports.importsForName(name).forEach {
-            getImportScope(it).recordLookup(name, location)
+        for (it in indexedImports.importsForName(name)) {
+            val scope = getImportScope(it)
+            if (scope !== ImportingScope.Empty) {
+                scope.recordLookup(name, location)
+            }
         }
     }
 }

compiler/frontend/src/org/jetbrains/kotlin/resolve/lazy/descriptors/LazyClassMemberScope.kt
@@ -58,25 +58,22 @@ open class LazyClassMemberScope(
     c, declarationProvider, thisClass, trace, scopeForDeclaredMembers
 ) {
 
-    private val descriptorsFromDeclaredElements = storageManager.createLazyValue {
-        computeDescriptorsFromDeclaredElements(
-            DescriptorKindFilter.ALL,
-            MemberScope.ALL_NAME_FILTER,
-            NoLookupLocation.WHEN_GET_ALL_DESCRIPTORS
+    private val allDescriptors = storageManager.createLazyValue {
+        val result = LinkedHashSet(
+            computeDescriptorsFromDeclaredElements(
+                DescriptorKindFilter.ALL,
+                MemberScope.ALL_NAME_FILTER,
+                NoLookupLocation.WHEN_GET_ALL_DESCRIPTORS
+            )
         )
-    }
-    private val extraDescriptors: NotNullLazyValue<Collection<DeclarationDescriptor>> = storageManager.createLazyValue {
-        computeExtraDescriptors(NoLookupLocation.FOR_ALREADY_TRACKED)
+        result.addAll(computeExtraDescriptors(NoLookupLocation.FOR_ALREADY_TRACKED))
+        result
     }
 
     override fun getContributedDescriptors(
         kindFilter: DescriptorKindFilter,
         nameFilter: (Name) -> Boolean
-    ): Collection<DeclarationDescriptor> {
-        val result = LinkedHashSet(descriptorsFromDeclaredElements())
-        result.addAll(extraDescriptors())
-        return result
-    }
+    ): Collection<DeclarationDescriptor> = allDescriptors()
 
     protected open fun computeExtraDescriptors(location: LookupLocation): Collection<DeclarationDescriptor> {
         val result = ArrayList<DeclarationDescriptor>()

core/descriptors.jvm/src/org/jetbrains/kotlin/load/java/lazy/descriptors/JvmPackageScope.kt
@@ -31,6 +31,7 @@ import org.jetbrains.kotlin.resolve.scopes.flatMapClassifierNamesOrNull
 import org.jetbrains.kotlin.storage.getValue
 import org.jetbrains.kotlin.util.collectionUtils.getFirstClassifierDiscriminateHeaders
 import org.jetbrains.kotlin.util.collectionUtils.getFromAllScopes
+import org.jetbrains.kotlin.util.collectionUtils.listOfNonEmptyScopes
 import org.jetbrains.kotlin.utils.Printer
 
 class JvmPackageScope(
@@ -41,9 +42,11 @@ class JvmPackageScope(
     internal val javaScope = LazyJavaPackageScope(c, jPackage, packageFragment)
 
     private val kotlinScopes by c.storageManager.createLazyValue {
-        packageFragment.binaryClasses.values.mapNotNull { partClass ->
-            c.components.deserializedDescriptorResolver.createKotlinPackagePartScope(packageFragment, partClass)
-        }.toList()
+        listOfNonEmptyScopes(
+            packageFragment.binaryClasses.values.mapNotNull { partClass ->
+                c.components.deserializedDescriptorResolver.createKotlinPackagePartScope(packageFragment, partClass)
+            }
+        ).toTypedArray()
     }
 
     override fun getContributedClassifier(name: Name, location: LookupLocation): ClassifierDescriptor? {
@@ -78,7 +81,7 @@ class JvmPackageScope(
         addAll(javaScope.getVariableNames())
     }
 
-    override fun getClassifierNames(): Set<Name>? = kotlinScopes.flatMapClassifierNamesOrNull()?.apply {
+    override fun getClassifierNames(): Set<Name>? = kotlinScopes.asIterable().flatMapClassifierNamesOrNull()?.apply {
         addAll(javaScope.getClassifierNames())
     }
 

core/descriptors/src/org/jetbrains/kotlin/resolve/scopes/GivenFunctionsMemberScope.kt
@@ -23,6 +23,7 @@ import org.jetbrains.kotlin.resolve.NonReportingOverrideStrategy
 import org.jetbrains.kotlin.resolve.OverridingUtil
 import org.jetbrains.kotlin.storage.StorageManager
 import org.jetbrains.kotlin.storage.getValue
+import org.jetbrains.kotlin.util.collectionUtils.filterIsInstanceAnd
 import org.jetbrains.kotlin.utils.Printer
 import org.jetbrains.kotlin.utils.compact
 import java.util.*
@@ -47,11 +48,11 @@ abstract class GivenFunctionsMemberScope(
     }
 
     override fun getContributedFunctions(name: Name, location: LookupLocation): Collection<SimpleFunctionDescriptor> {
-        return allDescriptors.filterIsInstance<SimpleFunctionDescriptor>().filter { it.name == name }
+        return allDescriptors.filterIsInstanceAnd { it.name == name }
     }
 
     override fun getContributedVariables(name: Name, location: LookupLocation): Collection<PropertyDescriptor> {
-        return allDescriptors.filterIsInstance<PropertyDescriptor>().filter { it.name == name }
+        return allDescriptors.filterIsInstanceAnd { it.name == name }
     }
 
     private fun createFakeOverrides(functionsFromCurrent: List<FunctionDescriptor>): List<DeclarationDescriptor> {

commit 25ff582153f1edfabd858bb14736eb25829fe5fb

K1: fix issue in MemberScopeImpl.getVariableNames
Filtering the list of variable descriptors to find function descriptors
is a bit pointless, since no variable descriptor can be a function
descriptor at the same time. This seems like a copy-paste bug introduced
in bf97323301.

This bug affected all `MemberScopeImpl` subclasses which do not override
`getVariableNames`. In particular, `StaticScopeForKotlinEnum` failed to
report `entries` as a possible variable name.

Even though K1 is deprecated and partially removed, it's still used in
the legacy implementation of kotlin-reflect. I've stumbled upon this bug
while refactoring the internal implementation of `KClass.members` to
make members stored and computed by name, rather than all at once.

 #KT-80710

core/descriptors/src/org/jetbrains/kotlin/resolve/scopes/MemberScopeImpl.kt
@@ -20,6 +20,7 @@ import org.jetbrains.kotlin.descriptors.ClassifierDescriptor
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
 import org.jetbrains.kotlin.descriptors.SimpleFunctionDescriptor
+import org.jetbrains.kotlin.descriptors.VariableDescriptor
 import org.jetbrains.kotlin.incremental.components.LookupLocation
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.utils.Printer
@@ -46,7 +47,7 @@ abstract class MemberScopeImpl : MemberScope {
     override fun getVariableNames(): Set<Name> =
         getContributedDescriptors(
             DescriptorKindFilter.VARIABLES, alwaysTrue()
-        ).filterIsInstanceMapTo<SimpleFunctionDescriptor, Name, MutableSet<Name>>(mutableSetOf()) { it.name }
+        ).filterIsInstanceMapTo<VariableDescriptor, Name, MutableSet<Name>>(mutableSetOf()) { it.name }
 
     override fun getClassifierNames(): Set<Name>? = null
 