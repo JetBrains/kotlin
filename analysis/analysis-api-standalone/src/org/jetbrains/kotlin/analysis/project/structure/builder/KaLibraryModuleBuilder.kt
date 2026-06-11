ewok-pulse/saber-e2e-kotlin

[Analysis API] Introduce 'KaModuleContainer'
The new class removes dependency on the Standalone implementation class
'KotlinStaticProjectStructureProvider'.

^KT-84911

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaLibraryModuleBuilder.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.analysis.project.structure.builder
 
 import com.intellij.core.CoreApplicationEnvironment
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.toNioPathOrNull
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.standalone.base.projectStructure.StandaloneProjectFactory
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
@@ -51,7 +50,7 @@ public open class KtLibraryModuleBuilder(
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtLibraryModule(init: KtLibraryModuleBuilder.() -> Unit): KaLibraryModule {
+public inline fun KtModuleContainerBuilder.buildKtLibraryModule(init: KtLibraryModuleBuilder.() -> Unit): KaLibraryModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaLibrarySourceModuleBuilder.kt
@@ -35,7 +35,7 @@ public class KtLibrarySourceModuleBuilder(private val project: Project) : KtModu
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtLibrarySourceModule(init: KtLibrarySourceModuleBuilder.() -> Unit): KaLibrarySourceModule {
+public inline fun KtModuleContainerBuilder.buildKtLibrarySourceModule(init: KtLibrarySourceModuleBuilder.() -> Unit): KaLibrarySourceModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaScriptModuleBuilder.kt
@@ -41,7 +41,7 @@ public class KtScriptModuleBuilder(private val project: Project) : KtModuleBuild
 
 @KaExperimentalApi
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtScriptModule(init: KtScriptModuleBuilder.() -> Unit): KaScriptModule {
+public inline fun KtModuleContainerBuilder.buildKtScriptModule(init: KtScriptModuleBuilder.() -> Unit): KaScriptModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaSdkModuleBuilder.kt
@@ -28,7 +28,7 @@ public class KtSdkModuleBuilder(
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtSdkModule(init: KtSdkModuleBuilder.() -> Unit): KaLibraryModule {
+public inline fun KtModuleContainerBuilder.buildKtSdkModule(init: KtSdkModuleBuilder.() -> Unit): KaLibraryModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaSourceModuleBuilder.kt
@@ -93,7 +93,7 @@ public class KtSourceModuleBuilder(
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtSourceModule(init: KtSourceModuleBuilder.() -> Unit): KaSourceModule {
+public inline fun KtModuleContainerBuilder.buildKtSourceModule(init: KtSourceModuleBuilder.() -> Unit): KaSourceModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

libraries/tools/analysis-api-based-klib-reader/src/org/jetbrains/kotlin/analysis/api/klib/reader/createKaModules.kt
@@ -10,7 +10,7 @@ import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaSourceModule
 import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
-import org.jetbrains.kotlin.analysis.project.structure.builder.KtModuleProviderBuilder
+import org.jetbrains.kotlin.analysis.project.structure.builder.KtModuleContainerBuilder
 import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
 import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
 import org.jetbrains.kotlin.library.metadata.KlibInputModule
@@ -62,7 +62,7 @@ public fun <Config> createKaModulesForStandaloneAnalysis(
     return KaModules(fakeSourceModule, binaryModules, platformLibraryModules)
 }
 
-private fun <Config> KtModuleProviderBuilder.inputModuleIntoKaLibraryModule(
+private fun <Config> KtModuleContainerBuilder.inputModuleIntoKaLibraryModule(
     input: KlibInputModule<Config>,
     targetPlatform: TargetPlatform,
 ): KaLibraryModule = addModule(

JetBrains/kotlin

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaLibraryModuleBuilder.kt
@@ -7,7 +7,6 @@ package org.jetbrains.kotlin.analysis.project.structure.builder
 
 import com.intellij.core.CoreApplicationEnvironment
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.toNioPathOrNull
 import org.jetbrains.kotlin.analysis.api.KaExperimentalApi
 import org.jetbrains.kotlin.analysis.api.standalone.base.projectStructure.StandaloneProjectFactory
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
@@ -51,7 +50,7 @@ public open class KtLibraryModuleBuilder(
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtLibraryModule(init: KtLibraryModuleBuilder.() -> Unit): KaLibraryModule {
+public inline fun KaModuleContainerBuilder.buildKtLibraryModule(init: KtLibraryModuleBuilder.() -> Unit): KaLibraryModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaLibrarySourceModuleBuilder.kt
@@ -35,7 +35,7 @@ public class KtLibrarySourceModuleBuilder(private val project: Project) : KtModu
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtLibrarySourceModule(init: KtLibrarySourceModuleBuilder.() -> Unit): KaLibrarySourceModule {
+public inline fun KaModuleContainerBuilder.buildKtLibrarySourceModule(init: KtLibrarySourceModuleBuilder.() -> Unit): KaLibrarySourceModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaScriptModuleBuilder.kt
@@ -41,7 +41,7 @@ public class KtScriptModuleBuilder(private val project: Project) : KtModuleBuild
 
 @KaExperimentalApi
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtScriptModule(init: KtScriptModuleBuilder.() -> Unit): KaScriptModule {
+public inline fun KaModuleContainerBuilder.buildKtScriptModule(init: KtScriptModuleBuilder.() -> Unit): KaScriptModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaSdkModuleBuilder.kt
@@ -28,7 +28,7 @@ public class KtSdkModuleBuilder(
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtSdkModule(init: KtSdkModuleBuilder.() -> Unit): KaLibraryModule {
+public inline fun KaModuleContainerBuilder.buildKtSdkModule(init: KtSdkModuleBuilder.() -> Unit): KaLibraryModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

analysis/analysis-api-standalone/src/org/jetbrains/kotlin/analysis/project/structure/builder/KaSourceModuleBuilder.kt
@@ -93,7 +93,7 @@ public class KtSourceModuleBuilder(
 }
 
 @OptIn(ExperimentalContracts::class)
-public inline fun KtModuleProviderBuilder.buildKtSourceModule(init: KtSourceModuleBuilder.() -> Unit): KaSourceModule {
+public inline fun KaModuleContainerBuilder.buildKtSourceModule(init: KtSourceModuleBuilder.() -> Unit): KaSourceModule {
     contract {
         callsInPlace(init, InvocationKind.EXACTLY_ONCE)
     }

libraries/tools/analysis-api-based-klib-reader/src/org/jetbrains/kotlin/analysis/api/klib/reader/createKaModules.kt
@@ -10,7 +10,7 @@ import org.jetbrains.kotlin.analysis.api.projectStructure.KaLibraryModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaSourceModule
 import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
-import org.jetbrains.kotlin.analysis.project.structure.builder.KtModuleProviderBuilder
+import org.jetbrains.kotlin.analysis.project.structure.builder.KaModuleContainerBuilder
 import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
 import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
 import org.jetbrains.kotlin.library.metadata.KlibInputModule
@@ -70,7 +70,7 @@ public fun <Config> createKaModulesForStandaloneAnalysis(
     return KaModules(fakeSourceModule, binaryModules, platformLibraryModules, cinteropReexportLibraryModules)
 }
 
-private fun <Config> KtModuleProviderBuilder.inputModuleIntoKaLibraryModule(
+private fun <Config> KaModuleContainerBuilder.inputModuleIntoKaLibraryModule(
     input: KlibInputModule<Config>,
     targetPlatform: TargetPlatform,
 ): KaLibraryModule = addModule(