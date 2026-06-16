51adbfc80d727726afdc160f75dd0a13fb22f8fd
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-06-16 17:31:43 +0200

    [JS] Preserve containers' names when lifting static members
    This results in better debugging experience.
    
    Some lowerings that also included the whole container name chain
    in synthetic declarations were adjusted so that they'd only generate
    the name chain up until the innermost class container.
    
    ^KT-87008 Fixed

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/WebCallableReferenceLowering.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/lower/WebCallableReferenceLowering.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2025 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -19,11 +19,7 @@ import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.IrRichFunctionReference
 import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrReturnImpl
-import org.jetbrains.kotlin.ir.types.IrSimpleType
-import org.jetbrains.kotlin.ir.types.IrType
-import org.jetbrains.kotlin.ir.types.classOrFail
-import org.jetbrains.kotlin.ir.types.getClass
-import org.jetbrains.kotlin.ir.types.typeWithArguments
+import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.name.SpecialNames
