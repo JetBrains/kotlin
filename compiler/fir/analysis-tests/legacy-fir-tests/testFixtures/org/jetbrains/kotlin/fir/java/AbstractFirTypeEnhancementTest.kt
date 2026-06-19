625f1a7881d79c486549545187c52f37f652f32f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-06-19 09:59:43 +0200

    Tests: drop unnecessary JavaElementFinder unregistration

--- compiler/fir/analysis-tests/legacy-fir-tests/testFixtures/org/jetbrains/kotlin/fir/java/AbstractFirTypeEnhancementTest.kt
+++ compiler/fir/analysis-tests/legacy-fir-tests/testFixtures/org/jetbrains/kotlin/fir/java/AbstractFirTypeEnhancementTest.kt
@@ -10,7 +10,6 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtilRt
 import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElementFinder
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiPackageStatement
 import com.intellij.psi.impl.PsiFileFactoryImpl
@@ -18,7 +17,6 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.testFramework.LightVirtualFile
 import org.jetbrains.kotlin.CoreEnvironmentDeprecation
 import org.jetbrains.kotlin.ObsoleteTestInfrastructure
-import org.jetbrains.kotlin.asJava.finder.JavaElementFinder
 import org.jetbrains.kotlin.cli.jvm.compiler.*
 import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.codegen.forTestCompile.JavaForeignAnnotationType
@@ -91,9 +89,7 @@ abstract class AbstractFirTypeEnhancementTest : KtUsefulTestCase() {
                 ConfigurationKind.JDK_NO_RUNTIME, TestJdkKind.FULL_JDK, classpath, listOf(javaFilesDir)
             ),
             EnvironmentConfigFiles.JVM_CONFIG_FILES
-        ).apply {
-            PsiElementFinder.EP.getPoint(project).unregisterExtension(JavaElementFinder::class.java)
-        }
+        )
     }
 
     @OptIn(ObsoleteTestInfrastructure::class)
