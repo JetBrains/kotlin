d9cdf53ca3d58b99c7ace89a4bdb6fadc8c6c34a
Brian Norman <brian.norman@jetbrains.com>
2026-06-22 08:15:33 -0500

    fixup! [FIR] Add support for `lateinit val` in compiler frontend

--- compiler/testData/diagnostics/tests/lateinit/lateinitValOverride.kt
+++ compiler/testData/diagnostics/tests/lateinit/lateinitValOverride.kt
@@ -5,12 +5,14 @@
 
 open class Base {
     open lateinit val foo: String
+    open lateinit val bar: String
 }
 
 // MODULE: main(lib)
 
 class Derived : Base() {
     override lateinit val foo: String
+    override <!LATEINIT_VAL_OVERRIDDEN_BY_VAL!>val<!> bar: String = ""
 }
 
 /* GENERATED_FIR_TAGS: assignment, classDeclaration, functionDeclaration, lateinit, propertyDeclaration, stringLiteral */
