655c3b4972ab6fe489d5006bf136e06c32b54a7c
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:39 +0200

    [stubs] migrate KtPlaceHolderWithTextStubElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtPlaceHolderWithTextStubElementType.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtPlaceHolderWithTextStubElementType.kt
@@ -3,14 +3,17 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+
 package org.jetbrains.kotlin.psi.stubs.elements
 
-import com.intellij.psi.stubs.StubElement
-import com.intellij.psi.stubs.StubInputStream
-import com.intellij.psi.stubs.StubOutputStream
+import com.intellij.psi.stubs.StubElementFactory
+import com.intellij.psi.stubs.StubSerializer
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.kotlin.psi.KtElementImplStub
 import org.jetbrains.kotlin.psi.stubs.KotlinPlaceHolderWithTextStub
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinPlaceHolderWithTextStubFactory
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinPlaceHolderWithTextStubSerializer
 import org.jetbrains.kotlin.psi.stubs.impl.KotlinPlaceHolderWithTextStubImpl
 
 class KtPlaceHolderWithTextStubElementType<T : KtElementImplStub<*>>(@NonNls debugName: String, psiClass: Class<T>) :
@@ -19,17 +22,10 @@ class KtPlaceHolderWithTextStubElementType<T : KtElementImplStub<*>>(@NonNls deb
         psiClass,
         KotlinPlaceHolderWithTextStub::class.java,
     ) {
+    private val stubFactory = KotlinPlaceHolderWithTextStubFactory(this)
+    private val stubSerializer = KotlinPlaceHolderWithTextStubSerializer(this)
 
-    override fun createStub(psi: T, parentStub: StubElement<*>): KotlinPlaceHolderWithTextStubImpl<T> {
-        return KotlinPlaceHolderWithTextStubImpl(parentStub, this, psi.text)
-    }
+    override fun getStubFactory(): StubElementFactory<KotlinPlaceHolderWithTextStubImpl<T>, T> = stubFactory
 
-    override fun serialize(stub: KotlinPlaceHolderWithTextStubImpl<T>, dataStream: StubOutputStream) {
-        dataStream.writeUTFFast(stub.text)
-    }
-
-    override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>): KotlinPlaceHolderWithTextStubImpl<T> {
-        val text = dataStream.readUTFFast()
-        return KotlinPlaceHolderWithTextStubImpl(parentStub, this, text)
-    }
+    override fun getStubSerializer(): StubSerializer<KotlinPlaceHolderWithTextStubImpl<T>> = stubSerializer
 }
---
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/factory/KotlinPlaceHolderWithTextStubFactory.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.psi.stubs.factory
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.*
+import org.jetbrains.kotlin.psi.KtElementImplStub
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementType
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinPlaceHolderWithTextStubImpl
+
+/**
+ * Builds the placeholder-with-text stub (short/literal/escape string-template entries), whose payload is the element
+ * text (KT-78356).
+ */
+@Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+internal class KotlinPlaceHolderWithTextStubFactory<T : KtElementImplStub<*>>(
+    private val elementType: KtStubElementType<KotlinPlaceHolderWithTextStubImpl<T>, T>,
+) : StubElementFactory<KotlinPlaceHolderWithTextStubImpl<T>, T> {
+    override fun createStub(psi: T, parentStub: StubElement<out PsiElement>?): KotlinPlaceHolderWithTextStubImpl<T> {
+        return KotlinPlaceHolderWithTextStubImpl(parentStub, elementType, psi.text)
+    }
+
+    override fun createPsi(stub: KotlinPlaceHolderWithTextStubImpl<T>): T {
+        return elementType.createPsiFromStub(stub)
+    }
+
+    override fun shouldCreateStub(node: ASTNode): Boolean {
+        return KtStubElementType.shouldCreateStubDependingOnParent(node)
+    }
+}
+
+internal class KotlinPlaceHolderWithTextStubSerializer<T : KtElementImplStub<*>>(
+    private val elementType: KtStubElementType<KotlinPlaceHolderWithTextStubImpl<T>, T>,
+) : StubSerializer<KotlinPlaceHolderWithTextStubImpl<T>> {
+    override fun getExternalId(): String = elementType.conventionalExternalId
+
+    override fun serialize(stub: KotlinPlaceHolderWithTextStubImpl<T>, dataStream: StubOutputStream) {
+        dataStream.writeUTFFast(stub.text)
+    }
+
+    override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>?): KotlinPlaceHolderWithTextStubImpl<T> {
+        val text = dataStream.readUTFFast()
+        return KotlinPlaceHolderWithTextStubImpl(parentStub, elementType, text)
+    }
+
+    override fun indexStub(stub: KotlinPlaceHolderWithTextStubImpl<T>, sink: IndexSink) {
+        // not indexed
+    }
+}
