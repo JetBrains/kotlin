f83a462b1a2bbb4f04b69931a126ceb991906324
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:46 +0200

    [stubs] migrate KtImportDirectiveElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtImportDirectiveElementType.java
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtImportDirectiveElementType.java
@@ -5,46 +5,29 @@
 
 package org.jetbrains.kotlin.psi.stubs.elements;
 
-import com.intellij.psi.stubs.StubElement;
-import com.intellij.psi.stubs.StubInputStream;
-import com.intellij.psi.stubs.StubOutputStream;
-import com.intellij.util.io.StringRef;
+import com.intellij.psi.stubs.StubElementFactory;
+import com.intellij.psi.stubs.StubSerializer;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.name.FqName;
 import org.jetbrains.kotlin.psi.KtImportDirective;
 import org.jetbrains.kotlin.psi.stubs.KotlinImportDirectiveStub;
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinImportDirectiveStubFactory;
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinImportDirectiveStubSerializer;
 import org.jetbrains.kotlin.psi.stubs.impl.KotlinImportDirectiveStubImpl;
 
-import java.io.IOException;
-
+@SuppressWarnings("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
 public class KtImportDirectiveElementType extends KtStubElementType<KotlinImportDirectiveStubImpl, KtImportDirective> {
     public KtImportDirectiveElementType(@NotNull @NonNls String debugName) {
         super(debugName, KtImportDirective.class, KotlinImportDirectiveStub.class);
     }
 
-    @NotNull
     @Override
-    public KotlinImportDirectiveStubImpl createStub(@NotNull KtImportDirective psi, StubElement<?> parentStub) {
-        FqName importedFqName = psi.getImportedFqName();
-        StringRef fqName = StringRef.fromString(importedFqName == null ? null : importedFqName.asString());
-        return new KotlinImportDirectiveStubImpl(parentStub, psi.isAllUnder(), fqName, psi.isValidImport());
+    public StubElementFactory<KotlinImportDirectiveStubImpl, KtImportDirective> getStubFactory() {
+        return KotlinImportDirectiveStubFactory.INSTANCE;
     }
 
     @Override
-    public void serialize(@NotNull KotlinImportDirectiveStubImpl stub, @NotNull StubOutputStream dataStream) throws IOException {
-        dataStream.writeBoolean(stub.isAllUnder());
-        FqName importedFqName = stub.getImportedFqName();
-        dataStream.writeName(importedFqName != null ? importedFqName.asString() : null);
-        dataStream.writeBoolean(stub.isValid());
-    }
-
-    @NotNull
-    @Override
-    public KotlinImportDirectiveStubImpl deserialize(@NotNull StubInputStream dataStream, StubElement parentStub) throws IOException {
-        boolean isAllUnder = dataStream.readBoolean();
-        StringRef importedName = dataStream.readName();
-        boolean isValid = dataStream.readBoolean();
-        return new KotlinImportDirectiveStubImpl(parentStub, isAllUnder, importedName, isValid);
+    public StubSerializer<KotlinImportDirectiveStubImpl> getStubSerializer() {
+        return KotlinImportDirectiveStubSerializer.INSTANCE;
     }
 }
---
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/factory/KotlinImportDirectiveStubFactory.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.psi.stubs.factory
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.*
+import com.intellij.util.io.StringRef
+import org.jetbrains.kotlin.psi.KtImportDirective
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementType
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinImportDirectiveStubImpl
+
+@Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+internal object KotlinImportDirectiveStubFactory : StubElementFactory<KotlinImportDirectiveStubImpl, KtImportDirective> {
+    override fun shouldCreateStub(node: ASTNode): Boolean = KtStubElementType.shouldCreateStubDependingOnParent(node)
+
+    override fun createStub(psi: KtImportDirective, parentStub: StubElement<out PsiElement>?): KotlinImportDirectiveStubImpl {
+        val importedFqName = psi.importedFqName
+        val fqName = StringRef.fromString(importedFqName?.asString())
+        return KotlinImportDirectiveStubImpl(parentStub, psi.isAllUnder, fqName, psi.isValidImport)
+    }
+
+    override fun createPsi(stub: KotlinImportDirectiveStubImpl): KtImportDirective = KtImportDirective(stub)
+}
+
+internal object KotlinImportDirectiveStubSerializer : StubSerializer<KotlinImportDirectiveStubImpl> {
+    override fun getExternalId(): String = "kotlin.IMPORT_DIRECTIVE"
+
+    override fun serialize(stub: KotlinImportDirectiveStubImpl, dataStream: StubOutputStream) {
+        dataStream.writeBoolean(stub.isAllUnder)
+        val importedFqName = stub.importedFqName
+        dataStream.writeName(importedFqName?.asString())
+        dataStream.writeBoolean(stub.isValid)
+    }
+
+    override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>?): KotlinImportDirectiveStubImpl {
+        val isAllUnder = dataStream.readBoolean()
+        val importedName = dataStream.readName()
+        val isValid = dataStream.readBoolean()
+        return KotlinImportDirectiveStubImpl(parentStub, isAllUnder, importedName, isValid)
+    }
+
+    override fun indexStub(stub: KotlinImportDirectiveStubImpl, sink: IndexSink) {
+        // not indexed
+    }
+}
