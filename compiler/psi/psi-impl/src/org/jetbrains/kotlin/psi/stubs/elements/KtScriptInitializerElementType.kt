8f12aa3c00a4449bee13cb07ee234396e7c4aec2
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:38 +0200

    [stubs] migrate KtScriptInitializerElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtScriptInitializerElementType.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtScriptInitializerElementType.kt
@@ -3,13 +3,23 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+
 package org.jetbrains.kotlin.psi.stubs.elements
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.stubs.StubElementFactory
 import org.jetbrains.kotlin.psi.KtScriptInitializer
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinPlaceHolderStubFactory
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinPlaceHolderStubImpl
 
 object KtScriptInitializerElementType : KtPlaceHolderStubElementType<KtScriptInitializer>(
     "SCRIPT_INITIALIZER", KtScriptInitializer::class.java,
 ) {
-    override fun shouldCreateStub(node: ASTNode): Boolean = true
+    private val stubFactory = object : KotlinPlaceHolderStubFactory<KtScriptInitializer>(this) {
+        override fun shouldCreateStub(node: ASTNode): Boolean = true
+    }
+
+    override fun getStubFactory(): StubElementFactory<KotlinPlaceHolderStubImpl<KtScriptInitializer>, KtScriptInitializer> =
+        stubFactory
 }
