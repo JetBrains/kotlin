91e7930ef3d19bfff60faea461a59bd715b598c3
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:39 +0200

    [stubs] migrate KtDotQualifiedExpressionElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtDotQualifiedExpressionElementType.java
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtDotQualifiedExpressionElementType.java
@@ -6,31 +6,43 @@
 package org.jetbrains.kotlin.psi.stubs.elements;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.stubs.StubElementFactory;
 import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression;
 import org.jetbrains.kotlin.psi.stubs.StubUtils;
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinPlaceHolderStubFactory;
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinPlaceHolderStubImpl;
 
+@SuppressWarnings("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
 public class KtDotQualifiedExpressionElementType extends KtPlaceHolderStubElementType<KtDotQualifiedExpression> {
+    private final StubElementFactory<KotlinPlaceHolderStubImpl<KtDotQualifiedExpression>, KtDotQualifiedExpression> stubFactory =
+            new KotlinPlaceHolderStubFactory<KtDotQualifiedExpression>(this) {
+                @Override
+                public boolean shouldCreateStub(@NotNull ASTNode node) {
+                    ASTNode treeParent = node.getTreeParent();
+                    if (treeParent == null) return false;
+
+                    IElementType parentElementType = treeParent.getElementType();
+                    if (parentElementType == KtStubElementTypes.IMPORT_DIRECTIVE ||
+                        parentElementType == KtStubElementTypes.PACKAGE_DIRECTIVE ||
+                        parentElementType == KtStubElementTypes.DOT_QUALIFIED_EXPRESSION ||
+                        StubUtils.isDeclaredInsideValueArgument$org_jetbrains_kotlin_psi_impl(node)
+                    ) {
+                        return super.shouldCreateStub(node);
+                    }
+
+                    return false;
+                }
+            };
+
     public KtDotQualifiedExpressionElementType(@NotNull @NonNls String debugName) {
         super(debugName, KtDotQualifiedExpression.class);
     }
 
     @Override
-    public boolean shouldCreateStub(ASTNode node) {
-        ASTNode treeParent = node.getTreeParent();
-        if (treeParent == null) return false;
-
-        IElementType parentElementType = treeParent.getElementType();
-        if (parentElementType == KtStubElementTypes.IMPORT_DIRECTIVE ||
-            parentElementType == KtStubElementTypes.PACKAGE_DIRECTIVE ||
-            parentElementType == KtStubElementTypes.DOT_QUALIFIED_EXPRESSION ||
-            StubUtils.isDeclaredInsideValueArgument$org_jetbrains_kotlin_psi_impl(node)
-        ) {
-            return super.shouldCreateStub(node);
-        }
-
-        return false;
+    public StubElementFactory<KotlinPlaceHolderStubImpl<KtDotQualifiedExpression>, KtDotQualifiedExpression> getStubFactory() {
+        return stubFactory;
     }
 }
