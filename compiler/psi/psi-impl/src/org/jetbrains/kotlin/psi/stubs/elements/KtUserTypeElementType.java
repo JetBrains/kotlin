0832eb190c2dba1ac7013b7b7d2551e3f07c9347
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:43 +0200

    [stubs] migrate KtUserTypeElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtUserTypeElementType.java
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtUserTypeElementType.java
@@ -5,41 +5,29 @@
 
 package org.jetbrains.kotlin.psi.stubs.elements;
 
-import com.intellij.psi.stubs.StubElement;
-import com.intellij.psi.stubs.StubInputStream;
-import com.intellij.psi.stubs.StubOutputStream;
+import com.intellij.psi.stubs.StubElementFactory;
+import com.intellij.psi.stubs.StubSerializer;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.psi.KtUserType;
 import org.jetbrains.kotlin.psi.stubs.KotlinUserTypeStub;
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinUserTypeStubFactory;
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinUserTypeStubSerializer;
 import org.jetbrains.kotlin.psi.stubs.impl.KotlinUserTypeStubImpl;
 
-import java.io.IOException;
-
-import static org.jetbrains.kotlin.psi.stubs.elements.TypeBeanSerializationKt.*;
-
+@SuppressWarnings("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
 public class KtUserTypeElementType extends KtStubElementType<KotlinUserTypeStubImpl, KtUserType> {
     public KtUserTypeElementType(@NotNull @NonNls String debugName) {
         super(debugName, KtUserType.class, KotlinUserTypeStub.class);
     }
 
-    @NotNull
     @Override
-    public KotlinUserTypeStubImpl createStub(@NotNull KtUserType psi, StubElement<?> parentStub) {
-        return new KotlinUserTypeStubImpl(parentStub, null, null);
+    public StubElementFactory<KotlinUserTypeStubImpl, KtUserType> getStubFactory() {
+        return KotlinUserTypeStubFactory.INSTANCE;
     }
 
     @Override
-    public void serialize(@NotNull KotlinUserTypeStubImpl stub, @NotNull StubOutputStream dataStream) throws IOException {
-        serializeTypeBean(dataStream, stub.getUpperBound());
-        serializeTypeBean(dataStream, stub.getAbbreviatedType());
-    }
-
-    @NotNull
-    @Override
-    public KotlinUserTypeStubImpl deserialize(@NotNull StubInputStream dataStream, StubElement parentStub) throws IOException {
-        return new KotlinUserTypeStubImpl((StubElement<?>) parentStub,
-                                          deserializeTypeBean(dataStream),
-                                          deserializeClassTypeBean(dataStream));
+    public StubSerializer<KotlinUserTypeStubImpl> getStubSerializer() {
+        return KotlinUserTypeStubSerializer.INSTANCE;
     }
 }
---
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/factory/KotlinUserTypeStubFactory.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.psi.stubs.factory
+
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.*
+import org.jetbrains.kotlin.psi.KtUserType
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementType
+import org.jetbrains.kotlin.psi.stubs.elements.deserializeClassTypeBean
+import org.jetbrains.kotlin.psi.stubs.elements.deserializeTypeBean
+import org.jetbrains.kotlin.psi.stubs.elements.serializeTypeBean
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinUserTypeStubImpl
+
+@Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+internal object KotlinUserTypeStubFactory : StubElementFactory<KotlinUserTypeStubImpl, KtUserType> {
+    override fun shouldCreateStub(node: ASTNode): Boolean = KtStubElementType.shouldCreateStubDependingOnParent(node)
+
+    override fun createStub(psi: KtUserType, parentStub: StubElement<out PsiElement>?): KotlinUserTypeStubImpl {
+        return KotlinUserTypeStubImpl(parentStub, null, null)
+    }
+
+    override fun createPsi(stub: KotlinUserTypeStubImpl): KtUserType = KtUserType(stub)
+}
+
+internal object KotlinUserTypeStubSerializer : StubSerializer<KotlinUserTypeStubImpl> {
+    override fun getExternalId(): String = "kotlin.USER_TYPE"
+
+    override fun serialize(stub: KotlinUserTypeStubImpl, dataStream: StubOutputStream) {
+        serializeTypeBean(dataStream, stub.upperBound)
+        serializeTypeBean(dataStream, stub.abbreviatedType)
+    }
+
+    override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>?): KotlinUserTypeStubImpl {
+        return KotlinUserTypeStubImpl(
+            parentStub,
+            deserializeTypeBean(dataStream),
+            deserializeClassTypeBean(dataStream),
+        )
+    }
+
+    override fun indexStub(stub: KotlinUserTypeStubImpl, sink: IndexSink) {
+        // not indexed
+    }
+}
