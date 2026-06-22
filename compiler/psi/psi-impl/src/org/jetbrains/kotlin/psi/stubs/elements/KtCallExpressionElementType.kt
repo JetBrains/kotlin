efcded102a3f0c3361f9ee5a6636aa450ce918f1
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:38 +0200

    [stubs] migrate KtCallExpressionElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtCallExpressionElementType.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtCallExpressionElementType.kt
@@ -3,16 +3,26 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+
 package org.jetbrains.kotlin.psi.stubs.elements
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.stubs.StubElementFactory
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.stubs.StubUtils
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinPlaceHolderStubFactory
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinPlaceHolderStubImpl
 
 internal object KtCallExpressionElementType : KtPlaceHolderStubElementType<KtCallExpression>(
     "CALL_EXPRESSION", KtCallExpression::class.java
 ) {
-    override fun shouldCreateStub(node: ASTNode): Boolean {
-        return StubUtils.isDeclaredInsideValueArgument(node) && super.shouldCreateStub(node)
+    private val stubFactory = object : KotlinPlaceHolderStubFactory<KtCallExpression>(this) {
+        override fun shouldCreateStub(node: ASTNode): Boolean {
+            return StubUtils.isDeclaredInsideValueArgument(node) && super.shouldCreateStub(node)
+        }
     }
+
+    override fun getStubFactory(): StubElementFactory<KotlinPlaceHolderStubImpl<KtCallExpression>, KtCallExpression> =
+        stubFactory
 }
