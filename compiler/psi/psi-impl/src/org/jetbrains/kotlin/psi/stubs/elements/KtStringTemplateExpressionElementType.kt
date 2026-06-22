3df043156e0ccb6ec746838748bde2e62d676f03
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-06-22 21:10:39 +0200

    [stubs] migrate KtStringTemplateExpressionElementType to stub factory
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtStringTemplateExpressionElementType.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/KtStringTemplateExpressionElementType.kt
@@ -3,21 +3,27 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:Suppress("UnstableApiUsage") // KT-78356: the platform stub-decoupling API is still @ApiStatus.Experimental
+
 package org.jetbrains.kotlin.psi.stubs.elements
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.stubs.StubElementFactory
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.stubs.StubUtils
+import org.jetbrains.kotlin.psi.stubs.factory.KotlinPlaceHolderStubFactory
+import org.jetbrains.kotlin.psi.stubs.impl.KotlinPlaceHolderStubImpl
 
 class KtStringTemplateExpressionElementType(@NonNls debugName: String) :
     KtPlaceHolderStubElementType<KtStringTemplateExpression>(debugName, KtStringTemplateExpression::class.java) {
 
-    override fun shouldCreateStub(node: ASTNode): Boolean {
-        if (!StubUtils.isDeclaredInsideValueArgument(node)) {
-            return false
+    private val stubFactory = object : KotlinPlaceHolderStubFactory<KtStringTemplateExpression>(this) {
+        override fun shouldCreateStub(node: ASTNode): Boolean {
+            return StubUtils.isDeclaredInsideValueArgument(node) && super.shouldCreateStub(node)
         }
-
-        return super.shouldCreateStub(node)
     }
+
+    override fun getStubFactory(): StubElementFactory<KotlinPlaceHolderStubImpl<KtStringTemplateExpression>, KtStringTemplateExpression> =
+        stubFactory
 }
