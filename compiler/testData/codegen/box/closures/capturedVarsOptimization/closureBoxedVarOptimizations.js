41aeeabcb9979e81df64e19bf670afd7e70625af
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-06-23 20:43:11 +0200

    [JS] Better naming for value class impl initializers
    These initializers are generated as static methods, which are later
    lifted to the top level by `StaticMembersLowering`, which also
    prefixes their names with the name of the original container.
    Thus it doesn't make sense to do it in `InlineClassDeclarationLowering`.

--- compiler/testData/codegen/box/closures/capturedVarsOptimization/closureBoxedVarOptimizations.js
+++ compiler/testData/codegen/box/closures/capturedVarsOptimization/closureBoxedVarOptimizations.js
@@ -27,34 +27,34 @@ function captureVarInLocalClassInInlineLambda() {
   var long = {_v: new Long(4, 0)};
   var float = {_v: 5.0};
   var double = {_v: 6.0};
-  var char = {_v: new Char(_Char___init__impl__6a9atx(97))};
+  var char = {_v: new Char(Char$__init__impl__fq95zl(97))};
   var boolean = {_v: true};
   // Inline function 'run' call
   (new captureVarInLocalClassInInlineLambda$1(any, byte, short, int, long, float, double, char, boolean)).foo_i36xoq_k$();
 }
 function captureValueClassVar() {
-  var any = {_v: new AnyWrapper(_AnyWrapper___init__impl__jd5khy(new Object()))};
-  var byte = {_v: new ByteWrapper(_ByteWrapper___init__impl__bcpnw2(1))};
-  var short = {_v: new ShortWrapper(_ShortWrapper___init__impl__bdjki2(2))};
-  var int = {_v: new IntWrapper(_IntWrapper___init__impl__y2azvh(3))};
-  var long = {_v: new LongWrapper(_LongWrapper___init__impl__et53uq(new Long(4, 0)))};
-  var float = {_v: new FloatWrapper(_FloatWrapper___init__impl__87f78q(5.0))};
-  var double = {_v: new DoubleWrapper(_DoubleWrapper___init__impl__um2ad3(6.0))};
-  var char = {_v: new CharWrapper(_CharWrapper___init__impl__iwctd0(_Char___init__impl__6a9atx(97)))};
-  var boolean = {_v: new BooleanWrapper(_BooleanWrapper___init__impl__qaxidm(true))};
+  var any = {_v: new AnyWrapper(AnyWrapper$__init__impl__qih0n2(new Object()))};
+  var byte = {_v: new ByteWrapper(ByteWrapper$__init__impl__4fcome(1))};
+  var short = {_v: new ShortWrapper(ShortWrapper$__init__impl__ibjnda(2))};
+  var int = {_v: new IntWrapper(IntWrapper$__init__impl__8wtj1d(3))};
+  var long = {_v: new LongWrapper(LongWrapper$__init__impl__lqi34e(new Long(4, 0)))};
+  var float = {_v: new FloatWrapper(FloatWrapper$__init__impl__f5fa3y(5.0))};
+  var double = {_v: new DoubleWrapper(DoubleWrapper$__init__impl__wx5l6l(6.0))};
+  var char = {_v: new CharWrapper(CharWrapper$__init__impl__byzu3c(Char$__init__impl__fq95zl(97)))};
+  var boolean = {_v: new BooleanWrapper(BooleanWrapper$__init__impl__1oe9ja(true))};
   run2(captureValueClassVar$lambda(any, byte, short, int, long, float, double, char, boolean));
 }
 function captureValueClassVar$lambda($any, $byte, $short, $int, $long, $float, $double, $char, $boolean) {
   return function () {
-    $any._v = new AnyWrapper(_AnyWrapper___init__impl__jd5khy(null));
-    $byte._v = new ByteWrapper(_ByteWrapper___init__impl__bcpnw2(101));
-    $short._v = new ShortWrapper(_ShortWrapper___init__impl__bdjki2(102));
-    $int._v = new IntWrapper(_IntWrapper___init__impl__y2azvh(103));
-    $long._v = new LongWrapper(_LongWrapper___init__impl__et53uq(new Long(104, 0)));
-    $float._v = new FloatWrapper(_FloatWrapper___init__impl__87f78q(105.0));
-    $double._v = new DoubleWrapper(_DoubleWrapper___init__impl__um2ad3(106.0));
-    $char._v = new CharWrapper(_CharWrapper___init__impl__iwctd0(_Char___init__impl__6a9atx(98)));
-    $boolean._v = new BooleanWrapper(_BooleanWrapper___init__impl__qaxidm(false));
+    $any._v = new AnyWrapper(AnyWrapper$__init__impl__qih0n2(null));
+    $byte._v = new ByteWrapper(ByteWrapper$__init__impl__4fcome(101));
+    $short._v = new ShortWrapper(ShortWrapper$__init__impl__ibjnda(102));
+    $int._v = new IntWrapper(IntWrapper$__init__impl__8wtj1d(103));
+    $long._v = new LongWrapper(LongWrapper$__init__impl__lqi34e(new Long(104, 0)));
+    $float._v = new FloatWrapper(FloatWrapper$__init__impl__f5fa3y(105.0));
+    $double._v = new DoubleWrapper(DoubleWrapper$__init__impl__wx5l6l(106.0));
+    $char._v = new CharWrapper(CharWrapper$__init__impl__byzu3c(Char$__init__impl__fq95zl(98)));
+    $boolean._v = new BooleanWrapper(BooleanWrapper$__init__impl__1oe9ja(false));
     return Unit$getInstance();
   };
 }
