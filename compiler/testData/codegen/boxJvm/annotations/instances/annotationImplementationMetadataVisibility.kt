7ba14c2c55eec3eb9563f895c7d4680f2c7fe297
aleksandr.shalygin <aleksandr.shalygin@jetbrains.com>
2026-06-24 14:11:34 +0200

    [Tests] more cases for Public ABI flag and visibility
    KT-84802, KT-86464
    
    # Conflicts:
    #	compiler/testData/codegen/boxJvm/callableReference/callableReferenceMetadataVisibility.kt
    #	compiler/testData/codegen/boxJvm/reflection/lambdaClasses/lambdaMetadataVisibility.kt

--- compiler/testData/codegen/boxJvm/annotations/instances/annotationImplementationMetadataVisibility.kt
+++ compiler/testData/codegen/boxJvm/annotations/instances/annotationImplementationMetadataVisibility.kt
@@ -4,20 +4,56 @@
 private const val SYNTHETIC_CLASS_VISIBILITY_SHIFT = 8
 private const val SYNTHETIC_CLASS_VISIBILITY_MASK = 0b111
 private const val LOCAL_VISIBILITY = 5
+private const val PUBLIC_ABI_FLAG = 1 shl 7
 
 private fun syntheticClassVisibility(javaClass: Class<*>): Int {
     val extraInt = javaClass.getAnnotation(Metadata::class.java).extraInt
     return (extraInt shr SYNTHETIC_CLASS_VISIBILITY_SHIFT) and SYNTHETIC_CLASS_VISIBILITY_MASK
 }
+private fun metadataExtraInt(javaClass: Class<*>): Int =
+    javaClass.getAnnotation(Metadata::class.java).extraInt
+
+private fun isPublicAbi(javaClass: Class<*>): Boolean =
+    metadataExtraInt(javaClass) and PUBLIC_ABI_FLAG != 0
 
 annotation class Ann(val value: String)
+annotation class InlineAnn(val value: String)
+annotation class PrivateInlineAnn(val value: String)
+
+inline fun createAnn(): Class<*> = InlineAnn("OK").javaClass
+
+private inline fun privateCreateAnn(): Class<*> = PrivateInlineAnn("OK").javaClass
 
 fun box(): String {
     val ann = Ann("OK")
+    val inlineAnn = createAnn()
+    val privateInlineAnn = privateCreateAnn()
 
-    val visibility = syntheticClassVisibility(ann.javaClass)
+    var visibility = syntheticClassVisibility(ann.javaClass)
     if (visibility != LOCAL_VISIBILITY) {
         return "Fail: expected LOCAL visibility (5), got $visibility"
+    }
+
+    if (isPublicAbi(ann.javaClass)) {
+        return "Fail: expected annotation implementation class to NOT be public ABI in non-inline context"
+    }
+
+    visibility = syntheticClassVisibility(createAnn())
+    if (visibility != LOCAL_VISIBILITY) {
+        return "Fail: expected LOCAL visibility (5), got $visibility"
+    }
+
+    if (!isPublicAbi(createAnn())) {
+        return "Fail: expected annotation implementation class to be public ABI in inline context"
+    }
+
+    visibility = syntheticClassVisibility(privateCreateAnn())
+    if (visibility != LOCAL_VISIBILITY) {
+        return "Fail: expected LOCAL visibility (5), got $visibility"
+    }
+
+    if (isPublicAbi(privateCreateAnn())) {
+        return "Fail: expected annotation implementation class to NOT be public ABI in private inline context"
     }
 
     return ann.value
