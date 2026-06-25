6baded84d664f3ddabb9656118a9dd6f300e9e4f
Nataliya.Valtman <nataliya.valtman@jetbrains.com>
2026-06-25 13:41:34 +0200

    Use playwright as npm dependency
    ^KT-86449

--- libraries/tools/kotlin-gradle-plugin/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin/build.gradle.kts
@@ -213,8 +213,6 @@ dependencies {
     commonImplementation(project(":compiler:build-tools:kotlin-build-statistics"))
     commonImplementation(project(":kotlin-util-klib-metadata")) // TODO: consider removing in KT-70247
 
-    commonImplementation(libs.playwrigt)
-
     commonRuntimeOnly(project(":kotlin-compiler-runner")) { // TODO: consider removing in KT-70247
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler-embeddable")
     }
@@ -242,6 +240,13 @@ dependencies {
     embedded(project(":kotlin-tooling-metadata")) { isTransitive = false }
     embedded("com.github.gundy:semver4j:0.16.4:nodeps") {
         exclude(group = "*")
+    }
+
+    commonCompileOnly(libs.playwrigt) {
+        exclude(group = "com.microsoft.playwright", module = "driver-bundle")
+    }
+    embedded(libs.playwrigt) {
+        exclude(group = "com.microsoft.playwright", module = "driver-bundle")
     }
 
     commonCompileOnly(libs.apache.commons.compress)
@@ -412,6 +417,8 @@ tasks {
                 exclusions.forEach { exclude(it) }
             }
         }
+
+        relocate("com.microsoft.playwright", "${baseTargetPackage}.com.microsoft.playwright")
 
         /*
         Disable Kotlin Module remapping to allow our own 'KotlinModuleMetadataVersionBasedSkippingTransformer' to run
