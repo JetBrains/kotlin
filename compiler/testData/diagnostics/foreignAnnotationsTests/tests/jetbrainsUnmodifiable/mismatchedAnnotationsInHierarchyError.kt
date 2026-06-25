0e02b9b7d8e0138edb2955cec9813aaac175caf6
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-06-25 15:28:56 +0200

    [Tests] Reproduce #KT-86655

---
+++ compiler/testData/diagnostics/foreignAnnotationsTests/tests/jetbrainsUnmodifiable/mismatchedAnnotationsInHierarchyError.kt
@@ -0,0 +1,31 @@
+// FULL_JDK
+// NULLABILITY_ANNOTATIONS: @org.jetbrains.annotations.Unmodifiable:strict
+
+// FILE: org/jetbrains/annotations/Unmodifiable.java
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE_USE})
+public @interface Unmodifiable {}
+
+// FILE: A.java
+import org.jetbrains.annotations.Unmodifiable;
+import java.util.List;
+
+public class A {
+    public @Unmodifiable List<String> foo() { return null; }
+}
+
+// FILE: B.java
+import org.jetbrains.annotations.Unmodifiable;
+import java.util.List;
+
+public class B extends A {
+    @Override public List<String> foo() { return null; }
+}
+
+// FILE: test.kt
+fun test(b: B) {
+    val list: MutableList<String> <!INITIALIZER_TYPE_MISMATCH!>=<!> b.foo()
+}
