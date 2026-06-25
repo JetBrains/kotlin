0562bdb90c6e0beb87a2bdcd90d2072ed8929ccb
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-06-25 10:09:05 +0200

    [stdlib] Add throwNoWhenBranchMatchedException
    It's a helper method that will be invoked from generated else branches
    in exhaustive whens.
    On JS, it uses `subject::class.simpleName ?: subject` because the
    default implementation of `toString` returns [object Object].
    On all other platforms, it just uses the `toString` representation
    of the subject.
    
    #KT-86518

---
+++ libraries/stdlib/js/src/kotlin/internal/throwNoWhenBranchMatchedException.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package kotlin.internal
+
+@PublishedApi
+@UsedFromCompilerGeneratedCode
+@SinceKotlin("2.5")
+internal actual fun throwNoWhenBranchMatchedException(subject: Any): Nothing {
+    throw NoWhenBranchMatchedException("No branch matched for subject: ${subject::class.simpleName ?: subject}")
+}
