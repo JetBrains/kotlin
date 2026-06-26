757279bbd7a76ef6f7332e3f70f74c2bdeb57ae9
Simon Barinka <simon.barinka@jetbrains.com>
2026-06-26 17:05:24 +0200

    Normalize away the empty category enabled warning

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/util/LLDBSessionSpec.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/util/LLDBSessionSpec.kt
@@ -49,6 +49,7 @@ abstract class LLDBSessionSpec {
             val breakpointOffset = Regex("""(Breakpoint .* \+ )\d+( at)""")
             val targetStoppedLine = Regex("""Target \d+: .* stopped\.\n""")
             val setFormatLine = Regex("""\(lldb\) settings set .*-format .*\n""")
+            val emptyCategoryWarning = Regex("""warning: empty category enabled \(typo\?\)""")
 
             return lldbOutput
                 .replace(executablePathRegexp, "<path to executable>")
@@ -60,6 +61,7 @@ abstract class LLDBSessionSpec {
                 .replace(breakpointOffset, "$1<breakpoint offset>$2")
                 .replace(targetStoppedLine, "")
                 .replace(setFormatLine, "")
+                .replace(emptyCategoryWarning, "")
         }
     }
 }
