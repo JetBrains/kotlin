4e65a94ee0f1f93e60da251ed2e7e141cb5a0cde
Simon Barinka <simon.barinka@jetbrains.com>
2026-06-26 13:56:04 +0200

    Add cache versions of canInspectValuesOfPrimitiveTypesWithCache

--- native/native.tests/testData/lldb/canInspectValuesOfPrimitiveTypesWithCache.kt
+++ native/native.tests/testData/lldb/canInspectValuesOfPrimitiveTypesWithCache.kt
@@ -1,10 +1,11 @@
 // KIND: STANDALONE_LLDB
-// IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE
-// IGNORE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE
-// IGNORE_NATIVE: cacheMode=STATIC_USE_HEADERS_EVERYWHERE
 // FIR_IDENTICAL
+// IGNORE_NATIVE: cacheMode=NO
+// IGNORE_NATIVE: cacheMode=STATIC_ONLY_DIST
+// IGNORE_NATIVE: cacheMode=STATIC_EVERYWHERE
+// IGNORE_NATIVE: cacheMode=STATIC_USE_HEADERS_EVERYWHERE
 // INPUT_DATA_FILE: canInspectValuesOfPrimitiveTypes.in
-// OUTPUT_DATA_FILE: canInspectValuesOfPrimitiveTypes.out
+// OUTPUT_DATA_FILE: canInspectValuesOfPrimitiveTypesWithCache.out
 fun main(args: Array<String>) {
     var a: Byte =  1
     var b: Int  =  2
