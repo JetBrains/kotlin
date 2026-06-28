a53b3c96336824c5b419fafb53cae8bac488d929
Egor Andreevich <egor.andreevich@jetbrains.com>
2026-06-28 08:59:55 +0200

    Log Throwable

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/playwright/PlaywrightTestExecutor.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/playwright/PlaywrightTestExecutor.kt
@@ -79,6 +79,7 @@ internal class PlaywrightTestExecutor() : TestExecuter<PwExecutionSpec> {
                                 } catch (t: Throwable) {
                                     val tsEnd = System.currentTimeMillis()
                                     closeSuiteWithFailingTestCause(suiteNode = this, tsEnd, failingTestCause = t)
+                                    log.error("Here's the Throwable we caught", t)
                                 }
                             }
                         }
