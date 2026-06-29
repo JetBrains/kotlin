a926c7d00a9c16c23761e3034e10208939022c7b
Simon Barinka <simon.barinka@jetbrains.com>
2026-06-29 17:16:47 +0200

    Adjust canInspectValuesOfPrimitiveTypes

--- native/native.tests/testData/lldb/canInspectValuesOfPrimitiveTypes.kt
+++ native/native.tests/testData/lldb/canInspectValuesOfPrimitiveTypes.kt
@@ -1,8 +1,10 @@
 // KIND: STANDALONE_LLDB
-// IGNORE_NATIVE: cacheMode=STATIC_PER_FILE_EVERYWHERE
 // FIR_IDENTICAL
 // INPUT_DATA_FILE: canInspectValuesOfPrimitiveTypes.in
 // OUTPUT_DATA_FILE: canInspectValuesOfPrimitiveTypes.out
+
+
+
 fun main(args: Array<String>) {
     var a: Byte =  1
     var b: Int  =  2
