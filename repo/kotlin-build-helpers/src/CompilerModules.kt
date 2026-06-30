df98499f67c91c2cc6d6ceb7a57719b259d92a70
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-06-30 20:02:43 +0200

    [JS] Move `:js:js.frontend.common` to `:compiler:resolution.common.js`
    The `:js:js.frontend.common` only contained a single file
    with an implementation of `TypeSpecificityComparator`.
    Other implementations of `TypeSpecificityComparator` reside in
    `:compiler:resolution.common` and `:compiler:resolution.common.jvm`,
    so it makes sense to use the same directory structure for
    `JsTypeSpecificityComparator` too.

--- repo/kotlin-build-helpers/src/CompilerModules.kt
+++ repo/kotlin-build-helpers/src/CompilerModules.kt
@@ -32,6 +32,7 @@ object CompilerModules {
         ":compiler:compiler.version",
         ":compiler:resolution.common",
         ":compiler:resolution.common.jvm",
+        ":compiler:resolution.common.js",
         ":compiler:backend.common.jvm",
         ":compiler:plugin-api",
         ":core:metadata",
@@ -58,7 +59,6 @@ object CompilerModules {
         ":kotlin-util-klib-metadata",
         ":compiler:build-tools:kotlin-build-statistics",
         ":js:js.config",
-        ":js:js.frontend.common",
         ":js:js.ast", // used by js fir checkers and js backend
         ":wasm:wasm.config",
         ":native:base",
