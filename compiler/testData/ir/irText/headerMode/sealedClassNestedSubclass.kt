24dce72e653647cc27164b38c7ea6af036745046
Jose Enrique Estremadoyro Fort <joseefort@google.com>
2026-07-01 18:35:11 +0000

    [FIR] Do not prune public classes inside a private sealed class

---
+++ compiler/testData/ir/irText/headerMode/sealedClassNestedSubclass.kt
@@ -0,0 +1,25 @@
+// FIR_PARSER: LightTree
+// TARGET_BACKEND: JVM_IR
+// MODULE: lib
+// HEADER_MODE
+// FILE: first.kt
+package lib
+
+private sealed class SealedClass {
+    class Implementation : SealedClass()
+}
+
+fun myFunc() {
+    val impl = SealedClass.Implementation()
+}
+
+// MODULE: main(lib)
+// FILE: main.kt
+package main
+import lib.myFunc
+
+class App {
+    fun run() {
+        myFunc()
+    }
+}
---
+++ compiler/testData/ir/irText/headerMode/sealedInterfaceTopLevelSubclass.kt
@@ -0,0 +1,25 @@
+// FIR_PARSER: LightTree
+// TARGET_BACKEND: JVM_IR
+// MODULE: lib
+// HEADER_MODE
+// FILE: first.kt
+package lib
+
+private sealed interface SealedInterface
+
+class TopLevelImplementation : SealedInterface
+
+fun myFunc() {
+    val impl = TopLevelImplementation()
+}
+
+// MODULE: main(lib)
+// FILE: main.kt
+package main
+import lib.myFunc
+
+class App {
+    fun run() {
+        myFunc()
+    }
+}
