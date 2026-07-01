312ea07cbf693c4c8794ce5a9739ac8dd41635e4
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-07-01 11:43:03 +0000

    [KLIB Tool] Add CLI test on KLIB tool usage
    There never been such test before. It would be helpful to check
    how exactly KLIB tool's CLI behaves.
    
    ^KT-87208

---
+++ native/native.tests/cli-tests/testFixtures/org/jetbrains/kotlin/konan/test/cli/AbstractKlibToolCliTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.konan.test.cli
+
+import org.jetbrains.kotlin.cli.AbstractCliTest
+import org.jetbrains.kotlin.konan.test.blackbox.support.RegularKotlinNativeClassLoader
+import org.jetbrains.kotlin.konan.test.blackbox.support.copyNativeHomeProperty
+import org.jetbrains.kotlin.konan.test.blackbox.support.util.invokeKlibTool
+import org.jetbrains.kotlin.test.TestDataAssertions.assertEqualsToFile
+import java.io.File
+
+abstract class AbstractKlibToolCliTest : AbstractCliTest() {
+    private val kotlinNativeClassLoader by lazy {
+        copyNativeHomeProperty()
+        RegularKotlinNativeClassLoader.kotlinNativeClassLoader.classLoader
+    }
+
+    fun doKlibToolTest(fileName: String) {
+        val actual = invokeKlibTool(kotlinNativeClassLoader, args = readArgs(fileName, tmpdir.path))
+
+        val outFile = File(fileName.replaceFirst("\\.args$".toRegex(), ".out"))
+        assertEqualsToFile(outFile, actual)
+    }
+}
