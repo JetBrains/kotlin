a8e2d0854d15218d70a330e24e5a4775fa3b6080
Ilmir Usmanov <me@ilmir.us>
2026-07-01 16:14:49 +0000

    JVM: Generate no-arg constructor if all parameters are default
    Even if some of the parameters are inline classes.
    
    It was previously covered by @JvmExposeBoxed, this change extends the
    support for all inline classes.
    
    This is safe because none of the inline classes are essentially exposed
    to Java. Additionally, it simplifies mental model - no-arg constructor
    is generated if all parameters are default and one does not need to
    remember to use @JvmExposeBoxed.
    
     #KT-78623 Fixed

---
+++ compiler/testData/codegen/boxJvm/inlineClasses/noArg/introducedAtIndependent.kt
@@ -0,0 +1,27 @@
+// WITH_STDLIB
+// TARGET_BACKEND: JVM_IR
+
+// FILE: Test.kt
+@file:OptIn(ExperimentalStdlibApi::class, ExperimentalVersionOverloading::class)
+
+@JvmInline
+value class Token(val value: String)
+
+class Holder(
+    val token: Token = Token("O"),
+    @IntroducedAt(version = "3") val text: String = "K",
+)
+
+// FILE: Main.java
+public class Main {
+    public Holder create() {
+        return new Holder();
+    }
+}
+
+// FILE: box.kt
+fun box(): String {
+    val holder = Main().create()
+    return holder.token.value + holder.text
+}
+
