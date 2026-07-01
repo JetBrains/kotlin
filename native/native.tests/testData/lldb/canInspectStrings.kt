b493b278a78be1ccda3619899260fab904c09f87
Simon Barinka <simon.barinka@jetbrains.com>
2026-07-01 15:47:50 +0200

    Adjust canInspectStrings

--- native/native.tests/testData/lldb/canInspectStrings.kt
+++ native/native.tests/testData/lldb/canInspectStrings.kt
@@ -5,6 +5,9 @@
 // FIR_IDENTICAL
 // INPUT_DATA_FILE: canInspectStrings.in
 // OUTPUT_DATA_FILE: canInspectStrings.out
+
+
+
 fun main(args: Array<String>) {
     val a = "string literal"
     val b = buildString {
