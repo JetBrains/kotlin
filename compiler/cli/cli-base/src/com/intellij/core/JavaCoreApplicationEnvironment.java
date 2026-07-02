372373a37f36e287facdab42974eac281100880a
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-07-02 17:07:42 +0200

    Compiler: cleanup hijacked JavaCoreApplicationEnvironment
    This commit removes inaccessible MethodSuperSearcher registration,
    adds CommonElementTypeConverterFactory and Registry.markAsLoaded
    to remove runtime errors, removes JavaParserDefinition as it's
    deprecated and JavaSyntaxDefinitionExtension is already with us,
    and makes some more minor cleanups.
    
    Done as a part of KT-82657

--- compiler/cli/cli-base/src/com/intellij/core/JavaCoreApplicationEnvironment.java
+++ compiler/cli/cli-base/src/com/intellij/core/JavaCoreApplicationEnvironment.java
@@ -17,7 +17,6 @@ import com.intellij.java.frontback.psi.impl.syntax.JavaSyntaxDefinitionExtension
 import com.intellij.lang.LanguageASTFactory;
 import com.intellij.lang.folding.LanguageFolding;
 import com.intellij.lang.java.JavaLanguage;
-import com.intellij.lang.java.JavaParserDefinition;
 import com.intellij.lang.java.syntax.JavaElementTypeConverterExtension;
 import com.intellij.navigation.ItemPresentationProviders;
 import com.intellij.openapi.Disposable;
@@ -27,25 +26,8 @@ import com.intellij.openapi.fileTypes.PlainTextLanguage;
 import com.intellij.openapi.fileTypes.PlainTextParserDefinition;
 import com.intellij.openapi.projectRoots.JavaVersionService;
 import com.intellij.openapi.util.registry.Registry;
-import com.intellij.platform.syntax.psi.ElementTypeConverters;
-import com.intellij.platform.syntax.psi.LanguageSyntaxDefinitions;
-import com.intellij.platform.syntax.psi.PsiSyntaxBuilderFactory;
-import com.intellij.platform.syntax.psi.PsiSyntaxBuilderFactoryImpl;
-import com.intellij.psi.ClassFileViewProviderFactory;
-import com.intellij.psi.DumbJavaModuleGraphHelper;
-import com.intellij.psi.FileTypeFileViewProviders;
-import com.intellij.psi.JavaDirectoryService;
-import com.intellij.psi.JavaModuleGraphHelper;
-import com.intellij.psi.PsiAnonymousClass;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiLocalVariable;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiNewExpression;
-import com.intellij.psi.PsiPackage;
-import com.intellij.psi.PsiParameter;
-import com.intellij.psi.PsiSubstitutorFactory;
+import com.intellij.platform.syntax.psi.*;
+import com.intellij.psi.*;
 import com.intellij.psi.compiled.ClassFileDecompilers;
 import com.intellij.psi.impl.LanguageConstantExpressionEvaluator;
 import com.intellij.psi.impl.PsiExpressionEvaluator;
@@ -54,14 +36,9 @@ import com.intellij.psi.impl.compiled.ClassFileStubBuilder;
 import com.intellij.psi.impl.compiled.ClsDecompilerImpl;
 import com.intellij.psi.impl.file.PsiPackageImplementationHelper;
 import com.intellij.psi.impl.java.stubs.JavaStubRegistryExtension;
-import com.intellij.psi.impl.search.MethodSuperSearcher;
 import com.intellij.psi.impl.source.tree.JavaASTFactory;
 import com.intellij.psi.impl.source.tree.PlainTextASTFactory;
-import com.intellij.psi.presentation.java.ClassPresentationProvider;
-import com.intellij.psi.presentation.java.FieldPresentationProvider;
-import com.intellij.psi.presentation.java.MethodPresentationProvider;
-import com.intellij.psi.presentation.java.PackagePresentationProvider;
-import com.intellij.psi.presentation.java.VariablePresentationProvider;
+import com.intellij.psi.presentation.java.*;
 import com.intellij.psi.search.searches.SuperMethodsSearch;
 import com.intellij.psi.stubs.BinaryFileStubBuilders;
 import com.intellij.psi.stubs.StubElementRegistryServiceImplKt;
@@ -75,7 +52,7 @@ import org.jetbrains.annotations.NotNull;
 // Shortly, when Kotlin is compiled against JDK 21, we may drop this file.
 // Or, when this class is out from java-indexing-impl and compiled against JDK 1.8, we may drop this file.
 
-@SuppressWarnings("UnusedDeclaration") // Used in Kotlin Compiler
+@SuppressWarnings("UnstableApiUsage")
 public class JavaCoreApplicationEnvironment extends CoreApplicationEnvironment {
     public JavaCoreApplicationEnvironment(@NotNull Disposable parentDisposable) {
         this(parentDisposable, true);
@@ -87,6 +64,7 @@ public class JavaCoreApplicationEnvironment extends CoreApplicationEnvironment {
         registerFileType(JavaClassFileType.INSTANCE, "class");
         registerFileType(JavaFileType.INSTANCE, "java");
         registerFileType(ArchiveFileType.INSTANCE, "jar;zip");
+        Registry.markAsLoaded();
         Registry.get("java.highest.language.level").setValue("26");
         registerFileType(PlainTextFileType.INSTANCE, "txt;sh;bat;cmd;policy;log;cgi;MF;jad;jam;htaccess");
 
@@ -99,8 +77,8 @@ public class JavaCoreApplicationEnvironment extends CoreApplicationEnvironment {
 
         addExplicitExtension(LanguageASTFactory.INSTANCE, JavaLanguage.INSTANCE, new JavaASTFactory());
         addExplicitExtension(LanguageSyntaxDefinitions.getINSTANCE(), JavaLanguage.INSTANCE, new JavaSyntaxDefinitionExtension());
+        addExplicitExtension(ElementTypeConverters.getInstance(), JavaLanguage.INSTANCE, new CommonElementTypeConverterFactory());
         addExplicitExtension(ElementTypeConverters.getInstance(), JavaLanguage.INSTANCE, new JavaElementTypeConverterExtension());
-        registerParserDefinition(new JavaParserDefinition());
         addExplicitExtension(LanguageConstantExpressionEvaluator.INSTANCE, JavaLanguage.INSTANCE, new PsiExpressionEvaluator());
 
         registerApplicationExtensionPoint(ContainerProvider.EP_NAME, ContainerProvider.class);
@@ -136,7 +114,6 @@ public class JavaCoreApplicationEnvironment extends CoreApplicationEnvironment {
         });
 
         registerApplicationExtensionPoint(SuperMethodsSearch.EP_NAME, QueryExecutor.class);
-        addExtension(SuperMethodsSearch.EP_NAME, new MethodSuperSearcher());
 
         registerApplicationDynamicExtensionPoint("com.intellij.filetype.decompiler", BinaryFileTypeDecompilers.class);
         registerApplicationDynamicExtensionPoint("com.intellij.psi.classFileDecompiler", ClassFileDecompilers.Decompiler.class);
@@ -144,7 +121,7 @@ public class JavaCoreApplicationEnvironment extends CoreApplicationEnvironment {
     }
 
     // overridden in upsource
-    protected CoreJavaDirectoryService createJavaDirectoryService() {
+    private static CoreJavaDirectoryService createJavaDirectoryService() {
         return new CoreJavaDirectoryService();
     }
 }
