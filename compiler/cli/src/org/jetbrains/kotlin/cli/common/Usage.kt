f7a92bc9734cc3882eeeff4f1b0fba959d9676f6
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-07-03 16:28:33 +0000

    [CLI] Convert `Usage.java` to `Usage.kt` with J2K

--- compiler/cli/src/org/jetbrains/kotlin/cli/common/Usage.kt
+++ compiler/cli/src/org/jetbrains/kotlin/cli/common/Usage.kt
@@ -13,138 +13,132 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.jetbrains.kotlin.cli.common
 
-package org.jetbrains.kotlin.cli.common;
+import com.intellij.openapi.util.SystemInfo
+import org.jetbrains.kotlin.cli.common.arguments.ARGFILE_ARGUMENT
+import org.jetbrains.kotlin.cli.common.arguments.ArgumentField
+import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
+import org.jetbrains.kotlin.cli.common.arguments.getArgumentsInfo
+import org.jetbrains.kotlin.cli.common.arguments.isAdvanced
+import org.jetbrains.kotlin.cli.common.arguments.isInternal
 
-import com.intellij.openapi.util.SystemInfo;
-import kotlin.KotlinVersion;
-import kotlin.text.StringsKt;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.cli.common.arguments.*;
-
-import java.util.stream.Stream;
-
-import static org.jetbrains.kotlin.cli.common.UtilsKt.parseKotlinVersion;
-import static org.jetbrains.kotlin.cli.common.arguments.ParseCommandLineArgumentsKt.getArgumentsInfo;
-
-public class Usage {
-    public static final String BAT_DELIMITER_CHARACTERS_NOTE =
-            "Note: on Windows, arguments that contain delimiter characters (whitespace, =, ;, ,) need to be surrounded with double quotes (\").";
+object Usage {
+    const val BAT_DELIMITER_CHARACTERS_NOTE: String =
+        "Note: on Windows, arguments that contain delimiter characters (whitespace, =, ;, ,) need to be surrounded with double quotes (\")."
 
     // The magic number 29 corresponds to the similar padding width in javac and scalac command line compilers
-    private static final int OPTION_NAME_PADDING_WIDTH = 29;
-    private static final String PADDING_STRING = StringsKt.repeat(" ", OPTION_NAME_PADDING_WIDTH);
+    private const val OPTION_NAME_PADDING_WIDTH = 29
+    private val PADDING_STRING = " ".repeat(OPTION_NAME_PADDING_WIDTH)
 
-    @NotNull
-    public static <A extends CommonCompilerArguments> String render(@NotNull CLICompiler<A> compiler, @NotNull A arguments) {
-        boolean extraHelp = arguments.getExtraHelp();
-        StringBuilder sb = new StringBuilder();
-        appendln(sb, "Usage: " + compiler.executableScriptFileName() + " <options> <source files>");
-        appendln(sb, "where " + (extraHelp ? "advanced" : "possible") + " options include:");
+    fun <A : CommonCompilerArguments> render(compiler: CLICompiler<A>, arguments: A): String {
+        val extraHelp = arguments.extraHelp
+        val sb = StringBuilder()
+        appendln(sb, "Usage: " + compiler.executableScriptFileName() + " <options> <source files>")
+        appendln(sb, "where " + (if (extraHelp) "advanced" else "possible") + " options include:")
 
         // Use distinct because same arguments can be bound to different names (shortName, deprecatedName)
-        Stream<ArgumentField> argumentFields = getArgumentsInfo(arguments.getClass()).getCliArgNameToArguments().values().stream().distinct();
-        argumentFields.forEach(argumentField -> fieldUsage(sb, argumentField, extraHelp));
+        val argumentFields = getArgumentsInfo(arguments.javaClass).cliArgNameToArguments.values.distinct()
+        argumentFields.forEach { argumentField -> fieldUsage(sb, argumentField, extraHelp) }
 
         if (extraHelp) {
-            appendln(sb, "");
-            appendln(sb, "Advanced options are non-standard and may be changed or removed without any notice.");
-        }
-        else {
-            renderOptionJUsage(sb);
-            renderArgfileUsage(sb);
+            appendln(sb, "")
+            appendln(sb, "Advanced options are non-standard and may be changed or removed without any notice.")
+        } else {
+            renderOptionJUsage(sb)
+            renderArgfileUsage(sb)
         }
 
         if (SystemInfo.isWindows) {
-            appendln(sb, "");
-            appendln(sb, BAT_DELIMITER_CHARACTERS_NOTE);
+            appendln(sb, "")
+            appendln(sb, BAT_DELIMITER_CHARACTERS_NOTE)
         }
 
         if (!extraHelp) {
-            appendln(sb, "");
-            appendln(sb, "For details, see https://kotl.in/cli");
+            appendln(sb, "")
+            appendln(sb, "For details, see https://kotl.in/cli")
         }
 
-        return sb.toString();
+        return sb.toString()
     }
 
-    private static void fieldUsage(@NotNull StringBuilder sb, @NotNull ArgumentField argumentField, boolean extraHelp) {
-        Argument argument = argumentField.getArgument();
+    private fun fieldUsage(sb: StringBuilder, argumentField: ArgumentField, extraHelp: Boolean) {
+        val argument = argumentField.argument
 
-        if (argument.isObsolete()) return;
-        if (ParseCommandLineArgumentsKt.isInternal(argument)) return;
-        if (extraHelp != ParseCommandLineArgumentsKt.isAdvanced(argument)) return;
+        if (argument.isObsolete) return
+        if (argument.isInternal) return
+        if (extraHelp != argument.isAdvanced) return
 
-        int startLength = sb.length();
-        sb.append("  ");
-        sb.append(argument.value());
+        val startLength = sb.length
+        sb.append("  ")
+        sb.append(argument.value)
 
-        if (!argument.shortName().isEmpty()) {
-            sb.append(" (");
-            sb.append(argument.shortName());
-            sb.append(")");
+        if (!argument.shortName.isEmpty()) {
+            sb.append(" (")
+            sb.append(argument.shortName)
+            sb.append(")")
         }
 
-        if (!argument.valueDescription().isEmpty()) {
-            sb.append(ParseCommandLineArgumentsKt.isAdvanced(argument) ? "=" : " ");
-            sb.append(argument.valueDescription());
+        if (!argument.valueDescription.isEmpty()) {
+            sb.append(if (argument.isAdvanced) "=" else " ")
+            sb.append(argument.valueDescription)
         }
 
-        int margin = startLength + OPTION_NAME_PADDING_WIDTH - 1;
-        if (sb.length() >= margin + 5) { // Break the line if it's too long
-            sb.append("\n");
-            margin += sb.length() - startLength;
+        var margin = startLength + OPTION_NAME_PADDING_WIDTH - 1
+        if (sb.length >= margin + 5) { // Break the line if it's too long
+            sb.append("\n")
+            margin += sb.length - startLength
         }
-        while (sb.length() < margin) {
-            sb.append(" ");
+        while (sb.length < margin) {
+            sb.append(" ")
         }
 
-        sb.append(" ");
+        sb.append(" ")
 
-        appendln(sb, argument.description().replace("\n", "\n" + PADDING_STRING));
+        appendln(sb, argument.description.replace("\n", "\n" + PADDING_STRING))
 
-        kotlin.Deprecated deprecatedAnnotation = argumentField.getDeprecatedAnnotation();
+        val deprecatedAnnotation = argumentField.deprecatedAnnotation
         if (deprecatedAnnotation != null) {
-            sb.append(PADDING_STRING);
-            sb.append("The option is ");
+            sb.append(PADDING_STRING)
+            sb.append("The option is ")
             // The value is generated automatically based on KotlinReleaseVersion entries, thus it's expected to be always valid.
-            KotlinVersion argDeprecatedVersion = parseKotlinVersion(argument.deprecatedVersion());
-            boolean isAlreadyDeprecated = argDeprecatedVersion.compareTo(KotlinVersion.CURRENT) <= 0;
-            sb.append(isAlreadyDeprecated ? "deprecated since " : "will be deprecated in ");
-            sb.append("Kotlin ").append(argDeprecatedVersion).append('.');
+            val argDeprecatedVersion = parseKotlinVersion(argument.deprecatedVersion)
+            val isAlreadyDeprecated = argDeprecatedVersion <= KotlinVersion.CURRENT
+            sb.append(if (isAlreadyDeprecated) "deprecated since " else "will be deprecated in ")
+            sb.append("Kotlin ").append(argDeprecatedVersion).append('.')
             if (isAlreadyDeprecated) {
-                sb.append(" It will be removed in one of the future releases.");
+                sb.append(" It will be removed in one of the future releases.")
             }
-            String message = deprecatedAnnotation.message();
+            val message = deprecatedAnnotation.message
             if (!message.isEmpty()) {
-                sb.append(' ').append(message.replace("\n", "\n" + PADDING_STRING));
+                sb.append(' ').append(message.replace("\n", "\n" + PADDING_STRING))
             }
-            sb.append('\n');
+            sb.append('\n')
         }
     }
 
-    private static void renderOptionJUsage(@NotNull StringBuilder sb) {
-        int descriptionStart = sb.length() + OPTION_NAME_PADDING_WIDTH;
-        sb.append("  -J<option>");
-        while (sb.length() < descriptionStart) {
-            sb.append(" ");
+    private fun renderOptionJUsage(sb: StringBuilder) {
+        val descriptionStart = sb.length + OPTION_NAME_PADDING_WIDTH
+        sb.append("  -J<option>")
+        while (sb.length < descriptionStart) {
+            sb.append(" ")
         }
-        appendln(sb, "Pass an option directly to JVM.");
+        appendln(sb, "Pass an option directly to JVM.")
     }
 
-    private static void renderArgfileUsage(@NotNull StringBuilder sb) {
-        int descriptionStart = sb.length() + OPTION_NAME_PADDING_WIDTH;
-        sb.append("  ");
-        sb.append(PreprocessCommandLineArgumentsKt.ARGFILE_ARGUMENT);
-        sb.append("<argfile>");
-        while (sb.length() < descriptionStart) {
-            sb.append(" ");
+    private fun renderArgfileUsage(sb: StringBuilder) {
+        val descriptionStart = sb.length + OPTION_NAME_PADDING_WIDTH
+        sb.append("  ")
+        sb.append(ARGFILE_ARGUMENT)
+        sb.append("<argfile>")
+        while (sb.length < descriptionStart) {
+            sb.append(" ")
         }
-        appendln(sb, "Read compiler arguments and file paths from the given file.");
+        appendln(sb, "Read compiler arguments and file paths from the given file.")
     }
 
-    private static void appendln(@NotNull StringBuilder sb, @NotNull String string) {
-        sb.append(string);
-        sb.append('\n');
+    private fun appendln(sb: StringBuilder, string: String) {
+        sb.append(string)
+        sb.append('\n')
     }
 }
