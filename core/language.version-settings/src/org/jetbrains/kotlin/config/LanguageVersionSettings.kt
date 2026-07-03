067ce6a187cd36e2f94f320bbaeb9324287d2585
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-07-03 15:02:07 +0200

    Align `LATEST_STABLE` in `LanguageVersionSettings` with the current `KotlinVersion`
    It's always aligned with `KotlinVersion`
    according to its updating throughout git history.

--- core/language.version-settings/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
+++ core/language.version-settings/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -811,7 +811,9 @@ enum class LanguageVersion(val major: Int, val minor: Int) : DescriptionAware, L
         val FIRST_NON_DEPRECATED = KOTLIN_2_2
 
         @JvmField
-        val LATEST_STABLE = KOTLIN_2_5
+        val LATEST_STABLE = LanguageVersion.entries.single {
+            it.major == KotlinVersion.CURRENT.major && it.minor == KotlinVersion.CURRENT.minor
+        }
     }
 }
 
