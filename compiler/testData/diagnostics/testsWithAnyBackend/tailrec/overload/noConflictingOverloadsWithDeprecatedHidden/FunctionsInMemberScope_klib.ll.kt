26e6ea6c62d3a1f058f1a0b68de0a3143bb61841
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-07-06 00:49:14 +0300

    [Tests] Move tailrec diagnostics testdata to testsWithAnyBackend
    ^KT-87442

---
+++ compiler/testData/diagnostics/testsWithAnyBackend/tailrec/overload/noConflictingOverloadsWithDeprecatedHidden/FunctionsInMemberScope_klib.ll.kt
@@ -0,0 +1,20 @@
+// LL_FIR_DIVERGENCE
+// LL FIR diagnostics tests do not run backends, so backend-only diagnostics are not reported.
+// LL_FIR_DIVERGENCE
+// DONT_TARGET_EXACT_BACKEND: JVM_IR
+// RUN_PIPELINE_TILL: BACKEND
+// LATEST_LV_DIFFERENCE
+// IGNORE_DEXING
+
+class MemberScope {
+    @Deprecated(message = "", level = DeprecationLevel.HIDDEN) <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun testIdenticalPresenceOfTailrecModifier() {}
+    <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun testIdenticalPresenceOfTailrecModifier() {}
+
+    @Deprecated(message = "", level = DeprecationLevel.HIDDEN) <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun testDifferencesInTailrecModifierPresence() {}
+    fun testDifferencesInTailrecModifierPresence() {}
+
+    <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun testDifferencesInTailrecModifierPresenceReverse() {}
+    @Deprecated(message = "", level = DeprecationLevel.HIDDEN) fun testDifferencesInTailrecModifierPresenceReverse() {}
+}
+
+/* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, stringLiteral, tailrec */
