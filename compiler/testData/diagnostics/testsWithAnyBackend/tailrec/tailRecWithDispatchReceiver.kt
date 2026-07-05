f9abe237e22825b881a9bf03c6cc6fc38f4b8c56
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-07-06 00:49:14 +0300

    [Tests][IR] Clone JVM tailrec diagnostic tests to non-JVM diagnostic tests
    ^KT-87442

---
+++ compiler/testData/diagnostics/testsWithAnyBackend/tailrec/tailRecWithDispatchReceiver.kt
@@ -0,0 +1,24 @@
+// RUN_PIPELINE_TILL: BACKEND
+class A(val a: A) {
+    <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun foo1() {
+        a.<!NON_TAIL_RECURSIVE_CALL!>foo1<!>()
+    }
+
+    tailrec fun foo2() {
+        this.foo2()
+    }
+
+    tailrec fun foo3() {
+        foo3()
+    }
+
+    <!NO_TAIL_CALLS_FOUND!>tailrec<!> fun foo4() {
+        with(a) {
+            <!NON_TAIL_RECURSIVE_CALL!>foo4<!>()
+            return
+        }
+    }
+}
+
+/* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, lambdaLiteral, primaryConstructor, propertyDeclaration,
+tailrec, thisExpression */
