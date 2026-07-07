bc0118c691e69f64f916e867d2d4804716c280f4
Jose E. Fort <joseefort@google.com>
2026-07-07 23:29:32 +0000

    [JKLIB] Fix fake override by using JVM type and overrideability rules in JKLIB linker
    (cherry picked from commit d446d8e01203e7255a3cd22be79a9a0ec071d1e4)
    
    To correctly resolve fake overrides when JKLIB interacts with Java, the linker needs JVM-specific type rules and overridability conditions.
    
    To avoid coupling the JKLIB serialization module directly to the JVM backend, we inject `IrTypeSystemContext` and `IrExternalOverridabilityCondition` via the `JKlibIrLinker` constructor.
    
    This enables JVM override resolution at the CLI callsite and allows us to un-ignore and pass 12 previously failing fake override tests.

--- compiler/cli/cli-jklib/src/org/jetbrains/kotlin/cli/jklib/pipeline/JKlibIrCompilationPhase.kt
+++ compiler/cli/cli-jklib/src/org/jetbrains/kotlin/cli/jklib/pipeline/JKlibIrCompilationPhase.kt
@@ -14,6 +14,8 @@ import org.jetbrains.kotlin.backend.common.serialization.DescriptorByIdSignature
 import org.jetbrains.kotlin.backend.common.serialization.DeserializationStrategy
 import org.jetbrains.kotlin.backend.common.serialization.signature.IdSignatureDescriptor
 import org.jetbrains.kotlin.backend.jvm.JvmGeneratorExtensionsImpl
+import org.jetbrains.kotlin.backend.jvm.JvmIrTypeSystemContext
+import org.jetbrains.kotlin.backend.jvm.overrides.IrJavaIncompatibilityRulesOverridabilityCondition
 import org.jetbrains.kotlin.builtins.jvm.JvmBuiltInClassDescriptorFactory
 import org.jetbrains.kotlin.builtins.jvm.JvmBuiltIns
 import org.jetbrains.kotlin.builtins.jvm.JvmBuiltInsPackageFragmentProvider
@@ -118,6 +120,8 @@ object JKlibIrCompilationPhase :
             configuration = configuration,
             symbolTable = symbolTable,
             descriptorMangler = mangler,
+            typeSystemContextFactory = ::JvmIrTypeSystemContext,
+            externalOverridabilityConditions = listOf(IrJavaIncompatibilityRulesOverridabilityCondition()),
         )
 
         // Deserialize modules
