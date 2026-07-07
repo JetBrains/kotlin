85ea35e3dc3c100deeaba03ace8a105b011a46ec
Veronika Sirotkina <veronika.sirotkina@jetbrains.com>
2026-07-07 16:45:00 +0200

    Use ARM emulator

--- compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/CodegenTestsOnAndroidRunner.kt
+++ compiler/android-tests/tests/org/jetbrains/kotlin/android/tests/CodegenTestsOnAndroidRunner.kt
@@ -25,12 +25,13 @@ import java.util.Base64
 
 class CodegenTestsOnAndroidRunner private constructor(private val pathManager: PathManager) {
     private fun detectArch(): String {
-        val arch = System.getProperty("os.arch")?.toLowerCaseAsciiOnly() ?: return Emulator.X86
-
-        return when {
-            arch.startsWith("arm") || arch == "aarch64" -> Emulator.ARM
-            else -> Emulator.X86
-        }
+        return Emulator.ARM
+//        val arch = System.getProperty("os.arch")?.toLowerCaseAsciiOnly() ?: return Emulator.X86
+//
+//        return when {
+//            arch.startsWith("arm") || arch == "aarch64" -> Emulator.ARM
+//            else -> Emulator.X86
+//        }
     }
 
     private suspend fun runTestsInEmulator(): TestSuite {
