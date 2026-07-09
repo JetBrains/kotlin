1c4b26a41b4b68a9bcaeb28e904962793a36d6c1
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-07-09 06:32:18 +0000

    [Test] Reproduce KT-87507

---
+++ compiler/testData/diagnostics/tests/j+k/missingTypeOfParameterInFakeOverride_2.kt
@@ -0,0 +1,37 @@
+// ISSUE: KT-87507
+// RUN_PIPELINE_TILL: FRONTEND
+// MODULE: a
+// FILE: a/b/Some.kt
+package a.b
+
+class C {
+    class Some
+}
+
+// MODULE: b(a)
+// FILE: a/b/d/Base.java
+package a.b.d;
+
+import a.b.C.Some;
+
+public abstract class Base {
+    public abstract void foo(Some some);
+}
+
+// FILE: a/b/d/Derived.kt
+package a.b.d
+
+import a.b.C.Some
+
+open class Derived : Base() {
+    override fun foo(some: Some) {}
+}
+
+// MODULE: c(b)
+// ^no dependency on `a`, it's important
+
+import a.b.d.Derived
+
+class Impl : Derived()
+
+/* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, javaType, override */
