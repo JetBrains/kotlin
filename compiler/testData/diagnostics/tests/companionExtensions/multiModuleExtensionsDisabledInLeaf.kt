c35b0ab856854ce77604b49063ad14ec933f9c30
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-07-09 13:17:38 +0200

    [Tests] Add tests for split of Companion Blocks & Extensions LFs
    #KT-87436 Fixed

---
+++ compiler/testData/diagnostics/tests/companionExtensions/multiModuleExtensionsDisabledInLeaf.kt
@@ -0,0 +1,28 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// MODULE: m1
+// LANGUAGE: +CompanionBlocks +CompanionExtensions
+// FILE: m1.kt
+open class C
+
+companion fun C.foo() {}
+companion val C.bar = 1
+companion operator fun C.invoke(s: String) {}
+
+// MODULE: m2(m1)
+// LANGUAGE: +CompanionBlocks
+// FILE: m2.kt
+class D : <!SUPERTYPE_NOT_INITIALIZED!>C<!> {
+    fun test() {
+        <!UNRESOLVED_REFERENCE!>foo<!>()
+        <!UNRESOLVED_REFERENCE!>bar<!>
+    }
+}
+
+fun test() {
+    C.<!UNRESOLVED_REFERENCE!>foo<!>()
+    C.<!UNRESOLVED_REFERENCE!>bar<!>
+    C(<!TOO_MANY_ARGUMENTS!>""<!>)
+}
+
+/* GENERATED_FIR_TAGS: classDeclaration, funWithExtensionReceiver, functionDeclaration, integerLiteral, operator,
+propertyDeclaration, propertyWithExtensionReceiver */
