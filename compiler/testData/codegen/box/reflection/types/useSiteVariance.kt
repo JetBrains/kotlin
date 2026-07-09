515c2fb1cbfb18176cc62b015872524f2492287d
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2026-07-10 00:43:44 +0200

    [Test, Wasm, JS] Don't target Wasm and JS backends for tests using returnType on callable reference

--- compiler/testData/codegen/box/reflection/types/useSiteVariance.kt
+++ compiler/testData/codegen/box/reflection/types/useSiteVariance.kt
@@ -1,9 +1,5 @@
-// IGNORE_BACKEND: JS_IR
-// IGNORE_BACKEND: JS_IR_ES6
-// TODO: muted automatically, investigate should it be ran for JS or not
-// IGNORE_BACKEND: WASM_JS, WASM_WASI
-// IGNORE_IR_DESERIALIZATION_TEST: JS_IR
-// ^^^ Source code is not compiled in JS.
+// DONT_TARGET_EXACT_BACKEND: JS_IR, JS_IR_ES6, WASM_JS, WASM_WASI
+// ^ returnType is not supported
 
 // WITH_REFLECT
 
