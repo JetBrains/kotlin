7f2da5d503a930563421e968e332caf7a7b0b272
Maksim Verevkin <maksim.verevkin@jetbrains.com>
2026-07-14 17:07:13 +0300

    Add tests that reproduce KT-87073
    This tests fail with IR-validation error. IR dump shows the parameter
    for `exampleFun` and `exampleVal` that should not be there: `<this>: org.example.lib.C`.
    
    Without IR-validation this would result in an exception during IR lowering.
    
    ^KT-87073

---
+++ compiler/testData/ir/irText/stubs/companionExtension.kt
@@ -0,0 +1,27 @@
+// IGNORE_BACKEND: JKLIB
+// TARGET_BACKEND: JVM
+// LANGUAGE: +CompanionBlocksAndExtensions
+
+// MODULE: lib
+// FILE: lib.kt
+// EXTERNAL_FILE
+
+package org.example.lib
+
+class C
+
+companion fun C.exampleFun() {}
+
+companion val C.exampleVal: Int get() = 42
+
+// MODULE: test(lib)
+// FILE: companionExtension.kt
+
+import org.example.lib.C
+import org.example.lib.exampleFun
+import org.example.lib.exampleVal
+
+fun test() {
+    C.exampleFun()
+    val value = C.exampleVal
+}
