29e357a73176abdd269a0b36d9a8e668b30c4541
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-07-15 18:01:45 +0200

    CLI: copy JDK 9+ VarHandleWrapperImpl from intellij
    Compile this file against JDK 9, place it under `META-INF/versions/9`,
    and make kotlin-compiler and kotlin-compiler-embeddable multi-release
    jars (see JEP 238).
    
    Intellij machinery loads this class reflectively in
    `com.intellij.util.containers.VarHandleWrapper`, falling back to
    Unsafe-using implementation.
    
    This fixes an issue with using Unsafe, when running kotlin-native tests
    on JDK 25+.

--- prepare/compiler/build.gradle.kts
+++ prepare/compiler/build.gradle.kts
@@ -317,6 +317,9 @@ val proguard by task<CacheableProguardTask> {
             !org/mozilla/javascript/xml/impl/xmlbeans/**,
             !net/sf/cglib/**,
             !META-INF/maven**,
+            !META-INF/versions/9/com/intellij/concurrency/VarHandleWrapperImpl.class,
+            !module-info.class,
+            !META-INF/versions/**/module-info.class,
             **.class,**.properties,**.kt,**.kotlin_*,**.jnilib,**.so,**.dll,**.txt,**.caps,
             custom-formatters.js,
             META-INF/services/**,META-INF/native/**,META-INF/extensions/**,META-INF/MANIFEST.MF,
@@ -367,8 +370,15 @@ val jar = runtimeJar {
         compilerVersion.map(::zipTree)
     }
 
+    // ProGuard does not preserve the META-INF/versions/9 multi-release layout, it rewrites classes to paths derived from their FQ name.
+    // To fix it, we exclude VarHandleWrapperImpl.class from injars (see above) and add it here to the correct location manually.
+    from(packCompiler.map { zipTree(it.archiveFile.get().asFile) }) {
+        include("META-INF/versions/9/com/intellij/concurrency/VarHandleWrapperImpl.class")
+    }
+
     manifest.attributes["Class-Path"] = compilerManifestClassPath
     manifest.attributes["Main-Class"] = "org.jetbrains.kotlin.cli.jvm.K2JVMCompiler"
+    manifest.attributes["Multi-Release"] = "true"
 }
 
 sourcesJar {
