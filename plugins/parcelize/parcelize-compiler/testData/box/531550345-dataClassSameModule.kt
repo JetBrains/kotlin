2f0534338310c3809725b5434d18d5351d1e10d0
Francesco Protopapa <fprotopapa@google.com>
2026-07-15 12:10:04 +0000

    [Parcelize] Add reproducer tests for 531550345

---
+++ plugins/parcelize/parcelize-compiler/testData/box/531550345-dataClassSameModule.kt
@@ -0,0 +1,51 @@
+// WITH_STDLIB
+
+@file:JvmName("TestKt")
+package test
+
+import kotlinx.parcelize.*
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Data(val i: Int, val s: String)
+data class NullableData(val i: Int?, val s: String?)
+data class DefaultData(val i: Int = 42, val s: String? = "default", val n: String? = null)
+data class NestedData(val data: Data)
+
+@Parcelize
+class DataWrapper @OptIn(Experimental::class) constructor(val data: @DataClass Data) : Parcelable
+
+@Parcelize
+class NullableWrapper @OptIn(Experimental::class) constructor(val data: @DataClass NullableData) : Parcelable
+
+@Parcelize
+class DefaultWrapper @OptIn(Experimental::class) constructor(val data: @DataClass DefaultData) : Parcelable
+
+@Parcelize
+class NestedWrapper @OptIn(Experimental::class) constructor(val data: @DataClass NestedData) : Parcelable
+
+fun box() = parcelTest { parcel ->
+    val vData = DataWrapper(Data(42, ""))
+    val vNull = NullableWrapper(NullableData(null, "not null"))
+    val vDef = DefaultWrapper(DefaultData())
+    val vNested = NestedWrapper(NestedData(Data(42, "nested")))
+
+    vData.writeToParcel(parcel, 0)
+    vNull.writeToParcel(parcel, 0)
+    vDef.writeToParcel(parcel, 0)
+    vNested.writeToParcel(parcel, 0)
+
+    val bytes = parcel.marshall()
+    parcel.unmarshall(bytes, 0, bytes.size)
+    parcel.setDataPosition(0)
+
+    val vData2 = parcelableCreator<DataWrapper>().createFromParcel(parcel)
+    val vNull2 = parcelableCreator<NullableWrapper>().createFromParcel(parcel)
+    val vDef2 = parcelableCreator<DefaultWrapper>().createFromParcel(parcel)
+    val vNested2 = parcelableCreator<NestedWrapper>().createFromParcel(parcel)
+
+    assert(vData.data == vData2.data) { "Data: expected ${vData.data}, got ${vData2.data}" }
+    assert(vNull.data == vNull2.data) { "Nullable: expected ${vNull.data}, got ${vNull2.data}" }
+    assert(vDef.data == vDef2.data) { "Default: expected ${vDef.data}, got ${vDef2.data}" }
+    assert(vNested.data == vNested2.data) { "Nested: expected ${vNested.data}, got ${vNested2.data}" }
+}
