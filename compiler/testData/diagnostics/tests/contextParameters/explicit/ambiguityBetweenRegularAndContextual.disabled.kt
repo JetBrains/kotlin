4145250b3fed900436f849042b1997a24f6e927b
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-07-16 11:39:30 +0000

    [Tests] Reproducer for introduced ambiguity when explicit context arguments are enabled
    #KT-87554
    #KT-87555

---
+++ compiler/testData/diagnostics/tests/contextParameters/explicit/ambiguityBetweenRegularAndContextual.disabled.kt
@@ -0,0 +1,21 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// LANGUAGE: +ContextParameters
+// LANGUAGE_FEATURE_TOGGLED: ExplicitContextArguments
+// MODULE: m1
+context(x: Int)
+fun foo(a: String) = "CONTEXT"
+
+fun foo(a: String, x: Int) = "NO-CONTEXT"
+
+// MODULE: m2(m1)
+context(x: Int)
+fun bar(a: String) = "CONTEXT"
+
+fun bar(a: String, x: Int) = "NO-CONTEXT"
+
+fun test() {
+    foo(a = "OK", x = 5)
+    bar(a = "OK", x = 5)
+}
+
+/* GENERATED_FIR_TAGS: functionDeclaration, functionDeclarationWithContext, integerLiteral, stringLiteral */
