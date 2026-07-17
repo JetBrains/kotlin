3059cfba06bd3ed9acc97a7c3f6718d8200dd10a
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-07-17 15:00:55 +0200

    [js-plain-objects] Add a reproducer for KT-80865
    (cherry picked from commit 22ed0bd5dd345c5bfda56a067f866cf2a1d7e64d)

---
+++ plugins/js-plain-objects/compiler-plugin/testData/box/diamondHierarchy.kt
@@ -0,0 +1,35 @@
+// ISSUE: KT-87127
+// IGNORE_BACKEND: JS_IR
+
+import kotlinx.js.JsPlainObject
+
+@JsPlainObject
+external interface A {
+    val value: String
+}
+
+@JsPlainObject
+external interface B: A
+
+@JsPlainObject
+external interface C: A
+
+@JsPlainObject
+external interface D: C, B
+
+fun box(): String {
+    val d = <!NO_VALUE_FOR_PARAMETER!>D<!>(value = "Miku")
+
+    if (d.value != "Miku") return "Fail: unexpected value ${d.value}"
+
+    val json1 = js("JSON.stringify(d)")
+    if (json1 != "{\"value\":\"Miku\"}") return "Fail: unexpected json $json1"
+
+    val dCopy = D.copy(d, value = "Teto")
+    if (dCopy.value != "Teto") return "Fail: unexpected value ${dCopy.value}"
+
+    val json2 = js("JSON.stringify(dCopy)")
+    if (json2 != "{\"value\":\"Teto\"}") return "Fail: unexpected json $json2"
+
+    return "OK"
+}
