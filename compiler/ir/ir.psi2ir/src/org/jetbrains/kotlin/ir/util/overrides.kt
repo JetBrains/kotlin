c54c1b50ef38be0d21a7ca7c6939301f89a2c8c2
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-07-17 16:03:32 +0200

    [IR] Move some util files to `psi2ir` module
    These utils are not used outside `psi2ir`.
    
    #KT-67017

--- compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/ir/util/overrides.kt
+++ compiler/ir/ir.psi2ir/src/org/jetbrains/kotlin/ir/util/overrides.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2024 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -11,7 +11,6 @@ import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.declarations.DescriptorMetadataSource
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
-import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
 import org.jetbrains.kotlin.resolve.descriptorUtil.isEffectivelyExternal
 import org.jetbrains.kotlin.utils.memoryOptimizedMap
 
