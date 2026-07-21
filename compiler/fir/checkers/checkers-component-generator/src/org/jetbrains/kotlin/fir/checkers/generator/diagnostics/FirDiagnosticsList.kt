e5a80d6cb93a3b6e51a9f03815d701d630d2bcea
Roman Venediktov <e2e4b6b7@gmail.com>
2026-07-21 18:46:45 +0200

    IE checker

--- compiler/fir/checkers/checkers-component-generator/src/org/jetbrains/kotlin/fir/checkers/generator/diagnostics/FirDiagnosticsList.kt
+++ compiler/fir/checkers/checkers-component-generator/src/org/jetbrains/kotlin/fir/checkers/generator/diagnostics/FirDiagnosticsList.kt
@@ -73,6 +73,10 @@ object DIAGNOSTICS_LIST : DiagnosticList("FirErrors") {
         val OTHER_ERROR_WITH_REASON by error<PsiElement> {
             parameter<String>("reason")
         }
+
+        val IE_WARNING by warning<PsiElement> {
+            parameter<String>("reason")
+        }
     }
 
     val GENERAL_SYNTAX by object : DiagnosticGroup("General syntax") {
--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
@@ -1325,7 +1325,11 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
         ) as FirFunctionCall
     }
 
-    private fun FirTypeRef.withTypeArgumentsForBareType(argument: FirExpression, operation: FirOperation): FirTypeRef {
+    private fun FirTypeRef.withTypeArgumentsForBareType(
+        argument: FirExpression,
+        operation: FirOperation,
+        onDiagnostic: (ConeDiagnostic) -> Unit
+    ): FirTypeRef {
         val type = coneTypeSafe<ConeClassLikeType>()?.fullyExpandedType() ?: return this
         if (type.typeArguments.isNotEmpty()) return this // TODO: Incorrect for local classes, KT-59686
         // TODO: Check equality of size of arguments and parameters?
@@ -1338,7 +1342,7 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
         }
 
         val outerClasses by lazy(LazyThreadSafetyMode.NONE) { firClass.symbol.getClassAndItsOuterClassesWhenLocal(session) }
-        val newType = components.computeRepresentativeTypeForBareType(type, originalType)
+        val newType = context(onDiagnostic) { components.computeRepresentativeTypeForBareType(type, originalType) }
             ?: if (
                 firClass.isLocal && firClass.typeParameters.none { it.symbol.containingDeclarationSymbol in outerClasses } &&
                 (operation == NOT_IS || operation == IS || operation == AS || operation == SAFE_AS)
@@ -1372,7 +1376,10 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
             resolved.prepareCSRHintForTypeOperatorIfNeeded()
         }
 
-        val conversionTypeRef = resolved.conversionTypeRef.withTypeArgumentsForBareType(resolved.argument, typeOperatorCall.operation)
+        val onDiagnostic = { coneDiagnostic: ConeDiagnostic ->
+            typeOperatorCall.replaceNonFatalDiagnostics(typeOperatorCall.nonFatalDiagnostics + coneDiagnostic)
+        }
+        val conversionTypeRef = resolved.conversionTypeRef.withTypeArgumentsForBareType(resolved.argument, typeOperatorCall.operation, onDiagnostic)
         resolved.transformChildren(object : FirDefaultTransformer<Any?>() {
             override fun <E : FirElement> transformElement(element: E, data: Any?): E {
                 return element
