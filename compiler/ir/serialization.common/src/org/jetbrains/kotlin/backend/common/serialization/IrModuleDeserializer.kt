fcb2a45d50fcb1def480649cfbe0f14af8b6a963
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-07-21 14:23:49 +0200

    [IR] Drop `IrModuleDeserializer.signatureDeserializerForFile` method
    It is not used anywhere.
    
    #KT-67017

--- compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrModuleDeserializer.kt
+++ compiler/ir/serialization.common/src/org/jetbrains/kotlin/backend/common/serialization/IrModuleDeserializer.kt
@@ -136,8 +136,6 @@ abstract class IrModuleDeserializer(
 
     val compatibilityMode: CompatibilityMode get() = CompatibilityMode(libraryAbiVersion)
 
-    open fun signatureDeserializerForFile(fileName: String): IdSignatureDeserializer = error("Unsupported")
-
     open fun getAllMatchingSignatures(callableId: CallableId, signatureKind: IrDeserializer.TopLevelSymbolKind): List<IdSignature> {
         return this.fileDeserializers().flatMap {
             it.getAllMatchingSignatures(callableId, signatureKind)
@@ -284,10 +282,6 @@ class IrModuleDeserializerWithBuiltIns(
         syntheticFunctionClassGenerator.typeSystem = IrTypeSystemContextImpl(irBuiltIns)
         @OptIn(UnstableBuiltInsApi::class)
         irBuiltIns.functionFactory = syntheticFunctionClassGenerator
-    }
-
-    override fun signatureDeserializerForFile(fileName: String): IdSignatureDeserializer {
-        return delegate.signatureDeserializerForFile(fileName)
     }
 
     override fun getAllMatchingSignatures(callableId: CallableId, signatureKind: IrDeserializer.TopLevelSymbolKind): List<IdSignature> {
