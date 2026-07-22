d4a774bb33e80be1eece6d68b1abe46abade99f5
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-07-22 11:37:44 +0000

    Tests: reproduce KT-82465

---
+++ compiler/testData/diagnostics/tests/contextSensitiveResolutionUsingExpectedType/falseNegativeWithExtraReceiver.kt
@@ -0,0 +1,28 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-82465
+
+class MyColor(val name: String) {
+    companion object {
+        val COMPANION_COLOR = MyColor("companion_color")
+    }
+}
+
+val MyColor.Companion.EXT_COLOR_TOP: MyColor get() = MyColor("ext_color")
+
+fun String.test() {
+    val myColorLocal: MyColor = <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>EXT_COLOR_TOP<!>
+    val myColorCompanionLocal: MyColor = COMPANION_COLOR
+}
+
+class Another {
+    val myColorMember: MyColor = <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>EXT_COLOR_TOP<!>
+    val myColorCompanionMember: MyColor = COMPANION_COLOR
+
+    fun usage() {
+        val myColorMemberLocal: MyColor = <!UNRESOLVED_REFERENCE_WRONG_RECEIVER!>EXT_COLOR_TOP<!>
+        val myColorCompanionMemberLocal: MyColor = COMPANION_COLOR
+    }
+}
+
+/* GENERATED_FIR_TAGS: classDeclaration, companionObject, funWithExtensionReceiver, functionDeclaration, getter,
+localProperty, objectDeclaration, primaryConstructor, propertyDeclaration, propertyWithExtensionReceiver, stringLiteral */
