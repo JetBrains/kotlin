ea7d64f774dae48558dfcd828cf488f15073ab72
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-07-22 11:50:14 +0200

    [test] Reproduce KT-87887

--- compiler/testData/ir/irText/js/dynamic/dynamicArrayIncrementDecrement.kt
+++ compiler/testData/ir/irText/js/dynamic/dynamicArrayIncrementDecrement.kt
@@ -1,9 +1,14 @@
 // TARGET_BACKEND: JS_IR
-// KT-65195
+// KT-65195, KT-87887
 
 fun testArrayIncrementDecrement(d: dynamic) {
     val t1 = ++d["prefixIncr"]
     val t2 = --d["prefixDecr"]
     val t3 = d["postfixIncr"]++
     val t4 = d["postfixDecr"]--
+
+    ++d["prefixIncr"]
+    --d["prefixDecr"]
+    d["postfixIncr"]++
+    d["postfixDecr"]--
 }
