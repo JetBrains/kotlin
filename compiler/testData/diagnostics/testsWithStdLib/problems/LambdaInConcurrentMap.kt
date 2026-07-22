3c60b2cfd2e812caaeb590c11aa3fc39a375812b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-07-22 11:58:14 +0200

    Tests (compiler): reproduce KT-8761

---
+++ compiler/testData/diagnostics/testsWithStdLib/problems/LambdaInConcurrentMap.kt
@@ -0,0 +1,21 @@
+// RUN_PIPELINE_TILL: BACKEND
+// ISSUE: KT-8761
+// FULL_JDK
+// FIR_DUMP
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentMap
+
+fun <K, V> ConcurrentMap<K, V>.forEachValue(action: (V) -> Unit) {}
+
+fun main() {
+    val map = ConcurrentHashMap<String, Int>()
+    map.forEachValue { v ->
+        // expected v to be `Int` here, got `Int!`
+        v?.plus(2)
+        v.minus(2)
+    }
+}
+
+/* GENERATED_FIR_TAGS: flexibleType, funWithExtensionReceiver, functionDeclaration, functionalType, javaFunction,
+lambdaLiteral, localProperty, nullableType, propertyDeclaration, typeParameter */
