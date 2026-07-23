b3c341abfbb9f469fa59c63c7e59a1e672e6964d
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2026-07-23 11:12:58 +0200

    [IR] Drop a dead statement in FinallyBlocksLowering
     ^KT-89020

--- compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/FinallyBlocksLowering.kt
+++ compiler/ir/backend.common/src/org/jetbrains/kotlin/backend/common/lower/FinallyBlocksLowering.kt
@@ -179,7 +179,6 @@ open class FinallyBlocksLowering(val context: CommonBackendContext, private val 
         val currentTryScope = tryScopes[index]
         currentTryScope.jumps.getOrPut(jump) {
             val type = (jump as? Return)?.target?.owner?.returnType(context) ?: value.type
-            jump.toString()
             val symbol = IrReturnableBlockSymbolImpl()
             with(currentTryScope) {
                 irBuilder.run {
