d327075085d7675c7ff75494ce34ec060667b938
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-07-23 16:26:20 +0000

    [Tests] Reproduce KT-29890
    Also, other "inline" properties of these constructors are covered in two
    extra tests.

---
+++ compiler/testData/diagnostics/tests/arrays/inlineConstructors/usageInlinable.kt
@@ -0,0 +1,22 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-29890
+// DIAGNOSTICS: -NOTHING_TO_INLINE
+
+inline fun testConstructor1(size: Int, init: (Int) -> Double) = DoubleArray(size, <!USAGE_IS_NOT_INLINABLE!>init<!>)
+inline fun testConstructor2(size: Int, init: (Int) -> Double) = DoubleArray(size) { init(it) }
+
+inline fun testConstructor3(size: Int, init: (Int) -> Double) = Array(size) { DoubleArray(size) { init(it) } }
+inline fun testConstructor4(size: Int, init: (Int) -> Double) = Array(size) { DoubleArray(size, <!USAGE_IS_NOT_INLINABLE!>init<!>) }
+
+typealias MyDoubleArray = Array<Double>
+
+inline fun testConstructor5(size: Int, init: (Int) -> Double) = MyDoubleArray(size, <!USAGE_IS_NOT_INLINABLE!>init<!>)
+inline fun testConstructor6(size: Int, init: (Int) -> Double) = MyDoubleArray(size) { init(it) }
+
+inline fun testConstructor7(size: Int, crossinline init: (Int) -> Double) = DoubleArray(size, <!USAGE_IS_NOT_INLINABLE!>init<!>)
+inline fun testConstructor8(size: Int, crossinline init: (Int) -> Double) = DoubleArray(size) { init(it) }
+
+inline fun testConstructor9(size: Int, noinline init: (Int) -> Double) = DoubleArray(size, init)
+inline fun testConstructor10(size: Int, noinline init: (Int) -> Double) = DoubleArray(size) { init(it) }
+
+/* GENERATED_FIR_TAGS: functionDeclaration, functionalType, inline, lambdaLiteral */
