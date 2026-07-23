7cf9432c092f51c9ec0d2e978663a83d5027c690
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2026-07-23 12:18:02 +0200

    [Native] ^KT-87702: introduce HotReload tag for logging

--- kotlin-native/runtime/src/main/cpp/Logging.hpp
+++ kotlin-native/runtime/src/main/cpp/Logging.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * Copyright 2010-2026 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the LICENSE file.
  */
 
@@ -41,8 +41,9 @@ enum class Tag : int32_t {
     kGCMark = 9,
     kGCScheduler = 10,
     kMemoryDump = 11,
+    kHotReload = 12,
 
-    kEnumSize = 12
+    kEnumSize = 13
 };
 
 namespace internal {
@@ -71,6 +72,7 @@ inline const char* name(Tag tag) {
         case Tag::kGCMark: return "gcMark";
         case Tag::kGCScheduler: return "gcScheduler";
         case Tag::kMemoryDump: return "memoryDump";
+        case Tag::kHotReload: return "hotReload";
 
         case Tag::kEnumSize: break;
     }
