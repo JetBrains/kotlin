5c36309fd3884aa3f42d576c5d4e5d052ae8ef02
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-07-23 19:22:53 +0200

    [JS] Fix scope tracking in `TemporaryVariableElimination`
    Blocks in JavaScript do define a new lexical scope.
    For `TemporaryVariableElimination` to correctly work with const/let,
    we must take into account `JsBlock`s too.
    
    KT-64348

--- js/js.translator/src/org/jetbrains/kotlin/js/inline/clean/TemporaryVariableElimination.kt
+++ js/js.translator/src/org/jetbrains/kotlin/js/inline/clean/TemporaryVariableElimination.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.js.inline.clean
@@ -205,6 +194,14 @@ internal class TemporaryVariableElimination(private val function: JsFunction) : 
             override fun visitDefault(x: JsDefault) = withNewScope { super.visitDefault(x) }
 
             override fun visitCatch(x: JsCatch) = withNewScope { super.visitCatch(x) }
+
+            override fun visitBlock(x: JsBlock) {
+                if (x.isTransparent) {
+                    super.visitBlock(x)
+                } else {
+                    withNewScope { super.visitBlock(x) }
+                }
+            }
 
             override fun visitFunction(x: JsFunction) {
                 for (freeVar in x.collectFreeVariables()) {
