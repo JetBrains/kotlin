bff64fecdda551a0f1665a1d60381d7489ee19d3
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-07-24 13:12:17 +0200

    [FIR2IR] Only skip bodies for rich references to local delegated properties
    #KT-86453

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/AdapterGenerator.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/generators/AdapterGenerator.kt
@@ -27,6 +27,7 @@ import org.jetbrains.kotlin.fir.resolve.substitution.ConeSubstitutor
 import org.jetbrains.kotlin.fir.resolve.toSymbol
 import org.jetbrains.kotlin.fir.resolve.typeParameterSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirFunctionSymbol
+import org.jetbrains.kotlin.fir.symbols.impl.FirLocalPropertySymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirTypeParameterSymbol
 import org.jetbrains.kotlin.fir.types.*
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
@@ -144,7 +145,10 @@ class AdapterGenerator(
         val boundReceiver: IrExpression? get() = boundDispatchReceiver ?: boundExtensionReceiver
         val hasBoundReceiver: Boolean get() = boundDispatchReceiver != null || boundExtensionReceiver != null
 
-        val isForDelegate = callableReferenceAccess.source?.kind is KtFakeSourceElementKind.DelegatedPropertyAccessor
+        val origin = runIf(callableReferenceAccess.source?.kind is KtFakeSourceElementKind.DelegatedPropertyAccessor) {
+            IrStatementOrigin.PROPERTY_REFERENCE_FOR_DELEGATE
+        }
+        val isForLocalDelegate = firAdaptee.symbol.let { it is FirLocalPropertySymbol && it.delegate != null }
 
         private fun findBoundReceiver(
             explicitReceiverExpression: IrExpression?,
@@ -222,7 +226,7 @@ class AdapterGenerator(
                         isSetter = true,
                     )
                 },
-                origin = runIf(context.isForDelegate) { IrStatementOrigin.PROPERTY_REFERENCE_FOR_DELEGATE },
+                origin = context.origin,
             ).apply { bindValues(context) }
         }
     }
@@ -269,7 +273,7 @@ class AdapterGenerator(
     ): IrSimpleFunction {
         val irAdapterFunction = createAdapterFunctionForCallableReference(startOffset, endOffset, isSetter)
 
-        if (!isForDelegate) {
+        if (!isForLocalDelegate) {
             irAdapterFunction.body = IrFactoryImpl.createBlockBody(UNDEFINED_OFFSET, UNDEFINED_OFFSET) {
                 val irCall = createAdapteeCallForCallableReference(adapteeSymbol, irAdapterFunction, isSetter)
                 if (isSetter || adaptedType.arguments.last().typeOrNull?.isUnit() == true) {
