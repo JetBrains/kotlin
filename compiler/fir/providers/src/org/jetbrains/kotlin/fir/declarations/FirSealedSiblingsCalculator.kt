6d3fd620a9abf49cd16c577afe592273ab0b8a69
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-07-24 13:28:26 +0300

    fixup! [FIR] Improve `getComplementarySymbols()`
    Remove unnecessary interface calls, set allocations and runtime casts.
    
    Update the KDoc for `relevantSealedUniverseCache`

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/declarations/FirSealedSiblingsCalculator.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/declarations/FirSealedSiblingsCalculator.kt
@@ -26,131 +26,147 @@ import org.jetbrains.kotlin.name.StandardClassIds
 
 @ThreadSafeMutableState
 class FirSealedSiblingsCalculator(private val session: FirSession) : FirSessionComponent {
-    private typealias CachedSubclasses = Any
+    private val singleValueCachedSubclass = emptySet<FirClassSymbol<*>>()
+    private typealias CachedSubclasses = Set<FirClassSymbol<*>>
 
-    private inline fun <T> CachedSubclasses.mapCachedSubclasses(
+    private inline fun <T> FirClassSymbol<*>.mapCachedSubclasses(
+        visited: LinkedHashSet<FirClassSymbol<*>>?,
         onSingle: (FirClassSymbol<*>) -> T,
         onMultiple: (Set<FirClassSymbol<*>>) -> T,
-    ): T = when (this) {
-        is FirClassSymbol<*> -> onSingle(this)
-        else -> @Suppress("UNCHECKED_CAST") onMultiple(this as Set<FirClassSymbol<*>>)
+    ): T {
+        val cached = allSubclassesCache.getValue(this, visited)
+
+        return when {
+            cached === singleValueCachedSubclass -> onSingle(this)
+            else -> onMultiple(cached)
+        }
     }
 
-    private inline fun CachedSubclasses.forEachCachedSubclass(action: (FirClassSymbol<*>) -> Unit) =
-        mapCachedSubclasses(onSingle = action, onMultiple = { it.forEach(action) })
+    private inline fun FirClassSymbol<*>.forEachCachedSubclass(
+        visited: LinkedHashSet<FirClassSymbol<*>>?,
+        action: (FirClassSymbol<*>) -> Unit,
+    ) = mapCachedSubclasses(visited, onSingle = action, onMultiple = { it.forEach(action) })
 
-    private fun CachedSubclasses.mapTo(mutableSet: MutableSet<FirClassSymbol<*>>): Boolean =
-        mapCachedSubclasses(mutableSet::add, mutableSet::addAll)
-
-    private fun CachedSubclasses.toSet(): Set<FirClassSymbol<*>> =
-        mapCachedSubclasses(onSingle = ::setOf, onMultiple = { it })
-
-    private val allSubclassesCache: FirCache<FirClassSymbol<*>, CachedSubclasses, MutableSet<FirClassSymbol<*>>?> =
+    private val allSubclassesCache: FirCache<FirClassSymbol<*>, CachedSubclasses, LinkedHashSet<FirClassSymbol<*>>?> =
         session.firCachesFactory.createCache { symbol, visited ->
             when {
-                visited != null && !visited.add(symbol) -> symbol
-                symbol !is FirRegularClassSymbol -> symbol
+                visited != null && !visited.add(symbol) -> singleValueCachedSubclass
+                symbol !is FirRegularClassSymbol -> singleValueCachedSubclass
                 symbol.fir.modality == Modality.SEALED -> buildSet {
                     if (symbol.fir.isJavaNonAbstractSealed == true) {
                         add(symbol)
                     }
 
-                    symbol.fir.getSealedClassInheritors(session).forEach {
+                    symbol.fir.getSealedClassInheritors(session).forEach { it ->
                         val inheritor = session.symbolProvider.getClassLikeSymbolByClassId(it) as? FirRegularClassSymbol ?: return@forEach
-                        allSubclassesCache.getValue(inheritor, visited ?: mutableSetOf(symbol)).mapTo(this)
+                        inheritor.forEachCachedSubclass(visited ?: linkedSetOf(symbol), ::add)
                     }
                 }
-                else -> symbol
+                else -> singleValueCachedSubclass
             }
         }
 
     fun collectAllSubclassesOfSealed(symbol: FirClassSymbol<*>): Set<FirClassSymbol<*>> =
-        allSubclassesCache.getValue(symbol, context = null).toSet()
+        symbol.mapCachedSubclasses(visited = null, onSingle = ::setOf, onMultiple = { it })
 
     private fun FirClassSymbol<*>.isSubclassOf(other: FirClassSymbol<*>): Boolean =
         isSubclassOf(other.toLookupTag(), session, isStrict = false, lookupInterfaces = true)
 
-    private fun areUnrelated(a: FirClassSymbol<*>, b: FirClassSymbol<*>): Boolean =
-        !a.isSubclassOf(b) && !b.isSubclassOf(a)
-
     private fun FirRegularClassSymbol.getImmediateSuperTypes(session: FirSession): List<ConeClassLikeType> =
         getSuperTypes(session, recursive = false)
 
-    private object EmptyCachedSealedUniverse
-    private typealias CachedSealedUniverse = Any
+    private val emptyCachedSealedUniverse = linkedSetOf<FirClassSymbol<*>>()
+    private typealias CachedSealedUniverse = LinkedHashSet<FirClassSymbol<*>>
 
     private inline fun <T> CachedSealedUniverse.mapCachedUniverse(
         onEmpty: () -> T,
         onNonEmpty: (LinkedHashSet<FirClassSymbol<*>>) -> T,
     ): T = when {
-        this != EmptyCachedSealedUniverse -> @Suppress("UNCHECKED_CAST") onNonEmpty(this as LinkedHashSet<FirClassSymbol<*>>)
+        this !== emptyCachedSealedUniverse -> onNonEmpty(this)
         else -> onEmpty()
     }
 
-    private inline fun CachedSealedUniverse.filterCachedUniverseTo(
-        destination: MutableSet<FirClassSymbol<*>>,
+    private inline fun CachedSealedUniverse.filterCachedUniverse(
         predicate: (FirClassSymbol<*>) -> Boolean,
-    ) = mapCachedUniverse(
-        onEmpty = { destination },
-        onNonEmpty = { it.filterTo(destination, predicate) },
+    ): Set<FirClassSymbol<*>> = mapCachedUniverse(
+        onEmpty = { emptyCachedSealedUniverse },
+        onNonEmpty = { it.filterTo(mutableSetOf(), predicate) },
     )
 
-    private inline fun CachedSealedUniverse.forEachSealedInCachedUniverse(action: (FirClassSymbol<*>) -> Unit) =
+    private inline fun CachedSealedUniverse.forEachInCachedUniverse(action: (FirClassSymbol<*>) -> Unit) =
         mapCachedUniverse(onEmpty = {}, onNonEmpty = { it.forEach(action) })
 
-    private fun CachedSealedUniverse.toMutableSet(): LinkedHashSet<FirClassSymbol<*>> =
-        mapCachedUniverse(onEmpty = ::LinkedHashSet, onNonEmpty = { it })
+    private fun CachedSealedUniverse.cachedUniverseToMutableSet(): CachedSealedUniverse =
+        mapCachedUniverse(onEmpty = ::linkedSetOf, onNonEmpty = { it })
 
     /**
      * Maps the given [FirRegularClassSymbol] to the set of [FirClassSymbol]s that coexist
-     * in the same `sealed` hierarchy, and are not its superclasses.
+     * in the same `sealed` hierarchy as its superclasses but are not these superclasses themselves.
      *
-     * For instance, in the following hierarchy:
-     * - `f(C) = setOf(D, F, G, I)`,
-     * - `f(H) = setOf(C, D, F, I)`,
-     * - `f(J) = setOf(C, D, F, I)`, and
-     * - `f(A) = emptySet()`.
+     * Consider the following examples.
+     * ```
+     * sealed interface A {          // f(A) = emptySet()
+     *     sealed interface B : A    // f(B) = setOf(C)
+     *     sealed interface C : A
+     * }
+     * ```
      * ```
      * sealed interface A {
-     *     sealed interface B : A {
-     *         interface C : B
-     *         interface D : B
+     *     interface B : A {
+     *         interface C : B       // f(C) = setOf(F)
      *     }
      *
      *     sealed interface E : A {
-     *         interface F : E
+     *         interface F : E       // f(F) = setOf(B)
      *     }
+     * }
+     * ```
+     * ```
+     * sealed interface A {
+     *     sealed interface E : A
      *
      *     interface G : A {
-     *         interface H : G {
+     *         interface H : G {     // f(H) = setOf(I)
      *             interface I : H, E
-     *             interface J : H
      *         }
      *     }
      * }
      * ```
+     * ```
+     * sealed interface A
+     * sealed interface B : A        // f(B) = emptySet()
+     * ```
+     * ```
+     * sealed interface I            // f(I) = emptySet()
+     *
+     * sealed class A                // f(A) = emptySet()
+     * class B : A                   // f(B) = setOf(C)
+     * class C : A, I                // f(C) = setOf(B, D)
+     *
+     * class D : I                   // f(D) = setOf(C)
+     * ```
      */
     private val relevantSealedUniverseCache: FirCache<FirRegularClassSymbol, CachedSealedUniverse, Nothing?> =
         session.firCachesFactory.createCache { symbol, _ ->
-            var result: CachedSealedUniverse = EmptyCachedSealedUniverse
+            var result: CachedSealedUniverse = emptyCachedSealedUniverse
 
             fun addIfNonSuper(other: FirClassSymbol<*>) = when {
-                !symbol.isSubclassOf(other) -> result = result.toMutableSet().apply { add(other) }
+                !symbol.isSubclassOf(other) -> result = result.cachedUniverseToMutableSet().apply { add(other) }
                 else -> {}
             }
 
             for (it in symbol.getImmediateSuperTypes(session)) {
                 val symbol = it.toRegularClassSymbol(session)?.takeIf { it.classId != StandardClassIds.Any } ?: continue
-                relevantSealedUniverseCache.getValue(symbol, null).forEachSealedInCachedUniverse(::addIfNonSuper)
-                allSubclassesCache.getValue(symbol, context = null).forEachCachedSubclass(::addIfNonSuper)
+                relevantSealedUniverseCache.getValue(symbol, null).forEachInCachedUniverse(::addIfNonSuper)
+                symbol.forEachCachedSubclass(null, ::addIfNonSuper)
             }
 
             result
         }
 
     fun collectSealedSiblingsFor(symbol: FirRegularClassSymbol): Set<FirClassSymbol<*>> =
-        relevantSealedUniverseCache.getValue(symbol, null).filterCachedUniverseTo(mutableSetOf()) {
-            (symbol.isFinal || it.isFinal || symbol.isClass && it.isClass) && areUnrelated(symbol, it)
+        relevantSealedUniverseCache.getValue(symbol, null).filterCachedUniverse {
+            (symbol.isFinal || it.isFinal || symbol.isClass && it.isClass) && !it.isSubclassOf(symbol)
         }
 }
 
