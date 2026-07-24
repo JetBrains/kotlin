14bc592af8388f5eec20685e5cfb5a3cddd119de
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-07-24 19:36:37 +0000

    [Tests][Native] Drop inliner-specific box  testrunners
    and use default crossmodule IR inliner setting for all box testrunners
    
    ^KT-81022

--- native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/NativeTestSupport.kt
+++ native/native.tests/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/support/NativeTestSupport.kt
@@ -285,11 +285,12 @@ object NativeTestSupport {
             default = CompilerOutputInterceptor.DEFAULT
         )
 
+    /*
+     * KT-81022: Tests are performed only with "ON" cross-module IR inliner setting, which is the default since 2.5
+     * This trivial function is kept just for the convenience of trying out compilation without the cross-module IR inliner
+     */
     private fun computeKlibIrInlinerMode(tags: Set<String>): KlibIrInlinerMode =
-        if (tags.contains(KLIB_IR_INLINER))
-            KlibIrInlinerMode.ON
-        else
-            KlibIrInlinerMode.OFF
+        KlibIrInlinerMode.ON
 
     private fun computeGCType(enforcedProperties: EnforcedProperties): GCType =
         ClassLevelProperty.GC_TYPE
