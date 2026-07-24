6815bf814b70f52f42c227d69474652e4d88182d
Radoslaw Panuszewski <radoslaw.panuszewski@jetbrains.com>
2026-07-25 00:01:25 +0200

    Add verification-metadata-for-bootstrap.xml

--- scripts/update-verification-metadata-for-bootstrap.sh
+++ scripts/update-verification-metadata-for-bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+cp gradle/verification-metadata.xml gradle/verification-metadata.xml.bak
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' -e '/<components>/,/<\/components>/d' gradle/verification-metadata.xml
 else
   sed -i -e '/<components>/,/<\/components>/d' gradle/verification-metadata.xml
 fi
+
 ./gradlew \
   --write-verification-metadata sha256 \
   -Pkotlin.native.enabled=true \
@@ -12,3 +15,6 @@ fi
   -Pbootstrap.local.path=build/repo \
   --continue \
   resolveDependencies
+
+mv gradle/verification-metadata.xml gradle/verification-metadata-for-bootstrap.xml
+mv gradle/verification-metadata.xml.bak gradle/verification-metadata.xml
