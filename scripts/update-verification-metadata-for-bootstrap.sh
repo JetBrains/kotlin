56cc1cd2d4dbacdef7c6f06287682a7fe2c73e6a
Radoslaw Panuszewski <radoslaw.panuszewski@jetbrains.com>
2026-07-26 00:06:36 +0200

    WIP

--- scripts/update-verification-metadata-for-bootstrap.sh
+++ scripts/update-verification-metadata-for-bootstrap.sh
@@ -2,6 +2,12 @@
 
 #cp gradle/verification-metadata.xml gradle/verification-metadata.xml.bak
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' -e '/<components>/,/<\/components>/d' gradle/verification-metadata.xml
+else
+  sed -i -e '/<components>/,/<\/components>/d' gradle/verification-metadata.xml
+fi
+
 ./gradlew \
   --write-verification-metadata sha256 \
   -Pkotlin.native.enabled=true \
