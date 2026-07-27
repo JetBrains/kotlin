efaf89ba4960f0a84d99abadcd79e5cd41ccd6e5
Radoslaw Panuszewski <radoslaw.panuszewski@jetbrains.com>
2026-07-27 14:27:08 +0200

    [Build] Mark `checkBuild` as IDEA test task

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
@@ -445,7 +445,9 @@ fun Project.configureTests() {
         }
     }
     // Aggregate task for build related checks
-    tasks.register("checkBuild")
+    tasks.register("checkBuild") {
+        markAsIdeaTestTask()
+    }
     configureTestRetriesForTestTasks()
 }
 
