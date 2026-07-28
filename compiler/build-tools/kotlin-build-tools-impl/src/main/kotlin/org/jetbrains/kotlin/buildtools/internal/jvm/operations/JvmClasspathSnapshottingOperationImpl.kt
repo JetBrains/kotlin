272b66c2b99bb0d7845278a5c05fa03cca3b2c9f
Iveta Kovalenko <iveta.kovalenko@jetbrains.com>
2026-07-28 05:42:13 +0200

    [no-push] Enable EXPAND_TYPE_ALIASES option

--- compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/jvm/operations/JvmClasspathSnapshottingOperationImpl.kt
+++ compiler/build-tools/kotlin-build-tools-impl/src/main/kotlin/org/jetbrains/kotlin/buildtools/internal/jvm/operations/JvmClasspathSnapshottingOperationImpl.kt
@@ -77,6 +77,7 @@ internal class JvmClasspathSnapshottingOperationImpl private constructor(
 
         val PARSE_INLINED_LOCAL_CLASSES: Option<Boolean> = Option("PARSE_INLINED_LOCAL_CLASSES", true)
 
-        val EXPAND_TYPE_ALIASES: Option<Boolean> = Option("EXPAND_TYPE_ALIASES", false)
+
+        val EXPAND_TYPE_ALIASES: Option<Boolean> = Option("EXPAND_TYPE_ALIASES", true)
     }
 }
