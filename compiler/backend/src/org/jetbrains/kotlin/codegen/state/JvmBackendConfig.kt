fa9fd73efad167d3e95a8f8878a24cdd98ad00f2
Maksim Verevkin <maksim.verevkin@jetbrains.com>
2026-07-28 12:58:19 +0300

    JVM: reintroduce JvmBackendConfig.useFir
    Some tests rely on language version and do not set USE_FIR, while others
    do the opposite (e.g. `K2JvmVersionRequirementTest.testDefinitelyNotNull`),
    so check both.

--- compiler/backend/src/org/jetbrains/kotlin/codegen/state/JvmBackendConfig.kt
+++ compiler/backend/src/org/jetbrains/kotlin/codegen/state/JvmBackendConfig.kt
@@ -97,6 +97,8 @@ class JvmBackendConfig(configuration: CompilerConfiguration) {
 
     val enhancedCoroutinesDebugging = configuration.getBoolean(JVMConfigurationKeys.ENHANCED_COROUTINES_DEBUGGING)
 
+    val useFir = languageVersionSettings.languageVersion.usesK2 || configuration.useFir
+
     val emitJvmTypeAnnotations: Boolean = configuration.getBoolean(JVMConfigurationKeys.EMIT_JVM_TYPE_ANNOTATIONS)
 
     // Fixed coroutines debugging uses stdlib function to null out spilled locals.
