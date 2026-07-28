91905dd1a9a65f89d35e323948c7c0d9270fed24
Maksim Verevkin <maksim.verevkin@jetbrains.com>
2026-07-28 12:58:19 +0300

    JVM: reintroduce JvmBackendConfig.useFir
    Some tests rely on language version and do not set USE_FIR, while others
    do the opposite (e.g. `K2JvmVersionRequirementTest.testDefinitelyNotNull`),
    so check both.

--- compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/ExternalPackageParentPatcherLowering.kt
+++ compiler/ir/backend.jvm/lower/src/org/jetbrains/kotlin/backend/jvm/lower/ExternalPackageParentPatcherLowering.kt
@@ -23,7 +23,7 @@ import org.jetbrains.kotlin.load.kotlin.FacadeClassSource
  */
 internal class ExternalPackageParentPatcherLowering(val context: JvmBackendContext) : FileLoweringPass {
     override fun lower(irFile: IrFile) {
-        if (context.config.languageVersionSettings.languageVersion.usesK2) {
+        if (context.config.useFir) {
             irFile.acceptVoid(Visitor())
         }
     }
