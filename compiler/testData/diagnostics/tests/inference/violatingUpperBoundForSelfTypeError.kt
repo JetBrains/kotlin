442ac3e286bbb5fc3654ef4773689e47af5b6863
Denis.Zharkov <denis.zharkov@jetbrains.com>
2026-07-28 19:52:54 +0200

    ~~ test data

--- compiler/testData/diagnostics/tests/inference/violatingUpperBoundForSelfTypeError.kt
+++ compiler/testData/diagnostics/tests/inference/violatingUpperBoundForSelfTypeError.kt
@@ -6,7 +6,7 @@ fun <E : Enum<E>> createMap(enumClass: Class<E>) {}
 
 fun reproduce() {
     val enumClass: Class<Enum<*>> = "any" as Class<Enum<*>>
-    <!TYPE_MISMATCH!><!UPPER_BOUND_VIOLATED_DEPRECATION_WARNING!>createMap<!>(enumClass)<!>
+    <!TYPE_MISMATCH!><!UPPER_BOUND_VIOLATED!>createMap<!>(enumClass)<!>
 }
 
 /* GENERATED_FIR_TAGS: asExpression, capturedType, functionDeclaration, localProperty, propertyDeclaration,
