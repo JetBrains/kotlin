ff132ca528326893bb41f532f5216f3e56ced59f
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2026-07-28 13:59:57 +0200

    [Tests] Add PL test for less visible members overriding abstract
    KT-88047

---
+++ compiler/testData/klib/partial-linkage/lessVisibleMemberOverridesPublic/lib1/l1.kt
@@ -0,0 +1,11 @@
+open class ClassWithAddedFinal {
+   fun test(): Int = 1
+}
+
+open class ClassWithAddedOpen {
+   fun test(): Int = 1
+}
+
+abstract class ClassWithAddedAbstract {
+   fun test(): Int = 1
+}
