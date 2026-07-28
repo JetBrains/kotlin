2f134cf7365be3ef8b7e462387bae8d47cbb309e
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2026-07-28 09:06:39 +0000

    [K/N] Documentation and assertions for GlobalData initialization
    GlobalData initialization is a lazy process that's driven from outside
    (from Runtime.cpp). To make sure the initialization happens correctly,
    a number of asserts are used.
    
    This commit adds a bit of a documentation and adjusts assertions to
    make them more clear.
    
    ^KT-87963

--- kotlin-native/runtime/src/mm/cpp/GlobalData.cpp
+++ kotlin-native/runtime/src/mm/cpp/GlobalData.cpp
@@ -5,17 +5,30 @@
 
 #include "GlobalData.hpp"
 
+#include <atomic>
 #include <condition_variable>
 #include <mutex>
+#include <thread>
 
 #include "CompilerConstants.hpp"
-#include "Porting.h"
+#include "ManuallyScoped.hpp"
 
 #if KONAN_WINDOWS
 #include "concurrent/Mutex.hpp"
 #endif
 
 using namespace kotlin;
+
+// TODO(KT-71143): try to simplify when the initialization code lives in a single place.
+//
+// `GlobalData` lifetime:
+// * created by the first thread entering K/N runtime (decided in Runtime.cpp) in the call to
+//   `GlobalData::init()`;
+// * all other threads call `waitGlobalDataInitialized()` during their first calls into K/N runtime;
+// * every access into `GlobalData` happens via `GlobalData::Instance()`:
+//   * the first thread is allowed that only during (or after the) call to `GlobalData::init()`
+//   * all other threads only after `waitGlobalDataInitialized()`
+// * `GlobalData` is never destroyed.
 
 namespace {
 
@@ -33,13 +46,13 @@ const char* initStateToString(InitState state) noexcept {
     }
 }
 
-std::atomic<InitState> globalDataInitState = InitState::kUninitialized;
-std::atomic<std::thread::id> globalDataInitializingThread{};
+std::atomic<InitState> globalDataInitState = InitState::kUninitialized; // can be read outside of mutex
+std::atomic<std::thread::id> globalDataInitializingThread{}; // can be read outside of mutex
 ManuallyScoped<mm::GlobalData> globalDataInstance{};
 
 #if KONAN_WINDOWS
 // On winpthreads, there's a weird bug if this is a regular `std::mutex`:
-// even though `constructGlobalDataInstance()` has already started (and so,
+// even though `GlobalData::init()` has already started (and so,
 // has already successfully tried locking this mutex), `waitGlobalDataInitialized`
 // may crash trying to lock it too.
 SpinLock globalDataInitMutex;
@@ -56,9 +69,25 @@ SpinLock globalDataInitMutex;
 // static
 mm::GlobalData& mm::GlobalData::Instance() noexcept {
     if (compiler::runtimeAssertsEnabled()) {
-        auto s = globalDataInitState.load(std::memory_order_relaxed);
-        auto initializingThread = globalDataInitializingThread.load(std::memory_order_relaxed);
-        RuntimeAssert(s == InitState::kInitialized || initializingThread == std::this_thread::get_id(), "Expected state %s, but was %s.", initStateToString(InitState::kInitialized), initStateToString(s));
+        // Synchronizes with release in `GlobalData::init`.
+        // NOTE: this acquire changes behaviour: if a thread didn't call `waitGlobalDataInitialized`, but the
+        //       `GlobalData` instance was created in parallel, then
+        //       * with assertions enabled, this acquire creates happens-before for stuff inside `globalDataInstance` below
+        //         and something like TSAN won't help here.
+        //       * with assertions disabled, the happens-before wouldn't be created and accessing `globalDataInstance` is illegal
+        switch (globalDataInitState.load(std::memory_order_acquire)) {
+            case InitState::kUninitialized:
+                RuntimeAssert(false, "The thread must have called GlobalData::init or waitGlobalDataInitialized first");
+                break;
+            case InitState::kInitializing:
+                // This relaxed read is fine, because it's written into before the release on `globalDataInitState`.
+                RuntimeAssert(
+                        globalDataInitializingThread.load(std::memory_order_relaxed) == std::this_thread::get_id(),
+                        "Only the initializing thread is allowed to access Instance in the initializing state");
+                break;
+            case InitState::kInitialized:
+                break;
+        }
     }
     return *globalDataInstance;
 }
@@ -66,34 +95,35 @@ mm::GlobalData& mm::GlobalData::Instance() noexcept {
 // static
 void mm::GlobalData::init() noexcept {
     std::unique_lock guard{globalDataInitMutex};
-    auto initialState = InitState::kUninitialized;
-    globalDataInitState.compare_exchange_strong(initialState, InitState::kInitializing, std::memory_order_acq_rel);
-    RuntimeAssert(initialState == InitState::kUninitialized, "Expected state %s, but was %s", initStateToString(InitState::kUninitialized), initStateToString(initialState));
-    globalDataInitializingThread.store(std::this_thread::get_id(), std::memory_order_relaxed);
+    if (compiler::runtimeAssertsEnabled()) {
+        auto s = globalDataInitState.load(std::memory_order_relaxed);
+        RuntimeAssert(s == InitState::kUninitialized, "GlobalData is expected to be uninitialized, but is %s", initStateToString(s));
+    }
+    globalDataInitializingThread.store(std::this_thread::get_id(), std::memory_order_relaxed); // must be before the release below
+    // Synchronizes with acquire in `GlobalData::Instance` (assert only)
+    globalDataInitState.store(InitState::kInitializing, std::memory_order_release);
 
     globalDataInstance.construct();
 
-    auto initializingState = InitState::kInitializing;
-    globalDataInitState.compare_exchange_strong(initializingState, InitState::kInitialized, std::memory_order_acq_rel);
-    RuntimeAssert(initializingState == InitState::kInitializing, "Expected state %s, but was %s", initStateToString(InitState::kInitializing), initStateToString(initializingState));
+    // Synchronizes with acquire in `waitGlobalDataInitialized` and `GlobalData::Instance` (assert only)
+    globalDataInitState.store(InitState::kInitialized, std::memory_order_release);
     guard.unlock();
     globalDataInitCV.notify_all();
 }
 
 void mm::waitGlobalDataInitialized() noexcept {
+    // Synchronizes with release in `GlobalData::init`.
     if (globalDataInitState.load(std::memory_order_acquire) == InitState::kInitialized) {
         return;
     }
-    if (compiler::runtimeAssertsEnabled()) {
-        auto initializingThread = globalDataInitializingThread.load(std::memory_order_relaxed);
-        RuntimeAssert(initializingThread != std::this_thread::get_id(), "A thread that initialized global data cannot be waiting for its initialization");
-    }
+    // We want this assert before taking the lock to catch a potential deadlock when `globalDataInitializingThread ==
+    // std::this_thread::get_id`. Relaxed read is fine for this condition: to be triggered, the write must have happened on our thread
+    // (other threads can't write our `get_id()`)
+    RuntimeAssert(
+            globalDataInitializingThread.load(std::memory_order_relaxed) != std::this_thread::get_id(),
+            "A thread that initialized global data cannot be waiting for its initialization");
     std::unique_lock guard{globalDataInitMutex};
-    globalDataInitCV.wait(guard, []() noexcept {
-            return globalDataInitState.load(std::memory_order_relaxed) == InitState::kInitialized;
-            });
+    globalDataInitCV.wait(guard, []() noexcept { return globalDataInitState.load(std::memory_order_relaxed) == InitState::kInitialized; });
 }
 
 mm::GlobalData::GlobalData() noexcept = default;
-
-// static
