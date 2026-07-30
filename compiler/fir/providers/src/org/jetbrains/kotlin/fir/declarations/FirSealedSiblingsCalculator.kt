95fa392c302cedf779a54bb4215dc3cd250634d3
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-07-30 15:23:01 +0000

    [FIR] Move `getComplementarySymbols` into the new calculator

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/declarations/FirSealedSiblingsCalculator.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/declarations/FirSealedSiblingsCalculator.kt
@@ -11,9 +11,16 @@ import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.fir.ThreadSafeMutableState
 import org.jetbrains.kotlin.fir.caches.FirCache
 import org.jetbrains.kotlin.fir.caches.firCachesFactory
+import org.jetbrains.kotlin.fir.declarations.utils.isClass
+import org.jetbrains.kotlin.fir.declarations.utils.isFinal
+import org.jetbrains.kotlin.fir.declarations.utils.isSealed
 import org.jetbrains.kotlin.fir.declarations.utils.modality
 import org.jetbrains.kotlin.fir.isJavaNonAbstractSealed
+import org.jetbrains.kotlin.fir.resolve.getSuperTypes
+import org.jetbrains.kotlin.fir.resolve.isSubclassOf
 import org.jetbrains.kotlin.fir.resolve.providers.symbolProvider
+import org.jetbrains.kotlin.fir.resolve.toRegularClassSymbol
+import org.jetbrains.kotlin.fir.resolve.toSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.fir.symbols.impl.FirRegularClassSymbol
 
@@ -40,6 +47,25 @@ class FirSealedSiblingsCalculator(private val session: FirSession) : FirSessionC
 
     fun collectAllSubclassesFor(symbol: FirClassSymbol<*>): Set<FirClassSymbol<*>> =
         allSubclassesCache.getValue(symbol, context = null)
+
+    private fun FirClassSymbol<*>.isSubclassOf(other: FirClassSymbol<*>): Boolean =
+        isSubclassOf(other.toLookupTag(), session, isStrict = false, lookupInterfaces = true)
+
+    private fun areUnrelated(a: FirClassSymbol<*>, b: FirClassSymbol<*>): Boolean =
+        !a.isSubclassOf(b) && !b.isSubclassOf(a)
+
+    fun collectComplementarySymbolsFor(symbol: FirRegularClassSymbol): List<FirClassSymbol<*>> {
+        val superTypes = symbol.getSuperTypes(session)
+            .mapNotNullTo(mutableSetOf()) { it.toRegularClassSymbol(session) }
+
+        return superTypes.flatMap { superType ->
+            if (!superType.isSealed) return@flatMap emptyList()
+
+            superType.fir.getSealedClassInheritors(session)
+                .mapNotNull { it.toSymbol(session) as? FirRegularClassSymbol }
+                .filter { (symbol.isFinal || it.isFinal || symbol.isClass && it.isClass) && areUnrelated(symbol, it) }
+        }
+    }
 }
 
 val FirSession.sealedSiblingsCalculator: FirSealedSiblingsCalculator by FirSession.sessionComponentAccessor()
