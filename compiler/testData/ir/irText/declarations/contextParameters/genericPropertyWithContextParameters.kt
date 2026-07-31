9336a71a438e3de7728f71d9f9796e121e9ada48
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2026-07-31 17:11:59 +0300

    [Tests] KT-85896: cases with nested class and multiple context type parameters
    ^KT-85896

--- compiler/testData/ir/irText/declarations/contextParameters/genericPropertyWithContextParameters.kt
+++ compiler/testData/ir/irText/declarations/contextParameters/genericPropertyWithContextParameters.kt
@@ -7,3 +7,19 @@ class C<Z> {
         get() = x
         set(value) {}
 }
+
+class A<Z> {
+    class N<T> {
+        context(x: X, y: Y)
+        var <X, Y> ctx: X
+            get() = x
+            set(value) {}
+    }
+}
+
+class B<Z> {
+    context(x: X, y: Map<X, Y>)
+    var <X, Y> ctx: X
+        get() = x
+        set(value) {}
+}
