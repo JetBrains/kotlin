34a6626f4248e1caa490304740d91637dd9c0d4b
Brian Norman <brian.norman@jetbrains.com>
2026-08-03 09:52:42 -0500

    [PowerAssert] Handle implicit context parameters in arguments
    Improve Power-Assert's detection of expressions with synthetic or
    undefined source offsets. These elements should be excluded from the
    resulting display, since they do not have a valid source range.
    
    ^RCA: Power-Assert tests for context parameters only used `with()` to
    introduce the available implicit argument. This results in different
    access expressions for IR from an implicit argument introduced using
    `contenxt()`. Adding tests using this function helped reproduce the
    regression.
    
    ^KT-88179 Fixed

--- plugins/power-assert/power-assert-compiler/testData/codegen/parameters/ContextReceivers.kt
+++ plugins/power-assert/power-assert-compiler/testData/codegen/parameters/ContextReceivers.kt
@@ -3,9 +3,13 @@
 // FUNCTION: context2Assert
 // DUMP_KT_IR
 
+// ISSUE: KT-88179
+
 fun box(): String = runAll(
     "test1" to { test1() },
     "test2" to { test2() },
+    "test3" to { test3() },
+    "test4" to { test4() },
 )
 
 data object Asserter
@@ -27,7 +31,19 @@ fun test1() {
 }
 
 fun test2() {
+    context(Asserter) {
+        context1Assert("test".length == 5)
+    }
+}
+
+fun test3() {
     with(Asserter) {
+        context2Assert("test".length == 5)
+    }
+}
+
+fun test4() {
+    context(Asserter) {
         context2Assert("test".length == 5)
     }
 }
