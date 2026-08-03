071506909c2d0f4ae3715b618d01d33e16d7f561
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-08-03 18:05:48 +0200

    Scripting: fix order of imported and main script
    ^KT-87958 fixed

--- libraries/tools/kotlin-main-kts-test/test/org/jetbrains/kotlin/mainKts/test/mainKtsTest.kt
+++ libraries/tools/kotlin-main-kts-test/test/org/jetbrains/kotlin/mainKts/test/mainKtsTest.kt
@@ -143,6 +143,17 @@ class MainKtsTest {
     }
 
     @Test
+    fun testImportKt87958() {
+
+        val out = captureOut {
+            val res = evalFile(File("$TEST_DATA_ROOT/kt87958/testing.main.kts"))
+            assertSucceeded(res)
+        }.lines()
+
+        Assert.assertEquals(listOf("Ok"), out)
+    }
+
+    @Test
     fun testImport() {
 
         val out = captureOut {
