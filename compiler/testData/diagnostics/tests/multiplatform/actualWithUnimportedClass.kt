1891e89d9d06d625d622d75932bc26ea0bc54a4f
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-08-03 10:43:33 +0200

    [Tests] Reproduce #KT-68819

---
+++ compiler/testData/diagnostics/tests/multiplatform/actualWithUnimportedClass.kt
@@ -0,0 +1,19 @@
+// IGNORE_FIR_DIAGNOSTICS
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-68819
+// MODULE: m1-common
+// FILE: common.kt
+package pkg
+
+class Foo
+
+// FILE: common2.kt
+import pkg.Foo
+
+<!EXPECT_ACTUAL_IR_MISMATCH{JVM}!>expect<!> fun foo(f: Foo)
+// MODULE: m1-jvm()()(m1-common)
+// FILE: jvm.kt
+
+actual fun <!ACTUAL_WITHOUT_EXPECT!>foo<!>(f: <!UNRESOLVED_REFERENCE!>Foo<!>) {}
+
+/* GENERATED_FIR_TAGS: actual, classDeclaration, expect, functionDeclaration */
