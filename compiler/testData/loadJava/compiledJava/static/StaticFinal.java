439fd2e1401ea7c3a56942d8f12eb54052db979c
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-08-03 10:06:04 +0200

    Tests: create dumps for AbstractJvmRuntimeDescriptorLoaderTest
    Remove existing .txt dumps because they were only used by K1 compiler
    tests, which no longer exist. Generate dumps from the runtime descriptor
    test and save them to `.runtime.txt`. This will allow to remove code
    that loads descriptors from sources in this test.

--- compiler/testData/loadJava/compiledJava/static/StaticFinal.java
+++ compiler/testData/loadJava/compiledJava/static/StaticFinal.java
@@ -1,4 +1,5 @@
-// SKIP_IN_RUNTIME_TEST because there's no stable way to determine if a field is initialized with a non-null value in runtime
+// SKIP_IN_RUNTIME_TEST
+// ^ There's no stable way to determine if a field is initialized with a non-null value in runtime.
 
 package test;
 
