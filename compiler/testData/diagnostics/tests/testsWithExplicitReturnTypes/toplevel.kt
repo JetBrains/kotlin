7619bfe006e0ab33e0d7208e791c9f224f2b9aaa
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-08-03 08:11:00 +0000

    [FIR] Add a test for `NO_EXPLICIT_RETURN_TYPE` with a `private` fun

--- compiler/testData/diagnostics/tests/testsWithExplicitReturnTypes/toplevel.kt
+++ compiler/testData/diagnostics/tests/testsWithExplicitReturnTypes/toplevel.kt
@@ -11,4 +11,6 @@ fun <!NO_EXPLICIT_RETURN_TYPE_IN_API_MODE!>bar<!>() = 10
 public fun <!NO_EXPLICIT_RETURN_TYPE_IN_API_MODE!>bar2<!>() = 10
 public fun bar3(): Int = 10
 
+private fun bar4() = 10
+
 /* GENERATED_FIR_TAGS: functionDeclaration, integerLiteral */
