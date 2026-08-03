26448490695c46efb79542723d8f6b1420c194c7
Jim M. R. Teichgräber <jim.teichgraeber@jetbrains.com>
2026-08-03 08:57:05 +0200

    [Wasm, Tests] Mute stepping tests in single-/multi-module (^KT-88234)
    These have been failing since the grouped tets infra for Wasm was
    introduced (#6231). This change required a large diff to increase
    the line number by 2/3 respectively for each line in each block.

--- compiler/testData/debug/stepping/kt82044.kt
+++ compiler/testData/debug/stepping/kt82044.kt
@@ -1,5 +1,7 @@
 // DONT_TARGET_EXACT_BACKEND: JS_IR
 // ^^^ Test hangs while executing the debugger.
+// WASM_IGNORE_FOR: mode=single-module
+// ^^^ KT-88234
 
 // MODULE: a
 // FILE: a.kt
@@ -19,21 +21,21 @@ fun box(): String {
 }
 
 // EXPECTATIONS JVM_IR
-// test.kt:15 box
-// a.kt:8 box
-// test.kt:16 box
 // test.kt:17 box
-
-// EXPECTATIONS NATIVE
-// test.kt:15 box
-// a.kt:8 box
-// test.kt:16 box
-// test.kt:16 box
-// test.kt:17 box
+// a.kt:10 box
+// test.kt:18 box
 // test.kt:19 box
 
+// EXPECTATIONS NATIVE
+// test.kt:17 box
+// a.kt:10 box
+// test.kt:18 box
+// test.kt:18 box
+// test.kt:19 box
+// test.kt:21 box
+
 // EXPECTATIONS WASM
-// test.kt:15 $box (4, 8)
-// a.kt:8 $box (17, 11)
-// test.kt:16 $box (21, 26, 21)
-// test.kt:17 $box (15, 8)
+// test.kt:17 $box (4, 8)
+// a.kt:10 $box (17, 11)
+// test.kt:18 $box (21, 26, 21)
+// test.kt:19 $box (15, 8)
