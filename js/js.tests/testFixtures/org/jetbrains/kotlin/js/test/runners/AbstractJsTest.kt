2758d60e95ace6d66c3ab929979a79605b74c800
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-08-03 14:12:42 +0300

    [Tests] Fix `AbstractJsCodegenBoxWithInlinedFunInKlibTest`
    Codegen box tests, such as `generticReferences.kt`, add a second copy of
    `IrTextDumpHandler` with `directive = DUMP_IR_AFTER_INLINE`, and a custom
    extension `.inlined.ir`, but these instances still use the same
    `DUMP_IR_DIFFERENCE` as the regular ones, making some internal asserts
    fail with "the `.inline.ir` file doesn't exist" kind of errors.
    
    Without this commit, the tests from the previous one fail.

--- js/js.tests/testFixtures/org/jetbrains/kotlin/js/test/runners/AbstractJsTest.kt
+++ js/js.tests/testFixtures/org/jetbrains/kotlin/js/test/runners/AbstractJsTest.kt
@@ -27,6 +27,7 @@ import org.jetbrains.kotlin.test.configuration.commonFirHandlersForCodegenTest
 import org.jetbrains.kotlin.test.configuration.commonIrHandlersForCodegenTest
 import org.jetbrains.kotlin.test.configuration.setupIrTextDumpHandlers
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.DUMP_IR_AFTER_INLINE
+import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.DUMP_IR_AFTER_INLINE_DIFFERENCE
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.IGNORE_BACKEND_K2_MULTI_MODULE
 import org.jetbrains.kotlin.test.directives.DiagnosticsDirectives
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
@@ -151,6 +152,7 @@ abstract class AbstractJsCodegenBoxWithInlinedFunInKlibTest(
                             artifactKind = artifactKind,
                             customExtension = "inlined.ir",
                             directive = DUMP_IR_AFTER_INLINE,
+                            directiveForIrDifference = DUMP_IR_AFTER_INLINE_DIFFERENCE,
                             showOffsets = true,
                         )
                     },
