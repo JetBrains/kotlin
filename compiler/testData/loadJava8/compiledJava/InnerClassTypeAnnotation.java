4696f5075412ac12b94eceb176670a1566554f5c
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-08-03 10:27:40 +0200

    Tests: remove unneeded mutes of runtime descriptor loader tests
    Now that there are .txt dumps, mute is only needed for one test
    (`StringArrayArgument.kt`) where a runtime exception happens.

--- compiler/testData/loadJava8/compiledJava/InnerClassTypeAnnotation.java
+++ compiler/testData/loadJava8/compiledJava/InnerClassTypeAnnotation.java
@@ -1,8 +1,6 @@
 // JAVAC_EXPECTED_FILE
 package test;
 
-// SKIP_IN_RUNTIME_TEST
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
