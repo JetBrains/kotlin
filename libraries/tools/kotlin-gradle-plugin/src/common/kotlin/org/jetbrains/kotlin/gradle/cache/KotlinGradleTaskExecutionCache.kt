434e190dc9180864434fbdb0f9d6ee0157ee8597
Anton Lakotka <anton.lakotka@jetbrains.com>
2026-08-04 08:32:59 +0200

    [Gradle] Rework KotlinGradleTaskExecutionCache to be thread safe
    This is a followup after PR discussion
    (https://github.com/JetBrains/kotlin/pull/6887)
    
    The new implementation is inspired by
    "Java Concurrency in Practice" book by
    p5.6. Building an Efficient, Scalable Result Cache
    
    There are few things:
    1. I should have named it not "Cache" but Memoizer :)
    2. FutureTask is used to compute value instead of using computeIfAbsent
    because being slow in computeIfAbsent would introduce more issues
    and cause blocking even on unrelated keys.
    3. Forbid double entries (not for every case as when thread
    A spawns thread B inside getOrCompute who calls another getOrCompute.
    But chances to that is low as there is no such practice in KGP.
    But protection from bug number '1D10T' on the same thread should help.

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/cache/KotlinGradleTaskExecutionCache.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/cache/KotlinGradleTaskExecutionCache.kt
@@ -11,7 +11,9 @@ import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
 import org.jetbrains.kotlin.gradle.utils.registerClassLoaderScopedBuildService
 import org.jetbrains.kotlin.tooling.core.Extras
-import org.jetbrains.kotlin.tooling.core.mutableExtrasOf
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.FutureTask
 
 internal val Project.kotlinGradleTaskExecutionCache: Provider<KotlinGradleTaskExecutionCache>
     get() = gradle.registerClassLoaderScopedBuildService(KotlinGradleTaskExecutionCache::class)
@@ -21,23 +23,81 @@ internal val Project.kotlinGradleTaskExecutionCache: Provider<KotlinGradleTaskEx
  * i.e. all subprojects can store and access shared instances in this storage service.
  */
 internal abstract class KotlinGradleTaskExecutionCache : BuildService<BuildServiceParameters.None> {
-    private val extras = mutableExtrasOf()
+    private val storage = KotlinConcurrentGetOrComputeStorage()
 
-    operator fun <V> contains(key: Extras.Key<V>): Boolean = extras.contains(key)
-    operator fun <V> set(key: Extras.Key<V>, value: V): V? = extras.set(key, value)
-    operator fun <V> get(key: Extras.Key<V>): V? = extras[key]
+    fun <V> getOrCompute(
+        key: Extras.Key<V>,
+        compute: () -> V
+    ): V = storage.getOrCompute(key, compute)
 }
 
-@Suppress("UNCHECKED_CAST")
-internal inline fun <V> KotlinGradleTaskExecutionCache.getOrPutSynchronized(
-    key: Extras.Key<V>,
-    compute: () -> V
-): V {
-    if (contains(key)) return get(key) as V
-    synchronized(this) {
-        if (contains(key)) return get(key) as V
-        val value = compute()
-        set(key, value)
-        return value
+/**
+ * Flag to prevent from double entries, even with different keys.
+ */
+private val computingOnThread = ThreadLocal.withInitial { false }
+
+/**
+ * Should be used only in [KotlinGradleTaskExecutionCache], extracted to separate class for testing needs
+ */
+internal class KotlinConcurrentGetOrComputeStorage {
+    private val hashMap = ConcurrentHashMap<Extras.Key<*>, FutureTask<*>>()
+
+    /**
+     * Gets existing value by [key] or computes new one using [compute].
+     * Computation will happen once. If it failed, the failure will be stored forever.
+     * This is expected and desired behavior for the [KotlinGradleTaskExecutionCache] usecases.
+     *
+     * TL;DR: [compute] should be idempotent and return the same result for the same [key].
+     *
+     * **Warning** double entry is not accepted!
+     * Avoid writing code like this:
+     * ```kotlin
+     * getOrCompute(key) {
+     *   getOrCompute(key) { // or even different key
+     *     // computation
+     *   }
+     * }
+     * ```
+     */
+    fun <V> getOrCompute(
+        key: Extras.Key<V>,
+        compute: () -> V
+    ): V {
+        /** implementation note:
+         * It is theoretically possible to use [ConcurrentHashMap.computeIfAbsent]
+         * but, according to its Javadoc, when [compute] is too slow, it will cause unnecessary blocks even for different keys.
+         * */
+
+        check(!computingOnThread.get()) { "Double entry is not accepted!" }
+        var existingTask = hashMap[key]
+        val task = if (existingTask != null) {
+            // Round 1: try getting fast
+            existingTask
+        } else {
+            // Round 2: allocate newTask and try to put it
+            val newTask = FutureTask(compute)
+            existingTask = hashMap.putIfAbsent(key, newTask)
+            if (existingTask == null) {
+                // Round 3: we're the first one, compute the value, and let other threads to wait
+                computingOnThread.set(true)
+                try {
+                    newTask.run()
+                } finally {
+                    computingOnThread.set(false)
+                }
+                newTask
+            } else existingTask
+        }
+
+        try {
+            // blocks if necessary to await completion by other thread
+            @Suppress("UNCHECKED_CAST")
+            return task.get() as V
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+            throw e
+        } catch (e: ExecutionException) {
+            throw e.cause ?: e
+        }
     }
 }
