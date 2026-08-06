281e993eded2f253c906a3e1ec904f832ccfa558
igor.yakovlev <igor.yakovlev@jetbrains.com>
2026-08-06 18:54:20 +0200

    [Wasm] Commonise FP parser and stringifier with Native
    Fix ^KT-88414

--- libraries/stdlib/wasm/builtins/kotlin/Primitives.kt
+++ libraries/stdlib/wasm/builtins/kotlin/Primitives.kt
@@ -2274,7 +2274,7 @@ public actual class Float private constructor(private val value: Float) : Number
 
     @kotlin.internal.IntrinsicConstEvaluation
     public actual override fun toString(): String =
-        dtoa(this.toDouble(), isSinglePrecision = true)
+        NumberConverter.convert(this)
 
     @kotlin.internal.IntrinsicConstEvaluation
     public actual override fun equals(other: Any?): Boolean =
@@ -2676,7 +2676,7 @@ public actual class Double private constructor(private val value: Double) : Numb
 
     @kotlin.internal.IntrinsicConstEvaluation
     public actual override fun toString(): String =
-        dtoa(this, isSinglePrecision = false)
+        NumberConverter.convert(this)
 
     @kotlin.internal.IntrinsicConstEvaluation
     public actual override fun equals(other: Any?): Boolean =
