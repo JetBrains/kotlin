74d2b8301ca49fd0ca2c31eaf9de33ec1e7c0a6e
Francesco Protopapa <fprotopapa@google.com>
2026-08-06 14:03:29 +0200

    [Parcelize] Fix `Parceler` supertype retrieving when creating a custom serializer

--- plugins/parcelize/parcelize-compiler/parcelize.backend/src/org/jetbrains/kotlin/parcelize/IrParcelerScope.kt
+++ plugins/parcelize/parcelize-compiler/parcelize.backend/src/org/jetbrains/kotlin/parcelize/IrParcelerScope.kt
@@ -9,10 +9,9 @@ import org.jetbrains.kotlin.ir.declarations.IrAnnotationContainer
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.types.*
 import org.jetbrains.kotlin.ir.util.classId
-import org.jetbrains.kotlin.ir.util.constructedClass
 import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.getAllSubstitutedSupertypes
 import org.jetbrains.kotlin.ir.util.isNullable
-import org.jetbrains.kotlin.ir.util.superTypes
 import org.jetbrains.kotlin.parcelize.ParcelizeNames.TYPE_PARCELER_FQ_NAMES
 import org.jetbrains.kotlin.parcelize.ParcelizeNames.WRITE_WITH_FQ_NAMES
 
@@ -35,8 +34,8 @@ fun IrParcelerScope?.getCustomSerializer(irType: IrType): IrParcelSerializer? {
     }
     writeWithArgumentType?.getClass()?.let { parceler ->
         val customSerializer = IrCustomParcelSerializer(parceler)
-        val parcelerSupertype = writeWithArgumentType.superTypes()
-            .single { it.classOrNull?.owner?.classId in ParcelizeNames.PARCELER_CLASS_IDS } as IrSimpleType
+        val parcelerSupertype = getAllSubstitutedSupertypes(parceler)
+            .first { it.classOrNull?.owner?.classId in ParcelizeNames.PARCELER_CLASS_IDS }
         val parcelerTypeArgument = parcelerSupertype.arguments.single().typeOrFail
         return if (irType.isNullable() && !parcelerTypeArgument.isNullable()) IrNullAwareParcelSerializer(customSerializer)
         else customSerializer
