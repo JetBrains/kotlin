33840d3d958e869240d88e40cff37b60b0f4503e
Aleksandr Shefer <aleksandr.shefer@jetbrains.com>
2026-08-06 17:18:53 +0000

    [Wasm] ignore failing coroutines stack switching tests via WASM_IGNORE_FOR (KT-88366)

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/generators/tests/GenerateWasmTests.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/generators/tests/GenerateWasmTests.kt
@@ -36,13 +36,6 @@ fun main(args: Array<String>) {
     // TODO: Remove excludedPattern below after fix of KT-78960 (it's simpler to exclude temporarily than to split test `boxInline/innerClasses/kt12126.kt`)
     val excludedPatternForBoxInlineTestsWithInliner = "kt12126.kt"
 
-    // These tests exercise low-level coroutine intrinsics.
-    // The Stack Switching implementation intentionally doesn't reproduce those intrinsic semantics.
-    // Supporting them would require extra flags/checks for working with special cases of internal testing.
-    // We exclude these tests instead of muting them per-mode.
-    val excludedPatternStackSwitchingCoroutines =
-        "^(intercepted|startCoroutineUninterceptedOrReturn|suspendCoroutineUninterceptedOrReturn)\\.kt$"
-
     generateTestGroupSuiteWithJUnit5(args) {
         testGroup(testsRoot, "compiler/testData/klib/partial-linkage") {
             testClass<AbstractWasmPartialLinkageNoICTestCase> {
@@ -153,27 +146,15 @@ fun main(args: Array<String>) {
             }
 
             testClass<AbstractWasmJsCodegenCoroutinesStackSwitchingTest> {
-                model(
-                    "codegen/box/coroutines",
-                    pattern = jsTranslatorTestPattern,
-                    excludedPattern = excludedPatternStackSwitchingCoroutines
-                )
+                model("codegen/box/coroutines", pattern = jsTranslatorTestPattern)
             }
 
             testClass<AbstractFirWasmJsCodegenCoroutinesStackSwitchingSingleModuleTest> {
-                model(
-                    "codegen/box/coroutines",
-                    pattern = jsTranslatorTestPattern,
-                    excludedPattern = excludedPatternStackSwitchingCoroutines
-                )
+                model("codegen/box/coroutines", pattern = jsTranslatorTestPattern)
             }
 
             testClass<AbstractWasmJsCodegenCoroutinesStackSwitchingMultiModuleTest> {
-                model(
-                    "codegen/box/coroutines",
-                    pattern = jsTranslatorTestPattern,
-                    excludedPattern = excludedPatternStackSwitchingCoroutines
-                )
+                model("codegen/box/coroutines", pattern = jsTranslatorTestPattern)
             }
 
             testClass<AbstractWasmJsCodegenBoxInlinedTest>(annotations = listOf(annotation<WasmJsBoxInlinedTest>())) {
