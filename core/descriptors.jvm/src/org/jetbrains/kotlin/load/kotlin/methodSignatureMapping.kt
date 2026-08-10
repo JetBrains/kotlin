0cbf9818c43e5c850e449e4f6c151dcab6056e9b
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-08-10 12:02:15 +0000

    [Native] Rewrite `getDefaultValueForOverriddenBuiltinFunction`
    Previously we used descriptors, but K1 is deprecated, and we are
    actvely removing it.
    
    This is 1-to-1 rewrite. In the next commit we are going to simplify
    code a little bit.
    
    #KT-85346

--- core/descriptors.jvm/src/org/jetbrains/kotlin/load/kotlin/methodSignatureMapping.kt
+++ core/descriptors.jvm/src/org/jetbrains/kotlin/load/kotlin/methodSignatureMapping.kt
@@ -82,7 +82,7 @@ fun forceSingleValueParameterBoxing(f: CallableDescriptor): Boolean {
 }
 
 // This method only returns not-null for class methods
-fun CallableDescriptor.computeJvmSignature(): String? = signatures {
+internal fun CallableDescriptor.computeJvmSignature(): String? = signatures {
     if (DescriptorUtils.isLocal(this@computeJvmSignature)) return null
 
     val classDescriptor = containingDeclaration as? ClassDescriptor ?: return null
