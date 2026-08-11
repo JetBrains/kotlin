8933c52462f1bd87159dad2bb15a370f89df44bb
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-08-11 15:26:00 +0200

    [test] Extract `ArgumentsHelper` into a separate file.
    We want to convert it to Kotlin — it's more convenient to extract
    it into a separate file first.
    
    KT-87774

--- js/js.tests/testFixtures/org/jetbrains/kotlin/js/testOld/utils/DirectiveTestUtils.java
+++ js/js.tests/testFixtures/org/jetbrains/kotlin/js/testOld/utils/DirectiveTestUtils.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2010-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.js.testOld.utils;
@@ -28,8 +17,6 @@ import org.junit.jupiter.api.Assertions;
 
 import java.io.File;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static kotlin.test.AssertionsKt.assertFalse;
 import static kotlin.test.AssertionsKt.assertTrue;
@@ -691,73 +678,6 @@ public class DirectiveTestUtils {
         @NotNull
         String getName() {
             return directive;
-        }
-    }
-
-    /**
-     * Arguments format: ((namedArg|positionalArg)\s+)*`
-     *
-     * Where: namedArg -- 'key=value' or 'key="spaced value"'
-     *        positionalArg -- 'value'
-     *
-     * Neither key, nor value should contain spaces.
-     */
-    private static class ArgumentsHelper {
-        private final List<String> positionalArguments = new ArrayList<>();
-        private final Map<String, String> namedArguments = new HashMap<>();
-        private final String entry;
-        private final Pattern argumentsPattern = Pattern.compile("[\\w$_;\\.]+(=((\".*?\")|[\\w$_;\\.]+))?");
-        final File sourceFile;
-        ArgumentsHelper(@NotNull String directiveEntry, @NotNull File directiveSourceFile) {
-            entry = directiveEntry;
-            sourceFile = directiveSourceFile;
-
-            Matcher matcher = argumentsPattern.matcher(directiveEntry);
-
-            while (matcher.find()) {
-                String argument = matcher.group();
-                String[] keyVal = argument.split("=");
-                switch (keyVal.length) {
-                    case 1: positionalArguments.add(keyVal[0]); break;
-                    case 2:
-                        String value = keyVal[1];
-                        if (value.charAt(0) == '"') {
-                            value = value.substring(1, value.length() - 1);
-                        }
-                        namedArguments.put(keyVal[0], value);
-                        break;
-                    default: throw new AssertionError("Wrong argument format: " + argument);
-                }
-            }
-        }
-
-        @NotNull
-        String getFirst() {
-            return getPositionalArgument(0);
-        }
-
-        @NotNull
-        String getPositionalArgument(int index) {
-            assert positionalArguments.size() > index: "Argument at index `" + index + "` not found in entry: " + entry;
-            return positionalArguments.get(index);
-        }
-
-        @NotNull
-        String getNamedArgument(@NotNull String name) {
-            assert namedArguments.containsKey(name): "Argument `" + name + "` not found in entry: " + entry;
-            return namedArguments.get(name);
-        }
-
-        @Nullable
-        String findNamedArgument(@NotNull String name) {
-            return namedArguments.get(name);
-        }
-
-        @NotNull
-        List<String> findNamedListArgument(@NotNull String name) {
-            String value = findNamedArgument(name);
-            if (value == null) return Collections.emptyList();
-            return StringUtil.split(value, ";");
         }
     }
 }
