28a08b7eddec91cd4d6af76dabfffb96624b3ad3
Dmitrii Galimzianov <dmitrii.galimzianov@jetbrains.com>
2026-08-11 05:41:59 +0200

    [K/N] CExport Phase 2 IR

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/cexport/CAdapterCodegen.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/cexport/CAdapterCodegen.kt
@@ -14,10 +14,9 @@ import org.jetbrains.kotlin.backend.konan.llvm.ExceptionHandler
 import org.jetbrains.kotlin.backend.konan.llvm.Lifetime
 import org.jetbrains.kotlin.backend.konan.lower.getLoweredConstructorFunction
 import org.jetbrains.kotlin.backend.konan.lower.getObjectClassInstanceFunction
-import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.util.isOverridable
-import org.jetbrains.kotlin.resolve.DescriptorUtils
+import org.jetbrains.kotlin.ir.util.isTopLevel
 
 /**
  * Second phase of C Export: build bitcode bridges from C wrappers to Kotlin functions.
@@ -41,8 +40,8 @@ internal class CAdapterCodegen(
     private fun buildCAdapter(exportedElement: ExportedElement): Unit = with(exportedElement) {
         when {
             isFunction -> {
-                val function = declaration as FunctionDescriptor
-                val irFunction = irSymbol.owner.let {
+                val originalFunction = irSymbol.owner as IrFunction
+                val irFunction = originalFunction.let {
                     it as? IrSimpleFunction ?: context.getLoweredConstructorFunction(it as IrConstructor)
                 }
                 cname = "_konan_function_${owner.nextFunctionIndex()}"
@@ -50,7 +49,7 @@ internal class CAdapterCodegen(
                 val bridgeFunctionProto = signature.toProto(cname, null, LLVMLinkage.LLVMExternalLinkage)
                 // If function is virtual, we need to resolve receiver properly.
                 generateFunction(codegen, bridgeFunctionProto) {
-                    val callee = if (!DescriptorUtils.isTopLevelDeclaration(function) && irFunction.isOverridable) {
+                    val callee = if (!originalFunction.isTopLevel && irFunction.isOverridable) {
                         codegen.getVirtualFunctionTrampoline(irFunction)
                     } else {
                         // KT-45468: Alias insertion may not be handled by LLVM properly, in case callee is in the cache.
