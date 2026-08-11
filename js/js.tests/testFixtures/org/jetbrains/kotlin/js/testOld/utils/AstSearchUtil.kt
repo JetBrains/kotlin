ebc8324a4e5d2a19a36928f8ab9caea454e25fba
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-08-11 15:13:36 +0200

    [test] Convert `AstSearchUtil`  from Java to Kotlin
    KT-87774

--- js/js.tests/testFixtures/org/jetbrains/kotlin/js/testOld/utils/AstSearchUtil.kt
+++ js/js.tests/testFixtures/org/jetbrains/kotlin/js/testOld/utils/AstSearchUtil.kt
@@ -1,83 +1,53 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.js.testOld.utils
 
-package org.jetbrains.kotlin.js.testOld.utils;
+import org.jetbrains.kotlin.js.backend.ast.*
+import org.jetbrains.kotlin.js.inline.util.collectNamedFunctions
 
-import org.jetbrains.kotlin.js.backend.ast.*;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.jetbrains.kotlin.js.inline.util.CollectUtilsKt.collectNamedFunctions;
-
-public class AstSearchUtil {
-    @NotNull
-    public static JsClass getClass(@NotNull JsNode searchRoot, @NotNull String name) {
-        JsClass[] jsClass = {null};
+object AstSearchUtil {
+    @JvmStatic
+    fun getClass(searchRoot: JsNode, name: String): JsClass {
+        var jsClass: JsClass? = null
         searchRoot.accept(
-                new JsVisitor() {
-                    @Override
-                    protected void visitElement(@NotNull JsNode node) {
-                        node.acceptChildren(this);
-                    }
+            object : JsVisitor() {
+                override fun visitElement(node: JsNode) {
+                    node.acceptChildren(this)
+                }
 
-                    @Override
-                    public void visitClass(@NotNull JsClass x) {
-                        if (x.getName() != null && x.getName().getIdent().equals(name)) {
-                            jsClass[0] = x;
-                        } else {
-                            x.acceptChildren(this);
-                        }
+                override fun visitClass(x: JsClass) {
+                    if (x.name?.ident == name) {
+                        jsClass = x
+                    } else {
+                        x.acceptChildren(this)
                     }
                 }
-        );
-        assert jsClass[0] != null: "Class `" + name + "` was not found";
-        return jsClass[0];
+            }
+        )
+        checkNotNull(jsClass) { "Class `" + name + "` was not found" }
+        return jsClass
     }
 
-    @NotNull
-    public static JsFunction getFunction(@NotNull JsNode searchRoot, @NotNull String name) {
-        JsFunction function = findByIdent(collectNamedFunctions(searchRoot), name);
-        assert function != null: "Function `" + name + "` was not found";
-        return function;
+    @JvmStatic
+    fun getFunction(searchRoot: JsNode, name: String): JsFunction =
+        checkNotNull(findByIdent(collectNamedFunctions(searchRoot), name)) { "Function `" + name + "` was not found" }
+
+    @JvmStatic
+    fun getFunctions(searchRoot: JsNode, name: String): List<JsFunction> {
+        val functions = collectNamedFunctions(searchRoot)
+        check(functions.isNotEmpty()) { "Function `" + name + "` was not found" }
+        return functions.mapNotNull { [key, value] -> value.takeIf { key.ident == name } }
     }
 
-    @NotNull
-    public static List<JsFunction> getFunctions(@NotNull JsNode searchRoot, @NotNull String name) {
-        Map<JsName, JsFunction> functions = collectNamedFunctions(searchRoot);
-        assert !functions.isEmpty() : "Function `" + name + "` was not found";
-        return functions
-                .entrySet()
-                .stream()
-                .filter(e -> e.getKey().getIdent().equals(name))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toList());
-    }
-
-    @Nullable
-    private static <T extends JsExpression> T findByIdent(@NotNull Map<JsName, T> properties, @NotNull String name) {
-        for (Map.Entry<JsName, T> entry : properties.entrySet()) {
-            if (entry.getKey().getIdent().equals(name)) {
-                return entry.getValue();
+    private fun <T : JsExpression> findByIdent(properties: Map<JsName, T>, name: String): T? {
+        for ([key, value] in properties) {
+            if (key.ident == name) {
+                return value
             }
         }
 
-        return null;
+        return null
     }
 }
