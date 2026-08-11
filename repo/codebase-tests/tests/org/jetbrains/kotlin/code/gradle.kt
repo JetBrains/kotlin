a7fb118093fcf2043e00a85396c351da52571ffb
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-08-11 13:14:59 +0000

    [build] codebase-tests: Reduce memory usage on TC
    - Fix idletimeout not being passed correctly (as System property)
    - Fix -Xmx not being passed correctly in those tests
    - Set Xms and aggressive GC settings

---
+++ repo/codebase-tests/tests/org/jetbrains/kotlin/code/gradle.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.code
+
+import kotlin.io.path.Path
+import kotlin.io.path.isRegularFile
+import kotlin.test.assertTrue
+
+val projectDir = Path("").also { path ->
+    assertTrue(path.resolve("settings.gradle.kts").isRegularFile())
+}
+
+const val defaultGradleXmx = "2g"
+
+fun defaultGradleArguments(xmx: String = defaultGradleXmx) = buildList {
+    add("-Dkotlin.daemon.options=\"autoshutdownIdleSeconds=10\"")
+    add("-Dorg.gradle.daemon.idletimeout=1000")
+    add("-Dorg.gradle.jvmargs=${defaultGradleJvmArguments(xmx = xmx).joinToString(" ")}")
+}
+
+fun defaultGradleJvmArguments(xmx: String = defaultGradleXmx) = buildList {
+    add("-Xms256m")
+    add("-Xmx${xmx}")
+
+    //progressively take memory, aggressive yielding memory back to the OS
+    add("-XX:MinHeapFreeRatio=10")
+    add("-XX:MaxHeapFreeRatio=30")
+}
