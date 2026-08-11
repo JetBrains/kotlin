b232d3cabe73b59174c7676e5495d2c039162abf
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-08-11 15:42:04 +0200

    [JS] [test] Convert `ArgumentsHelper` from Java to Kotlin
    KT-87774

--- js/js.tests/testFixtures/org/jetbrains/kotlin/js/testOld/utils/ArgumentsHelper.kt
+++ js/js.tests/testFixtures/org/jetbrains/kotlin/js/testOld/utils/ArgumentsHelper.kt
@@ -2,85 +2,61 @@
  * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.js.testOld.utils
 
-package org.jetbrains.kotlin.js.testOld.utils;
-
-import com.intellij.openapi.util.text.StringUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.File;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.io.File
 
 /**
- * Arguments format: ((namedArg|positionalArg)\s+)*`
- * <p>
- * Where: namedArg -- 'key=value' or 'key="spaced value"'
- * positionalArg -- 'value'
- * <p>
+ * Arguments format: `((namedArg|positionalArg)\s+)*`
+ *
+ * Where:
+ *   - `namedArg` is `key=value` or `key="spaced value"`
+ *   - `positionalArg` is `value`
+ *
  * Neither key, nor value should contain spaces.
  */
-public class ArgumentsHelper {
-    private final List<String> positionalArguments = new ArrayList<>();
-    private final Map<String, String> namedArguments = new HashMap<>();
-    private final String entry;
-    private final Pattern argumentsPattern = Pattern.compile("[\\w$_;\\.]+(=((\".*?\")|[\\w$_;\\.]+))?");
-    final File sourceFile;
+internal class ArgumentsHelper internal constructor(private val entry: String, @JvmField val sourceFile: File) {
+    companion object {
+        private val argumentsPattern = Regex($$"""[\w$_;.]+(=((".*?")|[\w$_;.]+))?""")
+    }
 
-    ArgumentsHelper(@NotNull String directiveEntry, @NotNull File directiveSourceFile) {
-        entry = directiveEntry;
-        sourceFile = directiveSourceFile;
+    private val positionalArguments: MutableList<String> = mutableListOf()
+    private val namedArguments: MutableMap<String, String> = hashMapOf()
 
-        Matcher matcher = argumentsPattern.matcher(directiveEntry);
-
-        while (matcher.find()) {
-            String argument = matcher.group();
-            String[] keyVal = argument.split("=");
-            switch (keyVal.length) {
-                case 1:
-                    positionalArguments.add(keyVal[0]);
-                    break;
-                case 2:
-                    String value = keyVal[1];
-                    if (value.charAt(0) == '"') {
-                        value = value.substring(1, value.length() - 1);
+    init {
+        for (match in argumentsPattern.findAll(entry)) {
+            val argument = match.value
+            val keyVal = argument.split("=").dropLastWhile { it.isEmpty() }
+            when (keyVal.size) {
+                1 -> positionalArguments.add(keyVal[0])
+                2 -> {
+                    var value = keyVal[1]
+                    if (value[0] == '"') {
+                        value = value.substring(1, value.length - 1)
                     }
-                    namedArguments.put(keyVal[0], value);
-                    break;
-                default:
-                    throw new AssertionError("Wrong argument format: " + argument);
+                    namedArguments[keyVal[0]] = value
+                }
+                else -> throw IllegalArgumentException("Wrong argument format: $argument")
             }
         }
     }
 
-    @NotNull
-    String getFirst() {
-        return getPositionalArgument(0);
+    val first: String
+        get() = getPositionalArgument(0)
+
+    fun getPositionalArgument(index: Int): String {
+        require(positionalArguments.size > index) { "Argument at index `" + index + "` not found in entry: " + entry }
+        return positionalArguments[index]
     }
 
-    @NotNull
-    String getPositionalArgument(int index) {
-        assert positionalArguments.size() > index : "Argument at index `" + index + "` not found in entry: " + entry;
-        return positionalArguments.get(index);
-    }
+    fun getNamedArgument(name: String): String =
+        requireNotNull(namedArguments[name]) { "Argument `$name` not found in entry: $entry" }
 
-    @NotNull
-    String getNamedArgument(@NotNull String name) {
-        assert namedArguments.containsKey(name) : "Argument `" + name + "` not found in entry: " + entry;
-        return namedArguments.get(name);
-    }
+    fun findNamedArgument(name: String): String? =
+        namedArguments[name]
 
-    @Nullable
-    String findNamedArgument(@NotNull String name) {
-        return namedArguments.get(name);
-    }
-
-    @NotNull
-    List<String> findNamedListArgument(@NotNull String name) {
-        String value = findNamedArgument(name);
-        if (value == null) return Collections.emptyList();
-        return StringUtil.split(value, ";");
+    fun findNamedListArgument(name: String): List<String> {
+        val value = findNamedArgument(name) ?: return emptyList()
+        return value.split(";")
     }
 }
