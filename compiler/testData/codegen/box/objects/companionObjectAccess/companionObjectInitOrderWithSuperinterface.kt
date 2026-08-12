f90db3942f61be52d7cacd54432a803f7e8d75ba
Andrii Rublov <andrii.rublov@jetbrains.com>
2026-08-12 08:03:02 +0000

    [K/JS, K/Wasm] Move super companion objects invocation to `static_init`
    Both companion blocks and companion objects now share a lot of
    common JVM-based logic for super types initialization invocation, which
    means there is no much sense to separate that logic anymore. For both
    K/JS and K/Wasm there is no need to keep backwards compatibility
    related to initialization order of super companion objects.
    
    ^KT-87516 Fixed

--- compiler/testData/codegen/box/objects/companionObjectAccess/companionObjectInitOrderWithSuperinterface.kt
+++ compiler/testData/codegen/box/objects/companionObjectAccess/companionObjectInitOrderWithSuperinterface.kt
@@ -1,8 +1,6 @@
 // LANGUAGE: +CompanionBlocks
 // ^ On Native `CompanionBlocks` language feature enables the JVM-like initialization.
 //   See nativeCompanionInitOrderLegacy for Native behavior without the language feature.
-// ISSUE: KT-87516 [K/JS, K/Wasm] Companion objects coming from super interfaces are not initialized
-// IGNORE_BACKEND: JS_IR, JS_IR_ES6, WASM_JS, WASM_WASI
 
 var l = ""
 private fun log(t: String) {
