cea08a037ed14709082ba3b7483e6e23e4acd851
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-08-12 05:46:39 +0000

    [Tests] Reproduce KT-88102

---
+++ compiler/testData/diagnostics/tests/override/clashesOnInheritance/kt88102.kt
@@ -0,0 +1,41 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// SCOPE_DUMP: P:foo, Q:foo, GP:foo, GP2:foo
+
+interface A {
+    fun foo(): A
+}
+
+interface B {
+    fun foo(): A
+}
+
+interface AB : A, B
+
+interface C {
+    fun foo(): C
+}
+
+object Impl : A, GA<A> {
+    override fun foo(): A = this
+}
+
+class P : A by Impl, AB, C
+<!RETURN_TYPE_MISMATCH_ON_INHERITANCE!>class Q<!> : A by Impl, C
+<!RETURN_TYPE_MISMATCH_ON_INHERITANCE!>class R<!> : A by Impl, B, C
+
+interface GA<T> {
+    fun foo(): T
+}
+
+interface GB<T> {
+    fun foo(): T
+}
+
+interface GAB<T> : GA<T>, GB<T>
+interface GAB2 : GA<A>, GB<A>
+
+<!RETURN_TYPE_MISMATCH_ON_INHERITANCE!>class GP<!> : GA<A> by Impl, GAB<A>, C
+class GP2 : GAB2, GA<A> by Impl, C
+
+/* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, inheritanceDelegation, interfaceDeclaration,
+objectDeclaration, override, thisExpression */
