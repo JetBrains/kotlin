a57668de68a2570f16f7ee5dd8302c74a9c4ce0e
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-08-13 21:25:05 +0200

    [Minor] Source code formatted

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/TopDownAnalyzerFacadeForKonan.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/TopDownAnalyzerFacadeForKonan.kt
@@ -54,10 +54,16 @@ internal object TopDownAnalyzerFacadeForKonan {
         val moduleContext = MutableModuleContextImpl(module, projectContext)
 
         val resolvedModuleDescriptors = nativeFactories.DefaultResolvedDescriptorsFactory.createResolved2(
-                config.resolvedLibraries.getFullList(), projectContext.storageManager, module.builtIns, config.languageVersionSettings,
-                config.friendModuleFiles, config.refinesModuleFiles,
-                config.loadedKlibs.included.map { it.path }.toSet(), listOf(module),
-                isForMetadataCompilation = config.metadataKlib)
+                libraries = config.resolvedLibraries.getFullList(),
+                storageManager = projectContext.storageManager,
+                builtIns = module.builtIns,
+                languageVersionSettings = config.languageVersionSettings,
+                friendModuleFiles = config.friendModuleFiles,
+                refinesModuleFiles = config.refinesModuleFiles,
+                includedLibraryFiles = config.loadedKlibs.included.map { it.path }.toSet(),
+                additionalDependencyModules = listOf(module),
+                isForMetadataCompilation = config.metadataKlib
+        )
 
         val additionalPackages = mutableListOf<PackageFragmentProvider>()
         if (!module.isNativeStdlib()) {
