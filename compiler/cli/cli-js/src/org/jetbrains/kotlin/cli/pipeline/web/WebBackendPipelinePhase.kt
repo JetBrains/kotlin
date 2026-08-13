251637fc1b386f9382fcca188a50a91b76a06138
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-08-13 17:22:40 +0200

    [JS & Wasm] [IC] Move `prepareIcCaches` into `WebBackendPipelinePhase`
    …closer to its only usage.

--- compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/pipeline/web/WebBackendPipelinePhase.kt
+++ compiler/cli/cli-js/src/org/jetbrains/kotlin/cli/pipeline/web/WebBackendPipelinePhase.kt
@@ -5,26 +5,29 @@
 
 package org.jetbrains.kotlin.cli.pipeline.web
 
+import org.jetbrains.kotlin.backend.wasm.ic.WasmICContextMultimodule
+import org.jetbrains.kotlin.backend.wasm.ic.WasmICContextSingleModule
+import org.jetbrains.kotlin.backend.wasm.ic.WasmICContextWholeWorld
 import org.jetbrains.kotlin.cli.CliDiagnostics.JS_IC_ERROR
 import org.jetbrains.kotlin.cli.js.IcCachesArtifacts
 import org.jetbrains.kotlin.cli.js.IcCachesConfigurationData
-import org.jetbrains.kotlin.cli.js.prepareIcCaches
 import org.jetbrains.kotlin.cli.pipeline.CheckCompilationErrors
 import org.jetbrains.kotlin.cli.pipeline.ConfigurationPipelineArtifact
 import org.jetbrains.kotlin.cli.pipeline.PipelinePhase
 import org.jetbrains.kotlin.cli.pipeline.executePhaseIsolatedWithActions
+import org.jetbrains.kotlin.cli.pipeline.web.wasm.WasmCompilationMode
 import org.jetbrains.kotlin.cli.pipeline.web.wasm.WasmCompilationMode.Companion.wasmCompilationMode
 import org.jetbrains.kotlin.cli.report
 import org.jetbrains.kotlin.cli.reportInfo
 import org.jetbrains.kotlin.cli.reportLog
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.perfManager
-import org.jetbrains.kotlin.ir.backend.js.ic.IncrementalCacheGuard
-import org.jetbrains.kotlin.ir.backend.js.ic.acquireAndRelease
-import org.jetbrains.kotlin.ir.backend.js.ic.tryAcquireAndRelease
+import org.jetbrains.kotlin.ir.backend.js.JsICContext
+import org.jetbrains.kotlin.ir.backend.js.ic.*
 import org.jetbrains.kotlin.js.config.*
 import org.jetbrains.kotlin.util.PhaseType
 import org.jetbrains.kotlin.wasm.config.WasmConfigurationKeys
+import java.io.File
 
 abstract class WebBackendPipelinePhase<Output : WebBackendPipelineArtifact, IntermediateOutput>(
     name: String
@@ -109,6 +112,87 @@ abstract class WebBackendPipelinePhase<Output : WebBackendPipelineArtifact, Inte
         return cacheGuard.tryAcquireAndRelease {
             compileIncrementally(icCaches, configuration)
         }
+    }
+
+    private fun prepareIcCaches(
+        cacheDirectory: String,
+        icConfigurationData: IcCachesConfigurationData,
+        outputDir: File,
+        targetConfiguration: CompilerConfiguration,
+        artifactConfiguration: WebArtifactConfiguration,
+    ): IcCachesArtifacts {
+
+        targetConfiguration.reportLog("")
+        targetConfiguration.reportLog("Building cache:")
+        targetConfiguration.reportLog("to: $outputDir")
+        targetConfiguration.reportLog("cache directory: $cacheDirectory")
+        targetConfiguration.reportLog(targetConfiguration.libraries.toString())
+
+        val start = System.currentTimeMillis()
+
+        val loadBodiesOnlyForMainModule: Boolean
+        val icContext = when (icConfigurationData) {
+            is IcCachesConfigurationData.Js -> {
+                loadBodiesOnlyForMainModule = false
+                JsICContext(
+                    icConfigurationData.granularity,
+                )
+            }
+            is IcCachesConfigurationData.Wasm -> {
+                loadBodiesOnlyForMainModule = icConfigurationData.mode == WasmCompilationMode.SINGLE_MODULE
+                val contextConstructor = when (icConfigurationData.mode) {
+                    WasmCompilationMode.REGULAR -> ::WasmICContextWholeWorld
+                    WasmCompilationMode.MULTI_MODULE -> ::WasmICContextMultimodule
+                    WasmCompilationMode.SINGLE_MODULE -> ::WasmICContextSingleModule
+                }
+                contextConstructor(
+                    false,
+                    !icConfigurationData.wasmDebug,
+                    !icConfigurationData.generateWat,
+                    !icConfigurationData.generateDebugInformation,
+                )
+            }
+        }
+        val cacheUpdater = CacheUpdater(
+            cacheDir = cacheDirectory,
+            compilerConfiguration = targetConfiguration,
+            artifactConfiguration = artifactConfiguration,
+            icContext = icContext,
+            checkForClassStructuralChanges = icConfigurationData is IcCachesConfigurationData.Wasm,
+            loadBodiesOnlyForMainModule = loadBodiesOnlyForMainModule,
+        )
+
+        val artifacts = cacheUpdater.actualizeCaches()
+
+        targetConfiguration.reportLog("IC rebuilt overall time: ${System.currentTimeMillis() - start}ms")
+        for ([event, duration] in cacheUpdater.getStopwatchLastLaps()) {
+            targetConfiguration.reportLog("  $event: ${(duration / 1e6).toInt()}ms")
+        }
+
+        var libIndex = 0
+        for ([libFile, srcFiles] in cacheUpdater.getDirtyFileLastStats()) {
+            val singleState = srcFiles.values.firstOrNull()?.singleOrNull()?.let { singleState ->
+                singleState.takeIf { srcFiles.values.all { it.singleOrNull() == singleState } }
+            }
+
+            val [msg, showFiles] = when {
+                singleState == DirtyFileState.NON_MODIFIED_IR -> continue
+                singleState == DirtyFileState.REMOVED_FILE -> "removed" to emptyMap()
+                singleState == DirtyFileState.ADDED_FILE -> "built clean" to emptyMap()
+                srcFiles.values.any { it.singleOrNull() == DirtyFileState.NON_MODIFIED_IR } -> "partially rebuilt" to srcFiles
+                else -> "fully rebuilt" to srcFiles
+            }
+            targetConfiguration.reportLog("${++libIndex}) module [${File(libFile.path).name}] was $msg")
+            var fileIndex = 0
+            for ([srcFile, stat] in showFiles) {
+                val filteredStats = stat.filter { it != DirtyFileState.NON_MODIFIED_IR }
+                val statStr = filteredStats.takeIf { it.isNotEmpty() }?.joinToString { it.str } ?: continue
+                // Use index, because MessageCollector ignores already reported messages
+                targetConfiguration.reportLog("  $libIndex.${++fileIndex}) file [${File(srcFile.path).name}]: ($statStr)")
+            }
+        }
+
+        return artifacts
     }
 
     protected abstract val klibLoadingPhase: WebIrLoadingPipelinePhase
