aed7b6ec69ff5c1b9879309e55aa15212209fb4a
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-08-14 10:28:15 +0200

    [build] Configure G1 for standard test JVMs

--- compiler/build-tools/kotlin-build-tools-api-forward-tests/build.gradle.kts
+++ compiler/build-tools/kotlin-build-tools-api-forward-tests/build.gradle.kts
@@ -316,7 +316,8 @@ testing {
                     testTask(
                         taskName = testTask.name,
                         javaLauncher = JdkMajorVersion.JDK_1_8,
-                        skipInLocalBuild = false
+                        skipInLocalBuild = false,
+                        garbageCollector = GarbageCollector.Parallel
                     ) {
                         systemProperty("kotlin.build-tools-api.log.level", "DEBUG")
                         systemProperty(
