b23447d5b2db80e0520571d0cec12a3c2ef8d31a
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-08-14 10:20:54 +0200

    [Native] Fix: Avoid returning earlier without disposal of `rootDisposable`
    ^KT-61096

--- native/cli-native/src/org/jetbrains/kotlin/cli/bc/K2Native.kt
+++ native/cli-native/src/org/jetbrains/kotlin/cli/bc/K2Native.kt
@@ -112,47 +112,46 @@ class K2Native : CLICompiler<K2NativeCompilerArguments>() {
 
         configuration.perfManager = performanceManager
         try {
-            val rootDisposable = Disposer.newDisposable("Disposable for ${CLICompiler::class.simpleName}.execImpl")
-            setupCommonArguments(configuration, arguments)
-            configuration.setupFromArguments(arguments, rootDisposable)
-            if (CheckDiagnosticCollector.checkHasErrorsAndReportToMessageCollector(configuration)) {
-                return ExitCode.COMPILATION_ERROR
-            }
+            withRootDisposable { rootDisposable ->
+                setupCommonArguments(configuration, arguments)
+                configuration.setupFromArguments(arguments, rootDisposable)
+                if (CheckDiagnosticCollector.checkHasErrorsAndReportToMessageCollector(configuration)) {
+                    return ExitCode.COMPILATION_ERROR
+                }
 
-            val canceledStatus = services[CompilationCanceledStatus::class.java]
-            ProgressIndicatorAndCompilationCanceledStatus.setCompilationCanceledStatus(canceledStatus)
+                val canceledStatus = services[CompilationCanceledStatus::class.java]
+                ProgressIndicatorAndCompilationCanceledStatus.setCompilationCanceledStatus(canceledStatus)
 
-            try {
-                setIdeaIoUseFallback()
+                try {
+                    setIdeaIoUseFallback()
 
-                val code = doExecute(arguments, configuration, rootDisposable, services)
+                    val code = doExecute(arguments, configuration, rootDisposable, services)
 
-                performanceManager.notifyCompilationFinished()
-                if (arguments.reportPerf) {
-                    collector.report(LOGGING, "PERF: " + performanceManager.getTargetInfo())
-                    performanceManager.forEachStringMeasurement {
-                        collector.report(LOGGING, "PERF: $it", null)
+                    performanceManager.notifyCompilationFinished()
+                    if (arguments.reportPerf) {
+                        collector.report(LOGGING, "PERF: " + performanceManager.getTargetInfo())
+                        performanceManager.forEachStringMeasurement {
+                            collector.report(LOGGING, "PERF: $it", null)
+                        }
+                    }
+
+                    if (arguments.dumpPerf != null) {
+                        performanceManager.dumpPerformanceReport(arguments.dumpPerf!!)
+                    }
+
+                    return if (CheckDiagnosticCollector.checkHasErrorsAndReportToMessageCollector(configuration)) ExitCode.COMPILATION_ERROR else code
+                } catch (e: CompilationCanceledException) {
+                    collector.reportCompilationCancelled(e)
+                    return ExitCode.OK
+                } catch (e: RuntimeException) {
+                    val cause = e.cause
+                    if (cause is CompilationCanceledException) {
+                        collector.reportCompilationCancelled(cause)
+                        return ExitCode.OK
+                    } else {
+                        throw e
                     }
                 }
-
-                if (arguments.dumpPerf != null) {
-                    performanceManager.dumpPerformanceReport(arguments.dumpPerf!!)
-                }
-
-                return if (CheckDiagnosticCollector.checkHasErrorsAndReportToMessageCollector(configuration)) ExitCode.COMPILATION_ERROR else code
-            } catch (e: CompilationCanceledException) {
-                collector.reportCompilationCancelled(e)
-                return ExitCode.OK
-            } catch (e: RuntimeException) {
-                val cause = e.cause
-                if (cause is CompilationCanceledException) {
-                    collector.reportCompilationCancelled(cause)
-                    return ExitCode.OK
-                } else {
-                    throw e
-                }
-            } finally {
-                disposeRootInWriteAction(rootDisposable)
             }
         } catch (_: CompilationErrorException) {
             return ExitCode.COMPILATION_ERROR
@@ -393,6 +392,15 @@ class K2Native : CLICompiler<K2NativeCompilerArguments>() {
                 if (doMainNoExit(K2Native(), args, messageRenderer) != ExitCode.OK) {
                     throw KonanCompilationException("Compilation finished with errors")
                 }
+            }
+        }
+
+        private inline fun <T> withRootDisposable(block: (rootDisposable: Disposable) -> T): T {
+            val rootDisposable = Disposer.newDisposable("Disposable for ${CLICompiler::class.simpleName}.execImpl")
+            try {
+                return block(rootDisposable)
+            } finally {
+                disposeRootInWriteAction(rootDisposable)
             }
         }
     }
