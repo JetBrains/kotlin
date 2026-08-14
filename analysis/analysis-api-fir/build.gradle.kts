ec0f6a2f139f112f7cb3f97d304468137c94dd5b
Oleg Makeev <oleg.makeev@jetbrains.com>
2026-08-14 15:02:46 +0200

    [Analysis API Standalone] Rename `analysis-api-standalone-base` module to `analysis-api-standalone-fir`
    As this module contains FIR-specific implementation, the name should reflect that as well.
    
    ^KT-85383
    ^KT-60487

--- analysis/analysis-api-fir/build.gradle.kts
+++ analysis/analysis-api-fir/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(libs.opentelemetry.api)
 
     testFixturesImplementation(testFixtures(project(":analysis:low-level-api-fir")))
-    testFixturesApi(project(":analysis:analysis-api-standalone:analysis-api-standalone-base"))
+    testFixturesApi(project(":analysis:analysis-api-standalone:analysis-api-standalone-fir"))
     testFixturesImplementation(testFixtures(project(":compiler:tests-common")))
     testFixturesApi(testFixtures(project(":compiler:test-infrastructure-utils")))
     testFixturesApi(testFixtures(project(":compiler:test-infrastructure")))
