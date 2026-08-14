cb4a04df054462d1d6046e2df44edba9b1eee5b7
Oleg Makeev <oleg.makeev@jetbrains.com>
2026-08-14 15:17:21 +0200

    [Analysis API Standalone] Rename `.xml` and `fakePluginDescriptor` to be consistent with the new module name
    Previously, `analysis-api-standalone-base` was renamed to `analysis-api-standalone-fir`.
    `.xml` and `fakePluginDescriptor` should have consistent names as well.
    
    ^KT-85383
    ^KT-60487

--- analysis/analysis-api-standalone/analysis-api-standalone-fir/src/org/jetbrains/kotlin/analysis/api/standalone/base/projectStructure/FirStandaloneServiceRegistrar.kt
+++ analysis/analysis-api-standalone/analysis-api-standalone-fir/src/org/jetbrains/kotlin/analysis/api/standalone/base/projectStructure/FirStandaloneServiceRegistrar.kt
@@ -35,7 +35,7 @@ import org.jetbrains.kotlin.serialization.deserialization.builtins.BuiltInSerial
 
 @OptIn(KaImplementationDetail::class)
 object FirStandaloneServiceRegistrar : AnalysisApiSimpleServiceRegistrar() {
-    private const val PLUGIN_RELATIVE_PATH = "/META-INF/analysis-api/analysis-api-standalone-base.xml"
+    private const val PLUGIN_RELATIVE_PATH = "/META-INF/analysis-api/analysis-api-standalone-fir.xml"
 
     @Suppress("UnstableApiUsage")
     override fun registerApplicationServices(application: MockApplication, disposable: Disposable) {
