e15ca12940c8ca2dd5267890508de901a4cde73e
Vadim Mishenev <vad-mishenev@yandex.ru>
2026-08-18 16:31:42 +0300

    [KDM] Make the subproject a root project
    ^KT-88283
    Original commit: https://github.com/Kotlin/dokka/commit/9c60e148b0115d90ef3366cc9754d0e975bec647

--- libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/dokkabuild.kotlin-jvm.gradle.kts
+++ libraries/tools/kotlin-documentation-model/analyzer/build-logic/src/main/kotlin/dokkabuild.kotlin-jvm.gradle.kts
@@ -2,7 +2,6 @@
  * Copyright 2014-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import dokkabuild.utils.configureGradleKotlinCompatibility
 
 plugins {
     id("dokkabuild.java")
@@ -11,13 +10,12 @@ plugins {
 
 val rootProjectsWithoutDependencyOnDokkaCore = listOf("dokka-integration-tests")
 
-configureGradleKotlinCompatibility()
 
 kotlin {
     explicitApi()
 
     compilerOptions {
-        allWarningsAsErrors = true
+        allWarningsAsErrors = false
 
         // These projects know nothing about the `@InternalDokkaApi` annotation, so the Kotlin compiler
         // will complain about an unresolved opt-in requirement marker and fail the build if it's not excluded.
