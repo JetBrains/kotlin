8b895e2249a7395c9272ad9d5f42297ecfb30a4a
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-08-19 14:15:43 -0400

    [AFU] KT-71791: Prohibit taking references to AFU's atomic functions

--- plugins/atomicfu/atomicfu-compiler/testData/diagnostics/AtomicFunctionReferences.kt
+++ plugins/atomicfu/atomicfu-compiler/testData/diagnostics/AtomicFunctionReferences.kt
@@ -11,8 +11,8 @@ private val a: AtomicInt = atomic(42)
 
 // Getting a reference to atomic function is prohibited
 fun whyWouldYouDoThat() {
-   val inc = a::incrementAndGet
+   val inc = a::<!ATOMIC_FUNCTION_CALLABLE_REFERENCES_ARE_FORBIDDEN!>incrementAndGet<!>
    callAndPrint(inc)
-   val inc2 = a::inc
+   val inc2 = a::<!ATOMIC_FUNCTION_CALLABLE_REFERENCES_ARE_FORBIDDEN!>inc<!>
    callAndPrint(inc)
 }
