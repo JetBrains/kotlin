904ef733e6bf147271ed887efce645038bd8b4a7
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2026-08-19 15:13:15 +0000

    [Tests] KQA-3061: inline properties as companion block/extension properties
    ^KQA-3061

--- compiler/testData/codegen/box/companionBlocksAndExtensions/inline_block.kt
+++ compiler/testData/codegen/box/companionBlocksAndExtensions/inline_block.kt
@@ -1,15 +1,26 @@
 // LANGUAGE: +CompanionBlocks +CompanionExtensions
 // DUMP_KLIB_ABI: DEFAULT
+// WITH_STDLIB
 
 class A {
     companion {
         val compBlockVal: UInt = 0u
         val compBlockValI: Int = 1
         inline fun compBlockFun(k: UInt = 0u) = " compBlockFun:$k"
+
+        inline val compBlockInlineVal: String
+            get() = " compBlockInlineVal:$compBlockValI"
+        var compBlockVarI: Int = 0
+        inline var compBlockInlineVar: String
+            get() = " compBlockInlineVar:$compBlockVarI"
+            set(value) {
+                compBlockVarI = value.toInt()
+            }
     }
 }
 
 fun box(): String {
-    val res = A.compBlockVal.toString() + A.compBlockValI.toString() + A.compBlockFun(5u)
-    return if(res == "01 compBlockFun:5") "OK" else "FAIL: $res"
+    A.compBlockInlineVar = "42"
+    val res = A.compBlockVal.toString() + A.compBlockValI.toString() + A.compBlockFun(5u) + A.compBlockInlineVal + A.compBlockInlineVar
+    return if(res == "01 compBlockFun:5 compBlockInlineVal:1 compBlockInlineVar:42") "OK" else "FAIL: $res"
 }
