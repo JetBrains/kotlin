bd0288d57d97b4c23428442a82ff92484809c188
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-08-19 12:18:22 +0200

    fixup! [Build] Implement global JUnit 5 test batching

--- repo/test-federation-runtime/src/main/kotlin/org/jetbrains/kotlin/testFederation/TestBatchPostDiscoveryFilter.kt
+++ repo/test-federation-runtime/src/main/kotlin/org/jetbrains/kotlin/testFederation/TestBatchPostDiscoveryFilter.kt
@@ -30,13 +30,38 @@ class TestBatchPostDiscoveryFilter : PostDiscoveryFilter {
         checksum.update(batchSeed.shr(16))
         checksum.update(batchSeed.shr(24))
 
-        checksum.update(test.uniqueId.toString().encodeToByteArray())
+        checksum.update(distributionKey(test).encodeToByteArray())
         val thisTestBatch = (checksum.value.toInt().absoluteValue % totalBatches) + 1
 
         return if (thisTestBatch == currentBatch) {
             included("Current batch: '$currentBatch'. Test batch: '$thisTestBatch'")
         } else {
             excluded("Current batch: '$currentBatch'. Test batch: '$thisTestBatch'")
+        }
+    }
+
+    /**
+     * Creates a String based key, which will be used to distribute a test across buckets.
+     * Returning the same key, will result in the same bucket being assigned for the test.
+     */
+    private fun distributionKey(test: TestDescriptor): String {
+        val uniqueId = test.uniqueId
+
+        /*
+        Certain test engines may at least require some thought on how to put tests into buckets.
+        While the junit-jupiter's behavior of using the uniqueId directly is a reasonable default,
+        it was deliberately chosen to fail on unexpected test engines, to ensure them being handled
+        and thought about instead of silently behaving undesirably
+         */
+        return when (uniqueId.engineId.get()) {
+            "junit-jupiter" -> uniqueId.toString()
+
+            /*
+            The compiler grouping test engine tries to group work done within a test class, we therefore select
+            the test class (parent) as the uniqueId instead of the full testId (which may be method based)
+             */
+            "kotlin-compiler-grouping-engine" -> uniqueId.removeLastSegment().toString()
+            else -> error("Unexpected Test Engine ID: ${uniqueId.engineId}")
         }
     }
 }
