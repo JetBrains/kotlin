67b40f92ded4d78497cc1cbf676c606c312e5a51
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-08-19 10:25:35 +0000

    Tests: remove obsolete JvmVersionRequirementTest
    All test cases except `nestedClassMembers.kt` were already checked in
    kotlinp tests.
    
    Also, remove unused `LoadDescriptorUtil` and obsolete
    `RecursiveDescriptorComparatorAdaptor`.

--- compiler/testData/versionRequirement/patchVersion.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
-package test
-
-import kotlin.internal.RequireKotlin
-
-@RequireKotlin("1.1.50", level = DeprecationLevel.HIDDEN)
-class Klass
