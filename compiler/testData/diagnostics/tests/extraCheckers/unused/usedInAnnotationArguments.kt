d3f0f644cf02aa8ea75518a0c947cc1fbacb2e6f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-08-19 14:29:21 +0200

    [FIR] Use more appropriate diagnostic to report usage of local val in const
    #KT-80964

--- compiler/testData/diagnostics/tests/extraCheckers/unused/usedInAnnotationArguments.kt
+++ compiler/testData/diagnostics/tests/extraCheckers/unused/usedInAnnotationArguments.kt
@@ -3,7 +3,7 @@ annotation class Ann(val value: Int)
 
 fun foo(): Int {
     val x = 3
-    @Ann(<!ANNOTATION_ARGUMENT_MUST_BE_CONST!>x<!>) val y = 5
+    @Ann(<!NON_CONST_VAL_USED_IN_CONSTANT_EXPRESSION!>x<!>) val y = 5
     return y
 }
 
