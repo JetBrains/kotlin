e34bf2ef0e0130921b480471014b472d47ab5d05
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-08-19 15:33:53 -0400

    [AFU] KT-71791: Prohibit nullable atomic properties

--- plugins/atomicfu/atomicfu-compiler/src/org/jetbrains/kotlinx/atomicfu/compiler/diagnostic/AtomicfuErrors.kt
+++ plugins/atomicfu/atomicfu-compiler/src/org/jetbrains/kotlinx/atomicfu/compiler/diagnostic/AtomicfuErrors.kt
@@ -28,5 +28,6 @@ object AtomicfuErrors : KtDiagnosticsContainer() {
     val ATOMIC_VALUE_PARAMETERS_ARE_FORBIDDEN by error0<KtNamedDeclaration>(SourceElementPositioningStrategies.DECLARATION_NAME)
     val ATOMIC_EXTENSION_MUST_BE_NON_PUBLIC_INLINE by error0<KtDeclaration>(SourceElementPositioningStrategies.DECLARATION_NAME)
     val ATOMIC_ARRAY_EXTENSION_PROPERTIES_ARE_FORBIDDEN by error0<KtProperty>(SourceElementPositioningStrategies.DECLARATION_NAME)
+    val NULLABLE_ATOMIC_PROPERTIES_ARE_FORBIDDEN by error0<KtProperty>(SourceElementPositioningStrategies.DECLARATION_RETURN_TYPE)
     override fun getRendererFactory(): BaseDiagnosticRendererFactory = AtomicfuErrorMessages
 }
