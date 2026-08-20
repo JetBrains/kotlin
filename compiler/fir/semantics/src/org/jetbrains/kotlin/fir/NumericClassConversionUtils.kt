142d335eb16d1e9fe3f82b15511915d822f7e645
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-08-20 14:52:20 +0300

    [FIR] Prioritize `foo(NumericClass)` if others aren't applicable
    Otherwise, `mySizeTVar.compareTo(0.convert())` becomes
    `CANNOT_INFER_PARAMETER_TYPE`, because numeric classes have an entire
    family of `compareTo()` overloads, resulting in resolution ambiguity.
    
    Definitely used in `cryptography-kotlin`.
    
    ^KT-87284

--- compiler/fir/semantics/src/org/jetbrains/kotlin/fir/NumericClassConversionUtils.kt
+++ compiler/fir/semantics/src/org/jetbrains/kotlin/fir/NumericClassConversionUtils.kt
@@ -48,7 +48,7 @@ private fun FirBasedSymbol<*>.supportsNumericClassConversionFrom(type: ConeKotli
 private fun FirBasedSymbol<*>.supportsNumericClassConversionTo(type: ConeKotlinType, session: FirSession): Boolean =
     getSupportedNumericClassConversions(session)?.all { it.fitsInto(type) } ?: false
 
-private fun FirBasedSymbol<*>.getSupportedNumericClassConversions(session: FirSession): List<ConeKotlinType>? {
+fun FirBasedSymbol<*>.getSupportedNumericClassConversions(session: FirSession): List<ConeKotlinType>? {
     val arguments = resolvedCompilerAnnotationsWithClassIds.getAnnotationByClassId(StandardClassIds.Annotations.NumericClass, session)
         ?.findArgumentByName(StandardClassIds.Annotations.ParameterNames.actualizations)
         ?.unwrapAndFlattenArgument(flattenArrays = true)
