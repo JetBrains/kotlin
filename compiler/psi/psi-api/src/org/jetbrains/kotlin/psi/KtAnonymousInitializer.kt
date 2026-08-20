dc98db3623859369a4a8b34453ce4ee7203d74af
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-08-20 12:14:07 +0200

    [psi] extract `KtScriptInitializer` to separate file
    ^KT-78356

--- compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtAnonymousInitializer.kt
+++ compiler/psi/psi-api/src/org/jetbrains/kotlin/psi/KtAnonymousInitializer.kt
@@ -5,12 +5,6 @@
 
 package org.jetbrains.kotlin.psi
 
-import com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.KtStubBasedElementTypes
-import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
-import org.jetbrains.kotlin.psi.stubs.KotlinPlaceHolderStub
-import org.jetbrains.kotlin.utils.sure
-
 /**
  * Represents an anonymous initializer: a piece of code that runs during initialization without introducing a named declaration.
  *
@@ -29,30 +23,3 @@ interface KtAnonymousInitializer : KtDeclaration, KtStatementExpression {
     val body: KtExpression?
 }
 
-/**
- * Represents an initializer expression in a script. Effectively, it is a wrapper for top-level expressions
- *
- * ### Example:
- *
- * ```kotlin
- * // In a .kts file:
- *    println("Hello")
- * // ^______________^
- * ```
- */
-class KtScriptInitializer : KtDeclarationStub<KotlinPlaceHolderStub<KtScriptInitializer>>, KtAnonymousInitializer {
-    @KtImplementationDetail
-    constructor(node: ASTNode) : super(node)
-
-    @KtImplementationDetail
-    constructor(stub: KotlinPlaceHolderStub<KtScriptInitializer>) : super(stub, KtStubBasedElementTypes.SCRIPT_INITIALIZER)
-
-    override val body: KtExpression?
-        get() = findChildByClass(KtExpression::class.java)
-
-    /** The [KtScript] that contains this top-level script initializer. */
-    override val containingDeclaration: KtScript
-        get() = getParentOfType<KtScript>(true).sure { "Should only be present in script" }
-
-    override fun <R, D> accept(visitor: KtVisitor<R, D>, data: D): R = visitor.visitScriptInitializer(this, data)
-}
