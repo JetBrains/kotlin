b4d172ea373ce02a9291d8665f42943963cd7c57
Aleksandra Klimantova <aleksandra.klimantova@jetbrains.com>
2026-08-21 01:31:27 +0200

    fixup! [BTA] Test basic Kotlin/Wasm compilation and linking operations

--- compiler/build-tools/kotlin-build-tools-api-forward-tests/src/testForwardCompatibility2.4.20-Beta1/kotlin/WasmKlibCompilationTest.kt
+++ compiler/build-tools/kotlin-build-tools-api-forward-tests/src/testForwardCompatibility2.4.20-Beta1/kotlin/WasmKlibCompilationTest.kt
@@ -5,14 +5,8 @@
 
 package org.jetbrains.kotlin.buildtools.forward.tests
 
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonCompilerArguments.Companion.OPT_IN
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.IR_OUTPUT_NAME
 import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.NOPACK
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.X_FRIEND_MODULES
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.X_IR_MODULE_NAME
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmCompilerKlibArguments.Companion.X_IR_PER_MODULE_OUTPUT_NAME
 import org.jetbrains.kotlin.buildtools.api.arguments.ExperimentalCompilerArgument
-import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerKlibArguments.Companion.X_WASM_KCLASS_FQN
 import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerKlibArguments.Companion.X_WASM_TARGET
 import org.jetbrains.kotlin.buildtools.api.arguments.enums.WasmTarget
 import org.jetbrains.kotlin.buildtools.forward.tests.compilation.BaseCompilationTest
@@ -77,128 +71,6 @@ class WasmKlibCompilationTest : BaseCompilationTest() {
         }
     }
 
-    @DisplayName("Fragments of a multi-level package are stored in a directory named after the whole package")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun multiLevelPackageSourcesProduceFragmentsInOnePackageDirectory(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            module("ic-scenarios/KT-85074").compile {
-                assertIsUnpackedKlibWithIr()
-                assertKnmFileCount(packageFqName = "org.example.foo", expectedCount = 2)
-                assertKnmFileCount(packageFqName = "root_package", expectedCount = 0)
-            }
-        }
-    }
-
-    @DisplayName("IR_OUTPUT_NAME is written to the klib manifest as unique_name value")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun irOutputNameIsWrittenToManifest(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        val outputName = "my-lib"
-        wasmProject(strategyConfig) {
-            module("js-ic-basic-lib").compile(compilationConfigAction = {
-                it.compilerArguments[IR_OUTPUT_NAME] = outputName
-            }) {
-                assertKlibManifestProperties("unique_name" to outputName)
-            }
-        }
-    }
-
-    @DisplayName("IR_OUTPUT_NAME defines the file name of the packed klib")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun irOutputNameDefinesThePackedKlibFileName(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        val outputName = "my-lib"
-        wasmProject(strategyConfig) {
-            module("js-ic-basic-lib").compile(compilationConfigAction = {
-                it.compilerArguments[IR_OUTPUT_NAME] = outputName
-                it.compilerArguments[NOPACK] = false
-            }) {
-                assertIsPackedKlib(outputName)
-            }
-        }
-    }
-
-    @DisplayName("X_IR_MODULE_NAME overrides the unique_name value written to the klib manifest")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun irModuleNameOverridesTheModuleNameInManifest(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        val moduleName = "renamed-lib"
-        wasmProject(strategyConfig) {
-            module("js-ic-basic-lib").compile(compilationConfigAction = {
-                it.compilerArguments[X_IR_MODULE_NAME] = moduleName
-            }) {
-                assertIsUnpackedKlibWithIr()
-                assertKlibManifestProperties("unique_name" to moduleName)
-            }
-        }
-    }
-
-    @DisplayName("X_IR_MODULE_NAME does not affect the file name of the packed klib")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun irModuleNameDoesNotAffectThePackedKlibFileName(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        val outputName = "my-lib"
-        val moduleName = "renamed-lib"
-        wasmProject(strategyConfig) {
-            module("js-ic-basic-lib").compile(compilationConfigAction = {
-                it.compilerArguments[IR_OUTPUT_NAME] = outputName
-                it.compilerArguments[X_IR_MODULE_NAME] = moduleName
-                it.compilerArguments[NOPACK] = false
-            }) {
-                assertIsPackedKlib(outputName)
-            }
-        }
-    }
-
-    @DisplayName("X_IR_PER_MODULE_OUTPUT_NAME is written to the klib manifest as the output name")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun perModuleOutputNameIsWrittenToManifest(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        val perModuleOutputName = "lib-output"
-        wasmProject(strategyConfig) {
-            val module = module("js-ic-basic-lib")
-            module.compile {
-                assertKlibManifestHasNoProperties("jsOutputName")
-            }
-            module.compile(compilationConfigAction = {
-                it.compilerArguments[X_IR_PER_MODULE_OUTPUT_NAME] = perModuleOutputName
-            }) {
-                assertKlibManifestProperties("jsOutputName" to perModuleOutputName)
-            }
-        }
-    }
-
-    @DisplayName("OPT_IN allows using an API that requires opt-in and compiles to an unpacked klib")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun optInAllowsUsingAnApiThatRequiresOptIn(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            module("opt-in-usage").compile(compilationConfigAction = {
-                it.compilerArguments[OPT_IN] = listOf("MyExperimentalApi")
-            }) {
-                assertIsUnpackedKlibWithIr()
-            }
-        }
-    }
-
-    @DisplayName("Using an API that requires opt-in without OPT_IN is reported as a compiler error")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun optInApiUsageWithoutOptInIsReportedAsError(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            module("opt-in-usage").compile {
-                expectFail()
-                assertLogContainsPatterns(LogLevel.ERROR, Regex(".*This API is experimental and requires opt-in.*"))
-            }
-        }
-    }
-
-    @DisplayName("X_WASM_TARGET set to wasm-js compiles the sources to an unpacked klib targeting wasm-js")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun wasmJsTargetCompilesToKlib(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            module("js-ic-basic-lib").compile(compilationConfigAction = {
-                it.compilerArguments[X_WASM_TARGET] = WasmTarget.WASM_JS
-            }) {
-                assertIsUnpackedKlibWithIr()
-                assertKlibManifestProperties("builtins_platform" to "WASM", "wasm_targets" to "wasm-js")
-            }
-        }
-    }
-
     @DisplayName("X_WASM_TARGET set to wasm-wasi compiles the sources to an unpacked klib targeting wasm-wasi")
     @BtaV2StrategyAgnosticCompilationTest
     fun wasmWasiTargetCompilesToKlib(strategyConfig: CompilerExecutionStrategyConfiguration) {
@@ -211,60 +83,6 @@ class WasmKlibCompilationTest : BaseCompilationTest() {
             ) {
                 assertIsUnpackedKlibWithIr()
                 assertKlibManifestProperties("builtins_platform" to "WASM", "wasm_targets" to "wasm-wasi")
-            }
-        }
-    }
-
-    @DisplayName("X_WASM_KCLASS_FQN enabled allows KClass.qualifiedName and compiles to an unpacked klib")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun wasmKClassFqnEnabledAllowsQualifiedName(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            module("wasm-kclass-fqn").compile(compilationConfigAction = {
-                it.compilerArguments[X_WASM_KCLASS_FQN] = true
-            }) {
-                assertIsUnpackedKlibWithIr()
-            }
-        }
-    }
-
-    @DisplayName("X_WASM_KCLASS_FQN disabled rejects KClass.qualifiedName as an unsupported reflection API")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun wasmKClassFqnDisabledRejectsQualifiedName(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            module("wasm-kclass-fqn").compile(compilationConfigAction = {
-                it.compilerArguments[X_WASM_KCLASS_FQN] = false
-            }) {
-                expectFail()
-                assertLogContainsPatterns(LogLevel.ERROR, Regex(".*This reflection API is not supported in Kotlin/Wasm.*"))
-            }
-        }
-    }
-
-    @DisplayName("X_FRIEND_MODULES makes internal declarations of a dependency visible")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun internalDeclarationsOfAFriendModuleAreVisible(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val libModule = module("friend-modules/lib")
-            val appModule = module("friend-modules/app", listOf(libModule))
-            libModule.compile()
-            appModule.compile(compilationConfigAction = {
-                it.compilerArguments[X_FRIEND_MODULES] = listOf(libModule.outputDirectory)
-            }) {
-                assertIsUnpackedKlibWithIr()
-            }
-        }
-    }
-
-    @DisplayName("Internal declarations of a dependency are not visible without X_FRIEND_MODULES")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun internalDeclarationsOfANonFriendModuleAreNotVisible(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val libModule = module("friend-modules/lib")
-            val appModule = module("friend-modules/app", listOf(libModule))
-            libModule.compile()
-            appModule.compile {
-                expectFail()
-                assertLogContainsPatterns(LogLevel.ERROR, Regex(".*[Cc]annot access.*internalGreeting.*"))
             }
         }
     }
--- compiler/build-tools/kotlin-build-tools-api-tests/src/testCompatibility/kotlin/WasmLinkingTest.kt
+++ compiler/build-tools/kotlin-build-tools-api-tests/src/testCompatibility/kotlin/WasmLinkingTest.kt
@@ -8,19 +8,10 @@ package org.jetbrains.kotlin.buildtools.tests
 import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.IR_OUTPUT_NAME
 import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.LIBRARIES
 import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.NOPACK
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmArguments.Companion.X_FRIEND_MODULES
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmCompilerLinkingArguments.Companion.MAIN
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmCompilerLinkingArguments.Companion.SOURCE_MAP
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmCompilerLinkingArguments.Companion.X_GENERATE_DTS
-import org.jetbrains.kotlin.buildtools.api.arguments.CommonJsAndWasmCompilerLinkingArguments.Companion.X_IR_DCE
 import org.jetbrains.kotlin.buildtools.api.arguments.CommonKlibBasedArgumentsLinkingArguments.Companion.X_PARTIAL_LINKAGE_LOGLEVEL
 import org.jetbrains.kotlin.buildtools.api.arguments.ExperimentalCompilerArgument
 import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerKlibArguments
 import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerKlibArguments.Companion.X_WASM_TARGET
-import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerLinkingArguments.Companion.X_WASM_DEBUGGER_CUSTOM_FORMATTERS
-import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerLinkingArguments.Companion.X_WASM_GENERATE_DWARF
-import org.jetbrains.kotlin.buildtools.api.arguments.WasmCompilerLinkingArguments.Companion.X_WASM_GENERATE_WAT
-import org.jetbrains.kotlin.buildtools.api.arguments.enums.JsMainCallMode
 import org.jetbrains.kotlin.buildtools.api.arguments.enums.PartialLinkageLogLevel
 import org.jetbrains.kotlin.buildtools.api.arguments.enums.WasmTarget
 import org.jetbrains.kotlin.buildtools.tests.compilation.BaseCompilationTest
@@ -114,109 +105,6 @@ class WasmLinkingTest : BaseCompilationTest() {
         }
     }
 
-    @DisplayName("X_WASM_GENERATE_WAT emits a .wat text module")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun watFileIsGeneratedWhenRequested(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-ic-basic-lib")
-            module.compile()
-
-            module.link(compilationConfigAction = { it.compilerArguments[X_WASM_GENERATE_WAT] = true }) {
-                val watFile = "${module.moduleName}.wat"
-                assertOutputFileCountWithExtension(".wat", 1)
-                assertOutputsContains(module.expectedOutputFileName, watFile)
-            }
-        }
-    }
-
-    @DisplayName("X_GENERATE_DTS emits a TypeScript declaration file")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun dtsFileIsGeneratedWhenRequested(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-ic-basic-lib")
-            module.compile()
-
-            module.link(compilationConfigAction = { it.compilerArguments[X_GENERATE_DTS] = true }) {
-                assertOutputFileCountWithExtension(".d.mts", 1)
-                assertOutputsContains(module.expectedOutputFileName, "${module.moduleName}.d.mts")
-            }
-        }
-    }
-
-    @DisplayName("X_WASM_GENERATE_DWARF adds DWARF debug sections to the generated Wasm")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun dwarfIsGeneratedWhenRequested(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-with-main")
-            module.compile()
-
-            module.link(compilationConfigAction = { it.compilerArguments[X_WASM_GENERATE_DWARF] = true }) {
-                assertOutputsContains(module.expectedOutputFileName)
-                assertOutputFileBytesContain(module.expectedOutputFileName, ".debug_info")
-            }
-        }
-    }
-
-    @DisplayName("X_WASM_DEBUGGER_CUSTOM_FORMATTERS emits a custom-formatters.js file")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun customFormattersFileIsGeneratedWhenRequested(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-ic-basic-lib")
-            module.compile()
-
-            module.link(compilationConfigAction = { it.compilerArguments[X_WASM_DEBUGGER_CUSTOM_FORMATTERS] = true }) {
-                assertOutputsContains(module.expectedOutputFileName, "custom-formatters.js")
-            }
-        }
-    }
-
-    @DisplayName("SOURCE_MAP produces a source map next to the generated Wasm artifact")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun sourceMapIsGeneratedWhenRequested(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-ic-basic-lib")
-            module.compile()
-            val sourceMapFileName = "${module.expectedOutputFileName}.map"
-
-            module.link(compilationConfigAction = { it.compilerArguments[SOURCE_MAP] = true }) {
-                assertOutputsContains(module.expectedOutputFileName, sourceMapFileName)
-                assertOutputFileBytesContain(module.expectedOutputFileName, sourceMapFileName)
-                assertOutputFileContains(sourceMapFileName, "useAInLibMain.kt")
-            }
-        }
-    }
-
-    @DisplayName("Without X_IR_DCE the unreachable declaration is kept in the generated Wasm")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun unreachableDeclarationsAreKeptWithoutDce(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-with-main")
-            module.compile()
-
-            module.link(compilationConfigAction = { it.compilerArguments[MAIN] = JsMainCallMode.CALL }) {
-                assertOutputFileBytesContain(module.expectedOutputFileName, "usedByMainMarker")
-                assertOutputFileBytesContain(module.expectedOutputFileName, "deadUnusedFunctionMarker")
-            }
-        }
-    }
-
-    @DisplayName("X_IR_DCE eliminates unreachable declarations from the generated Wasm")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun dceEliminatesUnreachableDeclarations(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val module = module("js-with-main")
-            module.compile()
-
-            module.link(compilationConfigAction = {
-                it.compilerArguments[MAIN] = JsMainCallMode.CALL
-                it.compilerArguments[X_IR_DCE] = true
-            }) {
-                assertOutputFileBytesContain(module.expectedOutputFileName, "usedByMainMarker")
-                assertOutputFileBytesDoNotContain(module.expectedOutputFileName, "deadUnusedFunctionMarker")
-            }
-        }
-    }
-
     @DisplayName("A lib+app graph links each module to its own Wasm and the app resolves the library across modules")
     @BtaV2StrategyAgnosticCompilationTest
     fun multiModuleGraphLinksEachModuleToItsOwnArtifact(strategyConfig: CompilerExecutionStrategyConfiguration) {
@@ -239,27 +127,6 @@ class WasmLinkingTest : BaseCompilationTest() {
         }
     }
 
-    @DisplayName("An internal declaration of a friend module is resolved across modules and linked into the app Wasm")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun friendModuleInternalDeclarationIsLinked(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val library = module("friend-modules/lib")
-            val app = module("friend-modules/app", dependencies = listOf(library))
-
-            library.compile()
-            app.compile(compilationConfigAction = {
-                it.compilerArguments[X_FRIEND_MODULES] = listOf(library.outputDirectory)
-            })
-
-            app.link(compilationConfigAction = {
-                it.compilerArguments[X_PARTIAL_LINKAGE_LOGLEVEL] = PartialLinkageLogLevel.ERROR
-            }) {
-                assertOutputsContains(app.expectedOutputFileName)
-                assertOutputFileBytesContain(app.expectedOutputFileName, "hello from the internal API")
-            }
-        }
-    }
-
     @DisplayName("A missing dependency fails Wasm linking when partial linkage is escalated to ERROR")
     @BtaV2StrategyAgnosticCompilationTest
     fun missingDependencyFailsLinkingWhenPartialLinkageIsError(strategyConfig: CompilerExecutionStrategyConfiguration) {
@@ -278,31 +145,6 @@ class WasmLinkingTest : BaseCompilationTest() {
                 expectFail()
                 assertLogContainsPatterns(
                     LogLevel.ERROR,
-                    ".*Function 'formatGreeting' can not be called: No function found for symbol.*"
-                        .toRegex(RegexOption.DOT_MATCHES_ALL),
-                )
-            }
-        }
-    }
-
-    @DisplayName("A missing dependency only warns when partial linkage is at WARNING")
-    @BtaV2StrategyAgnosticCompilationTest
-    fun missingDependencyOnlyWarnsWhenPartialLinkageIsWarning(strategyConfig: CompilerExecutionStrategyConfiguration) {
-        wasmProject(strategyConfig) {
-            val library = module("missing-dep/lib")
-            val app = module("missing-dep/app", dependencies = listOf(library))
-
-            library.compile()
-            app.compile()
-
-            app.link(compilationConfigAction = {
-                // drop the library from the linker classpath so the dependency is genuinely missing at link time
-                it.compilerArguments[LIBRARIES] = listOf(currentKotlinWasmStdlibKlibLocation)
-                it.compilerArguments[X_PARTIAL_LINKAGE_LOGLEVEL] = PartialLinkageLogLevel.WARNING
-            }) {
-                assertOutputsContains(app.expectedOutputFileName)
-                assertLogContainsPatterns(
-                    LogLevel.WARN,
                     ".*Function 'formatGreeting' can not be called: No function found for symbol.*"
                         .toRegex(RegexOption.DOT_MATCHES_ALL),
                 )
