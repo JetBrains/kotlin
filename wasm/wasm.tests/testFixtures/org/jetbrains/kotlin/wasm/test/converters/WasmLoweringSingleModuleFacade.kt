19d1201d676e4052d8e5069966c516a325468ed8
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-08-20 13:14:51 +0300

    [Wasm] Store `WasmModule` in `WasmCompilerResult`
    This will help us in the next commit, where we will extract
    binary generation into a separate pipeline phase
    and use that phase in tests. The artifact generated
    by that phase must contain `WasmModule` so it can be inspected
    in tests.

--- wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/converters/WasmLoweringSingleModuleFacade.kt
+++ wasm/wasm.tests/testFixtures/org/jetbrains/kotlin/wasm/test/converters/WasmLoweringSingleModuleFacade.kt
@@ -90,7 +90,7 @@ class WasmLoweringSingleModuleFacade(testServices: TestServices) :
         val compileResult = compileWasmIrToBinary(compiledIr, linkedModule)
 
         return WasmCompilationSetsBinaryArtifact(
-            WasmCompilationSet(linkedModule, compileResult)
+            WasmCompilationSet(compileResult)
         )
     }
 }
