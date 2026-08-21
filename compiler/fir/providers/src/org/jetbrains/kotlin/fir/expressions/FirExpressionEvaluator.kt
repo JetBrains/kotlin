33d78ccf36be55b76005afe1dceb31db6d5cde68
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-08-21 12:46:38 +0200

    fixup! [FIR] Normalize evaluation result in case of NaN value

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/expressions/FirExpressionEvaluator.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/expressions/FirExpressionEvaluator.kt
@@ -809,7 +809,7 @@ private fun evaluateUnary(arg: FirExpression, argType: ConeKotlinType, callableI
         callableId.callableName.asString(),
         compileTimeType,
         opr
-    ).normalize()
+    )
 }
 
 // Binary operators
@@ -850,12 +850,12 @@ private fun evaluateBinary(
         opr1,
         rightCompileTimeType,
         opr2
-    ).normalize()
+    )
 }
 
 private fun Any?.normalize(): Any? {
-    if (this is Float && this.isNaN()) return Float.NaN
-    if (this is Double && this.isNaN()) return Double.NaN
+    if (this is Float && this.isNaN()) return Float.fromBits(0x7fc00000)
+    if (this is Double && this.isNaN()) return Double.fromBits(0x7ff8000000000000L)
     return this
 }
 
@@ -974,7 +974,7 @@ private fun Any?.toConstExpression(
     return buildLiteralExpression(
         originalExpression.source,
         kind,
-        kind.convertToGivenKind(this),
+        kind.convertToGivenKind(this).normalize(),
         originalExpression.annotations.takeIf { it.isNotEmpty() }?.toMutableList(),
         setType = false,
     ).apply { replaceConeTypeOrNull(originalExpression.resolvedType) }
