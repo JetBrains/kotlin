a0ca8c7a1222bbb4a85c9e365a01f73f099127b1
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2026-08-21 12:19:31 +0200

    [Tests] Combine K1 and K2 JS IC tests
    There is no test runner implementation for K1 tests, so there
    is no need to keep additional abstraction.
    
    #KT-88539

--- compiler/incremental-compilation-impl/testFixtures/org/jetbrains/kotlin/incremental/AbstractIncrementalJsKlibCompilerRunnerTest.kt
+++ compiler/incremental-compilation-impl/testFixtures/org/jetbrains/kotlin/incremental/AbstractIncrementalJsKlibCompilerRunnerTest.kt
@@ -9,19 +9,19 @@ import org.jetbrains.kotlin.incremental.utils.TestCompilationResult
 import org.jetbrains.kotlin.incremental.utils.TestICReporter
 import java.io.File
 
-abstract class AbstractIncrementalK1JsKlibCompilerRunnerTest : AbstractIncrementalCompilerRunnerTestBase<K2JSCompilerArguments>() {
+abstract class AbstractIncrementalJsKlibCompilerRunnerTest : AbstractIncrementalCompilerRunnerTestBase<K2JSCompilerArguments>() {
     override fun createCompilerArguments(destinationDir: File, testDir: File): K2JSCompilerArguments =
         K2JSCompilerArguments().apply {
             libraries = ForTestCompileRuntime.stdlibJs().absolutePath
             outputDir = destinationDir.path
             moduleName = testDir.name
             sourceMap = false
-            languageVersion = "1.9"
+            languageVersion = LanguageVersion.LATEST_STABLE.versionString
         }
 
     override val buildLogFinder: BuildLogFinder
         get() = super.buildLogFinder.copy(
-            isFirEnabled = false,
+            isFirEnabled = true,
             isKlibEnabled = true,
             isJsEnabled = true,
             isScopeExpansionEnabled = scopeExpansionMode != CompileScopeExpansionMode.NEVER,
@@ -35,21 +35,10 @@ abstract class AbstractIncrementalK1JsKlibCompilerRunnerTest : AbstractIncrement
     }
 
     protected open val scopeExpansionMode = CompileScopeExpansionMode.NEVER
-}
-
-abstract class AbstractIncrementalK1JsKlibCompilerWithScopeExpansionRunnerTest : AbstractIncrementalK1JsKlibCompilerRunnerTest() {
-    override val scopeExpansionMode = CompileScopeExpansionMode.ALWAYS
-}
-
-abstract class AbstractIncrementalK2JsKlibCompilerWithScopeExpansionRunnerTest : AbstractIncrementalK1JsKlibCompilerWithScopeExpansionRunnerTest() {
-    override fun createCompilerArguments(destinationDir: File, testDir: File): K2JSCompilerArguments {
-        return super.createCompilerArguments(destinationDir, testDir).apply {
-            languageVersion = LanguageVersion.LATEST_STABLE.versionString
-        }
-    }
-
-    override val buildLogFinder: BuildLogFinder
-        get() = super.buildLogFinder.copy(isFirEnabled = true)
 
     override fun failFile(testDir: File): File = testDir.resolve("fail_js_k2.txt")
+}
+
+abstract class AbstractIncrementalJsKlibCompilerWithScopeExpansionRunnerTest : AbstractIncrementalJsKlibCompilerRunnerTest() {
+    override val scopeExpansionMode = CompileScopeExpansionMode.ALWAYS
 }
