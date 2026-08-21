9f5a1c0f38c4d68ae37a07605cdbfe21c6d805f6
Brian Norman <brian.norman@jetbrains.com>
2026-08-21 07:36:14 -0500

    fixup! [Language] Drop language and API versions through 2.1

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/TaskExecutionDiagnosticsIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/TaskExecutionDiagnosticsIT.kt
@@ -105,6 +105,7 @@ class TaskExecutionDiagnosticsIT : KGPBaseTest() {
             gradleVersion,
             btaVersion = null,
             expectedSeverity = KotlinToolingDiagnosticsSeverity.ERROR, // it's rendered as ERROR because of warning-mode=fail
+            customizedKotlinVersion = KotlinVersion.KOTLIN_2_2,
         )
 
     @DisplayName("KT-79851: emit unsupported language version kotlin-dsl diagnostic, custom compiler via BTA with deprecation")
