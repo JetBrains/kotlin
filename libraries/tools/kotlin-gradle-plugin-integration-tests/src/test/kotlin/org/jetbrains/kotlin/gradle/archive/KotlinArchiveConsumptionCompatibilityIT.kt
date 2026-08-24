bbfd0a7a12f4233e0ed531e4eb1b124bf5a2ed57
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2026-08-24 11:27:05 +0200

    [Gradle] Support compatibility for direct dependencies on platforms
    While discouraged, it was always possible to depend directly on
    platform version of library (i.e. lib-iosarm64, instead of just lib)
    in your build script.
    
    Scanning maven central shows, that it's typically happening by error,
    and people are going back to root dependency in next version, although
    there are some exception. Anyway, we should try to not break it.
    
    Unfortunately, part of the value brought by Kotlin Archive is
    removing platform publications, which would break people, who
    transitively depend on libraries who directly depend on platforms.
    
    To deal with it, we declare capability of old publication on
    platform variant in Kotlin Archive. This would lead to capability
    conflict, resolved in favor of Kotlin Archive by newly introduced
    compatibility rule.
    
    ^KT-88529

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/archive/KotlinArchiveConsumptionCompatibilityIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/archive/KotlinArchiveConsumptionCompatibilityIT.kt
@@ -11,14 +11,27 @@ import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
 import org.gradle.kotlin.dsl.kotlin
 import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.KOTLIN_VERSION
 import org.jetbrains.kotlin.gradle.plugin.diagnostics.KotlinToolingDiagnostics
 import org.jetbrains.kotlin.gradle.testbase.*
+import org.jetbrains.kotlin.gradle.testbase.addKgpToBuildScriptCompilationClasspath
+import org.jetbrains.kotlin.gradle.testbase.buildScriptInjection
+import org.jetbrains.kotlin.gradle.testbase.buildScriptReturn
+import org.jetbrains.kotlin.gradle.testbase.project
+import org.jetbrains.kotlin.gradle.testbase.settingsBuildScriptInjection
+import org.jetbrains.kotlin.gradle.testing.ResolvedComponentWithArtifacts
+import org.jetbrains.kotlin.gradle.testing.compilationResolution
+import org.jetbrains.kotlin.gradle.testing.runtimeResolution
+import org.jetbrains.kotlin.gradle.testing.prettyPrinted
 import org.jetbrains.kotlin.gradle.uklibs.PublishedProject
+import org.jetbrains.kotlin.gradle.uklibs.PublisherConfiguration
 import org.jetbrains.kotlin.gradle.uklibs.addPublishedProjectToRepositories
 import org.jetbrains.kotlin.gradle.uklibs.applyJvm
 import org.jetbrains.kotlin.gradle.uklibs.applyMultiplatform
+import org.jetbrains.kotlin.gradle.uklibs.ignoreAccessViolations
 import org.jetbrains.kotlin.gradle.uklibs.publish
 import org.junit.jupiter.api.DisplayName
+import kotlin.test.assertEquals
 
 @DisplayName("Consumption of a project published in the Kotlin Archive format by the previous Kotlin release")
 @MppGradlePluginTests
@@ -140,6 +153,96 @@ class KotlinArchiveConsumptionCompatibilityIT : KGPBaseTest() {
         }
     }
 
+    @GradleTest
+    fun legacyPlatformDependencyIsReplacedByArchiveTest(gradleVersion: GradleVersion) {
+        val library = project("empty", gradleVersion) {
+            addKgpToBuildScriptCompilationClasspath()
+            settingsBuildScriptInjection {
+                settings.rootProject.name = "library"
+            }
+            addSourceFile("commonMain", "fun libraryCommon() {}")
+            addSourceFile("jsMain", "fun libraryJs() {}")
+            buildScriptInjection {
+                project.applyMultiplatform {
+                    jvm()
+                    js()
+                }
+            }
+        }
+        val legacyLibrary = library.publish(publisherConfiguration = libraryPublisherConfiguration("1.0"))
+        val archiveLibrary = library.publish(
+            ENABLE_KAR_PUBLICATION,
+            publisherConfiguration = libraryPublisherConfiguration("2.0"),
+        )
+
+        val intermediateLibrary = project("empty", gradleVersion) {
+            addKgpToBuildScriptCompilationClasspath()
+            addPublishedProjectToRepositories(legacyLibrary)
+            settingsBuildScriptInjection {
+                settings.rootProject.name = "intermediateLibrary"
+            }
+            addSourceFile("jsMain", "fun intermediateJs() {\n    libraryJs()\n}")
+            buildScriptInjection {
+                project.applyMultiplatform {
+                    jvm()
+                    js()
+
+                    // That's quite unconventional way to depend, but people to it sometimes
+                    // It causes a problem - in KAR there is no more `library-js`
+                    sourceSets.jsMain.dependencies {
+                        api("${TEST_GROUP}:library-js:1.0")
+                    }
+                }
+            }
+        }.publish(
+            publisherConfiguration = PublisherConfiguration(group = TEST_GROUP, version = "1.0"),
+        )
+
+        val app = project("empty", gradleVersion) {
+            addKgpToBuildScriptCompilationClasspath()
+            addPublishedProjectToRepositories(archiveLibrary)
+            addPublishedProjectToRepositories(intermediateLibrary)
+            settingsBuildScriptInjection {
+                settings.rootProject.name = "app"
+            }
+            addSourceFile("jsMain", "fun appJs() {\n    libraryCommon()\n    libraryJs()\n    intermediateJs()\n}")
+            buildScriptInjection {
+                project.applyMultiplatform {
+                    this.jvm()
+                    this.js()
+
+                    sourceSets.commonMain.dependencies {
+                        implementation(intermediateLibrary.rootCoordinate)
+                        implementation(archiveLibrary.rootCoordinate)
+                    }
+                }
+            }
+        }
+
+        app.build(":compileKotlinJs") {
+            assertTasksExecuted(":compileKotlinJs")
+        }
+
+        val jsAppResolutionResult = app.buildScriptReturn {
+            project.ignoreAccessViolations {
+                val js = kotlinMultiplatform.targets.getByName("js")
+                mapOf(
+                    "compile" to js.compilationResolution(),
+                    "runtime" to js.runtimeResolution(),
+                )
+            }
+        }.buildAndReturn(":compileKotlinJs")
+        assertEquals(
+            expectedAppJsResolution.prettyPrinted,
+            jsAppResolutionResult.prettyPrinted,
+        )
+    }
+
+    private fun libraryPublisherConfiguration(version: String) = PublisherConfiguration(
+        group = TEST_GROUP,
+        version = version,
+    )
+
     /**
      * The archive is published by the Kotlin version under test, and consumed by [TESTED_KOTLIN_VERSION].
      */
@@ -166,6 +269,154 @@ class KotlinArchiveConsumptionCompatibilityIT : KGPBaseTest() {
     companion object {
         // TODO: change to 2.4.20, once it is released
         private const val TESTED_KOTLIN_VERSION = "2.4.20-RC"
+
+        private const val ENABLE_KAR_PUBLICATION = "-Pkotlin.publicationFormat=KOTLIN_ARCHIVE"
+
+        // Note: the group can't contain dots, as [PublishedProject] resolves it as a single directory
+        private const val TEST_GROUP = "kotlinArchiveTest"
+
+        private val expectedAppJsResolution = mapOf(
+            "compile" to mapOf(
+                "kotlinArchiveTest:intermediateLibrary-js:1.0" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "klib",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-api",
+                            "org.jetbrains.kotlin.cinteropCommonizerArtifactType" to "klib",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsApiElements-published",
+                ),
+                "kotlinArchiveTest:intermediateLibrary:1.0" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                    ),
+                    configuration = "jsApiElements-published",
+                ),
+                "kotlinArchiveTest:library:2.0" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "xz",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-api",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.kar.compression.method" to "NONE",
+                            "org.jetbrains.kotlin.kar.state" to "PLATFORM_ARTIFACTS_EXTRACTED",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsApiElements-published",
+                ),
+                "org.jetbrains.kotlin:kotlin-dom-api-compat:$KOTLIN_VERSION" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "klib",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-api",
+                            "org.jetbrains.kotlin.cinteropCommonizerArtifactType" to "klib",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsApiElements-published",
+                ),
+                "org.jetbrains.kotlin:kotlin-stdlib-js:$KOTLIN_VERSION" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "klib",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-api",
+                            "org.jetbrains.kotlin.cinteropCommonizerArtifactType" to "klib",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.klib.packaging" to "packed",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsApiElements",
+                ),
+                "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                    ),
+                    configuration = "jsApiElements",
+                ),
+            ),
+            "runtime" to mapOf(
+                "kotlinArchiveTest:intermediateLibrary-js:1.0" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "klib",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-runtime",
+                            "org.jetbrains.kotlin.cinteropCommonizerArtifactType" to "klib",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsRuntimeElements-published",
+                ),
+                "kotlinArchiveTest:intermediateLibrary:1.0" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                    ),
+                    configuration = "jsRuntimeElements-published",
+                ),
+                "kotlinArchiveTest:library:2.0" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "xz",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-runtime",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.kar.compression.method" to "NONE",
+                            "org.jetbrains.kotlin.kar.state" to "PLATFORM_ARTIFACTS_EXTRACTED",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsRuntimeElements-published",
+                ),
+                "org.jetbrains.kotlin:kotlin-dom-api-compat:$KOTLIN_VERSION" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "klib",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-runtime",
+                            "org.jetbrains.kotlin.cinteropCommonizerArtifactType" to "klib",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsRuntimeElements-published",
+                ),
+                "org.jetbrains.kotlin:kotlin-stdlib-js:$KOTLIN_VERSION" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                        mapOf(
+                            "artifactType" to "klib",
+                            "org.gradle.category" to "library",
+                            "org.gradle.jvm.environment" to "non-jvm",
+                            "org.gradle.usage" to "kotlin-runtime",
+                            "org.jetbrains.kotlin.cinteropCommonizerArtifactType" to "klib",
+                            "org.jetbrains.kotlin.js.compiler" to "ir",
+                            "org.jetbrains.kotlin.klib.packaging" to "packed",
+                            "org.jetbrains.kotlin.platform.type" to "js",
+                        ),
+                    ),
+                    configuration = "jsRuntimeElements",
+                ),
+                "org.jetbrains.kotlin:kotlin-stdlib:$KOTLIN_VERSION" to ResolvedComponentWithArtifacts(
+                    artifacts = listOf(
+                    ),
+                    configuration = "jsRuntimeElements",
+                ),
+            ),
+        )
 
         private val consumerCallingJvmDeclarations = "fun consume() {\n    commonMain()\n    jvmMain()\n}"
 
