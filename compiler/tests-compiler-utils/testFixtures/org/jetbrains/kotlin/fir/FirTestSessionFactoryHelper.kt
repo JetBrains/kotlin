4a89f5f636f7b1a48bc0f684b90bbf1511b39d99
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-08-24 12:13:30 +0200

    Tests: remove obsolete CodegenTestFiles, GenerationUtils

--- compiler/tests-compiler-utils/testFixtures/org/jetbrains/kotlin/fir/FirTestSessionFactoryHelper.kt
+++ compiler/tests-compiler-utils/testFixtures/org/jetbrains/kotlin/fir/FirTestSessionFactoryHelper.kt
@@ -5,10 +5,6 @@
 
 package org.jetbrains.kotlin.fir
 
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.StandardFileSystems
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.ObsoleteTestInfrastructure
 import org.jetbrains.kotlin.cli.create
 import org.jetbrains.kotlin.cli.jvm.compiler.VfsBasedProjectEnvironment
@@ -18,12 +14,11 @@ import org.jetbrains.kotlin.config.LanguageVersionSettings
 import org.jetbrains.kotlin.config.LanguageVersionSettingsImpl
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.fir.session.FirSessionFactoryHelper
-import org.jetbrains.kotlin.load.kotlin.PackagePartProvider
+import org.jetbrains.kotlin.jvm.environment.JvmClasspath
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import java.nio.file.Path
 import kotlin.io.path.pathString
-import org.jetbrains.kotlin.jvm.environment.JvmClasspath
 
 object FirTestSessionFactoryHelper {
     @ObsoleteTestInfrastructure
@@ -44,34 +39,6 @@ object FirTestSessionFactoryHelper {
             configuration,
             projectEnvironment.javaInterop(configuration),
             librariesClasspath,
-            incrementalCompilationContext = null,
-            extensionRegistrars = emptyList(),
-            dependenciesConfigurator = {
-                friendDependencies(friendsPaths.map { it.pathString })
-            }
-        )
-    }
-
-    @ObsoleteTestInfrastructure
-    fun createSessionForTests(
-        project: Project,
-        configuration: CompilerConfiguration,
-        moduleName: String = "TestModule",
-        friendsPaths: List<Path> = emptyList(),
-        getPackagePartProvider: (GlobalSearchScope) -> PackagePartProvider,
-    ): FirSession {
-        val projectEnvironment = VfsBasedProjectEnvironment(
-            project,
-            VirtualFileManager.getInstance().getFileSystem(StandardFileSystems.FILE_PROTOCOL),
-            getPackagePartProvider
-        )
-        return FirSessionFactoryHelper.createSessionWithDependencies(
-            Name.identifier(moduleName),
-            JvmPlatforms.unspecifiedJvmPlatform,
-            projectEnvironment,
-            configuration,
-            projectEnvironment.javaInterop(configuration),
-            JvmClasspath.ProjectLibraries(),
             incrementalCompilationContext = null,
             extensionRegistrars = emptyList(),
             dependenciesConfigurator = {
