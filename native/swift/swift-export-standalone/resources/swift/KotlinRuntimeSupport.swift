8c0ffc4c9c009bb89a57a6c8bb721416c27fb44a
Artem Olkov <artem.olkov@jetbrains.com>
2026-08-24 11:13:06 +0200

    [Swift Export] remove unused @used marker

--- native/swift/swift-export-standalone/resources/swift/KotlinRuntimeSupport.swift
+++ native/swift/swift-export-standalone/resources/swift/KotlinRuntimeSupport.swift
@@ -266,6 +266,5 @@ extension NSObject {
 // MARK: - Link anchor
 
 /// Anchor that keeps this compilation unit in the linked image. Without it this module gets stripped
-@used
 @_cdecl("KotlinRuntimeSupport_linkAnchor")
 func _kotlinRuntimeSupportLinkAnchor() {}
