5081ac4b66d0b088afa4f06fe0a0e9185543731a
Dmitrii Gridin <dmitry.gridin@jetbrains.com>
2026-08-25 14:09:28 +0000

    [stubs] Drop `KtStubElementType.isExpression`
    The flag was a reflective KtExpression.class.isAssignableFrom(psiClass)
    check stored on every stub element type instance for its single caller,
    ElementTypeUtils.isExpression. Element types migrating to the stub
    factory stop being KtStubElementType, so the check moves to the caller.
    
    Spell out the 42 element types whose PSI class is not a KtExpression;
    every remaining stub element type is an expression, so the fallback
    branch is now unconditionally true. Declarations are absent from the
    list because KtDeclaration extends KtExpression.
    
    ^KT-78356

--- compiler/psi/psi-impl/src/org/jetbrains/kotlin/ElementTypeUtils.kt
+++ compiler/psi/psi-impl/src/org/jetbrains/kotlin/ElementTypeUtils.kt
@@ -45,13 +45,64 @@ object ElementTypeUtils {
         return getSingleChildOrNull(tree)!!.tokenType as KtToken
     }
 
-    fun LighterASTNode.isExpression(): Boolean {
-        return when (val tokenType = this.tokenType) {
-            is KtNodeType,
-            LAMBDA_EXPRESSION,
-                -> true
-            is KtStubElementType<*, *> -> tokenType.isExpression
-            else -> false
-        }
+    /**
+     * Whether the node represents [org.jetbrains.kotlin.psi.KtExpression].
+     *
+     * Must be updated once a new non-expression element is added to the parser.
+     */
+    fun LighterASTNode.isExpression(): Boolean = when (tokenType) {
+        // Stub-based element types that are not `KtExpression`
+        CLASS_BODY,
+        COMPANION_BLOCK,
+        INITIALIZER_LIST,
+        VALUE_PARAMETER_LIST,
+        CONTEXT_PARAMETER_LIST,
+        CONTEXT_RECEIVER,
+        TYPE_PARAMETER_LIST,
+        TYPE_CONSTRAINT_LIST,
+        TYPE_CONSTRAINT,
+        SUPER_TYPE_LIST,
+        DELEGATED_SUPER_TYPE_ENTRY,
+        SUPER_TYPE_CALL_ENTRY,
+        SUPER_TYPE_ENTRY,
+        MODIFIER_LIST,
+        ANNOTATION,
+        ANNOTATION_ENTRY,
+        ANNOTATION_TARGET,
+        TYPE_REFERENCE,
+        USER_TYPE,
+        DYNAMIC_TYPE,
+        FUNCTION_TYPE,
+        FUNCTION_TYPE_RECEIVER,
+        NULLABLE_TYPE,
+        INTERSECTION_TYPE,
+        TYPE_PROJECTION,
+        LONG_STRING_TEMPLATE_ENTRY,
+        SHORT_STRING_TEMPLATE_ENTRY,
+        LITERAL_STRING_TEMPLATE_ENTRY,
+        ESCAPE_STRING_TEMPLATE_ENTRY,
+        STRING_INTERPOLATION_PREFIX,
+        TYPE_ARGUMENT_LIST,
+        VALUE_ARGUMENT_LIST,
+        VALUE_ARGUMENT,
+        CONTRACT_EFFECT_LIST,
+        CONTRACT_EFFECT,
+        LAMBDA_ARGUMENT,
+        VALUE_ARGUMENT_NAME,
+        PACKAGE_DIRECTIVE,
+        FILE_ANNOTATION_LIST,
+        IMPORT_LIST,
+        IMPORT_DIRECTIVE,
+        IMPORT_ALIAS,
+            -> false
+
+        is KtNodeType,
+        LAMBDA_EXPRESSION,
+            -> true
+
+        // All stub-based element types that are not `KtExpression` are listed above
+        is KtStubElementType<*, *> -> true
+
+        else -> false
     }
 }
