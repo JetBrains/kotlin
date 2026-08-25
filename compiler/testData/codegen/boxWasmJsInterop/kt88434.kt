df0dab52b4f648c25541a9f84ff3dfeea0653e05
Charles Zhang <6234337+karlosz@users.noreply.github.com>
2026-08-25 15:47:20 +0200

    [wasm]: Don't erase js external interop types when caching adapters. (#7548)
    As the test case shows, it's possible for different external JS types
    such as Array and Object to have different converter behavior w.r.t
    casting. However, the js interop code was too eager in erasing and hence
    caching the same converter for these types which need different
    handling.
    
    ^KT-88434 Fixed

---
+++ compiler/testData/codegen/boxWasmJsInterop/kt88434.kt
@@ -0,0 +1,39 @@
+// WITH_STDLIB
+// RUN_THIRD_PARTY_OPTIMIZER
+// TARGET_BACKEND: WASM
+
+// KT-88434
+//
+// Two distinct external function types whose only difference is their (external)
+// argument/return type collapse to the same JS-erased signature string
+// `(Js)->Js` in JsInteropFunctionsLowering. The JS->Kotlin closure converter is
+// cached per that signature string, so the converter created for the first
+// type was reused for the second, carrying the first type's runtime checks
+// (e.g. an instanceof/ref.test for `Array`). When the reused converter was
+// invoked on a value of the second type the check fails and a
+// ClassCastException is thrown.
+
+@JsName("Array")
+external class A : JsAny {
+    val length: Int
+}
+
+external interface B : JsAny {
+    val x: Int
+}
+
+fun array(): A = js("[]")
+fun obj(): B = js("({ x: 1 })")
+
+fun arrayClosure(): (A) -> A = js("x => x")
+fun objectClosure(): (B) -> B = js("x => x")
+
+fun box(): String {
+    val a: A = arrayClosure()(array())
+    if (a.length != 0) return "Fail: array closure returned wrong length"
+
+    val b: B = objectClosure()(obj())
+    if (b.x != 1) return "Fail: object closure returned wrong x"
+
+    return "OK"
+}
