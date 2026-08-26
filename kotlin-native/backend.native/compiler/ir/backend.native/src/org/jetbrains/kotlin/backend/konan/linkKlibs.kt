6282b76caa1d9f8b403c36127d94eccff831504e
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-08-26 10:44:51 +0200

    [Native] Keep the list of useful IR modules in RTO as `List`
    Refactor `NativeBackendContext` and `LinkKlibsOutput` classes to
    keep the list of "useful" IR modules in the reverse topological
    order as `List<IrModuleFragment>` instead of a `Map`.
    
    ^KT-61096
    
    Add the appropriate comments to the source code.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/linkKlibs.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/linkKlibs.kt
@@ -38,7 +38,6 @@ import org.jetbrains.kotlin.library.uniqueName
 import org.jetbrains.kotlin.resolve.CommonCompilerDeserializationConfiguration
 import org.jetbrains.kotlin.resolve.descriptorUtil.module
 import org.jetbrains.kotlin.serialization.deserialization.DeserializationConfiguration
-import java.nio.file.Path
 
 internal interface LinkKlibsContext : NativeBackendPhaseContext {
     val symbolTable: SymbolTable?
@@ -55,8 +54,13 @@ data class LinkKlibsInput(
         val moduleDescriptor: ModuleDescriptor,
 )
 
+/**
+ * @property irModules The list of IR module fragments in the reverse topological order. This list only contains IR modules
+ *   that are treated as "useful", i.e. each of them either was explicitly passed via CLI argument to the compiler or was loaded
+ *   from the Kotlin/Native distribution implicitly and has at least one declaration that has been loaded/linked from it.
+ */
 internal class LinkKlibsOutput(
-        val irModules: Map<Path, IrModuleFragment>,
+        val irModules: List<IrModuleFragment>,
         val irModule: IrModuleFragment,
         val irBuiltIns: IrBuiltIns,
         val symbols: BackendNativeSymbols,
@@ -133,9 +137,8 @@ internal fun LinkKlibsContext.linkKlibs(
         }
     }
 
-    val irModulesForLinkKlibsOutput: Map<Path, IrModuleFragment> = sortedUsefulModuleDependencies.allDependencies
+    val irModulesForLinkKlibsOutput: List<IrModuleFragment> = sortedUsefulModuleDependencies.allDependencies
             .filter { it.name != KlibResolvedModuleDescriptorsFactoryImpl.FORWARD_DECLARATIONS_MODULE_NAME && it.descriptor !== moduleDescriptor }
-            .associateBy { it.kotlinLibrary!!.path }
 
     return if (libraryToCache == null) {
         val mainModule = IrModuleFragmentImpl(moduleDescriptor)
@@ -148,10 +151,10 @@ internal fun LinkKlibsContext.linkKlibs(
                 irLinker = irLinker
         )
     } else {
-        val libraryPath: Path = libraryToCache.klib.path
-        val libraryModule = irModulesForLinkKlibsOutput[libraryPath] ?: error("No module for the library being cached: $libraryPath")
+        val [libraryModules, otherModules] = irModulesForLinkKlibsOutput.partition { it.kotlinLibrary == libraryToCache.klib }
+        val libraryModule = libraryModules.firstOrNull() ?: error("No module for the library being cached: ${libraryToCache.klib}")
         LinkKlibsOutput(
-                irModules = irModulesForLinkKlibsOutput.filterKeys { it != libraryPath },
+                irModules = otherModules,
                 irModule = libraryModule,
                 irBuiltIns = irBuiltIns,
                 symbols = symbols,
