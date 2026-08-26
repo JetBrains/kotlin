JetBrains/kotlin

commit 23142f0f95803f6e435739760a96785b471d6ef4

[Native] Reduce the use of `config.friendModuleFiles` property
We should rely on `LoadedNativeKlibs.friends` instead.

^KT-61096

compiler/cli/cli-native-klib/src/main/kotlin/org/jetbrains/kotlin/native/pipeline/NativeFrontendPipelinePhase.kt
@@ -22,12 +22,13 @@ import org.jetbrains.kotlin.fir.*
 import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrar
 import org.jetbrains.kotlin.fir.pipeline.*
 import org.jetbrains.kotlin.fir.resolve.ImplicitIntegerCoercionModuleCapability
+import org.jetbrains.kotlin.io.canonicalPathString
 import org.jetbrains.kotlin.konan.config.konanPrintFiles
 import org.jetbrains.kotlin.library.metadata.isCInteropLibrary
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.native.NativeFirstStageCompilationConfig
 import org.jetbrains.kotlin.native.NativeFirstStagePhaseContext
-import kotlin.io.path.absolutePathString
+import kotlin.io.path.pathString
 
 object NativeFrontendPipelinePhase : PipelinePhase<ConfigurationPipelineArtifact, NativeFrontendArtifact>(
     name = "NativeFrontendPhase",
@@ -70,9 +71,9 @@ object NativeFrontendPipelinePhase : PipelinePhase<ConfigurationPipelineArtifact
         val dependencyList = DependencyListForCliModule.build {
             val [interopLibs, regularLibs] = config.loadedKlibs.all.partition { it.isCInteropLibrary() }
             defaultDependenciesSet(mainModuleName) {
-                dependencies(regularLibs.map { it.path.absolutePathString() })
-                friendDependencies(config.friendModuleFiles.map { it.absolutePathString() })
-                dependsOnDependencies(config.refinesModuleFiles.map { it.absolutePathString() })
+                dependencies(regularLibs.map { it.canonicalPath.pathString })
+                friendDependencies(config.loadedKlibs.friends.map { it.canonicalPath.pathString })
+                dependsOnDependencies(config.refinesModuleFiles.map { it.canonicalPathString() })
             }
             if (interopLibs.isNotEmpty()) {
                 val interopModuleData =
@@ -80,7 +81,7 @@ object NativeFrontendPipelinePhase : PipelinePhase<ConfigurationPipelineArtifact
                         Name.special("<regular interop dependencies of $mainModuleName>"),
                         FirModuleCapabilities.create(listOf(ImplicitIntegerCoercionModuleCapability))
                     )
-                dependencies(interopModuleData, interopLibs.map { it.path.absolutePathString() })
+                dependencies(interopModuleData, interopLibs.map { it.canonicalPath.pathString })
             }
             // TODO: !!! dependencies module data?
         }

compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/NativeCompilationConfig.kt
@@ -46,6 +46,7 @@ interface NativeCompilationConfig {
     val metadataKlib: Boolean
         get() = configuration.metadataKlib
 
+    // TODO(KT-61096): Read friend paths from `LoadedNativeKlibs.friends`, drop this property.
     val friendModuleFiles: Set<Path>
         get() = configuration.konanFriendLibraries.map { Path(it) }.toSet()
 

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/CacheBuilder.kt
@@ -38,6 +38,7 @@ import kotlin.io.path.deleteRecursively
 import kotlin.io.path.exists
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.name
+import kotlin.io.path.pathString
 import kotlin.io.path.writeText
 
 internal fun KotlinLibrary.getAllTransitiveDependencies(allLibraries: Map<String, KotlinLibrary>): List<KotlinLibrary> {
@@ -548,9 +549,9 @@ class CacheBuilder(
             config.configuration.konanHome?.let {
                 this.konanHome = it
             }
-            val libraryPath = library.path.absolutePathString()
-            val libraries = dependencies.filter { it.isExplicitlySpecifiedByUserInCLIArgument }.map { it.path.absolutePathString() }
-            val cachedLibraries = dependencies.zip(dependencyCaches).associate { it.first.path.absolutePathString() to it.second }
+            val libraryPath = library.canonicalPath.pathString
+            val libraries = dependencies.filter { it.isExplicitlySpecifiedByUserInCLIArgument }.map { it.canonicalPath.pathString }
+            val cachedLibraries = dependencies.zip(dependencyCaches).associate { it.first.canonicalPath.pathString to it.second }
             configuration.reportLog(
                     "-p static_cache -Xadd-cache=${library.path} \\\n" +
                             libraries.joinToString("\n") { "-library $it \\" } + "\n" +
@@ -569,7 +570,7 @@ class CacheBuilder(
             konanLibraries = libraries + libraryPath
             val generateTestRunner = this@CacheBuilder.generateTestRunner
             if (generateTestRunner != TestRunnerKind.NONE && libraryPath in this@CacheBuilder.includedLibraries) {
-                konanFriendLibraries = config.friendModuleFiles.map { it.absolutePathString() }
+                konanFriendLibraries = config.loadedKlibs.friends.map { it.canonicalPath.pathString }
                 this.generateTestRunner = generateTestRunner
                 konanIncludedLibraries = listOf(libraryPath)
                 configuration.testDumpOutputPath?.let { testDumpOutputPath = it }

kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/TopDownAnalyzerFacadeForKonan.kt
@@ -59,7 +59,7 @@ internal object TopDownAnalyzerFacadeForKonan {
                 storageManager = projectContext.storageManager,
                 builtIns = module.builtIns,
                 languageVersionSettings = config.languageVersionSettings,
-                friendModuleFiles = config.friendModuleFiles,
+                friendModuleFiles = config.friendModuleFiles, // TODO(KT-61096): Read friend paths from `LoadedNativeKlibs.friends`
                 refinesModuleFiles = config.refinesModuleFiles,
                 includedLibraryFiles = config.loadedKlibs.included.map { it.path }.toSet(),
                 additionalDependencyModules = listOf(module),

commit 68d23e95d1e23aef868bdaae1cb866757805abbe

[KLIB Resolve] Implement KlibLibraryPathFilterTest
To reproduce failures with symlinks

In scope of: KT-61096
Regression introduced in: 23142f0f95803f6e435739760a96785b471d6ef4

compiler/tests/org/jetbrains/kotlin/klib/KlibLibraryPathFilterTest.kt
@@ -1,0 +1,102 @@
+/*
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.klib
+
+import org.jetbrains.kotlin.fir.deserialization.LibraryPathFilter
+import org.jetbrains.kotlin.library.KotlinLibraryVersioning
+import org.jetbrains.kotlin.library.impl.BuiltInsPlatform
+import org.jetbrains.kotlin.library.loader.KlibLoader
+import org.jetbrains.kotlin.library.writer.KlibWriter
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assumptions.assumeTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.pathString
+
+class KlibLibraryPathFilterTest {
+    @TempDir
+    lateinit var tempDir: Path
+
+    @Test
+    fun testCanonicalFilterAcceptsLibraryLoadedThroughSymlink() {
+        val klib = createKlib(tempDir.resolve("library"))
+        val library = loadKlib(createSymlink(tempDir.resolve("library-link"), klib))
+        assertNotEquals(library.path, library.canonicalPath)
+
+        val filter = LibraryPathFilter.LibraryList(setOf(library.canonicalPath))
+        assertTrue(filter.accepts(library.path))
+    }
+
+    @Test
+    fun testCanonicalDirectoryFilterAcceptsLibraryLoadedThroughSymlinkedDirectory() {
+        val realDirectory = tempDir.resolve("real").createDirectories()
+        createKlib(realDirectory.resolve("library"))
+        val linkedDirectory = createSymlink(tempDir.resolve("linked"), realDirectory)
+        val library = loadKlib(linkedDirectory.resolve("library"))
+
+        val filter = LibraryPathFilter.LibraryList(setOf(realDirectory))
+        assertTrue(filter.accepts(library.path))
+    }
+
+    @Test
+    fun testCanonicalFilterRejectsDifferentLibraryLoadedThroughSymlink() {
+        val firstKlib = createKlib(tempDir.resolve("first"))
+        val secondKlib = createKlib(tempDir.resolve("second"))
+        val firstLibrary = loadKlib(createSymlink(tempDir.resolve("first-link"), firstKlib))
+
+        val filter = LibraryPathFilter.LibraryList(setOf(secondKlib.toRealPath()))
+        assertFalse(filter.accepts(firstLibrary.path))
+    }
+
+    @Test
+    fun testLexicalFiltering() {
+        val libraryDirectory = tempDir.resolve("libraries")
+        val filter = LibraryPathFilter.LibraryList(setOf(libraryDirectory))
+
+        assertAll(
+            { assertTrue(filter.accepts(libraryDirectory)) },
+            { assertTrue(filter.accepts(libraryDirectory.resolve("library.klib"))) },
+            { assertFalse(filter.accepts(tempDir)) },
+            { assertFalse(filter.accepts(tempDir.resolve("other-libraries"))) },
+            { assertFalse(filter.accepts(null)) },
+        )
+    }
+
+    @Test
+    fun testNonExistingPathsDoNotMatchThroughRealPathFallback() {
+        val filter = LibraryPathFilter.LibraryList(setOf(tempDir.resolve("missing-library")))
+        assertFalse(filter.accepts(tempDir.resolve("other-missing-library")))
+    }
+
+    private fun createKlib(path: Path): Path {
+        KlibWriter {
+            manifest {
+                moduleName(path.fileName.pathString)
+                versions(KotlinLibraryVersioning(null, null, null))
+                platformAndTargets(BuiltInsPlatform.COMMON)
+            }
+        }.writeTo(path.pathString)
+        return path
+    }
+
+    private fun createSymlink(link: Path, target: Path): Path {
+        assumeTrue(runCatching { Files.createSymbolicLink(link, target) }.isSuccess)
+        return link
+    }
+
+    private fun loadKlib(path: Path) = KlibLoader { libraryPaths(path) }.load().let { result ->
+        assertFalse(result.hasProblems)
+        assertEquals(1, result.librariesStdlibFirst.size)
+        result.librariesStdlibFirst.single()
+    }
+}