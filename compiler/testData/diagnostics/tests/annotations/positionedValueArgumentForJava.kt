ec4773fed595607ab14c3a3b070aa1dd5b3ac454
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-08-27 13:14:08 +0200

    [Tests] Reproduce #KT-88667

---
+++ compiler/testData/diagnostics/tests/annotations/positionedValueArgumentForJava.kt
@@ -0,0 +1,22 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-88667
+// FILE: TestAnn.java
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TestAnn {
+    String message();
+}
+// FILE: test.kt
+import java.lang.Deprecated as deprecated
+
+@TestAnn(<!POSITIONED_VALUE_ARGUMENT_FOR_JAVA_ANNOTATION!>"message"<!>)
+class A {
+    @get:TestAnn("message") @get:deprecated
+    val x: Int = 10
+    fun test() = TestAnn("message")
+}
+
+/* GENERATED_FIR_TAGS: annotationUseSiteTargetPropertyGetter, classDeclaration, functionDeclaration, integerLiteral,
+interfaceDeclaration, javaProperty, propertyDeclaration, stringLiteral */
