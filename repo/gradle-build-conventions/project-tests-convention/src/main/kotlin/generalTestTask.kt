69164153b5c06c91a63965d990136906be1c9710
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-08-27 11:12:24 +0200

    [Build] Universally support test muting
    In scope of: KT-88863
    ^KT-88863 fixed

--- repo/gradle-build-conventions/project-tests-convention/src/main/kotlin/generalTestTask.kt
+++ repo/gradle-build-conventions/project-tests-convention/src/main/kotlin/generalTestTask.kt
@@ -22,22 +22,6 @@ import java.io.File
 import java.nio.file.Files
 import javax.inject.Inject
 
-private abstract class MuteWithDatabaseArgumentProvider @Inject constructor(objects: ObjectFactory) : CommandLineArgumentProvider {
-    @get:InputFile
-    @get:PathSensitive(PathSensitivity.NONE)
-    val mutesFile: RegularFileProperty = objects.fileProperty()
-
-    override fun asArguments(): Iterable<String> =
-        listOf("-Dorg.jetbrains.kotlin.test.mutes.file=${mutesFile.get().asFile.canonicalPath}")
-}
-
-private fun Test.muteWithDatabase() {
-    jvmArgumentProviders.add(
-        project.objects.newInstance<MuteWithDatabaseArgumentProvider>().apply {
-            mutesFile.fileValue(File(project.rootDir, "tests/mute-common.csv"))
-        })
-    systemProperty("junit.jupiter.extensions.autodetection.enabled", "true")
-}
 
 abstract class GeneralTestArgumentProvider @Inject constructor() : CommandLineArgumentProvider {
     @get:Inject
@@ -114,8 +98,6 @@ internal fun Project.createGeneralTestTask(
             classpath.from(ideaHomeForTests)
             directory.value(ideaHomePathForTests())
         })
-
-        muteWithDatabase()
 
         if (shouldInstrument) {
             val agentJar = configurations.detachedConfiguration(dependencies.project(":test-instrumenter")).apply { isTransitive = false }
