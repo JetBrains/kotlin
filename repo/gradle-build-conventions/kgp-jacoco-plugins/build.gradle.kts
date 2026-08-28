ef0175fc6872d6dbb044d71298d0d099cf529f65
Andrei Tyrin <andrei.tyrin@jetbrains.com>
2026-08-28 11:42:15 +0200

    fixup! Add test coverage collection for KGP integration tests

--- repo/gradle-build-conventions/kgp-jacoco-plugins/build.gradle.kts
+++ repo/gradle-build-conventions/kgp-jacoco-plugins/build.gradle.kts
@@ -3,7 +3,6 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 plugins {
     `java-gradle-plugin`
-    `embedded-kotlin`
     `kotlin-dsl`
 }
 
@@ -11,23 +10,25 @@ description = "Plugins for KGP code coverage"
 
 kotlin {
     @OptIn(ExperimentalKotlinGradlePluginApi::class, ExperimentalBuildToolsApi::class)
-    compilerVersion = libs.versions.kotlin.`for`.gradle.plugins.compilation
+    compilerVersion = embeddedKotlinVersion
+    coreLibrariesVersion = embeddedKotlinVersion
     jvmToolchain(17)
 }
 
 dependencies {
-    compileOnly(gradleApi())
-    compileOnly(gradleKotlinDsl())
     api(project(":gradle-plugins-common"))
     implementation(kotlinBuildHelpers())
 }
 
-project.configurations.named(org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME + "Main") {
-    resolutionStrategy {
-        eachDependency {
-            if (this.requested.group == "org.jetbrains.kotlin") useVersion(libs.versions.kotlin.`for`.gradle.plugins.compilation.get())
+listOf(
+    org.jetbrains.kotlin.gradle.plugin.PLUGIN_CLASSPATH_CONFIGURATION_NAME + "Main",
+    "compilePluginsBlocksPluginClasspathElements",
+).forEach { confName ->
+    project.configurations.named(confName) {
+        resolutionStrategy {
+            eachDependency {
+                if (this.requested.group == "org.jetbrains.kotlin") useVersion(embeddedKotlinVersion)
+            }
         }
     }
 }
-
-kotlin.compilerOptions.moduleName.value(project.name)
