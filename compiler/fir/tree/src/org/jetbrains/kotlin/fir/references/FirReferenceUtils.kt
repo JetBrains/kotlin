cffe3798c5090522bbf9e8246d49db573706099c
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-08-31 11:13:48 +0300

    [FIR] Add contracts in `toResolvedSymbol` reference utilities

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/references/FirReferenceUtils.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/references/FirReferenceUtils.kt
@@ -3,6 +3,8 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+@file:OptIn(ExperimentalContracts::class)
+
 package org.jetbrains.kotlin.fir.references
 
 import org.jetbrains.kotlin.KtSourceElement
@@ -29,6 +31,7 @@ val FirReference.resolved: FirResolvedNamedReference? get() = this as? FirResolv
 inline fun <reified T : FirBasedSymbol<*>> FirReference.toResolvedSymbol(
     discardErrorReference: Boolean = false
 ): @kotlin.internal.NoInfer T? {
+    contract { returnsNotNull() implies (this@toResolvedSymbol is FirResolvedNamedReference) }
     if (discardErrorReference && this is FirResolvedErrorReference) {
         return null
     }
@@ -36,46 +39,55 @@ inline fun <reified T : FirBasedSymbol<*>> FirReference.toResolvedSymbol(
 }
 
 fun FirReference.toResolvedBaseSymbol(discardErrorReference: Boolean = false): FirBasedSymbol<*>? {
+    contract { returnsNotNull() implies (this@toResolvedBaseSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirBasedSymbol<*>>(discardErrorReference)
 }
 
 fun FirReference.toResolvedCallableSymbol(discardErrorReference: Boolean = false): FirCallableSymbol<*>? {
+    contract { returnsNotNull() implies (this@toResolvedCallableSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirCallableSymbol<*>>(discardErrorReference)
 }
 
 fun FirReference.toResolvedTypeParameterSymbol(discardErrorReference: Boolean = false): FirTypeParameterSymbol? {
+    contract { returnsNotNull() implies (this@toResolvedTypeParameterSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirTypeParameterSymbol>(discardErrorReference)
 }
 
 fun FirReference.toResolvedVariableSymbol(discardErrorReference: Boolean = false): FirVariableSymbol<*>? {
+    contract { returnsNotNull() implies (this@toResolvedVariableSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirVariableSymbol<*>>(discardErrorReference)
 }
 
 fun FirReference.toResolvedPropertySymbol(discardErrorReference: Boolean = false): FirPropertySymbol? {
+    contract { returnsNotNull() implies (this@toResolvedPropertySymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirPropertySymbol>(discardErrorReference)
 }
 
 fun FirReference.toResolvedValueParameterSymbol(discardErrorReference: Boolean = false): FirValueParameterSymbol? {
+    contract { returnsNotNull() implies (this@toResolvedValueParameterSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirValueParameterSymbol>(discardErrorReference)
 }
 
 fun FirReference.toResolvedFunctionSymbol(discardErrorReference: Boolean = false): FirFunctionSymbol<*>? {
+    contract { returnsNotNull() implies (this@toResolvedFunctionSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirFunctionSymbol<*>>(discardErrorReference)
 }
 
 fun FirReference.toResolvedNamedFunctionSymbol(discardErrorReference: Boolean = false): FirNamedFunctionSymbol? {
+    contract { returnsNotNull() implies (this@toResolvedNamedFunctionSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirNamedFunctionSymbol>(discardErrorReference)
 }
 
 fun FirReference.toResolvedConstructorSymbol(discardErrorReference: Boolean = false): FirConstructorSymbol? {
+    contract { returnsNotNull() implies (this@toResolvedConstructorSymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirConstructorSymbol>(discardErrorReference)
 }
 
 fun FirReference.toResolvedEnumEntrySymbol(discardErrorReference: Boolean = false): FirEnumEntrySymbol? {
+    contract { returnsNotNull() implies (this@toResolvedEnumEntrySymbol is FirResolvedNamedReference) }
     return this.toResolvedSymbol<FirEnumEntrySymbol>(discardErrorReference)
 }
 
-@OptIn(ExperimentalContracts::class)
 fun FirReference.isError(): Boolean {
     contract {
         returns(true) implies (this@isError is FirDiagnosticHolder)
