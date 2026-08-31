e8e581b36f0fb742b5e7c905c89b3cfd417a7890
Vladimir Sukharev <Vladimir.Sukharev@jetbrains.com>
2026-08-31 23:28:29 +0300

    [Tests][Klibs] Adjust 6 synthetic accessors tests to contain tailrec fun
    ^KT-87442

--- compiler/testData/klib/syntheticAccessors/topLevelPrivate/singleFile/useInfixTailrecPrivateTopLevelFunThroughInternalInlineFun.kt
+++ compiler/testData/klib/syntheticAccessors/topLevelPrivate/singleFile/useInfixTailrecPrivateTopLevelFunThroughInternalInlineFun.kt
@@ -1,7 +1,11 @@
-private infix tailrec fun Int.sum(that: Int): Int =
-    if (this > that) 0 else that + (this sum (that - 1))
+private infix tailrec fun Int.gcd(other: Int): Int =
+    if (other == 0) {
+        this
+    } else {
+        other gcd (this % other)
+    }
 
-internal inline fun internalFun(x: Int) = (x - 2) sum x
+internal inline fun internalFun(x: Int) = (x - 2) gcd x
 
 fun box(): String =
-    if (internalFun(4) == 9) "OK" else "FAIL"
+    if (internalFun(4) == 2) "OK" else "FAIL"
