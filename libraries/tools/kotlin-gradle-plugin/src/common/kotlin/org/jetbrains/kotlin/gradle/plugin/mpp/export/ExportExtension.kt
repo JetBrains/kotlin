5280e436c5982bb71693dfe40e8da5f6b4ab7aa8
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2026-08-31 17:06:00 +0200

    [Gradle] Introduce xcodeIntegration in export.swift extension
    ^KT-88850

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/export/ExportExtension.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/mpp/export/ExportExtension.kt
@@ -7,13 +7,15 @@ package org.jetbrains.kotlin.gradle.plugin.mpp.export
 
 import org.gradle.api.Action
 import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.dsl.KotlinGradlePluginDsl
 import org.jetbrains.kotlin.gradle.export.ExperimentalExportDsl
 import org.jetbrains.kotlin.gradle.swiftexport.ExperimentalSwiftExportDsl
 import javax.inject.Inject
 
-const val EXPORT_EXTENSION_NAME = "export"
+internal const val EXPORT_EXTENSION_NAME = "export"
 
 /**
  * An *experimental* plugin DSL extension to configure platform-specific export functionality.
@@ -43,14 +45,16 @@ See Gradle issue https://github.com/gradle/gradle/issues/32019
 abstract class ExportExtension @Inject constructor(
     objectFactory: ObjectFactory,
 ) {
-    internal val swiftExportConfiguration: SwiftExportConfiguration = DefaultSwiftExportConfiguration(objectFactory)
+    private val defaultSwiftExportConfiguration = DefaultSwiftExportConfiguration(objectFactory)
+
+    internal val swiftExportConfiguration: SwiftExportConfiguration get() = defaultSwiftExportConfiguration
 
     /**
      * Configure Swift Export.
      */
     @ExperimentalExportDsl
     fun swift(configure: SwiftExportConfigurationDsl.() -> Unit) {
-        (swiftExportConfiguration as SwiftExportConfigurationDsl).configure()
+        defaultSwiftExportConfiguration.configure()
     }
 
     /**
@@ -82,6 +86,7 @@ abstract class ExportExtension @Inject constructor(
  * @since 2.5.0
  */
 @ExperimentalSwiftExportDsl
+@KotlinGradlePluginDsl
 interface SwiftExportConfigurationDsl {
     /**
      * Configure the name of this module that will be used in Swift Export.
@@ -92,6 +97,33 @@ interface SwiftExportConfigurationDsl {
      * Configure this module's root package. If provided, the root package will be used for package flattening in Swift Export.
      */
     val rootPackage: Property<String>
+
+    /**
+     * Activate the Xcode integration for this module.
+     *
+     * Repeated calls configure the same integration, so the order of the calls doesn't matter.
+     * 
+     * The integration is only activated in the projects where this function is called.
+     */
+    fun xcodeIntegration()
+
+    /**
+     * Activate and configure the Xcode integration for this module.
+     *
+     * Repeated calls configure the same integration, so the order of the calls doesn't matter.
+     *
+     * The integration is only activated in the projects where this function is called.
+     */
+    fun xcodeIntegration(configure: SwiftExportXcodeIntegration.() -> Unit)
+
+    /**
+     * Activate and configure the Xcode integration for this module.
+     *
+     * Repeated calls configure the same integration, so the order of the calls doesn't matter.
+     *
+     * The integration is only activated in the projects where this function is called.
+     */
+    fun xcodeIntegration(configure: Action<SwiftExportXcodeIntegration>)
 }
 
 /**
@@ -110,11 +142,78 @@ internal interface SwiftExportConfiguration {
      * This module's root package.
      */
     val rootPackage: Property<String>
+
+    /**
+     * The Xcode integration activated via [SwiftExportConfigurationDsl.xcodeIntegration],
+     * or `null` if it was never activated for this module.
+     */
+    val activatedXcodeIntegration: SwiftExportXcodeIntegrationConfiguration?
 }
 
-private class DefaultSwiftExportConfiguration(objectFactory: ObjectFactory) : SwiftExportConfiguration, SwiftExportConfigurationDsl {
+/**
+ * Represents the Xcode integration activated for an exported module.
+ *
+ * This API is experimental and may change in future versions.
+ */
+@ExperimentalSwiftExportDsl
+internal interface SwiftExportXcodeIntegrationConfiguration {
+    /**
+     * The settings passed to Swift Export for this module.
+     */
+    val settings: Provider<Map<String, String>>
+}
+
+/**
+ * Represents Swift Export integration for consumers.
+ *
+ * This API is experimental and may change in future versions.
+ */
+@ExperimentalSwiftExportDsl
+@KotlinGradlePluginDsl
+interface SwiftExportIntegration {
+    /**
+     * Configure the settings passed to Swift Export for this module.
+     */
+    val settings: MapProperty<String, String>
+}
+
+/**
+ * Represents Swift Export integration for Xcode consumers.
+ *
+ * This API is experimental and may change in future versions.
+ */
+@ExperimentalSwiftExportDsl
+@KotlinGradlePluginDsl
+interface SwiftExportXcodeIntegration : SwiftExportIntegration
+
+private class DefaultSwiftExportConfiguration(
+    private val objectFactory: ObjectFactory,
+) : SwiftExportConfiguration, SwiftExportConfigurationDsl {
     override val moduleName: Property<String> = objectFactory.property(String::class.java)
     override val rootPackage: Property<String> = objectFactory.property(String::class.java)
+
+    private var xcodeIntegrationConfiguration: DefaultSwiftExportXcodeIntegration? = null
+
+    override val activatedXcodeIntegration: SwiftExportXcodeIntegrationConfiguration?
+        get() = xcodeIntegrationConfiguration
+
+    override fun xcodeIntegration() = xcodeIntegration { }
+
+    override fun xcodeIntegration(configure: SwiftExportXcodeIntegration.() -> Unit) {
+        val integration = xcodeIntegrationConfiguration
+            ?: DefaultSwiftExportXcodeIntegration(objectFactory).also { xcodeIntegrationConfiguration = it }
+        integration.configure()
+    }
+
+    override fun xcodeIntegration(configure: Action<SwiftExportXcodeIntegration>) = xcodeIntegration {
+        configure.execute(this)
+    }
+}
+
+private class DefaultSwiftExportXcodeIntegration(
+    objectFactory: ObjectFactory,
+) : SwiftExportXcodeIntegration, SwiftExportXcodeIntegrationConfiguration {
+    override val settings: MapProperty<String, String> = objectFactory.mapProperty(String::class.java, String::class.java)
 }
 
 internal fun ObjectFactory.ExportExtension(): ExportExtension = newInstance(ExportExtension::class.java)
