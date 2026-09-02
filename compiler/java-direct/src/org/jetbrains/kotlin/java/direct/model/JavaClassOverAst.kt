0288084d9f30b6822f8ca5ea0963e02ef4780851
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-09-02 17:43:40 +0200

    Java Direct: add java.lang.Record as an implicit supertype when needed
    fixes CliTestGenerated.DiagnosticTests.testRecordAsSingleFileRoot
    with java-direct

--- compiler/java-direct/src/org/jetbrains/kotlin/java/direct/model/JavaClassOverAst.kt
+++ compiler/java-direct/src/org/jetbrains/kotlin/java/direct/model/JavaClassOverAst.kt
@@ -85,11 +85,13 @@ class JavaClassOverAst(
     override val supertypes: Collection<JavaClassifierType> by lazy(LazyThreadSafetyMode.PUBLICATION) {
         val result = mutableListOf<JavaClassifierType>()
 
-        if (isEnum) {
-            result.add(EnumSupertypeForJavaDirect(this, memberResolutionContext))
-        } else if (isAnnotationType) {
-            result.add(SimpleClassifierType("java.lang.annotation.Annotation", memberResolutionContext))
-        }
+            if (isEnum) {
+                result.add(EnumSupertypeForJavaDirect(this, memberResolutionContext))
+            } else if (isAnnotationType) {
+                result.add(SimpleClassifierType("java.lang.annotation.Annotation", memberResolutionContext))
+            } else if (isRecord) {
+                result.add(SimpleClassifierType("java.lang.Record", memberResolutionContext))
+            }
 
         tree.findChildByType(node, JavaSyntaxElementType.EXTENDS_LIST)?.let { extList ->
             tree.getChildrenByType(extList, JavaSyntaxElementType.JAVA_CODE_REFERENCE).forEach {
