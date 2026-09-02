aa59534f5b389b8fbf18450e7e1f7cfcda394805
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-09-02 11:59:40 +0300

    [FIR] Allow numeric class <-> builtins coercion in assignments
    Saves users from having to write, e.g.:
    
    ```
    val version = cValue<NSOperatingSystemVersion> {
        majorVersion = 12.convert<support.AppleSSizeT>()
        minorVersion = 0.convert<support.AppleSSizeT>()
        patchVersion = 0.convert<support.AppleSSizeT>()
    }
    ```
    
    Definitely used in `ktor`.
    
    It's also worth mentioning that we have additional logic in DFA that
    applies smart casts to local variables based on the initializer's type.
    The existence of a separate FIR node for numeric conversions implicitly
    protects us from a CCE (`class kotlin.Long cannot be cast to class
    kotlin.Int`) on code like `a <= 0`. If `a = 20` brings a smart cast to
    `Int`, then we'll resolve the call to `Int::compareTo`, leaving out
    the appropriate `convert()` call over the receiver.
    
    ^KT-87284

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
@@ -1664,7 +1664,9 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
                 withExpectedType(
                     variableAssignment.lValue.resolvedType.toFirResolvedTypeRef(),
                 ),
-            )
+            ).also {
+                it.replaceRValue(it.rValue.wrapIntoNumericClassConversionIfNeeded(it.lValue.resolvedType, session))
+            }
         }
 
         // for cases like
