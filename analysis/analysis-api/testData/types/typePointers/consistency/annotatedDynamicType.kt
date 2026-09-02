065cb191cf104ff18db7ddf3243f49281743143b
Oleg Makeev <oleg.makeev@jetbrains.com>
2026-09-02 18:38:38 +0200

    [Analysis API] Add type pointer tests for annotated types
    ^KT-79041

--- analysis/analysis-api/testData/types/typePointers/consistency/annotatedDynamicType.kt
+++ analysis/analysis-api/testData/types/typePointers/consistency/annotatedDynamicType.kt
@@ -1,7 +1,9 @@
+// TARGET_PLATFORM: JS
+
 @Target(AnnotationTarget.TYPE)
 annotation class Anno
 
 @Target(AnnotationTarget.TYPE)
 annotation class AnnoWithArgs(val x: String)
 
-fun test(value: <expr>@Anno @AnnoWithArgs("") List<@Anno @AnnoWithArgs("") String></expr>) {}
+fun test(value: <expr>@Anno @AnnoWithArgs("") dynamic</expr>) {}
---
+++ analysis/analysis-api/testData/types/typePointers/consistency/annotatedTypeInvalidAnnotation.kt
@@ -0,0 +1,1 @@
+fun <T> test(value: <expr>@T @Unresolved @AnotherOne("") List<@T @Unresolved @AnotherOne(1) String></expr>) {}
