7b28bf7c98b1c40d74b1e853fbf99e804295be95
Aleksei.Cherepanov <aleksei.cherepanov@jetbrains.com>
2026-09-03 11:33:44 +0200

    [Kotlin/JS] Pin Mocha version in EsmBundleKotlinJsTests tests
    Follow-up commit for e12a48e8
    
    ^KT-89033 Fixed

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/WebpackBundleKotlinJsTests.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/targets/js/testing/WebpackBundleKotlinJsTests.kt
@@ -203,21 +203,23 @@ constructor(
         }
 
         val output = outputBundleDir.get().asFile.resolve(TEST_HTML_FILE_NAME)
-        output.writeText(
-            html
-                .replace(MOCHA_CSS_URL, PINNED_MOCHA_CSS_URL)
-                .replace(MOCHA_SCRIPT_URL, PINNED_MOCHA_SCRIPT_URL)
-        )
+        output.writeText(pinMochaCdnUrls(html))
     }
 
     companion object {
         private const val TEST_HTML_FILE_NAME = "test.html"
-        private const val MOCHA_CSS_URL = "https://unpkg.com/mocha/mocha.css"
-        private const val MOCHA_SCRIPT_URL = "https://unpkg.com/mocha/mocha.js"
-        private const val PINNED_MOCHA_CSS_URL = "https://unpkg.com/mocha@11.8.0/mocha.css"
-        private const val PINNED_MOCHA_SCRIPT_URL = "https://unpkg.com/mocha@11.8.0/mocha.js"
     }
 }
+
+internal fun pinMochaCdnUrls(html: String): String =
+    html
+        .replace(MOCHA_CSS_URL, PINNED_MOCHA_CSS_URL)
+        .replace(MOCHA_SCRIPT_URL, PINNED_MOCHA_SCRIPT_URL)
+
+private const val MOCHA_CSS_URL = "https://unpkg.com/mocha/mocha.css"
+private const val MOCHA_SCRIPT_URL = "https://unpkg.com/mocha/mocha.js"
+private const val PINNED_MOCHA_CSS_URL = "https://unpkg.com/mocha@11.8.0/mocha.css"
+private const val PINNED_MOCHA_SCRIPT_URL = "https://unpkg.com/mocha@11.8.0/mocha.js"
 
 internal fun KotlinJsIrCompilation.locateOrRegisterBrowserTestBundleTask(
     configure: WebpackBundleKotlinJsTests.() -> Unit
