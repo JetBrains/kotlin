46704795fc30a4c6e3732168a202657de41ddf4c
Nikolai Kasyanov <136644+nikolaykasyanov@users.noreply.github.com>
2026-09-03 10:28:37 +0200

    Update libraries/stdlib/common/src/kotlin/JvmAnnotationsH.kt
    Co-authored-by: ilya-g <ilya.gorbunov@jetbrains.com>

--- libraries/stdlib/common/src/kotlin/JvmAnnotationsH.kt
+++ libraries/stdlib/common/src/kotlin/JvmAnnotationsH.kt
@@ -197,7 +197,7 @@ public expect annotation class Strictfp()
  * Note that for an extension function, the monitor of the facade class, where it gets compiled to a static method, is used.
  * Therefore, this annotation is recommended to be applied only to member functions and properties.
  *
- * If you need to annotate a common method as JVM-synchronized, add a new [OptionalExpectation] annotation in your common code:
+ * If you need to annotate a common method as JVM-synchronized, introduce a new annotation marked with [OptionalExpectation] in your common code:
  * ```kotlin
  * @OptionalExpectation
  * expect annotation class JvmSynchronized
