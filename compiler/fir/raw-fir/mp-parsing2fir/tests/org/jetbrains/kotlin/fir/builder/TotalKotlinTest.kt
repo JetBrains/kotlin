8c8d0f92dd24de3f02a4911ae1bbbff18de0a2f2
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-09-03 15:01:32 +0200

    Compiler: add more complex tests for mp-parsing2fir module
    #KT-86282 Fixed

---
+++ compiler/fir/raw-fir/mp-parsing2fir/tests/org/jetbrains/kotlin/fir/builder/TotalKotlinTest.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.fir.builder
+
+import com.intellij.psi.impl.DebugUtil
+import com.intellij.testFramework.TestDataPath
+import org.jetbrains.kotlin.*
+import org.jetbrains.kotlin.fir.renderer.FirRenderer
+import org.jetbrains.kotlin.fir.session.FirSessionFactoryHelper
+import org.jetbrains.kotlin.parsing.KotlinLightParser
+import org.jetbrains.kotlin.test.util.walkRepositoryKotlinFilesWithoutTestData
+import org.junit.jupiter.api.Test
+import kotlin.system.measureNanoTime
+
+@TestDataPath("/")
+class TotalKotlinTest : AbstractRawFirBuilderTestCase() {
+    private fun generateFirFromLightTree(
+        onlyLightTree: Boolean, converter: MultiplatformParsing2Fir,
+        text: CharSequence, sourceFile: KtSourceFile, linesMapping: KtSourceFileLinesMapping
+    ) {
+        if (onlyLightTree) {
+            val lightTree = KotlinLightParser.buildLightTree(text, sourceFile, errorListener = null)
+            DebugUtil.lightTreeToString(lightTree, false)
+        } else {
+            val firFile = converter.buildFirFile(text, sourceFile, linesMapping)
+            FirRenderer().renderElementAsString(firFile)
+        }
+    }
+
+    private fun totalKotlinLight(onlyLightTree: Boolean) {
+        val path = System.getProperty("user.dir")
+        var counter = 0
+        var time = 0L
+
+        @OptIn(ObsoleteTestInfrastructure::class)
+        val lightTreeConverter = MultiplatformParsing2Fir(
+            session = FirSessionFactoryHelper.createEmptySession(),
+            scopeProvider = StubFirScopeProvider,
+            diagnosticsReporter = null
+        )
+
+        if (onlyLightTree) println("LightTree generation") else println("Fir from LightTree converter")
+        println("BASE PATH: $path")
+        path.walkRepositoryKotlinFilesWithoutTestData {
+            val sourceFile = KtIoFileSourceFile(it)
+            val [code, linesMapping] = it.inputStream().reader(Charsets.UTF_8).use {
+                it.readSourceFileWithMapping()
+            }
+            time += measureNanoTime {
+                generateFirFromLightTree(onlyLightTree, lightTreeConverter, code, sourceFile, linesMapping)
+            }
+
+            counter++
+        }
+        println("SUCCESS!")
+        println("TIME PER FILE: ${(time / counter) * 1e-6} ms, COUNTER: $counter")
+
+    }
+
+    @Test
+    fun testTotalKotlinOnlyLightTree() {
+        totalKotlinLight(true)
+    }
+
+    @Test
+    fun testTotalKotlinFirFromLightTree() {
+        totalKotlinLight(false)
+    }
+}
