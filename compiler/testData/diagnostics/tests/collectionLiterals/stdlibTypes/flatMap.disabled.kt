e9b7f8035a88fae9c3f0f179291d7cc62daf0ab0
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-04 15:18:20 +0300

    [Tests] Reproduce known inference quirks related to collection literals

---
+++ compiler/testData/diagnostics/tests/collectionLiterals/stdlibTypes/flatMap.disabled.kt
@@ -0,0 +1,83 @@
+// LANGUAGE: +CollectionLiterals
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-86060
+// LANGUAGE_FEATURE_TOGGLED: EagerLambdaAnalysis
+
+fun testOnIterable() {
+    [1, 2, 3].flatMap {
+        [it]
+    }
+    [1, null, 3].flatMap {
+        if (it == null) return@flatMap []
+        [it]
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.collections.List<kotlin.Int>")!>[it]<!>
+        else []
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) [it]
+        else listOf()
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) return@flatMap []
+        listOf(it)
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) return@flatMap listOf()
+        [it]
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) [it]
+        else sequenceOf()
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) return@flatMap []
+        sequenceOf(it)
+    }
+    [1, 2, 3].flatMap {
+        if (it % 2 == 0) return@flatMap [it]
+        listOf()
+    }
+}
+
+fun testOnSequence() {
+    sequenceOf(1, 2, 3).flatMap {
+        [it]
+    }
+    sequenceOf(1, null, 3).flatMap {
+        if (it == null) return@flatMap []
+        [it]
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) <!DEBUG_INFO_EXPRESSION_TYPE("kotlin.sequences.Sequence<kotlin.Int>")!>[it]<!>
+        else []
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) [it]
+        else listOf()
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) return@flatMap []
+        listOf(it)
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) return@flatMap listOf()
+        [it]
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) [it]
+        else sequenceOf()
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) return@flatMap []
+        sequenceOf(it)
+    }
+    sequenceOf(1, 2, 3).flatMap {
+        if (it % 2 == 0) return@flatMap [it]
+        listOf()
+    }
+}
+
+/* GENERATED_FIR_TAGS: equalityExpression, functionDeclaration, ifExpression, integerLiteral, lambdaLiteral,
+multiplicativeExpression, nullableType, smartcast */
---
+++ compiler/testData/diagnostics/tests/collectionLiterals/stdlibTypes/plus.kt
@@ -0,0 +1,68 @@
+// LANGUAGE: +CollectionLiterals
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-86060
+
+fun testWithPlus() {
+    [1, 2, 3] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    setOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    sequenceOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    intArrayOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    arrayOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    [1, 2, 3] as Collection<Int> <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    [1, 2, 3] as Iterable<Int> <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+}
+
+fun testWithMinus() {
+    [1, 2, 3] <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [4, 5, 6]
+    setOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [4, 5, 6]
+    sequenceOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [4, 5, 6]
+    [1, 2, 3] as Collection<Int> <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [4, 5, 6]
+    [1, 2, 3] as Iterable<Int> <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [4, 5, 6]
+}
+
+class TestWithAssign(
+    val a: MutableList<Int>,
+    val b: MutableSet<Int>,
+    val c: MutableCollection<Int>,
+    val d: MutableIterable<Int>,
+) {
+    fun testPlus() {
+        a <!OVERLOAD_RESOLUTION_AMBIGUITY!>+=<!> [4, 5, 6]
+        b <!OVERLOAD_RESOLUTION_AMBIGUITY!>+=<!> [4, 5, 6]
+        c <!OVERLOAD_RESOLUTION_AMBIGUITY!>+=<!> [4, 5, 6]
+        <!VAL_REASSIGNMENT!>d<!> <!OVERLOAD_RESOLUTION_AMBIGUITY!>+=<!> [4, 5, 6]
+    }
+
+    fun testMinus() {
+        a <!OVERLOAD_RESOLUTION_AMBIGUITY!>-=<!> [4, 5, 6]
+        b <!OVERLOAD_RESOLUTION_AMBIGUITY!>-=<!> [4, 5, 6]
+        c <!OVERLOAD_RESOLUTION_AMBIGUITY!>-=<!> [4, 5, 6]
+        <!VAL_REASSIGNMENT!>d<!> <!OVERLOAD_RESOLUTION_AMBIGUITY!>-=<!> [4, 5, 6]
+    }
+}
+
+fun testWithIf(cond: Boolean) {
+    [1, 2, 3] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> if (cond) [4, 5, 6] else [7, 8, 9]
+    setOf(1, 2, 3) <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> if (cond) [4, 5, 6] else [7, 8, 9]
+}
+
+fun testNestedCollections(cond: Boolean) {
+    [[1, 2, 3]] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    [[1, 2, 3]] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [[4, 5, 6]]
+    [[1, 2, 3]] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> <!CANNOT_INFER_PARAMETER_TYPE!>[]<!>
+    [setOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [4, 5, 6]
+    [setOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [[4, 5, 6]]
+    [setOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> [setOf(4, 5, 6)]
+    [setOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> <!CANNOT_INFER_PARAMETER_TYPE!>[]<!>
+    [sequenceOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [4, 5, 6]
+    [sequenceOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [[4, 5, 6]]
+    [sequenceOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> [sequenceOf(4, 5, 6)]
+    [sequenceOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>-<!> <!CANNOT_INFER_PARAMETER_TYPE!>[]<!>
+
+    [setOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> if (cond) [] else [4, 5, 6]
+    [setOf(1, 2, 3)] <!OVERLOAD_RESOLUTION_AMBIGUITY!>+<!> if (cond) [[]] else [[4, 5, 6]]
+    [setOf(1, 2, 3)] + if (cond) <!CANNOT_INFER_PARAMETER_TYPE!>[]<!> else <!CANNOT_INFER_PARAMETER_TYPE!>[]<!>
+}
+
+/* GENERATED_FIR_TAGS: additiveExpression, asExpression, assignment, classDeclaration, functionDeclaration, ifExpression,
+integerLiteral, primaryConstructor, propertyDeclaration */
