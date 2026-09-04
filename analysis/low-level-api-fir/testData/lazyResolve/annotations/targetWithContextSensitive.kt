3e3510c8581a02b6b7765cc11447d9fc32ba0212
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-04 23:11:09 +0300

    fixup! [Tests] Add test with lazy resolve and compiler-required CSR argument

--- analysis/low-level-api-fir/testData/lazyResolve/annotations/targetWithContextSensitive.kt
+++ analysis/low-level-api-fir/testData/lazyResolve/annotations/targetWithContextSensitive.kt
@@ -1,8 +1,4 @@
 // LANGUAGE: +ContextSensitiveResolutionUsingExpectedType
-class UseTarget {
-    @Anno
-    val pro<caret>p = 42
-}
 
 @Target(FIELD)
-annotation class Anno
+annotation class An<caret>no
