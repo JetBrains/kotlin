e8b3c54ebf0c732ca9ef295fc33fd9234d948762
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-04 13:51:16 +0200

    [Raw FIR] Set status bit for rich error classes and objects
    #KT-89097

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
@@ -2003,6 +2003,7 @@ open class PsiRawFirBuilder(
                         isValue = classOrObject.hasModifier(VALUE_KEYWORD)
                         isFun = classOrObject.hasModifier(FUN_KEYWORD)
                         isExternal = classOrObject.hasModifier(EXTERNAL_KEYWORD)
+                        isRichError = classOrObject.hasModifier(ERROR_KEYWORD)
                     }
                     val firTypeParameters = classOrObject.convertTypeParameters(classSymbol)
 
