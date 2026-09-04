c74a38b01895ca78da94087d78b434587dd5772d
anzhela.sukhanova <anzhela.sukhanova@jetbrains.com>
2026-09-04 16:50:54 +0300

    [Tests] KQA-3368: IntrinsicConstEvaluation cases with operator-call syntax
    Codegen box cases with operator-call syntax and char constructor
    
    ^KQA-3368

--- compiler/testData/codegen/box/evaluate/intrinsicConst/charConstructor.kt
+++ compiler/testData/codegen/box/evaluate/intrinsicConst/charConstructor.kt
@@ -2,10 +2,14 @@
 // WITH_STDLIB
 fun <T> T.id() = this
 
+const val x = 66
+
 const val char1 = Char(65)
+const val char2 = Char(x)
 
 fun box(): String {
     if (char1.id() != 'A') return "Fail 1"
+    if (char2.id() != 'B') return "Fail 2"
 
     return "OK"
 }
