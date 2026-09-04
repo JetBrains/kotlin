246daa13fcaba8e84e925cca4ba35a9f9fcdc730
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-09-04 16:30:50 +0300

    [Test] Unmute `AbstractNativeCodegenBoxCoreTest`
    ^KT-89132 Fixed

--- native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/AbstractNativeCodegenBoxCoreTest.kt
+++ native/native.tests/klib-ir-inliner/testFixtures/org/jetbrains/kotlin/konan/test/blackbox/AbstractNativeCodegenBoxCoreTest.kt
@@ -31,12 +31,9 @@ import org.jetbrains.kotlin.test.services.configuration.CommonEnvironmentConfigu
 import org.jetbrains.kotlin.test.services.configuration.NativeFirstStageEnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.configuration.NativeSecondStageEnvironmentConfigurator
 import org.jetbrains.kotlin.utils.bind
-import org.junit.jupiter.api.Assumptions
 
 abstract class AbstractNativeCodegenBoxCoreTest : AbstractTwoStageNativeCoreTest() {
     override fun configure(builder: TwoStageTestConfigurationBuilder): Unit = with(builder) {
-        Assumptions.abort<Nothing>("temporarily muted because of OOM") // @Disabled annotation does not work on such tests somewhy!!!
-
         super.configure(builder)
         commonConfiguration {
             defaultDirectives {
