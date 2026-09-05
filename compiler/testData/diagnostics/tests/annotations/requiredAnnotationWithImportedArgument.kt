6a21599600685ee70da7262d7107741598b28855
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-05 07:06:28 +0000

    [Tests] Fix compilation after disallowing non-literals in `JvmName`

--- compiler/testData/diagnostics/tests/annotations/requiredAnnotationWithImportedArgument.kt
+++
@@ -1,9 +0,0 @@
-// RUN_PIPELINE_TILL: BACKEND
-// ISSUE: KT-75844
-
-import kotlin.annotation.AnnotationTarget.ANNOTATION_CLASS
-
-@Target(ANNOTATION_CLASS)
-annotation class MyAnnotation
-
-/* GENERATED_FIR_TAGS: annotationDeclaration */
