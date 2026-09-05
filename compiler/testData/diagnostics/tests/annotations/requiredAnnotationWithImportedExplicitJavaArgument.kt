2fb4c11fa297d0cea0a80486800321f1a192fb87
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-05 07:06:28 +0000

    [Tests] Fix compilation after disallowing non-literals in `JvmName`

--- compiler/testData/diagnostics/tests/annotations/requiredAnnotationWithImportedExplicitJavaArgument.kt
+++
@@ -1,9 +0,0 @@
-// RUN_PIPELINE_TILL: BACKEND
-// ISSUE: KT-75844
-
-import java.lang.annotation.ElementType
-
-<!ANNOTATION_TARGETS_ONLY_IN_JAVA, DEPRECATED_JAVA_ANNOTATION!>@java.lang.annotation.Target(ElementType.ANNOTATION_TYPE)<!>
-annotation class MyAnnotation
-
-/* GENERATED_FIR_TAGS: annotationDeclaration, javaProperty */
