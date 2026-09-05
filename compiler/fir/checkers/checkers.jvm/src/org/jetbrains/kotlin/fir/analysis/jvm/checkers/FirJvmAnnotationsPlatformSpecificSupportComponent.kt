4435d9c81e91ec3c2bd132b451d69c6a436c9528
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-05 07:06:07 +0000

    [FIR] Introduce small DSL to declare compiler-required annotation params
    Included information:
     - Name of the parameter;
     - Its annotation;
     - Its position in the parameter list (or if it is `vararg`);
     - Its expected class if it is enum.
    
    Currently, only enums and strings can be value compiler required
    parameters.
    
    Note that for `Deprecated` only its `level` parameter becomes
    compiler-required. This is important: in the future commits, we will
    report errors (or warnings) for non-literal arguments reported for
    "string" compiler-required parameters (since they can only be evaluated
    on `ANNOTATION_PARAMETERS`, which is too late).
    
    ^KT-87072
    ^KT-86556

--- compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/FirJvmAnnotationsPlatformSpecificSupportComponent.kt
+++ compiler/fir/checkers/checkers.jvm/src/org/jetbrains/kotlin/fir/analysis/jvm/checkers/FirJvmAnnotationsPlatformSpecificSupportComponent.kt
@@ -5,26 +5,39 @@
 
 package org.jetbrains.kotlin.fir.analysis.jvm.checkers
 
+import org.jetbrains.kotlin.builtins.StandardNames
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.declarations.AnnotationsPosition
 import org.jetbrains.kotlin.fir.declarations.FirAnnotationsPlatformSpecificSupportComponent
+import org.jetbrains.kotlin.fir.declarations.FirCompilerRequiredParameterDescription
+import org.jetbrains.kotlin.fir.declarations.FirCraParameterKind
 import org.jetbrains.kotlin.fir.declarations.FirProperty
 import org.jetbrains.kotlin.fir.declarations.toAnnotationClassIdSafe
 import org.jetbrains.kotlin.fir.expressions.FirAnnotation
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.JvmStandardClassIds
+import org.jetbrains.kotlin.types.ConstantValueKind
 
 object FirJvmAnnotationsPlatformSpecificSupportComponent : FirAnnotationsPlatformSpecificSupportComponent() {
-    override val requiredAnnotationsWithArguments: Set<ClassId> = setOf(
-        JvmStandardClassIds.Annotations.Java.Target,
-        JvmStandardClassIds.Annotations.JvmName,
-    )
+    override val requiredAnnotationsWithArguments: CompilerRequiredParametersMap = buildMap {
+        this[JvmStandardClassIds.Annotations.Java.Target] = [
+            FirCompilerRequiredParameterDescription(
+                name = StandardNames.DEFAULT_VALUE_PARAMETER,
+                kind = FirCraParameterKind.EnumParameter(JvmStandardClassIds.Annotations.Java.ElementType, isVararg = true),
+                position = null,
+            )
+        ]
 
-    override val requiredArguments: Set<ClassId> = setOf(
-        JvmStandardClassIds.Annotations.Java.ElementType,
-    )
+        this[JvmStandardClassIds.Annotations.JvmName] = [
+            FirCompilerRequiredParameterDescription(
+                name = StandardNames.NAME,
+                kind = FirCraParameterKind.LiteralParameter(ConstantValueKind.String),
+                position = 0,
+            )
+        ]
+    }
 
-    override val requiredAnnotations: Set<ClassId> = requiredAnnotationsWithArguments + setOf(
+    override val requiredAnnotations: Set<ClassId> = requiredAnnotationsWithArguments.keys + setOf(
         JvmStandardClassIds.Annotations.Java.Deprecated,
         JvmStandardClassIds.Annotations.JvmRecord,
         JvmStandardClassIds.Annotations.JvmInline,
