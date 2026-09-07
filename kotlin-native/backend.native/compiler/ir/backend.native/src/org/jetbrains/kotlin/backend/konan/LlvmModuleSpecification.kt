50ce85e6e10305dc4694af132aa96d1c5d75276a
Miłosz Piekutowski <milosz.piekutowski@jetbrains.com>
2026-09-07 10:29:29 +0200

    [Native] Drop unused `LlvmModuleSpecification.containsPackageFragment`
    KT-87954

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/LlvmModuleSpecification.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/LlvmModuleSpecification.kt
@@ -8,7 +8,6 @@ package org.jetbrains.kotlin.backend.konan
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.declarations.IrPackageFragment
 import org.jetbrains.kotlin.library.KotlinLibrary
 
 /**
@@ -21,6 +20,5 @@ interface LlvmModuleSpecification {
     fun containsLibrary(library: KotlinLibrary): Boolean
     fun containsModule(module: ModuleDescriptor): Boolean
     fun containsModule(module: IrModuleFragment): Boolean
-    fun containsPackageFragment(packageFragment: IrPackageFragment): Boolean
     fun containsDeclaration(declaration: IrDeclaration): Boolean
 }
