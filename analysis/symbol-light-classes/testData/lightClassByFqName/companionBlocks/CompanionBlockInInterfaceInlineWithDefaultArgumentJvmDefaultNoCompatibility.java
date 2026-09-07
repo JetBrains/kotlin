e0afed6794c8c3b4a2c00b27b079a99de1e3129f
Alexey Belkov <alexey.belkov@jetbrains.com>
2026-09-07 14:47:46 +0400

    [SLC] Expose companion block members in interfaces
    Use the combined declared member scope to expose interface companion
    functions and accessors as static Java methods. The non-static scope
    previously omitted them, causing false unresolved references in Java.
    
    Exclude companion members when collecting DefaultImpls methods, matching
    the JVM behavior introduced by cc5c50823c0c.
    
    ^KT-89204 Fixed

--- analysis/symbol-light-classes/testData/lightClassByFqName/companionBlocks/CompanionBlockInInterfaceInlineWithDefaultArgumentJvmDefaultNoCompatibility.java
+++ analysis/symbol-light-classes/testData/lightClassByFqName/companionBlocks/CompanionBlockInInterfaceInlineWithDefaultArgumentJvmDefaultNoCompatibility.java
@@ -1,2 +1,10 @@
 public abstract interface I /* I*/ {
+  @org.jetbrains.annotations.NotNull()
+  public static @org.jetbrains.annotations.NotNull() java.lang.String f1(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String);//  f1(@org.jetbrains.annotations.NotNull() java.lang.String)
+
+  @org.jetbrains.annotations.NotNull()
+  public static @org.jetbrains.annotations.NotNull() java.lang.String f2();//  f2()
+
+  @org.jetbrains.annotations.NotNull()
+  public static @org.jetbrains.annotations.NotNull() java.lang.String f3(@org.jetbrains.annotations.NotNull() @org.jetbrains.annotations.NotNull() java.lang.String);//  f3(@org.jetbrains.annotations.NotNull() java.lang.String)
 }
