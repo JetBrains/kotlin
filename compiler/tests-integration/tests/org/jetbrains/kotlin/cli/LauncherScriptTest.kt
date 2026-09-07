83f18123282d036d2dfc44c8c339d4a1907fbda4
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-09-07 10:39:45 +0300

    [Tests] Run `LauncherScriptTest` tests using JDK 17 instead of JDK 8

--- compiler/tests-integration/tests/org/jetbrains/kotlin/cli/LauncherScriptTest.kt
+++ compiler/tests-integration/tests/org/jetbrains/kotlin/cli/LauncherScriptTest.kt
@@ -32,7 +32,6 @@ import com.intellij.openapi.util.SystemInfo
 import org.junit.jupiter.api.Assumptions.assumeFalse
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.nio.file.Files
@@ -45,7 +44,7 @@ class LauncherScriptTest : TestCaseWithTmpdir() {
         checkStderr: (String) -> Unit,
         expectedExitCode: Int,
         workDirectory: File? = null,
-        environment: Map<String, String> = mapOf("JAVA_HOME" to KtTestUtil.getJdk8Home().absolutePath),
+        environment: Map<String, String> = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath),
         launcherFile: File? = null,
     ) {
         CliProcessUtils.runProcess(
@@ -69,7 +68,7 @@ class LauncherScriptTest : TestCaseWithTmpdir() {
         expectedStderr: String = "",
         expectedExitCode: Int = 0,
         workDirectory: File? = null,
-        environment: Map<String, String> = mapOf("JAVA_HOME" to KtTestUtil.getJdk8Home().absolutePath),
+        environment: Map<String, String> = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath),
         launcherFile: File? = null,
     ) {
         CliProcessUtils.runProcess(
@@ -148,7 +147,7 @@ class LauncherScriptTest : TestCaseWithTmpdir() {
             tmpdir.path,
             K2JSCompilerArguments::moduleName.cliArgument,
             "out",
-            environment = mapOf("JAVA_HOME" to KtTestUtil.getJdk8Home().absolutePath)
+            environment = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath)
         )
     }
 
@@ -162,7 +161,7 @@ class LauncherScriptTest : TestCaseWithTmpdir() {
             KotlinWasmCompilerArguments::nopack.cliArgument,
             KotlinWasmCompilerArguments::outputDir.cliArgument(tmpdir.path),
             KotlinWasmCompilerArguments::moduleName.cliArgument("out"),
-            environment = mapOf("JAVA_HOME" to KtTestUtil.getJdk8Home().absolutePath)
+            environment = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath)
         )
     }
 
@@ -370,30 +369,30 @@ class LauncherScriptTest : TestCaseWithTmpdir() {
 
     @Test
     fun testKotlinUseJdkModuleFromMainClass() {
-        val jdk11 = mapOf("JAVA_HOME" to KtTestUtil.getJdk11Home().absolutePath)
+        val jdk17 = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath)
         runProcess(
             "kotlinc", "$testDataDirectory/jdkModuleUsage.kt", K2JVMCompilerArguments::destination.cliArgument, tmpdir.path,
-            environment = jdk11,
+            environment = jdk17,
         )
         runProcess(
             "kotlinr", K2JVMCompilerArguments::classpath.cliArgument, tmpdir.path, "test.JdkModuleUsageKt",
             expectedStdout = "interface java.sql.Driver\n",
-            environment = jdk11,
+            environment = jdk17,
         )
     }
 
     @Test
     fun testKotlinUseJdkModuleFromJar() {
-        val jdk11 = mapOf("JAVA_HOME" to KtTestUtil.getJdk11Home().absolutePath)
+        val jdk17 = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath)
         val output = tmpdir.resolve("out.jar")
         runProcess(
             "kotlinc", "$testDataDirectory/jdkModuleUsage.kt", K2JVMCompilerArguments::destination.cliArgument, output.path,
-            environment = jdk11,
+            environment = jdk17,
         )
         runProcess(
             "kotlinr", output.path,
             expectedStdout = "interface java.sql.Driver\n",
-            environment = jdk11,
+            environment = jdk17,
         )
     }
 
@@ -419,7 +418,7 @@ class LauncherScriptTest : TestCaseWithTmpdir() {
         val testKt = tmpdir.resolve("test.kt").apply {
             writeText("fun main() {}")
         }
-        val jdk11 = mapOf("JAVA_HOME" to KtTestUtil.getJdk11Home().absolutePath)
+        val jdk11 = mapOf("JAVA_HOME" to KtTestUtil.getJdk17Home().absolutePath)
         runProcess(
             "kotlinc", moduleInfo.absolutePath, testKt.absolutePath, K2JVMCompilerArguments::destination.cliArgument, tmpdir.path,
             environment = jdk11,
