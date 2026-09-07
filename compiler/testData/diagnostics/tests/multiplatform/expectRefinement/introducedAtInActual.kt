2b0f0236f32fffb5c4e0618c67405b9ab342f33a
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-09-07 14:10:51 +0300

    [Test] Reproduce KT-89196

---
+++ compiler/testData/diagnostics/tests/multiplatform/expectRefinement/introducedAtInActual.kt
@@ -0,0 +1,24 @@
+// LANGUAGE: +ExpectRefinement
+// WITH_STDLIB
+// RUN_PIPELINE_TILL: FRONTEND
+// ISSUE: KT-89196
+
+// MODULE: common
+expect class Foo {
+    fun foo(param: Int = 1)
+}
+
+// MODULE: intermediate1()()(common)
+@OptIn(ExperimentalMultiplatform::class)
+@kotlin.experimental.ExpectRefinement
+expect class Foo {
+    fun foo(param: Int)
+}
+
+// MODULE: main()()(intermediate1)
+@OptIn(ExperimentalVersionOverloading::class)
+actual class Foo {
+    actual fun foo(<!INVALID_VERSIONING_ON_NON_OPTIONAL!>@IntroducedAt("1")<!> param: Int) {}
+}
+
+/* GENERATED_FIR_TAGS: actual, classDeclaration, classReference, expect, functionDeclaration */
