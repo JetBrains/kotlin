e15e97c663ff24172d268063535190d9a37de365
igor.yakovlev <igor.yakovlev@jetbrains.com>
2026-09-08 12:41:28 +0200

    fixup! [Native/Wasm] Restore C implementation for FP parser and stringifier on Native

--- libraries/stdlib/wasm/src/kotlin/internal/dtoa/cbigint.kt
+++ libraries/stdlib/wasm/src/kotlin/internal/dtoa/cbigint.kt
@@ -173,12 +173,12 @@ private fun simpleMultiplyHighPrecision(arg1: ULongArray, length: Int, arg2: ULo
     return highU32FromVar(product)
 }
 
-internal value class ULongArrayView(val array: ULongArray)
+private value class ULongArrayView(val array: ULongArray)
 
-internal operator fun ULongArrayView.get(idx32: Int): UInt =
+private operator fun ULongArrayView.get(idx32: Int): UInt =
     if (idx32 % 2 == 0) lowU32FromPtr(array, idx32 / 2) else highU32FromPtr(array, idx32 / 2)
 
-internal operator fun ULongArrayView.set(idx32: Int, value: UInt) =
+private operator fun ULongArrayView.set(idx32: Int, value: UInt) =
     if (idx32 % 2 == 0) setLowU32Ptr(array, idx32 / 2, value) else setHighU32Ptr(array, idx32 / 2, value)
 
 private fun simpleMultiplyAddHighPrecision(arg1: ULongArray, length: Int, arg2: ULong, result: ULongArrayView, resultOffset: Int) {
