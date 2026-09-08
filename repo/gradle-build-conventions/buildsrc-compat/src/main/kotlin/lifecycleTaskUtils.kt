f48b2887e3b4daa1f904c29bb386eb393bc59c7d
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-09-08 07:03:03 +0000

    [Build] Introduce checked Quality Gates
    TestLifecycleTasks defined in the root build.gradle.kts
    can specify a quality gate.
    
    The quality gates are:
    - None: The task should *not* be part of any quality gate
    - Nightly: The task should be part of the nightly quality gate
    - Master: The task should be part of the master quality gate
         Which is required for merging (Also called 'aggregate)'
    
    A special gate 'Undefined' can be set, which is also the default
    convention, to allow phasing in and out of tasks (or changing
    their quality gate)
    
    TeamCity:
    Providing a given quality gate is **checked** on TeamCity:
    The build configuration on TeamCity will pass meta information
    about the quality gates to the 'CodebaseTests' which is then
    able to verify if the definitions within kotlin.git are in sync
    with the TeamCity DSL.
    
    If e.g. a task requires to participate within the master quality gate,
    but is not present there, then this test will fail, reporting
    that the build configuration is not in sync.
    
    In scope of: KTI-3599

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/lifecycleTaskUtils.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/lifecycleTaskUtils.kt
@@ -3,9 +3,12 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
+import TestLifecycleTask.QualityGate
 import org.gradle.api.Action
 import org.gradle.api.DefaultTask
 import org.gradle.api.Task
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.TaskProvider
 
@@ -21,15 +24,58 @@ fun Task.dependsOnAll(task: String, projects: List<String>) {
 /**
  * Registers a task that does not execute any tests by itself, but depends on other test tasks.
  * IntelliJ IDEA will recognize it as a test task and show the test execution UI.
+ *
+ * @param qualityGate The expected [QualityGate] of this task.
+ * e.g. Use [QualityGate.Master] if the test task is expected to execute before merging to master
+ * Use [QualityGate.Nightly] if the test task is expected to only be executed nightly
+ * Use [QualityGate.None] if the test task is not expected to participate in any quality gate (yet)
+ * Use [QualityGate.Undefined] to transition a task in or out of a given quality gate (to allow the TeamCity configuration to catch up)
  */
-fun TaskContainer.testLifecycleTask(name: String, action: Action<Task>): TaskProvider<out Task> {
+fun TaskContainer.testLifecycleTask(
+    name: String,
+    qualityGate: QualityGate,
+    action: Action<TestLifecycleTask>,
+): TaskProvider<TestLifecycleTask> {
     return register(name, TestLifecycleTask::class.java) {
         extensions.extraProperties["idea.internal.test"] = "true"
+        this.qualityGate.set(qualityGate)
         action.execute(this)
     }
 }
 
-internal abstract class TestLifecycleTask : DefaultTask() {
+abstract class TestLifecycleTask : DefaultTask() {
+    enum class QualityGate {
+        /**
+         * The given task is allowed to appear in any Quality Gate (or none).
+         * This is useful when e.g. introducing a new task, allowing the TeamCity configuration
+         * to pick up the change gradually. When a new task is introduced with the [QualityGate.Undefined],
+         * and the TeamCity configuration 'caught up', then a second commit can promote the
+         * given task to the correct [QualityGate]
+         */
+        Undefined,
+
+        /**
+         * This task is not expected to participate in any defined [QualityGate] (yet).
+         */
+        None,
+
+        /**
+         * The task is only checked during nightly builds.
+         * The task is not executed before merging commits to master.
+         */
+        Nightly,
+
+        /**
+         * This task is required to be part of the 'master' quality gate.
+         * Commits, merging to master, will have this task in the safe-merge aggregate.
+         */
+        Master
+    }
+
+    @get:Internal
+    val qualityGate: Property<QualityGate> = project.objects.property(QualityGate::class.java)
+        .convention(QualityGate.Undefined)
+
     init {
         group = "verification"
         description = "Lifecycle Test Task"
