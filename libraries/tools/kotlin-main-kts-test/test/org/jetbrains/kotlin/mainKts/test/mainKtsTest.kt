5acec47d93759687b8f3d5090ff29aab9ab1b643
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2026-09-08 18:21:52 +0200

    Scripting: store collected dependencies recursively
    Previously, the collected dependencies were stored only for the root
    script, which resulted in problems with executing the script with
    imports and dependencies from the cache.
    
    ^KT-89247 fixed

--- libraries/tools/kotlin-main-kts-test/test/org/jetbrains/kotlin/mainKts/test/mainKtsTest.kt
+++ libraries/tools/kotlin-main-kts-test/test/org/jetbrains/kotlin/mainKts/test/mainKtsTest.kt
@@ -18,12 +18,14 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.PrintStream
 import java.util.*
+import kotlin.io.path.createTempDirectory
 import kotlin.script.experimental.api.*
 import kotlin.script.experimental.host.toScriptSource
 import kotlin.script.experimental.jvm.baseClassLoader
 import kotlin.script.experimental.jvm.jvm
 import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
 import kotlin.script.experimental.jvmhost.createJvmScriptDefinitionFromTemplate
+import kotlin.test.assertEquals
 
 internal const val SCRIPT_BASE_COMPILER_ARGUMENTS_PROPERTY = "kotlin.script.base.compiler.arguments"
 internal val isRunningTestOnK2 = System.getProperty(SCRIPT_BASE_COMPILER_ARGUMENTS_PROPERTY)?.contains("-language-version 1.9") != true
@@ -187,6 +189,26 @@ class MainKtsTest {
         // TODO: the second error is due to the late cycle detection, see TODO in makeCompiledScript$makeOtherScripts
         // TODO: third error is due to the early IR backend error, consider processing it in makeCompiledScript$makeOtherScripts
         assertFailedAny("Unable to handle recursive script dependencies", "is already bound", "Duplicate JVM class name", res = res)
+    }
+
+    @Test
+    fun testKt89247() {
+        val cacheDir = createTempDirectory("main.kts.test.cache").toFile()
+        try {
+            val script = File("$TEST_DATA_ROOT/kt89247/main.main.kts")
+            // the second run is served from the compilation cache, and the dependencies of the imported script should
+            // be restored from the cached jar as well
+            repeat(2) { run ->
+                var res: ResultWithDiagnostics<EvaluationResult>? = null
+                val out = captureOut {
+                    res = evalFile(script, cacheDir = cacheDir)
+                }.lines()
+                assertSucceeded(res!!)
+                assertEquals(listOf("Ok"), out, "run $run, script result: ${res.valueOrNull()?.returnValue}")
+            }
+        } finally {
+            cacheDir.deleteRecursively()
+        }
     }
 
     @Test
