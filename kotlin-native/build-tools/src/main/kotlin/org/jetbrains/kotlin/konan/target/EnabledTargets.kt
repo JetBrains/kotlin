0638aa94953d8b0d6f23a1a18b047fb19e81cdce
Artem Olkov <artem.olkov@jetbrains.com>
2026-09-08 15:14:08 +0200

    [Native] remove macos x64 from crossdist ^KT-84955

--- kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/konan/target/EnabledTargets.kt
+++ kotlin-native/build-tools/src/main/kotlin/org/jetbrains/kotlin/konan/target/EnabledTargets.kt
@@ -5,12 +5,12 @@
 
 package org.jetbrains.kotlin.konan.target
 
-// Targets already removed from the in-tree compiler but still present in the bootstrap.
+// Targets no longer supported by the in-tree compiler but still present in the bootstrap.
 // Matched by name so this keeps compiling after the bootstrap drops the KonanTarget constants.
 // todo: KT-78078
-private val removedTargetNames = setOf("watchos_arm32")
+internal val unsupportedTargetNames = setOf("watchos_arm32", "macos_x64")
 
 fun enabledTargets(platformManager: PlatformManager) = platformManager.enabled.filterNot {
-    it.name in removedTargetNames
+    it.name in unsupportedTargetNames
             || (it in KonanTarget.deprecatedTargets && it !in KonanTarget.toleratedDeprecatedTargets)
 }
