8e4ab6e9048a41ceef2172d0fc300ed49130bbb4
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-09 14:46:55 +0200

    [FIR] Error safe call operator resolution
    #KT-89098

--- compiler/testData/diagnostics/tests/richErrorsDisabled.kt
+++ compiler/testData/diagnostics/tests/richErrorsDisabled.kt
@@ -5,7 +5,7 @@
 <!UNSUPPORTED_FEATURE, WRONG_MODIFIER_TARGET!>error<!> object Bar
 
 fun foo(x: <!UNSUPPORTED_FEATURE!>String | Foo<!>) {
-    <!UNNECESSARY_SAFE_CALL!>x<!UNSUPPORTED_FEATURE!>|.<!><!UNRESOLVED_REFERENCE!>length<!><!>
+    <!UNNECESSARY_SAFE_CALL!>x<!UNSUPPORTED_FEATURE!>|.<!>length<!>
 }
 
 /* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, nullableType, objectDeclaration, safeCall */
