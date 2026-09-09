caed1355c20b9311e1c2f0dd94b5bde3cc06b038
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-09 08:04:19 +0000

    [Tests] Reproduce KT-89259
    The issue was found in a user project and will be handled in later merge
    requests.

---
+++ compiler/testData/diagnostics/tests/collectionLiterals/missingDependencyClassInArrayOfInAnnotation.kt
@@ -0,0 +1,27 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// LANGUAGE: -CollectionLiterals
+// LANGUAGE_FEATURE_TOGGLED: CollectionLiteralsBasedAnnotationResolution
+// WITH_STDLIB
+// ISSUE: KT-89259
+
+// MODULE: m1
+
+open class Missing
+
+// MODULE: m2(m1)
+
+annotation class WithMissingInParams(val param: Array<kotlin.reflect.KClass<out Missing>>)
+
+// MODULE: m3(m2)
+
+@WithMissingInParams(<!MISSING_DEPENDENCY_CLASS!>[]<!>)
+class Annotated
+
+@WithMissingInParams(<!MISSING_DEPENDENCY_CLASS!>arrayOf<!>())
+class Annotated2
+
+fun forReference() {
+    <!MISSING_DEPENDENCY_CLASS!>WithMissingInParams<!>(<!MISSING_DEPENDENCY_CLASS!>arrayOf<!>())
+}
+
+/* GENERATED_FIR_TAGS: annotationDeclaration, classDeclaration, outProjection, primaryConstructor, propertyDeclaration */
