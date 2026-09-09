70ab33b8d6bd44cb914e7b5a355bb3ebf0c4bafa
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2026-09-09 15:56:39 +0300

    [IR] Respect diagnostic suppressions for backend diagnostics
    ^KT-87442

--- compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/KtDiagnosticReporterWithImplicitIrBasedContext.kt
+++ compiler/ir/ir.tree/src/org/jetbrains/kotlin/ir/KtDiagnosticReporterWithImplicitIrBasedContext.kt
@@ -182,33 +182,17 @@ internal class IrBasedSuppressCache : AbstractKotlinSuppressCache<IrElement>() {
             }
         }
 
-        private fun collectSuppressAnnotationKeys(element: IrElement): Boolean =
-            (element as? IrAnnotationContainer)?.annotations?.filter {
-                it.isAnnotationWithEqualFqName(SUPPRESS)
-            }?.flatMap {
-                buildList {
-                    fun addIfStringConst(irConst: IrConst) {
-                        if (irConst.kind == IrConstKind.String) {
-                            add((irConst.value as String).lowercase())
-                        }
-                    }
-
-                    for (arg in it.argumentMapping.values) {
-                        when (arg) {
-                            is IrConst -> addIfStringConst(arg)
-                            is IrConstantArray -> arg.elements.filterIsInstance<IrConstantPrimitive>().forEach {
-                                addIfStringConst(it.value)
-                            }
-                            // TODO: consider leaving only this branch
-                            is IrVararg -> arg.elements.filterIsInstance<IrConst>().forEach {
-                                addIfStringConst(it)
-                            }
-                        }
-                    }
+        private fun collectSuppressAnnotationKeys(element: IrElement): Boolean {
+            val names = buildSet {
+                addAll(element.sourceSuppressedDiagnosticNames.orEmpty())
+                (element as? IrAnnotationContainer)?.annotations?.forEach { annotation ->
+                    addAll(annotation.suppressedDiagnosticNames())
                 }
-            }?.takeIf { it.isNotEmpty() }?.also {
-                annotationKeys[element] = it.toSet()
-            } != null
+            }
+            return names.isNotEmpty().also {
+                if (it) annotationKeys[element] = names
+            }
+        }
     }
 
     override fun getClosestAnnotatedAncestorElement(element: IrElement, rootElement: IrElement, excludeSelf: Boolean): IrElement? {
@@ -220,3 +204,26 @@ internal class IrBasedSuppressCache : AbstractKotlinSuppressCache<IrElement>() {
 }
 
 private val SUPPRESS = FqName("kotlin.Suppress")
+
+fun IrAnnotation.suppressedDiagnosticNames(): Set<String> {
+    if (!isAnnotationWithEqualFqName(SUPPRESS)) return emptySet()
+
+    return buildSet {
+        fun addIfStringConst(irConst: IrConst) {
+            if (irConst.kind == IrConstKind.String) {
+                add((irConst.value as String).lowercase())
+            }
+        }
+
+        for (arg in argumentMapping.values) {
+            when (arg) {
+                is IrConst -> addIfStringConst(arg)
+                is IrConstantArray -> arg.elements.filterIsInstance<IrConstantPrimitive>().forEach {
+                    addIfStringConst(it.value)
+                }
+                // TODO: consider leaving only this branch
+                is IrVararg -> arg.elements.filterIsInstance<IrConst>().forEach(::addIfStringConst)
+            }
+        }
+    }
+}
