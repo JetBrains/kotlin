475382273aaa761f8a30f520c1f98c1634ae09e7
Aleksandr Shefer <aleksandr.shefer@jetbrains.com>
2026-09-11 13:54:12 +0200

    fixup! [Wasm] place back @DoNotInlineOnFirstStage on suspendCoroutineUninterceptedOrReturn

--- libraries/stdlib/wasm/internal/kotlin/wasm/internal/Coroutines.kt
+++ libraries/stdlib/wasm/internal/kotlin/wasm/internal/Coroutines.kt
@@ -30,6 +30,7 @@ internal suspend inline fun getCoroutineContext(): CoroutineContext =
     getCoroutineContextImpl()
 
 @PublishedApi
+@UsedFromCompilerGeneratedCode
 internal suspend fun getCoroutineContextImpl(): CoroutineContext =
     getContinuation<Any?>().context
 
