f2febf0a4a1aba278023859125a583bae53d7d5b
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2026-09-12 14:17:32 +0200

    ~~ disable build cache for test tasks and require all test domains
    ^test: *

--- repo/gradle-build-conventions/project-tests-convention/src/main/kotlin/configureTestCacheDisabling.kt
+++ repo/gradle-build-conventions/project-tests-convention/src/main/kotlin/configureTestCacheDisabling.kt
@@ -12,14 +12,15 @@ import kotlin.text.toBoolean
 
 internal fun Project.configureTestCacheDisabling() {
     tasks.withType<AbstractTestTask>().configureEach {
-        val rootDir = project.rootDir
-        val testCacheDisabled = providers.gradleProperty("kotlin.build.cache.tests.disabled").orElse("false").get().toBoolean()
-        // `kotlin.build.cache.tests.disabled` property is used for master builds to always run the tests
-        // We don't atually disable the tests, just upToDateWhen, so we still push to the BuildCache
-        outputs.upToDateWhen { !testCacheDisabled }
-
-        if (this is Test || this is ProcessForkOptions) {
-            outputs.doNotCacheIf("Caching tests is disabled because `workingDir` is set to `rootDir`") { workingDir == rootDir }
-        }
+        outputs.doNotCacheIf("Build cache for test tasks is disabled") { true }
+//        val rootDir = project.rootDir
+//        val testCacheDisabled = providers.gradleProperty("kotlin.build.cache.tests.disabled").orElse("false").get().toBoolean()
+//        // `kotlin.build.cache.tests.disabled` property is used for master builds to always run the tests
+//        // We don't atually disable the tests, just upToDateWhen, so we still push to the BuildCache
+//        outputs.upToDateWhen { !testCacheDisabled }
+//
+//        if (this is Test || this is ProcessForkOptions) {
+//            outputs.doNotCacheIf("Caching tests is disabled because `workingDir` is set to `rootDir`") { workingDir == rootDir }
+//        }
     }
 }
