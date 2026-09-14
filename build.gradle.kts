27a7780942a00ee77861066dfa7d918f36b9dbee
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2026-09-14 18:29:37 +0200

    Create lifecycle task for JS box tests with JSC on Windows (KTI-3559)

--- build.gradle.kts
+++ build.gradle.kts
@@ -359,6 +359,10 @@ tasks {
         dependsOn(":wasm:wasm.tests:wasmJsBoxTest")
     }
 
+    testLifecycleTask("wasmJsBoxWithJscOnWindows", QualityGate.Undefined) {
+        dependsOn(":wasm:wasm.tests:wasmJsBoxWithJscOnWindows")
+    }
+
     testLifecycleTask("wasmJsSplittingTest", QualityGate.Master) {
         dependsOn(":wasm:wasm.tests:wasmJsSplittingTest")
     }
