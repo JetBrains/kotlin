6229fa6d3b72903d0985585114c532a4dbc3c1cb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-09-14 10:54:08 +0300

    [Test] Extract `LoweringCliJvmFacade` from `BackendCliJvmFacade`

--- compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/runners/codegen/AbstractJvmBlackBoxCodegenWithSeparateKmpCompilationTestBase.kt
+++ compiler/tests-common-new/testFixtures/org/jetbrains/kotlin/test/runners/codegen/AbstractJvmBlackBoxCodegenWithSeparateKmpCompilationTestBase.kt
@@ -9,11 +9,13 @@ import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.backend.BlackBoxCodegenSuppressor
 import org.jetbrains.kotlin.test.backend.handlers.IrNoExpectSymbolsHandler
-import org.jetbrains.kotlin.test.backend.ir.BackendCliJvmFacade
+import org.jetbrains.kotlin.test.backend.ir.CodegenCliJvmFacade
+import org.jetbrains.kotlin.test.backend.ir.LoweringCliJvmFacade
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.builders.firHandlersStep
 import org.jetbrains.kotlin.test.builders.irHandlersStep
 import org.jetbrains.kotlin.test.builders.jvmArtifactsHandlersStep
+import org.jetbrains.kotlin.test.builders.loweredIrHandlersStep
 import org.jetbrains.kotlin.test.configuration.configureCommonHandlersForBoxTest
 import org.jetbrains.kotlin.test.configuration.configureDumpHandlersForCodegenTest
 import org.jetbrains.kotlin.test.configuration.configureJvmBoxCodegenSettings
@@ -80,7 +82,10 @@ abstract class AbstractJvmBlackBoxCodegenWithSeparateKmpCompilationTestBase(val 
             useHandlers(::IrNoExpectSymbolsHandler)
         }
 
-        facadeStep(::BackendCliJvmFacade)
+        facadeStep(::LoweringCliJvmFacade)
+        loweredIrHandlersStep {}
+
+        facadeStep(::CodegenCliJvmFacade)
 
         jvmArtifactsHandlersStep()
 
