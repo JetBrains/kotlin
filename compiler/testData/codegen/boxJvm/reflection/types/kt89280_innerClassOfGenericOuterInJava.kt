JetBrains/kotlin

commit fa7b4fa3cd459a21f7a51740e8ebafdb00e666e5

Reflection: support descriptor-less Java instance methods
`JavaKNamedFunction` now supports non-static methods, and
`KClassImpl.declaredNonStaticMembers` (which is a part of `members`)
creates them for Java classes.

The major and most complicated part of this commit is implementation of
the type enhancement logic. It has been mostly adapted from the K1
implementation, with several simplifications, the main one being that
there's no support for JSR-305 yet.

 #KT-80710

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ConvertFromJava.kt
@@ -10,6 +10,25 @@ import org.jetbrains.kotlin.descriptors.runtime.structure.safeClassLoader
 import org.jetbrains.kotlin.load.java.FakePureImplementationsProvider
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.util.OperatorNameConventions.ASSIGNMENT_OPERATIONS
+import org.jetbrains.kotlin.util.OperatorNameConventions.BINARY_OPERATION_NAMES
+import org.jetbrains.kotlin.util.OperatorNameConventions.COMPARE_TO
+import org.jetbrains.kotlin.util.OperatorNameConventions.CONTAINS
+import org.jetbrains.kotlin.util.OperatorNameConventions.DEC
+import org.jetbrains.kotlin.util.OperatorNameConventions.EQUALS
+import org.jetbrains.kotlin.util.OperatorNameConventions.GET
+import org.jetbrains.kotlin.util.OperatorNameConventions.GET_VALUE
+import org.jetbrains.kotlin.util.OperatorNameConventions.HAS_NEXT
+import org.jetbrains.kotlin.util.OperatorNameConventions.INC
+import org.jetbrains.kotlin.util.OperatorNameConventions.INVOKE
+import org.jetbrains.kotlin.util.OperatorNameConventions.ITERATOR
+import org.jetbrains.kotlin.util.OperatorNameConventions.NEXT
+import org.jetbrains.kotlin.util.OperatorNameConventions.PROVIDE_DELEGATE
+import org.jetbrains.kotlin.util.OperatorNameConventions.SET
+import org.jetbrains.kotlin.util.OperatorNameConventions.SET_VALUE
+import org.jetbrains.kotlin.util.OperatorNameConventions.SIMPLE_UNARY_OPERATION_NAMES
+import org.jetbrains.kotlin.util.OperatorNameConventions.isComponentN
 import java.lang.reflect.*
 import kotlin.reflect.*
 import kotlin.reflect.full.createType
@@ -225,9 +244,6 @@ private val TypeVariable<*>.kotlinContainer: KTypeParameterOwnerImpl
                 )
         }
         is Method -> {
-            require(Modifier.isStatic(container.modifiers)) {
-                "Only static methods are supported for now: $container"
-            }
             val containingClass = container.declaringClass.kotlin as KClassImpl<*>
             containingClass.functions.singleOrNull { it.javaMethod == container } as JavaKFunction?
                 ?: throw KotlinReflectionInternalError(
@@ -323,3 +339,34 @@ internal fun getPurelyImplementedSupertype(kClass: KClassImpl<*>): KType? {
     val result = createJavaSimpleType(superClass, superClass.kotlin, typeArguments, isMarkedNullable = false)
     return result.createMutableCollectionType(superClass) ?: result
 }
+
+// Based on `OperatorFunctionChecks` from the compiler.
+internal fun Method.isJavaMethodAnOperator(): Boolean {
+    if (Modifier.isStatic(modifiers)) return false
+    val name = Name.identifier(name)
+    val paramCount = parameterTypes.size
+    val notVararg = !isVarArgs
+    return when (name) {
+        GET -> paramCount >= 1
+        SET -> paramCount >= 2 && notVararg
+        GET_VALUE -> notVararg && paramCount == 2 && isKPropertySupertype(parameterTypes[1])
+        SET_VALUE -> notVararg && paramCount == 3 && isKPropertySupertype(parameterTypes[1])
+        PROVIDE_DELEGATE -> notVararg && paramCount == 2 && isKPropertySupertype(parameterTypes[1])
+        INVOKE -> true
+        CONTAINS -> paramCount == 1 && notVararg && returnType == Boolean::class.javaPrimitiveType
+        ITERATOR -> paramCount == 0
+        NEXT -> paramCount == 0
+        HAS_NEXT -> paramCount == 0 && returnType == Boolean::class.javaPrimitiveType
+        EQUALS -> paramCount == 1 && parameterTypes[0] == Any::class.java
+        COMPARE_TO -> returnType == Int::class.javaPrimitiveType && paramCount == 1 && notVararg
+        in BINARY_OPERATION_NAMES -> paramCount == 1 && notVararg
+        in SIMPLE_UNARY_OPERATION_NAMES -> paramCount == 0
+        INC, DEC -> declaringClass.isAssignableFrom(returnType)
+        in ASSIGNMENT_OPERATIONS -> returnType == Void.TYPE && paramCount == 1 && notVararg
+        // TODO (KT-86404): support `of`.
+        else -> isComponentN(name)
+    }
+}
+
+private fun isKPropertySupertype(type: Class<*>): Boolean =
+    type.isAssignableFrom(KProperty::class.java)

commit 7d64bb539fd4002212522771c7b04ae2fc46717d

Reflection: fix NoSuchElementException in ConvertFromJava.kt
 #RCA: caused by fa7b4fa3cd (part of KT-80710), no further steps.

 #KT-89280 Fixed

core/reflection.jvm/src/kotlin/reflect/jvm/internal/ConvertFromJava.kt
@@ -289,7 +289,7 @@ private fun SimpleKType.toFlexibleArrayType(
     ) as FlexibleKType
 
 private fun Type.argumentsMakeSenseOnlyForMutableContainer(mutableType: SimpleKType?): Boolean =
-    this is ParameterizedType && actualTypeArguments.last().let {
+    this is ParameterizedType && actualTypeArguments.lastOrNull().let {
         it is WildcardType && it.lowerBounds.size == 1
     } && mutableType != null && (mutableType.classifier as KClass<*>).typeParameters.last().variance == KVariance.OUT
 

compiler/testData/codegen/boxJvm/reflection/types/kt89280_innerClassOfGenericOuterInJava.kt
@@ -1,0 +1,26 @@
+// TARGET_BACKEND: JVM
+// WITH_REFLECT
+// FILE: test/Outer.java
+package test;
+
+public class Outer<T> {
+    public class Inner {
+        public Inner self() { return this; }
+        public Inner other(Inner x) { return x; }
+    }
+}
+
+// FILE: box.kt
+import kotlin.reflect.jvm.kotlinFunction
+import kotlin.test.assertEquals
+import test.Outer
+
+fun box(): String {
+    val self = Outer.Inner::class.java.getMethod("self").kotlinFunction!!
+    assertEquals("fun test.Outer<T>.Inner.self(): test.Outer<T!>.Inner!", self.toString())
+
+    val other = Outer.Inner::class.java.getMethod("other", Outer.Inner::class.java).kotlinFunction!!
+    assertEquals("fun test.Outer<T>.Inner.other(test.Outer<T!>.Inner!): test.Outer<T!>.Inner!", other.toString())
+
+    return "OK"
+}