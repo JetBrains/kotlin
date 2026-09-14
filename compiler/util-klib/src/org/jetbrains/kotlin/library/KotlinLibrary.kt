bfb03344c13bc2dd497d01109aaf092e76b255df
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-09-14 14:00:36 +0200

    [KLIB API] Drop `BaseKotlinLibrary.*Dependencies` properties
    The following extension properties are obsolete after removal
    of the KLIB resolver and should not be used anymore:
    - BaseKotlinLibrary.unresolvedDependencies
    - BaseKotlinLibrary.hasDependencies
    
    ^KT-61096

--- compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibrary.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibrary.kt
@@ -131,16 +131,6 @@ val BaseKotlinLibrary.uniqueName: String
 val BaseKotlinLibrary.shortName: String?
     get() = manifestProperties.getProperty(KLIB_PROPERTY_SHORT_NAME)
 
-val BaseKotlinLibrary.unresolvedDependencies: List<RequiredUnresolvedLibrary>
-    get() = unresolvedDependencies(lenient = false).map { it as RequiredUnresolvedLibrary }
-
-fun BaseKotlinLibrary.unresolvedDependencies(lenient: Boolean = false): List<UnresolvedLibrary> =
-    manifestProperties.propertyList(KLIB_PROPERTY_DEPENDS, escapeInQuotes = true)
-        .map { UnresolvedLibrary(it, lenient = lenient) }
-
-val BaseKotlinLibrary.hasDependencies: Boolean
-    get() = !manifestProperties.getProperty(KLIB_PROPERTY_DEPENDS).isNullOrBlank()
-
 interface KotlinLibrary : Klib, BaseKotlinLibrary
 
 val KotlinLibrary.jsOutputName: String?
