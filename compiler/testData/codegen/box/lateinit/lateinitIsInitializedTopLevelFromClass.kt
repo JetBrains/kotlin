7f84cb4525c864e599b937b96d908429b2e08786
Andrii Rublov <andrii.rublov@jetbrains.com>
2026-09-14 12:12:24 +0200

    [Native] Mute `lateinitIsInitializedFromNestedClass` for Native
    ^KT-89375

--- compiler/testData/codegen/box/lateinit/lateinitIsInitializedTopLevelFromClass.kt
+++ compiler/testData/codegen/box/lateinit/lateinitIsInitializedTopLevelFromClass.kt
@@ -1,5 +1,7 @@
 // KT-89290
-// IGNORE_IR_DESERIALIZATION_TEST: JS_IR
+// IGNORE_BACKEND: NATIVE
+// ^^^ KT-89416 Native: top-level properties are not initialized when checked for initialization state from another class
+// IGNORE_IR_DESERIALIZATION_TEST: JS_IR, NATIVE
 // ^^^ KT-89456: Top-level declaration order differs before KLIB serialization vs after deserialization
 
 // The same setup as in lateinitIsInitializedFromNestedClass.kt, but `late` and `init` are top-level.
