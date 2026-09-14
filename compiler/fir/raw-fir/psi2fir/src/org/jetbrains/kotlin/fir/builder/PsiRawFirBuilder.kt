4cc294b065936c1dcb03f8483a958f9086cfe6fc
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-09-14 08:55:00 +0200

    Compiler: make forEachChildren(ReturnList) inline in FIR builder
    This commit allows to restore 2-3% performance for FIR/LT builder
    according to our measurements.
    
    Related to KT-86282

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/PsiRawFirBuilder.kt
@@ -121,6 +121,10 @@ open class PsiRawFirBuilder(
         return children.firstOrNull { it.node.elementType == type }
     }
 
+    override fun PsiElement?.getChildrenAsArray(): Array<out PsiElement?> {
+        error("Should not be called")
+    }
+
     override fun PsiElement.getReferencedNameAsName(): Name {
         return (this as KtSimpleNameExpression).getReferencedNameAsName()
     }
