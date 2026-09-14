89ad3c0507a868473cda57c2674e530f6f927a99
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-09-14 15:46:08 +0200

    [KLIB API] Move constants to the proper source file
    Move constants from `SearchPathResolver.kt` to `KotlinLibrary.kt`.
    Drop `SearchPathResolver.kt`, as "search path resolver" was a part
    of the KLIB resolver that was just removed.
    
    ^KT-61096

--- compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibrary.kt
+++ compiler/util-klib/src/org/jetbrains/kotlin/library/KotlinLibrary.kt
@@ -13,6 +13,15 @@ import org.jetbrains.kotlin.metadata.deserialization.MetadataVersion
 import java.util.*
 
 /**
+ * Some well-known constants:
+ */
+const val KOTLIN_NATIVE_STDLIB_NAME: String = "stdlib"
+const val KOTLIN_JS_STDLIB_NAME: String = "kotlin"
+const val KOTLIN_WASM_STDLIB_NAME: String = "kotlin"
+const val KOTLIN_JKLIB_STDLIB_NAME: String = "kotlin-stdlib"
+const val KOTLINTEST_MODULE_NAME: String = "kotlin-test"
+
+/**
  * [org.jetbrains.kotlin.library.KotlinAbiVersion]
  */
 const val KLIB_PROPERTY_ABI_VERSION = "abi_version"
--- compiler/util-klib/src/org/jetbrains/kotlin/library/SearchPathResolver.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.kotlin.library
-
-const val KOTLIN_NATIVE_STDLIB_NAME: String = "stdlib"
-const val KOTLIN_JS_STDLIB_NAME: String = "kotlin"
-const val KOTLIN_WASM_STDLIB_NAME: String = "kotlin"
-const val KOTLIN_JKLIB_STDLIB_NAME: String = "kotlin-stdlib"
-const val KOTLINTEST_MODULE_NAME: String = "kotlin-test"
-
