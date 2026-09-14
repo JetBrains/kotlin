365183603f411107344ed85c9900b226ab2bfb22
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2026-09-14 15:34:29 +0200

    [KLIB API] Drop the legacy API that was kept for kotlinx-benchmarks
    Since KT-82882, the Gradle plugin for kotlinx-benchmarks library
    has been migrated from K1 + KLIB resolver to KSP + KlibLoader.
    We don't need to maintain legacy API for backward binary compatibility
    with old kotlinx-benchmarks Gradle plugin versions.
    
    ^KT-82882

--- compiler/util-klib-metadata/src/org/jetbrains/kotlin/library/metadata/KlibMetadataDeserializationUtils.kt
+++ compiler/util-klib-metadata/src/org/jetbrains/kotlin/library/metadata/KlibMetadataDeserializationUtils.kt
@@ -12,9 +12,3 @@ fun parsePackageFragment(packageMetadata: ByteArray): ProtoBuf.PackageFragment =
 
 fun parseModuleHeader(libraryMetadata: ByteArray): KlibMetadataProtoBuf.Header =
     KlibMetadataProtoBuf.Header.parseFrom(libraryMetadata, KlibMetadataSerializerProtocol.extensionRegistry)
-
-@Deprecated(
-    "Preserved for binary compatibility with existing versions of the kotlinx-benchmarks Gradle plugin. See KT-82882.",
-    level = DeprecationLevel.HIDDEN
-)
-interface PackageAccessHandler
