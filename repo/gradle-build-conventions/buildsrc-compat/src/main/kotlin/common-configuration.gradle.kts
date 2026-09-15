7ba95cce8a81f8b0f9c4d43badaf48bdb48a7ce1
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-09-15 07:56:50 +0000

    Deprecate runtime JDK<17 and implement `-Xallow-pre-17-runtime-jdk`
    Also, move `getJavaVersion()` -> `getRuntimeJdkVersion()`.
    
    ^KT-89211
    ^KT-88174
    
    (cherry picked from commit 6843669933c2aec6bf7276b7777f6fc0da6ace68)

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/common-configuration.gradle.kts
@@ -192,6 +192,7 @@ fun Project.configureKotlinCompilationOptions() {
                         "-Xname-based-destructuring=complete".takeUnless { skipNewLanguageFeatures },
                         "-Xcollection-literals".takeUnless { skipNewLanguageFeatures },
                         "-Xexplicit-context-arguments".takeUnless { skipNewLanguageFeatures },
+                        "-Xallow-pre-17-runtime-jdk", // KT-88174
                         // Between making a language feature stable and the next bootstrap, we need to keep providing the compiler argument.
                         // But this produces a warning
                         // "The argument ... is redundant for the current language version ..."
