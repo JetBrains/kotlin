3c4e8825235543bec80e481c23ebc1ad5058dcdc
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-09-16 09:20:47 +0200

    fixup! Stdlib: introduce PlatformInline annotation class

--- libraries/stdlib/jvm/runtime/kotlin/PlatformInline.kt
+++ libraries/stdlib/jvm/runtime/kotlin/PlatformInline.kt
@@ -5,4 +5,7 @@
 
 package kotlin
 
+// About this suppression: we should support this typealias separately
+// in JvmSupertypeUpdater to do things properly
+@Suppress("TYPEALIAS_EXPANDS_TO_COMPILER_REQUIRED_ANNOTATION_ERROR")
 public actual typealias PlatformInline = kotlin.jvm.JvmInline
