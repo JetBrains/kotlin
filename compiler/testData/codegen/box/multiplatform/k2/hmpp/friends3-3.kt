b41839b9e8208cab8a1007abf7a0643f73e0972e
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2026-09-16 12:18:47 +0300

    [Test] Reproduce problem with invisible transitive friends
    After fix of KT-89391 in KGP the friend dependencies for common
    sourcesets `isVisible` check stopped work properly for intermediate
    fragments.

---
+++ compiler/testData/codegen/box/multiplatform/k2/hmpp/friends3-3.kt
@@ -0,0 +1,29 @@
+// LANGUAGE: +MultiPlatformProjects
+// ISSUE: KT-89391
+// WITH_STDLIB
+
+// MODULE: lib-common
+internal fun foo() {}
+
+// MODULE: lib-inter()()(lib-common)
+// MODULE: lib-platform()()(lib-inter)
+
+
+// MODULE: app-common()(lib-common)
+fun test_common() {
+    foo()
+}
+
+// MODULE: app-inter()(lib-inter, lib-common)(app-common)
+fun test_inter() {
+    <!INVISIBLE_REFERENCE{PLATFORM}!>foo<!>()
+}
+
+// MODULE: app-platform()(lib-platform)(app-inter)
+fun test_platform() {
+    foo()
+}
+
+fun box(): String {
+    return "OK"
+}
