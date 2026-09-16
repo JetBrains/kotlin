4490af8b01bd3a96f6447e591856943ca14f6f1b
Alexander Udalov <alexander.udalov@jetbrains.com>
2026-09-16 13:24:53 +0200

    Tests: remove obsolete TargetBackend.JVM_IR_SERIALIZE
    IR serialization for JVM was removed in KT-84406.

--- compiler/testData/codegen/box/reified/reifiedTypeArgumentWithRecursion.kt
+++ compiler/testData/codegen/box/reified/reifiedTypeArgumentWithRecursion.kt
@@ -1,4 +1,4 @@
-// IGNORE_BACKEND_MULTI_MODULE: JVM_IR, JVM_IR_SERIALIZE
+// IGNORE_BACKEND_MULTI_MODULE: JVM_IR
 // ^^^KT-79201: This function has a reified type parameter and thus can only be inlined at compilation time, not called directly.
 
 // WITH_STDLIB
