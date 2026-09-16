f930ec8c45f50779d9a468954acea5f78606365e
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2026-09-16 07:36:56 +0000

    [FE, JVM] Move inlineUtil into frontend.common.jvm

--- compiler/frontend.common.jvm/src/org/jetbrains/kotlin/inline/inlineUtil.kt
+++ compiler/frontend.common.jvm/src/org/jetbrains/kotlin/inline/inlineUtil.kt
@@ -1,23 +1,11 @@
 /*
- * Copyright 2010-2015 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2010-2026 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
 package org.jetbrains.kotlin.inline
 
-import org.jetbrains.kotlin.K1Deprecation
-import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
+import org.jetbrains.kotlin.descriptors.Visibilities
 import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
 import org.jetbrains.kotlin.metadata.ProtoBuf
 import org.jetbrains.kotlin.metadata.deserialization.Flags
@@ -28,14 +16,11 @@ import org.jetbrains.kotlin.metadata.jvm.JvmProtoBuf
 import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmMemberSignature
 import org.jetbrains.kotlin.metadata.jvm.deserialization.JvmProtoBufUtil
 import org.jetbrains.kotlin.serialization.deserialization.ProtoEnumFlags
-import org.jetbrains.kotlin.serialization.deserialization.descriptorVisibility
 
-@K1Deprecation
 sealed interface InlineFunctionOrAccessor {
     val jvmMethodSignature: JvmMemberSignature.Method
 }
 
-@K1Deprecation
 data class InlineFunction(
     override val jvmMethodSignature: JvmMemberSignature.Method,
 
@@ -43,7 +28,6 @@ data class InlineFunction(
     val kotlinFunctionName: String
 ) : InlineFunctionOrAccessor
 
-@K1Deprecation
 data class InlinePropertyAccessor(
     override val jvmMethodSignature: JvmMemberSignature.Method,
 
@@ -51,7 +35,6 @@ data class InlinePropertyAccessor(
     val propertyName: String
 ) : InlineFunctionOrAccessor
 
-@K1Deprecation
 fun inlineFunctionsAndAccessors(header: KotlinClassHeader, excludePrivateMembers: Boolean = false): List<InlineFunctionOrAccessor> {
     val data = header.data ?: return emptyList()
     val strings = header.strings ?: return emptyList()
@@ -122,4 +105,4 @@ fun inlinePropertyAccessors(
     return inlineAccessors
 }
 
-private fun isPrivate(flags: Int) = DescriptorVisibilities.isPrivate(ProtoEnumFlags.descriptorVisibility(Flags.VISIBILITY.get(flags)))
+private fun isPrivate(flags: Int) = Visibilities.isPrivate(ProtoEnumFlags.visibility(Flags.VISIBILITY.get(flags)))
