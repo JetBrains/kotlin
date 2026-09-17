88a620659ac2e44d0b33ffd06695254851ce1ca3
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-17 10:51:27 +0200

    [FIR] Enforce RichErrors LF on references to Value & RichError
    #KT-89103

--- compiler/testData/diagnostics/tests/richErrorsDisabled.ll.kt
+++ compiler/testData/diagnostics/tests/richErrorsDisabled.ll.kt
@@ -1,18 +1,21 @@
+// LL_FIR_DIVERGENCE
+//   Value is available in AA even when the LF is disabled
+// LL_FIR_DIVERGENCE
 // RUN_PIPELINE_TILL: FRONTEND
 // LANGUAGE: -RichErrors
 // WITH_STDLIB
 
-<!UNSUPPORTED_FEATURE, WRONG_MODIFIER_TARGET!>error<!> class Foo
-<!UNSUPPORTED_FEATURE, WRONG_MODIFIER_TARGET!>error<!> object Bar
+<!UNSUPPORTED_FEATURE!>error<!> class Foo
+<!UNSUPPORTED_FEATURE!>error<!> object Bar
 
-fun <T : <!UNSUPPORTED_FEATURE!>RichError<!>, V : <!UNRESOLVED_REFERENCE!>Value<!>> foo(
+fun <T : <!UNSUPPORTED_FEATURE!>RichError<!>, V : <!UNSUPPORTED_FEATURE!>Value<!>> foo(
     x: <!UNSUPPORTED_FEATURE!>String | Foo<!>,
     re: <!UNSUPPORTED_FEATURE!>RichError<!>,
-    v: <!UNRESOLVED_REFERENCE!>Value<!>,
+    v: <!UNSUPPORTED_FEATURE!>Value<!>,
 ) {
     <!UNNECESSARY_SAFE_CALL!>x<!UNSUPPORTED_FEATURE!>|.<!>length<!>
     <!UNSUPPORTED_FEATURE!><!UNSUPPORTED_FEATURE!>RichError<!>::class.java<!>
-    <!UNRESOLVED_REFERENCE!>Value<!>::class.<!CANNOT_INFER_PARAMETER_TYPE, UNRESOLVED_REFERENCE_WRONG_RECEIVER!>java<!>
+    <!UNSUPPORTED_FEATURE!><!UNSUPPORTED_FEATURE!>Value<!>::class.java<!>
 }
 
 /* GENERATED_FIR_TAGS: classDeclaration, functionDeclaration, nullableType, objectDeclaration, safeCall */
