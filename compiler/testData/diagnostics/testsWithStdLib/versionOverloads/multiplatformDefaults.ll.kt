508d2623608160fed58bef91d1a8ae8ef7a4759c
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-17 09:17:28 +0200

    [Tests] Reproduce #KT-89453

--- compiler/testData/diagnostics/testsWithStdLib/versionOverloads/multiplatformDefaults.ll.kt
+++ compiler/testData/diagnostics/testsWithStdLib/versionOverloads/multiplatformDefaults.ll.kt
@@ -16,6 +16,11 @@ expect fun missingDefault(
     <!INVALID_VERSIONING_ON_NON_OPTIONAL!>@IntroducedAt("2")<!> y: String,
 ): String
 
+expect class P1 {
+    fun m(a: Int, b: Int = 5, c: Int = 50)
+}
+
+
 // MODULE: m2-jvm()()(m1-common)
 // FILE: jvm.kt
 
@@ -28,5 +33,10 @@ actual fun missingDefault(
     x: String,
     <!ACTUAL_FUNCTION_WITH_DEFAULT_ARGUMENTS!>@IntroducedAt("2") y: String = "bar"<!>,
 ): String = x + y
+
+actual class P1 {
+    actual fun m(a: Int, @IntroducedAt("1") b: Int, <!INVALID_NON_OPTIONAL_PARAMETER_POSITION!>c: Int<!>) {}
+}
+
 
 /* GENERATED_FIR_TAGS: actual, additiveExpression, expect, functionDeclaration, stringLiteral */
