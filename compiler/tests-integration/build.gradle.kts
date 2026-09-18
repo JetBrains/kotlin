ce856d6d87717148b157d62061b9c7016202e3a6
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2026-09-18 16:32:18 +0300

    Bump `DEFAULT_JAVA_LAUNCHER_FOR_TESTS` to JDK_17 and drop old overrides
    ^KT-88173

--- compiler/tests-integration/build.gradle.kts
+++ compiler/tests-integration/build.gradle.kts
@@ -87,7 +87,6 @@ projectTests {
             JdkMajorVersion.JDK_17_0,
             JdkMajorVersion.JDK_21_0
         ),
-        javaLauncher = JdkMajorVersion.JDK_17_0,
         maxHeapSize = testMaxHeapSizeLarge,
     ) {
         dependsOn(":dist")
