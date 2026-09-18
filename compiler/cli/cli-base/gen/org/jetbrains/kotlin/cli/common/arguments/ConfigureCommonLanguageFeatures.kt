6368f1f5b52ec4203fab961d53c09adc017c9d55
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2026-09-18 09:56:53 +0000

    [CLI] Deprecate `Xmulti-dollar-interpolation`, `Xnon-local-break-continue`, `-Xwhen-guards` flags
    They are mandatory to be deprecated/removed after the generation guard
    that was introduced by the previous commit.

--- compiler/cli/cli-base/gen/org/jetbrains/kotlin/cli/common/arguments/ConfigureCommonLanguageFeatures.kt
+++ compiler/cli/cli-base/gen/org/jetbrains/kotlin/cli/common/arguments/ConfigureCommonLanguageFeatures.kt
@@ -100,6 +100,7 @@ internal fun MutableMap<LanguageFeature, LanguageFeature.State>.configureCommonL
         put(LanguageFeature.LocalTypeAliases, LanguageFeature.State.ENABLED)
     }
 
+    @Suppress("DEPRECATION")
     if (arguments.multiDollarInterpolation) {
         put(LanguageFeature.MultiDollarInterpolation, LanguageFeature.State.ENABLED)
     }
@@ -128,6 +129,7 @@ internal fun MutableMap<LanguageFeature, LanguageFeature.State>.configureCommonL
         put(LanguageFeature.NestedTypeAliases, LanguageFeature.State.ENABLED)
     }
 
+    @Suppress("DEPRECATION")
     if (arguments.nonLocalBreakContinue) {
         put(LanguageFeature.BreakContinueInInlineLambdas, LanguageFeature.State.ENABLED)
     }
@@ -136,6 +138,7 @@ internal fun MutableMap<LanguageFeature, LanguageFeature.State>.configureCommonL
         put(LanguageFeature.StrictEqualsForStructuralClasses, LanguageFeature.State.ENABLED)
     }
 
+    @Suppress("DEPRECATION")
     if (arguments.whenGuards) {
         put(LanguageFeature.WhenGuards, LanguageFeature.State.ENABLED)
     }
