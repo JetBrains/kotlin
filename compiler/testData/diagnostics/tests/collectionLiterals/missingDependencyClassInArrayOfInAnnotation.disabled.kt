4c4a16be3221eb4a5de93016f9d306865f568a5b
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-18 11:30:40 +0300

    [Tests] Extend tests for KT-89259

--- compiler/testData/diagnostics/tests/collectionLiterals/missingDependencyClassInArrayOfInAnnotation.disabled.kt
+++ compiler/testData/diagnostics/tests/collectionLiterals/missingDependencyClassInArrayOfInAnnotation.disabled.kt
@@ -5,23 +5,42 @@
 // ISSUE: KT-89259
 
 // MODULE: m1
+// FILE: f1.kt
+package p1
 
 open class Missing
 
 // MODULE: m2(m1)
+// FILE: f2.kt
+package p2
 
-annotation class WithMissingInParams(val param: Array<kotlin.reflect.KClass<out Missing>>)
+import kotlin.reflect.KClass
+
+open class ChildOfMissing : p1.Missing()
+
+annotation class WithMissingInParams(val param: Array<KClass<out p1.Missing>>)
+annotation class WithChildOfMissingInParams(val param: Array<KClass<out ChildOfMissing>>)
 
 // MODULE: m3(m2)
+// FILE: f3.kt
+package p3
 
-@WithMissingInParams([])
+import kotlin.reflect.KClass
+
+@p2.WithMissingInParams([])
+@p2.WithChildOfMissingInParams([])
 class Annotated
 
-@WithMissingInParams(arrayOf())
+@p2.WithMissingInParams(arrayOf())
+@p2.WithChildOfMissingInParams(arrayOf())
 class Annotated2
 
+annotation class SuperClassMissingInConstructor(val param: Array<KClass<p2.ChildOfMissing>> = [])
+annotation class SuperClassMissingInConstructorVararg(vararg val param: KClass<p2.ChildOfMissing> = [])
+
 fun forReference() {
-    <!MISSING_DEPENDENCY_CLASS!>WithMissingInParams<!>(<!MISSING_DEPENDENCY_CLASS!>arrayOf<!>())
+    p2.WithChildOfMissingInParams(arrayOf())
+    p2.<!MISSING_DEPENDENCY_CLASS!>WithMissingInParams<!>(<!MISSING_DEPENDENCY_CLASS!>arrayOf<!>())
 }
 
 /* GENERATED_FIR_TAGS: annotationDeclaration, classDeclaration, outProjection, primaryConstructor, propertyDeclaration */
