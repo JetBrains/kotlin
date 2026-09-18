7d48ab87f187e7afb4982c7d024ee7b772538356
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-09-18 16:37:21 +0200

    [test] Migrate some JS line number tests to stepping tests
    KT-54761

--- compiler/testData/debug/stepping/classCapturingLocals.kt
+++ compiler/testData/debug/stepping/classCapturingLocals.kt
@@ -1,3 +1,6 @@
+// WITH_STDLIB
+
+// FILE: test.kt
 class A
 
 fun A.foo() {
@@ -14,4 +17,85 @@ fun A.foo() {
 
 fun baz() = 23
 
-// LINES: 1 1 3 3 4 11 11 12 12 15 15 15 15 5 * * 5 * 6 7 7 8 8
+fun box() {
+    val a = A()
+    a.foo()
+}
+
+// EXPECTATIONS JVM_IR
+// test.kt:21 box
+// test.kt:4 <init>
+// test.kt:21 box
+// test.kt:22 box
+// test.kt:7 foo
+// test.kt:18 baz
+// test.kt:7 foo
+// test.kt:14 foo
+// test.kt:15 foo
+// test.kt:8 <init>
+// test.kt:15 foo
+// test.kt:10 bar
+// test.kt:11 bar
+// test.kt:12 bar
+// test.kt:16 foo
+// test.kt:23 box
+
+// EXPECTATIONS JS_IR
+// test.kt:21 box
+// test.kt:4 <init>
+// test.kt:22 box
+// test.kt:7 foo
+// test.kt:18 baz
+// test.kt:14 foo
+// test.kt:15 foo
+// test.kt:8 <init>
+// test.kt:15 foo
+// test.kt:10 bar
+// test.kt:11 bar
+// test.kt:12 bar
+// test.kt:16 foo
+// test.kt:23 box
+
+// EXPECTATIONS WASM
+// test.kt:21 $box (12)
+// test.kt:4 $A.<init> (7)
+// test.kt:22 $box (4, 6)
+// test.kt:7 $foo (12)
+// test.kt:18 $baz (12, 14)
+// test.kt:7 $foo (12)
+// test.kt:14 $foo (13, 21, 12, 4)
+// test.kt:15 $foo (4)
+// test.kt:13 $B.<init> (5)
+// test.kt:15 $foo (8)
+// test.kt:10 $B.bar (20, 12)
+// test.kt:11 $B.bar (21, 31, 20, 12)
+// test.kt:12 $B.bar (9)
+// test.kt:16 $foo (1)
+// test.kt:23 $box (1)
+
+// EXPECTATIONS NATIVE
+// test.kt:21 box
+// test.kt:4 <init>
+// test.kt:21 box
+// test.kt:22 box
+// test.kt:6 foo
+// test.kt:7 foo
+// test.kt:18 baz
+// test.kt:7 foo
+// test.kt:14 foo
+// test.kt:14 foo
+// test.kt:14 foo
+// test.kt:14 foo
+// test.kt:15 foo
+// test.kt:8 <init>
+// test.kt:13 <init>
+// test.kt:15 foo
+// test.kt:9 bar
+// test.kt:10 bar
+// test.kt:10 bar
+// test.kt:11 bar
+// test.kt:11 bar
+// test.kt:11 bar
+// test.kt:12 bar
+// test.kt:16 foo
+// test.kt:23 box
--- compiler/testData/debug/stepping/functionReferences/closure.kt
+++ compiler/testData/debug/stepping/functionReferences/closure.kt
@@ -1,8 +1,44 @@
-fun box(x: Int, y: Int): Int {
+// FILE: test.kt
+
+fun test(x: Int, y: Int): Int {
     fun foo(
             z: Int) =
                 x + z
     return foo(y)
 }
 
-// LINES: 1 1 5 5 2 * 3 4 4 4
+fun box() {
+    test(109, 136)
+}
+
+// EXPECTATIONS JVM_IR
+// test.kt:11 box
+// test.kt:7 test
+// test.kt:6 test$foo
+// test.kt:7 test
+// test.kt:11 box
+// test.kt:12 box
+
+// EXPECTATIONS JS_IR
+// test.kt:11 box
+// test.kt:7 test
+// test.kt:6 test$foo
+// test.kt:12 box
+
+// EXPECTATIONS WASM
+// test.kt:11 $box (9, 14, 4)
+// test.kt:7 $test (11, 15, 11)
+// test.kt:6 $foo (16, 20, 16, 21)
+// test.kt:7 $test (4)
+// test.kt:11 $box (4)
+// test.kt:12 $box (1)
+
+// EXPECTATIONS NATIVE
+// test.kt:11 box
+// test.kt:3 test
+// test.kt:7 test
+// test.kt:4 test$foo
+// test.kt:6 test$foo
+// test.kt:7 test
+// test.kt:8 test
+// test.kt:12 box
