782323503cdb6939015c2631e1f3b5962b6acdf5
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-21 14:25:53 +0300

    [FIR] Don't run `FirExpressionWithErrorTypeChecker` for wrapped args
    Both `FirNamedArgumentExpression` and `FirSpreadArgumentExpression` have
    the type of the underlying expression => we can get duplicating
    diagnostics because of that checker. Also, in KT-89533 we reported (the
    second) `UNRESOLVED_COLLECTION_LITERAL` on the whole `KtValueArgument`
    because of that while this diagnostic is expected to be reported
    precisely on `KtCollectionLiteral`.
    
    ^KT-89533 Fixed

--- compiler/testData/diagnostics/tests/unresolvedReferenceInSpread.kt
+++ compiler/testData/diagnostics/tests/unresolvedReferenceInSpread.kt
@@ -3,7 +3,7 @@
 fun foo(vararg x: Int) {}
 
 fun test() {
-    foo(<!UNRESOLVED_REFERENCE!><!SPREAD_OF_NULLABLE!>*<!><!UNRESOLVED_REFERENCE!>Unresolved<!><!>)
+    foo(<!SPREAD_OF_NULLABLE!>*<!><!UNRESOLVED_REFERENCE!>Unresolved<!>)
 }
 
 /* GENERATED_FIR_TAGS: functionDeclaration, vararg */
