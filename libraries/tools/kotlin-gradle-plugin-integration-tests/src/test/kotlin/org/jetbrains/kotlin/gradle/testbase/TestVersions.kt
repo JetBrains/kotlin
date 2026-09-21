d4770e828eb03549c8a5166545af14df0cd12dd3
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2026-09-21 11:48:34 +0200

    fixup! [Gradle] Run integration tests against Gradle 9.8.0 release

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -56,11 +56,12 @@ interface TestVersions {
         const val AGP_91 = "9.1.1"
         const val AGP_92 = "9.2.1"
         const val AGP_93 = "9.3.2"
-        const val AGP_94 = "9.4.0"
+        const val AGP_94 = "9.4.1"
+        const val AGP_95 = "9.5.0-alpha06"
 
         // Should be in sync with KotlinMultiplatformAndroidGradlePluginCompatibilityHealthCheck
         const val MIN_SUPPORTED = AGP_88 // AgpCompatibilityCheck.minimalSupportedAgpVersion
-        const val MAX_SUPPORTED = AGP_94 // Update once the Gradle MAX_SUPPORTED version is bumped
+        const val MAX_SUPPORTED = AGP_95 // Update once the Gradle MAX_SUPPORTED version is bumped
     }
 
     enum class AgpCompatibilityMatrix(
@@ -79,7 +80,8 @@ interface TestVersions {
         AGP_91(AGP.AGP_91, GradleVersion.version(Gradle.G_9_3), GradleVersion.version(Gradle.G_9_5), JavaVersion.VERSION_17),
         AGP_92(AGP.AGP_92, GradleVersion.version(Gradle.G_9_4), GradleVersion.version(Gradle.G_9_5), JavaVersion.VERSION_17),
         AGP_93(AGP.AGP_93, GradleVersion.version(Gradle.G_9_5), GradleVersion.version(Gradle.G_9_7), JavaVersion.VERSION_17),
-        AGP_94(AGP.AGP_94, GradleVersion.version(Gradle.G_9_6), GradleVersion.version(Gradle.G_9_8), JavaVersion.VERSION_17),
+        AGP_94(AGP.AGP_94, GradleVersion.version(Gradle.G_9_6), GradleVersion.version(Gradle.G_9_7), JavaVersion.VERSION_17),
+        AGP_95(AGP.AGP_95, GradleVersion.version(Gradle.G_9_7), GradleVersion.version(Gradle.G_9_8), JavaVersion.VERSION_17),
         ;
 
         companion object {
