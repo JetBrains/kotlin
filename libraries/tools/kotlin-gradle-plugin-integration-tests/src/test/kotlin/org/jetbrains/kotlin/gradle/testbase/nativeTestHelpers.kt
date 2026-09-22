2c70cb4ed70de3437debe035080ed9a9c870f22d
Egor Andreevich <egor.andreevich@jetbrains.com>
2026-09-22 18:57:14 +0200

    fixup! Clean usages except iosX64 in integration-tests
    Fix integration test assertions

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/nativeTestHelpers.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/nativeTestHelpers.kt
@@ -89,7 +89,7 @@ internal object MPPNativeTargets {
         else -> error("Unknown host")
     }
 
-    val supported = listOf("linux64", "macos64", "mingw64").filter { !unsupported.contains(it) }
+    val supported = listOf("linux64", "macosArm64", "mingw64").filter { !unsupported.contains(it) }
 }
 
 fun computeCacheDirName(
