142aac40f6cff0f4a255263c10eb26316aa82faf
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-09-22 11:49:54 +0200

    Revert "[FIR] Allow numeric class <-> builtins coercion in assignments"
    This reverts commit aa59534f5b389b8fbf18450e7e1f7cfcda394805.

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirDeclarationsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirDeclarationsResolveTransformer.kt
@@ -210,11 +210,6 @@ open class FirDeclarationsResolveTransformer(
                     if (!initializerIsAlreadyResolved) {
                         val resolutionMode = withExpectedType(property.returnTypeRef)
                         property.transformInitializer(transformer, resolutionMode)
-                        val initializer = property.initializer
-                        val expectedType = property.returnTypeRef.coneTypeOrNull
-                        if (initializer != null && expectedType != null) {
-                            property.replaceInitializer(initializer.wrapIntoNumericClassConversionIfNeeded(expectedType, session))
-                        }
                         property.replaceBodyResolveState(FirPropertyBodyResolveState.INITIALIZER_RESOLVED)
                     }
 
@@ -701,11 +696,6 @@ open class FirDeclarationsResolveTransformer(
                 val resolutionMode = withExpectedType(variable.returnTypeRef)
                 if (variable.initializer != null && variable.bodyResolveState < FirPropertyBodyResolveState.INITIALIZER_RESOLVED) {
                     variable.transformInitializer(transformer, resolutionMode)
-                    val initializer = variable.initializer
-                    val expectedType = variable.returnTypeRef.coneTypeOrNull
-                    if (initializer != null && expectedType != null) {
-                        variable.replaceInitializer(initializer.wrapIntoNumericClassConversionIfNeeded(expectedType, session))
-                    }
                     storeVariableReturnType(variable)
                 }
                 variable.transformBackingField(transformer, withExpectedType(variable.returnTypeRef))
--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/body/resolve/FirExpressionsResolveTransformer.kt
@@ -1657,9 +1657,7 @@ open class FirExpressionsResolveTransformer(transformer: FirAbstractBodyResolveT
                 withExpectedType(
                     variableAssignment.lValue.resolvedType.toFirResolvedTypeRef(),
                 ),
-            ).also {
-                it.replaceRValue(it.rValue.wrapIntoNumericClassConversionIfNeeded(it.lValue.resolvedType, session))
-            }
+            )
         }
 
         // for cases like
