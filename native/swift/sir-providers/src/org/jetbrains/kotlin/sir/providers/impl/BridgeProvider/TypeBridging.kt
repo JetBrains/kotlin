33b261dbd94d1399433d0057b8a59826de9abd27
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-09-21 20:12:03 -0400

    [Native] Prepare the project to enable MURV by default
    ^KT-89020

--- native/swift/sir-providers/src/org/jetbrains/kotlin/sir/providers/impl/BridgeProvider/TypeBridging.kt
+++ native/swift/sir-providers/src/org/jetbrains/kotlin/sir/providers/impl/BridgeProvider/TypeBridging.kt
@@ -1240,7 +1240,7 @@ internal sealed interface Bridge {
             context(sir: SirSession)
             fun helperBridges(typeNamer: SirTypeNamer): List<SirBridge> {
                 return bridgeProxy?.createSirBridges {
-                    val actualArgs = argNames.drop(1).also { if (extensionReceiverParameter != null) it.drop(1) }
+                    val actualArgs = argNames.drop(1 + if (extensionReceiverParameter != null) 1 else 0)
                     buildCall("(${argNames.first()} as ${typeNamer.kotlinFqName(swiftType, SirTypeNamer.KotlinNameType.PARAMETRIZED)}).invoke(${actualArgs.joinToString()})")
                 } ?: emptyList()
             }
