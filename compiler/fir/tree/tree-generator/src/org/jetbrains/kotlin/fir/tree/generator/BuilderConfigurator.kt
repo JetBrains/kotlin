a8499e8586675107c358a99a925db032a73a5905
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2026-09-22 11:49:19 +0200

    Revert "[FIR] Add `withCopy` to `FirGetClassCallBuilder`"
    This reverts commit f8d2448899b5abc59604c8456c8d3ac0cae5dc68.

--- compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/BuilderConfigurator.kt
+++ compiler/fir/tree/tree-generator/src/org/jetbrains/kotlin/fir/tree/generator/BuilderConfigurator.kt
@@ -230,7 +230,6 @@ class BuilderConfigurator(model: Model) : AbstractFirBuilderConfigurator<Abstrac
 
         builder(getClassCall) {
             parents += callBuilder
-            withCopy()
         }
 
         val variableBuilder by builder {
