a45487b3da72f0c90944c528d649aa3bd761521c
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-22 11:13:37 +0200

    fixup! [stdlib] Introduce RichError class

--- libraries/stdlib/src/kotlin/RichError.kt
+++ libraries/stdlib/src/kotlin/RichError.kt
@@ -7,11 +7,12 @@
 package kotlin
 
 import kotlin.internal.RequireKotlin
+import kotlin.internal.RequireKotlinVersionKind
 
 /**
  * The superclass of all `error class` and `error object` types.
  */
-@RequireKotlin("2.5", versionKind = COMPILER_VERSION)
+@RequireKotlin("2.5", versionKind = RequireKotlinVersionKind.COMPILER_VERSION)
 @SinceKotlin("2.5")
 public abstract class RichError
 
