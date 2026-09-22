e1a73caa75f6b2e089895e3ed36bcc704bb3c4c0
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2026-09-22 13:18:13 +0200

    [Tests] Reproduce INVALID_VERSIONING_ON_VALUE_CLASS_PARAMETER on full value class primary parameter
    #KT-89523

---
+++ compiler/testData/diagnostics/testsWithStdLib/versionOverloads/fullValueClass.kt
@@ -0,0 +1,19 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// LANGUAGE: +FullValueClasses
+// ISSUE: KT-89523
+
+value class Point(
+    val x: Int,
+    <!INVALID_VERSIONING_ON_VALUE_CLASS_PARAMETER!>@IntroducedAt("2.0")<!> val y: Int = 0,
+)
+
+value class SingleField(
+    <!INVALID_VERSIONING_ON_VALUE_CLASS_PARAMETER!>@IntroducedAt("2.0")<!> val x: Int = 0,
+)
+
+@JvmInline
+value class Inline(
+    <!INVALID_VERSIONING_ON_VALUE_CLASS_PARAMETER!>@IntroducedAt("2.0")<!> val x: Int = 0,
+)
+
+/* GENERATED_FIR_TAGS: classDeclaration, integerLiteral, primaryConstructor, propertyDeclaration, stringLiteral, value */
