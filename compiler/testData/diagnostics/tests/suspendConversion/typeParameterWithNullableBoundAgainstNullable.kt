32b629cadd4ac3ed9ec3ebb1236dc668757f1b73
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-23 11:41:38 +0000

    [Tests] Reproduce existing behavior around suspend conversion
    The type of the suspend conversion expression is not nullable
    despite the expected type being nullable and the argument being a
    type parameter type with nullable bounds.
    
    #KT-89451

---
+++ compiler/testData/diagnostics/tests/suspendConversion/typeParameterWithNullableBoundAgainstNullable.kt
@@ -0,0 +1,10 @@
+// RUN_PIPELINE_TILL: CODEGEN
+suspend fun acceptSuspend(f: (suspend () -> String)?) {
+}
+
+suspend fun <T : (() -> String)?> test(f : T) {
+    acceptSuspend(<!DEBUG_INFO_EXPRESSION_TYPE("suspend () -> kotlin.String"), DEBUG_INFO_EXPRESSION_TYPE("T")!>f<!>)
+}
+
+/* GENERATED_FIR_TAGS: functionDeclaration, functionTypeConversion, functionalType, nullableType, suspend,
+typeConstraint, typeParameter */
