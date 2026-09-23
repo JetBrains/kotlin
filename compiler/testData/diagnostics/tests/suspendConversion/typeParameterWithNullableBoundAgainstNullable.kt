998265abcae5ff20432c85668571d87e9c5d4c0b
Kirill Rakhman <kirill.rakhman@jetbrains.com>
2026-09-23 11:41:42 +0000

    [FIR] Fix nullability of function type conversion expressions
    ... when the expected type is nullable and the argument is a
    non-null-marked type that can be null (like type parameter with
    nullable bounds).
    
    #KT-89451

--- compiler/testData/diagnostics/tests/suspendConversion/typeParameterWithNullableBoundAgainstNullable.kt
+++ compiler/testData/diagnostics/tests/suspendConversion/typeParameterWithNullableBoundAgainstNullable.kt
@@ -3,7 +3,7 @@ suspend fun acceptSuspend(f: (suspend () -> String)?) {
 }
 
 suspend fun <T : (() -> String)?> test(f : T) {
-    acceptSuspend(<!DEBUG_INFO_EXPRESSION_TYPE("suspend () -> kotlin.String"), DEBUG_INFO_EXPRESSION_TYPE("T")!>f<!>)
+    acceptSuspend(<!DEBUG_INFO_EXPRESSION_TYPE("(suspend () -> kotlin.String)?"), DEBUG_INFO_EXPRESSION_TYPE("T")!>f<!>)
 }
 
 /* GENERATED_FIR_TAGS: functionDeclaration, functionTypeConversion, functionalType, nullableType, suspend,
