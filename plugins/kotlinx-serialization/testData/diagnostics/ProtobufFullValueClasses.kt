e250d6ee55afa11afde185a70a4ae4b5bc0174e5
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2026-09-23 23:20:15 +0200

    [Tests] Reproduce Protobuf behaviour for full value classes
    #KT-84904 #KT-89643

---
+++ plugins/kotlinx-serialization/testData/diagnostics/ProtobufFullValueClasses.kt
@@ -0,0 +1,60 @@
+// LANGUAGE: +FullValueClasses
+// WITH_STDLIB
+
+// MODULE: lib
+// FILE: libtest.kt
+
+package kotlinx.serialization.protobuf
+
+import kotlinx.serialization.*
+
+public enum class ProtoIntegerType { DEFAULT, SIGNED, FIXED }
+
+@SerialInfo
+@Target(AnnotationTarget.PROPERTY)
+public annotation class ProtoType(public val type: ProtoIntegerType)
+
+// MODULE: main(lib)
+// FILE: test.kt
+
+import kotlinx.serialization.*
+import kotlinx.serialization.protobuf.*
+
+@Serializable
+@JvmInline
+value class InlineId(val raw: Long)
+
+@Serializable
+value class SingleFieldId(val raw: Long)
+
+@Serializable
+value class MultiFieldId(val high: Long, val low: Long)
+
+@Serializable
+value object ValueObjectId
+
+@Serializable
+class RegularId(val raw: Long)
+
+@Serializable
+object RegularObjectId
+
+@Serializable
+class Ids(
+    @ProtoType(ProtoIntegerType.FIXED) val inline: InlineId,
+    @ProtoType(ProtoIntegerType.FIXED) val singleField: SingleFieldId,
+    @ProtoType(ProtoIntegerType.FIXED) val multiField: MultiFieldId,
+    @ProtoType(ProtoIntegerType.FIXED) val valueObject: ValueObjectId,
+    <!PROTOBUF_ANNOTATION_INAPPLICABLE_TYPE!>@ProtoType(ProtoIntegerType.FIXED)<!> val regular: RegularId,
+    <!PROTOBUF_ANNOTATION_INAPPLICABLE_TYPE!>@ProtoType(ProtoIntegerType.FIXED)<!> val regularObject: RegularObjectId,
+)
+
+@Serializable
+class IdCollections(
+    @ProtoType(ProtoIntegerType.FIXED) val inlines: List<InlineId>,
+    @ProtoType(ProtoIntegerType.FIXED) val singleFields: List<SingleFieldId>,
+    @ProtoType(ProtoIntegerType.FIXED) val multiFields: List<MultiFieldId>,
+    <!PROTOBUF_ANNOTATION_INAPPLICABLE_TYPE!>@ProtoType(ProtoIntegerType.FIXED)<!> val regulars: List<RegularId>,
+    @ProtoType(ProtoIntegerType.FIXED) val multiFieldValues: Map<String, MultiFieldId>,
+    <!PROTOBUF_ANNOTATION_INAPPLICABLE_TYPE!>@ProtoType(ProtoIntegerType.FIXED)<!> val regularValues: Map<String, RegularId>,
+)
