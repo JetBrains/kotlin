47c684c59b7c68facd49e6694ce061d1dca6967e
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2026-09-23 21:40:27 +0200

    [Tests] Create reproducer for typed equals with full value classes
    #KT-84904 #KT-89642

---
+++ compiler/testData/diagnostics/tests/fullValueClasses/typedEqualsOperatorModifier.kt
@@ -0,0 +1,32 @@
+// RUN_PIPELINE_TILL: FRONTEND
+// WITH_STDLIB
+// RENDER_DIAGNOSTIC_ARGUMENTS
+// LANGUAGE: +FullValueClasses +CustomEqualsInValueClasses
+
+value class SingleField(val x: Int) {
+    <!INAPPLICABLE_OPERATOR_MODIFIER("must override 'equals()' in Any or define 'equals(other: SingleField): Boolean'")!>operator<!> fun equals(other: SingleField): Boolean = x == other.x
+}
+
+value class MultiField(val x: Int, val y: Int) {
+    <!INAPPLICABLE_OPERATOR_MODIFIER("must override 'equals()' in Any or define 'equals(other: MultiField): Boolean'")!>operator<!> fun equals(other: MultiField): Boolean = x == other.x && y == other.y
+}
+
+value class Generic<T>(val x: T) {
+    <!INAPPLICABLE_OPERATOR_MODIFIER("must override 'equals()' in Any or define 'equals(other: Generic<*>): Boolean'")!>operator<!> fun equals(other: Generic<*>): Boolean = x == other.x
+}
+
+abstract value class Abstract {
+    <!INAPPLICABLE_OPERATOR_MODIFIER("must override 'equals()' in Any or define 'equals(other: Abstract): Boolean'")!>operator<!> fun equals(other: Abstract): Boolean = true
+}
+
+value object ValueObject {
+    <!INAPPLICABLE_OPERATOR_MODIFIER("must override 'equals()' in Any or define 'equals(other: ValueObject): Boolean'")!>operator<!> fun equals(other: ValueObject): Boolean = true
+}
+
+@JvmInline
+value class Inline(val x: Int) {
+    operator fun equals(other: Inline): Boolean = x == other.x
+}
+
+/* GENERATED_FIR_TAGS: andExpression, capturedType, classDeclaration, equalityExpression, functionDeclaration,
+nullableType, objectDeclaration, operator, primaryConstructor, propertyDeclaration, starProjection, typeParameter, value */
