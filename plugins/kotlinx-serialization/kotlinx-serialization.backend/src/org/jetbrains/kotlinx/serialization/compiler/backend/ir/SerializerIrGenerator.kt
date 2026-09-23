JetBrains/kotlin

commit 88f8b24224bced682afc435f9ac87cc17f1f2c59

[Serialization] Remap serialized property types into the serializer's scope
Pass the serializer `irClass` into `serializeAllProperties` and add an
`IrSerializableProperty.typeInScope()` helper so each property type is
remapped onto the serializer's scope instead of leaking the serializable
class's type parameters.

KT-69305
KT-86224

plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerIrGenerator.kt
@@ -233,7 +233,8 @@ open class SerializerIrGenerator(
                     this@SerializerIrGenerator,
                     irFun.dispatchReceiverParameter!!,
                     property,
-                    cachedChildSerializerByIndex(index)
+                    cachedChildSerializerByIndex(index),
+                    property.getTypeInScope()
                 )
             ) { "Property ${property.name} must have a serializer" }
         }
@@ -338,7 +339,8 @@ open class SerializerIrGenerator(
             val ir = localSerializersFieldsDescriptors[it]
             irGetField(irGet(dispatchReceiver), ir.backingField!!)
         },
-        returnTypeHint
+        returnTypeHint,
+        substitutedPropertyType = property.getTypeInScope(),
     )
 
     // returns null: Any? for boxed types and 0: <number type> for primitives
@@ -403,7 +405,7 @@ open class SerializerIrGenerator(
         // var local0 = null, local1 = null ...
         val serialPropertiesMap = serializableProperties.mapIndexed { i, prop -> i to prop }.associate { [i, serializableProp] ->
             val ir = serializableProp.ir
-            val [expr, type] = defaultValueAndType(ir, serializableProp.type)
+            val [expr, type] = defaultValueAndType(ir, serializableProp.getTypeInScope())
             ir to irTemporary(expr, "local$i", type, isMutable = true)
         }
         // var transient0 = null, transient1 = null ...
@@ -445,7 +447,7 @@ open class SerializerIrGenerator(
                                                              it.owner.name.asString() == "${CallingConventions.decode}${sti.elementMethodPrefix}${CallingConventions.elementPostfix}" &&
                                                                      it.owner.hasShape(dispatchReceiver = true, regularParameters = 2)
                                                          } to listOf(localSerialDesc.get(), irInt(index))
-                                                     }, cachedChildSerializerByIndex(index), returnTypeHint = property.type)
+                                                     }, cachedChildSerializerByIndex(index), returnTypeHint = property.getTypeInScope())
                     // local$i = localInput.decode...(...)
                     +irSet(
                         serialPropertiesMap.getValue(property.ir).symbol,
@@ -628,6 +630,8 @@ open class SerializerIrGenerator(
         }
     }
 
+    protected fun IrSerializableProperty.getTypeInScope() =
+        getSubstitutedType(serializableIrClass, irClass)
 
     companion object {
         @OptIn(ValueClassBackendAgnosticApi::class)

plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/BaseIrGenerator.kt
@@ -189,16 +189,18 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
         cachedChildSerializerByIndex: (Int) -> IrExpression?,
         genericGetter: ((Int, IrType) -> IrExpression)?
     ) {
+        val ownerType = objectToSerialize.type
 
-        fun IrSerializableProperty.irGet(): IrExpression {
-            val ownerType = objectToSerialize.symbol.owner.type
-            val propertyType = this.type
-            return getProperty(irGet(ownerType, objectToSerialize.symbol), ir, propertyType)
-        }
+        fun IrSerializableProperty.irGet(substitutedPropertyType: IrType): IrExpression = getProperty(
+            receiver = irGet(ownerType, objectToSerialize.symbol),
+            property = ir,
+            expectedPropertyType = substitutedPropertyType
+        )
 
         for ([index, property] in serializableProperties.withIndex()) {
             if (index < ignoreIndexTo) continue
             // output.writeXxxElementValue(classDesc, index, value)
+            val substitutedPropertyType = property.getSubstitutedType(ownerType.classOrFail.owner, currentClass)
             val elementCall = formEncodeDecodePropertyCall(
                 irGet(localOutput),
                 property, { innerSerial, sti ->
@@ -208,17 +210,18 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
                         irGet(localSerialDesc),
                         irInt(index),
                         innerSerial,
-                        property.irGet()
+                        property.irGet(substitutedPropertyType)
                     )
                 }, {
                     val f =
                         kOutputClass.functionByName("${CallingConventions.encode}${it.elementMethodPrefix}${CallingConventions.elementPostfix}")
                     val args: MutableList<IrExpression> = mutableListOf(irGet(localSerialDesc), irInt(index))
-                    if (it.elementMethodPrefix != "Unit") args.add(property.irGet())
+                    if (it.elementMethodPrefix != "Unit") args.add(property.irGet(substitutedPropertyType))
                     f to args
                 },
                 cachedChildSerializerByIndex(index),
-                genericGetter
+                genericGetter,
+                substitutedPropertyType = substitutedPropertyType,
             )
 
             // check for call to .shouldEncodeElementDefault
@@ -230,7 +233,7 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
                 // emit call right away
                 +elementCall
             } else {
-                val partB = irNotEquals(property.irGet(), initializerAdapter(initializer))
+                val partB = irNotEquals(property.irGet(substitutedPropertyType), initializerAdapter(initializer))
 
                 val condition = if (encodeDefaults == false) {
                     // drop default without call to .shouldEncodeElementDefault
@@ -258,19 +261,20 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
         whenDoNot: (sti: IrSerialTypeInfo) -> FunctionWithArgs,
         cachedSerializer: IrExpression?,
         genericGetter: ((Int, IrType) -> IrExpression)? = null,
-        returnTypeHint: IrType? = null
+        returnTypeHint: IrType? = null,
+        substitutedPropertyType: IrType,
     ): IrExpression {
         val sti = getIrSerialTypeInfo(property, compilerContext)
         val innerSerial = cachedSerializer ?: serializerInstance(
             sti.serializer,
             compilerContext,
-            property.type,
+            substitutedPropertyType,
             property.genericIndex,
             property.ir.parentClassOrNull,
             genericGetter
         )
         val [functionToCall, args: List<IrExpression>] = if (innerSerial != null) whenHaveSerializer(innerSerial, sti) else whenDoNot(sti)
-        val typeArgs = if (functionToCall.owner.typeParameters.isNotEmpty()) listOf(property.type) else listOf()
+        val typeArgs = if (functionToCall.owner.typeParameters.isNotEmpty()) listOf(substitutedPropertyType) else listOf()
         return irInvoke(functionToCall, listOf(encoder) + args, typeArgs, returnTypeHint)
     }
 
@@ -332,7 +336,8 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
         generator: SerializerIrGenerator,
         dispatchReceiverParameter: IrValueParameter,
         property: IrSerializableProperty,
-        cachedSerializer: IrExpression?
+        cachedSerializer: IrExpression?,
+        substitutedPropertyType: IrType,
     ): IrExpression? {
         val nullableSerClass = compilerContext.finderForBuiltins().findProperties(SerialEntityNames.wrapIntoNullableCallableId).single()
 
@@ -349,7 +354,7 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
             serializerInstance(
                 serializerClassSymbol,
                 compilerContext,
-                property.type,
+                substitutedPropertyType,
                 genericIndex = property.genericIndex,
                 property.ir.parentClassOrNull,
             ) { it, _ ->
@@ -358,7 +363,7 @@ abstract class BaseIrGenerator(private val currentClass: IrClass, final override
             }
         }
 
-        return serializerExpression?.let { expr -> wrapWithNullableSerializerIfNeeded(property.type, expr, nullableSerClass) }
+        return serializerExpression?.let { expr -> wrapWithNullableSerializerIfNeeded(substitutedPropertyType, expr, nullableSerClass) }
     }
 
     context(irBuilder: IrBuilderWithScope) internal fun wrapWithNullableSerializerIfNeeded(

plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/IrSerializableProperties.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrProperty
 import org.jetbrains.kotlin.ir.types.IrSimpleType
+import org.jetbrains.kotlin.ir.types.IrType
 import org.jetbrains.kotlin.ir.util.*
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlinx.serialization.compiler.resolve.*
@@ -30,6 +31,9 @@ class IrSerializableProperty(
     val genericIndex = type.genericIndex
     fun serializableWith(ctx: SerializationBaseContext) =
         ir.annotations.serializableWith() ?: analyzeSpecialSerializers(ctx, ir.annotations)
+
+    fun getSubstitutedType(serializableClass: IrClass, originalClass: IrClass): IrType =
+        type.remapTypeParameters(serializableClass, originalClass)
 
     override val optional = !ir.annotations.hasAnnotation(SerializationAnnotations.requiredAnnotationFqName) && declaresDefaultValue
     override val transient = ir.annotations.hasAnnotation(SerializationAnnotations.serialTransientFqName) || !hasBackingField

plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerForInlineClassGenerator.kt
@@ -38,7 +38,7 @@ class SerializerForInlineClassGenerator(
         val inlineEncoder = irTemporary(encodeInlineCall, nameHint = "inlineEncoder")
 
         val property = serializableProperties.first()
-        val value = getProperty(irGet(saveFunc.parameters[2]), property.ir, property.type)
+        val value = getProperty(irGet(saveFunc.parameters[2]), property.ir, property.getTypeInScope())
 
         // inlineEncoder.encodeInt/String/SerializableValue
         val elementCall = formEncodeDecodePropertyCall(irGet(inlineEncoder), saveFunc.dispatchReceiverParameter!!, property, {innerSerial, sti ->
@@ -72,7 +72,7 @@ class SerializerForInlineClassGenerator(
         val inlineDecoder: IrExpression = irInvoke(decodeInline, irGet(loadFunc.parameters[1]), serialDescGetter)
 
         val property = serializableProperties.first()
-        val inlinedType = property.type
+        val inlinedType = property.getTypeInScope()
         val actualCall = formEncodeDecodePropertyCall(inlineDecoder, loadFunc.dispatchReceiverParameter!!, property, { innerSerial, sti ->
             decoderClass.functionByName( "${CallingConventions.decode}${sti.elementMethodPrefix}SerializableValue") to listOf(innerSerial)
         }, {

commit eb3c5b72c30e14092bfe15205d4548865fe5f2b6

[Serialization] Copy type parameter bounds onto generated serializers
The frontend declares `$serializer<T>` with an implicit `Any?` bound
because the bounds of the serializable class's type parameters are not
resolved yet when the nested class is generated, so once `88f8b24224bc`
retyped the `deserialize` locals into the serializer's scope, Wasm
erased them to `Any` while `Box.<init>` still expects the bound, which
produced a module that fails validation.

^RCA: Regression from `88f8b24224bc`, Wasm only. Before that commit the
serializer body referenced the serializable class's own type parameter,
an out-of-scope leak that happened to carry the right bound. Fixing the
leak exposed that the FIR extension generates `$serializer<T>` without
bounds, because they are unresolved at nested-class generation time.
Only Wasm turns a class type parameter bound into codegen (locals are
erased to the bound), so the JVM, JS and Native box tests, which do
cover generic classes with class upper bounds, stayed green, and the
`kotlinx-serialization` plugin has no Wasm test runner. IR dumps render
the old and the new `T` identically, so goldens could not show the
change either, and the introducing MR changed no tests.

Preventive steps: an IR-dump test that pins the bound on the generated
serializer and a multi-module box test that pins the klib signature of
the companion getter, since giving its type parameter the same bound
breaks cross-module linking (function type parameter bounds are mangled,
class ones are not). The fix itself was validated on Wasm manually.

Future steps: add a Wasm box runner for the serialization plugin, which
is the only test that reproduces the ticket's failure, and make the
frontend declare the same bounds as the serializable class on the
generated serializer and its companion getter, so that metadata,
IR and klib signatures describe one declaration; today the plugin API
requires an already resolved bound, and the class's bounds are not

plugins/kotlinx-serialization/kotlinx-serialization.backend/src/org/jetbrains/kotlinx/serialization/compiler/backend/ir/SerializerIrGenerator.kt
@@ -30,6 +30,7 @@ import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.Name
 import org.jetbrains.kotlin.platform.jvm.isJvm
 import org.jetbrains.kotlin.util.OperatorNameConventions
+import org.jetbrains.kotlin.utils.memoryOptimizedMap
 import org.jetbrains.kotlinx.serialization.compiler.extensions.SerializationPluginContext
 import org.jetbrains.kotlinx.serialization.compiler.resolve.CallingConventions
 import org.jetbrains.kotlinx.serialization.compiler.resolve.SerialEntityNames
@@ -640,6 +641,9 @@ open class SerializerIrGenerator(
             context: SerializationPluginContext,
         ) {
             val serializableDesc = getSerializableClassDescriptorBySerializer(irClass) ?: return
+            if (irClass.isFromPlugin()) {
+                irClass.copyTypeParameterBoundsFrom(serializableDesc)
+            }
             val generator = when {
                 serializableDesc.isEnumWithLegacyGeneratedSerializer() -> SerializerForEnumsGenerator(irClass, context)
                 serializableDesc.isInlineClass(treatCompatibleFullValueClassesAsInline = !context.platform.isJvm()) ->
@@ -653,6 +657,19 @@ open class SerializerIrGenerator(
             }
             irClass.addDefaultConstructorBodyIfAbsent(context)
             irClass.patchDeclarationParents(irClass.parent)
+        }
+
+        /**
+         * The frontend can't copy the bounds when it generates the serializer class, because they are not
+         * resolved yet at that point. Wasm erases type parameters to their bounds, so without them it emits
+         * an invalid call to the serializable class's constructor (KT-89275).
+         */
+        private fun IrClass.copyTypeParameterBoundsFrom(serializableIrClass: IrClass) {
+            for ([typeParameter, serializableTypeParameter] in typeParameters.zip(serializableIrClass.typeParameters)) {
+                typeParameter.superTypes = serializableTypeParameter.superTypes.memoryOptimizedMap {
+                    it.remapTypeParameters(serializableIrClass, this)
+                }
+            }
         }
     }
 }