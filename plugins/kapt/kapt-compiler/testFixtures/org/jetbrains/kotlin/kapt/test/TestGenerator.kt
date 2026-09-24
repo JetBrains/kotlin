a592abc43c257f159e2e0f2aa1c4d150043869c2
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-24 15:53:09 +0000

    [Kapt,Tests] Move generated tests in `kapt-compiler` to `build/`
    (Don't track them in git.)

--- plugins/kapt/kapt-compiler/testFixtures/org/jetbrains/kotlin/kapt/test/TestGenerator.kt
+++ plugins/kapt/kapt-compiler/testFixtures/org/jetbrains/kotlin/kapt/test/TestGenerator.kt
@@ -8,8 +8,9 @@ package org.jetbrains.kotlin.kapt.test
 import org.jetbrains.kotlin.generators.dsl.junit5.generateTestGroupSuiteWithJUnit5
 
 fun main(args: Array<String>) {
+    val testsRoot = args[0]
     generateTestGroupSuiteWithJUnit5(args) {
-        testGroup("plugins/kapt/kapt-compiler/tests-gen", "plugins/kapt/kapt-compiler/testData") {
+        testGroup(testsRoot, "plugins/kapt/kapt-compiler/testData") {
             testClass<AbstractIrKotlinKaptContextTest> {
                 model("kotlinRunner")
             }
