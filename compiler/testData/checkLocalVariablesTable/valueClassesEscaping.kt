de8e62dd44d628a1034a8c5ac0513f9db4e62446
Evgeniy Zhelenskiy <zhelenskiy2000@yandex.ru>
2026-09-24 19:35:30 +0000

    [JVM] Encode value class origin into names of unboxed parameters
    ^KT-73995

--- compiler/testData/checkLocalVariablesTable/valueClassesEscaping.kt
+++ compiler/testData/checkLocalVariablesTable/valueClassesEscaping.kt
@@ -13,11 +13,11 @@ class `$-$` {
 
 // METHOD : a_/$-$$$-.foo-sMXUCbs(IIIIIIII)V
 // VARIABLE : NAME=$$- TYPE=I
-// VARIABLE : NAME=$$-- TYPE=I
 // VARIABLE : NAME=$context-Int TYPE=I
-// VARIABLE : NAME=$context-UInt TYPE=I
-// VARIABLE : NAME=$this$foo TYPE=I
-// VARIABLE : NAME=arg0 TYPE=I
+// VARIABLE : NAME=$v$c$a_-$$--$$-$$--$-$this$foo TYPE=I
+// VARIABLE : NAME=$v$c$a_-$$--$$-$$--$-this TYPE=I
+// VARIABLE : NAME=$v$c$kotlin-UInt$-$$-- TYPE=I
+// VARIABLE : NAME=$v$c$kotlin-UInt$-$context-UInt TYPE=I
+// VARIABLE : NAME=$v$c$kotlin-UInt$-y TYPE=I
 // VARIABLE : NAME=arg0 TYPE=I
 // VARIABLE : NAME=x TYPE=I
-// VARIABLE : NAME=y TYPE=I
