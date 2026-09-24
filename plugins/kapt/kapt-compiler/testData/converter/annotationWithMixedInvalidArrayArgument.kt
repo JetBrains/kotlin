e356721b0b8c03c41dfd2c9b3bb140d94e86d13b
Grigorii.Solnyshkin <Grigorii.Solnyshkin@jetbrains.com>
2026-09-24 15:53:12 +0000

    [Tests] Drop `annotationWithMixedInvalidArgument` test in KAPT
    `Missing::class` is never `String`.

--- plugins/kapt/kapt-compiler/testData/converter/annotationWithMixedInvalidArrayArgument.kt
+++
@@ -1,8 +0,0 @@
-// WITH_STDLIB
-// IGNORE_COLLECTION_LITERALS_RESOLUTION
-@file:Suppress("UNRESOLVED_REFERENCE", "ANNOTATION_ARGUMENT_MUST_BE_CONST", "TYPE_MISMATCH")
-
-annotation class A(val value: Array<String>)
-
-@A(value = [Missing::class, "ok"])
-class Test
