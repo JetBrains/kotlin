JetBrains/kotlin

commit 5038534d6ebfcb3273728cc6531f0602442cc6b0

[Build] Deprecation of Kotlin DSL delegated properties
https://docs.gradle.org/9.7.1/userguide/upgrading_version_9.html#kotlin_dsl_delegated_properties

libraries/tools/kotlin-stdlib-docs/buildSrc/build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 plugins {
     `kotlin-dsl`
 }
-val dokka_version: String by project
+val dokka_version = project.property("dokka_version") as String
 
 dependencies {
     implementation(libs.kotlin.dokka)

dependencies/protobuf/build.gradle.kts
@@ -6,8 +6,10 @@ How to Publish
 3. Execute `./gradlew -p dependencies/protobuf publish -PkotlinSpaceUsername=usr -PkotlinSpacePassword=token`
  */
 
-val protobufVersion by extra("2.6.1")
-val publishedVersion by extra("2.6.1-2")
+val protobufVersion = "2.6.1"
+extra.set("protobufVersion", protobufVersion)
+val publishedVersion = "2.6.1-2"
+extra.set("publishedVersion", publishedVersion)
 
 allprojects {
     group = "org.jetbrains.kotlin"

dependencies/protobuf/protobuf-lite/build.gradle.kts
@@ -11,10 +11,10 @@ plugins {
     `maven-publish`
 }
 
-val relocatedProtobuf by configurations.creating
-val relocatedProtobufSources by configurations.creating
+val relocatedProtobuf = configurations.create("relocatedProtobuf")
+val relocatedProtobufSources = configurations.create("relocatedProtobufSources")
 
-val protobufVersion: String by rootProject.extra
+val protobufVersion = rootProject.extra["protobufVersion"] as String
 val outputJarPath = "$buildDir/libs/protobuf-lite-$protobufVersion.jar"
 val sourcesJarName = "protobuf-lite-$protobufVersion-sources.jar"
 
@@ -22,7 +22,7 @@ dependencies {
     relocatedProtobuf(project(":protobuf-relocated"))
 }
 
-val prepare by tasks.registering {
+val prepare = tasks.register("prepare") {
     inputs.files(relocatedProtobuf) // this also adds a dependency
     outputs.file(outputJarPath)
     doFirst {

dependencies/protobuf/protobuf-patches/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 
-val protobufVersion: String by rootProject.extra
+val protobufVersion = rootProject.extra["protobufVersion"] as String
 
 dependencies {
     implementation("com.google.protobuf:protobuf-java:$protobufVersion")

dependencies/protobuf/protobuf-relocated/build.gradle.kts
@@ -8,11 +8,11 @@ plugins {
 }
 
 
-val baseProtobuf by configurations.creating
-val baseProtobufSources by configurations.creating
-val protobufPatches by configurations.creating
+val baseProtobuf = configurations.create("baseProtobuf")
+val baseProtobufSources = configurations.create("baseProtobufSources")
+val protobufPatches = configurations.create("protobufPatches")
 
-val protobufVersion: String by rootProject.extra
+val protobufVersion = rootProject.extra["protobufVersion"] as String
 val protobufJarPrefix = "protobuf-$protobufVersion"
 
 val renamedSources = "$buildDir/renamedSrc/"

kotlin-native/backend.native/tests/samples/build.gradle.kts
@@ -2,11 +2,11 @@ buildscript {
     repositories {
         mavenCentral()
 
-        val kotlinCompilerRepo: String? by rootProject
+        val kotlinCompilerRepo = rootProject.findProperty("kotlinCompilerRepo") as String?
         kotlinCompilerRepo?.let { maven(it) }
     }
 
-    val kotlin_version: String by rootProject
+    val kotlin_version = rootProject.property("kotlin_version") as String
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
@@ -16,7 +16,7 @@ allprojects {
     repositories {
         mavenCentral()
 
-        val kotlinCompilerRepo: String? by rootProject
+        val kotlinCompilerRepo = rootProject.findProperty("kotlinCompilerRepo") as String?
         kotlinCompilerRepo?.let { maven(it) }
     }
 

kotlin-native/backend.native/tests/samples/tensorflow/build.gradle.kts
@@ -43,5 +43,5 @@ val downloadTensorflow = tasks.create("downloadTensorflow", Exec::class) {
     commandLine("./downloadTensorflow.sh")
 }
 
-val tensorflow: KotlinNativeTarget by kotlin.targets
+val tensorflow = kotlin.targets.getByName<KotlinNativeTarget>("tensorflow")
 tasks[tensorflow.compilations["main"].cinterops["tensorflow"].interopProcessingTaskName].dependsOn(downloadTensorflow)

kotlin-native/build-tools/build.gradle.kts
@@ -7,8 +7,8 @@ import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 buildscript {
-    @Suppress("DEPRECATION")
-    val rootBuildDirectory by extra(project.file("../.."))
+    val rootBuildDirectory = project.file("../..")
+    extra.set("rootBuildDirectory", rootBuildDirectory)
     apply(from = rootBuildDirectory.resolve("kotlin-native/gradle/loadRootProperties.gradle"))
 
     dependencies {

commit 716333dadaefc0e2000cd063a9e5f3ce0e6f0442

[stdlib, docs] Drop unused and missing property
Fixes a problem introduced in 5038534d6ebfcb3

libraries/tools/kotlin-stdlib-docs/buildSrc/build.gradle.kts
@@ -4,7 +4,6 @@ import org.gradle.kotlin.dsl.`kotlin-dsl`
 plugins {
     `kotlin-dsl`
 }
-val dokka_version = project.property("dokka_version") as String
 
 dependencies {
     implementation(libs.kotlin.dokka)